ede94b4842282511e805a4cb1c96325e082a5389
Johan Janssens <johan@nooku.org>
2005-10-19 17:31:34 +0000

    Added xml:lang and ltr attributes to html tag
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@563 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- administrator/templates/joomla_admin/index.php
+++ administrator/templates/joomla_admin/index.php
@@ -18,7 +18,7 @@ $tstart = mosProfiler::getmicrotime();
 
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" lang="<?php echo $_LANG->isoCode();?>">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $_LANG->isoCode();?>" lang="<?php echo $_LANG->isoCode();?>" dir="<?php echo $_LANG->rtl() ? 'rtl' : 'ltr'; ?>">
 <head>
 <title><?php echo $mosConfig_sitename; ?> - <?php echo $_LANG->_( 'Administration' ); ?> [Joomla]</title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
