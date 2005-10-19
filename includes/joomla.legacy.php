02404c02ceb8e8b80c97dc0283e89bb2a9cf1217
Johan Janssens <johan@nooku.org>
2005-10-19 18:19:27 +0000

    Initial import
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@566 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

---
+++ includes/joomla.legacy.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+* @version $Id:  $
+* @package Joomla
+* @copyright Copyright (C) 2005 Open Source Matters. All rights reserved.
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
+* Joomla! is free software. This version may have been modified pursuant
+* to the GNU General Public License, and as distributed it includes or
+* is derivative of works licensed under the GNU General Public License or
+* other free or open source software licenses.
+* See COPYRIGHT.php for copyright notices and details.
+*/
+
+/* _ISO defined not used anymore. All output is forced as utf-8 */
+DEFINE('_ISO','charset=utf-8');
+
+?>
