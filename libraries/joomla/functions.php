ab90da7636c956a29c072cdf997799221306680d
Rey Gigataras <stingrey@gmail.com>
2005-10-31 09:50:15 +0000

    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@766 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- libraries/joomla/functions.php
+++ libraries/joomla/functions.php
@@ -138,13 +138,9 @@ function mosBindArrayToObject( $array, &$obj, $ignore='', $prefix=NULL, $checkSl
 		return (false);
 	}
 
-	if ( $ignore == '' ) {
-		$ignore = array(0);
-	}
-	
 	foreach (get_object_vars($obj) as $k => $v) {
 		if( substr( $k, 0, 1 ) != '_' ) {			// internal attributes of an object are ignored
-			if ( !in_array( $k, $ignore ) ) {
+			if (strpos( $ignore, $k) === false) {
 				if ($prefix) {
 					$ak = $prefix . $k;
 				} else {
