0140bae16a1256c4b21c5b2fc6c475dae7804b05
Rey Gigataras <stingrey@gmail.com>
2005-10-31 01:46:33 +0000

    upgrade to Tinymce 2.0 RC4 [WIP]
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@730 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- mambots/editors/tinymce/jscripts/tiny_mce/plugins/emotions/jscripts/functions.js
+++
@@ -1,8 +0,0 @@
-function init() {
-	tinyMCEPopup.resizeToInnerSize();
-}
-
-function insertEmotion(file_name, title) {
-	tinyMCE.insertImage(tinyMCE.baseURL + "/plugins/emotions/images/" + file_name, tinyMCE.getLang(title));
-	tinyMCEPopup.close();
-}
--- mambots/editors/tinymce/jscripts/tiny_mce/plugins/zoom/langs/he.js
+++
@@ -1,5 +0,0 @@
-// HE lang variables by Liron Newman, http://eesh.net
-
-tinyMCE.addToLang('',{
-zoom_prefix : 'æ&aring;í'
-});
