f9b6fea2472b1653a5c1c3fac315940042fb2466
Johan Janssens <johan@nooku.org>
2005-11-02 00:13:29 +0000

    Only load tabs javascript onces
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@804 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- libraries/joomla/html.php
+++ libraries/joomla/html.php
@@ -794,12 +794,19 @@ class mosTabs {
 	function mosTabs( $useCookies, $xhtml=NULL ) {
 		global $mosConfig_live_site, $_LANG, $mainframe;
 		
+		if($mainframe->get( 'loadTabs')) {
+			return;
+		}
+
 		$css = $_LANG->rtl() ? 'tabpane_rtl.css' : 'tabpane.css';
 		
 		$mainframe->addCustomHeadTag( '<link rel="stylesheet" type="text/css" media="screen, projection" href="'.$mosConfig_live_site.'/includes/js/tabs/'.$css.'" id="luna-tab-style-sheet" />' );		
 		$mainframe->addCustomHeadTag( '<script type="text/javascript" src="'.$mosConfig_live_site.'/includes/js/tabs/tabpane_mini.js"></script>' );		
 
 		$this->useCookies = $useCookies;
+		
+		$mainframe->set( 'loadTabs', true );
+
 	}
 
 	/**
