34d563958f3c02e58da819132cb864ba408e48be
Johan Janssens <johan@nooku.org>
2005-11-02 15:11:28 +0000

    Revert revision 807
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@825 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- installation/sql/upgrade10to11.sql
+++ installation/sql/upgrade10to11.sql
@@ -11,6 +11,7 @@ INSERT INTO `#__mambots` VALUES (0, 'Search XML-RPC Bot', 'search.xmlrpcbot', 'x
 
 # Module Additions
 INSERT INTO `#__modules` VALUES (0, 'Footer', '', 1, 'footer', 0, '0000-00-00 00:00:00', 1, 'mod_footer', 0, 0, 1, '', 1, 0);
+INSERT INTO `#__modules` VALUES (0, 'Footer', '', 0, 'footer', 0, '0000-00-00 00:00:00', 1, 'mod_footer', 0, 0, 1, '', 1, 1);
 
 # Expand content title lengths
 ALTER TABLE `#__content` CHANGE `title` `title` varchar(255) NOT NULL default '';
