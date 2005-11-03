e89c513a2aa24570d960f18ae198b02d74b00afa
Johan Janssens <johan@nooku.org>
2005-11-03 02:20:45 +0000

    Added new JObject class
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@838 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

---
+++ libraries/joomla/classes/object.php
@@ -0,0 +1,84 @@
+<?php
+
+/**
+* @version $Id$
+* @package Joomla 
+* @copyright Copyright (C) 2005 Open Source Matters. All rights reserved.
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
+* Joomla! is free software. This version may have been modified pursuant
+* to the GNU General Public License, and as distributed it includes or
+* is derivative of works licensed under the GNU General Public License or
+* other free or open source software licenses.
+* See COPYRIGHT.php for copyright notices and details.
+*/
+
+/**
+ * Object class, allowing __construct and __destruct in PHP4.
+ *
+ * @package Joomla
+ * @subpackage 
+ * @since 1.1
+ */
+ 
+class JObject
+{
+	/**
+	 * A hack to support __construct() on PHP 4
+	 * Hint: descendant classes have no PHP4 class_name() constructors,
+	 * so this constructor gets called first and calls the top-layer __construct()
+	 * which (if present) should call parent::__construct()
+	 *
+	 * @return Object
+	 */
+	function Object()
+	{
+		$args = func_get_args();
+		register_shutdown_function(array(&$this, '__destruct'));
+		call_user_func_array(array(&$this, '__construct'), $args);
+	}
+
+	/**
+	 * Class constructor, overridden in descendant classes.
+	 */
+	function __construct() {}
+
+	/**
+	 * Class destructor, overridden in descendant classes.
+	 */
+	function __destruct() {}
+	
+	
+	/**
+	* @param string The name of the property
+	* @param mixed The value of the property to set
+	*/
+	function set( $property, $value=null ) {
+		$this->$property = $value;
+	}
+
+	/**
+	* @param string The name of the property
+	* @param mixed  The default value
+	* @return mixed The value of the property
+	*/
+	function get($property, $default=null) {
+		if(isset($this->$property)) {
+			return $this->$property;
+		} else {
+			return $default;
+		}
+	}
+
+	/**
+	 * Object-to-string conversion.
+	 * Each class can override it as necessary.
+	 *
+	 * @return string This name of this class
+	 */
+	function toString()
+	{
+		return get_class($this);
+	}
+}
+
+?>
