2f32900d621c5cd6b9dc172646fe546c3476e734
Samuel Moffatt <pasamio@gmail.com>
2005-11-03 05:55:02 +0000

    # Fixed up bug which prevented component uninstall if another XML file was in the directory
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@844 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- administrator/components/com_installer/component/component.class.php
+++ administrator/components/com_installer/component/component.class.php
@@ -292,6 +292,7 @@ class mosInstallerComponent extends mosInstaller {
 		$filesindir = mosReadDirectory( mosPathName( $mosConfig_absolute_path.'/administrator/components/'.$row->option ), '.xml$');
 		if (count($filesindir) > 0) {
 			$ismosinstall = false;
+			$found = 0;
 			foreach ($filesindir as $file) {
 				$xmlDoc = new DOMIT_Lite_Document();
 				$xmlDoc->resolveErrors( true );
@@ -301,10 +302,11 @@ class mosInstallerComponent extends mosInstaller {
 				$root = &$xmlDoc->documentElement;
 
 				if ($root->getTagName() != 'mosinstall') {
-					HTML_installer::showInstallMessage('XML File invalid',$_LANG->_( 'Uninstall - error' ),
-						$this->returnTo( $option, 'component', $client ) );
-					exit();
+//					HTML_installer::showInstallMessage('XML File invalid',$_LANG->_( 'Uninstall - error' ),
+//						$this->returnTo( $option, 'component', $client ) );
+					continue;
 				}
+				$found = 1;
 
 				$query_element = &$root->getElementsbyPath( 'uninstall/queries', 1 );
 				if(!is_null($query_element))
@@ -321,6 +323,12 @@ class mosInstallerComponent extends mosInstaller {
 						}
 					}
 				}
+				break;
+			}
+			if(!$found) {
+                                        HTML_installer::showInstallMessage('XML File invalid or not found',$_LANG->_( 'Uninstall - error' ),$this->returnTo( $option, 'component', $client ) );
+					exit();
+											
 			}
 		} else {
 			/*
