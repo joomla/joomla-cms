6c8d346a4e55b93c3f785b91c5901a4f05a747d8
Rey Gigataras <stingrey@gmail.com>
2005-11-03 13:44:23 +0000

    + modduleclass_sfx support to mod_poll  # Fixed artf1902 : Incorrect number of table cells in mod_poll
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@849 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- modules/mod_poll.php
+++ modules/mod_poll.php
@@ -18,18 +18,18 @@ if (!defined( '_MOS_POLL_MODULE' )) {
 	/** ensure that functions are declared only once */
 	define( '_MOS_POLL_MODULE', 1 );
 
-	function show_poll_vote_form( $Itemid ) {
+	function show_poll_vote_form( $Itemid, $moduleclass_sfx ) {
 		global $database;
 
 		$Itemid = mosGetParam( $_REQUEST, 'Itemid', 0 );
 
-		$query1 = "SELECT p.id, p.title"
+		$query = "SELECT p.id, p.title"
 		. "\n FROM #__polls AS p, #__poll_menu AS pm"
 		. "\n WHERE ( pm.menuid = $Itemid OR pm.menuid = 0 )"
 		. "\n AND p.id = pm.pollid"
 		. "\n AND p.published = 1";
 
-		$database->setQuery( $query1 );
+		$database->setQuery( $query );
 		$polls = $database->loadObjectList();
 
 		if($database->getErrorNum()) {
@@ -46,68 +46,74 @@ if (!defined( '_MOS_POLL_MODULE' )) {
 				. "\n AND text <> ''"
 				. "\n ORDER BY id";
 				$database->setQuery($query);
+				
 				if(!($options = $database->loadObjectList())) {
 					echo "MD ".$database->stderr(true);
 					return;
 				}
-				poll_vote_form_html( $poll, $options, $Itemid );
+				
+				poll_vote_form_html( $poll, $options, $Itemid, $moduleclass_sfx );
 			}
 		}
 	}
 
-	function poll_vote_form_html( &$poll, &$options, $Itemid ) {
+	function poll_vote_form_html( &$poll, &$options, $Itemid, $moduleclass_sfx ) {
 		global $_LANG;
 		
 		$tabclass_arr = array( 'sectiontableentry2', 'sectiontableentry1' );
 		$tabcnt = 0;
 		?>
 		<form name="form2" method="post" action="<?php echo sefRelToAbs("index.php?option=com_poll&amp;Itemid=$Itemid"); ?>">
-		<table width="95%" border="0" cellspacing="0" cellpadding="1" align="center">
-			<tr>
-				<td colspan="2" class="poll">
-					<b><?php echo $poll->title; ?></b>
-				</td>
-			</tr>
-			<tr>
-				<td align="center">
-					<table class="pollstableborder" cellspacing="0" cellpadding="0" border="0">
+		
+		<table width="95%" border="0" cellspacing="0" cellpadding="1" align="center" class="poll<?php echo $moduleclass_sfx; ?>">
+		<tr>
+			<td>
+				<b><?php echo $poll->title; ?></b>
+			</td>
+		</tr>
+		<tr>
+			<td align="center">
+				<table class="pollstableborder<?php echo $moduleclass_sfx; ?>" cellspacing="0" cellpadding="0" border="0">
+				<?php
+				for ($i=0, $n=count( $options ); $i < $n; $i++) { ?>
+							<tr>
+								<td class="<?php echo $tabclass_arr[$tabcnt]; ?><?php echo $moduleclass_sfx; ?>" valign="top">
+									<input type="radio" name="voteid" id="voteid<?php echo $options[$i]->id;?>" value="<?php echo $options[$i]->id;?>" alt="<?php echo $options[$i]->id;?>" />
+								</td>
+								<td class="<?php echo $tabclass_arr[$tabcnt]; ?><?php echo $moduleclass_sfx; ?>" valign="top">
+									<label for="voteid<?php echo $options[$i]->id;?>">
+										<?php echo $options[$i]->text; ?>
+									</label>
+								</td>
+							</tr>
 					<?php
-					for ($i=0, $n=count( $options ); $i < $n; $i++) { ?>
-								<tr>
-									<td class='<?php echo $tabclass_arr[$tabcnt]; ?>' valign="top">
-										<input type="radio" name="voteid" id="voteid<?php echo $options[$i]->id;?>" value="<?php echo $options[$i]->id;?>" alt="<?php echo $options[$i]->id;?>" />
-									</td>
-									<td class='<?php echo $tabclass_arr[$tabcnt]; ?>' valign="top">
-										<label for="voteid<?php echo $options[$i]->id;?>">
-											<?php echo $options[$i]->text; ?>
-										</label>
-									</td>
-								</tr>
-						<?php
-						if ($tabcnt == 1){
-							$tabcnt = 0;
-						} else {
-							$tabcnt++;
-						}
+					if ($tabcnt == 1){
+						$tabcnt = 0;
+					} else {
+						$tabcnt++;
 					}
-					?>
-					</table>
-				</td>
-			</tr>
-			<tr>
-				<td colspan="2" align="center">
-					<input type="submit" name="task_button" class="button" value="<?php echo $_LANG->_('Vote'); ?>" />&nbsp;&nbsp;
+				}
+				?>
+				</table>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<div align="center">
+					<input type="submit" name="task_button" class="button" value="<?php echo $_LANG->_('Vote'); ?>" />
+					&nbsp;
 					<input type="button" name="option" class="button" value="<?php echo $_LANG->_('Results'); ?>" onclick="document.location.href='<?php echo sefRelToAbs("index.php?option=com_poll&amp;task=results&amp;id=$poll->id"); ?>';" />
-				</td>
-			</tr>
+				</div>
+			</td>
+		</tr>
 		</table>
 
 		<input type="hidden" name="id" value="<?php echo $poll->id;?>" />
 		<input type="hidden" name="task" value="vote" />
-	</form>
-	<?php
+		</form>
+		<?php
 	}
 }
 
-show_poll_vote_form( $Itemid );
+show_poll_vote_form( $Itemid, $moduleclass_sfx );
 ?>
