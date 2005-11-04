a534c93a186121c6d308b25ebd8c904b27030762
Andrew Eddie <andrew.eddie@newlifeinit.com>
2005-11-04 03:51:05 +0000

    New classes
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@858 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

---
+++ libraries/joomla/tree.php
@@ -0,0 +1,194 @@
+<?php
+/**
+ * @version $Id: joomla.xml.php 618 2005-10-24 00:21:08Z Jinx $
+ * @package Joomla
+ * @copyright Copyright (C) 2005 Open Source Matters. All rights reserved.
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
+ * Joomla! is free software. This version may have been modified pursuant
+ * to the GNU General Public License, and as distributed it includes or
+ * is derivative of works licensed under the GNU General Public License or
+ * other free or open source software licenses.
+ * See COPYRIGHT.php for copyright notices and details.
+ */
+
+// ensure this file is being included by a parent file
+defined( '_VALID_MOS' ) or die( 'Restricted access' );
+
+/**
+ * @package Joomla
+ */
+class JTree {
+	/** @var string The name of the primary key */
+	var $idName = '';
+	/** @var string The name of the parent field */
+	var $parentName = '';
+	/** @var string The name of the text field to display */
+	var $textName = '';
+	/** @var array The data array of objects */
+	var $_data = null;
+	/** @var array Child relationships */
+	var $_children = null;
+	/** @var string Prefix for text value */
+	var $_prefix = '';
+	/** @var string The indent spacer */
+	var $_spacer = '';
+	/** @var int Maximum levels to process */
+	var $_maxLevel = 0;
+
+	/**
+	 * Constructor
+	 */
+	function JTree() {
+		$this->_maxLevel = 9999;
+	}
+
+	/**
+	 * @param array An array of data objects
+	 * @param string The name of the primary key or id field, defaults to id
+	 * @param string The name of the parent field, defaults to parent_id
+	 * @param string The name of the text field to display, defaults to name
+	 */
+	function importObjectList( &$data, $idName='id', $parentName='parent_id', $textName='name' ) {
+		$this->idName = $idName;
+		$this->parentName = $parentName;
+		$this->textName = $textName;
+
+		$this->_data = &$data;
+		$this->_maxLevel = 9999;
+
+		// preprocess children relationships
+		$this->_children = array();
+		foreach ($this->_data as $v) {
+			$this->addChild( $v->$parentName, $v );
+		}
+	}
+
+	/**
+	 * Add child data
+	 * @param mixed The id field for the parent item
+	 * @param mixed The data variable
+	 */
+	function addChild( $parent_id, &$data ) {
+		if (!isset( $this->_children[$parent_id] )) {
+			$this->_children[$parent_id] = array();
+		}
+		$this->_children[$parent_id][] = $data;
+	}
+
+	/**
+	 * Sets a predfined indent style
+	 * @param int 0=none, 1=dashed, 2=hooked
+	 */
+	function setIndentType( $type=0 ) {
+		switch ($type) {
+			case 0:
+				break;
+			case 1:
+				$this->_prefix = '- ';
+				$this->_spacer = '&nbsp;&nbsp;';
+				break;
+			case 2:
+				$this->_prefix = '<sup>L</sup>&nbsp;';
+				$this->_spacer = '.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
+				break;
+		}
+	}
+
+	/**
+	 * Manual sets the indent style
+	 * @param string The text prefix
+	 * @param string The spacer for the indent
+	 */
+	function setIntent( $prefix, $spacer ) {
+		$this->_prefix = $prefix;
+		$this->_spacer = $spacer;
+	}
+
+	/**
+	 * @param int The maximum levels to process
+	 */
+	function setMaxLevel( $maxLevel ) {
+		$this->_maxLevel = intval( $maxLevel );
+	}
+
+	/**
+	 * @param int The offset to slice the tree array
+	 * @param int The number of items to have in the returned array
+	 * @return array
+	 */
+	function process( $limitstart=0, $limit=0 ) {
+		$list = $this->_recurse();
+
+		if ($limitstart > 0 && $limit > 0) {
+			$list = array_slice( $list, $limitstart, $limit );
+		}
+
+		return $list;
+	}
+
+	/**
+	 * @return array
+	 */
+	function _recurse( $id=0, $indent='', $list=array(), $level=0 ) {
+		$idName = $this->idName;
+		$parentName = $this->parentName;
+		$textName = $this->textName;
+
+		if (@$this->_children[$id] && $level <= $this->_maxLevel) {
+			foreach ($this->_children[$id] as $v) {
+				$id = $v->$idName;
+				$text = $v->$textName;
+				$parent = $v->$parentName;
+
+				if ($parent > 0) {
+					$text = $this->_prefix . $text;
+				}
+
+				$list[$id] = $v;
+				$list[$id]->treename = $indent . $text;
+				$list[$id]->treelevel = $level;
+				$list[$id]->children = count( @$this->_children[$id] );
+				$list = $this->_recurse( $id, $indent . $this->_spacer, $list, $level+1 );
+			}
+		}
+		return $list;
+	}
+
+	/**
+	 * Render list in unordered list format with templates
+	 * @param object patTemplate object
+	 * @return string
+	 */
+	function toUL( &$tmpl ) {
+		return $this->_toUL( $tmpl );
+	}
+
+	/**
+	 * Recursive renderer for unordered lists
+	 * @param object patTemplate
+	 * @param mixed The array index of the current element
+	 * @param int The current level in the tree
+	 * @return string
+	 */
+	function _toUL( &$tmpl, $id=0, $level=0 ) {
+		$idName = $this->idName;
+		$html = '';
+
+		if (@$this->_children[$id] && $level <= $this->_maxLevel) {
+			$tmpl->clearTemplate( 'tree-open' );
+			$tmpl->addVar( 'tree-open', 'level', $level );
+			$html .= $tmpl->getParsedTemplate( 'tree-open' );
+			foreach ($this->_children[$id] as $v) {
+				$id = $v->$idName;
+
+				$tmpl->clearTemplate( 'tree-item' );
+				$tmpl->addObject( 'tree-item', $v );
+				$html .= $tmpl->getParsedTemplate( 'tree-item' );
+				$html .= $this->_render( $tmpl, $id, $level+1 );
+			}
+			$html .= $tmpl->getParsedTemplate( 'tree-close' );
+		}
+		return $html;
+	}
+}
+?>
