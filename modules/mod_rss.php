3aca3162242f718c5f572cfafa739da374a3fdb6
Rey Gigataras <stingrey@gmail.com>
2005-11-05 06:28:20 +0000

    ^ Separated newsfeed ability from custom/new module into its own module = `Newsfeed` [mod_rss.php]    Backward compatability retained for existing custom modules with newsfeed params
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@886 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- modules/mod_rss.php
+++ modules/mod_rss.php
@@ -14,149 +14,145 @@
 /** ensure this file is being included by a parent file */
 defined( '_VALID_MOS' ) or die( 'Direct Access to this location is not allowed.' );
 
-function modRSS( &$params ) {
-	global $mosConfig_absolute_path;
-	//check if cache diretory is writable as cache files will be created for the feed
-	$cacheDir = $mosConfig_absolute_path .'/cache/';
-	if ( !is_writable( $cacheDir ) ) {
-		echo '<div>';
-		echo $_LANG->_( 'Please make cache directory writable.' );
-		echo '</div>';
-		
-		exit;
+//check if cache diretory is writable as cache files will be created for the feed
+$cacheDir = $mosConfig_absolute_path .'/cache/';
+if ( !is_writable( $cacheDir ) ) {
+	echo '<div>';
+	echo $_LANG->_( 'Please make cache directory writable.' );
+	echo '</div>';
+	
+	exit;
+}
+
+// module params
+$moduleclass_sfx   	= $params->get( 'moduleclass_sfx' );
+$rssurl 			= $params->get( 'rssurl' );
+$rssitems 			= $params->get( 'rssitems', 5 );
+$rssdesc 			= $params->get( 'rssdesc', 1 );
+$rssimage 			= $params->get( 'rssimage', 1 );
+$rssitemdesc		= $params->get( 'rssitemdesc', 1 );
+$words 				= $params->def( 'word_count', 0 );
+$rsstitle			= $params->get( 'rsstitle', 1 );
+
+$cacheDir 			= $mosConfig_absolute_path .'/cache/';
+$LitePath 			= $mosConfig_absolute_path .'/includes/Cache/Lite.php';
+require_once( $mosConfig_absolute_path .'/includes/domit/xml_domit_rss.php' );
+
+$rssDoc = new xml_domit_rss_document();
+$rssDoc->useCacheLite(true, $LitePath, $cacheDir, 3600);
+$rssDoc->loadRSS( $rssurl );
+$totalChannels 	= $rssDoc->getChannelCount();
+
+for ( $i = 0; $i < $totalChannels; $i++ ) {
+	$currChannel =& $rssDoc->getChannel($i);
+	$elements 	= $currChannel->getElementList();
+	$iUrl		= 0;
+	foreach ( $elements as $element ) {
+		//image handling
+		if ( $element == 'image' ) {
+			$image =& $currChannel->getElement( DOMIT_RSS_ELEMENT_IMAGE );
+			$iUrl	= $image->getUrl();
+			$iTitle	= $image->getTitle();
+		}
 	}
-	
-	// module params
-	$moduleclass_sfx   	= $params->get( 'moduleclass_sfx' );
-	$rssurl 			= $params->get( 'rssurl' );
-	$rssitems 			= $params->get( 'rssitems', 5 );
-	$rssdesc 			= $params->get( 'rssdesc', 1 );
-	$rssimage 			= $params->get( 'rssimage', 1 );
-	$rssitemdesc		= $params->get( 'rssitemdesc', 1 );
-	$words 				= $params->def( 'word_count', 0 );
-	$rsstitle			= $params->get( 'rsstitle', 1 );
-	
-	$cacheDir 			= $mosConfig_absolute_path .'/cache/';
-	$LitePath 			= $mosConfig_absolute_path .'/includes/Cache/Lite.php';
-	require_once( $mosConfig_absolute_path .'/includes/domit/xml_domit_rss.php' );
-	
-	$rssDoc = new xml_domit_rss_document();
-	$rssDoc->useCacheLite(true, $LitePath, $cacheDir, 3600);
-	$rssDoc->loadRSS( $rssurl );
-	$totalChannels 	= $rssDoc->getChannelCount();
-	
-	for ( $i = 0; $i < $totalChannels; $i++ ) {
-		$currChannel =& $rssDoc->getChannel($i);
-		$elements 	= $currChannel->getElementList();
-		$iUrl		= 0;
-		foreach ( $elements as $element ) {
-			//image handling
-			if ( $element == 'image' ) {
-				$image =& $currChannel->getElement( DOMIT_RSS_ELEMENT_IMAGE );
-				$iUrl	= $image->getUrl();
-				$iTitle	= $image->getTitle();
-			}
-		}
-	
-		// feed title
-		?>
-		<table cellpadding="0" cellspacing="0" class="moduletable<?php echo $moduleclass_sfx; ?>">
-		<?php
-		// feed description
-		if ( $currChannel->getTitle() && $rsstitle ) {
-			?>
-			<tr>
-				<td>
-					<strong>
-					<a href="<?php echo ampReplace( $currChannel->getLink() ); ?>" target="_blank">
-						<?php echo $currChannel->getTitle(); ?></a>
-					</strong>
-				</td>
-			</tr>
-			<?php
-		}
-	
-		// feed description
-		if ( $rssdesc ) {
-			?>
-			<tr>
-				<td>
-					<?php echo $currChannel->getDescription(); ?>
-				</td>
-			</tr>
-			<?php
-		}
-	
-		// feed image
-		if ( $rssimage && $iUrl ) {
-			?>
-			<tr>
-				<td align="center">
-					<image src="<?php echo $iUrl; ?>" alt="<?php echo @$iTitle; ?>"/>
-				</td>
-			</tr>
-			<?php
-		}
-	
-		$actualItems = $currChannel->getItemCount();
-		$setItems = $rssitems;
-	
-		if ($setItems > $actualItems) {
-			$totalItems = $actualItems;
-		} else {
-			$totalItems = $setItems;
-		}
-	
+
+	// feed title
+	?>
+	<table cellpadding="0" cellspacing="0" class="moduletable<?php echo $moduleclass_sfx; ?>">
+	<?php
+	// feed description
+	if ( $currChannel->getTitle() && $rsstitle ) {
 		?>
 		<tr>
 			<td>
-				<ul class="newsfeed<?php echo $moduleclass_sfx; ?>">
-				<?php
-				for ($j = 0; $j < $totalItems; $j++) {
-					$currItem =& $currChannel->getItem($j);
-					// item title
-					?>
-					<li class="newsfeed<?php echo $moduleclass_sfx; ?>">
-						<strong>
-						<a href="<?php echo ampReplace( $currItem->getLink() ); ?>" target="_blank">
-	                        <?php echo str_replace('&apos;', "'", html_entity_decode( $currItem->getTitle() ) ); ?></a>
-						</strong>
-						<?php
-						// item description
-						if ( $rssitemdesc ) {
-							// item description
-							$text = html_entity_decode( $currItem->getDescription() );
-	                        $text = str_replace('&apos;', "'", $text);
-	
-							// word limit check
-							if ( $words ) {
-								$texts = explode( ' ', $text );
-								$count = count( $texts );
-								if ( $count > $words ) {
-									$text = '';
-									for( $i=0; $i < $words; $i++ ) {
-										$text .= ' '. $texts[$i];
-									}
-									$text .= '...';
-								}
-							}
-							?>
-							<div>
-								<?php echo $text; ?>
-							</div>
-							<?php
-						}
-						?>
-					</li>
-					<?php
-				}
-				?>
-				</ul>
+				<strong>
+				<a href="<?php echo ampReplace( $currChannel->getLink() ); ?>" target="_blank">
+					<?php echo $currChannel->getTitle(); ?></a>
+				</strong>
 			</td>
 		</tr>
-		</table>
 		<?php
 	}
-}
 
-modRSS( $params );
+	// feed description
+	if ( $rssdesc ) {
+		?>
+		<tr>
+			<td>
+				<?php echo $currChannel->getDescription(); ?>
+			</td>
+		</tr>
+		<?php
+	}
+
+	// feed image
+	if ( $rssimage && $iUrl ) {
+		?>
+		<tr>
+			<td align="center">
+				<image src="<?php echo $iUrl; ?>" alt="<?php echo @$iTitle; ?>"/>
+			</td>
+		</tr>
+		<?php
+	}
+
+	$actualItems = $currChannel->getItemCount();
+	$setItems = $rssitems;
+
+	if ($setItems > $actualItems) {
+		$totalItems = $actualItems;
+	} else {
+		$totalItems = $setItems;
+	}
+
+	?>
+	<tr>
+		<td>
+			<ul class="newsfeed<?php echo $moduleclass_sfx; ?>">
+			<?php
+			for ($j = 0; $j < $totalItems; $j++) {
+				$currItem =& $currChannel->getItem($j);
+				// item title
+				?>
+				<li class="newsfeed<?php echo $moduleclass_sfx; ?>">
+					<strong>
+					<a href="<?php echo ampReplace( $currItem->getLink() ); ?>" target="_blank">
+                        <?php echo str_replace('&apos;', "'", html_entity_decode( $currItem->getTitle() ) ); ?></a>
+					</strong>
+					<?php
+					// item description
+					if ( $rssitemdesc ) {
+						// item description
+						$text = html_entity_decode( $currItem->getDescription() );
+                        $text = str_replace('&apos;', "'", $text);
+
+						// word limit check
+						if ( $words ) {
+							$texts = explode( ' ', $text );
+							$count = count( $texts );
+							if ( $count > $words ) {
+								$text = '';
+								for( $i=0; $i < $words; $i++ ) {
+									$text .= ' '. $texts[$i];
+								}
+								$text .= '...';
+							}
+						}
+						?>
+						<div>
+							<?php echo $text; ?>
+						</div>
+						<?php
+					}
+					?>
+				</li>
+				<?php
+			}
+			?>
+			</ul>
+		</td>
+	</tr>
+	</table>
+	<?php
+}
+?>
