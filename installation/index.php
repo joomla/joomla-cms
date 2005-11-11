259b3937d7d9765c4391563bd7d9d192e99a6b8d
Johan Janssens <johan@nooku.org>
2005-11-11 02:08:46 +0000

    Added Content-Type header
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@973 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- installation/index.php
+++ installation/index.php
@@ -22,6 +22,7 @@ require_once( 'includes/installation.php' );
 
 header( 'Cache-Control: no-cache, must-revalidate');	// HTTP/1.1
 header( 'Pragma: no-cache');	// HTTP/1.0
+header(' Content-Type: text/html; charset=UTF-8');
 
 $vars = mosGetParam( $_POST, 'vars', array() );
 $mosConfig_lang = mosGetParam( $vars, 'lang', detectLanguage() );
