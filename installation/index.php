7232f5651594407e6e5f3dac60b7c347a031346a
Johan Janssens <johan@nooku.org>
2005-11-11 01:39:41 +0000

    Installation alterations, backported Mambo 4.5.3 installation
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@969 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

---
+++ installation/index.php
@@ -0,0 +1,64 @@
+<?php
+/**
+* @version $Id$
+* @package Joomla
+* @subpackage Installation
+* @copyright Copyright (C) 2005 Open Source Matters. All rights reserved.
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
+* Joomla! is free software and parts of it may contain or be derived from the
+* GNU General Public License or other free or open source software licenses.
+* See COPYRIGHT.php for copyright notices and details.
+*/
+
+define( '_VALID_MOS', 1 );
+
+//if (file_exists( '../configuration.php' ) && filesize( '../configuration.php' ) > 10) {
+//	header( 'Location: ../index.php' );
+//	exit();
+//}
+
+// include support libraries
+require_once( 'includes/joomla.php' );
+
+header( 'Cache-Control: no-cache, must-revalidate');	// HTTP/1.1
+header( 'Pragma: no-cache');	// HTTP/1.0
+
+$vars = mosGetParam( $_POST, 'vars', array() );
+$mosConfig_lang = mosGetParam( $vars, 'lang', detectLanguage() );
+
+$task = mosGetParam( $_REQUEST, 'task', '' );
+
+switch ($task) {
+	case 'preinstall':
+		installationTasks::preInstall();
+		break;
+
+	case 'license':
+		installationTasks::license();
+		break;
+
+	case 'dbconfig':
+		installationTasks::dbConfig();
+		break;
+
+	case 'makedb':
+		if (installationTasks::makeDB()) {
+			installationTasks::mainConfig( 1 );
+		}
+		break;
+
+	case 'mainconfig':
+		installationTasks::mainConfig();
+		break;
+
+	case 'saveconfig':
+		$buffer = installationTasks::saveConfig();
+		installationTasks::finish( $buffer );
+		break;
+
+	case 'lang':
+	default:
+		installationTasks::chooseLanguage();
+		break;
+}
+?>
