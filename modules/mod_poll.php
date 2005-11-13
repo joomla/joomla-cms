7b7249ff08258dc84708189a411e081fbceb60e6
Rey Gigataras <stingrey@gmail.com>
2005-11-13 18:05:22 +0000

    mod_poll sql vunerability
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@1011 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- modules/mod_poll.php
+++ modules/mod_poll.php
@@ -21,7 +21,7 @@ if (!defined( '_JOS_POLL_MODULE' )) {
 	function show_poll_vote_form( $Itemid, $moduleclass_sfx ) {
 		global $database;
 
-		$Itemid = mosGetParam( $_REQUEST, 'Itemid', 0 );
+		$Itemid = intval( mosGetParam( $_REQUEST, 'Itemid', 0 ) );
 
 		$query = "SELECT p.id, p.title"
 		. "\n FROM #__polls AS p, #__poll_menu AS pm"
