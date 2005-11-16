3a48ed99a56eb13c61283df60e71f3b2e8b73742
Johan Janssens <johan@nooku.org>
2005-11-16 23:37:25 +0000

    Fixed xml prolog rendering
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@1066 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- templates/rhuk_solarflare_ii/index.php
+++ templates/rhuk_solarflare_ii/index.php
@@ -1,7 +1,7 @@
 <?php
 defined( '_VALID_MOS' ) or die( 'Restricted access' );
+echo '<?xml version="1.0" encoding="UTF-8"?' .'>';
 ?>
-<?xml version="1.0"?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
