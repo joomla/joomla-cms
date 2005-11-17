d0822e2bd3120ce4d012f571456f370d259f978b
Andrew Eddie <andrew.eddie@newlifeinit.com>
2005-11-17 03:48:04 +0000

    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@1075 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

---
+++ libraries/joomla/models/model.php
@@ -0,0 +1,580 @@
+<?php
+
+/**
+* @version $Id: category.php 1037 2005-11-14 17:59:40Z Jinx $
+* @package Joomla 
+* @copyright Copyright (C) 2005 Open Source Matters. All rights reserved.
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
+* Joomla! is free software. This version may have been modified pursuant
+* to the GNU General Public License, and as distributed it includes or
+* is derivative of works licensed under the GNU General Public License or
+* other free or open source software licenses.
+* See COPYRIGHT.php for copyright notices and details.
+*/
+
+jimport( 'joomla.classes.object' );
+
+/**
+ * JModel Abstract Class.
+ * @abstract
+ * @package Joomla
+ * @subpackage Database
+ * Parent classes to all models.  Customisation will generally
+ * not involve tampering with this object.
+ * @author Andrew Eddie <eddieajau@users.sourceforge.net
+ * @since 1.1
+ */
+class JModel extends JObject {
+	/** @var string Name of the table in the db schema relating to child class */
+	var $_tbl		= '';
+	/** @var string Name of the primary key field in the table */
+	var $_tbl_key	= '';
+	/** @var string Error message */
+	var $_error		= '';
+	/** @var mosDatabase Database connector */
+	var $_db		= null;
+
+	/**
+	*	Object constructor to set table and key field
+	*
+	*	Can be overloaded/supplemented by the child class
+	*	@param string $table name of the table in the db schema relating to child class
+	*	@param string $key name of the primary key field in the table
+	*/
+	function __construct( $table, $key, &$db ) {
+		$this->_tbl		= $table;
+		$this->_tbl_key	= $key;
+		$this->_db		=& $db;
+	}
+	/**
+	 * Filters public properties
+	 * @access protected
+	 * @param array List of fields to ignore
+	 */
+	function filter( $ignoreList=null ) {
+		$ignore = is_array( $ignoreList );
+
+		$iFilter = new InputFilter();
+		foreach ($this->getPublicProperties() as $k) {
+			if ($ignore && in_array( $k, $ignoreList ) ) {
+				continue;
+			}
+			$this->$k = $iFilter->process( $this->$k );
+		}
+	}
+	/**
+	 *	@return string Returns the error message
+	 */
+	function getError() {
+		return $this->_error;
+	}
+	
+	/**
+	 * Returns an array of public properties
+	 * @return array
+	 */
+	function getPublicProperties() {
+		static $cache = null;
+		if (is_null( $cache )) {
+			$cache = array();
+			foreach (get_class_vars( get_class( $this ) ) as $key=>$val) {
+				if (substr( $key, 0, 1 ) != '_') {
+					$cache[] = $key;
+				}
+			}
+		}
+		return $cache;
+	}
+	
+	/**
+	*	binds a named array/hash to this object
+	*
+	*	can be overloaded/supplemented by the child class
+	*	@param array $hash named array
+	*	@return null|string	null is operation was satisfactory, otherwise returns an error
+	*/
+	function bind( $array, $ignore="" ) {
+		if (!is_array( $array )) {
+			$this->_error = strtolower(get_class( $this ))."::bind failed.";
+			return false;
+		} else {
+			return mosBindArrayToObject( $array, $this, $ignore );
+		}
+	}
+
+	/**
+	*	binds an array/hash to this object
+	*	@param int $oid optional argument, if not specifed then the value of current key is used
+	*	@return any result from the database operation
+	*/
+	function load( $oid=null ) {
+		$k = $this->_tbl_key;
+		if ($oid !== null) {
+			$this->$k = $oid;
+		}
+		$oid = $this->$k;
+		if ($oid === null) {
+			return false;
+		}
+		$this->_db->setQuery( "SELECT * FROM $this->_tbl WHERE $this->_tbl_key='$oid'" );
+		return $this->_db->loadObject( $this );
+	}
+
+	/**
+	*	generic check method
+	*
+	*	can be overloaded/supplemented by the child class
+	*	@return boolean True if the object is ok
+	*/
+	function check() {
+		return true;
+	}
+
+	/**
+	* Inserts a new row if id is zero or updates an existing row in the database table
+	*
+	* Can be overloaded/supplemented by the child class
+	* @param boolean If false, null object variables are not updated
+	* @return null|string null if successful otherwise returns and error message
+	*/
+	function store( $updateNulls=false ) {
+		$k = $this->_tbl_key;
+		global $migrate;
+		if( $this->$k && !$migrate) {
+			$ret = $this->_db->updateObject( $this->_tbl, $this, $this->_tbl_key, $updateNulls );
+		} else {
+			$ret = $this->_db->insertObject( $this->_tbl, $this, $this->_tbl_key );
+		}
+		if( !$ret ) {
+			$this->_error = strtolower(get_class( $this ))."::store failed <br />" . $this->_db->getErrorMsg();
+			return false;
+		} else {
+			return true;
+		}
+	}
+	/**
+	*/
+	function move( $dirn, $where='' ) {
+		$k = $this->_tbl_key;
+
+		$sql = "SELECT $this->_tbl_key, ordering FROM $this->_tbl";
+
+		if ($dirn < 0) {
+			$sql .= "\n WHERE ordering < $this->ordering";
+			$sql .= ($where ? "\n	AND $where" : '');
+			$sql .= "\n ORDER BY ordering DESC";
+			$sql .= "\n LIMIT 1";
+		} else if ($dirn > 0) {
+			$sql .= "\n WHERE ordering > $this->ordering";
+			$sql .= ($where ? "\n	AND $where" : '');
+			$sql .= "\n ORDER BY ordering";
+			$sql .= "\n LIMIT 1";
+		} else {
+			$sql .= "\nWHERE ordering = $this->ordering";
+			$sql .= ($where ? "\n AND $where" : '');
+			$sql .= "\n ORDER BY ordering";
+			$sql .= "\n LIMIT 1";
+		}
+
+		$this->_db->setQuery( $sql );
+//echo 'A: ' . $this->_db->getQuery();
+
+
+		$row = null;
+		if ($this->_db->loadObject( $row )) {
+			$query = "UPDATE $this->_tbl"
+			. "\n SET ordering = '$row->ordering'"
+			. "\n WHERE $this->_tbl_key = '". $this->$k ."'"
+			;
+			$this->_db->setQuery( $query );
+
+			if (!$this->_db->query()) {
+				$err = $this->_db->getErrorMsg();
+				die( $err );
+			}
+//echo 'B: ' . $this->_db->getQuery();
+
+			$query = "UPDATE $this->_tbl"
+			. "\n SET ordering = '$this->ordering'"
+			. "\n WHERE $this->_tbl_key = '". $row->$k. "'"
+			;
+			$this->_db->setQuery( $query );
+//echo 'C: ' . $this->_db->getQuery();
+
+			if (!$this->_db->query()) {
+				$err = $this->_db->getErrorMsg();
+				die( $err );
+			}
+
+			$this->ordering = $row->ordering;
+		} else {
+			$query = "UPDATE $this->_tbl"
+			. "\n SET ordering = '$this->ordering'"
+			. "\n WHERE $this->_tbl_key = '". $this->$k ."'"
+			;
+			$this->_db->setQuery( $query );
+//echo 'D: ' . $this->_db->getQuery();
+
+
+			if (!$this->_db->query()) {
+				$err = $this->_db->getErrorMsg();
+				die( $err );
+			}
+		}
+	}
+	/**
+	* Compacts the ordering sequence of the selected records
+	* @param string Additional where query to limit ordering to a particular subset of records
+	*/
+	function updateOrder( $where='' ) {
+		$k = $this->_tbl_key;
+
+		if (!array_key_exists( 'ordering', get_class_vars( strtolower(get_class( $this )) ) )) {
+			$this->_error = "WARNING: ".strtolower(get_class( $this ))." does not support ordering.";
+			return false;
+		}
+
+		if ($this->_tbl == "#__content_frontpage") {
+			$order2 = ", content_id DESC";
+		} else {
+			$order2 = "";
+		}
+
+		$query = "SELECT $this->_tbl_key, ordering"
+		. "\n FROM $this->_tbl"
+		. ( $where ? "\n WHERE $where" : '' )
+		. "\n ORDER BY ordering$order2 "
+		;
+		$this->_db->setQuery( $query );
+		if (!($orders = $this->_db->loadObjectList())) {
+			$this->_error = $this->_db->getErrorMsg();
+			return false;
+		}
+		// first pass, compact the ordering numbers
+		for ($i=0, $n=count( $orders ); $i < $n; $i++) {
+			if ($orders[$i]->ordering >= 0) {
+				$orders[$i]->ordering = $i+1;
+			}
+		}
+
+		$shift = 0;
+		$n=count( $orders );
+		for ($i=0; $i < $n; $i++) {
+			//echo "i=$i id=".$orders[$i]->$k." order=".$orders[$i]->ordering;
+			if ($orders[$i]->$k == $this->$k) {
+				// place 'this' record in the desired location
+				$orders[$i]->ordering = min( $this->ordering, $n );
+				$shift = 1;
+			} else if ($orders[$i]->ordering >= $this->ordering && $this->ordering > 0) {
+				$orders[$i]->ordering++;
+			}
+		}
+	//echo '<pre>';print_r($orders);echo '</pre>';
+		// compact once more until I can find a better algorithm
+		for ($i=0, $n=count( $orders ); $i < $n; $i++) {
+			if ($orders[$i]->ordering >= 0) {
+				$orders[$i]->ordering = $i+1;
+				$query = "UPDATE $this->_tbl"
+				. "\n SET ordering = '". $orders[$i]->ordering ."'"
+				. "\n WHERE $k = '". $orders[$i]->$k ."'"
+				;
+				$this->_db->setQuery( $query);
+				$this->_db->query();
+	//echo '<br />'.$this->_db->getQuery();
+			}
+		}
+
+		// if we didn't reorder the current record, make it last
+		if ($shift == 0) {
+			$order = $n+1;
+			$query = "UPDATE $this->_tbl"
+			. "\n SET ordering = '$order'"
+			. "\n WHERE $k = '". $this->$k ."'"
+			;
+			$this->_db->setQuery( $query );
+			$this->_db->query();
+	//echo '<br />'.$this->_db->getQuery();
+		}
+		return true;
+	}
+	/**
+	*	Generic check for whether dependancies exist for this object in the db schema
+	*
+	*	can be overloaded/supplemented by the child class
+	*	@param string $msg Error message returned
+	*	@param int Optional key index
+	*	@param array Optional array to compiles standard joins: format [label=>'Label',name=>'table name',idfield=>'field',joinfield=>'field']
+	*	@return true|false
+	*/
+	function canDelete( $oid=null, $joins=null ) {
+		$k = $this->_tbl_key;
+		if ($oid) {
+			$this->$k = intval( $oid );
+		}
+		if (is_array( $joins )) {
+			$select = "$k";
+			$join = "";
+			foreach( $joins as $table ) {
+				$select .= ",\n COUNT(DISTINCT {$table['idfield']}) AS {$table['idfield']}";
+				$join .= "\n LEFT JOIN {$table['name']} ON {$table['joinfield']} = $k";
+			}
+
+			$query = "SELECT $select"
+			. "\n FROM $this->_tbl"
+			. $join
+			. "\n WHERE $k = ". $this->$k .""
+			. "\n GROUP BY $k"
+			;
+			$this->_db->setQuery( $query );
+
+			if ($obj = $this->_db->loadObject()) {
+				$this->_error = $this->_db->getErrorMsg();
+				return false;
+			}
+			$msg = array();
+			foreach( $joins as $table ) {
+				$k = $table['idfield'];
+				if ($obj->$k) {
+					$msg[] = $AppUI->_( $table['label'] );
+				}
+			}
+
+			if (count( $msg )) {
+				$this->_error = "noDeleteRecord" . ": " . implode( ', ', $msg );
+				return false;
+			} else {
+				return true;
+			}
+		}
+
+		return true;
+	}
+
+	/**
+	*	Default delete method
+	*
+	*	can be overloaded/supplemented by the child class
+	*	@return true if successful otherwise returns and error message
+	*/
+	function delete( $oid=null ) {
+		//if (!$this->canDelete( $msg )) {
+		//	return $msg;
+		//}
+
+		$k = $this->_tbl_key;
+		if ($oid) {
+			$this->$k = intval( $oid );
+		}
+
+		$query = "DELETE FROM $this->_tbl"
+		. "\n WHERE $this->_tbl_key = '". $this->$k ."'"
+		;
+		$this->_db->setQuery( $query );
+
+		if ($this->_db->query()) {
+			return true;
+		} else {
+			$this->_error = $this->_db->getErrorMsg();
+			return false;
+		}
+	}
+
+	function checkout( $who, $oid=null ) {
+		if (!array_key_exists( 'checked_out', get_class_vars( strtolower(get_class( $this )) ) )) {
+			$this->_error = "WARNING: ".strtolower(get_class( $this ))." does not support checkouts.";
+			return false;
+		}
+		$k = $this->_tbl_key;
+		if ($oid !== null) {
+			$this->$k = $oid;
+		}
+		$time = date( 'Y-m-d H:i:s' );
+		if (intval( $who )) {
+			// new way of storing editor, by id
+			$query = "UPDATE $this->_tbl"
+			. "\n SET checked_out = $who, checked_out_time = '$time'"
+			. "\n WHERE $this->_tbl_key = '". $this->$k ."'"
+			;
+			$this->_db->setQuery( $query );
+
+			$this->checked_out = $who;
+			$this->checked_out_time = $time;
+		} else {
+			// old way of storing editor, by name
+			$query = "UPDATE $this->_tbl"
+			. "\n SET checked_out = 1, checked_out_time = '$time', editor = '".$who."' "
+			. "\n WHERE $this->_tbl_key = '". $this->$k ."'"
+			;
+			$this->_db->setQuery( $query );
+
+			$this->checked_out = 1;
+			$this->checked_out_time = $time;
+			$this->checked_out_editor = $who;
+		}
+
+		return $this->_db->query();
+	}
+
+	function checkin( $oid=null ) {
+		if (!array_key_exists( 'checked_out', get_class_vars( strtolower(get_class( $this )) ) )) {
+			$this->_error = "WARNING: ".strtolower(get_class( $this ))." does not support checkin.";
+			return false;
+		}
+		$k = $this->_tbl_key;
+		if ($oid !== null) {
+			$this->$k = $oid;
+		}
+		$time = date( 'H:i:s' );
+		$query = "UPDATE $this->_tbl"
+		. "\n SET checked_out = 0, checked_out_time = '$this->_db->_nullDate'"
+		. "\n WHERE $this->_tbl_key = '". $this->$k ."'"
+		;
+		$this->_db->setQuery( $query );
+
+		$this->checked_out = 0;
+		$this->checked_out_time = '';
+
+		return $this->_db->query();
+	}
+
+	function hit( $oid=null ) {
+		global $mosConfig_enable_log_items;
+
+		$k = $this->_tbl_key;
+		if ($oid !== null) {
+			$this->$k = intval( $oid );
+		}
+
+		$query = "UPDATE $this->_tbl"
+		. "\n SET hits = ( hits + 1 )"
+		. "\n WHERE $this->_tbl_key = '$this->id'"
+		;
+		$this->_db->setQuery( $query );
+		$this->_db->query();
+
+		if (@$mosConfig_enable_log_items) {
+			$now = date( 'Y-m-d' );
+			$query = "SELECT hits"
+			. "\n FROM #__core_log_items"
+			. "\n WHERE time_stamp = '$now'"
+			. "\n AND item_table = '$this->_tbl'"
+			. "\n AND item_id = ". $this->$k .""
+			;
+			$this->_db->setQuery( $query );
+			$hits = intval( $this->_db->loadResult() );
+			if ($hits) {
+				$query = "UPDATE #__core_log_items"
+				. "\n SET hits = ( hits + 1 )"
+				. "\n WHERE time_stamp = '$now'"
+				. "\n AND item_table = '$this->_tbl'"
+				. "\n AND item_id = ".$this->$k.""
+				;
+				$this->_db->setQuery( $query );
+				$this->_db->query();
+			} else {
+				$query = "INSERT INTO #__core_log_items"
+				. "\n VALUES ( '$now', '$this->_tbl', ". $this->$k .", 1 )"
+				;
+				$this->_db->setQuery( $query );
+				$this->_db->query();
+			}
+		}
+	}
+
+	/**
+	 * Tests if item is checked out
+	 * @param int A user id
+	 * @return boolean
+	 */
+	function isCheckedOut( $user_id=0 ) {
+		if ($user_id) {
+			return ($this->checked_out && $this->checked_out <> $user_id);
+		} else {
+			return $this->checked_out;
+		}
+	}
+
+	/**
+	* Generic save function
+	* @param array Source array for binding to class vars
+	* @param string Filter for the order updating
+	* @returns TRUE if completely successful, FALSE if partially or not succesful.
+	*/
+	function save( $source, $order_filter ) {
+		if (!$this->bind( $_POST )) {
+			return false;
+		}
+		if (!$this->check()) {
+			return false;
+		}
+		if (!$this->store()) {
+			return false;
+		}
+		if (!$this->checkin()) {
+			return false;
+		}
+		$filter_value = $this->$order_filter;
+		$this->updateOrder( $order_filter ? "`$order_filter` = '$filter_value'" : '' );
+		$this->_error = '';
+		return true;
+	}
+
+	/**
+	* Generic Publish/Unpublish function
+	* @param array An array of id numbers
+	* @param integer 0 if unpublishing, 1 if publishing
+	* @param integer The id of the user performnig the operation
+	*/
+	function publish_array( $cid=null, $publish=1, $myid=0 ) {
+		if (!is_array( $cid ) || count( $cid ) < 1) {
+			$this->_error = "No items selected.";
+			return false;
+		}
+
+		$cids = implode( ',', $cid );
+
+		$query = "UPDATE $this->_tbl"
+		. "\n SET published = " . intval( $publish )
+		. "\n WHERE $this->_tbl_key IN ( $cids )"
+		. "\n AND ( checked_out = 0 OR ( checked_out = $myid ) )"
+		;
+		$this->_db->setQuery( $query );
+		if (!$this->_db->query()) {
+			$this->_error = $this->_db->getErrorMsg();
+			return false;
+		}
+
+		if (count( $cid ) == 1) {
+			$this->checkin( $cid[0] );
+		}
+		$this->_error = '';
+		return true;
+	}
+
+	/**
+	* Export item list to xml
+	* @param boolean Map foreign keys to text values
+	*/
+	function toXML( $mapKeysToText=false ) {
+		$xml = '<record table="' . $this->_tbl . '"';
+
+		if ($mapKeysToText) {
+			$xml .= ' mapkeystotext="true"';
+		}
+		$xml .= '>';
+		foreach (get_object_vars( $this ) as $k => $v) {
+			if (is_array($v) or is_object($v) or $v === NULL) {
+				continue;
+			}
+			if ($k[0] == '_') { // internal field
+				continue;
+			}
+			$xml .= '<' . $k . '><![CDATA[' . $v . ']]></' . $k . '>';
+		}
+		$xml .= '</record>';
+
+		return $xml;
+	}
+}
+?>
+
