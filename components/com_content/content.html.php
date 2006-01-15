b54b175d6775c10aa82e6f28c1223de07d0459b9
Louis Landry <louis.landry@joomla.org>
2006-01-15 18:35:57 +0000

    Small fixes to content component
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@1844 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- components/com_content/content.html.php
+++ components/com_content/content.html.php
@@ -562,7 +562,7 @@ class JContentView
 		if ($params->get('item_title') || $params->get('pdf') || $params->get('print') || $params->get('email'))
 		{
 			// link used by print button
-			$print_link = '/index2.php?option=com_content&amp;task=view&amp;id='.$row->id.'&amp;Itemid='.$Itemid.'&amp;pop=1&amp;page='.@ $page;
+			$print_link = $mainframe->getCfg('live_site').'/index2.php?option=com_content&amp;task=view&amp;id='.$row->id.'&amp;Itemid='.$Itemid.'&amp;pop=1&amp;page='.@ $page;
 			?>
 			<table class="contentpaneopen<?php echo $params->get( 'pageclass_sfx' ); ?>">
 			<tr>
--- components/com_content/content.php
+++ components/com_content/content.php
@@ -22,7 +22,9 @@ require_once (JApplicationHelper :: getPath('front_html', 'com_content'));
  * Get some variables
  */
 global $Itemid;
-$now = date('Y-m-d H:i', time() + $mainframe->getCfg('offset') * 60 * 60);
+$now 		= date('Y-m-d H:i', time() + $mainframe->getCfg('offset') * 60 * 60);
+$limit		= JRequest::getVar('limit', 1, '', 'int');
+$limitstart	= JRequest::getVar('limitstart', 0, '', 'int');
 
 // Editor access object
 $access = new stdClass();
