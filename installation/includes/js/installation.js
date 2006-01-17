5598f30d76ea921c7ebc986d8eddafd8db51fd11
Johan Janssens <johan@nooku.org>
2006-01-17 23:10:30 +0000

    Removed advanced setting checkboxes
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@1890 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- installation/includes/js/installation.js
+++ installation/includes/js/installation.js
@@ -19,34 +19,4 @@ function getElementByName( f, name ) {
 function submitForm( frm, task ) {
 	frm.task.value = task;
 	frm.submit();
-}
-function changeFilePermsMode( mode ) {
-    if(document.getElementById) {
-        switch (mode) {
-            case 0:
-                document.getElementById('filePermsFlags').style.display = 'none';
-                document.getElementById('filePermsMode0').checked = true;
-                document.getElementById('filePermsMode1').checked = false;
-                break;
-            default:
-                document.getElementById('filePermsFlags').style.display = '';
-                document.getElementById('filePermsMode0').checked = false;
-                document.getElementById('filePermsMode1').checked = true;
-        } // switch
-    } // if
-}
-function changeDirPermsMode( mode ) {
-    if(document.getElementById) {
-        switch (mode) {
-            case 0:
-                document.getElementById('dirPermsFlags').style.display = 'none';
-                document.getElementById('dirPermsMode0').checked = true;
-                document.getElementById('dirPermsMode1').checked = false;
-                break;
-            default:
-                document.getElementById('dirPermsFlags').style.display = '';
-                document.getElementById('dirPermsMode0').checked = false;
-                document.getElementById('dirPermsMode1').checked = true;
-        } // switch
-    } // if
 }
