42dd8754b0c85c71a5ffff9cef5f16c65470bcb0
Emir Sakic <emir.sakic@gmail.com>
2006-01-26 19:54:28 +0000

    Supressed iconv notice
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@1988 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- libraries/phputf8/iconv/core.php
+++ libraries/phputf8/iconv/core.php
@@ -29,10 +29,11 @@ function utf8_strrpos($str, $search, $offset = FALSE){
 * Wrapper around iconv_substr
 */
 function utf8_substr($str, $offset, $length = FALSE){
-    
+
 	if ( $length === FALSE ) {
-        return iconv_substr($str, $offset);
+		return iconv_substr($str, $offset);
     } else {
-       return iconv_substr($str, $offset, $length);
+    	// supressed for now due to the problems in PHP 5.0
+		return @iconv_substr($str, $offset, $length);
     }
 }
