cf5c50bbfa23cf9125e96fce20449aa148f0a3ad
Johan Janssens <johan@nooku.org>
2006-02-02 23:33:43 +0000

    Moved deldir form compat/functions to prevent function collisions
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@2230 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- administrator/components/com_installer/installer.class.php
+++ administrator/components/com_installer/installer.class.php
@@ -25,4 +25,27 @@ class mosInstaller extends JInstaller
 	}
 }
 
+/**
+ * Legacy function, use JFolder::delete($path)
+ * 
+ * @deprecated	As of version 1.1
+ */
+function deldir( $dir ) 
+{
+	$current_dir = opendir( $dir );
+	$old_umask = umask(0);
+	while ($entryname = readdir( $current_dir )) {
+		if ($entryname != '.' and $entryname != '..') {
+			if (is_dir( $dir . $entryname )) {
+				deldir( mosPathName( $dir . $entryname ) );
+			} else {
+                @chmod($dir . $entryname, 0777);
+				unlink( $dir . $entryname );
+			}
+		}
+	}
+	umask($old_umask);
+	closedir( $current_dir );
+	return rmdir( $dir );
+}
 ?>
