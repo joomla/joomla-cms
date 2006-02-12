c4d058c0fe85be5ea9c5eef589e2898aaa063ece
Rey Gigataras <stingrey@gmail.com>
2006-02-12 08:25:57 +0000

    # Fixed artf3438 : RSS Feed Created it not base on the same encoding of the content
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@2302 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- libraries/bitfolge/feedcreator.class.php
+++ libraries/bitfolge/feedcreator.class.php
@@ -408,7 +408,7 @@ class UniversalFeedCreator extends FeedCreator {
 		$vars = get_object_vars($this);
 		foreach ($vars as $key => $value) {
 			// prevent overwriting of properties "contentType", "encoding"; do not copy "_feed" itself
-			if (!in_array($key, array("_feed", "contentType", "encoding"))) {
+			if (!in_array($key, array("_feed", "contentType"))) {
 				$this->_feed->{$key} = $this->{$key};
 			}
 		}
