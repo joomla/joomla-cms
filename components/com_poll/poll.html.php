602cb1f836ee93973ec96786cda79dd2e244d45f
Rey Gigataras <stingrey@gmail.com>
2006-03-06 16:46:44 +0000

    xhtml fixes
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@2679 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- components/com_poll/poll.html.php
+++ components/com_poll/poll.html.php
@@ -122,7 +122,7 @@ class poll_html {
 		$sumval = array_sum( $data_arr["hits"] );
 		?>
 		<br />
-		<table class='pollstableborder' cellspacing="0" cellpadding="0" border="0">
+		<table class="pollstableborder" cellspacing="0" cellpadding="0" border="0">
 		<tr>
 			<td colspan="2" class="sectiontableheader">
 				<img src="components/com_poll/images/poll.png" align="middle" border="0" width="12" height="14" alt="" />
