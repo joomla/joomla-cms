33c92fb3b1651f394f28757876499720321a6b04
Johan Janssens <johan@nooku.org>
2006-03-06 15:54:00 +0000

    Reworked moo.fx behavior
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@2676 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- installation/template/js/moo.fx.effect.js
+++ installation/template/js/moo.fx.effect.js
@@ -1,21 +1,66 @@
-function init_moofx(){
+fx.Slide = Class.create();
+fx.Slide.prototype = {
+	setOptions: function(options) {
+		this.options = {
+			delay: 50,
+			opacity: false
+		}
+		Object.extend(this.options, options || {});
+	},
 
-	var stretchers = document.getElementsByClassName('section-smenu'); //div that stretches
-	var toggles = document.getElementsByClassName('title-smenu'); //h3s where I click on
+	initialize: function(togglers, sliders, options) {
+		this.sliders = sliders;
+		this.setOptions(options);
+		sliders.each(function(el, i){
+			options.onComplete = function(){
+				if (el.offsetHeight > 0) el.style.height = '1%';
+			}
+			el.fx = new fx.Combo(el, options);
+			el.fx.hide();
+		});
 
-	//accordion effect
-	var smenuAccordion = new fx.Accordion(
-	toggles, stretchers, {opacity: true, duration: 400}
-	);
+		togglers.each(function(toggler, i){
+			toggler.onclick = function(){
+				this.toggle(sliders[i], toggler);
+			}.bind(this);
+		}.bind(this));
+	},
+
+	toggle: function(slider, toggler){
+		if (slider.offsetHeight == 0) {
+			setTimeout(function(){this.clear(slider);}.bind(this), this.options.delay);
+			Element.addClassName(toggler, 'moofx-toggler-down');
+		}
+		
+		if (slider.offsetHeight > 0) {
+			setTimeout(function(){this.clear(slider);}.bind(this), this.options.delay);
+				Element.removeClassName(toggler, 'moofx-toggler-down');
+		}
+	},
+
+	clear: function(slider){
+		slider.fx.clearTimer();
+		slider.fx.toggle();
+	}
+}
+
+function init_moofx()
+{
+	var sliders  = document.getElementsByClassName('moofx-slider'); 	//div that stretches
+	var togglers = document.getElementsByClassName('moofx-toggler'); 	//h3s where I click on
+
+	var slide = new fx.Slide(togglers, sliders, {opacity: true, duration: 400});
 
 	//hash functions
 	var found = false;
-	toggles.each(function(h3, i){
-		var div = Element.find(h3, 'nextSibling'); //element.find is located in prototype.lite
-		if (window.location.href.indexOf(h3.title) > 0) {
-			smenuAccordion.showThisHideOpen(div);
-			found = true;
-		}
+	togglers.each(function(h3, i)
+	{
+			var div = Element.find(h3, 'nextSibling'); //element.find is located in prototype.lite
+			if (window.location.href.indexOf(h3.title) > 0) 
+			{
+					slide.toggle(div);
+					found = true;
+			}
 	});
-	if (!found) smenuAccordion.showThisHideOpen(stretchers[0]);
+	if (!found) slide.toggle(stretchers[0]);
 }
