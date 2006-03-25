7bbd76e5ebef0f64a5b189c79692aa4283e2ec00
Johan Janssens <johan@nooku.org>
2006-03-25 15:33:07 +0000

    Removed PCL library, already present in the framework.
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@2907 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- administrator/includes/pcl/pclerror.lib.php
+++
@@ -1,3 +0,0 @@
-<?php // compatibility
-require_once( dirname(__FILE__)  .'/../../../libraries/pcl/pclerror.lib.php' );
-?>
--- administrator/includes/pcl/pcltrace.lib.php
+++
@@ -1,3 +0,0 @@
-<?php // compatibility
-require_once( dirname(__FILE__)  .'/../../../libraries/pcl/pcltrace.lib.php' );
-?>
