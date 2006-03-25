184e047dc4ab374cf87a7c3d2e139629bbde6a46
Johan Janssens <johan@nooku.org>
2006-03-25 18:28:13 +0000

    Concatenated define.php and framework.php into application.php
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@2910 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- administrator/index3.php
+++ administrator/index3.php
@@ -16,7 +16,6 @@ define( '_JEXEC', 1 );
 
 define('JPATH_BASE', dirname(__FILE__) );
 
-require_once ( JPATH_BASE .'/includes/defines.php'     );
 require_once ( JPATH_BASE .'/includes/application.php' );
 require_once ( JPATH_BASE .'/includes/template.php'    );
 
--- includes/application.php
+++ includes/application.php
@@ -14,7 +14,99 @@
 // no direct access
 defined( '_JEXEC' ) or die( 'Restricted access' );
 
-require_once( JPATH_BASE.'/includes/framework.php' );
+/**
+* Joomla! Application define
+*/
+
+//Global definitions
+define( 'DS', DIRECTORY_SEPARATOR );
+
+//Joomla framework paht definitions
+$parts = explode( DS, JPATH_BASE );
+
+//Defines
+define( 'JPATH_ROOT'         , implode( DS, $parts ) );
+
+define( 'JPATH_SITE'         , JPATH_ROOT );
+define( 'JPATH_CONFIGURATION', JPATH_ROOT );
+define( 'JPATH_ADMINISTRATOR', JPATH_ROOT . DS . 'administrator' );
+define( 'JPATH_LIBRARIES'    , JPATH_ROOT . DS . 'libraries' );
+define( 'JPATH_INSTALLATION' , JPATH_ROOT . DS . 'installation' );
+
+/**
+ * Joomla! system checks
+ */
+
+@set_magic_quotes_runtime( 0 );
+
+if (!file_exists( JPATH_CONFIGURATION . DS . 'configuration.php' ) || (filesize( JPATH_CONFIGURATION . DS . 'configuration.php' ) < 10)) {
+	header( 'Location: installation/index.php' );
+	exit();
+}
+
+if (in_array( 'globals', array_keys( array_change_key_case( $_REQUEST, CASE_LOWER ) ) ) ) {
+	die( 'Fatal error.  Global variable hack attempted.' );
+}
+if (in_array( '_post', array_keys( array_change_key_case( $_REQUEST, CASE_LOWER ) ) ) ) {
+	die( 'Fatal error.  Post variable hack attempted.' );
+}
+
+/**
+ * Joomla! system startup
+ */
+
+//System includes
+require_once( JPATH_SITE      	  . DS . 'globals.php' );
+require_once( JPATH_CONFIGURATION . DS . 'configuration.php' );
+require_once( JPATH_LIBRARIES 	  . DS . 'loader.php' );
+
+//System configuration
+$CONFIG = new JConfig();
+
+if (@$CONFIG->error_reporting === 0) {
+	error_reporting( 0 );
+} else if (@$CONFIG->error_reporting > 0) {
+	error_reporting( $CONFIG->error_reporting );
+}
+
+define('JDEBUG', $CONFIG->debug); 
+
+unset($CONFIG);
+
+//System profiler
+if(JDEBUG) {
+	jimport('joomla.utilities.profiler');
+	$_PROFILER =& JProfiler::getInstance('Application');
+}
+
+/**
+ * Joomla! framework loading
+ */
+
+//Joomla library imports
+jimport( 'joomla.common.compat.compat' );
+
+jimport( 'joomla.version' );
+jimport( 'joomla.utilities.functions' );
+jimport( 'joomla.utilities.error');
+jimport( 'joomla.application.user.user' );
+jimport( 'joomla.application.environment.session' );
+jimport( 'joomla.application.environment.request' );
+jimport( 'joomla.database.table' );
+jimport( 'joomla.presentation.html' );
+jimport( 'joomla.factory' );
+jimport( 'joomla.presentation.parameter.parameter' );
+jimport( 'joomla.i18n.language' );
+jimport( 'joomla.i18n.string' );
+jimport( 'joomla.application.event' );
+jimport( 'joomla.application.extension.plugin' );
+jimport( 'joomla.application.application');
+jimport( 'joomla.application.menu' );
+
+// support for legacy classes & functions that will be depreciated
+jimport( 'joomla.common.legacy.*' );
+
+JDEBUG ? $_PROFILER->mark('afterLoadFramework') : null;
 
 /**
 * Joomla! Application class
