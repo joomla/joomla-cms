d852baddfd4de130c7726c807756a30a55f82b42
Johan Janssens <johan@nooku.org>
2006-04-18 02:08:11 +0000

    Cleanup
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@3150 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- templates/rhuk_milkyway/index.php
+++ templates/rhuk_milkyway/index.php
@@ -118,16 +118,12 @@ See COPYRIGHT.php for copyright notices and details.
 											</jdoc:sub>
 										</jdoc:tmpl>
 									</td>
-									<?php 
-									if(mosCountModules('right') && $task != 'edit' ) { 
-										?>
+									<jdoc:exists type="modules" condition="right" >
 										<td class="greyline">&nbsp;</td>
 										<td width="170">
 											<jdoc:include type="modules" name="right" style="-2"/>
 										</td>
-										<?php 
-									} 
-									?>
+									</jdoc:exists>
 								</tr>
 							</table>
 
