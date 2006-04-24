a56abc80fc5f1a794b09014fca8cab29e5c74b3b
Andy Miller <rhuk@mac.com>
2006-04-24 03:58:49 +0000

    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@3239 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- administrator/components/com_modules/toolbar.modules.html.php
+++ administrator/components/com_modules/toolbar.modules.html.php
@@ -21,7 +21,7 @@ class TOOLBAR_modules {
 	* Draws the menu for a New module
 	*/
 	function _NEW($client)	{
-		JMenuBar::title( JText::_( 'New Module' ).': <small><small>['. JText::_( $client->name ) .']</small></small>', 'module.png' );
+		JMenuBar::title( JText::_( 'Module Manager' ) . ' - ' . JText::_( 'New' ), 'module.png' );
 		JMenuBar::customX( 'edit', 'next.png', 'next_f2.png', 'Next', true );
 		JMenuBar::cancel();
 		JMenuBar::help( 'screen.modules.new' );
@@ -35,7 +35,7 @@ class TOOLBAR_modules {
 
 		$moduleType = JRequest::getVar( 'module' );
 
-		JMenuBar::title( JText::_( 'Module' ) .' <small>'. JText::_( $client->name ) .'</small>: <small><small>[ Edit ]</small></small>', 'module.png' );
+		JMenuBar::title( JText::_( 'Module Manager' ) .' - ' . JText::_( 'Edit' ), 'module.png' );
 
 		if($moduleType == 'custom') {
 			JMenuBar::Preview('index3.php?option=com_modules&client='.$client.'&pollid='.$id);
