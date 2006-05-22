54e0565cf55617adedacc459ae20fb50f0e50d40
David Gal <davidgal123@gmail.com>
2006-05-22 22:13:34 +0000

    bug fix
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@3599 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- installation/sql/joomla.sql
+++ installation/sql/joomla.sql
@@ -376,6 +376,12 @@ CREATE TABLE `#__menu` (
 
 INSERT INTO `#__menu` VALUES (1, 'mainmenu', 'Home', 'index.php?option=com_frontpage', 'components', 1, 0, 10, 0, 1, 0, '0000-00-00 00:00:00', 0, 0, 0, 3, 'leading=1\r\nintro=2\r\nlink=1\r\nimage=1\r\npage_title=0\r\nheader=Welcome to the Frontpage\r\norderby_sec=front\r\nprint=0\r\npdf=0\r\nemail=0\r\nback_button=0', '', '', '', '', '0', '0');
 
+# --------------------------------------------------------
+
+#
+# Table structure for table `#__menu_types`
+#
+
 CREATE TABLE `#__menu_types` (
   `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
   `menutype` VARCHAR(75) NOT NULL DEFAULT '',
