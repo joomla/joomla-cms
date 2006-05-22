ef74c6e4f94984a3b7be4d62cbe9e90010667011
Andrew Eddie <andrew.eddie@newlifeinit.com>
2006-05-22 07:59:37 +0000

    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@3592 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

---
+++ libraries/joomla/database/table/menutypes.php
@@ -0,0 +1,50 @@
+<?php
+/**
+* @version $Id: menu.php 3497 2006-05-15 02:35:23Z eddieajau $
+* @package Joomla
+* @copyright Copyright (C) 2005 - 2006 Open Source Matters. All rights reserved.
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
+* Joomla! is free software. This version may have been modified pursuant
+* to the GNU General Public License, and as distributed it includes or
+* is derivative of works licensed under the GNU General Public License or
+* other free or open source software licenses.
+* See COPYRIGHT.php for copyright notices and details.
+*/
+
+/**
+ * Menu Types table
+ *
+ * @package 	Joomla.Framework
+ * @subpackage 	Model
+ * @since		1.0
+ */
+class JTableMenuTypes extends JTable
+{
+	/** @var int Primary key */
+	var $id					= null;
+	/** @var string */
+	var $menutype			= null;
+
+	/**
+	 * Constructor
+	 *
+	 * @access protected
+	 * @param database A database connector object
+	 */
+	function __construct( &$db ) {
+		parent::__construct( '#__menu_types', 'id', $db );
+	}
+
+	/**
+	 * @return boolean
+	 */
+	function check()
+	{
+		if (strstr($this->menutype, '\'')) {
+			$this->_error( JText::_( 'The menu name cannot contain a \'', true ) );
+			return false;
+		}
+		return true;	
+	}
+}
+?>
