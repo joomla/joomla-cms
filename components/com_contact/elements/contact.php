2ff7d98da7ca8667b5048609490cd299c9b07a92
Andrew Eddie <andrew.eddie@newlifeinit.com>
2006-05-25 08:20:04 +0000

    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@3658 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

---
+++ components/com_contact/elements/contact.php
@@ -0,0 +1,55 @@
+<?php
+/**
+* @version $Id: category.php 3222 2006-04-24 01:49:01Z webImagery $
+* @package Joomla
+* @copyright Copyright (C) 2005 - 2006 Open Source Matters. All rights reserved.
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
+* Joomla! is free software. This version may have been modified pursuant
+* to the GNU General Public License, and as distributed it includes or
+* is derivative of works licensed under the GNU General Public License or
+* other free or open source software licenses.
+* See COPYRIGHT.php for copyright notices and details.
+*/
+
+/**
+ * Renders a category element
+ *
+ * @author 		Johan Janssens <johan.janssens@joomla.org>
+ * @package 	Joomla.Framework
+ * @subpackage 	Parameter
+ * @since		1.5
+ */
+
+class JElement_Contact extends JElement
+{
+   /**
+	* Element name
+	*
+	* @access	protected
+	* @var		string
+	*/
+	var	$_name = 'Contact';
+
+	function fetchElement($name, $value, &$node, $control_name)
+	{
+		global $database;
+
+		$scope = $node->attributes('scope');
+		if (!isset ($scope)) {
+			$scope = 'content';
+		}
+
+		if ($scope == 'content') {
+			// This might get a conflict with the dynamic translation - TODO: search for better solution
+			$query = "SELECT c.id, CONCAT_WS( '/',s.title, c.title ) AS title"."\n FROM #__categories AS c"."\n LEFT JOIN #__sections AS s ON s.id=c.section"."\n WHERE c.published = 1"."\n AND s.scope = '$scope'"."\n ORDER BY c.title";
+		} else {
+			$query = "SELECT c.id, c.title"."\n FROM #__categories AS c"."\n WHERE c.published = 1"."\n AND c.section = '$scope'"."\n ORDER BY c.title";
+		}
+		$database->setQuery($query);
+		$options = $database->loadObjectList();
+		array_unshift($options, mosHTML::makeOption('0', '- '.JText::_('Select Category').' -', 'id', 'title'));
+
+		return mosHTML::selectList($options, ''.$control_name.'['.$name.']', 'class="inputbox"', 'id', 'title', $value, $control_name.$name );
+	}
+}
+?>
