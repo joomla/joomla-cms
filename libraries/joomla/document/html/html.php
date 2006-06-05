d37d7221e8bd419aeb70dd19c24a5028c1eb1e33
Andrew Eddie <andrew.eddie@newlifeinit.com>
2006-06-05 09:49:19 +0000

    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@3868 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- libraries/joomla/document/html/html.php
+++ libraries/joomla/document/html/html.php
@@ -102,6 +102,7 @@ class JDocumentHTML extends JDocument
      */
     function addFavicon($href, $type = 'image/x-icon', $relation = 'shortcut icon')
 	{
+        $href = str_replace( '\\', '/', $href );
         $this->_links[] = "<link href=\"$href\" rel=\"$relation\" type=\"$type\"";
     }
 
