d58eee598523502ab458f00572490abbc778231d
Louis Landry <louis.landry@joomla.org>
2006-06-13 12:06:32 +0000

    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@4020 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

---
+++ libraries/joomla/presentation/parameter/element/password.php
@@ -0,0 +1,40 @@
+<?php
+/**
+* @version $Id: text.php 4013 2006-06-13 08:53:28Z webImagery $
+* @package Joomla
+* @copyright Copyright (C) 2005 - 2006 Open Source Matters. All rights reserved.
+* @license GNU/GPL, see LICENSE.php
+* Joomla! is free software. This version may have been modified pursuant
+* to the GNU General Public License, and as distributed it includes or
+* is derivative of works licensed under the GNU General Public License or
+* other free or open source software licenses.
+* See COPYRIGHT.php for copyright notices and details.
+*/
+
+/**
+ * Renders a password element
+ *
+ * @author 		Louis Landry <louis.landry@joomla.org>
+ * @package 	Joomla.Framework
+ * @subpackage 	Parameter
+ * @since		1.5
+ */
+
+class JElement_Password extends JElement {
+   /**
+	* Element name
+	*
+	* @access	protected
+	* @var		string
+	*/
+	var	$_name = 'Password';
+
+	function fetchElement($name, $value, &$node, $control_name)
+	{
+		$size = ( $node->attributes('size') ? 'size="'.$node->attributes('size').'"' : '' );
+		$class = ( $node->attributes('class') ? 'class="'.$node->attributes('class').'"' : 'class="text_area"' );
+
+		return '<input type="password" name="'.$control_name.'['.$name.']" id="'.$control_name.$name.'" value="'.$value.'" '.$class.' '.$size.' />';
+	}
+}
+?>
