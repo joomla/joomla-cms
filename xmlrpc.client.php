b74048102b93f5e8c0994d317ada097213e5af6c
Andrew Eddie <andrew.eddie@newlifeinit.com>
2006-06-21 05:24:03 +0000

    Missing defines include
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@4083 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- xmlrpc.client.php
+++ xmlrpc.client.php
@@ -16,6 +16,9 @@ define( "_JEXEC", 1 );
 
 define('JPATH_BASE', dirname(__FILE__) );
 
+error_reporting( E_ALL );
+
+require_once ( JPATH_BASE .'/includes/defines.php' );
 require_once ( JPATH_BASE .'/includes/application.php' );
 
 //if (!$mainframe->getCfg('xmlrpc_server')) {
@@ -24,7 +27,6 @@ require_once ( JPATH_BASE .'/includes/application.php' );
 
 jimport('phpxmlrpc.xmlrpc');
 
-error_reporting( E_ALL );
 
 $uri 	= dirname( $_SERVER['PHP_SELF'] );
 
