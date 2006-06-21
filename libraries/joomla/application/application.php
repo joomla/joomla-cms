e3af5b99aee9abe467992bcf262a42ed65e30502
Rey Gigataras <stingrey@gmail.com>
2006-06-21 18:59:30 +0000

    * A4 - Secured getUserStateFromRequest() from XSS vulnerability
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@4100 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- libraries/joomla/application/application.php
+++ libraries/joomla/application/application.php
@@ -173,7 +173,12 @@ class JApplication extends JObject
 		$old_state = $this->getUserState( $key );
 		$cur_state = isset( $old_state ) ? $old_state : $default;
 		$new_state = isset( $_REQUEST[$request] ) ? $_REQUEST[$request] : $cur_state;
-
+		
+		// filter input
+		jimport( 'phpinputfilter.inputfilter' );
+		$iFilter = new InputFilter();			
+		$new_state = $iFilter->process( $new_state );
+		
 		$this->setUserState( $key, $new_state );
 
 		return $new_state;
