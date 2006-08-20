fee5c6553c35eeb28cc696112cbe8c9cd68785f0
Louis Landry <louis.landry@joomla.org>
2006-08-20 02:57:32 +0000

    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@4596 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- index.php
+++ index.php
@@ -18,7 +18,6 @@ define('JPATH_BASE', dirname(__FILE__) );
 
 require_once ( JPATH_BASE .'/includes/defines.php' );
 require_once ( JPATH_BASE .'/includes/application.php' );
-require_once ( JPATH_BASE .'/includes/template.php'  );
 
 /**
  * CREATE THE APPLICATION
