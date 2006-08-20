82e7f9c7323fad757cbe38a5feee3175cff9fbc6
Louis Landry <louis.landry@joomla.org>
2006-08-20 01:25:51 +0000

    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@4594 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

---
+++ templates/rhuk_milkyway/html/modules.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @version $Id$
+ * @package Joomla
+ * @copyright Copyright (C) 2005 - 2006 Open Source Matters. All rights reserved.
+ * @license GNU/GPL, see LICENSE.php
+ * Joomla! is free software. This version may have been modified pursuant
+ * to the GNU General Public License, and as distributed it includes or
+ * is derivative of works licensed under the GNU General Public License or
+ * other free or open source software licenses.
+ * See COPYRIGHT.php for copyright notices and details.
+ */
+
+// no direct access
+defined('_JEXEC') or die('Restricted access');
+
+/**
+ * This is a file to add template specific chrome to module rendering.  To use it you would
+ * set the style attribute for the given module(s) include in your template to use the style
+ * for each given modChrome function.
+ * 
+ * eg.  To render a module mod_test in the sliders style, you would use the following include:
+ * <jdoc:include type="module" name="test" style="sliders" />
+ * 
+ * This gives template designers ultimate control over how modules are rendered.
+ * 
+ * NOTICE: All chrome wrapping methods should be named: modChrome_{STYLE} and take the same
+ * two arguments.
+ */
+
+/*
+ * Module chrome for rendering the module in a slider
+ */
+function modChrome_sliders($module, & $params)
+{
+	jimport('joomla.presentation.pane');
+	// Initialize variables
+	$sliders = & JPane::getInstance('sliders');
+	$sliders->startPanel( JText::_( $module->title ), 'module' . $module->id );
+	echo $module->content;
+	$sliders->endPanel();
+}
+?>
