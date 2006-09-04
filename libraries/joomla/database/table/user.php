7a3d4a8fcebc7506e7d5061bf7ef0bcf7d1610d3
Andrew Eddie <andrew.eddie@newlifeinit.com>
2006-09-04 07:52:16 +0000

    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@4929 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- libraries/joomla/database/table/user.php
+++ libraries/joomla/database/table/user.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
-* @version $Id: user.php 4330 2006-07-26 06:24:14Z webImagery $
+* @version $Id$
 * @package Joomla
 * @copyright Copyright (C) 2005 - 2006 Open Source Matters. All rights reserved.
 * @license GNU/GPL, see LICENSE.php
