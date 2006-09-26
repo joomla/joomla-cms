1d4015de4b2f1da1654d7e0771438f158782b2fa
Johan Janssens <johan@nooku.org>
2006-09-26 00:40:33 +0000

    Moved pagination deprecated functions in legacy classes
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@5188 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- includes/pageNavigation.php
+++ includes/pageNavigation.php
@@ -28,5 +28,55 @@ class mosPageNav extends JPagination {
 	function mosPageNav( $total, $limitstart, $limit ) {
 		parent::__construct($total, $limitstart, $limit);
 	}
+	
+	/**
+	 * Writes the dropdown select list for number of rows to show per page
+	 * Use: print $pagination->getLimitBox();
+	 *
+	 * @deprecated as of 1.5
+	 */
+	function writeLimitBox($link = null) {
+		echo $this->getLimitBox($link);
+	}
+
+	/**
+	 * Writes the counter string
+	 * Use: print $pagination->getLimitBox();
+	 *
+	 * @deprecated as of 1.5
+	 */
+	function writePagesCounter() {
+		echo $this->getPagesCounter();
+	}
+
+	/**
+	 * Writes the page list string
+	 * Use: print $pagination->getPagesLinks();
+	 *
+	 * @deprecated as of 1.5
+	 */
+	function writePagesLinks($link = null) {
+		echo $this->getPagesLinks($link);
+	}
+
+	/**
+	 * Writes the html for the leafs counter, eg, Page 1 of x
+	 * Use: print $pagination->getPagesCounter();
+	 *
+	 * @deprecated as of 1.5
+	 */
+	function writeLeafsCounter() {
+		echo $this->getPagesCounter();
+	}
+
+	/**
+	 * Returns the pagination offset at an index
+	 * Use: $pagination->getRowOffset($index); instead
+	 *
+	 * @deprecated as of 1.5
+	 */
+	function rowNumber($index) {
+		return $index +1 + $this->limitstart;
+	}
 }
 ?>
