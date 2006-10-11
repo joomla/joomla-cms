b6421c2e6b1aebf7caed9f120464a813258a9dfd
Hannes Papenberg <info@joomlager.de>
2006-10-11 12:03:47 +0000

    Fixed statistics plugin
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@5432 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- plugins/system/joomla.visitorbot.php
+++ plugins/system/joomla.visitorbot.php
@@ -41,7 +41,7 @@ function botDetectVisitor()
 
 	setcookie( 'mosvisitor', '1' );
 
-	$agent    = $browser->getFeature('parent');
+	$agent    = $browser->getBrowser();
 	$platform = $browser->getPlatform();
 
 	if (phpversion() <= '4.2.1') {
@@ -73,14 +73,14 @@ function botDetectVisitor()
 
 	$query = "SELECT COUNT(*)"
 	. "\n FROM #__stats_agents"
-	. "\n WHERE agent = '$agent'"
+	. "\n WHERE agent = '$platform'"
 	. "\n AND type = '1'"
 	;
 	$db->setQuery( $query );
 	if ($db->loadResult()) {
 		$query = "UPDATE #__stats_agents"
 		. "\n SET hits = ( hits + 1 )"
-		. "\n WHERE agent = '$agent'"
+		. "\n WHERE agent = '$platform'"
 		. "\n AND type = '1'"
 		;
 		$db->setQuery( $query );
