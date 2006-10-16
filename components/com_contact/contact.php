00312454fa75bbdaf31516febc6847b0bf14d674
Enno Klasing <info@eklasing.com>
2006-10-16 21:15:49 +0000

    # Fixed [topic,104724] : contact component uses hardcoded /  # Fixed [topic,104391] : Admin Write a Message Needs To: Capitalization  # Fixed [topic,103944] : Typo in example content
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@5492 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- components/com_contact/contact.php
+++ components/com_contact/contact.php
@@ -17,7 +17,7 @@ defined('_JEXEC') or die('Restricted access');
 
 jimport('joomla.application.component.helper');
 
-require_once( JPATH_COMPONENT . '/controller.php' );
+require_once( JPATH_COMPONENT . DS . 'controller.php' );
 
 // Set the table directory
 JTable::addTableDir(JPATH_ADMINISTRATOR.DS.'components'.DS.'com_contact'.DS.'tables');
