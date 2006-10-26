4abf49dc2cb8070982b5342ed4c127955d8b5348
Laurens Vandeput <laurens@joomlatools.org>
2006-10-26 18:01:19 +0000

    Fixed [artf6234] : "Finish Publishing" box bug
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@5577 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- administrator/components/com_content/controller.php
+++ administrator/components/com_content/controller.php
@@ -169,10 +169,10 @@ class ContentController extends JController
 				"\n AND c.state <> -2" .
 				"\n GROUP BY u.name" .
 				"\n ORDER BY u.name";
-		$authors[] = JHTMLSelect::option('0', '- '.JText::_('Select Author').' -', 'created_by', 'name');
+		$authors[] = JHTML::makeOption('0', '- '.JText::_('Select Author').' -', 'created_by', 'name');
 		$db->setQuery($query);
 		$authors = array_merge($authors, $db->loadObjectList());
-		$lists['authorid'] = JHTMLSelect::genericList($authors, 'filter_authorid', 'class="inputbox" size="1" onchange="document.adminForm.submit( );"', 'created_by', 'name', $filter_authorid);
+		$lists['authorid'] = JHTML::selectList($authors, 'filter_authorid', 'class="inputbox" size="1" onchange="document.adminForm.submit( );"', 'created_by', 'name', $filter_authorid);
 
 		// state filter
 		$lists['state'] = JCommonHTML::selectState($filter_state, 'Published', 'Unpublished', 'Archived');
@@ -388,9 +388,9 @@ class ContentController extends JController
 				$row->images = array ();
 			}
 
-			$row->created  = _validateDate($row->created);
-			$row->modified = _validateDate($row->modified);
-			$row->publish_down = _validateDate($row->publish_down);
+			$row->created  		= ContentController::_validateDate($row->created);
+			$row->modified 		= ContentController::_validateDate($row->modified);
+			$row->publish_down 	= ContentController::_validateDate($row->publish_down);
 
 			$query = "SELECT name" .
 					"\n FROM #__users" .
@@ -451,10 +451,10 @@ class ContentController extends JController
 				"\n ORDER BY s.ordering";
 		$db->setQuery($query);
 
-		$sections[] = JHTMLSelect::option('-1', '- '.JText::_('Select Section').' -', 'id', 'title');
-		$sections[] = JHTMLSelect::option('0', JText::_('Uncategorized'), 'id', 'title');
+		$sections[] = JHTML::makeOption('-1', '- '.JText::_('Select Section').' -', 'id', 'title');
+		$sections[] = JHTML::makeOption('0', JText::_('Uncategorized'), 'id', 'title');
 		$sections = array_merge($sections, $db->loadObjectList());
-		$lists['sectionid'] = JHTMLSelect::genericList($sections, 'sectionid', 'class="inputbox" size="1" '.$javascript, 'id', 'title', intval($row->sectionid));
+		$lists['sectionid'] = JHTML::selectList($sections, 'sectionid', 'class="inputbox" size="1" '.$javascript, 'id', 'title', intval($row->sectionid));
 
 		foreach ($sections as $section)
 		{
@@ -473,7 +473,7 @@ class ContentController extends JController
 
 		$sectioncategories = array ();
 		$sectioncategories[-1] = array ();
-		$sectioncategories[-1][] = JHTMLSelect::option('-1', JText::_( 'Select Category' ), 'id', 'name');
+		$sectioncategories[-1][] = JHTML::makeOption('-1', JText::_( 'Select Category' ), 'id', 'name');
 		$section_list = implode('\', \'', $section_list);
 
 		$query = "SELECT id, name, section" .
@@ -500,7 +500,7 @@ class ContentController extends JController
 				}
 			}
 			foreach ($rows2 as $row2) {
-				$sectioncategories[$section->id][] = JHTMLSelect::option($row2->id, $row2->name, 'id', 'name');
+				$sectioncategories[$section->id][] = JHTML::makeOption($row2->id, $row2->name, 'id', 'name');
 			}
 		}
 
@@ -508,9 +508,9 @@ class ContentController extends JController
 			$categoriesA[] = $cat;
 		}
 
-		$categories[] = JHTMLSelect::option('-1', JText::_( 'Select Category' ), 'id', 'name');
+		$categories[] = JHTML::makeOption('-1', JText::_( 'Select Category' ), 'id', 'name');
 		$categories = array_merge($categories, $categoriesA);
-		$lists['catid'] = JHTMLSelect::genericList($categories, 'catid', 'class="inputbox" size="1"', 'id', 'name', intval($row->catid));
+		$lists['catid'] = JHTML::selectList($categories, 'catid', 'class="inputbox" size="1"', 'id', 'name', intval($row->catid));
 
 		// build the html select list for ordering
 		$query = "SELECT ordering AS value, title AS text" .
@@ -521,9 +521,9 @@ class ContentController extends JController
 		$lists['ordering'] = JAdminMenus::SpecificOrdering($row, $id, $query, 1);
 
 		// build the html radio buttons for frontpage
-		$lists['frontpage'] = JHTMLSelect::yesnoList('frontpage', '', $row->frontpage);
+		$lists['frontpage'] = JHTML::yesnoRadioList('frontpage', '', $row->frontpage);
 		// build the html radio buttons for published
-		$lists['state'] = JHTMLSelect::yesnoList('state', '', $row->state);
+		$lists['state'] = JHTML::yesnoRadioList('state', '', $row->state);
 		// build list of users
 		$active = (intval($row->created_by) ? intval($row->created_by) : $user->get('id'));
 		$lists['created_by'] = JAdminMenus::UserSelect('created_by', $active);
@@ -1330,21 +1330,13 @@ class ContentController extends JController
 		ContentView::previewContent();
 	}
 
-	function insertPagebreak()
-	{
-		global $mainframe;
-
-		$mainframe->setPageTitle(JText::_('PGB ARTICLE PAGEBRK'));
-		ContentView::insertPagebreak();
-	}
-
 	function _validateDate($date) {
-		$db = &JFactory::getDBO();
+		$db =& JFactory::getDBO();
 
-		if ($date == $db->getNullDate() || JHTML::Date($date, '%Y') < date('Y')) {
+		if (JHTML::Date($date, '$Y') == 1969 || $date == $db->getNullDate()) {
 			$newDate = JText::_('Never');
 		} else {
-			$newDate =  JHTML::Date($date, '%Y-%m-%d %H:%M:%S');
+			$newDate = JHTML::Date($date, '%Y-%m-%d %H:%M:%S');
 		}
 
 		return $newDate;
