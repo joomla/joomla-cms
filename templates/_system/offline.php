e46959641ae58784911b644011f5a5e2b0dcf103
Andrew Eddie <andrew.eddie@newlifeinit.com>
2006-11-06 00:54:02 +0000

    Fixed bug in jdoc: syntax
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@5659 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- templates/_system/offline.php
+++ templates/_system/offline.php
@@ -12,7 +12,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?= $this->language; ?>" lang="<?= $this->language; ?>" dir="<?= $this->direction; ?>">
 <head>
-	<jdoc: type="head" />
+	<jdoc:include type="head" />
 	<link rel="stylesheet" href="templates/_system/css/offline.css" type="text/css" />
 </head>
 <body>
