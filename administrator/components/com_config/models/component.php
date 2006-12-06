7a082eeca5bb5a60b362cfebbff4b3b4a19d26a5
Andrew Eddie <andrew.eddie@newlifeinit.com>
2006-12-06 07:07:14 +0000

    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@5935 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- administrator/components/com_config/models/component.php
+++ administrator/components/com_config/models/component.php
@@ -31,19 +31,17 @@ class ConfigModelComponent extends JModel
 		{
 			$component	= JRequest::getVar( 'component' );
 
+			$table = JTable::getInstance('component');
+			$table->loadByOption( $component );
+			
 			// work out file path
 			if ($path = JRequest::getVar( 'path' )) {
 				$path = JPath::clean( JPATH_SITE.DS.$path );
 				JPath::check( $path );
 			} else {
-				$path = JPATH_ADMINISTRATOR . '/components/' . $option . '/config.xml';
+				$option	= preg_replace( '#\W#', '', $table->option );
+				$path	= JPATH_ADMINISTRATOR . '/components/' . $option . '/config.xml';
 			}
-			
-			$table = JTable::getInstance('component');
-			$table->loadByOption( $component );
-			
-			$option = preg_replace( '#\W#', '', $table->option );
-
 			
 			if (file_exists( $path )) {
 				$instance = new JParameter( $table->params, $path );
