4064220e80c4d5aa14d1070e1f198936b6efc436
Chris Davenport <chris.davenport@joomla.org>
2007-02-14 00:22:01 +0000

    [artf7550] Amended phpDoc tags
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@6618 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- libraries/joomla/common/abstract/object.php
+++ libraries/joomla/common/abstract/object.php
@@ -45,18 +45,23 @@ class JObject
 	function __construct() {}
 
 	/**
-	 * @param string The name of the property
-	 * @param mixed The value of the property to set
-	 */
+	 * Modifies a property of the object, creating it if it does not already exist.
+	 *
+	 * @param string $property The name of the property
+	 * @param mixed  $value The value of the property to set
+ 	 */
 	function set( $property, $value=null ) {
 		$this->$property = $value;
 	}
 
 	/**
-	 * @param string The name of the property
-	 * @param mixed  The default value
+	 * Returns a property of the object or the default value if the property is not set.
+	 *
+	 * @param string $property The name of the property
+	 * @param mixed  $default The default value
 	 * @return mixed The value of the property
-	 */
+	 * @see get(), getPublicProperties()
+ 	 */
 	function get($property, $default=null)
 	{
 		if(isset($this->$property)) {
@@ -68,9 +73,10 @@ class JObject
 	/**
 	 * Returns an array of public properties
 	 *
-	 * @param	boolean	If true, returns an associative key=>value array
-	 * @return	array
-	 */
+	 * @param   boolean $assoc If true, returns an associative key=>value array
+	 * @return  array
+	 * @see get(), toString()
+ 	 */
 	function getPublicProperties( $assoc = false )
 	{
 		$vars = array(array(),array());
@@ -90,7 +96,8 @@ class JObject
 	 * Each class can override it as necessary.
 	 *
 	 * @return string This name of this class
-	 */
+	 * @see get(), getPublicProperties()
+ 	 */
 	function toString()
 	{
 		return get_class($this);
