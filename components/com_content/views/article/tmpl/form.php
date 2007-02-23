b26aab647b72f91a1ad9b7c5a71c0ef05800a693
Hannes Papenberg <info@joomlager.de>
2007-02-23 03:15:51 +0000

    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@6704 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- components/com_content/views/article/tmpl/form.php
+++ components/com_content/views/article/tmpl/form.php
@@ -206,3 +206,4 @@ echo $this->editor->display('text', $this->article->text, '100%', '400', '70', '
 <input type="hidden" name="referer" value="<?php echo ampReplace( @$_SERVER['HTTP_REFERER'] ); ?>" />
 <input type="hidden" name="task" value="" />
 </form>
+<?php JHTML::keepAlive(); ?>
