fd2d77e6b4cdb85667bc5e9969e8c557827b304b
Toby Patterson <dev@gmitc.biz>
2007-04-09 02:31:17 +0000

    Added HTML base tag to _system template files.
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@7111 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- templates/_system/403.php
+++ templates/_system/403.php
@@ -8,11 +8,13 @@
  * other free or open source software licenses.
  * See COPYRIGHT.php for copyright notices and details.
  */
+jimport('joomla.environment.uri');
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>">
 <head>
 	<title>403 - <?php echo $this->title; ?></title>
+	<base href="<?php echo JURI::base() ?>" />
 	<link rel="stylesheet" href="templates/_system/css/error.css" type="text/css" />
 </head>
 <body>
