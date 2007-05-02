9b954e9e13947180db85c08fa10eb71d6c74bef6
Johan Janssens <johan@nooku.org>
2007-05-02 10:59:36 +0000

    Added JEventHandler class for consistency
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@7259 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- libraries/joomla/event/handler.php
+++ libraries/joomla/event/handler.php
@@ -1,6 +1,6 @@
 <?php
 /**
-* @version		$Id$
+* @version		$Id: plugin.php 7180 2007-04-23 16:51:53Z jinx $
 * @package		Joomla.Framework
 * @subpackage	Application
 * @copyright	Copyright (C) 2005 - 2007 Open Source Matters. All rights reserved.
@@ -15,18 +15,18 @@
 // Check to ensure this file is within the rest of the framework
 defined('JPATH_BASE') or die();
 
-jimport( 'joomla.event.handler' );
+jimport( 'joomla.base.observer' );
 
 /**
- * JPlugin Class
+ * JEventHandler Class
  *
  * @abstract
- * @author		Louis Landry <louis.landry@joomla.org>
+ * @author		Johan Janssens <johan.janssens@joomla.org>
  * @package		Joomla.Framework
  * @subpackage	Event
  * @since		1.5
  */
-class JPlugin extends JEventHandler
+class JEventHandler extends JObserver
 {
 
 	/**
@@ -39,7 +39,35 @@ class JPlugin extends JEventHandler
 	 * @param object $subject The object to observe
 	 * @since 1.5
 	 */
-	function JPlugin(& $subject) {
+	function JEventHandler(& $subject) {
 		parent::__construct($subject);
+	}
+
+	/**
+	 * Method to trigger events
+	 *
+	 * @access public
+	 * @param array Arguments
+	 * @return mixed Routine return value
+	 * @since 1.5
+	 */
+	function update(& $args)
+	{
+		/*
+		 * First lets get the event from the argument array.  Next we will unset the
+		 * event argument as it has no bearing on the method to handle the event.
+		 */
+		$event = $args['event'];
+		unset($args['event']);
+
+		/*
+		 * If the method to handle an event exists, call it and return its return
+		 * value.  If it does not exist, return null.
+		 */
+		if (method_exists($this, $event)) {
+			return call_user_func_array ( array($this, $event), $args );
+		} else {
+			return null;
+		}
 	}
 }
