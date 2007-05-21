2174f8bdb8aa00a84b3976efa92c1667628aaef7
Louis Landry <louis.landry@joomla.org>
2007-05-21 16:04:14 +0000

    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@7456 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- components/com_search/views/search/view.php
+++ components/com_search/views/search/view.php
@@ -33,6 +33,8 @@ class SearchViewSearch extends JView
 
 		$uri =& JFactory::getURI();
 
+		$this->searchword = htmlentities($this->searchword);
+
 		$searchword		= $this->searchword;
 		$searchphrase	= $this->searchphrase;
 		$ordering		= $this->ordering;
@@ -41,7 +43,7 @@ class SearchViewSearch extends JView
 		jimport('joomla.html.pagination');
 		$pagination = new JPagination($this->total, $this->limitstart, $this->limit);
 
-		$this->result	= JText::sprintf( 'TOTALRESULTSFOUND', $this->total, htmlentities($this->searchword) );
+		$this->result	= JText::sprintf( 'TOTALRESULTSFOUND', $this->total, $this->searchword );
 		$this->image	= JHTML::_('image.site',  'google.png', '/images/M_images/', NULL, NULL, 'Google', 'Google', 1 );
 
 		for($i = 0; $i < count($this->results); $i++ )
