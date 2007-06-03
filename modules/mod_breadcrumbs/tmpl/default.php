42bb2439caa7fb8aa25c8905ffef665f47f018d9
Johan Janssens <johan@nooku.org>
2007-06-03 20:18:27 +0000

    Fixed issue with correct rendering of last breadcrumb
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@7610 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- modules/mod_breadcrumbs/tmpl/default.php
+++ modules/mod_breadcrumbs/tmpl/default.php
@@ -1,15 +1,14 @@
 <?php // no direct access
 defined('_JEXEC') or die('Restricted access'); ?>
 <span class="breadcrumbs pathway">
-<?php echo $list[0]->name; ?>
-<?php if ($count > 1) : 
-	echo ' '.$separator.' ';
-endif; ?>
-<?php for ($i = 1; $i < $count; $i ++) :
-	echo $list[$i]->link;
+<?php for ($i = 0; $i < $count; $i ++) :
+	
 	// If not the last item in the breadcrumbs add the separator
 	if ($i < $count -1) {
+		echo $list[$i]->link;
 		echo ' '.$separator.' ';
+	}  else { // when $i == $count -1
+	    echo $list[$i]->name;
 	}
 endfor; ?>
 </span>
