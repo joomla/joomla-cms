9b7ef65530ea9927a94d992f7395eab80803bae5
Toby Patterson <dev@gmitc.biz>
2007-06-05 15:18:14 +0000

    forgot to add the default template.
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@7641 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

---
+++ administrator/components/com_installer/views/default/tmpl/default_message.php
@@ -0,0 +1,19 @@
+<?php
+	$state			= &$this->get('State');
+	$message1		= $state->get('message');
+	$message2		= $state->get('extension.message');
+?>
+<table class="adminform">
+	<tbody>
+		<?php if($message1) : ?>
+		<tr>
+			<th><?php echo $message1 ?></th>
+		</tr>
+		<?php endif ?>
+		<?php if($message2) : ?>
+		<tr>
+			<td><?php echo $message2 ?></td>
+		</tr>
+		<?php endif ?>
+	</tbody>
+</table>
---
+++ administrator/components/com_installer/views/default/view.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * @version		$Id: view.php 7286 2007-05-03 01:44:29Z jinx $
+ * @package		Joomla
+ * @subpackage	Menus
+ * @copyright	Copyright (C) 2005 - 2007 Open Source Matters. All rights reserved.
+ * @license		GNU/GPL, see LICENSE.php
+ * Joomla! is free software. This version may have been modified pursuant
+ * to the GNU General Public License, and as distributed it includes or
+ * is derivative of works licensed under the GNU General Public License or
+ * other free or open source software licenses.
+ * See COPYRIGHT.php for copyright notices and details.
+ */
+
+// no direct access
+defined( '_JEXEC' ) or die( 'Restricted access' );
+
+jimport('joomla.application.component.view');
+
+/**
+ * Extension Manager Default View
+ *
+ * @author		Toby Patterson <tcp@gmitc.biz>
+ * @package		Joomla
+ * @subpackage	Installer
+ * @since		1.5
+ */
+class InstallerViewDefault extends JView
+{
+	function __construct($config = null)
+	{
+		parent::__construct($config);
+		$this->_addPath('template', $this->_basePath.DS.'views'.DS.'default'.DS.'tmpl');
+	}
+	
+	function display($tpl=null)
+	{
+		/*
+		 * Set toolbar items for the page
+		 */
+		JToolBarHelper::title( JText::_( 'Extension Manager'), 'install.png' );
+
+		// Document
+		$document = & JFactory::getDocument();
+		$document->setTitle(JText::_('Extension Manager').' : '.JText::_( $this->getName() ));
+
+		// Get data from the model
+		$state		= &$this->get('State');
+
+		// Are there messages to display ?
+		$showMessage	= false;
+		if ( is_object($state) )
+		{
+			$message1		= $state->get('message');
+			$message2		= $state->get('extension.message');
+			$showMessage	= ( $message1 || $message2 );
+		}
+		
+		$this->assign('showMessage',	$showMessage);
+		$this->assignRef('state',		$state);
+		
+		JHTML::_('behavior.tooltip');
+		parent::display($tpl);
+	}
+
+	/**
+	 * Should be overloaded by extending view
+	 *
+	 * @param	int $index
+	 */
+	function loadItem($index=0)
+	{
+	}
+}
+?>
