76b740d8fd66e4a05da7c89d58a0a3d26a7676aa
Jason Kendall <jakendall@coolacid.net>
2007-06-11 23:25:39 +0000

    Fixed [#5322] Section Blog Layout - Catagory was not showing
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@7720 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- CHANGELOG.php
+++ CHANGELOG.php
@@ -39,6 +39,7 @@ Legend:
 
 11-Jun-2007 Jason Kendall
  # Fixed [#5324] Section Blog layout date fix
+ # Fixed [#5322] Section Blog Layout - Catagory was not showing
 
 11-Jun-2007 Rob Schley
  # Fixed [#5646] Repeated strings at site admin file en-GB.com_installer.ini
