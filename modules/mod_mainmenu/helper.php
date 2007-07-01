aa2e71de61f66461aa12de7735a864af7a57107a
Louis Landry <louis.landry@joomla.org>
2007-07-01 06:56:49 +0000

    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@7818 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- modules/mod_mainmenu/helper.php
+++ modules/mod_mainmenu/helper.php
@@ -105,7 +105,7 @@ class modMainMenuHelper
 			}
 		}
 
-		if ($doc) {
+		if ($doc && is_callable($decorator)) {
 			$doc->map($decorator, array('end'=>$end, 'children'=>$sChild));
 		}
 		return $doc;
@@ -137,9 +137,9 @@ class modMainMenuHelper
 				// Include the new menu class
 				$xml = modMainMenuHelper::getXML($params->get('menutype'), $params, $callback);
 				if ($xml) {
-					if ($class = $params->get('class_sfx')) {
-						$xml->addAttribute('class', $class);
-					}
+					$class = $params->get('class_sfx');
+					$xml->addAttribute('class', 'menu'.$class);
+
 					echo JOutputFilter::ampReplace($xml->toString());
 				}
 				break;
@@ -367,67 +367,5 @@ class JMenuNode extends JNode
 		$this->link		= $link;
 		$this->class	= $class;
 	}
-}
-
-function modMainMenuXMLCallback(&$node, $args)
-{
-	$user	= &JFactory::getUser();
-	$menu	= &JMenu::getInstance();
-	$active	= $menu->getActive();
-	$path	= isset($active) ? array_reverse($active->tree) : null;
-
-	if (($args['end']) && ($node->attributes('level') >= $args['end']))
-	{
-		$children = &$node->children();
-		foreach ($node->children() as $child)
-		{
-			if ($child->name() == 'ul') {
-				$node->removeChild($child);
-			}
-		}
-	}
-
-	if ($node->name() == 'ul') {
-		foreach ($node->children() as $child)
-		{
-			if ($child->attributes('access') > $user->get('aid', 0)) {
-				$node->removeChild($child);
-			}
-		}
-	}
-
-	if (($node->name() == 'li') && isset($node->ul)) {
-		$node->addAttribute('class', 'parent');
-	}
-
-	if (isset($path) && in_array($node->attributes('id'), $path))
-	{
-		if ($node->attributes('class')) {
-			$node->addAttribute('class', $node->attributes('class').' active');
-		} else {
-			$node->addAttribute('class', 'active');
-		}
-	}
-	else
-	{
-		if (isset($args['children']) && !$args['children'])
-		{
-			$children = $node->children();
-			foreach ($node->children() as $child)
-			{
-				if ($child->name() == 'ul') {
-					$node->removeChild($child);
-				}
-			}
-		}
-	}
-
-	if (isset($path) && $node->attributes('id') == $path[0]) {
-		$node->addAttribute('id', 'current');
-	} else {
-		$node->removeAttribute('id');
-	}
-	$node->removeAttribute('level');
-	$node->removeAttribute('access');
 }
 ?>
--- modules/mod_mainmenu/tmpl/default.php
+++ modules/mod_mainmenu/tmpl/default.php
@@ -1,5 +1,76 @@
 <?php // no direct access
 defined('_JEXEC') or die('Restricted access');
 
+
+function modMainMenuXMLCallback(&$node, $args)
+{
+	$user	= &JFactory::getUser();
+	$menu	= &JMenu::getInstance();
+	$active	= $menu->getActive();
+	$path	= isset($active) ? array_reverse($active->tree) : null;
+
+	if (($args['end']) && ($node->attributes('level') >= $args['end']))
+	{
+		$children = &$node->children();
+		foreach ($node->children() as $child)
+		{
+			if ($child->name() == 'ul') {
+				$node->removeChild($child);
+			}
+		}
+	}
+
+	if ($node->name() == 'ul') {
+		foreach ($node->children() as $child)
+		{
+			if ($child->attributes('access') > $user->get('aid', 0)) {
+				$node->removeChild($child);
+			}
+		}
+	}
+
+	if (($node->name() == 'li') && isset($node->ul)) {
+		$node->addAttribute('class', 'parent');
+	}
+
+	if (isset($path) && in_array($node->attributes('id'), $path))
+	{
+		if ($node->attributes('class')) {
+			$node->addAttribute('class', $node->attributes('class').' active');
+		} else {
+			$node->addAttribute('class', 'active');
+		}
+	}
+	else
+	{
+		if (isset($args['children']) && !$args['children'])
+		{
+			$children = $node->children();
+			foreach ($node->children() as $child)
+			{
+				if ($child->name() == 'ul') {
+					$node->removeChild($child);
+				}
+			}
+		}
+	}
+
+	if (($node->name() == 'li') && ($id = $node->attributes('id'))) {
+		if ($node->attributes('class')) {
+			$node->addAttribute('class', $node->attributes('class').' item'.$id);
+		} else {
+			$node->addAttribute('class', 'item'.$id);
+		}
+	}
+
+	if (isset($path) && $node->attributes('id') == $path[0]) {
+		$node->addAttribute('id', 'current');
+	} else {
+		$node->removeAttribute('id');
+	}
+	$node->removeAttribute('level');
+	$node->removeAttribute('access');
+}
+
 modMainMenuHelper::render($params, 'modMainMenuXMLCallback');
 ?>
