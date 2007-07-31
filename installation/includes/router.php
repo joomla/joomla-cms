8f6ce37393649892518e196869a853d5f54fa319
Johan Janssens <johan@nooku.org>
2007-07-31 21:02:09 +0000

    Added router.php
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@8267 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

---
+++ installation/includes/router.php
@@ -0,0 +1,56 @@
+<?php
+/**
+* @version		$Id: router.php 8180 2007-07-23 05:52:29Z eddieajau $
+* @package		Joomla.Framework
+* @subpackage	Application
+* @copyright	Copyright (C) 2005 - 2007 Open Source Matters. All rights reserved.
+* @license		GNU/GPL, see LICENSE.php
+* Joomla! is free software. This version may have been modified pursuant
+* to the GNU General Public License, and as distributed it includes or
+* is derivative of works licensed under the GNU General Public License or
+* other free or open source software licenses.
+* See COPYRIGHT.php for copyright notices and details.
+*/
+
+// Check to ensure this file is within the rest of the framework
+defined('JPATH_BASE') or die();
+
+jimport( 'joomla.application.router' );
+
+/**
+ * Class to create and parse routes
+ *
+ * @author		Johan Janssens <johan.janssens@joomla.org>
+ * @package 	Joomla
+ * @since		1.5
+ */
+class JRouterInstallation extends JObject
+{
+	/**
+	 * Function to convert a route to an internal URI
+	 *
+	 * @access public
+	 */
+	function parse($url)
+	{
+		return true;
+	}
+
+	/**
+	 * Function to convert an internal URI to a route
+	 *
+	 * @param	string	$string	The internal URL
+	 * @return	string	The absolute search engine friendly URL
+	 * @since	1.5
+	 */
+	function build($url, $xhtml = true)
+	{
+		$url = str_replace('&amp;', '&', $url);
+		
+		if($xhtml) {
+			$url = str_replace( '&', '&amp;', $url );
+		}
+		
+		return $url;
+	}
+}
