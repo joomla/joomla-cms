a1cecf4f14a97a6c11df2e742610d331ab68d2a1
Andrew Eddie <andrew.eddie@newlifeinit.com>
2007-08-01 23:55:50 +0000

    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@8296 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- administrator/modules/mod_logged/mod_logged.php
+++ administrator/modules/mod_logged/mod_logged.php
@@ -37,18 +37,18 @@ if ( $user->get('gid') == 23 ) {
 
 // get the total number of records
 $query = 'SELECT COUNT(*)'
-. ' FROM #__session'
-. ' WHERE userid != 0'
-. $and
-. ' ORDER BY usertype, username'
-;
+	. ' FROM #__session'
+	. ' WHERE userid != 0'
+	. $and
+	. ' ORDER BY usertype, username'
+	;
 $db->setQuery( $query );
 $total = $db->loadResult();
 
 // page navigation
 $pageNav = new JPagination( $total, $limitstart, $limit );
 
-$query = 'SELECT *'
+$query = 'SELECT username, time, userid, usertype, client_id'
 . ' FROM #__session'
 . ' WHERE userid != 0'
 . $and
@@ -56,75 +56,5 @@ $query = 'SELECT *'
 ;
 $db->setQuery( $query );
 $rows = $db->loadObjectList();
-?>
-<table class="adminlist">
-<tr>
-	<td class="title">
-		<strong><?php echo '#' ?></strong>
-	</td>
-	<td class="title">
-		<strong><?php echo JText::_( 'Name' ); ?></strong>
-	</td>
-	<td class="title">
-		<strong><?php echo JText::_( 'Group' ); ?></strong>
-	</td>
-	<td class="title">
-		<strong><?php echo JText::_( 'Client' ); ?></strong>
-	</td>
-	<td class="title">
-		<strong><?php echo JText::_( 'Logout' ); ?></strong>
-	</td>
-</tr>
-<?php
-$i = 0;
-foreach ( $rows as $row )
-{
-	if ( $user->authorize( 'com_users', 'manage' ) )
-	{
-		$link 	= 'index.php?option=com_users&amp;task=edit&amp;cid[]='. $row->userid;
-		$name 	= '<a href="'. $link .'" title="'. JText::_( 'Edit User' ) .'">'. $row->username .'</a>';
-	} else {
-		$name 	= $row->username;
-	}
 
-	$clientInfo = JApplicationHelper::getClientInfo($row->client_id);
-	?>
-	<tr>
-		<td width="5%">
-		<?php echo $pageNav->getRowOffset( $i ); ?>
-		</td>
-		<td>
-		<?php echo $name;?>
-		</td>
-		<td>
-		<?php echo $row->usertype;?>
-		</td>
-		<td>
-		<?php echo $clientInfo->name;?>
-		</td>
-		<?php
-		if ( $user->authorize( 'com_users', 'manage' ) && $user->get('gid') > 24 && $row->userid != $user->get('id')) {
-			?>
-			<td>
-			<a href="index.php?option=com_users&amp;task=logout&amp;cid[]=<?php echo $row->userid ?>&amp;client=<?php echo $row->client_id; ?>">
-			<img src="images/publish_x.png" width="16" height="16" border="0" alt="<?php echo JText::_( 'Logout' ); ?>" title="<?php echo JText::_( 'Force Logout User' ); ?>" />
-			</a>
-			</td>
-			<?php
-		}
-		else
-		{
-			?>
-			<td>
-
-			</td>
-			<?php
-		}
-		?>
-	</tr>
-	<?php
-	$i++;
-}
-?>
-</table>
-<input type="hidden" name="option" value="com_admin" />
+require( dirname( __FILE__ ).DS.'tmpl'.DS.'default.php' );
---
+++ administrator/modules/mod_logged/tmpl/default.php
@@ -0,0 +1,69 @@
+<?php /** $Id$ */ defined( '_JEXEC' ) or die(); ?>
+
+<table class="adminlist">
+	<thead>
+		<tr>
+			<td class="title">
+				<strong><?php echo '#' ?></strong>
+			</td>
+			<td class="title">
+				<strong><?php echo JText::_( 'Name' ); ?></strong>
+			</td>
+			<td class="title">
+				<strong><?php echo JText::_( 'Group' ); ?></strong>
+			</td>
+			<td class="title">
+				<strong><?php echo JText::_( 'Client' ); ?></strong>
+			</td>
+			<td class="title">
+				<strong><?php echo JText::_( 'Last Activity' ); ?></strong>
+			</td>
+			<td class="title">
+				<strong><?php echo JText::_( 'Logout' ); ?></strong>
+			</td>
+		</tr>
+	</thead>
+	<tbody>
+<?php
+	$i		= 0;
+	$now	= time();
+	foreach ($rows as $row) :
+		if ($user->authorize( 'com_users', 'manage' )) :
+			$link 	= 'index.php?option=com_users&amp;task=edit&amp;cid[]='. $row->userid;
+			$name 	= '<a href="'. $link .'" title="'. JText::_( 'Edit User' ) .'">'. $row->username .'</a>';
+		else :
+			$name 	= $row->username;
+		endif;
+
+		$clientInfo = JApplicationHelper::getClientInfo($row->client_id);
+		?>
+		<tr>
+			<td width="5%">
+				<?php echo $pageNav->getRowOffset( $i ); ?>
+			</td>
+			<td>
+				<?php echo $name;?>
+			</td>
+			<td>
+				<?php echo $row->usertype;?>
+			</td>
+			<td>
+				<?php echo $clientInfo->name;?>
+			</td>
+			<td>
+				<?php echo JText::sprintf( 'activity hours', ($now - $row->time)/3600.0 );?>
+			</td>
+			<td>
+			<?php if ( $user->authorize( 'com_users', 'manage' ) && $user->get('gid') > 24 && $row->userid != $user->get('id')) : ?>
+				<a href="index.php?option=com_users&amp;task=logout&amp;cid[]=<?php echo $row->userid ?>&amp;client=<?php echo $row->client_id; ?>">
+					<img src="images/publish_x.png" width="16" height="16" border="0" alt="<?php echo JText::_( 'Logout' ); ?>" title="<?php echo JText::_( 'Force Logout User' ); ?>" /></a>
+			<?php endif; ?>
+			</td>
+		</tr>
+		<?php
+		$i++;
+	endforeach;
+	?>
+	</tbody>
+</table>
+<input type="hidden" name="option" value="com_admin" />
