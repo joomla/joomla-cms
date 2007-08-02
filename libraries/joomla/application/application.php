717b3e8f41479b5ce28fe37d55f94951d5c4491f
Johan Janssens <johan@nooku.org>
2007-08-02 23:32:59 +0000

    Moved jimport( 'joomla.event.*'); from framework application.php
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@8321 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- libraries/joomla/application/application.php
+++ libraries/joomla/application/application.php
@@ -15,9 +15,6 @@
 // Check to ensure this file is within the rest of the framework
 defined('JPATH_BASE') or die();
 
-// Include library dependencies
-jimport('joomla.event.*');
-
 /**
 * Base class for a Joomla! application.
 *
