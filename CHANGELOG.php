194990923d4800beb18500492dd9dc4663d32ac4
Johan Janssens <johan@nooku.org>
2007-08-23 09:16:30 +0000

    Fixed [#6649] wrong image path in category blog
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@8528 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- CHANGELOG.php
+++ CHANGELOG.php
@@ -37,6 +37,9 @@ Legend:
 - -> Removed
 ! -> Note
 
+23-Aug-2007 Johan Janssens
+ # Fixed [#6649] wrong image path in category blog
+ 
 23-Aug-2007 Toby Patterson
  # Fixed [193612] RC1 - Mass mail throws language string error
 
