b906e7e74287629895a0e25639020c9d074718ba
Rob Schley <rschley@ebay.com>
2007-09-04 19:12:21 +0000

    Fixed [topic,207969] Added new language string to administrator com_template language file: THERE ARE NO PARAMETERS FOR THIS ITEM.
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@8742 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- CHANGELOG.php
+++ CHANGELOG.php
@@ -38,6 +38,7 @@ Legend:
 ! -> Note
 
 04-Sep-2007 Rob Schley
+ + Fixed [topic,207969] Added new language string to administrator com_template language file: THERE ARE NO PARAMETERS FOR THIS ITEM.
  # Fixed some various issues with Login/Logout page parameters
  ^ Note: The menu manager has no way to disable the system page title option which is not applicable to the login/logout views.
  # Fixed [topic,208954] No parameters found when creating a link to the user->login
