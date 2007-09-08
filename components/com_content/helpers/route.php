415bf28e5524258b8bdb327b8c2bb07f761f996f
Hannes Papenberg <info@joomlager.de>
2007-09-08 22:28:03 +0000

    Major improvements in ContentHelperRoute
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@8787 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- components/com_content/helpers/route.php
+++ components/com_content/helpers/route.php
@@ -35,25 +35,12 @@ class ContentHelperRoute
 	{
 		$item = ContentHelperRoute::_getArticleMenuInfo((int)$id, (int)$catid, (int)$sectionid);
 
-		$link = 'index.php?option=com_content';
+		$link = 'index.php?option=com_content&view=article&catid='.$catid.'&id='. $id;
 
 		if(isset($item))
 		{
-			if($item->link_parts['view'] == 'article') {
-				$link .=  '&Itemid='. $item->id;
-			}
-
-			if($item->link_parts['view'] == 'category') {
-				$link .= '&view=article&catid='.$catid.'&id='. $id .'&Itemid='. $item->id;
-			}
-
-			if($item->link_parts['view'] == 'section') {
-				$link .= '&view=article&catid='.$catid.'&id='. $id . '&Itemid='. $item->id;
-			}
-		} else {
-			$link .= '&view=article&catid='.$catid.'&id='. $id;
+			$link .= '&Itemid='. $item->id;
 		}
-
 		return JRoute::_( $link );
 	}
 
@@ -61,7 +48,7 @@ class ContentHelperRoute
 	{
 		$item = ContentHelperRoute::_getSectionMenuInfo((int)$sectionid);
 
-		$link = 'index.php?option=com_content&view=category&id='.$sectionid;
+		$link = 'index.php?option=com_content&view=section&id='.$sectionid;
 
 		if(isset($item))
 		{
@@ -75,21 +62,12 @@ class ContentHelperRoute
 	{
 		$item = ContentHelperRoute::_getCategoryMenuInfo((int)$catid, (int)$sectionid);
 
-		$link = 'index.php?option=com_content';
+		$link = 'index.php?option=com_content&view=category&id='.$catid;
 
 		if(isset($item))
 		{
-			if($item->link_parts['view'] == 'category') {
-				$link .= '&view=category&id='.$catid.'&layout='.$item->link_parts['layout'].'&Itemid='. $item->id;
-			}
-
-			if($item->link_parts['view'] == 'section') {
-				$link .= '&view=category&id='.$catid.'&layout='.$item->link_parts['layout'].'&Itemid='. $item->id;
-			}
-		} else {
-			$link .= '&view=category&id='.$catid;
+			$link .= '&layout='.$item->link_parts['layout'].'&Itemid='. $item->id;
 		}
-
 		return JRoute::_( $link );
 	}
 
@@ -98,47 +76,37 @@ class ContentHelperRoute
 	 */
 	function _getArticleMenuInfo($id, $catid = 0, $sectionid = 0)
 	{
-		$component	=& JComponentHelper::getComponent('com_content');
+		$items = ContentHelperRoute::_getComponentMenuItems('com_content');
 
-		$menus		=& JSite::getMenu();
-		$items		= $menus->getItems('componentid', $component->id);
-
-		$n = count( $items );
-		if (!$n) {
-			return null;
-		}
-
-		for ($i = 0; $i < $n; $i++)
+		if(isset($items['section']))
 		{
-			$item = &$items[$i];
-			$url = str_replace('index.php?', '', $item->link);
-			$url = str_replace('&amp;', '&', $url);
-			$parts = null;
-			parse_str($url, $parts);
-
-			if(!isset($parts['id'])) {
-				continue;
-			}
-
-			// set the link parts
-			$item->link_parts = $parts;
-
-			// Do we have a content item linked to the menu with this id?
-			if (($item->published) && ($item->link_parts['id'] == $id) && ($item->link_parts['view'] == 'article')) {
-				return $item;
-			}
-
-			// Check to see if it is in a published category
-			if (($item->published) && ($item->link_parts['id'] == $catid) && $item->link_parts['view'] == 'category') {
-				return $item;
-			}
-
-			// Check to see if it is in a published section
-			if (($item->published) && ($item->link_parts['id'] == $sectionid) && $item->link_parts['view'] == 'section') {
-				return $item;
+			foreach($items['section'] as $item)
+			{
+				if (($item->published) && ($item->link_parts['id'] == $sectionid)) {
+					return $item;
+				}
 			}
 		}
 
+		if(isset($items['category']))
+		{
+			foreach($items['category'] as $item)
+			{
+				if (($item->published) && ($item->link_parts['id'] == $catid)) {
+					return $item;
+				}
+			}
+		}
+
+		if(isset($items['article']))
+		{
+			foreach($items['article'] as $item)
+			{
+				if (($item->published) && ($item->link_parts['id'] == $id)) {
+					return $item;
+				}
+			}
+		}
 		return null;
 	}
 
@@ -147,42 +115,27 @@ class ContentHelperRoute
 	 */
 	function _getCategoryMenuInfo($catid, $sectionid = 0)
 	{
-		$component	=& JComponentHelper::getComponent('com_content');
+		$items = ContentHelperRoute::_getComponentMenuItems('com_content');
 
-		$menus		=& JSite::getMenu();
-		$items		= $menus->getItems('componentid', $component->id);
-
-		$n = count( $items );
-		if (!$n) {
-			return null;
-		}
-
-		for ($i = 0; $i < $n; $i++)
+		if(isset($items['section']))
 		{
-			$item = &$items[$i];
-			$url = str_replace('index.php?', '', $item->link);
-			$url = str_replace('&amp;', '&', $url);
-			$parts = null;
-			parse_str($url, $parts);
-
-			if(!isset($parts['id'])) {
-				continue;
-			}
-
-			// set the link parts
-			$item->link_parts = $parts;
-
-			// Check to see if it is in a published category
-			if (($item->published) && ($item->link_parts['id'] == $catid) && $item->link_parts['view'] == 'category') {
-				return $item;
-			}
-
-			// Check to see if it is in a published section
-			if (($item->published) && ($item->link_parts['id'] == $sectionid) && $item->link_parts['view'] == 'section') {
-				return $item;
+			foreach($items['section'] as $item)
+			{
+				if (($item->published) && ($item->link_parts['id'] == $sectionid)) {
+					return $item;
+				}
 			}
 		}
 
+		if(isset($items['category']))
+		{
+			foreach($items['category'] as $item)
+			{
+				if (($item->published) && ($item->link_parts['id'] == $catid)) {
+					return $item;
+				}
+			}
+		}
 		return null;
 	}
 
@@ -191,39 +144,43 @@ class ContentHelperRoute
 	 */
 	function _getSectionMenuInfo($sectionid)
 	{
-		$component	=& JComponentHelper::getComponent('com_content');
+		$items = ContentHelperRoute::_getComponentMenuItems('com_content');
 
-		$menus		=& JSite::getMenu();
-		$items		= $menus->getItems('componentid', $component->id);
-
-		$n = count( $items );
-		if (!$n) {
-			return null;
-		}
-
-		for ($i = 0; $i < $n; $i++)
+		if(isset($items['section']))
 		{
-			$item = &$items[$i];
-			$url = str_replace('index.php?', '', $item->link);
-			$url = str_replace('&amp;', '&', $url);
-			$parts = null;
-			parse_str($url, $parts);
-
-			if(!isset($parts['id'])) {
-				continue;
-			}
-
-			// set the link parts
-			$item->link_parts = $parts;
-
-			// Check to see if it is in a published section
-			if (($item->published) && ($item->link_parts['id'] == $sectionid) && $item->link_parts['view'] == 'section') {
-				return $item;
+			foreach($items['section'] as $item)
+			{
+				if (($item->published) && ($item->link_parts['id'] == $sectionid) && $item->link_parts['view'] == 'section') {
+					return $item;
+				}
 			}
 		}
-
 		return null;
 	}
 
+	function & _getComponentMenuItems($component)
+	{
+		static $items;
+
+		if(!$items)
+		{
+			$comp	=& JComponentHelper::getComponent($component);
+
+			$menus		=& JSite::getMenu();
+			$menuitems		= $menus->getItems('componentid', $comp->id);
+
+			foreach($menuitems as $menuitem)
+			{
+				$url = str_replace('index.php?', '', $menuitem->link);
+				$url = str_replace('&amp;', '&', $url);
+				$parts = null;
+				parse_str($url, $parts);
+				$menuitem->link_parts = $parts;
+
+				$items[$menuitem->link_parts['view']][] = $menuitem;
+			} 
+		}
+		return $items;
+	}
 }
 ?>
