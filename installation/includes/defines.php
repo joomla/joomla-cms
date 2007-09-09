e73bf0eee4c1584cc6a90a92d05d04ffd2bae0da
Johan Janssens <johan@nooku.org>
2007-09-09 22:03:17 +0000

    Fixed issue with missing defines
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@8812 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- installation/includes/defines.php
+++ installation/includes/defines.php
@@ -15,9 +15,6 @@
 // no direct access
 defined( '_JEXEC' ) or die( 'Restricted access' );
 
-//Global definitions
-define( 'DS', DIRECTORY_SEPARATOR );
-
 //Joomla framework path definitions
 $parts = explode( DS, JPATH_BASE );
 array_pop( $parts );
--- installation/index.php
+++ installation/index.php
@@ -14,8 +14,10 @@ define( '_JEXEC', 1 );
 
 define( 'JPATH_BASE', dirname( __FILE__ ) );
 
-require_once( JPATH_BASE .'/includes/defines.php' );
-require_once( JPATH_BASE .'/includes/framework.php' );
+define( 'DS', DIRECTORY_SEPARATOR );
+
+require_once ( JPATH_BASE .DS.'includes'.DS.'defines.php' );
+require_once ( JPATH_BASE .DS.'includes'.DS.'framework.php' );
 
 // create the mainframe object
 $mainframe =& JFactory::getApplication('installation');
