e9d4d3e464ae85fdd9fabc18a51df9462ed4a0f2
Hannes Papenberg <info@joomlager.de>
2007-09-09 18:10:10 +0000

    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@8805 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- components/com_content/models/category.php
+++ components/com_content/models/category.php
@@ -355,6 +355,7 @@ class ContentModelCategory extends JModel
 		$query = 'SELECT a.id, a.title, a.title_alias, a.introtext, a.fulltext, a.sectionid, a.state, a.catid, a.created, a.created_by, a.created_by_alias, a.modified, a.modified_by,' .
 			' a.checked_out, a.checked_out_time, a.publish_up, a.publish_down, a.attribs, a.hits, a.images, a.urls, a.ordering, a.metakey, a.metadesc, a.access,' .
 			' CASE WHEN CHAR_LENGTH(a.alias) THEN CONCAT_WS(":", a.id, a.alias) ELSE a.id END as slug,'.
+			' CASE WHEN CHAR_LENGTH(cc.alias) THEN CONCAT_WS(":", cc.id, cc.alias) ELSE cc.id END as catslug,'.
 			' CHAR_LENGTH( a.`fulltext` ) AS readmore, u.name AS author, u.usertype, g.name AS groups'.$voting['select'] .
 			' FROM #__content AS a' .
 			' LEFT JOIN #__categories AS cc ON a.catid = cc.id' .
