3abd896eae31d26928917aa6f2be274d7cf4764c
Hannes Papenberg <info@joomlager.de>
2007-09-16 10:48:45 +0000

    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@8925 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- administrator/components/com_templates/admin.templates.html.php
+++ administrator/components/com_templates/admin.templates.html.php
@@ -393,7 +393,7 @@ class TemplatesView
 			<fieldset class="adminform">
 				<legend><?php echo JText::_( 'Parameters' ); ?></legend>
 				<?php $templatefile = DS.'templates'.DS.$template.DS.'params.ini';
-				echo is_writable($client->path.templatefile) ? JText::sprintf('PARAMSWRITABLE', $templatefile):JText::sprintf('PARAMSUNWRITABLE', $templatefile); ?>
+				echo is_writable($client->path.$templatefile) ? JText::sprintf('PARAMSWRITABLE', $templatefile):JText::sprintf('PARAMSUNWRITABLE', $templatefile); ?>
 				<table class="admintable">
 				<tr>
 					<td>
