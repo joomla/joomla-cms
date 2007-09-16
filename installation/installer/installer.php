62496ab43328340b53c4619c46d55ae1933919c2
Hannes Papenberg <info@joomlager.de>
2007-09-16 10:57:43 +0000

    fixed check in installation for configuration.php
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@8926 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- installation/installer/installer.php
+++ installation/installer/installer.php
@@ -25,7 +25,7 @@ $config = array();
 // check on proper task:
 // lang for installation
 // removedir for remove directory message
-if (file_exists( JPATH_CONFIGURATION . DS . 'configuration.php' ) &&file_exists( JPATH_INSTALLATION . DS . 'index.php' )) {
+if (file_exists( JPATH_CONFIGURATION . DS . 'configuration.php' ) && (filesize( JPATH_CONFIGURATION . DS . 'configuration.php' ) > 10) && file_exists( JPATH_INSTALLATION . DS . 'index.php' )) {
 	$config['default_task']	= 'removedir';
 } else {
 	$config['default_task']	= 'lang';
