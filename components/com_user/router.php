8e6a89693837649b9c419b168c14f3b6c23fa021
Johan Janssens <johan@nooku.org>
2007-09-25 23:40:34 +0000

    Added router Fixed login/logout image path issue
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@9018 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

---
+++ components/com_user/router.php
@@ -0,0 +1,51 @@
+<?php
+/**
+* @version		$Id$
+* @package		Joomla
+* @copyright	Copyright (C) 2005 - 2007 Open Source Matters. All rights reserved.
+* @license		GNU/GPL, see LICENSE.php
+* Joomla! is free software. This version may have been modified pursuant
+* to the GNU General Public License, and as distributed it includes or
+* is derivative of works licensed under the GNU General Public License or
+* other free or open source software licenses.
+* See COPYRIGHT.php for copyright notices and details.
+*/
+
+function UserBuildRoute(&$query)
+{
+	$segments = array();
+	
+	if(isset($query['view'])) 
+	{
+		if(empty($query['Itemid'])) {
+			$segments[] = $query['view'];
+		} 
+		
+		unset($query['view']);
+	};
+	
+	return $segments;
+}
+
+function UserParseRoute($segments)
+{
+	$vars = array();
+
+	//Get the active menu item
+	$menu =& JSite::getMenu();
+	$item =& $menu->getActive();
+	
+	// Count route segments
+	$count = count($segments);
+	
+	//Standard routing for articles
+	if(!isset($item)) 
+	{
+		$vars['view']  = $segments[0];
+		$vars['id']    = $segments[$count - 1];
+		return $vars;
+	}
+
+	return $vars;
+}
+?>
