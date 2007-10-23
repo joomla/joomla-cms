329a648211aeef88fd7524ca6909d393bd8844a5
Ray Tsai <mimi_huhu@hotmail.com>
2007-10-23 18:34:08 +0000

    # Fixed partial reset function issues at back-end
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@9270 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- administrator/components/com_menus/views/list/tmpl/default.php
+++ administrator/components/com_menus/views/list/tmpl/default.php
@@ -8,7 +8,7 @@
 				<?php echo JText::_( 'Filter' ); ?>:
 				<input type="text" name="search" id="search" value="<?php echo $this->lists['search'];?>" class="text_area" onchange="document.adminForm.submit();" />
 				<button onclick="this.form.submit();"><?php echo JText::_( 'Go' ); ?></button>
-				<button onclick="getElementById('search').value='';this.form.submit();"><?php echo JText::_( 'Reset' ); ?></button>
+				<button onclick="document.getElementById('search').value='';this.form.getElementById('levellimit').value='10';this.form.getElementById('filter_state').value='';this.form.submit();"><?php echo JText::_( 'Reset' ); ?></button>
 			</td>
 			<td nowrap="nowrap">
 				<?php
