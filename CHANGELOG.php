a54d9619505302b982adc0912f2d9cedd14e3c11
Samuel Moffatt <pasamio@gmail.com>
2007-12-03 03:23:00 +0000

    # Fixed [#7565] Untranslated string found during Joomla! installation
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@9472 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- CHANGELOG.php
+++ CHANGELOG.php
@@ -37,6 +37,9 @@ Legend:
 - -> Removed
 ! -> Note
 
+3-Dec-2007 Sam Moffatt
+ # Fixed [#7565] Untranslated string found during Joomla! installation
+
 30-Nov-2007 Toby Patterson
  # Fixed 9470 - One letter domainname in email not valid
 
