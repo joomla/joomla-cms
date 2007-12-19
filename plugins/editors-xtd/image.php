30c84c12edcfdeb642cb8819ec9ac1ca94429f90
Andrew Eddie <andrew.eddie@newlifeinit.com>
2007-12-19 23:37:52 +0000

    Cosmetics
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@9680 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- plugins/editors-xtd/image.php
+++ plugins/editors-xtd/image.php
@@ -1,15 +1,15 @@
 <?php
 /**
-* @version		$Id$
-* @package		Joomla
-* @copyright	Copyright (C) 2005 - 2007 Open Source Matters. All rights reserved.
-* @license		GNU/GPL, see LICENSE.php
-* Joomla! is free software. This version may have been modified pursuant
-* to the GNU General Public License, and as distributed it includes or
-* is derivative of works licensed under the GNU General Public License or
-* other free or open source software licenses.
-* See COPYRIGHT.php for copyright notices and details.
-*/
+ * @version		$Id$
+ * @package		Joomla
+ * @copyright	Copyright (C) 2005 - 2007 Open Source Matters. All rights reserved.
+ * @license		GNU/GPL, see LICENSE.php
+ * Joomla! is free software. This version may have been modified pursuant
+ * to the GNU General Public License, and as distributed it includes or
+ * is derivative of works licensed under the GNU General Public License or
+ * other free or open source software licenses.
+ * See COPYRIGHT.php for copyright notices and details.
+ */
 
 // no direct access
 defined( '_JEXEC' ) or die( 'Restricted access' );
@@ -25,7 +25,6 @@ jimport( 'joomla.plugin.plugin' );
  */
 class plgButtonImage extends JPlugin
 {
-
 	/**
 	 * Constructor
 	 *
@@ -37,7 +36,8 @@ class plgButtonImage extends JPlugin
 	 * @param 	array  $config  An array that holds the plugin configuration
 	 * @since 1.5
 	 */
-	function plgButtonImage(& $subject, $config) {
+	function plgButtonImage(& $subject, $config)
+	{
 		parent::__construct($subject, $config);
 	}
 
@@ -67,4 +67,3 @@ class plgButtonImage extends JPlugin
 		return $button;
 	}
 }
-?>
--- plugins/editors-xtd/pagebreak.php
+++ plugins/editors-xtd/pagebreak.php
@@ -1,15 +1,15 @@
 <?php
 /**
-* @version		$Id$
-* @package		Joomla
-* @copyright	Copyright (C) 2005 - 2007 Open Source Matters. All rights reserved.
-* @license		GNU/GPL, see LICENSE.php
-* Joomla! is free software. This version may have been modified pursuant
-* to the GNU General Public License, and as distributed it includes or
-* is derivative of works licensed under the GNU General Public License or
-* other free or open source software licenses.
-* See COPYRIGHT.php for copyright notices and details.
-*/
+ * @version		$Id$
+ * @package		Joomla
+ * @copyright	Copyright (C) 2005 - 2007 Open Source Matters. All rights reserved.
+ * @license		GNU/GPL, see LICENSE.php
+ * Joomla! is free software. This version may have been modified pursuant
+ * to the GNU General Public License, and as distributed it includes or
+ * is derivative of works licensed under the GNU General Public License or
+ * other free or open source software licenses.
+ * See COPYRIGHT.php for copyright notices and details.
+ */
 
 // no direct access
 defined( '_JEXEC' ) or die( 'Restricted access' );
@@ -36,7 +36,8 @@ class plgButtonPagebreak extends JPlugin
 	 * @param 	array  $config  An array that holds the plugin configuration
 	 * @since 1.5
 	 */
-	function plgButtonPagebreak(& $subject, $config) {
+	function plgButtonPagebreak(& $subject, $config)
+	{
 		parent::__construct($subject, $config);
 	}
 
@@ -66,4 +67,3 @@ class plgButtonPagebreak extends JPlugin
 		return $button;
 	}
 }
-?>
