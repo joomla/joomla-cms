ac9e10577a20392131d27b1255e41e8ab044ef33
Andrew Eddie <andrew.eddie@newlifeinit.com>
2007-12-20 22:02:41 +0000

    Cosmetics
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@9706 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- administrator/components/com_installer/helper.php
+++ administrator/components/com_installer/helper.php
@@ -30,6 +30,8 @@ class InstallerHelper
 	 */
 	function writable( $folder )
 	{
-		return is_writable( JPATH_ROOT.DS.$folder ) ? "<strong><span class=\"writable\">".JText::_( 'Writable' )."</span></strong>" : "<strong><span class=\"unwritable\">".JText::_( 'Unwritable' )."</span></strong>";
+		return is_writable( JPATH_ROOT.DS.$folder )
+			? '<strong><span class="writable">'.JText::_( 'Writable' ).'</span></strong>'
+			: '<strong><span class="unwritable">'.JText::_( 'Unwritable' ).'</span></strong>';
 	}
 }
