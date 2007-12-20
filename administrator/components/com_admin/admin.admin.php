e57bdd5f127029e355fe90c89152033dcb23be7d
Andrew Eddie <andrew.eddie@newlifeinit.com>
2007-12-20 21:42:07 +0000

    Cosmetics
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@9694 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- administrator/components/com_admin/admin.admin.php
+++ administrator/components/com_admin/admin.admin.php
@@ -1,22 +1,23 @@
 <?php
 /**
-* @version		$Id$
-* @package		Joomla
-* @subpackage	Admin
-* @copyright	Copyright (C) 2005 - 2007 Open Source Matters. All rights reserved.
-* @license		GNU/GPL, see LICENSE.php
-* Joomla! is free software. This version may have been modified pursuant
-* to the GNU General Public License, and as distributed it includes or
-* is derivative of works licensed under the GNU General Public License or
-* other free or open source software licenses.
-* See COPYRIGHT.php for copyright notices and details.
-*/
+ * @version		$Id$
+ * @package		Joomla
+ * @subpackage	Admin
+ * @copyright	Copyright (C) 2005 - 2007 Open Source Matters. All rights reserved.
+ * @license		GNU/GPL, see LICENSE.php
+ * Joomla! is free software. This version may have been modified pursuant
+ * to the GNU General Public License, and as distributed it includes or
+ * is derivative of works licensed under the GNU General Public License or
+ * other free or open source software licenses.
+ * See COPYRIGHT.php for copyright notices and details.
+ */
 
 // no direct access
 defined( '_JEXEC' ) or die( 'Restricted access' );
 require_once( JApplicationHelper::getPath( 'admin_html' ) );
 
-switch ($task) {
+switch ($task)
+{
 	case 'sysinfo':
 		HTML_admin_misc::system_info( );
 		break;
@@ -45,4 +46,3 @@ switch ($task) {
 		return;
 		break;
 }
-?>
