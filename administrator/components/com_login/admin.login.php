fa246430c4df73b308ef0bce2ff6793ae39ccd83
Andrew Eddie <andrew.eddie@newlifeinit.com>
2008-01-03 00:45:28 +0000

    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@9814 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- administrator/components/com_login/admin.login.php
+++ administrator/components/com_login/admin.login.php
@@ -52,11 +52,8 @@ class LoginController
 	{
 		global $mainframe;
 
-		// Check for request forgeries.
-		$token = JUtility::getToken();
-		if (!JRequest::getInt($token, 0, 'post')) {
-			JError::raiseError(403, 'Request Forbidden');
-		}
+		// Check for request forgeries
+		JRequest::checkToken() or die( 'Invalid Token' );
 
 		$credentials = array();
 
