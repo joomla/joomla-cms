7a85a0511dbdb38ccada37d3675664b6f9523486
Andrew Eddie <andrew.eddie@newlifeinit.com>
2009-01-02 23:08:28 +0000

    Docblock updates
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@11384 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- components/com_banners/banners.php
+++ components/com_banners/banners.php
@@ -5,11 +5,6 @@
  * @subpackage	Banners
  * @copyright	Copyright (C) 2005 - 2008 Open Source Matters, Inc. All rights reserved.
  * @license		GNU General Public License, see LICENSE.php
- * Joomla! is free software. This version may have been modified pursuant to the
- * GNU General Public License, and as distributed it includes or is derivative
- * of works licensed under the GNU General Public License or other free or open
- * source software licenses. See COPYRIGHT.php for copyright notices and
- * details.
  */
 
 // no direct access
