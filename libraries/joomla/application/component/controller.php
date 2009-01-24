d926d47fa8ea8d2cf897cded05dc3e2a09369fea
Rob Schley <rschley@ebay.com>
2009-01-24 12:06:04 +0000

    Added JController::getInstance() to make creating controller instances more generic and to allow for the singleton pattern and to allow for the concept of a protocol specific controller.
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@11424 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- libraries/joomla/application/component/controller.php
+++ libraries/joomla/application/component/controller.php
@@ -126,6 +126,78 @@ abstract class JController extends JObject
 	protected $_params = null;
 
 	/**
+	 * Method to get a singleton controller instance.
+	 *
+	 * @access	public
+	 * @param	string		$name		The prefix for the controller.
+	 * @param	array		$config		An array of optional constructor options.
+	 * @return	mixed		JController derivative class or JException on error.
+	 * @since	1.6
+	 */
+	public static function &getInstance($prefix, $config = array())
+	{
+		static $instance;
+
+		if (!empty($instance)) {
+			return $instance;
+		}
+
+		// Get the environment configuration.
+		$basePath	= array_key_exists('base_path', $config) ? $config['base_path'] : JPATH_COMPONENT;
+		$protocol	= JRequest::getWord('protocol');
+		$command	= JRequest::getCmd('task', 'display');
+
+		// Check for a controller.task command.
+		if (strpos($command, '.') !== false)
+		{
+			// Explode the controller.task command.
+			list($type, $task) = explode('.', $command);
+
+			// Define the controller filename and path.
+			$file	= $this->_createFileName('controller', array('name' => $type, 'protocol' => $protocol));
+			$path	= $basePath.DS.'controllers'.DS.$file;
+
+			// Reset the task without the contoller context.
+			JRequest::setVar('task', $task);
+		}
+		else
+		{
+			// Base controller.
+			$type	= null;
+			$task	= $command;
+
+			// Define the controller filename and path.
+			$file	= $this->_createFileName('controller', array('name' => 'controller', 'protocol' => $protocol));
+			$path	= $basePath.DS.$file;
+		}
+
+		// Get the controller class name.
+		$class = ucfirst($prefix).'Controller'.ucfirst($type);
+
+		// Include the class if not present.
+		if (!class_exists($class))
+		{
+			// If the controller file path exists, include it.
+			if (file_exists($path)) {
+				require_once $path;
+			} else {
+				$error = JError::raiseError(500, JText::sprintf('INVALID CONTROLLER', $type));
+				return $error;
+			}
+		}
+
+		// Instantiate the class.
+		if (class_exists($class)) {
+			$instance = new $class($config);
+		} else {
+			$error = JError::raiseError(500, JText::sprintf('INVALID CONTROLLER CLASS', $class));
+			return $error;
+		}
+
+		return $instance;
+	}
+
+	/**
 	 * Constructor.
 	 *
 	 * @access	protected
@@ -705,6 +777,14 @@ abstract class JController extends JObject
 
 		switch ( $type )
 		{
+			case 'controller':
+				if (!empty($parts['protocol'])) {
+					$parts['protocol'] = '.'.$parts['protocol'];
+				}
+
+				$filename = strtolower($parts['name']).$parts['protocol'].'.php';
+				break;
+
 			case 'view':
 				if ( !empty( $parts['type'] ) ) {
 					$parts['type'] = '.'.$parts['type'];
