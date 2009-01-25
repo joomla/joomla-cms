0eeb3e604a0530edd0cf3415816e11762be0c599
Louis Landry <louis.landry@joomla.org>
2009-01-25 09:35:04 +0000

    Some mootools fixes to media manager (stop gap)
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@11497 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- administrator/components/com_media/assets/mediamanager.js
+++ administrator/components/com_media/assets/mediamanager.js
@@ -13,50 +13,47 @@
 /**
  * JMediaManager behavior for media component
  * 
- * @author Johan Janssens <johan.janssens@joomla.org>
- * @author Louis Landry <louis.landry@joomla.org>
  * @package Joomla.Extensions
  * @subpackage Media
  * @since 1.5
  */
 var MediaManager = {
 
-	initialize : function() {
-		this.folderframe = $('folderframe');
+	initialize: function()
+	{
 		this.folderpath = $('folderpath');
-
 		this.updatepaths = $$('input.update-folder');
 
-		this.frame = window.frames['folderframe'];
-		this.frameurl = this.frame.location.href;
-		// this.frameurl = window.frames['folderframe'].location.href;
+		this.frame = new IFrame('folderframe').addEvent('load', function(){
+			MediaManager.onloadframe();
+		});
+		this.frameurl = this.frame.contentWindow.location.href;
 
-		this.tree = new MooTreeControl(
-				{
-					div :'media-tree_tree',
-					mode :'folders',
-					grid :true,
-					theme :'components/com_media/assets/mootree.gif',
-					onClick : function(node) {
-						target = $chk(node.data.target) ? node.data.target
-								: '_self';
-						window.frames[target].location.href = node.data.url;
-					}
-				},
-				{
-					text :'Media',
-					open :true,
-					data : {
-						url :'index.php?option=com_media&view=mediaList&tmpl=component',
-						target :'folderframe'
-					}
-				});
+		this.tree = new MooTreeControl({
+			div :'media-tree_tree',
+			mode :'folders',
+			grid :true,
+			theme :'components/com_media/assets/mootree.gif',
+			onClick: function(node) {
+				target = $chk(node.data.target) ? node.data.target
+						: '_self';
+				window.frames[target].location.href = node.data.url;
+			}
+		},
+		{
+			text :'Media',
+			open :true,
+			data : {
+				url :'index.php?option=com_media&view=mediaList&tmpl=component',
+				target :'folderframe'
+			}
+		});
 		this.tree.adopt('media-tree');
 	},
 
-	submit : function(task) {
-		form = window.frames['folderframe'].document
-				.getElementById('mediamanager-form');
+	submit: function(task)
+	{
+		form = window.frames['folderframe'].document.getElementById('mediamanager-form');
 		form.task.value = task;
 		if ($('username')) {
 			form.username.value = $('username').value;
@@ -65,27 +62,28 @@ var MediaManager = {
 		form.submit();
 	},
 
-	onloadframe : function() {
+	onloadframe: function()
+	{
 		// Update the frame url
-		this.frameurl = this.frame.location.href;
+		this.frameurl = this.frame.contentWindow.location.href;
 
 		var folder = this.getFolder();
 		if (folder) {
-			this.updatepaths.each( function(path) {
-				path.value = folder;
+			this.updatepaths.each(function(path) {
+				path.set('value', folder);
 			});
-			this.folderpath.value = basepath + '/' + folder;
-			node = this.tree.get('node_' + folder);
-			console.log(this.tree);
-			console.log(node);
+			this.folderpath.value = basepath+'/'+folder;
+			node = this.tree.get('node_'+folder);
 			node.toggle(false, true);
-		} else {
+		}
+		else {
 			this.updatepaths.each( function(path) {
-				path.value = '';
+				path.set('value', '');
 			});
 			this.folderpath.value = basepath;
 			node = this.tree.root;
 		}
+		console.log(this.frameurl);return;
 
 		if (node) {
 			this.tree.select(node, true);
@@ -97,7 +95,7 @@ var MediaManager = {
 		q = $H(this._getQueryObject(a.query));
 		q.set('folder', folder);
 		var query = [];
-		q.each( function(v, k) {
+		q.each(function(v, k) {
 			if ($chk(v)) {
 				this.push(k + '=' + v);
 			}
@@ -105,100 +103,95 @@ var MediaManager = {
 		a.query = query.join('&');
 		if (a.port) {
 			$('uploadForm').setProperty(
-					'action',
-					a.scheme + '://' + a.domain + ':' + a.port + a.path + '?'
-							+ a.query);
-		} else {
-			$('uploadForm').setProperty('action',
-					a.scheme + '://' + a.domain + a.path + '?' + a.query);
+				'action',
+				a.scheme+'://'+a.domain+':'+a.port+a.path+'?'+a.query
+			);
+		}
+		else {
+			$('uploadForm').setProperty(
+				'action',
+				a.scheme+'://'+a.domain+a.path+'?'+a.query
+			);
 		}
 	},
 
-	oncreatefolder : function() {
+	oncreatefolder: function()
+	{
 		if ($('foldername').value.length) {
 			$('dirpath').value = this.getFolder();
 			submitbutton('createfolder');
 		}
 	},
 
-	setViewType : function(type) {
+	setViewType: function(type)
+	{
 		$(type).addClass('active');
 		$(viewstyle).removeClass('active');
 		viewstyle = type;
 		var folder = this.getFolder();
-		this
-				._setFrameUrl('index.php?option=com_media&view=mediaList&tmpl=component&folder='
-						+ folder + '&layout=' + type);
+		this._setFrameUrl('index.php?option=com_media&view=mediaList&tmpl=component&folder='+folder+'&layout='+type);
 	},
 
-	refreshFrame : function() {
+	refreshFrame: function()
+	{
 		this._setFrameUrl();
 	},
 
-	getFolder : function() {
-		var url = this.frame.location.search.substring(1);
+	getFolder: function()
+	{
+		var url = this.frame.contentWindow.location.search.substring(1);
 		var args = this.parseQuery(url);
-
-		if (args['folder'] == "undefined") {
-			args['folder'] = "";
-		}
-
-		return args['folder'];
+		console.log(args.get('folder'));
+		return args.get('folder');
 	},
 
-	parseQuery : function(query) {
-		var params = new Object();
+	parseQuery: function(query)
+	{
+		var params = new Hash();
 		if (!query) {
 			return params;
 		}
 		var pairs = query.split(/[;&]/);
-		for ( var i = 0; i < pairs.length; i++) {
+		for ( var i = 0; i < pairs.length; i++)
+		{
 			var KeyVal = pairs[i].split('=');
 			if (!KeyVal || KeyVal.length != 2) {
 				continue;
 			}
-			var key = unescape(KeyVal[0]);
-			var val = unescape(KeyVal[1]).replace(/\+ /g, ' ');
-			params[key] = val;
+			params.set(unescape(KeyVal[0]), unescape(KeyVal[1]).replace(/\+ /g, ' '));
 		}
 		return params;
 	},
 
-	_setFrameUrl : function(url) {
+	_setFrameUrl: function(url) {
 		if ($chk(url)) {
 			this.frameurl = url;
 		}
-		this.frame.location.href = this.frameurl;
+		this.frame.contentWindow.location.href = this.frameurl;
 	},
 
-	_getQueryObject : function(q) {
+	_getQueryObject: function(q)
+	{
 		var vars = q.split(/[&;]/);
 		var rs = {};
-		if (vars.length)
-			vars
-					.each( function(val) {
-						var keys = val.split('=');
-						if (keys.length && keys.length == 2)
-							rs[encodeURIComponent(keys[0])] = encodeURIComponent(keys[1]);
-					});
+		if (vars.length) {
+			vars.each(function(val) {
+				var keys = val.split('=');
+				if (keys.length && keys.length == 2) {
+					rs[encodeURIComponent(keys[0])] = encodeURIComponent(keys[1]);
+				}
+			});
+		}
 		return rs;
 	},
 
-	_getUriObject : function(u) {
-		var bits = u
-				.match(/^(?:([^:\/?#.]+):)?(?:\/\/)?(([^:\/?#]*)(?::(\d*))?)((\/(?:[^?#](?![^?#\/]*\.[^?#\/.]+(?:[\?#]|$)))*\/?)?([^?#\/]*))?(?:\?([^#]*))?(?:#(.*))?/);
-		return (bits) ? bits.associate( [ 'uri', 'scheme', 'authority',
-				'domain', 'port', 'path', 'directory', 'file', 'query',
-				'fragment' ]) : null;
+	_getUriObject: function(u)
+	{
+		var bits = u.match(/^(?:([^:\/?#.]+):)?(?:\/\/)?(([^:\/?#]*)(?::(\d*))?)((\/(?:[^?#](?![^?#\/]*\.[^?#\/.]+(?:[\?#]|$)))*\/?)?([^?#\/]*))?(?:\?([^#]*))?(?:#(.*))?/);
+		return (bits) ? bits.associate([ 'uri', 'scheme', 'authority', 'domain', 'port', 'path', 'directory', 'file', 'query', 'fragment' ]) : null;
 	}
 };
 
 window.addEvent('domready', function() {
-	// Added to populate data on iframe load
-		MediaManager.initialize();
-		MediaManager.trace = 'start';
-		document.updateUploader = function() {
-			MediaManager.onloadframe();
-		};
-		MediaManager.onloadframe();
-	});
+	MediaManager.initialize();
+});
