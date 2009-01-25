cb6d86b5ea721526a0f8f24a3c2665dd4744e391
Anthony Ferrara <ircmaxell@yahoo.com>
2009-01-25 03:46:01 +0000

    Wrapped installer index.php in try{}catch() block
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@11444 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- installation/index.php
+++ installation/index.php
@@ -14,22 +14,25 @@ define( '_JEXEC', 1 );
 define( 'JPATH_BASE', dirname( __FILE__ ) );
 
 define( 'DS', DIRECTORY_SEPARATOR );
-
-require_once(JPATH_BASE . DS . 'includes' . DS . 'defines.php');
-require_once(JPATH_BASE . DS . 'includes' . DS . 'framework.php');
-
-// create the mainframe object
-$mainframe = JFactory::getApplication('installation');
-
-// initialuse the application
-$mainframe->initialise();
-
-// render the application
-$mainframe->render();
-
-
-
-/**
- * RETURN THE RESPONSE
- */
-echo JResponse::toString();
+try {
+	require_once(JPATH_BASE . DS . 'includes' . DS . 'defines.php');
+	require_once(JPATH_BASE . DS . 'includes' . DS . 'framework.php');
+	
+	// create the mainframe object
+	$mainframe = JFactory::getApplication('installation');
+	
+	// initialuse the application
+	$mainframe->initialise();
+	
+	// render the application
+	$mainframe->render();
+	
+	/**
+	* RETURN THE RESPONSE
+ 	*/
+	echo JResponse::toString();
+	
+} catch(JException $e) {
+	$e->set('level', E_ERROR);
+	JError::throwError($e);	
+}
