142eee51f2444b59f38d21b3ee86dd6ab52db661
Wilco Jansen <jansen.wilco@gmail.com>
2009-01-25 08:01:46 +0000

    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@11492 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- libraries/joomla/backup/backupadapter.php
+++ libraries/joomla/backup/backupadapter.php
@@ -1,22 +1,14 @@
 <?php
 /**
- * Document Description
- * 
- * Document Long Description 
- * 
- * PHP4/5
- *  
- * Created on Jan 16, 2009
- * 
- * @package package_name
- * @author Your Name <author@toowoombarc.qld.gov.au>
- * @author Toowoomba Regional Council Information Management Branch
- * @license GNU/GPL http://www.gnu.org/licenses/gpl.html
- * @copyright 2009 Toowoomba Regional Council/Developer Name 
- * @version SVN: $Id$
- * @see http://joomlacode.org/gf/project/   JoomlaCode Project:    
+ * @version		$Id$
+ * @package		Joomla.Framework
+ * @subpackage	Backup
+ * @copyright	Copyright (C) 2005 - 2009 Open Source Matters, Inc. All rights reserved.
+ * @license		GNU General Public License, see LICENSE.php
  */
- 
+
+// No direct access
+defined('JPATH_BASE') or die();
  
 interface JBackupAdapter {
 	public function remove($options=Array());
