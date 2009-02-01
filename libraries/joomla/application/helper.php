4433cb3a2504c2ce9412da183051106c98699dcc
Andrew Eddie <andrew.eddie@newlifeinit.com>
2009-02-01 04:23:38 +0000

    Bug fix parameters path
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@11558 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- libraries/joomla/application/helper.php
+++ libraries/joomla/application/helper.php
@@ -217,7 +217,7 @@ abstract class JApplicationHelper
 				$j15path 	= DS.'plugins'.DS. $user_option .'.xml';
 				$parts = explode(DS, $user_option);
 				$j16path   = DS.'plugins'.DS. $user_option.DS.$parts[0].'.xml';
-				$j15 = JApplicationHelper::_checkPath($path, 0);
+				$j15 = JApplicationHelper::_checkPath($j15path, 0);
 				$j16 = JApplicationHelper::_checkPath($j16path, 0);
 				// return 1.6 if working otherwise default to whatever 1.5 gives us
 				$result = $j16 ? $j16 : $j15;
