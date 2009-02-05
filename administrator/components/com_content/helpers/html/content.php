bbc5b85fa0b90f25a73516e76d83a9fd357bc8aa
Andrew Eddie <andrew.eddie@newlifeinit.com>
2009-02-05 10:52:38 +0000

    Add warning icon for empty meta elements
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@11596 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- administrator/components/com_content/helpers/html/content.php
+++ administrator/components/com_content/helpers/html/content.php
@@ -18,47 +18,71 @@
 class JHtmlContent
 {
 	/**
+	 * Displays warnings relating to an article (emtpy metadecs, metakey, etc)
+	 *
+	 * @param	object $article	The article object
+	 * @return	string
+	 */
+	function warnings($article)
+	{
+		$html		= '';
+		$warnings	= array();
+		if (empty($article->metadesc)) {
+			$warnings[] = JText::_('Content_Warning_Empty_metadesc');
+		}
+		if (empty($article->metakey)) {
+			$warnings[] = JText::_('Content_Warning_Empty_metakey');
+		}
+		if (!empty($warnings)) {
+			JHtml::_('behavior.tooltip');
+			$html	= '<span class="hasTip" title="'.JText::_('Content_Warning_Title').'::'.implode('<br />', $warnings).'">'
+					. '<img src="'.JURI::root().'media/system/images/warning.png" border="0"  alt="" /></span>';
+		}
+		return $html;
+	}
+
+	/**
 	 * Displays the publishing state legend for articles
 	 */
-	function Legend( )
+	function Legend()
 	{
 		?>
 		<table cellspacing="0" cellpadding="4" border="0" align="center">
 		<tr align="center">
 			<td>
-			<img src="images/publish_y.png" width="16" height="16" border="0" alt="<?php echo JText::_( 'Pending' ); ?>" />
+			<img src="images/publish_y.png" width="16" height="16" border="0" alt="<?php echo JText::_('Pending'); ?>" />
 			</td>
 			<td>
-			<?php echo JText::_( 'Published, but is' ); ?> <u><?php echo JText::_( 'Pending' ); ?></u> |
+			<?php echo JText::_('Published, but is'); ?> <u><?php echo JText::_('Pending'); ?></u> |
 			</td>
 			<td>
-			<img src="images/publish_g.png" width="16" height="16" border="0" alt="<?php echo JText::_( 'Visible' ); ?>" />
+			<img src="images/publish_g.png" width="16" height="16" border="0" alt="<?php echo JText::_('Visible'); ?>" />
 			</td>
 			<td>
-			<?php echo JText::_( 'Published and is' ); ?> <u><?php echo JText::_( 'Current' ); ?></u> |
+			<?php echo JText::_('Published and is'); ?> <u><?php echo JText::_('Current'); ?></u> |
 			</td>
 			<td>
-			<img src="images/publish_r.png" width="16" height="16" border="0" alt="<?php echo JText::_( 'Finished' ); ?>" />
+			<img src="images/publish_r.png" width="16" height="16" border="0" alt="<?php echo JText::_('Finished'); ?>" />
 			</td>
 			<td>
-			<?php echo JText::_( 'Published, but has' ); ?> <u><?php echo JText::_( 'Expired' ); ?></u> |
+			<?php echo JText::_('Published, but has'); ?> <u><?php echo JText::_('Expired'); ?></u> |
 			</td>
 			<td>
-			<img src="images/publish_x.png" width="16" height="16" border="0" alt="<?php echo JText::_( 'Finished' ); ?>" />
+			<img src="images/publish_x.png" width="16" height="16" border="0" alt="<?php echo JText::_('Finished'); ?>" />
 			</td>
 			<td>
-			<?php echo JText::_( 'Not Published' ); ?> |
+			<?php echo JText::_('Not Published'); ?> |
 			</td>
 			<td>
-			<img src="images/disabled.png" width="16" height="16" border="0" alt="<?php echo JText::_( 'Archived' ); ?>" />
+			<img src="images/disabled.png" width="16" height="16" border="0" alt="<?php echo JText::_('Archived'); ?>" />
 			</td>
 			<td>
-			<?php echo JText::_( 'Archived' ); ?>
+			<?php echo JText::_('Archived'); ?>
 			</td>
 		</tr>
 		<tr>
 			<td colspan="10" align="center">
-			<?php echo JText::_( 'Click on icon to toggle state.' ); ?>
+			<?php echo JText::_('Click on icon to toggle state.'); ?>
 			</td>
 		</tr>
 		</table>
--- administrator/components/com_content/views/articles/tmpl/default.php
+++ administrator/components/com_content/views/articles/tmpl/default.php
@@ -19,10 +19,10 @@
 	<table>
 		<tr>
 			<td width="100%">
-				<?php echo JText::_( 'Filter' ); ?>:
-				<input type="text" name="search" id="search" value="<?php echo $this->filter->search;?>" class="text_area" onchange="document.adminForm.submit();" title="<?php echo JText::_( 'Filter by title or enter article ID' );?>"/>
-				<button onclick="this.form.submit();"><?php echo JText::_( 'Go' ); ?></button>
-				<button onclick="document.getElementById('search').value='';this.form.getElementById('filter_sectionid').value='-1';this.form.getElementById('filter_catid').value='0';this.form.getElementById('filter_authorid').value='0';this.form.getElementById('filter_state').value='';this.form.submit();"><?php echo JText::_( 'Reset' ); ?></button>
+				<?php echo JText::_('Filter'); ?>:
+				<input type="text" name="search" id="search" value="<?php echo $this->filter->search;?>" class="text_area" onchange="document.adminForm.submit();" title="<?php echo JText::_('Filter by title or enter article ID');?>"/>
+				<button onclick="this.form.submit();"><?php echo JText::_('Go'); ?></button>
+				<button onclick="document.getElementById('search').value='';this.form.getElementById('filter_sectionid').value='-1';this.form.getElementById('filter_catid').value='0';this.form.getElementById('filter_authorid').value='0';this.form.getElementById('filter_state').value='';this.form.submit();"><?php echo JText::_('Reset'); ?></button>
 			</td>
 			<td nowrap="nowrap">
 				<?php
@@ -39,44 +39,44 @@
 	<thead>
 		<tr>
 			<th width="5">
-				<?php echo JText::_( 'Num' ); ?>
+				<?php echo JText::_('Num'); ?>
 			</th>
 			<th width="5">
-				<input type="checkbox" name="toggle" value="" onclick="checkAll(<?php echo count( $this->rows ); ?>);" />
+				<input type="checkbox" name="toggle" value="" onclick="checkAll(<?php echo count($this->rows); ?>);" />
 			</th>
 			<th class="title">
-				<?php echo JHtml::_('grid.sort',   'Title', 'c.title', @$this->filter->order_Dir, @$this->filter->order ); ?>
+				<?php echo JHtml::_('grid.sort',   'Title', 'c.title', @$this->filter->order_Dir, @$this->filter->order); ?>
 			</th>
 			<th width="1%" nowrap="nowrap">
-				<?php echo JHtml::_('grid.sort',   'Published', 'c.state', @$this->filter->order_Dir, @$this->filter->order ); ?>
+				<?php echo JHtml::_('grid.sort',   'Published', 'c.state', @$this->filter->order_Dir, @$this->filter->order); ?>
 			</th>
 			<th nowrap="nowrap" width="1%">
-				<?php echo JHtml::_('grid.sort',   'Front Page', 'frontpage', @$this->filter->order_Dir, @$this->filter->order ); ?>
+				<?php echo JHtml::_('grid.sort',   'Front Page', 'frontpage', @$this->filter->order_Dir, @$this->filter->order); ?>
 			</th>
 			<th width="12%">
-				<?php echo JHtml::_('grid.sort',   'Order', 'section_name', @$this->filter->order_Dir, @$this->filter->order ); ?>
-				<?php echo JHtml::_('grid.order',  $this->rows ); ?>
+				<?php echo JHtml::_('grid.sort',   'Order', 'section_name', @$this->filter->order_Dir, @$this->filter->order); ?>
+				<?php echo JHtml::_('grid.order',  $this->rows); ?>
 			</th>
 			<th width="7%">
-				<?php echo JHtml::_('grid.sort',   'Access', 'groupname', @$this->filter->order_Dir, @$this->filter->order ); ?>
+				<?php echo JHtml::_('grid.sort',   'Access', 'groupname', @$this->filter->order_Dir, @$this->filter->order); ?>
 			</th>
 			<th class="title" width="8%" nowrap="nowrap">
-				<?php echo JHtml::_('grid.sort',   'Section', 'section_name', @$this->filter->order_Dir, @$this->filter->order ); ?>
+				<?php echo JHtml::_('grid.sort',   'Section', 'section_name', @$this->filter->order_Dir, @$this->filter->order); ?>
 			</th>
 			<th  class="title" width="8%" nowrap="nowrap">
-				<?php echo JHtml::_('grid.sort',   'Category', 'cc.name', @$this->filter->order_Dir, @$this->filter->order ); ?>
+				<?php echo JHtml::_('grid.sort',   'Category', 'cc.name', @$this->filter->order_Dir, @$this->filter->order); ?>
 			</th>
 			<th  class="title" width="8%" nowrap="nowrap">
-				<?php echo JHtml::_('grid.sort',   'Author', 'author', @$this->filter->order_Dir, @$this->filter->order ); ?>
+				<?php echo JHtml::_('grid.sort',   'Author', 'author', @$this->filter->order_Dir, @$this->filter->order); ?>
 			</th>
 			<th align="center" width="10">
-				<?php echo JHtml::_('grid.sort',   'Date', 'c.created', @$this->filter->order_Dir, @$this->filter->order ); ?>
+				<?php echo JHtml::_('grid.sort',   'Date', 'c.created', @$this->filter->order_Dir, @$this->filter->order); ?>
 			</th>
 			<th align="center" width="10">
-				<?php echo JHtml::_('grid.sort',   'Hits', 'c.hits', @$this->filter->order_Dir, @$this->filter->order ); ?>
+				<?php echo JHtml::_('grid.sort',   'Hits', 'c.hits', @$this->filter->order_Dir, @$this->filter->order); ?>
 			</th>
 			<th width="1%" class="title">
-				<?php echo JHtml::_('grid.sort',   'ID', 'c.id', @$this->filter->order_Dir, @$this->filter->order ); ?>
+				<?php echo JHtml::_('grid.sort',   'ID', 'c.id', @$this->filter->order_Dir, @$this->filter->order); ?>
 			</th>
 		</tr>
 	</thead>
@@ -90,127 +90,127 @@
 	<tbody>
 	<?php
 	$k = 0;
-	for ($i=0, $n=count( $this->rows ); $i < $n; $i++)
+	for ($i=0, $n=count($this->rows); $i < $n; $i++)
 	{
 		$row = &$this->rows[$i];
 
 		$link 	= 'index.php?option=com_content&sectionid='. $this->redirect .'&task=edit&cid[]='. $row->id;
 
-		$row->sect_link = JRoute::_( 'index.php?option=com_sections&task=edit&cid[]='. $row->sectionid );
-		$row->cat_link 	= JRoute::_( 'index.php?option=com_categories&task=edit&cid[]='. $row->catid );
+		$row->sect_link = JRoute::_('index.php?option=com_sections&task=edit&cid[]='. $row->sectionid);
+		$row->cat_link 	= JRoute::_('index.php?option=com_categories&task=edit&cid[]='. $row->catid);
 
 		$publish_up = new JDate($row->publish_up);
 		$publish_down = new JDate($row->publish_down);
 		$publish_up->setOffset($config->getValue('config.offset'));
 		$publish_down->setOffset($config->getValue('config.offset'));
-		if ( $now->toUnix() <= $publish_up->toUnix() && $row->state == 1 ) {
+		if ($now->toUnix() <= $publish_up->toUnix() && $row->state == 1) {
 			$img = 'publish_y.png';
-			$alt = JText::_( 'Published' );
-		} else if ( ( $now->toUnix() <= $publish_down->toUnix() || $row->publish_down == $nullDate ) && $row->state == 1 ) {
+			$alt = JText::_('Published');
+		} else if (($now->toUnix() <= $publish_down->toUnix() || $row->publish_down == $nullDate) && $row->state == 1) {
 			$img = 'publish_g.png';
-			$alt = JText::_( 'Published' );
-		} else if ( $now->toUnix() > $publish_down->toUnix() && $row->state == 1 ) {
+			$alt = JText::_('Published');
+		} else if ($now->toUnix() > $publish_down->toUnix() && $row->state == 1) {
 			$img = 'publish_r.png';
-			$alt = JText::_( 'Expired' );
-		} else if ( $row->state == 0 ) {
+			$alt = JText::_('Expired');
+		} else if ($row->state == 0) {
 			$img = 'publish_x.png';
-			$alt = JText::_( 'Unpublished' );
-		} else if ( $row->state == -1 ) {
+			$alt = JText::_('Unpublished');
+		} else if ($row->state == -1) {
 			$img = 'disabled.png';
-			$alt = JText::_( 'Archived' );
+			$alt = JText::_('Archived');
 		}
 		$times = '';
 		if (isset($row->publish_up)) {
 			if ($row->publish_up == $nullDate) {
-				$times .= JText::_( 'Start: Always' );
+				$times .= JText::_('Start: Always');
 			} else {
-				$times .= JText::_( 'Start' ) .": ". $publish_up->toFormat();
+				$times .= JText::_('Start') .": ". $publish_up->toFormat();
 			}
 		}
 		if (isset($row->publish_down)) {
 			if ($row->publish_down == $nullDate) {
-				$times .= "<br />". JText::_( 'Finish: No Expiry' );
+				$times .= "<br />". JText::_('Finish: No Expiry');
 			} else {
-				$times .= "<br />". JText::_( 'Finish' ) .": ". $publish_down->toFormat();
+				$times .= "<br />". JText::_('Finish') .": ". $publish_down->toFormat();
 			}
 		}
 
-		if ( $user->authorize( 'com_users', 'manage' ) ) {
-			if ( $row->created_by_alias ) {
+		if ($user->authorize('com_users', 'manage')) {
+			if ($row->created_by_alias) {
 				$author = $row->created_by_alias;
 			} else {
 				$linkA 	= 'index.php?option=com_users&task=edit&cid[]='. $row->created_by;
-				$author = '<a href="'. JRoute::_( $linkA ) .'" title="'. JText::_( 'Edit User' ) .'">'. $row->author .'</a>';
+				$author = '<a href="'. JRoute::_($linkA) .'" title="'. JText::_('Edit User') .'">'. $row->author .'</a>';
 			}
 		} else {
-			if ( $row->created_by_alias ) {
+			if ($row->created_by_alias) {
 				$author = $row->created_by_alias;
 			} else {
 				$author = $row->author;
 			}
 		}
 
-		$access 	= JHtml::_('grid.access',   $row, $i, $row->state );
-		$checked 	= JHtml::_('grid.checkedout',   $row, $i );
+		$checked 	= JHtml::_('grid.checkedout',   $row, $i);
 		?>
 		<tr class="<?php echo "row$k"; ?>">
 			<td>
-				<?php echo $this->pagination->getRowOffset( $i ); ?>
+				<?php echo $this->pagination->getRowOffset($i); ?>
 			</td>
 			<td align="center">
 				<?php echo $checked; ?>
 			</td>
 			<td>
 			<?php
-				if (  JTable::isCheckedOut($user->get ('id'), $row->checked_out ) ) {
+				if ( JTable::isCheckedOut($user->get ('id'), $row->checked_out)) {
 					echo $row->title;
 				} else if ($row->state == -1) {
 					echo htmlspecialchars($row->title, ENT_QUOTES, 'UTF-8');
-					echo ' [ '. JText::_( 'Archived' ) .' ]';
+					echo ' [ '. JText::_('Archived') .' ]';
 				} else {
 					?>
-					<a href="<?php echo JRoute::_( $link ); ?>">
+					<a href="<?php echo JRoute::_($link); ?>">
 						<?php echo htmlspecialchars($row->title, ENT_QUOTES); ?></a>
 					<?php
 				}
 				?>
+				<div style="float:right"><?php echo JHtml::_('content.warnings', $row);?></div>
 			</td>
 			<?php
-			if ( $times ) {
+			if ($times) {
 				?>
 				<td align="center">
-					<span class="editlinktip hasTip" title="<?php echo JText::_( 'Publish Information' );?>::<?php echo $times; ?>"><a href="javascript:void(0);" onclick="return listItemTask('cb<?php echo $i;?>','<?php echo $row->state ? 'unpublish' : 'publish' ?>')">
+					<span class="editlinktip hasTip" title="<?php echo JText::_('Publish Information');?>::<?php echo $times; ?>"><a href="javascript:void(0);" onclick="return listItemTask('cb<?php echo $i;?>','<?php echo $row->state ? 'unpublish' : 'publish' ?>')">
 						<img src="images/<?php echo $img;?>" width="16" height="16" border="0" alt="<?php echo $alt; ?>" /></a></span>
 				</td>
 				<?php
 			}
 			?>
 			<td align="center">
-				<a href="javascript:void(0);" onclick="return listItemTask('cb<?php echo $i;?>','toggle_frontpage')" title="<?php echo ( $row->frontpage ) ? JText::_( 'Yes' ) : JText::_( 'No' );?>">
-					<img src="images/<?php echo ( $row->frontpage ) ? 'tick.png' : ( $row->state != -1 ? 'publish_x.png' : 'disabled.png' );?>" width="16" height="16" border="0" alt="<?php echo ( $row->frontpage ) ? JText::_( 'Yes' ) : JText::_( 'No' );?>" /></a>
+				<a href="javascript:void(0);" onclick="return listItemTask('cb<?php echo $i;?>','toggle_frontpage')" title="<?php echo ($row->frontpage) ? JText::_('Yes') : JText::_('No');?>">
+					<img src="images/<?php echo ($row->frontpage) ? 'tick.png' : ($row->state != -1 ? 'publish_x.png' : 'disabled.png');?>" width="16" height="16" border="0" alt="<?php echo ($row->frontpage) ? JText::_('Yes') : JText::_('No');?>" /></a>
 			</td>
 			<td class="order">
-				<span><?php echo $this->pagination->orderUpIcon( $i, ($row->catid == @$this->rows[$i-1]->catid), 'orderup', 'Move Up', $ordering); ?></span>
-				<span><?php echo $this->pagination->orderDownIcon( $i, $n, ($row->catid == @$this->rows[$i+1]->catid), 'orderdown', 'Move Down', $ordering ); ?></span>
+				<span><?php echo $this->pagination->orderUpIcon($i, ($row->catid == @$this->rows[$i-1]->catid), 'orderup', 'Move Up', $ordering); ?></span>
+				<span><?php echo $this->pagination->orderDownIcon($i, $n, ($row->catid == @$this->rows[$i+1]->catid), 'orderdown', 'Move Down', $ordering); ?></span>
 				<?php $disabled = $ordering ?  '' : 'disabled="disabled"'; ?>
 				<input type="text" name="order[]" size="5" value="<?php echo $row->ordering; ?>" <?php echo $disabled; ?> class="text_area" style="text-align: center" />
 			</td>
 			<td align="center">
-				<?php echo $access;?>
+				<?php echo $row->groupname;?>
 			</td>
 				<td>
-					<a href="<?php echo $row->sect_link; ?>" title="<?php echo JText::_( 'Edit Section' ); ?>">
+					<a href="<?php echo $row->sect_link; ?>" title="<?php echo JText::_('Edit Section'); ?>">
 						<?php echo $row->section_name; ?></a>
 				</td>
 			<td>
-				<a href="<?php echo $row->cat_link; ?>" title="<?php echo JText::_( 'Edit Category' ); ?>">
+				<a href="<?php echo $row->cat_link; ?>" title="<?php echo JText::_('Edit Category'); ?>">
 					<?php echo $row->name; ?></a>
 			</td>
 			<td>
 				<?php echo $author; ?>
 			</td>
 			<td nowrap="nowrap">
-				<?php echo JHtml::_('date',  $row->created, JText::_('DATE_FORMAT_LC4') ); ?>
+				<?php echo JHtml::_('date',  $row->created, JText::_('DATE_FORMAT_LC4')); ?>
 			</td>
 			<td nowrap="nowrap" align="center">
 				<?php echo $row->hits ?>
@@ -233,5 +233,5 @@
 <input type="hidden" name="redirect" value="<?php echo $this->redirect;?>" />
 <input type="hidden" name="filter_order" value="<?php echo $this->filter->order; ?>" />
 <input type="hidden" name="filter_order_Dir" value="<?php echo $this->filter->order_Dir; ?>" />
-<?php echo JHtml::_( 'form.token' ); ?>
+<?php echo JHtml::_('form.token'); ?>
 </form>
