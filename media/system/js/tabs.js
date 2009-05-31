8fe8f73b677aa0635b0404f979716467187fe686
Ercan Ozkaya <ozkayaercan@gmail.com>
2009-05-31 21:33:18 +0000

    First go at JTabs refactor
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@11948 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- media/system/js/tabs.js
+++ media/system/js/tabs.js
@@ -12,37 +12,35 @@
  * @since		1.5
  */
 var JTabs = new Class({
-
-    getOptions: function(){
-        return {
-
-            display: 0,
-
-            onActive: function(title, description){
-                description.setStyle('display', 'block');
-                title.addClass('open').removeClass('closed');
-            },
-
-            onBackground: function(title, description){
-                description.setStyle('display', 'none');
-                title.addClass('closed').removeClass('open');
-            }
-        };
-    },
+	Implements: [Options, Events],
+	
+	options : {
+		display: 0,
+		onActive: function(title, description) {
+			description.setStyle('display', 'block');
+			title.addClass('open').removeClass('closed');
+		},
+		onBackground: function(title, description){
+			description.setStyle('display', 'none');
+			title.addClass('closed').removeClass('open');
+		},
+		titleSelector: 'dt',
+		descriptionSelector: 'dd',		
+	},
 
     initialize: function(dlist, options){
+		this.setOptions(options);
         this.dlist = $(dlist);
-        this.setOptions(this.getOptions(), options);
-        this.titles = this.dlist.getElements('dt');
-        this.descriptions = this.dlist.getElements('dd');
-        this.content = new Element('div').injectAfter(this.dlist).addClass('current');
+        this.titles = this.dlist.getElements(this.options.titleSelector);
+        this.descriptions = this.dlist.getElements(this.options.descriptionSelector);
+        this.content = new Element('div').inject(this.dlist, 'after').addClass('current');
 
         for (var i = 0, l = this.titles.length; i < l; i++){
             var title = this.titles[i];
             var description = this.descriptions[i];
             title.setStyle('cursor', 'pointer');
             title.addEvent('click', this.display.bind(this, i));
-            description.injectInside(this.content);
+            description.inject(this.content);
         }
 
         if ($chk(this.options.display)) this.display(this.options.display);
@@ -50,17 +48,14 @@ var JTabs = new Class({
         if (this.options.initialize) this.options.initialize.call(this);
     },
 
-    hideAllBut: function(but){
+    hideAllBut: function(but) {
         for (var i = 0, l = this.titles.length; i < l; i++){
-            if (i != but) this.fireEvent('onBackground', [this.titles[i], this.descriptions[i]])
+            if (i != but) this.fireEvent('onBackground', [this.titles[i], this.descriptions[i]]);
         }
     },
 
-    display: function(i){
+    display: function(i) {
         this.hideAllBut(i);
-        this.fireEvent('onActive', [this.titles[i], this.descriptions[i]])
+        this.fireEvent('onActive', [this.titles[i], this.descriptions[i]]);
     }
 });
-
-JTabs.implement(new Events);
-JTabs.implement(new Options);
