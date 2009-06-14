15fb5eed1486804572a6d1027975e9ba1867813a
Hannes Papenberg <info@joomlager.de>
2009-06-14 15:37:56 +0000

    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@12071 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- components/com_content/views/category/tmpl/blog.php
+++ components/com_content/views/category/tmpl/blog.php
@@ -23,6 +23,18 @@ $cparams =& JComponentHelper::getParams('com_media');
 	</td>
 </tr>
 <?php endif; ?>
+<tr>
+	<td>
+	<ul>
+	<?php
+	foreach($this->children as $child)
+	{
+		echo '<li><a href="'.JRoute::_(ContentHelperRoute::getCategoryRoute($child->id)).'">'.$child->title.'</a> ('.$child->numitems.')</li>';
+	}
+	?>
+	</ul>
+	</td>
+</tr>
 <?php if ($this->params->get('num_leading_articles')) : ?>
 <tr>
 	<td valign="top">
