b5a75d0fc00f35df628dd30db5909387be4b8ff8
Samuel Moffatt <pasamio@gmail.com>
2009-06-21 12:15:50 +0000

    Language and defines updates
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@12238 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- includes/defines.php
+++ includes/defines.php
@@ -29,3 +29,5 @@ define('JPATH_PLUGINS',		JPATH_ROOT.DS.'plugins'  );
 define('JPATH_INSTALLATION',	JPATH_ROOT.DS.'installation');
 define('JPATH_THEMES'	   ,	JPATH_BASE.DS.'templates');
 define('JPATH_CACHE',			JPATH_BASE.DS.'cache');
+define('JPATH_MANIFESTS',		JPATH_ADMINISTRATOR.DS.'manifests');
+define( 'JPATH_BACKUPS',			JPATH_ADMINISTRATOR.DS.'backups');
