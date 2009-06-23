6fc6e10021b8a030f4ae3ecc30aefcfc5bb7234c
Andrew Eddie <andrew.eddie@newlifeinit.com>
2009-06-23 11:50:29 +0000

    Change mod_custom into a real module; add param to run content through content plugins.
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@12316 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

---
+++ administrator/modules/mod_custom/mod_custom.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @version		$Id$
+ * @package		Joomla.Site
+ * @subpackage	mod_custom
+ * @copyright	Copyright (C) 2005 - 2009 Open Source Matters, Inc. All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// no direct access
+defined('_JEXEC') or die;
+
+if ($params->def('prepare_content', 1))
+{
+	JPluginHelper::importPlugin('content');
+	$module->content = JHtml::_('content.prepare', $module->content);
+}
+
+require JModuleHelper::getLayoutPath('mod_custom');
---
+++ modules/mod_custom/tmpl/default.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @version		$Id$
+ * @package		Joomla.Site
+ * @subpackage	mod_custom
+ * @copyright	Copyright (C) 2005 - 2009 Open Source Matters, Inc. All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// no direct access
+defined('_JEXEC') or die;
+
+echo $module->content;
