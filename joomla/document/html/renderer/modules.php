14e57a19092b14e39cc223dd82db8a783017336e
Andrew Eddie <andrew.eddie@newlifeinit.com>
2009-06-23 11:41:56 +0000

    Code formatting.
    --HG--
    extra : convert_revision : svn%3A6f6e1ebd-4c2b-0410-823f-f34bde69bce9/development/trunk/libraries%4012314

--- joomla/document/html/renderer/modules.php
+++ joomla/document/html/renderer/modules.php
@@ -1,8 +1,6 @@
 <?php
 /**
  * @version		$Id$
- * @package		Joomla.Framework
- * @subpackage	Document
  * @copyright	Copyright (C) 2005 - 2009 Open Source Matters, Inc. All rights reserved.
  * @license		GNU General Public License version 2 or later; see LICENSE.txt
  */
@@ -22,19 +20,18 @@ class JDocumentRendererModules extends JDocumentRenderer
 	/**
 	 * Renders multiple modules script and returns the results as a string
 	 *
-	 * @access public
-	 * @param string 	$name		The position of the modules to render
-	 * @param array 	$params		Associative array of values
-	 * @return string	The output of the script
+	 * @param	string 	$name		The position of the modules to render
+	 * @param	array 	$params		Associative array of values
+	 * @return	string	The output of the script
 	 */
-	function render($position, $params = array(), $content = null)
+	public function render($position, $params = array(), $content = null)
 	{
-		$renderer = & $this->_doc->loadRenderer('module');
+		$renderer	= &$this->_doc->loadRenderer('module');
+		$buffer		= '';
 
-		$contents = '';
-		foreach (JModuleHelper::getModules($position) as $mod)  {
-			$contents .= $renderer->render($mod, $params, $content);
+		foreach (JModuleHelper::getModules($position) as $mod) {
+			$buffer .= $renderer->render($mod, $params, $content);
 		}
-		return $contents;
+		return $buffer;
 	}
 }
