eca75eb9b0e137627a53a1cab35a99a579f66c21
Gergo Erdosi <gergo.erdosi@gmail.com>
2009-06-27 17:12:48 +0000

    Replaced vote content plugin with its refactored version.
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@12383 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- plugins/content/vote.php
+++ plugins/content/vote.php
@@ -1,62 +1,73 @@
 <?php
 /**
  * @version		$Id$
- * @package		Joomla
  * @copyright	Copyright (C) 2005 - 2009 Open Source Matters, Inc. All rights reserved.
  * @license		GNU General Public License version 2 or later; see LICENSE.txt
  */
 
-// no direct access
+// No direct access.
 defined('_JEXEC') or die;
 
-$mainframe->registerEvent('onBeforeDisplayContent', 'plgContentVote');
+jimport('joomla.plugin.plugin');
 
-function plgContentVote(&$row, &$params, $page=0)
+/**
+ * Vote plugin.
+ * 
+ * @package		Joomla
+ * @subpackage	plg_vote
+ */
+class plgContentVote extends JPlugin
 {
-	$uri = & JFactory::getURI();
-
-	$id 	= $row->id;
-	$html 	= '';
-
-	if (isset($row->rating_count) && $params->get('show_vote') && !$params->get('popup'))
-	{
-		JPlugin::loadLanguage('plg_content_vote');
-		$html .= '<form method="post" action="' . $uri->toString() . '">';
-		$img = '';
-
-		// look for images in template if available
-		$starImageOn 	= JHtml::_('image.site',  'rating_star.png', '/images/M_images/');
-		$starImageOff 	= JHtml::_('image.site',  'rating_star_blank.png', '/images/M_images/');
-
-		for ($i=0; $i < $row->rating; $i++) {
-			$img .= $starImageOn;
-		}
-		for ($i=$row->rating; $i < 5; $i++) {
-			$img .= $starImageOff;
-		}
-		$html .= '<span class="content_rating">';
-		$html .= JText::_('User Rating') .':'. $img .'&nbsp;/&nbsp;';
-		$html .= intval($row->rating_count);
-		$html .= "</span>\n<br />\n";
-
-		if (!$params->get('intro_only'))
-		{
-			$html .= '<span class="content_vote">';
-			$html .= JText::_('Poor');
-			$html .= '<input type="radio" alt="vote 1 star" name="user_rating" value="1" />';
-			$html .= '<input type="radio" alt="vote 2 star" name="user_rating" value="2" />';
-			$html .= '<input type="radio" alt="vote 3 star" name="user_rating" value="3" />';
-			$html .= '<input type="radio" alt="vote 4 star" name="user_rating" value="4" />';
-			$html .= '<input type="radio" alt="vote 5 star" name="user_rating" value="5" checked="checked" />';
-			$html .= JText::_('Best');
-			$html .= '&nbsp;<input class="button" type="submit" name="submit_vote" value="'. JText::_('Rate') .'" />';
-			$html .= '<input type="hidden" name="task" value="vote" />';
-			$html .= '<input type="hidden" name="option" value="com_content" />';
-			$html .= '<input type="hidden" name="cid" value="'. $id .'" />';
-			$html .= '<input type="hidden" name="url" value="'.  $uri->toString() .'" />';
-			$html .= '</span>';
-		}
-		$html .= '</form>';
+	public function __construct(&$subject, $options = array()) {
+		parent::__construct($subject, $options);
+		$this->loadLanguage();
 	}
-	return $html;
+
+	public function onBeforeDisplayContent(&$row, &$params, $page=0)
+	{
+		$uri = &JFactory::getURI();
+
+		$id = $row->id;
+		$html = '';
+
+		if (isset($row->rating_count) && $params->get('show_vote') && !$params->get('popup'))
+		{
+			$html .= '<form method="post" action="' . $uri->toString() . '">';
+			$img = '';
+
+			// Look for images in template if available.
+			$starImageOn = JHtml::_('image.site', 'rating_star.png', '/images/M_images/');
+			$starImageOff = JHtml::_('image.site', 'rating_star_blank.png', '/images/M_images/');
+			for ($i=0; $i < $row->rating; $i++) {
+				$img .= $starImageOn;
+			}
+			for ($i=$row->rating; $i < 5; $i++) {
+				$img .= $starImageOff;
+			}
+			$html .= '<span class="content_rating">';
+			$html .= JText::_('User Rating') .':'. $img .'&nbsp;/&nbsp;';
+			$html .= intval($row->rating_count);
+			$html .= "</span>\n<br />\n";
+
+			if (!$params->get('intro_only'))
+			{
+				$html .= '<span class="content_vote">';
+				$html .= JText::_('Poor');
+				$html .= '<input type="radio" alt="vote 1 star" name="user_rating" value="1" />';
+				$html .= '<input type="radio" alt="vote 2 star" name="user_rating" value="2" />';
+				$html .= '<input type="radio" alt="vote 3 star" name="user_rating" value="3" />';
+				$html .= '<input type="radio" alt="vote 4 star" name="user_rating" value="4" />';
+				$html .= '<input type="radio" alt="vote 5 star" name="user_rating" value="5" checked="checked" />';
+				$html .= JText::_('Best');
+				$html .= '&nbsp;<input class="button" type="submit" name="submit_vote" value="'. JText::_('Rate') .'" />';
+				$html .= '<input type="hidden" name="task" value="vote" />';
+				$html .= '<input type="hidden" name="option" value="com_content" />';
+				$html .= '<input type="hidden" name="cid" value="'. $id .'" />';
+				$html .= '<input type="hidden" name="url" value="'.  $uri->toString() .'" />';
+				$html .= '</span>';
+			}
+			$html .= '</form>';
+		}
+		return $html;
+	}
 }
