a1cdd904d895cf868bdedf64048c83a340a52d86
Andrew Eddie <andrew.eddie@newlifeinit.com>
2009-07-02 05:48:27 +0000

    Bug fix missing info required for pathway.
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@12405 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- includes/menu.php
+++ includes/menu.php
@@ -52,7 +52,18 @@ class JMenuSite extends JMenu
 
 			foreach ($menus as &$menu)
 			{
-				//Create the query array
+				// Get parent information
+				$parent_tree  = array();
+				if (($parent = $menu->parent_id) && (isset($menus[$parent])) &&
+					(is_object($menus[$parent])) && (isset($menus[$parent]->route)) && isset($menus[$parent]->tree)) {
+					$parent_tree  = $menus[$parent]->tree;
+				}
+
+				// Create tree.
+				array_push($parent_tree, $menu->id);
+				$menu->tree = $parent_tree;
+
+				// Create the query array.
 				$url = str_replace('index.php?', '', $menu->link);
 				if (strpos($url, '&amp;') !== false) {
 				   $url = str_replace('&amp;','&',$url);
