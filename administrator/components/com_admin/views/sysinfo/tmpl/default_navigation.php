aabb32f9bff9e46d55f4d08f97b10c3e87b0a164
Gergo Erdosi <gergo.erdosi@gmail.com>
2009-07-07 16:43:04 +0000

    [#17119] [patch] Add keyboard access to admin com_admin
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@12510 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- administrator/components/com_admin/views/sysinfo/tmpl/default_navigation.php
+++ administrator/components/com_admin/views/sysinfo/tmpl/default_navigation.php
@@ -10,27 +10,30 @@
 // no direct access
 defined('_JEXEC') or die;
 ?>
+<div id="submenu-box">
+<div class="t"><div class="t"><div class="t"></div></div></div>
+<div class="m">
 <div class="submenu-box">
 	<div class="submenu-pad">
 		<ul id="submenu" class="information">
 			<li>
-				<a id="site" class="active">
+				<a href="#" onclick="return false;" id="site" class="active">
 					<?php echo JText::_('Admin_System_Info'); ?></a>
 			</li>
 			<li>
-				<a id="phpsettings">
+				<a href="#" onclick="return false;" id="phpsettings">
 					<?php echo JText::_('Admin_PHP_Settings'); ?></a>
 			</li>
 			<li>
-				<a id="config">
+				<a href="#" onclick="return false;" id="config">
 					<?php echo JText::_('Admin_Configuration_File'); ?></a>
 			</li>
 			<li>
-				<a id="directory">
+				<a href="#" onclick="return false;" id="directory">
 					<?php echo JText::_('Admin_Directory_Permissions'); ?></a>
 			</li>
 			<li>
-				<a id="phpinfo">
+				<a href="#" onclick="return false;" id="phpinfo">
 					<?php echo JText::_('Admin_PHP_Information'); ?></a>
 			</li>
 		</ul>
@@ -38,3 +41,7 @@ defined('_JEXEC') or die;
 	</div>
 </div>
 <div class="clr"></div>
+</div>
+<div class="b"><div class="b"><div class="b"></div></div></div>
+</div>
+
