71197e754f320dba14bf5767f8ba12fc23536d2c
Gergo Erdosi <gergo.erdosi@gmail.com>
2009-09-12 12:17:43 +0000

    Reverted accidental commit (r12700)
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@12706 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

---
+++ administrator/components/com_modules/modules.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @version		$Id$
+ * @package		Joomla.Administrator
+ * @subpackage	Modules
+ * @copyright	Copyright (C) 2005 - 2009 Open Source Matters, Inc. All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// no direct access
+defined('_JEXEC') or die;
+
+$user = & JFactory::getUser();
+if (!$user->authorize('core.modules.manage')) {
+	JFactory::getApplication()->redirect('index.php', JText::_('ALERTNOTAUTH'));
+}
+
+// Helper classes
+JHtml::addIncludePath(JPATH_COMPONENT.DS.'classes');
+
+// Require the base controller
+require_once JPATH_COMPONENT.DS.'controller.php';
+
+$controller	= new ModulesController();
+
+// Perform the Request task
+$controller->execute(JRequest::getCmd('task'));
+$controller->redirect();
---
+++ administrator/components/com_modules/views/preview/view.html.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @version		$Id$
+ * @package		Joomla.Administrator
+ * @subpackage	Modules
+ * @copyright	Copyright (C) 2005 - 2009 Open Source Matters, Inc. All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// Check to ensure this file is included in Joomla!
+defined('_JEXEC') or die;
+
+jimport('joomla.application.component.view');
+
+/**
+ * HTML View class for the Modules component
+ *
+ * @static
+ * @package		Joomla.Administrator
+ * @subpackage	Modules
+ * @since 1.6
+ */
+class ModulesViewPreview extends JView
+{
+	function display($tpl = null)
+	{
+		$editor = &JFactory::getEditor();
+
+		$this->assignRef('editor',		$editor);
+
+		parent::display($tpl);
+	}
+}
