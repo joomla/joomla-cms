bdb9a051c22e4eb7a374eb82a3ac9ce20b548483
Andrew Eddie <andrew.eddie@newlifeinit.com>
2009-09-28 07:25:04 +0000

    Added ContentLanguages JForm field and JElement.
    --HG--
    extra : convert_revision : svn%3A6f6e1ebd-4c2b-0410-823f-f34bde69bce9/development/trunk/libraries%4012882

---
+++ joomla/html/parameter/element/contentlanguages.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * @version		$Id$
+ * @package		Joomla.Framework
+ * @subpackage	Parameter
+ * @copyright	Copyright (C) 2005 - 2009 Open Source Matters, Inc. All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// No direct access
+defined('JPATH_BASE') or die;
+
+require_once dirname(__FILE__).DS.'list.php';
+
+/**
+ * Renders a select list of Asset Groups
+ *
+ * @package 	Joomla.Framework
+ * @subpackage	Parameter
+ * @since		1.6
+ */
+class JElementContentLanguages extends JElementList
+{
+	/**
+	* Element name
+	*
+	* @access	protected
+	* @var		string
+	*/
+	protected $_name = 'ContentLanguages';
+
+	/**
+	 * Get the options for the element
+	 *
+	 * @param	object $node
+	 * @return	array
+	 */
+	protected function _getOptions(&$node)
+	{
+		$db		= &JFactory::getDbo();
+		$query	= new JQuery;
+
+		$query->select('a.lang_code AS value, a.title AS text, a.title_native');
+		$query->from('#__languages AS a');
+		$query->where('a.published >= 0');
+		$query->order('a.title');
+
+		// Get the options.
+		$db->setQuery($query);
+		$options = $db->loadObjectList();
+
+		// Check for a database error.
+		if ($db->getErrorNum()) {
+			JError::raiseWarning(500, $db->getErrorMsg());
+		}
+
+		// Merge any additional options in the XML definition.
+		$options = array_merge(parent::_getOptions($node), $options);
+
+		return $options;
+	}
+}
