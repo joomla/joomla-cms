4d9eb742dc546bf2b011bddbea35b1b9ae9f34a2
Andrew Eddie <andrew.eddie@newlifeinit.com>
2009-09-28 07:25:04 +0000

    Added ContentLanguages JForm field and JElement.
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@12882 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

---
+++ libraries/joomla/form/fields/contentlanguages.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * @version		$Id$
+ * @copyright	Copyright (C) 2005 - 2009 Open Source Matters, Inc. All rights reserved.
+ * @copyright	Copyright (C) 2008 - 2009 JXtended, LLC. All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('JPATH_BASE') or die;
+
+jimport('joomla.html.html');
+jimport('joomla.database.query');
+require_once dirname(__FILE__).DS.'list.php';
+
+/**
+ * Form Field class for the Joomla Framework.
+ *
+ * @package		Joomla.Framework
+ * @subpackage	Form
+ * @since		1.6
+ */
+class JFormFieldContentLanguages extends JFormFieldList
+{
+	/**
+	 * The field type.
+	 *
+	 * @var		string
+	 */
+	public $type = 'ContentLanguages';
+
+	/**
+	 * Method to get a list of options for a list input.
+	 *
+	 * @return	array		An array of JHtml options.
+	 */
+	protected function _getOptions()
+	{
+		$db		= &JFactory::getDbo();
+		$query	= new JQuery;
+
+		$query->select('a.lang_code AS value, a.title AS text, a.title_native');
+		$query->from('#__languages AS a');
+		$query->where('a.published >= 0');
+		$query->order('a.title');
+
+		// Get the options.
+		$db->setQuery($query);
+		$options = $db->loadObjectList();
+
+		// Check for a database error.
+		if ($db->getErrorNum()) {
+			JError::raiseWarning(500, $db->getErrorMsg());
+		}
+
+		// Merge any additional options in the XML definition.
+		$options = array_merge(parent::_getOptions(), $options);
+
+		return $options;
+	}
+}
