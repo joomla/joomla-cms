6f937a643c072a17ef250f2e37a3038e29fcab21
Ron Severdia <severdia@gmail.com>
2009-10-10 21:00:23 +0000

    Search component UI cleanup
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@13135 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- administrator/components/com_search/views/search/tmpl/default.php
+++ administrator/components/com_search/views/search/tmpl/default.php
@@ -1,25 +1,26 @@
 <?php defined('_JEXEC') or die; ?>
 
 <form action="index.php" method="post" name="adminForm">
-	<table>
+	<table class="width-100">
 		<tr>
-			<td align="left" width="100%">
+			<td class="left" width="80%">
 				<?php echo JText::_('Filter'); ?>:
 				<input type="text" name="search" id="search" value="<?php echo $this->escape($this->search); ?>" class="text_area" onchange="document.adminForm.submit();" />
 				<button onclick="this.form.submit();"><?php echo JText::_('Go'); ?></button>
 				<button onclick="document.getElementById('search').value='';this.form.submit();"><?php echo JText::_('Reset'); ?></button>
 			</td>
-			<td nowrap="nowrap">
-				<span class="componentheading"><?php echo JText::_('Search Logging'); ?> :
-				<?php echo $this->enabled ? '<b><font color="green">'. JText::_('Enabled') .'</font></b>' : '<b><font color="red">'. JText::_('Disabled') .'</font></b>' ?>
-				</span>
+			<td class="nowrap">
+				<span class="adminlist-status"><?php echo JText::_('Search Logging'); ?>:</span>
+				<?php echo $this->enabled ? '<span class="enabled">'. JText::_('Enabled') .'</span>' : '<span class="disabled">'. JText::_('Disabled') .'</span>' ?>
 			</td>
-			<td nowrap="nowrap" align="right">
+			<td class="nowrap right">
+			<span class="adminlist-searchstatus">
 			<?php if ($this->showResults) : ?>
 				<a href="index.php?option=com_search&amp;search_results=0"><?php echo JText::_('Hide Search Results'); ?></a>
 			<?php else : ?>
 				<a href="index.php?option=com_search&amp;search_results=1"><?php echo JText::_('Show Search Results'); ?></a>
 			<?php endif; ?>
+			</span>
 			</td>
 		</tr>
 	</table>
@@ -28,28 +29,18 @@
 		<table class="adminlist">
 			<thead>
 				<tr>
-					<th width="10">
-						<?php echo JText::_('NUM'); ?>
-					</th>
-					<th class="title">
-						<?php echo JHtml::_('grid.sort',   'Search Text', 'search_term', @$this->lists['order_Dir'], @$this->lists['order']); ?>
-					</th>
-					<th nowrap="nowrap" width="20%">
-						<?php echo JHtml::_('grid.sort',   'Times Requested', 'hits', @$this->lists['order_Dir'], @$this->lists['order']); ?>
-					</th>
+					<th width="10"><?php echo JText::_('NUM'); ?></th>
+					<th><?php echo JHtml::_('grid.sort',   'Search Text', 'search_term', @$this->lists['order_Dir'], @$this->lists['order']); ?></th>
+					<th class="nowrap" width="20%"><?php echo JHtml::_('grid.sort',   'Times Requested', 'hits', @$this->lists['order_Dir'], @$this->lists['order']); ?></th>
 					<?php
 					if ($this->showResults) : ?>
-						<th nowrap="nowrap" width="20%">
-							<?php echo JText::_('Results Returned'); ?>
-						</th>
+						<th class="nowrap" width="20%"><?php echo JText::_('Results Returned'); ?></th>
 					<?php endif; ?>
 				</tr>
 			</thead>
 			<tfoot>
 				<tr>
-					<td colspan="4">
-						<?php echo $this->pageNav->getListFooter(); ?>
-					</td>
+					<td colspan="4"><?php echo $this->pageNav->getListFooter(); ?></td>
 				</tr>
 			</tfoot>
 			<tbody>
@@ -59,17 +50,17 @@
 				$row = &$this->items[$i];
 				?>
 				<tr class="row<?php echo $k;?>">
-					<td align="right">
+					<td class="right">
 						<?php echo $i+1+$this->pageNav->limitstart; ?>
 					</td>
 					<td>
 						<?php echo htmlspecialchars($row->search_term, ENT_QUOTES, 'UTF-8'); ?>
 					</td>
-					<td align="center">
+					<td class="center">
 						<?php echo $row->hits; ?>
 					</td>
 					<?php if ($this->showResults) : ?>
-					<td align="center">
+					<td class="center">
 						<?php echo $row->returns; ?>
 					</td>
 					<?php endif; ?>
