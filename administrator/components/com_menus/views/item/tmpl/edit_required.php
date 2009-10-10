41b35b1bedcedfeff593fa4dd56664c85f8f37cd
Ron Severdia <severdia@gmail.com>
2009-10-10 19:06:42 +0000

    Fix closed tag.
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@13130 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- administrator/components/com_menus/views/item/tmpl/edit_required.php
+++ administrator/components/com_menus/views/item/tmpl/edit_required.php
@@ -14,7 +14,7 @@ defined('_JEXEC') or die;
 
 	foreach ($this->paramsform->getFields('request') as $field) :
 		?>
-		<div class="paramrow" />
+		<div class="paramrow">
 			<?php echo $field->label; ?>
 			<?php echo $field->input; ?>
 		</div>
