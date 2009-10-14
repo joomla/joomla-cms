0fd959933d214d7c8949ad24eb5306c6934f0cb1
Andrew Eddie <andrew.eddie@newlifeinit.com>
2009-10-14 06:39:05 +0000

    Fix up stray outbound links in docblocks.
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@13191 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- administrator/components/com_contact/controllers/contact.php
+++ administrator/components/com_contact/controllers/contact.php
@@ -1,9 +1,8 @@
 <?php
 /**
- * @version
+ * @version		$Id$
  * @copyright	Copyright (C) 2005 - 2009 Open Source Matters, Inc. All rights reserved.
  * @license		GNU General Public License version 2 or later; see LICENSE.txt
- * @link		http://www.theartofjoomla.com
  */
 
 // no direct access
--- administrator/components/com_languages/controllers/languages.php
+++ administrator/components/com_languages/controllers/languages.php
@@ -3,7 +3,6 @@
  * @version		$Id$
  * @copyright	Copyright (C) 2005 - 2009 Open Source Matters, Inc. All rights reserved.
  * @license		GNU General Public License version 2 or later; see LICENSE.txt
- * @link		http://www.theartofjoomla.com
  */
 
 // no direct access
