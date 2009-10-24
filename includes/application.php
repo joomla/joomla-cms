85bbfd3f1af69b3ab38d49606386a857acf6ca42
Andrew Eddie <andrew.eddie@newlifeinit.com>
2009-10-24 02:00:44 +0000

    # Fixed [#18460] Login does not redirect correctly.
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@13300 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- includes/application.php
+++ includes/application.php
@@ -225,10 +225,11 @@ final class JSite extends JApplication
 				$uri		= JFactory::getURI();
 				$return		= $uri->toString();
 
-				$url  = 'index.php?option=com_users&view=login';
-				$url .= '&return='.base64_encode($return);
+				$this->setUserState('users.login.form.data',array( 'return' => $return ) );
 
-				//$url	= JRoute::_($url, false);
+				$url	= 'index.php?option=com_users&view=login';
+				$url	= JRoute::_($url, false);
+
 				$this->redirect($url, JText::_('You must login first'));
 			}
 			else {
