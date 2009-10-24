0891cb1711adf50651ccb465d65514e5e3bd3d04
Andrew Eddie <andrew.eddie@newlifeinit.com>
2009-10-24 21:41:33 +0000

    [#17088] Error on loading demodata during installation IE
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@13325 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- installation/template/js/installation.js
+++ installation/template/js/installation.js
@@ -16,6 +16,7 @@ if (typeof(Install) === 'undefined') {
  */
 Install.sampleData = function(el) {
 	// make the ajax call
+	el = $(el);
 	var req = new Request({
 		method: 'get',
 		url: 'index.php?'+document.id(el.form).toQueryString(),
@@ -40,6 +41,7 @@ Install.sampleData = function(el) {
  */
 Install.detectFtpRoot = function(el) {
 	// make the ajax call
+	el = $(el);
 	var req = new Request({
 		method: 'get',
 		url: 'index.php?'+document.id(el.form).toQueryString(),
@@ -64,6 +66,7 @@ Install.detectFtpRoot = function(el) {
  */
 Install.verifyFtpSettings = function(el) {
 	// make the ajax call
+	el = $(el);
 	var req = new Request({
 		method: 'get',
 		url: 'index.php?'+document.id(el.form).toQueryString(),
