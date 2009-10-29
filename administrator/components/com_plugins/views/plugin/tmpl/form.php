a8c6f4b8d674fcb33878f84f1f60eef28905a5bd
Andrew Eddie <andrew.eddie@newlifeinit.com>
2009-10-29 00:28:10 +0000

    Revert to last good working state.
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@13374 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- administrator/components/com_plugins/views/plugin/tmpl/form.php
+++ administrator/components/com_plugins/views/plugin/tmpl/form.php
@@ -36,41 +36,84 @@
 <div class="width-60 fltlft">
 	<fieldset class="adminform">
 	<legend><?php echo JText::_( 'Details' ); ?></legend>
-	
-		<label id="jform_plugname-lbl" class="required" for="jform_plugname"><?php echo JText::_( 'Name' ); ?>:</label>
-		<input id="jform_plugname" class="inputbox" type="text" size="35" value="<?php echo $this->plugin->name; ?>" name="jform[plugname]"/>
-		
-				
-		<label id="jform_plugenabled-lbl" class="" for="jform_plugenabled"><?php echo JText::_( 'JEnabled' ); ?>:</label>
-		<fieldset id="jform_plugenabled" class="radio">
-			<?php echo $this->lists['enabled']; ?>
-		</fieldset>
-		
-		<label id="jform_plugtype-lbl" class="" for="jform_plugtype"><?php echo JText::_( 'Type' ); ?>:</label>
-		<input id="jform_plugtype" class="readonly" type="text" readonly="readonly" size="16" value="<?php echo $this->plugin->folder; ?>" name="jform[plugtype]"/>
-		
-		<label id="jform_plugfile-lbl" class="" for="jform_plugfile"><?php echo JText::_( 'Plugin file' ); ?>:</label>
-		<input id="jform_plugfile" class="readonly" type="text" readonly="readonly" size="25" value="<?php echo $this->plugin->element; ?>.php" name="jform[plugfile]"/>
-		
-		<label id="jform_plugaccess-lbl" class="" for="jform_plugaccess"><?php echo JText::_( 'Access Level' ); ?>:</label>
-		<?php echo $this->lists['access']; ?>
-		
-		<label id="jform_plugorder-lbl" class="" for="jform_plugorder"><?php echo JText::_( 'Order' ); ?>:</label>
-		<?php echo $this->lists['ordering']; ?>
-		
-		<label id="jform_plugdesc-lbl" class="" for="jform_plugdesc"><?php echo JText::_( 'Description' ); ?>:</label>
-		<p class="jform_desc"><?php echo JText::_( $this->plugin->description ); ?></p>
-	
+	<table class="admintable">
+		<tr>
+			<td width="100" class="key">
+				<label for="name">
+					<?php echo JText::_( 'Name' ); ?>:
+				</label>
+			</td>
+			<td>
+				<input class="text_area" type="text" name="name" id="name" size="35" value="<?php echo $this->plugin->name; ?>" />
+			</td>
+		</tr>
+		<tr>
+			<td valign="top" class="key">
+				<?php echo JText::_( 'Enabled' ); ?>:
+			</td>
+			<td>
+				<?php echo $this->lists['enabled']; ?>
+			</td>
+		</tr>
+		<tr>
+			<td valign="top" class="key">
+				<label for="folder">
+					<?php echo JText::_( 'Type' ); ?>:
+				</label>
+			</td>
+			<td>
+				<?php echo $this->plugin->folder; ?>
+			</td>
+		</tr>
+		<tr>
+			<td valign="top" class="key">
+				<label for="element">
+					<?php echo JText::_( 'Plugin file' ); ?>:
+				</label>
+			</td>
+			<td>
+				<input class="text_area" type="text" name="element" id="element" size="35" value="<?php echo $this->plugin->element; ?>" />.php
+			</td>
+		</tr>
+		<tr>
+			<td valign="top" class="key">
+				<label for="access">
+					<?php echo JText::_( 'Access Level' ); ?>:
+				</label>
+			</td>
+			<td>
+				<?php echo $this->lists['access']; ?>
+			</td>
+		</tr>
+		<tr>
+			<td valign="top" class="key">
+				<?php echo JText::_( 'Order' ); ?>:
+			</td>
+			<td>
+				<?php echo $this->lists['ordering']; ?>
+			</td>
+		</tr>
+		<tr>
+			<td valign="top" class="key">
+				<?php echo JText::_( 'Description' ); ?>:
+			</td>
+			<td>
+				<?php echo JText::_( $this->plugin->description ); ?>
+			</td>
+		</tr>
+		</table>
 	</fieldset>
 </div>
 <div class="width-40 fltrt">
+	<fieldset class="adminform">
+	<legend><?php echo JText::_( 'Parameters' ); ?></legend>
 	<?php
 		jimport('joomla.html.pane');
 		$pane = &JPane::getInstance('sliders');
 		echo $pane->startPane('plugin-pane');
 		echo $pane->startPanel(JText :: _('Plugin Parameters'), 'param-page');
 		if($output = $this->params->render('params')) :
-			echo "<fieldset class='panelform-legacy'>".$output."</fieldset>";
+			echo $output;
 		else :
 			echo "<div class=\"noparams-notice\">".JText::_('There are no parameters for this item')."</div>";
 		endif;
@@ -79,14 +122,16 @@
 		if ($this->params->getNumParams('advanced')) {
 			echo $pane->startPanel(JText :: _('Advanced Parameters'), "advanced-page");
 			if($output = $this->params->render('params', 'advanced')) :
-				echo "<fieldset class='panelform-legacy'>".$output."</fieldset>";
+				echo $output;
 			else :
 				echo "<div class=\"noparams-notice\">".JText::_('There are no advanced parameters for this item')."</div>";
 			endif;
 			echo $pane->endPanel();
 		}
+
 		echo $pane->endPane();
 	?>
+	</fieldset>
 </div>
 <div class="clr"></div>
 
