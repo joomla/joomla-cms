844f5fef657d878a5dfe85797c4774923e870eb9
Andrew Eddie <andrew.eddie@newlifeinit.com>
2009-11-13 00:20:40 +0000

    Fix injection of field name: sessionhandlers to sessionhandler
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@13479 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- libraries/joomla/form/fields/sessionhandler.php
+++ libraries/joomla/form/fields/sessionhandler.php
@@ -18,14 +18,14 @@ require_once dirname(__FILE__).DS.'list.php';
  * @subpackage	Form
  * @since		1.6
  */
-class JFormFieldSessionHandlers extends JFormFieldList
+class JFormFieldSessionHandler extends JFormFieldList
 {
 	/**
 	 * The field type.
 	 *
 	 * @var		string
 	 */
-	public $type = 'SessionHandlers';
+	public $type = 'SessionHandler';
 
 	/**
 	 * Method to get a list of options for a list input.
