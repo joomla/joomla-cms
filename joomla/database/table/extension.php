1fef83b6d849f413fe589245a7904f4d1164233c
Andrew Eddie <andrew.eddie@newlifeinit.com>
2009-11-13 02:53:07 +0000

    Added JTableExtension::publish override
    --HG--
    extra : convert_revision : svn%3A6f6e1ebd-4c2b-0410-823f-f34bde69bce9/development/trunk/libraries%4013490

--- joomla/database/table/extension.php
+++ joomla/database/table/extension.php
@@ -62,8 +62,8 @@ class JTableExtension extends JTable
 	 * @access var
 	 * @param database A database connector object
 	 */
-	function __construct( &$db ) {
-		parent::__construct( '#__extensions', 'extension_id', $db );
+	function __construct(&$db) {
+		parent::__construct('#__extensions', 'extension_id', $db);
 	}
 
 	/**
@@ -76,8 +76,8 @@ class JTableExtension extends JTable
 	function check()
 	{
 		// check for valid name
-		if (trim( $this->name ) == '' || trim( $this->element ) == '') {
-			$this->setError(JText::sprintf( 'must contain a title', JText::_( 'Extension') ));
+		if (trim($this->name) == '' || trim($this->element) == '') {
+			$this->setError(JText::sprintf('must contain a title', JText::_('Extension')));
 			return false;
 		}
 		return true;
@@ -94,14 +94,14 @@ class JTableExtension extends JTable
 	*/
 	function bind($array, $ignore = '')
 	{
-		if (isset( $array['params'] ) && is_array($array['params']))
+		if (isset($array['params']) && is_array($array['params']))
 		{
 			$registry = new JRegistry();
 			$registry->loadArray($array['params']);
 			$array['params'] = $registry->toString();
 		}
 
-		if (isset( $array['control'] ) && is_array( $array['control'] ))
+		if (isset($array['control']) && is_array($array['control']))
 		{
 			$registry = new JRegistry();
 			$registry->loadArray($array['control']);
@@ -111,7 +111,8 @@ class JTableExtension extends JTable
 		return parent::bind($array, $ignore);
 	}
 
-	function find($options=Array()) {
+	function find($options=array())
+	{
 		$dbo =& JFactory::getDBO();
 		$where = Array();
 		foreach($options as $col=>$val) {
@@ -121,4 +122,84 @@ class JTableExtension extends JTable
 		$dbo->setQuery($query);
 		return $dbo->loadResult();
 	}
+
+	/**
+	 * Method to set the publishing state for a row or list of rows in the database
+	 * table.  The method respects checked out rows by other users and will attempt
+	 * to checkin rows that it can after adjustments are made.
+	 *
+	 * @param	mixed	An optional array of primary key values to update.  If not
+	 * 					set the instance property value is used.
+	 * @param	integer The publishing state. eg. [0 = unpublished, 1 = published]
+	 * @param	integer The user id of the user performing the operation.
+	 * @return	boolean	True on success.
+	 */
+	public function publish($pks = null, $state = 1, $userId = 0)
+	{
+		// Initialize variables.
+		$k = $this->_tbl_key;
+
+		// Sanitize input.
+		JArrayHelper::toInteger($pks);
+		$userId = (int) $userId;
+		$state  = (int) $state;
+
+		// If there are no primary keys set check to see if the instance key is set.
+		if (empty($pks))
+		{
+			if ($this->$k) {
+				$pks = array($this->$k);
+			}
+			// Nothing to set publishing state on, return false.
+			else {
+				$this->setError(JText::_('No_Rows_Selected'));
+				return false;
+			}
+		}
+
+		// Build the WHERE clause for the primary keys.
+		$where = $k.'='.implode(' OR '.$k.'=', $pks);
+
+		// Determine if there is checkin support for the table.
+		if (!property_exists($this, 'checked_out') || !property_exists($this, 'checked_out_time')) {
+			$checkin = '';
+		}
+		else {
+			$checkin = ' AND (checked_out = 0 OR checked_out = '.(int) $userId.')';
+		}
+
+		// Update the publishing state for rows with the given primary keys.
+		$this->_db->setQuery(
+			'UPDATE `'.$this->_tbl.'`' .
+			' SET `enabled` = '.(int) $state .
+			' WHERE ('.$where.')' .
+			$checkin
+		);
+		$this->_db->query();
+
+		// Check for a database error.
+		if ($this->_db->getErrorNum()) {
+			$this->setError($this->_db->getErrorMsg());
+			return false;
+		}
+
+		// If checkin is supported and all rows were adjusted, check them in.
+		if ($checkin && (count($pks) == $this->_db->getAffectedRows()))
+		{
+			// Checkin the rows.
+			foreach($pks as $pk)
+			{
+				$this->checkin($pk);
+			}
+		}
+
+		// If the JTable instance value is in the list of primary keys that were set, set the instance.
+		if (in_array($this->$k, $pks)) {
+			$this->enabled = $state;
+		}
+
+		$this->setError('');
+		return true;
+	}
+
 }
