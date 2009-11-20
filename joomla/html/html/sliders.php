bbdde2efc5a0f70e7a5f05b1fd1b8354feb93f42
Louis Landry <louis.landry@joomla.org>
2009-11-20 13:55:17 +0000

    [#18432] multiple independent pane instance
     + Added JHtmlSliders
     + Added JHtmlTabs
     ! JPane and child classes has been deprecated in favor of JHtml helpers.
     ! Big thanks to Christophe Demko for his work on this.
    
    --HG--
    extra : convert_revision : svn%3A6f6e1ebd-4c2b-0410-823f-f34bde69bce9/development/trunk/libraries%4013545

---
+++ joomla/html/html/sliders.php
@@ -0,0 +1,126 @@
+<?php
+/**
+ * @version		$Id$
+ * @package		Joomla.Framework
+ * @subpackage	HTML
+ * @copyright	Copyright (C) 2005 - 2009 Open Source Matters, Inc. All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+/**
+ * Utility class for Sliders elements
+ *
+ * @static
+ * @package 	Joomla.Framework
+ * @subpackage	HTML
+ * @since		1.6
+ */
+abstract class JHtmlSliders
+{
+	protected static $opened = array();
+
+	/**
+	 * Creates a panes and loads the javascript behavior for it.
+	 *
+	 * @param	string	The pane identifier.
+	 * @param	array	An array of options.
+	 * @return	string
+	 * @since	1.6
+	 */
+	public static function start($group = 'sliders', $params = array())
+	{
+		JHtmlSliders::_loadBehavior($group,$params);
+		array_push(JHtmlSliders::$opened,false);
+
+		return '<div id="'.$group.'" class="pane-sliders">';
+	}
+
+	/**
+	 * Close the current pane.
+	 *
+	 * @return	string
+	 * @since	1.6
+	 */
+	public static function end()
+	{
+		if (array_pop(JHtmlSliders::$opened))
+		{
+			$close = '</div></div>';
+		}
+		else
+		{
+			$close = '';
+		}
+
+		return $close.'</div>';
+	}
+
+	/**
+	 * Begins the display of a new panel.
+	 *
+	 * @param	string	Text to display.
+	 * @param	string	Identifier of the panel.
+	 * @return	string
+	 * @since	1.6
+	 */
+	public static function panel($text, $id)
+	{
+		if (JHtmlSliders::$opened[count(JHtmlSliders::$opened)-1])
+		{
+			$close = '</div></div>';
+		}
+		else
+		{
+			JHtmlSliders::$opened[count(JHtmlSliders::$opened)-1] = true;
+			$close = '';
+		}
+
+		return $close.'<div class="panel"><h3 class="jpane-toggler title" id="'.$id.'"><a href="#"><span>'.$text.'</span></a></h3><div class="jpane-slider content">';
+	}
+
+	/**
+	 * Load the JavaScript behavior.
+	 *
+	 * @param	string	The pane identifier.
+	 * @param	array	Array of options.
+	 * @return	void
+	 * @since	1.6
+	 */
+	protected static function _loadBehavior($group, $params = array())
+	{
+		static $loaded=array();
+		if (!array_key_exists($group,$loaded))
+		{
+			$loaded[$group] = true;
+			// Include mootools framework.
+			JHtml::_('behavior.framework', true);
+
+			$document = &JFactory::getDocument();
+
+			$display = (isset($params['startOffset']) && ($params['startTransition'])) ? (int)$params['startOffset'] : null;
+			$show = (isset($params['startOffset']) && (!$params['startTransition'])) ? (int)$params['startOffset'] : null;
+			$options = '{';
+			$opt['onActive']	 = 'function(toggler, i) {toggler.addClass(\'jpane-toggler-down\');toggler.removeClass(\'jpane-toggler\');Cookie.write(\'jpanesliders_'.$group.'\',$$(\'div#'.$group.'.pane-sliders .panel h3\').indexOf(toggler));}';
+			$opt['onBackground']	 = "function(toggler, i) {toggler.addClass('jpane-toggler');toggler.removeClass('jpane-toggler-down');if($$('div#".$group.".pane-sliders .panel h3').length==$$('div#".$group.".pane-sliders .panel h3.jpane-toggler').length) Cookie.write('jpanesliders_".$group."',-1);}";
+			$opt['duration']	 = (isset($params['duration'])) ? (int)$params['duration'] : 300;
+			$opt['display']		 = (isset($params['useCookie']) && $params['useCookie']) ? JRequest::getInt('jpanesliders_' . $group, $display, 'cookie') : $display ;
+			$opt['show']		 = (isset($params['useCookie']) && $params['useCookie']) ? JRequest::getInt('jpanesliders_' . $group, $show, 'cookie') : $show ;
+			$opt['opacity']		 = (isset($params['opacityTransition']) && ($params['opacityTransition'])) ? 'true' : 'false' ;
+			$opt['alwaysHide']	 = (isset($params['allowAllClose']) && (!$params['allowAllClose'])) ? 'false' : 'true';
+			foreach ($opt as $k => $v)
+			{
+				if ($v) {
+					$options .= $k.': '.$v.',';
+				}
+			}
+			if (substr($options, -1) == ',') {
+				$options = substr($options, 0, -1);
+			}
+			$options .= '}';
+
+			$js = '	window.addEvent(\'domready\', function(){ new Accordion($$(\'div#'.$group.'.pane-sliders .panel h3.jpane-toggler\'), $$(\'div#'.$group.'.pane-sliders .panel div.jpane-slider\'), '.$options.'); });';
+
+			$document->addScriptDeclaration($js);
+		}
+	}
+}
---
+++ joomla/html/html/tabs.php
@@ -0,0 +1,123 @@
+<?php
+/**
+ * @version		$Id$
+ * @package		Joomla.Framework
+ * @subpackage	HTML
+ * @copyright	Copyright (C) 2005 - 2009 Open Source Matters, Inc. All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+/**
+ * Utility class for Tabs elements.
+ *
+ * @static
+ * @package 	Joomla.Framework
+ * @subpackage	HTML
+ * @version		1.6
+ */
+abstract class JHtmlTabs
+{
+	protected static $opened = array();
+
+	/**
+	 * Creates a panes and creates the JavaScript object for it.
+	 *
+	 * @param	string	The pane identifier
+	 * @param	array	An array of option.
+	 * @return	string
+	 * @since	1.6
+	 */
+	public static function start($group='tabs', $params=array())
+	{
+		JHtmlTabs::_loadBehavior($group,$params);
+		array_push(JHtmlTabs::$opened,false);
+
+		return '<dl class="tabs" id="'.$group.'">';
+	}
+
+	/**
+	 * Close the current pane
+	 *
+	 * @return	string
+	 * @since	1.6
+	 */
+	public static function end()
+	{
+		if (array_pop(JHtmlTabs::$opened))
+		{
+			$close = '</dd>';
+		}
+		else
+		{
+			$close = '';
+		}
+
+		return $close.'</dl>';
+	}
+
+	/**
+	 * Begins the display of a new panel.
+	 *
+	 * @param	string	Text to display.
+	 * @param	string	Identifier of the panel.
+	 * @return	string
+	 * @since	1.6
+	 */
+	public static function panel($text, $id)
+	{
+		if (JHtmlTabs::$opened[count(JHtmlTabs::$opened)-1])
+		{
+			$close = '</dd>';
+		}
+		else
+		{
+			JHtmlTabs::$opened[count(JHtmlTabs::$opened)-1] = true;
+			$close = '';
+		}
+
+		return $close.'<dt class="'.$id.'"><span>'.$text.'</span></dt><dd>';
+	}
+
+	/**
+	 * Load the JavaScript behavior.
+	 *
+	 * @param	string	The pane identifier.
+	 * @param	array	Array of options.
+	 * @return	void
+	 * @since	1.6
+	 */
+	protected static function _loadBehavior($group, $params = array())
+	{
+		static $loaded = array();
+
+		if (!array_key_exists($group,$loaded))
+		{
+			// Include mootools framework
+			JHtml::_('behavior.framework', true);
+
+			$display = (isset($params['startOffset'])) ? (int)$params['startOffset'] : null ;
+			$options = '{';
+			$opt['onActive']	 = (isset($params['onActive'])) ? $params['onActive'] : null ;
+			$opt['onBackground'] = (isset($params['onBackground'])) ? $params['onBackground'] : null ;
+			$opt['display']		 = (isset($params['useCookie']) && $params['useCookie']) ? JRequest::getInt('jpanetabs_' . $group, $display, 'cookie') : $display ;
+			foreach ($opt as $k => $v)
+			{
+				if ($v) {
+					$options .= $k.': '.$v.',';
+				}
+			}
+			if (substr($options, -1) == ',') {
+				$options = substr($options, 0, -1);
+			}
+			$options .= '}';
+
+			$js = '	window.addEvent(\'domready\', function(){ $$(\'dl#'.$group.'.tabs\').each(function(tabs){ new JTabs(tabs, '.$options.'); }); });';
+
+			$document = JFactory::getDocument();
+			$document->addScriptDeclaration($js);
+			$document->addScript(JURI::root(true). '/media/system/js/tabs.js');
+
+			$loaded[$group] = true;
+		}
+	}
+}
