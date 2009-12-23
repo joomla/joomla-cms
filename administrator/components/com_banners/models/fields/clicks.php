99ed3a9e429e4030b2c443c535adfb2afdabe415
Andrew Eddie <andrew.eddie@newlifeinit.com>
2009-12-23 01:03:06 +0000

    JXtended copyrights removed (happy now?)
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@13825 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- administrator/components/com_banners/models/fields/clicks.php
+++ administrator/components/com_banners/models/fields/clicks.php
@@ -2,7 +2,6 @@
 /**
  * @version		$Id: media.php 12774 2009-09-18 04:47:09Z eddieajau $
  * @copyright	Copyright (C) 2005 - 2009 Open Source Matters, Inc. All rights reserved.
- * @copyright	Copyright (C) 2008 - 2009 JXtended, LLC. All rights reserved.
  * @license		GNU General Public License version 2 or later; see LICENSE.txt
  */
 
--- libraries/joomla/form/rules/rules.php
+++ libraries/joomla/form/rules/rules.php
@@ -4,7 +4,6 @@
  * @package		Joomla.Framework
  * @subpackage	Form
  * @copyright	Copyright (C) 2005 - 2009 Open Source Matters, Inc. All rights reserved.
- * @copyright	Copyright (C) 2008 - 2009 JXtended, LLC. All rights reserved.
  * @license		GNU General Public License version 2 or later; see LICENSE.txt
  */
 
