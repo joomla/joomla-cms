32ab750a7a67b4a3de0e2fbb327880b0624e4f72
Mark Dexter <dextercowley@gmail.com>
2009-12-30 11:18:06 +0000

    Add templates folder for TinyMCE
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@13907 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

---
+++ plugins/editors/tinymce/templates/template_list.js
@@ -0,0 +1,20 @@
+/**
+ * Tinymce template_list.js sample file
+ * @version tinymce 3.2.6
+ * @package		Joomla
+ * @copyright	Copyright (C) 2005 - 2009 Open Source Matters. All rights reserved.
+ * @license		GNU/GPL, see LICENSE.php
+ * Joomla! is free software. This version may have been modified pursuant
+ * to the GNU General Public License, and as distributed it includes or
+ * is derivative of works licensed under the GNU General Public License or
+ * other free or open source software licenses.
+ * See COPYRIGHT.php for copyright notices and details.
+ */
+
+var tinyMCETemplateList = [
+	// Name, URL, Description
+
+	["Simple snippet", "plugins/editors/tinymce/templates/snippet1.html", "Simple HTML snippet."],
+	["Layout", "plugins/editors/tinymce/templates/layout1.html", "HTMLLayout."]
+
+];
