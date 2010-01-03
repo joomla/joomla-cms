b9aa4f7d8502764d48d978515b975b05cf102f4a
Andrew Eddie <andrew.eddie@newlifeinit.com>
2010-01-03 23:19:54 +0000

    Fixed bug preventing correct language file loading.
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@13970 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- administrator/components/com_plugins/models/plugin.php
+++ administrator/components/com_plugins/models/plugin.php
@@ -204,6 +204,7 @@ class PluginsModelPlugin extends JModelForm
 
 		// Load the core and/or local language file(s).
 		$lang->load('plg_'.$folder.'_'.$element, JPATH_SITE);
+		$lang->load('plg_'.$folder.'_'.$element, JPATH_ADMINISTRATOR);
 		$lang->load('joomla', $client->path.'/plugins/'.$folder.'/'.$element);
 
 		// If an XML file was found in the component, load it first.
