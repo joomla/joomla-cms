de0b8ae300cab97113c6f8aa529305082461b898
Andrew Eddie <andrew.eddie@newlifeinit.com>
2010-01-04 22:57:44 +0000

    Accepted [#19275] Refactoring Administrator/mod_submenu.
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@14002 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

---
+++ administrator/modules/mod_submenu/tmpl/default.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @version		$Id$
+ * @package		Joomla.Administrator
+ * @subpackage	mod_submenu
+ * @copyright	Copyright (C) 2005 - 2009 Open Source Matters, Inc. All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// No direct access.
+defined('_JEXEC') or die;
+
+$hide = JRequest::getInt('hidemainmenu');
+?>
+<ul id="submenu">
+	<?php foreach ($list as $item) : ?>
+	<li>
+	<?php
+	if ($hide) :
+		if (isset ($item[2]) && $item[2] == 1) :
+			?><span class="nolink active"><?php echo $item[0]; ?></span><?php
+		else :
+			?><span class="nolink"><?php echo $item[0]; ?></span><?php
+		endif;
+	else :
+		if (isset ($item[2]) && $item[2] == 1) :
+			?><a class="active" href="<?php echo JFilterOutput::ampReplace($item[1]); ?>"><?php echo $item[0]; ?></a><?php
+		else :
+			?><a href="<?php echo JFilterOutput::ampReplace($item[1]); ?>"><?php echo $item[0]; ?></a><?php
+		endif;
+	endif;
+	?>
+	</li>
+	<?php endforeach; ?>
+</ul>
