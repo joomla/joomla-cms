32daeb71d6a9f464311b7f1a933f319c1c287d03
Christophe Demko <chdemko@gmail.com>
2010-01-16 00:50:42 +0000

    Correct of bug #18458 in JPagination class
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@14217 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- libraries/joomla/html/pagination.php
+++ libraries/joomla/html/pagination.php
@@ -94,7 +94,7 @@ class JPagination extends JObject
 		 * If limitstart is greater than total (i.e. we are asked to display records that don't exist)
 		 * then set limitstart to display the last natural page of results
 		 */
-		if ($this->limitstart > $this->total) {
+		if ($this->limitstart > $this->total - $this->limit) {
 			$this->limitstart = (int)(ceil($this->total / $this->limit) - 1) * $this->limit;
 		}
 
@@ -552,10 +552,10 @@ class JPagination extends JObject
 		{
 			$page = ($this->get('pages.current') -2) * $this->limit;
 
-			$page = $page == 0 ? '' : $page; //set the empty for removal from route
+			//$page = $page == 0 ? '' : $page; //set the empty for removal from route
 
 			$data->start->base	= '0';
-			$data->start->link	= JRoute::_($params.'&'.$this->prefix.'limitstart=');
+			$data->start->link	= JRoute::_($params.'&'.$this->prefix.'limitstart=0');
 			$data->previous->base	= $page;
 			$data->previous->link	= JRoute::_($params.'&'.$this->prefix.'limitstart='.$page);
 		}
@@ -581,7 +581,7 @@ class JPagination extends JObject
 		{
 			$offset = ($i -1) * $this->limit;
 
-			$offset = $offset == 0 ? '' : $offset;  //set the empty for removal from route
+			//$offset = $offset == 0 ? '' : $offset;  //set the empty for removal from route
 
 			$data->pages[$i] = new JPaginationObject($i, $this->prefix);
 			if ($i != $this->get('pages.current') || $this->_viewall)
