89b50c5155d9e3fd8289c6e66fc688ad32a58bce
Andrew Eddie <andrew.eddie@newlifeinit.com>
2010-01-16 00:10:49 +0000

    Added (temporarily) menus.resync in menus sub-controller to help repair incorrect component_id values in the jos_menu table.
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@14214 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- administrator/components/com_menus/controllers/menus.php
+++ administrator/components/com_menus/controllers/menus.php
@@ -47,8 +47,7 @@ class MenusControllerMenus extends JController
 
 		if (!is_array($cid) || count($cid) < 1) {
 			JError::raiseWarning(500, JText::_('JError_No_items_selected'));
-		}
-		else {
+		} else {
 			// Get the model.
 			$model = $this->getModel();
 
@@ -65,7 +64,6 @@ class MenusControllerMenus extends JController
 		$this->setRedirect('index.php?option=com_menus&view=menus');
 	}
 
-
 	/**
 	 * Rebuild the menu tree.
 	 *
@@ -80,16 +78,82 @@ class MenusControllerMenus extends JController
 		// Initialise variables.
 		$model = &$this->getModel('Item');
 
-		if ($model->rebuild())
-		{
+		if ($model->rebuild()) {
 			// Reorder succeeded.
 			$this->setMessage(JText::_('Menus_Rebuild_success'));
 			return true;
-		}
-		else {
+		} else {
 			// Rebuild failed.
 			$this->setMessage(JText::sprintf('Menus_Rebuild_failed', $model->getMessage()));
 			return false;
+		}
+	}
+
+	/**
+	 * Temporary method. This should go into the 1.5 to 1.6 upgrade routines.
+	 */
+	public function resync()
+	{
+		// Initialise variables.
+		$db = JFactory::getDbo();
+		$parts = null;
+
+		// Load a lookup table of all the component id's.
+		$components = $db->setQuery(
+			'SELECT element, extension_id' .
+			' FROM #__extensions' .
+			' WHERE type = '.$db->quote('component')
+		)->loadAssocList('element', 'extension_id');
+
+		if ($error = $db->getErrorMsg()) {
+			return JError::raiseWarning(500, $error);
+		}
+
+		// Load all the component menu links
+		$items = $db->setQuery(
+			'SELECT id, link, component_id' .
+			' FROM #__menu' .
+			' WHERE type = '.$db->quote('component')
+		)->loadObjectList();
+
+		if ($error = $db->getErrorMsg()) {
+			return JError::raiseWarning(500, $error);
+		}
+
+		foreach ($items as $item) {
+			// Parse the link.
+			parse_str(parse_url($item->link, PHP_URL_QUERY), $parts);
+
+			// Tease out the option.
+			if (isset($parts['option'])) {
+				$option = $parts['option'];
+
+				// Lookup the component ID
+				if (isset($components[$option])) {
+					$componentId = $components[$option];
+				} else {
+					// Mismatch. Needs human intervention.
+					$componentId = -1;
+				}
+
+				// Check for mis-matched component id's in the menu link.
+				if ($item->component_id != $componentId) {
+					// Update the menu table.
+					$log = "Link $item->id refers to $item->component_id, converting to $componentId ($item->link)";
+					echo "<br/>$log";
+
+					$db->setQuery(
+						'UPDATE #__menu' .
+						' SET component_id = '.$componentId.
+						' WHERE id = '.$item->id
+					)->query();
+					//echo "<br>".$db->getQuery();
+
+					if ($error = $db->getErrorMsg()) {
+						return JError::raiseWarning(500, $error);
+					}
+				}
+			}
 		}
 	}
 }
