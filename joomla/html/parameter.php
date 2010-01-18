798431f564f8ae114842aa273098b05ea7237f4d
Andrew Eddie <andrew.eddie@newlifeinit.com>
2010-01-18 19:21:25 +0000

    Fix incremental problem with JParameter group default.
    --HG--
    extra : convert_revision : svn%3A6f6e1ebd-4c2b-0410-823f-f34bde69bce9/development/trunk/libraries%4014279

--- joomla/html/parameter.php
+++ joomla/html/parameter.php
@@ -108,7 +108,7 @@ class JParameter extends JRegistry
 	 * @return	string
 	 * @since	1.5
 	 */
-	public function get($key, $default = '', $group = '_default')
+	public function get($key, $default = '', $group = 'default')
 	{
 		$value = $this->getValue($group.'.'.$key);
 		$result = (empty($value) && ($value !== 0) && ($value !== '0')) ? $default : $value;
