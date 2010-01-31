9a2f1c8f71bbcc25237d1f45bab707b4b22279a4
Hannes Papenberg <info@joomlager.de>
2010-01-31 14:32:22 +0000

    # Fixed group bug in JParameter - Thanks James Kennard
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@14466 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- libraries/joomla/html/parameter.php
+++ libraries/joomla/html/parameter.php
@@ -217,7 +217,7 @@ class JParameter extends JRegistry
 		}
 		$results = array();
 		foreach ($this->_xml[$group]->children() as $param)  {
-			$result = $this->getParam($param, $name);
+			$result = $this->getParam($param, $name, $group);
 			$results[$result[5]] = $result;
 		}
 		return $results;
@@ -272,7 +272,7 @@ class JParameter extends JRegistry
 
 		$results = array();
 		foreach ($this->_xml[$group]->children() as $param)  {
-			$results[] = $this->getParam($param, $name);
+			$results[] = $this->getParam($param, $name, $group);
 		}
 		return $results;
 	}
