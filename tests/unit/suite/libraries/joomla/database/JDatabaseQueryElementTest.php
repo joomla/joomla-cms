85dcf9e52e2f4ae521007a53f9484fe5cce7922d
Andrew Eddie <andrew.eddie@newlifeinit.com>
2010-02-04 19:18:15 +0000

    Fix bugs in JDatabaseQuery tests
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@14600 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- tests/unit/suite/libraries/joomla/database/JDatabaseQueryElementTest.php
+++ tests/unit/suite/libraries/joomla/database/JDatabaseQueryElementTest.php
@@ -5,7 +5,7 @@
  * @license		GNU General Public License version 2 or later; see LICENSE.txt
  */
 
-require_once JPATH_BASE.'/libraries/joomla/database/query.php';
+require_once JPATH_BASE.'/libraries/joomla/database/databasequery.php';
 
 /**
  * Test class for JDatabaseQueryElement.
