72b4193cd03f07e5ec0b64fd8da52e181c0b4d97
Jean-Marie Simonet <infograf768@gmail.com>
2010-02-10 05:06:04 +0000

    Refactored mod_login (site) ini/keys + bug
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@14771 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- modules/mod_login/tmpl/default.php
+++ modules/mod_login/tmpl/default.php
@@ -14,15 +14,15 @@ defined('_JEXEC') or die;
 <form action="index.php" method="post" name="form-login" id="form-login">
 <?php if ($params->get('greeting')) : ?>
 	<div>
-	<?php if ($params->get('name')) : {
-		echo JText::sprintf('HINAME', $user->get('name'));
+	<?php if($params->get('name') == 0) : {
+		echo JText::sprintf('MOD_LOGIN_HINAME', $user->get('name'));
 	} else : {
-		echo JText::sprintf('HINAME', $user->get('username'));
+		echo JText::sprintf('MOD_LOGIN_HINAME', $user->get('username'));
 	} endif; ?>
 	</div>
 <?php endif; ?>
 	<div align="center">
-		<input type="submit" name="Submit" class="button" value="<?php echo JText::_('BUTTON_LOGOUT'); ?>" />
+		<input type="submit" name="Submit" class="button" value="<?php echo JText::_('MOD_LOGIN_BUTTON_LOGOUT'); ?>" />
 	</div>
 
 	<input type="hidden" name="option" value="com_users" />
@@ -45,36 +45,36 @@ endif; ?>
 	<?php echo $params->get('pretext'); ?>
 	<fieldset class="input">
 	<p id="form-login-username">
-		<label for="modlgn_username"><?php echo JText::_('Username') ?></label>
+		<label for="modlgn_username"><?php echo JText::_('MOD_LOGIN_VALUE_USERNAME') ?></label>
 		<input id="modlgn_username" type="text" name="username" class="inputbox" alt="username" size="18" />
 	</p>
 	<p id="form-login-password">
-		<label for="modlgn_passwd"><?php echo JText::_('Password') ?></label>
+		<label for="modlgn_passwd"><?php echo JText::_('MOD_LOGIN_PASSWORD') ?></label>
 		<input id="modlgn_passwd" type="password" name="password" class="inputbox" size="18" alt="password" />
 	</p>
 	<?php if (JPluginHelper::isEnabled('system', 'remember')) : ?>
 	<p id="form-login-remember">
-		<label for="modlgn_remember"><?php echo JText::_('REMEMBER_ME') ?></label>
-		<input id="modlgn_remember" type="checkbox" name="remember" class="inputbox" value="yes" alt="<?php echo JText::_('REMEMBER_ME') ?>" />
+		<label for="modlgn_remember"><?php echo JText::_('MOD_LOGIN_REMEMBER_ME') ?></label>
+		<input id="modlgn_remember" type="checkbox" name="remember" class="inputbox" value="yes" alt="<?php echo JText::_('MOD_LOGIN_REMEMBER_ME') ?>" />
 	</p>
 	<?php endif; ?>
-	<input type="submit" name="Submit" class="button" value="<?php echo JText::_('LOGIN') ?>" />
+	<input type="submit" name="Submit" class="button" value="<?php echo JText::_('MOD_LOGIN_BUTTON_LOGIN') ?>" />
 	</fieldset>
 	<ul>
 		<li>
 			<a href="<?php echo JRoute::_('index.php?option=com_users&view=reset'); ?>">
-			<?php echo JText::_('FORGOT_YOUR_PASSWORD'); ?></a>
+			<?php echo JText::_('MOD_LOGIN_FORGOT_YOUR_PASSWORD'); ?></a>
 		</li>
 		<li>
 			<a href="<?php echo JRoute::_('index.php?option=com_users&view=remind'); ?>">
-			<?php echo JText::_('FORGOT_YOUR_USERNAME'); ?></a>
+			<?php echo JText::_('MOD_LOGIN_FORGOT_YOUR_USERNAME'); ?></a>
 		</li>
 		<?php
 		$usersConfig = &JComponentHelper::getParams('com_users');
 		if ($usersConfig->get('allowUserRegistration')) : ?>
 		<li>
 			<a href="<?php echo JRoute::_('index.php?option=com_users&view=registration'); ?>">
-				<?php echo JText::_('REGISTER'); ?></a>
+				<?php echo JText::_('MOD_LOGIN_REGISTER'); ?></a>
 		</li>
 		<?php endif; ?>
 	</ul>
