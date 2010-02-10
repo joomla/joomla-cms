77e31a84a9642c0c67106e84fda596c9f285c91a
Hannes Papenburg <info@joomlager.de>
2010-02-10 13:13:15 +0000

    Reverting revisions 14760, 14761, 14773, 14775, 14776, 14778
    --HG--
    extra : convert_revision : svn%3A6f6e1ebd-4c2b-0410-823f-f34bde69bce9/development/trunk/libraries%4014781

--- joomla/application/component/helper.php
+++ joomla/application/component/helper.php
@@ -119,10 +119,8 @@ class JComponentHelper
 
 		// Load common and local language files.
 		$lang = &JFactory::getLanguage();
-			$lang->load($option, JPATH_BASE, null, false, false)
-		||	$lang->load($option, JPATH_COMPONENT, null, false, false)
-		||	$lang->load($option, JPATH_BASE, $lang->getDefault(), false, false)
-		||	$lang->load($option, JPATH_COMPONENT, $lang->getDefault(), false, false);
+		$lang->load($option, JPATH_COMPONENT);
+		$lang->load($option);
 
 		// Handle template preview outlining.
 		$contents = null;
--- joomla/installer/adapters/file.php
+++ joomla/installer/adapters/file.php
@@ -37,14 +37,10 @@ class JInstallerFile extends JAdapterInstance
 		$extension = strtolower(JFilterInput::getInstance()->clean((string)$this->manifest->name, 'cmd'));
 		$lang =& JFactory::getLanguage();
 		$source = $path;
-			$lang->load($extension . '.manage', $source, null, false, false)
-		||	$lang->load($extension, $source, null, false, false)
-		||	$lang->load($extension . '.manage', JPATH_SITE, null, false, false)
-		||	$lang->load($extension , JPATH_SITE, null, false, false)
-		||	$lang->load($extension . '.manage', $source, $lang->getDefault(), false, false)
-		||	$lang->load($extension, $source, $lang->getDefault(), false, false)
-		||	$lang->load($extension . '.manage', JPATH_SITE, $lang->getDefault(), false, false)
-		||	$lang->load($extension , JPATH_SITE, $lang->getDefault(), false, false);
+		$lang->load($extension , JPATH_SITE);		
+		$lang->load($extension . '.manage', JPATH_SITE);		
+		$lang->load($extension, $source);
+		$lang->load($extension . '.manage', $source);
 	}
 	/**
 	 * Custom install method
