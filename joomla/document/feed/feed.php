ee77d3dc146629df4b17558751313956c6e9b1f8
Jean-Marie Simonet <infograf768@gmail.com>
2010-03-02 05:10:10 +0000

    $^ Refactored com_newsfeeds ini/keys + some JCommon.
    --HG--
    extra : convert_revision : svn%3A6f6e1ebd-4c2b-0410-823f-f34bde69bce9/development/trunk/libraries%4015130

--- joomla/document/feed/feed.php
+++ joomla/document/feed/feed.php
@@ -212,7 +212,7 @@ class JDocumentFeed extends JDocument
 		// Instantiate feed renderer and set the mime encoding
 		$renderer = &$this->loadRenderer(($type) ? $type : 'rss');
 		if (!is_a($renderer, 'JDocumentRenderer')) {
-			JError::raiseError(404, JText::_('Resource Not Found'));
+			JError::raiseError(404, JText::_('JCOMMON_RESOURCE_NOT_FOUND'));
 		}
 		$this->setMimeEncoding($renderer->getContentType());
 
