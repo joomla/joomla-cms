2c5ed821f178f7b790e43d9b7b73b10efabc1a1d
Mark Dexter <dextercowley@gmail.com>
2010-03-31 14:55:09 +0000

    Add CSS classes to pagination (Angie Radtke)
    --HG--
    extra : convert_revision : svn%3A6f6e1ebd-4c2b-0410-823f-f34bde69bce9/development/trunk/libraries%4015732

--- joomla/html/pagination.php
+++ joomla/html/pagination.php
@@ -474,17 +474,15 @@ class JPagination extends JObject
 	protected function _list_render($list)
 	{
 		// Reverse output rendering for right-to-left display.
-		$html = '&lt;&lt; ';
-		$html .= $list['start']['data'];
-		$html .= ' &lt; ';
-		$html .= $list['previous']['data'];
+		$html = '<ul>';
+		$html .= '<li class="pagination-start">'.$list['start']['data'].'</li>';
+		$html .= '<li class="pagination-prev">'.$list['previous']['data'].'</li>';
 		foreach($list['pages'] as $page) {
-			$html .= ' '.$page['data'];
+			$html .= '<li>'.$page['data'].'</li>';
 		}
-		$html .= ' '. $list['next']['data'];
-		$html .= ' &gt;';
-		$html .= ' '. $list['end']['data'];
-		$html .= ' &gt;&gt;';
+		$html .= '<li class="pagination-next">'. $list['next']['data'].'</li>';
+		$html .= '<li class="pagination-end">'. $list['end']['data'].'</li>';
+		$html .= '</ul>';
 
 		return $html;
 	}
