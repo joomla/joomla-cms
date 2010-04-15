5cf21254e6adcbee19f0e77bdc24db8c621f8b60
Ian MacLennan <ian.maclennan@joomla.org>
2010-04-15 09:52:37 +0000

    # Minor Docblock fix
    --HG--
    extra : convert_revision : svn%3A6f6e1ebd-4c2b-0410-823f-f34bde69bce9/development/trunk/libraries%4016102

--- joomla/application/component/modelform.php
+++ joomla/application/component/modelform.php
@@ -1,8 +1,8 @@
 <?php
 /**
  * @version		$Id$
- * @package		Joomla
- * @subpackage	com_users
+ * @package		Joomla.Framework
+ * @subpackage	Application
  * @copyright	Copyright (C) 2005 - 2010 Open Source Matters, Inc. All rights reserved.
  * @license		GNU General Public License version 2 or later; see LICENSE.txt
  */
