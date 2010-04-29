199d03fc44e54d1659cbe994a0d88ec149a16595
Christophe Demko <chdemko@gmail.com>
2010-04-29 01:00:42 +0000

    Fixed some warning in php 5.3
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@16575 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- libraries/joomla/client/ftp.php
+++ libraries/joomla/client/ftp.php
@@ -1168,7 +1168,7 @@ class JFTP extends JObject
 		// Find out the format of the directory listing by matching one of the regexps
 		$osType = null;
 		foreach ($regexps as $k=>$v) {
-			if (ereg($v, $contents[0])) {
+			if (@ereg($v, $contents[0])) {
 				$osType = $k;
 				$regexp = $v;
 				break;
@@ -1185,7 +1185,7 @@ class JFTP extends JObject
 		if ($osType == 'UNIX') {
 			foreach ($contents as $file) {
 				$tmp_array = null;
-				if (ereg($regexp, $file, $regs)) {
+				if (@ereg($regexp, $file, $regs)) {
 					$fType = (int) strpos("-dl", $regs[1] { 0 });
 					//$tmp_array['line'] = $regs[0];
 					$tmp_array['type'] = $fType;
@@ -1194,7 +1194,7 @@ class JFTP extends JObject
 					$tmp_array['user'] = $regs[3];
 					$tmp_array['group'] = $regs[4];
 					$tmp_array['size'] = $regs[5];
-					$tmp_array['date'] = date("m-d", strtotime($regs[6]));
+					$tmp_array['date'] = @date("m-d", strtotime($regs[6]));
 					$tmp_array['time'] = $regs[7];
 					$tmp_array['name'] = $regs[9];
 				}
@@ -1214,7 +1214,7 @@ class JFTP extends JObject
 		elseif ($osType == 'MAC') {
 			foreach ($contents as $file) {
 				$tmp_array = null;
-				if (ereg($regexp, $file, $regs)) {
+				if (@ereg($regexp, $file, $regs)) {
 					$fType = (int) strpos("-dl", $regs[1] { 0 });
 					//$tmp_array['line'] = $regs[0];
 					$tmp_array['type'] = $fType;
@@ -1242,7 +1242,7 @@ class JFTP extends JObject
 		} else {
 			foreach ($contents as $file) {
 				$tmp_array = null;
-				if (ereg($regexp, $file, $regs)) {
+				if (@ereg($regexp, $file, $regs)) {
 					$fType = (int) ($regs[7] == '<DIR>');
 					$timestamp = strtotime("$regs[3]-$regs[1]-$regs[2] $regs[4]:$regs[5]$regs[6]");
 					//$tmp_array['line'] = $regs[0];
