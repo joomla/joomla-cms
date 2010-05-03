1599946b859a7c593be0f74cf8a8e1b6d1334395
Andrew Eddie <andrew.eddie@newlifeinit.com>
2010-05-03 18:25:23 +0000

    Merge patched to cache the query building in JModelList (Christophe Demko).
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@16704 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- components/com_banners/models/banners.php
+++ components/com_banners/models/banners.php
@@ -52,72 +52,70 @@ class BannersModelBanners extends JModelList
 	 */
 	function getListQuery()
 	{
-		static $query;
-		if (!isset($query)) {
-			$db		= $this->getDbo();
-			$query	= $db->getQuery(true);
-			$ordering	= $this->getState('filter.ordering');
-			$tagSearch	= $this->getState('filter.tag_search');
-			$cid		= $this->getState('filter.client_id');
-			$catid		= $this->getState('filter.category_id');
-			$keywords	= $this->getState('filter.keywords');
-			$randomise	= ($ordering == 'random');
+		$db			= $this->getDbo();
+		$query		= $db->getQuery(true);
+		$ordering	= $this->getState('filter.ordering');
+		$tagSearch	= $this->getState('filter.tag_search');
+		$cid		= $this->getState('filter.client_id');
+		$catid		= $this->getState('filter.category_id');
+		$keywords	= $this->getState('filter.keywords');
+		$randomise	= ($ordering == 'random');
 
-			$query->select(
-				'a.id as id,'.
-				'a.type as type,'.
-				'a.name as name,'.
-				'a.clickurl as clickurl,'.
-				'a.cid as cid,'.
-				'a.params as params,'.
-				'a.custombannercode as custombannercode,'.
-				'a.track_impressions as track_impressions'
-			);
-			$query->from('#__banners as a');
-			$query->where('a.state=1');
-			$query->where("(NOW() >= a.publish_up OR a.publish_up='0000-00-00 00:00:00')");
-			$query->where("(NOW() <= a.publish_down OR a.publish_down='0000-00-00 00:00:00')");
-			$query->where('(a.imptotal = 0 OR a.impmade = a.imptotal)');
+		$query->select(
+			'a.id as id,'.
+			'a.type as type,'.
+			'a.name as name,'.
+			'a.clickurl as clickurl,'.
+			'a.cid as cid,'.
+			'a.params as params,'.
+			'a.custombannercode as custombannercode,'.
+			'a.track_impressions as track_impressions'
+		);
+		$query->from('#__banners as a');
+		$query->where('a.state=1');
+		$query->where("(NOW() >= a.publish_up OR a.publish_up='0000-00-00 00:00:00')");
+		$query->where("(NOW() <= a.publish_down OR a.publish_down='0000-00-00 00:00:00')");
+		$query->where('(a.imptotal = 0 OR a.impmade = a.imptotal)');
 
-			if ($cid) {
-				$query->where('a.cid = ' . (int) $cid);
-				$query->join('LEFT', '#__banner_clients AS cl ON cl.id = a.cid');
-				$query->select('cl.track_impressions as client_track_impressions');
-				$query->where('cl.state = 1');
-			}
-
-			if ($catid) {
-				$query->where('a.catid = ' . (int) $catid);
-				$query->join('LEFT', '#__categories AS cat ON cat.id = a.catid');
-				$query->where('cat.published = 1');
-			}
-
-			if ($tagSearch) {
-				if (count($keywords) == 0) {
-					$query->where('0');
-				} else {
-					$temp = array();
-					$config = &JComponentHelper::getParams('com_banners');
-					$prefix = $config->get('metakey_prefix');
-
-					foreach ($keywords as $keyword) {
-						$keyword=trim($keyword);
-						$condition1 = "a.own_prefix=1 AND  a.metakey_prefix=SUBSTRING('".$keyword."',1,LENGTH( a.metakey_prefix)) OR a.own_prefix=0 AND cl.own_prefix=1 AND cl.metakey_prefix=SUBSTRING('".$keyword."',1,LENGTH(cl.metakey_prefix)) OR a.own_prefix=0 AND cl.own_prefix=0 AND ".($prefix==substr($keyword,0,strlen($prefix))?'1':'0');
-
-						$condition2="a.metakey REGEXP '[[:<:]]".$db->getEscaped($keyword) . "[[:>:]]'";
-						if ($cid) {
-							$condition2.=" OR cl.metakey REGEXP '[[:<:]]".$db->getEscaped($keyword) . "[[:>:]]'";
-						}
-						if ($catid) {
-							$condition2.=" OR cat.metakey REGEXP '[[:<:]]".$db->getEscaped($keyword) . "[[:>:]]'";
-						}
-						$temp[]="($condition1) AND ($condition2)";
-					}
-					$query->where('(' . implode(' OR ', $temp). ')');
-				}
-			}
-			$query->order('a.sticky DESC,'. ($randomise ? 'RAND()' : 'a.ordering'));
+		if ($cid) {
+			$query->where('a.cid = ' . (int) $cid);
+			$query->join('LEFT', '#__banner_clients AS cl ON cl.id = a.cid');
+			$query->select('cl.track_impressions as client_track_impressions');
+			$query->where('cl.state = 1');
 		}
+
+		if ($catid) {
+			$query->where('a.catid = ' . (int) $catid);
+			$query->join('LEFT', '#__categories AS cat ON cat.id = a.catid');
+			$query->where('cat.published = 1');
+		}
+
+		if ($tagSearch) {
+			if (count($keywords) == 0) {
+				$query->where('0');
+			} else {
+				$temp = array();
+				$config = &JComponentHelper::getParams('com_banners');
+				$prefix = $config->get('metakey_prefix');
+
+				foreach ($keywords as $keyword) {
+					$keyword=trim($keyword);
+					$condition1 = "a.own_prefix=1 AND  a.metakey_prefix=SUBSTRING('".$keyword."',1,LENGTH( a.metakey_prefix)) OR a.own_prefix=0 AND cl.own_prefix=1 AND cl.metakey_prefix=SUBSTRING('".$keyword."',1,LENGTH(cl.metakey_prefix)) OR a.own_prefix=0 AND cl.own_prefix=0 AND ".($prefix==substr($keyword,0,strlen($prefix))?'1':'0');
+
+					$condition2="a.metakey REGEXP '[[:<:]]".$db->getEscaped($keyword) . "[[:>:]]'";
+					if ($cid) {
+						$condition2.=" OR cl.metakey REGEXP '[[:<:]]".$db->getEscaped($keyword) . "[[:>:]]'";
+					}
+					if ($catid) {
+						$condition2.=" OR cat.metakey REGEXP '[[:<:]]".$db->getEscaped($keyword) . "[[:>:]]'";
+					}
+					$temp[]="($condition1) AND ($condition2)";
+				}
+				$query->where('(' . implode(' OR ', $temp). ')');
+			}
+		}
+		$query->order('a.sticky DESC,'. ($randomise ? 'RAND()' : 'a.ordering'));
+
 		return $query;
 	}
 
