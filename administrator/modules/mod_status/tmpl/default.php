5a084be52ca9ab4862e27eaf5971cce65e581ca2
Andrew Eddie <andrew.eddie@newlifeinit.com>
2010-05-03 20:57:38 +0000

    Merge patch [#20330] mod_status show / hide params (Phil Snell).
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@16712 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- administrator/modules/mod_status/tmpl/default.php
+++ administrator/modules/mod_status/tmpl/default.php
@@ -13,10 +13,14 @@ defined('_JEXEC') or die;
 $output = array();
 
 // Print the logged in users.
+if ($params->get('show_loggedin_users', 1)) :
 	$output[] = "<span class=\"loggedin-users\">".$online_num. " " . JText::_('MOD_STATUS_USERS') . "</span>";
+endif;
 
 //  Print the inbox message.
+if ($params->get('show_messages', 1)) :
 	$output[] = "<span class=\"$inboxClass\"><a href=\"$inboxLink\">". $unread . " " . JText::_('MOD_STATUS_MESSAGES'). "</a></span>";
+endif;
 
 // Print the Preview link to Main site.
 	$output[] = "<span class=\"viewsite\"><a href=\"".JURI::root()."\" target=\"_blank\">".JText::_('MOD_STATUS_VIEW_SITE')."</a></span>";
@@ -25,14 +29,16 @@ $output = array();
 	$output[] = "<span class=\"logout\"><a href=\"$logoutLink\">".JText::_('MOD_STATUS_LOG_OUT')."</a></span>";
 
 // Print the back-end logged in users.
+if ($params->get('show_loggedin_users_admin', 1)) :
 	$output[] = "<span class=\"loggedin-users\">".$count. " " . JText::_('MOD_STATUS_BACKEND_USERS') . "</span>";
+endif;
 
 // Reverse rendering order for rtl display.
-if ($lang->isRTL()) {
+if ($lang->isRTL()) :
 	$output = array_reverse($output);
-}
+endif;
 
 // Output the items.
-foreach ($output as $item){
+foreach ($output as $item) :
 	echo $item;
-}
+endforeach;
