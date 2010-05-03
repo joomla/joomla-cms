7174d0bbaea479bf9d8ac4d0c4e1762c1e5ddbd1
Christophe Demko <chdemko@gmail.com>
2010-05-03 01:20:35 +0000

    Add an xml attribute to editor field: 'hide' to specify which buttons to hide
    --HG--
    extra : convert_revision : svn%3A6f6e1ebd-4c2b-0410-823f-f34bde69bce9/development/trunk/libraries%4016677

--- joomla/form/fields/editor.php
+++ joomla/form/fields/editor.php
@@ -61,10 +61,12 @@ class JFormFieldEditor extends JFormField
 			$buttons = explode(',', $buttons);
 		}
 
+		$hide		= ((string) $this->element['hide']) ? explode(',',(string) $this->element['hide']) : array();
+
 		// Get an editor object.
 		$editor = $this->getEditor();
 
-		return $editor->display($this->name, htmlspecialchars($this->value, ENT_COMPAT, 'UTF-8'), $width, $height, $cols, $rows, $buttons, $this->id);
+		return $editor->display($this->name, htmlspecialchars($this->value, ENT_COMPAT, 'UTF-8'), $width, $height, $cols, $rows, $buttons ? (is_array($buttons) ? array_merge($buttons,$hide) : $hide) : false, $this->id);
 	}
 
 	/**
