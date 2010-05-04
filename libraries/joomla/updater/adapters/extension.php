277f71b72d0f44de171bfc47494e45c3c3a2df16
Samuel Moffatt <pasamio@gmail.com>
2010-05-04 07:28:12 +0000

    Fixed formatting for updater and added mark bad capability for update sites that fail to work properly.
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@16753 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- libraries/joomla/updater/adapters/extension.php
+++ libraries/joomla/updater/adapters/extension.php
@@ -88,7 +88,12 @@ class JUpdaterExtension extends JUpdateAdapter
 		$dbo =& $this->parent->getDBO();
 
 		if (!($fp = @fopen($url, "r"))) {
-			// TODO: Add a 'mark bad' setting here somehow
+			$query = $dbo->getQuery(true);
+			$query->update('#__update_sites');
+			$query->set('enabled = 0');
+			$query->where('update_site_id = '. $this->_update_site_id);
+			$dbo->setQuery($query);
+			$dbo->Query();
 			JError::raiseWarning('101', JText::sprintf('JLIB_UPDATER_ERROR_EXTENSION_OPEN_URL', $url));
 			return false;
 		}
