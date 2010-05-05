9671033cf32453a3838721134c4c8ad655995ab2
Louis Landry <louis.landry@joomla.org>
2010-05-05 10:37:09 +0000

    Fixed minor reference issue.
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@16823 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- components/com_content/views/article/view.html.php
+++ components/com_content/views/article/view.html.php
@@ -96,7 +96,7 @@ class ContentViewArticle extends JView
 		// Process the content plugins.
 		//
 		JPluginHelper::importPlugin('content');
-		$results = $dispatcher->trigger('onContentPrepare', array ('com_content.article', $item, $params, $offset));
+		$results = $dispatcher->trigger('onContentPrepare', array ('com_content.article', &$item, &$params, $offset));
 		if ($item->params->get('show_intro', 1) == 1) {
 			$item->text = $item->introtext . ' ' . $item->fulltext;
 		} else {
