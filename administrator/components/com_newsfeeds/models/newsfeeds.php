0dbf0471a5129cc3f9382968a2c64742835fa7ed
Christophe Demko <chdemko@gmail.com>
2010-05-07 08:12:57 +0000

    Fix missing language_title field in newsfeeds view
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@16886 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- administrator/components/com_newsfeeds/models/newsfeeds.php
+++ administrator/components/com_newsfeeds/models/newsfeeds.php
@@ -99,6 +99,10 @@ class NewsfeedsModelNewsfeeds extends JModelList
 		);
 		$query->from('`#__newsfeeds` AS a');
 
+		// Join over the language
+		$query->select('l.title AS language_title');
+		$query->join('LEFT', '`#__languages` AS l ON l.lang_code = a.language');
+
 		// Join over the users for the checked out user.
 		$query->select('uc.name AS editor');
 		$query->join('LEFT', '#__users AS uc ON uc.id=a.checked_out');
