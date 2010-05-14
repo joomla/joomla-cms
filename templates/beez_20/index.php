bfb4cd2a5439edb8b44ba2b165c284964635e38b
Mark Dexter <dextercowley@gmail.com>
2010-05-14 17:48:55 +0000

    tweak to beez to fix Eclipse validation
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@17063 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- templates/beez_20/index.php
+++ templates/beez_20/index.php
@@ -31,7 +31,7 @@ $templateparams	= $app->getTemplate(true)->params;
 <?php if(!$templateparams->get('html5', 0)): ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <?php else: ?>
-<!DOCTYPE html>
+	<?php echo '<!DOCTYPE html>'; ?>
 <?php endif; ?>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>" >
 	<head>
