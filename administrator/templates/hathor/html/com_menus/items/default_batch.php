8fdbb47f43361b4dbe4227039ab212c1c2078829
Andrew Eddie <andrew.eddie@newlifeinit.com>
2010-05-17 04:27:39 +0000

    Fixed issue [#20386] [patch] update Hathor overrides with changes made to com_menu default_batch.php (Andrea Tarr).
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@17122 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- administrator/templates/hathor/html/com_menus/items/default_batch.php
+++ administrator/templates/hathor/html/com_menus/items/default_batch.php
@@ -15,28 +15,28 @@ $options = array(
 	JHtml::_('select.option', 'c', JText::_('COM_MENUS_BATCH_COPY')),
 	JHtml::_('select.option', 'm', JText::_('COM_MENUS_BATCH_MOVE'))
 );
-$published = (int) $this->state->get('filter.published');
+$published = $this->state->get('filter.published');
 ?>
-	<fieldset class="batch">
-		<legend><?php echo JText::_('COM_MENUS_BATCH_OPTIONS');?></legend>
+<fieldset class="batch">
+	<legend><?php echo JText::_('COM_MENUS_BATCH_OPTIONS');?></legend>
 
-				<label id="batch-access-lbl" for="batch-access">
-					<?php echo JText::_('COM_MENUS_BATCH_ACCESS_LABEL'); ?>
+			<label id="batch-access-lbl" for="batch-access">
+				<?php echo JText::_('COM_MENUS_BATCH_ACCESS_LABEL'); ?>
+			</label>
+			<?php echo JHtml::_('access.assetgrouplist', 'batch[assetgroup_id]', '', 'class="inputbox"', array('title' => '', 'id' => 'batch-access'));?>
+
+			<?php if ($published >= 0) : ?>
+				<label id="batch-choose-action-lbl" for="batch-menu-id">
+					<?php echo JText::_('COM_MENUS_BATCH_MENU_LABEL'); ?>
 				</label>
-				<?php echo JHtml::_('access.assetgrouplist', 'batch[assetgroup_id]', '', 'class="inputbox"', array('title' => '', 'id' => 'batch-access'));?>
+					<select name="batch[menu_id]" class="inputbox" id="batch-menu-id">
+						<option></option>
+						<?php echo JHtml::_('select.options', JHtml::_('menu.menuitems', array('published' => $published)));?>
+					</select>
+					<?php echo JHTML::_( 'select.radiolist', $options, 'batch[move_copy]', '', 'value', 'text', 'm'); ?>
+			<?php endif; ?>
 
-				<?php if ($published >= 0) : ?>
-					<label id="batch-choose-action-lbl" for="batch-menu-id">
-						<?php echo JText::_('COM_MENUS_BATCH_MENU_LABEL'); ?>
-					</label>
-						<select name="batch[menu_id]" class="inputbox" id="batch-menu-id">
-							<option></option>
-							<?php echo JHtml::_('select.options', JHtml::_('menu.menuitems', array('published' => $published)));?>
-						</select>
-						<?php echo JHTML::_( 'select.radiolist', $options, 'batch[move_copy]', '', 'value', 'text', 'm'); ?>
-				<?php endif; ?>
-
-			<button type="submit" onclick="submitbutton('item.batch');">
-				<?php echo JText::_('COM_MENUS_BATCH_PROCESS'); ?>
-			</button>
-	</fieldset>
+		<button type="submit" onclick="submitbutton('item.batch');">
+			<?php echo JText::_('COM_MENUS_BATCH_PROCESS'); ?>
+		</button>
+</fieldset>
