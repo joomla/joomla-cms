16addbda89e2701ead97b9e1eaa63e8a1840e8b0
Ron Severdia <severdia@gmail.com>
2010-05-17 06:30:14 +0000

    # Fix CSS & language strings for Weblinks UI + Added missing icon
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@17133 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- administrator/components/com_weblinks/views/weblink/tmpl/edit_metadata.php
+++ administrator/components/com_weblinks/views/weblink/tmpl/edit_metadata.php
@@ -18,21 +18,21 @@ foreach ($fieldSets as $name => $fieldSet) :
 	endif;
 	?>
 	<fieldset class="panelform">
-	<ul class="adminformlist">
-		<?php if ($name == 'jmetadata') : // Include the real fields in this panel. ?>
-			<li><?php echo $this->form->getLabel('metadesc'); ?>
-			<?php echo $this->form->getInput('metadesc'); ?></li>
-
-			<li><?php echo $this->form->getLabel('metakey'); ?>
-			<?php echo $this->form->getInput('metakey'); ?></li>
-
-			<li><?php echo $this->form->getLabel('xreference'); ?>
-			<?php echo $this->form->getInput('xreference'); ?></li>
-		<?php endif; ?>
-		<?php foreach ($this->form->getFieldset($name) as $field) : ?>
-			<li><?php echo $field->label; ?>
-			<?php echo $field->input; ?></li>
-		<?php endforeach; ?>
+		<ul class="adminformlist">
+			<?php if ($name == 'jmetadata') : // Include the real fields in this panel. ?>
+				<li><?php echo $this->form->getLabel('metadesc'); ?>
+				<?php echo $this->form->getInput('metadesc'); ?></li>
+	
+				<li><?php echo $this->form->getLabel('metakey'); ?>
+				<?php echo $this->form->getInput('metakey'); ?></li>
+	
+				<li><?php echo $this->form->getLabel('xreference'); ?>
+				<?php echo $this->form->getInput('xreference'); ?></li>
+			<?php endif; ?>
+			<?php foreach ($this->form->getFieldset($name) as $field) : ?>
+				<li><?php echo $field->label; ?>
+				<?php echo $field->input; ?></li>
+			<?php endforeach; ?>
 		</ul>
 	</fieldset>
 <?php endforeach; ?>
