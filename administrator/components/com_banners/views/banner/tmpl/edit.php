452f735f8e4aee7707cb20e6ae6ef35078ada9a1
Jean-Marie Simonet <infograf768@gmail.com>
2010-05-20 17:43:30 +0000

    # Saving Banners urls in the clickurl db field [20532]
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@17204 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- administrator/components/com_banners/views/banner/tmpl/edit.php
+++ administrator/components/com_banners/views/banner/tmpl/edit.php
@@ -27,10 +27,12 @@ JHtml::_('behavior.formvalidation');
 	window.addEvent('domready', function() {
 		document.id('jform_type0').addEvent('click', function(e){
 			document.id('image').setStyle('display', 'block');
+			document.id('url').setStyle('display', 'block');
 			document.id('custom').setStyle('display', 'none');
 		});
 		document.id('jform_type1').addEvent('click', function(e){
 			document.id('image').setStyle('display', 'none');
+			document.id('url').setStyle('display', 'none');
 			document.id('custom').setStyle('display', 'block');
 		});
 		if(document.id('jform_type0').checked==true) {
@@ -78,11 +80,15 @@ JHtml::_('behavior.formvalidation');
 					<?php echo $this->form->getInput('custombannercode'); ?>
 				</div>
 				</li>
+				
 				<li><?php echo $this->form->getLabel('description'); ?>
 				<?php echo $this->form->getInput('description'); ?></li>
-
-				<li><?php echo $this->form->getLabel('clickurl'); ?>
-				<?php echo $this->form->getInput('clickurl'); ?></li>
+				
+				<li><div id="url">
+				<?php echo $this->form->getLabel('clickurl'); ?>
+				<?php echo $this->form->getInput('clickurl'); ?>
+				</div>
+				</li>
 
 				<li><?php echo $this->form->getLabel('language'); ?>
 				<?php echo $this->form->getInput('language'); ?></li>
