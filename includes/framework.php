2470c9291dbb7d0832db6b960a7530b8a648ff0f
Andrew Eddie <andrew.eddie@newlifeinit.com>
2010-05-25 02:10:02 +0000

    Fixed issue [#20226] Recursion in some circumstances when reinstalling (Elin Waring, Christophe Demko).
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@17249 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- includes/framework.php
+++ includes/framework.php
@@ -10,50 +10,44 @@
 // No direct access.
 defined('_JEXEC') or die;
 
-/*
- * Joomla! system checks.
- */
+//
+// Joomla system checks.
+//
 
 @set_magic_quotes_runtime(0);
 @ini_set('zend.ze1_compatibility_mode', '0');
 
-/*
- * Installation check, and check on removal of the install directory.
- */
+//
+// Installation check, and check on removal of the install directory.
+//
 
-if (!file_exists(JPATH_CONFIGURATION.DS.'configuration.php') || (filesize(JPATH_CONFIGURATION.DS.'configuration.php') < 10) /*|| file_exists(JPATH_INSTALLATION.DS.'index.php')*/)
-{
-	if (file_exists(JPATH_INSTALLATION.DS.'index.php'))
-	{
-		header('Location: installation/index.php');
+if (!file_exists(JPATH_CONFIGURATION.DS.'configuration.php') || (filesize(JPATH_CONFIGURATION.DS.'configuration.php') < 10) /*|| file_exists(JPATH_INSTALLATION.DS.'index.php')*/) {
+
+	if (file_exists(JPATH_INSTALLATION.DS.'index.php')) {
+		header('Location: '.substr($_SERVER['REQUEST_URI'],0,strpos($_SERVER['REQUEST_URI'],'index.php')).'installation/index.php');
 		exit();
-	}
-	else
-	{
+	} else {
 		echo 'No configuration file found and no installation code available. Exiting...';
 		exit();
 	}
 }
 
-
-/*
- * Joomla! system startup.
- */
+//
+// Joomla system startup.
+//
 
 // System includes.
-require_once JPATH_LIBRARIES.DS.'joomla'.DS.'import.php';
+require_once JPATH_LIBRARIES.'/joomla/import.php';
 
 // Pre-Load configuration.
-require_once JPATH_CONFIGURATION.DS.'configuration.php';
+require_once JPATH_CONFIGURATION.'/configuration.php';
 
 // System configuration.
 $CONFIG = new JConfig();
 
 if (@$CONFIG->error_reporting === 0) {
 	error_reporting(0);
-}
-else if (@$CONFIG->error_reporting > 0)
-{
+} else if (@$CONFIG->error_reporting > 0) {
 	error_reporting($CONFIG->error_reporting);
 	ini_set('display_errors', 1);
 }
@@ -62,18 +56,20 @@ define('JDEBUG', $CONFIG->debug);
 
 unset($CONFIG);
 
-/*
- * Joomla! framework loading.
- */
+//
+// Joomla framework loading.
+//
 
 // System profiler.
-if (JDEBUG)
-{
+if (JDEBUG) {
 	jimport('joomla.error.profiler');
-	$_PROFILER = &JProfiler::getInstance('Application');
+	$_PROFILER = JProfiler::getInstance('Application');
 }
 
-// Joomla! library imports.
+//
+// Joomla library imports.
+//
+
 jimport('joomla.application.menu');
 jimport('joomla.user.user');
 jimport('joomla.environment.uri');
