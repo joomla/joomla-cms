a3877e534366e96a86b48a0e6c7cbc13f4723b31
Andrew Eddie <andrew.eddie@newlifeinit.com>
2010-05-28 23:43:29 +0000

    Fixed issue [#20494] Quick Icons Module Should Take into Account More Permissions (Omar Ramos).
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@17339 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- administrator/modules/mod_quickicon/helper.php
+++ administrator/modules/mod_quickicon/helper.php
@@ -1,21 +1,26 @@
 <?php
 /**
  * @version		$Id$
+ * @package		Joomla.Administrator
+ * @subpackage	mod_quickicon
  * @copyright	Copyright (C) 2005 - 2010 Open Source Matters, Inc. All rights reserved.
  * @license		GNU General Public License version 2 or later; see LICENSE.txt
  */
 
-// Check to ensure this file is included in Joomla!
+// No direct access.
 defined('_JEXEC') or die;
 
 /**
  * @package		Joomla.Administrator
  * @subpackage	mod_quickicon
+ * @since		1.6
  */
 abstract class QuickIconHelper
 {
 	/**
 	 * Stack to hold default buttons
+	 *
+	 * @since	1.6
 	 */
 	protected static $buttons = array();
 
@@ -23,19 +28,19 @@ abstract class QuickIconHelper
 	 * Helper method to generate a button in administrator panel
 	 *
 	 * @param	array	A named array with keys link, image, text, access and imagePath
+	 *
 	 * @return	string	HTML for button
+	 * @since	1.6
 	 */
 	public static function button($button)
 	{
-		if (!empty($button['access']))
-		{
-			if (!JFactory::getUser()->authorize($button['access'])) {
+		if (!empty($button['access'])) {
+			if (!JFactory::getUser()->authorise($button['access'][0], $button['access'][1])) {
 				return '';
 			}
 		}
 
-		if (empty($button['imagePath']))
-		{
+		if (empty($button['imagePath'])) {
 			$template = JFactory::getApplication()->getTemplate();
 			$button['imagePath'] = '/templates/'. $template .'/images/header/';
 		}
@@ -53,63 +58,72 @@ abstract class QuickIconHelper
 	 * used to add custom buttons or remove default ones.
 	 *
 	 * @return	array	An array of buttons
+	 * @since	1.6
 	 */
 	public static function &getButtons()
 	{
-		if (empty(self::$buttons))
-		{
+		if (empty(self::$buttons)) {
 			self::$buttons = array(
 				array(
 					'link' => JRoute::_('index.php?option=com_content&task=article.add'),
 					'image' => 'icon-48-article-add.png',
-					'text' => JText::_('MOD_QUICKICON_ADD_NEW_ARTICLE')
+					'text' => JText::_('MOD_QUICKICON_ADD_NEW_ARTICLE'),
+					'access' => array('core.manage', 'com_content')
 				),
 				array(
 					'link' => JRoute::_('index.php?option=com_content'),
 					'image' => 'icon-48-article.png',
-					'text' => JText::_('MOD_QUICKICON_ARTICLE_MANAGER')
+					'text' => JText::_('MOD_QUICKICON_ARTICLE_MANAGER'),
+					'access' => array('core.manage', 'com_content')
 				),
 				array(
 					'link' => JRoute::_('index.php?option=com_categories&extension=com_content'),
 					'image' => 'icon-48-category.png',
-					'text' => JText::_('MOD_QUICKICON_CATEGORY_MANAGER')
+					'text' => JText::_('MOD_QUICKICON_CATEGORY_MANAGER'),
+					'access' => array('core.manage', 'com_content')
 				),
 				array(
 					'link' => JRoute::_('index.php?option=com_media'),
 					'image' => 'icon-48-media.png',
-					'text' => JText::_('MOD_QUICKICON_MEDIA_MANAGER')
+					'text' => JText::_('MOD_QUICKICON_MEDIA_MANAGER'),
+					'access' => array('core.manage', 'com_media')
 				),
 				array(
 					'link' => JRoute::_('index.php?option=com_menus'),
 					'image' => 'icon-48-menumgr.png',
 					'text' => JText::_('MOD_QUICKICON_MENU_MANAGER'),
-					'access' => 'core.menus.manage'
+					'access' => array('core.manage', 'com_menus')
 				),
 				array(
 					'link' => JRoute::_('index.php?option=com_users'),
 					'image' => 'icon-48-user.png',
 					'text' => JText::_('MOD_QUICKICON_USER_MANAGER'),
-					'access' => 'core.users.manage'
+					'access' => array('core.manage', 'com_users')
 				),
 				array(
 					'link' => JRoute::_('index.php?option=com_modules'),
 					'image' => 'icon-48-module.png',
 					'text' => JText::_('MOD_QUICKICON_MODULE_MANAGER'),
+					'access' => array('core.manage', 'com_modules')
 				),
 				array(
 					'link' => JRoute::_('index.php?option=com_installer'),
 					'image' => 'icon-48-extension.png',
 					'text' => JText::_('MOD_QUICKICON_EXTENSION_MANAGER'),
-				),array(
+					'access' => array('core.manage', 'com_installer')
+				),
+				array(
 					'link' => JRoute::_('index.php?option=com_languages'),
 					'image' => 'icon-48-language.png',
 					'text' => JText::_('MOD_QUICKICON_LANGUAGE_MANAGER'),
-				),array(
+					'access' => array('core.manage', 'com_languages')
+				),
+				array(
 					'link' => JRoute::_('index.php?option=com_config'),
 					'image' => 'icon-48-config.png',
 					'text' => JText::_('MOD_QUICKICON_GLOBAL_CONFIGURATION'),
+					'access' => array('core.manage', 'com_config')
 				)
-
 			);
 		}
 
