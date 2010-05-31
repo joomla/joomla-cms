920a15985c3e3c98ea09b1434ca903ef26080e8b
Ian MacLennan <ian.maclennan@joomla.org>
2010-05-31 21:10:36 +0000

    Updated version information
    --HG--
    extra : convert_revision : svn%3A6f6e1ebd-4c2b-0410-823f-f34bde69bce9/development/trunk/libraries%4017430

--- joomla/version.php
+++ joomla/version.php
@@ -22,7 +22,7 @@ class JVersion
 	/** @public int Main Release Level */
 	public $RELEASE	= '1.6';
 	/** @public string Development Status */
-	public $DEV_STATUS	= 'Beta';
+	public $DEV_STATUS	= 'Beta2';
 	/** @public int Sub Release Level */
 	public $DEV_LEVEL	= '0';
 	/** @public int build Number */
@@ -30,7 +30,7 @@ class JVersion
 	/** @public string Codename */
 	public $CODENAME	= 'Hope';
 	/** @public string Date */
-	public $RELDATE	= '17-May-2010';
+	public $RELDATE	= '31-May-2010';
 	/** @public string Time */
 	public $RELTIME	= '23:00';
 	/** @public string Timezone */
