e5390e79be19b10ca512dbacca9e37097716227e
Christophe Demko <chdemko@gmail.com>
2010-06-06 17:05:32 +0000

    Fixed issue [#19986] implement JAUTHENTICATE_STATUS_CANCEL (Dietrich Streifert)
    --HG--
    extra : convert_revision : svn%3A6f6e1ebd-4c2b-0410-823f-f34bde69bce9/development/trunk/libraries%4017504

--- joomla/user/authentication.php
+++ joomla/user/authentication.php
@@ -28,6 +28,11 @@ define('JAUTHENTICATE_STATUS_CANCEL', 2);
 define('JAUTHENTICATE_STATUS_FAILURE', 4);
 
 /**
+ * This is the status code returned when the authentication chain has to be broken
+ */
+define('JAUTHENTICATE_STATUS_STOP', 8);
+
+/**
  * Authenthication class, provides an interface for the Joomla authentication system
  *
  * @package		Joomla.Framework
@@ -135,6 +140,11 @@ class JAuthentication extends JObservable
 					$response->password = $credentials['password'];
 				}
 
+				break;
+			}
+			// On cancel break the authentication chain
+			else if($response->status === JAUTHENTICATE_STATUS_STOP) 
+			{
 				break;
 			}
 		}
