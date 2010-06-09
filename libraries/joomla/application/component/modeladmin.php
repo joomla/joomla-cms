4ecfd6153fe8c8065801b112880c2a69c16eaf16
Andrew Eddie <andrew.eddie@newlifeinit.com>
2010-06-09 02:48:12 +0000

    Fixed issue [#20927] [patch] JModelAdmin Improvements (Omar Ramos).
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@17571 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- libraries/joomla/application/component/modeladmin.php
+++ libraries/joomla/application/component/modeladmin.php
@@ -60,7 +60,8 @@ abstract class JModelAdmin extends JModelForm
 	/**
 	 * Constructor.
 	 *
-	 * @param	array An optional associative array of configuration settings.
+	 * @param	array	$config	An optional associative array of configuration settings.
+	 *
 	 * @see		JController
 	 * @since	1.6
 	 */
@@ -109,7 +110,8 @@ abstract class JModelAdmin extends JModelForm
 	/**
 	 * Method to test whether a record can be deleted.
 	 *
-	 * @param	object	A record object.
+	 * @param	object	$record	A record object.
+	 *
 	 * @return	boolean	True if allowed to delete the record. Defaults to the permission set in the component.
 	 * @since	1.6
 	 */
@@ -122,7 +124,8 @@ abstract class JModelAdmin extends JModelForm
 	/**
 	 * Method to test whether a record can be deleted.
 	 *
-	 * @param	object	A record object.
+	 * @param	object	$record	A record object.
+	 *
 	 * @return	boolean	True if allowed to change the state of the record. Defaults to the permission set in the component.
 	 * @since	1.6
 	 */
@@ -135,7 +138,8 @@ abstract class JModelAdmin extends JModelForm
 	/**
 	 * Method override to check-in a record or an array of record
 	 *
-	 * @param	integer|array	The ID of the primary key or an array of IDs
+	 * @param	integer|array	$pks	The ID of the primary key or an array of IDs
+	 *
 	 * @return	boolean
 	 * @since	1.6
 	 */
@@ -145,23 +149,24 @@ abstract class JModelAdmin extends JModelForm
 		$user		= JFactory::getUser();
 		$pks		= (array) $pks;
 		$table		= $this->getTable();
+
 		if (empty($pks)) {
-			$pks=array((int) $this->getState($this->getName().'.id'));
+			$pks = array((int) $this->getState($this->getName().'.id'));
 		}
 
 		// Check in all items.
 		foreach ($pks as $i => $pk) {
+
 			if ($table->load($pk)) {
+
 				if ($table->checked_out>0) {
 					if(!parent::checkin($pk)) {
 						return false;
 					}
-				}
-				else {
+				} else {
 					unset($pks[$i]);
 				}
-			}
-			else {
+			} else {
 				$this->setError($table->getError());
 				return false;
 			}
@@ -172,7 +177,8 @@ abstract class JModelAdmin extends JModelForm
 	/**
 	 * Method override to check-out a record.
 	 *
-	 * @param	int		The ID of the primary key.
+	 * @param	int		$pk	The ID of the primary key.
+	 *
 	 * @return	boolean	True if successful, false if an error occurs.
 	 * @since	1.6
 	 */
@@ -187,7 +193,8 @@ abstract class JModelAdmin extends JModelForm
 	/**
 	 * Method to delete one or more records.
 	 *
-	 * @param	array	An array of record primary keys.
+	 * @param	array	$pks	An array of record primary keys.
+	 *
 	 * @return	boolean	True if successful, false if an error occurs.
 	 * @since	1.6
 	 */
@@ -245,7 +252,8 @@ abstract class JModelAdmin extends JModelForm
 	/**
 	 * Method to get a single record.
 	 *
-	 * @param	integer	The id of the primary key.
+	 * @param	integer	$pk	The id of the primary key.
+	 *
 	 * @return	mixed	Object on success, false on failure.
 	 * @since	1.6
 	 */
@@ -281,7 +289,8 @@ abstract class JModelAdmin extends JModelForm
 	/**
 	 * A protected method to get a set of ordering conditions.
 	 *
-	 * @param	object	A record object.
+	 * @param	object	$record	A record object.
+	 *
 	 * @return	array	An array of conditions to add to add to ordering queries.
 	 * @since	1.6
 	 */
@@ -293,6 +302,7 @@ abstract class JModelAdmin extends JModelForm
 	/**
 	 * Stock method to auto-populate the model state.
 	 *
+	 * @return	void
 	 * @since	1.6
 	 */
 	protected function populateState()
@@ -303,6 +313,7 @@ abstract class JModelAdmin extends JModelForm
 		if (!($pk = (int) $app->getUserState($this->option.'.edit.'.$this->getName().'.id'))) {
 			$pk = (int) JRequest::getInt('id');
 		}
+
 		$this->setState($this->getName().'.id', $pk);
 
 		// Load the parameters.
@@ -313,7 +324,9 @@ abstract class JModelAdmin extends JModelForm
 	/**
 	 * Prepare and sanitise the table data prior to saving.
 	 *
-	 * @param	JTable	A JTable object.
+	 * @param	JTable	$table	A JTable object.
+	 *
+	 * @return	void
 	 * @since	1.6
 	 */
 	protected function prepareTable($table)
@@ -324,8 +337,9 @@ abstract class JModelAdmin extends JModelForm
 	/**
 	 * Method to change the published state of one or more records.
 	 *
-	 * @param	array	A list of the primary keys to change.
-	 * @param	int		The value of the published state.
+	 * @param	array	$pks	A list of the primary keys to change.
+	 * @param	int		$value	The value of the published state.
+	 *
 	 * @return	boolean	True on success.
 	 * @since	1.6
 	 */
@@ -342,6 +356,8 @@ abstract class JModelAdmin extends JModelForm
 
 		// Access checks.
 		foreach ($pks as $i => $pk) {
+			$table->reset();
+
 			if ($table->load($pk)) {
 				if (!$this->canEditState($table)) {
 					// Prune items that you can't change.
@@ -361,6 +377,7 @@ abstract class JModelAdmin extends JModelForm
 
 		// Trigger the onContentChangeState event.
 		$result = $dispatcher->trigger($this->event_change_state, array($context, $pks, $value));
+
 		if (in_array(false, $result, true)) {
 			$this->setError($table->getError());
 			return false;
@@ -372,9 +389,13 @@ abstract class JModelAdmin extends JModelForm
 	/**
 	 * Method to adjust the ordering of a row.
 	 *
-	 * @param	int		The ID of the primary key to move.
-	 * @param	integer	Increment, usually +1 or -1
-	 * @return	boolean	False on failure or error, true otherwise.
+	 * Returns NULL if the user did not have edit
+	 * privileges for any of the selected primary keys.
+	 *
+	 * @param	int				$pks	The ID of the primary key to move.
+	 * @param	integer			$delta	Increment, usually +1 or -1
+	 *
+	 * @return	boolean|null	False on failure or error, true on success.
 	 * @since	1.6
 	 */
 	public function reorder($pks, $delta = 0)
@@ -385,8 +406,11 @@ abstract class JModelAdmin extends JModelForm
 		$pks	= (array) $pks;
 		$result	= true;
 
+		$allowed = true;
+
 		foreach ($pks as $i => $pk) {
 			$table->reset();
+
 			if ($table->load($pk) && $this->checkout($pk)) {
 				// Access checks.
 				if (!$this->canEditState($table)) {
@@ -394,15 +418,19 @@ abstract class JModelAdmin extends JModelForm
 					unset($pks[$i]);
 					$this->checkin($pk);
 					JError::raiseWarning(403, JText::_('JERROR_CORE_EDIT_STATE_NOT_PERMITTED'));
+					$allowed = false;
 					continue;
 				}
+
 				$where = array();
 				$where = $this->getReorderConditions($table);
+
 				if (!$table->move($delta, $where)) {
 					$this->setError($table->getError());
 					unset($pks[$i]);
 					$result = false;
 				}
+
 				$this->checkin($pk);
 			} else {
 				$this->setError($table->getError());
@@ -411,13 +439,18 @@ abstract class JModelAdmin extends JModelForm
 			}
 		}
 
+		if ($allowed === false && empty($pks)) {
+			$result = null;
+		}
+
 		return $result;
 	}
 
 	/**
 	 * Method to save the form data.
 	 *
-	 * @param	array	The form data.
+	 * @param	array	$data	The form data.
+	 *
 	 * @return	boolean	True on success.
 	 * @since	1.6
 	 */
@@ -474,6 +507,7 @@ abstract class JModelAdmin extends JModelForm
 		$dispatcher->trigger($this->event_after_save, array($this->option.'.'.$this->name, $table, $isNew));
 
 		$pkName = $table->getKeyName();
+
 		if (isset($table->$pkName)) {
 			$this->setState($this->getName().'.id', $table->$pkName);
 		}
@@ -485,8 +519,10 @@ abstract class JModelAdmin extends JModelForm
 	/**
 	 * Saves the manually set order of records.
 	 *
-	 * @param	array	An array of primary key ids.
-	 * @param	int		+/-1
+	 * @param	array	$pks	An array of primary key ids.
+	 * @param	int		$order	+/-1
+	 *
+	 * @return	mixed
 	 * @since	1.6
 	 */
 	function saveorder($pks, $order)
@@ -511,19 +547,23 @@ abstract class JModelAdmin extends JModelForm
 				JError::raiseWarning(403, JText::_('JLIB_APPLICATION_ERROR_EDIT_STATE_NOT_PERMITTED'));
 			} else if ($table->ordering != $order[$i]) {
 				$table->ordering = $order[$i];
+
 				if (!$table->store()) {
 					$this->setError($table->getError());
 					return false;
 				}
+
 				// remember to reorder within position and client_id
 				$condition = $this->getReorderConditions($table);
 				$found = false;
+
 				foreach ($conditions as $cond) {
 					if ($cond[1] == $condition) {
 						$found = true;
 						break;
 					}
 				}
+
 				if (!$found) {
 					$conditions[] = array ($table->id, $condition);
 				}
