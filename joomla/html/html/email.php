bc5a9f552b87716fe34c449b68fa74ee10652aa5
Jean-Marie Simonet <infograf768@gmail.com>
2010-06-23 01:41:58 +0000

    $ Added missing string
    --HG--
    extra : convert_revision : svn%3A6f6e1ebd-4c2b-0410-823f-f34bde69bce9/development/trunk/libraries%4017839

--- joomla/html/html/email.php
+++ joomla/html/html/email.php
@@ -72,7 +72,7 @@ abstract class JHtmlEmail
 		$replacement	.= "\n document.write('<span style=\'display: none;\'>');";
 		$replacement	.= "\n //-->";
 		$replacement	.= "\n </script>";
-		$replacement	.= JText::_('CLOAKING');
+		$replacement	.= JText::_('JLIB_HTML_CLOAKING');
 		$replacement	.= "\n <script type='text/javascript'>";
 		$replacement	.= "\n <!--";
 		$replacement	.= "\n document.write('</');";
