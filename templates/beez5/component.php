1d2517c0e32520c387f1cbca6d3130823d2ef824
Jean-Marie Simonet <infograf768@gmail.com>
2010-06-27 11:57:25 +0000

    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@17895 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- templates/beez5/component.php
+++ templates/beez5/component.php
@@ -2,7 +2,7 @@
 /**
  * @version		$Id: component.php 16465 2010-04-26 01:46:24Z eddieajau $
  * @package		Joomla.Site
- * @subpackage	tpl_beez2
+ * @subpackage	tpl_beez5
  * @copyright	Copyright (C) 2005 - 2010 Open Source Matters, Inc. All rights reserved.
  * @license		GNU General Public License version 2 or later; see LICENSE.txt
  */
@@ -16,14 +16,13 @@ $color = $this->params->get('templatecolor');
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>">
 <head>
 	<jdoc:include type="head" />
-	<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/beez/css/template.css" type="text/css" />
-	<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/beez_20/css/layout.css" type="text/css" media="screen,projection" />
-    <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/beez_20/css/print.css" type="text/css" media="Print" />
-    <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/beez_20/css/<?php echo $color; ?>.css" type="text/css" />
-
+	<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/beez5/css/template.css" type="text/css" />
+	<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/beez5css/layout.css" type="text/css" media="screen,projection" />
+	<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/beez5/css/print.css" type="text/css" media="Print" />
+	<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/beez5/css/<?php echo $color; ?>.css" type="text/css" />
 
 <?php if($this->direction == 'rtl') : ?>
-	<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/beez/css/template_rtl.css" type="text/css" />
+	<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/beez5/css/template_rtl.css" type="text/css" />
 <?php endif; ?>
 </head>
 <body class="contentpane">
