d5f4cdd8f97366e8485424bcfbb9085bb1eb8493
Ian MacLennan <ian.maclennan@joomla.org>
2010-06-28 14:17:27 +0000

    Preparation for Beta 4
    --HG--
    extra : convert_revision : svn%3A6f6e1ebd-4c2b-0410-823f-f34bde69bce9/development/trunk/libraries%4017947

--- joomla/version.php
+++ joomla/version.php
@@ -22,7 +22,7 @@ class JVersion
 	/** @public int Main Release Level */
 	public $RELEASE	= '1.6';
 	/** @public string Development Status */
-	public $DEV_STATUS	= 'Beta3';
+	public $DEV_STATUS	= 'Beta4';
 	/** @public int Sub Release Level */
 	public $DEV_LEVEL	= '0';
 	/** @public int build Number */
@@ -30,7 +30,7 @@ class JVersion
 	/** @public string Codename */
 	public $CODENAME	= 'Hope';
 	/** @public string Date */
-	public $RELDATE	= '14-June-2010';
+	public $RELDATE	= '28-June-2010';
 	/** @public string Time */
 	public $RELTIME	= '23:00';
 	/** @public string Timezone */
