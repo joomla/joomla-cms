7a160a8af3ee29c232b60d1c429a8bfea6fa1acf
Ian MacLennan <ian.maclennan@joomla.org>
2010-06-28 09:30:00 +0000

    [#21213] mod_syndicate missing class
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@17915 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- modules/mod_syndicate/tmpl/default.php
+++ modules/mod_syndicate/tmpl/default.php
@@ -10,5 +10,5 @@
 // no direct access
 defined('_JEXEC') or die;
 ?>
-<a href="<?php echo $link ?>">
+<a href="<?php echo $link ?>" class="syndicate-module<?php echo $params->get('moduleclass_sfx') ?>">
 	<?php echo JHTML::_('image','system/livemarks.png', 'feed-image', NULL, true); ?> <span><?php echo $params->get('text') ?></span></a>
