6dfd009834d79ebff6cd9b8d9178d638845ea1d9
Jean-Marie Simonet <infograf768@gmail.com>
2010-06-29 10:04:36 +0000

    # [#21233] New Category Image Window Not 100%. Thanks Bill.
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@17958 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- libraries/joomla/form/fields/media.php
+++ libraries/joomla/form/fields/media.php
@@ -101,7 +101,7 @@ class JFormFieldMedia extends JFormField
 		$html[] = '	<div class="blank">';
 		$html[] = '		<a class="modal" title="'.JText::_('JSELECT').'"' .
 					' href="'.($this->element['readonly'] ? '' : ($link ? $link : 'index.php?option=com_media&view=images&tmpl=component') . '&fieldid='.$this->id.'&folder='.$folder).'"' .
-					' rel="{handler: \'iframe\', size: {x: 650, y: 375}}">';
+					' rel="{handler: \'iframe\', size: {x: 800, y: 500}}">';
 		$html[] = '			'.JText::_('JSELECT').'</a>';
 		$html[] = '	</div>';
 		$html[] = '</div>';
