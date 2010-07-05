8d3e218a3c90bab16233221e29860f79a92f988d
Mark Dexter <dextercowley@gmail.com>
2010-07-05 11:17:47 +0000

    # [#21117] Save Order button doesn't work in Menu Manager:Menu Items
    --HG--
    extra : convert_revision : svn%3A6f6e1ebd-4c2b-0410-823f-f34bde69bce9/development/trunk/libraries%4018029

--- joomla/application/component/controlleradmin.php
+++ joomla/application/component/controlleradmin.php
@@ -227,10 +227,21 @@ class JControllerAdmin extends JController
 		$model = $this->getModel();
 
 		// Save the ordering
-		$model->saveorder($pks, $order);
+		$return = $model->saveorder($pks, $order);
 
-		$this->setMessage(JText::_('JLIB_APPLICATION_SUCCESS_ORDERING_SAVED'));
-		$this->setRedirect(JRoute::_('index.php?option='.$this->option.'&view='.$this->view_list, false));
+		if ($return === false)
+		{
+			// Reorder failed
+			$message = JText::sprintf('JLIB_APPLICATION_ERROR_REORDER_FAILED', $model->getError());
+			$this->setRedirect(JRoute::_('index.php?option='.$this->option.'&view='.$this->view_list, false), $message, 'error');
+			return false;
+		} else 
+		{
+			// Reorder succeeded.
+			$this->setMessage(JText::_('JLIB_APPLICATION_SUCCESS_ORDERING_SAVED'));
+			$this->setRedirect(JRoute::_('index.php?option='.$this->option.'&view='.$this->view_list, false));
+			return true;
+		}
 	}
 
 	/**
