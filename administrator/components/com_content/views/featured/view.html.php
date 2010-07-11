7c1bd74d129e93b88274a3cd1c6d23fb1531b480
Jean-Marie Simonet <infograf768@gmail.com>
2010-07-11 02:34:07 +0000

    # [#21366] * Checkin featured articles fails.
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@18070 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- administrator/components/com_content/views/featured/view.html.php
+++ administrator/components/com_content/views/featured/view.html.php
@@ -66,7 +66,7 @@ class ContentViewFeatured extends JView
 			}
 		}
 		if(JFactory::getUser()->authorise('core.manage','com_checkin')) {
-			JToolBarHelper::custom('featured.checkin', 'checkin.png', 'checkin_f2.png', 'JTOOLBAR_CHECKIN', true);
+			JToolBarHelper::custom('articles.checkin', 'checkin.png', 'checkin_f2.png', 'JTOOLBAR_CHECKIN', true);
 		}
 		if ($canDo->get('core.edit.state')) {
 			JToolBarHelper::custom('featured.delete','remove.png','remove_f2.png','JTOOLBAR_REMOVE', true);
