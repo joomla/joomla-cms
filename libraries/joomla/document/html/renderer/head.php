6b851d58bfc277e77bfb93ececbad19286207ccf
Jean-Marie Simonet <infograf768@gmail.com>
2010-07-14 01:31:04 +0000

    # [#21342] JDocumentRendererHead can generate empty meta description. Thanks Janich.
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@18123 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- libraries/joomla/document/html/renderer/head.php
+++ libraries/joomla/document/html/renderer/head.php
@@ -67,8 +67,13 @@ class JDocumentRendererHead extends JDocumentRenderer
 				}
 			}
 		}
-
-		$buffer .= $tab.'<meta name="description" content="'.$document->getDescription().'" />'.$lnEnd;
+		
+		// dont add empty descriptions
+		$documentDescription = $document->getDescription();
+		if ($documentDescription) {
+			$buffer .= $tab.'<meta name="description" content="'.$documentDescription.'" />'.$lnEnd;
+		}
+		
 		$buffer .= $tab.'<meta name="generator" content="'.$document->getGenerator().'" />'.$lnEnd;
 		$buffer .= $tab.'<title>'.htmlspecialchars($document->getTitle(), ENT_COMPAT, 'UTF-8').'</title>'.$lnEnd;
 
