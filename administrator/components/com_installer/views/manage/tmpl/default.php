fc09ed216c3addfa5fade7805221d6827fce9568
Andrew Eddie <andrew.eddie@newlifeinit.com>
2010-07-20 23:02:31 +0000

    Fixed issue [#21349] Manage view of com_installer does not use the default_item.php sub-template (Rune Sjøen).
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@18195 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- administrator/components/com_installer/views/manage/tmpl/default.php
+++ administrator/components/com_installer/views/manage/tmpl/default.php
@@ -29,25 +29,58 @@ $listDirn	= $this->state->get('list.direction');
 	<table class="adminlist">
 		<thead>
 			<tr>
-				<th width="20"><input type="checkbox" name="checkall-toggle" value="" onclick="checkAll(this)" /></th>
-				<th class="nowrap"><?php echo JHTML::_('grid.sort', 'COM_INSTALLER_HEADING_NAME', 'name', $listDirn, $listOrder); ?></th>
-				<th ><?php echo JHTML::_('grid.sort', 'COM_INSTALLER_HEADING_TYPE', 'type', $listDirn, $listOrder); ?></th>
-				<th width="10%" class="center"><?php echo JHTML::_('grid.sort', 'JENABLED', 'enabled', $listDirn, $listOrder); ?></th>
-				<th width="10%" class="center"><?php echo JText::_('JVERSION'); ?></th>
-				<th width="10%"><?php echo JText::_('JDATE'); ?></th>
-				<th><?php echo JHTML::_('grid.sort', 'COM_INSTALLER_HEADING_FOLDER', 'folder', $listDirn, $listOrder); ?></th>
-				<th><?php echo JHTML::_('grid.sort', 'COM_INSTALLER_HEADING_LOCATION', 'client_id', $listDirn, $listOrder); ?></th>
-				<th width="15%"><?php echo JText::_('JAUTHOR'); ?></th>
-				<th width="10"><?php echo JHTML::_('grid.sort', 'COM_INSTALLER_HEADING_ID', 'extension_id', $listDirn, $listOrder); ?></th>
+				<th width="20">
+					<input type="checkbox" name="checkall-toggle" value="" onclick="checkAll(this)" />
+				</th>
+				<th class="nowrap">
+					<?php echo JHTML::_('grid.sort', 'COM_INSTALLER_HEADING_NAME', 'name', $listDirn, $listOrder); ?>
+				</th>
+				<th>
+					<?php echo JHTML::_('grid.sort', 'COM_INSTALLER_HEADING_TYPE', 'type', $listDirn, $listOrder); ?>
+				</th>
+				<th width="10%" class="center">
+					<?php echo JHTML::_('grid.sort', 'JENABLED', 'enabled', $listDirn, $listOrder); ?>
+				</th>
+				<th width="10%" class="center">
+					<?php echo JText::_('JVERSION'); ?>
+				</th>
+				<th width="10%">
+					<?php echo JText::_('JDATE'); ?>
+				</th>
+				<th>
+					<?php echo JHTML::_('grid.sort', 'COM_INSTALLER_HEADING_FOLDER', 'folder', $listDirn, $listOrder); ?>
+				</th>
+				<th>
+					<?php echo JHTML::_('grid.sort', 'COM_INSTALLER_HEADING_LOCATION', 'client_id', $listDirn, $listOrder); ?>
+				</th>
+				<th width="15%">
+					<?php echo JText::_('JAUTHOR'); ?>
+				</th>
+				<th width="10">
+					<?php echo JHTML::_('grid.sort', 'COM_INSTALLER_HEADING_ID', 'extension_id', $listDirn, $listOrder); ?>
+				</th>
 			</tr>
 		</thead>
-		<tfoot><tr><td colspan="11"><?php echo $this->pagination->getListFooter(); ?></td></tr></tfoot>
+		<tfoot>
+			<tr>
+				<td colspan="11">
+					<?php echo $this->pagination->getListFooter(); ?>
+				</td>
+			</tr>
+		</tfoot>
 		<tbody>
 		<?php foreach ($this->items as $i => $item): ?>
 			<tr class="row<?php echo $i%2;?>" style="<?php if ($item->protected) echo 'color:#999999;';?>">
-				<td><?php echo JHtml::_('grid.id', $i, $item->extension_id); ?></td>
-				<td><span class="bold hasTip" title="<?php echo htmlspecialchars($item->name.'::'.$item->description); ?>"><?php echo $item->name; ?></span></td>
-				<td class="center"><?php echo JText::_('COM_INSTALLER_TYPE_' . $item->type); ?></td>
+				<td>
+					<?php echo JHtml::_('grid.id', $i, $item->extension_id); ?>
+				</td>
+				<td>
+					<span class="bold hasTip" title="<?php echo htmlspecialchars($item->name.'::'.$item->description); ?>">
+						<?php echo $item->name; ?></span>
+				</td>
+				<td class="center">
+					<?php echo JText::_('COM_INSTALLER_TYPE_' . $item->type); ?>
+				</td>
 				<td class="center">
 					<?php if (!$item->element) : ?>
 					<strong>X</strong>
@@ -55,27 +88,34 @@ $listDirn	= $this->state->get('list.direction');
 						<?php echo JHtml::_('jgrid.published', $item->enabled, $i, 'manage.', !$item->protected);?>
 					<?php endif; ?>
 				</td>
-				<td class="center"><?php echo @$item->version != '' ? $item->version : '&#160;'; ?></td>
-				<td class="center"><?php echo @$item->creationDate != '' ? $item->creationDate : '&#160;'; ?></td>
-				<td class="center"><?php echo @$item->folder != '' ? $item->folder : JText::_('COM_INSTALLER_TYPE_NONAPPLICABLE'); ?></td>
-				<td class="center"><?php echo $item->client; ?></td>
+				<td class="center">
+					<?php echo @$item->version != '' ? $item->version : '&#160;'; ?>
+				</td>
+				<td class="center">
+					<?php echo @$item->creationDate != '' ? $item->creationDate : '&#160;'; ?>
+				</td>
+				<td class="center">
+					<?php echo @$item->folder != '' ? $item->folder : JText::_('COM_INSTALLER_TYPE_NONAPPLICABLE'); ?>
+				</td>
+				<td class="center">
+					<?php echo $item->client; ?>
+				</td>
 				<td class="center">
 					<span class="editlinktip hasTip" title="<?php echo addslashes(htmlspecialchars(JText::_('COM_INSTALLER_AUTHOR_INFORMATION').'::'.$item->author_info)); ?>">
-						<?php echo @$item->author != '' ? $item->author : '&#160;'; ?>
-					</span>
+						<?php echo @$item->author != '' ? $item->author : '&#160;'; ?></span>
 				</td>
-				<td><?php echo $item->extension_id ?></td>
+				<td>
+					<?php echo $item->extension_id ?>
+				</td>
 			</tr>
 		<?php endforeach; ?>
 		</tbody>
 	</table>
 	<?php endif; ?>
 
-	<div>
-		<input type="hidden" name="task" value="" />
-		<input type="hidden" name="boxchecked" value="0" />
-		<input type="hidden" name="filter_order" value="<?php echo $listOrder; ?>" />
-		<input type="hidden" name="filter_order_Dir" value="<?php echo $listDirn; ?>" />
-		<?php echo JHTML::_('form.token'); ?>
-	</div>
+	<input type="hidden" name="task" value="" />
+	<input type="hidden" name="boxchecked" value="0" />
+	<input type="hidden" name="filter_order" value="<?php echo $listOrder; ?>" />
+	<input type="hidden" name="filter_order_Dir" value="<?php echo $listDirn; ?>" />
+	<?php echo JHTML::_('form.token'); ?>
 </form>
