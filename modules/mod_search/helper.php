39df70f50e8169821a4a075a7be1a517a8d2c5f5
Andrew Eddie <andrew.eddie@newlifeinit.com>
2010-07-22 06:26:25 +0000

    Fixed issue [#21517] modSearchHelper's methods should be public static (Franklin Tse).
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@18213 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- modules/mod_search/helper.php
+++ modules/mod_search/helper.php
@@ -10,9 +10,23 @@
 // no direct access
 defined('_JEXEC') or die;
 
+/**
+ * @package		Joomla.Site
+ * @subpackage	mod_search
+ * @since		1.5
+ */
 class modSearchHelper
 {
-	function getSearchImage($button_text) {
+	/**
+	 * Display the search button as an image.
+	 *
+	 * @param	string	$button_text	The alt text for the button.
+	 *
+	 * @return	string	The HTML for the image.
+	 * @since	1.5
+	 */
+	public static function getSearchImage($button_text)
+	{
 		$img = JHTML::_('image','searchButton.gif', $button_text, NULL, true, true);
 		return $img;
 	}
