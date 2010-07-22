a9acf1f1a184f0c78fdaf65719e53b1b198f834e
Andrew Eddie <andrew.eddie@newlifeinit.com>
2010-07-22 04:16:13 +0000

    Fixed issue [#21178] Dropdown on Extension manager>Manage to filter results in error - ? due to ftp fields attempting to be populated (Ruth Cheesley, Bill Richardson).
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@18207 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- administrator/components/com_installer/views/default/tmpl/default_ftp.php
+++ administrator/components/com_installer/views/default/tmpl/default_ftp.php
@@ -11,13 +11,13 @@
 // no direct access
 defined('_JEXEC') or die;
 ?>
-<fieldset title="<?php echo JText::_('INSTALLER_MSG_DESCFTPTITLE'); ?>">
+<fieldset title="<?php echo JText::_('COM_INSTALLER_MSG_DESCFTPTITLE'); ?>">
 	<legend><?php echo JText::_('COM_INSTALLER_MSG_DESCFTPTITLE'); ?></legend>
 
 	<?php echo JText::_('COM_INSTALLER_MSG_DESCFTP'); ?>
 
 	<?php if (JError::isError($this->ftp)): ?>
-		<p><?php echo JText::_($this->ftp->message); ?></p>
+		<p><?php echo JText::_($this->ftp->getMessage()); ?></p>
 	<?php endif; ?>
 
 	<table class="adminform">
