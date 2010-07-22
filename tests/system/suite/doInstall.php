36752961ed40f4886aa4d1c82f522ed5e7b39ea7
Andrew Eddie <andrew.eddie@newlifeinit.com>
2010-07-22 04:12:18 +0000

    Fixed issue [#21250] Token message Error on Install Sample Data (Pete Nurse).
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@18206 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- tests/system/suite/doInstall.php
+++ tests/system/suite/doInstall.php
@@ -11,76 +11,94 @@ require_once 'SeleniumJoomlaTestCase.php';
 
 class DoInstall extends SeleniumJoomlaTestCase
 {
-  function testDoInstall()
-  {
-  	$this->setUp();
-  	$cfg = $this->cfg;
-   	$configFile = "../../configuration.php";
-  	if (file_exists($configFile)) {
-  		echo "Delete configuration file\n";
-  		unlink($configFile);
-  	}
-  	else {
-  		echo "No configuration file found\n";
-  	}
-  	echo("Starting Installation\n");
-	echo "Page through screen 1\n";
-    $this->open($cfg->path ."/installation/index.php");
-    $this->click("link=Next");
-    $this->waitForPageToLoad("30000");
-    echo "Page through screen 2\n";
-    $this->click("link=Next");
-    $this->waitForPageToLoad("30000");
-    echo "Page through screen 3\n";
-    $this->click("link=Next");
-    $this->waitForPageToLoad("30000");
-    echo "Enter database information\n";
-    $this->type("jform_db_host", $cfg->db_host);
-    $this->type("jform_db_user", $cfg->db_user);
-    $this->type("jform_db_pass", $cfg->db_pass);
-    $this->type("jform_db_name", $cfg->db_name);
-    $this->click("jform_db_old0");
-    $this->click("link=Next");
-    $this->waitForPageToLoad("30000");
-    echo "Enter site information\n";
-    $this->click("link=Next");
-    $this->waitForPageToLoad("30000");
-    $this->type("jform_site_name", $cfg->site_name);
-    $this->type("jform_admin_user", $cfg->username);
-    $this->type("jform_admin_email", $cfg->admin_email);
-    $this->type("jform_admin_password", $cfg->password);
-    $this->type("jform_admin_password2", $cfg->password);
-    echo "Install sample data and wait for success message\n";
-    $this->click("instDefault");
-    // wait up to 15 seconds for success message on sample data
-    for ($second = 0; ; $second++) {
-        if ($second >= 15) $this->fail("timeout");
-        try {
-            if (stripos($this->getValue("instDefault"),'SUCCESS')) break;
-        } catch (Exception $e) {}
-        sleep(1);
-    }
+	function testDoInstall()
+	{
+	  	$this->setUp();
+	  	$cfg = $this->cfg;
+	   	$configFile = "../../configuration.php";
 
-    echo "Finish installation\n";
-    $this->click("link=Next");
-    $this->waitForPageToLoad("30000");
-	$this->assertTrue(true);
-	echo "Login to back end\n";
-	$this->gotoAdmin();
-	$this->doAdminLogin();
-	echo "Check for site menu\n";
-	$this->assertEquals("Site", $this->getText("link=Site"));
-	echo "Change error level to maximum\n";
-	$this->jClick('Global Configuration');
-    $this->click("server");
-    $this->select("jform_error_reporting", "label=Maximum");
-    echo "Turn caching off\n";
-    $this->click("system");
-    $this->select("jform_caching", "label=OFF - Caching disabled");
-    $this->click("//li[@id='toolbar-save']/a/span");
-    $this->waitForPageToLoad("30000");
-	$this->doAdminLogout();
+	  	if (file_exists($configFile)) {
+	  		echo "Delete configuration file\n";
+	  		unlink($configFile);
+	  	}
+	  	else {
+	  		echo "No configuration file found\n";
+	  	}
 
-  }
+	  	echo("Starting Installation\n");
+		echo "Page through screen 1\n";
+		$this->open($cfg->path ."/installation/index.php");
+		$this->click("link=Next");
+		$this->waitForPageToLoad("30000");
+
+		echo "Page through screen 2\n";
+		$this->click("link=Next");
+		$this->waitForPageToLoad("30000");
+
+		echo "Page through screen 3\n";
+		$this->click("link=Next");
+		$this->waitForPageToLoad("30000");
+
+		echo "Enter database information\n";
+		$this->type("jform_db_host", $cfg->db_host);
+		$this->type("jform_db_user", $cfg->db_user);
+		$this->type("jform_db_pass", $cfg->db_pass);
+		$this->type("jform_db_name", $cfg->db_name);
+		$this->click("jform_db_old0");
+		$this->click("link=Next");
+		$this->waitForPageToLoad("30000");
+
+		echo "Enter site information\n";
+		$this->click("link=Next");
+		$this->waitForPageToLoad("30000");
+		$this->type("jform_site_name", $cfg->site_name);
+		$this->type("jform_admin_user", $cfg->username);
+		$this->type("jform_admin_email", $cfg->admin_email);
+		$this->type("jform_admin_password", $cfg->password);
+		$this->type("jform_admin_password2", $cfg->password);
+
+		echo "Install sample data and wait for success message\n";
+		$this->click("instDefault");
+
+		// wait up to 30 seconds for success message on sample data
+		for ($second = 0; ; $second++) {
+			if ($second >= 30) {
+				$this->fail('timeout');
+			}
+
+			try {
+				if (stripos($this->getValue("instDefault"),'SUCCESS')) {
+					break;
+				}
+			}
+			catch (Exception $e) {
+			}
+
+			sleep(1);
+		}
+
+		echo "Finish installation\n";
+		$this->click("link=Next");
+		$this->waitForPageToLoad("30000");
+		$this->assertTrue(true);
+
+		echo "Login to back end\n";
+		$this->gotoAdmin();
+		$this->doAdminLogin();
+
+		echo "Check for site menu\n";
+		$this->assertEquals("Site", $this->getText("link=Site"));
+
+		echo "Change error level to maximum\n";
+		$this->jClick('Global Configuration');
+		$this->click("server");
+		$this->select("jform_error_reporting", "label=Maximum");
+
+		echo "Turn caching off\n";
+		$this->click("system");
+		$this->select("jform_caching", "label=OFF - Caching disabled");
+		$this->click("//li[@id='toolbar-save']/a/span");
+		$this->waitForPageToLoad("30000");
+		$this->doAdminLogout();
+	}
 }
-?>
