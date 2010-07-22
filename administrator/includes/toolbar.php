07fce61f712ac96d93ec13c4a9d8941057470481
Jean-Marie Simonet <infograf768@gmail.com>
2010-07-22 16:31:44 +0000

    # [#21550] Flash Uploader issue
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@18217 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- administrator/includes/toolbar.php
+++ administrator/includes/toolbar.php
@@ -496,7 +496,8 @@ abstract class JToolBarHelper
 		$left = 0;
 		$bar = JToolBar::getInstance('toolbar');
 		// Add a configuration button.
-		$bar->appendButton('Popup', 'options', $alt, 'index.php?option=com_config&amp;view=component&amp;component='.$component.'&amp;path='.$path.'&amp;tmpl=component', $width, $height);
+		$bar->appendButton('Popup', 'options', $alt, 'index.php?option=com_config&amp;view=component&amp;component='.$component.'&amp;path='.$path.'&amp;tmpl=component', $width, $height, $top, $left, $onClose);
+		
 	}
 }
 
