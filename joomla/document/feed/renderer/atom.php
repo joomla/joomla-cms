da4dda3ce8fe246088fc3ee3cab41dd88c85eff8
Jean-Marie Simonet <infograf768@gmail.com>
2010-07-25 15:22:04 +0000

    # [#21524] Atom feed renderer reports wrong Joomla! version. Thanks Franklin.
    --HG--
    extra : convert_revision : svn%3A6f6e1ebd-4c2b-0410-823f-f34bde69bce9/development/trunk/libraries%4018244

--- joomla/document/feed/renderer/atom.php
+++ joomla/document/feed/renderer/atom.php
@@ -68,7 +68,7 @@ defined('JPATH_BASE') or die;
 			}
 			$feed.= "	</author>\n";
 		}
-		$feed.= "	<generator uri=\"http://joomla.org\" version=\"1.5\">".$data->getGenerator()."</generator>\n";
+		$feed.= "	<generator uri=\"http://joomla.org\" version=\"1.6\">".$data->getGenerator()."</generator>\n";
 		$feed.= '<link rel="self" type="application/atom+xml" href="'.str_replace(' ','%20',$url.$syndicationURL). "\" />\n";
 
 		for ($i=0;$i<count($data->items);$i++)
