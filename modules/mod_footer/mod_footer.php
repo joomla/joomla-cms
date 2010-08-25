a0c79acd8df39bb6eba93198c8148bc94497639e
Andrew Eddie <andrew.eddie@newlifeinit.com>
2010-08-25 04:46:03 +0000

    Fixed issue [#21976] deprecated function ereg_replace mod_footer (Ronald Pijpers, Bill Richardson).
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@18629 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- modules/mod_footer/mod_footer.php
+++ modules/mod_footer/mod_footer.php
@@ -16,14 +16,14 @@ $cur_year	= $date->format('Y');
 $csite_name	= $app->getCfg('sitename');
 
 if (JString::strpos(JText :: _('MOD_FOOTER_LINE1'), '%date%')) {
-	$line1 = ereg_replace('%date%', $cur_year, JText :: _('MOD_FOOTER_LINE1'));
+	$line1 = str_replace('%date%', $cur_year, JText :: _('MOD_FOOTER_LINE1'));
 }
 else {
 	$line1 = JText :: _('MOD_FOOTER_LINE1');
 }
 
 if (JString::strpos($line1, '%sitename%')) {
-	$lineone = ereg_replace('%sitename%', $csite_name, $line1);
+	$lineone = str_replace('%sitename%', $csite_name, $line1);
 }
 else {
 	$lineone = $line1;
