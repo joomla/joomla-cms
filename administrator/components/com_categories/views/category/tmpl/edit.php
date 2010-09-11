0c16744e64bd7d8f232d371c8506f8933bc80209
Ian MacLennan <ian.maclennan@joomla.org>
2010-09-11 02:15:00 +0000

    # [#22174] Inconsistent behaviour of Article and Category Manager with regard to Permissions table
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@18840 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- administrator/components/com_categories/views/category/tmpl/edit.php
+++ administrator/components/com_categories/views/category/tmpl/edit.php
@@ -10,14 +10,13 @@
 // no direct access
 defined('_JEXEC') or die;
 
-defined('_JEXEC') or die;
-
 // Include the component HTML helpers.
 JHtml::addIncludePath(JPATH_COMPONENT.'/helpers/html');
 
 // Load the tooltip behavior.
 JHtml::_('behavior.tooltip');
 JHtml::_('behavior.formvalidation');
+JHtml::_('behavior.keepalive');
 ?>
 
 <script type="text/javascript">
@@ -71,7 +70,7 @@ JHtml::_('behavior.formvalidation');
 
 	<div class="width-40 fltrt">
 
-		<?php echo JHtml::_('sliders.start','plugin-sliders-'.$this->item->id); ?>
+		<?php echo JHtml::_('sliders.start','plugin-sliders-'.$this->item->id, array('useCookie'=>1)); ?>
 			<?php echo $this->loadTemplate('options'); ?>
 			<div class="clr"></div>
 
--- administrator/templates/hathor/html/com_categories/category/edit.php
+++ administrator/templates/hathor/html/com_categories/category/edit.php
@@ -11,14 +11,13 @@
 // no direct access
 defined('_JEXEC') or die;
 
-defined('_JEXEC') or die;
-
 // Include the component HTML helpers.
 JHtml::addIncludePath(JPATH_COMPONENT.'/helpers/html');
 
 // Load the tooltip behavior.
 JHtml::_('behavior.tooltip');
 JHtml::_('behavior.formvalidation');
+JHtml::_('behavior.keepalive');
 ?>
 
 <script type="text/javascript">
@@ -75,7 +74,7 @@ JHtml::_('behavior.formvalidation');
 
 	<div class="col options-section">
 
-		<?php echo JHtml::_('sliders.start','plugin-sliders-'.$this->item->id); ?>
+		<?php echo JHtml::_('sliders.start','plugin-sliders-'.$this->item->id, array('useCookie'=>1)); ?>
 			<?php echo $this->loadTemplate('options'); ?>
 			<div class="clr"></div>
 
