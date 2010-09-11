24c47846dc2eb138048da9acc6c1946149dde5a0
Ian MacLennan <ian.maclennan@joomla.org>
2010-09-11 03:28:33 +0000

    Fixed indentation
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@18842 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- plugins/system/sef/sef.php
+++ plugins/system/sef/sef.php
@@ -34,10 +34,10 @@ class plgSystemSef extends JPlugin
 		$base	= JURI::base(true).'/';
 		$buffer = JResponse::getBody();
 
-			$regex  = '#href="index.php\?([^"]*)#m';
+		$regex  = '#href="index.php\?([^"]*)#m';
 		$buffer = preg_replace_callback($regex, array('plgSystemSEF', 'route'), $buffer);
 
-			$protocols	= '[a-zA-Z0-9]+:'; //To check for all unknown protocals (a protocol must contain at least one alpahnumeric fillowed by :
+		$protocols	= '[a-zA-Z0-9]+:'; //To check for all unknown protocals (a protocol must contain at least one alpahnumeric fillowed by :
 		$regex		= '#(src|href)="(?!/|'.$protocols.'|\#|\')([^"]*)"#m';
 		$buffer		= preg_replace($regex, "$1=\"$base\$2\"", $buffer);
 		$regex		= '#(onclick="window.open\(\')(?!/|'.$protocols.'|\#)([^/]+[^\']*?\')#m';
