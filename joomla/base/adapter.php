d143f8ddd1060c1df4a1e98ba91be12cfc66ce1e
Andrew Eddie <andrew.eddie@newlifeinit.com>
2010-09-13 06:52:14 +0000

    Code formatting.
    --HG--
    extra : convert_revision : svn%3A6f6e1ebd-4c2b-0410-823f-f34bde69bce9/development/trunk/libraries%4018864

--- joomla/base/adapter.php
+++ joomla/base/adapter.php
@@ -19,56 +19,62 @@ defined('JPATH_BASE') or die;
  * @subpackage	Base
  * @since		1.6
  */
-class JAdapter extends JObject {
+class JAdapter extends JObject
+{
 	/**
-	 * Associative array of adapters
-	 * @var array
+	 * @var		array	Associative array of adapters
+	 * @since	1.6
 	 */
 	protected $_adapters = array();
 
 	/**
-	 * Adapter Folder
-	 * @var string
+	 * @var		string	Adapter Folder
+	 * @since	1.6
 	 */
 	protected $_adapterfolder = 'adapters';
 
 	/**
-	 * Adapter Class Prefix
-	 * @var string
+	 * @var		string	Adapter Class Prefix
+	 * @since	1.6
 	 */
 	protected $_classprefix = 'J';
 
 	/**
-	 * Base Path for the adapter instance
-	 * @var string
+	 * @var		string	Base Path for the adapter instance
+	 * @since	1.6
 	 */
 	protected $_basepath = null;
 
 	/**
-	 * Database Connector Object
-	 * @var object
+	 * @var		object	Database Connector Object
+	 * @since	1.6
 	 */
 	protected $_db;
 
 	/**
 	 * Constructor
-	 * @param string Base Path of the adapters
-	 * @param string Class prefix of adapters
-	 * @param string Name of folder to append to base path
+	 *
+	 * @param	string	$basepath		Base Path of the adapters
+	 * @param	string	$classprefix	Class prefix of adapters
+	 * @param	string	$adapterfolder	Name of folder to append to base path
+	 *
+	 * @return	JAdapter
+	 * @since	1.6
 	 */
-	public function __construct($basepath, $classprefix=null, $adapterfolder=null) {
-		$this->_basepath = $basepath;
-		$this->_classprefix = $classprefix ? $classprefix : 'J';
-		$this->_adapterfolder = $adapterfolder ? $adapterfolder : 'adapters';
+	public function __construct($basepath, $classprefix = null, $adapterfolder = null)
+	{
+		$this->_basepath		= $basepath;
+		$this->_classprefix		= $classprefix ? $classprefix : 'J';
+		$this->_adapterfolder	= $adapterfolder ? $adapterfolder : 'adapters';
+
 		$this->_db = JFactory::getDBO();
 	}
 
 	/**
 	 * Get the database connector object
 	 *
-	 * @access	public
 	 * @return	object	Database connector object
-	 * @since	1.5
+	 * @since	1.6
 	 */
 	public function getDBO()
 	{
@@ -78,64 +84,85 @@ class JAdapter extends JObject {
 	/**
 	 * Set an adapter by name
 	 *
-	 * @access	public
 	 * @param	string	$name		Adapter name
 	 * @param	object	$adapter	Adapter object
 	 * @param	array	$options	Adapter options
+	 *
 	 * @return	boolean True if successful
-	 * @since	1.5
+	 * @since	1.6
 	 */
 	public function setAdapter($name, &$adapter = null, $options = Array())
 	{
-		if (!is_object($adapter))
-		{
+		if (!is_object($adapter)) {
 			$fullpath = $this->_basepath.DS.$this->_adapterfolder.DS.strtolower($name).'.php';
-			if(!file_exists($fullpath)) {
+
+			if (!file_exists($fullpath)) {
 				return false;
 			}
+
 			// Try to load the adapter object
 			require_once $fullpath;
+
 			$class = $this->_classprefix.ucfirst($name);
 			if (!class_exists($class)) {
 				return false;
 			}
+
 			$adapter = new $class($this, $this->_db, $options);
 		}
+
 		$this->_adapters[$name] = &$adapter;
+
 		return true;
 	}
 
 	/**
-	 * Return an adapter
-	 * @param 	string 	name of adapter to return
-	 * @param 	Array 	$options Adapter options
+	 * Return an adapter.
+	 *
+	 * @param 	string 	$name		Name of adapter to return
+	 * @param 	array 	$options	Adapter options
+	 *
 	 * @return 	object 	Adapter of type 'name' or false
+	 * @since	1.6
 	 */
-	public function getAdapter($name, $options = Array()) {
-		if(!array_key_exists($name, $this->_adapters)) {
-			if(!$this->setAdapter($name, $options)) {
+	public function getAdapter($name, $options = Array())
+	{
+		if (!array_key_exists($name, $this->_adapters)) {
+			if (!$this->setAdapter($name, $options)) {
 				$false = false;
+
 				return $false;
 			}
 		}
+
 		return $this->_adapters[$name];
 	}
 
 	/**
-	 * Loads all adapters
-	 * @params array	$options	Adapter options
+	 * Loads all adapters.
+	 *
+	 * @params	array	$options	Adapter options
+	 *
+	 * @return	void
+	 * @since	1.6
 	 */
-	public function loadAllAdapters($options = Array()) {
+	public function loadAllAdapters($options = array())
+	{
 		$list = JFolder::files($this->_basepath.DS.$this->_adapterfolder);
-		foreach($list as $filename) {
-			if(JFile::getExt($filename) == 'php') {
+
+		foreach ($list as $filename)
+		{
+			if (JFile::getExt($filename) == 'php') {
 				// Try to load the adapter object
 				require_once $this->_basepath.DS.$this->_adapterfolder.DS.$filename;
+
 				$name = JFile::stripExt($filename);
 				$class = $this->_classprefix.ucfirst($name);
+
 				if (!class_exists($class)) {
 					continue; // skip to next one
 				}
+
 				$adapter = new $class($this, $this->_db, $options);
 				$this->_adapters[$name] = clone $adapter;
 			}
