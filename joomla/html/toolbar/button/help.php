c140fae690b60be1340bda4619e8808708e1d614
Christophe Demko <chdemko@gmail.com>
2010-10-05 21:32:38 +0000

    [#22710] com_categories component does not manage well help url
    --HG--
    extra : convert_revision : svn%3A6f6e1ebd-4c2b-0410-823f-f34bde69bce9/development/trunk/libraries%4019053

--- joomla/html/toolbar/button/help.php
+++ joomla/html/toolbar/button/help.php
@@ -29,15 +29,16 @@ class JButtonHelp extends JButton
 	 * @param	string	$ref		The name of the help screen (its key reference).
 	 * @param	boolean	$com		Use the help file in the component directory.
 	 * @param	string	$override	Use this URL instead of any other.
+	 * @param	string	$component	Name of component to get Help (null for current component)
 	 *
 	 * @return	string
 	 * @since	1.5
 	 */
-	public function fetchButton($type = 'Help', $ref = '', $com = false, $override = null)
+	public function fetchButton($type = 'Help', $ref = '', $com = false, $override = null, $component = null)
 	{
 		$text	= JText::_('JTOOLBAR_HELP');
 		$class	= $this->fetchIconClass('help');
-		$doTask	= $this->_getCommand($ref, $com, $override);
+		$doTask	= $this->_getCommand($ref, $com, $override, $component);
 
 		$html = "<a href=\"#\" onclick=\"$doTask\" rel=\"help\" class=\"toolbar\">\n";
 		$html .= "<span class=\"$class\">\n";
@@ -69,15 +70,16 @@ class JButtonHelp extends JButton
 	 * @param	string	$ref		The name of the help screen (its key reference).
 	 * @param	boolean	$com		Use the help file in the component directory.
 	 * @param	string	$override	Use this URL instead of any other.
+	 * @param	string	$component	Name of component to get Help (null for current component)
 	 *
 	 * @return	string	JavaScript command string
 	 * @since	1.5
 	 */
-	protected function _getCommand($ref, $com, $override)
+	protected function _getCommand($ref, $com, $override, $component)
 	{
 		// Get Help URL
 		jimport('joomla.language.help');
-		$url = JHelp::createURL($ref, $com, $override);
+		$url = JHelp::createURL($ref, $com, $override, $component);
 		$url = htmlspecialchars($url, ENT_QUOTES);
 		$cmd = "popupWindow('$url', '".JText::_('JHELP', true)."', 700, 500, 1)";
 
