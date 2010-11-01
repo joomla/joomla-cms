a50283e97afc9119256b03b317813374b222ed28
Christophe Demko <chdemko@gmail.com>
2010-11-01 00:54:46 +0000

    [#21446] Use local storage for the font size changer in the Beez templates
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@19307 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- templates/beez5/javascript/md_stylechanger.js
+++ templates/beez5/javascript/md_stylechanger.js
@@ -2,6 +2,10 @@ var prefsLoaded = false;
 var defaultFontSize =100;
 var currentFontSize = defaultFontSize;
 
+function supportsLocalStorage() {
+	return ('localStorage' in window) && window['localStorage'] !== null;
+};
+
 if (document.addEventListener) {
 	document.addEventListener("DOMContentLoaded", onLoadEvents, false)
 } else {
@@ -16,67 +20,69 @@ function onLoadEvents() {
 };
 
 function revertStyles() {
-        currentFontSize = defaultFontSize;
-        changeFontSize(0);
+	currentFontSize = defaultFontSize;
+	changeFontSize(0);
 };
 
 function toggleColors() {
-        if(currentStyle == "White"){
-                setColor("Black");
-        }else{
-                setColor("White");
-        }
+	if (currentStyle == "White") {
+		setColor("Black");
+	} else {
+		setColor("White");
+	}
 };
 
 function changeFontSize(sizeDifference) {
-        currentFontSize = parseInt(currentFontSize) + parseInt(sizeDifference * 5);
-
-        if(currentFontSize > 180){
-                currentFontSize = 180;
-        }else if(currentFontSize < 60){
-                currentFontSize = 60;
-        }
-
-        setFontSize(currentFontSize);
-
+	currentFontSize = parseInt(currentFontSize) + parseInt(sizeDifference * 5);
+	if (currentFontSize > 180) {
+		currentFontSize = 180;
+	} else if (currentFontSize < 60) {
+		currentFontSize = 60;
+	}
+	setFontSize(currentFontSize);
 };
 
 function setFontSize(fontSize) {
-        var stObj = (document.getElementById) ? document.getElementById('content_area') : document.all('content_area');
-        document.body.style.fontSize = fontSize + '%';
-        $('header').style.fontSize=fontSize + '%';
+	document.body.style.fontSize = fontSize + '%';
+	document.head.style.fontSize = fontSize + '%';
 };
 
 
-function createCookie(name,value,days) {
-  if (days) {
-    var date = new Date();
-    date.setTime(date.getTime()+(days*24*60*60*1000));
-    var expires = "; expires="+date.toGMTString();
-  }
-  else expires = "";
-  document.cookie = name+"="+value+expires+"; path=/";
+function writeFontSize(value) {
+	if (supportsLocalStorage()) {
+		localStorage["fontSize"] = value;
+	} else {
+		var date = new Date();
+		date.setTime(date.getTime()+(365*24*60*60*1000));
+		var expires = "; expires="+date.toGMTString();
+		document.cookie = "fontSize="+value+expires+"; path=/";
+	}
 };
 
-function readCookie(name) {
-  var nameEQ = name + "=";
-  var ca = document.cookie.split(';');
-  for(var i=0;i < ca.length;i++) {
-    var c = ca[i];
-    while (c.charAt(0)==' ') c = c.substring(1,c.length);
-    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-  }
-  return null;
+function readFontSize() {
+	if (supportsLocalStorage()) {
+		return localStorage["fontSize"];
+	} else {
+		var nameEQ = "fontSize=";
+		var ca = document.cookie.split(';');
+		for(var i=0;i < ca.length;i++) {
+			var c = ca[i];
+			while (c.charAt(0)==' ')
+				c = c.substring(1,c.length);
+			if (c.indexOf(nameEQ) == 0)
+				return c.substring(nameEQ.length,c.length);
+		}
+		return null;
+	}
 };
 
 function setUserOptions(){
-        if(!prefsLoaded){
-                cookie = readCookie("fontSize");
-                currentFontSize = cookie ? cookie : defaultFontSize;
-                setFontSize(currentFontSize);
-
-                prefsLoaded = true;
-        }
+	if (!prefsLoaded) {
+		var cookie = readFontSize();
+		currentFontSize = cookie ? cookie : defaultFontSize;
+		setFontSize(currentFontSize);
+		prefsLoaded = true;
+	}
 };
 
 function addControls() {
@@ -128,5 +134,5 @@ function addControls() {
 };
 
 function saveSettings() {
-	createCookie("fontSize", currentFontSize, 365);
+	writeFontSize(currentFontSize);
 };
