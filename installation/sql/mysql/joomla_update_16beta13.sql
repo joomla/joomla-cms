1039704e3b989662ea2eae7294c1e49ec5d78020
Andrew Eddie <andrew.eddie@newlifeinit.com>
2010-11-03 09:51:40 +0000

    Updated Beta 13 SQL upgrade diff
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@19337 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- installation/sql/mysql/joomla_update_16beta13.sql
+++ installation/sql/mysql/joomla_update_16beta13.sql
@@ -6,3 +6,13 @@
 
 ALTER TABLE `#__template_styles`
  CHANGE `params` `params` varchar(10240) NOT NULL DEFAULT '';
+
+ALTER TABLE `#__menu`
+ ADD COLUMN `client_id` TINYINT(4) NOT NULL DEFAULT 0 AFTER `language`;
+
+ALTER TABLE `#__menu`
+ ADD UNIQUE `idx_alias_parent_id` (`client_id`,`parent_id`,`alias`);
+
+UPDATE `jos_menu`
+ SET `menutype` = 'menu', `client_id` = 1
+ WHERE `menutype` = '_adminmenu';
