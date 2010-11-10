bf80d2acf3eff4df76c99a0e503f30c4594db257
Christophe Demko <chdemko@gmail.com>
2010-11-10 15:42:23 +0000

    [#23068] Add admin rights to article security exploit
    --HG--
    extra : convert_revision : svn%3A6f6e1ebd-4c2b-0410-823f-f34bde69bce9/development/trunk/libraries%4019439

--- joomla/form/rules/rules.php
+++ joomla/form/rules/rules.php
@@ -40,6 +40,78 @@ class JFormRuleRules extends JFormRule
 	 */
 	public function test(& $element, $value, $group = null, & $input = null, & $form = null)
 	{
+		// Get the possible field actions and the ones posted to validate them.
+		$fieldActions = self::getFieldActions($element);
+		$valueActions = self::getValueActions($value);
+
+		// Make sure that all posted actions are in the list of possible actions for the field.
+		foreach ($valueActions as $action)
+		{
+			if (!in_array($action, $fieldActions)) {
+				return false;
+			}
+		}
+
 		return true;
+	}
+
+	/**
+	 * Method to get the list of permission action names from the form field value.
+	 *
+	 * @param	mixed	$value		The form field value to validate.
+	 *
+	 * @return	array	A list of permission action names from the form field value.
+	 * @since	1.6
+	 */
+	protected function getValueActions($value)
+	{
+		// Initialise variables.
+		$actions = array();
+
+		// Iterate over the asset actions and add to the actions.
+		foreach ((array) $value as $name => $rules)
+		{
+			$actions[] = $name;
+		}
+
+		return $actions;
+	}
+
+	/**
+	 * Method to get the list of possible permission action names for the form field.
+	 *
+	 * @param	object	$element	The JXMLElement object representing the <field /> tag for the
+	 * 								form field object.
+	 *
+	 * @return	array	A list of permission action names from the form field element definition.
+	 * @since	1.6
+	 */
+	protected function getFieldActions($element)
+	{
+		// Initialise variables.
+		$actions = array();
+
+		// Initialise some field attributes.
+		$section	= $element['section'] ? (string) $element['section'] : '';
+		$component	= $element['component'] ? (string) $element['component'] : '';
+
+		// Get the asset actions for the element.
+		$elActions	= JAccess::getActions($component, $section);
+
+		// Iterate over the asset actions and add to the actions.
+		foreach ($elActions as $item)
+		{
+			$actions[] = $item->name;
+		}
+
+		// Iterate over the children and add to the actions.
+		foreach ($element->children() as $el)
+		{
+			if ($el->getName() == 'action') {
+				$actions[] = (string) $el['name'];
+			}
+		}
+
+		return $actions;
 	}
 }
