8cd70543933fcbe38a5278e052edfcf60a83369a
Andrew Eddie <andrew.eddie@newlifeinit.com>
2010-11-23 00:40:34 +0000

    Code formatting.
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@19622 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- components/com_weblinks/weblinks.php
+++ components/com_weblinks/weblinks.php
@@ -7,11 +7,12 @@
  * @license		GNU General Public License version 2 or later; see LICENSE.txt
  */
 
-// no direct access
+// No direct access.
 defined('_JEXEC') or die;
 
 jimport('joomla.application.component.controller');
 require_once JPATH_COMPONENT.'/helpers/route.php';
+
 $controller	= JController::getInstance('Weblinks');
 $controller->execute(JRequest::getCmd('task'));
 $controller->redirect();
