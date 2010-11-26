41a7cdaa8787dc5a2686e00a6569ed8712a49725
Jean-Marie Simonet <infograf768@gmail.com>
2010-11-26 11:16:12 +0000

    # [#23476] code editor textarea missing border. Thanks Andrea.
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@19653 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- media/editors/codemirror/js/codemirror.js
+++ media/editors/codemirror/js/codemirror.js
@@ -139,7 +139,7 @@ var CodeMirror = (function(){
 	forEach(options.basefiles.concat(options.parserfile), function(file) {
 	  html.push("<script type=\"text/javascript\" src=\"" + options.path + file + "\"></script>");
 	});
-	html.push("</head><body style=\"border-width: 0;\" class=\"editbox\" spellcheck=\"" +
+	html.push("</head><body class=\"editbox\" spellcheck=\"" +
 			  (options.disableSpellcheck ? "false" : "true") + "\"></body></html>");
 
 	var doc = this.win.document;
