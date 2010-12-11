d8530f724b63d4988e9b7e304c89fbc296bf12d1
Christophe Demko <chdemko@gmail.com>
2010-12-11 14:01:38 +0000

    [#23424] Minor improvements to md_stylechanger.js
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@19830 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- templates/beez_20/javascript/md_stylechanger.js
+++ templates/beez_20/javascript/md_stylechanger.js
@@ -1,113 +1,97 @@
+/*global window, localStorage, fontSizeTitle, bigger, reset, smaller, biggerTitle, resetTitle, smallerTitle */
 var prefsLoaded = false;
-var defaultFontSize =100;
+var defaultFontSize = 100;
 var currentFontSize = defaultFontSize;
 
 function supportsLocalStorage() {
-	return ('localStorage' in window) && window['localStorage'] !== null;
-};
-
-if (document.addEventListener) {
-	document.addEventListener("DOMContentLoaded", onLoadEvents, false)
-} else {
-	window.onload = onLoadEvents;
+	return ('localStorage' in window) && window.localStorage !== null;
 }
 
-window.onunload = saveSettings;
-
-function onLoadEvents() {
-	setUserOptions();
-	addControls();
-};
-
-function revertStyles() {
-	currentFontSize = defaultFontSize;
-	changeFontSize(0);
-};
-
-function toggleColors() {
-	if (currentStyle == "White") {
-		setColor("Black");
-	} else {
-		setColor("White");
-	}
-};
+function setFontSize(fontSize) {
+	document.body.style.fontSize = fontSize + '%';
+}
 
 function changeFontSize(sizeDifference) {
-	currentFontSize = parseInt(currentFontSize) + parseInt(sizeDifference * 5);
+	currentFontSize = parseInt(currentFontSize, 10) + parseInt(sizeDifference * 5, 10);
 	if (currentFontSize > 180) {
 		currentFontSize = 180;
 	} else if (currentFontSize < 60) {
 		currentFontSize = 60;
 	}
 	setFontSize(currentFontSize);
-};
+}
 
-function setFontSize(fontSize) {
-	document.body.style.fontSize = fontSize + '%';
-	document.head.style.fontSize = fontSize + '%';
-};
-
+function revertStyles() {
+	currentFontSize = defaultFontSize;
+	changeFontSize(0);
+}
 
 function writeFontSize(value) {
 	if (supportsLocalStorage()) {
-		localStorage["fontSize"] = value;
+		localStorage.fontSize = value;
 	} else {
 		var date = new Date();
 		date.setTime(date.getTime()+(365*24*60*60*1000));
 		var expires = "; expires="+date.toGMTString();
 		document.cookie = "fontSize="+value+expires+"; path=/";
 	}
-};
+}
 
 function readFontSize() {
 	if (supportsLocalStorage()) {
-		return localStorage["fontSize"];
+		return localStorage.fontSize;
 	} else {
 		var nameEQ = "fontSize=";
 		var ca = document.cookie.split(';');
-		for(var i=0;i < ca.length;i++) {
+		for (var i=0;i < ca.length;i++) {
 			var c = ca[i];
-			while (c.charAt(0)==' ')
-				c = c.substring(1,c.length);
-			if (c.indexOf(nameEQ) == 0)
-				return c.substring(nameEQ.length,c.length);
+			while (c.charAt(0) === ' ') {
+				c = c.substring(1, c.length);
+			}
+			if (c.indexOf(nameEQ) === 0) {
+				return c.substring(nameEQ.length, c.length);
+			}
 		}
 		return null;
 	}
-};
+}
 
-function setUserOptions(){
+function setUserOptions() {
 	if (!prefsLoaded) {
-		var cookie = readFontSize();
-		currentFontSize = cookie ? cookie : defaultFontSize;
+		var size = readFontSize();
+		currentFontSize = size ? size : defaultFontSize;
 		setFontSize(currentFontSize);
 		prefsLoaded = true;
 	}
-};
+}
 
 function addControls() {
+	var xhtml = "http://www.w3.org/1999/xhtml";
 	var container = document.getElementById('fontsize');
-	
+
 	var link = new Array(4);
-	var linkClass = new Array("larger", "reset", "smaller");
-	var linkTitle = new Array(biggerTitle, resetTitle, smallerTitle);
-	var linkAction = new Array("changeFontSize(2); return false;", "revertStyles(); return false;", "changeFontSize(-2); return false;");
-	var linkContent = new Array(document.createTextNode(bigger), document.createTextNode(reset), document.createTextNode(smaller));
-	
+	var linkClass = ["larger", "reset", "smaller"];
+	var linkTitle = [biggerTitle, resetTitle, smallerTitle];
+	var linkAction = ["changeFontSize(2); return false;", "revertStyles(); return false;", "changeFontSize(-2); return false;"];
+	var linkContent = [document.createTextNode(bigger), document.createTextNode(reset), document.createTextNode(smaller)];
+
 	var headingText = document.createTextNode(fontSizeTitle);
-	
+
+	var heading;
+	var p;
+	var span;
 	if (document.createElementNS) {
-		var heading = document.createElementNS("http://www.w3.org/1999/xhtml","h3");
-		var p = document.createElementNS("http://www.w3.org/1999/xhtml","p");
-		link[3] = document.createElementNS("http://www.w3.org/1999/xhtml","a");
-		var span = document.createElementNS("http://www.w3.org/1999/xhtml","span");
+		heading = document.createElementNS(xhtml, "h3");
+		p = document.createElementNS(xhtml, "p");
+		link[3] = document.createElementNS(xhtml, "a");
+		span = document.createElementNS(xhtml, "span");
 	} else {
-		var heading = document.createElement("h3");
-		var p = document.createElement("p");
+		heading = document.createElement("h3");
+		p = document.createElement("p");
 		link[3] = document.createElement("a");
-		var span = document.createElement("span");
+		span = document.createElement("span");
 	}
-	
+
 	p.setAttribute("class", "fontsize");
 	span.setAttribute("class", "unseen");
 	link[3].setAttribute("href", "index.php");
@@ -119,20 +103,33 @@ function addControls() {
 		link[x].setAttribute("onclick", linkAction[x]);
 		link[x].appendChild(linkContent[x]);
 	}
-	
-	span.appendChild(document.createTextNode('\u00a0'));	//This is a nbsp
+
+	span.appendChild(document.createTextNode('\u00a0')); //This is a nbsp
 	heading.appendChild(headingText);
-	
+
 	p.appendChild(link[0]);
 	p.appendChild(span.cloneNode(true));
 	p.appendChild(link[1]);
 	p.appendChild(link[2]);
 	p.appendChild(span);
-	
+
 	container.appendChild(heading);
 	container.appendChild(p);
-};
+}
 
 function saveSettings() {
 	writeFontSize(currentFontSize);
-};
+}
+
+function onLoadEvents() {
+	setUserOptions();
+	addControls();
+}
+
+if (document.addEventListener) {
+	document.addEventListener("DOMContentLoaded", onLoadEvents, false);
+} else {
+	window.onload = onLoadEvents;
+}
+
+window.onunload = saveSettings;
