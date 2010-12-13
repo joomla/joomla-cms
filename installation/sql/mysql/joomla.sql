9cb48c1f9f417a801b3a1a6a9e41d8c22b84e2f2
Mark Dexter <dextercowley@gmail.com>
2010-12-13 15:58:32 +0000

    # Sample data fixes (Elin Waring)
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@19867 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- installation/sql/mysql/joomla.sql
+++ installation/sql/mysql/joomla.sql
@@ -1057,7 +1057,7 @@ CREATE TABLE IF NOT EXISTS `#__viewlevels` (
 #
 
 INSERT INTO `#__viewlevels` (`id`, `title`, `ordering`, `rules`) VALUES
-(1, 'Public', 0, '[]'),
+(1, 'Public', 0, '[1]'),
 (2, 'Registered', 1, '[6,2,8]'),
 (3, 'Special', 2, '[6,3,8]');
 
