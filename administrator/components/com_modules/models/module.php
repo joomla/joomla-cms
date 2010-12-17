8f32de26268770f616b1bbfff56485ac856d0e78
Ian MacLennan <ian.maclennan@joomla.org>
2010-12-17 05:20:05 +0000

    # [#22035] Custom HTML module filtering issues
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@19902 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- administrator/components/com_modules/models/module.php
+++ administrator/components/com_modules/models/module.php
@@ -507,6 +507,21 @@ class ModulesModelModule extends JModelAdmin
 	}
 
 	/**
+	 * Loads ContentHelper for filters before validating data.
+	 *
+	 * @param	object		$form		The form to validate against.
+	 * @param	array		$data		The data to validate.
+	 * @return	mixed		Array of filtered data if valid, false otherwise.
+	 * @since	1.1
+	 */
+	function validate($form, $data)
+	{
+		require_once(JPATH_ADMINISTRATOR.'/components/com_content/helpers/content.php');
+
+		return parent::validate($form, $data);
+	}
+
+	/**
 	 * Method to save the form data.
 	 *
 	 * @param	array	$data	The form data.
