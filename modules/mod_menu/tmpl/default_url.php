ee8ba146645639dbe28470bd057ac6b5f1a4cd30
Jean-Marie Simonet <infograf768@gmail.com>
2010-12-20 09:46:23 +0000

    # [#23903] Menu item type external url issues
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@19923 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- modules/mod_menu/tmpl/default_url.php
+++ modules/mod_menu/tmpl/default_url.php
@@ -32,7 +32,7 @@ switch ($item->browserNav) :
 		break;
 	case 2:
 		// window.open
-		$attribs = 'toolbar=no,location=no,status=no,menubar=no,scrollbars=yes,resizable=yes,'.$item->params->get('window_open');
-?><a <?php echo $class; ?>href="<?php echo $item->flink.'&tmpl=component'; ?>" onclick="window.open(this.href,'targetWindow','<?php echo $attribs;?>');return false;" <?php echo $title; ?>><?php echo $linktype; ?></a><?php
+		$attribs = 'toolbar=no,location=no,status=no,menubar=no,scrollbars=yes,resizable=yes,'.$params->get('window_open');
+?><a <?php echo $class; ?>href="<?php echo $item->flink; ?>" onclick="window.open(this.href,'targetWindow','<?php echo $attribs;?>');return false;" <?php echo $title; ?>><?php echo $linktype; ?></a><?php
 		break;
 endswitch;
