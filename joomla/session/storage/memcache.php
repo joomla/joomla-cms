ec2e594614e505f2f483062645963ba639e88e07
Christophe Demko <chdemko@gmail.com>
2010-12-24 08:15:10 +0000

    [#23923] PHP Strict validation in session classes
    --HG--
    extra : convert_revision : svn%3A6f6e1ebd-4c2b-0410-823f-f34bde69bce9/development/trunk/libraries%4019974

--- joomla/session/storage/memcache.php
+++ joomla/session/storage/memcache.php
@@ -169,7 +169,7 @@ class JSessionStorageMemcache extends JSessionStorage
 	 * @param integer $maxlifetime  The maximum age of a session.
 	 * @return boolean  True on success, false otherwise.
 	 */
-	function gc($maxlifetime)
+	function gc($maxlifetime = null)
 	{
 		return true;
 	}
