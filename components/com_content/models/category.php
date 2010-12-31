73c93d5feed3d3b1025e18ffab08937a497b67fc
Christophe Demko <chdemko@gmail.com>
2010-12-31 11:45:27 +0000

    [#23972] Category List does not show non-empty subcategories
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@20043 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- components/com_content/models/category.php
+++ components/com_content/models/category.php
@@ -256,7 +256,7 @@ class ContentModelCategory extends JModelItem
 			if( isset( $this->state->params ) ) {
 				$params = $this->state->params;
 				$options = array();
-				$options['countItems'] = $params->get('show_cat_num_articles', 1);
+				$options['countItems'] = $params->get('show_cat_num_articles', 1) || !$params->get('show_empty_categories_cat', 0);
 			}
 			else {
 				$options['countItems'] = 0;
