d489c20b9eb7a74f3d203a36535604bb6ad32c3e
Andrew Eddie <andrew.eddie@newlifeinit.com>
2011-01-05 00:40:30 +0000

    Fixed issue [#23717] Add a field for E-Mail to JForm (Rouven Weßling).
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@20081 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

---
+++ libraries/joomla/form/fields/email.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * @version		$Id$
+ * @package		Joomla.Framework
+ * @subpackage	Form
+ * @copyright	Copyright (C) 2005 - 2010 Open Source Matters, Inc. All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('JPATH_BASE') or die;
+
+jimport('joomla.form.formfield');
+
+/**
+ * Form Field class for the Joomla Framework.
+ *
+ * @package		Joomla.Framework
+ * @subpackage	Form
+ * @since		1.6
+ */
+class JFormFieldEMail extends JFormField
+{
+	/**
+	 * The form field type.
+	 *
+	 * @var		string
+	 * @since	1.6
+	 */
+	protected $type = 'Email';
+
+	/**
+	 * Method to get the field input markup.
+	 *
+	 * @return	string	The field input markup.
+	 * @since	1.6
+	 */
+	protected function getInput()
+	{
+		// Initialize some field attributes.
+		$size		= $this->element['size'] ? ' size="'.(int) $this->element['size'].'"' : '';
+		$maxLength	= $this->element['maxlength'] ? ' maxlength="'.(int) $this->element['maxlength'].'"' : '';
+		$class		= $this->element['class'] ? ' '.(string) $this->element['class'] : '';
+		$readonly	= ((string) $this->element['readonly'] == 'true') ? ' readonly="readonly"' : '';
+		$disabled	= ((string) $this->element['disabled'] == 'true') ? ' disabled="disabled"' : '';
+
+		// Initialize JavaScript field attributes.
+		$onchange	= $this->element['onchange'] ? ' onchange="'.(string) $this->element['onchange'].'"' : '';
+
+		return '<input type="text" name="'.$this->name.'" class="validate-email '.$class.'" id="'.$this->id.'"' .
+				' value="'.htmlspecialchars($this->value, ENT_COMPAT, 'UTF-8').'"' .
+				$size.$disabled.$readonly.$onchange.$maxLength.'/>';
+	}
+}
