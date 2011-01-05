2dabbd37826e3ecff1f53142d9314a428175ee71
Andrew Eddie <andrew.eddie@newlifeinit.com>
2011-01-05 05:28:03 +0000

    Add missing Joomla content plugin to SQL diff.
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@20089 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- installation/sql/mysql/joomla_update_16ga.sql
+++ installation/sql/mysql/joomla_update_16ga.sql
@@ -21,3 +21,8 @@ REPLACE INTO `#__extensions` (`extension_id`, `name`, `type`, `element`, `folder
 
 ALTER TABLE `#__banner_tracks`
  CHANGE `track_date` `track_date` datetime NOT NULL;
+
+# Add missing Joomla content plugin.
+
+REPLACE INTO `#__extensions` (`extension_id`, `name`, `type`, `element`, `folder`, `client_id`, `enabled`, `access`, `protected`, `manifest_cache`, `params`, `custom_data`, `system_data`, `checked_out`, `checked_out_time`, `ordering`, `state`) VALUES
+(435, 'plg_content_joomla', 'plugin', 'joomla', 'content', 0, 1, 1, 0, '', '{}', '', '', 0, '0000-00-00 00:00:00', 0, 0);
