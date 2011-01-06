70ef33b6c6e8608385b82d0b7a256955c89b2dc4
Ian MacLennan <ian.maclennan@joomla.org>
2011-01-06 14:46:34 +0000

    # [#23823] Front-end URL Error
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@20118 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- components/com_content/router.php
+++ components/com_content/router.php
@@ -30,100 +30,97 @@ function ContentBuildRoute(&$query)
 	// we need a menu item.  Either the one specified in the query, or the current active one if none specified
 	if (empty($query['Itemid'])) {
 		$menuItem = $menu->getActive();
+		$menuItemGiven = false;
 	}
 	else {
 		$menuItem = $menu->getItem($query['Itemid']);
+		$menuItemGiven = true;
 	}
 
-	$mView	= (empty($menuItem->query['view'])) ? null : $menuItem->query['view'];
-	$mCatid	= (empty($menuItem->query['catid'])) ? null : $menuItem->query['catid'];
-	$mId	= (empty($menuItem->query['id'])) ? null : $menuItem->query['id'];
-
-	// we determine the view from the query.  If we don't have an Itemid in the query
-	// then we put the view onto the URL
 	if (isset($query['view'])) {
 		$view = $query['view'];
-		if (empty($query['Itemid'])) {
-			$segments[] = $query['view'];
-		}
-		unset($query['view']);
-	}
-
-
-	// we determine the category id.  If we are dealing with an article, it is the category
-	// that the article is in (if specified in the passed in query).  If we are dealing with
-	// a category, it is the id of the category.  Otherwise we set it to false
-	// so that we don't look for it in the loop below
-	if (isset($view) && $view == 'article' && isset($query['catid'])) {
-		$catid = $query['catid'];
-	}
-	elseif (isset($view) && $view != 'article' && isset($query['id'])) {
-		$catid = $query['id'];
 	}
 	else {
-		$catid = false;
+		// we need to have a view in the query or it is an invalid URL
+		return $segments;
 	}
 
-	// in the section below, we find the category that our menu item points to and we
-	// build the URL off of it.
-	if (isset($view) and ($view == 'category' or $view == 'article') and $catid) {
-		$menuCatid = $mId;
-		$categories = JCategories::getInstance('Content');
-		$category = $categories->get($catid);
-		
-		if ($category) {
-			$path = $category->getPath();
+	// are we dealing with an article or category that is attached to a menu item?
+	if ($menuItem->query['view'] == $query['view'] && isset($query['id']) && $menuItem->query['id'] == intval($query['id'])) {
+		unset($query['view']);
 
-			// $foundMenuCategory keeps track of whether or not we have traversed the path to our
-			// target category to the point where our menu item points to.  i.e. if our path has five elements
-			// (we are nested that deep), and our menu item points to a category in the third level, we want to
-			// place the last two levels onto the URL.  So we don't start adding categories until we've found the
-			// menu category in the path
-			$foundMenuCategory = false;
-
-			$array = array();
-
-			// we walk the path to the category
-			foreach($path as $id)
-			{
-				// we always strip our ids.  At the end, we will add in the id
-				// of our target category
-				list($tmp, $current_id) = explode(':', $id, 2);
-
-				if ($foundMenuCategory) {
-					$array[] = $current_id;
-				} else {
-					// once we arrive at our menu item category we set foundMenuCategory to true.  Thus on the next item in
-					// the path we will start adding it
-					if ($tmp == $menuCatid) {
-						$foundMenuCategory = true;
-					}
-				}
-			}
-
-			// if we don't have a menu item pointing to our category we put the category path onto the url
-			if (!$foundMenuCategory) {
-				foreach($path AS $id)
-				{
-					list($tmp, $current_id) = explode(':', $id, 2);
-					$array[] = $current_id;
-				}
-			}
-
-			// we add the category id back in if we've adding a category to the URL.  If we are pointing at an article
-			// and it is in the category that the menu item points to, then we don't add the category to the URL, because
-			// there is no appropriate category to add
-			if (!$advanced && count($array)) {
-				$array[0] = $catid.':'.$array[0];
-			}
-
-			$segments = array_merge($segments, $array);
+		if (isset($query['catid'])) {
+			unset($query['catid']);
 		}
 
-		// if we are pointing at an article and the menu doesn't point at an article, we add
-		// the id and alias of the article to the URL.  We don't want to add the article info
-		// if it is already there in the menu item.
-		if ($view == 'article' && $mView != 'article') {
+		unset($query['id']);
+
+		return $segments;
+	}
+
+	if ($view == 'category' || $view == 'article')
+	{
+		if (!$menuItemGiven) {
+			$segments[] = $view;
+		}
+
+		unset($query['view']);
+
+		if ($view == 'article') {
+			if (isset($query['id']) && $query['catid']) {
+				$catid = $query['catid'];
+				$id = $query['id'];
+			} else {
+				// we should have these two set for this view.  If we don't, it is an error
+				return $segments;
+			}
+		}
+		else {
+			if (isset($query['id'])) {
+				$catid = $query['id'];
+			} else {
+				// we should have id set for this view.  If we don't, it is an error
+				return $segments;
+			}
+		}
+
+		if ($menuItemGiven) {
+			$mCatid = $menuItem->query['id'];
+		} else {
+			$mCatid = 0;
+		}
+
+		$categories = JCategories::getInstance('Content');
+		$category = $categories->get($catid);
+
+		if (!$category) {
+			// we couldn't find the category we were given.  Bail.
+			return $segments;
+		}
+
+		$path = array_reverse($category->getPath());
+
+		$array = array();
+
+		foreach($path AS $id) {
+			if ((int)$id == (int)$mCatid) {
+				break;
+			}
+
+			list($tmp, $id) = explode(':', $id, 2);
+
+			$array[] = $id;
+		}
+
+		$array = array_reverse($array);
+
+		if (!$advanced && count($array)) {
+			$array[0] = $catid.':'.$array[0];
+		}
+
+		$segments = array_merge($segments, $array);
+
+		if ($view == 'article') {
 			if ($advanced) {
 				list($tmp, $id) = explode(':', $query['id'], 2);
 			}
@@ -132,30 +129,35 @@ function ContentBuildRoute(&$query)
 			}
 			$segments[] = $id;
 		}
-
-		// we unset id and catid so they don't show up in the URL.
 		unset($query['id']);
 		unset($query['catid']);
 	}
 
-	if (isset($query['year'])) {
-		if (!empty($query['Itemid'])) {
-			$segments[] = $query['year'];
-			unset($query['year']);
+	if ($view == 'archive') {
+		if (!$menuItemGiven) {
+			$segments[] = $view;
+			unset($query['view']);
 		}
-	}
 
-	if (isset($query['month'])) {
-		if (!empty($query['Itemid'])) {
-			$segments[] = $query['month'];
-			unset($query['month']);
+		if (isset($query['year'])) {
+			if ($menuItemGiven) {
+				$segments[] = $query['year'];
+				unset($query['year']);
+			}
+		}
+
+		if (isset($query['year']) && isset($query['month'])) {
+			if ($menuItemGiven) {
+				$segments[] = $query['month'];
+				unset($query['month']);
+			}
 		}
 	}
 
 	// if the layout is specified and it is the same as the layout in the menu item, we
 	// unset it so it doesn't go into the query string.
 	if (isset($query['layout'])) {
-		if (!empty($query['Itemid']) && isset($menuItem->query['layout'])) {
+		if ($menuItemGiven && isset($menuItem->query['layout'])) {
 			if ($query['layout'] == $menuItem->query['layout']) {
 
 				unset($query['layout']);
@@ -168,13 +170,10 @@ function ContentBuildRoute(&$query)
 		}
 	}
 
-	if (isset($query['id'])) {
-		$segments[] = $query['id'];
-		unset($query['id']);
-	}
-
 	return $segments;
 }
+
+
 
 /**
  * Parse the segments of a URL.
@@ -212,6 +211,12 @@ function ContentParseRoute($segments)
 	// we test it first to see if it is a category.  If the id and alias match a category
 	// then we assume it is a category.  If they don't we assume it is an article
 	if ($count == 1) {
+		// we check to see if an alias is given.  If not, we assume it is an article
+		if (strpos($segments[0], ':') === false) {
+			$vars['view'] = 'article';
+			$vars['id'] = $segments[0];
+		}
+
 		list($id, $alias) = explode(':', $segments[0], 2);
 
 		// first we check if it is a category
