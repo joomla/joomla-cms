a614263808de3eac22c31fe20a048a5a3d344c5f
Christophe Demko <chdemko@gmail.com>
2011-01-06 15:38:28 +0000

    [#24118] Update Error pages for Beez2 and Beez5
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@20122 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- templates/beez5/error.php
+++ templates/beez5/error.php
@@ -7,22 +7,194 @@
  */
 
 defined('_JEXEC') or die;
+
+?>
+<?php 
+$templateparams	=  JFactory::getApplication()->getTemplate(true)->params;
+if(!$templateparams->get('html5', 0)): ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<?php else: ?>
+	<?php echo '<!DOCTYPE html>'; ?>
+<?php endif; ?>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<?php  $doc=JFactory::getDocument();
+ $language=$doc->language;?>
+<meta name="language" content="<?php echo $language; ?>" />
+
+	<title><?php echo $this->error->getCode(); ?> - <?php echo $this->title; ?></title>
+<?php if ($this->error->getCode()>=400 && $this->error->getCode() < 500) { 	?>
+
+
+		<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/system/css/system.css" type="text/css" />
+		<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/beez5/css/position.css" type="text/css" media="screen,projection" />
+		<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/beez5/css/layout.css" type="text/css" media="screen,projection" />
+		<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/beez5/css/print.css" type="text/css" media="Print" />
+		<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/beez5/css/beez5.css" type="text/css" />
+<?php
+	$files = JHtml::_('stylesheet','templates/beez5/css/general.css',null,false,true);
+	if ($files):
+		if (!is_array($files)):
+			$files = array($files);
+		endif;
+		foreach($files as $file):
+?>
+		<link rel="stylesheet" href="<?php echo $file;?>" type="text/css" />
+<?php
+	 	endforeach;
+	endif;
+?>
+		<?php if ($this->direction == 'rtl') : ?>
+		<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/beez5/css/template_rtl.css" type="text/css" />
+		<?php endif; ?>
+		<!--[if lte IE 6]>
+			<link href="<?php echo $this->baseurl ?>/templates/beez5/css/ieonly.css" rel="stylesheet" type="text/css" />
+		<![endif]-->
+		<!--[if IE 7]>
+			<link href="<?php echo $this->baseurl ?>/templates/beez5/css/ie7only.css" rel="stylesheet" type="text/css" />
+		<![endif]-->
+	
+	
+<style type="text/css">
+			<!--
+			#errorboxbody
+			{margin:30px}
+			#errorboxbody h2
+			{font-weight:normal; 
+			font-size:1.5em}
+			#searchbox 
+			{background:#eee; 
+			padding:10px; 
+			margin-top:20px; 
+			border:solid 1px #ddd
+			}
+			-->
+</style>
+
+</head>
+
+<body>
+
+
+	<div id="all">
+		<div id="back">
+			<?php if(!$templateparams->get('html5', 0)): ?>
+		<div id="header">
+			<?php else: ?>
+		<header id="header">
+			<?php endif; ?>
+					<div class="logoheader">
+						<?php
+								$params = JFactory::getApplication()->getTemplate(true)->params;
+								$logo =  $params->get('logo'); 
+							?>
+						
+							<?php jimport( 'joomla.application.module.helper' ); ?>
+
+						<h1 id="logo">
+						<?php if ($logo != null ): ?>
+						<img src="<?php echo $this->baseurl  .'/'. $logo; ?>" alt="<?php echo htmlspecialchars($params->get('sitetitle'));?>" />
+						<?php else: ?>
+						<?php echo htmlspecialchars($params->get('sitetitle'));?>
+						<?php endif; ?>
+						<span class="header1">
+						<?php echo htmlspecialchars($params->get('sitedescription'));?>
+						</span></h1>
+					</div><!-- end logoheader -->
+	
+						<ul class="skiplinks">
+							<li><a href="#wrapper2" class="u2"><?php echo JText::_('TPL_BEEZ5_SKIP_TO_ERROR_CONTENT'); ?></a></li>
+							<li><a href="#nav" class="u2"><?php echo JText::_('TPL_BEEZ5_ERROR_JUMP_TO_NAV'); ?></a></li>
+							
+						</ul>
+						<div id="line"></div>
+					<div id="header-image">
+					<img src="<?php echo $this->baseurl ?>/templates/beez5/images/fruits.jpg"  alt="<?php echo JText::_('TPL_BEEZ5_LOGO'); ?>" />
+					</div>
+				
+		<?php if(!$templateparams->get('html5', 0)): ?>
+				</div><!-- end header -->
+			<?php else: ?>
+				</header><!-- end header -->
+			<?php endif; ?>
+		<div id="contentarea2" >
+			
+						<?php if(!$templateparams->get('html5', 0)): ?>
+							<div class="left1" id="nav">
+						<?php else: ?>
+							<nav class="left1" id="nav">
+						<?php endif; ?>
+				<h2 class="unseen"><?php echo JText::_('TPL_BEEZ5_NAVIGATION'); ?></h2>
+											<?php $module = JModuleHelper::getModule( 'menu' );
+											echo JModuleHelper::renderModule( $module);	?>
+					<?php if(!$templateparams->get('html5', 0)): ?>
+							</div>
+						<?php else: ?>
+							</nav>
+						<?php endif; ?><!-- end navi -->
+			<div id="wrapper2">
+			<div id="errorboxbody">
+						<h2><?php echo JText::_('JERROR_AN_ERROR_HAS_OCCURRED'); ?><br />
+								<?php echo JText::_('JERROR_LAYOUT_PAGE_NOT_FOUND'); ?></h2>
+											<div id="searchbox">
+														<h3 class="unseen"><?php echo JText::_('TPL_BEEZ5_SEARCH'); ?></h3>
+											<p><?php echo JText::_('JERROR_LAYOUT_SEARCH'); ?></p>
+											<?php $module = JModuleHelper::getModule( 'search' );
+											echo JModuleHelper::renderModule( $module);	?>
+												<p><a href="<?php echo $this->baseurl; ?>/index.php" title="<?php echo JText::_('JERROR_LAYOUT_GO_TO_THE_HOME_PAGE'); ?>"><?php echo JText::_('JERROR_LAYOUT_HOME_PAGE'); ?></a></p>	
+										</div>
+							
+									<p><?php echo $this->error->getCode() ; echo $this->error->getMessage();?><br />
+							
+					<?php echo JText::_('JERROR_LAYOUT_PLEASE_CONTACT_THE_SYSTEM_ADMINISTRATOR'); ?>.</p>
+				</div><!-- end wrapper -->			
+			</div><!-- end contentarea -->
+
+						<?php if ($this->debug) :
+							echo $this->renderBacktrace();
+						endif; ?>
+					
+
+			</div>  <!--end all -->
+			</div>
+			</div> 
+
+		<div id="footer-sub">
+
+			<?php if (!$templateparams->get('html5', 0)): ?>
+				<div id="footer">
+			<?php else: ?>
+				<footer id="footer">
+			<?php endif; ?>
+
+					<p>
+						<?php echo JText::_('TPL_BEEZ5_POWERED_BY');?> <a href="http://www.joomla.org/">Joomla!&#174;</a>
+					</p>
+
+			<?php if (!$templateparams->get('html5', 0)): ?>
+				</div><!-- end footer -->
+			<?php else: ?>
+				</footer>
+			<?php endif; ?>
+
+			</div>
+</body>
+</html>
+<?php } else { ?>
+<?php 
 if (!isset($this->error)) {
 	$this->error = JError::raiseWarning(404, JText::_('JERROR_ALERTNOAUTHOR'));
 	$this->debug = false; 
 }
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>">
-<head>
-	<title><?php echo $this->error->getCode(); ?> - <?php echo $this->title; ?></title>
 	<link rel="stylesheet" href="<?php echo $this->baseurl; ?>/templates/system/css/error.css" type="text/css" />
 </head>
 <body>
 	<div class="error">
 		<div id="outline">
 		<div id="errorboxoutline">
-			<div id="errorboxheader"><?php echo $this->error->getCode(); ?> - <?php echo $this->error->getMessage(); ?></div>
+			<div id="errorboxheader"> <?php echo $this->title; ?></div>
 			<div id="errorboxbody">
 			<p><strong><?php echo JText::_('JERROR_LAYOUT_NOT_ABLE_TO_VISIT'); ?></strong></p>
 				<ol>
@@ -37,6 +209,8 @@ if (!isset($this->error)) {
 
 				<ul>
 					<li><a href="<?php echo $this->baseurl; ?>/index.php" title="<?php echo JText::_('JERROR_LAYOUT_GO_TO_THE_HOME_PAGE'); ?>"><?php echo JText::_('JERROR_LAYOUT_HOME_PAGE'); ?></a></li>
+					<li><a href="<?php echo $this->baseurl; ?>/index.php?option=com_search" title="<?php echo JText::_('JERROR_LAYOUT_SEARCH_PAGE'); ?>"><?php echo JText::_('JERROR_LAYOUT_SEARCH_PAGE'); ?></a></li>
+
 				</ul>
 
 			<p><?php echo JText::_('JERROR_LAYOUT_PLEASE_CONTACT_THE_SYSTEM_ADMINISTRATOR'); ?>.</p>
@@ -54,3 +228,6 @@ if (!isset($this->error)) {
 	</div>
 </body>
 </html>
+
+
+<?php } ?>
--- templates/beez_20/error.php
+++ templates/beez_20/error.php
@@ -7,22 +7,193 @@
  */
 
 defined('_JEXEC') or die;
+$params = JFactory::getApplication()->getTemplate(true)->params;
+$logo =  $params->get('logo');
+$showRightColumn = 0;
+$showleft = 0;
+$showbottom = 0;
+
+// get params
+$color              = $params->get('templatecolor');
+$navposition        = $params->get('navposition');
+
+?>
+<?php 
+$templateparams	=  JFactory::getApplication()->getTemplate(true)->params;
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<?php  $doc=JFactory::getDocument();
+ $language=$doc->language;?>
+<meta name="language" content="<?php echo $language; ?>" />
+
+	<title><?php echo $this->error->getCode(); ?> - <?php echo $this->title; ?></title>
+<?php if ($this->error->getCode()>=400 && $this->error->getCode() < 500) { 	?>
+
+
+		<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/system/css/system.css" type="text/css" />
+		<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/beez_20/css/position.css" type="text/css" media="screen,projection" />
+		<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/beez_20/css/layout.css" type="text/css" media="screen,projection" />
+		<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/beez_20/css/print.css" type="text/css" media="Print" />
+		<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/beez_20/css/<?php echo htmlspecialchars($color); ?>.css" type="text/css" />     
+<?php
+	$files = JHtml::_('stylesheet','templates/beez5/css/general.css',null,false,true);
+	if ($files):
+		if (!is_array($files)):
+			$files = array($files);
+		endif;
+		foreach($files as $file):
+?>
+		<link rel="stylesheet" href="<?php echo $file;?>" type="text/css" />
+<?php
+	 	endforeach;
+	endif;
+?>
+		<?php if ($this->direction == 'rtl') : ?>
+		<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/beez_20/css/template_rtl.css" type="text/css" />
+		<?php endif; ?>
+		<!--[if lte IE 6]>
+			<link href="<?php echo $this->baseurl ?>/templates/beez_20/css/ieonly.css" rel="stylesheet" type="text/css" />
+		<![endif]-->
+		<!--[if IE 7]>
+			<link href="<?php echo $this->baseurl ?>/templates/beez_20/css/ie7only.css" rel="stylesheet" type="text/css" />
+		<![endif]-->
+	
+	
+<style type="text/css">
+			<!--
+			#errorboxbody
+			{margin:30px}
+			#errorboxbody h2
+			{font-weight:normal; 
+			font-size:1.5em}
+			#searchbox 
+			{background:#eee; 
+			padding:10px; 
+			margin-top:20px; 
+			border:solid 1px #ddd
+			}
+			-->
+</style>
+
+</head>
+
+<body>
+
+
+	<div id="all">
+		<div id="back">
+
+		<div id="header">
+		
+					<div class="logoheader">
+						<?php
+								$params = JFactory::getApplication()->getTemplate(true)->params;
+								$logo =  $params->get('logo'); 
+							?>
+						
+							<?php jimport( 'joomla.application.module.helper' ); ?>
+
+						 <h1 id="logo">
+
+                                        <?php if ($logo): ?>
+                                        <img src="<?php echo $this->baseurl ?>/<?php echo htmlspecialchars($logo); ?>"  alt="<?php echo htmlspecialchars($params->get('sitetitle'));?>" />
+                                        <?php endif;?>
+                                        <?php if (!$logo ): ?>
+                                        <?php echo htmlspecialchars($params->get('sitetitle'));?>
+                                        <?php endif; ?>
+                                        <span class="header1">
+                                        <?php echo htmlspecialchars($params->get('sitedescription'));?>
+                                        </span></h1>
+					</div><!-- end logoheader -->
+	
+						<ul class="skiplinks">
+							<li><a href="#wrapper2" class="u2"><?php echo JText::_('TPL_BEEZ2_SKIP_TO_ERROR_CONTENT'); ?></a></li>
+							<li><a href="#nav" class="u2"><?php echo JText::_('TPL_BEEZ2_ERROR_JUMP_TO_NAV'); ?></a></li>
+							
+						</ul>
+						<div id="line"></div>
+					
+				
+	
+				</div><!-- end header -->
+		
+		<div id="contentarea2" >
+			
+				
+							<div class="left1" id="nav">
+					
+				<h2 class="unseen"><?php echo JText::_('TPL_BEEZ2_NAVIGATION'); ?></h2>
+											<?php $module = JModuleHelper::getModule( 'menu' );
+											echo JModuleHelper::renderModule( $module);	?>
+				
+							</div>
+					
+					<!-- end navi -->
+			<div id="wrapper2">
+			<div id="errorboxbody">
+						<h2><?php echo JText::_('JERROR_AN_ERROR_HAS_OCCURRED'); ?><br />
+								<?php echo JText::_('JERROR_LAYOUT_PAGE_NOT_FOUND'); ?></h2>
+											<div id="searchbox">
+											<h3 class="unseen"><?php echo JText::_('TPL_BEEZ2_SEARCH'); ?></h3>
+											<p><?php echo JText::_('JERROR_LAYOUT_SEARCH'); ?></p>
+											<?php $module = JModuleHelper::getModule( 'search' );
+											echo JModuleHelper::renderModule( $module);	?>
+												<p><a href="<?php echo $this->baseurl; ?>/index.php" title="<?php echo JText::_('JERROR_LAYOUT_GO_TO_THE_HOME_PAGE'); ?>"><?php echo JText::_('JERROR_LAYOUT_HOME_PAGE'); ?></a></p>	
+										</div>
+							
+									<p><?php echo $this->error->getCode() ; echo $this->error->getMessage();?><br />
+							
+					<?php echo JText::_('JERROR_LAYOUT_PLEASE_CONTACT_THE_SYSTEM_ADMINISTRATOR'); ?>.</p>
+				</div><!-- end wrapper -->			
+			</div><!-- end contentarea -->
+
+						<?php if ($this->debug) :
+							echo $this->renderBacktrace();
+						endif; ?>
+					
+
+			</div>  <!--end all -->
+			
+			</div>
+			</div> 
+			 <div id="footer-outer">
+		
+			<div id="footer-sub">
+
+
+                                <div id="footer">
+                                        <p>
+                                                <?php echo JText::_('TPL_BEEZ2_POWERED_BY');?> <a href="http://www.joomla.org/">Joomla!&#174;</a>
+                                        </p>
+
+                                </div><!-- end footer -->
+                         </div><!-- end footer-sub -->
+              
+                       </div>
+            
+
+
+</body>
+</html>
+<?php } else { ?>
+<?php 
 if (!isset($this->error)) {
 	$this->error = JError::raiseWarning(404, JText::_('JERROR_ALERTNOAUTHOR'));
 	$this->debug = false; 
 }
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>">
-<head>
-	<title><?php echo $this->error->getCode(); ?> - <?php echo $this->title; ?></title>
 	<link rel="stylesheet" href="<?php echo $this->baseurl; ?>/templates/system/css/error.css" type="text/css" />
 </head>
 <body>
 	<div class="error">
 		<div id="outline">
 		<div id="errorboxoutline">
-			<div id="errorboxheader"><?php echo $this->error->getCode(); ?> - <?php echo $this->error->getMessage(); ?></div>
+			<div id="errorboxheader"> <?php echo $this->title; ?></div>
 			<div id="errorboxbody">
 			<p><strong><?php echo JText::_('JERROR_LAYOUT_NOT_ABLE_TO_VISIT'); ?></strong></p>
 				<ol>
@@ -37,6 +208,8 @@ if (!isset($this->error)) {
 
 				<ul>
 					<li><a href="<?php echo $this->baseurl; ?>/index.php" title="<?php echo JText::_('JERROR_LAYOUT_GO_TO_THE_HOME_PAGE'); ?>"><?php echo JText::_('JERROR_LAYOUT_HOME_PAGE'); ?></a></li>
+					<li><a href="<?php echo $this->baseurl; ?>/index.php?option=com_search" title="<?php echo JText::_('JERROR_LAYOUT_SEARCH_PAGE'); ?>"><?php echo JText::_('JERROR_LAYOUT_SEARCH_PAGE'); ?></a></li>
+
 				</ul>
 
 			<p><?php echo JText::_('JERROR_LAYOUT_PLEASE_CONTACT_THE_SYSTEM_ADMINISTRATOR'); ?>.</p>
@@ -54,3 +227,6 @@ if (!isset($this->error)) {
 	</div>
 </body>
 </html>
+
+
+<?php } ?>
