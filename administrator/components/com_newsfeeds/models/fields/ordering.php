283cfaf7674356af5fc989ba2f0e04612c7ccbfe
Andrew Eddie <andrew.eddie@newlifeinit.com>
2011-01-08 09:13:28 +0000

    Fixed issue [#24201] Value in input tag is not escaped (Ian MacLennan).
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@20182 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- administrator/components/com_newsfeeds/models/fields/ordering.php
+++ administrator/components/com_newsfeeds/models/fields/ordering.php
@@ -62,7 +62,7 @@ class JFormFieldOrdering extends JFormField
 		// Create a read-only list (no name) with a hidden input to store the value.
 		if ((string) $this->element['readonly'] == 'true') {
 			$html[] = JHtml::_('list.ordering', '', $query, trim($attr), $this->value, $newsfeedId ? 0 : 1);
-			$html[] = '<input type="hidden" name="'.$this->name.'" value="'.$this->value.'"/>';
+			$html[] = '<input type="hidden" name="'.$this->name.'" value="'.htmlspecialchars($this->value).'"/>';
 		}
 		// Create a regular list.
 		else {
