9a87a6d8bac63532119ecda17653989684e5fcaf
Ian MacLennan <ian.maclennan@joomla.org>
2011-02-01 14:20:14 -0500

    Imported JRule and JRules tests

---
+++ tests/suite/joomla/access/JRuleTest.php
@@ -0,0 +1,177 @@
+<?php
+/**
+ * @version		$Id: JRuleTest.php 20196 2011-01-09 02:40:25Z ian $
+ * @copyright	Copyright (C) 2005 - 2011 Open Source Matters. All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ * @package		JoomlaFramework
+ */
+
+require_once JPATH_LIBRARIES.'/joomla/access/rule.php';
+
+/**
+ * @package		JoomlaFramework
+ */
+class JRuleTest extends PHPUnit_Framework_TestCase
+{
+	public function setUp()
+	{
+
+	}
+
+	public function testConstructor()
+	{
+		$array = array(
+			-42	=> 1,
+			2	=> 1,
+			3	=> 0
+		);
+
+
+
+
+		// Get the string representation.
+		$string		  = json_encode($array);
+
+
+		// Test constructor with array.
+		$rule1	= new JRule($array);
+
+		// Check that import equals export.
+		$this->assertEquals(
+			$string,
+			(string) $rule1
+		);
+
+		// Test constructor with string.
+
+
+		// Check that import equals export.
+
+                //**// Check that import equals not export.
+
+                 $array_A = array(
+			-44	=> 1,
+			2	=> 1,
+			3	=> 0
+		);
+
+                $string_A          = json_encode($array_A);
+                $rule_A	= new JRule($string_A);
+		$this->assertNotEquals(
+			$string,
+			(string) $rule_A
+		);
+	}
+
+	public function testMergeIdentity()
+	{
+		// Construct an rule with no identities.
+		$rule = new JRule('');
+
+		// Add the identity with allow.
+		$rule->mergeIdentity(-42, true);
+		$this->assertEquals(
+			'{"-42":1}',
+			(string) $rule
+		);
+
+		// Readd the identity, but deny.
+		$rule->mergeIdentity(-42, false);
+		$this->assertEquals(
+			'{"-42":0}',
+			(string) $rule
+		);
+
+		// Readd the identity with allow (checking deny wins).
+		$rule->mergeIdentity(-42, true);
+		$this->assertEquals(
+			'{"-42":0}',
+			(string) $rule
+		);
+	}
+
+	public function testMergeIdentities()
+	{
+		$array = array(
+			-42	=> 1,
+			2	=> 1,
+			3	=> 0
+		);
+
+		// Construct an rule with no identities.
+		$rule = new JRule('');
+
+		$rule->mergeIdentities($array);
+		$this->assertEquals(
+			json_encode($array),
+			(string) $rule
+		);
+
+                // Check that import equals export.
+
+
+                //**Test testMergeIdentities with object
+
+                $rule_A = new JRule($array);
+                $rule->mergeIdentities($rule_A);
+                $this->assertEquals(
+			json_encode($array),
+			(string) $rule
+		);
+
+                $this->assertEquals(
+			(string) $rule_A,
+			(string) $rule
+		);
+
+
+
+		// Merge a new set, flipping some bits.
+		$array = array(
+			-42	=> 0,
+			2	=> 1,
+			3	=> 1,
+			4	=> 1
+		);
+
+		// Ident 3 should remain false, 4 should be added.
+		$result = array(
+			-42	=> 0,
+			2	=> 1,
+			3	=> 0,
+			4	=> 1
+		);
+		$rule->mergeIdentities($array);
+		$this->assertEquals(
+			json_encode($result),
+			(string) $rule
+		);
+	}
+
+	public function testAllow()
+	{
+		// Simple allow and deny test.
+		$array = array(
+			-42	=> 0,
+			2	=> 1
+		);
+		$rule = new JRule($array);
+
+		// This one should be denied.
+		$this->assertFalse(
+			$rule->allow(-42)
+		);
+
+                $this->assertEquals(Null,$rule->allow(Null));
+
+		// This one should be allowed.
+		$this->assertTrue(
+			$rule->allow(2)
+		);
+
+		// This one should be denied.
+		$this->assertFalse(
+			$rule->allow(array(-42, 2))
+		);
+	}
+}
---
+++ tests/suite/joomla/access/JRulesTest.php
@@ -0,0 +1,252 @@
+<?php
+/**
+ * @version		$Id: JRulesTest.php 20196 2011-01-09 02:40:25Z ian $
+ * @copyright	Copyright (C) 2005 - 2011 Open Source Matters. All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ * @package		JoomlaFramework
+ */
+
+
+require_once JPATH_LIBRARIES.'/joomla/access/rules.php';
+
+/**
+ * @package		JoomlaFramework
+ */
+class JRulesTest extends PHPUnit_Framework_TestCase
+{
+	public function setUp()
+	{
+
+	}
+
+	/**
+	 * This method tests both the contructor and the __toString magic method.
+	 *
+	 * The input for this class could come from a posted form, or from a JSON string
+	 * stored in the database.  We need to ensure that the resulting JSON is the same
+	 * as the input.
+	 */
+	public function testConstructor()
+	{
+		$array = array(
+			'edit' => array(
+				-42	=> 1,
+				2	=> 1,
+				3	=> 0
+			)
+		);
+
+		$string = json_encode($array);
+
+		// Test input as string.
+		$rules	= new JRules($string);
+		$this->assertThat(
+			(string) $rules,
+			$this->equalTo($string)
+		);
+
+		// Test input as array.
+		$rules	= new JRules($array);
+		$this->assertThat(
+			(string) $rules,
+			$this->equalTo($string)
+		);
+
+	}
+
+	public function testMergeRule()
+	{
+		$identities = array(
+			-42	=> 1,
+			2	=> 1,
+			3	=> 0
+		);
+
+		$result = array(
+			'edit' => array(
+				-42	=> 1,
+				2	=> 1,
+				3	=> 0
+			)
+		);
+
+		// Construct and empty JRules.
+		$rules = new JRules('');
+		$rules->mergeAction('edit', $identities);
+
+		$this->assertThat(
+			(string) $rules,
+			$this->equalTo(json_encode($result))
+		);
+
+		// Merge a new set, flipping some bits.
+		$identities = array(
+			-42	=> 0,
+			2	=> 1,
+			3	=> 1,
+			4	=> 1
+		);
+
+		// Ident 3 should remain false, 4 should be added.
+		$result = array(
+			'edit' => array(
+				-42	=> 0,
+				2	=> 1,
+				3	=> 0,
+				4	=> 1
+			)
+		);
+
+		$rules->mergeAction('edit', $identities);
+
+		$this->assertThat(
+			(string) $rules,
+			$this->equalTo(json_encode($result))
+		);
+	}
+
+	/**
+	 * This method can merge an array of rules,
+	 * a single JRules object,
+	 * or a JSON rules string.
+	 */
+	public function testMerge()
+	{
+		$array1 = array(
+			'edit' => array(
+				-42	=> 1
+			),
+			'delete' => array(
+				-42	=> 0
+			)
+		);
+		$string1 = json_encode($array1);
+
+		$array2 = array(
+			'create' => array(
+				2	=> 1
+			),
+			'delete' => array(
+				2	=> 0
+			)
+		);
+
+		$result2 = array(
+			'edit' => array(
+				-42	=> 1
+			),
+			'delete' => array(
+				-42	=> 0,
+				2	=> 0
+			),
+			'create' => array(
+				2	=> 1
+			),
+		);
+
+		// Test construction by string
+		$rules1 = new JRules($string1);
+		$this->assertThat(
+			(string) $rules1,
+			$this->equalTo($string1)
+		);
+
+		// Test construction by array.
+		$rules1 = new JRules($array1);
+		$this->assertThat(
+			(string) $rules1,
+			$this->equalTo($string1)
+		);
+
+		// Test merge by JRules.
+		$rules1 = new JRules($array1);
+		$rules2 = new JRules('');
+		$rules2->merge($rules1);
+		$this->assertThat(
+			(string) $rules2,
+			$this->equalTo($string1)
+		);
+
+		$rules1 = new JRules($array1);
+		$rules1->merge($array2);
+		$this->assertThat(
+			(string) $rules1,
+			$this->equalTo(json_encode($result2))
+		);
+
+	}
+
+	function testAllow()
+	{
+		$array1 = array(
+			'edit' => array(
+				-42	=> 1
+			),
+			'delete' => array(
+				-42	=> 0,
+				2	=> 1
+			)
+		);
+
+		$rules = new JRules($array1);
+
+		// Explicit allow.
+		$this->assertTrue(
+			$rules->allow('edit', -42)
+		);
+
+		// Check string or int works.
+		$this->assertTrue(
+			$rules->allow('edit', '-42')
+		);
+
+		// Implicit deny
+		$this->assertNull(
+			$rules->allow('edit', 999)
+		);
+
+		// Explicit deny
+		$this->assertFalse(
+			$rules->allow('delete', -42)
+		);
+
+		// This should be true, implicit deny does not win.
+		$this->assertTrue(
+			$rules->allow('edit', array(-42, 999))
+		);
+
+		// This should be false, explicit deny wins.
+		$this->assertFalse(
+			$rules->allow('delete', array(-42, 2))
+		);
+	}
+
+	function testGetAllowed()
+	{
+		$array1 = array(
+			'create' => array(
+				-42	=> 1
+			),
+			'edit' => array(
+				-42	=> 1
+			),
+			'delete' => array(
+				-42	=> 0,
+				2	=> 1
+			)
+		);
+
+		$result = new JObject;
+		$result->set('create', true);
+		$result->set('edit', true);
+
+		$rules		= new JRules($array1);
+		$allowed	= $rules->getAllowed(-42);
+
+
+		$this->assertThat(
+			$result,
+			$this->equalTo($allowed)
+		);
+	}
+}
