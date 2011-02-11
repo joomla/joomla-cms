01e26880ff6859086d65c5f931ba222987a4bda0
Jean-Marie Simonet <infograf768@gmail.com>
2011-02-11 11:36:45 +0000

    # [#24833] Debug position in beez 20 template doesn't work.
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@20667 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- templates/beez_20/index.php
+++ templates/beez_20/index.php
@@ -251,6 +251,6 @@ $templateparams     = $app->getTemplate(true)->params;
                         </div>
 
                 </div>
-
+				<jdoc:include type="modules" name="debug" />
         </body>
 </html>
