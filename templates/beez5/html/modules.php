bb76c3d77afac2ef70c169ff93148ef1017a3ec5
Jean-Marie Simonet <infograf768@gmail.com>
2011-02-15 05:53:59 +0000

    $# [#24897] Untranslated strings TPL_BEEZ5_ISCLOSED and TPL_BEEZ5_LOGO. Thanks Serge.
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@20699 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- templates/beez5/html/modules.php
+++ templates/beez5/html/modules.php
@@ -46,7 +46,7 @@ function modChrome_beezHide($module, &$params, &$attribs)
 	onclick="auf('module_<?php echo $module->id; ?>'); return false"
 	class="opencloselink" id="link_<?php echo $module->id?>"> <span
 	class="no"><img src="templates/beez_20/images/plus.png"
-	alt="<?php if ($state == 1) { echo JText::_('TPL_BEEZ5_ISOPEN');} else {echo JText::_('TPL_BEEZ5_ISCLOSED');} ?>" />
+	alt="<?php if ($state == 1) { echo JText::_('TPL_BEEZ5_ALTOPEN');} else {echo JText::_('TPL_BEEZ5_ALTCLOSE');} ?>" />
 </span></a></span></span></h<?php echo $headerLevel; ?>> <?php endif; ?>
 <div class="module_content <?php if ($state==1){echo "open";} ?>"
 	id="module_<?php echo $module->id; ?>" tabindex="-1"><?php echo $module->content; ?></div>
