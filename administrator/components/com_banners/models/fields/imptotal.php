5b514ab2725e40d95d401f070f49bffa9e753cc4
Jean-Marie Simonet <infograf768@gmail.com>
2011-02-17 16:26:27 +0000

    # [#24869] Accommodating for longer string value in Banners Edit page. Thanks Bill and Andrea.
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@20742 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- administrator/components/com_banners/models/fields/imptotal.php
+++ administrator/components/com_banners/models/fields/imptotal.php
@@ -40,6 +40,8 @@ class JFormFieldImpTotal extends JFormField
 		$value		= empty($this->value) ? '' : $this->value;
 		$checked	= empty($this->value) ? ' checked="checked"' : '';
 
-		return '<input type="text" name="'.$this->name.'" id="'.$this->id.'" value="'.htmlspecialchars($value, ENT_COMPAT, 'UTF-8').'" '.$class.$onchange.' /><input id="'.$this->id.'_unlimited" type="checkbox"'.$checked.$onclick.' /><input style="border:0;" type="text" value="'.JText::_('COM_BANNERS_UNLIMITED').'" readonly="readonly" id="jform-imp"/>';
+		return '<input type="text" name="'.$this->name.'" id="'.$this->id.'" size="9" value="'.htmlspecialchars($value, ENT_COMPAT, 'UTF-8').'" '.$class.$onchange.' />
+		<fieldset class="checkboxes impunlimited"><input id="'.$this->id.'_unlimited" type="checkbox"'.$checked.$onclick.' />
+		<label for="'.$this->id.'_unlimited" id="jform-imp" type="text">'.JText::_('COM_BANNERS_UNLIMITED').'</label></fieldset>';
 	}
 }
