3a7a68e79fc7ebe6e721e1215fbcec68bc343515
Mark Dexter <dextercowley@gmail.com>
2011-02-18 00:32:08 +0000

    [#24231] JS error notices default template in IE7+8
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@20745 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- templates/beez_20/javascript/hide.js
+++ templates/beez_20/javascript/hide.js
@@ -49,7 +49,7 @@ window.addEvent('domready', function () {
 	}
 
 	if ($('mod-search-searchword')) {
-		$('mod-search-searchword').form.setProperties( {
+		$($('mod-search-searchword').form).set( {
 			role : 'search'
 		});
 	}
