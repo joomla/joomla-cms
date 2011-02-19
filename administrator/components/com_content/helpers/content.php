453d181fd3c910983c52327713df63633896eb21
Jean-Marie Simonet <infograf768@gmail.com>
2011-02-19 19:03:24 +0000

    # [#24545] Stripped code in contents and custom html module. Thanks Amy and Janich.
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@20786 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- administrator/components/com_content/helpers/content.php
+++ administrator/components/com_content/helpers/content.php
@@ -176,28 +176,30 @@ class ContentHelper
 
 		// Unfiltered assumes first priority.
 		if ($unfiltered) {
-			$filter = JFilterInput::getInstance(array(), array(), 1, 1, 0);
+			// Dont apply filtering.
 		}
-		// Black lists take second precedence.
-		else if ($blackList) {
-			// Remove the white-listed attributes from the black-list.
-			$filter = JFilterInput::getInstance(
-				array_diff($blackListTags, $whiteListTags), 			// blacklisted tags
-				array_diff($blackListAttributes, $whiteListAttributes), // blacklisted attributes
-				1,														// blacklist tags
-				1														// blacklist attributes
-			);
-		}
-		// White lists take third precedence.
-		else if ($whiteList) {
-			$filter	= JFilterInput::getInstance($whiteListTags, $whiteListAttributes, 0, 0, 0);  // turn off xss auto clean
-		}
-		// No HTML takes last place.
 		else {
-			$filter = JFilterInput::getInstance();
+			// Black lists take second precedence.
+			if ($blackList) {
+				// Remove the white-listed attributes from the black-list.
+				$filter = JFilterInput::getInstance(
+					array_diff($blackListTags, $whiteListTags), 			// blacklisted tags
+					array_diff($blackListAttributes, $whiteListAttributes), // blacklisted attributes
+					1,														// blacklist tags
+					1														// blacklist attributes
+				);
+			}
+			// White lists take third precedence.
+			else if ($whiteList) {
+				$filter	= JFilterInput::getInstance($whiteListTags, $whiteListAttributes, 0, 0, 0);  // turn off xss auto clean
+			}
+			// No HTML takes last place.
+			else {
+				$filter = JFilterInput::getInstance();
+			}
+			
+			$text = $filter->clean($text, 'html');
 		}
-
-		$text = $filter->clean($text, 'html');
 
 		return $text;
 	}
