8df4adae02c9f022030b9b0394334000ef74f06d
Louis Landry <louis.landry@joomla.org>
2011-03-25 17:03:22 -0700

    Adding scaffolding for a files input class type as well.
    --HG--
    extra : rebase_source : 40018ca6a1276bfda80db7ba338cec46ce6a00f5

---
+++ libraries/joomla/application/input/files.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * @package     Joomla.Platform
+ * @subpackage  Application
+ *
+ * @copyright   Copyright (C) 2005 - 2011 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+
+defined('JPATH_PLATFORM') or die;
+
+/**
+ * Joomla! Input Base Class
+ *
+ * This is an abstracted input class used to manage retrieving data from the application environment.
+ *
+ * @package     Joomla.Platform
+ * @subpackage  Application
+ * @since       11.1
+ */
+class JInputFiles
+{
+	/**
+	 * Constructor.
+	 *
+	 * @param   array  $source   Source data (Optional, default is $_REQUEST)
+	 * @param   array  $options  Array of configuration parameters (Optional)
+	 *
+	 * @return  void
+	 *
+	 * @since   11.1
+	 */
+	public function __construct($source = null, $options = array ())
+	{
+		// If the input classes haven't been registered let's get that done.
+		if (!self::$registered) {
+			self::register();
+			self::$registered = true;
+		}
+
+		if (isset ($options['filter'])) {
+			$this->filter = $options['filter'];
+		} else {
+			$this->filter = JFilterInput::getInstance();
+		}
+
+		if (is_null($source)) {
+			$this->_data = $_REQUEST;
+		} else {
+			$this->_data = $source;
+		}
+
+		// Set the options for the class.
+		$this->options = $options;
+	}
+
+	/**
+	 * Gets a value from the input data.
+	 *
+	 * @param   string  $name     Name of the value to get.
+	 * @param   mixed   $default  Default value to return if variable does not exist.
+	 * @param   string  $filter   Filter to apply to the value.
+	 * 
+	 * @return  mixed  The filtered input value.
+	 * 
+	 * @since   11.1
+	 */
+	public function get($name, $default, $filter = 'cmd')
+	{
+		if (isset ($this->_data[$name])) {
+			return $this->filter->clean($this->_data[$name], $filter);
+		}
+
+		foreach ($this->_inputs AS $input)
+		{
+			$return = $input->get($name, $default, $filter);
+
+			if ($return != null) {
+				return $return;
+			}
+		}
+		
+		return $default;
+	}
+
+	/**
+	 * Sets a value
+	 *
+	 * @param   string  $name   Name of the value to set.
+	 * @param   mixed   $value  Value to assign to the input.
+	 * 
+	 * @return  void
+	 * 
+	 * @since   11.1
+	 */
+	public function set($name, $value)
+	{
+		$this->_data[$name] = $value;
+	}
+}
