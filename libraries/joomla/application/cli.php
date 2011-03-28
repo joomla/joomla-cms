347f5c2227992b3de65056f00a1ef7428d3b8ea6
Louis Landry <louis.landry@joomla.org>
2011-03-27 22:13:57 -0700

    Adding basic JCli class.

---
+++ libraries/joomla/application/cli.php
@@ -0,0 +1,264 @@
+<?php
+/**
+ * @package     Joomla.Platform
+ * @subpackage  Application
+ *
+ * @copyright   Copyright (C) 2005 - 2011 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+
+defined('JPATH_PLATFORM') or die;
+
+jimport('joomla.application.input.cli');
+jimport('joomla.event.dispatcher');
+jimport('joomla.log.log');
+jimport('joomla.registry.registry');
+
+/**
+ * Base class for a Joomla! command line application.
+ *
+ * @package     Joomla.Platform
+ * @subpackage  Application
+ * @since       11.1
+ */
+class JCli
+{
+	/**
+	 * The name of the program.
+	 *
+	 * @var    string
+	 * @since  11.1
+	 */
+	protected $name;
+
+	/**
+	 * The application configuration object.
+	 *
+	 * @var    JRegistry
+	 * @since  11.1
+	 */
+	protected $config;
+
+	/**
+	 * The application input object.
+	 *
+	 * @var		JInput
+	 * @since   11.1
+	 */
+	public $input;
+
+	/**
+	 * Class constructor.
+	 *
+	 * @param   array  A configuration array.
+	 *
+	 * @return  void
+	 *
+	 * @since   11.1
+	 */
+	protected function __construct($config = array())
+	{
+		// Close the application if we are not executed from the command line.
+		if (!defined(STDOUT) || !defined(STDIN) || !isset($_SERVER['argv'])) {
+			$this->close();
+		}
+
+		// Set the site object name.
+		$this->name = isset($config['name']) ? $config['name'] : 'Joomla CLI';
+
+		// Get the command line options
+		$this->input = new JInputCli();
+
+		// Create the registry with a default namespace of config
+		$this->config = new JRegistry();
+
+		// Set the configuration file name.
+		$config['configFile'] = isset($config['configFile']) ? $config['configFile'] : JPATH_APPLICATION.'/configuration.php';
+
+		// Load the configuration object.
+		$this->loadConfiguration($config['configFile']);
+
+		// Set the execution datetime and timestamp;
+		$this->set('execution.datetime', gmdate('Y-m-d H:i:s'));
+		$this->set('execution.timestamp', time());
+
+		// Set the current directory.
+		$this->set('cwd', getcwd());
+	}
+
+	/**
+	 * Returns a reference to the global JCli object, only creating it if it
+	 * doesn't already exist.
+	 *
+	 * This method must be invoked as:
+	 * 		<pre>$cli = JCli::getInstance();</pre>
+	 *
+	 * @param   mixed  The site object name.
+	 * @param   array  An optional associative array of configuration settings.
+	 *
+	 * @return  JCli
+	 *
+	 * @since   11.1
+	 */
+	public static function getInstance($name = 'Joomla CLI', $config = array())
+	{
+		// Initialize the static array.
+		static $instances;
+		if (!isset($instances)) {
+			$instances = array();
+		}
+
+		// Only create the object if it doesn't exist.
+		if (empty($instances[$name]))
+		{
+			// If no name is explicitly set, use the instance name.
+			$config['name'] = isset($config['name']) ? $config['name'] : $name;
+			$instances[$name] = new JCli($config);
+		}
+
+		return $instances[$name];
+	}
+
+	/**
+	 * Execute the application.
+	 *
+	 * @return  boolean  True on success.
+	 *
+	 * @since   11.1
+	 */
+	public function execute()
+	{
+		return true;
+	}
+
+	/**
+	 * Exit the application.
+	 *
+	 * @param   integer  Exit code.
+	 *
+	 * @return  void
+	 *
+	 * @since   11.1
+	 */
+	public function close($code = 0)
+	{
+		exit($code);
+	}
+
+	/**
+	 * Write a string to stndard output.
+	 *
+	 * @param   string  The text to display.
+	 *
+	 * @return  void
+	 *
+	 * @since   11.1
+	 */
+	public function out($text = '', $nl = true)
+	{
+		fwrite(STDOUT, $text.($nl ? "\n" : null));
+	}
+
+	/**
+	 * Get a value from standard input.
+	 *
+	 * @return  string  The input string from standard input.
+	 *
+	 * @since   11.1
+	 */
+	public function in()
+	{
+		return rtrim(fread(STDIN, 8192), "\n");
+	}
+
+	/**
+	 * Registers a handler to a particular event group.
+	 *
+	 * @param   string  The event name.
+	 * @param   mixed   The handler, a function or an instance of a event object.
+	 *
+	 * @return  void
+	 *
+	 * @since   11.1
+	 */
+	function registerEvent($event, $handler)
+	{
+		JDispatcher::getInstance()->register($event, $handler);
+	}
+
+	/**
+	 * Calls all handlers associated with an event group.
+	 *
+	 * @param   string  The event name.
+	 * @param   array   An array of arguments.
+	 *
+	 * @return  array   An array of results from each function call.
+	 *
+	 * @since   11.1
+	 */
+	function triggerEvent($event, $args=null)
+	{
+		return JDispatcher::getInstance()->trigger($event, $args);
+	}
+
+	/**
+	 * Returns a property of the object or the default value if the property is not set.
+	 *
+	 * @param   string  The name of the property
+	 * @param   mixed   The default value if none is set.
+	 *
+	 * @return  mixed   The value of the configuration.
+	 *
+	 * @since   11.1
+ 	 */
+	public function get($key, $default=null)
+	{
+		return $this->config->get($key, $default);
+	}
+
+	/**
+	 * Modifies a property of the object, creating it if it does not already exist.
+	 *
+	 * @param   string  The name of the property
+	 * @param   mixed   The value of the property to set
+	 *
+	 * @return  mixed   Previous value of the property
+	 *
+	 * @since   11.1
+	 */
+	public function set($key, $value = null)
+	{
+		$previous = $this->config->get($key);
+		$this->config->set($key, $value);
+		return $previous;
+	}
+
+	/**
+	 * Load the configuration file into the site object.
+	 *
+	 * @param   string   The path to the configuration file.
+	 *
+	 * @return  boolean  True on success.
+	 *
+	 * @since   11.1
+	 */
+	protected function loadConfiguration($file)
+	{
+		// Import the configuration file.
+		if (!is_file($file)) {
+			return false;
+		}
+		require_once $file;
+
+		// Instantiate the configuration object.
+		if (!class_exists('JConfig')) {
+			return false;
+		}
+		$config = new JConfig();
+
+		// Load the configuration values into the registry
+		$this->config->loadObject($config);
+
+		return true;
+	}
+}
