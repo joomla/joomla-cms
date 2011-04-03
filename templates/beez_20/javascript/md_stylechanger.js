f20ee48f02fd4090fb578cf58a868f8ddbcbd92a
Mark Dexter <dextercowley@gmail.com>
2011-04-03 00:52:53 +0000

    [#25403] "Font Size" not working with Internet Explorer 7
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@21055 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- templates/beez_20/javascript/md_stylechanger.js
+++ templates/beez_20/javascript/md_stylechanger.js
@@ -2,10 +2,19 @@
 var prefsLoaded = false;
 var defaultFontSize = 100;
 var currentFontSize = defaultFontSize;
+var fontSizeTitle;
+var bigger;
+var smaller;
+var reset;
+var biggerTitle;
+var smallerTitle;
+var resetTitle;
 
-function supportsLocalStorage() {
-	return ('localStorage' in window) && window.localStorage !== null;
-}
+Object.append(Browser.Features, {
+	localstorage: (function() {
+		return ('localStorage' in window) && window.localStorage !== null;
+	})()
+});
 
 function setFontSize(fontSize) {
 	document.body.style.fontSize = fontSize + '%';
@@ -27,7 +36,7 @@ function revertStyles() {
 }
 
 function writeFontSize(value) {
-	if (supportsLocalStorage()) {
+	if (Browser.Features.localstorage) {
 		localStorage.fontSize = value;
 	} else {
 		Cookie.write("fontSize", value, {duration: 180});
@@ -35,7 +44,7 @@ function writeFontSize(value) {
 }
 
 function readFontSize() {
-	if (supportsLocalStorage()) {
+	if (Browser.Features.localstorage) {
 		return localStorage.fontSize;
 	} else {
 		return Cookie.read("fontSize");
@@ -52,55 +61,9 @@ function setUserOptions() {
 }
 
 function addControls() {
-	var xhtml = "http://www.w3.org/1999/xhtml";
 	var container = document.id('fontsize');
-
-	var link = [];
-	var linkClass = ["larger", "reset", "smaller"];
-	var linkTitle = [biggerTitle, resetTitle, smallerTitle];
-	var linkAction = ["changeFontSize(2); return false;", "revertStyles(); return false;", "changeFontSize(-2); return false;"];
-	var linkContent = [document.createTextNode(bigger), document.createTextNode(reset), document.createTextNode(smaller)];
-
-	var headingText = document.createTextNode(fontSizeTitle);
-
-	var heading;
-	var p;
-	var span;
-	if (document.createElementNS) {
-		heading = document.createElementNS(xhtml, "h3");
-		p = document.createElementNS(xhtml, "p");
-		link[3] = document.createElementNS(xhtml, "a");
-		span = document.createElementNS(xhtml, "span");
-	} else {
-		heading = document.createElement("h3");
-		p = document.createElement("p");
-		link[3] = document.createElement("a");
-		span = document.createElement("span");
-	}
-
-	p.setAttribute("class", "fontsize");
-	span.setAttribute("class", "unseen");
-	link[3].setAttribute("href", "index.php");
-
-	for (var x = 0; x < 3; x++) {
-		link[x] = link[3].cloneNode(true);
-		link[x].setAttribute("class", linkClass[x]);
-		link[x].setAttribute("title", linkTitle[x]);
-		link[x].setAttribute("onclick", linkAction[x]);
-		link[x].appendChild(linkContent[x]);
-	}
-
-	span.appendChild(document.createTextNode('\u00a0')); //This is a nbsp
-	heading.appendChild(headingText);
-
-	p.appendChild(link[0]);
-	p.appendChild(span.cloneNode(true));
-	p.appendChild(link[1]);
-	p.appendChild(link[2]);
-	p.appendChild(span);
-
-	container.appendChild(heading);
-	container.appendChild(p);
+	var content = '<h3>'+ fontSizeTitle +'</h3><p><a title="'+ biggerTitle +'"  href="#" onclick="changeFontSize(2); return false">'+ bigger +'</a><span class="unseen">.</span><a href="#" title="'+resetTitle+'" onclick="revertStyles(); return false">'+ reset +'</a><span class="unseen">.</span><a href="#"  title="'+ smallerTitle +'" onclick="changeFontSize(-2); return false">'+ smaller +'</a></p>';
+	container.set('html', content);
 }
 
 function saveSettings() {
