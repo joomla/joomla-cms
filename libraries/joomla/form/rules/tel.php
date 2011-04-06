e2050a98fdda344a86095e6957b06f918391631f
elin <elin.waring@gmail.com>
2011-04-06 12:20:41 -0400

    fixing tel rule and tel tests

--- libraries/joomla/form/rules/tel.php
+++ libraries/joomla/form/rules/tel.php
@@ -3,7 +3,7 @@
  * @copyright	Copyright (C) 2005 - 2011 Open Source Matters, Inc. All rights reserved.
  */
 
-defined('JOOMLA_PLATFORM') or die;
+//defined('JOOMLA_PLATFORM') or die;
 
 jimport('joomla.form.formrule');
 
@@ -49,24 +49,39 @@ class JFormRuleTel extends JFormRule
 		//Regex by Steve Levithan
 		//see http://blog.stevenlevithan.com/archives/validate-phone-number
 		$regexarray = array(
-			NANP => '/^(?:\+?1[-. ]?)?\(?([2-9][0-8][0-9])\)?[-. ]?([2-9][0-9]{2})[-. ]?([0-9]{4})$/',
-			ITU-T=> '/^\+(?:[0-9] ?){6,14}[0-9]$/',
-			EPP => '/^\+[0-9]{1,3}\.[0-9]{4,14}(?:x.+)?$/'
+			'NANP' => '/^(?:\+?1[-. ]?)?\(?([2-9][0-8][0-9])\)?[-. ]?([2-9][0-9]{2})[-. ]?([0-9]{4})$/',
+			'ITU-T'=> '/^\+(?:[0-9] ?){6,14}[0-9]$/',
+			'EPP' => '/^\+[0-9]{1,3}\.[0-9]{4,14}(?:x.+)?$/'
 		);
-		if ($plan =='northamerica' || $plan == 'us' ) {
-			$plan = 'NANP';
-		} else
-		 if ( $plan == 'International' || $plan == 'int' || $plan == 'missdn' || !$plan) {
-		 	$plan = 'ITU-T';
-		 } else 
-		 if ( $plan == 'IETF') {
-		 	$plan='EPP';
-		 }
-		$plan = (string) $element['plan'];
-		$regex = (string) $regexarray[$plan];
-		// Test the value against the regular expression.
-		if (preg_match($regex, $value) == false) {
-			return false;
+		if (isset($element['plan'])){
+		
+			$plan = (string) $element['plan'];
+			if ($plan =='northamerica' || $plan == 'us' ) {
+				$plan = 'NANP';
+			} else
+			 if ( $plan == 'International' || $plan == 'int' || $plan == 'missdn' || !$plan) {
+			 	$plan = 'ITU-T';
+			 } else 
+			 if ( $plan == 'IETF') {
+			 	$plan='EPP';
+			 }
+	
+			$regex = $regexarray[$plan];
+			// Test the value against the regular expression.
+			
+			if (preg_match($regex, $value) == false) {
+				return false;
+			}
+		} else {
+			//If the rule is set but no plan is selected just check that there are betwen 
+			//7 and 15 digits inclusive. 
+			$cleanvalue = preg_replace('/[+. -(\)]/','',$value);
+			$regex = '/^[0-9]{7,15}?$/';
+			if (preg_match($regex, $cleanvalue) == true) {
+				return true;
+			} else {
+				return false;
+			}
 		}			
 		return true;	
 	}
---
+++ tests/suite/joomla/form/rules/JFormRuleTelTest.php
@@ -0,0 +1,279 @@
+<?php
+/**
+ * @copyright   Copyright (C) 2005 - 2011 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ * @package     Joomla.UnitTest
+ */
+
+defined('JPATH_PLATFORM') or die;
+
+/**
+ * Test class for JForm.
+ *
+ * @package		Joomla.UnitTest
+ * @subpackage	Form
+ *
+ */
+class JFormRuleTelTest extends JoomlaTestCase
+{
+	/**
+	 * set up for testing
+	 *
+	 * @return void
+	 */
+	public function setUp()
+	{
+		jimport('joomla.form.formrule');
+		jimport('joomla.utilities.xmlelement');
+		require_once JPATH_PLATFORM.'/joomla/form/rules/tel.php';
+	}
+
+	/**
+	 * Test the JFormRuleTel::test method.
+	 */
+	public function testTel()
+	{
+		// Initialise variables.
+
+		$rule = new JFormRuleTel;
+		$xml = simplexml_load_string('<form><field name="tel1" plan="NANP" />
+			<field name="tel2" plan="ITU-T" /><field name="tel3" plan="EPP" />
+			<field name="tel4" /></form>', 
+			'JXMLElement');
+
+		// Test fail conditions NANP.
+		$this->assertThat(
+			$rule->test($xml->field[0], 'bogus'),
+			$this->isFalse(),
+			'Line:'.__LINE__.' The rule should fail and return false.'
+		);
+		$this->assertThat(
+			$rule->test($xml->field[0], '123451234512'),
+			$this->isFalse(),
+			'Line:'.__LINE__.' The rule should fail and return false.'
+		);	
+		$this->assertThat(
+			$rule->test($xml->field[0], 'anything_5555555555'),
+			$this->isFalse(),
+			'Line:'.__LINE__.' The rule should fail and return false.'
+		);				
+		$this->assertThat(
+			$rule->test($xml->field[0], '5555555555_anything'),
+			$this->isFalse(),
+			'Line:'.__LINE__.' The rule should fail and return false.'
+		);				
+
+		// Test fail conditions ITU-T.
+		$this->assertThat(
+			$rule->test($xml->field[1], 'bogus'),
+			$this->isFalse(),
+			'Line:'.__LINE__.' The rule should fail and return false.'
+		);
+		$this->assertThat(
+			$rule->test($xml->field[1], '123451234512'),
+			$this->isFalse(),
+			'Line:'.__LINE__.' The rule should fail and return false.'
+		);	
+		$this->assertThat(
+			$rule->test($xml->field[1], 'anything_5555555555'),
+			$this->isFalse(),
+			'Line:'.__LINE__.' The rule should fail and return false.'
+		);				
+		$this->assertThat(
+			$rule->test($xml->field[1], '5555555555_anything'),
+			$this->isFalse(),
+			'Line:'.__LINE__.' The rule should fail and return false.'
+		);				
+		$this->assertThat(
+			$rule->test($xml->field[1], '1 2 3 4 5 6 '),
+			$this->isFalse(),
+			'Line:'.__LINE__.' The rule should fail and return false.'
+		);				
+		$this->assertThat(
+			$rule->test($xml->field[1], '5552345678'),
+			$this->isFalse(),
+			'Line:'.__LINE__.' The rule should fail and return false.'
+		);					
+		$this->assertThat(
+			$rule->test($xml->field[1], 'anything_555.5555555'),
+			$this->isFalse(),
+			'Line:'.__LINE__.' The rule should fail and return false.'
+		);				
+		$this->assertThat(
+			$rule->test($xml->field[1], '555.5555555_anything'),
+			$this->isFalse(),
+			'Line:'.__LINE__.' The rule should fail and return false.'
+		);				
+		
+		// Test fail conditions EPP.
+		$this->assertThat(
+			$rule->test($xml->field[2], 'bogus'),
+			$this->isFalse(),
+			'Line:'.__LINE__.' The rule should fail and return false.'
+		);
+		$this->assertThat(
+			$rule->test($xml->field[2], '12345123451234512345'),
+			$this->isFalse(),
+			'Line:'.__LINE__.' The rule should fail and return false.'
+		);	
+		$this->assertThat(
+			$rule->test($xml->field[2], '123.1234'),
+			$this->isFalse(),
+			'Line:'.__LINE__.' The rule should fail and return false.'
+		);			
+		$this->assertThat(
+			$rule->test($xml->field[2], '23.1234'),
+			$this->isFalse(),
+			'Line:'.__LINE__.' The rule should fail and return false.'
+		);	
+		$this->assertThat(
+			$rule->test($xml->field[2], '3.1234'),
+			$this->isFalse(),
+			'Line:'.__LINE__.' The rule should fail and return false.'
+		);		
+		// Test fail conditions no plan.
+		$this->assertThat(
+			$rule->test($xml->field[3], 'bogus'),
+			$this->isFalse(),
+			'Line:'.__LINE__.' The rule should fail and return false.'
+		);
+
+		$this->assertThat(
+			$rule->test($xml->field[3], 'anything_555.5555555'),
+			$this->isFalse(),
+			'Line:'.__LINE__.' The rule should fail and return false.'
+		);				
+		$this->assertThat(
+			$rule->test($xml->field[3], '555.5555555x555_anything'),
+			$this->isFalse(),
+			'Line:'.__LINE__.' The rule should fail and return false.'
+		);				
+		$this->assertThat(
+			$rule->test($xml->field[3], '.5555555'),
+			$this->isTrue(),
+			'Line:'.__LINE__.' The rule should pass and return true.'
+		);			
+		$this->assertThat(
+			$rule->test($xml->field[3], '555.'),
+			$this->isFalse(),
+			'Line:'.__LINE__.' The rule should fail and return false.'
+		);				
+		$this->assertThat(
+			$rule->test($xml->field[3], '1 2 3 4 5 6 '),
+			$this->isFalse(),
+			'Line:'.__LINE__.' The rule should fail and return false.'
+		);			
+		// Test pass conditions.
+		//For NANP
+		$this->assertThat(
+			$rule->test($xml->field[0], '(555) 234-5678'),
+			$this->isTrue(),
+			'Line:'.__LINE__.' The rule should pass and return true.'
+		);				
+		$this->assertThat(
+			$rule->test($xml->field[0], '1-555-234-5678'),
+			$this->isTrue(),
+			'Line:'.__LINE__.' The rule should pass and return true.'
+		);			
+		$this->assertThat(
+			$rule->test($xml->field[0], '+1-555-234-5678'),
+			$this->isTrue(),
+			'Line:'.__LINE__.' The rule should pass and return true.'
+		);	
+		$this->assertThat(
+			$rule->test($xml->field[0], '555-234-5678'),
+			$this->isTrue(),
+			'Line:'.__LINE__.' The rule should pass and return true.'
+		);					
+		$this->assertThat(
+			$rule->test($xml->field[0], '1-555-234-5678'),
+			$this->isTrue(),
+			'Line:'.__LINE__.' The rule should pass and return true.'
+		);					
+		$this->assertThat(
+			$rule->test($xml->field[0], '1 555 234 5678'),
+			$this->isTrue(),
+			'Line:'.__LINE__.' The rule should pass and return true.'
+		);							
+		//For ITU-T
+		$this->assertThat(
+			$rule->test($xml->field[1], '+555 234 5678'),
+			$this->isTrue(),
+			'Line:'.__LINE__.' The rule should pass and return true.'
+		);				
+		$this->assertThat(
+			$rule->test($xml->field[1], '+123 555 234 5678'),
+			$this->isTrue(),
+			'Line:'.__LINE__.' The rule should pass and return true.'
+		);			
+		$this->assertThat(
+			$rule->test($xml->field[1], '+2 52 34 55'),
+			$this->isTrue(),
+			'Line:'.__LINE__.' The rule should pass and return true.'
+		);	
+		$this->assertThat(
+			$rule->test($xml->field[1], '+5552345678'),
+			$this->isTrue(),
+			'Line:'.__LINE__.' The rule should pass and return true.'
+		);					
+		
+		//For EPP
+		$this->assertThat(
+			$rule->test($xml->field[2], '+123.1234'),
+			$this->isTrue(),
+			'Line:'.__LINE__.' The rule should pass and return true.'
+		);			
+		$this->assertThat(
+			$rule->test($xml->field[2], '+23.1234'),
+			$this->isTrue(),
+			'Line:'.__LINE__.' The rule should pass and return true.'
+		);	
+		$this->assertThat(
+			$rule->test($xml->field[2], '+3.1234'),
+			$this->isTrue(),
+			'Line:'.__LINE__.' The rule should pass and return true.'
+		);		
+		$this->assertThat(
+			$rule->test($xml->field[2], '+3.1234x555'),
+			$this->isTrue(),
+			'Line:'.__LINE__.' The rule should pass and return true.'
+		);						
+					
+		//For no plan
+		$this->assertThat(
+			$rule->test($xml->field[3], '555 234 5678'),
+			$this->isTrue(),
+			'Line:'.__LINE__.' The rule should pass and return true.'
+		);				
+		$this->assertThat(
+			$rule->test($xml->field[3], '+123 555 234 5678'),
+			$this->isTrue(),
+			'Line:'.__LINE__.' The rule should pass and return true.'
+		);			
+		$this->assertThat(
+			$rule->test($xml->field[3], '+2 52 34 55'),
+			$this->isTrue(),
+			'Line:'.__LINE__.' The rule should pass and return true.'
+		);	
+		$this->assertThat(
+			$rule->test($xml->field[3], '5552345678'),
+			$this->isTrue(),
+			'Line:'.__LINE__.' The rule should pass and return true.'
+		);					
+		$this->assertThat(
+			$rule->test($xml->field[3], '+5552345678'),
+			$this->isTrue(),
+			'Line:'.__LINE__.' The rule should pass and return true.'
+		);					
+		$this->assertThat(
+			$rule->test($xml->field[3], '1 2 3 4 5 6 7'),
+			$this->isTrue(),
+			'Line:'.__LINE__.' The rule should pass and return true.'
+		);				
+		$this->assertThat(
+			$rule->test($xml->field[3], '123451234512'),
+			$this->isTrue(),
+			'Line:'.__LINE__.' The rule should pass and return true.'
+		);							
+	}
+}
