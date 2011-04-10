dad0c99ea4fea3d4e8fd58c7bc593bcd65adc5fd
Jean-Marie Simonet <infograf768@gmail.com>
2011-04-10 16:18:21 +0000

    $ Adding lib_joomla.ini into site language folder and localise.php in admin language folder to prevent confusion for TTs.
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@21133 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

---
+++ administrator/language/en-GB/en-GB.localise.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * @version		$Id: language.php 15628 2010-03-27 05:20:29Z infograf768 $
+ * @copyright	Copyright (C) 2005 - 2011 Open Source Matters, Inc. All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+/**
+ * en-GB localise class
+ *
+ * @package		Joomla.Site
+ * @since		1.6
+ */
+abstract class en_GBLocalise {
+	/**
+	 * Returns the potential suffixes for a specific number of items
+	 *
+	 * @param	int $count  The number of items.
+	 * @return	array  An array of potential suffixes.
+	 * @since	1.6
+	 */
+	public static function getPluralSuffixes($count) {
+		if ($count == 0) {
+			$return =  array('0');
+		}
+		elseif($count == 1) {
+			$return =  array('1');
+		}
+		else {
+			$return = array('MORE');
+		}
+		return $return;
+	}
+	/**
+	 * Returns the ignored search words
+	 *
+	 * @return	array  An array of ignored search words.
+	 * @since	1.6
+	 */
+	public static function getIgnoredSearchWords() {
+		$search_ignore = array();
+		$search_ignore[] = "and";
+		$search_ignore[] = "in";
+		$search_ignore[] = "on";
+		return $search_ignore;
+	}
+	/**
+	 * Returns the lower length limit of search words
+	 *
+	 * @return	integer  The lower length limit of search words.
+	 * @since	1.6
+	 */
+	public static function getLowerLimitSearchWord() {
+		return 3;
+	}
+	/**
+	 * Returns the upper length limit of search words
+	 *
+	 * @return	integer  The upper length limit of search words.
+	 * @since	1.6
+	 */
+	public static function getUpperLimitSearchWord() {
+		return 20;
+	}
+	/**
+	 * Returns the number of chars to display when searching
+	 *
+	 * @return	integer  The number of chars to display when searching.
+	 * @since	1.6
+	 */
+	public static function getSearchDisplayedCharactersNumber() {
+		return 200;
+	}
+}
+
