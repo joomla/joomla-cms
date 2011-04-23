1c4218562587ea4a73d8cbb723500eb331dd68de
Jean-Marie Simonet <infograf768@gmail.com>
2011-04-23 05:44:08 +0000

    #$ [#25724] *Improving the langswitcher module, correcting some ini strings
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@21184 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- modules/mod_languages/tmpl/default.php
+++ modules/mod_languages/tmpl/default.php
@@ -15,21 +15,33 @@ JHtml::_('stylesheet', 'mod_languages/template.css', array(), true);
 <?php if ($headerText) : ?>
 	<div class="pretext"><p><?php echo $headerText; ?></p></div>
 <?php endif; ?>
+
+<?php if ($params->get('dropdown',1)) : ?>
+	<form name="lang" method="submit">
+	<select class="inputbox" onchange="document.location.replace(this.value);" >
+	<?php foreach($list as $language):?>
+		<option value="<?php echo $language->link;?>" <?php echo $language->active ? 'selected="selected"' : ''?>><?php echo $language->title_native;?></option>
+	<?php endforeach; ?>
+	</select>
+	</form>
+<?php else : ?>
 	<ul class="<?php echo $params->get('inline', 1) ? 'lang-inline' : 'lang-block';?>">
-<?php foreach($list as $language):?>
-	<?php if ($params->get('show_active', 0) || !$language->active):?>
-		<li class="<?php echo $language->active ? 'lang-active' : '';?>">
-		<a href="<?php echo $language->link;?>">
-		<?php if ($params->get('image', 1)):?>
-			<?php echo JHtml::_('image', 'mod_languages/'.$language->image.'.gif', $language->title, array('title'=>$language->title), true);?>
-		<?php else:?>
-			<?php echo $language->title;?>
-		<?php endif;?>
-				</a>
+	<?php foreach($list as $language):?>
+		<?php if ($params->get('show_active', 0) || !$language->active):?>
+			<li class="<?php echo $language->active ? 'lang-active' : '';?>">
+			<a href="<?php echo $language->link;?>">
+			<?php if ($params->get('image', 1)):?>
+				<?php echo JHtml::_('image', 'mod_languages/'.$language->image.'.gif', $language->title_native, array('title'=>$language->title_native), true);?>
+			<?php else : ?>
+				<?php echo $params->get('full_name', 1) ? $language->title_native : strtoupper($language->sef);?>
+			<?php endif; ?>
+			</a>
 			</li>
-	<?php endif;?>
-<?php endforeach;?>
+		<?php endif;?>
+	<?php endforeach;?>
 	</ul>
+<?php endif; ?>
+
 <?php if ($footerText) : ?>
 	<div class="posttext"><p><?php echo $footerText; ?></p></div>
 <?php endif; ?>
