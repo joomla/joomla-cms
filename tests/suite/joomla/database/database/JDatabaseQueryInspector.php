1f3b336235c79ccffff81384aacc79d8969f8172
Andrew Eddie <andrew.eddie@newlifeinit.com>
2011-05-08 23:59:53 -0500

    Add new inspector classes to help test JDatabaseQuery and JDatabaseQueryElement.

---
+++ tests/suite/joomla/database/database/JDatabaseQueryInspector.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * @copyright  Copyright (C) 2005 - 2011 Open Source Matters. All rights reserved.
+ * @license    GNU General Public License
+ */
+
+require_once JPATH_PLATFORM.'/joomla/database/databasequery.php';
+
+/**
+ * Class to expose protected properties and methods in JDatabaseQueryExporter for testing purposes.
+ *
+ * @package    Joomla.UnitTest
+ * @subpackage Database
+ */
+class JDatabaseQueryInspector extends JDatabaseQuery
+{
+	/**
+	 * Gets any property from the class.
+	 *
+	 * @param   string  $property  The name of the class property.
+	 *
+	 * @return  mixed   The value of the class property.
+	 * @since   11.1
+	 */
+	public function __get($property)
+	{
+		return $this->$property;
+	}
+
+	/**
+	 * Sets any property from the class.
+	 *
+	 * @param   string  $property  The name of the class property.
+	 * @param   string  $value     The value of the class property.
+	 *
+	 * @return  void
+	 * @since   11.1
+	 */
+	public function __set($property, $value)
+	{
+		return $this->$property = $value;
+	}
+
+	/**
+	 * Dummy method to just return the text.
+	 *
+	 * @param   string  The string to be escaped.
+	 * @param   bool    Optional parameter to provide extra escaping.
+	 *
+	 * @return  string  The escaped string.
+	 *
+	 * @since   11.1
+	 */
+	public function escape($text, $extra = false)
+	{
+		return $text;
+	}
+}
