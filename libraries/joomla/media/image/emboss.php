e5aa955e7f38384b01ae787aa8fc1c292bfc1a75
Rune V. Sjøen <rvsjoen@gmail.com>
2011-05-15 19:38:37 +0200

    Add emboss and negate filters
    Change-Id: I69c3f749d5e13e2ed4d1b031eb99a966277f6dbc

---
+++ libraries/joomla/media/image/emboss.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @package     Joomla.Platform
+ * @subpackage  Media
+ *
+ * @copyright   Copyright (C) 2005 - 2011 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+
+defined('JPATH_PLATFORM') or die;
+
+jimport('joomla.media.imagefilter');
+
+/**
+ * Image Filter class to emboss an image.
+ *
+ * @package     Joomla.Platform
+ * @subpackage  Media
+ * @since       11.1
+ */
+class JImageFilterEmboss extends JImageFilter
+{
+	/**
+	 * Method to apply a filter to an image resource.
+	 *
+	 * @param   resource  The image resource on which to apply the filter.
+	 *
+	 * @return  void
+	 *
+	 * @since   11.1
+	 */
+	function execute($handle)
+	{
+		// Make sure the file handle is valid.
+		if ((!is_resource($handle) || get_resource_type($handle) != 'gd')) {
+			JLog::add('The image is invalid.', JLog::ERROR);
+			throw new MediaException();
+		}
+
+		imagefilter($handle, IMG_FILTER_EMBOSS);
+	}
+}
