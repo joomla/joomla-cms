a47709f430fc42fc07a9ea6ecfe507c4f388a3cb
Rouven Weßling <me@rouvenwessling.de>
2011-05-20 17:36:48 +0200

    Modify JDocumentRendererMessage to always render <div id="system-message-container"></div>. This makes it possible to render messages via JavaScript.
    Added Joomla.renderMessages() to core.js. This functions enabled rendering messages via JavaScript.

--- libraries/joomla/document/html/renderer/message.php
+++ libraries/joomla/document/html/renderer/message.php
@@ -36,8 +36,7 @@ class JDocumentRendererMessage extends JDocumentRenderer
 		$messages = JFactory::getApplication()->getMessageQueue();
 
 		// Build the sorted message list
-		if (is_array($messages) && count($messages))
-		{
+		if (is_array($messages) && !empty($messages)) {
 			foreach ($messages as $msg)
 			{
 				if (isset($msg['type']) && isset($msg['message'])) {
@@ -46,19 +45,20 @@ class JDocumentRendererMessage extends JDocumentRenderer
 			}
 		}
 
+		// Build the return string
+		$buffer .= "\n<div id=\"system-message-container\">";
+
 		// If messages exist render them
-		if (is_array($lists))
-		{
-			// Build the return string
+		if (is_array($lists)) {
 			$buffer .= "\n<dl id=\"system-message\">";
 			foreach ($lists as $type => $msgs)
 			{
-			if (count($msgs))
-				{
+				if (count($msgs)) {
 					$buffer .= "\n<dt class=\"".strtolower($type)."\">".JText::_($type)."</dt>";
 					$buffer .= "\n<dd class=\"".strtolower($type)." message\">";
 					$buffer .= "\n\t<ul>";
-					foreach ($msgs as $msg) {
+					foreach ($msgs as $msg)
+					{
 						$buffer .="\n\t\t<li>".$msg."</li>";
 					}
 					$buffer .= "\n\t</ul>";
@@ -67,6 +67,8 @@ class JDocumentRendererMessage extends JDocumentRenderer
 			}
 			$buffer .= "\n</dl>";
 		}
+
+		$buffer .= "\n</div>";
 		return $buffer;
 	}
 }
