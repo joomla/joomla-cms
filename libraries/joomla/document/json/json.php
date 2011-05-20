55b7c4a534e83b4a5f7eda1a4321739827021b93
Rouven Weßling <me@rouvenwessling.de>
2011-05-20 23:15:05 +0200

    Remove getHeadData(), setHeadData() and mergeHeadData() from JDocument since it only applies to JDocumentHTML.

--- libraries/joomla/document/json/json.php
+++ libraries/joomla/document/json/json.php
@@ -63,24 +63,6 @@ class JDocumentJSON extends JDocument
 	}
 
 	/**
-	 * Get the document head data
-	 *
-	 * @return  array  The document head data in array form
-	 */
-	public function getHeadData()
-	{
-	}
-
-	/**
-	 * Set the document head data
-	 *
-	 * @param   array  $data  The document head data in array form
-	 */
-	public function setHeadData($data)
-	{
-	}
-
-	/**
 	 * Returns the document name
 	 *
 	 * @return  string
