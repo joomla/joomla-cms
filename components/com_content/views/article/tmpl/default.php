c6627cc761f8fb16639a2916572dab40b22eedcf
Mark Dexter <dextercowley@gmail.com>
2011-05-31 01:09:02 +0000

    [#25988] Fix missing author contact itemid in com_content
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@21407 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- components/com_content/views/article/tmpl/default.php
+++ components/com_content/views/article/tmpl/default.php
@@ -117,14 +117,15 @@ endif; ?>
 <?php endif; ?>
 <?php if ($params->get('show_author') && !empty($this->item->author )) : ?>
 	<dd class="createdby">
-	<?php $author =  $this->item->author; ?>
-	<?php $author = ($this->item->created_by_alias ? $this->item->created_by_alias : $author);?>
-
-	<?php if (!empty($this->item->contactid ) &&  $params->get('link_author') == true):?>
-		<?php 	echo JText::sprintf('COM_CONTENT_WRITTEN_BY' ,
-		 JHtml::_('link',JRoute::_('index.php?option=com_contact&view=contact&id='.$this->item->contactid),$author)); ?>
-
-	<?php else :?>
+	<?php $author = $this->item->created_by_alias ? $this->item->created_by_alias : $this->item->author; ?>
+	<?php if (!empty($this->item->contactid) && $params->get('link_author') == true): ?>
+	<?php
+		$needle = 'index.php?option=com_contact&view=contact&id=' . $this->item->contactid;
+		$item = JSite::getMenu()->getItems('link', $needle, true);
+		$cntlink = !empty($item) ? $needle . '&Itemid=' . $item->id : $needle;
+	?>
+		<?php echo JText::sprintf('COM_CONTENT_WRITTEN_BY', JHtml::_('link', JRoute::_($cntlink), $author)); ?>
+	<?php else: ?>
 		<?php echo JText::sprintf('COM_CONTENT_WRITTEN_BY', $author); ?>
 	<?php endif; ?>
 	</dd>
