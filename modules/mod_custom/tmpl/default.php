eb5a3d9db3ecd6025a3feb5cf62633a38dbbee33
Andrew Eddie <andrew.eddie@newlifeinit.com>
2011-06-04 05:13:37 +0000

    Added feature [#25461] Mod_custom with backgroundimages (Angie Radtke).
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@21436 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- modules/mod_custom/tmpl/default.php
+++ modules/mod_custom/tmpl/default.php
@@ -10,6 +10,8 @@
 // no direct access
 defined('_JEXEC') or die;
 ?>
-<div class="custom<?php echo $moduleclass_sfx ?>">
+
+
+<div class="custom<?php echo $moduleclass_sfx ?>" <?php if ($params->get('backgroundimage')): ?> style="background-image:url(<?php echo $params->get('backgroundimage');?>)<?php endif;?>">
 	<?php echo $module->content;?>
 </div>
