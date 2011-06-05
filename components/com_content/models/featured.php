0f6782702bcba7b8cf4878aef88e0ce515203de0
Mark Dexter <dextercowley@gmail.com>
2011-06-05 00:54:46 +0000

    # Revert [#25873] *Featured Articles Menu Option change since this causes errors on new install
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@21453 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- components/com_content/models/featured.php
+++ components/com_content/models/featured.php
@@ -60,8 +60,7 @@ class ContentModelFeatured extends ContentModelArticles
 		}
 
 		// check for category selection
-		if (implode(',', $params->get('featured_categories'))  == true) {
-			$featuredCategories = $params->get('featured_categories');
+		if (is_array($featuredCategories = $params->get('featured_categories'))) {
 			$this->setState('filter.frontpage.categories', $featuredCategories);
 		}
 	}
