8102d99e5ec3e75b814fb98ca994444bf7c94625
GeraintEdwards <github@copyn.plus.com>
2011-06-09 07:12:53 -0700

    Edited libraries/joomla/application/component/modellist.php via GitHub

--- libraries/joomla/application/component/modellist.php
+++ libraries/joomla/application/component/modellist.php
@@ -212,7 +212,7 @@ class JModelList extends JModel
 
 		// Load the total.
 		$query = $this->_getListQuery();
-		$total = (int) $this->_getListCount((string) $query);
+		$total = (int) $this->_getListCount( $query);
 
 		// Check for a database error.
 		if ($this->_db->getErrorNum()) {
