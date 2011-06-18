62f6428f58f071c1b16711b0f388c16ead7a5578
Rouven Weßling <me@rouvenwessling.de>
2011-06-19 00:12:53 +0200

    Use local storage for tabs

--- media/system/js/tabs.js
+++ media/system/js/tabs.js
@@ -3,6 +3,12 @@
  * @copyright	Copyright (C) 2005 - 2011 Open Source Matters, Inc. All rights reserved.
  * @license		GNU General Public License version 2 or later; see LICENSE.txt
  */
+ 
+Object.append(Browser.Features, {
+	localstorage: (function() {
+		return ('localStorage' in window) && window.localStorage !== null;
+	})()
+});
 
 /**
  * Tabs behavior
@@ -16,6 +22,7 @@ var JTabs = new Class({
 
 	options : {
 		display: 0,
+		useStorage: true,
 		onActive: function(title, description) {
 			description.setStyle('display', 'block');
 			title.addClass('open').removeClass('closed');
@@ -34,10 +41,22 @@ var JTabs = new Class({
 		this.titles = this.dlist.getChildren(this.options.titleSelector);
 		this.descriptions = this.dlist.getChildren(this.options.descriptionSelector);
 		this.content = new Element('div').inject(this.dlist, 'after').addClass('current');
+		this.storageName = 'jpanetabs_'+this.dlist.id;
 
-	this.options.display = this.options.display.toInt().limit(0, this.titles.length-1);
+		if (this.options.useStorage) {
+			if (Browser.Features.localstorage) {
+				this.options.display = Cookie.read(this.storageName);
+			} else {
+				this.options.display = localstorage[this.storageName];
+			}
+		}
+		if (this.options.display === null) {
+			this.options.display = 0;
+		}
+		this.options.display = this.options.display.toInt().limit(0, this.titles.length-1);
 
-		for (var i = 0, l = this.titles.length; i < l; i++){
+		for (var i = 0, l = this.titles.length; i < l; i++)
+		{
 			var title = this.titles[i];
 			var description = this.descriptions[i];
 			title.setStyle('cursor', 'pointer');
@@ -51,7 +70,8 @@ var JTabs = new Class({
 	},
 
 	hideAllBut: function(but) {
-		for (var i = 0, l = this.titles.length; i < l; i++){
+		for (var i = 0, l = this.titles.length; i < l; i++)
+		{
 			if (i != but) this.fireEvent('onBackground', [this.titles[i], this.descriptions[i]]);
 		}
 	},
@@ -59,6 +79,12 @@ var JTabs = new Class({
 	display: function(i) {
 		this.hideAllBut(i);
 		this.fireEvent('onActive', [this.titles[i], this.descriptions[i]]);
-	Cookie.write('jpanetabs_' + this.dlist.id, i);
+		if (this.options.useStorage) {
+			if (Browser.Features.localstorage) {
+				Cookie.write(this.storageName, i);
+			} else {
+				localstorage[this.storageName] = i;
+			}
+		}
 	}
 });
