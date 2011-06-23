3bdabe7156a23ceac780e93cf1c25d03002301f0
Christophe Demko <chdemko@gmail.com>
2011-06-23 05:31:49 +0000

    [#26040] Redirect of article enabled with 'register to read more' function results in 404 Category not found
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@21651 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- components/com_content/views/category/tmpl/blog_item.php
+++ components/com_content/views/category/tmpl/blog_item.php
@@ -135,7 +135,7 @@ JHtml::core();
 		$active = $menu->getActive();
 		$itemId = $active->id;
 		$link1 = JRoute::_('index.php?option=com_users&view=login&Itemid=' . $itemId);
-		$returnURL = JRoute::_(ContentHelperRoute::getArticleRoute($this->item->slug));
+		$returnURL = JRoute::_(ContentHelperRoute::getArticleRoute($this->item->slug, $this->item->catid));
 		$link = new JURI($link1);
 		$link->setVar('return', base64_encode($returnURL));
 	endif;
