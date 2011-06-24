a797818f801763bda122435a33f51837692b0d39
Rafael <rxcorral@gmail.com>
2011-06-23 20:55:21 -0400

    Rearranged the if statement
    It will be quicker to check for null everytime than the $plugin variable

--- libraries/joomla/plugin/helper.php
+++ libraries/joomla/plugin/helper.php
@@ -99,7 +99,7 @@ abstract class JPluginHelper
 
 			// Get the specified plugin(s).
 			for ($i = 0, $t = count($plugins); $i < $t; $i++) {
-				if ($plugins[$i]->type == $type && ($plugins[$i]->name == $plugin ||  $plugin === null)) {
+				if ($plugins[$i]->type == $type && ($plugin === null || $plugins[$i]->name == $plugin)) {
 					self::_import($plugins[$i], $autocreate, $dispatcher);
 					$results = true;
 				}
