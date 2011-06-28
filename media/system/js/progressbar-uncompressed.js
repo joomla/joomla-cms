eccdf468430b2403c5991576fcb1354f5bef406c
Mark Dexter <dextercowley@gmail.com>
2011-06-28 03:58:50 +0000

    [#26088] Add WAI-ARIA and HTML5 support the progressbar.js (Rouven Weßling)
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@21697 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- media/system/js/progressbar-uncompressed.js
+++ media/system/js/progressbar-uncompressed.js
@@ -1,11 +1,12 @@
 /**
  * Fx.ProgressBar
  *
- * @version		1.1
+ * @version		1.2
  *
  * @license		MIT License
  *
  * @author		Harald Kirschner <mail [at] digitarald [dot] de>
+ * @author		Rouven Weßling <me [at] rouvenwessling [dot] de>
  * @copyright	Authors
  */
 
@@ -18,12 +19,25 @@ Fx.ProgressBar = new Class({
 		url: null,
 		transition: Fx.Transitions.Circ.easeOut,
 		fit: true,
-		link: 'cancel'
+		link: 'cancel',
+		html5: true
 	},
 
 	initialize: function(element, options) {
 		this.element = $(element);
 		this.parent(options);
+
+		//WAI-ARIA
+		this.element.set('role', 'progressbar');
+		this.element.set('aria-valuenow', '0');
+		this.element.set('aria-valuemin', '0');
+		this.element.set('aria-valuemax', '100');
+
+		if (this.options.html5) {
+			this.progressElement = new Element('progress').wraps(this.element);
+			this.progressElement.max = 100;
+			this.progressElement.value = 0;
+		}
 
 		var url = this.options.url;
 		if (url) {
@@ -59,13 +73,17 @@ Fx.ProgressBar = new Class({
 		var css = (this.fill)
 			? (((this.fill / -2) + (to / 100) * (this.element.width || 1) || 0).round() + 'px')
 			: ((100 - to) + '%');
-
+		
 		this.element.setStyle('backgroundPosition', css + ' 0px').title = Math.round(to) + '%';
+		this.element.set('aria-valuenow', to);
+
+		if (this.options.html5) {
+			this.progressElement.value = to;
+		}
 
 		var text = $(this.options.text);
 		if (text) text.set('text', Math.round(to) + '%');
 
 		return this;
 	}
-
 });
