1f39d5e4799f8779ad59676cd5e61915276d1c1a
Christophe Demko <chdemko@gmail.com>
2011-07-06 09:16:33 +0000

    [#26314] Unused setting in Contact Manager Options
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@21748 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- components/com_contact/views/category/view.feed.php
+++ components/com_contact/views/category/view.feed.php
@@ -52,9 +52,7 @@ class ContactViewCategory extends JView
 			// url link to article
 			$link = JRoute::_(ContactHelperRoute::getContactRoute($row->slug, $row->catid));
 
-			// strip html from feed item description text
-			// TODO: Only pull fulltext if necessary (actually, just get the necessary fields).
-			$description	= ($params->get('feed_summary', 0) ? $row->introtext/*.$row->fulltext*/ : $row->introtext);
+			$description	= $row->introtext;
 			$author			= $row->created_by_alias ? $row->created_by_alias : $row->author;
 			@$date			= ($row->created ? date('r', strtotime($row->created)) : '');
 
