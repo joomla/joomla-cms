ca1843d089b5f082d7bf50e76fbab0db4dbe40f6
Andrew Eddie <andrew.eddie@newlifeinit.com>
2011-07-09 02:18:57 +0000

    Fix JPlatform loader problem.
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@21775 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- libraries/import.php
+++ libraries/import.php
@@ -37,7 +37,7 @@ if (!class_exists('JLoader')) {
 
 // Register the JPlatform version class for lazy loading.
 if (!class_exists('JPlatform')) {
-	JLoader::register('JPlatform', JPATH_PLATFORM.'/joomla/platform.php');
+	JLoader::register('JPlatform', JPATH_PLATFORM.'/platform.php');
 }
 
 // Define the Joomla Platform version if not already defined.
