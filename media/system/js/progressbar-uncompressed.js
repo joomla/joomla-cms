479aacacee75d1ead630e6b036386e2b92d9ee96
Christophe Demko <chdemko@gmail.com>
2011-07-10 08:11:24 +0000

    [#26357] Some minor enhancements to progressbar.js
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@21801 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- media/system/js/progressbar-uncompressed.js
+++ media/system/js/progressbar-uncompressed.js
@@ -1,14 +1,18 @@
-/**
- * Fx.ProgressBar
- *
- * @version		1.2
- *
- * @license		MIT License
- *
- * @author		Harald Kirschner <mail [at] digitarald [dot] de>
- * @author		Rouven Weßling <me [at] rouvenwessling [dot] de>
- * @copyright	Authors
- */
+/*
+name: Fx.ProgressBar
+
+description: Creates a progressbar with WAI-ARIA and optional HTML5 support.
+
+license: MIT-style
+
+authors:
+- Harald Kirschner <mail [at] digitarald [dot] de>
+- Rouven Weßling <me [at] rouvenwessling [dot] de>
+
+requires: [Core/Fx, Core/Class, Core/Element]
+
+provides: Fx.ProgressBar
+*/
 
 Fx.ProgressBar = new Class({
 
@@ -24,30 +28,31 @@ Fx.ProgressBar = new Class({
 	},
 
 	initialize: function(element, options) {
-		this.element = $(element);
+		this.element = document.id(element);
 		this.parent(options);
+		var url = this.options.url;
+		this.useHtml5 = this.options.html5 && this.supportsHtml5();
 
-		//WAI-ARIA
-		this.element.set('role', 'progressbar');
-		this.element.set('aria-valuenow', '0');
-		this.element.set('aria-valuemin', '0');
-		this.element.set('aria-valuemax', '100');
-
-		if (this.options.html5) {
-			this.progressElement = new Element('progress').wraps(this.element);
+		if (this.useHtml5) {
+			this.progressElement = new Element('progress').replaces(this.element);
 			this.progressElement.max = 100;
 			this.progressElement.value = 0;
+		} else {
+			//WAI-ARIA
+			this.element.set('role', 'progressbar');
+			this.element.set('aria-valuenow', '0');
+			this.element.set('aria-valuemin', '0');
+			this.element.set('aria-valuemax', '100');
+
+			if (url) {
+				this.element.setStyles({
+					'background-image': 'url(' + url + ')',
+					'background-repeat': 'no-repeat'
+				});
+			}
 		}
 
-		var url = this.options.url;
-		if (url) {
-			this.element.setStyles({
-				'background-image': 'url(' + url + ')',
-				'background-repeat': 'no-repeat'
-			});
-		}
-
-		if (this.options.fit) {
+		if (this.options.fit && !this.useHtml5) {
 			url = url || this.element.getStyle('background-image').replace(/^url\(["']?|["']?\)$/g, '');
 			if (url) {
 				var fill = new Image();
@@ -64,24 +69,29 @@ Fx.ProgressBar = new Class({
 		}
 	},
 
+	supportsHtml5: function () {
+		return 'value' in document.createElement('progress');
+	},
+
 	start: function(to, total) {
 		return this.parent(this.now, (arguments.length == 1) ? to.limit(0, 100) : to / total * 100);
 	},
 
 	set: function(to) {
 		this.now = to;
-		var css = (this.fill)
+
+		if (this.useHtml5) {
+			this.progressElement.value = to;
+		} else {
+			var css = (this.fill)
 			? (((this.fill / -2) + (to / 100) * (this.element.width || 1) || 0).round() + 'px')
 			: ((100 - to) + '%');
 		
-		this.element.setStyle('backgroundPosition', css + ' 0px').title = Math.round(to) + '%';
-		this.element.set('aria-valuenow', to);
-
-		if (this.options.html5) {
-			this.progressElement.value = to;
+			this.element.setStyle('backgroundPosition', css + ' 0px').title = Math.round(to) + '%';
+			this.element.set('aria-valuenow', to);
 		}
 
-		var text = $(this.options.text);
+		var text = document.id(this.options.text);
 		if (text) text.set('text', Math.round(to) + '%');
 
 		return this;
