d7f352a370eb8dc6382502d09503f2c72f84c6b7
Elin Waring <elin.waring@gmail.com>
2011-07-15 21:30:39 -0400

    Fix typo

--- libraries/joomla/form/rules/options.php
+++ libraries/joomla/form/rules/options.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @package		Joomla.FrameworkPlatform
+ * @package		Joomla.Platform
  * @subpackage	Form
  * @copyright	Copyright (C) 2005 - 2011 Open Source Matters, Inc. All rights reserved.
  * @license		GNU General Public License version 2 or later; see LICENSE.txt
