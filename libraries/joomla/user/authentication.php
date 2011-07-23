0cf9227281291cc54f226c6b67f194da3b897e92
Sam Moffatt <pasamio@gmail.com>
2011-07-24 01:20:07 +1000

    Added @since tags for 11.2, perhaps a tad presumptious

--- libraries/joomla/user/authentication.php
+++ libraries/joomla/user/authentication.php
@@ -13,20 +13,21 @@ jimport('joomla.base.observable');
 jimport('joomla.plugin.helper');
 jimport('joomla.event.dispatcher');
 
-// Shared success status
 /**
  * This is the status code returned when the authentication is success (permit login)
+ * @deprecated Use JAuthentication::STATUS_SUCCESS
  */
 define('JAUTHENTICATE_STATUS_SUCCESS', 1);
 
-// These are for authentication purposes (username and password is valid)
 /**
  * Status to indicate cancellation of authentication (unused)
+ * @deprecated 
  */
 define('JAUTHENTICATE_STATUS_CANCEL', 2);
 
 /**
  * This is the status code returned when the authentication failed (prevent login if no success)
+ * @deprecated Use JAuthentication::STATUS_FAILURE
  */
 define('JAUTHENTICATE_STATUS_FAILURE', 4);
 
@@ -42,39 +43,45 @@ class JAuthentication extends JObservable
 	// Shared success status
 	/**
 	 * This is the status code returned when the authentication is success (permit login)
-	 * @const STATUS_SUCCESS successful response
+	 * @const  STATUS_SUCCESS successful response
+	 * @since  11.2
 	 */
 	const STATUS_SUCCESS = 1;
 
 	// These are for authentication purposes (username and password is valid)
 	/**
 	 * Status to indicate cancellation of authentication (unused)
-	 * @const STATUS_CANCEL cancelled request (unused)
+	 * @const  STATUS_CANCEL cancelled request (unused)
+	 * @since  11.2
 	 */
 	const STATUS_CANCEL = 2;
 
 	/**
 	 * This is the status code returned when the authentication failed (prevent login if no success)
-	 * @const STATUS_FAILURE failed request
+	 * @const  STATUS_FAILURE failed request
+	 * @since  11.2
 	 */
 	const STATUS_FAILURE = 4;
 
 	// These are for authorisation purposes (can the user login)
 	/**
 	 * This is the status code returned when the account has expired (prevent login)
-	 * @const STATUS_EXPIRED an expired account (will prevent login)
+	 * @const  STATUS_EXPIRED an expired account (will prevent login)
+	 * @since  11.2
 	 */
 	const STATUS_EXPIRED = 8;
 
 	/**
 	 * This is the status code returned when the account has been denied (prevent login)
-	 * @const STATUS_DENIED denied request (will prevent login)
+	 * @const  STATUS_DENIED denied request (will prevent login)
+	 * @since  11.2
 	 */
 	const STATUS_DENIED = 16;
 
 	/**
 	 * This is the status code returned when the account doesn't exist (not an error)
-	 * @const STATUS_UNKNOWN unknown account (won't permit or prevent login)
+	 * @const  STATUS_UNKNOWN unknown account (won't permit or prevent login)
+	 * @since  11.2
 	 */
 	const STATUS_UNKNOWN = 32;
 
