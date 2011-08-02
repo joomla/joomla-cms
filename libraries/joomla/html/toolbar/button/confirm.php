2a3cf73e2cbbf5ac539343af49d93336ab54217a
Elin Waring <elin.waring@gmail.com>
2011-08-02 00:57:08 -0400

    Revert "More toolbar doc blocks"
    This reverts commit 5526d0c817f366c4fe1cd0249a44ee447a66cd3f.

--- libraries/joomla/html/toolbar/button/confirm.php
+++ libraries/joomla/html/toolbar/button/confirm.php
@@ -25,13 +25,11 @@ class JButtonConfirm extends JButton
 	 * @since  11.1
 	 */
 	protected $_name = 'Confirm';
-
 	/**
 	 * Fetch the HTML for the button
 	 * 
-	 * @param   string   $type      Unused string.
-	 * @param   string   $msg       Message to render
-	 * @param   string   $text      Button text
+	 * @param   string   $type       Unused string.
+	 * @param   string   $msg        Message to render
 	 * @param   string   $name      Name to be used as apart of the id
 	 * @param   string   $task      The task associated with the button
 	 * @param   boolean  $list      True to allow use of lists
@@ -62,7 +60,6 @@ class JButtonConfirm extends JButton
 	 * 
 	 * @param   string   $type      Button type
 	 * @param   string   $name      Name to be used as apart of the id
-	 * @param   string   $text      Button text
 	 * @param   string   $task      The task associated with the button
 	 * @param   boolean  $list      True to allow use of lists
 	 * @param   boolean  $hideMenu  True to hide the menu on click
@@ -82,7 +79,7 @@ class JButtonConfirm extends JButton
 	 * @param   string   $msg   Message associated with the button
 	 * @param   string   $name  Name to be used as apart of the id
 	 * @param   string   $task  The task
-	 * @param   boolean  $list  True to allow use of lists
+	 * @param   boolean  True to allow use of lists
 	 *
 	 * @return  string  JavaScript command string
 	 *
--- libraries/joomla/html/toolbar/button/custom.php
+++ libraries/joomla/html/toolbar/button/custom.php
@@ -26,17 +26,6 @@ class JButtonCustom extends JButton
 	 */
 	protected $_name = 'Custom';
 
-	/**
-	 * Fetch the HTML for the button
-	 * 
-	 * @param   string   $type      Button type, unused string.
-	 * @param   string   $html      HTML strng for the button
-	 * @param   string   $id        CSS id for the button
-	 *
-	 * @return  string   HTML string for the button
-	 *
-	 * @since   11.1
-	 */
 	public function fetchButton($type = 'Custom', $html = '', $id = 'custom')
 	{
 		return $html;
