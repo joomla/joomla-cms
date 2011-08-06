0e92259b9fb16ccd59bcdd7af466988d4a1d07aa
Rouven Weßling <me@rouvenwessling.de>
2011-08-06 19:44:21 +0200

    Take of the svn revision from media files.

--- media/system/js/switcher-uncompressed.js
+++ media/system/js/switcher-uncompressed.js
@@ -1,5 +1,4 @@
 /**
- * @version		$Id: switcher-uncompressed.js 14276 2010-01-18 14:20:28Z louis $
  * @copyright	Copyright (C) 2005 - 2011 Open Source Matters, Inc. All rights reserved.
  * @license		GNU General Public License version 2 or later; see LICENSE.txt
  */
