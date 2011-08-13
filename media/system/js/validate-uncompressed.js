6f70c21e4c7b01722b4dea4049ce6078eb2bbbd9
Rouven Weßling <me@rouvenwessling.de>
2011-08-13 12:53:17 +0200

    [#26448] global configuration broken in IE9

--- media/system/js/validate-uncompressed.js
+++ media/system/js/validate-uncompressed.js
@@ -3,6 +3,14 @@
  * @license		GNU General Public License version 2 or later; see LICENSE.txt
  */
 
+Object.append(Browser.Features, {
+	inputemail: (function() {
+		var i = document.createElement("input");
+		i.setAttribute("type", "email");
+		return i.type !== "text";
+	})()
+});
+
 /**
  * Unobtrusive Form Validation library
  *
@@ -73,7 +81,7 @@ var JFormValidator = new Class({
 				}
 			} else {
 				el.addEvent('blur', function(){return document.formvalidator.validate(this);});
-				if (el.hasClass('validate-email')) {
+				if (el.hasClass('validate-email') && Browser.Features.inputemail) {
 					el.type = 'email';
 				}
 			}
