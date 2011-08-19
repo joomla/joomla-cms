501d0885a2fb248bd1c4dc84333f0b2a02c186d7
Elin Waring <elin.waring@gmail.com>
2011-08-19 19:56:02 -0400

    Work on doc blocks in html

--- libraries/joomla/html/parameter/element/folderlist.php
+++ libraries/joomla/html/parameter/element/folderlist.php
@@ -27,7 +27,15 @@ class JElementFolderlist extends JElement
 	protected $_name = 'Folderlist';
 
 	/**
+	 * Fetch a folderlist element
 	 *
+	 * @param   string  $name          Element name
+	 * @param   string  $value         Element value
+	 * @param   object  $node          Element object
+	 * @param   string  $control_name  Control name
+	 * 
+	 * @return  string
+	 * 
 	 * @since   11.1
 	 *
 	 * @deprecated    12.1  Use JFormFieldFolderlist::getOptions instead.
