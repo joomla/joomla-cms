838689c8a7639363c870180b16c78c32260b274b
Elin Waring <elin.waring@gmail.com>
2011-08-19 23:16:47 -0400

    More work in docblocks

--- libraries/joomla/html/parameter/element/list.php
+++ libraries/joomla/html/parameter/element/list.php
@@ -60,9 +60,10 @@ class JElementList extends JElement
 	 * @param   object  $node          The current XML node.
 	 * @param   string  $control_name  The name of the HTML control.
 	 *
-	 * @since   11.1
+	 * @return  string
 	 *
 	 * @deprecated    12.1
+	 * @since   11.1
 	 */
 	public function fetchElement($name, $value, &$node, $control_name)
 	{
