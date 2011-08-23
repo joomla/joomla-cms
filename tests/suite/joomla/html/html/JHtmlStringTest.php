06d06926f89e4349807912b12a1175468a472c56
eddieajau <andrew.eddie@newlifeinit.com>
2011-08-23 16:33:59 +1000

    Added $noSplit and $allowHtml arguments to the JHtmlString HTML helper.

--- tests/suite/joomla/html/html/JHtmlStringTest.php
+++ tests/suite/joomla/html/html/JHtmlStringTest.php
@@ -55,72 +55,121 @@ class JHtmlStringTest extends PHPUnit_Framework_TestCase
 			'No change case' => array(
 				'Plain text',
 				0,
+				true,
+				true,
 				'Plain text',
 			),
 			'Plain text under the limit' => array(
 				'Plain text',
 				100,
+				true,
+				true,
 				'Plain text',
 			),
 			'Plain text at the limit' => array(
 				'Plain text',
 				10,
+				true,
+				true,
 				'Plain text',
 			),
 			'Plain text over the limit by two words' => array(
 				'Plain text test',
 				12,
+				true,
+				true,
 				'Plain...',
 			),
 			'Plain text over the limit by one word' => array(
 				'Plain text test',
 				13,
+				true,
+				true,
 				'Plain text...',
 			),
 			'Plain text over the limit with short trailing words' => array(
 				'Plain text a b c d',
 				13,
+				true,
+				true,
 				'Plain text...',
 			),
 			'Plain text over the limit splitting first word' => array(
 				'Plain text',
 				3,
+				true,
+				true,
 				'Pla...',
+			),
+			'Plain text with word split' => array(
+				'Plain split-less',
+				7,
+				false,
+				false,
+				'Plain s...',
 			),
 			'Plain html under the limit' => array(
 				'<span>Plain text</span>',
 				100,
+				true,
+				true,
 				'<span>Plain text</span>',
 			),
 			'Plain html at the limit' => array(
 				'<span>Plain text</span>',
 				23,
+				true,
+				true,
 				'<span>Plain text</span>',
 			),
 			'Plain html over the limit' => array(
 				'<span>Plain text</span>',
 				22,
+				true,
+				true,
 				'<span>Plain text</span>...',
 			),
 			'Plain html over the limit by one word' => array(
 				'<span>Plain text</span>',
 				12,
+				true,
+				true,
 				'<span>Plain</span>...',
 			),
 			'Plain html over the limit splitting first word' => array(
 				'<span>Plain text</span>',
 				10,
+				true,
+				true,
 				'<span>Plai</span>...',
 			),
 			'Complex html over the limit' => array(
 				'<div><span><i>Plain</i> <b>text</b> foo</span></div>',
 				37,
+				true,
+				true,
 				'<div><span><i>Plain</i></span></div>...',
 			),
 			'Complex html over the limit 2' => array(
 				'<div><span><i>Plain</i> <b>text</b> foo</span></div>',
 				38,
+				true,
+				true,
 				'<div><span><i>Plain</i> <b>text</b></span></div>...',
+			),
+			'HTML not allowed, split words' => array(
+				'<div><span><i>Plain</i> <b>text</b> foo</span></div>',
+				8,
+				false,
+				false,
+				'Plain te...',
+			),
+			'HTML not allowed, no split' => array(
+				'<div><span><i>Plain</i> <b>text</b> foo</span></div>',
+				8,
+				true,
+				false,
+				'Plain...',
 			),
 		);
 	}
@@ -128,10 +177,10 @@ class JHtmlStringTest extends PHPUnit_Framework_TestCase
 	/**
 	 * Tests the JHtmlString::abridge method.
 	 *
-	 * @param   string   $text      The text to truncate.
-	 * @param   integer  $length    The maximum length of the text.
-	 * @param   integer  $intro     The maximum length of the intro text.
-	 * @param   string   $expected  The expected result.
+	 * @param   string   $text       The text to truncate.
+	 * @param   integer  $length     The maximum length of the text.
+	 * @param   integer  $intro      The maximum length of the intro text.
+	 * @param   string   $expected   The expected result.
 	 *
 	 * @return  void
 	 *
@@ -151,17 +200,19 @@ class JHtmlStringTest extends PHPUnit_Framework_TestCase
 	 *
 	 * @param   string   $text      The text to truncate.
 	 * @param   integer  $length    The maximum length of the text.
-	 * @param   string   $expected  The expected result.
+	 * @param   boolean  $noSplit    Don't split a word if that is where the cutoff occurs (default: true).
+	 * @param   boolean  $allowHtml  Allow HTML tags in the output, and close any open tags (default: true).
+	 * @param   string   $expected   The expected result.
 	 *
 	 * @return  void
 	 *
 	 * @dataProvider  getTestTruncateData
 	 * @since   11.3
 	 */
-	public function testTruncate($text, $length, $expected)
+	public function testTruncate($text, $length, $noSplit, $allowedHtml, $expected)
 	{
 		$this->assertThat(
-			JHtmlString::truncate($text, $length),
+			JHtmlString::truncate($text, $length, $noSplit, $allowedHtml),
 			$this->equalTo($expected)
 		);
 	}
