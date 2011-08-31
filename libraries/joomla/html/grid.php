d197bc055a607eb857234b494bc19c019e99c2a1
Hannes Papenberg <info@joomlager.de>
2011-08-31 13:33:53 +0200

    Fixed formatting

--- libraries/joomla/html/grid.php
+++ libraries/joomla/html/grid.php
@@ -1,10 +1,12 @@
 <?php
 /**
+ * JGrid class to dynamically generate HTML tables
+ * 
  * @package     Joomla.Platform
  * @subpackage  HTML
  *
  * @copyright   Copyright (C) 2005 - 2011 Open Source Matters, Inc. All rights reserved.
- * @license     GNU General Public License version 2 or later; see LICENSE
+ * @license     GNU General Public License version 2 or later; see LICENSE 
  */
 
 defined('JPATH_PLATFORM') or die;
@@ -14,7 +16,7 @@ defined('JPATH_PLATFORM') or die;
  *
  * @package     Joomla.Platform
  * @subpackage  HTML
- * @since       11.1
+ * @since       11.3
  */
 class JGrid
 {
@@ -51,7 +53,7 @@ class JGrid
 	/**
 	 * Constructor for a JGrid object
 	 * 
-	 * @param array Associative array of attributes for the table-tag
+	 * @param array $options Associative array of attributes for the table-tag
 	 */
 	function __construct($options = array())
 	{
@@ -59,15 +61,26 @@ class JGrid
 	}
 	
 	/**
+	 * Magic function to render this object as a table.
+	 *
+	 * @return  string
+	 */
+	function __toString()
+	{
+		return $this->toString();
+	}
+	
+	/**
 	 * Method to set the attributes for a table-tag
 	 * 
-	 * @param array Associative array of attributes for the table-tag
-	 * @param bool Replace possibly existing attributes
+	 * @param array $options Associative array of attributes for the table-tag
+	 * @param bool 	$replace Replace possibly existing attributes
+	 * 
 	 * @return JGrid This object for chaining
 	 */
 	function setTableOptions($options = array(), $replace = false)
 	{
-		if($replace) {
+		if ($replace) {
 			$this->options = $options;
 		} else {
 			$this->options = array_merge($this->options, $options);
@@ -77,6 +90,7 @@ class JGrid
 	
 	/**
 	 * Get the Attributes of the current table
+	 * 
 	 * @return array Associative array of attributes
 	 */
 	function getTableOptions()
@@ -87,7 +101,8 @@ class JGrid
 	/**
 	 * Add new column name to process
 	 * 
-	 * @param string Internal column name
+	 * @param string $name Internal column name
+	 * 
 	 * @return JGrid This object for chaining
 	 */
 	function addColumn($name)
@@ -99,6 +114,7 @@ class JGrid
 	
 	/**
 	 * Returns the list of internal columns
+	 * 
 	 * @return array List of internal columns
 	 */
 	function getColumns()
@@ -109,14 +125,14 @@ class JGrid
 	/**
 	 * Delete column by name
 	 * 
-	 * @param string Name of the column to be deleted
+	 * @param string $name Name of the column to be deleted
+	 * 
 	 * @return JGrid This object for chaining
 	 */
 	function deleteColumn($name)
 	{
 		$index = array_search($name, $this->columns);
-		if($index !== false)
-		{
+		if ($index !== false) {
 			unset($this->columns[$index]);
 		}
 		
@@ -127,7 +143,8 @@ class JGrid
 	 * Method to set a whole range of columns at once
 	 * This can be used to re-order the columns, too
 	 * 
-	 * @param array List of internal column names
+	 * @param array $columns List of internal column names
+	 * 
 	 * @return JGrid This object for chaining
 	 */
 	function setColumns($columns)
@@ -141,16 +158,17 @@ class JGrid
 	 * Adds a row to the table and sets the currently
 	 * active row to the new row
 	 * 
-	 * @param array Associative array of attributes for the row
-	 * @param int 1 for a new row in the header, 2 for a new row in the footer
+	 * @param array $options Associative array of attributes for the row
+	 * @param int 	$special 1 for a new row in the header, 2 for a new row in the footer
+	 * 
 	 * @return JGrid This object for chaining
 	 */
 	function addRow($options = array(), $special = false)
 	{
 		$this->rows[]['_row'] = $options;
 		$this->activeRow = count($this->rows) - 1;
-		if($special) {
-			if($special === 1) {
+		if ($special) {
+			if ($special === 1) {
 				$this->specialRows['header'][] = $this->activeRow;
 			} else {
 				$this->specialRows['footer'][] = $this->activeRow;
@@ -163,7 +181,8 @@ class JGrid
 	/**
 	 * Set the currently active row
 	 * 
-	 * @param int ID of the row to be set to current
+	 * @param int $id ID of the row to be set to current
+	 * 
 	 * @return JGrid This object for chaining
 	 */
 	function setActiveRow($id)
@@ -176,16 +195,16 @@ class JGrid
 	 * Add information for a specific column for the
 	 * currently active row
 	 * 
-	 * @param string Name of the column
-	 * @param string Content for the cell
-	 * @param array Associative array of attributes for the td-element
-	 * @param bool If false, the content is appended to the current content of the cell
+	 * @param string 	$name 		Name of the column
+	 * @param string 	$content 	Content for the cell
+	 * @param array 	$option 	Associative array of attributes for the td-element
+	 * @param bool 		$replace 	If false, the content is appended to the current content of the cell
+	 * 
 	 * @return JGrid This object for chaining
 	 */
 	function addRowCell($name, $content, $option = array(), $replace = true)
 	{
-		if($replace || !isset($this->rows[$this->activeRow][$name]))
-		{
+		if ($replace || !isset($this->rows[$this->activeRow][$name])) {
 			$cell = new stdClass();
 			$cell->options = $option;
 			$cell->content = $content;
@@ -201,7 +220,8 @@ class JGrid
 	/**
 	 * Get all data for a row
 	 * 
-	 * @param int ID of the row to return
+	 * @param int $id ID of the row to return
+	 * 
 	 * @return array Array of columns of a table row
 	 */
 	function getRow($id)
@@ -212,13 +232,14 @@ class JGrid
 	/**
 	 * Get the IDs of all rows in the table
 	 * 
-	 * @param int false for the standard rows, 1 for the header rows, 2 for the footer rows
+	 * @param int $special false for the standard rows, 1 for the header rows, 2 for the footer rows
+	 * 
 	 * @return array Array of IDs
 	 */
 	function getRows($special = false)
 	{
-		if($special) {
-			if($special === 1) {
+		if ($special) {
+			if ($special === 1) {
 				return array_keys($this->specialRows['header']);
 			} else {
 				return array_keys($this->specialRows['footer']);
@@ -230,18 +251,19 @@ class JGrid
 	/**
 	 * Delete a row from the object
 	 * 
-	 * @param int ID of the row to be deleted
+	 * @param int $id ID of the row to be deleted
+	 * 
 	 * @return JGrid This object for chaining
 	 */
 	function deleteRow($id)
 	{
 		unset($this->rows[$id]);
 		
-		if(in_array($id, $this->specialRows['header'])) {
+		if (in_array($id, $this->specialRows['header'])) {
 			unset($this->specialRows['header'][array_search($id, $this->specialRows['header'])]);
 		}
 
-		if(in_array($id, $this->specialRows['footer'])) {
+		if (in_array($id, $this->specialRows['footer'])) {
 			unset($this->specialRows['footer'][array_search($id, $this->specialRows['footer'])]);
 		}
 		
@@ -253,7 +275,7 @@ class JGrid
 	 * 
 	 * @return string The rendered HTML table
 	 */
-	function render()
+	function toString()
 	{
 		$output = array();
 		$output[] = '<table'.$this->renderAttributes($this->getTableOptions()).'>';
@@ -268,16 +290,20 @@ class JGrid
 		return implode('', $output);		
 	}
 	
+	/**
+	 * Renders the table header
+	 * 
+	 * @return string The table header
+	 */
 	protected function renderHeader()
 	{
 		$output = array();
-		if(count($this->specialRows['header'])) {
+		if (count($this->specialRows['header'])) {
 			$output[] = "<thead>\n";
-			foreach($this->specialRows['header'] as $id) {
+			foreach ($this->specialRows['header'] as $id) {
 				$output[] = "\t<tr>\n";
-				foreach($this->getColumns() as $name)
-				{
-					if(isset($this->rows[$id][$name])) {
+				foreach ($this->getColumns() as $name) {
+					if (isset($this->rows[$id][$name])) {
 						$column = $this->rows[$id][$name];
 						$output[] = "\t\t<th".$this->renderAttributes($column->options).'>'.$column->content."</th>\n";
 					}	
@@ -290,17 +316,21 @@ class JGrid
 		return implode('', $output);
 	} 
 
+	/**
+	 * Renders the table body
+	 * 
+	 * @return string the body of the table
+	 */
 	protected function renderBody()
 	{
 		$output = array();
-		if(count(array_diff(array_keys($this->rows), $this->specialRows['header']))) {
+		if (count(array_diff(array_keys($this->rows), $this->specialRows['header']))) {
 			$output[] = "<tbody>\n";
 			$row_ids = array_diff(array_keys($this->rows), $this->specialRows['header']);
-			foreach($row_ids as $id) {
+			foreach ($row_ids as $id) {
 				$output[] = "\t<tr>\n";
-				foreach($this->getColumns() as $name)
-				{
-					if(isset($this->rows[$id][$name])) {
+				foreach ($this->getColumns() as $name) {
+					if (isset($this->rows[$id][$name])) {
 						$column = $this->rows[$id][$name];
 						$output[] = "\t\t<td".$this->renderAttributes($column->options).'>'.$column->content."</td>\n";
 					}	
@@ -313,16 +343,20 @@ class JGrid
 		return implode('', $output);
 	} 
 	
+	/**
+	 * Renders the footer of an HTML table
+	 * 
+	 * @return string The footer of the table
+	 */
 	protected function renderFooter()
 	{
 		$output = array();
-		if(count($this->specialRows['footer'])) {
+		if (count($this->specialRows['footer'])) {
 			$output[] = "<tfooter>\n";
-			foreach($this->specialRows['footer'] as $id) {
+			foreach ($this->specialRows['footer'] as $id) {
 				$output[] = "\t<tr>\n";
-				foreach($this->getColumns() as $name)
-				{
-					if(isset($this->rows[$id][$name])) {
+				foreach ($this->getColumns() as $name) {
+					if (isset($this->rows[$id][$name])) {
 						$column = $this->rows[$id][$name];
 						$output[] = "\t\t<th".$this->renderAttributes($column->options).'>'.$column->content."</th>\n";
 					}	
@@ -335,14 +369,20 @@ class JGrid
 		return implode('', $output);
 	}
 	
+	/**
+	 * Renders an HTML attribute from an associative array
+	 * 
+	 * @param array $attributes Associative array of attributes
+	 * 
+	 * @return string The HTML attribute string
+	 */
 	protected function renderAttributes($attributes)
 	{
-		if(count((array)$attributes) == 0) {
+		if (count((array)$attributes) == 0) {
 			return '';
 		}
 		$return = array();
-		foreach($attributes as $key => $option)
-		{
+		foreach ($attributes as $key => $option) {
 			$return[] = $key.'="'.$option.'"';
 		}
 		return ' '.implode(' ', $return);
