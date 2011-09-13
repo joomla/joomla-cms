177c8cfee79a00b99c96f116b1cc54f032e14a02
Mark Dexter <dextercowley@gmail.com>
2011-09-13 16:11:11 +0000

    Fix inconsistent datatype in database update script
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@22069 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- installation/sql/mysql/joomla_update_170to171.sql
+++ installation/sql/mysql/joomla_update_170to171.sql
@@ -3,6 +3,6 @@
 #
 # Database updates for 1.7.0 to 1.7.1
 #
-ALTER TABLE #__categories MODIFY description LONGTEXT;
+ALTER TABLE #__categories MODIFY description MEDIUMTEXT;
 ALTER TABLE #__session MODIFY data MEDIUMTEXT;
 ALTER TABLE #__session MODIFY session_id varchar(200);
