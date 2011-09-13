dcc8328a5676151fbeba0448ce1d91b683f6a5f7
Jean-Marie Simonet <infograf768@gmail.com>
2011-09-13 09:03:39 +0000

    # [#26356] Users can lock everyone out of the administrator. Thanks Irfan and Elin.
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@22066 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- administrator/components/com_login/models/login.php
+++ administrator/components/com_login/models/login.php
@@ -49,4 +49,116 @@ class LoginModelLogin extends JModel
 
 		$this->setState('return', $return);
 	}
+	
+	/**
+	 * Get the administrator login module by name (real, eg 'login' or folder, eg 'mod_login')
+	 *
+	 * @param   string  $name   The name of the module
+	 * @param   string  $title  The title of the module, optional
+	 *
+	 * @return  object  The Module object
+	 * 
+	 * @since   11.1
+	 */
+	public static function &getLoginModule($name = 'login', $title = null)
+	{
+		$result		= null;
+		$modules	= LoginModelLogin::_load();
+		$total		= count($modules);
+		
+		for ($i = 0; $i < $total; $i++)
+		{
+			// Match the name of the module
+			// Even though this is normally 'login' it is possible someone will use a differnt name or module
+			if ($modules[$i]->name == $name)
+			{
+				// Match the title if we're looking for a specific instance of the module
+				if (!$title || $modules[$i]->title == $title) {
+					$result = &$modules[$i];
+					break;	// Found it
+				}
+			}
+		}
+
+		// If we didn't find it, and the name is mod_something, create a dummy object
+		if (is_null($result) && substr($name, 0, 4) == 'mod_') {
+			$result				= new stdClass;
+			$result->id			= 0;
+			$result->title		= '';
+			$result->module		= $name;
+			$result->position	= '';
+			$result->content	= '';
+			$result->showtitle	= 0;
+			$result->control	= '';
+			$result->params		= '';
+			$result->user		= 0;
+		}
+
+		return $result;
+	}
+	/**
+	 * Load login modules.
+	 * 
+	 * Note that we load regardless of state or access level since access
+	 * for public is the only thing that makes sense since users are not logged in
+	 * and the module lets them log in.
+	 * This is put in as a failsafe to avoid super user lock out caused by an unpublished
+	 * login module or by a module set to have a viewing access level that is not Public.
+	 *
+	 * @return  array
+	 * 
+	 * @since   11.1
+	 */
+	protected static function &_load()
+	{
+		static $clean;
+
+		if (isset($clean)) {
+			return $clean;
+		}
+
+		$app		= JFactory::getApplication();
+		$lang 		= JFactory::getLanguage()->getTag();
+		$clientId 	= (int) $app->getClientId();
+
+		$cache 		= JFactory::getCache ('com_modules', '');
+		$cacheid 	= md5(serialize(array( $clientId, $lang)));
+
+		if (!($clean = $cache->get($cacheid))) {
+			$db	= JFactory::getDbo();
+
+			$query = $db->getQuery(true);
+			$query->select('m.id, m.title, m.module, m.position, m.showtitle, m.params');
+			$query->from('#__modules AS m');
+			$query->where('m.module =' . $db->Quote('mod_login') .' AND m.client_id = 1');
+
+			$query->join('LEFT','#__extensions AS e ON e.element = m.module AND e.client_id = m.client_id');
+			$query->where('e.enabled = 1');
+
+			// Filter by language
+			if ($app->isSite() && $app->getLanguageFilter()) {
+				$query->where('m.language IN (' . $db->Quote($lang) . ',' . $db->Quote('*') . ')');
+			}
+
+			$query->order('m.position, m.ordering');
+
+			// Set the query
+			$db->setQuery($query);
+			$modules = $db->loadObjectList();
+			$loginmodule	= array();
+
+			if ($db->getErrorNum()){
+				JError::raiseWarning(500, JText::sprintf('JLIB_APPLICATION_ERROR_MODULE_LOAD', $db->getErrorMsg()));
+				return $loginmodule;
+			}
+
+			
+			// Return to simple indexing that matches the query order.
+			$loginmodule = array_values($loginmodule);
+
+			$cache->store($loginmodule, $cacheid);
+		}
+
+		return $loginmodule;
+	}
 }
--- administrator/components/com_login/views/login/tmpl/default.php
+++ administrator/components/com_login/views/login/tmpl/default.php
@@ -11,9 +11,20 @@
 defined('_JEXEC') or die;
 
 // Get the login modules
+// If you want to use a completely different login module change the value of name 
+// in your layout override.
+
+$loginmodule =LoginModelLogin::getLoginModule('mod_login');
+	echo JModuleHelper::renderModule($loginmodule, array('style' => 'rounded', 'id' => 'section-box'));
+
+
+//Get any other modules in the login position.
+//If you want to use a different position for the modules, change the name here in your override.
 $modules = JModuleHelper::getModules('login');
 
 foreach ($modules as $module)
 // Render the login modules
-	echo JModuleHelper::renderModule($module, array('style' => 'rounded', 'id' => 'section-box'));
 
+if ($module->module != 'mod_login'){
+	echo JModuleHelper::renderModule($module, array('style' => 'rounded', 'id' => 'section-box'));
+}
