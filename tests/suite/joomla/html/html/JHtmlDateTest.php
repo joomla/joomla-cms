846fb676db8dfbd1194fb36af18d6b610963e79d
Michael Babker <mbabker@flbab.com>
2011-09-18 04:26:54 +0430

    Add test skeleton

---
+++ tests/suite/joomla/html/html/JHtmlDateTest.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @package     Joomla.UnitTest
+ * @subpackage  HTML
+ *
+ * @copyright   Copyright (C) 2005 - 2011 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+
+require_once JPATH_PLATFORM.'/joomla/html/html/date.php';
+
+/**
+ * Test class for JHtmlDate.
+ *
+ * @since  11.3
+ */
+class JHtmlDateTest extends PHPUnit_Framework_TestCase
+{
+	/**
+	 * @todo Implement testRelative().
+	 */
+	public function testRelative()
+	{
+		// Remove the following lines when you implement this test.
+		$this->markTestIncomplete(
+		'This test has not been implemented yet.'
+		);
+	}
+}
