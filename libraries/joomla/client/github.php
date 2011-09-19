e9d5d83da38ff7a2c03ec8174581278d1b6e60a8
Ian MacLennan <ian.maclennan@joomla.org>
2011-09-18 21:05:24 -0400

    Added initial work for Github client

---
+++ libraries/joomla/client/github.php
@@ -0,0 +1,110 @@
+<?php
+/**
+ * @package     Joomla.Platform
+ * @subpackage  Client
+ *
+ * @copyright   Copyright (C) 2005 - 2011 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+
+defined('JPATH_PLATFORM') or die;
+
+jimport('joomla.environment.uri');
+jimport('joomla.client.github.gists');
+require_once JPATH_PLATFORM.'/joomla/client/github/gists.php';
+
+/**
+ * HTTP client class.
+ *
+ * @package     Joomla.Platform
+ * @subpackage  Client
+ * @since       11.1
+ */
+class JGithub
+{
+	/**
+	 * Authentication Method
+	 * 
+	 * Possible values are 0 - no authentication, 1 - basic authentication, 2 - OAuth
+	 *
+	 * @var    string
+	 * @since  11.1
+	 */
+	protected $authentication_method = 0;
+
+	protected $gists = null;
+
+	/**
+	 * Constructor.
+	 *
+	 * @param   array  $options  Array of configuration options for the client.
+	 *
+	 * @return  void
+	 *
+	 * @since   11.1
+	 */
+	public function __construct($options = array())
+	{
+		$this->http = new JHttp;
+		
+		if (isset($options['username']) && isset($options['password'])) {
+			$this->credentials['username'] = $options['username'];
+			$this->credentials['password'] = $options['password'];
+			$this->authentication_method = 1;
+		} elseif (isset($options['token'])) {
+			$this->credentials['token'] = $options['token'];
+			$this->authentication_method = 2;
+		} else {
+			$this->authentication_method = 0;
+		}
+
+		$this->http = curl_init();
+	}
+
+	public function __get($name)
+	{
+		if ($name == 'gists') {
+			if ($this->gists == null) {
+				$this->gists = new JGithubGists($this);
+			}
+			return $this->gists;
+		}
+	}
+
+	public function sendRequest($url, $data = array(), $method = 'get', $options = array())
+	{
+		// $this->http = new JHttp;
+		$curl_options = array(
+			CURLOPT_URL => 'https://api.github.com'.$url,
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_HEADER => false,
+			CURLOPT_FOLLOWLOCATION => false,
+			CURLOPT_USERAGENT => 'JGithub',
+			CURLOPT_CONNECTTIMEOUT => 120,
+			CURLOPT_TIMEOUT => 120
+		);
+		
+		switch ($method) {
+			case 'post':
+				$curl_options[CURLOPT_POST] = true;
+				$curl_options[CURLOPT_POSTFIELDS] = $data;
+				break;
+
+			case 'put':
+				$curl_options[CURLOPT_CUSTOMREQUEST] = 'PUT';
+				$curl_options[CURLOPT_POST] = false;
+				$curl_options[CURLOPT_HTTPGET] = false;
+				break;
+
+			case 'get':
+				$curl_options[CURLOPT_POST] = false;
+				$curl_options[CURLOPT_HTTPGET] = true;
+				break;
+		}
+
+		curl_setopt_array($this->http, $curl_options);
+		$response = curl_exec($this->http);
+		return json_decode($response);
+	}
+
+}
