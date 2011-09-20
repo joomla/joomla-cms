bcf54920593d00607fc51b99cd1391fb0df5cee8
Mark Dexter <dextercowley@gmail.com>
2011-09-20 21:43:21 +0000

    [#24275] Submenu items in deeper menu-levels appear although they have no active parent item
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@22112 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- modules/mod_menu/helper.php
+++ modules/mod_menu/helper.php
@@ -51,7 +51,7 @@ class modMenuHelper
 					|| ($end && $item->level > $end)
 					|| (!$showAll && $item->level > 1 && !in_array($item->parent_id, $path))
 					|| ($maxdepth && $item->level > $maxdepth)
-					|| ($start > 1 && !in_array($item->tree[0], $path))
+					|| ($start > 1 && !in_array($item->tree[$start-2], $path))
 				) {
 					unset($items[$i]);
 					continue;
