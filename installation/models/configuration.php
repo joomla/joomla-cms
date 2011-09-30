077a26322c466be52fcffbb811354fd81fd1fddf
Jean-Marie Simonet <infograf768@gmail.com>
2011-09-30 09:04:32 +0000

    #$ [#26868] *Implementing default offline message per language. Thanks Christophe
    git-svn-id: http://joomlacode.org/svn/joomla/development/trunk@22183 6f6e1ebd-4c2b-0410-823f-f34bde69bce9

--- installation/models/configuration.php
+++ installation/models/configuration.php
@@ -52,6 +52,7 @@ class JInstallationModelConfiguration extends JModel
 		/* Site Settings */
 		$registry->set('offline', 0);
 		$registry->set('offline_message', JText::_('INSTL_STD_OFFLINE_MSG'));
+		$registry->set('display_offline_message', 1);
 		$registry->set('sitename', $options->site_name);
 		$registry->set('editor', 'tinymce');
 		$registry->set('list_limit', 20);
