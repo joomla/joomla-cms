4bd55c42526acbcbc2356ae6ae8d903be269bba4
Michael Babker <mbabker@flbab.com>
2011-10-15 11:24:20 +0430

    Move switches within parameter checks

--- modules/mod_finder/tmpl/default.php
+++ modules/mod_finder/tmpl/default.php
@@ -32,54 +32,54 @@ if ($params->get('show_label', 1))
 	$label	= '<label for="mod-finder-searchword" class="finder'.$suffix.'">'
 			. $params->get('alt_label', JText::_('JSEARCH_FILTER_SUBMIT'))
 			. '</label>';
+
+	switch ($params->get('label_pos', 'left')):
+		case 'top' :
+			$label = $label.'<br />';
+			$output = $label.$output;
+		break;
+
+		case 'bottom' :
+			$label = '<br />'.$label;
+			$output = $output.$label;
+			break;
+
+		case 'right' :
+			$output = $output.$label;
+			break;
+
+		case 'left' :
+		default :
+			$output = $label.$output;
+			break;
+	endswitch;
 }
 
 if ($params->get('show_button', 1))
 {
 	$button	= '<button class="button'.$suffix.' finder'.$suffix.'" type="submit">'.JText::_('MOD_FINDER_SEARCH_BUTTON').'</button>';
+
+	switch ($params->get('button_pos', 'right')):
+		case 'top' :
+			$button = $button.'<br />';
+			$output = $button.$output;
+			break;
+
+		case 'bottom' :
+			$button = '<br />'.$button;
+			$output = $output.$button;
+			break;
+
+		case 'right' :
+			$output = $output.$button;
+			break;
+
+		case 'left' :
+		default :
+			$output = $button.$output;
+			break;
+	endswitch;
 }
-
-switch ($params->get('label_pos', 'left')):
-    case 'top' :
-	    $label = $label.'<br />';
-	    $output = $label.$output;
-	    break;
-
-    case 'bottom' :
-	    $label = '<br />'.$label;
-	    $output = $output.$label;
-	    break;
-
-    case 'right' :
-	    $output = $output.$label;
-	    break;
-
-    case 'left' :
-    default :
-	    $output = $label.$output;
-	    break;
-endswitch;
-
-switch ($params->get('button_pos', 'right')):
-    case 'top' :
-	    $button = $button.'<br />';
-	    $output = $button.$output;
-	    break;
-
-    case 'bottom' :
-	    $button = '<br />'.$button;
-	    $output = $output.$button;
-	    break;
-
-    case 'right' :
-	    $output = $output.$button;
-	    break;
-
-    case 'left' :
-    default :
-	    $output = $button.$output;
-	    break;
-endswitch;
 
 JHtml::stylesheet('com_finder/finder.css', false, true, false);
 ?>
