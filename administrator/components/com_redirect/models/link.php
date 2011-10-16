90cdd5cf1ff2ab077b58660b1eb62faabc484c2f
chdemko <chdemko@gmail.com>
2011-10-16 14:03:25 +0200

    [#26904] JDatabasequery work in com_redirect and com_search

--- administrator/components/com_redirect/models/link.php
+++ administrator/components/com_redirect/models/link.php
@@ -155,9 +155,9 @@ class RedirectModelLink extends JModelAdmin
 		if (!empty($pks)) {
 			// Update the link rows.
 			$db->setQuery(
-				'UPDATE `#__redirect_links`' .
-				' SET `new_url` = '.$db->Quote($url).', `published` = 1, `comment` = '.$db->Quote($comment) .
-				' WHERE `id` IN ('.implode(',', $pks).')'
+				'UPDATE '.$db->nameQuote('#__redirect_links') .
+				' SET '.$db->nameQuote('new_url').' = '.$db->Quote($url).', '.$db->nameQuote('published').' = 1, '.$db->nameQuote('comment').' = '.$db->Quote($comment) .
+				' WHERE '.$db->nameQuote('id').' IN ('.implode(',', $pks).')'
 			);
 			$db->query();
 
