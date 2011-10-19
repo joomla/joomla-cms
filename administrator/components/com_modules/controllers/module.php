b4009657322d760d83b9ce71d95571ea12cb4e2a
Michael Babker <mbabker@flbab.com>
2011-10-19 18:13:57 +0430

    Add batch access/language to com_modules

--- administrator/components/com_modules/controllers/module.php
+++ administrator/components/com_modules/controllers/module.php
@@ -1,8 +1,10 @@
 <?php
 /**
- * @version		$Id$
- * @copyright	Copyright (C) 2005 - 2011 Open Source Matters, Inc. All rights reserved.
- * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ * @package     Joomla.Administrator
+ * @subpackage  com_modules
+ *
+ * @copyright   Copyright (C) 2005 - 2011 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 // No direct access.
@@ -13,14 +15,18 @@ jimport('joomla.application.component.controllerform');
 /**
  * Module controller class.
  *
- * @package		Joomla.Administrator
- * @subpackage	com_modules
- * @version		1.6
+ * @package     Joomla.Administrator
+ * @subpackage  com_modules
+ * @since       1.6
  */
 class ModulesControllerModule extends JControllerForm
 {
 	/**
 	 * Override parent add method.
+	 *
+	 * @return  mixed  True if the record can be added, a JError object if not.
+	 *
+	 * @since   1.6
 	 */
 	public function add()
 	{
@@ -29,13 +35,15 @@ class ModulesControllerModule extends JControllerForm
 
 		// Get the result of the parent method. If an error, just return it.
 		$result = parent::add();
-		if (JError::isError($result)) {
+		if (JError::isError($result))
+		{
 			return $result;
 		}
 
 		// Look for the Extension ID.
 		$extensionId = JRequest::getInt('eid');
-		if (empty($extensionId)) {
+		if (empty($extensionId))
+		{
 			$this->setRedirect(JRoute::_('index.php?option='.$this->option.'&view='.$this->view_item.'&layout=edit', false));
 			return JError::raiseWarning(500, JText::_('COM_MODULES_ERROR_INVALID_EXTENSION'));
 		}
@@ -45,6 +53,12 @@ class ModulesControllerModule extends JControllerForm
 
 	/**
 	 * Override parent cancel method to reset the add module state.
+	 *
+	 * @param   string  $key  The name of the primary key of the URL variable.
+	 *
+	 * @return  boolean  True if access level checks pass, false otherwise.
+	 *
+	 * @since   1.6
 	 */
 	public function cancel($key = null)
 	{
@@ -58,11 +72,19 @@ class ModulesControllerModule extends JControllerForm
 
 	/**
 	 * Override parent allowSave method.
+	 *
+	 * @param   array   $data  An array of input data.
+	 * @param   string  $key   The name of the key for the primary key.
+	 *
+	 * @return  boolean
+	 *
+	 * @since   1.6
 	 */
 	protected function allowSave($data, $key = 'id')
 	{
 		// use custom position if selected
-		if (empty($data['position'])) {
+		if (empty($data['position']))
+		{
 			$data['position'] = $data['custom_position'];
 		}
 
@@ -72,12 +94,36 @@ class ModulesControllerModule extends JControllerForm
 	}
 
 	/**
+	 * Method to run batch operations.
+	 *
+	 * @param   string  $model  The model
+	 *
+	 * @return	boolean  True on success.
+	 *
+	 * @since	1.7
+	 */
+	public function batch($model = null)
+	{
+		JRequest::checkToken() or jexit(JText::_('JINVALID_TOKEN'));
+
+		// Set the model
+		$model	= $this->getModel('Module', '', array());
+
+		// Preset the redirect
+		$this->setRedirect(JRoute::_('index.php?option=com_modules&view=modules'.$this->getRedirectToListAppend(), false));
+
+		return parent::batch($model);
+	}
+
+	/**
 	 * Function that allows child controller access to model data after the data has been saved.
 	 *
-	 * @param	JModel	$model	The data model object.
+	 * @param   JModel  &$model     The data model object.
+	 * @param   array   $validData  The validated data.
 	 *
-	 * @return	void
-	 * @since	1.6
+	 * @return  void
+	 *
+	 * @since   1.6
 	 */
 	protected function postSaveHook(JModel &$model, $validData = array())
 	{
