55d84273d296acc929ae181226bf93b95234f3ce
infograf768 <infografjms@gmail.com>
2011-10-25 09:27:42 +0200

    #$ [#26988] Incongruous Language Implementation in Article List Header

--- components/com_content/views/featured/tmpl/default_item.php
+++ components/com_content/views/featured/tmpl/default_item.php
@@ -96,7 +96,7 @@ $canEdit	= $this->item->params->get('access-edit');
 <?php endif; ?>
 <?php if ($params->get('show_publish_date')) : ?>
 		<dd class="published">
-		<?php echo JText::sprintf('COM_CONTENT_PUBLISHED_DATE', JHtml::_('date',$this->item->publish_up, JText::_('DATE_FORMAT_LC2'))); ?>
+		<?php echo JText::sprintf('COM_CONTENT_PUBLISHED_DATE_ON', JHtml::_('date',$this->item->publish_up, JText::_('DATE_FORMAT_LC2'))); ?>
 		</dd>
 <?php endif; ?>
 <?php if ($params->get('show_author') && !empty($this->item->author )) : ?>
--- templates/beez5/html/com_content/archive/default_items.php
+++ templates/beez5/html/com_content/archive/default_items.php
@@ -73,7 +73,7 @@ $params = &$this->params;
 <?php endif; ?>
 <?php if ($params->get('show_publish_date')) : ?>
 		<dd class="published">
-		<?php echo JText::sprintf('COM_CONTENT_PUBLISHED_DATE', JHtml::_('date',$item->publish_up, JText::_('DATE_FORMAT_LC2'))); ?>
+		<?php echo JText::sprintf('COM_CONTENT_PUBLISHED_DATE_ON', JHtml::_('date',$item->publish_up, JText::_('DATE_FORMAT_LC2'))); ?>
 		</dd>
 <?php endif; ?>
 <?php if ($params->get('show_author') && !empty($item->author )) : ?>
