5c175f495df38e0f0df04fad94a9b14e9cb9b404
Hannes Papenberg <info@joomlager.de>
2011-11-08 16:18:30 +0100

    Changing all cases of static instance variables in functions to class static properties

--- libraries/joomla/updater/updater.php
+++ libraries/joomla/updater/updater.php
@@ -27,6 +27,12 @@ jimport('joomla.log.log');
 class JUpdater extends JAdapter
 {
 	/**
+	 * @var    JUpdater  JUpdater instance container.
+	 * @since  11.3
+	 */
+	protected static $instance;
+
+	/**
 	 * Constructor
 	 *
 	 * @since   11.1
@@ -47,13 +53,11 @@ class JUpdater extends JAdapter
 	 */
 	public static function &getInstance()
 	{
-		static $instance;
-
-		if (!isset($instance))
+		if (!isset(self::$instance))
 		{
-			$instance = new JUpdater;
+			self::$instance = new JUpdater;
 		}
-		return $instance;
+		return self::$instance;
 	}
 
 	/**
