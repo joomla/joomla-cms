9bf7fcca7d1bf5020c09f864c461e7d276efb100
Michael Babker <mbabker@flbab.com>
2011-11-23 12:45:24 +0430

    Change charset back to UTF-8

--- libraries/phpmailer/phpmailer.php
+++ libraries/phpmailer/phpmailer.php
@@ -53,7 +53,7 @@ class PHPMailer {
    * Sets the CharSet of the message.
    * @var string
    */
-  public $CharSet           = 'iso-8859-1';
+  public $CharSet           = 'utf-8';
 
   /**
    * Sets the Content-type of the message.
