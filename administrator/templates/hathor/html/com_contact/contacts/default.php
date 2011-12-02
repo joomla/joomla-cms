de7c53b4179e46ba76b20f53e5190e437ef007de
Michael Babker <mbabker@flbab.com>
2011-12-02 17:25:05 +0600

    Add batch processing to com_contact

--- administrator/templates/hathor/html/com_contact/contacts/default.php
+++ administrator/templates/hathor/html/com_contact/contacts/default.php
@@ -189,6 +189,9 @@ $saveOrder	= $listOrder == 'a.ordering';
 		</tbody>
 	</table>
 
+	<?php //Load the batch processing form. ?>
+	<?php echo $this->loadTemplate('batch'); ?>
+
 	<?php echo $this->pagination->getListFooter(); ?>
 
 	<input type="hidden" name="task" value="" />
