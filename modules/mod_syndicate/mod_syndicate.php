90d12ae261e3bea88c6d3e8f2d237841cd0d39b6
Jean-Marie Simonet <infografjms@gmail.com>
2011-12-03 10:29:38 -0800

    [#27229] *Implementing default text per language in syndicate module

--- modules/mod_syndicate/mod_syndicate.php
+++ modules/mod_syndicate/mod_syndicate.php
@@ -13,7 +13,6 @@ defined('_JEXEC') or die;
 // Include the syndicate functions only once
 require_once dirname(__FILE__).'/helper.php';
 
-$params->def('text', 'Feed Entries');
 $params->def('format', 'rss');
 
 $link = modSyndicateHelper::getLink($params);
