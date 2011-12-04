12bab3aaef54b022ab2b78b836ddb708acdb8567
Rouven Weßling <me@rouvenwessling.de>
2011-12-04 18:15:17 +0100

    Clean up JFormFieldPlugins. Deprecate JFormFieldEditors.

--- libraries/joomla/form/fields/plugins.php
+++ libraries/joomla/form/fields/plugins.php
@@ -1,28 +1,31 @@
 <?php
 /**
- * @version		$Id: editors.php 13698 2009-12-11 18:38:48Z eddieajau $
- * @copyright	Copyright (C) 2005 - 2009 Open Source Matters, Inc. All rights reserved.
- * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ * @package     Joomla.Platform
+ * @subpackage  Form
+ *
+ * @copyright   Copyright (C) 2005 - 2011 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
  */
 
-defined('JPATH_BASE') or die;
+defined('JPATH_PLATFORM') or die;
 
-jimport('joomla.html.html');
-require_once dirname(__FILE__).DS.'list.php';
+jimport('joomla.form.formfield');
+JFormHelper::loadFieldClass('list');
 
 /**
  * Form Field class for the Joomla Framework.
  *
- * @package		Joomla.Framework
- * @subpackage	Form
- * @since		1.6
+ * @package     Joomla.Platform
+ * @subpackage  Form
+ * @since       11.4
  */
 class JFormFieldPlugins extends JFormFieldList
 {
 	/**
 	 * The field type.
 	 *
-	 * @var		string
+	 * @var    string
+	 * @since  11.4
 	 */
 	protected $type = 'Plugins';
 
@@ -30,20 +33,22 @@ class JFormFieldPlugins extends JFormFieldList
 	 * Method to get a list of options for a list input.
 	 *
 	 * @return	array		An array of JHtml options.
+	 *
+	 * @since   11.4
 	 */
 	protected function getOptions()
 	{
 		// Initialise variables
 		$folder	= $this->element['folder'];
 
-		if(!empty($folder))
+		if (!empty($folder))
 		{
 			// Get list of plugins
-			$db		= JFactory::getDbo();
-			$query	= $db->getQuery(true);
+			$db     = JFactory::getDbo();
+			$query  = $db->getQuery(true);
 			$query->select('element AS value, name AS text');
 			$query->from('#__extensions');
-			$query->where('folder = "'.$folder.'"');
+			$query->where('folder = "' . $folder . '"');
 			$query->where('enabled = 1');
 			$query->order('ordering, name');
 			$db->setQuery($query);
@@ -51,9 +56,8 @@ class JFormFieldPlugins extends JFormFieldList
 			$options = $db->loadObjectList();
 
 			$lang = JFactory::getLanguage();
-			foreach($options as $i => $item)
+			foreach ($options as $i => $item)
 			{
-
 				$source = JPATH_PLUGINS . '/' . $folder . '/' . $item->value;
 				$extension = 'plg_' . $folder . '_' . $item->value;
 					$lang->load($extension . '.sys', JPATH_ADMINISTRATOR, null, false, false)
@@ -69,7 +73,9 @@ class JFormFieldPlugins extends JFormFieldList
 				return '';
 			}
 
-		} else{
+		}
+		else
+		{
 			JError::raiseWarning(500, JText::_('JFRAMEWORK_FORM_FIELDS_PLUGINS_ERROR_FOLDER_EMPTY'));
 		}
 
