6436ed13e259762cbae880d17415b68d87eb8ca7
Chraneco <chraneco@joomgallery.net>
2011-12-06 20:47:34 +0100

    Accidentally commited file removed

--- logs/error.php
+++
@@ -1,7 +0,0 @@
-#<?php die('Forbidden.'); ?>
-#Date: 2011-12-04 17:29:54 UTC
-#Software: Joomla Platform 11.3.0 Stable [ Dennis Ritchie ] 23-Nov-2011 00:00 GMT
-
-#Fields: date	time	priority	clientip	category	message
-2011-12-04	17:29:54	INFO	::1	Joomla FAILURE: 	Username and password do not match or you do not have an account yet.
-2011-12-06	18:17:29	INFO	::1	Joomla FAILURE: 	Username and password do not match or you do not have an account yet.
