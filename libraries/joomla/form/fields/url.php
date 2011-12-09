58d8f0cc837ffc74c8a12ab91469cbed8f904d6a
elinw <elin.waring@gmail.com>
2011-12-09 11:48:22 -0500

    More checkstyle issues.

--- libraries/joomla/form/fields/url.php
+++ libraries/joomla/form/fields/url.php
@@ -28,31 +28,31 @@ class JFormFieldUrl extends JFormField
 	 * The form field type.
 	 *
 	 * @var    string
-	 * @since  11.4
+	 * @since  11.1
 	 */
 	protected $type = 'Url';
 
 	/**
 	* Method to get the field input markup.
-	* When used in conjunction with the url filter in JFormField, ifthe relative element is
+	* When used in conjunction with the url filter in JFormField, if the relative element is
 	* false the method assumes most URLS are external.
-	* When relative is true a url that does not include a protocol is assumed to be local.
-	* This method does not validate a url which should be done using JFormRuleUrl.
+	* When relative is true a URL that does not include a protocol is assumed to be local.
+	* This method does not validate a URL which should be done using JFormRuleUrl.
 	*
 	* @return  string
 	*
 	* @see     JFormRuleUrl, JForm::Filter
-	* @since   11.1
+	* @since   11.4
 	*/
 	protected function getInput()
 	{
 		// Initialize some field attributes.
-		$accept		= $this->element['accept'] ? ' accept="'.(string) $this->element['accept'].'"' : '';
-		$size		= $this->element['size'] ? ' size="'.(int) $this->element['size'].'"' : '';
-		$class		= $this->element['class'] ? ' class="'.(string) $this->element['class'].'"' : '';
+		$accept		= $this->element['accept'] ? ' accept="' . (string) $this->element['accept'] . '"' : '';
+		$size		= $this->element['size'] ? ' size="' . (int) $this->element['size'] . '"' : '';
+		$class		= $this->element['class'] ? ' class="' . (string) $this->element['class'] . '"' : '';
 		$disabled	= ((string) $this->element['disabled'] == 'true') ? ' disabled="disabled"' : '';
 		$readonly	= ((string) $this->element['readonly'] == 'true') ? ' readonly="readonly"' : '';
-		$maxLength	= $this->element['maxlength'] ? ' maxlength="'.(int) $this->element['maxlength'].'"' : '';
+		$maxLength	= $this->element['maxlength'] ? ' maxlength="' . (int) $this->element['maxlength'].'"' : '';
 		// Element to assume  of relative URLs without protocols are local. If not set or false, URLS
 		// without protocols are assumed to be external (with some exceptions based on string matching).
 		// Do not use if you intend to use the URL rule to validate.
@@ -60,11 +60,11 @@ class JFormFieldUrl extends JFormField
 
 
 			// Initialize JavaScript field attributes.
-			$onchange	= $this->element['onchange'] ? ' onchange="'.(string) $this->element['onchange'].'"' : '';
+			$onchange	= $this->element['onchange'] ? ' onchange="' . (string) $this->element['onchange'] . '"' : '';
 
-			return '<input type="text" name="'.$this->name.'" id="'.$this->id.'"' .
-					' value="'.htmlspecialchars($this->value, ENT_COMPAT, 'UTF-8').'"' .
-					$class.$size.$disabled.$relative.$readonly.$onchange.$maxLength.'/>';
+			return '<input type="text" name="'. $this->name . '" id="' . $this->id . '"' .
+					' value="' . htmlspecialchars($this->value, ENT_COMPAT, 'UTF-8') . '"' .
+					$class . $size . $disabled . $relative . $readonly . $onchange . $maxLength . '/>';
 
 		}
 }
