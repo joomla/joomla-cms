3b234bba3343011ddad40e3f7f15fdb1e717e4aa
elinw <elin.waring@gmail.com>
2011-12-20 08:49:10 -0500

    Add a finder content plugin to call extension specific plugins.

--- plugins/finder/content/content.php
+++ plugins/finder/content/content.php
@@ -149,7 +149,7 @@ class PlgFinderContent extends FinderIndexerAdapter
 	 * Method to determine if the access level of an item changed.
 	 *
 	 * @param   string   $context  The context of the content passed to the plugin.
-	 * @param   JTable   &$row     A JTable object
+	 * @param   JTable   $row     A JTable object
 	 * @param   boolean  $isNew    If the content is just about to be created
 	 *
 	 * @return  boolean  True on success.
@@ -157,7 +157,7 @@ class PlgFinderContent extends FinderIndexerAdapter
 	 * @since   2.5
 	 * @throws  Exception on database error.
 	 */
-	public function onFinderAfterSave($context, &$row, $isNew)
+	public function onFinderAfterSave($context, $row, $isNew)
 	{
 		// We only want to handle articles here
 		if ($context == 'com_content.article')
@@ -220,7 +220,7 @@ class PlgFinderContent extends FinderIndexerAdapter
 	 * to queue the item to be indexed later.
 	 *
 	 * @param   string   $context  The context of the content passed to the plugin.
-	 * @param   JTable   &$row     A JTable object
+	 * @param   JTable   $row     A JTable object
 	 * @param   boolean  $isNew    If the content is just about to be created
 	 *
 	 * @return  boolean  True on success.
@@ -228,7 +228,7 @@ class PlgFinderContent extends FinderIndexerAdapter
 	 * @since   2.5
 	 * @throws  Exception on database error.
 	 */
-	public function onFinderBeforeSave($context, &$row, $isNew)
+	public function onFinderBeforeSave($context, $row, $isNew)
 	{
 		// We only want to handle articles here
 		if ($context == 'com_content.article')
