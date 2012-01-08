8e158417a3344edc882f1597dd3cd65cee25d691
Mark Dexter <dextercowley@gmail.com>
2012-01-08 09:06:56 -0800

    Revert [#24634] Menu items don't become active (i.e. highlight) if the Menu Item Type is "Menu Item Alias" (Bernie Sumption)

--- modules/mod_menu/tmpl/default.php
+++ modules/mod_menu/tmpl/default.php
@@ -27,17 +27,10 @@ foreach ($list as $i => &$item) :
 		$class .= ' current';
 	}
 
-	if (in_array($item->id, $path)) {
+	if (	$item->type == 'alias' &&
+			in_array($item->params->get('aliasoptions'),$path)
+		||	in_array($item->id, $path)) {
 		$class .= ' active';
-	}
-	elseif ($item->type == 'alias') {
-		$aliasToId = $item->params->get('aliasoptions');
-		if ($aliasToId == $path[count($path)-1]) {
-			$class .= ' active';
-		}
-		elseif (in_array($aliasToId, $path)) {
-			$class .= ' alias-parent-active';
-		}
 	}
 
 	if ($item->deeper) {
