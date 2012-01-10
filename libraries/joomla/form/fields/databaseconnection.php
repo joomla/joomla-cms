1ff88beab7dd43109182d0a116dbe891d595d9f2
Nikolai Plath <der.el.kuku@gmail.com>
2012-01-10 16:31:39 -0500

    Be positive: Allow testing experimental database drivers

--- libraries/joomla/form/fields/databaseconnection.php
+++ libraries/joomla/form/fields/databaseconnection.php
@@ -40,38 +40,28 @@ class JFormFieldDatabaseConnection extends JFormFieldList
 	 * @return  array    The field option objects.
 	 *
 	 * @since   11.3
-	 * @see		JDatabase
+	 * @see     JDatabase
 	 */
 	protected function getOptions()
 	{
-		// Initialize variables.
-		// This gets the connectors available in the platform and supported by the server.
-		$available = JDatabase::getConnectors();
-
 		/**
-		 * This gets the list of database types supported by the application.
+		 * This gets the list of database types unsupported by the application.
 		 * This should be entered in the form definition as a comma separated list.
-		 * If no supported databases are listed, it is assumed all available databases
+		 * If no unsupported databases are listed, it is assumed all available databases
 		 * are supported.
 		 */
-		$supported = $this->element['supported'];
-		if (!empty($supported))
+		$unsupported = explode(',', $this->element['unsupported']);
+
+		// This gets the connectors available in the platform and supported by the server and the application.
+		foreach (JDatabase::getConnectors() as $connector)
 		{
-			$supported = explode(',', $supported);
-			foreach ($supported as $support)
+			if(in_array($connector, $unsupported))
 			{
-				if (in_array($support, $available))
-				{
-					$options[$support] = ucfirst($support);
-				}
+				// The connector is not supported by the application
+				continue;
 			}
-		}
-		else
-		{
-			foreach ($available as $support)
-			{
-				$options[$support] = ucfirst($support);
-			}
+
+			$options[$connector] = ucfirst($connector);
 		}
 
 		// This will come into play if an application is installed that requires
@@ -80,6 +70,7 @@ class JFormFieldDatabaseConnection extends JFormFieldList
 		{
 			$options[''] = JText::_('JNONE');
 		}
+
 		return $options;
 	}
 }
