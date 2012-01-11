60ca791de04c244ba76db98a3ea3c5cbaa8ca175
Rouven Weßling <me@rouvenwessling.de>
2012-01-10 16:59:39 -0800

    [#27686] Backport some fixes from the platform

--- libraries/joomla/log/loggers/formattedtext.php
+++ libraries/joomla/log/loggers/formattedtext.php
@@ -203,6 +203,8 @@ class JLoggerFormattedText extends JLogger
 		// If the no php flag is not set add the php die statement.
 		if (empty($this->options['text_file_no_php']))
 		{
+			// blank line to prevent information disclose: https://bugs.php.net/bug.php?id=60677
+			$head[] = '#';
 			$head[] = '#<?php die(\'Forbidden.\'); ?>';
 		}
 		$head[] = '#Date: ' . gmdate('Y-m-d H:i:s') . ' UTC';
