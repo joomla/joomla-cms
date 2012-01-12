37b0fe338bd2b2fae134ac8fd07cc95afcce31f7
Rouven Weßling <me@rouvenwessling.de>
2012-01-12 20:49:52 +0100

    Move JFormFieldMedia and JFormFieldHelpsite to the CMS.

--- tests/suite/cms/form/field/JFormFieldMediaTest.php
+++ tests/suite/cms/form/field/JFormFieldMediaTest.php
@@ -20,8 +20,8 @@ class JFormFieldMediaTest extends JoomlaTestCase
 	 */
 	protected function setUp()
 	{
-		require_once JPATH_PLATFORM.'/joomla/form/fields/media.php';
-		include_once dirname(__DIR__).'/inspectors.php';
+		require_once JPATH_PLATFORM . '/cms/form/fields/media.php';
+		include_once dirname(__DIR__) . '/inspectors.php';
 	}
 
 	/**
