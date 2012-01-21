9f7cc71f8fc857fe564fbf6f80e2be0753aa3f05
Andrea Tarr <atarr@tarrconsulting.com>
2012-01-21 08:22:14 +0100

    # [#27557] Code style fix and Hathor CSS fixes to Extensions/Installer. Thanks Andrea.

--- administrator/components/com_installer/views/database/tmpl/default.php
+++ administrator/components/com_installer/views/database/tmpl/default.php
@@ -30,34 +30,35 @@ defined('_JEXEC') or die;
 
 	<?php $panelName = JText::plural('COM_INSTALLER_MSG_N_DATABASE_ERROR_PANEL', $errors); ?>
 	<?php echo JHtml::_('sliders.panel', $panelName, 'error-panel'); ?>
-		<fieldset class="panelform">
-	<ul>
-	<?php if (!strncmp($this->schemaVersion, JVERSION, 5) === 0) : ?>
-		<li><?php echo JText::sprintf('COM_INSTALLER_MSG_DATABASE_SCHEMA_ERROR', $this->schemaVersion, JVERSION); ?></li>
-	<?php endif; ?>
-
-	<?php foreach($this->errors as $line => $error) : ?>
-	<?php $key = 'COM_INSTALLER_MSG_DATABASE_' . $error->queryType;
-		$msgs = $error->msgElements;
-		$file = basename($error->file);
-		$msg0 = (isset($msgs[0])) ? $msgs[0] : ' ';
-		$msg1 = (isset($msgs[1])) ? $msgs[1] : ' ';
-		$msg2 = (isset($msgs[2])) ? $msgs[2] : ' ';
-		$message = JText::sprintf($key, $file, $msg0, $msg1, $msg2); ?>
-		<li><?php echo $message; ?></li>
-	<?php endforeach; ?></ul>
+	<fieldset class="panelform">
+		<ul>
+			<?php if (!strncmp($this->schemaVersion, JVERSION, 5) === 0) : ?>
+				<li><?php echo JText::sprintf('COM_INSTALLER_MSG_DATABASE_SCHEMA_ERROR', $this->schemaVersion, JVERSION); ?></li>
+			<?php endif; ?>
+	
+			<?php foreach($this->errors as $line => $error) : ?>
+				<?php $key = 'COM_INSTALLER_MSG_DATABASE_' . $error->queryType;
+				$msgs = $error->msgElements;
+				$file = basename($error->file);
+				$msg0 = (isset($msgs[0])) ? $msgs[0] : ' ';
+				$msg1 = (isset($msgs[1])) ? $msgs[1] : ' ';
+				$msg2 = (isset($msgs[2])) ? $msgs[2] : ' ';
+				$message = JText::sprintf($key, $file, $msg0, $msg1, $msg2); ?>
+				<li><?php echo $message; ?></li>
+			<?php endforeach; ?>
+		</ul>
 	</fieldset>
 <?php endif; ?>
 
 <?php echo JHtml::_('sliders.panel', JText::_('COM_INSTALLER_MSG_DATABASE_INFO'), 'furtherinfo-pane'); ?>
 	<fieldset class="panelform">
-<ul>
-	<li><?php echo JText::sprintf('COM_INSTALLER_MSG_DATABASE_SCHEMA_VERSION', $this->schemaVersion); ?></li>
-	<li><?php echo JText::sprintf('COM_INSTALLER_MSG_DATABASE_DRIVER', JFactory::getDbo()->name); ?></li>
-	<li><?php echo JText::sprintf('COM_INSTALLER_MSG_DATABASE_CHECKED_OK', count($this->results['ok'])); ?></li>
-	<li><?php echo JText::sprintf('COM_INSTALLER_MSG_DATABASE_SKIPPED', count($this->results['skipped'])); ?></li>
-</ul>
-</fieldset>
+	<ul>
+		<li><?php echo JText::sprintf('COM_INSTALLER_MSG_DATABASE_SCHEMA_VERSION', $this->schemaVersion); ?></li>
+		<li><?php echo JText::sprintf('COM_INSTALLER_MSG_DATABASE_DRIVER', JFactory::getDbo()->name); ?></li>
+		<li><?php echo JText::sprintf('COM_INSTALLER_MSG_DATABASE_CHECKED_OK', count($this->results['ok'])); ?></li>
+		<li><?php echo JText::sprintf('COM_INSTALLER_MSG_DATABASE_SKIPPED', count($this->results['skipped'])); ?></li>
+	</ul>
+	</fieldset>
 <?php echo JHtml::_('sliders.end'); ?>
 
 <div class="clr"> </div>
