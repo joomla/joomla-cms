f272438ad160f72d38fd422bd10b534dfdc5b584
Rouven Weßling <me@rouvenwessling.de>
2012-01-29 03:56:01 +0100

    Deprecate the exceptions that are based on packages.

--- libraries/joomla/database/exception.php
+++ libraries/joomla/database/exception.php
@@ -15,6 +15,7 @@ defined('JPATH_PLATFORM') or die;
  * @package     Joomla.Platform
  * @subpackage  Database
  * @since       11.1
+ * @deprecated  12.3 Use semantic exceptions instead
  */
 class JDatabaseException extends Exception
 {
