0f458cb24ceba78967b4210c0519207db7caf38a
Gabriele Pongelli <gabriele.pongelli@gmail.com>
2012-01-31 00:23:31 +0100

    Fixed some erroneous queries.

--- administrator/components/com_admin/sql/updates/postgresql/1.7.4-2011-11-23.sql
+++ administrator/components/com_admin/sql/updates/postgresql/1.7.4-2011-11-23.sql
@@ -1,16 +1,16 @@
 ALTER TABLE "#__newsfeeds" ALTER COLUMN "alias" TYPE character varying(255);
-ALTER TABLE "#__newsfeeds" ALTER COLUMN "position" SET DEFAULT '';
-ALTER TABLE "#__newsfeeds" ALTER COLUMN "position" SET NOT NULL;
+ALTER TABLE "#__newsfeeds" ALTER COLUMN "alias" SET DEFAULT '';
+ALTER TABLE "#__newsfeeds" ALTER COLUMN "alias" SET NOT NULL;
 
 ALTER TABLE "#__content" ALTER COLUMN "alias" TYPE character varying(255);
-ALTER TABLE "#__content" ALTER COLUMN "position" SET DEFAULT '';
-ALTER TABLE "#__content" ALTER COLUMN "position" SET NOT NULL;
+ALTER TABLE "#__content" ALTER COLUMN "alias" SET DEFAULT '';
+ALTER TABLE "#__content" ALTER COLUMN "alias" SET NOT NULL;
 
 COMMENT ON COLUMN "#__content"."alias" IS '';
 
 
 ALTER TABLE "#__content" ALTER COLUMN "title_alias" TYPE character varying(255);
-ALTER TABLE "#__content" ALTER COLUMN "position" SET DEFAULT '';
-ALTER TABLE "#__content" ALTER COLUMN "position" SET NOT NULL;
+ALTER TABLE "#__content" ALTER COLUMN "title_alias" SET DEFAULT '';
+ALTER TABLE "#__content" ALTER COLUMN "title_alias" SET NOT NULL;
 
 COMMENT ON COLUMN "#__content"."title_alias" IS 'Deprecated in Joomla! 3.0';
