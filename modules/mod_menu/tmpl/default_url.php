6dca6fb13338c4f8c54d49fe1fe3c80b1092662b
Elin Waring <elin.waring@gmail.com>
2012-02-01 10:24:59 +0100

    # [#27890] mod_menu invalid rendering of items of type alias and url. Thanks Elin.

--- modules/mod_menu/tmpl/default_url.php
+++ modules/mod_menu/tmpl/default_url.php
@@ -8,7 +8,7 @@
 
 // No direct access.
 defined('_JEXEC') or die;
-
+jimport('joomla.filter.output');
 // Note. It is important to remove spaces between elements.
 $class = $item->anchor_css ? 'class="'.$item->anchor_css.'" ' : '';
 $title = $item->anchor_title ? 'title="'.$item->anchor_title.'" ' : '';
@@ -20,18 +20,20 @@ if ($item->menu_image) {
 else { $linktype = $item->title;
 }
 $flink = $item->flink;
+$flink = JFilterOutput::ampReplace(htmlspecialchars($flink));
+
 switch ($item->browserNav) :
 	default:
 	case 0:
-?><a <?php echo $class; ?>href="<?php echo htmlspecialchars($flink); ?>" <?php echo $title; ?>><?php echo $linktype; ?></a><?php
+?><a <?php echo $class; ?>href="<?php echo $flink; ?>" <?php echo $title; ?>><?php echo $linktype; ?></a><?php
 		break;
 	case 1:
 		// _blank
-?><a <?php echo $class; ?>href="<?php echo htmlspecialchars($flink); ?>" target="_blank" <?php echo $title; ?>><?php echo $linktype; ?></a><?php
+?><a <?php echo $class; ?>href="<?php echo $flink; ?>" target="_blank" <?php echo $title; ?>><?php echo $linktype; ?></a><?php
 		break;
 	case 2:
 		// window.open
 		$options = 'toolbar=no,location=no,status=no,menubar=no,scrollbars=yes,resizable=yes,'.$params->get('window_open');
-			?><a <?php echo $class; ?>href="<?php echo htmlspecialchars($flink); ?>" onclick="window.open(this.href,'targetWindow','<?php echo $options;?>');return false;" <?php echo $title; ?>><?php echo $linktype; ?></a><?php
+			?><a <?php echo $class; ?>href="<?php echo $flink; ?>" onclick="window.open(this.href,'targetWindow','<?php echo $options;?>');return false;" <?php echo $title; ?>><?php echo $linktype; ?></a><?php
 		break;
 endswitch;
