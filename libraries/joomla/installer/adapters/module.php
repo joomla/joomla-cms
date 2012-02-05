b85e251eccba207176e706b1eb139f00b02d3f19
Reinhard Hiebl <service@hieblmedia.de>
2012-02-04 17:43:41 -0800

    [#27802] The package installer insert two database entries on templates

--- libraries/joomla/installer/adapters/module.php
+++ libraries/joomla/installer/adapters/module.php
@@ -243,7 +243,7 @@ class JInstallerModule extends JAdapterInstance
 			// Update function available or
 			// Update tag detected
 			if ($this->parent->getUpgrade() || ($this->parent->manifestClass && method_exists($this->parent->manifestClass, 'update'))
-				|| is_a($updateElement, 'JXMLElement'))
+				|| $updateElement)
 			{
 				// Force this one
 				$this->parent->setOverwrite(true);
--- libraries/joomla/installer/adapters/plugin.php
+++ libraries/joomla/installer/adapters/plugin.php
@@ -210,7 +210,7 @@ class JInstallerPlugin extends JAdapterInstance
 			// Update function available or
 			// Update tag detected
 			if ($this->parent->getUpgrade() || ($this->parent->manifestClass && method_exists($this->parent->manifestClass, 'update'))
-				|| is_a($updateElement, 'JXMLElement'))
+				|| $updateElement)
 			{
 				// Force this one
 				$this->parent->setOverwrite(true);
