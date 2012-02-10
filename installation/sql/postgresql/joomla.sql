457bda79abd7bbcebf1b3e31dd26e304043e009e
Gabriele Pongelli <gabriele.pongelli@gmail.com>
2012-02-10 08:07:46 +0100

    Added SOUNDEX function replacement for those who can't enable fuzzystrmatch contrib module.

--- installation/sql/postgresql/joomla.sql
+++ installation/sql/postgresql/joomla.sql
@@ -2320,3 +2320,65 @@ INSERT INTO "#__viewlevels" ("id", "title", "ordering", "rules") VALUES
 
 SELECT nextval('#__viewlevels_id_seq');
 SELECT setval('#__viewlevels_id_seq', 4, false);
+
+
+-- 
+-- Here is SOUNDEX replacement for those who can't enable fuzzystrmatch module
+--   from contrib folder.
+-- This function comes from https://wiki.postgresql.org/wiki/Soundex
+--   and is distributed with GPL license.
+-- Thanks to its author, Marti Raudsepp, that published this piece of code.
+--
+CREATE OR REPLACE FUNCTION soundex(input text) RETURNS text
+IMMUTABLE STRICT COST 500 LANGUAGE plpgsql
+AS $$
+DECLARE
+  soundex text = '';
+  char text;
+  symbol text;
+  last_symbol text = '';
+  pos int = 1;
+BEGIN
+  WHILE length(soundex) < 4 LOOP
+    char = upper(substr(input, pos, 1));
+    pos = pos + 1;
+    CASE char
+    WHEN '' THEN
+      -- End of input string
+      IF soundex = '' THEN
+        RETURN '';
+      ELSE
+        RETURN rpad(soundex, 4, '0');
+      END IF;
+    WHEN 'B', 'F', 'P', 'V' THEN
+      symbol = '1';
+    WHEN 'C', 'G', 'J', 'K', 'Q', 'S', 'X', 'Z' THEN
+      symbol = '2';
+    WHEN 'D', 'T' THEN
+      symbol = '3';
+    WHEN 'L' THEN
+      symbol = '4';
+    WHEN 'M', 'N' THEN
+      symbol = '5';
+    WHEN 'R' THEN
+      symbol = '6';
+    ELSE
+      -- Not a consonant; no output, but next similar consonant will be re-recorded
+      symbol = '';
+    END CASE;
+ 
+    IF soundex = '' THEN
+      -- First character; only accept strictly English ASCII characters
+      IF char ~>=~ 'A' AND char ~<=~ 'Z' THEN
+        soundex = char;
+        last_symbol = symbol;
+      END IF;
+    ELSIF last_symbol != symbol THEN
+      soundex = soundex || symbol;
+      last_symbol = symbol;
+    END IF;
+  END LOOP;
+ 
+  RETURN soundex;
+END;
+$$;
