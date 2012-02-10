a12dd042a006f883c7c2c65a3a5d7a47f06ca063
Mark Dexter <dextercowley@gmail.com>
2012-02-10 11:53:21 -0800

    Add Module0002 test class

--- tests/system/suite/TestSuite.php
+++ tests/system/suite/TestSuite.php
@@ -32,6 +32,7 @@ require_once 'com_users/group0001Test.php';
 require_once 'com_users/group0002Test.php';
 require_once 'com_users/group0003Test.php';
 require_once 'modules/module0001Test.php';
+require_once 'modules/module0002Test.php';
 require_once 'redirect/redirect0001Test.php';
 require_once 'sample_data/sample_data0001Test.php';
 require_once 'acl/acl0001Test.php';
@@ -76,6 +77,7 @@ class TestSuite
 		$suite->addTestSuite('Group0002Test');
 		$suite->addTestSuite('Group0003Test');
 		$suite->addTestSuite('Module0001');
+		$suite->addTestSuite('Module0002');
 		$suite->addTestSuite('Redirect0001Test');
 		$suite->addTestSuite('SampleData0001');
 		$suite->addTestSuite('Acl0001Test');
