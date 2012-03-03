f15d8f8ed80b4617aaba21ca586a0f8188181cac
Patrick Alt <chraneco@joomgallery.net>
2012-03-03 07:57:07 +0100

    # [#28179] JText values in js of quick icons force to escape single quotes. Thanks Patrick.

--- plugins/quickicon/joomlaupdate/joomlaupdate.php
+++ plugins/quickicon/joomlaupdate/joomlaupdate.php
@@ -52,9 +52,9 @@ class plgQuickiconJoomlaupdate extends JPlugin
 		$script = "var plg_quickicon_joomlaupdate_ajax_url = '$ajax_url';\n";
 		$script .= 'var plg_quickicon_jupdatecheck_jversion = "'.JVERSION.'";'."\n";
 		$script .= 'var plg_quickicon_joomlaupdate_text = {"UPTODATE" : "'.
-			JText::_('PLG_QUICKICON_JOOMLAUPDATE_UPTODATE').'", "UPDATEFOUND": "'.
-			JText::_('PLG_QUICKICON_JOOMLAUPDATE_UPDATEFOUND').'", "ERROR": "'.
-			JText::_('PLG_QUICKICON_JOOMLAUPDATE_ERROR')."\"};\n";
+			JText::_('PLG_QUICKICON_JOOMLAUPDATE_UPTODATE', true).'", "UPDATEFOUND": "'.
+			JText::_('PLG_QUICKICON_JOOMLAUPDATE_UPDATEFOUND', true).'", "ERROR": "'.
+			JText::_('PLG_QUICKICON_JOOMLAUPDATE_ERROR', true)."\"};\n";
 		$script .= 'var plg_quickicon_joomlaupdate_img = {"UPTODATE" : "'.
 			JURI::base(true) .'/templates/'. $cur_template .'/images/header/icon-48-jupdate-uptodate.png'.'", "ERROR": "'.
 			JURI::base(true) .'/templates/'. $cur_template .'/images/header/icon-48-deny.png'.'", "UPDATEFOUND": "'.
