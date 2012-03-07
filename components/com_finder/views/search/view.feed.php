10c47e10634ddb1dedfb3890a199d603e6f188ef
Benjamin <benjamin4ruby@github.com>
2012-03-07 08:54:56 +0100

    Revert "[#28154] RSS feed of com_finder throws fatal error"
    This reverts commit db11b3c780d6f0cbb4bbce314a496b2741dcd268.
    (To remove it from the pull request.)

--- components/com_finder/views/search/view.feed.php
+++ components/com_finder/views/search/view.feed.php
@@ -48,7 +48,7 @@ class FinderViewSearch extends JView
 		$explained = JHtml::_('query.explained', $query);
 
 		// Set the document title.
-		$title = $params->get('page_title', '');
+		$title = $this->params->get('page_title', '');
 
 		if (empty($title))
 		{
