531d2926e3b93978635a956df64ef40a4a8586c8
Hannes Papenberg <info@joomlager.de>
2012-03-24 12:02:16 +0100

    Fixing paths for JTree/JNode Tests

--- tests/suites/unit/legacy/base/JNodeTest.php
+++ tests/suites/unit/legacy/base/JNodeTest.php
@@ -7,7 +7,7 @@
  * @license     GNU General Public License version 2 or later; see LICENSE
  */
 
-require_once JPATH_PLATFORM.'/joomla/base/tree.php';
+require_once JPATH_PLATFORM.'/legacy/base/tree.php';
 
 /**
  * Test class for JNode.
