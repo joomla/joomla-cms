ed4222939591453f3165dea8d1e8adbc35bec072
bembelimen <bembelimen@web.de>
2012-03-25 09:40:32 +0300

    Fixed conflict between JGrid and JHtmlGrid when autoloaded.

--- libraries/joomla/html/html.php
+++ libraries/joomla/html/html.php
@@ -108,7 +108,7 @@ abstract class JHtml
 
 		$className = $prefix . ucfirst($file);
 
-		if (!class_exists($className))
+		if (!class_exists($className, false))
 		{
 			$path = JPath::find(self::$includePaths, strtolower($file) . '.php');
 			if ($path)
