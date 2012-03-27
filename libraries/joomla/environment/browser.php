65d7bfa6c2f79b9ad0338f729795022f36bfe48d
Nikolai Plath <der.el.kuku@gmail.com>
2012-03-27 12:06:07 -0500

    Rename protected members in JBrowser

--- libraries/joomla/environment/browser.php
+++ libraries/joomla/environment/browser.php
@@ -28,57 +28,57 @@ class JBrowser
 
 	/**
 	 * @var    integer  Major version number
-	 * @since  11.1
+	 * @since  12.1
 	 */
-	protected $_majorVersion = 0;
+	protected $majorVersion = 0;
 
 	/**
 	 * @var    integer  Minor version number
-	 * @since  11.1
+	 * @since  12.1
 	 */
-	protected $_minorVersion = 0;
+	protected $minorVersion = 0;
 
 	/**
 	 * @var    string  Browser name.
-	 * @since  11.1
+	 * @since  12.1
 	 */
-	protected $_browser = '';
+	protected $browser = '';
 
 	/**
 	 * @var    string  Full user agent string.
-	 * @since  11.1
+	 * @since  12.1
 	 */
-	protected $_agent = '';
+	protected $agent = '';
 
 	/**
 	 * @var    string  Lower-case user agent string
-	 * @since  11.1
+	 * @since  12.1
 	 */
-	protected $_lowerAgent = '';
+	protected $lowerAgent = '';
 
 	/**
 	 * @var    string  HTTP_ACCEPT string.
-	 * @since  11.1
+	 * @since  12.1
 	 */
-	protected $_accept = '';
+	protected $accept = '';
 
 	/**
 	 * @var    array  Parsed HTTP_ACCEPT string
-	 * @since  11.1
+	 * @since  12.1
 	 */
-	protected $_accept_parsed = array();
+	protected $acceptParsed = array();
 
 	/**
 	 * @var    string  Platform the browser is running on
-	 * @since  11.1
+	 * @since  12.1
 	 */
-	protected $_platform = '';
+	protected $platform = '';
 
 	/**
 	 * @var    array  Known robots.
-	 * @since  11.1
+	 * @since  12.1
 	 */
-	protected $_robots = array(
+	protected $robots = array(
 		/* The most common ones. */
 		'Googlebot',
 		'msnbot',
@@ -129,18 +129,18 @@ class JBrowser
 
 	/**
 	 * @var    boolean  Is this a mobile browser?
-	 * @since  11.1
+	 * @since  12.1
 	 */
-	protected $_mobile = false;
+	protected $mobile = false;
 
 	/**
 	 * List of viewable image MIME subtypes.
 	 * This list of viewable images works for IE and Netscape/Mozilla.
 	 *
 	 * @var    array
-	 * @since  11.1
+	 * @since  12.1
 	 */
-	protected $_images = array('jpeg', 'gif', 'png', 'pjpeg', 'x-png', 'bmp');
+	protected $images = array('jpeg', 'gif', 'png', 'pjpeg', 'x-png', 'bmp');
 
 	/**
 	 * @var    array  JBrowser instances container.
@@ -202,195 +202,195 @@ class JBrowser
 		{
 			if (isset($_SERVER['HTTP_USER_AGENT']))
 			{
-				$this->_agent = trim($_SERVER['HTTP_USER_AGENT']);
+				$this->agent = trim($_SERVER['HTTP_USER_AGENT']);
 			}
 		}
 		else
 		{
-			$this->_agent = $userAgent;
+			$this->agent = $userAgent;
 		}
-		$this->_lowerAgent = strtolower($this->_agent);
+		$this->lowerAgent = strtolower($this->agent);
 
 		// Set our accept string.
 		if (is_null($accept))
 		{
 			if (isset($_SERVER['HTTP_ACCEPT']))
 			{
-				$this->_accept = strtolower(trim($_SERVER['HTTP_ACCEPT']));
+				$this->accept = strtolower(trim($_SERVER['HTTP_ACCEPT']));
 			}
 		}
 		else
 		{
-			$this->_accept = strtolower($accept);
+			$this->accept = strtolower($accept);
 		}
 
-		if (!empty($this->_agent))
+		if (!empty($this->agent))
 		{
 			$this->_setPlatform();
 
-			if (strpos($this->_lowerAgent, 'mobileexplorer') !== false
-				|| strpos($this->_lowerAgent, 'openwave') !== false
-				|| strpos($this->_lowerAgent, 'opera mini') !== false
-				|| strpos($this->_lowerAgent, 'opera mobi') !== false
-				|| strpos($this->_lowerAgent, 'operamini') !== false)
+			if (strpos($this->lowerAgent, 'mobileexplorer') !== false
+				|| strpos($this->lowerAgent, 'openwave') !== false
+				|| strpos($this->lowerAgent, 'opera mini') !== false
+				|| strpos($this->lowerAgent, 'opera mobi') !== false
+				|| strpos($this->lowerAgent, 'operamini') !== false)
 			{
-				$this->_mobile = true;
+				$this->mobile = true;
 			}
-			elseif (preg_match('|Opera[/ ]([0-9.]+)|', $this->_agent, $version))
+			elseif (preg_match('|Opera[/ ]([0-9.]+)|', $this->agent, $version))
 			{
 				$this->setBrowser('opera');
-				list ($this->_majorVersion, $this->_minorVersion) = explode('.', $version[1]);
+				list ($this->majorVersion, $this->minorVersion) = explode('.', $version[1]);
 
 				/* Due to changes in Opera UA, we need to check Version/xx.yy,
 				 * but only if version is > 9.80. See: http://dev.opera.com/articles/view/opera-ua-string-changes/ */
-				if ($this->_majorVersion == 9 && $this->_minorVersion >= 80)
+				if ($this->majorVersion == 9 && $this->minorVersion >= 80)
 				{
 					$this->identifyBrowserVersion();
 				}
 			}
-			elseif (preg_match('|Chrome[/ ]([0-9.]+)|', $this->_agent, $version))
+			elseif (preg_match('|Chrome[/ ]([0-9.]+)|', $this->agent, $version))
 			{
 				$this->setBrowser('chrome');
-				list ($this->_majorVersion, $this->_minorVersion) = explode('.', $version[1]);
+				list ($this->majorVersion, $this->minorVersion) = explode('.', $version[1]);
 			}
-			elseif (preg_match('|CrMo[/ ]([0-9.]+)|', $this->_agent, $version))
+			elseif (preg_match('|CrMo[/ ]([0-9.]+)|', $this->agent, $version))
 			{
 				$this->setBrowser('chrome');
-				list ($this->_majorVersion, $this->_minorVersion) = explode('.', $version[1]);
+				list ($this->majorVersion, $this->minorVersion) = explode('.', $version[1]);
 			}
-			elseif (strpos($this->_lowerAgent, 'elaine/') !== false
-				|| strpos($this->_lowerAgent, 'palmsource') !== false
-				|| strpos($this->_lowerAgent, 'digital paths') !== false)
+			elseif (strpos($this->lowerAgent, 'elaine/') !== false
+				|| strpos($this->lowerAgent, 'palmsource') !== false
+				|| strpos($this->lowerAgent, 'digital paths') !== false)
 			{
 				$this->setBrowser('palm');
-				$this->_mobile = true;
+				$this->mobile = true;
 			}
-			elseif ((preg_match('|MSIE ([0-9.]+)|', $this->_agent, $version)) || (preg_match('|Internet Explorer/([0-9.]+)|', $this->_agent, $version)))
+			elseif ((preg_match('|MSIE ([0-9.]+)|', $this->agent, $version)) || (preg_match('|Internet Explorer/([0-9.]+)|', $this->agent, $version)))
 			{
 				$this->setBrowser('msie');
 
 				if (strpos($version[1], '.') !== false)
 				{
-					list ($this->_majorVersion, $this->_minorVersion) = explode('.', $version[1]);
+					list ($this->majorVersion, $this->minorVersion) = explode('.', $version[1]);
 				}
 				else
 				{
-					$this->_majorVersion = $version[1];
-					$this->_minorVersion = 0;
+					$this->majorVersion = $version[1];
+					$this->minorVersion = 0;
 				}
 
 				/* Some Handhelds have their screen resolution in the
 				 * user agent string, which we can use to look for
 				 * mobile agents.
 				 */
-				if (preg_match('/; (120x160|240x280|240x320|320x320)\)/', $this->_agent))
+				if (preg_match('/; (120x160|240x280|240x320|320x320)\)/', $this->agent))
 				{
-					$this->_mobile = true;
+					$this->mobile = true;
 				}
 			}
-			elseif (preg_match('|amaya/([0-9.]+)|', $this->_agent, $version))
+			elseif (preg_match('|amaya/([0-9.]+)|', $this->agent, $version))
 			{
 				$this->setBrowser('amaya');
-				$this->_majorVersion = $version[1];
+				$this->majorVersion = $version[1];
 				if (isset($version[2]))
 				{
-					$this->_minorVersion = $version[2];
+					$this->minorVersion = $version[2];
 				}
 			}
-			elseif (preg_match('|ANTFresco/([0-9]+)|', $this->_agent, $version))
+			elseif (preg_match('|ANTFresco/([0-9]+)|', $this->agent, $version))
 			{
 				$this->setBrowser('fresco');
 			}
-			elseif (strpos($this->_lowerAgent, 'avantgo') !== false)
+			elseif (strpos($this->lowerAgent, 'avantgo') !== false)
 			{
 				$this->setBrowser('avantgo');
-				$this->_mobile = true;
+				$this->mobile = true;
 			}
-			elseif (preg_match('|Konqueror/([0-9]+)|', $this->_agent, $version) || preg_match('|Safari/([0-9]+)\.?([0-9]+)?|', $this->_agent, $version))
+			elseif (preg_match('|Konqueror/([0-9]+)|', $this->agent, $version) || preg_match('|Safari/([0-9]+)\.?([0-9]+)?|', $this->agent, $version))
 			{
 				// Konqueror and Apple's Safari both use the KHTML
 				// rendering engine.
 				$this->setBrowser('konqueror');
-				$this->_majorVersion = $version[1];
+				$this->majorVersion = $version[1];
 				if (isset($version[2]))
 				{
-					$this->_minorVersion = $version[2];
+					$this->minorVersion = $version[2];
 				}
 
-				if (strpos($this->_agent, 'Safari') !== false && $this->_majorVersion >= 60)
+				if (strpos($this->agent, 'Safari') !== false && $this->majorVersion >= 60)
 				{
 					// Safari.
 					$this->setBrowser('safari');
 					$this->identifyBrowserVersion();
 				}
 			}
-			elseif (preg_match('|Mozilla/([0-9.]+)|', $this->_agent, $version))
+			elseif (preg_match('|Mozilla/([0-9.]+)|', $this->agent, $version))
 			{
 				$this->setBrowser('mozilla');
 
-				list ($this->_majorVersion, $this->_minorVersion) = explode('.', $version[1]);
+				list ($this->majorVersion, $this->minorVersion) = explode('.', $version[1]);
 			}
-			elseif (preg_match('|Lynx/([0-9]+)|', $this->_agent, $version))
+			elseif (preg_match('|Lynx/([0-9]+)|', $this->agent, $version))
 			{
 				$this->setBrowser('lynx');
 			}
-			elseif (preg_match('|Links \(([0-9]+)|', $this->_agent, $version))
+			elseif (preg_match('|Links \(([0-9]+)|', $this->agent, $version))
 			{
 				$this->setBrowser('links');
 			}
-			elseif (preg_match('|HotJava/([0-9]+)|', $this->_agent, $version))
+			elseif (preg_match('|HotJava/([0-9]+)|', $this->agent, $version))
 			{
 				$this->setBrowser('hotjava');
 			}
-			elseif (strpos($this->_agent, 'UP/') !== false || strpos($this->_agent, 'UP.B') !== false || strpos($this->_agent, 'UP.L') !== false)
+			elseif (strpos($this->agent, 'UP/') !== false || strpos($this->agent, 'UP.B') !== false || strpos($this->agent, 'UP.L') !== false)
 			{
 				$this->setBrowser('up');
-				$this->_mobile = true;
+				$this->mobile = true;
 			}
-			elseif (strpos($this->_agent, 'Xiino/') !== false)
+			elseif (strpos($this->agent, 'Xiino/') !== false)
 			{
 				$this->setBrowser('xiino');
-				$this->_mobile = true;
+				$this->mobile = true;
 			}
-			elseif (strpos($this->_agent, 'Palmscape/') !== false)
+			elseif (strpos($this->agent, 'Palmscape/') !== false)
 			{
 				$this->setBrowser('palmscape');
-				$this->_mobile = true;
+				$this->mobile = true;
 			}
-			elseif (strpos($this->_agent, 'Nokia') !== false)
+			elseif (strpos($this->agent, 'Nokia') !== false)
 			{
 				$this->setBrowser('nokia');
-				$this->_mobile = true;
+				$this->mobile = true;
 			}
-			elseif (strpos($this->_agent, 'Ericsson') !== false)
+			elseif (strpos($this->agent, 'Ericsson') !== false)
 			{
 				$this->setBrowser('ericsson');
-				$this->_mobile = true;
+				$this->mobile = true;
 			}
-			elseif (strpos($this->_lowerAgent, 'wap') !== false)
+			elseif (strpos($this->lowerAgent, 'wap') !== false)
 			{
 				$this->setBrowser('wap');
-				$this->_mobile = true;
+				$this->mobile = true;
 			}
-			elseif (strpos($this->_lowerAgent, 'docomo') !== false || strpos($this->_lowerAgent, 'portalmmm') !== false)
+			elseif (strpos($this->lowerAgent, 'docomo') !== false || strpos($this->lowerAgent, 'portalmmm') !== false)
 			{
 				$this->setBrowser('imode');
-				$this->_mobile = true;
+				$this->mobile = true;
 			}
-			elseif (strpos($this->_agent, 'BlackBerry') !== false)
+			elseif (strpos($this->agent, 'BlackBerry') !== false)
 			{
 				$this->setBrowser('blackberry');
-				$this->_mobile = true;
+				$this->mobile = true;
 			}
-			elseif (strpos($this->_agent, 'MOT-') !== false)
+			elseif (strpos($this->agent, 'MOT-') !== false)
 			{
 				$this->setBrowser('motorola');
-				$this->_mobile = true;
+				$this->mobile = true;
 			}
-			elseif (strpos($this->_lowerAgent, 'j-') !== false)
+			elseif (strpos($this->lowerAgent, 'j-') !== false)
 			{
 				$this->setBrowser('mml');
-				$this->_mobile = true;
+				$this->mobile = true;
 			}
 		}
 	}
@@ -408,17 +408,17 @@ class JBrowser
 	 */
 	protected function _setPlatform()
 	{
-		if (strpos($this->_lowerAgent, 'wind') !== false)
+		if (strpos($this->lowerAgent, 'wind') !== false)
 		{
-			$this->_platform = 'win';
+			$this->platform = 'win';
 		}
-		elseif (strpos($this->_lowerAgent, 'mac') !== false)
+		elseif (strpos($this->lowerAgent, 'mac') !== false)
 		{
-			$this->_platform = 'mac';
+			$this->platform = 'mac';
 		}
 		else
 		{
-			$this->_platform = 'unix';
+			$this->platform = 'unix';
 		}
 	}
 
@@ -431,7 +431,7 @@ class JBrowser
 	 */
 	public function getPlatform()
 	{
-		return $this->_platform;
+		return $this->platform;
 	}
 
 	/**
@@ -442,15 +442,15 @@ class JBrowser
 	 */
 	protected function identifyBrowserVersion()
 	{
-		if (preg_match('|Version[/ ]([0-9.]+)|', $this->_agent, $version))
+		if (preg_match('|Version[/ ]([0-9.]+)|', $this->agent, $version))
 		{
-			list ($this->_majorVersion, $this->_minorVersion) = explode('.', $version[1]);
+			list ($this->majorVersion, $this->minorVersion) = explode('.', $version[1]);
 			return;
 		}
 		// Can't identify browser version
-		$this->_majorVersion = 0;
-		$this->_minorVersion = 0;
-		JLog::add("Can't identify browser version. Agent: " . $this->_agent, JLog::NOTICE);
+		$this->majorVersion = 0;
+		$this->minorVersion = 0;
+		JLog::add("Can't identify browser version. Agent: " . $this->agent, JLog::NOTICE);
 	}
 
 	/**
@@ -464,7 +464,7 @@ class JBrowser
 	 */
 	public function setBrowser($browser)
 	{
-		$this->_browser = $browser;
+		$this->browser = $browser;
 	}
 
 	/**
@@ -476,7 +476,7 @@ class JBrowser
 	 */
 	public function getBrowser()
 	{
-		return $this->_browser;
+		return $this->browser;
 	}
 
 	/**
@@ -488,7 +488,7 @@ class JBrowser
 	 */
 	public function getMajor()
 	{
-		return $this->_majorVersion;
+		return $this->majorVersion;
 	}
 
 	/**
@@ -500,7 +500,7 @@ class JBrowser
 	 */
 	public function getMinor()
 	{
-		return $this->_minorVersion;
+		return $this->minorVersion;
 	}
 
 	/**
@@ -512,7 +512,7 @@ class JBrowser
 	 */
 	public function getVersion()
 	{
-		return $this->_majorVersion . '.' . $this->_minorVersion;
+		return $this->majorVersion . '.' . $this->minorVersion;
 	}
 
 	/**
@@ -524,7 +524,7 @@ class JBrowser
 	 */
 	public function getAgentString()
 	{
-		return $this->_agent;
+		return $this->agent;
 	}
 
 	/**
@@ -564,16 +564,16 @@ class JBrowser
 		$mimetype = strtolower($mimetype);
 		list ($type, $subtype) = explode('/', $mimetype);
 
-		if (!empty($this->_accept))
+		if (!empty($this->accept))
 		{
 			$wildcard_match = false;
 
-			if (strpos($this->_accept, $mimetype) !== false)
+			if (strpos($this->accept, $mimetype) !== false)
 			{
 				return true;
 			}
 
-			if (strpos($this->_accept, '*/*') !== false)
+			if (strpos($this->accept, '*/*') !== false)
 			{
 				$wildcard_match = true;
 				if ($type != 'image')
@@ -583,7 +583,7 @@ class JBrowser
 			}
 
 			// Deal with Mozilla pjpeg/jpeg issue
-			if ($this->isBrowser('mozilla') && ($mimetype == 'image/pjpeg') && (strpos($this->_accept, 'image/jpeg') !== false))
+			if ($this->isBrowser('mozilla') && ($mimetype == 'image/pjpeg') && (strpos($this->accept, 'image/jpeg') !== false))
 			{
 				return true;
 			}
@@ -599,7 +599,7 @@ class JBrowser
 			return false;
 		}
 
-		return (in_array($subtype, $this->_images));
+		return (in_array($subtype, $this->images));
 	}
 
 	/**
@@ -613,7 +613,7 @@ class JBrowser
 	 */
 	public function isBrowser($browser)
 	{
-		return ($this->_browser === $browser);
+		return ($this->browser === $browser);
 	}
 
 	/**
@@ -625,9 +625,9 @@ class JBrowser
 	 */
 	public function isRobot()
 	{
-		foreach ($this->_robots as $robot)
+		foreach ($this->robots as $robot)
 		{
-			if (strpos($this->_agent, $robot) !== false)
+			if (strpos($this->agent, $robot) !== false)
 			{
 				return true;
 			}
@@ -644,7 +644,7 @@ class JBrowser
 	 */
 	public function isMobile()
 	{
-		return $this->_mobile;
+		return $this->mobile;
 	}
 
 	/**
