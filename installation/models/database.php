127c325ab9c042710fd8f7c9c3690efac0825b03
Gabriele Pongelli <gabriele.pongelli@gmail.com>
2012-03-30 21:42:47 +0200

    Added validation for database's port, only five numbers or empty field for default use. Changed helpers' call.

--- installation/models/database.php
+++ installation/models/database.php
@@ -44,6 +44,12 @@ class JInstallationModelDatabase extends JModel
 			return false;
 		}
 
+		// Validate database port.
+		if (!empty($options->db_port) && !preg_match('/^[0-9]{0,5}$/', $options->db_port)) {
+			$this->setError(JText::_('INSTL_DATABASE_INVALID_DB_PORT'));
+			return false;
+		}
+
 		// Ensure that a database name was input.
 		if (empty($options->db_name)) {
 			$this->setError(JText::_('INSTL_DATABASE_EMPTY_NAME'));
@@ -73,7 +79,7 @@ class JInstallationModelDatabase extends JModel
 			// Get a database object.
 			try
 			{
-				$db = JInstallationHelperDatabase::getDbo($options->db_type, $options->db_host, $options->db_user, $options->db_pass, null, $options->db_prefix, false);
+				$db = JInstallationHelperDatabase::getDbo($options->db_type, $options->db_host, $options->db_port, $options->db_user, $options->db_pass, null, $options->db_prefix, false);
 			}
 			catch (JDatabaseException $e)
 			{
@@ -273,7 +279,7 @@ class JInstallationModelDatabase extends JModel
 		// Get a database object.
 		try
 		{
-			$db = JInstallationHelperDatabase::getDBO($options->db_type, $options->db_host, $options->db_user, $options->db_pass, $options->db_name, $options->db_prefix);
+			$db = JInstallationHelperDatabase::getDBO($options->db_type, $options->db_host, $options->db_port, $options->db_user, $options->db_pass, $options->db_name, $options->db_prefix);
 		}
 		catch (JDatabaseException $e)
 		{
