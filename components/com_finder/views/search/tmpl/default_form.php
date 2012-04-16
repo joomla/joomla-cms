9abbc2bd5bc83fc055e4d8da83da513d84a4a19d
fititnt <emerson@webdesign.eng.br>
2012-04-16 08:00:04 -0300

    com_finder: remove duplicated code on frontend search view ! The behavior of allow or not submit form is handled by javascript on page. The HTML, by default, will allow, so will fallback even if javascript disabled

--- components/com_finder/views/search/tmpl/default_form.php
+++ components/com_finder/views/search/tmpl/default_form.php
@@ -92,11 +92,7 @@ defined('_JEXEC') or die;
 			<?php echo JText::_('COM_FINDER_SEARCH_TERMS'); ?>
 		</label>
 		<input type="text" name="q" id="q" size="30" value="<?php echo $this->escape($this->query->input); ?>" class="inputbox" />
-		<?php if ($this->escape($this->query->input) != '' || $this->params->get('allow_empty_search')):?>
-			<button name="Search" id="s" type="submit" class="button"><?php echo JText::_('JSEARCH_FILTER_SUBMIT');?></button>
-		<?php else: ?>
-			<button name="Search" id="s" type="submit" class="button"><?php echo JText::_('JSEARCH_FILTER_SUBMIT');?></button>
-		<?php endif; ?>
+		<button name="Search" id="s" type="submit" class="button"><?php echo JText::_('JSEARCH_FILTER_SUBMIT');?></button>
 </fieldset>
 
 	<?php if ($this->params->get('show_advanced', 1)): ?>
