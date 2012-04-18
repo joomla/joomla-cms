7dfcc78bed54798ac4d8fe0ed148579a7b245b01
Michael Babker <mbabker@flbab.com>
2012-04-18 10:36:37 +0200

    # [#28351] Remove extra images from finder. Thanks Rouven and Michael

--- administrator/components/com_finder/views/index/tmpl/default.php
+++ administrator/components/com_finder/views/index/tmpl/default.php
@@ -117,7 +117,7 @@ Joomla.submitbutton = function(pressbutton) {
 				</td>
 				<td>
 					<?php if (intval($item->publish_start_date) or intval($item->publish_end_date) or intval($item->start_date) or intval($item->end_date)) : ?>
-					<img src="<?php echo JURI::root();?>/media/com_finder/images/calendar.png" style="border:1px;float:right" class="hasTip" title="<?php echo JText::sprintf('COM_FINDER_INDEX_DATE_INFO', $item->publish_start_date, $item->publish_end_date, $item->start_date, $item->end_date);?>" />
+					<img src="<?php echo JURI::root();?>/media/system/images/calendar.png" style="border:1px;float:right" class="hasTip" title="<?php echo JText::sprintf('COM_FINDER_INDEX_DATE_INFO', $item->publish_start_date, $item->publish_end_date, $item->start_date, $item->end_date);?>" />
 					<?php endif; ?>
 					<?php echo $this->escape($item->title); ?>
 				</td>
