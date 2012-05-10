cfc4750c6753bc6e125c787bab59db970f1f6485
chdemko <chdemko@gmail.com>
2012-05-10 20:57:34 +0200

    Correcting checkstyle

--- libraries/joomla/input/input.php
+++ libraries/joomla/input/input.php
@@ -68,7 +68,7 @@ class JInput implements Serializable
 	/**
 	 * Constructor.
 	 *
-	 * @param   array  $source   Source data (Optional, default is $_REQUEST)
+	 * @param   array  &$source  Source data (Optional, default is $_REQUEST)
 	 * @param   array  $options  Array of configuration parameters (Optional)
 	 *
 	 * @since   11.1
