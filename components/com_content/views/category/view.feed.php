29cc1a45cb8d1ca0ed0773d0ed43d736ed09e0df
Twentronix <info@twentronix.nl>
2012-05-24 12:44:15 +0300

    Added languagetag for readmore link. Reformated comments.

--- components/com_content/views/category/view.feed.php
+++ components/com_content/views/category/view.feed.php
@@ -36,46 +36,38 @@ class ContentViewCategory extends JView
 
 		foreach ($rows as $row)
 		{
-			// strip html from feed item title
+			// Strip html from feed item title
 			$title = $this->escape($row->title);
 			$title = html_entity_decode($title, ENT_COMPAT, 'UTF-8');
 
-			// compute the article slug
+			// Compute the article slug
 			$row->slug = $row->alias ? ($row->id . ':' . $row->alias) : $row->id;
 
-			// url link to article
+			// Url link to article
 			$link = JRoute::_(ContentHelperRoute::getArticleRoute($row->slug, $row->catid));
 
-			// get row fulltext
-			// TODO: $row->fulltext fix in model (?), so it works like it's supposed to
-			// temporarily solved this way because it's not working (fulltext is protected in MySQL!). 
-			// the `` are missing somewhere in a model I think, perhaps in /public_html/components/com_content/models/article.php
-			// add it with function $db->quoteName('fulltext')
+			// Get row fulltext
 			$db 				=& JFactory::getDBO();
 			$query 				= "SELECT `fulltext` FROM #__content WHERE id =".$row->id.";";
 			$db->setQuery($query);
 			$row->fulltext 		= $db->loadResult();
 
-			// strip html from feed item description text 
-			// TODO (?): I don't see this happening. Use a parameter for this if it's needed: Strip HTML?)
 			$description		= ($params->get('feed_summary', 0) ? $row->introtext.$row->fulltext : $row->introtext);
 			// TODO(?): if created_by_alias is empty, it won't show anything
 			$author				= $row->created_by_alias ? $row->created_by_alias : $row->author;
 			@$date				= ($row->created ? date('r', strtotime($row->created)) : '');
 
-			// load individual item creator class
+			// Load individual item creator class
 			$item 				= new JFeedItem();
 			$item->title		= $title;
 			$item->link			= $link;
 			$item->description	= $description;
 
-			// add readmore to description if introtext is shown and showreadmore is true
-			if (!$params->get('feed_summary', 0) && $params->get('feed_show_readmore', 0)) { 
-				// only show readmore if there is more to read
+			// Add readmore to description if introtext is shown and showreadmore is true
+			if (!$params->get('feed_summary', 0) && $params->get('feed_show_readmore', 0)) {
+				// Only add readmore link if there is more to read 
 				if ($row->fulltext != '') {
-					// add readmore link
-					// TODO: Language tag for readmore
-					$item->description .= '<br /><a class="feed-readmore" target="_blank" href ="'.rtrim(JURI::base(), "/").str_replace(' ', '%20', $item->link).'">Read more »</a>';
+					$item->description .= '<br /><a class="feed-readmore" target="_blank" href ="'.rtrim(JURI::base(), "/").str_replace(' ', '%20', $item->link).'">'.JText::_('COM_CONTENT_FEED_READMORE').'</a>';
 				}
 			}
 
@@ -85,7 +77,7 @@ class ContentViewCategory extends JView
 			// TODO: add option in Joomla core to let the authorEmail empty (Feed e-mail:none). Users will love this for spam prevention.
 			$item->authorEmail	= (($feedEmail == 'site') ? $siteEmail : $row->author_email);
 
-			// loads item info into rss array
+			// Loads item info into rss array
 			$doc->addItem($item);
 		}
 	}
