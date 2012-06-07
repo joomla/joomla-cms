f15b613e0a41f375d5b3ea3ad514c9b7736bdf23
oc666 Ofer Cohen <oc666@netvision.net.il>
2012-06-07 16:44:16 +0200

    # [#28192] Bug in mod_articles_news in version 1.7 and 2.5. Thanks Heikki and Ofer.

--- modules/mod_articles_news/tmpl/_item.php
+++ modules/mod_articles_news/tmpl/_item.php
@@ -8,17 +8,18 @@
 
 // no direct access
 defined('_JEXEC') or die;
+$item_heading = $params->get('item_heading', 'h4');
 ?>
 <?php if ($params->get('item_title')) : ?>
 
-	<<?php echo $params->get('item_heading'); ?> class="newsflash-title<?php echo $params->get('moduleclass_sfx'); ?>">
+	<<?php echo $item_heading; ?> class="newsflash-title<?php echo $params->get('moduleclass_sfx'); ?>">
 	<?php if ($params->get('link_titles') && $item->link != '') : ?>
 		<a href="<?php echo $item->link;?>">
 			<?php echo $item->title;?></a>
 	<?php else : ?>
 		<?php echo $item->title; ?>
 	<?php endif; ?>
-	</<?php echo $params->get('item_heading'); ?>>
+	</<?php echo $item_heading; ?>>
 
 <?php endif; ?>
 
