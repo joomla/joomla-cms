2cd951f2ba8cda7bd8ece04787aa769b5c933a3b
Mark Dexter <dextercowley@gmail.com>
2012-06-19 09:15:28 -0700

    Fix new lines on SQL files

--- administrator/components/com_admin/sql/updates/sqlazure/2.5.5.sql
+++ administrator/components/com_admin/sql/updates/sqlazure/2.5.5.sql
@@ -1,1 +1,3 @@
-ALTER TABLE [#__redirect_links] ADD [hits] INTEGER CONSTRAINT DF_redirect_links_hits DEFAULT '' NOT NULL;ALTER TABLE [#__users] ADD [lastResetTime] [datetime] NOT NULL;ALTER TABLE [#__users] ADD [resetCount] [int] NOT NULL;
+ALTER TABLE [#__redirect_links] ADD [hits] INTEGER CONSTRAINT DF_redirect_links_hits DEFAULT '' NOT NULL;
+ALTER TABLE [#__users] ADD [lastResetTime] [datetime] NOT NULL;
+ALTER TABLE [#__users] ADD [resetCount] [int] NOT NULL;
