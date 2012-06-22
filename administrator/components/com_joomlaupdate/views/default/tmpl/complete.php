f9765e4479e8c59fb29a345e35e64523d76148dd
Mark Dexter <dextercowley@gmail.com>
2012-06-22 10:33:03 +0200

    # [#28709] Commit of progress bar feature missed a layout file. Thanks Mark

---
+++ administrator/components/com_joomlaupdate/views/default/tmpl/complete.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_joomlaupdate
+ * @copyright   Copyright (C) 2005 - 2012 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ * @since       2.5.5
+ */
+
+defined('_JEXEC') or die;
+?>
+
+<fieldset>
+	<legend>
+		<?php echo JText::_('COM_JOOMLAUPDATE_VIEW_COMPLETE_HEADING') ?>
+	</legend>
+	<p>
+		<?php echo JText::sprintf('COM_JOOMLAUPDATE_VIEW_COMPLETE_MESSAGE', JVERSION); ?>
+	</p>
+</fieldset>
