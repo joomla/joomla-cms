fb69dff95f43aa906210c0445f09cc88027d6a14
Rouven Weßling <me@rouvenwessling.de>
2012-07-03 17:33:22 +0200

    Fix the colored badges in the installation.

--- installation/helpers/html/installation.php
+++ installation/helpers/html/installation.php
@@ -45,12 +45,12 @@ class JHtmlInstallation
 		}
 
  		$html = '<ul class="nav nav-tabs">' .
-			'<li class="step'.($on == 1 ? ' active' : '').'" id="language"><a href="#"><span class="badge '.($on == 1 ? 'badge-warning' : '').'">1</span> '.JText::_('INSTL_STEP_1_LABEL').'</a></li>' .
-			'<li class="step'.($on == 2 ? ' active' : '').'" id="preinstall"><a href="#"><span class="badge '.($on == 2 ? 'badge-warning' : '').'">2</span> '.JText::_('INSTL_STEP_2_LABEL').'</a></li>' .
-			'<li class="step'.($on == 3 ? ' active' : '').'" id="database"><a href="#"><span class="badge '.($on == 3 ? 'badge-warning' : '').'">3</span> '.JText::_('INSTL_STEP_4_LABEL').'</a></li>' .
-			'<li class="step'.($on == 4 ? ' active' : '').'" id="filesystem"><a href="#"><span class="badge '.($on == 4 ? 'badge-warning' : '').'">4</span> '.JText::_('INSTL_STEP_5_LABEL').'</a></li>' .
-			'<li class="step'.($on == 5 ? ' active' : '').'" id="site"><a href="#"><span class="badge '.($on == 5 ? 'badge-warning' : '').'">5</span> '.JText::_('INSTL_STEP_6_LABEL').'</a></li>' .
-			'<li class="step'.($on == 6 ? ' active' : '').'" id="complete"><a href="#"><span class="badge '.($on == 6 ? 'badge-warning' : '').'">6</span> '.JText::_('INSTL_STEP_7_LABEL').'</a></li>'.
+			'<li class="step'.($on == 1 ? ' active' : '').'" id="language"><a href="#"><span class="badge">1</span> '.JText::_('INSTL_STEP_1_LABEL').'</a></li>' .
+			'<li class="step'.($on == 2 ? ' active' : '').'" id="preinstall"><a href="#"><span class="badge">2</span> '.JText::_('INSTL_STEP_2_LABEL').'</a></li>' .
+			'<li class="step'.($on == 3 ? ' active' : '').'" id="database"><a href="#"><span class="badge">3</span> '.JText::_('INSTL_STEP_4_LABEL').'</a></li>' .
+			'<li class="step'.($on == 4 ? ' active' : '').'" id="filesystem"><a href="#"><span class="badge">4</span> '.JText::_('INSTL_STEP_5_LABEL').'</a></li>' .
+			'<li class="step'.($on == 5 ? ' active' : '').'" id="site"><a href="#"><span class="badge">5</span> '.JText::_('INSTL_STEP_6_LABEL').'</a></li>' .
+			'<li class="step'.($on == 6 ? ' active' : '').'" id="complete"><a href="#"><span class="badge">6</span> '.JText::_('INSTL_STEP_7_LABEL').'</a></li>'.
 			'</ul>';
 			return $html;
 	}
