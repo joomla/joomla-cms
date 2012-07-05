99ebd12239312a84257a4ae7bfcf9ceb3dad14f0
dianaprajescu <diana.prajescu@gmail.com>
2012-07-05 17:01:37 +0300

    Improve the Help object.
    Add getConfiguration() method to JTwitterHelp.
    
    Add test() method to JTwitterHelp.

--- libraries/joomla/twitter/help.php
+++ libraries/joomla/twitter/help.php
@@ -37,6 +37,48 @@ class JTwitterHelp extends JTwitterObject
 		$path = $base;
 
 		// Send the request.
-		return $this->sendRequest($path, 200);
+		return $this->sendRequest($path);
+	}
+
+	/**
+	 * Method to get the current configuration used by Twitter including twitter.com slugs which are not usernames,
+	 * maximum photo resolutions, and t.co URL lengths.
+	 *
+	 * @return  array  The decoded JSON response
+	 *
+	 * @since   12.1
+	 */
+	public function getConfiguration()
+	{
+		// Check the rate limit for remaining hits
+		$this->checkRateLimit();
+
+		// Set the API base
+		$base = '/1/help/configuration.json';
+
+		// Build the request path.
+		$path = $base;
+
+		// Send the request.
+		return $this->sendRequest($path);
+	}
+
+	/**
+	 * Method for sending a HEAD request to determine Twitter's servers current time.
+	 *
+	 * @return  string  The string "ok" in the requested format with a 200 OK HTTP status code.
+	 *
+	 * @since   12.1
+	 */
+	public function test()
+	{
+		// Set the API base
+		$base = '/1/help/test.json';
+
+		// Build the request path.
+		$path = $base;
+
+		// Send the request.
+		return $this->sendRequest($path);
 	}
 }
