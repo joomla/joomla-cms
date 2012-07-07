405e02abb0c36d54caea1c8474d6bdfe4f73ad8f
kyleledbetter <kyle@kyleledbetter.com>
2012-07-07 11:04:51 -0500

    - adding left sidebar and submenu to Banners

--- administrator/components/com_banners/views/clients/tmpl/default.php
+++ administrator/components/com_banners/views/clients/tmpl/default.php
@@ -21,112 +21,131 @@ $params		= (isset($this->state->params)) ? $this->state->params : new JObject;
 ?>
 
 <form action="<?php echo JRoute::_('index.php?option=com_banners&view=clients'); ?>" method="post" name="adminForm" id="adminForm">
-	<div id="filter-bar" class="btn-toolbar">
-		<div class="btn-group pull-right">
-			<a data-toggle="collapse" data-target="#filters" class="btn"><?php echo JText::_('JSEARCH_FILTER_LABEL'); ?> <span class="caret"></span></a>
+	<div class="row-fluid">
+		<!-- Begin Sidebar -->
+		<div id="sidebar" class="span2">
+			<div class="sidebar-nav">
+				<?php
+					// Display the submenu position modules
+					$this->modules = JModuleHelper::getModules('submenu');
+					foreach ($this->modules as $module) {
+						$output = JModuleHelper::renderModule($module);
+						$params = new JRegistry;
+						$params->loadString($module->params);
+						echo $output;
+					}
+				?>
+				<hr />
+				<div class="filter-select">
+					<h4 class="page-header"><?php echo JText::_('JSEARCH_FILTER_LABEL');?></h4>
+					<select name="filter_state" class="span12 small" onchange="this.form.submit()">
+						<option value=""><?php echo JText::_('JOPTION_SELECT_PUBLISHED');?></option>
+						<?php echo JHtml::_('select.options', JHtml::_('jgrid.publishedOptions'), 'value', 'text', $this->state->get('filter.state'), true);?>
+					</select>
+				</div>
+			</div>
 		</div>
-		<div class="filter-search btn-group pull-left">
-			<input type="text" name="filter_search" id="filter_search" placeholder="<?php echo JText::_('COM_BANNERS_SEARCH_IN_TITLE'); ?>" value="<?php echo $this->escape($this->state->get('filter.search')); ?>" title="<?php echo JText::_('COM_BANNERS_SEARCH_IN_TITLE'); ?>" />
+		<!-- End Sidebar -->
+		<!-- Begin Content -->
+		<div class="span10">
+			<div id="filter-bar" class="btn-toolbar">
+				<div class="filter-search btn-group pull-left">
+					<input type="text" name="filter_search" id="filter_search" placeholder="<?php echo JText::_('COM_BANNERS_SEARCH_IN_TITLE'); ?>" value="<?php echo $this->escape($this->state->get('filter.search')); ?>" title="<?php echo JText::_('COM_BANNERS_SEARCH_IN_TITLE'); ?>" />
+				</div>
+				<div class="btn-group pull-left">
+					<button class="btn" rel="tooltip" type="submit" title="<?php echo JText::_('JSEARCH_FILTER_SUBMIT'); ?>"><i class="icon-search"></i></button>
+					<button class="btn" rel="tooltip" type="button" title="<?php echo JText::_('JSEARCH_FILTER_CLEAR'); ?>" onclick="document.id('filter_search').value='';this.form.submit();"><i class="icon-remove"></i></button>
+				</div>
+			</div>
+			<div class="clearfix"> </div>
+			<table class="table table-striped">
+				<thead>
+					<tr>
+						<th width="1%">
+							<input type="checkbox" name="checkall-toggle" value="" title="<?php echo JText::_('JGLOBAL_CHECK_ALL'); ?>" onclick="Joomla.checkAll(this)" />
+						</th>
+						<th>
+							<?php echo JHtml::_('grid.sort', 'COM_BANNERS_HEADING_CLIENT', 'a.name', $listDirn, $listOrder); ?>
+						</th>
+						<th width="30%">
+							<?php echo JHtml::_('grid.sort', 'COM_BANNERS_HEADING_CONTACT', 'contact', $listDirn, $listOrder); ?>
+						</th>
+						<th width="5%">
+							<?php echo JHtml::_('grid.sort', 'COM_BANNERS_HEADING_ACTIVE', 'nbanners', $listDirn, $listOrder); ?>
+						</th>
+						<th width="5%" class="nowrap">
+							<?php echo JText::_('COM_BANNERS_HEADING_METAKEYWORDS'); ?>
+						</th>
+						<th width="10%">
+							<?php echo JText::_('COM_BANNERS_HEADING_PURCHASETYPE'); ?>
+						</th>
+						<th width="1%" class="nowrap">
+							<?php echo JHtml::_('grid.sort', 'JGRID_HEADING_ID', 'a.id', $listDirn, $listOrder); ?>
+						</th>
+					</tr>
+				</thead>
+				<tfoot>
+					<tr>
+						<td colspan="8">
+							<?php echo $this->pagination->getListFooter(); ?>
+						</td>
+					</tr>
+				</tfoot>
+				<tbody>
+				<?php foreach ($this->items as $i => $item) :
+					$ordering	= ($listOrder == 'ordering');
+					$canCreate	= $user->authorise('core.create',		'com_banners');
+					$canEdit	= $user->authorise('core.edit',			'com_banners');
+					$canCheckin	= $user->authorise('core.manage',		'com_checkin') || $item->checked_out==$user->get('id') || $item->checked_out==0;
+					$canChange	= $user->authorise('core.edit.state',	'com_banners') && $canCheckin;
+					?>
+					<tr class="row<?php echo $i % 2; ?>">
+						<td class="center">
+							<?php echo JHtml::_('grid.id', $i, $item->id); ?>
+						</td>
+						<td class="nowrap">
+							<?php echo JHtml::_('jgrid.published', $item->state, $i, 'clients.', $canChange);?>
+							<?php if ($item->checked_out) : ?>
+								<?php echo JHtml::_('jgrid.checkedout', $i, $item->editor, $item->checked_out_time, 'clients.', $canCheckin); ?>
+							<?php endif; ?>
+							<?php if ($canEdit) : ?>
+								<a href="<?php echo JRoute::_('index.php?option=com_banners&task=client.edit&id='.(int) $item->id); ?>">
+									<?php echo $this->escape($item->name); ?></a>
+							<?php else : ?>
+									<?php echo $this->escape($item->name); ?>
+							<?php endif; ?>
+						</td>
+						<td class="small">
+							<?php echo $item->contact;?>
+						</td>
+						<td class="center">
+							<?php echo $item->nbanners; ?>
+						</td>
+						<td class="small">
+							<?php echo $item->metakey; ?>
+						</td>
+						<td class="small">
+							<?php if ($item->purchase_type<0):?>
+								<?php echo JText::sprintf('COM_BANNERS_DEFAULT', JText::_('COM_BANNERS_FIELD_VALUE_'.$params->get('purchase_type')));?>
+							<?php else:?>
+								<?php echo JText::_('COM_BANNERS_FIELD_VALUE_'.$item->purchase_type);?>
+							<?php endif;?>
+						</td>
+						<td class="center">
+							<?php echo $item->id; ?>
+						</td>
+					</tr>
+					<?php endforeach; ?>
+				</tbody>
+			</table>
+			<div>
+				<input type="hidden" name="task" value="" />
+				<input type="hidden" name="boxchecked" value="0" />
+				<input type="hidden" name="filter_order" value="<?php echo $listOrder; ?>" />
+				<input type="hidden" name="filter_order_Dir" value="<?php echo $listDirn; ?>" />
+				<?php echo JHtml::_('form.token'); ?>
+			</div>
 		</div>
-		<div class="btn-group pull-left">
-			<button class="btn tip" type="submit" title="<?php echo JText::_('JSEARCH_FILTER_SUBMIT'); ?>"><i class="icon-search"></i></button>
-			<button class="btn tip" type="button" title="<?php echo JText::_('JSEARCH_FILTER_CLEAR'); ?>" onclick="document.id('filter_search').value='';this.form.submit();"><i class="icon-remove"></i></button>
-		</div>
-	</div>
-	<div class="clearfix"> </div>
-	<div class="collapse" id="filters">
-		<div class="filter-select well">
-			<select name="filter_state" class="inputbox" onchange="this.form.submit()">
-				<option value=""><?php echo JText::_('JOPTION_SELECT_PUBLISHED');?></option>
-				<?php echo JHtml::_('select.options', JHtml::_('jgrid.publishedOptions'), 'value', 'text', $this->state->get('filter.state'), true);?>
-			</select>
-		</div>
-	</div>
-	<table class="table table-striped">
-		<thead>
-			<tr>
-				<th width="1%">
-					<input type="checkbox" name="checkall-toggle" value="" title="<?php echo JText::_('JGLOBAL_CHECK_ALL'); ?>" onclick="Joomla.checkAll(this)" />
-				</th>
-				<th>
-					<?php echo JHtml::_('grid.sort', 'COM_BANNERS_HEADING_CLIENT', 'a.name', $listDirn, $listOrder); ?>
-				</th>
-				<th width="30%">
-					<?php echo JHtml::_('grid.sort', 'COM_BANNERS_HEADING_CONTACT', 'contact', $listDirn, $listOrder); ?>
-				</th>
-				<th width="5%">
-					<?php echo JHtml::_('grid.sort', 'COM_BANNERS_HEADING_ACTIVE', 'nbanners', $listDirn, $listOrder); ?>
-				</th>
-				<th width="5%" class="nowrap">
-					<?php echo JText::_('COM_BANNERS_HEADING_METAKEYWORDS'); ?>
-				</th>
-				<th width="10%">
-					<?php echo JText::_('COM_BANNERS_HEADING_PURCHASETYPE'); ?>
-				</th>
-				<th width="1%" class="nowrap">
-					<?php echo JHtml::_('grid.sort', 'JGRID_HEADING_ID', 'a.id', $listDirn, $listOrder); ?>
-				</th>
-			</tr>
-		</thead>
-		<tfoot>
-			<tr>
-				<td colspan="8">
-					<?php echo $this->pagination->getListFooter(); ?>
-				</td>
-			</tr>
-		</tfoot>
-		<tbody>
-		<?php foreach ($this->items as $i => $item) :
-			$ordering	= ($listOrder == 'ordering');
-			$canCreate	= $user->authorise('core.create',		'com_banners');
-			$canEdit	= $user->authorise('core.edit',			'com_banners');
-			$canCheckin	= $user->authorise('core.manage',		'com_checkin') || $item->checked_out==$user->get('id') || $item->checked_out==0;
-			$canChange	= $user->authorise('core.edit.state',	'com_banners') && $canCheckin;
-			?>
-			<tr class="row<?php echo $i % 2; ?>">
-				<td class="center">
-					<?php echo JHtml::_('grid.id', $i, $item->id); ?>
-				</td>
-				<td class="nowrap">
-					<?php echo JHtml::_('jgrid.published', $item->state, $i, 'clients.', $canChange);?>
-					<?php if ($item->checked_out) : ?>
-						<?php echo JHtml::_('jgrid.checkedout', $i, $item->editor, $item->checked_out_time, 'clients.', $canCheckin); ?>
-					<?php endif; ?>
-					<?php if ($canEdit) : ?>
-						<a href="<?php echo JRoute::_('index.php?option=com_banners&task=client.edit&id='.(int) $item->id); ?>">
-							<?php echo $this->escape($item->name); ?></a>
-					<?php else : ?>
-							<?php echo $this->escape($item->name); ?>
-					<?php endif; ?>
-				</td>
-				<td class="small">
-					<?php echo $item->contact;?>
-				</td>
-				<td class="center">
-					<?php echo $item->nbanners; ?>
-				</td>
-				<td class="small">
-					<?php echo $item->metakey; ?>
-				</td>
-				<td class="small">
-					<?php if ($item->purchase_type<0):?>
-						<?php echo JText::sprintf('COM_BANNERS_DEFAULT', JText::_('COM_BANNERS_FIELD_VALUE_'.$params->get('purchase_type')));?>
-					<?php else:?>
-						<?php echo JText::_('COM_BANNERS_FIELD_VALUE_'.$item->purchase_type);?>
-					<?php endif;?>
-				</td>
-				<td class="center">
-					<?php echo $item->id; ?>
-				</td>
-			</tr>
-			<?php endforeach; ?>
-		</tbody>
-	</table>
-	<div>
-		<input type="hidden" name="task" value="" />
-		<input type="hidden" name="boxchecked" value="0" />
-		<input type="hidden" name="filter_order" value="<?php echo $listOrder; ?>" />
-		<input type="hidden" name="filter_order_Dir" value="<?php echo $listDirn; ?>" />
-		<?php echo JHtml::_('form.token'); ?>
+		<!-- End Content -->
 	</div>
 </form>
--- administrator/components/com_banners/views/tracks/tmpl/default.php
+++ administrator/components/com_banners/views/tracks/tmpl/default.php
@@ -20,94 +20,115 @@ $listOrder	= $this->escape($this->state->get('list.ordering'));
 $listDirn	= $this->escape($this->state->get('list.direction'));
 ?>
 <form action="<?php echo JRoute::_('index.php?option=com_banners&view=tracks'); ?>" method="post" name="adminForm" id="adminForm">
-	<div id="filter-bar" class="btn-toolbar">
-		<div class="btn-group pull-right">
-			<a data-toggle="collapse" data-target="#filters" class="btn"><?php echo JText::_('JSEARCH_FILTER_LABEL'); ?> <span class="caret"></span></a>
+	<div class="row-fluid">
+		<!-- Begin Sidebar -->
+		<div id="sidebar" class="span2">
+			<div class="sidebar-nav">
+				<?php
+					// Display the submenu position modules
+					$this->modules = JModuleHelper::getModules('submenu');
+					foreach ($this->modules as $module) {
+						$output = JModuleHelper::renderModule($module);
+						$params = new JRegistry;
+						$params->loadString($module->params);
+						echo $output;
+					}
+				?>
+				<hr />
+				<div class="filter-select">
+					<h4 class="page-header"><?php echo JText::_('JSEARCH_FILTER_LABEL');?></h4>
+					<select name="filter_client_id" class="span12 small" onchange="this.form.submit()">
+						<option value=""><?php echo JText::_('COM_BANNERS_SELECT_CLIENT');?></option>
+						<?php echo JHtml::_('select.options', BannersHelper::getClientOptions(), 'value', 'text', $this->state->get('filter.client_id'));?>
+					</select>
+					<hr class="hr-condensed" />
+					<?php $category = $this->state->get('filter.category_id');?>
+					<select name="filter_category_id" class="span12 small" onchange="this.form.submit()">
+						<option value=""><?php echo JText::_('JOPTION_SELECT_CATEGORY');?></option>
+						<?php echo JHtml::_('select.options', JHtml::_('category.options', 'com_banners'), 'value', 'text', $category);?>
+					</select>
+					<hr class="hr-condensed" />
+			        <select name="filter_type" class="span12 small" onchange="this.form.submit()">
+						<?php echo JHtml::_('select.options', array(JHtml::_('select.option', '0', JText::_('COM_BANNERS_SELECT_TYPE')), JHtml::_('select.option', 1, JText::_('COM_BANNERS_IMPRESSION')), JHtml::_('select.option', 2, JText::_('COM_BANNERS_CLICK'))), 'value', 'text', $this->state->get('filter.type'));?>
+					</select>
+				</div>
+			</div>
 		</div>
-		<div class="filter-search btn-group pull-left">
-			<label class="filter-hide-lbl" for="filter_begin"><?php echo JText::_('COM_BANNERS_BEGIN_LABEL'); ?></label>
-			<?php echo JHtml::_('calendar', $this->state->get('filter.begin'), 'filter_begin', 'filter_begin', '%Y-%m-%d' , array('size'=>10, 'onchange'=>"this.form.fireEvent('submit');this.form.submit()"));?>
+		<!-- End Sidebar -->
+		<!-- Begin Content -->
+		<div class="span10">
+			<div id="filter-bar" class="btn-toolbar">
+				<div class="filter-search btn-group pull-left">
+					<label class="filter-hide-lbl" for="filter_begin"><?php echo JText::_('COM_BANNERS_BEGIN_LABEL'); ?></label>
+					<?php echo JHtml::_('calendar', $this->state->get('filter.begin'), 'filter_begin', 'filter_begin', '%Y-%m-%d' , array('size'=>10, 'onchange'=>"this.form.fireEvent('submit');this.form.submit()"));?>
+				</div>
+				<div class="filter-search btn-group pull-left">
+					<label class="filter-hide-lbl" for="filter_end"><?php echo JText::_('COM_BANNERS_END_LABEL'); ?></label>
+					<?php echo JHtml::_('calendar', $this->state->get('filter.end'), 'filter_end', 'filter_end', '%Y-%m-%d' , array('size'=>10, 'onchange'=>"this.form.fireEvent('submit');this.form.submit()"));?>
+				</div>
+			</div>
+			<div class="clearfix"> </div>
+			<table class="table table-striped">
+				<thead>
+					<tr>
+						<th class="title">
+							<?php echo JHtml::_('grid.sort', 'COM_BANNERS_HEADING_NAME', 'b.name', $listDirn, $listOrder); ?>
+						</th>
+						<th width="20%" class="nowrap">
+							<?php echo JHtml::_('grid.sort', 'COM_BANNERS_HEADING_CLIENT', 'cl.name', $listDirn, $listOrder); ?>
+						</th>
+						<th width="10%" class="nowrap">
+							<?php echo JHtml::_('grid.sort', 'COM_BANNERS_HEADING_TYPE', 'track_type', $listDirn, $listOrder); ?>
+						</th>
+						<th width="10%" class="nowrap">
+							<?php echo JHtml::_('grid.sort', 'COM_BANNERS_HEADING_COUNT', 'count', $listDirn, $listOrder); ?>
+						</th>
+						<th width="10%" class="nowrap">
+							<?php echo JHtml::_('grid.sort', 'JDATE', 'track_date', $listDirn, $listOrder); ?>
+						</th>
+					</tr>
+				</thead>
+				<tfoot>
+					<tr>
+						<td colspan="6">
+							<?php echo $this->pagination->getListFooter(); ?>
+						</td>
+					</tr>
+				</tfoot>
+				<tbody>
+				<?php foreach ($this->items as $i => $item) :?>
+					<tr class="row<?php echo $i % 2; ?>">
+						<td>
+							<?php echo $item->name;?>
+							<div class="small">
+								<?php echo $item->category_title;?>
+							</div>
+						</td>
+						<td>
+							<?php echo $item->client_name;?>
+						</td>
+						<td class="small">
+							<?php echo $item->track_type==1 ? JText::_('COM_BANNERS_IMPRESSION'): JText::_('COM_BANNERS_CLICK');?>
+						</td>
+						<td>
+							<?php echo $item->count;?>
+						</td>
+						<td>
+							<?php echo JHtml::_('date', $item->track_date, JText::_('DATE_FORMAT_LC4').' H:i');?>
+						</td>
+					</tr>
+				<?php endforeach; ?>
+				</tbody>
+			</table>
+		
+			<div>
+				<input type="hidden" name="task" value="" />
+				<input type="hidden" name="boxchecked" value="0" />
+				<input type="hidden" name="filter_order" value="<?php echo $listOrder; ?>" />
+				<input type="hidden" name="filter_order_Dir" value="<?php echo $listDirn; ?>" />
+				<?php echo JHtml::_('form.token'); ?>
+			</div>
 		</div>
-		<div class="filter-search btn-group pull-left">
-			<label class="filter-hide-lbl" for="filter_end"><?php echo JText::_('COM_BANNERS_END_LABEL'); ?></label>
-			<?php echo JHtml::_('calendar', $this->state->get('filter.end'), 'filter_end', 'filter_end', '%Y-%m-%d' , array('size'=>10, 'onchange'=>"this.form.fireEvent('submit');this.form.submit()"));?>
-		</div>
-	</div>
-	<div class="clearfix"> </div>
-	<div class="collapse" id="filters">
-		<div class="filter-select fltrt">
-			<select name="filter_client_id" class="inputbox" onchange="this.form.submit()">
-				<option value=""><?php echo JText::_('COM_BANNERS_SELECT_CLIENT');?></option>
-				<?php echo JHtml::_('select.options', BannersHelper::getClientOptions(), 'value', 'text', $this->state->get('filter.client_id'));?>
-			</select>
-			<?php $category = $this->state->get('filter.category_id');?>
-			<select name="filter_category_id" class="inputbox" onchange="this.form.submit()">
-				<option value=""><?php echo JText::_('JOPTION_SELECT_CATEGORY');?></option>
-				<?php echo JHtml::_('select.options', JHtml::_('category.options', 'com_banners'), 'value', 'text', $category);?>
-			</select>
-	        <select name="filter_type" class="inputbox" onchange="this.form.submit()">
-				<?php echo JHtml::_('select.options', array(JHtml::_('select.option', '0', JText::_('COM_BANNERS_SELECT_TYPE')), JHtml::_('select.option', 1, JText::_('COM_BANNERS_IMPRESSION')), JHtml::_('select.option', 2, JText::_('COM_BANNERS_CLICK'))), 'value', 'text', $this->state->get('filter.type'));?>
-			</select>
-		</div>
-	</div>
-	<table class="table table-striped">
-		<thead>
-			<tr>
-				<th class="title">
-					<?php echo JHtml::_('grid.sort', 'COM_BANNERS_HEADING_NAME', 'b.name', $listDirn, $listOrder); ?>
-				</th>
-				<th width="20%" class="nowrap">
-					<?php echo JHtml::_('grid.sort', 'COM_BANNERS_HEADING_CLIENT', 'cl.name', $listDirn, $listOrder); ?>
-				</th>
-				<th width="10%" class="nowrap">
-					<?php echo JHtml::_('grid.sort', 'COM_BANNERS_HEADING_TYPE', 'track_type', $listDirn, $listOrder); ?>
-				</th>
-				<th width="10%" class="nowrap">
-					<?php echo JHtml::_('grid.sort', 'COM_BANNERS_HEADING_COUNT', 'count', $listDirn, $listOrder); ?>
-				</th>
-				<th width="10%" class="nowrap">
-					<?php echo JHtml::_('grid.sort', 'JDATE', 'track_date', $listDirn, $listOrder); ?>
-				</th>
-			</tr>
-		</thead>
-		<tfoot>
-			<tr>
-				<td colspan="6">
-					<?php echo $this->pagination->getListFooter(); ?>
-				</td>
-			</tr>
-		</tfoot>
-		<tbody>
-		<?php foreach ($this->items as $i => $item) :?>
-			<tr class="row<?php echo $i % 2; ?>">
-				<td>
-					<?php echo $item->name;?>
-					<div class="small">
-						<?php echo $item->category_title;?>
-					</div>
-				</td>
-				<td>
-					<?php echo $item->client_name;?>
-				</td>
-				<td class="small">
-					<?php echo $item->track_type==1 ? JText::_('COM_BANNERS_IMPRESSION'): JText::_('COM_BANNERS_CLICK');?>
-				</td>
-				<td>
-					<?php echo $item->count;?>
-				</td>
-				<td>
-					<?php echo JHtml::_('date', $item->track_date, JText::_('DATE_FORMAT_LC4').' H:i');?>
-				</td>
-			</tr>
-		<?php endforeach; ?>
-		</tbody>
-	</table>
-
-	<div>
-		<input type="hidden" name="task" value="" />
-		<input type="hidden" name="boxchecked" value="0" />
-		<input type="hidden" name="filter_order" value="<?php echo $listOrder; ?>" />
-		<input type="hidden" name="filter_order_Dir" value="<?php echo $listDirn; ?>" />
-		<?php echo JHtml::_('form.token'); ?>
+		<!-- End Content -->
 	</div>
 </form>
