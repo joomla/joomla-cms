ff23cabc95054a974513800be340f9bb3e5d116b
kyleledbetter <kyle@kyleledbetter.com>
2012-07-08 14:49:43 -0500

    - restructuring Joomla strapped site template to a more generic, modern layout
    - adding Open Sans google font

--- templates/strapped/index.php
+++ templates/strapped/index.php
@@ -52,6 +52,7 @@ $user = JFactory::getUser();
     endif;
 	?>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300' rel='stylesheet' type='text/css'>
 	<script src="<?php echo $this->baseurl; ?>/templates/system/js/jquery.js"></script>
 	<script src="<?php echo $this->baseurl; ?>/templates/system/js/bootstrap.min.js"></script>
 	<script type="text/javascript">
@@ -62,52 +63,63 @@ $user = JFactory::getUser();
 </head>
 
 <body class="site <?php echo $option . " view-" . $view . " layout-" . $layout . " task-" . $task . " itemid-" . $itemid . " ";?>">
-	<!-- Header -->
-	<div class="header">
-		<div class="header-inner">
-			<div class="container">
-				<a class="brand pull-left" href="<?php echo $this->baseurl; ?>">
-					<img src="<?php echo $this->baseurl; ?>/templates/<?php echo $this->template ?>/images/logo.png" alt="<?php echo $sitename; ?>" />
-				</a>
-				<div class="header-search pull-right">
-					<jdoc:include type="modules" name="smartsearchload" style="none" />
+	
+	<!-- Body -->
+	<div class="body">
+		<div class="container">
+			<!-- Header -->
+			<div class="header">
+				<div class="header-inner">
+					<a class="brand pull-left" href="<?php echo $this->baseurl; ?>">
+						<img src="<?php echo $this->baseurl; ?>/templates/<?php echo $this->template ?>/images/logo.png" alt="<?php echo $sitename; ?>" />
+					</a>
+					<div class="header-search pull-right">
+						<jdoc:include type="modules" name="smartsearchload" style="none" />
+					</div>
+					<div class="clearfix"></div>
 				</div>
-				<jdoc:include type="modules" name="position-1" style="none" />
-
 			</div>
+			<?php if ($this->countModules('position-1')): ?>
+			<div class="navigation">
+				<jdoc:include type="modules" name="position-1" style="none" />
+			</div>
+			<?php endif; ?>
+			<!-- Banner -->
+			<div class="banner">
+				<jdoc:include type="modules" name="banner" style="xhtml" />
+			</div>
+			<div class="row-fluid">
+				<?php if ($this->countModules('position-8')): ?>
+				<!-- Begin Sidebar -->
+				<div id="sidebar" class="span3">
+					<div class="sidebar-nav">
+						<jdoc:include type="modules" name="position-8" style="xhtml" />
+					</div>
+				</div>
+				<!-- End Sidebar -->
+				<?php endif; ?>
+				<div id="content" class="<?php echo $span;?>">
+					<!-- Begin Content -->
+					<jdoc:include type="modules" name="position-3" style="xhtml" />
+					<jdoc:include type="message" />
+					<jdoc:include type="component" />
+					<jdoc:include type="modules" name="position-2" style="none" />
+					<!-- End Content -->
+				</div>
+				<?php if ($this->countModules('position-7')) : ?>
+				<div id="aside" class="span3">
+					<!-- Begin Right Sidebar -->
+					<jdoc:include type="modules" name="position-7" style="well" />
+					<!-- End Right Sidebar -->
+				</div>
+				<?php endif; ?>
+			</div>		
 		</div>
 	</div>
-	<!-- Container -->
-	<div class="container">
-		<jdoc:include type="modules" name="banner" style="xhtml" />
-		<div class="row">
-			<?php if ($this->countModules('position-7')): ?>
-			<!-- Begin Sidebar -->
-			<div id="sidebar" class="span3">
-				<div class="sidebar-nav">
-					<jdoc:include type="modules" name="position-7" style="xhtml" />
-				</div>
-			</div>
-			<!-- End Sidebar -->
-			<?php endif; ?>
-			<div id="content" class="<?php echo $span;?>">
-				<!-- Begin Content -->
-				<jdoc:include type="modules" name="position-3" style="xhtml" />
-				<jdoc:include type="message" />
-				<jdoc:include type="component" />
-				<jdoc:include type="modules" name="position-2" style="none" />
-				<!-- End Content -->
-			</div>
-			<?php if ($this->countModules('position-8')) : ?>
-			<div id="aside" class="span3">
-				<!-- Begin Right Sidebar -->
-				<jdoc:include type="modules" name="position-8" style="xhtml" />
-				<!-- End Right Sidebar -->
-			</div>
-			<?php endif; ?>
-		</div>
-		<hr />
-		<div class="footer">
+	<!-- Footer -->
+	<div class="footer">
+		<div class="container">
+			<hr />
 			<jdoc:include type="modules" name="footer" style="none" />
 			<p class="pull-right"><a href="#top" id="back-top">Back to top</a></p>
 			<p>&copy; <?php echo $sitename; ?> <?php echo date('Y');?></p>
