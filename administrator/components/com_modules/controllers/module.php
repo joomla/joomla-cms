f26981f2957457262975d28b9970165aa79b8df3
Rouven Weßling <me@rouvenwessling.de>
2012-07-13 13:59:59 +0200

    Fix more E_STRICT errors due to platform changes.

--- administrator/components/com_modules/controllers/module.php
+++ administrator/components/com_modules/controllers/module.php
@@ -123,14 +123,14 @@ class ModulesControllerModule extends JControllerForm
 	/**
 	 * Function that allows child controller access to model data after the data has been saved.
 	 *
-	 * @param   JModelLegacy  &$model     The data model object.
+	 * @param   JModelLegacy  $model      The data model object.
 	 * @param   array         $validData  The validated data.
 	 *
 	 * @return  void
 	 *
 	 * @since   1.6
 	 */
-	protected function postSaveHook(JModelLegacy &$model, $validData = array())
+	protected function postSaveHook(JModelLegacy $model, $validData = array())
 	{
 		// Initialise variables.
 		$app = JFactory::getApplication();
--- libraries/cms/form/field/captcha.php
+++ libraries/cms/form/field/captcha.php
@@ -28,17 +28,17 @@ class JFormFieldCaptcha extends JFormField
 	/**
 	 * Method to attach a JForm object to the field.
 	 *
-	 * @param   SimpleXMLElement  &$element  The SimpleXMLElement object representing the <field /> tag for the form field object.
-	 * @param   mixed             $value     The form field value to validate.
-	 * @param   string            $group     The field name group control value. This acts as as an array container for the field.
-	 *                                       For example if the field has name="foo" and the group value is set to "bar" then the
-	 *                                       full field name would end up being "bar[foo]".
+	 * @param   SimpleXMLElement  $element  The SimpleXMLElement object representing the <field /> tag for the form field object.
+	 * @param   mixed             $value    The form field value to validate.
+	 * @param   string            $group    The field name group control value. This acts as as an array container for the field.
+	 *                                      For example if the field has name="foo" and the group value is set to "bar" then the
+	 *                                      full field name would end up being "bar[foo]".
 	 *
 	 * @return  boolean  True on success.
 	 *
 	 * @since   2.5
 	 */
-	public function setup(&$element, $value, $group = null)
+	public function setup(SimpleXMLElement $element, $value, $group = null)
 	{
 		$result = parent::setup($element, $value, $group);
 
@@ -65,7 +65,7 @@ class JFormFieldCaptcha extends JFormField
 	/**
 	 * Method to get the field input.
 	 *
-	 * @return	string		The field input.
+	 * @return  string  The field input.
 	 *
 	 * @since   2.5
 	 */
