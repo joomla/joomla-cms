73994d705f4dc40a59369b33feb73562d3c093b9
Nikolai Plath <der.el.kuku@gmail.com>
2012-07-15 04:36:55 +0000

    Testing

--- tests/system-sqlite-go/suite/TestSuite.php
+++ tests/system-sqlite-go/suite/TestSuite.php
@@ -30,23 +30,23 @@ require_once 'menus/menu0002Test.php';
 /*
 require_once 'com_users/user0001Test.php';
 require_once 'com_users/user0002Test.php';
-require_once 'com_users/group0001Test.php';
-require_once 'com_users/group0002Test.php';
-require_once 'com_users/group0003Test.php';
+//require_once 'com_users/group0001Test.php';
+//require_once 'com_users/group0002Test.php';
+//require_once 'com_users/group0003Test.php';
 require_once 'modules/module0001Test.php';
 require_once 'modules/module0002Test.php';
 require_once 'redirect/redirect0001Test.php';
 require_once 'sample_data/sample_data0001Test.php';
-require_once 'acl/acl0001Test.php';
+//require_once 'acl/acl0001Test.php';
 require_once 'acl/acl0002Test.php';
 require_once 'acl/acl0003Test.php';
 require_once 'acl/acl0004Test.php';
 require_once 'acl/acl0005Test.php';
 require_once 'acl/acl0006Test.php';
-require_once 'language/language0001Test.php';
-require_once 'language/language0002Test.php';
+//require_once 'language/language0001Test.php';
+//require_once 'language/language0002Test.php';
 require_once 'cache/cache0001Test.php';
-require_once 'security/security0001Test.php';
+//require_once 'security/security0001Test.php';
 */
 
 class TestSuite
@@ -78,24 +78,24 @@ class TestSuite
 		/*
 		$suite->addTestSuite('User0001Test');
 		$suite->addTestSuite('User0002Test');
-		$suite->addTestSuite('Group0001Test');
-		$suite->addTestSuite('Group0002Test');
-		$suite->addTestSuite('Group0003Test');
+		//$suite->addTestSuite('Group0001Test');
+		//$suite->addTestSuite('Group0002Test');
+		//$suite->addTestSuite('Group0003Test');
 		$suite->addTestSuite('Module0001');
 		$suite->addTestSuite('Module0002');
 		$suite->addTestSuite('Redirect0001Test');
 		$suite->addTestSuite('SampleData0001');
-		$suite->addTestSuite('Acl0001Test');
+		//$suite->addTestSuite('Acl0001Test');
 		$suite->addTestSuite('Acl0002Test');
 		$suite->addTestSuite('DoInstall');
 		$suite->addTestSuite('Acl0003Test');
 		$suite->addTestSuite('Acl0004Test');
 		$suite->addTestSuite('Acl0005Test');
 		$suite->addTestSuite('Acl0006Test');
-		$suite->addTestSuite('Language0001Test');
-		$suite->addTestSuite('Language0002Test');
+		//$suite->addTestSuite('Language0001Test');
+		//$suite->addTestSuite('Language0002Test');
 		$suite->addTestSuite('Cache0001Test');
-		$suite->addTestSuite('Security0001Test');
+		//$suite->addTestSuite('Security0001Test');
 		*/
 
 		return $suite;
