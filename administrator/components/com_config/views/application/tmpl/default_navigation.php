eff95fadd5b38717a676442824f193b1b5257011
kyleledbetter <kyle@kyleledbetter.com>
2012-07-15 12:37:47 -0500

    - first draft for consolidating all configs for core components and extensions
    - TODO: loop/array through core components, check permissions to show each list item
    - TODO: load language string from each component (or use the same method the admin menu uses)
    - TODO: loop/array through installed components and print under the Extensions nav header
    - TODO: add sidebar nav-list and page title/toolbar to component configs

--- administrator/components/com_config/views/application/tmpl/default_navigation.php
+++ administrator/components/com_config/views/application/tmpl/default_navigation.php
@@ -9,18 +9,28 @@
 
 defined('_JEXEC') or die;
 ?>
-<div id="submenu-box">
-	<div class="submenu-box">
-		<div class="submenu-pad">
-			<ul id="submenu" class="configuration nav nav-list">
-				<li><a href="#" onclick="return false;" id="site" class="active"><?php echo JText::_('JSITE'); ?></a></li>
-				<li><a href="#" onclick="return false;" id="system"><?php echo JText::_('COM_CONFIG_SYSTEM'); ?></a></li>
-				<li><a href="#" onclick="return false;" id="server"><?php echo JText::_('COM_CONFIG_SERVER'); ?></a></li>
-				<li><a href="#" onclick="return false;" id="permissions"><?php echo JText::_('COM_CONFIG_PERMISSIONS'); ?></a></li>
-				<li><a href="#" onclick="return false;" id="filters"><?php echo JText::_('COM_CONFIG_TEXT_FILTERS')?></a></li>
-			</ul>
-			<div class="clr"></div>
-		</div>
-	</div>
-	<div class="clr"></div>
-</div>
+<ul class="nav nav-list">
+	<li class="nav-header"><?php echo JText::_('COM_CONFIG_SYSTEM'); ?></li>
+	<li class="active"><a href="index.php?option=com_config"><?php echo JText::_('COM_CONFIG_GLOBAL_CONFIGURATION'); ?></a></li>
+	<li class="divider"></li>
+	<li class="nav-header"><?php echo JText::_('COM_CONFIG_COMPONENT_FIELDSET_LABEL'); ?></li>
+	<li><a href="index.php?option=com_config&view=component&component=com_banners&path="><?php echo JText::_('Banners'); ?></a></li>
+	<li><a href="index.php?option=com_config&view=component&component=com_categories&path="><?php echo JText::_('Categories'); ?></a></li>
+	<li><a href="index.php?option=com_config&view=component&component=com_contact&path="><?php echo JText::_('Contacts'); ?></a></li>
+	<li><a href="index.php?option=com_config&view=component&component=com_content&path="><?php echo JText::_('Content'); ?></a></li>
+	<li><a href="index.php?option=com_config&view=component&component=com_installer&path="><?php echo JText::_('Extensions'); ?></a></li>
+	<li><a href="index.php?option=com_config&view=component&component=com_languages&path="><?php echo JText::_('Languages'); ?></a></li>
+	<li><a href="index.php?option=com_config&view=component&component=com_media&path="><?php echo JText::_('Media'); ?></a></li>
+	<li><a href="index.php?option=com_config&view=component&component=com_menus&path="><?php echo JText::_('Menus'); ?></a></li>
+	<li><a href="index.php?option=com_config&view=component&component=com_messages&path="><?php echo JText::_('Messages'); ?></a></li>
+	<li><a href="index.php?option=com_config&view=component&component=com_modules&path="><?php echo JText::_('Modules'); ?></a></li>
+	<li><a href="index.php?option=com_config&view=component&component=com_newsfeeds&path="><?php echo JText::_('Newsfeeds'); ?></a></li>
+	<li><a href="index.php?option=com_config&view=component&component=com_redirect&path="><?php echo JText::_('Redirect'); ?></a></li>
+	<li><a href="index.php?option=com_config&view=component&component=com_search&path="><?php echo JText::_('Search'); ?></a></li>
+	<li><a href="index.php?option=com_config&view=component&component=com_finder&path="><?php echo JText::_('Smart Search'); ?></a></li>
+	<li><a href="index.php?option=com_config&view=component&component=com_templates&path="><?php echo JText::_('Templates'); ?></a></li>
+	<li><a href="index.php?option=com_config&view=component&component=com_users&path="><?php echo JText::_('Users'); ?></a></li>
+	<li><a href="index.php?option=com_config&view=component&component=com_weblinks&path="><?php echo JText::_('Weblinks'); ?></a></li>
+	<li class="divider"></li>
+	<li class="nav-header"><?php echo JText::_('Extensions'); ?></li>
+</ul>
