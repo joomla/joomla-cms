cac8dc6d6e59137cef258b9f9d8bfdb7295be180
Nikolai Plath <der.el.kuku@gmail.com>
2012-07-15 02:19:06 +0000

    Testing

--- tests/system-sqlite-go/suite/TestSuite.php
+++ tests/system-sqlite-go/suite/TestSuite.php
@@ -21,13 +21,13 @@ require_once 'control_panel/control_panel0005Test.php';
 require_once 'menus/menu0001Test.php';
 require_once 'menus/menu0002Test.php';
 */
-require_once 'articles/article0001Test.php';
-require_once 'articles/article0002Test.php';
-require_once 'articles/article0003Test.php';
-require_once 'articles/article0004Test.php';
+//require_once 'articles/article0001Test.php';
+//require_once 'articles/article0002Test.php';
+//require_once 'articles/article0003Test.php';
+//require_once 'articles/article0004Test.php';
 require_once 'articles/featured0001Test.php';
-require_once 'articles/featured0002Test.php';
 /*
+require_once 'articles/featured0002Test.php';
 require_once 'com_users/user0001Test.php';
 require_once 'com_users/user0002Test.php';
 require_once 'com_users/group0001Test.php';
@@ -69,13 +69,13 @@ class TestSuite
 		$suite->addTestSuite('Menu0001');
 		$suite->addTestSuite('Menu0002');
 		*/
-		$suite->addTestSuite('Article0001');
-		$suite->addTestSuite('Article0002');
-		$suite->addTestSuite('Article0003');
-		$suite->addTestSuite('Article0004');
+		//$suite->addTestSuite('Article0001');
+		//$suite->addTestSuite('Article0002');
+		//$suite->addTestSuite('Article0003');
+		//$suite->addTestSuite('Article0004');
 		$suite->addTestSuite('Featured0001Test');
-		$suite->addTestSuite('Featured0002Test');
 		/*
+		$suite->addTestSuite('Featured0002Test');
 		$suite->addTestSuite('User0001Test');
 		$suite->addTestSuite('User0002Test');
 		$suite->addTestSuite('Group0001Test');
