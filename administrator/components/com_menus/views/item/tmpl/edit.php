15bcc20477f9078f043b387ee073ca40dea6c08b
kyleledbetter <kyle@kyleledbetter.com>
2012-07-15 17:14:34 -0500

    - adding second grid span for general menu item params

--- administrator/components/com_menus/views/item/tmpl/edit.php
+++ administrator/components/com_menus/views/item/tmpl/edit.php
@@ -47,183 +47,183 @@ JHtml::_('behavior.modal');
 <form action="<?php echo JRoute::_('index.php?option=com_menus&layout=edit&id='.(int) $this->item->id); ?>" method="post" name="adminForm" id="item-form" class="form-validate form-horizontal">
 	<fieldset>
 		<ul class="nav nav-tabs">
-		  <li class="active"><a href="#details" data-toggle="tab"><?php echo JText::_('COM_MENUS_ITEM_DETAILS');?></a></li>
-		  <li><a href="#basic" data-toggle="tab"><?php echo JText::_('COM_MENUS_BASIC_FIELDSET_LABEL');?></a></li>
-		  <li><a href="#options" data-toggle="tab"><?php echo JText::_('COM_MENUS_ADVANCED_FIELDSET_LABEL');?></a></li>
-		  <?php if (!empty($this->modules)) : ?>
-		  	<li><a href="#modules" data-toggle="tab"><?php echo JText::_('COM_MENUS_ITEM_MODULE_ASSIGNMENT');?></a></li>
-		  <?php endif; ?>
+			<li class="active"><a href="#details" data-toggle="tab"><?php echo JText::_('COM_MENUS_ITEM_DETAILS');?></a></li>
+			<li><a href="#options" data-toggle="tab"><?php echo JText::_('COM_MENUS_ADVANCED_FIELDSET_LABEL');?></a></li>
+			<?php if (!empty($this->modules)) : ?>
+				<li><a href="#modules" data-toggle="tab"><?php echo JText::_('COM_MENUS_ITEM_MODULE_ASSIGNMENT');?></a></li>
+			<?php endif; ?>
 		</ul>
 		<div class="tab-content">
-		  <div class="tab-pane active" id="details">
-		  	<div class="control-group">
-				<div class="control-label">
-					<?php echo $this->form->getLabel('type'); ?>
-				</div>
-				<div class="controls">
-					<?php echo $this->form->getInput('type'); ?>
-				</div>
-			</div>
-			<div class="control-group">
-				<div class="control-label">
-					<?php echo $this->form->getLabel('title'); ?>
-				</div>
-				<div class="controls">
-					<?php echo $this->form->getInput('title'); ?>
-				</div>
-			</div>
-			<?php if ($this->item->type =='url'): ?>
-				<?php $this->form->setFieldAttribute('link', 'readonly', 'false');?>
-				<div class="control-group">
-					<div class="control-label">
-						<?php echo $this->form->getLabel('link'); ?>
-					</div>
-					<div class="controls">
-						<?php echo $this->form->getInput('link'); ?>
-					</div>
-				</div>
-			<?php endif; ?>
-
-			<?php if ($this->item->type == 'alias'): ?>
-				<div class="control-group">
-					<div class="control-label">
-						<?php echo $this->form->getLabel('aliastip'); ?>
-					</div>
-				</div>
-			<?php endif; ?>
-
-			<?php if ($this->item->type !='url'): ?>
-				<div class="control-group">
-					<div class="control-label">
-						<?php echo $this->form->getLabel('alias'); ?>
-					</div>
-					<div class="controls">
-						<?php echo $this->form->getInput('alias'); ?>
-					</div>
-				</div>
-			<?php endif; ?>
-
-			<?php if ($this->item->type !=='url'): ?>
-				<div class="control-group">
-					<div class="control-label">
-						<?php echo $this->form->getLabel('link'); ?>
-					</div>
-					<div class="controls">
-						<?php echo $this->form->getInput('link'); ?>
-					</div>
-				</div>
-			<?php endif ?>
-
-			<div class="control-group">
-				<div class="control-label">
-					<?php echo $this->form->getLabel('published'); ?>
-				</div>
-				<div class="controls">
-					<?php echo $this->form->getInput('published'); ?>
-				</div>
-			</div>
+			<div class="tab-pane active" id="details">
+				<div class="row-fluid">
+					<div class="span6">
+						<div class="control-group">
+							<div class="control-label">
+								<?php echo $this->form->getLabel('published'); ?>
+							</div>
+							<div class="controls">
+								<?php echo $this->form->getInput('published'); ?>
+							</div>
+						</div>
+						
+						<div class="control-group">
+							<div class="control-label">
+								<?php echo $this->form->getLabel('type'); ?>
+							</div>
+							<div class="controls">
+								<?php echo $this->form->getInput('type'); ?>
+							</div>
+						</div>
+						<div class="control-group">
+							<div class="control-label">
+								<?php echo $this->form->getLabel('title'); ?>
+							</div>
+							<div class="controls">
+								<?php echo $this->form->getInput('title'); ?>
+							</div>
+						</div>
+						<?php if ($this->item->type =='url'): ?>
+							<?php $this->form->setFieldAttribute('link', 'readonly', 'false');?>
+							<div class="control-group">
+								<div class="control-label">
+									<?php echo $this->form->getLabel('link'); ?>
+								</div>
+								<div class="controls">
+									<?php echo $this->form->getInput('link'); ?>
+								</div>
+							</div>
+						<?php endif; ?>
 			
-			<div class="control-group">
-				<div class="control-label">
-					<?php echo $this->form->getLabel('access'); ?>
-				</div>
-				<div class="controls">
-					<?php echo $this->form->getInput('access'); ?>
-				</div>
-			</div>
-
-			<div class="control-group">
-				<div class="control-label">
-					<?php echo $this->form->getLabel('menutype'); ?>
-				</div>
-				<div class="controls">
-					<?php echo $this->form->getInput('menutype'); ?>
-				</div>
-			</div>
-
-			<div class="control-group">
-				<div class="control-label">
-					<?php echo $this->form->getLabel('parent_id'); ?>
-				</div>
-				<div class="controls">
-					<?php echo $this->form->getInput('parent_id'); ?>
-				</div>
-			</div>
-
-			<div class="control-group">
-				<div class="control-label">
-					<?php echo $this->form->getLabel('menuordering'); ?>
-				</div>
-				<div class="controls">
-					<?php echo $this->form->getInput('menuordering'); ?>
-				</div>
-			</div>
-
-			<div class="control-group">
-				<div class="control-label">
-					<?php echo $this->form->getLabel('browserNav'); ?>
-				</div>
-				<div class="controls">
-					<?php echo $this->form->getInput('browserNav'); ?>
-				</div>
-			</div>
-
-			<?php if ($this->item->type == 'component') : ?>
-				<div class="control-group">
-					<div class="control-label">
-						<?php echo $this->form->getLabel('home'); ?>
+						<?php if ($this->item->type == 'alias'): ?>
+							<div class="control-group">
+								<div class="control-label">
+									<?php echo $this->form->getLabel('aliastip'); ?>
+								</div>
+							</div>
+						<?php endif; ?>
+			
+						<?php if ($this->item->type !='url'): ?>
+							<div class="control-group">
+								<div class="control-label">
+									<?php echo $this->form->getLabel('alias'); ?>
+								</div>
+								<div class="controls">
+									<?php echo $this->form->getInput('alias'); ?>
+								</div>
+							</div>
+						<?php endif; ?>
+			
+						<?php if ($this->item->type !=='url'): ?>
+							<div class="control-group">
+								<div class="control-label">
+									<?php echo $this->form->getLabel('link'); ?>
+								</div>
+								<div class="controls">
+									<?php echo $this->form->getInput('link'); ?>
+								</div>
+							</div>
+						<?php endif ?>
+			
+						<div class="control-group">
+							<div class="control-label">
+								<?php echo $this->form->getLabel('access'); ?>
+							</div>
+							<div class="controls">
+								<?php echo $this->form->getInput('access'); ?>
+							</div>
+						</div>
+			
+						<div class="control-group">
+							<div class="control-label">
+								<?php echo $this->form->getLabel('menutype'); ?>
+							</div>
+							<div class="controls">
+								<?php echo $this->form->getInput('menutype'); ?>
+							</div>
+						</div>
+			
+						<div class="control-group">
+							<div class="control-label">
+								<?php echo $this->form->getLabel('parent_id'); ?>
+							</div>
+							<div class="controls">
+								<?php echo $this->form->getInput('parent_id'); ?>
+							</div>
+						</div>
+			
+						<div class="control-group">
+							<div class="control-label">
+								<?php echo $this->form->getLabel('menuordering'); ?>
+							</div>
+							<div class="controls">
+								<?php echo $this->form->getInput('menuordering'); ?>
+							</div>
+						</div>
+			
+						<div class="control-group">
+							<div class="control-label">
+								<?php echo $this->form->getLabel('browserNav'); ?>
+							</div>
+							<div class="controls">
+								<?php echo $this->form->getInput('browserNav'); ?>
+							</div>
+						</div>
 					</div>
-					<div class="controls">
-						<?php echo $this->form->getInput('home'); ?>
+					<div class="span6">
+						<?php if ($this->item->type == 'component') : ?>
+							<div class="control-group">
+								<div class="control-label">
+									<?php echo $this->form->getLabel('home'); ?>
+								</div>
+								<div class="controls">
+									<?php echo $this->form->getInput('home'); ?>
+								</div>
+							</div>
+						<?php endif; ?>
+						<div class="control-group">
+							<div class="control-label">
+								<?php echo $this->form->getLabel('note'); ?>
+							</div>
+							<div class="controls">
+								<?php echo $this->form->getInput('note'); ?>
+							</div>
+						</div>
+						<div class="control-group">
+							<div class="control-label">
+								<?php echo $this->form->getLabel('language'); ?>
+							</div>
+							<div class="controls">
+								<?php echo $this->form->getInput('language'); ?>
+							</div>
+						</div>
+						
+						<div class="control-group">
+							<div class="control-label">
+								<?php echo $this->form->getLabel('template_style_id'); ?>
+							</div>
+							<div class="controls">
+								<?php echo $this->form->getInput('template_style_id'); ?>
+							</div>
+						</div>
+						<div class="control-group">
+							<div class="control-label">
+								<?php echo $this->form->getLabel('id'); ?>
+							</div>
+							<div class="controls">
+								<?php echo $this->form->getInput('id'); ?>
+							</div>
+						</div>
 					</div>
+				</div>
+			</div>
+			<div class="tab-pane" id="options">
+				<?php echo $this->loadTemplate('options'); ?>
+			</div>
+			<?php if (!empty($this->modules)) : ?>
+				<div class="tab-pane" id="modules">
+					<?php echo $this->loadTemplate('modules'); ?>
 				</div>
 			<?php endif; ?>
-
-		  </div>
-		  <div class="tab-pane" id="basic">
-		  	<div class="control-group">
-		  		<div class="control-label">
-		  			<?php echo $this->form->getLabel('note'); ?>
-		  		</div>
-		  		<div class="controls">
-		  			<?php echo $this->form->getInput('note'); ?>
-		  		</div>
-		  	</div>
-		  	<div class="control-group">
-				<div class="control-label">
-					<?php echo $this->form->getLabel('language'); ?>
-				</div>
-				<div class="controls">
-					<?php echo $this->form->getInput('language'); ?>
-				</div>
-			</div>
-
-			<div class="control-group">
-				<div class="control-label">
-					<?php echo $this->form->getLabel('template_style_id'); ?>
-				</div>
-				<div class="controls">
-					<?php echo $this->form->getInput('template_style_id'); ?>
-				</div>
-			</div>
-
-			<div class="control-group">
-				<div class="control-label">
-					<?php echo $this->form->getLabel('id'); ?>
-				</div>
-				<div class="controls">
-					<?php echo $this->form->getInput('id'); ?>
-				</div>
-			</div>
-		  </div>
-	  	  <div class="tab-pane" id="options">
-	  	  	<?php echo $this->loadTemplate('options'); ?>
-	  	  </div>
-		  <?php if (!empty($this->modules)) : ?>
-			  <div class="tab-pane" id="modules">
-			  	<?php echo $this->loadTemplate('modules'); ?>
-			  </div>
-		  <?php endif; ?>
 		</div>
 	</fieldset>
 
