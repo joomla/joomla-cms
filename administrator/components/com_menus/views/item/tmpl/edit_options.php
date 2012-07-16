40c65776e44f5489dc70f3310ec8a8ed76d0bf30
kyleledbetter <kyle@kyleledbetter.com>
2012-07-15 21:10:01 -0500

    - moving component item options into main menu edit view

--- administrator/components/com_menus/views/item/tmpl/edit_options.php
+++ administrator/components/com_menus/views/item/tmpl/edit_options.php
@@ -10,32 +10,6 @@
 defined('_JEXEC') or die;
 ?>
 <?php
-	$fieldSets = $this->form->getFieldsets('request');
-
-	if (!empty($fieldSets)) {
-		$fieldSet = array_shift($fieldSets);
-		$label = !empty($fieldSet->label) ? $fieldSet->label : 'COM_MENUS_'.$fieldSet->name.'_FIELDSET_LABEL';
-		if (isset($fieldSet->description) && trim($fieldSet->description)) :
-			echo '<p class="tip">'.$this->escape(JText::_($fieldSet->description)).'</p>';
-		endif;
-	?>
-			<?php $hidden_fields = ''; ?>
-				<?php foreach ($this->form->getFieldset('request') as $field) : ?>
-				<?php if (!$field->hidden) : ?>
-				<div class="control-group">
-					<div class="control-label">
-						<?php echo $field->label; ?>
-					</div>
-					<div class="controls">
-						<?php echo $field->input; ?>
-					</div>
-				</div>
-				<?php else : $hidden_fields.= $field->input; ?>
-				<?php endif; ?>
-				<?php endforeach; ?>
-			<?php echo $hidden_fields; ?>
-<?php
-	}
 
 	$fieldSets = $this->form->getFieldsets('params');
 
