7b3e663b57d7a950ebc20b70f959ea76a3fa661a
kyleledbetter <kyle@kyleledbetter.com>
2012-07-15 21:12:11 -0500

    - changing menu item type <ul>s into Bootstrap collapse accordions
    - TODO: get javascript to properly pass menu type to the menu item edit view when not using modal

--- administrator/components/com_menus/views/menutypes/tmpl/default.php
+++ administrator/components/com_menus/views/menutypes/tmpl/default.php
@@ -8,38 +8,69 @@
  */
 
 defined('_JEXEC') or die;
+
+// Checking if loaded via index.php or component.php
+$tmpl = JRequest::getCmd('tmpl', '');
 ?>
+
 <script type="text/javascript">
 	setmenutype = function(type)
 	{
-		window.parent.Joomla.submitbutton('item.setType', type);
-		window.parent.SqueezeBox.close();
+		<?php if($tmpl) : ?>
+			window.parent.Joomla.submitbutton('item.setType', type);
+			window.parent.SqueezeBox.close();
+		<?php else : ?>
+			window.location="index.php?option=com_menus&view=item&task=item.setType&layout=edit&type="+('item.setType', type);
+		<?php endif; ?>
 	}
 </script>
 
-<h2 class="modal-title"><?php echo JText::_('COM_MENUS_TYPE_CHOOSE'); ?></h2>
-<ul class="menu_types">
-	<?php foreach ($this->types as $name => $list): ?>
-	<li><dl class="menu_type">
-			<dt><?php echo JText::_($name); ?></dt>
-			<dd><ul>
-					<?php foreach ($list as $item): ?>
-					<li><a class="choose_type" href="#" title="<?php echo JText::_($item->description); ?>"
-							onclick="javascript:setmenutype('<?php echo base64_encode(json_encode(array('id' => $this->recordId, 'title' => $item->title, 'request' => $item->request))); ?>')">
-							<?php echo JText::_($item->title);?>
-						</a>
-					</li>
-					<?php endforeach; ?>
-				</ul>
-			</dd>
-		</dl>
-	</li>
-	<?php endforeach; ?>
+<h4 class="modal-title"><?php echo JText::_('COM_MENUS_TYPE_CHOOSE'); ?></h2>
 
-	<li><dl class="menu_type">
-			<dt><?php echo JText::_('COM_MENUS_TYPE_SYSTEM'); ?></dt>
-			<dd>
-				<ul>
+<!-- Begin Accordions -->
+<div class="accordion" id="collapseTypes">
+	<?php 
+		$i = 0;
+		foreach ($this->types as $name => $list): ?>
+		<!-- Begin Accordion Group -->
+		<div class="accordion-group">
+			<div class="accordion-heading">
+				<h4>
+					<a class="accordion-toggle" data-toggle="collapse" data-parent="#collapseTypes" href="#collapse<?php echo $i++; ?>">
+						<?php echo JText::_($name); ?>
+					</a>
+				</h4>
+			</div>
+			<div id="collapse<?php echo $i+-1; ?>" class="accordion-body collapse">
+				<div class="accordion-inner">
+					<ul class="nav nav-list">
+						<?php foreach ($list as $item): ?>
+							<li>
+								<a class="choose_type" href="#" rel="tooltip" title="<?php echo JText::_($item->description); ?>"
+									onclick="javascript:setmenutype('<?php echo base64_encode(json_encode(array('id' => $this->recordId, 'title' => $item->title, 'request' => $item->request))); ?>')">
+									<?php echo JText::_($item->title);?>
+								</a>
+							</li>
+						<?php endforeach; ?>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<!-- End Accordion Group -->
+	<?php endforeach; ?>
+	
+	<!-- Begin Accordion Group -->
+	<div class="accordion-group">
+		<div class="accordion-heading">
+			<h4>
+				<a class="accordion-toggle" data-toggle="collapse" data-parent="#collapseTypes" href="#collapseSystem">
+					<?php echo JText::_('COM_MENUS_TYPE_SYSTEM'); ?>
+				</a>
+			</h4>
+		</div>
+		<div id="collapseSystem" class="accordion-body collapse">
+			<div class="accordion-inner">
+				<ul class="nav nav-list">
 					<li><a class="choose_type" href="#" title="<?php echo JText::_('COM_MENUS_TYPE_EXTERNAL_URL_DESC'); ?>"
 							onclick="javascript:setmenutype('<?php echo base64_encode(json_encode(array('id' => $this->recordId, 'title'=>'url'))); ?>')">
 							<?php echo JText::_('COM_MENUS_TYPE_EXTERNAL_URL'); ?>
@@ -56,7 +87,9 @@ defined('_JEXEC') or die;
 						</a>
 					</li>
 				</ul>
-			</dd>
-		</dl>
-	</li>
-</ul>
+			</div>
+		</div>
+	</div>
+	<!-- End Accordion Group -->
+</div>
+<!-- End Accordions -->
