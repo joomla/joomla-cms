b1a0ea7a69b071eefa38be627650c2965068a9a6
Peter van Westen <peter@nonumber.nl>
2012-07-17 11:11:43 +0200

    Fixed javascript bug in installer (jform_db_old > jform_sample_file)

--- installation/template/js/installation.js
+++ installation/template/js/installation.js
@@ -23,7 +23,7 @@ var Installation = new Class({
 		$$('form.form-validate').each(function(form){ this.attachToForm(form); }, document.formvalidator);
 
 		if (this.view == 'site' && this.sampleDataLoaded) {
-			var select = document.id('jform_db_old');
+			var select = document.id('jform_sample_file');
 			var button = document.id('theDefault').children[0];
 			button.setAttribute('disabled', 'disabled');
 			select.setAttribute('disabled', 'disabled');
