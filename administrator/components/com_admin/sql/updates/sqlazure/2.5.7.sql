0c47179b4ef1f1607ed0c66514eff93b1829499f
elinw <elin.waring@gmail.com>
2012-07-19 10:55:23 -0400

    Fix missing category in usernotes drop down.

--- administrator/components/com_admin/sql/updates/sqlazure/2.5.7.sql
+++ administrator/components/com_admin/sql/updates/sqlazure/2.5.7.sql
@@ -1,5 +1,3 @@
 # Placeholder file for database changes for version 2.5.7
 UPDATE  [#__assets] SET name=REPLACE( name, 'com_user.notes.category','com_users.category'  );
 UPDATE  [#__assets] SET name=REPLACE( name, 'com_user.notes.category','com_users.category'  );
-
-
