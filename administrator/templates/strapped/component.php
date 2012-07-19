adec4659f4b5eaf1169872f4462ca00da5f7f1db
Michael Babker <mbabker@flbab.com>
2012-07-18 21:55:24 -0500

    Correct media links

--- administrator/templates/strapped/component.php
+++ administrator/templates/strapped/component.php
@@ -6,20 +6,35 @@
 // no direct access
 defined('_JEXEC') or die;
 
+jimport('joomla.filesystem.file');
+
 $app = JFactory::getApplication();
 $doc = JFactory::getDocument();
+$lang  = JFactory::getLanguage();
 
 // Add Stylesheets
-$doc->addStyleSheet('../templates/system/css/bootstrap.css');
-$doc->addStyleSheet('../templates/system/css/bootstrap-extended.css');
-$doc->addStyleSheet('../templates/system/css/chosen.css');
+$doc->addStyleSheet('../media/jui/css/bootstrap.min.css');
+$doc->addStyleSheet('../media/jui/css/bootstrap-extended.css');
+$doc->addStyleSheet('../media/jui/css/chosen.min.css');
+
+// If Right-to-Left
+if ($this->direction == 'rtl') :
+	$doc->addStyleSheet('../media/jui/css/bootstrap-rtl.css');
+endif;
+
+// Load specific language related CSS
+$file = 'language/' . $lang->getTag() . '/' . $lang->getTag() . '.css';
+if (JFile::exists($file)) :
+	$doc->addStyleSheet($file);
+endif;
+
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" >
 <head>
-	<script src="../templates/system/js/jquery.js"></script>
-	<script src="../templates/system/js/bootstrap.min.js"></script>
-	<script src="../templates/system/js/chosen.jquery.min.js"></script>
+	<script src="../media/jui/js/jquery.js"></script>
+	<script src="../media/jui/js/bootstrap.min.js"></script>
+	<script src="../media/jui/js/chosen.jquery.min.js"></script>
 	<script type="text/javascript">
 	  jQuery.noConflict();
 	</script>
@@ -34,19 +49,19 @@ $doc->addStyleSheet('../templates/system/css/chosen.css');
 			$('*[rel=tooltip]').tooltip()
 			$('.tip-bottom').tooltip({placement: "bottom"})
 			$('*[rel=popover]').popover()
-			
+
 		    // Chosen select boxes
 		    $("select").chosen({disable_search_threshold : 10 });
-		    
+
 		    // Turn radios into btn-group
 		    $('.radio.btn-group label').addClass('btn')
 		    $(".btn-group label:not(.active)").click(function(){
 		        var label = $(this);
 		        var input = $('#' + label.attr('for'));
-		        
+
 		        if (!input.prop('checked')){
-		            label.closest('.btn-group').find("label").removeClass('active btn-primary');                        
-		            label.addClass('active btn-primary'); 
+		            label.closest('.btn-group').find("label").removeClass('active btn-primary');
+		            label.addClass('active btn-primary');
 		            input.prop('checked', true);
 		        }
 		    });
