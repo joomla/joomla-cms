f2fdc00e26ae89cbcd3fbb8478ebf884a65e72ef
Anderson Grüdtner Martins <andergmartins@gmail.com>
2012-08-03 03:10:27 -0300

    Add ability to add custom position, translated module positions, fix bug and fix code style
    - Add the ability to insert a custom module position;
    
    - Move the methods from tmpl to module helper;
    
    - Add translation for module positions;
    
    - Fix "chosen" plugin, to be able to add a new option just if it was configured for that;

--- administrator/components/com_modules/views/module/tmpl/edit_positions.php
+++ administrator/components/com_modules/views/module/tmpl/edit_positions.php
@@ -9,48 +9,55 @@
 
 defined('_JEXEC') or die;
 
-require_once JPATH_ADMINISTRATOR.'/components/com_templates/helpers/templates.php';
+require_once JPATH_ADMINISTRATOR . '/components/com_templates/helpers/templates.php';
 
-JHtml::addIncludePath(JPATH_COMPONENT.'/helpers/html');
+JHtml::addIncludePath(JPATH_COMPONENT . '/helpers/html');
 $clientId       = $this->item->client_id;
 $state          = $this->state->get('filter.state');
 $templates      = array_keys(ModulesHelper::getTemplates($clientId, $state));
 $templateGroups = array();
 
 // Add an empty value to be able to deselect a module position
-$option = new stdClass;
-$option->value = '';
-$option->text  = '';
-
-$group = array();
-$group['value'] = '';
-$group['text']  = '';
-$group['items'] = array($option);
-
-$templateGroups[''] = $group;
+$option = ModulesHelper::createOption();
+$templateGroups[''] = ModulesHelper::createOptionGroup('', array($option));
 
 // Add positions from templates
+$isTemplatePosition = false;
 foreach ($templates as $template)
 {
-	$group = array();
-	$group['value'] = $template;
-	$group['text']  = $template;
-	$group['items'] = array();
+	$options = array();
 
 	$positions = TemplatesHelper::getPositions($clientId, $template);
 	foreach ($positions as $position)
 	{
-		$option = new stdClass();
-		$option->value = $position;
-		$option->text = $position;
+		$text = ModulesHelper::getTranslatedModulePosition($template, $position) . ' [' . $position . ']';
+		$options[] = ModulesHelper::createOption($position, $text);
 
-		$group['items'][] = $option;
+		if (!$isTemplatePosition && $this->item->position === $position)
+		{
+			$isTemplatePosition = true;
+		}
 	}
 
-	$templateGroups[$template] = $group;
+	$templateGroups[$template] = ModulesHelper::createOptionGroup(ucfirst($template), $options);
 }
 
-echo JHtml::_(
-	'select.groupedlist', $templateGroups, 'jform[position]',
-	array('id' => 'jform_position', 'list.select' => $this->item->position)
+// Add custom position to options
+$customGroupText = JText::_('COM_MODULES_CUSTOM_POSITION');
+if (!empty($this->item->position) && !$isTemplatePosition)
+{
+	$option = ModulesHelper::createOption($this->item->position);
+	$templateGroups[$customGroupText] = ModulesHelper::createOptionGroup($customGroupText, array($option));
+}
+
+// Build field
+$attr = array(
+	'id'          => 'jform_position',
+	'list.select' => $this->item->position,
+	'list.attr'   => 'class="chzn-custom-value input-xlarge" '
+		. 'data-custom_group_text="' . $customGroupText . '" '
+		. 'data-no_results_text="' . JText::_('COM_MODULES_ADD_CUSTOM_POSITION') . '" '
+		. 'data-placeholder="' . JText::_('COM_MODULES_TYPE_OR_SELECT_POSITION') . '" '
 );
+
+echo JHtml::_('select.groupedlist', $templateGroups, 'jform[position]', $attr);
