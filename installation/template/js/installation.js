df3d74b2ff340966b1d0514222b9a6d7242da081
Michael Babker <mbabker@flbab.com>
2012-08-03 15:19:54 -0500

    Remove unneeded JS bit

--- installation/template/js/installation.js
+++ installation/template/js/installation.js
@@ -21,14 +21,6 @@ var Installation = new Class({
     	this.addToggler();
 		// Attach the validator
 		$$('form.form-validate').each(function(form){ this.attachToForm(form); }, document.formvalidator);
-
-		if (this.view == 'site' && this.sampleDataLoaded) {
-			var select = document.id('jform_sample_file');
-			var button = document.id('theDefault').children[0];
-			button.setAttribute('disabled', 'disabled');
-			select.setAttribute('disabled', 'disabled');
-			button.set('text', Joomla.JText._('INSTL_SITE_SAMPLE_LOADED', 'Sample Data Installed Successfully.'));
-		}
     },
 
 	submitform: function() {
