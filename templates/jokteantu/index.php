efe9133367bce964897a9ddbdb55242cfb77fc9b
snaphuman <fhernandezn@gmail.com>
2012-08-05 10:56:36 -0500

    Utilizo los nuevos elementos HTML5 sobre el markup de jokteantu. Inicializo las propiedades de estilo, fix menor al markup separando mejor las secciones

--- templates/jokteantu/index.php
+++ templates/jokteantu/index.php
@@ -45,6 +45,7 @@ require_once (JPATH_BASE . DS . 'templates' . DS . $this->template . '/asset/ski
              * de los nuevos elementos HTML
              */
         ?>
+        <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/css/html5.css" />
 
 		<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/system/css/system.css" type="text/css" />
 		<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/system/css/jokte.css" type="text/css" />
@@ -102,77 +103,79 @@ font-family:'<?php echo $typoespecial; ?>',gargi,Verdana, Geneva, sans-serif;}
    
         <div id="bodyBg" style="width:<?php echo $bodyw; ?>px;"><!-- ancho flexible según parametro -->
             
-            <div id="top"> 
-            	<?php 
-				 /*
-				  * Logo - Opciones
-				  * - Por módulo HTML personalizado en la posición User1
-				  * - Por Imagen seleccionable desde los parámetros de la plantilla
-				  * - En texto plano dentro de la etiqueta H1 maquetable vía CSS de la plantilla  
-				  */ ?>
-                <?php if (!empty($user1) && empty($logo)): ?>
-                    <div id="user1" style="width: <?php echo $user1w; ?>%;">
-                        <jdoc:include type="modules" name="user1" style="xhtml" />
-                    </div>
-                <?php elseif (!empty($logo)): ?>
-                    <div id="logo">
-                        <a href="index.php"><img src="<?php echo $this->baseurl ?>/<?php echo $logo; ?>" alt="<?php echo $sitename; ?>" title="<?php echo $siteslogan; ?>"/></a>
-                    </div>
-                <?php else :?>
-                	<div id="logo">
-                		<?php if ($tslogan == '0') : ?>                			
-                			<h1><a href="index.php" title="<?php echo $siteslogan; ?>"><?php echo $sitename; ?></a></h1>
-                		<?php else : ?>
-                			<h1><a href="index.php"><?php echo $sitename; ?></a></h1>
-                			<small><?php echo $siteslogan; ?></small>
-                		<?php endif; ?>
-                	</div>
-                <?php endif; ?>
-                
-                
-                <?php 
-				  /*
-				  * Posiciones para redes sociales
-				  * Módulo en la posición socialnet 
-				  */ ?>
-                <?php if (!empty($socialnet)) : ?>				
-                    <div id="micon">
-                        <jdoc:include type="modules" name="socialnet" style="jxhtml"/>
-                    </div>
+            <header> 
+                    <?php 
+                     /*
+                      * Logo - Opciones
+                      * - Por módulo HTML personalizado en la posición User1
+                      * - Por Imagen seleccionable desde los parámetros de la plantilla
+                      * - En texto plano dentro de la etiqueta H1 maquetable vía CSS de la plantilla  
+                      */ ?>
+                    <?php if (!empty($user1) && empty($logo)): ?>
+                        <hgroup>
+                            <section id="user1" style="width: <?php echo $user1w; ?>%;">
+                                <jdoc:include type="modules" name="user1" style="xhtml" />
+                            </section>
+                    <?php elseif (!empty($logo)): ?>
+                        <section id="logo">
+                            <a href="index.php"><img src="<?php echo $this->baseurl ?>/<?php echo $logo; ?>" alt="<?php echo $sitename; ?>" title="<?php echo $siteslogan; ?>"/></a>
+                        </section>
+                    <?php else :?>
+                        <section div id="logo">
+                            <?php if ($tslogan == '0') : ?>                			
+                                <h1><a href="index.php" title="<?php echo $siteslogan; ?>"><?php echo $sitename; ?></a></h1>
+                            <?php else : ?>
+                                <h1><a href="index.php"><?php echo $sitename; ?></a></h1>
+                                <small><?php echo $siteslogan; ?></small>
+                            <?php endif; ?>
+                        </section>
+                    <?php endif; ?>
+                    
+                    
+                    <?php 
+                      /*
+                      * Posiciones para redes sociales
+                      * Módulo en la posición socialnet 
+                      */ ?>
+                    <?php if (!empty($socialnet)) : ?>				
+                        <aside id="micon">
+                            <jdoc:include type="modules" name="socialnet" style="jxhtml"/>
+                        </aside>
+                    </hgroup>
+                        <?php
+                    endif;?>
+            </header>
+                    
+                      <div id="cls"></div>
+
+                    <?php 
+                     /*
+                      * Posiciones para banners
+                      * Dos posiciones de ancho variable  
+                      * El ancho de las posiciones se indica en los parámetros de la plantilla
+                      */?>				
+                    <?php if (!empty($banner1) || !empty($banner2)) : ?>
+                        <hgroup id="banner">	
+                         <?php if (!empty($banner1)) : ?>		
+                            <aside id="banner1" style=" <?php echo $banner1w; ?>;">
+                                <jdoc:include type="modules" name="banner1" style="jxhtml" />
+                            </aside>
                     <?php
-                endif;?>
-				
-				  <div id="cls"></div>
-				<?php 
-				 /*
-				  * Posiciones para banners
-				  * Dos posiciones de ancho variable  
-				  * El ancho de las posiciones se indica en los parámetros de la plantilla
-				  */?>				
-                <?php if (!empty($banner1) || !empty($banner2)) : ?>
-                   	<div id="banner">	
-                   	 <?php if (!empty($banner1)) : ?>		
-                        <div id="banner1" style=" <?php echo $banner1w; ?>;">
-                           	<jdoc:include type="modules" name="banner1" style="jxhtml" />
-                        </div>
-				<?php
-				endif;
-                if (!empty($banner2)) : ?>
-                   	<div id="banner2" style="<?php echo $banner2w; ?>">
-                       	<jdoc:include type="modules" name="banner2" style="jxhtml" />
-                   	</div>
-               
-				<?php endif; ?>  
-				</div>
-               
-            <?php endif; ?> 
+                      endif; 
+                    if (!empty($banner2)) : ?>
+                        <aside id="banner2" style="<?php echo $banner2w; ?>">
+                            <jdoc:include type="modules" name="banner2" style="jxhtml" />
+                        </aside>
+                    <?php endif; ?>  
+                    </hgroup>
+                <?php endif; ?> 
                <div id="cls"></div>
             <?php 
 			 /*
 			  * Posición para menu superior				 
 			  */?>
             <?php if (!empty($topmenu)) : ?> 
-                <div id="tmen"><jdoc:include type="modules" name="topmenu" style="jxmenu" /></div> 
+                <nav id="tmen"><jdoc:include type="modules" name="topmenu" style="jxmenu" /></nav> 
                 <?php
             endif;
 			
@@ -182,7 +185,7 @@ font-family:'<?php echo $typoespecial; ?>',gargi,Verdana, Geneva, sans-serif;}
 			  */
             if (!empty($slide)) :
                 ?> 
-                <div id="slide"><jdoc:include type="modules" name="slide" style="jxhtml" /></div>
+                <section id="slide"><jdoc:include type="modules" name="slide" style="jxhtml" /></section>
             <?php endif; ?>
 
 
@@ -191,77 +194,77 @@ font-family:'<?php echo $typoespecial; ?>',gargi,Verdana, Geneva, sans-serif;}
 			  * Posiciones User 2 - User 3 y User 4				 
 			  */?>
             <?php if (!empty($user2) || !empty($user3) || !empty($user4)) : ?>
-                <div id="topcentro">
+                <hgroup id="topcentro">
                     <?php if (!empty($user2)) : ?>
-                        <div id="user2" style="<?php echo $user2w; ?>">
+                        <section id="user2" style="<?php echo $user2w; ?>">
                             <jdoc:include type="modules" name="user2" style="jxhtml" />
-                        </div>
+                        </section>
                         <?php
                     endif;
 
                     if (!empty($user3)) :
                         ?>
-                        <div id="user3" style="<?php echo $user3w; ?>">
+                        <section id="user3" style="<?php echo $user3w; ?>">
                             <jdoc:include type="modules" name="user3" style="jxhtml" />
-                        </div>
+                        </section>
                         <?php
                     endif;
 
                     if (!empty($user4)) :
                         ?>
-                        <div id="user4" style="<?php echo $user4w; ?>">
+                        <section id="user4" style="<?php echo $user4w; ?>">
                             <jdoc:include type="modules" name="user4" style="jxhtml" />
-                        </div>
+                        </section>
                     <?php endif; ?>
-                </div>
+                </hgroup>
             <?php endif; ?>
             
             <?php 
 			 /*
 			  * Posiciones right - content y left				 
 			  */?>
-            <div id="centro">   
+            <div id="centro" role="main">   
             	<?php if (!empty($left)) : ?> 
-            	<div id="izquierda" style="<?php echo $leftw; ?>">
+            	<aside role="izquierda" id="izquierda" style="<?php echo $leftw; ?>">
                    		<?php if (!empty($lefttop)) : ?>
-						<div id="izq-ini">
+						<section id="izq-ini">
 							<jdoc:include type="modules" name="lefttop" style="jxhtml" />
-						</div>
+						</section>
 					<?php endif; ?>	
 					<?php if (!empty($insetleft) or ($outsetleft)) : ?> 
-						<div id="centroizq">
+						<hgroup id="centroizq">
 							<?php if (!empty($insetleft)) : ?>
-								<div id="izquierda1" >
+								<section id="izquierda1" >
 									<jdoc:include type="modules" name="insetleft" style="jxhtml" />
-								</div>
+								</section>
 							<?php endif; ?>	
 							<?php if (!empty($outsetleft)) : ?>
-								<div id="izquierda2">					
+								<section id="izquierda2">					
 									<jdoc:include type="modules" name="outsetleft" style="jxhtml" />
-									</div>
+									</section>
 								<?php endif; ?>
-							</div>
+							</hgroup>
 						<?php endif; ?>
 					<?php if (!empty($leftbottom)) : ?>
 						<div id="izq-fin">
 							<jdoc:include type="modules" name="leftbottom" style="jxhtml" />
 						</div>
 					<?php endif; ?>
-				</div>
+				</aside>
 			  <?php endif; ?>
               <!-- breadcrumbs + SobreContenedor +Contenedor + bajocontenedor. Ajustado a ancho de contenedor -->  
              
               
                 <div id="contenedor" style="<?php echo $contentw; ?>">
                 <?php if (!empty($bread)) : ?>
-             <div id="breadcrumb">
+             <nav id="breadcrumb">
 				<jdoc:include type="modules" name="breadcrumb" /> 
-             </div>
+             </nav>
               <?php endif; ?>
                 <?php if (!empty($sobrecontent)) : ?>
-             <div id="sobrecontent">
+             <section id="sobrecontent">
 				<jdoc:include type="modules" name="sobrecontent" style="jxhtml" /> 
-             </div>
+             </section>
               <?php endif; ?>
               <jdoc:include type="message" />
                 <div id="margen">
@@ -271,20 +274,20 @@ font-family:'<?php echo $typoespecial; ?>',gargi,Verdana, Geneva, sans-serif;}
                     
                 
                  <?php if (!empty($bajocontent)) : ?>
-             <div id="bajocontent">
+             <section id="bajocontent">
 				<jdoc:include type="modules" name="bajocontent" style="jxhtml" /> 
-             </div>
+             </section>
               <?php endif; ?>
 				</div>
 				<!-- Derecha con dos posiciones interiores según publicación de modulos -->
-				<div id="derecha" style="<?php echo $rightw; ?>">
+				<aside id="derecha" style="<?php echo $rightw; ?>" role="derecha">
 					<?php if (!empty($righttop)) : ?>
-						<div id="d-ini">
+						<section id="d-ini">
 							<jdoc:include type="modules" name="righttop" style="jxhtml" />
-						</div>
+						</section>
 					<?php endif; ?>	
 					<?php if (!empty($inset) or ($outset)) : ?> 
-						<div id="centroder">
+						<hgroup id="centroder">
 							<?php if (!empty($inset)) : ?>
 								<div id="derecha1">
 									<jdoc:include type="modules" name="inset" style="jxhtml" />
@@ -295,43 +298,43 @@ font-family:'<?php echo $typoespecial; ?>',gargi,Verdana, Geneva, sans-serif;}
 									<jdoc:include type="modules" name="outset" style="jxhtml" />
 									</div>
 								<?php endif; ?>
-							</div>
+							</hgroup>
 						<?php endif; ?>
 					<?php if (!empty($rightbot)) : ?>
-						<div id="d-fin">
+						<section id="d-fin">
 							<jdoc:include type="modules" name="rightbottom" style="jxhtml" />
-						</div>
+						</section>
 				 <?php endif; ?>
-				</div>	
+				</aside>	
 
                 <?php 
 			 	/*
 			  	 * Posiciones User 5 - User 6 y User 7				 
 			  	*/?>
                 <?php if (!empty($user5) || !empty($user6) || !empty($user7)) : ?>
-                    <div id="topcentro">
+                    <hgroup id="topcentro">
                         <?php if (!empty($user5)) : ?>
-                            <div id="user5" style="<?php echo $user5w; ?>">
+                            <section id="user5" style="<?php echo $user5w; ?>">
                                 <jdoc:include type="modules" name="user5" style="jxhtml" />
-                            </div>
+                            </section>
                             <?php
                         endif;
 
                         if (!empty($user6)) :
                             ?>
-                            <div id="user6" style="<?php echo $user6w; ?>">
+                            <section id="user6" style="<?php echo $user6w; ?>">
                                 <jdoc:include type="modules" name="user6" style="jxhtml" />
-                            </div>
+                            </section>
                             <?php
                         endif;
 
                         if (!empty($user7)) :
                             ?>
-                            <div id="user7" style="<?php echo $user7w; ?>">
+                            <section id="user7" style="<?php echo $user7w; ?>">
                                 <jdoc:include type="modules" name="user7" style="jxhtml" />
-                            </div>
+                            </section>
                         <?php endif; ?> 
-                    </div>
+                    </hgroup>
                 <?php endif; ?> 
 
                 
@@ -340,16 +343,17 @@ font-family:'<?php echo $typoespecial; ?>',gargi,Verdana, Geneva, sans-serif;}
 			  	 * Posiciones footmenu y foot				 
 			  	 */?>
                 <?php if (!empty($footmenu) || !empty($foot)) : ?>
-                    <div id="piefin">
-                        <div id="piemenu">
-                            <jdoc:include type="modules" name="footmenu" style="jxhtml" />
-                        </div>
-                        <div id="pie">
-                            <jdoc:include type="modules" name="foot" style="jxhtml" />
-                        </div>
-                    </div>
+                    <footer id="piefin">
+                        <hgroup>
+                            <section id="piemenu">
+                                <jdoc:include type="modules" name="footmenu" style="jxhtml" />
+                            </section>
+                            <section id="pie">
+                                <jdoc:include type="modules" name="foot" style="jxhtml" />
+                            </section>
+                        </hgroup>
+                    </footer>
                 <?php endif; ?>  
-            </div>
             <?php
             	if ($ga == '1'): ?>
             	<script type="text/javascript">
