d42fd23a3b5951771b28f02af8acff464a4e0a3c
Elin Waring <elin.waring@gmail.com>
2012-08-11 17:48:41 +0200

    #+ [#25295] Email exposed through RSS and Atom feeds. Thanks Elin and Peter

--- components/com_weblinks/models/category.php
+++ components/com_weblinks/models/category.php
@@ -124,8 +124,10 @@ class WeblinksModelCategory extends JModelList
 				$query->where('c.published = '.(int) $cpublished);
 			}
 		}
+
 		// Join over the users for the author and modified_by names.
 		$query->select("CASE WHEN a.created_by_alias > ' ' THEN a.created_by_alias ELSE ua.name END AS author");
+		$query->select("ua.email AS author_email");
 
 		$query->join('LEFT', '#__users AS ua ON ua.id = a.created_by');
 		$query->join('LEFT', '#__users AS uam ON uam.id = a.modified_by');
