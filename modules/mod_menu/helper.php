4c0b08f50b60ed9cbd4d1c022232072c752ce96b
elinw <elin.waring@gmail.com>
2012-08-22 18:00:53 -0400

    Fix problem with menus starting at level greater than 1.

--- modules/mod_menu/helper.php
+++ modules/mod_menu/helper.php
@@ -58,7 +58,7 @@ class modMenuHelper
 					if (($start && $start > $item->level)
 						|| ($end && $item->level > $end)
 						|| (!$showAll && $item->level > 1 && !in_array($item->parent_id, $path))
-						|| ($start > 1 && !in_array($item->tree[$start - 2], $path))
+						|| ($start > 1 && !in_array($item->tree[$start-2],$path) && ($item->menutype == $active->menutype))
 					) {
 						unset($items[$i]);
 						continue;
