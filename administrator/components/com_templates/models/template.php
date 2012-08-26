8e1870d7b382a967d870dd2f014061dbf38bcbda
Julio Pontes <juliopfneto@gmail.com>
2012-08-26 11:52:47 +0200

    # [#28985] JFile class not loaded when use cache handler != file. Thanks Julio.

--- administrator/components/com_templates/models/template.php
+++ administrator/components/com_templates/models/template.php
@@ -255,6 +255,7 @@ class TemplatesModelTemplate extends JModelLegacy
 		$newName = strtolower($this->getState('new_name'));
 		$oldName = $this->getTemplate()->element;
 
+		jimport('joomla.filesystem.file');
 		foreach ($files as $file)
 		{
 			$newFile = str_replace($oldName, $newName, $file);
