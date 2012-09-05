14f51ed20f8242a3ed5447721015329472ff8f02
Peter van Westen <peter@nonumber.nl>
2012-09-05 20:53:28 +0200

    Fixed dropdowns not working in menu select

--- administrator/components/com_modules/views/module/tmpl/edit_assignment.php
+++ administrator/components/com_modules/views/module/tmpl/edit_assignment.php
@@ -217,7 +217,7 @@ $menuTypes = MenusHelper::getMenuLinks();
 			<div style="display:none;" id="treeselectmenu">
 				<div class="pull-left nav-hover treeselect-menu">
 					<div class="btn-group" style="margin-left: 6px; display:none;">
-						<a href="javascript://" data-toggle="dropdown" class="dropdown-toggle btn btn-mini">
+						<a href="#" data-toggle="dropdown" class="dropdown-toggle btn btn-mini">
 							<span class="caret"></span>
 						</a>
 						<ul class="dropdown-menu">
