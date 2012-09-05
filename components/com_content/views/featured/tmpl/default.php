52a141bd0c0dc533f0545c1fbdb03727586cb3e5
Matt Thomas <matt@betweenbrain.com>
2012-09-05 19:44:13 -0400

    Tweaking implementation basic HTML5 semantic markup

--- components/com_content/views/featured/tmpl/default.php
+++ components/com_content/views/featured/tmpl/default.php
@@ -17,80 +17,79 @@ JHtml::_('behavior.caption');
 // It will be a separate class if the user starts it with a space
 ?>
 <section class="blog-featured<?php echo $this->pageclass_sfx;?>">
-<?php if ($this->params->get('show_page_heading') != 0) : ?>
+	<?php if ($this->params->get('show_page_heading') != 0) : ?>
+	<header>
+		<h1 class="page-header">
+			<?php echo $this->escape($this->params->get('page_heading')); ?>
+		</h1>
+	</header>
+	<?php endif; ?>
 
-<h1 class="page-header">
-<?php echo $this->escape($this->params->get('page_heading')); ?>
-</h1>
-
-<?php endif; ?>
-
-<?php $leadingcount = 0; ?>
-<?php if (!empty($this->lead_items)) : ?>
-<section class="items-leading clearfix">
-	<?php foreach ($this->lead_items as &$item) : ?>
-		<article class="leading-<?php echo $leadingcount; ?><?php echo $item->state == 0 ? ' system-unpublished' : null; ?>">
+	<?php $leadingcount = 0; ?>
+	<?php if (!empty($this->lead_items)) : ?>
+	<section class="items-leading clearfix">
+		<?php foreach ($this->lead_items as &$item) : ?>
+		<article class="leading-<?php echo $leadingcount; ?><?php echo $item->state == 0 ? ' system-unpublished' : NULL; ?>">
 			<?php
-				$this->item = &$item;
-				echo $this->loadTemplate('item');
+			$this->item = &$item;
+			echo $this->loadTemplate('item');
 			?>
 		</article>
 		<?php
-			$leadingcount++;
+		$leadingcount++;
 		?>
-	<?php endforeach; ?>
-</section>
-<?php endif; ?>
-<?php
+		<?php endforeach; ?>
+	</section>
+	<?php endif; ?>
+	<?php
 	$introcount = (count($this->intro_items));
-	$counter = 0;
-?>
-<?php if (!empty($this->intro_items)) : ?>
-<section class="items-intro">
-	<?php foreach ($this->intro_items as $key => &$item) : ?>
+	$counter    = 0;
+	?>
+	<?php if (!empty($this->intro_items)) : ?>
+	<section class="items-intro">
+		<?php foreach ($this->intro_items as $key => &$item) : ?>
 
 		<?php
-		$key = ($key - $leadingcount) + 1;
-		$rowcount = (((int) $key - 1) % (int) $this->columns) + 1;
-		$row = $counter / $this->columns;
+		$key      = ($key - $leadingcount) + 1;
+		$rowcount = (((int)$key - 1) % (int)$this->columns) + 1;
+		$row      = $counter / $this->columns;
 
 		if ($rowcount == 1) : ?>
 
-		<div class="items-row cols-<?php echo (int) $this->columns;?> <?php echo 'row-'.$row; ?> row-fluid">
+		<div class="items-row cols-<?php echo (int)$this->columns;?> <?php echo 'row-' . $row; ?> row-fluid">
 		<?php endif; ?>
-			<article class="item column-<?php echo $rowcount;?><?php echo $item->state == 0 ? ' system-unpublished' : null; ?> span<?php echo round((12 / $this->columns));?>">
+		<article class="item column-<?php echo $rowcount;?><?php echo $item->state == 0 ? ' system-unpublished' : NULL; ?> span<?php echo round((12 / $this->columns));?>">
 			<?php
-					$this->item = &$item;
-					echo $this->loadTemplate('item');
+			$this->item = &$item;
+			echo $this->loadTemplate('item');
 			?>
-			</article>
-			<?php $counter++; ?>
+		</article>
+		<?php $counter++; ?>
 
-			<?php if (($rowcount == $this->columns) or ($counter == $introcount)): ?>
+		<?php if (($rowcount == $this->columns) or ($counter == $introcount)): ?>
 
 		</div>
 		<?php endif; ?>
 
-	<?php endforeach; ?>
-</section>
-<?php endif; ?>
+		<?php endforeach; ?>
+	</section>
+	<?php endif; ?>
 
-<?php if (!empty($this->link_items)) : ?>
+	<?php if (!empty($this->link_items)) : ?>
 	<div class="items-more">
-	<?php echo $this->loadTemplate('links'); ?>
+		<?php echo $this->loadTemplate('links'); ?>
 	</div>
-<?php endif; ?>
+	<?php endif; ?>
 
-<?php if ($this->params->def('show_pagination', 2) == 1  || ($this->params->get('show_pagination') == 2 && $this->pagination->pagesTotal > 1)) : ?>
+	<?php if ($this->params->def('show_pagination', 2) == 1 || ($this->params->get('show_pagination') == 2 && $this->pagination->pagesTotal > 1)) : ?>
 	<nav class="pagination">
-
 		<?php if ($this->params->def('show_pagination_results', 1)) : ?>
-			<p class="counter pull-right">
-				<?php echo $this->pagination->getPagesCounter(); ?>
-			</p>
-		<?php  endif; ?>
-				<?php echo $this->pagination->getPagesLinks(); ?>
+		<p class="counter pull-right">
+			<?php echo $this->pagination->getPagesCounter(); ?>
+		</p>
+		<?php endif; ?>
+		<?php echo $this->pagination->getPagesLinks(); ?>
 	</nav>
-<?php endif; ?>
+	<?php endif; ?>
 
 </section>
--- components/com_content/views/featured/tmpl/default_item.php
+++ components/com_content/views/featured/tmpl/default_item.php
@@ -10,15 +10,16 @@
 defined('_JEXEC') or die;
 
 // Create a shortcut for params.
-$params = &$this->item->params;
-$images = json_decode($this->item->images);
-$canEdit	= $this->item->params->get('access-edit');
+$params  = &$this->item->params;
+$images  = json_decode($this->item->images);
+$canEdit = $this->item->params->get('access-edit');
 ?>
 <?php if ($this->item->state == 0) : ?>
 <section class="system-unpublished">
-	<?php endif; ?>
-	<?php if ($params->get('show_print_icon') || $params->get('show_email_icon') || $canEdit) : ?>
-	<div class="btn-group pull-right"> <a class="btn dropdown-toggle" data-toggle="dropdown" href="#"> <i class="icon-cog"></i><span class="caret"></span> </a>
+<?php endif; ?>
+<?php if ($params->get('show_print_icon') || $params->get('show_email_icon') || $canEdit) : ?>
+	<div class="btn-group pull-right"><a class="btn dropdown-toggle" data-toggle="dropdown" href="#">
+		<i class="icon-cog"></i><span class="caret"></span> </a>
 		<ul class="dropdown-menu">
 			<?php if ($params->get('show_print_icon')) : ?>
 			<li class="print-icon"> <?php echo JHtml::_('icon.print_popup', $this->item, $params); ?> </li>
@@ -32,31 +33,31 @@ $canEdit	= $this->item->params->get('access-edit');
 		</ul>
 	</div>
 	<?php endif; ?>
-	<?php if ($params->get('show_title')) : ?>
+<?php if ($params->get('show_title')) : ?>
 	<h2 class="item-title">
 		<?php if ($params->get('link_titles') && $params->get('access-view')) : ?>
 		<a href="<?php echo JRoute::_(ContentHelperRoute::getArticleRoute($this->item->slug, $this->item->catid)); ?>"> <?php echo $this->escape($this->item->title); ?></a>
 		<?php else : ?>
 		<?php echo $this->escape($this->item->title); ?>
 		<?php endif; ?>
-		<?php if ($params->get('show_author') && !empty($this->item->author )) : ?>
+		<?php if ($params->get('show_author') && !empty($this->item->author)) : ?>
 		<small class="createdby">
-		<?php $author = $this->item->author; ?>
-		<?php $author = ($this->item->created_by_alias ? $this->item->created_by_alias : $author);?>
-		<?php if (!empty($this->item->contactid ) &&  $params->get('link_author') == true):?>
-		<?php
-		echo JText::sprintf('COM_CONTENT_WRITTEN_BY',
-			JHtml::_('link', JRoute::_('index.php?option=com_contact&view=contact&id='.$this->item->contactid), $author)
-		); ?>
-		<?php else :?>
-		<?php echo JText::sprintf('COM_CONTENT_WRITTEN_BY', $author); ?>
-		<?php endif; ?>
+			<?php $author = $this->item->author; ?>
+			<?php $author = ($this->item->created_by_alias ? $this->item->created_by_alias : $author);?>
+			<?php if (!empty($this->item->contactid) && $params->get('link_author') == TRUE): ?>
+			<?php
+			echo JText::sprintf('COM_CONTENT_WRITTEN_BY',
+				JHtml::_('link', JRoute::_('index.php?option=com_contact&view=contact&id=' . $this->item->contactid), $author)
+			); ?>
+			<?php else : ?>
+			<?php echo JText::sprintf('COM_CONTENT_WRITTEN_BY', $author); ?>
+			<?php endif; ?>
 		</small>
 		<?php endif; ?>
 	</h2>
 	<?php endif; ?>
-	<?php // to do not that elegant would be nice to group the params ?>
-	<?php if (($params->get('show_category')) or ($params->get('show_create_date')) or ($params->get('show_parent_category'))) : ?>
+<?php // to do not that elegant would be nice to group the params ?>
+<?php if (($params->get('show_category')) or ($params->get('show_create_date')) or ($params->get('show_parent_category'))) : ?>
 	<div class="btn-toolbar">
 		<?php if ($params->get('show_create_date')) : ?>
 		<div class="btn-group create"> <?php echo JText::sprintf('COM_CONTENT_CREATED_DATE_ON', JHtml::_('date', $this->item->created, JText::_('DATE_FORMAT_LC3'))); ?> </div>
@@ -64,7 +65,7 @@ $canEdit	= $this->item->params->get('access-edit');
 		<?php if ($params->get('show_parent_category') && $this->item->parent_id != 1) : ?>
 		<div class="btn-group parent-category-name">
 			<?php $title = $this->escape($this->item->parent_title);
-					$url = '<a href="' . JRoute::_(ContentHelperRoute::getCategoryRoute($this->item->parent_slug)) . '">' . $title . '</a>'; ?>
+			$url         = '<a href="' . JRoute::_(ContentHelperRoute::getCategoryRoute($this->item->parent_slug)) . '">' . $title . '</a>'; ?>
 			<?php if ($params->get('link_parent_category') and $this->item->parent_slug) : ?>
 			<?php echo JText::sprintf('COM_CONTENT_PARENT', $url); ?>
 			<?php else : ?>
@@ -75,7 +76,7 @@ $canEdit	= $this->item->params->get('access-edit');
 		<?php if ($params->get('show_category')) : ?>
 		<div class="btn-group category-name">
 			<?php $title = $this->escape($this->item->category_title);
-					$url = '<a href="'.JRoute::_(ContentHelperRoute::getCategoryRoute($this->item->catslug)).'">'.$title.'</a>';?>
+			$url         = '<a href="' . JRoute::_(ContentHelperRoute::getCategoryRoute($this->item->catslug)) . '">' . $title . '</a>';?>
 			<?php if ($params->get('link_category') and $this->item->catslug) : ?>
 			<?php echo JText::sprintf('COM_CONTENT_CATEGORY', $url); ?>
 			<?php else : ?>
@@ -85,50 +86,55 @@ $canEdit	= $this->item->params->get('access-edit');
 		<?php endif; ?>
 	</div>
 	<?php endif; ?>
-	<?php  if (isset($images->image_intro) and !empty($images->image_intro)) : ?>
+<?php if (isset($images->image_intro) and !empty($images->image_intro)) : ?>
 	<?php $imgfloat = (empty($images->float_intro)) ? $params->get('float_intro') : $images->float_intro; ?>
-	<div class="pull-<?php echo htmlspecialchars($imgfloat); ?> item-image"> <img
-			<?php if ($images->image_intro_caption):
-				echo 'class="caption"'.' title="' .htmlspecialchars($images->image_intro_caption) .'"';
-			endif; ?>
-			src="<?php echo htmlspecialchars($images->image_intro); ?>" alt="<?php echo htmlspecialchars($images->image_intro_alt); ?>"/> </div>
-	<?php endif; ?>
-	<?php if (!$params->get('show_intro')) : ?>
-	<?php echo $this->item->event->afterDisplayTitle; ?>
-	<?php endif; ?>
-	<?php echo $this->item->event->beforeDisplayContent; ?> <?php echo $this->item->introtext; ?>
-	<?php if (($params->get('show_modify_date')) or ($params->get('show_publish_date')) or ($params->get('show_hits'))) : ?>
-	<div class="btn-toolbar">
-		<?php if ($params->get('show_modify_date')) : ?>
-		<div class="btn-group modified"> <i class="icon-calendar"></i> <?php echo JText::sprintf('COM_CONTENT_LAST_UPDATED', JHtml::_('date', $this->item->modified, JText::_('DATE_FORMAT_LC3'))); ?> </div>
-		<?php endif; ?>
-		<?php if ($params->get('show_publish_date')) : ?>
-		<div class="btn-group published"> <i class="icon-calendar"></i> <?php echo JText::sprintf('COM_CONTENT_PUBLISHED_DATE_ON', JHtml::_('date', $this->item->publish_up, JText::_('DATE_FORMAT_LC3'))); ?> </div>
-		<?php endif; ?>
-		<?php if ($params->get('show_hits')) : ?>
-		<div class="btn-group hits"> <i class="icon-eye-open"></i> <?php echo JText::sprintf('COM_CONTENT_ARTICLE_HITS', $this->item->hits); ?> </div>
-		<?php endif; ?>
-		<?php endif; ?>
-		<?php if (($params->get('show_author')) or ($params->get('show_category')) or ($params->get('show_create_date')) or ($params->get('show_modify_date')) or ($params->get('show_publish_date')) or ($params->get('show_parent_category')) or ($params->get('show_hits'))) : ?>
+	<div class="pull-<?php echo htmlspecialchars($imgfloat); ?> item-image"><img
+		<?php if ($images->image_intro_caption):
+			echo 'class="caption"' . ' title="' . htmlspecialchars($images->image_intro_caption) . '"';
+		endif; ?>
+			src="<?php echo htmlspecialchars($images->image_intro); ?>" alt="<?php echo htmlspecialchars($images->image_intro_alt); ?>" />
 	</div>
 	<?php endif; ?>
-	<?php if ($params->get('show_readmore') && $this->item->readmore) :
+<?php if (!$params->get('show_intro')) : ?>
+	<?php echo $this->item->event->afterDisplayTitle; ?>
+	<?php endif; ?>
+<?php echo $this->item->event->beforeDisplayContent; ?> <?php echo $this->item->introtext; ?>
+<?php if (($params->get('show_modify_date')) or ($params->get('show_publish_date')) or ($params->get('show_hits'))) : ?>
+	<div class="btn-toolbar">
+		<?php if ($params->get('show_modify_date')) : ?>
+		<div class="btn-group modified">
+			<i class="icon-calendar"></i> <?php echo JText::sprintf('COM_CONTENT_LAST_UPDATED', JHtml::_('date', $this->item->modified, JText::_('DATE_FORMAT_LC3'))); ?>
+		</div>
+		<?php endif; ?>
+	<?php if ($params->get('show_publish_date')) : ?>
+		<div class="btn-group published">
+			<i class="icon-calendar"></i> <?php echo JText::sprintf('COM_CONTENT_PUBLISHED_DATE_ON', JHtml::_('date', $this->item->publish_up, JText::_('DATE_FORMAT_LC3'))); ?>
+		</div>
+		<?php endif; ?>
+	<?php if ($params->get('show_hits')) : ?>
+		<div class="btn-group hits">
+			<i class="icon-eye-open"></i> <?php echo JText::sprintf('COM_CONTENT_ARTICLE_HITS', $this->item->hits); ?>
+		</div>
+		<?php endif; ?>
+	<?php endif; ?>
+<?php if (($params->get('show_author')) or ($params->get('show_category')) or ($params->get('show_create_date')) or ($params->get('show_modify_date')) or ($params->get('show_publish_date')) or ($params->get('show_parent_category')) or ($params->get('show_hits'))) : ?>
+	</div>
+	<?php endif; ?>
+<?php if ($params->get('show_readmore') && $this->item->readmore) :
 	if ($params->get('access-view')) :
-		$link = JRoute::_(ContentHelperRoute::getArticleRoute($this->item->slug, $this->item->catid));
-	else :
-		$menu = JFactory::getApplication()->getMenu();
-		$active = $menu->getActive();
-		$itemId = $active->id;
-		$link1 = JRoute::_('index.php?option=com_users&view=login&Itemid=' . $itemId);
+		$link = JRoute::_(ContentHelperRoute::getArticleRoute($this->item->slug, $this->item->catid)); else :
+		$menu      = JFactory::getApplication()->getMenu();
+		$active    = $menu->getActive();
+		$itemId    = $active->id;
+		$link1     = JRoute::_('index.php?option=com_users&view=login&Itemid=' . $itemId);
 		$returnURL = JRoute::_(ContentHelperRoute::getArticleRoute($this->item->slug, $this->item->catid));
-		$link = new JURI($link1);
+		$link      = new JURI($link1);
 		$link->setVar('return', base64_encode($returnURL));
 	endif;
 	?>
 	<a class="btn" href="<?php echo $link; ?>"> <i class="icon-chevron-right"></i>
-	<?php if (!$params->get('access-view')) :
-			echo JText::_('COM_CONTENT_REGISTER_TO_READ_MORE');
-		elseif ($readmore = $this->item->alternative_readmore) :
+		<?php if (!$params->get('access-view')) :
+			echo JText::_('COM_CONTENT_REGISTER_TO_READ_MORE'); elseif ($readmore = $this->item->alternative_readmore) :
 			echo $readmore;
 			if ($params->get('show_readmore_title', 0) != 0) :
 				echo JHtml::_('string.truncate', ($this->item->title), $params->get('readmore_limit'));
@@ -141,7 +147,7 @@ $canEdit	= $this->item->params->get('access-edit');
 		endif; ?>
 	</a>
 	<?php endif; ?>
-	<?php if ($this->item->state == 0) : ?>
+<?php if ($this->item->state == 0) : ?>
 </section>
 <?php endif; ?>
 <?php echo $this->item->event->afterDisplayContent; ?>
