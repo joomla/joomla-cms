1b83bcc26872216a2cf5a7e017700b72b2a67ec4
elinw <elin.waring@gmail.com>
2012-09-10 19:41:43 -0400

    Fix unmatched </dd> and  <address> in wrong context

--- components/com_contact/views/contact/tmpl/default_address.php
+++ components/com_contact/views/contact/tmpl/default_address.php
@@ -15,56 +15,52 @@ defined('_JEXEC') or die;
  */
 ?>
 <dl class="contact-address">
-<?php if (($this->params->get('address_check') > 0) &&  ($this->contact->address || $this->contact->suburb  || $this->contact->state || $this->contact->country || $this->contact->postcode)) : ?>
-	<?php if ($this->params->get('address_check') > 0) : ?>
-	<dt>
-		<span class="<?php echo $this->params->get('marker_class'); ?>" >
-			<?php echo $this->params->get('marker_address'); ?>
-		</span>
-	</dt>
-	<?php endif; ?>
-	<address>
-	<?php if ($this->contact->address && $this->params->get('show_street_address')) : ?>
-
-		<dd>
-			<span class="contact-street">
-				<?php echo nl2br($this->contact->address .'<br/>'); ?>
-			</span>
-		</dd>
+	<?php if (($this->params->get('address_check') > 0) &&  
+		($this->contact->address || $this->contact->suburb  || $this->contact->state || $this->contact->country || $this->contact->postcode)) : ?>
+		<?php if ($this->params->get('address_check') > 0) : ?>
+			<dt>
+				<span class="<?php echo $this->params->get('marker_class'); ?>" >
+					<?php echo $this->params->get('marker_address'); ?>
+				</span>
+			</dt>
 		<?php endif; ?>
-		
+
+		<?php if ($this->contact->address && $this->params->get('show_street_address')) : ?>
+			<dd>
+				<span class="contact-street">
+					<?php echo $this->contact->address .'<br/>'; ?>
+				</span>
+			</dd>
+		<?php endif; ?>
+
 		<?php if ($this->contact->suburb && $this->params->get('show_suburb')) : ?>
 			<dd>
-			<span class="contact-suburb">
-				<?php echo nl2br($this->contact->suburb .'<br/>'); ?>
-			</span>
+				<span class="contact-suburb">
+					<?php echo $this->contact->suburb .'<br/>'; ?>
+				</span>
 			</dd>
 		<?php endif; ?>
 		<?php if ($this->contact->state && $this->params->get('show_state')) : ?>
 			<dd>
-			<span class="contact-state">
-				<?php echo $this->contact->state . '<br/>'; ?>
-			</span>
+				<span class="contact-state">
+					<?php echo $this->contact->state . '<br/>'; ?>
+				</span>
 			</dd>
 		<?php endif; ?>
 		<?php if ($this->contact->postcode && $this->params->get('show_postcode')) : ?>
 			<dd>
-			<span class="contact-postcode">
-				<?php echo $this->contact->postcode .'<br/>'; ?>
-			</span>
+				<span class="contact-postcode">
+					<?php echo $this->contact->postcode .'<br/>'; ?>
+				</span>
 			</dd>
 		<?php endif; ?>
 		<?php if ($this->contact->country && $this->params->get('show_country')) : ?>
 		<dd>
 			<span class="contact-country">
-				<?php echo nl2br($this->contact->country .'<br/>'); ?>
+				<?php echo $this->contact->country .'<br/>'; ?>
 			</span>
 		</dd>
 		<?php endif; ?>
-	<?php endif; ?>
-	<?php if ($this->params->get('address_check') > 0) : ?>
-		</address>
-		</dd>
 	<?php endif; ?>
 
 <?php if ($this->contact->email_to && $this->params->get('show_email')) : ?>
