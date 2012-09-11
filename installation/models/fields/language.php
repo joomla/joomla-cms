joomla/joomla-cms

commit cbf683e0685892814832ca22bf6f1078eb997337

# Installation languages alpha ordering broken on hosts. Adding method. Thanks Dennis

installation/models/fields/language.php
@@ -65,6 +65,11 @@ class JFormFieldLanguage extends JFormFieldList
 		{
 			$options = array();
 		}
+		// Sort languages by name
+		else
+		{
+			usort($options, array($this, '_sortLanguages'));
+		}
 
 		// Set the default value from the native language.
 		$this->value = $native;
@@ -74,4 +79,15 @@ class JFormFieldLanguage extends JFormFieldList
 
 		return $options;
 	}
+	
+	/**
+	 * Method to sort languages by name.
+	 *
+	 * @since	3.0
+	 */
+	protected function _sortLanguages($a, $b)
+	{
+		return strcmp($a['text'], $b['text']);
+	}
+
 }

commit 8adfcdd269f9961b3633cc4210c1f855b324255b

Fix codestyle error introduced in cbf683e0685892814832ca22bf6f1078eb997337.

installation/models/fields/language.php
@@ -79,7 +79,7 @@ class JFormFieldLanguage extends JFormFieldList
 
 		return $options;
 	}
-	
+
 	/**
 	 * Method to sort languages by name.
 	 *