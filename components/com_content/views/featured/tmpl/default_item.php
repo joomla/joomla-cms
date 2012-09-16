ee3402b8b8a621133b7285db7ada5e87cf09f86b
elinw <elin.waring@gmail.com>
2012-09-16 06:16:52 -0400

    Make unpublished badge use JText

--- components/com_content/views/featured/tmpl/default_item.php
+++ components/com_content/views/featured/tmpl/default_item.php
@@ -42,7 +42,7 @@ $canEdit	= $this->item->params->get('access-edit');
 	</h2>
 	<?php endif; ?>
 	<?php if ($this->item->state == 0): ?>
-		<span class="label label-warning">Unpublished</span>
+		<span class="label label-warning"><?php echo JText::_('JUNPUBLISHED'); ?></span>
 	<?php endif; ?>
 	<?php if ($params->get('show_author') && !empty($this->item->author )) : ?>
 	<small class="createdby">
