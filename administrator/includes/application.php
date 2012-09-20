90f4bf366ae04d4a87d50c549ba37baf433cce9a
Hernán Roa <hernan@nogsus.com>
2012-09-19 22:48:04 -0300

    [Update] Corrección texto metadescription L:151
    Se ha modificado el texto:
    Jokte! - Open Source Content Management
    por:
    Jokte! - CMS Lationamericano Cien por Ciento Libre

--- administrator/includes/application.php
+++ administrator/includes/application.php
@@ -148,7 +148,7 @@ class JAdministrator extends JApplication
 
 			$document->setTitle($this->getCfg('sitename'). ' - ' .JText::_('JADMINISTRATION'));
 			$document->setDescription($this->getCfg('MetaDesc'));
-			$document->setGenerator('Jokte! - Open Source Content Management');
+			$document->setGenerator('Jokte! - CMS Lationamericano Cien por Ciento Libre');
 
 			$contents = JComponentHelper::renderComponent($component);
 			$document->setBuffer($contents, 'component');
