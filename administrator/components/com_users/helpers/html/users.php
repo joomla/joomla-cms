8ed717ab1240867ec41985a4d3f44a9b3015c4e3
Yannick Gaultier <shumisha@gmail.com>
2012-09-22 19:41:56 +0200

    Code formatting

--- administrator/components/com_users/helpers/html/users.php
+++ administrator/components/com_users/helpers/html/users.php
@@ -19,20 +19,20 @@ defined('_JEXEC') or die;
 class JHtmlUsers
 {
 	/**
-	* Display an image.
-	*
-	* @param   string  $src  The source of the image
-	*
-	* @return  string  A <img> element if the specified file exists, otherwise, a null string
-	*
-	* @since   2.5
-	*/
+	 * Display an image.
+	 *
+	 * @param   string  $src  The source of the image
+	 *
+	 * @return  string  A <img> element if the specified file exists, otherwise, a null string
+	 *
+	 * @since   2.5
+	 */
 	public static function image($src)
 	{
 		$src = preg_replace('#[^A-Z0-9\-_\./]#i', '', $src);
 		$file = JPATH_SITE . '/' . $src;
 
-    jimport('joomla.filesystem.path');
+		jimport('joomla.filesystem.path');
 		JPath::check($file);
 
 		if (!file_exists($file))
@@ -44,32 +44,32 @@ class JHtmlUsers
 	}
 
 	/**
-	* Displays an icon to add a note for this user.
-	*
-	* @param   integer  $userId  The user ID
-	*
-	* @return  string  A link to add a note
-	*
-	* @since   2.5
-	*/
+	 * Displays an icon to add a note for this user.
+	 *
+	 * @param   integer  $userId  The user ID
+	 *
+	 * @return  string  A link to add a note
+	 *
+	 * @since   2.5
+	 */
 	public static function addNote($userId)
 	{
 		$title = JText::_('COM_USERS_ADD_NOTE');
 
 		return '<a href="' . JRoute::_('index.php?option=com_users&task=note.add&u_id=' . (int) $userId) . '">'
-				. '<span class="label label-info"><i class="icon-vcard"></i>' . $title . '</span></a>';
+		. '<span class="label label-info"><i class="icon-vcard"></i>' . $title . '</span></a>';
 	}
 
 	/**
-	* Displays an icon to filter the notes list on this user.
-	*
-	* @param   integer  $count   The number of notes for the user
-	* @param   integer  $userId  The user ID
-	*
-	* @return	string  A link to apply a filter
-	*
-	* @since   2.5
-	*/
+	 * Displays an icon to filter the notes list on this user.
+	 *
+	 * @param   integer  $count   The number of notes for the user
+	 * @param   integer  $userId  The user ID
+	 *
+	 * @return	string  A link to apply a filter
+	 *
+	 * @since   2.5
+	 */
 	public static function filterNotes($count, $userId)
 	{
 		if (empty($count))
@@ -80,19 +80,19 @@ class JHtmlUsers
 		$title = JText::_('COM_USERS_FILTER_NOTES');
 
 		return '<a href="' . JRoute::_('index.php?option=com_users&view=notes&filter_search=uid:' . (int) $userId) . '">'
-				. JHtml::_('image', 'admin/filter_16.png', 'COM_USERS_NOTES', array('title' => $title), true) . '</a>';
+		. JHtml::_('image', 'admin/filter_16.png', 'COM_USERS_NOTES', array('title' => $title), true) . '</a>';
 	}
 
 	/**
-   * Displays a note icon.
-   *
-   * @param   integer  $count   The number of notes for the user
-   * @param   integer  $userId  The user ID
-   *
-   * @return	string  A link to a modal window with the user notes
-   *
-   * @since   2.5
-   */
+	 * Displays a note icon.
+	 *
+	 * @param   integer  $count   The number of notes for the user
+	 * @param   integer  $userId  The user ID
+	 *
+	 * @return	string  A link to a modal window with the user notes
+	 *
+	 * @since   2.5
+	 */
 	public static function notes($count, $userId)
 	{
 		if (empty($count))
@@ -103,68 +103,68 @@ class JHtmlUsers
 		$title = JText::plural('COM_USERS_N_USER_NOTES', $count);
 
 		return '<a class="modal"' .
-			' href="' . JRoute::_('index.php?option=com_users&view=notes&tmpl=component&layout=modal&u_id=' . (int) $userId) . '"' .
-			' rel="{handler: \'iframe\', size: {x: 800, y: 450}}">' .
-			'<span class="label label-info"><i class="icon-drawer-2"></i>' . $title . '</span></a>';
+				' href="' . JRoute::_('index.php?option=com_users&view=notes&tmpl=component&layout=modal&u_id=' . (int) $userId) . '"' .
+				' rel="{handler: \'iframe\', size: {x: 800, y: 450}}">' .
+				'<span class="label label-info"><i class="icon-drawer-2"></i>' . $title . '</span></a>';
 	}
 
 	/**
-	* Build an array of block/unblock user states to be used by jgrid.state,
-	* State options will be different for any user
-	* and for currently logged in user
-	*
-	* @param   boolean  $self  True if state array is for currently logged in user
-	*
-	* @return  array  a list of possible states to display
-	*
-	* @since  3.0
-	*/
+	 * Build an array of block/unblock user states to be used by jgrid.state,
+	 * State options will be different for any user
+	 * and for currently logged in user
+	 *
+	 * @param   boolean  $self  True if state array is for currently logged in user
+	 *
+	 * @return  array  a list of possible states to display
+	 *
+	 * @since  3.0
+	 */
 	public static function blockStates( $self = false)
 	{
 		if ($self)
 		{
 			$states = array(
-				1 => array(
-					'task' 				    => 'unblock',
-					'text' 				    => '',
-					'active_title' 		=> JText::_('COM_USERS_USER_FIELD_BLOCK_DESC'),
-					'inactive_title' 	=> '',
-					'tip' 				    => true,
-					'active_class' 		=> 'unpublish',
-					'inactive_class' 	=> 'unpublish'
-				),
-				0 => array(
-					'task' 				    => 'block',
-					'text' 				    => '',
-					'active_title' 		=> '',
-					'inactive_title' 	=> JText::_('COM_USERS_USERS_ERROR_CANNOT_BLOCK_SELF'),
-					'tip' 				    => true,
-					'active_class' 		=> 'publish',
-					'inactive_class' 	=> 'publish'
-				)
+					1 => array(
+							'task' 				    => 'unblock',
+							'text' 				    => '',
+							'active_title' 		=> JText::_('COM_USERS_USER_FIELD_BLOCK_DESC'),
+							'inactive_title' 	=> '',
+							'tip' 				    => true,
+							'active_class' 		=> 'unpublish',
+							'inactive_class' 	=> 'unpublish'
+					),
+					0 => array(
+							'task' 				    => 'block',
+							'text' 				    => '',
+							'active_title' 		=> '',
+							'inactive_title' 	=> JText::_('COM_USERS_USERS_ERROR_CANNOT_BLOCK_SELF'),
+							'tip' 				    => true,
+							'active_class' 		=> 'publish',
+							'inactive_class' 	=> 'publish'
+					)
 			);
 		}
 		else
 		{
 			$states = array(
-				1 => array(
-					'task' 				    => 'unblock',
-					'text' 				    => '',
-					'active_title' 		=> JText::_('COM_USERS_TOOLBAR_UNBLOCK'),
-					'inactive_title' 	=> '',
-					'tip' 				    => true,
-					'active_class' 		=> 'unpublish',
-					'inactive_class' 	=> 'unpublish'
-				),
-				0 => array(
-					'task' 				    => 'block',
-					'text' 				    => '',
-					'active_title' 		=> JText::_('COM_USERS_USER_FIELD_BLOCK_DESC'),
-					'inactive_title' 	=> '',
-					'tip' 				    => true,
-					'active_class' 		=> 'publish',
-					'inactive_class' 	=> 'publish'
-				)
+					1 => array(
+							'task' 				    => 'unblock',
+							'text' 				    => '',
+							'active_title' 		=> JText::_('COM_USERS_TOOLBAR_UNBLOCK'),
+							'inactive_title' 	=> '',
+							'tip' 				    => true,
+							'active_class' 		=> 'unpublish',
+							'inactive_class' 	=> 'unpublish'
+					),
+					0 => array(
+							'task' 				    => 'block',
+							'text' 				    => '',
+							'active_title' 		=> JText::_('COM_USERS_USER_FIELD_BLOCK_DESC'),
+							'inactive_title' 	=> '',
+							'tip' 				    => true,
+							'active_class' 		=> 'publish',
+							'inactive_class' 	=> 'publish'
+					)
 			);
 		}
 
@@ -172,33 +172,33 @@ class JHtmlUsers
 	}
 
 	/**
-	* Build an array of activate states to be used by jgrid.state,
-	*
-	* @return  array  a list of possible states to display
-	*
-	* @since  3.0
-	*/
+	 * Build an array of activate states to be used by jgrid.state,
+	 *
+	 * @return  array  a list of possible states to display
+	 *
+	 * @since  3.0
+	 */
 	public static function activateStates()
 	{
 		$states = array(
-			1	=> array(
-				'task' 				    => 'activate',
-				'text' 				    => '',
-				'active_title' 		=> JText::_('COM_USERS_TOOLBAR_ACTIVATE'),
-				'inactive_title' 	=> '',
-				'tip' 				    => true,
-				'active_class' 		=> 'unpublish',
-				'inactive_class' 	=> 'unpublish'
-			),
-			0	=> array(
-				'task' 				    => '',
-				'text' 				    => '',
-				'active_title' 		=> '',
-				'inactive_title' 	=> JText::_('COM_USERS_ACTIVATED'),
-				'tip' 				    => true,
-				'active_class' 		=> 'publish',
-				'inactive_class' 	=> 'publish'
-			)
+				1	=> array(
+						'task' 				    => 'activate',
+						'text' 				    => '',
+						'active_title' 		=> JText::_('COM_USERS_TOOLBAR_ACTIVATE'),
+						'inactive_title' 	=> '',
+						'tip' 				    => true,
+						'active_class' 		=> 'unpublish',
+						'inactive_class' 	=> 'unpublish'
+				),
+				0	=> array(
+						'task' 				    => '',
+						'text' 				    => '',
+						'active_title' 		=> '',
+						'inactive_title' 	=> JText::_('COM_USERS_ACTIVATED'),
+						'tip' 				    => true,
+						'active_class' 		=> 'publish',
+						'inactive_class' 	=> 'publish'
+				)
 		);
 		return $states;
 	}
--- administrator/components/com_users/views/users/tmpl/default.php
+++ administrator/components/com_users/views/users/tmpl/default.php
@@ -21,61 +21,65 @@ $listDirn = $this->escape($this->state->get('list.direction'));
 $loggeduser = JFactory::getUser();
 ?>
 
-<form action="<?php echo JRoute::_('index.php?option=com_users&view=users');?>" method="post" name="adminForm" id="adminForm">
+<form
+	action="<?php echo JRoute::_('index.php?option=com_users&view=users');?>"
+	method="post" name="adminForm" id="adminForm">
 	<div id="filter-bar" class="btn-toolbar">
 		<div class="filter-search btn-group pull-left">
-			<input type="text" name="filter_search" id="filter_search" placeholder="<?php echo JText::_('COM_USERS_SEARCH_USERS'); ?>" value="<?php echo $this->escape($this->state->get('filter.search')); ?>" title="<?php echo JText::_('COM_USERS_SEARCH_USERS'); ?>" />
+			<input type="text" name="filter_search" id="filter_search"
+				placeholder="<?php echo JText::_('COM_USERS_SEARCH_USERS'); ?>"
+				value="<?php echo $this->escape($this->state->get('filter.search')); ?>"
+				title="<?php echo JText::_('COM_USERS_SEARCH_USERS'); ?>" />
 		</div>
 		<div class="btn-group pull-left">
-			<button type="submit" class="btn tip" title="<?php echo JText::_('JSEARCH_FILTER_SUBMIT'); ?>"><i class="icon-search"></i></button>
-			<button type="button" class="btn tip" onclick="document.id('filter_search').value='';this.form.submit();" title="<?php echo JText::_('JSEARCH_RESET'); ?>"><i class="icon-remove"></i></button>
+			<button type="submit" class="btn tip"
+				title="<?php echo JText::_('JSEARCH_FILTER_SUBMIT'); ?>">
+				<i class="icon-search"></i>
+			</button>
+			<button type="button" class="btn tip"
+				onclick="document.id('filter_search').value='';this.form.submit();"
+				title="<?php echo JText::_('JSEARCH_RESET'); ?>">
+				<i class="icon-remove"></i>
+			</button>
 		</div>
 	</div>
-	<div class="clearfix"> </div>
+	<div class="clearfix"></div>
 	<table class="table table-striped">
 		<thead>
 			<tr>
-				<th width="1%" class="nowrap center">
-					<input type="checkbox" name="checkall-toggle" value="" title="<?php echo JText::_('JGLOBAL_CHECK_ALL'); ?>" onclick="Joomla.checkAll(this)" />
+				<th width="1%" class="nowrap center"><input type="checkbox"
+					name="checkall-toggle" value=""
+					title="<?php echo JText::_('JGLOBAL_CHECK_ALL'); ?>"
+					onclick="Joomla.checkAll(this)" />
 				</th>
-				<th class="left">
-					<?php echo JHtml::_('grid.sort', 'COM_USERS_HEADING_NAME', 'a.name', $listDirn, $listOrder); ?>
+				<th class="left"><?php echo JHtml::_('grid.sort', 'COM_USERS_HEADING_NAME', 'a.name', $listDirn, $listOrder); ?>
 				</th>
-				<th width="10%" class="nowrap center">
-					<?php echo JHtml::_('grid.sort', 'JGLOBAL_USERNAME', 'a.username', $listDirn, $listOrder); ?>
+				<th width="10%" class="nowrap center"><?php echo JHtml::_('grid.sort', 'JGLOBAL_USERNAME', 'a.username', $listDirn, $listOrder); ?>
 				</th>
-				<th width="5%" class="nowrap center">
-					<?php echo JHtml::_('grid.sort', 'COM_USERS_HEADING_ENABLED', 'a.block', $listDirn, $listOrder); ?>
+				<th width="5%" class="nowrap center"><?php echo JHtml::_('grid.sort', 'COM_USERS_HEADING_ENABLED', 'a.block', $listDirn, $listOrder); ?>
 				</th>
-				<th width="5%" class="nowrap center">
-					<?php echo JHtml::_('grid.sort', 'COM_USERS_HEADING_ACTIVATED', 'a.activation', $listDirn, $listOrder); ?>
+				<th width="5%" class="nowrap center"><?php echo JHtml::_('grid.sort', 'COM_USERS_HEADING_ACTIVATED', 'a.activation', $listDirn, $listOrder); ?>
 				</th>
-				<th width="10%" class="nowrap center">
-					<?php echo JText::_('COM_USERS_HEADING_GROUPS'); ?>
+				<th width="10%" class="nowrap center"><?php echo JText::_('COM_USERS_HEADING_GROUPS'); ?>
 				</th>
-				<th width="15%" class="nowrap center">
-					<?php echo JHtml::_('grid.sort', 'JGLOBAL_EMAIL', 'a.email', $listDirn, $listOrder); ?>
+				<th width="15%" class="nowrap center"><?php echo JHtml::_('grid.sort', 'JGLOBAL_EMAIL', 'a.email', $listDirn, $listOrder); ?>
 				</th>
-				<th width="10%" class="nowrap center">
-					<?php echo JHtml::_('grid.sort', 'COM_USERS_HEADING_LAST_VISIT_DATE', 'a.lastvisitDate', $listDirn, $listOrder); ?>
+				<th width="10%" class="nowrap center"><?php echo JHtml::_('grid.sort', 'COM_USERS_HEADING_LAST_VISIT_DATE', 'a.lastvisitDate', $listDirn, $listOrder); ?>
 				</th>
-				<th width="10%" class="nowrap center">
-					<?php echo JHtml::_('grid.sort', 'COM_USERS_HEADING_REGISTRATION_DATE', 'a.registerDate', $listDirn, $listOrder); ?>
+				<th width="10%" class="nowrap center"><?php echo JHtml::_('grid.sort', 'COM_USERS_HEADING_REGISTRATION_DATE', 'a.registerDate', $listDirn, $listOrder); ?>
 				</th>
-				<th width="1%" class="nowrap center">
-					<?php echo JHtml::_('grid.sort', 'JGRID_HEADING_ID', 'a.id', $listDirn, $listOrder); ?>
+				<th width="1%" class="nowrap center"><?php echo JHtml::_('grid.sort', 'JGRID_HEADING_ID', 'a.id', $listDirn, $listOrder); ?>
 				</th>
 			</tr>
 		</thead>
 		<tfoot>
 			<tr>
-				<td colspan="15">
-					<?php echo $this->pagination->getListFooter(); ?>
+				<td colspan="15"><?php echo $this->pagination->getListFooter(); ?>
 				</td>
 			</tr>
 		</tfoot>
 		<tbody>
-		<?php foreach ($this->items as $i => $item) :
+			<?php foreach ($this->items as $i => $item) :
 			$canEdit   = $canDo->get('core.edit');
 			$canChange = $loggeduser->authorise('core.edit.state',	'com_users');
 
@@ -85,71 +89,55 @@ $loggeduser = JFactory::getUser();
 				$canEdit   = false;
 				$canChange = false;
 			}
-		?>
+			?>
 			<tr class="row<?php echo $i % 2; ?>">
-				<td class="center">
-					<?php if ($canEdit) : ?>
-						<?php echo JHtml::_('grid.id', $i, $item->id); ?>
+				<td class="center"><?php if ($canEdit) : ?> <?php echo JHtml::_('grid.id', $i, $item->id); ?>
 					<?php endif; ?>
 				</td>
-				<td>
-					<?php if ($canEdit) : ?>
-						<a href="<?php echo JRoute::_('index.php?option=com_users&task=user.edit&id='.(int) $item->id); ?>" title="<?php echo JText::sprintf('COM_USERS_EDIT_USER', $this->escape($item->name)); ?>">
-							<?php echo $this->escape($item->name); ?></a>
-					<?php else : ?>
+				<td><?php if ($canEdit) : ?> <a
+					href="<?php echo JRoute::_('index.php?option=com_users&task=user.edit&id='.(int) $item->id); ?>"
+					title="<?php echo JText::sprintf('COM_USERS_EDIT_USER', $this->escape($item->name)); ?>">
 						<?php echo $this->escape($item->name); ?>
-					<?php endif; ?>
+				</a> <?php else : ?> <?php echo $this->escape($item->name); ?> <?php endif; ?>
 					<div>
 						<?php echo JHtml::_('users.filterNotes', $item->note_count, $item->id); ?>
 						<?php echo JHtml::_('users.notes', $item->note_count, $item->id); ?>
 						<?php echo JHtml::_('users.addNote', $item->id); ?>
-					</div>
-					<?php if (JDEBUG) : ?>
-						<div class="small"><a href="<?php echo JRoute::_('index.php?option=com_users&view=debuguser&user_id='.(int) $item->id);?>">
-						<?php echo JText::_('COM_USERS_DEBUG_USER');?></a></div>
+					</div> <?php if (JDEBUG) : ?>
+					<div class="small">
+						<a
+							href="<?php echo JRoute::_('index.php?option=com_users&view=debuguser&user_id='.(int) $item->id);?>">
+							<?php echo JText::_('COM_USERS_DEBUG_USER');?>
+						</a>
+					</div> <?php endif; ?>
+				</td>
+				<td class="center"><?php echo $this->escape($item->username); ?>
+				</td>
+				<td class="center"><?php if ($canChange) : ?> <?php 
+				$self = $loggeduser->id == $item->id;
+				echo JHtml::_('jgrid.state', JHtmlUsers::blockStates( $self), $item->block, $i, 'users.', !$self);
+				?> <?php else : ?> <?php echo JText::_($item->block ? 'JNO' : 'JYES'); ?>
 					<?php endif; ?>
 				</td>
-				<td class="center">
-					<?php echo $this->escape($item->username); ?>
+				<td class="center"><?php 
+				$activated = empty( $item->activation) ? 0 : 1;
+				echo JHtml::_('jgrid.state', JHtmlUsers::activateStates(), $activated, $i, 'users.', (boolean) $activated);
+				?>
 				</td>
-				<td class="center">
-					<?php if ($canChange) : ?>
-							<?php 
-							  $self = $loggeduser->id == $item->id;
-							  echo JHtml::_('jgrid.state', JHtmlUsers::blockStates( $self), $item->block, $i, 'users.', !$self); 
-							  ?>
-					<?php else : ?>
-						<?php echo JText::_($item->block ? 'JNO' : 'JYES'); ?>
-					<?php endif; ?>
+				<td class="center"><?php if (substr_count($item->group_names, "\n") > 1) : ?>
+					<span class="hasTip"
+					title="<?php echo JText::_('COM_USERS_HEADING_GROUPS').'::'.nl2br($item->group_names); ?>"><?php echo JText::_('COM_USERS_USERS_MULTIPLE_GROUPS'); ?>
+				</span> <?php else : ?> <?php echo nl2br($item->group_names); ?> <?php endif; ?>
 				</td>
-				<td class="center">
-					<?php 
-					  $activated = empty( $item->activation) ? 0 : 1;
-					  echo JHtml::_('jgrid.state', JHtmlUsers::activateStates(), $activated, $i, 'users.', (boolean) $activated);
-					?>
+				<td class="center"><?php echo $this->escape($item->email); ?>
 				</td>
-				<td class="center">
-					<?php if (substr_count($item->group_names, "\n") > 1) : ?>
-						<span class="hasTip" title="<?php echo JText::_('COM_USERS_HEADING_GROUPS').'::'.nl2br($item->group_names); ?>"><?php echo JText::_('COM_USERS_USERS_MULTIPLE_GROUPS'); ?></span>
-					<?php else : ?>
-						<?php echo nl2br($item->group_names); ?>
-					<?php endif; ?>
+				<td class="center"><?php if ($item->lastvisitDate != '0000-00-00 00:00:00'):?>
+					<?php echo JHtml::_('date', $item->lastvisitDate, 'Y-m-d H:i:s'); ?>
+					<?php else:?> <?php echo JText::_('JNEVER'); ?> <?php endif;?>
 				</td>
-				<td class="center">
-					<?php echo $this->escape($item->email); ?>
+				<td class="center"><?php echo JHtml::_('date', $item->registerDate, 'Y-m-d H:i:s'); ?>
 				</td>
-				<td class="center">
-					<?php if ($item->lastvisitDate != '0000-00-00 00:00:00'):?>
-						<?php echo JHtml::_('date', $item->lastvisitDate, 'Y-m-d H:i:s'); ?>
-					<?php else:?>
-						<?php echo JText::_('JNEVER'); ?>
-					<?php endif;?>
-				</td>
-				<td class="center">
-					<?php echo JHtml::_('date', $item->registerDate, 'Y-m-d H:i:s'); ?>
-				</td>
-				<td class="center">
-					<?php echo (int) $item->id; ?>
+				<td class="center"><?php echo (int) $item->id; ?>
 				</td>
 			</tr>
 			<?php endforeach; ?>
@@ -159,9 +147,9 @@ $loggeduser = JFactory::getUser();
 	<?php //Load the batch processing form. ?>
 	<?php echo $this->loadTemplate('batch'); ?>
 
-	<input type="hidden" name="task" value="" />
-	<input type="hidden" name="boxchecked" value="0" />
-	<input type="hidden" name="filter_order" value="<?php echo $listOrder; ?>" />
-	<input type="hidden" name="filter_order_Dir" value="<?php echo $listDirn; ?>" />
+	<input type="hidden" name="task" value="" /> <input type="hidden"
+		name="boxchecked" value="0" /> <input type="hidden"
+		name="filter_order" value="<?php echo $listOrder; ?>" /> <input
+		type="hidden" name="filter_order_Dir" value="<?php echo $listDirn; ?>" />
 	<?php echo JHtml::_('form.token'); ?>
 </form>
