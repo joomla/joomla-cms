14eea1e2bf9993af8c5554e31f85e453f205ac30
Sergey Romanov <serg4172@mail.ru>
2012-10-02 13:01:37 +0800

    fixed raw html in menu path

--- administrator/components/com_menus/views/items/tmpl/default.php
+++ administrator/components/com_menus/views/items/tmpl/default.php
@@ -211,7 +211,7 @@ $sortFields = $this->getSortFields();
 						<div class="small" title="<?php echo $this->escape($item->path);?>">
 							<?php echo str_repeat('<span class="gtr">&mdash;</span>', $item->level - 1) ?>
 							<span title="<?php echo isset($item->item_type_desc) ? htmlspecialchars($this->escape($item->item_type_desc), ENT_COMPAT, 'UTF-8') : ''; ?>">
-								<?php echo $this->escape($item->item_type); ?></span>
+								<?php echo $item->item_type; ?></span>
 						</div>
 					</td>
 					<td class="center hidden-phone">
