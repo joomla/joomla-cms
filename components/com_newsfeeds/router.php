b83abb10a1937018cf9a73cda68cf573f7eeb976
Júlio Pontes <juliopfneto@gmail.com>
2012-10-03 14:17:17 -0300

    Updated to JDatabaseQuery Object

--- components/com_newsfeeds/router.php
+++ components/com_newsfeeds/router.php
@@ -172,7 +172,8 @@ function NewsfeedsParseRoute($segments)
 			if($advanced)
 			{
 				$db = JFactory::getDBO();
-				$query = 'SELECT id FROM #__newsfeeds WHERE catid = '.$vars['catid'].' AND alias = '.$db->Quote($segment);
+				$query = $db->getQuery(true);
+				$query->select('c.id')->from($db->quoteName('#__newsfeeds').' AS c')->where('c.catid = '.$vars['catid'])->where('c.alias = '.$db->Quote($segment));
 				$db->setQuery($query);
 				$nid = $db->loadResult();
 			} else {
