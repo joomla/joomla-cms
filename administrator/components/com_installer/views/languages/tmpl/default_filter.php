e235a9b04f88701cef2cd0bbed6e988eb0b2bb6f
Jean-Marie Simonet <infografjms@gmail.com>
2012-10-03 17:40:09 +0200

    # [#29426] *Missing list limit in Extensions Manager => Manage and Install Languages

--- administrator/components/com_installer/views/languages/tmpl/default_filter.php
+++ administrator/components/com_installer/views/languages/tmpl/default_filter.php
@@ -11,6 +11,10 @@ defined('_JEXEC') or die;
 JHtml::_('bootstrap.tooltip');
 ?>
 <div id="filter-bar" class="btn-toolbar">
+	<div class="btn-group pull-right hidden-phone">
+		<label for="limit" class="element-invisible"><?php echo JText::_('JFIELD_PLG_SEARCH_SEARCHLIMIT_DESC');?></label>
+			<?php echo $this->pagination->getLimitBox(); ?>
+	</div>
 	<div class="filter-search btn-group pull-left">
 		<label for="filter_search" class="element-invisible"><?php echo JText::_('JSEARCH_FILTER_LABEL');?></label>
 		<input type="text" name="filter_search" placeholder="<?php echo JText::_('COM_INSTALLER_LANGUAGES_FILTER_SEARCH_DESC'); ?>" id="filter_search" value="<?php echo $this->escape($this->state->get('filter.search')); ?>" title="<?php echo JText::_('COM_INSTALLER_LANGUAGES_FILTER_SEARCH_DESC'); ?>" />
