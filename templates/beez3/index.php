c13893c8125acdcbfafde8772a76cbd60902c994
Constantin Romankiewicz <harmageddon9@web.de>
2012-10-08 09:05:02 +0200

    # [#29451] beez3: element closed before opened. Thanks Constantin.

--- templates/beez3/index.php
+++ templates/beez3/index.php
@@ -109,9 +109,9 @@ $doc->addScript($this->baseurl . '/templates/' . $this->template . '/javascript/
 						<?php elseif (!$logo AND $config->get('sitename')): ?>
 							<?php echo htmlspecialchars($config->get('sitename'));?>
 						<?php endif; ?>
-						</span></h1>
 						<span class="header1">
 						<?php echo htmlspecialchars($templateparams->get('sitedescription'));?>
+						</span></h1>
 
 					</div><!-- end logoheader -->
 					<ul class="skiplinks">
