ea42a19652a2dd5c5c82d9ac02365101571066e7
tuxmerlin <migueltuyare@gmail.com>
2012-10-09 19:52:59 -0300

    [upgrade] Cambio definición de clase componente login y controller. Eliminación de librería

--- administrator/components/com_login/models/login.php
+++ administrator/components/com_login/models/login.php
@@ -4,10 +4,7 @@
  * @license		GNU General Public License, see LICENSE.php
  */
 
-// No direct access.
 defined('_JEXEC') or die;
-
-jimport( 'joomla.application.component.model' );
 
 /**
  * Login Model
@@ -16,7 +13,7 @@ jimport( 'joomla.application.component.model' );
  * @subpackage	com_login
  * @since		1.5
  */
-class LoginModelLogin extends JModel
+class LoginModelLogin extends JModelLegacy
 {
 	/**
 	 * Method to auto-populate the model state.
