1ac68a4af6f0d310d0ca22e694a2b684c888d3b9
tuxmerlin <migueltuyare@gmail.com>
2012-10-09 20:27:54 -0300

    [upgrade] Cambio definición de clases

--- administrator/modules/mod_latest/helper.php
+++ administrator/modules/mod_latest/helper.php
@@ -4,10 +4,9 @@
  * @license		GNU General Public License version 2 or later; see LICENSE.txt
  */
 
-// No direct access.
 defined('_JEXEC') or die;
 
-JModel::addIncludePath(JPATH_ADMINISTRATOR.'/components/com_content/models', 'ContentModel');
+JModelLegacy::addIncludePath(JPATH_ADMINISTRATOR.'/components/com_content/models', 'ContentModel');
 
 jimport('joomla.application.categories');
 
@@ -30,7 +29,7 @@ abstract class modLatestHelper
 		$user = JFactory::getuser();
 
 		// Get an instance of the generic articles model
-		$model = JModel::getInstance('Articles', 'ContentModel', array('ignore_request' => true));
+		$model = JModelLegacy::getInstance('Articles', 'ContentModel', array('ignore_request' => true));
 
 		// Set List SELECT
 		$model->setState('list.select', 'a.id, a.title, a.checked_out, a.checked_out_time, ' .
