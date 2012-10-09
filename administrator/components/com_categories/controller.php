7e36dbe2464211dcbb73ae32a39198d161968b91
tuxmerlin <migueltuyare@gmail.com>
2012-10-09 19:22:51 -0300

    [upgrade] Cambio definición de clase componente categories y controller. Eliminación de librería

--- administrator/components/com_categories/controller.php
+++ administrator/components/com_categories/controller.php
@@ -4,10 +4,7 @@
  * @license		GNU General Public License version 2 or later; see LICENSE.txt
  */
 
-// No direct access
 defined('_JEXEC') or die;
-
-jimport('joomla.application.component.controller');
 
 /**
  * Categories view class for the Category package.
@@ -16,7 +13,7 @@ jimport('joomla.application.component.controller');
  * @subpackage	com_categories
  * @since		1.6
  */
-class CategoriesController extends JController
+class CategoriesController extends JControllerLegacy
 {
 	/**
 	 * @var		string	The extension for which the categories apply.
