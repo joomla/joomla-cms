b80a02f4ff403cc32fe3ce4abc95e2d7ae3dc0f8
tuxmerlin <migueltuyare@gmail.com>
2012-10-09 20:43:28 -0300

    [fix] [upgrade] Error en llamada al parámetro para mostrar cabecera de página

--- components/com_contact/views/category/tmpl/default.php
+++ components/com_contact/views/category/tmpl/default.php
@@ -11,7 +11,7 @@ defined('_JEXEC') or die;
 
 ?>
 <div class="contact-category<?php echo $this->pageclass_sfx;?>">
-<?php if ($this->params->get('show_page_heading', 1)) : ?>
+<?php if ($this->params->get('show_page_heading')) : ?>
 <h1>
 	<?php echo $this->escape($this->params->get('page_heading')); ?>
 </h1>
