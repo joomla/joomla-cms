17717dc1f66cb3e1b85e54c74ea0caad808648ec
tuxmerlin <migueltuyare@gmail.com>
2012-10-09 20:07:43 -0300

    [upgrade] Cambio definición de clase componente plugins y controller. Eliminación de librería

--- administrator/components/com_plugins/controller.php
+++ administrator/components/com_plugins/controller.php
@@ -4,10 +4,7 @@
  * @license		GNU General Public License version 2 or later; see LICENSE.txt
  */
 
-// No direct access.
 defined('_JEXEC') or die;
-
-jimport('joomla.application.component.controller');
 
 /**
  * Plugins master display controller.
@@ -16,7 +13,7 @@ jimport('joomla.application.component.controller');
  * @subpackage	com_plugins
  * @since		1.5
  */
-class PluginsController extends JController
+class PluginsController extends JControllerLegacy
 {
 	/**
 	 * Method to display a view.
