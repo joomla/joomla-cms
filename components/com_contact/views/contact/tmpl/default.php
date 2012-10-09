dc67ee27849252415b892531917ec12f2932d163
tuxmerlin <migueltuyare@gmail.com>
2012-10-09 20:46:43 -0300

    [fix] [upgrade] Error en llamada al parámetro para mostrar cabecera de página y carga de librerías

--- components/com_contact/views/contact/tmpl/default.php
+++ components/com_contact/views/contact/tmpl/default.php
@@ -11,7 +11,7 @@ defined('_JEXEC') or die;
 $cparams = JComponentHelper::getParams ('com_media');
 ?>
 <div class="contact<?php echo $this->pageclass_sfx?>">
-<?php if ($this->params->get('show_page_heading', 1)) : ?>
+<?php if ($this->params->get('show_page_heading')) : ?>
 <h1>
 	<?php echo $this->escape($this->params->get('page_heading')); ?>
 </h1>
