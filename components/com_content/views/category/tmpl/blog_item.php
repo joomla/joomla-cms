4a185346b64ababe17b81372919a94bbe77f9fe2
dustwolf <jure.sah@denia.si>
2012-10-20 19:25:16 +0300

    Update components/com_content/views/category/tmpl/blog_item.php
    [#29543] Category on article is sometimes not displayed

--- components/com_content/views/category/tmpl/blog_item.php
+++ components/com_content/views/category/tmpl/blog_item.php
@@ -70,7 +70,7 @@ JHtml::_('behavior.framework');
 
 	<?php // to do not that elegant would be nice to group the params ?>
 	<?php $useDefList = (($params->get('show_modify_date')) or ($params->get('show_publish_date'))
-		or ($params->get('show_hits'))); ?>
+		or ($params->get('show_hits')) or ($params->get('show_category'))); ?>
 	<?php if ($useDefList AND ($info == 0 OR $info == 2)) : ?>
 		<div class="article-info muted">
 			<dl class="article-info">
