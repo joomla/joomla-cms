4515476ff219e4867a3d266510038affa9e2947a
dustwolf <jure.sah@denia.si>
2012-10-23 22:13:24 +0300

    Update components/com_content/views/article/tmpl/default.php
    or to uppercase "to be consistent with entire Joomla code".

--- components/com_content/views/article/tmpl/default.php
+++ components/com_content/views/article/tmpl/default.php
@@ -87,8 +87,8 @@ if (!empty($this->item->pagination) AND $this->item->pagination && !$this->item-
 	</div>
 	<?php endif; ?>
 
-<?php $useDefList = (($params->get('show_modify_date')) or ($params->get('show_publish_date'))
-	or ($params->get('show_hits')) or ($params->get('show_category') or ($params->get('show_parent_category')); ?>
+<?php $useDefList = (($params->get('show_modify_date')) OR ($params->get('show_publish_date'))
+	or ($params->get('show_hits')) OR ($params->get('show_category') OR ($params->get('show_parent_category')); ?>
 	<?php if ($useDefList AND ($info == 0 OR $info == 2)) : ?>
 		<div class="article-info muted">
 			<dl class="article-info">
