7177d3514d11f278980e64ce34ae741c23d27b08
dongilbert <don@dongilbert.net>
2012-10-24 12:12:20 -0500

    Adding JMail::IsHTML and have it return  for chaining. Includes tests.

--- libraries/joomla/mail/mail.php
+++ libraries/joomla/mail/mail.php
@@ -351,6 +351,21 @@ class JMail extends PHPMailer
 	}
 
 	/**
+	 * Sets message type to HTML
+	 *
+	 * @param   bool  $ishtml  Boolean true or false. 
+	 *
+	 * @return  JMail  Returns this object for chaining.
+	 *
+	 * @since   12.3
+	 */
+	public function IsHTML($ishtml = true)
+	{
+		parent::IsHTML($ishtml);
+		return $this;
+	}
+
+	/**
 	 * Use sendmail for sending the email
 	 *
 	 * @param   string  $sendmail  Path to sendmail [optional]
