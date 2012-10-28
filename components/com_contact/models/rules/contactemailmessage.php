723955e867fe9ac38f7f727a53adff107845785e
elinw <elin.waring@gmail.com>
2012-10-28 17:41:44 -0400

    Fix problem in contact rules and more code style

--- components/com_contact/models/rules/contactemailmessage.php
+++ components/com_contact/models/rules/contactemailmessage.php
@@ -18,15 +18,15 @@ defined('_JEXEC') or die;
 class JFormRuleContactEmailMessage extends JFormRule
 {
 	/**
-	 * Method to test for a valid color in hexadecimal.
+	 * Method to test a message for banned words
 	 *
 	 * @param   SimpleXMLElement  $element  The SimpleXMLElement object representing the <field /> tag for the form field object.
 	 * @param   mixed             $value     The form field value to validate.
 	 * @param   string            $group     The field name group control value. This acts as as an array container for the field.
 	 *                                       For example if the field has name="foo" and the group value is set to "bar" then the
 	 *                                       full field name would end up being "bar[foo]".
-	 * @param   object            $input    An optional JRegistry object with the entire data set to validate against the entire form.
-	 * @param   object            $form     The form object for which the field is being tested.
+	 * @param   JRegistry         $input    An optional JRegistry object with the entire data set to validate against the entire form.
+	 * @param   JForm             $form     The form object for which the field is being tested.
 	 *
 	 * @return  boolean  True if the value is valid, false otherwise.
 	 */
@@ -35,9 +35,12 @@ class JFormRuleContactEmailMessage extends JFormRule
 		$params = JComponentHelper::getParams('com_contact');
 		$banned = $params->get('banned_text');
 
-		foreach(explode(';', $banned) as $item){
+		foreach(explode(';', $banned) as $item)
+		{
 			if (JString::stristr($item, $value) !== false)
-					return false;
+			{
+				return false;
+			}
 		}
 
 		return true;
