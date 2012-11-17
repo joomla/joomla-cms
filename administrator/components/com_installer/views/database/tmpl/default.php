8fa01ad784996b36827b745a4694ba825cf221ce
tuxmerlin <migueltuyare@gmail.com>
2012-11-17 09:21:37 -0300

    [cambio] Nueva salida html actualizador de schemas de base de datos para versiones para Jokte!

--- administrator/components/com_installer/views/database/tmpl/default.php
+++ administrator/components/com_installer/views/database/tmpl/default.php
@@ -31,12 +31,12 @@ defined('_JEXEC') or die;
 				<li><?php echo JText::_('COM_INSTALLER_MSG_DATABASE_FILTER_ERROR'); ?>
 			<?php endif; ?>
 
-			<?php if (!(strncmp($this->schemaVersion, JVERSION, 5) === 0)) : ?>
-				<li><?php echo JText::sprintf('COM_INSTALLER_MSG_DATABASE_SCHEMA_ERROR', $this->schemaVersion, JVERSION); ?></li>
+			<?php if (!(strncmp($this->schemaVersion, VJOKTE, 5) === 0)) : ?>
+				<li><?php echo JText::sprintf('COM_INSTALLER_MSG_DATABASE_SCHEMA_ERROR', $this->schemaVersion, VJOKTE); ?></li>
 			<?php endif; ?>
 
-			<?php if (($this->updateVersion != JVERSION)) : ?>
-				<li><?php echo JText::sprintf('COM_INSTALLER_MSG_DATABASE_UPDATEVERSION_ERROR', $this->updateVersion, JVERSION); ?></li>
+			<?php if (($this->updateVersion != VJOKTE)) : ?>
+				<li><?php echo JText::sprintf('COM_INSTALLER_MSG_DATABASE_UPDATEVERSION_ERROR', $this->updateVersion, VJOKTE); ?></li>
 			<?php endif; ?>
 
 			<?php foreach($this->errors as $line => $error) : ?>
