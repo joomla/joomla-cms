f6b7be16f0c44cf65c74d1a89a46e1d4daac666f
Nick Savov <nick@iowawebcompany.com>
2012-11-23 23:48:41 -0600

    Update administrator/components/com_joomlaupdate/models/default.php
    Added pre-upgrade check code

--- administrator/components/com_joomlaupdate/models/default.php
+++ administrator/components/com_joomlaupdate/models/default.php
@@ -684,4 +684,203 @@ ENDDATA;
 		// Unset the update filename from the session
 		JFactory::getApplication()->setUserState('com_joomlaupdate.file', null);
 	}
+	
+	/**
+	 * Gets PHP options.
+	 *
+	 * @return	array
+	 *
+	 * @since   3.0
+	 */
+	public function getPhpOptions(SimpleXMLElement $xml = null)
+	{
+		$options = array();
+
+		// Check the PHP Version.
+		$option = new stdClass;
+		$option->label  = JText::_('COM_JOOMLAUPDATE_INSTL_PHP_VERSION') . ' >= 5.3.1';
+		$option->state  = version_compare(PHP_VERSION, (string)$xml->php_version, '>=');
+		$option->notice = null;
+		$options[] = $option;
+
+		// Check for magic quotes gpc.
+		$option = new stdClass;
+		$option->label  = JText::_('COM_JOOMLAUPDATE_INSTL_MAGIC_QUOTES_GPC');
+		$option->state  = (ini_get('magic_quotes_gpc') == ((string)$xml->magic_quotes_gpc == "true" ? true : false));
+		$option->notice = null;
+		$options[] = $option;
+
+		// Check for register globals.
+		$option = new stdClass;
+		$option->label  = JText::_('COM_JOOMLAUPDATE_INSTL_REGISTER_GLOBALS');
+		$option->state  = (ini_get('register_globals') == ((string)$xml->register_globals == "true" ? true : false));
+		$option->notice = null;
+		$options[] = $option;
+
+		// Check for zlib support.
+		$option = new stdClass;
+		$option->label  = JText::_('COM_JOOMLAUPDATE_INSTL_ZLIB_COMPRESSION_SUPPORT');
+		$option->state  = extension_loaded('zlib');
+		$option->notice = null;
+		$options[] = $option;
+
+		// Check for XML support.
+		$option = new stdClass;
+		$option->label  = JText::_('COM_JOOMLAUPDATE_INSTL_XML_SUPPORT');
+		$option->state  = extension_loaded('xml');
+		$option->notice = null;
+		$options[] = $option;
+
+		// Check for database support.
+		// We are satisfied if there is at least one database driver available.
+		$available = JDatabase::getConnectors();
+		$option = new stdClass;
+		$option->label  = JText::_('COM_JOOMLAUPDATE_INSTL_DATABASE_SUPPORT');
+		$option->label .= '<br />(' . implode(', ', $available) . ')';
+		$option->state  = count($available);
+		$option->notice = null;
+		$options[] = $option;
+
+		// Check for mbstring options.
+		if (extension_loaded('mbstring'))
+		{
+			// Check for default MB language.
+			$option = new stdClass;
+			$option->label  = JText::_('COM_JOOMLAUPDATE_INSTL_MB_LANGUAGE_IS_DEFAULT');
+			$option->state  = (strtolower(ini_get('mbstring.language')) == (string)$xml->mbstring_language);
+			$option->notice = ($option->state) ? null : JText::_('COM_JOOMLAUPDATE_INSTL_NOTICEMBLANGNOTDEFAULT');
+			$options[] = $option;
+
+			// Check for MB function overload.
+			$option = new stdClass;
+			$option->label  = JText::_('COM_JOOMLAUPDATE_INSTL_MB_STRING_OVERLOAD_OFF');
+			$option->state  = (ini_get('mbstring.func_overload') == (int)$xml->mbstring_func_overload);
+			$option->notice = ($option->state) ? null : JText::_('COM_JOOMLAUPDATE_INSTL_NOTICEMBSTRINGOVERLOAD');
+			$options[] = $option;
+		}
+
+		// Check for a missing native parse_ini_file implementation
+		$option = new stdClass;
+		$option->label  = JText::_('COM_JOOMLAUPDATE_INSTL_PARSE_INI_FILE_AVAILABLE');
+		$option->state  = $this->getIniParserAvailability();
+		$option->notice = null;
+		$options[] = $option;
+
+		// Check for missing native json_encode / json_decode support
+		$option = new stdClass;
+		$option->label  = JText::_('COM_JOOMLAUPDATE_INSTL_JSON_SUPPORT_AVAILABLE');
+		$option->state  = function_exists('json_encode') && function_exists('json_decode');
+		$option->notice = null;
+		$options[] = $option;
+
+		// Check for configuration file writeable.
+		$option = new stdClass;
+		$option->label  = JText::sprintf('COM_JOOMLAUPDATE_INSTL_WRITABLE', 'configuration.php');
+		$option->state  = (is_writable('../configuration.php') || (!file_exists('../configuration.php') && is_writable('../')));
+		$option->notice = ($option->state) ? null : JText::_('COM_JOOMLAUPDATE_INSTL_NOTICEYOUCANSTILLINSTALL');
+		$options[] = $option;
+
+		return $options;
+	}
+	
+	/**
+	 * Gets PHP Settings.
+	 *
+	 * @return  array
+	 *
+	 * @since   3.0
+	 */
+	public function getPhpSettings(SimpleXMLElement $xml = null)
+	{
+		$settings = array();
+
+		// Check for safe mode.
+		$setting = new stdClass;
+		$setting->label = JText::_('COM_JOOMLAUPDATE_INSTL_SAFE_MODE');
+		$setting->state = (bool) ini_get('safe_mode');
+		$setting->recommended = false;
+		$settings[] = $setting;
+
+		// Check for display errors.
+		$setting = new stdClass;
+		$setting->label = JText::_('COM_JOOMLAUPDATE_INSTL_DISPLAY_ERRORS');
+		$setting->state = (bool) ini_get('display_errors');
+		$setting->recommended = false;
+		$settings[] = $setting;
+
+		// Check for file uploads.
+		$setting = new stdClass;
+		$setting->label = JText::_('COM_JOOMLAUPDATE_INSTL_FILE_UPLOADS');
+		$setting->state = (bool) ini_get('file_uploads');
+		$setting->recommended = true;
+		$settings[] = $setting;
+
+		// Check for magic quotes runtimes.
+		$setting = new stdClass;
+		$setting->label = JText::_('COM_JOOMLAUPDATE_INSTL_MAGIC_QUOTES_RUNTIME');
+		$setting->state = (bool) ini_get('magic_quotes_runtime');
+		$setting->recommended = false;
+		$settings[] = $setting;
+
+		// Check for output buffering.
+		$setting = new stdClass;
+		$setting->label = JText::_('COM_JOOMLAUPDATE_INSTL_OUTPUT_BUFFERING');
+		$setting->state = (bool) ini_get('output_buffering');
+		$setting->recommended = false;
+		$settings[] = $setting;
+
+		// Check for session auto-start.
+		$setting = new stdClass;
+		$setting->label = JText::_('COM_JOOMLAUPDATE_INSTL_SESSION_AUTO_START');
+		$setting->state = (bool) ini_get('session.auto_start');
+		$setting->recommended = false;
+		$settings[] = $setting;
+
+		// Check for native ZIP support
+		$setting = new stdClass;
+		$setting->label = JText::_('COM_JOOMLAUPDATE_INSTL_ZIP_SUPPORT_AVAILABLE');
+		$setting->state = function_exists('zip_open') && function_exists('zip_read');
+		$setting->recommended = true;
+		$settings[] = $setting;
+
+		return $settings;
+	}
+	
+	/**
+	 * Checks the availability of the parse_ini_file and parse_ini_string functions.
+	 *
+	 * @return	boolean
+	 * @since	1.6
+	 */
+	public function getIniParserAvailability()
+	{
+		$disabled_functions = ini_get('disable_functions');
+
+		if (!empty($disabled_functions)) {
+			// Attempt to detect them in the disable_functions black list
+			$disabled_functions = explode(',', trim($disabled_functions));
+			$number_of_disabled_functions = count($disabled_functions);
+
+			for ($i = 0; $i < $number_of_disabled_functions; $i++)
+			{
+				$disabled_functions[$i] = trim($disabled_functions[$i]);
+			}
+
+			if (phpversion() >= '5.3.0') {
+				$result = !in_array('parse_ini_string', $disabled_functions);
+			} else {
+				$result = !in_array('parse_ini_file', $disabled_functions);
+			}
+		} else {
+
+			// Attempt to detect their existence; even pure PHP implementation of them will trigger a positive response, though.
+			if (phpversion() >= '5.3.0') {
+				$result = function_exists('parse_ini_string');
+			} else {
+				$result = function_exists('parse_ini_file');
+			}
+		}
+
+		return $result;
+	}	
 }
