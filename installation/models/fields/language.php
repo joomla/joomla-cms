01662959ad190e4380e84f3c8338c4b5002cff83
Jean-Marie Simonet <infografjms@gmail.com>
2012-12-21 07:28:45 +0100

    $^ Porting installation language list alpha ordering from 3.0

--- installation/models/fields/language.php
+++ installation/models/fields/language.php
@@ -54,10 +54,16 @@ class JFormFieldLanguage extends JFormFieldList
 			$native = $session['language'];
 		}
 
-		// Get the list of available languages.
+			// Get the list of available languages.
 		$options = JLanguageHelper::createLanguageList($native);
-		if (!$options || $options  instanceof Exception) {
+		if (!$options || $options  instanceof Exception)
+		{
 			$options = array();
+		}
+		// Sort languages by name
+		else
+		{
+			usort($options, array($this, '_sortLanguages'));
 		}
 
 		// Set the default value from the native language.
@@ -68,4 +74,15 @@ class JFormFieldLanguage extends JFormFieldList
 
 		return $options;
 	}
+
+	/**
+	 * Method to sort languages by name.
+	 *
+	 * @ported from	3.0
+	 */
+	protected function _sortLanguages($a, $b)
+	{
+		return strcmp($a['text'], $b['text']);
+	}
+
 }
