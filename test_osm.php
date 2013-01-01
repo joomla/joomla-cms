08ef2d4c811019f8ae3e8bf5d370a9acdd1d881c
Buddhima <buddhimawijeweera@yahoo.com>
2013-01-01 05:44:51 +0530

    Modified the test_osm

--- test_osm.php
+++ test_osm.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ * An example JOpenstreetmap application built on the Joomla Platform.
+ *
+ * To run this example, copy or soft-link this folder to your web server tree.
+ *
+ * @package    Joomla.Examples
+ * @copyright  Copyright (C) 2005 - 2012 Open Source Matters, Inc. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE
+ */
+
 // We are a valid Joomla entry point.
 define('_JEXEC', 1);
 
@@ -10,98 +20,81 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 // Bootstrap the application.
-require dirname(__FILE__).'/tests/bootstrap.php';
+require dirname(__FILE__) . '/tests/bootstrap.php';
 
+/**
+ * An example JApplicationWeb application class.
+ *
+ * @package  Joomla.Examples
+ * @since    12.3
+ */
 class OsmApp extends JApplicationWeb
 {
 	/**
 	 * Display the application.
+	 * 
+	 * @return void
 	 */
-	function doExecute(){
+	function doExecute()
+	{
+
+		// Provide the key and secret obtained from Open Street Map account
 		$key = "8DVVqjUxRiruXsMYZegClYbWODIMoKZxLl8w9pXR";
 		$secret = "HvZkpFOsyq9oD7GEHYASyRgzKasRAMsvQ53Qb483";
-// 		$key = "9o2s3leTWNqLhg4NvBguBR2fT4M8Q7nYSORwRP0W";
-// 		$secret = "ZJrLaPyu3gANBDVH5v2x5Heiw04t4cybquFvNTSb";
 
+		// Create a new Jregistry to store key and secret
 		$option = new JRegistry;
 		$option->set('consumer_key', $key);
 		$option->set('consumer_secret', $secret);
 		$option->set('sendheaders', true);
 
+		// Oauth authentication purposes
 		$oauth = new JOpenstreetmapOauth($option);
 
-		
-		//$access_token = array('key' => '617544537-uMhDHjkCPGbgsb8NASkyWOfQj6wkIGWNjtZOIxDX', 'secret' => 'x9VpWp0tGK7q7lIlTyij7c0kfpRKWEWNJo2daPqHU8');
-		//$oauth->setToken($access_token);
-		
-	//	$new_token = $oauth->authenticate();
-		
-//  		$oauth->setToken($new_token);
-		
 		$oauth->authenticate();
- 		$osm=new JOpenstreetmap($oauth);
-// 		$osm=new JOpenstreetmap();
-		$changeset= $osm ->changesets;
- 		//$result = $changeset -> readChangeset($oauth, '14153877');
-		
-		//print_r($result);
-		//echo '<br />';
-		
-		$element=$osm->elements;
-		//$result= $element->createNode($oauth, '1', '34', '54', array("A"=>"Apple","B"=>"Ball"));
-		
-		$gps=$osm->gps;
-		
+
+		// Create a new base object with authentication
+
+		$osm = new JOpenstreetmap($oauth);
+
+		// A new base object without authentication
+		// $osm=new JOpenstreetmap();
+
+		// New changeset object obtained
+		$changeset = $osm ->changesets;
+
+		// New element object obtained
+		$element = $osm->elements;
+
+		// New gps object obtained
+		$gps = $osm->gps;
+
 		$changesets = array
 		(
-// 				array
-// 				(
-// 						"A"=>"Apple",
-// 						"B"=>"Ball",
-// 						"c"=>"Call"JTwitterPlaces
-// 				),
-// 				array
-// 				(
-// 						"B"=>"Ball"
-// 				),
-				array
-				(
-						"comment"=>"my changeset comment",
-						"created_by"=>"JOSM/1.0 (5581 en)"
-				),
-				array
-				(
-						"A"=>"Apple",
-						"F"=>"Apple",
-						"B"=>"Ball"
-				) 
+				"comment" => "my changeset comment",
+				"created_by" => "JOSM/1.0 (5581 en)"
 		);
-		
-// 		$result = $changeset ->createChangeset($changesets);
-// 		$result = $changeset ->updateChangeset($oauth, '14153877',array("C"=>"Cat","D"=>"Dogs"));
-		//$result = $changeset ->closeChangeset($oauth, '14153877');
-		//$result = $changeset -> readChangeset($oauth, '14153877');
-// 		$result=$element->createNode($oauth, '14153708', '5.5', '6.7', array("C"=>"Cat","D"=>"Dogs"));
-// 		print_r($result);
-		echo '$$$$<br />';
-//  		$result = $changeset -> readChangeset('10');
- 		print_r($result);
-		echo '<br />';
-// 		$result=$element->readElement($oauth, 'node', '2050021859');
- 		$result=$element->readElement('node', 123);
- 		print_r($result);
-		
-		//$result = $changeset ->updateChangeset($oauth, '1',$tags);
-		
-// 		print_r($result);
-// 		echo '<br />';
-		
-// 		$node_list=array(array(4,5),array(6,7));
-// 		$result = $changeset ->expandBBoxChangeset($oauth,'1',$node_list);
-// 		print_r('DDDDPPPPP');
-		//print_r($new_token);
-		$result =$gps->downloadTraceMetadetails('1370260','bswije','buddhima');
+
+		// Creates a new changeset
+		$result = $changeset ->createChangeset($changesets);
+
+		// Prints the number of newly created changeset
 		print_r($result);
+		echo '<br />';
+
+		// Reads the 'node' with id = 123
+		$result = $element->readElement('node', 123);
+
+		// Prints the detail about the node read
+		print_r($result);
+		echo '<br />';
+
+		// Download Metadetails of a trace
+		$result = $gps->downloadTraceMetadetails('1370260', 'username', 'password');
+
+		// Prints the downloaded metadetails
+		print_r($result);
+		echo '<br />';
 
 	}
 }
@@ -110,7 +103,9 @@ $web = JApplicationWeb::getInstance('OsmApp');
 JFactory::$application = $web;
 
 $session = JFactory::getSession();
-if($session->isActive() == false){
+
+if ($session->isActive() == false)
+{
 	$session->initialise(JFactory::getApplication()->input);
 	$session->start();
 }
