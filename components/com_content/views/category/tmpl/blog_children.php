3e54c054e7bf29d0f7666e4d7384ba3a8a4b2724
Jean-Marie Simonet <infografjms@gmail.com>
2013-01-17 12:06:55 +0100

    # [#29954] *RTL: Wrong display of Subcategories titles in Content Category Blog and Categories menu items

--- components/com_content/views/category/tmpl/blog_children.php
+++ components/com_content/views/category/tmpl/blog_children.php
@@ -11,6 +11,7 @@ defined('_JEXEC') or die;
 $class = ' class="first"';
 
 JHtml::_('bootstrap.tooltip');
+$lang	= JFactory::getLanguage();
 
 if (count($this->children[$this->category->id]) > 0 && $this->maxLevel != 0) : ?>
 
@@ -23,30 +24,44 @@ if (count($this->children[$this->category->id]) > 0 && $this->maxLevel != 0) : ?
 		?>
 		<div<?php echo $class; ?>>
 			<?php $class = ''; ?>
+			<?php if ($lang->isRTL()) : ?>
+			<h3 class="page-header item-title">
+				<?php if ( $this->params->get('show_cat_num_articles', 1)) : ?>
+					<span class="badge badge-info tip hasTooltip" title="<?php echo JText::_('COM_CONTENT_NUM_ITEMS'); ?>">
+						<?php echo $child->getNumItems(true); ?>
+					</span>
+				<?php endif; ?>
+				<a href="<?php echo JRoute::_(ContentHelperRoute::getCategoryRoute($child->id)); ?>">
+				<?php echo $this->escape($child->title); ?></a>
+
+				<?php if (count($child->getChildren()) > 0) : ?>
+					<a href="#category-<?php echo $child->id;?>" data-toggle="collapse" data-toggle="button" class="btn btn-mini pull-right"><span class="icon-plus"></span></a>
+				<?php endif;?>
+			<?php else : ?>
 			<h3 class="page-header item-title"><a href="<?php echo JRoute::_(ContentHelperRoute::getCategoryRoute($child->id));?>">
 				<?php echo $this->escape($child->title); ?></a>
 				<?php if ( $this->params->get('show_cat_num_articles', 1)) : ?>
-				<span class="badge badge-info tip hasTooltip" title="<?php echo JText::_('COM_CONTENT_NUM_ITEMS'); ?>">
-					<?php echo $child->getNumItems(true); ?>
-				</span>
+					<span class="badge badge-info tip hasTooltip" title="<?php echo JText::_('COM_CONTENT_NUM_ITEMS'); ?>">
+						<?php echo $child->getNumItems(true); ?>
+					</span>
 				<?php endif; ?>
 
 				<?php if (count($child->getChildren()) > 0) : ?>
-				<a href="#category-<?php echo $child->id;?>" data-toggle="collapse" data-toggle="button" class="btn btn-mini pull-right"><span class="icon-plus"></span></a>
+					<a href="#category-<?php echo $child->id;?>" data-toggle="collapse" data-toggle="button" class="btn btn-mini pull-right"><span class="icon-plus"></span></a>
+				<?php endif;?>
 			<?php endif;?>
-
 			</h3>
 
-			<?php if ($this->params->get('show_subcat_desc') == 1) :?>
+			<?php if ($this->params->get('show_subcat_desc') == 1) : ?>
 			<?php if ($child->description) : ?>
 				<div class="category-desc">
 					<?php echo JHtml::_('content.prepare', $child->description, '', 'com_content.category'); ?>
 				</div>
 			<?php endif; ?>
-            <?php endif; ?>
+			<?php endif; ?>
 
-			<?php if (count($child->getChildren()) > 0) :?>
-			<div class="collapse fade" id="category-<?php echo $child->id;?>">
+			<?php if (count($child->getChildren()) > 0) : ?>
+			<div class="collapse fade" id="category-<?php echo $child->id; ?>">
 				<?php
 				$this->children[$child->id] = $child->getChildren();
 				$this->category = $child;
