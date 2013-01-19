1af120e2d11ff3983fb187d7ea1066c407796651
Elin Waring <elin.waring@gmail.com>
2013-01-19 09:24:49 -0500

    Don't forget to add to index.

--- tests/plugins/content/example/example.php
+++ tests/plugins/content/example/example.php
@@ -140,27 +140,11 @@ class plgContentExample extends JPlugin
 	/**
 	 * Example after delete method.
 	 *
-<<<<<<< Upstream, based on master
-<<<<<<< Upstream, based on master
-	 * @param	string	The context for the content passed to the plugin.
-	 * @param	array	A list of primary key ids of the content that has changed state.
-	 * @param	int		The value of the state that the content has been changed to.
-	 * @return	boolean
-	 * @since	1.6
-=======
 	 * @param   string	The context for the content passed to the plugin.
 	 * @param   array	A list of primary key ids of the content that has changed state.
 	 * @param   integer  The value of the state that the content has been changed to.
 	 * @return  boolean
 	 * @since   1.6
->>>>>>> 9252ac1 Some more clean up plus the issues currently causing scans to fail.
-=======
-	 * @param	string	The context for the content passed to the plugin.
-	 * @param	array	A list of primary key ids of the content that has changed state.
-	 * @param	int		The value of the state that the content has been changed to.
-	 * @return	boolean
-	 * @since	1.6
->>>>>>> 92055c8 We're not touching /libraries/joomla or tests.
 	 */
 	public function onContentChangeState($context, $pks, $value)
 	{
@@ -172,27 +156,11 @@ class plgContentExample extends JPlugin
 	 *
 	 * Method is called by the view
 	 *
-<<<<<<< Upstream, based on master
-<<<<<<< Upstream, based on master
-	 * @param	string	The context of the content being passed to the plugin.
-	 * @param	object	The content object.  Note $article->text is also available
-	 * @param	object	The content params
-	 * @param	int		The 'page' number
-	 * @since	1.6
-=======
 	 * @param   string	The context of the content being passed to the plugin.
 	 * @param   object	The content object.  Note $article->text is also available
 	 * @param   object	The content params
 	 * @param   integer  The 'page' number
 	 * @since   1.6
->>>>>>> 9252ac1 Some more clean up plus the issues currently causing scans to fail.
-=======
-	 * @param	string	The context of the content being passed to the plugin.
-	 * @param	object	The content object.  Note $article->text is also available
-	 * @param	object	The content params
-	 * @param	int		The 'page' number
-	 * @since	1.6
->>>>>>> 92055c8 We're not touching /libraries/joomla or tests.
 	 */
 	public function onContentPrepare($context, &$article, &$params, $limitstart)
 	{
