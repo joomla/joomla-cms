a1e680ccf158ff8916926b249f46cb3c7d734d2d
Mark Dexter <dextercowley@gmail.com>
2013-02-03 15:50:43 -0800

    Update copyright to 2013

--- build/build.php
+++ build/build.php
@@ -18,7 +18,7 @@
  * 4. Check the archives in the tmp directory.
  *
  * @package		Joomla.Build
- * @copyright	Copyright (C) 2005 - 2012 Open Source Matters, Inc. All rights reserved.
+ * @copyright	Copyright (C) 2005 - 2013 Open Source Matters, Inc. All rights reserved.
  * @license		GNU General Public License version 2 or later; see LICENSE.txt
 
  */
