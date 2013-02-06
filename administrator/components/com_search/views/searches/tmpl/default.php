041d412da8b7f467bd541b11336f124e0936a22a
Jean-Marie Simonet <infografjms@gmail.com>
2013-02-06 12:00:21 +0100

    # [#29965] Add pagination in COM_SEARCH component

--- administrator/components/com_search/views/searches/tmpl/default.php
+++ administrator/components/com_search/views/searches/tmpl/default.php
@@ -27,6 +27,10 @@ $canDo		= SearchHelper::getActions();
 			<button class="btn hasTooltip" type="submit" title="<?php echo JText::_('JSEARCH_FILTER_SUBMIT'); ?>"><i class="icon-search"></i></button>
 			<button class="btn hasTooltip" type="button" title="<?php echo JText::_('JSEARCH_FILTER_CLEAR'); ?>" onclick="document.id('filter_search').value='';this.form.submit();"><i class="icon-remove"></i></button>
 		</div>
+		<div class="btn-group pull-right hidden-phone">
+			<label for="limit" class="element-invisible"><?php echo JText::_('JFIELD_PLG_SEARCH_SEARCHLIMIT_DESC');?></label>
+			<?php echo $this->pagination->getLimitBox(); ?>
+		</div>
 		<div class="filter-select btn-group pull-left">
 			<span class="adminlist-searchstatus">
 			<?php if ($this->state->get('filter.results')) : ?>
@@ -104,8 +108,6 @@ $canDo		= SearchHelper::getActions();
 			<?php endforeach; ?>
 			</tbody>
 		</table>
-
-
 	<div>
 		<input type="hidden" name="task" value="" />
 		<input type="hidden" name="boxchecked" value="0" />
