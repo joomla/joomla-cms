f40ecfb65771e2f08cb38824d6b2d8ea14f46d6d
Elin Waring <elin.waring@gmail.com>
2013-03-03 06:55:25 -0500

    Use layouts in single category list views

--- components/com_content/views/category/tmpl/default.php
+++ components/com_content/views/category/tmpl/default.php
@@ -14,45 +14,13 @@ JHtml::addIncludePath(JPATH_COMPONENT.'/helpers');
 JHtml::_('behavior.caption');
 ?>
 <div class="category-list<?php echo $this->pageclass_sfx;?>">
+<?php
+$this->subtemplatename = 'articles';
+?>
 
-	<?php if ($this->params->get('show_page_heading')) : ?>
-	<h1>
-		<?php echo $this->escape($this->params->get('page_heading')); ?>
-	</h1>
-	<?php endif; ?>
+<?php
+	$layouttop  = new JLayoutFile('joomla.content.category_default');
+	echo $layouttop->render($this);
+?>
 
-	<?php if ($this->params->get('show_category_title', 1) or $this->params->get('page_subheading')) : ?>
-	<h2>
-		<?php echo $this->escape($this->params->get('page_subheading')); ?>
-		<?php if ($this->params->get('show_category_title')) : ?>
-			<span class="subheading-category"><?php echo $this->category->title;?></span>
-		<?php endif; ?>
-	</h2>
-	<?php endif; ?>
-
-	<?php if ($this->params->get('show_description', 1) || $this->params->def('show_description_image', 1)) : ?>
-	<div class="category-desc">
-		<?php if ($this->params->get('show_description_image') && $this->category->getParams()->get('image')) : ?>
-			<img src="<?php echo $this->category->getParams()->get('image'); ?>"/>
-		<?php endif; ?>
-		<?php if ($this->params->get('show_description') && $this->category->description) : ?>
-			<?php echo JHtml::_('content.prepare', $this->category->description, '', 'com_content.category'); ?>
-		<?php endif; ?>
-		<div class="clr"></div>
-	</div>
-	<?php endif; ?>
-
-	<div class="cat-items">
-		<?php echo $this->loadTemplate('articles'); ?>
-	</div>
-
-	<?php if (!empty($this->children[$this->category->id])&& $this->maxLevel != 0) : ?>
-	<div class="cat-children">
-		<h3>
-			<?php echo JTEXT::_('JGLOBAL_SUBCATEGORIES'); ?>
-		</h3>
-
-		<?php echo $this->loadTemplate('children'); ?>
-	</div>
-	<?php endif; ?>
 </div>
