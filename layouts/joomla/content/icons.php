14c3d89c6cd9d1d7566bda7ad47af6b098701839
Yannick Gaultier <shumisha@gmail.com>
2013-03-04 14:56:41 +0100

    Using JLayout to display icons on front-end content views

--- layouts/joomla/content/icons.php
+++ layouts/joomla/content/icons.php
@@ -9,35 +9,36 @@
 
 defined('JPATH_BASE') or die;
 
-$params  = $displayData->params;
-$canEdit = $params->get('access-edit');
+$canEdit = $displayData['params']->get('access-edit');
 
 ?>
 
 <div id="icons">
-	<?php if (empty($this->print)) : ?>
+	<?php if (empty($displayData['print'])) : ?>
 
-		<?php if ($canEdit || $params->get('show_print_icon') || $params->get('show_email_icon')) : ?>
+		<?php if ($canEdit || $displayData['params']->get('show_print_icon') || $displayData['params']->get('show_email_icon')) : ?>
 			<div class="btn-group pull-right">
-				<a class="btn dropdown-toggle" data-toggle="dropdown" href="#"> <i class="icon-cog"></i> <span class="caret"></span> </a>
+				<a class="btn dropdown-toggle" data-toggle="dropdown" href="#"> <span class="icon-cog"></span> <span class="caret"></span> </a>
 				<?php // Note the actions class is deprecated. Use dropdown-menu instead. ?>
-				<ul class="dropdown-menu actions">
-					<?php if ($params->get('show_print_icon')) : ?>
-						<li class="print-icon"> <?php echo JHtml::_('icon.print_popup', $displayData, $params); ?> </li>
+				<ul class="dropdown-menu">
+					<?php if ($displayData['params']->get('show_print_icon')) : ?>
+						<li class="print-icon"> <?php echo JHtml::_('icon.print_popup', $displayData['item'], $displayData['params']); ?> </li>
 					<?php endif; ?>
-					<?php if ($params->get('show_email_icon')) : ?>
-						<li class="email-icon"> <?php echo JHtml::_('icon.email', $displayData, $params); ?> </li>
+					<?php if ($displayData['params']->get('show_email_icon')) : ?>
+						<li class="email-icon"> <?php echo JHtml::_('icon.email', $displayData['item'], $displayData['params']); ?> </li>
 					<?php endif; ?>
 					<?php if ($canEdit) : ?>
-						<li class="edit-icon"> <?php echo JHtml::_('icon.edit', $displayData, $params); ?> </li>
+						<li class="edit-icon"> <?php echo JHtml::_('icon.edit', $displayData['item'], $displayData['params']); ?> </li>
 					<?php endif; ?>
 				</ul>
 			</div>
 		<?php endif; ?>
+
 	<?php else : ?>
+
 		<div class="pull-right">
-			<?php echo JHtml::_('icon.print_screen', $displayData, $params); ?>
+			<?php echo JHtml::_('icon.print_screen', $displayData['item'], $displayData['params']); ?>
 		</div>
+
 	<?php endif; ?>
 </div>
-
