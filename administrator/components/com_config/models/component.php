704d5bd8122071f1905d465e9431e1f7db5ba2b6
Nicholas Antimisiaris <nant@joomlapolis.com>
2013-03-05 09:39:40 +0100

    + [#29769] com_config component missing triggers on save. Thanks Nicholas Antimisiaris

--- administrator/components/com_config/models/component.php
+++ administrator/components/com_config/models/component.php
@@ -18,6 +18,22 @@ jimport('joomla.application.component.modelform');
 class ConfigModelComponent extends JModelForm
 {
 	/**
+	 * The event to trigger before saving the data.
+	 *
+	 * @var    string
+	 * @since  2.5.10
+	 */
+	protected $event_before_save = 'onConfigurationBeforeSave';
+
+	/**
+	 * The event to trigger before deleting the data.
+	 *
+	 * @var    string
+	 * @since  2.5.10
+	 */
+	protected $event_after_save = 'onConfigurationAfterSave';
+
+	/**
 	 * Method to auto-populate the model state.
 	 *
 	 * Note. Calling getState in this method will result in recursion.
@@ -68,7 +84,8 @@ class ConfigModelComponent extends JModelForm
 				'/config'
 			);
 
-		if (empty($form)) {
+		if (empty($form))
+		{
 			return false;
 		}
 
@@ -108,14 +125,18 @@ class ConfigModelComponent extends JModelForm
 	 */
 	public function save($data)
 	{
+		$dispatcher = JDispatcher::getInstance();
 		$table	= JTable::getInstance('extension');
+		$isNew = true;
 
 		// Save the rules.
-		if (isset($data['params']) && isset($data['params']['rules'])) {
+		if (isset($data['params']) && isset($data['params']['rules']))
+		{
 			$rules	= new JAccessRules($data['params']['rules']);
 			$asset	= JTable::getInstance('asset');
 
-			if (!$asset->loadByName($data['option'])) {
+			if (!$asset->loadByName($data['option']))
+			{
 				$root	= JTable::getInstance('asset');
 				$root->loadByName('root.1');
 				$asset->name = $data['option'];
@@ -124,7 +145,8 @@ class ConfigModelComponent extends JModelForm
 			}
 			$asset->rules = (string) $rules;
 
-			if (!$asset->check() || !$asset->store()) {
+			if (!$asset->check() || !$asset->store())
+			{
 				$this->setError($asset->getError());
 				return false;
 			}
@@ -135,7 +157,8 @@ class ConfigModelComponent extends JModelForm
 		}
 
 		// Load the previous Data
-		if (!$table->load($data['id'])) {
+		if (!$table->load($data['id']))
+		{
 			$this->setError($table->getError());
 			return false;
 		}
@@ -143,25 +166,40 @@ class ConfigModelComponent extends JModelForm
 		unset($data['id']);
 
 		// Bind the data.
-		if (!$table->bind($data)) {
+		if (!$table->bind($data))
+		{
 			$this->setError($table->getError());
 			return false;
 		}
 
 		// Check the data.
-		if (!$table->check()) {
+		if (!$table->check())
+		{
+			$this->setError($table->getError());
+			return false;
+		}
+
+		// Trigger the oonConfigurationBeforeSave event.
+		$result = $dispatcher->trigger($this->event_before_save, array($this->option . '.' . $this->name, $table, $isNew));
+
+		if (in_array(false, $result, true))
+		{
 			$this->setError($table->getError());
 			return false;
 		}
 
 		// Store the data.
-		if (!$table->store()) {
+		if (!$table->store())
+		{
 			$this->setError($table->getError());
 			return false;
 		}
 
 		// Clean the component cache.
 		$this->cleanCache('_system');
+
+		// Trigger the onConfigurationAfterSave event.
+		$dispatcher->trigger($this->event_after_save, array($this->option . '.' . $this->name, $table, $isNew));
 
 		return true;
 	}
