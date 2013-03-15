add917b3328fb2d130abd3f987776c139f474ab3
Roberto - phproberto <roberto@phproberto.com>
2013-03-15 07:23:02 +0100

    [imp] ajax tag field with on the fly tag creation

--- administrator/components/com_contact/views/contact/tmpl/edit.php
+++ administrator/components/com_contact/views/contact/tmpl/edit.php
@@ -10,10 +10,11 @@
 defined('_JEXEC') or die;
 
 // Include the component HTML helpers.
-JHtml::addIncludePath(JPATH_COMPONENT.'/helpers/html');
+JHtml::addIncludePath(JPATH_COMPONENT . '/helpers/html');
 JHtml::_('behavior.tooltip');
 JHtml::_('behavior.formvalidation');
 JHtml::_('formbehavior.chosen', 'select');
+JHtml::_('tag.ajaxfield', '#jform_tags');
 
 $app = JFactory::getApplication();
 $input = $app->input;
