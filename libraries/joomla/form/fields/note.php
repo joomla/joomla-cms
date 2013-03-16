77805586e86d26726564a5f291b90485b1b3f8ad
Peter van Westen <peter@nonumber.nl>
2013-03-16 01:31:54 +0100

    Added note form field

---
+++ libraries/joomla/form/fields/note.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * @package     Joomla.Platform
+ * @subpackage  Form
+ *
+ * @copyright   Copyright (C) 2005 - 2012 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+
+defined('JPATH_PLATFORM') or die;
+
+/**
+ * Form Field class for the Joomla Platform.
+ * Supports a one line text field.
+ *
+ * @package     Joomla.Platform
+ * @subpackage  Form
+ * @link        http://www.w3.org/TR/html-markup/input.text.html#input.text
+ * @since       11.1
+ */
+class JFormFieldNote extends JFormField
+{
+	/**
+	 * The form field type.
+	 *
+	 * @var    string
+	 *
+	 * @since  11.1
+	 */
+	protected $type = 'Note';
+
+	/**
+	 * Method to get the field label markup.
+	 *
+	 * @return  string  The field label markup.
+	 *
+	 * @since   11.1
+	 */
+	protected function getLabel()
+	{
+		if (empty($this->element['label']) && empty($this->element['description'])) {
+			return '';
+		}
+
+		$title = $this->element['label'] ? (string) $this->element['label'] : ($this->element['title'] ? (string) $this->element['title'] : '');
+		$description = (string) $this->element['description'];
+		$class = $this->element['class'] ? ' class="' . trim((string) $this->element['class']) . '"' : '';
+		$close = (string) $this->element['close'];
+
+		$html = array();
+		if ($close) {
+			$close = $close == 'true' ? 'alert' : $close;
+			$html[] = '<button type="button" class="close" data-dismiss="' . $close . '">&times;</button>';
+		}
+		$html[] = !empty($title) ? '<h4>' . $title . '</h4>' : '';
+		$html[] = $description;
+
+		return '<div ' . $class . '">' . implode('', $html) . '</div>';
+	}
+
+	/**
+	 * Method to get the field input markup.
+	 *
+	 * @return  string  The field input markup.
+	 *
+	 * @since   11.1
+	 */
+	protected function getInput()
+	{
+		return '';
+	}
+}
