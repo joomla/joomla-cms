03380d49db462b6699e1fcd85e3070e47cd43c75
Elin Waring <elin.waring@gmail.com>
2013-03-16 14:36:28 -0400

    default file

--- components/com_content/views/category/tmpl/default.php
+++ components/com_content/views/category/tmpl/default.php
@@ -14,6 +14,7 @@ JHtml::addIncludePath(JPATH_COMPONENT.'/helpers');
 JHtml::_('behavior.caption');
 ?>
 <div class="category-list<?php echo $this->pageclass_sfx;?>">
+
 <?php
 $this->subtemplatename = 'articles';
 echo JLayoutHelper::render('joomla.content.category_default', $this);
