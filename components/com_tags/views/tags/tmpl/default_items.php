ffa1405ba475d387a8ca53709a9a66d9086e07bb
David Hurley <david@sparkbuilt.com>
2013-03-21 11:56:13 -0400

    Updated to remove spaces

--- components/com_tags/views/tags/tmpl/default_items.php
+++ components/com_tags/views/tags/tmpl/default_items.php
@@ -9,7 +9,7 @@
 
 defined('_JEXEC') or die;
 
-JHtml::addIncludePath(JPATH_COMPONENT.'/helpers');
+JHtml::addIncludePath(JPATH_COMPONENT . '/helpers');
 JHtml::_('behavior.tooltip');
 JHtml::_('behavior.framework');
 
@@ -22,22 +22,24 @@ $canCreate = $user->authorise('core.create', 'com_tags');
 $canEditState = $user->authorise('core.edit.state', 'com_tags');
 
 $columns = $this->params->get('tag_columns', 1);
+
 // Avoid division by 0 and negative columns.
 if ($columns < 1)
 {
-  $columns = 1;
+	$columns = 1;
 }
+
 $bsspans = floor(12 / $columns);
+
 if ($bsspans < 1)
 {
-  $bsspans = 1;
+	$bsspans = 1;
 }
 
 $bscolumns = min($columns, floor(12 / $bsspans));
 
 $n = count($this->items);
 ?>
-
 <?php if ($this->items == false || $n == 0) : ?>
 	<p> <?php echo JText::_('COM_TAGS_NO_TAGS'); ?></p>
 <?php else : ?>
@@ -52,30 +54,29 @@ $n = count($this->items);
                   				<?php if (!empty($images->image_intro)): ?>
                     				<div class="item-image"> <img
                     				<?php if ($images->image_intro_caption):
-                      					echo ' title="' . htmlspecialchars($images->image_intro_caption) . '"';
-                    				endif; ?>
-                  					src="<?php echo $images->image_intro; ?>" alt="<?php echo htmlspecialchars($images->image_fulltext_alt); ?>"/> </div>
-                  				<?php endif; ?>
+										echo ' title="' . htmlspecialchars($images->image_intro_caption) . '"';
+									endif; ?>
+									src="<?php echo $images->image_intro; ?>" alt="<?php echo htmlspecialchars($images->image_fulltext_alt); ?>"/> </div>
+								<?php endif; ?>
 							<?php endif; ?>
 						<div class="caption">
-		                <?php  echo '<h3><a href="' . JRoute::_(TagsHelperRoute::getTagRoute($item->id . ':' . $item->alias)) . '">'
-		                    . $this->escape($item->title) . '</a> </h3>';  ?>
-		                <?php  if ($this->params->get('all_tags_show_tag_description', 1)) : ?>
-		                  <span class="tag-body">
-		                    <?php echo JHtmlString::truncate($item->description, $this->params->get('tag_list_item_maximum_characters')); ?>
-		                  </span>
-		                <?php endif; ?>
-		                <?php  if ($this->params->get('all_tags_show_tag_hits')) : ?>
-		                    <span class="list-hits badge badge-info">
-		                      <?php echo JText::sprintf('JGLOBAL_HITS_COUNT', $item->hits); ?>
-		                    </span>
-		                <?php endif; ?>
-		              </div>
-		            </li>
-		          <?php endif;?>
-
+						<?php  echo '<h3><a href="' . JRoute::_(TagsHelperRoute::getTagRoute($item->id . ':' . $item->alias)) . '">'
+								. $this->escape($item->title) . '</a> </h3>';  ?>
+						<?php  if ($this->params->get('all_tags_show_tag_description', 1)) : ?>
+							<span class="tag-body">
+								<?php echo JHtmlString::truncate($item->description, $this->params->get('tag_list_item_maximum_characters')); ?>
+							</span>
+						<?php endif; ?>
+						<?php  if ($this->params->get('all_tags_show_tag_hits')) : ?>
+							<span class="list-hits badge badge-info">
+								<?php echo JText::sprintf('JGLOBAL_HITS_COUNT', $item->hits); ?>
+							</span>
+						<?php endif; ?>
+						</div>
+					</li>
+				<?php endif;?>
 		        <?php if (($i == 0 && $n == 1) || $i == $n - 1 || $bscolumns == 1 || (($i + 1) % $bscolumns == 0)) :  ?>
-		          </ul>
+						</ul>
 		        <?php endif; ?>
 		<?php endforeach; ?>
-	<?php  endif;?>
+	<?php endif;	
