f8899084a81887d5ea0e6a976d3dd4566470a57e
Elin Waring <elin.waring@gmail.com>
2013-03-23 15:08:46 +0100

    # [#30348] Popular Tags Module showing multiple copies of links. Thanks Elin

--- modules/mod_tags_popular/helper.php
+++ modules/mod_tags_popular/helper.php
@@ -28,10 +28,29 @@ abstract class ModTagsPopularHelper
 
 		$query = $db->getQuery(true);
 
-		$query->select(array($db->quoteName('tag_id'), $db->quoteName('type_alias'), $db->quoteName('content_item_id'), ' COUNT(*) AS count', 't.title', 't.access', 't.alias'));
-		$query->group($db->quoteName(array('tag_id', 'type_alias', 'content_item_id', 't.title', 't.access', 't.alias')));
+		$query->select(array('MAX(' . $db->quoteName('tag_id') . ') AS tag_id',
+					' COUNT(*) AS count', 'MAX(' . $db->quoteName('t.title') . ') AS title',
+					'MAX(' .$db->quoteName('t.access') . ') AS access',
+					'MAX(' .$db->quoteName('t.alias') . ') AS alias')
+				);
+		$query->group($db->quoteName(array('tag_id', 'title', 'access', 'alias')));
 		$query->from($db->quoteName('#__contentitem_tag_map'));
-		$query->where('t.access IN (' . $groups . ')');
+		$query->where($db->quoteName('t.access') . ' IN (' . $groups . ')');
+
+		// Only return published tags
+		$query->where($db->quoteName('t.published') . ' = 1 ');
+
+		// Optionally filter on language
+		$language = JComponentHelper::getParams('com_tags')->get('tag_list_language_filter', 'all');
+
+		if ($language != 'all')
+		{
+			if ($language == 'current_language')
+			{
+				$language = JHelperContent::getCurrentLanguage();
+			}
+			$query->where($db->qn('t.language') . ' IN (' . $db->q($language) . ', ' . $db->q('*') . ')');
+		}
 
 		if ($timeframe != 'alltime')
 		{
@@ -39,7 +58,7 @@ abstract class ModTagsPopularHelper
 			$query->where($db->quoteName('tag_date') . ' > ' . $query->dateAdd($now->toSql('date'), '-1', strtoupper($timeframe)));
 		}
 
-		$query->join('LEFT', '#__tags AS t ON tag_id=t.id');
+		$query->join('INNER', $db->quoteName('#__tags', 't') . ' ON ' . $db->quoteName('tag_id') . ' = ' . $db->quoteName('t.id'));
 		$query->order('count DESC');
 		$db->setQuery($query, 0, $maximum);
 		$results = $db->loadObjectList();
