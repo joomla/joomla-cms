e0b43a4f00afc752a1b67798193e510e50b8db57
Elin Waring <elin.waring@gmail.com>
2013-03-25 01:35:58 -0400

    Just fix the one issue. Remover commented code.

--- modules/mod_weblinks/helper.php
+++ modules/mod_weblinks/helper.php
@@ -79,32 +79,16 @@ class ModWeblinksHelper
 
 		$items = $model->getItems();
 
-		/*
-		 * This was in the previous code before we changed over to using the
-		 * weblinkscategory model but I don't see any models using checked_out filters
-		 * in their getListQuery() methods so I believe we should not be adding this now
-		 */
-
-		/*
-		 $query->where('(a.checked_out = 0 OR a.checked_out = '.$user->id.')');
-		 */
-		if ($items)
+		for ($i = 0, $count = count($items); $i < $count; $i++)
 		{
-			for ($i = 0, $count = count($items); $i < $count; $i++)
+			$item = &$items[$i];
+			if ($item->params->get('count_clicks', $params->get('count_clicks')) == 1)
 			{
-				$item = &$items[$i];
-				if ($item->params->get('count_clicks', $params->get('count_clicks')) == 1)
-				{
-					$item->link	= JRoute::_('index.php?option=com_weblinks&task=weblink.go&catid='.$item->catslug.'&id='. $item->slug);
-				} else {
-					$item->link = $item->url;
-				}
+				$item->link	= JRoute::_('index.php?option=com_weblinks&task=weblink.go&catid='.$item->catslug.'&id='. $item->slug);
+			} else {
+				$item->link = $item->url;
 			}
-			return $items;
 		}
-		else
-		{
-			return;
-		}
+		return $items;
 	}
 }
