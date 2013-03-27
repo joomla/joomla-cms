ba462b0d4dde95e49c192bf992fe53a25f8a6d42
Andrew Eddie <mamboblue@gmail.com>
2013-03-28 08:13:37 +1000

    Fix code style issues.

--- libraries/joomla/input/cli.php
+++ libraries/joomla/input/cli.php
@@ -116,87 +116,87 @@ class JInputCLI extends JInput
 	 *
 	 * @since   11.1
 	 */
-    protected function parseArguments()
-    {
-        $argv                           = $_SERVER['argv'];
+	protected function parseArguments()
+	{
+		$argv = $_SERVER['argv'];
 
-        $this->executable = array_shift($argv);
+		$this->executable = array_shift($argv);
 
-        $out                            = array();
+		$out = array();
 
-        for ($i = 0, $j = count($argv); $i < $j; $i++)
-        {
-            $arg                        = $argv[$i];
+		for ($i = 0, $j = count($argv); $i < $j; $i++)
+		{
+			$arg = $argv[$i];
 
-            // --foo --bar=baz
-            if (substr($arg, 0, 2) === '--')
-            {
-                $eqPos                  = strpos($arg, '=');
+			// --foo --bar=baz
+			if (substr($arg, 0, 2) === '--')
+			{
+				$eqPos = strpos($arg, '=');
 
-                // --foo
-                if ($eqPos === false)
-                {
-                    $key                = substr($arg, 2);
+				// --foo
+				if ($eqPos === false)
+				{
+					$key = substr($arg, 2);
 
-                    // --foo value
-                    if ($i + 1 < $j && $argv[$i + 1][0] !== '-')
-                    {
-                        $value          = $argv[$i + 1];
-                        $i++;
-                    }
-                    else
-                    {
-                        $value          = isset($out[$key]) ? $out[$key] : true;
-                    }
-                    $out[$key]          = $value;
-                }
+					// --foo value
+					if ($i + 1 < $j && $argv[$i + 1][0] !== '-')
+					{
+						$value = $argv[$i + 1];
+						$i++;
+					}
+					else
+					{
+						$value = isset($out[$key]) ? $out[$key] : true;
+					}
 
-                // --bar=baz
-                else
-                {
-                    $key                = substr($arg, 2, $eqPos - 2);
-                    $value              = substr($arg, $eqPos + 1);
-                    $out[$key]          = $value;
-                }
-            }
+					$out[$key] = $value;
+				}
 
-            // -k=value -abc
-            else if (substr($arg, 0, 1) === '-')
-            {
-                // -k=value
-                if (substr($arg, 2, 1) === '=')
-                {
-                    $key                = substr($arg, 1, 1);
-                    $value              = substr($arg, 3);
-                    $out[$key]          = $value;
-                }
-                // -abc
-                else
-                {
-                    $chars              = str_split(substr($arg, 1));
-                    foreach ($chars as $char)
-                    {
-                        $key            = $char;
-                        $value          = isset($out[$key]) ? $out[$key] : true;
-                        $out[$key]      = $value;
-                    }
+				// --bar=baz
+				else
+				{
+					$key = substr($arg, 2, $eqPos - 2);
+					$value = substr($arg, $eqPos + 1);
+					$out[$key] = $value;
+				}
+			}
+			elseif (substr($arg, 0, 1) === '-')
+			// -k=value -abc
+			{
+				// -k=value
+				if (substr($arg, 2, 1) === '=')
+				{
+					$key = substr($arg, 1, 1);
+					$value = substr($arg, 3);
+					$out[$key] = $value;
+				}
+				else
+				// -abc
+				{
+					$chars = str_split(substr($arg, 1));
 
-                    // -a a-value
-                    if ((count($chars) === 1) && ($i + 1 < $j) && ($argv[$i + 1][0] !== '-'))
-                    {
-                        $out[$key]      = $argv[$i + 1];
-                        $i++;
-                    }
-                }
-            }
+					foreach ($chars as $char)
+					{
+						$key = $char;
+						$value = isset($out[$key]) ? $out[$key] : true;
+						$out[$key] = $value;
+					}
 
-            // plain-arg
-            else
-            {
-                $this->args[]           = $arg;
-            }
-        }
+					// -a a-value
+					if ((count($chars) === 1) && ($i + 1 < $j) && ($argv[$i + 1][0] !== '-'))
+					{
+						$out[$key] = $argv[$i + 1];
+						$i++;
+					}
+				}
+			}
+			else
+			{
+				// Plain-arg
+				$this->args[] = $arg;
+			}
+		}
 
-        $this->data                     = $out;
-    }
+		$this->data = $out;
+	}
 }
