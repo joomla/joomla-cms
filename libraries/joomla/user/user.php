4db79a5416b1e72ad7a31b7aec92dba2e960fe1e
wilsonge <georgejameswilson@googlemail.com>
2013-03-31 23:54:07 +0100

    Returns empty user if User not found

--- libraries/joomla/user/user.php
+++ libraries/joomla/user/user.php
@@ -242,9 +242,8 @@ class JUser extends JObject
 		{
 			if (!$id = JUserHelper::getUserId($identifier))
 			{
-				JLog::add(JText::sprintf('JLIB_USER_ERROR_ID_NOT_EXISTS', $identifier), JLog::WARNING, 'jerror');
-				$retval = false;
-				return $retval;
+				// If the $identifier doesn't match with any id, just return an empty JUser.
+				return new JUser;
 			}
 		}
 		else
