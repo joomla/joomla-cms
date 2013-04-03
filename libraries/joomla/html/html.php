dc138e087493a34714aa35a7566ca3071968deba
Achal-Aggarwal <theachalaggarwal@gmail.com>
2013-04-04 02:17:35 +0530

    Fix calender popup on enter
    [#30413] Calender popup problem in pages containing calendar fields
    http://joomlacode.org/gf/project/joomla/tracker/?action=TrackerItemEdit&tracker_item_id=30413
    
    When we press enter while a text field is in focus browser automatically submits the form. So to submit a form browser triggers click event on the first submit button present in dom hierarchy. So the first button is calender display button, because of that calender popup is coming. 
    
    There is no need to be calender display button to be submit button. So to make it of type="button" instead of type="submit" (by default id nothing is specified in button tag then it is a submit button) I added type="button" in button tag.

--- libraries/joomla/html/html.php
+++ libraries/joomla/html/html.php
@@ -912,7 +912,7 @@ abstract class JHtml
 			}
 			return '<div class="input-append"><input type="text" title="' . (0 !== (int) $value ? self::_('date', $value, null, null) : '')
 				. '" name="' . $name . '" id="' . $id . '" value="' . htmlspecialchars($value, ENT_COMPAT, 'UTF-8') . '" ' . $attribs . ' />'
-				. '<button class="btn" id="' . $id . '_img"><i class="icon-calendar"></i></button></div>';
+				. '<button type="button" class="btn" id="' . $id . '_img"><i class="icon-calendar"></i></button></div>';
 
 		}
 		else
