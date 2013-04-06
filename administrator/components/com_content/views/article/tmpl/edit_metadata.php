65f690590d1898abdcd1642128cde9cc99332f02
Elin Waring <elin.waring@gmail.com>
2013-04-06 11:18:32 -0400

    Fix layouts, add some jlayouts

--- administrator/components/com_content/views/article/tmpl/edit_metadata.php
+++ administrator/components/com_content/views/article/tmpl/edit_metadata.php
@@ -8,28 +8,6 @@
  */
 
 defined('_JEXEC') or die;
+
+echo JLayoutHelper::render('joomla.edit.metadata', $this);
 ?>
-<fieldset>
-<div class="control-group">
-	<?php echo $this->form->getLabel('metadesc'); ?>
-	<div class="controls">
-		<?php echo $this->form->getInput('metadesc'); ?>
-	</div>
-</div>
-<div class="control-group">
-	<?php echo $this->form->getLabel('metakey'); ?>
-	<div class="controls">
-		<?php echo $this->form->getInput('metakey'); ?>
-	</div>
-</div>
-<?php foreach ($this->form->getGroup('metadata') as $field) : ?>
-<div class="control-group">
-	<?php if (!$field->hidden) : ?>
-		<?php echo $field->label; ?>
-	<?php endif; ?>
-	<div class="controls">
-		<?php echo $field->input; ?>
-	</div>
-</div>
-<?php endforeach; ?>
-</fieldset>
