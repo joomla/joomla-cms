546d52e9bc84c3919e7b96636efc3ae7337ce5bf
Miguel Tuyare <migueltuyare@gmail.com>
2013-04-10 10:38:16 -0300

    [nueva] Plantilla de salida HTML para redes sociales

---
+++ components/com_content/views/article/tmpl/default_social.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @package		Jokte.Site
+ * @subpackage	com_content
+ * @copyright	Copyleft 2012 - 2013 Comunidad Juuntos.
+ * @license		GNU General Public License version 3
+ */
+
+// Acceso directo prohibido
+defined('_JEXEC') or die;
+
+// Atajo para los parámetros.
+$params = $this->item->params;
+$title = $this->item->title;
+$link = $this->item->readmore_link;
+?>
+<div style="clear:both"></div>
+<div class="socialButtons" style="text-align: center;float: <?php echo $params->get('align_socialbuttons') ?>; clear:right;">
+	<?php 
+	if ($params->get('show_facebook')) : 
+		echo JHtml::_('utiles.sbFacebook',$title,$link, $params); 
+	endif;
+	?>
+	<?php 
+	if ($params->get('show_twitter')) : 
+		echo JHtml::_('utiles.sbTwitter',$title,$link, $params); 
+	endif;
+	?>
+	<?php 
+	if ($params->get('show_one')) : 
+		echo JHtml::_('utiles.sbGooglePlus',$title,$link,$params); 
+	endif;
+	?>
+</div>
