84796d3f7b22fdc6dd028c434487dee8da55eb06
Miguel Tuyare <migueltuyare@gmail.com>
2013-04-10 10:25:48 -0300

    [ajuste] Por error en filtro multilenguaje y usuarios

--- components/com_contact/models/contact.php
+++ components/com_contact/models/contact.php
@@ -328,8 +328,8 @@ class ContactModelContact extends JModelForm
 				$query->where('a.access IN ('. $groups.')');
 				$query->order('a.state DESC, a.created DESC');
 				// filter per language if plugin published
-				if (JFactory::getApplication()->getLanguageFilter()) {
-					$query->where('a.language='.$db->quote(JFactory::getLanguage()->getTag()).' OR a.language='.$db->quote('*'));
+				if (JLanguageMultilang::isEnabled()) {
+					$query->where(('a.created_by = ' . (int) $result->user_id) AND ('a.language='.$db->quote(JFactory::getLanguage()->getTag()).' OR a.language='.$db->quote('*')));
 				}
 				if (is_numeric($published)) {
 					$query->where('a.state IN (1,2)');
