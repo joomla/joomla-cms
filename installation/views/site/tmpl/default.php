45f5a8eede24457b41794c78945685dbffad1a46
Miguel Tuyare <migueltuyare@gmail.com>
2013-04-10 10:23:00 -0300

    [instalador] Re tabulado de código en vista html

--- installation/views/site/tmpl/default.php
+++ installation/views/site/tmpl/default.php
@@ -12,28 +12,40 @@ $this->validateInput();
 <div id="installer">
 	<div class="m">
 		<form action="index.php" method="post" id="adminForm" class="form-validate" enctype="multipart/form-data">
-                    
+			<div id="step">
+				<div class="far-right">
+				<?php if ($this->document->direction == 'ltr') : ?>
+					<div class="button1-right">
+						<div class="prev">
+							<a href="index.php?view=filesystem" onclick="return Install.goToPage('database');" rel="prev" title="<?php echo JText::_('JPrevious'); ?>"><?php echo JText::_('JPrevious'); ?></a>
+						</div>
+					</div>
+					<div class="button1-left">
+						<div class="next"><a href="#" onclick="javascript:$('adminForm').submit();" rel="next" title="<?php echo JText::_('JNext'); ?>"><?php echo JText::_('JNext'); ?></a>
+					</div>
+				</div>
+				<?php elseif ($this->document->direction == 'rtl') : ?>
+				<div class="button1-right">
+					<div class="prev">
+						<a href="#" onclick="javascript:$('adminForm').submit();" rel="next" title="<?php echo JText::_('JNext'); ?>"><?php echo JText::_('JNext'); ?></a>
+					</div>
+				</div>
+				<div class="button1-left">
+					<div class="next"><a href="index.php?view=filesystem" onclick="return Install.goToPage('database');" rel="prev" title="<?php echo JText::_('JPrevious'); ?>"><?php echo JText::_('JPrevious'); ?></a>
+					</div>
+				</div>
+				<?php endif; ?>
+			</div>
+				<h2><?php echo JText::_('INSTL_SITE'); ?></h2>
+			</div>
 
-                    <div id="step">
-                            <div class="far-right">
-                    <?php if ($this->document->direction == 'ltr') : ?>
-                                    <div class="button1-right"><div class="prev"><a href="index.php?view=filesystem" onclick="return Install.goToPage('database');" rel="prev" title="<?php echo JText::_('JPrevious'); ?>"><?php echo JText::_('JPrevious'); ?></a></div></div>
-                                    <div class="button1-left"><div class="next"><a href="#" onclick="javascript:$('adminForm').submit();" rel="next" title="<?php echo JText::_('JNext'); ?>"><?php echo JText::_('JNext'); ?></a></div></div>
-                    <?php elseif ($this->document->direction == 'rtl') : ?>
-                                    <div class="button1-right"><div class="prev"><a href="#" onclick="javascript:$('adminForm').submit();" rel="next" title="<?php echo JText::_('JNext'); ?>"><?php echo JText::_('JNext'); ?></a></div></div>
-                                    <div class="button1-left"><div class="next"><a href="index.php?view=filesystem" onclick="return Install.goToPage('database');" rel="prev" title="<?php echo JText::_('JPrevious'); ?>"><?php echo JText::_('JPrevious'); ?></a></div></div>
-                    <?php endif; ?>
-                            </div>
-                            <h2><?php echo JText::_('INSTL_SITE'); ?></h2>
-                    </div>
-                    
-                    
 			<h3><?php echo JText::_('INSTL_SITE_NAME_TITLE'); ?></h3>
 			<div class="install-text">
 				<?php echo JText::_('INSTL_SITE_NAME_DESC'); ?>
 			</div>
 			<div class="install-body">
 				<div class="m">
+					
 					<h4 class="title-smenu" title="<?php echo JText::_('INSTL_BASIC_SETTINGS'); ?>">
 						<?php echo JText::_('INSTL_BASIC_SETTINGS'); ?>
 					</h4>
@@ -49,10 +61,7 @@ $this->validateInput();
 							</tr>
 						</table>
 					</div>
-                                    
-                                    
-                                    
-                                    <h4 class="title-smenu" title="<?php echo JText::_('INSTL_IMG_SITE'); ?>">
+					<h4 class="title-smenu" title="<?php echo JText::_('INSTL_IMG_SITE'); ?>">
 						<?php echo JText::_('INSTL_IMG_SITE_SETTINGS'); ?>
 					</h4>
 					<div class="section-smenu">
@@ -67,45 +76,42 @@ $this->validateInput();
 							</tr>
 						</table>
 					</div>
-                                    
-                                    
-
+					
 					<h4 class="title-smenu moofx-toggler" title="<?php echo JText::_('INSTL_SITE_META_ADVANCED_SETTINGS'); ?>">
 						<a href="#"><?php echo JText::_('INSTL_SITE_META_ADVANCED_SETTINGS'); ?></a>
 					</h4>
 					<div class="section-smenu moofx-slider">
-							<table class="content2">
-								<tr>
-									<td title="<?php echo JText::_('INSTL_SITE_METADESC_TITLE_LABEL'); ?>">
-										<?php echo $this->form->getLabel('site_metadesc'); ?>
-									</td>
-									<td>
-										<?php echo $this->form->getInput('site_metadesc'); ?>
-									</td>
-								</tr>
-								<tr>
-									<td title="<?php echo JText::_('INSTL_SITE_METAKEYS_TITLE_LABEL'); ?>">
-										<?php echo $this->form->getLabel('site_metakeys'); ?>
-									</td>
-									<td>
-										<?php echo $this->form->getInput('site_metakeys'); ?>
-									</td>
-								</tr>
-								<tr>
-									<td title="<?php echo JText::_('INSTL_SITE_OFFLINE_TITLE_LABEL'); ?>">
-										<?php echo $this->form->getLabel('site_offline'); ?>
-									</td>
-									<td>
-										<?php echo $this->form->getInput('site_offline'); ?>
-									</td>
-								</tr>
-							</table>
-						</div>
+						<table class="content2">
+							<tr>
+								<td title="<?php echo JText::_('INSTL_SITE_METADESC_TITLE_LABEL'); ?>">
+									<?php echo $this->form->getLabel('site_metadesc'); ?>
+								</td>
+								<td>
+									<?php echo $this->form->getInput('site_metadesc'); ?>
+								</td>
+							</tr>
+							<tr>
+								<td title="<?php echo JText::_('INSTL_SITE_METAKEYS_TITLE_LABEL'); ?>">
+									<?php echo $this->form->getLabel('site_metakeys'); ?>
+								</td>
+								<td>
+									<?php echo $this->form->getInput('site_metakeys'); ?>
+								</td>
+							</tr>
+							<tr>
+								<td title="<?php echo JText::_('INSTL_SITE_OFFLINE_TITLE_LABEL'); ?>">
+									<?php echo $this->form->getLabel('site_offline'); ?>
+								</td>
+								<td>
+									<?php echo $this->form->getInput('site_offline'); ?>
+								</td>
+							</tr>
+						</table>
 					</div>
 				</div>
-
-				<div class="newsection"></div>
-
+			</div>
+			
+			<div class="newsection"></div>
 				<h4><?php echo JText::_('INSTL_SITE_CONF_TITLE'); ?></h4>
 				<div class="install-text">
 					<?php echo JText::_('INSTL_SITE_CONF_DESC'); ?>
@@ -149,12 +155,11 @@ $this->validateInput();
 							</table>
 						</fieldset>
 					</div>
-                                        <input type="hidden" name="view" value="site" />
+					<input type="hidden" name="view" value="site" />
 					<?php echo JHtml::_('form.token'); ?>
 					<?php echo $this->form->getInput('sample_installed'); ?>
 				</div>
 			</form>
-
 			<div class="clr"></div>
 
 			<form enctype="multipart/form-data" action="index.php" method="post" id="filename">
@@ -198,7 +203,6 @@ $this->validateInput();
 					<?php echo JHtml::_('form.token'); ?>
 				</div>
 			</form>
-
 		<div class="clr"></div>
 	</div>
 </div>
