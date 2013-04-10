1dec2ad92abd98b96102ae0f6c03deb81e89a658
Miguel Tuyare <migueltuyare@gmail.com>
2013-04-10 09:26:45 -0300

    [modulo] Arreglo error strict en modulo Jokte

--- modules/mod_mjokte/helper.php
+++ modules/mod_mjokte/helper.php
@@ -4,7 +4,7 @@ defined( '_JEXEC' ) or die( 'Restricted access' );
 
 class modMJOKTE
 {	
-	function createOutput()
+	static function createOutput()
 	{
 		$output = '<a href="http://jokte.org" target="_blank" title="JOKTE! CMS 100% Libre" style="background:url('.JURI::root().'/modules/mod_mjokte/jokte.gif)
 		 no-repeat 50% 50%;display:block;width:150px;height:50px;text-decoration:none;margin: 0 auto; padding-top: 10px; padding-left: 0px; font-size: 10px; color: #000"></a>';
