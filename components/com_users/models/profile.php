0c5fa4391ae6f43d129f696efd69216090f1cc45
Miguel Tuyare <migueltuyare@gmail.com>
2013-04-10 10:28:30 -0300

    [ajuste] Por error en invocación parámetros del usuario

--- components/com_users/models/profile.php
+++ components/com_users/models/profile.php
@@ -236,8 +236,11 @@ class UsersModelProfile extends JModelForm
 		$data['email']		= $data['email1'];
 		$data['password']	= $data['password1'];
 
-		// Unset the username so it does not get overwritten
-		unset($data['username']);
+		// Unset the username if it should not be overwritten
+		if (!JComponentHelper::getParams('com_users')->get('change_login_name'))
+		{
+			unset($data['username']);
+		}
 
 		// Unset the block so it does not get overwritten
 		unset($data['block']);
