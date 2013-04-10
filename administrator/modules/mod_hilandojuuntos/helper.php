a784da26b48de549b864f62b0ab82b9e4e4d52ac
Miguel Tuyare <migueltuyare@gmail.com>
2013-04-10 10:55:50 -0300

    [ajuste] Cambios en módulo backend Hilando Juuntos por soporte nueva versión de la librería Simplepie

--- administrator/modules/mod_hilandojuuntos/helper.php
+++ administrator/modules/mod_hilandojuuntos/helper.php
@@ -60,15 +60,14 @@ abstract class modHilandojuuntosHelper
 		$rssitems			= $params->get('rssitems', 5);
 		$filter = JFilterInput::getInstance();
 		
-		$options = array();
-		$options['rssUrl']		= $rssurl;
+		// get RSS parsed object
+		$cache_time = 0;
 		if ($params->get('cache')) {
-			$options['cache_time']  = $params->get('cache_time', 15) ;
-			$options['cache_time']	*= 60;
-		} else {
-			$options['cache_time'] = null;
+			$cache_time  = $params->get('cache_time', 15) * 60;
 		}
-		$rssDoc = JFactory::getXMLParser('RSS', $options);
+
+		$rssDoc = JFactory::getFeedParser($rssurl, $cache_time);
+
 
 		if ($rssDoc != false)
 		{
