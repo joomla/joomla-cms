2438a98e4cca6f36dbab6f670e9f51021153afb8
Miguel Tuyare <migueltuyare@gmail.com>
2013-04-10 11:06:34 -0300

    [ajuste] Salida HTML de iconos según mime

--- administrator/components/com_media/views/imageslist/tmpl/default_document.php
+++ administrator/components/com_media/views/imageslist/tmpl/default_document.php
@@ -14,9 +14,9 @@ $dispatcher->trigger('onContentBeforeDisplay', array('com_media.file', &$this->_
 ?>
 		<div class="item">
 			<a href="javascript:ImageManager.populateFields('<?php echo $this->_tmp_doc->path_relative; ?>')" title="<?php echo $this->_tmp_doc->name; ?>" >
-				<?php echo JHtml::_('image', $this->baseURL.'/'.$this->_tmp_doc->icon_32, JText::sprintf('COM_MEDIA_IMAGE_TITLE', $this->_tmp_doc->title, MediaHelper::parseSize($this->_tmp_doc->size)), array('width' => $this->_tmp_doc->width_60, 'height' => $this->_tmp_doc->height_60)); ?>
+				<?php echo JHtml::_('image', $this->_tmp_doc->icon_32, JText::sprintf('COM_MEDIA_IMAGE_TITLE', $this->_tmp_doc->title, MediaHelper::parseSize($this->_tmp_doc->size)), array('height' => 32, 'width' => 32), true); ?>
 				<span title="<?php echo $this->_tmp_doc->name; ?>"><?php echo $this->_tmp_doc->title; ?></span></a>
 		</div>
 <?php
 $dispatcher->trigger('onContentAfterDisplay', array('com_media.file', &$this->_tmp_doc, &$params));
-?>
+?> 
