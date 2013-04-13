efc4ab940df0b1fa7f3321cfb04f609824e41303
Best Quality Web Products <novinfard@gmail.com>
2013-04-13 11:22:57 +0430

    Update helper.php
    capability of override Joomla! core module helpers
    
    http://joomlacode.org/gf/project/joomla/tracker/?action=TrackerItemEdit&tracker_id=8103&tracker_item_id=28907

--- modules/mod_articles_latest/helper.php
+++ modules/mod_articles_latest/helper.php
@@ -19,102 +19,105 @@ JModelLegacy::addIncludePath(JPATH_SITE . '/components/com_content/models', 'Con
  * @package     Joomla.Site
  * @subpackage  mod_articles_latest
  */
-abstract class ModArticlesLatestHelper
+if(!class_exists('modArticlesLatestHelper'))
 {
-	public static function getList(&$params)
+	abstract class ModArticlesLatestHelper
 	{
-		// Get the dbo
-		$db = JFactory::getDbo();
-
-		// Get an instance of the generic articles model
-		$model = JModelLegacy::getInstance('Articles', 'ContentModel', array('ignore_request' => true));
-
-		// Set application parameters in model
-		$app = JFactory::getApplication();
-		$appParams = $app->getParams();
-		$model->setState('params', $appParams);
-
-		// Set the filters based on the module params
-		$model->setState('list.start', 0);
-		$model->setState('list.limit', (int) $params->get('count', 5));
-		$model->setState('filter.published', 1);
-
-		// Access filter
-		$access = !JComponentHelper::getParams('com_content')->get('show_noauth');
-		$authorised = JAccess::getAuthorisedViewLevels(JFactory::getUser()->get('id'));
-		$model->setState('filter.access', $access);
-
-		// Category filter
-		$model->setState('filter.category_id', $params->get('catid', array()));
-
-		// User filter
-		$userId = JFactory::getUser()->get('id');
-		switch ($params->get('user_id'))
+		public static function getList(&$params)
 		{
-			case 'by_me':
-				$model->setState('filter.author_id', (int) $userId);
-				break;
-			case 'not_me':
-				$model->setState('filter.author_id', $userId);
-				$model->setState('filter.author_id.include', false);
-				break;
-
-			case '0':
-				break;
-
-			default:
-				$model->setState('filter.author_id', (int) $params->get('user_id'));
-				break;
-		}
-
-		// Filter by language
-		$model->setState('filter.language', $app->getLanguageFilter());
-
-		//  Featured switch
-		switch ($params->get('show_featured'))
-		{
-			case '1':
-				$model->setState('filter.featured', 'only');
-				break;
-			case '0':
-				$model->setState('filter.featured', 'hide');
-				break;
-			default:
-				$model->setState('filter.featured', 'show');
-				break;
-		}
-
-		// Set ordering
-		$order_map = array(
-			'm_dsc' => 'a.modified DESC, a.created',
-			'mc_dsc' => 'CASE WHEN (a.modified = ' . $db->quote($db->getNullDate()) . ') THEN a.created ELSE a.modified END',
-			'c_dsc' => 'a.created',
-			'p_dsc' => 'a.publish_up',
-		);
-		$ordering = JArrayHelper::getValue($order_map, $params->get('ordering'), 'a.publish_up');
-		$dir = 'DESC';
-
-		$model->setState('list.ordering', $ordering);
-		$model->setState('list.direction', $dir);
-
-		$items = $model->getItems();
-
-		foreach ($items as &$item)
-		{
-			$item->slug = $item->id . ':' . $item->alias;
-			$item->catslug = $item->catid . ':' . $item->category_alias;
-
-			if ($access || in_array($item->access, $authorised))
+			// Get the dbo
+			$db = JFactory::getDbo();
+	
+			// Get an instance of the generic articles model
+			$model = JModelLegacy::getInstance('Articles', 'ContentModel', array('ignore_request' => true));
+	
+			// Set application parameters in model
+			$app = JFactory::getApplication();
+			$appParams = $app->getParams();
+			$model->setState('params', $appParams);
+	
+			// Set the filters based on the module params
+			$model->setState('list.start', 0);
+			$model->setState('list.limit', (int) $params->get('count', 5));
+			$model->setState('filter.published', 1);
+	
+			// Access filter
+			$access = !JComponentHelper::getParams('com_content')->get('show_noauth');
+			$authorised = JAccess::getAuthorisedViewLevels(JFactory::getUser()->get('id'));
+			$model->setState('filter.access', $access);
+	
+			// Category filter
+			$model->setState('filter.category_id', $params->get('catid', array()));
+	
+			// User filter
+			$userId = JFactory::getUser()->get('id');
+			switch ($params->get('user_id'))
 			{
-				// We know that user has the privilege to view the article
-				$item->link = JRoute::_(ContentHelperRoute::getArticleRoute($item->slug, $item->catslug));
+				case 'by_me':
+					$model->setState('filter.author_id', (int) $userId);
+					break;
+				case 'not_me':
+					$model->setState('filter.author_id', $userId);
+					$model->setState('filter.author_id.include', false);
+					break;
+	
+				case '0':
+					break;
+	
+				default:
+					$model->setState('filter.author_id', (int) $params->get('user_id'));
+					break;
 			}
-			else
+	
+			// Filter by language
+			$model->setState('filter.language', $app->getLanguageFilter());
+	
+			//  Featured switch
+			switch ($params->get('show_featured'))
 			{
-				$item->link = JRoute::_('index.php?option=com_users&view=login');
+				case '1':
+					$model->setState('filter.featured', 'only');
+					break;
+				case '0':
+					$model->setState('filter.featured', 'hide');
+					break;
+				default:
+					$model->setState('filter.featured', 'show');
+					break;
 			}
+	
+			// Set ordering
+			$order_map = array(
+				'm_dsc' => 'a.modified DESC, a.created',
+				'mc_dsc' => 'CASE WHEN (a.modified = ' . $db->quote($db->getNullDate()) . ') THEN a.created ELSE a.modified END',
+				'c_dsc' => 'a.created',
+				'p_dsc' => 'a.publish_up',
+			);
+			$ordering = JArrayHelper::getValue($order_map, $params->get('ordering'), 'a.publish_up');
+			$dir = 'DESC';
+	
+			$model->setState('list.ordering', $ordering);
+			$model->setState('list.direction', $dir);
+	
+			$items = $model->getItems();
+	
+			foreach ($items as &$item)
+			{
+				$item->slug = $item->id . ':' . $item->alias;
+				$item->catslug = $item->catid . ':' . $item->category_alias;
+	
+				if ($access || in_array($item->access, $authorised))
+				{
+					// We know that user has the privilege to view the article
+					$item->link = JRoute::_(ContentHelperRoute::getArticleRoute($item->slug, $item->catslug));
+				}
+				else
+				{
+					$item->link = JRoute::_('index.php?option=com_users&view=login');
+				}
+			}
+	
+			return $items;
 		}
-
-		return $items;
 	}
 }
