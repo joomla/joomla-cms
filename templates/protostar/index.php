2e428b23df33b664b9d6610d90181a4b0d9d62e2
Seth <seth@internet-inspired.com>
2013-04-23 09:15:40 +0100

    Accessibility improvements
    Adds html5 elements, WAI_ARIA Landmark roles and anchor titles
    
    Signed-off-by: Seth <seth@internet-inspired.com>

--- templates/protostar/index.php
+++ templates/protostar/index.php
@@ -71,11 +71,11 @@ if ($this->params->get('logoFile'))
 }
 elseif ($this->params->get('sitetitle'))
 {
-	$logo = '<span class="site-title" title="'. $sitename .'">'. htmlspecialchars($this->params->get('sitetitle')) .'</span>';
+	$logo = '<span class="site-title">'. htmlspecialchars($this->params->get('sitetitle')) .'</span>';
 }
 else
 {
-	$logo = '<span class="site-title" title="'. $sitename .'">'. $sitename .'</span>';
+	$logo = '<span class="site-title">'. $sitename .'</span>';
 }
 ?>
 <!DOCTYPE html>
@@ -144,20 +144,20 @@ else
 	<div class="body">
 		<div class="container<?php echo ($params->get('fluidContainer') ? '-fluid' : '');?>">
 			<!-- Header -->
-			<div class="header">
+			<header class="header" role="banner">
 				<div class="header-inner clearfix">
-					<a class="brand pull-left" href="<?php echo $this->baseurl; ?>">
+					<a class="brand pull-left" href="<?php echo $this->baseurl; ?>" title="<?php echo $sitename;?>">
 						<?php echo $logo;?> <?php if ($this->params->get('sitedescription')) { echo '<div class="site-description">'. htmlspecialchars($this->params->get('sitedescription')) .'</div>'; } ?>
 					</a>
 					<div class="header-search pull-right">
 						<jdoc:include type="modules" name="position-0" style="none" />
 					</div>
 				</div>
-			</div>
+			</header>
 			<?php if ($this->countModules('position-1')) : ?>
-			<div class="navigation">
+			<nav class="navigation" role="navigation">
 				<jdoc:include type="modules" name="position-1" style="none" />
-			</div>
+			</nav>
 			<?php endif; ?>
 			<jdoc:include type="modules" name="banner" style="xhtml" />
 			<div class="row-fluid">
@@ -170,14 +170,14 @@ else
 				</div>
 				<!-- End Sidebar -->
 				<?php endif; ?>
-				<div id="content" class="<?php echo $span;?>">
+				<main id="content" role="main" class="<?php echo $span;?>">
 					<!-- Begin Content -->
 					<jdoc:include type="modules" name="position-3" style="xhtml" />
 					<jdoc:include type="message" />
 					<jdoc:include type="component" />
 					<jdoc:include type="modules" name="position-2" style="none" />
 					<!-- End Content -->
-				</div>
+				</main>
 				<?php if ($this->countModules('position-7')) : ?>
 				<div id="aside" class="span3">
 					<!-- Begin Right Sidebar -->
@@ -189,14 +189,14 @@ else
 		</div>
 	</div>
 	<!-- Footer -->
-	<div class="footer">
+	<footer class="footer" role="contentinfo">
 		<div class="container<?php echo ($params->get('fluidContainer') ? '-fluid' : '');?>">
 			<hr />
 			<jdoc:include type="modules" name="footer" style="none" />
-			<p class="pull-right"><a href="#top" id="back-top"><?php echo JText::_('TPL_PROTOSTAR_BACKTOTOP'); ?></a></p>
+			<p class="pull-right"><a href="#top" class="back-top" title="<?php echo JText::_('TPL_PROTOSTAR_BACKTOTOP'); ?>"><?php echo JText::_('TPL_PROTOSTAR_BACKTOTOP'); ?></a></p>
 			<p>&copy; <?php echo $sitename; ?> <?php echo date('Y');?></p>
 		</div>
-	</div>
+	</footer>
 	<jdoc:include type="modules" name="debug" style="none" />
 </body>
 </html>
