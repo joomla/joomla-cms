ad6132fe6d1396b11f5a2938760161fe161d65d1
Mark Dexter <dextercowley@gmail.com>
2013-04-26 20:43:04 -0700

    Prepare for 3.1.2 development

--- libraries/cms/version/version.php
+++ libraries/cms/version/version.php
@@ -23,7 +23,7 @@ final class JVersion
 	public $RELEASE = '3.1';
 
 	/** @var  string  Maintenance version. */
-	public $DEV_LEVEL = '1';
+	public $DEV_LEVEL = '2';
 
 	/** @var  string  Development STATUS. */
 	public $DEV_STATUS = 'Stable';
@@ -35,7 +35,7 @@ final class JVersion
 	public $CODENAME = 'Ember';
 
 	/** @var  string  Release date. */
-	public $RELDATE = '26-April-2013';
+	public $RELDATE = '26-June-2013';
 
 	/** @var  string  Release time. */
 	public $RELTIME = '14:00';
