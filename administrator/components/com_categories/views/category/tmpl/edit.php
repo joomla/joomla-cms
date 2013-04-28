73059979014f546ca10dda4128fe940300bc828a
Elin Waring <elin.waring@gmail.com>
2013-04-28 09:14:26 -0400

    Fix situation in which a component is using com_categories but has not created a category type in the content_types table.

--- administrator/components/com_categories/views/category/tmpl/edit.php
+++ administrator/components/com_categories/views/category/tmpl/edit.php
@@ -202,7 +202,7 @@ JHtml::_('formbehavior.chosen', 'select');
 				</div>
 				<div class="control-group">
 					<?php foreach ($this->form->getFieldset('jmetadata') as $field) : ?>
-						<?php if ($field->name == 'jform[metadata][tags][]') :?>
+						<?php if ($this->typeId && $field->name == 'jform[metadata][tags][]') :?>
 						<div class="control-group">
 							<div class="control-label"><?php echo $field->label; ?></div>
 							<div class="controls"><?php echo $field->input; ?></div>
