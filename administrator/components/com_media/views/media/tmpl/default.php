97c279b994e826cc6ec959ea243c5cf31595c626
Mark Dexter <dextercowley@gmail.com>
2013-04-29 06:38:45 +0200

    # [#30723] Can't upload file after update from 2.5.9 to 2.5.10. Thanks Mark Dexter

--- administrator/components/com_media/views/media/tmpl/default.php
+++ administrator/components/com_media/views/media/tmpl/default.php
@@ -60,7 +60,7 @@ $user = JFactory::getUser();
 
 			<?php if ($user->authorise('core.create', 'com_media')):?>
 			<!-- File Upload Form -->
-			<form action="<?php echo JURI::base(); ?>index.php?option=com_media&amp;task=file.upload&amp;tmpl=component&amp;<?php echo $this->session->getName().'='.$this->session->getId(); ?>&amp;<?php echo JSession::getFormToken();?>=1&amp;format=<?php echo $this->config->get('enable_flash')=='1' ? 'json' : 'html' ?>" id="uploadForm" name="uploadForm" method="post" enctype="multipart/form-data">
+			<form action="<?php echo JURI::base(); ?>index.php?option=com_media&amp;task=file.upload&amp;tmpl=component&amp;<?php echo $this->session->getName().'='.$this->session->getId(); ?>&amp;<?php echo JSession::getFormToken();?>=1&amp;format=html" id="uploadForm" name="uploadForm" method="post" enctype="multipart/form-data">
 				<fieldset id="uploadform">
 					<legend><?php echo $this->config->get('upload_maxsize')=='0' ? JText::_('COM_MEDIA_UPLOAD_FILES_NOLIMIT') : JText::sprintf('COM_MEDIA_UPLOAD_FILES', $this->config->get('upload_maxsize')); ?></legend>
 					<fieldset id="upload-noflash" class="actions">
