a0917326c91b872b9d18829a2e6bf61c850e14ff
Puneet Kala <puneet.0191@gmail.com>
2013-05-01 16:35:51 +0530

    TagEditPage.php file is the page class file for Edit option along with Doc Blocks for Field Types

--- tests/system/webdriver/Pages/Components/TagEditPage.php
+++ tests/system/webdriver/Pages/Components/TagEditPage.php
@@ -13,13 +13,37 @@ use SeleniumClient\WebElement;
  */
 class TagEditPage extends AdminEditPage
 {
-  protected $waitForXpath =  "//form[@id='item-form']";
+        /**
+	 * The field type.
+	 *
+	 * @waitforXpath 	string		Contains the Xpath for the Edit Tag Page Form 
+	 */
+        protected $waitForXpath =  "//form[@id='item-form']";
+	/**
+	 * The field type.
+	 *
+	 * @url 	string		Contains the URL for the Edit Tag Page 
+	 */
 	protected $url = 'administrator/index.php?option=com_tags&view=tag&layout=edit';
-	
+	/**
+	 * The field type.
+	 *
+	 * @tabs 	string array		Contains all the Tabs that are present on the Page 
+	 */
 	public $tabs = array('general', 'publishing', 'metadata');
-	
+	/**
+	 * The field type.
+	 *
+	 * @tabLabels 	string array		Contains all the labels of the Tabs that are present on the Page 
+	 */
 	public $tabLabels = array('Tag Details', 'Publishing Options', 'Metadata Options');
-
+	
+	/**
+	 * The field type.
+	 *
+	 * @inputFields 	string array		Contains all the field Details of the Edit page 
+	 */
+	
 	public $inputFields = array (
 			array('label' => 'Title', 'id' => 'jform_title', 'type' => 'input', 'tab' => 'general'),
 			array('label' => 'Status', 'id' => 'jform_published', 'type' => 'select', 'tab' => 'general'),
