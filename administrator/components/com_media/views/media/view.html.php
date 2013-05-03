75c65b9d6d929a71f197fc5607851c2d67aabd2c
David Hurley <david@sparkbuilt.com>
2013-05-03 07:33:24 +0200

    # [#30694] Backend toolbar buttons have forbidden attribute href. Thanks David Hurley

--- administrator/components/com_media/views/media/view.html.php
+++ administrator/components/com_media/views/media/view.html.php
@@ -128,7 +128,7 @@ class MediaViewMedia extends JViewLegacy
 		if ($user->authorise('core.delete', 'com_media'))
 		{
 			$title = JText::_('JTOOLBAR_DELETE');
-			$dhtml = "<button href=\"#\" onclick=\"MediaManager.submit('folder.delete')\" class=\"btn btn-small\">
+			$dhtml = "<button onclick=\"MediaManager.submit('folder.delete')\" class=\"btn btn-small\">
 						<i class=\"icon-remove\" title=\"$title\"></i>
 						$title</button>";
 			$bar->appendButton('Custom', $dhtml, 'delete');
