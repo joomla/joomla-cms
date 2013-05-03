ae74e1de30bcad814bc2ef7ba30cc6be160391d4
RolandD <contact@csvimproved.com>
2013-05-03 08:06:24 +0200

    Add bootstrap styling to overrides list

--- administrator/components/com_languages/views/overrides/tmpl/default.php
+++ administrator/components/com_languages/views/overrides/tmpl/default.php
@@ -37,7 +37,7 @@ $listDirn  = $this->escape($this->state->get('list.direction')); ?>
 		</div>
 		<div class="clearfix"></div>
 
-		<table class="adminlist">
+		<table id="overrideList" class="table table-striped">
 			<thead>
 				<tr>
 					<th width="1%">
