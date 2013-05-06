f02e310a6d6df8063496ada3b7332f8c5fa36eb6
Elin Waring <elin.waring@gmail.com>
2013-05-05 22:29:52 -0400

    Make proper string for select tag

--- administrator/components/com_contact/views/contacts/view.html.php
+++ administrator/components/com_contact/views/contacts/view.html.php
@@ -144,7 +144,7 @@ class ContactViewContacts extends JViewLegacy
 		);
 
 		JHtmlSidebar::addFilter(
-		'-' . JText::_('JSELECT') . ' ' . JText::_('JTAG') . '-',
+		JText::_('JOPTION_SELECT_TAG'),
 		'filter_tag',
 		JHtml::_('select.options', JHtml::_('tag.options', true, true), 'value', 'text', $this->state->get('filter.tag'))
 		);
