10471698538cc4d4916366bc40c38591e7beda91
Aleksander Linkov <aleks.linya@gmail.com>
2013-05-06 17:15:48 +0300

    [#30755] Login dialog after Tag search with '.' in it.

--- components/com_tags/views/tag/tmpl/default_items.php
+++ components/com_tags/views/tag/tmpl/default_items.php
@@ -58,42 +58,42 @@ $n = count($this->items);
 		<p> <?php echo JText::_('COM_TAGS_NO_ITEMS'); ?></p>
 	<?php else : ?>
 
-	<ul class="category list-striped list-condensed">
-		<?php foreach ($items as $i => $item) : ?>
-			<?php if ($item->core_state == 0) : ?>
-				<li class="system-unpublished cat-list-row<?php echo $i % 2; ?>">
-			<?php else: ?>
-				<li class="cat-list-row<?php echo $i % 2; ?> clearfix" >
-				<h3>
-					<a href="<?php echo JRoute::_(TagsHelperRoute::getItemRoute($item->content_item_id, $item->core_alias, $item->core_catid, $item->core_language, $item->type_alias, $item->router)); ?>">
-						<?php echo $this->escape($item->core_title); ?>
-					</a>
-				</h3>
-			<?php endif; ?>
-			<?php $images  = json_decode($item->core_images);?>
-			<?php if ($this->params->get('tag_list_show_item_image', 1) == 1 && !empty($images->image_intro)) :?>
-				<img src="<?php echo htmlspecialchars($images->image_intro);?>" alt="<?php echo htmlspecialchars($images->image_intro_alt); ?>">
-			<?php endif; ?>
-			<?php if ($this->params->get('tag_list_show_item_description', 1)) : ?>
-				<span class="tag-body">
-					<?php echo JHtml::_('string.truncate', $item->core_body, $this->params->get('tag_list_item_maximum_characters')); ?>
-				</span>
-			<?php endif; ?>
-				</li>
-		<?php endforeach; ?>
-	</ul>
+		<ul class="category list-striped list-condensed">
+			<?php foreach ($items as $i => $item) : ?>
+				<?php if ($item->core_state == 0) : ?>
+					<li class="system-unpublished cat-list-row<?php echo $i % 2; ?>">
+				<?php else: ?>
+					<li class="cat-list-row<?php echo $i % 2; ?> clearfix" >
+					<h3>
+						<a href="<?php echo JRoute::_(TagsHelperRoute::getItemRoute($item->content_item_id, $item->core_alias, $item->core_catid, $item->core_language, $item->type_alias, $item->router)); ?>">
+							<?php echo $this->escape($item->core_title); ?>
+						</a>
+					</h3>
+				<?php endif; ?>
+				<?php $images  = json_decode($item->core_images);?>
+				<?php if ($this->params->get('tag_list_show_item_image', 1) == 1 && !empty($images->image_intro)) :?>
+					<img src="<?php echo htmlspecialchars($images->image_intro);?>" alt="<?php echo htmlspecialchars($images->image_intro_alt); ?>">
+				<?php endif; ?>
+				<?php if ($this->params->get('tag_list_show_item_description', 1)) : ?>
+					<span class="tag-body">
+						<?php echo JHtml::_('string.truncate', $item->core_body, $this->params->get('tag_list_item_maximum_characters')); ?>
+					</span>
+				<?php endif; ?>
+					</li>
+			<?php endforeach; ?>
+		</ul>
 
-	<?php if ($this->params->get('show_pagination')) : ?>
-	 <div class="pagination">
-		<?php if ($this->params->get('show_pagination_results', 1)) : ?>
-			<p class="counter">
-				<?php echo $this->pagination->getPagesCounter(); ?>
-			</p>
+		<?php if ($this->params->get('show_pagination')) : ?>
+		 <div class="pagination">
+			<?php if ($this->params->get('show_pagination_results', 1)) : ?>
+				<p class="counter">
+					<?php echo $this->pagination->getPagesCounter(); ?>
+				</p>
+			<?php endif; ?>
+				<?php echo $this->pagination->getPagesLinks(); ?>
+			</div>
+			</br>
 		<?php endif; ?>
-			<?php echo $this->pagination->getPagesLinks(); ?>
-		</div>
-		</br>
+
 	<?php endif; ?>
 </form>
-
-<?php endif; ?>
