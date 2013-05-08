203c75a279cba6997717bec8951b3c03fc7b7ff1
peterpeter <abt@masterhomepage.ch>
2013-05-08 23:23:34 +0200

    Update updatesnotifier_cron.php
    Refactored version:
    - db-error check, - debug output
    + logging (file), + notifications more informative (with update-names and version numbers)

--- cli/updatesnotifier_cron.php
+++ cli/updatesnotifier_cron.php
@@ -6,28 +6,32 @@
  * @license    GNU General Public License version 2 or later; see LICENSE
  */
 
-// Make sure we're being called from the command line, not a web interface
-if (array_key_exists('REQUEST_METHOD', $_SERVER))
-{
-  die();
-}
-
-// Configure error reporting to maximum for CLI output.
-error_reporting(E_ALL | E_NOTICE);
+// Configure error reporting
+error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 // Switch register_argc_argv on
 @ini_set('register_argc_argv', 1);
 
+/**
+ * This is a command-line script, that notifies about new extension updates.
+ * Perfect as a daily cronjob, to keep the whole site up to date.
+ *
+ * Recipient mail address is taken from the global configuration
+ * but can be overritten by a calling argument to notifiy directly
+ * a/the responsible webmaster/web-agency:
+ *
+ * Without mail override:
+ * /path/to/php /path/to/site/cli/updatesnotifier_cron.php
+ *
+ * With mail override:
+ * /path/to/php /path/to/site/cli/updatesnotifier_cron.php webmaster@mysite.org
+ *
+ * (just add the address after a space)
+ */
+
 // Set flag that this is a parent file.
 define('_JEXEC', 1);
-define('DS', DIRECTORY_SEPARATOR);
-
-// Load system defines
-if (file_exists(dirname(dirname(__FILE__)) . '/defines.php'))
-{
-	require_once dirname(dirname(__FILE__)) . '/defines.php';
-}
 
 if (!defined('_JDEFINES'))
 {
@@ -38,45 +42,27 @@ if (!defined('_JDEFINES'))
 require_once JPATH_LIBRARIES . '/import.php';
 require_once JPATH_LIBRARIES . '/cms.php';
 
-
-// Import and Load Library language
-jimport('joomla.language.language');
-
 /**
- * This is a command-line script, that notifies about new updates and db-errors.
- * Mail is sending only if somthing was found to notfy.
- * Otherwise in debug mode it sends on every call a mail.
- * 
- * Perfect as a daily cronjob, to keep the site up to date. 
- * Recipient mail address is taken from the global configuration
- * but can be overritten by a calling argument e.g.:
- * 
- * Without mail override:
- * /path/to/php /path/to/site/cli/updatesnotifier_cron.php
- * 
- * With mail-override (depends on the server-os):
- * /path/to/php /path/to/site/cli/updatesnotifier_cron.php example@example.org
- * 
- * Is the override mail address invalid (JMailHelper::isEmailAddress())
- * it switches back to global conf. address
+ * Class to search for new updates for all
+ * installed 'one-click-updateable' extensions
+ * and notify per mail if any detected
  *
  * @package  Joomla.CLI
  * @since    2.5
  */
 class Updatesnotifier extends JApplicationCli
 {
-
 	/**
-	 * Message-Container
+	 * Updates-Container
 	 * @var array
 	 */
-	protected $messages = array();
+	protected $updates 			= array();
 
 	/**
-	 * Script-Error: true if a script error occurs
-	 * @var bool
+	 * Mail-Address to be notified
+	 * @var string
 	 */
-	protected $scriptError = false;
+	protected $mailRecipient	= '';
 
 	/**
 	 * the constructor
@@ -86,289 +72,163 @@ class Updatesnotifier extends JApplicationCli
 	public function __construct()
 	{
 		parent::__construct();
-
-		// Get the commandline arguments
-		$this->input = $_SERVER['argv'];
+		$file = $this->input->executable;
+		$mailOverride	= !empty($this->input->args[0]) ? $this->input->args[0] : false;
+		$this->mailRecipient	= $mailOverride ? $mailOverride : $this->config->get('mailfrom');
 
 		// Load the language
 		$lang = JFactory::getLanguage();
-		$lang->load('cli_updatesnotifier_cron', JPATH_SITE, null, false, false) || $lang->load('cli_updatesnotifier_cron', JPATH_SITE, null, true);
+		$lang->load('cli_updatesnotifier_cron', JPATH_SITE, null, false, false)
+			|| $lang->load('cli_updatesnotifier_cron', JPATH_SITE, null, true);
 
-		// Set debug constant if needed
-		if (!defined('JDEBUG'))
+		// Setting up the Logger
+		$logger	= array(
+						/* Logger type */
+						'logger' 			=> 'formattedtext',
+						/* Filename: one log file per month */
+						'text_file' 		=> strftime('%b') . '_log.php',
+						/* Setting the log-path */
+						'text_file_path' 	=> $this->config->get('log_path') . '/updatesnotifier_cron'
+		);
+		JLog::addLogger($logger, JLog::ALL, array('updatesnotifier_cron'));
+		JLog::add(
+			JText::sprintf(
+			'CLI_UPDATESNOTIFIER_LOG_INFO_SETUP', $this->mailRecipient, ($mailOverride) ? JText::_('JYES') : JText::_('JNO')
+			),
+			JLog::INFO, 'updatesnotifier_cron'
+		);
+
+		// Filename-Check:
+		if (false === strpos($this->input->executable, 'updatesnotifier_cron.php'))
 		{
-			define('JDEBUG', $this->config->get('debug', 0));
-		}
-
-		// If we are in debug-mode, communicate that
-		if (JDEBUG)
-		{
-			$this->addMessage(JText::_('CLI_UPDATESNOTIFIER_DEBUG_INFO_DEBUG_MODE_RUNNING'));
-		}
-
-		/* Checks the scriptname: For securirity reason, file rename are allowed, but only as
-		   additional filename-prefix, to keep the possibility of batch processing
-		   by shellscripts/directory-parsing
-		*/
-		if (!empty($this->input[0]) && false === strpos($this->input[0], 'updatesnotifier_cron.php'))
-		{
-			$this->addMessage(JText::_('CLI_UPDATESNOTIFIER_FILE_ERROR_RENAME'));
-			$this->scriptError = true;
-		}
-
-		// Looking for a mail-recipient override
-		if (!empty($this->input[1]))
-		{
-			jimport('joomla.mail.helper');
-			$mailOverride = $this->input[1];
-			$isValidMailaddress = JMailHelper::isEmailAddress($mailOverride);
-
-			if (true === $isValidMailaddress)
-			{
-				// Override the global mail address
-				$this->config->set('mailfrom', $this->input[1]);
-
-				if (JDEBUG)
-				{
-					$this->addMessage(JText::sprintf('CLI_UPDATESNOTIFIER_DEBUG_INFO_MAIL_OVERRIDE', $this->config->get('mailfrom')));
-				}
-			}
-			elseif (JDEBUG)
-			{
-				$this->addMessage(JText::sprintf('CLI_UPDATESNOTIFIER_DEBUG_INFO_MAIL_OVERRIDE_INVALID', $mailOverride));
-			}
+			JLog::add(JText::_('CLI_UPDATESNOTIFIER_LOG_ERROR_FILERENAME'), JLog::ERROR, 'updatesnotifier_cron');
+			JLog::add(JText::_('CLI_UPDATESNOTIFIER_LOG_ERROR_SCRIPT_ABORTET'), JLog::NOTICE, 'updatesnotifier_cron');
+			die;
 		}
 	}
 
 	/**
-	 * execute the 'script'
-	 * 
+	 * Method to purge the update cache and find
+	 * updates for installed extensions
+	 *
 	 * @return void
 	 */
-	public function doExecute()
+	protected function getUpdates()
 	{
-		// Find all updates and process the result
-		$this->getExtensionsUpdates();
-		$this->getJoomlaUpdate();
-		$this->getDbErrors();
-		$this->proceedMessages();
-	}
 
-	/**
-	 * Handles the messages
-	 * 
-	 * @return void
-	 */
-	protected function proceedMessages()
-	{
-		if (!empty($this->messages))
-		{
-			// Proceed mail
-			$this->proceedMail();
-
-			// Proceed console output
-			$this->out(implode("\n", $this->messages));
-
-			// 2Do: Good point to log some script-errors
-			// if(true === $this->scriptError)
-		}
-	}
-
-	/**
-	 * purge the cache and checks for updates
-	 * for the installed extensions
-	 * (those who have implemented the update feature)
-	 * 
-	 * @return void
-	 */
-	protected function getExtensionsUpdates()
-	{
 		try
 		{
-			$model = new InstallerModelUpdateCli;
+			JLoader::register('installerModelUpdate', JPATH_ADMINISTRATOR . '/components/com_installer/models/update.php');
+			$model = new InstallerModelUpdate(array('ignore_request' => true));
 
-			if (!$model->purge() and JDEBUG)
+			if (!$model->purge())
 			{
-				$this->addMessage(JText::_('CLI_UPDATESNOTIFIER_DEBUG_INFO_EXTENSIONS_CACHE_NOT_PURGED'));
-				$this->scriptError = true;
+				Jlog::add(JText::_('CLI_UPDATESNOTIFIER_LOG_INFO_DB_CACHE_PURGED'), JLog::INFO, 'updatesnotifier_cron');
 			}
-			$getUpdates = $model->findUpdates();
-			$updatesFound = $model->getItems();
+
+			if (!$model->findUpdates())
+			{
+				Jlog::add(JText::_('CLI_UPDATESNOTIFIER_LOG_ERROR_FIND_UPDATES'), JLog::ERROR, 'updatesnotifier_cron');
+				JLog::add(JText::_('CLI_UPDATESNOTIFIER_LOG_ERROR_SCRIPT_ABORTET'), JLog::NOTICE, 'updatesnotifier_cron');
+
+				return;
+			}
+
+			// Set some model states
+			$model->setState('list.ordering', 'name');
+			$model->setState('list.direction', 'ASC');
+
+			// Get joomla update
+			$model->setState('filter.extension_id', 700);
+			$joomlaUpdate = ($model->getItems());
+
+			// Change the store_id to prevent caching
+			$model->setState('list.ordering', 'type');
+
+			// Get updates from all others installed extensions
+			$model->setState('filter.extension_id', 0);
+			$extensionUpdates = $model->getItems();
+
 		}
 		catch (Exception $e)
 		{
-			$this->addMessage(JText::_('CLI_UPDATESNOTIFIER_EXTENSIONS_EXCEPTION'));
-			$this->scriptError = true;
-
-			return;
+			JLog::add(JText::sprintf('CLI_UPDATESNOTIFIER_LOG_ERROR_FIND_UPDATES', $e->getMessage()), JLog::ERROR, 'updatesnotifier_cron');
 		}
 
-		if (is_array($updatesFound) && !empty($updatesFound))
-		{
-			$this->addMessage(JText::_('CLI_UPDATESNOTIFIER_EXTENSIONS_NEW_UPDATES_FOUND'));
+		$updates = array_merge($joomlaUpdate, $extensionUpdates);
 
-			return;
+		if (count($updates))
+		{
+			$this->updates = $updates;
+			Jlog::add(JText::sprintf('CLI_UPDATESNOTIFIER_LOG_INFO_UPDATES_FOUND', count($updates)), JLog::INFO, 'updatesnotifier_cron');
 		}
-
-		if (JDEBUG)
+		else
 		{
-			$this->addMessage(JText::_('CLI_UPDATESNOTIFIER_EXTENSIONS_NO_UPDATES_FOUND'));
+			Jlog::add(JText::_('CLI_UPDATESNOTIFIER_LOG_INFO_NO_NEW_UPDATES_FOUND'), JLog::INFO, 'updatesnotifier_cron');
 		}
 	}
 
 	/**
-	 * checks for a new Joomla! core update
-	 * 
+	 * Sends the notification
+	 *
 	 * @return void
 	 */
-	protected function getJoomlaUpdate()
+	protected function proceedNotification()
 	{
-		try
+		$nl			= "\n";
+		$cfg		= $this->config;
+		$subject	= JText::sprintf('CLI_UPDATESNOTIFIER_MAIL_SUBJECT_UPDATES_FOUND', $cfg->get('sitename'));
+		$mailBody	= JText::_('CLI_UPDATESNOTIFIER_MAIL_BODY_INTRO_I') . ' "' . $cfg->get('sitename') . '"';
+		$mailBody	.= JText::_('CLI_UPDATESNOTIFIER_MAIL_BODY_INTRO_II');
+
+		for ($i = 0; $i < count($this->updates); $i++)
 		{
-			require_once JPATH_ADMINISTRATOR . '/components/com_joomlaupdate/models/default.php';
-			$model = new JoomlaupdateModelDefault;
-			$result = $model->getUpdateInformation();
-			$updateFound = !empty($result['object']);
+			$textLine	= $i + 1 . '. ' . $this->updates[$i]->name . ' (' . $this->updates[$i]->version . ')';
+			$mailBody .= $textLine . $nl;
+			Jlog::add($textLine, JLog::INFO, 'updatesnotifier_cron');
 		}
-		catch (Exception $e)
+
+		$mailBody 	.= $nl . $nl . JText::_('CLI_UPDATESNOTIFIER_MAIL_BODY_OUTRO');
+		$mailer 	= JFactory::getMailer();
+		$mailSent 	= $mailer->sendMail(
+							$cfg->get('mailfrom'), $cfg->get('fromname'), $this->mailRecipient,
+							$subject, $mailBody, false, null, null, null, $cfg->get('mailfrom'), $cfg->get('fromname')
+							);
+
+		if ($mailSent)
 		{
-			$this->addMessage(JText::_('CLI_UPDATESNOTIFIER_JOOMLAUPDATE_EXCEPTION'));
-			$this->scriptError = true;
+			Jlog::add(
+					JText::sprintf('CLI_UPDATESNOTIFIER_MAIL_SENT_SUCCESS', $this->mailRecipient),
+					JLog::INFO, 'updatesnotifier_cron'
+					);
 
 			return;
 		}
-
-		if (true === $updateFound)
-		{
-			$this->addMessage(JText::_('CLI_UPDATESNOTIFIER_JOOMLAUPDATE_NEW_UPDATE_FOUND'));
-		}
-		elseif (JDEBUG)
-		{
-			$this->addMessage(JText::_('CLI_UPDATESNOTIFIER_JOOMLAUPDATE_NO_UPDATE_FOUND'));
-		}
+		Jlog::add(
+			JText::sprintf('CLI_UPDATESNOTIFIER_MAIL_SENT_ERROR', $this->mailRecipient),
+			JLog::INFO, 'updatesnotifier_cron'
+		);
 	}
 
 	/**
-	 * compares db scheme and checks for 
-	 * db errors
-	 * 
+	 * execute method of the 'script'
+	 *
 	 * @return void
 	 */
-	protected function getDbErrors()
+	protected function doExecute()
 	{
-		try
-		{
-			require_once JPATH_ADMINISTRATOR . '/components/com_installer/models/database.php';
-			$model = new InstallerModelDatabase;
-			$changeSet = $model->getItems();
-			$dbErrors = count($changeSet->check());
-			$schemaVersion = $model->getSchemaVersion();
-			$filterParams = $model->getDefaultTextFilters();
-			$updateVersion = $model->getUpdateVersion();
+		JLog::add(JText::_('CLI_UPDATESNOTIFIER_LOG_INFO_START'), JLog::INFO, 'updatesnotifier_cron');
 
-			if (!(strncmp($schemaVersion, JVERSION, 5) === 0) || !$filterParams || $updateVersion != JVERSION)
-			{
-				$dbErrors++;
-			}
-		}
-		catch (Exception $e)
-		{
-			$this->addMessage(JText::_('CLI_UPDATESNOTIFIER_DATABASE_EXCEPTION'));
-			$this->scriptError = true;
+		$this->getUpdates();
 
-			return;
+		if (count($this->updates))
+		{
+			$this->proceedNotification();
 		}
 
-		if ($dbErrors > 0)
-		{
-			$this->addMessage(JText::_('CLI_UPDATESNOTIFIER_DATABASE_NEW_ERROR'));
-		}
-		elseif (JDEBUG)
-		{
-			$this->addMessage(JText::_('CLI_UPDATESNOTIFIER_DATABASE_NO_ERROR'));
-		}
-	}
-
-	/**
-	 * function to send a notification mail, if
-	 * new updates are available or a script error
-	 * occurs
-	 * 
-	 * @return void
-	 */
-	protected function proceedMail()
-	{
-		try
-		{
-			$recipient = $this->config->get('mailfrom');
-			$siteName = $this->config->get('sitename');
-			$mailer = JFactory::getMailer();
-			$body = JText::sprintf('CLI_UPDATESNOTIFIER_MAIL_BODY_INTRO', $siteName);
-			$body .= implode("\n", $this->messages);
-			$body .= JText::_('CLI_UPDATESNOTIFIER_MAIL_BODY_OUTRO');
-			$mailer->sendMail($recipient, $siteName, $recipient, 'updates@' . $siteName, $body, 0, null, null, null, $recipient, $siteName);
-		}
-		catch (Exception $e)
-		{
-			$this->addMessage('CLI_UPDATESNOTIFIER_MAIL_MESSAGE_SENT_ERROR');
-			$this->scriptError = true;
-
-			return;
-		}
-		$this->addMessage(JText::sprintf('CLI_UPDATESNOTIFIER_MAIL_MESSAGE_SENT_SUCCESS', $recipient));
-	}
-
-	/**
-	 * Adds a message to the message-stack
-	 * 
-	 * @var string $message
-	 * @return void
-	 */
-	protected function addMessage($message)
-	{
-		$this->messages[] = $message;
+		JLog::add(JText::_('CLI_UPDATESNOTIFIER_LOG_INFO_END'), JLog::INFO, 'updatesnotifier_cron');
 	}
 }
 
-require_once JPATH_ADMINISTRATOR . '/components/com_installer/models/update.php';
-/**
- * Inherits the InstallerModelUpdate for method overloading
- * 
- * @package  Joomla.CLI
- * @since    2.5
- */
-class InstallerModelUpdateCli extends InstallerModelUpdate
-{
-	/**
-	 * overrides the unneeded populate state method
-	 * cause there is no propper JUri Instance
-	 * and thus throws an error
-	 * 
-	 * @return void
-	 */
-	public function populateState()
-	{
-	}
-
-	/**
-	 * overrides the getListQuery method
-	 * cause here exists no extension-id filter
-	 * 
-	 * @return JQuery Object
-	 */
-	protected function getListQuery()
-	{
-		$db = $this->getDbo();
-		$query = $db->getQuery(true);
-
-		// Grab updates ignoring new installs and joomla core
-		$query->select('*')
-			->from('#__updates')
-			->where('extension_id != 0')
-			->where($db->nq('extension_id') . ' != ' . $db->q(700));
-
-		return $query;
-	}
-}
-
-JApplicationCli::getInstance('Updatesnotifier')->execute();
+	// Load and execute the class
+	JApplicationCli::getInstance('Updatesnotifier')->execute();
