7b8632fd4632cf0f6359b94bcbdac9971d2176a4
Brian Teeman <brian@teeman.net>
2013-05-09 20:51:17 +0200

    Add debug css to error page for ISIS
    http://joomlacode.org/gf/project/joomla/tracker/?action=TrackerItemEdit&tracker_item_id=30754

--- administrator/templates/isis/error.php
+++ administrator/templates/isis/error.php
@@ -65,6 +65,15 @@ else
 	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 	<meta name="language" content="<?php echo $this->language; ?>" />
 	<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/css/template.css" type="text/css" />
+	<?php // If debug  mode
+		$debug = JFactory::getConfig()->get('debug_lang');
+		if ((defined('JDEBUG') && JDEBUG) || $debug)
+		{
+	?>
+		<link rel="stylesheet" href="<?php echo JURI::root() ?>/media/cms/css/debug.css" type="text/css" />
+	<?php
+		}
+	?>
 	<?php
 	// If Right-to-Left
 	if ($this->direction == 'rtl')
