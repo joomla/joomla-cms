702b59b5452a824016a1175d0f508c81e4be6fab
diana <diana.prajescu@gmail.com>
2013-05-17 17:58:56 +0300

    Make Search work with v1.1

--- libraries/joomla/twitter/search.php
+++ libraries/joomla/twitter/search.php
@@ -28,11 +28,8 @@ class JTwittersearch extends JTwitterObject
 	 * @param   string   $lang         Restricts tweets to the given language, given by an ISO 639-1 code.
 	 * @param   string   $locale       Specify the language of the query you are sending (only ja is currently effective). This is intended for
 	 * 								   language-specific clients and the default should work in the majority of cases.
-	 * @param   integer  $page         The page number (starting at 1) to return, up to a max of roughly 1500 results (based on rpp * page).
 	 * @param   string   $result_type  Specifies what type of search results you would prefer to receive. The current default is "mixed."
-	 * @param   integer  $rpp          The number of tweets to return per page, up to a max of 100.
-	 * @param   boolean  $show_user    When true, prepends ":" to the beginning of the tweet. This is useful for readers that do not display Atom's
-	 * 								   author field.
+	 * @param   integer  $count        The number of tweets to return per page, up to a maximum of 100. Defaults to 15.
 	 * @param   string   $until        Returns tweets generated before the given date. Date should be formatted as YYYY-MM-DD.
 	 * @param   integer  $since_id     Returns results with an ID greater than (that is, more recent than) the specified ID.
 	 * @param   integer  $max_id       Returns results with an ID less than (that is, older than) or equal to the specified ID.
@@ -43,92 +40,80 @@ class JTwittersearch extends JTwitterObject
 	 *
 	 * @since   12.3
 	 */
-	public function search($query, $callback = null, $geocode = null, $lang = null, $locale = null, $page = 0, $result_type = null,
-		$rpp = 0, $show_user = false, $until = null, $since_id = 0, $max_id = 0, $entities = false)
+	public function search($query, $callback = null, $geocode = null, $lang = null, $locale = null, $result_type = null, $count = 15,
+		$until = null, $since_id = 0, $max_id = 0, $entities = null)
 	{
 		// Check the rate limit for remaining hits
-		$this->checkRateLimit();
+		$this->checkRateLimit('search', 'tweets');
 
-		// Set the API base
-		$base = 'http://search.twitter.com/search.json';
+		// Set the API path
+		$path = '/search/tweets.json';
 
 		// Set query parameter.
-		$parameters['q'] = rawurlencode($query);
+		$data['q'] = rawurlencode($query);
 
 		// Check if callback is specified.
 		if ($callback)
 		{
-			$parameters['callback'] = $callback;
+			$data['callback'] = $callback;
 		}
 
 		// Check if geocode is specified.
 		if ($geocode)
 		{
-			$parameters['geocode'] = $geocode;
+			$data['geocode'] = $geocode;
 		}
 
 		// Check if lang is specified.
 		if ($lang)
 		{
-			$parameters['lang'] = $lang;
+			$data['lang'] = $lang;
 		}
 
 		// Check if locale is specified.
 		if ($locale)
 		{
-			$parameters['locale'] = $locale;
-		}
-
-		// Check if page is specified.
-		if ($page > 0)
-		{
-			$parameters['page'] = $page;
+			$data['locale'] = $locale;
 		}
 
 		// Check if result_type is specified.
 		if ($result_type)
 		{
-			$parameters['result_type'] = $result_type;
+			$data['result_type'] = $result_type;
 		}
 
-		// Check if rpp is specified.
-		if ($rpp > 0)
+		// Check if count is specified.
+		if ($count != 15)
 		{
-			$parameters['rpp'] = $rpp;
-		}
-
-		// Check if show_user is true.
-		if ($show_user)
-		{
-			$parameters['show_user'] = $show_user;
+			$data['count'] = $count;
 		}
 
 		// Check if until is specified.
 		if ($until)
 		{
-			$parameters['until'] = $until;
+			$data['until'] = $until;
 		}
 
 		// Check if since_id is specified.
 		if ($since_id > 0)
 		{
-			$parameters['since_id'] = $since_id;
+			$data['since_id'] = $since_id;
 		}
 
 		// Check if max_id is specified.
 		if ($max_id > 0)
 		{
-			$parameters['max_id'] = $max_id;
+			$data['max_id'] = $max_id;
 		}
 
-		// Check if entities is true.
-		if ($entities)
+		// Check if entities is specified.
+		if (!is_null($entities))
 		{
-			$parameters['include_entities'] = $entities;
+			$data['include_entities'] = $entities;
 		}
 
 		// Send the request.
-		return $this->sendRequest($base, 'get', $parameters);
+		return $this->sendRequest($path, 'GET', $data);
 	}
 
 	/**
@@ -141,24 +126,13 @@ class JTwittersearch extends JTwitterObject
 	public function getSavedSearches()
 	{
 		// Check the rate limit for remaining hits
-		$this->checkRateLimit();
+		$this->checkRateLimit('saved_searches', 'list');
 
-		// Set the API base
-		$base = '/1/saved_searches.json';
-
-		$token = $this->oauth->getToken();
-
-		// Set parameters.
-		$parameters = array(
-			'oauth_token' => $token['key']
-		);
-
-		// Build the request path.
-		$path = $this->getOption('api.url') . $base;
+		// Set the API path
+		$path = '/saved_searches/list.json';
 
 		// Send the request.
-		$response = $this->oauth->oauthRequest($path, 'GET', $parameters);
-		return json_decode($response->body);
+		return $this->sendRequest($path);
 	}
 
 	/**
@@ -173,24 +147,13 @@ class JTwittersearch extends JTwitterObject
 	public function getSavedSearchesById($id)
 	{
 		// Check the rate limit for remaining hits
-		$this->checkRateLimit();
+		$this->checkRateLimit('saved_searches', 'show/:id');
 
-		// Set the API base
-		$base = '/1/saved_searches/' . $id . '.json';
-
-		$token = $this->oauth->getToken();
-
-		// Set parameters.
-		$parameters = array(
-			'oauth_token' => $token['key']
-		);
-
-		// Build the request path.
-		$path = $this->getOption('api.url') . $base;
+		// Set the API path
+		$path = '/saved_searches/show/' . $id . '.json';
 
 		// Send the request.
-		$response = $this->oauth->oauthRequest($path, 'GET', $parameters);
-		return json_decode($response->body);
+		return $this->sendRequest($path);
 	}
 
 	/**
@@ -204,25 +167,14 @@ class JTwittersearch extends JTwitterObject
 	 */
 	public function createSavedSearch($query)
 	{
-		// Set the API base
-		$base = '/1/saved_searches/create.json';
-
-		$token = $this->oauth->getToken();
-
-		// Set parameters.
-		$parameters = array(
-			'oauth_token' => $token['key']
-		);
+		// Set the API path
+		$path = '/saved_searches/create.json';
 
 		// Set POST request data
 		$data['query'] = rawurlencode($query);
 
-		// Build the request path.
-		$path = $this->getOption('api.url') . $base;
-
 		// Send the request.
-		$response = $this->oauth->oauthRequest($path, 'POST', $parameters, $data);
-		return json_decode($response->body);
+		return $this->sendRequest($path, 'POST', $data);
 	}
 
 	/**
@@ -236,21 +188,13 @@ class JTwittersearch extends JTwitterObject
 	 */
 	public function deleteSavedSearch($id)
 	{
-		// Set the API base
-		$base = '/1/saved_searches/destroy/' . $id . '.json';
+		// Check the rate limit for remaining hits
+		$this->checkRateLimit('saved_searches', 'destroy/:id');
 
-		$token = $this->oauth->getToken();
-
-		// Set parameters.
-		$parameters = array(
-			'oauth_token' => $token['key']
-		);
-
-		// Build the request path.
-		$path = $this->getOption('api.url') . $base;
+		// Set the API path
+		$path = '/saved_searches/destroy/' . $id . '.json';
 
 		// Send the request.
-		$response = $this->oauth->oauthRequest($path, 'POST', $parameters);
-		return json_decode($response->body);
+		return $this->sendRequest($path, 'POST');
 	}
 }
