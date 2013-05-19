9d881600e09c826802789703561379631f1f3248
wilsonge <georgejameswilson@googlemail.com>
2013-05-20 00:14:32 +0100

    Change a few tags references to contact

--- tests/system/webdriver/tests/components/ContactManager0002Test.php
+++ tests/system/webdriver/tests/components/ContactManager0002Test.php
@@ -16,7 +16,7 @@ use SeleniumClient\WebDriverWait;
 use SeleniumClient\DesiredCapabilities;
 
 /**
- * This class tests the  Tags: Add / Edit  Screen.
+ * This class tests the  Contact: Add / Edit  Screen.
  *
  * @package     Joomla.Test
  * @subpackage  Webdriver
@@ -89,34 +89,34 @@ class ContactManager0002Test extends JoomlaWebdriverTestCase
 	 */
 	public function setFilter_TestFilters_ShouldFilterContacts()
 	{
-		$tagName_1 = 'Test Filter 1';
-		$tagName_2 = 'Test Filter 2';
+		$contactName_1 = 'Test Filter 1';
+		$contactName_2 = 'Test Filter 2';
 
-		$this->contactManagerPage->addContact($tagName_1, false);
+		$this->contactManagerPage->addContact($contactName_1, false);
 		$message = $this->contactManagerPage->getAlertMessage();
 		$this->assertTrue(strpos($message, 'Contact successfully saved') >= 0, 'Contact save should return success');
-		$state = $this->contactManagerPage->getState($tagName_1);
+		$state = $this->contactManagerPage->getState($contactName_1);
 		$this->assertEquals('published', $state, 'Initial state should be published');
 
 
-		$this->contactManagerPage->addContact($tagName_2, false);
+		$this->contactManagerPage->addContact($contactName_2, false);
 		$message = $this->contactManagerPage->getAlertMessage();
 		$this->assertTrue(strpos($message, 'Contact successfully saved') >= 0, 'Contact save should return success');
-		$state = $this->contactManagerPage->getState($tagName_2);
+		$state = $this->contactManagerPage->getState($contactName_2);
 		$this->assertEquals('published', $state, 'Initial state should be published');
-		$this->contactManagerPage->changeContactState($tagName_2, 'unpublished');
+		$this->contactManagerPage->changeContactState($contactName_2, 'unpublished');
 
 		$test = $this->contactManagerPage->setFilter('filter_published', 'Unpublished');
-		$this->assertFalse($this->contactManagerPage->getRowNumber($tagName_1), 'Contact should not show');
-		$this->assertEquals(1, $this->contactManagerPage->getRowNumber($tagName_2), 'Contact should be in row 1');
+		$this->assertFalse($this->contactManagerPage->getRowNumber($contactName_1), 'Contact should not show');
+		$this->assertEquals(1, $this->contactManagerPage->getRowNumber($contactName_2), 'Contact should be in row 1');
 
 		$test = $this->contactManagerPage->setFilter('filter_published', 'Published');
-		$this->assertFalse($this->contactManagerPage->getRowNumber($tagName_2), 'Contact should not show');
-		$this->assertEquals(8, $this->contactManagerPage->getRowNumber($tagName_1), 'Contact should be in row 8');
+		$this->assertFalse($this->contactManagerPage->getRowNumber($contactName_2), 'Contact should not show');
+		$this->assertEquals(8, $this->contactManagerPage->getRowNumber($contactName_1), 'Contact should be in row 8');
 
 		$this->contactManagerPage->setFilter('Select Status', 'Select Status');
-		$this->contactManagerPage->deleteItem($tagName_1);
-		$this->contactManagerPage->deleteItem($tagName_2);
+		$this->contactManagerPage->deleteItem($contactName_1);
+		$this->contactManagerPage->deleteItem($contactName_2);
 	}
 
 }
