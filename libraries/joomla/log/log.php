dacae3547716357cce79cfa81726ece890c175a8
Michael Babker <mbabker@flbab.com>
2013-06-02 15:58:58 -0500

    Sync Log package to Platform 13.1

--- libraries/joomla/log/log.php
+++ libraries/joomla/log/log.php
@@ -180,7 +180,7 @@ class JLog
 		}
 		$options['logger'] = strtolower($options['logger']);
 
-		// Special case - if a Closure object is sent as the callback (in case of JLoggerCallback)
+		// Special case - if a Closure object is sent as the callback (in case of JLogLoggerCallback)
 		// Closure objects are not serializable so swap it out for a unique id first then back again later
 		if (isset($options['callback']) && is_a($options['callback'], 'closure'))
 		{
@@ -249,6 +249,7 @@ class JLog
 			{
 
 				$class = 'JLogLogger' . ucfirst($this->configurations[$signature]['logger']);
+
 				if (class_exists($class))
 				{
 					$this->loggers[$signature] = new $class($this->configurations[$signature]);
