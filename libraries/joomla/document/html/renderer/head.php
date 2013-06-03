0bbe2e98a735966e20b8b222938d7ce18dbba920
anibalsanchez <anibal.sanchez@extly.com>
2013-06-03 10:38:33 -0300

    Initialization also in a self-invoking anonymous function

--- libraries/joomla/document/html/renderer/head.php
+++ libraries/joomla/document/html/renderer/head.php
@@ -227,30 +227,31 @@ class JDocumentRendererHead extends JDocumentRenderer
 		if (count($document->_modules))
 		{
 			$buffer .= $tab . '<script type="text/javascript">' . $lnEnd;
-			$buffer .= $tab . $tab . 'var jPaths = [];' . $lnEnd;
-			$buffer .= $tab . $tab . 'var jDependencies = [];' . $lnEnd;
-			$buffer .= $tab . $tab . 'jQuery(document).ready(function () {' . $lnEnd;
-			$buffer .= $tab . $tab . $tab . '"use strict";' . $lnEnd;
-			$buffer .= $tab . $tab . $tab . 'var allKeys = [], config = {shim: {}, paths: {}};' . $lnEnd;
+			$buffer .= $tab . $tab . '(function() {' . $lnEnd;
+			$buffer .= $tab . $tab . $tab . 'var jPaths = [];' . $lnEnd;
+			$buffer .= $tab . $tab . $tab . 'var jDependencies = [];' . $lnEnd;
+			$buffer .= $tab . $tab . $tab . 'jQuery(document).ready(function () {' . $lnEnd;
+			$buffer .= $tab . $tab . $tab . $tab . '"use strict";' . $lnEnd;
+			$buffer .= $tab . $tab . $tab . $tab . 'var allKeys = [], config = {shim: {}, paths: {}};' . $lnEnd;
 
 			// To load all paths in the loader configuration
-			$buffer .= $tab . $tab . $tab . 'jQuery.each(jPaths, function(index, path) {allKeys.push(path.key); config.paths[path.key] = path.value;});' . $lnEnd;
+			$buffer .= $tab . $tab . $tab . $tab . 'jQuery.each(jPaths, function(index, path) {allKeys.push(path.key); config.paths[path.key] = path.value;});' . $lnEnd;
 
 			// To load all dependencies in the loader configuration
-			$buffer .= $tab . $tab . $tab . 'jQuery.each(jDependencies, function(index, dependency) {config.shim[dependency.key] = dependency.value;});' . $lnEnd;
+			$buffer .= $tab . $tab . $tab . $tab . 'jQuery.each(jDependencies, function(index, dependency) {config.shim[dependency.key] = dependency.value;});' . $lnEnd;
 
 			// Loader configuration
-			$buffer .= $tab . $tab . $tab . 'require.config(config);' . $lnEnd;
+			$buffer .= $tab . $tab . $tab . $tab . 'require.config(config);' . $lnEnd;
 
 			// Loader initialization of main jcore, it depends on all previously declared modules
-			$buffer .= $tab . $tab . $tab . 'require(allKeys, function() {});' . $lnEnd;
+			$buffer .= $tab . $tab . $tab . $tab . 'require(allKeys, function() {});' . $lnEnd;
 
-			$buffer .= $tab . $tab . '});' . $lnEnd;
+			$buffer .= $tab . $tab . $tab . '});' . $lnEnd;
 
 			foreach ($document->_modules as $moduleId => $moduleAttr)
 			{
 				$url = str_replace('.js', '', $moduleAttr['url']);
-				$buffer .= $tab . $tab . 'jPaths.push({key:\'' . $moduleId . '\', value: \'' . $url . '\'});' . $lnEnd;
+				$buffer .= $tab . $tab . $tab . 'jPaths.push({key:\'' . $moduleId . '\', value: \'' . $url . '\'});' . $lnEnd;
 
 				if (count($moduleAttr['dependencies']))
 				{
@@ -261,9 +262,10 @@ class JDocumentRendererHead extends JDocumentRenderer
 								$value = '\'' . $value . '\'';
 							});
 					$dependencies = implode(',', $dependencies);
-					$buffer .= $tab . $tab . 'jDependencies.push({key:\'' . $moduleId . '\', value: {deps: [' . $dependencies . ']}});' . $lnEnd;
+					$buffer .= $tab . $tab . $tab . 'jDependencies.push({key:\'' . $moduleId . '\', value: {deps: [' . $dependencies . ']}});' . $lnEnd;
 				}
 			}
+			$buffer .= $tab . $tab . '})();' . $lnEnd;
 			$buffer .= $tab . '</script>' . $lnEnd;
 		}
 
