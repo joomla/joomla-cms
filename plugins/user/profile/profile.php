066a3a68e803ee71a7186ff21cf982d23c6b1fc6
Elin Waring <elin.waring@gmail.com>
2013-06-14 16:15:39 -0400

    Changes to profile

--- plugins/user/profile/profile.php
+++ plugins/user/profile/profile.php
@@ -127,7 +127,9 @@ class PlgUserProfile extends JPlugin
 		}
 		else
 		{
-			$value = htmlspecialchars($value);
+			// Convert website url to utf8 for display
+			$value = JStringPunycode::urlToUTF8(htmlspecialchars($value));
+
 			if (substr($value, 0, 4) == "http")
 			{
 				return '<a href="' . $value . '">' . $value . '</a>';
