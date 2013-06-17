d89a7c9e0f6f864e70d1eb846f6ae434b0fc8779
Constantin Romankiewicz <harmageddon95@web.de>
2013-06-17 18:22:43 +0200

    Tag Cloud Module

---
+++ modules/mod_tags_cloud/tmpl/default.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  mod_tags_cloud
+ *
+ * @copyright   Copyright (C) 2005 - 2013 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+?>
+<?php JLoader::register('TagsHelperRoute', JPATH_BASE . '/components/com_tags/helpers/route.php'); ?>
+<div class="tagcloud<?php echo $moduleclass_sfx; ?>">
+<?php foreach ($list as $item) :	?>
+  <?php $route = new TagsHelperRoute; ?>
+	<a style="font-size: <?php echo $item->size.'em'; ?>" href="<?php echo JRoute::_(TagsHelperRoute::getTagRoute($item->tag_id . ':' . $item->alias)); ?>">
+		<?php echo htmlspecialchars($item->title); ?>
+  </a>
+<?php endforeach; ?>
+</div>
