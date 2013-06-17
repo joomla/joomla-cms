e32850afa8fa63b5378c079539a5311bf4402169
Achal-Aggarwal <theachalaggarwal@gmail.com>
2013-06-17 17:29:20 +0530

    Inserting comment in JFormFieldText.

--- libraries/joomla/form/fields/text.php
+++ libraries/joomla/form/fields/text.php
@@ -49,6 +49,8 @@ class JFormFieldText extends JFormField
 
 		// Initialize JavaScript field attributes.
 		$onchange = $this->element['onchange'] ? ' onchange="' . (string) $this->element['onchange'] . '"' : '';
+
+		// Including fallback code for HTML5 non supported browsers.
 		JHtml::_('jquery.framework');
 		JHtml::_('script', 'system/html5fallback.js', false, true);
 
