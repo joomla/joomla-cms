9680049115cb15713fb45af017a173683ec1a3f2
David Hurley <david@sparkbuilt.com>
2013-06-20 11:58:01 +0200

    # [#31125] Category images don't show in Categories list. Thanks David Hurley (Fix #1279)

--- components/com_content/views/categories/tmpl/default_items.php
+++ components/com_content/views/categories/tmpl/default_items.php
@@ -37,6 +37,9 @@ if (count($this->items[$this->parent->id]) > 0 && $this->maxLevelcat != 0) :
 					<a href="#category-<?php echo $item->id;?>" data-toggle="collapse" data-toggle="button" class="btn btn-mini pull-right"><span class="icon-plus"></span></a>
 				<?php endif;?>
 			</h3>
+			<?php if ($this->params->get('show_description_image') && $item->getParams()->get('image')) : ?>
+				<img src="<?php echo $item->getParams()->get('image'); ?>"/>
+			<?php endif; ?>
 			<?php if ($this->params->get('show_subcat_desc_cat') == 1) :?>
 				<?php if ($item->description) : ?>
 					<div class="category-desc">
