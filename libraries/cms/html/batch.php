d8520d6f484da958ef5f621edbcc51d83710d4ab
Peter van Westen <peter@nonumber.nl>
2013-06-25 00:33:45 +0200

    Fixed code syntax

--- libraries/cms/html/batch.php
+++ libraries/cms/html/batch.php
@@ -28,12 +28,12 @@ abstract class JHtmlBatch
 	public static function access()
 	{
 		// Create the batch selector to change an access level on a selection list.
-		$lines = array(
+		return
 			'<label id="batch-access-lbl" for="batch-access" class="hasToolip"'
-			. 'title="' . JHtml::tooltipText('JLIB_HTML_BATCH_ACCESS_LABEL', 'JLIB_HTML_BATCH_ACCESS_LABEL_DESC') . '">',
-			JText::_('JLIB_HTML_BATCH_ACCESS_LABEL'),
-			'</label>',
-			JHtml::_(
+			. 'title="' . JHtml::tooltipText('JLIB_HTML_BATCH_ACCESS_LABEL', 'JLIB_HTML_BATCH_ACCESS_LABEL_DESC') . '">'
+			. JText::_('JLIB_HTML_BATCH_ACCESS_LABEL')
+			. '</label>'
+			. JHtml::_(
 				'access.assetgrouplist',
 				'batch[assetgroup_id]', '',
 				'class="inputbox"',
@@ -41,8 +41,7 @@ abstract class JHtmlBatch
 					'title' => JText::_('JLIB_HTML_BATCH_NOCHANGE'),
 					'id' => 'batch-access'
 				)
-			)
-		);
+			);
 
 		return implode("\n", $lines);
 	}
@@ -59,23 +58,23 @@ abstract class JHtmlBatch
 	public static function item($extension)
 	{
 		// Create the copy/move options.
-		$options = array(JHtml::_('select.option', 'c', JText::_('JLIB_HTML_BATCH_COPY')),
-			JHtml::_('select.option', 'm', JText::_('JLIB_HTML_BATCH_MOVE')));
-
-		// Create the batch selector to change select the category by which to move or copy.
-		$lines = array(
-			'<label id="batch-choose-action-lbl" for="batch-choose-action">', JText::_('JLIB_HTML_BATCH_MENU_LABEL'), '</label>',
-			'<div id="batch-choose-action" class="control-group">',
-			'<select name="batch[category_id]" class="inputbox" id="batch-category-id">',
-			'<option value="">' . JText::_('JSELECT') . '</option>',
-			JHtml::_('select.options', JHtml::_('category.options', $extension)),
-			'</select>',
-			'</div>', '<div id="batch-move-copy" class="control-group radio">',
-			JHtml::_('select.radiolist', $options, 'batch[move_copy]', '', 'value', 'text', 'm'),
-			'</div><hr />'
+		$options = array(
+			JHtml::_('select.option', 'c', JText::_('JLIB_HTML_BATCH_COPY')),
+			JHtml::_('select.option', 'm', JText::_('JLIB_HTML_BATCH_MOVE'))
 		);
 
-		return implode("\n", $lines);
+		// Create the batch selector to change select the category by which to move or copy.
+		return
+			'<label id="batch-choose-action-lbl" for="batch-choose-action">' . JText::_('JLIB_HTML_BATCH_MENU_LABEL') . '</label>'
+			. '<div id="batch-choose-action" class="control-group">'
+			. '<select name="batch[category_id]" class="inputbox" id="batch-category-id">'
+			. '<option value="">' . JText::_('JSELECT') . '</option>'
+			. JHtml::_('select.options', JHtml::_('category.options', $extension))
+			. '</select>'
+			. '</div>'
+			. '<div id="batch-move-copy" class="control-group radio">'
+			. JHtml::_('select.radiolist', $options, 'batch[move_copy]', '', 'value', 'text', 'm')
+			. '</div><hr />';
 	}
 
 	/**
@@ -88,18 +87,15 @@ abstract class JHtmlBatch
 	public static function language()
 	{
 		// Create the batch selector to change the language on a selection list.
-		$lines = array(
+		return
 			'<label id="batch-language-lbl" for="batch-language-id" class="hasToolip"'
-			. ' title="' . JHtml::tooltipText('JLIB_HTML_BATCH_LANGUAGE_LABEL', 'JLIB_HTML_BATCH_LANGUAGE_LABEL_DESC') . '">',
-			JText::_('JLIB_HTML_BATCH_LANGUAGE_LABEL'),
-			'</label>',
-			'<select name="batch[language_id]" class="inputbox" id="batch-language-id">',
-			'<option value="">' . JText::_('JLIB_HTML_BATCH_LANGUAGE_NOCHANGE') . '</option>',
-			JHtml::_('select.options', JHtml::_('contentlanguage.existing', true, true), 'value', 'text'),
-			'</select>',
-		);
-
-		return implode("\n", $lines);
+			. ' title="' . JHtml::tooltipText('JLIB_HTML_BATCH_LANGUAGE_LABEL', 'JLIB_HTML_BATCH_LANGUAGE_LABEL_DESC') . '">'
+			. JText::_('JLIB_HTML_BATCH_LANGUAGE_LABEL')
+			. '</label>'
+			. '<select name="batch[language_id]" class="inputbox" id="batch-language-id">'
+			. '<option value="">' . JText::_('JLIB_HTML_BATCH_LANGUAGE_NOCHANGE') . '</option>'
+			. JHtml::_('select.options', JHtml::_('contentlanguage.existing', true, true), 'value', 'text')
+			. '</select>';
 	}
 
 	/**
@@ -113,8 +109,6 @@ abstract class JHtmlBatch
 	 */
 	public static function user($noUser = true)
 	{
-		JHtml::_('bootstrap.tooltip');
-
 		$optionNo = '';
 		if ($noUser)
 		{
@@ -122,19 +116,16 @@ abstract class JHtmlBatch
 		}
 
 		// Create the batch selector to select a user on a selection list.
-		$lines = array(
+		return
 			'<label id="batch-user-lbl" for="batch-user" class="hasTooltip"'
-			. ' title="' . JHtml::tooltipText('JLIB_HTML_BATCH_USER_LABEL', 'JLIB_HTML_BATCH_USER_LABEL_DESC') . '">',
-			JText::_('JLIB_HTML_BATCH_USER_LABEL'),
-			'</label>',
-			'<select name="batch[user_id]" class="inputbox" id="batch-user-id">',
-			'<option value="">' . JText::_('JLIB_HTML_BATCH_USER_NOCHANGE') . '</option>',
-			$optionNo,
-			JHtml::_('select.options', JHtml::_('user.userlist'), 'value', 'text'),
-			'</select>'
-		);
-
-		return implode("\n", $lines);
+			. ' title="' . JHtml::tooltipText('JLIB_HTML_BATCH_USER_LABEL', 'JLIB_HTML_BATCH_USER_LABEL_DESC') . '">'
+			. JText::_('JLIB_HTML_BATCH_USER_LABEL')
+			. '</label>'
+			. '<select name="batch[user_id]" class="inputbox" id="batch-user-id">'
+			. '<option value="">' . JText::_('JLIB_HTML_BATCH_USER_NOCHANGE') . '</option>'
+			. $optionNo
+			. JHtml::_('select.options', JHtml::_('user.userlist'), 'value', 'text')
+			. '</select>';
 	}
 
 	/**
@@ -146,20 +137,15 @@ abstract class JHtmlBatch
 	 */
 	public static function tag()
 	{
-		JHtml::_('bootstrap.tooltip');
-
 		// Create the batch selector to tag items on a selection list.
-		$lines = array(
+		return
 			'<label id="batch-tag-lbl" for="batch-tag-id" class="hasTooltip"'
-			. ' title="' . JHtml::tooltipText('JLIB_HTML_BATCH_TAG_LABEL', 'JLIB_HTML_BATCH_TAG_LABEL_DESC') . '">',
-			JText::_('JLIB_HTML_BATCH_TAG_LABEL'),
-			'</label>',
-			'<select name="batch[tag]" class="inputbox" id="batch-tag-id">',
-			'<option value="">' . JText::_('JLIB_HTML_BATCH_TAG_NOCHANGE') . '</option>',
-			JHtml::_('select.options', JHtml::_('tag.tags', array('filter.published' => array(1))), 'value', 'text'),
-			'</select>'
-		);
-
-		return implode("\n", $lines);
+			. ' title="' . JHtml::tooltipText('JLIB_HTML_BATCH_TAG_LABEL', 'JLIB_HTML_BATCH_TAG_LABEL_DESC') . '">'
+			. JText::_('JLIB_HTML_BATCH_TAG_LABEL')
+			. '</label>'
+			. '<select name="batch[tag]" class="inputbox" id="batch-tag-id">'
+			. '<option value="">' . JText::_('JLIB_HTML_BATCH_TAG_NOCHANGE') . '</option>'
+			. JHtml::_('select.options', JHtml::_('tag.tags', array('filter.published' => array(1))), 'value', 'text')
+			. '</select>';
 	}
 }
