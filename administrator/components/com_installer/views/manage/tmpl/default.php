83820ccb02de30a205a9193c2c056b32547a556e
Peter van Westen <peter@nonumber.nl>
2013-06-26 15:13:51 +0200

    Fixed double translation in com_installer manage view

--- administrator/components/com_installer/views/manage/tmpl/default.php
+++ administrator/components/com_installer/views/manage/tmpl/default.php
@@ -99,7 +99,7 @@ $listDirn	= $this->escape($this->state->get('list.direction'));
 						<?php echo JHtml::_('grid.id', $i, $item->extension_id); ?>
 					</td>
 					<td>
-						<span class="bold hasTooltip" title="<?php echo JHtml::tooltipText($item->name, $item->description); ?>">
+						<span class="bold hasTooltip" title="<?php echo JHtml::tooltipText($item->name, $item->description, 0); ?>">
 							<?php echo $item->name; ?>
 						</span>
 					</td>
