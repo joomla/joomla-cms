bfaddf219d3259ba92bab30f88b0ab0f9ba70c35
Peter van Westen <peter@nonumber.nl>
2013-06-26 12:41:54 +0200

    Fixed issue with tooltips on disabled ordering icons

--- administrator/components/com_contact/views/contacts/tmpl/default.php
+++ administrator/components/com_contact/views/contacts/tmpl/default.php
@@ -141,22 +141,23 @@ $assoc		= isset($app->item_associations) ? $app->item_associations : 0;
 				?>
 				<tr class="row<?php echo $i % 2; ?>" sortable-group-id="<?php echo $item->catid?>">
 					<td class="order nowrap center hidden-phone">
-						<?php if ($canChange) :
-							$disableClassName = '';
-							$disabledLabel	  = '';
-							if (!$saveOrder) :
-								$disabledLabel    = 'JORDERINGDISABLED';
-								$disableClassName = 'inactive tip-top';
-							endif; ?>
-							<span class="sortable-handler hasTooltip<?php echo $disableClassName?>" title="<?php echo JHtml::tooltipText($disabledLabel); ?>">
-								<i class="icon-menu"></i>
-							</span>
+						<?php
+						$iconClass = '';
+						if (!$canChange)
+						{
+							$iconClass = ' inactive';
+						}
+						else if (!$saveOrder)
+						{
+							$iconClass = ' inactive tip-top hasTooltip" title="' . JHtml::tooltipText('JORDERINGDISABLED');
+						}
+						?>
+						<span class="sortable-handler<?php echo $iconClass ?>">
+							<i class="icon-menu"></i>
+						</span>
+						<?php if ($canChange && $saveOrder) : ?>
 							<input type="text" style="display:none" name="order[]" size="5"
-							value="<?php echo $item->ordering;?>" class="width-20 text-area-order " />
-						<?php else : ?>
-							<span class="sortable-handler inactive" >
-								<i class="icon-menu"></i>
-							</span>
+								value="<?php echo $item->ordering; ?>" class="width-20 text-area-order " />
 						<?php endif; ?>
 					</td>
 					<td class="center hidden-phone">
