7b5cecc7bcf041db839a45a3151131a86b952e18
Jérôme GLATIGNY <jerome@obsidev.com>
2013-06-28 18:36:17 +0200

    ^ [#31100] UX "showon": Dynamic options in the Joomla Backend. Thanks Jérôme GLATIGNY (Fix #1240)

--- administrator/components/com_config/views/application/tmpl/default_locale.php
+++ administrator/components/com_config/views/application/tmpl/default_locale.php
@@ -8,17 +8,7 @@
  */
 
 defined('_JEXEC') or die;
-?>
-<fieldset class="form-horizontal">
-	<legend><?php echo JText::_('COM_CONFIG_LOCATION_SETTINGS'); ?></legend>
-	<?php
-	foreach ($this->form->getFieldset('locale') as $field):
-	?>
-		<div class="control-group">
-			<div class="control-label"><?php echo $field->label; ?></div>
-			<div class="controls"><?php echo $field->input; ?></div>
-		</div>
-	<?php
-	endforeach;
-	?>
-</fieldset>
+
+$this->name = JText::_('COM_CONFIG_LOCATION_SETTINGS');
+$this->fieldsname = 'locale';
+echo JLayoutHelper::render('joomla.content.options_default', $this);
