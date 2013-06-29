08b4c44827bb55568fd85ef52240a3119b9419ac
Jean-Marie Simonet <infografjms@gmail.com>
2013-06-29 07:52:29 +0200

    Missing strings and +

--- libraries/cms/form/rule/password.php
+++ libraries/cms/form/rule/password.php
@@ -157,7 +157,7 @@ class JFormRulePassword extends JFormRule
 			if (strlen((string) $value) < $minimumLength)
 			{
 				JFactory::getApplication()->enqueueMessage(
-					JText::plural('COM_USERS_MSG_PASSWORD_TOO_SHORT', count($minimumLength)),
+					JText::sprintf('COM_USERS_MSG_PASSWORD_TOO_SHORT', count($minimumLength)),
 					'warning'
 					);
 
