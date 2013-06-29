8529c7f93e3b6c12e94239e9df068f6b78f488eb
Chraneco <chraneco@meindebian.informatik.uni-stuttgart.de>
2013-06-29 19:14:27 +0200

    Added JResponseJson as a CMS library class

--- administrator/components/com_languages/controllers/strings.json.php
+++ administrator/components/com_languages/controllers/strings.json.php
@@ -19,22 +19,6 @@ defined('_JEXEC') or die;
 class LanguagesControllerStrings extends JControllerAdmin
 {
 	/**
-	 * Constructor
-	 *
-	 * @param   	array	An optional associative array of configuration settings
-	 *
-	 * @return  void
-	 *
-	 * @since		2.5
-	 */
-	public function __construct($config = array())
-	{
-		parent::__construct($config);
-
-		require_once JPATH_COMPONENT . '/helpers/jsonresponse.php';
-	}
-
-	/**
 	 * Method for refreshing the cache in the database with the known language strings
 	 *
 	 * @return  void
