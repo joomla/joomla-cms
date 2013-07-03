8755f93c2eadfdfa54e19c6ee6064db16f3b5c69
Beat <beat@joomlapolis.com>
2013-07-03 19:26:46 +0200

    Fixed bug #31327 Debug plugin (git master version only) does not catch SQL errors

--- plugins/system/debug/debug.php
+++ plugins/system/debug/debug.php
@@ -1228,6 +1228,12 @@ class PlgSystemDebug extends JPlugin
 					// Display duration in ms with the unit instead of seconds:
 					$html[] = sprintf('%.1f&nbsp;ms', $td * 1000);
 				}
+				elseif ($k == 'Error')
+				{
+					// An error in the EXPLAIN query occured, display it instead of the result (means original query had syntax error most probably):
+					$html[] = '<td class="dbg-warning">' . htmlspecialchars($td);
+					$hasWarnings = true;
+				}
 				elseif ($k == 'key')
 				{
 					if ($td === 'NULL')
@@ -1315,8 +1321,14 @@ class PlgSystemDebug extends JPlugin
 
 				if ((stripos($query, 'select') === 0) || ($dbVersion56 && ((stripos($query, 'delete') === 0) || (stripos($query, 'update') === 0))))
 				{
-					$db->setQuery('EXPLAIN ' . ($dbVersion56 ? 'EXTENDED ' : '') . $query);
-					$this->explains[$k] = $db->loadAssocList();
+					try
+					{
+						$db->setQuery('EXPLAIN ' . ($dbVersion56 ? 'EXTENDED ' : '') . $query);
+						$this->explains[$k] = $db->loadAssocList();
+					}
+					catch (Exception $e) {
+						$this->explains[$k] = array(array('Error' => $e->getMessage()));
+					}
 				}
 			}
 		}
