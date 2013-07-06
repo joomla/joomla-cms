3b79c2a854980634cd3099b077004b2df5be5adf
Michael Babker <mbabker@flbab.com>
2013-07-06 17:26:58 -0500

    Sync Access and Application packages to Platform 13.1 (CS & unused code only)

--- libraries/legacy/web/web.php
+++ libraries/legacy/web/web.php
@@ -34,7 +34,7 @@ class JWeb extends JApplicationWeb
 	 *                          the application's client object, otherwise a default client object is created.
 	 *
 	 * @since   11.3
-	 * @deprecated  12.3 Use JApplicationWeb instead.
+	 * @deprecated  12.3 (Platform) & 4.0 (CMS) Use JApplicationWeb instead.
 	 */
 	public function __construct(JInput $input = null, JRegistry $config = null, JApplicationWebClient $client = null)
 	{
