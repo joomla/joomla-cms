202f14abadb782d8b0ef2b84d184620edae82c89
Janich Rasmussen <janich@gmail.com>
2013-07-09 01:44:47 +0200

    Updated Mod Maps Helper

--- modules/mod_maps/helper.php
+++ modules/mod_maps/helper.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package     Joomla.Site
- * @subpackage  mod_tags_popular
+ * @subpackage  mod_maps
  *
  * @copyright   Copyright (C) 2005 - 2013 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
@@ -10,10 +10,19 @@
 defined('_JEXEC') or die;
 
 /**
- * Helper for mod_tags_popular
+ * Helper for mod_maps
  *
  * @package     Joomla.Site
- * @subpackage  mod_tags_popular
- * @since       3.1
+ * @subpackage  mod_maps
+ * @since       3.2
  */
-
+class ModMapsHelper
+{
+	/*
+	 * @since  3.2
+	 */
+	public static function getSize(&$size)
+	{
+		return (strpos($size, '%') === false ? $size . 'px' : $size);
+	}
+}
