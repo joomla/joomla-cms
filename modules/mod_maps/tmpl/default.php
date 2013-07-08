a50e8c89056a4976ef96aac0853905177b4fd747
lasinducharith <lasinducharith@gmail.com>
2013-07-08 23:25:21 +0530

    changes to default.php javascript

--- modules/mod_maps/tmpl/default.php
+++ modules/mod_maps/tmpl/default.php
@@ -12,53 +12,54 @@ defined('_JEXEC') or die;
 ?>
 
 <div class="google-maps map<?php echo $moduleclass_sfx; ?>">
-	<!--	<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>-->
 	<script type="text/javascript">
-		var map;
-		var geocoder;
-		var pos;
-		var results;
-		var status;
-		function initialize() {
+		var map, geocoder, pos;
 
+		function initialize() {
 
 			var address = "<?php echo $params->get('mapCenterAddress')?>";
 			var centerCordinate = "<?php echo $params->get('mapCenterCoordinate') ?>";
 			var mapCenterType = "<?php echo $params->get('mapCenterType') ?>";
 			var mapOptions = {
-					zoom: <?php echo $params->get('zoom', '15')?>,
-//					center: pos,
-					mapTypeId: google.maps.MapTypeId.<?php echo $params->get('mapType','ROADMAP'); ?>,
-					zoomControl: true,
-					panControl: true,
-					zoomControlOptions: {
-						style: google.maps.ZoomControlStyle.LARGE
-					}
-				}
-				;
-			map = new google.maps.Map(document.getElementById('map-canvas-<?php echo $id; ?>'),
-				mapOptions);
-
+				zoom: <?php echo $params->get('zoom', '15')?>,
+				mapTypeId: google.maps.MapTypeId.<?php echo $params->get('mapType','ROADMAP'); ?>,
+				zoomControl: false,
+				panControl: false
+			};
+			map = new google.maps.Map(document.getElementById('map-canvas-<?php echo $id; ?>'), mapOptions);
 
 			if (mapCenterType == "address" && address != null) {
 				geocoder = new google.maps.Geocoder();
-				geocoder.geocode({ 'address': address}, function (results, status) {
-					if (status == google.maps.GeocoderStatus.OK) {
-						pos = new google.maps.LatLng(results[0].geometry.location.lat(), results[0].geometry.location.lng());
-						map.setCenter(pos);
-					}
-				});
+				codeAddress(address);
 			}
 			if (mapCenterType == "coordinate" && centerCordinate != null) {
-				pos = new google.maps.LatLng(<?php echo $params->get('mapCenterCoordinate'); ?>);
-				map.setCenter(pos);
+				mapLongLat();
 			}
+		}
 
-			var marker = new google.maps.Marker({
-				position: pos,
-				map: map
+		function codeAddress(address) {
+			geocoder.geocode({ 'address': address}, function (results, status) {
+				if (status == google.maps.GeocoderStatus.OK) {
+					map.setCenter(results[0].geometry.location);
+					var marker = new google.maps.Marker({
+						map: map,
+						position: results[0].geometry.location
+					});
+				} else {
+					alert("Geocode was not successful for the following reason: " + status);
+				}
 			});
 		}
+
+		function mapLongLat() {
+			pos = new google.maps.LatLng(<?php echo $params->get('mapCenterCoordinate'); ?>);
+			map.setCenter(pos);
+			var marker = new google.maps.Marker({
+				map: map,
+				position: pos
+			});
+		}
+
 		google.maps.event.addDomListener(window, 'load', initialize);
 
 	</script>
