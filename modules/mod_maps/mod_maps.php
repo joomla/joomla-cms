7bb8293d29dddb60899b86e447f936ed6ad5bcd3
Janich Rasmussen <janich@gmail.com>
2013-07-09 01:46:19 +0200

    Updated Mod Maps template

--- modules/mod_maps/mod_maps.php
+++ modules/mod_maps/mod_maps.php
@@ -11,13 +11,28 @@ defined('_JEXEC') or die;
 
 // Include the syndicate functions only once
 require_once __DIR__ . '/helper.php';
-$id	= $module->id;
-$height = $params->get('height', 350);
 
-//$document = ModMapsHelper::getData($params);
 $doc = JFactory::getDocument();
-$doc->addScript('https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false'); //Add map api script
-$doc->addStyledeclaration('#map-canvas-'.$id.' {margin:0;padding:0;height:' . $height . 'px}'); //Add inline stlesheet
+
+// Ready parameters
+$id                 = $module->id;
+$width              = $params->get('width', '100%');
+$height             = $params->get('height', '350');
+$mapType            = $params->get('mapType','ROADMAP');
+$zoomLevel          = $params->get('zoom', '1');
+$centerType         = $params->get('mapCenterType', 'address');
+$centerCoordinate   = $params->get('mapCenterCoordinate', '0,0');
+$centerAddress      = $params->get('mapCenterAddress', 'Earth');
+$key                = $params->get('api_key', false);
+
+// Add map api
+if ($key) {
+	$gmapjs = 'https://maps.googleapis.com/maps/api/js?v=3&key='. $key .'&sensor=false';
+}
+else {
+	$gmapjs = 'https://maps.googleapis.com/maps/api/js?v=3&sensor=false';
+}
+$doc->addScript($gmapjs);
 
 $moduleclass_sfx = htmlspecialchars($params->get('moduleclass_sfx'));
 require JModuleHelper::getLayoutPath('mod_maps', $params->get('layout', 'default'));
--- modules/mod_maps/tmpl/default.php
+++ modules/mod_maps/tmpl/default.php
@@ -5,6 +5,7 @@
  *
  * @copyright   Copyright (C) 2005 - 2013 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ * @see         https://developers.google.com/maps/documentation/javascript/geocoding#GeocodingRequests
  */
 
 defined('_JEXEC') or die;
@@ -12,56 +13,55 @@ defined('_JEXEC') or die;
 ?>
 
 <div class="google-maps map<?php echo $moduleclass_sfx; ?>">
-	<script type="text/javascript">
-		var map, geocoder, pos;
-
-		function initialize() {
-
-			var address = "<?php echo $params->get('mapCenterAddress')?>";
-			var centerCordinate = "<?php echo $params->get('mapCenterCoordinate') ?>";
-			var mapCenterType = "<?php echo $params->get('mapCenterType') ?>";
-			var mapOptions = {
-				zoom: <?php echo $params->get('zoom', '15')?>,
-				mapTypeId: google.maps.MapTypeId.<?php echo $params->get('mapType','ROADMAP'); ?>,
-				zoomControl: false,
-				panControl: false
-			};
-			map = new google.maps.Map(document.getElementById('map-canvas-<?php echo $id; ?>'), mapOptions);
-
-			if (mapCenterType == "address" && address != null) {
-				geocoder = new google.maps.Geocoder();
-				codeAddress(address);
-			}
-			if (mapCenterType == "coordinate" && centerCordinate != null) {
-				mapLongLat();
-			}
-		}
-
-		function codeAddress(address) {
-			geocoder.geocode({ 'address': address}, function (results, status) {
-				if (status == google.maps.GeocoderStatus.OK) {
-					map.setCenter(results[0].geometry.location);
-					var marker = new google.maps.Marker({
-						map: map,
-						position: results[0].geometry.location
-					});
-				} else {
-					alert("Geocode was not successful for the following reason: " + status);
-				}
-			});
-		}
-
-		function mapLongLat() {
-			pos = new google.maps.LatLng(<?php echo $params->get('mapCenterCoordinate'); ?>);
-			map.setCenter(pos);
-			var marker = new google.maps.Marker({
-				map: map,
-				position: pos
-			});
-		}
-
-		google.maps.event.addDomListener(window, 'load', initialize);
-
-	</script>
 	<div id="map-canvas-<?php echo $id; ?>"></div>
 </div>
+
+
+<?php
+
+// Compile Google Map JS
+$js = '
+		jQuery(document).ready(function() {
+				var mapOptions = {
+					zoom: '. $zoomLevel .',
+					mapTypeId: google.maps.MapTypeId.'. $mapType .'
+				};
+
+				var map = new google.maps.Map(document.getElementById("map-canvas-'. $id .'"), mapOptions);
+				';
+
+
+if ($centerType == 'coordinate'):
+	$js .= 'map.setCenter(new google.maps.LatLng('. $centerCoordinate .'));';
+else:
+	$js .= 'var geocoder = new google.maps.Geocoder();
+
+				geocoder.geocode({"address": "'. $centerAddress .'"}, function(results, status) {
+					switch (status) {
+						case google.maps.GeocoderStatus.OK:
+							map.setCenter(results[0].geometry.location);
+							break;
+						case google.maps.GeocoderStatus.ZERO_RESULTS:
+							alert("The address could not be found");
+							break;
+						case google.maps.GeocoderStatus.OVER_QUERY_LIMIT:
+								alert("You have exceeded your map quota");
+							break;
+						default:
+							alert("An unknown error occured. Please try reloading the page");
+					}
+				});
+				';
+endif;
+
+$js .= '});';
+$doc->addScriptDeclaration($js);
+
+// Compile CSS
+$css = '#map-canvas-'. $id .' {
+	margin: 0;
+	padding: 0;
+	width: '. modMapsHelper::getSize($width) .';
+	height:' . modMapsHelper::getSize($height) .';
+}';
+$doc->addStyleDeclaration($css);
