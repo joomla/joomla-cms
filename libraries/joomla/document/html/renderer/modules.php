2a63ef899074f294a2651e4e89ec6149585fb9f1
Beat <beat@joomlapolis.com>
2013-07-10 00:58:34 +0100

    [JUX2] Front-end modules and menu editing: part 2: Add Front-end Modules editing (first version)

--- libraries/joomla/document/html/renderer/modules.php
+++ libraries/joomla/document/html/renderer/modules.php
@@ -31,12 +31,48 @@ class JDocumentRendererModules extends JDocumentRenderer
 	 */
 	public function render($position, $params = array(), $content = null)
 	{
+		static $jsNotOut =true;
+
+		$app = JFactory::getApplication();
+
 		$renderer = $this->_doc->loadRenderer('module');
 		$buffer = '';
 
 		foreach (JModuleHelper::getModules($position) as $mod)
 		{
-			$buffer .= $renderer->render($mod, $params, $content);
+			$moduleHtml = trim($renderer->render($mod, $params, $content));
+
+			if (!$moduleHtml)
+			{
+				continue;
+			}
+
+
+			if ($app->isAdmin()
+				|| !JFactory::getUser()->authorise('core.manage', 'com_modules')
+				|| preg_match('/<(?:div|span|nav|ul) [^>]*class="[^"]* jmoddiv"/', $moduleHtml))
+			{
+				// Module isn't enclosing in a div with class or handles already module edit button:
+				$buffer .= $moduleHtml;
+				continue;
+			}
+
+			// Add css class jmoddiv and data attributes for module-editing URL and for the tooltip:
+			$editUrl = JURI::base() . 'administrator/' . 'index.php?option=com_modules&view=module&layout=edit&id=' . (int) $mod->id;
+
+			$buffer .= preg_replace('/^(<(?:div|span|nav|ul) [^>]*class="[^"]*)"/',
+					'\\1 jmoddiv" data-jmodediturl="' . $editUrl . '" data-jmodtip="' . sprintf(JHtml::tooltipText('JLIB_HTML_EDIT_MODULE', 'JLIB_HTML_EDIT_MODULE_IN_POSITION'), htmlspecialchars($position)) . '"', $moduleHtml);
+
+			if ($jsNotOut)
+			{
+				// Load once booststrap tooltip and add stylesheet and javascript to head:
+				$jsNotOut = false;
+				JHtml::_('bootstrap.tooltip');
+
+				JFactory::getDocument()->addStyleSheet('media/system/css/frontediting.css')
+					->addScript('media/system/js/frontediting.js');
+			}
+
 		}
 		return $buffer;
 	}
