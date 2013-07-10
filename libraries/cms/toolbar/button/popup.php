dc6c4285c9cba28f840a67e8460b0e9d15069d96
Peter van Westen <peter@nonumber.nl>
2013-07-10 10:43:33 +0200

    Oops: Re-added my fix (got lost in merge)

--- libraries/cms/toolbar/button/popup.php
+++ libraries/cms/toolbar/button/popup.php
@@ -66,6 +66,9 @@ class JToolbarButtonPopup extends JToolbarButton
 		$html = array();
 		$html[] = $layout->render($options);
 
+		// Place modal div and scripts in a new div
+		$html[] = '</div><div class="btn-group" style="width: 0; margin: 0">';
+
 		// Build the options array for the modal
 		$params = array();
 		$params['title']  = $options['title'];
