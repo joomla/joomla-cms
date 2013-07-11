647da82af54400b91e3d4b4aae5a64733c757d81
Mihail Irintchev <m.irintchev@siteground.com>
2013-07-11 12:17:53 +0300

    Restoring changes dure to another fix

--- libraries/joomla/updater/adapters/collection.php
+++ libraries/joomla/updater/adapters/collection.php
@@ -98,11 +98,7 @@ class JUpdaterCollection extends JUpdateAdapter
 		array_push($this->_stack, $name);
 		$tag = $this->_getStackLocation();
 		// Reset the data
-		if (isset($this->$tag))
-		{	
-			$this->$tag->_data = "";
-		}
-		
+		eval('$this->' . $tag . '->_data = "";');	
 		switch ($name)
 		{
 			case 'CATEGORY':
