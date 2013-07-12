aa865f63524680c1351f22ae86af3ab63e0337db
Víctor Escobar <>
2013-07-12 15:50:23 +0200

    Fix for com_users modal window error:
    Joomla Tracker Error:
    [#31438] News Feed Manager: News Feed Page. --> Sorting is not working.

--- administrator/components/com_users/views/users/tmpl/modal.php
+++ administrator/components/com_users/views/users/tmpl/modal.php
@@ -12,6 +12,7 @@ defined('_JEXEC') or die;
 JHtml::addIncludePath(JPATH_COMPONENT.'/helpers/html');
 
 JHtml::_('bootstrap.tooltip');
+JHtml::_('behavior.keepalive');
 JHtml::_('formbehavior.chosen', 'select');
 
 $input     = JFactory::getApplication()->input;
