9e5cbca5ff89a0f3a79a960f36e4378d02cae066
landor <landor@aught.org>
2013-07-13 01:03:45 -0400

    fix problems with memcache session storage

--- libraries/joomla/session/storage/memcache.php
+++ libraries/joomla/session/storage/memcache.php
@@ -33,8 +33,6 @@ class JSessionStorageMemcache extends JSessionStorage
 			throw new RuntimeException('Memcache Extension is not available', 404);
 		}
 
-		parent::__construct($options);
-
 		$config = JFactory::getConfig();
 
 		// This will be an array of loveliness
@@ -45,6 +43,8 @@ class JSessionStorageMemcache extends JSessionStorage
 				'port' => $config->get('memcache_server_port', 11211)
 			)
 		);
+
+		parent::__construct($options);
 	}
 
 	/**
@@ -56,7 +56,7 @@ class JSessionStorageMemcache extends JSessionStorage
 	 */
 	public function register()
 	{
-		ini_set('session.save_path', $this->_servers['host'] . ':' . $this->_servers['port']);
+		ini_set('session.save_path', $this->_servers[0]['host'] . ':' . $this->_servers[0]['port']);
 		ini_set('session.save_handler', 'memcache');
 	}
 
