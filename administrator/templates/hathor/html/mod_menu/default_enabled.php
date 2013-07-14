562f861b10358c6344e379f5aa80e77827a9dc06
zero-24 <zero.mail@gmx.de>
2013-07-14 13:43:33 +0200

    Update default_enabled.php
    Tracker: #31462
    http://joomlacode.org/gf/project/joomla/tracker/?action=TrackerItemEdit&tracker_item_id=31462

--- administrator/templates/hathor/html/mod_menu/default_enabled.php
+++ administrator/templates/hathor/html/mod_menu/default_enabled.php
@@ -20,7 +20,59 @@ $lang = JFactory::getLanguage();
 // Site SubMenu
 //
 $menu->addChild(
-	new JMenuNode(JText::_('MOD_MENU_CONTROL_PANEL'), 'index.php', 'class:cpanel'), true
+	new JMenuNode(JText::_('JSITE'), '#'), true
+);
+$menu->addChild(
+	new JMenuNode(JText::_('MOD_MENU_CONTROL_PANEL'), 'index.php', 'class:cpanel')
+);
+
+$menu->addSeparator();
+
+$menu->addChild(
+	new JMenuNode(JText::_('MOD_MENU_USER_PROFILE'), 'index.php?option=com_admin&task=profile.edit&id='.$user->id, 'class:profile')
+);
+$menu->addSeparator();
+
+if ($user->authorise('core.admin'))
+{
+	$menu->addChild(new JMenuNode(JText::_('MOD_MENU_CONFIGURATION'), 'index.php?option=com_config', 'class:config'));
+	$menu->addSeparator();
+}
+
+$chm = $user->authorise('core.admin', 'com_checkin');
+$cam = $user->authorise('core.manage', 'com_cache');
+
+if ($chm || $cam )
+{
+	$menu->addChild(
+		new JMenuNode(JText::_('MOD_MENU_MAINTENANCE'), 'index.php?option=com_checkin', 'class:maintenance'), true
+	);
+
+	if ($chm)
+	{
+		$menu->addChild(new JMenuNode(JText::_('MOD_MENU_GLOBAL_CHECKIN'), 'index.php?option=com_checkin', 'class:checkin'));
+		$menu->addSeparator();
+	}
+	if ($cam)
+	{
+		$menu->addChild(new JMenuNode(JText::_('MOD_MENU_CLEAR_CACHE'), 'index.php?option=com_cache', 'class:clear'));
+		$menu->addChild(new JMenuNode(JText::_('MOD_MENU_PURGE_EXPIRED_CACHE'), 'index.php?option=com_cache&view=purge', 'class:purge'));
+	}
+
+	$menu->getParent();
+}
+
+$menu->addSeparator();
+if ($user->authorise('core.admin'))
+{
+	$menu->addChild(
+		new JMenuNode(JText::_('MOD_MENU_SYSTEM_INFORMATION'), 'index.php?option=com_admin&view=sysinfo', 'class:info')
+	);
+	$menu->addSeparator();
+}
+
+$menu->addChild(
+	new JMenuNode(JText::_('MOD_MENU_LOGOUT'), JRoute::_('index.php?option=com_login&task=logout&'. JSession::getFormToken() .'=1'), 'class:logout')
 );
 
 $menu->getParent();
@@ -341,41 +393,3 @@ if ($showhelp == 1)
 	);
 	$menu->getParent();
 }
-//
-// Admin Settingss Submenu
-//
-$su = $user->authorise('core.admin');
-$cam = $user->authorise('core.manage', 'com_cache');
-$cim = $user->authorise('core.manage', 'com_checkin');
-
-	$menu->addChild(new JMenuNode(JText::_('MOD_MENU_SETTINGS'), '#'), true);
-
-	if ($su):
-		$menu->addChild(
-		new JMenuNode(JText::_('MOD_MENU_CONFIGURATION'), 'index.php?option=com_config', 'class:config')
-		);
-		$menu->addChild(
-		new JMenuNode(JText::_('MOD_MENU_SYSTEM_INFORMATION'), 'index.php?option=com_admin&view=sysinfo', 'class:info')
-	);
-	endif;
-	if  ($cam):
-		$menu->addChild(
-		new JMenuNode(JText::_('MOD_MENU_CLEAR_CACHE'), 'index.php?option=com_cache', 'class:clear')
-		);
-		$menu->addChild(
-		new JMenuNode(JText::_('MOD_MENU_PURGE_EXPIRED_CACHE'), 'index.php?option=com_cache&view=purge', 'class:purge')
-		);
-	endif;
-	if  ($cim):
-		$menu->addChild(
-		new JMenuNode(JText::_('MOD_MENU_GLOBAL_CHECKIN'), 'index.php?option=com_checkin', 'class:checkin')
-		);
-	endif;
-		$menu->addChild(
-		new JMenuNode(JText::_('MOD_MENU_USER_PROFILE'), 'index.php?option=com_admin&task=profile.edit&id='. $user->id, 'class:profile')
-		);
-		$menu->addChild(
-		new JMenuNode(JText::_('MOD_MENU_LOGOUT'), 'index.php?option=com_login&task=logout&'. JSession::getFormToken() .'=1', 'class:logout')
-		);
-
-	$menu->getParent();
