e462d8e61805d4f635f99f6278589e686cee4049
Roland Soos <roland@nextendweb.com>
2013-07-15 12:00:04 +0200

    I think there should be set the pointer to the end as plugin indexing will be unsynced with the methods array.
    http://joomlacode.org/gf/project/joomla/tracker/?action=TrackerItemEdit&tracker_id=8103&tracker_item_id=31072

--- libraries/joomla/event/dispatcher.php
+++ libraries/joomla/event/dispatcher.php
@@ -227,6 +227,7 @@ class JEventDispatcher extends JObject
 
 			$this->_observers[] = $observer;
 			$methods = array_diff(get_class_methods($observer), get_class_methods('JPlugin'));
+			end($this->_observers);
 		}
 
 		$key = key($this->_observers);
