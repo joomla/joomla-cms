9bafa82d34f14eecefe50313c4b4f3933af5f1bd
Elin Waring <elin.waring@gmail.com>
2013-07-17 15:07:30 -0400

    Eliminate duplicate code in category.feed.php files.

--- libraries/cms/helper/route.php
+++ libraries/cms/helper/route.php
@@ -146,7 +146,7 @@ class JHelperRoute
 		{
 			self::$lookup[$language] = array();
 
-			$component = JComponentHelper::getComponent($this->extension);
+			$component = JComponentHelper::getComponent($app->input->getString('option'));
 
 			$attributes = array('component_id');
 			$values = array($component->id);
@@ -215,6 +215,155 @@ class JHelperRoute
 		// If not found, return language specific home link
 		$default = $menus->getDefault($language);
 
+		return !empty($default->id) ? $default->id : null;
+	}
+
+	public static function getCategoryRoute($catid, $language = 0)
+	{
+		if ($catid instanceof JCategoryNode)
+		{
+			$id = $catid->id;
+			$category = $catid;
+		}
+		else
+		{
+			$id = (int) $catid;
+			$category = JCategories::getInstance('Content')->get($id);
+		}
+
+		if ($id < 1)
+		{
+			$link = '';
+		}
+		else
+		{
+			$link = 'index.php?option=com_content&view=category&id='.$id;
+
+			$needles = array(
+					'category' => array($id)
+			);
+
+			if ($language && $language != "*" && JLanguageMultilang::isEnabled())
+			{
+				$db		= JFactory::getDbo();
+				$query	= $db->getQuery(true)
+				->select('a.sef AS sef')
+				->select('a.lang_code AS lang_code')
+				->from('#__languages AS a');
+
+				$db->setQuery($query);
+				$langs = $db->loadObjectList();
+				foreach ($langs as $lang)
+				{
+					if ($language == $lang->lang_code)
+					{
+						$link .= '&lang='.$lang->sef;
+						$needles['language'] = $language;
+					}
+				}
+			}
+
+			if ($item = self::_findItem($needles))
+			{
+				$link .= '&Itemid='.$item;
+			}
+			else
+			{
+				//Create the link
+				if ($category)
+				{
+					$catids = array_reverse($category->getPath());
+					$needles['category'] = $catids;
+					$needles['categories'] = $catids;
+
+					if ($item = self::_findItem($needles))
+					{
+						$link .= '&Itemid='.$item;
+					}
+					elseif ($item = self::_findItem())
+					{
+						$link .= '&Itemid='.$item;
+					}
+				}
+			}
+		}
+
+		return $link;
+	}
+
+
+	protected static function _findItem($needles = null)
+	{
+		$app		= JFactory::getApplication();
+		$menus		= $app->getMenu('site');
+		$language	= isset($needles['language']) ? $needles['language'] : '*';
+
+		// Prepare the reverse lookup array.
+		if (!isset(self::$lookup[$language]))
+		{
+			self::$lookup[$language] = array();
+
+			$component	= JComponentHelper::getComponent($app->input->getString('option'));
+
+			$attributes = array('component_id');
+			$values = array($component->id);
+
+			if ($language != '*')
+			{
+				$attributes[] = 'language';
+				$values[] = array($needles['language'], '*');
+			}
+
+			$items		= $menus->getItems($attributes, $values);
+
+			foreach ($items as $item)
+			{
+				if (isset($item->query) && isset($item->query['view']))
+				{
+					$view = $item->query['view'];
+					if (!isset(self::$lookup[$language][$view]))
+					{
+						self::$lookup[$language][$view] = array();
+					}
+					if (isset($item->query['id'])) {
+
+						// here it will become a bit tricky
+						// language != * can override existing entries
+						// language == * cannot override existing entries
+						if (!isset(self::$lookup[$language][$view][$item->query['id']]) || $item->language != '*')
+						{
+							self::$lookup[$language][$view][$item->query['id']] = $item->id;
+						}
+					}
+				}
+			}
+		}
+
+		if ($needles)
+		{
+			foreach ($needles as $view => $ids)
+			{
+				if (isset(self::$lookup[$language][$view]))
+				{
+					foreach ($ids as $id)
+					{
+						if (isset(self::$lookup[$language][$view][(int) $id]))
+						{
+							return self::$lookup[$language][$view][(int) $id];
+						}
+					}
+				}
+			}
+		}
+
+		$active = $menus->getActive();
+		if ($active && $active->component == $content && ($active->language == '*' || !JLanguageMultilang::isEnabled()))
+		{
+			return $active->id;
+		}
+
+		// if not found, return language specific home link
+		$default = $menus->getDefault($language);
 		return !empty($default->id) ? $default->id : null;
 	}
 }
