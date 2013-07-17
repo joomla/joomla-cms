59625adf22d4302dd45024c75219f5e61ba44df4
Alex Marin <alex.ukf@gmail.com>
2013-07-17 16:58:11 +0300

    Amazons3 getBucketCors

--- libraries/joomla/amazons3/operations/buckets/get.php
+++ libraries/joomla/amazons3/operations/buckets/get.php
@@ -46,7 +46,7 @@ class JAmazons3OperationsBucketsGet extends JAmazons3OperationsBuckets
 	}
 
 	/**
-	 * Creates the request for getting a bucket acl and returns the response from Amazon
+	 * Creates the request for getting a bucket's acl and returns the response from Amazon
 	 *
 	 * @param   string  $bucket  The bucket name
 	 *
@@ -57,6 +57,34 @@ class JAmazons3OperationsBucketsGet extends JAmazons3OperationsBuckets
 	public function getBucketAcl($bucket)
 	{
 		$url = "https://" . $bucket . "." . $this->options->get("api.url") . "/?acl";
+		$headers = array(
+			"Date" => date("D, d M Y H:i:s O"),
+		);
+		$authorization = $this->createAuthorization("GET", $url, $headers);
+		$headers["Authorization"] = $authorization;
+
+		// Send the http request
+		$response = $this->client->get($url, $headers);
+
+		// Process the response
+		$response_body = $this->processResponse($response);
+
+		return $response_body;
+	}
+
+	/**
+	 * Creates the request for getting a bucket's cors configuration information set
+	 * and returns the response from Amazon
+	 *
+	 * @param   string  $bucket  The bucket name
+	 *
+	 * @return string  The response body
+	 *
+	 * @since   ??.?
+	 */
+	public function getBucketCors($bucket)
+	{
+		$url = "https://" . $bucket . "." . $this->options->get("api.url") . "/?cors";
 		$headers = array(
 			"Date" => date("D, d M Y H:i:s O"),
 		);
