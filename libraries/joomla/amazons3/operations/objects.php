02c0ff39cbd771cfb6a2194f0fcf3df770233348
Alex Marin <alex.ukf@gmail.com>
2013-07-17 12:41:03 +0300

    Amazons3 JAmazons3OperationsObjects

--- libraries/joomla/amazons3/operations/objects.php
+++ libraries/joomla/amazons3/operations/objects.php
@@ -18,4 +18,68 @@ defined('JPATH_PLATFORM') or die;
  */
 class JAmazons3OperationsObjects extends JAmazons3Object
 {
+	/**
+	 * @var    JAmazons3OperationsObjectsDelete  Amazons3 API object for DELETE operations on objects.
+	 * @since  ??.?
+	 */
+	protected $delete;
+
+	/**
+	 * @var    JAmazons3OperationsObjectsGet  Amazons3 API object for GET operations on objects.
+	 * @since  ??.?
+	 */
+	protected $get;
+
+	/**
+	 * @var    JAmazons3OperationsObjectsGet  Amazons3 API object for HEAD operations on objects.
+	 * @since  ??.?
+	 */
+	protected $head;
+
+	/**
+	 * @var    JAmazons3OperationsObjectsPut  Amazons3 API object for OPTIONS operations on objects.
+	 * @since  ??.?
+	 */
+	protected $options;
+
+	/**
+	 * @var    JAmazons3OperationsObjectsPut  Amazons3 API object for POST operations on objects.
+	 * @since  ??.?
+	 */
+	protected $post;
+
+	/**
+	 * @var    JAmazons3OperationsObjectsPut  Amazons3 API object for PUT operations on objects.
+	 * @since  ??.?
+	 */
+	protected $put;
+
+	/**
+	 * Magic method to lazily create API objects
+	 *
+	 * @param   string  $name  Name of property to retrieve.
+	 *
+	 * @return  JAmazons3Object  Amazons3 API object
+	 *
+	 * @since   ??.?
+	 * @throws  InvalidArgumentException
+	 */
+	public function __get($name)
+	{
+		$class = 'JAmazons3OperationsObjects' . ucfirst($name);
+
+		if (class_exists($class))
+		{
+			if (false == isset($this->$name))
+			{
+				$this->$name = new $class($this->options, $this->client);
+			}
+
+			return $this->$name;
+		}
+
+		throw new InvalidArgumentException(
+			sprintf('Argument %s produced an invalid class name: %s', $name, $class)
+		);
+	}
 }
