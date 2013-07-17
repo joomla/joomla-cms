47bd909766ceb8809a186ec36aacbee0d1cb79a9
Alex Marin <alex.ukf@gmail.com>
2013-07-17 13:19:24 +0300

    Amazons3 getBucket

--- libraries/joomla/amazons3/operations/buckets/get.php
+++ libraries/joomla/amazons3/operations/buckets/get.php
@@ -8,3 +8,40 @@
  */
 
 defined('JPATH_PLATFORM') or die;
+
+/**
+ * Defines the GET operations on buckets
+ *
+ * @package     Joomla.Platform
+ * @subpackage  Amazons3
+ * @since       ??.?
+ */
+class JAmazons3OperationsBucketsGet extends JAmazons3OperationsBuckets
+{
+	/**
+	 * Creates the request for getting a bucket and returns the response from Amazon
+	 *
+	 * @param   string  $bucket  The bucket name
+	 *
+	 * @return string  The response body
+	 *
+	 * @since   ??.?
+	 */
+	public function getBucket($bucket)
+	{
+		$url = "https://" . $bucket . "." . $this->options->get("api.url") . "/";
+		$headers = array(
+			"Date" => date("D, d M Y H:i:s O"),
+		);
+		$authorization = $this->createAuthorization("GET", $url, $headers);
+		$headers["Authorization"] = $authorization;
+
+		// Send the http request
+		$response = $this->client->get($url, $headers);
+
+		// Process the response
+		$response_body = $this->processResponse($response);
+
+		return $response_body;
+	}
+}
