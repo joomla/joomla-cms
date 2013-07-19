3dd3f6dec84ac433b0c29a2722ec75ace996382a
Elin Waring <elin.waring@gmail.com>
2013-07-19 10:27:54 -0400

    Add tags field back to hathor edit layouts.

--- administrator/templates/hathor/html/com_categories/category/edit.php
+++ administrator/templates/hathor/html/com_categories/category/edit.php
@@ -71,15 +71,11 @@ JHtml::_('behavior.keepalive');
 						<?php echo $this->form->getInput('language'); ?>
 					</li>
 					<!-- Tag field -->
-					<?php foreach ($this->get('form')->getFieldset('jmetadata') as $field) : ?>
-						<?php if ($field->name == 'jform[metadata][tags][]') : ?>
-							<li>
-								<?php echo $field->label; ?>
-								<?php echo $field->input; ?>
+					<li><?php echo $this->form->getLabel('tags'); ?>
+					<div class="is-tagbox">
+					<?php echo $this->form->getInput('tags'); ?>
+					</div></li>
 
-							</li>
-						<?php endif; ?>
-					<?php endforeach; ?>
 					<li>
 						<?php echo $this->form->getLabel('id'); ?>
 						<?php echo $this->form->getInput('id'); ?>
