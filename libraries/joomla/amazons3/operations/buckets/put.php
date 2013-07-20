bf4b6c035ce533de1b4b61bb4bc6a14418666c29
Alex Marin <alex.ukf@gmail.com>
2013-07-21 02:32:08 +0300

    Amazons3 basic request for creating a new bucket

--- libraries/joomla/amazons3/operations/buckets/put.php
+++ libraries/joomla/amazons3/operations/buckets/put.php
@@ -18,4 +18,47 @@ defined('JPATH_PLATFORM') or die;
  */
 class JAmazons3OperationsBucketsPut extends JAmazons3OperationsBuckets
 {
+	/**
+	 * Creates the request for creating a bucket and returns the response from Amazon
+	 *
+	 * @param   string  $bucket        The bucket name
+	 * @param   string  $bucketRegion  The bucket region (default: US Standard)
+	 *
+	 * @return string  The response body
+	 *
+	 * @since   ??.?
+	 */
+	public function putBucket($bucket, $bucketRegion = "")
+	{
+		$url = "https://" . $bucket . "." . $this->options->get("api.url") . "/";
+		$headers = array();
+		$content = "";
+
+		if ($bucketRegion != "")
+		{
+			$content = "<CreateBucketConfiguration xmlns=\"http://s3.amazonaws.com/doc/2006-03-01/\">"
+				. "<LocationConstraint>" . $bucketRegion . "</LocationConstraint>"
+				. "</CreateBucketConfiguration>";
+		}
+
+		// Create the headers
+		if ($bucketRegion != "")
+		{
+			$headers["Content-type"] = "application/x-www-form-urlencoded; charset=utf-8";
+			$headers["Content-Length"] = strlen($content);
+		}
+
+		$headers["Date"] = date("D, d M Y H:i:s O");
+		$authorization = $this->createAuthorization("PUT", $url, $headers);
+		$headers["Authorization"] = $authorization;
+		unset($headers["Content-type"]);
+
+		// Send the http request
+		$response = $this->client->put($url, $content, $headers);
+
+		// Process the response
+		$response_body = $this->processResponse($response);
+
+		return $response_body;
+	}
 }
