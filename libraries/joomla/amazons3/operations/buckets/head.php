6e46a5fcdef2af0dd4e5f6b07f45ebefe0c651df
Alex Marin <alex.ukf@gmail.com>
2013-07-20 03:03:54 +0300

    Amazons3 HEAD for buckets

--- libraries/joomla/amazons3/operations/buckets/head.php
+++ libraries/joomla/amazons3/operations/buckets/head.php
@@ -8,3 +8,42 @@
  */
 
 defined('JPATH_PLATFORM') or die;
+
+/**
+ * Defines the HEAD operations on buckets
+ *
+ * @package     Joomla.Platform
+ * @subpackage  Amazons3
+ * @since       ??.?
+ */
+class JAmazons3OperationsBucketsHead extends JAmazons3OperationsBuckets
+{
+	/**
+	 * Creates a request to determine if a bucket exists and you have permission to access it.
+	 *
+	 * @param   string  $bucket  The bucket name
+	 *
+	 * @return string  The response body
+	 *
+	 * @since   ??.?
+	 */
+	public function headBucket($bucket)
+	{
+		$url = "https://" . $bucket . "." . $this->options->get("api.url") . "/";
+
+		// Create the headers
+		$headers = array(
+			"Date" => date("D, d M Y H:i:s O"),
+		);
+		$authorization = $this->createAuthorization("HEAD", $url, $headers);
+		$headers["Authorization"] = $authorization;
+
+		// Send the http request
+		$response = $this->client->head($url, $headers);
+
+		// Process the response
+		$response_body = $this->processResponse($response);
+
+		return $response_body;
+	}
+}
