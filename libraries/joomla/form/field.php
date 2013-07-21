1f9c17cb7c7f951ee6e3526d6c8ce05a2305cd99
Achal-Aggarwal <theachalaggarwal@gmail.com>
2013-07-21 22:18:29 +0530

    Adding new attributes in __get method for accessibility.

--- libraries/joomla/form/field.php
+++ libraries/joomla/form/field.php
@@ -324,6 +324,13 @@ abstract class JFormField
 			case 'labelClass':
 			case 'fieldname':
 			case 'group':
+			case 'disabled':
+			case 'readonly':
+			case 'autofocus':
+			case 'autocomplete':
+			case 'spellcheck':
+			case 'hint':
+			case 'dirname':
 				return $this->$name;
 
 			case 'input':
