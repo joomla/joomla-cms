c18145fee262fb9469e615e46e2518c7b18eb42b
Alex Marin <alex.ukf@gmail.com>
2013-07-24 02:26:38 +0300

    Amazons3 deleteBucket + unit test

--- libraries/joomla/amazons3/operations/buckets/delete.php
+++ libraries/joomla/amazons3/operations/buckets/delete.php
@@ -18,4 +18,32 @@ defined('JPATH_PLATFORM') or die;
  */
 class JAmazons3OperationsBucketsDelete extends JAmazons3OperationsBuckets
 {
+	/**
+	 * Deletes the bucket named in the URI
+	 *
+	 * @param   string  $bucket  The bucket name
+	 *
+	 * @return string  The response body
+	 *
+	 * @since   ??.?
+	 */
+	public function deleteBucket($bucket)
+	{
+		$url = "https://" . $bucket . "." . $this->options->get("api.url") . "/";
+
+		// Send the request and process the response
+		$headers = array(
+			"Date" => date("D, d M Y H:i:s O"),
+		);
+		$authorization = $this->createAuthorization("DELETE", $url, $headers);
+		$headers["Authorization"] = $authorization;
+
+		// Send the http request
+		$response = $this->client->delete($url, $headers);
+
+		// Process the response
+		$response_body = $this->processResponse($response);
+
+		return $response_body;
+	}
 }
