200782b175b8bcb09101f71b6b8a6d0bbec31059
Alex Marin <alex.ukf@gmail.com>
2013-07-24 16:18:25 +0300

    Amazons3 deleteMultipleObjects + unit tests

--- libraries/joomla/amazons3/operations/objects/post.php
+++ libraries/joomla/amazons3/operations/objects/post.php
@@ -18,4 +18,75 @@ defined('JPATH_PLATFORM') or die;
  */
 class JAmazons3OperationsObjectsPost extends JAmazons3OperationsObjects
 {
+	/**
+	 * Deletes multiple objects from a bucket
+	 *
+	 * @param   string  $bucket     The bucket name
+	 * @param   array   $objects    An array of objects to be deleted
+	 * @param   array   $quiet      In quiet mode the response includes only keys
+	 *                              where the delete operation encountered an error
+	 * @param   string  $serialNr   The serial number is generated using either a hardware or
+	 *                              a virtual MFA device. Required for MfaDelete
+	 * @param   string  $tokenCode  Also required for MfaDelete
+	 *
+	 * @return string  The response body
+	 *
+	 * @since   ??.?
+	 */
+	public function deleteMultipleObjects($bucket, $objects, $quiet = false, $serialNr = null, $tokenCode = null)
+	{
+		$url = "https://" . $bucket . "." . $this->options->get("api.url") . "/?delete";
+		$content = "";
+		$headers = array(
+			"Date" => date("D, d M Y H:i:s O"),
+		);
+
+		if (is_array($objects))
+		{
+			$content = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+				. "<Delete>\n";
+
+			if ($quiet)
+			{
+				$content .= "<Quiet>true</Quiet>\n";
+			}
+
+			foreach ($objects as $object)
+			{
+				$content .= "<Object>\n";
+
+				foreach ($object as $key => $value)
+				{
+					$content .= "<" . $key . ">"
+						. $value
+						. "</" . $key . ">\n";
+				}
+
+				$content .= "</Object>\n";
+			}
+
+			$content .= "</Delete>";
+		}
+
+		if (! is_null($serialNr))
+		{
+			$headers["x-amz-mfa"] = $serialNr . " " . $tokenCode;
+		}
+
+		// Set the content related headers
+		$headers["Content-type"] = "application/x-www-form-urlencoded; charset=utf-8";
+		$headers["Content-Length"] = strlen($content);
+		$headers["Content-MD5"] = base64_encode(md5($content, true));
+		$authorization = $this->createAuthorization("POST", $url, $headers);
+		$headers["Authorization"] = $authorization;
+		unset($headers["Content-type"]);
+
+		// Send the http request
+		$response = $this->client->post($url, $content, $headers);
+
+		// Process the response
+		$response_body = $this->processResponse($response);
+
+		return $response_body;
+	}
 }
