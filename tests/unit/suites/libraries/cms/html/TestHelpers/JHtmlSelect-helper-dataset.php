fd43bbb45493c6ead7a3d447b02519d580be2682
Achal-Aggarwal <theachalaggarwal@gmail.com>
2013-07-30 02:20:30 +0530

    Adding data for testOptions and adding testOption with data provider.

--- tests/unit/suites/libraries/cms/html/TestHelpers/JHtmlSelect-helper-dataset.php
+++ tests/unit/suites/libraries/cms/html/TestHelpers/JHtmlSelect-helper-dataset.php
@@ -27,5 +27,116 @@ class JHtmlSelectTest_DataSet
 				),
 			),
 		),
+		array(
+			"<option value=\"1\" disabled=\"disabled\">&nbsp;Test</option>\n",
+			array(
+				array(
+					'value' => '1',
+					'text' => '&nbsp;Test',
+					'disable' => true,
+				),
+			),
+		),
+		array(
+			"<option value=\"1\">&nbsp;Test</option>\n",
+			array(
+				array(
+					'optionValue' => '1',
+					'optionText' => '&nbsp;Test',
+				),
+			),
+			array(
+				'option.key' => 'optionValue',
+				'option.text' => 'optionText'
+			),
+		),
+		array(
+			"<option value=\"1\" id=\"myId\" label=\"My Label\" readonly>&nbsp;Test</option>\n",
+			array(
+				array(
+					'value' => '1',
+					'text' => '&nbsp;Test -         ',
+					'label' => 'My Label',
+					'id' => 'myId',
+					'extraAttrib' => 'readonly',
+				),
+			),
+			array(
+				'option.label' => 'label',
+				'option.id' => 'id',
+				'option.attr' => 'extraAttrib',
+			),
+		),
+		array(
+			"<option value=\"1\" class=\"foo bar\" style=\"color:red;\">&nbsp;Test</option>\n",
+			array(
+				array(
+					'value' => '1',
+					'text' => '&nbsp;Test -         ',
+					'label' => 'My Label',
+					'id' => 'myId',
+					'attrs' => array('class' => "foo bar",'style' => 'color:red;',),
+				),
+			),
+			array(
+				'option.attr' => 'attrs',
+			),
+		),
+	);
+
+	static public $optionTest = array(
+		// @todo remove: array($expected, $value, $text = '', $optKey = 'value', $optText = 'text', $disable = false)
+		array(
+			array(
+				'value' => 'optionValue',
+				'text' => 'optionText',
+				'disable' => false,
+			),
+			'optionValue',
+			'optionText'
+		),
+		array(
+			array(
+				'fookey' => 'optionValue',
+				'bartext' => 'optionText',
+				'disable' => false,
+			),
+			'optionValue',
+			'optionText',
+			'fookey',
+			'bartext',
+		),
+		array(
+			array(
+				'value' => 'optionValue',
+				'text' => 'optionText',
+				'disable' => true,
+			),
+			'optionValue',
+			'optionText',
+			'value',
+			'text',
+			true,
+		),
+		array(
+			array(
+				'optionValue' => 'optionValue',
+				'optionText' => 'optionText',
+				'foobarDisabled' => false,
+				'lebal' => 'My Label',
+				'class' => 'foo bar',
+			),
+			'optionValue',
+			'optionText',
+			array(
+				'option.disable' => 'foobarDisabled',
+				'option.attr' => 'class',
+				'attr' => 'foo bar',
+				'option.label' => 'lebal',
+				'label' => "My Label",
+				'option.key' => 'optionValue',
+				'option.text' => 'optionText',
+			),
+		),
 	);
 }
