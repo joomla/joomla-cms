8ff6dc989783ff12a2eeee790aaff76ba81c2063
Elin Waring <elin.waring@gmail.com>
2013-08-01 16:56:33 -0400

    Change mb functions to JString.

--- components/com_search/views/search/view.html.php
+++ components/com_search/views/search/view.html.php
@@ -168,15 +168,15 @@ class SearchViewSearch extends JViewLegacy
 
 				foreach ($searchwords as $hlword)
 				{
-					if (($pos = mb_strpos($srow, strtolower(SearchHelper::remove_accents($hlword)))) !== false)
+					if (($pos = JString::strpos($srow, strtolower(SearchHelper::remove_accents($hlword)))) !== false)
 					{
-						$pos += $cnt++ * mb_strlen($hl1 . $hl2);
+						$pos += $cnt++ * JString::strlen($hl1 . $hl2);
 
 						// iconv transliterates '€' to 'EUR'
 						// TODO: add other expanding translations?
 						$eur_compensation = $pos > 0 ? substr_count($row, "\xE2\x82\xAC", 0, $pos) * 2 : 0;
 						$pos -= $eur_compensation;
-						$row = mb_substr($row, 0, $pos) . $hl1 . mb_substr($row, $pos, mb_strlen($hlword)) . $hl2 . mb_substr($row, $pos + mb_strlen($hlword));
+						$row = JString::substr($row, 0, $pos) . $hl1 . JString::substr($row, $pos, JString::strlen($hlword)) . $hl2 . JString::substr($row, $pos + JString::strlen($hlword));
 					}
 				}
 
