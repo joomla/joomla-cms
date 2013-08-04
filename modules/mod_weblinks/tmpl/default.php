084feea45468e6c0cc8515f46df18c211a4bb5cc
wilsonge <georgejameswilson@googlemail.com>
2013-08-04 22:04:11 +0100

    Fix PHPCS in mod_weblinks

--- modules/mod_weblinks/tmpl/default.php
+++ modules/mod_weblinks/tmpl/default.php
@@ -10,38 +10,46 @@
 defined('_JEXEC') or die;
 ?>
 <ul class="weblinks<?php echo $moduleclass_sfx; ?>">
-<?php foreach ($list as $item) :	?>
-<li>
 	<?php
-	$link = $item->link;
-	switch ($params->get('target', 3))
-	{
-		case 1:
-			// open in a new window
-			echo '<a href="'. $link .'" target="_blank" rel="'.$params->get('follow', 'nofollow').'">'.
-			htmlspecialchars($item->title, ENT_COMPAT, 'UTF-8') .'</a>';
-			break;
+	foreach ($list as $item) :
+		?>
+		<li>
+			<?php
+			$link = $item->link;
 
-		case 2:
-			// open in a popup window
-			echo "<a href=\"#\" onclick=\"window.open('". $link ."', '', 'toolbar=no,location=no,status=no,menubar=no,scrollbars=yes,resizable=yes,width=780,height=550'); return false\">".
-				htmlspecialchars($item->title, ENT_COMPAT, 'UTF-8') .'</a>';
-			break;
+			switch ($params->get('target', 3))
+			{
+				case 1:
+					// Open in a new window
+					echo '<a href="' . $link . '" target="_blank" rel="' . $params->get('follow', 'nofollow') . '">' .
+						htmlspecialchars($item->title, ENT_COMPAT, 'UTF-8') . '</a>';
+					break;
 
-		default:
-			// open in parent window
-			echo '<a href="'. $link .'" rel="'.$params->get('follow', 'nofollow').'">'.
-				htmlspecialchars($item->title, ENT_COMPAT, 'UTF-8') .'</a>';
-			break;
-	}
+				case 2:
+					// Open in a popup window
+					echo "<a href=\"#\" onclick=\"window.open('" . $link . "', '', 'toolbar=no,location=no,status=no,menubar=no,scrollbars=yes,resizable=yes,width=780,height=550'); return false\">" .
+						htmlspecialchars($item->title, ENT_COMPAT, 'UTF-8') . '</a>';
+					break;
+
+				default:
+					// Open in parent window
+					echo '<a href="' . $link . '" rel="' . $params->get('follow', 'nofollow') . '">' .
+						htmlspecialchars($item->title, ENT_COMPAT, 'UTF-8') . '</a>';
+					break;
+			}
+
+			if ($params->get('description', 0))
+			{
+				echo nl2br($item->description);
+			}
+
+			if ($params->get('hits', 0))
+			{
+				echo '(' . $item->hits . ' ' . JText::_('MOD_WEBLINKS_HITS') . ')';
+			}
+			?>
+		</li>
+	<?php
+	endforeach;
 	?>
-	<?php if ($params->get('description', 0)) : ?>
-		<?php echo nl2br($item->description); ?>
-	<?php endif; ?>
-
-	<?php if ($params->get('hits', 0)) : ?>
-		<?php echo '(' . $item->hits . ' ' . JText::_('MOD_WEBLINKS_HITS') . ')'; ?>
-	<?php endif; ?>
-</li>
-<?php endforeach; ?>
 </ul>
