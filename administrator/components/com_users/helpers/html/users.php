c593e2cf065f314ff85eb5f6f96004987633ffe5
mariopro <mcruip@sapo.pt>
2013-08-05 11:14:32 +0100

    Standardized button for notes filtering
    Replaces the old icon with the jui css class btn to the filter note link. Note: the method is only used in the admin com_users users list view, so no implications elsewhere.

--- administrator/components/com_users/helpers/html/users.php
+++ administrator/components/com_users/helpers/html/users.php
@@ -66,7 +66,7 @@ class JHtmlUsers
 	 * @param   integer  $count   The number of notes for the user
 	 * @param   integer  $userId  The user ID
 	 *
-	 * @return	string  A link to apply a filter
+	 * @return  string  A button link to apply a filter
 	 *
 	 * @since   2.5
 	 */
@@ -78,9 +78,11 @@ class JHtmlUsers
 		}
 
 		$title = JText::_('COM_USERS_FILTER_NOTES');
+		
+		$html  = '<a class="btn btn-micro hasTooltip" href="' . JRoute::_('index.php?option=com_users&view=notes&filter_search=uid:' . (int)$userId) . '" ';
+		$html .= 'title="' . $title . '"><i class="icon icon-filter text-info"></i></a>';
 
-		return '<a href="' . JRoute::_('index.php?option=com_users&view=notes&filter_search=uid:' . (int) $userId) . '">'
-			. JHtml::_('image', 'admin/filter_16.png', 'COM_USERS_NOTES', array('title' => $title), true) . '</a>';
+		return $html;
 	}
 
 	/**
