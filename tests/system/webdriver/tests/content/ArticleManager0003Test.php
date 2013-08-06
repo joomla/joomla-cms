c5cca883a5e4c8536666dbe26913baa55c8ab017
Puneet Kala <puneet.0191@gmail.com>
2013-08-06 20:16:06 +0530

    Adding Two More Batch System Tests

--- tests/system/webdriver/tests/content/ArticleManager0003Test.php
+++ tests/system/webdriver/tests/content/ArticleManager0003Test.php
@@ -162,5 +162,107 @@ class ArticleManager0003Test extends JoomlaWebdriverTestCase
 	 	$this->articleManagerPage->changeAccessLevel('Archive Module', 'Public');
 	 	$currentAccessLevel = $this->articleManagerPage->getAccessLevel('Archive Module');
 	 	$this->assertEquals('Public',$currentAccessLevel, 'Current Access Level Should have changed back to public');
-	 }		
+	 }
+	 
+	 /**
+	  * @test
+	  */
+	  public function batchCopy_BatchCopyArticle_ArticleCopied()
+	  {
+		  $this->articleManagerPage = $this->getPageObject('ArticleManagerPage');
+		  $originalCategory = 'Content Modules';
+		  //Category to which we will copy the artcile using Batch Process
+		  $newCategory = 'Park Site';
+		  $value = $this->driver->findElement(By::xPath("//tbody/tr[2]/td[4]/div/div[@class='small']"))->getText();
+		  $this->assertEquals($value,'Category: Content Modules','Initially Archive Module Must belong to Content Modules Category');
+		  $this->driver->findElement(By::xPath("//input[@id='cb1']"))->click();
+		  $this->driver->findElement(By::xPath("//div[@id='toolbar-batch']/button"))->click();
+		  $this->driver->findElement(By::xPath("//div[@id='batch_category_id_chzn']/a"))->click();
+		  $this->driver->findElement(By::xPath("//div[@id='batch_category_id_chzn']/div/div/input"))->sendKeys('Park');
+		  $this->driver->findElement(By::xPath("//div[@id='batch_category_id_chzn']//ul[@class='chzn-results']/li[contains(.,'".$newCategory."')]"))->click();
+		  $this->driver->findElement(By::xPath("//input[@id='batch[move_copy]c']"))->click();
+		  $this->driver->findElement(By::xPath("//button[contains(text(),'Process')]"))->click();
+		  $this->driver->findElement(By::xPath("//div[@id='filter_category_id_chzn']/a"))->click();
+		  $this->driver->findElement(By::xPath("//div[@id='filter_category_id_chzn']/div/div/input"))->sendKeys('Park');
+		  $this->driver->findElement(By::xPath("//div[@id='filter_category_id_chzn']//ul[@class='chzn-results']/li[contains(.,'".$newCategory."')]"))->click();
+		  $this->driver->waitForElementUntilIsPresent(By::xPath("//a[contains(text(),'Archive Module')]"),10);
+		  $value = $this->driver->findElement(By::xPath("//tbody/tr[1]/td[4]/div/div[@class='small']"))->getText();
+		  $this->assertEquals($value,'Category: Park Site','The Article Must have got copied into the new Category');
+		  $this->driver->findElement(By::xPath("//input[@id='cb0']"))->click();
+		  $this->driver->findElement(By::xPath("//div[@id='toolbar-trash']/button"))->click();
+		  $this->driver->findElement(By::xPath("//div[@id='filter_published_chzn']/a"))->click();
+		  $this->driver->findElement(By::xPath("//div[@id='filter_published_chzn']//ul[@class='chzn-results']/li[contains(.,'Trashed')]"))->click();
+		  $this->driver->waitForElementUntilIsPresent(By::xPath("//a[contains(text(),'Archive Module')]"),10);
+		  $this->driver->findElement(By::xPath("//input[@id='cb0']"))->click();
+		  $this->driver->findElement(By::xPath("//div[@id='toolbar-delete']/button"))->click();
+		  $this->driver->findElement(By::xPath("//div[@id='filter_published_chzn']/a"))->click();
+		  $this->driver->findElement(By::xPath("//div[@id='filter_published_chzn']//ul[@class='chzn-results']/li[contains(.,'Select Status')]"))->click();
+		  $this->driver->findElement(By::xPath("//div[@id='filter_category_id_chzn']/a"))->click();
+		  $this->driver->findElement(By::xPath("//div[@id='filter_category_id_chzn']/div/div/input"))->sendKeys('Select');
+		  $this->driver->findElement(By::xPath("//div[@id='filter_category_id_chzn']//ul[@class='chzn-results']/li[contains(.,'Select Category')]"))->click();
+		  $this->driver->waitForElementUntilIsPresent(By::xPath("//a[contains(text(),'Archive Module')]"),10);	
+		  //Now we will copy the article into same category using Batch Process		  
+		  $this->driver->findElement(By::xPath("//input[@id='cb1']"))->click();
+		  $this->driver->findElement(By::xPath("//div[@id='toolbar-batch']/button"))->click();
+		  $this->driver->findElement(By::xPath("//div[@id='batch_category_id_chzn']/a"))->click();
+		  $this->driver->findElement(By::xPath("//div[@id='batch_category_id_chzn']/div/div/input"))->sendKeys('Content');
+		  $this->driver->findElement(By::xPath("//div[@id='batch_category_id_chzn']//ul[@class='chzn-results']/li[contains(.,'".$originalCategory."')]"))->click();
+		  $this->driver->findElement(By::xPath("//input[@id='batch[move_copy]c']"))->click();
+		  $this->driver->findElement(By::xPath("//button[contains(text(),'Process')]"))->click();
+		  $value = $this->driver->findElement(By::xPath("//tbody/tr[3]/td[4]/div/div[@class='small']"))->getText();
+		  $this->assertEquals($value,'Category: Content Modules','The Article Must have got copied into the same Old Category');
+		  $this->driver->findElement(By::xPath("//input[@id='cb2']"))->click();
+		  $this->driver->findElement(By::xPath("//div[@id='toolbar-trash']/button"))->click();
+		  $this->driver->findElement(By::xPath("//div[@id='filter_published_chzn']/a"))->click();
+		  $this->driver->findElement(By::xPath("//div[@id='filter_published_chzn']//ul[@class='chzn-results']/li[contains(.,'Trashed')]"))->click();
+		  $this->driver->findElement(By::xPath("//input[@id='cb0']"))->click();
+		  $this->driver->findElement(By::xPath("//div[@id='toolbar-delete']/button"))->click();
+		  $this->driver->findElement(By::xPath("//div[@id='filter_published_chzn']/a"))->click();
+		  $this->driver->findElement(By::xPath("//div[@id='filter_published_chzn']//ul[@class='chzn-results']/li[contains(.,'Select Status')]"))->click();
+		  $this->driver->findElement(By::xPath("//div[@id='filter_category_id_chzn']/a"))->click();
+		  $this->driver->findElement(By::xPath("//div[@id='filter_category_id_chzn']/div/div/input"))->sendKeys('Select');
+		  $this->driver->findElement(By::xPath("//div[@id='filter_category_id_chzn']//ul[@class='chzn-results']/li[contains(.,'Select Category')]"))->click();
+	  }
+	  
+	  /**
+	  * @test
+	  */
+	  public function batchMove_BatchMoveArticle_ArticleMoved()
+	  {
+		  $this->articleManagerPage = $this->getPageObject('ArticleManagerPage');
+		  $originalCategory = 'Content Modules';
+		  //Category to which we will copy the artcile using Batch Process
+		  $newCategory = 'Languages';
+		  $value = $this->driver->findElement(By::xPath("//tbody/tr[2]/td[4]/div/div[@class='small']"))->getText();
+		  $this->assertEquals($value,'Category: Content Modules','Initially Archive Module Must belong to Content Modules Category');
+		  $this->driver->findElement(By::xPath("//input[@id='cb1']"))->click();
+		  $this->driver->findElement(By::xPath("//div[@id='toolbar-batch']/button"))->click();
+		  $this->driver->findElement(By::xPath("//div[@id='batch_category_id_chzn']/a"))->click();
+		  $this->driver->findElement(By::xPath("//div[@id='batch_category_id_chzn']/div/div/input"))->sendKeys('lang');
+		  $this->driver->findElement(By::xPath("//div[@id='batch_category_id_chzn']//ul[@class='chzn-results']/li[contains(.,'".$newCategory."')]"))->click();
+		  $this->driver->findElement(By::xPath("//input[@id='batch[move_copy]m']"))->click();
+		  $this->driver->findElement(By::xPath("//button[contains(text(),'Process')]"))->click();
+		  $this->driver->findElement(By::xPath("//div[@id='filter_category_id_chzn']/a"))->click();
+		  $this->driver->findElement(By::xPath("//div[@id='filter_category_id_chzn']/div/div/input"))->sendKeys('lang');
+		  $this->driver->findElement(By::xPath("//div[@id='filter_category_id_chzn']//ul[@class='chzn-results']/li[contains(.,'".$newCategory."')]"))->click();
+		  $this->driver->waitForElementUntilIsPresent(By::xPath("//a[contains(text(),'Archive Module')]"),10);
+		  $value = $this->driver->findElement(By::xPath("//tbody/tr[1]/td[4]/div/div[@class='small']"))->getText();
+		  $this->assertEquals($value,'Category: Languages','The Article Must have got moved into the new Category');
+		  
+		  //Move Article Back to Original Category
+		  $this->driver->findElement(By::xPath("//input[@id='cb0']"))->click();
+		  $this->driver->findElement(By::xPath("//div[@id='toolbar-batch']/button"))->click();
+		  $this->driver->findElement(By::xPath("//div[@id='batch_category_id_chzn']/a"))->click();
+		  $this->driver->findElement(By::xPath("//div[@id='batch_category_id_chzn']/div/div/input"))->sendKeys('content');
+		  $this->driver->findElement(By::xPath("//div[@id='batch_category_id_chzn']//ul[@class='chzn-results']/li[contains(.,'".$originalCategory."')]"))->click();
+		  $this->driver->findElement(By::xPath("//input[@id='batch[move_copy]m']"))->click();
+		  $this->driver->findElement(By::xPath("//button[contains(text(),'Process')]"))->click();
+		  $this->driver->findElement(By::xPath("//div[@id='filter_published_chzn']/a"))->click();
+		  $this->driver->findElement(By::xPath("//div[@id='filter_published_chzn']//ul[@class='chzn-results']/li[contains(.,'Select Status')]"))->click();
+		  $this->driver->findElement(By::xPath("//div[@id='filter_category_id_chzn']/a"))->click();
+		  $this->driver->findElement(By::xPath("//div[@id='filter_category_id_chzn']/div/div/input"))->sendKeys('Select');
+		  $this->driver->findElement(By::xPath("//div[@id='filter_category_id_chzn']//ul[@class='chzn-results']/li[contains(.,'Select Category')]"))->click();
+		  $value = $this->driver->findElement(By::xPath("//tbody/tr[2]/td[4]/div/div[@class='small']"))->getText();
+		  $this->assertEquals($value,'Category: Content Modules','The Article Must have got copied into the same Old Category');
+	  }
 }
