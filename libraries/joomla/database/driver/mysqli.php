ba921cb5eb5934ab0bf73092e4fd94639de5f2a8
Elin Waring <elin.waring@gmail.com>
2013-08-07 20:25:36 -0400

    Mysqli_ping warning in CMS 3.

--- libraries/joomla/database/driver/mysqli.php
+++ libraries/joomla/database/driver/mysqli.php
@@ -231,7 +231,7 @@ class JDatabaseDriverMysqli extends JDatabaseDriver
 	 */
 	public function connected()
 	{
-		if (is_object($this->connection))
+		if (is_object($this->connection)  && !is_null($this->get('connection')))
 		{
 			return mysqli_ping($this->connection);
 		}
