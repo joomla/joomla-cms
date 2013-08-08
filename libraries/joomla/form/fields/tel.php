4700fae555b73054e35a9acab9f5979515f39557
Achal-Aggarwal <theachalaggarwal@gmail.com>
2013-08-08 20:35:27 +0530

    Adding getInput method in tel field class.

--- libraries/joomla/form/fields/tel.php
+++ libraries/joomla/form/fields/tel.php
@@ -31,4 +31,40 @@ class JFormFieldTel extends JFormFieldText
 	 * @since  11.1
 	 */
 	protected $type = 'Tel';
+
+	/**
+	 * Method to get the field input markup.
+	 *
+	 * @return  string  The field input markup.
+	 *
+	 * @since   3.1.2 (CMS)
+	 */
+	protected function getInput()
+	{
+		// Translate placeholder text
+		$hint = $this->translateHint ? JText::_($this->hint) : $this->hint;
+
+		// Initialize some field attributes.
+		$size = $this->element['size'] ? ' size="' . (int) $this->element['size'] . '"' : '';
+		$maxLength = $this->element['maxlength'] ? ' maxlength="' . (int) $this->element['maxlength'] . '"' : '';
+		$class = !empty($this->class) ? ' class="' . $this->class . '"' : '';
+		$readonly = $this->readonly ? ' readonly' : '';
+		$disabled = $this->disabled ? ' disabled' : '';
+		$required = $this->required ? ' required aria-required="true"' : '';
+		$hint = $hint ? ' placeholder="' . $hint . '"' : '';
+		$autocomplete = !$this->autocomplete ? ' autocomplete="off"' : ' autocomplete="' . (string) $this->element['autocomplete'] . '"';
+		$autofocus = $this->autofocus ? ' autofocus' : '';
+		$spellcheck = $this->spellcheck ? ' spellcheck="true"' : ' spellcheck="false"';
+
+		// Initialize JavaScript field attributes.
+		$onchange = $this->element['onchange'] ? ' onchange="' . (string) $this->element['onchange'] . '"' : '';
+
+		// Including fallback code for HTML5 non supported browsers.
+		JHtml::_('jquery.framework');
+		JHtml::_('script', 'system/html5fallback.js', false, true);
+
+		return '<input type="tel" name="' . $this->name . '"' . $class . ' id="' . $this->id . '" value="'
+			. htmlspecialchars($this->value, ENT_COMPAT, 'UTF-8') . '"' . $size . $disabled . $readonly
+			. $hint . $autocomplete . $autofocus . $spellcheck . $onchange . $maxLength . $required . '/>';
+	}
 }
