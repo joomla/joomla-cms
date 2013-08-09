74066e768c68ea907500ed6e5cdc0c077713ad30
Ashan Fernando <ashan256@gmail.com>
2013-08-09 06:57:30 +0530

    Task:Multiselect Including the compressed version of multiselect.js and adding additional file(multiselect-uncompressed.js) which contains the uncompressed version

--- media/system/js/multiselect.js
+++ media/system/js/multiselect.js
@@ -1,43 +1,4 @@
-/**
- * @copyright   Copyright (C) 2005 - 2013 Open Source Matters, Inc. All rights reserved.
- * @license     GNU General Public License version 2 or later; see LICENSE.txt
- */
-
-/**
- * JavaScript behavior to allow shift select in administrator grids
- */
-(function($) {
-    
-    Joomla = Joomla || {};
-    var $boxes;
-    Joomla.JMultiSelect = function(table) {
-        var $last;
-
-        var initialize = function(table) {
-            $boxes = $('#' + table).find('input[type=checkbox]');
-            $boxes.on('click', function(e) {
-                doselect(e)
-            });
-        }
-        
-        var doselect = function(e) {
-            var $current = $(e.target);
-            if (e.shiftKey && $last.length) {
-                var isChecked = $current.is(':checked');
-                var lastIndex = $boxes.index($last);
-                var currentIndex = $boxes.index($current);
-                if (currentIndex < lastIndex) {
-                    // handle selection from bottom up
-                    var swap = lastIndex;
-                    lastIndex = currentIndex;
-                    currentIndex = swap;
-                }
-                $boxes.slice(lastIndex, currentIndex + 1).attr('checked', isChecked);
-            }
-
-            $last = $current;
-        }
-        initialize(table);
-    }
-
-})(jQuery);
+/*
+        GNU General Public License version 2 or later; see LICENSE.txt
+*/
+(function(b){Joomla=Joomla||{};var a;Joomla.JMultiSelect=function(f){var e,c=function(g){a=b("#"+g).find("input[type=checkbox]");a.on("click",function(h){d(h)})},d=function(j){var h=b(j.target),l,k,g,i;if(j.shiftKey&&e.length){l=h.is(":checked");k=a.index(e);g=a.index(h);if(g<k){i=k;k=g;g=i}a.slice(k,g+1).attr("checked",l)}e=h};c(f)}})(jQuery);
