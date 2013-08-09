1c9cb4a62c60b5d770e7b245f182fff6adc9f12b
Ashan Fernando <ashan256@gmail.com>
2013-08-09 08:06:56 +0530

    Task:Validation Removing validate-jquery-uncompressed.js and including jquery version in original files, including compressed jquery version, js style improvements

--- libraries/cms/html/behavior.php
+++ libraries/cms/html/behavior.php
@@ -126,7 +126,7 @@ abstract class JHtmlBehavior
 		// Add validate.js language strings
 		JText::script('JLIB_FORM_FIELD_INVALID');
 
-		JHtml::_('script', 'system/validate-jquery-uncompressed.js', true, true);
+		JHtml::_('script', 'system/validate.js', true, true);
 		static::$loaded[__METHOD__] = true;
 	}
 
