663b2e2c7c94b3a5fbdc39c0e6fb00046ba9cde0
Achal Aggarwal <theachalaggarwal@gmail.com>
2013-08-10 19:22:47 +0200

    # [#31697] *Tags menu items: impossible to delete the last "Type". Thanks Achal Aggarwal (Fix #1709)

--- libraries/joomla/form/form.php
+++ libraries/joomla/form/form.php
@@ -225,19 +225,13 @@ class JForm
 			// Get the field value from the data input.
 			if ($group)
 			{
-				// Filter the value if it exists.
-				if ($input->exists($group . '.' . $name))
-				{
-					$output->set($group . '.' . $name, $this->filterField($field, $input->get($group . '.' . $name, (string) $field['default'])));
-				}
+				// Filter the value if it exists otherwise filters its default value.
+				$output->set($group . '.' . $name, $this->filterField($field, $input->get($group . '.' . $name, (string) $field['default'])));
 			}
 			else
 			{
-				// Filter the value if it exists.
-				if ($input->exists($name))
-				{
-					$output->set($name, $this->filterField($field, $input->get($name, (string) $field['default'])));
-				}
+				// Filter the value if it exists otherwise filters its default value.
+				$output->set($name, $this->filterField($field, $input->get($name, (string) $field['default'])));
 			}
 		}
 
