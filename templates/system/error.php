d9a081aec985f510dc3daf683a5fdf5e546411d0
Joey3000 <Joey3000@users.noreply.github.com>
2013-08-11 21:07:05 +0200

    Corrected hard-coded charset to one obtained from $this->getCharset()
    See following bug report: https://joomlacode.org/gf/project/joomla/tracker/?action=TrackerItemEdit&tracker_item_id=31724
    
    This is a correction of the previous update, where the charset was added, but as a hard-coded string instead of obtaining it from $this->getCharset().
    
    Tested on Joomla 1.5, but needs testing on current branch - I'll add a test procedure to the bug report.

--- templates/system/error.php
+++ templates/system/error.php
@@ -21,7 +21,7 @@ $this->direction = $doc->direction;
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>">
 <head>
-	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<meta http-equiv="content-type" content="text/html; charset=<?php echo $this->getCharset(); ?>" />
 	<title><?php echo $this->error->getCode(); ?> - <?php echo $this->title; ?></title>
 	<link rel="stylesheet" href="<?php echo $this->baseurl; ?>/templates/system/css/error.css" type="text/css" />
 	<?php if ($this->direction == 'rtl') : ?>
