58ee324f85b91e51a70a629811d4cf010bb4dbe5
Michael Babker <mbabker@flbab.com>
2013-08-15 12:11:47 -0500

    Fix limit processing for queries

--- tests/unit/suites/database/driver/postgresql/JDatabaseIteratorPostgresqlTest.php
+++ tests/unit/suites/database/driver/postgresql/JDatabaseIteratorPostgresqlTest.php
@@ -129,7 +129,7 @@ class JDatabaseIteratorPostgresqlTest extends TestCaseDatabasePostgresql
 			$this->setExpectedException($exception);
 		}
 
-		self::$driver->setQuery(self::$driver->getQuery(true)->select($select)->from($from)->setLimit($limit, $offset));
+		self::$driver->setQuery(self::$driver->getQuery(true)->select($select)->from($from), $offset, $limit);
 		$iterator = self::$driver->getIterator($column, $class);
 
 		// Run the Iterator pattern
@@ -156,14 +156,14 @@ class JDatabaseIteratorPostgresqlTest extends TestCaseDatabasePostgresql
 			__LINE__
 		);
 
-		self::$driver->setQuery(self::$driver->getQuery(true)->select('title')->from('#__dbtest')->setLimit(2));
+		self::$driver->setQuery(self::$driver->getQuery(true)->select('title')->from('#__dbtest'), 0, 2);
 		$this->assertThat(
 			count(self::$driver->getIterator()),
 			$this->equalTo(2),
 			__LINE__
 		);
 
-		self::$driver->setQuery(self::$driver->getQuery(true)->select('title')->from('#__dbtest')->setLimit(2, 3));
+		self::$driver->setQuery(self::$driver->getQuery(true)->select('title')->from('#__dbtest'), 3, 2);
 		$this->assertThat(
 			count(self::$driver->getIterator()),
 			$this->equalTo(1),
