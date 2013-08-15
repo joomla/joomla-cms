faa7655a76face31e976f516d81ec98d82f8be41
Michael Babker <mbabker@flbab.com>
2013-08-15 16:22:05 -0500

    PHPCS

--- libraries/joomla/filter/input.php
+++ libraries/joomla/filter/input.php
@@ -698,10 +698,18 @@ class JFilterInput
 		$source = strtr($source, $ttr);
 
 		// Convert decimal
-		$source = preg_replace_callback('/&#(\d+);/m', function($m){return utf8_encode(chr($m[1]));}, $source); // decimal notation
+		$source = preg_replace_callback('/&#(\d+);/m', function($m)
+		{
+			return utf8_encode(chr($m[1]));
+		}, $source
+		);
 
 		// Convert hex
-		$source = preg_replace_callback('/&#x([a-f0-9]+);/mi', function($m){return utf8_encode(chr('0x'.$m[1]));}, $source); // hex notation
+		$source = preg_replace_callback('/&#x([a-f0-9]+);/mi', function($m)
+		{
+			return utf8_encode(chr('0x' . $m[1]));
+		}, $source
+		);
 
 		return $source;
 	}
