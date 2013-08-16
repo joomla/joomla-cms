c99d730a389f8f207ef028a885ab240b405490d3
Nobody <anothercoder@users.noreply.github.com>
2013-08-16 09:27:41 +0100

    #31743 (for master branch) special TEST ONLY version

--- plugins/system/log/log.php
+++ plugins/system/log/log.php
@@ -31,13 +31,27 @@ class PlgSystemLog extends JPlugin
 
 			case JAuthentication::STATUS_FAILURE:
 				$errorlog['status']  = $response['type'] . " FAILURE: ";
-				if ($this->params->get('log_username', 0))
+				$errorlog['comment'] = $response['error_message'];
+				if ($this->params->get('log_username', 0) && $this->params->get('log_sourceip',0))
 				{
-					$errorlog['comment'] = $response['error_message'] . ' ("' . $response['username'] . '")';
+					$alternativeIPData = $this->retrieveAlternativeIPAddress();
+					if (is_array($alternativeIPData) && (!JDEBUG || !JFactory::getApplication()->getCfg('debug_lang'))) {
+						$errorlog['comment'] .= ' (username="' . $response['username'] . 
+									'",REMOTE_ADDR=' . $_SERVER["REMOTE_ADDR"] .
+									' ' . $alternativeIPData[0] . '=' . $alternativeIPData[1] . ')';
+					} elseif (is_array($alternativeIPData) && (JDEBUG || JFactory::getApplication()->getCfg('debug_lang'))) {
+						$errorlog['comment'] .= ' (username="' . $response['username'] . '",DEBUG=';
+						foreach ( $alternativeIPData as $source) {
+							$errorlog['comment'] .= $source . '//';
+						}
+						$errorlog['comment'] .= ')';
+					} else {
+						$errorlog['comment'] .= ' (username="' . $response['username'] . '",srcip=' . $_SERVER["REMOTE_ADDR"] . ')';
+					}
 				}
-				else
+				elseif ($this->params->get('log_username', 0) && $this->params->get('log_sourceip',1))
 				{
-					$errorlog['comment'] = $response['error_message'];
+					$errorlog['comment'] .= ' (username="' . $response['username'] . '")';
 				}
 				break;
 
@@ -48,5 +62,30 @@ class PlgSystemLog extends JPlugin
 		}
 		JLog::addLogger(array(), JLog::INFO);
 		JLog::add($errorlog['comment'], JLog::INFO, $errorlog['status']);
+	}
+	
+	private function retrieveAlternativeIPAddress()
+	{
+		$result = false;
+		$ip_sources = array('HTTP_CF_CONNECTING_IP','HTTP_X_FORWARDED_FOR','HTTP_X_FORWARDED','HTTP_FORWARDED','HTTP_FORWARDED_FOR','HTTP_X_CLUSTER_CLIENT_IP');
+		if (JDEBUG || JFactory::getApplication()->getCfg('debug_lang'))
+		{	$result=array();
+			foreach ( $ip_sources as $source) {
+				$result=array_push($_SERVER[$source]);
+			}
+		}
+		else 
+		{
+			foreach ( $ip_sources as $source) {
+				if (array_key_exists($source,$_SERVER) && isset($_SERVER[$source])
+							       && !empty($_SERVER[$source])
+							       && filter_var($_SERVER[$source],
+							       FILTER_VALIDATE_IP, FILTER_FLAG_IPV6 | FILTER_FLAG_IPV4 | FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE)) {
+				$result=array($source,trim($_SERVER[$source]));
+				break;
+				}
+			}
+		}
+		return $result;
 	}
 }
