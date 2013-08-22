51048a60cf1ff30eec4e3c07dca44643f2262cde
Matt Thomas <matt@betweenbrain.com>
2013-08-22 09:11:02 -0400

    Updates docblocks

--- components/com_ajax/ajax.php
+++ components/com_ajax/ajax.php
@@ -1,12 +1,11 @@
 <?php defined('_JEXEC') or die;
 
 /**
- * File       ajax.php
- * Created    5/20/13 4:54 PM
- * Author     Matt Thomas | matt@betweenbrain.com | http://betweenbrain.com
- * Support    https://github.com/betweenbrain/Joomla-Ajax-Interface/issues
- * Copyright  Copyright (C) 2013 betweenbrain llc. All Rights Reserved.
- * License    GNU General Public License version 2, or later.
+ * @package     Joomla.Site
+ * @subpackage  com_ajax
+ *
+ * @copyright   Copyright (C) 2013 betweenbrain llc. All Rights Reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 // Reference global application object
