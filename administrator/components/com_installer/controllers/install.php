09cebd5bad2d1d606f10002005b473162306fbf5
Ashwin Date <ashwin.date@tekdi.net>
2013-08-22 17:13:17 +0530

    Removed the temporary html renderer fucntion

--- administrator/components/com_installer/controllers/install.php
+++ administrator/components/com_installer/controllers/install.php
@@ -53,60 +53,8 @@ class InstallerControllerInstall extends JControllerLegacy
 		$model = $this->getModel('install');
 		$html = $model->installfromweb();
 		
-		//echo $this->temphtml();
 		echo $html;
 		jexit();
 	}
-	
-	public function temphtml() {
-		$html = <<<EOT
-<div class="row-fluid">
-        <div class="span3">
-          <div class="well sidebar-nav">
-            <strong>Category Nav here</strong>
-            <ul class="nav nav-list">
-              <li class="nav-header">Sidebar</li>
-              <li class="active"><a href="#">Link</a></li>
-              <li><a href="#">Link</a></li>
-              <li><a href="#">Link</a></li>
-              <li><a href="#">Link</a></li>
-              <li class="nav-header">Sidebar</li>
-              <li><a href="#">Link</a></li>
-              <li><a href="#">Link</a></li>
-              <li><a href="#">Link</a></li>
-              <li><a href="#">Link</a></li>
-              <li><a href="#">Link</a></li>
-              <li><a href="#">Link</a></li>
-              <li class="nav-header">Sidebar</li>
-              <li><a href="#">Link</a></li>
-              <li><a href="#">Link</a></li>
-              <li><a href="#">Link</a></li>
-            </ul>
-          </div><!--/.well -->
-        </div><!--/span-->
-        <div class="span9">
-          <div class="row-fluid">
-            <div class="span4">
-              <h2>JBolo!</h2>
-              <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-              <p><a class="btn" href="#" onclick="Joomla.installfromweb('')">Install »</a> <a class="btn" href="#">Details »</a></p>
-            </div><!--/span-->
-            <div class="span4">
-              <h2>Akeeba Backup</h2>
-              <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-              <p><a class="btn" href="#" onclick="Joomla.installfromweb('')">Install »</a> <a class="btn" href="#">Details »</a></p>
-            </div><!--/span-->
-            <div class="span4">
-              <h2>Advanced Module Manager</h2>
-              <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-              <p><a class="btn" href="#" onclick="Joomla.installfromweb('http://download.nonumber.nl/?ext=advancedmodulemanager', 'Advanced Module Manager')">Install »</a> <a class="btn" href="#">Details »</a></p>
-            </div><!--/span-->
-          </div><!--/row-->
-        </div><!--/span-->
-      </div>
-EOT;
-	
-	return $html;
-	}	
 	
 }
