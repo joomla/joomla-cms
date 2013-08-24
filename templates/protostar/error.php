7f1bd1d945deacc9c9cf0bf50f64a30c15ae7fb6
Ervis Tusha <ervistusha@gmail.com>
2013-08-24 17:31:51 -0500

    [#31629] Fix https problem if you are using Google Fonts protostar Template

--- templates/protostar/error.php
+++ templates/protostar/error.php
@@ -80,7 +80,7 @@ else
 	if ($params->get('googleFont'))
 	{
 	?>
-		<link href='http://fonts.googleapis.com/css?family=<?php echo $params->get('googleFontName');?>' rel='stylesheet' type='text/css'>
+		<link href='//fonts.googleapis.com/css?family=<?php echo $params->get('googleFontName');?>' rel='stylesheet' type='text/css'>
 	<?php
 	}
 	?>
