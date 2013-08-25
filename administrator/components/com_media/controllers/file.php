4039b2a242d9e0d21d1b6be6738184348ea5c1c6
Jean-Marie Simonet <infografjms@gmail.com>
2013-08-25 08:22:15 +0200

    code quality

--- administrator/components/com_media/controllers/file.php
+++ administrator/components/com_media/controllers/file.php
@@ -61,10 +61,10 @@ class MediaControllerFile extends JControllerLegacy
 		if (($params->get('upload_maxsize', 0) * 1024 * 1024) != 0)
 		{
 			if (
-				$_SERVER['CONTENT_LENGTH'] > ($params->get('upload_maxsize', 0) * 1024 * 1024) ||
-				$_SERVER['CONTENT_LENGTH'] > (int) (ini_get('upload_max_filesize')) * 1024 * 1024 ||
-				$_SERVER['CONTENT_LENGTH'] > (int) (ini_get('post_max_size')) * 1024 * 1024 ||
-				(($_SERVER['CONTENT_LENGTH'] > (int) (ini_get('memory_limit')) * 1024 * 1024) && ((int) (ini_get('memory_limit')) != -1))
+				$_SERVER['CONTENT_LENGTH'] > ($params->get('upload_maxsize', 0) * 1024 * 1024)
+				|| $_SERVER['CONTENT_LENGTH'] > (int)(ini_get('upload_max_filesize'))* 1024 * 1024
+				|| $_SERVER['CONTENT_LENGTH'] > (int)(ini_get('post_max_size'))* 1024 * 1024
+				|| (($_SERVER['CONTENT_LENGTH'] > (int) (ini_get('memory_limit')) * 1024 * 1024) && ((int) (ini_get('memory_limit')) != -1))
 			)
 			{
 				JError::raiseWarning(100, JText::_('COM_MEDIA_ERROR_WARNFILETOOLARGE'));
