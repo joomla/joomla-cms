6dcee2e5e4aa708453349b4c7049e0115b0edb6d
Puneet Kala <puneet.0191@gmail.com>
2013-08-27 20:13:14 -0700

    Add some system tests for front end. Thanks Puneet Kala!

--- tests/system/webdriver/tests/content/ArticleManager0003Test.php
+++ tests/system/webdriver/tests/content/ArticleManager0003Test.php
@@ -87,4 +87,53 @@ class ArticleManager0003Test extends JoomlaWebdriverTestCase
 		 $arrayTitles = $this->archivedArticlePage->getArticleTitles();
 		 $this->assertFalse(in_array('Beginners',$arrayTitles),'Beginners article must not be present');
 	 }
+	 
+	 /**
+	 *@test
+	 */
+	 public function frontEndSingleArticleState_ChangeArticleState_ArticleStateChanged()
+	 {
+		 $cfg = new SeleniumConfig();
+		 $urlHome = $this->cfg->host.$this->cfg->path.'index.php';
+		 $homePage = $this->getPageObject('SiteContentFeaturedPage', true, $urlHome);
+		 $urlGettingStarted = $this->cfg->host.$this->cfg->path.'index.php/getting-started';
+		 $this->driver->get($urlGettingStarted);
+		 $gettingStartedPage = $this->getPageObject('SiteSingleArticlePage', true, $urlGettingStarted);		 	 
+		 $this->assertTrue($gettingStartedPage->isArticlePresent('Getting Started'), 'Getting Started Must be Present');
+		 
+		 $this->doAdminLogin();
+		 $articleManager='administrator/index.php?option=com_content';
+		 $this->driver->get($cfg->host.$cfg->path.$articleManager);
+		 $this->articleManagerPage = $this->getPageObject('ArticleManagerPage');
+		 $this->articleManagerPage->changeArticleState('Getting Started', 'unpublished');
+		 $this->driver->get($urlGettingStarted);
+		 $gettingStartedPage = $this->getPageObject('SiteSingleArticlePage', true, $urlGettingStarted);
+		 $this->assertFalse($gettingStartedPage->isArticlePresent('Getting Started'), 'Getting Started Must not be Present');
+		 $this->doSiteLogin();
+		 $this->driver->get($urlGettingStarted);
+		 $gettingStartedPage = $this->getPageObject('SiteSingleArticlePage', true, $urlGettingStarted);
+		 $this->assertTrue($gettingStartedPage->isEditPresent(),'Edit Icons Must be Present');
+		 $this->doSiteLogout();
+		 
+		 $cpPage = $this->doAdminLogin();
+		 $this->articleManagerPage = $cpPage->clickMenu('Article Manager', 'ArticleManagerPage');
+		 $this->articleManagerPage = $this->getPageObject('ArticleManagerPage');
+		 $this->articleManagerPage->changeFilter('Status','Unpublished');
+		 $this->articleManagerPage->changeArticleState('Getting Started', 'published');
+		 $this->driver->get($urlGettingStarted);
+		 $gettingStartedPage = $this->getPageObject('SiteSingleArticlePage', true, $urlGettingStarted);
+		 $this->assertTrue($gettingStartedPage->isArticlePresent('Getting Started'), 'Getting Started Must be Present');
+		 
+		 $this->driver->get($cfg->host.$cfg->path.$articleManager);
+		 $this->articleManagerPage = $this->getPageObject('ArticleManagerPage');
+		 $this->articleManagerPage->changeFilter('Status','Published');	 
+		 $this->articleManagerPage->changeArticleState('Getting Started', 'archived');
+		 $this->driver->get($urlGettingStarted);
+		 $gettingStartedPage = $this->getPageObject('SiteSingleArticlePage', true, $urlGettingStarted);
+		 $this->assertTrue($gettingStartedPage->isArticlePresent('Getting Started'), 'Getting Started Must be Present');
+		 $this->driver->get($cfg->host.$cfg->path.$articleManager);
+		 $this->articleManagerPage = $this->getPageObject('ArticleManagerPage');
+		 $this->articleManagerPage->changeFilter('Status','Archived');
+		 $this->articleManagerPage->changeArticleState('Getting Started', 'published');
+	 }
 }
