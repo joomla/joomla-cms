43708051e354d652e01dbb4d3443f48aa19034f1
Joey3000 <Joey3000@users.noreply.github.com>
2013-08-30 03:17:04 +0200

    [#31878] Follow-up after error pages update on #31724 (hardening against XSS)
    http://joomlacode.org/gf/project/joomla/tracker/?action=TrackerItemEdit&tracker_id=8103&tracker_item_id=31878

--- administrator/templates/hathor/error.php
+++ administrator/templates/hathor/error.php
@@ -15,7 +15,7 @@ defined('_JEXEC') or die;
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>" >
 <head>
 	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-	<title><?php echo $this->error->getCode(); ?> - <?php echo htmlspecialchars($this->error->getMessage()); ?></title>
+	<title><?php echo $this->error->getCode(); ?> - <?php echo htmlspecialchars($this->error->getMessage(), ENT_QUOTES, 'UTF-8'); ?></title>
 	<link rel="stylesheet" href="templates/<?php echo  $this->template ?>/css/error.css" type="text/css" />
 
 	<!-- Load additional CSS styles for rtl sites -->
@@ -27,10 +27,9 @@ defined('_JEXEC') or die;
 <body class="errors">
 	<div>
 		<h1><?php echo $this->error->getCode() ?> - <?php echo JText::_('JERROR_AN_ERROR_HAS_OCCURRED') ?></h1>
-		<p><?php echo htmlspecialchars($this->error->getMessage()); ?></p>
 	</div>
 	<div>
-		<p><?php echo $this->error->getMessage(); ?></p>
+		<p><?php echo htmlspecialchars($this->error->getMessage(), ENT_QUOTES, 'UTF-8'); ?></p>
 		<p><a href="index.php"><?php echo JText::_('JGLOBAL_TPL_CPANEL_LINK_TEXT') ?></a></p>
 		<?php if ($this->debug) :
 			echo $this->renderBacktrace();
