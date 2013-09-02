321d061af390d1041a95c8202ec7bbc0d5f237b4
Alex Marin <alex.ukf@gmail.com>
2013-09-02 10:43:17 +0300

    Rackspace Create or Update Object

--- libraries/joomla/rackspace/storage/object.php
+++ libraries/joomla/rackspace/storage/object.php
@@ -29,7 +29,7 @@ class JRackspaceStorageObject extends JRackspaceStorage
 	 *
 	 * @since   ??.?
 	 */
-	public function retrieveObject($container, $object, $options)
+	public function retrieveObject($container, $object, $options = null)
 	{
 		$authTokenHeaders = $this->getAuthTokenHeaders();
 		$url = $authTokenHeaders["X-Storage-Url"] . "/" . $container
@@ -51,5 +51,57 @@ class JRackspaceStorageObject extends JRackspaceStorage
 		$response = $this->client->get($url, $headers);
 
 		return $response->body;
+	}
+
+	/**
+	 * PUT operations are used to write, or overwrite, an object's content and metadata.
+	 *
+	 * @param   string  $container  The container name
+	 * @param   string  $object     The object name
+	 * @param   array   $etag       The MD5 checksum  of the object's data
+	 * @param   array   $options    Additional headers
+	 *
+	 * @return string  The response body
+	 *
+	 * @since   ??.?
+	 */
+	public function createOrUpdateObject($container, $object, $etag = null, $options = null)
+	{
+		$authTokenHeaders = $this->getAuthTokenHeaders();
+		$url = $authTokenHeaders["X-Storage-Url"] . "/" . $container . "/" . $object;
+
+		// Create the headers
+		$headers = array(
+			"Host" => $this->options->get("storage.host"),
+		);
+		$headers["X-Auth-Token"] = $authTokenHeaders["X-Auth-Token"];
+
+		// Set the ETag
+		if ($etag != null)
+		{
+			$headers["ETag"] = $etag;
+		}
+
+		// Set additional headers
+		foreach ($options as $key => $value)
+		{
+			$headers[$key] = $value;
+		}
+
+		// Set the content
+		$data = file_get_contents($object);
+
+		// Send the http request
+		$response = $this->client->put($url, $data, $headers);
+
+		if ($response->code == 201)
+		{
+			return "The \"" . $object . "\" object was successfully created.\n";
+		}
+		else
+		{
+			return "The \"" . $object . "\" object was not successfully created.\n"
+				. "Response code: " . $response->code . ".";
+		}
 	}
 }
