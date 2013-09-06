c32b86c5d76dac7dc99518d8728e69657d7a2422
wilsonge <georgejameswilson@googlemail.com>
2013-09-06 02:47:31 +0100

    PHPCS Fixes for Mod_menu

--- modules/mod_menu/tmpl/default_separator.php
+++ modules/mod_menu/tmpl/default_separator.php
@@ -10,14 +10,19 @@
 defined('_JEXEC') or die;
 
 // Note. It is important to remove spaces between elements.
-$title = $item->anchor_title ? ' title="'.$item->anchor_title.'" ' : '';
+$title = $item->anchor_title ? ' title="' . $item->anchor_title . '" ' : '';
 if ($item->menu_image)
 	{
 		$item->params->get('menu_text', 1) ?
-		$linktype = '<img src="'.$item->menu_image.'" alt="'.$item->title.'" /><span class="image-title">'.$item->title.'</span> ' :
-		$linktype = '<img src="'.$item->menu_image.'" alt="'.$item->title.'" />';
+		$linktype = '<img src="' . $item->menu_image . '" alt="' . $item->title . '" /><span class="image-title">' . $item->title . '</span> ' :
+		$linktype = '<img src="' . $item->menu_image . '" alt="' . $item->title . '" />';
 }
-else { $linktype = $item->title;
+else
+{
+	$linktype = $item->title;
 }
 
-?><span class="separator"<?php echo $title; ?>><?php echo $linktype; ?></span>
+?>
+<span class="separator"<?php echo $title; ?>>
+	<?php echo $linktype; ?>
+</span>
