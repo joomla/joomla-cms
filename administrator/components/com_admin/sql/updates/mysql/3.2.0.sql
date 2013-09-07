6072082a86ba2d90dd98772454d052a5981a4f4c
Elin Waring <elin.waring@gmail.com>
2013-09-07 09:27:44 -0400

    Add table in mysql

--- administrator/components/com_admin/sql/updates/mysql/3.2.0.sql
+++ administrator/components/com_admin/sql/updates/mysql/3.2.0.sql
@@ -38,3 +38,18 @@ INSERT INTO `#__update_sites_extensions` (`update_site_id`, `extension_id`) VALU
 ALTER TABLE `#__users` ADD COLUMN `otpKey` varchar(1000) NOT NULL DEFAULT '' COMMENT 'Two factor authentication encrypted keys';
 
 ALTER TABLE `#__users` ADD COLUMN `otep` varchar(1000) NOT NULL DEFAULT '' COMMENT 'One time emergency passwords';
+
+CREATE TABLE IF NOT EXISTS `#__user_keys` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` varchar(255) NOT NULL,
+  `token` varchar(255) NOT NULL,
+  `series` varchar(255) NOT NULL,
+  `invalid` tinyint(4) NOT NULL,
+  `time` varchar(200) NOT NULL,
+  `uastring` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `series` (`series`),
+  UNIQUE KEY `series_2` (`series`),
+  UNIQUE KEY `series_3` (`series`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
