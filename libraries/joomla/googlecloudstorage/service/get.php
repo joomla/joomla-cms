287638d864da8f611a698cc867700ef33e686a8e
Alex Marin <alex.ukf@gmail.com>
2013-09-10 16:12:44 +0300

    GSC Get Service

---
+++ libraries/joomla/googlecloudstorage/service/get.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * @package     Joomla.Platform
+ * @subpackage  Googlecloudstorage
+ *
+ * @copyright   Copyright (C) 2005 - 2013 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+
+defined('JPATH_PLATFORM') or die;
+
+/**
+ * Defines the GET operation on the service
+ *
+ * @package     Joomla.Platform
+ * @subpackage  Googlecloudstorage
+ * @since       ??.?
+ */
+class JGooglecloudstorageServiceGet extends JGooglecloudstorageService
+{
+	/**
+	 * Creates the get request and returns the response
+	 *
+	 * @return string  The response body
+	 *
+	 * @since   ??.?
+	 */
+	public function getService()
+	{
+		$url = "https://" . $this->options->get("api.url") . "/";
+
+		// The headers may be optionally set in advance
+		$headers = array(
+			"Host" => $this->options->get("api.url"),
+			"Date" => date("D, d M Y H:i:s O"),
+			"Content-Length" => 0,
+			"x-goog-api-version" => 2,
+			"x-goog-project-id" => $this->options->get("project.id"),
+		);
+
+		$authorization = $this->createAuthorization("GET", $url, $headers);
+		$headers["Authorization"] = $authorization;
+
+		// Send the http request
+		$response = $this->client->get($url, $headers);
+
+		// Process the response
+		$response_body = $this->processResponse($response);
+
+		return $response_body;
+	}
+}
