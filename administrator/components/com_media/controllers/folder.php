581c1f1f309bb029531932cbdf41377249f9c7ac
Marco Richter <info@e-commerz.eu>
2013-09-12 19:24:59 +0200

    Update folder.php

--- administrator/components/com_media/controllers/folder.php
+++ administrator/components/com_media/controllers/folder.php
@@ -172,9 +172,9 @@ class MediaControllerFolder extends JControllerLegacy
 
 			if (($folderCheck !== null) && ($folder !== $folderCheck))
 			{
-			$app = JFactory::getApplication();
-                	$app->enqueueMessage(JText::_('COM_MEDIA_ERROR_UNABLE_TO_CREATE_FOLDER_WARNDIRNAME'), 'warning');				
-                	return false;
+				$app = JFactory::getApplication();
+				$app->enqueueMessage(JText::_('COM_MEDIA_ERROR_UNABLE_TO_CREATE_FOLDER_WARNDIRNAME'), 'notice');
+				return false;
 			}
 
 			$path = JPath::clean(COM_MEDIA_BASE . '/' . $parent . '/' . $folder);
