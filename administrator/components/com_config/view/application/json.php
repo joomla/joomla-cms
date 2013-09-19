b8b5e7e811c753d37ca0ce908dedb6bc3ce8c1a9
Elin Waring <elin.waring@gmail.com>
2013-09-19 13:16:33 -0400

    Add a JSON base class (still extending from html however).

--- administrator/components/com_config/view/application/json.php
+++ administrator/components/com_config/view/application/json.php
@@ -16,7 +16,7 @@ defined('_JEXEC') or die;
  * @subpackage  com_config
  * @since       3.2
  */
-class ConfigViewApplicationJson extends JViewHtmlCms
+class ConfigViewApplicationJson extends JViewJson
 {
 
 	public $state;
