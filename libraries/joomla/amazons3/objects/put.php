44a41282fe3539cae94f139a299e12593ce31432
Alex Marin <alex.ukf@gmail.com>
2013-09-24 15:13:24 +0300

    Amazons3 ACL permissions for putBucket

--- libraries/joomla/amazons3/objects/put.php
+++ libraries/joomla/amazons3/objects/put.php
@@ -25,17 +25,37 @@ class JAmazons3ObjectsPut extends JAmazons3Objects
 	 * @param   string  $object          The object to be added
 	 * @param   string  $content         The content of the object
 	 * @param   array   $requestHeaders  An array of request headers
+	 * @param   string  $acl             An array containing the ACL permissions
+	 *                                   (either canned or explicitly specified)
 	 *
 	 * @return string  The response body
 	 *
 	 * @since   ??.?
 	 */
-	public function putObject($bucket, $object, $content = "", $requestHeaders = null)
+	public function putObject($bucket, $object, $content = "", $requestHeaders = null, $acl = null)
 	{
 		$url = "https://" . $bucket . "." . $this->options->get("api.url") . "/" . $object;
 		$headers = array(
 			"Date" => date("D, d M Y H:i:s O"),
 		);
+
+		// Check for ACL permissions
+		if (is_array($acl))
+		{
+			// Check for canned ACL permission
+			if (array_key_exists("acl", $acl))
+			{
+				$headers["x-amz-acl"] = $acl["acl"];
+			}
+			else
+			{
+				// Access permissions were specified explicitly
+				foreach ($acl as $aclPermission => $aclGrantee)
+				{
+					$headers["x-amz-grant-" . $aclPermission] = $aclGrantee;
+				}
+			}
+		}
 
 		if (! is_null($requestHeaders))
 		{
