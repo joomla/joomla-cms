075464b739424e35995e088d88cf5ebcdeb7c852
Beat <beat@joomlapolis.com>
2013-10-02 09:51:22 +0200

    [JUX2] Quick Icons: Put back i instead of span to fix regression on Joomla and Extensions update checker

--- layouts/joomla/links/link.php
+++ layouts/joomla/links/link.php
@@ -13,11 +13,11 @@ $id      = empty($displayData['id']) ? '' : (' id="' . $displayData['id'] . '"')
 $target  = empty($displayData['target']) ? '' : (' target="' . $displayData['target'] . '"');
 $onclick = empty($displayData['onclick']) ? '' : (' onclick="' . $displayData['onclick'] . '"');
 $title   = empty($displayData['title']) ? '' : (' title="' . $this->escape($displayData['title']) . '"');
-$text    = empty($displayData['text']) ? '' : ('<span>' . $displayData['text'] . '</span>')
+$text    = empty($displayData['text']) ? '' : ('<span class="j-links-link">' . $displayData['text'] . '</span>')
 
 ?>
 <li<?php echo $id; ?>>
 	<a href="<?php echo JFilterOutput::ampReplace($displayData['link']); ?>"<?php echo $target . $onclick . $title; ?>>
-		<span class="icon-<?php echo $displayData['image']; ?>"></span> <?php echo $text; ?>
+		<i class="icon-<?php echo $displayData['image']; ?>"></i> <?php echo $text; ?>
 	</a>
 </li>
