a38c59c3e24094e1b9d908fa527df23643c7393e
Roberto - phproberto <roberto@phproberto.com>
2013-10-03 22:38:28 +0200

    [fix] test array before foreach

--- libraries/joomla/access/access.php
+++ libraries/joomla/access/access.php
@@ -498,13 +498,11 @@ class JAccess
 			// If unable to find the file return false.
 			return false;
 		}
-		else
-		{
-			// Else return the actions from the xml.
-			$xml = simplexml_load_file($file);
 
-			return self::getActionsFromData($xml, $xpath);
-		}
+		// Else return the actions from the xml.
+		$xml = simplexml_load_file($file);
+
+		return self::getActionsFromData($xml, $xpath);
 	}
 
 	/**
