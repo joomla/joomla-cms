651df1168b2ea6249c641b5198ebd37926edc7d0
Michael Babker <mbabker@flbab.com>
2013-10-03 19:48:26 -0500

    Replace JLess use in Template Manager

--- administrator/components/com_templates/models/template.php
+++ administrator/components/com_templates/models/template.php
@@ -693,7 +693,15 @@ class TemplatesModelTemplate extends JModelForm
 			$explodeArray = explode('/', $inFile);
 			$fileName     = end($explodeArray);
 			$outFile      = reset(explode('.', $fileName));
-			$less         = new JLess;
+
+			// Load the RAD layer to use its LESS compiler
+			if (!defined('FOF_INCLUDED'))
+			{
+				require_once JPATH_LIBRARIES . '/fof/include.php';
+			}
+
+			$less = new FOFLess;
+			$less->setFormatter(new FOFLessFormatterJoomla);
 
 			try
 			{
