7ba25081c97b32c43a1f717fc3acf72d1c40073c
Jean-Marie Simonet <infografjms@gmail.com>
2013-10-05 12:08:24 +0200

    [#30586] Usernames that should not be accepted can be actualy registered. (2.5.x)

--- libraries/joomla/database/table/user.php
+++ libraries/joomla/database/table/user.php
@@ -194,7 +194,7 @@ class JTableUser extends JTable
 			return false;
 		}
 
-		if (preg_match("#[<>\"'%;()&]#i", $this->username) || strlen(utf8_decode($this->username)) < 2)
+		if (preg_match('#[<>"\'%;()&\\s\\\\]|\\.\\./#', $this->username) || strlen(utf8_decode($this->username)) < 2)
 		{
 			$this->setError(JText::sprintf('JLIB_DATABASE_ERROR_VALID_AZ09', 2));
 			return false;
