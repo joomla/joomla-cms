bd42e10981962a0ce8840c6bca5bf2ce6defac6d
Lefteris Kavadas <lefteris.kavadas@gmail.com>
2013-10-10 15:41:08 +0300

    Updated the mod_login layout to detect the menu Itemid for registration, password reset and passwrod remind links.

--- modules/mod_login/tmpl/default.php
+++ modules/mod_login/tmpl/default.php
@@ -9,6 +9,8 @@
 
 defined('_JEXEC') or die;
 
+require_once JPATH_SITE.'/components/com_users/helpers/route.php';
+
 JHtml::_('behavior.keepalive');
 JHtml::_('bootstrap.tooltip');
 
@@ -96,15 +98,15 @@ JHtml::_('bootstrap.tooltip');
 			if ($usersConfig->get('allowUserRegistration')) : ?>
 			<ul class="unstyled">
 				<li>
-					<a href="<?php echo JRoute::_('index.php?option=com_users&view=registration'); ?>">
+					<a href="<?php echo JRoute::_('index.php?option=com_users&view=registration&Itemid='.UsersHelperRoute::getRegistrationRoute()); ?>">
 					<?php echo JText::_('MOD_LOGIN_REGISTER'); ?> <span class="icon-arrow-right"></span></a>
 				</li>
 				<li>
-					<a href="<?php echo JRoute::_('index.php?option=com_users&view=remind'); ?>">
+					<a href="<?php echo JRoute::_('index.php?option=com_users&view=remind&Itemid='.UsersHelperRoute::getRemindRoute()); ?>">
 					  <?php echo JText::_('MOD_LOGIN_FORGOT_YOUR_USERNAME'); ?></a>
 				</li>
 				<li>
-					<a href="<?php echo JRoute::_('index.php?option=com_users&view=reset'); ?>"><?php echo JText::_('MOD_LOGIN_FORGOT_YOUR_PASSWORD'); ?></a>
+					<a href="<?php echo JRoute::_('index.php?option=com_users&view=reset&Itemid='.UsersHelperRoute::getResetRoute()); ?>"><?php echo JText::_('MOD_LOGIN_FORGOT_YOUR_PASSWORD'); ?></a>
 				</li>
 
 			</ul>
