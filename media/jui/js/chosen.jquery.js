93cbb0ebc56ee04297d273bb280819f574924631
Fedik <getthesite@gmail.com>
2013-10-10 20:56:46 +0300

    chosen.js fix zero width

--- media/jui/js/chosen.jquery.js
+++ media/jui/js/chosen.jquery.js
@@ -434,7 +434,7 @@
       if (this.options.width != null) {
         return this.options.width;
       } else {
-        return "" + this.form_field.offsetWidth + "px";
+        return $(this.form_field).css("width") || "" + this.form_field.offsetWidth + "px";
       }
     };
 
