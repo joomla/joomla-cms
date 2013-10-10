7eb8a32baf068a3a755d74b1c4727ceffdad4e53
Bakual <werbemails@bakual.ch>
2013-10-10 18:23:27 +0200

    Adding files for new module "mod_postinstall"

---
+++ administrator/modules/mod_postinstall/tmpl/default.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  mod_postinstall
+ *
+ * @copyright   Copyright (C) 2005 - 2013 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+?>
+<div class="row-striped">
+	<div class="row-fluid"><?php echo JText::_('MOD_POSTINSTALL_MESSAGES_BODY'); ?></div>
+	<div class="row-fluid"><?php echo JText::_('MOD_POSTINSTALL_MESSAGES_BODYMORE'); ?></div>
+	<div class="row-fluid">
+		<a href="index.php?option=com_postinstall&eid=<?php echo $eid; ?>" class="btn btn-primary btn-large" >
+			<?php echo JText::_('MOD_POSTINSTALL_MESSAGES_REVIEW'); ?>
+		</a>
+	</div>
+</div>
+</div>
