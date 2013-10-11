226804254f744fedd1259e96f19ae864966e44bd
Peter van Westen <peter@nonumber.nl>
2013-10-12 00:10:06 +0200

    Update edit.php

--- administrator/components/com_plugins/views/plugin/tmpl/edit.php
+++ administrator/components/com_plugins/views/plugin/tmpl/edit.php
@@ -96,14 +96,6 @@ $this->fieldsets = $this->form->getFieldsets('params');
 				<div class="form-vertical">
 					<div class="control-group">
 						<div class="control-label">
-							<?php echo $this->form->getLabel('ordering'); ?>
-						</div>
-						<div class="controls">
-							<?php echo $this->form->getInput('ordering'); ?>
-						</div>
-					</div>
-					<div class="control-group">
-						<div class="control-label">
 							<?php echo $this->form->getLabel('folder'); ?>
 						</div>
 						<div class="controls">
