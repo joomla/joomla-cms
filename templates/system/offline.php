f2c5a7c6e3cce30da0fa25b6529341a0dd011d89
zero-24 <zero.mail@gmx.de>
2013-10-13 15:31:53 +0200

    Update offline.php

--- templates/system/offline.php
+++ templates/system/offline.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * @package		Joomla.Site
+ * @package	Joomla.Site
  * @copyright	Copyright (C) 2005 - 2013 Open Source Matters, Inc. All rights reserved.
- * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ * @license	GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 defined('_JEXEC') or die;
