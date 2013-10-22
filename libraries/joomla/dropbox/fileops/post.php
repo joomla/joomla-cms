da18a5eb335696e4bc7a68e6e94105349b628212
Alex Marin <alex.ukf@gmail.com>
2013-10-23 02:26:43 +0300

    Dropbox postCreateFolder

--- libraries/joomla/dropbox/fileops/post.php
+++ libraries/joomla/dropbox/fileops/post.php
@@ -53,4 +53,40 @@ class JDropboxFileopsPost extends JDropboxFiles
 		// Process the response
 		return $this->processResponse($response);
 	}
+
+	/**
+	 * Creates a folder.
+	 *
+	 * @param   array  $params  The parameters to be used in the request.
+	 *                          "root" and "path" are required parameters.
+	 *
+	 * @return string  The response body
+	 *
+	 * @since   ??.?
+	 */
+	public function postCreateFolder($params)
+	{
+		$url = "https://" . $this->options->get("api.url") . "/1/fileops/create_folder";
+		$paramsString = "";
+
+		foreach ($params as $key => $param)
+		{
+			$paramsString .= "&" . $key . "=" . $param;
+		}
+
+		if (! empty($params))
+		{
+			$paramsString[0] = "?";
+			$url .= $paramsString;
+		}
+
+		// Creates an array with the default Host and Authorization headers
+		$headers = $this->getDefaultHeaders();
+
+		// Send the http request
+		$response = $this->client->post($url, "", $headers);
+
+		// Process the response
+		return $this->processResponse($response);
+	}
 }
