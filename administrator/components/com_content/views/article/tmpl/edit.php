6ccaa15aa9dec9391716f97e1288a1b8914ca05f
Jean-Marie Simonet <infografjms@gmail.com>
2013-10-23 09:22:31 +0200

    normalising display

--- administrator/components/com_content/views/article/tmpl/edit.php
+++ administrator/components/com_content/views/article/tmpl/edit.php
@@ -139,13 +139,15 @@ if (isset($this->item->attribs['show_urls_images_backend']) && $this->item->attr
 		<?php if ($this->canDo->get('core.admin')) : ?>
 			<?php echo JHtml::_('bootstrap.addTab', 'myTab', 'editor', JText::_('COM_CONTENT_SLIDER_EDITOR_CONFIG', true)); ?>
 				<?php foreach ($this->form->getFieldset('editorConfig') as $field) : ?>
-				<div class="control-group">
-						<?php echo $field->label; ?>
-					<div class="controls">
-						<?php echo $field->input; ?>
+					<div class="control-group">
+						<div class="control-label">
+							<?php echo $field->label; ?>
+						</div>
+						<div class="controls">
+							<?php echo $field->input; ?>
+						</div>
 					</div>
-				</div>
-			<?php endforeach; ?>
+				<?php endforeach; ?>
 			<?php echo JHtml::_('bootstrap.endTab'); ?>
 		<?php endif; ?>
 
