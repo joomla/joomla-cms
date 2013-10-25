7ada0c6308f50102413989972c94a78d22134607
Alex Marin <alex.ukf@gmail.com>
2013-10-25 17:31:28 +0300

    Dropbox JDropboxObjectTest

---
+++ tests/unit/suites/libraries/joomla/dropbox/stubs/JDropboxObjectMock.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @package     Joomla.UnitTest
+ * @subpackage  Application
+ *
+ * @copyright   Copyright (C) 2005 - 2013 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+
+/**
+ * JDropboxObjectMock class.
+ *
+ * @package     Joomla.UnitTest
+ * @subpackage  Dropbox
+ *
+ * @since       11.1
+ */
+class JDropboxObjectMock extends JDropboxObject
+{
+}
