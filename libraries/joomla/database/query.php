362f8ab45a1706d53bd8f7266883f7b2f87e4fba
Nikolai Plath <der.el.kuku@gmail.com>
2013-10-29 20:52:43 -0500

    Formatting

--- libraries/joomla/database/query.php
+++ libraries/joomla/database/query.php
@@ -1130,7 +1130,6 @@ abstract class JDatabaseQuery
 		{
 			$this->join = array();
 		}
-
 		$this->join[] = new JDatabaseQueryElement(strtoupper($type) . ' JOIN', $conditions);
 
 		return $this;
@@ -1557,6 +1556,7 @@ abstract class JDatabaseQuery
 		{
 			$glue = ')' . PHP_EOL . 'UNION (';
 			$name = 'UNION ()';
+
 		}
 
 		// Get the JDatabaseQueryElement if it does not exist
