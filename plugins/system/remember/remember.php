e25ac5b965d1aba96227bc27e4bcd69c2d03c037
Elin Waring <elin.waring@gmail.com>
2013-11-05 06:56:00 -0500

    Delete an invalid cookie

--- plugins/system/remember/remember.php
+++ plugins/system/remember/remember.php
@@ -128,6 +128,15 @@ class PlgSystemRemember extends JPlugin
 
 			if ($rememberArray !== false)
 			{
+				if (count($rememberArray) != 3)
+				{
+					// Destroy the cookie in the browser.
+					$this->app->input->cookie->set(end($rememberArray), false, time() - 42000, $this->app->get('cookie_path'), $this->app->get('cookie_domain'));
+					JLog::add('Invalid cookie detected.', JLog::WARNING, 'error');
+
+					return false;
+				}
+
 				list($privateKey, $series, $uastring) = $rememberArray;
 
 				if (!JUserHelper::clearExpiredTokens($this))
