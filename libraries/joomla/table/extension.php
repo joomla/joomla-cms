19999995c4bf440cb091f99bef973b1152815bfc
Michael Babker <mbabker@flbab.com>
2013-11-06 00:55:52 -0600

    Typehinted params in JTable breaks 2.5 updates

--- libraries/joomla/table/extension.php
+++ libraries/joomla/table/extension.php
@@ -26,7 +26,7 @@ class JTableExtension extends JTable
 	 *
 	 * @since   11.1
 	 */
-	public function __construct(JDatabaseDriver $db)
+	public function __construct($db)
 	{
 		parent::__construct('#__extensions', 'extension_id', $db);
 	}
