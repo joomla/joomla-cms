a73d2899827b16b504a016db2f74f14f5363ea11
George Wilson <georgejameswilson@googlemail.com>
2013-11-10 03:40:14 +0000

    [#32622] CSS Layout Issue with Protostar and Weblinks Front End Submit

--- components/com_weblinks/views/form/tmpl/edit.php
+++ components/com_weblinks/views/form/tmpl/edit.php
@@ -128,7 +128,7 @@ $params = $this->state->get('params');
 			<div class="control-label">
 				<?php echo $this->form->getLabel('description'); ?>
 			</div>
-			<div class="controls">
+			<div class="controls" style="overflow: hidden;position: relative;">
 				<?php echo $this->form->getInput('description'); ?>
 			</div>
 		</div>
