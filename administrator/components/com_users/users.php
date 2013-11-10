01570067229ccc1a0ebc6c67387e007e99fd4d3f
AshanFernando <ashan256@gmail.com>
2013-11-10 11:14:26 +0530

    Modifying the script inclusion place

--- administrator/components/com_users/users.php
+++ administrator/components/com_users/users.php
@@ -9,8 +9,6 @@
 
 defined('_JEXEC') or die;
 JHtml::_('behavior.tabstate');
-// Load the modal behavior script.
-JHtml::_('behavior.modal', 'a.modal');
 
 if (!JFactory::getUser()->authorise('core.manage', 'com_users'))
 {
