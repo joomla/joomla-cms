8d74e69d68aad58f924c8ed3d924cd36f7d981ff
Jerome [Obsidev] <jerome@obsidev.com>
2013-11-10 04:44:46 +0100

    Fixing isis template with non existing message type (or wrong one)

--- administrator/templates/isis/html/message.php
+++ administrator/templates/isis/html/message.php
@@ -21,7 +21,7 @@ function renderMessage($msgList)
 		$buffer .= '<button type="button" class="close" data-dismiss="alert">&times;</button>';
 		foreach ($msgList as $type => $msgs)
 		{
-			$buffer .= '<div class="alert ' . $alert[$type]. '">';
+			$buffer .= '<div class="alert ' . (isset($alert[$type]) ? $alert[$type] : '') . '">';
 			$buffer .= "\n<h4 class=\"alert-heading\">" . JText::_($type) . "</h4>";
 			if (count($msgs))
 			{
