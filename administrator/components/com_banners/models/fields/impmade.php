0f8beaecfc506b76407e9ed0ba5ee1bb00ab6680
George Wilson <georgejameswilson@googlemail.com>
2013-11-17 01:35:20 +0000

    Codestyle [32427] Banners form field PHPCS fixes Fixes #1907

--- administrator/components/com_banners/models/fields/impmade.php
+++ administrator/components/com_banners/models/fields/impmade.php
@@ -30,12 +30,15 @@ class JFormFieldImpMade extends JFormField
 	 * Method to get the field input markup.
 	 *
 	 * @return  string	The field input markup.
+	 *
 	 * @since   1.6
 	 */
 	protected function getInput()
 	{
-		$onclick	= ' onclick="document.id(\''.$this->id.'\').value=\'0\';"';
+		$onclick	= ' onclick="document.id(\'' . $this->id . '\').value=\'0\';"';
 
-		return '<input class="input-small" type="text" name="'.$this->name.'" id="'.$this->id.'" value="'.htmlspecialchars($this->value, ENT_COMPAT, 'UTF-8').'" readonly="readonly" /> <a class="btn" '.$onclick.'><i class="icon-refresh"></i> '.JText::_('COM_BANNERS_RESET_IMPMADE').'</a>';
+		return '<input class="input-small" type="text" name="' . $this->name . '" id="' . $this->id . '" value="'
+			. htmlspecialchars($this->value, ENT_COMPAT, 'UTF-8') . '" readonly="readonly" /> <a class="btn" ' . $onclick . '><i class="icon-refresh"></i> '
+			. JText::_('COM_BANNERS_RESET_IMPMADE') . '</a>';
 	}
 }
