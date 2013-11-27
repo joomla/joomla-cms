c17479d4226c985481f7d3c3f0d34fddc3f57af6
Matt Thomas <matt@betweenbrain.com>
2013-11-27 16:32:56 -0500

    Implements a translatable language constant for the InvalidArgumentException

--- components/com_ajax/ajax.php
+++ components/com_ajax/ajax.php
@@ -33,7 +33,7 @@ $results = null;
 // Check for valid format
 if (!$format)
 {
-	$results = new InvalidArgumentException('Please specify response format other that HTML (json, raw, etc.)', 404);
+	$results = new InvalidArgumentException(JText::_('COM_AJAX_SPECIFY_FORMAT'), 404);
 }
 /*
  * Module support.
