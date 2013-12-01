06b2d6810475a738fb7793feb06fc9522a369510
Asika <asika32764@gmail.com>
2013-12-01 18:21:18 +0800

    Add render backtrace in isis template with bootstrap style

--- administrator/templates/isis/error.php
+++ administrator/templates/isis/error.php
@@ -251,6 +251,61 @@ $stickyToolbar = $params->get('stickyToolbar', '1');
 							<span class="label label-inverse"><?php echo $this->error->getCode(); ?></span> <?php echo $this->error->getMessage();?>
 						</blockquote>
 						<p><a href="<?php echo $this->baseurl; ?>" class="btn"><i class="icon-dashboard"></i> <?php echo JText::_('JGLOBAL_TPL_CPANEL_LINK_TEXT'); ?></a></p>
+						<p>
+						<?php
+						$contents = null;
+						$backtrace = $this->error->getTrace();
+
+						if (is_array($backtrace))
+						{
+						$j = 1;
+						?>
+						<table cellpadding="0" cellspacing="0" class="Table table table-striped">
+							<thead>
+							<tr>
+								<th colspan="3" class="th"><strong>Call stack</strong></th>
+							</tr>
+							<tr>
+								<th class="th"><strong>#</strong></th>
+								<th class="th"><strong>Function</strong></th>
+								<th class="th"><strong>Location</strong></th>
+							</tr>
+							</thead>
+							<tbody>
+							<?php for ($i = count($backtrace) - 1; $i >= 0; $i--):?>
+								<tr>
+									<td class="TD"><?php echo $j;?></td>
+									<td class="TD">
+										<?php
+										if (isset($backtrace[$i]['class']))
+										{
+											echo $backtrace[$i]['class'] . $backtrace[$i]['type'] . $backtrace[$i]['function'] . '()';
+										}
+										else
+										{
+											echo $backtrace[$i]['function'] . '()';
+										}
+										?>
+									</td>
+									<td class="TD">
+										<?php
+										if (isset($backtrace[$i]['file']))
+										{
+											echo $backtrace[$i]['file'] . ':' . $backtrace[$i]['line'];
+										}
+										else
+										{
+											echo '&#160';
+										}
+										?>
+									</td>
+								</tr>
+								<?php $j++;?>
+							<?php endfor;?>
+							</tbody>
+						</table>
+						<?php } ?>
+						</p>
 						<!-- End Content -->
 					</div>
 			</div>
