3e7907961abfa5cff6214395d33e09cd81efdbd8
Khanh Le <khanhlh@gmail.com>
2013-12-03 16:55:52 +0700

    Correct the position of caption container for Image
    Solve the issue 
    http://joomlacode.org/gf/project/joomla/tracker/?action=TrackerItemEdit&tracker_id=8103&tracker_item_id=32458

--- media/system/js/caption-uncompressed.js
+++ media/system/js/caption-uncompressed.js
@@ -39,7 +39,7 @@ var JCaption = function(_selector) {
                 "width" : width
             }
         });
-        $el.parent().before($container, $el);
+        $el.before($container);
         $container.append($el);
         if (caption !== "") {
             $container.append($p);
