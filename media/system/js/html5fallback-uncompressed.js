c75e24a8b0fc509820fff2e68827711f76856360
Frank Mayer <frank@frankmayer.net>
2013-12-04 00:18:37 +0200

    Update html5fallback-uncompressed.js
    Formatting...

--- media/system/js/html5fallback-uncompressed.js
+++ media/system/js/html5fallback-uncompressed.js
@@ -118,8 +118,8 @@
 
 			for(var i=0; i<flen; i++) {
 				var elem = f[i];
-		                elem.isRequired = !!elem.required,
-                		elem.isDisabled = !!elem.disabled;
+				elem.isRequired = !!elem.required,
+				elem.isDisabled = !!elem.disabled;
 
 				//Do Validation
 				if(!elem.isDisabled) {
