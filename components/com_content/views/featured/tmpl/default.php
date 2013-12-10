d3490a26a8f515d9e6d2e26de505c9349a6843a6
Seth Warburton <seth@internet-inspired.com>
2013-12-10 10:21:19 +0000

    [imp] Removes calls to caption.js in blog views, as it's no longer required.

--- components/com_content/views/featured/tmpl/default.php
+++ components/com_content/views/featured/tmpl/default.php
@@ -11,8 +11,6 @@ defined('_JEXEC') or die;
 
 JHtml::addIncludePath(JPATH_COMPONENT . '/helpers');
 
-JHtml::_('behavior.caption');
-
 // If the page class is defined, add to class as suffix.
 // It will be a separate class if the user starts it with a space
 ?>
