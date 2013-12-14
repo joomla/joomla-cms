c6c844d33816a5f96d2455a57a7be0e5fd6f6048
Don Gilbert <don@electriceasel.com>
2013-12-14 11:13:50 -0600

    Add verifyPassword from @Hackwar. Bring in PHPass.

--- libraries/joomla/user/user.php
+++ libraries/joomla/user/user.php
@@ -719,6 +719,7 @@ class JUser extends JObject
 		$this->params = (string) $this->_params;
 		$table->bind($this->getProperties());
 
+
 		// Allow an exception to be thrown.
 		try
 		{
@@ -757,8 +758,15 @@ class JUser extends JObject
 			// Check if I am a Super Admin
 			$iAmSuperAdmin = $my->authorise('core.admin');
 
+			$iAmRehashingSuperadmin = false;
+
+			if (($my->id == 0 && !$isNew) && $this->id == $oldUser->id && $oldUser->authorise('core.admin') && $oldUser->password != $this->password)
+			{
+				$iAmRehashingSuperadmin = true;
+			}
+
 			// We are only worried about edits to this account if I am not a Super Admin.
-			if ($iAmSuperAdmin != true)
+			if ($iAmSuperAdmin != true && $iAmRehashingSuperadmin != true)
 			{
 				if ($isNew)
 				{
