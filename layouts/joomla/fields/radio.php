a2d4a34b11c32be1194030acbf65cbc48f2691de
Elijah Madden <elijah.madden@nikkoam.com>
2013-12-16 15:27:08 +0900

    add some comments to explain what these layout variables mean

--- layouts/joomla/fields/radio.php
+++ layouts/joomla/fields/radio.php
@@ -9,19 +9,73 @@
 
 defined('JPATH_BASE') or die;
 
+/**
+ * If true, the field should get an 'autofocus' attribute.
+ *
+ * @var boolean
+ */
 $autofocus = $displayData['autofocus'];
-$classes   = $displayData['classes'];
-$disabled  = $displayData['disabled'];
-$field     = $displayData['field'];
-$options   = $displayData['options'];
-$readonly  = $displayData['readonly'];
-$required  = $displayData['required'];
-$value     = $displayData['value'];
 
+/**
+ * A list of classes for this field.
+ *
+ * @var array
+ */
+$classes = $displayData['classes'];
+
+/**
+ * If true, the field should be disabled.
+ *
+ * @var boolean
+ */
+$disabled = $displayData['disabled'];
+
+/**
+ * The field object.
+ *
+ * @var JFormField
+ */
+$field = $displayData['field'];
+
+/**
+ * A list of options for the field.
+ *
+ * @var array
+ */
+$options = $displayData['options'];
+
+/**
+ * If true, the field should be read-only.
+ *
+ * @var boolean
+ */
+$readonly = $displayData['readonly'];
+
+/**
+ * If true, this is a required field.
+ *
+ * @var boolean
+ */
+$required = $displayData['required'];
+
+/**
+ * The current value of the field.
+ *
+ * @var string
+ */
+$value = $displayData['value'];
+
+// Always use the 'radio' class.
 $classes[] = 'radio';
 
+// The format of the input tag to be filled in using sprintf.
+//     %1 - id
+//     %2 - name
+//     %3 - value
+//     %4 = any other attributes
 $format = '<input type="radio" id="%1$s" name="%2$s" value="%3$s" %4$s />';
 
+// The alt option for JText::alt
 $alt = preg_replace('/[^a-zA-Z0-9_\-]/', '_', $field->fieldname);
 ?>
 
