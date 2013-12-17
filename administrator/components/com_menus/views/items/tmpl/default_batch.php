a65eb0feb5c0d92e2987e39ebb37009f88cc4fcc
zero-24 <zero.mail@gmx.de>
2013-12-17 14:47:23 +0100

    Update default_batch.php

--- administrator/components/com_menus/views/items/tmpl/default_batch.php
+++ administrator/components/com_menus/views/items/tmpl/default_batch.php
@@ -54,10 +54,10 @@ $published = $this->state->get('filter.published');
 		</div>
 	</div>
 	<div class="modal-footer">
-		<button class="btn" type="button" onclick="document.id('batch-category-id').value='';document.id('batch-access').value='';document.id('batch-language-id').value='';document.id('batch-user-id').value='';document.id('batch-tag-id)').value=''" data-dismiss="modal">
+		<button class="btn" type="button" onclick="document.id('batch-menu-id').value='';document.id('batch-access').value='';document.id('batch-language-id').value=''" data-dismiss="modal">
 			<?php echo JText::_('JCANCEL'); ?>
 		</button>
-		<button class="btn btn-primary" type="submit" onclick="Joomla.submitbutton('contact.batch');">
+		<button class="btn btn-primary" type="submit" onclick="Joomla.submitbutton('item.batch');">
 			<?php echo JText::_('JGLOBAL_BATCH_PROCESS'); ?>
 		</button>
 	</div>
