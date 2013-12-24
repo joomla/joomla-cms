316ad26cf44219881fde29d296737fc9fedbec52
Elijah Madden <elijah.madden@nikkoam.com>
2013-12-24 11:22:44 +0900

    [imp][32396] set the X-UA-Compatible header to IE=edge in Isis; Closes #2725

--- administrator/templates/isis/error.php
+++ administrator/templates/isis/error.php
@@ -62,6 +62,7 @@ $stickyToolbar = $params->get('stickyToolbar', '1');
 <head>
 	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 	<title><?php echo $this->title; ?> <?php echo htmlspecialchars($this->error->getMessage()); ?></title>
 	<?php // If debug  mode
 		$debug = JFactory::getConfig()->get('debug_lang');
