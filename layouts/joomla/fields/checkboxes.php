1bcbc844b98f004ec0e26b72209489c687611a75
Elijah Madden <elijah.madden@nikkoam.com>
2013-12-27 18:02:48 +0900

    JFormField object not passed to layout

--- layouts/joomla/fields/checkboxes.php
+++ layouts/joomla/fields/checkboxes.php
@@ -9,56 +9,10 @@
 
 defined('JPATH_BASE') or die;
 
-/**
- * If true, the field should get an 'autofocus' attribute.
- *
- * @var boolean
- */
-$autofocus = $displayData['autofocus'];
-
-/**
- * A list of options that should be checked. These may either be stored or default values (from the field element).
- *
- * @var array
- */
-$checkedOptions = $displayData['checkedOptions'];
-
-/**
- * A list of classes for this field.
- *
- * @var array
- */
-$classes = $displayData['classes'];
-
-/**
- * The field object.
- *
- * @var JFormField
- */
-$field = $displayData['field'];
-
-/**
- * If true, the field has a stored value so that default values should not be used.
- *
- * @var boolean
- */
-$hasValue = $displayData['hasValue'];
-
-/**
- * A list of options for the field.
- *
- * @var array
- */
-$options = $displayData['options'];
-
-/**
- * If true, this is a required field.
- *
- * @var boolean
- */
-$required = $displayData['required'];
+$data = new JRegistry($displayData);
 
 // Always use the 'checkboxes' class.
+$classes = $data->get('classes', array());
 $classes[] = 'checkboxes';
 
 // The format of the input tag to be filled in using sprintf.
@@ -68,39 +22,44 @@ $classes[] = 'checkboxes';
 //     %4 = any other attributes
 $format = '<input type="checkbox" id="%1$s" name="%2$s" value="%3$s" %4$s />';
 
+$id = $data->get('id', '');
+$name = $data->get('name', '');
+
+$options = $data->get('options', array());
+
 // The alt option for JText::alt
-$alt = preg_replace('/[^a-zA-Z0-9_\-]/', '_', $field->fieldname);
+$alt = preg_replace('/[^a-zA-Z0-9_\-]/', '_', $name);
 ?>
 
-<fieldset id="<?php echo $field->id; ?>" class="<?php echo implode(' ', $classes); ?>"
-	<?php echo $required ? 'required aria-required="true"' : '';?>
-	<?php echo $autofocus ? 'autofocus' : ''; ?>>
+<fieldset id="<?php echo $data->get('id'); ?>" class="<?php echo implode(' ', $classes); ?>"
+	<?php echo $data->get('required') ? 'required aria-required="true"' : '';?>
+	<?php echo $data->get('autofocus') ? 'autofocus' : ''; ?>>
 
 	<?php if (!empty($options)) : ?>
 	<ul>
 		<?php foreach ($options as $i => $option) : ?>
 		<?php
 		// Initialize some option attributes.
-		$checked = in_array((string) $option->value, $checkedOptions) ? 'checked' : '';
+		$checked = in_array((string) $option->value, $data->get('checkedOptions')) ? 'checked' : '';
 
 		// In case there is no stored value, use the option's default state.
-		$checked = (!$hasValue && $option->checked) ? 'checked' : $checked;
+		$checked = (!$data->get('hasValue') && $option->checked) ? 'checked' : $checked;
 
 		$class = !empty($option->class) ? 'class="' . $option->class . '"' : '';
-		$disabled = !empty($option->disable) || $field->disabled ? 'disabled' : '';
+		$disabled = !empty($option->disable) || $data->get('disabled') ? 'disabled' : '';
 
 		// Initialize some JavaScript option attributes.
 		$onclick = !empty($option->onclick) ? 'onclick="' . $option->onclick . '"' : '';
 		$onchange = !empty($option->onchange) ? 'onchange="' . $option->onchange . '"' : '';
 
-		$id = $field->id . $i;
+		$oid = $id . $i;
 		$value = htmlspecialchars($option->value, ENT_COMPAT, 'UTF-8');
 		$attributes = array_filter(array($checked, $class, $disabled, $onchange, $onclick));
 		?>
 
 		<li>
-			<?php echo sprintf($format, $id, $field->name, $value, implode(' ', $attributes)); ?>
-			<label for="<?php echo $id; ?>" <?php echo $class; ?>><?php echo JText::alt($option->text, $alt); ?></label>
+			<?php echo sprintf($format, $oid, $name, $value, implode(' ', $attributes)); ?>
+			<label for="<?php echo $oid; ?>" <?php echo $class; ?>><?php echo JText::alt($option->text, $alt); ?></label>
 		</li>
 
 		<?php endforeach; ?>
