294ef7d4d889525ee2b738d89186514d84d98749
zero-24 <zero.mail@gmx.de>
2014-01-07 11:06:59 +0100

    Update index.php
    Tracker: #33088
    http://joomlacode.org/gf/project/joomla/tracker/?action=TrackerItemEdit&tracker_id=8103&tracker_item_id=33088

--- administrator/templates/isis/index.php
+++ administrator/templates/isis/index.php
@@ -245,7 +245,8 @@ $stickyToolbar = $this->params->get('stickyToolbar', '1');
 			<div class="btn-group pull-right">
 				<p>
 					<jdoc:include type="modules" name="footer" style="no" />
-					&copy; <?php echo $sitename; ?> <?php echo date('Y'); ?></p>
+					&copy; <?php echo date('Y'); ?> <?php echo $sitename; ?>
+				</p>
 
 			</div>
 			<jdoc:include type="modules" name="status" style="no" />
