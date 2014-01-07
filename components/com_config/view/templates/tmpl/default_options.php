943d18b115d18e93aba975b68e5882cfb40cb41c
Piotr <piotr@smartwebstudio.com>
2014-01-07 12:46:07 +0100

    Site Components

--- components/com_config/view/templates/tmpl/default_options.php
+++ components/com_config/view/templates/tmpl/default_options.php
@@ -48,7 +48,7 @@ JHtml::_('formbehavior.chosen', 'select');
 	$label = !empty($fieldSet->label) ? $fieldSet->label : 'COM_CONFIG_' . $name . '_FIELDSET_LABEL';
 
 	if (isset($fieldSet->description) && trim($fieldSet->description)) :
-		echo '<p class="tip">' . $this->escape(JText::_($fieldSet->description)) . '</p>';
+		echo '<p class="tip">' . JText::_($fieldSet->description) . '</p>';
 	endif;
 	?>
 
