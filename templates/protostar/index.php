f4b52ce83768f4c4d3417ee9cc1927c659a1e6aa
zero-24 <zero.mail@gmx.de>
2014-01-14 15:27:54 +0100

    Update index.php

--- templates/protostar/index.php
+++ templates/protostar/index.php
@@ -194,8 +194,14 @@ else
 		<div class="container<?php echo ($params->get('fluidContainer') ? '-fluid' : '');?>">
 			<hr />
 			<jdoc:include type="modules" name="footer" style="none" />
-			<p class="pull-right"><a href="#top" id="back-top"><?php echo JText::_('TPL_PROTOSTAR_BACKTOTOP'); ?></a></p>
-			<p>&copy; <?php echo $sitename; ?> <?php echo date('Y');?></p>
+			<p class="pull-right">
+				<a href="#top" id="back-top">
+					<?php echo JText::_('TPL_PROTOSTAR_BACKTOTOP'); ?>
+				</a>
+			</p>
+			<p>
+				&copy; <?php echo date('Y'); ?> <?php echo $sitename; ?>
+			</p>
 		</div>
 	</footer>
 	<jdoc:include type="modules" name="debug" style="none" />
