e6e4bcba86e8afa82cab445523ba28f5b46c37dd
jms2win <info@jms2win.com>
2014-01-17 18:04:27 +0100

    Use alternative CURL when allow_url_open is OFF
    We propose to add an alternative method to get the feed when the allow_url_open is OFF.
    When the allow_url_open is OFF, the XMLReader::open always fails because the URL can not be connected.
    
    So we propose to get the Feed with the alternative CURL when it is present.

--- libraries/joomla/feed/factory.php
+++ libraries/joomla/feed/factory.php
@@ -43,7 +43,35 @@ class JFeedFactory
 		// Open the URI within the stream reader.
 		if (!$reader->open($uri, null, LIBXML_NOERROR | LIBXML_ERR_NONE | LIBXML_NOWARNING))
 		{
-			throw new RuntimeException('Unable to open the feed.');
+			// IF allow_url_fopen is enabled or CURL is not available
+			if( ini_get('allow_url_fopen') || !function_exists( "curl_init" ) )
+			{
+				// This is an error
+				throw new RuntimeException('Unable to open the feed.');
+			}
+			// Otherwise, retry with usage of CURL
+			else
+			{
+				// Resuce with CURL
+				$ch = curl_init();
+				$timeout = 5;
+				curl_setopt ($ch, CURLOPT_URL, $uri);
+				curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
+				curl_setopt ($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+				$file_contents = curl_exec($ch);
+				$error  = curl_error( $ch);
+				curl_close($ch);
+				
+				if( !empty( $error ))
+				{
+					throw new RuntimeException('Unable to open the feed with CURL.');
+				}
+				// Set the value to the XMLReader parser
+				if ( !$reader->xml( $file_contents, null, LIBXML_NOERROR | LIBXML_ERR_NONE | LIBXML_NOWARNING))
+				{
+					throw new RuntimeException('Unable to parse the feed.');
+				}
+			}
 		}
 
 		try
