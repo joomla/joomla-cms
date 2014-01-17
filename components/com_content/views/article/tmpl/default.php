0837cab0e7fcb5f05b13b582f977ce0534d42c33
Jean-Marie Simonet <infografjms@gmail.com>
2014-01-17 10:57:24 +0100

    Revert 2256 to commit to staging

--- components/com_content/views/article/tmpl/default.php
+++ components/com_content/views/article/tmpl/default.php
@@ -90,8 +90,14 @@ if (!empty($this->item->pagination) && $this->item->pagination && !$this->item->
 			<?php if ($params->get('show_author') && !empty($this->item->author )) : ?>
 				<dd class="createdby">
 					<?php $author = $this->item->created_by_alias ? $this->item->created_by_alias : $this->item->author; ?>
-					<?php if (!empty($this->item->contact_link) && $params->get('link_author') == true) : ?>
-						<?php echo JText::sprintf('COM_CONTENT_WRITTEN_BY', JHtml::_('link', $this->item->contact_link, $author)); ?>
+					<?php if (!empty($this->item->contactid) && $params->get('link_author') == true) : ?>
+						<?php
+						$needle = 'index.php?option=com_contact&view=contact&id=' . $this->item->contactid;
+						$menu = JFactory::getApplication()->getMenu();
+						$item = $menu->getItems('link', $needle, true);
+						$cntlink = !empty($item) ? $needle . '&Itemid=' . $item->id : $needle;
+						?>
+						<?php echo JText::sprintf('COM_CONTENT_WRITTEN_BY', JHtml::_('link', JRoute::_($cntlink), $author)); ?>
 					<?php else: ?>
 						<?php echo JText::sprintf('COM_CONTENT_WRITTEN_BY', $author); ?>
 					<?php endif; ?>
@@ -189,10 +195,16 @@ if (!empty($this->item->pagination) && $this->item->pagination && !$this->item->
 				<?php if ($params->get('show_author') && !empty($this->item->author )) : ?>
 					<dd class="createdby">
 						<?php $author = $this->item->created_by_alias ? $this->item->created_by_alias : $this->item->author; ?>
-						<?php if (!empty($this->item->contact_link) && $params->get('link_author') == true) : ?>
-							<?php echo JText::sprintf('COM_CONTENT_WRITTEN_BY', JHtml::_('link', $this->item->contact_link, $author)); ?>
+						<?php if (!empty($this->item->contactid) && $params->get('link_author') == true) : ?>
+						<?php
+						$needle = 'index.php?option=com_contact&view=contact&id=' . $this->item->contactid;
+						$menu = JFactory::getApplication()->getMenu();
+						$item = $menu->getItems('link', $needle, true);
+						$cntlink = !empty($item) ? $needle . '&Itemid=' . $item->id : $needle;
+						?>
+						<?php echo JText::sprintf('COM_CONTENT_WRITTEN_BY', JHtml::_('link', JRoute::_($cntlink), $author)); ?>
 						<?php else: ?>
-							<?php echo JText::sprintf('COM_CONTENT_WRITTEN_BY', $author); ?>
+						<?php echo JText::sprintf('COM_CONTENT_WRITTEN_BY', $author); ?>
 						<?php endif; ?>
 					</dd>
 				<?php endif; ?>
--- layouts/joomla/content/info_block/author.php
+++ layouts/joomla/content/info_block/author.php
@@ -10,12 +10,15 @@
 defined('JPATH_BASE') or die;
 
 ?>
-<dd class="createdby">
-	<?php $author = $displayData['item']->author; ?>
-	<?php $author = ($displayData['item']->created_by_alias ? $displayData['item']->created_by_alias : $author); ?>
-	<?php if (!empty($displayData['item']->contact_link ) && $displayData['params']->get('link_author') == true) : ?>
-		<?php echo JText::sprintf('COM_CONTENT_WRITTEN_BY', JHtml::_('link', $displayData['item']->contact_link, $author)); ?>
-	<?php else :?>
-		<?php echo JText::sprintf('COM_CONTENT_WRITTEN_BY', $author); ?>
-	<?php endif; ?>
-</dd>
+			<dd class="createdby">
+				<?php $author = $displayData['item']->author; ?>
+				<?php $author = ($displayData['item']->created_by_alias ? $displayData['item']->created_by_alias : $author); ?>
+				<?php if (!empty($displayData['item']->contactid ) && $displayData['params']->get('link_author') == true) : ?>
+					<?php
+					echo JText::sprintf('COM_CONTENT_WRITTEN_BY',
+						JHtml::_('link', JRoute::_('index.php?option=com_contact&view=contact&id='.$displayData['item']->contactid), $author)
+					); ?>
+				<?php else :?>
+					<?php echo JText::sprintf('COM_CONTENT_WRITTEN_BY', $author); ?>
+				<?php endif; ?>
+			</dd>
