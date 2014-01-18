6b6cf03b9df1be54bb292cc46eba1d6f62cf7c1c
kunal-bajpai <kunalkb20@gmail.com>
2014-01-18 18:02:28 +0530

    removed unnecessary line

--- libraries/joomla/html/html.php
+++ libraries/joomla/html/html.php
@@ -891,7 +891,6 @@ abstract class JHtml
 				);
 				$done[] = $id;
 			}
-	
 			return '<input type="text" title="' . (0 !== (int) $value ? self::_('date', $value, null, null) : '') . '" name="' . $name . '" id="' . $id
 				. '" value="' . htmlspecialchars($value, ENT_COMPAT, 'UTF-8') . '" ' . $attribs . ' />'
 				. self::_('image', 'system/calendar.png', JText::_('JLIB_HTML_CALENDAR'), array('class' => 'calendar', 'id' => $id . '_img'), true);
