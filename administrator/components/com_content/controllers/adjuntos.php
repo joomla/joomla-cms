c928094317f5644ee9e5ccc665b91fb627b58204
snaphuman <fhernandezn@gmail.com>
2014-01-19 20:23:11 -0500

    [adjuntos] realiza la subida del archivo al directorio indicado

--- administrator/components/com_content/controllers/adjuntos.php
+++ administrator/components/com_content/controllers/adjuntos.php
@@ -12,11 +12,43 @@
 defined('_JEXEC') or die;
 
 jimport('joomla.application.component.controllerform');
+jimport('joomla.filesystem.file');
+
 
 class ContentControllerAdjuntos extends JControllerForm
 {
     public function subir() {
-        $archivos = $_FILES;
-        print_r($archivos);
+
+        // Esta tarea debe accionarse sólamente cuándo el artículo ha sido previamente guardado,
+        // con el fin de evitar subir archivos huerfanos
+        // TODO: Validar si el artículo actual existe en la base de datos
+
+        // Obtiene la variable @campo enviada en el request 
+        $jinput = JFactory::getApplication()->input;
+        $campo = $jinput->get->get('campo', null, null);
+
+        $archivo = $jinput->files->get($campo);
+        print_r($archivo);
+
+        if(isset($archivo)){
+
+            // Sanea el nombre de archivo evitando caracteres no deseados
+            $nombreArchivo = strtolower(JFile::makeSafe($archivo['name']));
+
+            // Define el origen y destino del archivo
+            // TODO: Crear directorio propio para los adjuntos del artículo 
+            // y usarlo como path destino.
+            $src = $archivo['tmp_name'];
+            $dest = JPATH_ROOT.DS.'uploads'.DS.sha1(time()).'-'.$nombreArchivo;
+
+            if(JFile::upload($src, $dest)) {
+                // TODO: Implementa/valida una estructura de datos para los nombres 
+                // de los archivos que se guardan en la base de datos
+               print_r("Archivo Subido"); 
+            } else {
+                print_r("Ha ocurrido un error");
+                print_r($archivo['error']);
+            }
+        }
     }
 }
