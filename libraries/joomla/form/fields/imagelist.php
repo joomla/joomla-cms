3ad063def6c40ff1afd2a5a052ae7c410b593ea6
Piotr Moćko <mocko.piotr@gmail.com>
2014-01-23 20:07:56 +0100

    Update imagelist.php

--- libraries/joomla/form/fields/imagelist.php
+++ libraries/joomla/form/fields/imagelist.php
@@ -27,31 +27,21 @@ class JFormFieldImageList extends JFormFieldFileList
 	 * @since  11.1
 	 */
 	protected $type = 'ImageList';
-	
-	
+
 	/**
-	 * Method to attach a JForm object to the field.
+	 * Method to get the list of images field options.
 	 * Use the filter attribute to specify allowable file extensions.
 	 *
-	 * @param   SimpleXMLElement  $element  The SimpleXMLElement object representing the <field /> tag for the form field object.
-	 * @param   mixed             $value    The form field value to validate.
-	 * @param   string            $group    The field name group control value. This acts as as an array container for the field.
-	 *                                      For example if the field has name="foo" and the group value is set to "bar" then the
-	 *                                      full field name would end up being "bar[foo]".
+	 * @return  array  The field option objects.
 	 *
-	 * @return  boolean  True on success.
-	 *
-	 * @see     JFormField::setup()
-	 * @since   3.2
+	 * @since   11.1
 	 */
-	public function setup(SimpleXMLElement $element, $value, $group = null)
+	protected function getOptions()
 	{
 		// Define the image file type filter.
-		$filter = '\.png$|\.gif$|\.jpg$|\.bmp$|\.ico$|\.jpeg$|\.psd$|\.eps$';
+		$this->filter = '\.png$|\.gif$|\.jpg$|\.bmp$|\.ico$|\.jpeg$|\.psd$|\.eps$';
 
-		// Set the form field element attribute for file type filter.
-		$element->addAttribute('filter', $filter);
-		
-		return parent::setup($element, $value, $group);
+		// Get the field options.
+		return parent::getOptions();
 	}
 }
