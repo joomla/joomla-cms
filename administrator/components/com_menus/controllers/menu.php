732c464dbdc922fc8d298f05ac527a5b437c8fdc
Jean-Marie Simonet <infografjms@gmail.com>
2014-01-27 11:09:15 +0100

    Porting PR #2843 to 2.5.x

--- administrator/components/com_menus/controllers/menu.php
+++ administrator/components/com_menus/controllers/menu.php
@@ -58,7 +58,9 @@ class MenusControllerMenu extends JControllerForm
 		}
 
 		// Make sure we are not trying to modify an administrator menu.
-		if (isset($data['client_id']) && $data['client_id'] == 1){
+		if ((isset($data['client_id']) && $data['client_id'] == 1) || strtolower($data['menutype']) == 'menu'
+			|| strtolower($data['menutype']) == 'main')
+		{
 			JError::raiseNotice(0, JText::_('COM_MENUS_MENU_TYPE_NOT_ALLOWED'));
 
 			// Redirect back to the edit screen.
