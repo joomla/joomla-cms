78ba947bfeb2d3f26a52f3bcaa5fa300a5a9e4ea
zero-24 <zero.mail@gmx.de>
2014-01-27 16:54:24 +0100

    Update debug.php

--- plugins/system/debug/debug.php
+++ plugins/system/debug/debug.php
@@ -80,17 +80,17 @@ class PlgSystemDebug extends JPlugin
 	{
 		parent::__construct($subject, $config);
 
+		// Log the deprecated API.
+		if ($this->params->get('log-deprecated'))
+		{
+			JLog::addLogger(array('text_file' => 'deprecated.php'), JLog::ALL, array('deprecated'));
+		}
+		
 		//Skip the plugin if debug is off
 		$app = JFactory::getApplication();
 		if ($app->getCfg('debug_lang') == '0' and $app->getCfg('debug')  == '0')
 		{
 			return;
-		}
-		
-		// Log the deprecated API.
-		if ($this->params->get('log-deprecated'))
-		{
-			JLog::addLogger(array('text_file' => 'deprecated.php'), JLog::ALL, array('deprecated'));
 		}
 
 		$this->debugLang = JFactory::getApplication()->getCfg('debug_lang');
