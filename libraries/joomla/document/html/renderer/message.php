26ba748460bebb1394e13f638013617be183f411
Elijah Madden <elijah.madden@nikkoam.com>
2014-01-28 09:58:28 +0900

    render system messages using layouts

--- libraries/joomla/document/html/renderer/message.php
+++ libraries/joomla/document/html/renderer/message.php
@@ -46,7 +46,7 @@ class JDocumentRendererMessage extends JDocumentRenderer
 			}
 		}
 
-		$buffer = ($itemOverride) ? renderMessage($msgList) : $this->renderDefaultMessage($msgList);
+		$buffer = ($itemOverride) ? renderMessage($msgList) : JLayoutHelper::render('joomla.system.message', array('msgList' => $msgList));
 
 		return $buffer;
 	}
@@ -81,49 +81,4 @@ class JDocumentRendererMessage extends JDocumentRenderer
 		return $lists;
 	}
 
-	/**
-	 * Render the system message if no message template file found
-	 *
-	 * @param   array  $msgList  An array contains system message
-	 *
-	 * @return  string  System message markup
-	 *
-	 * @since   12.2
-	 */
-	private function renderDefaultMessage($msgList)
-	{
-		// Build the return string
-		$buffer = '';
-		$buffer .= "\n<div id=\"system-message-container\">";
-
-		// If messages exist render them
-		if (is_array($msgList))
-		{
-			$buffer .= "\n<div id=\"system-message\">";
-			foreach ($msgList as $type => $msgs)
-			{
-				$buffer .= "\n<div class=\"alert alert-" . $type . "\">";
-
-				// This requires JS so we should add it trough JS. Progressive enhancement and stuff.
-				$buffer .= "<a class=\"close\" data-dismiss=\"alert\">×</a>";
-
-				if (count($msgs))
-				{
-					$buffer .= "\n<h4 class=\"alert-heading\">" . JText::_($type) . "</h4>";
-					$buffer .= "\n<div>";
-					foreach ($msgs as $msg)
-					{
-						$buffer .= "\n\t\t<p>" . $msg . "</p>";
-					}
-					$buffer .= "\n</div>";
-				}
-				$buffer .= "\n</div>";
-			}
-			$buffer .= "\n</div>";
-		}
-
-		$buffer .= "\n</div>";
-
-		return $buffer;
-	}
 }
