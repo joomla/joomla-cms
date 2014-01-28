c8a4fdb20b0c535f238927bb691f7cb0ba92c5a2
Peter van Westen <peter@nonumber.nl>
2014-01-28 22:19:07 +0100

    [CODE STYLE] Corrected whitespace

--- administrator/components/com_banners/controllers/tracks.raw.php
+++ administrator/components/com_banners/controllers/tracks.raw.php
@@ -19,16 +19,17 @@ defined('_JEXEC') or die;
 class BannersControllerTracks extends JControllerLegacy
 {
 	/**
-	 * @var		string	The context for persistent state.
-	 * @since   1.6
+	 * @var    string  The context for persistent state.
+	 * 
+	 * @since  1.6
 	 */
 	protected $context = 'com_banners.tracks';
 
 	/**
 	 * Proxy for getModel.
 	 *
-	 * @param   string	$name	The name of the model.
-	 * @param   string	$prefix	The prefix for the model class name.
+	 * @param   string  $name    The name of the model.
+	 * @param   string  $prefix  The prefix for the model class name.
 	 *
 	 * @return  JModel
 	 * @since   1.6
@@ -42,12 +43,12 @@ class BannersControllerTracks extends JControllerLegacy
 	/**
 	 * Display method for the raw track data.
 	 *
-	 * @param   boolean			If true, the view output will be cached
-	 * @param   array  An array of safe url parameters and their variable types, for valid values see {@link JFilterInput::clean()}.
+	 * @param   boolean      If true, the view output will be cached
+	 * @param   array        An array of safe url parameters and their variable types, for valid values see {@link JFilterInput::clean()}.
 	 *
-	 * @return  JController		This object to support chaining.
+	 * @return  JController  This object to support chaining.
 	 * @since   1.5
-	 * @todo	This should be done as a view, not here!
+	 * @todo    his should be done as a view, not here!
 	 */
 	public function display($cachable = false, $urlparams = false)
 	{
