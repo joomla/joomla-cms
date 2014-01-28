3369b4e3e6e96c0943eccbcc4defddb5d2cbcf30
Peter van Westen <peter@nonumber.nl>
2014-01-28 21:39:14 +0100

    [CODE STYLE] Added missing whitespace

--- administrator/components/com_admin/views/profile/tmpl/edit.php
+++ administrator/components/com_admin/views/profile/tmpl/edit.php
@@ -10,7 +10,7 @@
 defined('_JEXEC') or die;
 
 // Include the component HTML helpers.
-JHtml::addIncludePath(JPATH_COMPONENT.'/helpers/html');
+JHtml::addIncludePath(JPATH_COMPONENT . '/helpers/html');
 
 JHtml::_('behavior.formvalidation');
 
