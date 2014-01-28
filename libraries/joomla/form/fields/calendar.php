83223e5032c15d05d2de5d00a0c140b36cf5bd0b
Hannes Papenberg <info@joomlager.de>
2014-01-28 11:41:47 +0100

    Remove wrong HTML in calendar form field
    Whatever these 2 lines are supposed to do, they have no business in the form field. If those are actually needed, they should be part of JHTML::calendar() and not here.

--- libraries/joomla/form/fields/calendar.php
+++ libraries/joomla/form/fields/calendar.php
@@ -207,10 +207,6 @@ class JFormFieldCalendar extends JFormField
 				break;
 		}
 
-		// Including fallback code for HTML5 non supported browsers.
-		JHtml::_('jquery.framework');
-		JHtml::_('script', 'system/html5fallback.js', false, true);
-
 		return JHtml::_('calendar', $this->value, $this->name, $this->id, $format, $attributes);
 	}
 }
