3de57ca7c05f41f85910bea848330c6e6d3eaaba
Gunjan Patel <gunjan@redcomponent.com>
2014-01-29 13:57:51 +0530

    [CODE STYLE] Corrected: Please start your comment with a capital letter

--- administrator/components/com_categories/helpers/categories.php
+++ administrator/components/com_categories/helpers/categories.php
@@ -61,7 +61,8 @@ class CategoriesHelper
 				if (is_callable(array($cName, 'addSubmenu')))
 				{
 					$lang = JFactory::getLanguage();
-					// loading language file from the administrator/language directory then
+
+					// Loading language file from the administrator/language directory then
 					// loading language file from the administrator/components/*extension*/language directory
 					$lang->load($component, JPATH_BASE, null, false, true)
 					|| $lang->load($component, JPath::clean(JPATH_ADMINISTRATOR . '/components/' . $component), null, false, true);
