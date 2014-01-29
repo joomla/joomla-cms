d3162b7e622072863b04a0d2aa5ed7a7ca6b6d5b
Peter van Westen <peter@nonumber.nl>
2014-01-29 02:07:12 +0100

    Update default.php

--- administrator/components/com_cache/views/purge/tmpl/default.php
+++ administrator/components/com_cache/views/purge/tmpl/default.php
@@ -11,14 +11,12 @@ defined('_JEXEC') or die;
 ?>
 
 <form action="<?php echo JRoute::_('index.php?option=com_cache'); ?>" method="post" name="adminForm" id="adminForm">
-  <?php if (!empty( $this->sidebar)) : ?>
-    <div id="j-sidebar-container" class="span2">
-      <?php echo $this->sidebar; ?>
-    </div>  
-    <div id="j-main-container" class="span10">
-  <?php else : ?>
-    <div id="j-main-container">
-  <?php endif;?>
+	<?php if (!empty($this->sidebar)) : ?>
+	<div id="j-sidebar-container" class="span2">
+		<?php echo $this->sidebar; ?>
+	</div>
+	<?php endif; ?>
+	<div id="j-main-container"<?php echo empty($this->sidebar) ? '' : ' class="span10"'; ?>>
   	<fieldset>
   		<legend><?php echo JText::_('COM_CACHE_PURGE_EXPIRED_ITEMS'); ?></legend>
   		<p><?php echo JText::_('COM_CACHE_PURGE_INSTRUCTIONS'); ?></p>
