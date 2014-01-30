06bf94be13fafcd40312754b8b88836e9336278c
Chris Davenport <chris.davenport@joomla.org>
2014-01-30 00:35:00 +0000

    [Code Style] DocBlock fixes and improvements.

--- plugins/content/finder/finder.php
+++ plugins/content/finder/finder.php
@@ -10,7 +10,7 @@
 defined('_JEXEC') or die;
 
 /**
- * Finder Content Plugin
+ * Smart Search Content Plugin
  *
  * @package     Joomla.Plugin
  * @subpackage  Content.finder
@@ -19,13 +19,15 @@ defined('_JEXEC') or die;
 class PlgContentFinder extends JPlugin
 {
 	/**
-	 * Finder after save content method
-	 * Article is passed by reference, but after the save, so no changes will be saved.
-	 * Method is called right after the content is saved
+	 * Smart Search after save content method.
+	 * Content is passed by reference, but after the save, so no changes will be saved.
+	 * Method is called right after the content is saved.
 	 *
 	 * @param   string  $context  The context of the content passed to the plugin (added in 1.6)
 	 * @param   object  $article  A JTableContent object
 	 * @param   bool    $isNew    If the content has just been created
+	 *
+	 * @return  void
 	 *
 	 * @since	2.5
 	 */
@@ -39,13 +41,14 @@ class PlgContentFinder extends JPlugin
 	}
 
 	/**
-	 * Finder before save content method
-	 * Article is passed by reference, but after the save, so no changes will be saved.
-	 * Method is called right after the content is saved
+	 * Smart Search before save content method.
+	 * Content is passed by reference. Method is called before the content is saved.
 	 *
-	 * @param   string  $context  The context of the content passed to the plugin (added in 1.6)
-	 * @param   object  $article  A JTableContent object
-	 * @param   bool    $isNew    If the content is just about to be created
+	 * @param   string  $context  The context of the content passed to the plugin (added in 1.6).
+	 * @param   object  $article  A JTableContent object.
+	 * @param   bool    $isNew    If the content is just about to be created.
+	 *
+	 * @return  void
 	 *
 	 * @since   2.5
 	 */
@@ -59,12 +62,13 @@ class PlgContentFinder extends JPlugin
 	}
 
 	/**
-	 * Finder after delete content method
-	 * Article is passed by reference, but after the save, so no changes will be saved.
-	 * Method is called right after the content is saved
+	 * Smart Search after delete content method.
+	 * Content is passed by reference, but after the deletion.
 	 *
-	 * @param   string  $context  The context of the content passed to the plugin (added in 1.6)
-	 * @param   object  $article  A JTableContent object
+	 * @param   string  $context  The context of the content passed to the plugin (added in 1.6).
+	 * @param   object  $article  A JTableContent object.
+	 *
+	 * @return  void
 	 *
 	 * @since   2.5
 	 */
@@ -78,7 +82,7 @@ class PlgContentFinder extends JPlugin
 	}
 
 	/**
-	 * Finder change state content method
+	 * Smart Search content state change method.
 	 * Method to update the link information for items that have been changed
 	 * from outside the edit screen. This is fired when the item is published,
 	 * unpublished, archived, or unarchived from the list view.
@@ -86,6 +90,8 @@ class PlgContentFinder extends JPlugin
 	 * @param   string   $context  The context for the content passed to the plugin.
 	 * @param   array    $pks      A list of primary key ids of the content that has changed state.
 	 * @param   integer  $value    The value of the state that the content has been changed to.
+	 *
+	 * @return  void
 	 *
 	 * @since   2.5
 	 */
@@ -99,13 +105,15 @@ class PlgContentFinder extends JPlugin
 	}
 
 	/**
-	 * Finder change category state content method
-	 * Article is passed by reference, but after the save, so no changes will be saved.
-	 * Method is called right after the content is saved
+	 * Smart Search change category state content method.
+	 * Method is called when the state of the category to which the
+	 * content item belongs is changed.
 	 *
 	 * @param   string   $extension  The extension whose category has been updated.
 	 * @param   array    $pks        A list of primary key ids of the content that has changed state.
 	 * @param   integer  $value      The value of the state that the content has been changed to.
+	 *
+	 * @return  void
 	 *
 	 * @since   2.5
 	 */
