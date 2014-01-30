ad8d2b67ad19391ff3c659c1fa1fae1f7ed544b3
Peter van Westen <peter@nonumber.nl>
2014-01-30 08:42:42 +0100

    [CODE STYLE] Removed redundant whitespace

--- administrator/components/com_banners/controllers/client.php
+++ administrator/components/com_banners/controllers/client.php
@@ -20,7 +20,7 @@ class BannersControllerClient extends JControllerForm
 {
 	/**
 	 * @var     string  The prefix to use with controller messages.
-	 * 
+	 *
 	 * @since   1.6
 	 */
 	protected $text_prefix = 'COM_BANNERS_CLIENT';
