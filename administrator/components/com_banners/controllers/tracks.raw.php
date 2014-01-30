cf176cfdde778889e21aed2decffe85a57318d1a
Peter van Westen <peter@nonumber.nl>
2014-01-30 08:45:47 +0100

    [CODE STYLE] Added missing whitespace

--- administrator/components/com_banners/controllers/tracks.raw.php
+++ administrator/components/com_banners/controllers/tracks.raw.php
@@ -66,19 +66,19 @@ class BannersControllerTracks extends JControllerLegacy
 			// Load the filter state.
 			$app = JFactory::getApplication();
 
-			$type = $app->getUserState($this->context.'.filter.type');
+			$type = $app->getUserState($this->context . '.filter.type');
 			$model->setState('filter.type', $type);
 
-			$begin = $app->getUserState($this->context.'.filter.begin');
+			$begin = $app->getUserState($this->context . '.filter.begin');
 			$model->setState('filter.begin', $begin);
 
-			$end = $app->getUserState($this->context.'.filter.end');
+			$end = $app->getUserState($this->context . '.filter.end');
 			$model->setState('filter.end', $end);
 
-			$categoryId = $app->getUserState($this->context.'.filter.category_id');
+			$categoryId = $app->getUserState($this->context . '.filter.category_id');
 			$model->setState('filter.category_id', $categoryId);
 
-			$clientId = $app->getUserState($this->context.'.filter.client_id');
+			$clientId = $app->getUserState($this->context . '.filter.client_id');
 			$model->setState('filter.client_id', $clientId);
 
 			$model->setState('list.limit', 0);
@@ -92,8 +92,8 @@ class BannersControllerTracks extends JControllerLegacy
 			$cookie_domain = $config->get('cookie_domain', '');
 			$cookie_path = $config->get('cookie_path', '/');
 
-			setcookie(JApplication::getHash($this->context.'.basename'), $form['basename'], time() + 365 * 86400, $cookie_path, $cookie_domain);
-			setcookie(JApplication::getHash($this->context.'.compressed'), $form['compressed'], time() + 365 * 86400, $cookie_path, $cookie_domain);
+			setcookie(JApplication::getHash($this->context . '.basename'), $form['basename'], time() + 365 * 86400, $cookie_path, $cookie_domain);
+			setcookie(JApplication::getHash($this->context . '.compressed'), $form['compressed'], time() + 365 * 86400, $cookie_path, $cookie_domain);
 
 			// Push the model into the view (as default).
 			$view->setModel($model, true);
