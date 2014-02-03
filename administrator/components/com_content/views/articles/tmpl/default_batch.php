db53bbd1097d43230def838f46fa1b31bd8ae27a
Bakual <werbemails@bakual.ch>
2014-02-03 17:18:37 +0100

    [CodeStyle] Make Jenkins happy again

--- administrator/components/com_content/views/articles/tmpl/default_batch.php
+++ administrator/components/com_content/views/articles/tmpl/default_batch.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * @package		Joomla.Administrator
- * @subpackage	com_content
+ * @package     Joomla.Administrator
+ * @subpackage  com_content
  *
- * @copyright	Copyright (C) 2005 - 2014 Open Source Matters, Inc. All rights reserved.
- * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ * @copyright   Copyright (C) 2005 - 2014 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 defined('_JEXEC') or die;
--- administrator/components/com_weblinks/views/weblinks/tmpl/default_batch.php
+++ administrator/components/com_weblinks/views/weblinks/tmpl/default_batch.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * @package		Joomla.Administrator
- * @subpackage	com_weblinks
+ * @package     Joomla.Administrator
+ * @subpackage  com_weblinks
  *
- * @copyright	Copyright (C) 2005 - 2014 Open Source Matters, Inc. All rights reserved.
- * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ * @copyright   Copyright (C) 2005 - 2014 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 defined('_JEXEC') or die;
