271ac7743168234d95f056f9ec1ef39f3886f3ab
Michael Babker <mbabker@flbab.com>
2014-02-06 09:20:38 -0600

    Prepare for 3.2.3 development

--- libraries/cms/version/version.php
+++ libraries/cms/version/version.php
@@ -25,10 +25,10 @@ final class JVersion
 	public $RELEASE = '3.2';
 
 	/** @var  string  Maintenance version. */
-	public $DEV_LEVEL = '2';
+	public $DEV_LEVEL = '3-dev';
 
 	/** @var  string  Development STATUS. */
-	public $DEV_STATUS = 'Stable';
+	public $DEV_STATUS = 'Development';
 
 	/** @var  string  Build number. */
 	public $BUILD = '';
