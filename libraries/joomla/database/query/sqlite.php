5d48a29b5c1824a34913e489b5c4d8a2a0729804
Nikolai Plath <der.el.kuku@gmail.com>
2014-02-08 12:33:22 -0500

    Remove methods now in core

--- libraries/joomla/database/query/sqlite.php
+++ libraries/joomla/database/query/sqlite.php
@@ -262,52 +262,6 @@ class JDatabaseQuerySqlite extends JDatabaseQueryPdo implements JDatabaseQueryPr
 	}
 
 	/**
-	 * Gets the number of characters in a string.
-	 *
-	 * Note, use 'length' to find the number of bytes in a string.
-	 *
-	 * Usage:
-	 * $query->select($query->charLength('a'));
-	 *
-	 * @param   string  $field      A value.
-	 * @param   string  $operator   Comparison operator between charLength integer value and $condition
-	 * @param   string  $condition  Integer value to compare charLength with.
-	 *
-	 * @return  string  The required char length call.
-	 *
-	 * @since 11.1
-	 */
-	public function charLength($field, $operator = null, $condition = null)
-	{
-		return 'LENGTH(' . $field . ')' . (isset($operator) && isset($condition) ? ' ' . $operator . ' ' . $condition : '');
-	}
-
-	/**
-	 * Concatenates an array of column names or values.
-	 *
-	 * Usage:
-	 * $query->select($query->concatenate(array('a', 'b')));
-	 *
-	 * @param   array   $values     An array of values to concatenate.
-	 * @param   string  $separator  As separator to place between each value.
-	 *
-	 * @return  string  The concatenated values.
-	 *
-	 * @since   11.1
-	 */
-	public function concatenate($values, $separator = null)
-	{
-		if ($separator)
-		{
-			return '(' . implode(' || ' . $this->quote($separator) . ' || ', $values) . ')';
-		}
-		else
-		{
-			return '(' . implode(' || ', $values) . ')';
-		}
-	}
-
-	/**
 	 * Gets the current date and time.
 	 *
 	 * Usage:
