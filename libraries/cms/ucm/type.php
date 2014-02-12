ea0bdccfe462a6306e13440110a5427f1daaa1d6
George Wilson <georgejameswilson@googlemail.com>
2014-02-12 03:09:14 +0000

    Update type.php

--- libraries/cms/ucm/type.php
+++ libraries/cms/ucm/type.php
@@ -140,7 +140,7 @@ class JUcmType implements JUcm
 
 		$query->where($this->db->quoteName('ct.type_alias') . ' = ' . (string) $typeAlias);
 		$this->db->setQuery($query);
-		print $query->dump();
+		JApplicationCli->out($query->dump());
 
 		$type = $this->db->loadObject();
 
