833bbc7e77bc66630d42fc92099fb77835263544
Michael Babker <mbabker@flbab.com>
2014-02-13 20:54:31 -0600

    Fix E_STRICT error with method declaration

--- components/com_contact/views/contact/view.vcf.php
+++ components/com_contact/views/contact/view.vcf.php
@@ -19,7 +19,14 @@ class ContactViewContact extends JViewLegacy
 
 	protected $item;
 
-	public function display()
+	/**
+	 * Execute and display a template script.
+	 *
+	 * @param   string  $tpl  The name of the template file to parse; automatically searches through the template paths.
+	 *
+	 * @return  mixed  A string if successful, otherwise a Error object.
+	 */
+	public function display($tpl = null)
 	{
 		// Get model data.
 		$item = $this->get('Item');
@@ -86,6 +93,5 @@ class ContactViewContact extends JViewLegacy
 		$vcard[]  = 'END:VCARD';
 
 		echo implode("\n", $vcard);
-		return true;
 	}
 }
