e42263b0a07622101bef4a8239040cbf095ba474
Mohamed Infaz <infaz9999@gmail.com>
2014-02-16 10:46:02 +0530

    comments added and indentations set

--- plugins/content/avatar/avatar.php
+++ plugins/content/avatar/avatar.php
@@ -18,27 +18,29 @@ defined('_JEXEC') or die; // Stopping Unauthorized access
 
 Class PlgContentAvatar extends JPlugin
 {
-    
-    protected $autoloadLanguage = true;
-    protected $defaultsize=100;
-    protected $GRAVATAR_SERVER="http://www.gravatar.com/avatar/";
-    protected $default="http://www.gravatar.com/";
-    protected $GRAVATAR_SECURE_SERVER="https://secure.gravatar.com/avatar";
-    protected $securedefault="https://secure.gravatar.com/";
-    protected $uri;
+    //Load the Language files and Initialize Avatar Servers
+        protected $autoloadLanguage = true;
+        protected $defaultsize=100;
+        protected $GRAVATAR_SERVER="http://www.gravatar.com/avatar/";
+        protected $default="http://www.gravatar.com/";
+        protected $GRAVATAR_SECURE_SERVER="https://secure.gravatar.com/avatar";
+        protected $securedefault="https://secure.gravatar.com/";
+        protected $uri;
 
 
-    public function onContentBeforeDisplay($context, &$row, &$params, $page=0)
-    {
+        public function onContentBeforeDisplay($context, &$row, &$params, $page=0)
+        {       
+            //get the scheme http or https
             $array=JURI::getInstance()->getScheme(); 
+            //Get input parameters if not use the default values
             $size=$this->params->get('size',$this->defaultsize);
             $GRAVATAR_SERVER=$this->params->get('avatar_http',$this->GRAVATAR_SERVER);
             $default=$this->params->get('profile_http',$this->default);
             $GRAVATAR_SECURE_SERVER=  $this->params->get('avatar_https',$this->GRAVATAR_SECURE_SERVER);
             $securedefault=  $this->params->get('profile_https',$this->securedefault);
-        
+            //if the article is featured 
             if($context=='com_content.featured')
-            {
+            {   //get the email of the Author 
                 $emailid=$row->author_email;
                
             
@@ -53,20 +55,20 @@ Class PlgContentAvatar extends JPlugin
                 }
             }
                 return implode("<br /> ", $html);
-    }
+        }
     
-    public function buildHTML($avatar,$gravatar_profile,$email,$size)
-    {
+        public function buildHTML($avatar,$gravatar_profile,$email,$size)
+        {
                 $gravurl=$avatar.md5( strtolower( trim( $email ) ) )."?d=".urlencode($this->default )."&s=".$size;
                 $str=  @file_get_contents("$gravatar_profile".md5($email).".php");
                 $profile=  unserialize($str);
                  
                 if ( is_array( $profile ) && isset( $profile['entry'] ) )
                 {      
-             
-                $name=$profile['entry'][0]['displayName'];   //Displaying My name
-                $myemail=$profile['entry'][0]['emails'][0]['value'];    //Displaying my email
-                $im_accounts=$profile['entry'][0]['ims'][0]['value'];   //Displaying my Ims accounts
+                //Reference the array to get details    
+                $name=$profile['entry'][0]['displayName'];   
+                $myemail=$profile['entry'][0]['emails'][0]['value'];    
+                $im_accounts=$profile['entry'][0]['ims'][0]['value'];   
                 
                 JHtml::_('jquery.framework');
                 $html[] = '<form="well span4">';
@@ -105,7 +107,7 @@ Class PlgContentAvatar extends JPlugin
                 } 
                 
          
-        return $html;
-    }
+                return $html;
+        }
         
 }
