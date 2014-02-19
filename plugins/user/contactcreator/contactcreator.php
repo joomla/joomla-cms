e902051fdf74f99c08e5a22fdc5b9bbace49e4b7
George Wilson <georgejameswilson@googlemail.com>
2014-02-19 11:54:10 +0000

    Add missing quoteName

--- plugins/user/contactcreator/contactcreator.php
+++ plugins/user/contactcreator/contactcreator.php
@@ -61,7 +61,7 @@ class PlgUserContactCreator extends JPlugin
 		// Grab the contact ID for this user; note $user_id is cleaned above
 		$query = $db->getQuery(true)
 			->select($db->quoteName('id'))
-			->from('#__contact_details')
+			->from($db->quoteName('#__contact_details'))
 			->where($db->quoteName('user_id') . ' = ' . $user_id);
 		$db->setQuery($query);
 		$id = $db->loadResult();
