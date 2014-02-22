92449d3c5b95d8fdae6db3222e3302daa1f9c176
Hannes Papenberg <info@joomlager.de>
2014-02-22 23:54:46 +0100

    Optimizations for JRoute and JRouter

--- libraries/cms/router/router.php
+++ libraries/cms/router/router.php
@@ -80,6 +80,14 @@ class JRouter
 		'build' => array(),
 		'parse' => array()
 	);
+	
+	/**
+	 * Caching of processed URIs
+	 *
+	 * @var    array
+	 * @since  3.5
+	 */
+	protected $cache = array();
 
 	/**
 	 * JRouter instances container.
@@ -190,7 +198,7 @@ class JRouter
 	/**
 	 * Function to convert an internal URI to a route
 	 *
-	 * @param   string  $url  The internal URL
+	 * @param   string  $url  The internal URL or an associative array
 	 *
 	 * @return  string  The absolute search engine friendly URL
 	 *
@@ -198,8 +206,15 @@ class JRouter
 	 */
 	public function build($url)
 	{
+		$key = md5(serialize($url));
+
+		if (isset($this->cache[$key]))
+		{
+			return clone($this->cache[$key]);
+		}
+		
 		// Create the URI object
-		$uri = $this->_createURI($url);
+		$uri = $this->createURI($url);
 
 		// Process the uri information based on custom defined rules
 		$this->_processBuildRules($uri);
@@ -215,6 +230,8 @@ class JRouter
 		{
 			$this->_buildSefRoute($uri);
 		}
+		
+		$this->cache[$key] = clone($uri);
 
 		return $uri;
 	}
@@ -547,7 +564,7 @@ class JRouter
 	/**
 	 * Create a uri based on a full or partial url string
 	 *
-	 * @param   string  $url  The URI
+	 * @param   string  $url  The URI or an associative array
 	 *
 	 * @return  JUri
 	 *
@@ -555,8 +572,14 @@ class JRouter
 	 */
 	protected function createURI($url)
 	{
+		if (is_array($url))
+		{
+			$uri = new JUri('index.php');
+			$uri->setQuery($url);
+			return $uri;
+		}
 		// Create full URL if we are only appending variables to it
-		if (substr($url, 0, 1) == '&')
+		elseif (substr($url, 0, 1) == '&')
 		{
 			$vars = array();
 
