9bb5d548e1549dcec4ce90887a3bc93ab5d94768
Mr Ben Griffin <ben@redsnapper.net>
2014-03-03 11:34:08 +0000

    update tooltipText()
    Tracker 32989. If we are escaping, we need to escape everything.

--- libraries/cms/html/html.php
+++ libraries/cms/html/html.php
@@ -889,55 +889,51 @@ abstract class JHtml
 	 * @return  string  The tooltip string
 	 *
 	 * @since   3.1.2
+	 * modified 2014-03-03. MrBenGriffin (Tracker ID 32989)
 	 */
 	public static function tooltipText($title = '', $content = '', $translate = 1, $escape = 1)
 	{
-		// Return empty in no title or content is given.
-		if ($title == '' && $content == '')
+		$result = '';
+		// don't process empty strings
+		if ($content != '' || $title != '')
 		{
-			return '';
-		}
+			// Split title into title and content if the title contains '::' (old Mootools format).
+			if ($content == '' && !(strpos($title, '::') === false))
+			{
+				list($title, $content) = explode('::', $title, 2);
+			}
 
-		// Split title into title and content if the title contains '::' (old Mootools format).
-		if ($content == '' && !(strpos($title, '::') === false))
-		{
-			list($title, $content) = explode('::', $title, 2);
-		}
+			// Pass texts through the JText if required.
+			if ($translate)
+			{
+				$title = JText::_($title);
+				$content = JText::_($content);
+			}
 
-		// Pass texts through the JText.
-		if ($translate)
-		{
-			$title = JText::_($title);
-			$content = JText::_($content);
+			// Use only the content if no title is given.
+			if ($title == '')
+			{
+				$result = $content;
+			}
+			// Use only the title, if title and text are the same.
+			elseif ($title == $content)
+			{
+				$result = '<strong>' . $title . '</strong>';
+			}
+			// Use a formatted string combining the title and content.
+			elseif ($content != '')
+			{
+				$result = '<strong>' . $title . '</strong><br />' . $content;
+			}
+			else {
+				$result = $title;
+			}
+			// escape everything, if required.
+			if ($escape) {
+				$result = htmlspecialchars($result);
+			}
 		}
-
-		// Escape the texts.
-		if ($escape)
-		{
-			$title = str_replace('"', '&quot;', $title);
-			$content = str_replace('"', '&quot;', $content);
-		}
-
-		// Return only the content if no title is given.
-		if ($title == '')
-		{
-			return $content;
-		}
-
-		// Return only the title if title and text are the same.
-		if ($title == $content)
-		{
-			return '<strong>' . $title . '</strong>';
-		}
-
-		// Return the formated sting combining the title and  content.
-		if ($content != '')
-		{
-			return '<strong>' . $title . '</strong><br />' . $content;
-		}
-
-		// Return only the title.
-		return $title;
+		return $result;
 	}
 
 	/**
