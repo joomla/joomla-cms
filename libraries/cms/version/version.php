47528481d39dcf6f6a2775359baec730fdbe754e
Michael Babker <mbabker@flbab.com>
2014-03-04 13:58:49 -0600

    Prepare 2.5.19

--- libraries/cms/version/version.php
+++ libraries/cms/version/version.php
@@ -23,10 +23,10 @@ final class JVersion
 	public $RELEASE = '2.5';
 
 	/** @var  string  Maintenance version. */
-	public $DEV_LEVEL = '19-dev';
+	public $DEV_LEVEL = '19';
 
 	/** @var  string  Development STATUS. */
-	public $DEV_STATUS = 'Development';
+	public $DEV_STATUS = 'Stable';
 
 	/** @var  string  Build number. */
 	public $BUILD = '';
@@ -35,10 +35,10 @@ final class JVersion
 	public $CODENAME = 'Ember';
 
 	/** @var  string  Release date. */
-	public $RELDATE = '6-February-2014';
+	public $RELDATE = '6-March-2014';
 
 	/** @var  string  Release time. */
-	public $RELTIME = '16:00';
+	public $RELTIME = '14:30';
 
 	/** @var  string  Release timezone. */
 	public $RELTZ = 'GMT';
