39c1d57139ebdf4e9d471034d326bfa26043c912
Marko D <chivitli@users.noreply.github.com>
2014-03-05 13:14:06 +0100

    Fixing onContentPrepareForm for multilang sites

--- administrator/components/com_content/models/article.php
+++ administrator/components/com_content/models/article.php
@@ -655,10 +655,6 @@ class ContentModelArticle extends JModelAdmin
 		if ($assoc)
 		{
 			$languages = JLanguageHelper::getLanguages('lang_code');
-
-			// force to array (perhaps move to $this->loadFormData())
-			$data = (array) $data;
-
 			$addform = new SimpleXMLElement('<form />');
 			$fields = $addform->addChild('fields');
 			$fields->addAttribute('name', 'associations');
@@ -668,7 +664,7 @@ class ContentModelArticle extends JModelAdmin
 			$add = false;
 			foreach ($languages as $tag => $language)
 			{
-				if (empty($data['language']) || $tag != $data['language'])
+				if (empty($data->language) || $tag != $data->language)
 				{
 					$add = true;
 					$field = $fieldset->addChild('field');
