b8435db627b1d21faee8f9764cb1c6a3a3b07f01
Marko D <chivitli@users.noreply.github.com>
2014-03-06 07:08:42 +0100

    Removing default value
    It's not supported for the text fields

--- administrator/components/com_admin/sql/updates/mysql/3.2.3-2014-03-05.sql
+++ administrator/components/com_admin/sql/updates/mysql/3.2.3-2014-03-05.sql
@@ -1,1 +1,1 @@
-ALTER TABLE `#__user_profiles` CHANGE `profile_value` `profile_value` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '';
+ALTER TABLE `#__user_profiles` CHANGE `profile_value` `profile_value` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
