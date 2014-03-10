dfa78019b2b9da35b93d8a69176cb1a7612349c4
itbra <Tinobee@gmail.com>
2014-03-10 11:53:35 +0100

    replaced previous solution by improved one

--- components/com_finder/models/search.php
+++ components/com_finder/models/search.php
@@ -1126,8 +1126,8 @@ class FinderModelSearch extends JModelList
 		$this->setState('list.start', $input->get('limitstart', 0, 'uint'));
 		$this->setState('list.limit', $input->get('limit', $app->getCfg('list_limit', 20), 'uint'));
 
-		// Load the sort ordering allowing the user to pass in a field name to order by or fall back to component parameter.
-		$order = $input->getWord('order', $params->get('sort_order', 'relevance'));
+		// Load the sort ordering.
+		$order = $params->get('sort_order', 'relevance');
 		switch ($order)
 		{
 			case 'date':
@@ -1142,17 +1142,13 @@ class FinderModelSearch extends JModelList
 				$this->setState('list.ordering', 'm.weight');
 				break;
 
-			case 'title':
-				$this->setState('list.ordering', 'l.title');
-				break;
-
 			default:
 				$this->setState('list.ordering', 'l.link_id');
 				break;
 		}
 
-		// Load the sort direction allowing the user to pass in a direction or fall back to component parameter.
-		$dirn = $input->getWord('dir', $params->get('sort_direction', 'desc'));
+		// Load the sort direction.
+		$dirn = $params->get('sort_direction', 'desc');
 		switch ($dirn)
 		{
 			case 'asc':
@@ -1164,6 +1160,31 @@ class FinderModelSearch extends JModelList
 				$this->setState('list.direction', 'DESC');
 				break;
 		}
+		
+		
+		// Allow customise the sort ordering. (currently this is 'hard' defined via menu item parameter)
+		$ordering = $app->getUserStateFromRequest("{$this->context}.ordercol", 'filter_order', $ordering);
+		$ordering = $filter->clean($ordering, 'CMD');
+		$ordering = strtolower("l.{$ordering}");
+		// If this information is missing in the user's session, add it.
+		if (!JArrayHelper::getValue($this->filter_fields, $ordering, null))
+		{
+			$app->setUserState("{$this->context}.ordercol", $ordering);
+		}
+		// Replace the model's state.
+		$this->setState('list.ordering', $ordering);
+		
+		// Allow customise the sort ordering direction. (currently this is 'hard' defined via menu item parameter)
+		$direction = $app->getUserStateFromRequest("{$this->context}.orderdir", 'filter_order_Dir', $direction);
+		$direction = $filter->clean($direction, 'WORD');
+		$direction = strtolower($direction);
+		// If this information is missing in the user's session, add it.
+		if (!JArrayHelper::getValue($this->filter_fields, $direction, null))
+		{
+			$app->setUserState("{$this->context}.orderdir", $direction);
+		}
+		// Replace the model's state.
+		$this->setState('list.direction', $direction);
 
 		// Set the match limit.
 		$this->setState('match.limit', 1000);
