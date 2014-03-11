75a03797e2a25afaeb343f1b3a4b9d1d6a10a4d4
Valentin Despa <info@vdespa.de>
2014-03-11 13:22:53 +0100

    Update postgresql.php
    small fix when installing components

--- libraries/joomla/database/driver/postgresql.php
+++ libraries/joomla/database/driver/postgresql.php
@@ -1056,8 +1056,8 @@ class JDatabaseDriverPostgresql extends JDatabaseDriver
 				continue;
 			}
 
-			// Ignore any internal fields and the primary key
-			if (($k[0] == "_") || ($k == $key))
+			// Ignore any internal fields or primary keys with value 0.
+			if (($k[0] == "_") || ($k == $key && $v === 0))
 			{
 				continue;
 			}
