c9920d2765c94622d893ee1229083d03b54b9f81
andrewnester <newaltgroup@bk.ru>
2014-03-12 15:20:06 +0300

    changes to indenting

--- tests/unit/suites/libraries/cms/application/JApplicationHelperTest.php
+++ tests/unit/suites/libraries/cms/application/JApplicationHelperTest.php
@@ -54,87 +54,85 @@ class JApplicationHelperTest extends TestCase
 {
 
 	/**
-	 * Test JApplicationHelper::getComponentName
-	 *
-	 * @return  void
-	 */
+	* Test JApplicationHelper::getComponentName
+	*
+	* @return  void
+	*/
 	public function testGetComponentName()
 	{
-        $testComponentName = 'com_test_component';
+		$testComponentName = 'com_test_component';
 
-        $mockApplication = $this->getMockApplication();
-        $mockInput = $this->getMockBuilder('JInput')
-                          ->setConstructorArgs(array(array('option' => $testComponentName)))
-                          ->getMock();
+		$mockApplication = $this->getMockApplication();
+		$mockInput = $this->getMockBuilder('JInput')
+							->setConstructorArgs(array(array('option' => $testComponentName)))
+							->getMock();
 
-        $mockApplication->input = $mockInput;
-        JFactory::$application = $mockApplication;
+		$mockApplication->input = $mockInput;
+		JFactory::$application = $mockApplication;
 
-        $mockInput->expects($this->once())->method('get')->with($this->equalTo('option'))->will($this->returnValue($testComponentName));
-        $componentName = JApplicationHelper::getComponentName();
+		$mockInput->expects($this->once())->method('get')->with($this->equalTo('option'))->will($this->returnValue($testComponentName));
+		$componentName = JApplicationHelper::getComponentName();
 
-        $this->assertEquals($testComponentName, $componentName);
-
+		$this->assertEquals($testComponentName, $componentName);
 	}
 
 	/**
-	 * Test JApplicationHelper::getClientInfo
-	 *
-	 * @return  void
-	 */
+	* Test JApplicationHelper::getClientInfo
+	*
+	* @return  void
+	*/
 	public function testGetClientInfo()
 	{
-        $infoId = 1;
-        $infoName = 'installation';
+		$infoId = 1;
+		$infoName = 'installation';
 
-        $clientInfo = JApplicationHelper::getClientInfo();
+		$clientInfo = JApplicationHelper::getClientInfo();
 
-        $existedClientInfoById = JApplicationHelper::getClientInfo($infoId);
-        $nonExistedClientInfoById = JApplicationHelper::getClientInfo(-1);
+		$existedClientInfoById = JApplicationHelper::getClientInfo($infoId);
+		$nonExistedClientInfoById = JApplicationHelper::getClientInfo(-1);
 
-        $existedClientInfoByName = JApplicationHelper::getClientInfo($infoName, true);
-        $nonExistedClientInfoByName = JApplicationHelper::getClientInfo('non-installation', true);
+		$existedClientInfoByName = JApplicationHelper::getClientInfo($infoName, true);
+		$nonExistedClientInfoByName = JApplicationHelper::getClientInfo('non-installation', true);
 
-        $expectedClientInfo = $this->expectedClientInfo();
+		$expectedClientInfo = $this->expectedClientInfo();
 
-        $this->assertEquals($expectedClientInfo, $clientInfo);
+		$this->assertEquals($expectedClientInfo, $clientInfo);
 
-        $this->assertEquals($expectedClientInfo[$infoId], $existedClientInfoById);
-        $this->assertNull($nonExistedClientInfoById);
+		$this->assertEquals($expectedClientInfo[$infoId], $existedClientInfoById);
+		$this->assertNull($nonExistedClientInfoById);
 
-        $this->assertNotNull($existedClientInfoByName);
-        $this->assertEquals($infoName, $existedClientInfoByName->name);
-        $this->assertNull($nonExistedClientInfoByName);
-
+		$this->assertNotNull($existedClientInfoByName);
+		$this->assertEquals($infoName, $existedClientInfoByName->name);
+		$this->assertNull($nonExistedClientInfoByName);
 	}
 
 
-    private function expectedClientInfo()
-    {
-        $clientInfo = array();
+	private function expectedClientInfo()
+	{
+		$clientInfo = array();
 
-        $obj = new stdClass;
+		$obj = new stdClass;
 
-        // Site Client
-        $obj->id = 0;
-        $obj->name = 'site';
-        $obj->path = JPATH_SITE;
-        $clientInfo[0] = clone $obj;
+		// Site Client
+		$obj->id = 0;
+		$obj->name = 'site';
+		$obj->path = JPATH_SITE;
+		$clientInfo[0] = clone $obj;
 
-        // Administrator Client
-        $obj->id = 1;
-        $obj->name = 'administrator';
-        $obj->path = JPATH_ADMINISTRATOR;
-        $clientInfo[1] = clone $obj;
+		// Administrator Client
+		$obj->id = 1;
+		$obj->name = 'administrator';
+		$obj->path = JPATH_ADMINISTRATOR;
+		$clientInfo[1] = clone $obj;
 
-        // Installation Client
-        $obj->id = 2;
-        $obj->name = 'installation';
-        $obj->path = JPATH_INSTALLATION;
-        $clientInfo[2] = clone $obj;
+		// Installation Client
+		$obj->id = 2;
+		$obj->name = 'installation';
+		$obj->path = JPATH_INSTALLATION;
+		$clientInfo[2] = clone $obj;
 
-        return $clientInfo;
-    }
+		return $clientInfo;
+	}
 
 
 }
