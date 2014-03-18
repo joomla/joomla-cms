246a14e5ce86cad1df56875bd1c6296d85285245
Constantin Romankiewicz <harmageddon95@web.de>
2014-03-18 18:12:35 +0100

    Removed strong tag from com_search view

--- components/com_search/views/search/tmpl/default_form.php
+++ components/com_search/views/search/tmpl/default_form.php
@@ -29,9 +29,7 @@ $upper_limit = $lang->getUpperLimitSearchWord();
 
 	<div class="searchintro<?php echo $this->params->get('pageclass_sfx'); ?>">
 		<?php if (!empty($this->searchword)):?>
-		<p>
-			<strong><?php echo JText::plural('COM_SEARCH_SEARCH_KEYWORD_N_RESULTS', '<span class="badge badge-info">'. $this->total. '</span>');?></strong>
-		</p>
+		<p><?php echo JText::plural('COM_SEARCH_SEARCH_KEYWORD_N_RESULTS', '<span class="badge badge-info">'. $this->total. '</span>');?></p>
 		<?php endif;?>
 	</div>
 
