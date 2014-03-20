efbb764fd7cfbd7ef291a7f5b28687d9e795480f
George Wilson <georgejameswilson@googlemail.com>
2014-03-20 18:35:34 +0000

    Use non-deprecated method

--- administrator/components/com_content/views/article/tmpl/edit.php
+++ administrator/components/com_content/views/article/tmpl/edit.php
@@ -138,7 +138,7 @@ if (isset($this->item->attribs['show_urls_images_backend']) && $this->item->attr
 
 		<?php if ($this->canDo->get('core.admin')) : ?>
 			<?php echo JHtml::_('bootstrap.addTab', 'myTab', 'editor', JText::_('COM_CONTENT_SLIDER_EDITOR_CONFIG', true)); ?>
-			<?php echo $this->form->getControlGroups('editorConfig'); ?>
+			<?php echo $this->form->renderFieldset('editorConfig'); ?>
 			<?php echo JHtml::_('bootstrap.endTab'); ?>
 		<?php endif; ?>
 
