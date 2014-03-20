6b17e5aa5cf69f31c2d751337e6cfaad8f02c27d
Puneet Kala <puneet.0191@gmail.com>
2014-03-20 11:41:22 +0530

    Updating Headers for Tests Class Files

--- tests/system/webdriver/tests/control_panel/ControlPanel0001Test.php
+++ tests/system/webdriver/tests/control_panel/ControlPanel0001Test.php
@@ -1,5 +1,11 @@
 <?php
-
+/**
+ * @package     Joomla.Test
+ * @subpackage  Webdriver
+ *
+ * @copyright   Copyright (C) 2005 - 2014 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
 require_once 'JoomlaWebdriverTestCase.php';
 
 use SeleniumClient\By;
