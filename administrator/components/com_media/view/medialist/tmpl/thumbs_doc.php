7ced10b4ad035045c53f329d16559db1f63bbca6
Buddhima <buddhimawijeweera@yahoo.com>
2014-03-22 23:41:15 +0530

    Modifications to thumbs-2

--- administrator/components/com_media/view/medialist/tmpl/thumbs_doc.php
+++ administrator/components/com_media/view/medialist/tmpl/thumbs_doc.php
@@ -17,18 +17,21 @@ $dispatcher->trigger('onContentBeforeDisplay', array('com_media.file', &$this->_
 		<li class="span2">
 			<article class="thumbnail center" >
 				<div class="height-20">
-					<?php if ($user->authorise('core.delete', 'com_media')):?>
-						<a class="close delete-item" target="_top" href="index.php?option=com_media&amp;task=file.delete&amp;tmpl=index&amp;<?php echo JSession::getFormToken(); ?>=1&amp;folder=<?php echo $this->state->folder; ?>&amp;rm[]=<?php echo $this->_tmp_doc->name; ?>" rel="<?php echo $this->_tmp_doc->name; ?>" title="<?php echo JText::_('JACTION_DELETE');?>">&#215;</a>
-						<input class="pull-left" type="checkbox" name="rm[]" value="<?php echo $this->_tmp_doc->name; ?>" />
-						<div class="clearfix"></div>
-					<?php endif;?>
+					
 				</div>
 				<div class="height-60">
 					<a style="display: block; width: 100%; height: 100%" title="<?php echo $this->_tmp_doc->name; ?>" >
 						<?php echo JHtml::_('image', $this->_tmp_doc->icon_32, $this->_tmp_doc->name, null, true, true) ? JHtml::_('image', $this->_tmp_doc->icon_32, $this->_tmp_doc->title, null, true) : JHtml::_('image', 'media/con_info.png', $this->_tmp_doc->name, null, true); ?></a>
 				</div>
 				<div class="height-20" title="<?php echo $this->_tmp_doc->name; ?>" >
-					<?php echo JHtml::_('string.truncate', $this->_tmp_doc->name, 10, false); ?>
+					<input class="pull-left" type="checkbox" name="rm[]" value="<?php echo $this->_tmp_doc->name; ?>" />
+					<?php echo JHtml::_('string.truncate', $this->_tmp_doc->name, 8, false); ?>
+					<?php if ($user->authorise('core.delete', 'com_media')):?>
+						<a class="close delete-item" target="_top" href="index.php?option=com_media&amp;task=file.delete&amp;tmpl=index&amp;<?php echo JSession::getFormToken(); ?>=1&amp;folder=<?php echo $this->state->folder; ?>&amp;rm[]=<?php echo $this->_tmp_doc->name; ?>" rel="<?php echo $this->_tmp_doc->name; ?>" title="<?php echo JText::_('JACTION_DELETE');?>">
+							<span class="label label-important">&#215;</span>
+						</a>
+						<div class="clearfix"></div>
+					<?php endif;?>
 				</div>
 			</article>
 		</li>
--- administrator/components/com_media/view/medialist/tmpl/thumbs_folder.php
+++ administrator/components/com_media/view/medialist/tmpl/thumbs_folder.php
@@ -13,11 +13,7 @@ $user = JFactory::getUser();
 		<li class="span2">
 			<article class="thumbnail center" >
 				<div class="height-20">
-				<?php if ($user->authorise('core.delete', 'com_media')):?>
-					<a class="close delete-item" target="_top" href="index.php?option=com_media&amp;controller=media.delete.medialist&amp;tmpl=index&amp;<?php echo JSession::getFormToken(); ?>=1&amp;folder=<?php echo $this->state->get('folder'); ?>&amp;rm[]=<?php echo $this->_tmp_folder->name; ?>" rel="<?php echo $this->_tmp_folder->name; ?> :: <?php echo $this->_tmp_folder->files + $this->_tmp_folder->folders; ?>" title="<?php echo JText::_('JACTION_DELETE');?>">&#215;</a>
-					<input class="pull-left" type="checkbox" name="rm[]" value="<?php echo $this->_tmp_folder->name; ?>" />
-					<div class="clearfix"></div>
-				<?php endif;?>
+				
 				</div>
 				<div class="height-60">
 					<a href="index.php?option=com_media&amp;controller=media.display.medialist&amp;view=medialist&amp;tmpl=component&amp;folder=<?php echo $this->_tmp_folder->path_relative; ?>" target="folderframe">
@@ -26,6 +22,13 @@ $user = JFactory::getUser();
 				</div>
 				<div class="height-20">
 					<a href="index.php?option=com_media&amp;controller=media.display.medialist&amp;view=medialist&amp;tmpl=component&amp;folder=<?php echo $this->_tmp_folder->path_relative; ?>" target="folderframe"><?php echo JHtml::_('string.truncate', $this->_tmp_folder->name, 10, false); ?></a>
+					<?php if ($user->authorise('core.delete', 'com_media')):?>
+					<a class="close delete-item" target="_top" href="index.php?option=com_media&amp;controller=media.delete.medialist&amp;tmpl=index&amp;<?php echo JSession::getFormToken(); ?>=1&amp;folder=<?php echo $this->state->get('folder'); ?>&amp;rm[]=<?php echo $this->_tmp_folder->name; ?>" rel="<?php echo $this->_tmp_folder->name; ?> :: <?php echo $this->_tmp_folder->files + $this->_tmp_folder->folders; ?>" title="<?php echo JText::_('JACTION_DELETE');?>">
+						<span class="label label-important">&#215;</span>
+					</a>
+					<input class="pull-left" type="checkbox" name="rm[]" value="<?php echo $this->_tmp_folder->name; ?>" />
+					<div class="clearfix"></div>
+				<?php endif;?>
 				</div>
 			</article>
 		</li>
