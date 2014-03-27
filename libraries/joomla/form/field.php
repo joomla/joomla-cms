4fe598cef9e35ead6049d765274c02272d55420d
Marko D <chivitli@users.noreply.github.com>
2014-03-27 20:45:29 +0100

    Update field.php

--- libraries/joomla/form/field.php
+++ libraries/joomla/form/field.php
@@ -900,18 +900,20 @@ abstract class JFormField
 			return $this->getInput();
 		}
 
-		if (empty($options['class']))
+		if (!isset($options['class']))
 		{
 			$options['class'] = '';
 		}
       
 		$options['rel'] = '';
 		
-		if (empty($options['hiddenLabel']) && $this->getAttribute('hiddenLabel')) {
+		if (empty($options['hiddenLabel']) && $this->getAttribute('hiddenLabel'))
+		{
 			$options['hiddenLabel'] = true;
 		}
       
-		if ($showon = $this->getAttribute('showon')) {
+		if ($showon = $this->getAttribute('showon'))
+		{
 			$showon   = explode(':', $showon, 2);
 			$options['class'] .= ' showon_' . implode(' showon_', explode(',', $showon[1]));
 			$id = $this->getName($showon[0]);
