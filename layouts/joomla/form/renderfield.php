8b06ce8220fbd65568886f090e1934c1feeb838d
Marko D <chivitli@users.noreply.github.com>
2014-03-27 16:59:45 +0100

    Update renderfield.php

--- layouts/joomla/form/renderfield.php
+++ layouts/joomla/form/renderfield.php
@@ -19,8 +19,8 @@ defined('_JEXEC') or die;
 
 ?>
 
-<div class="control-group">
-	<?php if (!isset($displayData['hiddenLabel']) || isset($displayData['hiddenLabel']) && $displayData['hiddenLabel'] == false) : ?>
+<div class="control-group <?php echo $displayData['options']['class']; ?>" <?php echo $displayData['options']['rel']; ?>>
+	<?php if (empty($displayData['options']['hiddenLabel'])) : ?>
 		<div class="control-label"><?php echo $displayData['label']; ?></div>
 	<?php endif; ?>
 	<div class="controls"><?php echo $displayData['input']; ?></div>
