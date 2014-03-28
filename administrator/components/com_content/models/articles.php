2174fb5fc2734aec7e8b69340af4ac5786261fbd
Thomas Hunziker <werbemails@bakual.ch>
2014-03-28 11:01:19 +0100

    Adding new default values to the getListQuery

--- administrator/components/com_content/models/articles.php
+++ administrator/components/com_content/models/articles.php
@@ -307,10 +307,10 @@ class ContentModelArticles extends JModelList
 		}
 
 		// Add the list ordering clause.
-		$orderCol = $this->state->get('list.ordering', 'a.title');
-		$orderDirn = $this->state->get('list.direction', 'asc');
-
-		if ($orderCol == 'a.ordering' || $orderCol == 'category_title')
+		$orderCol = $this->state->get('list.ordering', 'a.id');
+		$orderDirn = $this->state->get('list.direction', 'desc');
+ 
+ 		if ($orderCol == 'a.ordering' || $orderCol == 'category_title')
 		{
 			$orderCol = 'c.title ' . $orderDirn . ', a.ordering';
 		}
