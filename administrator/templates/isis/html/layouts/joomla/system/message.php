a23a7130aed59722202aeaeb485de30e3230d641
Bakual <werbemails@bakual.ch>
2014-04-04 15:01:46 +0200

    Correcting DocBloc

--- administrator/templates/isis/html/layouts/joomla/system/message.php
+++ administrator/templates/isis/html/layouts/joomla/system/message.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * @package     Joomla.Site
- * @subpackage  Layout
+ * @package     Joomla.Administrator
+ * @subpackage  Template.Isis
  *
  * @copyright   Copyright (C) 2005 - 2014 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
