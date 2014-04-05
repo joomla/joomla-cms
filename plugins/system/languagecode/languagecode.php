c4bcd024418953689d7336a8b3d92de74a6515c2
Marko D <chivitli@users.noreply.github.com>
2014-04-05 12:08:14 +0200

    Update languagecode.php

--- plugins/system/languagecode/languagecode.php
+++ plugins/system/languagecode/languagecode.php
@@ -95,7 +95,7 @@ class PlgSystemLanguagecode extends JPlugin
 	 * Prepare form.
 	 *
 	 * @param   JForm  $form  The form to be altered.
-	 * @param   mixed $data  The associated data for the form.
+	 * @param   mixed  $data  The associated data for the form.
 	 *
 	 * @return  boolean
 	 *
