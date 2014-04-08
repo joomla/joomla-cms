c05d7efb78863c76ed5aaf498a658ba79131fb6d
Marko D <chivitli@users.noreply.github.com>
2014-04-08 18:24:27 +0200

    Update joomla.sql

--- installation/sql/mysql/joomla.sql
+++ installation/sql/mysql/joomla.sql
@@ -1932,7 +1932,7 @@ CREATE TABLE IF NOT EXISTS `#__user_notes` (
 CREATE TABLE IF NOT EXISTS `#__user_profiles` (
   `user_id` int(11) NOT NULL,
   `profile_key` varchar(100) NOT NULL,
-  `profile_value` varchar(255) NOT NULL,
+  `profile_value` text NOT NULL,
   `ordering` int(11) NOT NULL DEFAULT 0,
   UNIQUE KEY `idx_user_id_profile_key` (`user_id`,`profile_key`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Simple user profile storage table';
