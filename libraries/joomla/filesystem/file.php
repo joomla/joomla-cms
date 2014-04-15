c15e8a4372132b2c1e2e042594d1b8884c432ebe
zero-24 <zero.mail@gmx.de>
2014-04-15 13:39:26 +0200

    Update file.php
    Same fix like #3448 for Joomla 2.5.x

--- libraries/joomla/filesystem/file.php
+++ libraries/joomla/filesystem/file.php
@@ -188,6 +188,11 @@ class JFile
 
 		foreach ($files as $file)
 		{
+			if (!is_file($file))
+			{
+				continue;
+			}
+			
 			$file = JPath::clean($file);
 
 			// Try making the file writable first. If it's read-only, it can't be deleted
