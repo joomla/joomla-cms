135086d00b68ace854157bf429272cfaa508a854
Bakual <werbemails@bakual.ch>
2014-04-17 15:09:01 +0200

    Codestyle

--- components/com_users/views/login/view.html.php
+++ components/com_users/views/login/view.html.php
@@ -53,7 +53,7 @@ class UsersViewLogin extends JViewLegacy
 		{
 			$this->setLayout($active->query['layout']);
 		}
-		
+
 		require_once JPATH_ADMINISTRATOR . '/components/com_users/helpers/users.php';
 		$tfa = UsersHelper::getTwoFactorMethods();
 		$this->tfa = is_array($tfa) && count($tfa) > 1;
