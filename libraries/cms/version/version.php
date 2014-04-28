fbd27dd6eb3aee1046ba538df138c09306dc2286
George Wilson <georgejameswilson@googlemail.com>
2014-04-28 11:20:39 +0100

    Remove unneeded constant

--- libraries/cms/version/version.php
+++ libraries/cms/version/version.php
@@ -63,7 +63,7 @@ final class JVersion
 	 */
 	public function isCompatible($minimum)
 	{
-		return version_compare(JVERSION, $minimum, 'ge');
+		return version_compare($this->getShortVersion(), $minimum, 'ge');
 	}
 
 	/**
