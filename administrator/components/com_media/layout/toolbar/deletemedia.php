caf6f55768e7bb5df7795e269f45844cb0876b8b
Buddhima <buddhimawijeweera@yahoo.com>
2014-04-30 23:07:38 +0530

    Fixed batch delete issue

--- administrator/components/com_media/layout/toolbar/deletemedia.php
+++ administrator/components/com_media/layout/toolbar/deletemedia.php
@@ -11,6 +11,6 @@ defined('_JEXEC') or die;
 
 $title = JText::_('JTOOLBAR_DELETE');
 ?>
-<button onclick="MediaManager.submit('media.delete.medialist')" class="btn btn-small">
+<button onclick="MediaManager.submit('media.delete.media')" class="btn btn-small">
 	<i class="icon-remove" title="<?php echo $title; ?>"></i> <?php echo $title; ?>
 </button>
