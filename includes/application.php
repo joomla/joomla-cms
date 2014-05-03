829631615e59ffad3571eade6b8d357ba0e3d754
tuxmerlin <migueltuyare@gmail.com>
2014-05-03 20:42:34 -0300

    [fix] Librería del  idioma  predeterminado

--- includes/application.php
+++ includes/application.php
@@ -123,11 +123,8 @@ final class JSite extends JApplication
 		$lang = JFactory::getLanguage();
 
 		// Try the lib_joomla file in the current language (without allowing the loading of the file in the default language)
-		$lang->load('lib_joomla', JPATH_SITE, null, false, false)
-		|| $lang->load('lib_joomla', JPATH_ADMINISTRATOR, null, false, false)
-		// Fallback to the lib_joomla file in the default language
-		|| $lang->load('lib_joomla', JPATH_SITE, null, true)
-		|| $lang->load('lib_joomla', JPATH_ADMINISTRATOR, null, true);
+		$lang->load('lib_joomla', JPATH_SITE, null, false, true)
+			|| $lang->load('lib_joomla', JPATH_ADMINISTRATOR, null, false, true);
 	}
 
 	/**
