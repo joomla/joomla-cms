9f6357f06b3a55c82f84e3bdc5454427704acf9d
Kubik-Rubik <admin@kubik-rubik.de>
2014-05-05 14:14:23 +0200

    [bug] [33701] User Notes Categories - Error in menu and filter selection Close #3559

--- administrator/components/com_users/views/notes/view.html.php
+++ administrator/components/com_users/views/notes/view.html.php
@@ -153,7 +153,7 @@ class UsersViewNotes extends JViewLegacy
 		JHtmlSidebar::addFilter(
 			JText::_('JOPTION_SELECT_CATEGORY'),
 			'filter_category_id',
-			JHtml::_('select.options', JHtml::_('category.options', 'com_users.notes'), 'value', 'text', $this->state->get('filter.category_id'))
+			JHtml::_('select.options', JHtml::_('category.options', 'com_users'), 'value', 'text', $this->state->get('filter.category_id'))
 		);
 	}
 
