649e538a02220eb9d85a091fde76c5bf3211bab2
zero-24 <zero.mail@gmx.de>
2014-05-05 14:30:33 +0200

    Update default_message.php
    See:
    http://issues.joomla.org/tracker/joomla-cms/3560
    and here:
    http://joomlacode.org/gf/project/joomla/tracker/?action=TrackerItemEdit&tracker_item_id=30564&start=0
    
    This PR is a rewrite against staging of this PR by @drmmr763 agianst master

--- administrator/components/com_installer/views/default/tmpl/default_message.php
+++ administrator/components/com_installer/views/default/tmpl/default_message.php
@@ -13,17 +13,14 @@ $state			= $this->get('State');
 $message1		= $state->get('message');
 $message2		= $state->get('extension_message');
 ?>
-<table class="adminform">
-	<tbody>
-		<?php if ($message1) : ?>
-		<tr>
-			<th><?php echo $message1 ?></th>
-		</tr>
-		<?php endif; ?>
-		<?php if ($message2) : ?>
-		<tr>
-			<td><?php echo $message2; ?></td>
-		</tr>
-		<?php endif; ?>
-	</tbody>
-</table>
+
+<?php if ($message1) : ?> 
+	<div class="span12"> 
+		<?php echo $message1; ?> 
+	</div> 
+<?php endif; ?> 
+<?php if ($message2) : ?> 
+	<div class="span12"> 
+		<?php echo $message2; ?> 
+	</div> 
+<?php endif; ?>
