45358020595ddb9a16ecd8c4d23dbe45edf964a3
Tino Brackebusch <Tinobee@gmail.com>
2014-05-07 17:53:24 +0200

    Created new helper class for JImage
    see #3568

---
+++ libraries/joomla/image/helper.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @package     Joomla.Platform
+ * @subpackage  Mail
+ *
+ * @copyright   Copyright (C) 2005 - 2014 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+
+defined('JPATH_PLATFORM') or die;
+
+/**
+ * Image helper class, provides static methods to perform various tasks relevant
+ * to the Joomla image routines.
+ *
+ * @package     Joomla.Platform
+ * @subpackage  Mail
+ * @since       13.1
+ */
+abstract class JMailHelper
+{
+}
