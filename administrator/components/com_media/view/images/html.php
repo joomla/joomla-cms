225ade599ec99c7d9f93a348b260a74b8c364738
Buddhima <buddhimawijeweera@yahoo.com>
2014-05-18 00:05:09 +0530

    Fixed issue with displaying image information

--- administrator/components/com_media/view/images/html.php
+++ administrator/components/com_media/view/images/html.php
@@ -42,9 +42,6 @@ class MediaViewImagesHtml extends ConfigViewCmsHtml
 		$this->config = $config;
 		$this->state = $this->model->getState();
 		$this->folderList = $this->model->getFolderList();
-
-		$document = JFactory::getDocument();
-// 		$document->addScriptDeclaration("var ImageManager = window.parent.ImageManager;");
 		
 		$this->require_ftp = $ftp;
 
