3fccf1879be17c6d9fa9c32fe0e9dd52f07f8049
Jurian Even <jurian@twentronix.com>
2014-05-31 18:38:55 +0200

    Don't wrap the update message with an alert box

--- administrator/components/com_installer/views/update/tmpl/default.php
+++ administrator/components/com_installer/views/update/tmpl/default.php
@@ -24,10 +24,7 @@ $listDirn	= $this->escape($this->state->get('list.direction'));
 	<div id="j-main-container" class="span10">
 
 	<?php if ($this->showMessage) : ?>
-		<div class="alert alert-info">
-			<a class="close" data-dismiss="alert" href="#">&times;</a>
-			<?php echo $this->loadTemplate('message'); ?>
-		</div>
+		<?php echo $this->loadTemplate('message'); ?>
 	<?php endif; ?>
 
 	<?php if ($this->ftp) : ?>
