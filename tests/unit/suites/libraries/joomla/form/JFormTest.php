4758f6e1f9ca8b70cc452bb1a14c309cae5a6b5b
Bakual <werbemails@bakual.ch>
2014-06-03 14:11:20 +0200

    Update UnitTest to reflect the new default class "advancedSelect" for the list formfield.

--- tests/unit/suites/libraries/joomla/form/JFormTest.php
+++ tests/unit/suites/libraries/joomla/form/JFormTest.php
@@ -1112,7 +1112,7 @@ class JFormTest extends TestCase
 		$this->assertThat(
 			$form->getInput('colours', 'params', 'blue'),
 			$this->equalTo(
-				'<select id="jform_params_colours" name="jform[params][colours][]" multiple>' .
+				'<select id="jform_params_colours" name="jform[params][colours][]" class="advancedSelect" multiple>' .
 					"\n\t" . '<option value="red">Red</option>' .
 					"\n\t" . '<option value="blue" selected="selected">Blue</option>' .
 					"\n\t" . '<option value="green">Green</option>' .
