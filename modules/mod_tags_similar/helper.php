3f1b863ec746dfcace919e2d3675b883f126ce0c
Viktor Vogel <admin@kubik-rubik.de>
2014-06-07 09:35:52 +0200

    "Similar Tags" module ignores access permissions  Fix for all content types

--- modules/mod_tags_similar/helper.php
+++ modules/mod_tags_similar/helper.php
@@ -78,12 +78,11 @@ abstract class ModTagssimilarHelper
 
 		$query->join('INNER', $db->quoteName('#__tags', 't') . ' ON m.tag_id = t.id')
 			->join('INNER', $db->quoteName('#__ucm_content', 'cc') . ' ON m.core_content_id = cc.core_content_id')
-			->join('INNER', $db->quoteName('#__content_types', 'ct') . ' ON m.type_alias = ct.type_alias')
-			->join('INNER', $db->quoteName('#__content', 'cn') . ' ON m.content_item_id = cn.id');
+			->join('INNER', $db->quoteName('#__content_types', 'ct') . ' ON m.type_alias = ct.type_alias');
 
 		$query->where($db->quoteName('m.tag_id') . ' IN (' . $tagsToMatch . ')');
 		$query->where('t.access IN (' . $groups . ')');
-		$query->where('cn.access IN (' . $groups . ')');
+		$query->where('cc.core_access IN (' . $groups . ')');
 
 		// Don't show current item
 		$query->where('(' . $db->quoteName('m.content_item_id') . ' <> ' . $id . ' OR ' . $db->quoteName('m.type_alias') . ' <> ' . $db->quote($prefix) . ')');
