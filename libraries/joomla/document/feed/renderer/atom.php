3bf1f4f14d8cf2a38d76898deb0334138f08a13f
George Wilson <georgejameswilson@googlemail.com>
2014-06-09 14:35:58 +0100

    Dynamically generate version

--- libraries/joomla/document/feed/renderer/atom.php
+++ libraries/joomla/document/feed/renderer/atom.php
@@ -111,7 +111,10 @@ class JDocumentRendererAtom extends JDocumentRenderer
 			}
 			$feed .= "	</author>\n";
 		}
-		$feed .= "	<generator uri=\"http://joomla.org\" version=\"1.6\">" . $data->getGenerator() . "</generator>\n";
+
+		$version = new JVersion;
+
+		$feed .= "	<generator uri=\"http://joomla.org\" version=\"" . $version->RELEASE . "\">" . $data->getGenerator() . "</generator>\n";
 		$feed .= '	<link rel="self" type="application/atom+xml" href="' . str_replace(' ', '%20', $url . $syndicationURL) . "\"/>\n";
 
 		for ($i = 0, $count = count($data->items); $i < $count; $i++)
