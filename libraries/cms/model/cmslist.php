5375af76106dc5bb41007bbef9b18a523629d8e1
wilsonge <georgejameswilson@googlemail.com>
2014-06-12 19:18:12 +0100

    Upload Elin's work on models

---
+++ libraries/cms/model/cmslist.php
@@ -0,0 +1,644 @@
+<?php
+/**
+ * @package     Joomla.Libraries
+ * @subpackage  Model
+ *
+ * @copyright   Copyright (C) 2005 - 2013 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+
+defined('JPATH_PLATFORM') or die;
+
+/**
+ * Prototype model for an item or collection of items.
+ *
+ * @package     Joomla.Libraries
+ * @subpackage  Model
+ * @since       3.4
+ */
+class JModelCmslist extends JModelCmsitem
+{
+	/**
+	 * Internal memory based cache array of data.
+	 *
+	 * @var    array
+	 * @since  3.4
+	 */
+	protected $cache = array();
+
+	/**
+	 * Valid filter fields or ordering.
+	 *
+	 * @var    array
+	 * @since  3.4
+	 */
+	protected $filter_fields = array();
+
+	/**
+	 * An internal cache for the last query used.
+	 *
+	 * @var    JDatabaseQuery
+	 * @since  3.4
+	 */
+	protected $query = array();
+
+	/**
+	 * The data type of the key that defines the list, usually integer, array, or string.
+	 *
+	 * @var    string
+	 * @since  3.4
+	 */
+	public $idSchema;
+
+	/**
+	 * Constructor.
+	 *
+	 * @param   array  $config  An optional associative array of configuration settings.
+	 *
+	 * @see     JModel
+	 * @since   3.4
+	 */
+	public function __construct($config = array())
+	{
+		parent::__construct($config);
+
+		// Add the ordering filtering fields white list.
+		if (isset($config['filter_fields']))
+		{
+			$this->filter_fields = $config['filter_fields'];
+		}
+
+		// Guess the context as Option.ModelName.
+		if (empty($this->context))
+		{
+			$this->context = strtolower($this->option . '.' . $this->getName());
+		}
+	}
+
+	/**
+	 * Method to cache the last query constructed.
+	 *
+	 * This method ensures that the query is constructed only once for a given state of the model.
+	 *
+	 * @return  JDatabaseQuery  A JDatabaseQuery object
+	 *
+	 * @since   3.4
+	 */
+	protected function _getListQuery()
+	{
+		// Capture the last store id used.
+		static $lastStoreId;
+
+		// Compute the current store id.
+		$currentStoreId = $this->getStoreId();
+
+		// If the last store id is different from the current, refresh the query.
+		if ($lastStoreId != $currentStoreId || empty($this->query))
+		{
+			$lastStoreId = $currentStoreId;
+			$this->query = $this->getListQuery();
+		}
+
+		return $this->query;
+	}
+
+	/**
+	 * Method to get a JDatabaseQuery object for retrieving the data set from a database.
+	 *
+	 * @return  JDatabaseQuery   A JDatabaseQuery object to retrieve the data set.
+	 *
+	 * @since   3.4
+	 */
+	protected function getListQuery()
+	{
+		$db = JFactory::getDbo();
+		$query = $db->getQuery(true);
+
+		return $query;
+	}
+
+
+	/**
+	 * Method to get an array of data items.
+	 *
+	 * @return  mixed  An array of data items on success, false on failure.
+	 *
+	 * @since   3.4
+	 */
+	public function getItems()
+	{
+		// Get a storage key.
+		$store = $this->getStoreId();
+
+		// Try to load the data from internal storage.
+		if (isset($this->cache[$store]))
+		{
+			return $this->cache[$store];
+		}
+
+		// Load the list items.
+		$query = $this->getListQuery();
+
+		try
+		{
+			$items = $this->getList($query, $this->getStart(), $this->state->get('list.limit'));
+		}
+		catch (RuntimeException $e)
+		{
+			$app = JFactory::getApplication();
+			$app->enqueueMessage($e->getMessage(), 'error');
+
+			return false;
+		}
+
+		// Add the items to the internal cache.
+		$this->cache[$store] = $items;
+
+		return $this->cache[$store];
+	}
+
+	/**
+	 * Method to get a JPagination object for the data set.
+	 *
+	 * @return  JPagination  A JPagination object for the data set.
+	 *
+	 * @since   3.4
+	 */
+	public function getPagination()
+	{
+		// Get a storage key.
+		$store = $this->getStoreId('getPagination');
+
+		// Try to load the data from internal storage.
+		if (isset($this->cache[$store]))
+		{
+			return $this->cache[$store];
+		}
+
+		// Create the pagination object.
+		$limit = (int) $this->state->get('list.limit') - (int) $this->state->get('list.links');
+		$page = new JPagination($this->getTotal(), $this->getStart(), $limit);
+
+		// Add the object to the internal cache.
+		$this->cache[$store] = $page;
+
+		return $this->cache[$store];
+	}
+
+	/**
+	 * Method to get a store id based on the model configuration state.
+	 *
+	 * This is necessary because the model is used by the component and
+	 * different modules that might need different sets of data or different
+	 * ordering requirements.
+	 *
+	 * @param   string  $id  An identifier string to generate the store id.
+	 *
+	 * @return  string  A store id.
+	 *
+	 * @since   3.4
+	 */
+	protected function getStoreId($id = '')
+	{
+		// Add the list state to the store id.
+		$id .= ':' . $this->state->get('list.start');
+		$id .= ':' . $this->state->get('list.limit');
+		$id .= ':' . $this->state->get('list.ordering');
+		$id .= ':' . $this->state->get('list.direction');
+
+		return md5($this->context . ':' . $id);
+	}
+
+	/**
+	 * Method to get the total number of items for the data set.
+	 *
+	 * @return  integer  The total number of items available in the data set.
+	 *
+	 * @since   3.4
+	 */
+	public function getTotal()
+	{
+		// Get a storage key.
+		$store = $this->getStoreId('getTotal');
+
+		// Try to load the data from internal storage.
+		if (isset($this->cache[$store]))
+		{
+			return $this->cache[$store];
+		}
+
+		// Load the total.
+		$query = $this->getListQuery();
+		try
+		{
+			$total = (int) $this->getListCount($query);
+		}
+		catch (RuntimeException $e)
+		{
+			$app = JFactory::getApplication();
+			$app->enqueueMessage($e->getMessage(), 'error');
+
+			return false;
+		}
+
+		// Add the total to the internal cache.
+		$this->cache[$store] = $total;
+
+		return $this->cache[$store];
+	}
+
+	/**
+	 * Method to get the starting number of items for the data set.
+	 *
+	 * @return  integer  The starting number of items available in the data set.
+	 *
+	 * @since   3.4
+	 */
+	public function getStart()
+	{
+		$store = $this->getStoreId('getstart');
+
+		// Try to load the data from internal storage.
+		if (isset($this->cache[$store]))
+		{
+			return $this->cache[$store];
+		}
+
+		$start = $this->state->get('list.start');
+		$limit = $this->state->get('list.limit');
+		$total = $this->getTotal();
+		if ($start > $total - $limit)
+		{
+			$start = max(0, (int) (ceil($total / $limit) - 1) * $limit);
+		}
+
+		// Add the total to the internal cache.
+		$this->cache[$store] = $start;
+
+		return $this->cache[$store];
+	}
+
+	/**
+	 * Method to auto-populate the model state.
+	 *
+	 * This method should only be called once per instantiation and is designed
+	 * to be called on the first call to the getState() method unless the model
+	 * configuration flag to ignore the request is set.
+	 *
+	 * Note. Calling getState in this method will result in recursion.
+	 *
+	 * @param   string  $ordering   An optional ordering field.
+	 * @param   string  $direction  An optional direction (asc|desc).
+	 *
+	 * @return  void
+	 *
+	 * @since   3.4
+	 */
+	protected function populateState($ordering = null, $direction = null)
+	{
+		// If the context is set, assume that stateful lists are used.
+		if ($this->context)
+		{
+			$app = JFactory::getApplication();
+
+			$value = $app->getUserStateFromRequest('global.list.limit', 'limit', $app->getCfg('list_limit'), 'uint');
+			$limit = $value;
+			$this->state->set('list.limit', $limit);
+
+			$value = $app->getUserStateFromRequest($this->context . '.limitstart', 'limitstart', 0);
+			$limitstart = ($limit != 0 ? (floor($value / $limit) * $limit) : 0);
+			$this->state->set('list.start', $limitstart);
+
+			// Check if the ordering field is in the white list, otherwise use the incoming value.
+			$value = $app->getUserStateFromRequest($this->context . '.ordercol', 'filter_order', $ordering);
+			if (!in_array($value, $this->filter_fields))
+			{
+				$value = $ordering;
+				$app->setUserState($this->context . '.ordercol', $value);
+			}
+			$this->state->set('list.ordering', $value);
+
+			// Check if the ordering direction is valid, otherwise use the incoming value.
+			$value = $app->getUserStateFromRequest($this->context . '.orderdirn', 'filter_order_Dir', $direction);
+			if (!in_array(strtoupper($value), array('ASC', 'DESC', '')))
+			{
+				$value = $direction;
+				$app->setUserState($this->context . '.orderdirn', $value);
+			}
+
+			$this->state->set('list.direction', $value);
+		}
+		else
+		{
+			$this->state->set('list.start', 0);
+			$this->state->set('list.limit', 0);
+		}
+	}
+
+	/**
+	 * Gets the value of a user state variable and sets it in the session
+	 *
+	 * This is the same as the method in JApplication except that this also can optionally
+	 * force you back to the first page when a filter has changed
+	 *
+	 * @param   string   $key        The key of the user state variable.
+	 * @param   string   $request    The name of the variable passed in a request.
+	 * @param   string   $default    The default value for the variable if not found. Optional.
+	 * @param   string   $type       Filter for the variable, for valid values see {@link JFilterInput::clean()}. Optional.
+	 * @param   boolean  $resetPage  If true, the limitstart in request is set to zero
+	 *
+	 * @return  The request user state.
+	 *
+	 * @since   3.4
+	 */
+	public function getUserStateFromRequest($key, $request, $default = null, $type = 'none', $resetPage = true)
+	{
+		$app = JFactory::getApplication();
+		$input     = $app->input;
+		$old_state = $app->getUserState($key);
+		$cur_state = (!is_null($old_state)) ? $old_state : $default;
+		$new_state = $input->get($request, null, $type);
+
+		if (($cur_state != $new_state) && ($resetPage))
+		{
+			$input->set('limitstart', 0);
+		}
+
+		// Save the new value only if it is set in this request.
+		if ($new_state !== null)
+		{
+			$app->setUserState($key, $new_state);
+		}
+		else
+		{
+			$new_state = $cur_state;
+		}
+
+		return $new_state;
+	}
+
+	/**
+	 * Gets an array of objects from the results of database query.
+	 *
+	 * @param   string   $query       The query.
+	 * @param   integer  $limitstart  Offset.
+	 * @param   integer  $limit       The number of records.
+	 *
+	 * @return  array  An array of results.
+	 *
+	 * @since   3.4
+	 * @throws  RuntimeException
+	 */
+	protected function getList($query, $limitstart = 0, $limit = 0)
+	{
+		$this->db->setQuery($query, $limitstart, $limit);
+		$result = $this->db->loadObjectList();
+
+		return $result;
+	}
+
+	/**
+	 * Returns a record count for the query.
+	 *
+	 * @param   JDatabaseQuery|string  $query  The query.
+	 *
+	 * @return  integer  Number of rows for query.
+	 *
+	 * @since   3.4
+	 */
+	protected function getListCount($query)
+	{
+		// Use fast COUNT(*) on JDatabaseQuery objects if there no GROUP BY or HAVING clause:
+		if ($query instanceof JDatabaseQuery
+				&& $query->type == 'select'
+				&& $query->group === null
+				&& $query->having === null)
+		{
+			$query = clone $query;
+			$query->clear('select')->clear('order')->select('COUNT(*)');
+
+			$this->_db->setQuery($query);
+			return (int) $this->_db->loadResult();
+		}
+
+		// Otherwise fall back to inefficient way of counting all results.
+		$this->_db->setQuery($query);
+		$this->_db->execute();
+
+		return (int) $this->_db->getNumRows();
+	}
+
+	/**
+	 * Method override to check-in a record or an array of record
+	 *
+	 * @param   mixed  $pks  The ID of the primary key or an array of IDs
+	 *
+	 * @return  mixed  Boolean false if there is an error, otherwise the count of records checked in.
+	 *
+	 * @since   3.4
+	 */
+	public function checkin($pks = array())
+	{
+		$pks = (array) $pks;
+		$table = $this->getTable();
+		$count = 0;
+
+		if (empty($pks))
+		{
+			$pks = array((int) $this->getState($this->getName() . '.id'));
+		}
+
+		// Check in all items.
+		foreach ($pks as $pk)
+		{
+			if ($table->load($pk))
+			{
+
+				if ($table->checked_out > 0)
+				{
+					if (!parent::checkin($pk))
+					{
+						return false;
+					}
+					$count++;
+				}
+			}
+			else
+			{
+				$this->setError($table->getError());
+
+				return false;
+			}
+		}
+
+		return $count;
+	}
+
+	/**
+	 * Method to change the published state of one or more records.
+	 *
+	 * @param   array    &$pks   A list of the primary keys to change.
+	 * @param   integer  $value  The value of the published state.
+	 *
+	 * @return  boolean  True on success.
+	 *
+	 * @since   3.4
+	 */
+	public function publish(&$pks, $value = 1)
+	{
+		$dispatcher = JEventDispatcher::getInstance();
+		$user = JFactory::getUser();
+		$table = $this->getTable();
+		$pks = (array) $pks;
+
+		// Include the content plugins for the change of state event.
+		JPluginHelper::importPlugin('content');
+
+		// Access checks.
+		foreach ($pks as $i => $pk)
+		{
+			$table->reset();
+
+			if ($table->load($pk))
+			{
+				if (!$this->canEditState($table))
+				{
+					// Prune items that you can't change.
+					unset($pks[$i]);
+					JLog::add(JText::_('JLIB_APPLICATION_ERROR_EDITSTATE_NOT_PERMITTED'), JLog::WARNING, 'jerror');
+
+					return false;
+				}
+			}
+		}
+
+		// Attempt to change the state of the records.
+		if (!$table->publish($pks, $value, $user->get('id')))
+		{
+			$this->setError($table->getError());
+
+			return false;
+		}
+
+		$context = $this->option . '.' . $this->name;
+
+		// Trigger the onContentChangeState event.
+		$result = $dispatcher->trigger($this->event_change_state, array($context, $pks, $value));
+
+		if (in_array(false, $result, true))
+		{
+			$this->setError($table->getError());
+
+			return false;
+		}
+
+		// Clear the component's cache
+		$this->cleanCache();
+
+		return true;
+	}
+
+	/**
+	 * Saves the manually set order of records.
+	 *
+	 * @param   array    $pks    An array of primary key ids.
+	 * @param   integer  $order  +1 or -1
+	 *
+	 * @return  mixed
+	 *
+	 * @since   3.4
+	 */
+	public function saveorder($pks = null, $order = null)
+	{
+		$table = $this->getTable();
+		$tableClassName = get_class($table);
+		$contentType = new JUcmType;
+		$type = $contentType->getTypeByTable($tableClassName);
+		if ($type)
+		{
+			$typeAlias = $type->type_alias;
+			$tagsObserver = $table->getObserverOfClass('JTableObserverTags');
+		}
+		$conditions = array();
+
+		if (empty($pks))
+		{
+			return JError::raiseWarning(500, JText::_($this->text_prefix . '_ERROR_NO_ITEMS_SELECTED'));
+		}
+
+		// Update ordering values
+		foreach ($pks as $i => $pk)
+		{
+			$table->load((int) $pk);
+
+			// Access checks.
+			if (!$this->canEditState($table))
+			{
+				// Prune items that you can't change.
+				unset($pks[$i]);
+				JLog::add(JText::_('JLIB_APPLICATION_ERROR_EDITSTATE_NOT_PERMITTED'), JLog::WARNING, 'jerror');
+			}
+			elseif ($table->ordering != $order[$i])
+			{
+				$table->ordering = $order[$i];
+
+				if ($type  && !empty($typeAlias))
+				{
+					$this->createTagsHelper($tagsObserver, $type, $pk, $typeAlias, $table);
+				}
+
+				if (!$table->store())
+				{
+					$this->setError($table->getError());
+					return false;
+				}
+
+				// Remember to reorder within position and client_id
+				$condition = $this->getReorderConditions($table);
+				$found = false;
+
+				foreach ($conditions as $cond)
+				{
+					if ($cond[1] == $condition)
+					{
+						$found = true;
+						break;
+					}
+				}
+
+				if (!$found)
+				{
+					$key = $table->getKeyName();
+					$conditions[] = array($table->$key, $condition);
+				}
+			}
+		}
+
+		// Execute reorder for each category.
+		foreach ($conditions as $cond)
+		{
+			$table->load($cond[0]);
+			$table->reorder($cond[1]);
+		}
+
+		// Clear the component's cache
+		$this->cleanCache();
+
+		return true;
+	}
+
+	/**
+	 * A protected method to get a set of ordering conditions.
+	 *
+	 * @param   JTable  $table  A JTable object.
+	 *
+	 * @return  array  An array of conditions to add to ordering queries.
+	 *
+	 * @since   3.4
+	 */
+	protected function getReorderConditions($table)
+	{
+		return array();
+	}
+
+}
