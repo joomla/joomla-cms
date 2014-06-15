e940722334bb4ebb3e7d8dd5be828e08535050c3
zero-24 <zero-24@users.noreply.github.com>
2014-06-15 12:24:53 +0200

    Update component.php

--- templates/protostar/component.php
+++ templates/protostar/component.php
@@ -9,16 +9,16 @@
 
 defined('_JEXEC') or die;
 
-$app   = JFactory::getApplication();
-$doc   = JFactory::getDocument();
-$this->language = $doc->language;
+$app             = JFactory::getApplication();
+$doc             = JFactory::getDocument();
+$this->language  = $doc->language;
 $this->direction = $doc->direction;
 
 // Add JavaScript Frameworks
 JHtml::_('bootstrap.framework');
 
 // Add Stylesheets
-$doc->addStyleSheet('templates/'.$this->template.'/css/template.css');
+$doc->addStyleSheet('templates/' . $this->template . '/css/template.css');
 
 // Load optional rtl Bootstrap css and Bootstrap bugfixes
 JHtmlBootstrap::loadCss($includeMaincss = false, $this->direction);
