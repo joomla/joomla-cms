1023ee77089e67594fc8284deddf2ca0d78800bc
George Wilson <georgejameswilson@googlemail.com>
2014-06-16 02:37:09 +0100

    Update doc block

--- libraries/cms/model/cmsbase.php
+++ libraries/cms/model/cmsbase.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @package     Joomla.Legacy
+ * @package     Joomla.Libraries
  * @subpackage  Model
  *
  * @copyright   Copyright (C) 2005 - 2013 Open Source Matters, Inc. All rights reserved.
