86a940f9e1c215f72067a7b616fbb1ad125a6c62
George Wilson <georgejameswilson@googlemail.com>
2014-06-16 03:06:47 +0100

    Create cmsform.php

---
+++ libraries/cms/model/cmsform.php
@@ -0,0 +1,237 @@
+<?php
+/**
+ * @package     Joomla.Libraries
+ * @subpackage  Model
+ *
+ * @copyright   Copyright (C) 2005 - 2013 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+
+defined('JPATH_PLATFORM') or die;
+
+/**
+ * Prototype item model.
+ *
+ * @package     Joomla.Model
+ * @subpackage  Model
+ * @since       3.4
+ */
+abstract class JModelCmsform extends JModelCmsitem implements JModelFormInterface
+{
+	/**
+	 * Array of form objects.
+	 *
+	 * @var    array
+	 * @since  3.4
+	 */
+	protected $forms = array();
+
+	/**
+	 * Abstract method for getting the form from the model.
+	 *
+	 * @param   array    $data      Data for the form.
+	 * @param   boolean  $loadData  True if the form is to load its own data (default case), false if not.
+	 *
+	 * @return  mixed  A JForm object on success, false on failure
+	 *
+	 * @since   3.4
+	 */
+	abstract public function getForm($data = array(), $loadData = true)
+
+	/**
+	 * Prepare and sanitise the table data prior to saving.
+	 *
+	 * @param   JTable  $table  A reference to a JTable object.
+	 *
+	 * @return  void
+	 *
+	 * @since   3.4
+	 */
+	protected function prepareTable($table)
+	{
+		// Derived class will provide its own implementation if required.
+	}
+
+	/**
+	 * Method to get a form object.
+	 *
+	 * @param   string   $name     The name of the form.
+	 * @param   string   $source   The form source. Can be XML string if file flag is set to false.
+	 * @param   array    $options  Optional array of options for the form creation.
+	 * @param   boolean  $clear    Optional argument to force load a new form.
+	 * @param   string   $xpath    An optional xpath to search for the fields.
+	 *
+	 * @return  mixed  JForm object on success, False on error.
+	 *
+	 * @see     JForm
+	 * @since   3.4
+	 */
+	protected function loadForm($name, $source = null, $options = array(), $clear = false, $xpath = false)
+	{
+		// Handle the optional arguments.
+		$options['control'] = JArrayHelper::getValue($options, 'control', false);
+
+		// Create a signature hash.
+		$hash = sha1($source . serialize($options));
+
+		// Check if we can use a previously loaded form.
+		if (isset($this->forms[$hash]) && !$clear)
+		{
+			return $this->forms[$hash];
+		}
+
+		// Get the form.
+		// Register the paths for the form -- failing here
+		JForm::addFormPath(JPATH_COMPONENT . '/models/forms');
+		JForm::addFieldPath(JPATH_COMPONENT . '/models/fields');
+		JForm::addFormPath(JPATH_COMPONENT . '/model/form');
+		JForm::addFieldPath(JPATH_COMPONENT . '/model/field');
+
+		try
+		{
+			$form = JForm::getInstance($name, $source, $options, false, $xpath);
+
+			if (isset($options['load_data']) && $options['load_data'])
+			{
+				// Get the data for the form.
+				$data = $this->loadFormData();
+			}
+			else
+			{
+				$data = array();
+			}
+
+			// Allow for additional modification of the form, and events to be triggered.
+			// We pass the data because plugins may require it.
+			$this->preprocessForm($form, $data);
+
+			// Load the data into the form after the plugins have operated.
+			$form->bind($data);
+
+		}
+		catch (Exception $e)
+		{
+
+			$app = JFactory::getApplication();
+			$app->enqueueMessage($e->getMessage());
+
+			return false;
+		}
+
+		// Store the form for later.
+		$this->forms[$hash] = $form;
+
+		return $form;
+	}
+
+	/**
+	 * Method to get the data that should be injected in the form.
+	 *
+	 * @return  array    The default data is an empty array.
+	 *
+	 * @since   3.4
+	 */
+	public function loadFormData()
+	{
+		return array();
+	}
+
+	/**
+	 * Method to allow derived classes to preprocess the data.
+	 *
+	 * @param   string  $context  The context identifier.
+	 * @param   mixed   &$data    The data to be processed. It gets altered directly.
+	 *
+	 * @return  void
+	 *
+	 * @since   3.4
+	 * @throws  RuntimeException
+	 */
+	protected function preprocessData($context, &$data)
+	{
+		// Get the dispatcher and load the plugins.
+		JPluginHelper::importPlugin('content');
+
+		// Trigger the data preparation event.
+		$results = $this->dispatcher->trigger('onContentPrepareData', array($context, $data));
+
+		// Check for errors encountered while preparing the data.
+		if (count($results) > 0 && in_array(false, $results, true))
+		{
+			throw new RuntimeException($this->dispatcher->getError());
+		}
+	}
+
+	/**
+	 * Method to allow derived classes to preprocess the form.
+	 *
+	 * @param   JForm   $form   A JForm object.
+	 * @param   mixed   $data   The data expected for the form.
+	 * @param   string  $group  The name of the plugin group to import (defaults to "content").
+	 *
+	 * @return  void
+	 *
+	 * @see     JFormField
+	 * @since   3.4
+	 * @throws  RuntimeException if there is an error in the form event.
+	 */
+	protected function preprocessForm(JForm $form, $data, $group = 'content')
+	{
+		// Import the appropriate plugin group.
+		JPluginHelper::importPlugin($group);
+
+		// Trigger the form preparation event.
+		$results = $this->dispatcher->trigger('onContentPrepareForm', array($form, $data));
+
+		// Check for errors encountered while preparing the form.
+		if (count($results) && in_array(false, $results, true))
+		{
+			throw new RuntimeException($this->dispatcher->getError());
+		}
+	}
+
+	/**
+	 * Method to validate the form data.
+	 *
+	 * @param   JForm            $form   The form to validate against.
+	 * @param   array            $data   The data to validate.
+	 * @param   string           $group  The name of the field group to validate.
+	 * @param   JApplicationCms  $app    An application object (optional)
+	 *
+	 * @return  mixed  Array of filtered data if valid, false otherwise.
+	 *
+	 * @see     JFormRule
+	 * @see     JFilterInput
+	 * @since   3.4
+	 */
+	public function validate($form, $data, $group = null, JApplicationCms $app)
+	{
+	  $app = $app ? $app : JFactory::getApplication();
+
+		// Filter and validate the form data.
+		$data = $form->filter($data);
+		$return = $form->validate($data, $group);
+
+		// Check for an error.
+		if ($return instanceof Exception)
+		{
+			$app->enqueueMessage($return->getMessage());
+
+			return false;
+		}
+
+		// Check the validation results.
+		if ($return === false)
+		{
+			// Get the validation messages from the form.
+			foreach ($form->getErrors() as $message)
+			{
+				$app->enqueueMessage($message);
+			}
+
+			return false;
+		}
+
+		return $data;
+	}
+}
