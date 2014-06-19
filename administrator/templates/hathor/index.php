c28c811c74595123861c1e6f4883f6114cbb872f
b92 <b92@wanadoo.fr>
2014-06-19 21:48:03 +0200

    Update index.php
    See description: http://joomlacode.org/gf/project/joomla/tracker/?action=TrackerItemEdit&tracker_item_id=33882&start=0

--- administrator/templates/hathor/index.php
+++ administrator/templates/hathor/index.php
@@ -98,7 +98,7 @@ $output    = array();
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo  $this->language; ?>" lang="<?php echo  $this->language; ?>" dir="<?php echo  $this->direction; ?>">
 	<head>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<jdoc:include type="head" />
 	<!-- Load additional CSS styles for Internet Explorer -->
 	<!--[if IE 8]>
