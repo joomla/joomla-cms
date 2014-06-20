c99799f9f73e512b022641cd042363cc2a918678
a <d.grammatiko@gmail.com>
2014-06-20 19:13:25 +0300

    Respect the overides

--- modules/mod_languages/tmpl/default.php
+++ modules/mod_languages/tmpl/default.php
@@ -12,47 +12,49 @@ defined('_JEXEC') or die;
 JHtml::_('stylesheet', 'mod_languages/template.css', array(), true);
 ?>
 <div class="mod-languages<?php echo $moduleclass_sfx ?>">
-<?php if ($headerText) : ?>
-	<div class="pretext"><p><?php echo $headerText; ?></p></div>
-<?php endif; ?>
+	<?php if ($headerText) : ?>
+		<div class="pretext"><p><?php echo $headerText; ?></p></div>
+	<?php endif; ?>
 
-<?php if ($params->get('dropdown', 1)) : ?>
-	<form name="lang" method="post" action="<?php echo htmlspecialchars(JUri::current()); ?>">
-	<select class="inputbox" onchange="document.location.replace(this.value);" >
-	<?php foreach ($list as $language) : ?>
-		<option dir=<?php echo JLanguage::getInstance($language->lang_code)->isRTL() ? '"rtl"' : '"ltr"'?> value="<?php echo $language->link;?>" <?php echo $language->active ? 'selected="selected"' : ''?>>
-		<?php echo $language->title_native;?></option>
-	<?php endforeach; ?>
-	</select>
-	</form>
-<?php else : ?>
-	<ul class="<?php echo $params->get('inline', 1) ? 'lang-inline' : 'lang-block';?>">
-	<?php foreach ($list as $language) : ?>
-		<?php if ($params->get('show_active', 0) || !$language->active):?>
-			<li class="<?php echo $language->active ? 'lang-active' : '';?>" dir="<?php echo JLanguage::getInstance($language->lang_code)->isRTL() ? 'rtl' : 'ltr' ?>">
-			<a href="<?php echo $language->link;?>">
-			<?php if ($params->get('image', 1)):?>
-				<?php
-				if ($params->get('encode'))
-				{
-					echo '<img src="data:image/gif;base64,' . base64_encode(file_get_contents(JPATH_ROOT . '/media/mod_languages/images/' . $language->image . '.gif')) . '" alt="' . $language->title_native . '" title="' . $language->title_native . '">';
-				}
-				else
-				{
-					echo JHtml::_('image', 'mod_languages/' . $language->image . '.gif', $language->title_native, array('title' => $language->title_native), true);
-				}
-				?>
-			<?php else : ?>
-				<?php echo $params->get('full_name', 1) ? $language->title_native : strtoupper($language->sef);?>
-			<?php endif; ?>
-			</a>
-			</li>
-		<?php endif;?>
-	<?php endforeach;?>
-	</ul>
-<?php endif; ?>
+	<?php if ($params->get('dropdown', 1)) : ?>
+		<form name="lang" method="post" action="<?php echo htmlspecialchars(JUri::current()); ?>">
+			<select class="inputbox" onchange="document.location.replace(this.value);" >
+				<?php foreach ($list as $language) : ?>
+					<option dir=<?php echo JLanguage::getInstance($language->lang_code)->isRTL() ? '"rtl"' : '"ltr"'?> value="<?php echo $language->link;?>" <?php echo $language->active ? 'selected="selected"' : ''?>>
+						<?php echo $language->title_native;?></option>
+				<?php endforeach; ?>
+			</select>
+		</form>
+	<?php else : ?>
+		<ul class="<?php echo $params->get('inline', 1) ? 'lang-inline' : 'lang-block';?>">
+			<?php foreach ($list as $language) : ?>
+				<?php if ($params->get('show_active', 0) || !$language->active):?>
+					<li class="<?php echo $language->active ? 'lang-active' : '';?>" dir="<?php echo JLanguage::getInstance($language->lang_code)->isRTL() ? 'rtl' : 'ltr' ?>">
+						<a href="<?php echo $language->link;?>">
+							<?php if ($params->get('image', 1)):?>
+								<?php
+								if ($params->get('encode'))
+								{
+									$flag = preg_match('/\< *[img][^\>]*[src] *= *[\"\']{0,1}([^\"\'\ >]*)/i', JHtml::_('image', 'mod_languages/' . $language->image . '.gif', $language->title_native, array('title' => $language->title_native), true), $match);
 
-<?php if ($footerText) : ?>
-	<div class="posttext"><p><?php echo $footerText; ?></p></div>
-<?php endif; ?>
+									echo '<img src="data:image/gif;base64,' . base64_encode(file_get_contents(JPATH_ROOT . $match[1])) . '" alt="' . $language->title_native . '" title="' . $language->title_native . '">';
+								}
+								else
+								{
+									echo JHtml::_('image', 'mod_languages/' . $language->image . '.gif', $language->title_native, array('title' => $language->title_native), true);
+								}
+								?>
+							<?php else : ?>
+								<?php echo $params->get('full_name', 1) ? $language->title_native : strtoupper($language->sef);?>
+							<?php endif; ?>
+						</a>
+					</li>
+				<?php endif;?>
+			<?php endforeach;?>
+		</ul>
+	<?php endif; ?>
+
+	<?php if ($footerText) : ?>
+		<div class="posttext"><p><?php echo $footerText; ?></p></div>
+	<?php endif; ?>
 </div>
