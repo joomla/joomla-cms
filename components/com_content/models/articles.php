c9bb05a6f6a1b8d869b13b479b347d70d4840885
zero-24 <zero-24@users.noreply.github.com>
2014-06-21 13:49:53 +0200

    Update articles.php

--- components/com_content/models/articles.php
+++ components/com_content/models/articles.php
@@ -76,7 +76,7 @@ class ContentModelArticles extends JModelList
 		$app = JFactory::getApplication();
 
 		// List state information
-		$value = $app->input->get('limit', $app->getCfg('list_limit', 0), 'uint');
+		$value = $app->input->get('limit', $app->get('list_limit', 0), 'uint');
 		$this->setState('list.limit', $value);
 
 		$value = $app->input->get('limitstart', 0, 'uint');
