dacb3d5f38fca603323f47b4108af5f7834d5992
infograf768 <infografjms@gmail.com>
2014-06-25 11:03:18 +0200

    Update cms.php
    codestyle

--- libraries/cms/application/cms.php
+++ libraries/cms/application/cms.php
@@ -301,6 +301,7 @@ class JApplicationCms extends JApplicationWeb
 	 * This method must be invoked as: $web = JApplicationCms::getInstance();
 	 *
 	 * @param   string  $name  The name (optional) of the JApplicationCms class to instantiate.
+	 * @param   array   $config  Configuration array
 	 *
 	 * @return  JApplicationCms
 	 *
