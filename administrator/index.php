9caf5f4aa86b62ebdaf2a3ef2ba19cd036addd68
Bradley Weston <bradwestonwigston@gmail.com>
2014-06-30 12:40:13 +0100

    Add composer detection. (Administrator)
    I know a lot of you don't use composer but this 4 lines of code would help me. First of all I could get dependencies from packagist.org or my own private packages. Packages don't have to be extensions a lot of them are utilities that help.

--- administrator/index.php
+++ administrator/index.php
@@ -11,6 +11,11 @@ if (version_compare(PHP_VERSION, '5.3.10', '<'))
 	die('Your host needs to use PHP 5.3.10 or higher to run this version of Joomla!');
 }
 
+if (file_exists($composer = __DIR__ . '/../vendor/autoload.php'))
+{
+	require $composer;
+}
+
 /**
  * Constant that is checked in included files to prevent direct access.
  * define() is used in the installation folder rather than "const" to not error for PHP 5.2 and lower
