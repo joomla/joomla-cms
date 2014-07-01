82538f35f686e25331dd6a78bd6ac58deda52d01
Buddhima <buddhimawijeweera@yahoo.com>
2014-07-01 21:08:22 +0530

    Changes in language files and 'Uncategorised' category added

--- administrator/components/com_admin/sql/updates/mysql/3.5.0-2014-06-28.sql
+++ administrator/components/com_admin/sql/updates/mysql/3.5.0-2014-06-28.sql
@@ -1,4 +1,5 @@
 INSERT INTO `#__content_types`(`type_id`, `type_title`, `type_alias`) VALUES
 (16, 'Image', 'com_media.image'),
 (17, 'Media Category', 'com_media.category');
-
+INSERT INTO `#__categories`(`id`, `asset_id`, `parent_id`, `lft`, `rgt`, `level`, `path`, `extension`, `title`, `alias`, `published`, `access`, `params`, `metadata`, `created_user_id`, `hits`, `language`, `version`) VALUES 
+(8, 33, 1, 13, 14, 1, 'uncategorised', 'com_media', 'Uncategorised', 'uncategorised', 1, 1, '{"category_layout":"","image":""}', '{"author":"","robots":""}', 42, 0, '*', 1);
--- administrator/components/com_admin/sql/updates/sqlazure/3.5.0-2014-06-28.sql
+++ administrator/components/com_admin/sql/updates/sqlazure/3.5.0-2014-06-28.sql
@@ -6,3 +6,12 @@ UNION ALL
 SELECT 17, 'Media Category', 'com_media.category';
 
 SET IDENTITY_INSERT #__content_types  OFF;
+
+SET IDENTITY_INSERT [#__categories]  ON;
+
+INSERT INTO [#__categories] ([id], [asset_id], [parent_id], [lft], [rgt], [level], [path], [extension], [title], [alias], [published], [access], [params], [metadata], [created_user_id], [hits], [language], [version])
+SELECT 8, 33, 1, 13, 14, 1, 'uncategorised', 'com_media', 'Uncategorised', 'uncategorised', 1, 1, '{"category_layout":"","image":""}', '{"author":"","robots":""}', 42, 0, '*', 1;
+
+SET IDENTITY_INSERT #__categories  OFF;
+
+
