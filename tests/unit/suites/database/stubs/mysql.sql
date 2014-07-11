2f3bd4edb995c8428eba61103dfd33cfed87b144
Valentin Despa <info@vdespa.de>
2014-07-11 23:33:48 +0200

    Setting test database collation

--- tests/unit/suites/database/stubs/mysql.sql
+++ tests/unit/suites/database/stubs/mysql.sql
@@ -7,6 +7,8 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+/* Set collation */;
+ALTER DATABASE joomla_ut CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 # Dump of table jos_dbtest
 # ------------------------------------------------------------
