dd3773752fb8d9f249389d7b41496c3b3b6078ad
webmasterab <abooij@live.nl>
2014-07-13 11:29:56 +0200

    Revert "It may or may not send the password"
    This reverts commit 8df7df0215b14af981026cb7afa14b535ff22751.

--- components/com_users/models/registration.php
+++ components/com_users/models/registration.php
@@ -424,32 +424,18 @@ class UsersModelRegistration extends JModelForm
 		else
 		{
 
-		$emailSubject = JText::sprintf(
-		'COM_USERS_EMAIL_ACCOUNT_DETAILS',
-		$data['name'],
-		$data['sitename']
-		);
- 
-		if ($sendpassword)
- 
-		$emailBody = JText::sprintf(
-		'COM_USERS_EMAIL_REGISTERED_BODY',
-		$data['name'],
-		$data['sitename'],
-		$data['siteurl'],
-		$data['username'],
-		$data['password_clear']
-		);
- 
-		else
- 
-		$emailBody = JText::sprintf(
-		'COM_USERS_EMAIL_REGISTERED_BODY_NOPW',
-		$data['name'],
-		$data['sitename'],
-		$data['siteurl']
-		);
+			$emailSubject	= JText::sprintf(
+				'COM_USERS_EMAIL_ACCOUNT_DETAILS',
+				$data['name'],
+				$data['sitename']
+			);
 
+			$emailBody = JText::sprintf(
+				'COM_USERS_EMAIL_REGISTERED_BODY',
+				$data['name'],
+				$data['sitename'],
+				$data['siteurl']
+			);
 		}
 
 		// Send the registration email.
