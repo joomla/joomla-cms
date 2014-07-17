97e3c50dec9a7f255c3703ede279dc2bc1cd410c
zero-24 <zero-24@users.noreply.github.com>
2014-07-17 13:13:02 +0200

    Update date.php

--- libraries/joomla/date/date.php
+++ libraries/joomla/date/date.php
@@ -103,13 +103,13 @@ class JDate extends DateTime
 		// If the date is numeric assume a unix timestamp and convert it.
 		date_default_timezone_set('UTC');
 		$date = is_numeric($date) ? date('c', $date) : $date;
-		
+
 		// Only continue if $date is valid by strtotime()
 		if (strtotime($date) === false)
 		{
 			$date = 'now';
 		}
-		
+
 		// Call the DateTime constructor.
 		parent::__construct($date, $tz);
 
