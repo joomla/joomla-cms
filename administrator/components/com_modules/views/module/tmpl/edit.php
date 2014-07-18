b4a0cdecc4dc712f4f8fbb63b13e8c9b2e750fd7
Elijah Madden <elijah.madden@nikkoam.com>
2014-07-18 10:21:29 +0900

    Use a proxy function to close modals. Fixes #3918.

--- administrator/components/com_modules/views/module/tmpl/edit.php
+++ administrator/components/com_modules/views/module/tmpl/edit.php
@@ -120,7 +120,7 @@ $script .= "
 							jQuery('#position-" . $this->item->id . "', parent.document).text(updPosition);
 							jQuery('#access-" . $this->item->id . "', parent.document).html(parent.viewLevels[updAccess]);
 					}
-					window.top.setTimeout('window.parent.SqueezeBox.close()', 1000);
+					window.top.setTimeout('window.parent.jModalClose()', 1000);
 				}
 			}
 	};";
