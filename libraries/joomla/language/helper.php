60a4f5326b63c7258b1136b00b8df089390320a3
Nadeeshaan Gunasinghe <nadeeshaangunasinghe@gmail.com>
2014-07-21 23:11:15 +0530

    improved readability for the table fields

--- libraries/joomla/language/helper.php
+++ libraries/joomla/language/helper.php
@@ -155,8 +155,23 @@ class JLanguageHelper
 				{
 					$db = JFactory::getDbo();
 					$query = $db->getQuery(true)
-						->select('lang_id,lang_code,title,title_native,sef,image,description,metakey,metadesc,
-						sitename,published,access,ordering')
+						->select(
+							array(
+								'lang_id',
+								'lang_code',
+								'title',
+								'title_native',
+								'sef',
+								'image',
+								'description',
+								'metakey',
+								'metadesc',
+								'sitename',
+								'published',
+								'access',
+								'ordering'
+							)
+						)
 						->from('#__languages')
 						->where('published=1')
 						->order('ordering ASC');
