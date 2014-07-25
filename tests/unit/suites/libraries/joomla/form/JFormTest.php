82d8459268177b3299c0505ddbb48ecb2269c6b2
Michael Babker <mbabker@flbab.com>
2014-07-24 21:50:53 -0400

    [fix] Address issues with current PHP 5.6 RC. Fixes #3961
    Add PHP 5.6 to Travis config
    
    Add PHP 5.6 to skipped tests with unserialize issues

--- tests/unit/suites/libraries/joomla/form/JFormTest.php
+++ tests/unit/suites/libraries/joomla/form/JFormTest.php
@@ -1551,14 +1551,16 @@ class JFormTest extends TestCaseDatabase
 	 */
 	public function testLoadFieldType()
 	{
+		$inspector = new JFormInspector('test');
+
 		$this->assertThat(
-			JFormInspector::loadFieldType('bogus'),
+			$inspector->loadFieldType('bogus'),
 			$this->isFalse(),
 			'Line:' . __LINE__ . ' loadFieldType should return false if class not found.'
 		);
 
 		$this->assertThat(
-			(JFormInspector::loadFieldType('list') instanceof JFormFieldList),
+			($inspector->loadFieldType('list') instanceof JFormFieldList),
 			$this->isTrue(),
 			'Line:' . __LINE__ . ' loadFieldType should return the correct class.'
 		);
@@ -1567,25 +1569,25 @@ class JFormTest extends TestCaseDatabase
 		JForm::addFieldPath(__DIR__ . '/_testfields');
 
 		$this->assertThat(
-			(JFormInspector::loadFieldType('test') instanceof JFormFieldTest),
+			($inspector->loadFieldType('test') instanceof JFormFieldTest),
 			$this->isTrue(),
 			'Line:' . __LINE__ . ' loadFieldType should return the correct custom class.'
 		);
 
 		$this->assertThat(
-			(JFormInspector::loadFieldType('foo.bar') instanceof FooFormFieldBar),
+			($inspector->loadFieldType('foo.bar') instanceof FooFormFieldBar),
 			$this->isTrue(),
 			'Line:' . __LINE__ . ' loadFieldType should return the correct custom class.'
 		);
 
 		$this->assertThat(
-			(JFormInspector::loadFieldType('modal_foo') instanceof JFormFieldModal_Foo),
+			($inspector->loadFieldType('modal_foo') instanceof JFormFieldModal_Foo),
 			$this->isTrue(),
 			'Line:' . __LINE__ . ' loadFieldType should return the correct custom class.'
 		);
 
 		$this->assertThat(
-			(JFormInspector::loadFieldType('foo.modal_bar') instanceof FooFormFieldModal_Bar),
+			($inspector->loadFieldType('foo.modal_bar') instanceof FooFormFieldModal_Bar),
 			$this->isTrue(),
 			'Line:' . __LINE__ . ' loadFieldType should return the correct custom class.'
 		);
