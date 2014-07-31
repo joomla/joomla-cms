31c5451282c15c18d3c96eb0703711d7b727c060
Jean-Marie Simonet <infografjms@gmail.com>
2014-07-31 11:34:08 +0200

    Code style

--- administrator/modules/mod_feed/tmpl/default.php
+++ administrator/modules/mod_feed/tmpl/default.php
@@ -18,7 +18,7 @@ else
 	$lang      = JFactory::getLanguage();
 	$myrtl     = $params->get('rssrtl');
 	$direction = " ";
-	
+
 	if ($lang->isRTL() && $myrtl == 0)
 	{
 		$direction = " redirect-rtl";
@@ -79,7 +79,7 @@ else
 	<?php if (!empty($feed)) : ?>
 		<ul class="newsfeed<?php echo $params->get('moduleclass_sfx'); ?>">
 		<?php for ($i = 0; $i < $params->get('rssitems', 5); $i++) :
-			
+
 			if (!$feed->offsetExists($i)) :
 				break;
 			endif;
