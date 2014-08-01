62e1294a4caf30b5711dc6ba2807afdf7028b0c9
Peter van Westen <peter@nonumber.nl>
2014-08-01 13:01:09 +0200

    Update weblinks.php

--- administrator/components/com_weblinks/models/weblinks.php
+++ administrator/components/com_weblinks/models/weblinks.php
@@ -194,7 +194,7 @@ class WeblinksModelWeblinks extends JModelList
 			}
 			else
 			{
-				$search = $db->quote('%' . $db->escape($search, true) . '%');
+				$search = $db->quote('%' . str_replace(' ', '%', $db->escape(trim($search), true) . '%'));
 				$query->where('(a.title LIKE ' . $search . ' OR a.alias LIKE ' . $search . ')');
 			}
 		}
