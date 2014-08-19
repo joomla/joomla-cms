06314d83d99c58ad997202a27258f60d91fa8b44
modernmagic <jeff@modernmagic.com>
2014-08-19 14:00:56 -0700

    make intro image link to article
    I believe that it should either be an option or always setup that the image also links to the article just like the title and readmore.  In the blog layout I watch my clients always rolling over the intro image first to click on it to see the full article.  I do this to all my sites as it seems to be a better UX.
    
    (This is my 2nd :-) try at contributing to Joomla. Please tell help me if I am doing this wrong. I am not a coder, but was able to get this to work for my client's site by copy/pasting similar code.)

--- layouts/joomla/content/intro_image.php
+++ layouts/joomla/content/intro_image.php
@@ -13,9 +13,9 @@ $params  = $displayData->params;
 <?php $images = json_decode($displayData->images); ?>
 <?php if (isset($images->image_intro) && !empty($images->image_intro)) : ?>
 	<?php $imgfloat = (empty($images->float_intro)) ? $params->get('float_intro') : $images->float_intro; ?>
-	<div class="pull-<?php echo htmlspecialchars($imgfloat); ?> item-image"> <img
+	<div class="pull-<?php echo htmlspecialchars($imgfloat); ?> item-image"><a href="<?php echo JRoute::_(ContentHelperRoute::getArticleRoute($displayData->slug, $displayData->catid)); ?>"> <img
 	<?php if ($images->image_intro_caption):
 		echo 'class="caption"' . ' title="' . htmlspecialchars($images->image_intro_caption) . '"';
 	endif; ?>
-	src="<?php echo htmlspecialchars($images->image_intro); ?>" alt="<?php echo htmlspecialchars($images->image_intro_alt); ?>" itemprop="thumbnailUrl"/> </div>
+	src="<?php echo htmlspecialchars($images->image_intro); ?>" alt="<?php echo htmlspecialchars($images->image_intro_alt); ?>" itemprop="thumbnailUrl"/></a> </div>
 <?php endif; ?>
