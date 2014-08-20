c884ab55068cf6e07cf6715d317436670b9fb50f
modernmagic <jeff@modernmagic.com>
2014-08-20 12:42:14 -0700

    Update intro_image.php
    added ($params->get('link_titles')

--- layouts/joomla/content/intro_image.php
+++ layouts/joomla/content/intro_image.php
@@ -13,9 +13,18 @@ $params  = $displayData->params;
 <?php $images = json_decode($displayData->images); ?>
 <?php if (isset($images->image_intro) && !empty($images->image_intro)) : ?>
 	<?php $imgfloat = (empty($images->float_intro)) ? $params->get('float_intro') : $images->float_intro; ?>
-	<div class="pull-<?php echo htmlspecialchars($imgfloat); ?> item-image"><a href="<?php echo JRoute::_(ContentHelperRoute::getArticleRoute($displayData->slug, $displayData->catid)); ?>"> <img
+	<div class="grid_six pull-<?php echo htmlspecialchars($imgfloat); ?> item-image"> 
+<?php if ($params->get('link_titles') && $params->get('access-view')) : ?>
+<a href="<?php echo JRoute::_(ContentHelperRoute::getArticleRoute($displayData->slug, $displayData->catid)); ?>"><img
 	<?php if ($images->image_intro_caption):
 		echo 'class="caption"' . ' title="' . htmlspecialchars($images->image_intro_caption) . '"';
 	endif; ?>
-	src="<?php echo htmlspecialchars($images->image_intro); ?>" alt="<?php echo htmlspecialchars($images->image_intro_alt); ?>" itemprop="thumbnailUrl"/></a> </div>
+	src="<?php echo htmlspecialchars($images->image_intro); ?>" alt="<?php echo htmlspecialchars($images->image_intro_alt); ?>" itemprop="thumbnailUrl"/></a>
+	<?php else : ?><img
+	<?php if ($images->image_intro_caption):
+		echo 'class="caption"' . ' title="' . htmlspecialchars($images->image_intro_caption) . '"';
+	endif; ?>
+	src="<?php echo htmlspecialchars($images->image_intro); ?>" alt="<?php echo htmlspecialchars($images->image_intro_alt); ?>" itemprop="thumbnailUrl"/>
+	<?php endif; ?> 
+</div>
 <?php endif; ?>
