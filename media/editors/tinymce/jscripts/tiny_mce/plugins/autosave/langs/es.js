53fdd57470558bbbccf1ece11cb3d5749b791e40
tuxmerlin <migueltuyare@gmail.com>
2014-08-21 00:04:31 -0300

    [Cambio] Idioma editor

---
+++ media/editors/tinymce/jscripts/tiny_mce/plugins/autosave/langs/es.js
@@ -0,0 +1,4 @@
+tinyMCE.addI18n('es.autosave',{
+restore_content: "Restaurar el contenido guardado autom\u00edticamente",
+warning_message: "Si usted restaura el contenido guardado, se perder\u00ed todo el contenido que actualmente se encuentre en el editor.\n\n¿Est\u00ed seguro de que desea restaurar el contenido guardado?"
+});
