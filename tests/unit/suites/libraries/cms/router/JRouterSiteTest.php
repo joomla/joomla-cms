7eeb7b7523af44efbfeea6c879ab22581ebe0c18
zero-24 <zero-24@users.noreply.github.com>
2014-08-31 00:22:50 +0200

    Update JRouterSiteTest.php

--- tests/unit/suites/libraries/cms/router/JRouterSiteTest.php
+++ tests/unit/suites/libraries/cms/router/JRouterSiteTest.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * @package	    Joomla.UnitTest
+ * @package     Joomla.UnitTest
  * @subpackage  Router
  *
  * @copyright   Copyright (C) 2005 - 2014 Open Source Matters, Inc. All rights reserved.
- * @license	    GNU General Public License version 2 or later; see LICENSE
+ * @license     GNU General Public License version 2 or later; see LICENSE
  */
 
 require_once __DIR__ . '/stubs/JRouterSiteInspector.php';
