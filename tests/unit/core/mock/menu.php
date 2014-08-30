9880766b8b45b02d09d519763655e1ce91d6819f
zero-24 <zero-24@users.noreply.github.com>
2014-08-30 23:45:09 +0200

    Update menu.php

--- tests/unit/core/mock/menu.php
+++ tests/unit/core/mock/menu.php
@@ -10,12 +10,12 @@
  * Class to mock JMenu.
  *
  * @package  Joomla.Test
- * @since    12.1
+ * @since    3.4
  */
 class TestMockMenu
 {
 	protected static $data = array();
-	
+
 	/**
 	 * Creates an instance of the mock JMenu object.
 	 *
@@ -52,17 +52,17 @@ class TestMockMenu
 			// Call original constructor.
 			false
 		);
-		
+
 		self::createMenuSampleData();
-		
+
 		$mockObject->expects($test->any())
 				->method('getItem')
 				->will($test->returnValueMap(self::prepareGetItemData()));
-		
+
 		$mockObject->expects($test->any())
 				->method('getMenu')
 				->will($test->returnValue(self::$data));
-		
+
 		if ($setDefault)
 		{
 			$mockObject->expects($test->any())
@@ -72,217 +72,218 @@ class TestMockMenu
 
 		return $mockObject;
 	}
-	
+
 	protected static function prepareGetItemData()
 	{
 		$return = array();
-		foreach(self::$data as $id => $item)
+
+		foreach (self::$data as $id => $item)
 		{
 			$return[] = array($id, $item);
 			$return[] = array((string)$id, $item);
 		}
-		
+
 		return $return;
 	}
-	
+
 	protected static function prepareDefaultData()
 	{
-		$return = array();
+		$return   = array();
 		$return[] = array('en-GB', self::$data[45]);
-		
+
 		return $return;
 	}
-	
+
 	protected static function prepareGetItemsData()
 	{
-		
+		// Why this is clear here?
 	}
-	
+
 	protected static function createMenuSampleData()
 	{
 		self::$data[42] = (object) array(
-			'id' => '42', 
-			'menutype' => 'testmenu', 
-			'title' => 'Test1', 
-			'alias' => 'test', 
-			'route' => 'test', 
-			'link' => 'index.php?option=com_test&view=test', 
-			'type' => 'component', 
-			'level' => '1', 
-			'language' => '*', 
-			'access' => '1', 
-			'params' => '{}', 
-			'home' => '0', 
+			'id'           => '42', 
+			'menutype'     => 'testmenu', 
+			'title'        => 'Test1', 
+			'alias'        => 'test', 
+			'route'        => 'test', 
+			'link'         => 'index.php?option=com_test&view=test', 
+			'type'         => 'component', 
+			'level'        => '1', 
+			'language'     => '*', 
+			'access'       => '1', 
+			'params'       => '{}', 
+			'home'         => '0', 
 			'component_id' => '1000', 
-			'parent_id' => '0', 
-			'component' => 'com_test',
-			'tree' => array(42),
-			'query' => array('option' => 'com_test', 'view' => 'test'));
+			'parent_id'    => '0', 
+			'component'    => 'com_test',
+			'tree'         => array(42),
+			'query'        => array('option' => 'com_test', 'view' => 'test'));
 
 		self::$data[43] = (object) array(
-			'id' => '43', 
-			'menutype' => 'testmenu', 
-			'title' => 'Test2', 
-			'alias' => 'test2', 
-			'route' => 'test2', 
-			'link' => 'index.php?option=com_test2&view=test', 
-			'type' => 'component', 
-			'level' => '1', 
-			'language' => '*', 
-			'access' => '1', 
-			'params' => '{}', 
-			'home' => '0', 
+			'id'           => '43', 
+			'menutype'     => 'testmenu', 
+			'title'        => 'Test2', 
+			'alias'        => 'test2', 
+			'route'        => 'test2', 
+			'link'         => 'index.php?option=com_test2&view=test', 
+			'type'         => 'component', 
+			'level'        => '1', 
+			'language'     => '*', 
+			'access'       => '1', 
+			'params'       => '{}', 
+			'home'         => '0', 
 			'component_id' => '1000', 
-			'parent_id' => '0', 
-			'component' => 'com_test2',
-			'tree' => array(43),
-			'query' => array('option' => 'com_test2', 'view' => 'test'));
-		
+			'parent_id'    => '0', 
+			'component'    => 'com_test2',
+			'tree'         => array(43),
+			'query'        => array('option' => 'com_test2', 'view' => 'test'));
+
 		self::$data[44] = (object) array(
-			'id' => '44', 
-			'menutype' => 'testmenu', 
-			'title' => 'Submenu', 
-			'alias' => 'sub-menu', 
-			'route' => 'test2/sub-menu', 
-			'link' => 'index.php?option=com_test2&view=test2', 
-			'type' => 'component', 
-			'level' => '2', 
-			'language' => '*', 
-			'access' => '1', 
-			'params' => '{}', 
-			'home' => '0', 
+			'id'           => '44', 
+			'menutype'     => 'testmenu', 
+			'title'        => 'Submenu', 
+			'alias'        => 'sub-menu', 
+			'route'        => 'test2/sub-menu', 
+			'link'         => 'index.php?option=com_test2&view=test2', 
+			'type'         => 'component', 
+			'level'        => '2', 
+			'language'     => '*', 
+			'access'       => '1', 
+			'params'       => '{}', 
+			'home'         => '0', 
 			'component_id' => '1000', 
-			'parent_id' => '43', 
-			'component' => 'com_test2',
-			'tree' => array(43, 44),
-			'query' => array('option' => 'com_test2', 'view' => 'test2'));
-		
+			'parent_id'    => '43', 
+			'component'    => 'com_test2',
+			'tree'         => array(43, 44),
+			'query'        => array('option' => 'com_test2', 'view' => 'test2'));
+
 		self::$data[45] = (object) array(
-			'id' => '45', 
-			'menutype' => 'testmenu', 
-			'title' => 'Home', 
-			'alias' => 'home', 
-			'route' => 'home', 
-			'link' => 'index.php?option=com_test3&view=test3', 
-			'type' => 'component', 
-			'level' => '1', 
-			'language' => '*', 
-			'access' => '1', 
-			'params' => '{}', 
-			'home' => '1', 
+			'id'           => '45', 
+			'menutype'     => 'testmenu', 
+			'title'        => 'Home', 
+			'alias'        => 'home', 
+			'route'        => 'home', 
+			'link'         => 'index.php?option=com_test3&view=test3', 
+			'type'         => 'component', 
+			'level'        => '1', 
+			'language'     => '*', 
+			'access'       => '1', 
+			'params'       => '{}', 
+			'home'         => '1', 
 			'component_id' => '1000', 
-			'parent_id' => '0', 
-			'component' => 'com_test3',
-			'tree' => array(43, 44),
-			'query' => array('option' => 'com_test3', 'view' => 'test3'));
-		
+			'parent_id'    => '0', 
+			'component'    => 'com_test3',
+			'tree'         => array(43, 44),
+			'query'        => array('option' => 'com_test3', 'view' => 'test3'));
+
 		self::$data[46] = (object) array(
-			'id' => '46', 
-			'menutype' => 'testmenu', 
-			'title' => 'Submenu', 
-			'alias' => 'sub-menu', 
-			'route' => 'test/sub-menu', 
-			'link' => 'index.php?option=com_test&view=test2', 
-			'type' => 'component', 
-			'level' => '2', 
-			'language' => '*', 
-			'access' => '1', 
-			'params' => '{}', 
-			'home' => '0', 
+			'id'           => '46', 
+			'menutype'     => 'testmenu', 
+			'title'        => 'Submenu', 
+			'alias'        => 'sub-menu', 
+			'route'        => 'test/sub-menu', 
+			'link'         => 'index.php?option=com_test&view=test2', 
+			'type'         => 'component', 
+			'level'        => '2', 
+			'language'     => '*', 
+			'access'       => '1', 
+			'params'       => '{}', 
+			'home'         => '0', 
 			'component_id' => '1000', 
-			'parent_id' => '42', 
-			'component' => 'com_test',
-			'tree' => array(42, 46),
-			'query' => array('option' => 'com_test', 'view' => 'test'));
-		
+			'parent_id'    => '42', 
+			'component'    => 'com_test',
+			'tree'         => array(42, 46),
+			'query'        => array('option' => 'com_test', 'view' => 'test'));
+
 	/**	self::$data[47] = (object) array(
-			'id' => '47', 
-			'menutype' => '', 
-			'title' => '', 
-			'alias' => '', 
-			'route' => '', 
-			'link' => '', 
-			'type' => '', 
-			'level' => '', 
-			'language' => '', 
-			'access' => '', 
-			'params' => '', 
-			'home' => '', 
+			'id'           => '47', 
+			'menutype'     => '', 
+			'title'        => '', 
+			'alias'        => '', 
+			'route'        => '', 
+			'link'         => '', 
+			'type'         => '', 
+			'level'        => '', 
+			'language'     => '', 
+			'access'       => '', 
+			'params'       => '', 
+			'home'         => '', 
 			'component_id' => '', 
-			'parent_id' => '', 
-			'component' => '',
-			'query' => array());
-		
+			'parent_id'    => '', 
+			'component'    => '',
+			'query'        => array());
+
 		self::$data[48] = (object) array(
-			'id' => '48', 
-			'menutype' => '', 
-			'title' => '', 
-			'alias' => '', 
-			'route' => '', 
-			'link' => '', 
-			'type' => '', 
-			'level' => '', 
-			'language' => '', 
-			'access' => '', 
-			'params' => '', 
-			'home' => '', 
+			'id'           => '48', 
+			'menutype'     => '', 
+			'title'        => '', 
+			'alias'        => '', 
+			'route'        => '', 
+			'link'         => '', 
+			'type'         => '', 
+			'level'        => '', 
+			'language'     => '', 
+			'access'       => '', 
+			'params'       => '', 
+			'home'         => '', 
 			'component_id' => '', 
-			'parent_id' => '', 
-			'component' => '',
-			'query' => array());
-		
+			'parent_id'    => '', 
+			'component'    => '',
+			'query'        => array());
+
 		self::$data[49] = (object) array(
-			'id' => '49', 
-			'menutype' => '', 
-			'title' => '', 
-			'alias' => '', 
-			'route' => '', 
-			'link' => '', 
-			'type' => '', 
-			'level' => '', 
-			'language' => '', 
-			'access' => '', 
-			'params' => '', 
-			'home' => '', 
+			'id'           => '49', 
+			'menutype'     => '', 
+			'title'        => '', 
+			'alias'        => '', 
+			'route'        => '', 
+			'link'         => '', 
+			'type'         => '', 
+			'level'        => '', 
+			'language'     => '', 
+			'access'       => '', 
+			'params'       => '', 
+			'home'         => '', 
 			'component_id' => '', 
-			'parent_id' => '', 
-			'component' => '',
-			'query' => array());
-		
+			'parent_id'    => '', 
+			'component'    => '',
+			'query'        => array());
+
 		self::$data[50] = (object) array(
-			'id' => '50', 
-			'menutype' => '', 
-			'title' => '', 
-			'alias' => '', 
-			'route' => '', 
-			'link' => '', 
-			'type' => '', 
-			'level' => '', 
-			'language' => '', 
-			'access' => '', 
-			'params' => '', 
-			'home' => '', 
+			'id'           => '50', 
+			'menutype'     => '', 
+			'title'        => '', 
+			'alias'        => '', 
+			'route'        => '', 
+			'link'         => '', 
+			'type'         => '', 
+			'level'        => '', 
+			'language'     => '', 
+			'access'       => '', 
+			'params'       => '', 
+			'home'         => '', 
 			'component_id' => '', 
-			'parent_id' => '', 
-			'component' => '',
-			'query' => array());
-		
+			'parent_id'    => '', 
+			'component'    => '',
+			'query'        => array());
+
 		self::$data[51] = (object) array(
-			'id' => '51', 
-			'menutype' => '', 
-			'title' => '', 
-			'alias' => '', 
-			'route' => '', 
-			'link' => '', 
-			'type' => '', 
-			'level' => '', 
-			'language' => '', 
-			'access' => '', 
-			'params' => '', 
-			'home' => '', 
+			'id'           => '51', 
+			'menutype'     => '', 
+			'title'        => '', 
+			'alias'        => '', 
+			'route'        => '', 
+			'link'         => '', 
+			'type'         => '', 
+			'level'        => '', 
+			'language'     => '', 
+			'access'       => '', 
+			'params'       => '', 
+			'home'         => '', 
 			'component_id' => '', 
-			'parent_id' => '', 
-			'component' => '',
-			'query' => array());**/
+			'parent_id'    => '', 
+			'component'    => '',
+			'query'        => array());**/
 	}
 }
