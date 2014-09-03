a1cb99313c4e18d11bd1ef67064e09b4c19f18c7
Michael Babker <mbabker@flbab.com>
2014-09-03 13:36:14 -0400

    Set DB schema version for weblinks component

--- administrator/components/com_admin/sql/updates/mysql/3.4.0-2014-09-01.sql
+++ administrator/components/com_admin/sql/updates/mysql/3.4.0-2014-09-01.sql
@@ -1,6 +1,9 @@
 INSERT INTO `#__extensions` (`extension_id`, `name`, `type`, `element`, `folder`, `client_id`, `enabled`, `access`, `protected`, `manifest_cache`, `params`, `custom_data`, `system_data`, `checked_out`, `checked_out_time`, `ordering`, `state`) VALUES
 (801, 'weblinks', 'package', 'pkg_weblinks', '', 0, 1, 1, 0, '', '', '', '', 0, '0000-00-00 00:00:00', 0, 0);
 
+INSERT INTO `#__schemas` (`extension_id`, `version_id`) VALUES
+(21, '3.4.0');
+
 INSERT INTO `#__update_sites` (`name`, `type`, `location`, `enabled`) VALUES
 ('Weblinks Update Site', 'extension', 'https://raw.githubusercontent.com/joomla-extensions/weblinks/master/manifest.xml', 1);
 
