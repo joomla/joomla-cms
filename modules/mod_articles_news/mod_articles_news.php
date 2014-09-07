f099d704f2f91f0a248daae5589f5fa117faeb2f
zero-24 <zero-24@users.noreply.github.com>
2014-09-07 18:51:42 +0200

    Update mod_articles_news.php

--- modules/mod_articles_news/mod_articles_news.php
+++ modules/mod_articles_news/mod_articles_news.php
@@ -12,7 +12,7 @@ defined('_JEXEC') or die;
 // Include the syndicate functions only once
 require_once __DIR__ . '/helper.php';
 
-$list = ModArticlesNewsHelper::getList($params);
+$list            = ModArticlesNewsHelper::getList($params);
 $moduleclass_sfx = htmlspecialchars($params->get('moduleclass_sfx'));
 
 require JModuleHelper::getLayoutPath('mod_articles_news', $params->get('layout', 'horizontal'));
