4f85399da047f3a5e72f4ecd41707ff1f05f9dc5
dgt41 <d.grammatiko@gmail.com>
2014-09-14 19:13:01 +0300

    Isis Visual Update

---
+++ administrator/templates/isis/html/mod_login/default.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  mod_login
+ *
+ * @copyright   Copyright (C) 2005 - 2014 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+JHtml::_('behavior.keepalive');
+JHtml::_('bootstrap.tooltip');
+JHtml::_('formbehavior.chosen');
+
+?>
+<form action="<?php echo JRoute::_('index.php', true, $params->get('usesecure')); ?>" method="post" id="form-login" class="form-inline">
+	<fieldset class="loginform">
+		<div class="control-group">
+			<div class="controls">
+				<div class="input-prepend input-append">
+					<span class="add-on">
+						<i class="icon-user hasTooltip" title="<?php echo JText::_('JGLOBAL_USERNAME'); ?>"></i>
+						<label for="mod-login-username" class="element-invisible">
+							<?php echo JText::_('JGLOBAL_USERNAME'); ?>
+						</label>
+					</span>
+					<input name="username" tabindex="1" id="mod-login-username" type="text" class="input-medium" placeholder="<?php echo JText::_('JGLOBAL_USERNAME'); ?>" size="15"/>
+					<a href="<?php echo JUri::root(); ?>index.php?option=com_users&view=remind" class="btn width-auto hasTooltip" title="<?php echo JText::_('MOD_LOGIN_REMIND'); ?>">
+						<i class="icon-help"></i>
+					</a>
+				</div>
+			</div>
+		</div>
+		<div class="control-group">
+			<div class="controls">
+				<div class="input-prepend input-append">
+					<span class="add-on">
+						<i class="icon-lock hasTooltip" title="<?php echo JText::_('JGLOBAL_PASSWORD'); ?>"></i>
+						<label for="mod-login-password" class="element-invisible">
+							<?php echo JText::_('JGLOBAL_PASSWORD'); ?>
+						</label>
+					</span>
+					<input name="passwd" tabindex="2" id="mod-login-password" type="password" class="input-medium" placeholder="<?php echo JText::_('JGLOBAL_PASSWORD'); ?>" size="15"/>
+					<a href="<?php echo JUri::root(); ?>index.php?option=com_users&view=reset" class="btn width-auto hasTooltip" title="<?php echo JText::_('MOD_LOGIN_RESET'); ?>">
+						<i class="icon-help"></i>
+					</a>
+				</div>
+			</div>
+		</div>
+		<?php if (count($twofactormethods) > 1): ?>
+		<div class="control-group">
+			<div class="controls">
+				<div class="input-prepend input-append">
+					<span class="add-on">
+						<i class="icon-star hasTooltip" title="<?php echo JText::_('JGLOBAL_SECRETKEY'); ?>"></i>
+						<label for="mod-login-secretkey" class="element-invisible">
+							<?php echo JText::_('JGLOBAL_SECRETKEY'); ?>
+						</label>
+					</span>
+					<input name="secretkey" autocomplete="off" tabindex="3" id="mod-login-secretkey" type="text" class="input-medium" placeholder="<?php echo JText::_('JGLOBAL_SECRETKEY'); ?>" size="15"/>
+					<span class="btn width-auto hasTooltip" title="<?php echo JText::_('JGLOBAL_SECRETKEY_HELP'); ?>">
+						<i class="icon-help"></i>
+					</span>
+				</div>
+			</div>
+		</div>
+		<?php endif; ?>
+		<?php if (!empty($langs)) : ?>
+			<div class="control-group">
+				<div class="controls">
+					<div class="input-prepend">
+						<span class="add-on">
+							<i class="icon-comment hasTooltip" title="<?php echo JHtml::tooltipText('MOD_LOGIN_LANGUAGE'); ?>"></i>
+							<label for="lang" class="element-invisible">
+								<?php echo JText::_('MOD_LOGIN_LANGUAGE'); ?>
+							</label>
+						</span>
+						<?php echo $langs; ?>
+					</div>
+				</div>
+			</div>
+		<?php endif; ?>
+		<hr/>
+		<div class="control-group">
+			<div class="controls">
+				<button tabindex="3" class="btn btn-primary btn-block">
+					<i class="icon-lock icon-white"></i> <?php echo JText::_('MOD_LOGIN_LOGIN'); ?>
+				</button>
+			</div>
+		</div>
+		<input type="hidden" name="option" value="com_login"/>
+		<input type="hidden" name="task" value="login"/>
+		<input type="hidden" name="return" value="<?php echo $return; ?>"/>
+		<?php echo JHtml::_('form.token'); ?>
+	</fieldset>
+</form>
