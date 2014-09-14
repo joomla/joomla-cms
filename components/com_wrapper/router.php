80d410a785cd1d46ea25f4c393477483c80d4f70
wilsonge <georgejameswilson@googlemail.com>
2014-09-14 02:03:31 +0100

    Code style for com_wrapper

--- components/com_wrapper/router.php
+++ components/com_wrapper/router.php
@@ -77,6 +77,10 @@ class WrapperRouter extends JComponentRouterBase
  * These functions are proxys for the new router interface
  * for old SEF extensions.
  *
+ * @param   array  &$query  An array of URL arguments
+ *
+ * @return  array  The URL arguments to use to assemble the subsequent URL.
+ *
  * @deprecated  4.0  Use Class based routers instead
  */
 function WrapperBuildRoute(&$query)
@@ -86,6 +90,18 @@ function WrapperBuildRoute(&$query)
 	return $router->build($query);
 }
 
+/**
+ * Wrapper router functions
+ *
+ * These functions are proxys for the new router interface
+ * for old SEF extensions.
+ *
+ * @param   array  &$segments  The segments of the URL to parse.
+ *
+ * @return  array  The URL attributes to be used by the application.
+ *
+ * @deprecated  4.0  Use Class based routers instead
+ */
 function WrapperParseRoute($segments)
 {
 	$router = new WrapperRouter;
