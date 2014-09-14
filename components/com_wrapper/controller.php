75b9ae40047574294e779c9e8f479aaf822cb600
wilsonge <georgejameswilson@googlemail.com>
2014-09-14 02:03:31 +0100

    [CS] Code Style fixes. Closes #4277, closes #4304, closes #4305

--- components/com_wrapper/controller.php
+++ components/com_wrapper/controller.php
@@ -21,13 +21,14 @@ class WrapperController extends JControllerLegacy
 	/**
 	 * Method to display a view.
 	 *
-	 * @param   boolean			If true, the view output will be cached
-	 * @param   array  An array of safe url parameters and their variable types, for valid values see {@link JFilterInput::clean()}.
+	 * @param   boolean  $cachable   If true, the view output will be cached
+	 * @param   array    $urlparams  An array of safe url parameters and their variable types, for valid values see {@link JFilterInput::clean()}.
 	 *
 	 * @return  JControllerLegacy  This object to support chaining.
+	 *
 	 * @since   1.5
 	 */
-	public function display($cachable = false, $urlparams = false)
+	public function display($cachable = false, $urlparams = array())
 	{
 		$cachable = true;
 
