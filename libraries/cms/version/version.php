2f5a4463873614cae1361872905fff6b8f8a27da
Michael Babker <mbabker@flbab.com>
2014-09-22 09:15:40 -0400

    Reset for development

--- libraries/cms/version/version.php
+++ libraries/cms/version/version.php
@@ -23,10 +23,10 @@ final class JVersion
 	public $RELEASE = '2.5';
 
 	/** @var  string  Maintenance version. */
-	public $DEV_LEVEL = '25';
+	public $DEV_LEVEL = '26-dev';
 
 	/** @var  string  Development STATUS. */
-	public $DEV_STATUS = 'Stable';
+	public $DEV_STATUS = 'Development';
 
 	/** @var  string  Build number. */
 	public $BUILD = '';
