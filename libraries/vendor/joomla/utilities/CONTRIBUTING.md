457bdd7c512ebaaa9abeb7ccb914fe0c4d68473c
Michael Babker <mbabker@flbab.com>
2014-09-23 11:43:14 -0400

    Add non-production files to .gitignore, remove them from repo

--- libraries/vendor/joomla/utilities/CONTRIBUTING.md
+++
@@ -1,3 +0,0 @@
-# Contributing to the Joomla! Framework
-
-Please review [http://framework.joomla.org/contribute](http://framework.joomla.org/contribute) for information on how to contribute to the Framework's development.
--- libraries/vendor/phpmailer/phpmailer/docs/generatedocs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# Regenerate PHPMailer documentation
-# Run from within the docs folder
-rm -rf phpdoc/*
-phpdoc --directory .. --target ./phpdoc --ignore test/,examples/,extras/,test_script/ --sourcecode --force --title PHPMailer --template="clean"
