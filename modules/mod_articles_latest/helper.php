b51c77df5cd0fc2b4b072438ba8250ab14839c09
shur <beshur@gmail.com>
2014-09-27 13:00:35 +0300

    Remove catslug in mod_articles_latest

--- modules/mod_articles_latest/helper.php
+++ modules/mod_articles_latest/helper.php
@@ -114,12 +114,11 @@ abstract class ModArticlesLatestHelper
 		foreach ($items as &$item)
 		{
 			$item->slug    = $item->id . ':' . $item->alias;
-			$item->catslug = $item->catid . ':' . $item->category_alias;
 
 			if ($access || in_array($item->access, $authorised))
 			{
 				// We know that user has the privilege to view the article
-				$item->link = JRoute::_(ContentHelperRoute::getArticleRoute($item->slug, $item->catslug));
+				$item->link = JRoute::_(ContentHelperRoute::getArticleRoute($item->slug, $item->catid));
 			}
 			else
 			{
