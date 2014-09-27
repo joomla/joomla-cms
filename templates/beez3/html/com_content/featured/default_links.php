7fca7470346d5cdb7e6aede790fd0bfa56409b46
shur <beshur@gmail.com>
2014-09-27 22:11:51 +0300

    remove catslug in featured links (beez3 template)

--- templates/beez3/html/com_content/featured/default_links.php
+++ templates/beez3/html/com_content/featured/default_links.php
@@ -8,17 +8,15 @@
  */
 
 defined('_JEXEC') or die;
-
 ?>
 
 <h3><?php echo JText::_('COM_CONTENT_MORE_ARTICLES'); ?></h3>
-
 <ol class="links">
 <?php foreach ($this->link_items as &$item) : ?>
 	<li>
-		<a href="<?php echo JRoute::_(ContentHelperRoute::getArticleRoute($item->slug, $item->catslug)); ?>">
-			<?php echo $item->title; ?></a>
+		<a href="<?php echo JRoute::_(ContentHelperRoute::getArticleRoute($item->slug, $item->catid)); ?>">
+			<?php echo $item->title; ?>
+		</a>
 	</li>
 <?php endforeach; ?>
 </ol>
-
