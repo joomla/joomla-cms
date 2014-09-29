01799923c847684c075e503a9394548ac12e1c72
Michael Babker <mbabker@flbab.com>
2014-09-29 19:35:18 -0400

    Prepare 2.5.26 release

--- libraries/cms/version/version.php
+++ libraries/cms/version/version.php
@@ -23,10 +23,10 @@ final class JVersion
 	public $RELEASE = '2.5';
 
 	/** @var  string  Maintenance version. */
-	public $DEV_LEVEL = '26-dev';
+	public $DEV_LEVEL = '26';
 
 	/** @var  string  Development STATUS. */
-	public $DEV_STATUS = 'Development';
+	public $DEV_STATUS = 'Stable';
 
 	/** @var  string  Build number. */
 	public $BUILD = '';
