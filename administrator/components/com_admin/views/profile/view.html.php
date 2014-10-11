fd106afe12a7aee70fa7426372df4689abca20f2
chrisdavenport <chris.davenport@joomla.org>
2014-10-11 17:35:37 +0100

    [Code Style] administrator/components/com_admin

--- administrator/components/com_admin/views/profile/view.html.php
+++ administrator/components/com_admin/views/profile/view.html.php
@@ -25,7 +25,11 @@ class AdminViewProfile extends JViewLegacy
 	protected $state;
 
 	/**
-	 * Display the view
+	 * Execute and display a template script.
+	 *
+	 * @param   string  $tpl  The name of the template file to parse; automatically searches through the template paths.
+	 *
+	 * @return  mixed  A string if successful, otherwise a Error object.
 	 */
 	public function display($tpl = null)
 	{
@@ -37,6 +41,7 @@ class AdminViewProfile extends JViewLegacy
 		if (count($errors = $this->get('Errors')))
 		{
 			JError::raiseError(500, implode("\n", $errors));
+
 			return false;
 		}
 
@@ -49,6 +54,8 @@ class AdminViewProfile extends JViewLegacy
 
 	/**
 	 * Add the page title and toolbar.
+	 *
+	 * @return  void
 	 *
 	 * @since   1.6
 	 */
