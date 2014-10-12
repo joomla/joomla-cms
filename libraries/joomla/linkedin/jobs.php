9df680263c2d616705e3ffa33ab70a267bd05c39
javier gomez <javier.gomez.corio@gmail.com>
2014-10-12 14:00:09 +0200

    [Code Style] Changes in tag standards on libraries/joomla/linkeding

--- libraries/joomla/linkedin/jobs.php
+++ libraries/joomla/linkedin/jobs.php
@@ -12,9 +12,7 @@ defined('JPATH_PLATFORM') or die();
 /**
  * Linkedin API Jobs class for the Joomla Platform.
  *
- * @package     Joomla.Platform
- * @subpackage  Linkedin
- * @since       13.1
+ * @since  13.1
  */
 class JLinkedinJobs extends JLinkedinObject
 {
