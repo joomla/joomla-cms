9834e9f70171d16ab855295c2dcbc29c4b7d17b5
Roland Dalmulder <contact@rolandd.com>
2014-10-12 11:02:40 +0200

    [Code Style]Clean up libraries/joomla, com_banners & com_config. Fixes #4588. Fixes #4616. Fixes #4615
    [Code Style]Clean up com_banners frontend.
    
    [Code Style]Clean up com_config frontend.

--- components/com_config/controller/templates/save.php
+++ components/com_config/controller/templates/save.php
@@ -14,7 +14,7 @@ defined('_JEXEC') or die('Restricted access');
  * @package     Joomla.Site
  * @subpackage  com_config
  * @since       3.2
-*/
+ */
 class ConfigControllerTemplatesSave extends JControllerBase
 {
 	/**
