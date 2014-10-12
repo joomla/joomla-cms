a4a8924afa48ad930cb950fa5c7bbd065179c6dc
javier gomez <javier.gomez.corio@gmail.com>
2014-10-12 15:13:49 +0200

    [Code Style] Changes in tag standards on libraries/joomla/document

--- libraries/joomla/document/image/image.php
+++ libraries/joomla/document/image/image.php
@@ -12,9 +12,7 @@ defined('JPATH_PLATFORM') or die;
 /**
  * DocumentImage class, provides an easy interface to output image data
  *
- * @package     Joomla.Platform
- * @subpackage  Document
- * @since       12.1
+ * @since  12.1
  */
 class JDocumentImage extends JDocument
 {
