70180eecc47278f723525cef27cdd5edba22842d
chrisdavenport <chris.davenport@joomla.org>
2014-10-12 12:26:33 +0100

    [Code Style] administrator/components/com_login

--- administrator/components/com_login/controller.php
+++ administrator/components/com_login/controller.php
@@ -10,7 +10,7 @@
 defined('_JEXEC') or die;
 
 /**
- * Login Controller
+ * Login Controller.
  *
  * @package     Joomla.Administrator
  * @subpackage  com_login
@@ -19,22 +19,22 @@ defined('_JEXEC') or die;
 class LoginController extends JControllerLegacy
 {
 	/**
-	 * Typical view method for MVC based architecture
+	 * Method to display a view.
 	 *
-	 * This function is provide as a default implementation, in most cases
-	 * you will need to override it in your own controllers.
+	 * @param   boolean  $cachable   If true, the view output will be cached
+	 * @param   array    $urlparams  An array of safe url parameters and their variable types, for valid values see {@link JFilterInput::clean()}.
 	 *
-	 * @param   boolean			If true, the view output will be cached
-	 * @param   array  An array of safe url parameters and their variable types, for valid values see {@link JFilterInput::clean()}.
 	 * @return  JController		This object to support chaining.
+	 *
 	 * @since   1.5
 	 */
 	public function display($cachable = false, $urlparams = false)
 	{
-		// Special treatment is required for this component, as this view may be called
-		// after a session timeout. We must reset the view and layout prior to display
-		// otherwise an error will occur.
-
+		/*
+		 * Special treatment is required for this component, as this view may be called
+		 * after a session timeout. We must reset the view and layout prior to display
+		 * otherwise an error will occur.
+		 */
 		$this->input->set('view', 'login');
 		$this->input->set('layout', 'default');
 
