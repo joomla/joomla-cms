00bcb2a9f6967e3ec31bf51b288401c2a296e7fa
George Wilson <georgejameswilson@googlemail.com>
2014-10-12 09:24:44 +0200

    Fix CS error

--- administrator/components/com_content/views/article/view.html.php
+++ administrator/components/com_content/views/article/view.html.php
@@ -87,8 +87,10 @@ class ContentViewArticle extends JViewLegacy
 
 		// Built the actions for new and existing records.
 		$canDo		= $this->canDo;
-		JToolbarHelper::title(JText::_('COM_CONTENT_PAGE_' . ($checkedOut ? 'VIEW_ARTICLE' : ($isNew ? 'ADD_ARTICLE' : 'EDIT_ARTICLE'))),
-			'pencil-2 article-add');
+		JToolbarHelper::title(
+			JText::_('COM_CONTENT_PAGE_' . ($checkedOut ? 'VIEW_ARTICLE' : ($isNew ? 'ADD_ARTICLE' : 'EDIT_ARTICLE'))),
+			'pencil-2 article-add'
+		);
 
 		// For new records, check the create permission.
 		if ($isNew && (count($user->getAuthorisedCategories('com_content', 'core.create')) > 0))
