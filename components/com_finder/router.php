98ca2e81214c19f0ad688c3f47aebbee0608a164
George Wilson <georgejameswilson@googlemail.com>
2014-10-13 15:56:21 +0200

    com_finder frontend cs

--- components/com_finder/router.php
+++ components/com_finder/router.php
@@ -139,6 +139,10 @@ class FinderRouter extends JComponentRouterBase
  * These functions are proxys for the new router interface
  * for old SEF extensions.
  *
+ * @param   array  &$query  An array of URL arguments
+ *
+ * @return  array  The URL arguments to use to assemble the subsequent URL.
+ *
  * @deprecated  4.0  Use Class based routers instead
  */
 function FinderBuildRoute(&$query)
@@ -148,6 +152,18 @@ function FinderBuildRoute(&$query)
 	return $router->build($query);
 }
 
+/**
+ * Finder router functions
+ *
+ * These functions are proxys for the new router interface
+ * for old SEF extensions.
+ *
+ * @param   array  &$segments  The segments of the URL to parse.
+ *
+ * @return  array  The URL attributes to be used by the application.
+ *
+ * @deprecated  4.0  Use Class based routers instead
+ */
 function FinderParseRoute($segments)
 {
 	$router = new FinderRouter;
