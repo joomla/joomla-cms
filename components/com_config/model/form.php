8b3dab7ae10dcd07602eeb15b206f2b9e1b35dde
Tran Duy Hung <nguyentieuhau@gmail.com>
2014-10-13 22:52:05 +0700

    Fix message is not displayed

--- components/com_config/model/form.php
+++ components/com_config/model/form.php
@@ -310,7 +310,14 @@ abstract class ConfigModelForm extends ConfigModelCms
 			// Get the validation messages from the form.
 			foreach ($form->getErrors() as $message)
 			{
-				JFactory::getApplication()->enqueueMessage($message, 'error');
+				if ($message instanceof Exception)
+				{
+					JFactory::getApplication()->enqueueMessage($message->getMessage(), 'error');
+				}
+				else
+				{
+					JFactory::getApplication()->enqueueMessage($message, 'error');
+				}
 			}
 
 			return false;
