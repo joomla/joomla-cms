cccf9ff2021164e06a69fc2a33ee815e989a8e0a
Nick Savov <nicksavov@users.noreply.github.com>
2014-10-16 02:29:35 -0500

    Use $this->fullPath for the conditional

--- libraries/cms/layout/file.php
+++ libraries/cms/layout/file.php
@@ -147,7 +147,9 @@ class JLayoutFile extends JLayoutBase
 			$rawPath  = str_replace('.', '/', $this->layoutId) . '.php';
 			$this->addDebugMessage('<strong>Searching layout for:</strong> ' . $rawPath);
 
-			if ($this->fullPath = JPath::find($this->includePaths, $rawPath))
+			$this->fullPath = JPath::find($this->includePaths, $rawPath)
+
+			if ($this->fullPath)
 			{
 				$this->addDebugMessage('<strong>Found layout:</strong> ' . $this->fullPath);
 			}
