62d6cbdc973fd13714c0dfe424719b9543001841
javier gomez <javier.gomez.corio@gmail.com>
2014-10-16 16:14:19 +0200

    Fix extra spaces

--- installation/controller/removefolder.php
+++ installation/controller/removefolder.php
@@ -165,7 +165,7 @@ class InstallationControllerRemovefolder extends JControllerBase
 /**
  * JSON Response class for the Joomla Installer.
  *
- * @since       3.1
+ * @since  3.1
  */
 class InstallationResponseJson
 {
