da10cd817d8667ea4961831fcdec910426717b35
sebek <kirapwn@gmail.com>
2014-10-17 16:23:32 +0200

    Added Javascript modal call

--- administrator/components/com_content/views/articles/tmpl/default.php
+++ administrator/components/com_content/views/articles/tmpl/default.php
@@ -14,6 +14,7 @@ JHtml::addIncludePath(JPATH_COMPONENT . '/helpers/html');
 JHtml::_('bootstrap.tooltip');
 JHtml::_('behavior.multiselect');
 JHtml::_('formbehavior.chosen', 'select');
+JHtml::_('behavior.modal', 'a.modal_jform_contenthistory');
 
 $app		= JFactory::getApplication();
 $user		= JFactory::getUser();
