89af7142aa7d16bf4b8eb8ddbbb6e0305a19bf12
David Jardin <d.jardin@djumla.de>
2014-10-17 18:05:50 -0400

    Improved testcases for JModel legacy classes (Fix #4807)

--- tests/unit/suites/libraries/joomla/model/JModelBaseTest.php
+++ tests/unit/suites/libraries/joomla/model/JModelBaseTest.php
@@ -29,7 +29,6 @@ class JModelBaseTest extends TestCase
 	 *
 	 * @return  void
 	 *
-	 * @covers  JModelBase::__construct
 	 * @since   12.1
 	 */
 	public function test__construct()
@@ -49,7 +48,6 @@ class JModelBaseTest extends TestCase
 	 *
 	 * @return  void
 	 *
-	 * @covers  JModelBase::getState
 	 * @since   12.1
 	 */
 	public function testGetState()
@@ -65,7 +63,6 @@ class JModelBaseTest extends TestCase
 	 *
 	 * @return  void
 	 *
-	 * @covers  JModelBase::setState
 	 * @since   12.1
 	 */
 	public function testSetState()
@@ -80,7 +77,6 @@ class JModelBaseTest extends TestCase
 	 *
 	 * @return  void
 	 *
-	 * @covers  JModelBase::loadState
 	 * @since   12.1
 	 */
 	public function testLoadState()
