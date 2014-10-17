80653d30c7d7418176bae8b7eaef9da5c0ac8c4c
Roland Dalmulder <contact@rolandd.com>
2014-10-17 18:01:49 +0200

    Add new files for testing

--- tests/unit/suites/libraries/cms/layout/JLayoutFileTest.php
+++ tests/unit/suites/libraries/cms/layout/JLayoutFileTest.php
@@ -9,17 +9,21 @@
 
 /**
  * Test class for JLayoutFile.
+ *
+ * @since  3.3.7
  */
-class JLayoutFileTest extends TestCase
+class JLayoutFileTest extends TestCaseDatabase
 {
 	/**
 	 * @var JLayoutFile
 	 */
-	protected $object;
+	protected $layoutFile;
 
 	/**
 	 * Sets up the fixture, for example, opens a network connection.
 	 * This method is called before a test is executed.
+	 *
+	 * @return  void
 	 */
 	protected function setUp()
 	{
@@ -29,7 +33,7 @@ class JLayoutFileTest extends TestCase
 
 		JFactory::$application = $this->getMockCmsApp();
 
-		$this->object = new JLayoutFile('joomla.sidebars.submenu');
+		$this->layoutFile = new JLayoutFile('jlayout.submenu', JPATH_TEST_STUBS);
 	}
 
 	/**
@@ -44,17 +48,230 @@ class JLayoutFileTest extends TestCase
 	}
 
 	/**
-	 * Tests the escape method
+	 * @testdox  Render the submenu layout file and compare output is unchanged.
 	 *
-	 * @return  void
-	 *
-	 * @since   3.0
+	 * @since  3.3.7
 	 */
-	public function testEscape()
+	public function testRenderTheSubmenuLayoutFileAndCompareOutputIsUnchanged()
 	{
-		$this->assertThat(
-			$this->object->escape('This is cool & fun to use!'),
-			$this->equalTo('This is cool &amp; fun to use!')
+		// Set the data we want to test
+		$options = new stdClass;
+		$options->displayMenu = true;
+		$options->list = array();
+		$options->displayFilters = false;
+		$options->filters = array();
+
+		// Run the test
+		$this->assertStringEqualsFile(
+			__DIR__ . '/output/submenu.txt',
+			$this->layoutFile->render($options)
+		);
+	}
+
+	/**
+	 * @testdox  Render the submenu layout file with debug enabled and check there is output.
+	 *
+	 * @since  3.3.7
+	 */
+	public function testRenderTheSubmenuLayoutFileWithDebugEnabledAndCheckThereIsOutput()
+	{
+		// Set the data we want to test
+		$options = new stdClass;
+		$options->displayMenu = true;
+		$options->list = array();
+		$options->displayFilters = false;
+		$options->filters = array();
+
+		$this->layoutFile->setLayout('');
+		$this->layoutFile->setOptions(array('debug' => true));
+
+		$output = $this->layoutFile->render($options);
+
+		$this->assertInternalType('string', $output);
+		$this->expectOutputString("<pre></pre>");
+	}
+
+	/**
+	 * @testdox  Add include files to the included path list.
+	 *
+	 * @since  3.3.7
+	 */
+	public function testAddIncludeFilesToTheIncludedPathList()
+	{
+		$this->layoutFile->addIncludePaths(array(JPATH_TEST_STUBS . '/jlayout/'));
+		$this->layoutFile->setLayout('includepath');
+		$this->layoutFile->render(array());
+		$this->markTestIncomplete('Waiting for a PR form phproberto to fix the addIncludePaths');
+		//$this->expectOutputString("OK");
+	}
+
+	/**
+	 * @testdox  Remove include files from the path by removing a path added by the test.
+	 *
+	 * @since  3.3.7
+	 */
+	public function testRemoveIncludeFilesFromThePathByRemovingAPathAddedByTheTest()
+	{
+		$this->layoutFile->addIncludePaths(array(JPATH_TEST_STUBS . '/jlayout/'));
+		$this->layoutFile->removeIncludePath(array(JPATH_TEST_STUBS . '/jlayout/'));
+		$this->layoutFile->setLayout('includepath');
+		$this->layoutFile->render(array());
+		$this->markTestIncomplete('Waiting for a PR form phproberto to fix the addIncludePaths');
+		//$this->expectOutputString("");
+	}
+
+	/**
+	 * Gets the data set to be loaded into the database during setup
+	 *
+	 * @return  PHPUnit_Extensions_Database_DataSet_CsvDataSet
+	 *
+	 * @since   3.2
+	 */
+	protected function getDataSet()
+	{
+		$dataSet = new PHPUnit_Extensions_Database_DataSet_CsvDataSet(',', "'", '\\');
+
+		$dataSet->addTable('jos_extensions', JPATH_TEST_DATABASE . '/jos_extensions.csv');
+
+		return $dataSet;
+	}
+
+	/**
+	 * @testdox  Set component and check if it is set.
+	 *
+	 * @since  3.3.7
+	 */
+	public function testSetComponentAndCheckIfItIsSet()
+	{
+		$this->layoutFile->setComponent('com_users');
+		$options = $this->layoutFile->getOptions();
+		$this->assertEquals('com_users', $options->get('component'));
+	}
+
+	/**
+	 * @testdox  Set invalid component and check if it is not set.
+	 *
+	 * @since  3.3.7
+	 */
+	public function testSetInvalidComponentAndCheckIfItIsNotSet()
+	{
+		$this->layoutFile->setComponent('com_mock');
+		$options = $this->layoutFile->getOptions();
+		$this->assertEquals('', $options->get('component'));
+	}
+
+	/**
+	 * @testdox  Set none component to empty the component in the options and check if it is not set.
+	 *
+	 * @since  3.3.7
+	 */
+	public function testSetNoneComponentToEmptyTheComponentInTheOptionsAndCheckIfItIsNotSet()
+	{
+		$this->layoutFile->setComponent('none');
+		$options = $this->layoutFile->getOptions();
+		$this->assertEquals('', $options->get('component'));
+	}
+
+	/**
+	 * @testdox  Set auto component using define and check if it is set.
+	 *
+	 * @since  3.3.7
+	 */
+	public function testSetAutoComponentUsingDefineAndCheckIfItIsSet()
+	{
+		define('JPATH_COMPONENT', 'com_users');
+		$this->layoutFile->setComponent('auto');
+		$options = $this->layoutFile->getOptions();
+		$this->assertEquals('com_users', $options->get('component'));
+	}
+
+	/**
+	 * @testdox  Set site client using string and check if it is set.
+	 *
+	 * @since  3.3.7
+	 */
+	public function testSetSiteClientUsingStringAndCheckIfItIsSet()
+	{
+		$this->layoutFile->setClient('site');
+		$options = $this->layoutFile->getOptions();
+		$this->assertEquals(0, $options->get('client'));
+	}
+
+	/**
+	 * @testdox  Set site client using int and check if it is set.
+	 *
+	 * @since  3.3.7
+	 */
+	public function testSetSiteClientUsingIntAndCheckIfItIsSet()
+	{
+		$this->layoutFile->setClient(0);
+		$options = $this->layoutFile->getOptions();
+		$this->assertEquals(0, $options->get('client'));
+	}
+
+	/**
+	 * @testdox  Set admin client using string and check if it is set.
+	 *
+	 * @since  3.3.7
+	 */
+	public function testSetAdminClientUsingStringAndCheckIfItIsSet()
+	{
+		$this->layoutFile->setClient('admin');
+		$options = $this->layoutFile->getOptions();
+		$this->assertEquals(1, $options->get('client'));
+	}
+
+	/**
+	 * @testdox  Set admin client using int and check if it is set.
+	 *
+	 * @since  3.3.7
+	 */
+	public function testSetAdminClientUsingIntAndCheckIfItIsSet()
+	{
+		$this->layoutFile->setClient(1);
+		$options = $this->layoutFile->getOptions();
+		$this->assertEquals(1, $options->get('client'));
+	}
+
+	/**
+	 * @testdox  Set debug and check debug is enabled.
+	 *
+	 * @since  3.3.7
+	 */
+	public function testSetDebugAndCheckDebugIsEnabled()
+	{
+		$this->layoutFile->setLayout('');
+		$this->layoutFile->setDebug(true);
+
+		$options = new stdClass;
+		$options->displayMenu = true;
+		$options->list = array();
+		$options->displayFilters = false;
+		$options->filters = array();
+
+		$this->layoutFile->render($options);
+
+		$this->expectOutputString("<pre></pre>");
+	}
+
+	/**
+	 * @testdox  Render a sublayout
+	 *
+	 * @since  3.3.7
+	 */
+	public function testRenderASublayout()
+	{
+		$options = new stdClass;
+		$options->displayMenu = true;
+		$options->list = array();
+		$options->displayFilters = false;
+		$options->filters = array();
+
+		$this->layoutFile->sublayout('submenu', $options);
+
+		$this->assertStringEqualsFile(
+			__DIR__ . '/output/submenu.txt',
+			$this->layoutFile->render($options)
 		);
 	}
 }
