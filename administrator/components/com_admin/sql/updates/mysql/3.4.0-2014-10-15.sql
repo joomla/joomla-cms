7437f5f6df27f84a64430c05f4a4b05a0ca5fa77
Gunjan Patel <gunjan@redcomponent.com>
2014-10-17 16:54:48 +0530

    Revert my MySQL changes

--- administrator/components/com_admin/sql/updates/mysql/3.4.0-2014-10-15.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE  `#__content` CHANGE  `images`  `images` TEXT NULL DEFAULT NULL;
-ALTER TABLE  `#__content` CHANGE  `urls`  `urls` TEXT NULL DEFAULT NULL;
-ALTER TABLE  `#__content` CHANGE  `attribs`  `attribs` VARCHAR( 5120 ) NULL DEFAULT NULL;
-ALTER TABLE  `#__content` CHANGE  `metadata`  `metadata` TEXT NULL DEFAULT NULL;
