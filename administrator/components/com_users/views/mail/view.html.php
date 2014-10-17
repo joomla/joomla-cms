06de313e263696e203a3626b6cf4d073c1072eb5
Mihail Irintchev <m.irintchev@siteground.com>
2014-10-17 14:28:40 +0300

    Complying to coding standard

--- administrator/components/com_users/views/mail/view.html.php
+++ administrator/components/com_users/views/mail/view.html.php
@@ -31,7 +31,8 @@ class UsersViewMail extends JViewLegacy
 	public function display($tpl = null)
 	{
 		// Redirect to admin index if mass mailer disabled in conf
-		if (JFactory::getApplication()->get('massmailoff') == 1) {
+		if (JFactory::getApplication()->get('massmailoff') == 1) 
+		{
 			JFactory::getApplication()->redirect(JRoute::_('index.php', false));
 		}
 
