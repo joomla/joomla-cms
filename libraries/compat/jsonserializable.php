7e96afc3825673a4dba45e398f9eb2c2e9dfa68f
javier gomez <javier.gomez.corio@gmail.com>
2014-10-17 16:46:10 +0200

    Fix @since tag comment indented incorrectly; expected 2 spaces but  found 4

--- libraries/compat/jsonserializable.php
+++ libraries/compat/jsonserializable.php
@@ -12,8 +12,8 @@ defined('JPATH_PLATFORM') or die;
  * JsonSerializable interface. This file provides backwards compatibility to PHP 5.3 and ensures
  * the interface is present in systems where JSON related code was removed.
  *
- * @link     http://www.php.net/manual/en/jsonserializable.jsonserialize.php
- * @since    12.2
+ * @link   http://www.php.net/manual/en/jsonserializable.jsonserialize.php
+ * @since  12.2
  */
 interface JsonSerializable
 {
