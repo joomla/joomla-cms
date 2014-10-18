1bae29780a74f70ba878158e5a98427c2f807ac8
Nokkaew <newpanasonic@gmail.com>
2014-10-19 04:43:21 +0700

    Update edit.php

--- administrator/components/com_menus/views/item/tmpl/edit.php
+++ administrator/components/com_menus/views/item/tmpl/edit.php
@@ -69,7 +69,7 @@ JFactory::getDocument()->addScriptDeclaration($script);
 		{
 			Joomla.submitform(task, document.id('item-form'));
 		}
-		else if(document.formvalidator.isValid(document.id('item-form')))
+		elseif(document.formvalidator.isValid(document.getElementById('item-form')))
 		{
 			// special case for modal popups validation response
 			$$('#item-form .modal-value.invalid').each(function(field)
