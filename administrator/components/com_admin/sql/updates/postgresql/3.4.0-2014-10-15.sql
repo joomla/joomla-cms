d051083792fa8e641babd54b40aa1caec79e5a2a
Gunjan Patel <gunjan@redcomponent.com>
2014-10-20 10:31:25 +0530

    [fix #3087] Set default JSON empty string

--- administrator/components/com_admin/sql/updates/postgresql/3.4.0-2014-10-15.sql
+++ administrator/components/com_admin/sql/updates/postgresql/3.4.0-2014-10-15.sql
@@ -1,4 +1,4 @@
-ALTER TABLE `#__content` ALTER COLUMN `images` DROP NOT NULL;
-ALTER TABLE `#__content` ALTER COLUMN `urls` DROP NOT NULL;
-ALTER TABLE `#__content` ALTER COLUMN `attribs` DROP NOT NULL;
-ALTER TABLE `#__content` ALTER COLUMN `metadata` DROP NOT NULL;
+ALTER TABLE `#__content` ALTER COLUMN `images` SET DEFAULT '{}';
+ALTER TABLE `#__content` ALTER COLUMN `urls` SET DEFAULT '{}';
+ALTER TABLE `#__content` ALTER COLUMN `attribs` SET DEFAULT '{}';
+ALTER TABLE `#__content` ALTER COLUMN `metadata` SET DEFAULT '{}';
