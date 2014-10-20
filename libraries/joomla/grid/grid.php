6415ec44562aada34e5ec3550e70d74c68e0e734
Thomas Hunziker <werbemails@bakual.ch>
2014-10-20 18:00:09 +0200

    [Code Style] Another indention issue

--- libraries/joomla/grid/grid.php
+++ libraries/joomla/grid/grid.php
@@ -3,8 +3,8 @@
  * @package     Joomla.Platform
  * @subpackage  Grid
 
- * @copyright  Copyright (C) 2005 - 2014 Open Source Matters, Inc. All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE
+ * @copyright   Copyright (C) 2005 - 2014 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
  */
 
 defined('JPATH_PLATFORM') or die;
