5fdc60a7d57aeac72c0c4defc58a6950323f63db
Troy T. Hall <troy@jowwow.net>
2014-10-23 11:51:03 -0500

    resolved merge conflict

--- administrator/components/com_content/models/fields/modal/article.php
+++ administrator/components/com_content/models/fields/modal/article.php
@@ -145,7 +145,7 @@ class JFormFieldModal_Article extends JFormField
 		if ($allowClear)
 		{
 			$html[] = '<button id="' . $this->id . '_clear" class="btn' . ($value ? '' : ' hidden') . '" onclick="return jClearArticle(\'' .
-				$this->id . '\')"><span class="icon-remove"></span>' . JText::_('JCLEAR') . '</button>';
+				$this->id . '\')"><span class="icon-remove"></span> ' . JText::_('JCLEAR') . '</button>';
 		}
 
 		$html[] = '</span>';
