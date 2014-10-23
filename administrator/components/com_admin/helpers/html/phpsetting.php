059e41c49d078e52d64e81a1456906b0613f11fb
zero-24 <zero-24@users.noreply.github.com>
2014-10-23 16:07:49 +0200

    Update phpsetting.php

--- administrator/components/com_admin/helpers/html/phpsetting.php
+++ administrator/components/com_admin/helpers/html/phpsetting.php
@@ -19,7 +19,9 @@ abstract class JHtmlPhpSetting
 	/**
 	 * Method to generate a boolean message for a value
 	 *
-	 * @param   boolean  $val  is the value set?
+	 * @param   boolean  $val  Is the value set?
+	 * 
+	 * @since   1.6
 	 *
 	 * @return  string html code
 	 */
@@ -38,7 +40,9 @@ abstract class JHtmlPhpSetting
 	/**
 	 * Method to generate a boolean message for a value
 	 *
-	 * @param   boolean  $val  is the value set?
+	 * @param   boolean  $val  Is the value set?
+	 * 
+	 * @since   1.6
 	 *
 	 * @return  string html code
 	 */
@@ -57,7 +61,9 @@ abstract class JHtmlPhpSetting
 	/**
 	 * Method to generate a string message for a value
 	 *
-	 * @param   string  $val  a php ini value
+	 * @param   string  $val  A php ini value
+	 * 
+	 * @since   1.6
 	 *
 	 * @return  string html code
 	 */
@@ -76,9 +82,11 @@ abstract class JHtmlPhpSetting
 	/**
 	 * Method to generate an integer from a value
 	 *
-	 * @param   string  $val  a php ini value
+	 * @param   string  $val  A php ini value
 	 *
 	 * @return  string html code
+	 * 
+	 * @since   1.6
 	 *
 	 * @deprecated  4.0  Use intval() or casting instead.
 	 */
