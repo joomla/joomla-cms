0c3927bb08a55c0cde923985d1487a7c91179fb8
zero-24 <zero-24@users.noreply.github.com>
2014-10-23 16:02:47 +0200

    [Codestyle] for sysinfo

--- administrator/components/com_admin/models/sysinfo.php
+++ administrator/components/com_admin/models/sysinfo.php
@@ -67,23 +67,23 @@ class AdminModelSysInfo extends JModelLegacy
 	{
 		if (is_null($this->php_settings))
 		{
-			$this->php_settings = array();
-			$this->php_settings['safe_mode']			= ini_get('safe_mode') == '1';
-			$this->php_settings['display_errors']		= ini_get('display_errors') == '1';
-			$this->php_settings['short_open_tag']		= ini_get('short_open_tag') == '1';
-			$this->php_settings['file_uploads']			= ini_get('file_uploads') == '1';
-			$this->php_settings['magic_quotes_gpc']		= ini_get('magic_quotes_gpc') == '1';
-			$this->php_settings['register_globals']		= ini_get('register_globals') == '1';
-			$this->php_settings['output_buffering']		= (bool) ini_get('output_buffering');
-			$this->php_settings['open_basedir']			= ini_get('open_basedir');
-			$this->php_settings['session.save_path']	= ini_get('session.save_path');
-			$this->php_settings['session.auto_start']	= ini_get('session.auto_start');
-			$this->php_settings['disable_functions']	= ini_get('disable_functions');
-			$this->php_settings['xml']					= extension_loaded('xml');
-			$this->php_settings['zlib']					= extension_loaded('zlib');
-			$this->php_settings['zip']					= function_exists('zip_open') && function_exists('zip_read');
-			$this->php_settings['mbstring']				= extension_loaded('mbstring');
-			$this->php_settings['iconv']				= function_exists('iconv');
+			$this->php_settings                       = array();
+			$this->php_settings['safe_mode']          = ini_get('safe_mode') == '1';
+			$this->php_settings['display_errors']     = ini_get('display_errors') == '1';
+			$this->php_settings['short_open_tag']     = ini_get('short_open_tag') == '1';
+			$this->php_settings['file_uploads']       = ini_get('file_uploads') == '1';
+			$this->php_settings['magic_quotes_gpc']   = ini_get('magic_quotes_gpc') == '1';
+			$this->php_settings['register_globals']   = ini_get('register_globals') == '1';
+			$this->php_settings['output_buffering']   = (bool) ini_get('output_buffering');
+			$this->php_settings['open_basedir']       = ini_get('open_basedir');
+			$this->php_settings['session.save_path']  = ini_get('session.save_path');
+			$this->php_settings['session.auto_start'] = ini_get('session.auto_start');
+			$this->php_settings['disable_functions']  = ini_get('disable_functions');
+			$this->php_settings['xml']                = extension_loaded('xml');
+			$this->php_settings['zlib']               = extension_loaded('zlib');
+			$this->php_settings['zip']                = function_exists('zip_open') && function_exists('zip_read');
+			$this->php_settings['mbstring']           = extension_loaded('mbstring');
+			$this->php_settings['iconv']              = function_exists('iconv');
 		}
 
 		return $this->php_settings;
@@ -100,9 +100,9 @@ class AdminModelSysInfo extends JModelLegacy
 	{
 		if (is_null($this->config))
 		{
-			$registry = new JRegistry(new JConfig);
+			$registry     = new JRegistry(new JConfig);
 			$this->config = $registry->toArray();
-			$hidden = array('host', 'user', 'password', 'ftp_user', 'ftp_pass', 'smtpuser', 'smtppass');
+			$hidden       = array('host', 'user', 'password', 'ftp_user', 'ftp_pass', 'smtpuser', 'smtppass');
 
 			foreach ($hidden as $key)
 			{
@@ -125,9 +125,9 @@ class AdminModelSysInfo extends JModelLegacy
 		if (is_null($this->info))
 		{
 			$this->info = array();
-			$version = new JVersion;
-			$platform = new JPlatform;
-			$db = JFactory::getDbo();
+			$version    = new JVersion;
+			$platform   = new JPlatform;
+			$db         = JFactory::getDbo();
 
 			if (isset($_SERVER['SERVER_SOFTWARE']))
 			{
@@ -138,15 +138,15 @@ class AdminModelSysInfo extends JModelLegacy
 				$sf = getenv('SERVER_SOFTWARE');
 			}
 
-			$this->info['php']			= php_uname();
-			$this->info['dbversion']	= $db->getVersion();
-			$this->info['dbcollation']	= $db->getCollation();
-			$this->info['phpversion']	= phpversion();
-			$this->info['server']		= $sf;
-			$this->info['sapi_name']	= php_sapi_name();
-			$this->info['version']		= $version->getLongVersion();
-			$this->info['platform']		= $platform->getLongVersion();
-			$this->info['useragent']	= isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : "";
+			$this->info['php']         = php_uname();
+			$this->info['dbversion']   = $db->getVersion();
+			$this->info['dbcollation'] = $db->getCollation();
+			$this->info['phpversion']  = phpversion();
+			$this->info['server']      = $sf;
+			$this->info['sapi_name']   = php_sapi_name();
+			$this->info['version']     = $version->getLongVersion();
+			$this->info['platform']    = $platform->getLongVersion();
+			$this->info['useragent']   = isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : "";
 		}
 
 		return $this->info;
@@ -194,9 +194,8 @@ class AdminModelSysInfo extends JModelLegacy
 		if (is_null($this->directories))
 		{
 			$this->directories = array();
-
-			$registry = JFactory::getConfig();
-			$cparams = JComponentHelper::getParams('com_media');
+			$registry          = JFactory::getConfig();
+			$cparams           = JComponentHelper::getParams('com_media');
 
 			$this->_addDirectory('administrator/components', JPATH_ADMINISTRATOR . '/components');
 			$this->_addDirectory('administrator/language', JPATH_ADMINISTRATOR . '/language');
@@ -325,7 +324,7 @@ class AdminModelSysInfo extends JModelLegacy
 	{
 		if (is_null($this->editor))
 		{
-			$config = JFactory::getConfig();
+			$config       = JFactory::getConfig();
 			$this->editor = $config->get('editor');
 		}
 
