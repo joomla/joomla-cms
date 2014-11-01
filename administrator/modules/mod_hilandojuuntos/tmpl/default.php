0cf7bfa76422ce64fa7df63652c7ba4ad307333b
tuxmerlin <migueltuyare@gmail.com>
2014-11-01 11:09:33 -0300

    [Cambio] URL al piede la salida html

--- administrator/modules/mod_hilandojuuntos/tmpl/default.php
+++ administrator/modules/mod_hilandojuuntos/tmpl/default.php
@@ -11,5 +11,5 @@ defined('_JEXEC') or die;
 	<?php echo modHilandojuuntosHelper::render($params); ?>
 </div>
 <div style="text-align:center;font-size:9px;padding:10px">
-Este módulo administrativo tiene licencia GNU/GPL v2 y es gratuito. <a href="http://www.juuntos.net" target="blank">Potenciado por la Comunidad Juuntos</a>
+Este módulo administrativo tiene licencia GNU/GPL v2 y es gratuito. <a href="http://www.juuntos.org" target="blank">Potenciado por la Comunidad Juuntos</a>
 </div>
