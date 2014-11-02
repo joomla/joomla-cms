4d05a1055226807361b13746f5297fc1034e4550
Roland Dalmulder <contact@rolandd.com>
2014-11-02 23:55:20 +0100

    [CS] Code style fixes on the libraries/cms folder. Closes #4976

--- libraries/cms/html/menu.php
+++ libraries/cms/html/menu.php
@@ -304,7 +304,7 @@ abstract class JHtmlMenu
 	 * @param   array    &$children  The children of the current item
 	 * @param   integer  $maxlevel   The maximum number of levels in the tree
 	 * @param   integer  $level      The starting level
-	 * @param   string   $type       Type of link: component, URL, alias, separator
+	 * @param   int      $type       Set the type of spacer to use. Use 1 for |_ or 0 for -
 	 *
 	 * @return  array
 	 *
