dc22a95d96cd5a57bffbc4616788a9a0763722e2
Michael Babker <mbabker@flbab.com>
2014-11-08 16:39:11 -0500

    Tweaks to the processing code

--- plugins/quickicon/eosnotify/eosnotify.php
+++ plugins/quickicon/eosnotify/eosnotify.php
@@ -10,9 +10,10 @@
 defined('_JEXEC') or die;
 
 /**
- * Joomla! update notification plugin
+ * Joomla! End of Support Notification Plugin
  *
- * @since		2.5
+ * @since       2.5.28
+ * @deprecated  3.0
  */
 class plgQuickiconEosnotify extends JPlugin
 {
@@ -24,7 +25,7 @@ class plgQuickiconEosnotify extends JPlugin
 	 *                             Recognized key values include 'name', 'group', 'params', 'language'
 	 *                             (this list is not meant to be comprehensive).
 	 *
-	 * @since   2.5
+	 * @since   2.5.28
 	 */
 	public function __construct(&$subject, $config = array())
 	{
@@ -38,32 +39,27 @@ class plgQuickiconEosnotify extends JPlugin
 	 * of icons. You can return an array which defines a single icon and it will
 	 * be rendered right after the stock Quick Icons.
 	 *
-	 * @param  $context  The calling context
+	 * @param   $context  The calling context
 	 *
-	 * @return array A list of icon definition associative arrays, consisting of the
-	 *				 keys link, image, text and access.
+	 * @return  array  A list of icon definition associative arrays, consisting of the
+	 *				   keys link, image, text and access.
 	 *
-	 * @since       2.5
+	 * @since   2.5.28
 	 */
 	public function onGetIcons($context)
 	{
-		$text = JText::_('PLG_EOSNOTIFY_SUPPORT_ENDING');
-		if(date('Ymd')>='20150101')
-			$text = JText::_('PLG_EOSNOTIFY_SUPPORT_ENDED');
-			
-		if (JAdministratorHelper::findOption() == 'com_cpanel')
-		{ 
-			$messtext = '<div style="background-color:#FFCFCF; font-size:16px; font-weight:bold; margin-bottom: 10px; padding: 10px; border-radius: 10px;">' . $text . ' ' . JText::_('PLG_EOSNOTIFY_CLICK_FOR_INFORMATION_WITH_LINK') . '</div>';
-			echo("<script>document.getElementById('system-message-container').innerHTML = '" . $messtext . "';</script>");
-		}
-		
-		return array(
-			array(
-				'link' => 'http://docs.joomla.org/Why_Migrate',
-				'image' => JURI::root() . 'plugins/quickicon/eosnotify/stop15.png',
-				'text' => '<span style="color:red;">' . $text . '<br />' . JText::_('PLG_EOSNOTIFY_CLICK_FOR_INFORMATION') . '.</span>',
-				'id' => 'plg_quickicon_eos'
-			)
-		);
+        if (!JFactory::getApplication()->isAdmin()) {
+            return;
+        }
+
+        $text = JText::_('PLG_EOSNOTIFY_SUPPORT_ENDING');
+
+        if (JFactory::getDate() >= '2015-01-01') {
+            $text = JText::_('PLG_EOSNOTIFY_SUPPORT_ENDED');
+        }
+
+        if (JAdministratorHelper::findOption() == 'com_cpanel') {
+            JFactory::getApplication()->enqueueMessage(JText::sprintf('PLG_EOSNOTIFY_CLICK_FOR_INFORMATION_WITH_LINK', $text), 'error');
+        }
 	}
 }
