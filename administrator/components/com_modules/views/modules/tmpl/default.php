394b5d6ff5d6fed99816a7b70c6a2ad731e67475
dgt41 <d.grammatiko@gmail.com>
2014-11-10 14:28:16 +0200

    Quotes

--- administrator/components/com_modules/views/modules/tmpl/default.php
+++ administrator/components/com_modules/views/modules/tmpl/default.php
@@ -34,7 +34,7 @@ jQuery(document).ready(function() {
 		table = document.getElementById("sortTable");
 		direction = document.getElementById("directionTable");
 		order = table.options[table.selectedIndex].value;
-		if (order != ' . $listOrder . ')
+		if (order != "' . $listOrder . '")
 		{
 			dirn = "asc";
 		}
