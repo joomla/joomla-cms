ccd2912dafc8805b6ef6b647586a10652e87dcce
dgt41 <d.grammatiko@gmail.com>
2014-11-15 07:33:52 +0200

    init

--- components/com_mailto/views/mailto/tmpl/default.php
+++ components/com_mailto/views/mailto/tmpl/default.php
@@ -8,24 +8,25 @@
  */
 
 defined('_JEXEC') or die;
+JHtml::_('behavior.core');
 JHtml::_('behavior.keepalive');
-?>
-<script type="text/javascript">
+
+$data	= $this->get('data');
+
+JFactory::getDocument()->addScriptDeclaration("
 	Joomla.submitbutton = function(pressbutton)
 	{
 		var form = document.getElementById('mailtoForm');
 
 		// do field validation
-		if (form.mailto.value == "" || form.from.value == "")
+		if (form.mailto.value == '' || form.from.value == '')
 		{
-			alert('<?php echo JText::_('COM_MAILTO_EMAIL_ERR_NOINFO'); ?>');
+			alert('" . JText::_('COM_MAILTO_EMAIL_ERR_NOINFO') . "');
 			return false;
 		}
 		form.submit();
 	}
-</script>
-<?php
-$data	= $this->get('data');
+");
 ?>
 
 <div id="mailto-window">
