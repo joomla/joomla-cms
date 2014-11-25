8d7dbcaf5f125eff92ceb5504ae69c3ed0e53fcb
Jean-Marie Simonet <infografjms@gmail.com>
2014-11-25 11:50:19 +0100

    Correcting comments

--- installation/application/router.php
+++ installation/application/router.php
@@ -32,7 +32,7 @@ class JRouterInstallation extends JRouter
 	/**
 	 * Function to convert an internal URI to a route
 	 *
-	 * @param   string  &$url  The internal URL
+	 * @param   string  $url  The internal URL
 	 *
 	 * @return  string  The absolute search engine friendly URL
 	 *
