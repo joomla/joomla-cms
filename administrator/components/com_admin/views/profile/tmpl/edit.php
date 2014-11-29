d469d6f77c0075f691264576ab529af15a4fca0d
dgt41 <d.grammatiko@gmail.com>
2014-11-30 01:00:14 +0200

    .ready

--- administrator/components/com_admin/views/profile/tmpl/edit.php
+++ administrator/components/com_admin/views/profile/tmpl/edit.php
@@ -15,7 +15,6 @@ JHtml::addIncludePath(JPATH_COMPONENT . '/helpers/html');
 JHtml::_('behavior.formvalidator');
 
 JFactory::getDocument()->addScriptDeclaration('
-jQuery(document).ready(function() {
 	Joomla.submitbutton = function(task)
 	{
 		if (task == "profile.cancel" || document.formvalidator.isValid(document.id("profile-form")))
@@ -23,7 +22,7 @@ jQuery(document).ready(function() {
 			Joomla.submitform(task, document.getElementById("profile-form"));
 		}
 	}
-});');
+');
 // Load chosen.css
 JHtml::_('formbehavior.chosen', 'select');
 
