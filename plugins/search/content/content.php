3aa50f81c0dcc4059739c39b651a60af689257e0
Thomas Hunziker <werbemails@bakual.ch>
2014-12-02 21:15:20 +0100

    Codestyle

--- plugins/search/content/content.php
+++ plugins/search/content/content.php
@@ -107,11 +107,11 @@ class PlgSearchContent extends JPlugin
 				{
 					$word = $db->quote('%' . $db->escape($word, true) . '%', false);
 					$wheres2 = array();
-					$wheres2[] = 'LOWER(a.title) LIKE LOWER(' . $word .')';
-                    			$wheres2[] = 'LOWER(a.introtext) LIKE LOWER(' . $word .')';
-                    			$wheres2[] = 'LOWER(a.fulltext) LIKE LOWER(' . $word .')';
-                    			$wheres2[] = 'LOWER(a.metakey) LIKE LOWER(' . $word .')';
-                    			$wheres2[] = 'LOWER(a.metadesc) LIKE LOWER(' . $word .')';
+					$wheres2[] = 'LOWER(a.title) LIKE LOWER(' . $word . ')';
+					$wheres2[] = 'LOWER(a.introtext) LIKE LOWER(' . $word . ')';
+					$wheres2[] = 'LOWER(a.fulltext) LIKE LOWER(' . $word . ')';
+					$wheres2[] = 'LOWER(a.metakey) LIKE LOWER(' . $word . ')';
+					$wheres2[] = 'LOWER(a.metadesc) LIKE LOWER(' . $word . ')';
 					$wheres[] = implode(' OR ', $wheres2);
 				}
 
