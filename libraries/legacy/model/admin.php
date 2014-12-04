e31f439aba9c8ccd8417c7516cf45169ddf12a71
Hannes Papenberg <info@joomlager.de>
2014-12-04 10:46:26 +0100

    Updating $contexts when doing batch copy

--- libraries/legacy/model/admin.php
+++ libraries/legacy/model/admin.php
@@ -220,6 +220,7 @@ abstract class JModelAdmin extends JModelForm
 
 				if (is_array($result))
 				{
+					$contexts = array_merge($contexts, array_fill_keys($result, $contexts[$pks[0]]));
 					$pks = $result;
 				}
 				else
