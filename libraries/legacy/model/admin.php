843b63f5b0d2fa516ad6ee329f6e4f42868ad3d0
Hannes Papenberg <info@joomlager.de>
2014-12-06 10:32:51 +0100

    Removing unused code

--- libraries/legacy/model/admin.php
+++ libraries/legacy/model/admin.php
@@ -197,17 +197,6 @@ abstract class JModelAdmin extends JModelForm
 			$this->type = $type->getTypeByAlias($this->typeAlias);
 		}
 
-		if ($this->type === false)
-		{
-			$type = new JUcmType;
-			$this->type = $type->getTypeByAlias($this->typeAlias);
-			$typeAlias = $this->type->type_alias;
-		}
-		else
-		{
-			$typeAlias = $this->type->type_alias;
-		}
-
 		$this->tagsObserver = $this->table->getObserverOfClass('JTableObserverTags');
 
 		if (!empty($commands['category_id']))
