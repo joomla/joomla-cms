23dae5412e3757799416b91eca315ee6301de6ba
shur <shur@users.noreply.github.com>
2014-12-07 12:22:07 +0300

    Update helper.php

--- modules/mod_articles_popular/helper.php
+++ modules/mod_articles_popular/helper.php
@@ -66,6 +66,7 @@ abstract class ModArticlesPopularHelper
 		foreach ($items as &$item)
 		{
 			$item->slug    = $item->id . ':' . $item->alias;
+			
 			// TODO: To remove because catslug is not used in non-SEF article URLs in com_content.
 			$item->catslug = $item->catid . ':' . $item->category_alias;
 
