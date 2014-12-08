0d3ef7c076c92715e6e2371f38f8c25cd72bf105
Hannes Papenberg <info@joomlager.de>
2014-12-08 11:00:19 +0100

    Fixing pagenavigation

--- plugins/content/pagenavigation/pagenavigation.php
+++ plugins/content/pagenavigation/pagenavigation.php
@@ -125,22 +125,16 @@ class PlgContentPagenavigation extends JPlugin
 			$query = $db->getQuery(true);
 
 			// Sqlsrv changes
-			$case_when = ' CASE WHEN ';
-			$case_when .= $query->charLength('a.alias', '!=', '0');
-			$case_when .= ' THEN ';
+			$case_when = ' CASE WHEN ' . $query->charLength('a.alias', '!=', '0');
 			$a_id = $query->castAsChar('a.id');
-			$case_when .= $query->concatenate(array($a_id, 'a.alias'), ':');
-			$case_when .= ' ELSE ';
-			$case_when .= $a_id . ' END as slug';
+			$case_when .= ' THEN ' . $query->concatenate(array($a_id, 'a.alias'), ':');
+			$case_when .= ' ELSE ' . $a_id . ' END as slug';
 
-			$case_when1 = ' CASE WHEN ';
-			$case_when1 .= $query->charLength('cc.alias', '!=', '0');
-			$case_when1 .= ' THEN ';
+			$case_when1 = ' CASE WHEN ' . $query->charLength('cc.alias', '!=', '0');
 			$c_id = $query->castAsChar('cc.id');
-			$case_when1 .= $query->concatenate(array($c_id, 'cc.alias'), ':');
-			$case_when1 .= ' ELSE ';
-			$case_when1 .= $c_id . ' END as catslug';
-			$query->select('a.id, a.title,' . $case_when . ',' . $case_when1)
+			$case_when1 .= ' THEN ' . $query->concatenate(array($c_id, 'cc.alias'), ':');
+			$case_when1 .= ' ELSE ' . $c_id . ' END as catslug';
+			$query->select('a.id, a.title, a.catid, a.language,' . $case_when . ',' . $case_when1)
 				->from('#__content AS a')
 				->join('LEFT', '#__categories AS cc ON cc.id = a.catid')
 				->where(
