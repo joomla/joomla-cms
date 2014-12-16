47dd79b2ba3aeac288acf42ae2cdecf95bbdb75d
zero-24 <zero-24@users.noreply.github.com>
2014-12-16 22:07:34 +0100

    Update default_options.php

--- components/com_config/view/modules/tmpl/default_options.php
+++ components/com_config/view/modules/tmpl/default_options.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @package     Joomla.site
+ * @package     Joomla.Site
  * @subpackage  com_config
  *
  * @copyright   Copyright (C) 2005 - 2014 Open Source Matters, Inc. All rights reserved.
@@ -15,41 +15,34 @@ echo JHtml::_('bootstrap.startAccordion', 'collapseTypes');
 $i = 0;
 
 foreach ($fieldSets as $name => $fieldSet) :
+	$label = !empty($fieldSet->label) ? $fieldSet->label : 'COM_MODULES_' . $name . '_FIELDSET_LABEL';
+	$class = isset($fieldSet->class) && !empty($fieldSet->class) ? $fieldSet->class : '';
+	
+	if (isset($fieldSet->description) && trim($fieldSet->description))
+	{
+		echo '<p class="tip">' . $this->escape(JText::_($fieldSet->description)) . '</p>';	
+	}
+	
+	echo JHtml::_('bootstrap.addSlide', 'collapseTypes', ucfirst($name), 'collapse' . ($i++)); ?>
 
-$label = !empty($fieldSet->label) ? $fieldSet->label : 'COM_MODULES_' . $name . '_FIELDSET_LABEL';
-$class = isset($fieldSet->class) && !empty($fieldSet->class) ? $fieldSet->class : '';
-
-
-if (isset($fieldSet->description) && trim($fieldSet->description)) :
-echo '<p class="tip">' . $this->escape(JText::_($fieldSet->description)) . '</p>';
-endif;
-?>
-<?php echo JHtml::_('bootstrap.addSlide', 'collapseTypes', ucfirst($name), 'collapse' . ($i++)); ?>
-
-<ul class="nav nav-tabs nav-stacked">
-<?php foreach ($this->form->getFieldset($name) as $field) : ?>
-
-	<li>
-		<div class="control-group">
-			<div class="control-label">
-				<?php echo $field->label; ?>
+	<ul class="nav nav-tabs nav-stacked">
+	<?php foreach ($this->form->getFieldset($name) as $field) : ?>
+		<li>
+			<div class="control-group">
+				<div class="control-label">
+					<?php echo $field->label; ?>
+				</div>
+				<div class="controls">
+					<?php // If multi-language site, make menu-type selection read-only ?>
+					<?php if (JLanguageMultilang::isEnabled() && $this->item['module'] == 'mod_menu' && $field->getAttribute('name') == 'menutype') : ?>
+						<?php $field->__set('readonly', true); ?>
+					<?php endif; ?>
+					<?php echo $field->input; ?>
+				</div>
 			</div>
-			<div class="controls">
-				<?php
-				// If multi-language site, make menu-type selection read-only
-				if (JLanguageMultilang::isEnabled() && $this->item['module'] == 'mod_menu' && $field->getAttribute('name') == 'menutype')
-				{
-					$field->__set('readonly', true);
-				}
-				echo $field->input;
-				?>
-			</div>
-		</div>
-	</li>
-
-<?php endforeach; ?>
-</ul>
-
-<?php echo JHtml::_('bootstrap.endSlide'); ?>
+		</li>
+	<?php endforeach; ?>
+	</ul>
+	<?php echo JHtml::_('bootstrap.endSlide'); ?>
 <?php endforeach; ?>
 <?php echo JHtml::_('bootstrap.endAccordion'); ?>
