aae38d0cb38d11883c782f879138d1636f676f94
zero-24 <zero-24@users.noreply.github.com>
2014-12-16 20:58:53 +0100

    Update cms.php

--- components/com_config/model/cms.php
+++ components/com_config/model/cms.php
@@ -214,12 +214,12 @@ abstract class ConfigModelCms extends JModelDatabase
 	 */
 	protected function cleanCache($group = null, $client_id = 0)
 	{
-		$conf = JFactory::getConfig();
+		$conf       = JFactory::getConfig();
 		$dispatcher = JEventDispatcher::getInstance();
 
 		$options = array(
 			'defaultgroup' => ($group) ? $group : (isset($this->option) ? $this->option : JFactory::getApplication()->input->get('option')),
-			'cachebase' => ($client_id) ? JPATH_ADMINISTRATOR . '/cache' : $conf->get('cache_path', JPATH_SITE . '/cache'));
+			'cachebase'    => ($client_id) ? JPATH_ADMINISTRATOR . '/cache' : $conf->get('cache_path', JPATH_SITE . '/cache'));
 
 		$cache = JCache::getInstance('callback', $options);
 		$cache->clean();
