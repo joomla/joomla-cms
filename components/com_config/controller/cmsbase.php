811bc2f85646551245dc399d81287151f9fac2a3
zero-24 <zero-24@users.noreply.github.com>
2014-12-16 21:46:46 +0100

    Update cmsbase.php

--- components/com_config/controller/cmsbase.php
+++ components/com_config/controller/cmsbase.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * @package     Joomla.Administrator
- * @subpackage  Joomla.Libraries
+ * @package     Joomla.Site
+ * @subpackage  com_config
  *
  * @copyright   Copyright (C) 2005 - 2014 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
