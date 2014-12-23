b40af83a1df646966c4e7be1cdaf653222324ff8
George Wilson <georgejameswilson@googlemail.com>
2014-12-23 00:57:24 +0000

    Fix undefined variable

--- libraries/joomla/form/fields/password.php
+++ libraries/joomla/form/fields/password.php
@@ -95,7 +95,7 @@ class JFormFieldPassword extends JFormField
 				break;
 
 			case 'meter':
-				$this->$meter = ($value === 'true' || $value === $name || $value === '1');
+				$this->meter = ($value === 'true' || $value === $name || $value === '1');
 				break;
 
 			default:
