0d7a8ce479d05c3d2f4a3332615002c8582eb22c
dgt41 <d.grammatiko@gmail.com>
2015-01-03 23:47:48 +0200

    Modal is not used in this view

--- administrator/components/com_users/views/users/tmpl/default.php
+++ administrator/components/com_users/views/users/tmpl/default.php
@@ -12,7 +12,6 @@ defined('_JEXEC') or die;
 JHtml::_('bootstrap.tooltip');
 JHtml::_('behavior.multiselect');
 JHtml::_('formbehavior.chosen', 'select');
-JHtml::_('behavior.modal', 'a.modal');
 
 $listOrder  = $this->escape($this->state->get('list.ordering'));
 $listDirn   = $this->escape($this->state->get('list.direction'));
