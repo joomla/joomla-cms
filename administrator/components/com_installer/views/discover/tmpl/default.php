019c2a90e3bf295b0e9e3562e442f9511a08bf0a
zero-24 <zero-24@users.noreply.github.com>
2015-01-11 02:42:47 +0100

    Update default.php

--- administrator/components/com_installer/views/discover/tmpl/default.php
+++ administrator/components/com_installer/views/discover/tmpl/default.php
@@ -90,7 +90,9 @@ $listDirn  = $this->escape($this->state->get('list.direction'));
 			<tbody>
 			<?php foreach ($this->items as $i => $item) : ?>
 				<tr class="row<?php echo $i % 2;?>">
-					<td><?php echo JHtml::_('grid.id', $i, $item->extension_id); ?></td>
+					<td class="center">
+						<?php echo JHtml::_('grid.id', $i, $item->extension_id); ?>
+					</td>
 					<td>
 						<label for="cb<?php echo $i;?>">
 							<span class="bold hasTooltip" title="<?php echo JHtml::tooltipText($item->name, $item->description, 0); ?>"><?php echo $item->name; ?></span>
