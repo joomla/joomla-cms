5128b076b40b8d73604ddfd712b1d333dc9a1b83
Brian Teeman <brian@teeman.net>
2015-01-18 12:46:54 +0000

    No image selected on delete

--- administrator/components/com_media/controllers/folder.php
+++ administrator/components/com_media/controllers/folder.php
@@ -50,6 +50,7 @@ class MediaControllerFolder extends JControllerLegacy
 		// Just return if there's nothing to do
 		if (empty($paths))
 		{
+			$this->setMessage(JText::_('COM_MEDIA_NO_IMAGE_SELECTED'), 'warning');
 			return true;
 		}
 
