839d2b0c9958169ad3eedc2bdb3a7f532e1176f8
zero-24 <zero-24@users.noreply.github.com>
2015-01-24 12:49:41 +0100

    Update default.php

--- administrator/templates/hathor/html/com_joomlaupdate/default/default.php
+++ administrator/templates/hathor/html/com_joomlaupdate/default/default.php
@@ -10,6 +10,40 @@
 defined('_JEXEC') or die;
 
 $ftpFieldsDisplay = $this->ftp['enabled'] ? '' : 'style = "display: none"';
+$params           = JComponentHelper::getParams('com_joomlaupdate');
+
+switch ($params->get('updatesource', 'default'))
+{
+	// "Minor & Patch Release for Current version AND Next Major Release".
+	case 'sts':
+	case 'next':
+		$langKey          = 'COM_JOOMLAUPDATE_VIEW_DEFAULT_UPDATES_INFO_NEXT';
+		$updateSourceKey  = JText::_('COM_JOOMLAUPDATE_CONFIG_UPDATESOURCE_NEXT');
+		break;
+
+	// "Testing"
+	case 'testing':
+		$langKey          = 'COM_JOOMLAUPDATE_VIEW_DEFAULT_UPDATES_INFO_TESTING';
+		$updateSourceKey  = JText::_('COM_JOOMLAUPDATE_CONFIG_UPDATESOURCE_TESTING');
+		break;
+
+	// "Custom"
+	case 'custom':
+		$langKey          = 'COM_JOOMLAUPDATE_VIEW_DEFAULT_UPDATES_INFO_CUSTOM';
+		$updateSourceKey  = JText::_('COM_JOOMLAUPDATE_CONFIG_UPDATESOURCE_CUSTOM');
+		break;
+
+	// "Minor & Patch Release for Current version (recommended and default)".
+	// The commented "case" below are for documenting where 'default' and legacy options falls
+	// case 'default':
+	// case 'lts':
+	// case 'nochange':
+	default:
+		$langKey          = 'COM_JOOMLAUPDATE_VIEW_DEFAULT_UPDATES_INFO_DEFAULT';
+		$updateSourceKey  = JText::_('COM_JOOMLAUPDATE_CONFIG_UPDATESOURCE_DEFAULT');
+}
+
+JHtml::_('formbehavior.chosen', 'select');
 
 ?>
 
@@ -21,6 +55,9 @@ $ftpFieldsDisplay = $this->ftp['enabled'] ? '' : 'style = "display: none"';
 	<legend>
 		<?php echo JText::_('COM_JOOMLAUPDATE_VIEW_DEFAULT_NOUPDATES') ?>
 	</legend>
+	<p>
+		<?php echo JText::sprintf($langKey, $updateSourceKey); ?>
+	</p>
 	<p>
 		<?php echo JText::sprintf('COM_JOOMLAUPDATE_VIEW_DEFAULT_NOUPDATESNOTICE', JVERSION); ?>
 	</p>
@@ -63,13 +100,23 @@ $ftpFieldsDisplay = $this->ftp['enabled'] ? '' : 'style = "display: none"';
 			</tr>
 			<tr class="row1">
 				<td>
+					<?php echo JText::_('COM_JOOMLAUPDATE_VIEW_DEFAULT_INFOURL'); ?>
+				</td>
+				<td>
+					<a href="<?php echo $this->updateInfo['object']->get('infourl')->_data; ?>">
+						<?php echo $this->updateInfo['object']->get('infourl')->title; ?>
+					</a>
+				</td>
+			</tr>
+			<tr class="row0">
+				<td>
 					<?php echo JText::_('COM_JOOMLAUPDATE_VIEW_DEFAULT_METHOD') ?>
 				</td>
 				<td>
 					<?php echo $this->methodSelect ?>
 				</td>
 			</tr>
-			<tr class="row0" id="row_ftp_hostname" <?php echo $ftpFieldsDisplay ?>>
+			<tr class="row1" id="row_ftp_hostname" <?php echo $ftpFieldsDisplay ?>>
 				<td>
 					<?php echo JText::_('COM_JOOMLAUPDATE_VIEW_DEFAULT_FTP_HOSTNAME') ?>
 				</td>
@@ -77,7 +124,7 @@ $ftpFieldsDisplay = $this->ftp['enabled'] ? '' : 'style = "display: none"';
 					<input type="text" name="ftp_host" value="<?php echo $this->ftp['host'] ?>" />
 				</td>
 			</tr>
-			<tr class="row1" id="row_ftp_port" <?php echo $ftpFieldsDisplay ?>>
+			<tr class="row0" id="row_ftp_port" <?php echo $ftpFieldsDisplay ?>>
 				<td>
 					<?php echo JText::_('COM_JOOMLAUPDATE_VIEW_DEFAULT_FTP_PORT') ?>
 				</td>
@@ -85,7 +132,7 @@ $ftpFieldsDisplay = $this->ftp['enabled'] ? '' : 'style = "display: none"';
 					<input type="text" name="ftp_port" value="<?php echo $this->ftp['port'] ?>" />
 				</td>
 			</tr>
-			<tr class="row0" id="row_ftp_username" <?php echo $ftpFieldsDisplay ?>>
+			<tr class="row1" id="row_ftp_username" <?php echo $ftpFieldsDisplay ?>>
 				<td>
 					<?php echo JText::_('COM_JOOMLAUPDATE_VIEW_DEFAULT_FTP_USERNAME') ?>
 				</td>
@@ -93,15 +140,15 @@ $ftpFieldsDisplay = $this->ftp['enabled'] ? '' : 'style = "display: none"';
 					<input type="text" name="ftp_user" value="<?php echo $this->ftp['username'] ?>" />
 				</td>
 			</tr>
-			<tr class="row1" id="row_ftp_password" <?php echo $ftpFieldsDisplay ?>>
+			<tr class="row0" id="row_ftp_password" <?php echo $ftpFieldsDisplay ?>>
 				<td>
 					<?php echo JText::_('COM_JOOMLAUPDATE_VIEW_DEFAULT_FTP_PASSWORD') ?>
 				</td>
 				<td>
-					<input type="text" name="ftp_pass" value="<?php echo $this->ftp['password'] ?>" />
+					<input type="password" name="ftp_pass" value="<?php echo $this->ftp['password'] ?>" />
 				</td>
 			</tr>
-			<tr class="row0" id="row_ftp_directory" <?php echo $ftpFieldsDisplay ?>>
+			<tr class="row1" id="row_ftp_directory" <?php echo $ftpFieldsDisplay ?>>
 				<td>
 					<?php echo JText::_('COM_JOOMLAUPDATE_VIEW_DEFAULT_FTP_DIRECTORY') ?>
 				</td>
