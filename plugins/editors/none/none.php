332cb4015d3774320a963612fa8095b6722e3d26
dgt41 <d.grammatiko@gmail.com>
2015-02-03 01:00:03 +0200

    Move part of the script to file, for none editor

--- plugins/editors/none/none.php
+++ plugins/editors/none/none.php
@@ -26,28 +26,7 @@ class PlgEditorNone extends JPlugin
 	 */
 	public function onInit()
 	{
-		JFactory::getDocument()->addScriptDeclaration("
-			function insertAtCursor(myField, myValue)
-			{
-				if (document.selection)
-				{
-					// IE support
-					myField.focus();
-					sel = document.selection.createRange();
-					sel.text = myValue;
-				} else if (myField.selectionStart || myField.selectionStart == '0')
-				{
-					// MOZILLA/NETSCAPE support
-					var startPos = myField.selectionStart;
-					var endPos = myField.selectionEnd;
-					myField.value = myField.value.substring(0, startPos)
-						+ myValue
-						+ myField.value.substring(endPos, myField.value.length);
-				} else {
-					myField.value += myValue;
-				}
-			}
-		");
+		JHtml::script('editors/none/none.min.js', false, true, false, false, true);
 
 		return;
 	}
