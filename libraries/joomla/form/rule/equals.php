3995bb034f52bc80ac30d5704c1738f624fe8415
CI <developpeur@code-internet.fr>
2015-02-05 11:32:56 +0100

    Fixing indentation/code style, space

--- libraries/joomla/form/rule/equals.php
+++ libraries/joomla/form/rule/equals.php
@@ -57,9 +57,9 @@ class JFormRuleEquals extends JFormRule
 			throw new InvalidArgumentException(sprintf('The value for $input must not be null in %s', get_class($this)));
 		}
 
-		if(isset($group))
+		if (isset($group))
 		{
-			$test = $input->get($group.'.'.$field);
+			$test = $input->get($group . '.' . $field);
 		}
 		else
 		{
