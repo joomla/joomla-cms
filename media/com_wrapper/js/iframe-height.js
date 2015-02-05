954f45e41c17977765c313d24a4ef20ebfc3941b
jowi <sjw@gmx.ch>
2015-02-05 11:42:36 +0100

    merged changes from pull 5117

--- media/com_wrapper/js/iframe-height.js
+++ media/com_wrapper/js/iframe-height.js
@@ -1,13 +1,13 @@
 function iFrameHeight()
-        {
-        var h = 0;
-        if (!document.all)
-        {
-                h = document.getElementById('blockrandom').contentDocument.height;
-                document.getElementById('blockrandom').style.height = h + 60 + 'px';
-        } else if (document.all)
-        {
-                h = document.frames('blockrandom').document.body.scrollHeight;
-                document.all.blockrandom.style.height = h + 20 + 'px';
-        }
+{
+	var h = 0;
+	if (!document.all)
+	{
+		h = document.getElementById('blockrandom').height;
+		document.getElementById('blockrandom').style.height = parseInt(h) + 60 + 'px';
+	} else if (document.all)
+	{
+		h = document.frames('blockrandom').document.body.scrollHeight;
+		document.all.blockrandom.style.height = parseInt(h) + 20 + 'px';
+	}
 }
