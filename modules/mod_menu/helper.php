daeae7b9d145f5f8812c28aa60e20b6594dadc54
Tuan Pham Ngoc <tuanpn@joomdonation.com>
2015-02-06 18:56:06 +0700

    Update helper.php

--- modules/mod_menu/helper.php
+++ modules/mod_menu/helper.php
@@ -97,8 +97,16 @@ class ModMenuHelper
 							break;
 
 						case 'alias':
-							// If this is an alias use the item id stored in the parameters to make the link.
-							$item->flink = 'index.php?Itemid=' . $item->params->get('aliasoptions');
+							$aliasItem = $menu->getItem($item->params->get('aliasoptions'));
+							if ($aliasItem->type == 'url')
+							{
+								$item->flink = $aliasItem->link;
+							}
+							else
+							{
+								// If this is an alias use the item id stored in the parameters to make the link.
+								$item->flink = 'index.php?Itemid=' . $item->params->get('aliasoptions');
+							}
 							break;
 
 						default:
