6a4996994e1410351ea1b4d360b65a2ebb486626
Radek Suski <suski.radek@gmail.com>
2015-02-08 19:41:51 +0100

    Fixes for travis to pull request #6021

--- administrator/components/com_admin/controller.php
+++ administrator/components/com_admin/controller.php
@@ -25,33 +25,32 @@ class AdminController extends JControllerLegacy
 		/** @var AdminModelSysInfo $model */
 		$model = $this->getModel('sysinfo');
 		$settingsToLoad = array('PhpSettings', 'Config', 'Info');
-		foreach($settingsToLoad as $section) {
+		foreach ($settingsToLoad as $section) {
 			$settings[$section] = $this->loadSetting($section, $model);
 		}
 		$directories = $this->loadSetting('Directory', $model);
-		if(count($directories)) {
-			foreach($directories as $directory => $data) {
-				$settings['Directory'][$directory] = $directory.($data['writable'] ? ' is writable' : 'is not writable');
+		if (count($directories)) {
+			foreach ($directories as $directory => $data) {
+				$settings['Directory'][$directory] = $data['writable'] ? ' is writable' : 'is not writable';
 			}
 		}
 		$phpInfo = $this->parsePhpInfo($model->getPHPInfo());
 		// remove sensitive information from the phpinfo output
 		$remove = array('HTTP_HOST','Server Administrator', 'Server Root', 'HTTP_ORIGIN', 'HTTP_REFERER', 'HTTP_COOKIE', 'SERVER_NAME', 'SERVER_ADDR', 'REMOTE_ADDR', 'DOCUMENT_ROOT', 'CONTEXT_DOCUMENT_ROOT', 'SERVER_ADMIN', 'SCRIPT_FILENAME', 'HTTP Request', 'Host', 'Referer');
-		foreach($phpInfo as $section => $values) {
-			foreach ( $values as $name => $setting ) {
-				if(in_array($name, $remove)) {
+		foreach ($phpInfo as $section => $values) {
+			foreach ($values as $name => $setting) {
+				if (in_array($name, $remove)) {
 					$setting = strlen($name) ? 'set' : 'not set';
 				}
 				$settings['PhpInfo'][$section][$name] = $setting;
 			}
 		}
 		header('Content-Description: File Transfer');
-		header('Content-Disposition: attachment; filename="systeminfo-' . microtime( true) . '.txt"');
+		header('Content-Disposition: attachment; filename="systeminfo-' . microtime(true) . '.txt"');
 		header('Cache-Control: must-revalidate');
 		$this->renderFile($settings);
 		JFactory::getApplication()->close();
 	}
-
 
 	/**
 	 * Parses array to generate plain text output
@@ -60,30 +59,30 @@ class AdminController extends JControllerLegacy
 	 */
 	protected function renderFile( $settings, $level = -1 )
 	{
-		if(count($settings)) {
+		if (count($settings)) {
 			$margin = null;
-			foreach($settings as $name => $value) {
-				if( $level > 0 ) {
+			foreach ($settings as $name => $value) {
+				if ($level > 0) {
 					$margin = str_repeat("\t", $level);
 				}
-				if( is_array($value)) {
-					if($name=='Directive') {
+				if (is_array($value)) {
+					if($name == 'Directive') {
 						continue;
 					}
 					echo "\n";
-					echo $margin."=============\n";
-					echo $margin.$name."\n";
-					echo $margin."=============\n";
-					$this->renderFile($value,$level+1);
+					echo $margin . "=============\n";
+					echo $margin . $name."\n";
+					echo $margin . "=============\n";
+					$this->renderFile($value, $level+1);
 				}
 				else {
-					if(is_bool($value)) {
+					if (is_bool($value)) {
 						$value = $value ? 'true' : 'false';
 					}
 					if(is_int($name) && ($name == 0 || $name == 1)) {
 						$name = ($name==0 ? 'Local Value' : 'Master Value');
 					}
-					echo $margin.$name.': '.$value."\n";
+					echo $margin . $name . ': ' . $value . "\n";
 				}
 			}
 		}
@@ -96,26 +95,27 @@ class AdminController extends JControllerLegacy
 	protected function parsePhpInfo($string)
 	{
 		$string = strip_tags($string, '<h2><th><td>');
-	    $string = preg_replace('/<th[^>]*>([^<]+)<\/th>/', '<info>\1</info>', $string);
-	    $string = preg_replace('/<td[^>]*>([^<]+)<\/td>/', '<info>\1</info>', $string);
-	    $t = preg_split('/(<h2[^>]*>[^<]+<\/h2>)/', $string, -1, PREG_SPLIT_DELIM_CAPTURE);
-	    $r = array(); $count = count($t);
-	    $p1 = '<info>([^<]+)<\/info>';
-	    $p2 = '/'.$p1.'\s*'.$p1.'\s*'.$p1.'/';
-	    $p3 = '/'.$p1.'\s*'.$p1.'/';
-	    for ($i = 1; $i < $count; $i++) {
-	        if (preg_match('/<h2[^>]*>([^<]+)<\/h2>/', $t[$i], $matchs)) {
-	            $name = trim($matchs[1]);
-	            $vals = explode("\n", $t[$i + 1]);
-	            foreach ($vals AS $val) {
-	                if (preg_match($p2, $val, $matchs)) { // 3cols
-	                    $r[$name][trim($matchs[1])] = array(trim($matchs[2]), trim($matchs[3]));
-	                } elseif (preg_match($p3, $val, $matchs)) { // 2cols
-	                    $r[$name][trim($matchs[1])] = trim($matchs[2]);
+		$string = preg_replace('/<th[^>]*>([^<]+)<\/th>/', '<info>\1</info>', $string);
+		$string = preg_replace('/<td[^>]*>([^<]+)<\/td>/', '<info>\1</info>', $string);
+		$t = preg_split('/(<h2[^>]*>[^<]+<\/h2>)/', $string, -1, PREG_SPLIT_DELIM_CAPTURE);
+		$r = array(); $count = count($t);
+		$p1 = '<info>([^<]+)<\/info>';
+		$p2 = '/' . $p1 . '\s*' . $p1 . '\s*' . $p1 . '/';
+		$p3 = '/' . $p1 . '\s*' . $p1 . '/';
+		for ($i = 1; $i < $count; $i++) {
+			if (preg_match('/<h2[^>]*>([^<]+)<\/h2>/', $t[$i], $matches)) {
+				$name = trim($matches[1]);
+				$vals = explode("\n", $t[$i + 1]);
+				foreach ($vals AS $val) {
+					if (preg_match($p2, $val, $matches)) { // 3cols
+						$r[$name][trim($matches[1])] = array(trim($matches[2]), trim($matches[3]));
 	                }
-	            }
-	        }
-	    }
+					elseif (preg_match($p3, $val, $matches)) { // 2cols
+						$r[$name][trim($matches[1])] = trim($matches[2]);
+					}
+				}
+			}
+		}
 		return $r;
 	}
 
@@ -129,9 +129,9 @@ class AdminController extends JControllerLegacy
 		$settings = array();
 		$method = 'get'.$section;
 		$sectionsValues = $model->$method();
-		if(count($sectionsValues)) {
-			foreach($sectionsValues as $setting => $value) {
-				if(in_array($setting, $remove)) {
+		if (count($sectionsValues)) {
+			foreach ($sectionsValues as $setting => $value) {
+				if (in_array($setting, $remove)) {
 					$value = strlen($value) ? 'set' : 'not set';
 				}
 				$settings[$setting] = $value;
--- administrator/components/com_admin/views/sysinfo/view.html.php
+++ administrator/components/com_admin/views/sysinfo/view.html.php
@@ -3,8 +3,8 @@
  * @package     Joomla.Administrator
  * @subpackage  com_admin
  *
- * @copyright	Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
- * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 defined('_JEXEC') or die;
