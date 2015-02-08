560794a988ea30c8fca0fa5098d887dd905a7d32
Radek Suski <suski.radek@gmail.com>
2015-02-08 20:04:34 +0100

    Fixes for travis to pull request #6021. I seriously hate this syntax :P

--- administrator/components/com_admin/controller.php
+++ administrator/components/com_admin/controller.php
@@ -25,20 +25,25 @@ class AdminController extends JControllerLegacy
 		/** @var AdminModelSysInfo $model */
 		$model = $this->getModel('sysinfo');
 		$settingsToLoad = array('PhpSettings', 'Config', 'Info');
-		foreach ($settingsToLoad as $section) {
+		foreach ($settingsToLoad as $section)
+		{
 			$settings[$section] = $this->loadSetting($section, $model);
 		}
 		$directories = $this->loadSetting('Directory', $model);
-		if (count($directories)) {
-			foreach ($directories as $directory => $data) {
+		if (count($directories))
+		{
+			foreach ($directories as $directory => $data)
+			{
 				$settings['Directory'][$directory] = $data['writable'] ? ' is writable' : 'is not writable';
 			}
 		}
 		$phpInfo = $this->parsePhpInfo($model->getPHPInfo());
 		// remove sensitive information from the phpinfo output
 		$remove = array('HTTP_HOST','Server Administrator', 'Server Root', 'HTTP_ORIGIN', 'HTTP_REFERER', 'HTTP_COOKIE', 'SERVER_NAME', 'SERVER_ADDR', 'REMOTE_ADDR', 'DOCUMENT_ROOT', 'CONTEXT_DOCUMENT_ROOT', 'SERVER_ADMIN', 'SCRIPT_FILENAME', 'HTTP Request', 'Host', 'Referer');
-		foreach ($phpInfo as $section => $values) {
-			foreach ($values as $name => $setting) {
+		foreach ($phpInfo as $section => $values)
+		{
+			foreach ($values as $name => $setting)
+			{
 				if (in_array($name, $remove)) {
 					$setting = strlen($name) ? 'set' : 'not set';
 				}
@@ -61,11 +66,13 @@ class AdminController extends JControllerLegacy
 	{
 		if (count($settings)) {
 			$margin = null;
-			foreach ($settings as $name => $value) {
+			foreach ($settings as $name => $value)
+			{
 				if ($level > 0) {
 					$margin = str_repeat("\t", $level);
 				}
-				if (is_array($value)) {
+				if (is_array($value))
+				{
 					if($name == 'Directive') {
 						continue;
 					}
@@ -75,12 +82,13 @@ class AdminController extends JControllerLegacy
 					echo $margin . "=============\n";
 					$this->renderFile($value, $level+1);
 				}
-				else {
+				else
+				{
 					if (is_bool($value)) {
 						$value = $value ? 'true' : 'false';
 					}
 					if(is_int($name) && ($name == 0 || $name == 1)) {
-						$name = ($name==0 ? 'Local Value' : 'Master Value');
+						$name = ($name == 0 ? 'Local Value' : 'Master Value');
 					}
 					echo $margin . $name . ': ' . $value . "\n";
 				}
@@ -98,19 +106,25 @@ class AdminController extends JControllerLegacy
 		$string = preg_replace('/<th[^>]*>([^<]+)<\/th>/', '<info>\1</info>', $string);
 		$string = preg_replace('/<td[^>]*>([^<]+)<\/td>/', '<info>\1</info>', $string);
 		$t = preg_split('/(<h2[^>]*>[^<]+<\/h2>)/', $string, -1, PREG_SPLIT_DELIM_CAPTURE);
-		$r = array(); $count = count($t);
+		$r = array();
+		$count = count($t);
 		$p1 = '<info>([^<]+)<\/info>';
 		$p2 = '/' . $p1 . '\s*' . $p1 . '\s*' . $p1 . '/';
 		$p3 = '/' . $p1 . '\s*' . $p1 . '/';
-		for ($i = 1; $i < $count; $i++) {
-			if (preg_match('/<h2[^>]*>([^<]+)<\/h2>/', $t[$i], $matches)) {
+		for ($i = 1; $i < $count; $i++)
+		{
+			if (preg_match('/<h2[^>]*>([^<]+)<\/h2>/', $t[$i], $matches))
+			{
 				$name = trim($matches[1]);
 				$vals = explode("\n", $t[$i + 1]);
-				foreach ($vals AS $val) {
-					if (preg_match($p2, $val, $matches)) { // 3cols
+				foreach ($vals AS $val)
+				{
+					if (preg_match($p2, $val, $matches))
+					{ // 3cols
 						$r[$name][trim($matches[1])] = array(trim($matches[2]), trim($matches[3]));
-	                }
-					elseif (preg_match($p3, $val, $matches)) { // 2cols
+					}
+					elseif (preg_match($p3, $val, $matches))
+					{ // 2cols
 						$r[$name][trim($matches[1])] = trim($matches[2]);
 					}
 				}
@@ -127,11 +141,13 @@ class AdminController extends JControllerLegacy
 		/** security sensitive information to be removed from the output */
 		static $remove = array('dbprefix', 'open_basedir', 'session.save_path', 'mailfrom', 'fromname', 'smtphost', 'log_path', 'tmp_path', 'proxy_host', 'proxy_user', 'proxy_pass', 'memcache_server_host', 'memcached_server_host', 'session_memcache_server_host', 'session_memcached_server_host');
 		$settings = array();
-		$method = 'get'.$section;
+		$method = 'get' . $section;
 		$sectionsValues = $model->$method();
 		if (count($sectionsValues)) {
-			foreach ($sectionsValues as $setting => $value) {
-				if (in_array($setting, $remove)) {
+			foreach ($sectionsValues as $setting => $value)
+			{
+				if (in_array($setting, $remove))
+				{
 					$value = strlen($value) ? 'set' : 'not set';
 				}
 				$settings[$setting] = $value;
