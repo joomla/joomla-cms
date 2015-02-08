7ff04f063f1d38b6dd1d049e403586a30da9866a
zero-24 <zero-24@users.noreply.github.com>
2015-02-08 18:07:19 +0100

    Load language file from module in ajax interface #3651
    This PR Load the language file from module in ajax interface; Coding is by @piotrmocko at #3651

--- components/com_ajax/ajax.php
+++ components/com_ajax/ajax.php
@@ -29,7 +29,7 @@ $format = strtolower($input->getWord('format'));
 
 // Initialize default response and module name
 $results = null;
-$parts = null;
+$parts   = null;
 
 // Check for valid format
 if (!$format)
@@ -69,6 +69,7 @@ elseif ($input->get('module'))
 		if ($parts)
 		{
 			$class = 'mod';
+
 			foreach ($parts as $part)
 			{
 				$class .= ucfirst($part);
@@ -88,6 +89,10 @@ elseif ($input->get('module'))
 
 			if (method_exists($class, $method . 'Ajax'))
 			{
+				// Load language file for module
+				$lang = JFactory::getLanguage();
+				$lang->load('mod_' . $module);
+
 				try
 				{
 					$results = call_user_func($class . '::' . $method . 'Ajax');
@@ -145,18 +150,20 @@ elseif ($input->get('plugin'))
 switch ($format)
 {
 	// JSONinzed
-	case 'json':
+	case 'json' :
 		echo new JResponseJson($results, null, false, $input->get('ignoreMessages', true, 'bool'));
+
 		break;
 
 	// Human-readable format
-	case 'debug':
+	case 'debug' :
 		echo '<pre>' . print_r($results, true) . '</pre>';
 		$app->close();
+
 		break;
 
 	// Handle as raw format
-	default:
+	default :
 		// Output exception
 		if ($results instanceof Exception)
 		{
