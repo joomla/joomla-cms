2fce0b894955de8c98d8e5a1545281b05804eb1e
javier gomez <javier.gomez.corio@gmail.com>
2015-02-12 16:19:58 +0100

    Fix Copyrights in tests and pages

--- tests/system/webdriver/tests/report/index.php
+++ tests/system/webdriver/tests/report/index.php
@@ -2,7 +2,7 @@
 /**
  * @package     RedShop
  * @subpackage  Report
- * @copyright   Copyright (C) 2012 - 2014 redCOMPONENT.com. All rights reserved.
+ * @copyright   Copyright (C) 2012 - 2015 redCOMPONENT.com. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
