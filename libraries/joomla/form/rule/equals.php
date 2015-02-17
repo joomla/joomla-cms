a4f0cf1d00a172d593331f26c94c3d4482bcb4a4
CI <developpeur@code-internet.fr>
2015-02-17 16:31:23 +0100

    Fixing invalidate when group is an empty string
    var group can be an empty string eg: $group = ''; cause of strval
    previously used (JForm)

--- libraries/joomla/form/rule/equals.php
+++ libraries/joomla/form/rule/equals.php
@@ -57,7 +57,7 @@ class JFormRuleEquals extends JFormRule
 			throw new InvalidArgumentException(sprintf('The value for $input must not be null in %s', get_class($this)));
 		}
 
-		if (isset($group))
+		if (isset($group) && $group != '')
 		{
 			$test = $input->get($group . '.' . $field);
 		}
