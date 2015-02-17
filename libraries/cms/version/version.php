3a7446dcb101529a8fa87012274149c978874ecb
George Wilson <georgejameswilson@googlemail.com>
2015-02-17 18:12:42 +0000

    Bump version to RC2-dev

--- libraries/cms/version/version.php
+++ libraries/cms/version/version.php
@@ -23,7 +23,7 @@ final class JVersion
 	public $RELEASE = '3.4';
 
 	/** @var  string  Maintenance version. */
-	public $DEV_LEVEL = '0-rc';
+	public $DEV_LEVEL = '0-rc2-dev';
 
 	/** @var  string  Development STATUS. */
 	public $DEV_STATUS = 'Beta';
