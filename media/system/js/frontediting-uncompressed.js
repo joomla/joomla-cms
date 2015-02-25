1df8c7015eff54f5051a710e603cd50958b18ebb
Oli <info@ws-theme.com>
2015-02-25 15:56:52 +0100

    Update for Tooltips
    Must be called again that a HTML Tooltip is possible! Some people disable HTML in Tooltips for not registered Users cause of XSS Attacks!
    So for registered Users I think it's ok to use HTML in Tooltips, but for all other I always disable HTML which breaks then the Layout ...
    
    Check this in the Tracker: http://issues.joomla.org/tracker/joomla-cms/6177
    
    Bootstrap itself says:
    """If false, jQuery's text method will be used to insert content into the DOM. Use text if you're worried about XSS attacks."""

--- media/system/js/frontediting-uncompressed.js
+++ media/system/js/frontediting-uncompressed.js
@@ -118,7 +118,7 @@
 				$(this).addClass('jmodinside')
 					.prepend('<a class="btn jmodedit" href="#" ><span class="icon-edit"></span></a>')
 					.children(":first").attr('href', moduleEditUrl).attr('title', moduleTip)
-					.tooltip({"container": false, placement: tooltipPlacer})
+					.tooltip({"container": false, html: true, placement: tooltipPlacer})
 					.jEditMakeAbsolute(true);
 				// This class was needed for positioning the icon before making it absolute at bottom of body: We can now remove it:
 				$(this).removeClass('jmodinside');
