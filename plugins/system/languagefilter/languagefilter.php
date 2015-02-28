8252e11ddb9b259f33c47813bf45b77c5df4be8f
Hannes Papenberg <info@joomlager.de>
2015-02-28 15:58:01 +0100

    Codestyle

--- plugins/system/languagefilter/languagefilter.php
+++ plugins/system/languagefilter/languagefilter.php
@@ -249,8 +249,10 @@ class PlgSystemLanguageFilter extends JPlugin
 				}
 				else
 				{
-					$lang_code = $this->app->input->cookie->getString(JApplicationHelper::getHash('language'),
-						JComponentHelper::getParams('com_languages')->get('site', 'en-GB'));
+					$lang_code = $this->app->input->cookie->getString(
+						JApplicationHelper::getHash('language'),
+						JComponentHelper::getParams('com_languages')->get('site', 'en-GB')
+					);
 
 					if ($lang_code == JComponentHelper::getParams('com_languages')->get('site', 'en-GB'))
 					{
