8c3809a925c9924bc58a439a6c6a3fe36114bdb6
Dmitry Rekun <bzzjuka@gmail.com>
2015-03-03 07:38:19 +0200

    Fix the page heading for all tags view. Fixes #6274

--- components/com_tags/views/tags/tmpl/default.php
+++ components/com_tags/views/tags/tmpl/default.php
@@ -16,7 +16,7 @@ $description = $this->params->get('all_tags_description');
 $descriptionImage = $this->params->get('all_tags_description_image');
 ?>
 <div class="tag-category<?php echo $this->pageclass_sfx; ?>">
-	<?php if ($this->state->get('show_page_heading')) : ?>
+	<?php if ($this->params->get('show_page_heading')) : ?>
 		<h1>
 			<?php echo $this->escape($this->params->get('page_heading')); ?>
 		</h1>
