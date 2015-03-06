248070a1f1eea67d6e0938e59e6c6ca14c448bae
zero-24 <zero-24@users.noreply.github.com>
2015-03-06 21:23:24 +0100

    Update mod_status.php

--- administrator/modules/mod_status/mod_status.php
+++ administrator/modules/mod_status/mod_status.php
@@ -14,11 +14,12 @@ $user   = JFactory::getUser();
 $db     = JFactory::getDbo();
 $lang   = JFactory::getLanguage();
 $input  = JFactory::getApplication()->input;
+$query	= $db->getQuery(true);
 
 if (JComponentHelper::isInstalled('com_messages'))
 {
 	// Get the number of unread messages in your inbox.
-	$query	= $db->getQuery(true)
+	$query->clear()
 		->select('COUNT(*)')
 		->from('#__messages')
 		->where('state = 0 AND user_id_to = ' . (int) $user->get('id'));
