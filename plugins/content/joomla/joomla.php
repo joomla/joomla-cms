b8c0025bd85b6bc0c80ec18f1fca21866ddf4e0a
zero-24 <zero-24@users.noreply.github.com>
2015-03-07 22:38:35 +0100

    Update joomla.php

--- plugins/content/joomla/joomla.php
+++ plugins/content/joomla/joomla.php
@@ -17,7 +17,7 @@ defined('_JEXEC') or die;
 class PlgContentJoomla extends JPlugin
 {
 	/**
-	 * Example after save content method
+	 * After save content method
 	 * Article is passed by reference, but after the save, so no changes will be saved.
 	 * Method is called right after the content is saved
 	 *
@@ -29,68 +29,78 @@ class PlgContentJoomla extends JPlugin
 	 *                    false depending on success of save function.
 	 *
 	 * @since   1.6
+	 *
+	 * @deprecated  4.0
 	 */
 	public function onContentAfterSave($context, $article, $isNew)
 	{
-		// Check we are handling the frontend edit form.
-		if ($context != 'com_content.form')
+		// This function don't work if com_messages ist not installed
+		if (JComponentHelper::isInstalled('com_messages'))
 		{
-			return true;
-		}
-
-		// Check if this function is enabled.
-		if (!$this->params->def('email_new_fe', 1))
-		{
-			return true;
-		}
-
-		// Check this is a new article.
-		if (!$isNew)
-		{
-			return true;
-		}
-
-		$db = JFactory::getDbo();
-		$query = $db->getQuery(true)
-			->select($db->quoteName('id'))
-			->from($db->quoteName('#__users'))
-			->where($db->quoteName('sendEmail') . ' = 1');
-		$db->setQuery($query);
-		$users = (array) $db->loadColumn();
-
-		if (empty($users))
-		{
-			return true;
-		}
-
-		$user = JFactory::getUser();
-
-		// Messaging for new items
-		JModelLegacy::addIncludePath(JPATH_ADMINISTRATOR . '/components/com_messages/models', 'MessagesModel');
-		JTable::addIncludePath(JPATH_ADMINISTRATOR . '/components/com_messages/tables');
-
-		$default_language = JComponentHelper::getParams('com_languages')->get('administrator');
-		$debug = JFactory::getConfig()->get('debug_lang');
-
-		foreach ($users as $user_id)
-		{
-			if ($user_id != $user->id)
+			// Check we are handling the frontend edit form.
+			if ($context != 'com_content.form')
 			{
-				// Load language for messaging
-				$receiver = JUser::getInstance($user_id);
-				$lang = JLanguage::getInstance($receiver->getParam('admin_language', $default_language), $debug);
-				$lang->load('com_content');
-				$message = array(
-					'user_id_to' => $user_id,
-					'subject' => $lang->_('COM_CONTENT_NEW_ARTICLE'),
-					'message' => sprintf($lang->_('COM_CONTENT_ON_NEW_CONTENT'), $user->get('name'), $article->title)
-				);
-				$model_message = JModelLegacy::getInstance('Message', 'MessagesModel');
-				$result = $model_message->save($message);
+				return true;
 			}
+
+			// Check if this function is enabled.
+			if (!$this->params->def('email_new_fe', 1))
+			{
+				return true;
+			}
+
+			// Check this is a new article.
+			if (!$isNew)
+			{
+				return true;
+			}
+
+			$db = JFactory::getDbo();
+			$query = $db->getQuery(true)
+				->select($db->quoteName('id'))
+				->from($db->quoteName('#__users'))
+				->where($db->quoteName('sendEmail') . ' = 1');
+			$db->setQuery($query);
+			$users = (array) $db->loadColumn();
+
+			if (empty($users))
+			{
+				return true;
+			}
+
+			$user = JFactory::getUser();
+
+			// Messaging for new items
+			JModelLegacy::addIncludePath(JPATH_ADMINISTRATOR . '/components/com_messages/models', 'MessagesModel');
+			JTable::addIncludePath(JPATH_ADMINISTRATOR . '/components/com_messages/tables');
+
+			$default_language = JComponentHelper::getParams('com_languages')->get('administrator');
+			$debug = JFactory::getConfig()->get('debug_lang');
+
+			foreach ($users as $user_id)
+			{
+				if ($user_id != $user->id)
+				{
+					// Load language for messaging
+					$receiver = JUser::getInstance($user_id);
+					$lang     = JLanguage::getInstance($receiver->getParam('admin_language', $default_language), $debug);
+					$lang->load('com_content');
+
+					$message = array(
+						'user_id_to' => $user_id,
+						'subject' => $lang->_('COM_CONTENT_NEW_ARTICLE'),
+						'message' => sprintf($lang->_('COM_CONTENT_ON_NEW_CONTENT'), $user->get('name'), $article->title)
+					);
+
+					$model_message = JModelLegacy::getInstance('Message', 'MessagesModel');
+					$result = $model_message->save($message);
+				}
+			}
+
+			return $result;
 		}
 
-		return $result;
+		return true;
 	}
 
 	/**
