b6f02b21f43b13db5a779fd6870cbd0ebf0eddc4
Nicola Galgano <extensions4joomla@gmail.com>
2015-03-07 06:22:36 +0100

    removed space before comma

--- libraries/joomla/database/driver/sqlsrv.php
+++ libraries/joomla/database/driver/sqlsrv.php
@@ -362,7 +362,7 @@ class JDatabaseDriverSqlsrv extends JDatabaseDriver
 		{
 			foreach ($fields as $field)
 			{
-				if (stristr(strtolower($field->type) , "nvarchar"))
+				if (stristr(strtolower($field->type), "nvarchar"))
 				{
 					$field->Default = " ";
 				}
