1eed15e33b4db225473de9037d3729fcd46dc3d2
Chris de Kok <chris.de.kok@gmail.com>
2015-03-09 13:54:06 +0100

    Change to tabs & add space

--- libraries/joomla/document/error/error.php
+++ libraries/joomla/document/error/error.php
@@ -86,10 +86,10 @@ class JDocumentError extends JDocument
 
 		// Set the status header
 		$status = $this->_error->getCode();
-                if ($status < 400 || $status > 599) {
-                    $status = 500;
-                }
-		JFactory::getApplication()->setHeader('status',  $status. ' ' . str_replace("\n", ' ', $this->_error->getMessage()));
+		if ($status < 400 || $status > 599) {
+			$status = 500;
+		}
+		JFactory::getApplication()->setHeader('status',  $status . ' ' . str_replace("\n", ' ', $this->_error->getMessage()));
 		$file = 'error.php';
 
 		// Check template
