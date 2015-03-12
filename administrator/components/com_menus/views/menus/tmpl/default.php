786198d7f8f5b35ce7425bdb476147e71a8f61f6
dgt41 <d.grammatiko@gmail.com>
2015-03-13 00:18:01 +0200

    init

--- administrator/components/com_menus/views/menus/tmpl/default.php
+++ administrator/components/com_menus/views/menus/tmpl/default.php
@@ -34,6 +34,27 @@ JFactory::getDocument()->addScriptDeclaration("
 			}
 		};
 ");
+
+$script = array();
+$script[] = "jQuery(document).ready(function() {";
+foreach ($this->items as $item) :
+	if ($user->authorise('core.edit', 'com_menus')) :
+		$script[] = '	function jSelectPosition_' . $item->id . '(name) {';
+		$script[] = '		document.getElementById("' . $item->id . '").value = name;';
+		$script[] = '		jQuery("#menusModuleModal").modal("hide");';
+		$script[] = '	};';
+	endif;
+endforeach;
+foreach ($this->modules[$item->menutype] as &$module) :
+	$script[] = '	jQuery("#module' . $module->id . 'Modal").on("hidden", function () {';
+	$script[] = '		setTimeout(function(){';
+	$script[] = '			window.parent.location.reload();';
+	$script[] = '		},1000);';
+	$script[] = '	});';
+endforeach;
+$script[] = "});";
+
+JFactory::getDocument()->addScriptDeclaration(implode("\n", $script));
 ?>
 <form action="<?php echo JRoute::_('index.php?option=com_menus&view=menus');?>" method="post" name="adminForm" id="adminForm">
 <?php if (!empty( $this->sidebar)) : ?>
@@ -142,8 +163,9 @@ JFactory::getDocument()->addScriptDeclaration("
 										<?php foreach ($this->modules[$item->menutype] as &$module) : ?>
 											<li>
 												<?php if ($canEdit) : ?>
-													<a class="small modal" href="<?php echo JRoute::_('index.php?option=com_modules&task=module.edit&id=' . $module->id . '&return=' . $return . '&tmpl=component&layout=modal');?>" rel="{handler: 'iframe', size: {x: 1024, y: 450}, onClose: function() {window.location.reload()}}" title="<?php echo JText::_('COM_MENUS_EDIT_MODULE_SETTINGS');?>">
-													<?php echo JText::sprintf('COM_MENUS_MODULE_ACCESS_POSITION', $this->escape($module->title), $this->escape($module->access_title), $this->escape($module->position)); ?></a>
+													<?php $link = JRoute::_('index.php?option=com_modules&task=module.edit&id=' . $module->id . '&return=' . $return . '&tmpl=component&layout=modal'); ?>
+													<a href="#module<?php echo $module->id; ?>Modal" role="button" class="button" data-toggle="modal" title="<?php echo JText::_('COM_MENUS_EDIT_MODULE_SETTINGS');?>">
+														<?php echo JText::sprintf('COM_MENUS_MODULE_ACCESS_POSITION', $this->escape($module->title), $this->escape($module->access_title), $this->escape($module->position)); ?></a>
 												<?php else :?>
 													<?php echo JText::sprintf('COM_MENUS_MODULE_ACCESS_POSITION', $this->escape($module->title), $this->escape($module->access_title), $this->escape($module->position)); ?>
 												<?php endif; ?>
@@ -151,9 +173,16 @@ JFactory::getDocument()->addScriptDeclaration("
 										<?php endforeach; ?>
 									</ul>
 								 </div>
+								<?php foreach ($this->modules[$item->menutype] as &$module) : ?>
+									<?php if ($canEdit) : ?>
+										<?php $link = JRoute::_('index.php?option=com_modules&task=module.edit&id=' . $module->id . '&return=' . $return . '&tmpl=component&layout=modal'); ?>
+										<?php echo JHtmlBootstrap::renderModal('module' . $module->id . 'Modal', array( 'url' => $link, 'title' => JText::_('COM_MENUS_EDIT_MODULE_SETTINGS'),'height' => '500px', 'width' => '800px'), ''); ?>
+									<?php endif; ?>
+								<?php endforeach; ?>
 							<?php elseif ($modMenuId) : ?>
 							<a href="<?php echo JRoute::_('index.php?option=com_modules&task=module.add&eid=' . $modMenuId . '&params[menutype]=' . $item->menutype); ?>">
 								<?php echo JText::_('COM_MENUS_ADD_MENU_MODULE'); ?></a>
+								<?php echo JHtmlBootstrap::renderModal('moduleModal', array( 'url' => $link, 'title' => JText::_('COM_MENUS_EDIT_MODULE_SETTINGS'),'height' => '500px', 'width' => '800px'), ''); ?>
 							<?php endif; ?>
 						</td>
 						<td class="center">
