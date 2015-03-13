e9e1e9e47d22123927abd7159b09b15947aba22f
zero-24 <zero-24@users.noreply.github.com>
2015-03-13 16:13:40 +0100

    Update administrator.php

--- libraries/cms/application/administrator.php
+++ libraries/cms/application/administrator.php
@@ -332,6 +332,7 @@ class JApplicationAdministrator extends JApplicationCms
 			$lang = preg_replace('/[^A-Z-]/i', '', $lang);
 			$this->setUserState('application.lang', $lang);
 
+			// Needs to be removed with 4.0
 			static::purgeMessages();
 		}
 
@@ -339,50 +340,56 @@ class JApplicationAdministrator extends JApplicationCms
 	}
 
 	/**
-	 * Purge the jos_messages table of old messages
+	 * Purge the #__messages table of old messages
 	 *
-	 * @return  void
+	 * @return     void
 	 *
-	 * @since   3.2
+	 * @since       3.2
+	 *
+	 * @deprecated  4.0  As com_messages is decoupled from core (see also line 336)
 	 */
 	public static function purgeMessages()
 	{
-		$user = JFactory::getUser();
-		$userid = $user->get('id');
-
-		$db = JFactory::getDbo();
-		$query = $db->getQuery(true)
-			->select('*')
-			->from($db->quoteName('#__messages_cfg'))
-			->where($db->quoteName('user_id') . ' = ' . (int) $userid, 'AND')
-			->where($db->quoteName('cfg_name') . ' = ' . $db->quote('auto_purge'), 'AND');
-		$db->setQuery($query);
-		$config = $db->loadObject();
-
-		// Check if auto_purge value set
-		if (is_object($config) and $config->cfg_name == 'auto_purge')
+		if (JComponentHelper::isInstalled('com_messages'))
 		{
-			$purge = $config->cfg_value;
-		}
-		else
-		{
-			// If no value set, default is 7 days
-			$purge = 7;
-		}
+			$user   = JFactory::getUser();
+			$userid = $user->get('id');
 
-		// If purge value is not 0, then allow purging of old messages
-		if ($purge > 0)
-		{
-			// Purge old messages at day set in message configuration
-			$past = JFactory::getDate(time() - $purge * 86400);
-			$pastStamp = $past->toSql();
-
-			$query->clear()
-				->delete($db->quoteName('#__messages'))
-				->where($db->quoteName('date_time') . ' < ' . $db->Quote($pastStamp), 'AND')
-				->where($db->quoteName('user_id_to') . ' = ' . (int) $userid, 'AND');
+			$db    = JFactory::getDbo();
+			$query = $db->getQuery(true)
+				->select('*')
+				->from($db->quoteName('#__messages_cfg'))
+				->where($db->quoteName('user_id') . ' = ' . (int) $userid, 'AND')
+				->where($db->quoteName('cfg_name') . ' = ' . $db->quote('auto_purge'), 'AND');
 			$db->setQuery($query);
-			$db->execute();
+
+			$config = $db->loadObject();
+
+			// Check if auto_purge value set
+			if (is_object($config) and $config->cfg_name == 'auto_purge')
+			{
+				$purge = $config->cfg_value;
+			}
+			else
+			{
+				// If no value set, default is 7 days
+				$purge = 7;
+			}
+
+			// If purge value is not 0, then allow purging of old messages
+			if ($purge > 0)
+			{
+				// Purge old messages at day set in message configuration
+				$past      = JFactory::getDate(time() - $purge * 86400);
+				$pastStamp = $past->toSql();
+
+				$query->clear()
+					->delete($db->quoteName('#__messages'))
+					->where($db->quoteName('date_time') . ' < ' . $db->Quote($pastStamp), 'AND')
+					->where($db->quoteName('user_id_to') . ' = ' . (int) $userid, 'AND');
+				$db->setQuery($query);
+				$db->execute();
+			}
 		}
 	}
 
