26649b3f89c4fb4b014088ae190b9395b96a9715
zero-24 <zero-24@users.noreply.github.com>
2015-03-13 22:25:44 +0100

    Update default.php

--- administrator/modules/mod_status/tmpl/default.php
+++ administrator/modules/mod_status/tmpl/default.php
@@ -42,21 +42,18 @@ if ($params->get('show_loggedin_users_admin', 1))
 		. '</div>';
 }
 
-if (JComponentHelper::isInstalled('com_messages'))
+if (JComponentHelper::isInstalled('com_messages') && $params->get('show_messages', 1))
 {
 	//  Print the inbox message.
-	if ($params->get('show_messages', 1))
-	{
-		$active   = $unread ? ' badge-warning' : '';
-		$output[] = '<div class="btn-group hasTooltip ' . $inboxClass . '"'
-			. ' title="' . JText::plural('MOD_STATUS_MESSAGES', $unread) . '">'
-			. ($hideLinks ? '' : '<a href="' . $inboxLink . '">')
-			. '<i class="icon-envelope"></i> '
-			. '<span class="badge' . $active . '">' . $unread . '</span>'
-			. ($hideLinks ? '' : '</a>')
-			. '<div class="btn-group divider"></div>'
-			. '</div>';
-	}
+	$active   = $unread ? ' badge-warning' : '';
+	$output[] = '<div class="btn-group hasTooltip ' . $inboxClass . '"'
+		. ' title="' . JText::plural('MOD_STATUS_MESSAGES', $unread) . '">'
+		. ($hideLinks ? '' : '<a href="' . $inboxLink . '">')
+		. '<i class="icon-envelope"></i> '
+		. '<span class="badge' . $active . '">' . $unread . '</span>'
+		. ($hideLinks ? '' : '</a>')
+		. '<div class="btn-group divider"></div>'
+		. '</div>';
 }
 
 // Print the logout link.
