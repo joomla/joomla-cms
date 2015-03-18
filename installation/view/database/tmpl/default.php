ccd2758c7d687fe56844a99d475565afda1f2bad
Ufuk Avcu <info@jooag.de>
2015-03-18 09:28:27 +0100

    Better RTL Support

--- installation/view/database/tmpl/default.php
+++ installation/view/database/tmpl/default.php
@@ -102,10 +102,12 @@ defined('_JEXEC') or die;
 	<input type="hidden" name="task" value="database" />
 	<?php echo JHtml::_('form.token'); ?>
 	
-	<div class="btn-toolbar text-right">
-		<div class="btn-group">
-			<a class="btn" href="#" onclick="return Install.goToPage('site');" rel="prev" title="<?php echo JText::_('JPrevious'); ?>"><i class="icon-arrow-left"></i> <?php echo JText::_('JPrevious'); ?></a>
-			<a  class="btn btn-primary" href="#" onclick="Install.submitform();" rel="next" title="<?php echo JText::_('JNext'); ?>"><i class="icon-arrow-right icon-white"></i> <?php echo JText::_('JNext'); ?></a>
+	<div class="row-fluid">
+		<div class="btn-toolbar pull-right">
+			<div class="btn-group">
+				<a class="btn" href="#" onclick="return Install.goToPage('site');" rel="prev" title="<?php echo JText::_('JPrevious'); ?>"><i class="icon-arrow-left"></i> <?php echo JText::_('JPrevious'); ?></a>
+				<a  class="btn btn-primary" href="#" onclick="Install.submitform();" rel="next" title="<?php echo JText::_('JNext'); ?>"><i class="icon-arrow-right icon-white"></i> <?php echo JText::_('JNext'); ?></a>
+			</div>
 		</div>
 	</div>
 </form>
