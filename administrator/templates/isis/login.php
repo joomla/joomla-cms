ae37e02cff3cbf32db0de15a1ba6bf00bde760cb
Niels van der Veer <niels_ik@kpnmail.nl>
2015-03-22 09:54:54 +0100

    CS

--- administrator/templates/isis/login.php
+++ administrator/templates/isis/login.php
@@ -24,7 +24,7 @@ JHtml::_('bootstrap.framework');
 JHtml::_('bootstrap.tooltip');
 
 // Add Stylesheets
- $doc->addStyleSheet($this->baseurl . '/templates/' . $this->template . '/css/template' . ($this->direction == 'rtl' ? '-rtl' : '') . '.css');
+$doc->addStyleSheet($this->baseurl . '/templates/' . $this->template . '/css/template' . ($this->direction == 'rtl' ? '-rtl' : '') . '.css');
 
 // Load optional RTL Bootstrap CSS
 JHtml::_('bootstrap.loadCss', false, $this->direction);
