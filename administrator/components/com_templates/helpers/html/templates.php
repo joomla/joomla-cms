8aab442c038f583fa1d1fb3973c02bbe9e89673c
dgt41 <d.grammatiko@gmail.com>
2015-03-27 15:25:12 +0200

    modal footer

--- administrator/components/com_templates/helpers/html/templates.php
+++ administrator/components/com_templates/helpers/html/templates.php
@@ -76,12 +76,15 @@ class JHtmlTemplates
 			if (file_exists($preview))
 			{
 				$preview = $baseUrl . '/templates/' . $template . '/template_preview.png';
+				$footer = '<button class="btn" data-dismiss="modal" aria-hidden="true">'
+					. JText::_('JTOOLBAR_CLOSE') . '</a>';
 
 				$html .= JHtmlBootstrap::renderModal(
 					$template . '-Modal', array(
 						'title' => JText::_('COM_TEMPLATES_BUTTON_PREVIEW'),
 						'height' => '500px',
-						'width' => '800px'
+						'width' => '800px',
+						'footer' => $footer
 					),
 					$body = '<div><img src="' . $preview . '" style="max-width:100%"></div>'
 				);
