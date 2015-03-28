07ef8a27a6b77bf3511e47ac3b970684cac5bb14
Michael Babker <mbabker@flbab.com>
2015-03-28 15:03:03 -0400

    Update to String package 1.3

--- libraries/vendor/autoload.php
+++ libraries/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInitf9aed076f12471aff0049364a504b3f7::getLoader();
+return ComposerAutoloaderInit0efb534ee20646bcb987f4359c38b3aa::getLoader();
