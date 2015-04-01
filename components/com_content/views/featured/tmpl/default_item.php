6200eebc9672806e799ad2faba3e2035e6d0b1aa
sovainfo <jsovainfo@gmail.com>
2015-04-01 16:00:52 +0200

    Using JLayoutHelper

--- components/com_content/views/featured/tmpl/default_item.php
+++ components/com_content/views/featured/tmpl/default_item.php
@@ -73,8 +73,7 @@ $info    = $this->item->params->get('info_block_position', 0);
 <?php if ($useDefList && ($info == 1 || $info == 2)) : ?>
 	<?php echo JLayoutHelper::render('joomla.content.info_block.block', array('item' => $this->item, 'params' => $params, 'position' => 'below')); ?>
 	<?php if ($params->get('show_tags', 1) && !empty($this->item->tags->itemTags)) : ?>
-		<?php $this->item->tagLayout = new JLayoutFile('joomla.content.tags'); ?>
-		<?php echo $this->item->tagLayout->render($this->item->tags->itemTags); ?>
+		<?php echo JLayoutHelper::render('joomla.content.tags', $this->item->tags->itemTags); ?>
 	<?php endif; ?>
 <?php endif; ?>
 
