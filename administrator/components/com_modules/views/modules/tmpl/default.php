c4561cdfa6be46cc3b67269ef4196ff5c41986da
bertmert <bertmert@users.noreply.github.com>
2015-04-06 05:25:52 +0200

    Clear #filter_position and #filter_module when client_id changes

--- administrator/components/com_modules/views/modules/tmpl/default.php
+++ administrator/components/com_modules/views/modules/tmpl/default.php
@@ -246,3 +246,9 @@ JFactory::getDocument()->addScriptDeclaration('
 		<?php echo JHtml::_('form.token'); ?>
 	</div>
 </form>
+<script type="text/javascript">
+jQuery("#filter_client_id").on("change", function()
+{
+ jQuery("#filter_position, #filter_module").val("");
+});
+</script>
