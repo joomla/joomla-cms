7c3e174797ec3f41e3b3f295fc6277fe1f3fe336
Nicola Galgano <extensions4joomla@gmail.com>
2015-04-08 20:04:53 +0200

    Handle DB errors in contacts search plugin
    updated to handle DB errors like #6710, #6632 and for #6591
    
    Testing info
    simulate a db error

--- plugins/search/contacts/contacts.php
+++ plugins/search/contacts/contacts.php
@@ -167,7 +167,15 @@ class PlgSearchContacts extends JPlugin
 		}
 
 		$db->setQuery($query, 0, $limit);
-		$rows = $db->loadObjectList();
+		try
+		{
+			$rows = $db->loadObjectList();	
+		}
+		catch (RuntimeException $e)
+		{
+			$rows = array();
+			JFactory::getApplication()->enqueueMessage($e->getMessage(), 'error');
+		}
 
 		if ($rows)
 		{
