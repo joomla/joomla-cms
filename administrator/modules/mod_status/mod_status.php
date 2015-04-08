c781260083fd9bc43ada4f29e5d625c0d40ce1a0
Peter Lose <mail@peterlose.dk>
2015-04-09 00:25:43 +0200

    Revert "Tweaks to footer"
    This reverts commit f295023f0df3c5ac38ac897655a332f64299dede.

--- administrator/modules/mod_status/mod_status.php
+++ administrator/modules/mod_status/mod_status.php
@@ -14,8 +14,6 @@ $user   = JFactory::getUser();
 $db     = JFactory::getDbo();
 $lang   = JFactory::getLanguage();
 $input  = JFactory::getApplication()->input;
-$hideLinks = $input->getBool('hidemainmenu');
-$task      = $input->getCmd('task');
 
 // Get the number of unread messages in your inbox.
 $query	= $db->getQuery(true)
@@ -53,16 +51,6 @@ if ($unread)
 else
 {
 	$inboxClass = 'no-unread-messages';
-}
-
-// Set logout link.
-if ($task == 'edit' || $task == 'editA' || $input->getInt('hidemainmenu'))
-{
-	$logoutLink = '';
-}
-else
-{
-	$logoutLink = JRoute::_('index.php?option=com_login&task=logout&' . JSession::getFormToken() . '=1');
 }
 
 // Get the number of frontend logged in users.
