3f0528ed02dd0b1720bf0db149f03be480e2f05d
Nicola Galgano <extensions4joomla@gmail.com>
2015-04-09 06:59:17 +0200

    travis
    travis whitespace

--- modules/mod_articles_archive/helper.php
+++ modules/mod_articles_archive/helper.php
@@ -48,13 +48,13 @@ class ModArchiveHelper
 		$db->setQuery($query, 0, (int) $params->get('count'));
 		try
 		{
-			$rows = (array) $db->loadObjectList();	
+			$rows = (array) $db->loadObjectList();
 		}
 		catch (RuntimeException $e)
- 		{
- 			JFactory::getApplication()->enqueueMessage($e->getMessage(), 'error');
- 			return;
- 		}
+		{
+			JFactory::getApplication()->enqueueMessage($e->getMessage(), 'error');
+			return;
+		}
 
 		$app    = JFactory::getApplication();
 		$menu   = $app->getMenu();
