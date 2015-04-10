b2cef6a97fa54b827fc51dbbde2c376a6a4d70f7
javier gomez <javier.gomez.corio@gmail.com>
2015-04-10 13:26:14 +0200

    Add debug param to runsystemtests.sh

--- tests/codeception/runsystemtests.sh
+++ tests/codeception/runsystemtests.sh
@@ -7,6 +7,6 @@ php cli/getjoomlacli.php
 printf "\nPreparing Codeception\n"
 php vendor/bin/codecept build
 printf "\nRunning Installation test\n"
-php vendor/bin/codecept run tests/acceptance/installation --steps
+php vendor/bin/codecept run tests/acceptance/installation --steps --debug
 printf "\nSetting Error Reporting to Development\n"
 php vendor/bin/codecept run tests/acceptance/administrator/setDevelopmentErrorReportingCept.php --steps
