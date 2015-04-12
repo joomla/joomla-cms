dc80822a4b92c492ac6a42c8dfc1d29119fe693c
sovainfo <jsovainfo@gmail.com>
2015-04-12 15:03:51 +0200

    Change type_alias to type_id in uc_ItemnameTagid

--- installation/sql/sqlazure/joomla.sql
+++ installation/sql/sqlazure/joomla.sql
@@ -667,7 +667,7 @@ CREATE TABLE [#__contentitem_tag_map](
 	[type_id] [int] NOT NULL,
  CONSTRAINT [#__contentitem_tag_map$uc_ItemnameTagid] UNIQUE NONCLUSTERED
 (
-	[type_alias] ASC,
+	[type_id] ASC,
 	[content_item_id] ASC,
 	[tag_id] ASC
 )WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
