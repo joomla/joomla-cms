307df512ac554c434df39fde29a54d82addb052b
Thomas Hunziker <werbemails@bakual.ch>
2015-04-15 08:19:44 +0200

    [fix] getMessage doesn't exist, use getError instead. Fixes #6783

--- administrator/components/com_menus/models/item.php
+++ administrator/components/com_menus/models/item.php
@@ -1090,7 +1090,18 @@ class MenusModelItem extends JModelAdmin
 		$query = $db->getQuery(true);
 		$table = $this->getTable();
 
-		if (!$table->rebuild())
+		try
+		{
+			$rebuildResult = $table->rebuild();
+		}
+		catch (Exception $e)
+		{
+			$this->setError($e->getMessage());
+
+			return false;
+		}
+
+		if (!$rebuildResult)
 		{
 			$this->setError($table->getError());
 
