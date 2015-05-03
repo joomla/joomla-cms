9ede8cc9cb8345adeeffde9e707a6553f5ae2b8a
Johan Janssens <johan@janssens.me>
2015-05-03 14:38:08 +0200

    Remove libraries/composer_autoload.php

--- administrator/components/com_admin/script.php
+++ administrator/components/com_admin/script.php
@@ -1160,6 +1160,7 @@ class JoomlaInstallerScript
 			'/media/editors/codemirror/js/php.js',
 			'/media/editors/codemirror/js/xml-fold.js',
 			'/media/editors/codemirror/js/xml.js',
+			'/libraries/composer_autoload.php',
 		);
 
 		// TODO There is an issue while deleting folders using the ftp mode
