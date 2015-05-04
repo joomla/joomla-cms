b0695fb8e2956cf1191f971efddea42fb1d337c3
photodude <photodude@users.noreply.github.com>
2015-05-04 15:31:09 -0600

    Changes for php 7

--- build/travis/travis.sh
+++ build/travis/travis.sh
@@ -5,12 +5,18 @@ VERSION=`phpenv version-name`
 if [ "${VERSION}" = 'hhvm' ]
 then
     PHPINI=/etc/hhvm/php.ini
+else if [ "${VERSION}" = '7.0' ]
+then
+    PHPINI=~/.phpenv/versions/$VERSION/etc/php.ini
+    echo "extension = memcache.so"  >> $PHPINI
+    echo "extension = memcached.so" >> $PHPINI
+    echo "extension = redis.so"     >> $PHPINI
+    phpenv config-add build/travis/phpenv/memcached.ini
+    phpenv config-add build/travis/phpenv/apc-$VERSION.ini
+    phpenv config-add build/travis/phpenv/redis.ini
 else
     PHPINI=~/.phpenv/versions/$VERSION/etc/php.ini
     phpenv config-add build/travis/phpenv/memcached.ini
     phpenv config-add build/travis/phpenv/apc-$VERSION.ini
     phpenv config-add build/travis/phpenv/redis.ini
-#    echo "extension = memcache.so"  >> $PHPINI
-#    echo "extension = memcached.so" >> $PHPINI
-#    echo "extension = redis.so"     >> $PHPINI
 fi
