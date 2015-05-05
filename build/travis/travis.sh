13e42262d2e33f97fe515c737f2812a619b91829
photodude <photodude@users.noreply.github.com>
2015-05-04 22:17:53 -0600

    Try installing apcu

--- build/travis/travis.sh
+++ build/travis/travis.sh
@@ -10,6 +10,7 @@ then
 elif [ "${VERSION}" = '7.0' ]
 then
     PHPINI=~/.phpenv/versions/$VERSION/etc/php.ini
+    pecl install apcu.so
     echo "extension = apcu.so"  >> $PHPINI
     echo "extension = memcache.so"  >> $PHPINI
     echo "extension = memcached.so" >> $PHPINI
