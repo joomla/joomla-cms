ed1aba71f219f84763b23dfebf233a9826b5eb11
AteDev <totogoulag@gmail.com>
2015-05-06 00:11:57 +0200

    Add anchor css for menu item separator
    This PR add a field to give possibility to add anchor css for menu item
    type of separator (like component, url ...)

--- modules/mod_menu/tmpl/default_separator.php
+++ modules/mod_menu/tmpl/default_separator.php
@@ -10,6 +10,7 @@
 defined('_JEXEC') or die;
 
 // Note. It is important to remove spaces between elements.
+$class = $item->anchor_css ? ' ' . $item->anchor_css : '';
 $title = $item->anchor_title ? ' title="' . $item->anchor_title . '" ' : '';
 if ($item->menu_image)
 	{
@@ -23,6 +24,6 @@ else
 }
 
 ?>
-<span class="separator"<?php echo $title; ?>>
+<span class="separator<?php echo $class;?>"<?php echo $title; ?>>
 	<?php echo $linktype; ?>
 </span>
