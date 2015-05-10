9dfda35537ab8f65aeb896922dce9ace2eeac33c
photodude <photodude@users.noreply.github.com>
2015-05-09 22:20:15 -0600

    PHP 7 needs zend-framework

--- build/travis/travis.sh
+++ build/travis/travis.sh
@@ -5,18 +5,26 @@ VERSION=`phpenv version-name`
 if [ "${VERSION}" = 'hhvm' ]
 then
     PHPINI=/etc/hhvm/php.ini
+    echo "extension_dir = /etc/hhvm" >> $PHPINI
     echo "memory_limit = -1"  >> $PHPINI
     sudo apt-get update -qq
     sudo apt-get install -y php5-apcu
+    sudo apt-get install -y memcached
+    sudo apt-get install -y libmemcached
     sudo apt-get install -y php5-memcached
     sudo apt-get install -y redis-server
     sudo apt-get install -y php5-redis
+    echo "extension = apcu.so" >> $PHPINI
+    echo "apc.enable_cli=true" >> $PHPINI
+    echo "extension = memcached.so" >> $PHPINI
+    echo "extension = redis.so" >> $PHPINI
     # phpenv config-add build/travis/phpenv/apc-$VERSION.ini
-    # echo "extension_dir = /etc/hhvm" >> $PHPINI
     # echo "hhvm.extensions[pgsql] = pgsql.so" >> $PHPINI
 elif [ "${VERSION}" = '7.0' ]
 then
     PHPINI=~/.phpenv/versions/$VERSION/etc/php.ini
+    sudo apt-get update -qq
+    sudo apt-get install -y zend-framework
     sudo apt-get install -y php5-apcu
     sudo apt-get install -y php5-redis
     phpenv config-add build/travis/phpenv/memcached.ini
