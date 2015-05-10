c37a63c154855ce1f58160de6e2376e1e09352c8
photodude <photodude@users.noreply.github.com>
2015-05-09 19:49:52 -0600

    Try installing a few dependencies for php 7 and hhvm

--- build/travis/travis.sh
+++ build/travis/travis.sh
@@ -5,11 +5,21 @@ VERSION=`phpenv version-name`
 if [ "${VERSION}" = 'hhvm' ]
 then
     PHPINI=/etc/hhvm/php.ini
+    echo "memory_limit = -1"  >> $PHPINI
+    sudo apt-get update -qq
+    sudo apt-get install -y hhvm-dbg
+    sudo apt-get install -y php5-apcu
+    sudo apt-get install -y php5-memcached
+    sudo apt-get install -y php5-redis
+    phpenv config-add build/travis/phpenv/apc-$VERSION.ini
     # echo "extension_dir = /etc/hhvm" >> $PHPINI
     # echo "hhvm.extensions[pgsql] = pgsql.so" >> $PHPINI
 elif [ "${VERSION}" = '7.0' ]
 then
     PHPINI=~/.phpenv/versions/$VERSION/etc/php.ini
+    sudo apt-get install -y php5-apcu
+    sudo apt-get install -y php5-memcached
+    sudo apt-get install -y php5-redis
     phpenv config-add build/travis/phpenv/memcached.ini
     phpenv config-add build/travis/phpenv/apc-$VERSION.ini
     phpenv config-add build/travis/phpenv/redis.ini
