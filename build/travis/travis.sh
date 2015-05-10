c4f16b9d5b20555efc9e9ad522d1bf07c096359a
photodude <photodude@users.noreply.github.com>
2015-05-10 10:33:03 -0600

    memory_limit = -1 didn't help with phpunit/php-timer/PHP/Timer.php exceeded memory limit issue
    - tweak php 7 to see if we can eliminate the following errors
    ```
    PHP Warning:  PHP Startup: Unable to load dynamic library '/home/travis/.phpenv/versions/nightly/lib/php/extensions/no-debug-zts-20141001/apcu.so' - /home/travis/.phpenv/versions/nightly/lib/php/extensions/no-debug-zts-20141001/apcu.so: cannot open shared object file: No such file or directory in Unknown on line 0
    
    PHP Warning:  PHP Startup: Unable to load dynamic library '/home/travis/.phpenv/versions/nightly/lib/php/extensions/no-debug-zts-20141001/memcached.so' - /home/travis/.phpenv/versions/nightly/lib/php/extensions/no-debug-zts-20141001/memcached.so: cannot open shared object file: No such file or directory in Unknown on line 0
    
    PHP Warning:  PHP Startup: Unable to load dynamic library '/home/travis/.phpenv/versions/nightly/lib/php/extensions/no-debug-zts-20141001/redis.so' - /home/travis/.phpenv/versions/nightly/lib/php/extensions/no-debug-zts-20141001/redis.so: cannot open shared object file: No such file or directory in Unknown on line 0
    ```

--- build/travis/travis.sh
+++ build/travis/travis.sh
@@ -6,7 +6,6 @@ if [ "${VERSION}" = 'hhvm' ]
 then
     PHPINI=/etc/hhvm/php.ini
     echo "extension_dir = /etc/hhvm" >> $PHPINI
-    echo "memory_limit = -1"  >> $PHPINI
     sudo apt-get update -qq
     sudo apt-get install -y php-pear
     sudo apt-get install -y php5-apcu
@@ -25,9 +24,13 @@ then
     sudo apt-get install -y zend-framework
     sudo apt-get install -y php5-apcu
     sudo apt-get install -y php5-redis
-    phpenv config-add build/travis/phpenv/memcached.ini
-    phpenv config-add build/travis/phpenv/apc-$VERSION.ini
-    phpenv config-add build/travis/phpenv/redis.ini
+    echo "extension = apcu.so" >> $PHPINI
+    echo "apc.enable_cli=true" >> $PHPINI
+    echo "extension = memcached.so" >> $PHPINI
+    echo "extension = redis.so" >> $PHPINI
+    #phpenv config-add build/travis/phpenv/memcached.ini
+    #phpenv config-add build/travis/phpenv/apc-$VERSION.ini
+    #phpenv config-add build/travis/phpenv/redis.ini
 else
     PHPINI=~/.phpenv/versions/$VERSION/etc/php.ini
     phpenv config-add build/travis/phpenv/memcached.ini
