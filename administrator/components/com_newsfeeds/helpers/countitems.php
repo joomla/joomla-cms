56573aa8b83cbe5987ce0b526024172afb31e97e
pe7er <github@pe7er.com>
2015-05-13 22:10:05 +0200

    Category Manager added "Count Items" for NewsFeeds

--- administrator/components/com_newsfeeds/helpers/countitems.php
+++ administrator/components/com_newsfeeds/helpers/countitems.php
@@ -30,19 +30,19 @@ class ContentitemsHelper extends JHelperContent
 	public static function countItems($query)
 	{
 		// Join articles to categories and
-		// Count published articles
+		// Count published items
 		$query->select('COUNT(DISTINCT cp.id) AS count_published');
 		$query->join('LEFT', '#__newsfeeds AS cp ON cp.catid = a.id AND cp.published = 1');
 
-		// Count unpublished articles
+		// Count unpublished items
 		$query->select('COUNT(DISTINCT cu.id) AS count_unpublished');
 		$query->join('LEFT', '#__newsfeeds AS cu ON cu.catid = a.id AND cu.published = 0');
 
-		// Count archived articles
+		// Count archived items
 		$query->select('COUNT(DISTINCT ca.id) AS count_archived');
 		$query->join('LEFT', '#__newsfeeds AS ca ON ca.catid = a.id AND ca.published = 2');
 
-		// Count trashed articles
+		// Count trashed items
 		$query->select('COUNT(DISTINCT ct.id) AS count_trashed');
 		$query->join('LEFT', '#__newsfeeds AS ct ON ct.catid = a.id AND ct.published = -2');
 
