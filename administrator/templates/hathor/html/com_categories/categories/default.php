1cdd0c695252045108e2cfac07bbf0c1aeec5397
pe7er <github@pe7er.com>
2015-05-13 20:04:18 +0200

    Changed tmpl override Hathor,replaced hardcoded hyperlink to $component

--- administrator/templates/hathor/html/com_categories/categories/default.php
+++ administrator/templates/hathor/html/com_categories/categories/default.php
@@ -24,8 +24,8 @@ $listDirn	= $this->escape($this->state->get('list.direction'));
 $ordering 	= ($listOrder == 'a.lft');
 $saveOrder 	= ($listOrder == 'a.lft' && $listDirn == 'asc');
 $assoc		= JLanguageAssociations::isEnabled();
-$component  = JUri::getInstance()->getVar('extension');
-
+$jinput 	= JFactory::getApplication()->input;
+$component	= $jinput->get('extension');
 ?>
 <div class="categories">
 	<form action="<?php echo JRoute::_('index.php?option=com_categories&view=categories'); ?>" method="post" name="adminForm" id="adminForm">
@@ -100,17 +100,22 @@ $component  = JUri::getInstance()->getVar('extension');
 								<?php echo JHtml::_('grid.order', $this->items, 'filesave.png', 'categories.saveorder'); ?>
 							<?php endif; ?>
 						</th>
-						<?php if ($component == 'com_content') : ?>
+						<?php if (isset($this->items[0]) && property_exists($this->items[0], count_published)) : ?>
 							<th width="1%" class="nowrap center hidden-phone">
 								<i class="icon-publish"></i>
 							</th>
+						<?php endif;?>
+						<?php if (isset($this->items[0]) && property_exists($this->items[0], count_unpublished)) : ?>
 							<th width="1%" class="nowrap center hidden-phone">
 								<i class="icon-unpublish"></i>
 							</th>
-							</th>
+						<?php endif;?>
+						<?php if (isset($this->items[0]) && property_exists($this->items[0], count_archived)) : ?>
 							<th width="1%" class="nowrap center hidden-phone">
 								<i class="icon-archive"></i>
 							</th>
+						<?php endif;?>
+						<?php if (isset($this->items[0]) && property_exists($this->items[0], count_trashed)) : ?>
 							<th width="1%" class="nowrap center hidden-phone">
 								<i class="icon-trash"></i>
 							</th>
@@ -179,28 +184,30 @@ $component  = JUri::getInstance()->getVar('extension');
 									<?php echo $orderkey + 1; ?>
 								<?php endif; ?>
 							</td>
-							<?php if ($component == 'com_content') : ?>
+							<?php if (isset($this->items[0]) && property_exists($this->items[0], count_published)) : ?>
 								<td class="center">
-									<a title="<?php echo JText::_('COM_CATEGORY_COUNT_PUBLISHED_ITEMS');?>" href="<?php echo JRoute::_('index.php?option=com_content&view=articles&filter[category_id]=' . (int) $item->id . '&filter[published]=1' . '&filter[level]=' . (int) $item->level);?>">
-										<?php echo $item->count_published; ?>
-									</a>
+									<a title="<?php echo JText::_('COM_CATEGORY_COUNT_PUBLISHED_ITEMS');?>" href="<?php echo JRoute::_('index.php?option=' . $component . '&filter[category_id]=' . (int) $item->id . '&filter[published]=1' . '&filter[level]=' . (int) $item->level);?>">
+										<?php echo $item->count_published; ?></a>
 								</td>
+							<?php endif;?>
+							<?php if (isset($this->items[0]) && property_exists($this->items[0], count_unpublished)) : ?>
 								<td class="center">
-									<a title="<?php echo JText::_('COM_CATEGORY_COUNT_UNPUBLISHED_ITEMS');?>" href="<?php echo JRoute::_('index.php?option=com_content&view=articles&filter[category_id]=' . (int) $item->id . '&filter[published]=0' . '&filter[level]=' . (int) $item->level);?>">
-										<?php echo $item->count_unpublished; ?>
-									</a>
+									<a title="<?php echo JText::_('COM_CATEGORY_COUNT_UNPUBLISHED_ITEMS');?>" href="<?php echo JRoute::_('index.php?option=' . $component . '&filter[category_id]=' . (int) $item->id . '&filter[published]=0' . '&filter[level]=' . (int) $item->level);?>">
+										<?php echo $item->count_unpublished; ?></a>
 								</td>
+							<?php endif;?>
+							<?php if (isset($this->items[0]) && property_exists($this->items[0], count_archived)) : ?>
 								<td class="center">
-									<a title="<?php echo JText::_('COM_CATEGORY_COUNT_ARCHIVED_ITEMS');?>" href="<?php echo JRoute::_('index.php?option=com_content&view=articles&filter[category_id]=' . (int) $item->id . '&filter[published]=2' . '&filter[level]=' . (int) $item->level);?>">
-										<?php echo $item->count_archived; ?>
-									</a>
+									<a title="<?php echo JText::_('COM_CATEGORY_COUNT_ARCHIVED_ITEMS');?>" href="<?php echo JRoute::_('index.php?option=' . $component . '&filter[category_id]=' . (int) $item->id . '&filter[published]=2' . '&filter[level]=' . (int) $item->level);?>">
+										<?php echo $item->count_archived; ?></a>
 								</td>
+							<?php endif;?>
+							<?php if (isset($this->items[0]) && property_exists($this->items[0], count_trashed)) : ?>
 								<td class="center">
-									<a title="<?php echo JText::_('COM_CATEGORY_COUNT_TRASHED_ITEMS');?>" href="<?php echo JRoute::_('index.php?option=com_content&view=articles&filter[category_id]=' . (int) $item->id . '&filter[published]=-2' . '&filter[level]=' . (int) $item->level);?>">
-										<?php echo $item->count_trashed; ?>
-									</a>
+									<a title="<?php echo JText::_('COM_CATEGORY_COUNT_TRASHED_ITEMS');?>" href="<?php echo JRoute::_('index.php?option=' . $component . '&filter[category_id]=' . (int) $item->id . '&filter[published]=-2' . '&filter[level]=' . (int) $item->level);?>">
+										<?php echo $item->count_trashed; ?></a>
 								</td>
-							<?php endif; ?>
+							<?php endif;?>
 							<td class="center">
 								<?php echo $this->escape($item->access_level); ?>
 							</td>
