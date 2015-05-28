7bbf4d218a4e9b35617535f9122d5811c7efa230
elioair <eliodisegno@gmail.com>
2015-05-28 13:49:57 +0300

    PR code style fix #7045
    Changed the code style of the PR.

--- libraries/cms/response/json.php
+++ libraries/cms/response/json.php
@@ -115,9 +115,11 @@ class JResponseJson
 	 */
 	public function __toString()
 	{
-		if (version_compare(PHP_VERSION, '5.4.0', '>=')) {
+		if (version_compare(PHP_VERSION, '5.4.0', '>=')) 
+		{
 			return json_encode($this, JSON_UNESCAPED_UNICODE);
 		}
+		
 		return json_encode($this);
 	}
 }
