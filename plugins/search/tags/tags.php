1476955eabc4d7dc8b13fcfccccf12bea81e47ae
Nicola Galgano <extensions4joomla@gmail.com>
2015-05-30 22:29:22 +0200

    added the generic error msg
    used the generic message string "There has been an error" as suggested in #6710 the string was added in #7073

--- plugins/search/tags/tags.php
+++ plugins/search/tags/tags.php
@@ -143,7 +143,8 @@ class PlgSearchTags extends JPlugin
 		catch (RuntimeException $e)
 		{
 			$rows = array();
-			JFactory::getApplication()->enqueueMessage($e->getMessage(), 'error');
+			JFactory::getApplication()->enqueueMessage(JText::_('JLIB_DATABASE_GENERIC_SQL_ERROR'), 'error');
+			JLog::add($e->getMessage(), JLog::ERROR, 'controller');
 		}
 
 		if ($rows)
