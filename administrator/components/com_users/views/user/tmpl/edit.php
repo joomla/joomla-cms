70a954b5564087e055a25fb8564c353fab58232e
Jean-Marie Simonet <infografjms@gmail.com>
2015-06-01 16:55:27 +0200

    Password: Fix for Firefox as autocomplete off does not work

--- administrator/components/com_users/views/user/tmpl/edit.php
+++ administrator/components/com_users/views/user/tmpl/edit.php
@@ -59,6 +59,9 @@ $fieldsets = $this->form->getFieldsets();
 							<?php echo $field->label; ?>
 						</div>
 						<div class="controls">
+							<?php if ($field->name = 'password') : ?>
+								<!-- disables autocomplete --><input type="text" style="display:none">
+							<?php endif; ?>
 							<?php echo $field->input; ?>
 						</div>
 					</div>
