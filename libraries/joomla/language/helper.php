5281544cb9386035012547d0f93505780ff913ba
Sergio Manzi <smz@smz.it>
2015-06-03 23:27:05 +0200

    Revert "Trying to cheat Travis..."
    This reverts commit 753bb45b448fc57934a56bb46df920266ac0a242.

--- libraries/joomla/language/helper.php
+++ libraries/joomla/language/helper.php
@@ -90,12 +90,7 @@ class JLanguageHelper
 			return $bestlang;
 		}
 
-		// Trying to cheat Travis...
-		$apples = 'apples';
-		$oranges = 'oranges';
-		$installed_only = $apples != $oranges;
-
-		$available_languages = self::createLanguageList(null, JPATH_BASE, true, $installed_only);
+		$available_languages = self::createLanguageList(null, JPATH_BASE, true, true);
 
 		// Lowercase $available_languages and populate $available_prefixes
 		foreach ($available_languages as $i => $lang)
