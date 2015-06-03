b774a0930ef0d27bc73154bc247aa0a5ab01fbcc
Jean-Marie Simonet <infografjms@gmail.com>
2015-06-03 10:10:05 +0200

    [imp] Password: Fix for Firefox as autocomplete off does not work. Closes #7094

--- components/com_users/views/profile/tmpl/edit.php
+++ components/com_users/views/profile/tmpl/edit.php
@@ -69,6 +69,9 @@ $lang->load('plg_user_profile', JPATH_ADMINISTRATOR);
 							<?php endif; ?>
 						</div>
 						<div class="controls">
+							<?php if ($field->fieldname == 'password1') : ?>
+								<?php // Disables autocomplete ?> <input type="text" style="display:none">
+							<?php endif; ?>
 							<?php echo $field->input; ?>
 						</div>
 					</div>
