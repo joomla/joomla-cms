1a8a147f590c4d5c993f0a83522be104b48627b7
Tuan Pham Ngoc <tuanpn@joomdonation.com>
2015-06-12 15:14:44 +0700

    Sorry, I updated wrong file before

--- components/com_content/views/article/view.html.php
+++ components/com_content/views/article/view.html.php
@@ -10,19 +10,21 @@
 defined('_JEXEC') or die;
 
 /**
- * HTML View class for the Content component
+ * HTML Article View class for the Content component
  *
  * @since  1.5
  */
-class ContentViewArchive extends JViewLegacy
+class ContentViewArticle extends JViewLegacy
 {
-	protected $state = null;
+	protected $item;
 
-	protected $item = null;
+	protected $params;
 
-	protected $items = null;
+	protected $print;
 
-	protected $pagination = null;
+	protected $state;
+
+	protected $user;
 
 	/**
 	 * Execute and display a template script.
@@ -33,109 +35,149 @@ class ContentViewArchive extends JViewLegacy
 	 */
 	public function display($tpl = null)
 	{
-		$user		= JFactory::getUser();
+		$app        = JFactory::getApplication();
+		$user       = JFactory::getUser();
+		$dispatcher = JEventDispatcher::getInstance();
 
-		$state 		= $this->get('State');
-		$items 		= $this->get('Items');
-		$pagination	= $this->get('Pagination');
+		$this->item  = $this->get('Item');
+		$this->print = $app->input->getBool('print');
+		$this->state = $this->get('State');
+		$this->user  = $user;
 
-		// Get the page/component configuration
-		$params = &$state->params;
-
-		foreach ($items as $item)
+		// Check for errors.
+		if (count($errors = $this->get('Errors')))
 		{
-			$item->catslug = ($item->category_alias) ? ($item->catid . ':' . $item->category_alias) : $item->catid;
-			$item->parent_slug = ($item->parent_alias) ? ($item->parent_id . ':' . $item->parent_alias) : $item->parent_id;
+			JError::raiseWarning(500, implode("\n", $errors));
 
-			// No link for ROOT category
-			if ($item->parent_alias == 'root')
-			{
-				$item->parent_slug = null;
-			}
-
-			$item->event = new stdClass;
-
-			$dispatcher = JEventDispatcher::getInstance();
-
-			// Old plugins: Ensure that text property is available
-			if (!isset($item->text))
-			{
-				$item->text = $item->introtext;
-			}
-
-			JPluginHelper::importPlugin('content');
-			$dispatcher->trigger('onContentPrepare', array ('com_content.archive', &$item, &$item->params, 0));
-
-			// Old plugins: Use processed text as introtext
-			$item->introtext = $item->text;
-
-			$results = $dispatcher->trigger('onContentAfterTitle', array('com_content.archive', &$item, &$item->params, 0));
-			$item->event->afterDisplayTitle = trim(implode("\n", $results));
-
-			$results = $dispatcher->trigger('onContentBeforeDisplay', array('com_content.archive', &$item, &$item->params, 0));
-			$item->event->beforeDisplayContent = trim(implode("\n", $results));
-
-			$results = $dispatcher->trigger('onContentAfterDisplay', array('com_content.archive', &$item, &$item->params, 0));
-			$item->event->afterDisplayContent = trim(implode("\n", $results));
+			return false;
 		}
 
-		$form = new stdClass;
+		// Create a shortcut for $item.
+		$item            = $this->item;
+		$item->tagLayout = new JLayoutFile('joomla.content.tags');
 
-		// Month Field
-		$months = array(
-			'' => JText::_('COM_CONTENT_MONTH'),
-			'01' => JText::_('JANUARY_SHORT'),
-			'02' => JText::_('FEBRUARY_SHORT'),
-			'03' => JText::_('MARCH_SHORT'),
-			'04' => JText::_('APRIL_SHORT'),
-			'05' => JText::_('MAY_SHORT'),
-			'06' => JText::_('JUNE_SHORT'),
-			'07' => JText::_('JULY_SHORT'),
-			'08' => JText::_('AUGUST_SHORT'),
-			'09' => JText::_('SEPTEMBER_SHORT'),
-			'10' => JText::_('OCTOBER_SHORT'),
-			'11' => JText::_('NOVEMBER_SHORT'),
-			'12' => JText::_('DECEMBER_SHORT')
-		);
-		$form->monthField = JHtml::_(
-			'select.genericlist',
-			$months,
-			'month',
-			array(
-				'list.attr' => 'size="1" class="inputbox"',
-				'list.select' => $state->get('filter.month'),
-				'option.key' => null
-			)
-		);
+		// Add router helpers.
+		$item->slug        = $item->alias ? ($item->id . ':' . $item->alias) : $item->id;
+		$item->catslug     = $item->category_alias ? ($item->catid . ':' . $item->category_alias) : $item->catid;
+		$item->parent_slug = $item->parent_alias ? ($item->parent_id . ':' . $item->parent_alias) : $item->parent_id;
 
-		// Year Field
-		$years = array();
-		$years[] = JHtml::_('select.option', null, JText::_('JYEAR'));
-
-		for ($year = date('Y'), $i = $year - 10; $i <= $year; $i++)
+		// No link for ROOT category
+		if ($item->parent_alias == 'root')
 		{
-			$years[] = JHtml::_('select.option', $i, $i);
+			$item->parent_slug = null;
 		}
 
-		$form->yearField = JHtml::_(
-			'select.genericlist',
-			$years,
-			'year',
-			array('list.attr' => 'size="1" class="inputbox"', 'list.select' => $state->get('filter.year'))
-		);
-		$form->limitField = $pagination->getLimitBox();
+		// TODO: Change based on shownoauth
+		$item->readmore_link = JRoute::_(ContentHelperRoute::getArticleRoute($item->slug, $item->catid, $item->language));
+
+		// Merge article params. If this is single-article view, menu params override article params
+		// Otherwise, article params override menu item params
+		$this->params = $this->state->get('params');
+		$active       = $app->getMenu()->getActive();
+		$temp         = clone $this->params;
+
+		// Check to see which parameters should take priority
+		if ($active)
+		{
+			$currentLink = $active->link;
+
+			// If the current view is the active item and an article view for this article, then the menu item params take priority
+			if (strpos($currentLink, 'view=article') && (strpos($currentLink, '&id=' . (string) $item->id)))
+			{
+				// Load layout from active query (in case it is an alternative menu item)
+				if (isset($active->query['layout']))
+				{
+					$this->setLayout($active->query['layout']);
+				}
+				// Check for alternative layout of article
+				elseif ($layout = $item->params->get('article_layout'))
+				{
+					$this->setLayout($layout);
+				}
+
+				// $item->params are the article params, $temp are the menu item params
+				// Merge so that the menu item params take priority
+				$item->params->merge($temp);
+			}
+			else
+			{
+				// Current view is not a single article, so the article params take priority here
+				// Merge the menu item params with the article params so that the article params take priority
+				$temp->merge($item->params);
+				$item->params = $temp;
+
+				// Check for alternative layouts (since we are not in a single-article menu item)
+				// Single-article menu item layout takes priority over alt layout for an article
+				if ($layout = $item->params->get('article_layout'))
+				{
+					$this->setLayout($layout);
+				}
+			}
+		}
+		else
+		{
+			// Merge so that article params take priority
+			$temp->merge($item->params);
+			$item->params = $temp;
+
+			// Check for alternative layouts (since we are not in a single-article menu item)
+			// Single-article menu item layout takes priority over alt layout for an article
+			if ($layout = $item->params->get('article_layout'))
+			{
+				$this->setLayout($layout);
+			}
+		}
+
+		$offset = $this->state->get('list.offset');
+
+		// Check the view access to the article (the model has already computed the values).
+		if ($item->params->get('access-view') == false && ($item->params->get('show_noauth', '0') == '0'))
+		{
+			JError::raiseWarning(403, JText::_('JERROR_ALERTNOAUTHOR'));
+
+			return;
+		}
+
+		if ($item->params->get('show_intro', '1') == '1')
+		{
+			$item->text = $item->introtext . ' ' . $item->fulltext;
+		}
+		elseif ($item->fulltext)
+		{
+			$item->text = $item->fulltext;
+		}
+		else
+		{
+			$item->text = $item->introtext;
+		}
+
+		$item->tags = new JHelperTags;
+		$item->tags->getItemTags('com_content.article', $this->item->id);
+
+		// Process the content plugins.
+
+		JPluginHelper::importPlugin('content');
+		$dispatcher->trigger('onContentPrepare', array ('com_content.article', &$item, &$item->params, $offset));
+
+		$item->event = new stdClass;
+		$results = $dispatcher->trigger('onContentAfterTitle', array('com_content.article', &$item, &$item->params, $offset));
+		$item->event->afterDisplayTitle = trim(implode("\n", $results));
+
+		$results = $dispatcher->trigger('onContentBeforeDisplay', array('com_content.article', &$item, &$item->params, $offset));
+		$item->event->beforeDisplayContent = trim(implode("\n", $results));
+
+		$results = $dispatcher->trigger('onContentAfterDisplay', array('com_content.article', &$item, &$item->params, $offset));
+		$item->event->afterDisplayContent = trim(implode("\n", $results));
+
+		// Increment the hit counter of the article.
+		if (!$this->params->get('intro_only') && $offset == 0)
+		{
+			$model = $this->getModel();
+			$model->hit();
+		}
 
 		// Escape strings for HTML output
-		$this->pageclass_sfx = htmlspecialchars($params->get('pageclass_sfx'));
-
-		$this->filter     = $state->get('list.filter');
-		$this->form       = &$form;
-		$this->items      = &$items;
-		$this->params     = &$params;
-		$this->user       = &$user;
-		$this->pagination = &$pagination;
-		$this->pagination->setAdditionalUrlParam("month", $state->get('filter.month'));
-		$this->pagination->setAdditionalUrlParam("year", $state->get('filter.year'));
+		$this->pageclass_sfx = htmlspecialchars($this->item->params->get('pageclass_sfx'));
 
 		$this->_prepareDocument();
 
@@ -143,15 +185,16 @@ class ContentViewArchive extends JViewLegacy
 	}
 
 	/**
-	 * Prepares the document
+	 * Prepares the document.
 	 *
 	 * @return  void.
 	 */
 	protected function _prepareDocument()
 	{
-		$app   = JFactory::getApplication();
-		$menus = $app->getMenu();
-		$title = null;
+		$app     = JFactory::getApplication();
+		$menus   = $app->getMenu();
+		$pathway = $app->getPathway();
+		$title   = null;
 
 		// Because the application sets a default page title,
 		// we need to get it from the menu item itself
@@ -168,6 +211,35 @@ class ContentViewArchive extends JViewLegacy
 
 		$title = $this->params->get('page_title', '');
 
+		$id = (int) @$menu->query['id'];
+
+		// If the menu item does not concern this article
+		if ($menu && ($menu->query['option'] != 'com_content' || $menu->query['view'] != 'article' || $id != $this->item->id))
+		{
+			// If this is not a single article menu item, set the page title to the article title
+			if ($this->item->title)
+			{
+				$title = $this->item->title;
+			}
+
+			$path     = array(array('title' => $this->item->title, 'link' => ''));
+			$category = JCategories::getInstance('Content')->get($this->item->catid);
+
+			while ($category && ($menu->query['option'] != 'com_content' || $menu->query['view'] == 'article' || $id != $category->id) && $category->id > 1)
+			{
+				$path[]   = array('title' => $category->title, 'link' => ContentHelperRoute::getCategoryRoute($category->id));
+				$category = $category->getParent();
+			}
+
+			$path = array_reverse($path);
+
+			foreach ($path as $item)
+			{
+				$pathway->addItem($item['title'], $item['link']);
+			}
+		}
+
+		// Check for empty title and add site name if param is set
 		if (empty($title))
 		{
 			$title = $app->get('sitename');
@@ -181,14 +253,27 @@ class ContentViewArchive extends JViewLegacy
 			$title = JText::sprintf('JPAGETITLE', $title, $app->get('sitename'));
 		}
 
+		if (empty($title))
+		{
+			$title = $this->item->title;
+		}
+
 		$this->document->setTitle($title);
 
-		if ($this->params->get('menu-meta_description'))
+		if ($this->item->metadesc)
+		{
+			$this->document->setDescription($this->item->metadesc);
+		}
+		elseif ($this->params->get('menu-meta_description'))
 		{
 			$this->document->setDescription($this->params->get('menu-meta_description'));
 		}
 
-		if ($this->params->get('menu-meta_keywords'))
+		if ($this->item->metakey)
+		{
+			$this->document->setMetadata('keywords', $this->item->metakey);
+		}
+		elseif ($this->params->get('menu-meta_keywords'))
 		{
 			$this->document->setMetadata('keywords', $this->params->get('menu-meta_keywords'));
 		}
@@ -196,6 +281,36 @@ class ContentViewArchive extends JViewLegacy
 		if ($this->params->get('robots'))
 		{
 			$this->document->setMetadata('robots', $this->params->get('robots'));
+		}
+
+		if ($app->get('MetaAuthor') == '1')
+		{
+			$author = $this->item->created_by_alias ? $this->item->created_by_alias : $this->item->author;
+			$this->document->setMetaData('author', $author);
+		}
+
+		$mdata = $this->item->metadata->toArray();
+
+		foreach ($mdata as $k => $v)
+		{
+			if ($v)
+			{
+				$this->document->setMetadata($k, $v);
+			}
+		}
+
+		// If there is a pagebreak heading or title, add it to the page title
+		if (!empty($this->item->page_title))
+		{
+			$this->item->title = $this->item->title . ' - ' . $this->item->page_title;
+			$this->document->setTitle(
+				$this->item->page_title . ' - ' . JText::sprintf('PLG_CONTENT_PAGEBREAK_PAGE_NUM', $this->state->get('list.offset') + 1)
+			);
+		}
+
+		if ($this->print)
+		{
+			$this->document->setMetaData('robots', 'noindex, nofollow');
 		}
 	}
 }
