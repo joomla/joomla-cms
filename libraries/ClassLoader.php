e1cfdc0bf85a9905adb7a7627f76af42e788cd44
Johan Janssens <johan@timble.net>
2015-06-17 21:46:32 +0100

    Use composer preferred way to override the autoloader
    Fixes #6335

--- libraries/ClassLoader.php
+++ libraries/ClassLoader.php
@@ -5,46 +5,38 @@
  * @copyright  Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE
  */
-
-namespace Composer\Autoload;
-
+ 
 defined('_JEXEC') or die;
 
-// We have to manually require the base ClassLoader as the autoloader isn't loaded yet, but only if it doesn't exist
-if (!class_exists('Composer\\Autoload\\ClassLoader'))
-{
-	require_once __DIR__ . '/vendor/composer/ClassLoader.php';
-}
-
 /**
- * Extended Composer ClassLoader for Joomla!
+ * Decorate Composer ClassLoader for Joomla!
  *
  * For backward compatibility due to class aliasing in the CMS, the loadClass() method was modified to call
  * the JLoader::applyAliasFor() method.
  *
- * @author  Nicholas Dionysopoulos
+ * @author  Johan Janssens
  * @since   3.4
  */
-class ClassLoaderJoomla extends ClassLoader
+class JClassLoader
 {
 	/**
-	 * Loads the given class or interface.
+	 * The composer class loader
 	 *
-	 * @param   string  $class  The name of the class
-	 *
-	 * @return  bool|null True if loaded, null otherwise
-	 *
-	 * @since   3.4
+	 * @var \Composer\Autoload\ClassLoader
 	 */
+	private $loader;
+
+	public function __construct(\Composer\Autoload\ClassLoader $loader)
+	{
+		$this->loader = $loader;
+	}
+	
 	public function loadClass($class)
 	{
-		if ($file = $this->findFile($class))
-		{
-			includeFile($file);
-
+		if ($result = $this->loader->loadClass($class)) {
 			\JLoader::applyAliasFor($class);
-
-			return true;
 		}
-	}
+		
+		return $result;
+    }
 }
