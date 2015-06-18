ca928b549650793a4818d75879f3393065cf5fa4
laoneo <allon.moritz@digital-peak.com>
2015-06-18 13:10:53 +0200

    Structured data support in Protostar
    When an extension enriches it's content with schema.org meta data, then the W3C validator will complain like
    
    `The itemprop attribute was specified, but the element is not a property of any item.
    <p itemprop="startDate" content="2015-06-18T21:35:00+00:00">`
    
    This situation can easily be solved with the proposed code change.

--- templates/protostar/index.php
+++ templates/protostar/index.php
@@ -78,7 +78,7 @@ else
 }
 ?>
 <!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>" itemscope itemtype="http://schema.org/WebPage">
 <head>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<jdoc:include type="head" />
