480b36cac5b822f1c6b31985fcfbea6f1c376d4b
Jean-Marie Simonet <infografjms@gmail.com>
2015-06-19 10:35:49 +0200

    Adding a Limit Box to Languages Manager: Installed languages

--- administrator/components/com_languages/views/installed/tmpl/default.php
+++ administrator/components/com_languages/views/installed/tmpl/default.php
@@ -11,21 +11,30 @@ defined('_JEXEC') or die;
 
 // Add specific helper files for html generation
 JHtml::addIncludePath(JPATH_COMPONENT . '/helpers/html');
+
+JHtml::_('formbehavior.chosen', 'select');
+
 $user		= JFactory::getUser();
 $userId		= $user->get('id');
 $client		= $this->state->get('filter.client_id', 0) ? JText::_('JADMINISTRATOR') : JText::_('JSITE');
 $clientId	= $this->state->get('filter.client_id', 0);
 ?>
 <form action="<?php echo JRoute::_('index.php?option=com_languages&view=installed&client=' . $clientId); ?>" method="post" id="adminForm" name="adminForm">
-	<?php if (!empty( $this->sidebar)) : ?>
-		<div id="j-sidebar-container" class="span2">
-			<?php echo $this->sidebar; ?>
+<?php if (!empty( $this->sidebar)) : ?>
+	<div id="j-sidebar-container" class="span2">
+		<?php echo $this->sidebar; ?>
+	</div>
+	<div id="j-main-container" class="span10">
+<?php else : ?>
+	<div id="j-main-container">
+<?php endif;?>
+		<div id="filter-bar" class="btn-toolbar">
+			<div class="btn-group pull-right hidden-phone">
+				<label for="limit" class="element-invisible"><?php echo JText::_('JFIELD_PLG_SEARCH_SEARCHLIMIT_DESC'); ?></label>
+				<?php echo $this->pagination->getLimitBox(); ?>
+			</div>
 		</div>
-		<div id="j-main-container" class="span10">
-	<?php else : ?>
-		<div id="j-main-container">
-	<?php endif;?>
-
+		<div class="clearfix"> </div>
 		<table class="table table-striped">
 			<thead>
 				<tr>
