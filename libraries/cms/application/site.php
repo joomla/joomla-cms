faf800d0c86819d07f882f4c7088d4f01ae4a7cb
Viktor Vogel <admin@kubik-rubik.de>
2015-06-21 21:42:04 +0200

    Make sure only a valid template can be loaded

--- libraries/cms/application/site.php
+++ libraries/cms/application/site.php
@@ -509,7 +509,23 @@ final class JApplicationSite extends JApplicationCms
 		}
 
 		// Allows for overriding the active template from the request
-		$template->template = $this->input->getCmd('template', $template->template);
+		$template_override = $this->input->getCmd('template', '');
+
+		// If the template is still empty, then set our default value again
+		if (!empty($template_override))
+		{
+			if (file_exists(JPATH_THEMES . '/' . $template_override . '/index.php'))
+			{
+				foreach ($templates as $tmpl)
+				{
+					if ($tmpl->template == $template_override)
+					{
+						$template->template = $template_override;
+						break;
+					}
+				}
+			}
+		}
 
 		// Need to filter the default value as well
 		$template->template = JFilterInput::getInstance()->clean($template->template, 'cmd');
