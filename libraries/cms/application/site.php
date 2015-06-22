43201ff3420350599aa4e6d62810b48108cbbfcf
Viktor Vogel <admin@kubik-rubik.de>
2015-06-22 09:39:12 +0200

    Make Travis happy!

--- libraries/cms/application/site.php
+++ libraries/cms/application/site.php
@@ -511,7 +511,7 @@ final class JApplicationSite extends JApplicationCms
 		// Allows for overriding the active template from the request
 		$template_override = $this->input->getCmd('template', '');
 
-		// Only set template override if it is a valid template
+		// Only set template override if it is a valid template (= it exists and is enabled)
 		if (!empty($template_override))
 		{
 			if (file_exists(JPATH_THEMES . '/' . $template_override . '/index.php'))
