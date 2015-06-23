d50f02e54f9a60bc2651be768509c3f49131bc74
Brian Teeman <brian@teeman.net>
2015-06-23 11:25:26 +0100

    Update modal.php

--- administrator/components/com_users/views/users/tmpl/modal.php
+++ administrator/components/com_users/views/users/tmpl/modal.php
@@ -39,11 +39,11 @@ $listDirn  = $this->escape($this->state->get('list.direction'));
 			</div>
 		</div>
 	</fieldset>
-	<?php if (empty($this->items)) : ?>
-		<div class="alert alert-no-items">
-			<?php echo JText::_('JGLOBAL_NO_MATCHING_RESULTS'); ?>
-		</div>
-	<?php else : ?>
+		<?php if (empty($this->items)) : ?>
+			<div class="alert alert-no-items">
+				<?php echo JText::_('JGLOBAL_NO_MATCHING_RESULTS'); ?>
+			</div>
+		<?php else : ?>
 	<table class="table table-striped table-condensed">
 		<thead>
 			<tr>
