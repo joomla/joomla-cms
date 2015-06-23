585ab865a4c7fe7e58dc16f0407664d519a6014b
Sergio Manzi <smz@smz.it>
2015-06-23 04:25:18 +0200

    Fix for #7247

--- layouts/joomla/modal/main.php
+++ layouts/joomla/modal/main.php
@@ -60,7 +60,7 @@ if (isset($params['keyboard']))
  * Specific hack for Bootstrap 2.3.x
  */
 $script[] = "jQuery(document).ready(function($) {";
-$script[] = "   $('#" . $selector . "').on('show', function() {";
+$script[] = "   $('#" . $selector . "').on('shown', function() {";
 $script[] = "       $('body').addClass('modal-open');";
 
 if (isset($params['url']))
@@ -73,7 +73,7 @@ if (isset($params['url']))
 	$script[] = "       modalBody.prepend('" . trim($iframeHtml) . "');";
 }
 
-$script[] = "   }).on('hidden', function () {";
+$script[] = "   }).on('hide', function () {";
 $script[] = "       $('body').removeClass('modal-open');";
 $script[] = "   });";
 $script[] = "});";
