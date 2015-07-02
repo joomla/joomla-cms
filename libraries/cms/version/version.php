d6f6cc45c199922811d46f3feffec9a6a309f74d
Robert Deutz <rdeutz@googlemail.com>
2015-07-02 21:54:40 +0200

    set to development mode

--- libraries/cms/version/version.php
+++ libraries/cms/version/version.php
@@ -23,10 +23,10 @@ final class JVersion
 	public $RELEASE = '3.4';
 
 	/** @var  string  Maintenance version. */
-	public $DEV_LEVEL = '3';
+	public $DEV_LEVEL = '4-dev';
 
 	/** @var  string  Development STATUS. */
-	public $DEV_STATUS = 'Stable';
+	public $DEV_STATUS = 'development';
 
 	/** @var  string  Build number. */
 	public $BUILD = '';
