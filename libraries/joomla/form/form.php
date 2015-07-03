b7a1de1e5f5bd0b60f7cf8f937ee9a5da3558775
Fedik <getthesite@gmail.com>
2015-07-03 20:50:42 +0300

    Revert "Save empty multi-value fields as empty array (instead of not at all). Fixes #2617"
    This reverts commit df5c4847b45fa27a7414ea068b2c1d5e8f0f276b.

--- libraries/joomla/form/form.php
+++ libraries/joomla/form/form.php
@@ -235,22 +235,22 @@ class JForm
 			$groups = array_map('strval', $attrs ? $attrs : array());
 			$group = implode('.', $groups);
 
-			$key = $group ? $group . '.' . $name : $name;
-
-			// Filter the value if it exists.
-			if ($input->exists($key))
+			// Get the field value from the data input.
+			if ($group)
 			{
-				$output->set($key, $this->filterField($field, $input->get($key, (string) $field['default'])));
+				// Filter the value if it exists.
+				if ($input->exists($group . '.' . $name))
+				{
+					$output->set($group . '.' . $name, $this->filterField($field, $input->get($group . '.' . $name, (string) $field['default'])));
+				}
 			}
-
-			// Get the JFormField object for this field, only it knows if it is supposed to be multiple.
-			$jfield = $this->getField($name, $group);
-
-			// Fields supporting multiple values must be stored as empty arrays when no values are selected.
-			// If not, they will appear to be unset and then revert to their default value.
-			if ($jfield && $jfield->multiple && !$output->exists($key))
+			else
 			{
-				$output->set($key, array());
+				// Filter the value if it exists.
+				if ($input->exists($name))
+				{
+					$output->set($name, $this->filterField($field, $input->get($name, (string) $field['default'])));
+				}
 			}
 		}
 
