8d4ad5ab6d8149ef9d3243be0ff78d9e95768bbc
zero-24 <zero-24@users.noreply.github.com>
2015-07-05 20:45:06 +0200

    revert doc block change and comment script php

--- administrator/components/com_banners/models/fields/bannerclient.php
+++ administrator/components/com_banners/models/fields/bannerclient.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @package     Banners
+ * @package     Joomla.Administrator
  * @subpackage  com_banners
  *
  * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
