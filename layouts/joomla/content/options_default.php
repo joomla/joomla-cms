02cf6751f603360d5785aebb89d23bbd79fb1d60
zero-24 <zero-24@users.noreply.github.com>
2015-07-08 15:41:30 +0200

    Update options_default.php

--- layouts/joomla/content/options_default.php
+++ layouts/joomla/content/options_default.php
@@ -13,27 +13,25 @@ JHtml::_('behavior.framework');
 
 ?>
 <fieldset class="<?php echo !empty($displayData->formclass) ? $displayData->formclass : 'form-horizontal'; ?>">
-	<legend><?php echo $displayData->name ?></legend>
-	<?php if (!empty($displayData->description)): ?>
+	<legend><?php echo $displayData->name; ?></legend>
+	<?php if (!empty($displayData->description)) : ?>
 		<p><?php echo $displayData->description; ?></p>
 	<?php endif; ?>
 	
-	<?php
-	$fieldsnames = explode(',', $displayData->fieldsname);
-	foreach($fieldsnames as $fieldname) :
-		foreach ($displayData->form->getFieldset($fieldname) as $field) :
-			$classnames = 'control-group';
-			$rel = '';
-			$showon = $displayData->form->getFieldAttribute($field->fieldname, 'showon');
-			if (!empty($showon)) :
-				JHtml::_('jquery.framework');
-				JHtml::_('script', 'jui/cms.js', false, true);
-
-				$id = $displayData->form->getFormControl();
-				$showon = explode(':', $showon, 2);
-				$classnames .= ' showon_' . implode(' showon_', explode(',', $showon[1]));
-				$rel = ' rel="showon_' . $id . '['. $showon[0] . ']"';
-			endif; ?>
+	<?php $fieldsnames = explode(',', $displayData->fieldsname); ?>
+	<?php foreach($fieldsnames as $fieldname) : ?>
+		<?php foreach ($displayData->form->getFieldset($fieldname) as $field) : ?>
+			<?php $classnames = 'control-group'; ?>
+			<?php $rel = ''; ?>
+			<?php $showon = $displayData->form->getFieldAttribute($field->fieldname, 'showon'); ?>
+			<?php if (!empty($showon)) : ?>
+				<?php JHtml::_('jquery.framework'); ?>
+				<?php JHtml::_('script', 'jui/cms.js', false, true); ?>
+				<?php $id = $displayData->form->getFormControl(); ?>
+				<?php $showon = explode(':', $showon, 2); ?>
+				<?php $classnames .= ' showon_' . implode(' showon_', explode(',', $showon[1])); ?>
+				<?php $rel = ' rel="showon_' . $id . '['. $showon[0] . ']"'; ?>
+			<?php endif; ?>
 
 			<div class="<?php echo $classnames; ?>"<?php echo $rel; ?>>
 				<?php if (!isset($displayData->showlabel) || $displayData->showlabel): ?>
