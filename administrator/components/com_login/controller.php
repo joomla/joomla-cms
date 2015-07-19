9477d15a15d9e57d4759197e0f9be035cebd5e5a
Steven Pignataro <steven@corephp.com>
2015-07-19 16:20:57 -0500

    Fixed formatting again
    Signed-off-by: Steven Pignataro <steven@corephp.com>

--- administrator/components/com_login/controller.php
+++ administrator/components/com_login/controller.php
@@ -63,9 +63,11 @@ class LoginController extends JControllerLegacy
 			if (JUri::isInternal($return))
 			{
 				// If &tmpl=component - redirect to index.php
-				if(strpos($return, "tmpl=component") === false) {
+				if(strpos($return, "tmpl=component") === false)
+				{
 					$app->redirect($return);
-				} else {
+				} else
+				{
 					$app->redirect('index.php');
 				}
 			}
