87df7b822683b2ae504053cebcabd86a5b4c62d0
wilsonge <georgejameswilson@googlemail.com>
2015-07-20 23:38:21 +0100

    Add some more JEditor Unit tests

--- tests/unit/suites/libraries/cms/editor/JEditorTest.php
+++ tests/unit/suites/libraries/cms/editor/JEditorTest.php
@@ -7,6 +7,8 @@
  * @license	    GNU General Public License version 2 or later; see LICENSE
  */
 
+require_once __DIR__ . '/stubs/EditorObserver.php';
+
 /**
  * Test class for JEditor.
  *
@@ -43,12 +45,13 @@ class JEditorTest extends PHPUnit_Framework_TestCase
 	 * @return  void
 	 *
 	 * @since   3.0
+	 * @covers JEditor::getInstance
 	 */
 	public function testGetInstance()
 	{
-		$this->assertThat(
-			JEditor::getInstance('none'),
-			$this->isInstanceOf('JEditor')
+		$this->assertInstanceOf(
+			'JEditor',
+			JEditor::getInstance('none')
 		);
 	}
 
@@ -58,15 +61,147 @@ class JEditorTest extends PHPUnit_Framework_TestCase
 	 * @return  void
 	 *
 	 * @since   3.0
+	 * @covers JEditor::getInstance
 	 */
 	public function testGetState()
 	{
 		// Preload the state to test it
 		TestReflection::setValue($this->object, '_state', 'JEditor::getState()');
 
-		$this->assertThat(
-			$this->object->getState(),
-			$this->equalTo('JEditor::getState()')
+		$this->assertEquals(
+			'JEditor::getState()',
+			$this->object->getState()
+		);
+	}
+
+	/**
+	 * @testdox Test attaching a single closure as an observer in the JEditor class
+	 *
+	 * @since  3.4.4
+	 */
+	public function testAttachWithClosure()
+	{
+		$testObserver = array(
+			'event' => 'onInit',
+			'handler' => function () {
+				return 'teststring';
+			}
+		);
+		$this->object->attach($testObserver);
+
+		$this->assertEquals(
+			array($testObserver),
+			TestReflection::getValue($this->object, '_observers'),
+			'Observer was not attached to the editor'
+		);
+
+		$this->assertEquals(
+			array(
+				'oninit' => array(
+					0 => 0
+				)
+			),
+			TestReflection::getValue($this->object, '_methods'),
+			'The method for the test observer was not stored correctly'
+		);
+	}
+
+	/**
+	 * @testdox Test attaching multiple closures as observers in the JEditor class using the same event names
+	 *
+	 * @since  3.4.4
+	 */
+	public function testAttachWithMultipleClosuresForSameEvent()
+	{
+		$testObserver = array(
+			'event' => 'onInit',
+			'handler' => function () {
+				return 'teststring';
+			}
+		);
+		$testObserver2 = array(
+			'event' => 'onInit',
+			'handler' => function () {
+				return 'secondTestString';
+			}
+		);
+		$this->object->attach($testObserver);
+		$this->object->attach($testObserver2);
+
+		$this->assertEquals(
+			array($testObserver, $testObserver2),
+			TestReflection::getValue($this->object, '_observers'),
+			'Observers were not attached to the editor'
+		);
+
+		$this->assertEquals(
+			array(
+				'oninit' => array(
+					0 => 0,
+					1 => 1,
+				)
+			),
+			TestReflection::getValue($this->object, '_methods'),
+			'The methods for the test observers were not stored correctly'
+		);
+	}
+
+	/**
+	 * @testdox Test attaching multiple closures as observers in the JEditor class with different event names
+	 *
+	 * @since  3.4.4
+	 */
+	public function testAttachWithMultipleClosuresForDifferentEvents()
+	{
+		$testObserver = array(
+			'event' => 'onInit',
+			'handler' => function () {
+				return 'teststring';
+			}
+		);
+		$testObserver2 = array(
+			'event' => 'onAfterStuff',
+			'handler' => function () {
+				return 'secondTestString';
+			}
+		);
+		$this->object->attach($testObserver);
+		$this->object->attach($testObserver2);
+
+		$this->assertEquals(
+			array($testObserver, $testObserver2),
+			TestReflection::getValue($this->object, '_observers'),
+			'Observers were not attached to the editor'
+		);
+
+		$this->assertEquals(
+			array(
+				'oninit' => array(
+					0 => 0,
+				),
+				'onafterstuff' => array(
+					0 => 1
+				)
+			),
+			TestReflection::getValue($this->object, '_methods'),
+			'The methods for the test observers were not stored correctly'
+		);
+	}
+
+	/**
+	 * @testdox Test an observer object is correctly stored in the JEditor class
+	 *
+	 * @since  3.4.4
+	 */
+	public function testAttachWithClass()
+	{
+		$testObserver = new EditorObserver;
+		$this->object->attach($testObserver);
+
+		$this->assertEquals(
+			array($testObserver),
+			TestReflection::getValue($this->object, '_observers'),
+			'The observer as a class was not attached to the editor'
 		);
 	}
 }
