cab9868fe53b5b77f7b58492d80001bd90c469aa
Peter van Westen <peter@nonumber.nl>
2015-07-21 18:41:48 +0200

    [codestyle] Removed redundant nesting

--- administrator/components/com_installer/models/extension.php
+++ administrator/components/com_installer/models/extension.php
@@ -113,22 +113,17 @@ class InstallerModel extends JModelList
 
 		foreach ($items as &$item)
 		{
-			if (strlen($item->manifest_cache))
+			if (strlen($item->manifest_cache) && $data = json_decode($item->manifest_cache))
 			{
-				$data = json_decode($item->manifest_cache);
-
-				if ($data)
+				foreach ($data as $key => $value)
 				{
-					foreach ($data as $key => $value)
+					if ($key == 'type')
 					{
-						if ($key == 'type')
-						{
-							// Ignore the type field
-							continue;
-						}
-
-						$item->$key = $value;
+						// Ignore the type field
+						continue;
 					}
+
+					$item->$key = $value;
 				}
 			}
 
