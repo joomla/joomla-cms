902e0e45b84b0ebe96730639633f72b0febba4e7
Pagchen <pagchen@designdesite.com>
2015-07-28 14:06:48 -0400

    Update view.html.php
    Unlock Button not showing if user just have admin.manage rights

--- administrator/components/com_checkin/views/checkin/view.html.php
+++ administrator/components/com_checkin/views/checkin/view.html.php
@@ -55,13 +55,13 @@ class CheckinViewCheckin extends JViewLegacy
 	{
 		JToolbarHelper::title(JText::_('COM_CHECKIN_GLOBAL_CHECK_IN'), 'checkin');
 
-		if (JFactory::getUser()->authorise('core.admin', 'com_checkin'))
-		{
 			JToolbarHelper::custom('checkin', 'checkin.png', 'checkin_f2.png', 'JTOOLBAR_CHECKIN', true);
-			JToolbarHelper::divider();
-			JToolbarHelper::preferences('com_checkin');
-			JToolbarHelper::divider();
-		}
+			if (JFactory::getUser()->authorise('core.admin', 'com_checkin'))
+			{
+				JToolbarHelper::divider();
+				JToolbarHelper::preferences('com_checkin');
+				JToolbarHelper::divider();
+			}
 
 		JToolbarHelper::help('JHELP_SITE_MAINTENANCE_GLOBAL_CHECK-IN');
 	}
