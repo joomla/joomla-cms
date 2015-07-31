5c43c9e6c17d67346b16ac54fa53e47d99dc21b5
Brian Teeman <brian@teeman.net>
2015-07-31 09:39:44 +0100

    tinymce - toolbar tidyup

--- plugins/editors/tinymce/tinymce.php
+++ plugins/editors/tinymce/tinymce.php
@@ -616,10 +616,10 @@ class PlgEditorTinymce extends JPlugin
 		$elements = implode(',', $elements);
 
 		// Prepare config variables
-		$toolbar1 = implode(' ', $toolbar1_add);
-		$toolbar2 = implode(' ', $toolbar2_add);
-		$toolbar3 = implode(' ', $toolbar3_add);
-		$toolbar4 = implode(' ', $toolbar4_add);
+		$toolbar1 = implode(' ', $toolbar1_add) . implode(' ', $toolbar2_add) . implode(' ', $toolbar3_add) . implode(' ', $toolbar4_add);
+		$toolbar3 = '';
+		$toolbar4 = '';
+		$toolbar2 = '';
 
 		// See if mobileVersion is activated
 		$mobileVersion = $this->params->get('mobile', 0);
@@ -681,8 +681,7 @@ class PlgEditorTinymce extends JPlugin
 
 			case 1:
 			default: /* Advanced mode*/
-				$toolbar1 = "bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | formatselect | bullist numlist";
-				$toolbar2 = "outdent indent | undo redo | link unlink anchor image code | hr table | subscript superscript | charmap";
+				$toolbar1 = "bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | formatselect | bullist numlist | outdent indent | undo redo | link unlink anchor image code | hr table | subscript superscript | charmap";
 				$return = $load .
 					"\t<script type=\"text/javascript\">
 				tinyMCE.init({
@@ -708,7 +707,6 @@ class PlgEditorTinymce extends JPlugin
 					plugins : \"table link image code hr charmap autolink lists importcss\",
 					// Toolbar
 					toolbar1: \"$toolbar1\",
-					toolbar2: \"$toolbar2\",
 					removed_menuitems: \"newdocument\",
 					// URL
 					relative_urls : $relative_urls,
