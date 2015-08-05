e61d1f9d0d89da3cd12978a08a80e767bf261373
Brian Teeman <brian@teeman.net>
2015-08-05 16:21:08 +0100

    com_categories and com_menus

--- administrator/components/com_menus/views/items/tmpl/default_batch_body.php
+++ administrator/components/com_menus/views/items/tmpl/default_batch_body.php
@@ -35,12 +35,13 @@ $published = $this->state->get('filter.published');
 			</label>
 			<div class="controls">
 				<select name="batch[menu_id]" id="batch-menu-id">
-					<option value=""><?php echo JText::_('JSELECT') ?></option>
+					<option value=""><?php echo JText::_('JLIB_HTML_BATCH_NO_CATEGORY') ?></option>
 					<?php echo JHtml::_('select.options', JHtml::_('menu.menuitems', array('published' => $published))); ?>
 				</select>
 			</div>
 		</div>
 		<div id="batch-copy-move" class="control-group radio">
+			<?php echo JText::_('JLIB_HTML_BATCH_MOVE_QUESTION'); ?>
 			<?php echo JHtml::_('select.radiolist', $options, 'batch[move_copy]', '', 'value', 'text', 'm'); ?>
 		</div>
 	<?php endif; ?>
