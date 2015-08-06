7ee14ad2173ec0be3e488a93aac38ba9787616dd
nonumber <peter@nonumber.nl>
2015-08-06 12:31:08 +0200

    Fixed merge conflicts

--- administrator/components/com_media/views/media/tmpl/default.php
+++ administrator/components/com_media/views/media/tmpl/default.php
@@ -14,16 +14,21 @@ $input = JFactory::getApplication()->input;
 ?>
 <div class="row-fluid">
 	<!-- Begin Sidebar -->
-	<div class="span2">
-		<div id="treeview">
+	<div id="j-sidebar-container" class="span2">
+		<?php echo $this->sidebar; ?>
+		<div class="j-toggle-sidebar-header">
+		<h3 style="padding-left: 10px;"><?php echo JText::_('COM_MEDIA_FOLDERS');?> </h3>
+		</div>
+		<div id="treeview" class="sidebar">
 			<div id="media-tree_tree" class="sidebar-nav">
 				<?php echo $this->loadTemplate('folders'); ?>
 			</div>
 		</div>
 	</div>
 	<!-- End Sidebar -->
+
 	<!-- Begin Content -->
-	<div class="span10">
+	<div id="j-main-container" class="span10">
 		<?php echo $this->loadTemplate('navigation'); ?>
 		<?php if (($user->authorise('core.create', 'com_media')) and $this->require_ftp) : ?>
 			<form action="index.php?option=com_media&amp;task=ftpValidate" name="ftpForm" id="ftpForm" method="post">
