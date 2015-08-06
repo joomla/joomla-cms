9c1dbca85814f4a6d522f7bb86dc5e63a44a31e0
Jisse Reitsma <jisse@yireo.com>
2015-08-06 17:12:11 +0200

    Fix merge issue

--- administrator/components/com_media/views/medialist/tmpl/details_folder.php
+++ administrator/components/com_media/views/medialist/tmpl/details_folder.php
@@ -13,7 +13,6 @@ $user = JFactory::getUser();
 JHtml::_('bootstrap.tooltip');
 ?>
 <tr>
-<<<<<<< HEAD
 	<?php
 	$data = array(
 		'item' => $this->_tmp_img,
@@ -22,29 +21,18 @@ JHtml::_('bootstrap.tooltip');
 	?>
 	<td class="imgTotal">
 		<a href="index.php?option=com_media&amp;view=mediaList&amp;tmpl=component&amp;folder=<?php echo $this->_tmp_folder->path_relative; ?>" target="folderframe">
-			<i class="icon-folder-2"></i></a>
-=======
-	<td class="imgTotal">
-		<a href="index.php?option=com_media&amp;view=mediaList&amp;tmpl=component&amp;folder=<?php echo $this->_tmp_folder->path_relative; ?>" target="folderframe">
-			<span class="icon-folder-2"></span></a>
->>>>>>> upstream/3.5-dev
+			<span class="icon-folder-2"></span>
+        </a>
 	</td>
 	<td class="description">
-		<a href="index.php?option=com_media&amp;view=mediaList&amp;tmpl=component&amp;folder=<?php echo $this->_tmp_folder->path_relative; ?>" target="folderframe"><?php echo $this->_tmp_folder->name; ?></a>
+		<a href="index.php?option=com_media&amp;view=mediaList&amp;tmpl=component&amp;folder=<?php echo $this->_tmp_folder->path_relative; ?>" target="folderframe">
+            <?php echo $this->_tmp_folder->name; ?>
+        </a>
 	</td>
-	<td>&#160;
-
+	<td>
+        &#160;
 	</td>
-	<td>&#160;
-
+	<td>
+        &#160;
 	</td>
-<<<<<<< HEAD
-=======
-	<?php if ($user->authorise('core.delete', 'com_media')):?>
-		<td>
-			<a class="delete-item" target="_top" href="index.php?option=com_media&amp;task=folder.delete&amp;tmpl=index&amp;folder=<?php echo $this->state->folder; ?>&amp;<?php echo JSession::getFormToken(); ?>=1&amp;rm[]=<?php echo $this->_tmp_folder->name; ?>" rel="<?php echo $this->_tmp_folder->name; ?>' :: <?php echo $this->_tmp_folder->files + $this->_tmp_folder->folders; ?>"><span class="icon-remove hasTooltip" title="<?php echo JHtml::tooltipText('JACTION_DELETE');?>"></span></a>
-			<input type="checkbox" name="rm[]" value="<?php echo $this->_tmp_folder->name; ?>" />
-		</td>
-	<?php endif;?>
->>>>>>> upstream/3.5-dev
 </tr>
