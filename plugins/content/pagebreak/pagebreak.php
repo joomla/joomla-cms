ebf08718ff6532cbc27635e508d8210bd7a31069
Michael Babker <mbabker@flbab.com>
2015-08-08 15:05:52 -0400

    Add active class for all pages too

--- plugins/content/pagebreak/pagebreak.php
+++ plugins/content/pagebreak/pagebreak.php
@@ -317,17 +317,11 @@ class PlgContentPagebreak extends JPlugin
 
 		if ($this->params->get('showall'))
 		{
-			$link = JRoute::_(ContentHelperRoute::getArticleRoute($row->slug, $row->catid, $row->language) . '&showall=1&limitstart=');
-			$class = ($showall == 1) ? 'toclink active' : 'toclink';
-			$row->toc .= '
-			<li>
-
-					<a href="' . $link . '" class="' . $class . '">'
-					. JText::_('PLG_CONTENT_PAGEBREAK_ALL_PAGES') .
-					'</a>
-
-			</li>
-			';
+			$link    = JRoute::_(ContentHelperRoute::getArticleRoute($row->slug, $row->catid, $row->language) . '&showall=1&limitstart=');
+			$liClass = ($limitstart == $i - 1) ? ' class="active"' : '';
+			$class   = ($limitstart == $i - 1) ? 'toclink active' : 'toclink';
+			$row->toc .= '<li' . $liClass . '><a href="' . $link . '" class="' . $class . '">'
+				. JText::_('PLG_CONTENT_PAGEBREAK_ALL_PAGES') . '</a></li>';
 		}
 
 		$row->toc .= '</ul></div>';
