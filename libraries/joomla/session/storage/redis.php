42eb19108d59d37607ece6a3c46fbe5c3a605284
alikon <extensions4joomla@gmail.com>
2015-08-11 08:36:42 +0200

    libraries/joomla/session/storage/redis.php travis cs fix

--- libraries/joomla/session/storage/redis.php
+++ libraries/joomla/session/storage/redis.php
@@ -14,7 +14,7 @@ defined('JPATH_PLATFORM') or die;
  *
  * @see    http://www.php.net/manual/en/function.session-set-save-handler.php
  * @see    https://github.com/phpredis/phpredis#php-session-handler
- * @since  12.1
+ * @since  3.5
  */
 class JSessionStorageRedis extends JSessionStorage
 {
@@ -25,17 +25,17 @@ class JSessionStorageRedis extends JSessionStorage
 	 *
 	 * @return  string  The session data.
 	 *
-	 * @since   11.1
+	 * @since   3.5
 	 */
 	public function read($id)
 	{
- 
 		// Get the databstore connection object and verify its connected.
 		$ds = JFactory::getDso();
+
 		try
 		{
-			// Get the session data from the datastore.	
-			$data= $ds->get('sess-'.$id);  		  
+			// Get the session data from the datastore.
+			$data   = $ds->get('sess-' . $id);  
 			$result = json_decode($data);
 			$result = str_replace('\0\0\0', chr(0) . '*' . chr(0), $result);
 
@@ -43,10 +43,9 @@ class JSessionStorageRedis extends JSessionStorage
 		}
 		catch (Exception $e)
 		{
-			throw new RuntimeException(JText::_('JERROR_SESSION_redis_read'));					 
+			throw new RuntimeException(JText::_('JERROR_SESSION_REDIS_READ'));
 			return false;
-		}
-	 
+		}	 
 	}
 
 	/**
@@ -57,49 +56,51 @@ class JSessionStorageRedis extends JSessionStorage
 	 *
 	 * @return  boolean  True on success, false otherwise.
 	 *
-	 * @since   11.1
+	 * @since   3.5
 	 */
 	public function write($id, $data)
 	{
 		// Get the datastore connection object and verify its connected.
-		
-		$ds = JFactory::getDso();
-				
-		$db = JFactory::getDbo();
-		$user = JFactory::getUser();
-				
-		$data = str_replace(chr(0) . '*' . chr(0), '\0\0\0', $data);
-		$key = 'sess-'.$id;
-	    $key4sessionuid = 'sessid-'.(int) $user->get('id').'-'.(int) JFactory::getApplication()->getClientId();
-		$key_uname = 'user-'.$user->get('username');
-		$jsonValue = json_encode($data);
+		$ds             = JFactory::getDso();
+		$db             = JFactory::getDbo();
+		$user           = JFactory::getUser();
+		$data           = str_replace(chr(0) . '*' . chr(0), '\0\0\0', $data);
+		$key            = 'sess-' . $id;
+		$key4sessionuid = 'sessid-' . (int) $user->get('id') . '-' . (int) JFactory::getApplication()->getClientId();
+		$key_uname      = 'user-' . $user->get('username');
+		$jsonValue      = json_encode($data);
+
 		// Calculate the session lifetime.
 		$lifetime = ((JFactory::getConfig()->get('lifetime')) ? JFactory::getConfig()->get('lifetime') * 60 : 900);
-		 
+
 		try
-		{   
-			$ds->setex( $key, $lifetime, $jsonValue );
-			
-			if ($user->get('id')>0)
-			{ 
-				$hash = array( 'client_id' => (int) JFactory::getApplication()->getClientId(),
-				              'guest' => $db->quote($user->get('guest')), 
-				              'time' => (int) JFactory::getSession()->get('session.timer.start'), 
-				              'userid' => (int) $user->get('id'),
-				              'username' => $db->quote($user->get('username'))
-				              );	
-				$jsonValue = json_encode($hash);												
-				$ds->setex( $key4sessionuid, $lifetime, $key );
-				$ds->setex( $key_uname, $lifetime, $jsonValue );
-				$ds->setex( $user->username, $lifetime, $user->id);
-				$ds->setex( $user->id, $lifetime, $user->username);		
-				$ds->sadd( 'utenti', $user->username );
-			}	
+		{ 
+			$ds->setex($key, $lifetime, $jsonValue);
+
+			if ($user->get('id') > 0)
+			{
+				$hash = array(
+					'client_id' => (int) JFactory::getApplication()->getClientId(),
+					'guest' => $db->quote($user->get('guest')),
+					'time' => (int) JFactory::getSession()->get('session.timer.start'),
+					'userid' => (int) $user->get('id'),
+					'username' => $db->quote($user->get('username')),
+				);
+
+				$jsonValue = json_encode($hash);
+				$ds->setex($key4sessionuid, $lifetime, $key);
+				$ds->setex($key_uname, $lifetime, $jsonValue);
+				$ds->setex($user->username, $lifetime, $user->id);
+				$ds->setex($user->id, $lifetime, $user->username);
+				$ds->sadd('utenti', $user->username);
+			}
+
 			return true;
 		}
 		catch (Exception $e)
-		{				
-			throw new RuntimeException(JText::_('JERROR_SESSION_redis_write'));			
+		{
+			throw new RuntimeException(JText::_('JERROR_SESSION_redis_write'));
+
 			return false;
 		}
 	}
@@ -111,11 +112,10 @@ class JSessionStorageRedis extends JSessionStorage
 	 *
 	 * @return  boolean  True on success, false otherwise.
 	 *
-	 * @since   11.1
+	 * @since   3.5
 	 */
 	public function destroy($id)
 	{
- 
 		$ds = JFactory::getDso();
 
 		try
@@ -123,9 +123,10 @@ class JSessionStorageRedis extends JSessionStorage
 			$ds->delete($id);
 		}
 		catch (Exception $e)
-		{			
-			throw new RuntimeException(JText::_('JERROR_SESSION_redis_destroy'));			
-			return false;			
+		{
+			throw new RuntimeException(JText::_('JERROR_SESSION_REDIS_DESTROY'));
+
+			return false;
 		}
 	}
 
@@ -136,43 +137,42 @@ class JSessionStorageRedis extends JSessionStorage
 	 *
 	 * @return  boolean  True on success, false otherwise.
 	 *
-	 * @since   11.1
+	 * @since   3.5
 	 */
 	public function gc($lifetime = 1440)
 	{
- 
 		$ds = JFactory::getDso();
+
 		try
 		{
 			$lista = $ds->smembers('utenti');
-		
+
 		}
 		catch (Exception $e)
 		{
-			throw new RuntimeException(JText::_('JERROR_SESSION_redis_destroy'));
-			
-			
-			return false;			
+			throw new RuntimeException(JText::_('JERROR_SESSION_REDIS_DESTROY'));
+
+			return false;
 		}
-		// 
+
 		foreach ($lista as $elm)
 		{
 			try
 			{
 				$exist = $ds->ttl($elm);
-		
 			}
 			catch (Exception $e)
 			{
-				throw new RuntimeException(JText::_('JERROR_SESSION_redis_destroy'));					
-				return false;			
+				throw new RuntimeException(JText::_('JERROR_SESSION_REDIS_DESTROY'));
+
+				return false;
 			}
-		}	
+		}
+
 		if ($exist == -1)
 		{
-			$ds->srem('utenti', $elm);		
-			$ds->delete('user-'.$elm);
+			$ds->srem('utenti', $elm);
+			$ds->delete('user-' . $elm);
 		}
-	
 	}
 }
