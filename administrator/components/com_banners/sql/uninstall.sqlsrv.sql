18fc9be38b127e5744f8a95b68403172ec0a062a
zero-24 <zero-24@users.noreply.github.com>
2015-09-02 09:06:44 +0200

    Update uninstall.sqlsrv.sql

--- administrator/components/com_banners/sql/uninstall.sqlsrv.sql
+++ administrator/components/com_banners/sql/uninstall.sqlsrv.sql
@@ -1,8 +1,8 @@
 DELETE FROM [#__content_types] WHERE [type_alias] IN ('com_banners.banner', 'com_banners.category', 'com_banners.client');
 
-DROP TABLE IF EXISTS `#__banners`;
+DROP TABLE IF EXISTS [#__banners];
 
-DROP TABLE IF EXISTS `#__banner_clients`;
+DROP TABLE IF EXISTS [#__banner_clients];
 
-DROP TABLE IF EXISTS `#__banner_tracks`;
+DROP TABLE IF EXISTS [#__banner_tracks];
 
