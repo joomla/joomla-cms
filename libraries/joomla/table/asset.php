5d699e4bcd08887e95226702eb6d29067f07579f
alikon <extensions4joomla@gmail.com>
2015-09-08 07:54:31 +0200

    merge with #7778

--- libraries/joomla/table/asset.php
+++ libraries/joomla/table/asset.php
@@ -98,7 +98,11 @@ class JTableAsset extends JTableNested
 	public function check()
 	{
 		$this->parent_id = (int) $this->parent_id;
-
+		
+		if (empty($this->rules))
+		{
+			$this->rules = '{}';
+		}
 		// JTableNested does not allow parent_id = 0, override this.
 		if ($this->parent_id > 0)
 		{
@@ -118,10 +122,7 @@ class JTableAsset extends JTableNested
 				$this->setError('Invalid Parent ID');
 
 				return false;
-			}
-			if (empty($this->rules))
-			{
-				$this->rules = '{}';
+
 			}
 		}
 
