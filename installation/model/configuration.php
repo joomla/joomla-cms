b766bd0dec71071310e9718f74e7c385d5667479
zero-24 <zero-24@users.noreply.github.com>
2015-09-19 23:28:16 +0200

    Just fixing the whitespace error

--- installation/model/configuration.php
+++ installation/model/configuration.php
@@ -135,7 +135,7 @@ class InstallationModelConfiguration extends JModelBase
 		// Feed settings.
 		$registry->set('feed_limit', 10);
 		$registry->set('feed_email', 'author');
-		
+
 		$registry->set('log_path', JPATH_ROOT . '/logs');
 		$registry->set('tmp_path', JPATH_ROOT . '/tmp');
 
