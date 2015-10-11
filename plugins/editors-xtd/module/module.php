0b1c99345fded4b5243523e9329517a1d39fd82b
dgt41 <d.grammatiko@gmail.com>
2015-10-11 18:33:36 +0300

    CS Thanks @zero-24

--- plugins/editors-xtd/module/module.php
+++ plugins/editors-xtd/module/module.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package     Joomla.Plugin
- * @subpackage  Editors-xtd.article
+ * @subpackage  Editors-xtd.module
  *
  * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
