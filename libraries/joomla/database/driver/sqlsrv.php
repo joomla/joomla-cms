c8c2cf424decb1d28e5bee763c47f4a6b5b49d56
zero-24 <zero-24@users.noreply.github.com>
2015-10-23 12:48:22 +0200

    Update sqlsrv.php

--- libraries/joomla/database/driver/sqlsrv.php
+++ libraries/joomla/database/driver/sqlsrv.php
@@ -633,10 +633,10 @@ class JDatabaseDriverSqlsrv extends JDatabaseDriver
 		// If an error occurred handle it.
 		if (!$this->cursor)
 		{
-			// Get the error number and message.
-			$errors         = sqlsrv_errors();
-			$this->errorNum = $errors[0]['SQLSTATE'];
-			$errorMessage   = (string) $errors[0]['message'];
+			// Get the error number and message before we execute any more queries.
+			$errors       = sqlsrv_errors();
+			$errorNum     = $errors[0]['SQLSTATE'];
+			$errorMessage = (string) $errors[0]['message'];
 
 			// Replace the Databaseprefix with `#__` if we are not in Debug
 			if (!$this->debug)
@@ -645,7 +645,7 @@ class JDatabaseDriverSqlsrv extends JDatabaseDriver
 				$errorMessage = str_replace($this->tablePrefix, '#__', $errorMessage);
 			}
 
-			$this->errorMsg = $errorMessage . 'SQL=' . $query;
+			$errorMsg = $errorMessage . 'SQL=' . $query;
 
 			// Check if the server was disconnected.
 			if (!$this->connected())
@@ -659,6 +659,20 @@ class JDatabaseDriverSqlsrv extends JDatabaseDriver
 				// If connect fails, ignore that exception and throw the normal exception.
 				catch (RuntimeException $e)
 				{
+					// Get the error number and message.
+					$errors         = sqlsrv_errors();
+					$this->errorNum = $errors[0]['SQLSTATE'];
+					$errorMessage   = (string) $errors[0]['message'];
+		
+					// Replace the Databaseprefix with `#__` if we are not in Debug
+					if (!$this->debug)
+					{
+						$query        = str_replace($this->tablePrefix, '#__', $query);
+						$errorMessage = str_replace($this->tablePrefix, '#__', $errorMessage);
+					}
+		
+					$this->errorMsg = $errorMessage . 'SQL=' . $query;
+
 					// Throw the normal query exception.
 					JLog::add(JText::sprintf('JLIB_DATABASE_QUERY_FAILED', $this->errorNum, $this->errorMsg), JLog::ERROR, 'database-error');
 
@@ -671,6 +685,10 @@ class JDatabaseDriverSqlsrv extends JDatabaseDriver
 			// The server was not disconnected.
 			else
 			{
+				// Get the error number and message from before we tried to reconnect.
+				$this->errorNum = $errorNum;
+				$this->errorMsg = $errorMsg;
+
 				// Throw the normal query exception.
 				JLog::add(JText::sprintf('JLIB_DATABASE_QUERY_FAILED', $this->errorNum, $this->errorMsg), JLog::ERROR, 'database-error');
 
