9f95e5712c264b64c6c7bd940c511eba06fa038c
zero-24 <zero-24@users.noreply.github.com>
2015-10-23 11:20:39 +0200

    Update pdo.php

--- libraries/joomla/database/driver/pdo.php
+++ libraries/joomla/database/driver/pdo.php
@@ -438,8 +438,18 @@ abstract class JDatabaseDriverPdo extends JDatabaseDriver
 		if (!$this->executed)
 		{
 			// Get the error number and message before we execute any more queries.
-			$errorNum = (int) $this->connection->errorCode();
-			$errorMsg = (string) 'SQL: ' . implode(", ", $this->connection->errorInfo());
+			$this->errorNum = (int) $this->connection->errorCode();
+
+			// The SQL Error Information
+			$query = implode(", ", $this->connection->errorInfo();
+
+			// Replace the Databaseprefix with `#__` if we are not in Debug
+			if (!$this->debug)
+			{
+				$query = str_replace($this->tablePrefix, '#__', $query);
+			}
+
+			$this->errorMsg = (string) 'SQL: ' . $query);
 
 			// Check if the server was disconnected.
 			if (!$this->connected())
@@ -453,10 +463,6 @@ abstract class JDatabaseDriverPdo extends JDatabaseDriver
 				// If connect fails, ignore that exception and throw the normal exception.
 				catch (RuntimeException $e)
 				{
-					// Get the error number and message.
-					$this->errorNum = (int) $this->connection->errorCode();
-					$this->errorMsg = (string) 'SQL: ' . implode(", ", $this->connection->errorInfo());
-
 					// Throw the normal query exception.
 					JLog::add(JText::sprintf('JLIB_DATABASE_QUERY_FAILED', $this->errorNum, $this->errorMsg), JLog::ERROR, 'database-error');
 
@@ -469,10 +475,6 @@ abstract class JDatabaseDriverPdo extends JDatabaseDriver
 			// The server was not disconnected.
 			else
 			{
-				// Get the error number and message from before we tried to reconnect.
-				$this->errorNum = $errorNum;
-				$this->errorMsg = $errorMsg;
-
 				// Throw the normal query exception.
 				JLog::add(JText::sprintf('JLIB_DATABASE_QUERY_FAILED', $this->errorNum, $this->errorMsg), JLog::ERROR, 'database-error');
 
