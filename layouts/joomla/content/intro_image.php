0321f39bee0a38ec28b965b38053e4ba2ab5856b
zero-24 <zero-24@users.noreply.github.com>
2015-10-25 13:59:55 +0100

    add image linking back

--- layouts/joomla/content/intro_image.php
+++ layouts/joomla/content/intro_image.php
@@ -16,11 +16,12 @@ $images = json_decode($displayData->images);
 <?php if (isset($images->image_intro) && !empty($images->image_intro)) : ?>
 	<?php $imgfloat = (empty($images->float_intro)) ? $params->get('float_intro') : $images->float_intro; ?>
 	<figure class="pull-<?php echo htmlspecialchars($imgfloat); ?> item-image">
-		<img
+		<?php if ($params->get('link_titles') && $params->get('access-view')) : ?>
+		<a href="<?php echo JRoute::_(ContentHelperRoute::getArticleRoute($displayData->slug, $displayData->catid, $displayData->language)); ?>"><img
 		<?php if ($images->image_intro_caption) : ?>
-			<?php echo 'title="' .htmlspecialchars($images->image_intro_caption) .'"'; ?>
+			<?php echo 'title="' . htmlspecialchars($images->image_intro_caption) .'"'; ?>
 		<?php endif; ?>
-		src="<?php echo htmlspecialchars($images->image_intro); ?>" alt="<?php echo htmlspecialchars($images->image_intro_alt); ?>"/>
+		src="<?php echo htmlspecialchars($images->image_intro); ?>" alt="<?php echo htmlspecialchars($images->image_intro_alt); ?>"/></a>
 		<?php if ($images->image_intro_caption) : ?>
 			<figcaption><?php echo htmlspecialchars($images->image_intro_caption); ?></figcaption>
 		<?php endif; ?>
