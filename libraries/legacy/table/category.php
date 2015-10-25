7ea3e29ea859b9e1fb4482b5d2000380d6c68492
astridx <astrid.guenther@dimna.de>
2015-10-25 15:22:07 +0100

    Deleted the spaces Travis found ...

--- libraries/legacy/table/category.php
+++ libraries/legacy/table/category.php
@@ -236,7 +236,7 @@ class JTableCategory extends JTableNested
 		}
 
 		// Verify if the alias starts with a number and send a notice in this case.
-		if (preg_match('/^\d/', $this->alias) === 1)
+		if(preg_match('/^\d/', $this->alias) === 1)
 		{
 			JError::raiseNotice( 100, JText::_('JLIB_DATABASE_ERROR_ALIAS_STARTS_WITH_A_NUMBER') );
 		}
