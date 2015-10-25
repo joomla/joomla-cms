349f38d98c72b7437020118bf99d4ea23465c29b
zero-24 <zero-24@users.noreply.github.com>
2015-10-25 14:10:02 +0100

    fix the links

--- layouts/joomla/content/intro_image.php
+++ layouts/joomla/content/intro_image.php
@@ -16,14 +16,17 @@ $images = json_decode($displayData->images);
 <?php if (isset($images->image_intro) && !empty($images->image_intro)) : ?>
 	<?php $imgfloat = (empty($images->float_intro)) ? $params->get('float_intro') : $images->float_intro; ?>
 	<figure class="pull-<?php echo htmlspecialchars($imgfloat); ?> item-image">
-		<?php if ($params->get('link_titles') && $params->get('access-view')) : ?>
-		<a href="<?php echo JRoute::_(ContentHelperRoute::getArticleRoute($displayData->slug, $displayData->catid, $displayData->language)); ?>"><img
-		<?php if ($images->image_intro_caption) : ?>
-			<?php echo 'title="' . htmlspecialchars($images->image_intro_caption) .'"'; ?>
-		<?php endif; ?>
-		src="<?php echo htmlspecialchars($images->image_intro); ?>" alt="<?php echo htmlspecialchars($images->image_intro_alt); ?>"/></a>
+	<?php if ($params->get('link_titles') && $params->get('access-view')) : ?>
+	<a href="<?php echo JRoute::_(ContentHelperRoute::getArticleRoute($displayData->slug, $displayData->catid, $displayData->language)); ?>">
 		<?php if ($images->image_intro_caption) : ?>
 			<figcaption><?php echo htmlspecialchars($images->image_intro_caption); ?></figcaption>
 		<?php endif; ?>
+		<img src="<?php echo htmlspecialchars($images->image_intro); ?>" alt="<?php echo htmlspecialchars($images->image_intro_alt); ?>" itemprop="thumbnailUrl"/></a>
+	<?php else : ?>
+		<?php if ($images->image_intro_caption) : ?>
+			<figcaption><?php echo htmlspecialchars($images->image_intro_caption); ?></figcaption>
+		<?php endif; ?>
+		<img src="<?php echo htmlspecialchars($images->image_intro); ?>" alt="<?php echo htmlspecialchars($images->image_intro_alt); ?>" itemprop="thumbnailUrl"/>
+	<?php endif; ?>
 	</figure>
 <?php endif; ?>
