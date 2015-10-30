551713d4487388ce9315f4deac81f6b4256ae722
Jean-Marie Simonet <infografjms@gmail.com>
2015-10-30 10:17:57 +0100

    SQL: Wrong title for Contacts submenu

---
+++ administrator/components/com_admin/sql/updates/mysql/3.5.0-2015-10-30.sql
@@ -0,0 +1,2 @@
+REPLACE INTO `#__menu` VALUES
+(8, 'menu', 'com_contact_contacts', 'Contacts', '', 'Contacts/Contacts', 'index.php?option=com_contact', 'component', 0, 7, 2, 8, 0, '0000-00-00 00:00:00', 0, 0, 'class:contact', 0, '', 12, 13, 0, '*', 1);
