b6174d345b62f548c6b542018445ce87ea77027d
dgt41 <d.grammatiko@gmail.com>
2015-11-02 16:50:52 +0200

    Hathor back to mootools

--- administrator/templates/hathor/html/layouts/joomla/toolbar/versions.php
+++ administrator/templates/hathor/html/layouts/joomla/toolbar/versions.php
@@ -9,43 +9,11 @@
 
 defined('_JEXEC') or die;
 
-/**
- * Layout variables
- * ---------------------
- *
- * @var  string   $itemId The item id number
- * @var  string   $typeId The type id number
- * @var  string   $title The link text
- * @var  string   $typeAlias The component type
- */
-extract($displayData);
+JHtml::_('behavior.modal', 'a.modal_jform_contenthistory');
 
-$link = 'index.php?option=com_contenthistory&amp;view=history&amp;layout=modal&amp;tmpl=component&amp;item_id='
-	. (int) $itemId . '&amp;type_id=' . $typeId . '&amp;type_alias='
-	. $typeAlias . '&amp;' . JSession::getFormToken() . '=1';
-
-echo JHtml::_(
-	'bootstrap.renderModal',
-	'versionsModal',
-	array(
-		'url'    => $link,
-		'title'  => JText::_('COM_CONTENTHISTORY_MODAL_TITLE'),
-		'height' => '300px',
-		'width'  => '800px',
-		'footer' => '<button class="btn" data-dismiss="modal" aria-hidden="true">'
-			. JText::_("JTOOLBAR_CLOSE") . '</button>'
-	)
-);
-
-JFactory::getDocument()->addStyleDeclaration(
-	"
-	div.toolbar-box h3 {
-		height: auto;
-		position: relative;
-	}
-	"
-);
 ?>
-<button onclick="jQuery('#versionsModal').modal('show')" class="btn btn-small" data-toggle="modal" title="<?php echo $title; ?>">
-	<span class="icon-32-restore"></span><?php echo $title; ?>
-</button>
+<a rel="{handler: 'iframe', size: {x: <?php echo $displayData['height']; ?>, y: <?php echo $displayData['width']; ?>}}"
+	href="index.php?option=com_contenthistory&amp;view=history&amp;layout=modal&amp;tmpl=component&amp;item_id=<?php echo (int) $displayData['itemId']; ?>&amp;type_id=<?php echo $displayData['typeId']; ?>&amp;type_alias=<?php echo $displayData['typeAlias']; ?>&amp;<?php echo JSession::getFormToken(); ?>=1"
+	title="<?php echo $displayData['title']; ?>" class="toolbar modal_jform_contenthistory">
+	<span class="icon-32-restore"></span> <?php echo $displayData['title']; ?>
+</a>
