5ac05d211dced0f3ee9d81edf2f04635fa2c11f4
wnnz34 <willy.noel.nzefameni@mni.thm.de>
2015-11-02 02:25:50 +0100

    updated !

--- administrator/components/com_installer/views/install/tmpl/default.php
+++ administrator/components/com_installer/views/install/tmpl/default.php
@@ -14,6 +14,32 @@ JHtml::_('behavior.framework', true);
 JHtml::_('bootstrap.tooltip');
 ?>
 <script type="text/javascript">
+	Joomla.submitbutton4 = function()
+	{
+		var form = document.getElementById('adminForm');
+
+		// do field validation
+		if (form.install_url.value == "" || form.install_url.value == "http://"){
+			alert("<?php echo JText::_('COM_INSTALLER_MSG_INSTALL_ENTER_A_URL', true); ?>");
+		}
+		else
+		{
+			jQuery('#loading').css('display', 'block');
+
+			form.installtype.value = 'url';
+			form.submit();
+		}
+	};
+
+	Joomla.submitbuttonInstallWebInstaller = function()
+	{
+		var form = document.getElementById('adminForm');
+
+		form.install_url.value = 'http://appscdn.joomla.org/webapps/jedapps/webinstaller.xml';
+
+		Joomla.submitbutton4();
+	};
+
 	// Add spindle-wheel for installations:
 	jQuery(document).ready(function($) {
 		var outerDiv = $('#installer-install');
@@ -32,6 +58,19 @@ JHtml::_('bootstrap.tooltip');
 			.appendTo(outerDiv);
 	});
 
+	// Set the first tab to active if there is other active tab
+	jQuery(document).ready(function($) {
+		var hasTab = function(href){
+			return $('a[data-toggle="tab"]a[href*=' + href + ']').length;
+		};
+
+		if (!hasTab(localStorage.getItem('tab-href')))
+		{
+			var tabAnchor = $("#myTabTabs li:first a");
+			window.localStorage.setItem('tab-href', tabAnchor.attr('href'));
+			tabAnchor.click();
+		}
+	});
 </script>
 
 <div id="installer-install" class="clearfix">
@@ -49,10 +88,14 @@ JHtml::_('bootstrap.tooltip');
 				<?php if ($this->showMessage) : ?>
 					<?php echo $this->loadTemplate('message'); ?>
 				<?php elseif ($this->showJedAndWebInstaller) : ?>
-
+					<div class="alert alert-info j-jed-message" style="margin-bottom: 40px; line-height: 2em; color:#333333;">
+						<a href="index.php?option=com_config&view=component&component=com_installer&path=&return=<?php echo urlencode(base64_encode(JUri::getInstance())); ?>" class="close hasTooltip" data-dismiss="alert" title="<?php echo str_replace('"', '&quot;', JText::_('COM_INSTALLER_SHOW_JED_INFORMATION_TOOLTIP')); ?>">&times;</a>
+						<p><?php echo JText::_('COM_INSTALLER_INSTALL_FROM_WEB_INFO'); ?>&nbsp;&nbsp;<?php echo JText::_('COM_INSTALLER_INSTALL_FROM_WEB_TOS'); ?></p>
+						<input class="btn" type="button" value="<?php echo JText::_('COM_INSTALLER_INSTALL_FROM_WEB_ADD_TAB'); ?>" onclick="Joomla.submitbuttonInstallWebInstaller()" />
+					</div>
 				<?php endif; ?>
 
-				<?php echo JHtml::_('bootstrap.startTabSet', 'myTab', array('active' => '--')); ?>
+				<?php echo JHtml::_('bootstrap.startTabSet', 'myTab'); ?>
 
 				<?php JEventDispatcher::getInstance()->trigger('onInstallerViewBeforeFirstTab', array()); ?>
 				<!-- Extension fieldset of the plugin installer urlFolderInstaller-->
@@ -70,6 +113,5 @@ JHtml::_('bootstrap.tooltip');
 				<?php echo JHtml::_('form.token'); ?>
 
 				<?php echo JHtml::_('bootstrap.endTabSet'); ?>
-
 	</form>
 </div>
