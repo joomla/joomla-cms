52efd17f8acdffb0664e55380e5d78f5c19c5799
Kubik-Rubik <admin@kubik-rubik.de>
2015-11-05 10:41:05 +0100

    Updated nestes sets in the installation and update database files

--- administrator/components/com_admin/sql/updates/sqlazure/3.5.0-2015-11-04.sql
+++ administrator/components/com_admin/sql/updates/sqlazure/3.5.0-2015-11-04.sql
@@ -1,1 +1,3 @@
 DELETE FROM [#__menu] WHERE [title] = 'com_messages_read' AND [client_id] = 1;
+UPDATE [#__menu] SET [lft] = [lft] - 2 WHERE [lft] > 20;
+UPDATE [#__menu] SET [rgt] = [rgt] - 2 WHERE [rgt] > 21;
