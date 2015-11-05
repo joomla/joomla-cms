f95aa247b2531aedba0b6f6aef8c31e50bb01e70
remoteshaman.com <winremos@gmail.com>
2015-11-05 10:51:21 +0200

    Update index.php
    JPATH_BASE must be define any way. _JDEFINES not need for include defines.php

--- index.php
+++ index.php
@@ -21,15 +21,12 @@ if (version_compare(PHP_VERSION, JOOMLA_MINIMUM_PHP, '<'))
  * define() is used in the installation folder rather than "const" to not error for PHP 5.2 and lower
  */
 define('_JEXEC', 1);
+define('JPATH_BASE', __DIR__);
 
-if (file_exists(__DIR__ . '/defines.php'))
+if (file_exists(JPATH_BASE . '/defines.php'))
 {
-	include_once __DIR__ . '/defines.php';
-}
-
-if (!defined('_JDEFINES'))
-{
-	define('JPATH_BASE', __DIR__);
+	include_once JPATH_BASE . '/defines.php';
+} else {
 	require_once JPATH_BASE . '/includes/defines.php';
 }
 
