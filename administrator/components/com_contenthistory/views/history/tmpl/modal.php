ba74815dc9a92d58a75eb7e255168732ecbeb41b
dgt41 <d.grammatiko@gmail.com>
2015-11-05 20:51:10 +0200

    checkbox needs to be available for mobiles

--- administrator/components/com_contenthistory/views/history/tmpl/modal.php
+++ administrator/components/com_contenthistory/views/history/tmpl/modal.php
@@ -111,7 +111,7 @@ JFactory::getDocument()->addScriptDeclaration("
 	<table class="table table-striped table-condensed">
 		<thead>
 			<tr>
-				<th width="1%" class="center hidden-phone">
+				<th width="1%" class="center">
 					<input type="checkbox" name="checkall-toggle" value="" title="<?php echo JText::_('JGLOBAL_CHECK_ALL'); ?>" onclick="Joomla.checkAll(this)" />
 				</th>
 				<th width="15%">
@@ -142,7 +142,7 @@ JFactory::getDocument()->addScriptDeclaration("
 		<?php $i = 0; ?>
 		<?php foreach ($this->items as $item) : ?>
 			<tr class="row<?php echo $i % 2; ?>">
-				<td class="center hidden-phone">
+				<td class="center">
 					<?php echo JHtml::_('grid.id', $i, $item->version_id); ?>
 				</td>
 				<td align="left">
