edcdd0ac618f22c7d30746660d213ce22b65ebc8
Jean-Marie Simonet <infografjms@gmail.com>
2015-11-09 11:46:23 +0100

    Debug Language: Star (*) is OK in lang constant

--- libraries/joomla/language/language.php
+++ libraries/joomla/language/language.php
@@ -905,7 +905,7 @@ class JLanguage
 				}
 
 				// Check that the line passes the necessary format.
-				if (!preg_match('#^[A-Z][A-Z0-9_\-\.]*\s*=\s*".*"(\s*;.*)?$#', $line))
+				if (!preg_match('#^[A-Z][A-Z0-9_\*\-\.]*\s*=\s*".*"(\s*;.*)?$#', $line))
 				{
 					$errors[] = $realNumber;
 					continue;
