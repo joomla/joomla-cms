29721950ce34aea01113c9e14ac466608aa4f4bc
Scott <scott@paragonie.com>
2015-11-12 16:57:35 -0500

    Use tabs, not spaces.

--- libraries/defuse/php-encryption/benchmark.php
+++ libraries/defuse/php-encryption/benchmark.php
@@ -7,36 +7,36 @@ require_once('Crypto.php');
 
 $start = microtime(true);
 for ($i = 0; $i < 1000; $i++) {
-    $key = Crypto::CreateNewRandomKey();
+	$key = Crypto::CreateNewRandomKey();
 }
 $end = microtime(true);
 showResults("CreateNewRandomKey()", $start, $end, 1000);
 
 $start = microtime(true);
 for ($i = 0; $i < 100; $i++) {
-    $ciphertext = Crypto::Encrypt(
-        str_repeat("A", 1024*1024), 
-        str_repeat("B", 16)
-    );
+	$ciphertext = Crypto::Encrypt(
+		str_repeat("A", 1024*1024), 
+		str_repeat("B", 16)
+	);
 }
 $end = microtime(true);
 showResults("Encrypt(1MB)", $start, $end, 100);
 
 $start = microtime(true);
 for ($i = 0; $i < 1000; $i++) {
-    $ciphertext = Crypto::Encrypt(
-        str_repeat("A", 1024), 
-        str_repeat("B", 16)
-    );
+	$ciphertext = Crypto::Encrypt(
+		str_repeat("A", 1024), 
+		str_repeat("B", 16)
+	);
 }
 $end = microtime(true);
 showResults("Encrypt(1KB)", $start, $end, 1000);
 
 function showResults($type, $start, $end, $count)
 {
-    $time = $end - $start;
-    $rate = $count / $time;
-    echo "$type: $rate calls/s\n";
+	$time = $end - $start;
+	$rate = $count / $time;
+	echo "$type: $rate calls/s\n";
 }
 
 ?>
--- libraries/defuse/php-encryption/tests/runtime.php
+++ libraries/defuse/php-encryption/tests/runtime.php
@@ -3,8 +3,8 @@
 // Set the encoding to something more "challenging."
 $ret = mb_internal_encoding('UTF-8');
 if ($ret === FALSE) {
-    echo "Couldn't set encoding.";
-    exit(1);
+	echo "Couldn't set encoding.";
+	exit(1);
 }
 
 // Dump out the settings / encoding for future reference.
@@ -16,17 +16,17 @@ echo "    mb_internal_encoding(): " . mb_internal_encoding() . "\n";
 // Perform the tests.
 require_once('Crypto.php');
 try {
-    Crypto::RuntimeTest();
-    echo "TEST PASSED!\n";
-    exit(0);
+	Crypto::RuntimeTest();
+	echo "TEST PASSED!\n";
+	exit(0);
 } catch (CryptoTestFailedException $ex) {
-    echo "TEST FAILED!\n";
-    var_dump($ex);
-    exit(1);
+	echo "TEST FAILED!\n";
+	var_dump($ex);
+	exit(1);
 } catch (CannotPerformOperationException $ex) {
-    echo "TEST FAILED\n";
-    var_dump($ex);
-    exit(1);
+	echo "TEST FAILED\n";
+	var_dump($ex);
+	exit(1);
 }
 
 ?>
