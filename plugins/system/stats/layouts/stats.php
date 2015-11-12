228a9c37c5fcb021c494bdcaa070f4eda9ce5c1a
Roberto Segura - phproberto <roberto@phproberto.com>
2015-11-12 01:30:07 +0100

    [imp] clean versions & translation strings for send data in plg_system_stats

--- plugins/system/stats/layouts/stats.php
+++ plugins/system/stats/layouts/stats.php
@@ -16,10 +16,12 @@ extract($displayData);
  * -----------------
  * @var  array  $statsData  Array containing the data that will be sent to the stats server
  */
+
+$versionFields = array('php_version', 'db_version', 'cms_version');
 ?>
 <dl class="dl-horizontal js-pstats-data-details"  style="display:none;">
 	<?php foreach ($statsData as $key => $value) : ?>
-		<dt><?php echo $key; ?></dt>
-		<dd><?php echo $value; ?></dd>
+		<dt><?php echo JText::_('PLG_SYSTEM_STATS_LABEL_' . strtoupper($key)); ?></dt>
+		<dd><?php echo in_array($key, $versionFields) ? (preg_match('/\d+(?:\.\d+)+/', $value, $matches) ? $matches[0] : $value) : $value; ?></dd>
 	<?php endforeach; ?>
 </dl>
