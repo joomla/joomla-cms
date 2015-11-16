af26ad252a432b9f378d74573e72dbf593caee60
andrepereirasilva <andrepereirasilva>
2015-11-16 21:51:10 +0000

    Improve regex

--- plugins/system/cache/cache.php
+++ plugins/system/cache/cache.php
@@ -172,7 +172,7 @@ class PlgSystemCache extends JPlugin
 					// Make sure the exclusion has some content
 					if (strlen($exclusion))
 					{
-						if (preg_match('%' . $exclusion . '%is', $this->_cache_key . '-' . $internal_uri, $match))
+						if (preg_match('/' . preg_quote($exclusion, '/') . '/i', $this->_cache_key . ' ' . $internal_uri, $match))
 						{
 							return true;
 						}
