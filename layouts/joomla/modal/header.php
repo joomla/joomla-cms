8fbb928feffe05dd03166141c86bbc437d12a806
Fedik <getthesite@gmail.com>
2015-11-23 18:24:28 +0200

    JS Validator: Ignore module buttons

--- layouts/joomla/modal/header.php
+++ layouts/joomla/modal/header.php
@@ -32,7 +32,7 @@ extract($displayData);
 ?>
 <div class="modal-header">
 	<?php if (!isset($params['closeButton']) || $params['closeButton']) : ?>
-		<button type="button" class="close" data-dismiss="modal">×</button>
+		<button type="button" class="close novalidate" data-dismiss="modal">×</button>
 	<?php endif; ?>
 	<?php if (isset($params['title'])) : ?>
 		<h3><?php echo $params['title']; ?></h3>
