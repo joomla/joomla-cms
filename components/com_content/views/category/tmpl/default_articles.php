354dee626479d46fd53a0a3766941f3f8c74cfe4
andrepereiradasilva <andrepereiradasilva>
2015-12-05 12:10:08 +0000

    make category filters form wcag 2.0 acessibile

--- components/com_content/views/category/tmpl/default_articles.php
+++ components/com_content/views/category/tmpl/default_articles.php
@@ -41,9 +41,10 @@ if (!empty($this->items))
 
 <?php else : ?>
 
+<?php if ($this->params->get('filter_field') != 'hide' || $this->params->get('show_pagination_limit')) :?>
 <form action="<?php echo htmlspecialchars(JUri::getInstance()->toString()); ?>" method="post" name="adminForm" id="adminForm" class="form-inline">
-	<?php if ($this->params->get('show_headings') || $this->params->get('filter_field') != 'hide' || $this->params->get('show_pagination_limit')) :?>
 	<fieldset class="filters btn-toolbar clearfix">
+		<legend class="hide"><?php echo JText::_('COM_CONTENT_FORM_FILTER_LEGEND'); ?></legend>
 		<?php if ($this->params->get('filter_field') != 'hide') :?>
 			<div class="btn-group">
 				<label class="filter-search-lbl element-invisible" for="filter-search">
@@ -60,143 +61,127 @@ if (!empty($this->items))
 				<?php echo $this->pagination->getLimitBox(); ?>
 			</div>
 		<?php endif; ?>
-
 		<input type="hidden" name="filter_order" value="" />
 		<input type="hidden" name="filter_order_Dir" value="" />
 		<input type="hidden" name="limitstart" value="" />
 		<input type="hidden" name="task" value="" />
 	</fieldset>
-	<?php endif; ?>
+</form>
+<?php endif; ?>
 
-	<table class="category table table-striped table-bordered table-hover">
-		<?php
-		$headerTitle    = '';
-		$headerDate     = '';
-		$headerAuthor   = '';
-		$headerHits     = '';
-		$headerEdit     = '';
-		?>
-		<?php if ($this->params->get('show_headings')) : ?>
-			<?php
-			$headerTitle    = 'headers="categorylist_header_title"';
-			$headerDate     = 'headers="categorylist_header_date"';
-			$headerAuthor   = 'headers="categorylist_header_author"';
-			$headerHits     = 'headers="categorylist_header_hits"';
-			$headerEdit     = 'headers="categorylist_header_edit"';
-			?>
-		<thead>
-			<tr>
-				<th id="categorylist_header_title">
-					<?php echo JHtml::_('grid.sort', 'JGLOBAL_TITLE', 'a.title', $listDirn, $listOrder); ?>
+<table class="category table table-striped table-bordered table-hover<?php if ($this->params->get('show_headings') != 1) { echo ' table-noheader'; } ?>">
+	<thead>
+		<tr>
+			<th id="categorylist_header_title">
+				<?php echo JHtml::_('grid.sort', 'JGLOBAL_TITLE', 'a.title', $listDirn, $listOrder); ?>
+			</th>
+			<?php if ($date = $this->params->get('list_show_date')) : ?>
+				<th id="categorylist_header_date">
+					<?php if ($date == "created") : ?>
+						<?php echo JHtml::_('grid.sort', 'COM_CONTENT_' . $date . '_DATE', 'a.created', $listDirn, $listOrder); ?>
+					<?php elseif ($date == "modified") : ?>
+						<?php echo JHtml::_('grid.sort', 'COM_CONTENT_' . $date . '_DATE', 'a.modified', $listDirn, $listOrder); ?>
+					<?php elseif ($date == "published") : ?>
+						<?php echo JHtml::_('grid.sort', 'COM_CONTENT_' . $date . '_DATE', 'a.publish_up', $listDirn, $listOrder); ?>
+					<?php endif; ?>
 				</th>
-				<?php if ($date = $this->params->get('list_show_date')) : ?>
-					<th id="categorylist_header_date">
-						<?php if ($date == "created") : ?>
-							<?php echo JHtml::_('grid.sort', 'COM_CONTENT_' . $date . '_DATE', 'a.created', $listDirn, $listOrder); ?>
-						<?php elseif ($date == "modified") : ?>
-							<?php echo JHtml::_('grid.sort', 'COM_CONTENT_' . $date . '_DATE', 'a.modified', $listDirn, $listOrder); ?>
-						<?php elseif ($date == "published") : ?>
-							<?php echo JHtml::_('grid.sort', 'COM_CONTENT_' . $date . '_DATE', 'a.publish_up', $listDirn, $listOrder); ?>
-						<?php endif; ?>
-					</th>
+			<?php endif; ?>
+			<?php if ($this->params->get('list_show_author')) : ?>
+				<th id="categorylist_header_author">
+					<?php echo JHtml::_('grid.sort', 'JAUTHOR', 'author', $listDirn, $listOrder); ?>
+				</th>
+			<?php endif; ?>
+			<?php if ($this->params->get('list_show_hits')) : ?>
+				<th id="categorylist_header_hits">
+					<?php echo JHtml::_('grid.sort', 'JGLOBAL_HITS', 'a.hits', $listDirn, $listOrder); ?>
+				</th>
+			<?php endif; ?>
+			<?php if ($isEditable) : ?>
+				<th id="categorylist_header_edit"><?php echo JText::_('COM_CONTENT_EDIT_ITEM'); ?></th>
+			<?php endif; ?>
+		</tr>
+	</thead>
+	<tbody>
+		<?php foreach ($this->items as $i => $article) : ?>
+			<?php if ($this->items[$i]->state == 0) : ?>
+			 <tr class="system-unpublished cat-list-row<?php echo $i % 2; ?>">
+			<?php else: ?>
+			<tr class="cat-list-row<?php echo $i % 2; ?>" >
+			<?php endif; ?>
+				<td headers="categorylist_header_title" class="list-title">
+					<?php if (in_array($article->access, $this->user->getAuthorisedViewLevels())) : ?>
+						<a href="<?php echo JRoute::_(ContentHelperRoute::getArticleRoute($article->slug, $article->catid, $article->language)); ?>">
+							<?php echo $this->escape($article->title); ?>
+						</a>
+					<?php else: ?>
+						<?php
+						echo $this->escape($article->title) . ' : ';
+						$menu   = JFactory::getApplication()->getMenu();
+						$active = $menu->getActive();
+						$itemId = $active->id;
+						$link   = new JUri(JRoute::_('index.php?option=com_users&view=login&Itemid=' . $itemId, false));
+						$link->setVar('return', base64_encode(JRoute::_(ContentHelperRoute::getArticleRoute($article->slug, $article->catid, $article->language), false)));
+						?>
+						<a href="<?php echo $link; ?>" class="register">
+							<?php echo JText::_('COM_CONTENT_REGISTER_TO_READ_MORE'); ?>
+						</a>
+					<?php endif; ?>
+					<?php if ($article->state == 0) : ?>
+						<span class="list-published label label-warning">
+							<?php echo JText::_('JUNPUBLISHED'); ?>
+						</span>
+					<?php endif; ?>
+					<?php if (strtotime($article->publish_up) > strtotime(JFactory::getDate())) : ?>
+						<span class="list-published label label-warning">
+							<?php echo JText::_('JNOTPUBLISHEDYET'); ?>
+						</span>
+					<?php endif; ?>
+					<?php if ((strtotime($article->publish_down) < strtotime(JFactory::getDate())) && $article->publish_down != JFactory::getDbo()->getNullDate()) : ?>
+						<span class="list-published label label-warning">
+							<?php echo JText::_('JEXPIRED'); ?>
+						</span>
+					<?php endif; ?>
+				</td>
+				<?php if ($this->params->get('list_show_date')) : ?>
+					<td headers="categorylist_header_date" class="list-date small">
+						<?php
+						echo JHtml::_(
+							'date', $article->displayDate,
+							$this->escape($this->params->get('date_format', JText::_('DATE_FORMAT_LC3')))
+						); ?>
+					</td>
 				<?php endif; ?>
-				<?php if ($this->params->get('list_show_author')) : ?>
-					<th id="categorylist_header_author">
-						<?php echo JHtml::_('grid.sort', 'JAUTHOR', 'author', $listDirn, $listOrder); ?>
-					</th>
-				<?php endif; ?>
-				<?php if ($this->params->get('list_show_hits')) : ?>
-					<th id="categorylist_header_hits">
-						<?php echo JHtml::_('grid.sort', 'JGLOBAL_HITS', 'a.hits', $listDirn, $listOrder); ?>
-					</th>
-				<?php endif; ?>
-				<?php if ($isEditable) : ?>
-					<th id="categorylist_header_edit"><?php echo JText::_('COM_CONTENT_EDIT_ITEM'); ?></th>
-				<?php endif; ?>
-			</tr>
-		</thead>
-		<?php endif; ?>
-		<tbody>
-			<?php foreach ($this->items as $i => $article) : ?>
-				<?php if ($this->items[$i]->state == 0) : ?>
-				 <tr class="system-unpublished cat-list-row<?php echo $i % 2; ?>">
-				<?php else: ?>
-				<tr class="cat-list-row<?php echo $i % 2; ?>" >
-				<?php endif; ?>
-					<td <?php echo $headerTitle; ?> class="list-title">
-						<?php if (in_array($article->access, $this->user->getAuthorisedViewLevels())) : ?>
-							<a href="<?php echo JRoute::_(ContentHelperRoute::getArticleRoute($article->slug, $article->catid, $article->language)); ?>">
-								<?php echo $this->escape($article->title); ?>
-							</a>
-						<?php else: ?>
-							<?php
-							echo $this->escape($article->title) . ' : ';
-							$menu   = JFactory::getApplication()->getMenu();
-							$active = $menu->getActive();
-							$itemId = $active->id;
-							$link   = new JUri(JRoute::_('index.php?option=com_users&view=login&Itemid=' . $itemId, false));
-							$link->setVar('return', base64_encode(JRoute::_(ContentHelperRoute::getArticleRoute($article->slug, $article->catid, $article->language), false)));
-							?>
-							<a href="<?php echo $link; ?>" class="register">
-								<?php echo JText::_('COM_CONTENT_REGISTER_TO_READ_MORE'); ?>
-							</a>
-						<?php endif; ?>
-						<?php if ($article->state == 0) : ?>
-							<span class="list-published label label-warning">
-								<?php echo JText::_('JUNPUBLISHED'); ?>
-							</span>
-						<?php endif; ?>
-						<?php if (strtotime($article->publish_up) > strtotime(JFactory::getDate())) : ?>
-							<span class="list-published label label-warning">
-								<?php echo JText::_('JNOTPUBLISHEDYET'); ?>
-							</span>
-						<?php endif; ?>
-						<?php if ((strtotime($article->publish_down) < strtotime(JFactory::getDate())) && $article->publish_down != JFactory::getDbo()->getNullDate()) : ?>
-							<span class="list-published label label-warning">
-								<?php echo JText::_('JEXPIRED'); ?>
-							</span>
+				<?php if ($this->params->get('list_show_author', 1)) : ?>
+					<td headers="categorylist_header_author" class="list-author">
+						<?php if (!empty($article->author) || !empty($article->created_by_alias)) : ?>
+							<?php $author = $article->author ?>
+							<?php $author = ($article->created_by_alias ? $article->created_by_alias : $author);?>
+							<?php if (!empty($article->contact_link) && $this->params->get('link_author') == true) : ?>
+								<?php echo JText::sprintf('COM_CONTENT_WRITTEN_BY', JHtml::_('link', $article->contact_link, $author)); ?>
+							<?php else: ?>
+								<?php echo JText::sprintf('COM_CONTENT_WRITTEN_BY', $author); ?>
+							<?php endif; ?>
 						<?php endif; ?>
 					</td>
-					<?php if ($this->params->get('list_show_date')) : ?>
-						<td <?php echo $headerDate; ?> class="list-date small">
-							<?php
-							echo JHtml::_(
-								'date', $article->displayDate,
-								$this->escape($this->params->get('date_format', JText::_('DATE_FORMAT_LC3')))
-							); ?>
-						</td>
-					<?php endif; ?>
-					<?php if ($this->params->get('list_show_author', 1)) : ?>
-						<td <?php echo $headerAuthor; ?> class="list-author">
-							<?php if (!empty($article->author) || !empty($article->created_by_alias)) : ?>
-								<?php $author = $article->author ?>
-								<?php $author = ($article->created_by_alias ? $article->created_by_alias : $author);?>
-								<?php if (!empty($article->contact_link) && $this->params->get('link_author') == true) : ?>
-									<?php echo JText::sprintf('COM_CONTENT_WRITTEN_BY', JHtml::_('link', $article->contact_link, $author)); ?>
-								<?php else: ?>
-									<?php echo JText::sprintf('COM_CONTENT_WRITTEN_BY', $author); ?>
-								<?php endif; ?>
-							<?php endif; ?>
-						</td>
-					<?php endif; ?>
-					<?php if ($this->params->get('list_show_hits', 1)) : ?>
-						<td <?php echo $headerHits; ?> class="list-hits">
-							<span class="badge badge-info">
-								<?php echo JText::sprintf('JGLOBAL_HITS_COUNT', $article->hits); ?>
-							</span>
-						</td>
-					<?php endif; ?>
-					<?php if ($isEditable) : ?>
-						<td <?php echo $headerEdit; ?> class="list-edit">
-							<?php if ($article->params->get('access-edit')) : ?>
-								<?php echo JHtml::_('icon.edit', $article, $params); ?>
-							<?php endif; ?>
-						</td>
-					<?php endif; ?>
-				</tr>
-			<?php endforeach; ?>
-		</tbody>
-	</table>
+				<?php endif; ?>
+				<?php if ($this->params->get('list_show_hits', 1)) : ?>
+					<td headers="categorylist_header_hits" class="list-hits">
+						<span class="badge badge-info">
+							<?php echo JText::sprintf('JGLOBAL_HITS_COUNT', $article->hits); ?>
+						</span>
+					</td>
+				<?php endif; ?>
+				<?php if ($isEditable) : ?>
+					<td headers="categorylist_header_edit" class="list-edit">
+						<?php if ($article->params->get('access-edit')) : ?>
+							<?php echo JHtml::_('icon.edit', $article, $params); ?>
+						<?php endif; ?>
+					</td>
+				<?php endif; ?>
+			</tr>
+		<?php endforeach; ?>
+	</tbody>
+</table>
 <?php endif; ?>
 
 <?php // Code to add a link to submit an article. ?>
@@ -218,5 +203,4 @@ if (!empty($this->items))
 		<?php echo $this->pagination->getPagesLinks(); ?>
 	</div>
 	<?php endif; ?>
-</form>
 <?php  endif; ?>
