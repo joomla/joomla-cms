6fee821745f57d1a930d2f276ea225b8415ef024
CoalaWeb <support@coalaweb.com>
2015-12-07 11:05:55 -0500

    Restored formatting to original to stop confusion on what was changed.

--- modules/mod_articles_news/helper.php
+++ modules/mod_articles_news/helper.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * @package     Joomla.Site
  * @subpackage  mod_articles_news
@@ -7,6 +6,7 @@
  * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
+
 defined('_JEXEC') or die;
 
 require_once JPATH_SITE . '/components/com_content/helpers/route.php';
@@ -21,92 +21,101 @@ JModelLegacy::addIncludePath(JPATH_SITE . '/components/com_content/models', 'Con
  *
  * @since       1.6
  */
-abstract class ModArticlesNewsHelper {
+abstract class ModArticlesNewsHelper
+{
+	/**
+	 * Get a list of the latest articles from the article model
+	 *
+	 * @param   \Joomla\Registry\Registry  &$params  object holding the models parameters
+	 *
+	 * @return  mixed
+	 *
+	 * @since 1.6
+	 */
+	public static function getList(&$params)
+	{
+		$app = JFactory::getApplication();
 
-    /**
-     * Get a list of the latest articles from the article model
-     *
-     * @param   \Joomla\Registry\Registry  &$params  object holding the models parameters
-     *
-     * @return  mixed
-     *
-     * @since 1.6
-     */
-    public static function getList(&$params) {
-        $app = JFactory::getApplication();
+		// Get an instance of the generic articles model
+		$model = JModelLegacy::getInstance('Articles', 'ContentModel', array('ignore_request' => true));
 
-        // Get an instance of the generic articles model
-        $model = JModelLegacy::getInstance('Articles', 'ContentModel', array('ignore_request' => true));
+		// Set application parameters in model
+		$appParams = JFactory::getApplication()->getParams();
+		$model->setState('params', $appParams);
 
-        // Set application parameters in model
-        $appParams = JFactory::getApplication()->getParams();
-        $model->setState('params', $appParams);
+		// Set the filters based on the module params
+		$model->setState('list.start', 0);
+		$model->setState('list.limit', (int) $params->get('count', 5));
+		$model->setState('filter.published', 1);
 
-        // Set the filters based on the module params
-        $model->setState('list.start', 0);
-        $model->setState('list.limit', (int) $params->get('count', 5));
-        $model->setState('filter.published', 1);
+		// Access filter
+		$access     = !JComponentHelper::getParams('com_content')->get('show_noauth');
+		$authorised = JAccess::getAuthorisedViewLevels(JFactory::getUser()->get('id'));
+		$model->setState('filter.access', $access);
 
-        // Access filter
-        $access = !JComponentHelper::getParams('com_content')->get('show_noauth');
-        $authorised = JAccess::getAuthorisedViewLevels(JFactory::getUser()->get('id'));
-        $model->setState('filter.access', $access);
+		// Category filter
+		$model->setState('filter.category_id', $params->get('catid', array()));
 
-        // Category filter
-        $model->setState('filter.category_id', $params->get('catid', array()));
+		// Filter by language
+		$model->setState('filter.language', $app->getLanguageFilter());
 
-        // Filter by language
-        $model->setState('filter.language', $app->getLanguageFilter());
+		// Set ordering
+		$ordering = $params->get('ordering', 'a.publish_up');
+		$model->setState('list.ordering', $ordering);
 
-        // Set ordering
-        $ordering = $params->get('ordering', 'a.publish_up');
-        $model->setState('list.ordering', $ordering);
+		if (trim($ordering) == 'rand()')
+		{
+			$model->setState('list.ordering', JFactory::getDbo()->getQuery(true)->Rand());
+		}
+		else
+		{
+			$direction = $params->get('direction', 1) ? 'DESC' : 'ASC';
+			$model->setState('list.direction', $direction);
+			$model->setState('list.ordering', $ordering);
+		}
 
-        if (trim($ordering) == 'rand()') {
-            $model->setState('list.ordering', JFactory::getDbo()->getQuery(true)->Rand());
-        } else {
-            $direction = $params->get('direction', 1) ? 'DESC' : 'ASC';
-            $model->setState('list.direction', $direction);
-            $model->setState('list.ordering', $ordering);
-        }
+		// Retrieve Content
+		$items = $model->getItems();
 
-        // Retrieve Content
-        $items = $model->getItems();
+		foreach ($items as &$item)
+		{
+			$item->readmore = strlen(trim($item->fulltext));
+			$item->slug     = $item->id . ':' . $item->alias;
+			$item->catslug  = $item->catid . ':' . $item->category_alias;
 
-        foreach ($items as &$item) {
-            $item->readmore = strlen(trim($item->fulltext));
-            $item->slug = $item->id . ':' . $item->alias;
-            $item->catslug = $item->catid . ':' . $item->category_alias;
+			if ($access || in_array($item->access, $authorised))
+			{
+				// We know that user has the privilege to view the article
+				$item->link     = JRoute::_(ContentHelperRoute::getArticleRoute($item->slug, $item->catid, $item->language));
+				$item->linkText = JText::_('MOD_ARTICLES_NEWS_READMORE');
+			}
+			else
+			{
+				$item->link = new JUri(JRoute::_('index.php?option=com_users&view=login', false));
+				$item->link->setVar('return', base64_encode(JRoute::_(ContentHelperRoute::getArticleRoute($item->slug, $item->catid, $item->language), false)));
+				$item->linkText = JText::_('MOD_ARTICLES_NEWS_READMORE_REGISTER');
+			}
 
-            if ($access || in_array($item->access, $authorised)) {
-                // We know that user has the privilege to view the article
-                $item->link = JRoute::_(ContentHelperRoute::getArticleRoute($item->slug, $item->catid, $item->language));
-                $item->linkText = JText::_('MOD_ARTICLES_NEWS_READMORE');
-            } else {
-                $item->link = new JUri(JRoute::_('index.php?option=com_users&view=login', false));
-                $item->link->setVar('return', base64_encode(JRoute::_(ContentHelperRoute::getArticleRoute($item->slug, $item->catid, $item->language), false)));
-                $item->linkText = JText::_('MOD_ARTICLES_NEWS_READMORE_REGISTER');
-            }
+                if ($params->get('prepare_content', 1)){
+                    $item->introtext = JHtml::_('content.prepare', $item->introtext, '', 'mod_articles_news.content');
+                }
 
-            if ($params->get('prepare_content', 1)) {
-                $item->introtext = JHtml::_('content.prepare', $item->introtext, '', 'mod_articles_news.content');
-            }
+			// New
+			if (!$params->get('image'))
+			{
+				$item->introtext = preg_replace('/<img[^>]*>/', '', $item->introtext);
+			}
 
-            // New
-            if (!$params->get('image')) {
-                $item->introtext = preg_replace('/<img[^>]*>/', '', $item->introtext);
-            }
-
-            if ($params->get('prepare_content', 1)) {
+            if ($params->get('prepare_content', 1)){
                 $results = $app->triggerEvent('onContentAfterDisplay', array('com_content.article', &$item, &$params, 1));
                 $item->afterDisplayTitle = trim(implode("\n", $results));
 
                 $results = $app->triggerEvent('onContentBeforeDisplay', array('com_content.article', &$item, &$params, 1));
                 $item->beforeDisplayContent = trim(implode("\n", $results));
             }
+
         }
 
-        return $items;
-    }
-
+		return $items;
+	}
 }
