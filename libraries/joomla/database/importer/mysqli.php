43e8acb3e77e9eebe13296c3139835fa1ca6c4dc
pollen8 <rob@pollen-8.co.uk>
2015-12-08 16:01:15 +0100

    adding Sub_part info to mySQLi db import/export code, allows for export and import of keys with length values

--- libraries/joomla/database/importer/mysqli.php
+++ libraries/joomla/database/importer/mysqli.php
@@ -373,6 +373,8 @@ class JDatabaseImporterMysqli extends JDatabaseImporter
 		$kNonUnique = (string) $columns[0]['Non_unique'];
 		$kName = (string) $columns[0]['Key_name'];
 		$kColumn = (string) $columns[0]['Column_name'];
+		$kLength = (string) $columns[0]['Sub_part'];
+		$kLength = $kLength == '' ? '' : '(' . $kLength . ')';
 
 		$prefix = '';
 
@@ -390,13 +392,13 @@ class JDatabaseImporterMysqli extends JDatabaseImporter
 
 		if ($nColumns == 1)
 		{
-			$kColumns[] = $this->db->quoteName($kColumn);
+			$kColumns[] = $this->db->quoteName($kColumn) . $kLength;
 		}
 		else
 		{
 			foreach ($columns as $column)
 			{
-				$kColumns[] = (string) $column['Column_name'];
+				$kColumns[] = (string) $column['Column_name'] . $kLength;
 			}
 		}
 
