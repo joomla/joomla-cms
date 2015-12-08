cfa1e241e37591b689a8391b219339e0facad350
jo-sf <jo-sf@users.noreply.github.com>
2015-12-08 18:23:58 +0100

    Move logout override to separate file
    Moved the logout override to default_logout.php similar to the mod_login module.

--- templates/beez3/html/mod_login/default.php
+++ templates/beez3/html/mod_login/default.php
@@ -12,27 +12,7 @@ defined('_JEXEC') or die;
 
 JHtml::_('behavior.keepalive');
 ?>
-<?php if ($type == 'logout') : ?>
-	<form action="<?php echo JRoute::_('index.php', true, $params->get('usesecure')); ?>" method="post" id="login-form">
-	<?php if ($params->get('greeting')) : ?>
-		<div class="login-greeting">
-		<?php if ($params->get('name') == 0) : ?>
-			<?php echo JText::sprintf('MOD_LOGIN_HINAME', htmlspecialchars($user->get('name'))); ?>
-		<?php else : ?>
-		 	<?php echo JText::sprintf('MOD_LOGIN_HINAME', htmlspecialchars($user->get('username'))); ?>
-		<?php endif; ?>
-		</div>
-	<?php endif; ?>
-	<div class="logout-button">
-		<input type="submit" name="Submit" class="button" value="<?php echo JText::_('JLOGOUT'); ?>" />
-		<input type="hidden" name="option" value="com_users" />
-		<input type="hidden" name="task" value="user.logout" />
-		<input type="hidden" name="return" value="<?php echo $return; ?>" />
-		<?php echo JHtml::_('form.token'); ?>
-	</div>
-	</form>
-<?php else : ?>
-	<form action="<?php echo JRoute::_('index.php', true, $params->get('usesecure')); ?>" method="post" id="login-form" >
+<form action="<?php echo JRoute::_('index.php', true, $params->get('usesecure')); ?>" method="post" id="login-form" >
 	<?php if ($params->get('pretext')) : ?>
 		<div class="pretext">
 		<p><?php echo $params->get('pretext'); ?></p>
@@ -96,5 +76,4 @@ JHtml::_('behavior.keepalive');
 		</div>
 	<?php endif; ?>
 	</fieldset>
-	</form>
-<?php endif; ?>
+</form>
