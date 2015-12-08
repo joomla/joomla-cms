bab36eb4bd780aa1c319f1785fe84374969221e5
pe7er <github@pe7er.com>
2015-12-08 17:12:25 +0100

    modify category form field for free input

--- administrator/components/com_categories/models/fields/categoryedit.php
+++ administrator/components/com_categories/models/fields/categoryedit.php
@@ -229,4 +229,85 @@ class JFormFieldCategoryEdit extends JFormFieldList
 		// Merge any additional options in the XML definition.
 		return array_merge(parent::getOptions(), $options);
 	}
+
+	/**
+	 * Method to get the field input markup for a generic list.
+	 * Use the multiple attribute to enable multiselect.
+	 *
+	 * @return  string  The field input markup.
+	 *
+	 * @since   3.6
+	 */
+	protected function getInput()
+	{
+		$html = array();
+		$class = array();
+		$attr = '';
+
+		// Initialize some field attributes.
+		$class[] = !empty($this->class) ? $this->class : '';
+
+		if ($this->element["allowAdd"])
+		{
+			$customGroupText = JText::_('JGLOBAL_CUSTOM_CATEGORY');
+
+			$class[] = 'chzn-custom-value';
+			$attr .= ' data-custom_group_text="' . $customGroupText . '" '
+					. 'data-no_results_text="' . JText::_('JGLOBAL_ADD_CUSTOM_CATEGORY') . '" '
+					. 'data-placeholder="' . JText::_('JGLOBAL_TYPE_OR_SELECT_CATEGORY') . '" ';
+		}
+
+		if ($class)
+		{
+			$attr .= 'class="' . implode(' ', $class) . '"';
+		}
+
+		$attr .= !empty($this->size) ? ' size="' . $this->size . '"' : '';
+		$attr .= $this->multiple ? ' multiple' : '';
+		$attr .= $this->required ? ' required aria-required="true"' : '';
+		$attr .= $this->autofocus ? ' autofocus' : '';
+
+		// To avoid user's confusion, readonly="true" should imply disabled="true".
+		if ((string) $this->readonly == '1' || (string) $this->readonly == 'true' || (string) $this->disabled == '1'|| (string) $this->disabled == 'true')
+		{
+			$attr .= ' disabled="disabled"';
+		}
+
+		// Initialize JavaScript field attributes.
+		$attr .= $this->onchange ? ' onchange="' . $this->onchange . '"' : '';
+
+		// Get the field options.
+		$options = (array) $this->getOptions();
+
+		// Create a read-only list (no name) with hidden input(s) to store the value(s).
+		if ((string) $this->readonly == '1' || (string) $this->readonly == 'true')
+		{
+			$html[] = JHtml::_('select.genericlist', $options, '', trim($attr), 'value', 'text', $this->value, $this->id);
+
+			// E.g. form field type tag sends $this->value as array
+			if ($this->multiple && is_array($this->value))
+			{
+				if (!count($this->value))
+				{
+					$this->value[] = '';
+				}
+
+				foreach ($this->value as $value)
+				{
+					$html[] = '<input type="hidden" name="' . $this->name . '" value="' . htmlspecialchars($value, ENT_COMPAT, 'UTF-8') . '"/>';
+				}
+			}
+			else
+			{
+				$html[] = '<input type="hidden" name="' . $this->name . '" value="' . htmlspecialchars($this->value, ENT_COMPAT, 'UTF-8') . '"/>';
+			}
+		}
+		else
+			// Create a regular list.
+		{
+			$html[] = JHtml::_('select.genericlist', $options, $this->name, trim($attr), 'value', 'text', $this->value, $this->id);
+		}
+
+		return implode($html);
+	}
 }
