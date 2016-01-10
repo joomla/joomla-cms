beef7fcf4eba26f20392cef3b8e2fbc9aee97eb5
wojsmol <wojsmol@wp.pl>
2016-01-10 19:28:43 +0100

    add typr="button" to \templates\protostar\html\layouts\joomla\form\field\contenthistory.php

--- templates/protostar/html/layouts/joomla/form/field/contenthistory.php
+++ templates/protostar/html/layouts/joomla/form/field/contenthistory.php
@@ -26,7 +26,7 @@ echo JHtml::_(
 		'title'  => $label,
 		'height' => '300px',
 		'width'  => '800px',
-		'footer' => '<button class="btn" data-dismiss="modal" aria-hidden="true">'
+		'footer' => '<button type="button" class="btn" data-dismiss="modal" aria-hidden="true">'
 			. JText::_("JLIB_HTML_BEHAVIOR_CLOSE") . '</button>'
 	)
 );
