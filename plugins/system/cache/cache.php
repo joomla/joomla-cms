44ee0055f4502f55da2f8cdea6d1d038f7b143dc
andrepereiradasilva <andrepereiradasilva@users.noreply.github.com>
2016-01-11 22:37:01 +0000

    update

--- plugins/system/cache/cache.php
+++ plugins/system/cache/cache.php
@@ -3,7 +3,7 @@
  * @package     Joomla.Plugin
  * @subpackage  System.cache
  *
- * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
+ * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
@@ -32,19 +32,27 @@ class PlgSystemCache extends JPlugin
 	{
 		parent::__construct($subject, $config);
 
-		// Set the language in the class.
+		// Get the application if not done by JPlugin.
+		if (!isset($this->app))
+		{
+			$this->app = JFactory::getApplication();
+		}
+
+		// Set the cache options.
 		$options = array(
 			'defaultgroup' => 'page',
 			'browsercache' => $this->params->get('browsercache', false),
 			'caching'      => false,
 		);
 
+		// Instantiate cache with previous options and create the cache key identifier.
 		$this->_cache     = JCache::getInstance('page', $options);
 		$this->_cache_key = JUri::getInstance()->toString();
 	}
 
 	/**
-	 * Converting the site URL to fit to the HTTP request.
+	 * After Initialise Event.
+	 * Checks if URL exists in cache, if so dumps it directly and closes.
 	 *
 	 * @return  void
 	 *
@@ -52,54 +60,58 @@ class PlgSystemCache extends JPlugin
 	 */
 	public function onAfterInitialise()
 	{
-		global $_PROFILER;
-
-		$app  = JFactory::getApplication();
-
-		if ($app->isAdmin() || count($app->getMessageQueue()) || $app->get('offline','0'))
+		if ($this->app->isAdmin() || count($this->app->getMessageQueue()) || $this->app->get('offline', '0'))
 		{
 			return;
 		}
 
-		if (JFactory::getUser()->get('guest') && $app->input->getMethod() == 'GET')
+		// If user is guest and method is equal to GET enable caching.
+		if (JFactory::getUser()->get('guest') && $this->app->input->getMethod() == 'GET')
 		{
 			$this->_cache->setCaching(true);
-		}
 
-		$data = $this->_cache->get($this->_cache_key);
+			// Gets page from cache.
+			$data = $this->_cache->get($this->_cache_key);
 
-		if ($data !== false)
-		{
-			// Set cached body.
-			$app->setBody($data);
-
-			echo $app->toString();
-
-			if (JDEBUG)
+			// If page exist in cache, show cached page.
+			if ($data !== false)
 			{
-				$_PROFILER->mark('afterCache');
-			}
+				// Set HTML page from cache.
+				$this->app->setBody($data);
 
-			$app->close();
+				// Dumps HTML page.
+				echo $this->app->toString();
+
+				// Mark afterCache in debug and run debug onAfterRespond events.
+				// e.g., show Joomla Debug Console if debug is active.
+				if (JDEBUG)
+				{
+					JProfiler::getInstance('Application')->mark('afterCache');
+					JEventDispatcher::getInstance()->trigger('onAfterRespond');
+				}
+
+				// Closes the application.
+				$this->app->close();
+			}
 		}
 	}
 
 	/**
-	 * Verifying that the page is not excluded from cache
+	 * After Route Event.
+	 * Verify if current page is not excluded from cache.
 	 *
 	 * @return   void
 	 *
-	 * @since   3.5
+	 * @since   3.6
 	 */
 	public function onAfterRoute()
 	{
-		$app = JFactory::getApplication();
-
-		if ($app->isAdmin() || count($app->getMessageQueue()) || $app->get('offline','0'))
+		if ($this->app->isAdmin() || count($this->app->getMessageQueue()) || $this->app->get('offline', '0'))
 		{
 			return;
 		}
 
+		// Page is excluded if excluded in plugin settings.
 		if ($this->isExcluded())
 		{
 			$this->_cache->setCaching(false);
@@ -107,7 +119,8 @@ class PlgSystemCache extends JPlugin
 	}
 
 	/**
-	 * After render.
+	 * After Respond Event.
+	 * Stores page in cache.
 	 *
 	 * @return   void
 	 *
@@ -115,16 +128,15 @@ class PlgSystemCache extends JPlugin
 	 */
 	public function onAfterRespond()
 	{
-		$app = JFactory::getApplication();
-
-		if ($app->isAdmin() || count($app->getMessageQueue()) || $app->get('offline','0'))
+		if ($this->app->isAdmin() || count($this->app->getMessageQueue()) || $this->app->get('offline', '0'))
 		{
 			return;
 		}
 
+		// We need to check if user is guest again here, because auto-login plugins have not been fired before the first aid check.
 		if (JFactory::getUser()->get('guest'))
 		{
-			// We need to check again here, because auto-login plugins have not been fired before the first aid check.
+			// Saves current page in cache.
 			$this->_cache->store(null, $this->_cache_key);
 		}
 	}
@@ -138,13 +150,11 @@ class PlgSystemCache extends JPlugin
 	 */
 	protected function isExcluded()
 	{
-		$app = JFactory::getApplication();
-		
-		// Check if menu items have been excluded
+		// Check if menu items have been excluded.
 		if ($exclusions = $this->params->get('exclude_menu_items', array()))
 		{
-			// Get the current menu item
-			$active = $app->getMenu()->getActive();
+			// Get the current menu item.
+			$active = $this->app->getMenu()->getActive();
 			
 			if ($active && $active->id && in_array($active->id, (array) $exclusions))
 			{
@@ -152,27 +162,28 @@ class PlgSystemCache extends JPlugin
 			}
 		}
 
-		// Check if regular expressions are being used
+		// Check if regular expressions are being used.
 		if ($exclusions = $this->params->get('exclude', ''))
 		{
-			// Normalize line endings
+			// Normalize line endings.
 			$exclusions = str_replace(array("\r\n", "\r"), "\n", $exclusions);
 
-			// Split them
+			// Split them.
 			$exclusions = explode("\n", $exclusions);
 
-			// Gets internal URI
-			$internal_uri	= '/index.php?' . JUri::getInstance()->buildQuery($app->getRouter()->getVars());
+			// Gets internal URI.
+			$internal_uri	= '/index.php?' . JUri::getInstance()->buildQuery($this->app->getRouter()->getVars());
 
-			// Loop through each pattern
+			// Loop through each pattern.
 			if ($exclusions)
 			{
 				foreach ($exclusions as $exclusion)
 				{
-					// Make sure the exclusion has some content
+					// Make sure the exclusion has some content.
 					if (strlen($exclusion))
 					{
-						if (preg_match('/' . preg_quote($exclusion, '/') . '/i', $this->_cache_key . ' ' . $internal_uri, $match))
+						// Test both external and internal URI
+						if (preg_match('#' . $exclusion . '#i', $this->_cache_key . ' ' . $internal_uri, $match))
 						{
 							return true;
 						}
