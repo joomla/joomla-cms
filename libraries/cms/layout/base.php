b8bc1765851242dc7281d63b1aaebb79ad661bf5
Roland Dalmulder <contact@rolandd.com>
2016-01-16 12:51:05 +0100

    Grammar fixes

--- libraries/cms/layout/base.php
+++ libraries/cms/layout/base.php
@@ -131,7 +131,7 @@ class JLayoutBase implements JLayout
 	/**
 	 * Method to render the layout.
 	 *
-	 * @param   array  $displayData  Array which properties are used inside the layout file to build displayed output
+	 * @param   array  $displayData  Array of properties available for use inside the layout file to build the displayed output
 	 *
 	 * @return  string  The necessary HTML to display the layout
 	 *
