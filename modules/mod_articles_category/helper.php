daa6035112769462d1c7f1a033445afb5b8dc0fc
alikon <optimus4joomla@gmail.com>
2016-01-16 23:31:53 +0000

    Random order option to Articles Category Module
    Fixes #8538

--- modules/mod_articles_category/helper.php
+++ modules/mod_articles_category/helper.php
@@ -163,8 +163,17 @@ abstract class ModArticlesCategoryHelper
 		}
 
 		// Ordering
-		$articles->setState('list.ordering', $params->get('article_ordering', 'a.ordering'));
-		$articles->setState('list.direction', $params->get('article_ordering_direction', 'ASC'));
+		$ordering = $params->get('article_ordering', 'a.ordering');
+
+		if (trim($ordering) == 'random')
+		{
+			$articles->setState('list.ordering', JFactory::getDbo()->getQuery(true)->Rand());
+		}
+		else
+		{
+			$articles->setState('list.ordering', $params->get('article_ordering', 'a.ordering'));
+			$articles->setState('list.direction', $params->get('article_ordering_direction', 'ASC'));
+		}
 
 		// New Parameters
 		$articles->setState('filter.featured', $params->get('show_front', 'show'));
