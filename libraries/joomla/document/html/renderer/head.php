c81980d4d34f72c360d3f3e60814e8dd22227728
andrepereiradasilva <andrepereiradasilva@users.noreply.github.com>
2016-01-17 21:37:56 +0000

    no change in old head renderer

--- libraries/joomla/document/html/renderer/head.php
+++ libraries/joomla/document/html/renderer/head.php
@@ -6,8 +6,11 @@
  * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE
  */
+ 
 defined('JPATH_PLATFORM') or die;
+
 JLog::add('JDocumentRendererHead is deprecated, use JDocumentRendererHtmlHead instead.', JLog::WARNING, 'deprecated');
+
 /**
  * JDocument head renderer
  *
