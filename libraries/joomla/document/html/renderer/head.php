4697be706826b9da4d3318dc4adcc3616f87003f
andrepereiradasilva <andrepereiradasilva@users.noreply.github.com>
2016-01-17 21:38:47 +0000

    cs space

--- libraries/joomla/document/html/renderer/head.php
+++ libraries/joomla/document/html/renderer/head.php
@@ -6,7 +6,7 @@
  * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE
  */
- 
+
 defined('JPATH_PLATFORM') or die;
 
 JLog::add('JDocumentRendererHead is deprecated, use JDocumentRendererHtmlHead instead.', JLog::WARNING, 'deprecated');
