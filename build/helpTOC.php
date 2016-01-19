47287ce181707686ce06ded3bb5372a5b697b3df
andrepereiradasilva <andrepereiradasilva@users.noreply.github.com>
2016-01-19 19:56:25 +0000

    change in build TOC for help too

--- build/helpTOC.php
+++ build/helpTOC.php
@@ -52,7 +52,7 @@ class MediawikiCli extends JApplicationCli
 	{
 		// Get the version data for the script
 		$version     = new JVersion;
-		$helpVersion = str_replace('.', '', $version->RELEASE);
+		$helpVersion = str_replace('.', '', $version::RELEASE);
 		$namespace   = 'Help' . $helpVersion . ':';
 
 		// Set up options for JMediawiki
@@ -63,7 +63,7 @@ class MediawikiCli extends JApplicationCli
 
 		// Get the category members (local hack)
 		$this->out('Fetching data from docs wiki', true);
-		$categoryMembers = $mediawiki->categories->getCategoryMembers('Category:Help_screen_' . $version->RELEASE, null, 'max');
+		$categoryMembers = $mediawiki->categories->getCategoryMembers('Category:Help_screen_' . $version::RELEASE, null, 'max');
 
 		$members = array();
 
