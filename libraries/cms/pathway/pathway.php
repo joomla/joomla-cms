43ac027c9a26df522fa74353b193221290f06828
alexva24 <a.vaganov@idivision.ru>
2016-01-26 18:09:00 +0300

    Reset pathways counter after the set pathways

--- libraries/cms/pathway/pathway.php
+++ libraries/cms/pathway/pathway.php
@@ -130,6 +130,7 @@ class JPathway
 
 		// Set the new pathway.
 		$this->_pathway = array_values((array) $pathway);
+		$this->_count = count($this->_pathway);
 
 		return array_values($oldPathway);
 	}
