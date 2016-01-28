93b3c0ec9add59d8628b54027df7536bda34f3d3
Roland Dalmulder <contact@rolandd.com>
2016-01-28 16:53:43 +0100

    Fixed fatal error message on session timeout

--- libraries/joomla/session/handler/native.php
+++ libraries/joomla/session/handler/native.php
@@ -216,21 +216,21 @@ class JSessionHandlerNative implements JSessionHandlerInterface
 	 */
 	public function save()
 	{
-		if (!$this->isStarted())
+		// If running PHP 5.4, try to use the native API
+		if ((version_compare(PHP_VERSION, '5.4', 'ge') && PHP_SESSION_ACTIVE === session_status())
+			|| (version_compare(PHP_VERSION, '5.4', 'lt') && $this->started && isset($_SESSION) && $this->getId()))
 		{
-			throw new RuntimeException('The session is not started.');
+			$session = JFactory::getSession();
+			$data    = $session->getData();
+
+			// Before storing it, let's serialize and encode the JRegistry object
+			$_SESSION['joomla'] = base64_encode(serialize($data));
+
+			session_write_close();
+
+			$this->closed  = true;
+			$this->started = false;
 		}
-
-		$session = JFactory::getSession();
-		$data    = $session->getData();
-
-		// Before storing it, let's serialize and encode the JRegistry object
-		$_SESSION['joomla'] = base64_encode(serialize($data));
-
-		session_write_close();
-
-		$this->closed  = true;
-		$this->started = false;
 	}
 
 	/**
