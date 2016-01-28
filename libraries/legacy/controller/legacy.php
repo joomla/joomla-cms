c795c614820dd4d834e43e4d70a2df34a9f0474b
Mai Vu <mvanvu@gmail.com>
2016-01-28 14:03:28 +0700

    Wrong ControllerLegacy::getName()

--- libraries/legacy/controller/legacy.php
+++ libraries/legacy/controller/legacy.php
@@ -792,7 +792,7 @@ class JControllerLegacy extends JObject
 		{
 			$r = null;
 
-			if (!preg_match('/(.*)Controller/i', get_class($this), $r))
+			if (!preg_match('/Controller(.*)/i', get_class($this), $r))
 			{
 				throw new Exception(JText::_('JLIB_APPLICATION_ERROR_CONTROLLER_GET_NAME'), 500);
 			}
