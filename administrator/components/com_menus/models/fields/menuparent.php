7e51119e771d7f1f41e9a783c61eb6c6f7ffca2a
Ilja Michajlow <ilja.michajlow@mni.thm.de>
2016-02-08 15:34:21 +0100

    Added alias
    Show alias after the title of a menu item by selecting a parent menu item

--- administrator/components/com_menus/models/fields/menuparent.php
+++ administrator/components/com_menus/models/fields/menuparent.php
@@ -78,7 +78,8 @@ class JFormFieldMenuParent extends JFormFieldList
 		// Pad the option text with spaces using depth level as a multiplier.
 		for ($i = 0, $n = count($options); $i < $n; $i++)
 		{
-			$options[$i]->text = str_repeat('- ', $options[$i]->level) . $options[$i]->text;
+			$titleAlias = '(' . JText::_('JFIELD_ALIAS_LABEL') . ': ' . $db->escape($options[$i]->alias) . ')';
+			$options[$i]->text = str_repeat('- ', $options[$i]->level) . $options[$i]->text . ' ' . $titleAlias;
 		}
 
 		// Merge any additional options in the XML definition.
