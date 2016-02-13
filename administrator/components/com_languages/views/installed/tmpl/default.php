031d6d164f492152dbf8e78550ce7c20f1be03aa
andrepereiradasilva <andrepereiradasilva@users.noreply.github.com>
2016-02-13 11:26:40 +0000

    no need for multiselect here

--- administrator/components/com_languages/views/installed/tmpl/default.php
+++ administrator/components/com_languages/views/installed/tmpl/default.php
@@ -12,7 +12,6 @@ defined('_JEXEC') or die;
 // Add specific helper files for html generation
 JHtml::addIncludePath(JPATH_COMPONENT . '/helpers/html');
 
-JHtml::_('behavior.multiselect');
 JHtml::_('formbehavior.chosen', 'select');
 JHtml::_('bootstrap.tooltip');
 
