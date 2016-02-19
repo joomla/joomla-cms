ab172225555f9fe08122f0d3c5773c041889ebe9
George Wilson <georgejameswilson@googlemail.com>
2016-02-19 19:38:31 +0100

    Reduce index length to 100 characters

--- administrator/components/com_admin/sql/updates/mysql/3.5.0-2015-07-01.sql
+++ administrator/components/com_admin/sql/updates/mysql/3.5.0-2015-07-01.sql
@@ -1,11 +1,19 @@
 -- WARNING: Do not rename this file with a different date. It MUST run before any other table updates when upgrading to Joomla! 3.5.0
 
 -- Index and field changes to cater for UTF-8 Multibyte (utf8mb4)
+ALTER TABLE `#__categories` DROP KEY `idx_alias`, ADD KEY `idx_alias` (`alias`(100));
+
+ALTER TABLE `#__menu` DROP KEY `idx_alias`, ADD KEY `idx_alias` (`alias`(100));
+
 ALTER TABLE `#__menu` DROP KEY `idx_client_id_parent_id_alias_language`, ADD UNIQUE KEY `idx_client_id_parent_id_alias_language` (`client_id`,`parent_id`,`alias`(100),`language`);
 
 ALTER TABLE `#__redirect_links` DROP KEY `idx_link_old`, ADD UNIQUE KEY `idx_link_old` (`old_url`(100));
 
 ALTER TABLE `#__session` MODIFY `session_id` varchar(191) NOT NULL DEFAULT '';
+
+ALTER TABLE `#__tags` DROP KEY `idx_alias`, ADD KEY `idx_alias` (`alias`(100));
+
+ALTER TABLE `#__ucm_content` DROP KEY `idx_alias`, ADD KEY `idx_alias` (`core_alias`(100));
 
 ALTER TABLE `#__user_keys` MODIFY `series` varchar(191) NOT NULL;
 
