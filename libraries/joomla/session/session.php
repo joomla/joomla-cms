9c890fbdd98c1f3949d285585ea221d28c09976e
Shubham Rathore <GABBAR1947@users.noreply.github.com>
2016-03-02 09:11:58 +0530

    Update session.php

--- libraries/joomla/session/session.php
+++ libraries/joomla/session/session.php
@@ -525,10 +525,10 @@ class JSession implements IteratorAggregate
 			return null;
 		}
 
-        $prev = $this->data->get($namespace . '.' . $name, null);
-        $this->data->set($namespace . '.' . $name, $value);
-    
-        return $prev;
+		$prev = $this->data->get($namespace . '.' . $name, null);
+		$this->data->set($namespace . '.' . $name, $value);
+		
+		return $prev;
 	}
 
 	/**
