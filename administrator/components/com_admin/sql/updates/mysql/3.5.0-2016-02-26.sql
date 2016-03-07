e05c15360bf0040c0c4d256ae0a13c0aaf95c6ff
richard67 <richard.fath@t-online.de>
2016-03-07 16:49:32 +0100

    Make installer handle sql queries from files correctly
    Make installer handle sql queries from files correctly regarding
    comments and utf8mb4 query downgrade (also if stoll old db driver in
    use) and change back the upate sql for creating the new utf8_conversion
    table with utf8mb4.

--- administrator/components/com_admin/sql/updates/mysql/3.5.0-2016-02-26.sql
+++ administrator/components/com_admin/sql/updates/mysql/3.5.0-2016-02-26.sql
@@ -9,6 +9,6 @@
 
 CREATE TABLE IF NOT EXISTS `#__utf8_conversion` (
   `converted` tinyint(4) NOT NULL DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 DEFAULT COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 DEFAULT COLLATE=utf8mb4_unicode_ci;
 
 INSERT INTO `#__utf8_conversion` (`converted`) VALUES (0);
