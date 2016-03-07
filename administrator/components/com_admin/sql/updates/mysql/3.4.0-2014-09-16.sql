42e6bcdb3ee360aaa31e7edd5a5ac23a5f73b412
richard67 <richard.fath@t-online.de>
2016-03-07 18:49:47 +0100

    Correct comments in 3.4.0-2014-09.16.sql
    It seems that comments "--" without having a space after it cause
    problems.

--- administrator/components/com_admin/sql/updates/mysql/3.4.0-2014-09-16.sql
+++ administrator/components/com_admin/sql/updates/mysql/3.4.0-2014-09-16.sql
@@ -3,4 +3,4 @@ ALTER TABLE `#__redirect_links` ADD COLUMN `header` smallint(3) NOT NULL DEFAULT
 -- The following statement has to be disabled because it conflicts with
 -- a later change added with Joomla! 3.5.0 for long URLs in this table
 --
---ALTER TABLE `#__redirect_links` MODIFY `new_url` varchar(255);
+-- ALTER TABLE `#__redirect_links` MODIFY `new_url` varchar(255);
