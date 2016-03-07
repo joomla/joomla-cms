b48668fb7856c4779e08688632004df05ed1d404
Michael Babker <mbabker@flbab.com>
2016-03-07 10:02:51 -0500

    Version bump

--- libraries/cms/version/version.php
+++ libraries/cms/version/version.php
@@ -38,7 +38,7 @@ final class JVersion
 	 * @var    string
 	 * @since  3.5
 	 */
-	const DEV_LEVEL = '0-beta4';
+	const DEV_LEVEL = '0-beta5';
 
 	/**
 	 * Development status.
@@ -78,7 +78,7 @@ final class JVersion
 	 * @var    string
 	 * @since  3.5
 	 */
-	const RELTIME = '01:00';
+	const RELTIME = '15:30';
 
 	/**
 	 * Release timezone.
