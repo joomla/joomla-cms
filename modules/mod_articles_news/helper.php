b7c14bd6ad397198a2fe17e893fcdbf4d4661894
CoalaWeb <support@coalaweb.com>
2016-03-10 12:59:20 -0500

    Small code style changes to pass travis.

--- modules/mod_articles_news/helper.php
+++ modules/mod_articles_news/helper.php
@@ -3,7 +3,7 @@
  * @package     Joomla.Site
  * @subpackage  mod_articles_news
  *
- * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
+ * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
@@ -34,13 +34,12 @@ abstract class ModArticlesNewsHelper
 	 */
 	public static function getList(&$params)
 	{
-		$app = JFactory::getApplication();
-
 		// Get an instance of the generic articles model
 		$model = JModelLegacy::getInstance('Articles', 'ContentModel', array('ignore_request' => true));
 
 		// Set application parameters in model
-		$appParams = JFactory::getApplication()->getParams();
+		$app       = JFactory::getApplication();
+		$appParams = $app->getParams();
 		$model->setState('params', $appParams);
 
 		// Set the filters based on the module params
@@ -96,23 +95,19 @@ abstract class ModArticlesNewsHelper
 				$item->linkText = JText::_('MOD_ARTICLES_NEWS_READMORE_REGISTER');
 			}
 
-                    $item->introtext = JHtml::_('content.prepare', $item->introtext, '', 'mod_articles_news.content');
-                
+			$item->introtext = JHtml::_('content.prepare', $item->introtext, '', 'mod_articles_news.content');
 
-			// New
 			if (!$params->get('image'))
 			{
 				$item->introtext = preg_replace('/<img[^>]*>/', '', $item->introtext);
 			}
 
-                $results = $app->triggerEvent('onContentAfterDisplay', array('mod_articles_news.content', &$item, &$params, 1));
-                $item->afterDisplayTitle = trim(implode("\n", $results));
+			$results                 = $app->triggerEvent('onContentAfterDisplay', array('mod_articles_news.content', &$item, &$params, 1));
+			$item->afterDisplayTitle = trim(implode("\n", $results));
 
-                $results = $app->triggerEvent('onContentBeforeDisplay', array('mod_articles_news.content', &$item, &$params, 1));
-                $item->beforeDisplayContent = trim(implode("\n", $results));
-            
-
-        }
+			$results                    = $app->triggerEvent('onContentBeforeDisplay', array('mod_articles_news.content', &$item, &$params, 1));
+			$item->beforeDisplayContent = trim(implode("\n", $results));
+		}
 
 		return $items;
 	}
