9df009fbd824eb51ac3a9be4c39ac366348fec4c
photodude <photodude@users.noreply.github.com>
2016-03-15 13:10:51 +0000

    Add back tags helper folder

--- administrator/components/com_admin/script.php
+++ administrator/components/com_admin/script.php
@@ -1494,7 +1494,6 @@ class JoomlaInstallerScript
 			'/libraries/vendor/symfony/yaml/Symfony/Component/Yaml',
 			'/libraries/vendor/symfony/yaml/Symfony/Component',
 			'/libraries/vendor/symfony/yaml/Symfony',
-			'/administrator/components/com_tags/helpers',
 			'/libraries/joomla/document/error',
 			'/libraries/joomla/document/image',
 			'/libraries/joomla/document/json',
