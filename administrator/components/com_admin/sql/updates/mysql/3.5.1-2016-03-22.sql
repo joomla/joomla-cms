b914f7ddd49b98cf6d82792b3a8b854bd4d174a0
richard67 <richard.fath@t-online.de>
2016-03-23 15:52:28 +0100

    Remove reset of utf8 conversion status
    For forcing an execution of the utf8 or utf8mb4 conversion after an
    update, we have to find another way. Via schema update is not
    practicable.

--- administrator/components/com_admin/sql/updates/mysql/3.5.1-2016-03-22.sql
+++ administrator/components/com_admin/sql/updates/mysql/3.5.1-2016-03-22.sql
@@ -3,11 +3,3 @@
 --
 
 ALTER TABLE `#__user_keys` MODIFY `user_id` varchar(150) NOT NULL;
-
---
--- Reset utf8/utf8mb4 conversion status to force conversion after update
--- because the conversion has been changed (handling of forgotten columns
--- and indexes added)
---
-
-UPDATE `#__utf8_conversion` SET `converted` = 0;
