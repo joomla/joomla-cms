b43cb3f0ecd5d05c7df3a1003c0ddf63135600a2
Nicola Galgano <optimus4joomla@gmail.com>
2016-03-25 18:27:57 +0100

    PostgreSQL: Warning: Database is not up to date
     type fix

--- administrator/components/com_admin/sql/updates/postgresql/3.5.0-2016-03-01.sql
+++ administrator/components/com_admin/sql/updates/postgresql/3.5.0-2016-03-01.sql
@@ -1,5 +1,5 @@
 ALTER TABLE "#__redirect_links" DROP CONSTRAINT "#__redirect_links_idx_link_old";
-ALTER TABLE "#__redirect_links" ALTER COLUMN "old_url" TYPE varchar(2048);
-ALTER TABLE "#__redirect_links" ALTER COLUMN "new_url" TYPE varchar(2048);
-ALTER TABLE "#__redirect_links" ALTER COLUMN "referer" TYPE varchar(2048);
+ALTER TABLE "#__redirect_links" ALTER COLUMN "old_url" TYPE character varying(2048);
+ALTER TABLE "#__redirect_links" ALTER COLUMN "new_url" TYPE character varying(2048);
+ALTER TABLE "#__redirect_links" ALTER COLUMN "referer" TYPE character varying(2048);
 CREATE INDEX "#__idx_link_old" ON "#__redirect_links" ("old_url");
