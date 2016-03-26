2e14033023e92be84ff211b322048eb4c10a47f5
zero-24 <zero-24@users.noreply.github.com>
2016-03-26 16:21:24 +0100

    Update default_nodownload.php

--- administrator/components/com_joomlaupdate/views/default/tmpl/default_nodownload.php
+++ administrator/components/com_joomlaupdate/views/default/tmpl/default_nodownload.php
@@ -16,6 +16,5 @@ defined('_JEXEC') or die;
     <?php echo JText::_('COM_JOOMLAUPDATE_VIEW_DEFAULT_NO_DOWNLOAD_URL'); ?>
   </legend>
   <p>
-    <?php echo JText::sprintf('COM_JOOMLAUPDATE_VIEW_DEFAULT_NO_DOWNLOAD_URL_DESC', $this->updateInfo['latest'], 'https://www.joomla.org/download.html'); ?>
-  </p>
+    <?php echo JText::sprintf('COM_JOOMLAUPDATE_VIEW_DEFAULT_NO_DOWNLOAD_URL_DESC', $this->updateInfo['latest']);
 </fieldset>
