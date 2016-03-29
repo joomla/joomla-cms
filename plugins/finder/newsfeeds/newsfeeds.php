f315baae894bf7b510a0aa045d518a56a2157605
Miguel Gouveia <miguelbgouveia@hotmail.com>
2016-03-29 11:53:54 +0100

    Correction in the change state verifycation

--- plugins/finder/newsfeeds/newsfeeds.php
+++ plugins/finder/newsfeeds/newsfeeds.php
@@ -168,9 +168,9 @@ class PlgFinderNewsfeeds extends FinderIndexerAdapter
 				$this->categoryAccessChange($row);
 			}
 			// Check if the state are different.
-			if (!$isNew && $this->old_catstate != $row->state)
+			if (!$isNew && $this->old_catstate != $row->published)
 			{
-				$this->categoryStateChange($row);
+				$this->updateStateByCategoryId($row->id);
 			}
 		}
 
