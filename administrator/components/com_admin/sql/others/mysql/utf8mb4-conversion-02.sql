f72fc570d302c3200dcb27f3534549a11df94225
richard67 <richard.fath@t-online.de>
2016-03-29 15:14:49 +0200

    Remove duplicate statement in conversion script
    Removed duplicate modification of column `name` in table `#__users`

--- administrator/components/com_admin/sql/others/mysql/utf8mb4-conversion-02.sql
+++ administrator/components/com_admin/sql/others/mysql/utf8mb4-conversion-02.sql
@@ -134,7 +134,6 @@ ALTER TABLE `#__menu` MODIFY `alias` varchar(400) CHARACTER SET utf8mb4 COLLATE 
 ALTER TABLE `#__newsfeeds` MODIFY `alias` varchar(400) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '';
 ALTER TABLE `#__tags` MODIFY `alias` varchar(400) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '';
 ALTER TABLE `#__ucm_content` MODIFY `core_alias` varchar(400) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '';
-ALTER TABLE `#__users` MODIFY `name` varchar(400) NOT NULL DEFAULT '';
 ALTER TABLE `#__utf8_conversion` MODIFY `md5_file1` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '';
 ALTER TABLE `#__utf8_conversion` MODIFY `md5_file2` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '';
 
