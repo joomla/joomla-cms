4c5716971b7f8fc968e0bc2a0a4a19d490948bdf
laoneo <allon.moritz@digital-peak.com>
2016-04-01 16:59:17 +0200

    Move render and edit class to the options tab
    Closes #25

--- administrator/components/com_fields/views/field/tmpl/edit.php
+++ administrator/components/com_fields/views/field/tmpl/edit.php
@@ -2,7 +2,7 @@
 /**
  * @package     Joomla.Administrator
  * @subpackage  com_fields
- * 
+ *
  * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
@@ -57,8 +57,6 @@ JFactory::getDocument()->addScriptDeclaration('
 				echo $this->form->renderField('label');
 				echo $this->form->renderField('required');
 				echo $this->form->renderField('default_value');
-				echo $this->form->renderField('render_class');
-				echo $this->form->renderField('class');
 
 				// rendering additional fields
 				foreach ($this->form->getFieldsets('fieldparams') as $name => $fieldSet)
@@ -101,11 +99,11 @@ JFactory::getDocument()->addScriptDeclaration('
 			</div>
 		</div>
 		<?php echo JHtml::_('bootstrap.endTab'); ?>
-		
+
 		<?php
 		$this->set('ignore_fieldsets', array('fieldparams'));
 		echo JLayoutHelper::render('joomla.edit.params', $this); ?>
-		
+
 		<?php if ($this->canDo->get('core.admin')) : ?>
 			<?php echo JHtml::_('bootstrap.addTab', 'myTab', 'rules', JText::_('COM_FIELDS_VIEW_FIELD_FIELDSET_RULES', true)); ?>
 			<?php echo $this->form->getInput('rules'); ?>
