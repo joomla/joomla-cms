0d72de6af5ff52ec7129755ed776740e5bc7865a
Shubham Rajput <shubham.rajput2013@vit.ac.in>
2016-04-08 02:09:32 +0530

    Update default_enabled.php

--- administrator/modules/mod_menu/tmpl/default_enabled.php
+++ administrator/modules/mod_menu/tmpl/default_enabled.php
@@ -103,7 +103,7 @@ if ($user->authorise('core.manage', 'com_users'))
 	{
 		$menu->addChild(
 			new JMenuNode(
-				JText::_('MOD_MENU_COM_CONTENT_NEW_CATEGORY'), 'index.php?option=com_categories&task=category.add&extension=com_users',
+				JText::_('MOD_MENU_COM_USERS_NOTE_NEW_CATEGORY'), 'index.php?option=com_categories&task=category.add&extension=com_users',
 				'class:newarticle'
 			)
 		);
