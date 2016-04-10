e0eb2c8c9512f83bc3f642c251f0110d99d454b1
stutteringp0et <github@richeyweb.com>
2016-04-10 13:41:04 -0500

    JApplicationWeb::setHeader behavior
    Formatting changes

--- libraries/joomla/application/web.php
+++ libraries/joomla/application/web.php
@@ -624,25 +624,37 @@ class JApplicationWeb extends JApplicationBase
 		// Sanitize the input values.
 		$name = (string) $name;
 		$value = (string) $value;
+
+		// Create an array of names to search for duplicates
+		$key = false;
+		if(count($this->response->headers))
+		{
+			$names = array();
+			foreach($this->response->headers as $key => $header)
+			{
+				$names[$key] = $header['name'];
+			}
+			
+			// Find existing headers by name
+			$key = array_search($name,$names); 
+		}
 		
-		// create an array of names to search for duplicates
-                $key = false;
-                if(count($this->response->headers)) {
-                	$names = array();
-	                foreach($this->response->headers as $key=>$header) {
-	                    $names[$key]=$header['name'];
-	                }
-                	$key = array_search($name,$names); // find existing headers by name
-                }
-                if($key !== false && $replace || $key === false) { // found & replace or not found
-                    if($key !== false) { // remove before insert when header is found
-                        unset($this->response->headers[$key]);
-                        // Clean up the array as unsetting nested arrays leaves some junk.
-                        $this->response->headers = array_values($this->response->headers);
-                    }
-                    // Add the header to the internal array.
-                    $this->response->headers[] = array('name' => $name, 'value' => $value);
-                }
+		// Found & replace or not found
+		if($key !== false && $replace || $key === false)
+		{
+			
+			// remove before insert when header is found
+			if($key !== false)
+			{ 
+				unset($this->response->headers[$key]);
+				
+				// Clean up the array as unsetting nested arrays leaves some junk.
+				$this->response->headers = array_values($this->response->headers);
+			}
+			
+			// Add the header to the internal array.
+			$this->response->headers[] = array('name' => $name, 'value' => $value);
+		}
 
 		return $this;
 	}
@@ -651,8 +663,7 @@ class JApplicationWeb extends JApplicationBase
 	 * Method to get the array of response headers to be sent when the response is sent
 	 * to the client.
 	 *
-	 * @return  array
-	 *
+	 * @return  array	 *
 	 * @since   11.3
 	 */
 	public function getHeaders()
