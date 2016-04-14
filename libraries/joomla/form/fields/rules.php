0831dd56580f516bb50111bb214d0bcbd387e653
infograf768 <infografjms@gmail.com>
2016-04-14 21:27:06 +0200

    Correcting tooltip double translations. (#9915)

--- libraries/joomla/form/fields/rules.php
+++ libraries/joomla/form/fields/rules.php
@@ -267,7 +267,7 @@ class JFormFieldRules extends JFormField
 				$html[] = '<tr>';
 				$html[] = '<td headers="actions-th' . $group->value . '">';
 				$html[] = '<label for="' . $this->id . '_' . $action->name . '_' . $group->value . '" class="hasTooltip" title="'
-					. JHtml::_('tooltipText', JText::_($action->title), JText::_($action->description)) . '">';
+					. JHtml::_('tooltipText', $action->title, $action->description) . '">';
 				$html[] = JText::_($action->title);
 				$html[] = '</label>';
 				$html[] = '</td>';
