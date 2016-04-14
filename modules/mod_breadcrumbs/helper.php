eb675f0c2a3b73013c2195a6daa36c3619e56c7a
Brian Teeman <brian@teeman.net>
2016-04-14 22:41:13 +0100

    add dimensions to breadcrumb seperator

--- modules/mod_breadcrumbs/helper.php
+++ modules/mod_breadcrumbs/helper.php
@@ -87,11 +87,11 @@ class ModBreadCrumbsHelper
 		{
 			if ($lang->isRtl())
 			{
-				$_separator = JHtml::_('image', 'system/arrow_rtl.png', null, null, true);
+				$_separator = JHtml::_('image', 'system/arrow_rtl.png', null, array('width' => '9px', 'height' => '9px'), true);
 			}
 			else
 			{
-				$_separator = JHtml::_('image', 'system/arrow.png', null, null, true);
+				$_separator = JHtml::_('image', 'system/arrow.png',null, array('width' => '9px', 'height' => '9px'), true);
 			}
 		}
 		else
