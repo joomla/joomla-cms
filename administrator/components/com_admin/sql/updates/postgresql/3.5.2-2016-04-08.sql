b87a347fc9a56a3a198c85e5ccdbb81a84b92091
andrepereiradasilva <andrepereiradasilva@users.noreply.github.com>
2016-04-16 19:21:58 +0100

    use 801 id instead of 650

--- administrator/components/com_admin/sql/updates/postgresql/3.5.2-2016-04-08.sql
+++ administrator/components/com_admin/sql/updates/postgresql/3.5.2-2016-04-08.sql
@@ -1,4 +1,4 @@
 INSERT INTO "#__extensions" ("extension_id", "name", "type", "element", "folder", "client_id", "enabled", "access", "protected", "manifest_cache", "params", "custom_data", "system_data", "checked_out", "checked_out_time", "ordering", "state") VALUES
-(650, 'English (United Kingdom)', 'package', 'pkg_en-GB', '', 0, 1, 1, 1, '', '', '', '', 0, '1970-01-01 00:00:00', 0, 0);
+(801, 'English (United Kingdom)', 'package', 'pkg_en-GB', '', 0, 1, 1, 1, '', '', '', '', 0, '1970-01-01 00:00:00', 0, 0);
 
-UPDATE "#__update_sites_extensions" SET "extension_id" = 650 WHERE "update_site_id" = 3 AND "extension_id" = 600;
+UPDATE "#__update_sites_extensions" SET "extension_id" = 801 WHERE "update_site_id" = 3 AND "extension_id" = 600;
