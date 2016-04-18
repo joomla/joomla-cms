42305a2969c1004c952b2a048411a24b2f3d85b9
andrepereiradasilva <andrepereiradasilva@users.noreply.github.com>
2016-04-18 19:28:16 +0100

    make menu item alias type generate a correct alias from title instead of a data alias

--- libraries/legacy/table/menu.php
+++ libraries/legacy/table/menu.php
@@ -106,7 +106,7 @@ class JTableMenu extends JTableNested
 		// If the alias field is empty, set it to the title.
 		$this->alias = trim($this->alias);
 
-		if ((empty($this->alias)) && ($this->type != 'alias' && $this->type != 'url'))
+		if ((empty($this->alias)) && ($this->type != 'url'))
 		{
 			$this->alias = $this->title;
 		}
