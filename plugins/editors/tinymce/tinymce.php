012b744b2758f5c05441b49304b06da92ef936f6
Christiane Maier-Stadtherr <dev@chmst.de>
2016-04-19 18:09:54 +0200

    Implement the global text filter settings to the Tiny MCE editor

--- plugins/editors/tinymce/tinymce.php
+++ plugins/editors/tinymce/tinymce.php
@@ -292,9 +292,28 @@ class PlgEditorTinymce extends JPlugin
 				$forcenewline = "force_br_newlines : false, force_p_newlines : true, forced_root_block : 'p',";
 			}
 
-			$invalid_elements  = $this->params->get('invalid_elements', 'script,applet,iframe');
-			$extended_elements = $this->params->get('extended_elements', '');
-			$valid_elements    = $this->params->get('valid_elements', '');
+			// Text filtering
+			if ($this->params->get('use_config_textfilters', 0))
+			{
+				// Use filters from com_config
+				$filter = self::filterText(true);
+
+				$tagBlacklist	= !empty($filter->tagBlacklist) ? $filter->tagBlacklist : array();
+				$attrBlacklist = !empty($filter->attrBlacklist) ? $filter->attrBlacklist : array();
+				$tagArray		= !empty($filter->tagArray) ? $filter->tagArray : array();
+				$attrArray		= !empty($filter->attrArray) ? $filter->attrArray : array();
+
+				$invalid_elements	= implode(',', array_merge($tagBlacklist, $attrBlacklist, $tagArray, $attrArray));
+				$extended_elements	= '';
+				$valid_elements		= '';
+			}
+			else
+			{
+				// Use filters from Tiny Params
+				$invalid_elements	= $this->params->get('invalid_elements', 'script,applet,iframe');
+				$extended_elements = $this->params->get('extended_elements', '');
+				$valid_elements		= $this->params->get('valid_elements', '');
+			}
 
 			// Advanced Options
 			$access = JFactory::getUser()->getAuthorisedViewLevels();
@@ -1120,5 +1139,186 @@ class PlgEditorTinymce extends JPlugin
 				'names'  => $btnsNames,
 				'script' => $tinyBtns
 		);
+	}
+
+	/**
+	 * Get the global text filters to arbitrary text as per settings for current user groups
+	 *
+	 * @param   string  $text  The string to filter
+	 *
+	 * @return  string  The filtered string
+	 *
+	 * @since   2.5
+	 */
+	public static function filterText($text)
+	{
+		// Punyencoding utf8 email addresses
+		$text = JFilterInput::getInstance()->emailToPunycode($text);
+
+		// Filter settings
+		$config   = JComponentHelper::getParams('com_config');
+		$user    = JFactory::getUser();
+		$userGroups = JAccess::getGroupsByUser($user->get('id'));
+
+		$filters = $config->get('filters');
+
+		$blackListTags    = array();
+		$blackListAttributes = array();
+
+		$customListTags    = array();
+		$customListAttributes = array();
+
+		$whiteListTags    = array();
+		$whiteListAttributes = array();
+
+		$whiteList  = false;
+		$blackList  = false;
+		$customList = false;
+		$unfiltered = false;
+
+		// Cycle through each of the user groups the user is in.
+		// Remember they are included in the Public group as well.
+		foreach ($userGroups as $groupId)
+		{
+			// May have added a group but not saved the filters.
+			if (!isset($filters->$groupId))
+			{
+				continue;
+			}
+
+			// Each group the user is in could have different filtering properties.
+			$filterData = $filters->$groupId;
+			$filterType = strtoupper($filterData->filter_type);
+
+			if ($filterType == 'NH')
+			{
+				// Maximum HTML filtering.
+			}
+			elseif ($filterType == 'NONE')
+			{
+				// No HTML filtering.
+				$unfiltered = true;
+			}
+			else
+			{
+				// Black or white list.
+				// Preprocess the tags and attributes.
+				$tags           = explode(',', $filterData->filter_tags);
+				$attributes     = explode(',', $filterData->filter_attributes);
+				$tempTags       = array();
+				$tempAttributes = array();
+
+				foreach ($tags as $tag)
+				{
+					$tag = trim($tag);
+
+					if ($tag)
+					{
+						$tempTags[] = $tag;
+					}
+				}
+
+				foreach ($attributes as $attribute)
+				{
+					$attribute = trim($attribute);
+
+					if ($attribute)
+					{
+						$tempAttributes[] = $attribute;
+					}
+				}
+
+				// Collect the black or white list tags and attributes.
+				// Each list is cummulative.
+				if ($filterType == 'BL')
+				{
+					$blackList           = true;
+					$blackListTags       = array_merge($blackListTags, $tempTags);
+					$blackListAttributes = array_merge($blackListAttributes, $tempAttributes);
+				}
+				elseif ($filterType == 'CBL')
+				{
+					// Only set to true if Tags or Attributes were added
+					if ($tempTags || $tempAttributes)
+					{
+						$customList           = true;
+						$customListTags       = array_merge($customListTags, $tempTags);
+						$customListAttributes = array_merge($customListAttributes, $tempAttributes);
+					}
+				}
+				elseif ($filterType == 'WL')
+				{
+					$whiteList           = true;
+					$whiteListTags       = array_merge($whiteListTags, $tempTags);
+					$whiteListAttributes = array_merge($whiteListAttributes, $tempAttributes);
+				}
+			}
+		}
+
+		// Remove duplicates before processing (because the black list uses both sets of arrays).
+		$blackListTags        = array_unique($blackListTags);
+		$blackListAttributes  = array_unique($blackListAttributes);
+		$customListTags       = array_unique($customListTags);
+		$customListAttributes = array_unique($customListAttributes);
+		$whiteListTags        = array_unique($whiteListTags);
+		$whiteListAttributes  = array_unique($whiteListAttributes);
+
+		// Unfiltered assumes first priority.
+		if ($unfiltered)
+		{
+			// Dont apply filtering.
+		}
+		else
+		{
+			// Custom blacklist precedes Default blacklist
+			if ($customList)
+			{
+				$filter = JFilterInput::getInstance(array(), array(), 1, 1);
+
+				// Override filter's default blacklist tags and attributes
+				if ($customListTags)
+				{
+					$filter->tagBlacklist = $customListTags;
+				}
+
+				if ($customListAttributes)
+				{
+					$filter->attrBlacklist = $customListAttributes;
+				}
+			}
+			// Black lists take second precedence.
+			elseif ($blackList)
+			{
+				// Remove the white-listed tags and attributes from the black-list.
+				$blackListTags       = array_diff($blackListTags, $whiteListTags);
+				$blackListAttributes = array_diff($blackListAttributes, $whiteListAttributes);
+
+				$filter = JFilterInput::getInstance($blackListTags, $blackListAttributes, 1, 1);
+
+				// Remove white listed tags from filter's default blacklist
+				if ($whiteListTags)
+				{
+					$filter->tagBlacklist = array_diff($filter->tagBlacklist, $whiteListTags);
+				}
+				// Remove white listed attributes from filter's default blacklist
+				if ($whiteListAttributes)
+				{
+					$filter->attrBlacklist = array_diff($filter->attrBlacklist, $whiteListAttributes);
+				}
+			}
+			// White lists take third precedence.
+			elseif ($whiteList)
+			{
+				// Turn off XSS auto clean
+				$filter = JFilterInput::getInstance($whiteListTags, $whiteListAttributes, 0, 0, 0);
+			}
+			// No HTML takes last place.
+			else
+			{
+				$filter = JFilterInput::getInstance();
+			}
+
+			return $filter;
+		}
 	}
 }
