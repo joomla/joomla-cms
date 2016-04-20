ab93648e3cab7a5be6f79fced6ab553a01710724
andrepereiradasilva <andrepereiradasilva@users.noreply.github.com>
2016-04-20 22:55:38 +0100

    update __content_types type

--- administrator/components/com_admin/sql/updates/sqlazure/3.5.2-2016-04-20.sql
+++ administrator/components/com_admin/sql/updates/sqlazure/3.5.2-2016-04-20.sql
@@ -1,1 +1,2 @@
 UPDATE [#__categories] SET [extension] = 'com_users.notes' WHERE [extension] = 'com_users';
+UPDATE [#__content_types] SET [type_alias] = 'com_users.notes.category' WHERE [type_alias] = 'com_users.category';
