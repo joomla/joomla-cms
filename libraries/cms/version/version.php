72ba4a0f36894faae7828e58ac9223bf2394d738
Michael Babker <mbabker@flbab.com>
2016-04-25 10:13:07 -0500

    Version bump dev branch

--- libraries/cms/version/version.php
+++ libraries/cms/version/version.php
@@ -30,7 +30,7 @@ final class JVersion
 	 * @var    string
 	 * @since  3.5
 	 */
-	const RELEASE = '3.5';
+	const RELEASE = '3.6';
 
 	/**
 	 * Maintenance version.
@@ -38,7 +38,7 @@ final class JVersion
 	 * @var    string
 	 * @since  3.5
 	 */
-	const DEV_LEVEL = '2-dev';
+	const DEV_LEVEL = '0-dev';
 
 	/**
 	 * Development status.
