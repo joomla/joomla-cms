77fde3d601ce6452cfb2a7cd10564e67c99dd08b
zero-24 <zero-24@users.noreply.github.com>
2016-04-26 16:25:40 +0200

    one missing line

--- components/com_contact/views/categories/tmpl/default_items.php
+++ components/com_contact/views/categories/tmpl/default_items.php
@@ -33,8 +33,7 @@ $class = ' class="first"';
 					<?php endif; ?>
 					<?php if (count($item->getChildren()) > 0 && $this->maxLevelcat > 1) : ?>
 						<a id="category-btn-<?php echo $item->id; ?>" href="#category-<?php echo $item->id; ?>"
-							data-toggle="collapse" data-toggle="button" class="btn btn-mini pull-right"><span class="icon-plus"></span>
-						</a>
+							data-toggle="collapse" data-toggle="button" class="btn btn-mini pull-right"><span class="icon-plus"></span></a>
 					<?php endif; ?>
 				</h3>
 				<?php if ($this->params->get('show_subcat_desc_cat') == 1) : ?>
