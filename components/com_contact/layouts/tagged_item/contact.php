595b35d06902102fcef067e7b3b582142cb111ef
Thomas Hunziker <werbemails@bakual.ch>
2016-05-04 23:16:38 +0200

    Add JLayouts

---
+++ components/com_contact/layouts/tagged_item/contact.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  Layout
+ *
+ * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('JPATH_BASE') or die;
+
+$item = $displayData['item'];
+JModelLegacy::addIncludePath('components/com_contact/models');
+$model   = JModelLegacy::getInstance('contact', 'ContactModel');
+$contact = $model->getItem($item->content_item_id);
+
+$lang = JFactory::getLanguage()->load('com_contact');
+?>
+<h3>
+	<a href="<?php echo JRoute::_(TagsHelperRoute::getItemRoute($item->content_item_id, $item->core_alias, $item->core_catid, $item->core_language, $item->type_alias, $item->router)); ?>">
+		<?php echo $this->escape($item->core_title); ?>
+	</a>
+</h3>
+<dl class="dl-horizontal">
+	<?php if ($contact->address) : ?>
+		<dt><?php echo JText::_('COM_CONTACT_ADDRESS') ?></dt>
+		<dd><?php echo $contact->address; ?></dd>
+	<?php endif; ?>
+	<?php if ($contact->country) : ?>
+		<dt><?php echo JText::_('COM_CONTACT_COUNTRY') ?></dt>
+		<dd><?php echo $contact->country; ?></dd>
+	<?php endif; ?>
+</dl>
+<?php if ($item->core_images) : ?>
+	<img src="<?php echo $item->core_images; ?>" class="ss-image pull-right img-polaroid">
+<?php endif; ?>
+<?php echo $item->core_body; ?>
---
+++ layouts/joomla/content/tagged_item.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  Layout
+ *
+ * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('JPATH_BASE') or die;
+
+$item = $displayData['item'];
+?>
+<h3>
+	<a href="<?php echo JRoute::_(TagsHelperRoute::getItemRoute($item->content_item_id, $item->core_alias, $item->core_catid, $item->core_language, $item->type_alias, $item->router)); ?>">
+		<?php echo $this->escape($item->core_title); ?>
+	</a>
+</h3>
+<?php echo $item->core_body; ?>
