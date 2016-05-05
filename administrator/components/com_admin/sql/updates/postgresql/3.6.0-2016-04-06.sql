251625d595eb716facb45941bfa050eb2a2a1c7f
richard67 <richard.fath@t-online.de>
2016-05-05 15:42:22 +0200

    Handle also postgresql just to be safe

---
+++ administrator/components/com_admin/sql/updates/postgresql/3.6.0-2016-04-06.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "#__redirect_links" ALTER COLUMN "old_url" TYPE character varying(2048) NOT NULL;
+ALTER TABLE "#__redirect_links" ALTER COLUMN "referer" TYPE character varying(2048) NOT NULL;
