ff847544a34fad4cbaaf8839928c6d0581b1de7b
Roland Dalmulder <contact@rolandd.com>
2016-05-06 11:09:16 +0200

    Clean up null dates

--- components/com_content/views/featured/tmpl/default_item.php
+++ components/com_content/views/featured/tmpl/default_item.php
@@ -95,7 +95,7 @@ $info    = $this->item->params->get('info_block_position', 0);
 <?php endif; ?>
 
 <?php if ($this->item->state == 0 || strtotime($this->item->publish_up) > strtotime(JFactory::getDate())
-	|| ((strtotime($this->item->publish_down) < strtotime(JFactory::getDate())) && $this->item->publish_down != '0000-00-00 00:00:00' )) : ?>
+	|| ((strtotime($this->item->publish_down) < strtotime(JFactory::getDate())) && $this->item->publish_down != $this->db->getNullDate() )) : ?>
 	</div>
 <?php endif; ?>
 
