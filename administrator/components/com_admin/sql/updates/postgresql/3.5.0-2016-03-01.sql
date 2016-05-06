724dabc6188ac8e60e75e7c18d14f7a469507a36
richard67 <richard.fath@t-online.de>
2016-05-06 11:09:48 +0200

    Change back PostgreSQL
    For PostgreSQL the PR is not necessary I just saw after testing and
    reading manuals.

--- administrator/components/com_admin/sql/updates/postgresql/3.5.0-2016-03-01.sql
+++ administrator/components/com_admin/sql/updates/postgresql/3.5.0-2016-03-01.sql
@@ -1,19 +1,5 @@
 ALTER TABLE "#__redirect_links" DROP CONSTRAINT "#__redirect_links_idx_link_old";
-
---
--- The following statement had to be modified for 3.6.0 by adding the
--- NOT NULL, to be consistent with new install.
--- See also 3.6.0-2016-04-06.sql for updating 3.5.0 or 3.5.1
---
-ALTER TABLE "#__redirect_links" ALTER COLUMN "old_url" TYPE character varying(2048) NOT NULL;
-
+ALTER TABLE "#__redirect_links" ALTER COLUMN "old_url" TYPE character varying(2048);
 ALTER TABLE "#__redirect_links" ALTER COLUMN "new_url" TYPE character varying(2048);
-
---
--- The following statement had to be modified for 3.6.0 by adding the
--- NOT NULL, to be consistent with new install.
--- See also 3.6.0-2016-04-06.sql for updating 3.5.0 or 3.5.1
---
-ALTER TABLE "#__redirect_links" ALTER COLUMN "referer" TYPE character varying(2048) NOT NULL;
-
+ALTER TABLE "#__redirect_links" ALTER COLUMN "referer" TYPE character varying(2048);
 CREATE INDEX "#__idx_link_old" ON "#__redirect_links" ("old_url");
