2514cdb70ee55ce19fcb2c781d8bc2805511e060
Nicola Galgano <optimus4joomla@gmail.com>
2016-05-07 17:49:23 +0200

    added postgresql for #4115
    added postgresql for #4115

---
+++ administrator/components/com_admin/sql/updates/postgresql/3.6.0-2016-05-07.sql
@@ -0,0 +1,6 @@
+CREATE INDEX "idx_published_ordering" ON "#__languages" ("published","ordering");
+CREATE INDEX "idx_client_id" ON "#__template_styles" ("client_id");
+CREATE INDEX "idx_alias_item_id" ON "#__contentitem_tag_map" ("type_alias","content_item_id");
+CREATE INDEX "idx_type_ordering" ON "#__extensions" ("type","ordering");
+CREATE INDEX "idx_client_id_published_lft" ON "#__menu" ("client_id","published","lft");
+CREATE INDEX "iidx_ordering_title" ON "#__viewlevels" ("ordering","title");
