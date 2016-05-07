145d286b709e592189431e2addd4f04b918cf33b
Nicola Galgano <optimus4joomla@gmail.com>
2016-05-07 18:33:23 +0200

    code style
    code style

--- administrator/components/com_admin/sql/updates/sqlazure/3.6.0-2016-05-07.sql
+++ administrator/components/com_admin/sql/updates/sqlazure/3.6.0-2016-05-07.sql
@@ -1,65 +1,36 @@
 CREATE NONCLUSTERED INDEX [idx_published_ordering] ON [#__languages]
-
 (
-	
-	[published] ASC
-,
+	[published] ASC,
 	[ordering] ASC
-
-)
-WITH (STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF);
+)WITH (STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF);
 
 CREATE NONCLUSTERED INDEX [idx_client_id] ON [#__template_styles]
-
 (
-	
 	[client_id] ASC
-,
-
 )
 WITH (STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF);
 
 CREATE NONCLUSTERED INDEX [idx_alias_item_id] ON [#__contentitem_tag_map]
-
 (
-	
-	[type_alias] ASC
-,
+	[type_alias] ASC,
 	[content_item_id] ASC
-
-)
-WITH (STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF);
+)WITH (STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF);
 
 CREATE NONCLUSTERED INDEX [idx_type_ordering] ON [#__extensions]
-
 (
-	
-	[type] ASC
-,
+	[type] ASC,
 	[ordering] ASC
-
-)
-WITH (STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF);
+)WITH (STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF);
 
 CREATE NONCLUSTERED INDEX [idx_client_id_published_lft] ON [#__menu]
-
 (
-	
-	[client_id] ASC
-,
+	[client_id] ASC,
 	[published] ASC,
 	[lft] ASC
-
-)
-WITH (STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF);
+)WITH (STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF);
 
 CREATE NONCLUSTERED INDEX [idx_ordering_title] ON [#__viewlevels]
-
 (
-	
-	[ordering] ASC
-,
+	[ordering] ASC,
 	[title] ASC
-
-)
-WITH (STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF);
+)WITH (STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF);
