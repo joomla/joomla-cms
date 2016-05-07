e3adefe49b65fcba339fa55e9b5a1b4d46618f1c
Jelle Kok <info@jellekoksolutions.nl>
2016-05-07 19:27:22 +0200

    Fix conflicts

--- administrator/components/com_installer/views/install/tmpl/default.php
+++ administrator/components/com_installer/views/install/tmpl/default.php
@@ -77,12 +77,11 @@ JFactory::getDocument()->addStyleDeclaration(
 
 <script type="text/javascript">
 	// Set the first tab to active if there is no other active tab
-	jQuery(document).ready(function($) {
-		var hasTab = function(href){
+	jQuery(document).ready(function ($) {
+		var hasTab = function (href) {
 			return $('a[data-toggle="tab"]a[href*=' + href + ']').length;
 		};
-		if (!hasTab(localStorage.getItem('tab-href')))
-		{
+		if (!hasTab(localStorage.getItem('tab-href'))) {
 			var tabAnchor = $("#myTabTabs li:first a");
 			window.localStorage.setItem('tab-href', tabAnchor.attr('href'));
 			tabAnchor.click();
@@ -91,55 +90,55 @@ JFactory::getDocument()->addStyleDeclaration(
 </script>
 
 <div id="installer-install" class="clearfix">
-    <form enctype="multipart/form-data" action="<?php echo JRoute::_('index.php?option=com_installer&view=install'); ?>"
-          method="post" name="adminForm" id="adminForm" class="form-horizontal">
-        <?php if (!empty($this->sidebar)) : ?>
-        <div id="j-sidebar-container" class="span2">
-            <?php echo $this->sidebar; ?>
-        </div>
-        <div id="j-main-container" class="span10">
-            <?php else : ?>
-            <div id="j-main-container">
-                <?php endif; ?>
+	<form enctype="multipart/form-data" action="<?php echo JRoute::_('index.php?option=com_installer&view=install'); ?>"
+		method="post" name="adminForm" id="adminForm" class="form-horizontal">
+		<?php if (!empty($this->sidebar)) : ?>
+		<div id="j-sidebar-container" class="span2">
+			<?php echo $this->sidebar; ?>
+		</div>
+		<div id="j-main-container" class="span10">
+			<?php else : ?>
+			<div id="j-main-container">
+				<?php endif; ?>
 
-                <!-- Render messages set by extension install scripts here -->
-                <?php if ($this->showMessage) : ?>
-                    <?php echo $this->loadTemplate('message'); ?>
-                <?php elseif ($this->showJedAndWebInstaller) : ?>
-                    <div class="alert alert-info j-jed-message"
-                         style="margin-bottom: 40px; line-height: 2em; color:#333333;">
-	                    <?php echo JHtml::_(
+				<!-- Render messages set by extension install scripts here -->
+				<?php if ($this->showMessage) : ?>
+					<?php echo $this->loadTemplate('message'); ?>
+				<?php elseif ($this->showJedAndWebInstaller) : ?>
+					<div class="alert alert-info j-jed-message"
+						style="margin-bottom: 40px; line-height: 2em; color:#333333;">
+						<?php echo JHtml::_(
 							'link',
 							JRoute::_('index.php?option=com_installer&view=discover' . urlencode(base64_encode(JUri::getInstance()))),
 							'&times;',
 							'class="close hasTooltip" data-dismiss="alert" title="' . str_replace('"', '&quot;', JText::_('COM_INSTALLER_SHOW_JED_INFORMATION_TOOLTIP')) . '"'
-							);
-	                    ?>
-                        <p><?php echo JText::_('COM_INSTALLER_INSTALL_FROM_WEB_INFO'); ?>
-                            &nbsp;&nbsp;<?php echo JText::_('COM_INSTALLER_INSTALL_FROM_WEB_TOS'); ?></p>
-                        <input class="btn" type="button"
-                               value="<?php echo JText::_('COM_INSTALLER_INSTALL_FROM_WEB_ADD_TAB'); ?>"
-                               onclick="Joomla.submitbuttonInstallWebInstaller()"/>
-                    </div>
-                <?php endif; ?>
+						);
+						?>
+						<p><?php echo JText::_('COM_INSTALLER_INSTALL_FROM_WEB_INFO'); ?>
+							&nbsp;&nbsp;<?php echo JText::_('COM_INSTALLER_INSTALL_FROM_WEB_TOS'); ?></p>
+						<input class="btn" type="button"
+							value="<?php echo JText::_('COM_INSTALLER_INSTALL_FROM_WEB_ADD_TAB'); ?>"
+							onclick="Joomla.submitbuttonInstallWebInstaller()" />
+					</div>
+				<?php endif; ?>
 
-                <?php echo JHtml::_('bootstrap.startTabSet', 'myTab'); ?>
+				<?php echo JHtml::_('bootstrap.startTabSet', 'myTab'); ?>
 
-                <?php JEventDispatcher::getInstance()->trigger('onInstallerViewBeforeFirstTab', array()); ?>
-                <!-- Extension fieldset of the plugin installer urlFolderInstaller-->
-                <?php JEventDispatcher::getInstance()->trigger('onInstallerViewAfterLastTab', array()); ?>
+				<?php JEventDispatcher::getInstance()->trigger('onInstallerViewBeforeFirstTab', array()); ?>
+				<!-- Extension fieldset of the plugin installer urlFolderInstaller-->
+				<?php JEventDispatcher::getInstance()->trigger('onInstallerViewAfterLastTab', array()); ?>
 
-                <?php if ($this->ftp) : ?>
-                    <?php echo JHtml::_('bootstrap.addTab', 'myTab', 'ftp', JText::_('COM_INSTALLER_MSG_DESCFTPTITLE', true)); ?>
-                    <?php echo $this->loadTemplate('ftp'); ?>
-                    <?php echo JHtml::_('bootstrap.endTab'); ?>
-                <?php endif; ?>
+				<?php if ($this->ftp) : ?>
+					<?php echo JHtml::_('bootstrap.addTab', 'myTab', 'ftp', JText::_('COM_INSTALLER_MSG_DESCFTPTITLE', true)); ?>
+					<?php echo $this->loadTemplate('ftp'); ?>
+					<?php echo JHtml::_('bootstrap.endTab'); ?>
+				<?php endif; ?>
 
-                <input type="hidden" name="type" value=""/>
-                <input type="hidden" name="installtype" value="upload"/>
-                <input type="hidden" name="task" value="install.install"/>
-                <?php echo JHtml::_('form.token'); ?>
+				<input type="hidden" name="type" value="" />
+				<input type="hidden" name="installtype" value="upload" />
+				<input type="hidden" name="task" value="install.install" />
+				<?php echo JHtml::_('form.token'); ?>
 
-                <?php echo JHtml::_('bootstrap.endTabSet'); ?>
-    </form>
+				<?php echo JHtml::_('bootstrap.endTabSet'); ?>
+	</form>
 </div>
