d475a042a2408e2fbcd6d3a19f29b78e0ceaafa4
Niels van der Veer (n9iels) <n.i.vdveer@gmail.com>
2016-05-08 12:49:26 +0200

    Codestyle

--- administrator/templates/isis/error.php
+++ administrator/templates/isis/error.php
@@ -225,7 +225,7 @@ $stickyToolbar = $params->get('stickyToolbar', '1');
 					<blockquote>
 						<span class="label label-inverse"><?php echo $this->error->getCode(); ?></span> <?php echo htmlspecialchars($this->error->getMessage(), ENT_QUOTES, 'UTF-8');?>
 					</blockquote>
-					<?php if ($this->debug) :?>
+					<?php if ($this->debug) : ?>
 						<?php echo $this->renderBacktrace(); ?>
 					<?php endif; ?>
 					<p><a href="<?php echo $this->baseurl; ?>" class="btn"><span class="icon-dashboard"></span> <?php echo JText::_('JGLOBAL_TPL_CPANEL_LINK_TEXT'); ?></a></p>
