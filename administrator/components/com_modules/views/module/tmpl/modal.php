693ba83a252755ff8ae0b6e15d6053c45543a3f9
Cyril Rezé <cyril.reze@joomlic.com>
2016-05-08 16:20:14 +0200

    Fix tooltip placement + container iframe

--- administrator/components/com_modules/views/module/tmpl/modal.php
+++ administrator/components/com_modules/views/module/tmpl/modal.php
@@ -9,6 +9,8 @@
 
 defined('_JEXEC') or die;
 
+JHtml::_('bootstrap.tooltip', '.hasTooltip', array('placement' => 'bottom'));
+
 // This code is needed for proper check out in case of modal close
 JFactory::getDocument()->addScriptDeclaration('
 	window.parent.jQuery(".modal").on("hidden", function () {
@@ -21,6 +23,7 @@ JFactory::getDocument()->addScriptDeclaration('
 <button id="saveBtn" type="button" class="hidden" onclick="Joomla.submitbutton('module.save');"></button>
 <button id="closeBtn" type="button" class="hidden" onclick="Joomla.submitbutton('module.cancel');"></button>
 
-<?php
-$this->setLayout('edit');
-echo $this->loadTemplate();
+<div class="container-popup">
+	<?php $this->setLayout('edit'); ?>
+	<?php echo $this->loadTemplate(); ?>
+</div>
