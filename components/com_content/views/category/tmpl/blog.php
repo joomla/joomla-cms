05a5433f286d9ede0bfc339806d4a8b5f9e9fb20
Sergio Manzi <smz@smz.it>
2016-05-08 23:24:17 +0200

    Filters in Category Blog

--- components/com_content/views/category/tmpl/blog.php
+++ components/com_content/views/category/tmpl/blog.php
@@ -20,6 +20,32 @@ JHtml::_('behavior.caption');
 		</div>
 	<?php endif; ?>
 
+<form action="<?php echo htmlspecialchars(JUri::getInstance()->toString()); ?>" method="post" name="adminForm" id="adminForm" class="form-inline">
+	<?php if ($this->params->get('filter_field') != 'hide') :?>
+	<fieldset class="filters btn-toolbar clearfix">
+		<?php if ($this->params->get('filter_field') != 'hide') :?>
+		      	<div class="btn-group">
+		        	<?php if ($this->params->get('filter_field') != 'tag') :?>
+					<label class="filter-search-lbl element-invisible" for="filter-search">
+						<?php echo JText::_('COM_CONTENT_' . $this->params->get('filter_field') . '_FILTER_LABEL') . '&#160;'; ?>
+					</label>
+					<input type="text" name="filter-search" id="filter-search" value="<?php echo $this->escape($this->state->get('list.filter')); ?>" class="inputbox" onchange="document.adminForm.submit();" title="<?php echo JText::_('COM_CONTENT_FILTER_SEARCH_DESC'); ?>" placeholder="<?php echo JText::_('COM_CONTENT_' . $this->params->get('filter_field') . '_FILTER_LABEL'); ?>" />
+		        	<?php else :?>
+		    			<select name="filter_tag" id="filter_tag" onchange="document.adminForm.submit();" >
+		    				<option value=""><?php echo JText::_('JOPTION_SELECT_TAG'); ?></option>
+		    				<?php echo JHtml::_('select.options', JHtml::_('tag.options', true, true), 'value', 'text', $this->state->get('filter.tag')); ?>
+		    			</select>
+		        	<?php endif; ?>
+		    	</div>
+		<?php endif; ?>
+		<input type="hidden" name="filter_order" value="" />
+		<input type="hidden" name="filter_order_Dir" value="" />
+		<input type="hidden" name="limitstart" value="" />
+		<input type="hidden" name="task" value="" />
+	</fieldset>
+	<?php endif; ?>
+</form>
+
 	<?php if ($this->params->get('show_category_title', 1) or $this->params->get('page_subheading')) : ?>
 		<h2> <?php echo $this->escape($this->params->get('page_subheading')); ?>
 			<?php if ($this->params->get('show_category_title')) : ?>
