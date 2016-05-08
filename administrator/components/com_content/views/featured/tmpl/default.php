b407c7224a082ea5d3ec6a3ce121e2c4960c36cd
Kubik-Rubik <admin@kubik-rubik.de>
2016-05-08 15:01:11 +0200

    Merge branch 'staging' of https://github.com/nishadi/joomla-cms into nishadi-staging
    Fixed Conflicts:
    #	administrator/components/com_content/views/featured/tmpl/default.php

--- administrator/components/com_content/views/featured/tmpl/default.php
+++ administrator/components/com_content/views/featured/tmpl/default.php
@@ -106,6 +106,7 @@ if ($saveOrder)
 							<td class="order nowrap center hidden-phone">
 								<?php
 								$iconClass = '';
+
 								if (!$canChange)
 								{
 									$iconClass = ' inactive';
