6105d0e5eed8c470d66f7480a5dbc412434d6e09
andrepereiradasilva <andrepereiradasilva@users.noreply.github.com>
2016-05-09 12:28:05 +0100

    change to 802 (sqlazure update) and don't use the update_site_id

--- administrator/components/com_admin/sql/updates/sqlazure/3.6.0-2016-04-08.sql
+++ administrator/components/com_admin/sql/updates/sqlazure/3.6.0-2016-04-08.sql
@@ -1,8 +1,8 @@
 SET IDENTITY_INSERT [#__extensions]  ON;
 
 INSERT [#__extensions] ([extension_id], [name], [type], [element], [folder], [client_id], [enabled], [access], [protected], [manifest_cache], [params], [custom_data], [system_data], [checked_out], [checked_out_time], [ordering], [state])
-SELECT 801, 'English (United Kingdom)', 'package', 'pkg_en-GB', '', 0, 1, 1, 1, '', '', '', '', 0, '1900-01-01 00:00:00', 0, 0;
+SELECT 802, 'English (United Kingdom)', 'package', 'pkg_en-GB', '', 0, 1, 1, 1, '', '', '', '', 0, '1900-01-01 00:00:00', 0, 0;
 
 SET IDENTITY_INSERT [#__extensions]  OFF;
 
-UPDATE [#__update_sites_extensions] SET [extension_id] = 801 WHERE [update_site_id] = 3 AND [extension_id] = 600;
+UPDATE [#__update_sites_extensions] SET [extension_id] = 802 WHERE [name] = 'Joomla! Update Component Update Site' AND [type] = 'extension' AND [extension_id] = 600;
