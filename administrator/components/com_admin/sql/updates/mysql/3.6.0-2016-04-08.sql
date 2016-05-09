89ca4e45b8f8aae3a9e900c41ce1e8d3cc4f5df0
andrepereiradasilva <andrepereiradasilva@users.noreply.github.com>
2016-05-09 13:23:40 +0100

    correct update query mysql

--- administrator/components/com_admin/sql/updates/mysql/3.6.0-2016-04-08.sql
+++ administrator/components/com_admin/sql/updates/mysql/3.6.0-2016-04-08.sql
@@ -3,4 +3,12 @@ INSERT INTO `#__extensions` (`extension_id`, `name`, `type`, `element`, `folder`
  VALUES (802, 'English (United Kingdom)', 'package', 'pkg_en-GB', '', 0, 1, 1, 1, '', '', '', '', 0, '0000-00-00 00:00:00', 0, 0);
 
 -- Change update site extension id to the new extension.
-UPDATE `#__update_sites_extensions` SET `extension_id` = 802 WHERE `name` = 'Accredited Joomla! Translations' AND `type` = 'collection' AND `extension_id` = 600;
+UPDATE `#__update_sites_extensions`
+SET `extension_id` = 802
+WHERE `update_site_id` IN (
+						SELECT `update_site_id`
+						FROM `#__update_sites`
+						WHERE `name` = 'Accredited Joomla! Translations'
+						AND `type` = 'collection'
+						)
+AND `extension_id` = 600;
