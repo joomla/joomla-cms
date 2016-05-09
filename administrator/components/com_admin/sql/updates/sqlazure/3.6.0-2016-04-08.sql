4393c07c8f74fe41373516281b9eacca271f867d
andrepereiradasilva <andrepereiradasilva@users.noreply.github.com>
2016-05-09 13:27:14 +0100

    remove extra tabs sqlazure

--- administrator/components/com_admin/sql/updates/sqlazure/3.6.0-2016-04-08.sql
+++ administrator/components/com_admin/sql/updates/sqlazure/3.6.0-2016-04-08.sql
@@ -8,9 +8,9 @@ SET IDENTITY_INSERT [#__extensions]  OFF;
 UPDATE [#__update_sites_extensions]
 SET [extension_id] = 802
 WHERE [update_site_id] IN (
-						SELECT [update_site_id]
-						FROM [#__update_sites]
-						WHERE [name] = 'Accredited Joomla! Translations'
-						AND [type] = 'collection'
-						)
+			SELECT [update_site_id]
+			FROM [#__update_sites]
+			WHERE [name] = 'Accredited Joomla! Translations'
+			AND [type] = 'collection'
+			)
 AND [extension_id] = 600;
