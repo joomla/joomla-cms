8e71c8fff62aace2dfdbb423624daf41acfd0061
Cyril Rezé <cyril.reze@joomlic.com>
2016-05-10 15:57:08 +0200

    adjust ident

--- administrator/components/com_menus/models/fields/menutype.php
+++ administrator/components/com_menus/models/fields/menutype.php
@@ -97,7 +97,7 @@ class JFormFieldMenutype extends JFormFieldList
 				'modalWidth' => '80',
 				'bodyHeight' => '70',
 				'footer'     => '<button type="button" class="btn" data-dismiss="modal" aria-hidden="true">'
-								. JText::_("JLIB_HTML_BEHAVIOR_CLOSE") . '</button>'
+				                . JText::_("JLIB_HTML_BEHAVIOR_CLOSE") . '</button>'
 			)
 		);
 		$html[] = '<input class="input-small" type="hidden" name="' . $this->name . '" value="'
