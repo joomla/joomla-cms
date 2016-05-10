3aa3a818424e38b6617ebf3b84a2016f79447f45
andrepereiradasilva <andrepereiradasilva@users.noreply.github.com>
2016-05-10 12:10:24 +0100

    mod_popular: add tooltip to date

--- administrator/modules/mod_popular/tmpl/default.php
+++ administrator/modules/mod_popular/tmpl/default.php
@@ -34,10 +34,9 @@ JHtml::_('bootstrap.tooltip');
 					</strong>
 				</div>
 				<div class="span3">
-					<span class="small">
-						<span class="icon-calendar"></span>
-						<?php echo JHtml::_('date', $item->created, JText::_('DATE_FORMAT_LC4')); ?>
-					</span>
+					<div class="small hasTooltip" title="<?php echo JHtml::tooltipText('JGLOBAL_FIELD_CREATED_LABEL') . '<br/>' . JHtml::_('date', $item->created, 'Y-m-d H:i:s'); ?>">
+						<span class="icon-calendar"></span> <?php echo JHtml::_('date', $item->created, JText::_('DATE_FORMAT_LC4')); ?>
+					</div>
 				</div>
 			</div>
 		<?php endforeach; ?>
