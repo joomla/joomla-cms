a535eeb72aec98c7d5584493448f3d54d15bdad3
Grigory K <grigorysergeevich@gmail.com>
2016-05-11 20:45:33 +0600

    Add layout article icons

---
+++ layouts/joomla/content/icons/create.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  Layout
+ *
+ * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('JPATH_BASE') or die;
+
+JHtml::_('bootstrap.tooltip');
+
+$params = $displayData['params'];
+$legacy = $displayData['legacy'];
+
+?>
+<?php if ($params->get('show_icons')) : ?>
+	<?php if ($legacy) : ?>
+		<?php echo JHtml::_('image', 'system/new.png', JText::_('JNEW'), null, true); ?>
+	<?php else : ?>
+		<span class="icon-plus"></span>
+		<?php echo JText::_('JNEW'); ?>
+	<?php endif; ?>
+<?php else : ?>
+	<?php echo JText::_('JNEW') . '&#160;'; ?>
+<?php endif; ?>
