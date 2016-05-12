10dd05fec4b29bdd070e1be9cdfe5e7e6f7203c2
Niels van der Veer (n9iels) <n.i.vdveer@gmail.com>
2016-05-12 23:55:15 +0200

    remove unwanted log files

--- logs/error.php
+++
@@ -1,7 +0,0 @@
-#
-#<?php die('Forbidden.'); ?>
-#Date: 2016-05-09 08:44:48 UTC
-#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
-
-#Fields: datetime	priority clientip	category	message
-2016-05-09T08:44:48+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
