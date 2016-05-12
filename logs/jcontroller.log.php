aac579abf6fbf82359ff120a4506846a7e22cc57
Niels van der Veer (n9iels) <n.i.vdveer@gmail.com>
2016-05-12 23:54:14 +0200

    Bring staging up-to-date

---
+++ logs/jcontroller.log.php
@@ -0,0 +1,10 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2016-05-08 19:58:44 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2016-05-08T19:58:44+00:00	INFO ::1	controller	Holding edit ID com_content.edit.article.24 Array (     [0] => 24 ) 
+2016-05-08T19:58:44+00:00	INFO ::1	controller	Checking edit ID com_content.edit.article.24: 1 Array (     [0] => 24 ) 
+2016-05-08T19:59:37+00:00	INFO ::1	controller	Checking edit ID com_content.edit.article.24: 1 Array (     [0] => 24 ) 
+2016-05-08T20:03:49+00:00	INFO ::1	controller	Releasing edit ID com_content.edit.article.24 Array ( ) 
