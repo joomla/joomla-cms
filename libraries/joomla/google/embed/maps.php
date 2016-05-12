9e0b08e0fe28451abd586b0aa0267d05af12024e
Sven Hurt <kontakt@sven-hurt.de>
2016-05-12 23:22:02 +0200

    Add the sensor parameter back to the api url since the tests seem to rely on it
    Add the sensor parameter back to the api url since the tests seem to rely on it. If that really is the case, we should implement be a better way of "parsing" the url and / or looking for an array index in the tests.

--- libraries/joomla/google/embed/maps.php
+++ libraries/joomla/google/embed/maps.php
@@ -782,7 +782,7 @@ class JGoogleEmbedMaps extends JGoogleEmbed
 	 */
 	public function geocodeAddress($address)
 	{
-		$uri = JUri::getInstance('https://maps.googleapis.com/maps/api/geocode/json');
+		$uri = JUri::getInstance('https://maps.googleapis.com/maps/api/geocode/json?sensor=false');
 
 		$uri->setVar('address', urlencode($address));
 
