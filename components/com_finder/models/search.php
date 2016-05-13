58ad25c32cc3a37a190ee5296fecf96be81c49ab
Tino Brackebusch <Tinobee@gmail.com>
2016-05-14 01:37:35 +0200

    Wrong context used to fetch component parameters

--- components/com_finder/models/search.php
+++ components/com_finder/models/search.php
@@ -1068,7 +1068,7 @@ class FinderModelSearch extends JModelList
 		// Get the configuration options.
 		$app = JFactory::getApplication();
 		$input = $app->input;
-		$params = $app->getParams();
+		$params = $app->getParams('com_finder');
 		$user = JFactory::getUser();
 		$filter = JFilterInput::getInstance();
 
