734643d75aa5fb4cf608ab76839034a14ff86d86
Wojciech Smoliński <wojsmol@wp.pl>
2016-05-14 12:44:21 +0200

    fix php error (#14)

--- templates/beez3/html/mod_login/default.php
+++ templates/beez3/html/mod_login/default.php
@@ -17,9 +17,9 @@ JHtml::_('behavior.keepalive');
 	<?php if ($params->get('greeting')) : ?>
 		<div class="login-greeting">
 		<?php if ($params->get('name') == 0) : ?>
-			<?php echo JText::sprintf('MOD_LOGIN_HINAME', htmlspecialchars($user->get('name')), ENT_COMPAT, 'UTF-8'); ?>
+			<?php echo JText::sprintf('MOD_LOGIN_HINAME', htmlspecialchars($user->get('name'), ENT_COMPAT, 'UTF-8')); ?>
 		<?php else : ?>
-		 	<?php echo JText::sprintf('MOD_LOGIN_HINAME', htmlspecialchars($user->get('username')), ENT_COMPAT, 'UTF-8'); ?>
+		 	<?php echo JText::sprintf('MOD_LOGIN_HINAME', htmlspecialchars($user->get('username'), ENT_COMPAT, 'UTF-8')); ?>
 		<?php endif; ?>
 		</div>
 	<?php endif; ?>
