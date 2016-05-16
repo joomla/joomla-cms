26a5537aa69f00c3de5ddc97e7eb391fd8237389
Ruchiranga <truchiranga@gmail.com>
2016-05-16 23:37:39 +0530

    New coeverage reporter plugin test with Travis

--- karma.conf.js
+++ karma.conf.js
@@ -30,12 +30,14 @@ module.exports = function (config) {
 
         // preprocess matching files before serving them to the browser
         // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-        preprocessors: {},
+        preprocessors: {
+            'media/system/js/*.js': ['coverage']
+        },
 
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['progress'],
+        reporters: ['progress', 'coverage'],
 
         // web server port
         port: 9876,
@@ -57,6 +59,13 @@ module.exports = function (config) {
         // Continuous Integration mode
         // if true, Karma captures browsers, runs the tests and exits
         singleRun: false,
+
+        plugins: [
+            'karma-jasmine',
+            'karma-firefox-launcher',
+            'karma-coverage',
+            'karma-requirejs'
+        ],
 
         // Concurrency level
         // how many browser should be started simultaneous
