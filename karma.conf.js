be722df092b29e5f37a7b4d5a66fe33f0cc797bc
Ruchiranga <truchiranga@gmail.com>
2016-05-17 23:55:15 +0530

    Issue 1 Fixing verbose reporter config for travis

--- karma.conf.js
+++ karma.conf.js
@@ -42,7 +42,7 @@ module.exports = function (config) {
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['progress', 'coverage', 'verbose'],
+        reporters: ['coverage', 'verbose'],
 
         // web server port
         port: 9876,
