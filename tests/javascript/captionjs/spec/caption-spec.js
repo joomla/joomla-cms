204487e5da52db335e2b701b9648d1c926f5977f
Ruchiranga <truchiranga@gmail.com>
2016-05-19 12:35:00 +0530

    Issue 4 Improved code standards and jquer selector efficiency

--- tests/javascript/captionjs/spec/caption-spec.js
+++ tests/javascript/captionjs/spec/caption-spec.js
@@ -11,56 +11,57 @@ define(['jquery', 'testsRoot/captionjs/spec/caption-spec-setup', 'jasmineJquery'
 
 	describe('JCaption applied to single image', function () {
 		it('Should have caption as "Joomla Title 1" under image', function () {
-			expect($('#single p')).toHaveText('Joomla Title 1');
+			expect($('#single').find('p')).toHaveText('Joomla Title 1');
 		});
 	});
 
 	describe('JCaption applied for multiple images', function () {
 		it('Should have caption "Joomla Title 1" under image 1', function () {
-			expect($('#multiple p').first()).toHaveText('Joomla Title 1');
+			expect($('#multiple').find('p').first()).toHaveText('Joomla Title 1');
 		});
 
 		it('Should have caption as "Joomla Title 2" under image 2', function() {
-			expect($('#multiple p').last()).toHaveText('Joomla Title 2');
+			expect($('#multiple').find('p').last()).toHaveText('Joomla Title 2');
 		});
 	});
 
 	describe('JCaption with empty title attribute value', function () {
 		it('Should not have a <p> element inside the image container', function () {
-			expect($('#empty img#empty-title').parent()).not.toContainElement('p');
+			expect($('#empty')).not.toContainElement('p');
 		});
 	});
 
 	describe('JCaption with no additional options', function () {
+		var $element = $('img#no-options');
 		it('Should have container CSS as {width: element.width, float: none}', function () {
-			expect($('#options img#no-options').parent()).toHaveCss({
+			expect($element.parent()).toHaveCss({
 				float: 'none',
-				width: $('#options img#no-options').width() + 'px'
+				width: $element.width() + 'px'
 			});
 		});
 	});
 
 	describe('JCaption with additional options', function () {
 		it('Should have 2 elements with class right', function () {
-			expect($('#options .right').length).toEqual(2);
+			expect($('#options').find('.right').length).toEqual(2);
 		});
 
 		it('Should have container width as 100 when element width attribute is set to 100', function () {
-			expect($('#options img#width-attr').parent().width()).toEqual(100);
+			expect($('img#width-attr').parent().width()).toEqual(100);
 		});
 
 		it('Should have container width as 90 when element style is set to width: 90px', function () {
-			expect($('#options img#width-style').parent().width()).toEqual(90);
+			expect($('img#width-style').parent().width()).toEqual(90);
 		});
 
 		it('Should have float: right in container CSS when element attribute align is set to right', function () {
-			expect($('#options img#align-attr').parent()).toHaveCss({
+			expect($('img#align-attr').parent()).toHaveCss({
 				float: 'right'
 			});
 		});
 
 		it('Should have float: right in container CSS when element style is set to float: right', function () {
-			expect($('#options img#align-style').parent()).toHaveCss({
+			expect($('img#align-style').parent()).toHaveCss({
 				float: 'right'
 			});
 		});
