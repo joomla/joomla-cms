3ba4d44b3a3cab5c0c16f4b1fa2adc97d9e9040c
wojsmol <wojsmol@wp.pl>
2016-05-22 02:34:20 +0200

    [mod_stats]Code style Fixes #10592

--- modules/mod_stats/helper.php
+++ modules/mod_stats/helper.php
@@ -76,6 +76,7 @@ class ModStatsHelper
 			$query->select('COUNT(id) AS count_users')
 				->from('#__users');
 			$db->setQuery($query);
+
 			try
 			{
 				$users = $db->loadResult();
@@ -90,6 +91,7 @@ class ModStatsHelper
 				->from('#__content')
 				->where('state = 1');
 			$db->setQuery($query);
+
 			try
 			{
 				$items = $db->loadResult();
@@ -122,6 +124,7 @@ class ModStatsHelper
 					->from('#__weblinks')
 					->where('state = 1');
 				$db->setQuery($query);
+
 				try
 				{
 					$links = $db->loadResult();
@@ -149,6 +152,7 @@ class ModStatsHelper
 				->from('#__content')
 				->where('state = 1');
 			$db->setQuery($query);
+
 			try
 			{
 				$hits = $db->loadResult();
