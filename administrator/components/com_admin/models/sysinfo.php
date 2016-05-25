89fde171d7178baed5d6835ab331468705217b92
Walt Sorensen <photodude@users.noreply.github.com>
2016-05-25 14:04:40 -0600

    Code Style (#10454)
    - When splitting array definitions onto several lines, the last value should also have a trailing comma.
    - Broke a few lines into multiline just for easier reading.

--- administrator/components/com_admin/models/sysinfo.php
+++ administrator/components/com_admin/models/sysinfo.php
@@ -102,7 +102,7 @@ class AdminModelSysInfo extends JModelLegacy
 			'session.save_path',
 			'upload_tmp_dir',
 			'User/Group',
-			'open_basedir'
+			'open_basedir',
 		),
 		'other' => array(
 			'db',
@@ -126,7 +126,7 @@ class AdminModelSysInfo extends JModelLegacy
 			'sitename',
 			'smtphost',
 			'tmp_path',
-			'open_basedir'
+			'open_basedir',
 		)
 	);
 
@@ -274,7 +274,7 @@ class AdminModelSysInfo extends JModelLegacy
 
 		$registry = new Registry(new JConfig);
 		$this->config = $registry->toArray();
-		$hidden = array('host', 'user', 'password', 'ftp_user', 'ftp_pass', 'smtpuser', 'smtppass');
+		$hidden = array('host', 'user', 'password', 'ftp_user', 'ftp_pass', 'smtpuser', 'smtppass',);
 
 		foreach ($hidden as $key)
 		{
@@ -312,7 +312,7 @@ class AdminModelSysInfo extends JModelLegacy
 			'sapi_name'             => php_sapi_name(),
 			'version'               => $version->getLongVersion(),
 			'platform'              => $platform->getLongVersion(),
-			'useragent'             => isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : ""
+			'useragent'             => isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : "",
 		);
 
 		return $this->info;
@@ -525,7 +525,10 @@ class AdminModelSysInfo extends JModelLegacy
 				continue;
 			}
 
-			$this->addDirectory('administrator/language/' . $folder->getFilename(), JPATH_ADMINISTRATOR . '/language/' . $folder->getFilename());
+			$this->addDirectory(
+				'administrator/language/' . $folder->getFilename(),
+				JPATH_ADMINISTRATOR . '/language/' . $folder->getFilename()
+			);
 		}
 
 		// List all manifests folders
@@ -538,7 +541,10 @@ class AdminModelSysInfo extends JModelLegacy
 				continue;
 			}
 
-			$this->addDirectory('administrator/manifests/' . $folder->getFilename(), JPATH_ADMINISTRATOR . '/manifests/' . $folder->getFilename());
+			$this->addDirectory(
+				'administrator/manifests/' . $folder->getFilename(),
+				JPATH_ADMINISTRATOR . '/manifests/' . $folder->getFilename()
+			);
 		}
 
 		$this->addDirectory('administrator/modules', JPATH_ADMINISTRATOR . '/modules');
@@ -558,7 +564,10 @@ class AdminModelSysInfo extends JModelLegacy
 				continue;
 			}
 
-			$this->addDirectory('images/' . $folder->getFilename(), JPATH_SITE . '/' . $cparams->get('image_path') . '/' . $folder->getFilename());
+			$this->addDirectory(
+				'images/' . $folder->getFilename(),
+				JPATH_SITE . '/' . $cparams->get('image_path') . '/' . $folder->getFilename()
+			);
 		}
 
 		$this->addDirectory('language', JPATH_SITE . '/language');
@@ -611,8 +620,16 @@ class AdminModelSysInfo extends JModelLegacy
 
 		if ($public)
 		{
-			$this->addDirectory('log', $registry->get('log_path', JPATH_ADMINISTRATOR . '/logs'), 'COM_ADMIN_LOG_DIRECTORY');
-			$this->addDirectory('tmp', $registry->get('tmp_path', JPATH_ROOT . '/tmp'), 'COM_ADMIN_TEMP_DIRECTORY');
+			$this->addDirectory(
+				'log',
+				$registry->get('log_path', JPATH_ADMINISTRATOR . '/logs'),
+				'COM_ADMIN_LOG_DIRECTORY'
+			);
+			$this->addDirectory(
+				'tmp',
+				$registry->get('tmp_path', JPATH_ROOT . '/tmp'),
+				'COM_ADMIN_TEMP_DIRECTORY'
+			);
 		}
 		else
 		{
@@ -621,7 +638,11 @@ class AdminModelSysInfo extends JModelLegacy
 				$registry->get('log_path', JPATH_ADMINISTRATOR . '/logs'),
 				'COM_ADMIN_LOG_DIRECTORY'
 			);
-			$this->addDirectory($registry->get('tmp_path', JPATH_ROOT . '/tmp'), $registry->get('tmp_path', JPATH_ROOT . '/tmp'), 'COM_ADMIN_TEMP_DIRECTORY');
+			$this->addDirectory(
+				$registry->get('tmp_path', JPATH_ROOT . '/tmp'),
+				$registry->get('tmp_path', JPATH_ROOT . '/tmp'),
+				'COM_ADMIN_TEMP_DIRECTORY'
+			);
 		}
 
 		return $this->directories;
@@ -640,7 +661,7 @@ class AdminModelSysInfo extends JModelLegacy
 	 */
 	private function addDirectory($name, $path, $message = '')
 	{
-		$this->directories[$name] = array('writable' => is_writable($path), 'message' => $message);
+		$this->directories[$name] = array('writable' => is_writable($path), 'message' => $message,);
 	}
 
 	/**
@@ -697,7 +718,7 @@ class AdminModelSysInfo extends JModelLegacy
 					// 3cols
 					if (preg_match($p2, $val, $matchs))
 					{
-						$r[$name][trim($matchs[1])] = array(trim($matchs[2]), trim($matchs[3]));
+						$r[$name][trim($matchs[1])] = array(trim($matchs[2]), trim($matchs[3]),);
 					}
 					// 2cols
 					elseif (preg_match($p3, $val, $matchs))
