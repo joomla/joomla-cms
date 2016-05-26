0c5b4360e09c1710d07421d2498dd9e50d1bbe2a
andrepereiradasilva <andrepereiradasilva@users.noreply.github.com>
2016-05-26 17:37:15 +0100

    index.php since JRoute doesn't work here

--- administrator/templates/system/error.php
+++ administrator/templates/system/error.php
@@ -35,7 +35,7 @@ if (is_callable(array($doc, 'setHtml5')))
 		<tr>
 			<td style="text-align: center;">
 				<p><?php echo htmlspecialchars($this->error->getMessage(), ENT_QUOTES, 'UTF-8'); ?></p>
-				<p><a href="<?php JRoute::_('index.php'); ?>"><?php echo JText::_('JGLOBAL_TPL_CPANEL_LINK_TEXT'); ?></a></p>
+				<p><a href="index.php"><?php echo JText::_('JGLOBAL_TPL_CPANEL_LINK_TEXT'); ?></a></p>
 				<?php if ($this->debug) : ?>
 					<div><?php echo $this->renderBacktrace(); ?></div>
 				<?php endif; ?>
