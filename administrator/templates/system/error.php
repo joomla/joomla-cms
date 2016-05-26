ddae82852673373d2f35b1e3e607ac05a50e4d6a
andrepereiradasilva <andrepereiradasilva@users.noreply.github.com>
2016-05-26 17:17:42 +0100

    html5 is system admin template

--- administrator/templates/system/error.php
+++ administrator/templates/system/error.php
@@ -8,34 +8,41 @@
  */
 
 defined('_JEXEC') or die;
+
+$doc = JFactory::getDocument();
+
+// Output document as HTML5.
+if (is_callable(array($doc, 'setHtml5')))
+{
+	$doc->setHtml5(true);
+}
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>" >
+<!DOCTYPE html>
+<html lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>">
 <head>
-	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<meta charset="utf-8" />
 	<title><?php echo $this->error->getCode(); ?> - <?php echo htmlspecialchars($this->error->getMessage(), ENT_QUOTES, 'UTF-8'); ?></title>
-	<link rel="stylesheet" href="templates/system/css/error.css" type="text/css" />
+	<link href="templates/system/css/error.css" rel="stylesheet" />
+	<!--[if lt IE 9]><script src="<?php echo $this->baseurl; ?>/media/jui/js/html5.js"></script><![endif]-->
 </head>
 <body>
-	<table width="550" align="center" class="outline">
-	<tr>
-		<td align="center">
-			<h1>
-				<?php echo $this->error->getCode() ?> - <?php echo JText::_('JERROR_AN_ERROR_HAS_OCCURRED') ?>
-			</h1>
-		</td>
-	</tr>
-	<tr>
-		<td width="39%" align="center">
-			<p><?php echo htmlspecialchars($this->error->getMessage(), ENT_QUOTES, 'UTF-8'); ?></p>
-			<p><a href="index.php"><?php echo JText::_('JGLOBAL_TPL_CPANEL_LINK_TEXT') ?></a></p>
-			<p>
-				<?php if ($this->debug) :
-					echo $this->renderBacktrace();
-				endif; ?>
-			</p>
-		</td>
-	</tr>
+	<table class="outline" style="text-align: center; width: 550px;">
+		<tr>
+			<td style="text-align: center;">
+				<h1>
+					<?php echo $this->error->getCode() ?> - <?php echo JText::_('JERROR_AN_ERROR_HAS_OCCURRED') ?>
+				</h1>
+			</td>
+		</tr>
+		<tr>
+			<td width="39%" style="text-align: center;">
+				<p><?php echo htmlspecialchars($this->error->getMessage(), ENT_QUOTES, 'UTF-8'); ?></p>
+				<p><a href="<?php JRoute::_('index.php'); ?>"><?php echo JText::_('JGLOBAL_TPL_CPANEL_LINK_TEXT') ?></a></p>
+				<?php if ($this->debug) : ?>
+					<div><?php echo $this->renderBacktrace(); ?></divp>
+				<?php endif; ?>
+			</td>
+		</tr>
 	</table>
 </body>
 </html>
