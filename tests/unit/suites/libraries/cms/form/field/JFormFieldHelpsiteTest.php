c1863151464098f9784c6a4aba61a6581f1360ab
Walt Sorensen <photodude@users.noreply.github.com>
2016-05-27 20:53:51 -0600

    unset($this->backupServer); in tearDown()

--- tests/unit/suites/libraries/cms/form/field/JFormFieldHelpsiteTest.php
+++ tests/unit/suites/libraries/cms/form/field/JFormFieldHelpsiteTest.php
@@ -58,7 +58,7 @@ class JFormFieldHelpsiteTest extends TestCase
 	protected function tearDown()
 	{
 		$_SERVER = $this->backupServer;
-
+		unset($this->backupServer);
 		$this->restoreFactoryState();
 
 		parent::tearDown();
