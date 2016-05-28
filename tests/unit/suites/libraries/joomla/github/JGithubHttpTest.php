01f1ffbfb2827a79bd969bbed721721d68c1ccbd
Walt Sorensen <photodude@users.noreply.github.com>
2016-05-28 16:25:03 -0600

    unset() the setup() objects in tearDown()

--- tests/unit/suites/libraries/joomla/github/JGithubHttpTest.php
+++ tests/unit/suites/libraries/joomla/github/JGithubHttpTest.php
@@ -66,6 +66,10 @@ class JGithubHttpTest extends PHPUnit_Framework_TestCase
 	 */
 	protected function tearDown()
 	{
+		unset($this->options);
+		unset($this->transport);
+		unset($this->object);
+		parent::tearDown();
 	}
 
 	/**
