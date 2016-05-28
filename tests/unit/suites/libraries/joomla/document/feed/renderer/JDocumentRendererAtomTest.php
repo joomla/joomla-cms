e8f25b7ac9a803011c7c4c0492377b88c7b743e6
Walt Sorensen <photodude@users.noreply.github.com>
2016-05-28 12:41:57 -0600

    retry unset() the setup() objects in tearDown()
    add backupServer

--- tests/unit/suites/libraries/joomla/document/feed/renderer/JDocumentRendererAtomTest.php
+++ tests/unit/suites/libraries/joomla/document/feed/renderer/JDocumentRendererAtomTest.php
@@ -23,6 +23,13 @@ class JDocumentRendererAtomTest extends TestCase
 	protected $object;
 
 	/**
+	 * Backup of the SERVER superglobal
+	 *
+	 * @var  array
+	 */
+	protected $backupServer;
+
+	/**
 	 * Sets up the fixture, for example, opens a network connection.
 	 * This method is called before a test is executed.
 	 *
@@ -35,7 +42,7 @@ class JDocumentRendererAtomTest extends TestCase
 	protected function setUp()
 	{
 		parent::setUp();
-
+		$this->backupServer = $_SERVER;
 		$this->markTestSkipped("Too tightly coupled to internals to be testable now");
 
 		require_once JPATH_PLATFORM . '/joomla/application/router.php';
@@ -83,7 +90,12 @@ class JDocumentRendererAtomTest extends TestCase
 	 */
 	protected function tearDown()
 	{
+		$_SERVER = $this->backupServer;
+		unset($this->backupServer);
 		$this->restoreFactoryState();
+		unset($input);
+		unset($this->object);
+		parent::tearDown();
 	}
 
 	/**
