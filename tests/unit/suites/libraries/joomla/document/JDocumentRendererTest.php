25fc1dced9ae94c55dfcf0ea98e4b35c79081824
Walt Sorensen <photodude@users.noreply.github.com>
2016-05-28 10:10:40 -0600

    unset() the setup() objects in tearDown()

--- tests/unit/suites/libraries/joomla/document/JDocumentRendererTest.php
+++ tests/unit/suites/libraries/joomla/document/JDocumentRendererTest.php
@@ -52,7 +52,9 @@ class JDocumentRendererTest extends PHPUnit_Framework_TestCase
 	protected function tearDown()
 	{
 		$_SERVER = $this->backupServer;
-
+		unset($this->backupServer);
+		unset($doc);
+		unset($this->object);
 		parent::tearDown();
 	}
 
