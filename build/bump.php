6bc0843b5b840d06f30fd9b77539f8b200595f02
Cyril Rezé <cyril.reze@joomlic.com>
2016-05-29 14:24:56 +0200

    Add core extensions version and creationDate bump

--- build/bump.php
+++ build/bump.php
@@ -36,13 +36,152 @@ function usage($command)
 const PHP_TAB = "\t";
 
 // File paths.
-$versionFile      = '/libraries/cms/version/version.php';
+$versionFile       = '/libraries/cms/version/version.php';
 
-$coreXmlFiles     = array(
+$coreXmlFiles      = array(
 			'/administrator/manifests/files/joomla.xml',
 			);
 
-$languageXmlFiles = array(
+$componentXmlFiles = array(
+			'/administrator/components/com_admin/admin.xml',
+			'/administrator/components/com_ajax/ajax.xml',
+			'/administrator/components/com_banners/banners.xml',
+			'/administrator/components/com_cache/cache.xml',
+			'/administrator/components/com_categories/categories.xml',
+			'/administrator/components/com_checkin/checkin.xml',
+			'/administrator/components/com_config/config.xml',
+			'/administrator/components/com_contact/contact.xml',
+			'/administrator/components/com_content/content.xml',
+			'/administrator/components/com_contenthistory/contenthistory.xml',
+			'/administrator/components/com_cpanel/cpanel.xml',
+			'/administrator/components/com_finder/finder.xml',
+			'/administrator/components/com_installer/installer.xml',
+			'/administrator/components/com_joomlaupdate/joomlaupdate.xml',
+			'/administrator/components/com_languages/languages.xml',
+			'/administrator/components/com_login/login.xml',
+			'/administrator/components/com_media/media.xml',
+			'/administrator/components/com_menus/menus.xml',
+			'/administrator/components/com_messages/messages.xml',
+			'/administrator/components/com_modules/modules.xml',
+			'/administrator/components/com_newsfeeds/newsfeeds.xml',
+			'/administrator/components/com_plugins/plugins.xml',
+			'/administrator/components/com_postinstall/postinstall.xml',
+			'/administrator/components/com_redirect/redirect.xml',
+			'/administrator/components/com_search/search.xml',
+			'/administrator/components/com_tags/tags.xml',
+			'/administrator/components/com_templates/templates.xml',
+			'/administrator/components/com_users/users.xml',
+			);
+
+$moduleXmlFiles    = array(
+			'/administrator/modules/mod_custom/mod_custom.xml',
+			'/administrator/modules/mod_feed/mod_feed.xml',
+			'/administrator/modules/mod_latest/mod_latest.xml',
+			'/administrator/modules/mod_logged/mod_logged.xml',
+			'/administrator/modules/mod_login/mod_login.xml',
+			'/administrator/modules/mod_menu/mod_menu.xml',
+			'/administrator/modules/mod_multilangstatus/mod_multilangstatus.xml',
+			'/administrator/modules/mod_popular/mod_popular.xml',
+			'/administrator/modules/mod_quickicon/mod_quickicon.xml',
+			'/administrator/modules/mod_stats_admin/mod_stats_admin.xml',
+			'/administrator/modules/mod_status/mod_status.xml',
+			'/administrator/modules/mod_submenu/mod_submenu.xml',
+			'/administrator/modules/mod_title/mod_title.xml',
+			'/administrator/modules/mod_toolbar/mod_toolbar.xml',
+			'/administrator/modules/mod_version/mod_version.xml',
+			'/modules/mod_articles_archive/mod_articles_archive.xml',
+			'/modules/mod_articles_categories/mod_articles_categories.xml',
+			'/modules/mod_articles_category/mod_articles_category.xml',
+			'/modules/mod_articles_latest/mod_articles_latest.xml',
+			'/modules/mod_articles_news/mod_articles_news.xml',
+			'/modules/mod_articles_popular/mod_articles_popular.xml',
+			'/modules/mod_banners/mod_banners.xml',
+			'/modules/mod_breadcrumbs/mod_breadcrumbs.xml',
+			'/modules/mod_custom/mod_custom.xml',
+			'/modules/mod_feed/mod_feed.xml',
+			'/modules/mod_finder/mod_finder.xml',
+			'/modules/mod_footer/mod_footer.xml',
+			'/modules/mod_languages/mod_languages.xml',
+			'/modules/mod_login/mod_login.xml',
+			'/modules/mod_menu/mod_menu.xml',
+			'/modules/mod_random_image/mod_random_image.xml',
+			'/modules/mod_related_items/mod_related_items.xml',
+			'/modules/mod_search/mod_search.xml',
+			'/modules/mod_stats/mod_stats.xml',
+			'/modules/mod_syndicate/mod_syndicate.xml',
+			'/modules/mod_tags_popular/mod_tags_popular.xml',
+			'/modules/mod_tags_similar/mod_tags_similar.xml',
+			'/modules/mod_users_latest/mod_users_latest.xml',
+			'/modules/mod_whosonline/mod_whosonline.xml',
+			'/modules/mod_wrapper/mod_wrapper.xml',
+			);
+
+$pluginXmlFiles    = array(
+			'/plugins/authentication/cookie/cookie.xml',
+			'/plugins/authentication/gmail/gmail.xml',
+			'/plugins/authentication/joomla/joomla.xml',
+			'/plugins/authentication/ldap/ldap.xml',
+			'/plugins/captcha/recaptcha/recaptcha.xml',
+			'/plugins/content/contact/contact.xml',
+			'/plugins/content/emailcloak/emailcloak.xml',
+			'/plugins/content/finder/finder.xml',
+			'/plugins/content/joomla/joomla.xml',
+			'/plugins/content/loadmodule/loadmodule.xml',
+			'/plugins/content/pagebreak/pagebreak.xml',
+			'/plugins/content/pagenavigation/pagenavigation.xml',
+			'/plugins/content/vote/vote.xml',
+			'/plugins/editors/codemirror/codemirror.xml',
+			'/plugins/editors/none/none.xml',
+			'/plugins/editors/tinymce/tinymce.xml',
+			'/plugins/editors-xtd/article/article.xml',
+			'/plugins/editors-xtd/image/image.xml',
+			'/plugins/editors-xtd/module/module.xml',
+			'/plugins/editors-xtd/pagebreak/pagebreak.xml',
+			'/plugins/editors-xtd/readmore/readmore.xml',
+			'/plugins/extension/joomla/joomla.xml',
+			'/plugins/finder/categories/categories.xml',
+			'/plugins/finder/contacts/contacts.xml',
+			'/plugins/finder/content/content.xml',
+			'/plugins/finder/newsfeeds/newsfeeds.xml',
+			'/plugins/finder/tags/tags.xml',
+			'/plugins/quickicon/extensionupdate/extensionupdate.xml',
+			'/plugins/quickicon/joomlaupdate/joomlaupdate.xml',
+			'/plugins/search/categories/categories.xml',
+			'/plugins/search/contacts/contacts.xml',
+			'/plugins/search/content/content.xml',
+			'/plugins/search/newsfeeds/newsfeeds.xml',
+			'/plugins/search/tags/tags.xml',
+			'/plugins/system/cache/cache.xml',
+			'/plugins/system/debug/debug.xml',
+			'/plugins/system/highlight/highlight.xml',
+			'/plugins/system/languagecode/languagecode.xml',
+			'/plugins/system/languagefilter/languagefilter.xml',
+			'/plugins/system/log/log.xml',
+			'/plugins/system/logout/logout.xml',
+			'/plugins/system/p3p/p3p.xml',
+			'/plugins/system/redirect/redirect.xml',
+			'/plugins/system/remember/remember.xml',
+			'/plugins/system/sef/sef.xml',
+			'/plugins/system/stats/stats.xml',
+			'/plugins/system/updatenotification/updatenotification.xml',
+			'/plugins/twofactorauth/totp/totp.xml',
+			'/plugins/twofactorauth/yubikey/yubikey.xml',
+			'/plugins/user/contactcreator/contactcreator.xml',
+			'/plugins/user/joomla/joomla.xml',
+			'/plugins/user/profile/profile.xml',
+			'/plugins/installer/folderinstaller/folderinstaller.xml',
+			'/plugins/installer/packageinstaller/packageinstaller.xml',
+			'/plugins/installer/urlinstaller/urlinstaller.xml',
+			);
+
+$templateXmlFiles = array(
+			'/administrator/templates/hathor/templateDetails.xml',
+			'/administrator/templates/isis/templateDetails.xml',
+			'/templates/beez3/templateDetails.xml',
+			'/templates/protostar/templateDetails.xml',
+			);
+
+$languageXmlFiles  = array(
 			'/language/en-GB/en-GB.xml',
 			'/language/en-GB/install.xml',
 			'/administrator/language/en-GB/en-GB.xml',
@@ -161,10 +300,12 @@ echo '- Release date:' . PHP_TAB . PHP_TAB . $version['reldate'] . PHP_EOL;
 echo '- Release time:' . PHP_TAB . PHP_TAB . $version['reltime'] . PHP_EOL;
 echo '- Release timezone:'  . PHP_TAB . $version['reltz'] . PHP_EOL;
 echo '- Creation date:' . PHP_TAB . $version['credate'] . PHP_EOL;
+
 if (!empty($version['codename']))
 {
 	echo '- Codename:' . PHP_TAB . PHP_TAB . $version['codename'] . PHP_EOL;
 }
+
 echo PHP_EOL;
 
 $rootPath = dirname(__DIR__);
@@ -180,10 +321,12 @@ if (file_exists($rootPath . $versionFile))
 	$fileContents = preg_replace("#RELDATE\s*=\s*'[^\']*'#", "RELDATE = '" . $version['reldate'] . "'", $fileContents);
 	$fileContents = preg_replace("#RELTIME\s*=\s*'[^\']*'#", "RELTIME = '" . $version['reltime'] . "'", $fileContents);
 	$fileContents = preg_replace("#RELTZ\s*=\s*'[^\']*'#", "RELTZ = '" . $version['reltz'] . "'", $fileContents);
+
 	if (!empty($version['codename']))
 	{
 		$fileContents = preg_replace("#CODENAME\s*=\s*'[^\']*'#", "CODENAME = '" . $version['codename'] . "'", $fileContents);
 	}
+
 	file_put_contents($rootPath . $versionFile, $fileContents);
 }
 
@@ -198,6 +341,98 @@ foreach ($coreXmlFiles as $coreXmlFile)
 		file_put_contents($rootPath . $coreXmlFile, $fileContents);
 	}
 }
+
+// Updates the version and creation date in component xml files.
+foreach ($componentXmlFiles as $componentXmlFile)
+{
+	if (file_exists($rootPath . $componentXmlFile))
+	{
+		$dirAdmin           = dirname($componentXmlFile);
+		$dirSite            = dirname(str_replace("/administrator", "", $dirAdmin));
+		$getLastModDirAdmin = filemtime($rootPath . $dirAdmin);
+		$getLastModDirSite  = is_dir($rootPath . $dirSite)
+							? filemtime($rootPath . $dirSite)
+							: $getLastModDirAdmin;
+		$getLastModXml      = filemtime($rootPath . $componentXmlFile);
+
+		if ($getLastModXml <= $getLastModDirAdmin
+			|| $getLastModXml <= $getLastModDirSite)
+		{
+			$fileContents = file_get_contents($rootPath . $componentXmlFile);
+			$fileContents = preg_replace('#<version>[^<]*</version>#', '<version>' . $version['release'] . '</version>', $fileContents);
+			$fileContents = preg_replace('#<creationDate>[^<]*</creationDate>#', '<creationDate>' . $version['credate'] . '</creationDate>', $fileContents);
+			file_put_contents($rootPath . $componentXmlFile, $fileContents);
+
+			echo 'Component update:' . PHP_TAB . 'com_' . basename($componentXmlFile, ".xml") . PHP_EOL;
+		}
+	}
+}
+
+// Updates the version and creation date in module xml files.
+foreach ($moduleXmlFiles as $moduleXmlFile)
+{
+	if (file_exists($rootPath . $moduleXmlFile))
+	{
+		$dirname       = dirname($moduleXmlFile);
+		$getLastModDir = filemtime($rootPath . $dirname);
+		$getLastModXml = filemtime($rootPath . $moduleXmlFile);
+
+		if ($getLastModXml <= $getLastModDir)
+		{
+			$fileContents = file_get_contents($rootPath . $moduleXmlFile);
+			$fileContents = preg_replace('#<version>[^<]*</version>#', '<version>' . $version['release'] . '</version>', $fileContents);
+			$fileContents = preg_replace('#<creationDate>[^<]*</creationDate>#', '<creationDate>' . $version['credate'] . '</creationDate>', $fileContents);
+			file_put_contents($rootPath . $moduleXmlFile, $fileContents);
+
+			echo 'Module update:' . PHP_TAB . PHP_TAB . basename($moduleXmlFile, ".xml") . PHP_EOL;
+		}
+	}
+}
+
+// Updates the version and creation date in plugin xml files.
+foreach ($pluginXmlFiles as $pluginXmlFile)
+{
+	if (file_exists($rootPath . $pluginXmlFile))
+	{
+		$dirname       = dirname($pluginXmlFile);
+		$getLastModDir = filemtime($rootPath . $dirname);
+		$getLastModXml = filemtime($rootPath . $pluginXmlFile);
+
+		if ($getLastModXml <= $getLastModDir)
+		{
+			$fileContents = file_get_contents($rootPath . $pluginXmlFile);
+			$fileContents = preg_replace('#<version>[^<]*</version>#', '<version>' . $version['release'] . '</version>', $fileContents);
+			$fileContents = preg_replace('#<creationDate>[^<]*</creationDate>#', '<creationDate>' . $version['credate'] . '</creationDate>', $fileContents);
+			file_put_contents($rootPath . $pluginXmlFile, $fileContents);
+
+			echo 'Plugin update:' . PHP_TAB . PHP_TAB . str_replace(array("/plugins/", basename($pluginXmlFile, ".xml")), "", $dirname)
+				. basename($pluginXmlFile, ".xml") . PHP_EOL;
+		}
+	}
+}
+
+// Updates the version and creation date in template xml files.
+foreach ($templateXmlFiles as $templateXmlFile)
+{
+	if (file_exists($rootPath . $templateXmlFile))
+	{
+		$dirname       = dirname($templateXmlFile);
+		$getLastModDir = filemtime($rootPath . $dirname);
+		$getLastModXml = filemtime($rootPath . $templateXmlFile);
+
+		if ($getLastModXml <= $getLastModDir)
+		{
+			$fileContents = file_get_contents($rootPath . $templateXmlFile);
+			$fileContents = preg_replace('#<version>[^<]*</version>#', '<version>' . $version['release'] . '</version>', $fileContents);
+			$fileContents = preg_replace('#<creationDate>[^<]*</creationDate>#', '<creationDate>' . $version['credate'] . '</creationDate>', $fileContents);
+			file_put_contents($rootPath . $templateXmlFile, $fileContents);
+
+			echo 'Template update:' . PHP_TAB . str_replace(array("/", "administrator", "templates"), "", $dirname) . PHP_EOL;
+		}
+	}
+}
+
+echo PHP_EOL;
 
 // Updates the version and creation date in language xml files.
 foreach ($languageXmlFiles as $languageXmlFile)
