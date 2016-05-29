a474675a37b4f988acd289825296a742377a720f
Walt Sorensen <photodude@users.noreply.github.com>
2016-05-29 16:17:33 -0600

    add tearDown()

--- tests/unit/suites/libraries/joomla/mediawiki/JMediawikiObjectTest.php
+++ tests/unit/suites/libraries/joomla/mediawiki/JMediawikiObjectTest.php
@@ -68,6 +68,22 @@ class JMediawikiObjectTest extends PHPUnit_Framework_TestCase
 	}
 
 	/**
+	 * Tears down the fixture, for example, closes a network connection.
+	 * This method is called after a test is executed.
+	 *
+	 * @return void
+	 *
+	 * @see     PHPUnit_Framework_TestCase::tearDown()
+	 * @since   3.6
+	 */
+	protected function tearDown()
+	{
+		unset($this->options);
+		unset($this->client);
+		unset($this->object);
+	}
+
+	/**
 	 * Tests the buildParameter method
 	 *
 	 * @return void
