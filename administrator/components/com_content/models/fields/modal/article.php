9385d065df4556952b5bb0af199217b3f9d57067
Cyril Rezé <cyril.reze@joomlic.com>
2016-05-29 14:40:08 +0200

    Do not close edit modal when escape key is pressed

--- administrator/components/com_content/models/fields/modal/article.php
+++ administrator/components/com_content/models/fields/modal/article.php
@@ -192,12 +192,12 @@ class JFormFieldModal_Article extends JFormField
 			'bootstrap.renderModal',
 			'articleSelect' . $this->id . 'Modal',
 			array(
-				'url'         => $urlSelect,
 				'title'       => JText::_('COM_CONTENT_CHANGE_ARTICLE'),
-				'width'       => '800px',
+				'url'         => $urlSelect,
 				'height'      => '400px',
-				'modalWidth'  => '80',
+				'width'       => '800px',
 				'bodyHeight'  => '70',
+				'modalWidth'  => '80',
 				'footer'      => '<button type="button" class="btn" data-dismiss="modal" aria-hidden="true">'
 						. JText::_("JLIB_HTML_BEHAVIOR_CLOSE") . '</button>'
 			)
@@ -208,14 +208,15 @@ class JFormFieldModal_Article extends JFormField
 			'bootstrap.renderModal',
 			'articleEdit' . $value . 'Modal',
 			array(
-				'url'         => $urlEdit,
 				'title'       => JText::_('COM_CONTENT_EDIT_ARTICLE'),
 				'backdrop'    => 'static',
+				'keyboard'    => false,
 				'closeButton' => false,
-				'width'       => '800px',
+				'url'         => $urlEdit,
 				'height'      => '400px',
-				'modalWidth'  => '80',
+				'width'       => '800px',
 				'bodyHeight'  => '70',
+				'modalWidth'  => '80',
 				'footer'      => '<button type="button" class="btn" data-dismiss="modal" aria-hidden="true"'
 						. ' onclick="jQuery(\'#articleEdit' . $value . 'Modal iframe\').contents().find(\'#closeBtn\').click();">'
 						. JText::_("JLIB_HTML_BEHAVIOR_CLOSE") . '</button>'
