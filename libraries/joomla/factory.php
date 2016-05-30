0bd1a0ccb1bccea7a58ced07a38fff2c9e2bd008
andrepereiradasilva <andrepereiradasilva@users.noreply.github.com>
2016-05-30 18:52:53 +0100

    revert sorry

--- libraries/joomla/factory.php
+++ libraries/joomla/factory.php
@@ -749,7 +749,7 @@ abstract class JFactory
 		$attributes = array(
 			'charset'      => 'utf-8',
 			'lineend'      => 'unix',
-			'tab'          => PHP_TAB,
+			'tab'          => "\t",
 			'language'     => $lang->getTag(),
 			'direction'    => $lang->isRtl() ? 'rtl' : 'ltr',
 			'mediaversion' => $version->getMediaVersion()
