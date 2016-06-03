3b31d8b00204351e2acf9bc569d2bb01bc967649
uglyeoin <eoin@squareballoon.co.uk>
2016-06-03 20:31:46 +0100

    updated item-featured
    included code golf version

--- modules/mod_related_items/tmpl/default.php
+++ modules/mod_related_items/tmpl/default.php
@@ -11,7 +11,7 @@ defined('_JEXEC') or die;
 ?>
 <ul class="relateditems<?php echo $moduleclass_sfx; ?>">
 <?php foreach ($list as $item) :	?>
-<li<?php echo ($item->featured) ? ' class="featured-article"' : ''; ?>>
+<li<?php echo (empty($item->featured)) ? '' : ' item-featured';?>>
 	<a href="<?php echo $item->route; ?>">
 		<?php if ($showDate) echo JHtml::_('date', $item->created, JText::_('DATE_FORMAT_LC4')) . " - "; ?>
 		<?php echo $item->title; ?></a>
