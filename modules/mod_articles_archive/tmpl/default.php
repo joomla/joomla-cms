8a2a8adb584bfc5d223fba3e304a97a668dd35d6
uglyeoin <eoin@squareballoon.co.uk>
2016-06-03 20:29:32 +0100

    updated item-featured
    included code golf version

--- modules/mod_articles_archive/tmpl/default.php
+++ modules/mod_articles_archive/tmpl/default.php
@@ -12,7 +12,7 @@ defined('_JEXEC') or die;
 <?php if (!empty($list)) : ?>
 	<ul class="archive-module<?php echo $moduleclass_sfx; ?>">
 	<?php foreach ($list as $item) : ?>
-	<li<?php echo ($item->featured) ? ' class="featured-article"' : ''; ?>>
+	<li<?php echo (empty($item->featured)) ? '' : ' item-featured';?>>
 		<a href="<?php echo $item->link; ?>">
 			<?php echo $item->text; ?>
 		</a>
