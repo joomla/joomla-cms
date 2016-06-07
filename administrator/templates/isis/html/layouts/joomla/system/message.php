1b938a5e6f56fae1501e8094cb15afe4cf181e4f
andrepereiradasilva <andrepereiradasilva@users.noreply.github.com>
2016-06-07 15:28:40 +0100

    fix conflicts final

--- administrator/templates/isis/html/layouts/joomla/system/message.php
+++ administrator/templates/isis/html/layouts/joomla/system/message.php
@@ -9,17 +9,26 @@
 
 defined('_JEXEC') or die;
 
-$msgList = $displayData['msgList'];
+$msgList = $displayData['msgQueue'];
 
 $alert = array('error' => 'alert-error', 'warning' => '', 'notice' => 'alert-info', 'message' => 'alert-success');
 ?>
 <div id="system-message-container">
 	<?php if (is_array($msgList) && $msgList) : ?>
-		<?php foreach ($msgList as $type => $msgs) : ?>
+		<button type="button" class="close" data-dismiss="alert">&times;</button>
+		<?php foreach ($msgList as $groupIdentifier => $msgs) : ?>
+			<?php $options = unserialize($groupIdentifier); ?>
+			<?php $type    = $options['type']; ?>
 			<div class="alert <?php echo isset($alert[$type]) ? $alert[$type] : 'alert-' . $type; ?>">
-				<button type="button" class="close" data-dismiss="alert">&times;</button>
-				<?php if (!empty($msgs)) : ?>
-					<h4 class="alert-heading"><?php echo JText::_($type); ?></h4>
+				<?php if ($msgs) : ?>
+					<?php if ($options['showTitle']) : ?>
+						<?php if ($options['customTitle'] !== '') : ?>
+							<?php $title = $options['customTitle']; ?>
+						<?php else : ?>
+							<?php $title = $type; ?>
+						<?php endif; ?>
+						<h4 class="alert-heading"><?php echo JText::_($title); ?></h4>
+					<?php endif; ?>
 					<?php foreach ($msgs as $msg) : ?>
 						<div class="alert-message"><?php echo $msg; ?></div>
 					<?php endforeach; ?>
