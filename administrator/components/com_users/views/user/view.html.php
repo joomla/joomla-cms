d08924b6bdea3f568eeda1d60809cc85fc66eccf
Jean-Marie Simonet <infografjms@gmail.com>
2016-06-11 12:30:33 +0200

    removing default

--- administrator/components/com_users/views/user/view.html.php
+++ administrator/components/com_users/views/user/view.html.php
@@ -39,7 +39,6 @@ class UsersViewUser extends JViewLegacy
 	{
 		$this->form      = $this->get('Form');
 		$this->item      = $this->get('Item');
-		$this->grouplist = $this->get('Groups');
 		$this->groups    = $this->get('AssignedGroups');
 		$this->state     = $this->get('State');
 		$this->tfaform   = $this->get('Twofactorform');
