9c9dba5f2cc990a0958fae854fe1287c6b84e4fb
Prital Patel <baldhapiyu@gmail.com>
2016-06-13 00:13:04 +0530

    Adding users.feature scenarios using gherkin

--- tests/_support/AcceptanceTester.php
+++ tests/_support/AcceptanceTester.php
@@ -1,6 +1,4 @@
 <?php
-
-
 /**
  * Inherited Methods
  * @method void wantToTest($text)
@@ -12,15 +10,150 @@
  * @method void am($role)
  * @method void lookForwardTo($achieveValue)
  * @method void comment($description)
- * @method \Codeception\Lib\Friend haveFriend($name, $actorClass = NULL)
+ * @method \Codeception\Lib\Friend haveFriend($name, $actorClass = null)
  *
  * @SuppressWarnings(PHPMD)
-*/
+ */
 class AcceptanceTester extends \Codeception\Actor
 {
-    use _generated\AcceptanceTesterActions;
+	use _generated\AcceptanceTesterActions;
 
-   /**
-    * Define custom actions here
-    */
+	/**
+	 * Method is to set Wait for page title
+	 *
+	 * @param   string   $title    Page Title text
+	 * @param   integer  $timeout  timeout time
+	 *
+	 * @return  void
+	 */
+	public function waitForPageTitle($title, $timeout = 60)
+	{
+		$I = $this;
+		$I->waitForText($title, $timeout, ['class' => 'page-title']);
+	}
+
+	/**
+	 * Function to check for PHP Notices or Warnings
+	 *
+	 * @param string $page Optional, if not given checks will be done in the current page
+	 *
+	 * @note: doAdminLogin() before
+	 */
+	public function checkForPhpNoticesOrWarnings($page = null)
+	{
+		$I = $this;
+
+		if ($page)
+		{
+			$I->amOnPage($page);
+		}
+
+		$I->dontSeeInPageSource('Notice:');
+		$I->dontSeeInPageSource('<b>Notice</b>:');
+		$I->dontSeeInPageSource('Warning:');
+		$I->dontSeeInPageSource('<b>Warning</b>:');
+		$I->dontSeeInPageSource('Strict standards:');
+		$I->dontSeeInPageSource('<b>Strict standards</b>:');
+		$I->dontSeeInPageSource('The requested page can\'t be found');
+	}
+
+	/**
+	 * Function to select Toolbar buttons in Joomla! Admin Toolbar Panel
+	 *
+	 * @param   string  $button  The full name of the button
+	 */
+	public function clickToolbarButton($button)
+	{
+		$I = $this;
+		$input = strtolower($button);
+
+		// @todo Needs to find way to work with different window size.
+		/*$screenSize = explode("x", $this->config['window_size']);
+		if($screenSize[0] <= 480)
+		{
+			$I->click('Toolbar');
+		}*/
+
+		switch($input)
+		{
+			case "new":
+				$I->click(['xpath' => "//div[@id='toolbar-new']//button"]);
+				break;
+			case "edit":
+				$I->click(['xpath' => "//div[@id='toolbar-edit']//button"]);
+				break;
+			case "publish":
+				$I->click(['xpath' => "//div[@id='toolbar-publish']//button"]);
+				break;
+			case "unpublish":
+				$I->click(['xpath' => "//div[@id='toolbar-unpublish']//button"]);
+				break;
+			case "archive":
+				$I->click(['xpath' => "//div[@id='toolbar-archive']//button"]);
+				break;
+			case "check-in":
+				$I->click(['xpath' => "//div[@id='toolbar-checkin']//button"]);
+				break;
+			case "batch":
+				$I->click(['xpath' => "//div[@id='toolbar-batch']//button"]);
+				break;
+			case "rebuild":
+				$I->click(['xpath' => "//div[@id='toolbar-refresh']//button"]);
+				break;
+			case "trash":
+				$I->click(['xpath' => "//div[@id='toolbar-trash']//button"]);
+				break;
+			case "save":
+				$I->click(['xpath' => "//div[@id='toolbar-apply']//button"]);
+				break;
+			case "save & close":
+				$I->click(['xpath' => "//div[@id='toolbar-save']//button"]);
+				break;
+			case "save & new":
+				$I->click(['xpath' => "//div[@id='toolbar-save-new']//button"]);
+				break;
+			case "cancel":
+				$I->click(['xpath' => "//div[@id='toolbar-cancel']//button"]);
+				break;
+			case "options":
+				$I->click(['xpath' => "//div[@id='toolbar-options']//button"]);
+				break;
+			case "empty trash":
+				$I->click(['xpath' => "//div[@id='toolbar-delete']//button"]);
+				break;
+		}
+	}
+
+	/**
+	 * Function to select all the item in the Search results in Administrator List
+	 *
+	 * Note: We recommend use of checkAllResults function only after searchForItem to be sure you are selecting only the desired result set
+	 *
+	 * @return void
+	 */
+	public function checkAllResults()
+	{
+		$I = $this;
+		//$this->debug("Selecting Checkall button");
+		$I->click(['xpath' => "//thead//input[@name='checkall-toggle' or @name='toggle']"]);
+	}
+
+	/**
+	 * Selects an option in a Chosen Selector based on its id
+	 *
+	 * @param   string  $selectId  The id of the <select> element
+	 * @param   string  $option    The text in the <option> to be selected in the chosen selector
+	 *
+	 * @return void
+	 */
+	public function selectOptionInChosenById($selectId, $option)
+	{
+		$chosenSelectID = $selectId . '_chzn';
+		$I = $this;
+		//$this->debug("I open the $chosenSelectID chosen selector");
+		$I->click(['xpath' => "//div[@id='$chosenSelectID']/a/div/b"]);
+		//$this->debug("I select $option");
+		$I->click(['xpath' => "//div[@id='$chosenSelectID']//li[text()='$option']"]);
+		$I->wait(1); // Gives time to chosen to close
+	}
 }
--- tests/_support/Step/Acceptance/Administrator/User.php
+++ tests/_support/Step/Acceptance/Administrator/User.php
@@ -4,20 +4,195 @@ namespace Step\Acceptance\Administrator;
 class User extends \AcceptanceTester
 {
 	/**
-	 * @Given I am registered administrator named :arg1
+	 * @Given There is a add user link
 	 */
-	public function iAmRegisteredAdministratorNamed($arg1)
+	public function thereIsAAddUserLink()
 	{
 		$I = $this;
-		$I->comment('@todo');
+		$I->amOnPage('administrator/index.php?option=com_users&view=users');
+		$I->clickToolbarButton('New');
 	}
 
 	/**
-	 * @Then I should see administrator dashboard
+	 * @When I fill mandatory fields for creating User
 	 */
-	public function iShouldSeeAdministratorDashboard()
+	public function iFillMandatoryFieldsForCreatingUser(\Behat\Gherkin\Node\TableNode $fields)
 	{
 		$I = $this;
-		$I->comment('@todo');
+		// iterate over all rows
+		foreach ($fields->getRows() as $index => $row)
+		{
+			if ($index === 0)
+			{ // first row to define fields
+				$keys = $row;
+				continue;
+			}
+			else
+			{
+				if ($row[0] == "Name")
+				{
+					$I->fillField(['id' => 'jform_name'], $row[1]);
+				}
+				if ($row[0] == "Login Name")
+				{
+					$I->fillField(['id' => 'jform_username'], $row[1]);
+				}
+				if ($row[0] == "Password")
+				{
+					$I->fillField(['id' => 'jform_password'], $row[1]);
+				}
+				if ($row[0] == "Confirm Password")
+				{
+					$I->fillField(['id' => 'jform_password2'], $row[1]);
+				}
+				if ($row[0] == "Email")
+				{
+					$I->fillField(['id' => 'jform_email'], $row[1]);
+				}
+			}
+		}
+	}
+
+	/**
+	 * @Then I Save the  user
+	 */
+	public function iSaveTheUser()
+	{
+		$I = $this;
+		$I->clickToolbarButton('Save & Close');
+	}
+
+	/**
+	 * @Then I see the :arg1 message
+	 */
+	public function iSeeTheMessage($message)
+	{
+		$I = $this;
+		$I->waitForPageTitle('Users');
+		$I->see($message, ['id' => 'system-message-container']);
+	}
+
+	/**
+	 * @Given I search and select the user with user name :arg1
+	 */
+	public function iSearchAndSelectTheUserWithUserName($username)
+	{
+		$I = $this;
+		$I->amOnPage('administrator/index.php?option=com_users&view=users');
+		$I->fillField(['id' => 'filter_search'], $username);
+		$I->click('.icon-search');
+		$I->checkAllResults();
+		$I->clickToolbarButton('edit');
+	}
+
+	/**
+	 *  @When I set name as an :arg1 and User Group as :arg1
+	 */
+	public function iAssignedNameAndUserGroup($name, $userGroup)
+	{
+		$I = $this;
+		$I->fillField(['id' => 'jform_name'], $name);
+		$I->click('Assigned User Groups');
+
+		// @todo use $userGroup variable to select user group dynamically
+		$I->checkOption('#1group_4');
+	}
+	/**
+	 * @Then I should display the :arg1 message
+	 */
+	public function iShouldDisplayTheMessage($message)
+	{
+		$I = $this;
+		$I->clickToolbarButton('Save & Close');
+		$I->waitForPageTitle('Users');
+		$I->see($message, ['id' => 'system-message-container']);
+	}
+
+	/**
+	 * @Given I have a user with user name :arg1
+	 */
+	public function iHaveAUserWithUserName($username)
+	{
+		$I = $this;
+		$I->amOnPage('administrator/index.php?option=com_users&view=users');
+		$I->fillField(['id' => 'filter_search'], $username);
+		$I->click('.icon-search');
+		$I->checkAllResults();
+	}
+
+	/**
+	 * @When I block the user
+	 */
+	public function iBlockTheUser()
+	{
+		$I = $this;
+		$I->clickToolbarButton('unpublish');
+	}
+
+	/**
+	 * @Then I should see the user block message :arg1
+	 */
+	public function iShouldSeeTheUserBlockMessage($message)
+	{
+		$I = $this;
+		$I->waitForPageTitle('Users');
+		$I->see($message, ['id' => 'system-message-container']);
+	}
+
+	/**
+	 * @Given I have a blocked user with user name :arg1
+	 */
+	public function iHaveABlockedUserWithUserName($username)
+	{
+		$I = $this;
+		$I->amOnPage('administrator/index.php?option=com_users&view=users');
+		$I->fillField(['id' => 'filter_search'], $username);
+		$I->click('.icon-search');
+		$I->checkAllResults();
+	}
+
+	/**
+	 * @When I unblock the user
+	 */
+	public function iUnblockTheUser()
+	{
+		$I = $this;
+		$I->waitForPageTitle('Users');
+		$I->click(['xpath' => "//div[@id='toolbar-unblock']//button"]);
+	}
+
+	/**
+	 * @Then I should see the user unblock message :arg1
+	 */
+	public function iShouldSeeTheUserUnblockMessage($message)
+	{
+		$I = $this;
+		$I->waitForPageTitle('Users');
+		$I->see($message, ['id' => 'system-message-container']);
+	}
+
+	/**
+	 * @When I Delete the user :arg1
+	 */
+	public function iDeleteTheUser($username)
+	{
+		$I = $this;
+		$I->amOnPage('administrator/index.php?option=com_users&view=users');
+		$I->fillField(['id' => 'filter_search'], $username);
+		$I->click('.icon-search');
+		$I->checkAllResults();
+		$I->clickToolbarButton('empty trash');
+		$I->acceptPopup();
+	}
+
+	/**
+	 * @Then I confirm the user should have been deleted by getting the message :arg1
+	 */
+	public function iConfirmTheUserDeleteSucessfully($message)
+	{
+		$I = $this;
+		$I->checkForPhpNoticesOrWarnings();
+		$I->expectTo('see a success message and the user is deleted');
+		$I->see($message, ['class' => 'alert-message']);
 	}
 }
