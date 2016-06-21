cfcf9b905a25ab8a274d8562c4b1b7a876bc4991
Michael Babker <mbabker@flbab.com>
2016-06-21 13:01:00 -0500

    Forgot to delete this line

--- modules/mod_tags_similar/mod_tags_similar.php
+++ modules/mod_tags_similar/mod_tags_similar.php
@@ -11,7 +11,6 @@ defined('_JEXEC') or die;
 
 // Include the tags_similar functions only once
 JLoader::register('ModTagssimilarHelper', __DIR__ . '/helper.php');
-require_once __DIR__ . '/helper.php';
 
 $cacheparams = new stdClass;
 $cacheparams->cachemode = 'safeuri';
