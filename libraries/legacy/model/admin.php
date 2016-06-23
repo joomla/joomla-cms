8e809199776e4ab125733927a21c964a68214854
Brian Teeman <brian@teeman.net>
2016-06-23 11:32:45 +0100

    notice to error

--- libraries/legacy/model/admin.php
+++ libraries/legacy/model/admin.php
@@ -1224,12 +1224,12 @@ abstract class JModelAdmin extends JModelForm
 				}
 			}
 
-			// Show a notice if the item isn't assigned to a language but we have associations.
+			// Show an error if the item isn't assigned to a language but we have associations.
 			if ($associations && ($table->language == '*'))
 			{
 				JFactory::getApplication()->enqueueMessage(
 					JText::_(strtoupper($this->option) . '_ERROR_ALL_LANGUAGE_ASSOCIATED'),
-					'notice'
+					'error'
 				);
 			}
 
