191f246ab06d92b762b956978df41400a580c985
Cyril Rezé <cyril.reze@joomlic.com>
2016-06-26 02:20:19 +0200

    Instantiate a new JLayoutFile instance and render the export button

--- administrator/components/com_banners/views/tracks/view.html.php
+++ administrator/components/com_banners/views/tracks/view.html.php
@@ -87,7 +87,20 @@ class BannersViewTracks extends JViewLegacy
 		JToolbarHelper::title(JText::_('COM_BANNERS_MANAGER_TRACKS'), 'bookmark banners-tracks');
 
 		$bar = JToolbar::getInstance('toolbar');
-		$bar->appendButton('Popup', 'download', 'JTOOLBAR_EXPORT', 'index.php?option=com_banners&amp;view=download&amp;tmpl=component', 600, 300);
+
+		// Instantiate a new JLayoutFile instance and render the export button
+		$title  = JText::_('JTOOLBAR_EXPORT');
+		$layout = new JLayoutFile('joomla.toolbar.modal');
+
+		$dhtml  = $layout->render(
+			array(
+				'selector' => 'downloadModal',
+				'icon'     => 'download',
+				'title'    => JText::_('JTOOLBAR_EXPORT'),
+			)
+		);
+
+		$bar->appendButton('Custom', $dhtml, 'download');
 
 		if ($canDo->get('core.delete'))
 		{
