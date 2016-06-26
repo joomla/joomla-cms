edb0ef8cce952d418156391b79c14d8d5c1df777
pe7er <github@pe7er.com>
2016-06-26 11:57:50 +0200

    link to front-end article

--- administrator/components/com_content/views/article/view.html.php
+++ administrator/components/com_content/views/article/view.html.php
@@ -86,8 +86,19 @@ class ContentViewArticle extends JViewLegacy
 		// Built the actions for new and existing records.
 		$canDo = $this->canDo;
 
+		$linkArticle = "";
+
+		if (!$checkedOut)
+		{
+			$linkArticle = '<a href="' . JRoute::_(
+					JURI::root() . 'index.php?option=com_content&view=article&id='
+			. (int) $this->item->id
+				) . '" title="' . JText::_('JGLOBAL_LINK_ITEM') . '" target="_blank"><span class="icon-out-2"></span></a>';
+		}
+
 		JToolbarHelper::title(
-			JText::_('COM_CONTENT_PAGE_' . ($checkedOut ? 'VIEW_ARTICLE' : ($isNew ? 'ADD_ARTICLE' : 'EDIT_ARTICLE'))),
+			JText::_('COM_CONTENT_PAGE_' . ($checkedOut ? 'VIEW_ARTICLE' : ($isNew ? 'ADD_ARTICLE' : 'EDIT_ARTICLE')))
+			. " " . $linkArticle,
 			'pencil-2 article-add'
 		);
 
