6ddf0c253c920019c1b7bbcd69235beceedb91a5
Prital Patel <baldhapiyu@gmail.com>
2016-06-27 16:18:09 +0530

    Fix the issue_#30

---
+++ tests/_support/Page/Acceptance/Administrator/UserAclPage.php
@@ -0,0 +1,11 @@
+<?php
+namespace Page\Acceptance\Administrator;
+
+use Page\Acceptance\Administrator\AdminPage;
+
+class UserAclPage extends AdminPage
+{
+    // include url of current page
+    public static $aclPageURL = 'administrator/index.php?option=com_users&view=levels';
+
+}
