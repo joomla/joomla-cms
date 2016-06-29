1e0e8f808e9cc6b270d02c73afee85970f3c5cbb
Michael Babker <mbabker@flbab.com>
2016-06-29 15:07:30 -0500

    Small logic tweak so Exceptions aren't skipped

--- templates/system/error.php
+++ templates/system/error.php
@@ -62,9 +62,10 @@ $this->direction = $doc->direction;
 					<?php // Check if there are more Exceptions and render their data as well ?>
 					<?php if ($this->error->getPrevious()) : ?>
 						<?php $loop = true; ?>
+						<?php // Reference $this->_error here and in the loop as setError() assigns errors to this property and we need this for the backtrace to work correctly ?>
+						<?php // Make the first assignment to setError() outside the loop so the loop does not skip Exceptions ?>
+						<?php $this->setError($this->_error->getPrevious()); ?>
 						<?php while ($loop === true) : ?>
-							<?php // Reference $this->_error in this loop as setError() assigns errors to this property and we need this for the backtrace to work correctly ?>
-							<?php $this->setError($this->_error->getPrevious()); ?>
 							<p><strong><?php echo JText::_('JERROR_LAYOUT_PREVIOUS_ERROR'); ?></strong></p>
 							<p><?php echo htmlspecialchars($this->_error->getMessage(), ENT_QUOTES, 'UTF-8'); ?></p>
 							<?php echo $this->renderBacktrace(); ?>
