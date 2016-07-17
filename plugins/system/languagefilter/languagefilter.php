df34366c339085423df1b063026b13fcf0632c4c
andrepereiradasilva <andrepereiradasilva@users.noreply.github.com>
2016-07-17 17:23:42 +0100

    REGRESSION: [language filter plugin] 301 redirect without caching in browser (#11161)
    * 301 redirect without caching in browser
    
    * exactly like app respond

--- plugins/system/languagefilter/languagefilter.php
+++ plugins/system/languagefilter/languagefilter.php
@@ -378,6 +378,13 @@ class PlgSystemLanguageFilter extends JPlugin
 				}
 			}
 
+			// Don't cache the redirect in browser.
+			$this->app->setHeader('Expires', 'Wed, 17 Aug 2005 00:00:00 GMT', true);
+			$this->app->setHeader('Last-Modified', gmdate('D, d M Y H:i:s') . ' GMT', true);
+			$this->app->setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0', false);
+			$this->app->setHeader('Pragma', 'no-cache');
+			$this->app->sendHeaders();
+
 			if ($this->mode_sef)
 			{
 				// Use the current language sef or the default one.
