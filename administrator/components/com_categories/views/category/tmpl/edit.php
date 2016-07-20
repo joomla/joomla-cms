f04ac729c5fde17bdf3274f213871bfcc6b384ea
andrepereiradasilva <andrepereiradasilva@users.noreply.github.com>
2016-07-20 18:02:57 +0100

    gsoc 16 multilanguage: fix edit component on submit: com_categories category

--- administrator/components/com_categories/views/category/tmpl/edit.php
+++ administrator/components/com_categories/views/category/tmpl/edit.php
@@ -46,7 +46,7 @@ $this->ignore_fieldsets = array('jmetadata', 'item_associations');
 // In case of modal
 $isModal = $input->get('layout') == 'modal' ? true : false;
 $layout = $isModal ? 'modal' : 'edit';
-$tmpl = $isModal ? '&tmpl=component' : '';
+$tmpl = $isModal || $input->get('tmpl', '', 'cmd') === 'component' ? '&tmpl=component' : '';
 ?>
 
 <form action="<?php echo JRoute::_('index.php?option=com_categories&extension=' . $input->getCmd('extension', 'com_content') . '&layout=' . $layout . $tmpl . '&id=' . (int) $this->item->id); ?>" method="post" name="adminForm" id="item-form" class="form-validate">
