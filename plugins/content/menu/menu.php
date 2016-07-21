04765d9c3df181417289124b5220675bc346f337
Shubham Rajput <shubham.rajput2013@vit.ac.in>
2016-07-22 00:34:09 +0530

    This commit saves menu data
    1. Menu data is saved in the menu table.
    2. Checked with content and contact component.

--- plugins/content/menu/menu.php
+++ plugins/content/menu/menu.php
@@ -11,6 +11,8 @@ defined('_JEXEC') or die;
 
 use Joomla\Registry\Registry;
 
+JLoader::import('joomla.application.component.model');
+
 /**
  * Menu Plugin
  *
@@ -61,6 +63,70 @@ class PlgContentMenu extends JPlugin
 			JForm::addFormPath(__DIR__ . '/forms');
 			$form->loadFile('menu', false);
 		}
+
+		return true;
+	}
+
+	/**
+	 * Sets the fields value from edit form
+	 *
+	 * @param   JForm  $context  The form to be altered.
+	 * @param   mixed  $article  The associated data for the form.
+	 *
+	 * @return  boolean
+	 *
+	 * @since   3.6
+	 */
+
+	public function onContentBeforeSave($context,$article)
+	{
+		$session =& JFactory::getSession();
+		$session->set("formData", JFactory::getApplication()->input->post->get('jform', array(), 'array'));
+
+		return true;
+	}
+
+	/**
+	 * Adds additional fields to the editing form
+	 *
+	 * @param   JForm  $context  The form to be altered.
+	 * @param   mixed  $article  The associated data for the form.
+	 *
+	 * @return  boolean
+	 *
+	 * @since   3.6
+	 */
+
+	public function onContentAfterSave($context,$article)
+	{
+		$session =& JFactory::getSession();
+		$data = $session->get("formData");
+
+		JModelLegacy::addIncludePath(JPATH_ADMINISTRATOR . '/components/com_menus/models', 'MenusModel');
+		$itemModel = JModelAdmin::getInstance('Item', 'MenusModel');
+		$itemModel->addTablePath(JPATH_ADMINISTRATOR . '/components/com_menus/tables');
+		$str = (explode(".", $context));
+
+		$menuData = array(
+			'id' => $data['menuid'],
+			'menutype' => $data['menutype'],
+			'title' => $data['menutitle'],
+			'alias' => $data['menualias'],
+			'link' => 'index.php?option=' . $str[0] . '&view=' . $str[1] . '&id=' . $article->id,
+			'type' => 'component',
+			'published' => 1,
+			'parent_id' => $data['parent_id'],
+			'level' => 1,
+			'component_id' => JComponentHelper::getComponent($str[0])->id,
+			'browserNav' => 0,
+			'access' => 1,
+			'template_style_id' => 0,
+			'home' => 0,
+			'language' => $data['language'],
+			'client_id' => 0
+		);
+
+		$itemModel->save($menuData);
 
 		return true;
 	}
