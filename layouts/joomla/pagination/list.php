7a9c8c4b06ef06a73f2098de8127a8eda6f5470b
Allon Moritz <allon.moritz@digital-peak.com>
2016-07-21 17:50:08 +0200

    Improving code style
    See PR Digital-Peak-Incubator/tpl_tauristar#94

--- layouts/joomla/pagination/list.php
+++ layouts/joomla/pagination/list.php
@@ -14,12 +14,9 @@ $list = $displayData['list'];
 <ul>
 	<li class="pagination-start"><?php echo $list['start']['data']; ?></li>
 	<li class="pagination-prev"><?php echo $list['previous']['data']; ?></li>
-	<?php
-	foreach ($list['pages'] as $page)
-	{
-		echo '<li>' . $page['data'] . '</li>';
-	}
-	?>
+	<?php foreach ($list['pages'] as $page) : ?>
+		<?php echo '<li>' . $page['data'] . '</li>'; ?>
+	<?php endforeach; ?>
 	<li class="pagination-next"><?php echo $list['next']['data']; ?></li>
 	<li class="pagination-end"><?php echo $list['end']['data']; ?></li>
 </ul>
