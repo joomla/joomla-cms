d22a66ddc61768a5cdaf6e018e2ef003a1e1c31a
Tomasz Narloch <tomasz.narloch@cleversite.pl>
2016-07-28 20:28:39 +0200

    Fix item alias

--- libraries/cms/router/site.php
+++ libraries/cms/router/site.php
@@ -231,6 +231,17 @@ class JRouterSite extends JRouter
 		{
 			$item = $this->menu->getItem($this->getVar('Itemid'));
 
+			if ($item && $item->type == 'alias')
+			{
+				$newItem = $this->menu->getItem($item->params->get('aliasoptions'));
+
+				if ($newItem)
+				{
+					$item->query     = array_merge($item->query, $newItem->query);
+					$item->component = $newItem->component;
+				}
+			}
+
 			if ($item !== null && is_array($item->query))
 			{
 				$vars = $vars + $item->query;
@@ -377,6 +388,17 @@ class JRouterSite extends JRouter
 
 			if ($found)
 			{
+				if ($found->type == 'alias')
+				{
+					$newItem = $this->menu->getItem($found->params->get('aliasoptions'));
+
+					if ($newItem)
+					{
+						$found->query     = array_merge($found->query, $newItem->query);
+						$found->component = $newItem->component;
+					}
+				}
+
 				$vars['Itemid'] = $found->id;
 				$vars['option'] = $found->component;
 			}
