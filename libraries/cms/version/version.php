ece8118d1db3b729ea051a1ce90d3e28473086e1
wilsonge <georgejameswilson@googlemail.com>
2016-07-28 23:26:31 +0100

    Tag 3.6.1 RC1

--- libraries/cms/version/version.php
+++ libraries/cms/version/version.php
@@ -38,7 +38,7 @@ final class JVersion
 	 * @var    string
 	 * @since  3.5
 	 */
-	const DEV_LEVEL = '1-dev';
+	const DEV_LEVEL = '1-rc1';
 
 	/**
 	 * Development status.
@@ -46,7 +46,7 @@ final class JVersion
 	 * @var    string
 	 * @since  3.5
 	 */
-	const DEV_STATUS = 'Development';
+	const DEV_STATUS = 'Release Candidate';
 
 	/**
 	 * Build number.
@@ -70,7 +70,7 @@ final class JVersion
 	 * @var    string
 	 * @since  3.5
 	 */
-	const RELDATE = '13-July-2016';
+	const RELDATE = '28-July-2016';
 
 	/**
 	 * Release time.
@@ -78,7 +78,7 @@ final class JVersion
 	 * @var    string
 	 * @since  3.5
 	 */
-	const RELTIME = '10:08';
+	const RELTIME = '23:25';
 
 	/**
 	 * Release timezone.
