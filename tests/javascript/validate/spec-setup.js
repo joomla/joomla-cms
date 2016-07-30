4bd219118b1564e843161f2b31d74b8e136b622b
Ruchiranga Wickramasinghe <truchiranga@gmail.com>
2016-07-30 13:52:39 +0530

    Issue 5 Added spies and made code improvements (#31)

--- tests/javascript/validate/spec-setup.js
+++ tests/javascript/validate/spec-setup.js
@@ -7,8 +7,8 @@
  * @version     1.0.0
  */
 
-define(['jquery', 'text!testsRoot/validate/fixtures/fixture.html', 'libs/validate', 'libs/core', 'jasmineJquery'], function ($, fixture) {
+define(['jquery', 'text!testsRoot/validate/fixtures/fixture.html', 'libs/validate', 'libs/core'], function ($, fixture) {
     $('body').append(fixture);
-
+    
     document.formvalidator = new JFormValidator();
 });
