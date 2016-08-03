dd79e23d0787f779b2c6729c836c12f85355a12a
wilsonge <georgejameswilson@googlemail.com>
2016-08-04 00:47:36 +0100

    Bump version to 3.6.2 Development

--- libraries/cms/version/version.php
+++ libraries/cms/version/version.php
@@ -38,7 +38,7 @@ final class JVersion
 	 * @var    string
 	 * @since  3.5
 	 */
-	const DEV_LEVEL = '1';
+	const DEV_LEVEL = '2-dev';
 
 	/**
 	 * Development status.
@@ -46,7 +46,7 @@ final class JVersion
 	 * @var    string
 	 * @since  3.5
 	 */
-	const DEV_STATUS = 'Stable';
+	const DEV_STATUS = 'Development';
 
 	/**
 	 * Build number.
@@ -70,7 +70,7 @@ final class JVersion
 	 * @var    string
 	 * @since  3.5
 	 */
-	const RELDATE = '2-August-2016';
+	const RELDATE = '4-August-2016';
 
 	/**
 	 * Release time.
@@ -78,7 +78,7 @@ final class JVersion
 	 * @var    string
 	 * @since  3.5
 	 */
-	const RELTIME = '22:59';
+	const RELTIME = '00:44';
 
 	/**
 	 * Release timezone.
