e6855d75157c2122bf5f11fe95454338562a2b44
Georgios Papadakis <ggppdk@users.noreply.github.com>
2016-08-03 22:05:04 +0300

    Code style, fix too long line

--- plugins/system/sef/sef.php
+++ plugins/system/sef/sef.php
@@ -153,7 +153,8 @@ class PlgSystemSef extends JPlugin
 		// Replace all unknown protocols in CSS background image.
 		if (strpos($buffer, 'style=') !== false)
 		{
-			$regex  = '#style=\s*([\'\"])(.*):\s*url\s*\(([\'\"]|\&\#0?3[4|9];)?(?!/|\&\#0?3[4|9];|' . $protocols . '|\#)([^\)\'\"]+)([\'\"]|\&\#0?3[4|9];)?\)#m';
+			$regex_url  = '\s*url\s*\(([\'\"]|\&\#0?3[4|9];)?(?!/|\&\#0?3[4|9];|' . $protocols . '|\#)([^\)\'\"]+)([\'\"]|\&\#0?3[4|9];)?\)';
+			$regex  = '#style=\s*([\'\"])(.*):' . $regex_url . '#m';
 			$buffer = preg_replace($regex, 'style=$1$2: url($3' . $base . '$4$5$6)', $buffer);
 			$this->checkBuffer($buffer);
 		}
