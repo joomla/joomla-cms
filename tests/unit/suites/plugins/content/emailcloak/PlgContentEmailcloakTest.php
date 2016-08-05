5167ece945facb659bbafde98c7623f26a80c9ff
Phil Taylor <phil@phil-taylor.com>
2016-08-05 13:00:29 +0100

    Add test for #11378 as an example & more documentation

--- tests/unit/suites/plugins/content/emailcloak/PlgContentEmailcloakTest.php
+++ tests/unit/suites/plugins/content/emailcloak/PlgContentEmailcloakTest.php
@@ -59,6 +59,9 @@ class PlgContentEmailcloakTest extends TestCaseDatabase
 
     /**
      * Provides the data to test the constructor method.
+     * more examples to add can be found here:
+     *  - https://github.com/joomla/joomla-cms/pull/4182#issuecomment-53395318
+     *  - https://github.com/joomla/joomla-cms/pull/3735#issue-35215540
      *
      * @return  array
      *
@@ -67,12 +70,52 @@ class PlgContentEmailcloakTest extends TestCaseDatabase
     public function dataTestOnContentPrepare()
     {
         return array(
-            array('this should not be parsed as it has no (at) sign in it - see what I did there? ;)',
-                '',
-                'this should not be parsed as it has no (at) sign in it - see what I did there? ;)'
+
+            # 0
+            array(
+                // This first row is the input, this is what would be in the article
+                'this should not be parsed as it has no (at) sign in it - see what I did there? ;)',
+
+                /**
+                 * This second row is what you would expect the JS, after rendering in a browser
+                 * At the moment there is a slight unit test bug in that what you see here will start with
+                 * the opening <a> tag, and not the full surrounding html from the article - I might fix that in future
+                 * but for now we are testing the actual replacement inside the <a> tags and not the surrounding html
+                 * due to the crazyness of the unit tests converting from JS to HTML to compare
+                 */
+                'this should not be parsed as it has no (at) sign in it - see what I did there? ;)',
+
+                // this third row is the full output of the cloak with inline javascript mode enabled
+                ''
+
             ),
+
+            # ? - see: https://github.com/joomla/joomla-cms/pull/11378#issuecomment-237829598
+            /**
+             * This is failing at the moment so Im excluding it while the above PR #11378 is worked on
+             * and while this unit test suite is being improved - but this test highlights the fact we need these tests!
+             *
+             * array(
+             * '<a href="mailto:toto@toto.com?subject=Mysubject" class="myclass" >email</a>',
+             * "<a href='mailto:toto@toto.com?subject=Mysubject' class='myclass' >email</a>",
+             * "
+             * <span id=\"cloak__HASH__\">JLIB_HTML_CLOAKING</span><script type='text/javascript'>
+             * document.getElementById('cloak__HASH__').innerHTML = '';
+             * var prefix = '&#109;a' + 'i&#108;' + '&#116;o';
+             * var path = 'hr' + 'ef' + '=';
+             * var addy__HASH__ = 't&#111;t&#111;' + '&#64;';
+             * addy__HASH__ = addy__HASH__ + 't&#111;t&#111;' + '&#46;' + 'c&#111;m?s&#117;bj&#101;ct=Mys&#117;bj&#101;ct';
+             * var addy_text__HASH__ = '&#101;m&#97;&#105;l';document.getElementById('cloak__HASH__').innerHTML += '<a ' + path + '\'' + prefix + ':' + addy__HASH__ + '\' class=\"myclass\" >'+addy_text__HASH__+'<\/a>';
+             * </script>
+             * "
+             * ),*/
+
+
+            # 1
             array(
                 '<a href="http://mce_host/ourdirectory/email@example.org">anytext</a>',
+
+                "<a href='mailto:email@example.org'>anytext</a>",
 
                 "<span id=\"cloak__HASH__\">JLIB_HTML_CLOAKING</span><script type='text/javascript'>
 				document.getElementById('cloak__HASH__').innerHTML = '';
@@ -82,12 +125,15 @@ class PlgContentEmailcloakTest extends TestCaseDatabase
 				addy__HASH__ = addy__HASH__ + '&#101;x&#97;mpl&#101;' + '&#46;' + '&#111;rg';
 				var addy_text__HASH__ = '&#97;nyt&#101;xt';document.getElementById('cloak__HASH__').innerHTML += '<a ' + path + '\'' + prefix + ':' + addy__HASH__ + '\'>'+addy_text__HASH__+'<\/a>';
 		</script>
-                ",
-
-                "<a href='mailto:email@example.org'>anytext</a>"
+                "
             ),
+
+            # 2
             array(
                 '<p><a href="mailto:joe@nowhere.com"><span style="font-style: 8pt;">Joe_fontsize8</span></a></p>',
+
+                // This is out expected output - note the comment in above for the reason it doesnt have the surrounding <p> tags
+                "<a href='mailto:joe@nowhere.com'><span style=\"font-style: 8pt;\">Joe_fontsize8</span></a>",
 
                 "<p><span id=\"cloak__HASH__\">JLIB_HTML_CLOAKING</span><script type='text/javascript'>
                 document.getElementById('cloak__HASH__').innerHTML = '';
@@ -99,8 +145,12 @@ class PlgContentEmailcloakTest extends TestCaseDatabase
 		</script></p>
                 "
             ),
+
+            # 3
             array(
                 '<p><a href="mailto:joe@nowhere13.com?subject= A text"><span style="font-size: 14pt;">Joe_subject_ fontsize13</span></a></p>',
+
+                '<a href=\'mailto:joe@nowhere13.com?subject= A text\'?subject= A text><span style="font-size: 14pt;">Joe_subject_ fontsize13</span></a>',
 
                 "
                 <p><span id=\"cloak__HASH__\">JLIB_HTML_CLOAKING</span><script type='text/javascript'>
@@ -111,9 +161,7 @@ class PlgContentEmailcloakTest extends TestCaseDatabase
 				addy__HASH__ = addy__HASH__ + 'nowhere13' + '.' + 'com?subject= A text';
 				var addy_text__HASH__ = '<span style=\"font-size: 14pt;\">Joe_subject_ fontsize13</span>';document.getElementById('cloak__HASH__').innerHTML += '<a ' + path + '\'' + prefix + ':' + addy__HASH__ + '\'?subject= A text>'+addy_text__HASH__+'<\/a>';				
 		</script></p>
-                ",
-
-                '<a href=\'mailto:joe@nowhere13.com?subject= A text\'?subject= A text><span style="font-size: 14pt;">Joe_subject_ fontsize13</span></a>'
+                "
             ),
 
 
@@ -175,7 +223,7 @@ class PlgContentEmailcloakTest extends TestCaseDatabase
      * @dataProvider  dataTestOnContentPrepare
      * @since         3.6.2
      */
-    public function testOnContentPrepareWithRowNoFinder($input, $expectedJs, $expectedHTML = NULL)
+    public function testOnContentPrepareWithRowNoFinder($input, $expectedHTML = NULL, $expectedJs)
     {
         $row = new \stdClass;
         $row->text = $input;
@@ -201,21 +249,30 @@ class PlgContentEmailcloakTest extends TestCaseDatabase
             $cloakHTML = '<span id="cloak' . $hash . '">JLIB_HTML_CLOAKING</span>';
             $this->assertContains($cloakHTML, $row->text);
 
-            // need to do this to overcome whitespace comparison issue in phpunit for some reason...
-            preg_match_all("/\<script type=\'text\/javascript\'\>(.*)<\/script>/ism", $row->text, $innerJS);
-            $result = trim($innerJS[1][0]);
 
-            preg_match_all("/\<script type=\'text\/javascript\'\>(.*)<\/script>/ism", $expectedJs, $innerJS);
-            $expected = trim($innerJS[1][0]);
+            if ($expectedJs) {
+                // need to do this to overcome whitespace comparison issue in phpunit for some reason...
+                preg_match_all("/\<script type=\'text\/javascript\'\>(.*)<\/script>/ism", $row->text, $innerJS);
+                $result = trim($innerJS[1][0]);
 
-            // assert the render is as the expected render with injected hash
-            $this->assertEquals(trim(str_replace('__HASH__', $hash, $expected)), $result);
+                preg_match_all("/\<script type=\'text\/javascript\'\>(.*)<\/script>/ism", $expectedJs, $innerJS);
+                $expected = trim($innerJS[1][0]);
 
+                // assert the render is as the expected render with injected hash
+                $this->assertEquals(trim(str_replace('__HASH__', $hash, $expected)), $result);
 
-            if (NULL !== $expectedHTML) {
-                $html = $this->convertJStoHTML($result, $hash);
-                $this->assertEquals($html, $expectedHTML);
+                if (NULL !== $expectedHTML) {
+                    $html = $this->convertJStoHTML($result, $hash);
+                    $this->assertEquals($html, $expectedHTML);
+                }
             }
+
+
+        } else {
+
+            // ok we never cloaked a email but lets ensure we did not screw up the article text anyway!
+            $this->assertEquals($expectedHTML, $row->text);
+
         }
     }
 
@@ -263,5 +320,23 @@ class PlgContentEmailcloakTest extends TestCaseDatabase
         }
 
         return trim($resultantHTML);
+    }
+
+    /**
+     * Tests that if we are the com_finder indexer that we return with no cloaking
+     * Tests also that we can set $row as string instead of a normal object
+     */
+    public function testIndexer()
+    {
+        $row = 'test string';
+        $params = new JRegistry;
+
+        // assert we have the correct event
+        $this->assertInstanceOf('PlgContentEmailcloak', $this->class);
+
+        // assert that we are getting a clean process
+        $res = $this->class->onContentPrepare('com_finder.indexer', $row, $params);
+        $this->assertEquals(1, $res);
+        $this->assertEquals('test string', $row);
     }
 }
