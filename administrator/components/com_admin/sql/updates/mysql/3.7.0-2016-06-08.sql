5e074a9adf2d68a2c5484d486291b501e297820f
Christian Uhl <github@wmchris>
2016-08-10 10:42:24 +0000

    Forgot to add in last commit

--- administrator/components/com_admin/sql/updates/mysql/3.7.0-2016-06-08.sql
+++ administrator/components/com_admin/sql/updates/mysql/3.7.0-2016-06-08.sql
@@ -1,8 +1,13 @@
+--
+-- Table structure for table `#__nullDate_conversion`
+--
+
 CREATE TABLE IF NOT EXISTS `#__nullDate_conversion` (
   `converted` datetime NOT NULL DEFAULT '0000-00-00 00:00:00'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 DEFAULT COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data for table `#__nullDate_conversion`
+--
+
 INSERT INTO `#__nullDate_conversion` (`converted`) VALUES ('0000-00-00 00:00:00');
-
-
-ALTER TABLE `#__menu` CHANGE `checked_out_time` `checked_out_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00';
