4ce7ee7ce5f6fad0681a3f5521dab992c296d2fe
Yves Hoppe <yves@compojoom.com>
2016-08-13 07:24:23 +0200

    Commented script

--- tests/unit/unit-tests.sh
+++ tests/unit/unit-tests.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 # Script for preparing the unit tests in Joomla!
 
+# Path to the Joomla! installation
 BASE="$1"
 
+# Abort travis execution if setup fails
 set -e
 
 # Make sure all dev dependencies are installed
