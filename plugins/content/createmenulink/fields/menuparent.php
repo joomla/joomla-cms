24272841754f9acb71f8ea9adaf1878237895688
Shubham Rajput <shubham.rajput2013@vit.ac.in>
2016-08-15 03:55:01 +0530

    Added code to get menuordering dynamically
    1. Made a new folder named fields which contains all the required fields
    specified in the xml file

---
+++ plugins/content/createmenulink/fields/menuparent.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * @package     Joomla.Plugin
+ * @subpackage  Content.createmenulink
+ *
+ * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('JPATH_BASE') or die;
+
+JFormHelper::loadFieldClass('list');
+
+/**
+ * Form Field class for the Joomla Framework.
+ *
+ * @since  3.6
+ */
+class JFormFieldMenuParent extends JFormFieldList
+{
+	/**
+	 * The form field type.
+	 *
+	 * @var        string
+	 * @since   3.6
+	 */
+	protected $type = 'MenuParent';
+
+	/**
+	 * Method to get the field options.
+	 *
+	 * @return  array  The field option objects.
+	 *
+	 * @since   3.6
+	 */
+	protected function getOptions()
+	{
+		$options = array();
+
+		$db = JFactory::getDbo();
+		$query = $db->getQuery(true)
+			->select('a.id AS value, a.title AS text, a.level')
+			->from('#__menu AS a')
+			->join('LEFT', $db->quoteName('#__menu') . ' AS b ON a.lft > b.lft AND a.rgt < b.rgt');
+
+		if ($menuType = $this->form->getValue('menutype'))
+		{
+			$query->where('a.menutype = ' . $db->quote($menuType));
+		}
+		else
+		{
+			$query->where('a.menutype != ' . $db->quote(''));
+		}
+
+		// Prevent parenting to children of this item.
+		if ($id = $this->form->getValue('id'))
+		{
+			$query->join('LEFT', $db->quoteName('#__menu') . ' AS p ON p.id = ' . (int) $id)
+				->where('NOT(a.lft >= p.lft AND a.rgt <= p.rgt)');
+		}
+
+		$query->where('a.published != -2')
+			->group('a.id, a.title, a.level, a.lft, a.rgt, a.menutype, a.parent_id, a.published')
+			->order('a.lft ASC');
+
+		// Get the options.
+		$db->setQuery($query);
+
+		try
+		{
+			$options = $db->loadObjectList();
+		}
+		catch (RuntimeException $e)
+		{
+			JError::raiseWarning(500, $e->getMessage());
+		}
+
+		// Pad the option text with spaces using depth level as a multiplier.
+		for ($i = 0, $n = count($options); $i < $n; $i++)
+		{
+			$options[$i]->text = str_repeat('- ', $options[$i]->level) . $options[$i]->text;
+		}
+
+		// Merge any additional options in the XML definition.
+		$options = array_merge(parent::getOptions(), $options);
+
+		return $options;
+	}
+}
