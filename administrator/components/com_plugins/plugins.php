e1253ed826fd62f28ea371e134b966716a54297b
andrepereiradasilva <andrepereiradasilva@users.noreply.github.com>
2016-08-14 01:28:02 +0100

    lowercase

--- administrator/components/com_plugins/plugins.php
+++ administrator/components/com_plugins/plugins.php
@@ -12,9 +12,7 @@ JHtml::_('behavior.tabstate');
 
 if (!JFactory::getUser()->authorise('core.manage', 'com_plugins'))
 {
-	JLoader::register('JControllerExceptionNotAllowed', JPATH_PLATFORM . '/joomla/controller/exception/notallowed.php');
-
-	throw new JControllerExceptionNotAllowed(JText::_('JERROR_ALERTNOAUTHOR'), 403);
+	throw new JControllerExceptionNotallowed(JText::_('JERROR_ALERTNOAUTHOR'), 403);
 }
 
 $controller = JControllerLegacy::getInstance('Plugins');
