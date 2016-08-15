86bbce17bcd9941aacea39d7bc44be5434efd257
Luca Marzo <luca.marzo@live.com>
2016-08-15 15:25:17 +0200

    fixed syntax SQL Azure

--- administrator/components/com_admin/sql/updates/sqlazure/3.6.3-2016-08-15.sql
+++ administrator/components/com_admin/sql/updates/sqlazure/3.6.3-2016-08-15.sql
@@ -2,4 +2,4 @@
 --- Increasing size of the URL field in com_newsfeeds
 ---
 
-ALTER TABLE [#__newsfeeds] ALTER COLUMN [link] NVARCHAR(2048) NOT NULL;
+ALTER TABLE [#__newsfeeds] ALTER COLUMN [link] [nvarchar](2048) NOT NULL;
