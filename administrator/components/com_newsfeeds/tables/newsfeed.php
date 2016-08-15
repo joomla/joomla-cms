c7a54a3b1ae3bfef10d4978a05cc95adf127a61e
andrepereiradasilva <andrepereiradasilva@users.noreply.github.com>
2016-08-15 19:15:20 +0100

    Update newsfeed.php

--- administrator/components/com_newsfeeds/tables/newsfeed.php
+++ administrator/components/com_newsfeeds/tables/newsfeed.php
@@ -17,6 +17,21 @@ defined('_JEXEC') or die;
 class NewsfeedsTableNewsfeed extends JTable
 {
 	/**
+	 * Mapping of database columns alias.
+	 *
+	 * @var    array
+	 * @since  __DEPLOY_VERSION__
+	 */
+	protected $_columnAlias = array(
+		'title'            => 'name',
+		'state'            => 'published',
+		'created_time'     => 'created',
+		'created_user_id'  => 'created_by',
+		'modified_time'    => 'modified',
+		'modified_user_id' => 'modified_by',
+	);
+
+	/**
 	 * Ensure the params, metadata and images are json encoded in the bind method
 	 *
 	 * @var    array
