004bf459cb77973f6075c40e92acbde18451d780
Luca Marzo <luca.marzo@live.com>
2016-08-15 19:32:12 +0200

    Fixed mistake in comment
    Updated comment from
    "Set if article is featured" to "Set if contact is featured"

--- installation/sql/mysql/joomla.sql
+++ installation/sql/mysql/joomla.sql
@@ -290,7 +290,7 @@ CREATE TABLE IF NOT EXISTS `#__contact_details` (
   `metakey` text NOT NULL,
   `metadesc` text NOT NULL,
   `metadata` text NOT NULL,
-  `featured` tinyint(3) unsigned NOT NULL DEFAULT 0 COMMENT 'Set if article is featured.',
+  `featured` tinyint(3) unsigned NOT NULL DEFAULT 0 COMMENT 'Set if contact is featured.',
   `xreference` varchar(50) NOT NULL COMMENT 'A reference to enable linkages to external data sets.',
   `publish_up` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   `publish_down` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
