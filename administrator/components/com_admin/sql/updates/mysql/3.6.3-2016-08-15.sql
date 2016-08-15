57205b52bd071dfd915077edd3f00827e44b4757
Luca Marzo <luca.marzo@live.com>
2016-08-15 15:26:14 +0200

    fixed syntax

--- administrator/components/com_admin/sql/updates/mysql/3.6.3-2016-08-15.sql
+++ administrator/components/com_admin/sql/updates/mysql/3.6.3-2016-08-15.sql
@@ -2,4 +2,4 @@
 --- Increasing size of the URL field in com_newsfeeds
 ---
 
-ALTER TABLE `#__newsfeeds` MODIFY `link` VARCHAR(2048);
+ALTER TABLE `#__newsfeeds` MODIFY `link` VARCHAR(2048) NOT NULL;
