09d4275801bacb39c469825ce31c33fceeae2b51
zero-24 <zero-24@users.noreply.github.com>
2016-08-18 00:43:37 +0200

    Fix the doc blocks for the new JS Tests [2] (#11652)
    * fix doc blocks
    
    * double spaces

--- tests/javascript/sendtestmail/spec.js
+++ tests/javascript/sendtestmail/spec.js
@@ -1,8 +1,10 @@
 /**
+ * @package     Joomla.Tests
+ * @subpackage  JavaScript Tests
+ *
  * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
- * @package     Joomla
- * @subpackage  JavaScript Tests
+ *
  * @since       __DEPLOY_VERSION__
  * @version     1.0.0
  */
