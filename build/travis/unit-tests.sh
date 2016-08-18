4fc3861dbe4f5fecb3e825ada870684614937e89
Tomasz Narloch <csthomas@users.noreply.github.com>
2016-08-18 14:33:37 +0200

    Update unit-tests.sh - typo, do not forget to test php memcache extension
    Fix typo.
    
    This is related to #11576. 
    IMHO this PR should be applied after PR #11565 will be merged.

--- build/travis/unit-tests.sh
+++ build/travis/unit-tests.sh
@@ -22,7 +22,7 @@ psql -d joomla_ut -a -f "$BASE/tests/unit/schema/postgresql.sql"
 # - ./build/travis/php-apache.sh
 # Enable additional PHP extensions
 
-if [[ $INSTALL_MEMCACHE == "yes" ]]; then phpenv config-add "$BASE/build/travis/phpenv/memcached.ini"; fi
+if [[ $INSTALL_MEMCACHE == "yes" ]]; then phpenv config-add "$BASE/build/travis/phpenv/memcache.ini"; fi
 if [[ $INSTALL_MEMCACHED == "yes" ]]; then phpenv config-add "$BASE/build/travis/phpenv/memcached.ini"; fi
 if [[ $INSTALL_APC == "yes" ]]; then phpenv config-add "$BASE/build/travis/phpenv/apc-$TRAVIS_PHP_VERSION.ini"; fi
 if [[ $INSTALL_APCU == "yes" && $TRAVIS_PHP_VERSION = 5.* ]]; then printf "\n" | pecl install apcu-4.0.10 && phpenv config-add "$BASE/build/travis/phpenv/apcu-$TRAVIS_PHP_VERSION.ini"; fi
