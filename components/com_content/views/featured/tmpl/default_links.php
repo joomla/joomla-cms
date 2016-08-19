3504b2bd3866a00dd2a2dbe6f4c5d03d95609629
Alessandro Rossi <alexred@alexred.com>
2016-08-19 11:13:38 +0100

    "More Articles ..." text don't appear
    Fix in the "Featured Articles" menu item the text "More Articles ..." don't appear after some article intro (5) and the box with links to other articles.

--- components/com_content/views/featured/tmpl/default_links.php
+++ components/com_content/views/featured/tmpl/default_links.php
@@ -9,6 +9,7 @@
 
 defined('_JEXEC') or die;
 ?>
+<h3><?php echo JText::_('COM_CONTENT_MORE_ARTICLES'); ?></h3>
 <ol class="nav nav-tabs nav-stacked">
 <?php foreach ($this->link_items as &$item) : ?>
 	<li>
