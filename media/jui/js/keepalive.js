bdd861b6d78ef7671d0f0dbc3ede27ee2bb401f4
andrepereiradasilva <andrepereiradasilva@users.noreply.github.com>
2016-08-21 20:53:04 +0100

    Update keepalive.js

--- media/jui/js/keepalive.js
+++ media/jui/js/keepalive.js
@@ -10,23 +10,17 @@
  *
  * @package  Joomla
  * @since    __DEPLOY_VERSION__
- * @version  1.0
  */
 !(function(){
 	'use strict';
-	// Keepalive function
-	window.keepalive = function() {
-		var keepalive_element  = document.getElementById('keepalive');
-		if (keepalive_element)
-		{
-			var keepalive_uri = keepalive_element.getAttribute('data-keepalive-uri'), keepalive_interval = keepalive_element.getAttribute('data-keepalive-interval');
-		}
-		// If id attribute is not found in the script tag (script loaders extensions) set defaults
-		else
-		{
-			var keepalive_uri = window.location.pathname + '?option=com_ajax&format=json', keepalive_interval = 0.75 * 60 * 1000;
-		}
-		setInterval(function() {
+
+	document.addEventListener('DOMContentLoaded', function() {
+
+		var keepaliveElement  = document.getElementById('keepalive');
+			keepaliveUri      = keepaliveElement ? keepaliveElement.getAttribute('data-uri') : window.location.pathname + '?option=com_ajax&format=json',
+			keepaliveInterval = keepaliveElement ? keepaliveElement.getAttribute('data-interval') : 0.75 * 60 * 1000;
+
+		window.setInterval(function() {
 			var r;
 			try
 			{
@@ -35,21 +29,11 @@
 			catch (e) {}
 			if (r)
 			{
-				r.open('GET', keepalive_uri, true);
+				r.open('GET', keepaliveUri, true);
 				r.send(null);
 			}
-		}, keepalive_interval);
-	};
+		}, keepaliveInterval);
 
-	// If document already loaded when script is executed run keepalive
-	if (document.readyState === 'complete' || document.readyState == "loaded")
-	{
-		window.keepalive();
-	}
-	// If document not yet loaded when script is executed run on onload event
-	else
-	{
-		var listenerMethod = window.addEventListener || window.attachEvent, eventName = window.addEventListener ? 'load' : 'onload';
-		listenerMethod(eventName, window.keepalive());
-	}
+	});
+
 })(window);
