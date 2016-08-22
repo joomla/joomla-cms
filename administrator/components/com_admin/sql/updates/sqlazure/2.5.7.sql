c05e0c6a6866c743a9e25ec40507a3446a6ce6bc
andrepereiradasilva <andrepereiradasilva@users.noreply.github.com>
2016-08-22 11:50:25 +0100

    Update 2.5.7.sql

--- administrator/components/com_admin/sql/updates/sqlazure/2.5.7.sql
+++ administrator/components/com_admin/sql/updates/sqlazure/2.5.7.sql
@@ -3,14 +3,14 @@ SET IDENTITY_INSERT [#__update_sites] ON;
 INSERT INTO [#__update_sites] ([name], [type], [location], [enabled], [last_check_timestamp])
 SELECT 'Accredited Joomla! Translations', 'collection', 'http://update.joomla.org/language/translationlist.xml', 1, 0;
 
-SET IDENTITY_INSERT [#__update_sites] ON;
+SET IDENTITY_INSERT [#__update_sites] OFF;
 
 SET IDENTITY_INSERT [#__update_sites_extensions] ON;
 
 INSERT INTO [#__update_sites_extensions] ([update_site_id], [extension_id])
 SELECT SCOPE_IDENTITY(), 600;
 
-SET IDENTITY_INSERT [#__update_sites_extensions] ON;
+SET IDENTITY_INSERT [#__update_sites_extensions] OFF;
 
 UPDATE [#__assets] SET [name] = REPLACE([name], 'com_user.notes.category','com_users.category');
 UPDATE [#__categories] SET [extension] = REPLACE([extension], 'com_user.notes.category','com_users.category');
