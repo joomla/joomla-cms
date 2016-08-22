c6d8a0b62983d4462670e967e851fd9677b4aa5a
andrepereiradasilva <andrepereiradasilva@users.noreply.github.com>
2016-08-22 13:28:58 +0100

    we are not setting identity here

--- administrator/components/com_admin/sql/updates/sqlazure/3.5.0-2015-11-05.sql
+++ administrator/components/com_admin/sql/updates/sqlazure/3.5.0-2015-11-05.sql
@@ -5,9 +5,5 @@ SELECT 454, 'plg_system_stats', 'plugin', 'stats', 'system', 0, 1, 1, 0, '', '',
 
 SET IDENTITY_INSERT [#__extensions] OFF;
 
-SET IDENTITY_INSERT [#__postinstall_messages] ON;
-
 INSERT INTO [#__postinstall_messages] ([extension_id], [title_key], [description_key], [action_key], [language_extension], [language_client_id], [type], [action_file], [action], [condition_file], [condition_method], [version_introduced], [enabled])
 SELECT 700, 'COM_CPANEL_MSG_STATS_COLLECTION_TITLE', 'COM_CPANEL_MSG_STATS_COLLECTION_BODY', '', 'com_cpanel', 1, 'message', '', '', 'admin://components/com_admin/postinstall/statscollection.php', 'admin_postinstall_statscollection_condition', '3.5.0', 1;
-
-SET IDENTITY_INSERT [#__postinstall_messages] OFF;
