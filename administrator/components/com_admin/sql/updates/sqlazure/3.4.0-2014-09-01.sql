155b0cb4a651b3e81506b4f9c900d5b71cc8d154
andrepereiradasilva <andrepereiradasilva@users.noreply.github.com>
2016-08-22 13:27:42 +0100

    we are not setting identity here

--- administrator/components/com_admin/sql/updates/sqlazure/3.4.0-2014-09-01.sql
+++ administrator/components/com_admin/sql/updates/sqlazure/3.4.0-2014-09-01.sql
@@ -5,16 +5,8 @@ SELECT 801, 'weblinks', 'package', 'pkg_weblinks', '', 0, 1, 1, 0, '', '', '', '
 
 SET IDENTITY_INSERT [#__extensions] OFF;
 
-SET IDENTITY_INSERT [#__update_sites] ON;
-
 INSERT INTO [#__update_sites] ([name], [type], [location], [enabled])
 SELECT 'Weblinks Update Site', 'extension', 'https://raw.githubusercontent.com/joomla-extensions/weblinks/master/manifest.xml', 1;
 
-SET IDENTITY_INSERT [#__update_sites] OFF;
-
-SET IDENTITY_INSERT [#__update_sites_extensions] ON;
-
 INSERT INTO [#__update_sites_extensions] ([update_site_id], [extension_id])
 SELECT (SELECT [update_site_id] FROM [#__update_sites] WHERE [name] = 'Weblinks Update Site'), 801;
-
-SET IDENTITY_INSERT [#__update_sites_extensions] OFF;
