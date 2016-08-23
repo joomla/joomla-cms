c6954845519a00e58c72a382fea10a04e34d9e92
andrepereiradasilva <andrepereiradasilva@users.noreply.github.com>
2016-08-23 11:56:43 +0100

    Create cs-tests.sh

---
+++ build/travis/cs-tests.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Script for preparing the unit tests in Joomla!
+
+# Path to the Joomla! installation
+BASE="$1"
+
+# Abort travis execution if setup fails
+set -e
+
+# Make sure all dev dependencies are installed
+composer install
