7de27c4644efb17dfcd65a56671e21af49eee680
andrepereiradasilva <andrepereiradasilva@users.noreply.github.com>
2016-08-23 22:04:05 +0100

    Update 3.6.3-2016-08-23.sql

--- administrator/components/com_admin/sql/updates/sqlazure/3.6.3-2016-08-23.sql
+++ administrator/components/com_admin/sql/updates/sqlazure/3.6.3-2016-08-23.sql
@@ -1,3 +1,5 @@
--- Add default value for custom_data and system_data.
+-- Add default value for custom_data and system_data fields in the #__extensions table.
 ALTER TABLE [#__extensions] ALTER COLUMN [custom_data] [nvarchar](max) NOT NULL DEFAULT '';
 ALTER TABLE [#__extensions] ALTER COLUMN [system_data] [nvarchar](max) NOT NULL DEFAULT '';
+-- Add default value for data field in the #__updates table.
+ALTER TABLE [#__updates] ALTER COLUMN [data] [nvarchar](max) NOT NULL DEFAULT '';
