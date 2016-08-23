0962afdc1498a912a345bd3f72dc95db2e0ed68f
andrepereiradasilva <andrepereiradasilva@users.noreply.github.com>
2016-08-23 22:04:10 +0100

    Update 3.6.3-2016-08-23.sql

--- administrator/components/com_admin/sql/updates/mysql/3.6.3-2016-08-23.sql
+++ administrator/components/com_admin/sql/updates/mysql/3.6.3-2016-08-23.sql
@@ -1,3 +1,5 @@
--- Add default value for custom_data and system_data.
+-- Add default value for custom_data and system_data fields in the #__extensions table.
 ALTER TABLE `#__extensions` MODIFY `custom_data` text NOT NULL DEFAULT '';
 ALTER TABLE `#__extensions` MODIFY `system_data` text NOT NULL DEFAULT '';
+-- Add default value for data field in the #__updates table.
+ALTER TABLE `#__updates` MODIFY `custom_data` text NOT NULL DEFAULT '';
