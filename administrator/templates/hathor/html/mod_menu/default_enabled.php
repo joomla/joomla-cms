908e4926fae59668a1417b24bf2bf0ca41378591
Shubham Rajput <shubham.rajput2013@vit.ac.in>
2016-08-24 02:04:53 +0530

    Removed uncessary code and improved language files

--- administrator/templates/hathor/html/mod_menu/default_enabled.php
+++ administrator/templates/hathor/html/mod_menu/default_enabled.php
@@ -91,7 +91,7 @@ if ($user->authorise('core.manage', 'com_users'))
 	if ($createUser)
 	{
 		$menu->addChild(
-			new JMenuNode(JText::_('MOD_MENU_COM_USERS_NOTE_NEW_CATEGORY'), 'index.php?option=com_categories&task=category.add&extension=com_users', 'class:newarticle')
+			new JMenuNode(JText::_('MOD_MENU_COM_CONTENT_NEW_CATEGORY'), 'index.php?option=com_categories&task=category.add&extension=com_users', 'class:newarticle')
 		);
 		$menu->getParent();
 	}
