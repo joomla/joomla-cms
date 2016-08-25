7e5e52d495e193410a1de0867a9c315a0748e48a
andrepereiradasilva <andrepereiradasilva@users.noreply.github.com>
2016-08-25 20:07:49 +0100

    Update 3.6.3-2016-08-23.sql

--- administrator/components/com_admin/sql/updates/sqlazure/3.6.3-2016-08-23.sql
+++ administrator/components/com_admin/sql/updates/sqlazure/3.6.3-2016-08-23.sql
@@ -5,3 +5,5 @@ ALTER TABLE [#__extensions] ALTER COLUMN [system_data] [nvarchar](max) NOT NULL 
 ALTER TABLE [#__updates] ALTER COLUMN [data] [nvarchar](max) NOT NULL DEFAULT '';
 -- Add default value for asset_id field in the #__languages table.
 ALTER TABLE [#__languages] ALTER COLUMN [asset_id] [bigint] NOT NULL DEFAULT 0;
+-- Add default value for asset_id field in the #__menu_types table.
+ALTER TABLE [#__menu_types] ALTER COLUMN [asset_id] [bigint] NOT NULL DEFAULT 0;
