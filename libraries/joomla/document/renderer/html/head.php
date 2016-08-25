813d2bcb4e9b6f4ca9b8e790186342f01ad4ec67
Jean-Marie Simonet <infografjms@gmail.com>
2016-08-25 10:09:47 +0200

    Taking of the chnage to the <base tag

--- libraries/joomla/document/renderer/html/head.php
+++ libraries/joomla/document/renderer/html/head.php
@@ -74,7 +74,7 @@ class JDocumentRendererHtmlHead extends JDocumentRenderer
 
 		if (!empty($base))
 		{
-			$buffer .= $tab . '<base href="' . urldecode($base) . '" />' . $lnEnd;
+			$buffer .= $tab . '<base href="' . $base . '" />' . $lnEnd;
 		}
 
 		// Generate META tags (needs to happen as early as possible in the head)
