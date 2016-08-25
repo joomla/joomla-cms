76e2e46baf8bc084e05f1fb463815f19318a0f46
andrepereiradasilva <andrepereiradasilva@users.noreply.github.com>
2016-08-25 20:02:17 +0100

    Update 3.6.3-2016-08-23.sql

--- administrator/components/com_admin/sql/updates/mysql/3.6.3-2016-08-23.sql
+++ administrator/components/com_admin/sql/updates/mysql/3.6.3-2016-08-23.sql
@@ -3,3 +3,5 @@ ALTER TABLE `#__extensions` MODIFY `custom_data` text NOT NULL DEFAULT '';
 ALTER TABLE `#__extensions` MODIFY `system_data` text NOT NULL DEFAULT '';
 -- Add default value for data field in the #__updates table.
 ALTER TABLE `#__updates` MODIFY `data` text NOT NULL DEFAULT '';
+-- Add default value for asset_id field in the #__languages table.
+ALTER TABLE `#__languages` MODIFY `asset_id` int(11) NOT NULL DEFAULT 0;
