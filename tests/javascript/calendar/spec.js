492ae1e3d68bb72facb1a78b6b90555bd8bbce7b
dgt41 <d.grammatiko@gmail.com>
2016-08-27 00:45:38 +0300

    coverage

--- tests/javascript/calendar/spec.js
+++ tests/javascript/calendar/spec.js
@@ -9,13 +9,36 @@
 
 define(['jquery', 'testsRoot/calendar/spec-setup', 'jasmineJquery'], function ($) {
 
+	var esc = $.Event("keydown", { keyCode: 27 });
+
 	beforeAll(function () {
 		JoomlaCalendar.init(".field-calendar");
 	});
 
-	describe('Calendar is applied for the input element', function () {
-		it('Should have calendar element under input', function () {
+	describe('Calendar set for the input element', function () {
+		it('Should have calendar element under the input element', function () {
 			expect($('body')).toContainElement('.j-calendar');
 		});
 	});
+
+	describe('Calendar should appear on button click', function () {
+		it('Should appear on button click', function () {
+			$('.field-calendar').find('button').click();
+			expect($('.j-calendar')).toHaveAttr('style', 'position: absolute; box-shadow: 0px 0px 70px 0px rgba(0, 0, 0, 0.67); padding: 0px; left: auto; top: auto; display: block;')
+		});
+	});
+
+	describe('Calendar should appear on button click', function () {
+		it('Should appear on button click', function () {
+			$('.field-calendar').find('button').click();
+			expect($('.j-calendar')).toHaveAttr('style', 'position: absolute; box-shadow: 0px 0px 70px 0px rgba(0, 0, 0, 0.67); padding: 0px; left: auto; top: auto; display: block;')
+		});
+	});
+
+	// describe('Calendar should disappear on document click', function () {
+	// 	it('Should close on document click', function () {
+	// 		jQuery(".modal-backdrop").trigger("click");
+	// 		expect($('.j-calendar')).toHaveAttr('style', 'position: absolute; box-shadow: 0px 0px 70px 0px rgba(0, 0, 0, 0.67); padding: 0px; left: auto; top: auto; display: none;');
+	// 	});
+	// });
 });
