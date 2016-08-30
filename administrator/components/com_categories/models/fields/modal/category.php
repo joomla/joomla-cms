5e663cde529caa8d2d7f2605bf1a51a309b4dbd8
andrepereiradasilva <andrepereiradasilva@users.noreply.github.com>
2016-08-31 00:19:26 +0100

    Update category.php

--- administrator/components/com_categories/models/fields/modal/category.php
+++ administrator/components/com_categories/models/fields/modal/category.php
@@ -215,13 +215,13 @@ class JFormFieldModal_Category extends JFormField
 					'width'       => '800px',
 					'bodyHeight'  => '70',
 					'modalWidth'  => '80',
-					'footer'      => '<a role="button" class="btn btn-cancel" aria-hidden="true"'
+					'footer'      => '<a role="button" class="btn" aria-hidden="true"'
 							. ' onclick="window.processModalEdit(this, \'' . $this->id . '\', \'add\', \'category\', \'cancel\', \'item-form\'); return false;">'
 							. JText::_("JLIB_HTML_BEHAVIOR_CLOSE") . '</a>'
-							. '<a role="button" class="btn btn-save btn-primary hidden" aria-hidden="true"'
+							. '<a role="button" class="btn btn-primary" aria-hidden="true"'
 							. ' onclick="window.processModalEdit(this, \'' . $this->id . '\', \'add\', \'category\', \'save\', \'item-form\'); return false;">'
 							. JText::_("JSAVE") . '</a>'
-							. '<a role="button" class="btn btn-apply btn-success" aria-hidden="true"'
+							. '<a role="button" class="btn btn-success" aria-hidden="true"'
 							. ' onclick="window.processModalEdit(this, \'' . $this->id . '\', \'add\', \'category\', \'apply\', \'item-form\'); return false;">'
 							. JText::_("JAPPLY") . '</a>',
 				)
@@ -244,13 +244,13 @@ class JFormFieldModal_Category extends JFormField
 					'width'       => '800px',
 					'bodyHeight'  => '70',
 					'modalWidth'  => '80',
-					'footer'      => '<a role="button" class="btn btn-cancel" aria-hidden="true"'
+					'footer'      => '<a role="button" class="btn" aria-hidden="true"'
 							. ' onclick="window.processModalEdit(this, \'' . $this->id . '\', \'edit\', \'category\', \'cancel\', \'item-form\'); return false;">'
 							. JText::_("JLIB_HTML_BEHAVIOR_CLOSE") . '</a>'
-							. '<a role="button" class="btn btn-save btn-primary" aria-hidden="true"'
+							. '<a role="button" class="btn btn-primary" aria-hidden="true"'
 							. ' onclick="window.processModalEdit(this, \'' . $this->id . '\', \'edit\', \'category\', \'save\', \'item-form\'); return false;">'
 							. JText::_("JSAVE") . '</a>'
-							. '<a role="button" class="btn btn-apply btn-success" aria-hidden="true"'
+							. '<a role="button" class="btn btn-success" aria-hidden="true"'
 							. ' onclick="window.processModalEdit(this, \'' . $this->id . '\', \'edit\', \'category\', \'apply\', \'item-form\'); return false;">'
 							. JText::_("JAPPLY") . '</a>',
 				)
