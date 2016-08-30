b87df6de016104aa110aec1059f298f6e0b1d938
andrepereiradasilva <andrepereiradasilva@users.noreply.github.com>
2016-08-30 11:05:24 +0100

    cs  ...

--- administrator/components/com_content/views/article/view.html.php
+++ administrator/components/com_content/views/article/view.html.php
@@ -75,7 +75,7 @@ class ContentViewArticle extends JViewLegacy
 			}
 		}
 		// If not in associations modal, block the language change if in edit modal, language not All and associations enabled.
-		else if ($this->item->id && $this->form->getValue('language', null, '*') != '*' && JLanguageAssociations::isEnabled())
+		elseif ($this->item->id && $this->form->getValue('language', null, '*') != '*' && JLanguageAssociations::isEnabled())
 		{
 			$this->form->setFieldAttribute('language', 'readonly', 'true');
 		}
