5b1f8453b0bdc1f1cd16b5264187f53117cf6085
nvyush <nvyush@yandex.ru>
2016-08-31 16:45:35 +0300

    Code optimization for generating pathway

--- components/com_content/views/article/view.html.php
+++ components/com_content/views/article/view.html.php
@@ -225,11 +225,9 @@ class ContentViewArticle extends JViewLegacy
 				$category = $category->getParent();
 			}
 
-			$path = array_reverse($path);
-
-			foreach ($path as $item)
+			for ($i = count($path) - 1; $i > -1; --$i)
 			{
-				$pathway->addItem($item['title'], $item['link']);
+				$this->pathway->addItem($path[$i]['title'], $path[$i]['link']);
 			}
 		}
 
