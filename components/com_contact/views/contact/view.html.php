f58ba7f2c5c06de528c73758e757ae195528fa1b
nvyush <nvyush@yandex.ru>
2016-08-31 16:42:11 +0300

    Code optimization for generating pathway

--- components/com_contact/views/contact/view.html.php
+++ components/com_contact/views/contact/view.html.php
@@ -342,11 +342,9 @@ class ContactViewContact extends JViewLegacy
 				$category = $category->getParent();
 			}
 
-			$path = array_reverse($path);
-
-			foreach ($path as $item)
+			for ($i = count($path) - 1; $i > -1; --$i)
 			{
-				$pathway->addItem($item['title'], $item['link']);
+				$this->pathway->addItem($path[$i]['title'], $path[$i]['link']);
 			}
 		}
 
