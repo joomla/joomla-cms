200a2e7a71625648f9d5365f0c592a6a034bd4f1
Nicola Galgano <optimus4joomla@gmail.com>
2016-09-03 09:15:20 +0200

    fix for filter fields and table headings 3/3
    fix for filter fields and table headings 3/3

--- components/com_newsfeeds/views/category/tmpl/default_items.php
+++ components/com_newsfeeds/views/category/tmpl/default_items.php
@@ -20,7 +20,9 @@ $listDirn  = $this->escape($this->state->get('list.direction'));
 <?php else : ?>
 
 <form action="<?php echo htmlspecialchars(JUri::getInstance()->toString(), ENT_COMPAT, 'UTF-8'); ?>" method="post" name="adminForm" id="adminForm">
-	<?php if (($this->params->get('filter_field') != 'hide') || $this->params->get('show_pagination_limit')) :?>
+	<?php 
+		if (($this->params->get('filter_field') != 'hide') || $this->params->get('show_pagination_limit')) :
+	?>
 	<fieldset class="filters btn-toolbar">
 		<?php if (($this->params->get('filter_field') != 'hide') && ($this->params->get('filter_field') == '1')) :?>
 			<div class="btn-group">
