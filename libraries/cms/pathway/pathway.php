8f66160f6174397d7324d843c21266e2f5220066
Michael Babker <mbabker@flbab.com>
2016-09-23 13:39:04 +0300

    Remove manual include behaviors

--- libraries/cms/pathway/pathway.php
+++ libraries/cms/pathway/pathway.php
@@ -71,24 +71,6 @@ class JPathway
 
 			if (!class_exists($classname))
 			{
-				// @deprecated 4.0 Everything in this block is deprecated but the warning is only logged after the file_exists
-				// Load the pathway object
-				$info = JApplicationHelper::getClientInfo($client, true);
-
-				if (is_object($info))
-				{
-					$path = $info->path . '/includes/pathway.php';
-
-					if (file_exists($path))
-					{
-						JLog::add('Non-autoloadable JPathway subclasses are deprecated, support will be removed in 4.0.', JLog::WARNING, 'deprecated');
-						include_once $path;
-					}
-				}
-			}
-
-			if (!class_exists($classname))
-			{
 				throw new RuntimeException(JText::sprintf('JLIB_APPLICATION_ERROR_PATHWAY_LOAD', $client), 500);
 			}
 
