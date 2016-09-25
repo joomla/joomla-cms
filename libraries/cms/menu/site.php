f9cb3fe371c855fa62cf101cac308890500b293c
frank <frank@frankmayer.net>
2016-09-25 20:19:07 +0300

    Merge unset() calls

--- libraries/cms/menu/site.php
+++ libraries/cms/menu/site.php
@@ -145,8 +145,7 @@ class JMenuSite extends JMenu
 			}
 			elseif ($values[$key] === null)
 			{
-				unset($attributes[$key]);
-				unset($values[$key]);
+				unset($attributes[$key], $values[$key]);
 			}
 
 			// Filter by access level if not set
@@ -157,8 +156,7 @@ class JMenuSite extends JMenu
 			}
 			elseif ($values[$key] === null)
 			{
-				unset($attributes[$key]);
-				unset($values[$key]);
+				unset($attributes[$key], $values[$key]);
 			}
 		}
 
