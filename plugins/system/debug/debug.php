a8f2a36888bb21346e4b418552c1be19a64b4df4
Frank Mayer <frank@frankmayer.net>
2016-09-29 14:39:20 +0300

    Replace substr() with strpos()

--- plugins/system/debug/debug.php
+++ plugins/system/debug/debug.php
@@ -234,7 +234,7 @@ class PlgSystemDebug extends JPlugin
 
 		// No debug for Safari and Chrome redirection.
 		if (strpos(strtolower(isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : ''), 'webkit') !== false
-			&& substr($contents, 0, 50) == '<html><head><meta http-equiv="refresh" content="0;')
+			&& strpos($contents, '<html><head><meta http-equiv="refresh" content="0;') === 0)
 		{
 			echo $contents;
 
