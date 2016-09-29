701fad75969479edf6b9e69e5772357a3961527f
Frank Mayer <frank@frankmayer.net>
2016-09-29 15:51:06 +0300

    Use Elvis instead of full ternary

--- modules/mod_finder/helper.php
+++ modules/mod_finder/helper.php
@@ -49,7 +49,7 @@ class ModFinderHelper
 		// Add a field for Itemid if we need one.
 		if ($needId)
 		{
-			$id       = $paramItem ? $paramItem : JFactory::getApplication()->input->get('Itemid', '0', 'int');
+			$id       = $paramItem ?: JFactory::getApplication()->input->get('Itemid', '0', 'int');
 			$fields[] = '<input type="hidden" name="Itemid" value="' . $id . '" />';
 		}
 
--- modules/mod_menu/helper.php
+++ modules/mod_menu/helper.php
@@ -134,9 +134,9 @@ class ModMenuHelper
 
 				if (isset($items[$lastitem]))
 				{
-					$items[$lastitem]->deeper     = (($start?$start:1) > $items[$lastitem]->level);
-					$items[$lastitem]->shallower  = (($start?$start:1) < $items[$lastitem]->level);
-					$items[$lastitem]->level_diff = ($items[$lastitem]->level - ($start?$start:1));
+					$items[$lastitem]->deeper     = (($start?:1) > $items[$lastitem]->level);
+					$items[$lastitem]->shallower  = (($start?:1) < $items[$lastitem]->level);
+					$items[$lastitem]->level_diff = ($items[$lastitem]->level - ($start?:1));
 				}
 			}
 
@@ -189,7 +189,7 @@ class ModMenuHelper
 	{
 		$menu = JFactory::getApplication()->getMenu();
 
-		return $menu->getActive() ? $menu->getActive() : self::getDefault();
+		return $menu->getActive() ?: self::getDefault();
 	}
 
 	/**
