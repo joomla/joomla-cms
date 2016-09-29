11597d78bd40a0792fca9addea9850710d540a30
Frank Mayer <frank@frankmayer.net>
2016-09-29 15:53:47 +0300

    Removed unnecessary parentheses

--- modules/mod_articles_category/tmpl/default.php
+++ modules/mod_articles_category/tmpl/default.php
@@ -63,13 +63,13 @@ defined('_JEXEC') or die;
 										<?php echo $readmore; ?>
 										<?php echo JHtml::_('string.truncate', $item->title, $params->get('readmore_limit')); ?>
 											<?php if ($params->get('show_readmore_title', 0) != 0) : ?>
-												<?php echo JHtml::_('string.truncate', ($this->item->title), $params->get('readmore_limit')); ?>
+												<?php echo JHtml::_('string.truncate', $this->item->title, $params->get('readmore_limit')); ?>
 											<?php endif; ?>
 									<?php elseif ($params->get('show_readmore_title', 0) == 0) : ?>
 										<?php echo JText::sprintf('MOD_ARTICLES_CATEGORY_READ_MORE_TITLE'); ?>
 									<?php else : ?>
 										<?php echo JText::_('MOD_ARTICLES_CATEGORY_READ_MORE'); ?>
-										<?php echo JHtml::_('string.truncate', ($item->title), $params->get('readmore_limit')); ?>
+										<?php echo JHtml::_('string.truncate', $item->title, $params->get('readmore_limit')); ?>
 									<?php endif; ?>
 								</a>
 							</p>
