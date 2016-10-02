0622cb79213af5b7e7edeba5c0887ff088579147
zero-24 <zero-24@users.noreply.github.com>
2016-10-03 00:33:44 +0200

    Update contact.php

--- plugins/editors-xtd/contact/contact.php
+++ plugins/editors-xtd/contact/contact.php
@@ -29,7 +29,7 @@ class PlgButtonContact extends JPlugin
 	 *
 	 * @param   string  $name  The name of the button to add
 	 *
-	 * @return  stdClass  The button options as stdClass
+	 * @return  JObject  The button options as JObject
 	 *
 	 * @since   __DEPLOY_VERSION__
 	 */
@@ -62,7 +62,7 @@ class PlgButtonContact extends JPlugin
 		 */
 		$link = 'index.php?option=com_contact&amp;view=contacts&amp;layout=modal&amp;tmpl=component&amp;' . JSession::getFormToken() . '=1';
 
-		$button = new stdClass;
+		$button = new JObject;
 		$button->modal   = true;
 		$button->class   = 'btn';
 		$button->link    = $link;
