848e7d867b4be07ec4ae23ffe777e62db007115f
Frank Mayer <frank@frankmayer.net>
2016-10-02 04:19:32 +0300

    Some Elvis's and a missing @since annotation

--- administrator/components/com_finder/helpers/indexer/driver/postgresql.php
+++ administrator/components/com_finder/helpers/indexer/driver/postgresql.php
@@ -132,8 +132,8 @@ class FinderIndexerDriverPostgresql extends FinderIndexer
 				. $db->quote($item->publish_end_date) . ', '
 				. $db->quote($item->start_date) . ', '
 				. $db->quote($item->end_date) . ', '
-				. (double) ($item->list_price ? $item->list_price : 0) . ', '
-				. (double) ($item->sale_price ? $item->sale_price : 0)
+				. (double) ($item->list_price ?: 0) . ', '
+				. (double) ($item->sale_price ?: 0)
 			);
 			$db->setQuery($query);
 			$db->execute();
@@ -159,8 +159,8 @@ class FinderIndexerDriverPostgresql extends FinderIndexer
 				->set($db->quoteName('publish_end_date') . ' = ' . $db->quote($item->publish_end_date))
 				->set($db->quoteName('start_date') . ' = ' . $db->quote($item->start_date))
 				->set($db->quoteName('end_date') . ' = ' . $db->quote($item->end_date))
-				->set($db->quoteName('list_price') . ' = ' . (double) ($item->list_price ? $item->list_price : 0))
-				->set($db->quoteName('sale_price') . ' = ' . (double) ($item->sale_price ? $item->sale_price : 0))
+				->set($db->quoteName('list_price') . ' = ' . (double) ($item->list_price ?: 0))
+				->set($db->quoteName('sale_price') . ' = ' . (double) ($item->sale_price ?: 0))
 				->where('link_id = ' . (int) $linkId);
 			$db->setQuery($query);
 			$db->execute();
