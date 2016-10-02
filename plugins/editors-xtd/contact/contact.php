26cfbb4e34ab13dc3ef0db74394afa1a9809e848
Brian Teeman <brian@teeman.net>
2016-10-02 12:46:09 +0100

    update sql

--- plugins/editors-xtd/contact/contact.php
+++ plugins/editors-xtd/contact/contact.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package     Joomla.Plugin
- * @subpackage  Editors-xtd.article
+ * @subpackage  Editors-xtd.contact
  *
  * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
