13f5fee517d8d91c5908d98119db3fd479d9bdef
Frank Mayer <frank@frankmayer.net>
2016-10-04 00:28:06 +0300

    A bit more...

--- components/com_content/views/category/tmpl/default_children.php
+++ components/com_content/views/category/tmpl/default_children.php
@@ -23,20 +23,24 @@ $lang  = JFactory::getLanguage();
 				$class = ' class="last"';
 			endif;
 		?>
-
+			<?php
+			$childNumItems = $child->getNumItems(true);
+			$childChildren = $child->getChildren();
+			$childChildrenCount = count($childChildren);
+			?>
 		<div<?php echo $class; ?>>
 			<?php $class = ''; ?>
 			<?php if ($lang->isRtl()) : ?>
 			<h3 class="page-header item-title">
 				<?php if ( $this->params->get('show_cat_num_articles', 1)) : ?>
 					<span class="badge badge-info tip hasTooltip" title="<?php echo JHtml::tooltipText('COM_CONTENT_NUM_ITEMS_TIP'); ?>">
-						<?php echo $child->getNumItems(true); ?>
+						<?php echo $childNumItems; ?>
 					</span>
 				<?php endif; ?>
 				<a href="<?php echo JRoute::_(ContentHelperRoute::getCategoryRoute($child->id));?>">
 				<?php echo $this->escape($child->title); ?></a>
 
-				<?php if ($this->maxLevel > 1 && count($child->getChildren()) > 0) : ?>
+				<?php if ($this->maxLevel > 1 && $childChildrenCount > 0) : ?>
 					<a href="#category-<?php echo $child->id;?>" data-toggle="collapse" data-toggle="button" class="btn btn-mini pull-right"><span class="icon-plus"></span></a>
 				<?php endif;?>
 			</h3>
@@ -45,11 +49,11 @@ $lang  = JFactory::getLanguage();
 				<?php echo $this->escape($child->title); ?></a>
 				<?php if ( $this->params->get('show_cat_num_articles', 1)) : ?>
 					<span class="badge badge-info tip hasTooltip" title="<?php echo JHtml::tooltipText('COM_CONTENT_NUM_ITEMS_TIP'); ?>">
-						<?php echo $child->getNumItems(true); ?>
+						<?php echo $childNumItems; ?>
 					</span>
 				<?php endif; ?>
 
-				<?php if ($this->maxLevel > 1 && count($child->getChildren()) > 0) : ?>
+				<?php if ($this->maxLevel > 1 && $childChildrenCount > 0) : ?>
 					<a href="#category-<?php echo $child->id;?>" data-toggle="collapse" data-toggle="button" class="btn btn-mini pull-right"><span class="icon-plus"></span></a>
 				<?php endif;?>
 			<?php endif;?>
@@ -62,10 +66,10 @@ $lang  = JFactory::getLanguage();
 				<?php endif; ?>
 			<?php endif; ?>
 
-			<?php if ($this->maxLevel > 1 && count($child->getChildren()) > 0) :?>
+			<?php if ($this->maxLevel > 1 && $childChildrenCount > 0) :?>
 			<div class="collapse fade" id="category-<?php echo $child->id;?>">
 				<?php
-				$this->children[$child->id] = $child->getChildren();
+				$this->children[$child->id] = $childChildren;
 				$this->category = $child;
 				$this->maxLevel--;
 				echo $this->loadTemplate('children');
