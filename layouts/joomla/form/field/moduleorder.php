0e73761c1b3f72c53d25058467fdb31215d84356
C-Lodder <lodder15@hotmail.co.uk>
2016-10-04 17:17:59 +0100

    Revert back to behavior.core in layout

--- layouts/joomla/form/field/moduleorder.php
+++ layouts/joomla/form/field/moduleorder.php
@@ -56,7 +56,7 @@ $attr .= !empty($size) ? ' size="' . $size . '"' : '';
 $attr .= !empty($onchange) ? ' onchange="' . $onchange . '"' : '';
 
 // Including fallback code for HTML5 non supported browsers.
-JHtml::_('script', 'system/core.js', false, true);
+JHtml::_('behavior.core');
 JHtml::_('jquery.framework');
 JHtml::_('formbehavior.chosen', 'select', null, array('disable_search_threshold' => 0));
 JHtml::_('script', 'system/moduleorder.js', false, true);
