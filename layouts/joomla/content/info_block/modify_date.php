0ee5b6bce3b34e3b173d4e48931f11f9628aa211
C-Lodder <lodder15@hotmail.co.uk>
2016-10-05 14:58:14 +0100

    remove whitespace

--- layouts/joomla/content/info_block/modify_date.php
+++ layouts/joomla/content/info_block/modify_date.php
@@ -8,11 +8,10 @@
  */
 
 defined('JPATH_BASE') or die;
-
 ?>
-			<dd class="modified">
-				<span class="icon-calendar"></span>
-				<time datetime="<?php echo JHtml::_('date', $displayData['item']->modified, 'c'); ?>" itemprop="dateModified">
-					<?php echo JText::sprintf('COM_CONTENT_LAST_UPDATED', JHtml::_('date', $displayData['item']->modified, JText::_('DATE_FORMAT_LC3'))); ?>
-				</time>
-			</dd>
+<dd class="modified">
+	<span class="icon-calendar"></span>
+	<time datetime="<?php echo JHtml::_('date', $displayData['item']->modified, 'c'); ?>" itemprop="dateModified">
+		<?php echo JText::sprintf('COM_CONTENT_LAST_UPDATED', JHtml::_('date', $displayData['item']->modified, JText::_('DATE_FORMAT_LC3'))); ?>
+	</time>
+</dd>
--- layouts/joomla/content/info_block/publish_date.php
+++ layouts/joomla/content/info_block/publish_date.php
@@ -9,9 +9,9 @@
 
 defined('JPATH_BASE') or die;
 ?>
-			<dd class="published">
-				<span class="icon-calendar"></span>
-				<time datetime="<?php echo JHtml::_('date', $displayData['item']->publish_up, 'c'); ?>" itemprop="datePublished">
-					<?php echo JText::sprintf('COM_CONTENT_PUBLISHED_DATE_ON', JHtml::_('date', $displayData['item']->publish_up, JText::_('DATE_FORMAT_LC3'))); ?>
-				</time>
-			</dd>
+<dd class="published">
+	<span class="icon-calendar"></span>
+	<time datetime="<?php echo JHtml::_('date', $displayData['item']->publish_up, 'c'); ?>" itemprop="datePublished">
+		<?php echo JText::sprintf('COM_CONTENT_PUBLISHED_DATE_ON', JHtml::_('date', $displayData['item']->publish_up, JText::_('DATE_FORMAT_LC3'))); ?>
+	</time>
+</dd>
