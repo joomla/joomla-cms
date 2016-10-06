6a2ea6c9f976850acbb3a75c0936dc7567ecf390
andrepereiradasilva <andrepereiradasilva@users.noreply.github.com>
2016-10-06 01:58:34 +0100

    let's make the layout more dynamic

--- layouts/joomla/edit/associations.php
+++ layouts/joomla/edit/associations.php
@@ -9,13 +9,17 @@
 
 defined('JPATH_BASE') or die;
 
+$form     = $displayData->getForm();
+$disabled = (int) ((int) $form->getValue('id') === 0 || JFactory::getApplication()->input->get('layout') === 'modal');
+$options  = array(
+	'formControl' => $form->getFormControl(),
+	'disabled'    => $disabled,
+	'hidden'      => (int) ($disabled || $form->getValue('language') === '*'),
+);
+
+JHtml::_('behavior.core');
+JFactory::getDocument()->addScriptOptions('system.associations.edit', $options);
+JHtml::script('system/associations-edit.js', false, true);
+
 // JLayout for standard handling of associations fields in the administrator items edit screens.
-if ($displayData->getForm()->getValue('id') != 0 && $displayData->getForm()->getValue('language') != "*")
-{
-	echo $displayData->getForm()->renderFieldset('item_associations');
-}
-else
-{
-	echo '<div class="alert alert-info">' . JText::_('JGLOBAL_ASSOC_NOT_POSSIBLE') . '</div>';
-	echo '<div class="hidden">' . $displayData->getForm()->renderFieldset('item_associations') . '</div>';
-}
+echo $form->renderFieldset('item_associations');
