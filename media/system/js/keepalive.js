27e0ab6bc0f017aca1508e1c6e3cff9e8ac9109b
C-Lodder <lodder15@hotmail.co.uk>
2016-10-06 14:39:04 +0100

    more merge fixes

--- media/system/js/keepalive.js
+++ media/system/js/keepalive.js
@@ -1,1 +1,39 @@
-!function(){"use strict";document.addEventListener("DOMContentLoaded",function(){var e=Joomla.getOptions("system.keepalive"),o=e.uri?e.uri.replace(/&amp;/g,"&"):window.location.pathname,n=e.interval?e.interval:45e3;window.setInterval(function(){Joomla.request({url:o,onSuccess:function(){},onError:function(e){Joomla.renderMessages(Joomla.ajaxErrorsMessages(e))}})},n)})}(window,document,Joomla);
+/**
+ * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+/**
+ * Keepalive javascript behavior
+ *
+ * Used for keeping the session alive
+ *
+ * @package  Joomla
+ * @since    __DEPLOY_VERSION__
+ */
+!(function(){
+	'use strict';
+
+	document.addEventListener('DOMContentLoaded', function() {
+
+		var keepaliveOptions  = Joomla.getOptions('system.keepalive'),
+		    keepaliveUri      = keepaliveOptions.uri ? keepaliveOptions.uri.replace(/&amp;/g, '&') : window.location.pathname,
+		    keepaliveInterval = keepaliveOptions.interval ? keepaliveOptions.interval : 45 * 1000;
+
+		window.setInterval(function() {
+			Joomla.request({
+				url:    keepaliveUri,
+				onSuccess: function(response, xhr)
+				{
+					// Do nothing
+				},
+				onError: function(xhr)
+				{
+					Joomla.renderMessages(Joomla.ajaxErrorsMessages(xhr));
+				}
+			});
+		}, keepaliveInterval);
+
+	});
+
+})(window, document, Joomla);
