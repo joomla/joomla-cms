9638ac99a3f6c34323a33ab03917c385e2704f04
Hannes Papenberg <info@joomlager.de>
2016-10-06 22:06:32 +0200

    Don't force SHA256 passwords to be reset

--- administrator/components/com_admin/sql/updates/mysql/4.0.0-2016-10-02.sql
+++ administrator/components/com_admin/sql/updates/mysql/4.0.0-2016-10-02.sql
@@ -1,4 +1,3 @@
 DELETE FROM `#__extensions` WHERE `extension_id` = 504;
 DELETE FROM `#__template_styles` WHERE `template` = 'hathor';
 ALTER TABLE `#__user_keys` DROP COLUMN `invalid`;
-UPDATE `#__users` SET `requireReset` = 1 WHERE `password` LIKE '{SHA256}%';
