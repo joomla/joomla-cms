9479024ce9c085bd0d03712361b20d2e6e8848fd
zero-24 <zero-24@users.noreply.github.com>
2016-10-07 16:53:47 +0200

    Update update.php

--- libraries/joomla/updater/update.php
+++ libraries/joomla/updater/update.php
@@ -432,13 +432,13 @@ class JUpdate extends JObject
 
 		$allowedResponseCodes = array(
 			// HTTP 200 Ok
-			'200',
+			200,
 			// HTTP 301 Moved Permanently
-			'301',
+			301,
 			// HTTP 302 Found
-			'302',
+			302,
 			// HTTP 303 See Other
-			'303',
+			303,
 		);
 
 		if ($response === null || !in_array($response->code, $allowedResponseCodes)
