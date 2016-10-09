a3d6254045c860584abf3f9950e28c817240049e
Brian Teeman <brian@teeman.net>
2016-10-09 13:29:35 +0100

    Installed Language flag layout

--- administrator/components/com_languages/views/languages/tmpl/default.php
+++ administrator/components/com_languages/views/languages/tmpl/default.php
@@ -139,7 +139,7 @@ if ($saveOrder)
 							<?php echo $this->escape($item->sef); ?>
 						</td>
 						<td class="hidden-phone">
-							<?php echo JHtml::_('image', 'mod_languages/' . $item->image . '.gif', $item->image, array('title' => $item->image), true); ?>&nbsp;<?php echo $this->escape($item->image); ?>
+							<?php echo JHtml::_('image', 'mod_languages/' . $item->image . '.gif', $item->image, array('title' => $item->image), true); ?><?php echo $this->escape($item->image); ?>
 						</td>
 						<td class="hidden-phone">
 							<?php echo $this->escape($item->access_level); ?>
