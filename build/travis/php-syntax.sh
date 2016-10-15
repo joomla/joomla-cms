8469b8a05ea349703044e15cb37907cb8c0c3c3a
zero-24 <zero-24@users.noreply.github.com>
2016-10-15 15:13:48 +0200

    fix syntax error & add more excluded folders

--- build/travis/php-syntax.sh
+++ build/travis/php-syntax.sh
@@ -5,4 +5,5 @@ RUN_COMMAND="php -d display_errors=stderr -n -l"
 
 if [[ $TRAVIS_PHP_VERSION = hhvm ]]; then RUN_COMMAND="hhvm -d display_errors=stderr -l"; fi
 
-find . \( -path ./libraries/vendor -o -path ./libraries/phpass -o -path ./libraries/fof -o -path ./tests -o -wholename './administrator/components/com_joomlaupdate/restore.php' \) -prune -o -type f -name "*.php" -print | xargs -n1 -P8 $RUN_COMMAND 1>/dev/null
+# We exclude any 3rd Party folders / files here
+find . \( -path ./libraries/vendor -o -path ./libraries/fof -o -path ./libraries/idna_convert -o -path ./libraries/php-encryption -o -path ./libraries/phputf8 -o -path ./libraries/phpass -o -path ./tests -o -wholename './administrator/components/com_joomlaupdate/restore.php' \) -prune -o -type f -name "*.php" -print | xargs -n1 -P8 $RUN_COMMAND 1>/dev/null
