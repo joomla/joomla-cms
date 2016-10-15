e5ae973d94fe91d839c9d390a5b056d6998647d3
zero-24 <zero-24@users.noreply.github.com>
2016-10-15 15:43:20 +0200

    some more cs love

--- administrator/components/com_users/views/users/tmpl/default.php
+++ administrator/components/com_users/views/users/tmpl/default.php
@@ -26,11 +26,8 @@ $loggeduser = JFactory::getUser();
 		<div id="j-main-container" class="span10">
 	<?php else : ?>
 		<div id="j-main-container">
-	<?php endif;?>
-		<?php
-		// Search tools bar
-		echo JLayoutHelper::render('joomla.searchtools.default', array('view' => $this));
-		?>
+	<?php endif; ?>
+		<?php echo JLayoutHelper::render('joomla.searchtools.default', array('view' => $this)); ?>
 		<?php if (empty($this->items)) : ?>
 			<div class="alert alert-no-items">
 				<?php echo JText::_('JGLOBAL_NO_MATCHING_RESULTS'); ?>
