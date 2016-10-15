e19604769f47c13672b42a5a25a5da88ede8b874
Nicola Galgano <optimus4joomla@gmail.com>
2016-10-15 08:07:31 +0200

    optimize missed tables
    #__finder_filters
    #__finder_taxonomy
    #__finder_terms_common
    #__finder_types

--- administrator/components/com_finder/helpers/indexer/driver/mysql.php
+++ administrator/components/com_finder/helpers/indexer/driver/mysql.php
@@ -538,11 +538,27 @@ class FinderIndexerDriverMysql extends FinderIndexer
 			$db->execute();
 		}
 
+		// Optimize the filters table.
+		$db->setQuery('OPTIMIZE TABLE ' . $db->quoteName('#__finder_filters'));
+		$db->execute();
+
+		// Optimize the terms common table.
+		$db->setQuery('OPTIMIZE TABLE ' . $db->quoteName('#__finder_terms_common'));
+		$db->execute();
+
+		// Optimize the types table.
+		$db->setQuery('OPTIMIZE TABLE ' . $db->quoteName('#__finder_types'));
+		$db->execute();
+
 		// Remove the orphaned taxonomy nodes.
 		FinderIndexerTaxonomy::removeOrphanNodes();
 
 		// Optimize the taxonomy mapping table.
 		$db->setQuery('OPTIMIZE TABLE ' . $db->quoteName('#__finder_taxonomy_map'));
+		$db->execute();
+
+		// Optimize the taxonomy table.
+		$db->setQuery('OPTIMIZE TABLE ' . $db->quoteName('#__finder_taxonomy'));
 		$db->execute();
 
 		return true;
