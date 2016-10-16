2f14141d8e6fff4320524c6dcb21ffc112e25083
andrepereiradasilva <andrepereiradasilva@users.noreply.github.com>
2016-10-16 15:15:51 +0100

    remove setHtml5 as this is now the default behaviour

--- administrator/templates/isis/error.php
+++ administrator/templates/isis/error.php
@@ -22,12 +22,6 @@ $this->direction = $doc->direction;
 $input           = $app->input;
 $user            = JFactory::getUser();
 
-// Output document as HTML5.
-if (is_callable(array($doc, 'setHtml5')))
-{
-	$doc->setHtml5(true);
-}
-
 // Gets the FrontEnd Main page Uri
 $frontEndUri = JUri::getInstance(JUri::root());
 $frontEndUri->setScheme(((int) $app->get('force_ssl', 0) === 2) ? 'https' : 'http');
