d4c0ee27c198debaf2365fd5bbea7ca9a74392bb
andrepereiradasilva <andrepereiradasilva@users.noreply.github.com>
2016-10-16 15:14:05 +0100

    remove setHtml5 as this is now the default behaviour

--- templates/protostar/component.php
+++ templates/protostar/component.php
@@ -14,9 +14,6 @@ $doc             = JFactory::getDocument();
 $this->language  = $doc->language;
 $this->direction = $doc->direction;
 
-// Output as HTML5
-$doc->setHtml5(true);
-
 // Add JavaScript Frameworks
 JHtml::_('bootstrap.framework');
 
