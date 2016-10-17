748d4c1df98d1fddb9b805e44c636700d2c17ace
andrepereiradasilva <andrepereiradasilva@users.noreply.github.com>
2016-10-18 00:14:42 +0100

    ok let's remove all that inline js ...

--- installation/template/index.php
+++ installation/template/index.php
@@ -22,6 +22,8 @@ JHtml::_('bootstrap.framework');
 JHtml::_('formbehavior.chosen', 'select');
 JHtml::_('behavior.keepalive');
 JHtml::_('behavior.formvalidator');
+JHtml::_('behavior.core');
+JHtml::_('behavior.polyfill', array('event'), 'lt IE 9');
 JHtml::_('script', 'installation/template/js/installation.js');
 
 // Load JavaScript message titles
@@ -41,122 +43,8 @@ JText::script('JLIB_JS_AJAX_ERROR_TIMEOUT');
 JText::script('INSTL_PROCESS_BUSY');
 JText::script('INSTL_FTP_SETTINGS_CORRECT');
 
-// Add inline scripts
-$this->addScriptDeclaration("
-	jQuery(function()
-	{
-		// Delay instantiation after document.formvalidation and other dependencies loaded
-		window.setTimeout(function(){
-			window.Install = new Installation('container-installation', '" . JUri::current() . "');
-		}, 500);
-	});
-
-	function initElements()
-	{
-		(function($){
-			$('.hasTooltip').tooltip()
-
-			// Chosen select boxes
-			$('select').chosen({
-				disable_search_threshold : 10,
-				allow_single_deselect : true
-			});
-
-			// Turn radios into btn-group
-			$('.radio.btn-group label').addClass('btn');
-
-			$('fieldset.btn-group').each(function() {
-				var \$self = $(this);
-				// Handle disabled, prevent clicks on the container, and add disabled style to each button
-				if (\$self.prop('disabled'))
-				{
-					\$self.css('pointer-events', 'none').off('click');
-					\$self.find('.btn').addClass('disabled');
-				}
-			});
-
-			$('.btn-group label:not(.active)').click(function()
-			{
-				var label = $(this);
-				var input = $('#' + label.attr('for'));
-
-				if (!input.prop('checked'))
-				{
-					label.closest('.btn-group').find('label').removeClass('active btn-success btn-danger btn-primary');
-
-					if (label.closest('.btn-group').hasClass('btn-group-reverse'))
-					{
-						if (input.val() == '')
-						{
-							label.addClass('active btn-primary');
-						}
-						else if (input.val() == 0)
-						{
-							label.addClass('active btn-danger');
-						}
-						else
-						{
-							label.addClass('active btn-success');
-						}
-					}
-					else
-					{
-						if (input.val() == '')
-						{
-							label.addClass('active btn-primary');
-						}
-						else if (input.val() == 0)
-						{
-							label.addClass('active btn-success');
-						}
-						else
-						{
-							label.addClass('active btn-danger');
-						}
-					}
-					input.prop('checked', true);
-				}
-			});
-			$('.btn-group input[checked=\'checked\']').each(function()
-			{
-				var \$self  = $(this);
-				var attrId = \$self.attr('id');
-
-				if (\$self.hasClass('btn-group-reverse'))
-				{
-					if (\$self.val() == '')
-					{
-						$('label[for=\'' + attrId + '\']').addClass('active btn-primary');
-					}
-					else if (\$self.val() == 0)
-					{
-						$('label[for=\'' + attrId + '\']').addClass('active btn-danger');
-					}
-					else
-					{
-						$('label[for=\'' + attrId + '\']').addClass('active btn-success');
-					}
-				}
-				else
-				{
-					if (\$self.val() == '')
-					{
-						$('label[for=\'' + attrId + '\']').addClass('active btn-primary');
-					}
-					else if (\$self.val() == 0)
-					{
-						$('label[for=\'' + attrId + '\']').addClass('active btn-success');
-					}
-					else
-					{
-						$('label[for=\'' + attrId + '\']').addClass('active btn-danger');
-					}
-				}
-			});
-		})(jQuery);
-	}
-	initElements();
-");
+// Add script options
+$this->addScriptOptions('system.installation', array('url' => JRoute::_('index.php')));
 ?>
 <!DOCTYPE html>
 <html lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>">
