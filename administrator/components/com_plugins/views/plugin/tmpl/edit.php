fa2b622524229800d7b9d18d7f91798d8b819a8e
infograf768 <infografjms@gmail.com>
2016-10-18 19:36:19 +0200

    Solving 12214 Plugin tooltip (#12215)

--- administrator/components/com_plugins/views/plugin/tmpl/edit.php
+++ administrator/components/com_plugins/views/plugin/tmpl/edit.php
@@ -13,6 +13,7 @@ JHtml::addIncludePath(JPATH_COMPONENT . '/helpers/html');
 
 JHtml::_('behavior.formvalidator');
 JHtml::_('formbehavior.chosen', 'select');
+JHtml::_('bootstrap.tooltip');
 $this->fieldsets = $this->form->getFieldsets('params');
 
 JFactory::getDocument()->addScriptDeclaration("
