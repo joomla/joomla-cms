38e51c003ca7aad17032bb01809c0a298c226ddd
Walt Sorensen <photodude@users.noreply.github.com>
2016-10-19 16:06:11 -0600

    Revert _load to private

--- libraries/loader.php
+++ libraries/loader.php
@@ -609,7 +609,7 @@ abstract class JLoader
 	 *
 	 * @since   12.1
 	 */
-	public static function _load($class, $lookup)
+	private static function _load($class, $lookup)
 	{
 		// Split the class name into parts separated by camelCase.
 		$parts = preg_split('/(?<=[a-z0-9])(?=[A-Z])/x', $class);
