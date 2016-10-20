b1b7f4ba90165554ac826e04072d400687df3412
andrepereiradasilva <andrepereiradasilva@users.noreply.github.com>
2016-10-20 14:24:29 +0100

    use mysql 5.7.8+ default sql mode

--- libraries/joomla/database/driver/mysqli.php
+++ libraries/joomla/database/driver/mysqli.php
@@ -183,8 +183,8 @@ class JDatabaseDriverMysqli extends JDatabaseDriver
 			throw new JDatabaseExceptionConnecting('Could not connect to MySQL.');
 		}
 
-		// Set sql_mode to non_strict mode
-		mysqli_query($this->connection, "SET @@SESSION.sql_mode = '';");
+		// Set sql_mode to MySql 5.7.8+ default strict mode.
+		mysqli_query($this->connection, "SET @@SESSION.sql_mode = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION';");
 
 		// If auto-select is enabled select the given database.
 		if ($this->options['select'] && !empty($this->options['database']))
