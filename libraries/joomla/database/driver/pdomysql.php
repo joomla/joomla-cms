552cf8a243f84f96bd68537456efa4358c04217b
andrepereiradasilva <andrepereiradasilva@users.noreply.github.com>
2016-10-20 15:56:24 +0100

    Update pdomysql.php

--- libraries/joomla/database/driver/pdomysql.php
+++ libraries/joomla/database/driver/pdomysql.php
@@ -152,7 +152,17 @@ class JDatabaseDriverPdomysql extends JDatabaseDriverPdo
 		$this->connection->setAttribute(PDO::ATTR_EMULATE_PREPARES, true);
 
 		// Set sql_mode to MySql 5.7.8+ default strict mode.
-		$this->connection->query("SET @@SESSION.sql_mode = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION';");
+		$sqlModes = array(
+			'ONLY_FULL_GROUP_BY',
+			'STRICT_TRANS_TABLES',
+			'NO_ZERO_IN_DATE',
+			'NO_ZERO_DATE',
+			'ERROR_FOR_DIVISION_BY_ZERO',
+			'NO_AUTO_CREATE_USER',
+			'NO_ENGINE_SUBSTITUTION',
+		);
+
+		$this->connection->query("SET @@SESSION.sql_mode = '" . implode(',', $sqlModes) . "';");
 	}
 
 	/**
