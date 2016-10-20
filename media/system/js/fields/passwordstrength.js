84e71bb10ab3be7fa1fc0e42800751331645c83f
Dimitris <d.grammatiko@gmail.com>
2016-10-20 21:04:05 +0300

    fixes

--- media/system/js/fields/passwordstrength.js
+++ media/system/js/fields/passwordstrength.js
@@ -38,89 +38,15 @@ PasswordStrength = (function() {
 })();
 
 document.addEventListener('DOMContentLoaded', function(){
-	var fields = document.querySelectorAll('.js-password-strength');
 
-	for(var i = 0, l = fields.length; i<l; i++) {
-
-		var startClass = '',
-			initialVal = '';
-
-		if (!fields[i].value.length) {
-			startClass =' progress-danger';
-			initialVal = 100;
-		}
-
-		// Create a progress meter and the label
-		var meter = document.createElement('progress');
-		meter.setAttribute('class', 'progress' + startClass);
-		meter.value = 0 + initialVal;
-		meter.max = 100;
-		meter.style.height = '2em';
-		meter.setAttribute('aria-describedby', 'password-' + i);
-
-		var label = document.createElement('div');
-		label.setAttribute('class', 'text-xs-center');
-		label.setAttribute('id', 'password-' + i);
-
-		fields[i].parentNode.append(label);
-		fields[i].parentNode.append(meter);
-
-		// Add a listener for keyup
-		fields[i].addEventListener('keyup', function(event) {
-			var $minLength = event.target.getAttribute('data-min-length'),
-				$minIntegers = event.target.getAttribute('data-min-integers'),
-				$minSymbols = event.target.getAttribute('data-min-symbols'),
-				$minUppercase = event.target.getAttribute('data-min-uppercase'),
-				$minLowercase = event.target.getAttribute('data-min-lowercase');
-
-			var strength = new PasswordStrength({
-				lowercase: $minLowercase ? $minLowercase : 0,
-				uppercase: $minUppercase ? $minUppercase : 0,
-				numbers: $minIntegers ? $minIntegers : 0,
-				special: $minSymbols ? $minSymbols : 0,
-				length: $minLength ? $minLength : 4
-			});
-
-			var score = strength.getScore(event.target.value),
-				meter = event.target.parentNode.querySelector('progress'),
-				i = meter.getAttribute('aria-describedby').replace( /^\D+/g, ''),
-				label = event.target.parentNode.querySelector('#password-' + i);
-
-			if (score > 79){
-				meter.setAttribute('class', 'progress progress-success');
-				label.innerHTML = Joomla.JText._('JFIELD_PASSWORD_INDICATE_VSTRONG');
-			}
-			if (score > 64 && score < 80){
-				meter.setAttribute('class', 'progress progress-success');
-				label.innerHTML = Joomla.JText._('JFIELD_PASSWORD_INDICATE_STRONG');
-			}
-			if (score > 50 && score < 65){
-				meter.setAttribute('class', 'progress progress-info');
-				label.innerHTML = Joomla.JText._('JFIELD_PASSWORD_INDICATE_NORMAL');
-			}
-			if (score > 40 && score < 51){
-				meter.setAttribute('class', 'progress progress-warning');
-				label.innerHTML = Joomla.JText._('JFIELD_PASSWORD_INDICATE_MEDIUM');
-			}
-			if (score < 41){
-				meter.setAttribute('class', 'progress progress-danger');
-				label.innerHTML = Joomla.JText._('JFIELD_PASSWORD_INDICATE_WEAK');
-			}
-
-			meter.value = score;
-		})
-	}
-
-	if (fields[0].getAttribute())
-	document.formvalidator.setHandler('passwordExtra', function(value) {
-console.log('called')
-		var returnedValue = false,
-			fields = document.querySelectorAll('.js-password-strength'),
-			$minLength = fields[0].getAttribute('data-min-length'),
-			$minIntegers = fields[0].getAttribute('data-min-integers'),
-			$minSymbols = fields[0].getAttribute('data-min-symbols'),
-			$minUppercase = fields[0].getAttribute('data-min-uppercase'),
-			$minLowercase = fields[0].getAttribute('data-min-lowercase');
+	/** Method to check the input and set the meter **/
+	var getMeter = function(element) {
+		var $minLength = element.getAttribute('data-min-length'),
+			$minIntegers = element.getAttribute('data-min-integers'),
+			$minSymbols = element.getAttribute('data-min-symbols'),
+			$minUppercase = element.getAttribute('data-min-uppercase'),
+			$minLowercase = element.getAttribute('data-min-lowercase'),
+			meter = element.parentNode.querySelector('progress');
 
 		var strength = new PasswordStrength({
 			lowercase: $minLowercase ? $minLowercase : 0,
@@ -130,10 +56,103 @@ console.log('called')
 			length: $minLength ? $minLength : 4
 		});
 
-		var score = strength.getScore(value);
-console.log(score)
-		if (score > 60 ) returnedValue = true;
+		var score = strength.getScore(element.value),
+			i = meter.getAttribute('aria-describedby').replace( /^\D+/g, ''),
+			label = element.parentNode.querySelector('#password-' + i);
 
-		return returnedValue;
-	});
+		if (score > 79){
+			meter.setAttribute('class', 'progress progress-success');
+			label.innerHTML = Joomla.JText._('JFIELD_PASSWORD_INDICATE_VSTRONG');
+		}
+		if (score > 64 && score < 80){
+			meter.setAttribute('class', 'progress progress-success');
+			label.innerHTML = Joomla.JText._('JFIELD_PASSWORD_INDICATE_STRONG');
+		}
+		if (score > 50 && score < 65){
+			meter.setAttribute('class', 'progress progress-info');
+			label.innerHTML = Joomla.JText._('JFIELD_PASSWORD_INDICATE_NORMAL');
+		}
+		if (score > 40 && score < 51){
+			meter.setAttribute('class', 'progress progress-warning');
+			label.innerHTML = Joomla.JText._('JFIELD_PASSWORD_INDICATE_MEDIUM');
+		}
+		if (score < 41){
+			meter.setAttribute('class', 'progress progress-danger');
+			label.innerHTML = Joomla.JText._('JFIELD_PASSWORD_INDICATE_WEAK');
+		}
+
+		meter.value = score;
+
+		if (!element.value.length) {
+			meter.setAttribute('class', 'progress');
+			label.innerHTML = '';
+			element.setAttribute('required', '');
+		}
+	};
+
+	var fields = document.querySelectorAll('.js-password-strength');
+
+	/** Loop  through the fields **/
+	for(var i = 0, l = fields.length; i<l; i++) {
+
+		var startClass = '',
+			initialVal = '';
+
+		if (!fields[i].value.length) {
+			startClass =' progress-danger';
+			initialVal = 0;
+		}
+
+		/** Create a progress meter and the label **/
+		var meter = document.createElement('progress');
+		meter.setAttribute('class', 'progress' + startClass);
+		meter.value = 0 + initialVal;
+		meter.max = 100;
+		meter.setAttribute('aria-describedby', 'password-' + i);
+
+		var label = document.createElement('div');
+		label.setAttribute('class', 'text-xs-center');
+		label.setAttribute('id', 'password-' + i);
+
+		fields[i].parentNode.append(label);
+		fields[i].parentNode.append(meter);
+
+		/** Add a data attribute for the required **/
+		if (fields[i].value.length > 0) {
+			fields[i].setAttribute('required', true);
+		}
+
+		/** Add a listener for input data change **/
+		fields[i].addEventListener('keyup', function(event) {
+			getMeter(event.target);
+		})
+	}
+
+	/** Set a handler for the validation script **/
+	if (fields[0]) {
+		document.formvalidator.setHandler('passwordExtra', function(value) {
+
+			var returnedValue = false,
+				fields = document.querySelectorAll('.js-password-strength'),
+				$minLength = fields[0].getAttribute('data-min-length'),
+				$minIntegers = fields[0].getAttribute('data-min-integers'),
+				$minSymbols = fields[0].getAttribute('data-min-symbols'),
+				$minUppercase = fields[0].getAttribute('data-min-uppercase'),
+				$minLowercase = fields[0].getAttribute('data-min-lowercase');
+
+			var strength = new PasswordStrength({
+				lowercase: $minLowercase ? $minLowercase : 0,
+				uppercase: $minUppercase ? $minUppercase : 0,
+				numbers: $minIntegers ? $minIntegers : 0,
+				special: $minSymbols ? $minSymbols : 0,
+				length: $minLength ? $minLength : 4
+			});
+
+			var score = strength.getScore(value);
+console.log(score);
+			if (score === 100 ) returnedValue = true;
+
+			return returnedValue;
+		});
+	}
 });
