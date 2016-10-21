ecf5c084bef5b4a3043a513ec10ce54b0f172bfa
Dimitris Grammatikogiannis <d.grammatiko@gmail.com>
2016-10-21 21:30:56 +0300

    Remove chosen from vendor, we use a fork

--- dev/assets.php
+++ dev/assets.php
@@ -9,7 +9,6 @@ abstract class ExternalAssets{
 			'bootstrap' => array('version' => '~4.0.0-alpha.5','dependencies' => 'jquery, tether'),
 			'tether' => array('version' => '1.3.7','dependencies' => 'jquery, tether'),
 			'font-awesome' => array('version' => '4.6.3','dependencies' => ''),
-			'chosenjs' => array('version' => '1.4.3','dependencies' => 'jquery, tether'),
 			'jquery-minicolors' => array('version' => '2.1.10','dependencies' => 'jquery'),
 			'jquery-sortable' => array('version' => '0.9.13','dependencies' => 'jquery'),
 			'jquery-ui' => array('version' => '1.12.1','dependencies' => 'jquery'),
