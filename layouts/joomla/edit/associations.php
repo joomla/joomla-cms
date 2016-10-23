576c8287315e44aca6adabec11164a3eb5488ddd
andrepereiradasilva <andrepereiradasilva@users.noreply.github.com>
2016-10-23 21:03:34 +0100

    fix conflicts final

--- layouts/joomla/edit/associations.php
+++ layouts/joomla/edit/associations.php
@@ -22,12 +22,4 @@ JFactory::getDocument()->addScriptOptions('system.associations.edit', $options);
 JHtml::script('system/associations-edit.js', false, true);
 
 // JLayout for standard handling of associations fields in the administrator items edit screens.
-if ($displayData->getForm()->getValue('id') != 0 && $displayData->getForm()->getValue('language') !== '*')
-{
-	echo $displayData->getForm()->renderFieldset('item_associations');
-}
-else
-{
-	echo '<div class="alert alert-info">' . JText::_('JGLOBAL_ASSOC_NOT_POSSIBLE') . '</div>';
-	echo '<div class="hidden">' . $displayData->getForm()->renderFieldset('item_associations') . '</div>';
-}
+echo $form->renderFieldset('item_associations');
