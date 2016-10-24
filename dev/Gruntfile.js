7a0cfa7719fa26f278fa38234010d1e85026d43e
Dimitris Grammatikogiannis <d.grammatiko@gmail.com>
2016-10-24 09:34:18 +0300

    something went wrong

--- dev/Gruntfile.js
+++ dev/Gruntfile.js
@@ -61,17 +61,11 @@ module.exports = function(grunt) {
 	// Project configuration.
 	grunt.initConfig({
 		folder : {
-<<<<<<< Updated upstream
-			system   : '../media/system/js',
-			fields   : '../media/system/js/fields',
-			legacy   : '../media/system/js/legacy',
-			puny     : '../media/vendor/punycode/js',
-			cmadd    : '../media/vendor/codemirror/addon',
-			cmkey    : '../media/vendor/codemirror/keymap',
-			cmlib    : '../media/vendor/codemirror/lib',
-			cmmod    : '../media/vendor/codemirror/mode',
-			cmthem   : '../media/vendor/codemirror/theme',
-			polyfills : '../media/vendor/polyfills/js',
+			system     : '../media/system/js',
+			fields     : '../media/system/js/fields',
+			legacy     : '../media/system/js/legacy',
+			puny       : '../media/vendor/punycode/js',
+			codemirror : '../media/vendor/codemirror',
 		},
 
 		// Let's clean up the system
@@ -94,20 +88,7 @@ module.exports = function(grunt) {
 					'../media/vendor/mediaelement/*',
 					'../media/vendor/chosenjs/*',
 					'../media/vendor/awesomplete/*',
-<<<<<<< Updated upstream
-					'../media/vendor/polyfills/*',
-				],
-				expand: true,
-				options: {
-					force: true
-				},
-			},
-			tmp: {
-				src: [
-					'assets/',
-=======
 					'assets/node_modules/**'
->>>>>>> Stashed changes
 				],
 				expand: true,
 				options: {
@@ -142,23 +123,6 @@ module.exports = function(grunt) {
 				dest: 'assets/tmp/autoc.zip'
 			},
 		},
-<<<<<<< Updated upstream
-		fetchpages: {
-			polyfills: {
-				options: {
-					baseURL: '',
-					destinationFolder: '../media/vendor/polyfills/js/',
-					urls: polyFillsUrls,
-					cleanHTML: false,
-					fetchBaseURL: false,
-					followLinks: false
-				}
-			}
-		},
-=======
-
-		// Unzip all the packages that were fetched with curl
->>>>>>> Stashed changes
 		unzip: {
 			'cmUnzip': {
 				router: function (filepath) {
@@ -186,6 +150,20 @@ module.exports = function(grunt) {
 				},
 				src: 'assets/tmp/jcrop.zip',
 				dest: 'assets/tmp/jcrop/'
+			}
+		},
+
+		// Fetch the polyfills
+		fetchpages: {
+			polyfills: {
+				options: {
+					baseURL: '',
+					destinationFolder: '../media/vendor/polyfills/js/',
+					urls: polyFillsUrls,
+					cleanHTML: false,
+					fetchBaseURL: false,
+					followLinks: false
+				}
 			}
 		},
 
@@ -320,324 +298,7 @@ module.exports = function(grunt) {
 						dest: '',
 						expand: true,
 						ext: '.min.js'
-<<<<<<< Updated upstream
-					},
-					{
-						src: ['<%= folder.cmkey %>/*.js','!<%= folder.cmkey %>/*.min.js'],
-						dest: '',
-						expand: true,
-						ext: '.min.js'
-					},
-					{
-						src: ['<%= folder.cmlib %>/*.js','!<%= folder.cmlib %>/*.min.js'],
-						dest: '',
-						expand: true,
-						ext: '.min.js'
-					},
-					{
-						src: ['<%= folder.cmmod %>/*/*.js','!<%= folder.cmmod %>/*/*.min.js'],
-						dest: '',
-						expand: true,
-						ext: '.min.js'
-					},
-					{
-						src: ['<%= folder.cmthem %>/*/*.js','!<%= folder.cmthem %>/*/*.min.js'],
-						dest: '',
-						expand: true,
-						ext: '.min.js'
-					},
-					{
-						src: ['<%= folder.jupdate %>/*.js','!<%= folder.jupdate %>/*.min.js'],
-						dest: '',
-						expand: true,
-						ext: '.min.js'
-					},
-					{
-						src: ['<%= folder.extupdate %>/*.js','!<%= folder.extupdate %>/*.min.js'],
-						dest: '',
-						expand: true,
-						ext: '.min.js'
-					},
-					{
-						src: '<%= folder.polyfills %>/polyfill.classlist.js',
-						dest: '<%= folder.polyfills %>/polyfill.classlist.min.js',
-					},
-					{
-						src: '<%= folder.polyfills %>/polyfill.event.js',
-						dest: '<%= folder.polyfills %>/polyfill.event.min.js',
-					},
-					// Uglifying punicode.js fails!!!
-					// {
-					// 	src: ['<%= folder.puny %>/*.js','!<%= folder.puny %>/*.min.js'],
-					// 	dest: '',
-					// 	expand: true,
-					// 	ext: '.min.js'
-					// }
-				]
-			}
-		},
-
-		// Transfer all the assets to media/vendor
-		copy: {
-			fromSource: {
-				files: [
-					{ // jQuery files
-						expand: true,
-						cwd: 'assets/node_modules/jquery/dist/',
-						src: ['*', '!(core.js)'],
-						dest: '../media/vendor/jquery/js/',
-						filter: 'isFile'
-					},
-					{ // jQuery migrate files
-						expand: true,
-						cwd: 'assets/node_modules/jquery-migrate/dist/',
-						src: ['**'],
-						dest: '../media/vendor/jquery/js/',
-						filter: 'isFile'
-					},
-					{ //Bootastrap js files
-						expand: true,
-						cwd: 'assets/node_modules/bootstrap/dist/js/',
-						src: ['**'],
-						dest: '../media/vendor/bootstrap/js/',
-						filter: 'isFile'
-					},
-					{ //Bootastrap scss files
-						expand: true,
-						cwd: 'assets/node_modules/bootstrap/scss/',
-						src: ['**'],
-						dest: '../media/vendor/bootstrap/scss/',
-						filter: 'isFile'
-					},
-					{ //Bootastrap css files
-						expand: true,
-						cwd: 'assets/node_modules/bootstrap/dist/css/',
-						src: ['**'],
-						dest: '../media/vendor/bootstrap/css/',
-						filter: 'isFile'
-					},
-					{ //Teether js files
-						expand: true,
-						cwd: 'assets/node_modules/tether/dist/js/',
-						src: ['**'],
-						dest: '../media/vendor/tether/js/',
-						filter: 'isFile'
-					},
-					{ // Punycode
-						expand: true,
-						cwd: 'assets/node_modules/punycode/',
-						src: ['punycode.js', 'LICENSE-MIT.txt'],
-						dest: '../media/vendor/punycode/js/',
-						filter: 'isFile'
-					},
-					{ // jcrop
-						expand: true,
-						cwd: 'assets/tmp/jcrop/jcrop-css',
-						src: ['**'],
-						dest: '../media/vendor/jcrop/css/',
-						filter: 'isFile'
-					},
-					{ // jcrop
-						expand: true,
-						cwd: 'assets/tmp/jcrop/jcrop-js',
-						src: ['jcrop.min.js', 'jcrop.js'],
-						dest: '../media/vendor/jcrop/js/',
-						filter: 'isFile'
-					},
-					{ // autocomplete
-						expand: true,
-						cwd: 'assets/tmp/autocomplete/dist',
-						src: ['jquery.autocomplete.min.js', 'jquery.autocomplete.js', 'license.txt'],
-						dest: '../media/vendor/autocomplete/js/',
-						filter: 'isFile'
-					},
-					{ // chosen
-						expand: true,
-						cwd: 'assets/node_modules/chosenjs',
-						src: ['chosen.css', 'chosen.min.js', 'chosen-sprite.png', 'chosen-sprite@2x.png'],
-						dest: '../media/vendor/chosenjs/css/',
-						filter: 'isFile'
-					},
-					{ // chosen
-						expand: true,
-						cwd: 'assets/node_modules/chosenjs',
-						src: ['chosen.jquery.min.js', 'chosen.jquery.js'],
-						dest: '../media/vendor/chosenjs/js/',
-						filter: 'isFile'
-					},
-					{ //Font Awesome css files
-						expand: true,
-						cwd: 'assets/node_modules/font-awesome/css/',
-						src: ['**'],
-						dest: '../media/vendor/font-awesome/css/',
-						filter: 'isFile'
-					},
-					{ //Font Awesome scss files
-						expand: true,
-						cwd: 'assets/node_modules/font-awesome/scss/',
-						src: ['**'],
-						dest: '../media/vendor/font-awesome/scss/',
-						filter: 'isFile'
-					},
-					{ //Font Awesome fonts files
-						expand: true,
-						cwd: 'assets/node_modules/font-awesome/fonts/',
-						src: ['**'],
-						dest: '../media/vendor/font-awesome/fonts/',
-						filter: 'isFile'
-					},
-					// tinyMCE
-					{ // tinyMCE files
-						expand: true,
-						cwd: 'assets/node_modules/tinymce/plugins/',
-						src: ['**'],
-						dest: '../media/vendor/tinymce/plugins/',
-						filter: 'isFile'
-					},
-					{ // tinyMCE files
-						expand: true,
-						cwd: 'assets/node_modules/tinymce/skins/',
-						src: ['**'],
-						dest: '../media/vendor/tinymce/skins/',
-						filter: 'isFile'
-					},
-					{ // tinyMCE files
-						expand: true,
-						cwd: 'assets/node_modules/tinymce/themes/',
-						src: ['**'],
-						dest: '../media/vendor/tinymce/themes/',
-						filter: 'isFile'
-					},
-					{ // tinyMCE files
-						expand: true,
-						cwd: 'assets/node_modules/tinymce/',
-						src: ['tinymce.js','tinymce.min.js','license.txt','changelog.txt'],
-						dest: '../media/vendor/tinymce/',
-						filter: 'isFile'
-					},
-					// Code mirror
-					{ // Code mirror files
-						expand: true,
-						cwd: 'assets/tmp/codemirror/addon/',
-						src: ['**'],
-						dest: '../media/vendor/codemirror/addon/',
-						filter: 'isFile'
-					},
-					{ // Code mirror files
-						expand: true,
-						cwd: 'assets/tmp/codemirror/keymap/',
-						src: ['**'],
-						dest: '../media/vendor/codemirror/keymap/',
-						filter: 'isFile'
-					},
-					{ // Code mirror files
-						expand: true,
-						cwd: 'assets/tmp/codemirror/lib',
-						src: ['**'],
-						dest: '../media/vendor/codemirror/lib/',
-						filter: 'isFile'
-					},
-					{ // Code mirror files
-						expand: true,
-						cwd: 'assets/tmp/codemirror/mode',
-						src: ['**'],
-						dest: '../media/vendor/codemirror/mode/',
-						filter: 'isFile'
-					},
-					{ // Code mirror files
-						expand: true,
-						cwd: 'assets/tmp/codemirror/theme',
-						src: ['**'],
-						dest: '../media/vendor/codemirror/theme/',
-						filter: 'isFile'
-					},
-					// Licenses
-					{ // jQuery
-						src: ['assets/node_modules/jquery/LICENSE.txt'],
-						dest: '../media/vendor/jquery/LICENSE.txt',
-					},
-					{ // jCrop
-						src: ['assets/tmp/jcop/jcrop-MIT-LICENSE.txt'],
-						dest: '../media/vendor/jcrop/jcrop-MIT-LICENSE.txt',
-					},
-					{ // Bootstrap
-						src: ['assets/node_modules/bootstrap/LICENSE'],
-						dest: '../media/vendor/bootstrap/LICENSE',
-					},
-					{ // tether
-						src: ['assets/node_modules/tether/LICENSE'],
-						dest: '../media/vendor/tether/LICENSE',
-					},
-					{ // Code mirror
-						src: ['assets/tmp/codemirror/LICENSE'],
-						dest: '../media/vendor/codemirror/LICENSE',
-					},
-					{ // Jcrop
-						src: ['assets/tmp/jcrop/jcrop-MIT-LICENSE.txt'],
-						dest: '../media/vendor/jcrop/jcrop-MIT-LICENSE.txt',
-					},
-					{ // Dragula
-						src: ['assets/node_modules/dragula/license'],
-						dest: '../media/vendor/dragula/license',
-					},
-					{ // Media Element
-						expand: true,
-						cwd: 'assets/node_modules/mediaelement/build',
-						src: ['*.js', '*.swf', '*.xap', '!jquery.js'],
-						dest: '../media/vendor/mediaelement/js/',
-						filter: 'isFile'
-					},
-					{ // Media Element
-						expand: true,
-						cwd: 'assets/node_modules/mediaelement/build',
-						src: ['*.css', '*.png', '*.svg', '*.gif'],
-						dest: '../media/vendor/mediaelement/css/',
-						filter: 'isFile'
-					},
-					{ // MiniColors
-						expand: true,
-						cwd: 'assets/node_modules/jquery-minicolors',
-						src: ['*.js'],
-						dest: '../media/vendor/minicolors/js/',
-						filter: 'isFile'
-					},
-					{ // MiniColors
-						expand: true,
-						cwd: 'assets/node_modules/jquery-minicolors',
-						src: ['*.css', '*.png'],
-						dest: '../media/vendor/minicolors/css/',
-						filter: 'isFile'
-					},
-					{ // Awesomplete
-						expand: true,
-						cwd: 'assets/node_modules/awesomplete',
-						src: ['awesomplete.js', 'awesomplete.min.js'],
-						dest: '../media/vendor/awesomplete/js/',
-						filter: 'isFile'
-					},
-					{ // Awesomplete
-						expand: true,
-						cwd: 'assets/node_modules/awesomplete',
-						src: ['awesomplete.css'],
-						dest: '../media/vendor/awesomplete/css/',
-					},
-					{ // Dragula
-						expand: true,
-						cwd: 'assets/node_modules/dragula/dist',
-						src: ['*.js'],
-						dest: '../media/vendor/dragula/js/',
-						filter: 'isFile'
-					},
-					{ // Dragula
-						expand: true,
-						cwd: 'assets/node_modules/dragula/dist',
-						src: ['*.css'],
-						dest: '../media/vendor/dragula/css/',
-						filter: 'isFile'
-					},
-=======
 					}
->>>>>>> Stashed changes
 				]
 			}
 		},
