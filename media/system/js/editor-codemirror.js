bcd82d73a152d4618ce98cc2e2a15deb39363071
dgt41 <d.grammatiko@gmail.com>
2016-10-26 03:38:51 +0300

    strict

--- media/system/js/editor-codemirror.js
+++ media/system/js/editor-codemirror.js
@@ -2,8 +2,9 @@
  * @copyright  Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
+"use strict";
 
-;(function (window, Joomla) {
+(function (window, Joomla) {
 	jQuery(document).ready(function() {
 		var cm = CodeMirror, $ = jQuery;
 
