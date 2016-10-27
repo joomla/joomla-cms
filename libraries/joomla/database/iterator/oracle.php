82cc5cf2a51077f6f405ed77aa0386055246882e
Omar Ramos <orware@gmail.com>
2016-10-27 00:54:41 -0700

    Fixing as many Codesniffer errors as I can so I can recheck again.

--- libraries/joomla/database/iterator/oracle.php
+++ libraries/joomla/database/iterator/oracle.php
@@ -17,18 +17,18 @@ defined('JPATH_PLATFORM') or die;
 class JDatabaseIteratorOracle extends JDatabaseIterator
 {
 	/**
-    * Is used to decide whether a result set
-    * should generate lowercase field names
-    *
-    * @var boolean
-    */
-    protected $tolower = true;
+	* Is used to decide whether a result set
+	* should generate lowercase field names
+	*
+	* @var boolean
+	*/
+	protected $tolower = true;
 
-    /**
-    * Is used to decide whether a result set
-    * should return the LOB values or the LOB objects
-    */
-    protected $returnlobs = true;
+	/**
+	* Is used to decide whether a result set
+	* should return the LOB values or the LOB objects
+	*/
+	protected $returnlobs = true;
 
 	/**
 	 * Get the number of rows in the result set for the executed SQL given by the cursor.
@@ -89,77 +89,86 @@ class JDatabaseIteratorOracle extends JDatabaseIterator
 	}
 
 	/**
-    * Sets the $tolower variable to true
-    * so that field names will be created
-    * using lowercase values.
-    *
-    * @return void
-    */
-    public function toLower()
-    {
-        $this->tolower = true;
-    }
+	* Sets the $tolower variable to true
+	* so that field names will be created
+	* using lowercase values.
+	*
+	* @return void
+	*/
+	public function toLower()
+	{
+	    $this->tolower = true;
+	}
 
-    /**
-    * Sets the $tolower variable to false
-    * so that field names will be created
-    * using uppercase values.
-    *
-    * @return void
-    */
-    public function toUpper()
-    {
-        $this->tolower = false;
-    }
+	/**
+	* Sets the $tolower variable to false
+	* so that field names will be created
+	* using uppercase values.
+	*
+	* @return void
+	*/
+	public function toUpper()
+	{
+	    $this->tolower = false;
+	}
 
-    /**
-    * Sets the $returnlobs variable to true
-    * so that LOB object values will be
-    * returned rather than an OCI-Lob Object.
-    *
-    * @return void
-    */
-    public function returnLobValues()
-    {
-        $this->returnlobs = true;
-    }
+	/**
+	* Sets the $returnlobs variable to true
+	* so that LOB object values will be
+	* returned rather than an OCI-Lob Object.
+	*
+	* @return void
+	*/
+	public function returnLobValues()
+	{
+	    $this->returnlobs = true;
+	}
 
-    /**
-    * Sets the $returnlobs variable to false
-    * so that OCI-Lob Objects will be returned.
-    *
-    * @return void
-    */
-    public function returnLobObjects()
-    {
-        $this->returnlobs = false;
-    }
+	/**
+	* Sets the $returnlobs variable to false
+	* so that OCI-Lob Objects will be returned.
+	*
+	* @return void
+	*/
+	public function returnLobObjects()
+	{
+	    $this->returnlobs = false;
+	}
 
-    /**
-    * Depending on the value for $returnlobs,
-    * this method returns the proper constant
-    * combinations to be passed to the oci* functions
-    *
-    * @return int
-    */
-    public function getMode($numeric = false)
-    {
-        if ($numeric === false) {
-            if ($this->returnlobs) {
-                $mode = OCI_ASSOC+OCI_RETURN_NULLS+OCI_RETURN_LOBS;
-            }
-            else {
-                $mode = OCI_ASSOC+OCI_RETURN_NULLS;
-            }
-        } else {
-            if ($this->returnlobs) {
-                $mode = OCI_NUM+OCI_RETURN_NULLS+OCI_RETURN_LOBS;
-            }
-            else {
-                $mode = OCI_NUM+OCI_RETURN_NULLS;
-            }
-        }
+	/**
+	* Depending on the value for $returnlobs,
+	* this method returns the proper constant
+	* combinations to be passed to the oci* functions
+	*
+	* @param   bool  $numeric  Assoc or Numeric Mode
+	*
+	* @return int
+	*/
+	public function getMode($numeric = false)
+	{
+	    if ($numeric === false)
+	    {
+	        if ($this->returnlobs)
+	        {
+	            $mode = OCI_ASSOC+OCI_RETURN_NULLS+OCI_RETURN_LOBS;
+	        }
+	        else
+	        {
+	            $mode = OCI_ASSOC+OCI_RETURN_NULLS;
+	        }
+	    }
+	    else
+	    {
+	        if ($this->returnlobs)
+	        {
+	            $mode = OCI_NUM+OCI_RETURN_NULLS+OCI_RETURN_LOBS;
+	        }
+	        else
+	        {
+	            $mode = OCI_NUM+OCI_RETURN_NULLS;
+	        }
+	    }
 
-        return $mode;
-    }
+	    return $mode;
+	}
 }
