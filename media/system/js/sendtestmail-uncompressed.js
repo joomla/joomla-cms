62295dd5aafeb08ba0fdd8536f8455d21a8e6509
Elijah Madden <elijah.madden@nikkoam.com>
2016-10-28 17:54:04 +0900

    Make sendTestMail globally accessible for some reason.

--- media/system/js/sendtestmail-uncompressed.js
+++ media/system/js/sendtestmail-uncompressed.js
@@ -3,6 +3,47 @@
  * @copyright       Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
  * @license         GNU General Public License version 2 or later; see LICENSE.txt
  */
+var sendTestMail = function () {
+	'use strict';
+
+	var email_data = {
+		smtpauth  : $('input[name="jform[smtpauth]"]:checked').val(),
+		smtpuser  : $('input[name="jform[smtpuser]"]').val(),
+		smtppass  : $('input[name="jform[smtppass]"]').val(),
+		smtphost  : $('input[name="jform[smtphost]"]').val(),
+		smtpsecure: $('select[name="jform[smtpsecure]"]').val(),
+		smtpport  : $('input[name="jform[smtpport]"]').val(),
+		mailfrom  : $('input[name="jform[mailfrom]"]').val(),
+		fromname  : $('input[name="jform[fromname]"]').val(),
+		mailer    : $('select[name="jform[mailer]"]').val(),
+		mailonline: $('input[name="jform[mailonline]"]:checked').val()
+	};
+
+	// Remove js messages, if they exist.
+	Joomla.removeMessages();
+
+	$.ajax({
+		method: "POST",
+		url: document.getElementById('sendtestmail').getAttribute('data-ajaxuri'),
+		data: email_data,
+		dataType: "json"
+	})
+	.fail(function (jqXHR, textStatus, error) {
+		Joomla.renderMessages(Joomla.ajaxErrorsMessages(jqXHR, textStatus, error));
+
+		window.scrollTo(0, 0);
+	})
+	.done(function (response) {
+		// Render messages, if any.
+		if (typeof response.messages == 'object' && response.messages !== null)
+		{
+			Joomla.renderMessages(response.messages);
+
+			window.scrollTo(0, 0);
+		}
+	});
+};
+
 jQuery(function ($)
 {
 	'use strict';
@@ -10,42 +51,5 @@ jQuery(function ($)
 	/**
 	* Calls the sending process of the config class
 	*/
-	$('#sendtestmail').click(function () {
-		var email_data = {
-			smtpauth  : $('input[name="jform[smtpauth]"]:checked').val(),
-			smtpuser  : $('input[name="jform[smtpuser]"]').val(),
-			smtppass  : $('input[name="jform[smtppass]"]').val(),
-			smtphost  : $('input[name="jform[smtphost]"]').val(),
-			smtpsecure: $('select[name="jform[smtpsecure]"]').val(),
-			smtpport  : $('input[name="jform[smtpport]"]').val(),
-			mailfrom  : $('input[name="jform[mailfrom]"]').val(),
-			fromname  : $('input[name="jform[fromname]"]').val(),
-			mailer    : $('select[name="jform[mailer]"]').val(),
-			mailonline: $('input[name="jform[mailonline]"]:checked').val()
-		};
-
-		// Remove js messages, if they exist.
-		Joomla.removeMessages();
-
-		$.ajax({
-			method: "POST",
-			url: document.getElementById('sendtestmail').getAttribute('data-ajaxuri'),
-			data: email_data,
-			dataType: "json"
-		})
-		.fail(function (jqXHR, textStatus, error) {
-			Joomla.renderMessages(Joomla.ajaxErrorsMessages(jqXHR, textStatus, error));
-
-			window.scrollTo(0, 0);
-		})
-		.done(function (response) {
-			// Render messages, if any.
-			if (typeof response.messages == 'object' && response.messages !== null)
-			{
-				Joomla.renderMessages(response.messages);
-
-				window.scrollTo(0, 0);
-			}
-		});
-	});
+	$('#sendtestmail').click(sendTestMail);
 });
