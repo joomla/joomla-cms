ee53660f8755c61cd4a9a978448867f2176e5bbe
C-Lodder <lodder15@hotmail.co.uk>
2016-10-30 17:01:41 +0000

    Update FontAwesome to 4.7.0 + SCSS tweaks

--- dev/assets.php
+++ dev/assets.php
@@ -8,7 +8,7 @@ abstract class ExternalAssets{
 			'jquery-migrate' => array('version' => '1.4.1','dependencies' => 'jquery'),
 			'bootstrap' => array('version' => '~4.0.0-alpha.5','dependencies' => 'jquery, tether'),
 			'tether' => array('version' => '1.3.7','dependencies' => 'jquery, tether'),
-			'font-awesome' => array('version' => '4.6.3','dependencies' => ''),
+			'font-awesome' => array('version' => '4.7.0','dependencies' => ''),
 			'jquery-minicolors' => array('version' => '2.1.10','dependencies' => 'jquery'),
 			'jquery-sortable' => array('version' => '0.9.13','dependencies' => 'jquery'),
 			'jquery-ui' => array('version' => '1.12.1','dependencies' => 'jquery'),
