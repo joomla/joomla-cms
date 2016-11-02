a49543e0e077f0a6ebf4eb995b08eec4c97d97d2
andrepereiradasilva <andrepereiradasilva@users.noreply.github.com>
2016-11-02 22:05:20 +0000

    add mediaversion to polyfill

--- libraries/cms/html/behavior.php
+++ libraries/cms/html/behavior.php
@@ -1000,7 +1000,8 @@ abstract class JHtmlBehavior
 			}
 
 			// If include according to browser.
-			$scriptOptions = !is_null($conditionalBrowser) ? array('relative' => true, 'conditional' => $conditionalBrowser) : array('relative' => true);
+			$scriptOptions = array('version' => 'auto', 'relative' => true);
+			$scriptOptions = $conditionalBrowser !== null ? array_replace($scriptOptions, array('conditional' => $conditionalBrowser)) : $scriptOptions;
 
 			JHtml::_('script', 'system/polyfill.' . $polyfillType . '.js', $scriptOptions);
 
