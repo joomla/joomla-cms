ccfd5723a5daf9b506643b6518ac7b0b5e39ea2b
andrepereiradasilva <andrepereiradasilva@users.noreply.github.com>
2016-11-04 14:11:07 +0000

    Update sample_testing.sql

--- installation/sql/mysql/sample_testing.sql
+++ installation/sql/mysql/sample_testing.sql
@@ -171,7 +171,7 @@ INSERT IGNORE INTO `#__assets` (`id`, `parent_id`, `lft`, `rgt`, `level`, `name`
 (166, 47, 187, 192, 6, 'com_content.category.75', 'Navigation Modules', '{}'),
 (167, 54, 295, 296, 4, 'com_content.category.76', 'Recipes', '{"core.create":{"12":1,"10":1},"core.edit.own":{"12":1,"10":1}}'),
 (168, 34, 106, 107, 3, 'com_content.article.67', 'What''s New in 1.5?', '{}'),
-(169, 24, 341, 342, 2, 'com_users.category.77', 'Uncategorised', ''),
+(169, 24, 341, 342, 2, 'com_users.category.77', 'Uncategorised', '{}'),
 (170, 50, 223, 224, 6, 'com_content.article.68', 'Captcha', '{}'),
 (171, 50, 225, 226, 6, 'com_content.article.69', 'Quick Icons', '{}'),
 (172, 96, 182, 183, 7, 'com_content.article.70', 'Smart Search', '{}'),
