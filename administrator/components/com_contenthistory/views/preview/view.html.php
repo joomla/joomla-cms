805f8da3ec29ee0352f8adb33365ca6d136d0e32
Phil Taylor <phil@phil-taylor.com>
2016-11-06 17:07:18 +0000

    make travis happy

--- administrator/components/com_contenthistory/views/preview/view.html.php
+++ administrator/components/com_contenthistory/views/preview/view.html.php
@@ -34,13 +34,14 @@ class ContenthistoryViewPreview extends JViewLegacy
 		$this->state = $this->get('State');
 		$this->item = $this->get('Item');
 		
-		if (FALSE === $this->item) {
+		if (false === $this->item) 
+		{
             		$language = JFactory::getLanguage();
-            		$language->load('com_content', JPATH_SITE, NULL, TRUE);
+            		$language->load('com_content', JPATH_SITE, null, true);
 			
             		JError::raiseError(404, JText::_('COM_CONTENT_ERROR_ARTICLE_NOT_FOUND'));
 
-            		return FALSE;
+            		return false;
         	}
 
 		// Check for errors.
