3205d858b14d4aff0ef5553ab4037ec93bd40326
andrepereiradasilva <andrepereiradasilva@users.noreply.github.com>
2016-11-06 23:49:27 +0000

    in hathor too

--- administrator/templates/hathor/html/com_banners/banners/default.php
+++ administrator/templates/hathor/html/com_banners/banners/default.php
@@ -17,7 +17,6 @@ $user      = JFactory::getUser();
 $userId    = $user->get('id');
 $listOrder = $this->escape($this->state->get('list.ordering'));
 $listDirn  = $this->escape($this->state->get('list.direction'));
-$canOrder  = $user->authorise('core.edit.state', 'com_banners.category');
 $saveOrder = $listOrder == 'ordering';
 ?>
 <form action="<?php echo JRoute::_('index.php?option=com_banners&view=banners'); ?>" method="post" name="adminForm" id="adminForm">
