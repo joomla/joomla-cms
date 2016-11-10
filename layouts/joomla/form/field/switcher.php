61a25e1b84f4b2563dbd1d87b562f337bb525b42
C-Lodder <lodder15@hotmail.co.uk>
2016-11-10 15:14:11 +0000

    Add support for danger and primary switchers

--- layouts/joomla/form/field/switcher.php
+++ layouts/joomla/form/field/switcher.php
@@ -54,11 +54,10 @@ JHtml::_('script', 'system/fields/switcher.js', false, true);
 ?>
 <fieldset id="<?php echo $id; ?>"
 	<?php echo $disabled ? 'disabled' : ''; ?>
-	<?php echo $required ? 'required aria-required="true"' : ''; ?>
-	<?php echo $class ? 'class="' . $class . '"' : ''; ?>>
+	<?php echo $required ? 'required aria-required="true"' : ''; ?>>
 
 	<?php if (!empty($options)) : ?>
-		<span class="js-switcher">
+		<span <?php echo $class ? 'class="js-switcher ' . $class . '"' : 'class="js-switcher"'; ?>>
 			<?php foreach ($options as $i => $option) : ?>
 				<?php
 					// Initialize some option attributes.
