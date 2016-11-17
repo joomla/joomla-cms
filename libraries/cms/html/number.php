78256e247612ca0f168a3070bca7534c7148ff71
andrepereiradasilva <andrepereiradasilva@users.noreply.github.com>
2016-11-17 19:48:09 +0000

    minor cs

--- libraries/cms/html/number.php
+++ libraries/cms/html/number.php
@@ -106,7 +106,7 @@ abstract class JHtmlNumber
 		}
 
 		return number_format(
-			round($oBytes / pow($base, $i), (int) $precision),(int) $precision, JText::_('DECIMALS_SEPARATOR'), JText::_('THOUSANDS_SEPARATOR')
+			round($oBytes / pow($base, $i), (int) $precision), (int) $precision, JText::_('DECIMALS_SEPARATOR'), JText::_('THOUSANDS_SEPARATOR')
 		) . ' ' . $suffix;
 	}
 }
