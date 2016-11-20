6586a1919bd5815efebee57999054a4f88469ace
andrepereiradasilva <andrepereiradasilva@users.noreply.github.com>
2016-11-20 20:43:23 +0000

    remove extra space

--- administrator/components/com_admin/sql/updates/sqlazure/3.7.0-2016-11-21.sql
+++ administrator/components/com_admin/sql/updates/sqlazure/3.7.0-2016-11-21.sql
@@ -3,4 +3,3 @@ ALTER TABLE [#__languages] DROP CONSTRAINT [#__languages$idx_image];
 CREATE NONCLUSTERED INDEX [idx_image] ON [#__languages] (
 	[image] ASC
 ) WITH (STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF);
-
