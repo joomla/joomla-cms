93f91c52d026762f3765ec2ccbbcc7064d9b7533
C-Lodder <lodder15@hotmail.co.uk>
2016-11-22 10:42:35 +0000

    basic calendar input styling tweaks

--- layouts/joomla/form/field/calendar.php
+++ layouts/joomla/form/field/calendar.php
@@ -105,29 +105,31 @@ JHtml::_('stylesheet', 'system/fields/calendar-vanilla' . $cssFileExt, array(), 
 ?>
 <div class="field-calendar">
 	<?php if (!$readonly && !$disabled) : ?>
-	<div class="input-append">
+	<div class="input-group">
 		<?php endif; ?>
 		<input type="text" name="<?php
 		echo $name; ?>" value="<?php
-		echo htmlspecialchars(($value != "0000-00-00 00:00:00") ? $value : '', ENT_COMPAT, 'UTF-8'); ?>"<?php echo  $attributes; ?>
+		echo htmlspecialchars(($value != "0000-00-00 00:00:00") ? $value : '', ENT_COMPAT, 'UTF-8'); ?>"<?php echo $attributes; ?>
 		<?php !empty($hint) ? 'placeholder="' . $hint . '"' : ''; ?> data-alt-value="<?php
 		echo htmlspecialchars($value, ENT_COMPAT, 'UTF-8') ?>" autocomplete="off"/>
-		<button type="button" class="<?php echo ($readonly || $disabled) ? 'hidden ' : ''; ?>btn btn-secondary"
-			id="<?php echo  $id; ?>_btn"
-			data-inputfield="<?php echo $id; ?>"
-			data-dayformat="<?php echo $format; ?>"
-			data-button="<?php echo $id; ?>_btn"
-			data-firstday="<?php echo JFactory::getLanguage()->getFirstDay();?>"
-			data-weekend="<?php echo JFactory::getLanguage()->getWeekEnd(); ?>"
-			data-today-btn="<?php echo $todaybutton; ?>"
-			data-week-numbers="<?php echo $weeknumbers; ?>"
-			data-show-time="<?php echo $showtime; ?>"
-			data-show-others="<?php echo $filltable; ?>"
-			data-time-24="<?php echo $timeformat; ?>"
-			data-only-months-nav="<?php echo $singleheader; ?>"
-			<?php echo !empty($minYear) ? 'data-min-year="' . $minYear . '"' : ''; ?>
-			<?php echo !empty($maxYear) ? 'data-max-year="' . $maxYear . '"' : ''; ?>
-		><span class="fa fa-calendar"></span></button>
+		<span class="input-group-btn">
+			<button type="button" class="<?php echo ($readonly || $disabled) ? 'hidden ' : ''; ?>btn btn-secondary"
+				id="<?php echo  $id; ?>_btn"
+				data-inputfield="<?php echo $id; ?>"
+				data-dayformat="<?php echo $format; ?>"
+				data-button="<?php echo $id; ?>_btn"
+				data-firstday="<?php echo JFactory::getLanguage()->getFirstDay();?>"
+				data-weekend="<?php echo JFactory::getLanguage()->getWeekEnd(); ?>"
+				data-today-btn="<?php echo $todaybutton; ?>"
+				data-week-numbers="<?php echo $weeknumbers; ?>"
+				data-show-time="<?php echo $showtime; ?>"
+				data-show-others="<?php echo $filltable; ?>"
+				data-time-24="<?php echo $timeformat; ?>"
+				data-only-months-nav="<?php echo $singleheader; ?>"
+				<?php echo !empty($minYear) ? 'data-min-year="' . $minYear . '"' : ''; ?>
+				<?php echo !empty($maxYear) ? 'data-max-year="' . $maxYear . '"' : ''; ?>
+			><span class="fa fa-calendar"></span></button>
+		</span>
 		<?php if (!$readonly && !$disabled) : ?>
 	</div>
 <?php endif; ?>
