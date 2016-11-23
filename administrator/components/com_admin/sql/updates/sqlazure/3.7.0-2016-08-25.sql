520dcc7c1d6ba6220e73d88c80e950cf7df2b14d
muhakh <muhakh2@gmail.com>
2016-11-23 05:50:09 +0200

    Fixing SQL conflicts and mistakes

--- administrator/components/com_admin/sql/updates/sqlazure/3.7.0-2016-08-25.sql
+++ administrator/components/com_admin/sql/updates/sqlazure/3.7.0-2016-08-25.sql
@@ -125,7 +125,7 @@ SELECT 34, 'com_userlogs', 'component', 'com_userlogs', '', 1, 1, 1, 0, '', '', 
 UNION ALL
 SELECT 458, 'plg_system_userlogs', 'plugin', 'userlogs', 'system', 0, 0, 1, 0, '', '{"logDeletePeriod":"200","ip_logging":"1","loggable_extensions":["com_banners","com_cache","com_categories","com_config","com_contact","com_content","com_installer","com_media","com_menus","com_messages","com_modules","com_newsfeeds","com_plugins","com_redirect","com_tags","com_templates","com_users"]}' '', '', 0, '1900-01-01 00:00:00', 0, 0;
 
-SET IDENTITY_INSERT #__extensions  OFF;
+SET IDENTITY_INSERT [#__extensions]  OFF;
 
 SET IDENTITY_INSERT [#__menu]  ON;
 
