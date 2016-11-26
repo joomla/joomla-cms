5e2d504d8dd3785fb49f144e2ee6bc1913b2adfa
andrepereiradasilva <andrepereiradasilva@users.noreply.github.com>
2016-11-26 16:33:42 +0000

    Update 3.7.0-2016-11-27.sql

--- administrator/components/com_admin/sql/updates/mysql/3.7.0-2016-11-27.sql
+++ administrator/components/com_admin/sql/updates/mysql/3.7.0-2016-11-27.sql
@@ -1,2 +1,2 @@
--- Normalize modules contend field with other db systems. Add default value.
+-- Normalize modules content field with other db systems. Add default value.
 ALTER TABLE `#__modules` MODIFY `content` text NOT NULL DEFAULT '';
