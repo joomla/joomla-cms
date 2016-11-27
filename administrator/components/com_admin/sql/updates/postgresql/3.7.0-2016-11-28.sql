0930640a24162c0234691e9deee6b89670099e24
andrepereiradasilva <andrepereiradasilva@users.noreply.github.com>
2016-11-27 10:57:26 +0000

    update core statement

--- administrator/components/com_admin/sql/updates/postgresql/3.7.0-2016-11-28.sql
+++ administrator/components/com_admin/sql/updates/postgresql/3.7.0-2016-11-28.sql
@@ -3,3 +3,7 @@ ALTER TABLE "#__extensions" ADD COLUMN "core" smallint DEFAULT 0 NOT NULL;
 
 COMMENT ON COLUMN "#__extensions"."protected" IS 'Flag to indicate if the extension can be enabled/disabled.';
 COMMENT ON COLUMN "#__extensions"."core" IS 'Flag to indicate if is a Joomla core extension. Core extensions can not be uninstalled.';
+
+UPDATE "#__extensions"
+SET "core" = 1
+WHERE "element" IN ();
--- administrator/components/com_admin/sql/updates/sqlazure/3.7.0-2016-11-28.sql
+++ administrator/components/com_admin/sql/updates/sqlazure/3.7.0-2016-11-28.sql
@@ -1,2 +1,6 @@
 -- Add core field to extensions table.
 ALTER TABLE [#__extensions] ADD [core] [smallint] NOT NULL DEFAULT 0;
+
+UPDATE [#__extensions]
+SET [core] = 1
+WHERE [element] IN ();
