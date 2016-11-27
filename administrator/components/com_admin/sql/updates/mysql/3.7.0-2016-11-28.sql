b82dfe6d9fc23dbd0548ccdb00fc8fdf39f35d4c
andrepereiradasilva <andrepereiradasilva@users.noreply.github.com>
2016-11-27 12:56:02 +0000

    obvius this should be ;

--- administrator/components/com_admin/sql/updates/mysql/3.7.0-2016-11-28.sql
+++ administrator/components/com_admin/sql/updates/mysql/3.7.0-2016-11-28.sql
@@ -1,6 +1,6 @@
 -- Add core field to extensions table.
-ALTER TABLE `#__extensions` MODIFY `protected` tinyint(3) NOT NULL DEFAULT 0 COMMENT 'Flag to indicate if the extension can be enabled/disabled.',
-ALTER TABLE `#__extensions` ADD COLUMN `core` tinyint(3) NOT NULL DEFAULT 0 COMMENT 'Flag to indicate if is a Joomla core extension. Core extensions can not be uninstalled.',
+ALTER TABLE `#__extensions` MODIFY `protected` tinyint(3) NOT NULL DEFAULT 0 COMMENT 'Flag to indicate if the extension can be enabled/disabled.';
+ALTER TABLE `#__extensions` ADD COLUMN `core` tinyint(3) NOT NULL DEFAULT 0 COMMENT 'Flag to indicate if is a Joomla core extension. Core extensions can not be uninstalled.';
 
 -- Set all core extensions as core 1 and unprotected them.
 UPDATE `#__extensions`
