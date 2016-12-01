d2e8c613811c2546ad37fd8798a144527ed6e58e
Dimitris Grammatikogiannis <d.grammatiko@gmail.com>
2016-12-01 14:09:19 +0200

    remove vendor prefixes for fn closest

--- administrator/templates/atum/js/template.js
+++ administrator/templates/atum/js/template.js
@@ -14,23 +14,12 @@
 
 		/** http://stackoverflow.com/questions/18663941/finding-closest-element-without-jquery */
 		function closest(el, selector) {
-			var matchesFn;
-
-			// find vendor prefix
-			['matches','webkitMatchesSelector','mozMatchesSelector','msMatchesSelector','oMatchesSelector'].some(function(fn) {
-				if (typeof document.body[fn] == 'function') {
-					matchesFn = fn;
-					return true;
-				}
-				return false;
-			});
-
 			var parent;
 
 			// traverse parents
 			while (el) {
 				parent = el.parentElement;
-				if (parent && parent[matchesFn](selector)) {
+				if (parent && parent['matches'](selector)) {
 					return parent;
 				}
 				el = parent;
