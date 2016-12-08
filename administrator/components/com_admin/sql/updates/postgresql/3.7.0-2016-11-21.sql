87abf626eaf5c63c4873e618635f9606b77d2873
andrepereiradasilva <andrepereiradasilva@users.noreply.github.com>
2016-12-08 10:58:33 +0000

    Update 3.7.0-2016-11-21.sql

--- administrator/components/com_admin/sql/updates/postgresql/3.7.0-2016-11-21.sql
+++ administrator/components/com_admin/sql/updates/postgresql/3.7.0-2016-11-21.sql
@@ -1,3 +1,3 @@
 -- Replace language image UNIQUE index for a normal INDEX.
 -- Note: This needs to be done in just one query because of the database schema checker.
-ALTER TABLE "#__languages" DROP CONSTRAINT "#__idx_image", ADD CONSTRAINT "#__idx_image" ("image");
+ALTER TABLE "#__languages" DROP CONSTRAINT "#__idx_image", ADD INDEX "#__idx_image" ("image");
