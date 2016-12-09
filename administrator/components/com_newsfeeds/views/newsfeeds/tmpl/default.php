bbee8884ee22a7c455ed4c2a019330961c69cb18
marcochirienti <marco.chirienti@gmail.com>
2016-12-09 11:18:59 +0100

    Fixed wrong place for space

--- administrator/components/com_newsfeeds/views/newsfeeds/tmpl/default.php
+++ administrator/components/com_newsfeeds/views/newsfeeds/tmpl/default.php
@@ -145,7 +145,7 @@ if ($saveOrder)
 										<?php echo $this->escape($item->name); ?>
 								<?php endif; ?>
 								<span class="small">
-									<?php echo JText::sprintf('JGLOBAL_LIST_ALIAS', $this->escape($item->alias)) ;?>
+									<?php echo JText::sprintf('JGLOBAL_LIST_ALIAS', $this->escape($item->alias)); ?>
 								</span>
 								<div class="small">
 									<?php echo JText::_('JCATEGORY') . ': ' . $this->escape($item->category_title); ?>
