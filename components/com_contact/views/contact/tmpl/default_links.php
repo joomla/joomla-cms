9f52cf221a0d95c67aa00f5a4f394fa72eaac109
marcochirienti <marco.chirienti@gmail.com>
2016-12-09 11:23:27 +0100

    Removed unnecessary space before PHP closing tag

--- components/com_contact/views/contact/tmpl/default_links.php
+++ components/com_contact/views/contact/tmpl/default_links.php
@@ -17,7 +17,7 @@ defined('_JEXEC') or die;
 	<?php echo JHtml::_('bootstrap.addTab', 'myTab', 'display-links', JText::_('COM_CONTACT_LINKS')); ?>
 <?php endif; ?>
 <?php if ($this->params->get('presentation_style') === 'plain') : ?>
-	<?php echo '<h3>' . JText::_('COM_CONTACT_LINKS') . '</h3>';  ?>
+	<?php echo '<h3>' . JText::_('COM_CONTACT_LINKS') . '</h3>'; ?>
 <?php endif; ?>
 
 <div class="contact-links">
