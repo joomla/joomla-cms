5d5057e458825fa7823cbd907077f757cdf3dc08
ggppdk <ggppdk@gmail.com>
2016-12-11 06:55:38 +0200

    Removed redudant pipe character

--- plugins/system/sef/sef.php
+++ plugins/system/sef/sef.php
@@ -153,7 +153,7 @@ class PlgSystemSef extends JPlugin
 		// Replace all unknown protocols in CSS background image.
 		if (strpos($buffer, 'style=') !== false)
 		{
-			$regex_url  = '\s*url\s*\(([\'\"]|\&\#0?3[4|9];)?(?!/|\&\#0?3[4|9];|' . $protocols . '|\#)([^\)\'\"]+)([\'\"]|\&\#0?3[4|9];)?\)';
+			$regex_url  = '\s*url\s*\(([\'\"]|\&\#0?3[49];)?(?!/|\&\#0?3[49];|' . $protocols . '|\#)([^\)\'\"]+)([\'\"]|\&\#0?3[49];)?\)';
 			$regex  = '#style=\s*([\'\"])(.*):' . $regex_url . '#m';
 			$buffer = preg_replace($regex, 'style=$1$2: url($3' . $base . '$4$5$6)', $buffer);
 			$this->checkBuffer($buffer);
