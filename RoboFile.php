8407a6ec21c6f0e7b30163ec90b34e16c96f1461
Jelle Kok <schumacher1986@hotmail.com>
2016-12-11 12:34:10 +0100

    Remove curl check (#101)
    We don't need it.

--- RoboFile.php
+++ RoboFile.php
@@ -185,8 +185,6 @@ class RoboFile extends \Robo\Tasks
 			$this->_copy('./htaccess.txt', $this->cmsPath . '/.htaccess');
 			$this->_exec('sed -e "s,# RewriteBase /,RewriteBase /tests/codeception/joomla-cms3/,g" -in-place tests/codeception/joomla-cms3/.htaccess');
 		}
-
-		$this->taskExec('curl -I http://localhost/tests/joomla-cms3/')->printed(true)->run();
 	}
 
 	/**
