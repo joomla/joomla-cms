36f60873e489cecf0518bf87dd91ff4c63470f3f
Frank Mayer <frank@frankmayer.net>
2016-12-15 01:46:16 +0200

    Replace unnecessary double quotes

--- modules/mod_related_items/tmpl/default.php
+++ modules/mod_related_items/tmpl/default.php
@@ -13,7 +13,7 @@ defined('_JEXEC') or die;
 <?php foreach ($list as $item) : ?>
 <li>
 	<a href="<?php echo $item->route; ?>">
-		<?php if ($showDate) echo JHtml::_('date', $item->created, JText::_('DATE_FORMAT_LC4')) . " - "; ?>
+		<?php if ($showDate) echo JHtml::_('date', $item->created, JText::_('DATE_FORMAT_LC4')) . ' - '; ?>
 		<?php echo $item->title; ?></a>
 </li>
 <?php endforeach; ?>
