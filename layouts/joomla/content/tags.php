460fc5c7634f180e93fdf7f8a47411d29e59ea6b
Benjamin Trenkle <bembelimen@users.noreply.github.com>
2016-12-14 21:05:22 +0100

    Don't load authorised view levels in foreach for tags generation (#13140)
    * Don't load authorised groups in foreach for tags generation
    
    * Add $
    
    * Use user method
    
    * Change variable name

--- layouts/joomla/content/tags.php
+++ layouts/joomla/content/tags.php
@@ -13,11 +13,13 @@ use Joomla\Registry\Registry;
 
 JLoader::register('TagsHelperRoute', JPATH_BASE . '/components/com_tags/helpers/route.php');
 
+$authorised = JFactory::getUser()->getAuthorisedViewLevels();
+
 ?>
 <?php if (!empty($displayData)) : ?>
 	<ul class="tags inline">
 		<?php foreach ($displayData as $i => $tag) : ?>
-			<?php if (in_array($tag->access, JAccess::getAuthorisedViewLevels(JFactory::getUser()->get('id')))) : ?>
+			<?php if (in_array($tag->access, $authorised)) : ?>
 				<?php $tagParams = new Registry($tag->params); ?>
 				<?php $link_class = $tagParams->get('tag_link_class', 'label label-info'); ?>
 				<li class="tag-<?php echo $tag->tag_id; ?> tag-list<?php echo $i ?>" itemprop="keywords">
