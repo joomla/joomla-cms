f376ba9b709759fa2f97922de3e04e594674433a
Frank Mayer <frank@frankmayer.net>
2016-12-16 17:06:36 +0200

    Simplify ternary operation using elvis operator

--- templates/beez3/html/com_content/category/default_articles.php
+++ templates/beez3/html/com_content/category/default_articles.php
@@ -155,7 +155,7 @@ if (!empty($this->items))
 					<td class="list-author">
 						<?php if (!empty($article->author) || !empty($article->created_by_alias)) : ?>
 							<?php $author = $article->author ?>
-							<?php $author = ($article->created_by_alias ? $article->created_by_alias : $author); ?>
+							<?php $author = ($article->created_by_alias ?: $author); ?>
 							<?php if (!empty($article->contact_link) &&  $this->params->get('link_author') == true):?>
 								<?php echo JText::sprintf('COM_CONTENT_WRITTEN_BY', JHtml::_('link', $article->contact_link, $author)); ?>
 							<?php else :?>
