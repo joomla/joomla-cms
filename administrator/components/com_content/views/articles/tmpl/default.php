07ce54ba59000627c49fc2fcce97834951c47bac
ciar4n <ciaran@joomla51.com>
2016-12-17 12:45:49 +0000

    Change articleList-title to itemList-title

--- administrator/components/com_content/views/articles/tmpl/default.php
+++ administrator/components/com_content/views/articles/tmpl/default.php
@@ -178,7 +178,7 @@ $assoc = JLanguageAssociations::isEnabled();
 									<?php $language = $item->language_title ? $this->escape($item->language_title) : JText::_('JUNDEFINED'); ?>
 								<?php endif; ?>
 								<?php if ($canEdit || $canEditOwn) : ?>
-									<a class="hasTooltip articleList-title" href="<?php echo JRoute::_('index.php?option=com_content&task=article.edit&id=' . $item->id); ?>" title="<?php echo JText::_('JACTION_EDIT'); ?>">
+									<a class="hasTooltip itemList-title" href="<?php echo JRoute::_('index.php?option=com_content&task=article.edit&id=' . $item->id); ?>" title="<?php echo JText::_('JACTION_EDIT'); ?>">
 										<span class="icon-edit indic-edit"> </span><?php echo $this->escape($item->title); ?></a>
 								<?php else : ?>
 									<span title="<?php echo JText::sprintf('JFIELD_ALIAS_LABEL', $this->escape($item->alias)); ?>"><?php echo $this->escape($item->title); ?></span>
