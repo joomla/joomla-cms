ea4f6c6a9e3796a3e9cf26f88fe79b7864e7dfe6
Thomas Hunziker <werbemails@bakual.ch>
2016-12-19 15:49:34 +0100

    Remove unneeded registry creation (#12571)
    * Contact params already are Registry objects.
    
    * Newsfeed params already are Registry objects.
    
    * Menu params already are Registry objects.
    
    * Fixing wrong merging

--- components/com_contact/views/category/view.html.php
+++ components/com_contact/views/category/view.html.php
@@ -9,8 +9,6 @@
 
 defined('_JEXEC') or die;
 
-use Joomla\Registry\Registry;
-
 /**
  * HTML View class for the Contacts component
  *
@@ -60,7 +58,7 @@ class ContactViewCategory extends JViewCategory
 		foreach ($this->items as $item)
 		{
 			$item->slug = $item->alias ? ($item->id . ':' . $item->alias) : $item->id;
-			$temp       = new Registry($item->params);
+			$temp       = $item->params;
 			$item->params = clone $this->params;
 			$item->params->merge($temp);
 
