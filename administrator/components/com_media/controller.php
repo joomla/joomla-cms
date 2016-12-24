7cf6fa188952129197b1668835fd2dcd94dc8aa5
David Neukirchen <david.neukirchen@gmail.com>
2016-12-24 13:12:56 +0100

    Show the new media manager for browsers that do support ECMAScript 5 (#68)
    * Code style
    
    * Move legacy component to subfolder
    
    * Adjust the component paths for the legacy app
    
    * Code style
    
    * Fix ie check
    
    * Make the condition more readable
    
    * Code style
    
    * Fix ECMAscript version
    
    * Create the new media manager base controller
    
    * Code style
    
    * Implement pr feedback
    
    * Remove line indent

--- administrator/components/com_media/controller.php
+++ administrator/components/com_media/controller.php
@@ -9,89 +9,23 @@
 
 defined('_JEXEC') or die;
 
-/**
- * Media Manager Component Controller
- *
- * @since  1.5
- */
 class MediaController extends JControllerLegacy
 {
 	/**
-	 * Method to display a view.
+	 * Typical view method for MVC based architecture
+	 *
+	 * This function is provide as a default implementation, in most cases
+	 * you will need to override it in your own controllers.
 	 *
 	 * @param   boolean  $cachable   If true, the view output will be cached
 	 * @param   array    $urlparams  An array of safe url parameters and their variable types, for valid values see {@link JFilterInput::clean()}.
 	 *
-	 * @return  JController		This object to support chaining.
+	 * @return  JControllerLegacy  A JControllerLegacy object to support chaining.
 	 *
-	 * @since   1.5
+	 * @since   __DEPLOY_VERSION__
 	 */
 	public function display($cachable = false, $urlparams = false)
 	{
-		JPluginHelper::importPlugin('content');
-
-		$vType    = JFactory::getDocument()->getType();
-		$vName    = $this->input->get('view', 'media');
-
-		switch ($vName)
-		{
-			case 'images':
-				$vLayout = $this->input->get('layout', 'default', 'string');
-				$mName   = 'manager';
-
-				break;
-
-			case 'imagesList':
-				$mName   = 'list';
-				$vLayout = $this->input->get('layout', 'default', 'string');
-
-				break;
-
-			case 'mediaList':
-				$app     = JFactory::getApplication();
-				$mName   = 'list';
-				$vLayout = $app->getUserStateFromRequest('media.list.layout', 'layout', 'thumbs', 'word');
-
-				break;
-
-			case 'media':
-			default:
-				$vName   = 'media';
-				$vLayout = $this->input->get('layout', 'default', 'string');
-				$mName   = 'manager';
-
-				break;
-		}
-
-		// Get/Create the view
-		$view = $this->getView($vName, $vType, '', array('base_path' => JPATH_COMPONENT_ADMINISTRATOR));
-
-		// Get/Create the model
-		if ($model = $this->getModel($mName))
-		{
-			// Push the model into the view (as default)
-			$view->setModel($model, true);
-		}
-
-		// Set the layout
-		$view->setLayout($vLayout);
-
-		// Display the view
-		$view->display();
-
-		return $this;
-	}
-
-	/**
-	 * Validate FTP credentials
-	 *
-	 * @return  void
-	 *
-	 * @since   1.5
-	 */
-	public function ftpValidate()
-	{
-		// Set FTP credentials, if given
-		JClientHelper::setCredentialsFromRequest('ftp');
+		echo 'Welcome to the new media manager';
 	}
 }
