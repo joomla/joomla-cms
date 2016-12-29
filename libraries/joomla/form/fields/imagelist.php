b10c98d18f95ffaa34cf3f47b6528d1efce2b916
Allon Moritz <allon.moritz@digital-peak.com>
2016-12-29 13:40:33 +0100

    Add imagelist plugin

--- libraries/joomla/form/fields/imagelist.php
+++ libraries/joomla/form/fields/imagelist.php
@@ -42,24 +42,4 @@ class JFormFieldImageList extends JFormFieldFileList
 		// Get the field options.
 		return parent::getOptions();
 	}
-
-	/**
-	 * Function to manipulate the DOM element of the field. The form can be
-	 * manipulated at that point.
-	 *
-	 * @param   stdClass    $field      The field.
-	 * @param   DOMElement  $fieldNode  The field node.
-	 * @param   JForm       $form       The form.
-	 *
-	 * @return  void
-	 *
-	 * @since   3.7.0
-	 */
-	protected function postProcessDomNode($field, DOMElement $fieldNode, JForm $form)
-	{
-		$fieldNode->setAttribute('hide_default', 'true');
-		$fieldNode->setAttribute('directory', '/images/' . $fieldNode->getAttribute('directory'));
-
-		return parent::postProcessDomNode($field, $fieldNode, $form);
-	}
 }
