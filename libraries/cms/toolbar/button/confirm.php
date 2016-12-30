a4d26bba19b8f3c937bfe0ec63ffe596232a7478
andrepereiradasilva <andrepereiradasilva@users.noreply.github.com>
2016-12-30 16:49:41 +0000

    Update confirm.php

--- libraries/cms/toolbar/button/confirm.php
+++ libraries/cms/toolbar/button/confirm.php
@@ -87,16 +87,13 @@ class JToolbarButtonConfirm extends JToolbarButton
 	 */
 	protected function _getCommand($msg, $name, $task, $list)
 	{
-		$message = JText::_('JLIB_HTML_PLEASE_MAKE_A_SELECTION_FROM_THE_LIST');
-		$message = addslashes($message);
+		JText::script('JLIB_HTML_PLEASE_MAKE_A_SELECTION_FROM_THE_LIST');
+
+		$cmd = "if (confirm('" . $msg . "')) { Joomla.submitbutton('" . $task . "'); }";
 
 		if ($list)
 		{
-			$cmd = "if (document.adminForm.boxchecked.value==0){alert('$message');}else{if (confirm('$msg')){Joomla.submitbutton('$task');}}";
-		}
-		else
-		{
-			$cmd = "if (confirm('$msg')){Joomla.submitbutton('$task');}";
+			$cmd = "if (document.adminForm.boxchecked.value == 0) { alert(Joomla.JText._('JLIB_HTML_PLEASE_MAKE_A_SELECTION_FROM_THE_LIST')); } else { " . $cmd . " }";
 		}
 
 		return $cmd;
