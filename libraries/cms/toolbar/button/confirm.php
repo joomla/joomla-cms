b5e83ede0a1666403f9b46872a34c401da7d71e8
andrepereiradasilva <andrepereiradasilva@users.noreply.github.com>
2016-12-30 17:26:56 +0000

    cs: space

--- libraries/cms/toolbar/button/confirm.php
+++ libraries/cms/toolbar/button/confirm.php
@@ -93,7 +93,7 @@ class JToolbarButtonConfirm extends JToolbarButton
 
 		if ($list)
 		{
-			$cmd  = "if (document.adminForm.boxchecked.value == 0) { alert(Joomla.JText._('JLIB_HTML_PLEASE_MAKE_A_SELECTION_FROM_THE_LIST')); }";
+			$cmd  = "if (document.adminForm.boxchecked.value == 0) { alert(Joomla.JText._('JLIB_HTML_PLEASE_MAKE_A_SELECTION_FROM_THE_LIST')); } ";
 			$cmd .= "else { " . $cmd . " }";
 		}
 
