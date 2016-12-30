62276eaa10cce38db7692043da70588f5ef46b3d
andrepereiradasilva <andrepereiradasilva@users.noreply.github.com>
2016-12-30 17:34:58 +0000

    final change

--- libraries/cms/toolbar/button/standard.php
+++ libraries/cms/toolbar/button/standard.php
@@ -98,8 +98,8 @@ class JToolbarButtonStandard extends JToolbarButton
 
 		if ($list)
 		{
-			$cmd  = "if (document.adminForm.boxchecked.value == 0) { alert(Joomla.JText._('JLIB_HTML_PLEASE_MAKE_A_SELECTION_FROM_THE_LIST')); } ";
-			$cmd .= "else { " . $cmd . " }";
+			$alert = "alert(Joomla.JText._('JLIB_HTML_PLEASE_MAKE_A_SELECTION_FROM_THE_LIST'));";
+			$cmd   = "if (document.adminForm.boxchecked.value == 0) { " . $alert . " } else { " . $cmd . " }";
 		}
 
 		return $cmd;
