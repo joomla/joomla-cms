9441988d23d925686b689c391da5eb0c19bece62
Nicola Galgano <optimus4joomla@gmail.com>
2016-12-30 08:44:39 +0100

    style the count like categories
    style the count like categories

--- administrator/modules/mod_stats_admin/tmpl/default.php
+++ administrator/modules/mod_stats_admin/tmpl/default.php
@@ -12,7 +12,7 @@ defined('_JEXEC') or die;
 <ul class="list-striped list-condensed stats-module<?php echo $moduleclass_sfx ?>">
 	<?php foreach ($list as $item) : ?>
 		<?php if(isset($item->link)) : ?>
-			<li><span class="icon-<?php echo $item->icon; ?>" title="<?php echo $item->title; ?>"></span> <a href ="<?php echo $item->link; ?>"><?php echo $item->title; ?></a> <?php echo $item->data; ?></li>
+			<li><span class="icon-<?php echo $item->icon; ?>" title="<?php echo $item->title; ?>"></span> <?php echo $item->title .' '; ?><a class="badge badge-info" href ="<?php echo $item->link; ?>"><?php echo $item->data; ?></a></li>
 		<?php else : ?>
 			<li><span class="icon-<?php echo $item->icon; ?>" title="<?php echo $item->title; ?>"></span> <?php echo $item->title; ?> <?php echo $item->data; ?></li>
 		<?php endif; ?>
