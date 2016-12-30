87789a2fd8bfe54e30ce9cde6a2fdd918409a8d1
Allon Moritz <allon.moritz@digital-peak.com>
2016-12-30 10:46:34 +0100

    Add url plugin

---
+++ plugins/fields/url/tmpl/url.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  com_fields
+ *
+ * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+defined('_JEXEC') or die;
+
+$value = $field->value;
+
+if ($value == '')
+{
+	return;
+}
+
+$attributes = '';
+
+if (!JUri::isInternal($value))
+{
+	$attributes = 'rel="nofollow" target="_blank"';
+}
+
+echo '<a href="' . $value . '" ' . $attributes . '>' . $value . '</a>';
