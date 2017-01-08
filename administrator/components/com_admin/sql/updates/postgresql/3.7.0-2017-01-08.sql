6b56a6b3d8d065400a9e20464c6328ee0b42d522
andrepereiradasilva <andrepereiradasilva@users.noreply.github.com>
2017-01-08 14:32:42 +0000

    postgresql modification updates

--- administrator/components/com_admin/sql/updates/postgresql/3.7.0-2017-01-08.sql
+++ administrator/components/com_admin/sql/updates/postgresql/3.7.0-2017-01-08.sql
@@ -1,1 +1,9 @@
 -- Normalize ucm_content_table default values.
+ALTER TABLE "#__ucm_content" ALTER COLUMN "core_title" SET DEFAULT '';
+ALTER TABLE "#__ucm_content" ALTER COLUMN "core_body" text SET DEFAULT '';
+ALTER TABLE "#__ucm_content" ALTER COLUMN "core_params" text SET DEFAULT '';
+ALTER TABLE "#__ucm_content" ALTER COLUMN "core_metadata" text SET DEFAULT '';
+ALTER TABLE "#__ucm_content" ALTER COLUMN "core_images" text SET DEFAULT '';
+ALTER TABLE "#__ucm_content" ALTER COLUMN "core_urls" text SET DEFAULT '';
+ALTER TABLE "#__ucm_content" ALTER COLUMN "core_metakey" text SET DEFAULT '';
+ALTER TABLE "#__ucm_content" ALTER COLUMN "core_metadesc" text SET DEFAULT '';
