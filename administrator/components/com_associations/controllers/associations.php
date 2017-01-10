b2fda8a8a8563b9f7a52100e95aafaa163b79b9e
zero-24 <zero-24@users.noreply.github.com>
2017-01-10 22:58:32 +0100

    on simple change

--- administrator/components/com_associations/controllers/associations.php
+++ administrator/components/com_associations/controllers/associations.php
@@ -7,7 +7,7 @@
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
-defined("_JEXEC") or die("Restricted access");
+defined('_JEXEC') or die;
 
 JLoader::register('AssociationsHelper', JPATH_ADMINISTRATOR . '/components/com_associations/helpers/associations.php');
 
