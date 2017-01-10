67d60001ef4680fc86318b28e877134d5577ec61
zero-24 <zero-24@users.noreply.github.com>
2017-01-10 23:00:06 +0100

    on simple change 2

--- administrator/components/com_associations/layouts/joomla/searchtools/default/bar.php
+++ administrator/components/com_associations/layouts/joomla/searchtools/default/bar.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package     Joomla.Administrator
- * @subpackage  Layout
+ * @subpackage  com_associations
  *
  * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
