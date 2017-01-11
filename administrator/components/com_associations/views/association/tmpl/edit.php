fc79b46342d4f5d66e0ff18cc354bc8394edef54
Robert Deutz <rdeutz@googlemail.com>
2017-01-11 21:13:14 +0100

    use typename directly

--- administrator/components/com_associations/views/association/tmpl/edit.php
+++ administrator/components/com_associations/views/association/tmpl/edit.php
@@ -27,16 +27,16 @@ $options = array(
 		data-hide-reference="<?php echo JText::_('COM_ASSOCIATIONS_EDIT_HIDE_REFERENCE'); ?>"><?php echo JText::_('COM_ASSOCIATIONS_EDIT_HIDE_REFERENCE'); ?>
 </button>
 
-<form action="<?php echo JRoute::_('index.php?option=com_associations&view=association&' . http_build_query($options)); ?>" method="post" name="adminForm" id="adminForm" data-associatedview="<?php echo $this->type->get('name'); ?>">
+<form action="<?php echo JRoute::_('index.php?option=com_associations&view=association&' . http_build_query($options)); ?>" method="post" name="adminForm" id="adminForm" data-associatedview="<?php echo $this->typeName; ?>">
 	<div class="sidebyside">
 		<div class="outer-panel" id="left-panel">
 			<div class="inner-panel">
 				<h3><?php echo JText::_('COM_ASSOCIATIONS_REFERENCE_ITEM'); ?></h3>
 				<iframe id="reference-association" name="reference-association"
-					src="<?php echo JRoute::_($this->editUri . '&task=' . $this->type->get('name') . '.edit&id=' . (int) $this->referenceId); ?>"
+					src="<?php echo JRoute::_($this->editUri . '&task=' . $this->typeName . '.edit&id=' . (int) $this->referenceId); ?>"
 					height="400" width="400"
 					data-action="edit"
-					data-item="<?php echo $this->type->get('name'); ?>"
+					data-item="<?php echo $this->typeName; ?>"
 					data-id="<?php echo $this->referenceId; ?>"
 					data-language="<?php echo $this->referenceLanguage; ?>"
 					data-editurl="<?php echo JRoute::_($this->editUri); ?>">
@@ -54,7 +54,7 @@ $options = array(
 					src="<?php echo $this->defaultTargetSrc; ?>"
 					height="400" width="400"
 					data-action="<?php echo $this->targetAction; ?>"
-					data-item="<?php echo $this->type->get('name'); ?>"
+					data-item="<?php echo $this->typeName; ?>"
 					data-id="<?php echo $this->targetId; ?>"
 					data-language="<?php echo $this->targetLanguage; ?>"
 					data-editurl="<?php echo JRoute::_($this->editUri); ?>">
