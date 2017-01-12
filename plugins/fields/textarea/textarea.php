f4e07d895496acea9be233e78c6bffb50e5fc429
wilsonge <georgejameswilson@googlemail.com>
2017-01-12 12:16:47 +0000

    It's 2017. Happy New Year

--- plugins/fields/textarea/textarea.php
+++ plugins/fields/textarea/textarea.php
@@ -3,7 +3,7 @@
  * @package     Joomla.Plugin
  * @subpackage  Fields.Textarea
  *
- * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
+ * @copyright   Copyright (C) 2005 - 2017 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
