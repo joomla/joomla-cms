8ddfbba7171c78f6d3dd4c41efc20f307cb5aed4
Allon Moritz <allon.moritz@digital-peak.com>
2017-01-16 08:00:41 +0100

    Disable filter on calendar form field

--- plugins/fields/calendar/calendar.php
+++ plugins/fields/calendar/calendar.php
@@ -18,4 +18,31 @@ JLoader::import('components.com_fields.libraries.fieldsplugin', JPATH_ADMINISTRA
  */
 class PlgFieldsCalendar extends FieldsPlugin
 {
+	/**
+	 * Transforms the field into an XML element and appends it as child on the given parent. This
+	 * is the default implementation of a field. Form fields which do support to be transformed into
+	 * an XML Element mut implemet the JFormDomfieldinterface.
+	 *
+	 * @param   stdClass    $field   The field.
+	 * @param   DOMElement  $parent  The field node parent.
+	 * @param   JForm       $form    The form.
+	 *
+	 * @return  DOMElement
+	 *
+	 * @since   3.7.0
+	 */
+	public function onCustomFieldsPrepareDom($field, DOMElement $parent, JForm $form)
+	{
+		$fieldNode = parent::onCustomFieldsPrepareDom($field, $parent, $form);
+
+		if (!$fieldNode)
+		{
+			return $fieldNode;
+		}
+
+		// Disable filter, otherwise the dates are moved in none UTC timezones
+		$fieldNode->setAttribute('filter', 'none');
+
+		return $fieldNode;
+	}
 }
