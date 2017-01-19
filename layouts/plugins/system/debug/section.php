67856f8fd533568e7ee2d57c9f889982208b7b3a
zero-24 <zero-24@users.noreply.github.com>
2017-01-19 19:23:33 +0100

    codestyle

--- layouts/plugins/system/debug/section.php
+++ layouts/plugins/system/debug/section.php
@@ -10,17 +10,15 @@
 defined('_JEXEC') or die;
 
 $section = $displayData['section'];
-$suffix = isset($displayData['suffix']) ? $displayData['suffix'] : '';
-$data = $displayData['data'];
-$title = isset($displayData['title']) ? $displayData['title'] : JText::_('PLG_DEBUG_' . strtoupper($section));
+$suffix  = isset($displayData['suffix']) ? $displayData['suffix'] : '';
+$data    = $displayData['data'];
+$title   = isset($displayData['title']) ? $displayData['title'] : JText::_('PLG_DEBUG_' . strtoupper($section));
 
 ?>
 
 <div class="dbg-header" onclick="toggleContainer('dbg_container_<?php echo $section, $suffix; ?>');">
     <a href="javascript:void(0);"><h3><?php echo $title; ?></h3></a>
 </div>
-
-
 <div style="display: none;" class="dbg-container" id="dbg_container_<?php echo $section, $suffix; ?>">
     <?php echo $this->sublayout($section, $data); ?>
 </div>
