3cf557b8e07acc1211a91fc8551acfa7d639d577
zero-24 <zero-24@users.noreply.github.com>
2017-01-19 19:32:21 +0100

    codestyle

--- layouts/plugins/system/debug/section/memory.php
+++ layouts/plugins/system/debug/section/memory.php
@@ -10,25 +10,25 @@
 defined('_JEXEC') or die;
 
 $memory = $displayData['memory'];
-$peak = $displayData['peak'];
-$limit = $displayData['limit'];
+$peak   = $displayData['peak'];
+$limit  = $displayData['limit'];
 
-$decimal = JText::_('DECIMALS_SEPARATOR');
+$decimal   = JText::_('DECIMALS_SEPARATOR');
 $thousands = JText::_('THOUSANDS_SEPARATOR');
-$bytes = JText::_('PLG_DEBUG_BYTES');
+$bytes     = JText::_('PLG_DEBUG_BYTES');
 
 ?>
 
 <div>
-    <span class="label label-default"><?php echo JHtml::_('number.bytes', $memory); ?></span>
-    (<span class="label label-default">
-        <?php echo number_format($memory, 0, $decimal, $thousands), ' ', $bytes; ?>
-    </span>)
+	<span class="label label-default"><?php echo JHtml::_('number.bytes', $memory); ?></span>
+	(<span class="label label-default">
+		<?php echo number_format($memory, 0, $decimal, $thousands), ' ', $bytes; ?>
+	</span>)
 </div>
 
 <div>
-    <span class="label label-default"><?php echo JHtml::_('number.bytes', $peak); ?></span>
-    (<span class="label label-default">
-        <?php echo number_format($peak, 0, $decimal, $thousands), ' ', $bytes; ?>
-    </span>)
+	<span class="label label-default"><?php echo JHtml::_('number.bytes', $peak); ?></span>
+	(<span class="label label-default">
+		<?php echo number_format($peak, 0, $decimal, $thousands), ' ', $bytes; ?>
+	</span>)
 </div>
