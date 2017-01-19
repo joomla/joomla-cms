c9f48b7625ed59a8894825c1e9734d87e33fd33b
zero-24 <zero-24@users.noreply.github.com>
2017-01-19 19:39:47 +0100

    Update profile.php

--- layouts/plugins/system/debug/section/profile.php
+++ layouts/plugins/system/debug/section/profile.php
@@ -1,3 +1,12 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  Layout
+ *
+ * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
 defined('_JEXEC') or die;
 
 $marks     = $displayData['marks'];
