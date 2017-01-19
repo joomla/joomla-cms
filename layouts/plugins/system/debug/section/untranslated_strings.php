6a0ca3fb2639d620a9424e1853548e2499cddb82
zero-24 <zero-24@users.noreply.github.com>
2017-01-19 19:47:45 +0100

    Update untranslated_strings.php

--- layouts/plugins/system/debug/section/untranslated_strings.php
+++ layouts/plugins/system/debug/section/untranslated_strings.php
@@ -13,19 +13,13 @@ $guesses = $displayData['guesses'];
 
 ?>
 <?php if (empty($guesses)) : ?>
-
-    <p><?php echo JText::_('JNONE'); ?></p>
-
+	<p><?php echo JText::_('JNONE'); ?></p>
 <?php else : ?>
-
-    <pre>
-    <?php foreach ($guesses as $file => $keys): ?>
-        <?php
-        echo '#',
-            $file ? JDebugHelper::formatLink($file) : JText::_('PLG_DEBUG_UNKNOWN_FILE'),
-            implode("\n", $keys);
-        ?>
-    <?php endforeach; ?>
-    </pre>
-
-<?php endif;
+	<pre>
+		<?php foreach ($guesses as $file => $keys): ?>
+			<?php echo '#'; ?>
+			<?php echo $file ? JDebugHelper::formatLink($file) : JText::_('PLG_DEBUG_UNKNOWN_FILE'); ?>
+			<?php echo implode("\n", $keys); ?>
+		<?php endforeach; ?>
+	</pre>
+<?php endif; ?>
