dee12e382d7dc83b803ad683092e1e0661e3a23b
zero-24 <zero-24@users.noreply.github.com>
2017-01-19 19:10:31 +0100

    Codestyle changes

--- layouts/plugins/system/debug/backtrace.php
+++ layouts/plugins/system/debug/backtrace.php
@@ -13,40 +13,31 @@ $backtrace = $displayData['backtrace'];
 
 if (!is_array($backtrace))
 {
-    return;
+	return;
 }
 
 $backtrace = array_reverse($backtrace);
-
 ?>
 
 <table cellpadding="0" cellspacing="0">
-
-    <tr>
-        <td colspan="3"><strong>Call stack</strong></td>
-    </tr>
-
-    <tr>
-        <th>#</th>
-        <th>Function</th>
-        <th>Location</th>
-    </tr>
-
-    <?php foreach ($backtrace as $i => $entry) : ?>
-        <?php $link = isset($entry['file']) ? JDebugHelper::formatLink($entry['file'], $entry['line']) : '&#160;'; ?>
-
-        <tr>
-            <td><?php echo $i + 1; ?></td>
-
-    		<?php if (isset($entry['class'])) : ?>
-                <td><?php echo $entry['class'], $entry['type'], $entry['function']; ?>()</td>
-    		<?php else : ?>
-                <td><?php echo $entry['function']; ?>()</td>';
-            <?php endif; ?>
-
-            <td><?php echo $link; ?></td>
-        </tr>
-
+	<tr>
+		<td colspan="3"><strong>Call stack</strong></td>
+	</tr>
+	<tr>
+		<th>#</th>
+		<th>Function</th>
+		<th>Location</th>
+	</tr>
+	<?php foreach ($backtrace as $i => $entry) : ?>
+		<?php $link = isset($entry['file']) ? JDebugHelper::formatLink($entry['file'], $entry['line']) : '&#160;'; ?>
+		<tr>
+			<td><?php echo $i + 1; ?></td>
+			<?php if (isset($entry['class'])) : ?>
+				<td><?php echo $entry['class'], $entry['type'], $entry['function']; ?>()</td>
+			<?php else : ?>
+				<td><?php echo $entry['function']; ?>()</td>';
+			<?php endif; ?>
+			<td><?php echo $link; ?></td>
+		</tr>
 	<?php endforeach; ?>
-
-</table>';
+</table>
