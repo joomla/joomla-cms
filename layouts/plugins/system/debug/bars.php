f1cc09f3fe964291ed7a72c65a19f34cf4384e23
zero-24 <zero-24@users.noreply.github.com>
2017-01-19 19:13:43 +0100

    Update bars.php

--- layouts/plugins/system/debug/bars.php
+++ layouts/plugins/system/debug/bars.php
@@ -9,38 +9,29 @@
 
 defined('_JEXEC') or die;
 
-$bars = is_array($displayData['bars']) ? $displayData['bars'] : array();
+$bars  = is_array($displayData['bars']) ? $displayData['bars'] : array();
 $class = isset($displayData['class']) ? $displayData['class'] : '';
-$id = isset($displayData['id']) ? $displayData['id'] : null;
+$id    = isset($displayData['id']) ? $displayData['id'] : null;
 
 ?>
 
 <div class="progress dbg-bars dbg-bars-<?php echo $class; ?>">
-    <?php foreach ($bars as $i => $bar) : ?>
-    	<?php if (isset($bar->pre) && $bar->pre) : ?>
-    		<div class="dbg-bar-spacer" style="width:<?php echo $bar->pre; ?>%;"></div>
-        <?php endif; ?>
-
-        <?php
-    	$barClass = trim('bar dbg-bar progress-bar ' . (isset($bar->class) ? $bar->class : ''));
-
-    	if ($id !== null && $i == $id)
-    	{
-    		$barClass .= ' dbg-bar-active';
-    	}
-
-    	$tip = '';
-
-    	if (isset($bar->tip) && $bar->tip)
-    	{
-    		$barClass .= ' hasTooltip';
-    		$tip = JHtml::tooltipText($bar->tip, '', 0);
-    	}
-        ?>
-
-    	<a class="bar dbg-bar <?php echo $barClass; ?>"
-            title="<?php echo $tip; ?>"
-            style="width: <?php echo $bar->width; ?>%;"
-            href="#dbg-<?php echo $class, '-', ($i + 1); ?>"></a>
-    <?php endforeach; ?>
+	<?php foreach ($bars as $i => $bar) : ?>
+		<?php if (isset($bar->pre) && $bar->pre) : ?>
+			<div class="dbg-bar-spacer" style="width:<?php echo $bar->pre; ?>%;"></div>
+		<?php endif; ?>
+		<?php $barClass = trim('bar dbg-bar progress-bar ' . (isset($bar->class) ? $bar->class : '')); ?>
+		<?php if ($id !== null && $i == $id) : ?>
+			<?php $barClass .= ' dbg-bar-active'; ?>
+		<?php endif; ?>
+		<?php $tip = ''; ?>
+		<?php if (isset($bar->tip) && $bar->tip) : ?>
+			<?php $barClass .= ' hasTooltip'; ?>
+			<?php $tip = JHtml::tooltipText($bar->tip, '', 0); ?>
+		<?php endif; ?>
+		<a class="bar dbg-bar <?php echo $barClass; ?>"
+			title="<?php echo $tip; ?>"
+			style="width: <?php echo $bar->width; ?>%;"
+			href="#dbg-<?php echo $class, '-', ($i + 1); ?>"></a>
+	<?php endforeach; ?>
 </div>
