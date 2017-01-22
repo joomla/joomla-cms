e9ae9700a5c7933fe06b4ec30cc8e345e3873329
Allon Moritz <allon.moritz@digital-peak.com>
2017-01-22 13:11:48 +0100

    Move JControllerLegacy to namespace

--- libraries/classmap.php
+++ libraries/classmap.php
@@ -6,15 +6,17 @@
  * @license    GNU General Public License version 2 or later; see LICENSE
  */
 
-JLoader::registerAlias('JRegistry',           '\\Joomla\\Registry\\Registry', '4.0');
-JLoader::registerAlias('JRegistryFormat',     '\\Joomla\\Registry\\AbstractRegistryFormat', '4.0');
-JLoader::registerAlias('JRegistryFormatIni',  '\\Joomla\\Registry\\Format\\Ini', '4.0');
-JLoader::registerAlias('JRegistryFormatJson', '\\Joomla\\Registry\\Format\\Json', '4.0');
-JLoader::registerAlias('JRegistryFormatPhp',  '\\Joomla\\Registry\\Format\\Php', '4.0');
-JLoader::registerAlias('JRegistryFormatXml',  '\\Joomla\\Registry\\Format\\Xml', '4.0');
-JLoader::registerAlias('JStringInflector',    '\\Joomla\\String\\Inflector', '4.0');
-JLoader::registerAlias('JStringNormalise',    '\\Joomla\\String\\Normalise', '4.0');
+JLoader::registerAlias('JRegistry',             '\\Joomla\\Registry\\Registry', '4.0');
+JLoader::registerAlias('JRegistryFormat',       '\\Joomla\\Registry\\AbstractRegistryFormat', '4.0');
+JLoader::registerAlias('JRegistryFormatIni',    '\\Joomla\\Registry\\Format\\Ini', '4.0');
+JLoader::registerAlias('JRegistryFormatJson',   '\\Joomla\\Registry\\Format\\Json', '4.0');
+JLoader::registerAlias('JRegistryFormatPhp',    '\\Joomla\\Registry\\Format\\Php', '4.0');
+JLoader::registerAlias('JRegistryFormatXml',    '\\Joomla\\Registry\\Format\\Xml', '4.0');
+JLoader::registerAlias('JStringInflector',      '\\Joomla\\String\\Inflector', '4.0');
+JLoader::registerAlias('JStringNormalise',      '\\Joomla\\String\\Normalise', '4.0');
 JLoader::registerAlias('JApplicationWebClient', '\\Joomla\\Application\\Web\\WebClient', '4.0');
-JLoader::registerAlias('JData',               '\\Joomla\\Data\\DataObject', '4.0');
-JLoader::registerAlias('JDataSet',            '\\Joomla\\Data\\DataSet', '4.0');
-JLoader::registerAlias('JDataDumpable',       '\\Joomla\\Data\\DumpableInterface', '4.0');
+JLoader::registerAlias('JData',                 '\\Joomla\\Data\\DataObject', '4.0');
+JLoader::registerAlias('JDataSet',              '\\Joomla\\Data\\DataSet', '4.0');
+JLoader::registerAlias('JDataDumpable',         '\\Joomla\\Data\\DumpableInterface', '4.0');
+
+JLoader::registerAlias('JControllerLegacy',     '\\Joomla\\Controller\\Base', '4.0');
--- libraries/cms.php
+++ libraries/cms.php
@@ -29,6 +29,9 @@ if (!class_exists('JLoader'))
 // Register the library base path for CMS libraries.
 JLoader::registerPrefix('J', JPATH_PLATFORM . '/cms', false, true);
 
+// Register the Joomla namespace in libraries
+JLoader::registerNamespace('Joomla', JPATH_LIBRARIES . '/src/Joomla', false, false, 'psr4');
+
 // Create the Composer autoloader
 $loader = require JPATH_LIBRARIES . '/vendor/autoload.php';
 $loader->unregister();
