de4fa376d9f3a9cd686af42da836791073c26f62
wilsonge <georgejameswilson@googlemail.com>
2017-01-25 13:17:42 +0000

    Codestyle

--- libraries/cms/component/router/rules/nomenu.php
+++ libraries/cms/component/router/rules/nomenu.php
@@ -62,8 +62,10 @@ class JComponentRouterRulesNomenu implements JComponentRouterRulesInterface
 	{
 		$active = $this->router->menu->getActive();
 
-		// We do this if we don't have an active menu item OR we have a active menu item but extra segments (i.e.
-		// when we've piped component segments onto an existing menu item)
+		/**
+		 * We do this if we don't have an active menu item OR we have a active menu item but extra segments (i.e.
+		 * when we've piped component segments onto an existing menu item)
+		 */
 		if (!is_object($active) || (!empty($segments) && is_object($active)))
 		{
 			$views = $this->router->getViews();
@@ -101,8 +103,8 @@ class JComponentRouterRulesNomenu implements JComponentRouterRulesInterface
 		{
 			$item = $this->router->menu->getItem($query['Itemid']);
 
-			if ((!isset($query['option']) || ($item && $item->query['option'] == $query['option'])) &&
-				(isset($query['view']) === false || $item->query['view'] !== key($path)))
+			if ((!isset($query['option']) || ($item && $item->query['option'] == $query['option']))
+				&& (isset($query['view']) === false || $item->query['view'] !== key($path)))
 			{
 				$menu_found = true;
 			}
