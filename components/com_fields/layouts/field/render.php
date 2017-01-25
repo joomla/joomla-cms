1a48a92169bdf4dcd7c8fa306618c9a006a2f7de
brianpeat <brian@thepeatgroup.com>
2017-01-25 09:22:27 -0600

    missed a semicolon

--- components/com_fields/layouts/field/render.php
+++ components/com_fields/layouts/field/render.php
@@ -29,6 +29,6 @@ if ($value == '')
 <dd class="field-entry <?php echo $class; ?>" id="field-entry-<?php echo $field->id; ?>">
 	<?php if ($hidelabel == 0) : ?>
 	<span class="field-label"><?php echo htmlentities($label, ENT_QUOTES | ENT_IGNORE, 'UTF-8'); ?>: </span>
-	<?php endif ?>
+	<?php endif; ?>
 	<span class="field-value"><?php echo $value; ?></span>
 </dd>
