93d41cbc3d798736fdcbf124049b46ea48341dd8
Allon Moritz <allon.moritz@digital-peak.com>
2017-01-26 08:37:53 +0100

    Default the gallery directory when empty

--- plugins/fields/gallery/gallery.php
+++ plugins/fields/gallery/gallery.php
@@ -18,4 +18,38 @@ JLoader::import('components.com_fields.libraries.fieldsplugin', JPATH_ADMINISTRA
  */
 class PlgFieldsGallery extends FieldsPlugin
 {
+	/**
+	 * Transforms the field into an XML element and appends it as child on the given parent. This
+	 * is the default implementation of a field. Form fields which do support to be transformed into
+	 * an XML Element mut implemet the JFormDomfieldinterface.
+	 *
+	 * @param   stdClass    $field   The field.
+	 * @param   DOMElement  $parent  The field node parent.
+	 * @param   JForm       $form    The form.
+	 *
+	 * @return  DOMElement
+	 *
+	 * @since   3.7.0
+	 */
+	public function onCustomFieldsPrepareDom($field, DOMElement $parent, JForm $form)
+	{
+		$fieldNode = parent::onCustomFieldsPrepareDom($field, $parent, $form);
+
+		if (!$fieldNode)
+		{
+			return $fieldNode;
+		}
+
+		$directory = $fieldNode->getAttribute('directory');
+
+		// Can be empty when the plugin doesn't get saved.
+		if (!$directory)
+		{
+			$directory = 'images';
+		}
+
+		$fieldNode->setAttribute('directory', $directory);
+
+		return $fieldNode;
+	}
 }
