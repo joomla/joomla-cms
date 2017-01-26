2aba65c7439937a610f2d18ac135064342fc64ea
Allon Moritz <allon.moritz@digital-peak.com>
2017-01-26 13:19:35 +0100

    Remove package information in docblock

--- libraries/src/Joomla/Cms/Model/Model.php
+++ libraries/src/Joomla/Cms/Model/Model.php
@@ -1,7 +1,6 @@
 <?php
 /**
- * @package     Joomla.Cms
- * @subpackage  Model
+ * Joomla! Content Management System
  *
  * @copyright   Copyright (C) 2005 - 2017 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE
--- libraries/src/Joomla/Cms/Table/Observer/ContentHistory.php
+++ libraries/src/Joomla/Cms/Table/Observer/ContentHistory.php
@@ -1,7 +1,6 @@
 <?php
 /**
- * @package     Joomla.Platform
- * @subpackage  Table
+ * Joomla! Content Management System
  *
  * @copyright   Copyright (C) 2005 - 2017 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE
