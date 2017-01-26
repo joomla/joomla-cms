1f3316007c6c025bc0c3646dc05e5db052db7fdf
Dimitri Joukoff <Dimitri.Joukoff@GriffithUni.edu.au>
2017-01-27 03:11:12 +1000

    Another attempt at fixing coding style errors.

--- administrator/components/com_menus/helpers/html/menus.php
+++ administrator/components/com_menus/helpers/html/menus.php
@@ -256,18 +256,18 @@ abstract class MenusHtmlMenus
 	}
 	
     /**
-	 * Returns an isInheritable state on a grid
-	 *
-	 * @param   integer       $value       The state value.
-	 * @param   integer       $i           The row index
-	 * @param   boolean       $enabled     An optional setting for access control on the action.
-	 * @param   string        $checkbox    An optional prefix for checkboxes.
-	 *
-	 * @return  string        The HTML markup
-	 *
-	 * @see     JHtmlJGrid::state()
-	 * @since   2017-01-25
-	 */
+     * Returns an isInheritable state on a grid
+     *
+     * @param   integer       $value       The state value.
+     * @param   integer       $i           The row index
+     * @param   boolean       $enabled     An optional setting for access control on the action.
+     * @param   string        $checkbox    An optional prefix for checkboxes.
+     *
+     * @return  string        The HTML markup
+     *
+     * @see     JHtmlJGrid::state()
+     * @since   2017-01-25
+     */
 	public static function isinheritable($value, $i, $enabled = true, $checkbox = 'cb')
 	{
 	
