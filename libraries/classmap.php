dd4837064e5eb72fc8b57dafd84916d459e1a4e0
Allon Moritz <allon.moritz@digital-peak.com>
2017-01-27 10:57:57 +0100

    Move JTableUser to namespace

--- libraries/classmap.php
+++ libraries/classmap.php
@@ -39,6 +39,7 @@ JLoader::registerAlias('JTableExtension',              '\\Joomla\\Cms\\Table\\Ex
 JLoader::registerAlias('JTableLanguage',               '\\Joomla\\Cms\\Table\\Language', '4.0');
 JLoader::registerAlias('JTableUpdate',                 '\\Joomla\\Cms\\Table\\Update', '4.0');
 JLoader::registerAlias('JTableUpdatesite',             '\\Joomla\\Cms\\Table\\UpdateSite', '4.0');
+JLoader::registerAlias('JTableUser',                   '\\Joomla\\Cms\\Table\\User', '4.0');
 JLoader::registerAlias('JTableObserver',               '\\Joomla\\Cms\\Table\\Observer\\AbstractObserver', '4.0');
 JLoader::registerAlias('JTableObserverContenthistory', '\\Joomla\\Cms\\Table\\Observer\\ContentHistory', '4.0');
 JLoader::registerAlias('JTableObserverTags',           '\\Joomla\\Cms\\Table\\Observer\\Tags', '4.0');
