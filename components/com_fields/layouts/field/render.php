b33e7e9c3c4572cc6eaaca0fa7caa8ca0a4d1f12
brianpeat <brian@thepeatgroup.com>
2017-01-26 18:40:33 -0600

    Changing switch to Show/Hide

--- components/com_fields/layouts/field/render.php
+++ components/com_fields/layouts/field/render.php
@@ -17,7 +17,7 @@ $field = $displayData['field'];
 $label = JText::_($field->label);
 $value = $field->value;
 $class = $field->params->get('render_class');
-$hidelabel = $field->params->get('hidelabel');
+$showlabel = $field->params->get('showlabel');
 
 if ($value == '')
 {
@@ -27,7 +27,7 @@ if ($value == '')
 ?>
 
 <dd class="field-entry <?php echo $class; ?>" id="field-entry-<?php echo $field->id; ?>">
-	<?php if ($hidelabel == 0) : ?>
+	<?php if ($showlabel == 1) : ?>
 	<span class="field-label"><?php echo htmlentities($label, ENT_QUOTES | ENT_IGNORE, 'UTF-8'); ?>: </span>
 	<?php endif; ?>
 	<span class="field-value"><?php echo $value; ?></span>
