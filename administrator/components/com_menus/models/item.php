8316dd9a1a61f3c74cf43c958085db7d2697c396
marrouchi <marrouchi.mohamed@gmail.com>
2017-01-27 12:45:31 +0100

    CodeSniffer fix : Blank line before new comment

--- administrator/components/com_menus/models/item.php
+++ administrator/components/com_menus/models/item.php
@@ -769,6 +769,7 @@ class MenusModelItem extends JModelAdmin
 		{
 			// Note that all request arguments become reserved parameter names.
 			$result->params = array_merge($result->params, $args);
+			
 			// Add Itemid to the alias link value
 			$result->link .= $result->id;
 		}
