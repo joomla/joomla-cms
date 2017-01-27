ec119b930ccec1e4e17d80440e072e13974cc1c1
Dimitri Joukoff <Dimitri.Joukoff@GriffithUni.edu.au>
2017-01-27 16:54:43 +1000

    Added an additional line to the SELECT statement to prevent line length overrun.

--- libraries/cms/menu/site.php
+++ libraries/cms/menu/site.php
@@ -79,7 +79,8 @@ class JMenuSite extends JMenu
 				{
 					$query = $db->getQuery(true)
 						->select('m.id, m.menutype, m.title, m.alias, m.note, m.path AS route, m.link, m.type, m.level, m.language')
-						->select($db->quoteName('m.browserNav') . ', m.access, m.inheritable, m.params, m.home, m.img, m.template_style_id, m.component_id, m.parent_id')
+						->select($db->quoteName('m.browserNav') . ', m.access, m.inheritable, m.params, m.home, m.img, m.template_style_id')
+						->select('m.component_id, m.parent_id')
 						->select('e.element as component')
 						->from('#__menu AS m')
 						->join('LEFT', '#__extensions AS e ON m.component_id = e.extension_id')
