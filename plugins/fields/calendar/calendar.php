f3682a01b6db085d5b92e3f759261d6b78fb792a
Thomas Hunziker <werbemails@bakual.ch>
2017-01-29 14:19:31 +0100

    Codestyle

--- plugins/fields/calendar/calendar.php
+++ plugins/fields/calendar/calendar.php
@@ -18,19 +18,19 @@ JLoader::import('components.com_fields.libraries.fieldsplugin', JPATH_ADMINISTRA
  */
 class PlgFieldsCalendar extends FieldsPlugin
 {
-/**
- * Transforms the field into an XML element and appends it as child on the given parent. This
- * is the default implementation of a field. Form fields which do support to be transformed into
- * an XML Element mut implemet the JFormDomfieldinterface.
- *
- * @param   stdClass    $field   The field.
- * @param   DOMElement  $parent  The field node parent.
- * @param   JForm       $form    The form.
- *
- * @return  DOMElement
- *
- * @since   __DEPLOY_VERSION__
- */
+	/**
+	 * Transforms the field into an XML element and appends it as child on the given parent. This
+	 * is the default implementation of a field. Form fields which do support to be transformed into
+	 * an XML Element mut implemet the JFormDomfieldinterface.
+	 *
+	 * @param   stdClass    $field   The field.
+	 * @param   DOMElement  $parent  The field node parent.
+	 * @param   JForm       $form    The form.
+	 *
+	 * @return  DOMElement
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 */
 	public function onCustomFieldsPrepareDom($field, DOMElement $parent, JForm $form)
 	{
 		$fieldNode = parent::onCustomFieldsPrepareDom($field, $parent, $form);
