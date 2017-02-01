a4ee85fed530d57b1d0fdc3fd4d8112346174c7f
Lodder <lodder15@hotmail.co.uk>
2017-02-01 13:40:39 +0000

    [4.0 - com_fields] Migrate to Bootstrap 4 (#13842)
    * Migrate com_fields to BS4
    
    * Revert JFactory fix for Allon's PR
    
    * Fix chosen selector

--- administrator/components/com_fields/views/fields/tmpl/modal.php
+++ administrator/components/com_fields/views/fields/tmpl/modal.php
@@ -18,7 +18,7 @@ if ($app->isSite())
 JHtml::addIncludePath(JPATH_COMPONENT . '/helpers/html');
 JHtml::_('bootstrap.tooltip');
 JHtml::_('behavior.framework', true);
-JHtml::_('formbehavior.chosen', 'select');
+JHtml::_('formbehavior.chosen', '.advancedSelect');
 
 $function  = $app->input->getCmd('function', 'jSelectField');
 $listOrder = $this->escape($this->state->get('list.ordering'));
@@ -43,14 +43,14 @@ $listDirn  = $this->escape($this->state->get('list.direction'));
 					<span class="icon-remove"></span><?php echo JText::_('JSEARCH_FILTER_CLEAR'); ?></button>
 			</div>
 		</div>
-		<hr class="hr-condensed" />
+		<hr>
 		<div class="filters">
-			<select name="filter_access" class="input-medium" onchange="this.form.submit()">
+			<select name="filter_access" onchange="this.form.submit()">
 				<option value=""><?php echo JText::_('JOPTION_SELECT_ACCESS');?></option>
 				<?php echo JHtml::_('select.options', JHtml::_('access.assetgroups'), 'value', 'text', $this->state->get('filter.access')); ?>
 			</select>
 
-			<select name="filter_published" class="input-medium" onchange="this.form.submit()">
+			<select name="filter_published" onchange="this.form.submit()">
 				<option value=""><?php echo JText::_('JOPTION_SELECT_PUBLISHED'); ?></option>
 				<?php echo JHtml::_('select.options', JHtml::_('jgrid.publishedOptions'), 'value', 'text', $this->state->get('filter.published'), true); ?>
 			</select>
@@ -68,14 +68,14 @@ $listDirn  = $this->escape($this->state->get('list.direction'));
 	</fieldset>
 
 	<?php if (empty($this->items)) : ?>
-		<div class="alert alert-no-items">
+		<div class="alert alert-warning alert-no-items">
 			<?php echo JText::_('JGLOBAL_NO_MATCHING_RESULTS'); ?>
 		</div>
 	<?php else : ?>
-		<table class="table table-striped table-condensed">
+		<table class="table table-striped table-sm">
 			<thead>
 				<tr>
-					<th width="1%" class="nowrap center">
+					<th width="1%" class="nowrap text-center">
 						<?php echo JHtml::_('grid.sort', 'JSTATUS', 'a.published', $listDirn, $listOrder); ?>
 					</th>
 					<th class="title">
@@ -84,13 +84,13 @@ $listDirn  = $this->escape($this->state->get('list.direction'));
 					<th>
 						<?php echo JHtml::_('grid.sort', 'COM_FIELDS_FIELD_TYPE_LABEL', 'a.type', $listDirn, $listOrder); ?>
 					</th>
-					<th width="15%" class="center nowrap">
+					<th width="15%" class="text-center nowrap">
 						<?php echo JHtml::_('grid.sort', 'JGRID_HEADING_ACCESS', 'access_level', $listDirn, $listOrder); ?>
 					</th>
-					<th width="5%" class="center nowrap">
+					<th width="5%" class="text-center nowrap">
 						<?php echo JHtml::_('grid.sort', 'JGRID_HEADING_LANGUAGE', 'language', $listDirn, $listOrder); ?>
 					</th>
-					<th width="1%" class="center nowrap">
+					<th width="1%" class="text-center nowrap">
 						<?php echo JHtml::_('grid.sort', 'JGRID_HEADING_ID', 'a.id', $listDirn, $listOrder); ?>
 					</th>
 				</tr>
@@ -105,7 +105,7 @@ $listDirn  = $this->escape($this->state->get('list.direction'));
 			<tbody>
 			<?php foreach ($this->items as $i => $item) : ?>
 				<tr class="row<?php echo $i % 2; ?>">
-					<td class="center">
+					<td class="text-center">
 						<?php echo JHtml::_('jgrid.published', $item->state, $i, 'fields.', false); ?>
 					</td>
 					<td>
@@ -142,17 +142,17 @@ $listDirn  = $this->escape($this->state->get('list.direction'));
 						<?php endif; ?>
 						<?php echo $this->escape(JText::_($label)); ?>
 					</td>
-					<td class="center">
+					<td class="text-center">
 						<?php echo $this->escape($item->access_level); ?>
 					</td>
-					<td class="center">
+					<td class="text-center">
 						<?php if ($item->language == '*'):?>
 							<?php echo JText::alt('JALL', 'language'); ?>
 						<?php else:?>
 							<?php echo $item->language_title ? $this->escape($item->language_title) : JText::_('JUNDEFINED'); ?>
 						<?php endif;?>
 					</td>
-					<td class="center">
+					<td class="text-center">
 						<?php echo (int) $item->id; ?>
 					</td>
 				</tr>
@@ -162,10 +162,10 @@ $listDirn  = $this->escape($this->state->get('list.direction'));
 	<?php endif; ?>
 
 	<div>
-		<input type="hidden" name="task" value="" />
-		<input type="hidden" name="boxchecked" value="0" />
-		<input type="hidden" name="filter_order" value="<?php echo $listOrder; ?>" />
-		<input type="hidden" name="filter_order_Dir" value="<?php echo $listDirn; ?>" />
+		<input type="hidden" name="task" value="">
+		<input type="hidden" name="boxchecked" value="0">
+		<input type="hidden" name="filter_order" value="<?php echo $listOrder; ?>">
+		<input type="hidden" name="filter_order_Dir" value="<?php echo $listDirn; ?>">
 		<?php echo JHtml::_('form.token'); ?>
 	</div>
 </form>
