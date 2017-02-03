c4f99b7f8dd90cf680c4ce0ee77862569b4ce1d4
Allon Moritz <allon.moritz@digital-peak.com>
2017-02-03 07:57:41 +0100

    Update script.php for the new content plugin

--- administrator/components/com_admin/script.php
+++ administrator/components/com_admin/script.php
@@ -504,6 +504,7 @@ class JoomlaInstallerScript
 			array('plugin', 'url', 'fields', 0),
 			array('plugin', 'user', 'fields', 0),
 			array('plugin', 'usergrouplist', 'fields', 0),
+			array('plugin', 'fields', 'content', 0),
 
 			// Templates
 			array('template', 'beez3', '', 0),
