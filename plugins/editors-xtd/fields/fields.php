d6a446f5e1fa297cd1e66b91f71ef068dbf56a49
Jean-Marie Simonet <infografjms@gmail.com>
2017-02-06 09:19:49 +0100

    Module and fields editor-xtd buttons should only display when their related plugins are active.

--- plugins/editors-xtd/fields/fields.php
+++ plugins/editors-xtd/fields/fields.php
@@ -35,31 +35,35 @@ class PlgButtonFields extends JPlugin
 	 */
 	public function onDisplay($name)
 	{
-		// Register FieldsHelper
-		JLoader::register('FieldsHelper', JPATH_ADMINISTRATOR . '/components/com_fields/helpers/fields.php');
-
-		// Guess the field context based on view.
-		$jinput = JFactory::getApplication()->input;
-		$context = $jinput->get('option') . '.' . $jinput->get('view');
-
-		// Validate context.
-		$context = implode('.', FieldsHelper::extract($context));
-		if (!FieldsHelper::getFields($context))
+		if (JPluginHelper::isEnabled('content', 'fields') && JPluginHelper::isEnabled('system', 'fields'))
 		{
-			return;
+			// Register FieldsHelper
+			JLoader::register('FieldsHelper', JPATH_ADMINISTRATOR . '/components/com_fields/helpers/fields.php');
+
+			// Guess the field context based on view.
+			$jinput = JFactory::getApplication()->input;
+			$context = $jinput->get('option') . '.' . $jinput->get('view');
+
+			// Validate context.
+			$context = implode('.', FieldsHelper::extract($context));
+
+			if (!FieldsHelper::getFields($context))
+			{
+				return;
+			}
+
+			$link = 'index.php?option=com_fields&amp;view=fields&amp;layout=modal&amp;tmpl=component&amp;context='
+				. $context . '&amp;editor=' . $name . '&amp;' . JSession::getFormToken() . '=1';
+
+			$button          = new JObject;
+			$button->modal   = true;
+			$button->class   = 'btn';
+			$button->link    = $link;
+			$button->text    = JText::_('PLG_EDITORS-XTD_FIELDS_BUTTON_FIELD');
+			$button->name    = 'puzzle';
+			$button->options = "{handler: 'iframe', size: {x: 800, y: 500}}";
+
+			return $button;
 		}
-
-		$link = 'index.php?option=com_fields&amp;view=fields&amp;layout=modal&amp;tmpl=component&amp;context='
-			. $context . '&amp;editor=' . $name . '&amp;' . JSession::getFormToken() . '=1';
-
-		$button          = new JObject;
-		$button->modal   = true;
-		$button->class   = 'btn';
-		$button->link    = $link;
-		$button->text    = JText::_('PLG_EDITORS-XTD_FIELDS_BUTTON_FIELD');
-		$button->name    = 'puzzle';
-		$button->options = "{handler: 'iframe', size: {x: 800, y: 500}}";
-
-		return $button;
 	}
 }
--- plugins/editors-xtd/module/module.php
+++ plugins/editors-xtd/module/module.php
@@ -39,24 +39,27 @@ class PlgButtonModule extends JPlugin
 		 * Use the built-in element view to select the module.
 		 * Currently uses blank class.
 		 */
-		$user  = JFactory::getUser();
-
-		if ($user->authorise('core.create', 'com_modules')
-			|| $user->authorise('core.edit', 'com_modules')
-			|| $user->authorise('core.edit.own', 'com_modules'))
+		if (JPluginHelper::isEnabled('content', 'loadmodule'))
 		{
-			$link = 'index.php?option=com_modules&amp;view=modules&amp;layout=modal&amp;tmpl=component&amp;editor='
-					. $name . '&amp;' . JSession::getFormToken() . '=1';
+			$user  = JFactory::getUser();
 
-			$button          = new JObject;
-			$button->modal   = true;
-			$button->class   = 'btn';
-			$button->link    = $link;
-			$button->text    = JText::_('PLG_MODULE_BUTTON_MODULE');
-			$button->name    = 'file-add';
-			$button->options = "{handler: 'iframe', size: {x: 800, y: 500}}";
+			if ($user->authorise('core.create', 'com_modules')
+				|| $user->authorise('core.edit', 'com_modules')
+				|| $user->authorise('core.edit.own', 'com_modules'))
+			{
+				$link = 'index.php?option=com_modules&amp;view=modules&amp;layout=modal&amp;tmpl=component&amp;editor='
+						. $name . '&amp;' . JSession::getFormToken() . '=1';
 
-			return $button;
+				$button          = new JObject;
+				$button->modal   = true;
+				$button->class   = 'btn';
+				$button->link    = $link;
+				$button->text    = JText::_('PLG_MODULE_BUTTON_MODULE');
+				$button->name    = 'file-add';
+				$button->options = "{handler: 'iframe', size: {x: 800, y: 500}}";
+
+				return $button;
+			}
 		}
 	}
 }
