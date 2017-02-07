3c4288e20749a3f7a4c4bce9bd3d889f41c759cf
Robert Deutz <rdeutz@googlemail.com>
2017-02-07 15:18:23 +0100

    add a reset head data function

--- libraries/joomla/document/html.php
+++ libraries/joomla/document/html.php
@@ -146,6 +146,76 @@ class JDocumentHtml extends JDocument
 	}
 
 	/**
+	 * Reset the HTML document head data
+	 *
+	 * @param   mixed  $types  type or types of the heads elements to reset
+	 *
+	 * @return  JDocumentHTML  instance of $this to allow chaining
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 */
+	public function resetHeadData($types = null)
+	{
+		if (is_null($types))
+		{
+			$this->title        = '';
+			$this->description  = '';
+			$this->link         = '';
+			$this->_metaTags    = array();
+			$this->_links       = array();
+			$this->_styleSheets = array();
+			$this->_style       = array();
+			$this->_scripts     = array();
+			$this->_script      = array();
+			$this->_custom      = array();
+		}
+
+		if (is_array($types))
+		{
+			foreach ($types as $type)
+			{
+				$this->resetHeadDatum($type);
+			}
+		}
+
+		if (is_string($types))
+		{
+			$this->resetHeadDatum($types);
+		}
+
+		return $this;
+	}
+
+	/**
+	 * Reset a part the HTML document head data
+	 *
+	 * @param   string  $type  type of the heads elements to reset
+	 *
+	 * @return  void
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 */
+	private function resetHeadDatum($type)
+	{
+		switch ($type)
+		{
+			case 'title':
+			case 'description':
+			case 'link':
+				$this->{$type} = '';
+
+			case '_metaTags':
+			case '_links':
+			case '_styleSheets':
+			case '_style':
+			case '_scripts':
+			case '_script':
+			case '_custom':
+				$this->{$type} = array();
+		}
+	}
+
+	/**
 	 * Set the HTML document head data
 	 *
 	 * @param   array  $data  The document head data in array form
