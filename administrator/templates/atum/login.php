6bf73203b97a3ff1471f899d7e81078534d7c336
C-Lodder <lodder15@hotmail.co.uk>
2017-02-09 15:00:01 +0000

    Add 'to-do'

--- administrator/templates/atum/login.php
+++ administrator/templates/atum/login.php
@@ -51,6 +51,7 @@ $sitename = $app->get('sitename');
 <head>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<?php // TO-DO: sync with _variables.scss ?>
 	<meta name="theme-color" content="#1c3d5c" />
 	<jdoc:include type="head" />
 	<style>
