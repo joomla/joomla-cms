0fb5de4e812099e2c875f67856f59d4f12c63ca3
Lodder <lodder15@hotmail.co.uk>
2017-02-15 13:54:14 +0000

    Update fields.php

--- plugins/system/fields/fields.php
+++ plugins/system/fields/fields.php
@@ -178,7 +178,6 @@ class PlgSystemFields extends JPlugin
 		}
 
 		$user = JFactory::getUser($userData['id']);
-		$user->params = (string) $user->getParam();
 
 		// Trigger the events with a real user
 		$this->onContentAfterSave('com_users.user', $user, false, $userData);
