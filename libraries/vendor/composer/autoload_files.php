fba78228be52502ed81a6f0c6a1088db44e1da43
Napoleon BlownApart <napoleon_blownapart@abms.net.au>
2017-02-16 18:10:37 +1000

    git status shows all these files modified but nothing was actually changed excpet file ownership on some from root to my user id.  I have installed composer and phpunit since the last commit and attempted to run the command 'phpunit . --debug --testdox'.  I think git got confused somehow.

--- libraries/vendor/composer/autoload_files.php
+++ libraries/vendor/composer/autoload_files.php
@@ -24,5 +24,6 @@ return array(
     'e40631d46120a9c38ea139981f8dab26' => $vendorDir . '/ircmaxell/password-compat/lib/password.php',
     'edc6464955a37aa4d5fbf39d40fb6ee7' => $vendorDir . '/symfony/polyfill-php55/bootstrap.php',
     '5255c38a0faeba867671b61dfda6d864' => $vendorDir . '/paragonie/random_compat/lib/random.php',
+    '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => $vendorDir . '/symfony/polyfill-mbstring/bootstrap.php',
     'bd9634f2d41831496de0d3dfe4c94881' => $vendorDir . '/symfony/polyfill-php56/bootstrap.php',
 );
--- libraries/vendor/composer/autoload_real.php
+++ libraries/vendor/composer/autoload_real.php
@@ -23,6 +23,10 @@ class ComposerAutoloaderInit205c915b9c7d3e718e7c95793ee67ffe
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit205c915b9c7d3e718e7c95793ee67ffe', 'loadClassLoader'));
 
+        $includePaths = require __DIR__ . '/include_paths.php';
+        array_push($includePaths, get_include_path());
+        set_include_path(implode(PATH_SEPARATOR, $includePaths));
+
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
             require_once __DIR__ . '/autoload_static.php';
