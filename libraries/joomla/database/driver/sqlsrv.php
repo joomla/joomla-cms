a7ede74ba004add90833bbc701be200a2cbad4cf
Brian Teeman <brian@teeman.net>
2017-02-19 14:54:17 +0000

    typo in comments

--- libraries/joomla/database/driver/sqlsrv.php
+++ libraries/joomla/database/driver/sqlsrv.php
@@ -240,7 +240,7 @@ class JDatabaseDriverSqlsrv extends JDatabaseDriver
 		// SQL Server does not accept NULL byte in query string
 		$result = str_replace("\0", "' + CHAR(0) + N'", $result);
 
-		// Fix for SQL Sever escape sequence, see https://support.microsoft.com/en-us/kb/164291
+		// Fix for SQL Server escape sequence, see https://support.microsoft.com/en-us/kb/164291
 		$result = str_replace(
 			array("\\\n",     "\\\r",     "\\\\\r\r\n"),
 			array("\\\\\n\n", "\\\\\r\r", "\\\\\r\n\r\n"),
