e6c945115fff4502791f51a9b3bdcbbddeed02a5
zero-24 <zero-24@users.noreply.github.com>
2017-02-21 15:16:35 +0100

    move 3.8 branch to 3.8 in the version file

--- libraries/cms/version/version.php
+++ libraries/cms/version/version.php
@@ -30,7 +30,7 @@ final class JVersion
 	 * @var    string
 	 * @since  3.5
 	 */
-	const RELEASE = '3.7';
+	const RELEASE = '3.8';
 
 	/**
 	 * Maintenance version.
@@ -38,7 +38,7 @@ final class JVersion
 	 * @var    string
 	 * @since  3.5
 	 */
-	const DEV_LEVEL = '0-beta3';
+	const DEV_LEVEL = '0-dev';
 
 	/**
 	 * Development status.
