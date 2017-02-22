a2cbaaf7de4906f9c6ed29d23a3fa909038d2e84
coolcat-creations <mail@coolcat-creations.com>
2017-02-22 22:31:52 +0100

    Add templates/protostar/pagination.php to remove unexisting files

--- administrator/components/com_admin/script.php
+++ administrator/components/com_admin/script.php
@@ -1725,6 +1725,8 @@ class JoomlaInstallerScript
 			'/administrator/modules/mod_menu/tmpl/default_enabled.php',
 			'/administrator/modules/mod_menu/tmpl/default_disabled.php',
 			'/administrator/templates/hathor/html/mod_menu/default_enabled.php',
+			'/templates/protostar/html/pagination.php',
+
 		);
 
 		// TODO There is an issue while deleting folders using the ftp mode
