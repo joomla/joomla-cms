3d3ea804a22c704aa993390eddbd9a845b2dd3f8
C-Lodder <lodder15@hotmail.co.uk>
2017-02-23 15:20:40 +0000

    remove old variables

--- templates/aurora/index.php
+++ templates/aurora/index.php
@@ -25,15 +25,6 @@ $task     = $app->input->getCmd('task', '');
 $itemid   = $app->input->getCmd('Itemid', '');
 $sitename = $app->get('sitename');
 
-if ($task === 'edit' || $layout === 'form')
-{
-	$fullWidth = 1;
-}
-else
-{
-	$fullWidth = 0;
-}
-
 // Add JavaScript Frameworks
 JHtml::_('bootstrap.framework');
 
