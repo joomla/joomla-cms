41465f62deeda8b719219cf219be49d2c576a927
zero-24 <zero-24@users.noreply.github.com>
2017-02-24 19:26:55 +0100

    ok now it works ..

--- administrator/includes/app.php
+++ administrator/includes/app.php
@@ -8,20 +8,18 @@
 
 defined('_JEXEC') or die;
 
-// Define the base path and require the other defines
-define('JPATH_BASE', dirname(__DIR__));
-
 // Saves the start time and memory usage.
 $startTime = microtime(1);
 $startMem  = memory_get_usage();
 
-if (file_exists(__DIR__ . '/defines.php'))
+if (file_exists(dirname(__DIR__) . '/defines.php'))
 {
-	include_once __DIR__ . '/defines.php';
+	include_once dirname(__DIR__) . '/defines.php';
 }
 
 if (!defined('_JDEFINES'))
 {
+	define('JPATH_BASE', dirname(__DIR__));
 	require_once JPATH_BASE . '/includes/defines.php';
 }
 
