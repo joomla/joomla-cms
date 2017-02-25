59012900350738185d64e749531d341e5b37f34e
Tuan Pham Ngoc <github@joomdonation.com>
2017-02-25 20:58:48 +0700

    Fix for ContentViewCategory

--- components/com_content/views/category/view.html.php
+++ components/com_content/views/category/view.html.php
@@ -192,27 +192,13 @@ class ContentViewCategory extends JViewCategory
 		$title = $this->params->get('page_title', '');
 
 		$id = (int) @$menu->query['id'];
-
-		// Check for empty title and add site name if param is set
-		if (empty($title))
-		{
-			$title = $app->get('sitename');
-		}
-		elseif ($app->get('sitename_pagetitles', 0) == 1)
-		{
-			$title = JText::sprintf('JPAGETITLE', $app->get('sitename'), $title);
-		}
-		elseif ($app->get('sitename_pagetitles', 0) == 2)
-		{
-			$title = JText::sprintf('JPAGETITLE', $title, $app->get('sitename'));
-		}
-
+		
 		if (empty($title))
 		{
 			$title = $this->category->title;
 		}
-
-		$this->document->setTitle($title);
+		
+		$this->setDocumentTitle($title);
 
 		if ($this->category->metadesc)
 		{
