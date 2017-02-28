ea85c41230861a666404da6962b5f23068deff76
Nicola Galgano <optimus4joomla@gmail.com>
2017-02-28 08:41:13 +0100

    pdomysql - exporter
    pdomysql exporter

--- libraries/joomla/database/exporter/pdomysql.php
+++ libraries/joomla/database/exporter/pdomysql.php
@@ -34,7 +34,15 @@ class JDatabaseExporterPdomysql extends JDatabaseExporter
 		$buffer[] = '<mysqldump xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">';
 		$buffer[] = ' <database name="">';
 
-		$buffer   = array_merge($buffer, $this->buildXmlStructure());
+		if ($this->options->withStructure)
+		{
+			$buffer = array_merge($buffer, $this->buildXmlStructure());
+		}
+
+		if ($this->options->withData)
+		{
+			$buffer = array_merge($buffer, $this->buildXmlData());
+		}
 
 		$buffer[] = ' </database>';
 		$buffer[] = '</mysqldump>';
@@ -77,10 +85,80 @@ class JDatabaseExporterPdomysql extends JDatabaseExporter
 				$buffer[] = '   <key Table="' . $table . '"' . ' Non_unique="' . $key->Non_unique . '"' . ' Key_name="' . $key->Key_name . '"' .
 					' Seq_in_index="' . $key->Seq_in_index . '"' . ' Column_name="' . $key->Column_name . '"' . ' Collation="' . $key->Collation . '"' .
 					' Null="' . $key->Null . '"' . ' Index_type="' . $key->Index_type . '"' .
+					' Sub_part ="' . $key->Sub_part . '"' .
 					' Comment="' . htmlspecialchars($key->Comment, ENT_COMPAT, 'UTF-8') . '"' . ' />';
 			}
 
 			$buffer[] = '  </table_structure>';
+		}
+
+		return $buffer;
+	}
+
+	/**
+	 * Builds the XML data to export.
+	 *
+	 * @return  array  An array of XML lines (strings).
+	 *
+	 * @since   3.6
+	 * @throws  Exception if an error occurs.
+	 */
+	protected function buildXmlData()
+	{
+		$buffer = array();
+
+		foreach ($this->from as $table)
+		{
+			// Replace the magic prefix if found.
+			$table = $this->getGenericTableName($table);
+
+			// Get the details columns information.
+			$fields  = $this->db->getTableColumns($table, false);
+			$colblob = array();
+
+			foreach ($fields as $field)
+			{
+				// Cacth blob for conversion xml
+				if ($field->Type == 'mediumblob')
+				{
+					$colblob[] = $field->Field;
+				}
+			}
+
+			$query = $this->db->getQuery(true);
+			$query->select($query->qn(array_keys($fields)))
+				->from($query->qn($table));
+			$this->db->setQuery($query);
+
+			$rows = $this->db->loadObjectList();
+
+			if (!count($rows))
+			{
+				continue;
+			}
+
+			$buffer[] = '  <table_data name="' . $table . '">';
+
+			foreach ($rows as $row)
+			{
+				$buffer[] = '   <row>';
+
+				foreach ($row as $key => $value)
+				{
+					if (!in_array($key, $colblob))
+					{
+						$buffer[] = '    <field name="' . $key . '">' . htmlspecialchars($value, ENT_COMPAT, 'UTF-8') . '</field>';
+					}
+					else
+					{
+						$buffer[] = '    <field name="' . $key . '">' . base64_encode($value) . '</field>';
+					}
+				}
+
+				$buffer[] = '   </row>';
+			}
+
+			$buffer[] = '  </table_data>';
 		}
 
 		return $buffer;
