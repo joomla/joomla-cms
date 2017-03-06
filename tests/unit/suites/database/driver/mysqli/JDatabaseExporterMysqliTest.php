ef6b5d55904c7415c6ecad249e15ab927e499ede
Nicola Galgano <optimus4joomla@gmail.com>
2017-03-06 10:26:55 +0100

    unit test Sub_part
    missed Sub_part

--- tests/unit/suites/database/driver/mysqli/JDatabaseExporterMysqliTest.php
+++ tests/unit/suites/database/driver/mysqli/JDatabaseExporterMysqliTest.php
@@ -129,7 +129,7 @@ class JDatabaseExporterMysqliTest extends TestCase
   <table_structure name="#__test">
    <field Field="id" Type="int(11) unsigned" Null="NO" Key="PRI" Default="" Extra="auto_increment" />
    <field Field="title" Type="varchar(255)" Null="NO" Key="" Default="" Extra="" />
-   <key Table="#__test" Non_unique="0" Key_name="PRIMARY" Seq_in_index="1" Column_name="id" Collation="A" Null="" Index_type="BTREE" Comment="" />
+   <key Table="#__test" Non_unique="0" Key_name="PRIMARY" Seq_in_index="1" Column_name="id" Collation="A" Null="" Index_type="BTREE" Sub_part="" Comment="" />
   </table_structure>
  </database>
 </mysqldump>';
