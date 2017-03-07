d895cdc72b01810732f689c45913a8fdc4a3be36
Nicola Galgano <optimus4joomla@gmail.com>
2017-03-07 08:03:46 +0100

    removed debug code
    removed debug code

--- libraries/joomla/database/importer/sqlsrv.php
+++ libraries/joomla/database/importer/sqlsrv.php
@@ -188,28 +188,6 @@ class JDatabaseImporterSqlsrv extends JDatabaseImporter
 							&& ((string) $newLookup[$name][$i]['Collation'] == $oldLookup[$name][$i]->Collation)
 							&& ((string) $newLookup[$name][$i]['Index_type'] == $oldLookup[$name][$i]->Index_type));
 
-						/*
-						Debug.
-						echo '<pre>';
-						echo '<br />Non_unique:   '.
-							((string) $newLookup[$name][$i]['Non_unique'] == $oldLookup[$name][$i]->Non_unique ? 'Pass' : 'Fail').' '.
-							(string) $newLookup[$name][$i]['Non_unique'].' vs '.$oldLookup[$name][$i]->Non_unique;
-						echo '<br />Column_name:  '.
-							((string) $newLookup[$name][$i]['Column_name'] == $oldLookup[$name][$i]->Column_name ? 'Pass' : 'Fail').' '.
-							(string) $newLookup[$name][$i]['Column_name'].' vs '.$oldLookup[$name][$i]->Column_name;
-						echo '<br />Seq_in_index: '.
-							((string) $newLookup[$name][$i]['Seq_in_index'] == $oldLookup[$name][$i]->Seq_in_index ? 'Pass' : 'Fail').' '.
-							(string) $newLookup[$name][$i]['Seq_in_index'].' vs '.$oldLookup[$name][$i]->Seq_in_index;
-						echo '<br />Collation:    '.
-							((string) $newLookup[$name][$i]['Collation'] == $oldLookup[$name][$i]->Collation ? 'Pass' : 'Fail').' '.
-							(string) $newLookup[$name][$i]['Collation'].' vs '.$oldLookup[$name][$i]->Collation;
-						echo '<br />Index_type:   '.
-							((string) $newLookup[$name][$i]['Index_type'] == $oldLookup[$name][$i]->Index_type ? 'Pass' : 'Fail').' '.
-							(string) $newLookup[$name][$i]['Index_type'].' vs '.$oldLookup[$name][$i]->Index_type;
-						echo '<br />Same = '.($same ? 'true' : 'false');
-						echo '</pre>';
-						 */
-
 						if (!$same)
 						{
 							// Break out of the loop. No need to check further.
