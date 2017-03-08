2e35e0325a8b2d36148295cd848c2f4c56e996fc
Nicola Galgano <optimus4joomla@gmail.com>
2017-03-08 19:47:24 +0100

    drop default not needed
    drop default not needed

--- administrator/components/com_admin/sql/updates/postgresql/3.7.0-2017-02-17.sql
+++ administrator/components/com_admin/sql/updates/postgresql/3.7.0-2017-02-17.sql
@@ -1,8 +1,5 @@
 -- Normalize contact_details table default values.
-ALTER TABLE "#__contact_details" ALTER COLUMN "name" DROP DEFAULT;
-ALTER TABLE "#__contact_details" ALTER COLUMN "alias" DROP DEFAULT;
 ALTER TABLE "#__contact_details" ALTER COLUMN "sortname1" SET DEFAULT '';
 ALTER TABLE "#__contact_details" ALTER COLUMN "sortname2" SET DEFAULT '';
 ALTER TABLE "#__contact_details" ALTER COLUMN "sortname3" SET DEFAULT '';
-ALTER TABLE "#__contact_details" ALTER COLUMN "language" DROP DEFAULT;
 ALTER TABLE "#__contact_details" ALTER COLUMN "xreference" SET DEFAULT '';
