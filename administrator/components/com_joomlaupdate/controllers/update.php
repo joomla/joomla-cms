212aad1ac990fa6d41b515550646ff80a8eeff0b
zero-24 <zero-24@users.noreply.github.com>
2017-03-13 15:21:22 +0100

    Update update.php

--- administrator/components/com_joomlaupdate/controllers/update.php
+++ administrator/components/com_joomlaupdate/controllers/update.php
@@ -130,6 +130,19 @@ class JoomlaupdateControllerUpdate extends JControllerLegacy
 			return false;
 		}
 
+		$options['format'] = '{DATE}\t{TIME}\t{LEVEL}\t{CODE}\t{MESSAGE}';
+		$options['text_file'] = 'joomla_update.php';
+		JLog::addLogger($options, JLog::INFO, array('Update', 'databasequery', 'jerror'));
+
+		try
+		{
+			JLog::add(JText::_('COM_JOOMLAUPDATE_UPDATE_LOG_FINALISE'), JLog::INFO, 'Update');
+		}
+		catch (RuntimeException $exception)
+		{
+			// Informational log only
+		}
+
 		$this->_applyCredentials();
 
 		/** @var JoomlaupdateModelDefault $model */
