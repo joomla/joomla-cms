05d502f541339e6e8cd713687c07d74db96897b9
Allon Moritz <allon.moritz@digital-peak.com>
2017-03-16 17:18:23 +0100

    Only allow super admins to create the field and set denied to all none super admin groups

--- plugins/fields/sql/sql.php
+++ plugins/fields/sql/sql.php
@@ -45,4 +45,54 @@ class PlgFieldsSql extends FieldsListPlugin
 
 		return $fieldNode;
 	}
+
+	/**
+	 * The save event.
+	 *
+	 * @param   string   $context  The context
+	 * @param   JTable   $item     The table
+	 * @param   boolean  $isNew    Is new item
+	 * @param   array    $data     The validated data
+	 *
+	 * @return  boolean
+	 *
+	 * @since   3.7.0
+	 */
+	public function onContentBeforeSave($context, $item, $isNew, $data = array())
+	{
+		// Only work on new SQL fields
+		if ($context != 'com_fields.field' || !isset($item->type) || $item->type != 'sql' || !$isNew)
+		{
+			return true;
+		}
+
+		// If we are not a super admin, don't let the user create a SQL field
+		if (!JAccess::getAssetRules(1)->allow('core.admin', JFactory::getUser()->getAuthorisedGroups()))
+		{
+			$item->setError(JText::_('PLG_FIELDS_SQL_CREATE_NOT_POSSIBLE'));
+			return false;
+		}
+
+		$changed = false;
+		// Loop trough the rules
+		foreach ($item->getRules()->getData() as $name => $rule)
+		{
+			// If the rule is not empty, means it was actively modified
+			if (!empty($rule->getData()))
+			{
+				continue;
+			}
+
+			// Set the debied flag on the root group
+			$rule->mergeIdentity(1, false);
+			$changed = true;
+		}
+
+		if ($changed)
+		{
+			JFactory::getApplication()->enqueueMessage(JText::_('PLG_FIELDS_SQL_RULES_ADAPTED'), 'warning');
+		}
+
+		return true;
+	}
 }
