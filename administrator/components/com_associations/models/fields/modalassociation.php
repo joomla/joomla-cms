3a6f0148986795862d1b4493809d120238a2e18d
Brian Teeman <brian@teeman.net>
2017-03-17 14:54:01 +0000

    a11y icons

--- administrator/components/com_associations/models/fields/modalassociation.php
+++ administrator/components/com_associations/models/fields/modalassociation.php
@@ -78,7 +78,7 @@ class JFormFieldModalAssociation extends JFormField
  				. ' class="btn' . ($value ? '' : ' hidden') . '"'
  				. ' onclick="return Joomla.submitbutton(\'undo-association\');"'
  				. ' id="remove-assoc">'
- 				. '<span class="icon-remove"></span>' . JText::_('JCLEAR')
+ 				. '<span class="icon-remove" aria-hidden="true"></span>' . JText::_('JCLEAR')
  				. '</button>';
 
 		$html[] = '<input type="hidden" id="' . $this->id . '_id" name="' . $this->name . '" value="' . $value . '" />';
