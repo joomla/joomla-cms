41824239669571e53e7337fe9eb9595d7101bedd
Yves Hoppe <yves.hoppe@community.joomla.org>
2017-03-19 11:42:48 +0100

    Making Drone happy

--- libraries/src/Cms/Controller/Controller.php
+++ libraries/src/Cms/Controller/Controller.php
@@ -331,10 +331,10 @@ class Controller  implements ControllerInterface
 	 * Constructor.
 	 *
 	 * @param   array             $config  An optional associative array of configuration settings.
-	 * Recognized key values include 'name', 'default_task', 'model_path', and
-	 * 'view_path' (this list is not meant to be comprehensive).
-	 * @param   \JApplicationCms  $app    The JApplication for the dispatcher
-	 * @param   \JInput           $input  Input
+	 *                                     Recognized key values include 'name', 'default_task', 'model_path', and
+	 *                                     'view_path' (this list is not meant to be comprehensive).
+	 * @param   \JApplicationCms  $app     The JApplication for the dispatcher
+	 * @param   \JInput           $input   Input
 	 *
 	 * @since   3.0
 	 */
