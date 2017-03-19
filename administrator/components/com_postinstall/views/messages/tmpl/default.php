e6a8edb130d318083a72db19ef8793a114134ba6
C-Lodder <lodder15@hotmail.co.uk>
2017-03-19 16:37:19 +0000

    Post-install styling

--- administrator/components/com_postinstall/views/messages/tmpl/default.php
+++ administrator/components/com_postinstall/views/messages/tmpl/default.php
@@ -26,9 +26,9 @@ $params = array('params' => json_encode($param));
 
 ?>
 
-<form action="index.php" method="post" name="adminForm" class="form-inline">
+<form action="index.php" method="post" name="adminForm" class="form-inline mb-3">
 	<input type="hidden" name="option" value="com_postinstall">
-	<label for="eid"><?php echo JText::_('COM_POSTINSTALL_MESSAGES_FOR'); ?></label>
+	<label for="eid" class="mr-sm-2"><?php echo JText::_('COM_POSTINSTALL_MESSAGES_FOR'); ?></label>
 	<?php echo JHtml::_('select.genericlist', $this->extension_options, 'eid', array('onchange' => 'this.form.submit()', 'class' => 'form-control custom-select'), 'value', 'text', $this->eid, 'eid'); ?>
 </form>
 
@@ -47,25 +47,27 @@ $params = array('params' => json_encode($param));
 	</div>
 <?php else : ?>
 	<?php foreach ($this->items as $item) : ?>
-	<fieldset>
-		<legend><?php echo JText::_($item->title_key); ?></legend>
-		<p class="small">
-			<?php echo JText::sprintf('COM_POSTINSTALL_LBL_SINCEVERSION', $item->version_introduced); ?>
-		</p>
-		<p><?php echo JText::_($item->description_key); ?></p>
-		<div>
-			<?php if ($item->type !== 'message') : ?>
-			<a href="index.php?option=com_postinstall&amp;view=messages&amp;task=action&amp;id=<?php echo $item->postinstall_message_id; ?>&amp;<?php echo $this->token; ?>=1" class="btn btn-primary">
-				<?php echo JText::_($item->action_key); ?>
-			</a>
-			<?php endif; ?>
-			<?php if (JFactory::getUser()->authorise('core.edit.state', 'com_postinstall')) : ?>
-			<a href="index.php?option=com_postinstall&amp;view=message&amp;task=unpublish&amp;id=<?php echo $item->postinstall_message_id; ?>&amp;<?php echo $this->token; ?>=1" class="btn btn-danger btn-sm">
-				<?php echo JText::_('COM_POSTINSTALL_BTN_HIDE'); ?>
-			</a>
-			<?php endif; ?>
+	<div class="card card-outline-secondary mb-3">
+		<div class="card-block">
+			<h3><?php echo JText::_($item->title_key); ?></h3>
+			<p class="small">
+				<?php echo JText::sprintf('COM_POSTINSTALL_LBL_SINCEVERSION', $item->version_introduced); ?>
+			</p>
+			<p><?php echo JText::_($item->description_key); ?></p>
+			<div>
+				<?php if ($item->type !== 'message') : ?>
+				<a href="index.php?option=com_postinstall&amp;view=messages&amp;task=action&amp;id=<?php echo $item->postinstall_message_id; ?>&amp;<?php echo $this->token; ?>=1" class="btn btn-primary">
+					<?php echo JText::_($item->action_key); ?>
+				</a>
+				<?php endif; ?>
+				<?php if (JFactory::getUser()->authorise('core.edit.state', 'com_postinstall')) : ?>
+				<a href="index.php?option=com_postinstall&amp;view=message&amp;task=unpublish&amp;id=<?php echo $item->postinstall_message_id; ?>&amp;<?php echo $this->token; ?>=1" class="btn btn-danger btn-sm">
+					<?php echo JText::_('COM_POSTINSTALL_BTN_HIDE'); ?>
+				</a>
+				<?php endif; ?>
+			</div>
 		</div>
-	</fieldset>
+	</div>
 	<?php endforeach; ?>
 <?php endif; ?>
 <?php if ($this->eid == 700) : ?>
