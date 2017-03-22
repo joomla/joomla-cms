01589ecd82eb5639e36a97ce8dc0706b9b4ff7cb
Brian Teeman <brian@teeman.net>
2017-03-22 14:56:43 +0000

    Subpackage names (#14845)
    Fix copy paste errors where the wrong subpackage is used
    Consistenccy check so that the case is consistent across the subpackage

--- administrator/components/com_menus/helpers/associations.php
+++ administrator/components/com_menus/helpers/associations.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package     Joomla.Administrator
- * @subpackage  com_content
+ * @subpackage  com_menus
  *
  * @copyright   Copyright (C) 2005 - 2017 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
