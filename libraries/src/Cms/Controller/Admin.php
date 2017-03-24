589a5afc2a03f33d061b53642ae3501577deeb53
Tuan Pham Ngoc <github@joomdonation.com>
2017-03-24 21:21:09 +0700

    Use StringInflector to guess view_list from view_item (#14880)
    * Use StringInflector to guess view_list from view_item
    
    * Use $this->app instead of calling JFactory::getApplication()

--- libraries/src/Cms/Controller/Admin.php
+++ libraries/src/Cms/Controller/Admin.php
@@ -116,14 +116,12 @@ class Admin extends Controller
 		// Check for request forgeries
 		\JSession::checkToken() or die(\JText::_('JINVALID_TOKEN'));
 
-		$app = \JFactory::getApplication();
-
 		// Get items to remove from the request.
 		$cid = $this->input->get('cid', array(), 'array');
 
 		if (!is_array($cid) || count($cid) < 1)
 		{
-			$app->getLogger()->warning(\JText::_($this->text_prefix . '_NO_ITEM_SELECTED'), array('category' => 'jerror'));
+			$this->app->getLogger()->warning(\JText::_($this->text_prefix . '_NO_ITEM_SELECTED'), array('category' => 'jerror'));
 		}
 		else
 		{
@@ -192,8 +190,6 @@ class Admin extends Controller
 		// Check for request forgeries
 		\JSession::checkToken() or die(\JText::_('JINVALID_TOKEN'));
 
-		$app = \JFactory::getApplication();
-
 		// Get items to publish from the request.
 		$cid   = $this->input->get('cid', array(), 'array');
 		$data  = array('publish' => 1, 'unpublish' => 0, 'archive' => 2, 'trash' => -2, 'report' => -3);
@@ -202,7 +198,7 @@ class Admin extends Controller
 
 		if (empty($cid))
 		{
-			$app->getLogger()->warning(\JText::_($this->text_prefix . '_NO_ITEM_SELECTED'), array('category' => 'jerror'));
+			$this->app->getLogger()->warning(\JText::_($this->text_prefix . '_NO_ITEM_SELECTED'), array('category' => 'jerror'));
 		}
 		else
 		{
@@ -403,6 +399,6 @@ class Admin extends Controller
 		}
 
 		// Close the application
-		\JFactory::getApplication()->close();
+		$this->app->close();
 	}
 }
