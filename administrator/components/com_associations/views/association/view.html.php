41766eaad9f62acaece13c0fc3e44daef0c87b80
Jean-Marie Simonet <infografjms@gmail.com>
2017-03-25 17:53:52 +0100

    [com_associations] Display Copy Reference to Target only for extensions using the save2copy task

--- administrator/components/com_associations/views/association/view.html.php
+++ administrator/components/com_associations/views/association/view.html.php
@@ -76,10 +76,25 @@ class AssociationsViewAssociation extends JViewLegacy
 		$this->app  = JFactory::getApplication();
 		$this->form = $this->get('Form');
 		$input      = $this->app->input;
-
 		$this->referenceId = $input->get('id', 0, 'int');
 
 		list($extensionName, $typeName) = explode('.', $input->get('itemtype'));
+
+		$extension = AssociationsHelper::getSupportedExtension($extensionName);
+		$types     = $extension->get('types');
+
+		if (array_key_exists($typeName, $types))
+		{
+			$this->type          = $types[$typeName];
+			$this->typeSupports  = array();
+			$details             = $this->type->get('details');
+
+			if (array_key_exists('support', $details))
+			{
+				$support = $details['support'];
+				$this->typeSupports = $support;
+			}
+		}
 
 		$this->extensionName = $extensionName;
 		$this->typeName      = $typeName;
@@ -187,7 +202,10 @@ class AssociationsViewAssociation extends JViewLegacy
 			. JText::_('COM_ASSOCIATIONS_SAVE_TARGET') . '</button>', 'target'
 		);
 
-		JToolBarHelper::custom('copy', 'copy.png', '', 'COM_ASSOCIATIONS_COPY_REFERENCE', false);
+		if ($this->typeName === 'category' || $this->extensionName === 'com_menus' || $this->typeSupports['save2copy'])
+		{
+			JToolBarHelper::custom('copy', 'copy.png', '', 'COM_ASSOCIATIONS_COPY_REFERENCE', false);
+		}
 
 		JToolbarHelper::cancel('association.cancel', 'JTOOLBAR_CLOSE');
 		JToolbarHelper::help('JHELP_COMPONENTS_ASSOCIATIONS_EDIT');
