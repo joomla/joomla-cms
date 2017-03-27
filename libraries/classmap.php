8bc73293cddf8dcb4a169408f25c6b15c16813b3
Allon Moritz <allon.moritz@digital-peak.com>
2017-03-27 10:35:20 +0200

    Move JCaptcha to namespace (#14225)
    * Move JCaptcha to namespace
    
    * Change CMS classes as well
    
    * Adapted intendation

--- libraries/classmap.php
+++ libraries/classmap.php
@@ -57,6 +57,8 @@ JLoader::registerAlias('JRules',                       '\\Joomla\\Cms\\Access\\R
 JLoader::registerAlias('JAuthenticationHelper',        '\\Joomla\\Cms\\Authentication\\AuthenticationHelper', '4.0');
 
 JLoader::registerAlias('JHelp',                        '\\Joomla\\Cms\\Help\\Help', '4.0');
+JLoader::registerAlias('JCaptcha',                     '\\Joomla\\Cms\\Captcha\\Captcha', '4.0');
 
 JLoader::registerAlias('JLanguageAssociations',        '\\Joomla\\Cms\\Language\\Associations', '4.0');
 JLoader::registerAlias('JLanguageMultilang',           '\\Joomla\\Cms\\Language\\Multilanguage', '4.0');
+
