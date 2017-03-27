6b0bab4fc1c2c56e50a25815188cf0405b7436db
Simon Asika <asika32764@gmail.com>
2017-03-27 08:35:18 +0800

    Fix based on review

--- plugins/installer/packageinstaller/tmpl/default.php
+++ plugins/installer/packageinstaller/tmpl/default.php
@@ -29,8 +29,145 @@ JFactory::getDocument()->addScriptDeclaration('
 		}
 	};
 ');
+
+// Drag and Drop installation scripts
+$token = JSession::getFormToken();
+$text = JText::_('PLG_INSTALLER_PACKAGEINSTALLER_DRAG_FILE_HERE');
+JText::script('PLG_INSTALLER_PACKAGEINSTALLER_DRAG_ERR_UNSUPPORTEDBROWSER');
+
+// Drag-drop installation
+JFactory::getDocument()->addScriptDeclaration(
+<<<JS
+    jQuery(document).ready(function($) {
+        var dragZone = $('body');
+        var tabContent = $('#package');
+        var cover = $('<div id="dragarea" style="display: none;"></div>');
+        
+        // Create drag cover first
+        dragZone.append(cover);
+        
+        dragZone.on('dragenter', function(e) {
+            e.preventDefault();
+            e.stopPropagation();
+            
+            if (!tabContent.hasClass('active')) {
+                return;
+            }
+            
+            cover.fadeIn();
+            
+            return false;
+        });
+
+        // Notify user when file is over the drop area
+        dragZone.on('dragover', function(e) {
+            e.preventDefault();
+            e.stopPropagation();
+            
+            if (!tabContent.hasClass('active')) {
+                return;
+            }
+            
+            cover.fadeIn();
+
+            return false;
+        });
+        
+        cover.on('dragleave', function(e) {
+            e.preventDefault();
+            e.stopPropagation();
+            cover.fadeOut();
+
+            return false;
+        });
+
+        dragZone.on('drop', function(e) {
+            e.preventDefault();
+            e.stopPropagation();
+            
+            if (!tabContent.hasClass('active')) {
+                return;
+            }
+            
+            if (typeof FormData === 'undefined') {
+                Joomla.renderMessages({'error': [Joomla.JText._("COM_INSTALLER_DRAG_ERR_UNSUPPORTEDBROWSER")]});
+                return;
+            }
+            
+            cover.fadeOut();
+
+            var files = e.originalEvent.target.files || e.originalEvent.dataTransfer.files;
+
+            if (!files.length) {
+                return;
+            }
+
+            var file = files[0];
+
+            var data = new FormData();
+            data.append('install_package', file);
+            data.append('installtype', 'upload');
+            data.append('{$token}', 1);
+
+            jQuery("#loading").css("display", "block");
+
+            $.ajax({
+                url: 'index.php?option=com_installer&task=install.ajax_upload',
+                data: data,
+                type: 'post',
+                processData: false,
+                cache: false,
+                contentType: false
+            }).done(function (res) {
+                if (res.success) {
+                    if (res.data.redirect) {
+                        location.href = res.data.redirect;
+                    } else {
+                        location.href = 'index.php?option=com_installer&view=install';
+                    }
+                } else {
+                    jQuery("#loading").css("display", "none");
+                    alert(res.message);
+                }
+            }).error (function (error) {
+                jQuery("#loading").css("display", "none");
+                alert(error.statusText);
+            });
+        });
+    });
+JS
+);
+
+JFactory::getDocument()->addStyleDeclaration(
+<<<CSS
+    #dragarea {
+        display: block;
+        background: rgba(255, 255, 255, .8);
+        position: fixed;
+        left: 0;
+        top: 0;
+        width: 100%;
+        height: 100%;
+        opacity: 0.8;
+        -ms-filter: progid:DXImageTransform.Microsoft.Alpha(Opacity = 80);
+        filter: alpha(opacity = 80);
+        overflow: hidden;
+    }
+
+    #dragarea::before {
+        content: "{$text}";
+        width: 100%;
+        display: block;
+        font-size: 36px;
+        text-align: center;
+        position: absolute;
+        top: 50%;
+    }
+CSS
+);
 ?>
 <legend><?php echo JText::_('PLG_INSTALLER_PACKAGEINSTALLER_UPLOAD_INSTALL_JOOMLA_EXTENSION'); ?></legend>
+<p class="lead"><?php echo JText::_('PLG_INSTALLER_PACKAGEINSTALLER_DRAG_FILE_NOTICE'); ?></p>
 <div class="control-group">
 	<label for="install_package" class="control-label"><?php echo JText::_('PLG_INSTALLER_PACKAGEINSTALLER_EXTENSION_PACKAGE_FILE'); ?></label>
 	<div class="controls">
