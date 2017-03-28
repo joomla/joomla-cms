9537d8808fb684b0759e0cc5ec17bb0e8628b7c3
Allon Moritz <allon.moritz@digital-peak.com>
2017-03-28 16:33:30 +0200

    Use the color rule for the color field

--- plugins/fields/color/color.php
+++ plugins/fields/color/color.php
@@ -18,4 +18,30 @@ JLoader::import('components.com_fields.libraries.fieldsplugin', JPATH_ADMINISTRA
  */
 class PlgFieldsColor extends FieldsPlugin
 {
+	/**
+	 * Transforms the field into an XML element and appends it as child on the given parent. This
+	 * is the default implementation of a field. Form fields which do support to be transformed into
+	 * an XML Element must implement the JFormDomfieldinterface.
+	 *
+	 * @param   stdClass    $field   The field.
+	 * @param   DOMElement  $parent  The field node parent.
+	 * @param   JForm       $form    The form.
+	 *
+	 * @return  DOMElement
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 */
+	public function onCustomFieldsPrepareDom($field, DOMElement $parent, JForm $form)
+	{
+		$fieldNode = parent::onCustomFieldsPrepareDom($field, $parent, $form);
+
+		if (!$fieldNode)
+		{
+			return $fieldNode;
+		}
+
+		$fieldNode->setAttribute('validate', 'color');
+
+		return $fieldNode;
+	}
 }
