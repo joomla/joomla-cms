f7b94ecac927494aa761af1ae73689b85998457f
Nilesh Prasad <prasadnilesh96@gmail.com>
2017-03-29 07:40:26 +0530

    improved button's tooltip consistency

--- administrator/modules/mod_logged/tmpl/default.php
+++ administrator/modules/mod_logged/tmpl/default.php
@@ -17,7 +17,7 @@ JHtml::_('bootstrap.tooltip');
 			<div class="span8">
 				<?php if ($user->client_id == 0) : ?>
 					<a title="<?php echo JHtml::_('tooltipText', 'MOD_LOGGED_LOGOUT'); ?>" href="<?php echo $user->logoutLink; ?>" class="btn btn-danger btn-mini hasTooltip">
-						<span class="icon-remove icon-white" title="<?php echo JText::_('JLOGOUT'); ?>"></span>
+						<span class="icon-remove icon-white" ></span>
 					</a>
 				<?php endif; ?>
 
