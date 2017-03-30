fc933d57f9721a6eea2f6b73131eeb1f10a6aa02
Thomas Hunziker <werbemails@bakual.ch>
2017-03-30 20:40:30 +0200

    Moving <dd> from field JLayout to fields JLayout

--- components/com_fields/layouts/field/render.php
+++ components/com_fields/layouts/field/render.php
@@ -23,12 +23,8 @@ if ($value == '')
 {
 	return;
 }
-
 ?>
-
-<dd class="field-entry <?php echo $class; ?>">
-	<?php if ($showlabel == 1) : ?>
+<?php if ($showlabel == 1) : ?>
 	<span class="field-label"><?php echo htmlentities($label, ENT_QUOTES | ENT_IGNORE, 'UTF-8'); ?>: </span>
-	<?php endif; ?>
-	<span class="field-value"><?php echo $value; ?></span>
-</dd>
+<?php endif; ?>
+<span class="field-value"><?php echo $value; ?></span>
