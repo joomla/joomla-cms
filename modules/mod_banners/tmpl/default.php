df9035b64db3dc43c301a39131af37422496a7f7
Bastian W <github20130717@mailbunker.de>
2017-03-31 14:57:58 +0200

    Update default.php to fix missing space between img attributes (#14894)
    * Update default.php
    
    * Update default.php

--- modules/mod_banners/tmpl/default.php
+++ modules/mod_banners/tmpl/default.php
@@ -41,10 +41,10 @@ $baseurl = JUri::base();
 							href="<?php echo $link; ?>" target="_blank" rel="noopener noreferrer"
 							title="<?php echo htmlspecialchars($item->name, ENT_QUOTES, 'UTF-8'); ?>">
 							<img
-								src="<?php echo $baseurl . $imageurl; ?>"
-								alt="<?php echo $alt; ?>"
-								<?php if (!empty($width)) echo 'width ="' . $width . '"'; ?>
-								<?php if (!empty($height)) echo 'height ="' . $height . '"'; ?>
+								src="<?php echo $baseurl . $imageurl;?>"
+								alt="<?php echo $alt;?>"
+								<?php if (!empty($width)) echo ' width="' . $width . '"';?>
+								<?php if (!empty($height)) echo ' height="' . $height . '"';?>
 							/>
 						</a>
 					<?php elseif ($target == 2) : ?>
@@ -55,10 +55,10 @@ $baseurl = JUri::base();
 								return false"
 							title="<?php echo htmlspecialchars($item->name, ENT_QUOTES, 'UTF-8'); ?>">
 							<img
-								src="<?php echo $baseurl . $imageurl; ?>"
-								alt="<?php echo $alt; ?>"
-								<?php if (!empty($width)) echo 'width ="' . $width . '"'; ?>
-								<?php if (!empty($height)) echo 'height ="' . $height . '"'; ?>
+								src="<?php echo $baseurl . $imageurl;?>"
+								alt="<?php echo $alt;?>"
+								<?php if (!empty($width)) echo ' width="' . $width . '"';?>
+								<?php if (!empty($height)) echo ' height="' . $height . '"';?>
 							/>
 						</a>
 					<?php else : ?>
@@ -67,28 +67,28 @@ $baseurl = JUri::base();
 							href="<?php echo $link; ?>"
 							title="<?php echo htmlspecialchars($item->name, ENT_QUOTES, 'UTF-8'); ?>">
 							<img
-								src="<?php echo $baseurl . $imageurl; ?>"
-								alt="<?php echo $alt; ?>"
-								<?php if (!empty($width)) echo 'width ="' . $width . '"'; ?>
-								<?php if (!empty($height)) echo 'height ="' . $height . '"'; ?>
+								src="<?php echo $baseurl . $imageurl;?>"
+								alt="<?php echo $alt;?>"
+								<?php if (!empty($width)) echo ' width="' . $width . '"';?>
+								<?php if (!empty($height)) echo ' height="' . $height . '"';?>
 							/>
 						</a>
 					<?php endif; ?>
 				<?php else : ?>
 					<?php // Just display the image if no link specified ?>
 					<img
-						src="<?php echo $baseurl . $imageurl; ?>"
-						alt="<?php echo $alt; ?>"
-						<?php if (!empty($width)) echo 'width ="' . $width . '"'; ?>
-						<?php if (!empty($height)) echo 'height ="' . $height . '"'; ?>
+						src="<?php echo $baseurl . $imageurl;?>"
+						alt="<?php echo $alt;?>"
+						<?php if (!empty($width)) echo ' width="' . $width . '"';?>
+						<?php if (!empty($height)) echo ' height="' . $height . '"';?>
 					/>
 				<?php endif; ?>
 			<?php elseif (BannerHelper::isFlash($imageurl)) : ?>
 				<object
 					classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000"
 					codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,0,0"
-					<?php if (!empty($width)) echo 'width ="' . $width . '"'; ?>
-					<?php if (!empty($height)) echo 'height ="' . $height . '"'; ?>
+					<?php if (!empty($width)) echo ' width="' . $width . '"';?>
+					<?php if (!empty($height)) echo ' height="' . $height . '"';?>
 				>
 					<param name="movie" value="<?php echo $imageurl; ?>" />
 					<embed
@@ -96,8 +96,8 @@ $baseurl = JUri::base();
 						loop="false"
 						pluginspage="http://www.macromedia.com/go/get/flashplayer"
 						type="application/x-shockwave-flash"
-						<?php if (!empty($width)) echo 'width ="' . $width . '"'; ?>
-						<?php if (!empty($height)) echo 'height ="' . $height . '"'; ?>
+						<?php if (!empty($width)) echo ' width="' . $width . '"';?>
+						<?php if (!empty($height)) echo ' height="' . $height . '"';?>
 					/>
 				</object>
 			<?php endif; ?>
