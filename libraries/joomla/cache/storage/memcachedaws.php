bbff6b607f1c074e84264aad8f444eb9b63f705e
zero-24 <zero-24@users.noreply.github.com>
2017-03-31 20:11:15 +0200

    spaces to tabs and some other cs things ;)

--- libraries/joomla/cache/storage/memcachedaws.php
+++ libraries/joomla/cache/storage/memcachedaws.php
@@ -3,7 +3,7 @@
  * @package     Joomla.Platform
  * @subpackage  Cache
  *
- * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
+ * @copyright   Copyright (C) 2005 - 2017 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE
  */
 
@@ -12,7 +12,7 @@ defined('JPATH_PLATFORM') or die;
 /**
  * Memcached cache storage handler using AWS ElastiCache PHP Client
  *
- * @see    http://docs.aws.amazon.com/AmazonElastiCache/latest/UserGuide/AutoDiscovery.html
+ * @see    https://docs.aws.amazon.com/AmazonElastiCache/latest/UserGuide/AutoDiscovery.html
  * @see    https://github.com/awslabs/aws-elasticache-cluster-client-memcached-for-php
  * @since  __DEPLOY_VERSION__
  */
@@ -25,15 +25,15 @@ class JCacheStorageMemcachedaws extends JCacheStorageMemcached
 	 *
 	 * @since   __DEPLOY_VERSION__
 	 */
-    public function __construct($options = array())
-    {
-        JCacheStorage::__construct($options);
+	public function __construct($options = array())
+	{
+		JCacheStorage::__construct($options);
 
-        if (parent::$_db === null)
-        {
-            $this->getConnection();
-        }
-    }
+		if (parent::$_db === null)
+		{
+			$this->getConnection();
+		}
+	}
 
 	/**
 	 * Create the MemcachedAWS connection
@@ -43,113 +43,113 @@ class JCacheStorageMemcachedaws extends JCacheStorageMemcached
 	 * @since   __DEPLOY_VERSION__
 	 * @throws  RuntimeException
 	 */
-    protected function getConnection()
-    {
-        if (!static::isSupported())
-        {
-            throw new RuntimeException('Memcached Extension is not available');
-        }
+	protected function getConnection()
+	{
+		if (!static::isSupported())
+		{
+			throw new RuntimeException('Memcached Extension is not available');
+		}
 
-        $config = JFactory::getConfig();
+		$config = JFactory::getConfig();
 
-        $host = $config->get('memcached_server_host', 'localhost');
-        $port = $config->get('memcached_server_port', 11211);
+		$host = $config->get('memcached_server_host', 'localhost');
+		$port = $config->get('memcached_server_port', 11211);
 
-        if ($config->get('memcached_persist', true))
-        {
-            parent::$_db = new Memcached($this->_hash);
-            $servers     = parent::$_db->getServerList();
+		if ($config->get('memcached_persist', true))
+		{
+			parent::$_db = new Memcached($this->_hash);
+			$servers	 = parent::$_db->getServerList();
 
-            if ($servers && ($servers[0]['host'] != $host || $servers[0]['port'] != $port))
-            {
-                parent::$_db->resetServerList();
-                $servers = array();
-            }
+			if ($servers && ($servers[0]['host'] != $host || $servers[0]['port'] != $port))
+			{
+				parent::$_db->resetServerList();
+				$servers = array();
+			}
 
-            if (!$servers)
-            {
-                parent::$_db->addServer($host, $port);
-            }
-        }
-        else
-        {
-            parent::$_db = new Memcached;
-            parent::$_db->addServer($host, $port);
-        }
+			if (!$servers)
+			{
+				parent::$_db->addServer($host, $port);
+			}
+		}
+		else
+		{
+			parent::$_db = new Memcached;
+			parent::$_db->addServer($host, $port);
+		}
 
-        parent::$_db->setOption(Memcached::OPT_COMPRESSION, $config->get('memcached_compress', false) ? Memcached::OPT_COMPRESSION : 0);
+		parent::$_db->setOption(Memcached::OPT_COMPRESSION, $config->get('memcached_compress', false) ? Memcached::OPT_COMPRESSION : 0);
 
-        if (JFactory::getConfig()->get('memcached_autodiscovery', 0))
-        {
-            $this->getDynamicClientConnection();
-        }
-        else
-        {
-            $this->getStaticClientConnection();
-        }
-    }
+		if (JFactory::getConfig()->get('memcached_autodiscovery', 0))
+		{
+			$this->getDynamicClientConnection();
+		}
+		else
+		{
+			$this->getStaticClientConnection();
+		}
+	}
 
-    /**
-     * The following will initialize a Memcached client to utilize the Auto Discovery feature.
-     *
-     * By configuring the client with the Dynamic client mode with single endpoint, the
-     * client will periodically use the configuration endpoint to retrieve the current cache
-     * cluster configuration. This allows scaling the cache cluster up or down in number of nodes
-     * without requiring any changes to the PHP application.
-     *
+	/**
+	 * The following will initialize a Memcached client to utilize the Auto Discovery feature.
+	 *
+	 * By configuring the client with the Dynamic client mode with single endpoint, the
+	 * client will periodically use the configuration endpoint to retrieve the current cache
+	 * cluster configuration. This allows scaling the cache cluster up or down in number of nodes
+	 * without requiring any changes to the PHP application.
+	 *
 	 * @return  void
 	 *
 	 * @since   __DEPLOY_VERSION__
 	 * @throws  RuntimeException
 	 */
-    protected function getDynamicClientConnection()
-    {
-        parent::$_db->setOption(Memcached::OPT_CLIENT_MODE, Memcached::DYNAMIC_CLIENT_MODE);
-        parent::$_db->setOption(Memcached::OPT_DISTRIBUTION, Memcached::DISTRIBUTION_CONSISTENT);
+	protected function getDynamicClientConnection()
+	{
+		parent::$_db->setOption(Memcached::OPT_CLIENT_MODE, Memcached::DYNAMIC_CLIENT_MODE);
+		parent::$_db->setOption(Memcached::OPT_DISTRIBUTION, Memcached::DISTRIBUTION_CONSISTENT);
 
-        $this->checkConnection();
-    }
+		$this->checkConnection();
+	}
 
-    /**
-     * Configuring the client with Static client mode disables the usage of Auto Discovery
-     * and the client operates as it did before the introduction of Auto Discovery.
+	/**
+	 * Configuring the client with Static client mode disables the usage of Auto Discovery
+	 * and the client operates as it did before the introduction of Auto Discovery.
 	 *
-     * @return  void
+	 * @return  void
 	 *
 	 * @since   __DEPLOY_VERSION__
 	 */
-    protected function getStaticClientConnection()
-    {
-        parent::$_db->setOption(Memcached::OPT_CLIENT_MODE, Memcached::STATIC_CLIENT_MODE);
+	protected function getStaticClientConnection()
+	{
+		parent::$_db->setOption(Memcached::OPT_CLIENT_MODE, Memcached::STATIC_CLIENT_MODE);
 
-        $this->checkConnection();
-    }
+		$this->checkConnection();
+	}
 
-    /**
-     * Check connection to memcached server.
-     *
+	/**
+	 * Check connection to memcached server.
+	 *
 	 * @return  void
 	 *
 	 * @since   __DEPLOY_VERSION__
 	 * @throws  JCacheExceptionConnecting
 	 */
-    protected function checkConnection()
-    {
-        $config = JFactory::getConfig();
+	protected function checkConnection()
+	{
+		$config = JFactory::getConfig();
 
-        $host = $config->get('memcached_server_host', 'localhost');
-        $port = $config->get('memcached_server_port', 11211);
+		$host = $config->get('memcached_server_host', 'localhost');
+		$port = $config->get('memcached_server_port', 11211);
 
-        $stats  = parent::$_db->getStats();
-        $result = !empty($stats["$host:$port"]) && $stats["$host:$port"]['pid'] > 0;
+		$stats  = parent::$_db->getStats();
+		$result = !empty($stats["$host:$port"]) && $stats["$host:$port"]['pid'] > 0;
 
-        if (!$result)
-        {
-            // Null out the connection to inform the constructor it will need to attempt to connect if this class is instantiated again
-            parent::$_db = null;
+		if (!$result)
+		{
+			// Null out the connection to inform the constructor it will need to attempt to connect if this class is instantiated again
+			parent::$_db = null;
 
-            throw new JCacheExceptionConnecting('Could not connect to memcached server. '. parent::$_db->getResultMessage());
-        }
-    }
+			throw new JCacheExceptionConnecting('Could not connect to memcached server. '. parent::$_db->getResultMessage());
+		}
+	}
 
 }
