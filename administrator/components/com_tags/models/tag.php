4eb371bd6fc92d06806f6300f47e7c57c9689cd3
bertmert <bertmert@users.noreply.github.com>
2017-04-01 14:21:27 +0200

    Undefined index: tag. Add batch_commands array

--- administrator/components/com_tags/models/tag.php
+++ administrator/components/com_tags/models/tag.php
@@ -32,6 +32,17 @@ class TagsModelTag extends JModelAdmin
 	public $typeAlias = 'com_tags.tag';
 
 	/**
+	 * Allowed batch commands
+	 *
+	 * @var    array
+	 * @since  3.4
+	 */
+	protected $batch_commands = array(
+		'assetgroup_id' => 'batchAccess',
+		'language_id' => 'batchLanguage',
+	);
+
+	/**
 	 * Method to test whether a record can be deleted.
 	 *
 	 * @param   object  $record  A record object.
