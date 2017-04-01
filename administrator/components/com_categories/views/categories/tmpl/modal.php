96e15759d9f8c9902b53f3999f2812e65b923bfd
Brian Teeman <brian@teeman.net>
2017-04-01 23:43:34 +0100

    put tab back

--- administrator/components/com_categories/views/categories/tmpl/modal.php
+++ administrator/components/com_categories/views/categories/tmpl/modal.php
@@ -107,7 +107,7 @@ $listDirn  = $this->escape($this->state->get('list.direction'));
 						?>
 						<tr class="row<?php echo $i % 2; ?>">
 							<td class="center">
-							<span class="<?php echo $iconStates[$this->escape($item->published)]; ?>" aria-hidden="true"></span>
+								<span class="<?php echo $iconStates[$this->escape($item->published)]; ?>" aria-hidden="true"></span>
 							</td>
 							<td>
 								<?php echo JLayoutHelper::render('joomla.html.treeprefix', array('level' => $item->level)); ?>
