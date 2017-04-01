e63146852295d47a490b3cd4ceba8baa74c1814f
Tuan Pham Ngoc <github@joomdonation.com>
2017-04-01 18:58:12 +0700

    Correct Table class namespace

--- libraries/src/Cms/Model/Model.php
+++ libraries/src/Cms/Model/Model.php
@@ -386,7 +386,6 @@ abstract class Model extends \JObject
 		// Clean the model name
 		$name = preg_replace('/[^A-Z0-9_]/i', '', $name);
 		$prefix = preg_replace('/[^A-Z0-9_\\\\]/i', '', $prefix);
-
 		// Make sure we are returning a DBO object
 		if (!array_key_exists('dbo', $config))
 		{
@@ -499,7 +498,7 @@ abstract class Model extends \JObject
 					$name = $inflector->toSingular($name);
 				}
 
-				$prefix = $this->namespace . '\\Administrator\\Table\\';
+				$prefix = $this->namespace . 'Administrator\\Table\\';
 			}
 			else
 			{
