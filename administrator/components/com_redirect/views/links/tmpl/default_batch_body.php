e9f7e0f8d7fcb49370ca9205725f5596fc01b316
Quy <quy@fluxbb.org>
2017-04-01 20:45:21 -0700

    Move code location

--- administrator/components/com_redirect/views/links/tmpl/default_batch_body.php
+++ administrator/components/com_redirect/views/links/tmpl/default_batch_body.php
@@ -12,8 +12,8 @@ $published = $this->state->get('filter.published');
 
 <div class="container-fluid">
 	<div class="row-fluid">
+		<p><?php echo JText::_('COM_REDIRECT_BATCH_TIP'); ?></p>
 		<div class="control-group span12">
-			<p><?php echo JText::_('COM_REDIRECT_BATCH_TIP'); ?></p>
 			<div class="controls">
 				<textarea class="span12" rows="10" aria-required="true" value="" id="batch_urls" name="batch_urls"></textarea>
 			</div>
