daf67195f7012963ac8915087445640b5ddfd87a
Brian Teeman <brian@teeman.net>
2017-04-02 20:44:02 +0100

    template assignment

--- administrator/components/com_templates/views/style/tmpl/edit_assignment.php
+++ administrator/components/com_templates/views/style/tmpl/edit_assignment.php
@@ -17,7 +17,7 @@ $user      = JFactory::getUser();
 <label id="jform_menuselect-lbl" for="jform_menuselect"><?php echo JText::_('JGLOBAL_MENU_SELECTION'); ?></label>
 <div class="btn-toolbar">
 	<button class="btn" type="button" class="jform-rightbtn" onclick="jQuery('.chk-menulink').attr('checked', !jQuery('.chk-menulink').attr('checked'));">
-		<span class="icon-checkbox-partial"></span> <?php echo JText::_('JGLOBAL_SELECTION_INVERT_ALL'); ?>
+		<span class="icon-checkbox-partial" aria-hidden="true"></span> <?php echo JText::_('JGLOBAL_SELECTION_INVERT_ALL'); ?>
 	</button>
 </div>
 <div id="menu-assignment">
@@ -27,7 +27,7 @@ $user      = JFactory::getUser();
 			<li>
 				<div class="menu-links-block">
 					<button class="btn" type="button" class="jform-rightbtn" onclick="jQuery('.<?php echo $type->menutype; ?>').attr('checked', !jQuery('.<?php echo $type->menutype; ?>').attr('checked'));">
-						<span class="icon-checkbox-partial"></span> <?php echo JText::_('JGLOBAL_SELECTION_INVERT'); ?>
+						<span class="icon-checkbox-partial" aria-hidden="true"></span> <?php echo JText::_('JGLOBAL_SELECTION_INVERT'); ?>
 					</button>
 					<h5><?php echo $type->title ? $type->title : $type->menutype; ?></h5>
 	
