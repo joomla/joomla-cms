1e3428618fc51320649edecb984b684c41480a0b
Brian Teeman <brian@teeman.net>
2017-04-02 10:59:52 +0100

    help modal popup (reverted from commit a26e541400cdad3afb3b38c77896c794bf27a320)

--- layouts/joomla/toolbar/modal.php
+++ layouts/joomla/toolbar/modal.php
@@ -27,6 +27,6 @@ $icon     = isset($displayData['icon']) ? $displayData['icon'] : 'out-3';
 $text     = isset($displayData['text']) ? $displayData['text'] : '';
 ?>
 <button class="<?php echo $class; ?>" data-toggle="modal" data-target="#<?php echo $selector; ?>">
-	<span class="icon-<?php echo $icon; ?>" aria-hidden="true"></span>
+	<span class="icon-<?php echo $icon; ?>"></span>
 	<?php echo $text; ?>
 </button>
