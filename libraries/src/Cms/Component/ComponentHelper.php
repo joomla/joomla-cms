9f5bb76a60d1825231435bd06edc2426e7678867
Tuan Pham Ngoc <github@joomdonation.com>
2017-04-02 15:53:52 +0700

    Bonus - codestyle

--- libraries/src/Cms/Component/ComponentHelper.php
+++ libraries/src/Cms/Component/ComponentHelper.php
@@ -3,8 +3,8 @@
  * @package     Joomla.Libraries
  * @subpackage  Component
  *
- * @copyright  Copyright (C) 2005 - 2017 Open Source Matters, Inc. All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE
+ * @copyright   Copyright (C) 2005 - 2017 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
  */
 
 namespace Joomla\Cms\Component;
