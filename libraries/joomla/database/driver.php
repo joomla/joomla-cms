adce9eebb7ef80d4e597a8a0404ee411328a264b
Artur Stępień <artur.stepien@bestproject.pl>
2017-04-08 20:01:13 +0200

    Update driver.php

--- libraries/joomla/database/driver.php
+++ libraries/joomla/database/driver.php
@@ -1407,14 +1407,17 @@ abstract class JDatabaseDriver extends JDatabase implements JDatabaseInterface
 	 * Method to get an array of the result set rows from the database query where each row is an array.  The array
 	 * of objects can optionally be keyed by a field offset, but defaults to a sequential numeric array.
 	 *
-	 * @param   integer  $key  The index of a field on which to key the result array.
+	 * NOTE: Choosing to key the result array by a non-unique field can result in unwanted
+	 * behavior and should be avoided.
+	 *
+	 * @param   integer  $index  The index of a field on which to key the result array.
 	 *
 	 * @return  mixed   The return value or null if the query failed.
 	 *
 	 * @since   11.1
 	 * @throws  RuntimeException
 	 */
-	public function loadRowList($key = null)
+	public function loadRowList($index = null)
 	{
 		$this->connect();
 
@@ -1429,9 +1432,9 @@ abstract class JDatabaseDriver extends JDatabase implements JDatabaseInterface
 		// Get all of the rows from the result set as arrays.
 		while ($row = $this->fetchArray($cursor))
 		{
-			if ($key !== null)
+			if ($index !== null)
 			{
-				$array[$row[$key]] = $row;
+				$array[$row[$index]] = $row;
 			}
 			else
 			{
