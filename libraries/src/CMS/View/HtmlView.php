110ed1ccf5cb66ce8202c41f5239fdc13eb372a2
Allon Moritz <allon.moritz@digital-peak.com>
2017-04-11 18:26:32 +0200

    remove the tmpl folder in the views folder

--- libraries/src/CMS/View/HtmlView.php
+++ libraries/src/CMS/View/HtmlView.php
@@ -134,9 +134,13 @@ class HtmlView extends AbstractView
 		{
 			$this->_setPath('template', $this->_basePath . '/view/' . $this->getName() . '/tmpl');
 		}
-		else
+		elseif (is_dir($this->_basePath . '/views/' . $this->getName() . '/tmpl'))
 		{
 			$this->_setPath('template', $this->_basePath . '/views/' . $this->getName() . '/tmpl');
+		}
+		else
+		{
+			$this->_setPath('template', $this->_basePath . '/views/' . $this->getName());
 		}
 
 		// Set the default helper search path
