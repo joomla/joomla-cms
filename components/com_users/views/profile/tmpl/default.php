2d2d1f7eac15b23a5b5398ef2f1f9204b258a936
Ciaran Walsh <ciaran@joomla51.com>
2017-04-16 16:07:23 +0100

    [4.0] pull-xxx to float-xxx - BS4 migration (#15280)
    * pull-xxx to float-xxx - BS4 migration
    
    * Revert FA vendor change

--- components/com_users/views/profile/tmpl/default.php
+++ components/com_users/views/profile/tmpl/default.php
@@ -18,7 +18,7 @@ defined('_JEXEC') or die;
 </div>
 <?php endif; ?>
 <?php if (JFactory::getUser()->id == $this->data->id) : ?>
-<ul class="btn-toolbar pull-right">
+<ul class="btn-toolbar float-right">
 	<li class="btn-group">
 		<a class="btn" href="<?php echo JRoute::_('index.php?option=com_users&task=profile.edit&user_id=' . (int) $this->data->id); ?>">
 			<span class="icon-user"></span> <?php echo JText::_('COM_USERS_EDIT_PROFILE'); ?></a>
