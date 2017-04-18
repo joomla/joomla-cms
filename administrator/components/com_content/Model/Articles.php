ebdde82517ec39d5bd076c6ac17e72c4737869b0
Benjamin Trenkle <bembelimen@users.noreply.github.com>
2017-04-18 09:25:07 +0200

    [4.0] Simplify for loop in articles model

--- administrator/components/com_content/Model/Articles.php
+++ administrator/components/com_content/Model/Articles.php
@@ -407,10 +407,10 @@ class Articles extends ListModel
 		{
 			$groups = \JFactory::getUser()->getAuthorisedViewLevels();
 
-			for ($x = 0, $count = count($items); $x < $count; $x++)
+			foreach ($items as $x => $item)
 			{
 				// Check the access level. Remove articles the user shouldn't see
-				if (!in_array($items[$x]->access, $groups))
+				if (!in_array($item->access, $groups))
 				{
 					unset($items[$x]);
 				}
