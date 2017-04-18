124c89cff424779210da3c932fb8717bf12166f6
Jim Dolinski <jim.dolinski@dotcomm.org>
2017-04-18 13:27:14 -0500

    Added Redis session handler support

---
+++ libraries/joomla/session/storage/redis.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * @package     Joomla.Platform
+ * @subpackage  Session
+ *
+ * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+
+defined('JPATH_PLATFORM') or die;
+
+/**
+ * Redis session storage handler for PHP
+ *
+ * @see    https://secure.php.net/manual/en/function.session-set-save-handler.php
+ * @since
+ */
+class JSessionStorageRedis extends JSessionStorage
+{
+
+    public function __construct($options = array()) {
+        if (!self::isSupported())
+        {
+            throw new RuntimeException('Redis Extension is not available', 404);
+        }
+        $config = JFactory::getConfig();
+        $this->_servers = array(
+            'host' => $config->get('session_redis_server_host', 'localhost'),
+            'port' => $config->get('session_redis_server_port', 11211)
+        );
+        parent::__construct($options);
+    }
+
+    /**
+     * Register the functions of this class with PHP's session handler
+     *
+     * @return  void
+     *
+     * @since   12.2
+     */
+    public function register()
+    {
+        if (!empty($this->_servers) && isset($this->_servers['host']) && isset($this->_servers['port']))
+        {
+            $serverConf = current($this->_servers);
+            ini_set('session.save_path', "{$this->_servers['host']}:{$this->_servers['port']}");
+            ini_set('session.save_handler', 'redis');
+            ini_set('zlib.output_compression', 'Off'); //this is required if the configuration.php gzip is turned on
+        }
+    }
+
+
+    /**
+     * Test to see if the SessionHandler is available.
+     *
+     * @return boolean  True on success, false otherwise.
+     *
+     * @since   12.1
+     */
+    public static function isSupported()
+    {
+        return extension_loaded('redis') && class_exists('Redis');
+    }
+
+}
