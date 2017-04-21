6be974d82dd1bb08e1c6db8f6370fa1b037d6451
Brian Teeman <brian@teeman.net>
2017-04-21 23:28:32 +0100

    [4.0] karma conf update
    We dont need to have this exclusion anymore as there are no longer any matching files

--- karma.conf.js
+++ karma.conf.js
@@ -31,11 +31,6 @@ module.exports = function (config) {
 			'tests/javascript/test-main.js'
 		],
 
-		// list of files to exclude
-		exclude: [
-			'media/system/js/*uncompressed.js'
-		],
-
 		// preprocess matching files before serving them to the browser
 		// available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
 		preprocessors: {
