32120c55107e05b856c413a05612a7949a250fee
Brian Teeman <brian@teeman.net>
2017-04-23 11:08:22 +0100

    [4.0] update karma part2 (#15485)
    As suggested by @dgt41

--- karma.conf.js
+++ karma.conf.js
@@ -34,7 +34,7 @@ module.exports = function (config) {
 		// preprocess matching files before serving them to the browser
 		// available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
 		preprocessors: {
-			'**/system/js/*!(uncompressed).js': ['coverage']
+			'**/system/js/*.js': ['coverage']
 		},
 
 		// coverage reporter configuration
