4d538bfda2280cf004d64ce6ae5fdb0c54a07845
Robert Deutz <rdeutz@googlemail.com>
2017-04-25 17:42:41 +0200

    set dev state right

--- libraries/cms/version/version.php
+++ libraries/cms/version/version.php
@@ -46,7 +46,7 @@ final class JVersion
 	 * @var    string
 	 * @since  3.5
 	 */
-	const DEV_STATUS = 'dev';
+	const DEV_STATUS = 'Development';
 
 	/**
 	 * Build number.
@@ -78,7 +78,7 @@ final class JVersion
 	 * @var    string
 	 * @since  3.5
 	 */
-	const RELTIME = '17:17';
+	const RELTIME = '16:42';
 
 	/**
 	 * Release timezone.
