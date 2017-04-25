80c822b3b6b788508e4c34882e16753e23c0ff7f
dgt41 <d.grammatiko@gmail.com>
2017-04-25 16:16:41 +0300

    more

--- media/system/js/helpsite.js
+++ media/system/js/helpsite.js
@@ -4,54 +4,58 @@
  * @license		GNU General Public License version 2 or later; see LICENSE.txt
  */
 
+Joomla = window.Joomla || {};
 /**
  * Rebuilds the list with the available help sites
  */
-"use strict";
+(function() {
+	"use strict";
 
-var resetHelpSiteList = function() {
+	Joomla.resetHelpSiteList = function() {
 
-	// Uses global variable helpsite_base for bast uri
-	var select_id   = this.getAttribute('rel');
-	var showDefault = this.getAttribute('showDefault');
+		// Uses global variable helpsite_base for bast uri
+		var select_id   = this.getAttribute('rel');
+		var showDefault = this.getAttribute('showDefault');
 
-	Joomla.request(
-		{
-			url:    'index.php?option=com_users&task=profile.gethelpsites&format=json',
-			method: 'GET',
-			data:    '',
-			perform: true,
-			headers: {'Content-Type': 'application/json;charset=utf-8'},
-			onSuccess: function(response, xhr)
+		Joomla.request(
 			{
-				response = JSON.parse(response);
+				url:    'index.php?option=com_users&task=profile.gethelpsites&format=json',
+				method: 'GET',
+				data:    '',
+				perform: true,
+				headers: {'Content-Type': 'application/json;charset=utf-8'},
+				onSuccess: function(response, xhr)
+				{
+					response = JSON.parse(response);
 
-				// The response contains the options to use in help site select field
-				var node;
+					// The response contains the options to use in help site select field
+					var node;
 
-				document.getElementById(select_id).innerHTML = '';
+					document.getElementById(select_id).innerHTML = '';
 
-				// Build options
-				response.forEach(function(item) {
-					if (item.value !== '' || showDefault === 'true') {
-						node = document.createElement('option');
-						node.value = item.value;
-						node.innerHTML = item.text;
-						document.getElementById(select_id).appendChild(node);
-					}
-				});
-			},
-			onError: function(xhr)
-			{
-				// Remove js messages, if they exist.
-				Joomla.removeMessages();
+					// Build options
+					response.forEach(function(item) {
+						if (item.value !== '' || showDefault === 'true') {
+							node = document.createElement('option');
+							node.value = item.value;
+							node.innerHTML = item.text;
+							document.getElementById(select_id).appendChild(node);
+						}
+					});
+				},
+				onError: function(xhr)
+				{
+					// Remove js messages, if they exist.
+					Joomla.removeMessages();
 
-				Joomla.renderMessages(Joomla.ajaxErrorsMessages(xhr));
+					Joomla.renderMessages(Joomla.ajaxErrorsMessages(xhr));
+				}
 			}
-		}
-	);
-};
+		);
+	};
 
-document.addEventListener('DOMContentLoaded', function() {
-	document.getElementById('helpsite-refresh').addEventListener('click', resetHelpSiteList);
-});
+	document.addEventListener('DOMContentLoaded', function() {
+		document.getElementById('helpsite-refresh').addEventListener('click', Joomla.resetHelpSiteList);
+	});
+
+})();
