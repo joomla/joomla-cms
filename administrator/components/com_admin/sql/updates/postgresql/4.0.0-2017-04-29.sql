15196d26200c737e234b5c9770dc6dbc7346ee7b
Allon Moritz <allon.moritz@digital-peak.com>
2017-04-27 11:06:06 +0200

    Fix postgres sql scripts

--- administrator/components/com_admin/sql/updates/postgresql/4.0.0-2017-04-29.sql
+++ administrator/components/com_admin/sql/updates/postgresql/4.0.0-2017-04-29.sql
@@ -1,4 +1,4 @@
 ALTER TABLE ""#__extensions" ADD "autoload"" VARCHAR(255) NULL DEFAULT NULL AFTER "params";
 
 -- Update com_content with autoloader file
-UPDATE `#__extensions` SET `autoload` = 'autoloader.php' WHERE `name` = 'com_content';
+UPDATE `#__extensions` SET `autoload` = 'autoload.php' WHERE `name` = 'com_content';
