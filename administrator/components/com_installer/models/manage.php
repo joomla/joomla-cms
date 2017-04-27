929b285697ad2fc9f87fc4da891c0d5d3be335a0
Michael Babker <mbabker@flbab.com>
2017-04-26 19:37:35 -0500

    Include plugins group too

--- administrator/components/com_installer/models/manage.php
+++ administrator/components/com_installer/models/manage.php
@@ -274,6 +274,8 @@ class InstallerModelManage extends InstallerModel
 		$this->cleanCache('_system', 1);
 		$this->cleanCache('com_modules', 0);
 		$this->cleanCache('com_modules', 1);
+		$this->cleanCache('com_plugins', 0);
+		$this->cleanCache('com_plugins', 1);
 		$this->cleanCache('mod_menu', 0);
 		$this->cleanCache('mod_menu', 1);
 
