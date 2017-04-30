165ff7ada4f7bfbeb359163eaf5b34201c94412a
Wojciech Smoliński <wojsmol@wp.pl>
2017-04-30 06:46:00 +0200

    Update 3.7.0-2016-11-19.sql
    postgresql

--- administrator/components/com_admin/sql/updates/postgresql/3.7.0-2016-11-19.sql
+++ administrator/components/com_admin/sql/updates/postgresql/3.7.0-2016-11-19.sql
@@ -1,3 +1,2 @@
-ALTER TABLE "#__menu_types" ADD "client_id" int DEFAULT 0 NOT NULL;
-
+ALTER TABLE "#__menu_types" ADD "client_id" int DEFAULT 0 NOT NULL AFTER "description";
 UPDATE "#__menu" SET "published" = 1 WHERE "menutype" = 'main' OR "menutype" = 'menu';
