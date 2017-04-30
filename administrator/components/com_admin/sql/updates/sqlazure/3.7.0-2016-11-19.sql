e8d6cfeb2d6e17711c3f2d646e38b579a27c0b33
Wojciech Smoliński <wojsmol@wp.pl>
2017-04-30 06:51:13 +0200

    sqlazure

--- administrator/components/com_admin/sql/updates/sqlazure/3.7.0-2016-11-19.sql
+++ administrator/components/com_admin/sql/updates/sqlazure/3.7.0-2016-11-19.sql
@@ -1,3 +1,2 @@
-ALTER TABLE [#__menu_types] ADD [client_id] [tinyint] NOT NULL DEFAULT 0;
-
+ALTER TABLE [#__menu_types] ADD [client_id] [tinyint] NOT NULL DEFAULT 0  AFTER [description];
 UPDATE [#__menu] SET [published] = 1 WHERE [menutype] = 'main' OR [menutype] = 'menu';
