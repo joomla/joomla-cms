2ecdca581b73bd5e72285852e1f5711d07667f8d
Wojciech Smoliński <wojsmol@wp.pl>
2017-04-30 06:37:37 +0200

    Update 3.7.0-2016-11-19.sql
    Try fixing https://github.com/joomla/joomla-cms/issues/15629 and future consistency of the columns order.

--- administrator/components/com_admin/sql/updates/mysql/3.7.0-2016-11-19.sql
+++ administrator/components/com_admin/sql/updates/mysql/3.7.0-2016-11-19.sql
@@ -1,3 +1,2 @@
-ALTER TABLE `#__menu_types` ADD COLUMN `client_id` int(11) NOT NULL DEFAULT 0;
-
+ALTER TABLE `#__menu_types` ADD COLUMN `client_id` int(11) NOT NULL DEFAULT 0 AFTER `description`;
 UPDATE `#__menu` SET `published` = 1 WHERE `menutype` = 'main' OR `menutype` = 'menu';
