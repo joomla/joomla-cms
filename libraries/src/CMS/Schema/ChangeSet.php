9505684dac358cbd9783d5d7274a8aacaf76e37a
Brian Teeman <brian@teeman.net>
2017-04-30 16:28:17 +0100

    [4.0] Azure cleanup
    As azure is removed from J4 this completes the removal of references to it

--- libraries/src/CMS/Schema/ChangeSet.php
+++ libraries/src/CMS/Schema/ChangeSet.php
@@ -257,12 +257,6 @@ class ChangeSet
 		// Get the folder from the database name
 		$sqlFolder = $this->db->getServerType();
 
-		// For `mssql` server types, convert the type to `sqlazure`
-		if ($sqlFolder === 'mssql')
-		{
-			$sqlFolder = 'sqlazure';
-		}
-
 		// Default folder to core com_admin
 		if (!$this->folder)
 		{
