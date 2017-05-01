dd922e1d7d5bd7636439a1eb3fe5a4771ba07120
Nicola Galgano <optimus4joomla@gmail.com>
2017-05-01 08:09:29 +0200

    pseudo FK mysql
    REPLACE menutype "main" cause is reserved

--- administrator/components/com_admin/sql/updates/mysql/3.7.0-2017-01-17.sql
+++ administrator/components/com_admin/sql/updates/mysql/3.7.0-2017-01-17.sql
@@ -1,3 +1,3 @@
 -- Replace menutype = 'main' is reserved for backend so replace it
-UPDATE `#__menu` SET `#__menu`.`menutype` = 'menutype_main_is_reserved', `client_id` = 1 WHERE `#__menu`.`menutype` = 'main';
-UPDATE `#__menu_types` SET `#__menu_types`.`menutype` = 'menutype_main_is_reserved', `client_id` = 1 WHERE  `#__menu_types`.`menutype` = 'main';
+UPDATE `#__menu` SET `menutype` = 'menutype_main_is_reserved', `client_id` = 1 WHERE `menutype` = 'main';
+UPDATE `#__menu_types` SET `menutype` = 'menutype_main_is_reserved', `client_id` = 1 WHERE `menutype` = 'main';
