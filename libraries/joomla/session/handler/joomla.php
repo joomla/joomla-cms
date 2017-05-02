633c8906e405a6449a9b8219051e846f20121240
Phil E. Taylor <phil@phil-taylor.com>
2017-05-02 15:45:53 +0100

    Closes #12153 Closes #15680
    Check if there is a valid session before messing with session ini values
    (NOT TESTED, coded on sight only - please test!)

--- libraries/joomla/session/handler/joomla.php
+++ libraries/joomla/session/handler/joomla.php
@@ -42,11 +42,14 @@ class JSessionHandlerJoomla extends JSessionHandlerNative
 	 */
 	public function __construct($options = array())
 	{
-		// Disable transparent sid support
-		ini_set('session.use_trans_sid', '0');
+		if (session_id() === "")
+		{
+			// Disable transparent sid support
+			ini_set('session.use_trans_sid', '0');
 
-		// Only allow the session ID to come from cookies and nothing else.
-		ini_set('session.use_only_cookies', '1');
+			// Only allow the session ID to come from cookies and nothing else.
+			ini_set('session.use_only_cookies', '1');
+		}
 
 		// Set options
 		$this->setOptions($options);
