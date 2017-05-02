d81eec4b618b090beaafaf3ebd2dec1508927140
Phil E. Taylor <phil@phil-taylor.com>
2017-05-02 21:36:11 +0100

    better handling of multiple tab widgets per page (by full xpath) and not saving the state if there is no id in the url (like on the CREATE page of content. giving better future proofing as well

--- media/system/js/tabs-state.js
+++ media/system/js/tabs-state.js
@@ -5,30 +5,89 @@
 
 /**
  * JavaScript behavior to allow selected tab to be remained after save or page reload
- * keeping state in sessionStorage
+ * keeping state in sessionStorage with better handling of multiple tab widgets per page
+ * and not saving the state if there is no id in the url (like on the CREATE page of content)
  */
 
-jQuery(function($) {
-    var loadTabs = function() {
-        function saveActiveTab(href) {
-            // Remove the old entry if exists, key is always dependant on the url
-            // This should be removed in the future
-            if (sessionStorage.getItem('active-tab')) {
-                sessionStorage.removeItem('active-tab');
+jQuery(function ($) {
+
+    $.urlParam = function (name) {
+        var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+        if (results) {
+            return results[1];
+        } else {
+            return null;
+        }
+    }
+
+    function xpath(el) {
+        if (typeof el == "string") return document.evaluate(el, document, null, 0, null)
+        if (!el || el.nodeType != 1) return ''
+        if (el.id) return "//*[@id='" + el.id + "']"
+        var sames = [].filter.call(el.parentNode.children, function (x) {
+            return x.tagName == el.tagName
+        })
+        return xpath(el.parentNode) + '/' + el.tagName.toLowerCase() + (sames.length > 1 ? '[' + ([].indexOf.call(sames, el) + 1) + ']' : '')
+    }
+
+    (function ($) {
+        $.xpath = function (exp, ctxt) {
+            var item, coll = [],
+                result = document.evaluate(exp, ctxt || document, null, 5, null);
+
+            while (item = result.iterateNext())
+                coll.push(item);
+
+            return $(coll);
+        }
+    })(jQuery);
+
+    var loadTabs = function () {
+
+        function remove_item(activeTabsHrefs, tabCollection) {
+            var b = '';
+            for (b in activeTabsHrefs) {
+                if (activeTabsHrefs[b].indexOf(tabCollection) > -1) {
+                    activeTabsHrefs.splice(b, 1);
+                }
+            }
+            return activeTabsHrefs;
+        }
+
+        function getStorageKey() {
+            return window.location.href.toString().split(window.location.host)[1].replace(/&return=[a-zA-Z0-9%]+/, '').replace(/#[a-zA-Z0-9%]+/, '');
+        }
+
+        function saveActiveTab(event) {
+
+            if (null == $.urlParam('id')) return;
+
+            var href = $(event.target).attr('href');
+            var tabCollection = xpath($(event.target).closest('.nav-tabs').first().get(0));
+
+            if (!tabCollection || typeof href == 'undefined') return;
+
+            var storageValue = tabCollection + '|' + href;
+
+            var activeTabsHrefs = JSON.parse(sessionStorage.getItem(getStorageKey()));
+
+            if (!activeTabsHrefs) {
+                var activeTabsHrefs = [];
             }
 
             // Reset the array
-            activeTabsHrefs = [];
+            remove_item(activeTabsHrefs, tabCollection);
 
             // Save clicked tab href to the array
-            activeTabsHrefs.push(href);
+            activeTabsHrefs.push(storageValue);
 
             // Store the selected tabs hrefs in sessionStorage
-            sessionStorage.setItem(window.location.href.toString().split(window.location.host)[1].replace(/&return=[a-zA-Z0-9%]+/, '').replace(/&[a-zA-Z-_]+=[0-9]+/, ''), JSON.stringify(activeTabsHrefs));
+            sessionStorage.setItem(getStorageKey(), JSON.stringify(activeTabsHrefs));
         }
 
-        function activateTab(href) {
-            $('a[data-toggle="tab"][href="' + href + '"]').tab('show');
+        function activateTab(tabFakexPath) {
+            var parts = tabFakexPath.split('|');
+            jQuery.xpath(parts[0]).find('a[data-toggle="tab"][href="' + parts[1] + '"]').tab('show');
         }
 
         function hasTab(href) {
@@ -36,41 +95,29 @@ jQuery(function($) {
         }
 
         // Array with active tabs hrefs
-        var activeTabsHrefs = JSON.parse(sessionStorage.getItem(window.location.href.toString().split(window.location.host)[1].replace(/&return=[a-zA-Z0-9%]+/, '').replace(/&[a-zA-Z-_]+=[0-9]+/, '')));
+        var activeTabsHrefs = JSON.parse(sessionStorage.getItem(getStorageKey()));
 
         // jQuery object with all tabs links
         var $tabs = $('a[data-toggle="tab"]');
 
-        $tabs.on('click', function(e) {
-            saveActiveTab($(e.target).attr('href'));
+        $tabs.on('click', function (e) {
+            saveActiveTab(e);
         });
 
         if (activeTabsHrefs !== null) {
+
             // Clean default tabs
             $tabs.parent('.active').removeClass('active');
 
             // When moving from tab area to a different view
-            $.each(activeTabsHrefs, function(index, tabHref) {
-                if (!hasTab(tabHref)) {
-                    sessionStorage.removeItem(window.location.href.toString().split(window.location.host)[1].replace(/&return=[a-zA-Z0-9%]+/, '').replace(/&[a-zA-Z-_]+=[0-9]+/, ''));
-
-                    return true;
-                }
+            $.each(activeTabsHrefs, function (index, tabFakexPath) {
 
                 // Add active attribute for selected tab indicated by url
-                activateTab(tabHref);
+                activateTab(tabFakexPath);
 
-                // Check whether internal tab is selected (in format <tabname>-<id>)
-                var seperatorIndex = tabHref.indexOf('-');
-
-                if (seperatorIndex !== -1) {
-                    var singular = tabHref.substring(0, seperatorIndex);
-                    var plural = singular + "s";
-                    activateTab(plural);
-                }
             });
         } else {
-            $tabs.parents('ul').each(function(index, ul) {
+            $tabs.parents('ul').each(function (index, ul) {
                 // If no tabs is saved, activate first tab from each tab set and save it
                 var href = $(ul).find('a').first().tab('show').attr('href');
                 saveActiveTab(href);
