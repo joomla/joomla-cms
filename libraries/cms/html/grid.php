7fc666612c3aeec61d2efb6124414ed7ca5bbce1
Brian Teeman <brian@teeman.net>
2017-05-05 22:18:34 +0100

    codestyle

--- libraries/cms/html/grid.php
+++ libraries/cms/html/grid.php
@@ -146,7 +146,8 @@ abstract class JHtmlGrid
 	 */
 	public static function id($rowNum, $recId, $title ='', $checkedOut = false, $name = 'cid', $stub = 'cb')
 	{
-		return $checkedOut ? '' : '<label for="' . $stub . $rowNum . '"><span class="element-invisible">' . JText::_('JSELECT') . ' ' . $title . '</span> <input type="checkbox" id="' . $stub . $rowNum . '" name="' . $name . '[]" value="' . $recId
+		return $checkedOut ? '' : '<label for="' . $stub . $rowNum . '"><span class="element-invisible">' . JText::_('JSELECT')
+			. ' ' . $title . '</span> <input type="checkbox" id="' . $stub . $rowNum . '" name="' . $name . '[]" value="' . $recId
 			. '" onclick="Joomla.isChecked(this.checked);" />';
 	}
 
