9281b35c93f1cb4a46663f3bbda9887e26a18fd3
Brian Teeman <brian@teeman.net>
2017-05-05 19:56:59 +0100

    update library to use the title param

--- libraries/cms/html/grid.php
+++ libraries/cms/html/grid.php
@@ -143,9 +143,9 @@ abstract class JHtmlGrid
 	 *
 	 * @since   1.5
 	 */
-	public static function id($rowNum, $recId, $checkedOut = false, $name = 'cid', $stub = 'cb')
+	public static function id($rowNum, $recId, $title, $checkedOut = false, $name = 'cid', $stub = 'cb')
 	{
-		return $checkedOut ? '' : '<label for="' . $stub . $rowNum . '"><span class="element-invisible">' . JText::_('JSELECT') . ' ' . $recId . '</span> <input type="checkbox" id="' . $stub . $rowNum . '" name="' . $name . '[]" value="' . $recId
+		return $checkedOut ? '' : '<label for="' . $stub . $rowNum . '"><span class="element-invisible">' . JText::_('JSELECT') . ' ' . $title . '</span> <input type="checkbox" id="' . $stub . $rowNum . '" name="' . $name . '[]" value="' . $recId
 			. '" onclick="Joomla.isChecked(this.checked);" />';
 	}
 
