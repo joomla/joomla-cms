3ac4c3246633c304f624ca5b257afe5f3b35633d
Brian Teeman <brian@teeman.net>
2017-05-05 20:02:02 +0100

    banner client

--- administrator/components/com_banners/views/clients/tmpl/default.php
+++ administrator/components/com_banners/views/clients/tmpl/default.php
@@ -94,7 +94,7 @@ $params     = (isset($this->state->params)) ? $this->state->params : new JObject
 						?>
 						<tr class="row<?php echo $i % 2; ?>">
 							<td class="center">
-								<?php echo JHtml::_('grid.id', $i, $item->id); ?>
+								<?php echo JHtml::_('grid.id', $i, $item->id, $item->name); ?>
 							</td>
 							<td class="center">
 								<div class="btn-group">
