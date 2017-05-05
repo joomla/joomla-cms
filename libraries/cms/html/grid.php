e045d94debaa54c4d6bcae4049e43f671908cf1e
Brian Teeman <brian@teeman.net>
2017-05-05 19:57:56 +0100

    docblock

--- libraries/cms/html/grid.php
+++ libraries/cms/html/grid.php
@@ -138,6 +138,7 @@ abstract class JHtmlGrid
 	 * @param   boolean  $checkedOut  True if item is checked out
 	 * @param   string   $name        The name of the form element
 	 * @param   string   $stub        The name of stub identifier
+	 * @param   string   $title       The name of the item
 	 *
 	 * @return  mixed    String of html with a checkbox if item is not checked out, null if checked out.
 	 *
