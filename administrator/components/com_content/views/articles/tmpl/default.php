744f466716ea1b98bd9568cead01bdfe11e65952
Thomas Hunziker <werbemails@bakual.ch>
2017-05-05 10:08:20 +0200

    Fix vote ordering (#15655)
    * Fixing ordering by rating and rating_count (don't use fullordering!)
    
    * Removing logic to check for the vote plugin from the JFormFieldList and add a custom formfield to handle that specific case.
    
    * Update votelist.php

--- administrator/components/com_content/views/articles/tmpl/default.php
+++ administrator/components/com_content/views/articles/tmpl/default.php
@@ -18,7 +18,7 @@ JHtml::_('formbehavior.chosen', 'select');
 $app       = JFactory::getApplication();
 $user      = JFactory::getUser();
 $userId    = $user->get('id');
-$listOrder = str_replace(' ' . $this->state->get('list.direction'), '', $this->state->get('list.fullordering'));
+$listOrder = $this->escape($this->state->get('list.ordering'));
 $listDirn  = $this->escape($this->state->get('list.direction'));
 $saveOrder = $listOrder == 'a.ordering';
 $columns   = 10;
--- administrator/components/com_content/views/featured/tmpl/default.php
+++ administrator/components/com_content/views/featured/tmpl/default.php
@@ -17,7 +17,7 @@ JHtml::_('formbehavior.chosen', 'select');
 
 $user      = JFactory::getUser();
 $userId    = $user->get('id');
-$listOrder = str_replace(' ' . $this->state->get('list.direction'), '', $this->state->get('list.fullordering'));
+$listOrder = $this->escape($this->state->get('list.ordering'));
 $listDirn  = $this->escape($this->state->get('list.direction'));
 $saveOrder = $listOrder == 'fp.ordering';
 $columns   = 10;
