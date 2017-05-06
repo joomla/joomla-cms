7541040a591e592b0792f51a3dc203b7f6c38e7a
Dimitris Grammatikogiannis <d.grammatiko@gmail.com>
2017-05-06 18:31:32 +0300

    Automatic language detection, links->select

--- installation/template/js/error.js
+++ installation/template/js/error.js
@@ -5,22 +5,21 @@
 			text1 = document.getElementById('descText1');
 
 		// Get the minimum PHP version
-		window.phpVersion = document.body.getAttribute('data-php-version');
+		window.phpVersion = document.body.getAttribute('data-php-version') || '5.5.9';
 
 		// Create links for all the languages
 		Object.keys(errorLocale).forEach(function(key) {
-			var ul = document.getElementById('translatedLanguages'), li, aLink;
-			li = document.createElement('li');
-			aLink = document.createElement('a');
-			aLink.setAttribute('href', '#');
-			aLink.innerHTML = errorLocale[key].language;
-			aLink.setAttribute('data-code', key);
+			var sel = document.getElementById('translatedLanguagesSelect');
+			var opt = document.createElement('option');
+			opt.text = errorLocale[key].language;
+			opt.value = key;
 
-			// Override click functionality for the link
-			aLink.addEventListener('click', function(e) {
-				e.preventDefault();
-				e.stopPropagation();
-				var ref = e.target.getAttribute('data-code');
+			if (key === 'en-GB') {
+				opt.setAttribute('selected', 'selected');
+			}
+
+			document.getElementById('translatedLanguagesSelect').addEventListener('change', function(e) {
+				var ref = e.target.value;
 				if (ref) {
 					header.innerHTML = errorLocale[ref].header;
 					text1.innerHTML = errorLocale[ref].text1.replace('{{phpversion}}', phpVersion);
@@ -32,8 +31,27 @@
 					helpLink.innerText = errorLocale[ref]["help-url-text"];
 				}
 			});
-			li.appendChild(aLink);
-			ul.appendChild(li);
+
+			sel.appendChild(opt)
+		});
+
+		// Select language based on Browser's language
+		Object.keys(errorLocale).forEach(function(key) {
+			if (navigator.language === key) {
+				// Remove the selected property
+				document.querySelector('#translatedLanguagesSelect option[value="en-GB"]').removeAttribute('selected');
+				document.querySelector("#translatedLanguagesSelect option[value='" + key + "']").setAttribute('selected', 'selected');
+
+				// Append the translated strings
+				header.innerHTML = errorLocale[key].header;
+				text1.innerHTML = errorLocale[key].text1.replace('{{phpversion}}', phpVersion);
+
+				var helpLink = document.getElementById('linkHelp');
+				if (helpLink) {
+					helpLink.href = errorLocale[key]["help-url"];
+					helpLink.innerText = errorLocale[key]["help-url-text"];
+				}
+			}
 		});
 	}
 })();
