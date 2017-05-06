2f57cfb5fad0a9f62d55704ff8e92fdb0c557134
wojsmol <wojsmol@wp.pl>
2017-05-06 14:04:35 +0200

    add COLUNN

--- administrator/components/com_admin/sql/updates/postgresql/3.7.0-2016-11-19.sql
+++ administrator/components/com_admin/sql/updates/postgresql/3.7.0-2016-11-19.sql
@@ -1,2 +1,2 @@
-ALTER TABLE "#__menu_types" ADD "client_id" int DEFAULT 0 NOT NULL AFTER "description";
+ALTER TABLE "#__menu_types" ADD COLUMN "client_id" int DEFAULT 0 NOT NULL AFTER "description";
 UPDATE "#__menu" SET "published" = 1 WHERE "menutype" = 'main' OR "menutype" = 'menu';
