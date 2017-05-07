06058fbdd6fc20fdfdb4dde0c6fdc45f740075d9
Michael Babker <mbabker@flbab.com>
2017-05-07 14:40:16 -0500

    Implementation of the preload manager

--- libraries/src/CMS/Document/PreloadManager.php
+++ libraries/src/CMS/Document/PreloadManager.php
@@ -10,15 +10,37 @@ namespace Joomla\CMS\Document;
 
 defined('_JEXEC') or die;
 
+use Fig\Link\GenericLinkProvider;
+use Fig\Link\Link;
 use Psr\Link\EvolvableLinkProviderInterface;
 
 /**
- * Joomla! Preload Manager Interface
+ * Joomla! Preload Manager
  *
  * @since  __DEPLOY_VERSION__
  */
-interface PreloadManagerInterface
+class PreloadManager implements PreloadManagerInterface
 {
+	/**
+	 * The link provider
+	 *
+	 * @var    EvolvableLinkProviderInterface
+	 * @since  __DEPLOY_VERSION__
+	 */
+	protected $linkProvider;
+
+	/**
+	 * PreloadManager constructor
+	 *
+	 * @param   EvolvableLinkProviderInterface  $linkProvider  The link provider
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 */
+	public function __construct(EvolvableLinkProviderInterface $linkProvider = null)
+	{
+		$this->linkProvider = $linkProvider ?: new GenericLinkProvider;
+	}
+
 	/**
 	 * Get the link provider
 	 *
@@ -26,7 +48,10 @@ interface PreloadManagerInterface
 	 *
 	 * @since   __DEPLOY_VERSION__
 	 */
-	public function getLinkProvider();
+	public function getLinkProvider()
+	{
+		return $this->linkProvider;
+	}
 
 	/**
 	 * Set the link provider
@@ -37,7 +62,12 @@ interface PreloadManagerInterface
 	 *
 	 * @since   __DEPLOY_VERSION__
 	 */
-	public function setLinkProvider(EvolvableLinkProviderInterface $linkProvider);
+	public function setLinkProvider(EvolvableLinkProviderInterface $linkProvider)
+	{
+		$this->linkProvider = $linkProvider;
+
+		return $this;
+	}
 
 	/**
 	 * Preloads a resource.
@@ -49,7 +79,10 @@ interface PreloadManagerInterface
 	 *
 	 * @since   __DEPLOY_VERSION__
 	 */
-	public function preload($uri, array $attributes = []);
+	public function preload($uri, array $attributes = [])
+	{
+		$this->link($uri, 'preload', $attributes);
+	}
 
 	/**
 	 * Resolves a resource origin as early as possible.
@@ -61,7 +94,10 @@ interface PreloadManagerInterface
 	 *
 	 * @since   __DEPLOY_VERSION__
 	 */
-	public function dnsPrefetch($uri, array $attributes = []);
+	public function dnsPrefetch($uri, array $attributes = [])
+	{
+		$this->link($uri, 'dns-prefetch', $attributes);
+	}
 
 	/**
 	 * Initiates a early connection to a resource (DNS resolution, TCP handshake, TLS negotiation).
@@ -73,7 +109,10 @@ interface PreloadManagerInterface
 	 *
 	 * @since   __DEPLOY_VERSION__
 	 */
-	public function preconnect($uri, array $attributes = []);
+	public function preconnect($uri, array $attributes = [])
+	{
+		$this->link($uri, 'preconnect', $attributes);
+	}
 
 	/**
 	 * Indicates to the client that it should prefetch this resource.
@@ -85,7 +124,10 @@ interface PreloadManagerInterface
 	 *
 	 * @since   __DEPLOY_VERSION__
 	 */
-	public function prefetch($uri, array $attributes = []);
+	public function prefetch($uri, array $attributes = [])
+	{
+		$this->link($uri, 'prefetch', $attributes);
+	}
 
 	/**
 	 * Indicates to the client that it should prerender this resource.
@@ -97,5 +139,31 @@ interface PreloadManagerInterface
 	 *
 	 * @since   __DEPLOY_VERSION__
 	 */
-	public function prerender($uri, array $attributes = []);
+	public function prerender($uri, array $attributes = [])
+	{
+		$this->link($uri, 'prerender', $attributes);
+	}
+
+	/**
+	 * Adds a "Link" HTTP header.
+	 *
+	 * @param   string  $uri         The relation URI
+	 * @param   string  $rel         The relation type (e.g. "preload", "prefetch", "prerender" or "dns-prefetch")
+	 * @param   array   $attributes  The attributes of this link (e.g. "array('as' => true)", "array('pr' => 0.5)")
+	 *
+	 * @return  void
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 */
+	private function link($uri, $rel, array $attributes = [])
+	{
+		$link = new Link($rel, $uri);
+
+		foreach ($attributes as $key => $value)
+		{
+			$link = $link->withAttribute($key, $value);
+		}
+
+		$this->setLinkProvider($this->getLinkProvider()->withLink($link));
+	}
 }
