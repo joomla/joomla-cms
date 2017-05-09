c80b1e9c62561cdf3e468d49a430391ff811dce6
Allon Moritz <allon.moritz@digital-peak.com>
2017-05-09 16:43:07 +0200

    argh

--- tests/unit/suites/libraries/joomla/JLoaderTest.php
+++ tests/unit/suites/libraries/joomla/JLoaderTest.php
@@ -107,7 +107,7 @@ class JLoaderTest extends \PHPUnit\Framework\TestCase
 	{
 		return array(
 			'fred.factory' => array('fred.factory', false, 'fred.factory does not exist'),
-			'browser' => array('joomla.class.loader', true, 'JClassLoader should load properly'));
+			'classloader' => array('cms.class.loader', true, 'JClassLoader should load properly'));
 	}
 
 	/**
