a5dd0b16f7201b27e1b7695c676038fe8de66374
Robert Deutz <rdeutz@googlemail.com>
2017-05-15 11:21:05 +0200

    Prepare 3.7.1-rc2 release

--- libraries/cms/version/version.php
+++ libraries/cms/version/version.php
@@ -38,7 +38,7 @@ final class JVersion
 	 * @var    string
 	 * @since  3.5
 	 */
-	const DEV_LEVEL = '1-dev';
+	const DEV_LEVEL = '1-rc2';
 
 	/**
 	 * Development status.
@@ -46,7 +46,7 @@ final class JVersion
 	 * @var    string
 	 * @since  3.5
 	 */
-	const DEV_STATUS = 'Development';
+	const DEV_STATUS = 'Release Candidate';
 
 	/**
 	 * Build number.
@@ -70,7 +70,7 @@ final class JVersion
 	 * @var    string
 	 * @since  3.5
 	 */
-	const RELDATE = '12-May-2017';
+	const RELDATE = '15-May-2017';
 
 	/**
 	 * Release time.
@@ -78,7 +78,7 @@ final class JVersion
 	 * @var    string
 	 * @since  3.5
 	 */
-	const RELTIME = '17:56';
+	const RELTIME = '10:20';
 
 	/**
 	 * Release timezone.
