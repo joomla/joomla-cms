95334aed4fa5507d188fdb23608a946ff612bfb8
Robert Deutz <rdeutz@googlemail.com>
2017-05-15 20:18:55 +0200

    consistent/valid status codes

--- libraries/joomla/application/web.php
+++ libraries/joomla/application/web.php
@@ -82,6 +82,9 @@ class JApplicationWeb extends JApplicationBase
 	 * @see    http://tools.ietf.org/pdf/rfc7231.pdf
 	 */
 	private $responseMap = array(
+		100	=> 'HTTP/1.1 100 Continue',
+		101	=> 'HTTP/1.1 101 Switching Protocols',
+		102	=> 'HTTP/1.1 102 Processing',
 		300 => 'HTTP/1.1 300 Multiple Choices',
 		301 => 'HTTP/1.1 301 Moved Permanently',
 		302 => 'HTTP/1.1 302 Found',
@@ -91,7 +94,54 @@ class JApplicationWeb extends JApplicationBase
 		306 => 'HTTP/1.1 306 (Unused)',
 		307 => 'HTTP/1.1 307 Temporary Redirect',
 		308 => 'HTTP/1.1 308 Permanent Redirect',
-	);
+		200	=> 'HTTP/1.1 200 OK',
+		201	=> 'HTTP/1.1 201 Created',
+		202	=> 'HTTP/1.1 202 Accepted',
+		203	=> 'HTTP/1.1 203 Non-Authoritative Information',
+		204	=> 'HTTP/1.1 204 No Content',
+		205	=> 'HTTP/1.1 205 Reset Content',
+		206	=> 'HTTP/1.1 206 Partial Content',
+		207	=> 'HTTP/1.1 207 Multi-Status',
+		208	=> 'HTTP/1.1 208 Already Reported',
+		226	=> 'HTTP/1.1 226 IM Used',
+		400	=> 'HTTP/1.1 400 Bad Request',
+		401	=> 'HTTP/1.1 401 Unauthorized',
+		402	=> 'HTTP/1.1 402 Payment Required',
+		403	=> 'HTTP/1.1 403 Forbidden',
+		404	=> 'HTTP/1.1 404 Not Found',
+		405	=> 'HTTP/1.1 405 Method Not Allowed',
+		406	=> 'HTTP/1.1 406 Not Acceptable',
+		407	=> 'HTTP/1.1 407 Proxy Authentication Required',
+		408	=> 'HTTP/1.1 408 Request Timeout',
+		409	=> 'HTTP/1.1 409 Conflict',
+		410	=> 'HTTP/1.1 410 Gone',
+		411	=> 'HTTP/1.1 411 Length Required',
+		412	=> 'HTTP/1.1 412 Precondition Failed',
+		413	=> 'HTTP/1.1 413 Payload Too Large',
+		414	=> 'HTTP/1.1 414 URI Too Long',
+		415	=> 'HTTP/1.1 415 Unsupported Media Type',
+		416	=> 'HTTP/1.1 416 Range Not Satisfiable',
+		417	=> 'HTTP/1.1 417 Expectation Failed',
+		421	=> 'HTTP/1.1 421 Misdirected Request',
+		422	=> 'HTTP/1.1 422 Unprocessable Entity',
+		423	=> 'HTTP/1.1 423 Locked',
+		424	=> 'HTTP/1.1 424 Failed Dependency',
+		426	=> 'HTTP/1.1 426 Upgrade Required',
+		428	=> 'HTTP/1.1 428 Precondition Required',
+		429	=> 'HTTP/1.1 429 Too Many Requests',
+		431	=> 'HTTP/1.1 431 Request Header Fields Too Large',
+		451	=> 'HTTP/1.1 451 Unavailable For Legal Reasons',
+		500	=> 'HTTP/1.1 500 Internal Server Error',
+		501	=> 'HTTP/1.1 501 Not Implemented',
+		502	=> 'HTTP/1.1 502 Bad Gateway',
+		503	=> 'HTTP/1.1 503 Service Unavailable',
+		504	=> 'HTTP/1.1 504 Gateway Timeout',
+		505	=> 'HTTP/1.1 505 HTTP Version Not Supported',
+		506	=> 'HTTP/1.1 506 Variant Also Negotiates',
+		507	=> 'HTTP/1.1 507 Insufficient Storage',
+		508	=> 'HTTP/1.1 508 Loop Detected',
+		510	=> 'HTTP/1.1 510 Not Extended',
+		511	=> 'HTTP/1.1 511 Network Authentication Required',	);
 
 	/**
          * A map of HTTP Response headers which may only send a single value, all others
@@ -550,7 +600,7 @@ class JApplicationWeb extends JApplicationBase
 			}
 			else
 			{
-				// Check if we have a boolean for the status variable for compatability with old $move parameter
+				// Check if we have a boolean for the status variable for compatibility with old $move parameter
 				// @deprecated 4.0
 				if (is_bool($status))
 				{
@@ -660,10 +710,10 @@ class JApplicationWeb extends JApplicationBase
 		}
 
 		/**
-                 * If no keys found, safe to insert (!$keys)
-                 * If ($keys && $replace) it's a replacement and previous have been deleted
-                 * if($keys && !in_array...) it's a multiple value header
-                 */
+         * If no keys found, safe to insert (!$keys)
+         * If ($keys && $replace) it's a replacement and previous have been deleted
+         * if($keys && !in_array...) it's a multiple value header
+         */
 		$single = in_array($name, $this->singleValueResponseHeaders);
 		if ($value && (!$keys || ($keys && ($replace || !$single))))
 		{
@@ -719,7 +769,9 @@ class JApplicationWeb extends JApplicationBase
 				if ('status' == strtolower($header['name']))
 				{
 					// 'status' headers indicate an HTTP status, and need to be handled slightly differently
-					$this->header('HTTP/1.1 ' . (int) $header['value'], true);
+					$status = $this->getHttpStatusValue($header['value']);
+
+					$this->header($status, true, (int) $header['value']);
 				}
 				else
 				{
@@ -730,6 +782,27 @@ class JApplicationWeb extends JApplicationBase
 		}
 
 		return $this;
+	}
+
+	/**
+	 * Check is a given value can be successfully mapped to a valid http status value
+	 *
+	 * @param   string  $value  The given status as int or string
+	 *
+	 * @return string
+	 *
+	 * @since  __DEPLOY_VERSION__
+	 */
+	protected function getHttpStatusValue($value)
+	{
+		$code = (int) $value;
+
+		if (array_key_exists($code, $this->responseMap))
+		{
+			return $this->responseMap[$code];
+		}
+
+		return 'HTTP/1.1 ' . $code;
 	}
 
 	/**
