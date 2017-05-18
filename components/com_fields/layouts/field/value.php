9ee9d1010781d13f098ea23d734b3c69ccbe0306
coolcat-creations <mail@coolcat-creations.com>
2017-05-18 11:48:27 +0200

    Update value.php
    Reduce Code

--- components/com_fields/layouts/field/value.php
+++ components/com_fields/layouts/field/value.php
@@ -6,19 +6,5 @@
  * @copyright   Copyright (C) 2005 - 2017 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
-defined('_JEXEC') or die;
-
-if (!key_exists('field', $displayData))
-{
-	return;
-}
-
-$field = $displayData['field'];
-$value = $field->value;
-
-if ($value == '')
-{
-	return;
-}
-?>
+defined('_JEXEC') or die; ?>
 <?php echo $value; ?>
