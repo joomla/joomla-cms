736f7283e805d4fb2bd58384897a1f9fb7356c2d
Robert Deutz <rdeutz@googlemail.com>
2017-05-20 21:35:28 +0200

    remove stub

--- tests/unit/suites/libraries/cms/helper/JHelperMediaTest.php
+++ tests/unit/suites/libraries/cms/helper/JHelperMediaTest.php
@@ -7,7 +7,7 @@
  * @license	    GNU General Public License version 2 or later; see LICENSE
  */
 
-require_once 'stubs/JComponentHelper.php';
+//require_once 'stubs/JComponentHelper.php';
 
 /**
  * Test class for JHelperMedia.
