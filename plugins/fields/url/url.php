d4afda76b9fb610505816e7bf38b0fe470fbbe13
Allon Moritz <allon.moritz@digital-peak.com>
2017-05-22 17:35:28 +0200

    Use the namespaced plugin base classes in the fields plugins

--- plugins/fields/url/url.php
+++ plugins/fields/url/url.php
@@ -9,14 +9,12 @@
 
 defined('_JEXEC') or die;
 
-JLoader::import('components.com_fields.libraries.fieldsplugin', JPATH_ADMINISTRATOR);
-
 /**
  * Fields URL Plugin
  *
  * @since  3.7.0
  */
-class PlgFieldsUrl extends FieldsPlugin
+class PlgFieldsUrl extends \Joomla\Component\Fields\Administrator\Plugin\FieldsPlugin
 {
 	/**
 	 * Transforms the field into a DOM XML element and appends it as a child on the given parent.
