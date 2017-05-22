3ec9b4a83a67100288e127b757742611993c395d
Florian Häusler <florian@hr-it-solutions.com>
2017-05-22 20:46:06 +0200

    Implementation of the new Exception-Handling (#15717)
    * Removed unneeded unreachable statement
    
    * Exception code must be second paramter
    
    * Implemented new exception handling (JError is deprecated)
    
    * Implemented new exception handling (JError is deprecated). Status Code for "not authorized" should be 403 Forbidden.

--- administrator/components/com_menus/views/items/view.html.php
+++ administrator/components/com_menus/views/items/view.html.php
@@ -64,9 +64,7 @@ class MenusViewItems extends JViewLegacy
 		// Check for errors.
 		if (count($errors = $this->get('Errors')))
 		{
-			JError::raiseError(500, implode("\n", $errors));
-
-			return false;
+			throw new Exception(implode("\n", $errors), 500);
 		}
 
 		$this->ordering = array();
