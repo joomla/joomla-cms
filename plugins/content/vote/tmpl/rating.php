bd63db72e53883288f99866ca5c910f9769aeafc
Phil E. Taylor <phil@phil-taylor.com>
2017-05-23 14:57:57 +0100

    Tweaks to force a build to pass

--- plugins/content/vote/tmpl/rating.php
+++ plugins/content/vote/tmpl/rating.php
@@ -27,12 +27,11 @@ if ($context == 'com_content.categories')
 
 $rating = (int) $row->rating;
 
-$img = '';
-
 // Look for images in template if available
 $starImageOn  = JHtml::_('image', 'system/rating_star.png', JText::_('PLG_VOTE_STAR_ACTIVE'), null, true);
 $starImageOff = JHtml::_('image', 'system/rating_star_blank.png', JText::_('PLG_VOTE_STAR_INACTIVE'), null, true);
 
+$img = '';
 for ($i = 0; $i < $rating; $i++)
 {
 	$img .= $starImageOn;
