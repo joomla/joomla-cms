8d2e00ca54355f509733d101a6c09c52ba83bbd5
Allon Moritz <allon.moritz@digital-peak.com>
2017-05-25 12:24:07 +0200

    Move JDate to namespace (#15900)
    * Move JDate to namespace
    
    * Change JFactory to use the namespaced date class
    
    * Load the cms before JError to have the namespaced date class available
    
    * Merge remote-tracking branch 'remotes/upstream/3.8-dev' into j3/namesapce/date
    
    # Conflicts:
    #	libraries/classmap.php

--- libraries/joomla/log/entry.php
+++ libraries/joomla/log/entry.php
@@ -28,7 +28,7 @@ class JLogEntry
 
 	/**
 	 * The date the message was logged.
-	 * @var    JDate
+	 * @var    Date
 	 * @since  11.1
 	 */
 	public $date;
@@ -102,7 +102,7 @@ class JLogEntry
 		// Get the current call stack and back trace (without args to save memory).
 		$this->callStack = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS);
 
-		// Get the date as a JDate object.
-		$this->date = new JDate($date ? $date : 'now');
+		// Get the date as a Date object.
+		$this->date = new Joomla\CMS\Date\Date($date ? $date : 'now');
 	}
 }
