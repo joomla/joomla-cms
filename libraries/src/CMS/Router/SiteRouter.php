cc1199e2da3995a3872f73ca29db225fc3fb4c56
George Wilson <georgejameswilson@googlemail.com>
2017-05-29 16:03:35 +0100

    Fix drone

--- libraries/src/CMS/Router/SiteRouter.php
+++ libraries/src/CMS/Router/SiteRouter.php
@@ -129,9 +129,11 @@ class SiteRouter extends Router
 		// Decode URL to convert percent-encoding to unicode so that strings match when routing.
 		$path = urldecode($uri->getPath());
 
-		// In some environments (e.g. CLI we can't form a valid base URL). In this case we catch the exception thrown
-		// by URI and set an empty base URI for further work.
-		// TODO: This should probably be handled better
+		/**
+		 * In some environments (e.g. CLI we can't form a valid base URL). In this case we catch the exception thrown
+		 * by URI and set an empty base URI for further work.
+		 * TODO: This should probably be handled better
+		 */
 		try
 		{
 			$baseUri = \JUri::base(true);
