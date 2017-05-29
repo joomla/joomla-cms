d408e88fd3aaa82267039fd6e364262b3ca88022
wilsonge <georgejameswilson@googlemail.com>
2017-05-29 18:03:10 +0100

    Start fixing mailto

--- components/com_mailto/dispatcher.php
+++ components/com_mailto/dispatcher.php
@@ -10,6 +10,7 @@
 defined('_JEXEC') or die;
 
 use Joomla\CMS\Dispatcher\Dispatcher;
+use Joomla\CMS\Controller\Controller;
 
 /**
  * Dispatcher class for com_mailto
@@ -26,4 +27,38 @@ class MailtoDispatcher extends Dispatcher
 	 * @since  __DEPLOY_VERSION__
 	 */
 	protected $namespace = 'Joomla\\Component\\Mailto';
+
+	/**
+	 * Get a controller from the component
+	 *
+	 * @param   string $name   Controller name
+	 * @param   string $client Optional client (like Administrator, Site etc.)
+	 * @param   array  $config Optional controller config
+	 *
+	 * @return  Controller
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 */
+	public function getController($name, $client = null, $config = array())
+	{
+		$controller = parent::getController($name, $client, $config);
+		$controller->registerDefaultTask('mailto');
+
+		return $controller;
+	}
+
+	/**
+	 * Dispatch a controller task. Redirecting the user if appropriate.
+	 *
+	 * @return  void
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 */
+	public function dispatch()
+	{
+		$command = $this->input->getCmd('task', 'mailto');
+		$this->input->set('task', $command);
+
+		parent::dispatch();
+	}
 }
