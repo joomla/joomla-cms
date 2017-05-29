3e0d863dbf89be521e16d1ac4783ce391dda92f9
Allon Moritz <allon.moritz@digital-peak.com>
2017-05-29 11:16:00 +0200

    Namespace user (#16214)
    * Namespace user
    
    * Namespace user
    
    * Use ns'd hash class
    
    * Merge remote-tracking branch 'remotes/upstream/3.8-dev' into j3/namespace/user
    
    # Conflicts:
    #	libraries/classmap.php
    #	libraries/src/Joomla/CMS/User/UserHelper.php

--- libraries/classmap.php
+++ libraries/classmap.php
@@ -200,6 +200,9 @@ JLoader::registerAlias('JRouter',                           '\\Joomla\\CMS\\Rout
 JLoader::registerAlias('JRouterAdministrator',              '\\Joomla\\CMS\\Router\\AdministratorRouter', '4.0');
 JLoader::registerAlias('JRouterSite',                       '\\Joomla\\CMS\\Router\\SiteRouter', '4.0');
 
+JLoader::registerAlias('JCategories',                       '\\Joomla\\CMS\\Categories\\Categories', '4.0');
+JLoader::registerAlias('JCategoryNode',                     '\\Joomla\\CMS\\Categories\\CategoryNode', '4.0');
+
 JLoader::registerAlias('JDate',                             '\\Joomla\\CMS\\Date\\Date', '4.0');
 
 JLoader::registerAlias('JLog',                              '\\Joomla\\CMS\\Log\\Log', '4.0');
@@ -238,3 +241,7 @@ JLoader::registerAlias('JCacheStorageXcache',               '\\Joomla\\CMS\\Cach
 JLoader::registerAlias('JCacheException',                   '\\Joomla\\CMS\\Cache\\Exception\\CacheExceptionInterface', '4.0');
 JLoader::registerAlias('JCacheExceptionConnecting',         '\\Joomla\\CMS\\Cache\\Exception\\CacheConnectingException', '4.0');
 JLoader::registerAlias('JCacheExceptionUnsupported',        '\\Joomla\\CMS\\Cache\\Exception\\UnsupportedCacheException', '4.0');
+
+JLoader::registerAlias('JUser',                             '\\Joomla\\CMS\\User\\User', '4.0');
+JLoader::registerAlias('JUserHelper',                       '\\Joomla\\CMS\\User\\UserHelper', '4.0');
+JLoader::registerAlias('JUserWrapperHelper',                '\\Joomla\\CMS\\User\\UserWrapper', '4.0');
