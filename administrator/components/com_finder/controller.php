12483fc1fd93812432ce4c6491868fe9bd8de1fc
Brian Teeman <brian@teeman.net>
2017-05-29 13:16:44 +0100

    LICENSE.txt
    The licence file we include is named LICENSE.txt but 748 files referred to just LICENSE
    
    NB I excluded vendor supplied files obviously AND fof and libraries/joomla
    as my understanding that any changes for these should be made upstream. If that is wrong I can always update this PR

--- administrator/components/com_finder/controller.php
+++ administrator/components/com_finder/controller.php
@@ -4,7 +4,7 @@
  * @subpackage  com_finder
  *
  * @copyright   Copyright (C) 2005 - 2017 Open Source Matters, Inc. All rights reserved.
- * @license     GNU General Public License version 2 or later; see LICENSE
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 defined('_JEXEC') or die;
--- tests/unit/suites/libraries/legacy/view/JViewLegacyTest.php
+++ tests/unit/suites/libraries/legacy/view/JViewLegacyTest.php
@@ -4,7 +4,7 @@
  * @subpackage  View
  *
  * @copyright   Copyright (C) 2005 - 2017 Open Source Matters, Inc. All rights reserved.
- * @license     GNU General Public License version 2 or later; see LICENSE
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 JLoader::register('ContentViewArticle', __DIR__ . '/stubs/ContentViewArticle.php');
