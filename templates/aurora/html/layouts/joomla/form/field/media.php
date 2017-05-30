6b2cc119dc9e90bb68c6550f42e6dc39851dfaf5
Lodder <lodder15@hotmail.co.uk>
2017-05-30 10:32:34 +0100

    [4.0] com_finder improvements + polyfills/mootools removal (#16324)
    * re-do com_finder improvements
    
    * re-do removal of polyfills
    
    * re-do removal of double dollar symbol
    
    * re-do removal of mootools
    
    * Fix PHP notices
    
    * rework results page a little
    
    * cleanup + fix switcher
    
    * Fix auto expand
    
    * Revert back JHtml for now
    
    * Update html.php
    
    * stupid Travis
    
    * Remove deprecated function instead of just the comment
    
    * Remove spaces

--- templates/aurora/html/layouts/joomla/form/field/media.php
+++ templates/aurora/html/layouts/joomla/form/field/media.php
@@ -86,7 +86,7 @@ if ($showPreview)
 $url    = ($readonly ? ''
 	: ($link ?: 'index.php?option=com_media&amp;view=images&amp;tmpl=component&amp;asset='
 		. $asset . '&amp;author=' . $authorId)
-	. '&amp;fieldid={field-media-id}&amp;ismoo=0&amp;folder=' . $folder);
+	. '&amp;fieldid={field-media-id}&amp;folder=' . $folder);
 ?>
 <div class="field-media-wrapper"
 	data-basepath="<?php echo JUri::root(); ?>"
