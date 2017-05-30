d845eb76a47a922a6a31c8b8b10b4417fc48dbcd
Matias Aguirre <maguirre@matware.com.ar>
2017-05-30 11:24:49 -0300

    Merge with upstream

--- libraries/classmap.php
+++ libraries/classmap.php
@@ -197,6 +197,10 @@ JLoader::registerAlias('JDocumentRendererMessage',          '\\Joomla\\CMS\\Docu
 JLoader::registerAlias('JDocumentRendererModule',           '\\Joomla\\CMS\\Document\\Renderer\\Html\\RendererHtmlModule', '5.0');
 JLoader::registerAlias('JDocumentRendererModules',          '\\Joomla\\CMS\\Document\\Renderer\\Html\\RendererHtmlModules', '5.0');
 
+JLoader::registerAlias('JFilterInput',                      '\\Joomla\\CMS\\Filter\\InputFilter', '5.0');
+JLoader::registerAlias('JFilterOutput',                     '\\Joomla\\CMS\\Filter\\OutputFilter', '5.0');
+JLoader::registerAlias('JFilterWrapperOutput',              '\\Joomla\\CMS\\Filter\\Wrapper\\OutputFilterWrapper', '5.0');
+
 JLoader::registerAlias('JInstaller',                        '\\Joomla\\CMS\\Installer\\Installer', '5.0');
 JLoader::registerAlias('JInstallerAdapter',                 '\\Joomla\\CMS\\Installer\\InstallerAdapter', '5.0');
 JLoader::registerAlias('JInstallerExtension',               '\\Joomla\\CMS\\Installer\\InstallerExtension', '5.0');
