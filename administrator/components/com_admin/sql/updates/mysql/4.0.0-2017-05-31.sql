b9c9b3dd9ed1d869ca1980d15882e926f8b67c13
Yves Hoppe <yves.hoppe@community.joomla.org>
2017-05-31 12:59:43 +0200

    Update menu links on update to j4

--- administrator/components/com_admin/sql/updates/mysql/4.0.0-2017-05-31.sql
+++ administrator/components/com_admin/sql/updates/mysql/4.0.0-2017-05-31.sql
@@ -1,2 +1,2 @@
-UPDATE `#__extensions` SET `link` = 'index.php?option=com_config&view=config' WHERE `link` = 'index.php?option=com_config&view=config&controller=config.display.config';
-UPDATE `#__extensions` SET `link` = 'index.php?option=com_config&view=templates' WHERE `link` = 'index.php?option=com_config&view=templates&controller=config.display.templates';
+UPDATE `#__menu` SET `link` = 'index.php?option=com_config&view=config' WHERE `link` = 'index.php?option=com_config&view=config&controller=config.display.config';
+UPDATE `#__menu` SET `link` = 'index.php?option=com_config&view=templates' WHERE `link` = 'index.php?option=com_config&view=templates&controller=config.display.templates';
