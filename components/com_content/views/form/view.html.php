6b585ae8eea28ad11a6e24b92904086788ea60b3
nvyush <nvyush@yandex.ru>
2017-06-01 11:49:05 +0300

    Remove unnecessary condition

--- components/com_content/views/form/view.html.php
+++ components/com_content/views/form/view.html.php
@@ -72,9 +72,9 @@ class ContentViewForm extends JViewLegacy
 		if (!empty($this->item->id))
 		{
 			$this->item->tags->getItemTags('com_content.article', $this->item->id);
-		}		
- 
-		if (isset($this->item->id))		
+		}
+
+		if (isset($this->item->id))
 		{
 			$this->item->images = json_decode($this->item->images);
 			$this->item->urls = json_decode($this->item->urls);
