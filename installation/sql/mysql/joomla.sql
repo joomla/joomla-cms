6be4d3d7ea63c3631326bcffc9ac3e7c6b1ef2e7
Klas Berlič <klas.berlic@gmail.com>
2017-06-03 12:22:31 +0200

    Finalize sql

--- installation/sql/mysql/joomla.sql
+++ installation/sql/mysql/joomla.sql
@@ -33,37 +33,37 @@ CREATE TABLE IF NOT EXISTS `#__assets` (
 --
 
 INSERT INTO `#__assets` (`id`, `parent_id`, `lft`, `rgt`, `level`, `name`, `title`, `rules`) VALUES
-(1, 0, 0, 103, 0, 'root.1', 'Root Asset', '{"core.login.site":{"6":1,"2":1},"core.login.admin":{"6":1},"core.login.offline":{"6":1},"core.admin":{"8":1},"core.manage":{"7":1},"core.create":{"6":1,"3":1},"core.delete":{"6":1},"core.edit":{"6":1,"4":1},"core.edit.state":{"6":1,"5":1},"core.edit.own":{"6":1,"3":1}}'),
+(1, 0, 0, 103, 0, 'root.1', 'Root Asset', '{}'),
 (2, 1, 1, 2, 1, 'com_admin', 'com_admin', '{}'),
-(3, 1, 3, 6, 1, 'com_banners', 'com_banners', '{"core.admin":{"7":1},"core.manage":{"6":1}}'),
-(4, 1, 7, 8, 1, 'com_cache', 'com_cache', '{"core.admin":{"7":1},"core.manage":{"7":1}}'),
-(5, 1, 9, 10, 1, 'com_checkin', 'com_checkin', '{"core.admin":{"7":1},"core.manage":{"7":1}}'),
+(3, 1, 3, 6, 1, 'com_banners', 'com_banners', '{}'),
+(4, 1, 7, 8, 1, 'com_cache', 'com_cache', '{}'),
+(5, 1, 9, 10, 1, 'com_checkin', 'com_checkin', '{}'),
 (6, 1, 11, 12, 1, 'com_config', 'com_config', '{}'),
-(7, 1, 13, 16, 1, 'com_contact', 'com_contact', '{"core.admin":{"7":1},"core.manage":{"6":1}}'),
-(8, 1, 17, 20, 1, 'com_content', 'com_content', '{"core.admin":{"7":1},"core.manage":{"6":1},"core.create":{"3":1},"core.edit":{"4":1},"core.edit.state":{"5":1}}'),
+(7, 1, 13, 16, 1, 'com_contact', 'com_contact', '{}'),
+(8, 1, 17, 20, 1, 'com_content', 'com_content', '{}'),
 (9, 1, 21, 22, 1, 'com_cpanel', 'com_cpanel', '{}'),
-(10, 1, 23, 24, 1, 'com_installer', 'com_installer', '{"core.manage":{"7":0},"core.delete":{"7":0},"core.edit.state":{"7":0}}'),
-(11, 1, 25, 26, 1, 'com_languages', 'com_languages', '{"core.admin":{"7":1}}'),
+(10, 1, 23, 24, 1, 'com_installer', 'com_installer', '{}'),
+(11, 1, 25, 26, 1, 'com_languages', 'com_languages', '{}'),
 (12, 1, 27, 28, 1, 'com_login', 'com_login', '{}'),
 (13, 1, 29, 30, 1, 'com_mailto', 'com_mailto', '{}'),
 (14, 1, 31, 32, 1, 'com_massmail', 'com_massmail', '{}'),
-(15, 1, 33, 34, 1, 'com_media', 'com_media', '{"core.admin":{"7":1},"core.manage":{"6":1},"core.create":{"3":1},"core.delete":{"5":1}}'),
-(16, 1, 35, 38, 1, 'com_menus', 'com_menus', '{"core.admin":{"7":1}}'),
-(17, 1, 39, 40, 1, 'com_messages', 'com_messages', '{"core.admin":{"7":1},"core.manage":{"7":1}}'),
-(18, 1, 41, 72, 1, 'com_modules', 'com_modules', '{"core.admin":{"7":1}}'),
-(19, 1, 73, 76, 1, 'com_newsfeeds', 'com_newsfeeds', '{"core.admin":{"7":1},"core.manage":{"6":1}}'),
-(20, 1, 77, 78, 1, 'com_plugins', 'com_plugins', '{"core.admin":{"7":1}}'),
-(21, 1, 79, 80, 1, 'com_redirect', 'com_redirect', '{"core.admin":{"7":1}}'),
-(22, 1, 81, 82, 1, 'com_search', 'com_search', '{"core.admin":{"7":1},"core.manage":{"6":1}}'),
-(23, 1, 83, 84, 1, 'com_templates', 'com_templates', '{"core.admin":{"7":1}}'),
-(24, 1, 85, 88, 1, 'com_users', 'com_users', '{"core.admin":{"7":1}}'),
+(15, 1, 33, 34, 1, 'com_media', 'com_media', '{}'),
+(16, 1, 35, 38, 1, 'com_menus', 'com_menus', '{}'),
+(17, 1, 39, 40, 1, 'com_messages', 'com_messages', '{}'),
+(18, 1, 41, 72, 1, 'com_modules', 'com_modules', '{}'),
+(19, 1, 73, 76, 1, 'com_newsfeeds', 'com_newsfeeds', '{}'),
+(20, 1, 77, 78, 1, 'com_plugins', 'com_plugins', '{}'),
+(21, 1, 79, 80, 1, 'com_redirect', 'com_redirect', '{}'),
+(22, 1, 81, 82, 1, 'com_search', 'com_search', '{}'),
+(23, 1, 83, 84, 1, 'com_templates', 'com_templates', '{}'),
+(24, 1, 85, 88, 1, 'com_users', 'com_users', '{}'),
 (26, 1, 89, 90, 1, 'com_wrapper', 'com_wrapper', '{}'),
 (27, 8, 18, 19, 2, 'com_content.category.2', 'Uncategorised', '{}'),
 (28, 3, 4, 5, 2, 'com_banners.category.3', 'Uncategorised', '{}'),
 (29, 7, 14, 15, 2, 'com_contact.category.4', 'Uncategorised', '{}'),
 (30, 19, 74, 75, 2, 'com_newsfeeds.category.5', 'Uncategorised', '{}'),
 (32, 24, 86, 87, 2, 'com_users.category.7', 'Uncategorised', '{}'),
-(33, 1, 91, 92, 1, 'com_finder', 'com_finder', '{"core.admin":{"7":1},"core.manage":{"6":1}}'),
+(33, 1, 91, 92, 1, 'com_finder', 'com_finder', '{}'),
 (34, 1, 93, 94, 1, 'com_joomlaupdate', 'com_joomlaupdate', '{}'),
 (35, 1, 95, 96, 1, 'com_tags', 'com_tags', '{}'),
 (36, 1, 97, 98, 1, 'com_contenthistory', 'com_contenthistory', '{}'),
@@ -2178,12 +2178,4 @@ INSERT INTO `#__permissions` (`id`, `permission`, `value`, `ugroup`, `assetid`) 
   (48, 'core.admin', 1, 7, 24),
   (49, 'core.admin', 1, 7, 33),
   (50, 'core.manage', 1, 6, 33);
-
-
--- To be moved to change sql later
--- ALTER TABLE `#__assets` ADD KEY `idx_id_level` ( `level` , `id` );
--- ALTER TABLE `#__assets` ADD UNIQUE KEY `idx_rgt_lft_id` ( `rgt` , `lft` , `id` );
-
--- TESTING ONLY - for easier #__assets merges
-UPDATE `#__assets` SET `rules`='{}';
 
