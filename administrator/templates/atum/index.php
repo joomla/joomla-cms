b1fa1ffb9a4e67d66e83fae1a3213a8e3f706451
Charlie Lodder <lodder15@hotmail.co.uk>
2017-06-04 14:13:04 +0100

    Add pulsating effect when there are notifications

--- administrator/templates/atum/index.php
+++ administrator/templates/atum/index.php
@@ -118,9 +118,9 @@ $doc->setMetaData('theme-color', '#1c3d5c');
 							</li>
 							<li class="nav-item">
 								<a class="nav-link dropdown-toggle" href="<?php echo JRoute::_('index.php?option=com_messages'); ?>" title="<?php echo JText::_('TPL_ATUM_PRIVATE_MESSAGES'); ?>">
-									<span class="fa fa-envelope-o" aria-hidden="true"></span>
-									<span class="sr-only"><?php echo JText::_('TPL_ATUM_PRIVATE_MESSAGES'); ?></span>
 									<?php $countUnread = JFactory::getSession()->get('messages.unread'); ?>
+									<span class="fa fa-envelope-o<?php echo (count($countUnread) > 0) ? ' pulsate-font-size' : ''; ?>" aria-hidden="true"></span>
+									<span class="sr-only"><?php echo JText::_('TPL_ATUM_PRIVATE_MESSAGES'); ?></span>
 									<?php if ($countUnread > 0) : ?>
 										<span class="badge badge-pill badge-success"><?php echo $countUnread; ?></span>
 									<?php endif; ?>
@@ -144,7 +144,7 @@ $doc->setMetaData('theme-color', '#1c3d5c');
 							<?php if ($user->authorise('core.manage', 'com_postinstall')) : ?>
 							<li class="nav-item dropdown">
 								<a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" title="<?php echo JText::_('TPL_ATUM_POST_INSTALLATION_MESSAGES'); ?>">
-									<span class="fa fa-bell-o" aria-hidden="true"></span>
+									<span class="fa fa-bell-o<?php echo count($messages) > 0 ? ' pulsate-font-size' : ''; ?>" aria-hidden="true"></span>
 									<?php if (count($messages) > 0) : ?>
 										<span class="badge badge-pill badge-success"><?php echo count($messages); ?></span>
 									<?php endif; ?>
