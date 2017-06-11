2780e4da31a03c72a983d43cb740fda067af8751
richard67 <richard.fath@t-online.de>
2017-06-11 17:18:53 +0200

    Again a correction for doc block comment.

--- libraries/cms/extension/helper.php
+++ libraries/cms/extension/helper.php
@@ -19,6 +19,7 @@ class JExtensionHelper
 	/**
 	 * Array of core extensions
 	 * Each element is an array with elements "type", "element", "folder" and
+	 * "client_id".
 	 *
 	 * @var    array
 	 * @since  __DEPLOY_VERSION__
