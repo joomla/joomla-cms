1908a16c3317118bdf3291af51d0e55881a1d704
Frank Mayer <frank@frankmayer.net>
2017-06-13 22:07:22 +0300

    Changes according to reviewer's comment

--- templates/beez3/html/com_content/article/default.php
+++ templates/beez3/html/com_content/article/default.php
@@ -133,7 +133,7 @@ if ($accessEdit || $showPrintIcon || $showEmailIcon) : ?>
 <?php if (!empty($this->item->author) && $params->get('show_author')) : ?>
 	<dd class="createdby">
 		<?php $author = $this->item->created_by_alias ?: $this->item->author; ?>
-		<?php if (!empty($this->item->contact_link) &&  $params->get('link_author') == true) : ?>
+		<?php if (!empty($this->item->contact_link) && $params->get('link_author') == true) : ?>
 			<?php echo JText::sprintf('COM_CONTENT_WRITTEN_BY', JHtml::_('link', $this->item->contact_link, $author)); ?>
 		<?php else : ?>
 			<?php echo JText::sprintf('COM_CONTENT_WRITTEN_BY', $author); ?>
