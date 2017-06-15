42563d60e560f81abd119a6e838d40069de911dc
Allon Moritz <allon.moritz@digital-peak.com>
2017-06-15 11:30:06 +0200

    Check if the mime_content_type exists (#248)
    * Check if the mime_content_type exists
    
    * Determine mime type trough the helper function

--- plugins/filesystem/local/Adapter/LocalAdapter.php
+++ plugins/filesystem/local/Adapter/LocalAdapter.php
@@ -278,7 +278,7 @@ class LocalAdapter implements AdapterInterface
 		$obj->path      = str_replace($this->rootPath, '/', $path);
 		$obj->extension = !$isDir ? \JFile::getExt($obj->name) : '';
 		$obj->size      = !$isDir ? filesize($path) : 0;
-		$obj->mime_type = mime_content_type($path);
+		$obj->mime_type = MediaHelper::getMimeType($path, MediaHelper::isImage($obj->name));
 		$obj->width     = 0;
 		$obj->height    = 0;
 
@@ -288,7 +288,7 @@ class LocalAdapter implements AdapterInterface
 		$obj->modified_date           = $modifiedDate->format('c', true);
 		$obj->modified_date_formatted = $modifiedDate->format(\JText::_('DATE_FORMAT_LC5'), true);
 
-		if (strpos($obj->mime_type, 'image/') === 0 && MediaHelper::isImage($obj->name))
+		if (MediaHelper::isImage($obj->name))
 		{
 			// Get the image properties
 			$props       = Image::getImageFileProperties($path);
