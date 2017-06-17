a6a5ff9012a4b623a7c62edf1c680dd2f93f99a4
zero-24 <zero-24@users.noreply.github.com>
2017-06-17 18:31:37 +0200

    bit cs

--- tests/codeception/acceptance/components/com_users/UserCest.php
+++ tests/codeception/acceptance/components/com_users/UserCest.php
@@ -3,9 +3,10 @@
  * @package     Joomla.Tests
  * @subpackage  Acceptance.tests
  *
- * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
+ * @copyright   Copyright (C) 2005 - 2017 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
+
 use Page\Acceptance\Administrator;
 
 /**
