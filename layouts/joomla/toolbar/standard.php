93da6613e07ba93b8e8768992bb477bae54f6736
Thomas Hunziker <werbemails@bakual.ch>
2017-06-21 14:21:01 +0200

    Remove double space

--- layouts/joomla/toolbar/standard.php
+++ layouts/joomla/toolbar/standard.php
@@ -20,12 +20,12 @@ $group    = $displayData['group'];
 ?>
 
 <?php if ($group) : ?>
-<a <?php echo $id; ?> href="#" onclick="<?php echo $doTask; ?>" class="dropdown-item">
+<a<?php echo $id; ?> href="#" onclick="<?php echo $doTask; ?>" class="dropdown-item">
 	<span class="<?php echo trim($class); ?>"></span>
 	<?php echo $text; ?>
 </a>
 <?php else : ?>
-<button <?php echo $id; ?> onclick="<?php echo $doTask; ?>" class="<?php echo $btnClass; ?>">
+<button<?php echo $id; ?> onclick="<?php echo $doTask; ?>" class="<?php echo $btnClass; ?>">
 	<span class="<?php echo trim($class); ?>" aria-hidden="true"></span>
 	<?php echo $text; ?>
 </button>
