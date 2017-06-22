142a7df6195b85dec543fa013f97f76b0a7063c4
dgt41 <d.grammatiko@gmail.com>
2017-06-22 13:03:28 +0300

    use one node-modules folder

--- karma.conf.js
+++ karma.conf.js
@@ -12,9 +12,9 @@ module.exports = function (config) {
 
 		// list of files / patterns to load in the browser
 		files: [
-			{pattern: 'tests/javascript/node_modules/jquery/dist/jquery.min.js', included: false},
-			{pattern: 'tests/javascript/node_modules/jasmine-jquery/lib/jasmine-jquery.js', included: false},
-			{pattern: 'tests/javascript/node_modules/text/text.js', included: false},
+			{pattern: 'node_modules/jquery/dist/jquery.min.js', included: false},
+			{pattern: 'node_modules/jasmine-jquery/lib/jasmine-jquery.js', included: false},
+			{pattern: 'node_modules/text/text.js', included: false},
 			{pattern: 'media/vendor/bootstrap/js/bootstrap.min.js', included: false},
 			{pattern: 'media/vendor/jquery-ui/js/jquery.ui.core.min.js', included: false},
 			{pattern: 'media/vendor/jquery-ui/js/jquery.ui.sortable.min.js', included: false},
