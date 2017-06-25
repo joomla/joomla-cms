0cb0b187b1f983d28465addb4f4c9b40e8235e2d
richard67 <richard.fath@t-online.de>
2017-06-25 11:33:59 +0200

    Fix sample data for mysql
    Let the new admin main menu type always have same ID as in clean install
    without sample data.

--- installation/sql/mysql/sample_data.sql
+++ installation/sql/mysql/sample_data.sql
@@ -132,8 +132,8 @@ INSERT INTO `#__menu` (`id`, `menutype`, `title`, `alias`, `note`, `path`, `link
 
 INSERT INTO `#__menu_types` (`id`, `asset_id`, `menutype`, `title`, `description`, `client_id`) VALUES
 (1, 0, 'mainmenu', 'Main Menu', 'The main menu for the site', 0),
-(2, 0, 'usermenu', 'User Menu', 'A Menu for logged-in Users', 0),
-(3, 0, 'main', 'Admin Main Menu', 'The main menu for the backend', 1);
+(2, 0, 'main', 'Admin Main Menu', 'The main menu for the backend', 1),
+(3, 0, 'usermenu', 'User Menu', 'A Menu for logged-in Users', 0);
 
 --
 -- Dumping data for table `#__modules`
