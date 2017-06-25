438871003ff510e4ba872f40dfa2ecd89c4054ae
andrepereiradasilva <andrepereiradasilva@users.noreply.github.com>
2017-06-25 16:33:07 +0100

    should be $lang

--- administrator/templates/atum/component.php
+++ administrator/templates/atum/component.php
@@ -24,7 +24,7 @@ JHtml::_('stylesheet', 'template' . ($this->direction === 'rtl' ? '-rtl' : '') .
 JHtml::_('stylesheet', 'user.css', array('version' => 'auto', 'relative' => true));
 
 // Load specific language related CSS
-JHtml::_('stylesheet', 'administrator/language/' . $this->language->getTag() . '/' . $this->language->getTag() . '.css', array('version' => 'auto'));
+JHtml::_('stylesheet', 'administrator/language/' . $lang->getTag() . '/' . $lang->getTag() . '.css', array('version' => 'auto'));
 ?>
 
 <!DOCTYPE html>
