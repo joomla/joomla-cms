d8b9dea1437204a549a7fcdf97cc0623ec65bf30
richard67 <richard.fath@t-online.de>
2017-06-25 11:59:10 +0200

    Correct the schema updates
    Insert ingnore is mysql, but even there we do not use it in any schema
    update up to now, so it is removed from all 3 scripts.

--- administrator/components/com_admin/sql/updates/sqlazure/3.7.3-2017-06-25.sql
+++ administrator/components/com_admin/sql/updates/sqlazure/3.7.3-2017-06-25.sql
@@ -1,2 +1,2 @@
-INSERT IGNORE INTO [#__menu_types] ([asset_id], [menutype], [title], [description], [client_id])
+INSERT INTO [#__menu_types] ([asset_id], [menutype], [title], [description], [client_id])
 VALUES (0, 'main', 'Admin Main Menu', 'The main menu for the backend', 1);
