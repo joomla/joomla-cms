629aa0fe98acb812e0fd2a9898e294d50b6e7c5e
photodude <walt@waltsorensen.com>
2017-06-29 18:14:51 -0600

    PHPCS2-autofixers
    - Multi-line function call not indented correctly;
    - No blank line found after control structure
    - Closing parenthesis of a multi-line function call must be on a line by
    itself
    - Please consider an empty line before the foreach statement;
    - Expected 1 space after "="; 2 found
    - Expected 1 space after FUNCTION keyword; 0 found
    - Expected 3 tabs before opening brace; 4 found
    - Whitespace found at end of line

--- administrator/components/com_installer/models/database.php
+++ administrator/components/com_installer/models/database.php
@@ -99,6 +99,7 @@ class InstallerModelDatabase extends InstallerModel
 
 			return false;
 		}
+
 		return $changeSet;
 	}
 
@@ -310,15 +311,18 @@ class InstallerModelDatabase extends InstallerModel
 		{
 			// Table messed up somehow, clear it
 			$db->setQuery('DELETE FROM ' . $db->quoteName('#__utf8_conversion')
-				. ';')->execute();
+				. ';'
+			)->execute();
 			$db->setQuery('INSERT INTO ' . $db->quoteName('#__utf8_conversion')
-				. ' (' . $db->quoteName('converted') . ') VALUES (0);')->execute();
+				. ' (' . $db->quoteName('converted') . ') VALUES (0);'
+			)->execute();
 		}
 		elseif ($count == 0)
 		{
 			// Record missing somehow, fix this
 			$db->setQuery('INSERT INTO ' . $db->quoteName('#__utf8_conversion')
-				. ' (' . $db->quoteName('converted') . ') VALUES (0);')->execute();
+				. ' (' . $db->quoteName('converted') . ') VALUES (0);'
+			)->execute();
 		}
 	}
 }
