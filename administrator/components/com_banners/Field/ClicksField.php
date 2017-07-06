dc51594873437ed9793cfab64bfa81bf728ec972
Allon Moritz <allon.moritz@digital-peak.com>
2017-07-06 12:07:26 +0200

    [4.0] Namespace banner fields (#16993)
    * Namespace banner fields
    
    * Revert session code

--- administrator/components/com_banners/Field/ClicksField.php
+++ administrator/components/com_banners/Field/ClicksField.php
@@ -6,15 +6,18 @@
  * @copyright   Copyright (C) 2005 - 2017 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
+namespace Joomla\Component\Banners\Administrator\Field;
 
 defined('JPATH_BASE') or die;
+
+use Joomla\CMS\Form\FormField;
 
 /**
  * Clicks field.
  *
  * @since  1.6
  */
-class JFormFieldClicks extends JFormField
+class ClicksField extends FormField
 {
 	/**
 	 * The form field type.
@@ -38,6 +41,6 @@ class JFormFieldClicks extends JFormField
 		return '<div class="input-group"><input class="form-control type="text" name="' . $this->name . '" id="' . $this->id . '" value="'
 			. htmlspecialchars($this->value, ENT_COMPAT, 'UTF-8') . '" readonly="readonly">'
 			. '<span class="input-group-btn"><a class="btn btn-secondary" ' . $onclick . '>'
-			. '<span class="icon-refresh" aria-hidden="true"></span> ' . JText::_('COM_BANNERS_RESET_CLICKS') . '</a></span></div>';
+			. '<span class="icon-refresh" aria-hidden="true"></span> ' . \JText::_('COM_BANNERS_RESET_CLICKS') . '</a></span></div>';
 	}
 }
