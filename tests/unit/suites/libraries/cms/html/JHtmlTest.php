beba193f88361739b62b56d4d383fdda9832dd98
Robert Deutz <rdeutz@googlemail.com>
2017-07-06 13:31:01 +0200

    use uniqid to make test solid when run parallel

--- tests/unit/suites/libraries/cms/html/JHtmlTest.php
+++ tests/unit/suites/libraries/cms/html/JHtmlTest.php
@@ -314,11 +314,11 @@ class JHtmlTest extends TestCase
 	public function testImage()
 	{
 		// These are some paths to pass to JHtml for testing purposes.
-		$urlpath = 'test1/';
-		$urlfilename = 'image1.jpg';
+		$urlpath = uniqid() . 'test1/';
+		$urlfilename = 'image' . uniqid() . '.jpg';
 
 		// We generate a random template name so that we don't collide or hit anything.
-		$template = 'mytemplate' . mt_rand(1, 10000);
+		$template = 'mytemplate' . uniqid();
 
 		// We create a stub (not a mock because we don't enforce whether it is called or not)
 		// to return a value from getTemplate.
@@ -1301,7 +1301,7 @@ class JHtmlTest extends TestCase
 	public function testTooltip()
 	{
 		// We generate a random template name so that we don't collide or hit anything
-		$template = 'mytemplate' . mt_rand(1, 10000);
+		$template = 'mytemplate' . uniqid();
 
 		// We create a stub (not a mock because we don't enforce whether it is called or not)
 		// to return a value from getTemplate
