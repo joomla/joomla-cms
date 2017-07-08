cbcf3f8e170913f7a007f746803b5215688b44cc
Helios Ciancio <info@eshiol.it>
2017-07-08 07:49:48 +0200

    fixes unit test

---
+++ administrator/components/com_admin/sql/updates/sqlazure/3.7.4-2017-07-07.sql
@@ -0,0 +1,2 @@
+ALTER TABLE [#__content] ADD [featured_up] [datetime] NOT NULL DEFAULT '1900-01-01T00:00:00.000';
+ALTER TABLE [#__content] ADD [featured_down] [datetime] NOT NULL DEFAULT '1900-01-01T00:00:00.000';
