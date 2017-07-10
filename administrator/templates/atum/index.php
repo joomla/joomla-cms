f64928256547ac13f1240bb8368999f1bdaaa977
Ciaran Walsh <ciaran@joomla51.com>
2017-07-10 17:00:27 +0100

    Header icons styling (#17046)

--- administrator/templates/atum/index.php
+++ administrator/templates/atum/index.php
@@ -102,13 +102,13 @@ $this->setMetaData('theme-color', '#1c3d5c');
 						<ul class="nav text-center">
 							<li class="nav-item">
 								<a class="nav-link" href="<?php echo JUri::root(); ?>" title="<?php echo JText::sprintf('TPL_ATUM_PREVIEW', $sitename); ?>" target="_blank">
-									<span class="fa fa-external-link" aria-hidden="true"></span>
+									<span class="fa fa-external-link-square" aria-hidden="true"></span>
 									<span class="sr-only"><?php echo JHtml::_('string.truncate', $sitename, 28, false, false); ?></span>
 								</a>
 							</li>
 							<li class="nav-item">
 								<a class="nav-link dropdown-toggle" href="<?php echo JRoute::_('index.php?option=com_messages'); ?>" title="<?php echo JText::_('TPL_ATUM_PRIVATE_MESSAGES'); ?>">
-									<span class="fa fa-envelope-o" aria-hidden="true"></span>
+									<span class="fa fa-envelope" aria-hidden="true"></span>
 									<span class="sr-only"><?php echo JText::_('TPL_ATUM_PRIVATE_MESSAGES'); ?></span>
 									<?php $countUnread = JFactory::getSession()->get('messages.unread'); ?>
 									<?php if ($countUnread > 0) : ?>
@@ -134,7 +134,7 @@ $this->setMetaData('theme-color', '#1c3d5c');
 							<?php if ($user->authorise('core.manage', 'com_postinstall')) : ?>
 							<li class="nav-item dropdown">
 								<a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" title="<?php echo JText::_('TPL_ATUM_POST_INSTALLATION_MESSAGES'); ?>">
-									<span class="fa fa-bell-o" aria-hidden="true"></span>
+									<span class="fa fa-bell" aria-hidden="true"></span>
 									<?php if (count($messages) > 0) : ?>
 										<span class="badge badge-pill badge-success"><?php echo count($messages); ?></span>
 									<?php endif; ?>
@@ -160,7 +160,7 @@ $this->setMetaData('theme-color', '#1c3d5c');
 							<?php endif; ?>
 							<li class="nav-item dropdown header-profile">
 								<a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown">
-									<span class="fa fa-user-o" aria-hidden="true"></span>
+									<span class="fa fa-user" aria-hidden="true"></span>
 									<span class="sr-only"><?php echo JText::_('TPL_ATUM_ADMIN_USER_MENU'); ?></span>
 								</a>
 								<div class="dropdown-menu dropdown-menu-right">
