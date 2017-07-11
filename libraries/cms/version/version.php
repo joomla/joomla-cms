0b302f2abb18b4f55b5a2559f12b8a9e2d186f30
Robert Deutz <rdeutz@googlemail.com>
2017-07-11 17:52:56 +0200

    bump to dev state

--- libraries/cms/version/version.php
+++ libraries/cms/version/version.php
@@ -38,7 +38,7 @@ final class JVersion
 	 * @var    string
 	 * @since  3.5
 	 */
-	const DEV_LEVEL = '4-beta1';
+	const DEV_LEVEL = '4-dev';
 
 	/**
 	 * Development status.
@@ -46,7 +46,7 @@ final class JVersion
 	 * @var    string
 	 * @since  3.5
 	 */
-	const DEV_STATUS = 'Beta';
+	const DEV_STATUS = 'Development';
 
 	/**
 	 * Build number.
@@ -78,7 +78,7 @@ final class JVersion
 	 * @var    string
 	 * @since  3.5
 	 */
-	const RELTIME = '16:40';
+	const RELTIME = '16:52';
 
 	/**
 	 * Release timezone.
