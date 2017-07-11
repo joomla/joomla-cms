947cfede33da7f0cc29acb54cffe6c3ca21f5838
Charlie Lodder <lodder15@hotmail.co.uk>
2017-07-11 16:22:04 +0100

    Remove params for admin mod_feed

--- administrator/modules/mod_feed/tmpl/default.php
+++ administrator/modules/mod_feed/tmpl/default.php
@@ -9,73 +9,48 @@
 
 defined('_JEXEC') or die;
 
+// Check if feed URL has been set
+if (empty ($rssurl))
+{
+	echo '<div>' . JText::_('MOD_FEED_ERR_NO_URL') . '</div>';
+
+	return;
+}
+
 if (!empty($feed) && is_string($feed))
 {
 	echo $feed;
 }
 else
 {
-	$lang      = JFactory::getLanguage();
-	$myrtl     = $params->get('rssrtl');
-	$direction = ' ';
-
-	if ($lang->isRtl() && $myrtl == 0)
-	{
-		$direction = ' redirect-rtl';
-	}
-
-	// Feed description
-	elseif ($lang->isRtl() && $myrtl == 1)
-	{
-		$direction = ' redirect-ltr';
-	}
-
-	elseif ($lang->isRtl() && $myrtl == 2)
-	{
-		$direction = ' redirect-rtl';
-	}
-
-	elseif ($myrtl == 0)
-	{
-		$direction = ' redirect-ltr';
-	}
-	elseif ($myrtl == 1)
-	{
-		$direction = ' redirect-ltr';
-	}
-	elseif ($myrtl == 2)
-	{
-		$direction = ' redirect-rtl';
-	}
-
 	if ($feed != false) :
 		// Image handling
 		$iUrl   = isset($feed->image) ? $feed->image : null;
 		$iTitle = isset($feed->imagetitle) ? $feed->imagetitle : null;
 		?>
-		<div style="direction: <?php echo $rssrtl ? 'rtl' :'ltr'; ?>; text-align: <?php echo $rssrtl ? 'right' :'left'; ?> ! important"  class="feed<?php echo $moduleclass_sfx; ?>">
+		<div class="feed<?php echo $moduleclass_sfx; ?>">
 		<?php
 
 		// Feed description
-		if (!is_null($feed->title) && $params->get('rsstitle', 1)) : ?>
-			<h2 class="<?php echo $direction; ?>">
+		if (!is_null($feed->title)) : ?>
+			<h2>
 				<a href="<?php echo str_replace('&', '&amp;', $rssurl); ?>" target="_blank">
 				<?php echo $feed->title; ?></a>
 			</h2>
 		<?php endif; ?>
 
-		<!-- Feed description -->
+		<?php // Feed description ?>
 		<?php if ($params->get('rssdesc', 1)) : ?>
 			<?php echo $feed->description; ?>
 		<?php endif; ?>
 
-		<!--  Feed image  -->
+		<?php // Feed image ?>
 		<?php if ($params->get('rssimage', 1) && $iUrl) : ?>
 			<img src="<?php echo $iUrl; ?>" alt="<?php echo @$iTitle; ?>">
 		<?php endif; ?>
 
 
-	<!-- Show items -->
+	<?php // Show items ?>
 	<?php if (!empty($feed)) : ?>
 		<ul class="newsfeed<?php echo $params->get('moduleclass_sfx'); ?>">
 		<?php for ($i = 0; $i < $params->get('rssitems', 5); $i++) :
@@ -93,7 +68,7 @@ else
 						<a href="<?php echo $uri; ?>" target="_blank">
 						<?php  echo $feed[$i]->title; ?></a></h5>
 					<?php else : ?>
-						<h5 class="feed-link"><?php  echo $feed[$i]->title; ?></h5>
+						<h5 class="feed-link"><?php echo $feed[$i]->title; ?></h5>
 					<?php  endif; ?>
 
 					<?php if ($params->get('rssitemdesc') && !empty($text)) : ?>
@@ -101,7 +76,7 @@ else
 						<?php
 							// Strip the images.
 							$text = JFilterOutput::stripImages($text);
-							$text = JHtml::_('string.truncate', $text, $params->get('word_count'), true, false);
+							$text = JHtml::_('string.truncate', $text, 0, true, false);
 							echo str_replace('&apos;', "'", $text);
 						?>
 						</div>
