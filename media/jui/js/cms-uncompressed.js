69137335fb8283f58faea5fcbc33fb40c46e6782
Geraint Edwards <github@copyn.plus.com>
2017-07-21 15:32:16 +0100

    JS page formatting

--- media/jui/js/cms-uncompressed.js
+++ media/jui/js/cms-uncompressed.js
@@ -4,218 +4,217 @@
  */
 
 // Only define the Joomla namespace if not defined.
-if (typeof(Joomla) === 'undefined') {
-	var Joomla = {};
+if (typeof (Joomla) === 'undefined') {
+    var Joomla = {};
 }
 
-!(function(document, Joomla) {
-	"use strict";
+!(function (document, Joomla) {
+    "use strict";
 
-/**
- * Sets the HTML of the container-collapse element
- */
-Joomla.setcollapse = function(url, name, height) {
-    if (!document.getElementById('collapse-' + name)) {
-			document.getElementById('container-collapse').innerHTML = '<div class="collapse fade" id="collapse-' + name + '"><iframe class="iframe" src="' + url + '" height="' + height + '" width="100%"></iframe></div>';
+    /**
+     * Sets the HTML of the container-collapse element
+     */
+    Joomla.setcollapse = function (url, name, height) {
+        if (!document.getElementById('collapse-' + name)) {
+            document.getElementById('container-collapse').innerHTML = '<div class="collapse fade" id="collapse-' + name + '"><iframe class="iframe" src="' + url + '" height="' + height + '" width="100%"></iframe></div>';
+        }
+    };
+
+    /**
+     * IE8 polyfill for indexOf()
+     */
+    if (!Array.prototype.indexOf) {
+        Array.prototype.indexOf = function (elt) {
+            var len = this.length >>> 0;
+
+            var from = Number(arguments[1]) || 0;
+            from = (from < 0) ? Math.ceil(from) : Math.floor(from);
+
+            if (from < 0) {
+                from += len;
+            }
+
+            for (; from < len; from++) {
+                if (from in this && this[from] === elt) {
+                    return from;
+                }
+            }
+            return -1;
+        };
     }
-};
+    /**
+     * JField 'showon' feature.
+     */
+    window.jQuery && (function ($) {
 
-/**
- * IE8 polyfill for indexOf()
- */
-	if (!Array.prototype.indexOf) {
-		Array.prototype.indexOf = function(elt) {
-		var len = this.length >>> 0;
+        /**
+         * Method to check condition and change the target visibility
+         * @param {jQuery}  target
+         * @param {Boolean} animate
+         */
+        function linkedoptions(target, animate) {
+            var showfield = true,
+                    jsondata = target.data('showon') || [],
+                    itemval, condition, fieldName, $fields;
 
-		var from = Number(arguments[1]) || 0;
-			from     = (from < 0) ? Math.ceil(from) : Math.floor(from);
+            // Check if target conditions are satisfied
+            for (var j = 0, lj = jsondata.length; j < lj; j++) {
+                condition = jsondata[j] || {};
+                fieldName = condition.field;
+                $fields = $('[name="' + fieldName + '"], [name="' + fieldName + '[]"]');
 
-			if (from < 0) {
-			from += len;
-		}
+                condition['valid'] = 0;
 
-			for (; from < len; from++) {
-				if (from in this && this[from] === elt) {
-				return from;
-			}
-		}
-		return -1;
-	};
-}
-	/**
-	 * JField 'showon' feature.
-	 */
-	window.jQuery && (function($) {
+                // Test in each of the elements in the field array if condition is valid
+                $fields.each(function () {
+                    var $field = $(this);
 
-		/**
-		 * Method to check condition and change the target visibility
-		 * @param {jQuery}  target
-		 * @param {Boolean} animate
-		 */
-		function linkedoptions(target, animate) {
-			var showfield = true,
-				jsondata  = target.data('showon') || [],
-				itemval, condition, fieldName, $fields;
+                    // If checkbox or radio box the value is read from properties
+                    if (['checkbox', 'radio'].indexOf($field.attr('type')) !== -1) {
+                        if (!$field.prop('checked')) {
+                            // unchecked fields will return a blank and so always match a != condition so we skip them
+                            return;
+                        }
+                        itemval = $field.val();
+                    } else {
+                        // select lists, textarea etc. Note that multiple-select list returns an Array here 
+                        // se we can always tream 'itemval' as an array
+                        itemval = $field.val();
+                        // a multi-select <select> $field  will return null when no elements are selected so we need to define itemval accordingly
+                        if (itemval == null && $field.prop("tagName").toLowerCase() == "select") {
+                            itemval = [];
+                        }
+                    }
 
-			// Check if target conditions are satisfied
-			for (var j = 0, lj = jsondata.length; j < lj; j++) {
-				condition = jsondata[j] || {};
-				fieldName = condition.field;
-				$fields   = $('[name="' + fieldName + '"], [name="' + fieldName + '[]"]');
+                    // Convert to array to allow multiple values in the field (e.g. type=list multiple)
+                    // and normalize as string
+                    if (!(typeof itemval === 'object')) {
+                        itemval = JSON.parse('["' + itemval + '"]');
+                    }
 
-				condition['valid'] = 0;
+                    // for (var i in itemval) loops over non-enumerable properties and prototypes which means that != will ALWAYS match 
+                    // see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...in
+                    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/getOwnPropertyNames
+                    // use native javascript Array forEach - see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach                                        
+                    // We can't use forEach because its not supported in MSIE 8 - once that is dropped this code could use forEach instead and not have to use propertyIsEnumerable
+                    // 
+                    // Test if any of the values of the field exists in showon conditions
+                    for (var i in itemval) {
+                        // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/propertyIsEnumerable
+                        // Needed otherwise we pick up unenumerable properties like length etc. and !: will always match one of these  !!
+                        if (!itemval.propertyIsEnumerable(i)) {
+                            continue;
+                        }
+                        // ":" Equal to one or more of the values condition
+                        if (jsondata[j]['sign'] == '=' && jsondata[j]['values'].indexOf(itemval[i]) !== -1) {
+                            jsondata[j]['valid'] = 1;
+                        }
+                        // "!:" Not equal to one or more of the values condition
+                        if (jsondata[j]['sign'] == '!=' && jsondata[j]['values'].indexOf(itemval[i]) === -1) {
+                            jsondata[j]['valid'] = 1;
+                        }
 
-				// Test in each of the elements in the field array if condition is valid
-				$fields.each(function() {
-					var $field = $(this);
+                    }
 
-					// If checkbox or radio box the value is read from properties
-					if (['checkbox', 'radio'].indexOf($field.attr('type')) !== -1) {
-                                                if (!$field.prop('checked')) {
-                                                    // unchecked fields will return a blank and so always match a != condition so we skip them
-                                                    return;
-                                                }
-						itemval = $field.val();
-					}
-					else {
-                                                // select lists, textarea etc. Note that multiple-select list returns an Array here 
-                                                // se we can always tream 'itemval' as an array
-						itemval = $field.val();
-                                                // a multi-select <select> $field  will return null when no elements are selected so we need to define itemval accordingly
-						if (itemval == null && $field.prop("tagName").toLowerCase() == "select") {
-                                                    itemval = [];
-                                                }
-					}
+                });
 
-					// Convert to array to allow multiple values in the field (e.g. type=list multiple)
-					// and normalize as string
-					if (!(typeof itemval === 'object')) {
-						itemval = JSON.parse('["' + itemval + '"]');
-					}
+                // Verify conditions
+                // First condition (no operator): current condition must be valid
+                if (condition['op'] === '') {
+                    if (condition['valid'] === 0) {
+                        showfield = false;
+                    }
+                }
+                // Other conditions (if exists)
+                else {
+                    // AND operator: both the previous and current conditions must be valid
+                    if (condition['op'] === 'AND' && condition['valid'] + jsondata[j - 1]['valid'] < 2) {
+                        showfield = false;
+                    }
+                    // OR operator: one of the previous and current conditions must be valid
+                    if (condition['op'] === 'OR' && condition['valid'] + jsondata[j - 1]['valid'] > 0) {
+                        showfield = true;
+                    }
+                }
+            }
 
-                                        // for (var i in itemval) loops over non-enumerable properties and prototypes which means that != will ALWAYS match 
-                                        // see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...in
-                                        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/getOwnPropertyNames
-                                        // use native javascript Array forEach - see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach                                        
-                                        // We can't use forEach because its not supported in MSIE 8 - once that is dropped this code could use forEach instead and not have to use propertyIsEnumerable
-                                        // 
-					// Test if any of the values of the field exists in showon conditions
-                                        for (var i in itemval) {
-                                                // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/propertyIsEnumerable
-                                                // Needed otherwise we pick up unenumerable properties like length etc. and !: will always match one of these  !!
-                                                if (!itemval.propertyIsEnumerable(i)) {
-                                                    continue;
-                                                }
-						// ":" Equal to one or more of the values condition
-						if (jsondata[j]['sign'] == '=' && jsondata[j]['values'].indexOf(itemval[i]) !== -1) {
-							jsondata[j]['valid'] = 1;
-						}
-						// "!:" Not equal to one or more of the values condition
-						if (jsondata[j]['sign'] == '!=' && jsondata[j]['values'].indexOf(itemval[i]) === -1) {
-							jsondata[j]['valid'] = 1;
-						}
-                                            
-                                        }
+            // If conditions are satisfied show the target field(s), else hide
+            if (animate && !target.is('option')) {
+                (showfield) ? target.slideDown() : target.slideUp();
+            } else if (target.is('option')) {
+                target.toggle(showfield);
+                target.attr('disabled', showfield ? false : true);
+                // if chosen active for the target select list then update it
+                if ($('#' + target.parent().attr('id') + '_chzn').length) {
+                    target.parent().trigger("liszt:updated");
+                    target.parent().trigger("chosen:updated");
+                }
+            } else {
+                target.toggle(showfield);
+            }
+        }
 
-				});
+        /**
+         * Method for setup the 'showon' feature, for the fields in given container
+         * @param {HTMLElement} container
+         */
+        function setUpShowon(container) {
+            container = container || document;
 
-				// Verify conditions
-				// First condition (no operator): current condition must be valid
-				if (condition['op'] === '') {
-					if (condition['valid'] === 0) {
-						showfield = false;
-					}
-				}
-				// Other conditions (if exists)
-				else {
-					// AND operator: both the previous and current conditions must be valid
-					if (condition['op'] === 'AND' && condition['valid'] + jsondata[j - 1]['valid'] < 2) {
-						showfield = false;
-					}
-					// OR operator: one of the previous and current conditions must be valid
-					if (condition['op'] === 'OR' && condition['valid'] + jsondata[j - 1]['valid'] > 0) {
-						showfield = true;
-					}
-				}
-			}
+            var $showonFields = $(container).find('[data-showon]');
 
-			// If conditions are satisfied show the target field(s), else hide
-			if (animate && !target.is('option')) {
-				(showfield) ? target.slideDown() : target.slideUp();
-			} else if (target.is('option')) {
-                                target.toggle(showfield);
-                                target.attr('disabled' ,showfield? false : true);
-                                // if chosen active for the target select list then update it
-                                if ($('#'+target.parent().attr('id')+'_chzn').length){
-                                    target.parent().trigger("liszt:updated");
-                                    target.parent().trigger("chosen:updated");
-                                }
-                        } else {
-				target.toggle(showfield);
-			}
-		}
+            // Setup each 'showon' field
+            for (var is = 0, ls = $showonFields.length; is < ls; is++) {
+                // Use anonymous function to capture arguments
+                (function () {
+                    var $target = $($showonFields[is]), jsondata = $target.data('showon') || [],
+                            field, $fields = $();
 
-		/**
-		 * Method for setup the 'showon' feature, for the fields in given container
-		 * @param {HTMLElement} container
-		 */
-		function setUpShowon(container) {
-			container = container || document;
+                    // Collect an all referenced elements
+                    for (var ij = 0, lj = jsondata.length; ij < lj; ij++) {
+                        field = jsondata[ij]['field'];
+                        $fields = $fields.add($('[name="' + field + '"], [name="' + field + '[]"]'));
+                    }
 
-			var $showonFields = $(container).find('[data-showon]');
+                    // Check current condition for element
+                    linkedoptions($target);
 
-			// Setup each 'showon' field
-			for (var is = 0, ls = $showonFields.length; is < ls; is++) {
-				// Use anonymous function to capture arguments
-				(function() {
-					var $target = $($showonFields[is]), jsondata = $target.data('showon') || [],
-						field, $fields                           = $();
+                    // Attach events to referenced element, to check condition on change
+                    $fields.on('change', function () {
+                        linkedoptions($target, true);
+                    });
+                })();
+            }
+        }
 
-					// Collect an all referenced elements
-					for (var ij = 0, lj = jsondata.length; ij < lj; ij++) {
-						field   = jsondata[ij]['field'];
-						$fields = $fields.add($('[name="' + field + '"], [name="' + field + '[]"]'));
-					}
+        /**
+         * Initialize 'showon' feature
+         */
+        $(document).ready(function () {
+            setUpShowon();
 
-					// Check current condition for element
-					linkedoptions($target);
+            // Setup showon feature in the subform field
+            $(document).on('subform-row-add', function (event, row) {
+                var $row = $(row),
+                        $elements = $row.find('[data-showon]'),
+                        baseName = $row.data('baseName'),
+                        group = $row.data('group'),
+                        search = new RegExp('\\[' + baseName + '\\]\\[' + baseName + 'X\\]', 'g'),
+                        replace = '[' + baseName + '][' + group + ']',
+                        $elm, showon;
 
-					// Attach events to referenced element, to check condition on change
-					$fields.on('change', function() {
-						linkedoptions($target, true);
-					});
-				})();
-			}
-		}
+                // Fix showon field names in a current group
+                for (var i = 0, l = $elements.length; i < l; i++) {
+                    $elm = $($elements[i]);
+                    showon = $elm.attr('data-showon').replace(search, replace);
 
-		/**
-		 * Initialize 'showon' feature
-		 */
-		$(document).ready(function() {
-			setUpShowon();
+                    $elm.attr('data-showon', showon);
+                }
 
-			// Setup showon feature in the subform field
-			$(document).on('subform-row-add', function(event, row) {
-				var $row      = $(row),
-					$elements = $row.find('[data-showon]'),
-					baseName  = $row.data('baseName'),
-					group     = $row.data('group'),
-					search    = new RegExp('\\[' + baseName + '\\]\\[' + baseName + 'X\\]', 'g'),
-					replace   = '[' + baseName + '][' + group + ']',
-					$elm, showon;
+                setUpShowon(row);
+            });
+        });
 
-				// Fix showon field names in a current group
-				for (var i = 0, l = $elements.length; i < l; i++) {
-					$elm   = $($elements[i]);
-					showon = $elm.attr('data-showon').replace(search, replace);
-
-					$elm.attr('data-showon', showon);
-				}
-
-				setUpShowon(row);
-			});
-		});
-
-	})(jQuery);
+    })(jQuery);
 
 })(document, Joomla);
