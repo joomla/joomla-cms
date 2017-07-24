f61098587eb37bd702dc0c0862ce19c330621cba
zero-24 <zero-24@users.noreply.github.com>
2017-07-24 20:26:32 +0200

    com_newsfeed view review

--- components/com_newsfeeds/views/categories/tmpl/default_items.php
+++ components/com_newsfeeds/views/categories/tmpl/default_items.php
@@ -16,18 +16,16 @@ JHtml::_('bootstrap.tooltip');
 <?php $class = ' class="first"'; ?>
 <?php if ($this->maxLevelcat != 0 && count($this->items[$this->parent->id]) > 0) : ?>
 	<?php foreach ($this->items[$this->parent->id] as $id => $item) : ?>
-		<?php
-		if ($this->params->get('show_empty_categories_cat') || $item->numitems || count($item->getChildren())) :
-			if (!isset($this->items[$this->parent->id][$id + 1]))
-			{
-				$class = ' class="last"';
-			}
-			?>
+		<?php if ($this->params->get('show_empty_categories_cat') || $item->numitems || count($item->getChildren())) : ?>
+			<?php if (!isset($this->items[$this->parent->id][$id + 1])) :  ?>
+				<?php $class = ' class="last"'; ?>
+			<?php endif; ?>
 			<div <?php echo $class; ?> >
-			<?php $class = ''; ?>
+				<?php $class = ''; ?>
 				<h3 class="page-header item-title">
-				<a href="<?php echo JRoute::_(NewsfeedsHelperRoute::getCategoryRoute($item->id, $item->language)); ?>">
-					<?php echo $this->escape($item->title); ?></a>
+					<a href="<?php echo JRoute::_(NewsfeedsHelperRoute::getCategoryRoute($item->id, $item->language)); ?>">
+						<?php echo $this->escape($item->title); ?>
+					</a>
 					<?php if ($this->params->get('show_cat_items_cat') == 1) :?>
 						<span class="badge badge-info tip hasTooltip" title="<?php echo JHtml::_('tooltipText', 'COM_NEWSFEEDS_NUM_ITEMS'); ?>">
 							<?php echo JText::_('COM_NEWSFEEDS_NUM_ITEMS'); ?>&nbsp;
@@ -46,17 +44,14 @@ JHtml::_('bootstrap.tooltip');
 						</div>
 					<?php endif; ?>
 				<?php endif; ?>
-
 				<?php if (count($item->getChildren()) > 0 && $this->maxLevelcat > 1) : ?>
 					<div class="collapse fade" id="category-<?php echo $item->id; ?>">
-					<?php
-					$this->items[$item->id] = $item->getChildren();
-					$this->parent = $item;
-					$this->maxLevelcat--;
-					echo $this->loadTemplate('items');
-					$this->parent = $item->getParent();
-					$this->maxLevelcat++;
-					?>
+					<?php $this->items[$item->id] = $item->getChildren(); ?>
+					<?php $this->parent = $item; ?>
+					<?php $this->maxLevelcat--; ?>
+					<?php echo $this->loadTemplate('items'); ?>
+					<?php $this->parent = $item->getParent(); ?>
+					<?php $this->maxLevelcat++; ?>
 					</div>
 				<?php endif; ?>
 			</div>
--- components/com_newsfeeds/views/category/tmpl/default_items.php
+++ components/com_newsfeeds/views/category/tmpl/default_items.php
@@ -12,32 +12,30 @@ defined('_JEXEC') or die;
 $n         = count($this->items);
 $listOrder = $this->escape($this->state->get('list.ordering'));
 $listDirn  = $this->escape($this->state->get('list.direction'));
-
 ?>
 
 <?php if (empty($this->items)) : ?>
 	<p><?php echo JText::_('COM_NEWSFEEDS_NO_ARTICLES'); ?></p>
 <?php else : ?>
-
-<form action="<?php echo htmlspecialchars(JUri::getInstance()->toString(), ENT_COMPAT, 'UTF-8'); ?>" method="post" name="adminForm" id="adminForm">
-	<?php if ($this->params->get('filter_field') !== 'hide' || $this->params->get('show_pagination_limit')) : ?>
-	<fieldset class="filters btn-toolbar">
-		<?php if ($this->params->get('filter_field') !== 'hide' && $this->params->get('filter_field') == '1') : ?>
-			<div class="btn-group">
-				<label class="filter-search-lbl element-invisible" for="filter-search"><span class="label label-warning"><?php echo JText::_('JUNPUBLISHED'); ?></span><?php echo JText::_('COM_NEWSFEEDS_FILTER_LABEL') . '&#160;'; ?></label>
-				<input type="text" name="filter-search" id="filter-search" value="<?php echo $this->escape($this->state->get('list.filter')); ?>" class="inputbox" onchange="document.adminForm.submit();" title="<?php echo JText::_('COM_NEWSFEEDS_FILTER_SEARCH_DESC'); ?>" placeholder="<?php echo JText::_('COM_NEWSFEEDS_FILTER_SEARCH_DESC'); ?>" />
-			</div>
+	<form action="<?php echo htmlspecialchars(JUri::getInstance()->toString(), ENT_COMPAT, 'UTF-8'); ?>" method="post" name="adminForm" id="adminForm">
+		<?php if ($this->params->get('filter_field') !== 'hide' || $this->params->get('show_pagination_limit')) : ?>
+			<fieldset class="filters btn-toolbar">
+				<?php if ($this->params->get('filter_field') !== 'hide' && $this->params->get('filter_field') == '1') : ?>
+					<div class="btn-group">
+						<label class="filter-search-lbl element-invisible" for="filter-search"><span class="label label-warning"><?php echo JText::_('JUNPUBLISHED'); ?></span><?php echo JText::_('COM_NEWSFEEDS_FILTER_LABEL') . '&#160;'; ?></label>
+						<input type="text" name="filter-search" id="filter-search" value="<?php echo $this->escape($this->state->get('list.filter')); ?>" class="inputbox" onchange="document.adminForm.submit();" title="<?php echo JText::_('COM_NEWSFEEDS_FILTER_SEARCH_DESC'); ?>" placeholder="<?php echo JText::_('COM_NEWSFEEDS_FILTER_SEARCH_DESC'); ?>" />
+					</div>
+				<?php endif; ?>
+				<?php if ($this->params->get('show_pagination_limit')) : ?>
+					<div class="btn-group pull-right">
+						<label for="limit" class="element-invisible">
+							<?php echo JText::_('JGLOBAL_DISPLAY_NUM'); ?>
+						</label>
+						<?php echo $this->pagination->getLimitBox(); ?>
+					</div>
+				<?php endif; ?>
+			</fieldset>
 		<?php endif; ?>
-		<?php if ($this->params->get('show_pagination_limit')) : ?>
-			<div class="btn-group pull-right">
-				<label for="limit" class="element-invisible">
-					<?php echo JText::_('JGLOBAL_DISPLAY_NUM'); ?>
-				</label>
-				<?php echo $this->pagination->getLimitBox(); ?>
-			</div>
-		<?php endif; ?>
-	</fieldset>
-	<?php endif; ?>
 		<ul class="category list-striped list-condensed">
 			<?php foreach ($this->items as $i => $item) : ?>
 				<?php if ($this->items[$i]->published == 0) : ?>
@@ -70,10 +68,9 @@ $listDirn  = $this->escape($this->state->get('list.direction'));
 				</li>
 			<?php endforeach; ?>
 		</ul>
-
 		<?php // Add pagination links ?>
 		<?php if (!empty($this->items)) : ?>
-			<?php if (($this->params->def('show_pagination', 2) == 1  || ($this->params->get('show_pagination') == 2)) && ($this->pagination->pagesTotal > 1)) : ?>
+			<?php if (($this->params->def('show_pagination', 2) == 1 || ($this->params->get('show_pagination') == 2)) && ($this->pagination->pagesTotal > 1)) : ?>
 				<div class="pagination">
 					<?php if ($this->params->def('show_pagination_results', 1)) : ?>
 						<p class="counter pull-right">
