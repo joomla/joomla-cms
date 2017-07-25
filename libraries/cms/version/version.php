ea0304a36ed979283b5ffb8daa50ed6c9733cada
Robert Deutz <rdeutz@googlemail.com>
2017-07-25 12:20:08 +0200

    Prepare 3.7.4 Stable Release

--- libraries/cms/version/version.php
+++ libraries/cms/version/version.php
@@ -38,7 +38,7 @@ final class JVersion
 	 * @var    string
 	 * @since  3.5
 	 */
-	const DEV_LEVEL = '4-dev';
+	const DEV_LEVEL = '4';
 
 	/**
 	 * Development status.
@@ -46,7 +46,7 @@ final class JVersion
 	 * @var    string
 	 * @since  3.5
 	 */
-	const DEV_STATUS = 'Development';
+	const DEV_STATUS = 'Stable';
 
 	/**
 	 * Build number.
@@ -70,7 +70,7 @@ final class JVersion
 	 * @var    string
 	 * @since  3.5
 	 */
-	const RELDATE = '18-July-2017';
+	const RELDATE = '25-July-2017';
 
 	/**
 	 * Release time.
@@ -78,7 +78,7 @@ final class JVersion
 	 * @var    string
 	 * @since  3.5
 	 */
-	const RELTIME = '16:03';
+	const RELTIME = '11:11';
 
 	/**
 	 * Release timezone.
