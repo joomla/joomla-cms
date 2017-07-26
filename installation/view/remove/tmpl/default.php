e021459a1c8c979222364daad09ce780813b369c
Brian Teeman <brian@teeman.net>
2017-07-26 16:05:36 +0100

    oops missed one

--- installation/view/remove/tmpl/default.php
+++ installation/view/remove/tmpl/default.php
@@ -20,7 +20,7 @@ defined('_JEXEC') or die;
 	</div>
 	<div class="alert">
 		<p><?php echo JText::sprintf('INSTL_COMPLETE_REMOVE_INSTALLATION', 'installation'); ?></p>
-		<input type="button" class="btn btn-warning" name="instDefault" onclick="Install.removeFolder(this);" value="<?php echo JText::_('INSTL_COMPLETE_REMOVE_FOLDER', 'installation'); ?>" />
+		<input type="button" class="btn btn-warning" name="instDefault" onclick="Install.removeFolder(this);" value="<?php echo JText::sprintf('INSTL_COMPLETE_REMOVE_FOLDER', 'installation'); ?>" />
 	</div>
 	<div class="btn-toolbar">
 		<div class="btn-group">
