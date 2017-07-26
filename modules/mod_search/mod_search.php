dea2ce24b9678262f8db256f67df6404de39ce79
C-Lodder <lodder15@hotmail.co.uk>
2017-07-26 17:32:15 +0100

    Namespace mod search

--- modules/mod_search/mod_search.php
+++ modules/mod_search/mod_search.php
@@ -9,41 +9,39 @@
 
 defined('_JEXEC') or die;
 
-// Include the search functions only once
-JLoader::register('ModSearchHelper', __DIR__ . '/helper.php');
+use Joomla\CMS\Helper\ModuleHelper;
+use Joomla\Module\Search\Site\Helper\SearchHelper;
+use Joomla\CMS\Uri\Uri;
+use Joomla\CMS\Factory;
 
-$lang = JFactory::getLanguage();
-$app  = JFactory::getApplication();
+$app = Factory::getApplication();
 
 if ($params->get('opensearch', 1))
 {
-	$doc = JFactory::getDocument();
-
-	$ostitle = $params->get('opensearch_title', JText::_('MOD_SEARCH_SEARCHBUTTON_TEXT') . ' ' . $app->get('sitename'));
-	$doc->addHeadLink(
-			JUri::getInstance()->toString(array('scheme', 'host', 'port'))
-			. JRoute::_('&option=com_search&format=opensearch'), 'search', 'rel',
-			array(
-				'title' => htmlspecialchars($ostitle, ENT_COMPAT, 'UTF-8'),
-				'type' => 'application/opensearchdescription+xml'
-			)
-		);
+	$ostitle = $params->get('opensearch_title', \JText::_('MOD_SEARCH_SEARCHBUTTON_TEXT') . ' ' . $app->get('sitename'));
+	Factory::getDocument()->addHeadLink(
+		Uri::getInstance()->toString(array('scheme', 'host', 'port')) . JRoute::_('&option=com_search&format=opensearch'), 'search', 'rel',
+		[
+			'title' => htmlspecialchars($ostitle, ENT_COMPAT, 'UTF-8'),
+			'type' => 'application/opensearchdescription+xml'
+		]
+	);
 }
 
-$upper_limit     = $lang->getUpperLimitSearchWord();
+$upper_limit     = Factory::getLanguage()->getUpperLimitSearchWord();
 $button          = $params->get('button', 0);
 $imagebutton     = $params->get('imagebutton', 0);
-$button_text     = htmlspecialchars($params->get('button_text', JText::_('MOD_SEARCH_SEARCHBUTTON_TEXT')), ENT_COMPAT, 'UTF-8');
+$button_text     = htmlspecialchars($params->get('button_text', \JText::_('MOD_SEARCH_SEARCHBUTTON_TEXT')), ENT_COMPAT, 'UTF-8');
 $maxlength       = $upper_limit;
-$text            = htmlspecialchars($params->get('text', JText::_('MOD_SEARCH_SEARCHBOX_TEXT')), ENT_COMPAT, 'UTF-8');
-$label           = htmlspecialchars($params->get('label', JText::_('MOD_SEARCH_LABEL_TEXT')), ENT_COMPAT, 'UTF-8');
+$text            = htmlspecialchars($params->get('text', \JText::_('MOD_SEARCH_SEARCHBOX_TEXT')), ENT_COMPAT, 'UTF-8');
+$label           = htmlspecialchars($params->get('label', \JText::_('MOD_SEARCH_LABEL_TEXT')), ENT_COMPAT, 'UTF-8');
 $set_Itemid      = (int) $params->get('set_itemid', 0);
 $moduleclass_sfx = htmlspecialchars($params->get('moduleclass_sfx'), ENT_COMPAT, 'UTF-8');
 
 if ($imagebutton)
 {
-	$img = ModSearchHelper::getSearchImage($button_text);
+	$img = SearchHelper::getSearchImage($button_text);
 }
 
 $mitemid = $set_Itemid > 0 ? $set_Itemid : $app->input->getInt('Itemid');
-require JModuleHelper::getLayoutPath('mod_search', $params->get('layout', 'default'));
+require ModuleHelper::getLayoutPath('mod_search', $params->get('layout', 'default'));
