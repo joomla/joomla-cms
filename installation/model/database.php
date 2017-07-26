e5f16f9927a3db4b8b330a3cc4487c0a9890fcfe
zero-24 <zero-24@users.noreply.github.com>
2017-07-26 11:02:16 +0200

    Update database.php

--- installation/model/database.php
+++ installation/model/database.php
@@ -174,9 +174,10 @@ class InstallationModelDatabase extends JModelBase
 		// Check the security file if the db_host is not localhost / 127.0.0.1 / ::1
 		if (!in_array($options->db_host, $localhost))
 		{
-			$check = JFactory::getSession()->get('remoteDbFileTestsPassed', false);
-
-			if ($check === false)
+			$remoteDbFileTestsPassed = JFactory::getSession()->get('remoteDbFileTestsPassed', false);
+			
+			// When all checks have been passed we don't need to do this here again.
+			if ($remoteDbFileTestsPassed === false)
 			{
 				// Add the general message
 				JFactory::getApplication()->enqueueMessage(
@@ -232,6 +233,7 @@ class InstallationModelDatabase extends JModelBase
 					return false;
 				}
 
+				// All tests for this session passed set it to the session
 				JFactory::getSession()->set('remoteDbFileTestsPassed', true);
 			}
 		}
