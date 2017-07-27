78ff0fad9ba9d379e82733d3cd0a9cf2f1eecd40
Allon Moritz <allon.moritz@digital-peak.com>
2017-07-27 17:04:43 +0200

    Strip the tags from the label

--- layouts/joomla/form/renderfield.php
+++ layouts/joomla/form/renderfield.php
@@ -33,6 +33,6 @@ $rel   = empty($options['rel']) ? '' : ' ' . $options['rel'];
 		<div class="control-label"><?php echo $label; ?></div>
 		<div class="controls"><?php echo $input; ?></div>
 	<?php else: ?>
-		<div class="controls controls-nolabel" aria-label="<?php echo $label; ?>"><?php echo $input; ?></div>
+		<div class="controls controls-nolabel" aria-label="<?php echo strip_tags($label); ?>"><?php echo $input; ?></div>
 	<?php endif; ?>
 </div>
