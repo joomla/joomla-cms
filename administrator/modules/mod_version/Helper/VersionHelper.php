b944e19128d721572efbbf178677c747845384f7
Allon Moritz <allon.moritz@digital-peak.com>
2017-07-29 18:48:07 +0200

    Namespace mod version (#17259)

--- administrator/modules/mod_version/Helper/VersionHelper.php
+++ administrator/modules/mod_version/Helper/VersionHelper.php
@@ -6,15 +6,18 @@
  * @copyright   Copyright (C) 2005 - 2017 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
+namespace Joomla\Module\Version\Administrator\Helper;
 
 defined('_JEXEC') or die;
+
+use Joomla\CMS\Version;
 
 /**
  * Helper for mod_version
  *
  * @since  1.6
  */
-abstract class ModVersionHelper
+abstract class VersionHelper
 {
 	/**
 	 * Get the member items of the submenu.
@@ -23,7 +26,7 @@ abstract class ModVersionHelper
 	 */
 	public static function getVersion()
 	{
-		$version = new JVersion;
+		$version = new Version();
 
 		return $version::PRODUCT . ' ' . $version->getShortVersion();
 	}
