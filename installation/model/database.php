d6938e1579f3db495c0e0eefd6fa8b49b2942efc
Brian Teeman <brian@teeman.net>
2017-07-30 10:45:28 +0100

    Remote file check comments (#17347)
    Just a small tidy up of the comments for clarity and to avoid any confusion

--- installation/model/database.php
+++ installation/model/database.php
@@ -189,14 +189,14 @@ class InstallationModelDatabase extends JModelBase
 
 			if ($remoteDbFile === false)
 			{
-				// This is the remote database file you need to remove if you want to use a remote database
+				// This is the file you need to remove if you want to use a remote database
 				$remoteDbFile = '_Joomla' . JUserHelper::genRandomPassword(21) . '.txt';
 				JFactory::getSession()->set('remoteDbFile', $remoteDbFile);
 
 				// Get the path
 				$remoteDbPath = JPATH_INSTALLATION . '/' . $remoteDbFile;
 
-				// When the path is not writable the user need to create the file itself
+				// When the path is not writable the user needs to create the file manually
 				if (!JFile::write($remoteDbPath, ''))
 				{
 					// Request to create the file manually
