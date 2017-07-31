0b6fc9d983cd571acd66184804060d82090ec7e9
C-Lodder <lodder15@hotmail.co.uk>
2017-07-31 12:03:07 +0100

    Move JS away from core.js

--- media/system/js/core.js
+++ media/system/js/core.js
@@ -967,37 +967,6 @@ Joomla.editors.instances = Joomla.editors.instances || {
 	};
 
 	/**
-	 * Update colour for selectbox
-	 *
-	 * @return void
-	 */
-	Joomla.updateSelectboxColour = function () {
-		var colourSelects = document.querySelectorAll('.custom-select-color-state');
-		for (var i = 0, l = colourSelects.length; i < l; i++) {
-			// Add class on page load
-			var selectBox = colourSelects[i];
-			if (selectBox.value == 1) {
-				selectBox.classList.add('custom-select-success');
-			}
-			else if (selectBox.value == 0) {
-				selectBox.classList.add('custom-select-danger');
-			}
-
-			// Add class when value is changed
-			selectBox.addEventListener('change', function() {
-				var self = this;
-				self.classList.remove('custom-select-success', 'custom-select-danger');
-				if (self.value == 1) {
-					self.classList.add('custom-select-success');
-				}
-				else if (self.value == 0 || self.value == parseInt(-2)) {
-					self.classList.add('custom-select-danger');
-				}
-			});
-		}
-	};
-
-	/**
 	 * DOMContentLoaded
 	 */
 	document.addEventListener('DOMContentLoaded', function() {
@@ -1011,9 +980,6 @@ Joomla.editors.instances = Joomla.editors.instances || {
 		if (toolbarOptions && !toolbarHelp) {
 			toolbarOptions.classList.add('ml-auto');
 		}
-
-		// Custom select colour states
-		Joomla.updateSelectboxColour();
 	});
 
 }( Joomla, document ));
