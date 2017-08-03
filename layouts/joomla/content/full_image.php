14b2bcc4ee50a5a09c3ba4ef2ef601f49985edb1
ciar4n <ciaran@joomla51.com>
2017-08-04 00:12:39 +0100

    Change layouts

--- layouts/joomla/content/full_image.php
+++ layouts/joomla/content/full_image.php
@@ -12,8 +12,8 @@ $params = $displayData->params;
 ?>
 <?php $images = json_decode($displayData->images); ?>
 	<?php if (isset($images->image_fulltext) && !empty($images->image_fulltext)) : ?>
-		<?php $imgfloat = empty($images->float_fulltext) ? $params->get('float_fulltext') : $images->float_fulltext; ?>
-		<div class="pull-<?php echo htmlspecialchars($imgfloat); ?> item-image"> <img
+		<?php $imgclass = empty($images->image_fulltext_class) ? $params->get('image_fulltext_class') : $images->image_fulltext_class; ?>
+		<div class="<?php echo htmlspecialchars($imgclass); ?> item-image"> <img
 		<?php if ($images->image_fulltext_caption) :
 			echo 'class="caption"' . ' title="' . htmlspecialchars($images->image_fulltext_caption) . '"';
 		endif; ?>
