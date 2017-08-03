132e9555586ed0b67262f26154a8da33dcc5189a
Brian Teeman <brian@teeman.net>
2017-08-03 10:19:11 +0100

    Make the change in the category select instead of the filter

--- administrator/components/com_categories/models/fields/categoryedit.php
+++ administrator/components/com_categories/models/fields/categoryedit.php
@@ -242,10 +242,16 @@ class JFormFieldCategoryEdit extends JFormFieldList
 			$db->setQuery($query);
 			$language = $db->loadResult();
 
+			if ($options[$i]->level != 0)
+			{
+				$options[$i]->level = $options[$i]->level -1;
+			}
+
 			if ($options[$i]->published == 1)
 			{
 				$options[$i]->text = str_repeat('- ', $options[$i]->level) . $options[$i]->text;
 			}
+
 			else
 			{
 				$options[$i]->text = str_repeat('- ', $options[$i]->level) . '[' . $options[$i]->text . ']';
