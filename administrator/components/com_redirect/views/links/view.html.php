aa7c544c4d79e612a9453c223bee83ba65d74e11
Brian Teeman <brian@teeman.net>
2017-08-06 17:06:24 +0100

    Thanks @quy

--- administrator/components/com_redirect/views/links/view.html.php
+++ administrator/components/com_redirect/views/links/view.html.php
@@ -128,12 +128,9 @@ class RedirectViewLinks extends JViewLegacy
 
 		if ($canDo->get('core.edit.state'))
 		{
-			if ($state->get('filter.state') != 2)
-			{
 				JToolbarHelper::divider();
 				JToolbarHelper::publish('links.publish', 'JTOOLBAR_ENABLE', true);
 				JToolbarHelper::unpublish('links.unpublish', 'JTOOLBAR_DISABLE', true);
-			}
 		}
 
 		if ($canDo->get('core.create'))
