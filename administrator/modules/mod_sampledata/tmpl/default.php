7b64a7b440f0cf9511fb8db40e0751758c1c4072
Brian Teeman <brian@teeman.net>
2017-08-11 12:51:15 +0100

    mod_sampledata accessibility (#17485)
    Adds the aria-hidden attribute to the icons to be consistent

--- administrator/modules/mod_sampledata/tmpl/default.php
+++ administrator/modules/mod_sampledata/tmpl/default.php
@@ -30,7 +30,7 @@ JFactory::getDocument()->addScriptDeclaration('
 					<div class="span4">
 						<a href="#" onclick="sampledataApply(this)" data-type="<?php echo $item->name; ?>" data-steps="<?php echo $item->steps; ?>">
 							<strong class="row-title">
-								<span class="icon-<?php echo $item->icon; ?>"> </span>
+								<span class="icon-<?php echo $item->icon; ?>" aria-hidden="true"> </span>
 								<?php echo htmlspecialchars($item->title, ENT_QUOTES, 'UTF-8'); ?>
 							</strong>
 						</a>
