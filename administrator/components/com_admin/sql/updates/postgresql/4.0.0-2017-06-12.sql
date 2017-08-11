61b9cb0b5ff780ec7c5f8541e8b66f2682c1f845
NunoLopes96 <l33520@alunos.uevora.pt>
2017-08-11 19:54:57 +0100

    SQL FILES

--- administrator/components/com_admin/sql/updates/postgresql/4.0.0-2017-06-12.sql
+++ administrator/components/com_admin/sql/updates/postgresql/4.0.0-2017-06-12.sql
@@ -1,2 +1,3 @@
 ALTER TABLE `#__update_sites` ADD COLUMN `checked_out` int(10) unsigned NOT NULL DEFAULT 0;
 ALTER TABLE `#__update_sites` ADD COLUMN `checked_out_time` timestamp without time zone DEFAULT '1970-01-01 00:00:00' NOT NULL;
+ALTER TABLE `#__update_sites` ADD COLUMN `ordering` int(10) unsigned NOT NULL DEFAULT 0;
