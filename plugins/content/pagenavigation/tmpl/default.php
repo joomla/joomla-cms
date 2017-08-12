9fe6fed28d35e787ae268470f78777b17f949bb5
zwiastunsw <s.wajda@joomla.pl>
2017-08-12 19:28:55 +0200

    fix copy/paste error

--- plugins/content/pagenavigation/tmpl/default.php
+++ plugins/content/pagenavigation/tmpl/default.php
@@ -23,7 +23,7 @@ $lang = JFactory::getLanguage(); ?>
 <?php if ($row->next) :
 	$direction = $lang->isRtl() ? 'left' : 'right'; ?>
 	<li class="next">
-		<a class="hasTooltip" title="<?php echo htmlspecialchars($rows[$location+1]->title); ?>" aria-label="<?php echo JText::sprintf('PLG_PAGENAVIGATION_NEXT_A11Y', htmlspecialchars($rows[$location-1]->title) ); ?>" href="<?php echo $row->next; ?>" rel="next">
+		<a class="hasTooltip" title="<?php echo htmlspecialchars($rows[$location+1]->title); ?>" aria-label="<?php echo JText::sprintf('PLG_PAGENAVIGATION_NEXT_A11Y', htmlspecialchars($rows[$location+1]->title) ); ?>" href="<?php echo $row->next; ?>" rel="next">
 			<?php echo '<span aria-hidden="true">' . $row->next_label . '</span> <span class="icon-chevron-' . $direction . '" aria-hidden="true"></span>'; ?>
 		</a>
 	</li>
