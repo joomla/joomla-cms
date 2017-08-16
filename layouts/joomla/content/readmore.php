0f5c2991e645958bdc4af5002467612a91cf4d1f
Brian Teeman <brian@teeman.net>
2017-08-16 09:47:39 +0100

    codestyle

--- layouts/joomla/content/readmore.php
+++ layouts/joomla/content/readmore.php
@@ -31,8 +31,8 @@ $direction = JFactory::getLanguage()->isRtl() ? 'left' : 'right';
 		</a>
 	<?php elseif ($params->get('show_readmore_title', 0) == 0) : ?>
 		<a class="btn" href="<?php echo $displayData['link']; ?>" itemprop="url" aria-label="<?php echo JText::_('COM_CONTENT_READ_MORE'); ?> <?php echo htmlspecialchars($item->title, ENT_QUOTES, 'UTF-8'); ?>">
-				<?php echo '<span class="icon-chevron-' . $direction . '" aria-hidden="true"></span>'; ?> 
-				<?php echo JText::sprintf('COM_CONTENT_READ_MORE_TITLE'); ?>
+			<?php echo '<span class="icon-chevron-' . $direction . '" aria-hidden="true"></span>'; ?> 
+			<?php echo JText::sprintf('COM_CONTENT_READ_MORE_TITLE'); ?>
 		</a>
 	<?php else : ?>
 		<a class="btn" href="<?php echo $displayData['link']; ?>" itemprop="url" aria-label="<?php echo JText::_('COM_CONTENT_READ_MORE'); ?> <?php echo htmlspecialchars($item->title, ENT_QUOTES, 'UTF-8'); ?>">
