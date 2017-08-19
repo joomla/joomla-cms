700e88abf6cdd1285ff54f8ec51e0966095b2b7a
C-Lodder <lodder15@hotmail.co.uk>
2017-08-19 15:09:54 +0100

    SCSS tweaks to dividers

--- administrator/modules/mod_menu/tmpl/default_submenu.php
+++ administrator/modules/mod_menu/tmpl/default_submenu.php
@@ -85,6 +85,8 @@ if ($current->get('link') === '#')
 	$link = '#collapse' . $this->tree->getCounter();
 }
 
+//var_dump($current->get('class'));
+
 if ($link != null && $current->get('target') != null)
 {
 	echo "<a" . $linkClass . $dataToggle . " href=\"" . $link . "\" target=\"" . $current->get('target') . "\">" 
