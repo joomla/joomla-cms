4bd9150389678147ce577f2c8b11624669c005fe
Sven Hurt <kontakt@sven-hurt.de>
2017-08-20 23:23:06 +0200

    Cleanup / Refactoring - variable usage, separating PHP / HTML markup

--- components/com_contact/layouts/fields/render.php
+++ components/com_contact/layouts/fields/render.php
@@ -6,73 +6,41 @@
  * @copyright   Copyright (C) 2005 - 2017 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
+
 defined('_JEXEC') or die;
 
-// Check if we have all the data
-if (!key_exists('item', $displayData) || !key_exists('context', $displayData))
-{
-	return;
-}
-
-// Setting up for display
-$item = $displayData['item'];
-
-if (!$item)
-{
-	return;
-}
-
-$context = $displayData['context'];
-
-if (!$context)
+if (empty($item = $displayData['item']) || !($item instanceof stdClass) || empty($context = $displayData['context']))
 {
 	return;
 }
 
 JLoader::register('FieldsHelper', JPATH_ADMINISTRATOR . '/components/com_fields/helpers/fields.php');
 
-$parts     = explode('.', $context);
-$component = $parts[0];
-$fields    = null;
+$fields = null;
 
-if (key_exists('fields', $displayData))
-{
-	$fields = $displayData['fields'];
-}
-else
+if (empty($fields = $displayData['fields']))
 {
 	$fields = $item->jcfields ?: FieldsHelper::getFields($context, $item, true);
 }
 
-if (!$fields)
+if (empty($fields))
 {
 	return;
 }
 
 // Check if we have mail context in first element
-$isMail = (reset($fields)->context == 'com_contact.mail');
+$isMail = (reset($fields)->context === 'com_contact.mail');
 
-if (!$isMail)
-{
-	// Print the container tag
-	echo '<dl class="fields-container contact-fields dl-horizontal">';
-}
-
-// Loop through the fields and print them
-foreach ($fields as $field)
-{
-	// If the value is empty do nothing
-	if (empty($field->value) && !$isMail)
-	{
+?>
+<?php if (!$isMail) : ?>
+<dl class="fields-container contact-fields dl-horizontal">
+<?php endif; ?>
+<?php foreach ($fields as $field) : ?>
+	<?php if (empty($field->value) && !$isMail) :
 		continue;
-	}
-
-	echo FieldsHelper::render($context, 'field.render', array('field' => $field));
-}
-
-if (!$isMail)
-{
-	// Close the container
-	echo '</dl>';
-}
-
+	endif; ?>
+	<?php echo FieldsHelper::render($context, 'field.render', array('field' => $field)); ?>
+<?php endforeach; ?>
+<?php if (!$isMail) : ?>
+</dl>
+<?php endif; ?>
