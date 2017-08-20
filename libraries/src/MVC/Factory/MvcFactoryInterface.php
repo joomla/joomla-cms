4508d592a3decb193afba7cbfc945463ad0adf66
Michael Babker <michael.babker@gmail.com>
2017-08-20 10:52:14 -0500

    Move files

--- libraries/src/MVC/Factory/MvcFactoryInterface.php
+++ libraries/src/MVC/Factory/MvcFactoryInterface.php
@@ -6,7 +6,7 @@
  * @license    GNU General Public License version 2 or later; see LICENSE
  */
 
-namespace Joomla\CMS\Mvc\Factory;
+namespace Joomla\CMS\MVC\Factory;
 
 defined('JPATH_PLATFORM') or die;
 
