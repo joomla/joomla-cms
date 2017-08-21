9b44e69f34cee0dc67baf407b6c738de4ec1d40d
Allon Moritz <allon.moritz@digital-peak.com>
2017-08-21 17:17:17 +0200

    cs

--- libraries/src/CMS/Form/Field/TimezoneField.php
+++ libraries/src/CMS/Form/Field/TimezoneField.php
@@ -1,7 +1,6 @@
 <?php
 /**
- * @package     Joomla.Platform
- * @subpackage  Form
+ * Joomla! Content Management System
  *
  * @copyright  Copyright (C) 2005 - 2017 Open Source Matters, Inc. All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
@@ -93,15 +92,15 @@ class TimezoneField extends \JFormFieldGroupedList
 	/**
 	 * Method to attach a JForm object to the field.
 	 *
-	 * @param   SimpleXMLElement  $element  The SimpleXMLElement object representing the `<field>` tag for the form field object.
-	 * @param   mixed             $value    The form field value to validate.
-	 * @param   string            $group    The field name group control value. This acts as an array container for the field.
-	 *                                      For example if the field has name="foo" and the group value is set to "bar" then the
-	 *                                      full field name would end up being "bar[foo]".
+	 * @param   \SimpleXMLElement  $element  The SimpleXMLElement object representing the `<field>` tag for the form field object.
+	 * @param   mixed              $value    The form field value to validate.
+	 * @param   string             $group    The field name group control value. This acts as an array container for the field.
+	 *                                       For example if the field has name="foo" and the group value is set to "bar" then the
+	 *                                       full field name would end up being "bar[foo]".
 	 *
 	 * @return  boolean  True on success.
 	 *
-	 * @see     JFormField::setup()
+	 * @see     FormField::setup()
 	 * @since   3.2
 	 */
 	public function setup(\SimpleXMLElement $element, $value, $group = null)
@@ -128,7 +127,7 @@ class TimezoneField extends \JFormFieldGroupedList
 		$groups = array();
 
 		// Get the list of time zones from the server.
-		$zones = DateTimeZone::listIdentifiers();
+		$zones = \DateTimeZone::listIdentifiers();
 
 		// Build the group lists.
 		foreach ($zones as $zone)
