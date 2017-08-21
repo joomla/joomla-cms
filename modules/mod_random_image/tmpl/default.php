beec9ff2c4093f37ca620fa006d1d7c468b155a3
twister65 <olaverre@free.fr>
2017-08-21 19:23:15 +0200

    Responsive Random Image Module
    Concatenation spacing (Joomla! coding rules)
    
    https://forum.joomla.org/viewtopic.php?t=840822

--- modules/mod_random_image/tmpl/default.php
+++ modules/mod_random_image/tmpl/default.php
@@ -13,7 +13,7 @@ defined('_JEXEC') or die;
 <?php if ($link) : ?>
 <a href="<?php echo $link; ?>">
 <?php endif; ?>
-	<?php echo '<img src="'.$image->folder.'/'.$image->name.'" style="max-width:'.($image->width>100?100:$image->width).'%; height:auto;" />'; ?>
+	<?php echo '<img src="' . $image->folder . '/' . $image->name . '" style="max-width:' . ($image->width>100?100:$image->width) . '%; height:auto;" />'; ?>
 <?php if ($link) : ?>
 </a>
 <?php endif; ?>
