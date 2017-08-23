fcfa3260e6805451bf2e60b3fa3b78b76cf33393
David Donges <info@david-donges.de>
2017-08-23 15:13:09 +0200

    Implement php options pre update check

--- administrator/components/com_joomlaupdate/models/default.php
+++ administrator/components/com_joomlaupdate/models/default.php
@@ -1136,6 +1136,7 @@ ENDDATA;
 
 	/**
 	 * Gets PHP options.
+	 * TODO: Outsource, build common code base for pre install and pre update check
 	 *
 	 * @return  array  Array of PHP config options
 	 *
@@ -1144,6 +1145,101 @@ ENDDATA;
 	public function getPhpOptions()
 	{
 		$options = array();
+
+		// Check the PHP Version.
+		// TODO: Check the PHP Version of the upcoming version
+		$option = new stdClass;
+		$option->label  = JText::sprintf('INSTL_PHP_VERSION_NEWER', JOOMLA_MINIMUM_PHP);
+		$option->state  = version_compare(PHP_VERSION, JOOMLA_MINIMUM_PHP, '>=');
+		$option->notice = null;
+		$options[] = $option;
+
+		// Check for magic quotes gpc.
+		$option = new stdClass;
+		$option->label  = JText::_('INSTL_MAGIC_QUOTES_GPC');
+		$option->state  = (ini_get('magic_quotes_gpc') == false);
+		$option->notice = null;
+		$options[] = $option;
+
+		// Check for register globals.
+		$option = new stdClass;
+		$option->label  = JText::_('INSTL_REGISTER_GLOBALS');
+		$option->state  = (ini_get('register_globals') == false);
+		$option->notice = null;
+		$options[] = $option;
+
+		// Check for zlib support.
+		$option = new stdClass;
+		$option->label  = JText::_('INSTL_ZLIB_COMPRESSION_SUPPORT');
+		$option->state  = extension_loaded('zlib');
+		$option->notice = null;
+		$options[] = $option;
+
+		// Check for XML support.
+		$option = new stdClass;
+		$option->label  = JText::_('INSTL_XML_SUPPORT');
+		$option->state  = extension_loaded('xml');
+		$option->notice = null;
+		$options[] = $option;
+
+		// Check for database support.
+		// We are satisfied if there is at least one database driver available.
+		$available = JDatabaseDriver::getConnectors();
+		$option = new stdClass;
+		$option->label  = JText::_('INSTL_DATABASE_SUPPORT');
+		$option->label .= '<br />(' . implode(', ', $available) . ')';
+		$option->state  = count($available);
+		$option->notice = null;
+		$options[] = $option;
+
+		// Check for mbstring options.
+		if (extension_loaded('mbstring'))
+		{
+			// Check for default MB language.
+			$option = new stdClass;
+			$option->label  = JText::_('INSTL_MB_LANGUAGE_IS_DEFAULT');
+			$option->state  = strtolower(ini_get('mbstring.language')) === 'neutral';
+			$option->notice = $option->state ? null : JText::_('INSTL_NOTICEMBLANGNOTDEFAULT');
+			$options[] = $option;
+
+			// Check for MB function overload.
+			$option = new stdClass;
+			$option->label  = JText::_('INSTL_MB_STRING_OVERLOAD_OFF');
+			$option->state  = ini_get('mbstring.func_overload') == 0;
+			$option->notice = $option->state ? null : JText::_('INSTL_NOTICEMBSTRINGOVERLOAD');
+			$options[] = $option;
+		}
+
+		// Check for a missing native parse_ini_file implementation.
+		$option = new stdClass;
+		$option->label  = JText::_('INSTL_PARSE_INI_FILE_AVAILABLE');
+		$option->state  = $this->getIniParserAvailability();
+		$option->notice = null;
+		$options[] = $option;
+
+		// Check for missing native json_encode / json_decode support.
+		$option = new stdClass;
+		$option->label  = JText::_('INSTL_JSON_SUPPORT_AVAILABLE');
+		$option->state  = function_exists('json_encode') && function_exists('json_decode');
+		$option->notice = null;
+		$options[] = $option;
+
+		// Check for mcrypt support
+		$option = new stdClass;
+		$option->label  = JText::_('INSTL_MCRYPT_SUPPORT_AVAILABLE');
+		$option->state  = is_callable('mcrypt_encrypt');
+		$option->notice = $option->state ? null : JText::_('INSTL_NOTICEMCRYPTNOTAVAILABLE');
+		$options[] = $option;
+
+		// Check for configuration file writable.
+		$writable = (is_writable(JPATH_CONFIGURATION . '/configuration.php')
+			|| (!file_exists(JPATH_CONFIGURATION . '/configuration.php') && is_writable(JPATH_ROOT)));
+
+		$option = new stdClass;
+		$option->label  = JText::sprintf('INSTL_WRITABLE', 'configuration.php');
+		$option->state  = $writable;
+		$option->notice = $option->state ? null : JText::_('INSTL_NOTICEYOUCANSTILLINSTALL');
+		$options[] = $option;
 
 		return $options;
 	}
@@ -1160,5 +1256,38 @@ ENDDATA;
 		$settings = array();
 
 		return $settings;
+	}
+
+	/**
+	 * Checks the availability of the parse_ini_file and parse_ini_string functions.
+	 *
+	 * @return  boolean  True if the method exists.
+	 *
+	 * @since   3.1
+	 */
+	public function getIniParserAvailability()
+	{
+		$disabled_functions = ini_get('disable_functions');
+
+		if (!empty($disabled_functions))
+		{
+			// Attempt to detect them in the disable_functions blacklist.
+			$disabled_functions = explode(',', trim($disabled_functions));
+			$number_of_disabled_functions = count($disabled_functions);
+
+			for ($i = 0; $i < $number_of_disabled_functions; $i++)
+			{
+				$disabled_functions[$i] = trim($disabled_functions[$i]);
+			}
+
+			$result = !in_array('parse_ini_string', $disabled_functions);
+		}
+		else
+		{
+			// Attempt to detect their existence; even pure PHP implementation of them will trigger a positive response, though.
+			$result = function_exists('parse_ini_string');
+		}
+
+		return $result;
 	}
 }
