6ae7a9a49567a60adaefd8d0889008f99746c080
David Donges <info@david-donges.de>
2017-08-24 12:38:26 +0200

    Add first version of client-side extension compatibility check
    Added javascript to pre update check that checks the compatibility of 3rd party extensions based on data attributes containing the current version and the update server url.

--- media/com_joomlaupdate/js/default.js
+++ media/com_joomlaupdate/js/default.js
@@ -11,3 +11,71 @@ function extractionMethodHandler(target, prefix)
 		document.getElementById(prefix + '_directory').style.display = displayStyle;
 	});
 }
+
+
+(function($, document, window) {
+    var PreUpdateChecker = {};  // PreUpdateChecker namespace
+
+    PreUpdateChecker.config = {
+        selector: '.extension-check'
+    };
+    
+    PreUpdateChecker.joomlaTargetVersion = window.joomlaTargetVersion;
+
+    PreUpdateChecker.run = function () {
+        var $extensions = $(PreUpdateChecker.config.selector);
+        $extensions.each(function () {
+            PreUpdateChecker.checkCompatibility($(this), PreUpdateChecker.setResultView);
+        });
+    }
+
+    PreUpdateChecker.checkCompatibility = function ($extension, callback) {
+        var extension = {
+            $element: $extension,
+            updateUrl: $extension.data('extensionUpdateUrl'),
+            version: $extension.data('extensionCurrentVersion'),
+            compatible: false
+        };
+
+        if(extension.updateUrl != undefined) {
+            // TODO: set status: no update server
+            callback(extension);
+        }
+
+        $.get(extension.updateUrl, function (data) {
+            extension.compatible = PreUpdateChecker.parseXML(extension.version, data);
+            callback(extension);
+        });
+    }
+
+    PreUpdateChecker.parseXML = function (currentVersion, xml) {
+        // Parse XML via browser's native parsing function and convert it to a valid jQuery object
+        var $xmlDoc = $($.parseXML(xml));
+        var compatible = false;
+        // Iterate all updates..
+        $xmlDoc.find('update').each(function() {
+            // TODO: fix check, respect 3.[1234] as well
+            // TODO: set state: current version compatible or update compatible
+            // Check if update matches new joomla version
+            if($(this).find('targetplatform[name="joomla"]').attr('version') == PreUpdateChecker.joomlaTargetVersion) {
+                compatible = true;
+                return;
+            }
+        });
+        // Return compatibility = false in case no update matches
+        return compatible;
+    }
+
+    PreUpdateChecker.setResultView = function (extensionData) {
+        extensionData.$element.empty();
+        // TODO: localize strings
+        var html = '<p class=\"label label-important\">NO</p>';
+
+        if(extensionData.compatible) {
+            html = '<p class=\"label label-success\">YES</p>';
+        }
+        extensionData.$element.html(html);
+    }
+    // Run PreUpdateChecker on document ready
+    $(PreUpdateChecker.run);
+})(jQuery, document, window);
