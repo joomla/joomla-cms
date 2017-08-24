ba3806ad728210915948d481742ffbebe7f64c93
Allon Moritz <allon.moritz@digital-peak.com>
2017-08-24 08:23:29 +0200

    Support legacy classes

--- libraries/src/Controller/Form.php
+++ libraries/src/Controller/Form.php
@@ -93,7 +93,15 @@ class Form extends Controller
 		{
 			$r = null;
 
-			if (!preg_match('/(.*)Controller\\\\(.*)/i', get_class($this), $r))
+			$match = 'Controller';
+
+			// If there is a namespace append a backslash
+			if (strpos(get_class($this), '\\'))
+			{
+				$match .= '\\\\';
+			}
+
+			if (!preg_match('/(.*)' . $match . '(.*)/i', get_class($this), $r))
 			{
 				throw new \Exception(\JText::_('JLIB_APPLICATION_ERROR_CONTROLLER_GET_NAME'), 500);
 			}
