6e89df410b9c50559fc04d268e5cd3f11f1bc02f
David Donges <info@david-donges.de>
2017-08-24 15:19:09 +0200

    Further error handling and clean up

--- media/com_joomlaupdate/js/default.js
+++ media/com_joomlaupdate/js/default.js
@@ -17,7 +17,15 @@ function extractionMethodHandler(target, prefix)
     var PreUpdateChecker = {};  // PreUpdateChecker namespace
 
     PreUpdateChecker.config = {
+        proxyUrl: 'index.php?option=com_joomlaupdate&task=update.fetchurl&url=',
         selector: '.extension-check'
+    };
+
+    PreUpdateChecker.STATE = {
+        INCOMPATIBLE: 0,
+        COMPATIBLE: 1,
+        MISSING_COMPATIBILITY_TAG: 2,
+        SERVER_ERROR: 3
     };
 
     PreUpdateChecker.joomlaTargetVersion = window.joomlaTargetVersion;
@@ -34,31 +42,39 @@ function extractionMethodHandler(target, prefix)
             $element: $extension,
             updateUrl: $extension.data('extensionUpdateUrl'),
             version: $extension.data('extensionCurrentVersion'),
-            compatible: false,
-            error: false
+            state: PreUpdateChecker.STATE.SERVER_ERROR,
+            compatibleVersion: 0
         };
 
         if(!extension.updateUrl) {
-            extension.error = true;
+            extension.state = PreUpdateChecker.STATE.MISSING_COMPATIBILITY_TAG;
             callback(extension);
         }
 
-        $.get('index.php?option=com_joomlaupdate&task=update.fetchurl&url=' + extension.updateUrl, function (data) {
-            extension.compatible = PreUpdateChecker.parseXML(extension.version, data);
+        $.get(PreUpdateChecker.config.proxyUrl + extension.updateUrl, function (data) {
+            var parseResult = PreUpdateChecker.parseXML(extension.version, data);
+            extension.compatibleVersion = parseResult.version;
+            extension.state = parseResult.state;
+
             callback(extension);
         }).fail(function() {
-            extension.error = true;
+            extension.state = PreUpdateChecker.STATE.SERVER_ERROR;
             callback(extension);
         });
     }
 
     PreUpdateChecker.parseXML = function (currentVersion, xml) {
-        var compatible = false;
+        var result = {
+            version: 0,
+            state: PreUpdateChecker.STATE.INCOMPATIBLE
+        };
+
         // Parse XML via browser's native parsing function and convert it to a valid jQuery object
         try {
             var $xmlDoc = $($.parseXML(xml));
         } catch(e) {
-            return compatible;
+            result.state = PreUpdateChecker.STATE.SERVER_ERROR;
+            return result;
         }
         
         // Iterate all updates..
@@ -66,26 +82,34 @@ function extractionMethodHandler(target, prefix)
             // TODO: fix check, respect 3.[1234] as well
             // TODO: set state: current version compatible or update compatible
             // TODO: check all updates, don't break loop. Chose oldest update
+            // TODO: check if .attr breaks code in case there is no targetplatform node
             // Check if update matches new joomla version
             if($(this).find('targetplatform[name="joomla"]').attr('version') == PreUpdateChecker.joomlaTargetVersion) {
-                compatible = $(this).find('version').text();
-                return;
+                result.state = PreUpdateChecker.STATE.COMPATIBLE;
+                result.version = $(this).find('version').text();
             }
         });
         // Return compatibility = false in case no update matches
-        return compatible;
+        return result;
     }
 
     PreUpdateChecker.setResultView = function (extensionData) {
         extensionData.$element.empty();
 
-        var html = '<p class="label label-important">' + COM_JOOMLAUPDATE_VIEW_DEFAULT_PREUPDATE_CHECK_NO + '</p>';
-        if(extensionData.error) {
-            html = '<p class="label">' + COM_JOOMLAUPDATE_VIEW_DEFAULT_EXTENSION_WARNING_UNKNOWN + '</p>';
-        }
-        if(extensionData.compatible) {
-            // TODO: distinct, current version compatible and future version compatible
-            html = '<p class="label label-success">' + COM_JOOMLAUPDATE_VIEW_DEFAULT_PREUPDATE_CHECK_YES + ' (' + extensionData.compatible + ')</p>';
+        var html = '';
+        switch (extensionData.state) {
+            case PreUpdateChecker.STATE.COMPATIBLE:
+                html = '<p class="label label-success">' + COM_JOOMLAUPDATE_VIEW_DEFAULT_PREUPDATE_CHECK_YES
+                    + ' (' + extensionData.compatibleVersion + ')</p>';
+                break;
+            case PreUpdateChecker.STATE.INCOMPATIBLE:
+                html = '<p class="label label-important">' + COM_JOOMLAUPDATE_VIEW_DEFAULT_PREUPDATE_CHECK_NO + '</p>';
+                break;
+            case PreUpdateChecker.STATE.MISSING_COMPATIBILITY_TAG:
+                html = '<p class="label">' + COM_JOOMLAUPDATE_VIEW_DEFAULT_EXTENSION_VERSION_MISSING + '</p>';
+                break;
+            default:
+                html = '<p class="label">' + COM_JOOMLAUPDATE_VIEW_DEFAULT_EXTENSION_WARNING_UNKNOWN + '</p>';
         }
         extensionData.$element.html(html);
     }
