b047491cd90e3f6c051383713f788eacdc1890eb
Unknown <cavallo.livio@tiscali.it>
2017-08-28 13:33:13 +0200

    improve input check and fix authorisation

--- components/com_content/models/article.php
+++ components/com_content/models/article.php
@@ -40,6 +40,19 @@ class ContentModelArticle extends JModelItem
 
 		// Load state from the request.
 		$pk = $app->input->getInt('id');
+
+		// Check for malformed url, lacking article id (empty $pk)
+		if (empty($pk))
+		{
+			// Retrieve article id from current menuitem
+			$currMenuItem = empty($menu = $app->getMenu()) ? null : $menu->getActive();
+			$pk = empty($currMenuItem) ? 0 : $currMenuItem->query['id'];
+			
+			// If also menuitem query is malformed, lacks article id, then use 0: unexistent article that wont't be found
+			// and subsequent authorisation will look up the tree, on category, component, root eventually denying permissions.
+			$pk = empty($pk) ? 0 : $pk;
+		}
+
 		$this->setState('article.id', $pk);
 
 		$offset = $app->input->getUInt('limitstart');
@@ -52,7 +65,7 @@ class ContentModelArticle extends JModelItem
 		// TODO: Tune these values based on other permissions.
 		$user = JFactory::getUser();
 
-		if ((!$user->authorise('core.edit.state', 'com_content.article.' . $pk)) && (!$user->authorise('core.edit', 'com_content.' . $pk)))
+		if ((!$user->authorise('core.edit.state', 'com_content.article.' . $pk)) && (!$user->authorise('core.edit', 'com_content.article.' . $pk)))
 		{
 			$this->setState('filter.published', 1);
 			$this->setState('filter.archived', 2);
