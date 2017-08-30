1a964277a09bf2d8d432693611b6df9d632d58e5
uglyeoin <eoin@squareballoon.co.uk>
2017-08-30 16:25:20 +0100

    Made calls consistent
    Removed bottom check for featured and copied first check.  Previously it checked for empty but this was an unnecessary check as featured is either on or off.  I think this may have come from earlier discussion as to which method loads quickest in PHP.  I'm going to leave it this way round but if someone thinks it can load quicker then I am happy to change again.

--- components/com_content/views/category/tmpl/blog.php
+++ components/com_content/views/category/tmpl/blog.php
@@ -98,7 +98,7 @@ $afterDisplayContent = trim(implode("\n", $results));
 				<div class="items-row cols-<?php echo (int) $this->columns; ?> <?php echo 'row-' . $row; ?> row-fluid clearfix">
 			<?php endif; ?>
 			<div class="span<?php echo round(12 / $this->columns); ?>">
-				<div class="item column-<?php echo $rowcount; echo $item->state == 0 ? ' system-unpublished' : null; echo (empty($item->featured)) ? '' : ' item-featured'; ?>"
+				<div class="item column-<?php echo $rowcount; echo $item->state == 0 ? ' system-unpublished' : null; echo $item->featured ? ' item-featured' : ''; ?>"
 					itemprop="blogPost" itemscope itemtype="https://schema.org/BlogPosting">
 					<?php
 					$this->item = & $item;
