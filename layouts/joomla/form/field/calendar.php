123b6c74e7a96f19c6382e69309fe8bdfaf1298c
akritianand <anand.akriti@gmail.com>
2017-09-01 01:34:42 +0530

    Update calendar.php

--- layouts/joomla/form/field/calendar.php
+++ layouts/joomla/form/field/calendar.php
@@ -100,7 +100,7 @@ JHtml::_('behavior.polyfill', array('event', 'classlist', 'map'), 'lte IE 11');
 // The static assets for the calendar
 JHtml::_('script', $localesPath, false, true, false, false, true);
 JHtml::_('script', $helperPath, false, true, false, false, true);
-JHtml::_('script', 'system/fields/calendar.js', false, true, false, false, true);
+JHtml::_('script', 'system/fields/calendar.min.js', false, true, false, false, true);
 JHtml::_('stylesheet', 'system/fields/calendar' . $cssFileExt, array(), true);
 ?>
 <div class="field-calendar">
