0cc0a8c5ead18a04c81df4f4178b20b0fecd5f8d
zero-24 <zero-24@users.noreply.github.com>
2017-09-01 19:05:30 +0200

    thanks @quy

--- components/com_search/views/search/tmpl/default_error.php
+++ components/com_search/views/search/tmpl/default_error.php
@@ -12,6 +12,6 @@ defined('_JEXEC') or die;
 ?>
 <?php if ($this->error) : ?>
 	<div class="error">
-			<?php echo $this->escape($this->error); ?>
+		<?php echo $this->escape($this->error); ?>
 	</div>
 <?php endif; ?>
--- components/com_search/views/search/tmpl/default_form.php
+++ components/com_search/views/search/tmpl/default_form.php
@@ -21,34 +21,42 @@ $upper_limit = $lang->getUpperLimitSearchWord();
 			<input type="text" name="searchword" title="<?php echo JText::_('COM_SEARCH_SEARCH_KEYWORD'); ?>" placeholder="<?php echo JText::_('COM_SEARCH_SEARCH_KEYWORD'); ?>" id="search-searchword" size="30" maxlength="<?php echo $upper_limit; ?>" value="<?php echo $this->escape($this->origkeyword); ?>" class="inputbox" />
 		</div>
 		<div class="btn-group pull-left">
-			<button name="Search" onclick="this.form.submit()" class="btn hasTooltip" title="<?php echo JHtml::_('tooltipText', 'COM_SEARCH_SEARCH');?>"><span class="icon-search"></span><?php echo JText::_('JSEARCH_FILTER_SUBMIT'); ?></button>
+			<button name="Search" onclick="this.form.submit()" class="btn hasTooltip" title="<?php echo JHtml::_('tooltipText', 'COM_SEARCH_SEARCH');?>">
+				<span class="icon-search"></span>
+				<?php echo JText::_('JSEARCH_FILTER_SUBMIT'); ?>
+			</button>
 		</div>
 		<input type="hidden" name="task" value="search" />
 		<div class="clearfix"></div>
 	</div>
 	<div class="searchintro<?php echo $this->params->get('pageclass_sfx'); ?>">
 		<?php if (!empty($this->searchword)) : ?>
-			<p><?php echo JText::plural('COM_SEARCH_SEARCH_KEYWORD_N_RESULTS', '<span class="badge badge-info">' . $this->total . '</span>'); ?></p>
+			<p>
+				<?php echo JText::plural('COM_SEARCH_SEARCH_KEYWORD_N_RESULTS', '<span class="badge badge-info">' . $this->total . '</span>'); ?>
+			</p>
 		<?php endif; ?>
 	</div>
 	<?php if ($this->params->get('search_phrases', 1)) : ?>
 		<fieldset class="phrases">
-			<legend><?php echo JText::_('COM_SEARCH_FOR'); ?>
+			<legend>
+				<?php echo JText::_('COM_SEARCH_FOR'); ?>
 			</legend>
-				<div class="phrases-box">
+			<div class="phrases-box">
 				<?php echo $this->lists['searchphrase']; ?>
-				</div>
-				<div class="ordering-box">
+			</div>
+			<div class="ordering-box">
 				<label for="ordering" class="ordering">
 					<?php echo JText::_('COM_SEARCH_ORDERING'); ?>
 				</label>
 				<?php echo $this->lists['ordering']; ?>
-				</div>
+			</div>
 		</fieldset>
 	<?php endif; ?>
 	<?php if ($this->params->get('search_areas', 1)) : ?>
 		<fieldset class="only">
-			<legend><?php echo JText::_('COM_SEARCH_SEARCH_ONLY'); ?></legend>
+			<legend>
+				<?php echo JText::_('COM_SEARCH_SEARCH_ONLY'); ?>
+			</legend>
 			<?php foreach ($this->searchareas['search'] as $val => $txt) : ?>
 				<?php $checked = is_array($this->searchareas['active']) && in_array($val, $this->searchareas['active']) ? 'checked="checked"' : ''; ?>
 				<label for="area-<?php echo $val; ?>" class="checkbox">
