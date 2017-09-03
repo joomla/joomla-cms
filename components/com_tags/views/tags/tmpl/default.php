ea503ac5c3b5cf4cceb605df5d8497fb18805f75
zero-24 <zero-24@users.noreply.github.com>
2017-09-03 18:30:12 +0200

    Frontend: com_tags view review (#17235)
    * com_tags view review
    
    * thanks @n9iels
    
    * thanks @laoneo
    
    * thanks @quy
    
    * thanks @quy
    
    * thanks @quy
    
    * thanks @quy
    
    * thanks @quy

--- components/com_tags/views/tags/tmpl/default.php
+++ components/com_tags/views/tags/tmpl/default.php
@@ -12,8 +12,9 @@ defined('_JEXEC') or die;
 // Note that there are certain parts of this layout used only when there is exactly one tag.
 
 JHtml::addIncludePath(JPATH_COMPONENT . '/helpers');
-$description = $this->params->get('all_tags_description');
+$description      = $this->params->get('all_tags_description');
 $descriptionImage = $this->params->get('all_tags_description_image');
+
 ?>
 <div class="tag-category<?php echo $this->pageclass_sfx; ?>">
 	<?php if ($this->params->get('show_page_heading')) : ?>
@@ -22,12 +23,14 @@ $descriptionImage = $this->params->get('all_tags_description_image');
 		</h1>
 	<?php endif; ?>
 	<?php if ($this->params->get('all_tags_show_description_image') && !empty($descriptionImage)) : ?>
-		<div><?php echo '<img src="' . $descriptionImage . '">'; ?></div>
+		<div>
+			<img src="<?php echo $descriptionImage; ?>" />
+		</div>
 	<?php endif; ?>
 	<?php if (!empty($description)) : ?>
-		<div><?php echo $description; ?></div>
+		<div>
+			<?php echo $description; ?>
+		</div>
 	<?php endif; ?>
-
 	<?php echo $this->loadTemplate('items'); ?>
-
 </div>
