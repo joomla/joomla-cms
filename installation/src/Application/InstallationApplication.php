979716870dfbda4adc512c6104da6b8861bb83da
Allon Moritz <allon.moritz@digital-peak.com>
2017-09-04 15:08:18 +0200

    Namespace JSON response

--- installation/src/Application/InstallationApplication.php
+++ installation/src/Application/InstallationApplication.php
@@ -15,6 +15,7 @@ use Joomla\Application\Web\WebClient;
 use Joomla\CMS\Date\Date;
 use Joomla\CMS\Factory;
 use Joomla\CMS\Input\Input;
+use Joomla\CMS\Installation\Response\JsonResponse;
 use Joomla\CMS\Language\LanguageHelper;
 use Joomla\CMS\Application\CMSApplication;
 use Joomla\CMS\Document\Document;
