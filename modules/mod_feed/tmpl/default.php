fc72422c4454fe000d8c62440bd77962ee5c3998
Quy <quy@fluxbb.org>
2017-09-04 12:31:24 -0700

    Remove leading space (#30)

--- modules/mod_feed/tmpl/default.php
+++ modules/mod_feed/tmpl/default.php
@@ -15,21 +15,21 @@ defined('_JEXEC') or die;
 <?php else : ?>
 	<?php $lang = JFactory::getLanguage(); ?>
 	<?php $myrtl = $params->get('rssrtl'); ?>
-	<?php $direction = ' '; ?>
+	<?php $direction = ''; ?>
 	<?php $isRtl = $lang->isRtl(); ?>
 	<?php if ($isRtl && $myrtl == 0) : ?>
-		<?php $direction = ' redirect-rtl'; ?>
+		<?php $direction = 'redirect-rtl'; ?>
 	<?php elseif ($isRtl && $myrtl == 1) : ?>
 		<?php // Feed description ?>
-		<?php $direction = ' redirect-ltr'; ?>
+		<?php $direction = 'redirect-ltr'; ?>
 	<?php elseif ($isRtl && $myrtl == 2) : ?>
-		<?php $direction = ' redirect-rtl'; ?>
+		<?php $direction = 'redirect-rtl'; ?>
 	<?php elseif ($myrtl == 0) : ?>
-		<?php $direction = ' redirect-ltr'; ?>
+		<?php $direction = 'redirect-ltr'; ?>
 	<?php elseif ($myrtl == 1): ?>
-		<?php $direction = ' redirect-ltr'; ?>
+		<?php $direction = 'redirect-ltr'; ?>
 	<?php elseif ($myrtl == 2) : ?>
-		<?php $direction = ' redirect-rtl'; ?>
+		<?php $direction = 'redirect-rtl'; ?>
 	<?php endif; ?>
 	<?php if ($feed !== false) : ?>
 		<?php // Image handling ?>
