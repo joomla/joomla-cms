6b79ceab71790c031b975abea4efbe1ebc15dbc5
Constantin Romankiewicz <harmageddon95@web.de>
2017-09-06 15:13:43 +0200

    Moved import below define check.

--- libraries/joomla/form/fields/subform.php
+++ libraries/joomla/form/fields/subform.php
@@ -7,9 +7,9 @@
  * @license     GNU General Public License version 2 or later; see LICENSE
  */
 
-use Joomla\CMS\Form\Form;
-
 defined('JPATH_PLATFORM') or die;
+
+use Joomla\CMS\Form\Form;
 
 jimport('joomla.filesystem.path');
 
