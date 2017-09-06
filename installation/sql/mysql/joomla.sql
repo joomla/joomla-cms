3511d6835337027109cd072ef564270cefab5458
christianboulbi <31202829+christianboulbi@users.noreply.github.com>
2017-09-06 13:15:48 +0200

    Update joomla.sql

--- installation/sql/mysql/joomla.sql
+++ installation/sql/mysql/joomla.sql
@@ -2105,7 +2105,7 @@ CREATE TABLE IF NOT EXISTS `#__item_associations` (
   `parentid` int(10) NOT NULL DEFAULT 0,
   `approved` tinyint(1) NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`)
-  )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 DEFAULT COLLATE=utf8mb4_unicode_ci AUTO_INCREMENT=7;
+  )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 DEFAULT COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `#__viewlevels`
