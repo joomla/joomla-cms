88a0198ac998eee858ed78288c7d78209fc86462
Benjamin Trenkle <benjamin.trenkle@wicked-chick.de>
2017-09-08 23:49:19 +0200

    Create a default state while creating a new workflow

--- administrator/components/com_workflow/Model/Workflow.php
+++ administrator/components/com_workflow/Model/Workflow.php
@@ -93,7 +93,26 @@ class Workflow extends Admin
 			}
 		}
 
-		return parent::save($data);
+		$result = parent::save($data);
+
+		// Create a default state
+		if ($result && $this->getState($this->getName() . '.new'))
+		{
+			$state = $this->getTable('State');
+
+			$newstate = new \stdClass;
+
+			$newstate->workflow_id = (int) $this->getState($this->getName() . '.id');
+			$newstate->title = \JText::_('COM_WORKFLOW_PUBLISHED');
+			$newstate->description = '';
+			$newstate->published = 1;
+			$newstate->condition = 1;
+			$newstate->default = 1;
+
+			$state->save($newstate);
+		}
+
+		return $result;
 	}
 
 	/**
