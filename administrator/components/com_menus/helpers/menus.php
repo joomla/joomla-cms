0e5f0dce0fafe541355c51c3796885054e3cbed3
Izhar Aazmi <izharaazmi@gmail.com>
2017-09-14 11:26:07 +0530

    Menu forum links conditional

--- administrator/components/com_menus/helpers/menus.php
+++ administrator/components/com_menus/helpers/menus.php
@@ -453,6 +453,9 @@ class MenusHelper
 			$components = ArrayHelper::getColumn((array) $components, 'element', 'extension_id');
 		}
 
+		$dispatcher = JEventDispatcher::getInstance();
+		$dispatcher->trigger('onPreprocessMenuItems', array('com_menus.administrator.import', &$items, null, true));
+
 		foreach ($items as &$item)
 		{
 			/** @var  JTableMenu  $table */
@@ -480,6 +483,20 @@ class MenusHelper
 			}
 			elseif ($item->type == 'url' || $item->type == 'component')
 			{
+				if (substr($item->link, 0, 8) === 'special:')
+				{
+					$special = substr($item->link, 8);
+
+					if ($special === 'language-forum')
+					{
+						$item->link = 'index.php?option=com_admin&amp;view=help&amp;layout=langforum';
+					}
+					elseif ($special === 'custom-forum')
+					{
+						$item->link = '';
+					}
+				}
+
 				// Try to match an existing record to have minimum collision for a link
 				$keys  = array(
 					'menutype'  => $menutype,
