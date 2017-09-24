9c34c29fd4a1c1161b7590ac894a228b34cf542a
David Neukirchen <david.neukirchen@gmail.com>
2017-09-24 19:00:30 +0300

    Feature/toggle actions (#372)
    * Remove unused file
    
    * Implement toggle selection
    
    * Rebuild assets
    
    * Rebuild assets

--- administrator/components/com_media/resources/scripts/components/browser/items/image.vue
+++ administrator/components/com_media/resources/scripts/components/browser/items/image.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="media-browser-image" @dblclick="openPreview()">
+    <div class="media-browser-image" @dblclick="openPreview()" @mouseleave="showActions = false">
         <div class="media-browser-item-preview">
             <div class="image-brackground">
                 <div class="image-cropped" :style="{ backgroundImage: 'url(' + thumbUrl + ')' }"></div>
@@ -9,16 +9,19 @@
             {{ item.name }} {{ item.filetype }}
         </div>
         <div class="media-browser-select" @click.stop="toggleSelect()"></div>
-        <div class="media-browser-actions">
+        <div class="media-browser-actions" :class="{'active': showActions}">
             <a href="#" class="action-toggle mr-auto">
-                <span class="image-browser-action fa fa-ellipsis-h" aria-hidden="true" @click.stop="toggleActions()"></span>
+                <span class="image-browser-action fa fa-ellipsis-h" aria-hidden="true"
+                      @click.stop="showActions = true"></span>
             </a>
             <div class="media-browser-actions-list">
                 <a href="#" class="action-preview">
-                    <span class="image-browser-action fa fa-search-plus" aria-hidden="true" @click.stop="openPreview()"></span>
+                    <span class="image-browser-action fa fa-search-plus" aria-hidden="true"
+                          @click.stop="openPreview()"></span>
                 </a>
                 <a href="#" class="action-rename">
-                    <span class="image-browser-action fa fa-text-width" aria-hidden="true" @click.stop="openRenameModal()"></span>
+                    <span class="image-browser-action fa fa-text-width" aria-hidden="true"
+                          @click.stop="openRenameModal()"></span>
                 </a>
                 <a href="#" class="action-edit" v-if="canEdit">
                     <span class="image-browser-action fa fa-pencil" aria-hidden="true" @click.stop="editItem()"></span>
@@ -36,6 +39,11 @@
 
     export default {
         name: 'media-browser-item-image',
+        data() {
+            return {
+                showActions: false,
+            }
+        },
         props: ['item'],
         computed: {
             /* Get the item url */
@@ -58,21 +66,21 @@
                 this.$store.dispatch('deleteItem', this.item);
             },
             /* Rename an item */
-	        openRenameModal() {
-	        	this.$store.commit(types.SELECT_BROWSER_ITEM, this.item);
-		        this.$store.commit(types.SHOW_RENAME_MODAL);
-	        },
+            openRenameModal() {
+                this.$store.commit(types.SELECT_BROWSER_ITEM, this.item);
+                this.$store.commit(types.SHOW_RENAME_MODAL);
+            },
             /* Edit an item */
             editItem() {
-              // TODO should we use relative urls here?
-              const fileBaseUrl = Joomla.getOptions('com_media').editViewUrl + '&path=';
+                // TODO should we use relative urls here?
+                const fileBaseUrl = Joomla.getOptions('com_media').editViewUrl + '&path=';
 
-              window.location.href = fileBaseUrl + this.item.path;
+                window.location.href = fileBaseUrl + this.item.path;
             },
             /* Toggle the item selection */
             toggleSelect() {
                 this.$store.dispatch('toggleBrowserItemSelect', this.item);
-            }
+            },
         }
     }
 </script>
