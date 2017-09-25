269767074bce10a7e0d5653d836e2e77842679b5
LivioCavallo <LivioCavallo@users.noreply.github.com>
2017-09-25 13:41:19 +0200

    Fix ACL checks for pagebreak, articles com_content layouts (and their editor XTD buttons) blocking legitimate editors (#17854)
    * removed unnecessary ACL checks (pagebreak, article XTD btns & content)
    
    * ACL check for articles modal view
    
    * fix typo
    
    * fixed edit permission chk and added ACL chks in btns
    
    * cleaned messages

--- plugins/editors-xtd/pagebreak/pagebreak.php
+++ plugins/editors-xtd/pagebreak/pagebreak.php
@@ -35,24 +35,35 @@ class PlgButtonPagebreak extends JPlugin
 	 */
 	public function onDisplay($name)
 	{
+		$input = JFactory::getApplication()->input;
 		$user  = JFactory::getUser();
 
-		if ($user->authorise('core.create', 'com_content')
-			|| $user->authorise('core.edit', 'com_content')
-			|| $user->authorise('core.edit.own', 'com_content'))
+		// Can create in any category (component permission) or at least in one category
+		$canCreateRecords = $user->authorise('core.create', 'com_content')
+			|| count($user->getAuthorisedCategories('com_content', 'core.create')) > 0;
+
+		// Instead of checking edit on all records, we can use **same** check as the form editing view
+		$values = (array) JFactory::getApplication()->getUserState('com_content.edit.article.id');
+		$isEditingRecords = count($values);
+
+		// This ACL check is probably a double-check (form view already performed checks)
+		$hasAccess = $canCreateRecords || $isEditingRecords;
+		if (!$hasAccess)
 		{
-			JFactory::getDocument()->addScriptOptions('xtd-pagebreak', array('editor' => $name));
-			$link = 'index.php?option=com_content&amp;view=article&amp;layout=pagebreak&amp;tmpl=component&amp;e_name=' . $name;
-
-			$button          = new JObject;
-			$button->modal   = true;
-			$button->class   = 'btn';
-			$button->link    = $link;
-			$button->text    = JText::_('PLG_EDITORSXTD_PAGEBREAK_BUTTON_PAGEBREAK');
-			$button->name    = 'copy';
-			$button->options = "{handler: 'iframe', size: {x: 500, y: 300}}";
-
-			return $button;
+			return;
 		}
+
+		JFactory::getDocument()->addScriptOptions('xtd-pagebreak', array('editor' => $name));
+		$link = 'index.php?option=com_content&amp;view=article&amp;layout=pagebreak&amp;tmpl=component&amp;e_name=' . $name;
+
+		$button          = new JObject;
+		$button->modal   = true;
+		$button->class   = 'btn';
+		$button->link    = $link;
+		$button->text    = JText::_('PLG_EDITORSXTD_PAGEBREAK_BUTTON_PAGEBREAK');
+		$button->name    = 'copy';
+		$button->options = "{handler: 'iframe', size: {x: 500, y: 300}}";
+
+		return $button;
 	}
 }
