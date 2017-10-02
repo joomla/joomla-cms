9301fdb8662a963304353945c402718c1dd66a81
Brian Teeman <brian@teeman.net>
2017-10-02 22:41:12 +0100

    extra check

--- administrator/components/com_menus/views/items/tmpl/default_batch_footer.php
+++ administrator/components/com_menus/views/items/tmpl/default_batch_footer.php
@@ -14,7 +14,7 @@ $menuType = JFactory::getApplication()->getUserState('com_menus.items.menutype')
 <a class="btn" type="button" onclick="document.getElementById('batch-menu-id').value='';document.getElementById('batch-access').value='';document.getElementById('batch-language-id').value=''" data-dismiss="modal">
 	<?php echo JText::_('JCANCEL'); ?>
 </a>
-<?php if ((strlen($menuType) && $menuType != '*') || ($published > 0 && $clientId == 1)): ?>
+<?php if ((strlen($menuType) && $menuType != '*' && $clientId == 0) || ($published > 0 && $clientId == 1)): ?>
 	<button class="btn btn-success" type="submit" onclick="Joomla.submitbutton('item.batch');">
 		<?php echo JText::_('JGLOBAL_BATCH_PROCESS'); ?>
 	</button>
