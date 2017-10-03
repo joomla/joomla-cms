fc7803e66a7829b67ae6a8ae9c7fc318c720be3b
Georgios Papadakis <ggppdk@users.noreply.github.com>
2017-10-03 20:53:56 +0300

    Use level filter based on each category

--- administrator/components/com_content/models/articles.php
+++ administrator/components/com_content/models/articles.php
@@ -266,22 +266,16 @@ class ContentModelArticles extends JModelList
 			$query->where('(a.state = 0 OR a.state = 1)');
 		}
 
-
-		// Filter by a single or group of categories.
-		$baselevel  = 1;
+		// Filter by categories and by level
 		$categoryId = $this->getState('filter.category_id');
+		$level = $this->getState('filter.level');
 
-		if (is_numeric($categoryId))
-		{
-			$categoryTable = JTable::getInstance('Category', 'JTable');
-			$categoryTable->load($categoryId);
-			$rgt       = $categoryTable->rgt;
-			$lft       = $categoryTable->lft;
-			$baselevel = (int) $categoryTable->level;
-			$query->where('c.lft >= ' . (int) $lft)
-				->where('c.rgt <= ' . (int) $rgt);
-		}
-		elseif (is_array($categoryId))
+		$categoryId = $categoryId && !is_array($categoryId)
+			? array($categoryId)
+			: $categoryId;
+
+		// Case: Using both categories filter and by level filter
+		if (count($categoryId))
 		{
 			$categoryId = ArrayHelper::toInteger($categoryId);
 			$categoryTable = JTable::getInstance('Category', 'JTable');
@@ -290,19 +284,19 @@ class ContentModelArticles extends JModelList
 			foreach ($categoryId as $filter_catid)
 			{
 				$categoryTable->load($filter_catid);
-				$baselevel = (int) $categoryTable->level > $baselevel
-					? (int) $categoryTable->level
-					: $baselevel;
-				$subcat_items_where[] = '(c.lft >= ' . (int) $categoryTable->lft . ' AND c.rgt <= ' . (int) $categoryTable->rgt . ')';
+				$subcat_items_where[] = '(' .
+					($level ? 'c.level <= ' . ((int) $level + (int) $categoryTable->level - 1) . ' AND ' : '') .
+					'c.lft >= ' . (int) $categoryTable->lft . ' AND ' .
+					'c.rgt <= ' . (int) $categoryTable->rgt . ')';
 			}
 
-			$query->where('(' . implode(' OR ', $subcat_items_where) . ')');
+			$query->where(implode(' OR ', $subcat_items_where));
 		}
 
-		// Filter on the level.
-		if ($level = $this->getState('filter.level'))
+		// Case: Using only the by level filter
+		elseif ($level)
 		{
-			$query->where('c.level <= ' . ((int) $level + (int) $baselevel - 1));
+			$query->where('c.level <= ' . (int) $level);
 		}
 
 		// Filter by author
