849d1f7ebe0abd1ba7e8c38a2ec0bf6e83c28380
Elijah Madden <elijah.madden@nikkoam.com>
2017-10-30 16:32:22 +0900

    Support the 'stylesheet/less' relation for less files. Also support other custom relations (but what for?).

--- libraries/src/Document/Renderer/Html/HeadRenderer.php
+++ libraries/src/Document/Renderer/Html/HeadRenderer.php
@@ -152,6 +152,10 @@ class HeadRenderer extends DocumentRenderer
 		// Generate stylesheet links
 		foreach ($document->_styleSheets as $src => $attribs)
 		{
+			// Find the relation
+			$relation = isset($attribs['relation']) ? $attribs['relation'] : 'stylesheet';
+			$relation = ($relation == 'stylesheet' && \JFile::getExt($src) == 'less') ? 'stylesheet/less' : 'stylesheet';
+
 			// Check if stylesheet uses IE conditional statements.
 			$conditional = isset($attribs['options']) && isset($attribs['options']['conditional']) ? $attribs['options']['conditional'] : null;
 
@@ -170,7 +174,7 @@ class HeadRenderer extends DocumentRenderer
 				$buffer .= '<!--[if ' . $conditional . ']>';
 			}
 
-			$buffer .= '<link href="' . $src . '" rel="stylesheet"';
+			$buffer .= '<link href="' . $src . '" rel="' . $relation . '"';
 
 			// Add script tag attributes.
 			foreach ($attribs as $attrib => $value)
