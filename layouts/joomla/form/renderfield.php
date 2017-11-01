62bc0d5df1a1fa475d8663481ee68a9d44de4e83
Lodder <lodder15@hotmail.co.uk>
2017-11-01 17:22:11 +0000

    Update renderfield.php

--- layouts/joomla/form/renderfield.php
+++ layouts/joomla/form/renderfield.php
@@ -14,9 +14,10 @@ extract($displayData);
 /**
  * Layout variables
  * ---------------------
- * 	$options         : (array)  Optional parameters
- * 	$label           : (string) The html code for the label (not required if $options['hiddenLabel'] is true)
- * 	$input           : (string) The input field html code
+ * 	$options      : (array)  Optional parameters
+ * 	$label        : (string) The html code for the label (not required if $options['hiddenLabel'] is true)
+ * 	$input        : (string) The input field html code
+ * 	$description  : (string)  An optional description to use in a tooltip
  */
 
 if (!empty($options['showonEnabled']))
