dbdd0805362d78cbd1db0e7cbeef5a01e6b5d913
Alireza Kasaaian <alirezakasaaian@gmail.com>
2017-11-15 16:41:44 +0330

    tag.php modified
    File structure corrected and solution for different language support applied

--- layouts/joomla/html/tag.php
+++ layouts/joomla/html/tag.php
@@ -6,8 +6,11 @@
  * @copyright   Copyright (C) 2005 - 2017 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
+
 defined('JPATH_BASE') or die;
+
 use Joomla\Registry\Registry;
+
 /**
  * Layout variables
  * ---------------------
@@ -16,7 +19,10 @@ use Joomla\Registry\Registry;
  * @var  string   $minTermLength  The minimum number of characters for the tag
  * @var  boolean  $allowCustom    Can we insert custom tags?
  */
+
 extract($displayData);
+
+
 // Tags field ajax
 $chosenAjaxSettings = new Registry(
 	array(
@@ -28,26 +34,34 @@ $chosenAjaxSettings = new Registry(
 		'minTermLength' => $minTermLength
 	)
 );
+
 JHtml::_('formbehavior.ajaxchosen', $chosenAjaxSettings);
+
 // Allow custom values?
 if ($allowCustom)
 {
 	JFactory::getDocument()->addScriptDeclaration(
 		"
 		jQuery(document).ready(function ($) {
+
 			var customTagPrefix = '#new#';
+
 			// Method to add tags pressing enter
 			$('" . $selector . "_chzn input').keypress(function(event) {
+
 				// Tag is greater than the minimum required chars and enter pressed
 				if (this.value && this.value.length >= " . $minTermLength . " && (event.which === 13 || event.charCode === 44)) {
+
 					// Search a highlighted result
 					var highlighted = $('" . $selector . "_chzn').find('li.active-result.highlighted').first();
+
 					// Add the highlighted option
 					if (event.which === 13 && highlighted.text() !== '')
 					{
 						// Extra check. If we have added a custom tag with this text remove it
 						var customOptionValue = customTagPrefix + highlighted.text();
 						$('" . $selector . " option').filter(function () { return $(this).val() == customOptionValue; }).remove();
+
 						// Select the highlighted result
 						var tagOption = $('" . $selector . " option').filter(function () { return $(this).html() == highlighted.text(); });
 						tagOption.attr('selected', 'selected');
@@ -56,6 +70,7 @@ if ($allowCustom)
 					else
 					{
 						var customTag = this.value;
+
 						// Extra check. Search if the custom tag already exists (typed faster than AJAX ready)
 						var tagOption = $('" . $selector . " option').filter(function () { return $(this).html() == customTag; });
 						if (tagOption.text() !== '')
@@ -67,13 +82,16 @@ if ($allowCustom)
 							var option = $('<option>');
 							option.text(this.value).val(customTagPrefix + this.value);
 							option.attr('selected','selected');
+
 							// Append the option and repopulate the chosen field
 							$('" . $selector . "').append(option);
 						}
 					}
+
 					this.value = '';
 					$('" . $selector . "').trigger('liszt:updated');
 					event.preventDefault();
+
 				}
 			});
 		});
