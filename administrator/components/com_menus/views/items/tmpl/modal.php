a71772c0046b014e7a07b7d438ef68d3566e3ef0
Quy <quy@fluxbb.org>
2017-11-16 08:38:03 -0800

    Fix popover hidden behind modal header

--- administrator/components/com_menus/views/items/tmpl/modal.php
+++ administrator/components/com_menus/views/items/tmpl/modal.php
@@ -22,6 +22,7 @@ JHtml::_('behavior.core');
 JHtml::_('behavior.polyfill', array('event'), 'lt IE 9');
 JHtml::_('script', 'com_menus/admin-items-modal.min.js', array('version' => 'auto', 'relative' => true));
 JHtml::_('bootstrap.tooltip', '.hasTooltip', array('placement' => 'bottom'));
+JHtml::_('bootstrap.popover', '.hasPopover', array('placement' => 'bottom'));
 JHtml::_('formbehavior.chosen', 'select');
 
 // Special case for the search field tooltip.
