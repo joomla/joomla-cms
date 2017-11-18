f91c1a9e3db183f89dcb5fd64369139b59a959ba
Allon Moritz <allon.moritz@digital-peak.com>
2017-11-18 21:50:14 +0100

    Observers doesn't exist anymore (#18678)

--- libraries/src/MVC/Model/AdminModel.php
+++ libraries/src/MVC/Model/AdminModel.php
@@ -149,14 +149,6 @@ abstract class AdminModel extends FormModel
 	protected $type = null;
 
 	/**
-	 * A tags Observer instance to handle assigned tags (re-usable in batch action methods, initialized via initBatch())
-	 *
-	 * @var     object
-	 * @since   3.8.2
-	 */
-	protected $tagsObserver = null;
-
-	/**
 	 * Constructor.
 	 *
 	 * @param   array                $config   An array of configuration options (name, state, dbo, table_path, ignore_request).
@@ -1483,9 +1475,6 @@ abstract class AdminModel extends FormModel
 			$this->contentType = new \JUcmType;
 			$this->type = $this->contentType->getTypeByTable($this->tableClassName)
 				?: $this->contentType->getTypeByAlias($this->typeAlias);
-
-			// Get tabs observer
-			$this->tagsObserver = $this->table->getObserverOfClass('Joomla\CMS\Table\Observer\Tags');
 		}
 	}
 }
