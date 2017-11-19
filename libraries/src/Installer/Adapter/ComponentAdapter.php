6fa9bdb0e6d042a3956f98746d5611ca203483d1
wilsonge <georgejameswilson@googlemail.com>
2017-11-19 10:16:37 +0000

    Fix uninstalling a component

--- libraries/src/Installer/Adapter/ComponentAdapter.php
+++ libraries/src/Installer/Adapter/ComponentAdapter.php
@@ -409,6 +409,8 @@ class ComponentAdapter extends InstallerAdapter
 		// Now we need to delete the installation directories. This is the final step in uninstalling the component.
 		if (trim($this->extension->element))
 		{
+			$retval = true;
+
 			// Delete the component site directory
 			if (is_dir($this->parent->getPath('extension_site')))
 			{
