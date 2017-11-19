304af08b374ed071ae7e2a27982b91dbad85aef6
George Wilson <georgejameswilson@googlemail.com>
2017-11-19 16:38:17 +0000

    Reset for dev

--- libraries/src/Version.php
+++ libraries/src/Version.php
@@ -60,7 +60,7 @@ final class Version
 	 * @var    string
 	 * @since  3.8.0
 	 */
-	const EXTRA_VERSION = 'alpha2';
+	const EXTRA_VERSION = 'dev';
 
 	/**
 	 * Development status.
@@ -68,7 +68,7 @@ final class Version
 	 * @var    string
 	 * @since  3.5
 	 */
-	const DEV_STATUS = 'Alpha';
+	const DEV_STATUS = 'Development';
 
 	/**
 	 * Code name.
