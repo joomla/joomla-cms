4962d065c495fc6e58a846ab77766e272d5351cd
Brian Teeman <brian@teeman.net>
2017-11-24 18:23:41 +0000

    Update filelist.php

--- libraries/joomla/form/fields/filelist.php
+++ libraries/joomla/form/fields/filelist.php
@@ -81,7 +81,7 @@ class JFormFieldFileList extends JFormFieldList
 	/**
 	 * Method to get certain otherwise inaccessible properties from the form field object.
 	 *
-	 * @param   string  $name  The property name for which to set the value.
+	 * @param   string  $name  The property name for which to get the value.
 	 *
 	 * @return  mixed  The property value or null.
 	 *
