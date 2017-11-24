675f5ce9b24cbb65f292e2dac215565cf5cd1243
photodude <walt@waltsorensen.com>
2017-11-24 16:04:15 -0700

    PHPCS2-autofixers
    Whitespace found at end of line
    No blank line found before/after control structure
    Multi-line function call not indented correctly
    Closing parenthesis of a multi-line function call must be on a line by
    itself
    Expected 1 newline after opening brace
    Blank line found at start of control structure
    Please consider an empty line before the return statement

--- administrator/components/com_modules/models/modules.php
+++ administrator/components/com_modules/models/modules.php
@@ -182,6 +182,7 @@ class ModulesModelModules extends JModelList
 			// Process pagination.
 			$total = count($result);
 			$this->cache[$this->getStoreId('getTotal')] = $total;
+
 			if ($total < $limitstart)
 			{
 				$limitstart = 0;
@@ -303,10 +304,10 @@ class ModulesModelModules extends JModelList
 
 		// Group (careful with PostgreSQL)
 		$query->group(
-				'a.id, a.title, a.note, a.position, a.module, a.language, a.checked_out, ' .
+			'a.id, a.title, a.note, a.position, a.module, a.language, a.checked_out, ' .
 					'a.checked_out_time, a.published, a.access, a.ordering, l.title, l.image, uc.name, ag.title, e.name, ' .
 					'l.lang_code, uc.id, ag.id, mm.moduleid, e.element, a.publish_up, a.publish_down, e.enabled'
-			);
+		);
 
 		// Filter by client.
 		$clientId = $this->getState('client_id');
@@ -316,7 +317,7 @@ class ModulesModelModules extends JModelList
 		$user = JFactory::getUser();
 
 		// Get the current user for authorisation checks
-		if ($user->authorise('core.admin') !== true) 
+		if ($user->authorise('core.admin') !== true)
 		{
 			$groups = implode(',', $user->getAuthorisedViewLevels());
 			$query->where('a.access IN (' . $groups . ')');
@@ -330,6 +331,7 @@ class ModulesModelModules extends JModelList
 
 		// Filter by published state.
 		$state = $this->getState('filter.state');
+
 		if (is_numeric($state))
 		{
 			$query->where($db->quoteName('a.published') . ' = ' . (int) $state);
@@ -385,12 +387,14 @@ class ModulesModelModules extends JModelList
 					(' . $subQuery1 . ') = 0
 					OR ((' . $subQuery1 . ') > 0 AND ' . $db->quoteName('a.id') . ' IN (' . $subQuery2 . '))
 					OR ((' . $subQuery1 . ') < 0 AND ' . $db->quoteName('a.id') . ' NOT IN (' . $subQuery3 . '))
-					)');
+					)'
+				);
 			}
 		}
 
 		// Filter by search in title or note or id:.
 		$search = $this->getState('filter.search');
+
 		if (!empty($search))
 		{
 			if (stripos($search, 'id:') === 0)
