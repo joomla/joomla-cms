57f1c8393f7b08230b141ec2922b60d3453be7da
Brian Teeman <brian@teeman.net>
2017-11-24 18:22:53 +0000

    Update color.php

--- libraries/joomla/form/fields/color.php
+++ libraries/joomla/form/fields/color.php
@@ -85,7 +85,7 @@ class JFormFieldColor extends JFormField
 	/**
 	 * Method to get certain otherwise inaccessible properties from the form field object.
 	 *
-	 * @param   string  $name  The property name for which to set the value.
+	 * @param   string  $name  The property name for which to get the value.
 	 *
 	 * @return  mixed  The property value or null.
 	 *
