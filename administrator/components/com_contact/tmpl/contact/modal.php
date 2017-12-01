cc3042b85998342ef61852f1b51c9db571754ae0
dgt41 <d.grammatiko@gmail.com>
2017-12-01 20:42:34 +0100

    all the rest views that contain this deprecated code

--- administrator/components/com_contact/tmpl/contact/modal.php
+++ administrator/components/com_contact/tmpl/contact/modal.php
@@ -8,23 +8,7 @@
  */
 
 defined('_JEXEC') or die;
-
-// @deprecated 4.0 the function parameter, the inline js and the buttons are not needed since 3.7.0.
-$function  = JFactory::getApplication()->input->getCmd('function', 'jEditContact_' . (int) $this->item->id);
-
-// Function to update input title when changed
-JFactory::getDocument()->addScriptDeclaration('
-	function jEditContactModal() {
-		if (window.parent && document.formvalidator.isValid(document.getElementById("contact-form"))) {
-			return window.parent.' . $this->escape($function) . '(document.getElementById("jform_name").value);
-		}
-	}
-');
 ?>
-<button id="applyBtn" type="button" class="hidden" onclick="Joomla.submitbutton('contact.apply'); jEditContactModal();"></button>
-<button id="saveBtn" type="button" class="hidden" onclick="Joomla.submitbutton('contact.save'); jEditContactModal();"></button>
-<button id="closeBtn" type="button" class="hidden" onclick="Joomla.submitbutton('contact.cancel');"></button>
-
 <div class="container-popup">
 	<?php $this->setLayout('edit'); ?>
 	<?php echo $this->loadTemplate(); ?>
