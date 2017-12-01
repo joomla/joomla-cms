ab473d12d9c166769ead17e3cf6fe2dd864402b8
dgt41 <d.grammatiko@gmail.com>
2017-12-01 17:56:17 +0100

    com_categories

--- administrator/components/com_categories/tmpl/category/modal.php
+++ administrator/components/com_categories/tmpl/category/modal.php
@@ -6,23 +6,21 @@
  * @copyright   Copyright (C) 2005 - 2017 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
+use Joomla\CMS\Factory;
+use Joomla\CMS\HTML\HTMLHelper;
 
 defined('_JEXEC') or die;
 
 // @deprecated 4.0 the function parameter, the inline js and the buttons are not needed since 3.7.0.
-$function  = JFactory::getApplication()->input->getCmd('function', 'jEditCategory_' . (int) $this->item->id);
+$function  = Factory::getApplication()->input->getCmd('function', 'jEditCategory_' . (int) $this->item->id);
 
-// Function to update input title when changed
-JFactory::getDocument()->addScriptDeclaration('
-	function jEditCategoryModal() {
-		if (window.parent && document.formvalidator.isValid(document.getElementById("item-form"))) {
-			return window.parent.' . $this->escape($function) . '(document.getElementById("jform_title").value);
-		}
-	}
-');
+// Pass the function name from PHP to Javascript
+Factory::getDocument()->addScriptOptions('categoryEdit', ['name' => $this->escape($function)]);
+
+HTMLHelper::_('script', 'media/com_categories/admin-category-modal.min.js', ['relative' => true, 'version' => 'auto']);
 ?>
-<button id="applyBtn" type="button" class="hidden" onclick="Joomla.submitbutton('category.apply'); jEditCategoryModal();"></button>
-<button id="saveBtn" type="button" class="hidden" onclick="Joomla.submitbutton('category.save'); jEditCategoryModal();"></button>
+<button id="applyBtn" type="button" class="hidden" onclick="Joomla.submitbutton('category.apply'); Joomla.jEditCategoryModal();"></button>
+<button id="saveBtn" type="button" class="hidden" onclick="Joomla.submitbutton('category.save'); Joomla.jEditCategoryModal();"></button>
 <button id="closeBtn" type="button" class="hidden" onclick="Joomla.submitbutton('category.cancel');"></button>
 
 <div class="container-popup">
