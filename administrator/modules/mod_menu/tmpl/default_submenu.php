b4a47c39dacd4551281f526294b2910e1afef14e
Lodder <lodder15@hotmail.co.uk>
2017-12-02 22:53:50 +0000

    mod_menu improvements (#18946)

--- administrator/modules/mod_menu/tmpl/default_submenu.php
+++ administrator/modules/mod_menu/tmpl/default_submenu.php
@@ -6,9 +6,11 @@
  * @copyright   Copyright (C) 2005 - 2017 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
-use Joomla\CMS\Menu\Node\Separator;
 
 defined('_JEXEC') or die;
+
+use Joomla\CMS\Menu\Node\Separator;
+use Joomla\CMS\Language\Text;
 
 /**
  * =========================================================================================================
@@ -18,6 +20,7 @@ defined('_JEXEC') or die;
  */
 /** @var  \Joomla\Module\Menu\Administrator\Menu\CssMenu  $this */
 $current = $this->tree->getCurrent();
+$class   = '';
 
 // Build the CSS class suffix
 if (!$this->enabled)
@@ -30,6 +33,8 @@ elseif ($current instanceOf Separator)
 }
 elseif ($current->hasChildren())
 {
+	$class = ' class="dropdown-submenu"';
+
 	if ($current->getLevel() == 1)
 	{
 		$class = ' class="parent"';
@@ -38,14 +43,6 @@ elseif ($current->hasChildren())
 	{
 		$class = ' class="dropdown scrollable-menu"';
 	}
-	else
-	{
-		$class = ' class="dropdown-submenu"';
-	}
-}
-else
-{
-	$class = '';
 }
 
 // Print the item
@@ -89,23 +86,23 @@ if ($link !== null && $current->get('target') !== null)
 {
 	echo "<a" . $linkClass . $dataToggle . " href=\"" . $link . "\" target=\"" . $current->get('target') . "\">" 
 		. $iconClass
-		. '<span class="sidebar-item-title">' . JText::_($current->get('title')) . "</span></a>";
+		. '<span class="sidebar-item-title">' . Text::_($current->get('title')) . "</span></a>";
 }
 elseif ($link !== null && $current->get('target') === null)
 {
 	echo "<a" . $linkClass . $dataToggle . " href=\"" . $link . "\">"
 		. $iconClass
-		. '<span class="sidebar-item-title" >' . JText::_($current->get('title')) . "</span></a>";
+		. '<span class="sidebar-item-title" >' . Text::_($current->get('title')) . "</span></a>";
 }
 elseif ($current->get('title') !== null && $current->get('class') !== 'separator')
 {
 	echo "<a" . $linkClass . $dataToggle . ">"
 		. $iconClass
-		. '<span class="sidebar-item-title" >' . JText::_($current->get('title')) . "</span></a>";
+		. '<span class="sidebar-item-title" >' . Text::_($current->get('title')) . "</span></a>";
 }
 else
 {
-	echo '<span>' . JText::_($current->get('title')) . '</span>';
+	echo '<span>' . Text::_($current->get('title')) . '</span>';
 }
 
 // Recurse through children if they exist
@@ -120,7 +117,7 @@ if ($this->enabled && $current->hasChildren())
 	else
 	{
 		echo '<ul id="collapse' . $this->tree->getCounter() . '" class="nav panel-collapse collapse-level-1 collapse" role="menu" aria-hidden="true">
-		   <li>' . JText::_($current->get('title')) . '<a href="#" class="close"><span aria-label="Close Menu">×</span></a></li>' . "\n";
+		   <li>' . Text::_($current->get('title')) . '<a href="#" class="close"><span aria-label="Close Menu">×</span></a></li>' . "\n";
 	}
 
 	// WARNING: Do not use direct 'include' or 'require' as it is important to isolate the scope for each call
