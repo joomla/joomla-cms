b1275b441385d6dfe6786623dde647d53041d3e4
Dimitri Grammatikogianni <mit505@upshift.gr>
2017-12-02 16:23:11 +0100

    [4.0] Clean up inline js #1 (#18944)
    * com_categories
    
    * Update admin-category-modal.min.js
    
    * oops, this is totally useless
    
    * all the rest views that contain this deprecated code

--- administrator/components/com_content/tmpl/article/modal.php
+++ administrator/components/com_content/tmpl/article/modal.php
@@ -8,23 +8,7 @@
  */
 
 defined('_JEXEC') or die;
-
-// @deprecated 4.0 the function parameter, the inline js and the buttons are not needed since 3.7.0.
-$function  = JFactory::getApplication()->input->getCmd('function', 'jEditArticle_' . (int) $this->item->id);
-
-// Function to update input title when changed
-JFactory::getDocument()->addScriptDeclaration('
-	function jEditArticleModal() {
-		if (window.parent && document.formvalidator.isValid(document.getElementById("item-form"))) {
-			return window.parent.' . $this->escape($function) . '(document.getElementById("jform_title").value);
-		}
-	}
-');
 ?>
-<button id="applyBtn" type="button" class="hidden" onclick="Joomla.submitbutton('article.apply'); jEditArticleModal();"></button>
-<button id="saveBtn" type="button" class="hidden" onclick="Joomla.submitbutton('article.save'); jEditArticleModal();"></button>
-<button id="closeBtn" type="button" class="hidden" onclick="Joomla.submitbutton('article.cancel');"></button>
-
 <div class="container-popup">
 	<?php $this->setLayout('edit'); ?>
 	<?php echo $this->loadTemplate(); ?>
