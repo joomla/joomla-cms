348c32c3e02bf40f9d9eec4c47c39dcc092901b1
Michael Richey <github@richeyweb.com>
2017-12-10 23:57:26 -0600

    Update media.php
    Presently, the only users who get a "Select" button must be "Super Administrator"  This enables the select button for everyone who has core.create access in com_media.

--- plugins/fields/media/media.php
+++ plugins/fields/media/media.php
@@ -37,6 +37,10 @@ class PlgFieldsMedia extends \Joomla\Component\Fields\Administrator\Plugin\Field
 		}
 
 		$fieldNode->setAttribute('hide_default', 'true');
+		if(JFactory::getApplication()->getIdentity()->authorise('core.create','com_media'))
+		{
+			$fieldNode->setAttribute('disabled', 'false');
+		}
 
 		return $fieldNode;
 	}
