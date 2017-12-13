3fe4b8963b0e5433339c70deaeb3c3cfc9e146e1
Lodder <lodder15@hotmail.co.uk>
2017-12-13 20:09:04 +0000

    [4.0] Fix missing filters (com_menus & com_associations) (#19044)
    * Fix filter for com_menus
    
    * more fixes

--- administrator/components/com_menus/layouts/joomla/searchtools/default.php
+++ administrator/components/com_menus/layouts/joomla/searchtools/default.php
@@ -15,8 +15,8 @@ $data = $displayData;
 // Receive overridable options
 $data['options'] = !empty($data['options']) ? $data['options'] : array();
 
-if ($data['view'] instanceof \Joomla\Component\Menus\Administrator\View\Items\Html
-	|| $data['view'] instanceof \Joomla\Component\Menus\Administrator\View\Menus\Html)
+if ($data['view'] instanceof \Joomla\Component\Menus\Administrator\View\Items\HtmlView
+	|| $data['view'] instanceof \Joomla\Component\Menus\Administrator\View\Menus\HtmlView)
 {
 	// Client selector doesn't have to activate the filter bar.
 	unset($data['view']->activeFilters['client_id']);
@@ -47,7 +47,7 @@ $filtersClass = isset($data['view']->activeFilters) && $data['view']->activeFilt
 <div class="js-stools clearfix">
 	<div class="clearfix">
         <?php
-        if ($data['view'] instanceof \Joomla\Component\Menus\Administrator\View\Items\Html)
+        if ($data['view'] instanceof \Joomla\Component\Menus\Administrator\View\Items\HtmlView)
         {
 	        // We will get the menutype filter & remove it from the form filters
 	        $menuTypeField = $data['view']->filterForm->getField('menutype');
@@ -69,7 +69,7 @@ $filtersClass = isset($data['view']->activeFilters) && $data['view']->activeFilt
             </div>
 	        <?php
         }
-        elseif ($data['view'] instanceof \Joomla\Component\Menus\Administrator\View\Menus\Html)
+        elseif ($data['view'] instanceof \Joomla\Component\Menus\Administrator\View\Menus\HtmlView)
         {
 	        // Add the client selector before the form filters.
 	        $clientIdField = $data['view']->filterForm->getField('client_id');
