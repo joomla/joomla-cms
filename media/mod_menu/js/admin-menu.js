a8972c03916dbf56b6352c0ce36d5971a2dfc731
C-Lodder <lodder15@hotmail.co.uk>
2017-12-16 12:13:54 +0000

    move sidebar to mod_menu

--- media/mod_menu/js/admin-menu.js
+++ media/mod_menu/js/admin-menu.js
@@ -1,10 +1,8 @@
 /**
- * @package     Joomla.Administrator
- * @subpackage  Templates.Atum
- * @copyright   Copyright (C) 2005 - 2017 Open Source Matters, Inc. All rights reserved.
- * @license     GNU General Public License version 2 or later; see LICENSE.txt
- * @since       4.0
+ * @copyright  Copyright (C) 2005 - 2017 Open Source Matters, Inc. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
+
 
 Joomla = window.Joomla || {};
 
