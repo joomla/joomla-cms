079b3fc36b85389fb4967ffcfc2da8000ba579a8
Nicola Galgano <optimus4joomla@gmail.com>
2017-12-17 10:23:15 +0100

    complete layout

---
+++ components/com_users/views/delete/tmpl/complete.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  com_users
+ *
+ * @copyright   Copyright (C) 2005 - 2017 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+?>
+<div class="delete-complete<?php echo $this->pageclass_sfx; ?>">
+	<?php if ($this->params->get('show_page_heading')) : ?>
+		<h1>
+			<?php echo $this->escape($this->params->get('page_heading')); ?>
+		</h1>
+	<?php endif; ?>
+</div>
+<p><?php echo JText::_('COM_USERS_DELETE_REQUEST_SUCCESS'); ?></p>
