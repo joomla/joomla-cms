24e7247bb53854d120c503b92dac8bed81467774
Tuan Pham Ngoc <github@joomdonation.com>
2018-01-01 09:22:01 +0700

    Remove redundant comment

--- components/com_content/views/category/view.html.php
+++ components/com_content/views/category/view.html.php
@@ -286,9 +286,8 @@ class ContentViewCategory extends JViewCategory
 
 		// Build path from current category until we found matching category from active menu item or root category
 		$path     = array(array('title' => $this->category->title, 'link' => ''));
-		$category = $this->category;
-
-		// Add category link to pathway until the category in active menu item or root category is found
+		$category = $this->category;		
+		
 		while ($category && $category->id != $menuCategoryId && $category->id > 1)
 		{
 			$path[]   = array('title' => $category->title, 'link' => ContentHelperRoute::getCategoryRoute($category->id));
