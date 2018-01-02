acac0d0fdd45426e8438bb6ab65ae3fb9ce45c25
Lodder <lodder15@hotmail.co.uk>
2018-01-02 21:53:02 +0000

    Use namepsaced classes for layouts (#19252)

--- layouts/joomla/editors/buttons/modal.php
+++ layouts/joomla/editors/buttons/modal.php
@@ -9,6 +9,10 @@
 
 defined('_JEXEC') or die;
 
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Uri\Uri;
+
 $button = $displayData;
 
 if (!$button->get('modal'))
@@ -19,7 +23,7 @@ if (!$button->get('modal'))
 $class    = ($button->get('class')) ? $button->get('class') : null;
 $class   .= ($button->get('modal')) ? ' modal-button' : null;
 $href     = '#' . str_replace(' ', '', $button->get('text')) . 'Modal';
-$link     = ($button->get('link')) ? JUri::base() . $button->get('link') : null;
+$link     = ($button->get('link')) ? Uri::base() . $button->get('link') : null;
 $onclick  = ($button->get('onclick')) ? ' onclick="' . $button->get('onclick') . '"' : '';
 $title    = ($button->get('title')) ? $button->get('title') : $button->get('text');
 $options  = is_array($button->get('options')) ? $button->get('options') : array();
@@ -32,7 +36,7 @@ if (is_array($button->get('options')) && isset($options['confirmText']) && isset
 		. $options['confirmText'] . '</a>';
 }
 // Create the modal
-echo JHtml::_(
+echo HTMLHelper::_(
 	'bootstrap.renderModal',
 	str_replace(' ', '', $button->get('text')) . 'Modal',
 	array(
@@ -43,6 +47,6 @@ echo JHtml::_(
 		'bodyHeight'  => array_key_exists('wibodyHeightdth', $options) ? $options['bodyHeight'] : '70',
 		'modalWidth'  => array_key_exists('modalWidth', $options) ? $options['modalWidth'] : '80',
 		'footer' => $confirm . '<button class="btn btn-secondary" data-dismiss="modal" aria-hidden="true">'
-			. JText::_("JLIB_HTML_BEHAVIOR_CLOSE") . '</button>'
+			. Text::_("JLIB_HTML_BEHAVIOR_CLOSE") . '</button>'
 	)
 );
