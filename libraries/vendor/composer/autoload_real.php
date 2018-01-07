f19583baf2e97c7a21257e2dd0a451fcce1c2e7c
C-Lodder <lodder15@hotmail.co.uk>
2018-01-07 13:30:41 +0000

    Remove radiobasic form field

--- libraries/vendor/composer/autoload_real.php
+++ libraries/vendor/composer/autoload_real.php
@@ -23,7 +23,7 @@ class ComposerAutoloaderInit8462306b3e4ab2d2c9bfd5cc383e4b0c
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit8462306b3e4ab2d2c9bfd5cc383e4b0c', 'loadClassLoader'));
 
-        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
+        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION');
         if ($useStaticLoader) {
             require_once __DIR__ . '/autoload_static.php';
 
