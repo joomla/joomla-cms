6e8f758ff4324ed2b53b92ef13df64e6495a972d
Nicola Galgano <optimus4joomla@gmail.com>
2018-01-13 10:20:22 +0100

    js side

--- media/com_modules/js/admin-modules-modal.js
+++ media/com_modules/js/admin-modules-modal.js
@@ -8,7 +8,8 @@ document.addEventListener('DOMContentLoaded', function() {
 
 	/** Get the elements **/
 	var modulesLinks = document.querySelectorAll('.js-module-insert'), i,
-		positionsLinks = document.querySelectorAll('.js-position-insert');
+		positionsLinks = document.querySelectorAll('.js-position-insert'),
+	    	idLinks = document.querySelectorAll('.js-modid-insert');
 
 	/** Assign listener for click event (for single module insertion) **/
 	for (i= 0; modulesLinks.length > i; i++) {
@@ -41,6 +42,24 @@ document.addEventListener('DOMContentLoaded', function() {
 				Joomla.editors.instances[editor].replaceSelection("{loadposition " + position + "}")
 			} else {
 				window.parent.jInsertEditorText("{loadposition " + position + "}", editor);
+			}
+
+			window.parent.jModalClose();
+		});
+	}
+
+	/** Assign listener for click event (for id insertion) **/
+	for (i= 0; idLinks.length > i; i++) {
+		idLinks[i].addEventListener('click', function(event) {
+			event.preventDefault();
+			var modid = event.target.getAttribute('data-modid'),
+				editor = event.target.getAttribute('data-editor');
+
+			/** Use the API, if editor supports it **/
+			if (window.Joomla && window.Joomla.editors && Joomla.editors.instances && Joomla.editors.instances.hasOwnProperty(editor)) {
+				Joomla.editors.instances[editor].replaceSelection("{loadmoduleid " + modid + "}")
+			} else {
+				window.parent.jInsertEditorText("{loadmoduleid " + modid + "}", editor);
 			}
 
 			window.parent.jModalClose();
