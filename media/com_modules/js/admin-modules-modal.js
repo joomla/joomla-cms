ed57a9be619353f6e38626b4e16a5c21201d6dad
Nicola Galgano <optimus4joomla@gmail.com>
2018-01-15 20:13:08 +0100

    replace loadmodule with loadmoduleid

--- media/com_modules/js/admin-modules-modal.js
+++ media/com_modules/js/admin-modules-modal.js
@@ -8,22 +8,20 @@ document.addEventListener('DOMContentLoaded', function() {
 
 	/** Get the elements **/
 	var modulesLinks = document.querySelectorAll('.js-module-insert'), i,
-		positionsLinks = document.querySelectorAll('.js-position-insert'),
-	    	idLinks = document.querySelectorAll('.js-modid-insert');
+		positionsLinks = document.querySelectorAll('.js-position-insert');
 
-	/** Assign listener for click event (for single module insertion) **/
+	/** Assign listener for click event (for single module id insertion) **/
 	for (i= 0; modulesLinks.length > i; i++) {
 		modulesLinks[i].addEventListener('click', function(event) {
 			event.preventDefault();
-			var type = event.target.getAttribute('data-module'),
-				name = event.target.getAttribute('data-title'),
+			var modid = event.target.getAttribute('data-module'),
 				editor = event.target.getAttribute('data-editor');
 
 			/** Use the API, if editor supports it **/
 			if (window.parent.Joomla && window.parent.Joomla.editors && window.parent.Joomla.editors.instances && window.parent.Joomla.editors.instances.hasOwnProperty(editor)) {
-				window.parent.Joomla.editors.instances[editor].replaceSelection("{loadmodule " + type + "," + name + "}")
+				window.parent.Joomla.editors.instances[editor].replaceSelection("{loadmoduleid " + modid + "}")
 			} else {
-				window.parent.jInsertEditorText("{loadmodule " + type + "," + name + "}", editor);
+				window.parent.jInsertEditorText("{loadmoduleid " + modid + "}", editor);
 			}
 
 			window.parent.jModalClose();
@@ -48,21 +46,4 @@ document.addEventListener('DOMContentLoaded', function() {
 		});
 	}
 
-	/** Assign listener for click event (for id insertion) **/
-	for (i= 0; idLinks.length > i; i++) {
-		idLinks[i].addEventListener('click', function(event) {
-			event.preventDefault();
-			var modid = event.target.getAttribute('data-modid'),
-				editor = event.target.getAttribute('data-editor');
-
-			/** Use the API, if editor supports it **/
-			if (window.Joomla && window.Joomla.editors && Joomla.editors.instances && Joomla.editors.instances.hasOwnProperty(editor)) {
-				Joomla.editors.instances[editor].replaceSelection("{loadmoduleid " + modid + "}")
-			} else {
-				window.parent.jInsertEditorText("{loadmoduleid " + modid + "}", editor);
-			}
-
-			window.parent.jModalClose();
-		});
-	}
 });
