joomla/joomla-cms

commit d0688686669d344d8d0a4c87d55611eff4b74d4d

Do not add default or active Itemid to every link without own menu item (#19099)
* Do not add default or active Itemid to every link without own menu item

* Allow to login when there is no menu item for login page and home page is restricted

* Correctly redirect user to preferred site language association

* Fix link for com_users login form

libraries/src/Router/SiteRouter.php
@@ -707,25 +707,13 @@ class SiteRouter extends Router
 
 		if ($itemid === null)
 		{
-			if ($option = $uri->getVar('option'))
+			if (!$uri->getVar('option'))
 			{
-				$item = $this->menu->getItem($this->getVar('Itemid'));
+				$option = $this->getVar('option');
 
-				if ($item !== null && $item->component === $option)
-				{
-					$uri->setVar('Itemid', $item->id);
-				}
-			}
-			else
-			{
-				if ($option = $this->getVar('option'))
+				if ($option)
 				{
 					$uri->setVar('option', $option);
-				}
-
-				if ($itemid = $this->getVar('Itemid'))
-				{
-					$uri->setVar('Itemid', $itemid);
 				}
 			}
 		}

tests/unit/suites/libraries/cms/router/JRouterSiteTest.php
@@ -1265,24 +1265,6 @@ class JRouterSiteTest extends TestCaseDatabase
 				'preset'   => array('option' => 'com_test'),
 				'expected' => 'index.php?var1=value1&option=com_test'
 			),
-			// Check if a URL with no Itemid and no option, but globally set Itemid is added the Itemid
-			array(
-				'url'      => 'index.php?var1=value1',
-				'preset'   => array('Itemid' => '42'),
-				'expected' => 'index.php?var1=value1&Itemid=42'
-			),
-			// Check if a URL without an Itemid, but with an option set and a global Itemid available, which fits the option of the menu item gets the Itemid appended
-			array(
-				'url'      => 'index.php?var1=value&option=com_test',
-				'preset'   => array('Itemid' => '42'),
-				'expected' => 'index.php?var1=value&option=com_test&Itemid=42'
-			),
-			// Check if a URL without an Itemid, but with an option set and a global Itemid available, which does not fit the option of the menu item gets returned identically
-			array(
-				'url'      => 'index.php?var1=value&option=com_test3',
-				'preset'   => array('Itemid' => '42'),
-				'expected' => 'index.php?var1=value&option=com_test3'
-			),
 		);
 	}
 

modules/mod_login/tmpl/default.php
@@ -15,7 +15,7 @@ JHtml::_('behavior.keepalive');
 JHtml::_('bootstrap.tooltip');
 
 ?>
-<form action="<?php echo JRoute::_('index.php', true, $params->get('usesecure')); ?>" method="post" id="login-form" class="form-inline">
+<form action="<?php echo JRoute::_('index.php?option=com_users&task=user.login', true, $params->get('usesecure')); ?>" method="post" id="login-form" class="form-inline">
 	<?php if ($params->get('pretext')) : ?>
 		<div class="pretext">
 			<p><?php echo $params->get('pretext'); ?></p>
@@ -111,8 +111,6 @@ JHtml::_('bootstrap.tooltip');
 					<?php echo JText::_('MOD_LOGIN_FORGOT_YOUR_PASSWORD'); ?></a>
 				</li>
 			</ul>
-		<input type="hidden" name="option" value="com_users" />
-		<input type="hidden" name="task" value="user.login" />
 		<input type="hidden" name="return" value="<?php echo $return; ?>" />
 		<?php echo JHtml::_('form.token'); ?>
 	</div>

templates/beez3/html/mod_login/default.php
@@ -12,7 +12,7 @@ defined('_JEXEC') or die;
 
 JHtml::_('behavior.keepalive');
 ?>
-<form action="<?php echo JRoute::_('index.php', true, $params->get('usesecure')); ?>" method="post" id="login-form" >
+<form action="<?php echo JRoute::_('index.php?option=com_users&task=user.login', true, $params->get('usesecure')); ?>" method="post" id="login-form" >
 <?php if ($params->get('pretext')) : ?>
 	<div class="pretext">
 	<p><?php echo $params->get('pretext'); ?></p>
@@ -49,8 +49,6 @@ JHtml::_('behavior.keepalive');
 	</p>
 <?php endif; ?>
 <input type="submit" name="Submit" class="button" value="<?php echo JText::_('JLOGIN') ?>" />
-<input type="hidden" name="option" value="com_users" />
-<input type="hidden" name="task" value="user.login" />
 <input type="hidden" name="return" value="<?php echo $return; ?>" />
 <?php echo JHtml::_('form.token'); ?>
 <ul>

libraries/src/Component/Router/Rules/MenuRules.php
@@ -156,20 +156,16 @@ class MenuRules implements RulesInterface
 			}
 		}
 
-		// Check if the active menuitem matches the requested language
-		if ($active && $active->component === 'com_' . $this->router->getName()
-			&& ($language === '*' || in_array($active->language, array('*', $language)) || !\JLanguageMultilang::isEnabled()))
+		// If there is no view and task in query then add the default item id
+		if (!isset($query['view']) && !isset($query['task']))
 		{
-			$query['Itemid'] = $active->id;
-			return;
-		}
+			// If not found, return language specific home link
+			$default = $this->router->menu->getDefault($language);
 
-		// If not found, return language specific home link
-		$default = $this->router->menu->getDefault($language);
-
-		if (!empty($default->id))
-		{
-			$query['Itemid'] = $default->id;
+			if (!empty($default->id))
+			{
+				$query['Itemid'] = $default->id;
+			}
 		}
 	}
 

plugins/system/languagefilter/languagefilter.php
@@ -597,7 +597,6 @@ class PlgSystemLanguageFilter extends JPlugin
 		{
 			if ($this->params->get('automatic_change', 1))
 			{
-				$assoc = JLanguageAssociations::isEnabled();
 				$lang_code = $user['language'];
 
 				// If no language is specified for this user, we set it to the site default language
@@ -617,10 +616,37 @@ class PlgSystemLanguageFilter extends JPlugin
 					$lang_code = $this->current_lang;
 				}
 
+				$foundAssociation = false;
+
+				$assoc = JLanguageAssociations::isEnabled();
+
+				// If association is enabled
+				if ($assoc)
+				{
+					// Retrieves the Itemid from a login form.
+					$uri = new JUri($this->app->getUserState('users.login.form.return'));
+
+					// Get Itemid from SEF or home page
+					$query = $this->app::getRouter()->parse($uri);
+
+					// Check, if the login form contains a menu item redirection.
+					if (!empty($query['Itemid']))
+					{
+						// Try to get associations from that menu item.
+						$associations = MenusHelper::getAssociations($query['Itemid']);
+
+						// If any association set to the user preferred site language, redirect to that page.
+						if (isset($associations[$lang_code]) && $menu->getItem($associations[$lang_code]))
+						{
+							$associationItemid = $associations[$lang_code];
+							$this->app->setUserState('users.login.form.return', 'index.php?Itemid=' . $associationItemid);
+							$foundAssociation = true;
+						}
+					}
+				}
+
 				// Try to get association from the current active menu item
 				$active = $menu->getActive();
-
-				$foundAssociation = false;
 
 				/**
 				 * Looking for associations.
@@ -629,33 +655,14 @@ class PlgSystemLanguageFilter extends JPlugin
 				 * In that case we use the redirect as defined in the menu item.
 				 *  Otherwise we redirect, when available, to the user preferred site language.
 				 */
-				if ($active && !$active->params['login_redirect_url'])
+				if (!$foundAssociation && $active && !$active->params['login_redirect_url'])
 				{
 					if ($assoc)
 					{
 						$associations = MenusHelper::getAssociations($active->id);
 					}
 
-					// Retrieves the Itemid from a login form.
-					$uri = new JUri($this->app->getUserState('users.login.form.return'));
-
-					if ($uri->getVar('Itemid'))
-					{
-						// The login form contains a menu item redirection. Try to get associations from that menu item.
-						// If any association set to the user preferred site language, redirect to that page.
-						if ($assoc)
-						{
-							$associations = MenusHelper::getAssociations($uri->getVar('Itemid'));
-						}
-
-						if (isset($associations[$lang_code]) && $menu->getItem($associations[$lang_code]))
-						{
-							$associationItemid = $associations[$lang_code];
-							$this->app->setUserState('users.login.form.return', 'index.php?Itemid=' . $associationItemid);
-							$foundAssociation = true;
-						}
-					}
-					elseif (isset($associations[$lang_code]) && $menu->getItem($associations[$lang_code]))
+					if (isset($associations[$lang_code]) && $menu->getItem($associations[$lang_code]))
 					{
 						/**
 						 * The login form does not contain a menu item redirection.

templates/protostar/offline.php
@@ -116,7 +116,7 @@ else
 				<?php endif; ?>
 				</div>
 				<jdoc:include type="message" />
-				<form action="<?php echo JRoute::_('index.php', true); ?>" method="post" id="form-login">
+				<form action="<?php echo JRoute::_('index.php?option=com_users&task=user.login'); ?>" method="post" id="form-login">
 					<fieldset>
 						<label for="username"><?php echo JText::_('JGLOBAL_USERNAME'); ?></label>
 						<input name="username" id="username" type="text" title="<?php echo JText::_('JGLOBAL_USERNAME'); ?>" />
@@ -131,8 +131,6 @@ else
 
 						<input type="submit" name="Submit" class="btn btn-primary" value="<?php echo JText::_('JLOGIN'); ?>" />
 
-						<input type="hidden" name="option" value="com_users" />
-						<input type="hidden" name="task" value="user.login" />
 						<input type="hidden" name="return" value="<?php echo base64_encode(JUri::base()); ?>" />
 						<?php echo JHtml::_('form.token'); ?>
 					</fieldset>

templates/system/offline.php
@@ -58,7 +58,7 @@ $twofactormethods = JAuthenticationHelper::getTwoFactorMethods();
 			<?php echo JText::_('JOFFLINE_MESSAGE'); ?>
 		</p>
 	<?php endif; ?>
-	<form action="<?php echo JRoute::_('index.php', true); ?>" method="post" id="form-login">
+	<form action="<?php echo JRoute::_('index.php?option=com_users&task=user.login'); ?>" method="post" id="form-login">
 	<fieldset class="input">
 		<p id="form-login-username">
 			<label for="username"><?php echo JText::_('JGLOBAL_USERNAME'); ?></label>
@@ -77,8 +77,6 @@ $twofactormethods = JAuthenticationHelper::getTwoFactorMethods();
 		<p id="submit-buton">
 			<input type="submit" name="Submit" class="button login" value="<?php echo JText::_('JLOGIN'); ?>" />
 		</p>
-		<input type="hidden" name="option" value="com_users" />
-		<input type="hidden" name="task" value="user.login" />
 		<input type="hidden" name="return" value="<?php echo base64_encode(JUri::base()); ?>" />
 		<?php echo JHtml::_('form.token'); ?>
 	</fieldset>

commit 3655a6e19ea7bc686603531042ac19d8270a4fa1

Regression at createUri from #19099

components/com_content/views/archive/tmpl/default.php
@@ -21,7 +21,7 @@ JHtml::_('behavior.caption');
 </h1>
 </div>
 <?php endif; ?>
-<form id="adminForm" action="<?php echo JRoute::_('index.php'); ?>" method="post" class="form-inline">
+<form id="adminForm" action="<?php echo JRoute::_('index.php?option=com_content&view=archive'); ?>" method="post" class="form-inline">
 	<fieldset class="filters">
 	<div class="filter-search">
 		<?php if ($this->params->get('filter_field') !== 'hide') : ?>
@@ -34,8 +34,6 @@ JHtml::_('behavior.caption');
 		<?php echo $this->form->limitField; ?>
 
 		<button type="submit" class="btn btn-primary" style="vertical-align: top;"><?php echo JText::_('JGLOBAL_FILTER_BUTTON'); ?></button>
-		<input type="hidden" name="view" value="archive" />
-		<input type="hidden" name="option" value="com_content" />
 		<input type="hidden" name="limitstart" value="0" />
 	</div>
 	<br />

libraries/src/Router/SiteRouter.php
@@ -716,6 +716,13 @@ class SiteRouter extends Router
 				{
 					$uri->setVar('option', $option);
 				}
+
+				$itemid = $this->getVar('Itemid');
+
+				if ($itemid)
+				{
+					$uri->setVar('Itemid', $itemid);
+				}
 			}
 		}
 		else

tests/unit/suites/libraries/cms/router/JRouterSiteTest.php
@@ -1265,6 +1265,18 @@ class JRouterSiteTest extends TestCaseDatabase
 				'preset'   => array('option' => 'com_test'),
 				'expected' => 'index.php?var1=value1&option=com_test'
 			),
+			// Check if a URL with no Itemid and no option, but globally set Itemid is added the Itemid
+			array(
+				'url'      => 'index.php?var1=value1',
+				'preset'   => array('Itemid' => '42'),
+				'expected' => 'index.php?var1=value1&Itemid=42'
+			),
+			// Check if a URL with no Itemid and no option, but with an option and a global Itemid available, which fits the option of the menu item gets the Itemid and option appended
+			array(
+				'url'      => 'index.php?var1=value1',
+				'preset'   => array('Itemid' => '42', 'option' => 'com_test'),
+				'expected' => 'index.php?var1=value1&option=com_test&Itemid=42'
+			),
 		);
 	}
 