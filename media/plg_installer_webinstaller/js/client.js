a54b90ecdc51e62129d4eb44d451f8a31066aaf8
Michael Babker <michael.babker@gmail.com>
2018-01-21 10:48:21 -0600

    Add language attributes to IFW server requests

--- media/plg_installer_webinstaller/js/client.js
+++ media/plg_installer_webinstaller/js/client.js
@@ -36,7 +36,7 @@ var apps_base_url = Joomla.getOptions('plg_installer_webinstaller', {}).base_url
 			return false;
 		}
 
-		url += '&product=' + Joomla.apps.options.product + '&release=' + Joomla.apps.options.release + '&dev_level=' + Joomla.apps.options.dev_level + '&list=' + (Joomla.apps.list ? 'list' : 'grid');
+		url += '&product=' + Joomla.apps.options.product + '&release=' + Joomla.apps.options.release + '&dev_level=' + Joomla.apps.options.dev_level + '&list=' + (Joomla.apps.list ? 'list' : 'grid') + '&lang=' + Joomla.apps.options.language;
 
 		if (Joomla.apps.ordering !== "" && document.querySelector('#com-apps-ordering').value) {
 			Joomla.apps.ordering = document.querySelector('#com-apps-ordering').value;
