6ec756531c2d8a947ecbe97d1a42dbb12de3af28
Allon Moritz <allon.moritz@digital-peak.com>
2018-02-02 09:59:37 +0100

    Use the language string for the override message (#493)
    * Use the language string for the override message
    
    * Change string
    
    * rebuild assets
    
    * Remove sprintf dependency
    
    * Revert
    
    * Remove switch

--- administrator/components/com_media/resources/scripts/plugins/translate.js
+++ administrator/components/com_media/resources/scripts/plugins/translate.js
@@ -4,11 +4,34 @@
 
 let Translate = {};
 
+Translate.translate = function (key) {
+	// Translate from Joomla text
+	return Joomla.JText._(key, key);
+}
+
+
+Translate.sprintf = function (string, ...args) {
+	string = this.translate(string);
+	var i = 0;
+	return string.replace(/%((%)|s|d)/g, function (m) {
+		var val = args[i];
+
+		if (m == '%d') {
+			val = parseFloat(val);
+			if (isNaN(val)) {
+				val = 0;
+			}
+		}
+		i++;
+		return val;
+	});
+}
+
 Translate.install = function (Vue, options) {
 	Vue.mixin({
 		methods: {
 			translate: function (key) {
-				 return Joomla.JText._(key, key);
+				return Translate.translate(key);
 			}
 		}
 	})
