bb7d599cd6f5409279033a3fef8bca666982ce7b
dgt41 <d.grammatiko@gmail.com>
2018-02-07 00:15:20 +0100

    upd

--- media/system/webcomponents/js/joomla-hidden-mail-es5.js
+++ media/system/webcomponents/js/joomla-hidden-mail-es5.js
@@ -1,36 +1,66 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 
-var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-var _createClass = function () {
-  function a(a, b) {
-    for (var c, d = 0; d < b.length; d++) {
-      c = b[d], c.enumerable = c.enumerable || !1, c.configurable = !0, 'value' in c && (c.writable = !0), Object.defineProperty(a, c.key, c);
-    }
-  }return function (b, c, d) {
-    return c && a(b.prototype, c), d && a(b, d), b;
-  };
-}();function _classCallCheck(a, b) {
-  if (!(a instanceof b)) throw new TypeError('Cannot call a class as a function');
-}function _possibleConstructorReturn(a, b) {
-  if (!a) throw new ReferenceError('this hasn\'t been initialised - super() hasn\'t been called');return b && ('object' == (typeof b === 'undefined' ? 'undefined' : _typeof(b)) || 'function' == typeof b) ? b : a;
-}function _inherits(a, b) {
-  if ('function' != typeof b && null !== b) throw new TypeError('Super expression must either be null or a function, not ' + (typeof b === 'undefined' ? 'undefined' : _typeof(b)));a.prototype = Object.create(b && b.prototype, { constructor: { value: a, enumerable: !1, writable: !0, configurable: !0 } }), b && (Object.setPrototypeOf ? Object.setPrototypeOf(a, b) : a.__proto__ = b);
-}(function () {
-  var a = function (a) {
-    function b() {
-      return _classCallCheck(this, b), _possibleConstructorReturn(this, (b.__proto__ || Object.getPrototypeOf(b)).apply(this, arguments));
-    }return _inherits(b, a), _createClass(b, [{ key: 'connectedCallback', value: function connectedCallback() {
-        var a;if ('1' === this.getAttribute('is-link')) {
-          a = document.createElement('a'), a.setAttribute('href', 'mailto:' + window.atob(this.getAttribute('first')) + '@' + window.atob(this.getAttribute('last')));for (var b, c = 0, d = this.attributes.length; c < d; ++c) {
-            if (b = this.attributes.item(c).nodeName, b) {
-              if (['is-link', 'is-email', 'first', 'last', 'text'].indexOf(b)) continue;var e = this.attributes.item(c).nodeValue;a.setAttribute(b, e);
-            }
-          }
-        } else a = document.createElement('span');'1' === this.getAttribute('is-email') && (a.innerText = '' === this.getAttribute('text') ? window.atob(this.getAttribute('first')) + '@' + window.atob(this.getAttribute('last')) : window.atob(this.getAttribute('text'))), this.innerText = '', this.appendChild(a);
-      } }]), b;
-  }(HTMLElement);customElements.define('joomla-hidden-mail', a);
-})();
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+;customElements.define('joomla-hidden-mail', function (_HTMLElement) {
+	_inherits(_class, _HTMLElement);
+
+	function _class() {
+		_classCallCheck(this, _class);
+
+		return _possibleConstructorReturn(this, (_class.__proto__ || Object.getPrototypeOf(_class)).apply(this, arguments));
+	}
+
+	_createClass(_class, [{
+		key: 'connectedCallback',
+		value: function connectedCallback() {
+			var newEl = void 0;
+
+			if (this.getAttribute('is-link') === '1') {
+				newEl = document.createElement('a');
+				newEl.setAttribute('href', 'mailto:' + window.atob(this.getAttribute('first')) + '@' + window.atob(this.getAttribute('last')));
+
+				// Get all of the original element attributes, and pass them to the link
+				for (var i = 0, l = this.attributes.length; i < l; ++i) {
+					var nodeName = this.attributes.item(i).nodeName;
+
+					if (nodeName) {
+						// We do care for some attributes
+						if (['is-link', 'is-email', 'first', 'last', 'text'].indexOf(nodeName) > -1) {
+							continue;
+						}
+
+						var nodeValue = this.attributes.item(i).nodeValue;
+
+						newEl.setAttribute(nodeName, nodeValue);
+					}
+				}
+			} else {
+				newEl = document.createElement('span');
+			}
+
+			if (this.getAttribute('text')) {
+				newEl.innerHTML = window.atob(this.getAttribute('text'));
+			} else {
+				newEl.innerText = window.atob(this.getAttribute('first')) + '@' + window.atob(this.getAttribute('last'));
+			}
+
+			// Remove the noscript message
+			this.innerText = '';
+
+			// Display the new element
+			this.appendChild(newEl);
+		}
+	}]);
+
+	return _class;
+}(HTMLElement));
 
 },{}]},{},[1]);
--- media/system/webcomponents/js/joomla-hidden-mail.js
+++ media/system/webcomponents/js/joomla-hidden-mail.js
@@ -1,43 +1,40 @@
-(() => {
-	class JoomlaHiddenMail extends HTMLElement {
-		connectedCallback() {
-			var newEl;
-			var isSpan = false;
+;customElements.define('joomla-hidden-mail', class extends HTMLElement {
+	connectedCallback() {
+		let newEl;
 
-			if (this.getAttribute('is-link') === '1') {
-				newEl = document.createElement('a');
-				newEl.setAttribute('href', 'mailto:' + window.atob(this.getAttribute('first')) + '@' + window.atob(this.getAttribute('last')));
+		if (this.getAttribute('is-link') === '1') {
+			newEl = document.createElement('a');
+			newEl.setAttribute('href', 'mailto:' + window.atob(this.getAttribute('first')) + '@' + window.atob(this.getAttribute('last')));
 
-				// Get all of the original element attributes, and pass them to the link
-				for(var i = 0, l = this.attributes.length; i < l; ++i){
-					var nodeName  = this.attributes.item(i).nodeName;
+			// Get all of the original element attributes, and pass them to the link
+			for(let i = 0, l = this.attributes.length; i < l; ++i){
+				const nodeName  = this.attributes.item(i).nodeName;
 
-					if (nodeName) {
-						// We do care for some attributes
-						if (['is-link', 'is-email', 'first', 'last', 'text'].indexOf(nodeName)) {
-							continue;
-						}
-
-						var nodeValue = this.attributes.item(i).nodeValue;
-
-						newEl.setAttribute(nodeName, nodeValue);
+				if (nodeName) {
+					// We do care for some attributes
+					if (['is-link', 'is-email', 'first', 'last', 'text'].indexOf(nodeName) > -1) {
+						continue;
 					}
+
+					const nodeValue = this.attributes.item(i).nodeValue;
+
+					newEl.setAttribute(nodeName, nodeValue);
 				}
-			} else {
-				newEl = document.createElement('span');
 			}
-
-			if (this.getAttribute('is-email') === '1') {
-				newEl.innerText = this.getAttribute('text') !== '' ? window.atob(this.getAttribute('text')) : window.atob(this.getAttribute('first')) + '@' + window.atob(this.getAttribute('last'));
-			}
-
-			// Remove the noscript message
-			this.innerText = '';
-
-			// Display the new element
-			this.appendChild(newEl);
+		} else {
+			newEl = document.createElement('span');
 		}
-	}
 
-	customElements.define('joomla-hidden-mail', JoomlaHiddenMail);
-})();
+		if (this.getAttribute('text')) {
+			newEl.innerHTML = window.atob(this.getAttribute('text'));
+		} else {
+			newEl.innerText = window.atob(this.getAttribute('first')) + '@' + window.atob(this.getAttribute('last'));
+		}
+
+		// Remove the noscript message
+		this.innerText = '';
+
+		// Display the new element
+		this.appendChild(newEl);
+	}
+});
