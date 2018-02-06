1a3d4bed367a07c3ae30463839751c7fcba5ce02
Michael Babker <michael.babker@gmail.com>
2018-02-05 19:54:17 -0600

    Add a CLI command to run PHP session gc

---
+++ libraries/src/Service/Provider/Console.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * @package     Joomla.Libraries
+ * @subpackage  Service
+ *
+ * @copyright   Copyright (C) 2005 - 2017 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+
+namespace Joomla\CMS\Service\Provider;
+
+defined('JPATH_PLATFORM') or die;
+
+use Joomla\CMS\Console\SessionGcCommand;
+use Joomla\DI\Container;
+use Joomla\DI\ServiceProviderInterface;
+
+/**
+ * Service provider for the application's console services
+ *
+ * @since  4.0
+ */
+class Console implements ServiceProviderInterface
+{
+	/**
+	 * Registers the service provider with a DI container.
+	 *
+	 * @param   Container  $container  The DI container.
+	 *
+	 * @return  void
+	 *
+	 * @since   4.0
+	 */
+	public function register(Container $container)
+	{
+		$container->share(
+			SessionGcCommand::class,
+			function (Container $container)
+			{
+				return new SessionGcCommand($container->get('session'));
+			},
+			true
+		);
+	}
+}
