056a9f8d211903666961a05f42c0ed6d36b11ce8
ReLater <ReLater@users.noreply.github.com>
2018-02-07 01:17:32 +0100

    mod_articles_news. Define $item_heading only if needed (#19439)
    * mod_articles_news. Define $item_heading only if needed
    
    * Update _item.php
    
    * Update _item.php

--- modules/mod_articles_news/tmpl/_item.php
+++ modules/mod_articles_news/tmpl/_item.php
@@ -8,11 +8,9 @@
  */
 
 defined('_JEXEC') or die;
-
-$item_heading = $params->get('item_heading', 'h4');
 ?>
 <?php if ($params->get('item_title')) : ?>
-
+	<?php $item_heading = $params->get('item_heading', 'h4'); ?>
 	<<?php echo $item_heading; ?> class="newsflash-title<?php echo $params->get('moduleclass_sfx'); ?>">
 	<?php if ($item->link !== '' && $params->get('link_titles')) : ?>
 		<a href="<?php echo $item->link; ?>">
@@ -22,7 +20,6 @@ $item_heading = $params->get('item_heading', 'h4');
 		<?php echo $item->title; ?>
 	<?php endif; ?>
 	</<?php echo $item_heading; ?>>
-
 <?php endif; ?>
 
 <?php if (!$params->get('intro_only')) : ?>
