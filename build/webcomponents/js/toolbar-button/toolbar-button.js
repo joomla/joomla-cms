ef55a80c7d641c20162e2273c4ac5e6f795d0b8d
Fedik <getthesite@gmail.com>
2018-02-11 13:35:53 +0200

    Add minified version

--- build/webcomponents/js/toolbar-button/toolbar-button.js
+++ build/webcomponents/js/toolbar-button/toolbar-button.js
@@ -17,28 +17,55 @@
         constructor() {
             super();
 
-            this.addEventListener('click', this.executeTask.bind(this));
+            // We need to do a button to support button behavior, because we cannot currently extend HTMLButtonElement
+            let button = document.createElement('button');
+            button.innerHTML = this.innerHTML;
+            button.className = this.className;
+            button.id        = this.id + '-button';
+            this.innerHTML   = '';
+            this.className   = '';
+
+            // If list selection are required, set button to disabled by default
+            if (this.listConfirmation) {
+                button.setAttribute('disabled', 'disabled');
+            }
+
+            // Keep the button for quick reference
+            this.buttonElement = button;
+
+            this.appendChild(button);
+            this.addEventListener('click', e => this.executeTask());
+        }
+
+        connectedCallback() {
+            // Check whether we have a form
+            let formSelector = this.form || '#adminForm';
+            this.formElement = document.querySelector(formSelector);
+
+            if (this.listConfirmation) {
+                if (!this.formElement) {
+                    throw new Error('The form "' + formSelector + '" is required to perform the task, but the form not found on the page.');
+                }
+
+                // Watch on list selection
+                this.formElement.addEventListener('change', (event) => {
+                    let target = event.target;
+                    if (target.nodeName !== 'INPUT' || (target.name !== 'cid[]' && target.name !== 'checkall-toggle')) {
+                        return;
+                    }
+
+                    // Check whether we have selected something
+                    if (this.formElement.boxchecked.value == 0) {
+                        this.buttonElement.setAttribute('disabled', 'disabled');
+                    } else {
+                        this.buttonElement.removeAttribute('disabled');
+                    }
+                });
+            }
         }
 
         executeTask() {
-            let formId  = this.form || 'adminForm';
-            let form    = document.getElementById(formId);
-            let perform = true;
-
-            if (this.listConfirmation) {
-                if (!form) {
-                    throw new Error('The form "' + formId + '" is required to perform the task, but the form not found on the page.');
-                }
-
-                if (form.boxchecked.value == 0) {
-                    perform = false;
-                    Joomla.renderMessages({'error': [Joomla.JText._('JLIB_HTML_PLEASE_MAKE_A_SELECTION_FROM_THE_LIST')]});
-                }
-            }
-
-            if (perform) {
-                Joomla.submitbutton(this.task, form, this.formValidation);
-            }
+            Joomla.submitbutton(this.task, this.form, this.formValidation);
         }
 
     }
---
+++ media/system/webcomponents/js/joomla-toolbar-button-es5.js
@@ -0,0 +1,119 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+'use strict';
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+/**
+ * @copyright  Copyright (C) 2005 - 2018 Open Source Matters, Inc. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+;(function (customElements) {
+    "use strict";
+
+    var JoomlaToolbarButton = function (_HTMLElement) {
+        _inherits(JoomlaToolbarButton, _HTMLElement);
+
+        _createClass(JoomlaToolbarButton, [{
+            key: 'task',
+
+
+            // Attribute getters
+            get: function get() {
+                return this.getAttribute('task');
+            }
+        }, {
+            key: 'listConfirmation',
+            get: function get() {
+                return this.hasAttribute('list-confirmation');
+            }
+        }, {
+            key: 'form',
+            get: function get() {
+                return this.getAttribute('form');
+            }
+        }, {
+            key: 'formValidation',
+            get: function get() {
+                return this.hasAttribute('form-validation');
+            }
+        }]);
+
+        function JoomlaToolbarButton() {
+            _classCallCheck(this, JoomlaToolbarButton);
+
+            // We need to do a button to support button behavior, because we cannot currently extend HTMLButtonElement
+            var _this = _possibleConstructorReturn(this, (JoomlaToolbarButton.__proto__ || Object.getPrototypeOf(JoomlaToolbarButton)).call(this));
+
+            var button = document.createElement('button');
+            button.innerHTML = _this.innerHTML;
+            button.className = _this.className;
+            button.id = _this.id + '-button';
+            _this.innerHTML = '';
+            _this.className = '';
+
+            // If list selection are required, set button to disabled by default
+            if (_this.listConfirmation) {
+                button.setAttribute('disabled', 'disabled');
+            }
+
+            // Keep the button for quick reference
+            _this.buttonElement = button;
+
+            _this.appendChild(button);
+            _this.addEventListener('click', function (e) {
+                return _this.executeTask();
+            });
+            return _this;
+        }
+
+        _createClass(JoomlaToolbarButton, [{
+            key: 'connectedCallback',
+            value: function connectedCallback() {
+                var _this2 = this;
+
+                // Check whether we have a form
+                var formSelector = this.form || '#adminForm';
+                this.formElement = document.querySelector(formSelector);
+
+                if (this.listConfirmation) {
+                    if (!this.formElement) {
+                        throw new Error('The form "' + formSelector + '" is required to perform the task, but the form not found on the page.');
+                    }
+
+                    // Watch on list selection
+                    this.formElement.addEventListener('change', function (event) {
+                        var target = event.target;
+                        if (target.nodeName !== 'INPUT' || target.name !== 'cid[]' && target.name !== 'checkall-toggle') {
+                            return;
+                        }
+
+                        // Check whether we have selected something
+                        if (_this2.formElement.boxchecked.value == 0) {
+                            _this2.buttonElement.setAttribute('disabled', 'disabled');
+                        } else {
+                            _this2.buttonElement.removeAttribute('disabled');
+                        }
+                    });
+                }
+            }
+        }, {
+            key: 'executeTask',
+            value: function executeTask() {
+                Joomla.submitbutton(this.task, this.form, this.formValidation);
+            }
+        }]);
+
+        return JoomlaToolbarButton;
+    }(HTMLElement);
+
+    customElements.define('joomla-toolbar-button', JoomlaToolbarButton);
+})(customElements);
+
+},{}]},{},[1]);
