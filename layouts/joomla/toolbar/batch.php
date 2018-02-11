629a76215215286a28970aba1b913c183ee05944
Fedik <getthesite@gmail.com>
2018-02-11 17:04:07 +0200

    joomla.toolbar.batch to <joomla-toolbar-button/>

--- layouts/joomla/toolbar/batch.php
+++ layouts/joomla/toolbar/batch.php
@@ -13,15 +13,13 @@ use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
 
 HTMLHelper::_('behavior.core');
+HTMLHelper::_('webcomponent', ['joomla-toolbar-button' => 'system/webcomponents/joomla-toolbar-button.min.js'], ['relative' => true, 'version' => 'auto', 'detectBrowser' => false, 'detectDebug' => true]);
 
 $id    = isset($displayData['id']) ? $displayData['id'] : '';
 $title = $displayData['title'];
-Text::script('JLIB_HTML_PLEASE_MAKE_A_SELECTION_FROM_THE_LIST');
-Text::script('ERROR');
-$message = "{'error': [Joomla.JText._('JLIB_HTML_PLEASE_MAKE_A_SELECTION_FROM_THE_LIST')]}";
-$alert = "Joomla.renderMessages(" . $message . ")";
+
 ?>
-<button<?php echo $id; ?> data-toggle="modal" onclick="if (document.adminForm.boxchecked.value==0){<?php echo $alert; ?>}else{jQuery( '#collapseModal' ).modal('show'); return true;}" class="btn btn-outline-primary btn-sm">
+<joomla-toolbar-button <?php echo $id; ?> execute="jQuery( '#collapseModal' ).modal('show');" list-confirmation class="btn btn-outline-primary btn-sm">
 	<span class="fa fa-square" aria-hidden="true" title="<?php echo $title; ?>"></span>
 	<?php echo $title; ?>
-</button>
+</joomla-toolbar-button>
