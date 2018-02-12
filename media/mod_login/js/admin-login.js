0ce360e3cdf481e817d5d0232dd27eb7b5ec6452
David Neukirchen <david.neukirchen@gmail.com>
2018-02-12 18:48:42 +0100

    Example-3: Execute compiler

--- media/mod_login/js/admin-login.js
+++ media/mod_login/js/admin-login.js
@@ -3,26 +3,23 @@
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 
-(function() {
-	'use strict';
+(function () {
+  var btn = document.getElementById('btn-login-submit');
+  if (btn) {
+    btn.addEventListener('click', function (e) {
+      e.preventDefault();
+      var form = document.getElementById('form-login');
+      if (form && document.formvalidator.isValid(form)) {
+        Joomla.submitbutton('login');
+      }
+    });
+  }
 
-	var btn = document.getElementById('btn-login-submit');
-	if (btn) {
-		btn.addEventListener('click', function(e) {
-			e.preventDefault();
-			var form = document.getElementById('form-login');
-			if (form && document.formvalidator.isValid(form)) {
-				Joomla.submitbutton('login')
-			}
-		});
-	}
-
-	var formTmp = document.querySelector('.login-initial');
-	if (formTmp) {
-		formTmp.style.display = 'block';
-		if (!document.querySelector('joomla-alert')) {
-			document.getElementById('mod-login-username').focus();
-		}
-	}
-
+  var formTmp = document.querySelector('.login-initial');
+  if (formTmp) {
+    formTmp.style.display = 'block';
+    if (!document.querySelector('joomla-alert')) {
+      document.getElementById('mod-login-username').focus();
+    }
+  }
 })();
