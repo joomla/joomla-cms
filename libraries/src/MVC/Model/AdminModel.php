1ec427b939ed3485dffa717443729be7fc38b3e6
Thomas Hunziker <werbemails@bakual.ch>
2018-02-12 08:26:07 +0100

    Defining typeAlias

--- libraries/src/MVC/Model/AdminModel.php
+++ libraries/src/MVC/Model/AdminModel.php
@@ -22,6 +22,14 @@ use Joomla\Utilities\ArrayHelper;
 abstract class AdminModel extends FormModel
 {
 	/**
+	 * The type alias for this content type (for example, 'com_content.article').
+	 *
+	 * @var    string
+	 * @since  __DEPLOY_VERSION__
+	 */
+	public $typeAlias;
+
+	/**
 	 * The prefix to use with controller messages.
 	 *
 	 * @var    string
