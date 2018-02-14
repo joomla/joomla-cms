4bcb1ddfab53c65a68b7053aa6c55a358a906e49
dgt41 <d.grammatiko@gmail.com>
2018-02-14 21:44:36 +0100

    IE11, EDGE and FIREFOX not supporting WC
    Let’s polyfill then

--- build/webcomponents/js/core-loader/core-loader.js
+++ build/webcomponents/js/core-loader/core-loader.js
@@ -3,14 +3,20 @@
 		super();
 
 		const template = document.createElement('template');
-
 		template.innerHTML = `<style>{{CSS_CONTENTS_AUTOMATICALLY_INSERTED_HERE}}</style>
 <div class="box"><span class="yellow"></span><span class="red"></span><span class="blue"></span><span class="green"></span><p>&reg;</p></div>`;
 
+		// Patch shadow DOM
+		if (ShadyCSS) {
+			ShadyCSS.prepareTemplate(template, 'joomla-core-loader');
+		}
+
 		this.attachShadow({mode: 'open'});
 		this.shadowRoot.appendChild(template.content.cloneNode(true));
-	}
-	connectedCallback() {
 
+		// Patch shadow DOM
+		if (ShadyCSS) {
+			ShadyCSS.styleElement(this)
+		}
 	}
 });
--- media/system/webcomponents/js/joomla-core-loader-es5.js
+++ media/system/webcomponents/js/joomla-core-loader-es5.js
@@ -1,8 +1,6 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
@@ -18,18 +16,22 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
 		var _this = _possibleConstructorReturn(this, (_class.__proto__ || Object.getPrototypeOf(_class)).call(this));
 
 		var template = document.createElement('template');
-
 		template.innerHTML = '<style>{{CSS_CONTENTS_AUTOMATICALLY_INSERTED_HERE}}</style>\n<div class="box"><span class="yellow"></span><span class="red"></span><span class="blue"></span><span class="green"></span><p>&reg;</p></div>';
+
+		// Patch shadow DOM
+		if (ShadyCSS) {
+			ShadyCSS.prepareTemplate(template, 'joomla-core-loader');
+		}
 
 		_this.attachShadow({ mode: 'open' });
 		_this.shadowRoot.appendChild(template.content.cloneNode(true));
+
+		// Patch shadow DOM
+		if (ShadyCSS) {
+			ShadyCSS.styleElement(_this);
+		}
 		return _this;
 	}
-
-	_createClass(_class, [{
-		key: 'connectedCallback',
-		value: function connectedCallback() {}
-	}]);
 
 	return _class;
 }(HTMLElement));
