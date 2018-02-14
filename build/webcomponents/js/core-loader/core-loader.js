7bbd18f18bea2bc5db0985c26ce8dea480944986
dgt41 <d.grammatiko@gmail.com>
2018-02-14 14:54:38 +0100

    back to the old spinner

--- build/webcomponents/js/core-loader/core-loader.js
+++ build/webcomponents/js/core-loader/core-loader.js
@@ -3,168 +3,135 @@
 		super();
 
 		// Define some things
-		this.css = `
-joomla-core-loader {
-display: block;
-top:0;
-left:0;
-position: fixed;
-width: 100%;
-height: 100%;
-opacity: .8;
-overflow: hidden;
-z-index: 10000;
-background-color: #fff;
+		this.css = `joomla-core-loader {
+  display: block;
+  top: 0;
+  left: 0;
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  opacity: .8;
+  overflow: hidden;
+  z-index: 10000;
+  background-color: #fff;
 }
-#joomla-loader{
-    transform: rotate(45deg);
-    position: absolute;
-    left: 50%;
+joomla-core-loader .jbox {
+  width: 300px;
+  height: 300px;
+  margin: 0 auto;
+  transform: rotate(45deg);
 }
-
-#joomla-loader-first, #joomla-loader-second, #joomla-loader-third, #joomla-loader-fourth{
-    width: 200px;
-    position: absolute;
+joomla-core-loader .j1, joomla-core-loader .j2, joomla-core-loader .j3, joomla-core-loader .j4 {
+  content: "";
+  position: absolute;
+  width: 90px;
+  height: 90px;
+  border-radius: 90px;
 }
-#joomla-loader-first{
-    left: 200px;
+joomla-core-loader .j1::before, joomla-core-loader .j2::before, joomla-core-loader .j3::before, joomla-core-loader .j4::before {
+  box-sizing: content-box;
+  -webkit-box-sizing: content-box;
+  content: "";
+  position: absolute;
+  width: 50px;
+  height: 35px;
+  margin: 66px 0 0 -30px;
+  background: transparent;
+  border-radius: 75px 75px 0 0;
 }
-#joomla-loader-second{
-    transform: rotate(90deg);
-    left: 340px;
-    top: 153px;
+joomla-core-loader .j1::after, joomla-core-loader .j2::after, joomla-core-loader .j3::after, joomla-core-loader .j4::after {
+  box-sizing: content-box;
+  -webkit-box-sizing: content-box;
+  content: "";
+  position: absolute;
+  width: 50px;
+  height: 101px;
+  margin: 150px 0 0 -30px;
+  background: transparent;
 }
-#joomla-loader-third{
-    transform: rotate(180deg);
-    top: 290px;
-    left: 198px;
+joomla-core-loader .j1 {
+  margin: 0 0 0 182px;
+  background: orange;
+  transform: rotate(0deg);
 }
-#joomla-loader-fourth{
-    transform: rotate(270deg);
-    top: 148px;
-    left:
-        59px;
+joomla-core-loader .j1::before, joomla-core-loader .j1::after {
+  border: 50px solid orange;
 }
-
-.top-circle{
-    border-radius: 50%;
-    width: 115px;
-    height: 115px;
-    margin-left: 45px;
+joomla-core-loader .j1::before {
+  border-width: 50px 50px 0 50px;
 }
-
-.top-arch{
-    width: 200px;
-    height: 100px;
-    /*border-radius: 50%;*/
-    margin-top: -30px;
-    margin-bottom: -1px;
-    margin-left: -1px;
-
+joomla-core-loader .j1::after {
+  border-width: 0 0 0 50px;
 }
-.inner-arch{
-    width: 100px;
-    height: 100px;
-    border-radius: 50%;
-    position: relative;
-    margin: auto;
-    top: 50px;
+joomla-core-loader .j2 {
+  margin: 182px 0 0 364px;
+  background: red;
+  transform: rotate(90deg);
 }
-
-.bottom-rect{
-    width: 61px;
+joomla-core-loader .j2::before, joomla-core-loader .j2::after {
+  border: 50px solid red;
 }
-
-.one {
-    height: 85px;
+joomla-core-loader .j2::before {
+  border-width: 50px 50px 0 50px;
 }
-.two {
-    height: 50px;
-    margin-top: -1px;
+joomla-core-loader .j2::after {
+  border-width: 0 0 0 50px;
 }
-#joomla-loader-second .one{
-    height: 15px;
+joomla-core-loader .j3 {
+  margin: 364px 0 0 182px;
+  background: blue;
+  transform: rotate(180deg);
 }
-#joomla-loader-second .two{
-    height: 61px;
+joomla-core-loader .j3::before, joomla-core-loader .j3::after {
+  border: 50px solid blue;
 }
-#joomla-loader-second .three{
-    height: 55px;
-    margin-top: -1px;
+joomla-core-loader .j3::before {
+  border-width: 50px 50px 0 50px;
 }
-#joomla-loader-third .one{
-    height: 15px;
+joomla-core-loader .j3::after {
+  border-width: 0 0 0 50px;
 }
-#joomla-loader-third .two{
-    height: 120px;
+joomla-core-loader .j4 {
+  margin: 182px 0 0 0;
+  background: green;
+  transform: rotate(270deg);
 }
-
-#joomla-loader-first .top-circle, #joomla-loader-first .bottom-rect .one, #joomla-loader-first .bottom-rect .two{
-    background-color: #F9A541;
+joomla-core-loader .j4::before, joomla-core-loader .j4::after {
+  border: 50px solid green;
 }
-#joomla-loader-first .top-arch{
-    background: rgba(255, 255, 255, 0); /* Old browsers */
-    background: -moz-radial-gradient(center, ellipse cover,  rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, 0) 27%, #f9a541 27%, #f9a541 70%, rgba(255, 255, 255, 0) 70%, rgba(255, 255, 255, 0) 100%); /* FF3.6-15 */
-    background: -webkit-radial-gradient(center, ellipse cover,  rgba(255, 255, 255, 0) 0%,rgba(255, 255, 255, 0) 27%,#f9a541 27%,#f9a541 70%,rgba(255, 255, 255, 0) 70%,rgba(255, 255, 255, 0) 100%); /* Chrome10-25,Safari5.1-6 */
-    background: radial-gradient(ellipse at center,  rgba(255, 255, 255, 0) 0%,rgba(255, 255, 255, 0) 27%,#f9a541 27%,#f9a541 70%,rgba(255, 255, 255, 0) 70%,rgba(255, 255, 255, 0) 100%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
-    filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='rgba(255, 255, 255, 0)', endColorstr='#f9a541',GradientType=1 ); /* IE6-9 fallback on horizontal gradient */
-    background-size: 200px 200px;
-    background-position: top center;
+joomla-core-loader .j4::before {
+  border-width: 50px 50px 0 50px;
 }
-
-#joomla-loader-second .top-circle, #joomla-loader-second .bottom-rect .one, #joomla-loader-second .bottom-rect .two, #joomla-loader-second .bottom-rect .three{
-    background-color: #F44321;
+joomla-core-loader .j4::after {
+  border-width: 0 0 0 50px;
 }
-#joomla-loader-second .top-arch{
-    background: rgba(255, 255, 255, 0); /* Old browsers */
-    background: -moz-radial-gradient(center, ellipse cover,  rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, 0) 27%, #F44321 27%, #F44321 70%, rgba(255, 255, 255, 0) 70%, rgba(255, 255, 255, 0) 100%); /* FF3.6-15 */
-    background: -webkit-radial-gradient(center, ellipse cover,  rgba(255, 255, 255, 0) 0%,rgba(255, 255, 255, 0) 27%,#F44321 27%,#F44321 70%,rgba(255, 255, 255, 0) 70%,rgba(255, 255, 255, 0) 100%); /* Chrome10-25,Safari5.1-6 */
-    background: radial-gradient(ellipse at center,  rgba(255, 255, 255, 0) 0%,rgba(255, 255, 255, 0) 27%,#F44321 27%,#F44321 70%,rgba(255, 255, 255, 0) 70%,rgba(255, 255, 255, 0) 100%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
-    filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='rgba(255, 255, 255, 0)', endColorstr='#F44321',GradientType=1 ); /* IE6-9 fallback on horizontal gradient */
-    background-size: 200px 200px;
-    background-position: top center;
+joomla-core-loader .jbox p {
+  float: left;
+  margin: -20px 0 0 252px;
+  font: normal 1.25em/1em Helvetica, Arial, sans-serif;
+  color: #999;
+  transform: rotate(-45deg);
 }
-
-#joomla-loader-third .top-circle, #joomla-loader-third .bottom-rect .one, #joomla-loader-third .bottom-rect .two{
-    background-color: #5091CD;
+joomla-core-loader .jbox > span {
+  animation: jspinner 1s infinite;
 }
-#joomla-loader-third .top-arch{
-    background: rgba(255, 255, 255, 0); /* Old browsers */
-    background: -moz-radial-gradient(center, ellipse cover,  rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, 0) 27%, #5091CD 27%, #5091CD 70%, rgba(255, 255, 255, 0) 70%, rgba(255, 255, 255, 0) 100%); /* FF3.6-15 */
-    background: -webkit-radial-gradient(center, ellipse cover,  rgba(255, 255, 255, 0) 0%,rgba(255, 255, 255, 0) 27%,#5091CD 27%,#5091CD 70%,rgba(255, 255, 255, 0) 70%,rgba(255, 255, 255, 0) 100%); /* Chrome10-25,Safari5.1-6 */
-    background: radial-gradient(ellipse at center,  rgba(255, 255, 255, 0) 0%,rgba(255, 255, 255, 0) 27%,#5091CD 27%,#5091CD 70%,rgba(255, 255, 255, 0) 70%,rgba(255, 255, 255, 0) 100%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
-    filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='rgba(255, 255, 255, 0)', endColorstr='#5091CD',GradientType=1 ); /* IE6-9 fallback on horizontal gradient */
-    background-size: 200px 200px;
-    background-position: top center;
+joomla-core-loader .jbox .j2 {
+  animation-delay: -1.8s;
+}
+joomla-core-loader .jbox .j3 {
+  animation-delay: -1.6s;
+}
+joomla-core-loader .jbox .j4 {
+  animation-delay: -1.4s;
 }
 
-#joomla-loader-fourth .top-circle, #joomla-loader-fourth .bottom-rect .one, #joomla-loader-fourth .bottom-rect .two{
-    background-color: #7AC143;
-}
-#joomla-loader-fourth .top-arch{
-    background: rgba(255, 255, 255, 0); /* Old browsers */
-    background: -moz-radial-gradient(center, ellipse cover,  rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, 0) 27%, #7AC143 27%, #7AC143 70%, rgba(255, 255, 255, 0) 70%, rgba(255, 255, 255, 0) 100%); /* FF3.6-15 */
-    background: -webkit-radial-gradient(center, ellipse cover,  rgba(255, 255, 255, 0) 0%,rgba(255, 255, 255, 0) 27%,#7AC143 27%,#7AC143 70%,rgba(255, 255, 255, 0) 70%,rgba(255, 255, 255, 0) 100%); /* Chrome10-25,Safari5.1-6 */
-    background: radial-gradient(ellipse at center,  rgba(255, 255, 255, 0) 0%,rgba(255, 255, 255, 0) 27%,#7AC143 27%,#7AC143 70%,rgba(255, 255, 255, 0) 70%,rgba(255, 255, 255, 0) 100%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
-    filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='rgba(255, 255, 255, 0)', endColorstr='#7AC143',GradientType=1 ); /* IE6-9 fallback on horizontal gradient */
-    background-size: 200px 200px;
-    background-position: top center;
-}
-
-.vanish{
-    animation: vanish 2s infinite;
-}
-
-@keyframes vanish{
-    0%{
-        opacity: 1;
-    }
-    50%{
-        opacity: 0;
-    }
-    100%{
-        opacity: 1;
-    }
+@keyframes jspinner {
+  0%, 40%, 100% {
+    opacity: 0.1;
+  }
+  50% {
+    opacity: 1;
+  }
 }`;
 		this.styleEl = document.createElement('style');
 		this.styleEl.id = 'joomla-loader-css';
@@ -172,42 +139,7 @@ background-color: #fff;
 
 		this.element = document.createElement('div');
 		this.element.id = 'joomla-loader';
-		this.element.innerHTML = `<div id="joomla-loader-first">
-            <div class="top-circle vanish"></div>
-            <div class="top-arch"></div>
-            <div class="bottom-rect">
-                <div class="one"></div>
-                <div class="two vanish"></div>
-            </div>
-        </div>
-
-        <div id="joomla-loader-second">
-            <div class="top-circle vanish"></div>
-            <div class="top-arch vanish"></div>
-            <div class="bottom-rect">
-                <div class="one vanish"></div>
-                <div class="two"></div>
-                <div class="three vanish"></div>
-            </div>
-        </div>
-
-        <div id="joomla-loader-third">
-            <div class="top-circle vanish"></div>
-            <div class="top-arch vanish"></div>
-            <div class="bottom-rect">
-                <div class="one vanish"></div>
-                <div class="two"></div>
-            </div>
-        </div>
-
-        <div id="joomla-loader-fourth">
-            <div class="top-circle vanish"></div>
-            <div class="top-arch"></div>
-            <div class="bottom-rect">
-                <div class="one"></div>
-                <div class="two"></div>
-            </div>
-        </div>`;
+		this.element.innerHTML = `<div class="jbox"><span class="j1"></span><span class="j2"></span><span class="j3"></span><span class="j4"></span><p>&reg;</p></div>`;
 
 		if (!document.head.querySelector('#joomla-loader-css')) {
 			document.head.appendChild(this.styleEl)
