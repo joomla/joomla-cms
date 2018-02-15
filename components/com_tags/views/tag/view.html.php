3db1d805a8cdd2bc22232956409efe77982a4137
Fiona Coulter <fcoulter@users.noreply.github.com>
2018-02-15 13:52:22 +0000

    removed extraneous space

--- components/com_tags/views/tag/view.html.php
+++ components/com_tags/views/tag/view.html.php
@@ -122,7 +122,7 @@ class TagsViewTag extends JViewLegacy
 		{
 			foreach ($items as $row)
 			{
-				  $row->core_images = json_encode(array('image_intro' => $row->core_params->get('image',''), 'image_intro_alt' => $row->core_params->get('image_alt', '')));
+				$row->core_images = json_encode(array('image_intro' => $row->core_params->get('image',''), 'image_intro_alt' => $row->core_params->get('image_alt', '')));
 			}
 		}
 
