796df87a9cc9d693c61d3c92e46ac9867580feac
David Neukirchen <david.neukirchen@gmail.com>
2018-02-20 22:52:04 +0100

    transform admin-multilangstatus.js to es6

--- media/mod_multilangstatus/js/admin-multilangstatus.es6.js
+++ media/mod_multilangstatus/js/admin-multilangstatus.es6.js
@@ -1,9 +1,4 @@
 /**
-* PLEASE DO NOT MODIFY THIS FILE. WORK ON THE ES6 VERSION.
-* OTHERWISE YOUR CHANGES WILL BE REPLACED ON THE NEXT BUILD.
-**/
-
-/**
  * @copyright  Copyright (C) 2005 - 2017 Open Source Matters, Inc. All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
@@ -11,13 +6,13 @@
 (function () {
   'use strict';
 
-  document.addEventListener('DOMContentLoaded', function () {
+  document.addEventListener('DOMContentLoaded', () => {
     // We need to use JS to move the modal before the closing body tag to avoid stacking issues
-    var multilangueModal = document.getElementById('multiLangModal');
+    const multilangueModal = document.getElementById('multiLangModal');
 
     if (multilangueModal) {
       // Clone the modal element
-      var clone = multilangueModal.cloneNode(true);
+      const clone = multilangueModal.cloneNode(true);
 
       // Remove the original modal element
       multilangueModal.parentNode.removeChild(multilangueModal);
@@ -26,4 +21,4 @@
       document.body.appendChild(clone);
     }
   });
-})();
+}());
