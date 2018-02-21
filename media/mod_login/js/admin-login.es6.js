4d9f6d7d50ac50f82dcfcdfad2d82d2206bd7d48
Lodder <lodder15@hotmail.co.uk>
2018-02-21 11:38:25 +0000

    Update admin-login.es6.js

--- media/mod_login/js/admin-login.es6.js
+++ media/mod_login/js/admin-login.es6.js
@@ -3,7 +3,7 @@
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 
-((() => {
+(() => {
   'use strict';
 
   const btn = document.getElementById('btn-login-submit');
@@ -24,4 +24,4 @@
       document.getElementById('mod-login-username').focus();
     }
   }
-}))();
+})();
