e0b90e5bf2783907d41c3aa6241ec021afa5b144
Aleksey Vaganov <a.vaganov@webdivision.ru>
2018-02-24 00:09:06 +0300

    The commit 31617a8a reverted

--- libraries/joomla/database/query.php
+++ libraries/joomla/database/query.php
@@ -703,7 +703,7 @@ abstract class JDatabaseQuery
 	public function delete($table = null)
 	{
 		$this->type = 'delete';
-		$this->delete = new JDatabaseQueryElement('DELETE', array($table));
+		$this->delete = new JDatabaseQueryElement('DELETE', null);
 
 		if (!empty($table))
 		{
