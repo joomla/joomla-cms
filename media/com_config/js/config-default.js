e7485921db991a598beb4cfc8c226a3003f6640c
David Neukirchen <david.neukirchen@gmail.com>
2018-02-23 19:09:04 +0100

    Remove inline scripts

---
+++ media/com_config/js/config-default.js
@@ -0,0 +1,42 @@
+/**
+ * @copyright   Copyright (C) 2005 - 2017 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+Joomla = window.Joomla || {};
+
+(function (document, submitForm) {
+  'use strict';
+
+  // Selectors used by this script
+  var buttonDataSelector = 'data-submit-task';
+  var formId = 'application-form';
+
+  /**
+   * Submit the task
+   * @param task
+   */
+  var submitTask = function (task) {
+    if (task == 'config.cancel' || document.formvalidator.isValid(document.getElementById(formId))) {
+      submitForm(task, document.getElementById('application-form'));
+    }
+  };
+
+  /**
+   * Register events
+   */
+  var registerEvents = function () {
+    var buttons = [].slice.call(document.querySelectorAll('[' + buttonDataSelector + ']'));
+    buttons.forEach(function (button) {
+      button.addEventListener('click', function (e) {
+        e.preventDefault();
+        var task = e.target.getAttribute(buttonDataSelector);
+        submitTask(task);
+      });
+    });
+  };
+
+  document.addEventListener('DOMContentLoaded', function () {
+    registerEvents();
+  });
+
+})(document, Joomla.submitform);
