ea33f5e1563b4df496afa30c354917948ce98efb
Michael Babker <michael.babker@gmail.com>
2018-02-25 09:53:43 -0600

    Make a session factory, move handler creation into it

--- libraries/src/Service/Provider/Session.php
+++ libraries/src/Service/Provider/Session.php
@@ -11,7 +11,6 @@ namespace Joomla\CMS\Service\Provider;
 
 defined('JPATH_PLATFORM') or die;
 
-use InvalidArgumentException;
 use Joomla\CMS\Application\AdministratorApplication;
 use Joomla\CMS\Application\ApplicationHelper;
 use Joomla\CMS\Application\CMSApplicationInterface;
@@ -19,23 +18,18 @@ use Joomla\CMS\Application\ConsoleApplication;
 use Joomla\CMS\Application\SiteApplication;
 use Joomla\CMS\Factory;
 use Joomla\CMS\Installation\Application\InstallationApplication;
+use Joomla\CMS\Session\SessionFactory;
 use Joomla\CMS\Session\Storage\JoomlaStorage;
-use Joomla\Database\DatabaseDriver;
 use Joomla\DI\Container;
 use Joomla\DI\ServiceProviderInterface;
 use Joomla\Event\DispatcherInterface;
 use Joomla\Registry\Registry;
-use Joomla\Session\Handler;
-use Joomla\Session\HandlerInterface;
 use Joomla\Session\SessionEvents;
 use Joomla\Session\SessionInterface;
 use Joomla\Session\Storage\RuntimeStorage;
 use Joomla\Session\StorageInterface;
 use Joomla\Session\Validator\AddressValidator;
 use Joomla\Session\Validator\ForwardedValidator;
-use Memcached;
-use Redis;
-use RuntimeException;
 
 /**
  * Service provider for the application's session dependency
@@ -81,7 +75,7 @@ class Session implements ServiceProviderInterface
 				}
 
 				return $this->buildSession(
-					new JoomlaStorage($app->input, $this->buildSessionHandler($container, $config)),
+					new JoomlaStorage($app->input, $container->get('session.factory')->createSessionHandler()),
 					$app,
 					$container->get(DispatcherInterface::class),
 					$options
@@ -111,7 +105,7 @@ class Session implements ServiceProviderInterface
 				];
 
 				return $this->buildSession(
-					new JoomlaStorage($app->input, $this->buildSessionHandler($container, $config)),
+					new JoomlaStorage($app->input, $container->get('session.factory')->createSessionHandler()),
 					$app,
 					$container->get(DispatcherInterface::class),
 					$options
@@ -146,7 +140,7 @@ class Session implements ServiceProviderInterface
 				}
 
 				return $this->buildSession(
-					new JoomlaStorage($app->input, $this->buildSessionHandler($container, $config)),
+					new JoomlaStorage($app->input, $container->get('session.factory')->createSessionHandler()),
 					$app,
 					$container->get(DispatcherInterface::class),
 					$options
@@ -185,6 +179,19 @@ class Session implements ServiceProviderInterface
 			},
 			true
 		);
+
+		$container->alias(SessionFactory::class, 'session.factory')
+			->share(
+				'session.factory',
+				function (Container $container)
+				{
+					$factory = new SessionFactory;
+					$factory->setContainer($container);
+
+					return $factory;
+				},
+				true
+			);
 	}
 
 	/**
@@ -218,116 +225,5 @@ class Session implements ServiceProviderInterface
 		$session->addValidator(new ForwardedValidator($input, $session));
 
 		return $session;
-	}
-
-	/**
-	 * Build the session's handler
-	 *
-	 * @param   Container  $container  The DI container.
-	 * @param   Registry   $config     The global configuration object.
-	 *
-	 * @return  HandlerInterface
-	 *
-	 * @since   4.0
-	 */
-	private function buildSessionHandler(Container $container, Registry $config): HandlerInterface
-	{
-		$handlerType = $config->get('session_handler', 'filesystem');
-
-		switch ($handlerType)
-		{
-			case 'apcu':
-				if (!Handler\ApcuHandler::isSupported())
-				{
-					throw new RuntimeException('APCu is not supported on this system.');
-				}
-
-				return new Handler\ApcuHandler;
-
-			case 'database':
-				return new Handler\DatabaseHandler($container->get(DatabaseDriver::class));
-
-			case 'filesystem':
-			case 'none':
-				// Try to use a custom configured path, fall back to the path in the PHP runtime configuration
-				$path = $config->get('session_filesystem_path', ini_get('session.save_path'));
-
-				// If we still have no path, as a last resort fall back to the system's temporary directory
-				if (empty($path))
-				{
-					$path = sys_get_temp_dir();
-				}
-
-				return new Handler\FilesystemHandler($path);
-
-			case 'memcached':
-				if (!Handler\MemcachedHandler::isSupported())
-				{
-					throw new RuntimeException('Memcached is not supported on this system.');
-				}
-
-				$host = $config->get('session_memcached_server_host', 'localhost');
-				$port = $config->get('session_memcached_server_port', 11211);
-
-				$memcached = new Memcached($config->get('session_memcached_server_id', 'joomla_cms'));
-				$memcached->addServer($host, $port);
-
-				ini_set('session.save_path', "$host:$port");
-				ini_set('session.save_handler', 'memcached');
-
-				return new Handler\MemcachedHandler($memcached, ['ttl' => $lifetime]);
-
-			case 'redis':
-				if (!Handler\RedisHandler::isSupported())
-				{
-					throw new RuntimeException('Redis is not supported on this system.');
-				}
-
-				$redis = new Redis;
-				$host  = $config->get('session_redis_server_host', '127.0.0.1');
-
-				// Use default port if connecting over a socket whatever the config value
-				$port = $host[0] === '/' ? $config->get('session_redis_server_port', 6379) : 6379;
-
-				if ($config->get('session_redis_persist', true))
-				{
-					$redis->pconnect(
-						$host,
-						$port
-					);
-				}
-				else
-				{
-					$redis->connect(
-						$host,
-						$port
-					);
-				}
-
-				if (!empty($config->get('session_redis_server_auth', '')))
-				{
-					$redis->auth($config->get('session_redis_server_auth', null));
-				}
-
-				$db = (int) $config->get('session_redis_server_db', 0);
-
-				if ($db !== 0)
-				{
-					$redis->select($db);
-				}
-
-				return new Handler\RedisHandler($redis, ['ttl' => $lifetime]);
-
-			case 'wincache':
-				if (!Handler\WincacheHandler::isSupported())
-				{
-					throw new RuntimeException('Wincache is not supported on this system.');
-				}
-
-				return new Handler\WincacheHandler;
-
-			default:
-				throw new InvalidArgumentException(sprintf('The "%s" session handler is not recognised.', $handlerType));
-		}
 	}
 }
