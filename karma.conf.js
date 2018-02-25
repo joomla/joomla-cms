c6eb9e0e622c641aa52e40e7219b05a6bc143b82
dgt41 <d.grammatiko@gmail.com>
2018-02-25 12:37:59 +0100

    js tests

--- karma.conf.js
+++ karma.conf.js
@@ -30,7 +30,7 @@ module.exports = function (config) {
 			{pattern: 'tests/javascript/**/spec.js', included: false},
 			{pattern: 'tests/javascript/**/spec-setup.js', included: false},
 			{pattern: 'media/system/webcomponents/js/*.js', included: false},
-			{pattern: 'media/polyfills/js/*.js', included: false},
+			{pattern: 'media/vendor/webcomponentsjs/js/*.js', included: false},
 			{pattern: 'images/*.png', included: false},
 
 			'tests/javascript/test-main.js'
