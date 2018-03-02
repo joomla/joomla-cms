f6639a1b9bddf5547c789d5b22ffa3b8713ebfa1
Allon Moritz <allon.moritz@digital-peak.com>
2018-03-02 09:59:54 +0100

    Make drone happy

--- libraries/src/Component/ComponentHelper.php
+++ libraries/src/Component/ComponentHelper.php
@@ -157,7 +157,7 @@ class ComponentHelper
 			{
 				// Maximum HTML filtering.
 			}
-			elseif ($filterType === 'NONE')
+			else if ($filterType === 'NONE')
 			{
 				// No HTML filtering.
 				$unfiltered = true;
@@ -199,7 +199,7 @@ class ComponentHelper
 					$blackListTags       = array_merge($blackListTags, $tempTags);
 					$blackListAttributes = array_merge($blackListAttributes, $tempAttributes);
 				}
-				elseif ($filterType === 'CBL')
+				else if ($filterType === 'CBL')
 				{
 					// Only set to true if Tags or Attributes were added
 					if ($tempTags || $tempAttributes)
@@ -209,7 +209,7 @@ class ComponentHelper
 						$customListAttributes = array_merge($customListAttributes, $tempAttributes);
 					}
 				}
-				elseif ($filterType === 'WL')
+				else if ($filterType === 'WL')
 				{
 					$whiteList           = true;
 					$whiteListTags       = array_merge($whiteListTags, $tempTags);
