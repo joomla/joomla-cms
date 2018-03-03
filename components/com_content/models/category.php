efc6c3ec5c0de8bd658e83f3de8ed8a834c10bac
Arkadiy Sedelnikov <a.sedelnikov@gmail.com>
2018-03-03 18:26:56 +0700

    Add states to filter content by article ID in category model

--- components/com_content/models/category.php
+++ components/com_content/models/category.php
@@ -161,6 +161,12 @@ class ContentModelCategory extends JModelList
 		$value = $this->getUserStateFromRequest('com_content.category.filter.' . $itemid . '.tag', 'filter_tag', 0, 'int', false);
 		$this->setState('filter.tag', $value);
 
+		$value = $this->getUserStateFromRequest('com_content.category.list.' . $itemid . 'filter.article_id_include', 'filter_article_id_include', false, 'boolen');
+		$this->setState('filter.article_id.include', $value);
+
+		$value = $this->getUserStateFromRequest('com_content.category.list.' . $itemid . 'filter.article_id', 'filter_article_id', null, 'array');
+		$this->setState('filter.article_id', $value);
+
 		// Optional filter text
 		$search = $app->getUserStateFromRequest('com_content.category.list.' . $itemid . '.filter-search', 'filter-search', '', 'string');
 		$this->setState('list.filter', $search);
@@ -242,6 +248,9 @@ class ContentModelCategory extends JModelList
 			$model->setState('list.direction', $this->getState('list.direction'));
 			$model->setState('list.filter', $this->getState('list.filter'));
 			$model->setState('filter.tag', $this->getState('filter.tag'));
+
+			$model->setState('filter.article_id.include', $this->getState('filter.article_id.include'));
+			$model->setState('filter.article_id', $this->getState('filter.article_id'));
 
 			// Filter.subcategories indicates whether to include articles from subcategories in the list or blog
 			$model->setState('filter.subcategories', $this->getState('filter.subcategories'));
