8a51f2326f9a0956f1b5355d71e9b75357fa5953
Izhar Aazmi <izharaazmi@gmail.com>
2018-03-09 07:51:36 +0530

    Jroute between sites (#16879)
    * allow to force site client when we are in admin
    
    * allow from site to admin too
    
    * first try to make language filter router work on admin too when called via JRoute
    
    * revert
    
    * revert 2
    
    * make language filter pluign work with JRoute from admin
    
    * cs
    
    * cs 2
    
    * use app
    
    * solve php notices
    
    * Few tweaks for the PR
    
    * Suggestions from @mbabker, and few other improvements.
    
    * Moved active client detection logic to JRoute::_() and disallow null client value in JRoute::link()
    
    * Expand mocking in JHtmlBehaviorTest
    
    * Mocking in JHtmlIconsTest
    
    * Fix reset of router property
    
    * Mocking in JPaginationTest
    
    * Mocking in JDocumentOpensearchTest
    
    * Mocking in JDocumentRendererHtmlModulesTest
    
    * Mocking in JFormFieldRulesTest
    
    * Test fixes
    
    * Moved SEF mode detection to CMSApplication for it being called from all application instances.
    
    * Remove magic methods to force using of JRoute::link() directly

--- libraries/src/Router/AdministratorRouter.php
+++ libraries/src/Router/AdministratorRouter.php
@@ -10,6 +10,8 @@ namespace Joomla\CMS\Router;
 
 defined('JPATH_PLATFORM') or die;
 
+use Joomla\CMS\Uri\Uri;
+
 /**
  * Class to create and parse routes
  *
@@ -20,7 +22,7 @@ class AdministratorRouter extends Router
 	/**
 	 * Function to convert a route to an internal URI.
 	 *
-	 * @param   \JUri  &$uri  The uri.
+	 * @param   Uri  &$uri  The uri.
 	 *
 	 * @return  array
 	 *
@@ -36,7 +38,7 @@ class AdministratorRouter extends Router
 	 *
 	 * @param   string  $url  The internal URL
 	 *
-	 * @return  string  The absolute search engine friendly URL
+	 * @return  Uri  The absolute search engine friendly URL
 	 *
 	 * @since   1.5
 	 */
@@ -49,7 +51,7 @@ class AdministratorRouter extends Router
 		$route = $uri->getPath();
 
 		// Add basepath to the uri
-		$uri->setPath(\JUri::base(true) . '/' . $route);
+		$uri->setPath(Uri::root(true) . '/' . basename(JPATH_ADMINISTRATOR) . '/' . $route);
 
 		return $uri;
 	}
