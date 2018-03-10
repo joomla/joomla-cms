a823fc7b2a750073fc3e7f8015502b6f7a8c3513
Anurag <30978328+Anu1601CS@users.noreply.github.com>
2018-03-10 21:40:33 +0530

    Transform com_content to ES6 (#97)
    * com_content 3
    
    * make simple
    
    * TODO
    
    * one left

--- media/com_content/js/admin-articles-modal.es6.js
+++ media/com_content/js/admin-articles-modal.es6.js
@@ -1,13 +1,8 @@
 /**
-* PLEASE DO NOT MODIFY THIS FILE. WORK ON THE ES6 VERSION.
-* OTHERWISE YOUR CHANGES WILL BE REPLACED ON THE NEXT BUILD.
-**/
-
-/**
  * @copyright  Copyright (C) 2005 - 2017 Open Source Matters, Inc. All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
-(function () {
+(() => {
   'use strict';
 
   /**
@@ -16,39 +11,36 @@
     * jSelectArticle creates the link tag, sends it to the editor,
     * and closes the select frame.
     * */
-
-  window.jSelectArticle = function (id, title, catid, object, link, lang) {
-    var hreflang = '';
+  window.jSelectArticle = (id, title, catid, object, link, lang) => {
+    let hreflang = '';
     if (!Joomla.getOptions('xtd-articles')) {
-      // Something went wrong!
-      // @TODO Close the modal
+    // Something went wrong!
+    // @TODO Close the modal
       return false;
     }
 
-    var _Joomla$getOptions = Joomla.getOptions('xtd-articles'),
-        editor = _Joomla$getOptions.editor;
+    const { editor } = Joomla.getOptions('xtd-articles');
 
     if (lang !== '') {
-      hreflang = 'hreflang="' + lang + '"';
+      hreflang = `hreflang="${lang}"`;
     }
 
-    var tag = '<a ' + hreflang + ' href="' + link + '">' + title + '</a>';
+    const tag = `<a ${hreflang} href="${link}">${title}</a>`;
     window.parent.Joomla.editors.instances[editor].replaceSelection(tag);
     // @TODO Close the modal
     return true;
   };
 
-  document.addEventListener('DOMContentLoaded', function () {
-    // Get the elements
-    var elements = document.querySelectorAll('.select-link');
+  document.addEventListener('DOMContentLoaded', () => {
+  // Get the elements
+    const elements = document.querySelectorAll('.select-link');
 
-    for (var i = 0, l = elements.length; l > i; i += 1) {
-      // Listen for click event
-      elements[i].addEventListener('click', function (event) {
+    for (let i = 0, l = elements.length; l > i; i += 1) {
+    // Listen for click event
+      elements[i].addEventListener('click', (event) => {
         event.preventDefault();
-        var target = event.target;
-
-        var functionName = target.getAttribute('data-function');
+        const { target } = event;
+        const functionName = target.getAttribute('data-function');
 
         if (functionName === 'jSelectArticle') {
           // Used in xtd_contacts
