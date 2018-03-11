2f4dd8ec768d3897bb4c5b679e1c6dc2f69d9427
Anurag <30978328+Anu1601CS@users.noreply.github.com>
2018-03-11 21:51:31 +0530

    com_content 1 (#95)

--- media/com_content/js/admin-article-pagebreak.es6.js
+++ media/com_content/js/admin-article-pagebreak.es6.js
@@ -1,19 +1,14 @@
 /**
-* PLEASE DO NOT MODIFY THIS FILE. WORK ON THE ES6 VERSION.
-* OTHERWISE YOUR CHANGES WILL BE REPLACED ON THE NEXT BUILD.
-**/
-
-/**
  * @copyright  Copyright (C) 2005 - 2017 Open Source Matters, Inc. All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 
-(function () {
+(() => {
   'use strict';
 
-  window.insertPagebreak = function (editor) {
+  window.insertPagebreak = (editor) => {
     // Get the pagebreak title
-    var title = document.getElementById('title').value;
+    let title = document.getElementById('title').value;
 
     if (!window.parent.Joomla.getOptions('xtd-pagebreak')) {
       // Something went wrong!
@@ -22,10 +17,10 @@
     }
 
     // Get the pagebreak toc alias -- not inserting for now don't know which attribute to use..
-    var alt = document.getElementById('alt').value;
-    title = title !== '' ? 'title="' + title + '"' : '';
-    alt = alt !== '' ? 'alt="' + alt + '"' : '';
-    var tag = '<hr class="system-pagebreak" ' + title + ' ' + alt + '>';
+    let alt = document.getElementById('alt').value;
+    title = (title !== '') ? `title="${title}"` : '';
+    alt = (alt !== '') ? `alt="${alt}"` : '';
+    const tag = `<hr class="system-pagebreak" ${title} ${alt}>`;
     window.parent.Joomla.editors.instances[editor].replaceSelection(tag);
     window.parent.jModalClose();
     return false;
