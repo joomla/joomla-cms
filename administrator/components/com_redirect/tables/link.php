joomla/joomla-cms

commit 1c9eaad987a6b08c890374be26a775cb97a14a47

Com_redirect: Differentiating utf8 old_url (#19734)

administrator/components/com_redirect/tables/link.php
@@ -86,7 +86,7 @@ class RedirectTableLink extends JTable
 		$query = $db->getQuery(true)
 			->select($db->quoteName('id'))
 			->from('#__redirect_links')
-			->where($db->quoteName('old_url') . ' = ' . $db->quote($this->old_url));
+			->where($db->quoteName('old_url') . ' = ' . $db->quote(rawurlencode($this->old_url)));
 		$db->setQuery($query);
 
 		$xid = (int) $db->loadResult();

commit 221d84860c5147af0aa1e7c2c78dc830b477e3b2

Fix for duplicate url check bug introduced by #19734 and support utf8… (#19950)
* Fix for duplicate url check bug introduced by #19734 and support utf8 on old_urls.
Couldn't find a solution to handle this within mysql. So a simple foreach handles it perfectly.

* Update link.php

administrator/components/com_redirect/tables/link.php
@@ -85,17 +85,20 @@ class RedirectTableLink extends JTable
 		// Check for existing name
 		$query = $db->getQuery(true)
 			->select($db->quoteName('id'))
+			->select($db->quoteName('old_url'))
 			->from('#__redirect_links')
-			->where($db->quoteName('old_url') . ' = ' . $db->quote(rawurlencode($this->old_url)));
+			->where($db->quoteName('old_url') . ' = ' . $db->quote($this->old_url));
 		$db->setQuery($query);
+		$urls = $db->loadAssocList();
 
-		$xid = (int) $db->loadResult();
-
-		if ($xid && $xid != (int) $this->id)
+		foreach ($urls as $url)
 		{
-			$this->setError(JText::_('COM_REDIRECT_ERROR_DUPLICATE_OLD_URL'));
+			if ($url['old_url'] === $this->old_url && (int) $url['id'] != (int) $this->id)
+			{
+				$this->setError(JText::_('COM_REDIRECT_ERROR_DUPLICATE_OLD_URL'));
 
-			return false;
+				return false;
+			}
 		}
 
 		return true;