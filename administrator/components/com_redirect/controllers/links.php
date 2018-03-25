b604311c54eded74cb336098d56145947fe583aa
tonypartridge <6GW7iqJ78B>
2018-03-25 00:15:27 +0000

    Basic check to make sure the bulk import seperator is being used. Added Import State function as to how the urls should be imported, enabled or disabled.

--- administrator/components/com_redirect/controllers/links.php
+++ administrator/components/com_redirect/controllers/links.php
@@ -133,7 +133,15 @@ class RedirectControllerLinks extends JControllerAdmin
 			if (!empty($batch_urls_line))
 			{
 				$params = JComponentHelper::getParams('com_redirect');
-				$batch_urls[] = array_map('trim', explode($params->get('separator', '|'), $batch_urls_line));
+				$seperator  = $params->get('separator', '|');
+
+				// Basic check to make sure the correct separator is being used
+				if (!\Joomla\String\StringHelper::strpos($batch_urls_line, $seperator)) {
+					$this->setMessage(JText::sprintf('COM_REDIRECT_NO_SEPERATOR_FOUND', $seperator), 'error');
+					$this->setRedirect('index.php?option=com_redirect&view=links');
+					return false;
+				}
+				$batch_urls[] = array_map('trim', explode($seperator, $batch_urls_line));
 			}
 		}
 
