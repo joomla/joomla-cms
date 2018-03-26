7b8fd33f765f9553e4c7529a900ec9435eaa7f5f
Baleshevic Srdjan <37658865+BaleshSrle@users.noreply.github.com>
2018-03-26 08:49:32 +0000

    Update default.php

--- modules/mod_wrapper/tmpl/default.php
+++ modules/mod_wrapper/tmpl/default.php
@@ -15,7 +15,10 @@ JHtml::_('script', 'com_wrapper/iframe-height.min.js', array('version' => 'auto'
 	id="blockrandom-<?php echo $id; ?>"
 	name="<?php echo $target; ?>"
 	src="<?php echo $url; ?>"
-	style="width: <?php echo $width; ?>; height: <?php echo $height; ?>; overflow: <?php echo $overflow; ?>; border: <?php echo $border; ?>;"
+	style="width: <?php echo $width; ?>;
+	       height: <?php echo $height; ?>;
+	       overflow: <?php echo $overflow; ?>;
+	       border: <?php echo $border; ?>;"
 	title="<?php echo $ititle; ?>"
 	class="wrapper<?php echo $moduleclass_sfx; ?>" >
 	<?php echo JText::_('MOD_WRAPPER_NO_IFRAMES'); ?>
