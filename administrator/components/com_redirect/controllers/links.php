b2cd2e183f1a58c18d3777251ad3d9514194c7be
Tony Partridge <admin@xtech.im>
2018-03-28 15:47:29 +0100

    Update links.php

--- administrator/components/com_redirect/controllers/links.php
+++ administrator/components/com_redirect/controllers/links.php
@@ -133,17 +133,17 @@ class RedirectControllerLinks extends JControllerAdmin
 			if (!empty($batch_urls_line))
 			{
 				$params = JComponentHelper::getParams('com_redirect');
-				$separator  = $params->get('separator', '|');
+				$separator = $params->get('separator', '|');
 
 				// Basic check to make sure the correct separator is being used
 				if (!\Joomla\String\StringHelper::strpos($batch_urls_line, $separator))
 				{
 					$this->setMessage(JText::sprintf('COM_REDIRECT_NO_SEPARATOR_FOUND', $separator), 'error');
 					$this->setRedirect('index.php?option=com_redirect&view=links');
-					
+
 					return false;
 				}
-				
+
 				$batch_urls[] = array_map('trim', explode($separator, $batch_urls_line));
 			}
 		}
