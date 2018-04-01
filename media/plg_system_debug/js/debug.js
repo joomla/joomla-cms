df1ba8003500abfcfe2c9c938287eb2557e27a5d
Kasun Vithanage <alankasun@gmail.com>
2018-04-01 20:33:44 +0530

    media/plg_system_debug/js/debug.js (#113)
    * es6 converted
    
    * es6 compiled

--- media/plg_system_debug/js/debug.js
+++ media/plg_system_debug/js/debug.js
@@ -1,4 +1,9 @@
 /**
+* PLEASE DO NOT MODIFY THIS FILE. WORK ON THE ES6 VERSION.
+* OTHERWISE YOUR CHANGES WILL BE REPLACED ON THE NEXT BUILD.
+**/
+
+/**
  * @copyright   Copyright (C) 2005 - 2017 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
@@ -7,6 +12,7 @@
   'use strict';
 
   // Selectors used by this script
+
   var debugSectionTogglerSelector = '.dbg-header';
   var toggleTargetAttribute = 'data-debug-toggle';
 
@@ -14,17 +20,17 @@
    * Toggle an element by id
    * @param id
    */
-  var toggle = function (id) {
+  var toggle = function toggle(id) {
     var element = document.getElementById(id);
     if (element) {
-      element.style.display = (element.style.display === 'block') ? 'none' : 'block';
+      element.style.display = element.style.display === 'block' ? 'none' : 'block';
     }
   };
 
   /**
    * Register events
    */
-  var registerEvents = function () {
+  var registerEvents = function registerEvents() {
     var sectionTogglers = [].slice.call(document.querySelectorAll(debugSectionTogglerSelector));
     sectionTogglers.forEach(function (toggler) {
       toggler.addEventListener('click', function (event) {
@@ -37,5 +43,4 @@
   document.addEventListener('DOMContentLoaded', function () {
     registerEvents();
   });
-
-}(document));
+})(document);
