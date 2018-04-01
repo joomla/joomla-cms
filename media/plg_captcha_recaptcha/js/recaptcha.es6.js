25642a58983d162e0b78295b26732900544a5861
Kasun Vithanage <alankasun@gmail.com>
2018-04-01 21:19:42 +0530

    media/plg_captcha_recaptcha/js/recaptcha.js (#112)
    * converted to es6
    
    * updated code to support IE
    
    * compile es6

--- media/plg_captcha_recaptcha/js/recaptcha.es6.js
+++ media/plg_captcha_recaptcha/js/recaptcha.es6.js
@@ -1,24 +1,19 @@
 /**
-* PLEASE DO NOT MODIFY THIS FILE. WORK ON THE ES6 VERSION.
-* OTHERWISE YOUR CHANGES WILL BE REPLACED ON THE NEXT BUILD.
-**/
-
-/**
  * @package     Joomla.JavaScript
  * @copyright   Copyright (C) 2005 - 2017 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
-window.JoomlaInitReCaptcha2 = function () {
+window.JoomlaInitReCaptcha2 = () => {
   'use strict';
 
-  var itemNodes = document.getElementsByClassName('g-recaptcha');
-  var items = [].slice.call(itemNodes);
-  items.forEach(function (item) {
-    var options = item.dataset ? item.dataset : {
+  const itemNodes = document.getElementsByClassName('g-recaptcha');
+  const items = [].slice.call(itemNodes);
+  items.forEach((item) => {
+    const options = item.dataset ? item.dataset : {
       sitekey: item.getAttribute('data-sitekey'),
       theme: item.getAttribute('data-theme'),
-      size: item.getAttribute('data-size')
+      size: item.getAttribute('data-size'),
     };
 
     /* global grecaptcha */
