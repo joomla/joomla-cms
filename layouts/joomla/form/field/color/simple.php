8a76be81f6911a0c3cc39f7df97485481a28dcd1
Allon Moritz <allon.moritz@digital-peak.com>
2018-04-03 14:00:55 +0200

    Update simple.php

--- layouts/joomla/form/field/color/simple.php
+++ layouts/joomla/form/field/color/simple.php
@@ -53,7 +53,7 @@ $class    = ' class="custom-select ' . trim($class) . '"';
 $disabled = $disabled ? ' disabled' : '';
 $readonly = $readonly ? ' readonly' : '';
 
-HTMLHelper::_('webcomponent', ['joomla-field-simple-color' => 'system/webcomponents/joomla-field-simple-color.min.js'], ['version' => 'auto', 'relative' => true]);
+HTMLHelper::_('webcomponent', 'system/webcomponents/joomla-field-simple-color.min.js', ['version' => 'auto', 'relative' => true]);
 ?>
 <joomla-field-simple-color text-select="<?php echo Text::_('JFIELD_COLOR_SELECT'); ?>" text-color="<?php echo Text::_('JFIELD_COLOR_VALUE'); ?>" text-close="<?php echo Text::_('JLIB_HTML_BEHAVIOR_CLOSE'); ?>" text-transparent="<?php echo Text::_('JFIELD_COLOR_TRANSPARENT'); ?>">
 	<select name="<?php echo $name; ?>" id="<?php echo $id; ?>"<?php
