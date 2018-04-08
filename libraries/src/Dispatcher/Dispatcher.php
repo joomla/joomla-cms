7e928969b15eda3263a67931ca7c7e44154f7899
Allon Moritz <allon.moritz@digital-peak.com>
2018-04-08 15:57:06 +0200

    Get the option form the input

--- libraries/src/Dispatcher/Dispatcher.php
+++ libraries/src/Dispatcher/Dispatcher.php
@@ -81,7 +81,7 @@ class Dispatcher implements DispatcherInterface
 		// If option is not provided, detect it from dispatcher class name, ie ContentDispatcher
 		if (empty($this->option))
 		{
-			$this->option = $app->scope;
+			$this->option = $input->get('option');
 		}
 
 		$this->loadLanguage();
