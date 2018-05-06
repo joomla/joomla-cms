ac775ec5c43f2b3d93f1698425ca2b14b66d49d4
Nicola Galgano <optimus4joomla@gmail.com>
2018-05-06 10:28:43 +0200

    plugin params

--- administrator/components/com_admin/sql/updates/mysql/3.9.0-2018-05-05.sql
+++ administrator/components/com_admin/sql/updates/mysql/3.9.0-2018-05-05.sql
@@ -1,6 +1,6 @@
 INSERT INTO `#__extensions` (`extension_id`, `name`, `type`, `element`, `folder`, `client_id`, `enabled`, `access`, `protected`, `manifest_cache`, `params`, `custom_data`, `system_data`, `checked_out`, `checked_out_time`, `ordering`, `state`) VALUES
 (36, 0, 'com_userlogs', 'component', 'com_userlogs', '', 1, 1, 1, 1, '', '', '', '', 0, '0000-00-00 00:00:00', 0, 0),
-(481, 0, 'plg_system_userlogs', 'plugin', 'confirmconsent', 'userlogs', 0, 0, 1, 0, '', '', '', '', 0, '0000-00-00 00:00:00', 0, 0);
+(481, 0, 'plg_system_userlogs', 'plugin', 'confirmconsent', 'userlogs', 0, 0, 1, 0, '', '{"logDeletePeriod":"0","ip_logging":"1","loggable_extensions":["com_banners","com_cache","com_categories","com_config","com_contact","com_content","com_installer","com_media","com_menus","com_messages","com_modules","com_newsfeeds","com_plugins","com_redirect","com_tags","com_templates","com_users"]}', '', '', 0, '0000-00-00 00:00:00', 0, 0);
 
 
 --
