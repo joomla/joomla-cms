8764951e4ab8fa506150d5c8ef882b428626d300
Nicola Galgano <optimus4joomla@gmail.com>
2018-05-06 07:44:17 +0200

    cs

--- plugins/system/userlogs/layouts/logstable.php
+++ plugins/system/userlogs/layouts/logstable.php
@@ -1,13 +1,14 @@
 <?php
 /**
- * @package	 Joomla.Administrator
+ * @package	Joomla.Administrator
  * @subpackage  com_userlogs
  *
- * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
- * @license	 GNU General Public License version 2 or later; see LICENSE.txt
+ * @copyright   Copyright (C) 2005 - 2018 Open Source Matters, Inc. All rights reserved.
+ * @license	GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 defined('_JEXEC') or die;
+
 $lang = JFactory::getLanguage();
 $source = JPATH_ADMINISTRATOR . '/components/' . 'com_userlogs';
 
