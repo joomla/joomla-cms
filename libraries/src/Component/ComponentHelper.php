94a6ef3dabc7105a1172ff52d15398e80e1c1318
Allon Moritz <allon.moritz@digital-peak.com>
2018-05-08 17:02:26 +0200

    Check if class is from the dispatcher

--- libraries/src/Component/ComponentHelper.php
+++ libraries/src/Component/ComponentHelper.php
@@ -13,6 +13,7 @@ defined('JPATH_PLATFORM') or die;
 use Joomla\CMS\Access\Access;
 use Joomla\CMS\Component\Exception\MissingComponentException;
 use Joomla\Registry\Registry;
+use Joomla\CMS\Dispatcher\Dispatcher;
 
 /**
  * Component helper class
@@ -475,7 +476,7 @@ class ComponentHelper
 	{
 		$reflect = new \ReflectionClass($object);
 
-		if (!$reflect->getNamespaceName() || strpos($reflect->getNamespaceName(), '\\Component') === false)
+		if (!$reflect->getNamespaceName() || get_class($object) == Dispatcher::class)
 		{
 			return 'com_' . strtolower($alternativeName);
 		}
