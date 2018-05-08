d121f01271ae9ae86a00d6868407868524f9fcdb
Benjamin Trenkle <benjamin.trenkle@wicked-software.de>
2018-05-08 15:18:13 +0200

    Moves state select field to the library folder

--- libraries/src/Form/Field/WorkflowStateField.php
+++ libraries/src/Form/Field/WorkflowStateField.php
@@ -6,7 +6,7 @@
  * @copyright   Copyright (C) 2005 - 2017 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
-namespace Joomla\Component\Workflow\Administrator\Field;
+namespace Joomla\CMS\Form\Field;
 
 defined('_JEXEC') or die;
 
