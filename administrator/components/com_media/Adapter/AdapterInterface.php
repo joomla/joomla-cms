0ef3efe2f1c6f088a4be041bea85e8102b9bcb67
wilsonge <georgejameswilson@googlemail.com>
2018-05-12 15:26:10 +0100

    Update administrator to 4.0 alpha 3

--- administrator/components/com_media/Adapter/AdapterInterface.php
+++ administrator/components/com_media/Adapter/AdapterInterface.php
@@ -3,7 +3,7 @@
  * @package     Joomla.Administrator
  * @subpackage  com_media
  *
- * @copyright   Copyright (C) 2005 - 2017 Open Source Matters, Inc. All rights reserved.
+ * @copyright   Copyright (C) 2005 - 2018 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
@@ -75,7 +75,7 @@ interface AdapterInterface
 	 *
 	 * @return  resource
 	 *
-	 * @since   __DEPLOY_VERSION__
+	 * @since   4.0.0
 	 * @throws  \Exception
 	 */
 	public function getResource($path);
