b9dcb433271183b258e2f886b85680242e19f0f6
Benjamin Trenkle <benjamin.trenkle@wicked-software.de>
2018-05-13 08:24:02 +0200

    Fix wrong table prefix

--- installation/sql/mysql/joomla.sql
+++ installation/sql/mysql/joomla.sql
@@ -30,7 +30,7 @@ CREATE TABLE IF NOT EXISTS `#__assets` (
 -- Dumping data for table `#__assets`
 --
 
-INSERT INTO `lmsw5_assets` (`id`, `parent_id`, `lft`, `rgt`, `level`, `name`, `title`, `rules`) VALUES
+INSERT INTO `#__assets` (`id`, `parent_id`, `lft`, `rgt`, `level`, `name`, `title`, `rules`) VALUES
 (1, 0, 0, 123, 0, 'root.1', 'Root Asset', '{\"core.login.site\":{\"6\":1,\"2\":1},\"core.login.admin\":{\"6\":1},\"core.login.offline\":{\"6\":1},\"core.admin\":{\"8\":1},\"core.manage\":{\"7\":1},\"core.create\":{\"6\":1,\"3\":1},\"core.delete\":{\"6\":1},\"core.edit\":{\"6\":1,\"4\":1},\"core.edit.state\":{\"6\":1,\"5\":1},\"core.edit.own\":{\"6\":1,\"3\":1}}'),
 (2, 1, 1, 2, 1, 'com_admin', 'com_admin', '{}'),
 (3, 1, 3, 6, 1, 'com_banners', 'com_banners', '{\"core.admin\":{\"7\":1},\"core.manage\":{\"6\":1}}'),
