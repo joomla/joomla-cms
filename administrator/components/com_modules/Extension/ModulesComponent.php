e73a9559d36228a3033a30c6e055f6c655232ae1
wilsonge <georgejameswilson@googlemail.com>
2018-05-13 09:39:25 +0100

    Give com_modules a service interface so that com_config can integrate

---
+++ administrator/components/com_modules/Extension/ModulesComponent.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_modules
+ *
+ * @copyright   Copyright (C) 2005 - 2018 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+namespace Joomla\Component\Modules\Administrator\Extension;
+
+defined('JPATH_PLATFORM') or die;
+
+use Joomla\CMS\Extension\Component;
+use Joomla\CMS\MVC\Factory\MVCFactoryServiceTrait;
+use Joomla\CMS\MVC\Factory\MVCFactoryServiceInterface;
+
+/**
+ * Component class for com_modules
+ *
+ * @since  __DEPLOY_VERSION__
+ */
+class ModulesComponent extends Component implements MVCFactoryServiceInterface
+{
+	use MVCFactoryServiceTrait;
+}
