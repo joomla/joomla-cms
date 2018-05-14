be1059e9075d7ddb4be42ea80cf5d791e2f51d02
Allon Moritz <allon.moritz@digital-peak.com>
2018-05-14 13:10:47 +0200

    Update copyright year to 2018

--- media/plg_installer_webinstaller/js/client.js
+++ media/plg_installer_webinstaller/js/client.js
@@ -7,7 +7,7 @@ var _createClass = function () { function defineProperties(target, props) { for 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 /**
- * @copyright  Copyright (C) 2005 - 2017 Open Source Matters, Inc. All rights reserved.
+ * @copyright  Copyright (C) 2005 - 2018 Open Source Matters, Inc. All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 
