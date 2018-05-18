0085488db90b737240016b3c7bd809a65cbe5eaa
dgrammatiko <d.grammatiko@gmail.com>
2018-05-18 13:14:06 +0200

    aes-js

--- administrator/components/com_joomlaupdate/tmpl/update/default.php
+++ administrator/components/com_joomlaupdate/tmpl/update/default.php
@@ -9,20 +9,23 @@
 
 defined('_JEXEC') or die;
 
-use Joomla\CMS\Factory;
 use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Uri\Uri;
+use Joomla\CMS\Language\Text;
 
+// @TODO Remove jQuery dependency, check https://github.com/joomla/joomla-cms/pull/18912
 // Include jQuery.
 JHtml::_('jquery.framework');
 
 // Load the scripts
-JHtml::_('script', 'com_joomlaupdate/encryption.js', array('version' => 'auto', 'relative' => true));
-JHtml::_('script', 'com_joomlaupdate/update.js', array('version' => 'auto', 'relative' => true));
+HTMLHelper::_('script', 'vendor/aes-js/index.min.js', array('version' => 'auto', 'relative' => true));
+HTMLHelper::_('script', 'com_joomlaupdate/update.js', array('version' => 'auto', 'relative' => true));
 
-$password = JFactory::getApplication()->getUserState('com_joomlaupdate.password', null);
-$filesize = JFactory::getApplication()->getUserState('com_joomlaupdate.filesize', null);
-$ajaxUrl = JUri::base() . 'components/com_joomlaupdate/restore.php';
-$returnUrl = 'index.php?option=com_joomlaupdate&task=update.finalise&' . JFactory::getSession()->getFormToken() . '=1';
+$password = Factory::getApplication()->getUserState('com_joomlaupdate.password', null);
+$filesize = Factory::getApplication()->getUserState('com_joomlaupdate.filesize', null);
+$ajaxUrl = Uri::base() . 'components/com_joomlaupdate/restore.php';
+$returnUrl = 'index.php?option=com_joomlaupdate&task=update.finalise&' . Factory::getSession()->getFormToken() . '=1';
 
 HTMLHelper::_('script', 'com_joomlaupdate/admin-update-default.js', ['relative' => true, 'version' => 'auto']);
 
@@ -37,7 +40,7 @@ Factory::getDocument()->addScriptOptions(
 );
 ?>
 
-<p class="nowarning"><?php echo JText::_('COM_JOOMLAUPDATE_VIEW_UPDATE_INPROGRESS'); ?></p>
+<p class="nowarning"><?php echo Text::_('COM_JOOMLAUPDATE_VIEW_UPDATE_INPROGRESS'); ?></p>
 
 <div id="update-progress">
 	<div id="extprogress">
@@ -45,19 +48,19 @@ Factory::getDocument()->addScriptOptions(
 			<div id="progress-bar" class="progress-bar progress-bar-striped progress-bar-animated" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
 		</div>
 		<div class="extprogrow">
-			<span class="extlabel"><?php echo JText::_('COM_JOOMLAUPDATE_VIEW_UPDATE_PERCENT'); ?></span>
+			<span class="extlabel"><?php echo Text::_('COM_JOOMLAUPDATE_VIEW_UPDATE_PERCENT'); ?></span>
 			<span class="extvalue" id="extpercent"></span>
 		</div>
 		<div class="extprogrow">
-			<span class="extlabel"><?php echo JText::_('COM_JOOMLAUPDATE_VIEW_UPDATE_BYTESREAD'); ?></span>
+			<span class="extlabel"><?php echo Text::_('COM_JOOMLAUPDATE_VIEW_UPDATE_BYTESREAD'); ?></span>
 			<span class="extvalue" id="extbytesin"></span>
 		</div>
 		<div class="extprogrow">
-			<span class="extlabel"><?php echo JText::_('COM_JOOMLAUPDATE_VIEW_UPDATE_BYTESEXTRACTED'); ?></span>
+			<span class="extlabel"><?php echo Text::_('COM_JOOMLAUPDATE_VIEW_UPDATE_BYTESEXTRACTED'); ?></span>
 			<span class="extvalue" id="extbytesout"></span>
 		</div>
 		<div class="extprogrow">
-			<span class="extlabel"><?php echo JText::_('COM_JOOMLAUPDATE_VIEW_UPDATE_FILESEXTRACTED'); ?></span>
+			<span class="extlabel"><?php echo Text::_('COM_JOOMLAUPDATE_VIEW_UPDATE_FILESEXTRACTED'); ?></span>
 			<span class="extvalue" id="extfiles"></span>
 		</div>
 	</div>
