edde03cc07b069c25b346ef2cf4b8c587bda9533
Tuan Pham Ngoc <github@joomdonation.com>
2018-05-19 10:59:11 +0700

    Improve export
    1. Add config option to allow choosing delimiter
    2. Translate message
    3. Show name instead of user id

--- administrator/components/com_userlogs/helpers/userlogs.php
+++ administrator/components/com_userlogs/helpers/userlogs.php
@@ -9,6 +9,7 @@
 
 defined('_JEXEC') or die;
 
+use Joomla\CMS\Component\ComponentHelper;
 use Joomla\CMS\Filesystem\Path;
 
 /**
@@ -29,8 +30,9 @@ class UserlogsHelper
 	 */
 	public static function dataToCsv($data)
 	{
-		$date     = JFactory::getDate();
-		$filename = "logs_" . $date;
+		$date         = JFactory::getDate();
+		$filename     = "logs_" . $date;
+		$csvDelimiter = ComponentHelper::getComponent('com_userlogs')->getParams()->get('csv_delimiter', ',');
 
 		$app = JFactory::getApplication();
 		$app->setHeader('Content-Type', 'application/csv', true)
@@ -44,17 +46,19 @@ class UserlogsHelper
 		$fp = fopen('php://temp', 'r+');
 		ob_end_clean();
 
-		fputcsv($fp, $headers);
+		fputcsv($fp, $headers, $csvDelimiter);
 
-		foreach ($data as $log)
+		foreach ($data as $row)
 		{
-			$log               = (array) $log;
-			$log['ip_address'] = JText::_($log['ip_address']);
-			$log['extension']  = self::translateExtensionName(strtoupper(strtok($log['extension'], '.')));
+			$log               = array();
+			$log['id']         = $row->id;
+			$log['message']    = strip_tags(self::getHumanReadableLogMessage($row));
+			$log['date']       = $row->log_date;
+			$log['extension']  = self::translateExtensionName(strtoupper(strtok($row->extension, '.')));
+			$log['name']       = $row->name;
+			$log['ip_address'] = JText::_($row->ip_address);
 
-			$app->triggerEvent('onLogMessagePrepare', array(&$log['message'], $log['extension']));
-
-			fputcsv($fp, $log, ',');
+			fputcsv($fp, $log, $csvDelimiter);
 		}
 
 		rewind($fp);
