539e5a4c09b42a914f9f88354576b3c8f4aa34cf
Allon Moritz <allon.moritz@digital-peak.com>
2018-05-21 10:54:15 +0200

    Inject in constructor

--- administrator/components/com_associations/services/provider.php
+++ administrator/components/com_associations/services/provider.php
@@ -43,9 +43,8 @@ return new class implements ServiceProviderInterface
 			ComponentInterface::class,
 			function (Container $container)
 			{
-				$component = new MVCComponent;
+				$component = new MVCComponent($container->get(DispatcherFactoryInterface::class));
 
-				$component->setDispatcherFactory($container->get(DispatcherFactoryInterface::class));
 				$component->setMvcFactoryFactory($container->get(MVCFactoryFactoryInterface::class));
 
 				return $component;
