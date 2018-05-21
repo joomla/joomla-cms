340e0b4e5198e405338be21ff94a9bc87fe5a796
infograf768 <infografjms@gmail.com>
2018-05-21 07:38:41 +0000

    [4.0] Notices in articles layouts (#20503)

--- layouts/joomla/content/intro_image.php
+++ layouts/joomla/content/intro_image.php
@@ -13,13 +13,12 @@ use Joomla\CMS\Router\Route;
 
 $params  = $displayData->params;
 $images  = json_decode($displayData->images);
-$caption = $images->image_intro_caption;
 ?>
 <?php if (isset($images->image_intro) && !empty($images->image_intro)) : ?>
 	<?php $imgfloat = empty($images->float_intro) ? $params->get('float_intro') : $images->float_intro; ?>
 	<figure class="pull-<?php echo htmlspecialchars($imgfloat, ENT_COMPAT, 'UTF-8'); ?> item-image">
-		<?php if ($caption) : ?>
-			<figcaption class="caption"><?php echo htmlspecialchars($caption); ?></figcaption>
+		<?php if ($images->image_intro_caption !== '') : ?>
+			<figcaption class="caption"><?php echo htmlspecialchars($images->image_intro_caption, ENT_COMPAT, 'UTF-8'); ?></figcaption>
 		<?php endif; ?>
 		<?php if ($params->get('link_titles') && $params->get('access-view')) : ?>
 			<a href="<?php echo Route::_(ContentHelperRoute::getArticleRoute($displayData->slug, $displayData->catid, $displayData->language)); ?>">
