cf2c9615e2262f14d21574cb7937f0bc0020240a
Allon Moritz <allon.moritz@digital-peak.com>
2018-05-23 06:50:58 +0200

    Update dispatcher

--- libraries/src/Dispatcher/ComponentDispatcher.php
+++ libraries/src/Dispatcher/ComponentDispatcher.php
@@ -2,7 +2,7 @@
 /**
  * Joomla! Content Management System
  *
- * @copyright  Copyright (C) 2005 - 2017 Open Source Matters, Inc. All rights reserved.
+ * @copyright  Copyright (C) 2005 - 2018 Open Source Matters, Inc. All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE
  */
 
@@ -64,7 +64,7 @@ abstract class ComponentDispatcher extends Dispatcher
 		{
 			$this->option = ComponentHelper::getComponentName(
 				$this,
-				strtolower(str_replace('Dispatcher', '', get_class($this)))
+				str_replace('com_', '', $input->get('option'))
 			);
 		}
 
