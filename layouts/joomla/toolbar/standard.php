c185f3d9b184bc2b2855fbf95dd26086c5d93ced
Fedik <getthesite@gmail.com>
2018-05-26 15:39:26 +0300

    toolbar-button2: button standart layout

--- layouts/joomla/toolbar/standard.php
+++ layouts/joomla/toolbar/standard.php
@@ -12,10 +12,10 @@ defined('JPATH_BASE') or die;
 use Joomla\CMS\HTML\HTMLHelper;
 
 HTMLHelper::_('behavior.core');
-HTMLHelper::_('webcomponent', ['joomla-toolbar-button' => 'system/webcomponents/joomla-toolbar-button.min.js'], ['relative' => true, 'version' => 'auto', 'detectDebug' => true]);
+HTMLHelper::_('webcomponent', 'system/webcomponents/joomla-toolbar-button.min.js', ['relative' => true, 'version' => 'auto', 'detectDebug' => true]);
 
 /**
- * @var  int     $id
+ * @var  string  $id
  * @var  string  $onclick
  * @var  string  $class
  * @var  string  $text
@@ -25,18 +25,33 @@ HTMLHelper::_('webcomponent', ['joomla-toolbar-button' => 'system/webcomponents/
  */
 extract($displayData, EXTR_OVERWRITE);
 
-$tagName = $tagName ?? 'button';
+$tagName  = $tagName ?? 'button';
+
+$task     = '';
+$id       = !empty($displayData['id'])       ? ' id="' . $displayData['id'] . '"' : '';
+$list     = !empty($displayData['list'])     ? ' list-selection' : '';
+$form     = !empty($displayData['form'])     ? ' form="' . $this->escape($displayData['form']) . '"' : '';
+$validate = !empty($displayData['validate']) ? ' form-validation' : '';
+$msg      = !empty($displayData['msg'])      ? ' confirm-message="' . $this->escape($displayData['msg']) . '"' : '';
+
+if (!empty($displayData['task']))
+{
+	$task = ' task="' . $displayData['task'] . '"';
+}
+elseif (!empty($displayData['doTask']))
+{
+	$task = ' execute="' . $displayData['doTask'] . '"';
+}
 ?>
 
+<joomla-toolbar-button <?php echo $id.$task.$list.$form.$validate.$msg; ?>>
 <?php if (!empty($group)) : ?>
-<a<?php echo $id ?? ''; ?> href="#" onclick="<?php echo $onclick ?? ''; ?>" class="dropdown-item">
+<a href="#" class="dropdown-item">
 	<span class="<?php echo trim($class ?? ''); ?>"></span>
 	<?php echo $text ?? ''; ?>
 </a>
 <?php else : ?>
 <<?php echo $tagName; ?>
-	id="<?php echo $id ?? ''; ?>"
-	onclick="<?php echo $onclick ?? ''; ?>"
 	class="<?php echo $btnClass ?? ''; ?>"
 	<?php echo $htmlAttributes ?? ''; ?>
 	>
@@ -44,3 +59,4 @@ $tagName = $tagName ?? 'button';
 	<?php echo $text ?? ''; ?>
 </<?php echo $tagName; ?>>
 <?php endif; ?>
+</joomla-toolbar-button>
