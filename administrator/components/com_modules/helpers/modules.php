d617b0cc6f38fa9a4cfce3b972d1f4962606d624
Allon Moritz <allon.moritz@digital-peak.com>
2018-05-31 10:24:21 +0200

    Revert file

---
+++ administrator/components/com_modules/helpers/modules.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_modules
+ *
+ * @copyright   Copyright (C) 2005 - 2018 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+/**
+ * Modules component helper.
+ *
+ * @since  1.6
+ *
+ * @deprecated  5.0  Use \Joomla\Component\Modules\Administrator\Helper\ModulesHelper instead
+ */
+abstract class ModulesHelper extends \Joomla\Component\Modules\Administrator\Helper\ModulesHelper
+{
+
+}
