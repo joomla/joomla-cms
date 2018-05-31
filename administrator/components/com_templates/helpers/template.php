9f93fa4761e07f17fcebf170d2d7d5104f665911
Allon Moritz <allon.moritz@digital-peak.com>
2018-05-31 10:28:36 +0200

    Revert file

---
+++ administrator/components/com_templates/helpers/template.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_templates
+ *
+ * @copyright   Copyright (C) 2005 - 2018 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+/**
+ * Template Helper class.
+ *
+ * @since  3.2
+ *
+ * @deprecated  5.0 Use \Joomla\Component\Templates\Administrator\Helper\TemplateHelper instead
+ */
+abstract class TemplateHelper extends \Joomla\Component\Templates\Administrator\Helper\TemplateHelper
+{
+
+}
---
+++ administrator/components/com_templates/helpers/templates.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_templates
+ *
+ * @copyright   Copyright (C) 2005 - 2018 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+/**
+ * Templates component helper.
+ *
+ * @since  1.6
+ *
+ * @deprecated  5.0 Use \Joomla\Component\Templates\Administrator\Helper\TemplatesHelper instead
+ */
+class TemplatesHelper extends \Joomla\Component\Templates\Administrator\Helper\TemplatesHelper
+{
+
+}
