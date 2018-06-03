17a2d268f38581b7fafb92772417e3f876127487
Hannes Papenberg <info@joomlager.de>
2018-06-03 22:31:53 +0200

    com_finder categories plugin needs to check right extension element

--- plugins/finder/categories/categories.php
+++ plugins/finder/categories/categories.php
@@ -252,15 +252,19 @@ class PlgFinderCategories extends FinderIndexerAdapter
 			return;
 		}
 
+		// Extract the extension element
+		$parts = explode('.', $item->extension);
+		$extension_element = $parts[0];
+
 		// Check if the extension that owns the category is also enabled.
-		if (ComponentHelper::isEnabled($item->extension) === false)
+		if (ComponentHelper::isEnabled($extension_element) === false)
 		{
 			return;
 		}
 
 		$item->setLanguage();
 
-		$extension = ucfirst(substr($item->extension, 4));
+		$extension = ucfirst(substr($extension_element, 4));
 
 		// Initialize the item parameters.
 		$item->params = new Registry($item->params);
@@ -288,12 +292,12 @@ class PlgFinderCategories extends FinderIndexerAdapter
 		$item->summary = FinderIndexerHelper::prepareContent($item->summary, $item->params);
 
 		// Build the necessary route and path information.
-		$item->url = $this->getUrl($item->id, $item->extension, $this->layout);
+		$item->url = $this->getUrl($item->id, $extension_element, $this->layout);
 
 		$class = $extension . 'HelperRoute';
 
 		// Need to import component route helpers dynamically, hence the reason it's handled here.
-		JLoader::register($class, JPATH_SITE . '/components/' . $item->extension . '/helpers/route.php');
+		JLoader::register($class, JPATH_SITE . '/components/' . $extension_element . '/helpers/route.php');
 
 		if (class_exists($class) && method_exists($class, 'getCategoryRoute'))
 		{
