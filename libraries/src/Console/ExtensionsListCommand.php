e2e2925dcfddfef3d29aca6c45efc88fa36e3e80
Bosun Egberinde <bosunski@gmail.com>
2018-06-09 10:55:10 +0100

    Updated help message for extension:list command

--- libraries/src/Console/ExtensionsListCommand.php
+++ libraries/src/Console/ExtensionsListCommand.php
@@ -98,7 +98,7 @@ class ExtensionsListCommand extends AbstractCommand
 
 		$this->addOption('type', null, InputOption::VALUE_REQUIRED, 'Type of the extension');
 
-		$help = "The <info>%command.name%</info> List all currently installed extensions \n <info>php %command.full_name%</info>";
+		$help = "The <info>%command.name%</info> List all currently installed extensions \nUsage: <info>php %command.full_name%</info> --type=package";
 		$this->setHelp($help);
 	}
 
