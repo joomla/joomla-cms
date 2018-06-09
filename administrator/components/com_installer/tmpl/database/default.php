cb6f5e69921fe464aa0cd3d9e6804f3942b1f4ff
Allon Moritz <allon.moritz@digital-peak.com>
2018-06-09 20:44:44 +0200

    Update default.php

--- administrator/components/com_installer/tmpl/database/default.php
+++ administrator/components/com_installer/tmpl/database/default.php
@@ -7,12 +7,12 @@
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
+defined('_JEXEC') or die;
+
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\Layout\LayoutHelper;
 use Joomla\CMS\Router\Route;
-
-defined('_JEXEC') or die;
 
 HTMLHelper::_('behavior.multiselect');
 HTMLHelper::_('bootstrap.tooltip');
