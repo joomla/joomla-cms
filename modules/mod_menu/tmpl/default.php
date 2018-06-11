67ff4cf3fbab26dfabd60d19dd5f9d58c4cf2467
ciar4n <ciaran@joomla51.com>
2018-06-11 14:45:06 +0100

    HTML class naming standard [frontend modules]

--- modules/mod_menu/tmpl/default.php
+++ modules/mod_menu/tmpl/default.php
@@ -20,7 +20,7 @@ if ($tagId = $params->get('tag_id', ''))
 
 // The menu class is deprecated. Use nav instead
 ?>
-<ul<?php echo $id; ?> class="nav flex-column <?php echo $class_sfx; ?>">
+<ul<?php echo $id; ?> class="mod-menu nav flex-column <?php echo $class_sfx; ?>">
 <?php foreach ($list as $i => &$item)
 {
 	$class = 'nav-item';
@@ -86,7 +86,7 @@ if ($tagId = $params->get('tag_id', ''))
 	// The next item is deeper.
 	if ($item->deeper)
 	{
-		echo '<ul class="list-unstyled small">';
+		echo '<ul class="mod-menu__sub list-unstyled small">';
 	}
 	// The next item is shallower.
 	elseif ($item->shallower)
