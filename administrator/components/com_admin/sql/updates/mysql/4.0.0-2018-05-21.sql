87e2cf444681ed08c609e59d6bce3994354517d3
Hannes Papenberg <info@joomlager.de>
2018-06-12 09:37:03 +0200

    Removing #__finder_termsx tables from update file

--- administrator/components/com_admin/sql/updates/mysql/4.0.0-2018-05-21.sql
+++ administrator/components/com_admin/sql/updates/mysql/4.0.0-2018-05-21.sql
@@ -13,23 +13,8 @@ ALTER TABLE `#__finder_links` MODIFY `access` int(5) NOT NULL DEFAULT 0;
 ALTER TABLE `#__finder_links` MODIFY `language` char(7) NOT NULL DEFAULT '';
 ALTER TABLE `#__finder_links` MODIFY `type_id` int(11) NOT NULL DEFAULT 0;
 ALTER TABLE `#__finder_links` ADD INDEX `idx_language` (`language`);
--- finder_links_termsx tables
-ALTER TABLE `#__finder_links_terms0` MODIFY `weight` float unsigned NOT NULL DEFAULT 0;
-ALTER TABLE `#__finder_links_terms1` MODIFY `weight` float unsigned NOT NULL DEFAULT 0;
-ALTER TABLE `#__finder_links_terms2` MODIFY `weight` float unsigned NOT NULL DEFAULT 0;
-ALTER TABLE `#__finder_links_terms3` MODIFY `weight` float unsigned NOT NULL DEFAULT 0;
-ALTER TABLE `#__finder_links_terms4` MODIFY `weight` float unsigned NOT NULL DEFAULT 0;
-ALTER TABLE `#__finder_links_terms5` MODIFY `weight` float unsigned NOT NULL DEFAULT 0;
-ALTER TABLE `#__finder_links_terms6` MODIFY `weight` float unsigned NOT NULL DEFAULT 0;
-ALTER TABLE `#__finder_links_terms7` MODIFY `weight` float unsigned NOT NULL DEFAULT 0;
-ALTER TABLE `#__finder_links_terms8` MODIFY `weight` float unsigned NOT NULL DEFAULT 0;
-ALTER TABLE `#__finder_links_terms9` MODIFY `weight` float unsigned NOT NULL DEFAULT 0;
-ALTER TABLE `#__finder_links_termsa` MODIFY `weight` float unsigned NOT NULL DEFAULT 0;
-ALTER TABLE `#__finder_links_termsb` MODIFY `weight` float unsigned NOT NULL DEFAULT 0;
-ALTER TABLE `#__finder_links_termsc` MODIFY `weight` float unsigned NOT NULL DEFAULT 0;
-ALTER TABLE `#__finder_links_termsd` MODIFY `weight` float unsigned NOT NULL DEFAULT 0;
-ALTER TABLE `#__finder_links_termse` MODIFY `weight` float unsigned NOT NULL DEFAULT 0;
-ALTER TABLE `#__finder_links_termsf` MODIFY `weight` float unsigned NOT NULL DEFAULT 0;
+-- finder_links_terms tables
+ALTER TABLE `#__finder_links_terms` MODIFY `weight` float unsigned NOT NULL DEFAULT 0;
 -- finder_taxonomy table
 ALTER TABLE `#__finder_taxonomy` MODIFY `title` varchar(255) NOT NULL DEFAULT '';
 -- finder_terms table
