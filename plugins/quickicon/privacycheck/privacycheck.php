237c050483e7e092f3d6660697a0766df615fa04
Nicola Galgano <extensions4joomla@gmail.com>
2018-06-14 08:57:28 +0200

    js clean 3/3

--- plugins/quickicon/privacycheck/privacycheck.php
+++ plugins/quickicon/privacycheck/privacycheck.php
@@ -11,7 +11,6 @@ defined('_JEXEC') or die;
 
 /**
  * Plugin to check privacy requests older than 14 days
- *
  * @since  3.9.0
  */
 class PlgQuickiconPrivacyCheck extends JPlugin
@@ -20,12 +19,12 @@ class PlgQuickiconPrivacyCheck extends JPlugin
 	 * Load plugin language files automatically
 	 *
 	 * @var    boolean
-	 * @since  3.7.0
+	 * @since  3.9.0
 	 */
 	protected $autoloadLanguage = true;
 
 	/**
-	 * Check privacy requests older than 14 days.
+	 * Check the PHP version after the admin component has been dispatched.
 	 *
 	 * @param   string  $context  The calling context
 	 *
@@ -43,19 +42,21 @@ class PlgQuickiconPrivacyCheck extends JPlugin
 		JHtml::_('jquery.framework');
 
 		$token    = JSession::getFormToken() . '=' . 1;
+	
 		$options  = array(
 			'plg_quickicon_privacycheck_url'      => JUri::base() . 'index.php?option=com_privacy&' . $token,
 			'plg_quickicon_privacycheck_ajax_url' => JUri::base() . 'index.php?option=com_privacy&task=ajax&' . $token,
 			'plg_quickicon_privacycheck_text'     => array(
-				"UPTODATE"            => JText::_('PLG_QUICKICON_PRIVACYCHECK_UPTODATE', true),
-				"UPDATEFOUND"         => JText::_('PLG_QUICKICON_PRIVACYCHECK_UPDATEFOUND', true),
-				"UPDATEFOUND_MESSAGE" => JText::_('PLG_QUICKICON_PRIVACYCHECK_UPDATEFOUND_MESSAGE', true),
-				"UPDATEFOUND_BUTTON"  => JText::_('PLG_QUICKICON_PRIVACYCHECK_UPDATEFOUND_BUTTON', true),
-				"ERROR"               => JText::_('PLG_QUICKICON_PRIVACYCHECK_ERROR', true),
+				"NOREQUEST"            => JText::_('PLG_QUICKICON_PRIVACYCHECK_NOREQUEST', true),
+				"REQUESTFOUND"         => JText::_('PLG_QUICKICON_PRIVACYCHECK_REQUESTFOUND', true),
+				"REQUESTFOUND_MESSAGE" => JText::_('PLG_QUICKICON_PRIVACYCHECK_REQUESTFOUND_MESSAGE', true),
+				"REQUESTFOUND_BUTTON"  => JText::_('PLG_QUICKICON_PRIVACYCHECK_REQUESTFOUND_BUTTON', true),
+				"ERROR"                => JText::_('PLG_QUICKICON_PRIVACYCHECK_ERROR', true),
 			)
 		);
 
 		JFactory::getDocument()->addScriptOptions('js-privacy-check', $options);
+ 
 		JHtml::_('script', 'plg_quickicon_privacycheck/privacycheck.js', array('version' => 'auto', 'relative' => true));
 
 		return array(
@@ -70,3 +71,4 @@ class PlgQuickiconPrivacyCheck extends JPlugin
 		);
 	}
 }
+
