cc9b00310e09cfac6bca99933c7fefa3a914a946
zero-24 <zero-24@users.noreply.github.com>
2018-06-17 08:45:08 +0200

    drone

--- administrator/components/com_installer/models/update.php
+++ administrator/components/com_installer/models/update.php
@@ -389,7 +389,7 @@ class InstallerModelUpdate extends JModelList
 	/**
 	 * Handles the actual update installation.
 	 *
-	 * @param   JUpdate  $update     An update definition
+	 * @param   JUpdate  $update  An update definition
 	 *
 	 * @return  boolean   Result of install
 	 *
