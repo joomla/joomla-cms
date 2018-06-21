239202ca6cbb1681f26e23d6d88d044325ac123a
Ciaran Walsh <ciaran@joomla51.com>
2018-06-21 15:11:10 +0100

    HTML class naming standard [frontend][com-users] (#20817)

--- components/com_users/tmpl/reset/complete.php
+++ components/com_users/tmpl/reset/complete.php
@@ -13,7 +13,7 @@ JHtml::_('behavior.keepalive');
 JHtml::_('behavior.formvalidator');
 
 ?>
-<div class="reset-complete">
+<div class="com-users-reset-complete reset-complete">
 	<?php if ($this->params->get('show_page_heading')) : ?>
 		<div class="page-header">
 			<h1>
@@ -21,7 +21,7 @@ JHtml::_('behavior.formvalidator');
 			</h1>
 		</div>
 	<?php endif; ?>
-	<form action="<?php echo JRoute::_('index.php?option=com_users&task=reset.complete'); ?>" method="post" class="form-validate form-horizontal well">
+	<form action="<?php echo JRoute::_('index.php?option=com_users&task=reset.complete'); ?>" method="post" class="com-users-reset-complete__form form-validate form-horizontal well">
 		<?php foreach ($this->form->getFieldsets() as $fieldset) : ?>
 			<fieldset>
 				<p><?php echo JText::_($fieldset->label); ?></p>
@@ -37,7 +37,7 @@ JHtml::_('behavior.formvalidator');
 				<?php endforeach; ?>
 			</fieldset>
 		<?php endforeach; ?>
-		<div class="control-group">
+		<div class="com-users-reset-complete__submit control-group">
 			<div class="controls">
 				<button type="submit" class="btn btn-primary validate">
 					<?php echo JText::_('JSUBMIT'); ?>
