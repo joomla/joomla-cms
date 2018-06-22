8f885683019e060eac05dd7278e325c4c378a79a
Tuan Pham Ngoc <github@joomdonation.com>
2018-06-22 07:50:15 +0700

    Fix issue 125 (#132)
    * Display correct action logs even plugins disabled
    
    * Revert "Display correct action logs even plugins disabled"
    
    This reverts commit dbf2649d9c77e2a92708d187c77bd340b116ba86.
    
    * Fix issue #125
    
    * CS
    
    * CS - space to tab
    
    * CS foreach

--- administrator/components/com_actionlogs/layouts/logstable.php
+++ administrator/components/com_actionlogs/layouts/logstable.php
@@ -12,6 +12,7 @@ defined('_JEXEC') or die;
 JFactory::getLanguage()->load("com_actionlogs", JPATH_ADMINISTRATOR, null, false, true);
 
 $messages = $displayData['messages'];
+$showIpColumn = $displayData['showIpColumn'];
 ?>
 <h1>
 	<?php echo JText::_('COM_ACTIONLOGS_EMAIL_SUBJECT'); ?>
@@ -25,22 +26,21 @@ $messages = $displayData['messages'];
 		<th><?php echo JText::_('COM_ACTIONLOGS_DATE'); ?></th>
 		<th><?php echo JText::_('COM_ACTIONLOGS_EXTENSION'); ?></th>
 		<th><?php echo JText::_('COM_ACTIONLOGS_NAME'); ?></th>
-		<th><?php echo JText::_('COM_ACTIONLOGS_IP_ADDRESS'); ?></th>
+		<?php if ($showIpColumn) : ?>
+			<th><?php echo JText::_('COM_ACTIONLOGS_IP_ADDRESS'); ?></th>
+		<?php endif; ?>
 	</thead>
 	<tbody>
-        <?php
-            foreach ($messages as $message)
-            {
-            ?>
-                <tr>
-                    <td><?php echo $message->message; ?></td>
-                    <td><?php echo $message->log_date; ?></td>
-                    <td><?php echo $message->extension; ?></td>
-                    <td><?php echo $displayData['username']; ?></td>
-                    <td><?php echo $message->ip_address; ?></td>
-                </tr>
-            <?php
-            }
-        ?>
+		<?php foreach ($messages as $message) : ?>
+			<tr>
+				<td><?php echo $message->message; ?></td>
+				<td><?php echo $message->log_date; ?></td>
+				<td><?php echo $message->extension; ?></td>
+				<td><?php echo $displayData['username']; ?></td>
+				<?php if ($showIpColumn) : ?>
+					<td><?php echo JText::_($message->ip_address); ?></td>
+				<?php endif; ?>
+			</tr>
+		<?php endforeach; ?>
 	</tbody>
 </table>
