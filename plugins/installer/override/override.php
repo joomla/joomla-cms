fc5702aa91676c4d44fa963d86b325cad01ea30c
Astrid <astridx@users.noreply.github.com>
2018-06-26 13:14:25 +0200

    correction (#26)

--- plugins/installer/override/override.php
+++ plugins/installer/override/override.php
@@ -121,13 +121,11 @@ class PlgInstallerOverride extends CMSPlugin
 		$size1  = count($after);
 		$size2  = count($before);
 
-		$result = null;
+		$result = array();
 
 		if ($size1 === $size2)
 		{
-			$result = array();
-
-			for ($i = 0; $i <= $size1; $i++)
+			for ($i = 0; $i < $size1; $i++)
 			{
 				if ($after[$i]->coreFile !== $before[$i]->coreFile)
 				{
