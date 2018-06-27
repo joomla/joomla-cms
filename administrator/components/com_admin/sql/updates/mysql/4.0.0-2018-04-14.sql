4fe4e3373befcb7b1da239386094e4a4bf6248ce
Alexandra Ciobica <alexandraciobica@gmail.com>
2018-06-27 13:24:11 +0300

    One line statements

--- administrator/components/com_admin/sql/updates/mysql/4.0.0-2018-04-14.sql
+++ administrator/components/com_admin/sql/updates/mysql/4.0.0-2018-04-14.sql
@@ -5,10 +5,8 @@ CREATE TABLE `#__finder_links_terms` (
 	PRIMARY KEY (`link_id`, `term_id`),
 	INDEX `idx_term_weight` (`term_id`, `weight`),
 	INDEX `idx_link_term_weight` (`link_id`, `term_id`, `weight`)
-)
-COLLATE='utf8mb4_general_ci'
-ENGINE=InnoDB
-;
+) COLLATE='utf8mb4_general_ci' ENGINE=InnoDB;
+
 DROP TABLE #__finder_links_terms0;
 DROP TABLE #__finder_links_terms1;
 DROP TABLE #__finder_links_terms2;
@@ -26,20 +24,14 @@ DROP TABLE #__finder_links_termsd;
 DROP TABLE #__finder_links_termse;
 DROP TABLE #__finder_links_termsf;
 
-ALTER TABLE `#__finder_terms`
-	CHANGE `language` `language` CHAR(7) NOT NULL DEFAULT '' AFTER `links`;
+ALTER TABLE `#__finder_terms` CHANGE `language` `language` CHAR(7) NOT NULL DEFAULT '' AFTER `links`;
 
-ALTER TABLE `#__finder_terms_common`
-	CHANGE `language` `language` CHAR(7) NOT NULL DEFAULT '' AFTER `term`;
+ALTER TABLE `#__finder_terms_common` CHANGE `language` `language` CHAR(7) NOT NULL DEFAULT '' AFTER `term`;
 
-ALTER TABLE `#__finder_tokens`
-	CHANGE `language` `language` CHAR(7) NOT NULL DEFAULT '' AFTER `context`;
+ALTER TABLE `#__finder_tokens` CHANGE `language` `language` CHAR(7) NOT NULL DEFAULT '' AFTER `context`;
 
-ALTER TABLE `#__finder_tokens_aggregate`
-	DROP COLUMN `map_suffix`;
+ALTER TABLE `#__finder_tokens_aggregate` DROP COLUMN `map_suffix`;
 
-ALTER TABLE `#__finder_tokens_aggregate`
-	CHANGE `language` `language` CHAR(7) NOT NULL DEFAULT '' AFTER `total_weight`;
+ALTER TABLE `#__finder_tokens_aggregate` CHANGE `language` `language` CHAR(7) NOT NULL DEFAULT '' AFTER `total_weight`;
 
-ALTER TABLE `#__finder_links`
-	CHANGE `language` `language` CHAR(7) NOT NULL DEFAULT '' AFTER `access`;
+ALTER TABLE `#__finder_links` CHANGE `language` `language` CHAR(7) NOT NULL DEFAULT '' AFTER `access`;
