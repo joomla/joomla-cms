bccbfa19a938c08e131600d0dc9f66b47308aa85
Alexandra Ciobica <alexandraciobica@gmail.com>
2018-06-27 11:36:36 +0300

    Removed "COLUMN" from CHANGE so the sql statement matches the schema parser expected format

--- administrator/components/com_admin/sql/updates/mysql/4.0.0-2018-04-14.sql
+++ administrator/components/com_admin/sql/updates/mysql/4.0.0-2018-04-14.sql
@@ -27,19 +27,19 @@ DROP TABLE #__finder_links_termse;
 DROP TABLE #__finder_links_termsf;
 
 ALTER TABLE `#__finder_terms`
-	CHANGE COLUMN `language` `language` CHAR(7) NOT NULL DEFAULT '' AFTER `links`;
+	CHANGE `language` `language` CHAR(7) NOT NULL DEFAULT '' AFTER `links`;
 
 ALTER TABLE `#__finder_terms_common`
-	CHANGE COLUMN `language` `language` CHAR(7) NOT NULL DEFAULT '' AFTER `term`;
+	CHANGE `language` `language` CHAR(7) NOT NULL DEFAULT '' AFTER `term`;
 
 ALTER TABLE `#__finder_tokens`
-	CHANGE COLUMN `language` `language` CHAR(7) NOT NULL DEFAULT '' AFTER `context`;
+	CHANGE `language` `language` CHAR(7) NOT NULL DEFAULT '' AFTER `context`;
 
 ALTER TABLE `#__finder_tokens_aggregate`
 	DROP COLUMN `map_suffix`;
 
 ALTER TABLE `#__finder_tokens_aggregate`
-	CHANGE COLUMN `language` `language` CHAR(7) NOT NULL DEFAULT '' AFTER `total_weight`;
+	CHANGE `language` `language` CHAR(7) NOT NULL DEFAULT '' AFTER `total_weight`;
 
 ALTER TABLE `#__finder_links`
-	CHANGE COLUMN `language` `language` CHAR(7) NOT NULL DEFAULT '' AFTER `access`;
+	CHANGE `language` `language` CHAR(7) NOT NULL DEFAULT '' AFTER `access`;
