7554ab65d5d54333d199b411379c4a580b52ff15
Nicola Galgano <info@alikonweb.it>
2018-06-27 18:19:40 +0200

    com_csp  postgresql

--- administrator/components/com_admin/sql/updates/postgresql/4.0.0-2018-06-03.sql
+++ administrator/components/com_admin/sql/updates/postgresql/4.0.0-2018-06-03.sql
@@ -13,5 +13,5 @@ CREATE TABLE IF NOT EXISTS "#__csp" (
   PRIMARY KEY ("id")
 );
 
-INSERT INTO "#__extensions" ("extension_id", "name", "type", "element", "folder", "client_id", "enabled", "access", "protected", "manifest_cache", "params", "custom_data", "system_data", "checked_out", "checked_out_time", "ordering", "state", "namespace") VALUES
-(35, 'com_csp', 'component', 'com_csp', ' ', 0, 0, 1, 0, '', '{}', '', '', 0, '1970-01-01 00:00:00', 0, 0, 'Joomla\\Component\\Csp');
+INSERT INTO "#__extensions" ("extension_id", "name", "type", "element", "folder", "client_id", "enabled", "access", "protected", "manifest_cache", "params", "checked_out", "checked_out_time", "ordering", "state", "namespace") VALUES
+(35, 'com_csp', 'component', 'com_csp', ' ', 0, 0, 1, 0, '', '{}', 0, '1970-01-01 00:00:00', 0, 0, 'Joomla\\Component\\Csp');
