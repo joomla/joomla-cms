a0fbae946097ab61eaccf0ace77e809b02f81278
Alexandra Ciobica <alexandraciobica@gmail.com>
2018-06-27 10:59:05 +0300

    Changed query to match the schema parser expected format

--- administrator/components/com_admin/sql/updates/mysql/4.0.0-2018-06-26.sql
+++ administrator/components/com_admin/sql/updates/mysql/4.0.0-2018-06-26.sql
@@ -1,1 +1,1 @@
-ALTER TABLE `#__user_notes` CHANGE COLUMN `modified_user_id` `modified_user_id` int(10) unsigned NOT NULL DEFAULT 0;
+ALTER TABLE `#__user_notes` CHANGE `modified_user_id` `modified_user_id` int(10) unsigned NOT NULL DEFAULT 0;
