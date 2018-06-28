84a2ba5ec4b9ab5be9737245c40c8eb7a973a246
Brian Teeman <brian@teeman.net>
2018-06-28 19:58:48 +0100

    [4.0] Namespace com_tags (#20913)
    * [4.0] Namespace com_tags
    
    Namespace of frontend components. Broken down into one PR for each component so that hopefully it can now be merged more easily and without the need for daily having to resolve merge conflicts.
    
    * Human friendly return string

--- components/com_tags/Model/TagModel.php
+++ components/com_tags/Model/TagModel.php
@@ -15,6 +15,9 @@ use Joomla\CMS\Helper\TagsHelper;
 use Joomla\CMS\MVC\Model\ListModel;
 use Joomla\Utilities\ArrayHelper;
 use Joomla\CMS\MVC\Factory\MVCFactoryInterface;
+use Joomla\CMS\Object\CMSObject;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Factory;
 
 /**
  * Tags Component Tag Model
@@ -174,7 +177,7 @@ class TagModel extends ListModel
 	 */
 	protected function populateState($ordering = 'c.core_title', $direction = 'ASC')
 	{
-		$app = \JFactory::getApplication();
+		$app = Factory::getApplication();
 
 		// Load the parameters.
 		$params = $app->isClient('administrator') ? ComponentHelper::getParams('com_tags') : $app->getParams();
@@ -294,14 +297,14 @@ class TagModel extends ListModel
 						}
 					}
 
-					if (!in_array($table->access, \JFactory::getUser()->getAuthorisedViewLevels()))
+					if (!in_array($table->access, Factory::getUser()->getAuthorisedViewLevels()))
 					{
 						continue;
 					}
 
-					// Convert the \JTable to a clean \JObject.
+					// Convert the Table to a clean CMSObject.
 					$properties = $table->getProperties(1);
-					$this->item[] = ArrayHelper::toObject($properties, 'JObject');
+					$this->item[] = ArrayHelper::toObject($properties, CMSObject::class);
 				}
 				catch (\RuntimeException $e)
 				{
@@ -314,7 +317,7 @@ class TagModel extends ListModel
 
 		if (!$this->item)
 		{
-			throw new \Exception(\JText::_('COM_TAGS_TAG_NOT_FOUND'), 404);
+			throw new \Exception(Text::_('COM_TAGS_TAG_NOT_FOUND'), 404);
 		}
 
 		return $this->item;
@@ -331,7 +334,7 @@ class TagModel extends ListModel
 	 */
 	public function hit($pk = 0)
 	{
-		$input    = \JFactory::getApplication()->input;
+		$input    = Factory::getApplication()->input;
 		$hitcount = $input->getInt('hitcount', 1);
 
 		if ($hitcount)
@@ -345,7 +348,7 @@ class TagModel extends ListModel
 
 			if (!$table->hasPrimaryKey())
 			{
-				throw new \Exception(\JText::_('COM_TAGS_TAG_NOT_FOUND'), 404);
+				throw new \Exception(Text::_('COM_TAGS_TAG_NOT_FOUND'), 404);
 			}
 		}
 
