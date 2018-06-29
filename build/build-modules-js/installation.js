70c2d151727b61db883bd27fc18ac555dbe44a69
dgrammatiko <d.grammatiko@gmail.com>
2018-06-29 10:29:24 +0200

    cs

--- build/build-modules-js/installation.js
+++ build/build-modules-js/installation.js
@@ -4,7 +4,6 @@ const Recurs = require("recursive-readdir");
 
 const rootPath = __dirname.replace('/build/build-modules-js', '').replace('\\build\\build-modules-js', '');
 const dir = `${rootPath}/installation/language`;
-const dest = `${rootPath}/templates/system/js`;
 const installationFile = `${rootPath}/templates/system/incompatible.html`;
 
 // Set the initial template
@@ -12,41 +11,41 @@ let template = `
 var errorLocale = {`;
 
 installation = () => {
-	let installationContent = fs.readFileSync(`${__dirname}/incompatible.html`, 'utf-8');
+  let installationContent = fs.readFileSync(`${__dirname}/incompatible.html`, 'utf-8');
 
-	Recurs(dir).then(
-		(files) => {
-			files.forEach((file) => {
-				const languageStrings = ini.parse(fs.readFileSync(file, 'UTF-8'));
-				if (languageStrings["MIN_PHP_ERROR_LANGUAGE"]) {
-					const name = file.replace('.ini', '').replace(/.+\//, '');
-					template += `
+  Recurs(dir).then(
+    (files) => {
+      files.forEach((file) => {
+        const languageStrings = ini.parse(fs.readFileSync(file, 'UTF-8'));
+        if (languageStrings["MIN_PHP_ERROR_LANGUAGE"]) {
+          const name = file.replace('.ini', '').replace(/.+\//, '');
+          template += `
   "${name}": {
     "language": "` + languageStrings["MIN_PHP_ERROR_LANGUAGE"] + `",
     "header": "` + languageStrings["MIN_PHP_ERROR_HEADER"] + `",
     "text1": "` + languageStrings["MIN_PHP_ERROR_TEXT"] + `",
     "help-url-text": "` + languageStrings["MIN_PHP_ERROR_URL_TEXT"] + `"
   },`;
-				}
-			});
+        }
+      });
 
-			template = template + `
+      template = template + `
 }`;
 
-			installationContent = installationContent.replace('{{jsonContents}}', template);
+      installationContent = installationContent.replace('{{jsonContents}}', template);
 
-			fs.writeFile(installationFile, installationContent, (err) => {
-				if (err) {
-					return console.log(err);
-				}
+      fs.writeFile(installationFile, installationContent, (err) => {
+        if (err) {
+          return console.log(err);
+        }
 
-				console.log("The installation error page was saved!");
-			});
-		},
-		(error) => {
-			console.error("something exploded", error);
-		}
-	);
+        console.log("The installation error page was saved!");
+      });
+    },
+    (error) => {
+      console.error("something exploded", error);
+    }
+  );
 };
 
 module.exports.installation = installation;
