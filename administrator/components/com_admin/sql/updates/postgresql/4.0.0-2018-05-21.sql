2aa2461d7e8fd30a7f821bfc7179e094383786fe
Hannes Papenberg <info@joomlager.de>
2018-06-30 23:54:43 +0200

    Removing obsolete SQL

--- administrator/components/com_admin/sql/updates/postgresql/4.0.0-2018-05-21.sql
+++ administrator/components/com_admin/sql/updates/postgresql/4.0.0-2018-05-21.sql
@@ -13,7 +13,7 @@ ALTER TABLE "#__finder_links" ALTER COLUMN "language" TYPE CHAR(7);
 ALTER TABLE "#__finder_links" ALTER COLUMN "language" SET DEFAULT '';
 ALTER TABLE "#__finder_links" ALTER COLUMN "type_id" SET DEFAULT 0;
 CREATE INDEX "#__finder_links_idx_language" on "#__finder_links" ("language");
--- finder_links_terms tables
+-- finder_links_terms table
 ALTER TABLE "#__finder_links_terms" ALTER COLUMN "weight" SET DEFAULT 0;
 -- finder_taxonomy table
 ALTER TABLE "#__finder_taxonomy" ALTER COLUMN "title" SET DEFAULT '';
@@ -38,7 +38,6 @@ CREATE INDEX "#__finder_tokens_idx_stem" on "#__finder_tokens" ("stem");
 CREATE INDEX "#__finder_tokens_idx_language" on "#__finder_tokens" ("language");
 -- finder_tokens_aggregate table
 ALTER TABLE "#__finder_tokens_aggregate" ALTER COLUMN "term_id" SET DEFAULT 0;
-ALTER TABLE "#__finder_tokens_aggregate" ALTER COLUMN "map_suffix" SET DEFAULT '';
 ALTER TABLE "#__finder_tokens_aggregate" ALTER COLUMN "term" SET DEFAULT '';
 ALTER TABLE "#__finder_tokens_aggregate" ALTER COLUMN "stem" SET DEFAULT '';
 ALTER TABLE "#__finder_tokens_aggregate" ALTER COLUMN "term_weight" SET DEFAULT 0;
