f395c6550dce9acd9d16443967fa5c4825f5ba79
Brian Teeman <brian@teeman.net>
2018-07-04 13:46:27 +0100

    [4.0] [a11y] aria-required
    The attribute aria-required is redundant in html5 if the required attribute is already set
    
    http://html5doctor.com/on-html-belts-and-aria-braces/
    
    There is one js file changes @dgrammatiko please check
    
    There is one file still to update that I would like some help on components\com_contact\layouts\joomla\form\renderfield.php

--- layouts/joomla/form/field/text.php
+++ layouts/joomla/form/field/text.php
@@ -66,7 +66,7 @@ $attributes = array(
 	strlen($hint) ? 'placeholder="' . htmlspecialchars($hint, ENT_COMPAT, 'UTF-8') . '"' : '',
 	$onchange ? ' onchange="' . $onchange . '"' : '',
 	!empty($maxLength) ? $maxLength : '',
-	$required ? 'required aria-required="true"' : '',
+	$required ? 'required' : '',
 	$autocomplete,
 	$autofocus ? ' autofocus' : '',
 	$spellcheck ? '' : 'spellcheck="false"',
