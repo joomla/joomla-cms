5510c5ef9f460abd93bbd3a1ff2baa10f7bdd4b7
Brian Teeman <brian@teeman.net>
2018-07-05 19:30:52 +0100

    my bad

--- administrator/components/com_redirect/tmpl/links/default_batch_body.php
+++ administrator/components/com_redirect/tmpl/links/default_batch_body.php
@@ -18,7 +18,7 @@ $separator = $params->get('separator', '|');
 		<div class="control-group col-md-12">
             <p><?php echo JText::sprintf('COM_REDIRECT_BATCH_TIP', $separator); ?></p>
 			<div class="controls">
-				<textarea class="col-md-12" rows="10" value="" id="batch_urls" name="batch_urls"></textarea>
+				<textarea class="col-md-12" rows="10" value="" id="batch_urls" name="batch_urls" required></textarea>
 			</div>
 		</div>
 	</div>
