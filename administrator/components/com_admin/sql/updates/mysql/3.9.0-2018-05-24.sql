95f3b6dee51339ff9155700a7f7e1a9a3488397b
Brian Teeman <brian@teeman.net>
2018-07-09 00:11:01 +0100

    disabled by default (#194)
    * disabled by default
    
    pr for #178
    
    * revert plg_privacy_user

--- administrator/components/com_admin/sql/updates/mysql/3.9.0-2018-05-24.sql
+++ administrator/components/com_admin/sql/updates/mysql/3.9.0-2018-05-24.sql
@@ -1,5 +1,5 @@
 INSERT INTO `#__extensions` (`extension_id`, `package_id`, `name`, `type`, `element`, `folder`, `client_id`, `enabled`, `access`, `protected`, `manifest_cache`, `params`, `custom_data`, `system_data`, `checked_out`, `checked_out_time`, `ordering`, `state`) VALUES
-(485, 0, 'plg_system_privacyconsent', 'plugin', 'privacyconsent', 'system', 0, 1, 1, 0, '', '{}', '', '', 0, '0000-00-00 00:00:00', 0, 0);
+(485, 0, 'plg_system_privacyconsent', 'plugin', 'privacyconsent', 'system', 0, 0, 1, 0, '', '{}', '', '', 0, '0000-00-00 00:00:00', 0, 0);
 
 CREATE TABLE IF NOT EXISTS `#__privacy_consents` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
