81414f9f49513a2fed0fa208e3f468692e4ea197
Jean-Marie Simonet <infografjms@gmail.com>
2018-07-10 15:17:12 +0200

    cs

--- libraries/src/MVC/Model/AdminModel.php
+++ libraries/src/MVC/Model/AdminModel.php
@@ -1552,10 +1552,10 @@ abstract class AdminModel extends FormModel
 		// Deal with categories associations
 		if ($this->text_prefix === 'COM_CATEGORIES')
 		{
-			$extension        = $app->input->get('extension', 'com_content');
-			$this->typeAlias  = $extension . '.category';
-			$component        = strtolower($this->text_prefix);
-			$view             = 'category';
+			$extension       = $app->input->get('extension', 'com_content');
+			$this->typeAlias = $extension . '.category';
+			$component       = strtolower($this->text_prefix);
+			$view            = 'category';
 		}
 		else
 		{
