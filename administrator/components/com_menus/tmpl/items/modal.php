366613431ffa7ece02b30fb0c61c76a037421422
Brian Teeman <brian@teeman.net>
2018-07-12 09:55:27 +0100

    Update modal.php

--- administrator/components/com_menus/tmpl/items/modal.php
+++ administrator/components/com_menus/tmpl/items/modal.php
@@ -16,7 +16,6 @@ use Joomla\CMS\Language\Multilanguage;
 use Joomla\CMS\Layout\LayoutHelper;
 use Joomla\CMS\Session\Session;
 use Joomla\CMS\Factory;
-use Joomla\CMS\HTML\HTMLHelper;
 
 $app = Factory::getApplication();
 
