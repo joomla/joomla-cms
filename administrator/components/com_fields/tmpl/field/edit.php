cbb459ec2ef51dea0bbe9a1363c19572e719d6f5
Brian Teeman <brian@teeman.net>
2018-07-13 13:51:36 +0100

    Update edit.php

--- administrator/components/com_fields/tmpl/field/edit.php
+++ administrator/components/com_fields/tmpl/field/edit.php
@@ -13,7 +13,6 @@ use Joomla\CMS\Language\Text;
 use Joomla\CMS\Router\Route;
 use Joomla\CMS\Layout\LayoutHelper;
 use Joomla\CMS\Factory;
-use Joomla\CMS\HTML\HTMLHelper;
 
 // Include the component HTML helpers.
 HTMLHelper::addIncludePath(JPATH_COMPONENT . '/helpers/html');
