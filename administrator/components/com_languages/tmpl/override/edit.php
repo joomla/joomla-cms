00831e47b51300f5fabf2e61206b282f90ab84bf
Brian Teeman <brian@teeman.net>
2018-07-13 08:52:25 +0100

    Update edit.php

--- administrator/components/com_languages/tmpl/override/edit.php
+++ administrator/components/com_languages/tmpl/override/edit.php
@@ -12,7 +12,6 @@ defined('_JEXEC') or die;
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\Router\Route;
-use Joomla\CMS\HTML\HTMLHelper;
 
 HTMLHelper::addIncludePath(JPATH_COMPONENT . '/helpers/html');
 
