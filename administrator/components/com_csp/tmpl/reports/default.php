f20837b6794d0306fcaddc6510ee505c8749610b
Brian Teeman <brian@teeman.net>
2018-07-15 19:11:15 +0100

    [4.0] Namespace com_CSP (#21130)
    Missed a few use statements

--- administrator/components/com_csp/tmpl/reports/default.php
+++ administrator/components/com_csp/tmpl/reports/default.php
@@ -12,6 +12,8 @@ defined('_JEXEC') or die;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\Router\Route;
 use Joomla\CMS\Layout\LayoutHelper;
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Factory;
 
 HTMLHelper::_('behavior.multiselect');
 HTMLHelper::_('behavior.tabstate');
