91a6b472cd9b69ad20becc9764254d39625cbb81
Brian Teeman <brian@teeman.net>
2018-07-17 15:28:14 +0100

    [4.0] Update table list view css/js
    This pr addresses the following issues
    
     - After #21127 the checkall box in the table header was misaligned
     - After #21127 when selectin a row (or select all) the cell with the article title (which has changed to a th) was not highlighted
     - Any thead that is not a link was in bold when the rest are not. Example is the Usergroups column header index.php?option=com_users&view=users

--- media/system/js/multiselect.js
+++ media/system/js/multiselect.js
@@ -61,17 +61,23 @@ Joomla = window.Joomla || {};
 
 		var rows = document.querySelectorAll('tr[class^="row"]');
 
-		// Changes the background-color on every <td> inside a <tr>
+		// Changes the background-color on every cell inside a <tr>
 		function changeBg(item, checkall) {
 			// Check if it should add or remove the background colour
 			if (checkall.checked) {
 				item.querySelectorAll('td').forEach (function(td) {
 					td.classList.add('row-selected');
 				});
+				item.querySelectorAll('tr').forEach (function(tr) {
+					tr.classList.add('row-selected');
+				});
 			}
 			else {
 				item.querySelectorAll('td').forEach (function(td) {
 					td.classList.remove('row-selected');
+				});
+				item.querySelectorAll('tr').forEach (function(tr) {
+					tr.classList.remove('row-selected');
 				});
 			}
 		}
