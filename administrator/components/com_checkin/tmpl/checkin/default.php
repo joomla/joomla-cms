9463959f833538fe7670ffa782aefc75e2c3d7c2
Brian Teeman <brian@teeman.net>
2018-07-17 09:52:35 +0100

    com_checkin

--- administrator/components/com_checkin/tmpl/checkin/default.php
+++ administrator/components/com_checkin/tmpl/checkin/default.php
@@ -31,9 +31,15 @@ $listDirn  = $this->escape($this->state->get('list.direction'));
 					<table id="global-checkin" class="table">
 						<thead>
 							<tr>
-								<th style="width:1%"><?php echo HTMLHelper::_('grid.checkall'); ?></th>
-								<th><?php echo HTMLHelper::_('searchtools.sort', 'COM_CHECKIN_DATABASE_TABLE', 'table', $listDirn, $listOrder); ?></th>
-								<th><?php echo HTMLHelper::_('searchtools.sort', 'COM_CHECKIN_ITEMS_TO_CHECK_IN', 'count', $listDirn, $listOrder); ?></th>
+								<td style="width:1%">
+									<?php echo HTMLHelper::_('grid.checkall'); ?>
+								</td>
+								<th scope="col">
+									<?php echo HTMLHelper::_('searchtools.sort', 'COM_CHECKIN_DATABASE_TABLE', 'table', $listDirn, $listOrder); ?>
+								</th>
+								<th scope="col">
+									<?php echo HTMLHelper::_('searchtools.sort', 'COM_CHECKIN_ITEMS_TO_CHECK_IN', 'count', $listDirn, $listOrder); ?>
+								</th>
 							</tr>
 						</thead>
 						<tfoot>
@@ -47,12 +53,14 @@ $listDirn  = $this->escape($this->state->get('list.direction'));
 							<?php $i = 0; ?>
 							<?php foreach ($this->items as $table => $count) : ?>
 								<tr class="row<?php echo $i % 2; ?>">
-									<td class="text-center"><?php echo HTMLHelper::_('grid.id', $i, $table); ?></td>
-									<td>
+									<td class="text-center">
+										<?php echo HTMLHelper::_('grid.id', $i, $table); ?>
+									</td>
+									<th scope="row">
 										<label for="cb<?php echo $i ?>">
 											<?php echo Text::sprintf('COM_CHECKIN_TABLE', $table); ?>
 										</label>
-									</td>
+									</th>
 									<td>
 										<span class="badge badge-secondary"><?php echo $count; ?></span>
 									</td>
