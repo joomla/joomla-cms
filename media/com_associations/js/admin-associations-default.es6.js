bd29849a8f8444439f0345ae6de6a86865fe7d4f
dGrammatiko <mit505@upshift.gr>
2018-07-19 17:17:30 +0200

    [4.0] Com_association js cleanup, fixes (#21176)
    * wip associat
    
    * do not lint the es5 files,
    
    * cs
    
    * more cs
    
    * cs again
    
    * fixes
    
    * allow selecting an associated item
    
    * cs
    
    * Remove commented code

--- media/com_associations/js/admin-associations-default.es6.js
+++ media/com_associations/js/admin-associations-default.es6.js
@@ -1,13 +1,8 @@
 /**
-* PLEASE DO NOT MODIFY THIS FILE. WORK ON THE ES6 VERSION.
-* OTHERWISE YOUR CHANGES WILL BE REPLACED ON THE NEXT BUILD.
-**/
-
-/**
  * @copyright   Copyright (C) 2005 - 2018 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
-Joomla.submitbutton = function (pressbutton) {
+Joomla.submitbutton = (pressbutton) => {
   if (pressbutton === 'associations.purge') {
     // eslint-disable-next-line no-restricted-globals
     if (confirm(Joomla.JText._('COM_ASSOCIATIONS_PURGE_CONFIRM_PROMPT'))) {
