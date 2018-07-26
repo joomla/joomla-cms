70c11dfc4f201e5d32ab346d1dcbc6ffac78773e
PaulinaNiederhoefer <paulina.niederhoefer@mni.thm.de>
2018-07-26 15:08:07 +0200

    restored if condition because there was no need for a change => changed by mistake

--- administrator/components/com_workflow/Model/StateModel.php
+++ administrator/components/com_workflow/Model/StateModel.php
@@ -265,7 +265,7 @@ class StateModel extends AdminModel
 
 		if ($table->load(array('id' => $pk)))
 		{
-			if ($table->published == 0)
+			if ($table->published !== 1)
 			{
 				$this->setError(\JText::_("COM_WORKFLOW_ITEM_MUST_PUBLISHED"));
 
