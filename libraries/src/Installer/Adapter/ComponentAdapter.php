8627bccee789ce71f0d2e40849ee0a6e6f6a2b75
Clay Freeman <git@clayfreeman.com>
2018-07-30 09:48:07 -0500

    modify installer to allow additional query params
    `Joomla\CMS\Installer\Adapter\ComponentAdapter` should allow (at a minimum) the
    same subset of query parameters for primary admin menu items that is allowed for
    submenu items.
    
    If desired, this can be expanded to allow full `link` override in the manifest
    file like currently possible for submenu items.

--- libraries/src/Installer/Adapter/ComponentAdapter.php
+++ libraries/src/Installer/Adapter/ComponentAdapter.php
@@ -971,7 +971,6 @@ class ComponentAdapter extends InstallerAdapter
 			$data['client_id'] = 1;
 			$data['title'] = (string) trim($menuElement);
 			$data['alias'] = (string) $menuElement;
-			$data['link'] = 'index.php?option=' . $option;
 			$data['type'] = 'component';
 			$data['published'] = 1;
 			$data['parent_id'] = 1;
@@ -980,6 +979,42 @@ class ComponentAdapter extends InstallerAdapter
 			$data['home'] = 0;
 			$data['path'] = '';
 			$data['params'] = '';
+
+			// Set the menu link
+			$request = array();
+
+			if ((string) $menuElement->attributes()->act)
+			{
+				$request[] = 'act=' . $menuElement->attributes()->act;
+			}
+
+			if ((string) $menuElement->attributes()->task)
+			{
+				$request[] = 'task=' . $menuElement->attributes()->task;
+			}
+
+			if ((string) $menuElement->attributes()->controller)
+			{
+				$request[] = 'controller=' . $menuElement->attributes()->controller;
+			}
+
+			if ((string) $menuElement->attributes()->view)
+			{
+				$request[] = 'view=' . $menuElement->attributes()->view;
+			}
+
+			if ((string) $menuElement->attributes()->layout)
+			{
+				$request[] = 'layout=' . $menuElement->attributes()->layout;
+			}
+
+			if ((string) $menuElement->attributes()->sub)
+			{
+				$request[] = 'sub=' . $menuElement->attributes()->sub;
+			}
+
+			$qstring = count($request) ? '&' . implode('&', $request) : '';
+			$data['link'] = 'index.php?option=' . $option . $qstring;
 		}
 		else
 		{
