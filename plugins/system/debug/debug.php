2ccb8449a829aee742830219b8a89723c81f877b
Nikolai Plath <der.el.kuku@gmail.com>
2018-07-31 10:09:31 -0500

    Let's fetch the files from the 'right' place...

--- plugins/system/debug/debug.php
+++ plugins/system/debug/debug.php
@@ -323,7 +323,7 @@ class PlgSystemDebug extends CMSPlugin
 		$html[] = '</div>';
 
 		$debugBarRenderer = $this->debugBar->getJavascriptRenderer();
-		$debugBarRenderer->setBaseUrl(JUri::root(true) . '/media/plg_system_debug/debugbar/');
+		$debugBarRenderer->setBaseUrl(JUri::root(true) . '/media/vendor/debugbar/');
 
 		$contents = str_replace('</head>', $debugBarRenderer->renderHead() . '</head>', $contents);
 
