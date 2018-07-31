674db82e4a4117db9b2ac1e2b88ff26a540ccd1e
Dieudonne Timma <dieudonne.timma.meyatchie@mni.thm.de>
2018-07-31 15:49:49 +0200

    fixed coding standard

--- administrator/components/com_menus/View/Items/HtmlView.php
+++ administrator/components/com_menus/View/Items/HtmlView.php
@@ -210,17 +210,16 @@ class HtmlView extends BaseHtmlView
 							else
 							{
 								// Get XML file from component folder for standard layouts
-                                $file = JPATH_SITE . '/components/' . $item->componentname . '/tmpl/' . $vars['view']
+								$file = JPATH_SITE . '/components/' . $item->componentname . '/tmpl/' . $vars['view']
 	                                . '/' . $vars['layout'] . '.xml';
-
 								if (!file_exists($file))
 								{
 									$file = JPATH_SITE . '/components/' . $item->componentname . '/views/'
 										. $vars['view'] . '/tmpl/' . $vars['layout'] . '.xml';
-								}else{
+								}
+								else{
 									$file = JPATH_SITE . '/components/' . $item->componentname . '/view/'
 										. $vars['view'] . '/tmpl/' . $vars['layout'] . '.xml';
-
 								}
 							}
 
