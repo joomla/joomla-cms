5c282dbd6676bdd7c4287c3def35299cb7fd43a2
SharkyKZ <sharkykz@gmail.com>
2018-08-01 10:03:02 +0300

    [mod_articles_latest/news] Featured articles not shown

--- modules/mod_articles_news/helper.php
+++ modules/mod_articles_news/helper.php
@@ -61,18 +61,20 @@ abstract class ModArticlesNewsHelper
 		// Filer by tag
 		$model->setState('filter.tag', $params->get('tag', array()));
 
-		//  Featured switch
-		switch ($params->get('show_featured'))
+		// Featured switch
+		$featured = $params->get('show_featured', '');
+
+		if ($featured === '')
 		{
-			case 1 :
-				$model->setState('filter.featured', 'only');
-				break;
-			case 0 :
-				$model->setState('filter.featured', 'hide');
-				break;
-			default :
-				$model->setState('filter.featured', 'show');
-				break;
+			$model->setState('filter.featured', 'show');
+		}
+		elseif ($featured)
+		{
+			$model->setState('filter.featured', 'only');
+		}
+		else
+		{
+			$model->setState('filter.featured', 'hide');
 		}
 
 		// Set ordering
