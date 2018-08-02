282c12b20e0f5fe04a03edb9054965126864db8c
Nikolai Plath <der.el.kuku@gmail.com>
2018-08-02 11:22:22 -0500

    Fix a doc block

--- components/com_content/tmpl/debug/default.php
+++ components/com_content/tmpl/debug/default.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * @package     ${NAMESPACE}
- * @subpackage
+ * @package     Joomla.Site
+ * @subpackage  com_content
  *
- * @copyright   A copyright
- * @license     A "Slug" license name e.g. GPL2
+ * @copyright   Copyright (C) 2018 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 ?>
