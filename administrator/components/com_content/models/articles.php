c9e2fa9ce73892093685679392d45e4875d57663
SharkyKZ <sharkykz@gmail.com>
2018-08-03 01:30:36 +0300

    [com_content] Redundant access check (#21169)
    * [com_content] Redundant access check
    
    * Cleanup
    
    * Cleanup

--- administrator/components/com_content/models/articles.php
+++ administrator/components/com_content/models/articles.php
@@ -409,33 +409,4 @@ class ContentModelArticles extends JModelList
 		// Return the result
 		return $db->loadObjectList();
 	}
-
-	/**
-	 * Method to get a list of articles.
-	 * Overridden to add a check for access levels.
-	 *
-	 * @return  mixed  An array of data items on success, false on failure.
-	 *
-	 * @since   1.6.1
-	 */
-	public function getItems()
-	{
-		$items = parent::getItems();
-
-		if (JFactory::getApplication()->isClient('site'))
-		{
-			$groups = JFactory::getUser()->getAuthorisedViewLevels();
-
-			foreach (array_keys($items) as $x)
-			{
-				// Check the access level. Remove articles the user shouldn't see
-				if (!in_array($items[$x]->access, $groups))
-				{
-					unset($items[$x]);
-				}
-			}
-		}
-
-		return $items;
-	}
 }
