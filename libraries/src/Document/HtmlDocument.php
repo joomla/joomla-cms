cb76f6b180cfe776f27284d2d13b812637b3d5bd
Fedik <getthesite@gmail.com>
2018-08-04 13:17:26 +0300

    c s

--- libraries/src/Document/HtmlDocument.php
+++ libraries/src/Document/HtmlDocument.php
@@ -561,8 +561,8 @@ class HtmlDocument extends Document
 	/**
 	 * Count the modules in the given position
 	 *
-	 * @param   string  $positionName      The position to use
-	 * @param   bool    $withContentOnly   Count only a modules which actually has a content
+	 * @param   string  $positionName     The position to use
+	 * @param   bool    $withContentOnly  Count only a modules which actually has a content
 	 *
 	 * @return  integer  Number of modules found
 	 *
