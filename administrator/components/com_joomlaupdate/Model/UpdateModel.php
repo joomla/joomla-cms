b8155c5cb60b89b4f6027211543c2b2f03fd3e80
Anu1601CS <anuragvns1111@gmail.com>
2018-08-05 18:12:23 +0530

    namespace

--- administrator/components/com_joomlaupdate/Model/UpdateModel.php
+++ administrator/components/com_joomlaupdate/Model/UpdateModel.php
@@ -26,6 +26,7 @@ use Joomla\CMS\Filesystem\Path;
 use Joomla\CMS\Log\Log;
 use Joomla\CMS\Http\HttpFactory;
 use Joomla\CMS\Installer\Installer;
+use Joomla\CMS\Plugin\PluginHelper;
 
 jimport('joomla.filesystem.folder');
 jimport('joomla.filesystem.file');
@@ -408,7 +409,7 @@ class UpdateModel extends BaseDatabaseModel
 	public function createRestorationFile($basename = null)
 	{
 		// Load overrides plugin.
-		\JPluginHelper::importPlugin('installer');
+		PluginHelper::importPlugin('installer');
 
 		// Get a password
 		$password = UserHelper::genRandomPassword(32);
@@ -850,9 +851,9 @@ ENDDATA;
 	public function cleanUp()
 	{
 		// Load overrides plugin.
-		\JPluginHelper::importPlugin('installer');
+		PluginHelper::importPlugin('installer');
 
-		$app = \JFactory::getApplication();
+		$app = Factory::getApplication();
 
 		// Trigger on before joomla event.
 		$app->triggerEvent('onJoomlaAfterUpdate');
