916eff7301ff4f615805e26efef36e460e057f36
Brian Teeman <brian@teeman.net>
2018-08-07 11:44:53 +0100

    Help Docs link - add a link to the docs site and meaningful text for screenreaders to the "lightbulb" [a11y]

--- installation/template/index.php
+++ installation/template/index.php
@@ -58,8 +58,9 @@ Text::script('INSTL_FTP_SETTINGS_CORRECT');
 					<img src="<?php echo $this->baseurl; ?>/template/images/logo.svg" alt="" class="logo"/>
 				</div>
 				<div class="j-header-help">
-					<a href="#">
+					<a href="https://docs.joomla.org/Special:MyLanguage/J4.x:Installing_Joomla">
 						<span class="fa fa-lightbulb-o" aria-hidden="true"></span>
+						<span class="sr-only"><?php echo Text::_('INSTL_HELP_LINK'); ?></span>
 					</a>
 				</div>
 			</header>
