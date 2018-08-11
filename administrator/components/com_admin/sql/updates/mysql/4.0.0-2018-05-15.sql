95d194f7907b4739fb226cf44ddc622fc5dc4991
Nicola Galgano <optimus4joomla@gmail.com>
2018-08-11 09:06:31 +0200

    the state alter

--- administrator/components/com_admin/sql/updates/mysql/4.0.0-2018-05-15.sql
+++ administrator/components/com_admin/sql/updates/mysql/4.0.0-2018-05-15.sql
@@ -122,3 +122,5 @@ INSERT INTO `#__extensions` (`extension_id`, `package_id`, `name`, `type`, `elem
 --
 INSERT INTO `#__workflow_associations` (`item_id`, `state_id`, `extension`)
 SELECT `id`, CASE WHEN `state` = -2 THEN 3 WHEN `state` = 0 THEN 1 WHEN `state` = 2 THEN 4 ELSE 2 END, 'com_content' FROM `#__content`;
+                   
+ALTER TABLE #__content` MODIFY `state` INTEGER;
