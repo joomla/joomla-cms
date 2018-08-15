57542fa8eea2e3ebf087be044edcf210fd62dfc0
Nicola Galgano <optimus4joomla@gmail.com>
2018-08-15 09:12:57 +0200

    [4.0] [workflow] - notice on edit plugin

--- layouts/joomla/edit/global.php
+++ layouts/joomla/edit/global.php
@@ -40,11 +40,6 @@ $fields = $displayData->get('fields') ?: array(
 	'version_note',
 );
 
-if ($displayData->get('item')->id !== null)
-{
-	array_unshift($fields, 'transition');
-}
-
 $hiddenFields = $displayData->get('hidden_fields') ?: array();
 
 if (!$saveHistory)
