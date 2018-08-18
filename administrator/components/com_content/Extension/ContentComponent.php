899805c4824f18d6eb4c5ac1521d1cb174a4ef89
Benjamin Trenkle <benjamin.trenkle@wicked-software.de>
2018-08-19 01:01:28 +0200

    Fix comment

--- administrator/components/com_content/Extension/ContentComponent.php
+++ administrator/components/com_content/Extension/ContentComponent.php
@@ -180,7 +180,7 @@ class ContentComponent extends MVCComponent implements
 	/**
 	 * Returns a table name for the state association
 	 *
-	 * @param   string  $section  An optional section to differ different areas in the component
+	 * @param   string  $section  An optional section to separate different areas in the component
 	 *
 	 * @return  string
 	 *
