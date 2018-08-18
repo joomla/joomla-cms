f3712ecfa56280d3b7fde0a80762830c1048e0a1
infograf768 <infografjms@gmail.com>
2018-08-18 10:31:18 +0000

    [4.0] Fixing contacts routing in multilingual sites (#21679)

--- components/com_contact/tmpl/contact/default.php
+++ components/com_contact/tmpl/contact/default.php
@@ -45,7 +45,7 @@ $tparams = $this->item->params;
 			<span class="contact-category"><?php echo $this->contact->category_title; ?></span>
 		</h3>
 	<?php elseif ($show_contact_category === 'show_with_link') : ?>
-		<?php $contactLink = ContactHelperRoute::getCategoryRoute($this->contact->catid); ?>
+		<?php $contactLink = ContactHelperRoute::getCategoryRoute($this->contact->catid, $this->contact->language); ?>
 		<h3>
 			<span class="contact-category"><a href="<?php echo $contactLink; ?>">
 				<?php echo $this->escape($this->contact->category_title); ?></a>
