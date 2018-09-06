8d5313ef285c3b3a196e0a0fb0afabf9842857f8
Jean-Marie Simonet <infografjms@gmail.com>
2018-09-06 14:12:08 +0200

    multiple lines

--- build/media_src/plg_quickicon_joomlaupdate/js/jupdatecheck.es6.js
+++ build/media_src/plg_quickicon_joomlaupdate/js/jupdatecheck.es6.js
@@ -34,7 +34,9 @@
             if (updateInfo.version !== options.version) {
               const messages = {
                 message: [
-                  `${Joomla.JText._('PLG_QUICKICON_JOOMLAUPDATE_UPDATEFOUND_MESSAGE').replace('%s', `<span class="badge badge-pill badge-danger">${updateInfoList.length}</span>`)}<button class="btn btn-primary" onclick="document.location='${options.url}'">${Joomla.JText._('PLG_QUICKICON_EXTENSIONUPDATE_UPDATEFOUND_BUTTON')}</button>`,
+                  `${Joomla.JText._('PLG_QUICKICON_JOOMLAUPDATE_UPDATEFOUND_MESSAGE').replace('%s', `<span class="badge badge-danger">${updateInfoList.length}</span>`)}`
+                  + `<button class="btn btn-primary" onclick="document.location='${options.url}'">`
+                  + `${Joomla.JText._('PLG_QUICKICON_JOOMLAUPDATE_UPDATEFOUND_BUTTON')}</button>`,
                 ],
                 error: ['info'],
               };
