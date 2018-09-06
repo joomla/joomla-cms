8ed4f605345766edab05271fdd153f1f62093f47
Michael Gilkes <jaido7@yahoo.com>
2018-09-06 17:13:24 -0400

    Fix for exclude attribute in folderlist field
    Currently, the exclude attribute for the folderlist does not work. This is because the list of folders that are tested against the regular expression in exclude contains the full folder path and not the folder name only. So, instead of removing the root part after you test for the exclude, remove the root path before.

--- libraries/joomla/form/fields/folderlist.php
+++ libraries/joomla/form/fields/folderlist.php
@@ -212,6 +212,9 @@ class JFormFieldFolderList extends JFormFieldList
 		{
 			foreach ($folders as $folder)
 			{
+				// Remove the root part and the leading /
+				$folder = trim(str_replace($path, '', $folder), '/');
+
 				// Check to see if the file is in the exclude mask.
 				if ($this->exclude)
 				{
@@ -220,9 +223,6 @@ class JFormFieldFolderList extends JFormFieldList
 						continue;
 					}
 				}
-
-				// Remove the root part and the leading /
-				$folder = trim(str_replace($path, '', $folder), '/');
 
 				$options[] = JHtml::_('select.option', $folder, $folder);
 			}
