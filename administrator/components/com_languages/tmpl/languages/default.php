fff581f03a8f1f3930a5e3ed9308c507f42d6a13
Brian Teeman <brian@teeman.net>
2018-09-07 11:19:27 +0100

    remove space

--- administrator/components/com_languages/tmpl/languages/default.php
+++ administrator/components/com_languages/tmpl/languages/default.php
@@ -48,7 +48,7 @@ if ($saveOrder)
 								<th scope="col" style="width:1%" class="text-center d-none d-md-table-cell">
 									<?php echo HTMLHelper::_('searchtools.sort', '', 'a.ordering', $listDirn, $listOrder, null, 'asc', 'JGRID_HEADING_ORDERING', 'icon-menu-2'); ?>
 								</th>
-								<td style="width:1%"  class="text-center">
+								<td style="width:1%" class="text-center">
 									<?php echo HTMLHelper::_('grid.checkall'); ?>
 								</td>
 								<th scope="col" style="width:1%" class="text-center">
