39c25c7f55a0d9e9af9ca445db7c54fc3e5d2eb7
Nicola Galgano <optimus4joomla@gmail.com>
2018-09-08 17:18:30 +0200

    the last quote
    thanks @sanderpotjer

--- plugins/system/privacyconsent/privacyconsent.php
+++ plugins/system/privacyconsent/privacyconsent.php
@@ -615,7 +615,7 @@ class PlgSystemPrivacyconsent extends JPlugin
 		$query = $db->getQuery(true);
 		$query->select($db->quoteName(array('id', 'user_id')))
 			->from($db->quoteName('#__privacy_consents'))
-			->where($query->dateAdd($now, $period, 'DAY') . ' > ' . $db->quoteName('created'))
+			->where($query->dateAdd($db->quote($now), $period, 'DAY') . ' > ' . $db->quoteName('created'))
 			->where($db->quoteName('subject') . ' = ' . $query->quote('PLG_SYSTEM_PRIVACYCONSENT_SUBJECT'))
 			->where($db->quoteName('state') . ' = 1');
 		$db->setQuery($query);
