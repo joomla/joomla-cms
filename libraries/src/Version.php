f27beadc61a767a78104c6721727b70f558cfc77
Michael Babker <michael.babker@gmail.com>
2018-09-09 12:24:29 -0500

    Correct version string

--- libraries/src/Version.php
+++ libraries/src/Version.php
@@ -60,7 +60,7 @@ final class Version
 	 * @var    string
 	 * @since  3.8.0
 	 */
-	const EXTRA_VERSION = 'alpha2-dev';
+	const EXTRA_VERSION = 'dev';
 
 	/**
 	 * Release version.
@@ -78,7 +78,7 @@ final class Version
 	 * @since  3.5
 	 * @deprecated  4.0  Use separated version constants instead
 	 */
-	const DEV_LEVEL = '0-alpha2-dev';
+	const DEV_LEVEL = '0-dev';
 
 	/**
 	 * Development status.
