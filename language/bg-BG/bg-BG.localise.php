34817d63d2257a96b762a5cd26ef45afc1bd9cb1
Release-Joomla <42966168+release-joomla@users.noreply.github.com>
2018-09-10 19:40:16 +0200

    New translations xx-XX.localise.txt (Bulgarian)

---
+++ language/bg-BG/bg-BG.localise.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * @package    Joomla.Language
+ *
+ * @copyright  Copyright (C) 2005 - 2018 Open Source Matters, Inc. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+/**
+ * bg-BG localise class.
+ *
+ * @since  1.6
+ */
+abstract class bg_BGLocalise
+{
+	/**
+	 * Returns the potential suffixes for a specific number of items
+	 *
+	 * @param   integer  $count  The number of items.
+	 *
+	 * @return  array  An array of potential suffixes.
+	 *
+	 * @since   1.6
+	 */
+	public static function getPluralSuffixes($count)
+	{
+		if ($count == 0)
+		{
+			return array('0');
+		}
+		elseif ($count == 1)
+		{
+			return array('1');
+		}
+		else
+		{
+			return array('MORE');
+		}
+	}
+
+	/**
+	 * Returns the ignored search words
+	 *
+	 * @return  array  An array of ignored search words.
+	 *
+	 * @since   1.6
+	 */
+	public static function getIgnoredSearchWords()
+	{
+		$search_ignore = array();
+$search_ignore[] = "и";
+$search_ignore[] = "в";
+$search_ignore[] = "или";
+$search_ignore[] = "на";
+$search_ignore[] = "за";
+$search_ignore[] = "по";
+$search_ignore[] = "с";
+return $search_ignore;
+	}
+
+	/**
+	 * Returns the lower length limit of search words
+	 *
+	 * @return  integer  The lower length limit of search words.
+	 *
+	 * @since   1.6
+	 */
+	public static function getLowerLimitSearchWord()
+	{
+		return 2;
+	}
+
+	/**
+	 * Returns the upper length limit of search words
+	 *
+	 * @return  integer  The upper length limit of search words.
+	 *
+	 * @since   1.6
+	 */
+	public static function getUpperLimitSearchWord()
+	{
+		return 100;
+	}
+
+	/**
+	 * Returns the number of chars to display when searching
+	 *
+	 * @return  integer  The number of chars to display when searching.
+	 *
+	 * @since   1.6
+	 */
+	public static function getSearchDisplayedCharactersNumber()
+	{
+		return 1000;
+	}
+}
