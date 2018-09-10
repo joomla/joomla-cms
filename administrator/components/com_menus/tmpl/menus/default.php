699f84b86f8bc635a74a0e7fa7cfec0ea396354e
chmst <dev@chmst.de>
2018-09-10 16:05:18 +0200

    Another pull request for issue 22091. Here the link to the respective menuitems is moved to an own column.

--- administrator/components/com_menus/tmpl/menus/default.php
+++ administrator/components/com_menus/tmpl/menus/default.php
@@ -60,6 +60,9 @@ HTMLHelper::_('script', 'com_menus/admin-menus-default.min.js', array('version' 
 								<th scope="col">
 									<?php echo HTMLHelper::_('searchtools.sort', 'JGLOBAL_TITLE', 'a.title', $listDirn, $listOrder); ?>
 								</th>
+								<th scope="col" style="width:5%" class="nowrap text-center">
+									<?php echo Text::_('COM_MENUS_MENUS'); ?>
+								</th>
 								<th scope="col" style="width:10%" class="nowrap text-center">
 									<span class="icon-publish" aria-hidden="true"></span>
 									<span class="d-none d-md-inline"><?php echo Text::_('COM_MENUS_HEADING_PUBLISHED_ITEMS'); ?></span>
@@ -90,23 +93,25 @@ HTMLHelper::_('script', 'com_menus/admin-menus-default.min.js', array('version' 
 								<td class="text-center">
 									<?php echo HTMLHelper::_('grid.id', $i, $item->id); ?>
 								</td>
-								<th scope="row">
-									<?php if ($canManageItems) : ?>
-										<a href="<?php echo Route::_('index.php?option=com_menus&view=items&menutype=' . $item->menutype); ?>" title="<?php echo Text::_('JACTION_EDIT'); ?> <?php echo $this->escape(addslashes($item->title)); ?>">
-											<span class="fa fa-pencil-square mr-2" aria-hidden="true"></span><?php echo $this->escape($item->title); ?></a>
-									<?php else : ?>
-										<?php echo $this->escape($item->title); ?>
-									<?php endif; ?>
-									<div class="small">
-										<?php echo Text::_('COM_MENUS_MENU_MENUTYPE_LABEL'); ?>:
+								<td>
+									<div class="name break-word">
 										<?php if ($canEdit) : ?>
-											<a href="<?php echo Route::_('index.php?option=com_menus&task=menu.edit&id=' . $item->id); ?>" title="<?php echo $this->escape($item->description); ?>">
-											<?php echo $this->escape($item->menutype); ?></a>
+											<a href="<?php echo Route::_('index.php?option=com_menus&task=menu.edit&id=' . $item->id); ?>">
+												<span class="fa fa-pencil-square mr-2" aria-hidden="true"></span>
+												<span class="sr-only"><?php echo Text::_('COM_MENUS_EDIT_MENU'); ?></span><?php echo $this->escape($item->title); ?>
+											</a>
 										<?php else : ?>
-											<?php echo $this->escape($item->menutype); ?>
+											<?php echo $this->escape($item->title); ?>
 										<?php endif; ?>
 									</div>
-								</th>
+								</td>
+								<td class="text-center btns">
+									<?php if ($canManageItems) : ?>
+										<a href="<?php echo Route::_('index.php?option=com_menus&view=items&menutype=' . $item->menutype); ?>">
+											<span class="fa fa-list text-center" aria-hidden="true"></span><span class="sr-only"><?php echo Text::_('COM_MENUS_MENUS'); ?></span>
+										</a>
+									<?php endif; ?>
+								</td>
 								<td class="text-center btns">
 									<?php if ($canManageItems) : ?>
 										<a class="badge<?php echo ($item->count_published > 0) ? ' badge-success' : ' badge-secondary'; ?>" href="<?php echo Route::_('index.php?option=com_menus&view=items&menutype=' . $item->menutype . '&filter[published]=1'); ?>">
