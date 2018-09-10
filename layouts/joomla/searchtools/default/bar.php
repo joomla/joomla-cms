77978257a66ace1191ba3f30f18cf8ff9be278e8
Brian Teeman <brian@teeman.net>
2018-09-10 07:45:41 +0100

    [4.0] [a11y] Move search label (#21985)
    Simple PR to move the search label (visually hidden) to being in the same div as the search input in the toolbar. This will help to improve accessibility

--- layouts/joomla/searchtools/default/bar.php
+++ layouts/joomla/searchtools/default/bar.php
@@ -32,16 +32,16 @@ $filters = $data['view']->filterForm->getGroup('filter');
 
 <?php if (!empty($filters['filter_search'])) : ?>
 	<?php if ($searchButton) : ?>
-		<label for="filter_search" class="sr-only">
-			<?php if (isset($filters['filter_search']->label)) : ?>
-				<?php echo Text::_($filters['filter_search']->label); ?>
-			<?php else : ?>
-				<?php echo Text::_('JSEARCH_FILTER'); ?>
-			<?php endif; ?>
-		</label>
 		<div class="btn-toolbar">
 			<div class="btn-group mr-2">
 				<div class="input-group">
+					<label for="filter_search" class="sr-only">
+						<?php if (isset($filters['filter_search']->label)) : ?>
+							<?php echo Text::_($filters['filter_search']->label); ?>
+						<?php else : ?>
+							<?php echo Text::_('JSEARCH_FILTER'); ?>
+						<?php endif; ?>
+					</label>
 					<?php echo $filters['filter_search']->input; ?>
 					<span class="input-group-append">
 						<button type="submit" class="btn btn-primary hasTooltip" title="<?php echo HTMLHelper::_('tooltipText', 'JSEARCH_FILTER_SUBMIT'); ?>"  aria-label="<?php echo Text::_('JSEARCH_FILTER_SUBMIT'); ?>">
