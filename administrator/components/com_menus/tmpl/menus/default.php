240d1c8ea9d4a085bc2177a39bf0883a3e671f33
chmst <dev@chmst.de>
2018-09-11 09:43:22 +0200

    Add " before text-center, break line

--- administrator/components/com_menus/tmpl/menus/default.php
+++ administrator/components/com_menus/tmpl/menus/default.php
@@ -63,7 +63,7 @@ HTMLHelper::_('script', 'com_menus/admin-menus-default.min.js', array('version' 
 								<th scope="col" style="width:5%" class="text-center">
 									<?php echo Text::_('COM_MENUS_MENUS'); ?>
 								</th>
-								<th scope="col" style="width:10%" class=text-center">
+								<th scope="col" style="width:10%" class="text-center">
 									<span class="icon-publish" aria-hidden="true"></span>
 									<span class="d-none d-md-inline"><?php echo Text::_('COM_MENUS_HEADING_PUBLISHED_ITEMS'); ?></span>
 								</th>
