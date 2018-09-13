94317765a65b599389e88e35e5f4f922262974b4
coolcat-creations <mail@coolcat-creations.com>
2018-09-13 18:23:47 +0200

    Fix Codestyle

--- administrator/templates/atum/login.php
+++ administrator/templates/atum/login.php
@@ -1,57 +1,56 @@
 <?php
-/**
- * @package     Joomla.Administrator
- * @subpackage  Templates.Atum
- *
- * @copyright   Copyright (C) 2005 - 2018 Open Source Matters, Inc. All rights reserved.
- * @license     GNU General Public License version 2 or later; see LICENSE.txt
- */
+	/**
+	 * @package     Joomla.Administrator
+	 * @subpackage  Templates.Atum
+	 * @copyright   Copyright (C) 2005 - 2018 Open Source Matters, Inc. All rights reserved.
+	 * @license     GNU General Public License version 2 or later; see LICENSE.txt
+	 */
 
-defined('_JEXEC') or die;
+	defined('_JEXEC') or die;
 
-use Joomla\CMS\Factory;
-use Joomla\CMS\Uri\Uri;
-use Joomla\CMS\Language\Text;
-use Joomla\CMS\HTML\HTMLHelper;
+	use Joomla\CMS\Factory;
+	use Joomla\CMS\Uri\Uri;
+	use Joomla\CMS\Language\Text;
+	use Joomla\CMS\HTML\HTMLHelper;
 
-/** @var JDocumentHtml $this */
+	/** @var JDocumentHtml $this */
 
-$app = Factory::getApplication();
-$lang = Factory::getLanguage();
+	$app  = Factory::getApplication();
+	$lang = Factory::getLanguage();
 
-// Add JavaScript Frameworks
-HTMLHelper::_('script', 'vendor/focus-visible/focus-visible.min.js', ['version' => 'auto', 'relative' => true]);
+	// Add JavaScript Frameworks
+	HTMLHelper::_('script', 'vendor/focus-visible/focus-visible.min.js', ['version' => 'auto', 'relative' => true]);
 
-// Load template CSS file
-HTMLHelper::_('stylesheet', 'bootstrap.css', ['version' => 'auto', 'relative' => true]);
-HTMLHelper::_('stylesheet', 'font-awesome.css', ['version' => 'auto', 'relative' => true]);
-HTMLHelper::_('stylesheet', 'template' . ($this->direction === 'rtl' ? '-rtl' : '') . '.css', ['version' => 'auto', 'relative' => true]);
+	// Load template CSS file
+	HTMLHelper::_('stylesheet', 'bootstrap.css', ['version' => 'auto', 'relative' => true]);
+	HTMLHelper::_('stylesheet', 'font-awesome.css', ['version' => 'auto', 'relative' => true]);
+	HTMLHelper::_('stylesheet', 'template' . ($this->direction === 'rtl' ? '-rtl' : '') . '.css', ['version' => 'auto', 'relative' => true]);
 
-// Load custom CSS file
-HTMLHelper::_('stylesheet', 'user.css', array('version' => 'auto', 'relative' => true));
+	// Load custom CSS file
+	HTMLHelper::_('stylesheet', 'user.css', array('version' => 'auto', 'relative' => true));
 
-// Load specific language related CSS
-HTMLHelper::_('stylesheet', 'administrator/language/' . $lang->getTag() . '/' . $lang->getTag() . '.css', array('version' => 'auto'));
+	// Load specific language related CSS
+	HTMLHelper::_('stylesheet', 'administrator/language/' . $lang->getTag() . '/' . $lang->getTag() . '.css', array('version' => 'auto'));
 
-// Detecting Active Variables
-$option = $app->input->getCmd('option', '');
-$view = $app->input->getCmd('view', '');
-$layout = $app->input->getCmd('layout', '');
-$task = $app->input->getCmd('task', '');
-$itemid = $app->input->getCmd('Itemid', '');
-$sitename = $app->get('sitename');
+	// Detecting Active Variables
+	$option   = $app->input->getCmd('option', '');
+	$view     = $app->input->getCmd('view', '');
+	$layout   = $app->input->getCmd('layout', '');
+	$task     = $app->input->getCmd('task', '');
+	$itemid   = $app->input->getCmd('Itemid', '');
+	$sitename = $app->get('sitename');
 
-// Template params
-$showSitename = $this->params->get('showSitename', '1');
-$loginLogo = $this->params->get('loginLogo', '');
+	// Template params
+	$showSitename = $this->params->get('showSitename', '1');
+	$loginLogo    = $this->params->get('loginLogo', '');
 
-// Set some meta data
-$this->setMetaData('viewport', 'width=device-width, initial-scale=1');
-// @TODO sync with _variables.scss
-$this->setMetaData('theme-color', '#1c3d5c');
+	// Set some meta data
+	$this->setMetaData('viewport', 'width=device-width, initial-scale=1');
+	// @TODO sync with _variables.scss
+	$this->setMetaData('theme-color', '#1c3d5c');
 
-// Set page title
-$this->setTitle($sitename . ' - ' . Text::_('JACTION_LOGIN_ADMIN'));
+	// Set page title
+	$this->setTitle($sitename . ' - ' . Text::_('JACTION_LOGIN_ADMIN'));
 
 ?>
 <!DOCTYPE html>
@@ -60,7 +59,6 @@ $this->setTitle($sitename . ' - ' . Text::_('JACTION_LOGIN_ADMIN'));
 	<jdoc:include type="metas"/>
 	<jdoc:include type="styles"/>
 </head>
-
 <body class="site <?php echo $option . ' view-' . $view . ' layout-' . $layout . ' task-' . $task . ' itemid-' . $itemid . ' '; ?>">
 <?php // Container ?>
 <main class="d-flex justify-content-center align-items-center h-100">
@@ -68,7 +66,7 @@ $this->setTitle($sitename . ' - ' . Text::_('JACTION_LOGIN_ADMIN'));
 	<div class="login">
 		<div class="login-logo">
 			<img src="<?php echo $this->baseurl; ?>/templates/<?php echo $this->template; ?>/images/logo-joomla-white.svg"
-			     alt="">
+				 alt="">
 		</div>
 		<div id="content">
 			<noscript>
@@ -97,8 +95,8 @@ $this->setTitle($sitename . ' - ' . Text::_('JACTION_LOGIN_ADMIN'));
 		<div class="mt-4 d-none d-md-flex justify-content-between">
 			<a href="<?php echo Uri::root(); ?>" target="_blank" class="text-white"><span
 						class="fa fa-external-link mr-1"
-						aria-hidden="true"></span><?php echo Text::_('TPL_ATUM_VIEW_SITE'); ?></a>
-			<span class="text-white">&nbsp;&copy; <?php echo date('Y'); ?> <?php echo $sitename; ?></span>
+						aria-hidden="true"></span><?php echo Text::_('TPL_ATUM_VIEW_SITE'); ?></a> <span
+					class="text-white">&nbsp;&copy; <?php echo date('Y'); ?> <?php echo $sitename; ?></span>
 		</div>
 	</div>
 </main>
