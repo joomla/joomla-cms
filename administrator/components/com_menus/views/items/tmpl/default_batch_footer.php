5b1766719597e57504304729a8ec26807f9cab0c
Izhar Aazmi <izharaazmi@gmail.com>
2018-09-14 16:16:52 +0530

    Do it the old way

--- administrator/components/com_menus/views/items/tmpl/default_batch_footer.php
+++ administrator/components/com_menus/views/items/tmpl/default_batch_footer.php
@@ -7,10 +7,14 @@
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 defined('_JEXEC') or die;
+$published = $this->state->get('filter.published');
+$menuType  = JFactory::getApplication()->getUserState('com_menus.items.menutype');
 ?>
 <a class="btn" type="button" onclick="document.getElementById('batch-menu-id').value='';document.getElementById('batch-access').value='';document.getElementById('batch-language-id').value=''" data-dismiss="modal">
 	<?php echo JText::_('JCANCEL'); ?>
 </a>
-<button class="btn btn-success" type="submit" onclick="Joomla.submitbutton('item.batch');">
-	<?php echo JText::_('JGLOBAL_BATCH_PROCESS'); ?>
-</button>
+<?php if (strlen($menuType) && $menuType != '*' && $published != 2): ?>
+	<button class="btn btn-success" type="submit" onclick="Joomla.submitbutton('item.batch');">
+		<?php echo JText::_('JGLOBAL_BATCH_PROCESS'); ?>
+	</button>
+<?php endif; ?>
--- administrator/components/com_menus/views/items/view.html.php
+++ administrator/components/com_menus/views/items/view.html.php
@@ -261,14 +261,13 @@ class MenusViewItems extends JViewLegacy
 	 */
 	protected function addToolbar()
 	{
-		$menuType      = $this->state->get('filter.menutype');
-		$menutypeId    = (int) $this->state->get('menutypeid');
-		$menuTypeTitle = $this->get('State')->get('menutypetitle');
-		$published     = $this->state->get('filter.published');
-		$protected     = $menuType == 'main';
+		$menutypeId = (int) $this->state->get('menutypeid');
 
 		$canDo = JHelperContent::getActions('com_menus', 'menu', (int) $menutypeId);
 		$user  = JFactory::getUser();
+
+		// Get the menu title
+		$menuTypeTitle = $this->get('State')->get('menutypetitle');
 
 		// Get the toolbar object instance
 		$bar = JToolbar::getInstance('toolbar');
@@ -286,6 +285,8 @@ class MenusViewItems extends JViewLegacy
 		{
 			JToolbarHelper::addNew('item.add');
 		}
+
+		$protected = $this->state->get('filter.menutype') == 'main';
 
 		if ($canDo->get('core.edit') && !$protected)
 		{
@@ -314,8 +315,7 @@ class MenusViewItems extends JViewLegacy
 		}
 
 		// Add a batch button
-		if ((!$protected && strlen($menuType) && $menuType != '*' && $published != - 2)
-			&& $user->authorise('core.create', 'com_menus')
+		if (!$protected && $user->authorise('core.create', 'com_menus')
 			&& $user->authorise('core.edit', 'com_menus')
 			&& $user->authorise('core.edit.state', 'com_menus'))
 		{
@@ -323,12 +323,12 @@ class MenusViewItems extends JViewLegacy
 
 			// Instantiate a new JLayoutFile instance and render the batch button
 			$layout = new JLayoutFile('joomla.toolbar.batch');
-			$dhtml  = $layout->render(array('title' => $title));
 
+			$dhtml = $layout->render(array('title' => $title));
 			$bar->appendButton('Custom', $dhtml, 'batch');
 		}
 
-		if (!$protected && $published == -2 && $canDo->get('core.delete'))
+		if (!$protected && $this->state->get('filter.published') == -2 && $canDo->get('core.delete'))
 		{
 			JToolbarHelper::deleteList('JGLOBAL_CONFIRM_DELETE', 'items.delete', 'JTOOLBAR_EMPTY_TRASH');
 		}
