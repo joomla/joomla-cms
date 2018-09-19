e2c95ebd3d102efa58a94eb4c98e5cea00df721b
Cyril Rezé <cyril.reze@joomlic.com>
2018-09-19 15:08:06 +0200

    Add language strings for domain name and description

--- plugins/privacy/actionlogs/actionlogs.php
+++ plugins/privacy/actionlogs/actionlogs.php
@@ -55,6 +55,68 @@ class PlgPrivacyActionlogs extends PrivacyPlugin
 			return array();
 		}
 
+		$lang = JFactory::getLanguage();
+
+		$receiver = JUser::getInstance($user->id);
+
+		/*
+		 * We don't know if the user has admin access, so we will check if they have an admin language in their parameters,
+		 * falling back to the site language, falling back to the site default language.
+		 */
+
+		$langCode = $receiver->getParam('admin_language', '');
+
+		if (!$langCode)
+		{
+			$langCode = $receiver->getParam('language', JComponentHelper::getParams('com_languages')->get('site'));
+		}
+
+		$lang = JLanguage::getInstance($langCode, $lang->getDebug());
+
+		// Ensure the right language files have been loaded.
+		$lang->load('plg_privacy_actionlogs', JPATH_ADMINISTRATOR, null, false, true)
+			|| $lang->load('plg_privacy_actionlogs', JPATH_SITE . '/plugins/privacy/actionlogs', null, false, true);
+
+		$domain = $this->createDomain(
+			$lang->_('PLG_PRIVACY_ACTIONLOGS_DOMAIN_LABEL'),
+			$lang->_('PLG_PRIVACY_ACTIONLOGS_DOMAIN_DESC')
+		);
+
+		$query = $this->db->getQuery(true)
+			->select('a.*, u.name')
+			->from('#__action_logs AS a')
+			->innerJoin('#__users AS u ON a.user_id = u.id')
+			->where($this->db->quoteName('a.user_id') . ' = ' . $user->id);
+
+		$this->db->setQuery($query);
+
+		$data = $this->db->loadObjectList();
+
+		if (!count($data))
+		{
+			return array();
+		}
+
+		$data = ActionlogsHelper::getCsvData($data);
+		array_shift($data);
+
+		foreach ($data as $item)
+		{
+			$domain->addItem($this->createItemFromArray($item));
+		}
+
+		return array($domain);
+	}
+}
+
+	 */
+	public function onPrivacyExportRequest(PrivacyTableRequest $request, JUser $user = null)
+	{
+		if (!$user)
+		{
+			return array();
+		}
+
 		$domain = $this->createDomain(
 			JText::_('PLG_PRIVACY_ACTIONLOGS_DOMAIN_LABEL'),
 			JText::_('PLG_PRIVACY_ACTIONLOGS_DOMAIN_DESC')
