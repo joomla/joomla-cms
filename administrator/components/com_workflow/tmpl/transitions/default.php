34315f9c2f5fe86fce4eb1a199591b79c8446e7f
chmst <dev@chmst.de>
2018-09-22 00:10:18 +0200

    Improve layout of transitions overview by adding the condition icons

--- administrator/components/com_workflow/tmpl/transitions/default.php
+++ administrator/components/com_workflow/tmpl/transitions/default.php
@@ -17,6 +17,7 @@ use Joomla\CMS\Router\Route;
 use Joomla\CMS\Session\Session;
 use Joomla\CMS\Factory;
 use Joomla\CMS\Layout\LayoutHelper;
+use Joomla\CMS\Workflow\Workflow;
 
 HTMLHelper::_('behavior.tooltip');
 HTMLHelper::_('behavior.multiselect');
@@ -54,7 +55,7 @@ if ($saveOrder)
 					<table class="table">
 						<thead>
 							<tr>
-								<th scope="col" style="width:1%" class="text-center hidden-sm-down">
+								<th scope="col" style="width:1%" class="text-center d-none d-md-table-cell">
 									<?php echo HTMLHelper::_('searchtools.sort', '', 't.ordering', $listDirn, $listOrder, null, 'asc', 'JGRID_HEADING_ORDERING', 'icon-menu-2'); ?>
 								</th>
 								<td style="width:1%" class="text-center hidden-sm-down">
@@ -63,16 +64,16 @@ if ($saveOrder)
 								<th scope="col" style="width:1%" class="text-center hidden-sm-down">
 									<?php echo HTMLHelper::_('searchtools.sort', 'JSTATUS', 't.published', $listDirn, $listOrder); ?>
 								</th>
-								<th scope="col" style="width:10%" class="hidden-sm-down">
+								<th scope="col" style="width:20%">
 									<?php echo HTMLHelper::_('searchtools.sort', 'COM_WORKFLOW_NAME', 't.title', $listDirn, $listOrder); ?>
 								</th>
-								<th scope="col" style="width:10%" class="text-center hidden-sm-down">
-									<?php echo HTMLHelper::_('searchtools.sort', 'COM_WORKFLOW_FROM_STAGE', 't.from_stage', $listDirn, $listOrder); ?>
+								<th scope="col" style="width:20%" class="d-none d-md-table-cell">
+									<?php echo HTMLHelper::_('searchtools.sort', 'COM_WORKFLOW_FROM_STAGE', 'from_stage', $listDirn, $listOrder); ?>
 								</th>
-								<th scope="col" style="width:10%" class="text-center hidden-sm-down">
-									<?php echo HTMLHelper::_('searchtools.sort', 'COM_WORKFLOW_TO_STAGE', 't.to_stage', $listDirn, $listOrder); ?>
+								<th scope="col" style="width:20%" class="d-none d-md-table-cell">
+									<?php echo HTMLHelper::_('searchtools.sort', 'COM_WORKFLOW_TO_STAGE', 'to_stage', $listDirn, $listOrder); ?>
 								</th>
-								<th scope="col" style="width:10%" class="text-right hidden-sm-down">
+								<th scope="col" style="width:3%" class="d-none d-md-table-cell">
 									<?php echo HTMLHelper::_('searchtools.sort', 'COM_WORKFLOW_ID', 't.id', $listDirn, $listOrder); ?>
 								</th>
 							</tr>
@@ -86,7 +87,7 @@ if ($saveOrder)
 								$canCheckin = true || $user->authorise('core.admin', 'com_checkin') || $item->checked_out == $userId || $item->checked_out == 0;
 								$canChange  = $user->authorise('core.edit.state', $this->extension . '.transition.' . $item->id) && $canCheckin;								?>
 								<tr class="row<?php echo $i % 2; ?>">
-									<td class="order text-center hidden-sm-down">
+									<td class="order text-center d-none d-md-table-cell">
 										<?php
 										$iconClass = '';
 										if (!$canChange)
@@ -124,18 +125,50 @@ if ($saveOrder)
 											<div class="small"><?php echo $this->escape(Text::_($item->description)); ?></div>
 										<?php endif; ?>
 									</th>
-									<td class="text-center">
-									<?php if ($item->from_stage_id < 0) : ?>
-										<?php echo Text::_('JALL'); ?>
-									<?php else : ?>
-										<?php echo $this->escape(Text::_($item->from_stage)); ?>
-									<?php endif; ?>
+									<td class="nowrap">
+										<?php if ($item->from_stage_id < 0) : ?>
+											<?php echo Text::_('JALL'); ?>
+										<?php else : ?>
+											<?php 
+											if ($item->from_condition == Workflow::CONDITION_ARCHIVED):
+												$icon = "icon-archive";
+												$sr_only_text = TEXT::_('COM_WORKFLOW_TRANSITIONS_CONDITION_ARCHIVED');
+											elseif ($item->from_condition == Workflow::CONDITION_TRASHED):
+												$icon = "icon-trash";
+												$sr_only_text = TEXT::_('COM_WORKFLOW_TRANSITIONS_CONDITION_TRASHED');
+											elseif ($item->from_condition == Workflow::CONDITION_PUBLISHED):
+												$icon = "icon-publish";
+												$sr_only_text = TEXT::_('COM_WORKFLOW_TRANSITIONS_CONDITION_PUBLISHED');
+											elseif ($item->from_condition == Workflow::CONDITION_UNPUBLISHED):
+												$icon = "icon-unpublish";
+												$sr_only_text = TEXT::_('COM_WORKFLOW_TRANSITIONS_CONDITION_UNPUBLISHED');
+											endif; ?>
+											<span class="<?php echo $icon; ?>" aria-hidden="true"></span>
+											<span class="sr-only"><?php echo $sr_only_text; ?></span>
+											<?php echo ' ' . $this->escape(Text::_($item->from_stage)); ?>
+										<?php endif; ?>
 									</td>
-									<td class="text-center">
-										<?php echo $this->escape(Text::_($item->to_stage)); ?>
+									<td class="nowrap">
+										<?php 
+										if ($item->to_condition == Workflow::CONDITION_ARCHIVED):
+											$icon = "icon-archive";
+											$sr_only_text = TEXT::_('COM_WORKFLOW_TRANSITIONS_CONDITION_ARCHIVED');
+										elseif ($item->to_condition == Workflow::CONDITION_TRASHED):
+											$icon = "icon-trash";
+											$sr_only_text = TEXT::_('COM_WORKFLOW_TRANSITIONS_CONDITION_TRASHED');;
+										elseif ($item->to_condition == Workflow::CONDITION_PUBLISHED):
+											$icon = "icon-publish";
+											$sr_only_text = TEXT::_('COM_WORKFLOW_TRANSITIONS_CONDITION_PUBLISHED');
+										elseif ($item->to_condition == Workflow::CONDITION_UNPUBLISHED):
+											$icon = "icon-unpublish";
+											$sr_only_text = TEXT::_('COM_WORKFLOW_TRANSITIONS_CONDITION_UNPUBLISHED');
+										endif; ?>
+										<span class="<?php echo $icon; ?>" aria-hidden="true"></span>
+										<span class="sr-only"><?php echo $sr_only_text; ?></span>
+										<?php echo ' ' . $this->escape(Text::_($item->to_stage)); ?>
 									</td>
-									<td class="text-right">
-										<?php echo $item->id; ?>
+									<td class="d-none d-md-table-cell">
+										<?php echo (int) $item->id; ?>
 									</td>
 								</tr>
 							<?php endforeach ?>
@@ -152,3 +185,4 @@ if ($saveOrder)
 			</div>
 		</div>
 	</div>
+</form>
