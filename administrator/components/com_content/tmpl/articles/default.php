f8c0983b0ed974fe45d5e864096a4e6cc5965356
Fedik <getthesite@gmail.com>
2018-09-21 21:25:27 +0300

    update multiselect fields in Articles filter

--- administrator/components/com_content/tmpl/articles/default.php
+++ administrator/components/com_content/tmpl/articles/default.php
@@ -24,10 +24,6 @@ use Joomla\CMS\Factory;
 use Joomla\CMS\HTML\HTMLHelper;
 
 HTMLHelper::_('behavior.multiselect');
-HTMLHelper::_('formbehavior.chosen', '.multipleTags', null, array('placeholder_text_multiple' => Text::_('JOPTION_SELECT_TAG')));
-HTMLHelper::_('formbehavior.chosen', '.multipleCategories', null, array('placeholder_text_multiple' => Text::_('JOPTION_SELECT_CATEGORY')));
-HTMLHelper::_('formbehavior.chosen', '.multipleAccessLevels', null, array('placeholder_text_multiple' => Text::_('JOPTION_SELECT_ACCESS')));
-HTMLHelper::_('formbehavior.chosen', '.multipleAuthors', null, array('placeholder_text_multiple' => Text::_('JOPTION_SELECT_AUTHOR')));
 
 $app       = Factory::getApplication();
 $user      = Factory::getUser();
