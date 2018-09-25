23ae4875c69281650025616e7d4e82251a3e179b
Jean-Marie Simonet <infografjms@gmail.com>
2018-09-25 10:35:58 +0200

    using sprintf

--- templates/system/html/modules.php
+++ templates/system/html/modules.php
@@ -170,10 +170,10 @@ function modChrome_outline($module, &$params, &$attribs)
 	<div class="mod-preview">
 		<div class="mod-preview-info">
 			<div class="mod-preview-position">
-				<span><?php echo JText::_('JGLOBAL_PREVIEW_POSITION'); ?></span><?php echo $module->position; ?>
+				<span><?php echo JText::sprintf('JGLOBAL_PREVIEW_POSITION', $module->position); ?></span>
 			</div>
 			<div class="mod-preview-style">
-				<span><?php echo JText::_('JGLOBAL_PREVIEW_STYLE'); ?></span><?php echo $module->style; ?>
+				<span><?php echo JText::sprintf('JGLOBAL_PREVIEW_STYLE', $module->style); ?></span>
 			</div>
 		</div>
 		<div class="mod-preview-wrapper">
