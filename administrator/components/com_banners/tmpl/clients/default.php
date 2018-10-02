8745b086ecdb26bf84b4f96fc0508eacd33c0153
Allon Moritz <allon.moritz@digital-peak.com>
2018-10-02 08:25:46 +0200

    Remove not needed helpers call in com_banners

--- administrator/components/com_banners/tmpl/clients/default.php
+++ administrator/components/com_banners/tmpl/clients/default.php
@@ -15,8 +15,6 @@ use Joomla\CMS\Layout\LayoutHelper;
 use Joomla\CMS\Factory;
 use Joomla\CMS\HTML\HTMLHelper;
 
-HTMLHelper::addIncludePath(JPATH_COMPONENT . '/helpers/html');
-
 HTMLHelper::_('behavior.multiselect');
 
 $purchaseTypes = array(
