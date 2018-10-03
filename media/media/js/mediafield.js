39b6f9ff1c3d5dd98aab2f8cb6371870aba110f6
ciar4n <ciaran@joomla51.com>
2018-10-03 09:53:15 +0100

    Tweaks

--- media/media/js/mediafield.js
+++ media/media/js/mediafield.js
@@ -60,6 +60,7 @@
 	$.fieldMedia.prototype.modalOpen = function() {
 		var $iframe = $('<iframe>', {
 			name: 'field-media-modal',
+			id: 'field-media-modal',
 			src: this.options.url.replace('{field-media-id}', this.$input.attr('id')),
 			width: this.options.modalWidth,
 			height: this.options.modalHeight
