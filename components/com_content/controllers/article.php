37dfedca6cc62c240b1a19351263b5f969bdc0b4
Simon Champion <Spudley@users.noreply.github.com>
2018-10-03 14:24:52 +0100

    Fix formatting

--- components/com_content/controllers/article.php
+++ components/com_content/controllers/article.php
@@ -441,6 +441,7 @@ class ContentControllerArticle extends JControllerForm
 			if ($model->storeVote($id, $user_rating))
 			{
 				$result = $dispatcher->trigger('onRatingAfterSave', array ('com_content.article', &$id, &$user_rating));
+
 				if (in_array(false, $result, true))
 				{
 					// Plugin can suppress default success message by returning false.
