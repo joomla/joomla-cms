818ebc5dc49bdcc9a2be564af7351898a03d6ba9
infograf768 <infografjms@gmail.com>
2018-10-06 18:14:26 +0000

    [4.0] Calendar js files not loading (#22513)
    * [4.0] Calendar js files not loading
    
    * Short array

--- layouts/joomla/form/field/calendar.php
+++ layouts/joomla/form/field/calendar.php
@@ -97,10 +97,10 @@ if (is_array($attributes))
 $cssFileExt = ($direction === 'rtl') ? '-rtl.css' : '.css';
 
 // The static assets for the calendar
-HTMLHelper::_('script', $localesPath, false, true, false, false, true);
-HTMLHelper::_('script', $helperPath, false, true, false, false, true);
-HTMLHelper::_('script', 'system/fields/calendar.min.js', false, true, false, false, true);
-HTMLHelper::_('stylesheet', 'system/fields/calendar' . $cssFileExt, array(), true);
+HTMLHelper::_('script', $localesPath, ['version' => 'auto', 'relative' => true]);
+HTMLHelper::_('script', $helperPath, ['version' => 'auto', 'relative' => true]);
+HTMLHelper::_('script', 'system/fields/calendar.min.js', ['version' => 'auto', 'relative' => true]);
+HTMLHelper::_('stylesheet', 'system/fields/calendar' . $cssFileExt, ['version' => 'auto', 'relative' => true]);
 ?>
 <div class="field-calendar">
 	<?php if (!$readonly && !$disabled) : ?>
