0589ab06d4e41cc38c82d17476de0dabd274878a
Tuan Pham Ngoc <github@joomdonation.com>
2018-10-10 04:46:12 +0700

    Fix wrong action log link when add/edit article (#22562)
    * Fix wrong action log link when add/edit article
    
    * Add @since tag
    
    * Correct codestyle

--- plugins/actionlog/joomla/joomla.php
+++ plugins/actionlog/joomla/joomla.php
@@ -31,6 +31,14 @@ class PlgActionlogJoomla extends ActionLogPlugin
 	protected $loggableExtensions = array();
 
 	/**
+	 * Context aliases
+	 *
+	 * @var    array
+	 * @since  3.9.0 
+	 */
+	protected $contextAliases = array('com_content.form' => 'com_content.article');
+
+	/**
 	 * Constructor.
 	 *
 	 * @param   object  &$subject  The object to observe.
@@ -62,6 +70,11 @@ class PlgActionlogJoomla extends ActionLogPlugin
 	 */
 	public function onContentAfterSave($context, $article, $isNew)
 	{
+		if (isset($this->contextAliases[$context]))
+		{
+			$context = $this->contextAliases[$context];
+		}
+
 		$option = $this->app->input->getCmd('option');
 
 		if (!$this->checkLoggable($option))
