f972787d1070bd3b2b925f5ae10de67d959d58a5
Allon Moritz <allon.moritz@digital-peak.com>
2018-10-16 14:31:25 +0200

    spaces instead of tabs

--- build/media_src/system/js/fields/passwordview.es6.js
+++ build/media_src/system/js/fields/passwordview.es6.js
@@ -7,42 +7,42 @@
  * Field switcher
  */
 ((document) => {
-	'use strict';
+  'use strict';
 
-	document.addEventListener('DOMContentLoaded', function() {
-		[].slice.call(document.querySelectorAll('input[type="password"]')).forEach((input) => {
-			const inputGroup = input.parentNode;
+  document.addEventListener('DOMContentLoaded', function() {
+    [].slice.call(document.querySelectorAll('input[type="password"]')).forEach((input) => {
+      const inputGroup = input.parentNode;
 
-			if (!inputGroup) {
-				return;
-			}
+      if (!inputGroup) {
+        return;
+      }
 
-			inputGroup.querySelector('.input-group-prepend, .input-group-append').addEventListener('click', (e) => {
-				const target = e.target;
-				const srText = target.previousSibling;
+      inputGroup.querySelector('.input-group-prepend, .input-group-append').addEventListener('click', (e) => {
+        const target = e.target;
+        const srText = target.previousSibling;
 
-				if (target.classList.contains('icon-eye')) {
-					// Update the icon class
-					target.classList.remove('icon-eye');
-					target.classList.add('icon-eye-close');
+        if (target.classList.contains('icon-eye')) {
+          // Update the icon class
+          target.classList.remove('icon-eye');
+          target.classList.add('icon-eye-close');
 
-					// Update the input type
-					input.type = 'text';
+          // Update the input type
+          input.type = 'text';
 
-					// Update the text for screenreaders
-					srText.innerText = Joomla.JText._('JSHOW');
-				} else {
-					// Update the icon class
-					target.classList.add('icon-eye');
-					target.classList.remove('icon-eye-close');
+          // Update the text for screenreaders
+          srText.innerText = Joomla.JText._('JSHOW');
+        } else {
+          // Update the icon class
+          target.classList.add('icon-eye');
+          target.classList.remove('icon-eye-close');
 
-					// Update the input type
-					input.type = 'password';
+          // Update the input type
+          input.type = 'password';
 
-					// Update the text for screenreaders
-					srText.innerText = Joomla.JText._('JHIDE');
-				}
-			});
-		});
-	});
+          // Update the text for screenreaders
+          srText.innerText = Joomla.JText._('JHIDE');
+        }
+      });
+    });
+  });
 })(document);
