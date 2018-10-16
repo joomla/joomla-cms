1fee3639c0790430b64dab11c9193dd5fb70ebf9
Cyril Rezé <cyril.reze@joomlic.com>
2018-10-16 21:17:56 +0200

    [ActionsLog] Improve UX for loggable extensions checkboxes with a select all/none

--- administrator/components/com_actionlogs/models/fields/logtype.php
+++ administrator/components/com_actionlogs/models/fields/logtype.php
@@ -28,6 +28,83 @@ class JFormFieldLogType extends JFormFieldCheckboxes
 	 * @since  3.9.0
 	 */
 	protected $type = 'LogType';
+	/**
+	 * Method to get the field input markup for check boxes.
+	 *
+	 * @return  string  The field input markup.
+	 *
+	 * @since   3.9.0
+	 */
+	public function getInput()
+	{
+		$html = '';
+
+		// Including fallback code for HTML5 non supported browsers.
+		JHtml::_('jquery.framework');
+		JHtml::_('script', 'system/html5fallback.js', false, true);
+
+		$script = "
+			// Checks/Uncheck all checkboxes
+			function eventsCheckAll(value)
+			{
+				events = document.querySelectorAll('.checkboxes input');
+
+				for (i = 0; i < events.length; i++) {
+					events[i].checked = value;
+				}
+			}
+		";
+
+		// Add the script to the document head
+		JFactory::getDocument()->addScriptDeclaration($script);
+
+		$html.= '<div class="well well-small">';
+		$html.= '<div class="form-inline">';
+		$html.= '<span class="small">' . JText::_('JSELECT') . ': ';
+		$html.= '<a id="checkAll" href="javascript://" onclick="eventsCheckAll(true)">' . JText::_('JALL') . '</a>, ';
+		$html.= '<a id="uncheckAll" href="javascript://" onclick="eventsCheckAll(false)">' . JText::_('JNONE') . '</a>';
+		$html.= '</span>';
+		$html.= '</div>';
+
+		$html.= '<div class="clearfix"></div>';
+
+		$html.= '<hr class="hr-condensed" />';
+
+		/**
+		 * The format of the input tag to be filled in using sprintf.
+		 *     %1 - id
+		 *     %2 - name
+		 *     %3 - value
+		 *     %4 = any other attributes
+		 */
+		$format = '<input type="checkbox" id="%1$s" name="%2$s" value="%3$s" %4$s />';
+
+		// Initialize the field checked options.
+		$checkedOptions = is_array($this->value) ? $this->value : explode(',', (string) $this->value);
+
+		// Get the field options.
+		$options = $this->getOptions();
+
+		$html.= '<fieldset id="' . $this->id . '" class="' . trim($this->class . ' checkboxes') . '">';
+
+		foreach ($options as $i => $option)
+		{
+			// Initialize some option attributes.
+			$checked = in_array((string) $option->value, $checkedOptions, true) ? 'checked' : '';
+			$oid     = $this->id . $i;
+			$value   = htmlspecialchars($option->value, ENT_COMPAT, 'UTF-8');
+
+			$html.= '<label for="' . $oid . '" class="checkbox">';
+			$html.= sprintf($format, $oid, $this->name, $value, ' ' . $checked);
+			$html.= $option->text . '</label>';
+		}
+
+		$html.= '</fieldset>';
+
+		$html.= '</div>';
+
+		return $html;
+	}
 
 	/**
 	 * Method to get the field options.
