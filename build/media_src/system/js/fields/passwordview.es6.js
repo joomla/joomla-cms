fad740e85bc95a98689c2b295d1078c1cd5b329c
Allon Moritz <allon.moritz@digital-peak.com>
2018-10-16 14:29:50 +0200

    revert copy paste

--- build/media_src/system/js/fields/passwordview.es6.js
+++ build/media_src/system/js/fields/passwordview.es6.js
@@ -1,9 +1,4 @@
 /**
- * PLEASE DO NOT MODIFY THIS FILE. WORK ON THE ES6 VERSION.
- * OTHERWISE YOUR CHANGES WILL BE REPLACED ON THE NEXT BUILD.
- **/
-
-/**
  * @copyright   Copyright (C) 2005 - 2018 Open Source Matters, Inc. All rights reserved.
  * @license      GNU General Public License version 2 or later; see LICENSE.txt
  */
@@ -11,43 +6,43 @@
 /**
  * Field switcher
  */
-(function (document) {
-  'use strict';
+((document) => {
+	'use strict';
 
-  document.addEventListener('DOMContentLoaded', function () {
-    [].slice.call(document.querySelectorAll('input[type="password"]')).forEach(function (input) {
-      var inputGroup = input.parentNode;
+	document.addEventListener('DOMContentLoaded', function() {
+		[].slice.call(document.querySelectorAll('input[type="password"]')).forEach((input) => {
+			const inputGroup = input.parentNode;
 
-      if (!inputGroup) {
-        return;
-      }
+			if (!inputGroup) {
+				return;
+			}
 
-      inputGroup.querySelector('.input-group-prepend, .input-group-append').addEventListener('click', function (e) {
-        var target = e.target;
-        var srText = target.previousSibling;
+			inputGroup.querySelector('.input-group-prepend, .input-group-append').addEventListener('click', (e) => {
+				const target = e.target;
+				const srText = target.previousSibling;
 
-        if (target.classList.contains('icon-eye')) {
-          // Update the icon class
-          target.classList.remove('icon-eye');
-          target.classList.add('icon-eye-close');
+				if (target.classList.contains('icon-eye')) {
+					// Update the icon class
+					target.classList.remove('icon-eye');
+					target.classList.add('icon-eye-close');
 
-          // Update the input type
-          input.type = 'text';
+					// Update the input type
+					input.type = 'text';
 
-          // Update the text for screenreaders
-          srText.innerText = Joomla.JText._('JSHOW');
-        } else {
-          // Update the icon class
-          target.classList.add('icon-eye');
-          target.classList.remove('icon-eye-close');
+					// Update the text for screenreaders
+					srText.innerText = Joomla.JText._('JSHOW');
+				} else {
+					// Update the icon class
+					target.classList.add('icon-eye');
+					target.classList.remove('icon-eye-close');
 
-          // Update the input type
-          input.type = 'password';
+					// Update the input type
+					input.type = 'password';
 
-          // Update the text for screenreaders
-          srText.innerText = Joomla.JText._('JHIDE');
-        }
-      });
-    });
-  });
+					// Update the text for screenreaders
+					srText.innerText = Joomla.JText._('JHIDE');
+				}
+			});
+		});
+	});
 })(document);
