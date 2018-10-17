baa7b6d8e33a111d820237874f461bccb703ab38
Nicola Galgano <optimus4joomla@gmail.com>
2018-10-17 14:49:32 +0200

    [plugin][invisible recapctha] - detect correct IP address

--- plugins/captcha/recaptcha_invisible/recaptcha_invisible.php
+++ plugins/captcha/recaptcha_invisible/recaptcha_invisible.php
@@ -10,6 +10,7 @@
 defined('_JEXEC') or die;
 
 use Joomla\CMS\Captcha\Google\HttpBridgePostRequestMethod;
+use Joomla\Utilities\IpHelper; 
 
 /**
  * Invisible reCAPTCHA Plugin.
@@ -129,7 +130,7 @@ class PlgCaptchaRecaptcha_Invisible extends \JPlugin
 	{
 		$input      = \JFactory::getApplication()->input;
 		$privatekey = $this->params->get('private_key');
-		$remoteip   = $input->server->get('REMOTE_ADDR', '', 'string');
+		$remoteip   = IpHelper::getIp();
 
 		$response  = $input->get('g-recaptcha-response', '', 'string');
 
