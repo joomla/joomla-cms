294cdb32eed03796596cf00c99f46866578e3ae0
Ciaran Walsh <ciaran@joomla51.com>
2018-10-18 06:04:16 +0100

    [4.0] Sidebar - switch to vertical collapsing (#22587)
    * [4.0] Sidebar redo
    
    * happy hound
    
    * more hound
    
    * Fix current and toggle
    
    * Increase parent hover contrast
    
    * cs
    
    * a11y styling
    
    * happy hound
    
    * hound
    
    * list style - switch to data uri icons
    
    * hound
    
    * hound
    
    * invalid property
    
    * Remove 'add new menu'
    
    * hound
    
    * remove redundant js
    
    * hound
    
    * es5
    
    * cs
    
    * conflicts
    
    * cs
    
    * Update default.php
    
    * Open second level if active

--- administrator/modules/mod_menu/tmpl/default.php
+++ administrator/modules/mod_menu/tmpl/default.php
@@ -13,11 +13,12 @@ use Joomla\CMS\Helper\ModuleHelper;
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
 
+HTMLHelper::_('script', 'vendor/metismenujs/metismenujs.min.js', ['version' => 'auto', 'relative' => true]);
 HTMLHelper::_('script', 'mod_menu/admin-menu.min.js', ['version' => 'auto', 'relative' => true], ['defer' => true]);
 
 $doc       = \Joomla\CMS\Factory::getDocument();
 $direction = $doc->direction === 'rtl' ? 'float-right' : '';
-$class     = $enabled ? 'nav navbar-nav nav-stacked main-nav clearfix ' . $direction : 'nav navbar-nav nav-stacked main-nav clearfix disabled ' . $direction;
+$class     = $enabled ? 'nav flex-column main-nav ' . $direction : 'nav flex-column main-nav disabled ' . $direction;
 
 // Recurse through children of root node if they exist
 $menuTree = $menu->getTree();
