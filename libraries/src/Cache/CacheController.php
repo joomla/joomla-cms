2bb6e688090e85299822bcb1534619ddc2c3e774
Allon Moritz <allon.moritz@digital-peak.com>
2018-10-18 09:26:55 +0200

    Revert "[4.0] Remove the deprecated code in the CacheController class (#22354)"
    This reverts commit cdbc8df

--- libraries/src/Cache/CacheController.php
+++ libraries/src/Cache/CacheController.php
@@ -149,4 +149,78 @@ class CacheController
 
 		return $paths;
 	}
+
+	/**
+	 * Get stored cached data by ID and group
+	 *
+	 * @param   string  $id     The cache data ID
+	 * @param   string  $group  The cache data group
+	 *
+	 * @return  mixed  Boolean false on no result, cached object otherwise
+	 *
+	 * @since   1.7.0
+	 * @deprecated  4.0  Implement own method in subclass
+	 */
+	public function get($id, $group = null)
+	{
+		$data = $this->cache->get($id, $group);
+
+		if ($data === false)
+		{
+			$locktest = $this->cache->lock($id, $group);
+
+			// If locklooped is true try to get the cached data again; it could exist now.
+			if ($locktest->locked === true && $locktest->locklooped === true)
+			{
+				$data = $this->cache->get($id, $group);
+			}
+
+			if ($locktest->locked === true)
+			{
+				$this->cache->unlock($id, $group);
+			}
+		}
+
+		// Check again because we might get it from second attempt
+		if ($data !== false)
+		{
+			// Trim to fix unserialize errors
+			$data = unserialize(trim($data));
+		}
+
+		return $data;
+	}
+
+	/**
+	 * Store data to cache by ID and group
+	 *
+	 * @param   mixed    $data        The data to store
+	 * @param   string   $id          The cache data ID
+	 * @param   string   $group       The cache data group
+	 * @param   boolean  $wrkarounds  True to use wrkarounds
+	 *
+	 * @return  boolean  True if cache stored
+	 *
+	 * @since   1.7.0
+	 * @deprecated  4.0  Implement own method in subclass
+	 */
+	public function store($data, $id, $group = null, $wrkarounds = true)
+	{
+		$locktest = $this->cache->lock($id, $group);
+
+		if ($locktest->locked === false && $locktest->locklooped === true)
+		{
+			// We can not store data because another process is in the middle of saving
+			return false;
+		}
+
+		$result = $this->cache->store(serialize($data), $id, $group);
+
+		if ($locktest->locked === true)
+		{
+			$this->cache->unlock($id, $group);
+		}
+
+		return $result;
+	}
 }
