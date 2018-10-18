9fd37e989896d6899ad0877c729733e6fc17ab06
Allon Moritz <allon.moritz@digital-peak.com>
2018-10-18 13:44:12 +0200

    Revert

--- libraries/src/Dispatcher/Dispatcher.php
+++ libraries/src/Dispatcher/Dispatcher.php
@@ -2,7 +2,7 @@
 /**
  * Joomla! Content Management System
  *
- * @copyright  Copyright (C) 2005 - 2017 Open Source Matters, Inc. All rights reserved.
+ * @copyright  Copyright (C) 2005 - 2018 Open Source Matters, Inc. All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE
  */
 
@@ -62,4 +62,3 @@ abstract class Dispatcher implements DispatcherInterface
 		return $this->app;
 	}
 }
-
