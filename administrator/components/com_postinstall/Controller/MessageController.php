05c9615854abc90447d04fa2ed215bd252d4e338
Allon Moritz <allon.moritz@digital-peak.com>
2018-10-19 06:48:18 +0200

    Alpha order imports of com_postinstall

--- administrator/components/com_postinstall/Controller/MessageController.php
+++ administrator/components/com_postinstall/Controller/MessageController.php
@@ -11,10 +11,10 @@ namespace Joomla\Component\Postinstall\Administrator\Controller;
 
 defined('_JEXEC') or die;
 
+use Joomla\CMS\Filesystem\File;
 use Joomla\CMS\MVC\Controller\BaseController;
 use Joomla\Component\Postinstall\Administrator\Helper\PostinstallHelper;
 use Joomla\Component\Postinstall\Administrator\Model\MessagesModel;
-use Joomla\CMS\Filesystem\File;
 
 /**
  * Postinstall message controller.
