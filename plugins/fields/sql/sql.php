8adf4f9e243c829dfa94af9a8c64ee271305a262
Allon Moritz <allon.moritz@digital-peak.com>
2018-10-30 13:52:27 +0100

    Use the namespaced Form class in the fields plugin doc block

--- plugins/fields/sql/sql.php
+++ plugins/fields/sql/sql.php
@@ -26,7 +26,7 @@ class PlgFieldsSql extends \Joomla\Component\Fields\Administrator\Plugin\FieldsL
 	 *
 	 * @param   stdClass    $field   The field.
 	 * @param   DOMElement  $parent  The field node parent.
-	 * @param   JForm       $form    The form.
+	 * @param   Form        $form    The form.
 	 *
 	 * @return  DOMElement
 	 *
