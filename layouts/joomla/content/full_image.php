0bde95cd363071f3f46cf82fb9a678f34dd82e90
Valentín García <htmgarcia@gmail.com>
2018-10-31 13:46:57 -0600

    Fix image's microdata

--- layouts/joomla/content/full_image.php
+++ layouts/joomla/content/full_image.php
@@ -13,9 +13,9 @@ $params = $displayData->params;
 <?php $images = json_decode($displayData->images); ?>
 	<?php if (isset($images->image_fulltext) && !empty($images->image_fulltext)) : ?>
 		<?php $imgfloat = empty($images->float_fulltext) ? $params->get('float_fulltext') : $images->float_fulltext; ?>
-		<div class="pull-<?php echo htmlspecialchars($imgfloat); ?> item-image"> <img
+		<div class="pull-<?php echo htmlspecialchars($imgfloat); ?> item-image" itemprop="image" itemscope itemtype="http://schema.org/ImageObject"> <img
 		<?php if ($images->image_fulltext_caption) :
 			echo 'class="caption"' . ' title="' . htmlspecialchars($images->image_fulltext_caption) . '"';
 		endif; ?>
-		src="<?php echo htmlspecialchars($images->image_fulltext); ?>" alt="<?php echo htmlspecialchars($images->image_fulltext_alt); ?>" itemprop="image"/> </div>
+		src="<?php echo htmlspecialchars($images->image_fulltext); ?>" alt="<?php echo htmlspecialchars($images->image_fulltext_alt); ?>" itemprop="url"/> </div>
 	<?php endif; ?>
