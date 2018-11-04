096222b076d01e28bc56e678ce826ef7f66ec75b
George Wilson <georgejameswilson@googlemail.com>
2018-11-04 21:17:08 +0000

    Remove import

--- components/com_csp/Controller/ReportController.php
+++ components/com_csp/Controller/ReportController.php
@@ -14,7 +14,6 @@ defined('_JEXEC') or die;
 use Joomla\CMS\Factory;
 use Joomla\CMS\MVC\Controller\BaseController;
 use Joomla\CMS\Plugin\PluginHelper;
-use Joomla\Component\Csp\Administrator\Table\ReportTable;
 use Joomla\Registry\Registry;
 
 /**
