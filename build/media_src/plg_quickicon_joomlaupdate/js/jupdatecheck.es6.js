661ee091b4a15d6f8cd82211434596e7b724bea9
Jean-Marie Simonet <infografjms@gmail.com>
2018-11-08 10:21:00 +0100

    missing commas

--- build/media_src/plg_quickicon_joomlaupdate/js/jupdatecheck.es6.js
+++ build/media_src/plg_quickicon_joomlaupdate/js/jupdatecheck.es6.js
@@ -37,7 +37,7 @@
                   `${Joomla.JText._('PLG_QUICKICON_JOOMLAUPDATE_UPDATEFOUND_MESSAGE').replace('%s', `<span class="badge badge-danger">${updateInfoList.length}</span>`)}`
                   + `<button class="btn btn-primary" onclick="document.location='${options.url}'">`
                   + `${Joomla.JText._('PLG_QUICKICON_JOOMLAUPDATE_UPDATEFOUND_BUTTON')}</button>`,
-                ]
+                ],
               };
 
               // Render the message
