fc7456c1f7e81a952809cd4eb2109328327ab896
Peter van Westen <info@regularlabs.com>
2018-11-14 11:13:23 +0100

    Removes redundant modifier in regex

--- libraries/src/Installer/InstallerHelper.php
+++ libraries/src/Installer/InstallerHelper.php
@@ -30,7 +30,7 @@ abstract class InstallerHelper
 	 *
 	 * @var    integer
 	 * @since  3.9.0
-	 */	
+	 */
 	const HASH_NOT_VALIDATED = 0;
 
 	/**
@@ -311,7 +311,7 @@ abstract class InstallerHelper
 		$filename = preg_replace('/[^a-z0-9\_\-\.]/i', '_', $filename);
 
 		// Replace multiple underscores with just one.
-		$filename = preg_replace('/__+/i', '_', trim($filename, '_'));
+		$filename = preg_replace('/__+/', '_', trim($filename, '_'));
 
 		// Return the cleaned filename or, if it is empty, a unique id.
 		return $filename ?: $default;
