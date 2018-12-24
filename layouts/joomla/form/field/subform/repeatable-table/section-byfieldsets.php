943f57dc02dfb59808e9c7aec7b1e18a9408a427
Chris Davenport <chris.davenport@davenporttechnology.com>
2018-12-24 20:44:24 +0000

    Re-add generic CSS classes in subforms. (#23190)

--- layouts/joomla/form/field/subform/repeatable-table/section-byfieldsets.php
+++ layouts/joomla/form/field/subform/repeatable-table/section-byfieldsets.php
@@ -36,17 +36,17 @@ extract($displayData);
 	<td>
 		<div class="btn-group">
 			<?php if (!empty($buttons['add'])) : ?>
-				<a class="btn btn-mini button btn-success group-add-<?php echo $unique_subform_id; ?>" aria-label="<?php echo JText::_('JGLOBAL_FIELD_ADD'); ?>">
+				<a class="btn btn-mini button btn-success group-add group-add-<?php echo $unique_subform_id; ?>" aria-label="<?php echo JText::_('JGLOBAL_FIELD_ADD'); ?>">
 					<span class="icon-plus" aria-hidden="true"></span>
 				</a>
 			<?php endif; ?>
 			<?php if (!empty($buttons['remove'])) : ?>
-				<a class="btn btn-mini button btn-danger group-remove-<?php echo $unique_subform_id; ?>" aria-label="<?php echo JText::_('JGLOBAL_FIELD_REMOVE'); ?>">
+				<a class="btn btn-mini button btn-danger group-remove group-remove-<?php echo $unique_subform_id; ?>" aria-label="<?php echo JText::_('JGLOBAL_FIELD_REMOVE'); ?>">
 					<span class="icon-minus" aria-hidden="true"></span>
 				</a>
 			<?php endif; ?>
 			<?php if (!empty($buttons['move'])) : ?>
-				<a class="btn btn-mini button btn-primary group-move-<?php echo $unique_subform_id; ?>" aria-label="<?php echo JText::_('JGLOBAL_FIELD_MOVE'); ?>">
+				<a class="btn btn-mini button btn-primary group-move group-move-<?php echo $unique_subform_id; ?>" aria-label="<?php echo JText::_('JGLOBAL_FIELD_MOVE'); ?>">
 					<span class="icon-move" aria-hidden="true"></span>
 				</a>
 			<?php endif; ?>
