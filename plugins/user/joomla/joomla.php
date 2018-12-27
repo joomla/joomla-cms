4467eb0196aa1f774ab0f114256ee4a24ec1931f
Quy <quy@fluxbb.org>
2018-12-27 18:01:40 +0100

    Update plugins/user/joomla/joomla.php
    Co-Authored-By: alikon <optimus4joomla@gmail.com>

--- plugins/user/joomla/joomla.php
+++ plugins/user/joomla/joomla.php
@@ -9,7 +9,7 @@
 
 defined('_JEXEC') or die;
 
-use Joomla\CMS\ComponentHelper;
+use Joomla\CMS\Component\ComponentHelper;
 use Joomla\CMS\Factory;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\Uri\Uri;
