ae32cfa44e9de9878133ab94673c847e8dcb1911
Benjamin Trenkle <benjamin.trenkle@wicked-software.de>
2019-01-06 04:15:41 +0100

    CS fix

--- administrator/components/com_workflow/View/Transitions/HtmlView.php
+++ administrator/components/com_workflow/View/Transitions/HtmlView.php
@@ -148,7 +148,8 @@ class HtmlView extends BaseHtmlView
 
 		$isCore = $this->workflow->core;
 
-		ToolbarHelper::link('index.php?option=com_workflow&view=workflows&extension=' . $this->escape($this->workflow->extension), 'JTOOLBAR_BACK', 'arrow-left');
+		ToolbarHelper::link('index.php?option=com_workflow&view=workflows&extension=' . $this->escape($this->workflow->extension),
+			'JTOOLBAR_BACK', 'arrow-left');
 
 		if ($canDo->get('core.create') && !$isCore)
 		{
