e60c9be1bd6fe4136a96221e3d565650301ae861
George Wilson <georgejameswilson@googlemail.com>
2019-01-08 15:09:16 +0000

    CS

--- administrator/components/com_workflow/View/Stages/HtmlView.php
+++ administrator/components/com_workflow/View/Stages/HtmlView.php
@@ -160,7 +160,7 @@ class HtmlView extends BaseHtmlView
 		ToolbarHelper::title(Text::sprintf('COM_WORKFLOW_STAGES_LIST', $this->escape($workflow)), 'address contact');
 
 		$isCore = $this->workflow->core;
-		$arrow = Factory::getLanguage()->isRtl() ? 'arrow-right' : 'arrow-left';
+		$arrow  = Factory::getLanguage()->isRtl() ? 'arrow-right' : 'arrow-left';
 
 		ToolbarHelper::link('index.php?option=com_workflow&view=workflows&extension=' . $this->escape($this->workflow->extension),
 			'JTOOLBAR_BACK', $arrow
