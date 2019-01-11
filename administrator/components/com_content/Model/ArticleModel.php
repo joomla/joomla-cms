5334b05c2906a495b997f844c9ca6d93484f7fae
Tuan Pham Ngoc <github@joomdonation.com>
2019-01-11 13:48:42 +0700

    Pass group name to removeField method

--- administrator/components/com_content/Model/ArticleModel.php
+++ administrator/components/com_content/Model/ArticleModel.php
@@ -714,7 +714,7 @@ class ArticleModel extends AdminModel
 		// Remove show_associations field if associations is not enabled
 		if (!$assoc)
 		{
-			$form->removeField('show_associations');
+			$form->removeField('show_associations', 'attribs');
 		}
 
 		return $form;
