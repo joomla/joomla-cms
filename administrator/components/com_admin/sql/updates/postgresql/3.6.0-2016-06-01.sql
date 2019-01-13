4ce15d60a93087fadf23832f0ebb20030dd13d82
alikon <optimus4joomla@gmail.com>
2019-01-13 10:28:55 +0100

    old one

--- administrator/components/com_admin/sql/updates/postgresql/3.6.0-2016-06-01.sql
+++ administrator/components/com_admin/sql/updates/postgresql/3.6.0-2016-06-01.sql
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-UPDATE "#__extensions" SET "protected" = 1 WHERE "name" = "com_ajax";
-=======
 UPDATE "#__extensions" SET "protected" = 1, "enabled" = 1 WHERE "name" = 'com_ajax';
->>>>>>> c17bfc4bfc75847c334176c519a87ba871c401de
