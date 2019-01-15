52eda259e38c4c468dfdc19fbbbf3958d6297914
Rene Pasing <r.pasing@continga.de>
2019-01-15 19:31:44 +0100

    Completely change the way new sub fields for our plg_fields_subfields plugin are being added.
    Previously this was done by offering the user a complete form to configure sub fields when
    creating a new instance of the subfields field, now this is done by just choosing an existing
    custom field which then will be used.

--- administrator/components/com_fields/models/fields/subfieldstype.php
+++ administrator/components/com_fields/models/fields/subfieldstype.php
@@ -22,6 +22,26 @@ class JFormFieldSubfieldstype extends JFormFieldList
 	public $type = 'Subfieldstype';
 
 	/**
+	 * Configuration option for this field type to could filter the displayed custom field instances
+	 * by a given context. Default value empty string. If given empty string, displays all custom fields.
+	 *
+	 * @var string
+	 *
+	 * @since __DEPLOY_VERSION__
+	 */
+	protected $context = '';
+
+	/**
+	 * Array to do a fast in-memory caching of all custom field items. Used to not bother the
+	 * FieldsHelper with a call every time this field is being rendered.
+	 *
+	 * @var array
+	 *
+	 * @since __DEPLOY_VERSION__
+	 */
+	protected static $customFieldsCache = array();
+
+	/**
 	 * Method to get the field options.
 	 *
 	 * @return  array  The field option objects.
@@ -32,17 +52,22 @@ class JFormFieldSubfieldstype extends JFormFieldList
 	{
 		$options = parent::getOptions();
 
-		$fieldTypes = FieldsHelper::getFieldTypes();
+		// Check whether we have a result for this context yet
+		if (!isset(static::$customFieldsCache[$this->context]))
+		{
+			static::$customFieldsCache[$this->context] = FieldsHelper::getFields($this->context);
+		}
 
-		foreach ($fieldTypes as $fieldType)
+		// Iterate over the custom fields for this context
+		foreach (static::$customFieldsCache[$this->context] as $customField)
 		{
 			// Skip our own subfields type. We won't have subfields in subfields.
-			if ($fieldType['type'] == 'subfields')
+			if ($customField->type == 'subfields')
 			{
 				continue;
 			}
 
-			$options[] = JHtml::_('select.option', $fieldType['type'], $fieldType['label']);
+			$options[] = JHtml::_('select.option', $customField->id, ($customField->title . ' (' . $customField->name . ')'));
 		}
 
 		// Sorting the fields based on the text which is displayed
@@ -55,5 +80,31 @@ class JFormFieldSubfieldstype extends JFormFieldList
 		);
 
 		return $options;
+	}
+
+	/**
+	 * Method to attach a JForm object to the field.
+	 *
+	 * @param   SimpleXMLElement  $element  The SimpleXMLElement object representing the `<field>` tag for the form field object.
+	 * @param   mixed             $value    The form field value to validate.
+	 * @param   string            $group    The field name group control value. This acts as an array container for the field.
+	 *                                      For example if the field has name="foo" and the group value is set to "bar" then the
+	 *                                      full field name would end up being "bar[foo]".
+	 *
+	 * @return  boolean  True on success.
+	 *
+	 * @see     JFormField::setup()
+	 * @since   __DEPLOY_VERSION_
+	 */
+	public function setup(SimpleXMLElement $element, $value, $group = null)
+	{
+		$return = parent::setup($element, $value, $group);
+
+		if ($return)
+		{
+			$this->context  = (string) $this->element['context'];
+		}
+
+		return $return;
 	}
 }
--- plugins/fields/subfields/subfields.php
+++ plugins/fields/subfields/subfields.php
@@ -29,6 +29,15 @@ class PlgFieldsSubfields extends FieldsPlugin
 	protected $renderCache = array();
 
 	/**
+	 * Array to do a fast in-memory caching of all custom field items.
+	 *
+	 * @var array
+	 *
+	 * @since __DEPLOY_VERSION__
+	 */
+	protected static $customFieldsCache = null;
+
+	/**
 	 * Handles the onContentPrepareForm event. Adds form definitions to relevant forms.
 	 *
 	 * @param   JForm         $form  The form to manipulate
@@ -40,6 +49,7 @@ class PlgFieldsSubfields extends FieldsPlugin
 	 */
 	public function onContentPrepareForm(JForm $form, $data)
 	{
+		// Get the path to our own form definition (basically ./params/subfields.xml)
 		$path = $this->getFormPath($form, $data);
 
 		if ($path === null)
@@ -47,99 +57,28 @@ class PlgFieldsSubfields extends FieldsPlugin
 			return;
 		}
 
-		// Load our own form definition
+		// Now load our own form definition into a DOMDocument, because we want to manipulate it
 		$xml = new DOMDocument;
 		$xml->load($path);
 
-		// Get this fields options
+		// In our form definition, find the one field of type `subfieldstype`
 		$xmlxpath   = new DOMXPath($xml);
-		$hiddenform = $xmlxpath->evaluate(
-			'/form/fields[@name="fieldparams"]/fieldset[@name="fieldparams"]/field[@name="options"]/form'
+		$valuefields = $xmlxpath->evaluate(
+			'//fieldset[@name="fieldparams"]/field[@name="options"]/form/field[@type="subfieldstype"]'
 		);
+		/* @var $valuefields \DOMNodeList */
 
-		if ($hiddenform->length != 1)
+		// If we haven't found it, something is wrong
+		if (!$valuefields || $valuefields->count() != 1)
 		{
-			// Something is wrong, abort.
 			return;
 		}
-		$hiddenform = $hiddenform->item(0);
 
-		// Iterate over all fields which we know (all our wanted subfields).
-		$fieldTypes = FieldsHelper::getFieldTypes();
+		// Now manipulate the field, set its parameter `context` to our context
+		$valuefield = $valuefields->item(0);
+		$valuefield->setAttribute('context', $data->context);
 
-		foreach ($fieldTypes as $fieldType)
-		{
-			// Skip our own subfields type, we dont want to allow subfields in subfields
-			// (to ease complexity)
-			if ($fieldType['type'] == 'subfields')
-			{
-				continue;
-			}
-			// Check whether the XML definition file for that type exists
-			$path = (JPATH_PLUGINS . '/' . $this->_type . '/' . $fieldType['type'] . '/params/' . $fieldType['type'] . '.xml');
-
-			if (!file_exists($path))
-			{
-				continue;
-			}
-
-			try
-			{
-				// Try to load the XML definition file of that field type into a DOMDocument
-				$subxml = new DOMDocument;
-				$subxml->load($path);
-				$subxmlxpath = new DOMXPath($subxml);
-
-				// Use XPath to find the top-level `fields` and `fieldset` elements in that XML
-				$toplevelElements = $subxmlxpath->evaluate(
-					'/form/fields[@name="fieldparams"]|/form/fields[@name="fieldparams"]/fieldset[@name="fieldparams"]'
-				);
-
-				// Iterate over those toplevel elements, they contain data we want to preserve in our own XML
-				foreach ($toplevelElements as $toplevelElement)
-				{
-					/* @var $toplevelElement \DOMElement */
-
-					// E.g. we want to preserve the `addfieldpath` property by passing it to \JForm
-					if ($toplevelElement->hasAttribute('addfieldpath'))
-					{
-						\JForm::addFieldPath(\JPATH_ROOT . $toplevelElement->getAttribute('addfieldpath'));
-					}
-				}
-
-				// XPath all fields from that XML document
-				$fields = $subxmlxpath->evaluate('/form/fields[@name="fieldparams"]/fieldset[@name="fieldparams"]/field');
-
-				for ($i = 0; $i < $fields->length; $i++)
-				{
-					$field = $fields->item($i);
-					/* @var $field \DOMElement */
-
-					// Rewrite this fields name, e.g. rewrite name=='buttons' for
-					// the editor fieldtype to '_type-editor_buttons'
-					$this->rewriteNodeNameRecursive(
-						$field,
-						('_type-' . $fieldType['type'] . '_')
-					);
-
-					// Only show this field when the field 'type' is of the specific type
-					$field->setAttribute('showon', 'type:' . $fieldType['type']);
-
-					// Those cannot be required, the 'showon' does only control visibility, and hence
-					// invisible elements would be required else
-					$field->setAttribute('required', '0');
-
-					// Import the rewritten field into our parent form
-					$hiddenform->appendChild($xml->importNode($field, true));
-				}
-			}
-			catch (Exception $e)
-			{
-				// Ignore this type for now.
-			}
-		}
-
-		// And finally load the form into the JForm
+		// And now load our manipulated form definition into the JForm
 		$form->load($xml->saveXML(), true, '/form/*');
 	}
 
@@ -235,11 +174,8 @@ class PlgFieldsSubfields extends FieldsPlugin
 			$row_subfields = new \stdClass;
 
 			// For each row, iterate over all the subfields
-			foreach ($this->getSubfieldsFromField($field) as $_subfield)
+			foreach ($this->getSubfieldsFromField($field) as $subfield)
 			{
-				// Clone this virtual subfield to not interfere with the other rows
-				$subfield = (clone $_subfield);
-
 				// Just to be sure, unset this subfields value (and rawvalue)
 				$subfield->rawvalue = $subfield->value = '';
 
@@ -250,10 +186,10 @@ class PlgFieldsSubfields extends FieldsPlugin
 					$subfield->rawvalue = $subfield->value = $row[$subfield->name];
 				}
 
-				// Do we want to render the value of our fields?
-				if ($field_params->get('render_values', '1') == '1')
+				// Do we want to render the value of this field?
+				if ($subfield->render_values == '1')
 				{
-					// Do we have a rendercache entry for this type?
+					// Do we have a renderCache entry for this type?
 					if (!isset($this->renderCache[$subfield->type]))
 					{
 						$this->renderCache[$subfield->type] = array();
@@ -427,7 +363,8 @@ class PlgFieldsSubfields extends FieldsPlugin
 	}
 
 	/**
-	 * Returns an array of all subfields for a given field.
+	 * Returns an array of all subfields for a given field. This will always return a bare clone
+	 * of a sub field, so manipulating it is safe.
 	 *
 	 * @param   \stdClass  $field  The field
 	 *
@@ -437,119 +374,42 @@ class PlgFieldsSubfields extends FieldsPlugin
 	 */
 	protected function getSubfieldsFromField(\stdClass $field)
 	{
+		if (static::$customFieldsCache === null)
+		{
+			// Prepare our cache
+			static::$customFieldsCache = array();
+
+			// Get all custom field instances
+			$customFields = FieldsHelper::getFields('');
+
+			foreach ($customFields as $customField)
+			{
+				// Store each custom field instance in our cache with its id as key
+				static::$customFieldsCache[$customField->id] = $customField;
+			}
+		}
+
 		$result = array();
 
+		// Iterate over all configured options for this field
 		foreach ($this->getOptionsFromField($field) as $option)
 		{
-			// The subfield starts with a bare copy of our own field
-			$subfield                = (clone $field);
-			$subfield->id            = null;
-			$subfield->title         = $option->label;
-			$subfield->fieldparams   = new Joomla\Registry\Registry;
-
-			/**
-			 * In onContentPrepareForm() we added a prefix for all type-dependant options (options for a sub-field
-			 * that were in the fieldparams of that field type). We did this to could support same-named options
-			 * for a sub-field for different types, because we need to construct the XML for the selection of the
-			 * type before we actually know the type. So, now that we have a prefix on the type-specific options, we
-			 * need to remove that prefix again, and add all those options under the fieldparams, so that we then have
-			 * a valid (sub-)field of that type.
-			 */
-			$prefix = ('_type-' . $option->type . '_');
-
-			// Copy all values from the subfields options into the subfield
-			foreach (array_keys(get_object_vars($option)) as $key)
+			// Check whether the wanted sub field really is an existing custom field
+			if (!isset(static::$customFieldsCache[$option->customfield]))
 			{
-				// If we have our prefix, copy the value into the fieldparams (and not directly into the subfield)
-				if (strpos($key, $prefix) === 0)
-				{
-					// If this is an array, make sure to remove the prefix also from its elements
-					$this->removeTypePrefixRecursive($option->{$key}, $prefix);
-
-					$subfield->fieldparams->set(
-						substr($key, strlen($prefix)), // Remove the prefix from the key
-						$option->{$key}
-					);
-				}
-				// If we have another type prefix, just ignore it
-				elseif (strpos($key, '_type-') === 0)
-				{
-					continue;
-				}
-				// Else copy the value from the option directly to the subfield
-				else
-				{
-					$subfield->{$key} = &$option->{$key};
-				}
+				continue;
 			}
 
-			$result[] = $subfield;
+			// Get a clone of the sub field, so we and the caller can do some manipulation with it.
+			$cur_field = (clone static::$customFieldsCache[$option->customfield]);
+
+			// Manipulate it and add our custom configuration to it
+			$cur_field->render_values = $option->render_values;
+
+			// And add it to our result
+			$result[] = $cur_field;
 		}
 
 		return $result;
-	}
-
-	/**
-	 * Recursively prefixes the 'name' attribute of $node with $prefix
-	 *
-	 * @param   \DOMElement  $node    The node
-	 * @param   string       $prefix  The prefix
-	 *
-	 * @return  void
-	 *
-	 * @since __DEPLOY_VERSION__
-	 */
-	protected function rewriteNodeNameRecursive(DOMElement $node, $prefix)
-	{
-		if ($node->hasAttribute('name'))
-		{
-			$node->setAttribute('name', ($prefix . $node->getAttribute('name')));
-		}
-
-		foreach ($node->childNodes as $childNode)
-		{
-			if ($childNode instanceof DOMElement)
-			{
-				$this->rewriteNodeNameRecursive($childNode, $prefix);
-			}
-		}
-	}
-
-	/**
-	 * Recursively removes our type prefix from array attribute names.
-	 *
-	 * @param   array   &$var    The object
-	 * @param   string  $prefix  The prefix
-	 *
-	 * @return  void
-	 *
-	 * @since __DEPLOY_VERSION__
-	 */
-	protected function removeTypePrefixRecursive(&$var, $prefix)
-	{
-		if (!is_array($var))
-		{
-			return;
-		}
-
-		// Iterate over each key in $var (done this way because we manipulate $var later on, so just to be sure...)
-		foreach (array_keys($var) as $key)
-		{
-			// If that key start with the prefix that shall be removed
-			if (strpos($key, $prefix) === 0)
-			{
-				// Construct the new key without the prefix
-				$new_key = substr($key, strlen($prefix));
-
-				// Insert the new key and delete the old one
-				$var[$new_key] = &$var[$key];
-				unset($var[$key]);
-
-				$key = $new_key;
-			}
-
-			// Now do the recursion
-			$this->removeTypePrefixRecursive($var[$key], $prefix);
-		}
 	}
 }
