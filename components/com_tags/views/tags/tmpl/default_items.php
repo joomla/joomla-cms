650262d52c2f5e34b2bd634e1909210553e2c13e
Quy <quy@fluxbb.org>
2019-01-20 00:56:16 +0000

    Update components/com_tags/views/tags/tmpl/default_items.php
    Co-Authored-By: brianteeman <brian@teeman.net>

--- components/com_tags/views/tags/tmpl/default_items.php
+++ components/com_tags/views/tags/tmpl/default_items.php
@@ -110,7 +110,7 @@ JFactory::getDocument()->addScriptDeclaration("
 					<?php endif; ?>
 				</span>
 			<?php endif; ?>
-			<?php if ($this->params->get('all_tags_show_tag_description', 1) or $this->params->get('all_tags_show_tag_hits')) : ?>
+			<?php if (($this->params->get('all_tags_show_tag_description', 1) && !empty($item->description)) || $this->params->get('all_tags_show_tag_hits')) : ?>
 				<div class="caption">
 					<?php if ($this->params->get('all_tags_show_tag_description', 1) && !empty($item->description)) : ?>
 						<span class="tag-body">
