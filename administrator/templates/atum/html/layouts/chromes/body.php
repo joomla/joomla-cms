e53e0a021511c4c024ec08eb4776089e9a779515
Thomas Hunziker <werbemails@bakual.ch>
2019-01-22 08:40:35 +0100

    Fix body chrome

--- administrator/templates/atum/html/layouts/chromes/body.php
+++ administrator/templates/atum/html/layouts/chromes/body.php
@@ -9,28 +9,43 @@
 
 defined('_JEXEC') or die;
 
+use Joomla\CMS\Factory;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Router\Route;
+
 $module  = $displayData['module'];
 $params  = $displayData['params'];
-$attribs = $displayData['attribs'];
 
-$modulePos   = $module->position;
+// Permission checks
+$user    = Factory::getUser();
+$canEdit = $user->authorise('core.edit', 'com_modules.module.' . $module->id);
+
 $moduleTag   = $params->get('module_tag', 'div');
-$headerTag   = htmlspecialchars($params->get('header_tag', 'h4'));
-$headerClass = htmlspecialchars($params->get('header_class', ''));
+$bootstrapSize  = (int) $params->get('bootstrap_size', 6);
+$moduleClass    = ($bootstrapSize) ? 'col-md-' . $bootstrapSize : 'col-md-12';
+$headerTag      = htmlspecialchars($params->get('header_tag', 'h2'));
+$moduleClassSfx = $params->get('moduleclass_sfx', '');
 
-if ($module->content)
-{
-	echo '<' . $moduleTag . ' class="' . $modulePos . ' card ' . htmlspecialchars($params->get('moduleclass_sfx')) . '">';
-	if ($module->showtitle && $headerClass !== 'card-title')
-	{
-		echo '<' . $headerTag . ' class="card-header' . $headerClass . '">' . $module->title . '</' . $headerTag . '>';
-	}
-	echo '<div class="card-body">';
-	if ($module->showtitle && $headerClass === 'card-title')
-	{
-		echo '<' . $headerTag . ' class="' . $headerClass . '">' . $module->title . '</' . $headerTag . '>';
-	}
-	echo $module->content;
-	echo '</div>';
-	echo '</' . $moduleTag . '>';
-}
+// Temporarily store header class in variable
+$headerClass    = $params->get('header_class');
+$headerClass    = ($headerClass) ? ' ' . htmlspecialchars($headerClass) : '';
+
+if ($module->content) : ?>
+	<div class="<?php echo $moduleClass; ?>">
+		<<?php echo $moduleTag; ?> class="card mb-3<?php echo $moduleClassSfx; ?>">
+			<?php if ($canEdit) : ?>
+				<div class="module-actions">
+					<a href="<?php echo Route::_('index.php?option=com_modules&task=module.edit&id=' . (int) $module->id); ?>">
+						<span class="fa fa-cog"><span class="sr-only"><?php echo Text::_('JACTION_EDIT') . " " . $module->title; ?></span></span>
+					</a>
+				</div>
+			<?php endif; ?>
+			<?php if ($module->showtitle) : ?>
+				<<?php echo $headerTag; ?> class="card-header<?php echo $headerClass; ?>"><?php echo $module->title; ?></<?php echo $headerTag; ?>>
+			<?php endif; ?>
+			<div class="module-body">
+				<?php echo $module->content; ?>
+			</div>
+		</<?php echo $moduleTag; ?>>
+	</div>
+<?php endif; ?>
