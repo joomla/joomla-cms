b5847ba5218f5afd56ca29014319c7b7da98d621
dGrammatiko <d.grammatiko@gmail.com>
2019-01-23 01:12:39 +0100

    [4.0] Fix for #23629

--- build/media_src/com_modules/js/admin-module-edit.es6.js
+++ build/media_src/com_modules/js/admin-module-edit.es6.js
@@ -95,10 +95,9 @@ Joomla = window.Joomla || {};
             window.parent.document.querySelector(`#access-${options.itemId}`).innerHTML = window.parent.viewLevels[updAccess];
           }
         }
-      }
-
-      if (task !== 'module.apply') {
-        window.parent.Joomla.Modal.getCurrent().close();
+        if (task !== 'module.apply') {
+            window.parent.Joomla.Modal.getCurrent().close();
+        }
       }
     }
   };
