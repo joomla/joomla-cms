4a0651d7b457998e7d7c4f97e5386a991e13e75b
Brian Teeman <brian@teeman.net>
2019-01-26 14:47:19 +0000

    Updated to use JDATE instead of unique string

--- administrator/modules/mod_logged/tmpl/default.php
+++ administrator/modules/mod_logged/tmpl/default.php
@@ -25,7 +25,7 @@ use Joomla\CMS\Language\Text;
 				<?php endif; ?>
 			</th>
 			<th scope="col" style="width:30%"><?php echo Text::_('JCLIENT'); ?></th>
-			<th scope="col" style="width:20%"><?php echo Text::_('MOD_LOGGED_LAST_ACTIVITY'); ?></th>
+			<th scope="col" style="width:20%"><?php echo Text::_('JDATE'); ?></th>
 		</tr>
 	</thead>
 	<tbody>
