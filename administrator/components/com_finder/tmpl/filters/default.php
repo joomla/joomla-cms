4f020402184847f2055678eedfce85619c5ff2d5
Quy <quy@fluxbb.org>
2019-01-29 13:11:06 +0530

    Update administrator/components/com_finder/tmpl/filters/default.php
    Co-Authored-By: amitranjan2 <41797766+amitranjan2@users.noreply.github.com>

--- administrator/components/com_finder/tmpl/filters/default.php
+++ administrator/components/com_finder/tmpl/filters/default.php
@@ -12,7 +12,7 @@ defined('_JEXEC') or die;
 use Joomla\CMS\Factory;
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
-use Joomla\CMS\Layout\LayoutHelper
+use Joomla\CMS\Layout\LayoutHelper;
 use Joomla\CMS\Router\Route;
 
 $user      = Factory::getUser();
