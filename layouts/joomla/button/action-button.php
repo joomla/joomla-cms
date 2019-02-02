c53c512af54f67f78eccba137701ee8087c031e8
chmst <dev@chmst.de>
2019-02-02 22:46:46 +0100

    Remove title again

--- layouts/joomla/button/action-button.php
+++ layouts/joomla/button/action-button.php
@@ -26,14 +26,13 @@ $taskPrefix = $options['task_prefix'];
 $checkboxName = $options['checkbox_name'];
 ?>
 <?php if(!empty($disabled)): ?>
-		<span class="<?php echo $this->escape($icon ?? ''); ?>" aria-hidden="true" title="<?php echo Text::_($title); ?>"></span>
+		<span class="<?php echo $this->escape($icon ?? ''); ?>" aria-hidden="true"></span>
 		<span class="sr-only"><?php echo Text::_($title); ?></span>
 <?php else: ?>
 	<button type="submit" class="tbody-icon data-state-<?php echo $this->escape($value ?? ''); ?> <?php echo $this->escape(!empty($disabled) ? 'disabled' : null); ?>"
 		<?php if(!empty($task) && empty($disabled)): ?>
 			onclick="return Joomla.listItemTask('<?php echo $checkboxName . $this->escape($row ?? ''); ?>', '<?php echo $this->escape(isset($task) ? $taskPrefix . $task : ''); ?>')"
 		<?php endif; ?>
-		title="<?php echo Text::_($title); ?>"
 	>
 		<span class="<?php echo $this->escape($icon ?? ''); ?>" aria-hidden="true"></span>
 		<span class="sr-only"><?php echo Text::_($title); ?></span>
