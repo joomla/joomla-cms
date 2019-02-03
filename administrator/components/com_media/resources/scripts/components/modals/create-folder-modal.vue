8dc1b89324858301a3b2d637b38f3266eeb93330
puneethchanda <puneethchanda2001@gmail.com>
2019-02-03 22:55:24 +0530

    Updated by removing the placeholder

--- administrator/components/com_media/resources/scripts/components/modals/create-folder-modal.vue
+++ administrator/components/com_media/resources/scripts/components/modals/create-folder-modal.vue
@@ -5,7 +5,7 @@
             <form class="form" @submit.prevent="save" novalidate>
                 <div class="form-group">
                     <label for="folder">{{ translate('COM_MEDIA_FOLDER') }}</label>
-                    <input id="folder" class="form-control" placeholder="Name of the new folder"
+                    <input id="folder" class="form-control" 
                            v-focus="true" v-model.trim="folder" @input="folder = $event.target.value"
                            required autocomplete="off">
                 </div>
