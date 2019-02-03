df41156fdbe20d9ef15247d017034c51c31b9f88
Dimitris Grammatikogiannis <d.grammatiko@gmail.com>
2019-02-03 20:16:17 +0100

    Joomla says hello to Web assembly

--- build/build-modules-js/gzip-assets.js
+++ build/build-modules-js/gzip-assets.js
@@ -1,14 +1,13 @@
 const Fs = require('fs');
-const RootPath = require('./rootpath.js')._();
-const WalkSync = require('./walk-sync.js');
-const zopfli = require('node-zopfli-es');
+const RootPath = require('./utils/rootpath.es6.js')._();
+const WalkSync = require('./utils/walk-sync.es6.js');
+const universalZopfli = require('@gfx/zopfli/dist');
 
 const options = {
   verbose: false,
   verbose_more: false,
   numiterations: 15,
   blocksplitting: true,
-  blocksplittinglast: false,
   blocksplittingmax: 15,
 };
 
@@ -28,20 +27,38 @@ const gzipFiles = () => {
         }
         if (file.match(/\.min\.js/) && !file.toLowerCase().match(/json/) && !file.toLowerCase().match(/license/)) {
           console.log(`Processing: ${file}`);
-          // Create the gziped file
-          Fs.createReadStream(file)
-            .pipe(zopfli.createGzip(options))
-            .pipe(Fs.createWriteStream(file.replace(/\.js$/, '.js.gz')));
+          // Gzip the file
+          universalZopfli.gzip(Fs.readFileSync(file, 'utf8'), options, (err, output) => {
+            if (!err) {
+              // Save the gzipped file
+              Fs.writeFileSync(
+                file.replace(/\.js$/, '.js.gz'),
+                output,
+                { encoding: 'utf8' },
+              )
+            } else {
+              console.dir(`error: ${err}`);
+            }
+          });
         }
         if (file.match(/\.min\.css/) && !file.match(/\.css\.map/) && !file.toLowerCase().match(/license/)) {
           console.log(`Processing: ${file}`);
-          // Create the gziped file
-          Fs.createReadStream(file)
-            .pipe(zopfli.createGzip(options))
-            .pipe(Fs.createWriteStream(file.replace(/\.css$/, '.css.gz')));
+          // Gzip the file
+          universalZopfli.gzip(Fs.readFileSync(file, 'utf8'), options, (err, output) => {
+            if (!err) {
+              // Save the gzipped file
+              Fs.writeFileSync(
+                file.replace(/\.css$/, '.css.gz'),
+                output,
+                { encoding: 'utf8' },
+              )
+            } else {
+              console.dir(`error: ${err}`);
+            }
+          });
         }
       });
   }
 };
 
-module.exports.run = gzipFiles
+module.exports.run = gzipFiles;
