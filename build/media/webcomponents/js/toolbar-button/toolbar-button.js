5d62959097cab13ead23db85178485da2704e025
C-Lodder <lodder15@hotmail.co.uk>
2019-02-04 18:43:08 +0000

    Toolbar custom element improvements

--- build/media/webcomponents/js/toolbar-button/toolbar-button.js
+++ build/media/webcomponents/js/toolbar-button/toolbar-button.js
@@ -2,99 +2,125 @@
  * @copyright  Copyright (C) 2005 - 2018 Open Source Matters, Inc. All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
+window.customElements.define('joomla-toolbar-button', class extends HTMLElement {
+  // Attribute getters
+  get task() {
+    return this.getAttribute('task');
+  }
 
-((customElements) => {
-  'use strict';
+  get listSelection() {
+    return this.hasAttribute('list-selection');
+  }
 
-  class JoomlaToolbarButton extends HTMLElement {
-    // Attribute getters
-    get task() { return this.getAttribute('task'); }
-    get listSelection() { return this.hasAttribute('list-selection'); }
-    get form() { return this.getAttribute('form'); }
-    get formValidation() { return this.hasAttribute('form-validation'); }
-    get confirmMessage() { return this.getAttribute('confirm-message'); }
+  get form() {
+    return this.getAttribute('form');
+  }
 
-    constructor() {
-      super();
+  get formValidation() {
+    return this.hasAttribute('form-validation');
+  }
 
-      // We need a button to support button behavior,
-      // because we cannot currently extend HTMLButtonElement
-      this.buttonElement = this.querySelector('button, a');
-      this.disabled = false;
+  get confirmMessage() {
+    return this.getAttribute('confirm-message');
+  }
 
-      // If list selection are required, set button to disabled by default
-      if (this.listSelection) {
-        this.setDisabled(true);
-      }
+  /**
+   * Lifecycle
+   */
+  constructor() {
+    super();
 
-      this.addEventListener('click', e => this.executeTask(e));
+    if (!Joomla) {
+      throw new Error('Joomla API is not properly initiated');
     }
 
-    connectedCallback() {
-      // Check whether we have a form
-      const formSelector = this.form || 'adminForm';
-      this.formElement = document.getElementById(formSelector);
+    // We need a button to support button behavior,
+    // because we cannot currently extend HTMLButtonElement
+    this.buttonElement = this.querySelector('button, a');
+    this.disabled = false;
 
-      if (this.listSelection) {
-        if (!this.formElement) {
-          throw new Error(`The form "${formSelector}" is required to perform the task, but the form not found on the page.`);
-        }
-
-        // Watch on list selection
-        this.formElement.boxchecked.addEventListener('change', (event) => {
-          // Check whether we have selected something
-          this.setDisabled(event.target.value < 1);
-        });
-      }
+    // If list selection are required, set button to disabled by default
+    if (this.listSelection) {
+      this.setDisabled(true);
     }
 
-    setDisabled(disabled) {
-      // Make sure we have a boolean value
-      this.disabled = !!disabled;
+    this.addEventListener('click', event => this.executeTask(event));
+  }
 
-      // Switch attribute for current element
-      if (this.disabled) {
-        this.setAttribute('disabled', true);
-      } else {
-        this.removeAttribute('disabled');
+  /**
+   * Lifecycle
+   */
+  connectedCallback() {
+    // Check whether we have a form
+    const formSelector = this.form || 'adminForm';
+    this.formElement = document.getElementById(formSelector);
+
+    if (this.listSelection) {
+      if (!this.formElement) {
+        throw new Error(`The form "${formSelector}" is required to perform the task, but the form not found on the page.`);
       }
 
-      // Switch attribute for native element
-      // An anchor do not support "disabled" attribute, so use class
-      if (this.buttonElement) {
-        if (this.disabled) {
-          if (this.buttonElement.nodeName === 'BUTTON') {
-            this.buttonElement.setAttribute('disabled', true)
-          } else {
-            this.buttonElement.classList.add('disabled');
-          }
-        } else {
-          if (this.buttonElement.nodeName === 'BUTTON') {
-            this.buttonElement.removeAttribute('disabled');
-          } else {
-            this.buttonElement.classList.remove('disabled');
-          }
-        }
-      }
-    }
-
-    executeTask() {
-      if (this.disabled) {
-        return false;
-      }
-
-      // eslint-disable-next-line no-restricted-globals
-      if (this.confirmMessage && !confirm(this.confirmMessage)) {
-        return false;
-      }
-
-      if (this.task) {
-        Joomla.submitbutton(this.task, this.form, this.formValidation);
-      }
-
-      return true;
+      // Watch on list selection
+      this.formElement.boxchecked.addEventListener('change', (event) => {
+        // Check whether we have selected something
+        this.setDisabled(event.target.value < 1);
+      });
     }
   }
 
-  customElements.define('joomla-toolbar-button', JoomlaToolbarButton);
-})(customElements);
+  /**
+   * Lifecycle
+   */
+  disconnectedCallback() {
+    if (this.formElement.boxchecked) {
+      this.formElement.boxchecked.removeEventListener('change');
+    }
+
+    this.removeEventListener('click');
+  }
+
+  setDisabled(disabled) {
+    // Make sure we have a boolean value
+    this.disabled = !!disabled;
+
+    // Switch attribute for current element
+    if (this.disabled) {
+      this.setAttribute('disabled', true);
+    } else {
+      this.removeAttribute('disabled');
+    }
+
+    // Switch attribute for native element
+    // An anchor do not support "disabled" attribute, so use class
+    if (this.buttonElement) {
+      if (this.disabled) {
+        if (this.buttonElement.nodeName === 'BUTTON') {
+          this.buttonElement.setAttribute('disabled', true);
+        } else {
+          this.buttonElement.classList.add('disabled');
+        }
+      } else if (this.buttonElement.nodeName === 'BUTTON') {
+        this.buttonElement.removeAttribute('disabled');
+      } else {
+        this.buttonElement.classList.remove('disabled');
+      }
+    }
+  }
+
+  executeTask() {
+    if (this.disabled) {
+      return false;
+    }
+
+    // eslint-disable-next-line no-restricted-globals
+    if (this.confirmMessage && !confirm(this.confirmMessage)) {
+      return false;
+    }
+
+    if (this.task) {
+      Joomla.submitbutton(this.task, this.form, this.formValidation);
+    }
+
+    return true;
+  }
+});
