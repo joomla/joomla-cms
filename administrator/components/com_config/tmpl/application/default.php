1f02dc9093e999bb5514b6c18af5cb668fca83ef
Dimitris Grammatikogiannis <d.grammatiko@gmail.com>
2019-02-06 17:07:14 +0100

    CS again

--- administrator/components/com_config/tmpl/application/default.php
+++ administrator/components/com_config/tmpl/application/default.php
@@ -44,64 +44,65 @@ Text::script('MESSAGE');
 			</div>
 		</div>
 		<div class="col-md-10">
-			<?php echo HTMLHelper::_('uitab.startTabSet', 'gc_config', array('active' => 'config-document')); ?>
+		<?php echo HTMLHelper::_('uitab.startTabSet', 'gc_config', array('active' => 'config-document')); ?>
 			<?php echo HTMLHelper::_('uitab.addTab', 'gc_config', 'config-document', Text::_('JSITE')); ?>
-			<div class="row">
-				<div class="col-lg-12 col-xl-6">
-					<?php echo $this->loadTemplate('site'); ?>
-					<?php echo $this->loadTemplate('metadata'); ?>
+				<div class="row">
+					<div class="col-lg-12 col-xl-6">
+						<?php echo $this->loadTemplate('site'); ?>
+						<?php echo $this->loadTemplate('metadata'); ?>
+					</div>
+					<div class="col-lg-12 col-xl-6">
+						<?php echo $this->loadTemplate('seo'); ?>
+						<?php echo $this->loadTemplate('cookie'); ?>
+					</div>
 				</div>
-				<div class="col-lg-12 col-xl-6">
-					<?php echo $this->loadTemplate('seo'); ?>
-					<?php echo $this->loadTemplate('cookie'); ?>
-				</div>
-			</div>
 			<?php echo JHtml::_('uitab.endTab'); ?>
 			<?php echo HTMLHelper::_('uitab.addTab', 'gc_config', 'page-system', Text::_('COM_CONFIG_SYSTEM')); ?>
-			<div class="row">
-				<div class="col-md-12">
-					<?php echo $this->loadTemplate('system'); ?>
-					<?php echo $this->loadTemplate('debug'); ?>
-					<?php echo $this->loadTemplate('cache'); ?>
-					<?php echo $this->loadTemplate('session'); ?>
+				<div class="row">
+					<div class="col-md-12">
+						<?php echo $this->loadTemplate('system'); ?>
+						<?php echo $this->loadTemplate('debug'); ?>
+						<?php echo $this->loadTemplate('cache'); ?>
+						<?php echo $this->loadTemplate('session'); ?>
+					</div>
 				</div>
-			</div>
 			<?php echo JHtml::_('uitab.endTab'); ?>
 			<?php echo HTMLHelper::_('uitab.addTab', 'gc_config', 'page-server', Text::_('COM_CONFIG_SERVER')); ?>
-			<div class="row">
-				<div class="col-lg-12 col-xl-6">
-					<?php echo $this->loadTemplate('server'); ?>
-					<?php echo $this->loadTemplate('locale'); ?>
-					<?php echo $this->loadTemplate('ftp'); ?>
-					<?php echo $this->loadTemplate('proxy'); ?>
+				<div class="row">
+					<div class="col-lg-12 col-xl-6">
+						<?php echo $this->loadTemplate('server'); ?>
+						<?php echo $this->loadTemplate('locale'); ?>
+						<?php echo $this->loadTemplate('ftp'); ?>
+						<?php echo $this->loadTemplate('proxy'); ?>
+					</div>
+					<div class="col-lg-12 col-xl-6">
+						<?php echo $this->loadTemplate('database'); ?>
+						<?php echo $this->loadTemplate('mail'); ?>
+					</div>
 				</div>
-				<div class="col-lg-12 col-xl-6">
-					<?php echo $this->loadTemplate('database'); ?>
-					<?php echo $this->loadTemplate('mail'); ?>
-				</div>
-			</div>
 			<?php echo JHtml::_('uitab.endTab'); ?>
 			<?php echo HTMLHelper::_('uitab.addTab', 'gc_config', 'page-filters', Text::_('COM_CONFIG_TEXT_FILTERS')); ?>
 				<div class="row">
-				<div class="col-md-12">
-					<?php echo $this->loadTemplate('filters'); ?>
+					<div class="col-md-12">
+						<?php echo $this->loadTemplate('filters'); ?>
+					</div>
 				</div>
-			</div>
 			<?php echo JHtml::_('uitab.endTab'); ?>
 			<?php if ($this->ftp) : ?>
 				<?php echo HTMLHelper::_('uitab.addTab', 'gc_config', 'page-ftp', Text::_('COM_CONFIG_FTP_SETTINGS')); ?>
-				<div class="col-md-12">
-					<?php echo $this->loadTemplate('ftplogin'); ?>
-				</div>
+					<div class="col-md-12">
+						<?php echo $this->loadTemplate('ftplogin'); ?>
+					</div>
 				<?php echo JHtml::_('uitab.endTab'); ?>
 			<?php endif; ?>
 			<?php echo HTMLHelper::_('uitab.addTab', 'gc_config', 'page-permissions', Text::_('COM_CONFIG_PERMISSIONS')); ?>
-			<div class="row">
-				<?php echo $this->loadTemplate('permissions'); ?>
-			</div>
+				<div class="row">
+					<?php echo $this->loadTemplate('permissions'); ?>
+				</div>
 			<?php echo JHtml::_('uitab.endTab'); ?>
 			<input type="hidden" name="task" value="">
 			<?php echo HTMLHelper::_('form.token'); ?>
+		<?php echo JHtml::_('uitab.endTabSet'); ?>
 		</div>
 	</div>
 </form>
