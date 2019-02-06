9e42f15297f0ff10bf8283349ccc5226020f36c3
Dimitris Grammatikogiannis <d.grammatiko@gmail.com>
2019-02-06 17:21:26 +0100

    Also the other view

--- administrator/components/com_config/tmpl/component/default.php
+++ administrator/components/com_config/tmpl/component/default.php
@@ -26,41 +26,22 @@ Text::script('MESSAGE');
 // Load the tooltip behavior.
 HTMLHelper::_('behavior.formvalidator');
 HTMLHelper::_('behavior.keepalive');
-HTMLHelper::_('behavior.tabstate');
 
 // @TODO delete this when custom elements modal is merged
 HTMLHelper::_('script', 'com_config/admin-application-default.min.js', ['version' => 'auto', 'relative' => true]);
 ?>
-
 <form action="<?php echo Route::_('index.php?option=com_config'); ?>" id="component-form" method="post" class="form-validate" name="adminForm" autocomplete="off" data-cancel-task="config.cancel.component">
 	<div class="row">
-
-		<?php // Begin Sidebar ?>
 		<div class="col-md-2" id="sidebar">
 			<div class="sidebar-nav">
 				<?php echo $this->loadTemplate('navigation'); ?>
 			</div>
 		</div>
-		<?php // End Sidebar ?>
-
 		<div class="col-md-10" id="config">
-
+		<?php echo HTMLHelper::_('uitab.startTabSet', 'gc_config_component', array('active' => 'config-document')); ?>
 			<?php if ($this->fieldsets): ?>
-			<ul class="nav nav-tabs" id="configTabs">
 				<?php foreach ($this->fieldsets as $name => $fieldSet) : ?>
-					<?php $dataShowOn = ''; ?>
-					<?php if (!empty($fieldSet->showon)) : ?>
-						<?php HTMLHelper::_('script', 'system/showon.min.js', array('version' => 'auto', 'relative' => true)); ?>
-						<?php $dataShowOn = ' data-showon=\'' . json_encode(FormHelper::parseShowOnConditions($fieldSet->showon, $this->formControl)) . '\''; ?>
-					<?php endif; ?>
-					<?php $label = empty($fieldSet->label) ? 'COM_CONFIG_' . $name . '_FIELDSET_LABEL' : $fieldSet->label; ?>
-					<li class="nav-item"<?php echo $dataShowOn; ?>><a class="nav-link" data-toggle="tab" href="#<?php echo $name; ?>"><?php echo Text::_($label); ?></a></li>
-				<?php endforeach; ?>
-			</ul>
-
-			<div class="tab-content" id="configContent">
-				<?php foreach ($this->fieldsets as $name => $fieldSet) : ?>
-					<div class="tab-pane" id="<?php echo $name; ?>">
+					<?php echo HTMLHelper::_('uitab.addTab', 'gc_config_component', $name, Text::_($fieldSet->label)); ?>
 						<?php if (isset($fieldSet->description) && !empty($fieldSet->description)) : ?>
 							<div class="alert alert-info">
 								<span class="icon-info" aria-hidden="true"></span> <?php echo Text::_($fieldSet->description); ?>
@@ -90,18 +71,16 @@ HTMLHelper::_('script', 'com_config/admin-application-default.min.js', ['version
 								</div>
 							<?php endif; ?>
 						<?php endforeach; ?>
-					</div>
+					<?php echo JHtml::_('uitab.endTab'); ?>
 				<?php endforeach; ?>
-			</div>
 			<?php else: ?>
 				<div class="alert alert-info">
 					<span class="icon-info" aria-hidden="true"></span>
 					<?php echo Text::_('COM_CONFIG_COMPONENT_NO_CONFIG_FIELDS_MESSAGE'); ?>
 				</div>
 			<?php endif; ?>
-
+			<?php echo JHtml::_('uitab.endTabSet'); ?>
 		</div>
-
 		<input type="hidden" name="id" value="<?php echo $this->component->id; ?>">
 		<input type="hidden" name="component" value="<?php echo $this->component->option; ?>">
 		<input type="hidden" name="return" value="<?php echo $this->return; ?>">
