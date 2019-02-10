b20c27a7fa457305a2c79695efb78f77a5f1e7c3
amitranjan2 <amit1iitp@gmail.com>
2019-02-10 06:12:45 +0530

    fi

--- administrator/components/com_templates/tmpl/style/edit_assignment.php
+++ administrator/components/com_templates/tmpl/style/edit_assignment.php
@@ -30,14 +30,14 @@ $user      = Factory::getUser();
 		<?php foreach ($menuTypes as &$type) : ?>
 			<li>
 				<div class="menu-links-block">
-					<button class="btn btn-sm btn-secondary jform-rightbtn mb-2" type="button" onclick="jQuery('.<?php echo $type->menutype; ?>').attr('checked', !jQuery('.<?php echo $type->menutype; ?>').attr('checked'));">
+					<button class="btn btn-sm btn-secondary jform-rightbtn mb-2" type="button" onclick="jQuery('.<?php echo str_replace(' ', '-', $type->menutype) ; ?>').attr('checked', !jQuery('.<?php echo str_replace(' ', '-', $type->menutype); ?>').attr('checked'));">
 						<span class="icon-checkbox-partial" aria-hidden="true"></span> <?php echo Text::_('JGLOBAL_SELECTION_INVERT'); ?>
 					</button>
 					<h5><?php echo $type->title ?: $type->menutype; ?></h5>
 
 					<?php foreach ($type->links as $link) : ?>
 						<label class="checkbox small" for="link<?php echo (int) $link->value; ?>" >
-						<input type="checkbox" name="jform[assigned][]" value="<?php echo (int) $link->value; ?>" id="link<?php echo (int) $link->value; ?>"<?php if ($link->template_style_id == $this->item->id) : ?> checked="checked"<?php endif; ?><?php if ($link->checked_out && $link->checked_out != $user->id) : ?> disabled="disabled"<?php else : ?> class="chk-menulink <?php echo $type->menutype; ?>"<?php endif; ?> />
+						<input type="checkbox" name="jform[assigned][]" value="<?php echo (int) $link->value; ?>" id="link<?php echo (int) $link->value; ?>"<?php if ($link->template_style_id == $this->item->id) : ?> checked="checked" <?php endif; ?><?php if ($link->checked_out && $link->checked_out != $user->id) : ?> disabled="disabled"<?php else : ?> class="chk-menulink <?php echo str_replace(' ', '-', $type->menutype); ?>"<?php endif; ?> />
 						<?php echo LayoutHelper::render('joomla.html.treeprefix', array('level' => $link->level)) . $link->text; ?>
 						</label>
 					<?php endforeach; ?>
