4fc51ede65437208a06f462aead6b1b2996bd8e6
Brian Teeman <brian@teeman.net>
2019-02-16 22:23:17 +0000

    url used in system-sef plugin

--- layouts/joomla/form/field/url.php
+++ layouts/joomla/form/field/url.php
@@ -48,9 +48,11 @@ extract($displayData);
 
 $autocomplete = !$autocomplete ? ' autocomplete="off"' : ' autocomplete="' . $autocomplete . '"';
 $autocomplete = $autocomplete === ' autocomplete="on"' ? '' : $autocomplete;
+$aria = $name . '-desc';
 
 $attributes = array(
 	!empty($size) ? ' size="' . $size . '"' : '',
+	!empty($description) ? ' aria-describedby="' . $aria . '"' : '',
 	$disabled ? ' disabled' : '',
 	$readonly ? ' readonly' : '',
 	strlen($hint) ? ' placeholder="' . htmlspecialchars($hint, ENT_COMPAT, 'UTF-8') . '"' : '',
