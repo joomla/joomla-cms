3550c8618558429fc52a8caab6d5ef2d0067355d
nadjak77 <web@lamisch.de>
2019-02-20 12:06:37 +0100

    calculate backgroundcolors change some colors

--- administrator/templates/atum/index.php
+++ administrator/templates/atum/index.php
@@ -74,9 +74,17 @@ if ($this->params->get('bg-dark'))
 
 	try {
 		$color = new Hex($bgcolor);
+		$colorHsl=$color->toHsl();
 
-		$root[] = '--atum-bg-dark-light: ' . (clone $color)->lighten(20) . ';';
-		$root[] = '--atum-bg-dark-bright: ' . (clone $color)->lighten(40) . ';';
+		$root[] = '--atum-contrast: ' . (clone $colorHsl)->lighten(-6)->spin(-30)->toHex()  . ';';
+		$root[] = '--atum-bg-dark-10: ' . (clone $colorHsl)->desaturate(86)->lighten(20.5)->spin(-6)->toHex()  . ';';
+        $root[] = '--atum-bg-dark-20: ' . (clone $colorHsl)->desaturate(76)->lighten(16.5)->spin(-6)->toHex()  . ';';
+        $root[] = '--atum-bg-dark-30: ' . (clone $colorHsl)->desaturate(60)->lighten(12)->spin(-5)->toHex()  . ';';
+        $root[] = '--atum-bg-dark-40: ' . (clone $colorHsl)->desaturate(41)->lighten(8)->spin(-3)->toHex()  . ';';
+        $root[] = '--atum-bg-dark-50: ' . (clone $colorHsl)->desaturate(19)->lighten(4)->spin(-1)->toHex()  . ';';
+        $root[] = '--atum-bg-dark-70: ' . (clone $colorHsl)->lighten(-6)->spin(4)->toHex()  . ';';
+        $root[] = '--atum-bg-dark-80: ' . (clone $colorHsl)->lighten(-11.5)->spin(7)->toHex()  . ';';
+        $root[] = '--atum-bg-dark-90: ' . (clone $colorHsl)->desaturate(1)->lighten(-17)->spin(10)->toHex()  . ';';
 	} catch (Exception $ex) {
 
 	}
