f788a0e02cda6fd6f2d78825474307d3b2c0ace7
Franciska Perisa <franciska.perisa@mni.thm.de>
2019-02-20 17:37:36 +0100

    update error page and some refactoring/formatting

--- administrator/templates/atum/login.php
+++ administrator/templates/atum/login.php
@@ -12,32 +12,32 @@ defined('_JEXEC') or die;
 use Joomla\CMS\Factory;
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
-use Joomla\CMS\Router\Route;
 use Joomla\CMS\Uri\Uri;
 use OzdemirBurak\Iris\Color\Hex;
 
 /** @var JDocumentHtml $this */
 
 $app  = Factory::getApplication();
-$lang = Factory::getLanguage();
+$lang = $app->getLanguage();
 
 // Add JavaScript Frameworks
-HTMLHelper::_('script', 'vendor/focus-visible/focus-visible.min.js', ['version' => 'auto', 'relative' => true]);
 HTMLHelper::_('script', 'vendor/css-vars-ponyfill/css-vars-ponyfill.min.js', ['version' => 'auto', 'relative' => true]);
 
 // Load template JS file
 HTMLHelper::_('script', 'media/templates/' . $this->template . '/js/template.min.js', ['version' => 'auto']);
 
 // Load template CSS file
-HTMLHelper::_('stylesheet', 'bootstrap.css', ['version' => 'auto', 'relative' => true]);
-HTMLHelper::_('stylesheet', 'font-awesome.css', ['version' => 'auto', 'relative' => true]);
-HTMLHelper::_('stylesheet', 'template' . ($this->direction === 'rtl' ? '-rtl' : '') . '.css', ['version' => 'auto', 'relative' => true]);
+HTMLHelper::_('stylesheet', 'bootstrap.min.css', ['version' => 'auto', 'relative' => true]);
+HTMLHelper::_('stylesheet', 'font-awesome.min.css', ['version' => 'auto', 'relative' => true]);
+HTMLHelper::_('stylesheet', 'template' . ($this->direction === 'rtl' ? '-rtl' : '') . '.min.css',
+	['version' => 'auto', 'relative' => true]);
 
 // Load custom CSS file
-HTMLHelper::_('stylesheet', 'user.css', array('version' => 'auto', 'relative' => true));
+HTMLHelper::_('stylesheet', 'user.css', ['version' => 'auto', 'relative' => true]);
 
 // Load specific language related CSS
-HTMLHelper::_('stylesheet', 'administrator/language/' . $lang->getTag() . '/' . $lang->getTag() . '.css', array('version' => 'auto'));
+HTMLHelper::_('stylesheet', 'administrator/language/' . $lang->getTag() . '/' . $lang->getTag() . '.css',
+	['version' => 'auto']);
 
 // Detecting Active Variables
 $option   = $app->input->getCmd('option', '');
@@ -46,23 +46,21 @@ $layout   = $app->input->getCmd('layout', 'default');
 $sitename = $app->get('sitename');
 
 // Template params
-$siteLogo = $this->params->get('siteLogo')
+$siteLogo  = $this->params->get('siteLogo')
 	? JUri::root() . $this->params->get('siteLogo')
-	: $this->params->get('siteLogo', $this->baseurl . '/templates/' . $this->template . '/images/logo-joomla-blue.svg');
+	: $this->baseurl . '/templates/' . $this->template . '/images/logo-joomla-blue.svg';
 $loginLogo = $this->params->get('loginLogo')
 	? JUri::root() . $this->params->get('loginLogo')
-	: $this->params->get('loginLogo', $this->baseurl . '/templates/' . $this->template . '/images/logo-blue.svg');
-
+	: $this->baseurl . '/templates/' . $this->template . '/images/logo-blue.svg';
 
 // Set some meta data
 $this->setMetaData('viewport', 'width=device-width, initial-scale=1');
 // @TODO sync with _variables.scss
 $this->setMetaData('theme-color', '#1c3d5c');
+$this->addScriptDeclaration('cssVars();');
 
 // Set page title
 $this->setTitle(Text::sprintf('TPL_ATUM_LOGIN_SITE_TITLE', $sitename));
-
-$this->addScriptDeclaration('cssVars();');
 
 // Opacity must be set before displaying the DOM, so don't move to a CSS file
 $css = '
@@ -80,70 +78,76 @@ $steps = 10;
 
 if ($this->params->get('bg-dark'))
 {
-    $bgcolor = trim($this->params->get('bg-dark'), '#');
+	$bgcolor = trim($this->params->get('bg-dark'), '#');
 
-    list($red, $green, $blue) = str_split($bgcolor, 2);
+	list($red, $green, $blue) = str_split($bgcolor, 2);
 
-    $root[] = '--atum-bg-dark: #' . $bgcolor . ';';
+	$root[] = '--atum-bg-dark: #' . $bgcolor . ';';
 
-    try {
-        $color = new Hex($bgcolor);
-        $colorHsl=$color->toHsl();
+	try
+	{
+		$color    = new Hex($bgcolor);
+		$colorHsl = $color->toHsl();
 
-        $root[] = '--atum-contrast: ' . (clone $colorHsl)->lighten(-6)->spin(-30)->toHex()  . ';';
-        $root[] = '--atum-bg-dark-10: ' . (clone $colorHsl)->desaturate(86)->lighten(20.5)->spin(-6)->toHex()  . ';';
-        $root[] = '--atum-bg-dark-20: ' . (clone $colorHsl)->desaturate(76)->lighten(16.5)->spin(-6)->toHex()  . ';';
-        $root[] = '--atum-bg-dark-30: ' . (clone $colorHsl)->desaturate(60)->lighten(12)->spin(-5)->toHex()  . ';';
-        $root[] = '--atum-bg-dark-40: ' . (clone $colorHsl)->desaturate(41)->lighten(8)->spin(-3)->toHex()  . ';';
-        $root[] = '--atum-bg-dark-50: ' . (clone $colorHsl)->desaturate(19)->lighten(4)->spin(-1)->toHex()  . ';';
-        $root[] = '--atum-bg-dark-70: ' . (clone $colorHsl)->lighten(-6)->spin(4)->toHex()  . ';';
-        $root[] = '--atum-bg-dark-80: ' . (clone $colorHsl)->lighten(-11.5)->spin(7)->toHex()  . ';';
-        $root[] = '--atum-bg-dark-90: ' . (clone $colorHsl)->desaturate(1)->lighten(-17)->spin(10)->toHex()  . ';';
-    } catch (Exception $ex) {
+		$root[] = '--atum-contrast: ' . (clone $colorHsl)->lighten(-6)->spin(-30)->toHex() . ';';
+		$root[] = '--atum-bg-dark-10: ' . (clone $colorHsl)->desaturate(86)->lighten(20.5)->spin(-6)->toHex() . ';';
+		$root[] = '--atum-bg-dark-20: ' . (clone $colorHsl)->desaturate(76)->lighten(16.5)->spin(-6)->toHex() . ';';
+		$root[] = '--atum-bg-dark-30: ' . (clone $colorHsl)->desaturate(60)->lighten(12)->spin(-5)->toHex() . ';';
+		$root[] = '--atum-bg-dark-40: ' . (clone $colorHsl)->desaturate(41)->lighten(8)->spin(-3)->toHex() . ';';
+		$root[] = '--atum-bg-dark-50: ' . (clone $colorHsl)->desaturate(19)->lighten(4)->spin(-1)->toHex() . ';';
+		$root[] = '--atum-bg-dark-70: ' . (clone $colorHsl)->lighten(-6)->spin(4)->toHex() . ';';
+		$root[] = '--atum-bg-dark-80: ' . (clone $colorHsl)->lighten(-11.5)->spin(7)->toHex() . ';';
+		$root[] = '--atum-bg-dark-90: ' . (clone $colorHsl)->desaturate(1)->lighten(-17)->spin(10)->toHex() . ';';
+	}
+	catch (Exception $ex)
+	{
 
-    }
+	}
 }
 
 if ($this->params->get('bg-light'))
 {
-    $root[] = '--atum-bg-light: ' . $this->params->get('bg-light') . ';';
+	$root[] = '--atum-bg-light: ' . $this->params->get('bg-light') . ';';
 }
 
 if ($this->params->get('text-dark'))
 {
-    $root[] = '--atum-text-dark: ' . $this->params->get('text-dark') . ';';
+	$root[] = '--atum-text-dark: ' . $this->params->get('text-dark') . ';';
 }
 
 if ($this->params->get('text-light'))
 {
-    $root[] = '--atum-text-light: ' . $this->params->get('text-light') . ';';
+	$root[] = '--atum-text-light: ' . $this->params->get('text-light') . ';';
 }
 
 if ($this->params->get('link-color'))
 {
-    $linkcolor = trim($this->params->get('link-color'), '#');
+	$linkcolor = trim($this->params->get('link-color'), '#');
 
-    list($red, $green, $blue) = str_split($linkcolor, 2);
+	list($red, $green, $blue) = str_split($linkcolor, 2);
 
-    $root[] = '--atum-link-color: #' . $linkcolor . ';';
+	$root[] = '--atum-link-color: #' . $linkcolor . ';';
 
-    try {
-        $color = new Hex($linkcolor);
+	try
+	{
+		$color = new Hex($linkcolor);
 
-        $root[] = '--atum-link-hover-color: ' . (clone $color)->darken(20) . ';';
-    } catch (Exception $ex) {
+		$root[] = '--atum-link-hover-color: ' . (clone $color)->darken(20) . ';';
+	}
+	catch (Exception $ex)
+	{
 
-    }
+	}
 }
 
 if ($this->params->get('special-color'))
 {
-    $root[] = '--atum-special-color: ' . $this->params->get('special-color') . ';';
+	$root[] = '--atum-special-color: ' . $this->params->get('special-color') . ';';
 }
 
 if (count($root))
 {
-    $css .= ':root {' . implode($root) . '}';
+	$css .= ':root {' . implode($root) . '}';
 }
 
 $this->addStyleDeclaration($css);
@@ -154,14 +158,13 @@ $this->addStyleDeclaration($css);
 	<jdoc:include type="metas"/>
 	<jdoc:include type="styles"/>
 </head>
-<body class="site <?php echo $option . ' view-' . $view . ' layout-' . $layout; ?>">
+<body class="admin <?php echo $option . ' view-' . $view . ' layout-' . $layout; ?>">
 <header id="header" class="header">
 	<div class="d-flex align-items-center">
 		<div class="header-title mr-auto">
-			<a class="logo" href="<?php echo Route::_('index.php'); ?>"
-			   aria-label="<?php echo Text::_('TPL_BACK_TO_CONTROL_PANEL'); ?>">
+			<div class="logo">
 				<img src="<?php echo $siteLogo; ?>" alt="">
-			</a>
+			</div>
 		</div>
 	</div>
 </header>
