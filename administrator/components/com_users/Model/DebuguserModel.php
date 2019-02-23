b0070593b185a476968f452d538319006a021da2
Jean-Marie Simonet <infografjms@gmail.com>
2019-02-23 17:52:00 +0100

    undo force load com_workflow.ini but use sys.ini

--- administrator/components/com_users/Model/DebuguserModel.php
+++ administrator/components/com_users/Model/DebuguserModel.php
@@ -124,7 +124,6 @@ class DebuguserModel extends ListModel
 	protected function populateState($ordering = 'a.lft', $direction = 'asc')
 	{
 		$app = Factory::getApplication();
-		Factory::getLanguage()->load('com_workflow', JPATH_ADMINISTRATOR);
 
 		// Adjust the context to support modal layouts.
 		$layout = $app->input->get('layout', 'default');
