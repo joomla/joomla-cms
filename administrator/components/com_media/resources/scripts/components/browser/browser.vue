b5e8e48c3914bff0be7b9d0b83bd3ae3486c6de8
Anurag <30978328+Anu1601CS@users.noreply.github.com>
2019-02-25 17:28:10 +0530

    Update browser.vue

--- administrator/components/com_media/resources/scripts/components/browser/browser.vue
+++ administrator/components/com_media/resources/scripts/components/browser/browser.vue
@@ -79,7 +79,7 @@
         methods: {
             /* Unselect all browser items */
             unselectAllBrowserItems(event) {
-                const clickedDelete = (event.target.id !== undefined && event.target.id === 'confirm_delete_item') ? true : false;
+                const clickedDelete = (event.target.id !== undefined && event.target.id === 'mediaDelete') ? true : false;
                 const notClickedBrowserItems = (this.$refs.browserItems && !this.$refs.browserItems.contains(event.target)) || event.target === this.$refs.browserItems;
                 const notClickedInfobar = this.$refs.infobar !== undefined && !this.$refs.infobar.$el.contains(event.target);
                 const clickedOutside = notClickedBrowserItems && notClickedInfobar && !clickedDelete;
