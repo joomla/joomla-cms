2ff27e043425aca65638fd500942a00e36dba239
Hannes Papenberg <info@joomlager.de>
2019-02-25 22:21:28 +0100

    Removing xhtml module chrome

--- templates/system/html/modules.php
+++ templates/system/html/modules.php
@@ -68,31 +68,6 @@ function modChrome_table($module, &$params, &$attribs)
 }
 
 /*
- * xhtml (divs and font header tags)
- * With the new advanced parameter it does the same as the html5 chrome
- */
-function modChrome_xhtml($module, &$params, &$attribs)
-{
-	$moduleTag      = htmlspecialchars($params->get('module_tag', 'div'), ENT_QUOTES, 'UTF-8');
-	$headerTag      = htmlspecialchars($params->get('header_tag', 'h3'), ENT_QUOTES, 'UTF-8');
-	$bootstrapSize  = (int) $params->get('bootstrap_size', 0);
-	$moduleClass    = $bootstrapSize !== 0 ? ' span' . $bootstrapSize : '';
-
-	// Temporarily store header class in variable
-	$headerClass    = $params->get('header_class');
-	$headerClass    = $headerClass ? ' class="' . htmlspecialchars($headerClass, ENT_COMPAT, 'UTF-8') . '"' : '';
-
-	if (!empty ($module->content)) : ?>
-		<<?php echo $moduleTag; ?> class="moduletable<?php echo htmlspecialchars($params->get('moduleclass_sfx'), ENT_COMPAT, 'UTF-8') . $moduleClass; ?>">
-			<?php if ((bool) $module->showtitle) : ?>
-				<<?php echo $headerTag . $headerClass . '>' . $module->title; ?></<?php echo $headerTag; ?>>
-			<?php endif; ?>
-			<?php echo $module->content; ?>
-		</<?php echo $moduleTag; ?>>
-	<?php endif;
-}
-
-/*
  * Module chrome that add preview information to the module
  */
 function modChrome_outline($module, &$params, &$attribs)
