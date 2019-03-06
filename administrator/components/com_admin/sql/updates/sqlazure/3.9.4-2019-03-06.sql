469b558abf9651f4f262729e7b8d8cb786c4f34e
SharkyKZ <sharkykz@gmail.com>
2019-03-06 10:52:47 +0200

    Add update file

---
+++ administrator/components/com_admin/sql/updates/sqlazure/3.9.4-2019-03-06.sql
@@ -0,0 +1,3 @@
+UPDATE "#__extensions" SET "element" = 'contact', "folder" = 'privacy' WHERE "name" = 'plg_privacy_contact';
+UPDATE "#__extensions" SET "element" = 'content', "folder" = 'privacy' WHERE "name" = 'plg_privacy_content';
+UPDATE "#__extensions" SET "element" = 'message', "folder" = 'privacy' WHERE "name" = 'plg_privacy_message';
