933d697b937bda005a2f34285ba9d716e083ea96
Brian Teeman <brian@teeman.net>
2019-03-07 14:22:11 +0000

    [4.0][a11y] RFC Adaptive Accessibility Menu
    This is a proof of concept for evaluation. Configuration options and translation have not been added at this point. Read more about the plugin here https://ranbuch.github.io/accessibility/
    
    To test you will need to
    1. apply the patch
    2. composer install
    3. npm i
    4. Discover the plugin
    5. Enable the plugin
    
    (I am not a fan of these tools but other people are - so I am offering it for evaluation.)

---
+++ plugins/system/accessibility/accessibility.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * @package     Joomla.Plugin
+ * @subpackage  System.accessibility
+ *
+ * @copyright   Copyright (C) 2005 - 2019 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Factory;
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Plugin\CMSPlugin;
+
+/**
+ * System plugin to add additional accessibility features to the administrator interface.".
+ *
+ * @since  __DEPLOY_VERSION__
+ */
+class PlgSystemAccessibility extends CMSPlugin
+{
+	/**
+	 * If true, language files will be loaded automatically.
+	 *
+	 * @var    boolean
+	 * @since  4.0.0
+	 */
+	protected $autoloadLanguage = true;
+
+	/**
+	 * Application object.
+	 *
+	 * @var    JApplicationCms
+	 * @since  4.0.0
+	 */
+	protected $app;
+
+	/**
+	 * Add the javascript for the accessibility menu
+	 *
+	 * @return  void
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 */
+	public function onBeforeCompileHead()
+	{
+		// Get the document object.
+		$document = Factory::getDocument();
+
+		if ($this->app->isClient('administrator'))
+		{
+		// Add strings for translations in Javascript.
+	    //	Factory::getDocument()->addScriptOptions(
+        //todo    
+
+		HTMLHelper::_('script', 'vendor/accessibility/accessibility.min.js', ['version' => 'auto', 'relative' => true], ['defer' => true]);
+		$document->addScriptDeclaration("window.addEventListener('load', function() { new Accessibility(); }, false);");
+		}
+	}
+}
