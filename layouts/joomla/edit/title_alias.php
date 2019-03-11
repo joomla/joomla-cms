9d7089d91aef2c110c10ee5999abf6110294fc37
chmst <dev@chmst.de>
2019-03-11 12:43:26 +0100

    Edit Form: Title and alias in a line

--- layouts/joomla/edit/title_alias.php
+++ layouts/joomla/edit/title_alias.php
@@ -14,9 +14,15 @@ $form  = $displayData->getForm();
 $title = $form->getField('title') ? 'title' : ($form->getField('name') ? 'name' : '');
 
 ?>
-<div class="m-t-2 m-b-3">
-	<?php
-	echo $title ? $form->renderField($title) : '';
-	echo $form->renderField('alias');
-	?>
-</div>
+<section  id="title-alias" class="title-alias" aria-labelledby="title-alias">
+	<fieldset class="form-vertical form-no-margin">
+		<div class="row">
+			<div class="col-lg-7 edit-title">
+				<?php echo $title ? $form->renderField($title) : ''; ?>
+			</div>
+			<div class="col-lg-5 edit-alias">
+				<?php echo $form->renderField('alias'); ?>
+			</div>
+		</div>
+	</fieldset>
+</section>
