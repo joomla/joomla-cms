f53d2a405924510e35da74030d00c94764626519
Lavinia Popa-Rössel <lavinia.popa-roessel@mni.thm.de>
2019-03-12 11:01:35 +0100

    changed icon of action-dropdowns

--- administrator/components/com_redirect/View/Links/HtmlView.php
+++ administrator/components/com_redirect/View/Links/HtmlView.php
@@ -157,7 +157,7 @@ class HtmlView extends BaseHtmlView
 			$dropdown = $toolbar->dropdownButton('status-group')
 				->text('JTOOLBAR_CHANGE_STATUS')
 				->toggleSplit(false)
-				->icon('fa fa-globe')
+				->icon('fa fa-exchange')
 				->buttonClass('btn btn-action')
 				->listCheck(true);
 
--- administrator/components/com_users/View/Notes/HtmlView.php
+++ administrator/components/com_users/View/Notes/HtmlView.php
@@ -149,7 +149,7 @@ class HtmlView extends BaseHtmlView
 			$dropdown = $toolbar->dropdownButton('status')
 					->text('JTOOLBAR_CHANGE_STATUS')
 					->toggleSplit(false)
-					->icon('fa fa-globe')
+					->icon('fa fa-exchange')
 					->buttonClass('btn btn-action')
 					->listCheck(true);
 
