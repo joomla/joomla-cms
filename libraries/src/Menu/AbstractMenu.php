febb334b719614c4d0866f7cad3df9ddf4267981
a-samoylov <a.samloylovua@gmail.com>
2019-03-20 18:07:09 +0200

    [#19123] - Public Submenu under Registered parent menu visible in incorrect place.

--- libraries/src/Menu/AbstractMenu.php
+++ libraries/src/Menu/AbstractMenu.php
@@ -253,10 +253,9 @@ class AbstractMenu
 	 */
 	public function getItems($attributes, $values, $firstonly = false)
 	{
-		$items = array();
-		$attributes = (array) $attributes;
-		$values = (array) $values;
-		$count = count($attributes);
+        $items      = array();
+        $attributes = (array) $attributes;
+        $values     = (array) $values;
 
 		foreach ($this->_items as $item)
 		{
@@ -265,15 +264,37 @@ class AbstractMenu
 				continue;
 			}
 
-			$test = true;
+            $isNeedAddItem = true;
 
-			for ($i = 0; $i < $count; $i++)
+			for ($i = 0; $i < count($attributes); $i++)
 			{
+			    // Check if parent item has access
+                if ($attributes[$i] == 'access') {
+
+                    $parentItem = $this->getItem($item->parent_id);
+
+                    while (!is_null($parentItem)) {
+
+                        if (!in_array($parentItem->{$attributes[$i]}, $values[$i])) {
+                            $isNeedAddItem = false;
+                            break;
+                        } else {
+                            if ($parentItem->{$attributes[$i]} != $values[$i])
+                            {
+                                $isNeedAddItem = false;
+                                break;
+                            }
+                        }
+
+                        $parentItem = $this->getItem($parentItem->parent_id);
+                    }
+                }
+
 				if (is_array($values[$i]))
 				{
 					if (!in_array($item->{$attributes[$i]}, $values[$i]))
 					{
-						$test = false;
+						$isNeedAddItem = false;
 						break;
 					}
 				}
@@ -281,13 +302,13 @@ class AbstractMenu
 				{
 					if ($item->{$attributes[$i]} != $values[$i])
 					{
-						$test = false;
+						$isNeedAddItem = false;
 						break;
 					}
 				}
 			}
 
-			if ($test)
+			if ($isNeedAddItem)
 			{
 				if ($firstonly)
 				{
