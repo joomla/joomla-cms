cd29f72ea924f5bfb3d08afa3b1ebe1f9afdfe86
Roland Dalmulder <contact@rolandd.com>
2019-03-23 09:43:58 +0100

    Update Postgres SQL #24026
    Signed-off-by: Roland Dalmulder <contact@rolandd.com>

--- administrator/components/com_admin/sql/updates/postgresql/4.0.0-2017-06-03.sql
+++ administrator/components/com_admin/sql/updates/postgresql/4.0.0-2017-06-03.sql
@@ -1,2 +1,2 @@
-ALTER TABLE "#__extensions" ADD COLUMN "changelogurl" text AFTER "element";
-ALTER TABLE "#__updates" ADD COLUMN "changelogurl" text AFTER `infourl`;
+ALTER TABLE "#__extensions" ADD COLUMN "changelogurl" text;
+ALTER TABLE "#__updates" ADD COLUMN "changelogurl" text;
