d8044e904c5c247aba302233f061c474e85273aa
Arpit Anshuman <f2016250@pilani.bits-pilani.ac.in>
2019-03-29 16:33:02 +0530

    Add password strength viewer

--- components/com_users/tmpl/registration/default.php
+++ components/com_users/tmpl/registration/default.php
@@ -71,3 +71,59 @@ HTMLHelper::_('behavior.formvalidator');
 		<?php echo HTMLHelper::_('form.token'); ?>
 	</form>
 </div>
+<script type="text/javascript">
+	$('input[type="password"]').eq(0).parent().parent().append('<div id="password-strength"> <div class="box box1"> <div class="bar-text"></div><div class="bar"></div></div><div class="box box2"> <div class="bar"></div></div><div class="box box3"> <div class="bar"></div></div><div class="box box4"> <div class="bar"></div></div></div>');
+	var result = $("#password-strength");
+
+	$('input[type="password"]').eq(0).keyup(function() {
+		$(".bar-text").html(checkStrength($('input[type="password"]').val()));
+	});
+
+	function checkStrength(password) {
+		//initial strength
+		var strength = 0;
+
+		if (password.length == 0) {
+			result.removeClass();
+			return "";
+		}
+		//if the password length is less than 4, return message, in line with existing validation.
+		if (password.length < 4) {
+			result.removeClass();
+			result.addClass("invalid");
+			return "Invalid";
+		}
+
+		//length is ok, lets continue.
+
+		//if length is 8 characters or more, increase strength value
+		if (password.length > 4) strength += 1;
+
+		//if password contains both lower and uppercase characters, increase strength value
+		if (password.match(/([a-z].*[A-Z])|([A-Z].*[a-z])/)) strength += 1;
+
+		//if it has one special character, increase strength value
+		if (password.match(/([!,%,&,@,#,$,^,*,?,_,~])/)) strength += 1;
+
+		//if it has two special characters, increase strength value
+		if (password.match(/(.*[!,%,&,@,#,$,^,*,?,_,~].*[!,",%,&,@,#,$,^,*,?,_,~])/))
+		strength += 1;
+
+		//now we have calculated strength value, we can return messages
+
+		//if value is less than 2
+		if (strength < 2) {
+			result.removeClass();
+			result.addClass("weak");
+			return "Weak";
+		} else if (strength == 2) {
+			result.removeClass();
+			result.addClass("medium");
+			return "Medium";
+		} else {
+			result.removeClass();
+			result.addClass("strong");
+			return "Strong";
+		}
+	}
+</script>
