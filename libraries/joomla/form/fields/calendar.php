89c4fb640af8d7b86b49d706aab59519892ab265
Arpit Anshuman <f2016250@pilani.bits-pilani.ac.in>
2019-03-30 16:31:04 +0530

    Format change to fix date issue
    Changing $this->format to the fixed format of "%Y-%m-%d %H:%M:%S" fixes the issue as the format is already converted and displayed properly using the calendar.js file so no need to change the format twice.

--- libraries/joomla/form/fields/calendar.php
+++ libraries/joomla/form/fields/calendar.php
@@ -249,7 +249,7 @@ class JFormFieldCalendar extends JFormField
 		{
 			$tz = date_default_timezone_get();
 			date_default_timezone_set('UTC');
-			$this->value = strftime($this->format, strtotime($this->value));
+			$this->value = strftime('%Y-%m-%d %H:%M:%S', strtotime($this->value));
 			date_default_timezone_set($tz);
 		}
 		else
