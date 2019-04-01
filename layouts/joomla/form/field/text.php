d70ea99fef44685011f7d16113fc0a5cb6b4f4d2
Brian Teeman <brian@teeman.net>
2019-04-01 13:18:55 +0100

    [4.0] Search box tips
    In Joomla 3 all the search boxes had a descriptive tooltip. This was added by javascript
    
    This tooltip is missing in J4 as we dont have the same js
    
    This PR puts the descriptive tooltip back WITHOUT using any js and in a fully accessible way

--- layouts/joomla/form/field/text.php
+++ layouts/joomla/form/field/text.php
@@ -97,6 +97,12 @@ $addonAfterHtml  = '<span class="input-group-append"><span class="input-group-te
 		<?php echo $dirname; ?>
 		<?php echo implode(' ', $attributes); ?>>
 
+	<?php if (!empty($description)) : ?>
+		<div role="tooltip" id="<?php echo $name . '-desc'; ?>">
+			<?php echo $description; ?>
+		</div>
+	<?php endif; ?>
+
 	<?php if (!empty($addonAfter)) : ?>
 		<?php echo $addonAfterHtml; ?>
 	<?php endif; ?>
