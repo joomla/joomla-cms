201ce916fadac041bffd1c92bba6d21c8b59218f
Nitish Bahl <nitishbahl24@gmail.com>
2019-04-04 22:14:25 +0530

    fixed workflow conditions

--- plugins/search/content/content.php
+++ plugins/search/content/content.php
@@ -289,9 +289,9 @@ class PlgSearchContent extends CMSPlugin
 				->select($db->quote('2') . ' AS browsernav')
 				->from($db->quoteName('#__content', 'a'))
 				->innerJoin($db->quoteName('#__categories', 'c') . ' ON c.id = a.catid')
-				->join('LEFT', '#__workflow_stages AS ws ON ws.id = a.state')
+				->join('LEFT', '#__workflow_stages AS ws ON ws.condition = a.state')
 				->where(
-					'(' . $where . ') AND ws.condition=0 AND c.published = 1 AND a.access IN (' . $groups . ') '
+					'(' . $where . ') AND ws.condition=1 AND c.published = 1 AND a.access IN (' . $groups . ') '
 						. 'AND c.access IN (' . $groups . ')'
 						. 'AND (a.publish_up = ' . $db->quote($nullDate) . ' OR a.publish_up <= ' . $db->quote($now) . ') '
 						. 'AND (a.publish_down = ' . $db->quote($nullDate) . ' OR a.publish_down >= ' . $db->quote($now) . ')'
