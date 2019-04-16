881c6ef1e4be14e5c3960d018ccb27e61a892437
Jean-Marie Simonet <infografjms@gmail.com>
2019-04-16 06:00:02 +0200

    using colspan

--- administrator/modules/mod_privacy_dashboard/tmpl/default.php
+++ administrator/modules/mod_privacy_dashboard/tmpl/default.php
@@ -23,22 +23,22 @@ $activeRequests = 0;
 	<caption class="sr-only"><?php echo $module->title; ?></caption>
 	<thead>
 		<tr>
-			<th scope="col" style="width:40%"><?php echo Text::_('COM_PRIVACY_DASHBOARD_HEADING_REQUEST_TYPE'); ?></th>
-			<th scope="col" style="width:30%"><?php echo Text::_('COM_PRIVACY_DASHBOARD_HEADING_REQUEST_STATUS'); ?></th>
-			<th scope="col" style="width:30%"><?php echo Text::_('COM_PRIVACY_DASHBOARD_HEADING_REQUEST_COUNT'); ?></th>
+			<th colspan="2" scope="col" style="width:40%"><?php echo Text::_('COM_PRIVACY_DASHBOARD_HEADING_REQUEST_TYPE'); ?></th>
+			<th colspan="2" scope="col" style="width:30%"><?php echo Text::_('COM_PRIVACY_DASHBOARD_HEADING_REQUEST_STATUS'); ?></th>
+			<th colspan="2" scope="col" style="width:30%"><?php echo Text::_('COM_PRIVACY_DASHBOARD_HEADING_REQUEST_COUNT'); ?></th>
 		</tr>
 	</thead>
 	<tbody>
 		<?php if (count($list)) : ?>
 		<?php foreach ($list as $row) : ?>
 		<tr>
-			<td>
-				<a href="<?php echo Route::_('index.php?option=com_privacy&view=requests&filter[request_type]=' . $row->request_type . '&filter[status]=' . $row->status); ?>" data-original-title="<?php echo Text::_('COM_PRIVACY_DASHBOARD_VIEW_REQUESTS'); ?>">
+			<td colspan="2">
+				<a href="<?php echo Route::_('index.php?option=com_privacy&view=requests&filter[request_type]=' . $row->request_type . '&filter[status]=' . $row->status); ?>">
 					<strong><?php echo Text::_('COM_PRIVACY_HEADING_REQUEST_TYPE_TYPE_' . $row->request_type); ?></strong>
 				</a>
 			</td>
-			<td><?php echo HTMLHelper::_('privacy.statusLabel', $row->status); ?></td>
-			<td><span class="badge badge-info"><?php echo $row->count; ?></span></td>
+			<td colspan="2"><?php echo HTMLHelper::_('privacy.statusLabel', $row->status); ?></td>
+			<td colspan="2"><span class="badge badge-info"><?php echo $row->count; ?></span></td>
 			<?php if (in_array($row->status, array(0, 1))) : ?>
 				<?php $activeRequests += $row->count; ?>
 			<?php endif; ?>
@@ -46,13 +46,12 @@ $activeRequests = 0;
 		</tr>
 		<?php endforeach; ?>
 		<tr>
-			<td><?php echo Text::plural('COM_PRIVACY_DASHBOARD_BADGE_TOTAL_REQUESTS', $totalRequests); ?></td>
-			<td><?php echo Text::plural('COM_PRIVACY_DASHBOARD_BADGE_ACTIVE_REQUESTS', $activeRequests); ?></td>
-			<td></td>
+			<td colspan="2" style="text-align:center"><?php echo Text::plural('COM_PRIVACY_DASHBOARD_BADGE_TOTAL_REQUESTS', $totalRequests); ?></td>
+			<td colspan="4" style="text-align:center"><?php echo Text::plural('COM_PRIVACY_DASHBOARD_BADGE_ACTIVE_REQUESTS', $activeRequests); ?></td>
 		</tr>
 		<?php else : ?>
 		<tr>
-			<td colspan="3">
+			<td colspan="6">
 				<?php echo Text::_('COM_PRIVACY_DASHBOARD_NO_REQUESTS'); ?>
 			</td>
 		</tr>
