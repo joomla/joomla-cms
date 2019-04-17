91210e6628d217f16461e2233337d9289f7aa78c
Quy <quy@fluxbb.org>
2019-04-17 08:19:45 +0200

    Update administrator/components/com_admin/sql/updates/mysql/3.9.6-2019-04-15.sql
    thanks @Quy
    
    Co-Authored-By: alikon <optimus4joomla@gmail.com>

--- administrator/components/com_admin/sql/updates/mysql/3.9.6-2019-04-15.sql
+++ administrator/components/com_admin/sql/updates/mysql/3.9.6-2019-04-15.sql
@@ -1,3 +1,3 @@
 ALTER TABLE `#__template_styles` DROP INDEX `idx_home`;
-ALTER TABLE `#__template_styles` MODIFY `home` tinyint(1) unsigned DEFAULT 0;
+ALTER TABLE `#__template_styles` MODIFY `home` tinyint(1) unsigned NOT NULL DEFAULT 0;
 ALTER TABLE `#__template_styles` ADD INDEX `idx_home` (`home`);
