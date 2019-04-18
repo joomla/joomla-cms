461e55719b5e9da93ce13dbe4b7922f99faf6dd3
Brian Teeman <brian@teeman.net>
2019-04-18 11:18:37 +0100

    [4.0] Remove module description popover
    When you go to select a NEW module you have a list of all the modules with a very short 90 character truncated description.
    
    If you hover over that description you get a popover with a short 200 character truncated description.
    
    This popover is not accessible - I doubt even most sighted users will know that it even exists as there is no visual indicator that it exists either.
    
    The original reason for the 90 character truncation was back in the day that this was a modal and there was limited space. That is no longer true  but we do need to have some form of truncation as some 3pd write essays for the description ;)
    
    This PR removes the popover and changes the displayed description to the 200 character version

--- administrator/components/com_modules/tmpl/select/default.php
+++ administrator/components/com_modules/tmpl/select/default.php
@@ -14,7 +14,6 @@ use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\Router\Route;
 
-HTMLHelper::_('bootstrap.popover');
 $document = Factory::getDocument();
 ?>
 
@@ -25,17 +24,15 @@ $document = Factory::getDocument();
 	<?php $link       = 'index.php?option=com_modules&task=module.add&eid=' . $item->extension_id; ?>
 	<?php $name       = $this->escape($item->name); ?>
 	<?php $desc       = HTMLHelper::_('string.truncate', $this->escape(strip_tags($item->desc)), 200); ?>
-	<?php $short_desc = HTMLHelper::_('string.truncate', $this->escape(strip_tags($item->desc)), 90); ?>
 
 	<?php if ($document->direction != 'rtl') : ?>
 	<li class="list-group-item">
-		<a href="<?php echo Route::_($link); ?>" class="mr-2">
-			<strong><?php echo $name; ?></strong></a>
-		<small class="hasPopover" data-placement="right" title="<?php echo $name; ?>" data-content="<?php echo $desc; ?>"><?php echo $short_desc; ?></small>
+		<a href="<?php echo Route::_($link); ?>" class="mr-2"><strong><?php echo $name; ?></strong></a>
+		<small><?php echo $desc; ?></small>
 	</li>
 	<?php else : ?>
 	<li class="list-group-item">
-		<small rel="popover" data-placement="left" title="<?php echo $name; ?>" data-content="<?php echo $desc; ?>"><?php echo $short_desc; ?></small>
+		<small><?php echo $desc; ?></small>
 		<a href="<?php echo Route::_($link); ?>" class="mr-2"><strong><?php echo $name; ?></strong></a>
 	</li>
 	<?php endif; ?>
