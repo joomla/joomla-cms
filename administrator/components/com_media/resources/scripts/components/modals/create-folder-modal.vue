9949e5ec01d99823c77f971453d7bb379db24a21
wilsonge <georgejameswilson@googlemail.com>
2019-04-21 10:33:05 +0100

    Fix modals tabbing

--- administrator/components/com_media/resources/scripts/components/modals/create-folder-modal.vue
+++ administrator/components/com_media/resources/scripts/components/modals/create-folder-modal.vue
@@ -6,7 +6,7 @@
                 <div class="form-group">
                     <label for="folder">{{ translate('COM_MEDIA_FOLDER_NAME') }}</label>
                     <input id="folder" class="form-control"
-                           v-focus="true" v-model.trim="folder" @input="folder = $event.target.value"
+                           v-model.trim="folder" @input="folder = $event.target.value"
                            required autocomplete="off">
                 </div>
             </form>
@@ -21,11 +21,9 @@
 
 <script>
     import * as types from "./../../store/mutation-types";
-    import {focus} from 'vue-focus';
 
     export default {
         name: 'media-create-folder-modal',
-        directives: {focus: focus},
         data() {
             return {
                 folder: '',
