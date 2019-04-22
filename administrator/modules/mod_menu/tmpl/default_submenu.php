5cd50a45aa061dbd4c66c5e62b63df1f33a7baf1
George Wilson <georgejameswilson@googlemail.com>
2019-04-22 21:34:00 +0100

    Fix codestyle

--- administrator/modules/mod_menu/tmpl/default_submenu.php
+++ administrator/modules/mod_menu/tmpl/default_submenu.php
@@ -127,9 +127,9 @@ if ($current->getParams()->get('menu-quicktask', false))
 
 if ($current->dashboard)
 {
-	$titleDashboard = Text::_('MOD_MENU_DASHBOARD_LINK') . ': ' . Text::_($current->title) ;
+	$titleDashboard = Text::_('MOD_MENU_DASHBOARD_LINK') . ': ' . Text::_($current->title);
 	echo '<span class="menu-dashboard"><a href="'
-		. JRoute::_('index.php?option=com_cpanel&view=cpanel&dashboard=' . $current->dashboard) .  '">'
+		. JRoute::_('index.php?option=com_cpanel&view=cpanel&dashboard=' . $current->dashboard) . '">'
 		. '<span class="fa fa-th-large" title="' . $titleDashboard . '" aria-hidden="true"></span>'
 		. '<span class="sr-only">' . $titleDashboard . '</span>'
 		. '</a></span>';
