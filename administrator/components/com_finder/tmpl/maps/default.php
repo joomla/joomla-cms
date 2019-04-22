fd11facb23b26e3ecf61feaea8f2f249382d90cc
Brian Teeman <brian@teeman.net>
2019-04-22 15:18:26 +0100

    [4.0] Finder missing class
    In Finder Maps there was no class to give a background when the value was 0. For consistency with similar views this PR adds the class.

--- administrator/components/com_finder/tmpl/maps/default.php
+++ administrator/components/com_finder/tmpl/maps/default.php
@@ -122,7 +122,7 @@ HTMLHelper::_('script', 'com_finder/maps.js', ['version' => 'auto', 'relative' =
 							</td>
 							<td class="text-center btns">
 							<?php if ($item->level > 1) : ?>
-								<a class="badge <?php if ((int) $item->count_unpublished > 0) echo 'badge-danger'; ?>" title="<?php echo Text::_('COM_FINDER_MAPS_COUNT_UNPUBLISHED_ITEMS'); ?>" href="<?php echo Route::_('index.php?option=com_finder&view=index&filter[state]=0&filter[content_map]=' . $item->id); ?>">
+								<a class="badge <?php echo ((int) $item->count_unpublished > 0) ? 'badge-danger' : 'badge-secondary'; ?>" title="<?php echo Text::_('COM_FINDER_MAPS_COUNT_UNPUBLISHED_ITEMS'); ?>" href="<?php echo Route::_('index.php?option=com_finder&view=index&filter[state]=0&filter[content_map]=' . $item->id); ?>">
 								<?php echo (int) $item->count_unpublished; ?></a>
 							<?php else : ?>
 								-
