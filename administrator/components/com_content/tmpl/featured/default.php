e9182fa947890b967d32883a0b906f269d8f9cb9
Hard!k 4garwal <hardik06agarwal@gmail.com>
2019-04-24 21:34:36 +0530

    Fix Code style

--- administrator/components/com_content/tmpl/featured/default.php
+++ administrator/components/com_content/tmpl/featured/default.php
@@ -194,7 +194,6 @@ HTMLHelper::_('script', 'com_content/admin-articles-workflow-buttons.js', ['rela
 								<td class="order text-center d-none d-md-table-cell">
 									<?php
 									$iconClass = '';
-
 									if (!$canChange)
 									{
 										$iconClass = ' inactive';
@@ -205,8 +204,8 @@ HTMLHelper::_('script', 'com_content/admin-articles-workflow-buttons.js', ['rela
 									}
 									?>
 									<span class="sortable-handler<?php echo $iconClass ?>">
-									<span class="icon-menu" aria-hidden="true"></span>
-								</span>
+										<span class="icon-menu" aria-hidden="true"></span>
+									</span>
 									<?php if ($canChange && $saveOrder) : ?>
 										<input type="text" style="display:none" name="order[]" size="5" value="<?php echo $item->ordering; ?>" class="width-20 text-area-order">
 									<?php endif; ?>
