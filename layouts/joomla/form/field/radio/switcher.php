190fafe489eca363204f0b30287bf08d23c52e1a
Brian Teeman <brian@teeman.net>
2019-04-29 23:21:10 +0100

    cs

--- layouts/joomla/form/field/radio/switcher.php
+++ layouts/joomla/form/field/radio/switcher.php
@@ -68,14 +68,14 @@ $input    = '<input type="radio" id="%1$s" name="%2$s" value="%3$s" %4$s>';
 	</legend>
 	<div class="switcher" role="switch">
 	<?php foreach ($options as $i => $option) : ?>
-	<?php
+		<?php
 		// Initialize some option attributes.
-		$checked = ((string) $option->value == $value) ? 'checked="checked"' : '';
-		$active  = ((string) $option->value == $value) ? 'class="active"' : '';
-		$oid        = $id . $i;
-		$ovalue     = htmlspecialchars($option->value, ENT_COMPAT, 'UTF-8');
-		$attributes = array_filter(array($checked, $active, null));
-		$text       = $options[$i]->text;
+		$checked	= ((string) $option->value == $value) ? 'checked="checked"' : '';
+		$active		= ((string) $option->value == $value) ? 'class="active"' : '';
+		$oid		= $id . $i;
+		$ovalue		= htmlspecialchars($option->value, ENT_COMPAT, 'UTF-8');
+		$attributes	= array_filter(array($checked, $active, null));
+		$text		= $options[$i]->text;
 		?>
 		<?php echo sprintf($input, $oid, $name, $ovalue, implode(' ', $attributes)); ?>
 		<?php echo '<label for="' . $oid . '">' . $text . '</label>'; ?>
