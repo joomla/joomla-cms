4cd854e7387f8aa33d5112705a980508c4ce7fff
Brian Teeman <brian@teeman.net>
2019-04-29 23:15:06 +0100

    cs

--- layouts/joomla/form/field/radio/switcher.php
+++ layouts/joomla/form/field/radio/switcher.php
@@ -63,23 +63,23 @@ $input    = '<input type="radio" id="%1$s" name="%2$s" value="%3$s" %4$s>';
 
 ?>
 <fieldset>
-  <legend class="switcher__legend">
-    <?php echo $label; ?>
-  </legend>
-  <div class="switcher" role="switch">
-  <?php foreach ($options as $i => $option) : ?>
-    <?php
-    // Initialize some option attributes.
-    $checked = ((string) $option->value == $value) ? 'checked="checked"' : '';
-    $active  = ((string) $option->value == $value) ? 'class="active"' : '';
-    $oid        = $id . $i;
-    $ovalue     = htmlspecialchars($option->value, ENT_COMPAT, 'UTF-8');
-    $attributes = array_filter(array($checked, $active, null));
-    $text       = $options[$i]->text;
-    ?>
-    <?php echo sprintf($input, $oid, $name, $ovalue, implode(' ', $attributes)); ?>
-    <?php echo '<label for="' . $oid . '">' . $text . '</label>'; ?>
+	<legend class="switcher__legend">
+		<?php echo $label; ?>
+	</legend>
+	<div class="switcher" role="switch">
+	<?php foreach ($options as $i => $option) : ?>
+	<?php
+		// Initialize some option attributes.
+		$checked = ((string) $option->value == $value) ? 'checked="checked"' : '';
+		$active  = ((string) $option->value == $value) ? 'class="active"' : '';
+		$oid        = $id . $i;
+		$ovalue     = htmlspecialchars($option->value, ENT_COMPAT, 'UTF-8');
+		$attributes = array_filter(array($checked, $active, null));
+		$text       = $options[$i]->text;
+		?>
+		<?php echo sprintf($input, $oid, $name, $ovalue, implode(' ', $attributes)); ?>
+		<?php echo '<label for="' . $oid . '">' . $text . '</label>'; ?>
 	<?php endforeach; ?>
-  <span class="toggle-outside"><span class="toggle-inside"></span></span>
-  </div>
+	<span class="toggle-outside"><span class="toggle-inside"></span></span>
+	</div>
 </fieldset>
