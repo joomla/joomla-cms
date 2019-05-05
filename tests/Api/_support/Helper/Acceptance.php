740cdaa963f7c2edf22cc7b616c96350b9af6008
Hannes Papenberg <info@joomlager.de>
2019-05-05 23:03:06 +0200

    Codestyle

--- tests/Api/_support/Helper/Acceptance.php
+++ tests/Api/_support/Helper/Acceptance.php
@@ -1,7 +1,7 @@
 <?php
 namespace Helper;
 
-// here you can define custom actions
+// Here you can define custom actions
 // all public methods declared in helper class will be available in $I
 
 class Acceptance extends \Codeception\Module
