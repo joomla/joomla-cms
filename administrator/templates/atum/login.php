022f477bf0e49e9cef942ca99a0e8255914e29dd
coolcat-creations <mail@coolcat-creations.com>
2019-05-06 23:43:57 +0200

    Change Filename of custom.css from user.css to custom.css

--- administrator/templates/atum/login.php
+++ administrator/templates/atum/login.php
@@ -32,7 +32,7 @@ HTMLHelper::_('stylesheet', 'fontawesome.css', ['version' => 'auto', 'relative' 
 HTMLHelper::_('stylesheet', 'template' . ($this->direction === 'rtl' ? '-rtl' : '') . '.css', ['version' => 'auto', 'relative' => true]);
 
 // Load custom CSS file
-HTMLHelper::_('stylesheet', 'user.css', ['version' => 'auto', 'relative' => true]);
+HTMLHelper::_('stylesheet', 'custom.css', ['version' => 'auto', 'relative' => true]);
 
 // Load specific language related CSS
 HTMLHelper::_('stylesheet', 'administrator/language/' . $lang->getTag() . '/' . $lang->getTag() . '.css', ['version' => 'auto']);
