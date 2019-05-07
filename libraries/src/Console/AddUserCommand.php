69d2c40687e7df8b9e53e823425937577d023b37
Felix Hamel <git@unser-hoster.de>
2019-05-07 18:17:32 +0200

    fix error handling

--- libraries/src/Console/AddUserCommand.php
+++ libraries/src/Console/AddUserCommand.php
@@ -133,12 +133,15 @@ class AddUserCommand extends AbstractCommand
 		{
 			switch ($userObj->getError())
 			{
-				case "LIB_DATABASE_ERROR_USERNAME_INUSE":
+				case "JLIB_DATABASE_ERROR_USERNAME_INUSE":
 					$this->ioStyle->error("The username already exists!");
-				case "JLIB_DATABASE_ERROR_EMAIL_INUSE  ":
+					break;
+				case "JLIB_DATABASE_ERROR_EMAIL_INUSE":
 					$this->ioStyle->error("The email address already exists!");
+					break;
 				case "JLIB_DATABASE_ERROR_VALID_MAIL":
 					$this->ioStyle->error("The email address is invalid!");
+					break;
 			}
 
 			return 1;
