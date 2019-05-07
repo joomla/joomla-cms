05d069609ce2dd1ad75798fce88e9fe24817ef02
Nicola Galgano <optimus4joomla@gmail.com>
2019-05-07 20:44:20 +0200

    modified is not nullable

--- administrator/components/com_admin/sql/updates/mysql/4.0.0-2019-04-22.sql
+++ administrator/components/com_admin/sql/updates/mysql/4.0.0-2019-04-22.sql
@@ -1,11 +1,8 @@
 ALTER TABLE `#__contact_details` MODIFY `publish_up` datetime NULL DEFAULT NULL;
 ALTER TABLE `#__contact_details` MODIFY `publish_down` datetime NULL DEFAULT NULL;
 ALTER TABLE `#__contact_details` MODIFY `checked_out_time` datetime NULL DEFAULT NULL;
-ALTER TABLE `#__contact_details` MODIFY `modified` datetime NULL DEFAULT NULL;
-
 
 UPDATE `#__contact_details` SET
 	`publish_up` = CASE WHEN `publish_up` IN ('0000-00-00 00:00:00', '1000-01-01 00:00:00') THEN NULL ELSE `publish_up` END,
 	`publish_down` = CASE WHEN `publish_down` IN ('0000-00-00 00:00:00', '1000-01-01 00:00:00') THEN NULL ELSE `publish_down` END,
-	`checked_out_time` = CASE WHEN `checked_out_time` IN ('0000-00-00 00:00:00', '1000-01-01 00:00:00') THEN NULL ELSE `checked_out_time` END,
-	`modified` = CASE WHEN `modified` IN ('0000-00-00 00:00:00', '1000-01-01 00:00:00') THEN NULL ELSE `modified` END;
+	`checked_out_time` = CASE WHEN `checked_out_time` IN ('0000-00-00 00:00:00', '1000-01-01 00:00:00') THEN NULL ELSE `checked_out_time` END;
