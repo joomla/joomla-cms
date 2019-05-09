8a2070a587c23f4257e7b0238cde81430a036733
coolcat-creations <mail@coolcat-creations.com>
2019-05-09 07:27:54 +0200

    Fix conflicts for new badge colors

--- administrator/modules/mod_sampledata/tmpl/default.php
+++ administrator/modules/mod_sampledata/tmpl/default.php
@@ -36,8 +36,8 @@ $app->getDocument()->addScriptOptions(
 						<span class="fa fa-<?php echo $item->icon; ?>" aria-hidden="true"></span>
 						<?php echo htmlspecialchars($item->title, ENT_QUOTES, 'UTF-8'); ?>
 					</div>
-					<button type="button" class="btn btn-primary btn-sm apply-sample-data" data-type="<?php echo $item->name; ?>" data-steps="<?php echo $item->steps; ?>">
-						<?php echo Text::_('JLIB_INSTALLER_INSTALL'); ?>
+					<button type="button" class="btn btn-secondary btn-sm apply-sample-data" data-type="<?php echo $item->name; ?>" data-steps="<?php echo $item->steps; ?>">
+						<span class="fa fa-cloud-download-alt"></span> <?php echo Text::_('JLIB_INSTALLER_INSTALL'); ?>
 						<span class="sr-only"><?php echo $item->title; ?></span>
 					</button>
 				</div>
