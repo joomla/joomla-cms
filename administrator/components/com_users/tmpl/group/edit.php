5610430cd6b79f8aee9df1f51f561c668c7bb291
Brian Teeman <brian@teeman.net>
2019-05-10 21:48:56 +0100

    [4.0] new usergroup form
    Update the markup for consistency with other similar views eg new access level form

--- administrator/components/com_users/tmpl/group/edit.php
+++ administrator/components/com_users/tmpl/group/edit.php
@@ -17,15 +17,13 @@ HTMLHelper::_('behavior.formvalidator');
 ?>
 
 <form action="<?php echo Route::_('index.php?option=com_users&layout=edit&id=' . (int) $this->item->id); ?>" method="post" name="adminForm" id="group-form" class="form-validate">
-	<?php echo JHtml::_('bootstrap.startTabSet', 'myTab', array('active' => 'details')); ?>
-	<?php echo JHtml::_('bootstrap.addTab', 'myTab', 'details', Text::_('COM_USERS_USERGROUP_DETAILS')); ?>
-		<?php echo $this->form->renderField('title'); ?>
-		<?php echo $this->form->renderField('parent_id'); ?>
-	<?php echo JHtml::_('bootstrap.endTab'); ?>
-	<?php $this->ignore_fieldsets = array('group_details'); ?>
-	<?php echo JLayoutHelper::render('joomla.edit.params', $this); ?>
-	<?php echo JHtml::_('bootstrap.endTabSet'); ?>
-
+	<fieldset>
+		<legend><?php echo Text::_('COM_USERS_USERGROUP_DETAILS'); ?></legend>
+		<div class="control-group">
+			<?php echo $this->form->renderField('title'); ?>
+			<?php echo $this->form->renderField('parent_id'); ?>
+		</div>
+	</fieldset>
 	<input type="hidden" name="task" value="">
 	<?php echo HTMLHelper::_('form.token'); ?>
 </form>
