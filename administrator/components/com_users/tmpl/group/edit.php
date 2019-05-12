e05d413fe003a99bb0c95d2f25d4b4a0fb0589ba
Brian Teeman <brian@teeman.net>
2019-05-12 10:37:21 +0100

    convert to uitab

--- administrator/components/com_users/tmpl/group/edit.php
+++ administrator/components/com_users/tmpl/group/edit.php
@@ -17,13 +17,18 @@ HTMLHelper::_('behavior.formvalidator');
 ?>
 
 <form action="<?php echo Route::_('index.php?option=com_users&layout=edit&id=' . (int) $this->item->id); ?>" method="post" name="adminForm" id="group-form" class="form-validate">
-	<fieldset>
-		<legend><?php echo Text::_('COM_USERS_USERGROUP_DETAILS'); ?></legend>
-		<div class="control-group">
-			<?php echo $this->form->renderField('title'); ?>
-			<?php echo $this->form->renderField('parent_id'); ?>
-		</div>
+	<?php echo HTMLHelper::_('uitab.startTabSet', 'myTab', array('active' => 'details')); ?>
+	<?php echo HTMLHelper::_('uitab.addTab', 'myTab', 'details', Text::_('COM_USERS_USERGROUP_DETAILS')); ?>		<fieldset>
+		<?php echo $this->form->renderField('title'); ?>
+		<?php echo $this->form->renderField('parent_id'); ?>
 	</fieldset>
+	<?php echo HTMLHelper::_('uitab.endTab'); ?>
+	<?php $this->ignore_fieldsets = array('group_details'); ?>
+	<?php echo JLayoutHelper::render('joomla.edit.params', $this); ?>
+	<?php echo HTMLHelper::_('uitab.endTabSet'); ?>
+
 	<input type="hidden" name="task" value="">
 	<?php echo HTMLHelper::_('form.token'); ?>
 </form>
+
+
