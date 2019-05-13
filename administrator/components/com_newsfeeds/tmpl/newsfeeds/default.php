b0f04ed0c7584777b462003bad42af9cfea92e17
Brian Teeman <brian@teeman.net>
2019-05-13 15:29:42 +0100

    Update administrator/components/com_newsfeeds/tmpl/newsfeeds/default.php
    Co-Authored-By: Quy <quy@fluxbb.org>

--- administrator/components/com_newsfeeds/tmpl/newsfeeds/default.php
+++ administrator/components/com_newsfeeds/tmpl/newsfeeds/default.php
@@ -35,7 +35,7 @@ if ($saveOrder && !empty($this->items))
 ?>
 <form action="<?php echo Route::_('index.php?option=com_newsfeeds&view=newsfeeds'); ?>" method="post" name="adminForm" id="adminForm">
 	<div class="row">
-        <div class="col-md-12">
+		<div class="col-md-12">
 			<div id="j-main-container" class="j-main-container">
 				<?php echo LayoutHelper::render('joomla.searchtools.default', array('view' => $this)); ?>
 				<?php if (empty($this->items)) : ?>
