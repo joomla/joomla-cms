1efef72375b09833a330ce84324f476c71352e6d
Brian Teeman <brian@teeman.net>
2019-05-13 14:55:42 +0100

    Update administrator/components/com_banners/tmpl/clients/default.php
    Co-Authored-By: Quy <quy@fluxbb.org>

--- administrator/components/com_banners/tmpl/clients/default.php
+++ administrator/components/com_banners/tmpl/clients/default.php
@@ -33,7 +33,7 @@ $params     = $this->state->params ?? new JObject;
 ?>
 <form action="<?php echo Route::_('index.php?option=com_banners&view=clients'); ?>" method="post" name="adminForm" id="adminForm">
 	<div class="row">
-        <div class="col-md-12">
+		<div class="col-md-12">
 			<div id="j-main-container" class="j-main-container">
 				<?php
 				// Search tools bar
