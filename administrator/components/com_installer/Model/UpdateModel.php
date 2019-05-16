f002849ce315f8f83ec95797bccf728bc0a7ffe2
Brainforge.UK <brainforge.uk@btinternet.com>
2019-05-16 20:28:37 +0100

    Removed previous change.

--- administrator/components/com_installer/Model/UpdateModel.php
+++ administrator/components/com_installer/Model/UpdateModel.php
@@ -456,13 +456,6 @@ class UpdateModel extends ListModel
 		// Unpack the downloaded package file
 		$package = InstallerHelper::unpack($tmp_dest . '/' . $p_file);
 
-		if ($package === false || !isset($package['type']) || $package['type'] === false)
-		{
-			Factory::getApplication()->enqueueMessage(JText::sprintf('COM_INSTALLER_MSG_UPDATE_INVALID_PKG', $url), 'error');
-
-			return false;
-		}
-
 		// Get an installer instance
 		$installer = Installer::getInstance();
 		$update->set('type', $package['type']);
