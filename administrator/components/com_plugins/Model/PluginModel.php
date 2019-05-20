ffbc7f0ab40125cb9f6caff7321c3ac038051d27
Brian Teeman <brian@teeman.net>
2019-05-20 20:18:17 +0100

    Update administrator/components/com_plugins/Model/PluginModel.php
    Co-Authored-By: Quy <quy@fluxbb.org>

--- administrator/components/com_plugins/Model/PluginModel.php
+++ administrator/components/com_plugins/Model/PluginModel.php
@@ -179,7 +179,7 @@ class PluginModel extends AdminModel
 
 			// Convert to the \JObject before adding other data.
 			$properties = $table->getProperties(1);
-			$this->_cache[$pk] = ArrayHelper::toObject($properties, 'CMSObject');
+			$this->_cache[$pk] = ArrayHelper::toObject($properties, CMSObject::class);
 
 			// Convert the params field to an array.
 			$registry = new Registry($table->params);
