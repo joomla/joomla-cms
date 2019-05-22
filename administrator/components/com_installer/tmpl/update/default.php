8512c0268e3ca3968e1ba250be269632cbf08959
Brian Teeman <brian@teeman.net>
2019-05-22 11:38:54 +0100

    [4.0] replace btn-xs with btn-sm
    In Bootstrap 4 they "dropped the .btn-xs class entirely as .btn-sm is proportionally much smaller than v3’s."
    
    A previous PR added the css back for the frontend template.
    
    This PR removes that additional class and instead replaces the uses of btn-xs with btn-sm
    
    Fixes #21244

--- administrator/components/com_installer/tmpl/update/default.php
+++ administrator/components/com_installer/tmpl/update/default.php
@@ -106,7 +106,7 @@ $listDirn  = $this->escape($this->state->get('list.direction'));
 									</td>
 									<td class="hidden-sm-down text-center">
 										<?php if ($item->changelogurl !== null) : ?>
-                                        <a href="#changelogModal" class="btn btn-info btn-xs" onclick="Joomla.loadChangelog(<?php echo $item->extension_id; ?>, 'update'); return false;" data-toggle="modal">
+                                        <a href="#changelogModal" class="btn btn-info btn-sm" onclick="Joomla.loadChangelog(<?php echo $item->extension_id; ?>, 'update'); return false;" data-toggle="modal">
 	                                        <?php echo Text::_('COM_INSTALLER_CHANGELOG'); ?>
                                         </a>
 										<?php
