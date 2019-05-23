755b4248bc777c57dde40a5456375a595b3846b8
chmst <dev@chmst.de>
2019-05-23 19:44:50 +0200

    Close span, add margin

--- administrator/templates/atum/html/layouts/chromes/well.php
+++ administrator/templates/atum/html/layouts/chromes/well.php
@@ -38,9 +38,12 @@ if ($module->content) :
 
 	// Get the module icon
 	$headerIcon = '';
+	
+	$margin = Factory::getLanguage()->isRtl() ? ' ml-2' : ' mr-2';
+	
 	if (!empty($params->get('header_icon')))
 	{
-		$headerIcon = '<span class="' . htmlspecialchars($params->get('header_icon')) . '" aria-hidden="true"> ';
+		$headerIcon = '<span class="' . htmlspecialchars($params->get('header_icon')) .  $margin . '" aria-hidden="true"></span>';
 	}
 	?>
 	<div class="module-wrapper">
