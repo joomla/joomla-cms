2274fa2c29f502cf7778937737e36ce26dc9bde1
chmst <dev@chmst.de>
2019-05-25 11:03:34 +0200

    aria-hidden for module actions button

--- administrator/templates/atum/html/layouts/chromes/well.php
+++ administrator/templates/atum/html/layouts/chromes/well.php
@@ -54,9 +54,8 @@ if ($module->content) :
 						<?php $dropdownPosition = Factory::getLanguage()->isRTL() ? 'left' : 'right'; ?>
 						<div class="module-actions dropdown">
 							<button type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="btn btn-link" id="dropdownMenuButton-<?php echo $id; ?>">
-								<span class="fa fa-cog"><span class="sr-only">
-									<?php echo Text::_('JACTION_EDIT') . ' ' . $module->title; ?>
-								</span></span>
+								<span class="fa fa-cog" aria-hidden="true"></span>
+								<span class="sr-only"><?php echo Text::_('JACTION_EDIT') . ' ' . $module->title; ?></span>
 							</button>
 							<div class="dropdown-menu dropdown-menu-<?php echo $dropdownPosition; ?>" aria-labelledby="dropdownMenuButton-<?php echo $id; ?>">
 								<?php if ($canEdit) : ?>
