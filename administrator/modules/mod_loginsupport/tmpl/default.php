835dec9477e43a7c9f157ae9719b907a0fce6ec0
chmst <dev@chmst.de>
2019-05-30 20:08:04 +0200

    New module login support with language keys

---
+++ administrator/modules/mod_loginsupport/tmpl/default.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  mod_loginsupport
+ *
+ * @copyright   Copyright (C) 2005 - 2019 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Language\Text;
+?>
+
+<section class="loginsupport">
+	<p><?php echo Text::_('MOD_LOGINSUPPORT_HEADLINE'); ?></p>
+	<ul class="list-unstyled">
+		<li><a href="https://forum.joomla.org/" target="_blank" rel="nofollow noopener"><?php echo Text::_('MOD_LOGINSUPPORT_FORUM'); ?></a></li>
+		<li><a href="https://docs.joomla.org" target="_blank" rel="nofollow noopener"><?php echo Text::_('MOD_LOGINSUPPORT_DOCUMENTATION'); ?></a></li>
+		<li><a href="https://www.joomla.org/" target="_blank" rel="nofollow noopener"><?php echo Text::_('MOD_LOGINSUPPORT_NEWS'); ?></a></li>
+	</ul>
+</section>
