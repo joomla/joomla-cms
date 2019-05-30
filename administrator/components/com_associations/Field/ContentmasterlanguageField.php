cc03c4874fd4934cb13035a93851d19df87a3312
Lavinia Popa-Rössel <lavinia.popa-roessel@mni.thm.de>
2019-05-30 18:51:41 +0200

    add masterlanguage information to the language select box
    If a global master language has been set, it is displayed as info in the Language select box , so that it is recognizable which master language has been set.

---
+++ administrator/components/com_associations/Field/ContentmasterlanguageField.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_associations
+ *
+ * @copyright   Copyright (C) 2005 - 2019 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+namespace Joomla\Component\Associations\Administrator\Field;
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Form\Field\ContentlanguageField;
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Language\Associations;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Language\LanguageHelper;
+use Joomla\Registry\Registry;
+
+
+/**
+ * Content Master Languages Field class.
+ *
+ * @since  4.0
+ */
+class ContentmasterlanguageField extends ContentlanguageField
+{
+	/**
+	 * The list field type.
+	 *
+	 * @var    string
+	 *
+	 * @since  4.0
+	 */
+	public $type = 'contentmasterlanguage';
+
+	/**
+	 * Method to get the field options.
+	 *
+	 * @return  array The field option objects.
+	 *
+	 * @since   4.0
+	 */
+	public function getOptions()
+	{
+		$globalMasterLanguage = Associations::getGlobalMasterLanguage();
+
+		$contentLanguages = LanguageHelper::getContentLanguages(array(0, 1));
+		$options = array();
+
+		foreach ($contentLanguages as $langCode)
+		{
+			// Add information to the language if it is the global master language
+			if ($langCode->lang_code == $globalMasterLanguage)
+			{
+				$options[] = HTMLHelper::_('select.option', $langCode->lang_code, $langCode->title . ' - '
+					. Text::_('COM_ASSOCIATIONS_MASTER_LANGUAGE')
+				);
+			}
+			else
+			{
+				$options[] = HTMLHelper::_('select.option', $langCode->lang_code, $langCode->title);
+			}
+		}
+
+		return $options;
+	}
+}
