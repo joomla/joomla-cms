2548407799ad80eeaf7225c04d10b4b4b260d007
SharkyKZ <sharkykz@gmail.com>
2019-05-30 23:09:39 +0300

    Remove unused import

--- plugins/fields/repeatable/repeatable.php
+++ plugins/fields/repeatable/repeatable.php
@@ -10,7 +10,6 @@
 defined('_JEXEC') or die;
 
 use Joomla\CMS\Form\Form;
-use Joomla\CMS\MVC\Model\BaseDatabaseModel;
 use Joomla\Component\Fields\Administrator\Helper\FieldsHelper;
 use Joomla\Component\Fields\Administrator\Plugin\FieldsPlugin;
 
