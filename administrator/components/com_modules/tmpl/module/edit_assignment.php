7f2ea844f3809756d606dabae1b6b76aca0789f9
Brian Teeman <brian@teeman.net>
2019-06-05 15:25:21 +0100

    sr-only

--- administrator/components/com_modules/tmpl/module/edit_assignment.php
+++ administrator/components/com_modules/tmpl/module/edit_assignment.php
@@ -39,13 +39,13 @@ HTMLHelper::_('script', 'com_modules/admin-module-edit_assignment.min.js', array
 		<div class="card card-secondary">
 			<div class="card-header">
 				<span class="small"><?php echo Text::_('JSELECT'); ?>:
-					<button id="treeCheckAll" class="btn btn-link" type="button"><?php echo Text::_('JALL'); ?></button>
+					<button id="treeCheckAll" class="btn btn-link" type="button"><span class="sr-only"><?php echo Text::_('JSELECT'); ?></span><?php echo Text::_('JALL'); ?></button>
 					<button id="treeUncheckAll" class="btn btn-link" type="button"><?php echo Text::_('JNONE'); ?></button>
 				</span>
 				<span class="width-20">|</span>
 				<span class="small"><?php echo Text::_('COM_MODULES_EXPAND'); ?>:
-					<button id="treeExpandAll" class="btn btn-link" type="button"><?php echo Text::_('JALL'); ?></button>
-					<button id="treeCollapseAll" class="btn btn-link" type="button"><?php echo Text::_('JNONE'); ?></button>
+					<button id="treeExpandAll" class="btn btn-link" type="button"><span class="sr-only"><?php echo Text::_('COM_MODULES_EXPAND'); ?></span><?php echo Text::_('JALL'); ?></button>
+					<button id="treeCollapseAll" class="btn btn-link" type="button"><?php echo Text::_('COM_MODULES_EXPAND'); ?><?php echo Text::_('JNONE'); ?></button>
 				</span>
 				<span role="search">
 					<label for="treeselectfilter" class="sr-only"><?php echo Text::_('JSEARCH_FILTER'); ?></label>
