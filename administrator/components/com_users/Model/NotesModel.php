fdea4dff4d1a6a39f099d73201a43f48204043c2
Nicola Galgano <optimus4joomla@gmail.com>
2019-06-05 21:24:47 +0200

    fix

--- administrator/components/com_users/Model/NotesModel.php
+++ administrator/components/com_users/Model/NotesModel.php
@@ -134,7 +134,7 @@ class NotesModel extends ListModel
 
 		if ($categoryId)
 		{
-			$query->where($db->quoteName('a.catid' . ' = :catid')
+			$query->where($db->quoteName('a.catid' . ' = :catid'))
 				->bind(':catid', $categoryId, ParameterType::INTEGER);
 		}
 
