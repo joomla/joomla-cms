93d9ce55aae31106b8568d06fad28c3914e79136
Nicola Galgano <optimus4joomla@gmail.com>
2019-06-05 21:19:11 +0200

    fix

--- administrator/components/com_users/Model/GroupsModel.php
+++ administrator/components/com_users/Model/GroupsModel.php
@@ -224,7 +224,10 @@ class GroupsModel extends ListModel
 		}
 
 		// Get total disabled users in group.
-		$query->clear('where')
+		$query->clear();
+		$query->select('map.group_id, COUNT(DISTINCT map.user_id) AS user_count')
+			->from($db->quoteName('#__user_usergroup_map', 'map'))
+			->join('LEFT', $db->quoteName('#__users', 'u') . ' ON ' . $db->quoteName('u.id') . ' = ' . $db->quoteName('map.user_id'))
 			->whereIn($db->quoteName('map.group_id'), $groupIds)
 			->where('u.block = 1');
 		$db->setQuery($query);
