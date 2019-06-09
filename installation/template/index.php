654057c19a8877a1a74d4cd8d97641fac60800e2
chmst <dev@chmst.de>
2019-06-09 09:34:37 +0200

    Simplify code for intaller header, center text

--- installation/template/index.php
+++ installation/template/index.php
@@ -59,25 +59,23 @@ Text::script('INSTL_FTP_SETTINGS_CORRECT');
 		<div class="j-install">
 			<?php // Header ?>
 			<header id="header" class="header">
-				<div class="d-flex">
-					<div class="w-100 d-flex ">
-						<div class="logo d-none d-md-block"> 
-							<img src="<?php echo $this->baseurl; ?>/template/images/logo-joomla-blue.svg" alt=""/>
-						</div>
-						<div class="mx-2 d-flex d-md-none">
-							<img class="logo-small d-flex d-md-none" src="<?php echo $this->baseurl; ?>/template/images/logo-blue.svg" alt="">
-						</div>
-						<div class="d-flex flex-wrap align-items-center mx-auto">
-							<h1 class="h2 mx-1 d-flex align-items-baseline">
-								<span class="fa fa-cogs d-none d-md-block mx-2 alig-items-center" aria-hidden="true"></span> 
-								<?php echo Text::_('INSTL_PAGE_TITLE'); ?>
-							</h1> 
-							<span class="small mx-1 d-flex align-items-baseline">
-								Joomla! <?php echo (new Version)->getShortVersion(); ?>
-							</span>
-						</div> 
+				<div class="d-flex ">
+					<div class="logo d-none d-md-block"> 
+						<img src="<?php echo $this->baseurl; ?>/template/images/logo-joomla-blue.svg" alt=""/>
+					</div>
+					<div class="mx-2 d-flex d-md-none">
+						<img class="logo-small d-flex d-md-none" src="<?php echo $this->baseurl; ?>/template/images/logo-blue.svg" alt="">
+					</div>
+					<div class="d-flex flex-wrap align-items-center mx-auto">
+						<h1 class="h2 mx-1 d-flex align-items-baseline">
+							<span class="fa fa-cogs d-none d-md-block mx-2 alig-items-center" aria-hidden="true"></span> 
+							<?php echo Text::_('INSTL_PAGE_TITLE'); ?>
+						</h1> 
+						<span class="small mx-1">
+							Joomla! <?php echo (new Version)->getShortVersion(); ?>
+						</span>
 					</div> 
-					<div class="mx-2 d-flex flex-shrink-1 align-items-center"> 
+					<div class="pull-right m-2"> 
 						<a href="https://docs.joomla.org/Special:MyLanguage/J4.x:Installing_Joomla"; target="_blank">
 							<span class="fa fa-question" aria-hidden="true"></span>
 							<span class="sr-only"><?php echo Text::_('INSTL_HELP_LINK'); ?></span>
