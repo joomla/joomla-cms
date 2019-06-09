89d7b7ff9ff451645fd2ba76391403a9ca01d856
Jean-Marie Simonet <infografjms@gmail.com>
2019-06-09 17:55:47 +0200

    [4.0] Fix History Versions Modal display (.es6.js instead of .es5)

--- build/media_source/com_contenthistory/js/admin-history-versions.es6.js
+++ build/media_source/com_contenthistory/js/admin-history-versions.es6.js
@@ -2,6 +2,7 @@
  * @copyright  Copyright (C) 2005 - 2019 Open Source Matters, Inc. All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
-window.addEventListener('DOMContentLoaded', function() {
-	document.body.appendChild(document.getElementById('versionsModal'));
+
+window.addEventListener('DOMContentLoaded', () => {
+  document.body.appendChild(document.getElementById('versionsModal'));
 });
