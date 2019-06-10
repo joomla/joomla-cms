829e7d7960e122851ff55a860dd58941ead9e6a8
Jean-Marie Simonet <infografjms@gmail.com>
2019-06-10 15:32:43 +0200

    oops

--- build/media_source/com_templates/js/admin-template-toggle-assignment.es6.js
+++ build/media_source/com_templates/js/admin-template-toggle-assignment.es6.js
@@ -3,10 +3,6 @@
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 
-/**
- * @copyright  Copyright (C) 2005 - 2019 Open Source Matters, Inc. All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
 Joomla = window.Joomla || {};
 
 ((Joomla) => {
