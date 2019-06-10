3b711d7503977b69a7d8477883f8f76824c3d40b
Jean-Marie Simonet <infografjms@gmail.com>
2019-06-10 12:03:24 +0200

    another hound test

--- build/media_source/com_templates/js/admin-template-toggle-assignment.es6.js
+++ build/media_source/com_templates/js/admin-template-toggle-assignment.es6.js
@@ -2,7 +2,7 @@
  * @copyright  Copyright (C) 2005 - 2019 Open Source Matters, Inc. All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
-toggleAll = () => {
+let toggleAll = () => {
   const checkBoxes = [].slice.call(document.querySelectorAll('.chk-menulink'));
   const value = checkBoxes[0].checked;
   checkBoxes.forEach((checkBox) => {
@@ -10,7 +10,7 @@ toggleAll = () => {
   });
 };
 
-toggleMenutype = (a) => {
+let toggleMenutype = (a) => {
   const checkBox = [].slice.call(document.getElementsByClassName(a));
   const value = checkBox[0].checked;
   checkBox.forEach((element) => {
