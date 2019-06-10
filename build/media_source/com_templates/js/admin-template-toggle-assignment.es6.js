47ba13dfdc680500e3fb8eb06828794152f36a47
Jean-Marie Simonet <infografjms@gmail.com>
2019-06-10 10:09:34 +0200

    more

--- build/media_source/com_templates/js/admin-template-toggle-assignment.es6.js
+++ build/media_source/com_templates/js/admin-template-toggle-assignment.es6.js
@@ -2,19 +2,18 @@
  * @copyright  Copyright (C) 2005 - 2019 Open Source Matters, Inc. All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
-
-function toggle_all() {
-  const checkBoxes = [].slice.call(document.querySelectorAll('.chk-menulink'));
-  const value = checkBoxes[0].checked;
-  checkBoxes.forEach(checkBox => {
+function toggleAll() {
+  var checkBoxes = [].slice.call(document.querySelectorAll('.chk-menulink'));
+  var value = checkBoxes[0].checked;
+  checkBoxes.forEach(function (checkBox) {
     checkBox.checked = !value;
   });
 }
 
-function toggle_menutype(a) {
-  const checkBox = [].slice.call(document.getElementsByClassName(a));
-  const value = checkBox[0].checked;
-  checkBox.forEach(element => {
+function toggleMenutype(a) {
+  var checkBox = [].slice.call(document.getElementsByClassName(a));
+  var value = checkBox[0].checked;
+  checkBox.forEach(function (element) {
     element.checked = !value;
   });
 }
