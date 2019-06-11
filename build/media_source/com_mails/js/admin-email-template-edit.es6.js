9c773a7b16999e1e39051a5c6c87969362089ead
Hannes Papenberg <info@joomlager.de>
2019-06-11 20:05:06 +0200

    JS Codestyle

--- build/media_source/com_mails/js/admin-email-template-edit.es6.js
+++ build/media_source/com_mails/js/admin-email-template-edit.es6.js
@@ -7,12 +7,11 @@
   'use strict';
 
   class EmailTemplateEdit {
-
     constructor(form, options) {
       // Set elements
       this.form = form;
       this.inputSubject = this.form.querySelector('#jform_subject');
-      this.inputBody    = this.form.querySelector('#jform_body');
+      this.inputBody = this.form.querySelector('#jform_body');
       this.inputHtmlBody = this.form.querySelector('#jform_htmlbody');
 
       // Set options
@@ -73,7 +72,6 @@
     }
 
     bindListeners() {
-
       // To enable editing of specific input
       this.form.addEventListener('joomla.switcher.on', (event) => {
         const type = event.target.id.slice(6, -9);
@@ -85,17 +83,18 @@
             this.inputSubject.disabled = false;
             this.inputSubject.value = inputValue;
             break;
-          case 'body':
-            this.inputBody.disabled = false;
-            this.setBodyValue(inputValue);
-
-            tagsContainer = this.form.querySelector('.tags-container-body');
-            break;
           case 'htmlbody':
             this.inputHtmlBody.disabled = false;
             this.setHtmlBodyValue(inputValue);
 
             tagsContainer = this.form.querySelector('.tags-container-htmlbody');
+            break;
+          case 'body':
+          default:
+            this.inputBody.disabled = false;
+            this.setBodyValue(inputValue);
+
+            tagsContainer = this.form.querySelector('.tags-container-body');
             break;
         }
 
@@ -116,17 +115,18 @@
             this.inputSubject.disabled = true;
             this.inputSubject.value = inputValue;
             break;
-          case 'body':
-            this.setBodyValue(inputValue);
-            this.inputBody.disabled = true;
-
-            tagsContainer = this.form.querySelector('.tags-container-body');
-            break;
           case 'htmlbody':
             this.setHtmlBodyValue(inputValue);
             this.inputHtmlBody.disabled = true;
 
             tagsContainer = this.form.querySelector('.tags-container-htmlbody');
+            break;
+          case 'body':
+          default:
+            this.setBodyValue(inputValue);
+            this.inputBody.disabled = true;
+
+            tagsContainer = this.form.querySelector('.tags-container-body');
             break;
         }
 
@@ -141,16 +141,14 @@
         button.addEventListener('click', (event) => {
           event.preventDefault();
           const el = event.target;
-          this.insertTag(el.dataset.tag, el.dataset.target)
+          this.insertTag(el.dataset.tag, el.dataset.target);
         });
       })
     }
-
   }
 
   document.addEventListener('DOMContentLoaded', () => {
     const editor = new EmailTemplateEdit(document.getElementById('item-form'), Joomla.getOptions('com_mails'));
     editor.bindListeners();
   });
-
 })(document, Joomla);
