0a83161f0e16b64aa669db25687dfc790dbb2207
Hannes Papenberg <info@joomlager.de>
2019-06-12 00:25:34 +0200

    Fixing SQL install files

--- installation/sql/mysql/joomla.sql
+++ installation/sql/mysql/joomla.sql
@@ -1267,23 +1267,6 @@ INSERT INTO `#__languages` (`lang_id`, `lang_code`, `title`, `title_native`, `se
 -- --------------------------------------------------------
 
 --
--- Table structure for table `#__mail_templates`
---
-
-CREATE TABLE IF NOT EXISTS `#__mail_templates` (
-  `mail_id` VARCHAR(127) NOT NULL DEFAULT '',
-  `language` char(7) NOT NULL DEFAULT '',
-  `subject` VARCHAR(255) NOT NULL DEFAULT '',
-  `body` TEXT NOT NULL,
-  `htmlbody` TEXT NOT NULL,
-  `attachments` TEXT NOT NULL,
-  `params` TEXT NOT NULL,
-  PRIMARY KEY (`mail_id`, `language`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 DEFAULT COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `#__menu`
 --
 
--- installation/sql/postgresql/joomla.sql
+++ installation/sql/postgresql/joomla.sql
@@ -1250,23 +1250,6 @@ INSERT INTO "#__languages" ("lang_id", "lang_code", "title", "title_native", "se
 SELECT setval('#__languages_lang_id_seq', 2, false);
 
 --
--- Table structure for table `#__mail_templates`
---
-
-CREATE TABLE IF NOT EXISTS "#__mail_templates" (
-  "mail_id" varchar(127) NOT NULL DEFAULT '',
-  "language" char(7) NOT NULL DEFAULT '',
-  "subject" varchar(255) NOT NULL DEFAULT '',
-  "body" TEXT NOT NULL,
-  "htmlbody" TEXT NOT NULL,
-  "attachments" TEXT NOT NULL,
-  "params" TEXT NOT NULL,
-  CONSTRAINT "#__mail_templates_idx_mail_id_language" UNIQUE ("mail_id", "language"),
-);
-CREATE INDEX "#__mail_templates_idx_mail_id" ON "#__mail_templates" ("mail_id");
-CREATE INDEX "#__mail_templates_idx_language" ON "#__mail_templates" ("language");
-
---
 -- Table structure for table `#__menu`
 --
 
