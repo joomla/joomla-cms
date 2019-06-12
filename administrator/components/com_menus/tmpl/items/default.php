4bec54995ebdcf6250af69e577a5becccc305a77
Jean-Marie Simonet <infografjms@gmail.com>
2019-06-12 11:27:08 +0200

    [4.0] Menus Manager: Fix system links menu items Status icon and add back tip

--- administrator/components/com_menus/tmpl/items/default.php
+++ administrator/components/com_menus/tmpl/items/default.php
@@ -170,7 +170,11 @@ $assoc   = Associations::isEnabled() && $this->state->get('filter.client_id') ==
 									<?php echo HTMLHelper::_('grid.id', $i, $item->id); ?>
 								</td>
 								<td class="text-center">
-									<?php echo HTMLHelper::_('jgrid.published', $item->published, $i, 'items.', $canChange, 'cb', $item->publish_up, $item->publish_down); ?>
+									<?php
+										// Show protected items as published always. We don't allow state change for them. Show/Hide is the module's job.
+										$published = $item->protected ? 3 : $item->published;
+										echo HTMLHelper::_('menus.state', $published, $i, $canChange && !$item->protected, 'cb', $item->publish_up, $item->publish_down);
+									?>
 								</td>
 								<th scope="row">
 									<?php $prefix = LayoutHelper::render('joomla.html.treeprefix', array('level' => $item->level)); ?>
