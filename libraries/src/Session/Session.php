7ac7ec1405bfca923f2f901779ace292a6e80efa
Roland Dalmulder <contact@rolandd.com>
2019-06-18 19:55:05 +0200

    Make sure the function matches the parent return type
    Signed-off-by: Roland Dalmulder <contact@rolandd.com>

--- libraries/src/Session/Session.php
+++ libraries/src/Session/Session.php
@@ -201,7 +201,7 @@ class Session extends BaseSession
 	 *
 	 * @since   1.5
 	 */
-	public function clear()
+	public function clear(): void
 	{
 		// Handle B/C by checking if parameters were passed to this method; if so proxy to the new remove() method, will be removed at 5.0
 		if (func_num_args() >= 1)
@@ -230,10 +230,10 @@ class Session extends BaseSession
 					$name = $args[1] . '.' . $name;
 				}
 
-				return $this->remove($name);
+				$this->remove($name);
 			}
 		}
 
-		return parent::clear();
+		parent::clear();
 	}
 }
