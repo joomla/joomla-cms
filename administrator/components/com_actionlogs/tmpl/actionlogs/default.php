2a035aab81b14622a639235dc2bc52ab70908c1f
Roland Dalmulder <contact@rolandd.com>
2019-06-19 07:27:06 +0200

    Update administrator/components/com_actionlogs/tmpl/actionlogs/default.php
    Co-Authored-By: Quy <quy@fluxbb.org>

--- administrator/components/com_actionlogs/tmpl/actionlogs/default.php
+++ administrator/components/com_actionlogs/tmpl/actionlogs/default.php
@@ -56,7 +56,6 @@ Factory::getDocument()->addScriptDeclaration('
 	<div id="j-main-container" class="j-main-container">
 		<?php // Search tools bar ?>
 		<?php echo LayoutHelper::render('joomla.searchtools.default', array('view' => $this)); ?>
-		?>
 		<?php if (empty($this->items)) : ?>
 			<div class="alert alert-info">
 				<span class="fa fa-info-circle" aria-hidden="true"></span><span class="sr-only"><?php echo Text::_('INFO'); ?></span>
