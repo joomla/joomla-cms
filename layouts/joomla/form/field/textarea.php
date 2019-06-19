acbf3107a1c7b53db90773e00954352f4c873af0
Brian Teeman <brian@teeman.net>
2019-06-19 14:11:35 +0100

    Update textarea.php

--- layouts/joomla/form/field/textarea.php
+++ layouts/joomla/form/field/textarea.php
@@ -59,7 +59,7 @@ if ($charcounter)
 
 	// Set the css class to be used as the trigger
 	$charcounter = 'charcount';
-	
+
 	// Set the text
 	$counterlabel = 'data-counter-label="' . Text::_('COM_CONFIG_METADESC_COUNTER') . '"';
 }
