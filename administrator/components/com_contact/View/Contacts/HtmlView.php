753187f85c71a5d4b1535fac3f92f839f6d4ff5a
Brian Teeman <brian@teeman.net>
2019-06-23 19:24:25 +0100

    [4.0] com_contacts sidebar
    This is some additional corrections to be applied after #23366 to complete the removaal of the sidebar for com_contacts

--- administrator/components/com_contact/View/Contacts/HtmlView.php
+++ administrator/components/com_contact/View/Contacts/HtmlView.php
@@ -21,7 +21,6 @@ use Joomla\CMS\MVC\View\GenericDataException;
 use Joomla\CMS\MVC\View\HtmlView as BaseHtmlView;
 use Joomla\CMS\Toolbar\Toolbar;
 use Joomla\CMS\Toolbar\ToolbarHelper;
-use Joomla\Component\Contact\Administrator\Helper\ContactHelper;
 
 /**
  * View class for a list of contacts.
@@ -66,13 +65,6 @@ class HtmlView extends BaseHtmlView
 	public $activeFilters;
 
 	/**
-	 * The sidebar markup
-	 *
-	 * @var  string
-	 */
-	protected $sidebar;
-
-	/**
 	 * Display the view.
 	 *
 	 * @param   string  $tpl  The name of the template file to parse; automatically searches through the template paths.
@@ -81,11 +73,6 @@ class HtmlView extends BaseHtmlView
 	 */
 	public function display($tpl = null)
 	{
-		if ($this->getLayout() !== 'modal')
-		{
-			ContactHelper::addSubmenu('contacts');
-		}
-
 		$this->items         = $this->get('Items');
 		$this->pagination    = $this->get('Pagination');
 		$this->state         = $this->get('State');
@@ -110,7 +97,6 @@ class HtmlView extends BaseHtmlView
 		if ($this->getLayout() !== 'modal')
 		{
 			$this->addToolbar();
-			$this->sidebar = \JHtmlSidebar::render();
 
 			// We do not need to filter by language when multilingual is disabled
 			if (!Multilanguage::isEnabled())
@@ -224,8 +210,6 @@ class HtmlView extends BaseHtmlView
 		}
 
 		$toolbar->help('JHELP_COMPONENTS_CONTACTS_CONTACTS');
-
-		HTMLHelper::_('sidebar.setAction', 'index.php?option=com_contact');
 	}
 
 	/**
