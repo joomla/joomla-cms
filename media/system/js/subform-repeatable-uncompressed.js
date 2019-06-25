f1750e4f4bd53c0fd58f6636d688e4a28d97041c
Fedik <getthesite@gmail.com>
2019-06-25 18:02:20 +0300

    Anothe subform fix for IE compatibility

--- media/system/js/subform-repeatable-uncompressed.js
+++ media/system/js/subform-repeatable-uncompressed.js
@@ -68,14 +68,14 @@
 			// Find the template element and get its HTML content, this is our template.
 			var $tmplElement = this.$container.find(this.options.rowTemplateSelector).last();
 
-			// Move the template out of the form scope, for IE compatibility.
-			// But only a root template (!!!)
-			if (!$tmplElement.parents(this.options.rowTemplateSelector).length) {
-				$(document.body).append($tmplElement);
-			}
-
 			this.template = $.trim($tmplElement.html()) || '';
-			this.$tmplElement = $tmplElement;
+
+			// This is IE fix for <template>
+			$tmplElement.css('display', 'none'); // Make sure it not visible
+			var map = {'SUBFORMLT': '<', 'SUBFORMGT': '>'};
+			this.template = this.template.replace(/(SUBFORMLT)|(SUBFORMGT)/g, function(match){
+				return map[match];
+			});
 		}
 		// create from existing rows
 		else {
