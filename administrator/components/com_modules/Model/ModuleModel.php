8af0dc5ac160227131d7e7c99451bba6cb64672c
Nicola Galgano <optimus4joomla@gmail.com>
2019-07-02 08:44:49 +0200

    Update administrator/components/com_modules/Model/ModuleModel.php
    Co-Authored-By: Quy <quy@fluxbb.org>

--- administrator/components/com_modules/Model/ModuleModel.php
+++ administrator/components/com_modules/Model/ModuleModel.php
@@ -1033,7 +1033,7 @@ class ModuleModel extends AdminModel
 				// Assign new module to `all` menu item associations.
 				$query->clear()
 					->insert($db->quoteName('#__modules_menu'))
-					->columns(array($db->quoteName('moduleid'), $db->quoteName('menuid')))
+					->columns([$db->quoteName('moduleid'), $db->quoteName('menuid')])
 					->values(':moduleid', 0);
 					->bind(':moduleid', $table->id, ParameterType::INTEGER);
 				$db->setQuery($query);
