055d8d1c60873d849207c02095529c2e392c0210
Quy <quy@fluxbb.org>
2019-07-02 20:34:48 -0700

    Fix indentation

--- administrator/components/com_associations/layouts/joomla/searchtools/default.php
+++ administrator/components/com_associations/layouts/joomla/searchtools/default.php
@@ -51,8 +51,8 @@ $filtersClass = isset($data['view']->activeFilters) && $data['view']->activeFilt
 		<?php $itemTypeField = $data['view']->filterForm->getField('itemtype'); ?>
 		<div class="js-stools-container-selector-first">
 			<div class="js-stools-field-selector js-stools-itemtype">
-        <div class="sr-only"><?php echo $itemTypeField->label; ?></div>
-        <?php echo $itemTypeField->input; ?>
+				<div class="sr-only"><?php echo $itemTypeField->label; ?></div>
+				<?php echo $itemTypeField->input; ?>
 			</div>
 		</div>
 	<?php endif; ?>
@@ -60,8 +60,8 @@ $filtersClass = isset($data['view']->activeFilters) && $data['view']->activeFilt
 		<?php $languageField = $data['view']->filterForm->getField('language'); ?>
 		<div class="js-stools-container-selector">
 			<div class="js-stools-field-selector js-stools-language">
-        <div class="sr-only"><?php echo $languageField->label; ?></div>
-        <?php echo $languageField->input; ?>
+				<div class="sr-only"><?php echo $languageField->label; ?></div>
+				<?php echo $languageField->input; ?>
 			</div>
 		</div>
 	<?php endif; ?>
