77bd61cdd4ff26c030229c98acaabd583f940d91
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2019-07-03 13:20:16 +0300

    Passwordless authentication with WebAuthn
    Add support for extra login buttons to com_users' login page.

--- components/com_users/tmpl/login/default_login.php
+++ components/com_users/tmpl/login/default_login.php
@@ -46,7 +46,7 @@ $usersConfig = ComponentHelper::getParams('com_users');
 	</div>
 	<?php endif; ?>
 
-	<form action="<?php echo Route::_('index.php?option=com_users&task=user.login'); ?>" method="post" class="com-users-login__form form-validate form-horizontal well">
+	<form action="<?php echo Route::_('index.php?option=com_users&task=user.login'); ?>" method="post" class="com-users-login__form form-validate form-horizontal well" id="com-users-login__form">
 
 		<fieldset>
 			<?php echo $this->form->renderFieldset('credentials', ['class' => 'com-users-login__input']); ?>
@@ -67,6 +67,28 @@ $usersConfig = ComponentHelper::getParams('com_users');
 					</div>
 				</div>
 			<?php endif; ?>
+
+			<?php foreach ($this->extraButtons as $button): ?>
+				<div class="com-users-login__submit control-group">
+					<div class="controls">
+						<button type="button"
+						        class="btn btn-secondary <?= $button['class'] ?? '' ?>"
+						        onclick="<?= $button['onclick'] ?>"
+						        title="<?= Text::_($button['label']) ?>"
+						        id="<?= $button['id'] ?>"
+						>
+							<?php if (!empty($button['icon'])): ?>
+								<span class="<?= $button['icon'] ?>"></span>
+							<?php elseif (!empty($button['image'])): ?>
+								<?= HTMLHelper::_('image', $button['image'], Text::_('PLG_SYSTEM_WEBAUTHN_LOGIN_DESC'), [
+									'class' => 'icon',
+								], true) ?>
+							<?php endif; ?>
+							<?= Text::_($button['label']) ?>
+						</button>
+					</div>
+				</div>
+			<?php endforeach; ?>
 
 			<div class="com-users-login__submit control-group">
 				<div class="controls">
