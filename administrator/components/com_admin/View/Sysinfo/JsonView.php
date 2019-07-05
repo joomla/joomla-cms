afec65b3d865a06a53d8aedbbad9ce2d72448b56
SharkyKZ <sharkykz@gmail.com>
2019-07-05 09:10:46 +0300

    Rename SysInfoModel to match view

--- administrator/components/com_admin/View/Sysinfo/JsonView.php
+++ administrator/components/com_admin/View/Sysinfo/JsonView.php
@@ -16,7 +16,7 @@ use Joomla\CMS\Access\Exception\Notallowed;
 use Joomla\CMS\Factory;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\MVC\View\AbstractView;
-use Joomla\Component\Admin\Administrator\Model\SysInfoModel;
+use Joomla\Component\Admin\Administrator\Model\SysinfoModel;
 
 /**
  * Sysinfo View class for the Admin component
@@ -64,7 +64,7 @@ class JsonView extends AbstractView
 	 */
 	protected function getLayoutData(): array
 	{
-		/** @var SysInfoModel $model */
+		/** @var SysinfoModel $model */
 		$model = $this->getModel();
 
 		return [
