b20267ede9abc1220dae79c3a85af2d48c02c0e3
Benjamin Trenkle <benjamin.trenkle@wicked-software.de>
2019-07-06 05:29:37 +0200

    Format menu edit screen Format message edit screen Format redirect edit screen

--- administrator/components/com_messages/tmpl/message/edit.php
+++ administrator/components/com_messages/tmpl/message/edit.php
@@ -17,17 +17,21 @@ HTMLHelper::_('behavior.keepalive');
 ?>
 <form action="<?php echo Route::_('index.php?option=com_messages'); ?>" method="post" name="adminForm" id="message-form" class="form-validate">
 	<div class="adminform mt-2">
-		<div class="form-group">
-			<?php echo $this->form->getLabel('user_id_to'); ?>
-			<?php echo $this->form->getInput('user_id_to'); ?>
-		</div>
-		<div class="form-group">
-			<?php echo $this->form->getLabel('subject'); ?>
-			<?php echo $this->form->getInput('subject'); ?>
-		</div>
-		<div class="form-group">
-			<?php echo $this->form->getLabel('message'); ?>
-			<?php echo $this->form->getInput('message'); ?>
+		<div class="card bg-light">
+			<div class="card-body">
+				<div class="form-group">
+					<?php echo $this->form->getLabel('user_id_to'); ?>
+					<?php echo $this->form->getInput('user_id_to'); ?>
+				</div>
+				<div class="form-group">
+					<?php echo $this->form->getLabel('subject'); ?>
+					<?php echo $this->form->getInput('subject'); ?>
+				</div>
+				<div class="form-group">
+					<?php echo $this->form->getLabel('message'); ?>
+					<?php echo $this->form->getInput('message'); ?>
+				</div>
+			</div>
 		</div>
 	</div>
 	<input type="hidden" name="task" value="">
