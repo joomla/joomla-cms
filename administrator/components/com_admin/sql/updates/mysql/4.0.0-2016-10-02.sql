3d1c426ed8f0a4e233a535ccad99bb6d2fd29f93
Nicola Galgano <optimus4joomla@gmail.com>
2019-07-08 08:27:47 +0200

    null date

--- administrator/components/com_admin/sql/updates/mysql/4.0.0-2016-10-02.sql
+++ administrator/components/com_admin/sql/updates/mysql/4.0.0-2016-10-02.sql
@@ -5,8 +5,8 @@ DELETE FROM `#__template_styles` WHERE `template` = 'isis' AND `client_id` = 1;
 ALTER TABLE `#__user_keys` DROP COLUMN `invalid`;
 
 INSERT INTO `#__extensions` (`name`, `type`, `element`, `folder`, `client_id`, `enabled`, `access`, `protected`, `manifest_cache`, `params`, `checked_out`, `checked_out_time`, `ordering`, `state`) VALUES
-('atum', 'template', 'atum', '', 1, 1, 1, 0, '{}', '{}', 0, '0000-00-00 00:00:00', 0, 0),
-('cassiopeia', 'template', 'cassiopeia', '', 0, 1, 1, 0, '{}', '{}', 0, '0000-00-00 00:00:00', 0, 0);
+('atum', 'template', 'atum', '', 1, 1, 1, 0, '{}', '{}', 0, '1000-01-01 00:00:00', 0, 0),
+('cassiopeia', 'template', 'cassiopeia', '', 0, 1, 1, 0, '{}', '{}', 0, '1000-01-01 00:00:00', 0, 0);
 
 INSERT INTO `#__template_styles` (`template`, `client_id`, `home`, `title`, `params`) VALUES
 ('atum', 0, '0', 'atum - Default', '{}'),
