df48aff1d8c864c2ba09c6a5ec78b5af6a31e5f7
Nicola Galgano <optimus4joomla@gmail.com>
2019-07-08 08:34:43 +0200

    null date

--- administrator/components/com_admin/sql/updates/mysql/4.0.0-2018-07-19.sql
+++ administrator/components/com_admin/sql/updates/mysql/4.0.0-2018-07-19.sql
@@ -6,13 +6,13 @@ CREATE TABLE IF NOT EXISTS `#__template_overrides` (
   `state` tinyint(1) NOT NULL DEFAULT 0,
   `action` varchar(50) NOT NULL DEFAULT '',
   `client_id` tinyint(1) unsigned NOT NULL DEFAULT 0,
-  `created_date` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `modified_date` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `created_date` datetime NOT NULL DEFAULT '1000-01-01 00:00:00',
+  `modified_date` datetime NOT NULL DEFAULT '1000-01-01 00:00:00',
   PRIMARY KEY (`id`),
   KEY `idx_template` (`template`),
   KEY `idx_extension_id` (`extension_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 DEFAULT COLLATE=utf8mb4_unicode_ci;
 
 INSERT INTO `#__extensions` (`package_id`, `name`, `type`, `element`, `folder`, `client_id`, `enabled`, `access`, `protected`, `manifest_cache`, `params`, `checked_out`, `checked_out_time`, `ordering`, `state`) VALUES
-(0, 'plg_installer_override', 'plugin', 'override', 'installer', 0, 1, 1, 1, '', '', 0, '0000-00-00 00:00:00', 4, 0),
-(0, 'plg_quickicon_overridecheck', 'plugin', 'overridecheck', 'quickicon', 0, 1, 1, 1, '', '', 0, '0000-00-00 00:00:00', 0, 0);
+(0, 'plg_installer_override', 'plugin', 'override', 'installer', 0, 1, 1, 1, '', '', 0, '1000-01-01 00:00:00', 4, 0),
+(0, 'plg_quickicon_overridecheck', 'plugin', 'overridecheck', 'quickicon', 0, 1, 1, 1, '', '', 0, '1000-01-01 00:00:00', 0, 0);
