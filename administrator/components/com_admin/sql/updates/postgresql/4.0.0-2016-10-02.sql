72318b5b08dc86e127beaf8fd5dca44190cabc07
Olivier Laverré <olaverre@free.fr>
2019-07-08 18:50:28 +0200

    Fix home cast (text to smallint)
    See 
    https://github.com/joomla/joomla-cms/issues/25467#issuecomment-509303284

--- administrator/components/com_admin/sql/updates/postgresql/4.0.0-2016-10-02.sql
+++ administrator/components/com_admin/sql/updates/postgresql/4.0.0-2016-10-02.sql
@@ -8,8 +8,8 @@ INSERT INTO "#__extensions" ("name", "type", "element", "folder", "client_id", "
 ('cassiopeia', 'template', 'cassiopeia', '', 0, 1, 1, 0, '{}', '{}', '', '', 0, '1970-01-01 00:00:00', 0, 0);
 
 INSERT INTO "#__template_styles" ("template", "client_id", "home", "title", "params") VALUES
-('atum', 1, (CASE WHEN (SELECT count FROM (SELECT count("id") AS count FROM "#__template_styles" WHERE home = '1' AND client_id = 1 AND "template" IN ('isis', 'hathor')) as c) = 0 THEN '0' ELSE '1' END), 'atum - Default', '{}'),
-('cassiopeia', 0, (CASE WHEN (SELECT count FROM (SELECT count("id") AS count FROM "#__template_styles" WHERE home = '1' AND client_id = 0 AND "template" IN ('protostar', 'beez3')) as c) = 0 THEN '0' ELSE '1' END), 'cassiopeia - Default', '{}');
+('atum', 1, (CASE WHEN (SELECT count FROM (SELECT count("id") AS count FROM "#__template_styles" WHERE home = 1 AND client_id = 1 AND "template" IN ('isis', 'hathor')) as c) = 0 THEN 0 ELSE 1 END), 'atum - Default', '{}'),
+('cassiopeia', 0, (CASE WHEN (SELECT count FROM (SELECT count("id") AS count FROM "#__template_styles" WHERE home = 1 AND client_id = 0 AND "template" IN ('protostar', 'beez3')) as c) = 0 THEN 0 ELSE 1 END), 'cassiopeia - Default', '{}');
 
 --
 -- Now we can clean up the old templates
