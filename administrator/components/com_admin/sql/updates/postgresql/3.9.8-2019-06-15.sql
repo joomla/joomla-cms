56be33c75cae4caa0384e8632e33055793c5b305
Richard Fath <richard67@users.noreply.github.com>
2019-07-09 22:53:42 +0200

    Correct old schema updates and rename new ones

--- administrator/components/com_admin/sql/updates/postgresql/3.9.8-2019-06-15.sql
+++ administrator/components/com_admin/sql/updates/postgresql/3.9.8-2019-06-15.sql
@@ -1,3 +1,4 @@
 DROP INDEX IF EXISTS "#__template_styles_idx_home";
-CREATE INDEX "#__template_styles_idx_client_id" ON "#__template_styles" ("client_id");
 # Queries removed, see https://github.com/joomla/joomla-cms/pull/25484
+CREATE INDEX "#__template_styles_idx_client_id" ON "#__template_styles" ("client_id");
+CREATE INDEX "#__template_styles_idx_client_id_home" ON "#__template_styles" ("client_id", "home");
