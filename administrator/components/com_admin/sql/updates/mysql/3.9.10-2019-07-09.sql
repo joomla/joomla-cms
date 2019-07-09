1b49fa10502b33b4ed08abd9d624313c8e670efb
Richard Fath <richard67@users.noreply.github.com>
2019-07-10 00:30:18 +0200

    Keep the index as it was

--- administrator/components/com_admin/sql/updates/mysql/3.9.10-2019-07-09.sql
+++ administrator/components/com_admin/sql/updates/mysql/3.9.10-2019-07-09.sql
@@ -1,3 +1,1 @@
-ALTER TABLE `#__template_styles` DROP INDEX `idx_client_id_home`;
 ALTER TABLE `#__template_styles` MODIFY `home` char(7) NOT NULL DEFAULT '0';
-ALTER TABLE `#__template_styles` ADD INDEX `idx_client_id_home_2` (`client_id`, `home`);
--- administrator/components/com_admin/sql/updates/postgresql/3.9.10-2019-07-09.sql
+++ administrator/components/com_admin/sql/updates/postgresql/3.9.10-2019-07-09.sql
@@ -1,4 +1,2 @@
-DROP INDEX IF EXISTS "#__template_styles_idx_client_id_home";
 ALTER TABLE "#__template_styles" ALTER COLUMN "home" TYPE varchar(7);
 ALTER TABLE "#__template_styles" ALTER COLUMN "home" SET DEFAULT '0';
-CREATE INDEX "#__template_styles_idx_client_id_home_2" ON "#__template_styles" ("client_id", "home");
