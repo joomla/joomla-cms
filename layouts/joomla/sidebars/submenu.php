c258cd0b585da332a4d80864c6074c18fa264b02
Benjamin Trenkle <benjamin.trenkle@wicked-software.de>
2019-07-10 10:40:12 +0200

    Add aria-hidden

--- layouts/joomla/sidebars/submenu.php
+++ layouts/joomla/sidebars/submenu.php
@@ -19,7 +19,7 @@ HTMLHelper::_('behavior.core');
 <div id="j-toggle-sidebar-wrapper">
 	<div id="sidebar" class="sidebar">
 		<button class="btn btn-sm btn-secondary my-2 options-menu d-md-none" type="button" data-toggle="collapse" data-target=".sidebar-nav" aria-controls="sidebar-nav" aria-expanded="false" aria-label="<?php echo Text::_('TPL_ATUM_TOGGLE_SIDEBAR'); ?>">
-			<span class="fas fa-align-justify"></span>
+			<span class="fas fa-align-justify" aria-hidden="true"></span>
 				<?php echo Text::_('TPL_ATUM_TOGGLE_SIDEBAR'); ?>
 		</button>
 		<div class="sidebar-nav bg-light p-2 my-2">
