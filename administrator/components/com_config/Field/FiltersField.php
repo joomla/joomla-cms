00e2d70c11bdf63de61e12e4f9ab7fe18472d929
Hannes Papenberg <info@joomlager.de>
2019-07-11 13:30:54 +0200

    Fixing warnings

--- administrator/components/com_config/Field/FiltersField.php
+++ administrator/components/com_config/Field/FiltersField.php
@@ -132,7 +132,8 @@ class FiltersField extends FormField
 			$html[] = '			' . LayoutHelper::render('joomla.html.treeprefix', array('level' => $group->level + 1)) . $group->text;
 			$html[] = '		</td>';
 			$html[] = '		<td>';
-			$html[] = '			<label for="' . $this->id . $group->value . '_filter_type" class="sr-only">' . Text::_('JGLOBAL_FILTER_TYPE_LABEL') . '</label>';
+			$html[] = '			<label for="' . $this->id . $group->value . '_filter_type" class="sr-only">'
+				. Text::_('JGLOBAL_FILTER_TYPE_LABEL') . '</label>';
 			$html[] = '				<select'
 				. ' name="' . $this->name . '[' . $group->value . '][filter_type]"'
 				. ' id="' . $this->id . $group->value . '_filter_type"'
@@ -153,7 +154,8 @@ class FiltersField extends FormField
 			$html[] = '				</select>';
 			$html[] = '		</td>';
 			$html[] = '		<td>';
-			$html[] = '			<label for="' . $this->id . $group->value . '_filter_tags" class="sr-only">' . Text::_('JGLOBAL_FILTER_TAGS_LABEL') . '</label>';
+			$html[] = '			<label for="' . $this->id . $group->value . '_filter_tags" class="sr-only">'
+				. Text::_('JGLOBAL_FILTER_TAGS_LABEL') . '</label>';
 			$html[] = '				<input'
 				. ' name="' . $this->name . '[' . $group->value . '][filter_tags]"'
 				. ' type="text"'
