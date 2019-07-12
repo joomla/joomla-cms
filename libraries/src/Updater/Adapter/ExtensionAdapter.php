f3547137740ce555b5c3145ae32299ac8f9b3dcb
Michael Babker <michael.babker@gmail.com>
2019-07-12 08:11:01 -0500

    Remove deprecated *_dev_level attributes from update server manifests (#25495)

--- libraries/src/Updater/Adapter/ExtensionAdapter.php
+++ libraries/src/Updater/Adapter/ExtensionAdapter.php
@@ -109,31 +109,9 @@ class ExtensionAdapter extends UpdateAdapter
 				// Lower case and remove the exclamation mark
 				$product = strtolower(InputFilter::getInstance()->clean(Version::PRODUCT, 'cmd'));
 
-				// Support for the min_dev_level and max_dev_level attributes is deprecated, a regexp should be used instead
-				if (isset($this->currentUpdate->targetplatform->min_dev_level) || isset($this->currentUpdate->targetplatform->max_dev_level))
-				{
-					Log::add(
-						'Support for the min_dev_level and max_dev_level attributes of an update\'s <targetplatform> tag is deprecated and'
-						. ' will be removed in 4.0. The full version should be specified in the version attribute and may optionally be a regexp.',
-						Log::WARNING,
-						'deprecated'
-					);
-				}
-
-				/*
-				 * Check that the product matches and that the version matches (optionally a regexp)
-				 *
-				 * Check for optional min_dev_level and max_dev_level attributes to further specify targetplatform (e.g., 3.0.1)
-				 */
-				$patchMinimumSupported = !isset($this->currentUpdate->targetplatform->min_dev_level)
-					|| Version::PATCH_VERSION >= $this->currentUpdate->targetplatform->min_dev_level;
-				$patchMaximumSupported = !isset($this->currentUpdate->targetplatform->max_dev_level)
-					|| Version::PATCH_VERSION <= $this->currentUpdate->targetplatform->max_dev_level;
-
+				// Check that the product matches and that the version matches (optionally a regexp)
 				if ($product == $this->currentUpdate->targetplatform['NAME']
-					&& preg_match('/^' . $this->currentUpdate->targetplatform['VERSION'] . '/', JVERSION)
-					&& $patchMinimumSupported
-					&& $patchMaximumSupported)
+					&& preg_match('/^' . $this->currentUpdate->targetplatform['VERSION'] . '/', JVERSION))
 				{
 					// Check if PHP version supported via <php_minimum> tag, assume true if tag isn't present
 					if (!isset($this->currentUpdate->php_minimum) || version_compare(PHP_VERSION, $this->currentUpdate->php_minimum, '>='))
