9d91f4187fab1dca5758d5810ed7243100ede031
George Wilson <georgejameswilson@googlemail.com>
2019-07-12 15:53:21 +0100

    Fix concat issues

--- plugins/webservices/privacy/privacy.php
+++ plugins/webservices/privacy/privacy.php
@@ -43,18 +43,18 @@ class PlgWebservicesPrivacy extends CMSPlugin
 		$getDefaults = array_merge(array('public' => false), $defaults);
 
 		$routes = array(
-			new Route(['GET'], 'v1/privacy/request', 'requests' . '.displayList', [], $getDefaults),
-			new Route(['GET'], 'v1/privacy/request' . '/:id', 'requests' . '.displayItem', ['id' => '(\d+)'], $getDefaults),
-			new Route(['POST'], 'v1/privacy/request', 'requests' . '.add', [], $defaults)
+			new Route(['GET'], 'v1/privacy/request', 'requests.displayList', [], $getDefaults),
+			new Route(['GET'], 'v1/privacy/request/:id', 'requests.displayItem', ['id' => '(\d+)'], $getDefaults),
+			new Route(['POST'], 'v1/privacy/request', 'requests.add', [], $defaults)
 		);
 
 		$router->addRoutes($routes);
 
 		$routes = array(
-			new Route(['GET'], 'v1/privacy/consent', 'consents' . '.displayList', [], $getDefaults),
-			new Route(['GET'], 'v1/privacy/consent' . '/:id', 'consents' . '.displayItem', ['id' => '(\d+)'], $getDefaults),
-			new Route(['POST'], 'v1/privacy/consent', 'consents' . '.add', [], $defaults),
-			new Route(['DELETE'], 'v1/privacy/consent' . '/:id', 'consents' . '.delete', ['id' => '(\d+)'], $defaults)
+			new Route(['GET'], 'v1/privacy/consent', 'consents.displayList', [], $getDefaults),
+			new Route(['GET'], 'v1/privacy/consent' . '/:id', 'consents.displayItem', ['id' => '(\d+)'], $getDefaults),
+			new Route(['POST'], 'v1/privacy/consent', 'consents.add', [], $defaults),
+			new Route(['DELETE'], 'v1/privacy/consent' . '/:id', 'consents.delete', ['id' => '(\d+)'], $defaults)
 		);
 
 		$router->addRoutes($routes);
