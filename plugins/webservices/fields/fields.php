0b4362a65bd3f0460dfaf9ef13a7b709c7ca67cb
a-samoylov <a.samoylovua@gmail.com>
2019-07-14 14:54:30 +0300

    add routes to fields contact/contact, contact/mail, contact/categories

--- plugins/webservices/fields/fields.php
+++ plugins/webservices/fields/fields.php
@@ -38,40 +38,116 @@ class PlgWebservicesFields extends CMSPlugin
 	 */
 	public function onBeforeApiRoute(&$router)
 	{
+		$this->createContentRoutes($router);
+		$this->createUserRoutes($router);
+		$this->createContactRoutes($router);
+	}
+
+	/**
+	 * Create content routes
+	 *
+	 * @param   ApiRouter  &$router  The API Routing object
+	 *
+	 * @return  void
+	 *
+	 * @since   4.0.0
+	 */
+	private function createContentRoutes(&$router)
+	{
 		$router->createCRUDRoutes(
-			'v1/fields/articles',
+			'v1/fields/content/articles',
 			'fields',
 			['component' => 'com_fields', 'context' => 'com_content.article']
 		);
 
 		$router->createCRUDRoutes(
-			'v1/fields/categories',
+			'v1/fields/content/categories',
 			'fields',
 			['component' => 'com_fields', 'context' => 'com_content.categories']
 		);
 
 		$router->createCRUDRoutes(
-			'v1/fields/users',
-			'fields',
-			['component' => 'com_fields', 'context' => 'com_users.user']
-		);
-
-		$router->createCRUDRoutes(
-			'v1/fields/groups/articles',
+			'v1/fields/groups/content/articles',
 			'groups',
 			['component' => 'com_fields', 'context' => 'com_content.article']
 		);
 
 		$router->createCRUDRoutes(
-			'v1/fields/groups/categories',
+			'v1/fields/groups/content/categories',
 			'groups',
 			['component' => 'com_fields', 'context' => 'com_content.categories']
 		);
+	}
+
+	/**
+	 * Create user routes
+	 *
+	 * @param   ApiRouter  &$router  The API Routing object
+	 *
+	 * @return  void
+	 *
+	 * @since   4.0.0
+	 */
+	private function createUserRoutes(&$router)
+	{
+		$router->createCRUDRoutes(
+			'v1/fields/user/users',
+			'fields',
+			['component' => 'com_fields', 'context' => 'com_users.user']
+		);
 
 		$router->createCRUDRoutes(
-			'v1/fields/groups/users',
+			'v1/fields/groups/user/users',
 			'groups',
 			['component' => 'com_fields', 'context' => 'com_users.user']
+		);
+	}
+
+	/**
+	 * Create contact routes
+	 *
+	 * @param   ApiRouter  &$router  The API Routing object
+	 *
+	 * @return  void
+	 *
+	 * @since   4.0.0
+	 */
+	private function createContactRoutes(&$router)
+	{
+		$router->createCRUDRoutes(
+			'v1/fields/contact/contact',
+			'fields',
+			['component' => 'com_fields', 'context' => 'com_contact.contact']
+		);
+
+		$router->createCRUDRoutes(
+			'v1/fields/contact/mail',
+			'fields',
+			['component' => 'com_fields', 'context' => 'com_contact.mail']
+		);
+
+		$router->createCRUDRoutes(
+			'v1/fields/contact/categories',
+			'fields',
+			['component' => 'com_fields', 'context' => 'com_contact.categories']
+		);
+
+		$router->createCRUDRoutes(
+			'v1/fields/groups/contact/contact',
+			'groups',
+			['component' => 'com_fields', 'context' => 'com_contact.contact']
+		);
+
+		$router->createCRUDRoutes(
+			'v1/fields/groups/contact/mail',
+			'groups',
+			['component' => 'com_fields', 'context' => 'com_contact.mail']
+		);
+
+		$router->createCRUDRoutes(
+			'v1/fields/groups/contact/categories',
+			'groups',
+			['component' => 'com_fields', 'context' => 'com_contact.categories']
 		);
 	}
 }
