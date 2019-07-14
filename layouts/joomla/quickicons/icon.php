643bf1005412a5b81ef93bbd34bef76f6862755d
chmst <dev@chmst.de>
2019-07-14 16:16:29 +0200

    Scss rtl-fixes for quickicons

--- layouts/joomla/quickicons/icon.php
+++ layouts/joomla/quickicons/icon.php
@@ -91,9 +91,9 @@ else
 	<?php // Add the link to the edit-form
 	if (!empty($displayData['linkadd'])): ?>
 			<a class="btn-block quickicon-linkadd j-links-link" href="<?php echo $displayData['linkadd']; ?>">
-				<span class="fa fa-plus mr-2" aria-hidden="true"></span>
+				<span class="fa fa-plus" aria-hidden="true"></span>
 				<span class="sr-only"><?php echo Text::sprintf('MOD_QUICKICON_ADD_NEW', $add); ?></span>
-				<span aria-hidden="true"><?php echo $add; ?></span>
+				<span class= mr-auto" aria-hidden="true"><?php echo $add; ?></span>
 			</a>
 	<?php endif; ?>
 </li>
