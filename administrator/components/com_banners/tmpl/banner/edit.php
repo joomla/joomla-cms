4cfd5e157e4bca894c05a7e376f9392f5d06b78b
Brian Teeman <brian@teeman.net>
2019-07-18 20:00:11 +0100

    [4.0] Missing behavior.keepalive
    > I'd go with a guideline of if a form gets submitted through POST then it should in most cases have keepalive since that form should have a CSRF check behind it (forms like a create item form, edit form, login form, contact form, etc.), and if it gets submitted through GET it doesn't need a keepalive behavior because these types of forms should essentially be nothing more than query filters for a page. I say this also realizing the backend views are in kind of a FUBAR state because the search filters trigger a POST form submission, and it looks like the frontend com_content.archive view has the same "issue" which means Joomla isn't always using the appropriate HTTP action to do things sadly.
    
    This PR is a result of checking all admin edit.php files and checking that if they have a form submitted through POST they have a behavior.keepalive and a form.token

--- administrator/components/com_banners/tmpl/banner/edit.php
+++ administrator/components/com_banners/tmpl/banner/edit.php
@@ -17,6 +17,7 @@ use Joomla\CMS\Router\Route;
 /** @var \Joomla\Component\Banners\Administrator\View\Banner\HtmlView $this */
 
 HTMLHelper::_('behavior.formvalidator');
+HTMLHelper::_('behavior.keepalive');
 
 HTMLHelper::_('script', 'com_banners/admin-banner-edit.min.js', array('version' => 'auto', 'relative' => true));
 HTMLHelper::_('script', 'com_contenthistory/admin-history-versions.js', ['version' => 'auto', 'relative' => true]);
