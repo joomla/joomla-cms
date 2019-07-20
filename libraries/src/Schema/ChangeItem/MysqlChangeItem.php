32711a0c264f563e3ef9a6bcdc05a82e48d29d01
Richard Fath <richard67@users.noreply.github.com>
2019-07-20 16:48:40 +0200

    Add forgotten UPPER()

--- libraries/src/Schema/ChangeItem/MysqlChangeItem.php
+++ libraries/src/Schema/ChangeItem/MysqlChangeItem.php
@@ -291,30 +291,32 @@ class MysqlChangeItem extends ChangeItem
 	 */
 	private function fixUtf8mb4TypeChecks($type)
 	{
-		$uType = strtoupper(str_replace(';', '', $type));
+		$fixedType = str_replace(';', '', $type);
 
 		if ($this->db->hasUTF8mb4Support())
 		{
+			$uType = strtoupper($fixedType);
+
 			if ($uType === 'TINYTEXT')
 			{
-				$typeCheck = 'type IN (' . $this->db->quote('TINYTEXT') . ',' . $this->db->quote('TEXT') . ')';
+				$typeCheck = 'UPPER(type) IN (' . $this->db->quote('TINYTEXT') . ',' . $this->db->quote('TEXT') . ')';
 			}
 			elseif ($uType === 'TEXT')
 			{
-				$typeCheck = 'type IN (' . $this->db->quote('TEXT') . ',' . $this->db->quote('MEDIUMTEXT') . ')';
+				$typeCheck = 'UPPER(type) IN (' . $this->db->quote('TEXT') . ',' . $this->db->quote('MEDIUMTEXT') . ')';
 			}
 			elseif ($uType === 'MEDIUMTEXT')
 			{
-				$typeCheck = 'type IN (' . $this->db->quote('MEDIUMTEXT') . ',' . $this->db->quote('LONGTEXT') . ')';
+				$typeCheck = 'UPPER(type) IN (' . $this->db->quote('MEDIUMTEXT') . ',' . $this->db->quote('LONGTEXT') . ')';
 			}
 			else
 			{
-				$typeCheck = 'type = ' . $this->db->quote($uType);
+				$typeCheck = 'UPPER(type) = ' . $this->db->quote($fixedType);
 			}
 		}
 		else
 		{
-			$typeCheck = 'type = ' . $this->db->quote($uType);
+			$typeCheck = 'UPPER(type) = ' . $this->db->quote($fixedType);
 		}
 
 		return $typeCheck;
