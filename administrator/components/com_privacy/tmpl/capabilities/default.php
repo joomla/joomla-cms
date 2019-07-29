de0cd65bf4c0346632c86d7d453b0fccc6aa07c0
Brian Teeman <brian@teeman.net>
2019-07-29 16:18:02 +0100

    [4.0] com_privacy capabilities
    This PR changes the heading from h4 to h2 as there should be no "gaps" in the headings
    
    This PR also changes the bootstrap accordion to the browser native Details/Summary element as used elsewhere.
    
    Styling of this is beyond the scope of this PR

--- administrator/components/com_privacy/tmpl/capabilities/default.php
+++ administrator/components/com_privacy/tmpl/capabilities/default.php
@@ -17,7 +17,7 @@ use Joomla\CMS\Language\Text;
 ?>
 <div id="j-main-container">
 	<div class="alert alert-info">
-		<h4 class="alert-heading"><?php echo Text::_('COM_PRIVACY_MSG_CAPABILITIES_ABOUT_THIS_INFORMATION'); ?></h4>
+		<h2 class="alert-heading"><?php echo Text::_('COM_PRIVACY_MSG_CAPABILITIES_ABOUT_THIS_INFORMATION'); ?></h2>
 		<?php echo Text::_('COM_PRIVACY_MSG_CAPABILITIES_INTRODUCTION'); ?>
 	</div>
 	<?php if (empty($this->capabilities)) : ?>
@@ -26,11 +26,9 @@ use Joomla\CMS\Language\Text;
 			<?php echo Text::_('COM_PRIVACY_MSG_CAPABILITIES_NO_CAPABILITIES'); ?>
 		</div>
 	<?php else : ?>
-		<?php $i = 0; ?>
-		<?php echo HTMLHelper::_('bootstrap.startAccordion', 'slide-capabilities', array('active' => 'slide-0')); ?>
-
 		<?php foreach ($this->capabilities as $extension => $capabilities) : ?>
-			<?php echo HTMLHelper::_('bootstrap.addSlide', 'slide-capabilities', $extension, 'slide-' . $i); ?>
+			<details>
+			<summary><?php echo $extension; ?></summary>
 				<?php if (empty($capabilities)) : ?>
 					<div class="alert alert-info">
 						<span class="fa fa-info-circle" aria-hidden="true"></span><span class="sr-only"><?php echo Text::_('INFO'); ?></span>
@@ -43,10 +41,7 @@ use Joomla\CMS\Language\Text;
 						<?php endforeach; ?>
 					</ul>
 				<?php endif; ?>
-			<?php echo HTMLHelper::_('bootstrap.endSlide'); ?>
-			<?php $i++; ?>
+			</details>
 		<?php endforeach; ?>
-
-		<?php echo HTMLHelper::_('bootstrap.endAccordion'); ?>
 	<?php endif; ?>
 </div>
