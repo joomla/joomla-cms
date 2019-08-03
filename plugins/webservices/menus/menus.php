d894b9887641968cc201de8d30a856ad336b8ed7
a-samoylov <a.samoylovua@gmail.com>
2019-08-03 19:56:16 +0300

    com_menus

---
+++ plugins/webservices/menus/menus.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @package     Joomla.Menus
+ * @subpackage  Webservices.Menus
+ *
+ * @copyright   Copyright (C) 2005 - 2019 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Plugin\CMSPlugin;
+use Joomla\CMS\Router\ApiRouter;
+
+/**
+ * Web Services adapter for com_menus.
+ *
+ * @since  4.0.0
+ */
+class PlgWebservicesMenus extends CMSPlugin
+{
+	/**
+	 * Load the language file on instantiation.
+	 *
+	 * @var    boolean
+	 * @since  4.0.0
+	 */
+	protected $autoloadLanguage = true;
+
+	/**
+	 * Registers com_menus's API's routes in the application
+	 *
+	 * @param   ApiRouter  &$router  The API Routing object
+	 *
+	 * @return  void
+	 *
+	 * @since   4.0.0
+	 */
+	public function onBeforeApiRoute(&$router)
+	{
+		$router->createCRUDRoutes('v1/menus', 'menus', ['component' => 'com_menus']);
+	}
+}
