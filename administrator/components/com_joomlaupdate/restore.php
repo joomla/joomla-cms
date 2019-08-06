06af3b0bb71535486fcca9eb1a9c8028ae9c44d8
SharkyKZ <sharkykz@gmail.com>
2019-08-06 13:34:41 +0300

    Revert restore.php

--- administrator/components/com_joomlaupdate/restore.php
+++ administrator/components/com_joomlaupdate/restore.php
@@ -6184,7 +6184,7 @@ class AKText extends AKAbstractObject
 				}
 
 				// Sections
-				if ($line[0] == '[')
+				if ($line{0} == '[')
 				{
 					$tmp        = explode(']', $line);
 					$sections[] = trim(substr($tmp[0], 1));
@@ -6201,7 +6201,7 @@ class AKText extends AKAbstractObject
 					$tmp = explode(';', $value);
 					if (count($tmp) == 2)
 					{
-						if ((($value[0] != '"') && ($value[0] != "'")) ||
+						if ((($value{0} != '"') && ($value{0} != "'")) ||
 							preg_match('/^".*"\s*;/', $value) || preg_match('/^".*;[^"]*$/', $value) ||
 							preg_match("/^'.*'\s*;/", $value) || preg_match("/^'.*;[^']*$/", $value)
 						)
@@ -6211,11 +6211,11 @@ class AKText extends AKAbstractObject
 					}
 					else
 					{
-						if ($value[0] == '"')
+						if ($value{0} == '"')
 						{
 							$value = preg_replace('/^"(.*)".*/', '$1', $value);
 						}
-						elseif ($value[0] == "'")
+						elseif ($value{0} == "'")
 						{
 							$value = preg_replace("/^'(.*)'.*/", '$1', $value);
 						}
