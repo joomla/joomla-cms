5fe477b57f8591f55a1e8525e8b012127a86f699
a-samoylov <a.samoylovua@gmail.com>
2019-08-09 01:42:52 +0300

    add pagination com_config

--- api/components/com_config/View/Application/JsonapiView.php
+++ api/components/com_config/View/Application/JsonapiView.php
@@ -14,6 +14,7 @@ defined('_JEXEC') or die;
 use Joomla\CMS\Extension\ExtensionHelper;
 use Joomla\CMS\MVC\View\JsonApiView as BaseApiView;
 use Joomla\CMS\Serializer\JoomlaSerializer;
+use Joomla\CMS\Uri\Uri;
 use Tobscure\JsonApi\Collection;
 USE Joomla\Component\Config\Administrator\Model\ApplicationModel;
 
@@ -37,17 +38,58 @@ class JsonapiView extends BaseApiView
 	{
 		/** @var ApplicationModel $model */
 		$model = $this->getModel();
-		$items = array();
+		$items = [];
 
 		foreach ($model->getData() as $key => $value)
 		{
-			$item    = (object) array($key => $value);
+			$item    = (object) [$key => $value];
 			$items[] = $this->prepareItem($item);
 		}
 
+		// Set up links for pagination
+		$currentUrl = Uri::getInstance();
+		$currentPageDefaultInformation = ['offset' => 0, 'limit' => 20];
+		$currentPageQuery = $currentUrl->getVar('page', $currentPageDefaultInformation);
+
+		$offset              = $currentPageQuery['offset'];
+		$limit               = $currentPageQuery['limit'];
+		$totalItemsCount     = count($items);
+		$totalPagesAvailable = ceil($totalItemsCount / $limit);
+
+		$items = array_splice($items, $offset, $limit);
+
+		$firstPage = clone $currentUrl;
+		$firstPageQuery = $currentPageQuery;
+		$firstPageQuery['offset'] = 0;
+		$firstPage->setVar('page', $firstPageQuery);
+
+		$nextPage = clone $currentUrl;
+		$nextPageQuery = $currentPageQuery;
+		$nextOffset = $currentPageQuery['offset'] + $limit;
+		$nextPageQuery['offset'] = ($nextOffset > ($totalPagesAvailable * $limit)) ? $totalPagesAvailable - $limit : $nextOffset;
+		$nextPage->setVar('page', $nextPageQuery);
+
+		$previousPage = clone $currentUrl;
+		$previousPageQuery = $currentPageQuery;
+		$previousOffset = $currentPageQuery['offset'] - $limit;
+		$previousPageQuery['offset'] = $previousOffset >= 0 ? $previousOffset : 0;
+		$previousPage->setVar('page', $previousPageQuery);
+
+		$lastPage = clone $currentUrl;
+		$lastPageQuery = $currentPageQuery;
+		$lastPageQuery['offset'] = $totalPagesAvailable - $limit;
+		$lastPage->setVar('page', $lastPageQuery);
+
 		$collection = (new Collection($items, new JoomlaSerializer($this->type)));
 
-		$this->document->setData($collection);
+		// Set the data into the document and render it
+		$this->document->addMeta('total-pages', $totalPagesAvailable)
+			->setData($collection)
+			->addLink('self', (string) $currentUrl)
+			->addLink('first', (string) $firstPage)
+			->addLink('next', (string) $nextPage)
+			->addLink('previous', (string) $previousPage)
+			->addLink('last', (string) $lastPage);
 
 		return $this->document->render();
 	}
--- api/components/com_config/View/Component/JsonapiView.php
+++ api/components/com_config/View/Component/JsonapiView.php
@@ -15,6 +15,7 @@ use Joomla\CMS\Component\ComponentHelper;
 use Joomla\CMS\Extension\ExtensionHelper;
 use Joomla\CMS\MVC\View\JsonApiView as BaseApiView;
 use Joomla\CMS\Serializer\JoomlaSerializer;
+use Joomla\CMS\Uri\Uri;
 use Tobscure\JsonApi\Collection;
 
 /**
@@ -41,6 +42,7 @@ class JsonapiView extends BaseApiView
 
 			if ($component === null || !$component->enabled)
 			{
+				// TODO: exception component unavailable
 				throw new \RuntimeException('Invalid component name', 400);
 			}
 
@@ -51,17 +53,58 @@ class JsonapiView extends BaseApiView
 			throw new \RuntimeException('Internal server error', 500, $e);
 		}
 
-		$items = array();
+		$items = [];
 
 		foreach ($data as $key => $value)
 		{
-			$item    = (object) array($key => $value);
+			$item    = (object) [$key => $value];
 			$items[] = $this->prepareItem($item);
 		}
 
+		// Set up links for pagination
+		$currentUrl = Uri::getInstance();
+		$currentPageDefaultInformation = ['offset' => 0, 'limit' => 20];
+		$currentPageQuery = $currentUrl->getVar('page', $currentPageDefaultInformation);
+
+		$offset              = $currentPageQuery['offset'];
+		$limit               = $currentPageQuery['limit'];
+		$totalItemsCount     = count($items);
+		$totalPagesAvailable = ceil($totalItemsCount / $limit);
+
+		$items = array_splice($items, $offset, $limit);
+
+		$firstPage = clone $currentUrl;
+		$firstPageQuery = $currentPageQuery;
+		$firstPageQuery['offset'] = 0;
+		$firstPage->setVar('page', $firstPageQuery);
+
+		$nextPage = clone $currentUrl;
+		$nextPageQuery = $currentPageQuery;
+		$nextOffset = $currentPageQuery['offset'] + $limit;
+		$nextPageQuery['offset'] = ($nextOffset > ($totalPagesAvailable * $limit)) ? $totalPagesAvailable - $limit : $nextOffset;
+		$nextPage->setVar('page', $nextPageQuery);
+
+		$previousPage = clone $currentUrl;
+		$previousPageQuery = $currentPageQuery;
+		$previousOffset = $currentPageQuery['offset'] - $limit;
+		$previousPageQuery['offset'] = $previousOffset >= 0 ? $previousOffset : 0;
+		$previousPage->setVar('page', $previousPageQuery);
+
+		$lastPage = clone $currentUrl;
+		$lastPageQuery = $currentPageQuery;
+		$lastPageQuery['offset'] = $totalPagesAvailable - $limit;
+		$lastPage->setVar('page', $lastPageQuery);
+
 		$collection = (new Collection($items, new JoomlaSerializer($this->type)));
 
-		$this->document->setData($collection);
+		// Set the data into the document and render it
+		$this->document->addMeta('total-pages', $totalPagesAvailable)
+			->setData($collection)
+			->addLink('self', (string) $currentUrl)
+			->addLink('first', (string) $firstPage)
+			->addLink('next', (string) $nextPage)
+			->addLink('previous', (string) $previousPage)
+			->addLink('last', (string) $lastPage);
 
 		return $this->document->render();
 	}
