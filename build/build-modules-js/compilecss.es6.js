aa7082b48f5466ca8bb27252265ea7d8d7059631
dGrammatiko <d.grammatiko@gmail.com>
2019-08-24 21:19:36 +0200

    Update compilecss.es6.js

--- build/build-modules-js/compilecss.es6.js
+++ build/build-modules-js/compilecss.es6.js
@@ -46,7 +46,7 @@ module.exports.compile = (options, path) => {
           `${RootPath}/administrator/templates/atum/scss/template.scss`,
           `${RootPath}/administrator/templates/atum/scss/template-rtl.scss`,
           `${RootPath}/administrator/templates/atum/scss/vendor/joomla-custom-elements/joomla-alert.scss`,
-					`${RootPath}/administrator/templates/atum/scss/vendor/joomla-custom-elements/joomla-tab.scss`,
+          `${RootPath}/administrator/templates/atum/scss/vendor/joomla-custom-elements/joomla-tab.scss`,
           `${RootPath}/installation/template/scss/template.scss`,
           `${RootPath}/installation/template/scss/template-rtl.scss`,
         ];
