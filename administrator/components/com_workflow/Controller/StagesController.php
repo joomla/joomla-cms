0cecfed26fd322a13d70ae760de67798df492009
Brian Teeman <brian@teeman.net>
2019-08-30 23:48:23 +0100

    .

--- administrator/components/com_workflow/Controller/StagesController.php
+++ administrator/components/com_workflow/Controller/StagesController.php
@@ -118,7 +118,7 @@ class StagesController extends AdminController
 
 		if (!$value)
 		{
-			$this->setMessage(Text::_('COM_WORKFLOW_STAGE_DISABLE_DEFAULT'), 'warning');
+			$this->setMessage(Text::_('COM_WORKFLOW_DISABLE_DEFAULT'), 'warning');
 			$this->setRedirect(
 				Route::_(
 					'index.php?option=' . $this->option . '&view=' . $this->view_list
