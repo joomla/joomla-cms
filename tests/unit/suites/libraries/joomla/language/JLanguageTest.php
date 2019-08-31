caa62ee459e3f807fdf76c6cd4e7af60ae908bc7
Thomas Hunziker <werbemails@bakual.ch>
2019-08-31 11:46:21 +0200

    Consistent "name" value in language manifest (#26098)
    * Consistent "name" value with "nativeName"
    
    * Consistent "name" value with "nativeName"
    
    * Update JLanguageTest.php

--- tests/unit/suites/libraries/joomla/language/JLanguageTest.php
+++ tests/unit/suites/libraries/joomla/language/JLanguageTest.php
@@ -832,7 +832,7 @@ class JLanguageTest extends \PHPUnit\Framework\TestCase
 
 		// Note: property = name, returns English (en-GB) (default language)
 		$this->assertEquals(
-			'English (en-GB)',
+			'English (United Kingdom)',
 			$this->object->get('name')
 		);
 	}
@@ -845,7 +845,7 @@ class JLanguageTest extends \PHPUnit\Framework\TestCase
 	public function testGetName()
 	{
 		$this->assertEquals(
-			'English (en-GB)',
+			'English (United Kingdom)',
 			$this->object->getName()
 		);
 	}
@@ -1031,7 +1031,7 @@ class JLanguageTest extends \PHPUnit\Framework\TestCase
 		// In this case, returns array with default language
 		// - same operation of get method with metadata property
 		$options = array(
-			'name'       => 'English (en-GB)',
+			'name'       => 'English (United Kingdom)',
 			'nativeName' => 'English (United Kingdom)',
 			'tag'        => 'en-GB',
 			'rtl'        => '0',
