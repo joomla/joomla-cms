2ae8755237284fee21163fb75a5db45c61fd39df
Brian Teeman <brian@teeman.net>
2019-09-02 16:55:03 +0100

    [4.0] search stats accessibility
    An iframe is required to have a title. This PR ensures that there is a title for the popup that is loaded when you select Statistics on the smart search main page

--- administrator/components/com_finder/View/Index/HtmlView.php
+++ administrator/components/com_finder/View/Index/HtmlView.php
@@ -200,7 +200,7 @@ class HtmlView extends BaseHtmlView
 			$childBar->unpublish('index.unpublish')->listCheck(true);
 		}
 
-		$toolbar->appendButton('Popup', 'bars', 'COM_FINDER_STATISTICS', 'index.php?option=com_finder&view=statistics&tmpl=component', 550, 350);
+		$toolbar->appendButton('Popup', 'bars', 'COM_FINDER_STATISTICS', 'index.php?option=com_finder&view=statistics&tmpl=component', 550, 350,'','','','COM_FINDER_STATISTICS_TITLE');
 
 		if ($canDo->get('core.delete'))
 		{
