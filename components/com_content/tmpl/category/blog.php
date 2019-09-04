d1bf15088dd1616e65a343c23f5bad9d45191856
Scrabble96 <github@businesspluspoint.co.uk>
2019-09-04 15:16:14 +0100

    Update blog.php
    Re-aligned php tag in line 79

--- components/com_content/tmpl/category/blog.php
+++ components/com_content/tmpl/category/blog.php
@@ -76,7 +76,7 @@ $afterDisplayContent = trim(implode("\n", $results));
 			<?php foreach ($this->lead_items as &$item) : ?>
 				<div class="com-content-category-blog__item blog-item"
 					itemprop="blogPost" itemscope itemtype="https://schema.org/BlogPosting">
-												<?php
+						<?php
 						$this->item = & $item;
 						echo $this->loadTemplate('item');
 						?>
