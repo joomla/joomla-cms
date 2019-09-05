2e885731c78622acc10c45079341db23a6b77682
VladikB <vladislav.bogac@icloud.com>
2019-09-05 18:04:35 +0200

    Add timefield

---
+++ libraries/src/Form/Field/TimeField.php
@@ -0,0 +1,188 @@
+<?php
+/**
+ * Joomla! Content Management System
+ *
+ * @copyright  Copyright (C) 2005 - 2019 Open Source Matters, Inc. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+namespace Joomla\CMS\Form\Field;
+
+defined('JPATH_PLATFORM') or die;
+
+use Joomla\CMS\Component\ComponentHelper;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Form\FormField;
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Uri\Uri;
+
+/**
+ * Form Field class for the Joomla Platform.
+ * Provides a select list of integers with specified first, last and step values.
+ *
+ * @since  1.7.0
+ */
+class TimeField extends FormField
+{
+	/**
+	 * The form field type.
+	 *
+	 * @var    string
+	 * @since  1.7.0
+	 */
+	protected $type = 'Time';
+
+	/**
+	 * The allowable minimal value of the field.
+	 *
+	 * @var    integer
+	 * @since  3.2
+	 */
+	protected $min;
+
+	/**
+	 * The allowable maximal value of the field.
+	 *
+	 * @var    integer
+	 * @since  3.2
+	 */
+	protected $max;
+
+	/**
+	 * Steps between different values
+	 *
+	 * @var    integer
+	 * @since  3.2
+	 */
+	protected $step;
+
+	/**
+	 * Input addon before
+	 *
+	 * @var    string
+	 * @since  4.0.0
+	 */
+	/**
+	 * Name of the layout being used to render the field
+	 *
+	 * @var    string
+	 * @since  3.7
+	 */
+	protected $layout = 'joomla.form.field.time';
+
+	/**
+	 * Method to set certain otherwise inaccessible properties of the form field object.
+	 *
+	 * @param   string  $name   The property name for which to set the value.
+	 * @param   mixed   $value  The value of the property.
+	 *
+	 * @return  void
+	 *
+	 * @since   3.2
+	 */
+
+	public function __set($name, $value)
+	{
+		switch ($name)
+		{
+			case 'min':
+				$this->min = (string) $value;
+				break;
+
+			case 'max':
+				$this->max = (string) $value;
+				break;
+
+			case 'step':
+				$this->step = (string) $value;
+				break;
+
+			default:
+				parent::__set($name, $value);
+		}
+	}
+
+	/**
+	 * Method to attach a Form object to the field.
+	 *
+	 * @param   \SimpleXMLElement  $element  The SimpleXMLElement object representing the `<field>` tag for the form field object.
+	 * @param   mixed              $value    The form field value to validate.
+	 * @param   string             $group    The field name group control value. This acts as an array container for the field.
+	 *                                       For example if the field has name="foo" and the group value is set to "bar" then the
+	 *                                       full field name would end up being "bar[foo]".
+	 *
+	 * @return  boolean  True on success.
+	 *
+	 * @see     FormField::setup()
+	 * @since   3.2
+	 */
+	public function setup(\SimpleXMLElement $element, $value, $group = null)
+	{
+		$return = parent::setup($element, $value, $group);
+
+		if ($return)
+		{
+			// It is better not to force any default limits if none is specified
+			$this->max  = isset($this->element['max']) ? (string) $this->element['max'] : null;
+			$this->min  = isset($this->element['min']) ? (string) $this->element['min'] : null;
+			$this->step = isset($this->element['step']) ? (float) $this->element['step'] : null;
+		}
+
+		return $return;
+	}
+
+	/**
+	 * Method to get certain otherwise inaccessible properties from the form field object.
+	 *
+	 * @param   string  $name  The property name for which to get the value.
+	 *
+	 * @return  mixed  The property value or null.
+	 *
+	 * @since   3.2
+	 */
+	public function __get($name)
+	{
+		switch ($name)
+		{
+			case 'min':
+			case 'max':
+			case 'step':
+				return $this->$name;
+		}
+
+		return parent::__get($name);
+	}
+
+	/**
+	 * Method to get the field input markup.
+	 *
+	 * @return  string  The field input markup.
+	 *
+	 * @since   1.7.0
+	 */
+	protected function getInput()
+	{
+		return $this->getRenderer($this->layout)->render($this->getLayoutData());
+	}
+
+	/**
+	 * Method to get the data to be passed to the layout for rendering.
+	 *
+	 * @return  array
+	 *
+	 * @since 3.7
+	 */
+	protected function getLayoutData()
+	{
+		$data = parent::getLayoutData();
+
+		$extraData = [
+			'min'  => $this->min,
+			'max'  => $this->max,
+			'step' => $this->step,
+		];
+
+		return array_merge($data, $extraData);
+	}
+}
