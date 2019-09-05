a8dbd9a5c929bdd0e145a446d2e31c0c66ad3f9d
Lu Nguyen <lu@redweb.dk>
2019-09-05 16:14:04 +0700

    Ordering by date DESC is not working.

--- components/com_content/helpers/query.php
+++ components/com_content/helpers/query.php
@@ -173,16 +173,16 @@ class ContentHelperQuery
 		switch ($orderDate)
 		{
 			case 'modified' :
-				$queryDate = ' CASE WHEN a.modified = ' . $db->quote($db->getNullDate()) . ' THEN a.created ELSE a.modified END';
+				$queryDate = ' (CASE WHEN a.modified = ' . $db->quote($db->getNullDate()) . ' THEN a.created ELSE a.modified END) ';
 				break;
 
 			// Use created if publish_up is not set
 			case 'published' :
-				$queryDate = ' CASE WHEN a.publish_up = ' . $db->quote($db->getNullDate()) . ' THEN a.created ELSE a.publish_up END ';
+				$queryDate = ' (CASE WHEN a.publish_up = ' . $db->quote($db->getNullDate()) . ' THEN a.created ELSE a.publish_up END) ';
 				break;
 
 			case 'unpublished' :
-				$queryDate = ' CASE WHEN a.publish_down = ' . $db->quote($db->getNullDate()) . ' THEN a.created ELSE a.publish_down END ';
+				$queryDate = ' (CASE WHEN a.publish_down = ' . $db->quote($db->getNullDate()) . ' THEN a.created ELSE a.publish_down END) ';
 				break;
 			case 'created' :
 			default :
