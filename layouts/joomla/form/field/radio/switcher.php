69a4335c75537a5b8e1df7486af5c598b36cc522
SharkyKZ <sharkykz@gmail.com>
2019-09-08 16:17:11 +0300

    Cleanup

--- layouts/joomla/form/field/radio/switcher.php
+++ layouts/joomla/form/field/radio/switcher.php
@@ -59,7 +59,7 @@ Factory::getApplication()->getDocument()->getWebAssetManager()->enableAsset('swi
  *     %3 - value
  *     %4 = any other attributes
  */
-$input    = '<input type="radio" id="%1$s" name="%2$s" value="%3$s" %4$s>';
+$input = '<input type="radio" id="%1$s" name="%2$s" value="%3$s" %4$s>';
 
 ?>
 <fieldset>
