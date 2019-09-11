6355b5dda4c6b81a001bb2e3791b820deb6abebe
Troy Hall <programming@hallhome.us>
2019-09-11 06:11:56 -0500

    redo due to #26237

--- components/com_content/tmpl/featured/default.php
+++ components/com_content/tmpl/featured/default.php
@@ -32,12 +32,10 @@ defined('_JEXEC') or die;
 			<?php foreach ($this->lead_items as &$item) : ?>
 				<div class="blog-item"
 					itemprop="blogPost" itemscope itemtype="https://schema.org/BlogPosting">
-					<div class="blog-item-content"><!-- Double divs required for IE11 grid fallback -->
 						<?php
 						$this->item = & $item;
 						echo $this->loadTemplate('item');
 						?>
-					</div>
 				</div>
 				<?php $leadingcount++; ?>
 			<?php endforeach; ?>
@@ -49,12 +47,10 @@ defined('_JEXEC') or die;
 		<?php foreach ($this->intro_items as $key => &$item) : ?>
 			<div class="blog-item"
 				itemprop="blogPost" itemscope itemtype="https://schema.org/BlogPosting">
-				<div class="blog-item-content"><!-- Double divs required for IE11 grid fallback -->
 					<?php
 					$this->item = & $item;
 					echo $this->loadTemplate('item');
 					?>
-				</div>
 			</div>
 		<?php endforeach; ?>
 		</div>
