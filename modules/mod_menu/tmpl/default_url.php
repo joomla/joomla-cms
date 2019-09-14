306183132e93fb783088e05389c9550c791bfec9
Troy Hall <programming@hallhome.us>
2019-09-14 12:22:23 -0500

    Revert "Delete default_url.php"
    This reverts commit b2826991

---
+++ modules/mod_menu/tmpl/default_url.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  mod_menu
+ *
+ * @copyright   Copyright (C) 2005 - 2019 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+$attributes = array();
+
+if ($item->anchor_title)
+{
+	$attributes['title'] = $item->anchor_title;
+}
+
+if ($item->anchor_css)
+{
+	$attributes['class'] = $item->anchor_css;
+}
+
+if ($item->anchor_rel)
+{
+	$attributes['rel'] = $item->anchor_rel;
+}
+
+$linktype = $item->title;
+
+if ($item->menu_image)
+{
+	if ($item->menu_image_css)
+	{
+		$image_attributes['class'] = $item->menu_image_css;
+		$linktype                  = JHtml::_('image', $item->menu_image, $item->title, $image_attributes);
+	}
+	else
+	{
+		$linktype = JHtml::_('image', $item->menu_image, $item->title);
+	}
+
+	if ($item->params->get('menu_text', 1))
+	{
+		$linktype .= '<span class="image-title">' . $item->title . '</span>';
+	}
+}
+
+if ($item->browserNav == 1)
+{
+	$attributes['target'] = '_blank';
+	$attributes['rel']    = 'noopener noreferrer';
+
+	if ($item->anchor_rel == 'nofollow')
+	{
+		$attributes['rel'] .= ' nofollow';
+	}
+}
+elseif ($item->browserNav == 2)
+{
+	$options = 'toolbar=no,location=no,status=no,menubar=no,scrollbars=yes,resizable=yes,' . $params->get('window_open');
+
+	$attributes['onclick'] = "window.open(this.href, 'targetWindow', '" . $options . "'); return false;";
+}
+
+if ($active_id == $item->id || $active_id == $item->params['aliasoptions'])
+{
+	$attributes['aria-current'] = "page";
+}
+
+echo JHtml::_('link', JFilterOutput::ampReplace(htmlspecialchars($item->flink, ENT_COMPAT, 'UTF-8', false)), $linktype, $attributes);
