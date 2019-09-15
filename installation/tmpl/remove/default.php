0beac2c50e1fcdb2babe048126fe830a5852b583
Lukas Schmitt <lukas.schmitt-2@mni.thm.de>
2019-09-15 22:22:15 +0200

    Update installation/tmpl/remove/default.php
    Co-Authored-By: Richard Fath <richard67@users.noreply.github.com>

--- installation/tmpl/remove/default.php
+++ installation/tmpl/remove/default.php
@@ -85,7 +85,7 @@ use Joomla\CMS\Uri\Uri;
 				<?php endif; ?>
 				<?php if ($this->development) : ?>
 					<div class="alert flex-column mb-1" id="removeInstallationTab">
-						<span class="mb-1 font-weight-bold"><?php echo Text::_('INSTL_SITE_DEVMODE_LABEL'); ?></span>
+					<span class="mb-1 font-weight-bold"><?php echo Text::_('INSTL_SITE_DEVMODE_LABEL'); ?></span>
                         <button class="btn btn-danger mb-1" id="removeInstallationFolder">Remove installation folder</button>
 					</div>
 				<?php endif; ?>
