e8ed84ff4d24cde1878620af64aebb4c85f16e0d
Lukas Schmitt <lukas.schmitt-2@mni.thm.de>
2019-09-15 15:22:29 +0200

    removed unused button

--- installation/tmpl/remove/default.php
+++ installation/tmpl/remove/default.php
@@ -88,7 +88,6 @@ use Joomla\CMS\Uri\Uri;
 						<span class="mb-1 font-weight-bold"><?php echo Text::_('INSTL_SITE_DEVMODE_LABEL'); ?></span>
                         <button class="btn btn-danger mb-1" id="removeInstallationFolder">Remove installation folder</button>
 					</div>
-					<!-- <input type="button" class="btn btn-warning" name="instDefault" onclick="Install.removeFolder(this);" value="<?php echo Text::_('INSTL_COMPLETE_REMOVE_FOLDER'); ?>"> -->
 				<?php endif; ?>
 				<?php echo HTMLHelper::_('form.token'); ?>
 
