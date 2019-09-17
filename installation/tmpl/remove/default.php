50eb04035f3caf3958997eb56dcbb02bd19c3fe9
Lukas Schmitt <lukas.schmitt-2@mni.thm.de>
2019-09-17 19:02:24 +0200

    code styling

--- installation/tmpl/remove/default.php
+++ installation/tmpl/remove/default.php
@@ -85,7 +85,7 @@ use Joomla\CMS\Uri\Uri;
 				<?php endif; ?>
 				<?php if ($this->development) : ?>
 					<div class="alert flex-column mb-1" id="removeInstallationTab">
-					<span class="mb-1 font-weight-bold"><?php echo Text::_('INSTL_SITE_DEVMODE_LABEL'); ?></span>
+						<span class="mb-1 font-weight-bold"><?php echo Text::_('INSTL_SITE_DEVMODE_LABEL'); ?></span>
 						<button class="btn btn-danger mb-1" id="removeInstallationFolder"><?php echo Text::_('INSTL_SITE_REMOVE_BUTTON'); ?></button>
 					</div>
 				<?php endif; ?>
