89a2b324a201de01d141baa08f80ec2b9c4bd815
Lukas Schmitt <lukas.schmitt-2@mni.thm.de>
2019-09-17 16:54:04 +0200

    added language string for "remove installation" button

--- installation/template/index.php
+++ installation/template/index.php
@@ -35,6 +35,7 @@ $this->addScriptOptions('system.installation', ['url' => Route::_('index.php')])
 // Load JavaScript message titles
 Text::script('ERROR');
 Text::script('INSTL_REMOVE_INST_FOLDER');
+Text::script('INSTL_SITE_REMOVE_BUTTON');
 Text::script('WARNING');
 Text::script('NOTICE');
 Text::script('MESSAGE');
