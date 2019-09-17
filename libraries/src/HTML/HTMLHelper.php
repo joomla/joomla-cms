a887369afa561bae6fcce6fd0d5cfb7109908286
Bear <programming@hallhome.us>
2019-09-16 23:01:05 -0500

    Update libraries/src/HTML/HTMLHelper.php
    yep, skipped that part ty.
    
    Co-Authored-By: Quy <quy@fluxbb.org>

--- libraries/src/HTML/HTMLHelper.php
+++ libraries/src/HTML/HTMLHelper.php
@@ -12,7 +12,6 @@ defined('JPATH_PLATFORM') or die;
 
 use Joomla\CMS\Environment\Browser;
 use Joomla\CMS\Factory;
-use Joomla\CMS\Filesystem\File;
 use Joomla\CMS\Layout\LayoutHelper;
 use Joomla\CMS\Log\Log;
 use Joomla\CMS\Uri\Uri;
