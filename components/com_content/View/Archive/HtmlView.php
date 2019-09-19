2a97bf7db8981671dac9563c3312ad21bec87b70
SharkyKZ <sharkykz@gmail.com>
2019-09-19 10:36:15 +0300

    Throw exception

--- components/com_content/View/Archive/HtmlView.php
+++ components/com_content/View/Archive/HtmlView.php
@@ -14,6 +14,7 @@ defined('_JEXEC') or die;
 use Joomla\CMS\Factory;
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
+use Joomla\CMS\MVC\View\GenericDataException;
 use Joomla\CMS\MVC\View\HtmlView as BaseHtmlView;
 use Joomla\CMS\Plugin\PluginHelper;
 
@@ -107,6 +108,11 @@ class HtmlView extends BaseHtmlView
 		$state      = $this->get('State');
 		$items      = $this->get('Items');
 		$pagination = $this->get('Pagination');
+
+		if ($errors = $this->getModel()->getErrors())
+		{
+			throw new GenericDataException(implode("\n", $errors), 500);
+		}
 
 		// Flag indicates to not add limitstart=0 to URL
 		$pagination->hideEmptyLimitstart = true;
