172c18a9d8defcf00a728b862dd8208e4114f52b
Richard Fath <richard67@users.noreply.github.com>
2019-09-22 17:13:22 +0200

    Contacts not nullable datetimes defaults - SQL changes

--- administrator/components/com_admin/sql/updates/mysql/4.0.0-2019-04-22.sql
+++ administrator/components/com_admin/sql/updates/mysql/4.0.0-2019-04-22.sql
@@ -1,6 +1,12 @@
+ALTER TABLE `#__contact_details` MODIFY `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP;
+ALTER TABLE `#__contact_details` MODIFY `modified` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP;
+
 ALTER TABLE `#__contact_details` MODIFY `publish_up` datetime NULL DEFAULT NULL;
 ALTER TABLE `#__contact_details` MODIFY `publish_down` datetime NULL DEFAULT NULL;
 ALTER TABLE `#__contact_details` MODIFY `checked_out_time` datetime NULL DEFAULT NULL;
+
+UPDATE `#__contact_details` SET `created` = '2005-08-17 00:00:00' WHERE `created` = '0000-00-00 00:00:00';
+UPDATE `#__contact_details` SET `modified` = `created` WHERE `modified` = '0000-00-00 00:00:00';
 
 UPDATE `#__contact_details` SET
 	`publish_up` = CASE WHEN `publish_up` IN ('0000-00-00 00:00:00', '1000-01-01 00:00:00') THEN NULL ELSE `publish_up` END,
--- administrator/components/com_admin/sql/updates/postgresql/4.0.0-2019-04-22.sql
+++ administrator/components/com_admin/sql/updates/postgresql/4.0.0-2019-04-22.sql
@@ -1,3 +1,9 @@
+ALTER TABLE "#__contact_details" ALTER COLUMN "created" SET DEFAULT CURRENT_TIMESTAMP;
+ALTER TABLE "#__contact_details" ALTER COLUMN "created" SET NOT NULL;
+
+ALTER TABLE "#__contact_details" ALTER COLUMN "modified" SET DEFAULT CURRENT_TIMESTAMP;
+ALTER TABLE "#__contact_details" ALTER COLUMN "modified" SET NOT NULL;
+
 ALTER TABLE "#__contact_details" ALTER COLUMN "publish_up" DROP NOT NULL;
 ALTER TABLE "#__contact_details" ALTER COLUMN "publish_up" DROP DEFAULT;
 
@@ -6,6 +12,9 @@ ALTER TABLE "#__contact_details" ALTER COLUMN "publish_down" DROP DEFAULT;
 
 ALTER TABLE "#__contact_details" ALTER COLUMN "checked_out_time" DROP NOT NULL;
 ALTER TABLE "#__contact_details" ALTER COLUMN "checked_out_time" DROP DEFAULT;
+
+UPDATE "#__contact_details" SET "created" = '2005-08-17 00:00:00' WHERE "created" = '1970-01-01 00:00:00';
+UPDATE "#__contact_details" SET "modified" = "created" WHERE "modified" = '1970-01-01 00:00:00';
 
 UPDATE "#__contact_details" SET
 	"publish_up" = CASE WHEN "publish_up" = '1970-01-01 00:00:00' THEN NULL ELSE "publish_up" END,
