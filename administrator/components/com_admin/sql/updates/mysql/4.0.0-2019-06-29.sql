f852f6bd197bfbb17cf9b6e8e500e580d4cee4dc
Richard Fath <richard67@users.noreply.github.com>
2019-09-22 20:05:01 +0200

    Newsfeeds not nullable datetimes defaults - SQL changes

--- administrator/components/com_admin/sql/updates/mysql/4.0.0-2019-06-29.sql
+++ administrator/components/com_admin/sql/updates/mysql/4.0.0-2019-06-29.sql
@@ -1,6 +1,12 @@
+ALTER TABLE `#__newsfeeds` MODIFY `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP;
+ALTER TABLE `#__newsfeeds` MODIFY `modified` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP;
+
 ALTER TABLE `#__newsfeeds` MODIFY `publish_up` datetime NULL DEFAULT NULL;
 ALTER TABLE `#__newsfeeds` MODIFY `publish_down` datetime NULL DEFAULT NULL;
 ALTER TABLE `#__newsfeeds` MODIFY `checked_out_time` datetime NULL DEFAULT NULL;
+
+UPDATE `#__newsfeeds` SET `created` = '2005-08-17 00:00:00' WHERE `created` = '0000-00-00 00:00:00';
+UPDATE `#__newsfeeds` SET `modified` = `created` WHERE `modified` = '0000-00-00 00:00:00';
 
 UPDATE `#__newsfeeds` SET
 	`publish_up` = CASE WHEN `publish_up` IN ('0000-00-00 00:00:00', '1000-01-01 00:00:00') THEN NULL ELSE `publish_up` END,
--- administrator/components/com_admin/sql/updates/postgresql/4.0.0-2019-06-29.sql
+++ administrator/components/com_admin/sql/updates/postgresql/4.0.0-2019-06-29.sql
@@ -1,3 +1,9 @@
+ALTER TABLE "#__newsfeeds" ALTER COLUMN "created" SET DEFAULT CURRENT_TIMESTAMP;
+ALTER TABLE "#__newsfeeds" ALTER COLUMN "created" SET NOT NULL;
+
+ALTER TABLE "#__newsfeeds" ALTER COLUMN "modified" SET DEFAULT CURRENT_TIMESTAMP;
+ALTER TABLE "#__newsfeeds" ALTER COLUMN "modified" SET NOT NULL;
+
 ALTER TABLE "#__newsfeeds" ALTER COLUMN "publish_up" DROP NOT NULL;
 ALTER TABLE "#__newsfeeds" ALTER COLUMN "publish_up" DROP DEFAULT;
 
@@ -6,6 +12,9 @@ ALTER TABLE "#__newsfeeds" ALTER COLUMN "publish_down" DROP DEFAULT;
 
 ALTER TABLE "#__newsfeeds" ALTER COLUMN "checked_out_time" DROP NOT NULL;
 ALTER TABLE "#__newsfeeds" ALTER COLUMN "checked_out_time" DROP DEFAULT;
+
+UPDATE "#__newsfeeds" SET "created" = '2005-08-17 00:00:00' WHERE "created" = '1970-01-01 00:00:00';
+UPDATE "#__newsfeeds" SET "modified" = "created" WHERE "modified" = '1970-01-01 00:00:00';
 
 UPDATE "#__newsfeeds" SET
 	"publish_up" = CASE WHEN "publish_up" = '1970-01-01 00:00:00' THEN NULL ELSE "publish_up" END,
