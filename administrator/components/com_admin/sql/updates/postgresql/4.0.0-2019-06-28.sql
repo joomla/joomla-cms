81528b114cf827b04824755b2c7926d48c70d53a
Richard Fath <richard67@users.noreply.github.com>
2019-09-27 14:01:14 +0200

    Handle UCM content

--- administrator/components/com_admin/sql/updates/postgresql/4.0.0-2019-06-28.sql
+++ administrator/components/com_admin/sql/updates/postgresql/4.0.0-2019-06-28.sql
@@ -29,3 +29,20 @@ UPDATE "#__banners" SET "reset" = NULL WHERE "reset" = '1970-01-01 00:00:00';
 	"checked_out_time" = CASE WHEN "checked_out_time" = '1970-01-01 00:00:00' THEN NULL ELSE "checked_out_time" END;
 
 UPDATE "#__banner_clients" SET "checked_out_time" = NULL WHERE "checked_out_time" = '1970-01-01 00:00:00';
+
+UPDATE "#__ucm_content" SET "core_created_time" = '2005-08-17 00:00:00'
+ WHERE "core_type_alias" = 'com_banners.banner'
+   AND "core_created_time" = '1970-01-01 00:00:00';
+UPDATE "#__ucm_content" SET "core_modified_time" = '2005-08-17 00:00:00'
+ WHERE "core_type_alias" = 'com_banners.banner'
+   AND ("core_modified_time" = '1970-01-01 00:00:00' OR "core_modified_time" < "core_created_time");
+
+UPDATE "#__ucm_content" SET "core_publish_up" = NULL
+ WHERE "core_type_alias" = 'com_banners.banner'
+   AND "core_publish_up" = '1970-01-01 00:00:00';
+UPDATE "#__ucm_content" SET "core_publish_down" = NULL
+ WHERE "core_type_alias" = 'com_banners.banner'
+   AND "core_publish_down" = '1970-01-01 00:00:00';
+UPDATE "#__ucm_content" SET "core_checked_out_time" = NULL
+ WHERE "core_type_alias" = 'com_banners.banner'
+   AND "core_checked_out_time" = '1970-01-01 00:00:00';
