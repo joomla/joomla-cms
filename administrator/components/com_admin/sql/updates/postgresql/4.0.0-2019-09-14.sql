a6203d217e71d118095256a7639df203ca63ce31
Richard Fath <richard67@users.noreply.github.com>
2019-09-27 12:55:26 +0200

    SQL for ucm content

--- administrator/components/com_admin/sql/updates/postgresql/4.0.0-2019-09-14.sql
+++ administrator/components/com_admin/sql/updates/postgresql/4.0.0-2019-09-14.sql
@@ -10,3 +10,13 @@ ALTER TABLE "#__content" ALTER COLUMN "checked_out_time" DROP DEFAULT;
 UPDATE "#__content" SET "publish_up" =  NULL WHERE "publish_up" = '1970-01-01 00:00:00';
 UPDATE "#__content" SET "publish_down" =  NULL WHERE "publish_down" = '1970-01-01 00:00:00';
 UPDATE "#__content" SET "checked_out_time" =  NULL WHERE "checked_out_time" = '1970-01-01 00:00:00';
+
+UPDATE "#__ucm_content" SET "core_publish_up" = NULL
+ WHERE "core_type_alias" = 'com_content.article'
+   AND "core_publish_up" = '1970-01-01 00:00:00';
+UPDATE "#__ucm_content" SET "core_publish_down" = NULL
+ WHERE "core_type_alias" = 'com_content.article'
+   AND "core_publish_down" = '1970-01-01 00:00:00';
+UPDATE "#__ucm_content" SET "core_checked_out_time" = NULL
+ WHERE "core_type_alias" = 'com_content.article'
+   AND "core_checked_out_time" = '1970-01-01 00:00:00';
