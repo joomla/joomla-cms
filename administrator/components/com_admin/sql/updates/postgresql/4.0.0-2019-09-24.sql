e4b6b808add034d7c27b025929e962d7e5f60beb
Richard Fath <richard67@users.noreply.github.com>
2019-09-28 16:10:23 +0200

    No defaults for not nullable, minimize updating old data

--- administrator/components/com_admin/sql/updates/postgresql/4.0.0-2019-09-24.sql
+++ administrator/components/com_admin/sql/updates/postgresql/4.0.0-2019-09-24.sql
@@ -1,4 +1,1 @@
-ALTER TABLE "#__action_logs" ALTER COLUMN "log_date" SET DEFAULT CURRENT_TIMESTAMP;
-ALTER TABLE "#__action_logs" ALTER COLUMN "log_date" SET NOT NULL;
-
-UPDATE "#__action_logs" SET "log_date" = '2005-08-17 00:00:00' WHERE "created_date" = '1970-01-01 00:00:00';
+ALTER TABLE "#__action_logs" ALTER COLUMN "log_date" DROP DEFAULT;
