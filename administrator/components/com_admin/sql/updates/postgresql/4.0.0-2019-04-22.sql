665f3321f1f62035e3c52585c1ebffc26b1e4b71
Richard Fath <richard67@users.noreply.github.com>
2019-09-29 00:43:42 +0200

    Update modified also on postgresql

--- administrator/components/com_admin/sql/updates/postgresql/4.0.0-2019-04-22.sql
+++ administrator/components/com_admin/sql/updates/postgresql/4.0.0-2019-04-22.sql
@@ -11,10 +11,16 @@ ALTER TABLE "#__contact_details" ALTER COLUMN "publish_down" DROP DEFAULT;
 ALTER TABLE "#__contact_details" ALTER COLUMN "checked_out_time" DROP NOT NULL;
 ALTER TABLE "#__contact_details" ALTER COLUMN "checked_out_time" DROP DEFAULT;
 
+UPDATE "#__contact_details" SET "modified" = "created" WHERE "modified" = '1970-01-01 00:00:00';
+
 UPDATE "#__contact_details" SET
 	"publish_up" = CASE WHEN "publish_up" = '1970-01-01 00:00:00' THEN NULL ELSE "publish_up" END,
 	"publish_down" = CASE WHEN "publish_down" = '1970-01-01 00:00:00' THEN NULL ELSE "publish_down" END,
 	"checked_out_time" = CASE WHEN "checked_out_time" = '1970-01-01 00:00:00' THEN NULL ELSE "checked_out_time" END;
+
+UPDATE "#__ucm_content" SET "core_modified_time" = "core_created_time"
+ WHERE "core_type_alias" = 'com_contact.contact'
+   AND "core_modified_time" = '1970-01-01 00:00:00';
 
 UPDATE "#__ucm_content" SET "core_publish_up" = NULL
  WHERE "core_type_alias" = 'com_contact.contact'
