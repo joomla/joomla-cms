4f6a444433e2e2140b36789a19221684b6235b2c
Richard Fath <richard67@users.noreply.github.com>
2019-09-28 15:09:49 +0200

    No defaults for not nullable, minimize updating old data, handle ucm content

--- administrator/components/com_admin/sql/updates/mysql/4.0.0-2019-04-22.sql
+++ administrator/components/com_admin/sql/updates/mysql/4.0.0-2019-04-22.sql
@@ -1,14 +1,27 @@
-ALTER TABLE `#__contact_details` MODIFY `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP;
-ALTER TABLE `#__contact_details` MODIFY `modified` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP;
+ALTER TABLE `#__contact_details` MODIFY `created` datetime NOT NULL;
+ALTER TABLE `#__contact_details` MODIFY `modified` datetime NOT NULL;
 
 ALTER TABLE `#__contact_details` MODIFY `publish_up` datetime NULL DEFAULT NULL;
 ALTER TABLE `#__contact_details` MODIFY `publish_down` datetime NULL DEFAULT NULL;
 ALTER TABLE `#__contact_details` MODIFY `checked_out_time` datetime NULL DEFAULT NULL;
 
-UPDATE `#__contact_details` SET `created` = '2005-08-17 00:00:00' WHERE `created` = '0000-00-00 00:00:00';
 UPDATE `#__contact_details` SET `modified` = `created` WHERE `modified` = '0000-00-00 00:00:00';
 
 UPDATE `#__contact_details` SET
 	`publish_up` = CASE WHEN `publish_up` IN ('0000-00-00 00:00:00', '1000-01-01 00:00:00') THEN NULL ELSE `publish_up` END,
 	`publish_down` = CASE WHEN `publish_down` IN ('0000-00-00 00:00:00', '1000-01-01 00:00:00') THEN NULL ELSE `publish_down` END,
 	`checked_out_time` = CASE WHEN `checked_out_time` IN ('0000-00-00 00:00:00', '1000-01-01 00:00:00') THEN NULL ELSE `checked_out_time` END;
+
+UPDATE `#__ucm_content` SET `core_modified_time` = `core_created_time`
+ WHERE `core_type_alias` = 'com_contact.contact'
+   AND `core_modified_time` = '0000-00-00 00:00:00';
+
+UPDATE `#__ucm_content` SET `core_publish_up` = NULL
+ WHERE `core_type_alias` = 'com_contact.contact'
+   AND `core_publish_up` = '0000-00-00 00:00:00';
+UPDATE `#__ucm_content` SET `core_publish_down` = NULL
+ WHERE `core_type_alias` = 'com_contact.contact'
+   AND `core_publish_down` = '0000-00-00 00:00:00';
+UPDATE `#__ucm_content` SET `core_checked_out_time` = NULL
+ WHERE `core_type_alias` = 'com_contact.contact'
+   AND `core_checked_out_time` = '0000-00-00 00:00:00';
--- administrator/components/com_admin/sql/updates/postgresql/4.0.0-2019-04-22.sql
+++ administrator/components/com_admin/sql/updates/postgresql/4.0.0-2019-04-22.sql
@@ -1,8 +1,6 @@
-ALTER TABLE "#__contact_details" ALTER COLUMN "created" SET DEFAULT CURRENT_TIMESTAMP;
-ALTER TABLE "#__contact_details" ALTER COLUMN "created" SET NOT NULL;
+ALTER TABLE "#__contact_details" ALTER COLUMN "created" DROP DEFAULT;
 
-ALTER TABLE "#__contact_details" ALTER COLUMN "modified" SET DEFAULT CURRENT_TIMESTAMP;
-ALTER TABLE "#__contact_details" ALTER COLUMN "modified" SET NOT NULL;
+ALTER TABLE "#__contact_details" ALTER COLUMN "modified" DROP DEFAULT;
 
 ALTER TABLE "#__contact_details" ALTER COLUMN "publish_up" DROP NOT NULL;
 ALTER TABLE "#__contact_details" ALTER COLUMN "publish_up" DROP DEFAULT;
@@ -13,10 +11,17 @@ ALTER TABLE "#__contact_details" ALTER COLUMN "publish_down" DROP DEFAULT;
 ALTER TABLE "#__contact_details" ALTER COLUMN "checked_out_time" DROP NOT NULL;
 ALTER TABLE "#__contact_details" ALTER COLUMN "checked_out_time" DROP DEFAULT;
 
-UPDATE "#__contact_details" SET "created" = '2005-08-17 00:00:00' WHERE "created" = '1970-01-01 00:00:00';
-UPDATE "#__contact_details" SET "modified" = "created" WHERE "modified" = '1970-01-01 00:00:00';
-
 UPDATE "#__contact_details" SET
 	"publish_up" = CASE WHEN "publish_up" = '1970-01-01 00:00:00' THEN NULL ELSE "publish_up" END,
 	"publish_down" = CASE WHEN "publish_down" = '1970-01-01 00:00:00' THEN NULL ELSE "publish_down" END,
 	"checked_out_time" = CASE WHEN "checked_out_time" = '1970-01-01 00:00:00' THEN NULL ELSE "checked_out_time" END;
+
+UPDATE "#__ucm_content" SET "core_publish_up" = NULL
+ WHERE "core_type_alias" = 'com_contact.contact'
+   AND "core_publish_up" = '1970-01-01 00:00:00';
+UPDATE "#__ucm_content" SET "core_publish_down" = NULL
+ WHERE "core_type_alias" = 'com_contact.contact'
+   AND "core_publish_down" = '1970-01-01 00:00:00';
+UPDATE "#__ucm_content" SET "core_checked_out_time" = NULL
+ WHERE "core_type_alias" = 'com_contact.contact'
+   AND "core_checked_out_time" = '1970-01-01 00:00:00';
