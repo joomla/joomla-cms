67a513925467dd3976e6d984cc9d19ab3cda0d26
Richard Fath <richard67@users.noreply.github.com>
2019-10-03 19:42:28 +0200

    Update old records

--- administrator/components/com_admin/sql/updates/mysql/4.0.0-2019-09-26.sql
+++ administrator/components/com_admin/sql/updates/mysql/4.0.0-2019-09-26.sql
@@ -2,3 +2,5 @@ ALTER TABLE `#__privacy_requests` MODIFY `requested_at` datetime NOT NULL;
 ALTER TABLE `#__privacy_requests` MODIFY `confirm_token_created_at` datetime NOT NULL;
 
 ALTER TABLE `#__privacy_consents` MODIFY `created` datetime NOT NULL;
+
+UPDATE `#__privacy_requests` SET `confirm_token_created_at` = `requested_at` WHERE `confirm_token_created_at` = '0000-00-00 00:00:00';
--- administrator/components/com_admin/sql/updates/postgresql/4.0.0-2019-09-26.sql
+++ administrator/components/com_admin/sql/updates/postgresql/4.0.0-2019-09-26.sql
@@ -2,3 +2,6 @@ ALTER TABLE "#__privacy_requests" ALTER COLUMN "requested_at" DROP DEFAULT;
 ALTER TABLE "#__privacy_requests" ALTER COLUMN "confirm_token_created_at" DROP DEFAULT;
 
 ALTER TABLE "#__privacy_consents" ALTER COLUMN "created" DROP DEFAULT;
+
+UPDATE "#__privacy_requests" SET "confirm_token_created_at" = "requested_at" WHERE "confirm_token_created_at" = '0000-00-00 00:00:00';
+
