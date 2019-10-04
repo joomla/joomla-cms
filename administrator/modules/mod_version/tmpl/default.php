bfeb8ecdcb7bd67493b54e1497ac55f872e0df47
astridx <info@astrid-guenther.de>
2019-10-04 19:43:18 +0200

    correciton for small mobil and debug

--- administrator/modules/mod_version/tmpl/default.php
+++ administrator/modules/mod_version/tmpl/default.php
@@ -13,8 +13,8 @@ use Joomla\CMS\Language\Text;
 
 ?>
 <div class="col-md-12">
-	<div class="float-right">
+	<div class="float-right mb-5 pb-5 mb-sm-0 pb-sm-0">
 		<span class="sr-only"><?php echo Text::sprintf('MOD_VERSION_CURRENT_VERSION_TEXT', $version); ?></span>
-		<span aria-hidden="true"><?php echo Text::sprintf('MOD_VERSION_CURRENT_VERSION_SHORTTEXT', $version); ?></span>
+		<span class="mb-5 pb-5 mb-sm-0 pb-sm-4" aria-hidden="true"><?php echo Text::sprintf('MOD_VERSION_CURRENT_VERSION_SHORTTEXT', $version); ?></span>
 	</div>
 </div>
