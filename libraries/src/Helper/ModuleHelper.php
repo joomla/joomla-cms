a1651fdc09c487d2e235e03b8ca895520f7d532f
Benjamin Trenkle <bembelimen@users.noreply.github.com>
2019-10-09 13:17:19 +0200

    Allow int value as ID comparison for module loader

--- libraries/src/Helper/ModuleHelper.php
+++ libraries/src/Helper/ModuleHelper.php
@@ -665,7 +665,7 @@ abstract class ModuleHelper
 		for ($i = 0; $i < $total; $i++)
 		{
 			// Match the id of the module
-			if ($modules[$i]->id === $id)
+			if ((int) $modules[$i]->id === (int) $id)
 			{
 				// Found it
 				return $modules[$i];
