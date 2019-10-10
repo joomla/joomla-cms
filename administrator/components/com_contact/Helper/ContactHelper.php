aabf13da09c3773a8b1480fcf677a298900a21eb
Nicola Galgano <optimus4joomla@gmail.com>
2019-10-10 19:36:12 +0200

    no need of sidebar

--- administrator/components/com_contact/Helper/ContactHelper.php
+++ administrator/components/com_contact/Helper/ContactHelper.php
@@ -22,29 +22,5 @@ use Joomla\CMS\Language\Text;
  */
 class ContactHelper extends ContentHelper
 {
-	/**
-	 * Configure the Linkbar.
-	 *
-	 * @param   string  $vName  The name of the active view.
-	 *
-	 * @return  void
-	 *
-	 * @since   1.6
-	 */
-	public static function addSubmenu($vName)
-	{
-		if (ComponentHelper::isEnabled('com_fields') && ComponentHelper::getParams('com_contact')->get('custom_fields_enable', '1'))
-		{
-			\JHtmlSidebar::addEntry(
-				Text::_('JGLOBAL_FIELDS'),
-				'index.php?option=com_fields&context=com_contact.contact',
-				$vName == 'fields.fields'
-			);
-			\JHtmlSidebar::addEntry(
-				Text::_('JGLOBAL_FIELD_GROUPS'),
-				'index.php?option=com_fields&view=groups&context=com_contact.contact',
-				$vName == 'fields.groups'
-			);
-		}
-	}
+	
 }
