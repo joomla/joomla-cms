fac281e1ccfe05c205c38e273d0dec10024f5803
Richard Fath <richard67@users.noreply.github.com>
2019-10-13 14:46:45 +0200

    Update TagsPopularHelper.php

--- modules/mod_tags_popular/Helper/TagsPopularHelper.php
+++ modules/mod_tags_popular/Helper/TagsPopularHelper.php
@@ -99,11 +99,11 @@ abstract class TagsPopularHelper
 		$query->where($db->quoteName('c.core_state') . ' = 1')
 			->whereIn($db->quoteName('c.core_access'), $groups)
 			->where('(' . $db->quoteName('c.core_publish_up') . ' IS NULL'
-				. ' OR ' . $db->quoteName('c.core_publish_up') . ' = :nullDate2)'
+				. ' OR ' . $db->quoteName('c.core_publish_up') . ' = :nullDate2'
 				. ' OR ' . $db->quoteName('c.core_publish_up') . ' <= :nowDate2)'
 			)
 			->where('(' . $db->quoteName('c.core_publish_down') . ' IS NULL'
-				. ' OR ' . $db->quoteName('c.core_publish_down') . ' = :nullDate3)'
+				. ' OR ' . $db->quoteName('c.core_publish_down') . ' = :nullDate3'
 				. ' OR ' . $db->quoteName('c.core_publish_down') . ' >= :nowDate3)'
 			)
 			->bind([':nullDate2', ':nullDate3'], $nullDate)
