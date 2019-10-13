5acdf7195f541dbc348ac46a2070f61a8528f249
dgrammatiko <d.grammatiko@gmail.com>
2019-10-13 17:59:49 +0200

    meh…

--- build/media_source/com_actiologs/js/admin-actionlogs-default.es6.js
+++ build/media_source/com_actiologs/js/admin-actionlogs-default.es6.js
@@ -2,14 +2,16 @@
  * @copyright   Copyright (C) 2005 - 2019 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
-Joomla.submitbutton = function(task) {
-  if (task === "actionlogs.exportLogs") {
-    Joomla.submitform(task, document.getElementById("exportForm"));
+Joomla.submitbutton = (task) => {
+  'use strict';
+
+  if (task === 'actionlogs.exportLogs') {
+    Joomla.submitform(task, document.getElementById('exportForm'));
 
     return;
   }
 
-  if (task === "actionlogs.exportSelectedLogs") {
+  if (task === 'actionlogs.exportSelectedLogs') {
     // Get id of selected action logs item and pass it to export form hidden input
     const cids = [];
     const elements = [].slice.call(document.querySelectorAll("input[name='cid[]']:checked"));
@@ -17,11 +19,11 @@ Joomla.submitbutton = function(task) {
     if (elements.length) {
       elements.forEach((element) => {
         cids.push(element.value);
-      })
+      });
     }
 
-    document.exportForm.cids.value = cids.join(",");
-    Joomla.submitform(task, document.getElementById("exportForm"));
+    document.exportForm.cids.value = cids.join(',');
+    Joomla.submitform(task, document.getElementById('exportForm'));
 
     return;
   }
