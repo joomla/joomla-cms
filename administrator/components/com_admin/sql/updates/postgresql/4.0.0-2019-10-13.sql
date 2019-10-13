2bb8c9c61aa3fc2c0f231f35e9c7a24b84cbea06
Richard Fath <richard67@users.noreply.github.com>
2019-10-13 16:03:10 +0200

    Fix UCM field mapping for registerDate for updates

---
+++ administrator/components/com_admin/sql/updates/postgresql/4.0.0-2019-10-13.sql
@@ -0,0 +1,3 @@
+UPDATE "#__content_types"
+   SET "field_mappings" = REPLACE("field_mappings",'"core_created_time":"registerdate"','"core_created_time":"registerDate"')
+ WHERE "type_alias" = 'com_users.user';
