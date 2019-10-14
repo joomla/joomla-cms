6e60ccf2f68a4086e2a9f67063a45389fa61e007
dgrammatiko <d.grammatiko@gmail.com>
2019-10-14 23:39:58 +0200

    🐶

--- build/media_source/com_fields/js/admin-field-changecontext.es6.js
+++ build/media_source/com_fields/js/admin-field-changecontext.es6.js
@@ -4,6 +4,6 @@ Joomla.fieldsChangeContext = (context) => {
   const regex = new RegExp('([?;&])context[^&;]*[;&]?');
   const url = window.location.href;
   const query = url.replace(regex, '$1').replace(/&$/, '');
-  // ESlint disable-next-line
+  // eslint-disable-next-line
   window.location.href = (query.length > 2 ? query + '&' : '?') + (context ? 'context=' + context : '');
 };
