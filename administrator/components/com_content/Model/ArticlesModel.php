04dd1eaf7cca1926600ef8b64bc1bbd8823014a6
Helios Ciancio <eshiol@users.noreply.github.com>
2019-10-19 12:02:45 +0200

    Update administrator/components/com_content/Model/ArticlesModel.php
    Co-Authored-By: Richard Fath <richard67@users.noreply.github.com>

--- administrator/components/com_content/Model/ArticlesModel.php
+++ administrator/components/com_content/Model/ArticlesModel.php
@@ -207,7 +207,7 @@ class ArticlesModel extends ListModel
 		$query->select(
 			$this->getState(
 				'list.select',
-				'DISTINCT a.id, a.asset_id, a.title, a.alias, a.checked_out, a.checked_out_time, a.catid' .
+				'a.id, a.asset_id, a.title, a.alias, a.checked_out, a.checked_out_time, a.catid' .
 				', a.state, a.access, a.created, a.created_by, a.created_by_alias, a.modified, a.ordering, a.featured, fp.featured_up, fp.featured_down' .
 				', a.language, a.hits, a.publish_up, a.publish_down, a.introtext, a.note'
 			)
