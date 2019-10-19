e222d89cab92d5ca8f25f44771b8a0ef612f2f4f
Richard Fath <richard67@users.noreply.github.com>
2019-10-19 19:34:12 +0200

    No default for created time of privacy consents for new installs

--- installation/sql/mysql/joomla.sql
+++ installation/sql/mysql/joomla.sql
@@ -1670,7 +1670,7 @@ CREATE TABLE IF NOT EXISTS `#__privacy_consents` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` int(10) unsigned NOT NULL DEFAULT 0,
   `state` INT(10) NOT NULL DEFAULT 1,
-  `created` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `created` datetime NOT NULL,
   `subject` varchar(255) NOT NULL DEFAULT '',
   `body` text NOT NULL,
   `remind` tinyint(4) NOT NULL DEFAULT 0,
