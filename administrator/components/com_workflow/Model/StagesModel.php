c3db49d318a50cd444d3e4028ad4ebe3facdf8d7
Christiane Maier-Stadtherr <dev@chmst.de>
2019-10-25 18:15:51 +0200

    Update administrator/components/com_workflow/Model/StagesModel.php
    Co-Authored-By: Quy <quy@fluxbb.org>

--- administrator/components/com_workflow/Model/StagesModel.php
+++ administrator/components/com_workflow/Model/StagesModel.php
@@ -163,7 +163,7 @@ class StagesModel extends ListModel
 
 		// Join over the users for the checked out user.
 		$query->select($db->quoteName('uc.name', 'editor'))
-			->join('LEFT', '#__users AS uc ON uc.id = s.checked_out');
+			->join('LEFT', $db->quoteName('#__users', 'uc'), $db->quoteName('uc.id') . ' = ' . $db->quoteName('s.checked_out'));
 
 		$status = (string) $this->getState('filter.published');
 
