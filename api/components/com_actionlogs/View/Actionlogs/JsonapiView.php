0e86598fab2ea5da6f545c18a6a9cd1d01807d38
Nicola Galgano <optimus4joomla@gmail.com>
2019-10-28 20:34:14 +0100

    webservices 2 of 8

---
+++ api/components/com_actionlogs/View/Actionlogs/JsonapiView.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * @package     Joomla.API
+ * @subpackage  com_actionlogs
+ *
+ * @copyright   Copyright (C) 2005 - 2019 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+namespace Joomla\Component\Actionlogs\Api\View\Actionlogs;
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\MVC\View\GenericDataException;
+use Joomla\CMS\MVC\View\JsonApiView as BaseApiView;
+use Joomla\CMS\Router\Exception\RouteNotFoundException;
+use Joomla\CMS\Serializer\JoomlaSerializer;
+use Joomla\CMS\Uri\Uri;
+use Tobscure\JsonApi\Resource;
+
+/**
+ * The consents view
+ *
+ * @since  4.0.0
+ */
+class JsonapiView extends BaseApiView
+{
+	/**
+	 * The fields to render item in the documents
+	 *
+	 * @var  array
+	 * @since  4.0.0
+	 */
+	protected $fieldsToRenderItem = [
+		'id',
+		'message_language_key',
+		'message',
+		'log_date',
+		'extension',
+		'user_id',
+		'item_id',
+		'ip_address',
+	];
+
+	/**
+	 * The fields to render items in the documents
+	 *
+	 * @var  array
+	 * @since  4.0.0
+	 */
+	protected $fieldsToRenderList = [
+		'id',
+		'message_language_key',
+		'message',
+		'log_date',
+		'extension',
+		'user_id',
+		'item_id',
+		'ip_address',
+	];
+
+	/**
+	 * Prepare item before render.
+	 *
+	 * @param   object  $item  The model item
+	 *
+	 * @return  object
+	 *
+	 * @since   4.0.0
+	 */
+	protected function prepareItem($item)
+	{
+		return parent::prepareItem($item);
+	}
+}
