055ee46978e7f1a512912a823388da2f512df503
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2019-11-08 11:39:00 +0200

    Token Authentication for the API application
    PHP file headers

--- plugins/user/token/token.php
+++ plugins/user/token/token.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package     Joomla.Plugin
- * @subpackage  User.terms
+ * @subpackage  User.token
  *
  * @copyright   Copyright (C) 2005 - 2019 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
