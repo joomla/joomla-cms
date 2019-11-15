9ac7f1954a44724fab9cc8f7909ba4edd0b15b1b
Nicola Galgano <optimus4joomla@gmail.com>
2019-11-15 10:23:18 +0100

    sql2prepared

--- administrator/components/com_messages/Model/MessageModel.php
+++ administrator/components/com_messages/Model/MessageModel.php
@@ -23,6 +23,7 @@ use Joomla\CMS\Router\Route;
 use Joomla\CMS\Table\Asset;
 use Joomla\CMS\Table\Table;
 use Joomla\CMS\User\User;
+use Joomla\Database\ParameterType;
 
 /**
  * Private Message model.
@@ -137,11 +138,18 @@ class MessageModel extends AdminModel
 					if ($replyId = $this->getState('reply.id'))
 					{
 						// If replying to a message, preload some data.
+						$replyId = (int) $replyId;
 						$db    = $this->getDbo();
 						$query = $db->getQuery(true)
-							->select($db->quoteName(array('subject', 'user_id_from')))
+							->select(
+								[
+									$db->quoteName('subject'),
+									$db->quoteName('user_id_from'),
+								]
+							)
 							->from($db->quoteName('#__messages'))
-							->where($db->quoteName('message_id') . ' = ' . (int) $replyId);
+							->where($db->quoteName('message_id') . ' = :messageid')
+							->bind(':messageid', $replyId, ParameterType::INTEGER);
 
 						try
 						{
@@ -176,7 +184,8 @@ class MessageModel extends AdminModel
 					$query = $db->getQuery(true)
 						->update($db->quoteName('#__messages'))
 						->set($db->quoteName('state') . ' = 1')
-						->where($db->quoteName('message_id') . ' = ' . $this->item->message_id);
+						->where($db->quoteName('message_id') . ' = :messageid')
+						->bind(':messageid', $this->item->message_id ,ParameterType::INTEGER);
 					$db->setQuery($query)->execute();
 				}
 			}
@@ -485,7 +494,7 @@ class MessageModel extends AdminModel
 			{
 				if ($enabled)
 				{
-					$groups[] = $db->quote($g);
+					$groups[] = $g;
 				}
 			}
 
@@ -499,8 +508,11 @@ class MessageModel extends AdminModel
 			$query = $db->getQuery(true)
 				->select($db->quoteName('map.user_id'))
 				->from($db->quoteName('#__user_usergroup_map', 'map'))
-				->join('LEFT', $db->quoteName('#__users', 'u') . ' ON ' . $db->quoteName('u.id') . ' = ' . $db->quoteName('map.user_id'))
-				->where($db->quoteName('map.group_id') . ' IN(' . implode(',', $groups) . ')')
+				->join('LEFT', 
+					$db->quoteName('#__users', 'u'), 
+					$db->quoteName('u.id') . ' = ' . $db->quoteName('map.user_id')
+				)
+				->whereIn($db->quoteName('map.group_id'), $groups)
 				->where($db->quoteName('u.block') . ' = 0')
 				->where($db->quoteName('u.sendEmail') . ' = 1');
 
