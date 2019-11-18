da1839292e74c1ef6961b062d592fb93e53fbdd2
Brian Teeman <brian@teeman.net>
2019-11-18 18:15:09 +0000

    update #__content_types

--- administrator/components/com_admin/sql/updates/postgresql/4.0.0-2019-11-18.sql
+++ administrator/components/com_admin/sql/updates/postgresql/4.0.0-2019-11-18.sql
@@ -1,1 +1,4 @@
 ALTER TABLE "#__banners" DROP COLUMN "alias";
+UPDATE "#__content_types"
+  SET "field_mappings" = REPLACE("field_mappings",',"core_alias":"alias"','')
+  WHERE "type_alias" = 'com_users.user';
