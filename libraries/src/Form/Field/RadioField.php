61b710852d8ce1b208fbe148feba878021ed5917
Quy <quy@fluxbb.org>
2019-11-18 08:52:45 -0800

    Assign false to 0 when casting as string

--- libraries/src/Form/Field/RadioField.php
+++ libraries/src/Form/Field/RadioField.php
@@ -94,6 +94,12 @@ class RadioField extends ListField
 	{
 		$data = parent::getLayoutData();
 
+		// Casting false as string returns an empty string so assign it 0
+		if (is_bool($this->value) && $this->value === false)
+		{
+			$this->value = '0';
+		}
+
 		$extraData = array(
 			'options' => $this->getOptions(),
 			'value'   => (string) $this->value,
