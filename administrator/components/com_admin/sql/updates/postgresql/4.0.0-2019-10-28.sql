fb1c21e414a3fef87d961e14920741803673f20b
Nicola Galgano <optimus4joomla@gmail.com>
2019-11-22 20:41:10 +0100

    option

--- administrator/components/com_admin/sql/updates/postgresql/4.0.0-2019-10-28.sql
+++ administrator/components/com_admin/sql/updates/postgresql/4.0.0-2019-10-28.sql
@@ -1,2 +1,3 @@
 INSERT INTO "#__extensions" ("package_id", "name", "type", "element", "folder", "client_id", "enabled", "access", "protected", "manifest_cache", "params", "checked_out", "checked_out_time", "ordering", "state") VALUES
 (0, 'plg_webservices_actionlog', 'plugin', 'actionlog', 'webservices', 0, 1, 1, 0, '', '{}', 0, NULL, 0, 0),
+INSERT INTO "#__action_logs_extensions" ("package_id") VALUES ('com_actionlogs');
