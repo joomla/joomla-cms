411ab265cfd2fa6ee5c2af0806ebd277f999a9b0
SharkyKZ <sharkykz@gmail.com>
2019-12-03 09:27:00 +0200

    Add own class for Switcher field

---
+++ libraries/src/Form/Field/SwitcherField.php
@@ -0,0 +1,116 @@
+<?php
+/**
+ * Joomla! Content Management System
+ *
+ * @copyright  Copyright (C) 2005 - 2019 Open Source Matters, Inc. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+namespace Joomla\CMS\Form\Field;
+
+\defined('JPATH_PLATFORM') or die;
+
+/**
+ * Switcher Form Field class.
+ *
+ * @since  __DEPLOY_VERSION__
+ */
+class SwitcherField extends ListField
+{
+	/**
+	 * The form field type.
+	 *
+	 * @var    string
+	 * @since  __DEPLOY_VERSION__
+	 */
+	protected $type = 'Switcher';
+
+	/**
+	 * Name of the layout being used to render the field
+	 *
+	 * @var    string
+	 * @since  __DEPLOY_VERSION__
+	 */
+	protected $layout = 'joomla.form.field.switcher';
+
+	/**
+	 * Method to attach a Form object to the field.
+	 *
+	 * @param   \SimpleXMLElement  $element  The SimpleXMLElement object representing the `<field>` tag for the form field object.
+	 * @param   mixed              $value    The form field value to validate.
+	 * @param   string             $group    The field name group control value. This acts as as an array container for the field.
+	 *                                       For example if the field has name="foo" and the group value is set to "bar" then the
+	 *                                       full field name would end up being "bar[foo]".
+	 *
+	 * @return  boolean  True on success.
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 */
+	public function setup(\SimpleXMLElement $element, $value, $group = null)
+	{
+		if (!parent::setup($element, $value, $group))
+		{
+			return false;
+		}
+
+		$this->hiddenLabel = true;
+
+		return true;
+	}
+
+	/**
+	 * Method to get the radio button field input markup.
+	 *
+	 * @return  string  The field input markup.
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 *
+	 * @throws  \UnexpectedValueException
+	 */
+	protected function getInput()
+	{
+		if ($this->layout === '')
+		{
+			throw new \UnexpectedValueException(sprintf('%s has no layout assigned.', $this->name));
+		}
+
+		return parent::getInput();
+	}
+
+	/**
+	 * Method to get the radio button field input markup.
+	 *
+	 * @return  string  The field input markup.
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 *
+	 * @throws  \UnexpectedValueException
+	 */
+	protected function getOptions()
+	{
+		$options = parent::getOptions();
+
+		if (\count($options) !== 2)
+		{
+			throw new \UnexpectedValueException(sprintf('%s field of type %s must have exactly 2 options.', $this->name, $this->type));
+		}
+
+		return $options;
+	}
+
+	/**
+	 * Method to get the data to be passed to the layout for rendering.
+	 *
+	 * @return  array
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 */
+	protected function getLayoutData()
+	{
+		$data            = parent::getLayoutData();
+		$data['options'] = $this->getOptions();
+		$data['value']   = $this->value;
+
+		return $data;
+	}
+}
