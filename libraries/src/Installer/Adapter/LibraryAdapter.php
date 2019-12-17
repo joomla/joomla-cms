b4d2c0a7712188738e4443091e93770b0115f592
Thomas Hunziker <werbemails@bakual.ch>
2019-12-17 23:08:32 +0100

    Don't allow installing a library which overwrites core folders

--- libraries/src/Installer/Adapter/LibraryAdapter.php
+++ libraries/src/Installer/Adapter/LibraryAdapter.php
@@ -229,6 +229,14 @@ class LibraryAdapter extends InstallerAdapter
 	{
 		$group = (string) $this->getManifest()->libraryname;
 
+		// Don't install libraries which would override core folders
+		$restricedFolders = array('cms', 'fof', 'idna_convert', 'joomla', 'legacy', 'php-encryption', 'phpass', 'phputf8', 'src', 'vendor');
+
+		if (in_array($group, $restricedFolders))
+		{
+			throw new \RuntimeException(\JText::_('JLIB_INSTALLER_ABORT_LIB_INSTALL_CORE_FOLDER'));
+		}
+
 		if (!$group)
 		{
 			throw new \RuntimeException(\JText::_('JLIB_INSTALLER_ABORT_LIB_INSTALL_NOFILE'));
