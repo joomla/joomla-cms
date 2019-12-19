350b82e47389e64a3f43edb16ef5969de73fe766
Hannes Papenberg <info@joomlager.de>
2019-12-19 13:48:09 +0100

    Preparing 3.10 for 4.0 content versioning (#23479)
    * Backporting a few of the versioning classes to ease transition from 3.x to 4.0
    
    * Update VersionableControllerTrait.php
    
    * Update VersionableModelTrait.php
    
    * Update VersionableTableInterface.php

---
+++ libraries/src/Versioning/VersionableModelTrait.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Joomla! Content Management System
+ *
+ * @copyright  Copyright (C) 2005 - 2019 Open Source Matters, Inc. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+namespace Joomla\CMS\Versioning;
+
+defined('JPATH_PLATFORM') or die;
+
+/**
+ * Defines the trait for a Versionable Model Class.
+ * Note: This is a placeholder to ease transition to the new system in 4.0
+ *
+ * @since  __DEPLOY_VERSION__
+ */
+trait VersionableModelTrait
+{
+}
