f73a4405e3939567d92d058c3712fc2a51b6fe23
brian teeman <brian@teeman.net>
2019-12-20 22:00:46 +0000

    [4.0] readmore with php7.4
    Pull request for #27239
    
    Fixes a b/c breaking change from php 7.4

--- plugins/editors-xtd/readmore/readmore.php
+++ plugins/editors-xtd/readmore/readmore.php
@@ -49,7 +49,7 @@ class PlgButtonReadmore extends CMSPlugin
 		);
 
 		$getContentResult = $this->getDispatcher()->dispatch('getContent', $event);
-		$getContent = $getContentResult['result'][0];
+		$getContent = isset($getContentResult['result'][0]);
 		HTMLHelper::_('script', 'com_content/admin-article-readmore.min.js', array('version' => 'auto', 'relative' => true));
 
 		// Pass some data to javascript
