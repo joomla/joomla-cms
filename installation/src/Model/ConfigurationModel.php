f6044f4d8c4b0c3b0f44a4ab398cf0908e36b6a4
Richard Fath <richard67@users.noreply.github.com>
2019-12-26 16:51:49 +0100

    Fix configuration model

--- installation/src/Model/ConfigurationModel.php
+++ installation/src/Model/ConfigurationModel.php
@@ -138,6 +138,7 @@ class ConfigurationModel extends BaseInstallationModel
 		$registry->set('dbsslkey', $options->db_sslkey);
 		$registry->set('dbsslcert', $options->db_sslcert);
 		$registry->set('dbsslca', $options->db_sslca);
+		$registry->set('dbsslcapath', '');
 		$registry->set('dbsslcipher', $options->db_sslcipher);
 
 		// Server settings.
