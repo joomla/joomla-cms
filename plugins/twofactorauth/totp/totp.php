a186457537ae7a1c32addef69473b9a77e19e025
brian teeman <brian@teeman.net>
2020-01-01 11:54:49 +0000

    less haste more speed

--- plugins/twofactorauth/totp/totp.php
+++ plugins/twofactorauth/totp/totp.php
@@ -111,11 +111,11 @@ class PlgTwofactorauthTotp extends CMSPlugin
 
 		// These are used by Google Authenticator to tell accounts apart
 		$username = Factory::getUser($user_id)->username;
-		$sitename = rawurlencode(Factory::getApplication()->get('sitename'));
+		$sitename = Factory::getApplication()->get('sitename');
 
 
 		// This is the URL to the QR code for Google Authenticator
-		$url = sprintf("otpauth://totp/%s/%s?secret=%s&issuer=%s", $sitename, $username, $secret, $sitename);
+		$url = sprintf("otpauth://totp/%s/%s?secret=%s&issuer=%s", rawurlencode($sitename), $username, $secret, rawurlencode($sitename));
 
 		// Is this a new TOTP setup? If so, we'll have to show the code validation field.
 		$new_totp = $otpConfig->method !== 'totp';
