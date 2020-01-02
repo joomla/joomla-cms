9974313e984b9e516453095173d6a4d8d3c750b1
Jelle Kok <schumacher1986@hotmail.com>
2020-01-02 16:51:32 +0100

    remove not needed use

--- libraries/src/Document/HtmlDocument.php
+++ libraries/src/Document/HtmlDocument.php
@@ -20,7 +20,6 @@ use Joomla\CMS\Uri\Uri;
 use Joomla\CMS\Utility\Utility;
 use Joomla\Database\ParameterType;
 use Joomla\Registry\Registry;
-use function array_key_exists;
 
 /**
  * HtmlDocument class, provides an easy interface to parse and display a HTML document
