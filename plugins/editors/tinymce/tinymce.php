01d669f69fb73cc1c0d79f06ce412a0fd902e0ff
brian teeman <brian@teeman.net>
2020-01-12 12:44:33 +0000

    [4.0] TinyMCE element_path
    Tinymce has an option in the plugin called Element Path but it doesn't do anything as there is no code to do anything.
    
    With this PR you can no disable/enable the display of the current element in the bottom left hand corner
    
    ### Testing
    Make sure that you set the option to be enabled/disabled in the correct set. Default for a super user is set 0
    
    ### Additional Note
    This is also broken in Joomla 3

--- plugins/editors/tinymce/tinymce.php
+++ plugins/editors/tinymce/tinymce.php
@@ -390,6 +390,9 @@ class PlgEditorTinymce extends CMSPlugin
 			$resizing = 'both';
 		}
 
+		// Check if the elementpath should be displayed
+		$elementpath       = (bool) $levelParams->get('element_path', true);
+
 		// Set of always available plugins
 		$plugins  = array(
 			'autolink',
@@ -594,6 +597,7 @@ class PlgEditorTinymce extends CMSPlugin
 				'image_title'        => true,
 				'height'             => $html_height,
 				'width'              => $html_width,
+				'elementpath'        => $elementpath,
 				'resize'             => $resizing,
 				'templates'          => $templates,
 				'image_advtab'       => (bool) $levelParams->get('image_advtab', false),
