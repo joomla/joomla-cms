8077b3791b13ec459114dba3449b34cea32bf151
SharkyKZ <sharkykz@gmail.com>
2020-01-16 11:13:44 +0200

    Ignore action log notification exceptions

--- administrator/components/com_actionlogs/Model/ActionlogModel.php
+++ administrator/components/com_actionlogs/Model/ActionlogModel.php
@@ -11,15 +11,14 @@ namespace Joomla\Component\Actionlogs\Administrator\Model;
 
 defined('_JEXEC') or die;
 
-use Exception;
 use Joomla\CMS\Component\ComponentHelper;
 use Joomla\CMS\Factory;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\Layout\FileLayout;
 use Joomla\CMS\MVC\Model\BaseDatabaseModel;
-use Joomla\CMS\MVC\View\GenericDataException;
 use Joomla\Component\Actionlogs\Administrator\Helper\ActionlogsHelper;
 use Joomla\Utilities\IpHelper;
+use PHPMailer\PHPMailer\Exception as phpMailerException;
 
 /**
  * Methods supporting a list of Actionlog records.
@@ -40,8 +39,6 @@ class ActionlogModel extends BaseDatabaseModel
 	 * @return  void
 	 *
 	 * @since   3.9.0
-	 *
-	 * @throws  Exception
 	 */
 	public function addLog($messages, $messageLanguageKey, $context, $userId = null)
 	{
@@ -88,8 +85,15 @@ class ActionlogModel extends BaseDatabaseModel
 			}
 		}
 
-		// Send notification email to users who choose to be notified about the action logs
-		$this->sendNotificationEmails($loggedMessages, $user->name, $context);
+		try
+		{
+			// Send notification email to users who choose to be notified about the action logs
+			$this->sendNotificationEmails($loggedMessages, $user->name, $context);
+		}
+		catch (\RuntimeException | phpMailerException $e)
+		{
+			// Ignore it
+		}
 	}
 
 	/**
@@ -103,7 +107,8 @@ class ActionlogModel extends BaseDatabaseModel
 	 *
 	 * @since   3.9.0
 	 *
-	 * @throws  Exception
+	 * @throws  \RuntimeException   if the mail function is disabled
+	 * @throws  phpmailerException  if sending mail failed
 	 */
 	protected function sendNotificationEmails($messages, $username, $context)
 	{
@@ -165,10 +170,6 @@ class ActionlogModel extends BaseDatabaseModel
 		$mailer->isHTML(true);
 		$mailer->Encoding = 'base64';
 		$mailer->setBody($body);
-
-		if (!$mailer->Send())
-		{
-			throw new GenericDataException(Text::_('JERROR_SENDING_EMAIL'), 500);
-		}
+		$mailer->Send();
 	}
 }
