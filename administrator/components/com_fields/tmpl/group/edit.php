c99193ea453b3563ae28656551c4baf3c24567ff
Ciaran Walsh <ciaran@joomla51.com>
2020-01-18 12:23:29 +0000

    [4.0] Switch remaining options from grid to column layout (#27153)

--- administrator/components/com_fields/tmpl/group/edit.php
+++ administrator/components/com_fields/tmpl/group/edit.php
@@ -63,7 +63,7 @@ $this->useCoreUI = true;
 		<?php echo HTMLHelper::_('uitab.addTab', 'myTab', 'publishing', Text::_('JGLOBAL_FIELDSET_PUBLISHING', true)); ?>
 		<div class="row form-horizontal-desktop">
 			<div class="col-md-6">
-				<fieldset id="fieldset-rules" class="options-grid-form options-grid-form-full">
+				<fieldset id="fieldset-rules" class="options-form">
 					<legend><?php echo Text::_('JGLOBAL_FIELDSET_PUBLISHING'); ?></legend>
 					<div>
 					<?php echo LayoutHelper::render('joomla.edit.publishingdata', $this); ?>
@@ -78,7 +78,7 @@ $this->useCoreUI = true;
 		<?php echo LayoutHelper::render('joomla.edit.params', $this); ?>
 		<?php if ($this->canDo->get('core.admin')) : ?>
 			<?php echo HTMLHelper::_('uitab.addTab', 'myTab', 'rules', Text::_('JGLOBAL_ACTION_PERMISSIONS_LABEL', true)); ?>
-			<fieldset id="fieldset-rules" class="options-grid-form options-grid-form-full">
+			<fieldset id="fieldset-rules" class="options-form">
 				<legend><?php echo Text::_('JGLOBAL_ACTION_PERMISSIONS_LABEL'); ?></legend>
 				<div>
 				<?php echo $this->form->getInput('rules'); ?>
