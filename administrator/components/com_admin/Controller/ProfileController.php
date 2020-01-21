406dfe3a2e04eb4fd510f75e8f0c217d5ab0b7a3
SharkyKZ <sharkykz@gmail.com>
2020-01-21 11:15:45 +0200

    Fix redirects

--- administrator/components/com_admin/Controller/ProfileController.php
+++ administrator/components/com_admin/Controller/ProfileController.php
@@ -13,6 +13,7 @@ defined('_JEXEC') or die;
 
 use Joomla\CMS\MVC\Controller\FormController;
 use Joomla\CMS\Router\Route;
+use Joomla\CMS\Uri\Uri;
 
 /**
  * User profile controller class.
@@ -36,5 +37,66 @@ class ProfileController extends FormController
 	protected function allowEdit($data = array(), $key = 'id')
 	{
 		return isset($data['id']) && $data['id'] == $this->app->getIdentity()->id;
+	}
+
+	/**
+	 * Overrides parent save method to check the submitted passwords match.
+	 *
+	 * @param   string  $key     The name of the primary key of the URL variable.
+	 * @param   string  $urlVar  The name of the URL variable if different from the primary key (sometimes required to avoid router collisions).
+	 *
+	 * @return  boolean  True if successful, false otherwise.
+	 *
+	 * @since   3.2
+	 */
+	public function save($key = null, $urlVar = null)
+	{
+		$result = parent::save();
+
+		if ($this->getTask() !== 'apply')
+		{
+			$return = base64_decode($this->input->get('return', '', 'BASE64'));
+
+			 if ($return !== '' && Uri::isInternal($return))
+			 {
+				// Redirect to return URL.
+				$this->setRedirect(Route::_($return, false));
+			 }
+			 else
+			 {
+				 // Redirect to the main page.
+				$this->setRedirect(Route::_('index.php', false));
+			 }
+		}
+
+		return $result;
+	}
+
+	/**
+	 * Method to cancel an edit.
+	 *
+	 * @param   string  $key  The name of the primary key of the URL variable.
+	 *
+	 * @return  boolean  True if access level checks pass, false otherwise.
+	 *
+	 * @since   1.6
+	 */
+	public function cancel($key = null)
+	{
+		$result = parent::cancel($key);
+		$return = base64_decode($this->input->get('return', '', 'BASE64'));
+
+		if ($return !== '' && Uri::isInternal($return))
+		{
+			// Redirect to return URL.
+			$this->setRedirect(Route::_($return, false));
+		}
+		else
+		{
+			// Redirect to the main page.
+			$this->setRedirect(Route::_('index.php', false));
+		}
+
+		return $result;
 	}
 }
