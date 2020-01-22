4172b68dac980a85c6070511622322006e98fb5b
Harald Leithner <leithner@itronic.at>
2020-01-22 23:07:06 +0100

    Prepare Joomla 3.9.15 release candidate

--- plugins/content/confirmconsent/fields/consentbox.php
+++ plugins/content/confirmconsent/fields/consentbox.php
@@ -3,7 +3,7 @@
  * @package     Joomla.Plugin
  * @subpackage  Content.confirmconsent
  *
- * @copyright   Copyright (C) 2005 - 2019 Open Source Matters, Inc. All rights reserved.
+ * @copyright   Copyright (C) 2005 - 2020 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
@@ -165,7 +165,7 @@ class JFormFieldConsentBox extends JFormFieldCheckboxes
 	 *
 	 * @return  string  The field input markup.
 	 *
-	 * @since   __DEPLOY_VERSION__
+	 * @since   3.9.2
 	 */
 	protected function getInput()
 	{
