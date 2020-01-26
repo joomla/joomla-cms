f54d303b6f21bc7b17d4905b334c13c55a625edc
SharkyKZ <sharkykz@gmail.com>
2020-01-26 23:09:22 +0200

    Align

--- plugins/content/confirmconsent/src/Field/ConsentBoxField.php
+++ plugins/content/confirmconsent/src/Field/ConsentBoxField.php
@@ -97,10 +97,10 @@ class ConsentBoxField extends CheckboxesField
 	 * Method to attach a JForm object to the field.
 	 *
 	 * @param   \SimpleXMLElement  $element  The SimpleXMLElement object representing the `<field>` tag for the form field object.
-	 * @param   mixed             $value    The form field value to validate.
-	 * @param   string            $group    The field name group control value. This acts as an array container for the field.
-	 *                                      For example if the field has name="foo" and the group value is set to "bar" then the
-	 *                                      full field name would end up being "bar[foo]".
+	 * @param   mixed              $value    The form field value to validate.
+	 * @param   string             $group    The field name group control value. This acts as an array container for the field.
+	 *                                       For example if the field has name="foo" and the group value is set to "bar" then the
+	 *                                       full field name would end up being "bar[foo]".
 	 *
 	 * @return  boolean  True on success.
 	 *
