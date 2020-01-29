818fe5c4ada98cbe17aa14f18d2283f1ca9d48eb
SharkyKZ <sharkykz@gmail.com>
2020-01-29 11:32:14 +0200

    CS

--- components/com_banners/Service/Router.php
+++ components/com_banners/Service/Router.php
@@ -23,7 +23,7 @@ class Router extends RouterBase
 	/**
 	 * Build the route for the com_banners component
 	 *
-	 * @param   array  &$query  An array of URL arguments
+	 * @param   array  $query  An array of URL arguments
 	 *
 	 * @return  array  The URL arguments to use to assemble the subsequent URL.
 	 *
@@ -58,7 +58,7 @@ class Router extends RouterBase
 	/**
 	 * Parse the segments of a URL.
 	 *
-	 * @param   array  &$segments  The segments of the URL to parse.
+	 * @param   array  $segments  The segments of the URL to parse.
 	 *
 	 * @return  array  The URL attributes to be used by the application.
 	 *
