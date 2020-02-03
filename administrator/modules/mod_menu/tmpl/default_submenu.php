8a9e9ec2a11b444af97ea5cf83e09e8228d15836
Brian Teeman <brian@teeman.net>
2020-02-03 19:12:55 +0000

    Update administrator/modules/mod_menu/tmpl/default_submenu.php
    Co-Authored-By: Quy <quy@fluxbb.org>

--- administrator/modules/mod_menu/tmpl/default_submenu.php
+++ administrator/modules/mod_menu/tmpl/default_submenu.php
@@ -147,6 +147,7 @@ else
 {
 	echo '<span>' . Text::_($current->title) . '</span>' . $ajax;
 }
+
 if ($currentParams->get('menu-quicktask', false) && $this->params->get('shownew', 1) === 1)
 {
 	$params = $current->getParams();
