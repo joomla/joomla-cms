2c5705f01c2f42feaff395dc74f84bed2339d496
brian teeman <brian@teeman.net>
2020-02-06 14:01:03 +0000

    masonry

--- build/media_source/com_cpanel/js/admin-cpanel-default.es6.js
+++ build/media_source/com_cpanel/js/admin-cpanel-default.es6.js
@@ -71,6 +71,29 @@
     document.removeEventListener('DOMContentLoaded', onBoot);
   };
 
+  function resizeGridItem(item){
+    grid = document.getElementsByClassName("card-columns")[0];
+    rowHeight = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-auto-rows'));
+    rowGap = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-row-gap'));
+    rowSpan = Math.ceil((item.querySelector('.card').getBoundingClientRect().height+rowGap)/(rowHeight+rowGap));
+      item.style.gridRowEnd = "span "+rowSpan;
+  }
+
+  function resizeAllGridItems(){
+    allItems = document.getElementsByClassName("module-wrapper");
+    for(x=0;x<allItems.length;x++){
+      resizeGridItem(allItems[x]);
+    }
+  }
+
+  function resizeInstance(instance){
+    item = instance.elements[0];
+    resizeGridItem(item);
+  }
+
+  window.onload = resizeAllGridItems();
+  window.addEventListener("resize", resizeAllGridItems);
+
   // Initialise
   document.addEventListener('DOMContentLoaded', onBoot);
 })(window, document, window.Joomla);
