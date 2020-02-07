51a0124a70add0816e51f3f7c0a7f0ac759a2d6f
SharkyKZ <sharkykz@gmail.com>
2020-02-07 09:03:53 +0200

    Not needed after recent changes

--- libraries/src/Form/Field/SwitcherField.php
+++ libraries/src/Form/Field/SwitcherField.php
@@ -34,31 +34,6 @@ class SwitcherField extends ListField
 	protected $layout = 'joomla.form.field.switcher';
 
 	/**
-	 * Method to attach a Form object to the field.
-	 *
-	 * @param   \SimpleXMLElement  $element  The SimpleXMLElement object representing the `<field>` tag for the form field object.
-	 * @param   mixed              $value    The form field value to validate.
-	 * @param   string             $group    The field name group control value. This acts as as an array container for the field.
-	 *                                       For example if the field has name="foo" and the group value is set to "bar" then the
-	 *                                       full field name would end up being "bar[foo]".
-	 *
-	 * @return  boolean  True on success.
-	 *
-	 * @since   __DEPLOY_VERSION__
-	 */
-	public function setup(\SimpleXMLElement $element, $value, $group = null)
-	{
-		if (!parent::setup($element, $value, $group))
-		{
-			return false;
-		}
-
-		$this->hiddenLabel = true;
-
-		return true;
-	}
-
-	/**
 	 * Method to get the radio button field input markup.
 	 *
 	 * @return  string  The field input markup.
