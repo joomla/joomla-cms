68b29fcf24708d0fa5cadcbafb75d778316a6c0c
Richard Fath <richard67@users.noreply.github.com>
2020-02-08 14:13:17 +0100

    Allow empty metakey values in database tables

---
+++ administrator/components/com_admin/sql/updates/mysql/4.0.0-2020-02-08.sql
@@ -0,0 +1,7 @@
+ALTER TABLE `#__banners` MODIFY `metakey` text NOT NULL DEFAULT '';
+ALTER TABLE `#__banner_clients` MODIFY `metakey` text NOT NULL DEFAULT '';
+ALTER TABLE `#__contact_details` MODIFY `metakey` text NOT NULL DEFAULT '';
+ALTER TABLE `#__content` MODIFY `metakey` text NOT NULL DEFAULT '';
+ALTER TABLE `#__languages` MODIFY `metakey` text NOT NULL DEFAULT '';
+ALTER TABLE `#__newsfeeds` MODIFY `metakey` text NOT NULL DEFAULT '';
+ALTER TABLE `#__tags` MODIFY `metakey` text NOT NULL DEFAULT '';
---
+++ administrator/components/com_admin/sql/updates/postgresql/4.0.0-2020-02-08.sql
@@ -0,0 +1,7 @@
+ALTER TABLE "#__banners" ALTER COLUMN "metakey" SET DEFAULT '';
+ALTER TABLE "#__banner_clients" ALTER COLUMN "metakey" SET DEFAULT '';
+ALTER TABLE "#__contact_details" ALTER COLUMN "metakey" SET DEFAULT '';
+ALTER TABLE "#__content" ALTER COLUMN "metakey" SET DEFAULT '';
+ALTER TABLE "#__languages" ALTER COLUMN "metakey" SET DEFAULT '';
+ALTER TABLE "#__newsfeeds" ALTER COLUMN "metakey" SET DEFAULT '';
+ALTER TABLE "#__tags" ALTER COLUMN "metakey" SET DEFAULT '';
