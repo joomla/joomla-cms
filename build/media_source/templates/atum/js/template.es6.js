25cfe8d316275e828a3583992303fac9ebced24c
Subhang23 <vsubhang4@gmail.com>
2020-02-13 15:02:01 +0530

    fix error in improper tag selection

--- build/media_source/templates/atum/js/template.es6.js
+++ build/media_source/templates/atum/js/template.es6.js
@@ -285,7 +285,7 @@
     }
   });
   document.addEventListener('click', (event) => {
-    if (document.getElementById('header-more-items').querySelector('.active') && !document.querySelector('.header-item-more').contains(event.target)) {
+    if (document.getElementById('header-more-items') && document.querySelector('.active') && !document.querySelector('.header-item-more').contains(event.target)) {
       document.getElementById('header-more-items').classList.remove('active');
     }
   });
