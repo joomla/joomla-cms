549130fb64109102dc940bcdc1356848cd838731
ravi nandan saxena <44205807+ravisaxena23@users.noreply.github.com>
2020-02-16 00:20:35 +0530

    Update layouts/joomla/edit/params.php
    Co-Authored-By: Quy <quy@fluxbb.org>

--- layouts/joomla/edit/params.php
+++ layouts/joomla/edit/params.php
@@ -145,7 +145,7 @@ foreach ($fieldSets as $name => $fieldSet)
 		{
 			echo '<fieldset id="fieldset-' . $name . '" class="options-form ' . (!empty($fieldSet->class) ? $fieldSet->class : '') . '">';
 			echo '<legend>' . $label . '</legend>';
-			echo $label=="Layout Builder" ?'<div class="">':'<div class="column-count-md-2 column-count-lg-3">';
+			echo $label === 'Layout Builder' ? '<div class="">' : '<div class="column-count-md-2 column-count-lg-3">';
 
 			$opentab = 2;
 		}
