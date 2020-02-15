c4d5130c29e12f67895f261654deac02ba437ae0
astridx <info@astrid-guenther.de>
2020-02-15 12:02:07 +0100

    wip

--- plugins/fields/media/media.php
+++ plugins/fields/media/media.php
@@ -40,12 +40,9 @@ class PlgFieldsMedia extends \Joomla\Component\Fields\Administrator\Plugin\Field
 
 		json_decode($field->value);
 
-		if ($field->value !== '' || json_last_error() !== JSON_ERROR_NONE)
-		{
-			$field->apivalue = $this->checkValue($field->value);
-			$field->rawvalue = $this->checkValue($field->value);
-			$field->value = $this->checkValue($field->value);
-		}
+		$field->apivalue = $this->checkValue($field->value);
+		$field->rawvalue = $this->checkValue($field->value);
+		$field->value = $this->checkValue($field->value);
 
 		$fieldNode->setAttribute('type', 'accessiblemedia');
 
