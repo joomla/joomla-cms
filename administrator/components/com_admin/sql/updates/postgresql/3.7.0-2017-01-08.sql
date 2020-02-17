798ec07cd696eabac5b3b9c5ae550080f0878cbe
Richard Fath <richard67@users.noreply.github.com>
2020-02-17 22:13:17 +0100

    Corrected English grammar in comment

--- administrator/components/com_admin/sql/updates/postgresql/3.7.0-2017-01-08.sql
+++ administrator/components/com_admin/sql/updates/postgresql/3.7.0-2017-01-08.sql
@@ -2,7 +2,7 @@
 ALTER TABLE "#__ucm_content" ALTER COLUMN "core_title" SET DEFAULT '';
 
 --
--- The following statements have to be disabled because it conflicts with
+-- The following statements have to be disabled because they conflict with
 -- a later change added with Joomla! 3.9.16, see file 3.9.16-2020-02-15.sql
 --
 -- ALTER TABLE "#__ucm_content" ALTER COLUMN "core_body" SET DEFAULT '';
