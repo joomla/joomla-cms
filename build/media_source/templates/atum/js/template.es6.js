a02351f16fbb7bfb80ac4c06cfd74f81db0b104b
Subhang23 <vsubhang4@gmail.com>
2020-02-18 00:05:32 +0530

    fix issue of screen size in between desktop and mobile

--- build/media_source/templates/atum/js/template.es6.js
+++ build/media_source/templates/atum/js/template.es6.js
@@ -221,10 +221,6 @@
    */
   function setDesktop() {
     const sidebarWrapper = doc.querySelector('.sidebar-wrapper');
-    const toolbar = doc.querySelector('.container-fluid');
-    toolbar.getElementsByClassName('subhead')[0].classList.add('show');
-    toolbar.getElementsByClassName('navbar-toggler toggler-toolbar')[0].setAttribute('aria-expanded', 'true');
-    toolbar.getElementsByClassName('navbar-toggler toggler-toolbar')[0].classList.remove('collapsed');
     if (!sidebarWrapper) {
       changeLogo('closed');
     } else {
@@ -241,6 +237,12 @@
    */
   function reactToResize() {
     window.addEventListener('resize', () => {
+      if (window.innerWidth >= 576) {
+        const toolbar = doc.querySelector('.container-fluid');
+        toolbar.getElementsByClassName('subhead')[0].classList.add('show');
+        toolbar.getElementsByClassName('navbar-toggler toggler-toolbar')[0].setAttribute('aria-expanded', 'true');
+        toolbar.getElementsByClassName('navbar-toggler toggler-toolbar')[0].classList.remove('collapsed');
+      }
       if (mobile.matches) {
         setMobile();
       } else {
