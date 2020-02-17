15f959ae00cd1dec55fa1b10612d1cf0a4772709
Bear <programming@hallhome.us>
2020-02-17 15:49:35 -0600

    Update administrator/components/com_newsfeeds/src/Field/Modal/NewsfeedField.php
    Co-Authored-By: Quy <quy@fluxbb.org>

--- administrator/components/com_newsfeeds/src/Field/Modal/NewsfeedField.php
+++ administrator/components/com_newsfeeds/src/Field/Modal/NewsfeedField.php
@@ -151,7 +151,7 @@ class NewsfeedField extends FormField
 				. ' data-toggle="modal"'
 				. ' type="button"'
 				. ' data-target="#ModalSelect' . $modalId . '">'
-				. '<span class="fas fa-check" aria-hidden="true"></span> ' . Text::_('JSELECT')
+				. '<span class="fas fa-file" aria-hidden="true"></span> ' . Text::_('JSELECT')
 				. '</button>';
 		}
 
