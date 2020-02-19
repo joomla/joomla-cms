f931c76c14d9a7ca773542df4cda3f71b09be5bd
Subhang23 <vsubhang4@gmail.com>
2020-02-19 23:09:31 +0530

    refactored code to make it more proper

--- administrator/components/com_media/resources/scripts/components/toolbar/toolbar.vue
+++ administrator/components/com_media/resources/scripts/components/toolbar/toolbar.vue
@@ -1,45 +1,47 @@
 <template>
-    <div class="media-toolbar"role="toolbar" :aria-label="translate('COM_MEDIA_TOOLBAR_LABEL')">
-        <div class="media-loader" v-if="isLoading"></div>
-        <div class="media-view-icons">
-            <a href="#" class="media-toolbar-icon media-toolbar-select-all"
-               @click.stop.prevent="toggleSelectAll()"
-               :aria-label="translate('COM_MEDIA_SELECT_ALL')">
-                <span :class="toggleSelectAllBtnIcon" aria-hidden="true"></span>
-            </a>
-        </div>
-        <media-breadcrumb></media-breadcrumb>
-        <div class="media-view-search-input" role="search">
-            <label for="media_search" class="sr-only">{{ translate('COM_MEDIA_SEARCH') }}</label>
-            <input id="media_search" type="text" @input="changeSearch" :placeholder="translate('COM_MEDIA_SEARCH')"/>
-        </div>
-        <div class="media-view-icons">
-            <button type="button" class="media-toolbar-icon media-toolbar-decrease-grid-size"
-               v-if="isGridView"
-               :class="{disabled: isGridSize('xs')}"
-               @click.stop.prevent="decreaseGridSize()"
-               :aria-label="translate('COM_MEDIA_DECREASE_GRID')">
-                <span class="fas fa-search-minus" aria-hidden="true"></span>
-            </button>
-            <button type="button" class="media-toolbar-icon media-toolbar-increase-grid-size"
-               v-if="isGridView"
-               :class="{disabled: isGridSize('xl')}"
-               @click.stop.prevent="increaseGridSize()"
-               :aria-label="translate('COM_MEDIA_INCREASE_GRID')">
-                <span class="fas fa-search-plus" aria-hidden="true"></span>
-            </button>
-            <button type="button" href="#" class="media-toolbar-icon media-toolbar-list-view"
-               @click.stop.prevent="changeListView()"
-               :aria-label="translate('COM_MEDIA_TOGGLE_LIST_VIEW')">
-                <span :class="toggleListViewBtnIcon" aria-hidden="true"></span>
-            </button>
-            <button type="button" href="#" class="media-toolbar-icon media-toolbar-info"
-               @click.stop.prevent="toggleInfoBar"
-               :aria-label="translate('COM_MEDIA_TOGGLE_INFO')">
-                <span class="fas fa-info" aria-hidden="true"></span>
-            </button>
-        </div>
-    </div>
+    <div>
+		<div class="media-toolbar"role="toolbar" :aria-label="translate('COM_MEDIA_TOOLBAR_LABEL')">
+			<div class="media-loader" v-if="isLoading"></div>
+			<div class="media-view-icons">
+				<a href="#" class="media-toolbar-icon media-toolbar-select-all"
+				   @click.stop.prevent="toggleSelectAll()"
+				   :aria-label="translate('COM_MEDIA_SELECT_ALL')">
+					<span :class="toggleSelectAllBtnIcon" aria-hidden="true"></span>
+				</a>
+			</div>
+			<div class="media-view-search-input" role="search">
+				<label for="media_search" class="sr-only">{{ translate('COM_MEDIA_SEARCH') }}</label>
+				<input id="media_search" type="text" @input="changeSearch" :placeholder="translate('COM_MEDIA_SEARCH')"/>
+			</div>
+			<div class="media-view-icons">
+				<button type="button" class="media-toolbar-icon media-toolbar-decrease-grid-size"
+						v-if="isGridView"
+						:class="{disabled: isGridSize('xs')}"
+						@click.stop.prevent="decreaseGridSize()"
+						:aria-label="translate('COM_MEDIA_DECREASE_GRID')">
+					<span class="fas fa-search-minus" aria-hidden="true"></span>
+				</button>
+				<button type="button" class="media-toolbar-icon media-toolbar-increase-grid-size"
+						v-if="isGridView"
+						:class="{disabled: isGridSize('xl')}"
+						@click.stop.prevent="increaseGridSize()"
+						:aria-label="translate('COM_MEDIA_INCREASE_GRID')">
+					<span class="fas fa-search-plus" aria-hidden="true"></span>
+				</button>
+				<button type="button" href="#" class="media-toolbar-icon media-toolbar-list-view"
+						@click.stop.prevent="changeListView()"
+						:aria-label="translate('COM_MEDIA_TOGGLE_LIST_VIEW')">
+					<span :class="toggleListViewBtnIcon" aria-hidden="true"></span>
+				</button>
+				<button type="button" href="#" class="media-toolbar-icon media-toolbar-info"
+						@click.stop.prevent="toggleInfoBar"
+						:aria-label="translate('COM_MEDIA_TOGGLE_INFO')">
+					<span class="fas fa-info" aria-hidden="true"></span>
+				</button>
+			</div>
+		</div>
+		<media-breadcrumb></media-breadcrumb>
+	</div>
 </template>
 
 <script>
