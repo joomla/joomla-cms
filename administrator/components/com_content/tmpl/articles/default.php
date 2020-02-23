f8498cb6e9afba0bc0fb4ddb71b12326f9d85abc
Richard Fath <richard67@users.noreply.github.com>
2020-02-23 15:02:33 +0100

    fa -> fas 2

--- administrator/components/com_content/tmpl/articles/default.php
+++ administrator/components/com_content/tmpl/articles/default.php
@@ -216,7 +216,7 @@ HTMLHelper::_('script', 'com_content/admin-articles-workflow-buttons.js', ['rela
 									}
 									?>
 									<span class="sortable-handler<?php echo $iconClass ?>">
-										<span class="fa fa-ellipsis-v" aria-hidden="true"></span>
+										<span class="fas fa-ellipsis-v" aria-hidden="true"></span>
 									</span>
 									<?php if ($canChange && $saveOrder) : ?>
 										<input type="text" name="order[]" size="5" value="<?php echo $item->ordering; ?>" class="width-20 text-area-order hidden">
