0bf9d30046cf9bb3a8c17af3b114a0c90951da75
zero-24 <zero-24@users.noreply.github.com>
2020-03-06 19:40:23 +0100

    sql updates

--- installation/sql/postgresql/joomla.sql
+++ installation/sql/postgresql/joomla.sql
@@ -2294,7 +2294,7 @@ CREATE TABLE IF NOT EXISTS "#__webauthn_credentials"
   "id" varchar(1000) NOT NULL,
   "user_id" varchar(128) NOT NULL,
   "label" varchar(190) NOT NULL,
-  "credential" TEXT  NOT NULL,
+  "credential" text NOT NULL,
   PRIMARY KEY ("id")
 );
 
