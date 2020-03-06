4029029983eb51eb662384fd6f0cdef2beee22ef
SharkyKZ <sharkykz@gmail.com>
2020-03-06 10:41:43 +0200

    Correct subpackage (#28245)

--- plugins/system/webauthn/Webauthn/Helper/CredentialsCreation.php
+++ plugins/system/webauthn/Webauthn/Helper/CredentialsCreation.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package     Joomla.Plugin
- * @subpackage  System.updatenotification
+ * @subpackage  System.Webauthn
  *
  * @copyright   Copyright (C) 2005 - 2019 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
