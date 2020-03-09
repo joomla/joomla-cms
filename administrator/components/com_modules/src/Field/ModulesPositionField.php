f8545c77402c9fd98663f09470835ea100dede8b
infograf768 <infografjms@gmail.com>
2020-03-09 08:15:26 +0100

    [4.0] Fix Module XTD position and type filters by adding client property to these fields (#28259)

--- administrator/components/com_modules/src/Field/ModulesPositionField.php
+++ administrator/components/com_modules/src/Field/ModulesPositionField.php
@@ -11,7 +11,6 @@ namespace Joomla\Component\Modules\Administrator\Field;
 
 defined('JPATH_BASE') or die;
 
-use Joomla\CMS\Factory;
 use Joomla\CMS\Form\Field\ListField;
 use Joomla\Component\Modules\Administrator\Helper\ModulesHelper;
 
@@ -31,6 +30,83 @@ class ModulesPositionField extends ListField
 	protected $type = 'ModulesPosition';
 
 	/**
+	 * Client name.
+	 *
+	 * @var    string
+	 * @since  4.0.0
+	 */
+	protected $client;
+
+	/**
+	 * Method to get certain otherwise inaccessible properties from the form field object.
+	 *
+	 * @param   string  $name  The property name for which to get the value.
+	 *
+	 * @return  mixed  The property value or null.
+	 *
+	 * @since   4.0.0
+	 */
+	public function __get($name)
+	{
+		switch ($name)
+		{
+			case 'client':
+				return $this->$name;
+		}
+
+		return parent::__get($name);
+	}
+
+	/**
+	 * Method to set certain otherwise inaccessible properties of the form field object.
+	 *
+	 * @param   string  $name   The property name for which to set the value.
+	 * @param   mixed   $value  The value of the property.
+	 *
+	 * @return  void
+	 *
+	 * @since   4.0.0
+	 */
+	public function __set($name, $value)
+	{
+		switch ($name)
+		{
+			case 'client':
+				$this->$name = (string) $value;
+				break;
+
+			default:
+				parent::__set($name, $value);
+		}
+	}
+
+	/**
+	 * Method to attach a Form object to the field.
+	 *
+	 * @param   \SimpleXMLElement  $element  The SimpleXMLElement object representing the `<field>` tag for the form field object.
+	 * @param   mixed              $value    The form field value to validate.
+	 * @param   string             $group    The field name group control value. This acts as an array container for the field.
+	 *                                       For example if the field has name="foo" and the group value is set to "bar" then the
+	 *                                       full field name would end up being "bar[foo]".
+	 *
+	 * @return  boolean  True on success.
+	 *
+	 * @see     FormField::setup()
+	 * @since   4.0.0
+	 */
+	public function setup(\SimpleXMLElement $element, $value, $group = null)
+	{
+		$result = parent::setup($element, $value, $group);
+
+		if ($result === true)
+		{
+			$this->client = $this->element['client'] ? (string) $this->element['client'] : 'site';
+		}
+
+		return $result;
+	}
+
+	/**
 	 * Method to get the field options.
 	 *
 	 * @return  array  The field option objects.
@@ -39,7 +115,8 @@ class ModulesPositionField extends ListField
 	 */
 	public function getOptions()
 	{
-		$options = ModulesHelper::getPositions(Factory::getApplication()->getUserState('com_modules.modules.client_id', 0));
+		$clientId = $this->client === 'administrator' ? 1 : 0;
+		$options  = ModulesHelper::getPositions($clientId);
 
 		return array_merge(parent::getOptions(), $options);
 	}
