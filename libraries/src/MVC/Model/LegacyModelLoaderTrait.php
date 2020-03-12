15dd70f6d29eec25eb954eb17774c5d413258dfb
SharkyKZ <sharkykz@gmail.com>
2020-03-12 08:24:46 +0200

    Fix typo

--- libraries/src/MVC/Model/LegacyModelLoaderTrait.php
+++ libraries/src/MVC/Model/LegacyModelLoaderTrait.php
@@ -42,7 +42,7 @@ trait LegacyModelLoaderTrait
 	 * @return  array  An array with directory elements. If prefix is equal to '', all directories are returned.
 	 *
 	 * @since       3.0
-	 * @deprecated  5.0 See LeagcyModelLoaderTrait\getInstance
+	 * @deprecated  5.0 See LegacyModelLoaderTrait\getInstance
 	 */
 	public static function addIncludePath($path = '', $prefix = '')
 	{
