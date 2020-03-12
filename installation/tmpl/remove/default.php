633e096ba7777fe34a5db15445eb3b6456bbccaa
astridx <info@astrid-guenther.de>
2020-03-12 11:22:25 +0100

    ups

--- installation/tmpl/remove/default.php
+++ installation/tmpl/remove/default.php
@@ -183,8 +183,89 @@ use Joomla\CMS\Uri\Uri;
 			</div>
 		</div>
 
-	
-	
+		<fieldset id="installLanguages" class="j-install-step">
+			<legend class="j-install-step-header">
+				<span class="fas fa-comment-dots" aria-hidden="true"></span> <?php echo Text::_('INSTL_LANGUAGES'); ?>
+			</legend>
+			<div class="j-install-step-form">
+				<?php if (!$this->items) : ?>
+				<p><?php echo Text::_('INSTL_LANGUAGES_WARNING_NO_INTERNET'); ?></p>
+				<p>
+					<a href="#"
+							class="btn btn-primary"
+							onclick="return Install.goToPage('remove');">
+						<span class="fas fa-arrow-left icon-white" aria-hidden="true"></span>
+						<?php echo Text::_('INSTL_LANGUAGES_WARNING_BACK_BUTTON'); ?>
+					</a>
+				</p>
+				<p><?php echo Text::_('INSTL_LANGUAGES_WARNING_NO_INTERNET2'); ?></p>
+			<?php else : ?>
+			<form action="index.php" method="post" id="languagesForm" class="form-validate">
+				<p id="wait_installing" class="hidden">
+					<?php echo Text::_('INSTL_LANGUAGES_MESSAGE_PLEASE_WAIT'); ?><br>
+				<div id="wait_installing_spinner" class="spinner spinner-img hidden"></div>
+				</p>
+				<table class="table table-sm">
+				<caption id="install_languages_desc"><?php echo Text::_('INSTL_LANGUAGES_DESC'); ?></caption>
+					<thead>
+					<tr>
+						<td width="1%" class="text-center">
+							&nbsp;
+						</td>
+						<th scope="col">
+							<?php echo Text::_('INSTL_LANGUAGES_COLUMN_HEADER_LANGUAGE'); ?>
+						</th>
+						<th scope="col" width="15%">
+							<?php echo Text::_('INSTL_LANGUAGES_COLUMN_HEADER_LANGUAGE_TAG'); ?>
+						</th>
+						<th scope="col" width="5%" class="text-center">
+							<?php echo Text::_('INSTL_LANGUAGES_COLUMN_HEADER_VERSION'); ?>
+						</th>
+					</tr>
+					</thead>
+					<tbody>
+					<?php $version = new \Joomla\CMS\Version; ?>
+					<?php $currentShortVersion = preg_replace('#^([0-9\.]+)(|.*)$#', '$1', $version->getShortVersion()); ?>
+					<?php foreach ($this->items as $i => $language) : ?>
+						<?php // Get language code and language image. ?>
+						<?php preg_match('#^pkg_([a-z]{2,3}-[A-Z]{2})$#', $language->element, $element); ?>
+						<?php $language->code = $element[1]; ?>
+						<tr>
+							<td>
+								<input type="checkbox" id="cb<?php echo $i; ?>" name="cid[]" value="<?php echo $language->update_id; ?>">
+							</td>
+							<th scope="row">
+								<label for="cb<?php echo $i; ?>"><?php echo $language->name; ?></label>
+							</th>
+							<td>
+								<?php echo $language->code; ?>
+							</td>
+							<td class="text-center">
+								<?php // Display a Note if language pack version is not equal to Joomla version ?>
+								<?php if (substr($language->version, 0, 3) != $version::MAJOR_VERSION . '.' . $version::MINOR_VERSION || substr($language->version, 0, 5) != $currentShortVersion) : ?>
+									<span class="badge badge-warning hasTooltip" title="<?php echo Text::_('JGLOBAL_LANGUAGE_VERSION_NOT_PLATFORM'); ?>"><?php echo $language->version; ?></span>
+								<?php else : ?>
+									<span class="badge badge-success"><?php echo $language->version; ?></span>
+								<?php endif; ?>
+							</td>
+						</tr>
+					<?php endforeach; ?>
+					</tbody>
+				</table>
+				<div class="form-group">
+					<?php echo HTMLHelper::_('form.token'); ?>
+					<?php endif; ?>
+					<button id="installLanguagesButton" class="btn btn-block btn-primary">
+						<?php echo Text::_('JNEXT'); ?>
+					</button>
+					<button id="skipLanguages" class="btn btn-block btn-secondary">
+					<?php echo Text::_('JSKIP'); ?>
+					</button>
+				</div>
+			</form>
+			</div>
+		</fieldset>
+
 		<fieldset id="installFinal" class="j-install-step">
 			<legend class="j-install-step-header">
 				<span class="fab fa-joomla" aria-hidden="true"></span> <?php echo Text::_('INSTL_COMPLETE_FINAL'); ?>
