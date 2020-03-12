2fd1fb55b260ed9c1c07fec7ff0035b377a28ec1
astridx <info@astrid-guenther.de>
2020-03-12 08:58:36 +0100

    show default language while installation wip

--- installation/tmpl/remove/default.php
+++ installation/tmpl/remove/default.php
@@ -27,6 +27,90 @@ use Joomla\CMS\Uri\Uri;
 				<button class="btn btn-primary btn-block" id="installAddFeatures"><?php echo Text::_('INSTL_COMPLETE_ADD_PRECONFIG'); ?> <span class="fas fa-chevron-right" aria-hidden="true"></span></button>
 			</div>
 		</div>
+		<div class="j-install-step-form" id="defaultLanguage">
+		<h2><?php echo Text::_('INSTL_DEFAULTLANGUAGE_ADMINISTRATOR'); ?></h2>		
+		<hr class="hr-condensed" />
+		<p><?php echo JText::_('INSTL_DEFAULTLANGUAGE_DESC'); ?></p>
+		<table class="table table-striped table-condensed">
+			<thead>
+			<tr>
+				<th>
+					<?php echo JText::_('INSTL_DEFAULTLANGUAGE_COLUMN_HEADER_SELECT'); ?>
+				</th>
+				<th>
+					<?php echo JText::_('INSTL_DEFAULTLANGUAGE_COLUMN_HEADER_LANGUAGE'); ?>
+				</th>
+				<th>
+					<?php echo JText::_('INSTL_DEFAULTLANGUAGE_COLUMN_HEADER_TAG'); ?>
+				</th>
+			</tr>
+			</thead>
+			<tbody>
+			<?php foreach ($this->items as $i => $lang) : ?>
+				<tr>
+					<td>
+						<input
+							id="admin-language-cb<?php echo $i; ?>"
+							type="radio"
+							name="administratorlang"
+							value="<?php echo $lang->language; ?>"
+							<?php if ($lang->published) echo 'checked="checked"'; ?>
+						/>
+					</td>
+					<td align="center">
+						<label for="admin-language-cb<?php echo $i; ?>">
+							<?php echo $lang->name; ?>
+						</label>
+					</td>
+					<td align="center">
+						<?php echo $lang->language; ?>
+					</td>
+				</tr>
+			<?php endforeach; ?>
+			</tbody>
+		</table>
+		<h3><?php echo JText::_('INSTL_DEFAULTLANGUAGE_FRONTEND'); ?></h3>
+		<hr class="hr-condensed" />
+		<p><?php echo JText::_('INSTL_DEFAULTLANGUAGE_DESC_FRONTEND'); ?></p>
+		<table class="table table-striped table-condensed">
+			<thead>
+			<tr>
+				<th>
+					<?php echo JText::_('INSTL_DEFAULTLANGUAGE_COLUMN_HEADER_SELECT'); ?>
+				</th>
+				<th>
+					<?php echo JText::_('INSTL_DEFAULTLANGUAGE_COLUMN_HEADER_LANGUAGE'); ?>
+				</th>
+				<th>
+					<?php echo JText::_('INSTL_DEFAULTLANGUAGE_COLUMN_HEADER_TAG'); ?>
+				</th>
+			</tr>
+			</thead>
+			<tbody>
+			<?php foreach ($this->items as $i => $lang) : ?>
+				<tr>
+					<td>
+						<input
+							id="site-language-cb<?php echo $i; ?>"
+							type="radio"
+							name="frontendlang"
+							value="<?php echo $lang->language; ?>"
+							<?php if ($lang->published) echo 'checked="checked"'; ?>
+						/>
+					</td>
+					<td align="center">
+						<label for="site-language-cb<?php echo $i; ?>">
+							<?php echo $lang->name; ?>
+						</label>
+					</td>
+					<td align="center">
+						<?php echo $lang->language; ?>
+					</td>
+			</tr>
+		<?php endforeach; ?>
+		</tbody>
+	</table>
+	</div>
 	</fieldset>
 
 		<div id="installRecommended" class="j-install-step active">
