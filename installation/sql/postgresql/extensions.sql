dca50989fc4f94045be7a48937fedf12ab274304
Hannes Papenberg <info@joomlager.de>
2020-03-15 20:47:55 +0100

    Fixing Postgres SQL

--- installation/sql/postgresql/extensions.sql
+++ installation/sql/postgresql/extensions.sql
@@ -613,68 +613,6 @@ CREATE TABLE IF NOT EXISTS "#__finder_types" (
 );
 
 --
--- Here is SOUNDEX replacement for those who can't enable fuzzystrmatch module
---   from contrib folder.
--- This function comes from https://wiki.postgresql.org/wiki/Soundex
---   and is distributed with GPL license.
--- Thanks to its author, Marti Raudsepp, that published this piece of code.
---
-
-CREATE OR REPLACE FUNCTION soundex(input text) RETURNS text
-IMMUTABLE STRICT COST 500 LANGUAGE plpgsql
-AS $$
-DECLARE
-  soundex text = '';
-  char text;
-  symbol text;
-  last_symbol text = '';
-  pos int = 1;
-BEGIN
-  WHILE length(soundex) < 4 LOOP
-    char = upper(substr(input, pos, 1));
-    pos = pos + 1;
-    CASE char
-    WHEN '' THEN
-      -- End of input string
-      IF soundex = '' THEN
-        RETURN '';
-      ELSE
-        RETURN rpad(soundex, 4, '0');
-      END IF;
-    WHEN 'B', 'F', 'P', 'V' THEN
-      symbol = '1';
-    WHEN 'C', 'G', 'J', 'K', 'Q', 'S', 'X', 'Z' THEN
-      symbol = '2';
-    WHEN 'D', 'T' THEN
-      symbol = '3';
-    WHEN 'L' THEN
-      symbol = '4';
-    WHEN 'M', 'N' THEN
-      symbol = '5';
-    WHEN 'R' THEN
-      symbol = '6';
-    ELSE
-      -- Not a consonant; no output, but next similar consonant will be re-recorded
-      symbol = '';
-    END CASE;
-
-    IF soundex = '' THEN
-      -- First character; only accept strictly English ASCII characters
-      IF char ~>=~ 'A' AND char ~<=~ 'Z' THEN
-        soundex = char;
-        last_symbol = symbol;
-      END IF;
-    ELSIF last_symbol != symbol THEN
-      soundex = soundex || symbol;
-      last_symbol = symbol;
-    END IF;
-  END LOOP;
-
-  RETURN soundex;
-END;
-$$;
-
---
 -- Table structure for table `#__messages`
 --
 
@@ -906,3 +844,66 @@ CREATE TABLE "#__action_logs_users" (
 );
 
 CREATE INDEX "#__action_logs_users_idx_notify" ON "#__action_logs_users" ("notify");
+
+--
+-- Here is SOUNDEX replacement for those who can't enable fuzzystrmatch module
+--   from contrib folder.
+-- This function comes from https://wiki.postgresql.org/wiki/Soundex
+--   and is distributed with GPL license.
+-- Thanks to its author, Marti Raudsepp, that published this piece of code.
+--
+
+CREATE OR REPLACE FUNCTION soundex(input text) RETURNS text
+IMMUTABLE STRICT COST 500 LANGUAGE plpgsql
+AS $$
+DECLARE
+  soundex text = '';
+  char text;
+  symbol text;
+  last_symbol text = '';
+  pos int = 1;
+BEGIN
+  WHILE length(soundex) < 4 LOOP
+    char = upper(substr(input, pos, 1));
+    pos = pos + 1;
+    CASE char
+    WHEN '' THEN
+      -- End of input string
+      IF soundex = '' THEN
+        RETURN '';
+      ELSE
+        RETURN rpad(soundex, 4, '0');
+      END IF;
+    WHEN 'B', 'F', 'P', 'V' THEN
+      symbol = '1';
+    WHEN 'C', 'G', 'J', 'K', 'Q', 'S', 'X', 'Z' THEN
+      symbol = '2';
+    WHEN 'D', 'T' THEN
+      symbol = '3';
+    WHEN 'L' THEN
+      symbol = '4';
+    WHEN 'M', 'N' THEN
+      symbol = '5';
+    WHEN 'R' THEN
+      symbol = '6';
+    ELSE
+      -- Not a consonant; no output, but next similar consonant will be re-recorded
+      symbol = '';
+    END CASE;
+
+    IF soundex = '' THEN
+      -- First character; only accept strictly English ASCII characters
+      IF char ~>=~ 'A' AND char ~<=~ 'Z' THEN
+        soundex = char;
+        last_symbol = symbol;
+      END IF;
+    ELSIF last_symbol != symbol THEN
+      soundex = soundex || symbol;
+      last_symbol = symbol;
+    END IF;
+  END LOOP;
+
+  RETURN soundex;
+END;
+$$;
+
