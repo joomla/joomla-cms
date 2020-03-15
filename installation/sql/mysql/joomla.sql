381f2425f465e2cf22ca6e65704c8ac1f071ef3f
Tobias Zulauf <zero-24@users.noreply.github.com>
2020-03-15 15:25:17 +0100

    Update installation/sql/mysql/joomla.sql
    Co-Authored-By: Nicola Galgano <optimus4joomla@gmail.com>

--- installation/sql/mysql/joomla.sql
+++ installation/sql/mysql/joomla.sql
@@ -2291,7 +2291,7 @@ CREATE TABLE IF NOT EXISTS `#__webauthn_credentials` (
   `user_id` VARCHAR(128) NOT NULL COMMENT 'User handle',
   `label` VARCHAR(190) NOT NULL COMMENT 'Human readable label',
   `credential` MEDIUMTEXT NOT NULL COMMENT 'Credential source data, JSON format',
-  PRIMARY KEY (`id`),
+   PRIMARY KEY (`id`(100)),
   INDEX (`user_id`(100))
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 DEFAULT COLLATE=utf8mb4_unicode_ci;
 
