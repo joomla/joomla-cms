8cf990821bf3e8328ea4fe03067a064d81e36b56
George Wilson <georgejameswilson@googlemail.com>
2020-03-16 14:13:03 +0000

    Cleanup formvalidation behaviour (#28352)

--- installation/tmpl/preinstall/default.php
+++ installation/tmpl/preinstall/default.php
@@ -11,6 +11,8 @@ defined('_JEXEC') or die;
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
 
+HTMLHelper::_('behavior.formvalidator');
+
 /** @var \Joomla\CMS\Installation\View\Preinstall\HtmlView $this */
 ?>
 <div id="installer-view" class="container" data-page-name="preinstall">
