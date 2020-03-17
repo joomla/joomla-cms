d295f5b9e305dd8eb1ca83cd4dbd4c072ad44028
George Wilson <georgejameswilson@googlemail.com>
2020-03-17 12:58:05 +0000

    Fix return type in controller

--- components/com_newsfeeds/src/Controller/DisplayController.php
+++ components/com_newsfeeds/src/Controller/DisplayController.php
@@ -27,7 +27,7 @@ class DisplayController extends BaseController
 	 * @param   boolean  $cachable   If true, the view output will be cached
 	 * @param   array    $urlparams  An array of safe URL parameters and their variable types, for valid values see {@link \JFilterInput::clean()}.
 	 *
-	 * @return  \JController		This object to support chaining.
+	 * @return  static  This object to support chaining.
 	 *
 	 * @since   1.5
 	 */
@@ -47,6 +47,6 @@ class DisplayController extends BaseController
 		$safeurlparams = array('id' => 'INT', 'limit' => 'UINT', 'limitstart' => 'UINT',
 								'filter_order' => 'CMD', 'filter_order_Dir' => 'CMD', 'lang' => 'CMD');
 
-		parent::display($cachable, $safeurlparams);
+		return parent::display($cachable, $safeurlparams);
 	}
 }
