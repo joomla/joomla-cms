51eb86db2020e577f31a85c97dc3cd701eb5e58d
Fedik <getthesite@gmail.com>
2020-03-21 12:35:26 +0200

    template.system site

--- administrator/templates/system/error.php
+++ administrator/templates/system/error.php
@@ -15,7 +15,7 @@ use Joomla\CMS\Router\Route;
 /** @var Joomla\CMS\Document\ErrorDocument $this */
 
 // Load template CSS file
-$this->getWebAssetManager()->registerAndUseStyle('template.system.error', 'error.css');
+$this->getWebAssetManager()->registerAndUseStyle('template.system.error', 'administrator/templates/system/css/error.css');
 
 // Set page title
 $this->setTitle($this->error->getCode() . ' - ' . htmlspecialchars($this->error->getMessage(), ENT_QUOTES, 'UTF-8'));
