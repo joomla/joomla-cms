9a6b540be3c9fb3abe2faac7277a62aa59d81237
Fedik <getthesite@gmail.com>
2020-03-22 11:06:01 +0200

    run 1

--- layouts/plugins/system/webauthn/manage.php
+++ layouts/plugins/system/webauthn/manage.php
@@ -10,7 +10,6 @@
 defined('JPATH_BASE') or die;
 
 use Joomla\CMS\Factory;
-use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\Layout\FileLayout;
 use Joomla\CMS\Uri\Uri;
@@ -41,6 +40,9 @@ try
 {
 	$app          = Factory::getApplication();
 	$loggedInUser = $app->getIdentity();
+
+	$app->getDocument()->getWebAssetManager()
+		->registerAndUseStyle('plg_system_webauthn.backend', 'plg_system_webauthn/backend.css');
 }
 catch (Exception $e)
 {
@@ -54,8 +56,6 @@ $defaultDisplayData = [
 	'error'       => '',
 ];
 extract(array_merge($defaultDisplayData, $displayData));
-
-HTMLHelper::_('stylesheet', 'plg_system_webauthn/backend.css', ['relative' => true]);
 
 /**
  * Why not push these configuration variables directly to JavaScript?
