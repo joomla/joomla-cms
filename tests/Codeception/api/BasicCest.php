0c0a88f64670ec159e0bd79f0cffad3594e5d8dc
wilsonge <georgejameswilson@googlemail.com>
2020-03-22 14:50:06 +0000

    Fix usergroup table too

--- tests/Codeception/api/BasicCest.php
+++ tests/Codeception/api/BasicCest.php
@@ -30,6 +30,7 @@ class BasicCest
 	{
 		$desiredUserId = 3;
 		$I->updateInDatabase('users', ['id' => 3], []);
+		$I->updateInDatabase('user_usergroup_map', ['user_id' => 3], []);
 		$enabledData = ['user_id' => $desiredUserId, 'profile_key' => 'joomlatoken.enabled', 'profile_value' => 1];
 		$tokenData = ['user_id' => $desiredUserId, 'profile_key' => 'joomlatoken.token', 'profile_value' => 'dOi2m1NRrnBHlhaWK/WWxh3B5tqq1INbdf4DhUmYTI4='];
 		$I->haveInDatabase('user_profiles', $enabledData);
