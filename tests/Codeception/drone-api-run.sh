b7d90e0f69e9ec5859b81886c47e7436267d4eae
wilsonge <georgejameswilson@googlemail.com>
2020-03-22 13:39:06 +0000

    Add constant site secret for API tests

--- tests/Codeception/drone-api-run.sh
+++ tests/Codeception/drone-api-run.sh
@@ -56,5 +56,9 @@ echo "[RUNNER] Run Codeception"
 php libraries/vendor/bin/codecept build
 php libraries/vendor/bin/codecept run --fail-fast --steps --debug --env mysql tests/Codeception/acceptance/01-install/
 
+# TODO: This sed command doesn't work on OSX - can we rewrite it into something that does work? Good enough for the
+#       linux docker container for now.
+sed -i "/\$secret/c\	public \$secret = 'tEstValue';" /tests/www/test-install/configuration.php
+
 # Executing API tests
-libraries/vendor/bin/codecept run api --fail-fast --steps --debug
+php libraries/vendor/bin/codecept run api --fail-fast --steps --debug
