319ef82c52bda3fcf5a57bfb0486153f7fc1386f
Brian Teeman <brian@teeman.net>
2020-03-25 12:37:08 +0000

    [4.0] Select Modules page (#27361)

--- administrator/components/com_modules/tmpl/select/default.php
+++ administrator/components/com_modules/tmpl/select/default.php
@@ -22,19 +22,31 @@ if ($function) :
 	HTMLHelper::_('script', 'com_modules/admin-select-modal.js', ['version' => 'auto', 'relative' => true]);
 endif;
 ?>
-
 <h2 class="mb-3"><?php echo Text::_('COM_MODULES_TYPE_CHOOSE'); ?></h2>
-<ul id="new-modules-list" class="list-group">
-<?php foreach ($this->items as &$item) : ?>
-	<?php // Prepare variables for the link. ?>
-	<?php $link = 'index.php?option=com_modules&task=module.add&client_id=' . $this->state->get('client_id', 0) . $this->modalLink . '&eid=' . $item->extension_id; ?>
-	<?php $name = $this->escape($item->name); ?>
-	<?php $desc = HTMLHelper::_('string.truncate', $this->escape(strip_tags($item->desc)), 200); ?>
+<div id="new-modules-list">
+	<div class="new-modules">
+		<div class="card-columns">
+			<?php foreach ($this->items as &$item) : ?>
+				<div class="card mb-4">
+					<?php // Prepare variables for the link. ?>
+					<?php $link = 'index.php?option=com_modules&task=module.add&client_id=' . $this->state->get('client_id', 0) . $this->modalLink . '&eid=' . $item->extension_id; ?>
+					<?php $name = $this->escape($item->name); ?>
+					<?php $desc = HTMLHelper::_('string.truncate', $this->escape(strip_tags($item->desc)), 200); ?>
 
-	<li class="list-group-item">
-		<a href="<?php echo Route::_($link); ?>" class="mr-2<?php echo $function ? ' select-link" data-function="' . $this->escape($function) : ''; ?>">
-			<strong><?php echo $name; ?></strong></a>
-		<small><?php echo $desc; ?></small>
-	</li>
-<?php endforeach; ?>
-</ul>
+					<div class="card-header">
+						<h3><?php echo $name; ?></h3>
+					</div>
+
+					<div class="card-body">
+						<p class="text-muted">
+							<?php echo $desc; ?>
+						</p>
+					</div>
+					<a href="<?php echo Route::_($link); ?>" class="btn btn-primary <?php echo $function ? ' select-link" data-function="' . $this->escape($function) : ''; ?>" aria-label="<?php echo Text::sprintf('COM_MODULES_SELECT_MODULE', $name); ?>">
+						<?php echo Text::_('JSELECT'); ?>
+					</a>
+				</div>
+			<?php endforeach; ?>
+		</div>
+	</div>
+</div>
