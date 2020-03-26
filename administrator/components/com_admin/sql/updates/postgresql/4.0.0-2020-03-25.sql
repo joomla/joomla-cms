08c1bef09246c85e7e55e03d6a53c458928fce86
Richard Fath <richard67@users.noreply.github.com>
2020-03-26 16:04:06 +0100

    Fix PostgreSQL installation error and so also system-test-postgresql in Drone and update to 4.0-dev (#1)
    * [RTL] Fix position of close button in modal
    
    * Fix error with PostgreSQL installation
    
    * Adjust update sql to latest components and modules as in joomla.sql
    
    * Adjust update sql to latest plugins as in joomla.sql
    
    * SQL CS - use spaces in IN lists
    
    * Revert unrelated scss change
    
    Co-authored-by: wilsonge <georgejameswilson@googlemail.com>

--- administrator/components/com_admin/sql/updates/postgresql/4.0.0-2020-03-25.sql
+++ administrator/components/com_admin/sql/updates/postgresql/4.0.0-2020-03-25.sql
@@ -37,9 +37,14 @@ WHERE ("type" = 'component' AND "element" IN (
 	'com_ajax',
 	'com_postinstall',
 	'com_fields',
-	'com_associations'
+	'com_associations',
+	'com_privacy',
+	'com_actionlogs',
+	'com_workflow',
+	'com_csp',
+	'com_mails'
 ))
-OR ("type" = 'module' AND "element" IN (
+OR ("type" = 'module' AND "client_id" = 0 AND "element" IN (
 	'mod_articles_archive',
 	'mod_articles_latest',
 	'mod_articles_popular',
@@ -61,17 +66,22 @@ OR ("type" = 'module' AND "element" IN (
 	'mod_articles_category',
 	'mod_articles_categories',
 	'mod_languages',
-	'mod_finder',
+	'mod_finder'
+))
+OR ("type" = 'module' AND "client_id" = 1 AND "element" IN (
 	'mod_custom',
 	'mod_feed',
 	'mod_latest',
 	'mod_logged',
 	'mod_login',
+	'mod_loginsupport',
 	'mod_menu',
 	'mod_popular',
 	'mod_quickicon',
-	'mod_status',
-	'mod_submenu',
+	'mod_frontend',
+	'mod_messages',
+	'mod_post_installation_messages',
+	'mod_user',
 	'mod_title',
 	'mod_toolbar',
 	'mod_multilangstatus',
@@ -79,24 +89,35 @@ OR ("type" = 'module' AND "element" IN (
 	'mod_stats_admin',
 	'mod_tags_popular',
 	'mod_tags_similar',
-	'mod_sampledata'
+	'mod_sampledata',
+	'mod_latestactions',
+	'mod_privacy_dashboard',
+	'mod_submenu',
+	'mod_privacy_status'
 ))
 OR ("type" = 'plugin' AND
 	(
-		("folder" = 'system' AND "element" IN ('languagefilter', 'p3p', 'cache', 'debug', 'log', 'redirect', 'remember', 'sef', 'logout', 'languagecode', 'highlight', 'updatenotification', 'stats', 'fields', 'sessiongc'))
-		OR ("folder" = 'content' AND "element" IN ('contact', 'emailcloak', 'loadmodule', 'pagebreak', 'pagenavigation', 'vote', 'joomla', 'finder', 'fields'))
-		OR ("folder" = 'user' AND "element" IN ('contactcreator', 'joomla', 'profile'))
-		OR ("folder" = 'quickicon' AND "element" IN ('joomlaupdate', 'extensionupdate', 'phpversioncheck'))
+		("folder" = 'actionlog' AND "element" IN ('joomla'))
+		OR ("folder" = 'api-authentication' AND "element" IN ('basic', 'token'))
+		OR ("folder" = 'authentication' AND "element" IN ('cookie', 'joomla', 'ldap'))
+		OR ("folder" = 'behaviour' AND "element" IN ('taggable', 'versionable'))
+		OR ("folder" = 'captcha' AND "element" IN ('recaptcha', 'recaptcha_invisible'))
+		OR ("folder" = 'content' AND "element" IN ('confirmconsent', 'contact', 'emailcloak', 'fields', 'finder', 'joomla', 'loadmodule', 'pagebreak', 'pagenavigation', 'vote'))
 		OR ("folder" = 'editors' AND "element" IN ('codemirror', 'none', 'tinymce'))
-		OR ("folder" = 'editors-xtd' AND "element" IN ('article', 'image', 'pagebreak', 'readmore', 'module', 'menu', 'contact', 'fields'))
-		OR ("folder" = 'authentication' AND "element" IN ('gmail', 'joomla', 'ldap', 'cookie'))
-		OR ("folder" = 'twofactorauth' AND "element" IN ('totp', 'yubikey'))
-		OR ("folder" = 'installer' AND "element" IN ('packageinstaller', 'folderinstaller', 'urlinstaller'))
-		OR ("folder" = 'extension' AND "element" IN ('joomla'))
-		OR ("folder" = 'captcha' AND "element" IN ('recaptcha'))
+		OR ("folder" = 'editors-xtd' AND "element" IN ('article', 'contact', 'fields', 'image', 'menu', 'module', 'pagebreak', 'readmore'))
+		OR ("folder" = 'extension' AND "element" IN ('finder', 'joomla', 'namespacemap'))
+		OR ("folder" = 'fields' AND "element" IN ('calendar', 'checkboxes', 'color', 'editor', 'imagelist', 'integer', 'list', 'media', 'radio', 'sql', 'subfields', 'text', 'textarea', 'url', 'user', 'usergrouplist'))
+		OR ("folder" = 'filesystem' AND "element" IN ('local'))
 		OR ("folder" = 'finder' AND "element" IN ('categories', 'contacts', 'content', 'newsfeeds', 'tags'))
-		OR ("folder" = 'fields' AND "element" IN ('calendar', 'checkboxes', 'color', 'editor', 'imagelist', 'integer', 'list', 'media', 'radio', 'sql', 'text', 'textarea', 'url', 'user', 'usergrouplist', 'repeatable'))
-		OR ("folder" = 'sampledata' AND "element" IN ('blog'))
+		OR ("folder" = 'installer' AND "element" IN ('folderinstaller', 'override', 'packageinstaller', 'urlinstaller', 'webinstaller'))
+		OR ("folder" = 'media-action' AND "element" IN ('crop', 'resize', 'rotate'))
+		OR ("folder" = 'privacy' AND "element" IN ('actionlogs', 'consents', 'contact', 'content', 'message', 'user'))
+		OR ("folder" = 'quickicon' AND "element" IN ('downloadkey', 'extensionupdate', 'joomlaupdate', 'overridecheck', 'phpversioncheck', 'privacycheck'))
+		OR ("folder" = 'sampledata' AND "element" IN ('blog', 'multilang', 'testing'))
+		OR ("folder" = 'system' AND "element" IN ('accessibility', 'actionlogs', 'cache', 'debug', 'fields', 'highlight', 'httpheaders', 'languagecode', 'languagefilter', 'log', 'logout', 'logrotation', 'privacyconsent', 'redirect', 'remember', 'sef', 'sessiongc', 'skipto', 'stats', 'updatenotification', 'webauthn'))
+		OR ("folder" = 'twofactorauth' AND "element" IN ('totp', 'yubikey'))
+		OR ("folder" = 'user' AND "element" IN ('contactcreator', 'joomla', 'profile', 'terms', 'token'))
+		OR ("folder" = 'webservices' AND "element" IN ('banners', 'config', 'contact', 'content', 'languages', 'menus', 'messages', 'modules', 'newsfeeds', 'plugins', 'privacy', 'redirect', 'tags', 'templates', 'users'))
 	)
 )
 OR ("type" = 'library' AND "element" IN ('joomla', 'phpass'))
