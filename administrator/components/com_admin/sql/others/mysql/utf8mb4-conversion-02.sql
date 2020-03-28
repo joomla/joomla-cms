6dc07f28c2ddcbfe632351a40c82c080f66d2b2c
Richard Fath <richard67@users.noreply.github.com>
2020-03-28 21:07:31 +0100

    Remove trailing white space

--- administrator/components/com_admin/sql/others/mysql/utf8mb4-conversion-02.sql
+++ administrator/components/com_admin/sql/others/mysql/utf8mb4-conversion-02.sql
@@ -48,7 +48,7 @@ ALTER TABLE `#__users` MODIFY `name` varchar(400) NOT NULL DEFAULT '';
 --
 -- Step 2.2: Convert all tables to utf8mb4 chracter set with utf8mb4_unicode_ci collation
 -- except #__finder_xxx tables, those will have utf8mb4_general_ci collation.
--- Note: The database driver for mysql will change utf8mb4 to utf8 if utf8mb4 is not supported     
+-- Note: The database driver for mysql will change utf8mb4 to utf8 if utf8mb4 is not supported
 --
 
 ALTER TABLE `#__action_logs` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
