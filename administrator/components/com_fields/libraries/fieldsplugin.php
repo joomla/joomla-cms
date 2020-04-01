5f99b673fedcca84d7db59a992a02b38cc7e5510
Allon Moritz <allon.moritz@digital-peak.com>
2020-04-01 07:26:35 +0200

    Update administrator/components/com_fields/libraries/fieldsplugin.php
    Co-Authored-By: Quy <quy@fluxbb.org>

--- administrator/components/com_fields/libraries/fieldsplugin.php
+++ administrator/components/com_fields/libraries/fieldsplugin.php
@@ -123,6 +123,7 @@ abstract class FieldsPlugin extends JPlugin
 
 		// Get the path for the layout file
 		$path = JPluginHelper::getLayoutPath('fields', $field->type, $field->type);
+
 		if (!file_exists($path))
 		{
 			$path = JPluginHelper::getLayoutPath('fields', $this->_name, $field->type);
