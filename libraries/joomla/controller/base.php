10665cdf9b4d0e5d47ff791a2bd0c4e036fe221a
Hannes Papenberg <info@joomlager.de>
2020-04-02 08:24:15 +0200

    Deprecating "new" MVC for 4.0 instead of 5.0 (#28366)

--- libraries/joomla/controller/base.php
+++ libraries/joomla/controller/base.php
@@ -15,7 +15,7 @@ use Joomla\Application\AbstractApplication;
  * Joomla Platform Base Controller Class
  *
  * @since       3.0.0
- * @deprecated  5.0 Use the default MVC library
+ * @deprecated  4.0 Use the default MVC library
  */
 abstract class JControllerBase implements JController
 {
