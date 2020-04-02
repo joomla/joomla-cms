ae5275283c6b3ae01918049dfbd8080785e99b1d
George Wilson <georgejameswilson@googlemail.com>
2020-04-02 10:17:53 +0100

    Fix doc block

--- libraries/src/MVC/Controller/AdminController.php
+++ libraries/src/MVC/Controller/AdminController.php
@@ -57,12 +57,12 @@ class AdminController extends BaseController
 	/**
 	 * Constructor.
 	 *
-	 * @param   array                $config   An optional associative array of configuration settings.
+	 * @param   array                       $config   An optional associative array of configuration settings.
 	 * Recognized key values include 'name', 'default_task', 'model_path', and
 	 * 'view_path' (this list is not meant to be comprehensive).
-	 * @param   MVCFactoryInterface  $factory  The factory.
-	 * @param   CMSApplication       $app      The JApplication for the dispatcher
-	 * @param   Input                $input    Input
+	 * @param   MVCFactoryInterface         $factory  The factory.
+	 * @param   CMSWebApplicationInterface  $app      The JApplication for the dispatcher
+	 * @param   Input                       $input    Input
 	 *
 	 * @since   3.0
 	 */
