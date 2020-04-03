a4e42ecea9fce7816e13ba61b1805750cc52f804
brian teeman <brian@teeman.net>
2020-04-03 23:06:18 +0100

    [4.0] webauthn subpackage
    Simple PR to correct a copy paste error with the @subpackage naame
    
    code review only needed

--- build/media_source/plg_system_webauthn/js/management.es6.js
+++ build/media_source/plg_system_webauthn/js/management.es6.js
@@ -1,6 +1,6 @@
 /**
  * @package     Joomla.Plugin
- * @subpackage  System.updatenotification
+ * @subpackage  System.webauthn
  *
  * @copyright   Copyright (C) 2005 - 2019 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
