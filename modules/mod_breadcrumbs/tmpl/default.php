3f20f298206ca1a7abec1e7d89ffc388268b3709
fertxunonimo <61228083+fertxunonimo@users.noreply.github.com>
2020-04-04 14:46:16 +0200

    Update default.php (#27987)
    Undefined property name changed to title

--- modules/mod_breadcrumbs/tmpl/default.php
+++ modules/mod_breadcrumbs/tmpl/default.php
@@ -8,19 +8,16 @@
  */
 
 defined('_JEXEC') or die;
-
-use Joomla\CMS\Language\Text;
-
 ?>
-<nav role="navigation" aria-label="<?php echo $module->name; ?>">
-	<ol itemscope itemtype="https://schema.org/BreadcrumbList" class="mod-breadcrumbs breadcrumb">
+<div aria-label="<?php echo $module->title; ?>" role="navigation">
+	<ul itemscope itemtype="https://schema.org/BreadcrumbList" class="breadcrumb<?php echo $moduleclass_sfx; ?>">
 		<?php if ($params->get('showHere', 1)) : ?>
-			<li class="mod-breadcrumbs__here float-left">
-				<?php echo Text::_('MOD_BREADCRUMBS_HERE'); ?>&#160;
+			<li>
+				<?php echo JText::_('MOD_BREADCRUMBS_HERE'); ?>&#160;
 			</li>
 		<?php else : ?>
-			<li class="mod-breadcrumbs__divider float-left">
-				<span class="divider fas fa-location" aria-hidden="true"></span>
+			<li class="active">
+				<span class="divider icon-location"></span>
 			</li>
 		<?php endif; ?>
 
@@ -46,22 +43,32 @@ use Joomla\CMS\Language\Text;
 		// Generate the trail
 		foreach ($list as $key => $item) :
 			if ($key !== $last_item_key) :
-				if (!empty($item->link)) :
-					$breadcrumbItem = '<a itemprop="item" href="' . $item->link . '" class="pathway"><span itemprop="name">' . $item->name . '</span></a>';
-				else :
-					$breadcrumbItem = '<span itemprop="name">' . $item->name . '</span>';
-				endif;
 				// Render all but last item - along with separator ?>
-				<li itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem" class="mod-breadcrumbs__item breadcrumb-item"><?php echo $breadcrumbItem; ?>
+				<li itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
+					<?php if (!empty($item->link)) : ?>
+						<a itemprop="item" href="<?php echo $item->link; ?>" class="pathway"><span itemprop="name"><?php echo $item->name; ?></span></a>
+					<?php else : ?>
+						<span itemprop="name">
+							<?php echo $item->name; ?>
+						</span>
+					<?php endif; ?>
+
+					<?php if (($key !== $penult_item_key) || $show_last) : ?>
+						<span class="divider">
+							<?php echo $separator; ?>
+						</span>
+					<?php endif; ?>
 					<meta itemprop="position" content="<?php echo $key + 1; ?>">
 				</li>
 			<?php elseif ($show_last) :
-				$breadcrumbItem = '<span itemprop="name">' . $item->name . '</span>';
 				// Render last item if reqd. ?>
-				<li aria-current="page" itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem" class="mod-breadcrumbs__item breadcrumb-item active"><?php echo $breadcrumbItem; ?>
+				<li itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem" class="active">
+					<span itemprop="name">
+						<?php echo $item->name; ?>
+					</span>
 					<meta itemprop="position" content="<?php echo $key + 1; ?>">
 				</li>
 			<?php endif;
 		endforeach; ?>
-	</ol>
-</nav>
+	</ul>
+</div>
