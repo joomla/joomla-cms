890c30ae0c916013cdcd73abf084fe033e716d07
Richard Fath <richard67@users.noreply.github.com>
2020-04-07 09:34:05 +0200

    Dummy change to trigger drone

--- administrator/components/com_admin/sql/updates/mysql/4.0.0-2018-07-29.sql
+++ administrator/components/com_admin/sql/updates/mysql/4.0.0-2018-07-29.sql
@@ -1,3 +1,4 @@
+-- Dummy change to trigger drone.
 INSERT INTO `#__extensions` (`package_id`, `name`, `type`, `element`, `folder`, `client_id`, `enabled`, `access`, `protected`, `manifest_cache`, `params`, `checked_out`, `checked_out_time`, `ordering`, `state`) VALUES
 (0, 'plg_extension_finder', 'plugin', 'finder', 'extension', 0, 1, 1, 0, '', '', 0, '0000-00-00 00:00:00', 0, 0);
 
