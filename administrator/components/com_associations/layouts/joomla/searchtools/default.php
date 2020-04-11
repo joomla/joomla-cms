2ae2cce5b4d0d59e6274fa7d7e0ec0e201b98748
Ciaran Walsh <ciaran@joomla51.com>
2020-04-11 22:05:12 +0100

    [4.0][Atum] Some searchtool fixes (#28644)

--- administrator/components/com_associations/layouts/joomla/searchtools/default.php
+++ administrator/components/com_associations/layouts/joomla/searchtools/default.php
@@ -84,12 +84,12 @@ $filtersActiveClass = $hideActiveFilters ? '' : ' js-stools-container-filters-vi
 // Load search tools
 HTMLHelper::_('searchtools.form', $data['options']['formSelector'], $data['options']);
 ?>
-<div class="js-stools d-flex flex-wrap" role="search">
+<div class="js-stools" role="search">
 	<?php // Add the itemtype and language selectors before the form filters. Do not display in modal. ?>
 	<?php $app = Factory::getApplication(); ?>
 	<?php if ($app->input->get('forcedItemType', '', 'string') == '') : ?>
 		<?php $itemTypeField = $data['view']->filterForm->getField('itemtype'); ?>
-		<div class="js-stools-container-selector-first">
+		<div class="js-stools-container-selector">
 			<div class="js-stools-field-selector js-stools-itemtype">
 				<div class="sr-only"><?php echo $itemTypeField->label; ?></div>
 				<?php echo $itemTypeField->input; ?>
