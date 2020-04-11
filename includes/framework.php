b34801245aa113f392cdb13b509fb883015ea504
Fedik <getthesite@gmail.com>
2020-04-11 11:36:41 +0300

    Remove the option, and replace the value in a model

--- includes/framework.php
+++ includes/framework.php
@@ -61,7 +61,7 @@ switch ($config->error_reporting)
 		break;
 
 	case 'maximum':
-	case 'development': // <= Stays for backward compatibility, can be removed in 5.0
+	case 'development': // <= Stays for backward compatibility, @TODO: can be removed in 5.0
 		error_reporting(E_ALL);
 		ini_set('display_errors', 1);
 
