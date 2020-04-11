3c98c7ca49815b63d9110b1741fce4a20602076f
Brian Teeman <brian@teeman.net>
2020-04-11 16:03:44 +0100

    [4.0] Associations warning translateable (#28650)
    Create a multilingual site
    Change a menu item from language all to any other language
    You will see an alert as below with a non translated warning
    
    Apply the pr and try again

--- layouts/joomla/edit/associations.php
+++ layouts/joomla/edit/associations.php
@@ -20,8 +20,15 @@ $options  = array(
 );
 
 HTMLHelper::_('behavior.core');
+
+// Load JavaScript message titles
+Text::script('ERROR');
+Text::script('WARNING');
+Text::script('NOTICE');
+Text::script('MESSAGE');
 Text::script('JGLOBAL_ASSOC_NOT_POSSIBLE');
 Text::script('JGLOBAL_ASSOCIATIONS_RESET_WARNING');
+
 Factory::getDocument()->addScriptOptions('system.associations.edit', $options);
 HTMLHelper::_('script', 'com_associations/associations-edit.min.js', array('version' => 'auto', 'relative' => true));
 
