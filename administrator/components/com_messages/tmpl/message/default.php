201aa14995a59028e9e018382eb86084cc5a83c7
Brian Teeman <brian@teeman.net>
2020-04-11 22:04:05 +0100

    [4.0] Improve styling of Private Message view (#28646)

--- administrator/components/com_messages/tmpl/message/default.php
+++ administrator/components/com_messages/tmpl/message/default.php
@@ -16,25 +16,49 @@ use Joomla\CMS\Router\Route;
 HTMLHelper::_('behavior.core');
 ?>
 <form action="<?php echo Route::_('index.php?option=com_messages'); ?>" method="post" name="adminForm" id="adminForm">
-	<fieldset>
-		<div class="form-group">
-			<?php echo Text::_('COM_MESSAGES_FIELD_USER_ID_FROM_LABEL'); ?>
-			<?php echo $this->item->get('from_user_name'); ?>
+	<div class="row mt-2">
+		<div class="col-md-9">
+			<div class="card">
+				<div class="card-body">
+					<fieldset>
+						<div class="form-group">
+							<div class="control-label">
+								<?php echo Text::_('COM_MESSAGES_FIELD_USER_ID_FROM_LABEL'); ?>
+							</div>
+							<div class="form-control" readonly>
+								<?php echo $this->item->get('from_user_name'); ?>
+							</div>
+						</div>
+						<div class="form-group">
+							<div class="control-label">
+								<?php echo Text::_('COM_MESSAGES_FIELD_DATE_TIME_LABEL'); ?>
+							</div>
+							<div class="form-control" readonly>
+								<?php echo HTMLHelper::_('date', $this->item->date_time, Text::_('DATE_FORMAT_LC2')); ?>
+							</div>
+						</div>
+						<div class="form-group">
+							<div class="control-label">
+								<?php echo Text::_('COM_MESSAGES_FIELD_SUBJECT_LABEL'); ?>
+							</div>
+							<div class="form-control" readonly>
+								<?php echo $this->item->subject; ?>
+							</div>
+						</div>
+						<div class="form-group">
+							<div class="control-label">
+								<?php echo Text::_('COM_MESSAGES_FIELD_MESSAGE_LABEL'); ?>
+							</div>
+							<div class="form-control" readonly>
+								<?php echo $this->item->message; ?>
+							</div>
+						</div>
+						<input type="hidden" name="task" value="">
+						<input type="hidden" name="reply_id" value="<?php echo $this->item->message_id; ?>">
+						<?php echo HTMLHelper::_('form.token'); ?>
+					</fieldset>
+				</div>
+			</div>
 		</div>
-		<div class="form-group">
-			<?php echo Text::_('COM_MESSAGES_FIELD_DATE_TIME_LABEL'); ?>
-			<?php echo HTMLHelper::_('date', $this->item->date_time, Text::_('DATE_FORMAT_LC2')); ?>
-		</div>
-		<div class="form-group">
-			<?php echo Text::_('COM_MESSAGES_FIELD_SUBJECT_LABEL'); ?>
-			<?php echo $this->item->subject; ?>
-		</div>
-		<div class="form-group">
-			<?php echo Text::_('COM_MESSAGES_FIELD_MESSAGE_LABEL'); ?>
-			<?php echo $this->item->message; ?>
-		</div>
-		<input type="hidden" name="task" value="">
-		<input type="hidden" name="reply_id" value="<?php echo $this->item->message_id; ?>">
-		<?php echo HTMLHelper::_('form.token'); ?>
-	</fieldset>
+	</div>
 </form>
