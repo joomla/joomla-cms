1a7fb828e0284b717bd71e183847823fcd0646af
Hannes Papenberg <info@joomlager.de>
2020-04-14 14:57:18 +0200

    Changing selector and wait times

--- tests/Codeception/_support/Step/Acceptance/Administrator/Admin.php
+++ tests/Codeception/_support/Step/Acceptance/Administrator/Admin.php
@@ -69,8 +69,8 @@ class Admin extends AcceptanceTester
 
 		$I->waitForText($text, $timeout, AdminPage::$systemMessageContainer);
 		$I->see($text, AdminPage::$systemMessageContainer);
-		$I->wait(0.2);
-		$I->click('.joomla-alert--close');
-		$I->wait(0.3);
+		$I->wait(0.5);
+		$I->click('#system-message .joomla-alert--close');
+		$I->wait(0.6);
 	}
 }
