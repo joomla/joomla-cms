cc3c571aac63c92000b0543b40ef7a57b350d3a8
Troy T. Hall <programming@hallhome.us>
2020-04-20 12:49:00 -0500

    working on toolbar cross-reference missing from part 1

--- layouts/joomla/toolbar/iconclass.php
+++ layouts/joomla/toolbar/iconclass.php
@@ -3,11 +3,102 @@
  * @package     Joomla.Site
  * @subpackage  Layout
  *
- * @copyright   Copyright (C) 2005 - 2020 Open Source Matters, Inc. All rights reserved.
+ * @copyright   Copyright (C) 2005 - 2019 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
-defined('_JEXEC') or die;
+defined('JPATH_BASE') or die;
 
+/* Convert icomoon to fa */
+$icon = $displayData['icon'];
+if ( $icon === 'archive' )
+{
+	$icon = 'folder';
+}
+
+if ( $icon === 'publish' )
+{
+	$icon = 'check';
+}
+
+if ( $icon === 'unpublish' || $icon === 'cancel' || $icon === 'delete' || $icon === 'remove' )
+{
+	$icon = 'times';
+}
+
+if ( $icon === 'new' || $icon === 'save-new')
+{
+	$icon = 'plus';
+}
+
+if ( $icon === 'apply' )
+{
+	$icon = 'save';
+}
+
+if ( $icon === 'mail' )
+{
+	$icon = 'envelope';
+}
+
+if ( $icon === 'featured' )
+{
+	$icon = 'star featured';
+}
+
+if ( $icon === 'unfeatured' )
+{
+	$icon = 'star';
+}
+
+if ( $icon === 'checkedout' )
+{
+	$icon = 'lock';
+}
+
+if ( $icon === 'eye-close' )
+{
+	$icon = 'eye-slash';
+}
+
+if ( $icon === 'eye-open' )
+{
+	$icon = 'eye';
+}
+
+if ( $icon === 'refresh' )
+{
+	$icon = 'sync';
+}
+
+if ( $icon === 'contract' )
+{
+	$icon = 'compress';
+}
+
+if ( $icon === 'purge' )
+{
+	$icon = 'trash';
+}
+
+if ( $icon === 'options' )
+{
+	$icon = 'cog';
+}
+
+if ( $icon === 'expired' )
+{
+	$icon = 'minus-circle';
+}
+
+if ( $icon === 'save-copy' )
+{
+	$icon = 'copy';
+}
+
+if ( $icon === 'checkin' )
+{
+	$icon = 'check-square';
+}
 ?>
-icon-<?php echo $displayData['icon']; ?>
+fas fa-<?php echo $icon; ?>
