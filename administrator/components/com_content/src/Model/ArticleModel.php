058773f017e2e3056e08febd5f9cd97d54d0a8e8
Nicola Galgano <optimus4joomla@gmail.com>
2020-04-21 08:39:32 +0200

    unneeded

--- administrator/components/com_content/src/Model/ArticleModel.php
+++ administrator/components/com_content/src/Model/ArticleModel.php
@@ -804,13 +804,6 @@ class ArticleModel extends AdminModel
 			}
 		}
 
-		if (strlen($data['title']) > 200)
-		{
-			Factory::getApplication()->enqueueMessage('title too long', 'warning');
-
-			return false;
-		}
-
 		return parent::validate($form, $data, $group);
 	}
 
