ec4240382d577b4da01b81f80321a6e53fbc1b7b
Troy T. Hall <programming@hallhome.us>
2020-04-27 13:11:05 -0500

    more loop->sync changes

--- administrator/components/com_joomlaupdate/src/View/Update/HtmlView.php
+++ administrator/components/com_joomlaupdate/src/View/Update/HtmlView.php
@@ -35,7 +35,7 @@ class HtmlView extends BaseHtmlView
 		Factory::getApplication()->input->set('hidemainmenu', true);
 
 		// Set the toolbar information.
-		ToolbarHelper::title(Text::_('COM_JOOMLAUPDATE_OVERVIEW'), 'loop install');
+		ToolbarHelper::title(Text::_('COM_JOOMLAUPDATE_OVERVIEW'), 'sync install');
 
 		// Render the view.
 		parent::display($tpl);
