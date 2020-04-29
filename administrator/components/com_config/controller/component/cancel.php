b924f7a3412d0791806118518d5b0c8583188992
Fotis Evangelou <fevangelou@users.noreply.github.com>
2020-04-29 18:38:22 +0300

    Updated spaces to tabs.

--- administrator/components/com_config/controller/component/cancel.php
+++ administrator/components/com_config/controller/component/cancel.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * @package     Joomla.Administrator
+ * @package	 Joomla.Administrator
  * @subpackage  com_config
  *
  * @copyright   Copyright (C) 2005 - 2020 Open Source Matters, Inc. All rights reserved.
- * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ * @license	 GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 defined('_JEXEC') or die;
@@ -16,44 +16,44 @@ defined('_JEXEC') or die;
  */
 class ConfigControllerComponentCancel extends ConfigControllerCanceladmin
 {
-    /**
-     * Application object - Redeclared for proper typehinting
-     *
-     * @var    JApplicationCms
-     * @since  3.2
-     */
-    protected $app;
+	/**
+	 * Application object - Redeclared for proper typehinting
+	 *
+	 * @var	JApplicationCms
+	 * @since  3.2
+	 */
+	protected $app;
 
-    /**
-     * Method to cancel global configuration component.
-     *
-     * @return  void
-     *
-     * @since   3.2
-     */
-    public function execute()
-    {
-        $this->context = 'com_config.config.global';
+	/**
+	 * Method to cancel global configuration component.
+	 *
+	 * @return  void
+	 *
+	 * @since   3.2
+	 */
+	public function execute()
+	{
+		$this->context = 'com_config.config.global';
 
-        $this->component = $this->input->get('component');
+		$this->component = $this->input->get('component');
 
-        $returnUri = $this->input->get('return', null, 'base64');
+		$returnUri = $this->input->get('return', null, 'base64');
 
-        $redirect = 'index.php?option=' . $this->component;
+		$redirect = 'index.php?option=' . $this->component;
 
-        if (!empty($returnUri))
-        {
-            $redirect = base64_decode($returnUri);
-        }
+		if (!empty($returnUri))
+		{
+			$redirect = base64_decode($returnUri);
+		}
 
-        // Don't redirect to an external URL.
-        if (!JUri::isInternal($redirect))
-        {
-            $redirect = JUri::base();
-        }
+		// Don't redirect to an external URL.
+		if (!JUri::isInternal($redirect))
+		{
+			$redirect = JUri::base();
+		}
 
-        $this->app->redirect(JRoute::_($redirect, false));
+		$this->app->redirect(JRoute::_($redirect, false));
 
-        parent::execute();
-    }
+		parent::execute();
+	}
 }
