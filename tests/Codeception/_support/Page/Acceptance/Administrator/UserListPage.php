fdfcd7a7c637cd12fc8f552b7c2bc20512f2f82e
Troy T. Hall <programming@hallhome.us>
2020-04-29 15:58:53 -0500

    chevron & publish

--- tests/Codeception/_support/Page/Acceptance/Administrator/UserListPage.php
+++ tests/Codeception/_support/Page/Acceptance/Administrator/UserListPage.php
@@ -183,7 +183,7 @@ class UserListPage extends AdminListPage
 	 * @var    array
 	 * @since  3.7.3
 	 */
-	public static $seeUnblocked = ['xpath' => "//table[@id='userList']//*//td[4]//span[@class='icon-publish']"];
+	public static $seeUnblocked = ['xpath' => "//table[@id='userList']//*//td[4]//span[@class='fas fa-check']"];
 
 	/**
 	 * Locator for user is deleted and not found.
