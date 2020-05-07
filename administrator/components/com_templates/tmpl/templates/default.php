6cc4cab73abb96e10563ea80fb329d2344637af1
SharkyKZ <sharkykz@gmail.com>
2020-05-07 15:39:47 +0300

    Remove unused imports

--- administrator/components/com_templates/tmpl/templates/default.php
+++ administrator/components/com_templates/tmpl/templates/default.php
@@ -14,7 +14,6 @@ use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\Layout\LayoutHelper;
 use Joomla\CMS\Router\Route;
-use Joomla\CMS\Uri\Uri;
 
 HTMLHelper::_('behavior.multiselect');
 
