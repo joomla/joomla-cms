07dad1081347bfcd92c3cecc60a5c125f694a4fd
brian teeman <brian@teeman.net>
2020-05-09 00:39:01 +0100

    put back comma and add space output is now Table of Contacts, Sorted by: Title - descending, Filtered by: Status: Published

--- administrator/components/com_contact/tmpl/contacts/default.php
+++ administrator/components/com_contact/tmpl/contacts/default.php
@@ -46,8 +46,8 @@ if ($saveOrder && !empty($this->items))
 				<?php else : ?>
 					<table class="table" id="contactList">
 						<caption id="captionTable" class="sr-only">
-							<?php echo Text::_('COM_CONTACT_TABLE_CAPTION'); ?>
-							<span id="orderedBy"><?php echo Text::_('JGLOBAL_SORTED_BY'); ?></span>
+							<?php echo Text::_('COM_CONTACT_TABLE_CAPTION'); ?>,
+							<span id="orderedBy"><?php echo Text::_('JGLOBAL_SORTED_BY'); ?> </span>,
 							<span id="filteredBy"><?php echo Text::_('JGLOBAL_FILTERED_BY'); ?></span>
 						</caption>
 						<thead>
