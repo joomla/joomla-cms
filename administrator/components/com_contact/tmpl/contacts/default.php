ada4e58c57387885e664ad5b3384c6343accc146
Brian Teeman <brian@teeman.net>
2020-05-09 13:10:21 +0100

    [4.0] Filtered item information [a11y] (#28997)
    Table of Contacts, Sorted by: Title - descending, Filtered by: Status - Published

--- administrator/components/com_contact/tmpl/contacts/default.php
+++ administrator/components/com_contact/tmpl/contacts/default.php
@@ -46,7 +46,9 @@ if ($saveOrder && !empty($this->items))
 				<?php else : ?>
 					<table class="table" id="contactList">
 						<caption id="captionTable" class="sr-only">
-							<?php echo Text::_('COM_CONTACT_TABLE_CAPTION'); ?>, <?php echo Text::_('JGLOBAL_SORTED_BY'); ?>
+							<?php echo Text::_('COM_CONTACT_TABLE_CAPTION'); ?>,
+							<span id="orderedBy"><?php echo Text::_('JGLOBAL_SORTED_BY'); ?> </span>,
+							<span id="filteredBy"><?php echo Text::_('JGLOBAL_FILTERED_BY'); ?></span>
 						</caption>
 						<thead>
 							<tr>
