1bd1c75e129e94b7ce4d67a67830631e45d5a099
Peter Martin <github@pe7er.com>
2020-05-12 13:29:53 +0200

    Menu Items overview - Filter on Component - v3

---
+++ administrator/components/com_menus/src/Field/MenuItemByComponentField.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_menus
+ *
+ * @copyright   Copyright (C) 2005 - 2020 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+namespace Joomla\Component\Menus\Administrator\Field;
+
+defined('JPATH_BASE') or die;
+
+use Joomla\CMS\Factory;
+use Joomla\CMS\Form\Field\ListField;
+use Joomla\CMS\Language\Text;
+use Joomla\Utilities\ArrayHelper;
+
+/**
+ * MenuItem by Component field.
+ *
+ * @since  4.0.0
+ */
+class MenuItemByComponentField extends ListField
+{
+	/**
+	 * The form field type.
+	 *
+	 * @var     string
+	 * @since   4.0.0
+	 */
+	protected $type = 'MenuItemByComponent';
+
+	/**
+	 * Method to get a list of options for a list input.
+	 *
+	 * @return    array  An array of JHtml options.
+	 *
+	 * @since   4.0.0
+	 */
+	protected function getOptions()
+	{
+		// Initialise variable.
+		$db      = Factory::getDbo();
+		$options = array();
+
+		$query = $db->getQuery(true);
+		$query->select('DISTINCT ' . $db->quoteName('extensions.name'))
+			->from($db->quoteName('#__menu', 'menu'))
+			->join(
+				'LEFT', $db->quoteName('#__extensions', 'extensions'),
+				$db->quoteName('extensions.extension_id') . ' = ' . $db->quoteName('menu.component_id')
+			)
+			->where($db->quoteName('menu.client_id') . ' = 0')
+			->where($db->quoteName('menu.type') . ' = ' . $db->quote('component'));
+
+		$db->setQuery($query);
+		$categoryTypes = $db->loadColumn();
+
+		foreach ($categoryTypes as $categoryType)
+		{
+			$option        = new \stdClass;
+			$option->value = $categoryType;
+
+			// Extract the component name and optional section name
+			$parts     = explode('.', $categoryType);
+			$component = $parts[0];
+			$section   = (count($parts) > 1) ? $parts[1] : null;
+
+			// Load component language files
+			$lang = Factory::getLanguage();
+			$lang->load($component, JPATH_BASE)
+			|| $lang->load($component, JPATH_ADMINISTRATOR . '/components/' . $component);
+
+			// If the component section string exists, let's use it
+			if ($lang->hasKey($component_section_key = strtoupper($component . ($section ? "_$section" : ''))))
+			{
+				$option->text = Text::_($component_section_key);
+			}
+			else
+				// Else use the component title
+			{
+				$option->text = Text::_(strtoupper($component));
+			}
+
+			$options[] = $option;
+		}
+
+		// Sort by name
+		$options = ArrayHelper::sortObjects($options, 'text', 1, true, true);
+
+		// Merge any additional options in the XML definition.
+		$options = array_merge(parent::getOptions(), $options);
+
+		return $options;
+	}
+}
