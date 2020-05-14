c8f7a4399ca7c7deaccf6867492dc8ccdb88da97
Bear <programming@hallhome.us>
2020-05-14 14:42:17 -0500

    Update build/media_source/system/js/fields/passwordview.es6.js
    Co-authored-by: SharkyKZ <sharkykz@gmail.com>

--- build/media_source/system/js/fields/passwordview.es6.js
+++ build/media_source/system/js/fields/passwordview.es6.js
@@ -32,8 +32,8 @@
           srText.innerText = Joomla.Text._('JHIDEPASSWORD');
         } else if (input.type === 'text') {
           // Update the icon class
-          icon.classList.add('fas fa-eye');
-          icon.classList.remove('fas fa-eye-slash');
+          icon.classList.add('fa-eye');
+          icon.classList.remove('fa-eye-slash');
 
           // Update the input type
           input.type = 'password';
