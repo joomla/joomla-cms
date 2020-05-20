3b275519f3c0a8008754b65f1e27eb5d184d0592
SharkyKZ <sharkykz@gmail.com>
2020-05-20 10:25:48 +0300

    Rename parameter

--- components/com_finder/src/Helper/FinderHelper.php
+++ components/com_finder/src/Helper/FinderHelper.php
@@ -34,7 +34,7 @@ class FinderHelper
 	 */
 	public static function logSearch(Query $searchquery, $resultCount = 0)
 	{
-		$enable_log_searches = ComponentHelper::getParams('com_finder')->get('logging_enabled', 0);
+		$enable_log_searches = ComponentHelper::getParams('com_finder')->get('gather_search_statistics', 0);
 
 		if (!$enable_log_searches)
 		{
