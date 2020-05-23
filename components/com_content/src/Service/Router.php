881f0209bd532316f29869cb03f49374f2886c8b
SharkyKZ <sharkykz@gmail.com>
2020-05-24 01:20:22 +0300

    [4.0] [com_content] Fix filtering by author alias (#29160)
    * Fix filtering by author alias
    
    * Parameterized queries

--- components/com_content/src/Service/Router.php
+++ components/com_content/src/Service/Router.php
@@ -158,10 +158,12 @@ class Router extends RouterView
 	{
 		if (!strpos($id, ':'))
 		{
+			$id      = (int) $id;
 			$dbquery = $this->db->getQuery(true);
 			$dbquery->select($this->db->quoteName('alias'))
 				->from($this->db->quoteName('#__content'))
-				->where('id = ' . $this->db->quote($id));
+				->where($this->db->quoteName('id') . ' = :id')
+				->bind(':id', $id, ParameterType::INTEGER);
 			$this->db->setQuery($dbquery);
 
 			$id .= ':' . $this->db->loadResult();
