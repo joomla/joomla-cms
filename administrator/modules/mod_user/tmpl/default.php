e72bec315db1e365841c1e8da41bd7abe6e8bfc8
SharkyKZ <sharkykz@gmail.com>
2020-05-26 18:03:24 +0300

    Remove from mod_user

--- administrator/modules/mod_user/tmpl/default.php
+++ administrator/modules/mod_user/tmpl/default.php
@@ -42,11 +42,6 @@ $hideLinks = $app->input->getBool('hidemainmenu');
 			<span class="fas fa-fw fa-user" aria-hidden="true"></span>
 			<?php echo Text::_('MOD_USER_EDIT_ACCOUNT'); ?>
 		</a>
-		<?php $route = 'index.php?option=com_users&task=user.edit&id=' . $user->id . '&return=' . base64_encode($uri) . '#attrib-accessibility'; ?>
-		<a class="dropdown-item" href="<?php echo Route::_($route); ?>">
-			<span class="fas fa-fw fa-universal-access" aria-hidden="true"></span>
-			<?php echo Text::_('MOD_USER_ACCESSIBILITY_SETTINGS'); ?>
-		</a>
 		<?php $route = 'index.php?option=com_login&task=logout&amp;' . Session::getFormToken() . '=1'; ?>
 		<a class="dropdown-item" href="<?php echo Route::_($route); ?>">
 			<span class="fas fa-fw fa-power-off" aria-hidden="true"></span>
