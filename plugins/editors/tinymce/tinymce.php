a7bccaca324261b4285f9c05ebdfa52afcce7c09
dGrammatiko <d.grammatiko@gmail.com>
2020-05-29 16:33:49 +0200

    remove lang string and useless import

--- plugins/editors/tinymce/tinymce.php
+++ plugins/editors/tinymce/tinymce.php
@@ -12,7 +12,6 @@ defined('_JEXEC') or die;
 use Joomla\CMS\Access\Access;
 use Joomla\CMS\Component\ComponentHelper;
 use Joomla\CMS\Factory;
-use Joomla\CMS\Filesystem\Folder;
 use Joomla\CMS\Filter\InputFilter;
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
