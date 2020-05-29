016314965ee7dec111dfd2e08bc2701abd9882cb
George Wilson <georgejameswilson@googlemail.com>
2020-05-29 14:16:41 +0100

    Update libraries/src/Versioning/Versioning.php
    Co-authored-by: Harald Leithner <leithner@itronic.at>

--- libraries/src/Versioning/Versioning.php
+++ libraries/src/Versioning/Versioning.php
@@ -38,7 +38,7 @@ class Versioning
 		$itemid = $typeAlias . '.' . $id;
 		$query = $db->getQuery(true);
 		$query->select($db->quoteName('h.version_note') . ',' . $db->quoteName('h.save_date') . ',' . $db->quoteName('u.name'))
-			->from($db->quoteName('#__history') . ' AS h ')
+			->from($db->quoteName('#__history', 'h'))
 			->leftJoin($db->quoteName('#__users') . ' AS u ON ' . $db->quoteName('u.id') . ' = ' . $db->quoteName('h.editor_user_id'))
 			->where($db->quoteName('item_id') . ' = :item_id')
 			->bind(':item_id', $itemid, ParameterType::STRING)
