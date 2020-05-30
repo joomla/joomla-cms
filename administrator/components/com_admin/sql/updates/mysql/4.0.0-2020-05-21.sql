85a313bfcecb29d683bc708093c52b2dc290a7b6
wilsonge <georgejameswilson@googlemail.com>
2020-05-30 17:40:01 +0100

    Fix failure of schema checker to understand update query

--- administrator/components/com_admin/sql/updates/mysql/4.0.0-2020-05-21.sql
+++ administrator/components/com_admin/sql/updates/mysql/4.0.0-2020-05-21.sql
@@ -1,7 +1,7 @@
 -- Renaming table
 RENAME TABLE `#__ucm_history` TO `#__history`;
 -- Rename ucm_item_id to item_id as the new primary identifier for the original content item
-ALTER TABLE `#__history` CHANGE COLUMN `ucm_item_id` `item_id` VARCHAR(50) NOT NULL DEFAULT '' AFTER `version_id`;
+ALTER TABLE `#__history` CHANGE `ucm_item_id` `item_id` VARCHAR(50) NOT NULL AFTER `version_id`;
 -- Extend the original field content with the alias of the content type
 UPDATE #__history AS h INNER JOIN #__content_types AS c ON h.ucm_type_id = c.type_id SET h.item_id = CONCAT(c.type_alias, '.', h.item_id);
 -- Now we don't need the ucm_type_id anymore and drop it.
