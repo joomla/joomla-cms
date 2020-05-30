3a7583b1cf2a9e497aa267d9a69457f4723cc056
Richard Fath <richard67@users.noreply.github.com>
2020-05-30 20:40:19 +0200

    Fix update for postgresql (#29280)

--- administrator/components/com_admin/sql/updates/postgresql/4.0.0-2020-05-21.sql
+++ administrator/components/com_admin/sql/updates/postgresql/4.0.0-2020-05-21.sql
@@ -2,7 +2,10 @@
 ALTER TABLE "#__ucm_history" RENAME TO "#__history";
 -- Rename ucm_item_id to item_id as the new primary identifier for the original content item
 ALTER TABLE "#__history" RENAME "ucm_item_id" TO "item_id";
-ALTER TABLE "#__history" ALTER COLUMN "item_id" VARCHAR(50) NOT NULL DEFAULT "";
+ALTER TABLE "#__history" ALTER COLUMN "item_id" TYPE character varying(50);
+ALTER TABLE "#__history" ALTER COLUMN "item_id" SET NOT NULL;
+ALTER TABLE "#__history" ALTER COLUMN "item_id" DROP DEFAULT;
+
 -- Extend the original field content with the alias of the content type
 UPDATE "#__history" AS h INNER JOIN "#__content_types" AS c ON h.ucm_type_id = c.type_id SET h.item_id = CONCAT(c.type_alias, ".", h.item_id);
 -- Now we don't need the ucm_type_id anymore and drop it.
