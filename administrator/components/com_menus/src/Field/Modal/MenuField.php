b94f0e08b7f84ccbed306406881ce7884d7b05e7
Quy <quy@fluxbb.org>
2020-05-30 10:21:49 -0700

    Fix JFactory not found (#29277)

--- administrator/components/com_menus/src/Field/Modal/MenuField.php
+++ administrator/components/com_menus/src/Field/Modal/MenuField.php
@@ -9,7 +9,7 @@
 
 namespace Joomla\Component\Menus\Administrator\Field\Modal;
 
-defined('JPATH_BASE') or die;
+defined('_JEXEC') or die;
 
 use Joomla\CMS\Factory;
 use Joomla\CMS\Form\FormField;
@@ -148,9 +148,12 @@ class MenuField extends FormField
 		{
 			$this->allowSelect = ((string) $this->element['select']) !== 'false';
 			$this->allowClear = ((string) $this->element['clear']) !== 'false';
-			$this->allowNew = ((string) $this->element['new']) === 'true';
-			$this->allowEdit = ((string) $this->element['edit']) === 'true';
 			$this->allowPropagate = ((string) $this->element['propagate']) === 'true';
+
+			// Creating/editing menu items is not supported in frontend.
+			$isAdministrator = Factory::getApplication()->isClient('administrator');
+			$this->allowNew = $isAdministrator ? ((string) $this->element['new']) === 'true' : false;
+			$this->allowEdit = $isAdministrator ? ((string) $this->element['edit']) === 'true' : false;
 		}
 
 		return $return;
