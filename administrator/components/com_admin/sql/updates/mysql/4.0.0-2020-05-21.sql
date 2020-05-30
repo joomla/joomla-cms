a92ed422b31fc8a03eedbb5952a9e2540f2f16b4
wilsonge <georgejameswilson@googlemail.com>
2020-05-30 17:03:03 +0100

    Remove column and index that shouldn't exist

--- administrator/components/com_admin/sql/updates/mysql/4.0.0-2020-05-21.sql
+++ administrator/components/com_admin/sql/updates/mysql/4.0.0-2020-05-21.sql
@@ -6,6 +6,4 @@ ALTER TABLE `#__history` CHANGE COLUMN `ucm_item_id` `item_id` VARCHAR(50) NOT N
 UPDATE #__history AS h INNER JOIN #__content_types AS c ON h.ucm_type_id = c.type_id SET h.item_id = CONCAT(c.type_alias, '.', h.item_id);
 -- Now we don't need the ucm_type_id anymore and drop it.
 ALTER TABLE `#__history` DROP COLUMN `ucm_type_id`;
-ALTER TABLE `#__history` ADD COLUMN `active_version` TINYINT(4) NOT NULL DEFAULT '0' AFTER `keep_forever`;
-ALTER TABLE `#__history` ADD INDEX `idx_active_version` (`active_version`);
 ALTER TABLE `#__history` MODIFY `save_date` datetime NOT NULL;
