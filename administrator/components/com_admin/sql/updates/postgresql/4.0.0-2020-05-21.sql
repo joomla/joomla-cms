22ac8e5f8be31453bb649211142f5699db2f7707
George Wilson <georgejameswilson@googlemail.com>
2020-05-30 20:39:30 +0100

    All in one line

--- administrator/components/com_admin/sql/updates/postgresql/4.0.0-2020-05-21.sql
+++ administrator/components/com_admin/sql/updates/postgresql/4.0.0-2020-05-21.sql
@@ -7,10 +7,7 @@ ALTER TABLE "#__history" ALTER COLUMN "item_id" SET NOT NULL;
 ALTER TABLE "#__history" ALTER COLUMN "item_id" DROP DEFAULT;
 
 -- Extend the original field content with the alias of the content type
-UPDATE "#__history" AS h
-   SET "item_id" = CONCAT(c."type_alias", '.', "item_id")
-  FROM "#__content_types" AS c
- WHERE h."ucm_type_id" = c."type_id";
+UPDATE "#__history" AS h SET "item_id" = CONCAT(c."type_alias", '.', "item_id") FROM "#__content_types" AS c WHERE h."ucm_type_id" = c."type_id";
 
 -- Now we don't need the ucm_type_id anymore and drop it.
 ALTER TABLE "#__history" DROP COLUMN "ucm_type_id";
