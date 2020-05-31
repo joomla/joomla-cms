ba4f0d38f38ce75ea08837266fdb90bb430fc1c5
brian teeman <brian@teeman.net>
2020-05-31 13:30:14 +0100

    [4.0] com_config buttons front end
    Removes the toolbar markup as it is not needed - they are just buttons.
    Bonus this also ensures the buttons are spaced correctly
    
    To test. Install sample data and log in to the front end
    Test the menu items for Site Settings and Template Settings and also test editing a module
    
    Before this PR the buttons are next to each other without any spacing
    
    After this PR the toolbar markup is removed and the buttons are spaced correctly

--- components/com_config/tmpl/modules/default.php
+++ components/com_config/tmpl/modules/default.php
@@ -46,20 +46,18 @@ if (Multilanguage::isEnabled())
 	<div class="row">
 		<div class="col-md-12">
 
-			<div role="toolbar" aria-label="<?php echo Text::_('JTOOLBAR'); ?>">
-				<button type="button" class="btn btn-primary" data-submit-task="modules.apply">
-					<span class="fas fa-check" aria-hidden="true"></span>
-					<?php echo Text::_('JAPPLY'); ?>
-				</button>
-				<button type="button" class="btn btn-secondary" data-submit-task="modules.save">
-					<span class="fas fa-check" aria-hidden="true"></span>
-					<?php echo Text::_('JSAVE'); ?>
-				</button>
-				<button type="button" class="btn btn-danger" data-submit-task="modules.cancel">
-					<span class="fas fa-times" aria-hidden="true"></span>
-					<?php echo Text::_('JCANCEL'); ?>
-				</button>
-			</div>
+			<button type="button" class="btn btn-primary" data-submit-task="modules.apply">
+				<span class="fas fa-check" aria-hidden="true"></span>
+				<?php echo Text::_('JAPPLY'); ?>
+			</button>
+			<button type="button" class="btn btn-secondary" data-submit-task="modules.save">
+				<span class="fas fa-check" aria-hidden="true"></span>
+				<?php echo Text::_('JSAVE'); ?>
+			</button>
+			<button type="button" class="btn btn-danger" data-submit-task="modules.cancel">
+				<span class="fas fa-times" aria-hidden="true"></span>
+				<?php echo Text::_('JCANCEL'); ?>
+			</button>
 
 			<hr>
 
