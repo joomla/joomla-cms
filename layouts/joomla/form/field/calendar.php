e202ba9e663266113b2c3559226e01fa2fd5c4c7
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2020-05-31 23:55:01 +0300

    [4.0.b1] Fix notice thrown by Calendar (#29347)
    Closes gh-29340

--- layouts/joomla/form/field/calendar.php
+++ layouts/joomla/form/field/calendar.php
@@ -114,7 +114,7 @@ $document->getWebAssetManager()
 			value="<?php echo htmlspecialchars(($value !== '0000-00-00 00:00:00') ? $value : '', ENT_COMPAT, 'UTF-8'); ?>"
 			<?php echo !empty($description) ? ' aria-describedby="' . $name . '-desc"' : ''; ?>
 			<?php echo $attributes; ?>
-			<?php echo $dataAttribute; ?>
+			<?php echo $dataAttribute ?? ''; ?>
 			<?php echo !empty($hint) ? 'placeholder="' . htmlspecialchars($hint, ENT_COMPAT, 'UTF-8') . '"' : ''; ?>
 			data-alt-value="<?php echo htmlspecialchars($value, ENT_COMPAT, 'UTF-8'); ?>" autocomplete="off">
 		<span class="input-group-append">
