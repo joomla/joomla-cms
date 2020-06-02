1b7dddc05f7899c1efa7dbd8d3269cbc1b29bbc7
brian teeman <brian@teeman.net>
2020-06-02 23:56:46 +0100

    [4.0] Nested fieldsets
    This PR stops splitting nested fieldsets using the params layout from being split again into columns

--- layouts/joomla/edit/params.php
+++ layouts/joomla/edit/params.php
@@ -118,7 +118,7 @@ foreach ($fieldSets as $name => $fieldSet)
 	{
 		echo '<fieldset id="fieldset-' . $name . '" class="options-form ' . (!empty($fieldSet->class) ? $fieldSet->class : '') . '">';
 		echo '<legend>' . $label . '</legend>';
-		echo '<div class="column-count-md-2 column-count-lg-3">';
+		echo '<div>';
 	}
 	// Tabs
 	elseif (!$hasParent)
