c65f34e4af5a0c83c1e85e6fc5b1bd1e519240b3
Hannes Papenberg <info@joomlager.de>
2020-06-03 11:19:40 +0200

    Codestyle

--- libraries/src/Table/Extension.php
+++ libraries/src/Table/Extension.php
@@ -30,8 +30,8 @@ class Extension extends Table
 	 */
 	protected $_supportNullValue = true;
 
-  /**
-   * Ensure the params in json encoded in the bind method
+	/**
+	 * Ensure the params in json encoded in the bind method
 	 *
 	 * @var    array
 	 * @since  4.0.0
