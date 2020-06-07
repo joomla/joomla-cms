381417564a22479fc1da104fa68bc8196ad47e07
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2020-06-07 09:25:14 +0300

    Filter the module types in the new module page
    Make the script deferred

--- build/media_source/com_modules/js/admin-module-search.es6.js
+++ build/media_source/com_modules/js/admin-module-search.es6.js
@@ -9,6 +9,11 @@
   /**
    * Add a keyboard event listener to the Select a Module Type search element.
    *
+   * IMPORTANT! This script is meant to be loaded deferred. This means that a. it's non-blocking
+   * (the browser can load it whenever) and b. it doesn't need an on DOMContentLoaded event handler
+   * because the browser is guaranteed to execute it only after the DOM content has loaded, the
+   * whole point of it being deferred.
+   *
    * The search box has a keybaord handler that fires every time you press a keyboard button or send
    * a keypress with a touch / virtual keyboard. We then iterate all module type cards and check if
    * the plain text (HTML stripped out) representation of the module title or description partially
@@ -30,65 +35,63 @@
    * probably going to be creating multiple instances of the same module, one after another, as is
    * typical when building a new Joomla! site.
    */
-  document.addEventListener('DOMContentLoaded', () => {
     // Make sure the element exists i.e. a template override has not removed it.
-    const elSearch = document.getElementById('comModulesSelectSearch');
-    const elSearchContainer = document.getElementById('comModulesSelectSearchContainer');
+  const elSearch = document.getElementById('comModulesSelectSearch');
+  const elSearchContainer = document.getElementById('comModulesSelectSearchContainer');
 
-    if (!elSearch || !elSearchContainer) {
-      return;
+  if (!elSearch || !elSearchContainer) {
+    return;
+  }
+
+  // Add the keyboard event listener which performs the live search in the cards
+  elSearch.addEventListener('keyup', (event) => {
+    /** @type {KeyboardEvent} event */
+    const partialSearch = event.target.value;
+    const elCards = document.querySelectorAll('.comModulesSelectCard');
+
+    // Save the search string into session storage
+    if (typeof (sessionStorage) !== 'undefined') {
+      sessionStorage.setItem('Joomla.com_modules.new.search', partialSearch);
     }
 
-    // Add the keyboard event listener which performs the live search in the cards
-    elSearch.addEventListener('keyup', (event) => {
-      /** @type {KeyboardEvent} event */
-      const partialSearch = event.target.value;
-      const elCards = document.querySelectorAll('.comModulesSelectCard');
+    // Iterate all the module cards
+    elCards.forEach((card) => {
+      const cardHeaderList = card.querySelectorAll('.card-header');
+      const cardBodyList = card.querySelectorAll('.card-body');
+      const title = cardHeaderList.length ? cardHeaderList[0].textContent : '';
+      const description = cardBodyList.length ? cardBodyList[0].textContent : '';
 
-      // Save the search string into session storage
-      if (typeof (sessionStorage) !== 'undefined') {
-        sessionStorage.setItem('Joomla.com_modules.new.search', partialSearch);
+      // First remove the classes which hide the module cards
+      card.classList.remove('d-none');
+
+      // An empty search string means that we should show everything
+      if (partialSearch === '') {
+        return;
       }
 
-      // Iterate all the module cards
-      elCards.forEach((card) => {
-        const cardHeaderList = card.querySelectorAll('.card-header');
-        const cardBodyList = card.querySelectorAll('.card-body');
-        const title = cardHeaderList.length ? cardHeaderList[0].textContent : '';
-        const description = cardBodyList.length ? cardBodyList[0].textContent : '';
-
-        // First remove the classes which hide the module cards
-        card.classList.remove('d-none');
-
-        // An empty search string means that we should show everything
-        if (partialSearch === '') {
-          return;
-        }
-
-        // If the module title or description doesn't match add a class to hide it.
-        if (!title.toLowerCase().includes(partialSearch.toLowerCase())
-          && !description.toLowerCase().includes(partialSearch.toLowerCase())) {
-          card.classList.add('d-none');
-        }
-      });
+      // If the module title or description doesn't match add a class to hide it.
+      if (!title.toLowerCase().includes(partialSearch.toLowerCase())
+        && !description.toLowerCase().includes(partialSearch.toLowerCase())) {
+        card.classList.add('d-none');
+      }
     });
-
-    // For reasons of progressive enhancement the search box is hidden by default.
-    elSearchContainer.classList.remove('d-none');
-
-    // Focus the just show element
-    elSearch.focus();
-
-    try {
-      if (typeof (sessionStorage) !== 'undefined') {
-        // Load the search string from session storage
-        elSearch.value = sessionStorage.getItem('Joomla.com_modules.new.search') || '';
-
-        // Trigger the keyboard handler event manually to initiate the search
-        elSearch.dispatchEvent(new KeyboardEvent('keyup'));
-      }
-    } catch (e) {
-      // This is probably Internet Explorer which doesn't support the KeyboardEvent constructor :(
-    }
   });
+
+  // For reasons of progressive enhancement the search box is hidden by default.
+  elSearchContainer.classList.remove('d-none');
+
+  // Focus the just show element
+  elSearch.focus();
+
+  try {
+    if (typeof (sessionStorage) !== 'undefined') {
+      // Load the search string from session storage
+      elSearch.value = sessionStorage.getItem('Joomla.com_modules.new.search') || '';
+
+      // Trigger the keyboard handler event manually to initiate the search
+      elSearch.dispatchEvent(new KeyboardEvent('keyup'));
+    }
+  } catch (e) {
+    // This is probably Internet Explorer which doesn't support the KeyboardEvent constructor :(
+  }
 })(document);
