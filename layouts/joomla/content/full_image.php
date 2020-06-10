6d5587c5fee20b0b7c01a8d0b25076eaac6bc241
SharkyKZ <sharkykz@gmail.com>
2020-06-11 01:33:06 +0300

    Undefined property notice (#29513)

--- layouts/joomla/content/full_image.php
+++ layouts/joomla/content/full_image.php
@@ -18,7 +18,7 @@ $images  = json_decode($displayData->images);
 		<img loading="lazy" src="<?php echo htmlspecialchars($images->image_fulltext, ENT_COMPAT, 'UTF-8'); ?>"
 			 alt="<?php echo htmlspecialchars($images->image_fulltext_alt, ENT_COMPAT, 'UTF-8'); ?>"
 			 itemprop="image"/>
-		<?php if ($images->image_fulltext_caption !== '') : ?>
+		<?php if (isset($images->image_fulltext_caption) && $images->image_fulltext_caption !== '') : ?>
 			<figcaption class="caption"><?php echo htmlspecialchars($images->image_fulltext_caption, ENT_COMPAT, 'UTF-8'); ?></figcaption>
 		<?php endif; ?>
 	</figure>
