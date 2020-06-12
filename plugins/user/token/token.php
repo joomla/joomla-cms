85d177fdec40b537abe4ca8d0ff6a6287f4150f3
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2020-06-13 01:56:57 +0300

    [4.0] Improve user token plugin display in the profile page (#29469)

--- plugins/user/token/token.php
+++ plugins/user/token/token.php
@@ -12,6 +12,7 @@ defined('_JEXEC') or die;
 use Joomla\CMS\Crypt\Crypt;
 use Joomla\CMS\Factory;
 use Joomla\CMS\Form\Form;
+use Joomla\CMS\Language\Text;
 use Joomla\CMS\Plugin\CMSPlugin;
 use Joomla\CMS\Plugin\PluginHelper;
 use Joomla\Database\ParameterType;
@@ -151,12 +152,39 @@ class PlgUserToken extends CMSPlugin
 			foreach ($results as $v)
 			{
 				$k = str_replace($this->profileKeyPrefix . '.', '', $v[0]);
+
 				$data->{$this->profileKeyPrefix}[$k] = $v[1];
 			}
-		}
-		catch (Exception $e)
+		} catch (Exception $e)
 		{
 			// We suppress any database error. It means we get no token saved by default.
+		}
+
+		/**
+		 * Modify the data for display in the user profile view page in the frontend.
+		 *
+		 * It's important to note that we deliberately not register HTMLHelper methods to do the
+		 * same (unlike e.g. the actionlogs system plugin) because the names of our fields are too
+		 * generic and we run the risk of creating naming clashes. Instead, we manipulate the data
+		 * directly.
+		 */
+		if (($context === 'com_users.profile') && ($this->app->input->get('layout') !== 'edit'))
+		{
+			$pluginData = $data->{$this->profileKeyPrefix};
+			$enabled    = $pluginData['enabled'];
+			$token      = $pluginData['token'];
+
+			$pluginData['enabled'] = Text::_('JDISABLED');
+			$pluginData['token']   = '';
+
+			if ($enabled)
+			{
+				$algo                  = $this->getAlgorithmFromFormFile();
+				$pluginData['enabled'] = Text::_('JENABLED');
+				$pluginData['token']   = $this->getTokenForDisplay($userId, $token, $algo);
+			}
+
+			$data->{$this->profileKeyPrefix} = $pluginData;
 		}
 
 		return true;
@@ -202,7 +230,7 @@ class PlgUserToken extends CMSPlugin
 
 		if (is_array($data))
 		{
-			$data = (object) $data;
+			$data = (object)$data;
 		}
 
 		// Check if the user belongs to an allowed user group
@@ -251,6 +279,12 @@ class PlgUserToken extends CMSPlugin
 			$form->removeField('savemeforotherpeople', 'joomlatoken');
 		}
 
+		// Remove the Reset field when displaying the user profile form
+		if (($form->getName() === 'com_users.profile') && ($this->app->input->get('layout') !== 'edit'))
+		{
+			$form->removeField('reset', 'joomlatoken');
+		}
+
 		return true;
 	}
 
@@ -289,6 +323,19 @@ class PlgUserToken extends CMSPlugin
 		// No Joomla token data. Set the $noToken flag which results in a new token being generated.
 		if (!isset($data[$this->profileKeyPrefix]))
 		{
+			/**
+			 * Is the user being saved programmatically, without passing the user profile
+			 * information? In this case I do not want to accidentally try to generate a new token!
+			 *
+			 * We determine that by examining whether the Joomla token field exists. If it does but
+			 * it wasn't passed when saving the user I know it's a programmatic user save and I have
+			 * to ignore it.
+			 */
+			if ($this->hasTokenProfileFields($userId))
+			{
+				return;
+			}
+
 			$noToken                       = true;
 			$data[$this->profileKeyPrefix] = [];
 		}
@@ -354,10 +401,10 @@ class PlgUserToken extends CMSPlugin
 		$query = $db->getQuery(true)
 			->insert($db->qn('#__user_profiles'))
 			->columns([
-				$db->qn('user_id'),
-				$db->qn('profile_key'),
-				$db->qn('profile_value'),
-				$db->qn('ordering')
+					$db->qn('user_id'),
+					$db->qn('profile_key'),
+					$db->qn('profile_value'),
+					$db->qn('ordering')
 				]
 			);
 
@@ -414,8 +461,7 @@ class PlgUserToken extends CMSPlugin
 			$query->bind(':profileKey', $profileKey, ParameterType::STRING);
 
 			$db->setQuery($query)->execute();
-		}
-		catch (Exception $e)
+		} catch (Exception $e)
 		{
 			// Do nothing.
 		}
@@ -463,8 +509,7 @@ class PlgUserToken extends CMSPlugin
 			$query->bind(':userId', $userId, ParameterType::INTEGER);
 
 			return $db->setQuery($query)->loadResult();
-		}
-		catch (Exception $e)
+		} catch (Exception $e)
 		{
 			return null;
 		}
@@ -528,4 +573,107 @@ class PlgUserToken extends CMSPlugin
 
 		return !empty($intersection);
 	}
+
+	/**
+	 * Returns the token formatted suitably for the user to copy.
+	 *
+	 * @param   string  $tokenSeed  The token seed data stored in the database
+	 *
+	 * @return  string
+	 * @since   4.0.0
+	 */
+	private function getTokenForDisplay(int $userId, string $tokenSeed,
+										string $algorithm = 'sha256'): string
+	{
+		if (empty($tokenSeed))
+		{
+			return '';
+		}
+
+		try
+		{
+			$siteSecret = $this->app->get('secret');
+		} catch (\Exception $e)
+		{
+			$siteSecret = '';
+		}
+
+		// NO site secret? You monster!
+		if (empty($siteSecret))
+		{
+			return '';
+		}
+
+		$rawToken  = base64_decode($tokenSeed);
+		$tokenHash = hash_hmac($algorithm, $rawToken, $siteSecret);
+		$message   = base64_encode("$algorithm:$userId:$tokenHash");
+
+		if ($userId !== $this->app->getIdentity()->id)
+		{
+			$message = '';
+		}
+
+		return $message;
+	}
+
+	/**
+	 * Get the token algorithm as defined in the form file
+	 *
+	 * We use a simple RegEx match instead of loading the form for better performance.
+	 *
+	 * @return  string  The configured algorithm, 'sha256' as a fallback if none is found.
+	 */
+	private function getAlgorithmFromFormFile(): string
+	{
+		$algo = 'sha256';
+
+		$file     = __DIR__ . '/forms/token.xml';
+		$contents = @file_get_contents($file);
+
+		if ($contents === false)
+		{
+			return $algo;
+		}
+
+		if (preg_match('/\s*algo=\s*"\s*([a-z0-9]+)\s*"/i', $contents, $matches) !== 1)
+		{
+			return $algo;
+		}
+
+		return $matches[1];
+	}
+
+	/**
+	 * Does the user have the Joomla Token profile fields?
+	 *
+	 * @param   int|null  $userId  The user we're interested in
+	 *
+	 * @return  bool  True if the user has Joomla Token profile fields
+	 */
+	private function hasTokenProfileFields(?int $userId): bool
+	{
+		if (is_null($userId) || ($userId <= 0))
+		{
+			return false;
+		}
+
+		$db = $this->db;
+		$q  = $db->getQuery(true)
+			->select('COUNT(*)')
+			->from($db->qn('#__user_profiles'))
+			->where($db->qn('user_id') . ' = ' . $userId)
+			->where($db->qn('profile_key') . ' = ' . $db->q($this->profileKeyPrefix . '.token'));
+
+		try
+		{
+			$numRows = $db->setQuery($q)->loadResult() ?? 0;
+		}
+		catch (Exception $e)
+		{
+			return false;
+		}
+
+		return $numRows > 0;
+	}
+
 }
