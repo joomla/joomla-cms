7919c0fd8ea1806c9d950d1e787c3ceab6ed19e1
Nicola Galgano <optimus4joomla@gmail.com>
2020-06-13 09:49:47 +0200

    sql

---
+++ administrator/components/com_admin/sql/updates/postgresql/4.0.0-2020-06-13.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS "#__jobs" (
+  "element" varchar(100) NOT NULL,
+  "folder" varchar(100) NOT NULL,
+  PRIMARY KEY ("element", "folder")
+);
+
+INSERT INTO "#__extensions" ("package_id", "name", "type", "element", "folder", "client_id", "enabled", "access", "protected", "manifest_cache", "params", "checked_out", "checked_out_time", "ordering", "state") VALUES
+(0, 'plg_system_scheduler', 'plugin', 'scheduler', 'system', 0, 1, 1, 0, '', '{"timeout":"3","webcron":"0","webcronkey":"mywebcronactivationkey","lastrun":"0","unit":"60","taskid":"0"}',  0, NULL, 0, 0);
