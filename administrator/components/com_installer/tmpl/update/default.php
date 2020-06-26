4aa2aa698d5e258f732046b0b90da26c6c32122c
Quy <quy@fluxbb.org>
2020-06-26 08:02:27 -0700

    [4.0][com_installer] Fix width of Name column (#29772)

--- administrator/components/com_installer/tmpl/update/default.php
+++ administrator/components/com_installer/tmpl/update/default.php
@@ -73,9 +73,6 @@ $listDirn  = $this->escape($this->state->get('list.direction'));
 								<th scope="col" class="d-none d-md-table-cell">
 									<?php echo Text::_('COM_INSTALLER_HEADING_INSTALLTYPE'); ?>
 								</th>
-								<th scope="col" class="w-40 d-none d-md-table-cell">
-									<?php echo Text::_('COM_INSTALLER_HEADING_DETAILSURL'); ?>
-								</th>
 							</tr>
 							</thead>
 							<tbody>
@@ -94,8 +91,14 @@ $listDirn  = $this->escape($this->state->get('list.direction'));
 										<div role="tooltip" id="tip<?php echo $i; ?>">
 											<?php echo $item->description; ?>
 										</div>
+										<div class="small break-word">
+										<?php echo $item->detailsurl; ?>
+											<?php if (!empty($item->infourl)) : ?>
+												<br>
+												<a href="<?php echo $item->infourl; ?>" target="_blank" rel="noopener noreferrer"><?php echo $this->escape(trim($item->infourl)); ?></a>
+											<?php endif; ?>
+										</div>
 										<?php if($item->isMissingDownloadKey): ?>
-										<br/>
 										<span class="badge badge-warning">
 											<span class="hasPopover"
 												  title="<?= Text::_('COM_INSTALLER_DOWNLOADKEY_MISSING_LABEL') ?>"
@@ -145,15 +148,6 @@ $listDirn  = $this->escape($this->state->get('list.direction'));
 									</td>
 									<td class="d-none d-md-table-cell">
 										<?php echo $item->install_type; ?>
-									</td>
-									<td class="d-none d-md-table-cell">
-										<span class="break-word">
-										<?php echo $item->detailsurl; ?>
-											<?php if (!empty($item->infourl)) : ?>
-												<br>
-												<a href="<?php echo $item->infourl; ?>" target="_blank" rel="noopener noreferrer"><?php echo $this->escape($item->infourl); ?></a>
-											<?php endif; ?>
-										</span>
 									</td>
 								</tr>
 							<?php endforeach; ?>
