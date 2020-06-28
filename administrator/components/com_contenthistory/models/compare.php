e1da08200013c79b73e1483b147ffb5fbef87a69
SharkyKZ <sharkykz@gmail.com>
2020-06-28 22:02:02 +0300

    Set error when using invalid IDs in history version comparison (#29683)
    * Set error when history version not found
    
    * Set errors when invalid IDs are passed
    
    * Alpha sort
    
    Co-authored-by: Quy <quy@fluxbb.org>
    
    Co-authored-by: Quy <quy@fluxbb.org>

--- administrator/components/com_contenthistory/models/compare.php
+++ administrator/components/com_contenthistory/models/compare.php
@@ -37,6 +37,14 @@ class ContenthistoryModelCompare extends JModelItem
 
 		$id1 = $input->getInt('id1');
 		$id2 = $input->getInt('id2');
+
+		if (!$id1 || \is_array($id1) || !$id2 || \is_array($id2))
+		{
+			$this->setError(\JText::_('COM_CONTENTHISTORY_ERROR_INVALID_ID'));
+
+			return false;
+		}
+
 		$result = array();
 
 		if ($table1->load($id1) && $table2->load($id2))
@@ -48,6 +56,8 @@ class ContenthistoryModelCompare extends JModelItem
 
 			if (!$contentTypeTable->load($ucmTypeId))
 			{
+				$this->setError(\JText::_('COM_CONTENTHISTORY_ERROR_FAILED_LOADING_CONTENT_TYPE'));
+
 				// Assume a failure to load the content type means broken data, abort mission
 				return false;
 			}
@@ -102,6 +112,8 @@ class ContenthistoryModelCompare extends JModelItem
 				return $result;
 			}
 		}
+
+		$this->setError(\JText::_('COM_CONTENTHISTORY_ERROR_VERSION_NOT_FOUND'));
 
 		return false;
 	}
