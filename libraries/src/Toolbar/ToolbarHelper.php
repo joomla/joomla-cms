fe97dcb540ace4a903c0175383a8f3eda3e0ecab
Florian Häusler <florian@hr-it-solutions.com>
2020-07-06 15:38:52 +0200

    Update libraries/src/Toolbar/ToolbarHelper.php
    Co-authored-by: Richard Fath <richard67@users.noreply.github.com>

--- libraries/src/Toolbar/ToolbarHelper.php
+++ libraries/src/Toolbar/ToolbarHelper.php
@@ -44,7 +44,6 @@ abstract class ToolbarHelper
 		$app = Factory::getApplication();
 		$app->JComponentTitle = $html;
 		$title = strip_tags($title) . ' - ' . $app->get('sitename');
-            
 		if ($app->isClient('administrator'))
 		{
 			$title .= ' - ' . Text::_('JADMINISTRATION');
