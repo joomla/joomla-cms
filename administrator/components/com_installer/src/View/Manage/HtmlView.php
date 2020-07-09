c1e2e1dd79754056327bfe10a3ef2370457ed3bc
Quy <quy@fluxbb.org>
2020-07-09 15:39:05 -0700

    Remove import

--- administrator/components/com_installer/src/View/Manage/HtmlView.php
+++ administrator/components/com_installer/src/View/Manage/HtmlView.php
@@ -16,7 +16,6 @@ use Joomla\CMS\Helper\ContentHelper;
 use Joomla\CMS\MVC\View\GenericDataException;
 use Joomla\CMS\Pagination\Pagination;
 use Joomla\CMS\Toolbar\Toolbar;
-use Joomla\CMS\Toolbar\ToolbarHelper;
 use Joomla\Component\Installer\Administrator\View\Installer\HtmlView as InstallerViewDefault;
 
 /**
