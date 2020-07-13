835aa196134183e3822629b1dcdf23515d0e6524
Phil E. Taylor <phil@phil-taylor.com>
2020-07-13 15:59:18 +0100

    cs

--- plugins/workflow/featuring/featuring.php
+++ plugins/workflow/featuring/featuring.php
@@ -309,11 +309,11 @@ class PlgWorkflowFeaturing extends CMSPlugin implements SubscriberInterface
 	 *
 	 * @param WorkflowTransitionEvent $event
 	 *
-	 * @return void
+	 * @return   void
 	 *
 	 * @since   4.0.0
 	 */
-	public function onWorkflowAfterTransition(WorkflowTransitionEvent $event): void 
+	public function onWorkflowAfterTransition(WorkflowTransitionEvent $event): void
 	{
 		$context       = $event->getArgument('extension');
 		$extensionName = $event->getArgument('extensionName');
