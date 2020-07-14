ec7045ae2c600d0073e5c803ecfff3776502d7e2
Troy T. Hall <programming@hallhome.us>
2020-07-14 12:36:19 -0500

    removed $active_class from tbody

--- libraries/src/HTML/Helpers/JGrid.php
+++ libraries/src/HTML/Helpers/JGrid.php
@@ -129,7 +129,7 @@ abstract class JGrid
 		}
 		else
 		{
-			$html[] = '<span class="tbody-icon ' . $active_class . '-disabled disabled jgrid"';
+			$html[] = '<span class="tbody-icon jgrid';
 			$html[] = $tip ? ' aria-labelledby="' . $ariaid . '"' : '';
 			$html[] = '>';
 
