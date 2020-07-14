6f99992a0beb151a4451a9414391f0857d2f55ef
Troy T. Hall <programming@hallhome.us>
2020-07-14 11:22:43 -0500

    fix improper far to fas

--- libraries/src/HTML/Helpers/JGrid.php
+++ libraries/src/HTML/Helpers/JGrid.php
@@ -345,7 +345,7 @@ abstract class JGrid
 	 * @see     JHtmlJGrid::state()
 	 * @since   1.6
 	 */
-	public static function isdefault($value, $i, $prefix = '', $enabled = true, $checkbox = 'cb', $formId = null, $active_class = 'icon-color-featured fas fa-star', $inactive_class = 'icon-color-unfeatured far fa-star')
+	public static function isdefault($value, $i, $prefix = '', $enabled = true, $checkbox = 'cb', $formId = null, $active_class = 'icon-color-featured fas fa-star', $inactive_class = 'icon-color-unfeatured fas fa-star')
 	{
 		if (is_array($prefix))
 		{
