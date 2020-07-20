0e106509babed1a1f5fd6a2d5bdbac0eea98b96c
Hans Kuijpers <hans2103@gmail.com>
2020-07-20 21:06:07 +0200

    Feature/draggable typo fixes (#29987)
    Replace "dragable" by "draggable"
    
    * Searched for dragable and replaced by draggable

--- administrator/components/com_plugins/tmpl/plugins/default.php
+++ administrator/components/com_plugins/tmpl/plugins/default.php
@@ -79,7 +79,7 @@ if ($saveOrder)
 					$canCheckin = $user->authorise('core.manage',     'com_checkin') || $item->checked_out == $user->get('id') || is_null($item->checked_out);
 					$canChange  = $user->authorise('core.edit.state', 'com_plugins') && $canCheckin;
 					?>
-					<tr class="row<?php echo $i % 2; ?>" data-dragable-group="<?php echo $item->folder; ?>">
+					<tr class="row<?php echo $i % 2; ?>" data-draggable-group="<?php echo $item->folder; ?>">
 						<td class="text-center">
 							<?php echo HTMLHelper::_('grid.id', $i, $item->extension_id); ?>
 						</td>
