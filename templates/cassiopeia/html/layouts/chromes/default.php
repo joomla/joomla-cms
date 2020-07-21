3aae9c4cdbe0a534f6db485b8168ef3aa6103138
Hans Kuijpers <hans2103@gmail.com>
2020-07-21 08:33:22 +0200

    :art: swap namespace lines with defined. @quy

--- templates/cassiopeia/html/layouts/chromes/default.php
+++ templates/cassiopeia/html/layouts/chromes/default.php
@@ -7,9 +7,9 @@
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
-use Joomla\Utilities\ArrayHelper;
-
 defined('_JEXEC') or die;
+
+use Joomla\Utilities\ArrayHelper;
 
 $module  = $displayData['module'];
 $params  = $displayData['params'];
