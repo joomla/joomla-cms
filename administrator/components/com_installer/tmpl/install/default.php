dda242580bd8200d8e16dfbfcf57981d74435b73
SharkyKZ <sharkykz@gmail.com>
2020-07-22 09:19:32 +0300

    Remove tabset when no plugins enabled

--- administrator/components/com_installer/tmpl/install/default.php
+++ administrator/components/com_installer/tmpl/install/default.php
@@ -28,9 +28,10 @@ $wa->useScript('core')
 	->usePreset('com_installer.installer')
 	->useScript('webcomponent.core-loader');
 
-$app = Factory::getApplication();
-?>
+$app  = Factory::getApplication();
+$tabs = $app->triggerEvent('onInstallerAddInstallationTab', []);
 
+?>
 <div id="installer-install" class="clearfix">
 
 	<form enctype="multipart/form-data" action="<?php echo Route::_('index.php?option=com_installer&view=install'); ?>" method="post" name="adminForm" id="adminForm">
@@ -41,31 +42,33 @@ $app = Factory::getApplication();
 					<?php if ($this->showMessage) : ?>
 						<?php echo $this->loadTemplate('message'); ?>
 					<?php endif; ?>
-					<?php $tabs = $app->triggerEvent('onInstallerAddInstallationTab', []); ?>
-					<?php echo HTMLHelper::_('uitab.startTabSet', 'myTab', ['active' => $tabs[0]['name'] ?? '']); ?>
-					<?php // Show installation tabs ?>
-					<?php foreach ($tabs as $tab) : ?>
-						<?php echo HTMLHelper::_('uitab.addTab', 'myTab', $tab['name'], $tab['label']); ?>
-						<fieldset class="uploadform option-fieldset options-form">
-							<?php echo $tab['content']; ?>
-						</fieldset>
-						<?php echo HTMLHelper::_('uitab.endTab'); ?>
-					<?php endforeach; ?>
+
 					<?php if (!$tabs) : ?>
 						<?php $app->enqueueMessage(Text::_('COM_INSTALLER_NO_INSTALLATION_PLUGINS_FOUND'), 'warning'); ?>
 					<?php endif; ?>
 
-					<?php if ($this->ftp) : ?>
-						<?php echo HTMLHelper::_('uitab.addTab', 'myTab', 'ftp', Text::_('COM_INSTALLER_MSG_DESCFTPTITLE')); ?>
-						<?php echo $this->loadTemplate('ftp'); ?>
-						<?php echo HTMLHelper::_('uitab.endTab'); ?>
+					<?php if ($tabs || $this->ftp) : ?>
+						<?php echo HTMLHelper::_('uitab.startTabSet', 'myTab', ['active' => $tabs[0]['name'] ?? '']); ?>
+						<?php // Show installation tabs ?>
+						<?php foreach ($tabs as $tab) : ?>
+							<?php echo HTMLHelper::_('uitab.addTab', 'myTab', $tab['name'], $tab['label']); ?>
+							<fieldset class="uploadform option-fieldset options-form">
+								<?php echo $tab['content']; ?>
+							</fieldset>
+							<?php echo HTMLHelper::_('uitab.endTab'); ?>
+						<?php endforeach; ?>
+
+						<?php if ($this->ftp) : ?>
+							<?php echo HTMLHelper::_('uitab.addTab', 'myTab', 'ftp', Text::_('COM_INSTALLER_MSG_DESCFTPTITLE')); ?>
+							<?php echo $this->loadTemplate('ftp'); ?>
+							<?php echo HTMLHelper::_('uitab.endTab'); ?>
+						<?php endif; ?>
+						<?php echo HTMLHelper::_('uitab.endTabSet'); ?>
 					<?php endif; ?>
 
 					<input type="hidden" name="installtype" value="">
 					<input type="hidden" name="task" value="install.install">
 					<?php echo HTMLHelper::_('form.token'); ?>
-
-					<?php echo HTMLHelper::_('uitab.endTabSet'); ?>
 				</div>
 			</div>
 		</div>
