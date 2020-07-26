cda76865c15690d99091589b0103bfccf6648fc8
dGrammatiko <d.grammatiko@gmail.com>
2020-07-26 18:48:11 +0200

    Fix the sql

--- installation/sql/mysql/base.sql
+++ installation/sql/mysql/base.sql
@@ -796,8 +796,8 @@ CREATE TABLE IF NOT EXISTS `#__template_styles` (
 --
 
 INSERT INTO `#__template_styles` (`id`, `template`, `client_id`, `home`, `title`, `inheritable`, `parent`, `params`) VALUES
-(10, 'atum', 1, '1', 'atum - Default', '0', '0', ''),
-(11, 'cassiopeia', 0, '1', 'cassiopeia - Default', '0', '0', '{"logoFile":"","fluidContainer":"0","sidebarLeftWidth":"3","sidebarRightWidth":"3"}');
+(10, 'atum', 1, '1', 'atum - Default', '0', '', ''),
+(11, 'cassiopeia', 0, '1', 'cassiopeia - Default', '0', '', '{"logoFile":"","fluidContainer":"0","sidebarLeftWidth":"3","sidebarRightWidth":"3"}');
 
 -- --------------------------------------------------------
 
