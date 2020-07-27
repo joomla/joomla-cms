3d20ace1f29c16b1da70482c0937865f2dc9218f
dGrammatiko <d.grammatiko@gmail.com>
2020-07-27 20:10:08 +0200

    Update InstallationApplication.php

--- installation/src/Application/InstallationApplication.php
+++ installation/src/Application/InstallationApplication.php
@@ -401,7 +401,7 @@ final class InstallationApplication extends CMSApplication
 	public function getTemplate($params = false)
 	{
 		// Pass the parent template to the state
-		$this->set('templateInherits', '');
+		$this->set('themeInherits', '');
 
 		if ($params)
 		{
