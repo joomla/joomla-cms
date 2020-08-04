62973cd8d5845e49f15068d98577222ee66dd56e
Hans Kuijpers <hans2103@gmail.com>
2020-08-04 10:36:47 +0200

    [4.0] change form in table by form in control-groups (#30168)
    * :recycle: change form in table by form in control-groups
    
    * replace form in table by form in control-groups
    
    * :art: code style as suggested by @quy
    
    * :bug: remove setting unneeded classname via JS
    
    * :bug: remove setting unneeded classname via JS
    
    * only execute when displayStyle !== false
    
    * only execute when displayStyle !== false
    
    * as requested by @Quy in https://github.com/joomla/joomla-cms/issues/29901#issuecomment-663652354
    
    * replace indent spaces by indent tabs
    
    * rewrite javascript to toggle className FTP fields

--- administrator/components/com_joomlaupdate/tmpl/joomlaupdate/default_update.php
+++ administrator/components/com_joomlaupdate/tmpl/joomlaupdate/default_update.php
@@ -9,6 +9,7 @@
 
 defined('_JEXEC') or die;
 
+use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
 
 /** @var \Joomla\Component\Joomlaupdate\Administrator\View\Joomlaupdate\Html $this */
@@ -22,107 +23,122 @@ use Joomla\CMS\Language\Text;
 		<?php echo Text::sprintf($this->langKey, $this->updateSourceKey); ?>
 	</p>
 
-	<table class="table">
-		<tbody>
-			<tr>
-				<td>
-					<?php echo Text::_('COM_JOOMLAUPDATE_VIEW_DEFAULT_INSTALLED'); ?>
-				</td>
-				<td>
-					<?php echo '&#x200E;' . $this->updateInfo['installed']; ?>
-				</td>
-			</tr>
-			<tr>
-				<td>
-					<?php echo Text::_('COM_JOOMLAUPDATE_VIEW_DEFAULT_LATEST'); ?>
-				</td>
-				<td>
-					<?php echo '&#x200E;' . $this->updateInfo['latest']; ?>
-				</td>
-			</tr>
-			<tr>
-				<td>
-					<?php echo Text::_('COM_JOOMLAUPDATE_VIEW_DEFAULT_PACKAGE'); ?>
-				</td>
-				<td>
-					<a href="<?php echo $this->updateInfo['object']->downloadurl->_data; ?>" target="_blank" rel="noopener noreferrer"
-						title="<?php echo Text::sprintf('JBROWSERTARGET_DOWNLOAD', $this->updateInfo['object']->downloadurl->_data); ?>">
-						<?php echo $this->updateInfo['object']->downloadurl->_data; ?>
-					</a>
-				</td>
-			</tr>
-			<?php if (isset($this->updateInfo['object']->get('infourl')->_data)
-				&& isset($this->updateInfo['object']->get('infourl')->title)) : ?>
-				<tr>
-					<td>
-						<?php echo Text::_('COM_JOOMLAUPDATE_VIEW_DEFAULT_INFOURL'); ?>
-					</td>
-					<td>
-						<a href="<?php echo $this->updateInfo['object']->get('infourl')->_data; ?>" target="_blank" rel="noopener noreferrer"
-							title="<?php echo Text::sprintf('JBROWSERTARGET_NEW_TITLE', $this->updateInfo['object']->get('infourl')->title); ?>">
-							<?php echo $this->updateInfo['object']->get('infourl')->title; ?>
-						</a>
-					</td>
-				</tr>
-			<?php endif; ?>
-			<tr>
-				<td>
-					<?php echo Text::_('COM_JOOMLAUPDATE_VIEW_DEFAULT_METHOD'); ?>
-				</td>
-				<td>
-					<?php echo $this->methodSelect; ?>
-				</td>
-			</tr>
-			<tr id="row_ftp_hostname" <?php echo $this->ftpFieldsDisplay; ?>>
-				<td>
-					<?php echo Text::_('COM_JOOMLAUPDATE_VIEW_DEFAULT_FTP_HOSTNAME'); ?>
-				</td>
-				<td>
-					<input class="form-control" type="text" name="ftp_host" value="<?php echo $this->ftp['host']; ?>">
-				</td>
-			</tr>
-			<tr id="row_ftp_port" <?php echo $this->ftpFieldsDisplay; ?>>
-				<td>
-					<?php echo Text::_('COM_JOOMLAUPDATE_VIEW_DEFAULT_FTP_PORT'); ?>
-				</td>
-				<td>
-					<input class="form-control" type="text" name="ftp_port" value="<?php echo $this->ftp['port']; ?>">
-				</td>
-			</tr>
-			<tr id="row_ftp_username" <?php echo $this->ftpFieldsDisplay; ?>>
-				<td>
-					<?php echo Text::_('COM_JOOMLAUPDATE_VIEW_DEFAULT_FTP_USERNAME'); ?>
-				</td>
-				<td>
-					<input class="form-control" type="text" name="ftp_user" value="<?php echo $this->ftp['username']; ?>">
-				</td>
-			</tr>
-			<tr id="row_ftp_password" <?php echo $this->ftpFieldsDisplay; ?>>
-				<td>
-					<?php echo Text::_('COM_JOOMLAUPDATE_VIEW_DEFAULT_FTP_PASSWORD'); ?>
-				</td>
-				<td>
-					<input class="form-control" type="password" name="ftp_pass" value="<?php echo $this->ftp['password']; ?>">
-				</td>
-			</tr>
-			<tr id="row_ftp_directory" <?php echo $this->ftpFieldsDisplay; ?>>
-				<td>
-					<?php echo Text::_('COM_JOOMLAUPDATE_VIEW_DEFAULT_FTP_DIRECTORY'); ?>
-				</td>
-				<td>
-					<input class="form-control" type="text" name="ftp_root" value="<?php echo $this->ftp['directory']; ?>">
-				</td>
-			</tr>
-		</tbody>
-		<tfoot>
-			<tr>
-				<td>&nbsp;</td>
-				<td>
-					<button class="btn btn-primary" type="submit">
-						<?php echo Text::_('COM_JOOMLAUPDATE_VIEW_DEFAULT_INSTALLUPDATE'); ?>
-					</button>
-				</td>
-			</tr>
-		</tfoot>
-	</table>
+	<div class="control-group">
+		<div class="control-label">
+			<?php echo Text::_('COM_JOOMLAUPDATE_VIEW_DEFAULT_INSTALLED'); ?>
+		</div>
+		<div class="controls">
+			<?php echo '&#x200E;' . $this->updateInfo['installed']; ?>
+		</div>
+	</div>
+
+	<div class="control-group">
+		<div class="control-label">
+			<?php echo Text::_('COM_JOOMLAUPDATE_VIEW_DEFAULT_LATEST'); ?>
+		</div>
+		<div class="controls">
+			<?php echo '&#x200E;' . $this->updateInfo['latest']; ?>
+		</div>
+	</div>
+
+	<div class="control-group">
+		<div class="control-label">
+			<?php echo Text::_('COM_JOOMLAUPDATE_VIEW_DEFAULT_PACKAGE'); ?>
+		</div>
+		<div class="controls">
+			<?php echo HTMLHelper::link(
+				$this->updateInfo['object']->downloadurl->_data,
+				$this->updateInfo['object']->downloadurl->_data,
+				[
+					'target' => '_blank',
+					'rel'    => 'noopener noreferrer',
+					'title'  => Text::sprintf('JBROWSERTARGET_DOWNLOAD', $this->updateInfo['object']->downloadurl->_data)
+				]
+			); ?>
+		</div>
+	</div>
+
+	<?php if (isset($this->updateInfo['object']->get('infourl')->_data)
+		&& isset($this->updateInfo['object']->get('infourl')->title)) : ?>
+		<div class="control-group">
+			<div class="control-label">
+				<?php echo Text::_('COM_JOOMLAUPDATE_VIEW_DEFAULT_INFOURL'); ?>
+			</div>
+			<div class="controls">
+				<?php echo HTMLHelper::link(
+					$this->updateInfo['object']->get('infourl')->_data,
+					$this->updateInfo['object']->get('infourl')->title,
+					[
+						'target' => '_blank',
+						'rel'    => 'noopener noreferrer',
+						'title'  => Text::sprintf('JBROWSERTARGET_NEW_TITLE', $this->updateInfo['object']->get('infourl')->title)
+					]
+				); ?>
+			</div>
+		</div>
+	<?php endif; ?>
+
+	<div class="control-group">
+		<label for="extraction_method" class="control-label">
+			<?php echo Text::_('COM_JOOMLAUPDATE_VIEW_DEFAULT_METHOD'); ?>
+		</label>
+		<div class="controls">
+			<?php echo $this->methodSelect; ?>
+		</div>
+	</div>
+
+	<div class="control-group" id="row_ftp_hostname" <?php echo $this->ftpFieldsDisplay; ?>>
+		<label for="ftp_host" class="control-label">
+			<?php echo Text::_('COM_JOOMLAUPDATE_VIEW_DEFAULT_FTP_HOSTNAME'); ?>
+		</label>
+		<div class="controls">
+			<input type="text" id="ftp_host" name="ftp_host" class="form-control" value="<?php echo $this->ftp['host']; ?>">
+		</div>
+	</div>
+
+	<div class="control-group" id="row_ftp_port" <?php echo $this->ftpFieldsDisplay; ?>>
+		<label for="ftp_port" class="control-label">
+			<?php echo Text::_('COM_JOOMLAUPDATE_VIEW_DEFAULT_FTP_PORT'); ?>
+		</label>
+		<div class="controls">
+			<input type="text" id="ftp_port" name="ftp_port" class="form-control" value="<?php echo $this->ftp['port']; ?>">
+		</div>
+	</div>
+
+	<div class="control-group" id="row_ftp_username" <?php echo $this->ftpFieldsDisplay; ?>>
+		<label for="ftp_user" class="control-label">
+			<?php echo Text::_('COM_JOOMLAUPDATE_VIEW_DEFAULT_FTP_USERNAME'); ?>
+		</label>
+		<div class="controls">
+			<input type="text" id="ftp_user" name="ftp_user" class="form-control" value="<?php echo $this->ftp['username']; ?>">
+		</div>
+	</div>
+
+	<div class="control-group" id="row_ftp_password" <?php echo $this->ftpFieldsDisplay; ?>>
+		<label for="ftp_pass" class="control-label">
+			<?php echo Text::_('COM_JOOMLAUPDATE_VIEW_DEFAULT_FTP_PASSWORD'); ?>
+		</label>
+		<div class="controls">
+			<input type="password" id="ftp_pass" name="ftp_pass" class="form-control" value="<?php echo $this->ftp['password']; ?>">
+		</div>
+	</div>
+
+	<div class="control-group" id="row_ftp_directory" <?php echo $this->ftpFieldsDisplay; ?>>
+		<label for="ftp_root" class="control-label">
+			<?php echo Text::_('COM_JOOMLAUPDATE_VIEW_DEFAULT_FTP_DIRECTORY'); ?>
+		</label>
+		<div class="controls">
+			<input type="text" id="ftp_root" name="ftp_root" class="form-control" value="<?php echo $this->ftp['directory']; ?>">
+		</div>
+	</div>
+
+	<hr>
+
+	<div class="control-group">
+		<div class="controls">
+			<button class="btn btn-warning" type="submit">
+				<?php echo Text::_('COM_JOOMLAUPDATE_VIEW_DEFAULT_INSTALLUPDATE'); ?>
+			</button>
+		</div>
+	</div>
 </fieldset>
