340a087a70443ba866d2fe5730e312282b6cc37d
Hans Kuijpers <hans2103@gmail.com>
2020-08-08 14:42:08 +0200

    [4.0] com_joomlaupdate reference to class HtmlView (#30181)
    * remove obsolete reference. It is not used
    
    * fix reference to class HtmlView
    
    * apply namespacing

--- administrator/components/com_joomlaupdate/tmpl/joomlaupdate/default_reinstall.php
+++ administrator/components/com_joomlaupdate/tmpl/joomlaupdate/default_reinstall.php
@@ -12,8 +12,9 @@ defined('_JEXEC') or die;
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\Updater\Update;
+use Joomla\Component\Joomlaupdate\Administrator\View\Joomlaupdate\HtmlView;
 
-/** @var \Joomla\Component\Joomlaupdate\Administrator\View\Joomlaupdate\HtmlView $this */
+/** @var HtmlView $this */
 ?>
 <fieldset class="options-form">
 	<legend>
