a8be0010d4ce1a581a22b8abd84a73776c858398
Hans Kuijpers <hans2103@gmail.com>
2020-08-10 21:09:32 +0200

    add all classNames in array and implode when needed

--- templates/cassiopeia/html/mod_menu/metismenu.php
+++ templates/cassiopeia/html/mod_menu/metismenu.php
@@ -28,52 +28,52 @@ $ulAttribs = [
 <?php foreach ($list as $i => &$item)
 {
 	$itemParams = $item->getParams();
-	$class      = 'nav-item item-' . $item->id;
+	$class[]    = 'metismenu-item item-' . $item->id;
 
 	if ($item->id == $default_id)
 	{
-		$class .= ' default';
+		$class[] = 'default';
 	}
 
 	if ($item->id == $active_id || ($item->type === 'alias' && $itemParams->get('aliasoptions') == $active_id))
 	{
-		$class .= ' current';
+		$class[] = 'current';
 	}
 
 	if (in_array($item->id, $path))
 	{
-		$class .= ' active';
+		$class[] = 'active';
 	}
-	elseif ($item->type === 'alias')
+	else if ($item->type === 'alias')
 	{
 		$aliasToId = $itemParams->get('aliasoptions');
 
 		if (count($path) > 0 && $aliasToId == $path[count($path) - 1])
 		{
-			$class .= ' active';
+			$class[] = 'active';
 		}
-		elseif (in_array($aliasToId, $path))
+		else if (in_array($aliasToId, $path))
 		{
-			$class .= ' alias-parent-active';
+			$class[] = 'alias-parent-active';
 		}
 	}
 
 	if ($item->type === 'separator')
 	{
-		$class .= ' divider';
+		$class[] = 'divider';
 	}
 
 	if ($item->deeper)
 	{
-		$class .= ' deeper';
+		$class[] = 'deeper';
 	}
 
 	if ($item->parent)
 	{
-		$class .= ' parent';
+		$class[] = 'parent';
 	}
 
-	echo '<li class="' . $class . '">';
+	echo '<li class="' . implode(' ', $class) . '">';
 
 	switch ($item->type) :
 		case 'separator':
