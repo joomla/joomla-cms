1cf2e66cdfe2ba11a3831c876770c98a1e6c4d23
George Wilson <georgejameswilson@googlemail.com>
2020-08-13 22:03:50 +0100

    Delete 4.0.0-2020-06-25.sql

--- administrator/components/com_admin/sql/updates/mysql/4.0.0-2020-06-25.sql
+++
@@ -1,6 +0,0 @@
---
--- The following statements have to be disabled because the new database columns
--- are added in Joomla 3.10 already. See Pull Request no. 30333 for details.
---
--- ALTER TABLE `#__template_styles` ADD COLUMN `inheritable` tinyint(1) NOT NULL DEFAULT 0;
--- ALTER TABLE `#__template_styles` ADD COLUMN `parent` varchar(50) DEFAULT '';
