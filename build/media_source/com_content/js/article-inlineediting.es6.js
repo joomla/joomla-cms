82bf9abd814b1c6a5eddf27184b342b5d1dfc9eb
John Deutesfeld <johndeutesfeld@gmail.com>
2020-08-17 17:33:27 +0200

    adjustments for inline editing

--- build/media_source/com_content/js/article-inlineediting.es6.js
+++ build/media_source/com_content/js/article-inlineediting.es6.js
@@ -3,59 +3,72 @@
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 ((document,submitForm) => {
-  'use strict';
+	'use strict';
 
-  // Selectors used by this script
-  const buttonDataSelector = 'data-submit-task';
-  const formId = 'adminForm';
+	// Selectors used by this script
+	const buttonDataSelector = 'data-submit-task';
+	const formId = 'adminForm';
 
-  tinymce.init({
-    selector: '.editable',
-    menubar: false,
-    inline: true,
-    plugins: [
-      'link',
-      'lists',
-      'powerpaste',
-      'autolink',
-      'tinymcespellchecker'
-    ],
-    toolbar: [
-      'undo redo | bold italic underline | fontselect fontsizeselect',
-      'forecolor backcolor | alignleft aligncenter alignright alignfull | numlist bullist outdent indent'
-    ]
-  });
+	tinymce.init({
+		selector: '.editable',
+		menubar: false,
+		inline: true,
+		plugins: [
+		'link',
+		'lists',
+		'powerpaste',
+		'autolink',
+		'tinymcespellchecker'
+		],
+		toolbar: [
+		'undo redo | bold italic underline | fontselect fontsizeselect',
+		'forecolor backcolor | alignleft aligncenter alignright alignfull | numlist bullist outdent indent'
+		]
+		}
+	);
   /**
    * Submit the task
    * @param task
    */
   const submitTask = (task) => {
-    const form = document.getElementById(formId);
-    if (task === 'article.cancel' || document.formvalidator.isValid(form)) {
-      submitForm(task, form);
-    }
-  };
+		const form = document.getElementById(formId);
+		if (task === 'article.cancel' || document.formvalidator.isValid(form)) {
+      var title = document.createElement("input");
+      title.type = "hidden";
+      title.name = "jform[title]";
+      title.value = document.getElementById("headline").innerHTML;
+      var articletext = document.createElement("input");
+      articletext.type = "hidden";
+      articletext.name = "jform[articletext]";
+      articletext.value = document.getElementById("articlebody").innerHTML;
 
-  // Register events
-  document.addEventListener('DOMContentLoaded', () => {
+			form.appendChild(title)
+      form.appendChild(articletext);
+		  submitForm(task, form);
+		}
 
-    var x = 5;
-    console.log(Joomla.getOptions('original_article_text'));
+	};
 
-    var articleBody = document.getElementById("test");
-    articleBody.addEventListener('onmousedown', (e) => {
-      articleBody.innerHTML = Joomla.getOptions('original_article_text');
-      });
+	// Register events
+	document.addEventListener('DOMContentLoaded', () => {
+		console.log(Joomla.getOptions('original_article_text'));
+		var articleBody = document.getElementById("articlebody");
+		articleBody.addEventListener('onmousedown', (e) => {
+			articleBody.innerHTML = Joomla.getOptions('original_article_text');
+			}
+		);
 
-    console.log(document.getElementById("test").innerHTML);
-
-    const buttons = [].slice.call(document.querySelectorAll(`[${buttonDataSelector}]`));
-    buttons.forEach((button) => {
-      button.addEventListener('click', (e) => {
-        e.preventDefault();
-        const task = e.target.getAttribute(buttonDataSelector);
-        submitTask(task);
-      });
-    });
-  });
+		console.log(document.getElementById("articlebody").innerHTML);
+		const buttons = [].slice.call(document.querySelectorAll(`[${buttonDataSelector}]`));
+		buttons.forEach((button) => {
+			button.addEventListener('click', (e) => {
+				e.preventDefault();
+				const task = e.target.getAttribute(buttonDataSelector);
+				submitTask(task);
+				}
+			);
+			}
+		);
+		}
+	);
 })(document, Joomla.submitform);
