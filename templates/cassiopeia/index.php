bbfdaa7823c630bc510f7c6665f589da6455adac
Viviana Menzel <vivianamenzel@dr-menzel-it.de>
2020-08-24 16:28:32 +0200

    Add if for menu or search and align search to right

--- templates/cassiopeia/index.php
+++ templates/cassiopeia/index.php
@@ -103,13 +103,15 @@ $stickyHeader = $this->params->get('stickyHeader') ? 'position-sticky sticky-top
 				<?php endif; ?>
 			</div>
 		</div>
-		<?php if ($this->countModules('menu')) : ?>
+		<?php if ($this->countModules('menu') || $this->countModules('search')) : ?>
 			<nav class="grid-child navbar navbar-expand-lg">
 				<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="<?php echo Text::_('TPL_CASSIOPEIA_TOGGLE'); ?>">
 					<span class="fas fa-bars" aria-hidden="true"></span>
 				</button>
 				<div class="collapse navbar-collapse" id="navbar">
-					<jdoc:include type="modules" name="menu" style="none" />
+					<?php if ($this->countModules('menu')) : ?>
+						<jdoc:include type="modules" name="menu" style="none" />
+					<?php endif; ?>
 					<?php if ($this->countModules('search')) : ?>
 						<div class="form-inline">
 							<jdoc:include type="modules" name="search" style="none" />
