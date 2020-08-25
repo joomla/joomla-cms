fc5569531413401cbd1f4e222a6b8ed8dc32d88d
wilsonge <georgejameswilson@googlemail.com>
2020-08-25 18:16:57 +0100

    Move tests to bearer token approach

--- tests/Codeception/api/com_contact/ContactCest.php
+++ tests/Codeception/api/com_contact/ContactCest.php
@@ -29,6 +29,14 @@ class ContactCest
 	 */
 	public function _before(ApiTester $I)
 	{
+		// TODO: Improve this to retrieve a specific ID to replace with a known ID
+		$desiredUserId = 3;
+		$I->updateInDatabase('users', ['id' => 3], []);
+		$I->updateInDatabase('user_usergroup_map', ['user_id' => 3], []);
+		$enabledData = ['user_id' => $desiredUserId, 'profile_key' => 'joomlatoken.enabled', 'profile_value' => 1];
+		$tokenData = ['user_id' => $desiredUserId, 'profile_key' => 'joomlatoken.token', 'profile_value' => 'dOi2m1NRrnBHlhaWK/WWxh3B5tqq1INbdf4DhUmYTI4='];
+		$I->haveInDatabase('user_profiles', $enabledData);
+		$I->haveInDatabase('user_profiles', $tokenData);
 	}
 
 	/**
@@ -57,7 +65,7 @@ class ContactCest
 	 */
 	public function testCrudOnContact(ApiTester $I)
 	{
-		$I->amHttpAuthenticated('admin', 'admin');
+		$I->amBearerAuthenticated('c2hhMjU2OjM6ZTJmMjJlYTNlNTU0NmM1MDJhYTIzYzMwN2MxYzAwZTQ5NzJhMWRmOTUyNjY5MTk2YjE5ODJmZWMwZTcxNzgwMQ==');
 		$I->haveHttpHeader('Content-Type', 'application/json');
 		$I->haveHttpHeader('Accept', 'application/vnd.api+json');
 
@@ -72,18 +80,18 @@ class ContactCest
 
 		$I->seeResponseCodeIs(HttpCode::OK);
 
-		$I->amHttpAuthenticated('admin', 'admin');
+		$I->amBearerAuthenticated('c2hhMjU2OjM6ZTJmMjJlYTNlNTU0NmM1MDJhYTIzYzMwN2MxYzAwZTQ5NzJhMWRmOTUyNjY5MTk2YjE5ODJmZWMwZTcxNzgwMQ==');
 		$I->haveHttpHeader('Accept', 'application/vnd.api+json');
 		$I->sendGET('/contact/1');
 		$I->seeResponseCodeIs(HttpCode::OK);
 
-		$I->amHttpAuthenticated('admin', 'admin');
+		$I->amBearerAuthenticated('c2hhMjU2OjM6ZTJmMjJlYTNlNTU0NmM1MDJhYTIzYzMwN2MxYzAwZTQ5NzJhMWRmOTUyNjY5MTk2YjE5ODJmZWMwZTcxNzgwMQ==');
 		$I->haveHttpHeader('Content-Type', 'application/json');
 		$I->haveHttpHeader('Accept', 'application/vnd.api+json');
 		$I->sendPATCH('/contact/1', ['name' => 'Frankie Blogs', 'state' => -2]);
 		$I->seeResponseCodeIs(HttpCode::OK);
 
-		$I->amHttpAuthenticated('admin', 'admin');
+		$I->amBearerAuthenticated('c2hhMjU2OjM6ZTJmMjJlYTNlNTU0NmM1MDJhYTIzYzMwN2MxYzAwZTQ5NzJhMWRmOTUyNjY5MTk2YjE5ODJmZWMwZTcxNzgwMQ==');
 		$I->haveHttpHeader('Accept', 'application/vnd.api+json');
 		$I->sendDELETE('/contact/1');
 		$I->seeResponseCodeIs(HttpCode::NO_CONTENT);
@@ -102,7 +110,7 @@ class ContactCest
 	 */
 	public function testCrudOnCategory(ApiTester $I)
 	{
-		$I->amHttpAuthenticated('admin', 'admin');
+		$I->amBearerAuthenticated('c2hhMjU2OjM6ZTJmMjJlYTNlNTU0NmM1MDJhYTIzYzMwN2MxYzAwZTQ5NzJhMWRmOTUyNjY5MTk2YjE5ODJmZWMwZTcxNzgwMQ==');
 		$I->haveHttpHeader('Content-Type', 'application/json');
 		$I->haveHttpHeader('Accept', 'application/vnd.api+json');
 
@@ -118,18 +126,18 @@ class ContactCest
 
 		$I->seeResponseCodeIs(HttpCode::OK);
 
-		$I->amHttpAuthenticated('admin', 'admin');
+		$I->amBearerAuthenticated('c2hhMjU2OjM6ZTJmMjJlYTNlNTU0NmM1MDJhYTIzYzMwN2MxYzAwZTQ5NzJhMWRmOTUyNjY5MTk2YjE5ODJmZWMwZTcxNzgwMQ==');
 		$I->haveHttpHeader('Accept', 'application/vnd.api+json');
 		$I->sendGET('/contact/categories/8');
 		$I->seeResponseCodeIs(HttpCode::OK);
 
-		$I->amHttpAuthenticated('admin', 'admin');
+		$I->amBearerAuthenticated('c2hhMjU2OjM6ZTJmMjJlYTNlNTU0NmM1MDJhYTIzYzMwN2MxYzAwZTQ5NzJhMWRmOTUyNjY5MTk2YjE5ODJmZWMwZTcxNzgwMQ==');
 		$I->haveHttpHeader('Content-Type', 'application/json');
 		$I->haveHttpHeader('Accept', 'application/vnd.api+json');
 		$I->sendPATCH('/contact/categories/8', ['title' => 'Another Title']);
 		$I->seeResponseCodeIs(HttpCode::OK);
 
-		$I->amHttpAuthenticated('admin', 'admin');
+		$I->amBearerAuthenticated('c2hhMjU2OjM6ZTJmMjJlYTNlNTU0NmM1MDJhYTIzYzMwN2MxYzAwZTQ5NzJhMWRmOTUyNjY5MTk2YjE5ODJmZWMwZTcxNzgwMQ==');
 		$I->haveHttpHeader('Accept', 'application/vnd.api+json');
 		$I->sendDELETE('/contact/categories/8');
 		$I->seeResponseCodeIs(HttpCode::NO_CONTENT);
