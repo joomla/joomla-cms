f3fc0e437c487a3c6d7d40e96b5224b88271b7a7
wilsonge <georgejameswilson@googlemail.com>
2020-08-25 21:08:46 +0100

    Fix patch banner

--- tests/Codeception/api/com_banners/BannerCest.php
+++ tests/Codeception/api/com_banners/BannerCest.php
@@ -95,7 +95,9 @@ class BannerCest
 		$I->amBearerAuthenticated('c2hhMjU2OjM6ZTJmMjJlYTNlNTU0NmM1MDJhYTIzYzMwN2MxYzAwZTQ5NzJhMWRmOTUyNjY5MTk2YjE5ODJmZWMwZTcxNzgwMQ==');
 		$I->haveHttpHeader('Content-Type', 'application/json');
 		$I->haveHttpHeader('Accept', 'application/vnd.api+json');
-		$I->sendPATCH('/banners/1', ['name' => 'Different Custom Advert', 'state' => -2]);
+
+		// Category is a required field for this patch request for now TODO: Remove this dependency
+		$I->sendPATCH('/banners/1', ['name' => 'Different Custom Advert', 'state' => -2, 'catid' => 3]);
 		$I->seeResponseCodeIs(HttpCode::OK);
 
 		$I->amBearerAuthenticated('c2hhMjU2OjM6ZTJmMjJlYTNlNTU0NmM1MDJhYTIzYzMwN2MxYzAwZTQ5NzJhMWRmOTUyNjY5MTk2YjE5ODJmZWMwZTcxNzgwMQ==');
