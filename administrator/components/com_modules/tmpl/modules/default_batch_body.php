6b059febc2b2b59c18af36b5522146836cadcb66
brian teeman <brian@teeman.net>
2020-08-28 21:40:05 +0100

    revert

--- administrator/components/com_modules/tmpl/modules/default_batch_body.php
+++ administrator/components/com_modules/tmpl/modules/default_batch_body.php
@@ -29,6 +29,7 @@ $attr = array(
 );
 
 Text::script('JGLOBAL_SELECT_NO_RESULTS_MATCH');
+Text::script('JGLOBAL_SELECT_PRESS_TO_SELECT');
 
 $this->document->getWebAssetManager()
 	->usePreset('choicesjs')
