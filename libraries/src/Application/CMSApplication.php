c9f2ae6f58fe6c57e5dbc4057bd920f4fd8d8a4d
zero-24 <zero-24@users.noreply.github.com>
2020-08-30 22:36:55 +0200

    implement 2fa enforcment per usergroup

--- libraries/src/Application/CMSApplication.php
+++ libraries/src/Application/CMSApplication.php
@@ -1172,9 +1172,9 @@ abstract class CMSApplication extends WebApplication implements ContainerAwareIn
 	 */
 	protected function isTwoFactorAuthenticationRequired(): bool
 	{
-		$userId = $this->getIdentity()->id;
+		$user = $this->getIdentity();
 
-		if (!$userId)
+		if (!$user->id)
 		{
 			return false;
 		}
@@ -1185,7 +1185,22 @@ abstract class CMSApplication extends WebApplication implements ContainerAwareIn
 			return false;
 		}
 
-		$enforce2faOptions = ComponentHelper::getComponent('com_users')->getParams()->get('enforce_2fa_options', 0);
+		$comUsersParams = ComponentHelper::getComponent('com_users')->getParams();
+
+		// Chech whether we for 2fa for the logged in user.
+		$forced2faGroups = (array) $comUsersParams->get('enforce_2fa_usergroups', []);
+
+		if (!empty($forced2faGroups))
+		{
+			$userGroups = (array) $user->get('groups', []);
+
+			if (!array_intersect($forced2faGroups, $userGroups))
+			{
+				return false;
+			}
+		}
+
+		$enforce2faOptions = $comUsersParams->get('enforce_2fa_options', 0);
 
 		if ($enforce2faOptions == 0 || !$enforce2faOptions)
 		{
