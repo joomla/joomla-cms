1db735f22023807dd1a949ed8aa62069063c6684
Hans Kuijpers <hans2103@gmail.com>
2020-08-30 19:50:43 +0200

    copy core files to prepare alternate layout

---
+++ templates/cassiopeia/html/mod_menu/default_heading.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  mod_menu
+ *
+ * @copyright   Copyright (C) 2005 - 2020 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\HTML\HTMLHelper;
+
+$title      = $item->anchor_title ? ' title="' . $item->anchor_title . '"' : '';
+$anchor_css = $item->anchor_css ?: '';
+$linktype   = $item->title;
+
+if ($item->menu_image)
+{
+	if ($item->menu_image_css)
+	{
+		$image_attributes['class'] = $item->menu_image_css;
+		$linktype = HTMLHelper::_('image', $item->menu_image, $item->title, $image_attributes);
+	}
+	else
+	{
+		$linktype = HTMLHelper::_('image', $item->menu_image, $item->title);
+	}
+
+	if ($itemParams->get('menu_text', 1))
+	{
+		$linktype .= '<span class="image-title">' . $item->title . '</span>';
+	}
+}
+
+?>
+<span class="mod-menu__heading nav-header <?php echo $anchor_css; ?>"<?php echo $title; ?>><?php echo $linktype; ?></span>
