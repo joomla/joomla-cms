dda661117c406d44c58a256b8cb0c1d49110d007
Troy T. Hall <programming@hallhome.us>
2020-09-02 00:14:11 -0500

    make icons even width

--- layouts/joomla/content/info_block/author.php
+++ layouts/joomla/content/info_block/author.php
@@ -14,7 +14,7 @@ use Joomla\CMS\Language\Text;
 
 ?>
 <dd class="createdby" itemprop="author" itemscope itemtype="https://schema.org/Person">
-	<span class="fas fa-user" aria-hidden="true"></span>
+	<span class="fas fa-user fa-fw" aria-hidden="true"></span>
 	<?php $author = ($displayData['item']->created_by_alias ?: $displayData['item']->author); ?>
 	<?php $author = '<span itemprop="name">' . $author . '</span>'; ?>
 	<?php if (!empty($displayData['item']->contact_link ) && $displayData['params']->get('link_author') == true) : ?>
