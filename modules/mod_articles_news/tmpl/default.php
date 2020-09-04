bcc033d978f0e29ded798fa19f7a08d2f04c050d
Viviana Menzel <vivianamenzel@dr-menzel-it.de>
2020-09-04 10:42:15 +0200

    Move web asset call to horizontal.php

--- modules/mod_articles_news/tmpl/default.php
+++ modules/mod_articles_news/tmpl/default.php
@@ -11,10 +11,6 @@ defined('_JEXEC') or die;
 
 use Joomla\CMS\Helper\ModuleHelper;
 
-/** @var Joomla\CMS\WebAsset\WebAssetManager $wa */
-$wa = $app->getDocument()->getWebAssetManager();
-$wa->registerAndUseStyle('mod_modules', 'mod_articles_news/template.css');
-
 if (!$list)
 {
 	return;
