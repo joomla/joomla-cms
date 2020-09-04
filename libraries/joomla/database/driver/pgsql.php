76e8e5597afef8e2e8aece033784358069fed10f
Vincent <v.billard@ellipsa.com>
2020-09-04 14:02:54 +0200

    patch to enable install in a dedicated schema on PostgreSQL databases

--- libraries/joomla/database/driver/pgsql.php
+++ libraries/joomla/database/driver/pgsql.php
@@ -152,6 +152,22 @@ class JDatabaseDriverPgsql extends JDatabaseDriverPdo
 	}
 
 	/**
+	 * Internal function to get the name of the default schema for the current PostgreSQL connexion.
+	 * That is the schema where tables are created by Joomla.
+	 *
+	 * @return  string
+	 *
+	 * @since   3.9.21
+	 */
+	private function getDefaultSchema()
+	{
+
+		$this->setQuery('SELECT (current_schemas(false))[1]'); // supported since PostgreSQL 7.3
+		return $this->loadResult();
+
+	}
+	
+	/**
 	 * Shows the table CREATE statement that creates the given tables.
 	 *
 	 * This is unsupported by PostgreSQL.
@@ -187,6 +203,8 @@ class JDatabaseDriverPgsql extends JDatabaseDriverPdo
 
 		$tableSub = $this->replacePrefix($table);
 
+		$defaultSchema = $this->getDefaultSchema();
+		
 		$this->setQuery('
 			SELECT a.attname AS "column_name",
 				pg_catalog.format_type(a.atttypid, a.atttypmod) as "type",
@@ -207,7 +225,7 @@ class JDatabaseDriverPgsql extends JDatabaseDriverPdo
 			WHERE a.attrelid =
 				(SELECT oid FROM pg_catalog.pg_class WHERE relname=' . $this->quote($tableSub) . '
 					AND relnamespace = (SELECT oid FROM pg_catalog.pg_namespace WHERE
-					nspname = \'public\')
+					nspname = ' . $this->quote($defaultSchema) . ')
 				)
 			AND a.attnum > 0 AND NOT a.attisdropped
 			ORDER BY a.attnum'
