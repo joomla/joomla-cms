0554ff9b89440a1c827cc3f6b4fdedb9ae83a1ff
astridx <info@astrid-guenther.de>
2020-09-05 13:04:01 +0200

    delete client id in administrator/components/com_modules/Controller/DisplayController.php

--- administrator/components/com_modules/src/Controller/DisplayController.php
+++ administrator/components/com_modules/src/Controller/DisplayController.php
@@ -49,27 +49,12 @@ class DisplayController extends BaseController
 		$layout = $this->input->get('layout', 'edit');
 		$id     = $this->input->getInt('id');
 
-		// Verify client
-		$clientId = $this->input->post->getInt('client_id');
-
-		if (!is_null($clientId))
-		{
-			$uri = Uri::getInstance();
-
-			if ((int) $uri->getVar('client_id') !== (int) $clientId)
-			{
-				$this->setRedirect(Route::_('index.php?option=com_modules&view=modules&client_id=' . $clientId, false));
-
-				return false;
-			}
-		}
-
 		// Check for edit form.
 		if ($layout == 'edit' && !$this->checkEditId('com_modules.edit.module', $id))
 		{
 			// Somehow the person just went to the form - we don't allow that.
 			$this->setMessage(Text::sprintf('JLIB_APPLICATION_ERROR_UNHELD_ID', $id), 'error');
-			$this->setRedirect(Route::_('index.php?option=com_modules&view=modules&client_id=' . $this->input->getInt('client_id'), false));
+			$this->setRedirect(Route::_('index.php?option=com_modules&view=modules', false));
 
 			return false;
 		}
