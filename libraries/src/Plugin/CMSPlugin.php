2b6a874faffdc753c17469565b80fbd726745694
SharkyKZ <sharkykz@gmail.com>
2020-09-05 15:13:05 +0300

    Unnecessary parentheses

--- libraries/src/Plugin/CMSPlugin.php
+++ libraries/src/Plugin/CMSPlugin.php
@@ -234,7 +234,7 @@ abstract class CMSPlugin implements DispatcherAwareInterface, PluginInterface
 			$paramName = $param->getName();
 
 			// No type hint / type hint class not an event and parameter name is not "event"? It's a legacy listener.
-			if ((empty($typeHint) || !$typeHint->implementsInterface('Joomla\\Event\\EventInterface')) || $paramName !== 'event')
+			if (empty($typeHint) || !$typeHint->implementsInterface('Joomla\\Event\\EventInterface') || $paramName !== 'event')
 			{
 				$this->registerLegacyListener($method->name);
 
