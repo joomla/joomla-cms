8241226de1f503a89708a33f5d68401e6c1dcfd0
chmst <dev@chmst.de>
2020-09-11 20:45:08 +0200

    Change sample data for banner

--- plugins/sampledata/blog/blog.php
+++ plugins/sampledata/blog/blog.php
@@ -1112,7 +1112,7 @@ class PlgSampledataBlog extends CMSPlugin
 				'showtitle'  => 0,
 				'params'     => array(
 					'prepare_content' => 0,
-					'backgroundimage' => 'images/banners/banner.jpg',
+					'backgroundimage' => '',
 					'layout'          => 'cassiopeia:banner',
 					'moduleclass_sfx' => '',
 					'cache'           => 1,
