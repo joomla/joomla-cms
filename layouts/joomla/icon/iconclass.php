43f4ff41c70219e763e5db1612e2d0843cd83f2e
Troy T. Hall <programming@hallhome.us>
2020-09-14 21:54:51 -0500

    Begin allowing for other icon families

--- layouts/joomla/icon/iconclass.php
+++ layouts/joomla/icon/iconclass.php
@@ -13,13 +13,16 @@ defined('_JEXEC') or die;
 $icon = $displayData['icon'];
 $html = $displayData['html'] ?? true;
 
+// Set default prefix to be fontawesome
+$iconPrefix = $displayData['prefix'] ?? 'fas fa-';
+
 switch ($icon)
 {
-	case (strpos($icon, 'fa-') !== false):
-		$icon = 'fas ' . str_ireplace('fas ', '', $icon);
-		break;
-
 	case (strpos($icon, 'icon-') !== false):
+	case (strpos($icon, 'fa ') !== false):
+	case (strpos($icon, 'fas ') !== false):
+	case (strpos($icon, 'fab ') !== false):
+		$iconPrefix = $displayData['prefix'] ?? null;
 		break;
 
 	case 'archive':
@@ -29,11 +32,11 @@ switch ($icon)
 	case 'folder-plus-2':
 	case 'folder-remove':
 	case 'drawer-2':
-		$icon = 'fas fa-folder';
+		$icon = 'folder';
 		break;
 
 	case 'publish':
-		$icon = 'fas fa-check';
+		$icon = 'check';
 		break;
 
 	case 'unpublish':
@@ -41,98 +44,98 @@ switch ($icon)
 	case 'delete':
 	case 'remove':
 	case 'times':
-		$icon = 'fas fa-times';
+		$icon = 'times';
 		break;
 
 	case 'new':
 	case 'save-new':
-		$icon = 'fas fa-plus';
+		$icon = 'plus';
 		break;
 
 	case 'apply':
 	case 'save':
-		$icon = 'fas fa-save';
+		$icon = 'save';
 		break;
 
 	case 'mail':
-		$icon = 'fas fa-envelope';
+		$icon = 'envelope';
 		break;
 
 	case 'unfeatured':
 	case 'asterisk':
-		$icon = 'fas fa-star';
+		$icon = 'star';
 		break;
 
 	case 'featured':
-		$icon = 'fas fa-star featured';
+		$icon = 'star featured';
 		break;
 
 	case 'checkedout':
 	case 'protected':
-		$icon = 'fas fa-lock';
+		$icon = 'lock';
 		break;
 
 	case 'eye-close':
-		$icon = 'fas fa-eye-slash';
+		$icon = 'eye-slash';
 		break;
 
 	case 'eye-open':
-		$icon = 'fas fa-eye';
+		$icon = 'eye';
 		break;
 
 	case 'loop':
 	case 'refresh':
 	case 'unblock':
-		$icon = 'fas fa-sync';
+		$icon = 'sync';
 		break;
 
 	case 'contract':
-		$icon = 'fas fa-compress';
+		$icon = 'compress';
 		break;
 
 	case 'purge':
 	case 'trash':
-		$icon = 'fas fa-trash';
+		$icon = 'trash';
 		break;
 
 	case 'options':
-		$icon = 'fas fa-cog';
+		$icon = 'cog';
 		break;
 
 	case 'expired':
-		$icon = 'fas fa-minus-circle';
+		$icon = 'minus-circle';
 		break;
 
 	case 'save-copy':
-		$icon = 'fas fa-copy';
+		$icon = 'copy';
 		break;
 
 	case 'checkin':
-		$icon = 'fas fa-check-square';
+		$icon = 'check-square';
 		break;
 
 	case 'generic':
-		$icon = 'fas fa-dot-circle';
+		$icon = 'dot-circle';
 		break;
 
 	case 'list-2':
-		$icon = 'fas fa-list-ul';
+		$icon = 'list-ul';
 		break;
 
 	case 'default':
-		$icon = 'fas fa-home';
+		$icon = 'home';
 		break;
 
 	case 'crop':
-		$icon = 'fas fa-crop';
+		$icon = 'crop';
 		break;
 
 	case 'chevron-down':
-		$icon = 'fas fa-chevron-down';
+		$icon = 'chevron-down';
 		break;
 
 	case 'move':
-		$icon = 'fas fa-arrows-alt';
+		$icon = 'arrows-alt';
 		break;
 
 	default:
@@ -142,7 +145,7 @@ switch ($icon)
 
 if ($html !== false)
 {
-	$icon = '<span class="' . $icon . '" aria-hidden="true"></span>';
+	$icon = '<span class="' . $iconPrefix . $icon . '" aria-hidden="true"></span>';
 }
 
-echo $icon;
+echo $iconPrefix . $icon;
