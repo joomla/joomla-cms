15c23c8f99421c54389f2edd7ef8229b6eeb3702
wilsonge <georgejameswilson@googlemail.com>
2020-09-15 19:16:25 +0100

    Bump back to devel

--- libraries/src/Version.php
+++ libraries/src/Version.php
@@ -61,7 +61,7 @@ final class Version
 	 * @var    string
 	 * @since  3.8.0
 	 */
-	const EXTRA_VERSION = 'beta4';
+	const EXTRA_VERSION = 'beta5-dev';
 
 	/**
 	 * Development status.
@@ -69,7 +69,7 @@ final class Version
 	 * @var    string
 	 * @since  3.5
 	 */
-	const DEV_STATUS = 'Beta';
+	const DEV_STATUS = 'Development';
 
 	/**
 	 * Code name.
@@ -93,7 +93,7 @@ final class Version
 	 * @var    string
 	 * @since  3.5
 	 */
-	const RELTIME = '13:46';
+	const RELTIME = '19:15';
 
 	/**
 	 * Release timezone.
