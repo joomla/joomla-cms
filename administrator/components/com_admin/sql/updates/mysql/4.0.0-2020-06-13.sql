49718cfa318f8dca7bbc332b06d27f2d589f084c
Nicola Galgano <optimus4joomla@gmail.com>
2020-09-15 13:48:41 +0200

    custom_data

--- administrator/components/com_admin/sql/updates/mysql/4.0.0-2020-06-13.sql
+++ administrator/components/com_admin/sql/updates/mysql/4.0.0-2020-06-13.sql
@@ -5,5 +5,5 @@ CREATE TABLE IF NOT EXISTS `#__jobs` (
   PRIMARY KEY (`element`, `folder`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 DEFAULT COLLATE=utf8mb4_unicode_ci;
 
-INSERT INTO `#__extensions` (`package_id`, `name`, `type`, `element`, `folder`, `client_id`, `enabled`, `access`, `protected`, `locked`, `manifest_cache`, `params`, `ordering`, `state`) VALUES
-(0, 'plg_system_scheduler', 'plugin', 'scheduler', 'system', 0, 1, 1, 0, 1, '', '{"timeout":"3","webcron":"0","webcronkey":"mywebcronactivationkey","lastrun":"0","unit":"60","taskid":"0"}', 0, 0);
+INSERT INTO `#__extensions` (`package_id`, `name`, `type`, `element`, `folder`, `client_id`, `enabled`, `access`, `protected`, `locked`, `manifest_cache`, `params`, `ordering`, `state`, `custom_data`) VALUES
+(0, 'plg_system_scheduler', 'plugin', 'scheduler', 'system', 0, 1, 1, 0, 1, '', '{"timeout":"3","webcron":"0","webcronkey":"mywebcronactivationkey","lastrun":"0","unit":"60","taskid":"0"}', 0, 0, '');
