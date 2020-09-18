ca7ddfe0fe89dd9e66b6e21527187c045cd2cd33
Scrabble96 <github@businesspluspoint.co.uk>
2020-09-18 17:47:15 +0100

    Create noTitle.php
    This is for modules that generally do not have their header/title displayed, e.g. main menu or search. If used in the module position in the template index.php as "style=noTitle" instead of "style=none" then this will allow a mod id and module class suffix to be added to the modules.

---
+++ templates/cassiopeia/html/layouts/chromes/noTitle.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  Templates.crocosmia
+ *
+ * @copyright   Copyright (C) 2005 - 2019 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+$module  = $displayData['module'];
+$params  = $displayData['params'];
+$attribs = $displayData['attribs'];
+
+$modulePos   = $module->position;
+$moduleTag   = $params->get('module_tag', 'div');
+$modId 		 = 'mod-' . $module->id;
+
+if ($module->content) : ?>
+	<div id="<?php echo $modId; ?>" class="<?php echo $modulePos; ?> <?php echo htmlspecialchars($params->get('moduleclass_sfx')); ?>"  aria-labelledby="<?php echo $module->title; ?>">
+		<div>
+		<?php echo $module->content; ?>
+		</div>
+	</<?php echo $moduleTag; ?>>
+<?php endif; ?>
