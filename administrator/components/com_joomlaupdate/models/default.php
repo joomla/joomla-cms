f846587e70267fd368cd884aa070414302a7a90e
Nicola Galgano <optimus4joomla@gmail.com>
2020-09-21 12:45:53 +0200

    getUserState

--- administrator/components/com_joomlaupdate/models/default.php
+++ administrator/components/com_joomlaupdate/models/default.php
@@ -923,7 +923,7 @@ ENDDATA;
 		// Unset the update filename from the session.
 		JFactory::getApplication()->setUserState('com_joomlaupdate.file', null);
 
-		$oldVersion = JFactory::getApplication()->getUserState('com_joomlaupdate.oldversion', JVERSION);
+		$oldVersion = JFactory::getApplication()->getUserState('com_joomlaupdate.oldversion');
 		// Trigger event after joomla update.
 		JFactory::getApplication()->triggerEvent('onJoomlaAfterUpdate', array($oldVersion));
 		JFactory::getApplication()->setUserState('com_joomlaupdate.oldversion', null);
