10e2c3fcedffa5608810fe3de6e7e97e07eb0d7a
Constantin Romankiewicz <harmageddon95@web.de>
2020-09-23 20:07:55 +0200

    Changed line orders based on suggestion.
    Co-authored-by: Quy <quy@fluxbb.org>

--- layouts/joomla/form/field/color/advanced.php
+++ layouts/joomla/form/field/color/advanced.php
@@ -7,9 +7,9 @@
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
-use Joomla\CMS\Language\Language;
-
 defined('_JEXEC') or die;
+
+use Joomla\CMS\Language\Language;
 
 extract($displayData);
 
