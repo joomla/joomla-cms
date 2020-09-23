8bc50053a37ce44577237d959b6db47a3c1cee76
zero-24 <zero-24@users.noreply.github.com>
2020-09-23 23:14:09 +0200

    When we have no image nothing is going to be displayed

--- layouts/joomla/content/full_image.php
+++ layouts/joomla/content/full_image.php
@@ -11,6 +11,12 @@ defined('_JEXEC') or die;
 
 use Joomla\Utilities\ArrayHelper;
 
+// When we have no image nothing is going to be displayed.
+if (empty($images->image_fulltext))
+{
+	return;
+}
+
 $params   = $displayData->params;
 $images   = json_decode($displayData->images);
 $imgfloat = empty($images->float_fulltext) ? $params->get('float_fulltext') : $images->float_fulltext;
@@ -33,19 +39,17 @@ if (count($imageParams))
 	}
 }
 ?>
-<?php if (!empty($images->image_fulltext)) : ?>
-	<figure class="float-<?php echo htmlspecialchars($imgfloat, ENT_COMPAT, 'UTF-8'); ?> item-image">
-		<img
-			loading="lazy"
-			src="<?php echo htmlspecialchars($src, ENT_COMPAT, 'UTF-8'); ?>"
-			alt="<?php echo htmlspecialchars($images->image_fulltext_alt, ENT_COMPAT, 'UTF-8'); ?>"
-			itemprop="image"
-			<?php echo ArrayHelper::toString($attr); ?>
-		/>
-		<?php if (isset($images->image_fulltext_caption) && !empty($images->image_fulltext_caption)) : ?>
-			<figcaption class="caption">
-				<?php echo htmlspecialchars($images->image_fulltext_caption, ENT_COMPAT, 'UTF-8'); ?>
-			</figcaption>
-		<?php endif; ?>
-	</figure>
-<?php endif; ?>
+<figure class="float-<?php echo htmlspecialchars($imgfloat, ENT_COMPAT, 'UTF-8'); ?> item-image">
+	<img
+		loading="lazy"
+		src="<?php echo htmlspecialchars($src, ENT_COMPAT, 'UTF-8'); ?>"
+		alt="<?php echo htmlspecialchars($images->image_fulltext_alt, ENT_COMPAT, 'UTF-8'); ?>"
+		itemprop="image"
+		<?php echo ArrayHelper::toString($attr); ?>
+	/>
+	<?php if (isset($images->image_fulltext_caption) && !empty($images->image_fulltext_caption)) : ?>
+		<figcaption class="caption">
+			<?php echo htmlspecialchars($images->image_fulltext_caption, ENT_COMPAT, 'UTF-8'); ?>
+		</figcaption>
+	<?php endif; ?>
+</figure>
--- layouts/joomla/content/intro_image.php
+++ layouts/joomla/content/intro_image.php
@@ -13,6 +13,12 @@ use Joomla\CMS\Router\Route;
 use Joomla\Component\Content\Site\Helper\RouteHelper;
 use Joomla\Utilities\ArrayHelper;
 
+// When we have no image nothing is going to be displayed.
+if (empty($images->image_intro))
+{
+	return;
+}
+
 $params   = $displayData->params;
 $images   = json_decode($displayData->images);
 $imgfloat = empty($images->float_intro) ? $params->get('float_intro') : $images->float_intro;
@@ -35,19 +41,9 @@ if (count($imageParams))
 	}
 }
 ?>
-<?php if (!empty($images->image_intro)) : ?>
-	<figure class="float-<?php echo htmlspecialchars($imgfloat, ENT_COMPAT, 'UTF-8'); ?> item-image">
-		<?php if ($params->get('link_titles') && $params->get('access-view')) : ?>
-			<a href="<?php echo Route::_(RouteHelper::getArticleRoute($displayData->slug, $displayData->catid, $displayData->language)); ?>">
-				<img
-					loading="lazy"
-					src="<?php echo htmlspecialchars($src, ENT_COMPAT, 'UTF-8'); ?>"
-					alt="<?php echo htmlspecialchars($images->image_intro_alt, ENT_COMPAT, 'UTF-8'); ?>"
-					itemprop="thumbnailUrl"
-					<?php echo ArrayHelper::toString($attr); ?>
-				/>
-			</a>
-		<?php else : ?>
+<figure class="float-<?php echo htmlspecialchars($imgfloat, ENT_COMPAT, 'UTF-8'); ?> item-image">
+	<?php if ($params->get('link_titles') && $params->get('access-view')) : ?>
+		<a href="<?php echo Route::_(RouteHelper::getArticleRoute($displayData->slug, $displayData->catid, $displayData->language)); ?>">
 			<img
 				loading="lazy"
 				src="<?php echo htmlspecialchars($src, ENT_COMPAT, 'UTF-8'); ?>"
@@ -55,11 +51,19 @@ if (count($imageParams))
 				itemprop="thumbnailUrl"
 				<?php echo ArrayHelper::toString($attr); ?>
 			/>
-		<?php endif; ?>
-		<?php if (isset($images->image_intro_caption) && !empty($images->image_intro_caption)) : ?>
-			<figcaption class="caption">
-				<?php echo htmlspecialchars($images->image_intro_caption, ENT_COMPAT, 'UTF-8'); ?>
-			</figcaption>
-		<?php endif; ?>
-	</figure>
-<?php endif; ?>
+		</a>
+	<?php else : ?>
+		<img
+			loading="lazy"
+			src="<?php echo htmlspecialchars($src, ENT_COMPAT, 'UTF-8'); ?>"
+			alt="<?php echo htmlspecialchars($images->image_intro_alt, ENT_COMPAT, 'UTF-8'); ?>"
+			itemprop="thumbnailUrl"
+			<?php echo ArrayHelper::toString($attr); ?>
+		/>
+	<?php endif; ?>
+	<?php if (isset($images->image_intro_caption) && !empty($images->image_intro_caption)) : ?>
+		<figcaption class="caption">
+			<?php echo htmlspecialchars($images->image_intro_caption, ENT_COMPAT, 'UTF-8'); ?>
+		</figcaption>
+	<?php endif; ?>
+</figure>
