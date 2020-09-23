eeb21873db80c2c0d24ac5c4a322a2c2b7f911d3
SharkyKZ <sharkykz@gmail.com>
2020-09-23 11:26:44 +0300

    Add comment

--- libraries/src/Filesystem/File.php
+++ libraries/src/Filesystem/File.php
@@ -37,6 +37,7 @@ class File
 	 */
 	public static function getExt($file)
 	{
+		// String manipulation should be faster than pathinfo() on newer PHP versions.
 		$dot = strrpos($file, '.');
 
 		if ($dot === false)
