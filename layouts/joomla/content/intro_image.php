dd54a9efffcd7616be5b5db8f230a666b4306f9e
dGrammatiko <d.grammatiko@gmail.com>
2020-09-26 18:20:12 +0200

    xxx

--- layouts/joomla/content/intro_image.php
+++ layouts/joomla/content/intro_image.php
@@ -9,32 +9,43 @@
 
 defined('_JEXEC') or die;
 
+use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Router\Route;
 use Joomla\Component\Content\Site\Helper\RouteHelper;
+use Joomla\Utilities\ArrayHelper;
 
 $params  = $displayData->params;
 $images  = json_decode($displayData->images);
+
+if (empty($images->image_intro))
+{
+	return;
+}
+
+$imgfloat = empty($images->float_intro) ? $params->get('float_intro') : $images->float_intro;
+
+$img = HTMLHelper::cleanImageURL($images->image_fulltext);
+
+// Set or not the lazyloading
+$img->attributes['loading'] = 'lazy';
 ?>
-<?php if (!empty($images->image_intro)) : ?>
-	<?php $imgfloat = empty($images->float_intro) ? $params->get('float_intro') : $images->float_intro; ?>
-	<figure class="float-<?php echo htmlspecialchars($imgfloat, ENT_COMPAT, 'UTF-8'); ?> item-image">
-		<?php if ($params->get('link_intro_image') && ($params->get('access-view') || $params->get('show_noauth', '0') == '1')) : ?>
-			<a href="<?php echo Route::_(
-				RouteHelper::getArticleRoute($displayData->slug, $displayData->catid, $displayData->language)
-				); ?>" itemprop="url" title="<?php echo $this->escape($displayData->title); ?>">
-				<img loading="lazy" src="<?php echo htmlspecialchars($images->image_intro, ENT_COMPAT, 'UTF-8'); ?>"
+<figure class="float-<?php echo htmlspecialchars($imgfloat, ENT_COMPAT, 'UTF-8'); ?> item-image">
+	<?php if ($params->get('link_titles') && $params->get('access-view')) : ?>
+		<a href="<?php echo Route::_(RouteHelper::getArticleRoute($displayData->slug, $displayData->catid, $displayData->language)); ?>">
+			<img src="<?php echo htmlspecialchars($images->image_intro, ENT_COMPAT, 'UTF-8'); ?>"
 					 alt="<?php echo htmlspecialchars($images->image_intro_alt, ENT_COMPAT, 'UTF-8'); ?>"
 					 itemprop="thumbnailUrl"
-				/>
-			</a>
-		<?php else : ?>
-			<img loading="lazy" src="<?php echo htmlspecialchars($images->image_intro, ENT_COMPAT, 'UTF-8'); ?>"
+					 <?php echo ArrayHelper::toString($img->attributes); ?>
+			/>
+		</a>
+	<?php else : ?>
+		<img src="<?php echo htmlspecialchars($images->image_intro, ENT_COMPAT, 'UTF-8'); ?>"
 				 alt="<?php echo htmlspecialchars($images->image_intro_alt, ENT_COMPAT, 'UTF-8'); ?>"
 				 itemprop="thumbnailUrl"
-			>
-		<?php endif; ?>
-		<?php if (isset($images->image_intro_caption) && $images->image_intro_caption !== '') : ?>
-			<figcaption class="caption"><?php echo htmlspecialchars($images->image_intro_caption, ENT_COMPAT, 'UTF-8'); ?></figcaption>
-		<?php endif; ?>
-	</figure>
-<?php endif; ?>
+				 <?php echo ArrayHelper::toString($img->attributes); ?>
+		/>
+	<?php endif; ?>
+	<?php if (isset($images->image_intro_caption) && $images->image_intro_caption !== '') : ?>
+		<figcaption class="caption"><?php echo htmlspecialchars($images->image_intro_caption, ENT_COMPAT, 'UTF-8'); ?></figcaption>
+	<?php endif; ?>
+</figure>
