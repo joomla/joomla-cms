bd82992bf1ad97bed5032b938fa1a41834af36bb
Tobias Zulauf <zero-24@users.noreply.github.com>
2020-09-29 17:55:45 +0200

    Update libraries/src/Application/CMSApplication.php

--- libraries/src/Application/CMSApplication.php
+++ libraries/src/Application/CMSApplication.php
@@ -1320,7 +1320,7 @@ abstract class CMSApplication extends WebApplication implements ContainerAwareIn
 	 *
 	 * @return void
 	 *
-	 * @since 4.0.0
+	 * @since   __DEPLOY_VERSION__
 	 */
 	private function setupLogging(): void
 	{
