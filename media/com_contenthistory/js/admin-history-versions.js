a8babe837ebe4f6a5a5f583838eada95dd8c3263
Constantin Romankiewicz <constantin@zweiiconkram.de>
2020-09-29 18:45:53 +0200

    Fixed version history modals.

---
+++ media/com_contenthistory/js/admin-history-versions.js
@@ -0,0 +1,7 @@
+/**
+ * @copyright  Copyright (C) 2005 - 2020 Open Source Matters, Inc. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
+window.addEventListener('DOMContentLoaded', function() {
+	document.body.appendChild(document.getElementById('versionsModal'));
+});
