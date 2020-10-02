ac5d9ff926b5982986efae9f36a610382fa0c13e
Quy <quy@fluxbb.org>
2020-10-02 15:55:06 -0700

    Align help icon

--- modules/mod_login/tmpl/default.php
+++ modules/mod_login/tmpl/default.php
@@ -86,10 +86,14 @@ Text::script('JHIDEPASSWORD');
 					</div>
 				<?php else : ?>
 					<label for="modlgn-secretkey-<?php echo $module->id; ?>"><?php echo Text::_('JGLOBAL_SECRETKEY'); ?></label>
-					<input id="modlgn-secretkey-<?php echo $module->id; ?>" autocomplete="one-time-code" type="text" name="secretkey" class="form-control" placeholder="<?php echo Text::_('JGLOBAL_SECRETKEY'); ?>">
-					<span class="btn width-auto" title="<?php echo Text::_('JGLOBAL_SECRETKEY_HELP'); ?>">
-						<span class="fas fa-question" aria-hidden="true"></span>
-					</span>
+					<div class="input-group">
+						<input id="modlgn-secretkey-<?php echo $module->id; ?>" autocomplete="one-time-code" type="text" name="secretkey" class="form-control" placeholder="<?php echo Text::_('JGLOBAL_SECRETKEY'); ?>">
+						<span class="input-group-append" title="<?php echo Text::_('JGLOBAL_SECRETKEY_HELP'); ?>">
+							<span class="input-group-text">
+								<span class="fas fa-question" aria-hidden="true"></span>
+							</span>
+						</span>
+					</div>
 				<?php endif; ?>
 			</div>
 		<?php endif; ?>
