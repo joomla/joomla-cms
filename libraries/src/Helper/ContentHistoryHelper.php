ee6867c02e2395f279eb941fca27caecc34f4bd9
SharkyKZ <sharkykz@gmail.com>
2020-10-06 09:22:58 +0300

    Use Versioning

--- libraries/src/Helper/ContentHistoryHelper.php
+++ libraries/src/Helper/ContentHistoryHelper.php
@@ -8,13 +8,12 @@
 
 namespace Joomla\CMS\Helper;
 
-\defined('JPATH_PLATFORM') or die;
+\defined('_JEXEC') or die;
 
-use Joomla\CMS\Component\ComponentHelper;
 use Joomla\CMS\Factory;
 use Joomla\CMS\Filter\InputFilter;
 use Joomla\CMS\Table\Table;
-use Joomla\Database\ParameterType;
+use Joomla\CMS\Versioning\Versioning;
 
 /**
  * Versions helper class, provides methods to perform various tasks relevant
@@ -55,22 +54,9 @@ class ContentHistoryHelper extends CMSHelper
 	 */
 	public function deleteHistory($table)
 	{
-		$id     = (int) $table->{$table->getKeyName()};
-		$typeId = Table::getInstance('Contenttype', 'JTable')->getTypeId($this->typeAlias);
-		$db     = Factory::getDbo();
-		$query  = $db->getQuery(true)
-			->delete($db->quoteName('#__ucm_history'))
-			->where(
-				[
-					$db->quoteName('ucm_item_id') . ' = :id',
-					$db->quoteName('ucm_type_id') . ' = :typeId',
-				]
-			)
-			->bind(':id', $id, ParameterType::INTEGER)
-			->bind(':typeId', $typeId, ParameterType::INTEGER);
-		$db->setQuery($query);
+		$id = (int) $table->{$table->getKeyName()};
 
-		return $db->execute();
+		return Versioning::delete($table->typeAlias, $id);
 	}
 
 	/**
@@ -89,23 +75,10 @@ class ContentHistoryHelper extends CMSHelper
 		$typeId = (int) $typeId;
 		$id     = (int) $id;
 
-		$db = Factory::getDbo();
-		$query = $db->getQuery(true)
-			->select($db->quoteName(['h.version_note', 'h.save_date', 'u.name']))
-			->from($db->quoteName('#__ucm_history', 'h'))
-			->join('LEFT', $db->quoteName('#__users', 'u'), $db->quoteName('u.id') . ' = ' . $db->quoteName('h.editor_user_id'))
-			->where(
-				[
-					$db->quoteName('h.ucm_item_id') . ' = :id',
-					$db->quoteName('h.ucm_type_id') . ' = :typeId',
-				]
-			)
-			->bind(':id', $id, ParameterType::INTEGER)
-			->bind(':typeId', $typeId, ParameterType::INTEGER)
-			->order($db->quoteName('h.save_date') . ' DESC');
-		$db->setQuery($query);
+		$contentType = Table::getInstance('ContentType');
+		$contentType->load(['type_id' => $typeId]);
 
-		return $db->loadObjectList();
+		return Versioning::get($contentType->type_alias, $id);
 	}
 
 	/**
@@ -120,65 +93,17 @@ class ContentHistoryHelper extends CMSHelper
 	public function store($table)
 	{
 		$dataObject = $this->getDataObject($table);
-		$historyTable = Table::getInstance('Contenthistory', 'JTable');
-		$typeTable = Table::getInstance('Contenttype', 'JTable');
-		$typeTable->load(array('type_alias' => $this->typeAlias));
-		$historyTable->set('ucm_type_id', $typeTable->type_id);
+		$key        = $table->getKeyName();
 
-		$key = $table->getKeyName();
-		$historyTable->set('ucm_item_id', $table->$key);
-
-		// Don't store unless we have a non-zero item id
-		if (!$historyTable->ucm_item_id)
-		{
-			return true;
-		}
-
-		$historyTable->set('version_data', json_encode($dataObject));
 		$input = Factory::getApplication()->input;
-		$data = $input->get('jform', array(), 'array');
-		$versionName = false;
+		$data  = $input->get('jform', [], 'array');
+		$versionNote  = '';
 
 		if (isset($data['version_note']))
 		{
-			$versionName = InputFilter::getInstance()->clean($data['version_note'], 'string');
-			$historyTable->set('version_note', $versionName);
+			$versionNote = InputFilter::getInstance()->clean($data['version_note'], 'STRING');
 		}
 
-		// Don't save if hash already exists and same version note
-		$historyTable->set('sha1_hash', $historyTable->getSha1($dataObject, $typeTable));
-
-		if ($historyRow = $historyTable->getHashMatch())
-		{
-			if (!$versionName || ($historyRow->version_note === $versionName))
-			{
-				return true;
-			}
-			else
-			{
-				// Update existing row to set version note
-				$historyTable->set('version_id', $historyRow->version_id);
-			}
-		}
-
-		$result = $historyTable->store();
-
-		// Load history_limit config from extension.
-		$aliasParts = explode('.', $this->typeAlias);
-
-		$context = $aliasParts[1] ?? '';
-
-		$maxVersionsContext = ComponentHelper::getParams($aliasParts[0])->get('history_limit_' . $context, 0);
-
-		if ($maxVersionsContext)
-		{
-			$historyTable->deleteOldVersions($maxVersionsContext);
-		}
-		elseif ($maxVersions = ComponentHelper::getParams($aliasParts[0])->get('history_limit', 0))
-		{
-			$historyTable->deleteOldVersions($maxVersions);
-		}
-
-		return $result;
+		return Versioning::store($table->typeAlias, $table->$key, $dataObject, $versionNote);
 	}
 }
