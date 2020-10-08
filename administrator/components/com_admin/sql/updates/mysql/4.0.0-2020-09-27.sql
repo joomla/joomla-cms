d78079389d61c74a81d12b40ca101af6dcc5b802
Richard Fath <richard67@users.noreply.github.com>
2020-10-08 11:19:30 +0200

    Add update SQL to delete plg_authentication_gmail from extensions table (#30941)

--- administrator/components/com_admin/sql/updates/mysql/4.0.0-2020-09-27.sql
+++ administrator/components/com_admin/sql/updates/mysql/4.0.0-2020-09-27.sql
@@ -1,1 +1,2 @@
 DELETE FROM `#__extensions` WHERE `name` = 'plg_content_imagelazyload' AND `type` = 'plugin' AND `element` = 'imagelazyload' AND `folder` = 'content' AND `client_id` = 0;
+DELETE FROM `#__extensions` WHERE `name` = 'plg_authentication_gmail' AND `type` = 'plugin' AND `element` = 'gmail' AND `folder` = 'authentication' AND `client_id` = 0;
