1491b98ec1d3bfd9f5f490e966f6a4d998bc8b22
sakiss <sakis@breakdesigns.net>
2020-10-15 17:45:13 +0300

    Add a default empty $pageclass

--- templates/cassiopeia/index.php
+++ templates/cassiopeia/index.php
@@ -20,13 +20,14 @@ $lang = $app->getLanguage();
 $wa   = $this->getWebAssetManager();
 
 // Detecting Active Variables
-$option   = $app->input->getCmd('option', '');
-$view     = $app->input->getCmd('view', '');
-$layout   = $app->input->getCmd('layout', '');
-$task     = $app->input->getCmd('task', '');
-$itemid   = $app->input->getCmd('Itemid', '');
-$sitename = htmlspecialchars($app->get('sitename'), ENT_QUOTES, 'UTF-8');
-$menu     = $app->getMenu()->getActive();
+$option    = $app->input->getCmd('option', '');
+$view      = $app->input->getCmd('view', '');
+$layout    = $app->input->getCmd('layout', '');
+$task      = $app->input->getCmd('task', '');
+$itemid    = $app->input->getCmd('Itemid', '');
+$sitename  = htmlspecialchars($app->get('sitename'), ENT_QUOTES, 'UTF-8');
+$pageclass = '';
+$menu      = $app->getMenu()->getActive();
 if ($menu)
 {
 	$pageclass = $menu->getParams()->get('pageclass_sfx');
