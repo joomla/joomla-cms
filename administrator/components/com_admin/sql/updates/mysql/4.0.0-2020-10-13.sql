49c316290b616c296c606042a4575448784468dc
brian teeman <brian@teeman.net>
2020-10-15 13:21:48 +0100

    oops forgot to commit the updates

---
+++ administrator/components/com_admin/sql/updates/mysql/4.0.0-2020-10-13.sql
@@ -0,0 +1,4 @@
+INSERT INTO `#__extensions` (`package_id`, `name`, `type`, `element`, `folder`, `client_id`, `enabled`, `access`, `protected`, `locked`, `manifest_cache`, `params`, `custom_data`, `checked_out`, `checked_out_time`, `ordering`, `state`) VALUES
+(0, 'plg_editors-xtd_achecker', 'plugin', 'achecker', 'editors-xtd', 0, 1, 1, 0, 1, '', '', '', 0, NULL, 0, 0),
+(0, 'plg_system_achecker', 'plugin', 'achecker', 'system', 1, 1, 1, 0, 1, '', '', '', 0, NULL, 0, 0);
+
