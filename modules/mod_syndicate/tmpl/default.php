6a6f6e7a03057193b921aaabefdaa7bf24a8d85b
chmst <dev@chmst.de>
2020-10-21 09:24:16 +0200

    Set alt-text for the feed-image if no other text is displayed

--- modules/mod_syndicate/tmpl/default.php
+++ modules/mod_syndicate/tmpl/default.php
@@ -3,15 +3,17 @@
  * @package     Joomla.Site
  * @subpackage  mod_syndicate
  *
- * @copyright   Copyright (C) 2005 - 2020 Open Source Matters, Inc. All rights reserved.
+ * @copyright   (C) 2006 Open Source Matters, Inc. <https://www.joomla.org>
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 defined('_JEXEC') or die;
 
+// Set alt-text for the image only if no text is displayed
+$alt = (!$params->get('display_text', 1) && empty($text)) ? JText::_('MOD_SYNDICATE_DEFAULT_FEED_ENTRIES') : '';
 ?>
 <a href="<?php echo $link; ?>" class="syndicate-module<?php echo $moduleclass_sfx; ?>">
-	<?php echo JHtml::_('image', 'system/livemarks.png', 'feed-image', null, true); ?>
+	<?php echo JHtml::_('image', 'system/livemarks.png', $alt, null, true); ?>
 	<?php if ($params->get('display_text', 1)) : ?>
 		<span>
 		<?php if (str_replace(' ', '', $text) !== '') : ?>
