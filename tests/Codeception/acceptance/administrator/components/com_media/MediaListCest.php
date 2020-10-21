490fa7815f8611c6ef04a6c1da0a6754a4a82492
Troy T. Hall <programming@hallhome.us>
2020-10-21 11:49:26 -0500

    testing drone fix

--- tests/Codeception/acceptance/administrator/components/com_media/MediaListCest.php
+++ tests/Codeception/acceptance/administrator/components/com_media/MediaListCest.php
@@ -260,6 +260,7 @@ class MediaListCest
 		$I->waitForMediaLoaded();
 		$I->seeInCurrentUrl(MediaListPage::$url);
 		$I->seeContents($this->contents['root']);
+		$I->resizeWindow(maximize);
 	}
 
 	/**
