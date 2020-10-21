da1677fb926efcdfed494eaad25b52ac74478d8a
Troy T. Hall <programming@hallhome.us>
2020-10-21 13:42:45 -0500

    maximize

--- tests/Codeception/acceptance/administrator/components/com_media/MediaListCest.php
+++ tests/Codeception/acceptance/administrator/components/com_media/MediaListCest.php
@@ -260,7 +260,7 @@ class MediaListCest
 		$I->waitForMediaLoaded();
 		$I->seeInCurrentUrl(MediaListPage::$url);
 		$I->seeContents($this->contents['root']);
-		$I->resizeWindow(1920, 1080);
+		$I->resizeWindow(maximize);
 	}
 
 	/**
