9170a08a8770243de69a463911a8ceec69dbb075
Hans Kuijpers <hans2103@gmail.com>
2020-10-21 08:53:02 +0200

    replace fas fa- => icon-

--- administrator/components/com_admin/tmpl/help/default.php
+++ administrator/components/com_admin/tmpl/help/default.php
@@ -21,7 +21,7 @@ use Joomla\CMS\Router\Route;
 	<div class="row mt-sm-3">
 		<div id="sidebar" class="col-md-3">
 			<button class="btn btn-sm btn-secondary my-2 options-menu d-md-none" type="button" data-toggle="collapse" data-target=".sidebar-nav" aria-controls="sidebar-nav" aria-expanded="false">
-				 <span class="fas fa-align-justify" aria-hidden="true"></span>
+				 <span class="icon-align-justify" aria-hidden="true"></span>
 				 <?php echo Text::_('JTOGGLE_SIDEBAR_MENU'); ?>
 			</button>
 			<div class="sidebar-nav" id="help-index">
