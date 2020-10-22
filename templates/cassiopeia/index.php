02e6bd89b3826981c037e3dd84a36e05578556dd
Richard Fath <richard67@users.noreply.github.com>
2020-10-22 20:02:13 +0200

    Fix merge

--- templates/cassiopeia/index.php
+++ templates/cassiopeia/index.php
@@ -26,7 +26,6 @@ $task     = $app->input->getCmd('task', '');
 $itemid   = $app->input->getCmd('Itemid', '');
 $sitename = htmlspecialchars($app->get('sitename'), ENT_QUOTES, 'UTF-8');
 $menu     = $app->getMenu()->getActive();
-
 $pageclass = $menu !== null ? $menu->getParams()->get('pageclass_sfx', '') : '';
 
 // Template path
