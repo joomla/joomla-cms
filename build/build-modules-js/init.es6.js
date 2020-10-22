5ceb8bc5b9958d1a63b75654f30c9c6d7d745d7a
SharkyKZ <sharkykz@gmail.com>
2020-10-22 18:59:59 +0300

    Move back to build directory

--- build/build-modules-js/init.es6.js
+++ build/build-modules-js/init.es6.js
@@ -327,10 +327,10 @@ const recreateMediaFolder = () => {
   console.log('Recreating the media folder...');
 
   Copydir.sync(Path.join(RootPath, 'build/media_source'), Path.join(RootPath, 'media'), (stat, filepath, filename) => {
-    if (stat === 'file' && (filepath.match(/\.es6\.js/) || filepath.match(/\.scss/) || filepath.match(/\.es5\.js/))) {
+    if (stat === 'file' && (filepath.match(/\.es6\.js/) || filepath.match(/\.es5\.js/))) {
       return false;
     }
-    if (stat === 'directory' && (filename.match(/webcomponent/) || filename.match(/scss/))) {
+    if (stat === 'directory' && filename.match(/webcomponent/)) {
       return false;
     }
     return true;
