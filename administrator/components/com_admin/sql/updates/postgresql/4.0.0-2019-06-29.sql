932b2a91364778b6c16f5390cf9dc7c1714615cf
Richard Fath <richard67@users.noreply.github.com>
2020-10-28 11:48:53 +0100

    Fix old null dates for created times on PostgreSQL databases

--- administrator/components/com_admin/sql/updates/postgresql/4.0.0-2019-06-29.sql
+++ administrator/components/com_admin/sql/updates/postgresql/4.0.0-2019-06-29.sql
@@ -11,11 +11,16 @@ ALTER TABLE "#__newsfeeds" ALTER COLUMN "publish_down" DROP DEFAULT;
 ALTER TABLE "#__newsfeeds" ALTER COLUMN "checked_out_time" DROP NOT NULL;
 ALTER TABLE "#__newsfeeds" ALTER COLUMN "checked_out_time" DROP DEFAULT;
 
+UPDATE "#__newsfeeds" SET "created" = '1980-01-01 00:00:00' WHERE "created" = '1970-01-01 00:00:00';
 UPDATE "#__newsfeeds" SET "modified" = "created", "modified_by" = "created_by" WHERE "modified" = '1970-01-01 00:00:00';
 
 UPDATE "#__newsfeeds" SET "publish_up" = NULL WHERE "publish_up" = '1970-01-01 00:00:00';
 UPDATE "#__newsfeeds" SET "publish_down" = NULL WHERE "publish_down" = '1970-01-01 00:00:00';
 UPDATE "#__newsfeeds" SET "checked_out_time" = NULL WHERE "checked_out_time" = '1970-01-01 00:00:00';
+
+UPDATE "#__ucm_content" SET "core_created_time" = '1980-01-01 00:00:00'
+ WHERE "core_type_alias" = 'com_newsfeeds.newsfeed'
+   AND "core_created_time" = '1970-01-01 00:00:00';
 
 UPDATE "#__ucm_content" SET "core_modified_time" = "core_created_time"
  WHERE "core_type_alias" = 'com_newsfeeds.newsfeed'
