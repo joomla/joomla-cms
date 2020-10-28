ba6719803632b2a7a5397bd2f38d7d9302327126
Troy T. Hall <programming@hallhome.us>
2020-10-28 13:13:03 -0500

    remove use file

--- administrator/modules/mod_login/tmpl/default.php
+++ administrator/modules/mod_login/tmpl/default.php
@@ -12,7 +12,6 @@ defined('_JEXEC') or die;
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\Router\Route;
-use Joomla\CMS\Filesystem\File;
 
 /** @var Joomla\CMS\WebAsset\WebAssetManager $wa */
 $wa = $app->getDocument()->getWebAssetManager();
