76607c9b7b73e6257b02cf82a7838829443999dc
Bear <programming@hallhome.us>
2020-10-29 01:45:32 -0500

    Update libraries/src/HTML/Helpers/Icons.php
    Co-authored-by: Quy <quy@fluxbb.org>

--- libraries/src/HTML/Helpers/Icons.php
+++ libraries/src/HTML/Helpers/Icons.php
@@ -104,7 +104,7 @@ abstract class Icons
 	{
 		if ($relative === true)
 		{
-			$file = HTMLHelper::_('image', $file,  '', '', true, true);
+			$file = HTMLHelper::_('image', $file, '', '', true, true);
 		}
 
 		$file = substr($file, \strlen(Uri::root(true)));
