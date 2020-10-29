1df0c1c5e3463ca6aab7e6cddebc4e4ba47af6a8
Richard Fath <richard67@users.noreply.github.com>
2020-10-29 13:49:05 +0100

    [Metismenu] Auto-collapse metismenu when click outside of menu (#210)
    * Auto-collapse metismenu when click outside of menu
    
    * Code style - apply fixes from eslint

--- templates/cassiopeia/js/mod_menu/menu-metismenu.js
+++ templates/cassiopeia/js/mod_menu/menu-metismenu.js
@@ -1,10 +1,17 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-	const allMenus = document.querySelectorAll('ul.mod-menu_metismenu');
+document.addEventListener('DOMContentLoaded', () => {
+  const allMenus = document.querySelectorAll('ul.mod-menu_metismenu');
 
-	allMenus.forEach(menu => {
-		// eslint-disable-next-line no-new, no-undef
-		new MetisMenu(menu, {
-			triggerElement: 'button.mm-toggler'
-		});
-	});
+  allMenus.forEach((menu) => {
+    // eslint-disable-next-line no-new, no-undef
+    const mm = new MetisMenu(menu, {
+      triggerElement: 'button.mm-toggler',
+    }).on('shown.metisMenu', (event) => {
+      window.addEventListener('click', function mmClick(e) {
+        if (!event.target.contains(e.target)) {
+          mm.hide(event.detail.shownElement);
+          window.removeEventListener('click', mmClick);
+        }
+      });
+    });
+  });
 });
