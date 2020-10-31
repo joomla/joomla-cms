82a77f6dca394e80717512fc3a40f21e9e62f89c
Bear <programming@hallhome.us>
2020-10-30 20:43:11 -0500

    Update administrator/components/com_cache/src/View/Cache/HtmlView.php
    Co-authored-by: Quy <quy@fluxbb.org>

--- administrator/components/com_cache/src/View/Cache/HtmlView.php
+++ administrator/components/com_cache/src/View/Cache/HtmlView.php
@@ -125,7 +125,7 @@ class HtmlView extends BaseHtmlView
 		// Get the toolbar object instance
 		$toolbar = Toolbar::getInstance('toolbar');
 
-		ToolbarHelper::custom('delete', 'delete.png', '', 'JTOOLBAR_DELETE', true);
+		ToolbarHelper::custom('delete', 'delete', '', 'JTOOLBAR_DELETE', true);
 		ToolbarHelper::custom('deleteAll', 'remove.png', '', 'JTOOLBAR_DELETE_ALL', false);
 		$toolbar->appendButton('Confirm', 'COM_CACHE_RESOURCE_INTENSIVE_WARNING', 'delete', 'COM_CACHE_PURGE_EXPIRED', 'purge', false);
 		ToolbarHelper::divider();
