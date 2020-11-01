79c46be92ec9522ca7aae3f7daf9ba6075041b49
AndySDH <andybadwool@hotmail.it>
2020-11-01 17:10:02 +0100

    I guess tabs in notepad++ are different than here

--- components/com_content/views/featured/view.feed.php
+++ components/com_content/views/featured/view.feed.php
@@ -55,10 +55,10 @@ class ContentViewFeatured extends JViewLegacy
 			
 			// Set feed image to image_intro or if that's empty, to image_fulltext
 			$introImage = 	!empty($obj->{'image_intro'}) ?
-							$obj->{'image_intro'} :
-							( !empty($obj->{'image_fulltext'}) ?
-							$obj->{'image_fulltext'} :
-							'');
+					$obj->{'image_intro'} :
+					( !empty($obj->{'image_fulltext'}) ?
+					$obj->{'image_fulltext'} :
+					'');
 
 			if ( !empty($introImage) )
 			{
