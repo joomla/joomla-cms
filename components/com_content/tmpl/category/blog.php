2478079e9b657388e54ef5b2a662177e700498a6
brian teeman <brian@teeman.net>
2020-11-06 12:34:21 +0000

    [4.0] Category ALT text

--- components/com_content/tmpl/category/blog.php
+++ components/com_content/tmpl/category/blog.php
@@ -54,7 +54,8 @@ $afterDisplayContent = trim(implode("\n", $results));
 	<?php if ($beforeDisplayContent || $afterDisplayContent || $this->params->get('show_description', 1) || $this->params->def('show_description_image', 1)) : ?>
 		<div class="category-desc clearfix">
 			<?php if ($this->params->get('show_description_image') && $this->category->getParams()->get('image')) : ?>
-				<img src="<?php echo $this->category->getParams()->get('image'); ?>" alt="<?php echo htmlspecialchars($this->category->getParams()->get('image_alt'), ENT_COMPAT, 'UTF-8'); ?>">
+				<?php $alt = empty($this->category->getParams()->get('image_alt')) && empty($this->category->getParams()->get('image_alt_empty')) ? '' : 'alt="'. htmlspecialchars($this->category->getParams()->get('image_alt'), ENT_COMPAT, 'UTF-8') .'"'; ?>
+				<img src="<?php echo $this->category->getParams()->get('image'); ?>" <?php echo $alt; ?>>
 			<?php endif; ?>
 			<?php echo $beforeDisplayContent; ?>
 			<?php if ($this->params->get('show_description') && $this->category->description) : ?>
