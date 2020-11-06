61bfe28ea597c526ccee8c0fb4de9cc938a9f58f
brian teeman <brian@teeman.net>
2020-11-06 12:08:41 +0000

    remove comment

--- plugins/fields/media/tmpl/media.php
+++ plugins/fields/media/tmpl/media.php
@@ -27,7 +27,6 @@ $buffer = '';
 if ($value)
 {
 	$path = $value['imagefile'];
-	// $alt = ' alt="' . htmlentities($value['alt_text'], ENT_COMPAT, 'UTF-8', true) . '"';
 	$alt = empty($value['alt_text']) && empty($value['alt_empty']) ? '' : 'alt="'. htmlspecialchars($value['alt_text'], ENT_COMPAT, 'UTF-8') .'"';
 
 	if (file_exists($path))
