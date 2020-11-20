b8f0832bf208cf2becbd0de72eaeabde32e03280
Harald Leithner <leithner@itronic.at>
2020-11-20 17:02:53 +0100

    Cleanup unnecessary files

--- libraries/vendor/bin/lessify.bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-setlocal DISABLEDELAYEDEXPANSION
-SET BIN_TARGET=%~dp0/../leafo/lessphp/lessify
-php "%BIN_TARGET%" %*
--- libraries/vendor/bin/plessc.bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-setlocal DISABLEDELAYEDEXPANSION
-SET BIN_TARGET=%~dp0/../leafo/lessphp/plessc
-php "%BIN_TARGET%" %*
