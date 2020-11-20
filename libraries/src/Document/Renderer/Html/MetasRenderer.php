42733830e3ab3ae880a5e31cc056bf00397157fa
dgrammatiko <d.grammatiko@gmail.com>
2020-11-20 13:15:23 +0100

    Move favicon lookup to the renderer

--- libraries/src/Document/Renderer/Html/MetasRenderer.php
+++ libraries/src/Document/Renderer/Html/MetasRenderer.php
@@ -13,7 +13,9 @@ namespace Joomla\CMS\Document\Renderer\Html;
 
 use Joomla\CMS\Document\DocumentRenderer;
 use Joomla\CMS\Factory;
+use Joomla\CMS\Factory as CmsFactory;
 use Joomla\CMS\Helper\TagsHelper;
+use Joomla\CMS\Uri\Uri;
 use Joomla\CMS\WebAsset\WebAssetAttachBehaviorInterface;
 use Joomla\Utilities\ArrayHelper;
 
@@ -108,6 +110,50 @@ class MetasRenderer extends DocumentRenderer
 		if (!empty($base))
 		{
 			$buffer .= $tab . '<base href="' . $base . '">' . $lnEnd;
+		}
+
+		$noFavicon = true;
+		$searchFor = 'image/vnd.microsoft.icon';
+
+		array_map(function($value) use(&$noFavicon, $searchFor) {
+			if ($value['attribs']['type'] === $searchFor)
+			{
+				$noFavicon = false;
+			}
+		}, array_values((array)$this->_doc->_links));
+
+		if ($noFavicon)
+		{
+			$client   = $app->isClient('administrator') === true ? 'administrator/' : 'site/';
+			$template = $app->getTemplate(true);
+
+			// Try to find a favicon by checking the template and root folder
+			$icon = '/favicon.ico';
+			$foldersToCheck = [
+				JPATH_BASE,
+				JPATH_ROOT . '/media/templates/' . $client . $template->template,
+				'templates/' . $template->template,
+			];
+
+			foreach ($foldersToCheck as $base => $dir)
+			{
+				if ($template->parent !== ''
+					&& $base === 1
+					&& !is_file(JPATH_ROOT . '/media/templates/' . $client . $template->template . $icon))
+				{
+					$dir = JPATH_ROOT . '/media/templates/' . $client . $template->parent;
+				}
+
+				if (is_file($dir . $icon))
+				{
+					$urlBase = in_array($base, [0, 2]) ? Uri::base(true) : Uri::root(true);
+					$base    = in_array($base, [0, 2]) ? JPATH_BASE : JPATH_ROOT;
+					$path    = str_replace($base, '', $dir);
+					$path    = str_replace('\\', '/', $path);
+					$this->_doc->addFavicon($urlBase . $path . $icon);
+					break;
+				}
+			}
 		}
 
 		// Generate META tags (needs to happen as early as possible in the head)
