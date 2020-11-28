e549634138095f99a07206b7ffc2213eb39b7819
Richard Fath <richard67@users.noreply.github.com>
2020-11-28 20:09:14 +0100

    Remove irrelevant comment

--- templates/cassiopeia/html/mod_menu/dropdown-metismenu.php
+++ templates/cassiopeia/html/mod_menu/dropdown-metismenu.php
@@ -19,7 +19,6 @@ $wa = $app->getDocument()->getWebAssetManager();
 $wa->registerAndUseScript('metismenu', 'media/templates/cassiopeia/js/mod_menu/menu-metismenu.min.js', [], [], ['metismenujs']);
 
 $attributes          = [];
-// The menu class is deprecated. Use mod-menu instead
 $attributes['class'] = 'mod-menu mod-menu_dropdown-metismenu metismenu mod-list ' . $class_sfx;
 
 if ($tagId = $params->get('tag_id', ''))
