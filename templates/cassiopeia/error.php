ad4349933c21ecc0a9faf5bf4b00c343f24160a4
brian teeman <brian@teeman.net>
2020-11-29 22:08:42 +0000

    [4.0] Cassiopeia copyright dates
    The changes to the copyright date by @nibra in #31504 can not possibly be correct as they predate the actual template creation. (yes I understand why those dates were found)
    
    This pr changes them to the start date of the actual template
    
    code review

--- templates/cassiopeia/error.php
+++ templates/cassiopeia/error.php
@@ -3,7 +3,7 @@
  * @package     Joomla.Site
  * @subpackage  Templates.cassiopeia
  *
- * @copyright   (C) 2012 Open Source Matters, Inc. <https://www.joomla.org>
+ * @copyright   (C) 2017 Open Source Matters, Inc. <https://www.joomla.org>
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
