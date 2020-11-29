f1b26257b262b91a70326ef5e5e870616b9e5762
Harald Leithner <leithner@itronic.at>
2020-11-29 12:48:55 +0100

    Test Systemtests

--- tests/Codeception/drone-system-run.sh
+++ tests/Codeception/drone-system-run.sh
@@ -50,7 +50,8 @@ apache2ctl -D FOREGROUND &
 google-chrome --version
 
 echo "[RUNNER] Start Selenium"
-selenium-standalone start >> selenium.log 2>&1 &
+selenium-standalone start
+#>> /tests/Codeception/_output/selenium.$DB_ENGINE.log 2>&1 &
 sleep 5
 
 echo "[RUNNER] Run Codeception"
