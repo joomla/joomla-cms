ea1c442544c502badf49fa671fbd026a7a311ec1
Hans Kuijpers <hans2103@gmail.com>
2020-12-01 08:39:57 +0100

    change copyright year from 2019 to 2020 (#265)

--- templates/cassiopeia/html/layouts/chromes/card.php
+++ templates/cassiopeia/html/layouts/chromes/card.php
@@ -3,7 +3,7 @@
  * @package     Joomla.Site
  * @subpackage  Templates.cassiopeia
  *
- * @copyright   (C) 2019 Open Source Matters, Inc. <https://www.joomla.org>
+ * @copyright   (C) 2020 Open Source Matters, Inc. <https://www.joomla.org>
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
