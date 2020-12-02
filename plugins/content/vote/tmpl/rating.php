8e697cd774b0827b7861d8b27bd392d0750ba34e
jsanders <j53.sanders@gmail.com>
2020-12-02 16:22:26 +0100

    Check icons in wrong place
    The check is now in the right place.

--- plugins/content/vote/tmpl/rating.php
+++ plugins/content/vote/tmpl/rating.php
@@ -33,18 +33,19 @@ if ($context === 'com_content.categories')
 	return;
 }
 
-// Get paths to icons
+// Get the icons
 $iconStar     = HTMLHelper::_('image', 'plg_content_vote/vote-star.svg', '', '', true, true);
 $iconHalfstar = HTMLHelper::_('image', 'plg_content_vote/vote-star-half.svg', '', '', true, true);
 
-$pathStar     = JPATH_ROOT . substr($iconStar, strlen(Uri::root(true)));
-$pathHalfstar = JPATH_ROOT . substr($iconHalfstar, strlen(Uri::root(true)));
-
-// Check paths
-if ($pathStar === null || $pathHalfstar === null)
+// If you can't find the icons then skip it
+if ($iconStar === null || $iconHalfstar === null)
 {
 	return;
 }
+
+// Get paths to icons
+$pathStar     = JPATH_ROOT . substr($iconStar, strlen(Uri::root(true)));
+$pathHalfstar = JPATH_ROOT . substr($iconHalfstar, strlen(Uri::root(true)));
 
 // Write inline '<svg>' elements
 $star     = file_get_contents($pathStar);
