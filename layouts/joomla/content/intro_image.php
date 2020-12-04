41e565c998c9e6463a067f2a807b4d417e18fbf9
brian teeman <brian@teeman.net>
2020-12-04 09:10:49 +0000

    auth and title

--- layouts/joomla/content/intro_image.php
+++ layouts/joomla/content/intro_image.php
@@ -35,8 +35,9 @@ if ((isset($img->attributes['width']) && (int) $img->attributes['width'] > 0)
 }
 ?>
 <figure class="<?php echo htmlspecialchars($imgclass, ENT_COMPAT, 'UTF-8'); ?> item-image">
-	<?php if ($params->get('link_intro_image') && $params->get('access-view')) : ?>
-		<a href="<?php echo Route::_(RouteHelper::getArticleRoute($displayData->slug, $displayData->catid, $displayData->language)); ?>">
+	<?php if ($params->get('link_intro_image') && ($params->get('access-view') || $params->get('show_noauth', '0') == '1')) : ?>
+		<a href="<?php echo Route::_(RouteHelper::getArticleRoute($displayData->slug, $displayData->catid, $displayData->language)); ?>"
+			itemprop="url" title="<?php echo $this->escape($displayData->title); ?>">
 			<img src="<?php echo htmlspecialchars($img->url, ENT_COMPAT, 'UTF-8'); ?>"
 					 <?php echo $alt; ?>
 					 itemprop="thumbnailUrl"
