bcbf0e69fe44128caf69627b82bd5bf2914da4e4
Thomas Hunziker <werbemails@bakual.ch>
2020-12-15 08:53:49 +0100

    Only one string for error "extension already exists"

--- libraries/src/Installer/Adapter/PluginAdapter.php
+++ libraries/src/Installer/Adapter/PluginAdapter.php
@@ -465,7 +465,7 @@ class PluginAdapter extends InstallerAdapter
 				// Install failed, roll back changes
 				throw new \RuntimeException(
 					Text::sprintf(
-						'JLIB_INSTALLER_ABORT_PLG_INSTALL_ALLREADY_EXISTS',
+						'JLIB_INSTALLER_ABORT_ALREADY_EXISTS',
 						Text::_('JLIB_INSTALLER_' . $this->route),
 						$this->name
 					)
