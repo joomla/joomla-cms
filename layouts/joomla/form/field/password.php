fbdc3fcf01222f2e7c595cc602e31cf12872b497
George Wilson <georgejameswilson@googlemail.com>
2020-12-16 00:20:39 +0000

    Brace on new line

--- layouts/joomla/form/field/password.php
+++ layouts/joomla/form/field/password.php
@@ -68,7 +68,8 @@ if ($meter)
 JHtml::_('jquery.framework');
 JHtml::_('script', 'system/html5fallback.js', array('version' => 'auto', 'relative' => true, 'conditional' => 'lt IE 9'));
 
-if ($lock) {
+if ($lock)
+{
 	// Load script on document load.
 	JFactory::getDocument()->addScriptDeclaration(
 			"
