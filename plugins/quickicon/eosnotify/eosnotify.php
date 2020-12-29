5d079772e2b74e73b411ebca46f2bf9c0fa3649a
softforge <phil@softforge.co.uk>
2020-12-29 14:56:15 +0000

    Add icons

--- plugins/quickicon/eosnotify/eosnotify.php
+++ plugins/quickicon/eosnotify/eosnotify.php
@@ -34,8 +34,25 @@ class plgQuickiconEosnotify extends JPlugin
 		return array(array(
 			'link' => 'http://www.google.com',
 			'image' => JURI::root() . 'plugins/quickicon/eosnotify/stop15.png',
-			'text' => '<span style="color:red;">Joomla 2.5 Support Has Ended!!<br />Click Here for More Information.</span>',
+			'text' => '<span style="color:red;">Joomla 3.10 Support Has Ended!!<br />Click Here for More Information.</span>',
 			'id' => 'plg_quickicon_eos'
 		));
+		$app = JFactory::getApplication();
+		if (!$app->isClient('administrator') || version_compare(JVERSION, '4.0', '>='))
+		{
+			return;
+		}
+
+		$text = JText::_('PLG_EOSNOTIFY_SUPPORT_ENDING');
+
+		if (JFactory::getDate() >= '2020-01-01') {
+			$text = JText::_('PLG_EOSNOTIFY_SUPPORT_ENDED');
+		}
+// Only to com_cpanel
+		if ($this->app->input->get('option') == 'com_cpanel')
+		{
+			$app->enqueueMessage(JText::sprintf('PLG_EOSNOTIFY_CLICK_FOR_INFORMATION_WITH_LINK', $text), 'warning');
+		}
+
 	}
 }
