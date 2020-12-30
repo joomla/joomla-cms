bedd1ef3e7d5eff1148bf22ef2cd3ee82c6d627c
Nicola Galgano <optimus4joomla@gmail.com>
2020-12-30 15:07:35 +0100

    Update plugins/content/pagenavigation/pagenavigation.php
    Co-authored-by: Richard Fath <richard67@users.noreply.github.com>

--- plugins/content/pagenavigation/pagenavigation.php
+++ plugins/content/pagenavigation/pagenavigation.php
@@ -90,7 +90,7 @@ class PlgContentPagenavigation extends CMSPlugin
 					// Use created if modified is not set
 					case 'modified':
 						$orderby = 'CASE WHEN ' . $db->quoteName('a.modified') . ' IS NULL THEN ' .
-						$db->quoteName('a.created') . ' ELSE ' . $db->quoteName('a.modified') . ' END';
+							$db->quoteName('a.created') . ' ELSE ' . $db->quoteName('a.modified') . ' END';
 						break;
 
 					// Use created if publish_up is not set
