6512836a9cb28ed4d25461c723cd24b948b77fab
Nicola Galgano <optimus4joomla@gmail.com>
2020-12-31 09:17:02 +0100

    RateLimitException

---
+++ libraries/src/Router/Exception/RateLimitException.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Joomla! Content Management System
+ *
+ * @copyright  Copyright (C) 2005 - 2020 Open Source Matters, Inc. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+namespace Joomla\CMS\Router\Exception;
+
+\defined('JPATH_PLATFORM') or die;
+
+/**
+ * Exception class defining a rate limit exceded event
+ *
+ * @since  4.0.0
+ */
+class RateLimitException extends \RuntimeException
+{
+}
