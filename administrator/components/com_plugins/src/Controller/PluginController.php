091498a7b8fc3c6ee38fc2c39e27b360c227b0b6
Nicola Galgano <optimus4joomla@gmail.com>
2020-12-31 09:51:13 +0100

    reset rate limit

--- administrator/components/com_plugins/src/Controller/PluginController.php
+++ administrator/components/com_plugins/src/Controller/PluginController.php
@@ -11,6 +11,8 @@ namespace Joomla\Component\Plugins\Administrator\Controller;
 \defined('_JEXEC') or die;
 
 use Joomla\CMS\MVC\Controller\FormController;
+use Joomla\CMS\Router\Route;
+use Joomla\CMS\Uri\Uri;
 
 /**
  * Plugin controller class.
@@ -19,4 +21,27 @@ use Joomla\CMS\MVC\Controller\FormController;
  */
 class PluginController extends FormController
 {
+	/**
+	 * Method to reset the public GET rate limit
+	 *
+	 * @return  void
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 *
+	 * @throws  Exception
+	 */
+	public function resetrate()
+	{
+		// Check for request forgeries.
+		$this->checkToken();
+
+		$id = $this->input->get('extension_id');
+
+		/** @var PluginModel $model */
+		$model = $this->getModel();
+
+		// Reset the rate limit
+		$model->resetRateLimit($id);
+		$this->setRedirect(Route::_('index.php?option=com_plugins', false), '');
+	}
 }
