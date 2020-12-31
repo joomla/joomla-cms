21431044d9213541ed8cfb2ed4cef300bf414bca
Nicola Galgano <optimus4joomla@gmail.com>
2020-12-31 09:29:27 +0100

    rate limit

--- plugins/webservices/content/content.php
+++ plugins/webservices/content/content.php
@@ -3,7 +3,7 @@
  * @package     Joomla.Plugin
  * @subpackage  Webservices.Content
  *
- * @copyright   (C) 2019 Open Source Matters, Inc. <https://www.joomla.org>
+ * @copyright   Copyright (C) 2005 - 2020 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
@@ -12,6 +12,7 @@ defined('_JEXEC') or die;
 use Joomla\CMS\Factory;
 use Joomla\CMS\Plugin\CMSPlugin;
 use Joomla\CMS\Router\ApiRouter;
+use Joomla\Registry\Registry;
 use Joomla\Router\Route;
 
 /**
@@ -21,6 +22,23 @@ use Joomla\Router\Route;
  */
 class PlgWebservicesContent extends CMSPlugin
 {
+	/**
+	 * Application object
+	 *
+	 * @var    JApplicationCms
+	 *
+	 * @since  4.0.0
+	 */
+	protected $app;
+
+	/**
+	 * Database Driver Instance
+	 *
+	 * @var    DatabaseDriver
+	 * @since  4.0.0
+	 */
+	protected $db;
+
 	/**
 	 * Load the language file on instantiation.
 	 *
@@ -48,8 +66,8 @@ class PlgWebservicesContent extends CMSPlugin
 	/**
 	 * Constructor.
 	 *
-	 * @param   object  &$subject  The object to observe.
-	 * @param   array   $config    An optional associative array of configuration settings.
+	 * @param   object  $subject  The object to observe.
+	 * @param   array   $config   An optional associative array of configuration settings.
 	 *
 	 * @since  4.0.0
 	 */
@@ -59,25 +77,28 @@ class PlgWebservicesContent extends CMSPlugin
 
 		$this->allowedVerbs = $this->params->get('restverbs', []);
 		$this->allowPublic  = $this->params->get('public', false);
+		$this->limit        = $this->params->get('limit', 0);
+		$this->taskid       = (int) $this->params->get('taskid', 0);
+
+		if ($this->taskid > $this->limit)
+		{
+			$endpoint = $this->_name . '.' . $this->_type . '.ratelimit';
+			$this->app->input->set($endpoint, $this->limit, 'int');
+		}
 	}
 
 	/**
 	 * Registers com_content's API's routes in the application
 	 *
-	 * @param   ApiRouter       &$router  The API Routing object
-	 * @param   ApiApplication  $object   The API Application object
+	 * @param   ApiRouter       $router  The API Routing object
+	 * @param   ApiApplication  $object  The API Application object1
 	 *
 	 * @return  void
 	 *
 	 * @since   4.0.0
 	 */
-	public function onBeforeApiRoute(&$router, $object)
+	public function onBeforeApiRoute(&$router)
 	{
-		if (!in_array($object->input->getMethod(), $this->allowedVerbs))
-		{
-			return;
-		}
-
 		$router->createCRUDRoutes(
 			'v1/content/article',
 			'articles',
@@ -102,7 +123,7 @@ class PlgWebservicesContent extends CMSPlugin
 	/**
 	 * Create fields routes
 	 *
-	 * @param   ApiRouter  &$router  The API Routing object
+	 * @param   ApiRouter  $router  The API Routing object
 	 *
 	 * @return  void
 	 *
@@ -146,7 +167,7 @@ class PlgWebservicesContent extends CMSPlugin
 	/**
 	 * Create contenthistory routes
 	 *
-	 * @param   ApiRouter  &$router  The API Routing object
+	 * @param   ApiRouter  $router  The API Routing object
 	 *
 	 * @return  void
 	 *
@@ -181,9 +202,80 @@ class PlgWebservicesContent extends CMSPlugin
 		$router->addRoutes($routes);
 	}
 
+	/**
+	 * Manage public GET request
+	 *
+	 * @return  void
+	 *
+	 * @since   4.0.0
+	 */
 	public function onAfterApiRoute($object, $publicApi)
 	{
 		$status = $publicApi ? 0 : 1;
-		Factory::getApplication()->input->set('isPublicApi', $status);
+		$this->app->input->set('isPublicApi', $status);
+	}
+
+	/**
+	 * Count webservice public GET usage
+	 *
+	 * @return  void
+	 *
+	 * @since   4.0.0
+	 */
+	public function onPublicGet($webservice)
+	{
+		if ($webservice !== 'content.webservice')
+		{
+			return;
+		}
+
+		$taskid   = null;
+		$db = $this->db;
+		$type = 'webservices';
+		$name = 'content';
+
+		$query = $db->getQuery(true);
+
+		$query->select($db->quoteName(['extension_id', 'params']))
+			->from($db->quoteName('#__extensions'))
+			->where($db->quoteName('element') . ' = :element')
+			->where($db->quoteName('folder') . ' = :folder')
+			->bind(':element', $name)
+			->bind(':folder', $type);
+
+		$db->setQuery($query);
+
+		$params = $db->loadObject();
+
+		$query  = $db->getQuery(true);
+		$now    = Factory::getDate()->toSql();
+		$query->update($db->quoteName('#__extensions'));
+
+		// Update last run and taskid
+		$taskParams = json_decode($params->params, true);
+		$taskid = $taskParams['taskid'];
+
+		$taskid++;
+		$registry = new Registry($taskParams);
+		$registry->set('taskid', $taskid);
+		$jsonparam = $registry->toString('JSON');
+
+		$query->set($db->quoteName('params') . ' = :params')
+			->where($db->quoteName('element') . ' = :element')
+			->where($db->quoteName('folder') . ' = :folder')
+			->bind(':params', $jsonparam)
+			->bind(':element', $name)
+			->bind(':folder', $type);
+
+		try
+		{
+			// Update the plugin parameters
+			$result = $db->setQuery($query)->execute();
+		}
+		catch (RuntimeException $e)
+		{
+			// If we failed to execute
+			return;
+		}
 	}
 }
