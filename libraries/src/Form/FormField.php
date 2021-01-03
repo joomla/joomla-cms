e0aa8dc1d92c9883596d08e04ec076cc8634031d
Harald Leithner <leithner@itronic.at>
2021-01-03 18:38:43 +0100

    Update libraries/src/Form/FormField.php
    Co-authored-by: Quy <quy@fluxbb.org>

--- libraries/src/Form/FormField.php
+++ libraries/src/Form/FormField.php
@@ -682,7 +682,7 @@ abstract class FormField
 
 		foreach ($attributes as $attributeName)
 		{
-				$this->__set($attributeName, $element[$attributeName]);
+			$this->__set($attributeName, $element[$attributeName]);
 		}
 
 		// Allow for repeatable elements
