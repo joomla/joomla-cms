9e5a291e560a4558f5c79d7268863ec5e604c9d8
brian teeman <brian@teeman.net>
2021-01-09 10:17:54 +0000

    [4.0] Export modal

--- administrator/components/com_banners/tmpl/tracks/default.php
+++ administrator/components/com_banners/tmpl/tracks/default.php
@@ -89,19 +89,6 @@ $listDirn   = $this->escape($this->state->get('list.direction'));
 				<?php echo HTMLHelper::_(
 					'bootstrap.renderModal',
 					'downloadModal',
-					[
-						'title'       => Text::_('COM_BANNERS_TRACKS_DOWNLOAD'),
-						'url'         => Route::_('index.php?option=com_banners&amp;view=download&amp;tmpl=component'),
-						'height'      => '370px',
-						'width'       => '300px',
-						'modalWidth'  => '40',
-						'footer'      => '<button type="button" class="btn" data-dismiss="modal"'
-								. ' onclick="Joomla.iframeButtonClick({iframeSelector: \'#downloadModal\', buttonSelector: \'#closeBtn\'})">'
-								. Text::_('COM_BANNERS_CANCEL') . '</button>'
-								. '<button type="button" class="btn btn-success"'
-								. ' onclick="Joomla.iframeButtonClick({iframeSelector: \'#downloadModal\', buttonSelector: \'#exportBtn\'})">'
-								. Text::_('COM_BANNERS_TRACKS_EXPORT') . '</button>',
-					]
 				); ?>
 				<input type="hidden" name="task" value="">
 				<input type="hidden" name="boxchecked" value="0">
