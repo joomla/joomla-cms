bb7a2e43ef9bd0968f53d8b9bf4158d6771786a8
Harald Leithner <leithner@itronic.at>
2021-01-11 11:56:41 +0100

    Update joomlaupdate login screen

--- administrator/components/com_joomlaupdate/tmpl/update/finaliseconfirm.php
+++ administrator/components/com_joomlaupdate/tmpl/update/finaliseconfirm.php
@@ -10,93 +10,34 @@
 defined('_JEXEC') or die;
 
 use Joomla\CMS\Factory;
-use Joomla\CMS\Helper\AuthenticationHelper;
-use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Helper\ModuleHelper;
 use Joomla\CMS\Language\Text;
-use Joomla\CMS\Router\Route;
+use Joomla\Component\Login\Administrator\Model\LoginModel;
+use Joomla\Registry\Registry;
 
-HTMLHelper::_('behavior.keepalive');
-
-$twofactormethods = AuthenticationHelper::getTwoFactorMethods();
+$loginmodule = LoginModel::getLoginModule('mod_login');
+$loginmodule->params = new Registry($loginmodule->params);
+$loginmodule->params->set('layout', 'update');
+$loginmodule->params->set('task', 'update.finaliseconfirm');
 
 ?>
-
-<div class="alert warning">
-	<h4 class="alert-heading">
-		<?php echo Text::_('COM_JOOMLAUPDATE_VIEW_UPDATE_FINALISE_HEAD'); ?>
-	</h4>
-	<p>
-		<?php echo Text::sprintf('COM_JOOMLAUPDATE_VIEW_UPDATE_FINALISE_HEAD_DESC', Factory::getApplication()->get('sitename')); ?>
-	</p>
+<div id="system-message-container" aria-live="polite">
+	<joomla-alert type="warning" class="joomla-alert--show">
+		<div class="alert-heading"><span class="warning"></span><span class="sr-only"><?php echo Text::_('WARNING'); ?></span></div>
+		<div class="alert-wrapper">
+			<div class="alert-message">
+				<h2>
+					<?php echo Text::_('COM_JOOMLAUPDATE_VIEW_UPDATE_FINALISE_HEAD'); ?>
+				</h2>
+				<p>
+					<?php echo Text::sprintf('COM_JOOMLAUPDATE_VIEW_UPDATE_FINALISE_HEAD_DESC', Factory::getApplication()->get('sitename')); ?>
+				</p>
+			</div>
+		</div>
+	</joomla-alert>
 </div>
-
-<hr>
-
-<form action="<?php echo Route::_('index.php', true); ?>" method="post" id="form-login" class="d-flex justify-content-center text-center">
-	<fieldset class="loginform">
-		<legend><?php echo Text::_('COM_JOOMLAUPDATE_CONFIRM'); ?></legend>
-		<div class="control-group">
-			<div class="controls">
-				<div class="input-group">
-					<input name="username" id="mod-login-username" type="text" class="form-control" placeholder="<?php echo Text::_('JGLOBAL_USERNAME'); ?>" size="15" autofocus="true">
-					<span class="input-group-append">
-						<span class="input-group-text">
-							<span class="icon-user" aria-hidden="true"></span>
-							<label for="mod-login-username" class="sr-only">
-								<?php echo Text::_('JGLOBAL_USERNAME'); ?>
-							</label>
-						</span>
-					</span>
-				</div>
-			</div>
-		</div>
-		<div class="control-group">
-			<div class="controls">
-				<div class="input-group">
-					<input name="passwd" id="mod-login-password" type="password" class="form-control" placeholder="<?php echo Text::_('JGLOBAL_PASSWORD'); ?>" size="15">
-					<span class="input-group-append">
-						<span class="input-group-text">
-							<span class="icon-lock" aria-hidden="true"></span>
-							<label for="mod-login-password" class="sr-only">
-								<?php echo Text::_('JGLOBAL_PASSWORD'); ?>
-							</label>
-						</span>
-					</span>
-				</div>
-			</div>
-		</div>
-		<?php if (count($twofactormethods) > 1) : ?>
-			<div class="control-group">
-				<div class="controls">
-					<div class="input-group">
-						<input name="secretkey" autocomplete="one-time-code" id="mod-login-secretkey" type="text" class="form-control" placeholder="<?php echo Text::_('JGLOBAL_SECRETKEY'); ?>" size="15">
-						<span class="input-group-append">
-							<span class="input-group-text" title="<?php echo Text::_('JGLOBAL_SECRETKEY_HELP'); ?>">
-								<span class="icon-star" aria-hidden="true"></span>
-								<label for="mod-login-secretkey" class="sr-only">
-									<?php echo Text::_('JGLOBAL_SECRETKEY'); ?>
-								</label>
-							</span>
-						</span>
-					</div>
-				</div>
-			</div>
-		<?php endif; ?>
-		<div class="control-group">
-			<div class="controls">
-				<div class="btn-group">
-					<a class="btn btn-danger" href="index.php?option=com_joomlaupdate">
-						<span class="icon-times" aria-hidden="true"></span> <?php echo Text::_('JCANCEL'); ?>
-					</a>
-					<button type="submit" class="btn btn-primary">
-						<span class="icon-play" aria-hidden="true"></span> <?php echo Text::_('COM_JOOMLAUPDATE_VIEW_UPDATE_FINALISE_CONFIRM_AND_CONTINUE'); ?>
-					</button>
-				</div>
-			</div>
-		</div>
-
-		<input type="hidden" name="option" value="com_joomlaupdate">
-		<input type="hidden" name="task" value="update.finaliseconfirm">
-		<?php echo HTMLHelper::_('form.token'); ?>
-	</fieldset>
-</form>
+<section class="view-login">
+	<div class="login mx-auto">
+		<?php echo ModuleHelper::renderModule($loginmodule, ['id' => 'section-box']); ?>
+	</div>
+</section>
