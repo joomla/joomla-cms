9580d5bf1107ddf090ef6c225fc6a0133d3ce143
wilsonge <georgejameswilson@googlemail.com>
2021-01-12 01:03:27 +0000

    Changes to make styling match

--- installation/tmpl/setup/default.php
+++ installation/tmpl/setup/default.php
@@ -24,7 +24,7 @@ HTMLHelper::_('behavior.formvalidator');
 				<span class="icon-language" aria-hidden="true"></span> <?php echo Text::_('INSTL_SELECT_INSTALL_LANG'); ?>
 			</legend>
 			<div class="j-install-step-form">
-				<div class="form-group">
+				<div class="mb-3">
 					<?php echo $this->form->getLabel('language'); ?>
 					<?php echo $this->form->getInput('language'); ?>
 				</div>
@@ -40,12 +40,14 @@ HTMLHelper::_('behavior.formvalidator');
 				<span class="icon-cog" aria-hidden="true"></span> <?php echo Text::_('INSTL_SETUP_SITE_NAME'); ?>
 			</legend>
 			<div class="j-install-step-form">
-				<div class="form-group">
+				<div class="mb-3">
 					<?php echo $this->form->getLabel('site_name'); ?>
 					<?php echo $this->form->getInput('site_name'); ?>
 				</div>
-				<div class="form-group">
-					<button class="btn btn-primary btn-block" id="step1"><?php echo Text::_('INSTL_SETUP_LOGIN_DATA'); ?> <span class="icon-chevron-right" aria-hidden="true"></span></button>
+				<div class="mb-3">
+					<div class="d-grid gap-2 installer-next-step">
+						<button class="btn btn-primary" id="step1"><?php echo Text::_('INSTL_SETUP_LOGIN_DATA'); ?> <span class="icon-chevron-right" aria-hidden="true"></span></button>
+					</div>
 				</div>
 			</div>
 		</fieldset>
@@ -54,24 +56,26 @@ HTMLHelper::_('behavior.formvalidator');
 				<span class="icon-lock" aria-hidden="true"></span> <?php echo Text::_('INSTL_LOGIN_DATA'); ?>
 			</legend>
 			<div class="j-install-step-form">
-				<div class="form-group">
+				<div class="mb-3">
 					<?php echo $this->form->getLabel('admin_user'); ?>
 					<?php echo $this->form->getInput('admin_user'); ?>
 				</div>
-				<div class="form-group">
+				<div class="mb-3">
 					<?php echo $this->form->getLabel('admin_username'); ?>
 					<?php echo $this->form->getInput('admin_username'); ?>
 				</div>
-				<div class="form-group">
+				<div class="mb-3">
 					<?php echo $this->form->getLabel('admin_password'); ?>
 					<?php echo $this->form->getInput('admin_password'); ?>
 				</div>
-				<div class="form-group">
+				<div class="mb-3">
 					<?php echo $this->form->getLabel('admin_email'); ?>
 					<?php echo $this->form->getInput('admin_email'); ?>
 				</div>
-				<div class="form-group">
-					<button class="btn btn-primary btn-block" id="step2"><?php echo Text::_('INSTL_CONNECT_DB'); ?> <span class="icon-chevron-right" aria-hidden="true"></span></button>
+				<div class="mb-3">
+					<div class="d-grid gap-2 installer-next-step">
+						<button class="btn btn-primary" id="step2"><?php echo Text::_('INSTL_CONNECT_DB'); ?> <span class="icon-chevron-right" aria-hidden="true"></span></button>
+					</div>
 				</div>
 			</div>
 		</fieldset>
@@ -80,27 +84,27 @@ HTMLHelper::_('behavior.formvalidator');
 				<span class="icon-database" aria-hidden="true"></span> <?php echo Text::_('INSTL_DATABASE'); ?>
 			</legend>
 			<div class="j-install-step-form">
-				<div class="form-group">
+				<div class="mb-3">
 					<?php echo $this->form->getLabel('db_type'); ?>
 					<?php echo $this->form->getInput('db_type'); ?>
 				</div>
-				<div class="form-group">
+				<div class="mb-3">
 					<?php echo $this->form->getLabel('db_host'); ?>
 					<?php echo $this->form->getInput('db_host'); ?>
 				</div>
-				<div class="form-group">
+				<div class="mb-3">
 					<?php echo $this->form->getLabel('db_user'); ?>
 					<?php echo $this->form->getInput('db_user'); ?>
 				</div>
-				<div class="form-group">
+				<div class="mb-3">
 					<?php echo $this->form->getLabel('db_pass'); ?>
 					<?php echo $this->form->getInput('db_pass'); ?>
 				</div>
-				<div class="form-group">
+				<div class="mb-3">
 					<?php echo $this->form->getLabel('db_name'); ?>
 					<?php echo $this->form->getInput('db_name'); ?>
 				</div>
-				<div class="form-group">
+				<div class="mb-3">
 					<?php echo $this->form->getLabel('db_prefix'); ?>
 					<?php echo $this->form->getInput('db_prefix'); ?>
 				</div>
@@ -110,12 +114,14 @@ HTMLHelper::_('behavior.formvalidator');
 				<?php echo $this->form->getField('db_sslverifyservercert')->renderField(); ?>
 				<?php echo $this->form->getField('db_sslca')->renderField(); ?>
 				<?php echo $this->form->getField('db_sslcipher')->renderField(); ?>
-				<div class="form-group">
+				<div class="mb-3">
 					<?php //echo $this->form->getLabel('db_old'); ?>
 					<?php echo $this->form->getInput('db_old'); ?>
 				</div>
-				<div class="form-group">
-					<button class="btn btn-primary btn-block" id="setupButton"><?php echo Text::_('INSTL_INSTALL_JOOMLA'); ?> <span class="icon-chevron-right" aria-hidden="true"></span></button>
+				<div class="mb-3">
+					<div class="d-grid gap-2 installer-next-step">
+						<button class="btn btn-primary" id="setupButton"><?php echo Text::_('INSTL_INSTALL_JOOMLA'); ?> <span class="icon-chevron-right" aria-hidden="true"></span></button>
+					</div>
 				</div>
 			</div>
 		</fieldset>
