2fc47847466029647fbe28059144d844d7c93bbc
Phil E. Taylor <phil@phil-taylor.com>
2021-01-15 12:49:22 +0000

    revert and improve #32013 to use htmlspecialchars on user supplied placeholders
    Signed-off-by: Phil E. Taylor <phil@phil-taylor.com>

--- layouts/joomla/form/field/password.php
+++ layouts/joomla/form/field/password.php
@@ -81,12 +81,12 @@ if ($lock)
 	Text::script('JCANCEL');
 
 	$disabled = true;
-	$hint = str_repeat('&#x2022;', strlen($value));
+	$hint = str_repeat('•', strlen($value));
 	$value = '';
 }
 
 $attributes = array(
-	strlen($hint) ? 'placeholder="' . $hint . '"' : '',
+	strlen($hint) ? 'placeholder="' . htmlspecialchars($hint, ENT_COMPAT, 'UTF-8') . '"' : '',
 	!empty($autocomplete) ? 'autocomplete="' . $autocomplete . '"' : '',
 	!empty($class) ? 'class="form-control ' . $class . '"' : 'class="form-control"',
 	!empty($description) ? 'aria-describedby="' . $name . '-desc"' : '',
