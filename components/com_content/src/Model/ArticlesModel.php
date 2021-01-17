ee5574f53f3cf2e420b115a1fbf38417d7886e14
Marco <marco.rensch@area59.ch>
2021-01-17 08:55:47 +0100

    Update components/com_content/src/Model/ArticlesModel.php
    Co-authored-by: SharkyKZ <sharkykz@gmail.com>

--- components/com_content/src/Model/ArticlesModel.php
+++ components/com_content/src/Model/ArticlesModel.php
@@ -572,8 +572,7 @@ class ArticlesModel extends ListModel
 				$relativeDate = (int) $this->getState('filter.relative_date', 0);
 				$query->where(
 					$db->quoteName($dateField) . ' IS NOT NULL AND '
-					. $db->quoteName($dateField) . ' >= ' . $query->dateAdd(':relativeNowDate ', -1 * $relativeDate, 'DAY')
-					->bind(':relativeNowDate ', $nowDate)
+					. $db->quoteName($dateField) . ' >= ' . $query->dateAdd($db->quote($nowDate), -1 * $relativeDate, 'DAY')
 				);
 				break;
 
