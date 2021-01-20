5135c3763e03559d25c5faa2135839598b1790de
George Wilson <georgejameswilson@googlemail.com>
2021-01-20 16:27:25 +0000

    Update libraries/src/HTML/Helpers/Grid.php
    Co-authored-by: Quy <quy@fluxbb.org>

--- libraries/src/HTML/Helpers/Grid.php
+++ libraries/src/HTML/Helpers/Grid.php
@@ -128,7 +128,7 @@ abstract class Grid
 
 		return $checkedOut ? '' : '<label for="' . $stub . $rowNum . '"><span class="visually-hidden">' . Text::_('JSELECT')
 			. ' ' . htmlspecialchars($title, ENT_COMPAT, 'UTF-8') . '</span></label>'
-			. '<input autocomplete="off" type="checkbox" id="' . $stub . $rowNum . '" name="' . $name . '[]" value="' . $recId
+			. '<input class="form-check-input" autocomplete="off" type="checkbox" id="' . $stub . $rowNum . '" name="' . $name . '[]" value="' . $recId
 			. '" onclick="Joomla.isChecked(this.checked);">';
 	}
 
