05f454e0c724cd8fc368d93f5aa2dd0130a8b648
dgrammatiko <d.grammatiko@gmail.com>
2021-01-21 19:56:41 +0100

    Fix frontend editing tooltips
    Please merge Brian’s PR

---
+++ build/media_source/system/js/frontediting.es6.js
@@ -0,0 +1,123 @@
+/**
+ * @copyright (C) 2013 Open Source Matters, Inc. <https://www.joomla.org>
+ * @license   GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+/**
+ * JavaScript behavior to add front-end hover edit icons with tooltips for modules and menu items.
+ */
+document.addEventListener('DOMContentLoaded', () => {
+  'strict';
+
+  // Modules edit icons:
+  const editableModules = Array.from(document.querySelectorAll('.jmoddiv'));
+  const attachListeners = (element) => {
+    element.addEventListener('mouseenter', (event) => {
+      // Get module editing URL and tooltip for module edit:
+      const self = event.target;
+      const moduleEditUrl = self.dataset.jmodediturl;
+      const moduleTip = self.dataset.jmodtip;
+      const moduleTarget = self.dataset.bsTarget;
+
+      // Stop timeout on previous tooltip and remove it:
+      const buttons = Array.from(document.querySelectorAll('btn.jmodedit'));
+      buttons.map((el) => {
+        if (Joomla.Bootstrap.Instances.Tooltip.get(el)) {
+          el.dispose();
+          el.remove();
+        }
+        return el;
+      });
+
+      // Add editing button with tooltip:
+      self.classList.add('jmodinside');
+      self.insertAdjacentHTML('afterbegin', `<a class="btn btn-link jmodedit" href="#" target="${moduleTarget}"><span class="icon-edit"></span></a>`);
+      const firstEl = self.firstElementChild;
+      firstEl.setAttribute('href', moduleEditUrl);
+      firstEl.setAttribute('title', moduleTip);
+
+      Joomla.Bootstrap.Initialise.Tooltip(firstEl, { container: firstEl.closest('.jmodedit'), html: true, placement: 'top' });
+    });
+
+    element.addEventListener('mouseleave', () => {
+      // Delay remove editing button if not hovering it:
+      const buttons = Array.from(document.querySelectorAll('btn.jmodedit'));
+      buttons.map((el) => {
+        if (Joomla.Bootstrap.Instances.Tooltip.get(el)) {
+          el.dispose();
+        }
+        return el;
+      });
+    });
+  };
+
+  editableModules.map((el) => attachListeners(el));
+
+  // Menu items edit icons:
+  const editableMenus = Array.from(document.querySelectorAll('.jmoddiv[data-jmenuedittip] .nav li,.jmoddiv[data-jmenuedittip].nav li,.jmoddiv[data-jmenuedittip] .nav .nav-child li,.jmoddiv[data-jmenuedittip].nav .nav-child li'));
+  const attachMenusListeners = (element) => {
+    element.addEventListener('mouseenter', (ev) => {
+      // Get menu ItemId from the item-nnn class of the li element of the menu:
+      const itemids = /\bitem-(\d+)\b/.exec(ev.target.getAttribute('class'));
+      let menuitemEditUrl;
+      let enclosingModuleDiv;
+      if (typeof itemids[1] === 'string') {
+        // Find module editing URL from enclosing module:
+        enclosingModuleDiv = ev.target.closest('.jmoddiv');
+        // Transform module editing URL into Menu Item editing url:
+        menuitemEditUrl = enclosingModuleDiv.data('jmodediturl').replace(
+          /\/index.php\?option=com_config&view=modules([^\d]+).+$/,
+          `/administrator/index.php?option=com_menus&view=item&layout=edit$1${itemids[1]}`,
+        );
+      }
+      // Get tooltip for menu items from enclosing module
+      const menuEditTip = enclosingModuleDiv.dataset.jmenuedittip.replace('%s', itemids[1]);
+
+      if (Joomla.Bootstrap.Instances.Popover.get(ev.target)) {
+        ev.target.hide();
+      }
+      Joomla.Bootstrap.Initialise.Popover(ev.target, {
+        html: true,
+        content: `<div><a class="btn jfedit-menu" href="${menuitemEditUrl}" title="${menuEditTip}" target="_blank"><span class="icon-edit"></span></a></div>`,
+        container: 'body',
+        trigger: 'manual',
+        animation: false,
+        placement: 'bottom',
+      });
+      ev.target.show();
+
+      const popovers = Array.from(document.querySelectorAll('body > div.popover'));
+      popovers.map((el) => {
+        el.addEventListener('mouseenter', (ev1) => {
+          if (Joomla.Bootstrap.Instances.Popover.get(ev1.target)) {
+            ev1.target.clearQueue();
+          }
+        });
+        el.addEventListener('mouseleave', (ev2) => {
+          if (Joomla.Bootstrap.Instances.Popover.get(ev2.target)) {
+            ev2.target.hide();
+          }
+        });
+        return el;
+      });
+
+      const allPopovers = Array.from(document.querySelectorAll('.jfedit-menu'));
+      allPopovers.map((el) => Joomla.Bootstrap.Initialise.Tooltip(el,
+        { container: false, html: true, placement: 'bottom' }));
+      // const allPopovers = Array.from(document.querySelectorAll('.jfedit-menu'));
+      // const xxx = $('body>div.popover').find('.jfedit-menu').get()[0];
+      // Joomla.Bootstrap.Initialise.Tooltip(xxx,
+      // { container: false, html: true, placement: 'bottom' });
+    });
+
+    element.addEventListener('mouseleave', (ev) => {
+      setTimeout(() => {
+        if (Joomla.Bootstrap.Instances.Tooltip.get(ev.target)) {
+          ev.target.hide();
+        }
+      }, 1500);
+    });
+  };
+
+  editableMenus.map((el) => attachMenusListeners(el));
+});
