a4b7669d9d25d59af8f56bc7f48a8417e3394788
George Wilson <georgejameswilson@googlemail.com>
2021-01-23 21:41:23 +0000

    Comment about security

--- build/media_source/vendor/bootstrap/js/tab.es6.js
+++ build/media_source/vendor/bootstrap/js/tab.es6.js
@@ -45,6 +45,7 @@ Joomla.Bootstrap.Initialise.Tab = (el, options) => {
             link.setAttribute('aria-controls', element.dataset.id);
             link.setAttribute('aria-selected', element.dataset.id);
 
+            // As we are re-rendering text already displayed on the page we judge that there isn't a risk of XSS attacks
             link.innerHTML = element.dataset.title;
 
             const li = document.createElement('li');
