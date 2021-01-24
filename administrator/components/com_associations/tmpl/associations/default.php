5da8e8eaefd7140ba25cf2211c7d1a4d2325bace
infograf768 <infografjms@gmail.com>
2021-01-24 11:44:37 +0100

    [4.0] RTL: wrong float in Associations manager (#32133)

--- administrator/components/com_associations/tmpl/associations/default.php
+++ administrator/components/com_associations/tmpl/associations/default.php
@@ -106,7 +106,7 @@ Text::script('COM_ASSOCIATIONS_PURGE_CONFIRM_PROMPT', true);
 									</td>
 								<?php endif; ?>
 								<th scope="row" class="has-context">
-									<div class="float-start break-word">
+									<div class="break-word">
 										<?php if (isset($item->level)) : ?>
 											<?php echo LayoutHelper::render('joomla.html.treeprefix', array('level' => $item->level)); ?>
 										<?php endif; ?>
