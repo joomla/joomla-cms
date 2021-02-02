c13cbb01ef77d87b21ec03876d37eb23b80a3495
wilsonge <georgejameswilson@googlemail.com>
2021-02-02 20:03:35 +0000

    Reset to dev

--- libraries/src/Version.php
+++ libraries/src/Version.php
@@ -61,7 +61,7 @@ final class Version
 	 * @var    string
 	 * @since  3.8.0
 	 */
-	const EXTRA_VERSION = 'beta7';
+	const EXTRA_VERSION = 'beta8-dev';
 
 	/**
 	 * Development status.
@@ -69,7 +69,7 @@ final class Version
 	 * @var    string
 	 * @since  3.5
 	 */
-	const DEV_STATUS = 'Beta';
+	const DEV_STATUS = 'Development';
 
 	/**
 	 * Code name.
@@ -93,7 +93,7 @@ final class Version
 	 * @var    string
 	 * @since  3.5
 	 */
-	const RELTIME = '19:23';
+	const RELTIME = '20:02';
 
 	/**
 	 * Release timezone.
