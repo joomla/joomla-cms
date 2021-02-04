5ab25f4fcae3a3fd2aad0e3529a4f21245bd3a32
Tuan Pham Ngoc <github@joomdonation.com>
2021-02-04 22:40:00 +0700

    Fix sitename added to breadcrumb in tag view

--- components/com_tags/src/View/Tag/HtmlView.php
+++ components/com_tags/src/View/Tag/HtmlView.php
@@ -320,21 +320,8 @@ class HtmlView extends BaseHtmlView
 			}
 		}
 
-		if (empty($title))
-		{
-			$title = $app->get('sitename');
-		}
-		elseif ($app->get('sitename_pagetitles', 0) == 1)
-		{
-			$title = Text::sprintf('JPAGETITLE', $app->get('sitename'), $title);
-		}
-		elseif ($app->get('sitename_pagetitles', 0) == 2)
-		{
-			$title = Text::sprintf('JPAGETITLE', $title, $app->get('sitename'));
-		}
-
-		$this->document->setTitle($title);
-
+		$this->setDocumentTitle($title);
+		
 		$pathway->addItem($title);
 
 		foreach ($this->item as $itemElement)
