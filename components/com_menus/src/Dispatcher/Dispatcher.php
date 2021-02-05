1d731fa5c0d7aa0148c0251a28d53243510950ef
Tuan Pham Ngoc <github@joomdonation.com>
2021-02-05 15:08:09 +0700

    Fake fatal error for fake URL in com_menus frontend

--- components/com_menus/src/Dispatcher/Dispatcher.php
+++ components/com_menus/src/Dispatcher/Dispatcher.php
@@ -45,9 +45,9 @@ class Dispatcher extends ComponentDispatcher
 	{
 		parent::checkAccess();
 
-		if ($this->input->get('view') === 'items'
-			&& $this->input->get('layout') === 'modal'
-			&& !$this->app->getIdentity()->authorise('core.create', 'com_menus'))
+		if ($this->input->get('view') !== 'items'
+			|| $this->input->get('layout') !== 'modal'
+			|| !$this->app->getIdentity()->authorise('core.create', 'com_menus'))
 		{
 			throw new NotAllowed;
 		}
