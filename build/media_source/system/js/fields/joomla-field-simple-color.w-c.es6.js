ad3821327ac847d6cc105bccffa301dafc925e10
Dimitris Grammatikogiannis <d.grammatiko@gmail.com>
2021-02-12 12:51:45 +0100

    Update joomla-field-simple-color.w-c.es6.js

--- build/media_source/system/js/fields/joomla-field-simple-color.w-c.es6.js
+++ build/media_source/system/js/fields/joomla-field-simple-color.w-c.es6.js
@@ -365,6 +365,8 @@
       this.icon.setAttribute('class', clss);
       this.icon.style.backgroundColor = bgcolor;
 
+      // trigger change event both on the select and on the custom element
+      this.select.dispatchEvent(new Event('change'));
       this.dispatchEvent(new CustomEvent('change', {
         detail: { value: color },
         bubbles: true,
