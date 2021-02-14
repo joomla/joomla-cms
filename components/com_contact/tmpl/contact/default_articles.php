43d9c24cdf17a81ad404d9d6621ebe117a2bbbd0
Viviana Menzel <vivianamenzel@dr-menzel-it.de>
2021-02-14 16:20:22 +0100

    Add BS5 classes to articles in contact

--- components/com_contact/tmpl/contact/default_articles.php
+++ components/com_contact/tmpl/contact/default_articles.php
@@ -16,10 +16,10 @@ use Joomla\Component\Content\Site\Helper\RouteHelper;
 ?>
 <?php if ($this->params->get('show_articles')) : ?>
 <div class="com-contact__articles contact-articles">
-	<ul class="nav nav-tabs nav-stacked">
+	<ul class="nav flex-column">
 		<?php foreach ($this->item->articles as $article) : ?>
-			<li>
-				<?php echo HTMLHelper::_('link', Route::_(RouteHelper::getArticleRoute($article->slug, $article->catid, $article->language)), htmlspecialchars($article->title, ENT_COMPAT, 'UTF-8')); ?>
+			<li class="nav-item">
+				<?php echo HTMLHelper::_('link', Route::_(RouteHelper::getArticleRoute($article->slug, $article->catid, $article->language)), htmlspecialchars($article->title, ENT_COMPAT, 'UTF-8'), array('class' => 'nav-link')); ?>
 			</li>
 		<?php endforeach; ?>
 	</ul>
