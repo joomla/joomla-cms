740574128a0f9f255c6354cbc4fcc884ade62d27
ciar4n <ciaran@joomla51.com>
2021-02-21 23:03:30 +0000

    Revert "revert display attribute"
    This reverts commit b1e13e15d61e234d2b1c2d88c31d87ac7c589cae.

--- libraries/src/Form/Field/CalendarField.php
+++ libraries/src/Form/Field/CalendarField.php
@@ -82,6 +82,13 @@ class CalendarField extends FormField
 	protected $layout = 'joomla.form.field.calendar';
 
 	/**
+	 * The display of the field
+	 *
+	 * @var  string
+	 */
+	protected $display;
+
+	/**
 	 * Method to get certain otherwise inaccessible properties from the form field object.
 	 *
 	 * @param   string  $name  The property name for which to get the value.
