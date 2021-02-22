f8e785532119e1da01c4d96ceea951a04532abcf
ciar4n <ciaran@joomla51.com>
2021-02-22 18:39:05 +0000

    inc component and login

--- administrator/templates/atum/component.php
+++ administrator/templates/atum/component.php
@@ -18,7 +18,16 @@ $wa = $this->getWebAssetManager();
 // Enable assets
 $wa->useStyle('template.atum.' . ($this->direction === 'rtl' ? 'rtl' : 'ltr'))
 	->useStyle('template.active.language')
-	->useStyle('template.user');
+	->useStyle('template.user')
+	->addInlineStyle(':root {
+		--hue: ' . $this->params->get('hue', '214') . ';
+		--atum-bg-light: ' . $this->params->get('bg-light', '--atum-bg-light') . ';
+		--atum-text-dark: ' . $this->params->get('text-dark', '--atum-text-dark') . ';
+		--atum-text-light: ' . $this->params->get('text-light', '--atum-text-light') . ';
+		--atum-link-color: ' . $this->params->get('link-color', '--atum-link-color') . ';
+		--atum-special-color: ' . $this->params->get('special-color', '--atum-special-color') . ';
+		--atum-sidebar-link-color: ' . $this->params->get('sidebar-link-color', '--atum-sidebar-link-color') . ';
+	}');
 
 // Override 'template.active' asset to set correct ltr/rtl dependency
 $wa->registerStyle('template.active', '', [], [], ['template.atum.' . ($this->direction === 'rtl' ? 'rtl' : 'ltr')]);
