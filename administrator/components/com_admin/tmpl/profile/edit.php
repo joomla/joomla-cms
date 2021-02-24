523ab3cbb3ee2db7aea45e4e50e7b644eb47cda8
dgrammatiko <d.grammatiko@gmail.com>
2021-02-24 23:55:06 +0100

    assets3

--- administrator/components/com_admin/tmpl/profile/edit.php
+++ administrator/components/com_admin/tmpl/profile/edit.php
@@ -18,7 +18,8 @@ use Joomla\Component\Users\Administrator\Helper\UsersHelper;
 
 HTMLHelper::_('behavior.formvalidator');
 HTMLHelper::_('behavior.keepalive');
-HTMLHelper::_('script', 'com_users/two-factor-switcher.js', ['version' => 'auto', 'relative' => true], ['defer' => true]);
+HTMLHelper::_('script', 'com_users/two-factor-switcher.js', ['version' => 'auto', 'relative' => true], ['type' => 'module']);
+HTMLHelper::_('script', 'com_users/two-factor-switcher-es5.js', ['version' => 'auto', 'relative' => true], ['defer' => true, 'nomodule' => true]);
 
 $input = Factory::getApplication()->input;
 
