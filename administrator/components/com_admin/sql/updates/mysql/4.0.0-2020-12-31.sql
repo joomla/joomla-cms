4ddda3b722421f2c85ebcd92fecab5de00702872
chmst <dev@chmst.de>
2021-02-28 10:37:34 +0100

    updates as suggested.

--- administrator/components/com_admin/sql/updates/mysql/4.0.0-2020-12-31.sql
+++ administrator/components/com_admin/sql/updates/mysql/4.0.0-2020-12-31.sql
@@ -1,3 +1,4 @@
 DELETE FROM `#__postinstall_messages` WHERE `title_key` = 'TPL_HATHOR_MESSAGE_POSTINSTALL_TITLE';
 DELETE FROM `#__postinstall_messages` WHERE `title_key` = 'PLG_PLG_RECAPTCHA_VERSION_1_POSTINSTALL_TITLE';
 DELETE FROM `#__postinstall_messages` WHERE `title_key` = 'COM_CPANEL_MSG_JOOMLA40_PRE_CHECKS_TITLE';
+DELETE FROM `#__postinstall_messages` WHERE `title_key` = 'COM_CPANEL_MSG_EACCELERATOR_TITLE';
