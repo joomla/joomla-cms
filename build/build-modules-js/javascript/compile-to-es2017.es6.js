aabb94b8d288390367d2c6c6085547de9a0c7863
Dimitris Grammatikogiannis <d.grammatiko@gmail.com>
2021-03-14 12:36:23 +0100

    Use node v12 compatible notation

--- build/build-modules-js/javascript/compile-to-es2017.es6.js
+++ build/build-modules-js/javascript/compile-to-es2017.es6.js
@@ -1,4 +1,4 @@
-const { access } = require('fs/promises');
+const { access } = require('fs').promises;
 const { constants } = require('fs');
 const Autoprefixer = require('autoprefixer');
 const CssNano = require('cssnano');
