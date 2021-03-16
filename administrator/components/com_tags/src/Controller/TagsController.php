8e5628766bc97e79199cf712ea66e606f815e729
Bongovo <56739773+Bongovo@users.noreply.github.com>
2021-03-16 22:58:14 +0100

    Update TagsController.php

--- administrator/components/com_tags/src/Controller/TagsController.php
+++ administrator/components/com_tags/src/Controller/TagsController.php
@@ -74,7 +74,7 @@ class TagsController extends AdminController
 	/**
 	 * Method to get the number of published tags for quickicons
 	 *
-	 * @return  string  The JSON-encoded amount of published tags
+	 * @return  string  The JSON-encoded amount of published articles
 	 *
 	 * @since   __DEPLOY_VERSION__
 	 */
