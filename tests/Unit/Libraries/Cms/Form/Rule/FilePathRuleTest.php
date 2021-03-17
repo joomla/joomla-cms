0e2b4410332ce26bb11d7a986edb7553513f3939
Phil E. Taylor <phil@phil-taylor.com>
2021-03-17 19:22:04 +0000

    add more test cases
    Signed-off-by: Phil E. Taylor <phil@phil-taylor.com>

--- tests/Unit/Libraries/Cms/Form/Rule/FilePathRuleTest.php
+++ tests/Unit/Libraries/Cms/Form/Rule/FilePathRuleTest.php
@@ -50,6 +50,11 @@ class FilePathRuleTest extends UnitTestCase
 			[true,	$xml, '\\images'], // Means \images
 			[true,	$xml, 'ftp://images'],
 			[true,	$xml, 'http://images'],
+			[true,	$xml, '/media'],
+			[true,	$xml, '/administrator'],
+			[true,	$xml, '/4711images'],
+			[true,	$xml, '/4711i/images'],
+			[false,	$xml, '../4711i/images'],
 		];
 	}
 
