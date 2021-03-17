55c9d6ab88ac3cf6a4a9f2504588ec19adc6d5ec
Phil E. Taylor <phil@phil-taylor.com>
2021-03-17 20:34:54 +0000

    more test cases
    Signed-off-by: Phil E. Taylor <phil@phil-taylor.com>

--- tests/Unit/Libraries/Cms/Form/Rule/FilePathRuleTest.php
+++ tests/Unit/Libraries/Cms/Form/Rule/FilePathRuleTest.php
@@ -41,20 +41,28 @@ class FilePathRuleTest extends UnitTestCase
 		/>');
 
 		return [
-			[true,	$xml, '.images'],
-			[true,	$xml, './images'],
-			[true,	$xml, '.\images'],
-			[false,	$xml, '../images'],
-			[false,	$xml, '.../images'],
-			[true,	$xml, 'c:\images'],
-			[true,	$xml, '\\images'], // Means \images
-			[true,	$xml, 'ftp://images'],
-			[true,	$xml, 'http://images'],
-			[true,	$xml, '/media'],
-			[true,	$xml, '/administrator'],
-			[true,	$xml, '/4711images'],
-			[true,	$xml, '/4711i/images'],
-			[false,	$xml, '../4711i/images'],
+			[false, $xml, '.images'],
+			[false, $xml, './images'],
+			[false, $xml, '.\images'],
+			[false, $xml, '../images'],
+			[false, $xml, '.../images'],
+			[true, $xml, 'c:\images'],
+			[false, $xml, '\\images'], // Means \images
+			[true, $xml, 'ftp://images'],
+			[true, $xml, 'http://images'],
+			[false, $xml, '/media'],
+			[false, $xml, '/administrator'],
+			[false, $xml, '/4711images'],
+			[false, $xml, '4711images'],
+			[false, $xml, '1'],
+			[false, $xml, '_'],
+			[false, $xml, '*'],
+			[false, $xml, '%'],
+			[false, $xml, '://foo'],
+			[false, $xml, '/4711i/images'],
+			[false, $xml, '../4711i/images'],
+			[false, $xml, 'Εικόνες'],
+			[false, $xml, 'Изображений'],
 		];
 	}
 
