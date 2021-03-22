e9d8718b06ed438d1f736a427a1f2081fb469e2f
Tuan Pham Ngoc <github@joomdonation.com>
2021-03-22 14:37:45 +0700

    Fix warning on PHP 8 (#32768)

--- components/com_content/src/View/Category/HtmlView.php
+++ components/com_content/src/View/Category/HtmlView.php
@@ -240,7 +240,7 @@ class HtmlView extends CategoryView
 		$path     = [['title' => $this->category->title, 'link' => '']];
 		$category = $this->category->getParent();
 
-		while ($id != $category->id && $category->id > 1)
+		while ($category !== null && $category->id !== 'root' && $category->id != $id)
 		{
 			$path[]   = ['title' => $category->title, 'link' => RouteHelper::getCategoryRoute($category->id, $category->language)];
 			$category = $category->getParent();
