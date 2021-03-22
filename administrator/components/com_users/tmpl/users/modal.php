abaedb500b9c191a819f6056a524bd667c85366d
NikitaEmberi <59737567+NikitaEmberi@users.noreply.github.com>
2021-03-23 01:36:32 +0530

    Update modal.php

--- administrator/components/com_users/tmpl/users/modal.php
+++ administrator/components/com_users/tmpl/users/modal.php
@@ -89,7 +89,7 @@ $onClick         = "window.parent.jSelectUser(this);window.parent.Joomla.Modal.g
 							</span>
 						</td>
 						<td class="text-center">
-						    	<span class="tbody-icon">
+						 	<span class="tbody-icon">
 								<span class="<?php echo $activatedStates[(empty($item->activation) ? 0 : 1)]; ?>"></span>
 							</span>
 						</td>
