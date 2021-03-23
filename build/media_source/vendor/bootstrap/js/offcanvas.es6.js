eabc4d842cf681db55df7b83e268a29446f24a16
dgrammatiko <d.grammatiko@gmail.com>
2021-03-23 22:54:25 +0100

    meh

--- build/media_source/vendor/bootstrap/js/offcanvas.es6.js
+++ build/media_source/vendor/bootstrap/js/offcanvas.es6.js
@@ -5,13 +5,22 @@ window.bootstrap.Offcanvas = Offcanvas;
 
 if (Joomla && Joomla.getOptions) {
   // Get the elements/configurations from the PHP
-  const offcanvases = Joomla.getOptions('bootstrap.alert');
+  const offcanvases = Joomla.getOptions('bootstrap.offcanvas');
   // Initialise the elements
-  if (offcanvases && offcanvases.length) {
-    offcanvases.forEach((selector) => {
-      Array.from(document.querySelectorAll(selector))
-        .map((el) => new window.bootstrap.Offcanvas(el));
-    });
+  if (typeof offcanvases === 'object' && offcanvases !== null) {
+    Object.keys(offcanvases)
+      .forEach((offcanvas) => {
+        const opt = offcanvases[offcanvas];
+        const options = {
+          interval: opt.interval ? opt.interval : 5000,
+          pause: opt.pause ? opt.pause : 'hover',
+        };
+
+        const elements = Array.from(document.querySelectorAll(offcanvas));
+        if (elements.length) {
+          elements.map((el) => new window.bootstrap.Offcanvas(el, options));
+        }
+      });
   }
 }
 
