2b36f1d7e9830a6c63b23703a8b267d24b275038
Saumya Sarkar <saumyasarkar27@gmail.com>
2021-03-23 16:38:46 +0530

    Addition of classes in tags
    Addition of classes in tags as per Bootstrap 5 and removal of internal styling in label

--- components/com_users/tmpl/login/default_login.php
+++ components/com_users/tmpl/login/default_login.php
@@ -61,11 +61,13 @@ $usersConfig = ComponentHelper::getParams('com_users');
 			<?php if (PluginHelper::isEnabled('system', 'remember')) : ?>
 				<div  class="com-users-login__remember control-group">
 					
-					<input id="remember" type="checkbox" name="remember" class="inputbox" value="yes">
-					
-					<label for="remember" style="margin-left:3px;">
-							<?php echo Text::_('COM_USERS_LOGIN_REMEMBER_ME'); ?>
-					</label>									
+					<div class="form-check">
+ 						 <input class="form-check-input" id="remember" type="checkbox" name="remember" value="yes">
+  						 <label class="form-check-label" for="remember">
+    							<?php echo Text::_('COM_USERS_LOGIN_REMEMBER_ME'); ?>
+ 						 </label>
+					</div>				
+														
 				</div>
 			<?php endif; ?>
 
