c9e3e5f055493839a3ab71c66f296686051dd56f
Phil Taylor <phil@phil-taylor.com>
2021-03-24 22:11:00 +0000

    Update DateTest.php

--- tests/Unit/Libraries/Cms/Date/DateTest.php
+++ tests/Unit/Libraries/Cms/Date/DateTest.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package     Joomla.UnitTest
- * @subpackage  Application
+ * @subpackage  Date
  *
  * @copyright   (C) 2019 Open Source Matters, Inc. <https://www.joomla.org>
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
