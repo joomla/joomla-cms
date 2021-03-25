96e9ae9bafa601ef962aa0cd51b7651cadc90d7f
Phil E. Taylor <phil@phil-taylor.com>
2021-03-25 20:15:04 +0000

    revert
    Signed-off-by: Phil E. Taylor <phil@phil-taylor.com>

--- administrator/components/com_config/model/application.php
+++ administrator/components/com_config/model/application.php
@@ -148,9 +148,7 @@ class ConfigModelApplication extends ConfigModelForm
 		}
 
 		// Check if we can set the Force SSL option
-		if ($data['behind_loadbalancer'] != 1
-			&& (int) $data['force_ssl'] !== 0
-			&& (int) $data['force_ssl'] !== (int) JFactory::getConfig()->get('force_ssl', '0'))
+		if ((int) $data['force_ssl'] !== 0 && (int) $data['force_ssl'] !== (int) JFactory::getConfig()->get('force_ssl', '0'))
 		{
 			try
 			{
