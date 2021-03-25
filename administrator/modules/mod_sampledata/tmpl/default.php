de05dddcc8d11f1f7580841bd78bc84a5757bb55
Shubham-com <66332826+Shubham-com@users.noreply.github.com>
2021-03-25 17:34:52 +0530

    Adding an attribute aria-valuemin and aria-valuemax in Progress bar

--- administrator/modules/mod_sampledata/tmpl/default.php
+++ administrator/modules/mod_sampledata/tmpl/default.php
@@ -45,7 +45,7 @@ $app->getDocument()->addScriptOptions(
 			<?php // Progress bar ?>
 			<li class="list-group-item sampledata-progress-<?php echo $item->name; ?> d-none">
 				<div class="progress">
-					<div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar"></div>
+					<div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width:10%"></div>
 				</div>
 			</li>
 			<?php // Progress messages ?>
