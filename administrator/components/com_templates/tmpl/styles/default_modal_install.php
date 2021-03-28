b52d7d9876f52a54cd7026ede3742b97d1d41806
dgrammatiko <d.grammatiko@gmail.com>
2021-03-28 19:18:43 +0200

    comments

--- administrator/components/com_templates/tmpl/styles/default_modal_install.php
+++ administrator/components/com_templates/tmpl/styles/default_modal_install.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * @package     Joomla.Plugin
- * @subpackage  Installer.packageinstaller
+ * @package     Joomla.Administrator
+ * @subpackage  com_templates
  *
- * @copyright   Copyright (C) 2005 - 2019 Open Source Matters, Inc. All rights reserved.
+ * @copyright   (C) 2021 Open Source Matters, Inc. <https://www.joomla.org>
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
