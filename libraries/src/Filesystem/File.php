e33b560aadf5e378f689802d14285c4d241a0100
Phil E. Taylor <phil@phil-taylor.com>
2021-04-03 14:44:04 +0100

    Invalidate opcache before deleting file
    Signed-off-by: Phil E. Taylor <phil@phil-taylor.com>

--- libraries/src/Filesystem/File.php
+++ libraries/src/Filesystem/File.php
@@ -254,12 +254,23 @@ class File
 				continue;
 			}
 
-			// Try making the file writable first. If it's read-only, it can't be deleted
-			// on Windows, even if the parent folder is writable
+			/**
+			 * Try making the file writable first. If it's read-only, it can't be deleted
+			 * on Windows, even if the parent folder is writable
+			 */
 			@chmod($file, 0777);
 
-			// In case of restricted permissions we zap it one way or the other
-			// as long as the owner is either the webserver or the ftp
+			/**
+			 * Invalidate the OPCache for the file before actually deleting it
+			 * @see https://github.com/joomla/joomla-cms/pull/32915#issuecomment-812865635
+			 * @see https://www.php.net/manual/en/function.opcache-invalidate.php#116372
+			 */
+			self::invalidateFileCache($file);
+
+			/**
+			 * In case of restricted permissions we delete it one way or the other
+			 * as long as the owner is either the webserver or the ftp
+			 */
 			if (@unlink($file))
 			{
 				// Do nothing
@@ -282,8 +293,6 @@ class File
 
 				return false;
 			}
-
-			self::invalidateFileCache($file);
 		}
 
 		return true;
