a2df403be32690ce97a8ce556e8a62260a5d3da2
Jean-Marie Simonet <infografjms@gmail.com>
2021-04-04 10:59:21 +0200

    [4.0] Articles frontend associations badges changed to buttons and more

--- components/com_content/tmpl/category/default_articles.php
+++ components/com_content/tmpl/category/default_articles.php
@@ -186,10 +186,12 @@ $currentDate = Factory::getDate()->format('Y-m-d H:i:s');
 							<?php foreach ($associations as $association) : ?>
 								<?php if ($this->params->get('flags', 1) && $association['language']->image) : ?>
 									<?php $flag = HTMLHelper::_('image', 'mod_languages/' . $association['language']->image . '.gif', $association['language']->title_native, array('title' => $association['language']->title_native), true); ?>
-									&nbsp;<a href="<?php echo Route::_($association['item']); ?>"><?php echo $flag; ?></a>&nbsp;
+									<a href="<?php echo Route::_($association['item']); ?>"><?php echo $flag; ?></a>
 								<?php else : ?>
-									<?php $class = 'badge bg-secondary badge-' . $association['language']->sef; ?>
-									&nbsp;<a class="<?php echo $class; ?>" title="<?php echo $association['language']->title_native; ?>" href="<?php echo Route::_($association['item']); ?>"><?php echo strtoupper($association['language']->sef); ?></a>&nbsp;
+									<?php $class = 'btn btn-badge btn-vsm btn-' . strtolower($association['language']->lang_code); ?>
+									<a class="<?php echo $class; ?>" title="<?php echo $association['language']->title_native; ?>" href="<?php echo Route::_($association['item']); ?>"><?php echo $association['language']->lang_code; ?>
+										<span class="sr-only"><?php echo $association['language']->title_native; ?></span>
+									</a>
 								<?php endif; ?>
 							<?php endforeach; ?>
 							</div>
@@ -210,10 +212,12 @@ $currentDate = Factory::getDate()->format('Y-m-d H:i:s');
 							<?php foreach ($associations as $association) : ?>
 								<?php if ($this->params->get('flags', 1)) : ?>
 									<?php $flag = HTMLHelper::_('image', 'mod_languages/' . $association['language']->image . '.gif', $association['language']->title_native, array('title' => $association['language']->title_native), true); ?>
-									&nbsp;<a href="<?php echo Route::_($association['item']); ?>"><?php echo $flag; ?></a>&nbsp;
+									<a href="<?php echo Route::_($association['item']); ?>"><?php echo $flag; ?></a>
 								<?php else : ?>
-									<?php $class = 'badge bg-secondary badge-' . $association['language']->sef; ?>
-									&nbsp;<a class="<?php echo $class; ?>" title="<?php echo $association['language']->title_native; ?>" href="<?php echo Route::_($association['item']); ?>"><?php echo strtoupper($association['language']->sef); ?></a>&nbsp;
+									<?php $class = 'btn btn-badge btn-vsm btn-' . strtolower($association['language']->lang_code); ?>
+									<a class="<?php echo $class; ?>" title="<?php echo $association['language']->title_native; ?>" href="<?php echo Route::_($association['item']); ?>"><?php echo $association['language']->lang_code; ?>
+										<span class="sr-only"><?php echo $association['language']->title_native; ?></span>
+									</a>
 								<?php endif; ?>
 							<?php endforeach; ?>
 							</div>
