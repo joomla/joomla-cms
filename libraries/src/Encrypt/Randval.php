f07554464e56a5dff5c1273f2ee6815d69101aaa
Richard Fath <richard67@users.noreply.github.com>
2021-04-05 22:01:04 +0200

    Fix conflict from upmerge

--- libraries/src/Encrypt/Randval.php
+++ libraries/src/Encrypt/Randval.php
@@ -1,17 +1,9 @@
 <?php
 /**
-<<<<<<< HEAD:libraries/src/Encrypt/Randval.php
  * Joomla! Content Management System
  *
  * @copyright  (C) 2016 Open Source Matters, Inc. <https://www.joomla.org>
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
-=======
- * @package     FrameworkOnFramework
- * @subpackage  utils
- * @copyright   Copyright (C) 2010-2016 Nicholas K. Dionysopoulos / Akeeba Ltd. All rights reserved.
- * @license     GNU General Public License version 2 or later; see LICENSE.txt
- * @note        This file has been modified by the Joomla! Project and no longer reflects the original work of its author.
->>>>>>> d17dde8:libraries/fof/encrypt/randval.php
  */
 
 namespace Joomla\CMS\Encrypt;
