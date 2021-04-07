712de05935752a2a13eb2cec5e42eb54f3e86731
dgrammatiko <d.grammatiko@gmail.com>
2021-04-07 19:27:47 +0200

    x

--- administrator/components/com_templates/tmpl/templates/default.php
+++ administrator/components/com_templates/tmpl/templates/default.php
@@ -110,3 +110,20 @@ if ($canCreate)
 	);
 }
 ?>
+<template id="modal-template">
+	<div class="modal" tabindex="-1">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h5 class="modal-title"></h5>
+					<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="<?php echo Text::_('JCLOSE'); ?>"></button>
+				</div>
+				<div class="modal-body"></div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-secondary" data-bs-dismiss="modal"><?php echo Text::_('JCLOSE'); ?></button>
+					<button type="button" class="btn btn-primary"></button>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
