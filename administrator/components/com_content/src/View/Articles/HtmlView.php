4f8ae457e6d891c257519bb5ce410c01ed94b67d
dgrammatiko <d.grammatiko@gmail.com>
2021-04-09 13:06:39 +0200

    why?

--- administrator/components/com_content/src/View/Articles/HtmlView.php
+++ administrator/components/com_content/src/View/Articles/HtmlView.php
@@ -104,7 +104,7 @@ class HtmlView extends BaseHtmlView
 		// We don't need toolbar in the modal window.
 		if ($this->getLayout() !== 'modal')
 		{
-			$this->addToolbar();
+//			$this->addToolbar();
 
 			// We do not need to filter by language when multilingual is disabled
 			if (!Multilanguage::isEnabled())
@@ -132,115 +132,5 @@ class HtmlView extends BaseHtmlView
 		}
 
 		return parent::display($tpl);
-	}
-
-	/**
-	 * Add the page title and toolbar.
-	 *
-	 * @return  void
-	 *
-	 * @since   1.6
-	 */
-	protected function addToolbar()
-	{
-		$canDo = ContentHelper::getActions('com_content', 'category', $this->state->get('filter.category_id'));
-		$user  = Factory::getUser();
-
-		// Get the toolbar object instance
-		$toolbar = Toolbar::getInstance('toolbar');
-
-		ToolbarHelper::title(Text::_('COM_CONTENT_ARTICLES_TITLE'), 'copy article');
-
-		if ($canDo->get('core.create') || count($user->getAuthorisedCategories('com_content', 'core.create')) > 0)
-		{
-			$toolbar->addNew('article.add');
-		}
-
-		if ($canDo->get('core.edit.state') || count($this->transitions))
-		{
-			$dropdown = $toolbar->dropdownButton('status-group')
-				->text('JTOOLBAR_CHANGE_STATUS')
-				->toggleSplit(false)
-				->icon('icon-ellipsis-h')
-				->buttonClass('btn btn-action')
-				->listCheck(true);
-
-			$childBar = $dropdown->getChildToolbar();
-
-			if (count($this->transitions))
-			{
-				$childBar->separatorButton('transition-headline')
-					->text('COM_CONTENT_RUN_TRANSITIONS')
-					->buttonClass('text-center py-2 h3');
-
-				$cmd = "Joomla.submitbutton('articles.runTransition');";
-				$messages = "{error: [Joomla.JText._('JLIB_HTML_PLEASE_MAKE_A_SELECTION_FROM_THE_LIST')]}";
-				$alert = 'Joomla.renderMessages(' . $messages . ')';
-				$cmd   = 'if (document.adminForm.boxchecked.value == 0) { ' . $alert . ' } else { ' . $cmd . ' }';
-
-				foreach ($this->transitions as $transition)
-				{
-					$childBar->standardButton('transition')
-						->text($transition['text'])
-						->buttonClass('transition-' . (int) $transition['value'])
-						->icon('icon-project-diagram')
-						->onclick('document.adminForm.transition_id.value=' . (int) $transition['value'] . ';' . $cmd);
-				}
-
-				$childBar->separatorButton('transition-separator');
-			}
-
-			if ($canDo->get('core.edit.state'))
-			{
-				$childBar->publish('articles.publish')->listCheck(true);
-
-				$childBar->unpublish('articles.unpublish')->listCheck(true);
-
-				$childBar->standardButton('featured')
-					->text('JFEATURE')
-					->task('articles.featured')
-					->listCheck(true);
-
-				$childBar->standardButton('unfeatured')
-					->text('JUNFEATURE')
-					->task('articles.unfeatured')
-					->listCheck(true);
-
-				$childBar->archive('articles.archive')->listCheck(true);
-
-				$childBar->checkin('articles.checkin')->listCheck(true);
-
-				if ($this->state->get('filter.published') != ContentComponent::CONDITION_TRASHED)
-				{
-					$childBar->trash('articles.trash')->listCheck(true);
-				}
-			}
-
-			// Add a batch button
-			if ($user->authorise('core.create', 'com_content')
-				&& $user->authorise('core.edit', 'com_content')
-				&& $user->authorise('core.execute.transition', 'com_content'))
-			{
-				$childBar->popupButton('batch')
-					->text('JTOOLBAR_BATCH')
-					->selector('collapseModal')
-					->listCheck(true);
-			}
-		}
-
-		if ($this->state->get('filter.published') == ContentComponent::CONDITION_TRASHED && $canDo->get('core.delete'))
-		{
-			$toolbar->delete('articles.delete')
-				->text('JTOOLBAR_EMPTY_TRASH')
-				->message('JGLOBAL_CONFIRM_DELETE')
-				->listCheck(true);
-		}
-
-		if ($user->authorise('core.admin', 'com_content') || $user->authorise('core.options', 'com_content'))
-		{
-			$toolbar->preferences('com_content');
-		}
-
-		$toolbar->help('JHELP_CONTENT_ARTICLE_MANAGER');
 	}
 }
--- administrator/components/com_content/tmpl/articles/default.php
+++ administrator/components/com_content/tmpl/articles/default.php
@@ -21,6 +21,9 @@ use Joomla\CMS\Language\Text;
 use Joomla\CMS\Layout\LayoutHelper;
 use Joomla\CMS\Router\Route;
 use Joomla\CMS\Session\Session;
+use Joomla\CMS\Toolbar\Toolbar;
+use Joomla\CMS\Toolbar\ToolbarHelper;
+use Joomla\Component\Content\Administrator\Extension\ContentComponent;
 use Joomla\Component\Content\Administrator\Helper\ContentHelper;
 use Joomla\Utilities\ArrayHelper;
 
@@ -93,6 +96,109 @@ $workflow_featured = Factory::getApplication()->bootComponent('com_content')->is
 endif;
 
 $assoc = Associations::isEnabled();
+
+// We don't need toolbar in the modal window.
+if ($this->getLayout() !== 'modal') {
+	$canDo = ContentHelper::getActions('com_content', 'category', $this->state->get('filter.category_id'));
+	$user  = Factory::getUser();
+
+	// Get the toolbar object instance
+	$toolbar = Toolbar::getInstance('toolbar');
+
+	ToolbarHelper::title(Text::_('COM_CONTENT_ARTICLES_TITLE'), 'copy article');
+
+	if ($canDo->get('core.create') || count($user->getAuthorisedCategories('com_content', 'core.create')) > 0)
+	{
+		$toolbar->addNew('article.add');
+	}
+
+	if ($canDo->get('core.edit.state') || count($this->transitions))
+	{
+		$dropdown = $toolbar->dropdownButton('status-group')
+				->text('JTOOLBAR_CHANGE_STATUS')
+				->toggleSplit(false)
+				->icon('icon-ellipsis-h')
+				->buttonClass('btn btn-action')
+				->listCheck(true);
+
+		$childBar = $dropdown->getChildToolbar();
+
+		if (count($this->transitions))
+		{
+			$childBar->separatorButton('transition-headline')
+					->text('COM_CONTENT_RUN_TRANSITIONS')
+					->buttonClass('text-center py-2 h3');
+
+			$cmd = "Joomla.submitbutton('articles.runTransition');";
+			$messages = "{error: [Joomla.JText._('JLIB_HTML_PLEASE_MAKE_A_SELECTION_FROM_THE_LIST')]}";
+			$alert = 'Joomla.renderMessages(' . $messages . ')';
+			$cmd   = 'if (document.adminForm.boxchecked.value == 0) { ' . $alert . ' } else { ' . $cmd . ' }';
+
+			foreach ($this->transitions as $transition)
+			{
+				$childBar->standardButton('transition')
+						->text($transition['text'])
+						->buttonClass('transition-' . (int) $transition['value'])
+						->icon('icon-project-diagram')
+						->onclick('document.adminForm.transition_id.value=' . (int) $transition['value'] . ';' . $cmd);
+			}
+
+			$childBar->separatorButton('transition-separator');
+		}
+
+		if ($canDo->get('core.edit.state'))
+		{
+			$childBar->publish('articles.publish')->listCheck(true);
+
+			$childBar->unpublish('articles.unpublish')->listCheck(true);
+
+			$childBar->standardButton('featured')
+					->text('JFEATURE')
+					->task('articles.featured')
+					->listCheck(true);
+
+			$childBar->standardButton('unfeatured')
+					->text('JUNFEATURE')
+					->task('articles.unfeatured')
+					->listCheck(true);
+
+			$childBar->archive('articles.archive')->listCheck(true);
+
+			$childBar->checkin('articles.checkin')->listCheck(true);
+
+			if ($this->state->get('filter.published') != ContentComponent::CONDITION_TRASHED)
+			{
+				$childBar->trash('articles.trash')->listCheck(true);
+			}
+		}
+
+		// Add a batch button
+		if ($user->authorise('core.create', 'com_content')
+				&& $user->authorise('core.edit', 'com_content')
+				&& $user->authorise('core.execute.transition', 'com_content'))
+		{
+			$childBar->popupButton('batch')
+					->text('JTOOLBAR_BATCH')
+					->selector('collapseModal')
+					->listCheck(true);
+		}
+	}
+
+	if ($this->state->get('filter.published') == ContentComponent::CONDITION_TRASHED && $canDo->get('core.delete'))
+	{
+		$toolbar->delete('articles.delete')
+				->text('JTOOLBAR_EMPTY_TRASH')
+				->message('JGLOBAL_CONFIRM_DELETE')
+				->listCheck(true);
+	}
+
+	if ($user->authorise('core.admin', 'com_content') || $user->authorise('core.options', 'com_content'))
+	{
+		$toolbar->preferences('com_content');
+	}
+
+	$toolbar->help('JHELP_CONTENT_ARTICLE_MANAGER');
+}
 ?>
 
 <form action="<?php echo Route::_('index.php?option=com_content&view=articles'); ?>" method="post" name="adminForm" id="adminForm">
