5ec75a173954190dd93b6763bd7ffbb15a37a915
Richard Fath <richard67@users.noreply.github.com>
2021-04-16 11:43:59 +0200

    PHP CS

--- libraries/src/Console/CheckUpdatesCommand.php
+++ libraries/src/Console/CheckUpdatesCommand.php
@@ -56,7 +56,6 @@ class CheckUpdatesCommand extends AbstractCommand
 
 		// Find all updates
 		$ret = Updater::getInstance()->findUpdates(0, $cache_timeout);
-		
 		if ($ret)
 		{
 			$symfonyStyle->note('There are available updates to apply');
