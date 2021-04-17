91c4cac07135a22eb0bbff43065d8ba5a7a2f128
Thomas Hunziker <werbemails@bakual.ch>
2021-04-17 10:24:48 +0200

    Adding some margins to the button

--- administrator/modules/mod_sampledata/tmpl/default.php
+++ administrator/modules/mod_sampledata/tmpl/default.php
@@ -54,7 +54,7 @@ $app->getDocument()->addScriptOptions(
 			</li>
 		<?php endforeach; ?>
 	</ul>
-	<a href="index.php?option=com_plugins&filter[folder]=sampledata" class="btn btn-secondary btn-sm manage-sample-data float-end">
+	<a href="index.php?option=com_plugins&filter[folder]=sampledata" class="btn btn-secondary btn-sm manage-sample-data float-end me-3 mb-3">
 		<span class="icon-tasks" aria-hidden="true"></span> <?php echo Text::_('MOD_SAMPLEDATA_MANAGE_SAMPLEDATA'); ?>
 		<span class="visually-hidden"><?php echo Text::_('MOD_SAMPLEDATA_MANAGE_SAMPLEDATA'); ?></span>
 	</a>
