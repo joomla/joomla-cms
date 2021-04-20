870b5cc6b10e7f6e38faf5fc78f286ac99c6d3ce
dgrammatiko <d.grammatiko@gmail.com>
2021-04-20 13:45:24 +0200

    com_redirect

--- administrator/components/com_redirect/tmpl/links/default_batch_body.php
+++ administrator/components/com_redirect/tmpl/links/default_batch_body.php
@@ -17,7 +17,7 @@ $separator = $params->get('separator', '|');
 
 <div class="container">
 	<div class="row">
-		<div class="control-group col-md-12">
+		<div class="control-group col-md-12 mt-3">
 			<label for="batch_urls" class="form-label"><?php echo Text::sprintf('COM_REDIRECT_BATCH_TIP', $separator); ?></label>
 			<div class="controls">
 				<textarea class="form-control" rows="10" value="" id="batch_urls" name="batch_urls"></textarea>
