9421383cb80df827e4f777b1bb2ede8b7662f03d
Tuan Pham Ngoc <github@joomdonation.com>
2021-04-22 23:10:21 +0700

    Show mail templates from enabled extensions only

---
+++ administrator/components/com_admin/sql/updates/postgresql/4.0.0-2021-04-22.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "#__extensions" ADD COLUMN "extension" VARCHAR(127) NOT NULL DEFAULT '' AFTER "template_id";
+UPDATE "#__mail_templates" SET "extension" = SUBSTRING("template_id", 1, POSITION('.' IN "template_id") - 1);
