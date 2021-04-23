711d7ca27c5e52959f24562d96305c010f850cf7
Richard Fath <richard67@users.noreply.github.com>
2021-04-23 19:53:25 +0200

    PHP CS

--- administrator/components/com_fields/src/Model/FieldModel.php
+++ administrator/components/com_fields/src/Model/FieldModel.php
@@ -586,7 +586,6 @@ class FieldModel extends AdminModel
 				// Field type could not be changed, so remove showon attribute to avoid js errors
 				$form->setFieldAttribute('only_use_in_subform', 'showon', '');
 			}
-		}
 
 		return $form;
 	}
