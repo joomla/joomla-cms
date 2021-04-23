e9cbb4319345e412e4cc2e343f44568f37c42aa1
Tuan Pham Ngoc <github@joomdonation.com>
2021-04-23 13:59:05 +0700

    Update administrator/components/com_admin/sql/updates/postgresql/4.0.0-2021-04-22.sql
    Co-authored-by: Richard Fath <richard67@users.noreply.github.com>

--- administrator/components/com_admin/sql/updates/postgresql/4.0.0-2021-04-22.sql
+++ administrator/components/com_admin/sql/updates/postgresql/4.0.0-2021-04-22.sql
@@ -1,2 +1,2 @@
-ALTER TABLE "#__extensions" ADD COLUMN "extension" VARCHAR(127) NOT NULL DEFAULT '';
+ALTER TABLE "#__mail_templates" ADD COLUMN "extension" VARCHAR(127) NOT NULL DEFAULT '';
 UPDATE "#__mail_templates" SET "extension" = SUBSTRING("template_id", 1, POSITION('.' IN "template_id") - 1);
