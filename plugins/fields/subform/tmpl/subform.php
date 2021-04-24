ed0105528786b279d023edc7f0b05db41a3e8441
Tuan Pham Ngoc <github@joomdonation.com>
2021-04-24 16:22:46 +0700

    [4.0] Subform Cleanup (#33262)

--- plugins/fields/subform/tmpl/subform.php
+++ plugins/fields/subform/tmpl/subform.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package     Joomla.Plugin
- * @subpackage  Fields.Subfields
+ * @subpackage  Fields.Subform
  *
  * @copyright   (C) 2019 Open Source Matters, Inc. <https://www.joomla.org>
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
