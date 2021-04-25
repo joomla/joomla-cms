0be39c6f18537ccdbbc3b2dd33631fa6fa1e7813
Phil E. Taylor <phil@phil-taylor.com>
2021-04-25 11:10:14 +0100

    [4] Correct link to private messages from module (#33285)

--- administrator/modules/mod_messages/tmpl/default.php
+++ administrator/modules/mod_messages/tmpl/default.php
@@ -11,7 +11,6 @@ defined('_JEXEC') or die;
 
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\Router\Route;
-use Joomla\CMS\Uri\Uri;
 
 $hideLinks = $app->input->getBool('hidemainmenu');
 
@@ -20,8 +19,7 @@ if ($hideLinks)
 	return;
 }
 
-$uri   = Uri::getInstance();
-$route = 'index.php?option=com_messages&view=messages&id=' . $app->getIdentity()->id . '&return=' . base64_encode($uri);
+$route = 'index.php?option=com_messages&view=messages';
 ?>
 
 <div class="header-item-content">
@@ -37,5 +35,3 @@ $route = 'index.php?option=com_messages&view=messages&id=' . $app->getIdentity()
 		<?php endif; ?>
 	</a>
 </div>
-
-
