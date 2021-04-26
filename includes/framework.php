3f32088dedda62ba472e8937021227a6895d606b
Phil E. Taylor <phil@phil-taylor.com>
2021-04-26 19:57:55 +0100

    add trailing comma

--- includes/framework.php
+++ includes/framework.php
@@ -85,7 +85,7 @@ if (JDEBUG || $config->error_reporting === 'maximum')
 	$errorHandler->setExceptionHandler(
 		[
 			new \Symfony\Component\ErrorHandler\ErrorHandler(null, true),
-			'renderException'
+			'renderException',
 		]
 	);
 }
