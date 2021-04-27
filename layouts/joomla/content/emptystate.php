943230b92a6b62b4d707e66698723600f5636a94
Phil E. Taylor <phil@phil-taylor.com>
2021-04-27 20:59:29 +0100

    merge from other pr

--- layouts/joomla/content/emptystate.php
+++ layouts/joomla/content/emptystate.php
@@ -26,16 +26,18 @@ $createURL  = $displayData['createURL'] ?? '';
 $helpURL    = $displayData['helpURL'] ?? '';
 $icon       = $displayData['icon'] ?? 'icon-copy article';
 $append     = $displayData['formAppend'] ?? '';
+$title      = $displayData['title'] ?? Text::_($textPrefix . '_EMPTYSTATE_TITLE');
+$content    = $displayData['content'] ?? Text::_($textPrefix . '_EMPTYSTATE_CONTENT');
 ?>
 
 <form action="<?php echo Route::_($formURL); ?>" method="post" name="adminForm" id="adminForm">
 
 	<div class="px-4 py-5 my-5 text-center">
 		<span class="fa-8x mb-4 <?php echo $icon; ?>" aria-hidden="true"></span>
-		<h1 class="display-5 fw-bold"><?php echo Text::_($textPrefix . '_EMPTYSTATE_TITLE'); ?></h1>
+		<h1 class="display-5 fw-bold"><?php echo $title; ?></h1>
 		<div class="col-lg-6 mx-auto">
 			<p class="lead mb-4">
-				<?php echo Text::_($textPrefix . '_EMPTYSTATE_CONTENT'); ?>
+				<?php echo $content; ?>
 			</p>
 			<div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
 				<?php if ($createURL && Factory::getApplication()->input->get('tmpl') !== 'component') : ?>
