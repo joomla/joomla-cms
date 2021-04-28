72cebebba2d10d88b304ec1aed26d210b2c424ee
Quy <quy@fluxbb.org>
2021-04-28 09:31:07 -0700

    remove formURL

--- administrator/components/com_checkin/tmpl/checkin/emptystate.php
+++ administrator/components/com_checkin/tmpl/checkin/emptystate.php
@@ -14,7 +14,6 @@ use Joomla\CMS\Layout\LayoutHelper;
 
 $displayData = [
 	'textPrefix' => 'COM_CHECKIN',
-	'formURL'    => 'index.php?option=com_checkin',
 	'helpURL'    => 'https://docs.joomla.org/Special:MyLanguage/Help4.x:Maintenance:_Global_Check-in',
 	'icon'       => 'icon-check-square',
 	'title'      => Text::_('COM_CHECKIN_GLOBAL_CHECK_IN'),
