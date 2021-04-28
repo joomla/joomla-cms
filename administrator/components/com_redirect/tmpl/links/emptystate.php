dd0c2e1738af0afcaa0cbbfca398f3dd8db249f2
Phil E. Taylor <phil@phil-taylor.com>
2021-04-28 18:54:40 +0100

    Update administrator/components/com_redirect/tmpl/links/emptystate.php
    Co-authored-by: Brian Teeman <brian@teeman.net>

--- administrator/components/com_redirect/tmpl/links/emptystate.php
+++ administrator/components/com_redirect/tmpl/links/emptystate.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package     Joomla.Administrator
- * @subpackage  com_checkin
+ * @subpackage  com_redirect
  *
  * @copyright   (C) 2021 Open Source Matters, Inc. <https://www.joomla.org>
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
