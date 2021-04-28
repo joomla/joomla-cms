4c56d433c0c46b600b6dfe8c0c29714c53c9224b
Phil E. Taylor <phil@phil-taylor.com>
2021-04-28 21:20:38 +0100

    Fix calling startTabSet twice causes PHP Error
    revert to a different fix
    
    remove unneeded if statement

--- libraries/src/HTML/Helpers/Bootstrap.php
+++ libraries/src/HTML/Helpers/Bootstrap.php
@@ -809,25 +809,24 @@ HTMLSTR;
 	 */
 	public static function startTabSet($selector = 'myTab', $params = []) :string
 	{
-		$sig = md5(serialize([$selector, $params]));
-
-		if (!isset(static::$loaded[__METHOD__][$sig]))
+		if (isset(static::$loaded[__METHOD__][$selector]))
 		{
-			// Setup options object
-			$opt['active'] = (isset($params['active']) && ($params['active'])) ? (string) $params['active'] : '';
-
-			// Initialise with the Joomla specifics
-			$opt['isJoomla'] = true;
-
-			// Include the Bootstrap Tab Component
-			HTMLHelper::_('bootstrap.tab', '#' . preg_replace('/^[\.#]/', '', $selector), $opt);
-
-			// Set static array
-			static::$loaded[__METHOD__][$sig] = true;
-			static::$loaded[__METHOD__][$selector]['active'] = $opt['active'];
-
-			return LayoutHelper::render('libraries.html.bootstrap.tab.starttabset', ['selector' => $selector]);
+			return '';
 		}
+
+		// Setup options object
+		$opt['active'] = (isset($params['active']) && ($params['active'])) ? (string) $params['active'] : '';
+
+		// Initialise with the Joomla specifics
+		$opt['isJoomla'] = true;
+
+		// Include the Bootstrap Tab Component
+		HTMLHelper::_('bootstrap.tab', '#' . preg_replace('/^[\.#]/', '', $selector), $opt);
+
+		// Set static array
+		static::$loaded[__METHOD__][$selector]['active'] = $opt['active'];
+
+		return LayoutHelper::render('libraries.html.bootstrap.tab.starttabset', ['selector' => $selector]);
 	}
 
 	/**
