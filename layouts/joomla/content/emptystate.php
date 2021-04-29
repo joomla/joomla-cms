f4ce3634d0a13792bdf32c92da45f3db675a9e51
Phil E. Taylor <phil@phil-taylor.com>
2021-04-29 19:05:00 +0100

    com_installer warnings empty state

--- layouts/joomla/content/emptystate.php
+++ layouts/joomla/content/emptystate.php
@@ -23,6 +23,8 @@ if (!$textPrefix)
 $formURL    = $displayData['formURL'] ?? '';
 $createURL  = $displayData['createURL'] ?? '';
 $helpURL    = $displayData['helpURL'] ?? '';
+$title      = $displayData['title'] ?? Text::_($textPrefix . '_EMPTYSTATE_TITLE');
+$content    = $displayData['content'] ?? Text::_($textPrefix . '_EMPTYSTATE_CONTENT');
 $icon       = $displayData['icon'] ?? 'icon-copy article';
 ?>
 
@@ -30,10 +32,10 @@ $icon       = $displayData['icon'] ?? 'icon-copy article';
 
 	<div class="px-4 py-5 my-5 text-center">
 		<span class="fa-8x mb-4 <?php echo $icon; ?>" aria-hidden="true"></span>
-		<h1 class="display-5 fw-bold"><?php echo Text::_($textPrefix . '_EMPTYSTATE_TITLE'); ?></h1>
+		<h1 class="display-5 fw-bold"><?php echo $title; ?></h1>
 		<div class="col-lg-6 mx-auto">
 			<p class="lead mb-4">
-				<?php echo Text::_($textPrefix . '_EMPTYSTATE_CONTENT'); ?>
+				<?php echo $content; ?>
 			</p>
 			<div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
 				<?php if ($createURL && Factory::getApplication()->input->get('tmpl') !== 'component') : ?>
