f5c0f108e14128dd206103a5e1270e8a0db8cee7
Yatharth Vyas <yatharth2000.vyas@gmail.com>
2021-05-02 00:25:28 +0530

    remove redundant div

--- administrator/components/com_users/tmpl/level/edit.php
+++ administrator/components/com_users/tmpl/level/edit.php
@@ -24,7 +24,6 @@ HTMLHelper::_('behavior.keepalive');
 		<?php echo HTMLHelper::_('uitab.addTab', 'myTab', 'details', Text::_('COM_USERS_LEVEL_DETAILS')); ?>
 			<fieldset class="options-form mt-4">
 				<legend><?php echo Text::_('COM_USERS_LEVEL_DETAILS'); ?></legend>
-				<div>
 				<div class="control-group">
 					<div class="control-label">
 						<?php echo $this->form->getLabel('title'); ?>
@@ -33,7 +32,6 @@ HTMLHelper::_('behavior.keepalive');
 						<?php echo $this->form->getInput('title'); ?>
 					</div>
 				</div>
-				</div>
 			</fieldset>
 		<?php echo HTMLHelper::_('uitab.endTab'); ?>
 
@@ -41,7 +39,7 @@ HTMLHelper::_('behavior.keepalive');
 			<fieldset class="options-form">
 				<legend><?php echo Text::_('COM_USERS_USER_GROUPS_HAVING_ACCESS'); ?></legend>
 				<div>
-				<?php echo HTMLHelper::_('access.usergroups', 'jform[rules]', $this->item->rules, true); ?>
+					<?php echo HTMLHelper::_('access.usergroups', 'jform[rules]', $this->item->rules, true); ?>
 				</div>
 			</fieldset>
 		<?php echo HTMLHelper::_('uitab.endTab'); ?>
