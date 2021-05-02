28333b5b478b5237c1c1f6518a7c08708028574c
Phil E. Taylor <phil@phil-taylor.com>
2021-05-02 15:36:46 +0100

    Update index.php

--- templates/cassiopeia/index.php
+++ templates/cassiopeia/index.php
@@ -114,7 +114,7 @@ $wa->getAsset('style', 'fontawesome')->setAttribute('rel', 'lazy-stylesheet');
 	. ($task ? ' task-' . $task : ' no-task')
 	. ($itemid ? ' itemid-' . $itemid : '')
 	. ($pageclass ? ' ' . $pageclass : '')
-	. ($hasClass ? ' ' . $hasClass : '')
+	. ($hasClass ? $hasClass : '')
 	. ($this->direction == 'rtl' ? ' rtl' : '');
 ?>">
 	<header class="header container-header full-width<?php echo $stickyHeader ? ' ' . $stickyHeader : ''; ?>">
