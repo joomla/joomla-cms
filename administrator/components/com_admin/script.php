9b0a9f7d638af9f1eba55239dbebfebf64848cf2
Hannes Papenberg <info@joomlager.de>
2021-05-03 17:32:57 +0200

    [4.0] Removing GUI parts of FTP layer (#33390)

--- administrator/components/com_admin/script.php
+++ administrator/components/com_admin/script.php
@@ -6909,7 +6909,6 @@ class JoomlaInstallerScript
 
 				if ($dryRun === false)
 				{
-					// TODO There is an issue while deleting folders using the ftp mode
 					if (Folder::delete(JPATH_ROOT . $folder))
 					{
 						$status['folders_deleted'][] = $folder;
