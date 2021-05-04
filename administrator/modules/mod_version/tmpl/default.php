71a2f68e6b30fa22539b0da80409042e1b99f04a
rjharishabh <rjharishabh@gmail.com>
2021-05-04 11:22:29 +0530

    joomla version

--- administrator/modules/mod_version/tmpl/default.php
+++ administrator/modules/mod_version/tmpl/default.php
@@ -14,10 +14,8 @@ use Joomla\CMS\Language\Text;
 ?>
 <div class="header-item-content">
 	<div class="joomlaversion d-flex text-muted">
-		<div class="d-flex align-items-end mx-auto">
+		<div class="mx-auto">
 			<span class="icon-joomla" aria-hidden="true"></span>
-		</div>
-		<div class="tiny mx-auto">
 			<span class="visually-hidden"><?php echo Text::sprintf('MOD_VERSION_CURRENT_VERSION_TEXT', $version); ?></span>
 			<span aria-hidden="true"><?php echo $version; ?></span>
 		</div>
