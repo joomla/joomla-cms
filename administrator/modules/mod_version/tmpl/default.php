addfb0b571728b5ba85c7120561891509a78ce7b
ciar4n <ciaran@joomla51.com>
2021-05-05 21:55:30 +0100

    text only header joomlaversion

--- administrator/modules/mod_version/tmpl/default.php
+++ administrator/modules/mod_version/tmpl/default.php
@@ -13,10 +13,8 @@ use Joomla\CMS\Language\Text;
 
 ?>
 <div class="header-item-content joomlaversion">
-	<div class="header-item-icon">
-		<span class="icon-joomla" aria-hidden="true"></span>
-	</div>
 	<div class="header-item-text no-link">
+		<span class="icon-joomla" aria-hidden="true"></span>
 		<span class="visually-hidden"><?php echo Text::sprintf('MOD_VERSION_CURRENT_VERSION_TEXT', $version); ?></span>
 		<span aria-hidden="true"><?php echo $version; ?></span>
 	</div>
