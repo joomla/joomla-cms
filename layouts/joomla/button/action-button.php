4b522da7b1f40ecebd330816aaeacd0e4546036e
Hans Kuijpers <hans2103@gmail.com>
2021-05-05 10:39:25 +0200

    :hammer: replace popover by role=tooltip

--- layouts/joomla/button/action-button.php
+++ layouts/joomla/button/action-button.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * @package     Joomla.Site
- * @subpackage  Layout
+ * @package         Joomla.Site
+ * @subpackage      Layout
  *
  * @copyright   (C) 2017 Open Source Matters, Inc. <https://www.joomla.org>
- * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ * @license         GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 use Joomla\CMS\HTML\HTMLHelper;
@@ -14,33 +14,31 @@ extract($displayData, EXTR_OVERWRITE);
 /**
  * Layout variables
  * -----------------
- * @var   string  $icon
- * @var   string  $title
- * @var   string  $value
- * @var   string  $task
- * @var   array   $options
+ * @var   string $icon
+ * @var   string $title
+ * @var   string $value
+ * @var   string $task
+ * @var   array  $options
  */
 
-$disabled = !empty($options['disabled']);
-$taskPrefix = $options['task_prefix'];
+$disabled     = !empty($options['disabled']);
+$taskPrefix   = $options['task_prefix'];
 $checkboxName = $options['checkbox_name'];
-$tip = !empty($options['tip']);
-$tipTitle = $options['tip_title'];
+$tip          = !empty($options['tip']);
+$id           = $options['id'];
+$tipTitle     = $options['tip_title'];
 
-if ($tip)
-{
-	HTMLHelper::_('bootstrap.popover', '.hasPopover', ['trigger' => 'hover focus']);
-}
 ?>
-<button type="submit" class="tbody-icon data-state-<?php echo $this->escape($value ?? ''); ?><?php echo $tip ? ' hasPopover' : ''; ?>"
-		title="<?php echo HTMLHelper::_('tooltipText', $tipTitle ?: $title, '', 0); ?>"
-		data-bs-content="<?php echo HTMLHelper::_('tooltipText', $title, '', 0); ?>"
-		data-bs-placement="top"
+<button type="submit" class="tbody-icon data-state-<?php echo $this->escape($value ?? ''); ?>"
+        aria-labelledby="<?php echo $id; ?>"
 	<?php echo !empty($disabled) ? 'disabled' : ''; ?>
 	<?php if (!empty($task) && empty($disabled)) : ?>
-		onclick="return Joomla.listItemTask('<?php echo $checkboxName . $this->escape($row ?? ''); ?>', '<?php echo $this->escape(isset($task) ? $taskPrefix . $task : ''); ?>')"
+        onclick="return Joomla.listItemTask('<?php echo $checkboxName . $this->escape($row ?? ''); ?>', '<?php echo $this->escape(isset($task) ? $taskPrefix . $task : ''); ?>')"
 	<?php endif; ?>
 >
-	<span class="<?php echo $this->escape($icon ?? ''); ?>" aria-hidden="true"></span>
-	<span class="visually-hidden"><?php echo $title; ?></span>
+    <span class="<?php echo $this->escape($icon ?? ''); ?>" aria-hidden="true"></span>
+    <span class="visually-hidden"><?php echo $title; ?></span>
 </button>
+<div id="<?php echo $id; ?>" role="tooltip">
+	<?php echo HTMLHelper::_('tooltipText', $tipTitle ?: $title, '', 0); ?>
+</div>
