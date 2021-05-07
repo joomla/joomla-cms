2b812ca362d94b7534417a1c7c92c9e5a97b989f
Brian Teeman <brian@teeman.net>
2021-05-07 07:44:15 +0100

    Update administrator/components/com_admin/sql/updates/postgresql/4.0.0-2021-05-07.sql
    Co-authored-by: Richard Fath <richard67@users.noreply.github.com>

--- administrator/components/com_admin/sql/updates/postgresql/4.0.0-2021-05-07.sql
+++ administrator/components/com_admin/sql/updates/postgresql/4.0.0-2021-05-07.sql
@@ -1,2 +1,4 @@
-UPDATE "#__mail_templates" SET "subject" = 'COM_PRIVACY_EMAIL_DATA_EXPORT_COMPLETED_SUBJECT' WHERE "template_id" = 'com_privacy.userdataexport';
-UPDATE "#__mail_templates" SET "body" = 'COM_PRIVACY_EMAIL_DATA_EXPORT_COMPLETED_BODY' WHERE "template_id" = 'com_privacy.userdataexport';
+UPDATE "#__mail_templates"
+   SET "subject" = 'COM_PRIVACY_EMAIL_DATA_EXPORT_COMPLETED_SUBJECT',
+       "body" = 'COM_PRIVACY_EMAIL_DATA_EXPORT_COMPLETED_BODY'
+ WHERE "template_id" = 'com_privacy.userdataexport';
