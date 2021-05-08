abc3c151511854480565a246ff191a53c9ed2713
brian teeman <brian@teeman.net>
2021-05-08 21:34:21 +0100

    remove second check

--- administrator/modules/mod_messages/tmpl/default.php
+++ administrator/modules/mod_messages/tmpl/default.php
@@ -23,11 +23,9 @@ $route = 'index.php?option=com_messages&view=messages';
 ?>
 <a class="header-item-content" href="<?php echo Route::_($route); ?>" title="<?php echo Text::_('MOD_MESSAGES_PRIVATE_MESSAGES'); ?>">
 	<div class="header-item-icon">
-	<div class="w-auto">
-		<span class="fa-fw icon-envelope" aria-hidden="true"></span>
-			<?php if ($countUnread > 0) : ?>
-				<small class="header-item-count"><?php echo $countUnread; ?></small>
-			<?php endif; ?>
+		<div class="w-auto">
+			<span class="fa-fw icon-envelope" aria-hidden="true"></span>
+			<small class="header-item-count"><?php echo $countUnread; ?></small>
 		</div>
 	</div>
 	<div class="header-item-text">
--- administrator/modules/mod_post_installation_messages/tmpl/default.php
+++ administrator/modules/mod_post_installation_messages/tmpl/default.php
@@ -26,9 +26,7 @@ if ($hideLinks || count($messages) < 1)
 		<div class="header-item-icon">
 			<div class="w-auto">
 				<span class="fa-fw icon-bell" aria-hidden="true"></span>
-				<?php if (count($messages) > 0) : ?>
-					<small class="header-item-count"><?php echo count($messages); ?></small>
-				<?php endif; ?>
+				<small class="header-item-count"><?php echo count($messages); ?></small>
 			</div>
 		</div>
 		<div class="header-item-text">
