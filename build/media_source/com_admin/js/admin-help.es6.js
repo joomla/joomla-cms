f27465ee749d077d418c2d2be47b55fca2b6e112
ceford <cliff@ford.myzen.co.uk>
2021-05-10 20:05:14 +0100

    Using javascript clip provided by dgrammatiko

--- build/media_source/com_admin/js/admin-help.es6.js
+++ build/media_source/com_admin/js/admin-help.es6.js
@@ -1,18 +1,9 @@
 /**
- * @copyright   (C) 2018 Open Source Matters, Inc. <https://www.joomla.org>
- * @license     GNU General Public License version 2 or later; see LICENSE.txt
- */
-
-((document) => {
-  'use strict';
-
-  document.addEventListener('DOMContentLoaded', () => {
-    const helpIndex = document.getElementById('help-index');
-    const links = helpIndex.querySelectorAll('a');
-    links && links.forEach(element => {
-      element.addEventListener('click', () => {
-        window.scroll(0,0);
-      });
-    })
-  });
-})(document);
+  * @copyright   (C) 2018 Open Source Matters, Inc. <https://www.joomla.org>
+  * @license     GNU General Public License version 2 or later; see LICENSE.txt
+  */
+const helpIndex = document.getElementById('help-index');
+if (helpIndex) {
+  [].slice.call(helpIndex.querySelectorAll('a'))
+    .map((element) => element.addEventListener('click', () => { window.scroll(0,0); }));
+}
