75296aba79ca0047c1bd4d7f3509c54b576bcca9
chmst <dev@chmst.de>
2021-05-10 18:39:22 +0200

    New moduleposition brand, option to switch off logo

--- templates/cassiopeia/index.php
+++ templates/cassiopeia/index.php
@@ -125,16 +125,25 @@ $wa->getAsset('style', 'fontawesome')->setAttribute('rel', 'lazy-stylesheet');
 			</div>
 		<?php endif; ?>
 
-		<div class="grid-child">
-			<div class="navbar-brand">
-				<a class="brand-logo" href="<?php echo $this->baseurl; ?>/">
-					<?php echo $logo; ?>
-				</a>
-				<?php if ($this->params->get('siteDescription')) : ?>
-					<div class="site-description"><?php echo htmlspecialchars($this->params->get('siteDescription')); ?></div>
-				<?php endif; ?>
+		<?php if ($this->countModules('brand')) : ?>
+			<div class="grid-child container-brand">
+				<jdoc:include type="modules" name="brand" style="none" />
 			</div>
-		</div>
+		<?php endif; ?>
+
+		<?php if ($this->params->get('brand')) : ?>
+			<div class="grid-child">
+				<div class="navbar-brand">
+					<a class="brand-logo" href="<?php echo $this->baseurl; ?>/">
+						<?php echo $logo; ?>
+					</a>
+					<?php if ($this->params->get('siteDescription')) : ?>
+						<div class="site-description"><?php echo htmlspecialchars($this->params->get('siteDescription')); ?></div>
+					<?php endif; ?>
+				</div>
+			</div>
+		<?php endif; ?>
+
 		<?php if ($this->countModules('menu', true) || $this->countModules('search', true)) : ?>
 			<div class="grid-child container-nav">
 				<?php if ($this->countModules('menu', true)) : ?>
