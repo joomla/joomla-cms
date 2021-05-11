32acd49f14baffe27a14c132764e01a52f019774
Himanshu <addyjeridiq@gmail.com>
2021-05-11 10:20:12 +0530

    This works

--- administrator/modules/mod_submenu/tmpl/default.php
+++ administrator/modules/mod_submenu/tmpl/default.php
@@ -51,9 +51,9 @@ use Joomla\CMS\Router\Route;
 						<?php // Only if Menu-show = true ?>
 						<?php if ($params->get('menu_show', 1)) : ?>
 							<li class="list-group-item d-flex align-items-center">
-								<a class="flex-grow-1" href="<?php echo $item->link; ?>"
+								<a class="flex-grow-1 text-decoration-underline" href="<?php echo $item->link; ?>"
 									<?php echo $item->target === '_blank' ? ' title="' . Text::sprintf('JBROWSERTARGET_NEW_TITLE', Text::_($item->title)) . '"' : ''; ?>
-									<?php echo $item->target ? ' target="' . $item->target . '"' : ''; ?>><u>
+									<?php echo $item->target ? ' target="' . $item->target . '"' : ''; ?>>
 									<?php if (!empty($params->get('menu_image'))) : ?>
 										<?php
 										$image = htmlspecialchars($params->get('menu_image'), ENT_QUOTES, 'UTF-8');
@@ -67,7 +67,7 @@ use Joomla\CMS\Router\Route;
 										<span class="menu-badge">
 											<span class="icon-spin icon-spinner mt-1 system-counter float-end" data-url="<?php echo $item->ajaxbadge; ?>"></span>
 										</span>
-									<?php endif; ?></u>
+									<?php endif; ?>
 								</a>
 								<?php if ($params->get('menu-quicktask')) : ?>
 									<?php $permission = $params->get('menu-quicktask-permission'); ?>
