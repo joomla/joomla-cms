de7ddb1172f9a38f0a0e25943c64c04ab9b3e133
Richard Fath <richard67@users.noreply.github.com>
2021-05-12 13:04:24 +0200

    Fix SQL error on update from 3.10

--- administrator/components/com_admin/sql/updates/postgresql/4.0.0-2018-06-03.sql
+++ administrator/components/com_admin/sql/updates/postgresql/4.0.0-2018-06-03.sql
@@ -1,1 +1,8 @@
--- This has been removed as com_csp has been removed from the final build
+-- Com_csp has been removed from the final build.
+-- But here it still needs to create the table so it can be later deleted on update with script
+-- 4.0.0-2021-05-04.sql regardles from which Joomla version an update is made.
+
+CREATE TABLE IF NOT EXISTS "#__csp" (
+  "id" serial NOT NULL,
+  PRIMARY KEY ("id")
+);
