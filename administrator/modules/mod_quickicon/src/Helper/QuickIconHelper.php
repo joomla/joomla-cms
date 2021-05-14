c10d925982ea0a9655cc52694c736d7da512e180
Phil E. Taylor <phil@phil-taylor.com>
2021-05-14 21:31:17 +0100

    change to a better icon

--- administrator/modules/mod_quickicon/src/Helper/QuickIconHelper.php
+++ administrator/modules/mod_quickicon/src/Helper/QuickIconHelper.php
@@ -15,6 +15,7 @@ use Joomla\CMS\Application\CMSApplication;
 use Joomla\CMS\Factory;
 use Joomla\CMS\Plugin\PluginHelper;
 use Joomla\CMS\Router\Route;
+use Joomla\CMS\Session\Session;
 use Joomla\Module\Quickicon\Administrator\Event\QuickIconsEvent;
 use Joomla\Registry\Registry;
 
@@ -172,12 +173,13 @@ class QuickIconHelper
 			if ($params->get('show_plugins'))
 			{
 				$tmp = [
-					'image'   => 'icon-plug',
-					'link'    => Route::_('index.php?option=com_plugins'),
-					'linkadd' => Route::_('index.php?option=com_installer'),
-					'name'    => 'MOD_QUICKICON_PLUGIN_MANAGER',
-					'access'  => array('core.manage', 'com_plugins'),
-					'group'   => 'MOD_QUICKICON_SITE'
+					'image'    => 'icon-plug',
+					'link'     => Route::_('index.php?option=com_plugins'),
+					'linkadd'  => Route::_('index.php?option=com_installer'),
+					'linkicon' => 'icon-upload',
+					'name'     => 'MOD_QUICKICON_PLUGIN_MANAGER',
+					'access'   => array('core.manage', 'com_plugins'),
+					'group'    => 'MOD_QUICKICON_SITE'
 				];
 
 				if ($params->get('show_plugins') == 2)
@@ -231,11 +233,13 @@ class QuickIconHelper
 			if ($params->get('show_cache'))
 			{
 				$tmp = [
-					'image'   => 'icon-cloud',
-					'link'    => Route::_('index.php?option=com_cache'),
-					'name'    => 'MOD_QUICKICON_CACHE',
-					'access'  => array('core.admin', 'com_cache'),
-					'group'   => 'MOD_QUICKICON_SYTEM'
+					'image'    => 'icon-cloud',
+					'link'     => Route::_('index.php?option=com_cache'),
+					'name'     => 'MOD_QUICKICON_CACHE',
+					'linkadd'  => Route::_('index.php?option=com_cache&task=deleteAll&' . Session::getFormToken() . '=1'),
+					'linkicon' => 'icon-times',
+					'access'   => array('core.admin', 'com_cache'),
+					'group'    => 'MOD_QUICKICON_SYTEM'
 				];
 
 				if ($params->get('show_cache') == 2)
