b24b53a50b27bff7c36a5a1373c7bd07e88d4867
Phil E. Taylor <phil@phil-taylor.com>
2021-05-18 21:08:59 +0100

    Update libraries/src/Application/ConsoleApplication.php
    Co-authored-by: Brian Teeman <brian@teeman.net>

--- libraries/src/Application/ConsoleApplication.php
+++ libraries/src/Application/ConsoleApplication.php
@@ -107,7 +107,7 @@ class ConsoleApplication extends Application implements DispatcherAwareInterface
 		?OutputInterface $output = null
 	)
 	{
-		// Close the application if we are not executed from the command line.
+		// Close the application if it is not executed from the command line.
 		if (!\defined('STDOUT') || !\defined('STDIN') || !isset($_SERVER['argv']))
 		{
 			$this->close();
