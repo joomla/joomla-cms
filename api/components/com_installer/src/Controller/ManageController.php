ebe64116caa15dcb03f2d35e262481457248c8f0
Phil E. Taylor <phil@phil-taylor.com>
2021-05-19 11:30:17 +0100

    revert

--- api/components/com_installer/src/Controller/ManageController.php
+++ api/components/com_installer/src/Controller/ManageController.php
@@ -48,7 +48,7 @@ class ManageController extends ApiController
 	 */
 	public function displayList()
 	{
-		$requestBool = $this->input->get('core', $this->input->get->get('core', null, 'string'));
+		$requestBool = $this->input->get('core', $this->input->get->get('core', null));
 
 		if (!is_null($requestBool) && $requestBool !== 'true' && $requestBool !== 'false')
 		{
