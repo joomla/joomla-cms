078b4107f2c265cd200999063fe59320e8203588
Hannes Papenberg <info@joomlager.de>
2021-05-20 19:11:41 +0200

    Routing: Fix notice in NoMenu rule

--- libraries/src/Component/Router/Rules/NomenuRules.php
+++ libraries/src/Component/Router/Rules/NomenuRules.php
@@ -139,7 +139,8 @@ class NomenuRules implements RulesInterface
 		{
 			$item = $this->router->menu->getItem($query['Itemid']);
 
-			if (!isset($query['option']) || ($item && $item->query['option'] === $query['option']))
+			if (!isset($query['option'])
+				|| ($item && isset($query['option']) && $item->query['option'] === $query['option']))
 			{
 				$menu_found = true;
 			}
