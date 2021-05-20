e5bf2be08946553452ff05b25e3b949fbfe54f7c
Phil E. Taylor <phil@phil-taylor.com>
2021-05-20 18:47:51 +0100

    [4] fixed undefined/wrong exception classname in com_privacy (#34005)

--- administrator/components/com_privacy/src/View/Request/HtmlView.php
+++ administrator/components/com_privacy/src/View/Request/HtmlView.php
@@ -14,6 +14,7 @@ namespace Joomla\Component\Privacy\Administrator\View\Request;
 use Joomla\CMS\Factory;
 use Joomla\CMS\Form\Form;
 use Joomla\CMS\Language\Text;
+use Joomla\CMS\MVC\View\GenericDataException;
 use Joomla\CMS\MVC\View\HtmlView as BaseHtmlView;
 use Joomla\CMS\Object\CMSObject;
 use Joomla\CMS\Router\Route;
@@ -102,7 +103,7 @@ class HtmlView extends BaseHtmlView
 		// Check for errors.
 		if (count($errors = $this->get('Errors')))
 		{
-			throw new \JViewGenericdataexception(implode("\n", $errors), 500);
+			throw new GenericDataException(implode("\n", $errors), 500);
 		}
 
 		$this->addToolbar();
