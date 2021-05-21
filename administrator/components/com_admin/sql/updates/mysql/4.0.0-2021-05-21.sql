ea3abbbb42c6d531ea6f47b2eb23b801088ce6b6
Richard Fath <richard67@users.noreply.github.com>
2021-05-21 21:56:09 +0200

    Revert update for module layouts

--- administrator/components/com_admin/sql/updates/mysql/4.0.0-2021-05-21.sql
+++ administrator/components/com_admin/sql/updates/mysql/4.0.0-2021-05-21.sql
@@ -4,17 +4,3 @@ UPDATE `#__modules`
    AND `module` = 'mod_menu'
    AND `position` = 'menu'
    AND `params` LIKE '{%"layout":"cassiopeia:dropdown-metismenu"%}';
-
-UPDATE `#__modules`
-   SET `params` = REPLACE(`params`,'"layout":"_:default"','"layout":"_:collapse-default"')
- WHERE `client_id` = 0
-   AND `module` = 'mod_menu'
-   AND `position` = 'menu'
-   AND `params` LIKE '{%"layout":"_:default"%}';
-
-UPDATE `#__modules`
-   SET `params` = REPLACE(`params`,'"layout":""','"layout":"_:collapse-default"')
- WHERE `client_id` = 0
-   AND `module` = 'mod_menu'
-   AND `position` = 'menu'
-   AND `params` LIKE '{%"layout":""%}';
