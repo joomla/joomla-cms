baa5869cf5125c08fbef1898bec77c22da0ce9ae
Richard Fath <richard67@users.noreply.github.com>
2021-05-21 21:30:57 +0200

    Update SQL for modules on the menu position

---
+++ administrator/components/com_admin/sql/updates/mysql/4.0.0-2021-05-21.sql
@@ -0,0 +1,20 @@
+UPDATE `#__modules`
+   SET `params` = REPLACE(`params`,'"layout":"cassiopeia:dropdown-metismenu"','"layout":"cassiopeia:collapse-metismenu"')
+ WHERE `client_id` = 0
+   AND `module` = 'mod_menu'
+   AND `position` = 'menu'
+   AND `params` LIKE '{%"layout":"cassiopeia:dropdown-metismenu"%}';
+
+UPDATE `#__modules`
+   SET `params` = REPLACE(`params`,'"layout":"_:default"','"layout":"_:collapse-default"')
+ WHERE `client_id` = 0
+   AND `module` = 'mod_menu'
+   AND `position` = 'menu'
+   AND `params` LIKE '{%"layout":"_:default"%}';
+
+UPDATE `#__modules`
+   SET `params` = REPLACE(`params`,'"layout":""','"layout":"_:collapse-default"')
+ WHERE `client_id` = 0
+   AND `module` = 'mod_menu'
+   AND `position` = 'menu'
+   AND `params` LIKE '{%"layout":""%}';
