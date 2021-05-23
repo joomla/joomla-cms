52e4d5c32f30cb81f0e32b77995600e2e349db3c
Richard Fath <richard67@users.noreply.github.com>
2021-05-23 16:24:05 +0200

    Combine SQL update scripts

--- administrator/components/com_admin/sql/updates/mysql/4.0.0-2020-04-11.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO `#__extensions` (`package_id`, `name`, `type`, `element`, `folder`, `client_id`, `enabled`, `access`, `protected`, `locked`, `manifest_cache`, `params`, `custom_data`, `checked_out`, `checked_out_time`, `ordering`, `state`) VALUES
-(0, 'plg_quickicon_downloadkey', 'plugin', 'downloadkey', 'quickicon', 0, 1, 1, 0, 1, '', '', '', 0, NULL, 0, 0);
--- administrator/components/com_admin/sql/updates/postgresql/4.0.0-2017-06-03.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "#__extensions" ADD COLUMN "changelogurl" text;
-ALTER TABLE "#__updates" ADD COLUMN "changelogurl" text;
