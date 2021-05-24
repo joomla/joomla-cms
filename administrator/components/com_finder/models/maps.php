4f8b0b25a64ccccc26b028f9fcd703a0aa8764fa
YatharthVyas <yatharth2000.vyas@gmail.com>
2021-05-24 12:44:13 +0530

    Fix delete batch for maps

--- administrator/components/com_finder/models/maps.php
+++ administrator/components/com_finder/models/maps.php
@@ -86,6 +86,17 @@ class FinderModelMaps extends JModelList
 		// Include the content plugins for the on delete events.
 		JPluginHelper::importPlugin('content');
 
+		// Iterate the items to check if all of them exist.
+		foreach ($pks as $i => $pk)
+		{
+			if (!$table->load($pk))
+			{
+				$this->setError($table->getError());
+
+				return false;
+			}
+		}
+
 		// Iterate the items to delete each one.
 		foreach ($pks as $i => $pk)
 		{
@@ -130,12 +141,6 @@ class FinderModelMaps extends JModelList
 						$this->setError(JText::_('JLIB_APPLICATION_ERROR_DELETE_NOT_PERMITTED'));
 					}
 				}
-			}
-			else
-			{
-				$this->setError($table->getError());
-
-				return false;
 			}
 		}
 
