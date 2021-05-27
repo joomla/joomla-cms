a3a1b8ba73c60f33c6eeabc9b5e751efde5e9fe0
Richard Fath <richard67@users.noreply.github.com>
2021-05-27 23:40:52 +0200

    [4.0] Combine update SQL scripts to reduce their number (#34139)

--- administrator/components/com_admin/sql/updates/postgresql/4.0.0-2019-10-29.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO "#__extensions" ("package_id", "name", "type", "element", "folder", "client_id", "enabled", "access", "protected", "manifest_cache", "params", "custom_data", "checked_out", "checked_out_time", "ordering", "state") VALUES
-(0, 'plg_webservices_installer', 'plugin', 'installer', 'webservices', 0, 1, 1, 0, '', '{}', '', 0, NULL, 0, 0);
