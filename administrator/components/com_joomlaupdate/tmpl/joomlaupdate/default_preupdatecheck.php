d29bcf85b10f9c9c5d0adc352bd819c622fa81c4
Quy <quy@fluxbb.org>
2021-05-28 08:33:02 -0700

    space

--- administrator/components/com_joomlaupdate/tmpl/joomlaupdate/default_preupdatecheck.php
+++ administrator/components/com_joomlaupdate/tmpl/joomlaupdate/default_preupdatecheck.php
@@ -188,7 +188,7 @@ $compatibilityTypes = array(
 				<?php $compatibilityTypeGroup    = $compatibilityData['group']; ?>
 				<fieldset id="compatibilitytype<?php echo $compatibilityTypeGroup;?>" class="col-md-12 compatibilitytypes">
 					<legend>
-						<h3 class="alert <?php echo $compatibilityDisplayClass;?>">
+						<h3 class="alert <?php echo $compatibilityDisplayClass; ?>">
 							<?php if ($compatibilityType !== "COM_JOOMLAUPDATE_VIEW_DEFAULT_EXTENSIONS_RUNNING_PRE_UPDATE_CHECKS") : ?>
 								<div class="compatibilitytoggle" data-state="closed"><?php echo Text::sprintf('COM_JOOMLAUPDATE_VIEW_DEFAULT_EXTENSIONS_SHOW_MORE_COMPATIBILITY_INFORMATION', '<span class="icon-chevron-right"></span>'); ?></div>
 							<?php endif; ?>
