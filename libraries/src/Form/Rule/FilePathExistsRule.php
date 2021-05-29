a78ec917eaafc3883dee6736a7531fb07230e6fe
Richard Fath <richard67@users.noreply.github.com>
2021-05-29 14:10:48 +0200

    Create FilePathExistsRule.php

---
+++ libraries/src/Form/Rule/FilePathExistsRule.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Joomla! Content Management System
+ *
+ * @copyright  Copyright (C) 2005 - 2020 Open Source Matters, Inc. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+namespace Joomla\CMS\Form\Rule;
+
+defined('JPATH_PLATFORM') or die;
+
+use Joomla\CMS\Form\Form;
+use Joomla\CMS\Form\Rule\FilePathRule;
+use Joomla\Registry\Registry;
+
+/**
+ * Form Rule class for the Joomla Platform.
+ *
+ * @since  4.0.0
+ */
+class FilePathExistsRule extends FilePathRule
+{
+	/**
+	 * Method to test if the file path is valid and points to an existing file in or below the Joomla root or a directory
+	 *
+	 * @param   \SimpleXMLElement  $element  The SimpleXMLElement object representing the `<field>` tag for the form field object.
+	 * @param   mixed              $value    The form field value to validate.
+	 * @param   string             $group    The field name group control value. This acts as an array container for the field.
+	 *                                       For example if the field has name="foo" and the group value is set to "bar" then the
+	 *                                       full field name would end up being "bar[foo]".
+	 * @param   Registry           $input    An optional Registry object with the entire data set to validate against the entire form.
+	 * @param   Form               $form     The form object for which the field is being tested.
+	 *
+	 * @return  boolean  True if the value is valid and points to an existing file in or below the Joomla root, false otherwise.
+	 *
+	 * @since   4.0.0
+	 */
+	public function test(\SimpleXMLElement $element, $value, $group = null, Registry $input = null, Form $form = null)
+	{
+		if (!parent::test($element, $value, $group, $input, $form))
+		{
+			return false;
+		}
+
+		// In case of a file list field we might have a directory, otherwise it's Joomla root
+		$parentPath = $element['directory'] ?? JPATH_ROOT;
+
+		return is_file($parentPath . '/' . $value);
+	}
+}
