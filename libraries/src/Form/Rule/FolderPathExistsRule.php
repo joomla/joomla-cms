cf98d0c89acf76dd7644c2e2f88734a392788dfd
Richard Fath <richard67@users.noreply.github.com>
2021-05-30 12:38:34 +0200

    Don't allow Joomla root in FolderPathExistsRule

--- libraries/src/Form/Rule/FolderPathExistsRule.php
+++ libraries/src/Form/Rule/FolderPathExistsRule.php
@@ -24,7 +24,7 @@ use Joomla\Registry\Registry;
 class FolderPathExistsRule extends FilePathRule
 {
 	/**
-	 * Method to test if the folder path is valid and points to an existing folder (directory) in or below the Joomla root
+	 * Method to test if the folder path is valid and points to an existing folder (directory) below the Joomla root
 	 *
 	 * @param   \SimpleXMLElement  $element  The SimpleXMLElement object representing the `<field>` tag for the form field object.
 	 * @param   mixed              $value    The form field value to validate.
@@ -34,7 +34,7 @@ class FolderPathExistsRule extends FilePathRule
 	 * @param   Registry           $input    An optional Registry object with the entire data set to validate against the entire form.
 	 * @param   Form               $form     The form object for which the field is being tested.
 	 *
-	 * @return  boolean  True if the value is valid and points to an existing folder in or below the Joomla root, false otherwise.
+	 * @return  boolean  True if the value is valid and points to an existing folder below the Joomla root, false otherwise.
 	 *
 	 * @since   __DEPLOY_VERSION__
 	 */
@@ -53,12 +53,14 @@ class FolderPathExistsRule extends FilePathRule
 			return true;
 		}
 
+		$rootCleaned = rtrim(Path::clean(JPATH_ROOT), \DIRECTORY_SEPARATOR);
+
 		// In case of a folder list field we might have a directory, otherwise it's Joomla root
 		if (isset($element['directory']) && trim((string) $element['directory']))
 		{
 			$parentPath = Path::clean((string) $element['directory']);
 
-			if (strpos($parentPath, Path::clean(JPATH_ROOT)) !== 0)
+			if (strpos($parentPath, $rootCleaned) !== 0)
 			{
 				$parentPath = JPATH_ROOT . '/' . $parentPath;
 			}
@@ -68,6 +70,14 @@ class FolderPathExistsRule extends FilePathRule
 			$parentPath = JPATH_ROOT;
 		}
 
-		return Folder::exists($parentPath . '/' . $value);
+		$pathCleaned = rtrim(Path::clean($parentPath . '/' . $value), \DIRECTORY_SEPARATOR);
+
+		// Joomla root is not allowed
+		if ($pathCleaned === $rootCleaned)
+		{
+			return false;
+		}
+
+		return Folder::exists($pathCleaned);
 	}
 }
