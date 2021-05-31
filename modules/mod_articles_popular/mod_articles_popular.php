f9ddeb00417459ed6a42aa97dbed91a07471fad1
Phil E. Taylor <phil@phil-taylor.com>
2021-05-31 22:04:21 +0100

    Move rendering from outside of the model

--- modules/mod_articles_popular/mod_articles_popular.php
+++ modules/mod_articles_popular/mod_articles_popular.php
@@ -9,9 +9,19 @@
 
 defined('_JEXEC') or die;
 
+use Joomla\CMS\Component\ComponentHelper;
 use Joomla\CMS\Helper\ModuleHelper;
+use Joomla\CMS\Language\Text;
 use Joomla\Module\ArticlesPopular\Site\Helper\ArticlesPopularHelper;
 
 $list = ArticlesPopularHelper::getList($params);
+
+// Exit early if hits are disabled.
+if (!ComponentHelper::getParams('com_content')->get('record_hits', 1))
+{
+	echo Text::_('JGLOBAL_RECORD_HITS_DISABLED');
+
+	return;
+}
 
 require ModuleHelper::getLayoutPath('mod_articles_popular', $params->get('layout', 'default'));
