95d28f02f20908dfd2f675a4e434eb306fed9207
Phil E. Taylor <phil@phil-taylor.com>
2021-05-31 22:43:00 +0100

    reflow code

--- libraries/src/Mail/MailTemplate.php
+++ libraries/src/Mail/MailTemplate.php
@@ -495,13 +495,14 @@ class MailTemplate
 			return '';
 		}
 
+		// Replace any placeholders.
+		$name = $this->replaceTags($name, $this->data);
+
+		// Get the file extension.
 		$ext = File::getExt($file);
 
 		// Strip off extension from $name and append extension of $file, if any
 		$name = File::stripExt($name) . ($ext ? '.' . $ext : '');
-
-		// Replace any placeholders.
-		$name = $this->replaceTags($name, $this->data);
 
 		return $name;
 	}
