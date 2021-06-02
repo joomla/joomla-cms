c89aed55005d3ca24ea4d229d34323abbd34dc37
Phil E. Taylor <phil@phil-taylor.com>
2021-06-02 19:42:14 +0100

    [4] Cleanup Module Empty State Layout (#34319)
    * cleanup on code reivew
    Co-authored-by: Quy <quy@fluxbb.org>

--- layouts/joomla/content/emptystate_module.php
+++ layouts/joomla/content/emptystate_module.php
@@ -12,29 +12,22 @@ defined('_JEXEC') or die;
 use Joomla\CMS\Factory;
 use Joomla\CMS\Language\Text;
 
-$textPrefix = $displayData['textPrefix'] ?? '';
 $icon = $displayData['icon'] ?? 'icon-copy article';
-$componentLangString = $textPrefix . '_EMPTYSTATE_TITLE';
-$moduleLangString = $textPrefix . '_EMPTYSTATE_MODULE_TITLE' . (array_key_exists('textSuffix', $displayData) ? $displayData['textSuffix'] : '');
+$textPrefix = $displayData['textPrefix'] ?? '';
+$textSuffix = $displayData['textSuffix'] ?? '';
+$title = $displayData['title'] ?? '';
+$componentLangString = $textPrefix . '_EMPTYSTATE_TITLE' . $textSuffix;
+$moduleLangString = $textPrefix . '_EMPTYSTATE_MODULE_TITLE' . $textSuffix;
 
-// Did we have a definitive title provided to the view?
-if (isset($displayData['title']))
+// Did we have a definitive title provided to the view? If not, let's find one!
+if (!$title)
 {
-	$title = Text::_($displayData['title']);
-}
-// Can we find a *_EMPTYSTATE_MODULE_TITLE translation?
-elseif (Factory::getApplication()->getLanguage()->hasKey($moduleLangString))
-{
-	$title = Text::_($moduleLangString);
-}
-// Else use the components *_EMPTYSTATE_TITLE string.
-else
-{
-	$title = Text::_($componentLangString);
+	// Can we find a *_EMPTYSTATE_MODULE_TITLE translation, Else use the components *_EMPTYSTATE_TITLE string
+	$title = Factory::getApplication()->getLanguage()->hasKey($moduleLangString) ? $moduleLangString : $componentLangString;
 }
 ?>
 <div class="mb-4">
 	<p class="fw-bold text-center text-muted">
-		<span class="<?php echo $icon; ?>" aria-hidden="true"></span> <?php echo $title; ?>
+		<span class="<?php echo $icon; ?>" aria-hidden="true"></span> <?php echo Text::_($title); ?>
 	</p>
 </div>
