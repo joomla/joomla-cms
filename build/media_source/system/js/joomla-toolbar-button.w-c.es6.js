6fa64d07b280138d60061eb25e44c1a9bd6a227d
shim-sao <35068017+shim-sao@users.noreply.github.com>
2021-06-03 15:15:19 +0200

    Update joomla-toolbar-button.w-c.es6.js

--- build/media_source/system/js/joomla-toolbar-button.w-c.es6.js
+++ build/media_source/system/js/joomla-toolbar-button.w-c.es6.js
@@ -101,9 +101,6 @@ window.customElements.define('joomla-toolbar-button', class extends HTMLElement 
       return false;
     }
 
-    // Avoid multiple call to the task like accidental double-click
-    this.disabled = true;
-
     // eslint-disable-next-line no-restricted-globals
     if (this.confirmMessage && !confirm(this.confirmMessage)) {
       return false;
@@ -112,9 +109,6 @@ window.customElements.define('joomla-toolbar-button', class extends HTMLElement 
     if (this.task) {
       Joomla.submitbutton(this.task, this.form, this.formValidation);
     }
-
-    // Enable call to task again after its execution
-    this.disabled = false;
 
     return true;
   }
