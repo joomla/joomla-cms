8a1e9c7a9994a1e7e77939a60b4e00d8f22ba134
zero-24 <zero-24@users.noreply.github.com>
2021-06-07 19:31:08 +0200

    more improvments for the plugin

--- plugins/quickicon/eos310/eos310.php
+++ plugins/quickicon/eos310/eos310.php
@@ -17,12 +17,12 @@ use Joomla\CMS\Language\Text;
 class plgQuickiconEos310 extends JPlugin
 {
 	/**
-	 * Indicates sending statistics is always allowed.
+	 * The EOS date for 3.10 to be updated at the stable release
 	 *
 	 * @var    integer
 	 * @since  __DEPLOY_VERSION__
 	 */
-	const EOS_DATE = '28.05.2021';
+	const EOS_DATE = '07.06.2023';
 
 	/**
 	 * Application object
@@ -40,7 +40,6 @@ class plgQuickiconEos310 extends JPlugin
 	 */
 	protected $autoloadLanguage = true;
 
-
 	/**
 	 * This method is called when the Quick Icons module is constructing its set
 	 * of icons. You can return an array which defines a single icon and it will
@@ -56,16 +55,24 @@ class plgQuickiconEos310 extends JPlugin
 	public function onGetIcons($context)
 	{
 		$diff = Factory::getDate()->diff(Factory::getDate(static::EOS_DATE));
-		$weeksUntilEOS = floor($diff->days / 7);
+		$monthsUntilEOS = floor($diff->days / 30.417);
 
-		$messageInfo = $this->getMessageInfo($diff->m, $diff->invert);
+		$messageInfo = $this->getMessageInfo($monthsUntilEOS, $diff->invert);
 
+		// No messages yet.
+		if (!$messageInfo)
+		{
+			return;
+		}
+
+		// Build the message to be displayed
 		$messageText = Text::sprintf(
 			$messageInfo['messageText'],
 			static::EOS_DATE,
 			$messageInfo['messageLink'],
 		);
 
+		// Check whether we show this message above the cpanel
 		if ($messageInfo['showMainMessage'] && $this->app->input->get('option') == 'com_cpanel')
 		{
 			$this->app->enqueueMessage(
@@ -74,14 +81,15 @@ class plgQuickiconEos310 extends JPlugin
 			);
 		}
 
+		// Check whether we show this message on the quick icon side.
 		if ($messageInfo['showQuickIconMessage'])
 		{
 			return array(array(
 				'link'  => $messageInfo['messageLink'],
-				'image' => 'info-circle',
+				'image' => $messageInfo['image'],
 				'text'  => $messageText,
 				'id'    => 'plg_quickicon_eos310',
-				'group' => Text::_('PLG_QUICKICON_EOS310_GROUP')
+				'group' => $messageInfo['groupText'],
 			));
 		}
 	}
@@ -92,7 +100,7 @@ class plgQuickiconEos310 extends JPlugin
 	 * @param  $monthsUntilEOS  The months until we reach EOS
 	 * @param  $inverted        Have we surpassed the EOS date
 	 *
-	 * @return  array  An array with the message do be displayed
+	 * @return  array|bool  An array with the message do be displayed or false
 	 *
 	 * @since   __DEPLOY_VERSION__
 	 */
@@ -102,48 +110,12 @@ class plgQuickiconEos310 extends JPlugin
 		if ($inverted === 1)
 		{
 			return array(
-				'messageText' => 'PLG_QUICKICON_EOS310_MESSAGE_ERROR_SUPPORT_ENDED',
-				'messageType' => 'danger',
-				'messageLink' => 'https://docs.joomla.org/Planning_for_Mini-Migration_-_Joomla_3.10.x_to_4.x',
-				'showMainMessage' => true,
-				'showQuickIconMessage' => true,
-
-			);
-		}
-
-		// Lets start our messages 2 month after the initial release, still 22 month to go
-		if ($monthsUntilEOS <= 22)
-		{
-			return array(
-				'messageText' => 'PLG_QUICKICON_EOS310_MESSAGE_INFO_MESSAGE_01',
-				'messageType' => 'info',
-				'messageLink' => 'https://joomla.org/4',
-				'showMainMessage' => false,
-				'showQuickIconMessage' => true,
-
-			);
-		}
-
-		// We still have 16 month to go, lets remind them about the pre upgrade checker
-		if ($monthsUntilEOS <= 16)
-		{
-			return array(
-				'messageText' => 'PLG_QUICKICON_EOS310_MESSAGE_INFO_MESSAGE_02',
-				'messageType' => 'info',
-				'messageLink' => 'https://docs.joomla.org/Pre-Update_Check',
-				'showMainMessage' => false,
-				'showQuickIconMessage' => true,
-			);
-		}
-
-		// We are in security only mode now, 12 month to go from now on
-		if ($monthsUntilEOS <= 12)
-		{
-			return array(
-				'messageText' => 'PLG_QUICKICON_EOS310_MESSAGE_WARNING_SECURITY_ONLY',
-				'messageType' => 'warning',
-				'messageLink' => 'https://docs.joomla.org/Planning_for_Mini-Migration_-_Joomla_3.10.x_to_4.x',
-				'showMainMessage' => true,
+				'messageText'          => 'PLG_QUICKICON_EOS310_MESSAGE_ERROR_SUPPORT_ENDED',
+				'messageType'          => 'error',
+				'image'                => 'minus-circle',
+				'messageLink'          => 'https://docs.joomla.org/Planning_for_Mini-Migration_-_Joomla_3.10.x_to_4.x',
+				'groupText'            => Text::_('PLG_QUICKICON_EOS310_GROUPNAME_EOS'),
+				'showMainMessage'      => true,
 				'showQuickIconMessage' => true,
 			);
 		}
@@ -152,17 +124,58 @@ class plgQuickiconEos310 extends JPlugin
 		if ($monthsUntilEOS <= 6)
 		{
 			return array(
-				'messageText' => 'PLG_QUICKICON_EOS310_MESSAGE_WARNING_SUPPORT_ENDING',
-				'messageType' => 'warning',
-				'messageLink' => 'https://docs.joomla.org/Planning_for_Mini-Migration_-_Joomla_3.10.x_to_4.x',
-				'showMainMessage' => true,
+				'messageText'          => 'PLG_QUICKICON_EOS310_MESSAGE_WARNING_SUPPORT_ENDING',
+				'messageType'          => 'warning',
+				'image'                => 'warning-circle',
+				'messageLink'          => 'https://docs.joomla.org/Planning_for_Mini-Migration_-_Joomla_3.10.x_to_4.x',
+				'groupText'            => Text::_('PLG_QUICKICON_EOS310_GROUPNAME_WARNING'),
+				'showMainMessage'      => true,
 				'showQuickIconMessage' => true,
 			);
 		}
 
-		return array(
-			'showMainMessage' => false,
-			'showQuickIconMessage' => false,
-		);
+		// We are in security only mode now, 12 month to go from now on
+		if ($monthsUntilEOS <= 12)
+		{
+			return array(
+				'messageText'          => 'PLG_QUICKICON_EOS310_MESSAGE_WARNING_SECURITY_ONLY',
+				'messageType'          => 'warning',
+				'image'                => 'warning-circle',
+				'messageLink'          => 'https://docs.joomla.org/Planning_for_Mini-Migration_-_Joomla_3.10.x_to_4.x',
+				'groupText'            => Text::_('PLG_QUICKICON_EOS310_GROUPNAME_WARNING'),
+				'showMainMessage'      => false,
+				'showQuickIconMessage' => true,
+			);
+		}
+
+		// We still have 16 month to go, lets remind them about the pre upgrade checker
+		if ($monthsUntilEOS <= 16)
+		{
+			return array(
+				'messageText'          => 'PLG_QUICKICON_EOS310_MESSAGE_INFO_MESSAGE_02',
+				'messageType'          => 'info',
+				'image'                => 'info-circle',
+				'messageLink'          => 'https://docs.joomla.org/Pre-Update_Check',
+				'groupText'            => Text::_('PLG_QUICKICON_EOS310_GROUPNAME_INFO'),
+				'showMainMessage'      => false,
+				'showQuickIconMessage' => true,
+			);
+		}
+
+		// Lets start our messages 2 month after the initial release, still 22 month to go
+		if ($monthsUntilEOS <= 22)
+		{
+			return array(
+				'messageText'          => 'PLG_QUICKICON_EOS310_MESSAGE_INFO_MESSAGE_01',
+				'messageType'          => 'info',
+				'image'                => 'info-circle',
+				'messageLink'          => 'https://joomla.org/4',
+				'groupText'            => Text::_('PLG_QUICKICON_EOS310_GROUPNAME_INFO'),
+				'showMainMessage'      => false,
+				'showQuickIconMessage' => true,
+			);
+		}
+
+		return false;
 	}
 }
