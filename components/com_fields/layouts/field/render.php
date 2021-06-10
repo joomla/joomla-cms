1866a0e6d29d1a6e21688f9d631db1ccabe05e62
pmleconte <pascal.leconte@conseilgouz.com>
2021-06-10 20:11:12 +0200

    [3.9] Custom Fields : missing value class rendering (#34391)
    when displaying a custom field, render class parameter is missing.

--- components/com_fields/layouts/field/render.php
+++ components/com_fields/layouts/field/render.php
@@ -18,6 +18,7 @@ $label = JText::_($field->label);
 $value = $field->value;
 $showLabel = $field->params->get('showlabel');
 $labelClass = $field->params->get('label_render_class');
+$renderClass = $field->params->get('render_class');
 
 if ($value == '')
 {
@@ -28,4 +29,4 @@ if ($value == '')
 <?php if ($showLabel == 1) : ?>
 	<span class="field-label <?php echo $labelClass; ?>"><?php echo htmlentities($label, ENT_QUOTES | ENT_IGNORE, 'UTF-8'); ?>: </span>
 <?php endif; ?>
-<span class="field-value"><?php echo $value; ?></span>
+<span class="field-value <?php echo $renderClass; ?>"><?php echo $value; ?></span>
