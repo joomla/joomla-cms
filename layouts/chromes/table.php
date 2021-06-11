adb86bce5bdf8c37081633818f4ce81509e0fae5
Сергей <koreshs@mail.ru>
2021-06-11 14:00:02 +0300

    Update table.php

--- layouts/chromes/table.php
+++ layouts/chromes/table.php
@@ -12,9 +12,10 @@ defined('_JEXEC') or die;
 
 $module  = $displayData['module'];
 $params  = $displayData['params'];
+$positionClass = isset($displayData['attribs']['class']) ? ' '.$displayData['attribs']['class'] : '';
 ?>
 <table
-	class="moduletable <?php echo htmlspecialchars($params->get('moduleclass_sfx'), ENT_COMPAT, 'UTF-8'); ?>">
+	class="moduletable <?php echo htmlspecialchars($params->get('moduleclass_sfx'), ENT_COMPAT, 'UTF-8') . $positionClass; ?>">
 	<?php if ((bool) $module->showtitle) : ?>
 		<tr>
 			<th>
