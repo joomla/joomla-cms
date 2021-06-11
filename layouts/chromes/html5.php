de0f610b190c8cfc4b08f3149da1dff9b7cc4f2b
Сергей <koreshs@mail.ru>
2021-06-11 13:39:37 +0300

    Update html5.php

--- layouts/chromes/html5.php
+++ layouts/chromes/html5.php
@@ -23,7 +23,7 @@ if ((string) $module->content === '')
 
 $moduleTag              = htmlspecialchars($params->get('module_tag', 'div'), ENT_QUOTES, 'UTF-8');
 $moduleAttribs          = [];
-$moduleAttribs['class'] = $module->position . ' moduletable '. (isset($attribs['class']) ? $attribs['class'] : '') . ' '
+$moduleAttribs['class'] = $module->position . ' moduletable '. (isset($attribs['class']) ? $attribs['class'].' ' : '') 
 		  	. htmlspecialchars($params->get('moduleclass_sfx'),  ENT_QUOTES, 'UTF-8');
 $bootstrapSize          = (int) $params->get('bootstrap_size', 0);
 $moduleAttribs['class'] .= $bootstrapSize !== 0 ? ' col-md-' . $bootstrapSize : '';
