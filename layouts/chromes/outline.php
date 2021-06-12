75728bbf5f0f3331895acecadd2880b7e3384a36
Сергей <koreshs@mail.ru>
2021-06-13 01:58:13 +0300

    Update outline.php

--- layouts/chromes/outline.php
+++ layouts/chromes/outline.php
@@ -18,7 +18,7 @@ Factory::getApplication()->getDocument()
 
 $module = $displayData['module'];
 ?>
-<div class="mod-preview <?php echo $displayData['attribs']['class'] ?? '' ?>">
+<div class="mod-preview <?php echo $displayData['attribs']['class'] ?>">
 	<div class="mod-preview-info">
 		<div class="mod-preview-position">
 			<?php echo Text::sprintf('JGLOBAL_PREVIEW_POSITION', $module->position); ?>
