5734a37762f209f781038193671d0f65d098f324
Jelle Kok <schumacher1986@hotmail.com>
2021-06-13 12:49:48 +0200

    [4.0] php-cs-fixer v3.0 Support - update exclude files

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -2,7 +2,7 @@
 /**
  * @package    Joomla.Site
  *
- * @copyright  (C) 2016 Open Source Matters, Inc. <https://www.joomla.org>
+ * @copyright  (C) 2021 Open Source Matters, Inc. <https://www.joomla.org>
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 
