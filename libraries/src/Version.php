44bfc41a0fc78f01c6be7ae4d54e7820f576a953
zero-24 <zero-24@users.noreply.github.com>
2021-06-13 14:50:08 +0200

    Joomla! 3.10.0 Alpha7

--- libraries/src/Version.php
+++ libraries/src/Version.php
@@ -60,7 +60,7 @@ final class Version
 	 * @var    string
 	 * @since  3.8.0
 	 */
-	const EXTRA_VERSION = 'alpha7-dev';
+	const EXTRA_VERSION = 'alpha7';
 
 	/**
 	 * Release version.
@@ -78,7 +78,7 @@ final class Version
 	 * @since  3.5
 	 * @deprecated  4.0  Use separated version constants instead
 	 */
-	const DEV_LEVEL = '0-alpha7-dev';
+	const DEV_LEVEL = '0-alpha7';
 
 	/**
 	 * Development status.
@@ -86,7 +86,7 @@ final class Version
 	 * @var    string
 	 * @since  3.5
 	 */
-	const DEV_STATUS = 'Development';
+	const DEV_STATUS = 'Alpha';
 
 	/**
 	 * Build number.
@@ -111,7 +111,7 @@ final class Version
 	 * @var    string
 	 * @since  3.5
 	 */
-	const RELDATE = '30-May-2021';
+	const RELDATE = '13-June-2021';
 
 	/**
 	 * Release time.
@@ -119,7 +119,7 @@ final class Version
 	 * @var    string
 	 * @since  3.5
 	 */
-	const RELTIME = '15:11';
+	const RELTIME = '13:44';
 
 	/**
 	 * Release timezone.
