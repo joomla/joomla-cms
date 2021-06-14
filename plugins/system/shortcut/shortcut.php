8d82ea49b219ed91ebffae4817f012f252b6b26e
Krshivam25 <49578630+Krshivam25@users.noreply.github.com>
2021-06-14 16:58:47 +0530

    Keboard Shortcuts Plugin Setup

---
+++ plugins/system/shortcut/shortcut.php
@@ -0,0 +1,34 @@
+<?php
+// no direct access
+defined( '_JEXEC' ) or die;
+
+use Joomla\CMS\Plugin\CMSPlugin;
+use Joomla\CMS\Language\Text;
+
+/**
+ * Skipto plugin to add accessible keyboard navigation to the site and administrator templates.
+ *
+ * @since  4.1
+ */
+
+class PlgSystemShortcut extends CMSPlugin
+{
+
+    protected $app;
+    protected $_basePath = 'media/plg_system_shortcut';
+    public function onBeforeCompileHead()
+    {
+        if ($this->app->isClient('administrator'))
+        {
+            $wa = $this->app->getDocument()->getWebAssetManager();
+
+            if (!$wa->assetExists('script', 'shortcut'))
+            {
+                $wa->registerScript('shortcut', $this->_basePath . '/js/shortcut.js', [], ['defer' => true]);
+            }
+            $wa->useScript('shortcut');
+            return true;
+        }
+        return true;
+    }
+}
