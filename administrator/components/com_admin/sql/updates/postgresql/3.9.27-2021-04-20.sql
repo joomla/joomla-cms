b4fdaac6ba6fcff03a71ec168d75a760ce87c47e
Richard Fath <richard67@users.noreply.github.com>
2021-06-14 09:06:26 +0200

    Remove 3.9.27 update SQL scripts (#34505)

--- administrator/components/com_admin/sql/updates/postgresql/3.9.27-2021-04-20.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO "#__postinstall_messages" ("extension_id", "title_key", "description_key", "language_extension", "language_client_id", "type", "version_introduced", "enabled")
-VALUES
-(700, 'COM_ADMIN_POSTINSTALL_MSG_FLOC_BLOCKER_TITLE', 'COM_ADMIN_POSTINSTALL_MSG_FLOC_BLOCKER_DESCRIPTION', 'com_admin', 1, 'message', '3.9.27', 1);
