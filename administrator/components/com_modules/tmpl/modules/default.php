3cca4440a7f149dd352b9cea02f4fa88e9900d1b
YatharthVyas <yatharth2000.vyas@gmail.com>
2021-06-14 13:40:11 +0530

    add client id to redirect
    In ModuleController, there is a function called getRedirectToItemAppend which takes the client_id from the get paramaters and appends that to the redirect URL. This used to always return an empty client_id because it wasnt added in the URL of the task.

--- administrator/components/com_modules/tmpl/modules/default.php
+++ administrator/components/com_modules/tmpl/modules/default.php
@@ -132,7 +132,7 @@ if ($saveOrder && !empty($this->items))
 									<?php echo HTMLHelper::_('jgrid.checkedout', $i, $item->editor, $item->checked_out_time, 'modules.', $canCheckin); ?>
 								<?php endif; ?>
 								<?php if ($canEdit) : ?>
-									<a href="<?php echo Route::_('index.php?option=com_modules&task=module.edit&id=' . (int) $item->id); ?>" title="<?php echo Text::_('JACTION_EDIT'); ?> <?php echo $this->escape($item->title); ?>">
+									<a href="<?php echo Route::_('index.php?option=com_modules&task=module.edit&id=' . (int) $item->id . '&client_id=' . $clientId); ?>" title="<?php echo Text::_('JACTION_EDIT'); ?> <?php echo $this->escape($item->title); ?>">
 										<?php echo $this->escape($item->title); ?></a>
 								<?php else : ?>
 									<?php echo $this->escape($item->title); ?>
