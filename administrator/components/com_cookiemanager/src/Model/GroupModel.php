0320fb0415a7f7cb2a74fd231ae3afbf3b37d865
Rishabh Ranjan Jha <rjharishabh@gmail.com>
2021-06-16 22:39:33 +0530

    Group model page

---
+++ administrator/components/com_cookiemanager/src/Model/GroupModel.php
@@ -0,0 +1,119 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_cookiemanager
+ *
+ * @copyright   (C) 2021 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+namespace Joomla\Component\Cookiemanager\Administrator\Model;
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Factory;
+use Joomla\CMS\Form\Form;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\MVC\Model\AdminModel;
+use Joomla\CMS\Table\Table;
+
+/**
+ * Item Model for a group to edit.
+ *
+ * @since  1.6
+ */
+
+class GroupModel extends AdminModel
+{
+	/**
+	 * The prefix to use with controller messages.
+	 *
+	 * @var    string
+	 * @since  1.6
+	 */
+	protected $text_prefix = 'COM_COOKIEMANAGER';
+
+	/**
+	 * Method to get a table object, load it if necessary.
+	 *
+	 * @param   string  $name     The table name. Optional.
+	 * @param   string  $prefix   The class prefix. Optional.
+	 * @param   array   $options  Configuration array for model. Optional.
+	 *
+	 * @return  Table  A Table object
+	 *
+	 * @since   3.0
+	 * @throws  \Exception
+	 */
+	public function getTable($name = '', $prefix = '', $options = array())
+	{
+		$name = 'groups';
+		$prefix = 'Table';
+
+		if ($table = $this->_createTable($name, $prefix, $options))
+		{
+			return $table;
+		}
+
+		throw new \Exception(Text::sprintf('JLIB_APPLICATION_ERROR_TABLE_NAME_NOT_SUPPORTED', $name), 0);
+	}
+
+	/**
+	 * Method to get the record form.
+	 *
+	 * @param   array    $data      Data for the form.
+	 * @param   boolean  $loadData  True if the form is to load its own data (default case), false if not.
+	 *
+	 * @return  Form|boolean  A Form object on success, false on failure
+	 *
+	 * @since   1.6
+	 */
+	public function getForm($data = array(), $loadData = true)
+	{
+		// Get the form.
+		$id=	$this->getState('group.id');
+
+		if ($id===1){
+				$form = $this->loadForm('com_cookiemanager.consent', 'consent', array('control' => 'jform', 'load_data' => $loadData));
+		}
+
+		elseif ($id===2){
+			$form = $this->loadForm('com_cookiemanager.settings', 'settings', array('control' => 'jform', 'load_data' => $loadData));
+		}
+
+		else{
+			$form = $this->loadForm('com_cookiemanager.information', 'information', array('control' => 'jform', 'load_data' => $loadData));
+		}
+
+		if (empty($form))
+		{
+			return false;
+		}
+		// $id = $this->item->id;
+
+		return $form;
+	}
+
+	/**
+	 * Method to get the data that should be injected in the form.
+	 *
+	 * @return  mixed  The data for the form.
+	 *
+	 * @since   1.6
+	 */
+	protected function loadFormData()
+	{
+		// Check the session for previously entered form data.
+		$app = Factory::getApplication();
+		$data = $app->getUserState('com_cookiemanager.edit.group.data', array());
+
+		if (empty($data))
+		{
+			$data = $this->getItem();
+		}
+
+		$this->preprocessData('com_cookiemanager.group', $data);
+
+		return $data;
+	}
+}
