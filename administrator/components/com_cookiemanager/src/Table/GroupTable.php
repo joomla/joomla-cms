1c0eb6981e96b8325c7bfb799f4cbc8a6201fba1
Rishabh Ranjan Jha <rjharishabh@gmail.com>
2021-06-18 02:24:10 +0530

    add bind method in table

--- administrator/components/com_cookiemanager/src/Table/GroupTable.php
+++ administrator/components/com_cookiemanager/src/Table/GroupTable.php
@@ -60,6 +60,28 @@ class GroupTable extends Table implements VersionableTableInterface, TaggableTab
 	}
 
 	/**
+	 * Overloaded bind function
+	 *
+	 * @param   mixed  $array   An associative array or object to bind to the \JTable instance.
+	 * @param   mixed  $ignore  An optional array or space separated list of properties to ignore while binding.
+	 *
+	 * @return  boolean  True on success
+	 *
+	 * @since   1.5
+	 */
+	public function bind($array, $ignore = array())
+	{
+		if (isset($array['params']) && is_array($array['params']))
+		{
+			$registry = new Registry($array['params']);
+
+			$array['params'] = (string) $registry;
+		}
+
+		return parent::bind($array, $ignore);
+	}
+
+	/**
 	 * Stores a contact.
 	 *
 	 * @param   boolean  $updateNulls  True to update fields even if they are null.
