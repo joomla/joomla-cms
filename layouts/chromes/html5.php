fcd8ef1989e1ab3f38080ed8b6776760e5bb5ebb
Сергей <koreshs@mail.ru>
2021-06-19 00:20:25 +0300

    Update html5.php

--- layouts/chromes/html5.php
+++ layouts/chromes/html5.php
@@ -15,6 +15,7 @@ use Joomla\Utilities\ArrayHelper;
 
 $module  = $displayData['module'];
 $params  = $displayData['params'];
+$attribs  = $displayData['attribs'];
 
 if ((string) $module->content === '')
 {
@@ -23,7 +24,7 @@ if ((string) $module->content === '')
 
 $moduleTag              = htmlspecialchars($params->get('module_tag', 'div'), ENT_QUOTES, 'UTF-8');
 $moduleAttribs          = [];
-$moduleAttribs['class'] = $displayData['attribs']['class'] . " $module->position moduletable "
+$moduleAttribs['class'] = "$attribs[class] $module->position moduletable "
 			. htmlspecialchars($params->get('moduleclass_sfx'), ENT_QUOTES, 'UTF-8');
 $bootstrapSize          = (int) $params->get('bootstrap_size', 0);
 $moduleAttribs['class'] .= $bootstrapSize !== 0 ? ' col-md-' . $bootstrapSize : '';
