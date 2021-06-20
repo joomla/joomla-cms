3c51c9dd77e64c280be0146696c5cf149df484ed
Harald Leithner <leithner@itronic.at>
2021-06-20 13:19:03 +0200

    Update Copyright

--- plugins/system/shortcut/shortcut.php
+++ plugins/system/shortcut/shortcut.php
@@ -1,5 +1,12 @@
 <?php
-// no direct access
+/**
+ * @package     Joomla.Plugins
+ * @subpackage  System.shortcut
+ *
+ * @copyright   (C) 2021 Open Source Matters, Inc. <https://www.joomla.org>
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+	 
 defined( '_JEXEC' ) or die;
 
 use Joomla\CMS\Plugin\CMSPlugin;
