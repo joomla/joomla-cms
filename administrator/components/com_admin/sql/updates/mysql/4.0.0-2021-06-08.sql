c5a3d710fdadf3518ea0da82f736e7954eb4ae4f
Rishabh Ranjan Jha <61203226+rjharishabh@users.noreply.github.com>
2021-06-20 18:25:06 +0530

    Update administrator/components/com_admin/sql/updates/mysql/4.0.0-2021-06-08.sql
    Co-authored-by: Richard Fath <richard67@users.noreply.github.com>

--- administrator/components/com_admin/sql/updates/mysql/4.0.0-2021-06-08.sql
+++ administrator/components/com_admin/sql/updates/mysql/4.0.0-2021-06-08.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS `#__cookiemanager_groups` (
   `params` varchar(1024) NOT NULL DEFAULT ''
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 DEFAULT COLLATE utf8mb4_unicode_ci;
 
-INSERT IGNORE INTO `#__cookiemanager_groups` (`title`) VALUES
-('Cookie Consent Banner'),
-('Cookie Settings Banner'),
-('Cookie Consent Information');
+INSERT INTO `#__cookiemanager_groups` (`id`, `title`, `params`) VALUES
+(1, 'Cookie Consent Banner', '{}'),
+(2, 'Cookie Settings Banner', '{}'),
+(3, 'Cookie Consent Information', '{}');
