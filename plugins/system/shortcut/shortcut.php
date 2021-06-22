f24b581b1cacaa1fc343bd77df76a5b99cf1127b
Krshivam25 <49578630+Krshivam25@users.noreply.github.com>
2021-06-22 13:50:06 +0530

    Fix phpcs error in shortcut.php file

--- plugins/system/shortcut/shortcut.php
+++ plugins/system/shortcut/shortcut.php
@@ -6,36 +6,38 @@
  * @copyright   (C) 2021 Open Source Matters, Inc. <https://www.joomla.org>
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
-	 
-defined( '_JEXEC' ) or die;
+
+defined('_JEXEC') or die;
 
 use Joomla\CMS\Plugin\CMSPlugin;
 use Joomla\CMS\Language\Text;
 
 /**
- * Skipto plugin to add accessible keyboard navigation to the site and administrator templates.
+ * Shortcut plugin to add accessible keyboard navigation to the site and administrator templates.
  *
  * @since  __DEPLOY_VERSION__
  */
-
 class PlgSystemShortcut extends CMSPlugin
 {
+	protected $app;
 
-    protected $app;
-    protected $_basePath = 'media/plg_system_shortcut';
-    public function onBeforeCompileHead()
-    {
-        if ($this->app->isClient('administrator'))
-        {
-            $wa = $this->app->getDocument()->getWebAssetManager();
+	protected $_basePath = 'media/plg_system_shortcut';
+	public function onBeforeCompileHead()
+	{
+		if ($this->app->isClient('administrator'))
+		{
+			$wa = $this->app->getDocument()->getWebAssetManager();
 
-            if (!$wa->assetExists('script', 'shortcut'))
-            {
-                $wa->registerScript('shortcut', $this->_basePath . '/js/shortcut.js', [], ['defer' => true , 'type' => 'module']);
-            }
-            $wa->useScript('shortcut');
-            return true;
-        }
-        return true;
-    }
+			if (!$wa->assetExists('script', 'shortcut'))
+			{
+				$wa->registerScript('shortcut', $this->_basePath . '/js/shortcut.js', [], ['defer' => true , 'type' => 'module']);
+			}
+
+			$wa->useScript('shortcut');
+
+			return true;
+		}
+
+		return true;
+	}
 }
