ab50984c6fc00119e93938fa0912c61cc2b564b7
Rishabh Ranjan Jha <rjharishabh@gmail.com>
2021-06-23 13:34:10 +0530

    table and edit cookie - new fields added

--- administrator/components/com_cookiemanager/src/Table/CookieTable.php
+++ administrator/components/com_cookiemanager/src/Table/CookieTable.php
@@ -26,13 +26,6 @@ use Joomla\Registry\Registry;
  */
 class CookieTable extends Table
 {
-	/**
-	 * Ensure the params in json encoded in the bind method
-	 *
-	 * @var    array
-	 * @since   __DEPLOY_VERSION__
-	 */
-	protected $_jsonEncode = array('params');
 
 	/**
 	 * Constructor
@@ -47,25 +40,130 @@ class CookieTable extends Table
 	}
 
 	/**
-	 * Overloaded bind function
+	 * Stores cookies.
 	 *
-	 * @param   mixed  $array   An associative array or object to bind to the \JTable instance.
-	 * @param   mixed  $ignore  An optional array or space separated list of properties to ignore while binding.
+	 * @param   boolean  $updateNulls  True to update fields even if they are null.
 	 *
-	 * @return  boolean  True on success
+	 * @return  boolean  True on success, false on failure.
 	 *
-	 * @since   __DEPLOY_VERSION__
+	 * @since   1.6
 	 */
-	public function bind($array, $ignore = array())
+	public function store($updateNulls = true)
 	{
-		if (isset($array['params']) && is_array($array['params']))
-		{
-			$registry = new Registry($array['params']);
+		$date   = Factory::getDate()->toSql();
+		$userId = Factory::getUser()->id;
 
-			$array['params'] = (string) $registry;
+		// Set created date if not set.
+		if (!(int) $this->created)
+		{
+			$this->created = $date;
 		}
 
-		return parent::bind($array, $ignore);
+		if ($this->id)
+		{
+			$this->modified_by = $userId;
+			$this->modified    = $date;
+		}
+		else
+		{
+			if (empty($this->created_by))
+			{
+				$this->created_by = $userId;
+			}
+
+			if (!(int) $this->modified)
+			{
+				$this->modified = $date;
+			}
+
+			if (empty($this->modified_by))
+			{
+				$this->modified_by = $userId;
+			}
+		}
+
+		// Verify that the alias is unique
+		$table = Table::getInstance('CookieTable', __NAMESPACE__ . '\\', array('dbo' => $this->getDbo()));
+
+		if ($table->load(array('alias' => $this->alias, 'catid' => $this->catid)) && ($table->id != $this->id || $this->id == 0))
+		{
+			$this->setError(Text::_('COM_COOOKIEMANAGER_ERROR_UNIQUE_ALIAS'));
+
+			return false;
+		}
+
+		return parent::store($updateNulls);
 	}
 
+	/**
+	 * Overloaded check function
+	 *
+	 * @return  boolean  True on success, false on failure
+	 *
+	 * @see     \JTable::check
+	 * @since   1.5
+	 */
+	public function check()
+	{
+		try
+		{
+			parent::check();
+		}
+		catch (\Exception $e)
+		{
+			$this->setError($e->getMessage());
+
+			return false;
+		}
+
+		// Check for valid title
+		if (trim($this->title) == '')
+		{
+			$this->setError(Text::_('COM_COOKIEMANAGER_WARNING_PROVIDE_VALID_TITLE'));
+
+			return false;
+		}
+
+		// Generate a valid alias
+		$this->generateAlias();
+
+		// Check for a valid category.
+		if (!$this->catid = (int) $this->catid)
+		{
+			$this->setError(Text::_('JLIB_DATABASE_ERROR_CATEGORY_REQUIRED'));
+
+			return false;
+		}
+
+		// Sanity check for user_id
+		if (!$this->user_id)
+		{
+			$this->user_id = 0;
+		}
+
+		return true;
+	}
+
+	/**
+	 * Generate a valid alias from title / date.
+	 * Remains public to be able to check for duplicated alias before saving
+	 *
+	 * @return  string
+	 */
+	public function generateAlias()
+	{
+		if (empty($this->alias))
+		{
+			$this->alias = $this->title;
+		}
+
+		$this->alias = ApplicationHelper::stringURLSafe($this->alias, $this->language);
+
+		if (trim(str_replace('-', '', $this->alias)) == '')
+		{
+			$this->alias = Factory::getDate()->format('Y-m-d-H-i-s');
+		}
+
+		return $this->alias;
+	}
 }
--- administrator/components/com_cookiemanager/tmpl/cookie/edit.php
+++ administrator/components/com_cookiemanager/tmpl/cookie/edit.php
@@ -19,21 +19,39 @@ use Joomla\Registry\Registry;
 HTMLHelper::_('behavior.formvalidator');
 HTMLHelper::_('behavior.keepalive');
 
-$this->ignore_fieldsets = [];
-$this->useCoreUI = true;
-
 ?>
 
-<form action="<?php echo Route::_('index.php?option=com_cookiemanager&view=group&layout=edit&id=' . (int) $this->item->id); ?>"
-	method="post" name="adminForm" id="group-form" class="form-validate">
+<form action="<?php echo Route::_('index.php?option=com_cookiemanager&layout=edit&id=' . (int) $this->item->id); ?>" method="post" name="adminForm" id="cookie-form" aria-label="<?php echo Text::_('COM_COOKIEMANAGER_FORM_TITLE_' . ((int) $this->item->id === 0 ? 'NEW' : 'EDIT'), true); ?>" class="form-validate">
 
 	<?php echo LayoutHelper::render('joomla.edit.title_alias', $this); ?>
 
-	<div>
-		<?php echo HTMLHelper::_('uitab.startTabSet', 'myTab'); ?>
+	<div class="main-card">
+		<?php echo HTMLHelper::_('uitab.startTabSet', 'myTab', array('active' => 'details')); ?>
 
-		<?php echo LayoutHelper::render('joomla.edit.params', $this); ?>
+		<?php echo HTMLHelper::_('uitab.addTab', 'myTab', 'details', Text::_('COM_COOKIEMANAGER_COOKIES')); ?>
+		<div class="row">
+			<div class="col-lg-8">
+				<?php echo $this->form->renderField('cookies_name'); ?>
+			</div>
+			<div class="col-lg-4">
+				<?php echo LayoutHelper::render('joomla.edit.global', $this); ?>
+			</div>
+		</div>
+		<?php echo HTMLHelper::_('uitab.endTab'); ?>
 
+		<?php echo HTMLHelper::_('uitab.addTab', 'myTab', 'publishing', Text::_('JGLOBAL_FIELDSET_PUBLISHING')); ?>
+		<div class="row">
+			<div class="col-md-6">
+				<fieldset id="fieldset-publishingdata" class="options-form">
+					<legend><?php echo Text::_('JGLOBAL_FIELDSET_PUBLISHING'); ?></legend>
+					<div>
+					<?php echo LayoutHelper::render('joomla.edit.publishingdata', $this); ?>
+					</div>
+				</fieldset>
+			</div>
+		</div>
+		<?php echo HTMLHelper::_('uitab.endTab'); ?>
+		
 		<?php echo HTMLHelper::_('uitab.endTabSet'); ?>
 	</div>
 
