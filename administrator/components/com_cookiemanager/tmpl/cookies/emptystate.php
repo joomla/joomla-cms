01095f6767afc792fcd7a394463e1bd9eb7d7bef
Rishabh Ranjan Jha <rjharishabh@gmail.com>
2021-06-23 08:18:02 +0530

    Add empty state

---
+++ administrator/components/com_cookiemanager/tmpl/cookies/emptystate.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_cookiemanager
+ *
+ * @copyright   (C) 2021 Open Source Matters, Inc. <https://www.joomla.org>
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Factory;
+use Joomla\CMS\Layout\LayoutHelper;
+
+$displayData = [
+	'textPrefix' => 'COM_COOKIEMANAGER_COOKIES',
+	'formURL'    => 'index.php?option=com_cookiemanager',
+	'helpURL'    => 'https://docs.joomla.org/Special:MyLanguage',
+	'icon'       => 'icon-generic cookies',
+];
+
+$user = Factory::getApplication()->getIdentity();
+
+if ($user->authorise('core.create', 'com_cookiemanager') || count($user->getAuthorisedCategories('com_cookiemanager', 'core.create')) > 0)
+{
+	$displayData['createURL'] = 'index.php?option=com_cookiemanager&task=cookie.add';
+}
+
+echo LayoutHelper::render('joomla.content.emptystate', $displayData);
