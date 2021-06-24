fed1b7f96a09a8d5c1fb12fa1f683240b44f8c3c
Rishabh Ranjan Jha <rjharishabh@gmail.com>
2021-06-24 18:58:22 +0530

    add sql in extensions

--- installation/sql/mysql/extensions.sql
+++ installation/sql/mysql/extensions.sql
@@ -882,3 +882,26 @@ CREATE TABLE IF NOT EXISTS `#__action_logs_users` (
   PRIMARY KEY (`user_id`),
   KEY `idx_notify` (`notify`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 DEFAULT COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `#__cookiemanager_cookies`
+--
+
+CREATE TABLE IF NOT EXISTS `#__cookiemanager_cookies` (
+  `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `title` varchar(255) NOT NULL,
+  `alias` varchar(400) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `cookies_name` varchar(400) NOT NULL,
+  `catid` int NOT NULL DEFAULT 0,
+  `published` tinyint NOT NULL DEFAULT 1,
+  `ordering` int NOT NULL DEFAULT 0,
+  `created` datetime NOT NULL,
+  `created_by` int unsigned NOT NULL DEFAULT 0,
+  `modified` datetime NOT NULL,
+  `modified_by` int unsigned NOT NULL DEFAULT 0,
+  KEY `idx_state` (`published`),
+  KEY `idx_catid` (`catid`),
+  KEY `idx_createdby` (`created_by`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 DEFAULT COLLATE utf8mb4_unicode_ci;
