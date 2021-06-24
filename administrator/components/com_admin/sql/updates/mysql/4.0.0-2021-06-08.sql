76cdd2fbe775e465dc7474dead7d8cb6301ff709
Rishabh Ranjan Jha <rjharishabh@gmail.com>
2021-06-24 18:46:32 +0530

    add PostgreSQL

--- administrator/components/com_admin/sql/updates/mysql/4.0.0-2021-06-08.sql
+++ administrator/components/com_admin/sql/updates/mysql/4.0.0-2021-06-08.sql
@@ -1,10 +1,14 @@
+--
+-- Table structure for table `#__cookiemanager_cookies`
+--
+
 CREATE TABLE IF NOT EXISTS `#__cookiemanager_cookies` (
   `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `title` varchar(255) NOT NULL,
   `alias` varchar(400) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
-  `cookies_name` varchar(400),
+  `cookies_name` varchar(400) NOT NULL,
   `catid` int NOT NULL DEFAULT 0,
-  `published` tinyint NOT NULL DEFAULT 0,
+  `published` tinyint NOT NULL DEFAULT 1,
   `ordering` int NOT NULL DEFAULT 0,
   `created` datetime NOT NULL,
   `created_by` int unsigned NOT NULL DEFAULT 0,
