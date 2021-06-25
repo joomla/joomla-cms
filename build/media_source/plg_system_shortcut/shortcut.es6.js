e34f5b8e37e905b566acf6cdde130679beac43a9
Krshivam25 <49578630+Krshivam25@users.noreply.github.com>
2021-06-25 10:15:02 +0530

    Updated Shortcut.es6.js

--- build/media_source/plg_system_shortcut/shortcut.es6.js
+++ build/media_source/plg_system_shortcut/shortcut.es6.js
@@ -10,40 +10,33 @@ class Shortcut {
       Joomla.addShortcut = (event, selector, prevent) => {
         const actionBtn = document.querySelector(selector);
         if (actionBtn) {
-          console.log(selector);
           if (prevent) {
             event.preventDefault();
           }
           actionBtn.click();
         }
       };
-
+      var options="";
+      document.addEventListener('DOMContentLoaded', ()=> {
+        if (Joomla.getOptions('system-shortcut'))
+        {
+           options =Joomla.getOptions('system-shortcut');
+        }
+      })
       function handleKeyPressEvent(e) {
-      // like this maintain a list of all the shortcuts
-      // add a check if the array is already defined, if not then define it.
-        window.Joomla.shortcutsEvent = [{ actionButton: 'meta+alt+s', selector: 'joomla-toolbar-button button.button-apply' },
-          { actionButton: 'meta+alt+n', selector: 'joomla-toolbar-button button.button-new' },
-          { actionButton: 'meta+alt+w', selector: 'joomla-toolbar-button button.button-save' },
-          { actionButton: 'meta+shift+alt+w', selector: 'joomla-toolbar-button button.button-save-new' },
-          { actionButton: 'meta+alt+x', selector: 'joomla-toolbar-button button.button-help' },
-          { actionButton: 'meta+alt+q', selector: 'joomla-toolbar-button button.button-cancel' },
-          { actionButton: 'meta+shift+alt+c', selector: 'joomla-toolbar-button button.button-copy' }];
-
-        // Then map shortcuts
-        window.Joomla.shortcutsEvent.map(({ actionButton, selector }) => {
-          const splitArr = actionButton.split('+');
+        for (var action in options){
           // meta+shift+alt+c => [meta, shift, alt, c]
-          const lastKey = actionButton.charAt(actionButton.length - 1);
+          const lastKey = options[action].keyEvent.charAt(options[action].keyEvent.length - 1);
           if (e.key.toLowerCase() == lastKey) {
-            Joomla.addShortcut(e, selector);
+            Joomla.addShortcut(e, options[action].selector);
           }
-        });
+      }
 
         if (window.navigator.platform.match('Mac') ? e.metaKey : e.altKey) {
         // On Press ALT + S
           const ch = e.key.toLowerCase();
           const list = [{ c: 'joomla-toolbar-button button.button-save-copy' },
-            { x: 'joomla-toolbar-button button.button-help' },
+            { h: 'joomla-toolbar-button button.button-help' },
             { q: 'joomla-toolbar-button button.button-cancel' },
             { s: 'joomla-toolbar-button button.button-apply' },
             { n: 'joomla-toolbar-button button.button-new' },
@@ -57,13 +50,7 @@ class Shortcut {
         }
       }
 
-      window.addEventListener('DOMContentLoaded', (event) => {
-        const shortcuts = Joomla.getOptions('joomla.shortcut');
-        console.log('');
-        if (shortcuts) {
-          Object.keys(shortcuts)
-          .map((shortcut) => Joomla.addShortcut(shortcuts[shortcut], undefined, true, undefined));
-        }
+      window.addEventListener('DOMContentLoaded', () => {
         document.addEventListener(
           'keydown',
           (e) => {
