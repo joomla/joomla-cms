0aa28f180dc1947361cf59cee8a5908235391708
YatharthVyas <yatharth2000.vyas@gmail.com>
2021-06-27 20:56:00 +0530

    js lint

--- build/media_source/plg_system_addmodulebutton/js/addmodulebutton.es6.js
+++ build/media_source/plg_system_addmodulebutton/js/addmodulebutton.es6.js
@@ -2,31 +2,31 @@
  * @copyright   (C) 2021 Open Source Matters, Inc. <https://www.joomla.org>
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
-(document => {
+((document) => {
   'use strict';
 
   document.addEventListener('DOMContentLoaded', () => {
-  	const addModuleBtnLabel = Joomla.getOptions('js-addModuleBtn');
+    const addModuleBtnLabel = Joomla.getOptions('js-addModuleBtn');
     const elMain = document.querySelector('main');
 
     // Create the form node
-		let elForm = document.createElement('form');
-		elForm.setAttribute('method', 'get');
+    const elForm = document.createElement('form');
+    elForm.setAttribute('method', 'get');
 
-		// Hidden input field to pass the get param for place module view
-		let placeModuleFlag = document.createElement('input');
-		placeModuleFlag.setAttribute('type', 'hidden');
-		placeModuleFlag.setAttribute('name', 'pm');
-		placeModuleFlag.setAttribute('value', '1');
-		elForm.appendChild(placeModuleFlag);
+    // Hidden input field to pass the get param for place module view
+    const placeModuleFlag = document.createElement('input');
+    placeModuleFlag.setAttribute('type', 'hidden');
+    placeModuleFlag.setAttribute('name', 'pm');
+    placeModuleFlag.setAttribute('value', '1');
+    elForm.appendChild(placeModuleFlag);
 
-		let addModuleBtn = document.createElement('button');
-		addModuleBtn.setAttribute('type', 'submit');
-		addModuleBtn.classList.add('btn', 'jmodadd');
-		addModuleBtn.innerText = addModuleBtnLabel;
-		elForm.appendChild(addModuleBtn);
+    const addModuleBtn = document.createElement('button');
+    addModuleBtn.setAttribute('type', 'submit');
+    addModuleBtn.classList.add('btn', 'jmodadd');
+    addModuleBtn.innerText = addModuleBtnLabel;
+    elForm.appendChild(addModuleBtn);
 
-		// Append the form
-		elMain.appendChild(elForm);
+    // Append the form
+    elMain.appendChild(elForm);
   });
 })(document);
