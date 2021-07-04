1417be42b28f17cbdf29ee69b2acbc07304c75eb
Krshivam25 <49578630+Krshivam25@users.noreply.github.com>
2021-07-04 22:04:38 +0530

    Added: Key combination features in modal

--- build/media_source/plg_system_shortcut/keyselect.es6.js
+++ build/media_source/plg_system_shortcut/keyselect.es6.js
@@ -22,28 +22,43 @@ class JoomlaShortcutModal {
 		
 		const modal_div = document.createElement("div");
 		this.setAttributes(modal_div, {"class": "modal fade", "id": "keySelectModal", "tabindex": "-1", "role": "dialog", "aria-labelledby": "keySelectModalLabel", "aria-hidden": "true"});
-		modal_div.innerHTML = '<div class="modal-dialog" role="document"><div class="modal-content"><div class="modal-header"><h5 class="modal-title" id="keySelectModalLabel">'+Joomla.getOptions('modal_header_title')+'</h5><button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button></div><div class="modal-body"><div class="p-3"><p>'+Joomla.getOptions('modal_combination_text')+' <span id="currentKeyCombination"></span></p><div class="form-group"><input id="keyCombination" class="form-control" type="text" autocomplete="off" maxlength="1" onkeypress="return /[a-z]/i.test(event.key)" /><input type="hidden" id="current_KeyEvent" value="" /></div></div></div><div class="modal-footer"></div></div></div>';
+		modal_div.innerHTML = '<div class="modal-dialog" role="document"><div class="modal-content"><div class="modal-header"><h5 class="modal-title" id="keySelectModalLabel">'+Joomla.getOptions('modal_header_title')+'</h5><button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button></div><div class="modal-body"><div class="p-3"><p>'+Joomla.getOptions('modal_combination_text')+' <span id="currentKeyCombination"></span></p><div class="form-group"><input id="keyCombination" class="form-control" type="text" autocomplete="off" maxlength="1" onkeypress="return /[a-z]/i.test(event.key)" /><input type="hidden" id="current_KeyEvent" value="" /></div></div></div><div class="modal-footer"  <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button> <button type="button" class="btn btn-primary">Save changes</button> </div></div></div>';
 		document.body.appendChild(modal_div); 
-		
+		Joomla.initialiseModal(keySelectModal);
 		const keyCombination = document.getElementById("keyCombination");
-		if (typeof keyCombination.addEventListener != "undefined") {
-			keyCombination.addEventListener("keyup", function (event) {
-				if(event.keyCode >= 65 && event.keyCode <= 90){
-					this.value = this.value.toUpperCase();
-					const currentKeyCombinationContent = document.getElementById("currentKeyCombination").innerHTML;
-					const currentKeyCombinationArr=currentKeyCombinationContent.split(' + ');
-					
-					currentKeyCombinationArr[currentKeyCombinationArr.length-1] = this.value;
-					const newKeySelect = currentKeyCombinationArr.join(' + ');
-					const current_KeyEventVal = document.getElementById("current_KeyEvent").value;
-					
-					document.getElementById("jform_params_"+current_KeyEventVal+"_keySelect_btn").innerHTML = newKeySelect;
-					document.getElementById("jform_params_"+current_KeyEventVal+"_keySelect").value = newKeySelect;
-					document.getElementById("jform_params_"+current_KeyEventVal+"_keyEvent").value = this.value.toLowerCase();
-					
-					window.bootstrap.Modal.getInstance(keySelectModal).hide();
+		keyCombination.onkeyup = function (event) {
+			if(event.keyCode >= 65 && event.keyCode <= 90){
+				
+				let keyValue = event.key;
+				keyValue = keyValue.toUpperCase();
+				const current_KeyEventVal = document.getElementById("current_KeyEvent").value;
+				document.getElementById("jform_params_"+current_KeyEventVal+"_hasControl").value = 0;
+				document.getElementById("jform_params_"+current_KeyEventVal+"_hasShift").value = 0;
+				document.getElementById("jform_params_"+current_KeyEventVal+"_hasAlt").value = 0;
+				const newKeySelectCombination = new Array();
+				if(event.ctrlKey){
+					newKeySelectCombination.push("CTRL");	
+					document.getElementById("jform_params_"+current_KeyEventVal+"_hasControl").value = 1;
 				}
-			});
+				if(event.shiftKey){
+					newKeySelectCombination.push("SHIFT");	
+					document.getElementById("jform_params_"+current_KeyEventVal+"_hasShift").value = 1;
+				}
+				if(navigator.platform.match('Mac') ? event.metaKey : event.altKey){
+					newKeySelectCombination.push("ALT");	
+					document.getElementById("jform_params_"+current_KeyEventVal+"_hasAlt").value = 1;
+				}
+				newKeySelectCombination.push(keyValue);
+				document.getElementById("jform_params_"+current_KeyEventVal+"_keyEvent").value = keyValue.toLowerCase();
+				const newKeySelect = newKeySelectCombination.join(' + ');
+				
+				
+				document.getElementById("jform_params_"+current_KeyEventVal+"_keySelect_btn").innerHTML = newKeySelect;
+				document.getElementById("jform_params_"+current_KeyEventVal+"_keySelect").value = newKeySelect;
+				
+				
+				window.bootstrap.Modal.getInstance(keySelectModal).hide();
+			}
 		}
 		//
 	}
@@ -57,7 +72,7 @@ class JoomlaShortcutModal {
 		document.getElementById("currentKeyCombination").innerHTML = this.textContent;
 		document.getElementById("current_KeyEvent").value = this.getAttribute('data-class');
 		const keySelectModal = document.getElementById("keySelectModal");
-		Joomla.initialiseModal(keySelectModal);
+		
 		window.bootstrap.Modal.getInstance(keySelectModal).show(keySelectModal);
 			
 	}
--- plugins/system/shortcut/shortcut.php
+++ plugins/system/shortcut/shortcut.php
@@ -36,115 +36,113 @@ class PlgSystemShortcut extends CMSPlugin
 	 * @since  4.1
 	 */
 	protected $_basePath = 'media/plg_system_shortcut';
-
-	public function onBeforeCompileHead()
-	{
-		if ($this->app->isClient('administrator'))
-		{
+	
+	public function onBeforeCompileHead(){
+		if ($this->app->isClient('administrator')){
 			$joomla_shortcut_keys = array(
 				'new' => array(
 				  'keyEvent' => $this->params->get('new_keyEvent', 'n'),
-				  'hasShift' => 0,
-				  'hasAlt' => 1,
-				  'hasControl' => 0,
+				  'hasShift' => $this->params->get('new_hasShift', 0),
+				  'hasAlt' => $this->params->get('new_hasAlt', 1),
+				  'hasControl' => $this->params->get('new_hasControl', 0),
 				  'selector' => 'joomla-toolbar-button button.button-new'
 				),
 				'apply' => array(
 				  'keyEvent' => $this->params->get('apply_keyEvent', 's'),
-				  'hasShift' => 0,
-				  'hasAlt' => 1,
-				  'hasControl' => 0,
+				  'hasShift' => $this->params->get('apply_hasShift', 0),
+				  'hasAlt' => $this->params->get('apply_hasAlt', 1),
+				  'hasControl' => $this->params->get('apply_hasControl', 0),
 				  'selector' => 'joomla-toolbar-button button.button-apply'
 				),
 				'save' => array(
 				  'keyEvent' => $this->params->get('save_keyEvent', 'w'),
-				  'hasShift' => 0,
-				  'hasAlt' => 1,
-				  'hasControl' => 0,
+				  'hasShift' => $this->params->get('save_hasShift', 0),
+				  'hasAlt' => $this->params->get('save_hasAlt', 1),
+				  'hasControl' => $this->params->get('save_hasControl', 0),
 				  'selector' => 'joomla-toolbar-button button.button-save'
 				),
 				'saveNew' => array(
 				  'keyEvent' => $this->params->get('saveNew_keyEvent', 'n'),
-				  'hasShift' => 1,
-				  'hasAlt' => 1,
-				  'hasControl' => 0,
+				  'hasShift' => $this->params->get('saveNew_hasShift', 1),
+				  'hasAlt' => $this->params->get('saveNew_hasAlt', 1),
+				  'hasControl' => $this->params->get('saveNew_hasControl', 0),
 				  'selector' => 'joomla-toolbar-button button.button-save-new'
 				),
 				'help' => array(
 				  'keyEvent' => $this->params->get('help_keyEvent', 'x'),
-				  'hasShift' => 0,
-				  'hasAlt' => 1,
-				  'hasControl' => 0,
+				  'hasShift' => $this->params->get('help_hasShift', 0),
+				  'hasAlt' => $this->params->get('help_hasAlt', 1),
+				  'hasControl' => $this->params->get('help_hasControl', 0),
 				  'selector' => 'joomla-toolbar-button button.button-help'
 				),
 				'cancel' => array(
 				  'keyEvent' => $this->params->get('cancel_keyEvent', 'q'),
-				  'hasShift' => 0,
-				  'hasAlt' => 1,
-				  'hasControl' => 0,
+				  'hasShift' => $this->params->get('cancel_hasShift', 0),
+				  'hasAlt' => $this->params->get('cancel_hasAlt', 1),
+				  'hasControl' => $this->params->get('cancel_hasControl', 0),
 				  'selector' => 'joomla-toolbar-button button.button-cancel'
 				),
 				'copy' => array(
 				  'keyEvent' => $this->params->get('copy_keyEvent', 'c'),
-				  'hasShift' => 1,
-				  'hasAlt' => 1,
-				  'hasControl' => 0,
+				  'hasShift' => $this->params->get('copy_hasShift', 1),
+				  'hasAlt' => $this->params->get('copy_hasAlt', 1),
+				  'hasControl' => $this->params->get('copy_hasControl', 0),
 				  'selector' => 'joomla-toolbar-button button.button-button-copy'
 				),
 				'article' => array(
 				  'keyEvent' => $this->params->get('article_keyEvent', 'a'),
-				  'hasShift' => 0,
-				  'hasAlt' => 1,
-				  'hasControl' => 1,
+				  'hasShift' => $this->params->get('article_hasShift', 0),
+				  'hasAlt' => $this->params->get('article_hasAlt', 1),
+				  'hasControl' => $this->params->get('article_hasControl', 1),
 				  'selector' => 'joomla-editor-option~article_modal'
 				),
 				'contact' => array(
 				  'keyEvent' => $this->params->get('contact_keyEvent', 'c'),
-				  'hasShift' => 0,
-				  'hasAlt' => 1,
-				  'hasControl' => 1,
+				  'hasShift' => $this->params->get('contact_hasShift', 0),
+				  'hasAlt' => $this->params->get('contact_hasAlt', 1),
+				  'hasControl' => $this->params->get('contact_hasControl', 1),
 				  'selector' => 'joomla-editor-option~contact_modal'
 				),
 				'fields' => array(
 				  'keyEvent' => $this->params->get('fields_keyEvent', 'f'),
-				  'hasShift' => 0,
-				  'hasAlt' => 1,
-				  'hasControl' => 1,
+				  'hasShift' => $this->params->get('fields_hasShift', 0),
+				  'hasAlt' => $this->params->get('fields_hasAlt', 1),
+				  'hasControl' => $this->params->get('fields_hasControl', 1),
 				  'selector' => 'joomla-editor-option~fields_modal'
 				),
 				'image' => array(
 				  'keyEvent' => $this->params->get('image_keyEvent', 'i'),
-				  'hasShift' => 0,
-				  'hasAlt' => 1,
-				  'hasControl' => 1,
+				  'hasShift' => $this->params->get('image_hasShift', 0),
+				  'hasAlt' => $this->params->get('image_hasAlt', 1),
+				  'hasControl' => $this->params->get('image_hasControl', 1),
 				  'selector' => 'joomla-editor-option~image_modal'
 				),
 				'menu' => array(
 				  'keyEvent' => $this->params->get('menu_keyEvent', 'm'),
-				  'hasShift' => 0,
-				  'hasAlt' => 1,
-				  'hasControl' => 1,
+				  'hasShift' => $this->params->get('menu_hasShift', 0),
+				  'hasAlt' => $this->params->get('menu_hasAlt', 1),
+				  'hasControl' => $this->params->get('menu_hasControl', 1),
 				  'selector' => 'joomla-editor-option~menu_modal'
 				),
 				'module' => array(
 				  'keyEvent' => $this->params->get('module_keyEvent', 'm'),
-				  'hasShift' => 1,
-				  'hasAlt' => 1,
-				  'hasControl' => 1,
+				  'hasShift' => $this->params->get('module_hasShift', 1),
+				  'hasAlt' => $this->params->get('module_hasAlt', 1),
+				  'hasControl' => $this->params->get('module_hasControl', 1),
 				  'selector' => 'joomla-editor-option~module_modal'
 				),
 				'pagebreak' => array(
 				  'keyEvent' => $this->params->get('pagebreak_keyEvent', 'p'),
-				  'hasShift' => 0,
-				  'hasAlt' => 1,
-				  'hasControl' => 1,
+				  'hasShift' => $this->params->get('pagebreak_hasShift', 0),
+				  'hasAlt' => $this->params->get('pagebreak_hasAlt', 1),
+				  'hasControl' => $this->params->get('pagebreak_hasControl', 1),
 				  'selector' => 'joomla-editor-option~pagebreak_modal'
 				),
 				'readmore' => array(
 				  'keyEvent' => $this->params->get('readmore_keyEvent', 'r'),
-				  'hasShift' => 0,
-				  'hasAlt' => 1,
-				  'hasControl' => 1,
+				  'hasShift' => $this->params->get('readmore_hasShift', 0),
+				  'hasAlt' => $this->params->get('readmore_hasAlt', 1),
+				  'hasControl' => $this->params->get('readmore_hasControl', 1),
 				  'selector' => 'joomla-editor-option~read_more'
 				)
 			);
@@ -153,17 +151,12 @@ class PlgSystemShortcut extends CMSPlugin
 			$editor = $config->get('editor', 'tinymce');
 			Factory::getDocument()->addScriptOptions('editor', $editor);
 			Factory::getDocument()->addScriptOptions('joomla-shortcut-keys', $joomla_shortcut_keys);
-
-			if (!$wa->assetExists('script', 'shortcut'))
-			{
+			if (!$wa->assetExists('script', 'shortcut')){
 				$wa->registerScript('shortcut', $this->_basePath . '/js/shortcut.js', [], ['defer' => true , 'type' => 'module']);
 			}
-
 			$wa->useScript('shortcut');
-
 			return true;
 		}
-
 		return true;
 	}
 }
