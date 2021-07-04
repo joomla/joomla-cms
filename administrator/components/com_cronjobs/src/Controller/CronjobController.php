b2f1d9462182148d687471d90133acf7e0c86478
dst27 <72784348+dst27@users.noreply.github.com>
2021-07-05 03:36:15 +0530

    Lay out the MVC to deal with single entries
    Adds a base for MVC classes meant to handle single 'Cronjob' entries.

---
+++ administrator/components/com_cronjobs/src/Controller/CronjobController.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Declares the MVC controller for CronjobModel.
+ *
+ * @package     Joomla.Administrator
+ * @subpackage  com_cronjobs
+ *
+ * @copyright   (C) 2021 Open Source Matters, Inc. <https://www.joomla.org>
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// Restrict direct access
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\MVC\Controller\FormController;
+
+/**
+ * The CronjobModel controller.
+ *
+ * @since __DEPLOY_VERSION__
+ */
+class CronjobController extends FormController
+{
+	// ! TODO
+}
---
+++ administrator/components/com_cronjobs/src/Model/CronjobModel.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Declares the CronjobModel MVC Model.
+ *
+ * @package Joomla.Administrator
+ * @subpackage com_cronjobs
+ *
+ * @copyright   (C) 2021 Open Source Matters, Inc. <https://www.joomla.org>
+ * @license	    GPL v3
+ */
+
+namespace Joomla\Component\Cronjobs\Administrator\Model;
+
+// Restrict direct access
+\defined('_JEXEC_') or die;
+
+use Joomla\CMS\MVC\Model\AdminModel;
+
+/**
+ * MVC Model to deal operations concerning a single 'Cronjob' entry.
+ *
+ * @since __DEPLOY_VERSION__
+ */
+class CronjobModel extends AdminModel
+{
+	// ! TODO
+}
