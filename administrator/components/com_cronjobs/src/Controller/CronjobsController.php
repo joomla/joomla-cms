f390e33a3929cbef9287e38d69a2ba4e5aaf92ec
dst27 <72784348+dst27@users.noreply.github.com>
2021-07-05 03:40:01 +0530

    Lay out the MVC to deal with multiple entries
    Adds a base for MVC classes meant to handle multiple 'Cronjob' entries.

---
+++ administrator/components/com_cronjobs/src/Controller/CronjobsController.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Declares the MVC controller for CronJobsModel.
+ *
+ * @package     Joomla.Administrator
+ * @subpackage  com_cronjobs
+ *
+ * @copyright   (C) 2021 Open Source Matters, Inc. <https://www.joomla.org>
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// Restrict direct access
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\MVC\Controller\AdminController;
+
+/**
+ * The CronjobsModel controller.
+ *
+ * @since __DEPLOY_VERSION__
+ */
+class CronjobsController extends AdminController
+{
+	// ! TODO
+}
---
+++ administrator/components/com_cronjobs/src/Model/CronjobsModel.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Declares the CronjobsModel MVC Model.
+ *
+ * @package Joomla.Administrator
+ * @subpackage com_cronjobs
+ *
+ * @copyright   (C) 2021 Open Source Matters, Inc. <https://www.joomla.org>
+ * @license	    GPL v3
+ */
+
+namespace Joomla\Component\Cronjobs\Administrator\Model;
+
+// Restrict direct access
+\defined('_JEXEC_') or die;
+
+use Joomla\CMS\MVC\Model\ListModel;
+
+/**
+ * MVC Model to deal with operations concerning multiple 'Cronjob' entries.
+ *
+ * @since __DEPLOY_VERSION__
+ */
+class CronjobsModel extends ListModel
+{
+	// ! TODO
+}
