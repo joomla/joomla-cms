edf8531e554a9cbb83825852706bf5c6cadb186b
dst27 <72784348+dst27@users.noreply.github.com>
2021-07-09 03:10:17 +0530

    Add path for SQL updates to manifest

--- administrator/components/com_cronjobs/sql/updates/mysql/08-07-2021.mysql.sql
+++ administrator/components/com_cronjobs/sql/updates/mysql/08-07-2021.mysql.sql
@@ -1,6 +1,5 @@
 ALTER TABLE `#__cronjobs`
     ADD COLUMN `asset_id` INT(10) NOT NULL DEFAULT '0';
-
 ALTER TABLE `#__cronjobs`
     ADD COLUMN `created` INT(10) NOT NULL DEFAULT '0';
 ALTER TABLE `#__cronjobs`
