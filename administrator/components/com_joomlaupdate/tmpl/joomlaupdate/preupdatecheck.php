5c6dacd66a4361b0d00051285d46ae8d08174c5c
Benjamin Trenkle <benjamin.trenkle@wicked-software.de>
2021-07-08 14:00:12 +0200

    Add icon to precheck headlines Update layout

--- administrator/components/com_joomlaupdate/tmpl/joomlaupdate/preupdatecheck.php
+++ administrator/components/com_joomlaupdate/tmpl/joomlaupdate/preupdatecheck.php
@@ -32,27 +32,32 @@ Factory::getDocument()->addScriptOptions('COM_JOOMLAUPDATE_VIEW_DEFAULT_EXTENSIO
 
 $compatibilityTypes = array(
 	'COM_JOOMLAUPDATE_VIEW_DEFAULT_EXTENSIONS_RUNNING_PRE_UPDATE_CHECKS' => array(
-		'class' => 'alert-secondary',
+		'class' => 'info',
+		'icon' => 'hourglass fa-spin',
 		'notes' => 'COM_JOOMLAUPDATE_VIEW_DEFAULT_EXTENSIONS_RUNNING_PRE_UPDATE_CHECKS_NOTES',
 		'group' => 0,
 	),
+	'COM_JOOMLAUPDATE_VIEW_DEFAULT_EXTENSIONS_REQUIRING_UPDATES_TO_BE_COMPATIBLE' => array(
+		'class' => 'danger',
+		'icon' => 'times',
+		'notes' => 'COM_JOOMLAUPDATE_VIEW_DEFAULT_EXTENSIONS_REQUIRING_UPDATES_TO_BE_COMPATIBLE_NOTES',
+		'group' => 2,
+	),
 	'COM_JOOMLAUPDATE_VIEW_DEFAULT_EXTENSIONS_PRE_UPDATE_CHECKS_FAILED' => array(
-		'class' => 'alert-danger',
+		'class' => 'warning',
+		'icon' => 'exclamation-triangle',
 		'notes' => 'COM_JOOMLAUPDATE_VIEW_DEFAULT_EXTENSIONS_PRE_UPDATE_CHECKS_FAILED_NOTES',
 		'group' => 4,
 	),
 	'COM_JOOMLAUPDATE_VIEW_DEFAULT_EXTENSIONS_UPDATE_SERVER_OFFERS_NO_COMPATIBLE_VERSION' => array(
-		'class' => 'alert-danger',
+		'class' => 'warning',
+		'icon' => 'exclamation-triangle',
 		'notes' => 'COM_JOOMLAUPDATE_VIEW_DEFAULT_EXTENSIONS_UPDATE_SERVER_OFFERS_NO_COMPATIBLE_VERSION_NOTES',
 		'group' => 1,
 	),
-	'COM_JOOMLAUPDATE_VIEW_DEFAULT_EXTENSIONS_REQUIRING_UPDATES_TO_BE_COMPATIBLE' => array(
-		'class' => 'alert-warning',
-		'notes' => 'COM_JOOMLAUPDATE_VIEW_DEFAULT_EXTENSIONS_REQUIRING_UPDATES_TO_BE_COMPATIBLE_NOTES',
-		'group' => 2,
-	),
 	'COM_JOOMLAUPDATE_VIEW_DEFAULT_EXTENSIONS_PROBABLY_COMPATIBLE' => array(
-		'class' => 'alert-success',
+		'class' => 'success',
+		'icon' => 'check',
 		'notes' => 'COM_JOOMLAUPDATE_VIEW_DEFAULT_EXTENSIONS_PROBABLY_COMPATIBLE_NOTES',
 		'group' => 3,
 	),
@@ -107,90 +112,94 @@ $updatePossible = true;
 		</div>
 
 		<div class="tab-content w-100">
-			<div class="tab-pane fade show active table-responsive" id="joomlaupdate-precheck-required-content" role="tabpanel" aria-labelledby="joomlaupdate-precheck-required-tab">
+			<div class="tab-pane fade show active" id="joomlaupdate-precheck-required-content" role="tabpanel" aria-labelledby="joomlaupdate-precheck-required-tab">
 				<h3>
 					<?php echo Text::_('COM_JOOMLAUPDATE_PREUPDATE_REQUIRED_SETTINGS'); ?>
 				</h3>
-				<table class="table table-striped" id="preupdatecheck">
-					<caption class="visually-hidden">
-						<?php echo Text::_('COM_JOOMLAUPDATE_PREUPDATE_CHECK_CAPTION'); ?>
-					</caption>
-					<thead>
-						<tr>
-							<th scope="col">
-								<?php echo Text::_('COM_JOOMLAUPDATE_PREUPDATE_HEADING_REQUIREMENT'); ?>
-							</th>
-							<th scope="col">
-								<?php echo Text::_('COM_JOOMLAUPDATE_PREUPDATE_HEADING_CHECKED'); ?>
-							</th>
-						</tr>
-					</thead>
-					<tbody>
-					<?php foreach ($this->phpOptions as $option) : ?>
-						<tr>
-							<th scope="row">
-								<?php echo $option->label; ?>
-								<?php if ($option->notice) : ?>
-								<div class="small">
-									<?php echo $option->notice; ?>
-								</div>
-								<?php endif; ?>
-							</th>
-							<td>
-								<span class="badge bg-<?php echo $option->state ? 'success' : 'danger'; ?>">
-									<?php echo Text::_($option->state ? 'JYES' : 'JNO'); ?>
-								</span>
-							</td>
-						</tr>
-					<?php endforeach; ?>
-					</tbody>
-				</table>
-			</div>
-			<div class="tab-pane fade show table-responsive" id="joomlaupdate-precheck-recommended-content" role="tabpanel" aria-labelledby="joomlaupdate-precheck-recommended-tab">
-				<h3>
-					<?php echo Text::_('COM_JOOMLAUPDATE_PREUPDATE_RECOMMENDED_SETTINGS'); ?>
-				</h3>
-				<table class="table table-striped" id="preupdatecheckphp">
-					<caption>
-						<?php echo Text::_('COM_JOOMLAUPDATE_VIEW_DEFAULT_RECOMMENDED_SETTINGS_DESC'); ?>
-					</caption>
-					<thead>
-						<tr>
-							<th scope="col">
-								<?php echo Text::_('COM_JOOMLAUPDATE_VIEW_DEFAULT_DIRECTIVE'); ?>
-							</th>
-							<th scope="col">
-								<?php echo Text::_('COM_JOOMLAUPDATE_VIEW_DEFAULT_RECOMMENDED'); ?>
-							</th>
-							<th scope="col">
-								<?php echo Text::_('COM_JOOMLAUPDATE_VIEW_DEFAULT_ACTUAL'); ?>
-							</th>
-						</tr>
-					</thead>
-					<tbody>
-						<?php foreach ($this->phpSettings as $setting) : ?>
+				<div class="table-responsive">
+					<table class="table table-striped" id="preupdatecheck">
+						<caption class="visually-hidden">
+							<?php echo Text::_('COM_JOOMLAUPDATE_PREUPDATE_CHECK_CAPTION'); ?>
+						</caption>
+						<thead>
+							<tr>
+								<th scope="col">
+									<?php echo Text::_('COM_JOOMLAUPDATE_PREUPDATE_HEADING_REQUIREMENT'); ?>
+								</th>
+								<th scope="col">
+									<?php echo Text::_('COM_JOOMLAUPDATE_PREUPDATE_HEADING_CHECKED'); ?>
+								</th>
+							</tr>
+						</thead>
+						<tbody>
+						<?php foreach ($this->phpOptions as $option) : ?>
 							<tr>
 								<th scope="row">
-									<?php echo $setting->label; ?>
+									<?php echo $option->label; ?>
+									<?php if ($option->notice) : ?>
+									<div class="small">
+										<?php echo $option->notice; ?>
+									</div>
+									<?php endif; ?>
 								</th>
 								<td>
-									<?php echo Text::_($setting->recommended ? 'JON' : 'JOFF'); ?>
-								</td>
-								<td>
-									<span class="badge bg-<?php echo ($setting->state === $setting->recommended) ? 'success' : 'warning'; ?>">
-										<?php echo Text::_($setting->state ? 'JON' : 'JOFF'); ?>
+									<span class="badge bg-<?php echo $option->state ? 'success' : 'danger'; ?>">
+										<?php echo Text::_($option->state ? 'JYES' : 'JNO'); ?>
 									</span>
 								</td>
 							</tr>
 						<?php endforeach; ?>
-					</tbody>
-				</table>
+						</tbody>
+					</table>
+				</div>
+			</div>
+			<div class="tab-pane fade show" id="joomlaupdate-precheck-recommended-content" role="tabpanel" aria-labelledby="joomlaupdate-precheck-recommended-tab">
+				<h3>
+					<?php echo Text::_('COM_JOOMLAUPDATE_PREUPDATE_RECOMMENDED_SETTINGS'); ?>
+				</h3>
+				<div class="table-responsive">
+					<table class="table table-striped" id="preupdatecheckphp">
+						<caption>
+							<?php echo Text::_('COM_JOOMLAUPDATE_VIEW_DEFAULT_RECOMMENDED_SETTINGS_DESC'); ?>
+						</caption>
+						<thead>
+							<tr>
+								<th scope="col">
+									<?php echo Text::_('COM_JOOMLAUPDATE_VIEW_DEFAULT_DIRECTIVE'); ?>
+								</th>
+								<th scope="col">
+									<?php echo Text::_('COM_JOOMLAUPDATE_VIEW_DEFAULT_RECOMMENDED'); ?>
+								</th>
+								<th scope="col">
+									<?php echo Text::_('COM_JOOMLAUPDATE_VIEW_DEFAULT_ACTUAL'); ?>
+								</th>
+							</tr>
+						</thead>
+						<tbody>
+							<?php foreach ($this->phpSettings as $setting) : ?>
+								<tr>
+									<th scope="row">
+										<?php echo $setting->label; ?>
+									</th>
+									<td>
+										<?php echo Text::_($setting->recommended ? 'JON' : 'JOFF'); ?>
+									</td>
+									<td>
+										<span class="badge bg-<?php echo ($setting->state === $setting->recommended) ? 'success' : 'warning'; ?>">
+											<?php echo Text::_($setting->state ? 'JON' : 'JOFF'); ?>
+										</span>
+									</td>
+								</tr>
+							<?php endforeach; ?>
+						</tbody>
+					</table>
+				</div>
 			</div>
 			<div class="tab-pane fade show" id="joomlaupdate-precheck-extensions-content" role="tabpanel" aria-labelledby="joomlaupdate-precheck-extensions-tab">
 				<h3>
 					<?php echo Text::_('COM_JOOMLAUPDATE_VIEW_DEFAULT_EXTENSIONS'); ?>
 				</h3>
-				<div id="preupdateCheckWarning" class="hidden">
+				<div id="preupdateCheckWarning">
 					<div class="alert alert-warning">
 						<h4 class="alert-heading">
 							<?php echo Text::_('WARNING'); ?>
@@ -198,19 +207,6 @@ $updatePossible = true;
 						<div class="alert-message">
 							<div class="preupdateCheckIncomplete">
 								<?php echo Text::_('COM_JOOMLAUPDATE_PREUPDATE_CHECK_NOT_COMPLETE'); ?>
-							</div>
-						</div>
-					</div>
-				</div>
-
-				<div id="preupdateconfirmation" class="hidden">
-					<div class="alert alert-info">
-						<h4 class="alert-heading">
-							<?php echo Text::_('INFO'); ?>
-						</h4>
-						<div class="alert-message">
-							<div class="preupdateCheckComplete">
-								<?php echo Text::_('COM_JOOMLAUPDATE_VIEW_DEFAULT_NON_CORE_PLUGIN_BEING_CHECKED'); ?>
 							</div>
 						</div>
 					</div>
@@ -230,29 +226,28 @@ $updatePossible = true;
 				</div>
 
 			<?php if (!empty($this->nonCoreExtensions)) : ?>
-				<div class="w-100 table-responsive">
-					<?php foreach ($compatibilityTypes as $compatibilityType => $compatibilityData) : ?>
-						<?php $compatibilityDisplayClass = $compatibilityData['class']; ?>
-						<?php $compatibilityDisplayNotes = $compatibilityData['notes']; ?>
-						<?php $compatibilityTypeGroup    = $compatibilityData['group']; ?>
-						<fieldset id="compatibilitytype<?php echo $compatibilityTypeGroup;?>" class="col-md-12 compatibilitytypes">
-							<legend>
-								<h3 class="alert <?php echo $compatibilityDisplayClass; ?>">
-									<?php if ($compatibilityType !== "COM_JOOMLAUPDATE_VIEW_DEFAULT_EXTENSIONS_RUNNING_PRE_UPDATE_CHECKS") : ?>
-										<div class="compatibilitytoggle" data-state="closed">
-											<?php echo Text::sprintf(
-												'COM_JOOMLAUPDATE_VIEW_DEFAULT_EXTENSIONS_SHOW_MORE_COMPATIBILITY_INFORMATION',
-												'<span class="icon-chevron-right"></span>'
-											); ?>
-										</div>
-									<?php endif; ?>
-									<?php echo Text::_($compatibilityType); ?>
-								</h3>
-							</legend>
-							<div class="compatibilityNotes">
-								<?php echo Text::_($compatibilityDisplayNotes); ?>
+				<div class="w-100">
+					<?php foreach ($compatibilityTypes as $compatibilityType => $data) : ?>
+						<h3 class="text-<?php echo $data['class']; ?>">
+							<?php if ($data['group'] > 0) : ?>
+								<?php /*<div class="compatibilitytoggle" data-state="closed">
+									<?php echo Text::sprintf(
+										'COM_JOOMLAUPDATE_VIEW_DEFAULT_EXTENSIONS_SHOW_MORE_COMPATIBILITY_INFORMATION',
+										'<span class="icon-chevron-right"></span>'
+									); ?>
+								</div> */ ?>
+							<?php endif; ?>
+							<span class="fa fa-<?php echo $data['icon']; ?> me-2"></span>
+							<?php echo Text::_($compatibilityType); ?>
+						</h3>
+
+						<div class="compatibilityNotes">
+							<div class="alert alert-info">
+							<?php echo Text::_($data['notes']); ?>
 							</div>
-							<table class="table">
+						</div>
+						<div class="table-responsive mb-5">
+							<table class="table table-striped">
 								<thead class="row-fluid">
 									<tr>
 										<th class="exname col-md-8">
@@ -274,7 +269,7 @@ $updatePossible = true;
 								</thead>
 								<tbody class="row-fluid">
 								<?php // Only include this row once since the javascript moves the results into the right place ?>
-								<?php if ($compatibilityType == "COM_JOOMLAUPDATE_VIEW_DEFAULT_EXTENSIONS_RUNNING_PRE_UPDATE_CHECKS") : ?>
+								<?php if ($data['group'] == 0) : ?>
 									<?php foreach ($this->nonCoreExtensions as $extension) : ?>
 										<tr>
 											<td class="exname col-md-8">
@@ -299,7 +294,7 @@ $updatePossible = true;
 								<?php endif; ?>
 								</tbody>
 							</table>
-						</fieldset>
+						</div>
 					<?php endforeach; ?>
 				</div>
 			<?php else: ?>
@@ -319,7 +314,7 @@ $updatePossible = true;
 
 	<?php if ($updatePossible) : ?>
 
-	<form action="<?php echo Route::_('index.php?option=com_joomlaupdate'); ?>" method="post" class="d-flex flex-sm-column mb-5">
+	<form action="<?php echo Route::_('index.php?option=com_joomlaupdate'); ?>" method="post" class="d-flex flex-column mb-5">
 
 		<div id="preupdatecheckbox">
 			<div class="form-check d-flex justify-content-center mb-3">
