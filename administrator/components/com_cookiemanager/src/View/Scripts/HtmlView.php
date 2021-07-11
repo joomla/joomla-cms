495ca3f76004c9d66c090492abcdc82691c6c78f
Rishabh Ranjan Jha <rjharishabh@gmail.com>
2021-07-11 14:11:19 +0530

    Create folder structure for scripts

---
+++ administrator/components/com_cookiemanager/src/View/Scripts/HtmlView.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_cookiemanager
+ *
+ * @copyright   (C) 2021 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+namespace Joomla\Component\Cookiemanager\Administrator\View\Cookies;
---
+++ administrator/components/com_cookiemanager/tmpl/scripts/default.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_cookiemanager
+ *
+ * @copyright   (C) 2021 Open Source Matters, Inc. <https://www.joomla.org>
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
