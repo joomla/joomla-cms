1cbbd5a2980a47a7af4eff5cb93f7153f61e7c26
Rishabh Ranjan Jha <rjharishabh@gmail.com>
2021-07-11 15:37:44 +0530

    add emptystate for scripts

--- administrator/components/com_cookiemanager/tmpl/scripts/emptystate.php
+++ administrator/components/com_cookiemanager/tmpl/scripts/emptystate.php
@@ -6,3 +6,24 @@
  * @copyright   (C) 2021 Open Source Matters, Inc. <https://www.joomla.org>
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
+
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\Factory;
+use Joomla\CMS\Layout\LayoutHelper;
+
+$displayData = [
+	'textPrefix' => 'COM_COOKIEMANAGER_SCRIPTS',
+	'formURL'    => 'index.php?option=com_cookiemanager&view=scripts',
+	'helpURL'    => 'https://docs.joomla.org/Special:MyLanguage',
+	'icon'       => 'icon-generic scripts',
+];
+
+$user = Factory::getApplication()->getIdentity();
+
+if ($user->authorise('core.create', 'com_cookiemanager') || count($user->getAuthorisedCategories('com_cookiemanager', 'core.create')) > 0)
+{
+	$displayData['createURL'] = 'index.php?option=com_cookiemanager&task=script.add';
+}
+
+echo LayoutHelper::render('joomla.content.emptystate', $displayData);
