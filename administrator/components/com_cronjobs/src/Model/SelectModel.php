7d32fd751fa18a19b79a9c3850bca58fcedbde6f
dst27 <72784348+dst27@users.noreply.github.com>
2021-07-15 13:51:51 +0530

    Implement SelectModel
    Implements in particular the getItems() method. Due significant changes
    in the plugin API particularly the $items object (currently array) used
    as the `onCronOptionsList` Event's 'subject' argument.

--- administrator/components/com_cronjobs/src/Model/SelectModel.php
+++ administrator/components/com_cronjobs/src/Model/SelectModel.php
@@ -12,9 +12,15 @@
 namespace Joomla\Component\Cronjobs\Administrator\Model;
 
 // Restrict direct access
-\defined('_JEXEC') or die;
+defined('_JEXEC') or die;
 
+use Exception;
+use Joomla\CMS\Application\CMSApplication;
+use Joomla\CMS\Event\AbstractEvent;
+use Joomla\CMS\Factory;
 use Joomla\CMS\MVC\Model\ListModel;
+use Joomla\CMS\Plugin\PluginHelper;
+use function defined;
 
 /**
  * MVC Model for SelectView
@@ -23,4 +29,46 @@ use Joomla\CMS\MVC\Model\ListModel;
  */
 class SelectModel extends ListModel
 {
+	/**
+	 * @var CMSApplication
+	 * @since __DEPLOY_VERSION__
+	 */
+	protected $app;
+
+
+	/**
+	 * SelectModel constructor.
+	 *
+	 * @throws Exception
+	 * @since __DEPLOY_VERSION__
+	 */
+	public function __construct()
+	{
+		$this->app = Factory::getApplication();
+		parent::__construct();
+	}
+
+	/**
+	 *
+	 * @return array  An array of items
+	 *
+	 * @since __DEPLOY_VERSION__
+	 */
+	public function getItems(): array
+	{
+		// TODO : Implement an object for $items
+		$items = [];
+		PluginHelper::importPlugin('job');
+		$this->app->getDispatcher()->dispatch('onCronOptionsList',
+			AbstractEvent::create(
+				'onCronOptionsList',
+				[
+					'eventClass' => 'Joomla\Component\Cronjobs\Administrator\Model\SelectModel',
+					'subject' => &$items
+				]
+			)
+		);
+
+		return $items;
+	}
 }
