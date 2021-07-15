59b3a99995eb2af375e69e09df48de9ebb370f73
Rishabh Ranjan Jha <rjharishabh@gmail.com>
2021-07-15 17:01:48 +0530

    Add sql for scripts

---
+++ administrator/components/com_admin/sql/updates/mysql/4.1.0-2021-07-14.sql
@@ -0,0 +1,17 @@
+--
+-- Table structure for table `#__cookiemanager_scripts`
+--
+
+CREATE TABLE IF NOT EXISTS `#__cookiemanager_scripts` (
+  `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `title` varchar(255) NOT NULL,
+  `alias` varchar(400) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `position` varchar(40) NOT NULL,
+  `type` varchar(20) NOT NULL,
+  `code` varchar(400) NOT NULL,
+  `catid` int NOT NULL DEFAULT 0,
+  `published` tinyint NOT NULL DEFAULT 1,
+  `ordering` int NOT NULL DEFAULT 0,
+  KEY `idx_state` (`published`),
+  KEY `idx_catid` (`catid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 DEFAULT COLLATE utf8mb4_unicode_ci;
