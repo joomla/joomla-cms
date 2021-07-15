8a49431b375808f60cc85d2a5c274c55b3f24706
chmst <dev@chmst.de>
2021-07-15 22:43:29 +0200

    Suggested changes

--- modules/mod_menu/tmpl/default_separator.php
+++ modules/mod_menu/tmpl/default_separator.php
@@ -15,38 +15,35 @@ $title      = $item->anchor_title ? ' title="' . $item->anchor_title . '"' : '';
 $anchor_css = $item->anchor_css ?: '';
 $linktype   = $item->title;
 
-if ($item->menu_image || $item->menu_icon )
+if ($item->menu_icon)
 {
-	if ($item->menu_image)
+	// The link is an icon
+	if ($itemParams->get('menu_text', 1))
 	{
-		// The link is an image, maybe with an own class
-		$image_attributes =[];
-
-		if ($item->menu_image_css)
-		{
-			$image_attributes['class'] = $item->menu_image_css;
-		}
-
-		$linktype = HTMLHelper::_('image', $item->menu_image, $item->title, $image_attributes);
-
-		if ($itemParams->get('menu_text', 1))
-		{
-			$linktype .= '<span class="image-title">' . $item->title . '</span>';
-		}
+		// If the link text is to be displayed, the icon is added with aria-hidden
+		$linktype = '<span class="p-2 ' . $item->menu_icon . '" aria-hidden="true"></span>' . $item->title;
 	}
 	else
 	{
-		// The link is an icon
-		if ($itemParams->get('menu_text', 1))
-		{
-			// If the link text is to be displayed, the icon is added with aria-hidden
-			$linktype = '<span class="p-2 ' . $item->menu_icon . '" aria-hidden="true"></span>' . $item->title;
-		}
-		else
-		{
-			// If the icon itself is the link, it needs a visually hidden text
-			$linktype = '<span class="p-2 ' . $item->menu_icon . '" ></span><span class="visually-hidden">' . $item->title . '</span>';
-		}
+		// If the icon itself is the link, it needs a visually hidden text
+		$linktype = '<span class="p-2 ' . $item->menu_icon . '" aria-hidden="true"></span><span class="visually-hidden">' . $item->title . '</span>';
+	}
+}
+elseif ($item->menu_image)
+{
+	// The link is an image, maybe with an own class
+	$image_attributes = [];
+
+	if ($item->menu_image_css)
+	{
+		$image_attributes['class'] = $item->menu_image_css;
+	}
+
+	$linktype = HTMLHelper::_('image', $item->menu_image, $item->title, $image_attributes);
+
+	if ($itemParams->get('menu_text', 1))
+	{
+		$linktype .= '<span class="image-title">' . $item->title . '</span>';
 	}
 }
 
