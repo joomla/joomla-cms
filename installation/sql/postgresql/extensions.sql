ce5c136f631e42714d45fc883377e2416b15c705
dst27 <72784348+dst27@users.noreply.github.com>
2021-07-18 20:43:08 +0530

    Fix PostegreSQL SQL scripts
    - Fixes blocking PostegreSQL issues causing builds to fails
    - Changes codestyle to follow existing Joomla SQL style

--- installation/sql/postgresql/extensions.sql
+++ installation/sql/postgresql/extensions.sql
@@ -911,24 +911,24 @@ $$;
 -- --------------------------------------------------------
 
 --
--- Create enumerated types for com_cronjobs
+-- Create enumerated types for "#__cronjobs"
 --
 
 DO
 $$
     BEGIN
-        CREATE TYPE job_type AS ENUM ('script', 'plugin');
+        CREATE TYPE job_type AS enum ('script', 'plugin');
     EXCEPTION
-        WHEN duplicate_object THEN null;
+        WHEN duplicate_object THEN NULL;
     END
 $$;
 
 DO
 $$
     BEGIN
-        CREATE TYPE trigger_type AS ENUM ('pseudo_cron', 'cron', 'visit_count');
+        CREATE TYPE trigger_type AS enum ('pseudo_cron', 'cron', 'visit_count');
     EXCEPTION
-        WHEN duplicate_object THEN null;
+        WHEN duplicate_object THEN NULL;
     END
 $$;
 
@@ -940,21 +940,21 @@ $$;
 
 CREATE TABLE IF NOT EXISTS "#__cronjobs"
 (
-    "id"                 INT GENERATED ALWAYS AS IDENTITY,
-    "asset_id"           BIGINT                   NOT NULL DEFAULT '0',
-    "title"              VARCHAR(255)             NOT NULL,
-    "type"               JOB_TYPE,
-    "execution_interval" INTERVAL                 NOT NULL,
-    "trigger"            TRIGGER_TYPE             NOT NULL DEFAULT 'pseudo_cron',
-    "state"              SMALLINT                 NOT NULL DEFAULT '0',
-    "last_exit_code"     INT                      NOT NULL DEFAULT '0',
-    "last_execution"     TIMESTAMP,
-    "next_execution"     TIMESTAMP,
-    "times_executed"     TIMESTAMP                NOT NULL,
-    "times_failed"       INT                               DEFAULT '0',
-    "note"               TEXT                              DEFAULT '',
-    "created"            TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT '0000-00-00 00:00:00',
-    "created_by"         BIGINT                   NOT NULL DEFAULT '0'
+    "id" int GENERATED ALWAYS AS IDENTITY,
+    "asset_id" bigint NOT NULL DEFAULT '0',
+    "title" varchar(255) NOT NULL,
+    "type" job_type,
+    "execution_interval" interval NOT NULL,
+    "trigger" trigger_type NOT NULL DEFAULT 'pseudo_cron',
+    "state" smallint NOT NULL DEFAULT '0',
+    "last_exit_code" int NOT NULL DEFAULT '0',
+    "last_execution" timestamp without time zone,
+    "next_execution" timestamp without time zone,
+    "times_executed" int NOT NULL DEFAULT '0',
+    "times_failed" int DEFAULT '0',
+    "note" text DEFAULT '',
+    "created" timestamp without time zone NOT NULL,
+    "created_by" bigint NOT NULL DEFAULT '0'
 );
 
 -- --------------------------------------------------------
@@ -965,10 +965,10 @@ CREATE TABLE IF NOT EXISTS "#__cronjobs"
 
 CREATE TABLE IF NOT EXISTS "#__cronjobs_scripts"
 (
-    "id"        INT GENERATED ALWAYS AS IDENTITY,
-    "job_id"    INT, -- References "#__cronjobs"(id)
-    "directory" VARCHAR(256) NOT NULL,
-    "file"      VARCHAR(128) NOT NULL
+    "id" int GENERATED ALWAYS AS IDENTITY,
+    "job_id" int, -- References "#__cronjobs"(id)
+    "directory" varchar(256) NOT NULL,
+    "file" varchar(128) NOT NULL
 );
 
 -- --------------------------------------------------------
