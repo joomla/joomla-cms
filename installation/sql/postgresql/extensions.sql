661e6c37b2be5f2fef48350ee94989cf82ad1180
dst27 <72784348+dst27@users.noreply.github.com>
2021-07-19 18:00:20 +0530

    Shift PostgreSQL DDL to before Soundex
    Not sure why, but DDL placed after soundex fails with the same error
    as in issue #6 in dev. Doing the same to b130fc2 yields a new error
    which #6 will be updated with. This commit state successfully installs
    with Postgre.

--- installation/sql/postgresql/extensions.sql
+++ installation/sql/postgresql/extensions.sql
@@ -846,6 +846,47 @@ CREATE TABLE "#__action_logs_users" (
 
 CREATE INDEX "#__action_logs_users_idx_notify" ON "#__action_logs_users" ("notify");
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table "#__cronjobs"
+--
+
+CREATE TABLE IF NOT EXISTS "#__cronjobs"
+(
+    "id" int GENERATED ALWAYS AS IDENTITY,
+    "asset_id" bigint NOT NULL DEFAULT '0',
+    "title" varchar(255) NOT NULL,
+    "type" varchar(6),
+    "execution_interval" interval NOT NULL,
+    "trigger" varchar(12) NOT NULL DEFAULT 'pseudo_cron',
+    "state" smallint NOT NULL DEFAULT '0',
+    "last_exit_code" int NOT NULL DEFAULT '0',
+    "last_execution" timestamp without time zone,
+    "next_execution" timestamp without time zone,
+    "times_executed" int NOT NULL DEFAULT '0',
+    "times_failed" int DEFAULT '0',
+    "note" text DEFAULT '',
+    "created" timestamp without time zone NOT NULL,
+    "created_by" bigint NOT NULL DEFAULT '0'
+);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table "#__cronjobs_scripts"
+--
+
+CREATE TABLE IF NOT EXISTS "#__cronjobs_scripts"
+(
+    "id" int GENERATED ALWAYS AS IDENTITY,
+    "job_id" int, -- References "#__cronjobs"(id)
+    "directory" varchar(256) NOT NULL,
+    "file" varchar(128) NOT NULL
+);
+
+-- --------------------------------------------------------
+
 --
 -- Here is SOUNDEX replacement for those who can't enable fuzzystrmatch module
 --   from contrib folder.
@@ -907,44 +948,3 @@ BEGIN
   RETURN soundex;
 END;
 $$;
-
--- --------------------------------------------------------
-
---
--- Table structure for table "#__cronjobs"
---
-
-CREATE TABLE IF NOT EXISTS "#__cronjobs"
-(
-    "id" int GENERATED ALWAYS AS IDENTITY,
-    "asset_id" bigint NOT NULL DEFAULT '0',
-    "title" varchar(255) NOT NULL,
-    "type" varchar(6),
-    "execution_interval" interval NOT NULL,
-    "trigger" varchar(12) NOT NULL DEFAULT 'pseudo_cron',
-    "state" smallint NOT NULL DEFAULT '0',
-    "last_exit_code" int NOT NULL DEFAULT '0',
-    "last_execution" timestamp without time zone,
-    "next_execution" timestamp without time zone,
-    "times_executed" int NOT NULL DEFAULT '0',
-    "times_failed" int DEFAULT '0',
-    "note" text DEFAULT '',
-    "created" timestamp without time zone NOT NULL,
-    "created_by" bigint NOT NULL DEFAULT '0'
-);
-
--- --------------------------------------------------------
-
---
--- Table structure for table "#__cronjobs_scripts"
---
-
-CREATE TABLE IF NOT EXISTS "#__cronjobs_scripts"
-(
-    "id" int GENERATED ALWAYS AS IDENTITY,
-    "job_id" int, -- References "#__cronjobs"(id)
-    "directory" varchar(256) NOT NULL,
-    "file" varchar(128) NOT NULL
-);
-
--- --------------------------------------------------------
