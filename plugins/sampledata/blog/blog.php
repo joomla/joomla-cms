bbe0bd985a4b3f60057924df59f61e9c5ebb5330
Tuan Pham Ngoc <github@joomdonation.com>
2021-07-21 18:11:47 +0700

    Correct namespace import (#34807)

--- plugins/sampledata/blog/blog.php
+++ plugins/sampledata/blog/blog.php
@@ -12,7 +12,6 @@ defined('_JEXEC') or die;
 use Joomla\CMS\Application\ApplicationHelper;
 use Joomla\CMS\Component\ComponentHelper;
 use Joomla\CMS\Extension\ExtensionHelper;
-use Joomla\CMS\Factory;
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Multilanguage;
 use Joomla\CMS\Language\Text;
