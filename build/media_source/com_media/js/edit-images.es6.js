e54f3716e973cc52fbdbd5ab81515b2282a96fed
dgrammatiko <d.grammatiko@gmail.com>
2021-07-23 20:21:05 +0200

    wrong place for the max-width

--- build/media_source/com_media/js/edit-images.es6.js
+++ build/media_source/com_media/js/edit-images.es6.js
@@ -128,7 +128,7 @@ class Edit {
     this.imagePreview = document.createElement('img');
     this.imagePreview.src = data.contents;
     this.imagePreview.id = 'image-preview';
-    this.baseContainer.style.maxWidth = '100%';
+    this.imagePreview.style.maxWidth = '100%';
     this.baseContainer.appendChild(this.imagePreview);
   }
 
