acca0c35e1409885a090f7bf05104a94fc44cb11
Viviana Menzel <vivianamenzel@dr-menzel-it.de>
2021-07-26 11:50:28 +0200

    Modified JS file

--- build/media_source/com_categories/js/shared-categories-accordion.es6.js
+++ build/media_source/com_categories/js/shared-categories-accordion.es6.js
@@ -2,60 +2,46 @@
  * @copyright  (C) 2018 Open Source Matters, Inc. <https://www.joomla.org>
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
-((Joomla) => {
-  'use strict';
 
-  if (!Joomla || !Joomla.Text) {
-    throw new Error('core.js was not properly initialised');
-  }
+if (!Joomla || !Joomla.Text) {
+  throw new Error('core.js was not properly initialised');
+}
 
-  // Selectors used by this script
-  const buttonsSelector = '[id^=category-btn-]';
+// Selectors used by this script
+const buttonsSelector = '[id^=category-btn-]';
 
-  /**
-   * Handle the category toggle button click event
-   * @param event
-   */
-  const handleCategoryToggleButtonClick = ({ currentTarget }) => {
-    const button = currentTarget;
-    const icon = button.querySelector('span');
+/**
+ * Handle the category toggle button click event
+ * @param event
+ */
+const handleCategoryToggleButtonClick = ({ currentTarget }) => {
+  const button = currentTarget;
+  const icon = button.querySelector('span');
 
-    // Toggle icon class
-    icon.classList.toggle('icon-plus');
-    icon.classList.toggle('icon-minus');
+  // Toggle icon class
+  icon.classList.toggle('icon-plus');
+  icon.classList.toggle('icon-minus');
 
-    // Toggle aria label, aria-expanded
-    const ariaLabel = button.getAttribute('aria-label');
-    const ariaExpanded = button.getAttribute('aria-expanded');
-    button.setAttribute(
-      'aria-label',
-      (
-        ariaLabel === Joomla.Text._('JGLOBAL_EXPAND_CATEGORIES') ? Joomla.Text._('JGLOBAL_COLLAPSE_CATEGORIES') : Joomla.Text._('JGLOBAL_EXPAND_CATEGORIES')
-      ),
-    );
-    button.setAttribute(
-      'aria-expanded',
-      (
-        ariaExpanded === 'false' ? 'true' : 'false'
-      ),
-    );
+  // Toggle aria label, aria-expanded
+  const ariaLabel = button.getAttribute('aria-label');
+  const ariaExpanded = button.getAttribute('aria-expanded');
+  button.setAttribute(
+    'aria-label',
+    (
+      ariaLabel === Joomla.Text._('JGLOBAL_EXPAND_CATEGORIES') ? Joomla.Text._('JGLOBAL_COLLAPSE_CATEGORIES') : Joomla.Text._('JGLOBAL_EXPAND_CATEGORIES')
+    ),
+  );
+  button.setAttribute(
+    'aria-expanded',
+    (
+      ariaExpanded === 'false' ? 'true' : 'false'
+    ),
+  );
 
-    const target = button.nextElementSibling;
-    target.toggleAttribute('hidden');
-  };
+  const target = button.nextElementSibling;
+  target.toggleAttribute('hidden');
+};
 
-  /**
-   * Script boot
-   */
-  const onBoot = () => {
-    const buttons = [].slice.call(document.querySelectorAll(buttonsSelector));
-    buttons.forEach((button) => {
-      button.addEventListener('click', handleCategoryToggleButtonClick);
-    });
-
-    // Cleanup
-    document.removeEventListener('DOMContentLoaded', onBoot);
-  };
-
-  document.addEventListener('DOMContentLoaded', onBoot);
-})(Joomla);
+Array.from(document.querySelectorAll(buttonsSelector)).forEach((button) => {
+  button.addEventListener('click', handleCategoryToggleButtonClick);
+});
