0679cb74d622078bce957c48556c2e9164345bf1
Dimitris Grammatikogiannis <d.grammatiko@gmail.com>
2021-07-26 21:00:37 +0200

    Test the tests

--- administrator/components/com_actionlogs/tmpl/actionlogs/default.php
+++ administrator/components/com_actionlogs/tmpl/actionlogs/default.php
@@ -6,7 +6,6 @@
  * @copyright   (C) 2018 Open Source Matters, Inc. <https://www.joomla.org>
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
-
 defined('_JEXEC') or die;
 
 use Joomla\CMS\HTML\HTMLHelper;
