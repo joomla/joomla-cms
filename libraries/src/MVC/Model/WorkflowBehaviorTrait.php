cf23886368971dea22a3f762195998cc7345bf47
YatharthVyas <yatharth2000.vyas@gmail.com>
2021-07-28 23:12:43 +0530

    Remove transition field for new modules

--- libraries/src/MVC/Model/WorkflowBehaviorTrait.php
+++ libraries/src/MVC/Model/WorkflowBehaviorTrait.php
@@ -364,12 +364,7 @@ trait WorkflowBehaviorTrait
 		}
 		else
 		{
-			$stage_id = $this->getStageForNewItem($form, $data);
-
-			if (!empty($stage_id))
-			{
-				$form->setFieldAttribute('transition', 'workflow_stage', (int) $stage_id);
-			}
+			$form->removeField('transition');
 		}
 	}
 
