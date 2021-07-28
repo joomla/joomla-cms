b74f4d6a6b5f75df5643f0a3bf2f75a73dbde70c
Dimitris Grammatikogiannis <d.grammatiko@gmail.com>
2021-07-28 11:22:33 +0200

    Don't upscale images

--- build/media_source/com_media/js/edit-images.es6.js
+++ build/media_source/com_media/js/edit-images.es6.js
@@ -125,7 +125,6 @@ class Edit {
     this.imagePreview = document.createElement('img');
     this.imagePreview.src = data.contents;
     this.imagePreview.id = 'image-preview';
-    this.imagePreview.style.width = '100%';
     this.imagePreview.style.height = 'auto';
     this.imagePreview.style.maxWidth = '100%';
     this.baseContainer.appendChild(this.imagePreview);
