a496f79c2e4daa3468cc822076f603ee6efa1762
dgrammatiko <d.grammatiko@gmail.com>
2021-07-29 23:54:24 +0200

    oops

--- plugins/editors-xtd/contact/contact.php
+++ plugins/editors-xtd/contact/contact.php
@@ -52,7 +52,6 @@ class PlgButtonContact extends CMSPlugin
 				. Session::getFormToken() . '=1&amp;editor=' . $name;
 
 		$button = new CMSObject;
-		$button->realName = 'PlgButtonContact';
 		$button->modal   = true;
 		$button->link    = $link;
 		$button->text    = Text::_('PLG_EDITORS-XTD_CONTACT_BUTTON_CONTACT');
