0a306ea57949eed46bc2076dcf3dd3957a088904
ditsuke <ditsuke@protonmail.com>
2021-08-01 17:50:10 +0530

    Add execution_rules handling to CronjobModel
    `execution_rules` from Cronjob form are sanitised, then used to build
    crontab-like `cron_rules`. These rules will be used internally to
    determine due executions. Handling for this is due to be added.

--- administrator/components/com_cronjobs/src/Model/CronjobModel.php
+++ administrator/components/com_cronjobs/src/Model/CronjobModel.php
@@ -17,15 +17,19 @@ defined('_JEXEC') or die;
 use Exception;
 use Joomla\CMS\Application\AdministratorApplication;
 use Joomla\CMS\Component\ComponentHelper;
+use Joomla\CMS\Factory;
 use Joomla\CMS\Form\Form;
 use Joomla\CMS\Form\FormFactoryInterface;
 use Joomla\CMS\MVC\Factory\MVCFactoryInterface;
 use Joomla\CMS\MVC\Model\AdminModel;
-use Joomla\CMS\Factory;
 use Joomla\CMS\Table\Table;
 use Joomla\Component\Cronjobs\Administrator\Helper\CronjobsHelper;
+use function array_diff;
+use function array_fill_keys;
 use function defined;
+use function gmdate;
 use function is_object;
+use function sort;
 
 /**
  * MVC Model to interact with the Cronjobs DB.
@@ -37,7 +41,7 @@ class CronjobModel extends AdminModel
 {
 	/**
 	 * Maps logical states to their values in the DB
-	 * ? : Do  we end up using this?
+	 * ? Do we end up using this?
 	 *
 	 * @var array
 	 * @since __DEPLOY_VERSION__
@@ -249,10 +253,6 @@ class CronjobModel extends AdminModel
 
 	/**
 	 * Overloads the parent getItem() method.
-	 * ! : Currently does nothing
-	 *
-	 * ? : Is this needed at all?
-	 *     Should be removed if we end up not needing any special handling.
 	 *
 	 * @param   integer  $pk  Primary key
 	 *
@@ -263,7 +263,6 @@ class CronjobModel extends AdminModel
 	 */
 	public function getItem($pk = null)
 	{
-		// TODO : Add CronjobModel specific handling or remove ⚠
 		$item = parent::getItem($pk);
 
 		if (!is_object($item))
@@ -274,6 +273,7 @@ class CronjobModel extends AdminModel
 		$cronOption = ($item->id ?? 0) ? CronjobsHelper::getCronOptions()->findOption($item->type ?? 0)
 			: ($this->getState('cronjob.option'));
 
+		CronjobsHelper::getCronOptions();
 		$item->set('cronOption', $cronOption);
 
 		return $item;
@@ -284,12 +284,13 @@ class CronjobModel extends AdminModel
 	 *
 	 * @return  boolean  True on success, false on failure
 	 *
+	 * @throws Exception
 	 * @since __DEPLOY_VERSION__
 	 */
 	public function save($data): bool
 	{
 		/**
-		 * @var   object $field Holds the record we're saving $data to
+		 * @var  object $field Holds the record we're saving $data to
 		 */
 		$field = null;
 
@@ -300,29 +301,127 @@ class CronjobModel extends AdminModel
 			$field = $this->getItem($data['id']);
 		}
 
-		/*
-		 * ! : Due change
-		 * TODO : Change execution interval in DB to TIME, change handling below
-		 * TODO : Custom fields and we might not need this ugly handling
-		 */
-		$intervalHours = str_pad($data['interval-hours'] ?? '0', 2, '0', STR_PAD_LEFT);
-		$intervalMinutes = str_pad($data['interval-minutes'] ?? '0', 2, '0', STR_PAD_LEFT);
-		$data['execution_interval'] = "$intervalHours:$intervalMinutes:00";
+		// Clean up execution rules
+		$this->processExecutionRules($data);
 
-		// TODO : Unset fields based on type and trigger selected
+		// Build the `cron_rules` column from `execution_rules`
+		$this->buildCronRule($data);
 
-		/*
-		 * The parent save() takes care of saving to the main
-		 * `#__cronjobs` table
-		 */
+		// Parent method takes care of saving to the table
 		if (!parent::save($data))
 		{
 			return false;
 		}
 
-		// TODO: Handle the type-specific tables below! ⚠
-
-		// No failures if we get here
 		return true;
+	}
+
+	/**
+	 * Clean up and standardise execution rules
+	 *
+	 * @param   array  $data  The form data [? can just replace with execution_interval]
+	 *
+	 * @return void
+	 *
+	 * @since __DEPLOY_VERSION__
+	 */
+	private function processExecutionRules(array &$data): void
+	{
+		$executionRules = &$data['execution_rules'];
+		$ruleType = $executionRules['rule-type'];
+		$retainKeys = ['rule-type', $ruleType, 'exec-day', 'exec-time'];
+		$executionRules = array_intersect_key($executionRules, array_flip($retainKeys));
+
+		$executionRules['exec-day'] = $executionRules['exec-day'] ?: (string) gmdate('d');
+		$executionRules['exec-time'] = $executionRules['exec-time'] ?: (string) gmdate('H:i');
+
+		// If custom ruleset, sort it
+		// ? Is this necessary
+		if ($ruleType === 'custom')
+		{
+			foreach ($executionRules['custom'] as &$values)
+			{
+				sort($values);
+			}
+		}
+	}
+
+	/**
+	 * Private method to build cron rules from input execution rules.
+	 * Cron rules are used internally to determine execution times/conditions.
+	 *
+	 * @param   array  $data  The form input data
+	 *
+	 * @return void
+	 *
+	 * @since __DEPLOY_VERSION__
+	 */
+	private function buildCronRule(array &$data): void
+	{
+		$executionRules = $data['execution_rules'];
+		$cronRules = &$data['cron_rules'];
+		$standardRules = ['minutes', 'hours', 'days_month', 'months', 'days_week'];
+		$cronRules = array_fill_keys($standardRules, '*');
+		$cronRules['visits'] = null;
+		$basisDayOfMonth = $executionRules['exec-day'];
+		$basisTime = $executionRules['exec-time'];
+		[$basisHour, $basisMinute] = explode(':', $basisTime);
+
+		switch ($executionRules['rule-type'])
+		{
+			case 'interval-minutes':
+				$cronRules['minutes'] = "*/${executionRules['interval-minutes']}";
+				break;
+			case 'interval-hours':
+				$cronRules['minutes'] = (int) $basisMinute;
+				$cronRules['hours'] = "*/${executionRules['interval-hours']}";
+				break;
+			case 'interval-days':
+				$cronRules['minutes'] = (int) $basisMinute;
+				$cronRules['hours'] = (int) $basisHour;
+				$cronRules['days'] = "*/${executionRules['interval-days']}";
+				break;
+			case 'interval-months':
+				$cronRules['minutes'] = (int) $basisMinute;
+				$cronRules['hours'] = (int) $basisHour;
+				$cronRules['days'] = (int) $basisDayOfMonth;
+				$cronRules['months'] = "*/${executionRules['interval-months']}";
+				break;
+			case 'custom':
+				$customRules = &$executionRules['custom'];
+				$cronRules['minutes'] = $this->wildcardIfMatch($customRules['minutes'], range(0, 59), true);
+				$cronRules['hours'] = $this->wildcardIfMatch($customRules['hours'], range(0, 23), true);
+				$cronRules['days_month'] = $this->wildcardIfMatch($customRules['days_month'], range(1, 31), true);
+				$cronRules['months'] = $this->wildcardIfMatch($customRules['months'], range(1, 12), true);
+				$cronRules['days_week'] = $this->wildcardIfMatch($customRules['days_week'], range(1, 7), true);
+		}
+	}
+
+	/**
+	 * Determine if an array is populated by all its possible values by comparison to a reference array.
+	 *
+	 * @param   array  $target       The target array
+	 * @param   array  $reference    The reference array, populated by the complete set of possible values in $target
+	 * @param   bool   $targetToInt  If true, converts $target array values to integers before comparing
+	 *
+	 * @return string|int[]  A wildcard string if $target is fully populated, else $target itself.
+	 *
+	 * @since __DEPLOY_VERSION__
+	 */
+	private function wildcardIfMatch(array $target, array $reference, bool $targetToInt = false)
+	{
+		if ($targetToInt)
+		{
+			$target = array_map(
+				function (string $x): int {
+					return (int) $x;
+				},
+				$target
+			);
+		}
+
+		$isMatch = array_diff($reference, $target) === [];
+
+		return $isMatch ? "*" : $target;
 	}
 }
