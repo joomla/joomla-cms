2cf5a96b0df7e51a0e77be219df4df108c2bff49
ditsuke <ditsuke@protonmail.com>
2021-08-03 20:14:20 +0530

    Update CronjobsView
    While still due more updates, this commit:
    - Removes access column from the table
    - Removes access refs from the filter form
    - Fixes table head sorting buttons
    - Shows job type titles instead of type identifiers
    - Improves styling and readability

--- administrator/components/com_cronjobs/tmpl/cronjobs/default.php
+++ administrator/components/com_cronjobs/tmpl/cronjobs/default.php
@@ -9,6 +9,7 @@
  * @codingStandardsIgnoreStart
  */
 
+// Restrict direct access
 defined('_JEXEC') or die;
 
 use Joomla\CMS\Factory;
@@ -55,9 +56,7 @@ if ($saveOrder && !empty($this->items))
 		?>
 
 		<!-- If no cronjobs -->
-		<?php if (empty($this->items))
-			:
-			?>
+		<?php if (empty($this->items)): ?>
 			<!-- No cronjobs -->
 			<div class="alert alert-info">
 				<span class="icon-info-circle" aria-hidden="true"></span><span
@@ -67,9 +66,7 @@ if ($saveOrder && !empty($this->items))
 		<?php endif; ?>
 
 		<!-- If there are cronjobs, we start with the table -->
-		<?php if (!empty($this->items))
-			:
-			?>
+		<?php if (!empty($this->items)): ?>
 			<!-- Cronjobs table starts here -->
 			<table class="table" id="categoryList">
 
@@ -82,59 +79,61 @@ if ($saveOrder && !empty($this->items))
 				<!-- Cronjobs table header -->
 				<thead>
 				<tr>
+
 					<!-- Select all -->
 					<td class="w-1 text-center">
 						<?php echo HTMLHelper::_('grid.checkall'); // "Select all" checkbox
 						?>
 					</td>
+
 					<!-- Ordering?-->
 					<th scope="col" class="w-1 d-none d-md-table-cell text-center">
 						<!-- Might need to adjust method args here -->
-						<?php echo HTMLHelper::_('searchtools.sort', '', 'a.job_id', $listDirn, $listOrder, null, 'asc', 'JGRID_HEADING_ORDERING', 'icon-sort'); ?>
+						<?php echo HTMLHelper::_('searchtools.sort', '', 'a.id', $listDirn, $listOrder, null, 'asc', 'JGRID_HEADING_ORDERING', 'icon-sort'); ?>
 					</th>
-					<!-- Status -->
+					<!-- Job State -->
 					<th scope="col" class="w-1 text-center">
-						<?php echo HTMLHelper::_('searchtools.sort', 'JSTATUS', 'a.enabled', $listDirn, $listOrder); ?>
+						<?php echo HTMLHelper::_('searchtools.sort', 'JSTATUS', 'a.state', $listDirn, $listOrder); ?>
 					</th>
-					<!-- Job name-->
+
+					<!-- Job title header -->
 					<th scope="col">
-						<?php echo HTMLHelper::_('searchtools.sort', 'JGLOBAL_TITLE', 'a.name', $listDirn, $listOrder); ?>
+						<?php echo HTMLHelper::_('searchtools.sort', 'JGLOBAL_TITLE', 'a.title', $listDirn, $listOrder); ?>
 					</th>
-					<!-- Job Type -->
+
+					<!-- Job type header -->
 					<th scope="col">
-						<?php echo HTMLHelper::_('searchtools.sort', 'JOB_TYPE', 'a.type', $listDirn, $listOrder) ?>
+						<?php echo HTMLHelper::_('searchtools.sort', 'COM_CRONJOBS_JOB_TYPE', 'j.type_title', $listDirn, $listOrder) ?>
 					</th>
-					<!-- Job access-->
-					<th scope="col" class="w-10 d-none d-md-table-cell">
-						<?php echo HTMLHelper::_('searchtools.sort', 'JGRID_HEADING_ACCESS', 'a.access', $listDirn, $listOrder); ?>
-					</th>
+
 					<!-- Job ID -->
 					<th scope="col" class="w-5 d-none d-md-table-cell">
-						<?php echo HTMLHelper::_('searchtools.sort', 'JGRID_HEADING_ID', 'a.job_id', $listDirn, $listOrder); ?>
+						<?php echo HTMLHelper::_('searchtools.sort', 'JGRID_HEADING_ID', 'a.id', $listDirn, $listOrder); ?>
 					</th>
 				</tr>
 				</thead>
 
 				<!-- Table body begins -->
-				<tbody <?php if ($saveOrder)
-					:
-					?>
+				<tbody <?php if ($saveOrder): ?>
 					class="js-draggable" data-url="<?php echo $saveOrderingUrl; ?>" data-direction="<?php echo strtolower($listDirn); ?>" data-nested="true" <?php endif; ?>>
 				<?php foreach ($this->items as $i => $item):
 					// TODO : Check if $user->authorise() calls work as they should
 					$orderKey = $item->id;
 					$canCreate = $user->authorise('core.create', 'com_cronjobs');
 					$canEdit = $user->authorise('core.edit', 'com_cronjobs');
-					$canChange = $user->authorise('core.edit.state', 'com_cronjobs'); ?>
+					$canChange = $user->authorise('core.edit.state', 'com_cronjobs');
+					?>
 
 					<!-- Row begins -->
 					<tr class="row<?php echo $i % 2; ?>"
 						data-item-id="<?php echo $item->id; ?>"
-						data-draggable-group="<?php echo $item->id; ?>">
+						data-draggable-group="<?php echo $item->id; ?>"
+					>
 						<!-- Item Checkbox -->
 						<td class="text-center">
 							<?php echo HTMLHelper::_('grid.id', $i, $item->id, false, 'cid', 'cb', $item->title); ?>
 						</td>
+
 						<!-- Draggable handle -->
 						<td class="text-center d-none d-md-table-cell">
 							<?php
@@ -145,11 +144,12 @@ if ($saveOrder && !empty($this->items))
 							} elseif (!$saveOrder)
 							{
 								$iconClass = ' inactive" title="' . Text::_('JORDERINGDISABLED');
-							} ?>
+							}
+							?>
 
 							<span class="sortable-handler <?php echo $iconClass ?>">
-								<span class="icon-ellipsis-v"></span>
-							</span>
+									<span class="icon-ellipsis-v"></span>
+								</span>
 
 							<?php if ($canChange && $saveOrder): ?>
 								<input type="text" class="hidden" name="order[]" size="5"
@@ -157,13 +157,13 @@ if ($saveOrder && !empty($this->items))
 							<?php endif; ?>
 						</td>
 
-						<!-- Enabled status -->
+						<!-- Item State -->
 						<td class="text-center">
 							<?php echo HTMLHelper::_('jgrid.published', $item->state, $i, 'cronjobs.', $canChange); ?>
 						</td>
+
 						<!-- Item name, edit link, and note (TODO: should it be moved?) -->
 						<th scope="row">
-
 							<?php if ($canEdit): ?>
 								<a href="<?php echo Route::_('index.php?option=com_cronjobs&task=cronjob.edit&id=' . $item->id); ?>"
 								   title="<?php echo Text::_('JACTION_EDIT'); ?> <?php echo $this->escape($item->title); ?>"> <?php echo $this->escape($item->title); ?></a>
@@ -181,7 +181,7 @@ if ($saveOrder && !empty($this->items))
 
 						<!-- Item type -->
 						<td class="small d-none d-md-table-cell">
-							<?php echo $this->escape($item->type); ?>
+							<?php echo $this->escape($item->safeTypeTitle); ?>
 						</td>
 
 						<!-- TODO: What should be done about Multi-lang? Is it needed here? -->
@@ -191,23 +191,16 @@ if ($saveOrder && !empty($this->items))
 							</td>
 						<?php endif; ?>
 
-						<!-- Cronjob access -->
-						<!-- TODO: Implement access in cronjobs and have a look here -->
-						<td class="small d-none d-md-table-cell text-center">
-							<span class="badge bg-info">
-								<?php echo 'Unknown'; ?>
-							</span>
-						</td>
-
+						<!-- Item ID -->
 						<td class="d-none d-md-table-cell">
-							<?php echo (int)$item->id; ?>
+							<?php echo (int) $item->id; ?>
 						</td>
 					</tr>
 				<?php endforeach; ?>
 				</tbody>
 			</table>
 
-			<?php // Load the pagination.
+			<?php // Load the pagination. (TODO: testing)
 			?>
 			<?php echo $this->pagination->getListFooter(); ?>
 
@@ -215,20 +208,21 @@ if ($saveOrder && !empty($this->items))
 			?>
 
 			<?php if ($user->authorise('core.create', 'com_cronjobs')
-				&& $user->authorise('core.edit', 'com_cronjobs')
-				&& $user->authorise('core.edit.state', 'com_cronjobs')): ?>
+					&& $user->authorise('core.edit', 'com_cronjobs')
+					&& $user->authorise('core.edit.state', 'com_cronjobs')):
+				?>
 
-			<?php echo HTMLHelper::_(
+				<?php echo HTMLHelper::_(
 					'bootstrap.renderModal',
 					'collapseModal',
-					array(
+					[
 							'title' => Text::_('com_cronjobs_BATCH_OPTIONS'),
 							'footer' => $this->loadTemplate('batch_footer'),
-					),
+					],
 					$this->loadTemplate('batch_body')
-			); ?>
-
-		<?php endif; ?>
+			);
+				?>
+			<?php endif; ?>
 		<?php endif; ?>
 
 		<input type="hidden" name="task" value="">
