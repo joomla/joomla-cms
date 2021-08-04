0f07418c15c8a3abc2c514ad2eea827e97465819
YatharthVyas <yatharth2000.vyas@gmail.com>
2021-08-04 23:37:33 +0530

    add selector field dropdown

--- administrator/components/com_content/tmpl/articles/default.php
+++ administrator/components/com_content/tmpl/articles/default.php
@@ -98,7 +98,7 @@ $assoc = Associations::isEnabled();
 			<div id="j-main-container" class="j-main-container">
 				<?php
 				// Search tools bar
-				echo LayoutHelper::render('joomla.searchtools.default', array('view' => $this));
+				echo LayoutHelper::render('joomla.searchtools.default', array('view' => $this, 'options' => ['selectorFieldName' => 'featured']));
 				?>
 				<?php if (empty($this->items)) : ?>
 					<div class="alert alert-info">
