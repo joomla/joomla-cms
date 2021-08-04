f55fc83ebf0b1ad012f05ace99220b24ba773cbd
ditsuke <ditsuke@protonmail.com>
2021-08-05 04:15:08 +0530

    Implement JobTypeField to list all job types
    JobTypeField is a standard select field with options sorted naturally.
    With this commit, the Cronjobs filter form uses this field.

--- administrator/components/com_cronjobs/src/Field/JobTypeField.php
+++ administrator/components/com_cronjobs/src/Field/JobTypeField.php
@@ -14,7 +14,13 @@ namespace Joomla\Component\Cronjobs\Administrator\Field;
 // Restrict direct access
 defined('_JEXEC') or die;
 
+use Exception;
 use Joomla\CMS\Form\Field\ListField;
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\Component\Cronjobs\Administrator\Cronjobs\CronOption;
+use Joomla\Component\Cronjobs\Administrator\Helper\CronjobsHelper;
+use Joomla\Utilities\ArrayHelper;
+use function array_map;
 
 /**
  * A list field with all available job types
@@ -23,5 +29,40 @@ use Joomla\CMS\Form\Field\ListField;
  */
 class JobTypeField extends ListField
 {
+	/**
+	 * The form field type.
+	 *
+	 * @var    string
+	 * @since  __DEPLOY_VERSION__
+	 */
+	protected $type = 'jobType';
 
+	/**
+	 * Method to get field options
+	 *
+	 * @return array
+	 *
+	 * @throws Exception
+	 * @since __DEPLOY_VERSION__
+	 */
+	protected function getOptions(): array
+	{
+		$options = parent::getOptions();
+
+		// Get all available job types and sort by title
+		$types = ArrayHelper::sortObjects(
+			CronjobsHelper::getCronOptions()->options,
+			'title', 1
+		);
+
+		// Closure to add a CronOption as a <select> option in $options: array
+		$addTypeAsOption = function (CronOption $type) use (&$options) {
+			$options[] = HTMLHelper::_('select.option', $type->type, $type->title);
+		};
+
+		// Call $addTypeAsOption on each type
+		array_map($addTypeAsOption, $types);
+
+		return $options;
+	}
 }
