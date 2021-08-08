d2d41f3674778c5a697969318ec95027af156398
ditsuke <ditsuke@protonmail.com>
2021-08-09 00:16:08 +0530

    Update cronjobs system plugin
    Adds private methods to support pseudo-locking/unlocking jobs
    and running them (triggering the event).

--- plugins/system/cronjobs/cronjobs.php
+++ plugins/system/cronjobs/cronjobs.php
@@ -11,9 +11,14 @@
 defined('_JEXEC') or die;
 
 use Joomla\CMS\Application\CMSApplication;
+use Joomla\CMS\Event\AbstractEvent;
 use Joomla\CMS\Extension\MVCComponent;
+use Joomla\CMS\Factory;
 use Joomla\CMS\Plugin\CMSPlugin;
+use Joomla\CMS\Plugin\PluginHelper;
+use Joomla\Component\Cronjobs\Administrator\Helper\ExecRuleHelper;
 use Joomla\Component\Cronjobs\Administrator\Model\CronjobsModel;
+use Joomla\Database\DatabaseInterface;
 use Joomla\Event\Event;
 use Joomla\Event\SubscriberInterface;
 
@@ -68,11 +73,24 @@ class PlgSystemCronjobs extends CMSPlugin implements SubscriberInterface
 	protected $app;
 
 	/**
+	 * @var DatabaseInterface
+	 * @since __DEPLOY_VERSION__
+	 */
+	protected $db;
+
+	/**
 	 * @var boolean
 	 * @since __DEPLOY_VERSION__
 	 */
 	protected $autoloadLanguage = true;
 
+	/**
+	 * Stores the pseudo-cron status
+	 *
+	 * @var string[]
+	 * @since __DEPLOY_VERSION__
+	 */
+	protected $snapshot = [];
 
 	/**
 	 * Returns event subscriptions
@@ -107,7 +125,7 @@ class PlgSystemCronjobs extends CMSPlugin implements SubscriberInterface
 
 		if ($dueJob)
 		{
-			// Pass - due implementation of the Cronjob class and Plugin API (trigger)
+			$this->runJob($dueJob[0]);
 		}
 
 		return;
@@ -118,7 +136,7 @@ class PlgSystemCronjobs extends CMSPlugin implements SubscriberInterface
 	 * ! Orphan filtering + pagination issues in the Model will break this if orphaned jobs exist [TODO]
 	 *
 	 * @param   CronjobsModel  $model   The CronjobsModel
-	 * @param   boolean 	   $single  If true, only a single job is returned
+	 * @param   boolean        $single  If true, only a single job is returned
 	 *
 	 * @return object[]
 	 * @throws Exception
@@ -148,6 +166,147 @@ class PlgSystemCronjobs extends CMSPlugin implements SubscriberInterface
 		$model->setState('list.ordering', 'a.next_execution');
 		$model->setState('list.direction', 'ASC');
 
+		// Get a class with a smarter class
+		$model->setState('list.customClass', true);
+
 		return $model->getItems();
+	}
+
+	/**
+	 * @param   object   $cronjob     The cronjob entry
+	 * @param   boolean  $scheduling  Respect scheduling settings and state
+	 *                                ! Does nothing
+	 *
+	 * @return boolean True on success
+	 *
+	 * @since __DEPLOY_VERSION__
+	 */
+	private function runJob(object $cronjob, bool $scheduling = true): bool
+	{
+		$this->snapshot['jobId'] = $cronjob->id;
+		$this->snapshot['jobTitle'] = $cronjob->title;
+		$this->snapshot['status'] = self::JOB_NO_TIME;
+		$this->snapshot['duration'] = 0;
+
+		if (!$setlock = $this->setLock($cronjob))
+		{
+			$this->snapshot['status'] = self::JOB_NO_LOCK;
+
+			return false;
+		}
+
+		$app = $this->app;
+		$results = [];
+		$event = AbstractEvent::create(
+			'onCronRun',
+			[
+				'subject' => $this,
+				'jobId' => $cronjob->type,
+				'params' => $cronjob->params,
+				'results' => &$results
+			]
+		);
+
+		// TODO: test
+		PluginHelper::importPlugin('job');
+		$app->getDispatcher()->dispatch('onCronRun', $event);
+
+		if (!$this->releaseLock($cronjob))
+		{
+			$this->snapshot['status'] = self::JOB_NO_RUN;
+			$this->snapshot['duration'] = 0;
+
+			return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 * @param   object  $cronjob  The cronjob entry
+	 *
+	 * @return boolean  True on success
+	 *
+	 * @since __DEPLOY_VERSION__
+	 */
+	private function setLock(object $cronjob): bool
+	{
+		$db = $this->db;
+		$query = $db->getQuery(true);
+
+		$query->update($db->qn('#__cronjobs', 'j'))
+			->set('j.locked = 1')
+			->where($db->qn('j.id') . ' = :jobId')
+			->where($db->qn('j.locked') . ' = 0')
+			->bind(':jobId', $cronjob->id);
+		$db->setQuery($query)->execute();
+
+		if (!$affRow = $db->getAffectedRows())
+		{
+			return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 * @param   object   $cronjob     The cronjob entry
+	 * @param   boolean  $scheduling  Respect scheduling settings and state
+	 *                                ! Does nothing
+	 *
+	 * @return boolean  True if success, else failure
+	 *
+	 * @throws Exception
+	 * @since __DEPLOY_VERSION__
+	 */
+	private function releaseLock(object $cronjob, bool $scheduling = true): bool
+	{
+		$db = $this->db;
+
+		$releaseQuery = $db->getQuery(true);
+		$releaseQuery->update($db->qn('#__cronjobs', 'j'))
+			->set('locked = 0')
+			->where($db->qn('id') . ' = :jobId')
+			->where($db->qn('locked') . ' = 1')
+			->bind(':jobId', $cronjob->id);
+		$db->setQuery($releaseQuery)->execute();
+
+		if (!$affRow = $db->getAffectedRows())
+		{
+			// Log?
+			return false;
+		}
+
+		$updateQuery = $db->getQuery(true);
+
+		$ruleType = $cronjob->get('cron_rules');
+		$nextExec = (new ExecRuleHelper($cronjob))->nextExec();
+		$exitCode = null ?? self::JOB_NO_EXIT;
+		$now = Factory::getDate('now', 'GMT')->toSql();
+
+		/*
+		 * [TODO] We are not aware of job exit code at the moment. That's due in the API.
+		 * [TODO] Failed status
+		 */
+		$updateQuery->update($db->qn('#__cronjobs', 'j'))
+			->set(
+				[
+					'j.last_execution = :now',
+					'j.next_execution = :nextExec',
+					'j.last_exit_code = :exitCode',
+					'j.times_executed = j.times_executed + 1'
+				]
+			)->bind(':nextExec', $nextExec)
+			->bind(':exitCode', $exitCode)
+			->bind(':now', $now);
+		$db->setQuery($updateQuery)->execute();
+
+		if (!$affRow = $db->getAffectedRows())
+		{
+			// Log ?
+			return false;
+		}
+
+		return true;
 	}
 }
