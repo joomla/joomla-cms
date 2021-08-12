8158b8810cb994794bff19931f3eda84a4df7e2d
dgrammatiko <d.grammatiko@gmail.com>
2021-08-12 15:36:36 +0200

    meh

--- templates/cassiopeia/offline.php
+++ templates/cassiopeia/offline.php
@@ -48,7 +48,7 @@ if ($paramsFontScheme) {
 		if (count($matches) > 0)
 		{
 			$fontStyles = '--cassiopeia-font-family-body: "' . str_replace('+', ' ', $matches[1][0]) . '", sans-serif;
-			--cassiopeia-font-family-headings: "' . (count($matches) === 3 ? str_replace('+', ' ', $matches[2][0]) : str_replace('+', ' ', $matches[1][0])) . '", sans-serif;
+			--cassiopeia-font-family-headings: "' . (count($matches[1]) === 2 ? str_replace('+', ' ', $matches[1][1]) : str_replace('+', ' ', $matches[1][0])) . '", sans-serif;
 			--cassiopeia-font-weight-normal: 400;
 			--cassiopeia-font-weight-headings: 700;';
 		}
