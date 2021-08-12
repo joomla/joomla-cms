999e649f867aefa8c094e9d065bf9424c70c9848
zero-24 <zero-24@users.noreply.github.com>
2021-08-13 00:49:07 +0200

    another phpmailer one

--- libraries/vendor/phpmailer/phpmailer/class.phpmaileroauthgoogle.php
+++ libraries/vendor/phpmailer/phpmailer/class.phpmaileroauthgoogle.php
@@ -66,7 +66,7 @@ class PHPMailerOAuthGoogle
     {
         $provider = $this->getProvider();
         $grant = $this->getGrant();
-        return $provider->getAccessToken($grant, ['refresh_token' => $this->oauthRefreshToken]);
+        return $provider->getAccessToken($grant, array('refresh_token' => $this->oauthRefreshToken));
     }
 
     public function getOauth64()
