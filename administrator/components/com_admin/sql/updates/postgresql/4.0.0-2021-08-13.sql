4e86e4f0e3208aba61bc682a91c897a98fb0e3c2
Sander Potjer <github@sanderpotjer.nl>
2021-08-13 09:12:19 +0200

    Update sql statements #35102

---
+++ administrator/components/com_admin/sql/updates/postgresql/4.0.0-2021-08-13.sql
@@ -0,0 +1,8 @@
+-- after 4.0.0 RC6
+UPDATE "#__assets"
+SET "name" = 'com_content.stage.1'
+WHERE "name" = 'com_content.state.1';
+
+UPDATE "#__workflow_stages"
+SET "asset_id" = 57
+WHERE "asset_id" = 0;
