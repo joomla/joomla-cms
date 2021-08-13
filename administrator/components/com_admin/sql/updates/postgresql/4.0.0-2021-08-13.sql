ac462453c547e808f56da0c42aee0936bcc43258
Sander Potjer <github@sanderpotjer.nl>
2021-08-13 10:01:23 +0200

    #35102 Remove hard-coded asset ID from update statements

--- administrator/components/com_admin/sql/updates/postgresql/4.0.0-2021-08-13.sql
+++ administrator/components/com_admin/sql/updates/postgresql/4.0.0-2021-08-13.sql
@@ -4,5 +4,5 @@ SET "name" = 'com_content.stage.1'
 WHERE "name" = 'com_content.state.1';
 
 UPDATE "#__workflow_stages"
-SET "asset_id" = 57
+SET "asset_id" = (SELECT MAX("id") FROM "#__assets" WHERE "name" = 'com_content.stage.1')
 WHERE "asset_id" = 0;
