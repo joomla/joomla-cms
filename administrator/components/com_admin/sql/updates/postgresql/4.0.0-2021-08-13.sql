1e23c57e0d303da353789a14aba1d8fa570a35c7
Sander Potjer <github@sanderpotjer.nl>
2021-08-13 10:06:06 +0200

    #35102 Code style

--- administrator/components/com_admin/sql/updates/postgresql/4.0.0-2021-08-13.sql
+++ administrator/components/com_admin/sql/updates/postgresql/4.0.0-2021-08-13.sql
@@ -1,7 +1,7 @@
 -- after 4.0.0 RC6
 UPDATE "#__assets"
-SET "name" = 'com_content.stage.1'
-WHERE "name" = 'com_content.state.1';
+   SET "name" = 'com_content.stage.1'
+ WHERE "name" = 'com_content.state.1';
 
 UPDATE "#__workflow_stages"
    SET "asset_id" = (SELECT MAX("id") FROM "#__assets" WHERE "name" = 'com_content.stage.1')
