f1bbf67e39d00a1c432813b3a888357ff4350ed9
ditsuke <ditsuke@protonmail.com>
2021-08-16 03:52:47 +0530

    Fix SQL update scripts
    Applies changes suggested by @richard67:
    - Fix a wrong value for the folder column for `#__extensions` in update scripts.
    
    Co-authored-by: Richard Fath <richard67@users.noreply.github.com>

--- administrator/components/com_admin/sql/updates/postgresql/4.1.0-2021-08-04.sql
+++ administrator/components/com_admin/sql/updates/postgresql/4.1.0-2021-08-04.sql
@@ -1,7 +1,7 @@
 -- Add "com_cronjobs" to "#__extensions"
 INSERT INTO "#__extensions" ("package_id", "name", "type", "element", "folder", "client_id", "enabled", "access",
 							 "protected", "locked", "manifest_cache", "params", "custom_data", "ordering", "state")
-VALUES (0, 'com_cronjobs', 'component', 'com_cronjobs', 1, 1, 1, 1, 0, 1, '', '{}', '', 0, 0);
+VALUES (0, 'com_cronjobs', 'component', 'com_cronjobs', '', 1, 1, 1, 0, 1, '', '{}', '', 0, 0);
 
 -- Add "plg_job_testjob" to "#__extensions"
 INSERT INTO "#__extensions" ("package_id", "name", "type", "element", "folder", "client_id", "enabled", "access",
