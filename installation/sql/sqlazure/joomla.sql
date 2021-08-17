1df6b40421c379b16ca3e0d9fae061a0ec97457e
zero-24 <zero-24@users.noreply.github.com>
2021-08-17 04:02:25 +0200

    add the install SQl too

--- installation/sql/sqlazure/joomla.sql
+++ installation/sql/sqlazure/joomla.sql
@@ -2579,6 +2579,8 @@ CREATE TABLE "#__template_styles" (
   "client_id" tinyint NOT NULL DEFAULT 0,
   "home" nvarchar(7) NOT NULL DEFAULT '0',
   "title" nvarchar(255) NOT NULL DEFAULT '',
+	"inheritable" smallint NOT NULL DEFAULT '0',
+  "parent" nvarchar(50) NOT NULL DEFAULT '',
   "params" nvarchar(max) NOT NULL,
  CONSTRAINT "PK_#__template_styles_id" PRIMARY KEY CLUSTERED
 (
