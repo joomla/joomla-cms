bf60b16bc79a036b5e83df1c79b5a0d60505fcae
Сергей <koreshs@mail.ru>
2021-08-20 08:37:11 +0300

    Update libraries/src/Table/TablePropertyInterface.php
    Co-authored-by: Phil E. Taylor <phil@phil-taylor.com>

--- libraries/src/Table/TablePropertyInterface.php
+++ libraries/src/Table/TablePropertyInterface.php
@@ -2,7 +2,7 @@
 /**
  * Joomla! Content Management System
  *
- * @copyright  (C) 2014 Open Source Matters, Inc. <https://www.joomla.org>
+ * @copyright  (C) 2021 Open Source Matters, Inc. <https://www.joomla.org>
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 
