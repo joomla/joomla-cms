8ab15612ac0da416509312313f79267d377cfd3a
YatharthVyas <yatharth2000.vyas@gmail.com>
2021-08-20 11:43:12 +0530

    remove EOL whitespace

--- plugins/system/addmodulebutton/addmodulebutton.php
+++ plugins/system/addmodulebutton/addmodulebutton.php
@@ -5,7 +5,7 @@
  * @copyright (C) 2021 Open Source Matters, Inc. <https://www.joomla.org>
  * @license GNU General Public License version 2 or later; see LICENSE.txt
  *
- * @since  __DEPLOY_VERSION__ 
+ * @since  __DEPLOY_VERSION__
  */
 
 defined('_JEXEC') or die;
