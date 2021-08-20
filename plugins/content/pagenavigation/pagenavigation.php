f839831db9b3d58d2cb454bd599999663126890b
George Wilson <georgejameswilson@googlemail.com>
2021-08-20 01:19:38 +0100

    Update plugins/content/pagenavigation/pagenavigation.php
    Co-authored-by: Phil E. Taylor <phil@phil-taylor.com>

--- plugins/content/pagenavigation/pagenavigation.php
+++ plugins/content/pagenavigation/pagenavigation.php
@@ -28,7 +28,7 @@ class PlgContentPagenavigation extends CMSPlugin
 	 * The application object
 	 *
 	 * @var    \Joomla\CMS\Application\CMSApplication
-	 * @since  4.0.0
+	 * @since  __DEPLOY_VERSION__
 	 */
 	protected $app;
 
