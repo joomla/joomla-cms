5d1d015e5751281792544e67b7a53467c8595779
Phil E. Taylor <phil@phil-taylor.com>
2021-08-21 07:20:40 +0100

    Update DisplayController.php

--- components/com_contenthistory/src/Controller/DisplayController.php
+++ components/com_contenthistory/src/Controller/DisplayController.php
@@ -28,9 +28,9 @@ class DisplayController extends BaseController
 	 * @param   array                $config   An optional associative array of configuration settings.
 	 * Recognized key values include 'name', 'default_task', 'model_path', and
 	 * 'view_path' (this list is not meant to be comprehensive).
-	 * @param   MVCFactoryInterface  $factory  The factory.
-	 * @param   CMSApplication|null  $app      The JApplication for the dispatcher
-	 * @param   \JInput              $input    Input
+	 * @param   MVCFactoryInterface|null  $factory  The factory.
+	 * @param   CMSApplication|null       $app      The JApplication for the dispatcher
+	 * @param   \JInput|null              $input    Input
 	 *
 	 * @since   3.0
 	 */
