ad1256e287e1f2e294236b8368137bfb7af76a19
Phil E. Taylor <phil@phil-taylor.com>
2021-08-25 13:24:06 +0100

    Update plugins/authentication/ldap/ldap.php
    Co-authored-by: Richard Fath <richard67@users.noreply.github.com>

--- plugins/authentication/ldap/ldap.php
+++ plugins/authentication/ldap/ldap.php
@@ -85,7 +85,7 @@ class PlgAuthenticationLdap extends CMSPlugin
 
 					$ldap->bind($dn, $this->params->get('password', ''));
 				}
-				catch (ConnectionException|LdapException $exception)
+				catch (ConnectionException | LdapException $exception)
 				{
 					$response->status = Authentication::STATUS_FAILURE;
 					$response->error_message = Text::_('JGLOBAL_AUTH_NOT_CONNECT');
