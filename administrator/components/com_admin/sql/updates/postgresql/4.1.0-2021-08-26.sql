93a953edd5a58fb49de36a9c7e33068fc01f8aef
Pavlo Rozbytskyi <pavlo.rozbytskyi@mni.thm.de>
2021-08-27 12:44:17 +0200

    fixed postgres script

--- administrator/components/com_admin/sql/updates/postgresql/4.1.0-2021-08-26.sql
+++ administrator/components/com_admin/sql/updates/postgresql/4.1.0-2021-08-26.sql
@@ -1,11 +1,13 @@
 -- after 4.0.0 RC1
-CREATE TABLE IF NOT EXISTS `#__draft` (
-  "article_id" integer unsigned NOT NULL,
-  "version_id" integer unsigned NOT NULL,
-  "state" smallint NOT NULL DEFAULT 0,
-  "hashval" varchar(2083) NOT NULL DEFAULT '',
-  "shared_date" timestamp without time zone DEFAULT NULL,
-  PRIMARY KEY("article_id", "version_id")
-);
-ALTER TABLE "#__content" ADD COLUMN "shared" smallint UNSIGNED NOT NULL DEFAULT 0;
-ALTER TABLE "#__content" ADD COLUMN "draft" smallint UNSIGNED NOT NULL DEFAULT 0;
+ -- SQLINES LICENSE FOR EVALUATION USE ONLY
+ CREATE TABLE IF NOT EXISTS "#__draft" (
+  article_id int check (article_id > 0) NOT NULL,
+  version_id int check (version_id > 0) NOT NULL,
+  state smallint NOT NULL DEFAULT '0',
+  hashval varchar(2083) NOT NULL DEFAULT '',
+  shared_date timestamp(0) DEFAULT NULL,
+  PRIMARY KEY(article_id, version_id)
+) ;
+
+ALTER TABLE "#__content"  ADD COLUMN shared tinyint(3) UNSIGNED NOT NULL DEFAULT '0';
+ALTER TABLE "#__content" ADD COLUMN draft tinyint(3) UNSIGNED NOT NULL DEFAULT '0';
