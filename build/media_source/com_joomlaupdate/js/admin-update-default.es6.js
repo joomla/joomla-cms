1054ec2a92ad10414ca4b88d3dbf053e715ab071
Nicholas K. Dionysopoulos <nicholas@akeeba.com>
2021-08-27 12:38:48 +0300

    Wrap the extraction start in a conditional
    Per @dgrammatiko suggestion

--- build/media_source/com_joomlaupdate/js/admin-update-default.es6.js
+++ build/media_source/com_joomlaupdate/js/admin-update-default.es6.js
@@ -139,8 +139,12 @@ Joomla.Update = window.Joomla.Update || {
 };
 
 const JoomlaUpdateOptions = Joomla.getOptions('joomlaupdate');
-Joomla.Update.password = JoomlaUpdateOptions.password;
-Joomla.Update.totalsize = JoomlaUpdateOptions.totalsize;
-Joomla.Update.ajax_url = JoomlaUpdateOptions.ajax_url;
-Joomla.Update.return_url = JoomlaUpdateOptions.return_url;
-Joomla.Update.startExtract();
+
+if (JoomlaUpdateOptions && Object.keys(JoomlaUpdateOptions).length) {
+  Joomla.Update.password = JoomlaUpdateOptions.password;
+  Joomla.Update.totalsize = JoomlaUpdateOptions.totalsize;
+  Joomla.Update.ajax_url = JoomlaUpdateOptions.ajax_url;
+  Joomla.Update.return_url = JoomlaUpdateOptions.return_url;
+
+  Joomla.Update.startExtract();
+}
