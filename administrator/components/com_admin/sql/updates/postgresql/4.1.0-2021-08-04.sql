737fa3997d5ef21860c5ad4a161a721c6d1a6a8c
ditsuke <ditsuke@protonmail.com>
2021-08-30 12:59:23 +0530

    Refactor plgTaskTesttasks => plgTaskDemotasks
    - Fixes author in manifest.
    - Fixes language file names.
    - Fixes license headers for language files.
    - Refactors plugin name.
    - Update SQL DDL.

--- administrator/components/com_admin/sql/updates/postgresql/4.1.0-2021-08-04.sql
+++ administrator/components/com_admin/sql/updates/postgresql/4.1.0-2021-08-04.sql
@@ -3,7 +3,7 @@ INSERT INTO "#__extensions" ("package_id", "name", "type", "element", "folder", 
 							 "protected", "locked", "manifest_cache", "params", "custom_data", "ordering", "state")
 VALUES (0, 'com_scheduler', 'component', 'com_scheduler', '', 1, 1, 1, 0, 1, '', '{}', '', 0, 0);
 
--- Add "plg_task_testtasks" to "#__extensions"
+-- Add "plg_task_demotasks" to "#__extensions"
 INSERT INTO "#__extensions" ("package_id", "name", "type", "element", "folder", "client_id", "enabled", "access",
 							 "protected", "locked", "manifest_cache", "params", "custom_data", "ordering", "state")
-VALUES (0, 'plg_task_testtasks', 'plugin', 'testtasks', 'task', 0, 1, 1, 0, 0, '', '{}', '', 15, 0);
+VALUES (0, 'plg_task_demotasks', 'plugin', 'demotasks', 'task', 0, 1, 1, 0, 0, '', '{}', '', 15, 0);
