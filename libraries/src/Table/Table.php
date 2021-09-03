4753e8e0c1660d4322f1d824bb5a1a8db24f7d67
Сергей <koreshs@mail.ru>
2021-09-03 18:57:13 +0300

    Update Table.php

--- libraries/src/Table/Table.php
+++ libraries/src/Table/Table.php
@@ -300,7 +300,7 @@ abstract class Table extends CMSObject implements TableInterface, DispatcherAwar
 			// Checking whether it is possible to determine the type of an uninitialized property.
 			if (!method_exists($prop, 'getType'))
 			{
-				$this->_propertyTypes[$prop->name] = gettype($this->{$prop->name});
+				$this->_propertyTypes[$prop->name] = null;
 				continue;
 			}
 
