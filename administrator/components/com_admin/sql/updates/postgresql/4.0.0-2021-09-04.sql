dfc469d44c31ee388b89771a1e84ec02cad6fe12
Richard Fath <richard67@users.noreply.github.com>
2021-09-05 16:01:51 +0200

    Fix delete statement

--- administrator/components/com_admin/sql/updates/postgresql/4.0.0-2021-09-04.sql
+++ administrator/components/com_admin/sql/updates/postgresql/4.0.0-2021-09-04.sql
@@ -23,9 +23,11 @@ DELETE FROM "#__update_sites"
 
 DELETE FROM "#__extensions"
  WHERE "type" = 'package' AND "element" = 'pkg_search'
-   AND (SELECT COUNT(a."extension_id")
-          FROM "#__extensions" a
-         WHERE (a."type" = 'component' AND a."element" = 'com_search')
-            OR (a."type" = 'module' AND a."element" = 'mod_search' AND a."client_id" = 0)
-            OR (a."type" = 'plugin' AND a."element" IN ('categories', 'contacts', 'content', 'newsfeeds', 'tags') AND a."folder" = 'search')
+   AND (SELECT b."count"
+          FROM (SELECT COUNT(a."extension_id") AS "count"
+                  FROM "#__extensions" a
+                 WHERE (a."type" = 'component' AND a."element" = 'com_search')
+                    OR (a."type" = 'module' AND a."element" = 'mod_search' AND a."client_id" = 0)
+                    OR (a."type" = 'plugin' AND a."element" IN ('categories', 'contacts', 'content', 'newsfeeds', 'tags') AND a."folder" = 'search')
+               ) b
        ) = 0;
