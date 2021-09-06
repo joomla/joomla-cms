f52c16804b8d6ec79f3ac5aaa3e6763189baa03b
impressionestudio <info@impression-estudio.gr>
2021-09-06 21:02:21 +0300

    Important but easy improvements to the code of the article view (6)
    These changes are part of the issue #35342 (https://github.com/joomla/joomla-cms/issues/35342).
    Mainly they are changes to the names of the classes.

--- layouts/joomla/content/info_block/create_date.php
+++ layouts/joomla/content/info_block/create_date.php
@@ -13,7 +13,7 @@ use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
 
 ?>
-<dd class="create">
+<dd class="article__info__created">
 	<span class="icon-calendar icon-fw" aria-hidden="true"></span>
 	<time datetime="<?php echo HTMLHelper::_('date', $displayData['item']->created, 'c'); ?>" itemprop="dateCreated">
 		<?php echo Text::sprintf('COM_CONTENT_CREATED_DATE_ON', HTMLHelper::_('date', $displayData['item']->created, Text::_('DATE_FORMAT_LC3'))); ?>
