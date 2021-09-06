dcd6fe30f6a79321ad345f5dd8f744b5f4d2cd5d
impressionestudio <info@impression-estudio.gr>
2021-09-06 21:01:41 +0300

    Important but easy improvements to the code of the article view (4)
    These changes are part of the issue #35342 (https://github.com/joomla/joomla-cms/issues/35342).
    Mainly they are changes to the names of the classes.

--- layouts/joomla/content/info_block/associations.php
+++ layouts/joomla/content/info_block/associations.php
@@ -18,7 +18,7 @@ use Joomla\CMS\Router\Route;
 <?php if (!empty($displayData['item']->associations)) : ?>
 <?php $associations = $displayData['item']->associations; ?>
 
-<dd class="association">
+<dd class="article__info__association">
 	<span class="icon-globe icon-fw" aria-hidden="true"></span>
 	<?php echo Text::_('JASSOCIATIONS'); ?>
 	<?php foreach ($associations as $association) : ?>
@@ -26,9 +26,9 @@ use Joomla\CMS\Router\Route;
 			<?php $flag = HTMLHelper::_('image', 'mod_languages/' . $association['language']->image . '.gif', $association['language']->title_native, array('title' => $association['language']->title_native), true); ?>
 			<a href="<?php echo Route::_($association['item']); ?>"><?php echo $flag; ?></a>
 		<?php else : ?>
-			<?php $class = 'btn btn-secondary btn-sm btn-' . strtolower($association['language']->lang_code); ?>
+			<?php $class = strtolower($association['language']->lang_code); ?>
 			<a class="<?php echo $class; ?>" title="<?php echo $association['language']->title_native; ?>" href="<?php echo Route::_($association['item']); ?>"><?php echo $association['language']->lang_code; ?>
-				<span class="visually-hidden"><?php echo $association['language']->title_native; ?></span>
+				<span><?php echo $association['language']->title_native; ?></span>
 			</a>
 		<?php endif; ?>
 	<?php endforeach; ?>
