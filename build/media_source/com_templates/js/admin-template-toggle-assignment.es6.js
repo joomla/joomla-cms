effdc84f6ceda07ef127d4cb85a8b481073ec074
dgrammatiko <d.grammatiko@gmail.com>
2021-09-06 19:33:58 +0200

    JS Cleanup

--- build/media_source/com_templates/js/admin-template-toggle-assignment.es6.js
+++ build/media_source/com_templates/js/admin-template-toggle-assignment.es6.js
@@ -2,21 +2,16 @@
  * @copyright  (C) 2019 Open Source Matters, Inc. <https://www.joomla.org>
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
+if (!Joomla) {
+  throw new Error('The Joomla API wasn\'t initialized properly');
+}
 
-Joomla = window.Joomla || {};
+Joomla.toggleAll = () => [].slice.call(document.querySelectorAll('.chk-menulink'))
+  .forEach((box) => {
+    box.checked = !box.checked;
+  });
 
-((Joomla) => {
-  Joomla.toggleAll = () => {
-    const checkBoxes = [].slice.call(document.querySelectorAll('.chk-menulink'));
-    checkBoxes.forEach((checkBox) => {
-      checkBox.checked = !checkBox.checked;
-    });
-  };
-
-  Joomla.toggleMenutype = (a) => {
-    const checkBox = [].slice.call(document.getElementsByClassName(`menutype-${a}`));
-    checkBox.forEach((element) => {
-      element.checked = !element.checked;
-    });
-  };
-})(Joomla);
+Joomla.toggleMenutype = (a) => [].slice.call(document.querySelectorAll(`.menutype-${a}`))
+  .forEach((box) => {
+    box.checked = !box.checked;
+  });
--- build/media_source/com_templates/js/admin-template-toggle-switch.es6.js
+++ build/media_source/com_templates/js/admin-template-toggle-switch.es6.js
@@ -2,103 +2,91 @@
  * @copyright  (C) 2018 Open Source Matters, Inc. <https://www.joomla.org>
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
-(() => {
-  'use strict';
 
-  const showDiffChangedOff = () => {
-    const diffMain = document.getElementById('diff-main');
+const diffMain = document.getElementById('diff-main');
+const override = document.getElementById('override-pane');
+const corePane = document.getElementById('core-pane');
+const JformShowDiffOn = document.getElementById('jform_show_diff1');
+const JformShowDiffOff = document.getElementById('jform_show_diff0');
+const JformShowCoreOn = document.getElementById('jform_show_core1');
+const JformShowCoreOff = document.getElementById('jform_show_core0');
 
-    if (diffMain) {
-      diffMain.classList.remove('active');
+const showDiffChangedOff = () => {
+  if (diffMain) {
+    diffMain.classList.remove('active');
 
-      if (typeof Storage !== 'undefined') {
-        localStorage.removeItem('diffSwitchState');
-      }
+    if (typeof Storage !== 'undefined') {
+      localStorage.removeItem('diffSwitchState');
     }
-  };
+  }
+};
 
-  const showDiffChangedOn = () => {
-    const diffMain = document.getElementById('diff-main');
+const showDiffChangedOn = () => {
+  if (diffMain) {
+    diffMain.classList.add('active');
 
-    if (diffMain) {
-      diffMain.classList.add('active');
-
-      if (typeof Storage !== 'undefined') {
-        localStorage.setItem('diffSwitchState', 'checked');
-      }
+    if (typeof Storage !== 'undefined') {
+      localStorage.setItem('diffSwitchState', 'checked');
     }
-  };
+  }
+};
 
-  const showCoreChangedOff = () => {
-    const override = document.getElementById('override-pane');
-    const corePane = document.getElementById('core-pane');
-    const fieldset = override.parentElement.parentElement;
+const showCoreChangedOff = () => {
+  const fieldset = override.parentElement.parentElement;
+  if (corePane && override) {
+    corePane.classList.remove('active');
 
-    if (corePane && override) {
-      corePane.classList.remove('active');
-
-      if (fieldset.classList.contains('options-grid-form-half')) {
-        fieldset.classList.remove('options-grid-form-half');
-        fieldset.classList.add('options-grid-form-full');
-      }
-
-      if (typeof Storage !== 'undefined') {
-        localStorage.removeItem('coreSwitchState');
-      }
-    }
-  };
-
-  const showCoreChangedOn = () => {
-    const override = document.getElementById('override-pane');
-    const corePane = document.getElementById('core-pane');
-    const fieldset = override.parentElement.parentElement;
-
-    if (corePane && override) {
-      corePane.classList.add('active');
-
-      if (fieldset.classList.contains('options-grid-form-full')) {
-        fieldset.classList.remove('options-grid-form-full');
-        fieldset.classList.add('options-grid-form-half');
-      }
-
-      if (Joomla.editors.instances.jform_core) {
-        Joomla.editors.instances.jform_core.refresh();
-      }
-
-      if (typeof Storage !== 'undefined') {
-        localStorage.setItem('coreSwitchState', 'checked');
-      }
-    }
-  };
-
-  document.addEventListener('DOMContentLoaded', () => {
-    const JformShowDiffOn = document.getElementById('jform_show_diff1');
-    const JformShowDiffOff = document.getElementById('jform_show_diff0');
-    const JformShowCoreOn = document.getElementById('jform_show_core1');
-    const JformShowCoreOff = document.getElementById('jform_show_core0');
-
-    if (JformShowDiffOn && JformShowDiffOff) {
-      JformShowDiffOn.addEventListener('click', showDiffChangedOn);
-      JformShowDiffOff.addEventListener('click', showDiffChangedOff);
+    if (fieldset.classList.contains('options-grid-form-half')) {
+      fieldset.classList.remove('options-grid-form-half');
+      fieldset.classList.add('options-grid-form-full');
     }
 
-    if (JformShowCoreOn && JformShowCoreOff) {
-      JformShowCoreOn.addEventListener('click', showCoreChangedOn);
-      JformShowCoreOff.addEventListener('click', showCoreChangedOff);
+    if (typeof Storage !== 'undefined') {
+      localStorage.removeItem('coreSwitchState');
+    }
+  }
+};
+
+const showCoreChangedOn = () => {
+  const fieldset = override.parentElement.parentElement;
+  if (corePane && override) {
+    corePane.classList.add('active');
+
+    if (fieldset.classList.contains('options-grid-form-full')) {
+      fieldset.classList.remove('options-grid-form-full');
+      fieldset.classList.add('options-grid-form-half');
     }
 
-    if (typeof Storage !== 'undefined' && localStorage.getItem('coreSwitchState') && JformShowCoreOn) {
-      JformShowCoreOn.checked = true;
-      JformShowCoreOff.checked = false;
-
-      showCoreChangedOn();
+    if (Joomla.editors.instances.jform_core) {
+      Joomla.editors.instances.jform_core.refresh();
     }
 
-    if (typeof Storage !== 'undefined' && localStorage.getItem('diffSwitchState') && JformShowDiffOn) {
-      JformShowDiffOn.checked = true;
-      JformShowDiffOff.checked = false;
-
-      showDiffChangedOn();
+    if (typeof Storage !== 'undefined') {
+      localStorage.setItem('coreSwitchState', 'checked');
     }
-  });
-})();
+  }
+};
+
+if (JformShowDiffOn && JformShowDiffOff) {
+  JformShowDiffOn.addEventListener('click', showDiffChangedOn);
+  JformShowDiffOff.addEventListener('click', showDiffChangedOff);
+}
+
+if (JformShowCoreOn && JformShowCoreOff) {
+  JformShowCoreOn.addEventListener('click', showCoreChangedOn);
+  JformShowCoreOff.addEventListener('click', showCoreChangedOff);
+}
+
+if (typeof Storage !== 'undefined' && localStorage.getItem('coreSwitchState') && JformShowCoreOn) {
+  JformShowCoreOn.checked = true;
+  JformShowCoreOff.checked = false;
+
+  showCoreChangedOn();
+}
+
+if (typeof Storage !== 'undefined' && localStorage.getItem('diffSwitchState') && JformShowDiffOn) {
+  JformShowDiffOn.checked = true;
+  JformShowDiffOff.checked = false;
+
+  showDiffChangedOn();
+}
