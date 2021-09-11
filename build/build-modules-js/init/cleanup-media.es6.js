73a26dc11439d478de42f583c00b9b59840157e6
Dimitris Grammatikogiannis <dg@dgrammatiko.dev>
2021-09-11 13:14:32 +0200

    Update cleanup-media.es6.js

--- build/build-modules-js/init/cleanup-media.es6.js
+++ build/build-modules-js/init/cleanup-media.es6.js
@@ -12,23 +12,33 @@ const RootPath = process.cwd();
  * @returns {Promise}
  */
 module.exports.cleanVendors = async () => {
-  // eslint-disable-next-line no-console
-  console.log('Cleanup the Vendor ');
-
-  try {
-    const mediaFolder = await stat(join(RootPath, 'libraries/vendor/maximebf/debugbar/src/DebugBar/Resources'));
-    await mediaFolder.isDirectory();
-  } catch (e) {
+  if (process.env.TRANSLATION_BOT === 'YES') {
     // eslint-disable-next-line no-console
-    console.error('You need to run `npm install` AFTER the command `composer install`!!!. The debug plugin HASN\'T installed all its front end assets');
+    console.log('Cleanup the Vendor skipped');
     return;
   }
 
-  // Recreate the media folder
-  await mkdir(join(RootPath, 'media/vendor/debugbar'), { recursive: true, mode: 0o755 });
+  // eslint-disable-next-line no-console
+  console.log('Cleanup the Vendor ');
 
-  // Copy some assets from a PHP package
-  await copy(join(RootPath, 'libraries/vendor/maximebf/debugbar/src/DebugBar/Resources'), join(RootPath, 'media/vendor/debugbar'), { preserveTimestamps: true });
-  await remove(join(RootPath, 'media/vendor/debugbar/vendor/font-awesome'));
-  await remove(join(RootPath, 'media/vendor/debugbar/vendor/jquery'));
+  const mediaFolder = await stat(join(RootPath, 'libraries/vendor/maximebf/debugbar/src/DebugBar/Resources'));
+
+  if (await mediaFolder.isDirectory()) {
+    // Remove the vendor folder
+    // await remove(join(RootPath, 'media'));
+    // eslint-disable-next-line no-console
+    // console.error('/media has been removed.');
+
+    // Recreate the media folder
+    await mkdir(join(RootPath, 'media/vendor/debugbar'), { recursive: true, mode: 0o755 });
+
+    // Copy some assets from a PHP package
+    await copy(join(RootPath, 'libraries/vendor/maximebf/debugbar/src/DebugBar/Resources'), join(RootPath, 'media/vendor/debugbar'), { preserveTimestamps: true });
+    await remove(join(RootPath, 'media/vendor/debugbar/vendor/font-awesome'));
+    await remove(join(RootPath, 'media/vendor/debugbar/vendor/jquery'));
+  } else {
+    // eslint-disable-next-line no-console
+    console.error('You need to run `npm install` AFTER the command `composer install`!!!. The debug plugin HASN\'T installed all its front end assets');
+    process.exit(1);
+  }
 };
