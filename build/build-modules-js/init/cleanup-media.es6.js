8d0dafc611c54243b811d768cac66a0a239f8426
Dimitris Grammatikogiannis <dg@dgrammatiko.dev>
2021-09-11 12:39:15 +0200

    Allow the translation bot to recompile the htmls

--- build/build-modules-js/init/cleanup-media.es6.js
+++ build/build-modules-js/init/cleanup-media.es6.js
@@ -11,28 +11,24 @@ const RootPath = process.cwd();
  *
  * @returns {Promise}
  */
-module.exports.cleanVendors = async () => {
+module.exports.cleanVendors = async (skip = true) => {
   // eslint-disable-next-line no-console
   console.log('Cleanup the Vendor ');
 
-  const mediaFolder = await stat(join(RootPath, 'libraries/vendor/maximebf/debugbar/src/DebugBar/Resources'));
-
-  if (await mediaFolder.isDirectory()) {
-    // Remove the vendor folder
-    // await remove(join(RootPath, 'media'));
-    // eslint-disable-next-line no-console
-    // console.error('/media has been removed.');
-
-    // Recreate the media folder
-    await mkdir(join(RootPath, 'media/vendor/debugbar'), { recursive: true, mode: 0o755 });
-
-    // Copy some assets from a PHP package
-    await copy(join(RootPath, 'libraries/vendor/maximebf/debugbar/src/DebugBar/Resources'), join(RootPath, 'media/vendor/debugbar'), { preserveTimestamps: true });
-    await remove(join(RootPath, 'media/vendor/debugbar/vendor/font-awesome'));
-    await remove(join(RootPath, 'media/vendor/debugbar/vendor/jquery'));
-  } else {
+  try {
+    const mediaFolder = await stat(join(RootPath, 'libraries/vendor/maximebf/debugbar/src/DebugBar/Resources'));
+    await mediaFolder.isDirectory();
+  } catch (e) {
     // eslint-disable-next-line no-console
     console.error('You need to run `npm install` AFTER the command `composer install`!!!. The debug plugin HASN\'T installed all its front end assets');
-    process.exit(1);
+    return;
   }
+
+  // Recreate the media folder
+  await mkdir(join(RootPath, 'media/vendor/debugbar'), { recursive: true, mode: 0o755 });
+
+  // Copy some assets from a PHP package
+  await copy(join(RootPath, 'libraries/vendor/maximebf/debugbar/src/DebugBar/Resources'), join(RootPath, 'media/vendor/debugbar'), { preserveTimestamps: true });
+  await remove(join(RootPath, 'media/vendor/debugbar/vendor/font-awesome'));
+  await remove(join(RootPath, 'media/vendor/debugbar/vendor/jquery'));
 };
