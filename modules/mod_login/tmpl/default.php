9409a8220bedb986f35452e6cd51147def9dc6aa
Krshivam25 <49578630+Krshivam25@users.noreply.github.com>
2021-09-22 16:38:16 +0530

    Removed Login file

--- modules/mod_login/tmpl/default.php
+++ modules/mod_login/tmpl/default.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @package     Joomla.Site
  * @subpackage  mod_login
@@ -6,7 +7,6 @@
  * @copyright   (C) 2006 Open Source Matters, Inc. <https://www.joomla.org>
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
-
 defined('_JEXEC') or die;
 
 use Joomla\CMS\Component\ComponentHelper;
@@ -19,25 +19,22 @@ $app->getDocument()->getWebAssetManager()
 	->useScript('core')
 	->useScript('keepalive')
 	->useScript('field.passwordview');
-
 Text::script('JSHOWPASSWORD');
 Text::script('JHIDEPASSWORD');
 ?>
 <form id="login-form-<?php echo $module->id; ?>" class="mod-login" action="<?php echo Route::_('index.php', true); ?>" method="post">
-
 	<?php if ($params->get('pretext')) : ?>
 		<div class="mod-login__pretext pretext">
 			<p><?php echo $params->get('pretext'); ?></p>
 		</div>
 	<?php endif; ?>
-
 	<div class="mod-login__userdata userdata">
 		<div class="mod-login__username form-group">
 			<?php if (!$params->get('usetext', 0)) : ?>
 				<div class="input-group">
 					<input id="modlgn-username-<?php echo $module->id; ?>" type="text" name="username" class="form-control" autocomplete="username" placeholder="<?php echo Text::_('MOD_LOGIN_VALUE_USERNAME'); ?>">
 					<label for="modlgn-username-<?php echo $module->id; ?>" class="visually-hidden"><?php echo Text::_('MOD_LOGIN_VALUE_USERNAME'); ?></label>
-					<span class="input-group-text">
+					<span class="input-group-text" title="<?php echo Text::_('MOD_LOGIN_VALUE_USERNAME'); ?>">
 						<span class="icon-user icon-fw" aria-hidden="true"></span>
 					</span>
 				</div>
@@ -46,7 +43,6 @@ Text::script('JHIDEPASSWORD');
 				<input id="modlgn-username-<?php echo $module->id; ?>" type="text" name="username" class="form-control" autocomplete="username" placeholder="<?php echo Text::_('MOD_LOGIN_VALUE_USERNAME'); ?>">
 			<?php endif; ?>
 		</div>
-
 		<div class="mod-login__password form-group">
 			<?php if (!$params->get('usetext', 0)) : ?>
 				<div class="input-group">
@@ -62,7 +58,6 @@ Text::script('JHIDEPASSWORD');
 				<input id="modlgn-passwd-<?php echo $module->id; ?>" type="password" name="password" autocomplete="current-password" class="form-control" placeholder="<?php echo Text::_('JGLOBAL_PASSWORD'); ?>">
 			<?php endif; ?>
 		</div>
-
 		<?php if (count($twofactormethods) > 1) : ?>
 			<div class="mod-login__twofactor form-group">
 				<?php if (!$params->get('usetext', 0)) : ?>
@@ -87,69 +82,55 @@ Text::script('JHIDEPASSWORD');
 				<?php endif; ?>
 			</div>
 		<?php endif; ?>
-
 		<?php if (PluginHelper::isEnabled('system', 'remember')) : ?>
 			<div class="mod-login__remember form-group">
 				<div id="form-login-remember-<?php echo $module->id; ?>" class="form-check">
-					<label for="form-check-label">
-						<input type="checkbox" name="remember" id="form-check-label" value="yes">
+					<label class="form-check-label">
+						<input type="checkbox" name="remember" class="form-check-input" value="yes">
 						<?php echo Text::_('MOD_LOGIN_REMEMBER_ME'); ?>
 					</label>
 				</div>
 			</div>
 		<?php endif; ?>
-
-		<?php foreach($extraButtons as $button):
+		<?php foreach ($extraButtons as $button) :
 			$dataAttributeKeys = array_filter(array_keys($button), function ($key) {
 				return substr($key, 0, 5) == 'data-';
 			});
-			?>
+		?>
 			<div class="mod-login__submit form-group">
-				<button type="button"
-						class="btn btn-secondary w-100 mt-4 <?php echo $button['class'] ?? '' ?>"
-						<?php foreach ($dataAttributeKeys as $key): ?>
-						<?php echo $key ?>="<?php echo $button[$key] ?>"
-						<?php endforeach; ?>
-						<?php if ($button['onclick']): ?>
-						onclick="<?php echo $button['onclick'] ?>"
-						<?php endif; ?>
-						title="<?php echo Text::_($button['label']) ?>"
-						id="<?php echo $button['id'] ?>"
-						>
-					<?php if (!empty($button['icon'])): ?>
+				<button type="button" class="btn btn-secondary w-100 mt-4 <?php echo $button['class'] ?? '' ?>" <?php foreach ($dataAttributeKeys as $key) : ?> <?php echo $key ?>="<?php echo $button[$key] ?>" <?php endforeach; ?> <?php if ($button['onclick']) : ?> onclick="<?php echo $button['onclick'] ?>" <?php endif; ?> title="<?php echo Text::_($button['label']) ?>" id="<?php echo $button['id'] ?>">
+					<?php if (!empty($button['icon'])) : ?>
 						<span class="<?php echo $button['icon'] ?>"></span>
-					<?php elseif (!empty($button['image'])): ?>
+					<?php elseif (!empty($button['image'])) : ?>
 						<?php echo $button['image']; ?>
-					<?php elseif (!empty($button['svg'])): ?>
+					<?php elseif (!empty($button['svg'])) : ?>
 						<?php echo $button['svg']; ?>
 					<?php endif; ?>
 					<?php echo Text::_($button['label']) ?>
 				</button>
 			</div>
 		<?php endforeach; ?>
-
 		<div class="mod-login__submit form-group">
 			<button type="submit" name="Submit" class="btn btn-primary"><?php echo Text::_('JLOGIN'); ?></button>
 		</div>
-
 		<?php
-			$usersConfig = ComponentHelper::getParams('com_users'); ?>
-			<ul class="mod-login__options list-unstyled">
-				<li>
-					<a href="<?php echo Route::_('index.php?option=com_users&view=reset'); ?>">
+		$usersConfig = ComponentHelper::getParams('com_users'); ?>
+		<ul class="mod-login__options list-unstyled">
+			<li>
+				<a href="<?php echo Route::_('index.php?option=com_users&view=reset'); ?>">
 					<?php echo Text::_('MOD_LOGIN_FORGOT_YOUR_PASSWORD'); ?></a>
-				</li>
-				<li>
-					<a href="<?php echo Route::_('index.php?option=com_users&view=remind'); ?>">
+			</li>
+			<li>
+				<a href="<?php echo Route::_('index.php?option=com_users&view=remind'); ?>">
 					<?php echo Text::_('MOD_LOGIN_FORGOT_YOUR_USERNAME'); ?></a>
-				</li>
-				<?php if ($usersConfig->get('allowUserRegistration')) : ?>
+			</li>
+			<?php if ($usersConfig->get('allowUserRegistration')) : ?>
 				<li>
 					<a href="<?php echo Route::_($registerLink); ?>">
-					<?php echo Text::_('MOD_LOGIN_REGISTER'); ?> <span class="icon-register" aria-hidden="true"></span></a>
+						<?php echo Text::_('MOD_LOGIN_REGISTER'); ?> <span class="icon-register" aria-hidden="true"></span></a>
 				</li>
-				<?php endif; ?>
-			</ul>
+			<?php endif; ?>
+		</ul>
 		<input type="hidden" name="option" value="com_users">
 		<input type="hidden" name="task" value="user.login">
 		<input type="hidden" name="return" value="<?php echo $return; ?>">
