a8843509ff230af2ab9f3fa2f08416d6e1f2b30f
Krshivam25 <49578630+Krshivam25@users.noreply.github.com>
2021-09-22 16:43:40 +0530

    Removed login file

--- modules/mod_login/tmpl/default.php
+++ modules/mod_login/tmpl/default.php
@@ -7,6 +7,7 @@
  * @copyright   (C) 2006 Open Source Matters, Inc. <https://www.joomla.org>
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
+
 defined('_JEXEC') or die;
 
 use Joomla\CMS\Component\ComponentHelper;
@@ -19,15 +20,18 @@ $app->getDocument()->getWebAssetManager()
 	->useScript('core')
 	->useScript('keepalive')
 	->useScript('field.passwordview');
+
 Text::script('JSHOWPASSWORD');
 Text::script('JHIDEPASSWORD');
 ?>
 <form id="login-form-<?php echo $module->id; ?>" class="mod-login" action="<?php echo Route::_('index.php', true); ?>" method="post">
+
 	<?php if ($params->get('pretext')) : ?>
 		<div class="mod-login__pretext pretext">
 			<p><?php echo $params->get('pretext'); ?></p>
 		</div>
 	<?php endif; ?>
+
 	<div class="mod-login__userdata userdata">
 		<div class="mod-login__username form-group">
 			<?php if (!$params->get('usetext', 0)) : ?>
@@ -43,6 +47,7 @@ Text::script('JHIDEPASSWORD');
 				<input id="modlgn-username-<?php echo $module->id; ?>" type="text" name="username" class="form-control" autocomplete="username" placeholder="<?php echo Text::_('MOD_LOGIN_VALUE_USERNAME'); ?>">
 			<?php endif; ?>
 		</div>
+
 		<div class="mod-login__password form-group">
 			<?php if (!$params->get('usetext', 0)) : ?>
 				<div class="input-group">
@@ -58,6 +63,7 @@ Text::script('JHIDEPASSWORD');
 				<input id="modlgn-passwd-<?php echo $module->id; ?>" type="password" name="password" autocomplete="current-password" class="form-control" placeholder="<?php echo Text::_('JGLOBAL_PASSWORD'); ?>">
 			<?php endif; ?>
 		</div>
+
 		<?php if (count($twofactormethods) > 1) : ?>
 			<div class="mod-login__twofactor form-group">
 				<?php if (!$params->get('usetext', 0)) : ?>
@@ -82,6 +88,7 @@ Text::script('JHIDEPASSWORD');
 				<?php endif; ?>
 			</div>
 		<?php endif; ?>
+
 		<?php if (PluginHelper::isEnabled('system', 'remember')) : ?>
 			<div class="mod-login__remember form-group">
 				<div id="form-login-remember-<?php echo $module->id; ?>" class="form-check">
@@ -92,6 +99,7 @@ Text::script('JHIDEPASSWORD');
 				</div>
 			</div>
 		<?php endif; ?>
+
 		<?php foreach ($extraButtons as $button) :
 			$dataAttributeKeys = array_filter(array_keys($button), function ($key) {
 				return substr($key, 0, 5) == 'data-';
@@ -110,9 +118,11 @@ Text::script('JHIDEPASSWORD');
 				</button>
 			</div>
 		<?php endforeach; ?>
+
 		<div class="mod-login__submit form-group">
 			<button type="submit" name="Submit" class="btn btn-primary"><?php echo Text::_('JLOGIN'); ?></button>
 		</div>
+
 		<?php
 		$usersConfig = ComponentHelper::getParams('com_users'); ?>
 		<ul class="mod-login__options list-unstyled">
