d7d77259a80c2c344c2291fe217e78cefb929133
dgrammatiko <d.grammatiko@gmail.com>
2021-09-26 19:25:42 +0200

    url

--- plugins/editors/tinymce/tinymce.php
+++ plugins/editors/tinymce/tinymce.php
@@ -252,8 +252,8 @@ class PlgEditorTinymce extends CMSPlugin
 		$levelParams->loadObject($extraOptions);
 
 		// Set the selected skin
-		$skinRelPath = dirname(HTMLHelper::stylesheet('plg_editors_tinymce/skins/ui/oxide/skin.css', ['version' => 'auto', 'relative' => true, 'pathOnly' => true]));
-		$skin = Uri::root() . substr($skinRelPath, 1);
+		$skinRelPath = dirname(HTMLHelper::stylesheet('plg_editors_tinymce/skins/ui/oxide/skin.css', ['relative' => true, 'pathOnly' => true]));
+		$skin = Uri::root(false) . substr($skinRelPath, 1);
 
 		$langMode   = $levelParams->get('lang_mode', 1);
 		$langPrefix = $levelParams->get('lang_code', 'en');
