8d6dc6ec3d2268f5c2e480656984198cb1f54713
Benjamin Trenkle <benjamin.trenkle@wicked-software.de>
2021-09-28 20:24:44 +0200

    Reset to dev

--- libraries/src/Version.php
+++ libraries/src/Version.php
@@ -61,7 +61,7 @@ final class Version
 	 * @var    string
 	 * @since  3.8.0
 	 */
-	const EXTRA_VERSION = 'alpha1';
+	const EXTRA_VERSION = 'dev';
 
 	/**
 	 * Development status.
@@ -69,7 +69,7 @@ final class Version
 	 * @var    string
 	 * @since  3.5
 	 */
-	const DEV_STATUS = 'Alpha';
+	const DEV_STATUS = 'Development';
 
 	/**
 	 * Code name.
@@ -93,7 +93,7 @@ final class Version
 	 * @var    string
 	 * @since  3.5
 	 */
-	const RELTIME = '18:00';
+	const RELTIME = '18:25';
 
 	/**
 	 * Release timezone.
