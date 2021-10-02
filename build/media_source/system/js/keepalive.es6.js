de9bfafc68d249158f6e8ad776ceebe861a75225
dgrammatiko <d.grammatiko@gmail.com>
2021-10-02 13:28:11 +0200

    more unneeded IIFE

--- build/media_source/system/js/keepalive.es6.js
+++ build/media_source/system/js/keepalive.es6.js
@@ -15,17 +15,15 @@ if (!window.Joomla) {
   throw new Error('Joomla API was not properly initialised');
 }
 
-(() => {
-  const keepAliveOptions = Joomla.getOptions('system.keepalive');
-  const keepAliveInterval = keepAliveOptions && keepAliveOptions.interval ? parseInt(keepAliveOptions.interval, 10) : 45 * 1000;
-  let keepAliveUri = keepAliveOptions && keepAliveOptions.uri ? keepAliveOptions.uri.replace(/&amp;/g, '&') : '';
+const keepAliveOptions = Joomla.getOptions('system.keepalive');
+const keepAliveInterval = keepAliveOptions && keepAliveOptions.interval ? parseInt(keepAliveOptions.interval, 10) : 45 * 1000;
+let keepAliveUri = keepAliveOptions && keepAliveOptions.uri ? keepAliveOptions.uri.replace(/&amp;/g, '&') : '';
 
-  // Fallback in case no keepalive uri was found.
-  if (keepAliveUri === '') {
-    const systemPaths = Joomla.getOptions('system.paths');
+// Fallback in case no keepalive uri was found.
+if (keepAliveUri === '') {
+  const systemPaths = Joomla.getOptions('system.paths');
 
-    keepAliveUri = `${(systemPaths ? `${systemPaths.root}/index.php` : window.location.pathname)}?option=com_ajax&format=json`;
-  }
+  keepAliveUri = `${(systemPaths ? `${systemPaths.root}/index.php` : window.location.pathname)}?option=com_ajax&format=json`;
+}
 
-  setInterval(() => navigator.sendBeacon(keepAliveUri), keepAliveInterval);
-  })();
+setInterval(() => navigator.sendBeacon(keepAliveUri), keepAliveInterval);
