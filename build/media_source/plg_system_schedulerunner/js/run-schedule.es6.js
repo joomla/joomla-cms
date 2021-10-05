14e1c83f2bdc00b7469c95a026fb6a2c030cf8e2
ditsuke <ditsuke@protonmail.com>
2021-10-05 22:10:27 +0530

    Add AJAX requests script to scheduleRunner
    JS sets up a navigator.sendBeacon() callback to make
    requests to trigger a `com_ajax` backed AJAX listener offered by
    plgSystemScheduleRunner.

---
+++ build/media_source/plg_system_schedulerunner/js/run-schedule.es6.js
@@ -0,0 +1,26 @@
+/**
+ * @copyright   (C) 2021 Open Source Matters, Inc. <https://www.joomla.org>
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+/**
+ * Makes calls to com_ajax to trigger the Scheduler.
+ *
+ * Used for lazy scheduling of tasks.
+ *
+ * @package  Joomla.Plugins
+ * @subpackage System.ScheduleRunner
+ *
+ * @since    __DEPLOY_VERSION__
+ */
+if (!window.Joomla) {
+  throw new Error('Joomla API was not properly initialised');
+}
+
+const scheduleRunnerOptions = Joomla.getOptions('plg_system.schedulerR');
+const systemPaths = Joomla.getOptions('system.paths');
+
+const scheduleRunnerInterval = scheduleRunnerOptions && scheduleRunnerOptions.interval ? parseInt(scheduleRunnerOptions.interval, 10) : 300 * 1000;
+const scheduleRunnerUri = `${(systemPaths ? `${systemPaths.root}/index.php` : window.location.pathname)}?option=com_ajax&format=raw&plugin=runScheduler&group=system`;
+
+setInterval(() => navigator.sendBeacon(scheduleRunnerUri), scheduleRunnerInterval);
