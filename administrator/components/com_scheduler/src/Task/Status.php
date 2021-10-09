88775093488d2f643d7d4f3c4e436fdabb95fc1d
ditsuke <ditsuke@protonmail.com>
2021-10-09 14:25:57 +0530

    Fix copyright formatting

--- administrator/components/com_scheduler/src/Task/Status.php
+++ administrator/components/com_scheduler/src/Task/Status.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * @package         Joomla.Administrator
- * @subpackage      com_scheduler
+ * @package     Joomla.Administrator
+ * @subpackage  com_scheduler
  *
  * @copyright   (C) 2021 Open Source Matters, Inc. <https://www.joomla.org>
- * @license         GNU General Public License version 2 or later; see LICENSE.txt
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 namespace Joomla\Component\Scheduler\Administrator\Task;
--- administrator/components/com_scheduler/src/Traits/TaskPluginTrait.php
+++ administrator/components/com_scheduler/src/Traits/TaskPluginTrait.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * @package       Joomla.Administrator
- * @subpackage    com_scheduler
+ * @package     Joomla.Administrator
+ * @subpackage  com_scheduler
  *
- * @copyright (C) 2021 Open Source Matters, Inc. <https://www.joomla.org>
- * @license       GNU General Public License version 2 or later; see LICENSE.txt
+ * @copyright   (C) 2021 Open Source Matters, Inc. <https://www.joomla.org>
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 /** Implements the TaskPluginTrait. */
