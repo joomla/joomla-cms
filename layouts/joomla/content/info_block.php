88f32198a1b1d0799911a1b05d9adfe98caf0145
impressionestudio <info@impression-estudio.gr>
2021-10-17 12:36:51 +0300

    Changes to the class names of info_block.php
    Detailed explanation of the changes:
    
    1. On row 17, the class "article-info" is replaced by "article__info" just to follow BEM. 
    The class "text-muted" is deleted because we don't want to have this class to every template we use. It is a matter of each template to give different style by using the main class, which in this case was "article-info".
    The old class name remained for compatibility reasons, separated by the character | to be indicated as deprecated.
    
    2. On row 23, the class "article-info-term" is replaced by "article__info__title" just to follow BEM.
    The old class name remained for compatibility reasons, separated by the character | to be indicated as deprecated.

--- layouts/joomla/content/info_block.php
+++ layouts/joomla/content/info_block.php
@@ -14,13 +14,13 @@ use Joomla\CMS\Language\Text;
 $blockPosition = $displayData['params']->get('info_block_position', 0);
 
 ?>
-<dl class="article-info text-muted">
+<dl class="article__info | article-info">
 
 	<?php if ($displayData['position'] === 'above' && ($blockPosition == 0 || $blockPosition == 2)
 			|| $displayData['position'] === 'below' && ($blockPosition == 1)
 			) : ?>
 
-		<dt class="article-info-term">
+		<dt class="article__info__title | article-info-term">
 			<?php if ($displayData['params']->get('info_block_show_title', 1)) : ?>
 				<?php echo Text::_('COM_CONTENT_ARTICLE_INFO'); ?>
 			<?php endif; ?>
