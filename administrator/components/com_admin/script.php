31c2b52e9c7269f8baa54060c3bb6ee9778215a2
Richard Fath <richard67@users.noreply.github.com>
2021-10-19 12:56:28 +0200

    Update deleted files and folders to changes since 4.0.3 (#35845)

--- administrator/components/com_admin/script.php
+++ administrator/components/com_admin/script.php
@@ -1456,6 +1456,7 @@ class JoomlaInstallerScript
 			'/administrator/components/com_joomlaupdate/helpers/select.php',
 			'/administrator/components/com_joomlaupdate/joomlaupdate.php',
 			'/administrator/components/com_joomlaupdate/models/default.php',
+			'/administrator/components/com_joomlaupdate/restore.php',
 			'/administrator/components/com_joomlaupdate/views/default/tmpl/complete.php',
 			'/administrator/components/com_joomlaupdate/views/default/tmpl/default.php',
 			'/administrator/components/com_joomlaupdate/views/default/tmpl/default.xml',
@@ -4020,8 +4021,12 @@ class JoomlaInstallerScript
 			'/media/com_contenthistory/js/jquery.pretty-text-diff.js',
 			'/media/com_contenthistory/js/jquery.pretty-text-diff.min.js',
 			'/media/com_finder/js/autocompleter.js',
+			'/media/com_joomlaupdate/js/encryption.js',
+			'/media/com_joomlaupdate/js/encryption.min.js',
 			'/media/com_joomlaupdate/js/json2.js',
 			'/media/com_joomlaupdate/js/json2.min.js',
+			'/media/com_joomlaupdate/js/update.js',
+			'/media/com_joomlaupdate/js/update.min.js',
 			'/media/contacts/images/con_address.png',
 			'/media/contacts/images/con_fax.png',
 			'/media/contacts/images/con_info.png',
@@ -6099,12 +6104,8 @@ class JoomlaInstallerScript
 			'/templates/cassiopeia/scss/global/fonts-web_fira-sans.scss',
 			'/templates/cassiopeia/scss/global/fonts-web_roboto+noto-sans.scss',
 			// From 4.0.3 to 4.0.4
-			'/administrator/components/com_joomlaupdate/restore.php',
-			'/media/com_joomlaupdate/js/encryption.js',
-			'/media/com_joomlaupdate/js/encryption.min.js',
+			'/administrator/templates/atum/scss/_mixin.scss',
 			'/media/com_joomlaupdate/js/encryption.min.js.gz',
-			'/media/com_joomlaupdate/js/update.js',
-			'/media/com_joomlaupdate/js/update.min.js',
 			'/media/com_joomlaupdate/js/update.min.js.gz',
 			'/templates/cassiopeia/images/system/sort_asc.png',
 			'/templates/cassiopeia/images/system/sort_desc.png',
@@ -7365,6 +7366,8 @@ class JoomlaInstallerScript
 			'/libraries/vendor/algo26-matthias/idna-convert/tests/unit',
 			'/libraries/vendor/algo26-matthias/idna-convert/tests/integration',
 			'/libraries/vendor/algo26-matthias/idna-convert/tests',
+			// From 4.0.3 to 4.0.4
+			'/templates/cassiopeia/images/system',
 		);
 
 		$status['files_checked'] = $files;
