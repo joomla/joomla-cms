62390be8d41f872ec83c50013760590abd3266f8
Benjamin Trenkle <bembelimen@users.noreply.github.com>
2021-10-19 12:35:55 +0200

    Update administrator/components/com_languages/src/Controller/LanguagesController.php
    Co-authored-by: Phil E. Taylor <phil@phil-taylor.com>

--- administrator/components/com_languages/src/Controller/LanguagesController.php
+++ administrator/components/com_languages/src/Controller/LanguagesController.php
@@ -43,7 +43,7 @@ class LanguagesController extends AdminController
 	 *
 	 * @return  string  The JSON-encoded amount of published languages
 	 *
-	 * @since   4.0
+	 * @since   __DEPLOY_VERSION__
 	 */
 	public function getQuickiconContent()
 	{
