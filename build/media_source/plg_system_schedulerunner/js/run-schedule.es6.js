1e14d61d603c4065fe965861805ea96ad3409c52
Benjamin Trenkle <benjamin.trenkle@wicked-software.de>
2021-10-25 03:24:53 +0200

    Fix broken interval run when setting custom value

--- build/media_source/plg_system_schedulerunner/js/run-schedule.es6.js
+++ build/media_source/plg_system_schedulerunner/js/run-schedule.es6.js
@@ -20,7 +20,7 @@ if (!window.Joomla) {
 const scheduleRunnerOptions = Joomla.getOptions('plg_system_schedulerunner');
 const systemPaths = Joomla.getOptions('system.paths');
 
-const scheduleRunnerInterval = scheduleRunnerOptions && scheduleRunnerOptions.interval ? parseInt(scheduleRunnerOptions.interval, 10) : 300 * 1000;
+const scheduleRunnerInterval = (scheduleRunnerOptions && scheduleRunnerOptions.interval ? parseInt(scheduleRunnerOptions.interval, 10) : 300) * 1000;
 const scheduleRunnerUri = `${(systemPaths ? `${systemPaths.root}/index.php` : window.location.pathname)}?option=com_ajax&format=raw&plugin=runScheduler&group=system`;
 
 setInterval(() => navigator.sendBeacon(scheduleRunnerUri), scheduleRunnerInterval);
