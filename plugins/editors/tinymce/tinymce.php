5925ef5abee23c7fabf27b9c9e6f76bf1aa7c7ac
brian teeman <brian@teeman.net>
2021-10-25 10:59:37 +0100

    deprecated

--- plugins/editors/tinymce/tinymce.php
+++ plugins/editors/tinymce/tinymce.php
@@ -575,6 +575,7 @@ class PlgEditorTinymce extends CMSPlugin
 		$scriptOptions   = array_merge(
 			$scriptOptions,
 			array(
+				'deprecation_warnings' => true,
 				'suffix'   => '.min',
 				'baseURL'  => Uri::root(true) . '/media/vendor/tinymce',
 				'directionality' => $text_direction,
@@ -596,8 +597,7 @@ class PlgEditorTinymce extends CMSPlugin
 				'quickbars_selection_toolbar' => 'bold italic underline | H2 H3 | link blockquote',
 
 				// Cleanup/Output
-				'inline_styles'    => true,
-				'gecko_spellcheck' => true,
+				'browser_spellcheck' => true,
 				'entity_encoding'  => $levelParams->get('entity_encoding', 'raw'),
 				'verify_html'      => !$ignore_filter,
 
@@ -642,14 +642,12 @@ class PlgEditorTinymce extends CMSPlugin
 		{
 			// Break
 			$scriptOptions['force_br_newlines'] = true;
-			$scriptOptions['force_p_newlines']  = false;
 			$scriptOptions['forced_root_block'] = '';
 		}
 		else
 		{
 			// Paragraph
 			$scriptOptions['force_br_newlines'] = false;
-			$scriptOptions['force_p_newlines']  = true;
 			$scriptOptions['forced_root_block'] = 'p';
 		}
 
