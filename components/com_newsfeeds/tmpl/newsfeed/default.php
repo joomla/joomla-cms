dd36e118727d72a89ca3bbfdd7ad1c8aec6f6573
Dimitris Grammatikogiannis <d.grammatiko@gmail.com>
2021-10-30 14:33:02 +0100

    Update components/com_newsfeeds/tmpl/newsfeed/default.php
    Co-authored-by: Phil E. Taylor <phil@phil-taylor.com>

--- components/com_newsfeeds/tmpl/newsfeed/default.php
+++ components/com_newsfeeds/tmpl/newsfeed/default.php
@@ -74,7 +74,7 @@ use Joomla\CMS\Layout\LayoutHelper;
 						'image' => [
 							'src' => $images->image_first,
 							'alt' => empty($images->image_first_alt) && empty($images->image_first_alt_empty) ? '' : $images->image_first_alt,
-							'attributes' => $imgAttribs
+							'attributes' => $imgAttribs,
 						],
 					]
 				); ?>
