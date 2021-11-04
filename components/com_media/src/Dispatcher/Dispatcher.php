4eca85aee40599eb9923dc09b3ee38060fabbb28
Patrick Jackson <pjackson@kpsystems.com.au>
2021-11-04 11:37:28 +1100

    Update Dispatcher.php
    Updating #35863 to incorporate changes made in #35886.
    
    #35886 removes some code no longer required for `$asset` and `$author` variables.
    
    #35863 then extends the changes to allow permissions for `core.create` in addition to `core.manage` to allow users with lower permissions to have create permissions assigned.

--- components/com_media/src/Dispatcher/Dispatcher.php
+++ components/com_media/src/Dispatcher/Dispatcher.php
@@ -48,16 +48,10 @@ class Dispatcher extends ComponentDispatcher
 	protected function checkAccess()
 	{
 		$user   = $this->app->getIdentity();
-		$asset  = $this->input->get('asset');
-		$author = $this->input->get('author');
 
 		// Access check
 		if (!$user->authorise('core.manage', 'com_media')
-		    	&& !$user->authorise('core.create', 'com_media')
-			&& (!$asset || (!$user->authorise('core.edit', $asset)
-			&& !$user->authorise('core.create', $asset)
-			&& count($user->getAuthorisedCategories($asset, 'core.create')) == 0)
-			&& !($user->id == $author && $user->authorise('core.edit.own', $asset))))
+		    	&& !$user->authorise('core.create', 'com_media'))
 		{
 			throw new NotAllowed($this->app->getLanguage()->_('JERROR_ALERTNOAUTHOR'), 403);
 		}
