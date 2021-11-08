579d28f85d877dd678393bc0c3f7ec2c59e2c46c
Peter van Westen <info@regularlabs.com>
2021-11-08 12:04:07 +0100

    Changes events to be on field elements instead of document

--- build/media_source/system/js/showon.es6.js
+++ build/media_source/system/js/showon.es6.js
@@ -199,10 +199,10 @@ class Showon {
       if (field.tagName !== 'option') {
         if (showfield) {
           field.classList.remove('hidden');
-          document.dispatchEvent(new CustomEvent('showon-show'));
+          field.dispatchEvent(new CustomEvent('showon-show'));
         } else {
           field.classList.add('hidden');
-          document.dispatchEvent(new CustomEvent('showon-hide'));
+          field.dispatchEvent(new CustomEvent('showon-hide'));
         }
       } else {
         // TODO: If chosen or choices.js is active we should update them
