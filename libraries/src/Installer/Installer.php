26a0958e24b97a24e7ba461e36965bdcd085bac3
dgrammatiko <d.grammatiko@gmail.com>
2021-11-09 15:32:10 +0100

    Fix the missing installer info

--- libraries/src/Installer/Installer.php
+++ libraries/src/Installer/Installer.php
@@ -2302,6 +2302,17 @@ class Installer extends Adapter
 		$data['description'] = (string) $xml->description;
 		$data['group'] = (string) $xml->group;
 
+		// Child templates specific fields.
+		if (isset($xml->inheritable))
+		{
+			$data['inheritable'] = (string) $xml->inheritable === '0' ? false : true;
+		}
+
+		if (isset($xml->parent) && (string) $xml->parent !== '')
+		{
+			$data['parent'] = (string) $xml->parent;
+		}
+
 		if ($xml->files && \count($xml->files->children()))
 		{
 			$filename = basename($path);
