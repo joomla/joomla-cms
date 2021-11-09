7495b04cb716bed40213cd47f30a5f6ac8472a23
ditsuke <ditsuke@protonmail.com>
2021-11-09 22:46:37 +0530

    Update Task class
    Minor updates, comments for the future.

--- administrator/components/com_scheduler/src/Task/Task.php
+++ administrator/components/com_scheduler/src/Task/Task.php
@@ -23,6 +23,7 @@ use Joomla\CMS\Plugin\PluginHelper;
 use Joomla\Component\Scheduler\Administrator\Event\ExecuteTaskEvent;
 use Joomla\Component\Scheduler\Administrator\Helper\ExecRuleHelper;
 use Joomla\Component\Scheduler\Administrator\Helper\SchedulerHelper;
+use Joomla\Component\Scheduler\Administrator\Scheduler\Scheduler;
 use Joomla\Database\DatabaseDriver;
 use Joomla\Database\DatabaseInterface;
 use Joomla\Database\ParameterType;
@@ -134,6 +135,22 @@ class Task extends Registry implements LoggerAwareInterface
 	 */
 	public function run(): bool
 	{
+		/**
+		 * We try to acquire the lock here, only if we don't already have one.
+		 * We do this, so we can support two ways of running tasks:
+		 *   1. Directly through {@see Scheduler}, which optimises acquiring a lock while fetching from the task queue.
+		 *   2. Running a task without a pre-acquired lock.
+		 * ! This needs some more thought, for whether it should be allowed or if the single-query optimisation
+		 *   should be used everywhere, although it doesn't make sense in the context of fetching
+		 *   a task when it doesn't need to be run. This might be solved if we force a re-fetch
+		 *   with the lock or do it here ourselves (using acquireLock as a proxy to the model's
+		 *   getter).
+		 */
+		if ($this->get('locked') === null)
+		{
+			$this->acquireLock();
+		}
+
 		// Exit early if task routine is not available
 		if (!SchedulerHelper::getTaskOptions()->findOption($this->get('type')))
 		{
@@ -204,6 +221,9 @@ class Task extends Registry implements LoggerAwareInterface
 
 	/**
 	 * Acquire a pseudo-lock on the task record.
+	 * ! At the moment, this method is not used anywhere as task locks are already
+	 *   acquired when they're fetched. As such this method is not functional and should
+	 *   not be reviewed until it is updated.
 	 *
 	 * @return boolean
 	 *
@@ -222,14 +242,15 @@ class Task extends Registry implements LoggerAwareInterface
 		$timeoutThreshold = (clone $now)->sub($timeout)->toSql();
 		$now              = $now->toSql();
 
-		$query->update($db->qn('#__scheduler_tasks', 't'))
-			->set('t.locked = :now')
-			->where($db->qn('t.id') . ' = :taskId')
+		// @todo update or remove this method
+		$query->update($db->qn('#__scheduler_tasks'))
+			->set('locked = :now')
+			->where($db->qn('id') . ' = :taskId')
 			->extendWhere(
 				'AND',
 				[
-					$db->qn('t.locked') . ' < :threshold',
-					$db->qn('t.locked') . 'IS NULL',
+					$db->qn('locked') . ' < :threshold',
+					$db->qn('locked') . 'IS NULL',
 				],
 				'OR'
 			)
@@ -239,14 +260,19 @@ class Task extends Registry implements LoggerAwareInterface
 
 		try
 		{
+			$db->lockTable('#__scheduler_tasks');
 			$db->setQuery($query)->execute();
 		}
 		catch (\RuntimeException $e)
 		{
 			return false;
 		}
+		finally
+		{
+			$db->unlockTables();
+		}
 
-		if (!$db->getAffectedRows())
+		if ($db->getAffectedRows() === 0)
 		{
 			return false;
 		}
