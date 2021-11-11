f29987868fc336db10ea73834cc1013a5a041f83
Dimitris Grammatikogiannis <d.grammatiko@gmail.com>
2021-11-11 21:33:04 +0100

    Update ComponentAdapter.php

--- libraries/src/Installer/Adapter/ComponentAdapter.php
+++ libraries/src/Installer/Adapter/ComponentAdapter.php
@@ -801,6 +801,7 @@ class ComponentAdapter extends InstallerAdapter
 		if (!$this->getManifest()->administration)
 		{
 			throw new \RuntimeException(Text::_('JLIB_INSTALLER_ERROR_COMP_INSTALL_ADMIN_ELEMENT'));
+
 			return;
 		}
 
