72b5a7c350faf40bb8d4392db883d7ac3aea4bc7
brian teeman <brian@teeman.net>
2021-11-29 14:21:39 +0000

    cleaning

--- administrator/components/com_admin/sql/updates/postgresql/4.1.0-2021-11-26.sql
+++ administrator/components/com_admin/sql/updates/postgresql/4.1.0-2021-11-26.sql
@@ -1,2 +1,2 @@
-INSERT INTO "#__extensions" ("name", "type", "element", "folder", "client_id", "enabled", "access", "protected", "manifest_cache", "params", "custom_data", "system_data", "checked_out", "checked_out_time", "ordering", "state") VALUES
-('plg_system_jooa11y', 'plugin', 'jooa11y', 'system', 0, 1, 1, 0, '', '', '', '', 0, '1970-01-01 00:00:00', 0, 0)
+INSERT INTO "#__extensions" ("name", "type", "element", "folder", "client_id", "enabled", "access", "protected", "locked", "manifest_cache", "params", "custom_data", "checked_out", "checked_out_time", "ordering", "state") VALUES
+('plg_system_jooa11y', 'plugin', 'jooa11y', 'system', 0, 1, 1, 0, 1, '', '', '', 0, NULL, 0, 0);
