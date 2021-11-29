384172bebeeac33cdc898d4dee67bfc5c24dbda2
brian teeman <brian@teeman.net>
2021-11-29 19:17:25 +0000

    options

--- build/media_source/plg_system_jooa11y/js/jooa11y.es6.js
+++ build/media_source/plg_system_jooa11y/js/jooa11y.es6.js
@@ -7,7 +7,9 @@ window.addEventListener('load', () => {
   // Set translations
   // Jooa11y.Lang.addI18n(Jooa11yLangEn.strings);
 
+  const options = Joomla.getOptions('jooa11yOptions');
+
   // Instantiate
-  const checker = new Jooa11y.Jooa11y(Jooa11y.options);
+  const checker = new Jooa11y.Jooa11y(options);
   checker.doInitialCheck();
 });
