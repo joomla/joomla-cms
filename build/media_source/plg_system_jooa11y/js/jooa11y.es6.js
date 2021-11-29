0711d1a1a42089579f0c50d7a6d9413388c823d2
brian teeman <brian@teeman.net>
2021-11-29 13:41:18 +0000

    tabs

--- build/media_source/plg_system_jooa11y/js/jooa11y.es6.js
+++ build/media_source/plg_system_jooa11y/js/jooa11y.es6.js
@@ -1,13 +1,13 @@
 window.addEventListener('load', () => {
-	// Check if Jooa11y is loaded
-	if (!Jooa11y) {
-		return;
-	}
+  // Check if Jooa11y is loaded
+  if (!Jooa11y) {
+    return;
+  }
 
-    // Set translations
-    Jooa11y.Lang.addI18n(Jooa11yLangEn.strings);
+  // Set translations
+  Jooa11y.Lang.addI18n(Jooa11yLangEn.strings);
 
-    // Instantiate
-    const checker = new Jooa11y.Jooa11y(Jooa11yLangEn.options);
-    checker.doInitialCheck();
+  // Instantiate
+  const checker = new Jooa11y.Jooa11y(Jooa11yLangEn.options);
+  checker.doInitialCheck();
 });
