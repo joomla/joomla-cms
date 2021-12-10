18bf4af938c7d8a15b1cfcb24a8020088592a28b
Nicola Galgano <optimus4joomla@gmail.com>
2021-12-10 11:48:08 +0100

    Update libraries/src/Date/Date.php
    Co-authored-by: Phil E. Taylor <phil@phil-taylor.com>

--- libraries/src/Date/Date.php
+++ libraries/src/Date/Date.php
@@ -409,7 +409,7 @@ class Date extends \DateTime
 	 * @since   1.7.0
 	 * @note    This method can't be type hinted due to a PHP bug: https://bugs.php.net/bug.php?id=61483
 	 */
-	public function setTimezone($tz) :Date
+	public function setTimezone($tz): Date
 	{
 		$this->tz = $tz;
 
