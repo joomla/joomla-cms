fa517adfe7e15d182e5a85388be2cdc654b7e66b
Harald Leithner <leithner@itronic.at>
2021-12-14 11:32:31 +0100

    debug

--- tests/Codeception/_support/Step/Acceptance/Administrator/Media.php
+++ tests/Codeception/_support/Step/Acceptance/Administrator/Media.php
@@ -336,7 +336,7 @@ class Media extends Admin
 			// TODO: Remove before merge
 		$I->comment('Directory Contents: ' . print_r($path, true));
 		$I->comment('cmspath Contents: ' . print_r($this->getCmsPath(), true));
-		$I->comment('Acceptence config Contents: ' . json_encode($this->getSuiteConfiguration()['modules']['config']['Helper\Acceptance']));
+		$I->comment('Acceptence config Contents: ' . json_encode($this->getSuiteConfiguration()));
 
 		return rtrim($this->getCmsPath(), '/') . '/' . ltrim($path, '/');
 	}
