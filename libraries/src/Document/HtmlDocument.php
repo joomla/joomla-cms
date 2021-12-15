60d445cb882736576ba2a0b085d561606c39a204
Fedik <getthesite@gmail.com>
2021-12-15 19:22:26 +0200

    Fix progressive caching

--- libraries/src/Document/HtmlDocument.php
+++ libraries/src/Document/HtmlDocument.php
@@ -11,6 +11,7 @@ namespace Joomla\CMS\Document;
 defined('JPATH_PLATFORM') or die;
 
 use Joomla\CMS\Cache\Cache;
+use Joomla\CMS\Factory as CmsFactory;
 use Joomla\CMS\Helper\ModuleHelper;
 use Joomla\CMS\Log\Log;
 use Joomla\CMS\Uri\Uri;
@@ -471,8 +472,17 @@ class HtmlDocument extends Document
 
 		if ($this->_caching == true && $type == 'modules' && $name !== 'debug')
 		{
-			$cache = \JFactory::getCache('com_modules', '');
-			$hash = md5(serialize(array($name, $attribs, null, get_class($renderer))));
+			$cache = CmsFactory::getCache('com_modules', '');
+			$hash = md5(
+				serialize(
+					array(
+						$name,
+						$attribs,
+						get_class($renderer),
+						CmsFactory::getApplication()->input->get('Itemid')
+					)
+				)
+			);
 			$cbuffer = $cache->get('cbuffer_' . $type);
 
 			if (isset($cbuffer[$hash]))
