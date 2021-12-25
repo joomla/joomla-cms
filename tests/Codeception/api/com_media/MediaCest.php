d5d97964f839c3750db7875369aff79834db1a29
Allon Moritz <allon.moritz@digital-peak.com>
2021-12-25 12:59:11 +0100

    Update tests/Codeception/api/com_media/MediaCest.php
    Co-authored-by: Phil E. Taylor <phil@phil-taylor.com>

--- tests/Codeception/api/com_media/MediaCest.php
+++ tests/Codeception/api/com_media/MediaCest.php
@@ -515,7 +515,7 @@ class MediaCest
 	/**
 	 * Test the DELETE media files endpoint of com_media from the API with adapter information.
 	 *
-	 * @param   mixed   ApiTester  $I  Api tester
+	 * @param   ApiTester  $I  Api tester
 	 *
 	 * @return  void
 	 *
