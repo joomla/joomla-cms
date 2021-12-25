282fa98ab43a2a7292a61c7429b0fa8f9d8cc2ab
Dimitris Grammatikogiannis <dg@dgrammatiko.dev>
2021-12-25 13:44:58 +0100

    Update modules/mod_articles_news/tmpl/_item.php
    Co-authored-by: Brian Teeman <brian@teeman.net>

--- modules/mod_articles_news/tmpl/_item.php
+++ modules/mod_articles_news/tmpl/_item.php
@@ -17,13 +17,13 @@ use Joomla\CMS\Layout\LayoutHelper;
 	<<?php echo $item_heading; ?> class="newsflash-title">
 	<?php if ($item->link !== '' && $params->get('link_titles')) : ?>
 		<a href="<?php echo $item->link; ?>">
- 			<?php echo $item->title; ?>
- 		</a>
- 	<?php else : ?>
- 		<?php echo $item->title; ?>
- 	<?php endif; ?>
- 	</<?php echo $item_heading; ?>>
- <?php endif; ?>
+			<?php echo $item->title; ?>
+		</a>
+	<?php else : ?>
+		<?php echo $item->title; ?>
+	<?php endif; ?>
+	</<?php echo $item_heading; ?>>
+<?php endif; ?>
 
 <?php if ($params->get('img_intro_full') !== 'none' && !empty($item->imageSrc)) : ?>
 	<figure class="newsflash-image">
