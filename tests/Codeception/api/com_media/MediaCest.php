2bc8a41383415a86bd50b41d9c68351f7935459b
Allon Moritz <allon.moritz@digital-peak.com>
2021-12-25 12:58:22 +0100

    Update tests/Codeception/api/com_media/MediaCest.php
    Co-authored-by: Phil E. Taylor <phil@phil-taylor.com>

--- tests/Codeception/api/com_media/MediaCest.php
+++ tests/Codeception/api/com_media/MediaCest.php
@@ -347,7 +347,7 @@ class MediaCest
 	/**
 	 * Test the PATCH media files endpoint of com_media from the API without adapter information.
 	 *
-	 * @param   mixed   ApiTester  $I  Api tester
+	 * @param  ApiTester  $I  Api tester
 	 *
 	 * @return  void
 	 *
