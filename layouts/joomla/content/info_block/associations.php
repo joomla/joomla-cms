f294cbf917ffe39dfdd94f52bf065768fba3fae6
impressionestudio <info@impression-estudio.gr>
2021-12-26 17:49:26 +0200

    Update associations.php

--- layouts/joomla/content/info_block/associations.php
+++ layouts/joomla/content/info_block/associations.php
@@ -19,18 +19,23 @@ use Joomla\CMS\Router\Route;
 <?php $associations = $displayData['item']->associations; ?>
 
 <dd class="association">
-	<span class="icon-globe icon-fw" aria-hidden="true"></span>
+	<span class="info-icon icon-globe icon-fw" aria-hidden="true"></span>
+	<span class="info-label">
 	<?php echo Text::_('JASSOCIATIONS'); ?>
+	</span>
+	<span class="info-value">
 	<?php foreach ($associations as $association) : ?>
 		<?php if ($displayData['item']->params->get('flags', 1) && $association['language']->image) : ?>
 			<?php $flag = HTMLHelper::_('image', 'mod_languages/' . $association['language']->image . '.gif', $association['language']->title_native, array('title' => $association['language']->title_native), true); ?>
-			<a href="<?php echo Route::_($association['item']); ?>"><?php echo $flag; ?></a>
+			<a class="<?php echo strtolower($association['language']->lang_code); ?>" href="<?php echo Route::_($association['item']); ?>"><?php echo $flag; ?></a>
 		<?php else : ?>
-			<?php $class = 'btn btn-secondary btn-sm btn-' . strtolower($association['language']->lang_code); ?>
-			<a class="<?php echo $class; ?>" title="<?php echo $association['language']->title_native; ?>" href="<?php echo Route::_($association['item']); ?>"><?php echo $association['language']->lang_code; ?>
-				<span class="visually-hidden"><?php echo $association['language']->title_native; ?></span>
+			<?php $class = 'btn btn-' . strtolower($association['language']->lang_code).' | btn-secondary btn-sm'; ?>
+			<a class="<?php echo $class; ?>" title="<?php echo $association['language']->title_native; ?>" href="<?php echo Route::_($association['item']); ?>">
+				<span class="lang-code"><?php echo $association['language']->lang_code; ?></span>
+				<span class="lang-title"><?php echo $association['language']->title_native; ?></span>
 			</a>
 		<?php endif; ?>
 	<?php endforeach; ?>
+	</span>
 </dd>
 <?php endif; ?>
