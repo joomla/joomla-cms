012dbd160dd6a348afa49d94b1da82e0cae4b18b
impressionestudio <info@impression-estudio.gr>
2021-12-26 19:29:01 +0200

    Update category.php

--- layouts/joomla/content/info_block/category.php
+++ layouts/joomla/content/info_block/category.php
@@ -15,16 +15,22 @@ use Joomla\CMS\Router\Route;
 use Joomla\Component\Content\Site\Helper\RouteHelper;
 
 ?>
-<dd class="category-name">
-	<?php echo LayoutHelper::render('joomla.icon.iconclass', ['icon' => 'icon-folder-open icon-fw']); ?>
+<dd class="category | category-name">
+	<?php echo LayoutHelper::render('joomla.icon.iconclass', ['icon' => 'info-icon icon-folder-open icon-fw']); ?>
 	<?php $title = $this->escape($displayData['item']->category_title); ?>
 	<?php if ($displayData['params']->get('link_category') && !empty($displayData['item']->catid)) : ?>
-		<?php $url = '<a href="' . Route::_(
+		<?php $url = '<a class="info-value" href="' . Route::_(
 			RouteHelper::getCategoryRoute($displayData['item']->catid, $displayData['item']->category_language)
 			)
 			. '" itemprop="genre">' . $title . '</a>'; ?>
-		<?php echo Text::sprintf('COM_CONTENT_CATEGORY', $url); ?>
+		<span class="info-label">
+		<?php echo Text::sprintf('COM_CONTENT_CATEGORY', ''); ?>	
+		</span>
+		<?php echo $url; ?>
 	<?php else : ?>
-		<?php echo Text::sprintf('COM_CONTENT_CATEGORY', '<span itemprop="genre">' . $title . '</span>'); ?>
+		<span class="info-label">
+		<?php echo Text::sprintf('COM_CONTENT_CATEGORY', ''); ?>	
+		</span>
+		<span class="info-value" itemprop="genre"><?php echo $title; ?></span>
 	<?php endif; ?>
 </dd>
