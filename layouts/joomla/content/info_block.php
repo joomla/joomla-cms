6c3485e052b732ed6392be1e06914144b21fd535
impressionestudio <info@impression-estudio.gr>
2021-12-26 16:09:04 +0200

    Update info_block.php

--- layouts/joomla/content/info_block.php
+++ layouts/joomla/content/info_block.php
@@ -14,13 +14,13 @@ use Joomla\CMS\Language\Text;
 $blockPosition = $displayData['params']->get('info_block_position', 0);
 
 ?>
-<dl class="article-info text-muted">
+<dl class="info | article-info text-muted">
 
 	<?php if ($displayData['position'] === 'above' && ($blockPosition == 0 || $blockPosition == 2)
 			|| $displayData['position'] === 'below' && ($blockPosition == 1)
 			) : ?>
 
-		<dt class="article-info-term">
+		<dt class="info-title | article-info-term">
 			<?php if ($displayData['params']->get('info_block_show_title', 1)) : ?>
 				<?php echo Text::_('COM_CONTENT_ARTICLE_INFO'); ?>
 			<?php endif; ?>
