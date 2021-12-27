304c096feb3985c98c3c3c080eea7501bb8a9e7e
impressionestudio <info@impression-estudio.gr>
2021-12-27 15:52:56 +0200

    Update tags.php

--- layouts/joomla/content/tags.php
+++ layouts/joomla/content/tags.php
@@ -18,13 +18,13 @@ $authorised = Factory::getUser()->getAuthorisedViewLevels();
 
 ?>
 <?php if (!empty($displayData)) : ?>
-	<ul class="tags list-inline">
+	<ul class="tags | list-inline">
 		<?php foreach ($displayData as $i => $tag) : ?>
 			<?php if (in_array($tag->access, $authorised)) : ?>
 				<?php $tagParams = new Registry($tag->params); ?>
 				<?php $link_class = $tagParams->get('tag_link_class', 'btn-info'); ?>
-				<li class="list-inline-item tag-<?php echo $tag->tag_id; ?> tag-list<?php echo $i; ?>" itemprop="keywords">
-					<a href="<?php echo Route::_(RouteHelper::getTagRoute($tag->tag_id . ':' . $tag->alias)); ?>" class="btn btn-sm <?php echo $link_class; ?>">
+				<li class="tag tag-<?php echo $tag->tag_id; ?> tag-index-<?php echo $i; ?> | list-inline-item tag-<?php echo $tag->tag_id; ?> tag-list<?php echo $i; ?>" itemprop="keywords">
+					<a href="<?php echo Route::_(RouteHelper::getTagRoute($tag->tag_id . ':' . $tag->alias)); ?>" class="btn <?php echo $link_class; ?> | btn-sm">
 						<?php echo $this->escape($tag->title); ?>
 					</a>
 				</li>
