4b279421dbc50933078145747e2231648aceecaf
impressionestudio <info@impression-estudio.gr>
2021-12-27 13:12:05 +0200

    Update create_date.php

--- layouts/joomla/content/info_block/create_date.php
+++ layouts/joomla/content/info_block/create_date.php
@@ -14,8 +14,11 @@ use Joomla\CMS\Language\Text;
 
 ?>
 <dd class="create">
-	<span class="icon-calendar icon-fw" aria-hidden="true"></span>
-	<time datetime="<?php echo HTMLHelper::_('date', $displayData['item']->created, 'c'); ?>" itemprop="dateCreated">
-		<?php echo Text::sprintf('COM_CONTENT_CREATED_DATE_ON', HTMLHelper::_('date', $displayData['item']->created, Text::_('DATE_FORMAT_LC3'))); ?>
-	</time>
+	<span class="info-icon icon-calendar icon-fw" aria-hidden="true"></span>
+	<span class="info-label">
+		<?php echo Text::sprintf('COM_CONTENT_CREATED_DATE_ON', ''); ?>
+	</span>
+	<span class="info-value">
+		<?php echo HTMLHelper::_('date', $displayData['item']->created, Text::_('DATE_FORMAT_LC3')); ?>
+	</span>
 </dd>
