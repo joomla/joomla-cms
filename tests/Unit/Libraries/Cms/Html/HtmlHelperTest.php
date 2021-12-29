bfe02de08239b15f9cf2d25ccf872d7058d935aa
Phil E. Taylor <phil@phil-taylor.com>
2021-12-29 17:58:40 +0000

    Move setup to parent class for reuseability

--- tests/Unit/Libraries/Cms/Html/HtmlHelperTest.php
+++ tests/Unit/Libraries/Cms/Html/HtmlHelperTest.php
@@ -9,18 +9,8 @@
 
 namespace Joomla\Tests\Unit\Libraries\Cms\Html;
 
-use Joomla\CMS\Application\CMSApplication;
-use Joomla\CMS\Document\Document;
-use Joomla\CMS\Document\FactoryInterface;
-use Joomla\CMS\Factory;
 use Joomla\CMS\HTML\HTMLHelper;
-use Joomla\CMS\Language\Language;
-use Joomla\CMS\WebAsset\WebAssetManager;
-use Joomla\Input\Input;
-use Joomla\Registry\Registry;
 use Joomla\Tests\Unit\UnitTestCase;
-use ReflectionClass;
-use stdClass;
 
 /**
  * Test class for HtmlHelperTest.
@@ -45,7 +35,7 @@ class HtmlHelperTest extends UnitTestCase
 		<button type="button" class="btn btn-primary"
 			id="%s_btn"
 			title="JLIB_HTML_BEHAVIOR_OPEN_CALENDAR"
-			data-inputfield="%s" data-button="%s_btn" data-date-format="%s" data-firstday="" data-weekend="" data-today-btn="1" data-week-numbers="1" data-show-time="0" data-show-others="1" data-time24="24" data-only-months-nav="0" data-min-year="" data-max-year="" data-date-type=""		><span class="icon-calendar" aria-hidden="true"></span>
+			data-inputfield="testId" data-button="testId_btn" data-date-format="%s" data-firstday="" data-weekend="0,6" data-today-btn="1" data-week-numbers="1" data-show-time="0" data-show-others="1" data-time24="24" data-only-months-nav="0" data-min-year="" data-max-year="" data-date-type="gregorian"		><span class="icon-calendar" aria-hidden="true"></span>
 		<span class="visually-hidden">JLIB_HTML_BEHAVIOR_OPEN_CALENDAR</span>
 		</button>
 			</div>
@@ -59,69 +49,13 @@ class HtmlHelperTest extends UnitTestCase
 	public function testCalendar()
 	{
 		$this->assertEquals(
-			sprintf($this->template, 'testId', 'testName', 'Mar', 'Mar', 'testId', 'testId', 'testId', '%b'),
+			sprintf($this->template, 'testId', 'testName', 'Mar', 'Mar', 'testId', '%b'),
 			HTMLHelper::calendar('1978-03-08 06:12:12', 'testName', 'testId', '%b', [])
 		);
 
 		$this->assertEquals(
-			sprintf($this->template, 'testId', 'testName', '1978-03-08', '1978-03-08', 'testId', 'testId', 'testId', '%Y-%m-%d'),
+			sprintf($this->template, 'testId', 'testName', '1978-03-08', '1978-03-08', 'testId', '%Y-%m-%d'),
 			HTMLHelper::calendar('1978-03-08 06:12:12', 'testName', 'testId', '%Y-%m-%d', [])
 		);
-	}
-
-	/**
-	 * Sets up the minimal amount of Joomla, mocked where possible, to run the
-	 * static methods in HTMLHelper class
-	 *
-	 * @return void
-	 *
-	 * @since  __DEPLOY_VERSION__
-	 */
-	protected function setUp(): void
-	{
-		// Start with the easy mocks.
-		$lang = $this->createMock(Language::class);
-		$input = $this->createMock(Input::class);
-		$factory = $this->createMock(Factory::class);
-
-		// Mock the Document object.
-		$doc = new Document(
-			[
-				'factory' => $this->createMock(FactoryInterface::class),
-			]
-		);
-
-		// Mock WA and some calls used that we are not directly testing.
-		$wa = $this->createMock(WebAssetManager::class);
-		$wa->expects($this->any())->method('__call')->will($this->returnValue($wa));
-		$doc->setWebAssetManager($wa);
-
-		// Inject the mocked document in the mocked factory.
-		$factory::$document = $doc;
-
-		// Mock a template that the app will return from getTemplate().
-		$template = new stdClass;
-		$template->template = 'system';
-		$template->params = new Registry;
-		$template->inheritable = 0;
-		$template->parent = '';
-
-		// Ensure the application can return all our mocked items.
-		$app = $this->createMock(CMSApplication::class);
-		$app->method('__get')
-			->with('input')
-			->willReturn($input);
-		$app->method('getLanguage')
-			->will($this->returnValue($lang));
-		$app->method('getDocument')
-			->will($this->returnValue($factory::$document));
-		$app->method('getTemplate')
-			->will($this->returnValue($template));
-
-		// Finally, set the application into the factory.
-		$reflection = new ReflectionClass($factory);
-		$reflection_property = $reflection->getProperty('application');
-		$reflection_property->setAccessible(true);
-		$reflection_property->setValue($factory, $app);
 	}
 }
