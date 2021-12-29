6fd8d77b9c1e923cba7e66773e29c0de035e6b08
Phil E. Taylor <phil@phil-taylor.com>
2021-12-29 16:40:54 +0000

    fix headers

--- tests/Unit/CMS/administrator/components/com_finder/src/Indexer/ResultTest.php
+++ tests/Unit/CMS/administrator/components/com_finder/src/Indexer/ResultTest.php
@@ -1,12 +1,12 @@
 <?php
 /**
  * @package     Joomla.UnitTest
- * @subpackage  Access
+ * @subpackage  com_finder
  *
- * @copyright   (C) 2019 Open Source Matters, Inc. <https://www.joomla.org>
+ * @copyright   (C) 2021 Open Source Matters, Inc. <https://www.joomla.org>
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
-namespace Joomla\Tests\Unit\Libraries\Cms\Access;
+namespace Joomla\Component\Finder\Administrator\Indexer;
 
 use Joomla\Component\Finder\Administrator\Indexer\Result;
 use Joomla\Tests\Unit\UnitTestCase;
