c972eb310eebd7ca9a217e22fad5f2103fe039a1
Phil E. Taylor <phil@phil-taylor.com>
2021-12-30 23:28:00 +0000

    Update components/com_config/src/Controller/ModulesController.php
    Co-authored-by: George Wilson <georgejameswilson@googlemail.com>

--- components/com_config/src/Controller/ModulesController.php
+++ components/com_config/src/Controller/ModulesController.php
@@ -36,7 +36,7 @@ class ModulesController extends BaseController
 	 *                                                  'view_path' (this list is not meant to be comprehensive).
 	 * @param   MVCFactoryInterface|null      $factory  The factory.
 	 * @param   CMSApplication|null           $app      The Application for the dispatcher
-	 * @param   \Joomla\CMS\Input\Input|null  $input
+	 * @param   \Joomla\CMS\Input\Input|null  $input  The Input object for the request
 	 *
 	 * @since   1.6
 	 */
