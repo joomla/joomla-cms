ed60d8252ab0f049a2f9b9cf795e1af578e7a981
Phil E. Taylor <phil@phil-taylor.com>
2021-12-30 23:37:23 +0000

    cs

--- components/com_modules/src/Controller/DisplayController.php
+++ components/com_modules/src/Controller/DisplayController.php
@@ -26,8 +26,8 @@ class DisplayController extends BaseController
 {
 	/**
 	 * @param   array                     $config   An optional associative array of configuration settings.
-	 * 										        Recognized key values include 'name', 'default_task', 'model_path', and
-	 * 										        'view_path' (this list is not meant to be comprehensive).
+	 *                                              Recognized key values include 'name', 'default_task', 'model_path', and
+	 *                                              'view_path' (this list is not meant to be comprehensive).
 	 * @param   MVCFactoryInterface|null  $factory  The factory.
 	 * @param   CMSApplication|null       $app      The Application for the dispatcher
 	 * @param   Input|null 	 			  $input
