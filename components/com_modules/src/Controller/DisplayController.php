f349b98268f0c6b90bba5271ebeefb3d3efd5622
Phil E. Taylor <phil@phil-taylor.com>
2021-12-30 23:38:22 +0000

    Update DisplayController.php

--- components/com_modules/src/Controller/DisplayController.php
+++ components/com_modules/src/Controller/DisplayController.php
@@ -30,7 +30,7 @@ class DisplayController extends BaseController
 	 *                                              'view_path' (this list is not meant to be comprehensive).
 	 * @param   MVCFactoryInterface|null  $factory  The factory.
 	 * @param   CMSApplication|null       $app      The Application for the dispatcher
-	 * @param   Input|null 	 			  $input
+	 * @param   Input|null                $input
 	 *
 	 * @since   3.0
 	 */
