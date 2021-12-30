a46801137cba7b955fd303fc3cdca0f81616a917
Phil E. Taylor <phil@phil-taylor.com>
2021-12-30 23:40:38 +0000

    Update DisplayController.php

--- components/com_contact/src/Controller/DisplayController.php
+++ components/com_contact/src/Controller/DisplayController.php
@@ -29,7 +29,7 @@ class DisplayController extends BaseController
 	 *                                                  'view_path' (this list is not meant to be comprehensive).
 	 * @param   MVCFactoryInterface|null      $factory  The factory.
 	 * @param   CMSApplication|null           $app      The Application for the dispatcher
-	 * @param   \Joomla\CMS\Input\Input|null  $input  The Input object for the request
+	 * @param   \Joomla\CMS\Input\Input|null  $input    The Input object for the request
 	 *
 	 * @since   3.0
 	 */
