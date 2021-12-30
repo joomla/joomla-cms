6f6157bc96f4d140e4982cc785efa89cd5bfe964
Phil E. Taylor <phil@phil-taylor.com>
2021-12-30 23:34:12 +0000

    weird

--- components/com_fields/src/Controller/DisplayController.php
+++ components/com_fields/src/Controller/DisplayController.php
@@ -25,7 +25,7 @@ class DisplayController extends \Joomla\CMS\MVC\Controller\BaseController
 	/**
 	 * Constructor.
 	 *
-	 * @param   array                		  $config   An optional associative array of configuration settings.
+	 * @param   array                         $config   An optional associative array of configuration settings.
 	 *                                                  Recognized key values include 'name', 'default_task', 'model_path', and
 	 *                                                  'view_path' (this list is not meant to be comprehensive).
 	 * @param   MVCFactoryInterface|null      $factory  The factory.
