15a0ade438a8652feac1579f18611d9aaa2173c3
dgrammatiko <d.grammatiko@gmail.com>
2022-01-06 13:45:40 +0100

    path

--- plugins/editors/tinymce/src/PluginTraits/DisplayTrait.php
+++ plugins/editors/tinymce/src/PluginTraits/DisplayTrait.php
@@ -367,7 +367,7 @@ trait DisplayTrait
 		// Use CodeMirror in the code view instead of plain text to provide syntax highlighting
 		if ($levelParams->get('highlightPlus', 1))
 		{
-			$externalPlugins['highlightPlus'] = HTMLHelper::_('script', 'plg_editors_tinymce/plugins/highlighter/plugin.min.js', ['relative' => true, 'version' => 'auto', 'pathOnly' => true]);
+			$externalPlugins['highlightPlus'] = HTMLHelper::_('script', 'plg_editors_tinymce/plugins/highlighter/plugin-es5.min.js', ['relative' => true, 'version' => 'auto', 'pathOnly' => true]);
 		}
 
 		$dragdrop = $levelParams->get('drag_drop', 1);
