b23aaeeaa291deb263a65d1778de5cac5dbd1639
Dimitris Grammatikogiannis <d.grammatiko@gmail.com>
2022-01-09 00:24:22 +0100

    Backport image Layout

---
+++ layouts/joomla/html/image.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  Layout
+ *
+ * @copyright   (C) 2021 Open Source Matters, Inc. <https://www.joomla.org>
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+/**
+ * Layout variables
+ * -----------------
+ * @var   array  $displayData  Array with all the given attributes for the image element.
+ *                             Eg: src, class, alt, width, height, loading, decoding, style, data-*
+ *                             Note: only the alt and src attributes are escaped by default!
+ */
+defined('_JEXEC') or die;
+
+use Joomla\Utilities\ArrayHelper;
+
+if (isset($displayData['alt']))
+{
+	$displayData['src'] = $this->escape($img->url);
+}
+
+if (isset($displayData['alt'])) {
+	if ($displayData['alt'] === false) {
+		unset($displayData['alt']);
+	} else {
+		$displayData['alt'] = $this->escape($displayData['alt']);
+	}
+}
+
+echo '<img ' . ArrayHelper::toString($displayData) . '>';
