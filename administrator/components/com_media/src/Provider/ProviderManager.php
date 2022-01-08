67460fa845bfb4fff38b40fd33f1a7cb84cf9a4a
Allon Moritz <allon.moritz@digital-peak.com>
2022-01-08 19:53:20 +0100

    [4.0] Function to unregister a provider in media manager (#36594)

--- administrator/components/com_media/src/Provider/ProviderManager.php
+++ administrator/components/com_media/src/Provider/ProviderManager.php
@@ -56,6 +56,32 @@ class ProviderManager
 	}
 
 	/**
+	 * Unregister a provider from the ProviderManager.
+	 * When no provider, or null is passed in, then all providers are cleared.
+	 *
+	 * @param   ProviderInterface|null  $provider  The provider to be unregistered
+	 *
+	 * @return  void
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 */
+	public function unregisterProvider(ProviderInterface $provider = null): void
+	{
+		if ($provider === null)
+		{
+			$this->providers = [];
+			return;
+		}
+
+		if (!array_key_exists($provider->getID(), $this->providers))
+		{
+			return;
+		}
+
+		unset($this->providers[$provider->getID()]);
+	}
+
+	/**
 	 * Returns the provider for a particular ID
 	 *
 	 * @param   string  $id  The ID for the provider
