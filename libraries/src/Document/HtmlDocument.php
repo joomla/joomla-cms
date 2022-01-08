2752bcbd998297205f7f8c56be14e58f93f9b9c4
Tobias Zulauf <zero-24@users.noreply.github.com>
2022-01-08 18:57:55 +0100

    Update libraries/src/Document/HtmlDocument.php

--- libraries/src/Document/HtmlDocument.php
+++ libraries/src/Document/HtmlDocument.php
@@ -473,6 +473,7 @@ class HtmlDocument extends Document
 		if ($this->_caching == true && $type == 'modules' && $name !== 'debug')
 		{
 			$cache = Factory::getCache('com_modules', '');
+			$itemId = (int) Factory::getApplication()->input->get('Itemid', 0, 'int');
 			$hash = md5(
 				serialize(
 					array(
