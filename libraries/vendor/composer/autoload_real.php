0093a18757be1fce3347da37d3490c1626911167
Tobias Zulauf <zero-24@users.noreply.github.com>
2022-01-08 18:56:11 +0100

    Update simplepie to 1.3.3 (#36358)
    * Update simplepie to 1.3.2 & phar-stream-wrapper to 2.2.2
    
    * update to simplepie 1.3.3 to fix the version

--- libraries/vendor/composer/autoload_real.php
+++ libraries/vendor/composer/autoload_real.php
@@ -65,16 +65,11 @@ class ComposerAutoloaderInitc14bde14f8c86840049f5c1809c453dd
     }
 }
 
-/**
- * @param string $fileIdentifier
- * @param string $file
- * @return void
- */
 function composerRequirec14bde14f8c86840049f5c1809c453dd($fileIdentifier, $file)
 {
     if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
-
         require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
