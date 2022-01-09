0824a924af57a374362bfd6ebaee50b043abb1a3
brian teeman <brian@teeman.net>
2022-01-09 17:18:41 +0000

    [4.0] Mail Templates [a11y]
    Correctly change the cells in the title column to use th scope=row as used elsewhere

--- administrator/components/com_mails/tmpl/templates/default.php
+++ administrator/components/com_mails/tmpl/templates/default.php
@@ -66,11 +66,11 @@ $listDirn = $this->escape($this->state->get('list.direction'));
 							$sub_id = str_replace('.', '_', $sub_id);
 							?>
 							<tr class="row<?php echo $i % 2; ?>">
-								<td>
+								<th scope="row">
 									<a href="<?php echo Route::_('index.php?option=com_mails&task=template.edit&template_id=' . $item->template_id . '&language=' . $this->defaultLanguage->lang_code); ?>">
 										<?php echo Text::_($component . '_MAIL_' . $sub_id . '_TITLE'); ?>
 									</a>
-								</td>
+								</th>
 								<td class="d-none d-md-table-cell">
 									<?php echo Text::_($component); ?>
 								</td>
