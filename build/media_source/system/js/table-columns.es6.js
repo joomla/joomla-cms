9b0d5c9de16e1b6790680f2a6dfd3e8ff1a1cd9f
Fedir Zinchuk <getthesite@gmail.com>
2022-01-09 12:13:02 +0200

    Protected columns (#157)
    * protected columns
    
    * Check if protected column not changed from last state

--- build/media_source/system/js/table-columns.es6.js
+++ build/media_source/system/js/table-columns.es6.js
@@ -11,8 +11,26 @@ class TableColumns {
     this.$rows = [].slice.call($table.querySelectorAll('tbody tr'));
     this.listOfHidden = [];
 
-    // Load previous state, and set up toggle menu
+    // Load previous state
     this.loadState();
+
+    // Find protected columns
+    this.protectedCols = [0];
+    if (this.$rows[0]) {
+      [].slice.call(this.$rows[0].children).forEach(($el, index) => {
+        if ($el.nodeName === 'TH') {
+          this.protectedCols.push(index);
+
+          // Make sure in not in the list of hidden
+          const ih = this.listOfHidden.indexOf(index);
+          if (ih !== -1){
+            this.listOfHidden.splice(ih, 1);
+          }
+        }
+      });
+    }
+
+    // Set up toggle menu
     this.createControls();
 
     // Restore sate
@@ -48,8 +66,8 @@ class TableColumns {
 
     // Collect a list of headers for dropdown
     this.$headers.forEach(($el, index) => {
-      // Skip the first column as we don't want to hide the row select checkbox
-      if (index === 0) return;
+      // Skip the protected columns
+      if (this.protectedCols.indexOf(index) !== -1) return;
 
       const $li = document.createElement('li');
       const $label = document.createElement('label');
@@ -109,7 +127,7 @@ class TableColumns {
    * Update button text
    */
   updateCounter() {
-    const total = this.$headers.length - 1;
+    const total = this.$headers.length - this.protectedCols.length;
     const visible = total - this.listOfHidden.length;
 
     this.$button.textContent = `${visible}/${total} ${Joomla.Text._('JGLOBAL_COLUMNS')}`;
@@ -122,6 +140,9 @@ class TableColumns {
    * @param {Boolean} force To force hide
    */
   toggleColumn(index, force) {
+    // Skip the protected columns
+    if (this.protectedCols.indexOf(index) !== -1) return;
+
     const i = this.listOfHidden.indexOf(index);
 
     if (i === -1) {
