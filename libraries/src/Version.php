e59e606f3dcd65e46498dbf48a86010c13fc5c31
wilsonge <georgejameswilson@googlemail.com>
2022-01-09 00:07:33 +0000

    Tag 4.0.6 RC2

--- libraries/src/Version.php
+++ libraries/src/Version.php
@@ -61,7 +61,7 @@ final class Version
 	 * @var    string
 	 * @since  3.8.0
 	 */
-	const EXTRA_VERSION = 'rc2-dev';
+	const EXTRA_VERSION = 'rc2';
 
 	/**
 	 * Development status.
@@ -69,7 +69,7 @@ final class Version
 	 * @var    string
 	 * @since  3.5
 	 */
-	const DEV_STATUS = 'Development';
+	const DEV_STATUS = 'Release Candidate';
 
 	/**
 	 * Code name.
@@ -85,7 +85,7 @@ final class Version
 	 * @var    string
 	 * @since  3.5
 	 */
-	const RELDATE = '8-January-2022';
+	const RELDATE = '9-January-2022';
 
 	/**
 	 * Release time.
@@ -93,7 +93,7 @@ final class Version
 	 * @var    string
 	 * @since  3.5
 	 */
-	const RELTIME = '21:19';
+	const RELTIME = '00:06';
 
 	/**
 	 * Release timezone.
