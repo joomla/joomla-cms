be523c4b6fcbd43d5041686239da15757db05491
unknown <niharikamahajan47@gmail.com>
2022-01-13 02:53:59 +0530

    test task to run task and ACL PERMISSION also updated

--- administrator/components/com_scheduler/tmpl/tasks/default.php
+++ administrator/components/com_scheduler/tmpl/tasks/default.php
@@ -126,7 +126,7 @@ $app->getDocument()->getWebAssetManager()->useScript('com_scheduler.test-task');
 
 					<!-- Test task -->
 					<th scope="col">
-						<?php echo Text::_('COM_SCHEDULER_TEST_TASK'); ?>
+						<?php echo Text::_('COM_SCHEDULER_TASK_RUN'); ?>
 					</th>
 
 					<!-- Task ID -->
@@ -221,7 +221,7 @@ $app->getDocument()->getWebAssetManager()->useScript('com_scheduler.test-task');
 						<td class="small d-none d-md-table-cell">
 							<button type="button" class="btn btn-sm btn-warning" <?php echo $item->state < 0 ? 'disabled' : ''; ?> data-id="<?php echo (int) $item->id; ?>" data-title="<?php echo htmlspecialchars($item->title); ?>" data-bs-toggle="modal" data-bs-backdrop="static" data-bs-target="#scheduler-test-modal">
 								<span class="fa fa-play fa-sm me-2"></span>
-								<?php echo Text::_('COM_SCHEDULER_TEST_RUN'); ?>
+								<?php echo Text::_('COM_SCHEDULER_TASK_RUN'); ?>
 							</button>
 						</td>
 
