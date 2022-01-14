0b42225131a9e109a8fb4a91823ba082ffd396cd
akshitrattan <78909027+akshitrattan@users.noreply.github.com>
2022-01-14 12:28:08 +0530

    Update libraries/src/Toolbar/ToolbarButton.php
    Co-authored-by: Phil E. Taylor <phil@phil-taylor.com>

--- libraries/src/Toolbar/ToolbarButton.php
+++ libraries/src/Toolbar/ToolbarButton.php
@@ -247,17 +247,16 @@ abstract class ToolbarButton
 		return $layout->render(array('icon' => $identifier));
 	}
 
-	/*
-	* Allow any class which extends Joomla\CMS\Toolbar\ToobarButton
-	* and/or Joomla\CMS\Toolbar\popupButton to customise the FileLayout object
-	* in any way required.
-	*
-	* @param  string  $layoutId  Dot separated path to the layout file, relative to base path [@since 3.0]
-	* @param  string  $basePath  Full path to actual layout files, after possible template override check [@since 3.0]
-	* @param  mixed   $options   Optional custom options to load. Registry or array format [@since 3.2]
-	*/
-
-	protected function getLayoutInstance($layoutId, $basePath = null, $options = null)
+	/**
+	 * Allow any class which extends \Joomla\CMS\Toolbar\ToobarButton
+	 * and/or \Joomla\CMS\Toolbar\popupButton to customise the FileLayout object
+	 * in any way required.
+	 *
+	 * @param  string  $layoutId  Dot separated path to the layout file, relative to base path [@since 3.0]
+	 * @param  string  $basePath  Full path to actual layout files, after possible template override check [@since 3.0]
+	 * @param  mixed   $options   Optional custom options to load. Registry or array format [@since 3.2]
+	 */
+	protected function getLayoutInstance($layoutId, $basePath = null, $options = null): FileLayout
 	{
 		return new FileLayout($layoutId, $basePath, $options);
 	}
