f869288219fcd815519aa15e1ad31b7e14464fb1
Allon Moritz <allon.moritz@digital-peak.com>
2022-01-15 14:19:50 +0100

    Update libraries/src/Router/Router.php
    Co-authored-by: Phil E. Taylor <phil@phil-taylor.com>

--- libraries/src/Router/Router.php
+++ libraries/src/Router/Router.php
@@ -95,7 +95,9 @@ class Router
 	 * @return  Router  A Router object.
 	 *
 	 * @since      1.5
+	 *
 	 * @throws     \RuntimeException
+	 *
 	 * @deprecated 5.0 Inject the router or load it from the dependency injection container
 	 */
 	public static function getInstance($client, $options = array())
