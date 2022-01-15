3d422939453101a2ac3025aa15d878627005eeba
Dimitris Grammatikogiannis <dg@dgrammatiko.dev>
2022-01-15 21:15:52 +0100

    Update the code to modern JS

--- build/media_source/plg_editors_tinymce/js/plugins/dragdrop/plugin.es5.js
+++ build/media_source/plg_editors_tinymce/js/plugins/dragdrop/plugin.es5.js
@@ -1,165 +1,154 @@
-(function () {
-  'use strict';
+(function() {
+  // Reset the drop area border
+  tinyMCE.DOM.bind(document, 'dragleave', () =>{
+    tinyMCE.activeEditor.contentAreaContainer.style.borderWidth = '1px 0 0';
+  });
 
-  /* eslint-disable no-undef */
-  tinymce.PluginManager.add('jdragndrop', function (editor) {
-    var responseData; // Reset the drop area border
+  function blobToBase64(blob) {
+    return new Promise((resolve, reject) => {
+      Object.assign(new FileReader(), {
+        onload: (e) => resolve(e.target.result),
+        onerror: (error) => reject(error)
+      }).readAsDataURL(blob);
+    });
+  }
 
-    tinyMCE.DOM.bind(document, 'dragleave', function (e) {
+  function helperDialog(urlPath, responseData, editor) {
+    function dialogClose(api) {
+      var dialogData = api.getData();
+      var altEmpty = dialogData.altEmpty ? ' alt=""' : '';
+      var altValue = dialogData.altText ? " alt=\"" + dialogData.altText + "\"" : altEmpty;
+      var lazyValue = dialogData.isLazy ? ' loading="lazy"' : '';
+      var width = dialogData.isLazy ? " width=\"" + responseData.width + "\"" : '';
+      var height = dialogData.isLazy ? " height=\"" + responseData.height + "\"" : '';
+      editor.execCommand('mceInsertContent', false, "<img src=\"" + urlPath + "\"" + altValue + lazyValue + width + height + "/>");
+    }
+
+    editor.windowManager.open({
+      title: Joomla.Text._('PLG_TINY_DND_ADDITIONALDATA'),
+      body: {
+        type: 'panel',
+        items: [{
+          type: 'input',
+          name: 'altText',
+          label: Joomla.Text._('PLG_TINY_DND_ALTTEXT')
+        }, {
+          type: 'checkbox',
+          name: 'altEmpty',
+          label: Joomla.Text._('PLG_TINY_DND_EMPTY_ALT')
+        }, {
+          type: 'checkbox',
+          name: 'isLazy',
+          label: Joomla.Text._('PLG_TINY_DND_LAZYLOADED')
+        }]
+      },
+      buttons: [{
+        type: 'cancel',
+        text: 'Cancel'
+      }, {
+        type: 'submit',
+        name: 'submitButton',
+        text: 'Save',
+        primary: true
+      }],
+      initialData: {
+        altText: '',
+        isLazy: true,
+        altEmpty: false
+      },
+      onSubmit: (api) => {
+        dialogClose(api);
+        api.close();
+      },
+      onCancel: (api) => {
+        dialogClose(api);
+      }
+    });
+  }
+
+  function uploadFile(name, rawContent, editor) {
+    const sliceIndex = rawContent.indexOf('base64') + 7;
+    if (sliceIndex < 1) {
+      return Promise.reject();
+    }
+
+    const url = new URL(`${editor.settings.uploadUri}&path=${editor.settings.comMediaAdapter}`;
+    const options = {
+      method: 'POST',
+      body: JSON.stringify({
+        'name': encodeURIComponent(name),
+        'action': 'upload',
+        'content': rawContent.slice(sliceIndex, rawContent.length),
+        'path': encodeURIComponent(editor.settings.parentUploadFolder),
+        [encodeURIComponent(editor.settings.csrfToken)]: 1,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    fetch(url, options)
+    .then(function (response) {
+      return response.json();
+    }).then(function (resp) {
+      if (resp.success) {
+        const responseData = resp.data;
+        let rootFull = Joomla.getOptions('system.paths').rootFull;
+        const urlPath = responseData.url.startsWith('http') || responseData.url.startsWith('//') ? responseData.url : response.data.url.split(rootFull)[1];
+
+        helperDialog(urlPath, responseData, editor);
+      } else {
+        editor.windowManager.alert(resp.message);
+        return Promise.reject();
+      }
+    }).catch(function (error) {
+      editor.windowManager.alert(`${error.statusText} ${error.status}`);
+      return Promise.reject();
+    });
+  }
+
+  window.tinymce.PluginManager.add('jdragndrop', function (editor) {
+    'use strict';
+
+    // Fix for Chrome
+    editor.on('dragenter', (e) => {
       e.stopPropagation();
-      e.preventDefault();
-      editor.contentAreaContainer.style.borderWidth = '1px 0 0';
       return false;
-    }); // Fix for Chrome
+    });
 
-    editor.on('dragenter', function (e) {
-      e.stopPropagation();
-      return false;
-    }); // Notify user when file is over the drop area
-
-    editor.on('dragover', function (e) {
+    // Notify user when file is over the drop area
+    editor.on('dragover', (e) => {
       e.preventDefault();
       editor.contentAreaContainer.style.borderStyle = 'dashed';
       editor.contentAreaContainer.style.borderWidth = '5px';
       return false;
     });
 
-    function uploadFile(name, content) {
-      var _data;
+    // Logic for the dropped file
+    editor.on('drop', (e) => {
+      if (!e.dataTransfer || !e.dataTransfer.files || e.dataTransfer.files.length !== 1) {
+        return;
+      }
 
-      var url = editor.settings.uploadUri + "&path=" + editor.settings.comMediaAdapter;
-      var data = (_data = {}, _data[editor.settings.csrfToken] = '1', _data.name = name, _data.content = content, _data.parent = editor.settings.parentUploadFolder, _data);
-      Joomla.request({
-        url: url,
-        method: 'POST',
-        data: JSON.stringify(data),
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        onSuccess: function onSuccess(resp) {
-          var response;
-
-          try {
-            response = JSON.parse(resp);
-          } catch (e) {
-            editor.windowManager.alert(Joomla.Text._('ERROR') + ": {e}");
-          }
-
-          if (response.data && response.data.path) {
-            responseData = response.data;
-            var urlPath; // For local adapters use relative paths
-
-            if (/local-/.test(responseData.adapter)) {
-              var _Joomla$getOptions = Joomla.getOptions('system.paths'),
-                  rootFull = _Joomla$getOptions.rootFull;
-
-              urlPath = "" + response.data.url.split(rootFull)[1];
-            } else if (responseData.url) {
-              // Absolute path for different domain
-              urlPath = responseData.url;
-            }
-
-            var dialogClose = function dialogClose(api) {
-              var dialogData = api.getData();
-              var altEmpty = dialogData.altEmpty ? ' alt=""' : '';
-              var altValue = dialogData.altText ? " alt=\"" + dialogData.altText + "\"" : altEmpty;
-              var lazyValue = dialogData.isLazy ? ' loading="lazy"' : '';
-              var width = dialogData.isLazy ? " width=\"" + responseData.width + "\"" : '';
-              var height = dialogData.isLazy ? " height=\"" + responseData.height + "\"" : '';
-              editor.execCommand('mceInsertContent', false, "<img src=\"" + urlPath + "\"" + altValue + lazyValue + width + height + "/>");
-            };
-
-            editor.windowManager.open({
-              title: Joomla.Text._('PLG_TINY_DND_ADDITIONALDATA'),
-              body: {
-                type: 'panel',
-                items: [{
-                  type: 'input',
-                  name: 'altText',
-                  label: Joomla.Text._('PLG_TINY_DND_ALTTEXT')
-                }, {
-                  type: 'checkbox',
-                  name: 'altEmpty',
-                  label: Joomla.Text._('PLG_TINY_DND_EMPTY_ALT')
-                }, {
-                  type: 'checkbox',
-                  name: 'isLazy',
-                  label: Joomla.Text._('PLG_TINY_DND_LAZYLOADED')
-                }]
-              },
-              buttons: [{
-                type: 'cancel',
-                text: 'Cancel'
-              }, {
-                type: 'submit',
-                name: 'submitButton',
-                text: 'Save',
-                primary: true
-              }],
-              initialData: {
-                altText: '',
-                isLazy: true,
-                altEmpty: false
-              },
-              onSubmit: function onSubmit(api) {
-                dialogClose(api);
-                api.close();
-              },
-              onCancel: function onCancel(api) {
-                dialogClose(api);
-              }
-            });
-          }
-        },
-        onError: function onError(xhr) {
-          editor.windowManager.alert("Error: " + xhr.statusText);
-        }
-      });
-    }
-
-    function readFile(file) {
-      // Create a new file reader instance
-      var reader = new FileReader(); // Add the on load callback
-
-      reader.onload = function (progressEvent) {
-        var result = progressEvent.target.result;
-        var splitIndex = result.indexOf('base64') + 7;
-        var content = result.slice(splitIndex, result.length); // Upload the file
-
-        uploadFile(file.name, content);
-      };
-
-      reader.readAsDataURL(file);
-    } // Listeners for drag and drop
-
-
-    if (typeof FormData !== 'undefined') {
-      // Logic for the dropped file
-      editor.on('drop', function (e) {
-        e.preventDefault(); // We override only for files
-
-        if (e.dataTransfer && e.dataTransfer.files && e.dataTransfer.files.length > 0) {
-          var files = [].slice.call(e.dataTransfer.files);
-          files.forEach(function (file) {
-            // Only images allowed
-            if (file.name.toLowerCase().match(/\.(jpg|jpeg|png|gif)$/)) {
-              // Upload the file(s)
-              readFile(file);
-            }
-          });
+      // We override only for files
+      e.preventDefault();
+      for (const file of [...e.dataTransfer.files]) {
+        // Only images allowed
+        if (!['image/jpeg', 'image/png', 'image/gif', 'image/webp'].includes(file.type)) {
+          return;
         }
 
-        editor.contentAreaContainer.style.borderWidth = '1px 0 0';
-      });
-    } else {
-      Joomla.renderMessages({
-        error: [Joomla.Text._('PLG_TINY_ERR_UNSUPPORTEDBROWSER')]
-      });
-      editor.on('drop', function (e) {
-        e.preventDefault();
-        return false;
-      });
-    }
+        Promise
+          .all([blobToBase64(file)])
+          .then((result) => {
+            // Upload the file
+
+            uploadFile(file.name, result[0], editor);
+          })
+          .catch((error) => {});
+      }
+
+      editor.contentAreaContainer.style.borderWidth = '1px 0 0';
+    });
   });
-
-}());
+})();
