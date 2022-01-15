003f0996f96a6f5b9dac7b58a7fdbc18914a3618
wilsonge <georgejameswilson@googlemail.com>
2022-01-15 16:43:48 +0000

    Tag RC3

--- libraries/src/Version.php
+++ libraries/src/Version.php
@@ -61,7 +61,7 @@ final class Version
 	 * @var    string
 	 * @since  3.8.0
 	 */
-	const EXTRA_VERSION = 'rc3-dev';
+	const EXTRA_VERSION = 'rc3';
 
 	/**
 	 * Development status.
@@ -69,7 +69,7 @@ final class Version
 	 * @var    string
 	 * @since  3.5
 	 */
-	const DEV_STATUS = 'Development';
+	const DEV_STATUS = 'Release Candidate';
 
 	/**
 	 * Code name.
@@ -85,7 +85,7 @@ final class Version
 	 * @var    string
 	 * @since  3.5
 	 */
-	const RELDATE = '9-January-2022';
+	const RELDATE = '15-January-2022';
 
 	/**
 	 * Release time.
@@ -93,7 +93,7 @@ final class Version
 	 * @var    string
 	 * @since  3.5
 	 */
-	const RELTIME = '00:18';
+	const RELTIME = '16:41';
 
 	/**
 	 * Release timezone.
