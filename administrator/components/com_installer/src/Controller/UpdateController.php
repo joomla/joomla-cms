997d3b0bd2ea475ade843aee85c1cd27f7f035a1
brian teeman <brian@teeman.net>
2022-01-18 12:07:02 +0000

    [4.0] Disabled Update Sites message

--- administrator/components/com_installer/src/Controller/UpdateController.php
+++ administrator/components/com_installer/src/Controller/UpdateController.php
@@ -12,6 +12,7 @@ namespace Joomla\Component\Installer\Administrator\Controller;
 \defined('_JEXEC') or die;
 
 use Joomla\CMS\Component\ComponentHelper;
+use Joomla\CMS\Factory;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\MVC\Controller\BaseController;
 use Joomla\CMS\Response\JsonResponse;
@@ -108,14 +109,14 @@ class UpdateController extends BaseController
 		if ($disabledUpdateSites)
 		{
 			$updateSitesUrl = Route::_('index.php?option=com_installer&view=updatesites');
-			$this->setMessage(Text::sprintf('COM_INSTALLER_MSG_UPDATE_SITES_COUNT_CHECK', $updateSitesUrl), 'warning');
+			Factory::getApplication()->enqueueMessage(Text::sprintf('COM_INSTALLER_MSG_UPDATE_SITES_COUNT_CHECK', $updateSitesUrl), 'warning');
 		}
 
 		$model->findUpdates(0, $cache_timeout, $minimum_stability);
 
 		if (0 === $model->getTotal())
 		{
-			$this->setMessage(Text::_('COM_INSTALLER_MSG_UPDATE_NOUPDATES'), 'info');
+			Factory::getApplication()->enqueueMessage(Text::_('COM_INSTALLER_MSG_UPDATE_NOUPDATES'), 'info');
 		}
 
 		$this->setRedirect(Route::_('index.php?option=com_installer&view=update', false));
