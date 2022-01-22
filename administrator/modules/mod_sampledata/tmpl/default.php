48e568473f1ebf6bcac8f2960dd4687ab46e1687
pritam825 <54195688+pritam825@users.noreply.github.com>
2022-01-22 21:15:07 +0530

    Update administrator/modules/mod_sampledata/tmpl/default.php
    Co-authored-by: Quy <quy@fluxbb.org>

--- administrator/modules/mod_sampledata/tmpl/default.php
+++ administrator/modules/mod_sampledata/tmpl/default.php
@@ -32,7 +32,7 @@ $app->getDocument()->addScriptOptions(
 		<?php foreach($items as $i => $item) : ?>
 			<li class="list-group-item sampledata-<?php echo $item->name; ?>">
 				<div class="d-flex justify-content-between align-items-center">
-					<div class="sample-data__title ">
+					<div class="sample-data__title”>
 						<span class="sample-data__icon icon-<?php echo $item->icon; ?> me-1" aria-hidden="true"></span>
 						<?php echo htmlspecialchars($item->title, ENT_QUOTES, 'UTF-8'); ?>
 					</div>
