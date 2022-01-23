834b5af97d33144c5cf32c64283bb2ea764a55b9
beat <beat@joomlapolis.com>
2022-01-23 01:21:47 +0100

    [3.10] [PHP 8.1] Fixes com_finder/indexer/query
    Fixes `Deprecated: html_entity_decode(): Passing null to parameter #1 ($string) of type string is deprecated in administrator/components/com_finder/helpers/indexer/query.php on line 733`

--- administrator/components/com_finder/helpers/indexer/query.php
+++ administrator/components/com_finder/helpers/indexer/query.php
@@ -730,7 +730,7 @@ class FinderIndexerQuery
 	protected function processString($input, $lang, $mode)
 	{
 		// Clean up the input string.
-		$input = html_entity_decode($input, ENT_QUOTES, 'UTF-8');
+		$input = html_entity_decode((string) $input, ENT_QUOTES, 'UTF-8');
 		$input = StringHelper::strtolower($input);
 		$input = preg_replace('#\s+#mi', ' ', $input);
 		$input = trim($input);
