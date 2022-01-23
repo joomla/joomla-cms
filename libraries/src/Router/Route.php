3f598a1412401f3a035f0104b450f11c36872513
beat <beat@joomlapolis.com>
2022-01-23 20:45:40 +0100

    Revert previous commit for different fix in mod_breadcrumbs/helper.php

--- libraries/src/Router/Route.php
+++ libraries/src/Router/Route.php
@@ -121,7 +121,7 @@ class Route
 	public static function link($client, $url, $xhtml = true, $tls = self::TLS_IGNORE, $absolute = false)
 	{
 		// If we cannot process this $url exit early.
-		if (is_null($url) || (!is_array($url) && (strpos($url, '&') !== 0) && (strpos($url, 'index.php') !== 0)))
+		if (!is_array($url) && (strpos($url, '&') !== 0) && (strpos($url, 'index.php') !== 0))
 		{
 			return $url;
 		}
