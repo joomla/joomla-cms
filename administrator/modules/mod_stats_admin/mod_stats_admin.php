af4471cc1be9db8c99d9c3ebb89ad85113bed275
Benjamin Trenkle <benjamin.trenkle@wicked-software.de>
2022-02-01 01:29:53 +0100

    Remove spaces

--- administrator/modules/mod_stats_admin/mod_stats_admin.php
+++ administrator/modules/mod_stats_admin/mod_stats_admin.php
@@ -14,8 +14,8 @@ use Joomla\CMS\Helper\ModuleHelper;
 use Joomla\Database\DatabaseInterface;
 use Joomla\Module\StatsAdmin\Administrator\Helper\StatsAdminHelper;
 
-$serverinfo      = $params->get('serverinfo');
-$siteinfo        = $params->get('siteinfo');
-$list            = StatsAdminHelper::getStats($params, $app, Factory::getContainer()->get(DatabaseInterface::class));
+$serverinfo = $params->get('serverinfo');
+$siteinfo   = $params->get('siteinfo');
+$list       = StatsAdminHelper::getStats($params, $app, Factory::getContainer()->get(DatabaseInterface::class));
 
 require ModuleHelper::getLayoutPath('mod_stats_admin', $params->get('layout', 'default'));
