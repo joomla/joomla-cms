b665479ad3695871606b6aa5a48b5f7e3296a5e2
dgrammatiko <d.grammatiko@gmail.com>
2022-02-13 15:30:16 +0100

    the rest
    Signed-off-by: dgrammatiko <d.grammatiko@gmail.com>

--- components/com_finder/tmpl/search/default_result.php
+++ components/com_finder/tmpl/search/default_result.php
@@ -12,14 +12,13 @@ defined('_JEXEC') or die;
 use Joomla\CMS\Factory;
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
+use Joomla\CMS\Layout\LayoutHelper;
 use Joomla\CMS\Router\Route;
 use Joomla\Component\Finder\Administrator\Indexer\Helper;
 use Joomla\Component\Finder\Administrator\Indexer\Taxonomy;
 use Joomla\String\StringHelper;
-use Joomla\Utilities\ArrayHelper;
 
-$user = Factory::getApplication()->getIdentity();
-
+$user             = Factory::getApplication()->getIdentity();
 $show_description = $this->params->get('show_description', 1);
 
 if ($show_description)
@@ -49,47 +48,39 @@ if ($show_description)
 	$description = HTMLHelper::_('string.truncate', StringHelper::substr($full_description, $start), $desc_length, true);
 }
 
-$showImage = $this->params->get('show_image', 0);
+$showImage  = $this->params->get('show_image', 0);
+$imageClass = $this->params->get('image_class', '');
+$extraAttr  = [];
 
-if ($showImage && !empty($this->result->imageUrl))
+if ($showImage && !empty($this->result->imageUrl) && '' !== $imageClass)
 {
-	$imageclass        = $this->params->get('image_class', '');
-	$image             = HTMLHelper::cleanImageURL($this->result->imageUrl);
-	$extraAttr         = '';
-
-	// Set lazyloading only for images which have width and height attributes
-	if ((isset($image->attributes['width']) && (int) $image->attributes['width'] > 0)
-		&& (isset($image->attributes['height']) && (int) $image->attributes['height'] > 0))
-	{
-		$extraAttr = ArrayHelper::toString($image->attributes) . ' loading="lazy"';
-	}
+	$extraAttr['class'] = $this->params->get('image_class', '');
 }
 
 $icon = '';
-if (!empty($this->result->mime)) :
+if (!empty($this->result->mime))
+{
 	$icon = '<span class="icon-file-' . $this->result->mime . '" aria-hidden="true"></span> ';
-endif;
+}
+
 
 $show_url = '';
-if ($this->params->get('show_url', 1)) :
+if ($this->params->get('show_url', 1))
+{
 	$show_url = '<cite class="result__title-url">' . $this->baseUrl . Route::_($this->result->cleanURL) . '</cite>';
-endif;
+}
 ?>
 <li class="result__item">
 	<?php if (isset($image)) : ?>
 		<figure class="<?php echo htmlspecialchars($imageclass, ENT_COMPAT, 'UTF-8'); ?> result__image">
 			<?php if ($this->params->get('link_image') && $this->result->route) : ?>
 				<a href="<?php echo Route::_($this->result->route); ?>">
-					<img src="<?php echo htmlspecialchars($image->url, ENT_COMPAT, 'UTF-8'); ?>"
-						 alt="<?php echo htmlspecialchars($this->result->imageAlt, ENT_COMPAT, 'UTF-8'); ?>"
-							<?php echo $extraAttr; ?>
-					/>
+					<?php $extraAttr = array_merge($extraAttr, ['src' => $image->url, 'alt' => $this->result->imageAlt]); ?>
+					<?php echo LayoutHelper::render('joomla.html.image', $extraAttr); ?>
 				</a>
 			<?php else : ?>
-				<img src="<?php echo htmlspecialchars($image->url, ENT_COMPAT, 'UTF-8'); ?>"
-					 alt="<?php echo htmlspecialchars($this->result->imageAlt, ENT_COMPAT, 'UTF-8'); ?>"
-						<?php echo $extraAttr; ?>
-				/>
+				<?php $extraAttr = array_merge($extraAttr, ['src' => $image->url, 'alt' => $this->result->imageAlt]); ?>
+				<?php echo LayoutHelper::render('joomla.html.image', $extraAttr); ?>
 			<?php endif; ?>
 		</figure>
 	<?php endif; ?>
