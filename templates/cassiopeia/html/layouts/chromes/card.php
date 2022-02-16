7c433949c731d3b8ce9e76235a53acdfc47e83a0
Rick R2H <rick@r2h.nl>
2022-02-16 22:09:20 +0100

    Revert tabs to spaces because it's ugly

--- templates/cassiopeia/html/layouts/chromes/card.php
+++ templates/cassiopeia/html/layouts/chromes/card.php
@@ -11,21 +11,21 @@ defined('_JEXEC') or die;
 
 use Joomla\Utilities\ArrayHelper;
 
-$module		= $displayData['module'];
-$params		= $displayData['params'];
-$attribs	= $displayData['attribs'];
+$module  = $displayData['module'];
+$params  = $displayData['params'];
+$attribs = $displayData['attribs'];
 
 if ($module->content === null || $module->content === '')
 {
 	return;
 }
 
-$moduleTag				= $params->get('module_tag', 'div');
-$moduleAttribs			= [];
-$moduleClassSuffix		= htmlspecialchars($params->get('moduleclass_sfx', ''), ENT_QUOTES, 'UTF-8');
-$headerTag				= htmlspecialchars($params->get('header_tag', 'h3'), ENT_QUOTES, 'UTF-8');
-$headerClass			= htmlspecialchars($params->get('header_class', ''), ENT_QUOTES, 'UTF-8');
-$headerAttribs			= [];
+$moduleTag              = $params->get('module_tag', 'div');
+$moduleAttribs          = [];
+$moduleClassSuffix      = htmlspecialchars($params->get('moduleclass_sfx', ''), ENT_QUOTES, 'UTF-8');
+$headerTag              = htmlspecialchars($params->get('header_tag', 'h3'), ENT_QUOTES, 'UTF-8');
+$headerClass            = htmlspecialchars($params->get('header_class', ''), ENT_QUOTES, 'UTF-8');
+$headerAttribs          = [];
 $headerAttribs['class'] = $headerClass;
 
 // Add a space if the module class suffix is set
