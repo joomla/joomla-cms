7967a595f7c2c53bdc0460d72609d5e6069306e8
Denitz <pandenitz@mail.ru>
2022-02-17 08:20:02 +0300

    CS

--- plugins/system/jooa11y/jooa11y.php
+++ plugins/system/jooa11y/jooa11y.php
@@ -4,7 +4,7 @@
  * @subpackage      System.jooa11y
  *
  * @copyright   (C) 2021 Open Source Matters, Inc. <https://www.joomla.org>
- * @license         GNU General Public License version 2 or later; see LICENSE.txt
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 defined('_JEXEC') or die;
