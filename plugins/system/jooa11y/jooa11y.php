49b4593fc6865bbce35b378b036358184cac8dba
Denitz <pandenitz@mail.ru>
2022-02-17 08:21:34 +0300

    CS

--- plugins/system/jooa11y/jooa11y.php
+++ plugins/system/jooa11y/jooa11y.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * @package         Joomla.Plugin
- * @subpackage      System.jooa11y
+ * @package     Joomla.Plugin
+ * @subpackage  System.jooa11y
  *
  * @copyright   (C) 2021 Open Source Matters, Inc. <https://www.joomla.org>
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
