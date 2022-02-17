efef8a4635b136e555d2db2f25e7d59479b7d5d6
Olivier Buisard <olivier.buisard@simplifyyourweb.com>
2022-02-17 11:11:09 -0500

    Additional layout usable in subforms
    The default layout render.php breaks the html markup when rendered inside a subform (missing <dl>).
    This layout prevents such issue.

---
+++ components/com_contact/layouts/field/subform.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  com_contact
+ *
+ * @copyright   (C) 2016 Open Source Matters, Inc. <https://www.joomla.org>
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Language\Text;
+
+if (!array_key_exists('field', $displayData))
+{
+	return;
+}
+
+$field = $displayData['field'];
+$label = Text::_($field->label);
+$value = $field->value;
+$showLabel = $field->params->get('showlabel');
+$prefix = Text::plural($field->params->get('prefix'), $value);
+$suffix = Text::plural($field->params->get('suffix'), $value);
+$labelClass = $field->params->get('label_render_class');
+$valueClass = $field->params->get('value_render_class');
+
+if ($field->context == 'com_contact.mail')
+{
+	// Prepare the value for the contact form mail
+	$value = html_entity_decode($value);
+
+	echo ($showLabel ? $label . ': ' : '') . $value . "\r\n";
+	return;
+}
+
+if ($value == '')
+{
+	return;
+}
+
+?>
+<?php if ($showLabel == 1) : ?>
+	<span class="field-label <?php echo $labelClass; ?>"><?php echo htmlentities($label, ENT_QUOTES | ENT_IGNORE, 'UTF-8'); ?>: </span>
+<?php endif; ?>
+<?php if ($prefix) : ?>
+	<span class="field-prefix"><?php echo htmlentities($prefix, ENT_QUOTES | ENT_IGNORE, 'UTF-8'); ?></span>
+<?php endif; ?>
+<span class="field-value <?php echo $valueClass; ?>"><?php echo $value; ?></span>
+<?php if ($suffix) : ?>
+	<span class="field-suffix"><?php echo htmlentities($suffix, ENT_QUOTES | ENT_IGNORE, 'UTF-8'); ?></span>
+<?php endif; ?>
