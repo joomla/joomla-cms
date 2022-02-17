5034279fec8a88da2ae5e6a1513a659ea633c20c
Olivier Buisard <olivier.buisard@simplifyyourweb.com>
2022-02-17 11:00:57 -0500

    Fixes to fully take advantage of all available parameters
    - adds the field name as class 
    - adds suffix/prefix
    - adds the value render class
    - removes empty space when there is no render class

--- components/com_contact/layouts/field/render.php
+++ components/com_contact/layouts/field/render.php
@@ -15,14 +15,21 @@ if (!array_key_exists('field', $displayData))
 	return;
 }
 
-$field = $displayData['field'];
-$label = Text::_($field->label);
-$value = $field->value;
+$field     = $displayData['field'];
+$label     = Text::_($field->label);
+$value     = $field->value;
 $showLabel = $field->params->get('showlabel');
 $prefix = Text::plural($field->params->get('prefix'), $value);
 $suffix = Text::plural($field->params->get('suffix'), $value);
 $labelClass = $field->params->get('label_render_class');
-$valueClass = $field->params->get('render_class');
+$valueClass = $field->params->get('value_render_class');
+
+$class = $field->name;
+
+if ($field->params->get('render_class'))
+{
+	$class .= ' ' . $field->params->get('render_class');
+}
 
 if ($field->context == 'com_contact.mail')
 {
@@ -39,13 +46,17 @@ if ($value == '')
 }
 
 ?>
-<?php if ($showLabel == 1) : ?>
-	<span class="field-label <?php echo $labelClass; ?>"><?php echo htmlentities($label, ENT_QUOTES | ENT_IGNORE, 'UTF-8'); ?>: </span>
-<?php endif; ?>
-<?php if ($prefix) : ?>
-	<span class="field-prefix"><?php echo htmlentities($prefix, ENT_QUOTES | ENT_IGNORE, 'UTF-8'); ?></span>
-<?php endif; ?>
-<span class="field-value <?php echo $valueClass; ?>"><?php echo $value; ?></span>
-<?php if ($suffix) : ?>
-	<span class="field-suffix"><?php echo htmlentities($suffix, ENT_QUOTES | ENT_IGNORE, 'UTF-8'); ?></span>
-<?php endif; ?>
+<dt class="contact-field-entry <?php echo $class; ?>">
+	<?php if ($showLabel == 1) : ?>
+		<span class="field-label <?php echo $labelClass; ?>"><?php echo htmlentities($label, ENT_QUOTES | ENT_IGNORE, 'UTF-8'); ?>: </span>
+	<?php endif; ?>
+</dt>
+<dd class="contact-field-entry <?php echo $class; ?>">
+	<?php if ($prefix) : ?>
+		<span class="field-prefix"><?php echo htmlentities($prefix, ENT_QUOTES | ENT_IGNORE, 'UTF-8'); ?></span>
+	<?php endif; ?>
+	<span class="field-value <?php echo $valueClass; ?>"><?php echo $value; ?></span>
+	<?php if ($suffix) : ?>
+		<span class="field-suffix"><?php echo htmlentities($suffix, ENT_QUOTES | ENT_IGNORE, 'UTF-8'); ?></span>
+	<?php endif; ?>
+</dd>
