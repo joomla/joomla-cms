a00fab348f6be484085c777ff4060f4dc0c34254
Сергей <koreshs@mail.ru>
2022-03-02 18:07:00 +0300

    Update CheckboxField.php

--- libraries/src/Form/Field/CheckboxField.php
+++ libraries/src/Form/Field/CheckboxField.php
@@ -138,7 +138,7 @@ class CheckboxField extends FormField
 	protected function getLayoutData()
 	{
 		$data            = parent::getLayoutData();
-		$data['value']   = $this->default ?: '1';
+		$data['value']	 = $this->default || $this->default === '0' ? $this->default : '1';
 		$data['checked'] = $this->checked || $this->value;
 
 		return $data;
