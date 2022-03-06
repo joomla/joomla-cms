9b31d62f9a5d8e508b1b00c43379e616374a5a69
Anmol Agrawal <anmol2002.aa1@gmail.com>
2022-03-06 11:57:02 +0530

    Commit in codebase regarding issue #37109

--- administrator/modules/mod_user/tmpl/default.php
+++ administrator/modules/mod_user/tmpl/default.php
@@ -19,43 +19,48 @@ $hideLinks = $app->input->getBool('hidemainmenu');
 
 if ($hideLinks)
 {
-	return;
+return;
 }
 
 // Load the Bootstrap Dropdown
 HTMLHelper::_('bootstrap.dropdown', '.dropdown-toggle');
 ?>
 <div class="header-item-content dropdown header-profile">
-	<button class="dropdown-toggle d-flex align-items-center ps-0 py-0" data-bs-toggle="dropdown" type="button"
-		title="<?php echo Text::_('MOD_USER_MENU'); ?>">
-		<div class="header-item-icon">
-			<span class="icon-user-circle" aria-hidden="true"></span>
-		</div>
-		<div class="header-item-text">
-			<?php echo Text::_('MOD_USER_MENU'); ?>
-		</div>
-		<span class="icon-angle-down" aria-hidden="true"></span>
-	</button>
-	<div class="dropdown-menu dropdown-menu-end">
-		<div class="dropdown-header">
-			<span class="icon-user-circle icon-fw" aria-hidden="true"></span>
-			<?php echo Text::sprintf('MOD_USER_TITLE', $user->name); ?>
-		</div>
-		<?php $uri   = Uri::getInstance(); ?>
-		<?php $route = 'index.php?option=com_users&task=user.edit&id=' . $user->id . '&return=' . base64_encode($uri) . '#attrib-user_details'; ?>
-		<a class="dropdown-item" href="<?php echo Route::_($route); ?>">
-			<span class="icon-user icon-fw" aria-hidden="true"></span>
-			<?php echo Text::_('MOD_USER_EDIT_ACCOUNT'); ?>
-		</a>
-		<?php $route = 'index.php?option=com_users&task=user.edit&id=' . $user->id . '&return=' . base64_encode($uri) . '#attrib-accessibility'; ?>
-		<a class="dropdown-item" href="<?php echo Route::_($route); ?>">
-			<span class="icon-universal-access icon-fw" aria-hidden="true"></span>
-			<?php echo Text::_('MOD_USER_ACCESSIBILITY_SETTINGS'); ?>
-		</a>
-		<?php $route = 'index.php?option=com_login&task=logout&amp;' . Session::getFormToken() . '=1'; ?>
-		<a class="dropdown-item" href="<?php echo Route::_($route); ?>">
-			<span class="icon-power-off icon-fw" aria-hidden="true"></span>
-			<?php echo Text::_('JLOGOUT'); ?>
-		</a>
-	</div>
+<button class="dropdown-toggle d-flex align-items-center ps-0 py-0" data-bs-toggle="dropdown" type="button"
+title="<?php echo Text::_('MOD_USER_MENU'); ?>">
+<div class="header-item-icon">
+<span class="icon-user-circle" aria-hidden="true"></span>
+</div>
+<div class="header-item-text">
+<?php echo Text::_('MOD_USER_MENU'); ?>
+</div>
+<span class="icon-angle-down" aria-hidden="true"></span>
+</button>
+<div class="dropdown-menu dropdown-menu-end">
+<div class="dropdown-header">
+<span class="icon-user-circle icon-fw" aria-hidden="true"></span>
+<?php echo Text::sprintf('MOD_USER_TITLE', $user->name); ?>
+</div>
+<?php $uri = Uri::getInstance(); ?>
+<?php $route = 'index.php?option=com_users&task=user.edit&id=' . $user->id . '&return=' . base64_encode($uri) . '#attrib-user_details'; ?>
+<a class="dropdown-item" href="<?php echo Route::_($route); ?>">
+<span class="icon-user icon-fw" aria-hidden="true"></span>
+<?php echo Text::_('MOD_USER_EDIT_ACCOUNT'); ?>
+</a>
+<?php $route = 'index.php?option=com_users&task=user.edit&id=' . $user->id . '&return=' . base64_encode($uri) . '#attrib-accessibility'; ?>
+<a class="dropdown-item" href="<?php echo Route::_($route); ?>">
+<span class="icon-universal-access icon-fw" aria-hidden="true"></span>
+<?php echo Text::_('MOD_USER_ACCESSIBILITY_SETTINGS'); ?>
+</a>
+<?php $route = 'index.php?option=com_login&task=logout&' . Session::getFormToken() . '=1'; ?>
+<a class="dropdown-item" href="<?php echo Route::_($route); ?>">
+<span class="icon-power-off icon-fw" aria-hidden="true"></span>
+<?php echo Text::_('JLOGOUT'); ?>
+</a>
+</div>
+<div class="header-item-content dropdown header-profile">
+<a href="<?php echo Route::_($route); ?>">
+<span class="icon-power-off icon-fw" aria-hidden="true"></span>
+</a>
+</div>
 </div>
