5362f4a60c4655a34220b180e76950ffffc3f45e
dgrammatiko <d.grammatiko@gmail.com>
2022-03-10 15:12:26 +0100

    meh

--- build/build-modules-js/init/patches.es6.js
+++ build/build-modules-js/init/patches.es6.js
@@ -35,15 +35,16 @@ module.exports.patchPackages = async (options) => {
   let faPath = join(mediaVendorPath, 'fontawesome-free/scss/_larger.scss');
   let newScss = (await readFile(faPath, { encoding: 'utf8' })).replace('(4em / 3)', '(4em * .333)').replace('(3em / 4)', '(3em * .025)');
   await writeFile(faPath, newScss, { encoding: 'utf8', mode: 0o644 });
-  await writeFile(join(RootPath, '/media/vendor/fontawesome-free/scss/_larger.scss'), newScss, { encoding: 'utf8', mode: 0o644 });
+  await writeFile(join(RootPath, 'node_modules/@fortawesome/fontawesome-free/scss/_larger.scss'), newScss, { encoding: 'utf8', mode: 0o644 });
+console.log(mediaVendorPath)
   // _list.scss
   faPath = join(mediaVendorPath, 'fontawesome-free/scss/_list.scss');
   newScss = (await readFile(faPath, { encoding: 'utf8' })).replace('5/4', '1.25');
   await writeFile(faPath, newScss, { encoding: 'utf8', mode: 0o644 });
-  await writeFile(join(RootPath, '/media/vendor/fontawesome-free/scss/_list.scss'), newScss, { encoding: 'utf8', mode: 0o644 });
+  await writeFile(join(RootPath, 'node_modules/@fortawesome/fontawesome-free/scss/_list.scss'), newScss, { encoding: 'utf8', mode: 0o644 });
   // _variables.scss
   faPath = join(mediaVendorPath, 'fontawesome-free/scss/_variables.scss');
   newScss = (await readFile(faPath, { encoding: 'utf8' })).replace('(20em / 16)', '(20em * .0625)');
   await writeFile(faPath, newScss, { encoding: 'utf8', mode: 0o644 });
-  await writeFile(join(RootPath, '/media/vendor/fontawesome-free/scss/_variables.scss'), newScss, { encoding: 'utf8', mode: 0o644 });
+  await writeFile(join(RootPath, 'node_modules/@fortawesome/fontawesome-free/scss/_variables.scss'), newScss, { encoding: 'utf8', mode: 0o644 });
 };
