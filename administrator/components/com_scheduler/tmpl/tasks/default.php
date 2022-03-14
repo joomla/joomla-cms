b4a0a75dc374d66a0c15f3ae865c2522d6ffd48e
heelc29 <66922325+heelc29@users.noreply.github.com>
2022-03-14 13:46:00 +0900

    Enhance checkout tasks (#36875)
    Function checkout for task enhance
    Fix database null by checkin tasks

--- administrator/components/com_scheduler/tmpl/tasks/default.php
+++ administrator/components/com_scheduler/tmpl/tasks/default.php
@@ -151,9 +151,10 @@ $wa->useScript('multiselect')
 				<tbody <?php if ($saveOrder): ?>
 					class="js-draggable" data-url="<?php echo $saveOrderingUrl; ?>" data-direction="<?php echo strtolower($listDirn); ?>" data-nested="true" <?php endif; ?>>
 				<?php foreach ($this->items as $i => $item):
-					$canCreate = $user->authorise('core.create', 'com_scheduler');
-					$canEdit = $user->authorise('core.edit', 'com_scheduler');
-					$canChange = $user->authorise('core.edit.state', 'com_scheduler');
+					$canCreate  = $user->authorise('core.create',     'com_scheduler');
+					$canEdit    = $user->authorise('core.edit',       'com_scheduler');
+					$canCheckin = $user->authorise('core.manage',     'com_checkin') || $item->checked_out == $userId || is_null($item->checked_out);
+					$canChange  = $user->authorise('core.edit.state', 'com_scheduler') && $canCheckin;
 					?>
 
 					<!-- Row begins -->
@@ -196,6 +197,9 @@ $wa->useScript('multiselect')
 
 						<!-- Item name, edit link, and note (@todo: should it be moved?) -->
 						<th scope="row">
+							<?php if ($item->checked_out) : ?>
+								<?php echo HTMLHelper::_('jgrid.checkedout', $i, $item->editor, $item->checked_out_time, 'tasks.', $canCheckin); ?>
+							<?php endif; ?>
 							<?php if ($item->locked) : ?>
 								<?php echo HTMLHelper::_('jgrid.action', $i, 'unlock', ['enabled' => $canChange, 'prefix' => 'tasks.',
 									'active_class' => 'none fa fa-running border-dark text-body',
