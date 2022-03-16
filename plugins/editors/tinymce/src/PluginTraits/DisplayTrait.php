1111e20387749734d93476c5a757d25b34714406
Dimitris Grammatikogiannis <dg@dgrammatiko.dev>
2022-03-16 20:02:08 +0100

    Wrong name

--- plugins/editors/tinymce/src/PluginTraits/DisplayTrait.php
+++ plugins/editors/tinymce/src/PluginTraits/DisplayTrait.php
@@ -366,7 +366,7 @@ trait DisplayTrait
 		}
 
 		// Use CodeMirror in the code view instead of plain text to provide syntax highlighting
-		if ($levelParams->get('highlightPlus', 1))
+		if ($levelParams->get('sourcecode', 1))
 		{
 			$externalPlugins['highlightPlus'] = HTMLHelper::_('script', 'plg_editors_tinymce/plugins/highlighter/plugin-es5.min.js', ['relative' => true, 'version' => 'auto', 'pathOnly' => true]);
 		}
