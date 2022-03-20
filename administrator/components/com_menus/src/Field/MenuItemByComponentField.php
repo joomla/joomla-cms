11bca7ec1d25a4c97fc95e836a3a79a7f5742a8b
Richard Fath <richard67@users.noreply.github.com>
2022-03-20 14:36:50 +0100

    Fix copyright year
    Co-authored-by: Brian Teeman <brian@teeman.net>

--- administrator/components/com_menus/src/Field/MenuItemByComponentField.php
+++ administrator/components/com_menus/src/Field/MenuItemByComponentField.php
@@ -3,7 +3,7 @@
  * @package     Joomla.Administrator
  * @subpackage  com_menus
  *
- * @copyright   (C) 2021 Open Source Matters, Inc. <https://www.joomla.org>
+ * @copyright   (C) 2022 Open Source Matters, Inc. <https://www.joomla.org>
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
