61c6f1fe8792fb350034f495a5112d71cfeb96c8
Roland Dalmulder <contact@rolandd.com>
2022-03-27 10:52:04 +0200

    Updated deleted/renamed files
    Signed-off-by: Roland Dalmulder <contact@rolandd.com>

--- administrator/components/com_admin/script.php
+++ administrator/components/com_admin/script.php
@@ -7712,6 +7712,8 @@ class JoomlaInstallerScript
 			'/libraries/vendor/tobscure/json-api/.git/hooks',
 			'/libraries/vendor/tobscure/json-api/.git/branches',
 			'/libraries/vendor/tobscure/json-api/.git',
+			// From 4.1.1 to 4.2.0
+			'/libraries/src/Service/Provider/ApiRouter.php'
 		);
 
 		$status['files_checked'] = $files;
