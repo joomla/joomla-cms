60fbda9a630c1169de1a11531c7293ef15fbc958
YatharthVyas <yatharth2000.vyas@gmail.com>
2022-03-27 17:12:38 +0530

    add client id filter in sql update script

--- administrator/components/com_admin/sql/updates/postgresql/4.2.0-2022-03-27.sql
+++ administrator/components/com_admin/sql/updates/postgresql/4.2.0-2022-03-27.sql
@@ -1,4 +1,5 @@
 -- Update link to featured
 UPDATE "#__menu"
    SET "link" = 'index.php?option=com_content&view=articles&featured=1'
- WHERE "link" = 'index.php?option=com_content&view=featured';
+ WHERE "link" = 'index.php?option=com_content&view=featured'
+   AND "client_id" = 1;
