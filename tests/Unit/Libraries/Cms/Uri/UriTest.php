61b48218f93c6b2ef29764cfefa1d38336eac983
Brian Teeman <brian@teeman.net>
2022-03-29 17:20:46 +0100

    [4.1] codestyle uri unittest
    Fix copy [paste error. Corrects the copyright date as its a new file.
    
    Looks like all the `@since ` values also need to be corrected as well

--- tests/Unit/Libraries/Cms/Uri/UriTest.php
+++ tests/Unit/Libraries/Cms/Uri/UriTest.php
@@ -3,7 +3,7 @@
  * @package     Joomla.UnitTest
  * @subpackage  Uri
  *
- * @copyright   (C) 2013 Open Source Matters, Inc. <https://www.joomla.org>
+ * @copyright   (C) 2022 Open Source Matters, Inc. <https://www.joomla.org>
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
