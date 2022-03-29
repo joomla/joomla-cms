794c47f8340916b1b05703085ac99c1b3a539c78
Roland Dalmulder <contact@rolandd.com>
2022-03-29 19:42:04 +0200

    Revert to dev

--- libraries/src/Version.php
+++ libraries/src/Version.php
@@ -61,7 +61,7 @@ final class Version
 	 * @var    string
 	 * @since  3.8.0
 	 */
-	const EXTRA_VERSION = 'alpha1-dev';
+	const EXTRA_VERSION = 'alpha2-dev';
 
 	/**
 	 * Development status.
@@ -93,7 +93,7 @@ final class Version
 	 * @var    string
 	 * @since  3.5
 	 */
-	const RELTIME = '18:00';
+	const RELTIME = '18:41';
 
 	/**
 	 * Release timezone.
