f49c451d1ecb5dcc28b6a99a9e33ef75dd6b4f45
Benjamin Trenkle <benjamin.trenkle@wicked-software.de>
2022-03-29 15:27:09 +0200

    Bump to 4.1.1 stable

--- libraries/src/Version.php
+++ libraries/src/Version.php
@@ -61,7 +61,7 @@ final class Version
 	 * @var    string
 	 * @since  3.8.0
 	 */
-	const EXTRA_VERSION = 'rc3-dev';
+	const EXTRA_VERSION = '';
 
 	/**
 	 * Development status.
@@ -69,7 +69,7 @@ final class Version
 	 * @var    string
 	 * @since  3.5
 	 */
-	const DEV_STATUS = 'Development';
+	const DEV_STATUS = 'Stable';
 
 	/**
 	 * Code name.
@@ -85,7 +85,7 @@ final class Version
 	 * @var    string
 	 * @since  3.5
 	 */
-	const RELDATE = '25-March-2022';
+	const RELDATE = '29-March-2022';
 
 	/**
 	 * Release time.
@@ -93,7 +93,7 @@ final class Version
 	 * @var    string
 	 * @since  3.5
 	 */
-	const RELTIME = '23:27';
+	const RELTIME = '16:00';
 
 	/**
 	 * Release timezone.
