daf30f8a2470bf49462b57ba08cf17ce5d79f12a
David Jardin <d.jardin@djumla.de>
2022-03-30 09:40:46 +0200

    remove namespace

--- plugins/user/joomla/joomla.php
+++ plugins/user/joomla/joomla.php
@@ -9,7 +9,6 @@
 
 defined('_JEXEC') or die;
 
-use Joomla\CMS\Authentication\ProviderAwareAuthenticationPluginInterface;
 use Joomla\CMS\Component\ComponentHelper;
 use Joomla\CMS\Factory;
 use Joomla\CMS\Language\LanguageFactoryInterface;
