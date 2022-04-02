c74f8da9360cd35715980cc7098875fe7afa6e55
Fedir Zinchuk <getthesite@gmail.com>
2022-04-02 14:19:24 +0300

    Apply suggestions from code review
    Co-authored-by: Brian Teeman <brian@teeman.net>

--- layouts/joomla/form/field/date.php
+++ layouts/joomla/form/field/date.php
@@ -3,7 +3,7 @@
  * @package     Joomla.Site
  * @subpackage  Layout
  *
- * @copyright   (C) 2016 Open Source Matters, Inc. <https://www.joomla.org>
+ * @copyright   (C) 2022 Open Source Matters, Inc. <https://www.joomla.org>
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
--- libraries/src/Form/Field/DatetimeField.php
+++ libraries/src/Form/Field/DatetimeField.php
@@ -2,7 +2,7 @@
 /**
  * Joomla! Content Management System
  *
- * @copyright  (C) 2009 Open Source Matters, Inc. <https://www.joomla.org>
+ * @copyright  (C) 2022 Open Source Matters, Inc. <https://www.joomla.org>
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 
