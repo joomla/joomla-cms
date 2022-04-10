93ea8b850e14aeda4c8031193d2338d1c29e4961
chmst <dev@chmst.de>
2022-04-10 12:37:48 +0200

    Fix error in empty state screen, remove add button

--- administrator/components/com_installer/tmpl/discover/emptystate.php
+++ administrator/components/com_installer/tmpl/discover/emptystate.php
@@ -11,17 +11,14 @@ defined('_JEXEC') or die;
 
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\Layout\LayoutHelper;
-use Joomla\CMS\Session\Session;
 
 $displayData = [
 	'textPrefix' => 'COM_INSTALLER',
 	'formURL'    => 'index.php?option=com_installer&task=discover.refresh',
 	'helpURL'    => 'https://docs.joomla.org/Special:MyLanguage/Help4.x:Extensions:_Discover',
 	'icon'       => 'icon-puzzle-piece install',
-	'createURL'  => 'index.php?option=com_installer&task=discover.refresh&' . Session::getFormToken() . '=1',
 	'content'    => Text::_('COM_INSTALLER_MSG_DISCOVER_DESCRIPTION'),
 	'title'      => Text::_('COM_INSTALLER_EMPTYSTATE_DISCOVER_TITLE'),
-	'btnadd'     => Text::_('COM_INSTALLER_EMPTYSTATE_DISCOVER_BUTTON_ADD'),
 ];
 
 echo LayoutHelper::render('joomla.content.emptystate', $displayData);
