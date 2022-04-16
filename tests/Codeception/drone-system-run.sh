2adbcd972f9f7adbd0a312474f7dc4d9ba9aaa39
Robert Deutz <rdeutz@googlemail.com>
2022-04-16 10:42:26 +0200

    just to have a look

--- tests/Codeception/drone-system-run.sh
+++ tests/Codeception/drone-system-run.sh
@@ -12,21 +12,21 @@ echo "[RUNNER] Copy files to test installation"
 rsync -a --exclude-from=tests/Codeception/exclude.txt $JOOMLA_BASE/ /tests/www/$DB_ENGINE/
 chown -R www-data /tests/www/$DB_ENGINE/
 
-echo "[RUNNER] Start Apache & Chrome"
-apache2ctl -D FOREGROUND &
-google-chrome --version
+# echo "[RUNNER] Start Apache & Chrome"
+# apache2ctl -D FOREGROUND &
+# google-chrome --version
 
 
-echo "[RUNNER] Prepare Selenium"
+#echo "[RUNNER] Prepare Selenium"
 mkdir -p tests/Codeception/_output
-if [[ -f "/usr/lib/node_modules/selenium-standalone/lib/default-config.js" ]]; then
-	cp /usr/lib/node_modules/selenium-standalone/lib/default-config.js tests/Codeception/_output/selenium.config.js
-fi
+#if [[ -f "/usr/lib/node_modules/selenium-standalone/lib/default-config.js" ]]; then
+#	cp /usr/lib/node_modules/selenium-standalone/lib/default-config.js tests/Codeception/_output/selenium.config.js
+#fi
 
-echo "[RUNNER] Start Selenium"
-selenium-standalone start > tests/Codeception/_output/selenium.$DB_ENGINE.log 2>&1 &
-echo "Waiting 6 seconds till Selenium is ready..."
-sleep 6
+#echo "[RUNNER] Start Selenium"
+#selenium-standalone start > tests/Codeception/_output/selenium.$DB_ENGINE.log 2>&1 &
+#echo "Waiting 6 seconds till Selenium is ready..."
+#sleep 6
 
 echo "[RUNNER] Run Codeception"
 php libraries/vendor/bin/codecept build
