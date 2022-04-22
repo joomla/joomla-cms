ca71060303aba3e139af8462c651e52249fbe044
vorayash <vorayash9028@gmail.com>
2022-04-22 09:46:35 +0530

    Improved packageinstaller validation

--- build/media_source/plg_installer_packageinstaller/js/packageinstaller.es6.js
+++ build/media_source/plg_installer_packageinstaller/js/packageinstaller.es6.js
@@ -125,6 +125,13 @@ Joomla = window.Joomla || {};
         return;
       }
 
+      if (file.name.split('.').pop() !== "zip") {
+        Joomla.renderMessages({
+          warning: [Joomla.Text._('COM_INSTALLER_MSG_WARNINGS_UPLOADFILENOTZIP')]
+        });
+        return;
+      }
+
       data.append('install_package', file);
       data.append('installtype', 'upload');
       dragZone.setAttribute('data-state', 'uploading');
