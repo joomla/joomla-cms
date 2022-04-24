3916853e4a324961f79cf5c63a80d4ef7336ff4d
Allon Moritz <allon.moritz@digital-peak.com>
2022-04-24 17:27:53 +0200

    imports

--- administrator/components/com_modules/src/Controller/DisplayController.php
+++ administrator/components/com_modules/src/Controller/DisplayController.php
@@ -11,9 +11,6 @@ namespace Joomla\Component\Modules\Administrator\Controller;
 
 \defined('_JEXEC') or die;
 
-use Joomla\CMS\Factory;
-use Joomla\CMS\Helper\ModuleHelper;
-use Joomla\CMS\Language\LanguageHelper;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\MVC\Controller\BaseController;
 use Joomla\CMS\Router\Route;
