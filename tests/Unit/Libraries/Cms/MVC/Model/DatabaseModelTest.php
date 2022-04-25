18e2f1d13c5eb62c4f83a2fd339f6dd73412c3b2
Allon Moritz <allon.moritz@digital-peak.com>
2022-04-25 20:57:05 +0200

    cs

--- tests/Unit/Libraries/Cms/MVC/Model/DatabaseModelTest.php
+++ tests/Unit/Libraries/Cms/MVC/Model/DatabaseModelTest.php
@@ -235,7 +235,7 @@ class DatabaseModelTest extends UnitTestCase
 		$this->assertEquals($db, $model->getDbo());
 	}
 
-	/*
+	/**
 	 * @testdox  Test that the BaseDatabaseModel operates normally even when no variable is declared
 	 *
 	 * @return  void
