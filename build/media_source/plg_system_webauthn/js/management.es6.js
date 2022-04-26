joomla/joomla-cms

commit cd9c48a406e3849ab2d80d24e7afd1197b5e1300

[4.1] webauthn table accessibility (#37464)
This pr adds the required table caption and column/row scope to the table.

There are no visual changes and brings this table into line with all other admin tables.

As webauthn requires https this PR cannot be tested on a site without https.

Co-authored-by: Quy <quy@fluxbb.org>

layouts/plugins/system/webauthn/manage.php
@@ -98,17 +98,20 @@ $postbackURL = base64_encode(rtrim(Uri::base(), '/') . '/index.php?' . Joomla::g
 	<?php endif; ?>
 
 	<table class="table table-striped">
+		<caption class="visually-hidden">
+			<?php echo Text::_('PLG_SYSTEM_WEBAUTHN_TABLE_CAPTION'); ?>,
+		</caption>
 		<thead class="table-dark">
 		<tr>
-			<th><?php echo Text::_('PLG_SYSTEM_WEBAUTHN_MANAGE_FIELD_KEYLABEL_LABEL') ?></th>
-			<th><?php echo Text::_('PLG_SYSTEM_WEBAUTHN_MANAGE_HEADER_ACTIONS_LABEL') ?></th>
+			<th scope="col"><?php echo Text::_('PLG_SYSTEM_WEBAUTHN_MANAGE_FIELD_KEYLABEL_LABEL') ?></th>
+			<th scope="col"><?php echo Text::_('PLG_SYSTEM_WEBAUTHN_MANAGE_HEADER_ACTIONS_LABEL') ?></th>
 		</tr>
 		</thead>
 		<tbody>
 		<?php // phpcs:ignore
 		foreach ($credentials as $method): ?>
 			<tr data-credential_id="<?php echo $method['id'] ?>">
-				<td><?php echo htmlentities($method['label']) ?></td>
+				<th scope="row"><?php echo htmlentities($method['label']) ?></th>
 				<td>
 					<button data-random-id="<?php echo $randomId; ?>" class="plg_system_webauthn-manage-edit btn btn-secondary">
 						<span class="icon-edit" aria-hidden="true"></span>

administrator/language/en-GB/plg_system_webauthn.ini
@@ -43,3 +43,4 @@ PLG_SYSTEM_WEBAUTHN_MANAGE_HEADER_NOMETHODS_LABEL="No authenticators have been s
 PLG_SYSTEM_WEBAUTHN_MSG_DELETED="The authenticator has been removed."
 PLG_SYSTEM_WEBAUTHN_MSG_SAVED_LABEL="The label has been saved."
 PLG_SYSTEM_WEBAUTHN_REQUIRES_GMP="Either of the PHP extensions GMP or BCmath must be loaded to add authenticators."
+PLG_SYSTEM_WEBAUTHN_TABLE_CAPTION="Table of WebAuthn authenticators."

commit 84b996aabf32c3b5306894c67d4ea9ca18ea2384

Fix the WebAuthn management page which was broken in https://github.com/joomla/joomla-cms/pull/37464

build/media_source/plg_system_webauthn/js/management.es6.js
@@ -191,7 +191,7 @@ window.Joomla = window.Joomla || {};
     // Find the UI elements
     const elTR = that.parentElement.parentElement;
     const credentialId = elTR.dataset.credential_id;
-    const elTDs = elTR.querySelectorAll('td');
+    const elTDs = elTR.querySelectorAll('.webauthnManagementCell');
     const elLabelTD = elTDs[0];
     const elButtonsTD = elTDs[1];
     const elButtons = elButtonsTD.querySelectorAll('button');

layouts/plugins/system/webauthn/manage.php
@@ -109,8 +109,8 @@ $publicKey = $allow_add ? base64_encode(json_encode(Authentication::getPubKeyCre
 		<?php // phpcs:ignore
 		foreach ($credentials as $method): ?>
 			<tr data-credential_id="<?php echo $method['id'] ?>">
-				<th scope="row"><?php echo htmlentities($method['label']) ?></th>
-				<td>
+				<th scope="row" class="webauthnManagementCell"><?php echo htmlentities($method['label']) ?></th>
+				<td class="webauthnManagementCell">
 					<button data-random-id="<?php echo $randomId; ?>" class="plg_system_webauthn-manage-edit btn btn-secondary">
 						<span class="icon-edit" aria-hidden="true"></span>
 						<?php echo Text::_('PLG_SYSTEM_WEBAUTHN_MANAGE_BTN_EDIT_LABEL') ?>