1fc794d13a06c358dfb14da2a1eba5fccfcf64b1
zero-24 <zero-24@users.noreply.github.com>
2022-05-01 14:41:57 +0200

    Joomla! 3.10.9 Release Candidate 1

--- libraries/src/Version.php
+++ libraries/src/Version.php
@@ -60,7 +60,7 @@ final class Version
 	 * @var    string
 	 * @since  3.8.0
 	 */
-	const EXTRA_VERSION = 'dev';
+	const EXTRA_VERSION = 'rc1';
 
 	/**
 	 * Release version.
@@ -78,7 +78,7 @@ final class Version
 	 * @since  3.5
 	 * @deprecated  4.0  Use separated version constants instead
 	 */
-	const DEV_LEVEL = '9-dev';
+	const DEV_LEVEL = '9-rc1';
 
 	/**
 	 * Development status.
@@ -86,7 +86,7 @@ final class Version
 	 * @var    string
 	 * @since  3.5
 	 */
-	const DEV_STATUS = 'Development';
+	const DEV_STATUS = 'Release Candidate';
 
 	/**
 	 * Build number.
@@ -111,7 +111,7 @@ final class Version
 	 * @var    string
 	 * @since  3.5
 	 */
-	const RELDATE = '30-March-2022';
+	const RELDATE = '1-May-2022';
 
 	/**
 	 * Release time.
@@ -119,7 +119,7 @@ final class Version
 	 * @var    string
 	 * @since  3.5
 	 */
-	const RELTIME = '15:05';
+	const RELTIME = '13:39';
 
 	/**
 	 * Release timezone.
