bef3f1149dd967cfb213a1f4bf8ea5d18a05ed98
Shazma Siddiqui <siddiquishaz7@gmail.com>
2022-05-01 09:10:50 +0530

    fixed recaptcha position in user registration form

--- components/com_users/tmpl/registration/default.php
+++ components/com_users/tmpl/registration/default.php
@@ -26,6 +26,7 @@ HTMLHelper::_('behavior.formvalidator');
 
 	<form id="member-registration" action="<?php echo Route::_('index.php?option=com_users&task=registration.register'); ?>" method="post" class="com-users-registration__form form-validate" enctype="multipart/form-data">
 		<?php // Iterate through the form fieldsets and display each one. ?>
+		<?php $captcha=''; ?>
 		<?php foreach ($this->form->getFieldsets() as $fieldset) : ?>
 			<?php $fields = $this->form->getFieldset($fieldset->name); ?>
 			<?php if (count($fields)) : ?>
@@ -34,10 +35,19 @@ HTMLHelper::_('behavior.formvalidator');
 					<?php if (isset($fieldset->label)) : ?>
 						<legend><?php echo Text::_($fieldset->label); ?></legend>
 					<?php endif; ?>
-					<?php echo $this->form->renderFieldset($fieldset->name); ?>
+					<?php foreach ($fields as $field): ?>			
+						<?php if ($field->name=="jform[captcha]"): ?> 
+							<?php $captcha=$field; ?>
+						<?php else: ?>
+							<?php echo $field->renderField();?>
+						<?php endif; ?>		
+					<?php endforeach; ?>
 				</fieldset>
 			<?php endif; ?>
 		<?php endforeach; ?>
+		<?php if($captcha): ?>
+			<?php echo $captcha->renderField();?>
+		<?php endif; ?>	
 		<div class="com-users-registration__submit control-group">
 			<div class="controls">
 				<button type="submit" class="com-users-registration__register btn btn-primary validate">
