c8404fe4f0fa67de947a4d70e84299012e15a84b
Brian Teeman <brian@teeman.net>
2022-05-06 09:56:51 +0100

    space

--- administrator/modules/mod_latest/mod_latest.php
+++ administrator/modules/mod_latest/mod_latest.php
@@ -16,7 +16,7 @@ use Joomla\Module\Latest\Administrator\Helper\LatestHelper;
 
 $model = $app->bootComponent('com_content')->getMVCFactory()->createModel('Articles', 'Administrator', ['ignore_request' => true]);
 $list = LatestHelper::getList($params, $model);
-$workflow_enabled  = ComponentHelper::getParams('com_content')->get('workflow_enabled');
+$workflow_enabled = ComponentHelper::getParams('com_content')->get('workflow_enabled');
 
 if ($workflow_enabled)
 {
