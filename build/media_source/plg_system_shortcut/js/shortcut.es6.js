81870e98475eee0c1200e4b6a43d9f6633e222b8
Benjamin Trenkle <benjamin.trenkle@wicked-software.de>
2022-05-09 23:42:58 +0200

    First step of moving to hotkeys

--- build/media_source/plg_system_shortcut/js/shortcut.es6.js
+++ build/media_source/plg_system_shortcut/js/shortcut.es6.js
@@ -1,170 +1,73 @@
-/*apply*/
-Mousetrap.bind('alt+s', function (e) {
-  document.querySelector('joomla-toolbar-button button.button-apply').click();
-});
+class Shortcut {
+  constructor() {
+    if (!Joomla) {
+      throw new Error('Joomla API is not properly initialised');
+    }
 
-/*new*/
-Mousetrap.bind('alt+n', function (e) {
-  document.querySelector('joomla-toolbar-button button.button-new').click();
-});
+    ((window, document, Joomla) => {
+      'use strict';
 
-/*save*/
-Mousetrap.bind('alt+w', function (e) {
-  document.querySelector('joomla-toolbar-button button.button-save').click();
-});
+      Joomla.addClickButtonShortcut = (hotkey, selector) => {
+        Joomla.addShortcut(hotkey, () => {
+          const actionBtn = document.querySelector(selector);
+          if (actionBtn) {
+            actionBtn.click();
+          }
+        });
+      };
 
-/*saveNew*/
-Mousetrap.bind('shift+alt+n', function (e) {
-  document.querySelector('joomla-toolbar-button button.button-save-new').click();
-});
+      Joomla.addShortcut = (hotkey, callback) => {
+        if (prevent) {
+          event.preventDefault();
+        }
+        hotkeys(hotkey, (event, handler) => {
+          event.preventDefault();
 
-/*help*/
-Mousetrap.bind('alt+x', function (e) {
-  document.querySelector('joomla-toolbar-button button.button-help').click();
-});
+          callback.call();
+        });
+      };
+      document.addEventListener('DOMContentLoaded', ()=> {
+        const options = Joomla.getOptions('plg_system_shortcut.shortcuts');
 
-/*cancel*/
-Mousetrap.bind('alt+q', function (e) {
-  document.querySelector('joomla-toolbar-button button.button-cancel').click();
-});
+        options.forEach((callback, hotkey) {
+          Joomla.addShortcut(hotkey, callback);
+        });
+      })
 
-/*copy*/
-Mousetrap.bind('shift+alt+c', function (e) {
-  document.querySelector('joomla-toolbar-button button.button-button-copy').click();
-});
+        if (window.navigator.platform.match('Mac') ? e.metaKey : e.altKey) {
+        // On Press ALT + S
+          const ch = e.key.toLowerCase();
+          const list = [{ c: 'joomla-toolbar-button button.button-save-copy' },
+            { h: 'joomla-toolbar-button button.button-help' },
+            { q: 'joomla-toolbar-button button.button-cancel' },
+            { s: 'joomla-toolbar-button button.button-apply' },
+            { n: 'joomla-toolbar-button button.button-new' },
+            { w: 'joomla-toolbar-button button.button-save' },
+            { n: 'joomla-toolbar-button button.button-save-new' },
+          ];
 
-/*article*/
-Mousetrap.bind('ctrl+alt+a', function (e) {
-  document.querySelector('joomla-editor-option~article_modal').click();
-});
+          list.map((l) => {
+            if (ch == l.first) { Joomla.addShortcut(e, l.second); }
+          });
+        }
+      }
 
-/*contact*/
-Mousetrap.bind('ctrl+alt+c', function (e) {
-  document.querySelector('joomla-editor-option~contact_modal').click();
-});
+      window.addEventListener('DOMContentLoaded', () => {
+        document.addEventListener(
+          'keydown',
+          (e) => {
+            handleKeyPressEvent(e);
+          },
+          false,
+        );
 
-/*contact*/
-Mousetrap.bind('ctrl+alt+c', function (e) {
-  document.querySelector('joomla-editor-option~contact_modal').click();
-});
-
-/*fields*/
-Mousetrap.bind('ctrl+alt+f', function (e) {
-  document.querySelector('joomla-editor-option~fields_modal').click();
-});
-
-/*image*/
-Mousetrap.bind('ctrl+alt+l', function (e) {
-  document.querySelector('joomla-editor-option~image_modal').click();
-});
-
-/*menu*/
-Mousetrap.bind('ctrl+alt+m', function (e) {
-  document.querySelector('joomla-editor-option~menu_modal').click();
-});
-
-/*module*/
-Mousetrap.bind('ctrl+shift+alt+m', function (e) {
-  document.querySelector('joomla-editor-option~module_modal').click();
-});
-
-/*pagebreak*/
-Mousetrap.bind('ctrl+alt+p', function (e) {
-  document.querySelector('joomla-editor-option~pagebreak_modal').click();
-});
-
-/*readmore*/
-Mousetrap.bind('ctrl+alt+r', function (e) {
-  document.querySelector('joomla-editor-option~read_more').click();
-});
-
-const defaultOptions = [
-  {
-    KeyBtn: 'apply :',
-    keyEvent: 'alt+s',
-    selector: 'joomla-toolbar-button button.button-apply',
-  },
-  {
-    KeyBtn: 'new :',
-    keyEvent: 'alt+n',
-    selector: 'joomla-toolbar-button button.button-new',
-  },
-  {
-    KeyBtn: 'save :',
-    keyEvent: 'alt+w',
-    selector: 'joomla-toolbar-button button.button-save',
-  },
-  {
-    KeyBtn: 'saveNew :',
-    keyEvent: 'shift+alt+n',
-    selector: 'joomla-toolbar-button button.button-save-new',
-  },
-  {
-    KeyBtn: 'help :',
-    keyEvent: 'alt+x',
-    selector: 'joomla-toolbar-button button.button-help',
-  },
-  {
-    KeyBtn: 'cancel :',
-    keyEvent: 'alt+q',
-    selector: 'joomla-toolbar-button button.button-cancel',
-  },
-  {
-    KeyBtn: 'copy :',
-    keyEvent: 'shift+alt+c',
-    selector: 'joomla-toolbar-button button.button-button-copy',
-  },
-  {
-    KeyBtn: 'article :',
-    keyEvent: 'ctrl+alt+a',
-    selector: 'joomla-editor-option~article_modal',
-  },
-  {
-    KeyBtn: 'contact :',
-    keyEvent: 'ctrl+alt+c',
-    selector: 'joomla-editor-option~contact_modal',
-  },
-  {
-    KeyBtn: 'fields :',
-    keyEvent: 'ctrl+alt+f',
-    selector: 'joomla-editor-option~fields_modal',
-  },
-  {
-    KeyBtn: 'image :',
-    keyEvent: 'ctrl+alt+l',
-    selector: 'joomla-editor-option~image_modal',
-  },
-  {
-    KeyBtn: 'menu :',
-    keyEvent: 'ctrl+alt+m',
-    selector: 'joomla-editor-option~menu_modal',
-  },
-  {
-    KeyBtn: 'module :',
-    keyEvent: 'ctrl+shift+alt+m',
-    selector: 'joomla-editor-option~module_modal',
-  },
-  {
-    KeyBtn: 'pagebreak :',
-    keyEvent: 'ctrl+alt+p',
-    selector: 'joomla-editor-option~pagebreak_modal',
-  },
-  {
-    KeyBtn: 'readmore :',
-    keyEvent: 'ctrl+alt+r',
-    selector: 'joomla-editor-option~read_more',
-  },
-];
-
-Mousetrap.bind('escape', function (e) {
-  let keys = 'Shortcut Keys \n\n';
-  defaultOptions.forEach(shortcutkeylist);
-
-  function shortcutkeylist(item, index) {
-    button = item.KeyBtn;
-    keyevent = item.keyEvent.toUpperCase();
-    keys += button + ' ' + keyevent + '\n';
+        try {
+          tinyMCE.activeEditor.on('keydown', (e) => {
+            handleKeyPressEvent(e);
+          });
+        } catch (e) {}
+      });
+    })(window, document, Joomla);
   }
-
-  alert(keys);
-});
+}
+new Shortcut();
--- plugins/system/shortcut/shortcut.php
+++ plugins/system/shortcut/shortcut.php
@@ -9,156 +9,219 @@
 
 defined('_JEXEC') or die;
 
+use Joomla\CMS\Application\AdministratorApplication;
+use Joomla\CMS\Component\ComponentHelper;
+use Joomla\CMS\Event\GenericEvent;
 use Joomla\CMS\Factory;
 use Joomla\CMS\Plugin\CMSPlugin;
-use Joomla\CMS\Language\Text;
+use Joomla\CMS\Plugin\PluginHelper;
+use Joomla\Event\Event;
+use Joomla\Event\SubscriberInterface;
 
 /**
  * Shortcut plugin to add accessible keyboard navigation to the site and administrator templates.
  *
  * @since  __DEPLOY_VERSION__
  */
-class PlgSystemShortcut extends CMSPlugin
+class PlgSystemShortcut extends CMSPlugin implements SubscriberInterface
 {
 	/**
 	 * Application object.
 	 *
-	 * @var    JApplicationCms
-	 * @since  4.1
+	 * @var    AdministratorApplication
+	 *
+	 * @since  __DEPLOY_VERSION__
 	 */
 	protected $app;
 
 	/**
-	 * Base path for keyboard shortcut
+	 * Returns an array of events this subscriber will listen to.
 	 *
-	 * @var    string
-	 * @since  4.1
+	 * The array keys are event names and the value can be:
+	 *
+	 *  - The method name to call (priority defaults to 0)
+	 *  - An array composed of the method name to call and the priority
+	 *
+	 * For instance:
+	 *
+	 *  * array('eventName' => 'methodName')
+	 *  * array('eventName' => array('methodName', $priority))
+	 *
+	 * @return  array
+	 *
+	 * @since   __DEPLOY_VERSION__
 	 */
-	protected $_basePath = 'media/plg_system_shortcut';
-
-	public function onBeforeCompileHead()
+	public static function getSubscribedEvents(): array
 	{
-		if ($this->app->isClient('administrator')) {
-			$joomla_shortcut_keys = array(
-				'new' => array(
-					'keyEvent' => $this->params->get('new_keyEvent', 'n'),
-					'hasShift' => $this->params->get('new_hasShift', 0),
-					'hasAlt' => $this->params->get('new_hasAlt', 1),
-					'hasControl' => $this->params->get('new_hasControl', 0),
-					'selector' => 'joomla-toolbar-button button.button-new'
-				),
-				'apply' => array(
-					'keyEvent' => $this->params->get('apply_keyEvent', 's'),
-					'hasShift' => $this->params->get('apply_hasShift', 0),
-					'hasAlt' => $this->params->get('apply_hasAlt', 1),
-					'hasControl' => $this->params->get('apply_hasControl', 0),
-					'selector' => 'joomla-toolbar-button button.button-apply'
-				),
-				'save' => array(
-					'keyEvent' => $this->params->get('save_keyEvent', 'w'),
-					'hasShift' => $this->params->get('save_hasShift', 0),
-					'hasAlt' => $this->params->get('save_hasAlt', 1),
-					'hasControl' => $this->params->get('save_hasControl', 0),
-					'selector' => 'joomla-toolbar-button button.button-save'
-				),
-				'saveNew' => array(
-					'keyEvent' => $this->params->get('saveNew_keyEvent', 'n'),
-					'hasShift' => $this->params->get('saveNew_hasShift', 1),
-					'hasAlt' => $this->params->get('saveNew_hasAlt', 1),
-					'hasControl' => $this->params->get('saveNew_hasControl', 0),
-					'selector' => 'joomla-toolbar-button button.button-save-new'
-				),
-				'help' => array(
-					'keyEvent' => $this->params->get('help_keyEvent', 'x'),
-					'hasShift' => $this->params->get('help_hasShift', 0),
-					'hasAlt' => $this->params->get('help_hasAlt', 1),
-					'hasControl' => $this->params->get('help_hasControl', 0),
-					'selector' => 'joomla-toolbar-button button.button-help'
-				),
-				'cancel' => array(
-					'keyEvent' => $this->params->get('cancel_keyEvent', 'q'),
-					'hasShift' => $this->params->get('cancel_hasShift', 0),
-					'hasAlt' => $this->params->get('cancel_hasAlt', 1),
-					'hasControl' => $this->params->get('cancel_hasControl', 0),
-					'selector' => 'joomla-toolbar-button button.button-cancel'
-				),
-				'copy' => array(
-					'keyEvent' => $this->params->get('copy_keyEvent', 'c'),
-					'hasShift' => $this->params->get('copy_hasShift', 1),
-					'hasAlt' => $this->params->get('copy_hasAlt', 1),
-					'hasControl' => $this->params->get('copy_hasControl', 0),
-					'selector' => 'joomla-toolbar-button button.button-button-copy'
-				),
-				'article' => array(
-					'keyEvent' => $this->params->get('article_keyEvent', 'a'),
-					'hasShift' => $this->params->get('article_hasShift', 0),
-					'hasAlt' => $this->params->get('article_hasAlt', 1),
-					'hasControl' => $this->params->get('article_hasControl', 1),
-					'selector' => 'joomla-editor-option~article_modal'
-				),
-				'contact' => array(
-					'keyEvent' => $this->params->get('contact_keyEvent', 'c'),
-					'hasShift' => $this->params->get('contact_hasShift', 0),
-					'hasAlt' => $this->params->get('contact_hasAlt', 1),
-					'hasControl' => $this->params->get('contact_hasControl', 1),
-					'selector' => 'joomla-editor-option~contact_modal'
-				),
-				'fields' => array(
-					'keyEvent' => $this->params->get('fields_keyEvent', 'f'),
-					'hasShift' => $this->params->get('fields_hasShift', 0),
-					'hasAlt' => $this->params->get('fields_hasAlt', 1),
-					'hasControl' => $this->params->get('fields_hasControl', 1),
-					'selector' => 'joomla-editor-option~fields_modal'
-				),
-				'image' => array(
-					'keyEvent' => $this->params->get('image_keyEvent', 'i'),
-					'hasShift' => $this->params->get('image_hasShift', 0),
-					'hasAlt' => $this->params->get('image_hasAlt', 1),
-					'hasControl' => $this->params->get('image_hasControl', 1),
-					'selector' => 'joomla-editor-option~image_modal'
-				),
-				'menu' => array(
-					'keyEvent' => $this->params->get('menu_keyEvent', 'm'),
-					'hasShift' => $this->params->get('menu_hasShift', 0),
-					'hasAlt' => $this->params->get('menu_hasAlt', 1),
-					'hasControl' => $this->params->get('menu_hasControl', 1),
-					'selector' => 'joomla-editor-option~menu_modal'
-				),
-				'module' => array(
-					'keyEvent' => $this->params->get('module_keyEvent', 'm'),
-					'hasShift' => $this->params->get('module_hasShift', 1),
-					'hasAlt' => $this->params->get('module_hasAlt', 1),
-					'hasControl' => $this->params->get('module_hasControl', 1),
-					'selector' => 'joomla-editor-option~module_modal'
-				),
-				'pagebreak' => array(
-					'keyEvent' => $this->params->get('pagebreak_keyEvent', 'p'),
-					'hasShift' => $this->params->get('pagebreak_hasShift', 0),
-					'hasAlt' => $this->params->get('pagebreak_hasAlt', 1),
-					'hasControl' => $this->params->get('pagebreak_hasControl', 1),
-					'selector' => 'joomla-editor-option~pagebreak_modal'
-				),
-				'readmore' => array(
-					'keyEvent' => $this->params->get('readmore_keyEvent', 'r'),
-					'hasShift' => $this->params->get('readmore_hasShift', 0),
-					'hasAlt' => $this->params->get('readmore_hasAlt', 1),
-					'hasControl' => $this->params->get('readmore_hasControl', 1),
-					'selector' => 'joomla-editor-option~read_more'
-				)
-			);
-			$wa = $this->app->getDocument()->getWebAssetManager();
-			$config = $this->app->getConfig();
-			$editor = $config->get('editor', 'tinymce');
+		return [
+			'onBeforeCompileHead' => 'initialize',
+			'onLoadShortcuts' => 'addShortcuts'
+		];
+	}
 
-			$document = $this->app->getDocument();
-			$document->addScript(JURI::Root().'media/plg_system_shortcut/js/mousetrap.min.js');
-			$document->addScriptOptions('editor', $editor);
-			$document->addScriptOptions('joomla-shortcut-keys', $joomla_shortcut_keys);
-			$document->addScript(JURI::Root().'media/plg_system_shortcut/js/shortcut.js');
-			
-			return true;
+
+	/**
+	 * Add the javascript for the shortcuts
+	 *
+	 * @return  void
+	 *
+	 * @since   __DEPLOY_VERSION__
+	 */
+	public function initialize()
+	{
+		if (!$this->app->isClient('administrator'))
+		{
+			return;
 		}
 
+		PluginHelper::importPlugin('shortcut');
+
+		$context = $this->app->get('option') . '.' . $this->app->get('view');
+
+		$shortcuts = [];
+
+		$event = new GenericEvent('onLoadShortcuts', [
+			'context' => $context,
+			'shortcuts' => $shortcuts
+		]);
+
+		$this->app->getDispatcher()->dispatch('onLoadShortcuts', $event);
+
+		$shortcuts = $event->getArgument('shortcuts');
+
+		print_r($shortcuts);
+
+		$wa = $this->app->getDocument()->getWebAssetManager();
+
+		$wa->registerAndUseScript('script', 'plg_system_shortcut/shortcut.min.js', ['dependencies' => ['hotkeys.js']]);
+
+		$this->app->getDocument()->addScriptOptions('plg_system_shortcut.shortcuts', $shortcuts);
+
 		return true;
+	}
+
+	/**
+	 * Add default shortcuts to the document
+	 *
+	 * @param Event $event The event
+	 *
+	 * @return void
+	 */
+	public function addShortcuts(Event $event)
+	{
+		$context = $event->getArgument('context');
+		$shortcuts = $event->getArgument('shortcuts');
+
+		$shortcuts['ctrl+alt+a'] = 'alert(\'test\');';
+
+		$event->setArgument('shortcuts', $shortcuts);
+
+		$joomla_shortcut_keys = array(
+			'new' => array(
+				'keyEvent' => $this->params->get('new_keyEvent', 'n'),
+				'hasShift' => $this->params->get('new_hasShift', 0),
+				'hasAlt' => $this->params->get('new_hasAlt', 1),
+				'hasControl' => $this->params->get('new_hasControl', 0),
+				'selector' => 'joomla-toolbar-button button.button-new'
+			),
+			'apply' => array(
+				'keyEvent' => $this->params->get('apply_keyEvent', 's'),
+				'hasShift' => $this->params->get('apply_hasShift', 0),
+				'hasAlt' => $this->params->get('apply_hasAlt', 1),
+				'hasControl' => $this->params->get('apply_hasControl', 0),
+				'selector' => 'joomla-toolbar-button button.button-apply'
+			),
+			'save' => array(
+				'keyEvent' => $this->params->get('save_keyEvent', 'w'),
+				'hasShift' => $this->params->get('save_hasShift', 0),
+				'hasAlt' => $this->params->get('save_hasAlt', 1),
+				'hasControl' => $this->params->get('save_hasControl', 0),
+				'selector' => 'joomla-toolbar-button button.button-save'
+			),
+			'saveNew' => array(
+				'keyEvent' => $this->params->get('saveNew_keyEvent', 'n'),
+				'hasShift' => $this->params->get('saveNew_hasShift', 1),
+				'hasAlt' => $this->params->get('saveNew_hasAlt', 1),
+				'hasControl' => $this->params->get('saveNew_hasControl', 0),
+				'selector' => 'joomla-toolbar-button button.button-save-new'
+			),
+			'help' => array(
+				'keyEvent' => $this->params->get('help_keyEvent', 'x'),
+				'hasShift' => $this->params->get('help_hasShift', 0),
+				'hasAlt' => $this->params->get('help_hasAlt', 1),
+				'hasControl' => $this->params->get('help_hasControl', 0),
+				'selector' => 'joomla-toolbar-button button.button-help'
+			),
+			'cancel' => array(
+				'keyEvent' => $this->params->get('cancel_keyEvent', 'q'),
+				'hasShift' => $this->params->get('cancel_hasShift', 0),
+				'hasAlt' => $this->params->get('cancel_hasAlt', 1),
+				'hasControl' => $this->params->get('cancel_hasControl', 0),
+				'selector' => 'joomla-toolbar-button button.button-cancel'
+			),
+			'copy' => array(
+				'keyEvent' => $this->params->get('copy_keyEvent', 'c'),
+				'hasShift' => $this->params->get('copy_hasShift', 1),
+				'hasAlt' => $this->params->get('copy_hasAlt', 1),
+				'hasControl' => $this->params->get('copy_hasControl', 0),
+				'selector' => 'joomla-toolbar-button button.button-button-copy'
+			),
+			'article' => array(
+				'keyEvent' => $this->params->get('article_keyEvent', 'a'),
+				'hasShift' => $this->params->get('article_hasShift', 0),
+				'hasAlt' => $this->params->get('article_hasAlt', 1),
+				'hasControl' => $this->params->get('article_hasControl', 1),
+				'selector' => 'joomla-editor-option~article_modal'
+			),
+			'contact' => array(
+				'keyEvent' => $this->params->get('contact_keyEvent', 'c'),
+				'hasShift' => $this->params->get('contact_hasShift', 0),
+				'hasAlt' => $this->params->get('contact_hasAlt', 1),
+				'hasControl' => $this->params->get('contact_hasControl', 1),
+				'selector' => 'joomla-editor-option~contact_modal'
+			),
+			'fields' => array(
+				'keyEvent' => $this->params->get('fields_keyEvent', 'f'),
+				'hasShift' => $this->params->get('fields_hasShift', 0),
+				'hasAlt' => $this->params->get('fields_hasAlt', 1),
+				'hasControl' => $this->params->get('fields_hasControl', 1),
+				'selector' => 'joomla-editor-option~fields_modal'
+			),
+			'image' => array(
+				'keyEvent' => $this->params->get('image_keyEvent', 'i'),
+				'hasShift' => $this->params->get('image_hasShift', 0),
+				'hasAlt' => $this->params->get('image_hasAlt', 1),
+				'hasControl' => $this->params->get('image_hasControl', 1),
+				'selector' => 'joomla-editor-option~image_modal'
+			),
+			'menu' => array(
+				'keyEvent' => $this->params->get('menu_keyEvent', 'm'),
+				'hasShift' => $this->params->get('menu_hasShift', 0),
+				'hasAlt' => $this->params->get('menu_hasAlt', 1),
+				'hasControl' => $this->params->get('menu_hasControl', 1),
+				'selector' => 'joomla-editor-option~menu_modal'
+			),
+			'module' => array(
+				'keyEvent' => $this->params->get('module_keyEvent', 'm'),
+				'hasShift' => $this->params->get('module_hasShift', 1),
+				'hasAlt' => $this->params->get('module_hasAlt', 1),
+				'hasControl' => $this->params->get('module_hasControl', 1),
+				'selector' => 'joomla-editor-option~module_modal'
+			),
+			'pagebreak' => array(
+				'keyEvent' => $this->params->get('pagebreak_keyEvent', 'p'),
+				'hasShift' => $this->params->get('pagebreak_hasShift', 0),
+				'hasAlt' => $this->params->get('pagebreak_hasAlt', 1),
+				'hasControl' => $this->params->get('pagebreak_hasControl', 1),
+				'selector' => 'joomla-editor-option~pagebreak_modal'
+			),
+			'readmore' => array(
+				'keyEvent' => $this->params->get('readmore_keyEvent', 'r'),
+				'hasShift' => $this->params->get('readmore_hasShift', 0),
+				'hasAlt' => $this->params->get('readmore_hasAlt', 1),
+				'hasControl' => $this->params->get('readmore_hasControl', 1),
+				'selector' => 'joomla-editor-option~read_more'
+			)
+		);
 	}
 }
