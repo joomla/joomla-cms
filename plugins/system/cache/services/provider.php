2eec592011e057a559632c11a7c6b829c741b5f7
Allon Moritz <allon.moritz@digital-peak.com>
2022-05-09 11:28:03 +0200

    Update plugins/system/cache/services/provider.php

--- plugins/system/cache/services/provider.php
+++ plugins/system/cache/services/provider.php
@@ -3,7 +3,7 @@
  * @package         Joomla.Plugin
  * @subpackage      System.cache
  *
- * @copyright   (C) 2021 Open Source Matters, Inc. <https://www.joomla.org>
+ * @copyright   (C) 2022 Open Source Matters, Inc. <https://www.joomla.org>
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
