61cfe95063a37b267200c69a0ebe0387ca81ab91
Nicholas K. Dionysopoulos <nicholas@akeeba.com>
2022-05-14 12:06:26 +0300

    Captive TFA
    Prepare SQL for new plugins

--- administrator/components/com_admin/sql/updates/mysql/4.2.0-2022-05-15.sql
+++ administrator/components/com_admin/sql/updates/mysql/4.2.0-2022-05-15.sql
@@ -1,3 +1,6 @@
+--
+-- Create the new table for captive TFA
+--
 CREATE TABLE IF NOT EXISTS `#__user_tfa` (
   `id`         int unsigned NOT NULL AUTO_INCREMENT,
   `user_id`    int unsigned NOT NULL,
@@ -9,3 +12,17 @@ CREATE TABLE IF NOT EXISTS `#__user_tfa` (
   `last_used`  DATETIME        NULL,
   INDEX `#__loginguard_tfa_user` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 DEFAULT COLLATE=utf8mb4_unicode_ci COMMENT='Two Factor Authentication settings';
+
+--
+-- Remove obsolete postinstallation message
+--
+DELETE FROM `#__postinstall_messages` WHERE `condition_file` = 'site://plugins/twofactorauth/totp/postinstall/actions.php';
+
+--
+-- Add new captive TFA plugins
+--
+INSERT INTO `#__extensions` (`package_id`, `name`, `type`, `element`, `folder`, `client_id`, `enabled`, `access`, `protected`, `locked`, `manifest_cache`, `params`, `custom_data`, `ordering`, `state`) VALUES
+(0, 'plg_system_tfa', 'plugin', 'tfa', 'system', 0, 1, 1, 0, 1, '', '', '', 24, 0),
+(0, 'plg_twofactorauth_fixed', 'plugin', 'fixed', 'twofactorauth', 0, 0, 1, 0, 1, '', '', '', 5, 0),
+(0, 'plg_twofactorauth_webauthn', 'plugin', 'webauthn', 'twofactorauth', 0, 1, 1, 0, 1, '', '', '', 3, 0),
+(0, 'plg_twofactorauth_email', 'plugin', 'email', 'twofactorauth', 0, 0, 1, 0, 1, '', '', '', 4, 0);
--- administrator/components/com_admin/sql/updates/postgresql/4.2.0-2022-05-15.sql
+++ administrator/components/com_admin/sql/updates/postgresql/4.2.0-2022-05-15.sql
@@ -1,3 +1,6 @@
+--
+-- Create the new table for captive TFA
+--
 CREATE TABLE IF NOT EXISTS "#__user_tfa" (
   "id" serial NOT NULL,
   "user_id" bigint DEFAULT 0 NOT NULL,
@@ -13,3 +16,17 @@ CREATE TABLE IF NOT EXISTS "#__user_tfa" (
 CREATE INDEX "#__user_tfa_idx_user_id" ON "#__user_tfa" ("user_id");
 
 COMMENT ON TABLE "#__user_tfa" IS 'Two Factor Authentication settings';
+
+--
+-- Remove obsolete postinstallation message
+--
+DELETE FROM "#__postinstall_messages" WHERE "condition_file" = 'site://plugins/twofactorauth/totp/postinstall/actions.php';
+
+--
+-- Add new captive TFA plugins
+--
+INSERT INTO "#__extensions" ("package_id", "name", "type", "element", "folder", "client_id", "enabled", "access", "protected", "locked", "manifest_cache", "params", "custom_data", "ordering", "state") VALUES
+(0, 'plg_system_tfa', 'plugin', 'tfa', 'system', 0, 1, 1, 0, 1, '', '', '', 24, 0),
+(0, 'plg_twofactorauth_fixed', 'plugin', 'fixed', 'twofactorauth', 0, 0, 1, 0, 1, '', '', '', 5, 0),
+(0, 'plg_twofactorauth_webauthn', 'plugin', 'webauthn', 'twofactorauth', 0, 1, 1, 0, 1, '', '', '', 3, 0),
+(0, 'plg_twofactorauth_email', 'plugin', 'email', 'twofactorauth', 0, 0, 1, 0, 1, '', '', '', 4, 0);
