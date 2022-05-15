4f54dcc10eed401af18b6473b52635ecff75ffdb
Allon Moritz <allon.moritz@digital-peak.com>
2022-05-15 19:27:01 +0200

    Move to root

--- plugins/actionlog/joomla/src/Joomla.php
+++ plugins/actionlog/joomla/src/Joomla.php
@@ -7,7 +7,7 @@
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
-namespace Joomla\Plugin\Actionlog\Joomla\Extension;
+namespace Joomla\Plugin\Actionlog\Joomla;
 
 defined('_JEXEC') or die;
 
