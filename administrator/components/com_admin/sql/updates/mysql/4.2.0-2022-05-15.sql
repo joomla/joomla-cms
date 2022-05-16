b27cb8fa7ea16a6df55af1f172423fc5e54e69fd
Nicholas K. Dionysopoulos <nicholas@akeeba.com>
2022-05-16 19:43:19 +0300

    Captive TFA
    Post-installation messages

--- administrator/components/com_admin/sql/updates/mysql/4.2.0-2022-05-15.sql
+++ administrator/components/com_admin/sql/updates/mysql/4.2.0-2022-05-15.sql
@@ -23,8 +23,14 @@ DELETE FROM `#__postinstall_messages` WHERE `condition_file` = 'site://plugins/t
 --
 INSERT INTO `#__extensions` (`package_id`, `name`, `type`, `element`, `folder`, `client_id`, `enabled`, `access`, `protected`, `locked`, `manifest_cache`, `params`, `custom_data`, `ordering`, `state`) VALUES
 (0, 'plg_twofactorauth_fixed', 'plugin', 'fixed', 'twofactorauth', 0, 0, 1, 0, 1, '', '', '', 5, 0),
-(0, 'plg_twofactorauth_webauthn', 'plugin', 'webauthn', 'twofactorauth', 0, 1, 1, 0, 1, '', '', '', 3, 0),
-(0, 'plg_twofactorauth_email', 'plugin', 'email', 'twofactorauth', 0, 1, 1, 0, 1, '', '', '', 4, 0);
+(0, 'plg_twofactorauth_webauthn', 'plugin', 'webauthn', 'twofactorauth', 0, 0, 1, 0, 1, '', '', '', 3, 0),
+(0, 'plg_twofactorauth_email', 'plugin', 'email', 'twofactorauth', 0, 0, 1, 0, 1, '', '', '', 4, 0);
+
+--
+-- Add post-installation message
+--
+INSERT IGNORE INTO `#__postinstall_messages` (`extension_id`, `title_key`, `description_key`, `action_key`, `language_extension`, `language_client_id`, `type`, `action_file`, `action`, `condition_file`, `condition_method`, `version_introduced`, `enabled`)
+SELECT `extension_id`, 'COM_USERS_POSTINSTALL_TWOFACTORAUTH_TITLE', 'COM_USERS_POSTINSTALL_TWOFACTORAUTH_BODY', 'COM_USERS_POSTINSTALL_TWOFACTORAUTH_ACTION', 'com_users', 1, 'action', 'admin://components/com_users/postinstall/twofactorauth.php', 'com_users_postinstall_action', 'admin://components/com_users/postinstall/twofactorauth.php', 'com_users_postinstall_condition', '4.2.0', 1 FROM `#__extensions` WHERE `name` = 'files_joomla';
 
 --
 -- Create a mail template for plg_twofactorauth_email
---
+++ administrator/components/com_users/postinstall/twofactorauth.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_users
+ *
+ * @copyright   (C) 2022 Open Source Matters, Inc. <https://www.joomla.org>
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Factory;
+use Joomla\CMS\Plugin\PluginHelper;
+use Joomla\Database\DatabaseDriver;
+use Joomla\Database\ParameterType;
+
+/**
+ * Post-installation message about the new Two Factor Authentication: condition check.
+ *
+ * Returns true if neither of the two new core TFA plugins are enabled.
+ *
+ * @return  boolean
+ * @since   __DEPLOY_VERSION__
+ */
+// phpcs:ignore
+function com_users_postinstall_condition(): bool
+{
+	return !PluginHelper::isEnabled('twofactorauth', 'webauthn')
+		&& !PluginHelper::isEnabled('twofactorauth', 'email');
+}
+
+/**
+ * Post-installation message about the new Two Factor Authentication: action.
+ *
+ * Enables the core TFA plugins.
+ *
+ * @return  void
+ * @since   __DEPLOY_VERSION__
+ */
+// phpcs:ignore
+function com_users_postinstall_action(): void
+{
+	/** @var DatabaseDriver $db */
+	$db             = Factory::getContainer()->get('DatabaseDriver');
+	$coreTfaPlugins = ['email', 'totp', 'webauthn', 'yubikey'];
+
+	$query = $db->getQuery(true)
+		->update($db->quoteName('#__extensions'))
+		->set($db->quoteName('enabled') . ' = 1')
+		->where($db->quoteName('type') . ' = ' . $db->quote('plugin'))
+		->where($db->quoteName('folder') . ' = ' . $db->quote('twofactorauth'))
+		->whereIn($db->quoteName('element'), $coreTfaPlugins, ParameterType::STRING);
+	$db->setQuery($query);
+	$db->execute();
+
+	$query = $db->getQuery(true)
+		->select('extension_id')
+		->from($db->quoteName('#__extensions'))
+		->where($db->quoteName('type') . ' = ' . $db->quote('plugin'))
+		->where($db->quoteName('folder') . ' = ' . $db->quote('system'))
+		->where($db->quoteName('element') . ' = ' . $db->quote('httpheaders'));
+	$db->setQuery($query);
+	$extensionId = $db->loadResult();
+
+	$url = 'index.php?option=com_plugins&task=plugin.edit&extension_id=' . $extensionId;
+	Factory::getApplication()->redirect($url);
+}
