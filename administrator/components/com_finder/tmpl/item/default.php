3022a9797981aad492ad33e10281328d3ae6406a
Hannes Papenberg <info@joomlager.de>
2022-05-17 10:41:10 +0200

    Fixing HTML of item debug output to be more accessible

--- administrator/components/com_finder/tmpl/item/default.php
+++ administrator/components/com_finder/tmpl/item/default.php
@@ -13,75 +13,81 @@ use Joomla\CMS\Language\Text;
 ?>
 <div role="main">
 	<h1 class="mb-3"><?php echo $this->item->title; ?></h1>
-	<fieldset>
-		<caption><?php echo Text::_('COM_FINDER_ITEM_FIELDSET_ITEM_TITLE'); ?></caption>
-		<dl class="row">
-			<?php foreach ($this->item as $key => $value) : ?>
-			<dt class="col-sm-3"><?php echo $key; ?></dt>
-			<dd class="col-sm-9"><?php echo $value; ?></dd>
-			<?php endforeach; ?>
-		</dl>
-	</fieldset>
-	<fieldset>
-		<caption><?php echo Text::_('COM_FINDER_ITEM_FIELDSET_TERMS_TITLE'); ?></caption>
-		<table class="table">
-			<thead>
-			<tr>
-				<th scope="col">id</th>
-				<th scope="col">term</th>
-				<th scope="col">stem</th>
-				<th scope="col">common</th>
-				<th scope="col">phrase</th>
-				<th scope="col">weight</th>
-				<th scope="col">links</th>
-				<th scope="col">language</th>
-			</tr>
-			</thead>
-			<tbody>
-			<?php foreach ($this->terms as $term) : ?>
-				<tr>
-					<td><?php echo $term->term_id; ?></td>
-					<td><?php echo $term->term; ?></td>
-					<td><?php echo $term->stem; ?></td>
-					<td><?php echo $term->common; ?></td>
-					<td><?php echo $term->phrase; ?></td>
-					<td><?php echo $term->weight; ?></td>
-					<td><?php echo $term->links; ?></td>
-					<td><?php echo $term->language; ?></td>
-				</tr>
-			<?php endforeach; ?>
-			</tbody>
-		</table>
-	</fieldset>
-	<fieldset>
-		<caption><?php echo Text::_('COM_FINDER_ITEM_FIELDSET_TAXONOMIES_TITLE'); ?></caption>
-		<table class="table">
-			<thead>
+	<div class="card mb-3">
+		<div class="card-header"><h2><?php echo Text::_('COM_FINDER_ITEM_FIELDSET_ITEM_TITLE'); ?></h2></div>
+		<div class="card-body">
+			<dl class="row">
+				<?php foreach ($this->item as $key => $value) : ?>
+				<dt class="col-sm-3"><?php echo $key; ?></dt>
+				<dd class="col-sm-9"><?php echo $value; ?></dd>
+				<?php endforeach; ?>
+			</dl>
+		</div>
+	</div>
+	<div class="card mb-3">
+		<div class="card-header"><h2><?php echo Text::_('COM_FINDER_ITEM_FIELDSET_TERMS_TITLE'); ?></h2></div>
+		<div class="card-body">
+			<table class="table">
+				<thead>
 				<tr>
 					<th scope="col">id</th>
-					<th scope="col">title</th>
-					<th scope="col">alias</th>
-					<th scope="col">lft</th>
-					<th scope="col">path</th>
-					<th scope="col">state</th>
-					<th scope="col">access</th>
+					<th scope="col">term</th>
+					<th scope="col">stem</th>
+					<th scope="col">common</th>
+					<th scope="col">phrase</th>
+					<th scope="col">weight</th>
+					<th scope="col">links</th>
 					<th scope="col">language</th>
 				</tr>
-			</thead>
-			<tbody>
-				<?php foreach ($this->taxonomies as $taxonomy) : ?>
+				</thead>
+				<tbody>
+				<?php foreach ($this->terms as $term) : ?>
 					<tr>
-						<td><?php echo $taxonomy->id; ?></td>
-						<td><?php echo $taxonomy->title; ?></td>
-						<td><?php echo $taxonomy->alias; ?></td>
-						<td><?php echo $taxonomy->lft; ?></td>
-						<td><?php echo $taxonomy->path; ?></td>
-						<td><?php echo $taxonomy->state; ?></td>
-						<td><?php echo $taxonomy->access; ?></td>
-						<td><?php echo $taxonomy->language; ?></td>
+						<th scope="row"><?php echo $term->term_id; ?></th>
+						<td><?php echo $term->term; ?></td>
+						<td><?php echo $term->stem; ?></td>
+						<td><?php echo $term->common; ?></td>
+						<td><?php echo $term->phrase; ?></td>
+						<td><?php echo $term->weight; ?></td>
+						<td><?php echo $term->links; ?></td>
+						<td><?php echo $term->language; ?></td>
 					</tr>
 				<?php endforeach; ?>
-			</tbody>
-		</table>
-	</fieldset>
+				</tbody>
+			</table>
+		</div>
+	</div>
+	<div class="card mb-3">
+		<div class="card-header"><h2><?php echo Text::_('COM_FINDER_ITEM_FIELDSET_TAXONOMIES_TITLE'); ?></h2></div>
+		<div class="card-body">
+			<table class="table">
+				<thead>
+					<tr>
+						<th scope="col">id</th>
+						<th scope="col">title</th>
+						<th scope="col">alias</th>
+						<th scope="col">lft</th>
+						<th scope="col">path</th>
+						<th scope="col">state</th>
+						<th scope="col">access</th>
+						<th scope="col">language</th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php foreach ($this->taxonomies as $taxonomy) : ?>
+						<tr>
+							<th scope="row"><?php echo $taxonomy->id; ?></th>
+							<td><?php echo $taxonomy->title; ?></td>
+							<td><?php echo $taxonomy->alias; ?></td>
+							<td><?php echo $taxonomy->lft; ?></td>
+							<td><?php echo $taxonomy->path; ?></td>
+							<td><?php echo $taxonomy->state; ?></td>
+							<td><?php echo $taxonomy->access; ?></td>
+							<td><?php echo $taxonomy->language; ?></td>
+						</tr>
+					<?php endforeach; ?>
+				</tbody>
+			</table>
+		</div>
+	</div>
 </div>
