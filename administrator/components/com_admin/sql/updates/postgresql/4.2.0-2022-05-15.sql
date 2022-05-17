3f4b0d0015cae9a261973f52712ede0dba435a0d
Richard Fath <richard67@users.noreply.github.com>
2022-05-17 11:05:52 +0200

    SQL code style and consistency fixes

--- administrator/components/com_admin/sql/updates/postgresql/4.2.0-2022-05-15.sql
+++ administrator/components/com_admin/sql/updates/postgresql/4.2.0-2022-05-15.sql
@@ -3,13 +3,13 @@
 --
 CREATE TABLE IF NOT EXISTS "#__user_tfa" (
   "id" serial NOT NULL,
-  "user_id" bigint DEFAULT 0 NOT NULL,
+  "user_id" bigint NOT NULL,
   "title" varchar(255) DEFAULT '' NOT NULL,
   "method" varchar(100) DEFAULT '' NOT NULL,
-  "default" smallint DEFAULT 0,
+  "default" smallint DEFAULT 0 NOT NULL,
   "options" text NOT NULL,
   "created_on" timestamp without time zone NOT NULL,
-  "last_used" timestamp without time zone NOT NULL,
+  "last_used" timestamp without time zone,
   PRIMARY KEY ("id")
 );
 
