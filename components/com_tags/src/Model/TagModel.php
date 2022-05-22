a98385a804a34c3696941a38bf953214869fcdfb
conseilgouz <pascal.leconte@conseilgouz.com>
2022-05-22 18:21:56 +0200

    Links from numeric tags cause error 404
    same fix than #37814

--- components/com_tags/src/Model/TagModel.php
+++ components/com_tags/src/Model/TagModel.php
@@ -186,7 +186,7 @@ class TagModel extends ListModel
 		$this->setState('params', $params);
 
 		// Load state from the request.
-		$ids = (array) $app->input->get('id', array());
+		$ids = (array) $app->input->get('id', array(),'int');
 
 		if (count($ids) == 1)
 		{
