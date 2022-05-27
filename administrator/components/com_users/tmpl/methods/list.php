f3eb6d680f2d7f390bbbc7721656ed04662a43b7
Nicholas K. Dionysopoulos <nicholas@akeeba.com>
2022-05-27 21:35:39 +0300

    Backup codes alert heading font size fix in backend

--- administrator/components/com_users/tmpl/methods/list.php
+++ administrator/components/com_users/tmpl/methods/list.php
@@ -67,7 +67,7 @@ $this->document->getWebAssetManager()->useScript('com_users.two-factor-list');
 								<div class="com-users-methods-list-method-record-info flex-grow-1 d-flex flex-column align-items-start gap-1">
 									<?php if ($methodName === 'backupcodes'): ?>
 										<div class="alert alert-info mt-1 w-100">
-											<h4 class="alert-heading fs-3">
+											<h4 class="alert-heading fs-4">
 												<span class="icon icon-info-circle icon-info-sign" aria-hidden="true"></span>
 												<?php echo Text::sprintf('COM_USERS_MFA_BACKUPCODES_PRINT_PROMPT_HEAD', Route::_('index.php?option=com_users&task=method.edit&id=' . (int) $record->id . ($this->returnURL ? '&returnurl=' . $this->escape(urlencode($this->returnURL)) : '') . '&user_id=' . $this->user->id)) ?>
 											</h4>
