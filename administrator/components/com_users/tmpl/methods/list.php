123e925862b7e0b762c0ede0a2823d552165cfe3
Nicholas K. Dionysopoulos <nicholas@akeeba.com>
2022-05-27 21:34:48 +0300

    Backup codes alert is narrower than page on super-wide screens

--- administrator/components/com_users/tmpl/methods/list.php
+++ administrator/components/com_users/tmpl/methods/list.php
@@ -66,7 +66,7 @@ $this->document->getWebAssetManager()->useScript('com_users.two-factor-list');
 							<div class="com-users-methods-list-method-record d-flex flex-row flex-wrap justify-content-start">
 								<div class="com-users-methods-list-method-record-info flex-grow-1 d-flex flex-column align-items-start gap-1">
 									<?php if ($methodName === 'backupcodes'): ?>
-										<div class="alert alert-info mt-1">
+										<div class="alert alert-info mt-1 w-100">
 											<h4 class="alert-heading fs-3">
 												<span class="icon icon-info-circle icon-info-sign" aria-hidden="true"></span>
 												<?php echo Text::sprintf('COM_USERS_MFA_BACKUPCODES_PRINT_PROMPT_HEAD', Route::_('index.php?option=com_users&task=method.edit&id=' . (int) $record->id . ($this->returnURL ? '&returnurl=' . $this->escape(urlencode($this->returnURL)) : '') . '&user_id=' . $this->user->id)) ?>
