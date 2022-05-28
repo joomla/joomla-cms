d7239dcde8dda0fcb5ec9e6a169bbb3deb80edc6
Robert Deutz <rdeutz@googlemail.com>
2022-05-28 15:32:40 +0200

    wip

--- plugins/task/checkin/services/provider.php
+++ plugins/task/checkin/services/provider.php
@@ -11,6 +11,7 @@ defined('_JEXEC') or die;
 
 use Joomla\CMS\Extension\PluginInterface;
 use Joomla\CMS\Plugin\PluginHelper;
+use Joomla\Database\DatabaseInterface;
 use Joomla\DI\Container;
 use Joomla\DI\ServiceProviderInterface;
 use Joomla\Event\DispatcherInterface;
@@ -35,7 +36,7 @@ return new class implements ServiceProviderInterface
 				$plugin     = PluginHelper::getPlugin('task', 'checkin');
 				$dispatcher = $container->get(DispatcherInterface::class);
 
-				$checkin = new Checkin(
+				$checkin = new Checkin (
 					$dispatcher,
 					(array) $plugin,
 				);
