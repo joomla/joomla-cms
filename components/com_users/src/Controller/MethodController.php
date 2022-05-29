ffc43c0e6759d64f304d6870578f19b14860807a
Nicholas K. Dionysopoulos <nicholas@akeeba.com>
2022-05-29 12:41:22 +0300

    Redirect users to login page in the frontend
    When they try to access a captive or methods / method page.

--- components/com_users/src/Controller/MethodController.php
+++ components/com_users/src/Controller/MethodController.php
@@ -9,6 +9,7 @@
 
 namespace Joomla\Component\Users\Site\Controller;
 
+use Joomla\CMS\Router\Route;
 use Joomla\Component\Users\Administrator\Controller\MethodController as AdminMethodController;
 
 /**
@@ -18,5 +19,37 @@ use Joomla\Component\Users\Administrator\Controller\MethodController as AdminMet
  */
 class MethodController extends AdminMethodController
 {
+	/**
+	 * Execute a task by triggering a Method in the derived class.
+	 *
+	 * @param   string  $task    The task to perform.
+	 *
+	 * @return  mixed   The value returned by the called Method.
+	 *
+	 * @throws  \Exception
+	 * @since   __DEPLOY_VERSION__
+	 */
+	public function execute($task)
+	{
+		try
+		{
+			return parent::execute($task);
+		}
+		catch (\Exception $e)
+		{
+			if ($e->getCode() !== 403)
+			{
+				throw $e;
+			}
 
+			if ($this->app->getIdentity()->guest)
+			{
+				$this->setRedirect(Route::_('index.php?option=com_users&view=login', false));
+
+				return null;
+			}
+		}
+
+		return null;
+	}
 }
