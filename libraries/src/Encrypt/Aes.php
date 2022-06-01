af0d17c7ef56b513da410746790cd7061888abcc
Allon Moritz <allon.moritz@digital-peak.com>
2022-06-01 10:27:34 +0200

    Revert "added deprecation note for 5.0 signature change (#37943)"
    This reverts commit e98815ce93f4329c168b81cb6600189c69c0265b.

--- libraries/src/Encrypt/Aes.php
+++ libraries/src/Encrypt/Aes.php
@@ -47,8 +47,6 @@ class Aes
 	 * @param   int             $strength Bit strength (128, 192 or 256) – ALWAYS USE 128 BITS. THIS PARAMETER IS DEPRECATED.
 	 * @param   string          $mode     Encryption mode. Can be ebc or cbc. We recommend using cbc.
 	 * @param   string          $priority Priority which adapter we should try first
-	 *
-	 * @deprecated 5.0 $strength will be removed
 	 */
 	public function __construct($key, $strength = 128, $mode = 'cbc', $priority = 'openssl')
 	{
