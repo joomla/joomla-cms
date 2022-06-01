de905402304d78193fc9e79ba40b9dca1493154c
conseilgouz <pascal.leconte@conseilgouz.com>
2022-06-01 10:00:42 +0200

    Update inlinehelp.es6.js

--- build/media_source/system/js/inlinehelp.es6.js
+++ build/media_source/system/js/inlinehelp.es6.js
@@ -42,12 +42,13 @@ Joomla.toggleInlineHelp = (toggleClass) => {
     // The class of the DIVs to toggle visibility on is defined by the data-class attribute of the click target.
     const toggleClass = elToggler.dataset.class ?? 'hide-aware-inline-help';
     const collection = document.getElementsByClassName(toggleClass);
-  
-    if (collection.length == 0) { // no description => hide inlinehelp button
+
+     // no description => hide inlinehelp button
+    if (collection.length === 0) {
       elToggler.classList.add('d-none');
       return;
     }
- 
+
     // Add the click handler.
     elToggler.addEventListener('click', (event) => {
       event.preventDefault();
