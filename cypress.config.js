7b5295dd4a2a4165204e0d40779f2f179e4d5a76
Robert Deutz <rdeutz@googlemail.com>
2022-06-05 23:03:32 +0200

    prepare for drone

--- cypress.config.js
+++ cypress.config.js
@@ -6,9 +6,22 @@ module.exports = defineConfig({
   screenshotsFolder: 'tests/cypress/screenshots',
   e2e: {
     setupNodeEvents(on, config) {},
-    baseUrl: 'http://localhost',
+    baseUrl: 'http://cypress.test',
     specPattern: 'tests/cypress/integration/**/*.cy.{js,jsx,ts,tsx}',
     supportFile: 'tests/cypress/support/index.js',
     scrollBehavior: 'center'
+  },
+  env: {
+    sitename: 'Joomla CMS Test',
+    username: 'jane doe',
+    email: 'admin@example.com',
+    user: 'ci-admin',
+    pass: 'joomla-17082005',
+    db_type: 'MySQLi',
+    db_host: 'localhost',
+    db_name: 'test_joomla',
+    db_user: 'root',
+    db_pass: '',
+    db_prefix: 'jos_',
   },
 })
