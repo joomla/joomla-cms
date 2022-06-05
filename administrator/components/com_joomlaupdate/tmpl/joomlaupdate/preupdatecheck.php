2acec13940b1e1c5f495ed2423c7f4e0fccfac23
Nicholas K. Dionysopoulos <nicholas@akeeba.com>
2022-06-05 10:59:03 +0300

    Pre-update check was not pushing JS library lang strings

--- administrator/components/com_joomlaupdate/tmpl/joomlaupdate/preupdatecheck.php
+++ administrator/components/com_joomlaupdate/tmpl/joomlaupdate/preupdatecheck.php
@@ -29,6 +29,7 @@ $wa->useScript('core')
 // Text::script doesn't have a sprintf equivalent so work around this
 $this->document->addScriptOptions('nonCoreCriticalPlugins', $this->nonCoreCriticalPlugins);
 
+// Push Joomla! Update client-side error messages
 Text::script('COM_JOOMLAUPDATE_VIEW_DEFAULT_POTENTIALLY_DANGEROUS_PLUGIN_CONFIRM_MESSAGE');
 Text::script('COM_JOOMLAUPDATE_VIEW_DEFAULT_EXTENSION_NO_COMPATIBILITY_INFORMATION');
 Text::script('COM_JOOMLAUPDATE_VIEW_DEFAULT_EXTENSION_WARNING_UNKNOWN');
@@ -40,6 +41,13 @@ Text::script('COM_JOOMLAUPDATE_VIEW_DEFAULT_POTENTIALLY_DANGEROUS_PLUGIN_DESC');
 Text::script('COM_JOOMLAUPDATE_VIEW_DEFAULT_POTENTIALLY_DANGEROUS_PLUGIN_LIST');
 Text::script('COM_JOOMLAUPDATE_VIEW_DEFAULT_POTENTIALLY_DANGEROUS_PLUGIN_CONFIRM_MESSAGE');
 Text::script('COM_JOOMLAUPDATE_VIEW_DEFAULT_HELP');
+
+// Push Joomla! core Joomla.Request error messages
+Text::script('JLIB_JS_AJAX_ERROR_CONNECTION_ABORT');
+Text::script('JLIB_JS_AJAX_ERROR_NO_CONTENT');
+Text::script('JLIB_JS_AJAX_ERROR_OTHER');
+Text::script('JLIB_JS_AJAX_ERROR_PARSE');
+Text::script('JLIB_JS_AJAX_ERROR_TIMEOUT');
 
 $compatibilityTypes = array(
 	'COM_JOOMLAUPDATE_VIEW_DEFAULT_EXTENSIONS_RUNNING_PRE_UPDATE_CHECKS' => array(
