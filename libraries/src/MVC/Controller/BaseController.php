79fe599aeb39c6d270ab6e473cd821bf4e9aae98
Tuan Pham Ngoc <github@joomdonation.com>
2022-06-09 23:32:34 +0700

    Remove unncessary check

--- libraries/src/MVC/Controller/BaseController.php
+++ libraries/src/MVC/Controller/BaseController.php
@@ -595,7 +595,7 @@ class BaseController implements ControllerInterface, DispatcherAwareInterface
 			return false;
 		}
 
-		if ($model instanceof CurrentUserInterface && $this->app->getIdentity())
+		if ($model instanceof CurrentUserInterface)
 		{
 			$model->setCurrentUser($this->app->getIdentity());
 		}
@@ -627,7 +627,7 @@ class BaseController implements ControllerInterface, DispatcherAwareInterface
 
 		$view = $this->factory->createView($name, $prefix, $type, $config);
 
-		if ($view instanceof CurrentUserInterface && $this->app->getIdentity())
+		if ($view instanceof CurrentUserInterface)
 		{
 			$view->setCurrentUser($this->app->getIdentity());
 		}
