b06ed4963ba664acccda1d23d43d617051c3a916
Franciska Perisa <9084265+fancyFranci@users.noreply.github.com>
2022-06-10 16:31:55 +0200

    What zero-24 says
    Co-authored-by: Tobias Zulauf <zero-24@users.noreply.github.com>

--- administrator/components/com_admin/sql/updates/mysql/4.2.0-2022-03-20.sql
+++ administrator/components/com_admin/sql/updates/mysql/4.2.0-2022-03-20.sql
@@ -15,4 +15,4 @@ CREATE TABLE IF NOT EXISTS `#__tuf_metadata` (
 
 -- --------------------------------------------------------
 INSERT INTO `#__tuf_metadata` (`extension_id`, `root_json`)
-SELECT `extension_id`, '{"keytype": "ed25519", "scheme": "ed25519", "keyid": "02c3130c26fb3fe13fda279d578f3bc251f2ca3a42e5878de063e0ee345533c9", "keyid_hash_algorithms": ["sha256", "sha512"], "keyval": {"public": "f813a2882b305389cac36a9b8ebee7576ba7a7de671d2617074b03c12fb003aa", "private": "b7cb4fab28bae035a6fc5d46736e6f2d10ea4ef943e6aace8c637c1fd141ac72"}}' FROM `tupsb_extensions` WHERE `type`='file' AND `element`='joomla';
+SELECT `extension_id`, '{"keytype": "ed25519", "scheme": "ed25519", "keyid": "02c3130c26fb3fe13fda279d578f3bc251f2ca3a42e5878de063e0ee345533c9", "keyid_hash_algorithms": ["sha256", "sha512"], "keyval": {"public": "f813a2882b305389cac36a9b8ebee7576ba7a7de671d2617074b03c12fb003aa", "private": "b7cb4fab28bae035a6fc5d46736e6f2d10ea4ef943e6aace8c637c1fd141ac72"}}' FROM `#__extensions` WHERE `type`='file' AND `element`='joomla';
