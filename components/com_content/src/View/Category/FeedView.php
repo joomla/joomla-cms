dc0b1516587b92dc692f98e4ef50decf7f8ee0ef
AndySDH <andybadwool@hotmail.it>
2022-06-14 19:29:15 +0200

    Update FeedView.php

--- components/com_content/src/View/Category/FeedView.php
+++ components/com_content/src/View/Category/FeedView.php
@@ -51,11 +51,11 @@ class FeedView extends CategoryFeedView
 		$obj = json_decode($item->images);
 
 		// Set feed image to image_intro or if that's empty, to image_fulltext
-		$itemImage = !empty($obj->{'image_intro'}) ?
-			     $obj->{'image_intro'} :
-			     ( !empty($obj->{'image_fulltext'}) ?
-			     $obj->{'image_fulltext'} :
-			     '');
+		$itemImage = 	!empty($obj->{'image_intro'}) ?
+				$obj->{'image_intro'} :
+				( !empty($obj->{'image_fulltext'}) ?
+				$obj->{'image_fulltext'} :
+				'');
 
 		if (!empty($itemImage))
 		{
