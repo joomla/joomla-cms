34e2f4d28231c7552a435f1dfd662b159f616930
tatankat <tatankat@users.noreply.github.com>
2022-06-16 16:10:38 +0200

    More precise WHERE clause and a bit code style.
    Co-authored-by: Richard Fath <richard67@users.noreply.github.com>

--- administrator/components/com_admin/sql/updates/mysql/4.1.5-2022-06-02.sql
+++ administrator/components/com_admin/sql/updates/mysql/4.1.5-2022-06-02.sql
@@ -1,11 +1,18 @@
 UPDATE `#__extensions`
-   SET `params` = REPLACE(`params`,'"negotiate_tls":1','"encryption":"tls"')
- WHERE `client_id` = 0
-   AND `name` = 'plg_authentication_ldap'
+   SET `params` = REPLACE(`params`, '"negotiate_tls":1', '"encryption":"tls"')
+ WHERE `name` = 'plg_authentication_ldap'
+   AND `type` = 'plugin'
+   AND `element` = 'plugin'
+   AND `folder` = 'authentication'
+   AND `client_id` = 0
    AND `params` LIKE '{%"negotiate_tls":1%}';
 
 UPDATE `#__extensions`
-   SET `params` = REPLACE(`params`,'"negotiate_tls":0','"encryption":"none"')
- WHERE `client_id` = 0
-   AND `name` = 'plg_authentication_ldap'
+   SET `params` = REPLACE(`params`, '"negotiate_tls":0', '"encryption":"none"')
+ WHERE `name` = 'plg_authentication_ldap'
+   AND `type` = 'plugin'
+   AND `element` = 'plugin'
+   AND `folder` = 'authentication'
+   AND `client_id` = 0
    AND `params` LIKE '{%"negotiate_tls":0%}';
+
