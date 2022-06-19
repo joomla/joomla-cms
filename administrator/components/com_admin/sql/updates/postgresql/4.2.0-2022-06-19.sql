0942d582b6cbaf4c8e01f6140e3781998b30d552
Franciska Perisa <franciska.perisa@bcmsolutions.de>
2022-06-19 11:42:50 +0200

    Update update.sql

--- administrator/components/com_admin/sql/updates/postgresql/4.2.0-2022-06-19.sql
+++ administrator/components/com_admin/sql/updates/postgresql/4.2.0-2022-06-19.sql
@@ -1,3 +1,3 @@
 -- See https://github.com/joomla/joomla-cms/pull/38092
 INSERT INTO `#__extensions` (`name`, `type`, `element`, `folder`, `client_id`, `enabled`, `access`, `protected`, `manifest_cache`, `params`, `custom_data`, `checked_out`, `checked_out_time`, `ordering`, `state`) VALUES
-('plg_system_shortcut', 'plugin', 'shortcut', 'system', 0, 1, 1, 0, '', '{}', '', 0, '1970-01-01 00:00:00', 0, 0);
+('plg_system_shortcut', 'plugin', 'shortcut', 'system', 0, 1, 1, 0, '', '{}', '', NULL, NULL, 0, 0);
