c15691aa6997162798c6350ded69c0e48b3f3284
Christian Heel <66922325+heelc29@users.noreply.github.com>
2022-06-19 16:22:35 +0200

    use whereIn

--- administrator/components/com_postinstall/src/Model/MessagesModel.php
+++ administrator/components/com_postinstall/src/Model/MessagesModel.php
@@ -210,7 +210,7 @@ class MessagesModel extends BaseDatabaseModel
 			->bind(':eid', $eid, ParameterType::INTEGER);
 
 		// Force filter only enabled messages
-		$query->where($db->quoteName('enabled') . ' IN (1,2)');
+		$query->whereIn($db->quoteName('enabled'), [1, 2]);
 		$db->setQuery($query);
 
 		try
