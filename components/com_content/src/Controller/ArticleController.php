630a9880f3705a97828f2f74d977355ebd7c34a6
Tuan Pham Ngoc <github@joomdonation.com>
2022-06-20 21:48:59 +0700

    Remove un-used import. Thanks @Quy

--- components/com_content/src/Controller/ArticleController.php
+++ components/com_content/src/Controller/ArticleController.php
@@ -12,7 +12,6 @@ namespace Joomla\Component\Content\Site\Controller;
 \defined('_JEXEC') or die;
 
 use Joomla\CMS\Application\SiteApplication;
-use Joomla\CMS\Factory;
 use Joomla\CMS\Language\Multilanguage;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\MVC\Controller\FormController;
--- components/com_finder/src/Controller/DisplayController.php
+++ components/com_finder/src/Controller/DisplayController.php
@@ -11,7 +11,6 @@ namespace Joomla\Component\Finder\Site\Controller;
 
 \defined('_JEXEC') or die;
 
-use Joomla\CMS\Factory;
 use Joomla\CMS\MVC\Controller\BaseController;
 use Joomla\Component\Finder\Administrator\Helper\LanguageHelper;
 
