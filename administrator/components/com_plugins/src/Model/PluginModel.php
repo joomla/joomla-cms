885b1e30bb8681e69ea3d5a35cf763592d2e9fb7
Сергей <koreshs@mail.ru>
2022-07-01 20:31:44 +0300

    Update PluginModel.php

--- administrator/components/com_plugins/src/Model/PluginModel.php
+++ administrator/components/com_plugins/src/Model/PluginModel.php
@@ -279,9 +279,9 @@ class PluginModel extends AdminModel
 
         // Load the core and/or local language file(s).
             $lang->load('plg_' . $folder . '_' . $element, JPATH_ADMINISTRATOR)
-                ||	$lang->load('plg_' . $folder . '_' . $element, JPATH_PLUGINS . '/' . $folder . '/' . $element)
-                ||	$lang->load('plg_' . $folder . '_' . strtolower($element), JPATH_ADMINISTRATOR)
-                ||	$lang->load('plg_' . $folder . '_' . strtolower($element), JPATH_PLUGINS . '/' . $folder . '/' . $element);
+                || $lang->load('plg_' . $folder . '_' . $element, JPATH_PLUGINS . '/' . $folder . '/' . $element)
+                || $lang->load('plg_' . $folder . '_' . strtolower($element), JPATH_ADMINISTRATOR)
+                || $lang->load('plg_' . $folder . '_' . strtolower($element), JPATH_PLUGINS . '/' . $folder . '/' . $element);
 
         if (file_exists($formFile)) {
             // Get the plugin form.
