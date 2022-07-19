156fe71b43f919c2353cbc3695173826e21b0180
FancyFranci <franci_perisa@hotmail.de>
2022-07-19 18:17:01 +0200

    Reset to dev

--- libraries/src/Version.php
+++ libraries/src/Version.php
@@ -62,7 +62,7 @@ final class Version
      * @var    string
      * @since  3.8.0
      */
-    public const EXTRA_VERSION = 'rc1';
+    public const EXTRA_VERSION = 'dev';
 
     /**
      * Development status.
@@ -70,7 +70,7 @@ final class Version
      * @var    string
      * @since  3.5
      */
-    public const DEV_STATUS = 'Release Candidate';
+    public const DEV_STATUS = 'Development';
 
     /**
      * Code name.
@@ -94,7 +94,7 @@ final class Version
      * @var    string
      * @since  3.5
      */
-    public const RELTIME = '17:00';
+    public const RELTIME = '17:15';
 
     /**
      * Release timezone.
