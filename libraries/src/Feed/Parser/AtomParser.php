e59c29a5c038f5fd1bf1b3c247fcfcb9ab50f29d
pmleconte <pascal.leconte@conseilgouz.com>
2022-07-20 17:35:47 +0200

    [4.2 RC1] Feed display module not working
    Fix #38310

--- libraries/src/Feed/Parser/AtomParser.php
+++ libraries/src/Feed/Parser/AtomParser.php
@@ -190,9 +190,7 @@ class AtomParser extends FeedParser
      */
     protected function initialise()
     {
-        // We want to move forward to the first XML Element after the xml doc type declaration
-        $this->moveToNextElement();
-
+        // We are on the first XML Element after the xml doc type declaration
         $this->version = ($this->stream->getAttribute('version') == '0.3') ? '0.3' : '1.0';
     }
 
