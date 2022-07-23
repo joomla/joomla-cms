4595c7b134ef4c0c903632530453979fd2af3c8a
Denitz <pandenitz@mail.ru>
2022-07-23 09:15:22 +0300

    remove useless isset() and restart drone

--- libraries/src/Session/Storage/JoomlaStorage.php
+++ libraries/src/Session/Storage/JoomlaStorage.php
@@ -291,7 +291,7 @@ class JoomlaStorage extends NativeStorage
         parent::start();
 
         // Try loading data from the session
-        if (isset($_SESSION['joomla']) && !empty($_SESSION['joomla'])) {
+        if (!empty($_SESSION['joomla'])) {
             $this->data = unserialize(base64_decode($_SESSION['joomla']));
         }
     }
