b63f680cb265a6c288437e64135673a8ece826ca
Hannes Papenberg <info@joomlager.de>
2022-07-26 22:06:40 +0200

    Docblock formatting

--- installation/joomla.php
+++ installation/joomla.php
@@ -1,10 +1,10 @@
 <?php
 
 /**
- * @package        Joomla.Installation
+ * @package    Joomla.Installation
  *
  * @copyright  (C) 2022 Open Source Matters, Inc. <https://www.joomla.org>
- * @license        GNU General Public License version 2 or later; see LICENSE.txt
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 /**
--- installation/src/Service/Provider/Application.php
+++ installation/src/Service/Provider/Application.php
@@ -1,11 +1,11 @@
 <?php
 
 /**
- * @package         Joomla.Installation
- * @subpackage      Service
+ * @package     Joomla.Installation
+ * @subpackage  Service
  *
  * @copyright   (C) 2016 Open Source Matters, Inc. <https://www.joomla.org>
- * @license         GNU General Public License version 2 or later; see LICENSE.txt
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 namespace Joomla\CMS\Installation\Service\Provider;
