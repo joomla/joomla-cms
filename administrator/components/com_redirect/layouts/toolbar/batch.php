48147cce661e873f00135a1fe635a38847bc09fd
Brian Teeman <brian@teeman.net>
2022-07-26 15:24:16 +0100

    nanmespace

--- administrator/components/com_redirect/layouts/toolbar/batch.php
+++ administrator/components/com_redirect/layouts/toolbar/batch.php
@@ -10,8 +10,6 @@
 
 defined('_JEXEC') or die;
 
-use Joomla\CMS\HTML\HTMLHelper;
-
 /** @var Joomla\CMS\WebAsset\WebAssetManager $wa */
 $wa = $app->getDocument()->getWebAssetManager();
 $wa->useScript('core');
