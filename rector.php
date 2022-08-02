89f595989cc4fa4ee35072dce5daaffcad9c2b50
Niels Braczek <nbraczek@bsds.de>
2022-08-02 16:46:52 +0200

    Update dependencies (PHP:^8.1)

--- rector.php
+++ rector.php
@@ -10,6 +10,18 @@ return static function(RectorConfig $rectorConfig): void {
         __DIR__,
     ]);
 
+    $rectorConfig->skip([
+        // Directories to skip
+        __DIR__ . '/administrator/cache',
+        __DIR__ . '/administrator/logs',
+        __DIR__ . '/cache',
+        __DIR__ . '/images',
+        __DIR__ . '/installation/cache',
+        __DIR__ . '/libraries/vendor',
+        __DIR__ . '/node_modules',
+        __DIR__ . '/tmp',
+    ]);
+
     $rectorConfig->sets([
         LevelSetList::UP_TO_PHP_81,
     ]);
