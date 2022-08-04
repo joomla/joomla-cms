dcd0238f85a604172cb803fbc336a08ac11ae9b6
khu5h1 <khushirauniyar8@gmail.com>
2022-08-04 22:00:41 +0530

    Fixed Indentation of (C)

--- administrator/components/com_guidedtours/src/Model/TourModel.php
+++ administrator/components/com_guidedtours/src/Model/TourModel.php
@@ -3,7 +3,8 @@
 /**
  * @package       Joomla.Administrator
  * @subpackage    com_guidedtours
- * @copyright (C) 2022 Open Source Matters, Inc. <https://www.joomla.org>
+ * 
+ * @copyright     (C) 2022 Open Source Matters, Inc. <https://www.joomla.org>
  * @license       GNU General Public License version 2 or later; see LICENSE.txt
  */
 
--- administrator/components/com_guidedtours/src/Service/HTML/oldAdministratorService.php
+++ administrator/components/com_guidedtours/src/Service/HTML/oldAdministratorService.php
@@ -7,6 +7,7 @@
  * @category  Component
  * @package   Joomla.Administrator
  * @author    Joomla! <admin@joomla.org>
+ * 
  * @copyright (C) 2022 Open Source Matters, Inc. <https://www.joomla.org>
  * @license   GNU General Public License version 2 or later; see LICENSE.txt
  * @link      admin@joomla.org
