1e41ab946ccf1a9070192ad3c350ef663fa0a19d
Richard Fath <richard67@users.noreply.github.com>
2022-08-13 16:24:20 +0200

    Fix extensions.sql for PostgreSQL
    The soundex function has to be the last thing in this file.

--- installation/sql/postgresql/extensions.sql
+++ installation/sql/postgresql/extensions.sql
@@ -893,70 +893,6 @@ CREATE INDEX "#__scheduler_tasks_idx_checked_out" ON "#__scheduler_tasks" ("chec
 -- --------------------------------------------------------
 
 --
--- Here is SOUNDEX replacement for those who can't enable fuzzystrmatch module
---   from contrib folder.
--- This function comes from https://wiki.postgresql.org/wiki/Soundex
---   and is distributed with GPL license.
--- Thanks to its author, Marti Raudsepp, that published this piece of code.
---
-
-CREATE OR REPLACE FUNCTION soundex(input text) RETURNS text
-IMMUTABLE STRICT COST 500 LANGUAGE plpgsql
-AS $$
-DECLARE
-  soundex text = '';
-  char text;
-  symbol text;
-  last_symbol text = '';
-  pos int = 1;
-BEGIN
-  WHILE length(soundex) < 4 LOOP
-    char = upper(substr(input, pos, 1));
-    pos = pos + 1;
-    CASE char
-    WHEN '' THEN
-      -- End of input string
-      IF soundex = '' THEN
-        RETURN '';
-      ELSE
-        RETURN rpad(soundex, 4, '0');
-      END IF;
-    WHEN 'B', 'F', 'P', 'V' THEN
-      symbol = '1';
-    WHEN 'C', 'G', 'J', 'K', 'Q', 'S', 'X', 'Z' THEN
-      symbol = '2';
-    WHEN 'D', 'T' THEN
-      symbol = '3';
-    WHEN 'L' THEN
-      symbol = '4';
-    WHEN 'M', 'N' THEN
-      symbol = '5';
-    WHEN 'R' THEN
-      symbol = '6';
-    ELSE
-      -- Not a consonant; no output, but next similar consonant will be re-recorded
-      symbol = '';
-    END CASE;
-
-    IF soundex = '' THEN
-      -- First character; only accept strictly English ASCII characters
-      IF char ~>=~ 'A' AND char ~<=~ 'Z' THEN
-        soundex = char;
-        last_symbol = symbol;
-      END IF;
-    ELSIF last_symbol != symbol THEN
-      soundex = soundex || symbol;
-      last_symbol = symbol;
-    END IF;
-  END LOOP;
-
-  RETURN soundex;
-END;
-$$;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `#__guidedtours`
 --
 
@@ -1036,3 +972,67 @@ INSERT INTO "#__guidedtour_steps" ("id", "tour_id", "title", "published", "descr
 (15, 1, 'Congratulations!!!', 1, '<p>You successfully created your first Guided Tour!</p>', 0, 1, 'bottom', '', 'administrator/index.php?option=com_guidedtours&view=step&layout=edit','', 430, '', 430);
 
 SELECT setval('#__guidedtour_steps_id_seq', 16, false);
+
+-- --------------------------------------------------------
+
+--
+-- Here is SOUNDEX replacement for those who can't enable fuzzystrmatch module
+--   from contrib folder.
+-- This function comes from https://wiki.postgresql.org/wiki/Soundex
+--   and is distributed with GPL license.
+-- Thanks to its author, Marti Raudsepp, that published this piece of code.
+--
+
+CREATE OR REPLACE FUNCTION soundex(input text) RETURNS text
+IMMUTABLE STRICT COST 500 LANGUAGE plpgsql
+AS $$
+DECLARE
+  soundex text = '';
+  char text;
+  symbol text;
+  last_symbol text = '';
+  pos int = 1;
+BEGIN
+  WHILE length(soundex) < 4 LOOP
+    char = upper(substr(input, pos, 1));
+    pos = pos + 1;
+    CASE char
+    WHEN '' THEN
+      -- End of input string
+      IF soundex = '' THEN
+        RETURN '';
+      ELSE
+        RETURN rpad(soundex, 4, '0');
+      END IF;
+    WHEN 'B', 'F', 'P', 'V' THEN
+      symbol = '1';
+    WHEN 'C', 'G', 'J', 'K', 'Q', 'S', 'X', 'Z' THEN
+      symbol = '2';
+    WHEN 'D', 'T' THEN
+      symbol = '3';
+    WHEN 'L' THEN
+      symbol = '4';
+    WHEN 'M', 'N' THEN
+      symbol = '5';
+    WHEN 'R' THEN
+      symbol = '6';
+    ELSE
+      -- Not a consonant; no output, but next similar consonant will be re-recorded
+      symbol = '';
+    END CASE;
+
+    IF soundex = '' THEN
+      -- First character; only accept strictly English ASCII characters
+      IF char ~>=~ 'A' AND char ~<=~ 'Z' THEN
+        soundex = char;
+        last_symbol = symbol;
+      END IF;
+    ELSIF last_symbol != symbol THEN
+      soundex = soundex || symbol;
+      last_symbol = symbol;
+    END IF;
+  END LOOP;
+
+  RETURN soundex;
+END;
+$$;
