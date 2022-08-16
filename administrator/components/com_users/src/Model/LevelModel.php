48ba60e19750601c4ac45fbbde68049d1d474789
Nicola Galgano <optimus4joomla@gmail.com>
2022-08-16 10:00:48 +0200

    Update LevelModel.php

--- administrator/components/com_users/src/Model/LevelModel.php
+++ administrator/components/com_users/src/Model/LevelModel.php
@@ -151,7 +151,7 @@ class LevelModel extends AdminModel
         $result = parent::getItem($pk);
 
         // Convert the params field to an array.
-        if ($pk !== null) {
+        if (property_exists($result, 'rules') && $result->rules !== null) {
             $result->rules = json_decode($result->rules);
         }
 
