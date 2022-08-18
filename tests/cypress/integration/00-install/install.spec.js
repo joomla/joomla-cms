ae6d0dc5e5febd474ac81d7c01b21f475f455962
Hannes Papenberg <info@joomlager.de>
2022-08-18 15:42:23 +0200

    Remove old install.spec.js

--- tests/cypress/integration/00-install/install.spec.js
+++
@@ -1,8 +0,0 @@
-// type definitions for Cypress object "cy"
-// <reference types="cypress" />
-
-describe('Install Joomla', () => {
-  it('Install Joomla', function () {
-    cy.installJoomla(this.joomlaconfig)
-  })
-})
