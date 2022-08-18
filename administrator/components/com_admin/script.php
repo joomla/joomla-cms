32f4961666d4c2006797b1fc6636e564fdcff6a6
Brian Teeman <brian@teeman.net>
2022-08-18 18:54:50 +0100

    richards suggested comment

--- administrator/components/com_admin/script.php
+++ administrator/components/com_admin/script.php
@@ -7802,7 +7802,7 @@ class JoomlaInstallerScript
             // From 4.2.0-beta2 to 4.2.0-beta3
             '/plugins/system/webauthn/src/Helper',
             '/plugins/system/webauthn/src/Exception',
-            // From 4.2.0-beta3 to 4.2.0
+            // From 4.2.0 to 4.2.1
             '/libraries/vendor/symfony/string/Resources/bin',
         );
 
