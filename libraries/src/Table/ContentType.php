46f47fb455bb4b8db6590c2e58383643083d6d94
Brian Teeman <brian@teeman.net>
2022-08-24 00:05:03 +0100

    cs

--- libraries/src/Table/ContentType.php
+++ libraries/src/Table/ContentType.php
@@ -80,7 +80,7 @@ class ContentType extends Table
         if (
             $table->load(array('alias' => $this->alias, 'parent_id' => (int) $this->parent_id))
             && ($table->id != $this->id || $this->id == 0)
-        ){
+        ) {
             $this->setError(Text::_('COM_TAGS_ERROR_UNIQUE_ALIAS'));
 
             return false;
