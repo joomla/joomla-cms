f3a97a689174718df216f019254c2fe8956ef13e
Brian Teeman <brian@teeman.net>
2022-08-24 22:19:37 +0100

    remove eslint-disable

--- build/media_source/layouts/js/joomla/html/batch/batch-copymove.es6.js
+++ build/media_source/layouts/js/joomla/html/batch/batch-copymove.es6.js
@@ -1,4 +1,3 @@
-/* eslint-disable linebreak-style */
 /**
  * @copyright  (C) 2018 Open Source Matters, Inc. <https://www.joomla.org>
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
