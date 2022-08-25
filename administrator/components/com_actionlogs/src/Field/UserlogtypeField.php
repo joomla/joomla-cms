10cb7a22ac0a1eb6cf101ca4628ec0a737375359
Nicola Galgano <optimus4joomla@gmail.com>
2022-08-25 09:25:41 +0200

    Update administrator/components/com_actionlogs/src/Field/UserlogtypeField.php
    Co-authored-by: heelc29 <66922325+heelc29@users.noreply.github.com>

--- administrator/components/com_actionlogs/src/Field/UserlogtypeField.php
+++ administrator/components/com_actionlogs/src/Field/UserlogtypeField.php
@@ -5,7 +5,6 @@
  * @subpackage  com_actionlogs
  *
  * @copyright   (C) 2022 Open Source Matters, Inc. <https://www.joomla.org>
-
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
