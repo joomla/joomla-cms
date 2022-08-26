d223a51ab1e60fb773e285cbd994893f08df98f4
Brian Teeman <brian@teeman.net>
2022-08-26 23:25:02 +0100

    Update administrator/components/com_tags/src/Model/TagModel.php
    Co-authored-by: heelc29 <66922325+heelc29@users.noreply.github.com>

--- administrator/components/com_tags/src/Model/TagModel.php
+++ administrator/components/com_tags/src/Model/TagModel.php
@@ -635,7 +635,7 @@ class TagModel extends AdminModel
         }
 
         // Check that user has create and edit permission for menus
-        if (!Factory::getApplication()->getIdentity()->authorise('core.create', 'com_tags')) {
+        if (!$this->getCurrentUser()->authorise('core.create', 'com_tags')) {
             $this->setError(Text::_('COM_TAGS_BATCH_COPY_MOVE_CANNOT_CREATE'));
 
             return false;
