df127bafbf9f239f90765567d90ddf8e8fb7909b
Сергей <koreshs@mail.ru>
2022-08-29 00:39:42 +0300

    Update HtmlView.php

--- components/com_content/src/View/Archive/HtmlView.php
+++ components/com_content/src/View/Archive/HtmlView.php
@@ -7,7 +7,7 @@
  * @copyright   (C) 2006 Open Source Matters, Inc. <https://www.joomla.org>
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
-
+ 
 namespace Joomla\Component\Content\Site\View\Archive;
 
 use Joomla\CMS\Factory;
