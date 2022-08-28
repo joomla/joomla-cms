3955b9c2bbbd0962032eb726b175ddec17b5a950
Roland Dalmulder <contact@rolandd.com>
2022-08-28 10:18:34 +0200

    Joomla! 4.2.1 Release Candidate 3

--- libraries/src/Version.php
+++ libraries/src/Version.php
@@ -66,7 +66,7 @@ final class Version
      * @var    string
      * @since  3.8.0
      */
-    public const EXTRA_VERSION = 'rc3-dev';
+    public const EXTRA_VERSION = 'rc3';
 
     /**
      * Development status.
@@ -74,7 +74,7 @@ final class Version
      * @var    string
      * @since  3.5
      */
-    public const DEV_STATUS = 'Development';
+    public const DEV_STATUS = 'Release Candidate';
 
     /**
      * Code name.
@@ -90,7 +90,7 @@ final class Version
      * @var    string
      * @since  3.5
      */
-    public const RELDATE = '21-August-2022';
+    public const RELDATE = '28-August-2022';
 
     /**
      * Release time.
@@ -98,7 +98,7 @@ final class Version
      * @var    string
      * @since  3.5
      */
-    public const RELTIME = '12:39';
+    public const RELTIME = '09:17';
 
     /**
      * Release timezone.
