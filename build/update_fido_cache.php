acc7a9b77766ee47662eff013c7519d87ef2eb7e
Nicholas K. Dionysopoulos <nicholas@akeeba.com>
2022-09-01 13:09:22 +0300

    Use cached FIDO metadata
    Simplify

--- build/update_fido_cache.php
+++ build/update_fido_cache.php
@@ -8,10 +8,6 @@
  * @phpcs          :disable PSR1.Classes.ClassDeclaration.MissingNamespace
  */
 
-use Joomla\CMS\Http\HttpFactory;
-use Lcobucci\JWT\Configuration;
-use Webauthn\MetadataService\MetadataStatement;
-
 echo <<< TEXT
 Update FIDO Cache version 1.0
 
@@ -20,69 +16,33 @@ any later version published by the Free Software Foundation.
 
 TEXT;
 
-echo "Loading Joomla libraries...\n";
-
-// Set flag that this is a parent file.
-const _JEXEC = 1;
-
-// Load system defines
-if (file_exists(dirname(__DIR__) . '/defines.php'))
-{
-    require_once dirname(__DIR__) . '/defines.php';
-}
-
-if (!defined('_JDEFINES'))
-{
-    define('JPATH_BASE', dirname(__DIR__));
-    require_once JPATH_BASE . '/includes/defines.php';
-}
-
-// Get the Platform with legacy libraries.
-require_once JPATH_LIBRARIES . '/bootstrap.php';
-
-$fullPath = JPATH_BASE;
-
-$possibleFile = end($argv);
-
 echo "Fetching FIDO metadata statements...\n";
 
-$http     = HttpFactory::getHttp();
+$context = stream_context_create([
+    'http' => [
+        'method' => 'GET',
+        'follow_location' => 1,
+        'timeout' => 5.0,
+    ]
+]);
 
-try
-{
-    $response = $http->get('https://mds.fidoalliance.org/', [], 5);
-    $rawJwt   = ($response->code < 200 || $response->code > 299) ? false : $response->body;
-}
-catch (\Throwable $e)
-{
-    echo "Cannot download FIDO metadata statements from https://mds.fidoalliance.org/";
+$rawJwt = @file_get_contents('https://mds.fidoalliance.org/', false, $context);
 
-    exit(1);
-}
+if ($rawJwt === false) {
+    echo "Could not get an updated fido.jwt file.\n";
 
-try
-{
-    $jwtConfig = Configuration::forUnsecuredSigner();
-    $token     = $jwtConfig->parser()->parse($rawJwt);
-}
-catch (Exception $e)
-{
-    echo "Invalid FIDO metadata statements.";
-
-    exit(2);
-}
-
-if (!($token instanceof \Lcobucci\JWT\Token\Plain))
-{
-    echo "Invalid FIDO metadata statements.";
-
-    exit(3);
+    exit (1);
 }
 
 echo "Saving JWT file in the plugin directory...\n";
+
+if (!isset($fullPath))
+{
+    $fullPath = dirname(__DIR__);
+}
 
 $filePath = rtrim($fullPath, '\\/') . '/plugins/system/webauthn/fido.jwt';
 
 file_put_contents($filePath, $rawJwt);
 
-echo "All done!";
+echo "File saved: $filePath\n";
