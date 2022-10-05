8868c4cc190d77adf705cfcf493c66f88747d333
AlterBrains <alterbrains@gmail.com>
2022-10-05 09:43:37 +0300

    typo

--- administrator/components/com_config/src/Model/ApplicationModel.php
+++ administrator/components/com_config/src/Model/ApplicationModel.php
@@ -851,11 +851,11 @@ class ApplicationModel extends FormModel
         if (is_null($permission)) {
             // Get data from input.
             $permission = array(
-                'component' => $app->input->Json->get('comp'),
-                'action'    => $app->input->Json->get('action'),
-                'rule'      => $app->input->Json->get('rule'),
-                'value'     => $app->input->Json->get('value'),
-                'title'     => $app->input->Json->get('title', '', 'RAW')
+                'component' => $app->input->json->get('comp'),
+                'action'    => $app->input->json->get('action'),
+                'rule'      => $app->input->json->get('rule'),
+                'value'     => $app->input->json->get('value'),
+                'title'     => $app->input->json->get('title', '', 'RAW')
             );
         }
 
