877432ab378c235b01ec8990cb35e9cd0226bc8e
Crystal Dionysopoulos <9141288+crystalenka@users.noreply.github.com>
2022-10-10 14:56:26 +0300

    Update administrator/components/com_media/resources/scripts/components/browser/actionItems/toggle.vue
    Co-authored-by: Allon Moritz <allon.moritz@digital-peak.com>

--- administrator/components/com_media/resources/scripts/components/browser/actionItems/toggle.vue
+++ administrator/components/com_media/resources/scripts/components/browser/actionItems/toggle.vue
@@ -3,7 +3,7 @@
     type="button"
     class="action-toggle"
     :aria-label="sprintf('COM_MEDIA_OPEN_ITEM_ACTIONS',(this.$parent.$props.item.name))"
-    :title="sprintf('COM_MEDIA_OPEN_ITEM_ACTIONS',(this.$parent.$props.item.name))"
+    :title="sprintf('COM_MEDIA_OPEN_ITEM_ACTIONS', (this.$parent.$props.item.name))"
     @keyup.enter="openActions()"
     @focus="focused(true)"
     @blur="focused(false)"
