c53c9b1faa34903f249a3ef96dfebb4e89bfee31
Allon Moritz <allon.moritz@digital-peak.com>
2022-10-23 12:54:58 +0200

    Fix code style errors from vue update (#39043)

--- administrator/components/com_media/resources/scripts/components/browser/actionItems/toggle.vue
+++ administrator/components/com_media/resources/scripts/components/browser/actionItems/toggle.vue
@@ -2,8 +2,8 @@
   <button
     type="button"
     class="action-toggle"
-    :aria-label="sprintf('COM_MEDIA_MANAGE_ITEM', (this.$parent.$props.item.name))"
-    :title="sprintf('COM_MEDIA_MANAGE_ITEM', (this.$parent.$props.item.name))"
+    :aria-label="sprintf('COM_MEDIA_MANAGE_ITEM', ($parent.$props.item.name))"
+    :title="sprintf('COM_MEDIA_MANAGE_ITEM', ($parent.$props.item.name))"
     @keyup.enter="openActions()"
     @focus="focused(true)"
     @blur="focused(false)"
