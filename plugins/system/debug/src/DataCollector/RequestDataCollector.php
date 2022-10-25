1d17e9d1fc1622fb58548561628c093c54c94072
Brian Teeman <brian@teeman.net>
2022-10-25 15:55:26 +0100

    Update RequestDataCollector.php

--- plugins/system/debug/src/DataCollector/RequestDataCollector.php
+++ plugins/system/debug/src/DataCollector/RequestDataCollector.php
@@ -4,7 +4,7 @@
  * @package     Joomla.Plugin
  * @subpackage  System.Debug
  *
- * @copyright   (C) 2018 Open Source Matters, Inc. <https://www.joomla.org>
+ * @copyright   (C) 2022 Open Source Matters, Inc. <https://www.joomla.org>
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
@@ -15,7 +15,7 @@ namespace Joomla\Plugin\System\Debug\DataCollector;
 // phpcs:enable PSR1.Files.SideEffects
 
 /**
- * Collects info about the request content while redacting potentially secret contents
+ * Collects info about the request content while redacting potentially secret content
  *
  * @since  4.2.4
  */
