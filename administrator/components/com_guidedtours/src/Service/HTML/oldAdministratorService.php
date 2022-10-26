a7e5e955878dfa0482fe5f58f2e45e9e9fd49d0d
khu5h1 <khushirauniyar8@gmail.com>
2022-10-26 18:01:54 +0530

    Code Cleanup

--- administrator/components/com_guidedtours/src/Service/HTML/oldAdministratorService.php
+++ administrator/components/com_guidedtours/src/Service/HTML/oldAdministratorService.php
@@ -1,16 +1,11 @@
 <?php
 
 /**
- * File Doc Comment_
- * PHP version 5
+ * @package       Joomla.Administrator
+ * @subpackage    com_guidedtours
  *
- * @category  Component
- * @package   Joomla.Administrator
- * @author    Joomla! <admin@joomla.org>
- *
- * @copyright (C) 2022 Open Source Matters, Inc. <https://www.joomla.org>
- * @license   GNU General Public License version 2 or later; see LICENSE.txt
- * @link      admin@joomla.org
+ * @copyright     (C) 2022 Open Source Matters, Inc. <https://www.joomla.org>
+ * @license       GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 namespace Joomla\Component\Guidedtours\Administrator\Service\HTML;
