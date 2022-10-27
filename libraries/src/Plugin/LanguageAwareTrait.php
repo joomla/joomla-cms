e43265c7ff9c0ae3e563bcf8c6076dba4ee5fff1
Nicholas K. Dionysopoulos <nicholas@akeeba.com>
2022-10-27 22:05:38 +0300

    Missing copyright header

--- libraries/src/Plugin/LanguageAwareTrait.php
+++ libraries/src/Plugin/LanguageAwareTrait.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+ * Joomla! Content Management System
+ *
+ * @copyright  (C) 2022 Open Source Matters, Inc. <https://www.joomla.org>
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
 namespace Joomla\CMS\Plugin;
 
 use Joomla\CMS\Factory;
