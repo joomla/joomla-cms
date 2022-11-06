e0748111e714a75b3e21ebe711f99360eba62415
FancyFranci <franci_perisa@hotmail.de>
2022-11-06 22:48:57 +0100

    Joomla! 4.2.5 Stable

--- libraries/src/Version.php
+++ libraries/src/Version.php
@@ -66,7 +66,7 @@ final class Version
      * @var    string
      * @since  3.8.0
      */
-    public const EXTRA_VERSION = 'rc2-dev';
+    public const EXTRA_VERSION = '';
 
     /**
      * Development status.
@@ -74,7 +74,7 @@ final class Version
      * @var    string
      * @since  3.5
      */
-    public const DEV_STATUS = 'Development';
+    public const DEV_STATUS = 'Stable';
 
     /**
      * Code name.
@@ -90,7 +90,7 @@ final class Version
      * @var    string
      * @since  3.5
      */
-    public const RELDATE = '29-October-2022';
+    public const RELDATE = '8-November-2022';
 
     /**
      * Release time.
@@ -98,7 +98,7 @@ final class Version
      * @var    string
      * @since  3.5
      */
-    public const RELTIME = '21:41';
+    public const RELTIME = '15:00';
 
     /**
      * Release timezone.
