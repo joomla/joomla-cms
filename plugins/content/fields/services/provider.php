0b7762637efb4ac90c4707bac03bbc9807e99b68
Allon Moritz <allon.moritz@digital-peak.com>
2022-11-07 09:59:03 +0100

    Update plugins/content/fields/services/provider.php

--- plugins/content/fields/services/provider.php
+++ plugins/content/fields/services/provider.php
@@ -39,7 +39,6 @@ return new class implements ServiceProviderInterface
                     $dispatcher,
                     (array) PluginHelper::getPlugin('content', 'fields')
                 );
-                $plugin->setApplication(Factory::getApplication());
 
                 return $plugin;
             }
