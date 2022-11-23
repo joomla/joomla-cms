86e8d3e7257f0fe5ce6b993518ce0fd245c78972
Octavian Cinciu <octavian@rsjoomla.com>
2022-11-23 21:52:07 +0200

    my bad

--- build/media_source/plg_editors_codemirror/js/joomla-editor-codemirror.w-c.es6.js
+++ build/media_source/plg_editors_codemirror/js/joomla-editor-codemirror.w-c.es6.js
@@ -63,7 +63,7 @@ class CodemirrorEditor extends HTMLElement {
         // Fix the x-php error
         if (['text/x-php', 'application/x-httpd-php', 'application/x-httpd-php-open'].includes(mode.mime)) {
           editor.setOption('mode', 'php');
-        } else if (mode.mime === 'text/html')
+        } else if (mode.mime === 'text/html') {
           editor.setOption('mode', mode.mode);
         } else {
           editor.setOption('mode', mode.mime);
