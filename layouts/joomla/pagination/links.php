e79720ded4a461a8c71ccec1dd8784a6226f7078
Lakshay Pruthi <91469717+Lakshay-Pruthi@users.noreply.github.com>
2022-12-01 16:49:15 +0530

    Update links.php

--- layouts/joomla/pagination/links.php
+++ layouts/joomla/pagination/links.php
@@ -48,7 +48,7 @@ if ($currentPage >= $step) {
 
 <?php if (!empty($pages)) : ?>
     <nav class="pagination__wrapper" aria-label="<?php echo Text::_('JLIB_HTML_PAGINATION'); ?>">
-        <div class="pagination pagination-toolbar text-center">
+        <div class="pagination pagination-toolbar text-center position-absolute end-0 px-4">
 
             <?php if ($showLimitBox) : ?>
                 <div class="limit float-end">
