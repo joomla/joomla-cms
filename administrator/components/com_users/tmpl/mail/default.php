5e9c4894b438d706d273711f5d356a0c0fd808ab
Hannes Papenberg <info@joomlager.de>
2022-12-08 11:19:28 +0100

    Fixing markup

--- administrator/components/com_users/tmpl/mail/default.php
+++ administrator/components/com_users/tmpl/mail/default.php
@@ -26,37 +26,35 @@ $comUserParams = ComponentHelper::getParams('com_users');
 ?>
 
 <form action="<?php echo Route::_('index.php?option=com_users&view=mail'); ?>" name="adminForm" method="post" id="mail-form" aria-label="<?php echo Text::_('COM_USERS_MASSMAIL_FORM_NEW'); ?>" class="main-card p-4 form-validate">
-    <div class="main-card">
-        <div class="row">
-            <div class="col-lg-9">
-                <div class="control-group">
-                    <?php echo $this->form->getLabel('subject'); ?>
-                    <span class="input-group">
-                        <?php if (!empty($comUserParams->get('mailSubjectPrefix'))) : ?>
-                            <span class="input-group-text"><?php echo $comUserParams->get('mailSubjectPrefix'); ?></span>
-                        <?php endif; ?>
-                        <?php echo $this->form->getInput('subject'); ?>
-                    </span>
-                </div>
-                <div class="control-group">
-                    <?php echo $this->form->getLabel('message'); ?>
-                    <?php echo $this->form->getInput('message'); ?>
-                    <?php if (!empty($comUserParams->get('mailBodySuffix'))) : ?>
-                        <div class="mt-1 card">
-                            <div class="card-body">
-                                <?php echo $comUserParams->get('mailBodySuffix'); ?>
-                            </div>
-                        </div>
+    <div class="row">
+        <div class="col-lg-9">
+            <div class="control-group">
+                <?php echo $this->form->getLabel('subject'); ?>
+                <span class="input-group">
+                    <?php if (!empty($comUserParams->get('mailSubjectPrefix'))) : ?>
+                        <span class="input-group-text"><?php echo $comUserParams->get('mailSubjectPrefix'); ?></span>
                     <?php endif; ?>
-                </div>
+                    <?php echo $this->form->getInput('subject'); ?>
+                </span>
             </div>
-            <div class="col-lg-3">
-                <?php echo $this->form->renderField('recurse'); ?>
-                <?php echo $this->form->renderField('mode'); ?>
-                <?php echo $this->form->renderField('disabled'); ?>
-                <?php echo $this->form->renderField('bcc'); ?>
-                <?php echo $this->form->renderField('group'); ?>
+            <div class="control-group">
+                <?php echo $this->form->getLabel('message'); ?>
+                <?php echo $this->form->getInput('message'); ?>
+                <?php if (!empty($comUserParams->get('mailBodySuffix'))) : ?>
+                    <div class="mt-1 card">
+                        <div class="card-body">
+                            <?php echo $comUserParams->get('mailBodySuffix'); ?>
+                        </div>
+                    </div>
+                <?php endif; ?>
             </div>
+        </div>
+        <div class="col-lg-3">
+            <?php echo $this->form->renderField('recurse'); ?>
+            <?php echo $this->form->renderField('mode'); ?>
+            <?php echo $this->form->renderField('disabled'); ?>
+            <?php echo $this->form->renderField('bcc'); ?>
+            <?php echo $this->form->renderField('group'); ?>
         </div>
     </div>
     <input type="hidden" name="task" value="">
