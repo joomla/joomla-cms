6b7552d24739ae6bb8cad79cc4dac4e99232a0d0
Shazma Siddiqui <43249198+shazmasiddiqui@users.noreply.github.com>
2022-12-31 15:23:15 +0530

    Update Book.php
    Added empty lines

--- plugins/schemaorg/book/src/Extension/Book.php
+++ plugins/schemaorg/book/src/Extension/Book.php
@@ -97,10 +97,13 @@ final class Book extends CMSPlugin implements SubscriberInterface
     public function onSchemaPrepareData(AbstractEvent $event)
     {
         $context = $event->getArgument('context');
+        
         if (!$this->isSupported($context) || !$this->isSchemaSupported($event)) {
             return false;
         }
+        
         $this->updateSchemaForm($event);
+        
         return true;
     }
 
@@ -115,12 +118,16 @@ final class Book extends CMSPlugin implements SubscriberInterface
     {
         $form = $event->getArgument('subject');
         $context = $form->getName();
+        
         if (!$this->isSupported($context)) {
             return false;
         }
+        
         $this->addSchemaType($event);
+        
         //Load the form fields
         $form->loadFile(JPATH_PLUGINS . '/' . $this->_type . '/' . $this->_name . '/src/forms/schemaorg.xml');
+        
         return true;
     }
 
@@ -139,7 +146,9 @@ final class Book extends CMSPlugin implements SubscriberInterface
         if ($form != $this->pluginName) {
             return false;
         }
+        
         $this->storeSchemaToStandardLocation($event);
+        
         return true;
     }
 
@@ -157,6 +166,7 @@ final class Book extends CMSPlugin implements SubscriberInterface
         if (is_object($schema)) {
             $schema = $this->cleanupDate($schema, ['datePublished']);
         }
+        
         return $schema;
     }
 }
