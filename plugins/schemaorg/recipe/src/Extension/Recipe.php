9cb82150d51579dd630f96506addc7fde8302543
Shazma Siddiqui <siddiquishaz7@gmail.com>
2022-12-31 22:07:26 +0530

    Updated file structure

--- plugins/schemaorg/recipe/src/Extension/Recipe.php
+++ plugins/schemaorg/recipe/src/Extension/Recipe.php
@@ -12,10 +12,8 @@
 namespace Joomla\Plugin\Schemaorg\Recipe\Extension;
 
 use Joomla\CMS\Application\CMSApplicationInterface;
-use Joomla\CMS\Form\Form;
 use Joomla\CMS\Plugin\CMSPlugin;
 use Joomla\CMS\Schemaorg\SchemaorgPluginTrait;
-use Joomla\CMS\Event\AbstractEvent;
 use Joomla\Registry\Registry;
 use Joomla\Event\SubscriberInterface;
 
@@ -55,102 +53,12 @@ final class Recipe extends CMSPlugin implements SubscriberInterface
     protected $app;
 
     /**
-     * The name of the supported name to check against
-     *
-     * @var   string
-     * @since 4.0.0
-     */
-    protected $supportFunctionality = 'core.state';
-
-    /**
      * The name of the schema form
      *
      * @var   string
      * @since 4.0.0
      */
     protected $pluginName = 'Recipe';
-
-    /**
-     * Returns an array of events this subscriber will listen to.
-     *
-     * @return  array
-     *
-     * @since   4.0.0
-     */
-    public static function getSubscribedEvents(): array
-    {
-        return [
-            'onSchemaPrepareData' => 'onSchemaPrepareData',
-            'onSchemaPrepareForm' => 'onSchemaPrepareForm',
-            'onSchemaAfterSave' => 'onSchemaAfterSave',
-            'onSchemaBeforeCompileHead' => 'pushSchema'
-        ];
-    }
-
-    /**
-     *  Update existing schema form with data from database
-     *
-     *  @param   $data  The form to be altered.
-     *
-     *  @return  boolean
-     */
-    public function onSchemaPrepareData(AbstractEvent $event)
-    {
-        $context = $event->getArgument('context');
-
-        if (!$this->isSupported($context) || !$this->isSchemaSupported($event)) {
-            return false;
-        }
-        
-        $this->updateSchemaForm($event);
-
-        return true;
-    }
-
-     /**
-     *  Add a new option to the schema type in the article editing page
-     *
-     *  @param   Form  $form  The form to be altered.
-     *
-     *  @return  boolean
-     */
-    public function onSchemaPrepareForm(AbstractEvent $event)
-    {
-        $form = $event->getArgument('subject');
-        $context = $form->getName();
-
-        if (!$this->isSupported($context)) {
-            return false;
-        }
-
-        $this->addSchemaType($event);
-
-        //Load the form fields
-        $form->loadFile(JPATH_PLUGINS . '/' . $this->_type . '/' . $this->_name . '/src/forms/schemaorg.xml');
-
-        return true;
-    }
-
-    /**
-     *  Saves the schema to the database
-     *
-     *  @param   AbstractEvent $event
-     *
-     *  @return  boolean
-     */
-    public function onSchemaAfterSave(AbstractEvent $event)
-    {
-        $data = $event->getArgument('data')->toArray();
-        $form = $data['schema']['schemaType'];
-
-        if ($form != $this->pluginName) {
-            return false;
-        }
-
-        $this->storeSchemaToStandardLocation($event);
-
-        return true;
-    }
 
     /**
      *  To add plugin specific functions
