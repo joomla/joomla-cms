ad1546146ec16496fccfb419a9e0dba9ebe8ff31
carlitorweb <crsanchez8646@gmail.com>
2023-01-01 09:20:43 -0500

    Helper class adaptation

--- modules/mod_articles_archive/src/Helper/ArticlesArchiveHelper.php
+++ modules/mod_articles_archive/src/Helper/ArticlesArchiveHelper.php
@@ -10,12 +10,19 @@
 
 namespace Joomla\Module\ArticlesArchive\Site\Helper;
 
+use Joomla\CMS\Application\SiteApplication;
+use Joomla\CMS\Cache\CacheControllerFactoryInterface;
+use Joomla\CMS\Cache\Controller\OutputController;
 use Joomla\CMS\Factory;
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\Router\Route;
 use Joomla\Component\Content\Administrator\Extension\ContentComponent;
+use Joomla\Component\Content\Site\Model\ArchiveModel;
+use Joomla\Database\DatabaseAwareInterface;
+use Joomla\Database\DatabaseAwareTrait;
 use Joomla\Database\ParameterType;
+use Joomla\Registry\Registry;
 
 // phpcs:disable PSR1.Files.SideEffects
 \defined('_JEXEC') or die;
@@ -24,73 +31,139 @@ use Joomla\Database\ParameterType;
 /**
  * Helper for mod_articles_archive
  *
- * @since  1.5
+ * @since  __DEPLOY_VERSION__
  */
-class ArticlesArchiveHelper
+class ArticlesArchiveHelper implements DatabaseAwareInterface
 {
+    use DatabaseAwareTrait;
+
+    /**
+     * The module instance
+     *
+     * @var    \stdClass
+     * @since  __DEPLOY_VERSION__
+     */
+    protected $module;
+
+    /**
+     * The input instance
+     *
+     * @var    Input
+     * @since  __DEPLOY_VERSION__
+     */
+    protected $input;
+
+    /**
+     * Constructor.
+     *
+     * @param  array  $config   An optional associative array of configuration settings.
+     *
+     * @since  __DEPLOY_VERSION__
+     */
+    public function __construct($config = [])
+    {
+        $this->module = $config['module'];
+        $this->input  = $config['input'];
+    }
+
+    /**
+     * Retrieve a list of archive article
+     *
+     * @param   Registry         $params  The module parameters.
+     * @param   SiteApplication  $app     The current application.
+     *
+     * @return  object[]
+     *
+     * @since   __DEPLOY_VERSION__
+     */
+    public function getArticles(Registry $moduleParams, SiteApplication $app)
+    {
+        $cacheKey = md5(serialize(array ($moduleParams->toString(), $this->module->module, $this->module->id)));
+
+        /** @var OutputController $cache */
+        $cache = Factory::getContainer()->get(CacheControllerFactoryInterface::class)
+            ->createCacheController('output', ['defaultgroup' => 'mod_articles_archive']);
+
+        if (!$cache->contains($cacheKey)) {
+            $mvcContentFactory = $app->bootComponent('com_content')->getMVCFactory();
+
+            /** @var ArchiveModel $archiveModel */
+            $archiveModel = $mvcContentFactory->createModel('Archive', 'Site', ['ignore_request' => true]);
+
+            // Set application parameters in model
+            $appParams = $app->getParams();
+            $archiveModel->setState('params', $appParams);
+
+            $archiveModel->setState('list.start', 0);
+
+            // Set the filters based on the module params
+            $archiveModel->setState('list.limit', (int) $moduleParams->get('count', 1));
+
+            // Filter by language
+            $archiveModel->setState('filter.language', $app->getLanguageFilter());
+
+            // Prepare the module output
+            $items  = [];
+            $menu   = $app->getMenu();
+
+            $menuItem       = $menu->getItems('link', 'index.php?option=com_content&view=archive', true);
+            $urlParamItemid = (isset($menuItem) && !empty($menuItem->id)) ? '&Itemid=' . $menuItem->id : '';
+
+            foreach ($archiveModel->getData() as $item) {
+                $items[] = static::prepareItem($item, $urlParamItemid);
+            }
+
+            // Cache the output and return
+            $cache->store($items, $cacheKey);
+
+            return $items;
+        }
+
+        // Return the cached output
+        return $cache->get($cacheKey);
+    }
+
+    /**
+     * Prepare the article before render.
+     *
+     * @param   object     $item            The article to prepare
+     * @param   \stdClass  $urlParamItemid  The Itemid param of the URL
+     *
+     * @return  \stdClass
+     *
+     * @since   __DEPLOY_VERSION__
+     */
+    protected static function prepareItem($item, $urlParamItemid): \stdClass
+    {
+        $date = Factory::getDate($item->created);
+
+        $createdMonth = $date->format('n');
+        $createdYear  = $date->format('Y');
+
+        $createdYearCal = HTMLHelper::_('date', $item->created, 'Y');
+        $monthNameCal   = HTMLHelper::_('date', $item->created, 'F');
+
+        $archivedArticle = new \stdClass();
+
+        $archivedArticle->link = Route::_('index.php?option=com_content&view=archive&year=' . $createdYear . '&month=' . $createdMonth . $urlParamItemid);
+        $archivedArticle->text = Text::sprintf('MOD_ARTICLES_ARCHIVE_DATE', $monthNameCal, $createdYearCal);
+
+        return $archivedArticle;
+    }
+
     /**
      * Retrieve list of archived articles
      *
      * @param   \Joomla\Registry\Registry  &$params  module parameters
      *
-     * @return  array
+     * @return  mixed
      *
-     * @since   1.5
+     * @since  __DEPLOY_VERSION__
+     *
+     * @deprecated 5.0 Use the none static function getArticles
      */
     public static function getList(&$params)
     {
-        $app       = Factory::getApplication();
-        $db        = Factory::getDbo();
-        $query     = $db->getQuery(true);
-
-        $query->select($query->month($db->quoteName('created')) . ' AS created_month')
-            ->select('MIN(' . $db->quoteName('created') . ') AS created')
-            ->select($query->year($db->quoteName('created')) . ' AS created_year')
-            ->from($db->quoteName('#__content', 'c'))
-            ->where($db->quoteName('c.state') . ' = ' . ContentComponent::CONDITION_ARCHIVED)
-            ->group($query->year($db->quoteName('c.created')) . ', ' . $query->month($db->quoteName('c.created')))
-            ->order($query->year($db->quoteName('c.created')) . ' DESC, ' . $query->month($db->quoteName('c.created')) . ' DESC');
-
-        // Filter by language
-        if ($app->getLanguageFilter()) {
-            $query->whereIn($db->quoteName('language'), [Factory::getLanguage()->getTag(), '*'], ParameterType::STRING);
-        }
-
-        $query->setLimit((int) $params->get('count'));
-        $db->setQuery($query);
-
-        try {
-            $rows = (array) $db->loadObjectList();
-        } catch (\RuntimeException $e) {
-            $app->enqueueMessage(Text::_('JERROR_AN_ERROR_HAS_OCCURRED'), 'error');
-
-            return [];
-        }
-
-        $menu   = $app->getMenu();
-        $item   = $menu->getItems('link', 'index.php?option=com_content&view=archive', true);
-        $itemid = (isset($item) && !empty($item->id)) ? '&Itemid=' . $item->id : '';
-
-        $i     = 0;
-        $lists = array();
-
-        foreach ($rows as $row) {
-            $date = Factory::getDate($row->created);
-
-            $createdMonth = $date->format('n');
-            $createdYear  = $date->format('Y');
-
-            $createdYearCal = HTMLHelper::_('date', $row->created, 'Y');
-            $monthNameCal   = HTMLHelper::_('date', $row->created, 'F');
-
-            $lists[$i] = new \stdClass();
-
-            $lists[$i]->link = Route::_('index.php?option=com_content&view=archive&year=' . $createdYear . '&month=' . $createdMonth . $itemid);
-            $lists[$i]->text = Text::sprintf('MOD_ARTICLES_ARCHIVE_DATE', $monthNameCal, $createdYearCal);
-
-            $i++;
-        }
-
-        return $lists;
+        return (new self())->getArticles($params, Factory::getApplication());
     }
 }
