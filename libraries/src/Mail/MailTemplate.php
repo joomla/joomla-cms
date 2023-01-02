fba940e9e3be604b7d1f5c13f89b28a9f709fee1
KaushikVishwakarma <100058812+Kaushik1216@users.noreply.github.com>
2023-01-02 11:58:17 +0530

    Using MailTemplate to create new Template record #39480
    Pull Request for issue #39480
    
    Summary of Changes 
    Add extension parameter in createTemplate static function.So that a new record will insert into database after creating email template.
    
    Testing Instruction 
    Just make same change as in this PR and create a mail-template and see mail-template after saving in database.
    
    Result before appling this PR
     After creating mail-template a new record not inserted into database in mail template table
    
    Expected result after this PR
     After this changes on creating mail-template a new record inserted into database in main template table.

--- libraries/src/Mail/MailTemplate.php
+++ libraries/src/Mail/MailTemplate.php
@@ -382,12 +382,12 @@ class MailTemplate
      * @param   string  $body      A default body (normally a translatable string)
      * @param   array   $tags      Associative array of tags to replace
      * @param   string  $htmlbody  A default htmlbody (normally a translatable string)
-     *
+     * @param   string  $extension For extension of mail template 
      * @return  boolean  True on success, false on failure
      *
      * @since   4.0.0
      */
-    public static function createTemplate($key, $subject, $body, $tags, $htmlbody = '')
+    public static function createTemplate($key, $subject, $body, $tags, $htmlbody = '',$extension)
     {
         $db = Factory::getDbo();
 
@@ -397,6 +397,7 @@ class MailTemplate
         $template->subject = $subject;
         $template->body = $body;
         $template->htmlbody = $htmlbody;
+        $template->extension = $extension;
         $template->attachments = '';
         $params = new \stdClass();
         $params->tags = [$tags];
