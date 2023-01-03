bdbf85339498ca994f952fe1fb13f229bd51e8bc
carlitorweb <crsanchez8646@gmail.com>
2023-01-02 22:35:31 -0500

    initial change in helper class

--- modules/mod_articles_categories/src/Helper/ArticlesCategoriesHelper.php
+++ modules/mod_articles_categories/src/Helper/ArticlesCategoriesHelper.php
@@ -10,7 +10,14 @@
 
 namespace Joomla\Module\ArticlesCategories\Site\Helper;
 
-use Joomla\CMS\Categories\Categories;
+use Joomla\CMS\Application\SiteApplication;
+use Joomla\CMS\Cache\CacheControllerFactoryInterface;
+use Joomla\CMS\Cache\Controller\OutputController;
+use Joomla\CMS\Component\ComponentHelper;
+use Joomla\CMS\Factory;
+use Joomla\Component\Content\Administrator\Extension\ContentComponent;
+use Joomla\Component\Content\Site\Model\CategoriesModel;
+use Joomla\Registry\Registry;
 
 // phpcs:disable PSR1.Files.SideEffects
 \defined('_JEXEC') or die;
@@ -19,10 +26,82 @@ use Joomla\CMS\Categories\Categories;
 /**
  * Helper for mod_articles_categories
  *
- * @since  1.5
+ * @since  __DEPLOY_VERSION__
  */
-abstract class ArticlesCategoriesHelper
+class ArticlesCategoriesHelper
 {
+    /**
+     * The module instance
+     *
+     * @var    \stdClass
+     *
+     * @since  __DEPLOY_VERSION__
+     */
+    protected $module;
+
+    /**
+     * Constructor.
+     *
+     * @param  array  $config   An optional associative array of configuration settings.
+     *
+     * @since  __DEPLOY_VERSION__
+     */
+    public function __construct($config = [])
+    {
+        $this->module = $config['module'];
+    }
+
+    /**
+     * Retrieve a list of months with archived articles
+     *
+     * @param   Registry         $params  The module parameters.
+     * @param   SiteApplication  $app     The current application.
+     *
+     * @return  object[]
+     *
+     * @since   __DEPLOY_VERSION__
+     */
+    public function getArticles(Registry $moduleParams, SiteApplication $app)
+    {
+        $cacheKey = md5(serialize(array ($moduleParams->toString(), $this->module->module, $this->module->id)));
+
+        /** @var OutputController $cache */
+        $cache = Factory::getContainer()->get(CacheControllerFactoryInterface::class)
+            ->createCacheController('output', ['defaultgroup' => 'mod_articles_categories']);
+
+        if (!$cache->contains($cacheKey)) {
+
+            $mvcContentFactory = $app->bootComponent('com_content')->getMVCFactory();
+
+            /** @var CategoriesModel $categoriesModel */
+            $categoriesModel = $mvcContentFactory->createModel('Categories', 'Site', ['ignore_request' => true]);
+
+            // Set application parameters in model
+            $appParams = $app->getParams();
+            $categoriesModel->setState('params', $appParams);
+
+            $categoriesModel->setState('list.start', 0);
+
+            $categoriesModel->setState('filter.published', ContentComponent::CONDITION_PUBLISHED);
+
+            // Set the filters based on the module params
+            $categoriesModel->setState('filter.parentId', $moduleParams->get('parent', 'root'));
+            $categoriesModel->setState('list.limit', (int) $moduleParams->get('count', 0));
+
+            // Access filter
+            $access = !ComponentHelper::getParams('com_content')->get('show_noauth');
+            $categoriesModel->setState('filter.access', $access);
+
+            $items = $categoriesModel->getItems();
+
+            // Cache the output and return
+            $cache->store($items, $cacheKey);
+        }
+
+        // Return the cached output
+        return $cache->get($cacheKey);
+    }
+
     /**
      * Get list of articles
      *
@@ -30,26 +109,10 @@ abstract class ArticlesCategoriesHelper
      *
      * @return  array
      *
-     * @since   1.5
+     * @since   __DEPLOY_VERSION__
      */
     public static function getList(&$params)
     {
-        $options               = [];
-        $options['countItems'] = $params->get('numitems', 0);
-
-        $categories = Categories::getInstance('Content', $options);
-        $category   = $categories->get($params->get('parent', 'root'));
-
-        if ($category !== null) {
-            $items = $category->getChildren();
-
-            $count = $params->get('count', 0);
-
-            if ($count > 0 && \count($items) > $count) {
-                $items = \array_slice($items, 0, $count);
-            }
-
-            return $items;
-        }
+        return (new self())->getArticles($params, Factory::getApplication());
     }
 }
