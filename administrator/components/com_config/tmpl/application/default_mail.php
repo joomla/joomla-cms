6fabc68f476af8dac751824f16c9ffe41f893764
Dimitris Grammatikogiannis <dg@dgrammatiko.dev>
2023-01-08 22:00:48 +0100

    Update default_mail.php

--- administrator/components/com_config/tmpl/application/default_mail.php
+++ administrator/components/com_config/tmpl/application/default_mail.php
@@ -12,7 +12,6 @@ use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\Layout\LayoutHelper;
 use Joomla\CMS\Router\Route;
-use Joomla\CMS\Session\Session;
 
 defined('_JEXEC') or die;
 
