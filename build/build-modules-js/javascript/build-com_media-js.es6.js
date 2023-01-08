714056984335abe82e5f0fb900fcabe5d00a255a
Olivier Buisard <olivier.buisard@simplifyyourweb.com>
2023-01-08 17:06:42 -0500

    Update build/build-modules-js/javascript/build-com_media-js.es6.js
    Co-authored-by: Dimitris Grammatikogiannis <dg@dgrammatiko.dev>

--- build/build-modules-js/javascript/build-com_media-js.es6.js
+++ build/build-modules-js/javascript/build-com_media-js.es6.js
@@ -132,7 +132,6 @@ module.exports.mediaManager = async () => {
         console.log('✅ ES2017 Media Manager ready');
         writeFile(resolve('media/com_media/js/media-manager.js'), content.code, { encoding: 'utf8', mode: 0o644 });
         return;
-      }
     })
     .then(() => buildLegacy(resolve('media/com_media/js/media-manager.js')))
     .catch((error) => {
