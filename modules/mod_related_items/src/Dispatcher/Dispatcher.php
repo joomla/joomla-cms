5b26cdde01af20ec9ed4cbf0e058b4d30a2d7fe1
carlitorweb <crsanchez8646@gmail.com>
2023-01-13 06:08:06 -0500

    adding the dispatcher

---
+++ modules/mod_related_items/src/Dispatcher/Dispatcher.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * @package     Joomla.Site
+ * @subpackage  mod_related_items
+ *
+ * @copyright   (C) 2022 Open Source Matters, Inc. <https://www.joomla.org>
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+namespace Joomla\Module\RelatedItems\Site\Dispatcher;
+
+use Joomla\CMS\Dispatcher\AbstractModuleDispatcher;
+use Joomla\CMS\Helper\HelperFactoryAwareInterface;
+use Joomla\CMS\Helper\HelperFactoryAwareTrait;
+
+// phpcs:disable PSR1.Files.SideEffects
+\defined('JPATH_PLATFORM') or die;
+// phpcs:enable PSR1.Files.SideEffects
+
+/**
+ * Dispatcher class for mod_articles_popular
+ *
+ * @since  __DEPLOY_VERSION__
+ */
+class Dispatcher extends AbstractModuleDispatcher implements HelperFactoryAwareInterface
+{
+    use HelperFactoryAwareTrait;
+
+    /**
+     * Returns the layout data.
+     *
+     * @return  array
+     *
+     * @since   __DEPLOY_VERSION__
+     */
+    protected function getLayoutData()
+    {
+        $data = parent::getLayoutData();
+
+        $data['list'] = $this->getHelperFactory()->getHelper('RelatedItemsHelper', $data)->getArticles($data['params'], $data['app']);
+
+        return $data;
+    }
+}
