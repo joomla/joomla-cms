6d2e8ec2a87581d51421d0f1e44d7fcb0e737651
carlitorweb <crsanchez8646@gmail.com>
2023-01-14 19:29:55 -0500

    add the js to the build

---
+++ build/media_source/com_users/js/active-user-send-mail.es6.js
@@ -0,0 +1,8 @@
+/**
+ * @copyright  (C) 2019 Open Source Matters, Inc. <https://www.joomla.org>
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+if (!Joomla) {
+  throw new Error("Joomla API was not properly initialized");
+}
