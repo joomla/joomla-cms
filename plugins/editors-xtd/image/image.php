f190741b1633f72bc637621e59da0dd54761a396
Allon Moritz <allon.moritz@digital-peak.com>
2023-01-16 14:17:48 +0100

    Do not hardcode the media manager API url to select an image

--- plugins/editors-xtd/image/image.php
+++ plugins/editors-xtd/image/image.php
@@ -15,6 +15,7 @@ use Joomla\CMS\Factory;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\Object\CMSObject;
 use Joomla\CMS\Plugin\CMSPlugin;
+use Joomla\CMS\Uri\Uri;
 
 // phpcs:disable PSR1.Files.SideEffects
 \defined('_JEXEC') or die;
@@ -74,9 +75,8 @@ class PlgButtonImage extends CMSPlugin
                 ->useScript('webcomponent.field-media')
                 ->useStyle('webcomponent.media-select');
 
-            $doc->addScriptOptions('xtdImageModal', [
-                $name . '_ImageModal',
-            ]);
+            $doc->addScriptOptions('xtdImageModal', [$name . '_ImageModal']);
+            $doc->addScriptOptions('media-picker-api', ['apiBaseUrl' => Uri::base() . 'index.php?option=com_media&format=json']);
 
             if (count($doc->getScriptOptions('media-picker')) === 0) {
                 $imagesExt = array_map(
