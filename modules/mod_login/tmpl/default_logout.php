11de4d90e852220f4ade7fee955c3c112cebfcc7
Brian Teeman <brian@teeman.net>
2023-01-24 16:50:32 +0000

    Update modules/mod_login/tmpl/default_logout.php
    Co-authored-by: Quy <quy@nomonkeybiz.com>

--- modules/mod_login/tmpl/default_logout.php
+++ modules/mod_login/tmpl/default_logout.php
@@ -15,7 +15,7 @@ use Joomla\CMS\Language\Text;
 use Joomla\CMS\Router\Route;
 
 /** @var Joomla\CMS\WebAsset\WebAssetManager $wa */
-$wa = \Joomla\CMS\Factory\Factory::getApplication()->getDocument()->getWebAssetManager();
+$wa = \Joomla\CMS\Factory::getApplication()->getDocument()->getWebAssetManager();
 $wa->useScript('keepalive');
 
 ?>
