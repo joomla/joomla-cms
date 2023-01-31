293db2b8ba2800aefef68a852d1dbce4648a2395
Allon Moritz <allon.moritz@digital-peak.com>
2023-01-31 11:34:25 +0100

    cs

--- plugins/fields/integer/services/provider.php
+++ plugins/fields/integer/services/provider.php
@@ -18,8 +18,7 @@ use Joomla\DI\ServiceProviderInterface;
 use Joomla\Event\DispatcherInterface;
 use Joomla\Plugin\Fields\Integer\Extension\Integer;
 
-return new class implements ServiceProviderInterface
-{
+return new class () implements ServiceProviderInterface {
     /**
      * Registers the service provider with a DI container.
      *
