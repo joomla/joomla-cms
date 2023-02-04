1ce9b401e792d104320bd03f8a1adf37f09c2709
Charvi <charvimehra0007@gmail.com>
2023-02-04 15:03:30 +0530

    1A_2A_2B_3C

--- modules/mod_login/tmpl/default.php
+++ modules/mod_login/tmpl/default.php
@@ -38,13 +38,10 @@ Text::script('JHIDEPASSWORD');
                 <div class="input-group">
                     <input id="modlgn-username-<?php echo $module->id; ?>" type="text" name="username" class="form-control" autocomplete="username" placeholder="<?php echo Text::_('MOD_LOGIN_VALUE_USERNAME'); ?>">
                     <label for="modlgn-username-<?php echo $module->id; ?>" class="visually-hidden"><?php echo Text::_('MOD_LOGIN_VALUE_USERNAME'); ?></label>
-                    <span class="input-group-text" title="<?php echo Text::_('MOD_LOGIN_VALUE_USERNAME'); ?>">
-                        <span class="icon-user icon-fw" aria-hidden="true"></span>
-                    </span>
                 </div>
             <?php else : ?>
                 <label for="modlgn-username-<?php echo $module->id; ?>"><?php echo Text::_('MOD_LOGIN_VALUE_USERNAME'); ?></label>
-                <input id="modlgn-username-<?php echo $module->id; ?>" type="text" name="username" class="form-control" autocomplete="username" placeholder="<?php echo Text::_('MOD_LOGIN_VALUE_USERNAME'); ?>">
+                <input id="modlgn-username-<?php echo $module->id; ?>" type="text" name="username" class="form-control" autocomplete="username">
             <?php endif; ?>
         </div>
 
@@ -59,8 +56,15 @@ Text::script('JHIDEPASSWORD');
                     </button>
                 </div>
             <?php else : ?>
+                
                 <label for="modlgn-passwd-<?php echo $module->id; ?>"><?php echo Text::_('JGLOBAL_PASSWORD'); ?></label>
-                <input id="modlgn-passwd-<?php echo $module->id; ?>" type="password" name="password" autocomplete="current-password" class="form-control" placeholder="<?php echo Text::_('JGLOBAL_PASSWORD'); ?>">
+                <div class="input-group">
+                    <input id="modlgn-passwd-<?php echo $module->id; ?>" type="password" name="password" autocomplete="current-password" class="form-control">
+                    <button type="button" class="btn btn-secondary input-password-toggle">
+                        <span class="icon-eye icon-fw" aria-hidden="true"></span>
+                        <span class="visually-hidden"><?php echo Text::_('JSHOWPASSWORD'); ?></span>
+                    </button>
+                </div>
             <?php endif; ?>
         </div>
 
