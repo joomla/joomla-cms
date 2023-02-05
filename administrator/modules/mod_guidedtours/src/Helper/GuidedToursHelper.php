9dbac05d0bc1283dbe653c33a85f52e055355d42
khu5h1 <khushirauniyar8@gmail.com>
2023-02-05 17:24:02 +0530

    Updated Copyright year

--- administrator/modules/mod_guidedtours/src/Helper/GuidedToursHelper.php
+++ administrator/modules/mod_guidedtours/src/Helper/GuidedToursHelper.php
@@ -4,7 +4,7 @@
  * @package     Joomla.Administrator
  * @subpackage  mod_guidedtours
  *
- * @copyright   (C) 2010 Open Source Matters, Inc. <https://www.joomla.org>
+ * @copyright   (C) 2023 Open Source Matters, Inc. <https://www.joomla.org>
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
