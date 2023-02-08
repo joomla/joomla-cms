869b1683f92d9e8a867fef29fe97e60c5d4a5bf6
rajputanuj31 <115709571+rajputanuj31@users.noreply.github.com>
2023-02-09 00:30:47 +0530

    updates

--- administrator/components/com_media/resources/scripts/components/browser/table/row.vue
+++ administrator/components/com_media/resources/scripts/components/browser/table/row.vue
@@ -125,13 +125,25 @@ export default {
       }
 
       // Handle clicks when the item was not selected
+      const startindex = this.$store.getters.getSelectedDirectoryContents.indexOf(this.$store.state.selectedItems[0]);
+      const endindex = this.$store.getters.getSelectedDirectoryContents.indexOf(this.item);
       if (!this.isSelected()) {
-        // Unselect all other selected items,
-        // if the shift key was not pressed during the click event
-        if (!(event.shiftKey || event.keyCode === 13)) {
+        if ((event.shiftKey || event.keyCode === 13)) {
+          if (startindex < endindex) {
+            for (let i = startindex; i <= endindex; i += 1) {
+              this.$store.commit(types.SELECT_BROWSER_ITEM, this.$store.getters.getSelectedDirectoryContents[i]);
+            }
+          } else {
+            for (let i = startindex; i >= endindex; i -= 1) {
+              this.$store.commit(types.SELECT_BROWSER_ITEM, this.$store.getters.getSelectedDirectoryContents[i]);
+            }
+          }
+        } else if ((event.ctrlKey || event.keyCode === 17)) {
+          this.$store.commit(types.SELECT_BROWSER_ITEM, this.item);
+        } else {
           this.$store.commit(types.UNSELECT_ALL_BROWSER_ITEMS);
+          this.$store.commit(types.SELECT_BROWSER_ITEM, this.item);
         }
-        this.$store.commit(types.SELECT_BROWSER_ITEM, this.item);
         return;
       }
 
