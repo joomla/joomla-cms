25efca64489493edb21237ae443d9bdebad37d1a
KaushikVishwakarma <100058812+Kaushik1216@users.noreply.github.com>
2023-02-08 14:52:08 +0530

    Add text for no search result in media manager

--- administrator/components/com_media/tmpl/media/default_texts.php
+++ administrator/components/com_media/tmpl/media/default_texts.php
@@ -48,6 +48,7 @@ Text::script('COM_MEDIA_MEDIA_NAME', true);
 Text::script('COM_MEDIA_MEDIA_SIZE', true);
 Text::script('COM_MEDIA_MEDIA_TYPE', true);
 Text::script('COM_MEDIA_NAME', true);
+Text::script('COM_MEDIA_NO_SEARCH_RESULT',true);
 Text::script('COM_MEDIA_OPEN_ITEM_ACTIONS', true);
 Text::script('COM_MEDIA_PLEASE_SELECT_ITEM', true);
 Text::script('COM_MEDIA_RENAME', true);
