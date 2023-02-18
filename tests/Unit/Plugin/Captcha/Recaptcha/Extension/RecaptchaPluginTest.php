f796febebd8a038204f6bcea2ed7194d5d665aad
Olivier Buisard <olivier.buisard@simplifyyourweb.com>
2023-02-18 09:01:55 -0500

    Update tests/Unit/Plugin/Captcha/Recaptcha/Extension/RecaptchaPluginTest.php
    Co-authored-by: Richard Fath <richard67@users.noreply.github.com>

--- tests/Unit/Plugin/Captcha/Recaptcha/Extension/RecaptchaPluginTest.php
+++ tests/Unit/Plugin/Captcha/Recaptcha/Extension/RecaptchaPluginTest.php
@@ -247,7 +247,7 @@ class RecaptchaPluginTest extends UnitTestCase
       */
     public function testFailedResponse()
     {
-        $language   = $this->createStub(Language::class);
+        $language = $this->createStub(Language::class);
         $language->method('_')->willReturn('test');
 
         $app = $this->createStub(CMSWebApplicationInterface::class);
