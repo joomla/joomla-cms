99db8677124fd228ed27fe3028319bf635ecd62b
Olivier Buisard <olivier.buisard@simplifyyourweb.com>
2023-02-18 08:59:59 -0500

    Update tests/Unit/Plugin/Captcha/InvisibleRecaptcha/Extension/InvisibleRecaptchaPluginTest.php
    Co-authored-by: Richard Fath <richard67@users.noreply.github.com>

--- tests/Unit/Plugin/Captcha/InvisibleRecaptcha/Extension/InvisibleRecaptchaPluginTest.php
+++ tests/Unit/Plugin/Captcha/InvisibleRecaptcha/Extension/InvisibleRecaptchaPluginTest.php
@@ -178,7 +178,7 @@ class InvisibleRecaptchaPluginTest extends UnitTestCase
       */
     public function testResponseEmptyPrivateKey()
     {
-        $language   = $this->createStub(Language::class);
+        $language = $this->createStub(Language::class);
         $language->method('_')->willReturn('test');
 
         $app = $this->createStub(CMSWebApplicationInterface::class);
