6c326296ee1fc685768ba04f69d055134370b1f1
Richard Fath <richard67@users.noreply.github.com>
2023-02-19 14:27:13 +0100

    Fix PHPCS

--- administrator/components/com_config/src/Model/ApplicationModel.php
+++ administrator/components/com_config/src/Model/ApplicationModel.php
@@ -299,15 +299,13 @@ class ApplicationModel extends FormModel
             'prefix'   => $data['dbprefix'],
         ];
 
-        if (in_array($options['driver'], ['pgsql', 'postgresql']) && !preg_match('#^[a-zA-Z_][0-9a-zA-Z_$]*$#', $options['database']))
-        {
+        if (in_array($options['driver'], ['pgsql', 'postgresql']) && !preg_match('#^[a-zA-Z_][0-9a-zA-Z_$]*$#', $options['database'])) {
             $app->enqueueMessage(Text::_('COM_CONFIG_FIELD_DATABASE_NAME_INVALID_MSG_POSTGRES'), 'warning');
 
             return false;
         }
 
-        if (in_array($options['driver'], ['mysql', 'mysqli']) && preg_match('#[\\\\\/\.]#', $options['database']))
-        {
+        if (in_array($options['driver'], ['mysql', 'mysqli']) && preg_match('#[\\\\\/\.]#', $options['database'])) {
             $app->enqueueMessage(Text::_('COM_CONFIG_FIELD_DATABASE_NAME_INVALID_MSG_MYSQL'), 'warning');
 
             return false;
