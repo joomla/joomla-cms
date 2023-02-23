592d23c57607db75677ecb74bb38fdf7d77b295c
Olivier Buisard <olivier.buisard@simplifyyourweb.com>
2023-02-23 12:50:05 -0500

    Display function with step and tour edit check

--- administrator/components/com_guidedtours/src/Controller/DisplayController.php
+++ administrator/components/com_guidedtours/src/Controller/DisplayController.php
@@ -10,7 +10,9 @@
 
 namespace Joomla\Component\Guidedtours\Administrator\Controller;
 
+use Joomla\CMS\Language\Text;
 use Joomla\CMS\MVC\Controller\BaseController;
+use Joomla\CMS\Router\Route;
 
 // phpcs:disable PSR1.Files.SideEffects
 \defined('_JEXEC') or die;
@@ -35,15 +37,32 @@ class DisplayController extends BaseController
      * Method to display a view.
      *
      * @param   boolean $cachable  If true, the view output will be cached
-     * @param   array   $urlparams An array of safe URL parameters and their
-     * variable types, for valid values see {@link \JFilterInput::clean()}.
+     * @param   array   $urlparams An array of safe URL parameters and their variable types, for valid values see {@link \JFilterInput::clean()}.
      *
-     * @return static  This object to support chaining.
+     * @return  static |boolean  This object to support chaining. False on failure.
      *
-     * @since __DEPLOY_VERSION__
+     * @since   __DEPLOY_VERSION__
      */
     public function display($cachable = false, $urlparams = [])
     {
+        $view   = $this->input->get('view', $this->default_view);
+        $layout = $this->input->get('layout', 'default');
+        $id     = $this->input->getInt('id');
+
+        if ($view == 'tour' && $layout == 'edit' && !$this->checkEditId('com_guidedtours.edit.tour', $id)) {
+            $this->setMessage(Text::sprintf('JLIB_APPLICATION_ERROR_UNHELD_ID', $id), 'error');
+            $this->setRedirect(Route::_('index.php?option=com_guidedtour&view=tours', false));
+
+            return false;
+        }
+
+        if ($view == 'step' && $layout == 'edit' && !$this->checkEditId('com_guidedtours.edit.step', $id)) {
+            $this->setMessage(Text::sprintf('JLIB_APPLICATION_ERROR_UNHELD_ID', $id), 'error');
+            $this->setRedirect(Route::_('index.php?option=com_guidedtour&view=steps', false));
+
+            return false;
+        }
+
         return parent::display();
     }
 }
