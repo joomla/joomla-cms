e2e48f06351685ba690a151604e4df4996ceb3c0
Olivier Buisard <olivier.buisard@simplifyyourweb.com>
2023-02-23 16:04:47 -0500

    Re-introduce Bootstrap dropdown removed during refactoring

--- administrator/modules/mod_guidedtours/tmpl/default.php
+++ administrator/modules/mod_guidedtours/tmpl/default.php
@@ -10,7 +10,6 @@
 
 defined('_JEXEC') or die;
 
-use Joomla\CMS\Component\ComponentHelper;
 use Joomla\CMS\Factory;
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
@@ -19,6 +18,9 @@ use Joomla\CMS\Uri\Uri;
 if (!$tours) {
     return;
 }
+
+// Load the Bootstrap Dropdown
+HTMLHelper::_('bootstrap.dropdown', '.dropdown-toggle');
 
 $app = Factory::getApplication();
 $lang = $app->getLanguage();
