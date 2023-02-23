310d2f5fb29079b3607e6e2db4aaabe13281fb66
Olivier Buisard <olivier.buisard@simplifyyourweb.com>
2023-02-23 12:47:19 -0500

    Display function moved to DisplayController

--- administrator/components/com_guidedtours/src/Controller/StepsController.php
+++ administrator/components/com_guidedtours/src/Controller/StepsController.php
@@ -27,41 +27,6 @@ use Joomla\CMS\Router\Route;
 class StepsController extends AdminController
 {
     /**
-     * The default view.
-     *
-     * @var   string
-     * @since __DEPLOY_VERSION__
-     */
-    protected $default_view = 'steps';
-
-    /**
-     * Method to display a view.
-     *
-     * @param   boolean $cachable  If true, the view output will be cached
-     * @param   array   $urlparams An array of safe URL parameters and
-     * their variable types, for valid values see {@link \JFilterInput::clean()}.
-     *
-     * @return static  This object to support chaining.
-     *
-     * @since __DEPLOY_VERSION__
-     */
-    public function display($cachable = false, $urlparams = [])
-    {
-        $view   = $this->input->get('view', $this->defaultView);
-        $layout = $this->input->get('layout', 'default');
-        $id     = $this->input->getInt('id');
-
-        if ($view == 'step' && $layout == 'edit' && !$this->checkEditId('com_guidedtours.edit.step', $id)) {
-            $this->setMessage(Text::sprintf('JLIB_APPLICATION_ERROR_UNHELD_ID', $id), 'error');
-            $this->setRedirect(Route::_('index.php?option=com_guidedtour&view=steps', false));
-
-            return false;
-        }
-
-        return parent::display();
-    }
-
-    /**
      * Proxy for getModel.
      *
      * @param   string $name   The model name. Optional.
