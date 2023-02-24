9e8f4211bd883228bf7e83bade08d1f71d7a8d16
Sergey Tolkachyov <musstudent@mail.ru>
2023-02-24 17:50:52 +0400

    Rebuild mod_custom with new Joomla 4 structure

---
+++ modules/mod_custom/src/Dispatcher/Dispatcher.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  mod_custom
+ *
+ * @copyright   (C) 2023 Open Source Matters, Inc. <https://www.joomla.org>
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+namespace Joomla\Module\Custom\Site\Dispatcher;
+
+// phpcs:disable PSR1.Files.SideEffects
+\defined('JPATH_PLATFORM') or die;
+// phpcs:enable PSR1.Files.SideEffects
+
+use Joomla\CMS\Dispatcher\AbstractModuleDispatcher;
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Plugin\PluginHelper;
+
+/**
+ * Dispatcher class for mod_custom
+ *
+ * @since  4.0.0
+ */
+class Dispatcher extends AbstractModuleDispatcher
+{
+    /**
+     * Returns the layout data.
+     *
+     * @return  array
+     *
+     * @since   4.0.0
+     */
+    protected function getLayoutData()
+    {
+        $data = parent::getLayoutData();
+
+        if (($data['params'])->get('prepare_content', 1)) {
+            PluginHelper::importPlugin('content');
+            ($data['module'])->content = HTMLHelper::_('content.prepare', ($data['module'])->content, '', 'mod_custom.content');
+        }
+
+        return $data;
+    }
+}
