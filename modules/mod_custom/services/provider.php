b5128b954cfb76e4f18dab840aa7662c23bcf7b1
Sergey Tolkachyov <info@web-tolk.ru>
2023-02-24 22:09:02 +0400

    Rebuild mod_custom with new Joomla 4 structure

---
+++ modules/mod_custom/services/provider.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  mod_custom
+ *
+ * @copyright   (C) 2023 Open Source Matters, Inc. <https://www.joomla.org>
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// phpcs:disable PSR1.Files.SideEffects
+\defined('JPATH_PLATFORM') or die;
+// phpcs:enable PSR1.Files.SideEffects
+
+use Joomla\CMS\Extension\Service\Provider\HelperFactory;
+use Joomla\CMS\Extension\Service\Provider\Module;
+use Joomla\CMS\Extension\Service\Provider\ModuleDispatcherFactory;
+use Joomla\DI\Container;
+use Joomla\DI\ServiceProviderInterface;
+
+/**
+ * The module Custom HTML service provider.
+ *
+ * @since  1.0.0
+ */
+return new class implements ServiceProviderInterface
+{
+    /**
+     * Registers the service provider with a DI container.
+     *
+     * @param   Container  $container  The DI container.
+     *
+     * @return  void
+     *
+     * @since   4.0.0
+     */
+    public function register(Container $container)
+    {
+        $container->registerServiceProvider(new ModuleDispatcherFactory('\\Joomla\\Module\\Custom'));
+        $container->registerServiceProvider(new HelperFactory('\\Joomla\\Module\\Custom\\Site\\Helper'));
+        $container->registerServiceProvider(new Module());
+    }
+};
--- modules/mod_custom/tmpl/default.php
+++ modules/mod_custom/tmpl/default.php
@@ -4,15 +4,29 @@
  * @package     Joomla.Site
  * @subpackage  mod_custom
  *
- * @copyright   (C) 2009 Open Source Matters, Inc. <https://www.joomla.org>
+ * @copyright   (C) 2023 Open Source Matters, Inc. <https://www.joomla.org>
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
-defined('_JEXEC') or die;
+// phpcs:disable PSR1.Files.SideEffects
+\defined('JPATH_PLATFORM') or die;
+// phpcs:enable PSR1.Files.SideEffects
 
 use Joomla\CMS\Factory;
-use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Uri\Uri;
+use Joomla\CMS\HTML\HTMLHelper;
+/**
+ * @var \stdClass               $module   The module
+ * @var CMSApplicationInterface $app      The application instance
+ * @var Input                   $input    The input instance
+ * @var Registry                $params   Module params
+ * @var Registry                $template Template params
+ *
+ * And your own vars wich you can set in your module displatcher
+ * src/Dispatcher/Dispatcher.php in function getLayoutData().
+ * When you return $data['your_own_variable_name'] function getLayoutData() here
+ * you'll access to it via $your_own_variable_name
+ */
 
 $modId = 'mod-custom' . $module->id;
 
