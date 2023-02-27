4f846e1e109e65a3c6eb02aa3b72e6b82ce56a2a
Nicolas Derumigny <nderumigny@gmail.com>
2023-02-26 21:33:20 -0700

    Update libraries/src/Form/Field/ImagelistField.php
    Co-authored-by: Richard Fath <richard67@users.noreply.github.com>

--- libraries/src/Form/Field/ImagelistField.php
+++ libraries/src/Form/Field/ImagelistField.php
@@ -3,8 +3,8 @@
 /**
  * Joomla! Content Management System
  *
- * @copyright (C) 2010 Open Source Matters, Inc. <https://www.joomla.org>
- * @license   GNU General Public License version 2 or later; see LICENSE.txt
+ * @copyright  (C) 2010 Open Source Matters, Inc. <https://www.joomla.org>
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 namespace Joomla\CMS\Form\Field;
