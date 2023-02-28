94d5d73f0ae32701be95e3017361211d8b83e461
George Wilson <georgejameswilson@googlemail.com>
2023-02-28 19:50:03 +0000

    Fix relative urls not SEF not having a csrf token

--- build/media_source/system/js/core.es6.js
+++ build/media_source/system/js/core.es6.js
@@ -623,8 +623,9 @@ window.Joomla.Modal = window.Joomla.Modal || {
         const token = Joomla.getOptions('csrf.token', '');
 
         // Use the CSRF only on the site's domain
-        // eslint-disable-next-line no-restricted-globals
-        if (token && (newOptions.url.startsWith('/') || newOptions.url.startsWith(location.origin))) {
+        if (token
+          // eslint-disable-next-line no-restricted-globals
+          && (newOptions.url.startsWith('index.php') || newOptions.url.startsWith('/') || newOptions.url.startsWith(location.origin))) {
           xhr.setRequestHeader('X-CSRF-Token', token);
         }
 
