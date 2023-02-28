4304f2f450267471cf4c8e818a36fb627c8f10e8
Hannes Papenberg <info@joomlager.de>
2023-02-28 17:26:19 +0100

    Update components/com_finder/tmpl/search/default_result.php
    Co-authored-by: Quy <quy@nomonkeybiz.com>

--- components/com_finder/tmpl/search/default_result.php
+++ components/com_finder/tmpl/search/default_result.php
@@ -106,14 +106,11 @@ if ($this->params->get('show_url', 1)) {
     <?php if (count($taxonomies) && $this->params->get('show_taxonomy', 1)) : ?>
         <ul class="result__taxonomy">
             <?php foreach ($taxonomies as $type => $taxonomy) : ?>
-                <?php
-                if ($type == 'Language' && (!Multilanguage::isEnabled() || (isset($taxonomy[0]) && $taxonomy[0]->title == '*'))) :
-                    continue;
-                endif;
-
-                $branch = Taxonomy::getBranch($type);
-
-                if ($branch->state == 1 && in_array($branch->access, $user->getAuthorisedViewLevels())) : ?>
+                <?php if ($type == 'Language' && (!Multilanguage::isEnabled() || (isset($taxonomy[0]) && $taxonomy[0]->title == '*'))) : ?>
+                    <?php continue; ?>
+                <?php endif; ?>
+                <?php $branch = Taxonomy::getBranch($type); ?>
+                <?php if ($branch->state == 1 && in_array($branch->access, $user->getAuthorisedViewLevels())) : ?>
                     <?php $taxonomy_text = []; ?>
                     <?php foreach ($taxonomy as $node) : ?>
                         <?php if ($node->state == 1 && in_array($node->access, $user->getAuthorisedViewLevels())) : ?>
