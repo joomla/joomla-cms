abdfe0bc741fe34b4fa7e8ed1287b67a1ec9d1df
Sergey Tolkachyov <info@web-tolk.ru>
2023-03-01 09:06:01 +0400

    Update Dispatcher.php

--- modules/mod_custom/src/Dispatcher/Dispatcher.php
+++ modules/mod_custom/src/Dispatcher/Dispatcher.php
@@ -11,7 +11,7 @@
 namespace Joomla\Module\Custom\Site\Dispatcher;
 
 // phpcs:disable PSR1.Files.SideEffects
-\defined('_JEXEC') or die;
+\defined('JPATH_PLATFORM') or die;
 // phpcs:enable PSR1.Files.SideEffects
 
 use Joomla\CMS\Dispatcher\AbstractModuleDispatcher;
