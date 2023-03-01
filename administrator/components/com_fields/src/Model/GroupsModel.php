0d2bc56d28eeb333de5684e106cefb2685983ed2
Сергей <koreshs@mail.ru>
2023-03-01 19:21:45 +0300

    Update GroupsModel.php

--- administrator/components/com_fields/src/Model/GroupsModel.php
+++ administrator/components/com_fields/src/Model/GroupsModel.php
@@ -192,9 +192,18 @@ class GroupsModel extends ListModel
                 $query->where($db->quoteName('a.id') . ' = :search')
                     ->bind(':id', $search, ParameterType::INTEGER);
             } else {
+                $idsPrepareStrings = explode(' ', str_replace(',', ' ', trim($search)));
+                $ids               = array_filter($idsPrepareStrings, function ($number) {
+                    return is_numeric($number) && (int)$number > -1;
+                });
+
                 $search = '%' . str_replace(' ', '%', trim($search)) . '%';
                 $query->where($db->quoteName('a.title') . ' LIKE :search')
                     ->bind(':search', $search);
+
+                if ($ids) {
+                    $query->orWhere($db->quoteName('a.id') . ' IN (' . implode(',', $query->bindArray($ids)) . ')');
+                }
             }
         }
 
