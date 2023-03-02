ad5c4b5b1082c75bc83e5bcff9799fa69a24d1b7
Сергей <koreshs@mail.ru>
2023-03-02 15:26:47 +0300

    Update ArticlesModel.php

--- administrator/components/com_content/src/Model/ArticlesModel.php
+++ administrator/components/com_content/src/Model/ArticlesModel.php
@@ -470,8 +470,7 @@ class ArticlesModel extends ListModel
                     ->bind([':search1', ':search2', ':search3'], $searchLike);
 
                 // Search by ID without the prefix ID:, used numbers from the search.
-                $idsPrepare = str_replace(',', ' ', $search);
-                $idsPrepare = explode(' ', $idsPrepare);
+                $idsPrepare = explode(',', $idsPrepare);
                 $ids        = array_filter($idsPrepareStrings, function ($number) {
                     $number = trim($number);
                     return is_numeric($number) && (int)$number > -1;
