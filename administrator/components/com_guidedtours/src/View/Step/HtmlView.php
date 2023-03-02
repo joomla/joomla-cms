a29983d26a2a7dc4d4c5628f336b6d88212da7ee
Olivier Buisard <olivier.buisard@simplifyyourweb.com>
2023-03-01 19:58:51 -0500

    Removed occurences of Multilanguage

--- administrator/components/com_guidedtours/src/View/Step/HtmlView.php
+++ administrator/components/com_guidedtours/src/View/Step/HtmlView.php
@@ -12,7 +12,6 @@ namespace Joomla\Component\Guidedtours\Administrator\View\Step;
 
 use Joomla\CMS\Factory;
 use Joomla\CMS\Helper\ContentHelper;
-use Joomla\CMS\Language\Multilanguage;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\MVC\View\GenericDataException;
 use Joomla\CMS\MVC\View\HtmlView as BaseHtmlView;
