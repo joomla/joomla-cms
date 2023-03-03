4ef740cd7c913d90451947ac33d58050a6afe4b6
Olivier Buisard <olivier.buisard@simplifyyourweb.com>
2023-03-02 21:52:11 -0500

    Added loop through the modal links and buttons

--- administrator/modules/mod_guidedtours/tmpl/default.php
+++ administrator/modules/mod_guidedtours/tmpl/default.php
@@ -114,6 +114,34 @@ $modalCode = HTMLHelper::_('bootstrap.renderModal', 'modGuidedTours-modal', $mod
 $app->getDocument()->getWebAssetManager()->addInlineScript("
 document.addEventListener('DOMContentLoaded', function() {
     document.body.insertAdjacentHTML('beforeend', " . json_encode($modalCode) . ");
-    var modal = document.getElementById('modGuidedTours-modal');
+    const modal = document.getElementById('modGuidedTours-modal');
+
+    // add all the elements inside modal which you want to make focusable
+    const focusableElements = 'button, [href]';
+    const firstFocusableElement = modal.querySelectorAll(focusableElements)[0]; // get first element to be focused inside modal
+    const focusableContent = modal.querySelectorAll(focusableElements);
+    const lastFocusableElement = focusableContent[focusableContent.length - 1]; // get last element to be focused inside modal
+
+    document.addEventListener('keydown', function(e) {
+      let isTabPressed = e.key === 'Tab' || e.keyCode === 9;
+
+      if (!isTabPressed) {
+        return;
+      }
+
+      if (e.shiftKey) { // if shift key pressed for shift + tab combination
+        if (document.activeElement === firstFocusableElement) {
+          lastFocusableElement.focus(); // add focus for the last focusable element
+          e.preventDefault();
+        }
+      } else { // if tab key is pressed
+        if (document.activeElement === lastFocusableElement) { // if focused has reached to last focusable element then focus first focusable element after pressing tab
+          firstFocusableElement.focus(); // add focus for the first focusable element
+          e.preventDefault();
+        }
+      }
+    });
+
+    firstFocusableElement.focus();
 });
 ");
