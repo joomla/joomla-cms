892dffce8932800e0fa0a869193738cb29ebb896
Allon Moritz <allon.moritz@digital-peak.com>
2023-03-06 17:44:48 +0100

    tests

--- tests/cypress/integration/site/components/com_content/Article.cy.js
+++ tests/cypress/integration/site/components/com_content/Article.cy.js
@@ -9,8 +9,16 @@ describe('Test view article on front page', () => {
   it('navigates to the article from the front page', function () {
     cy.db_createArticle({'title': 'automated test article'}).then((article) => {
         cy.visit('/');
-        cy.get('a').click();
-        cy.contains('automated test article', 'h2');
+        cy.get('.item-title a').click();
+        cy.contains('h2', 'automated test article');
+    });
+  });
+
+  it('navigates to the category from the front page', function () {
+    cy.db_createArticle({'title': 'automated test article'}).then((article) => {
+        cy.visit('/');
+        cy.get('.category-name a').click();
+        cy.contains('h1', 'Uncategorised');
     });
   });
 });
