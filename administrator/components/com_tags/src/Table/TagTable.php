2ca9256a4acbb930e2611233fd69f6d495b56509
Tuan Pham Ngoc <github@joomdonation.com>
2023-03-09 22:19:25 +0700

    CS

--- administrator/components/com_tags/src/Table/TagTable.php
+++ administrator/components/com_tags/src/Table/TagTable.php
@@ -191,7 +191,7 @@ class TagTable extends Nested implements VersionableTableInterface
         // Verify that the alias is unique
         $table = new static($this->getDbo());
 
-        if ($table->load(array('alias' => $this->alias)) && ($table->id != $this->id || $this->id == 0)) {
+        if ($table->load(['alias' => $this->alias]) && ($table->id != $this->id || $this->id == 0)) {
             $this->setError(Text::_('COM_TAGS_ERROR_UNIQUE_ALIAS'));
 
             // Is the existing tag trashed?
