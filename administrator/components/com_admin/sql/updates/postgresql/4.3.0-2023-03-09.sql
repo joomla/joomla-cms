ae157c26ee1364b1a54a339cf0687567e88db345
khu5h1 <khushirauniyar8@gmail.com>
2023-03-10 00:27:45 +0530

    updated where clause

--- administrator/components/com_admin/sql/updates/postgresql/4.3.0-2023-03-09.sql
+++ administrator/components/com_admin/sql/updates/postgresql/4.3.0-2023-03-09.sql
@@ -1,6 +1,6 @@
 UPDATE "#__guidedtour_steps"
 SET "target" = '#jform_published'
-WHERE "target" = '#jform_state';
+WHERE "target" = '#jform_state' and "id" = 87;
 
 UPDATE "#__guidedtour_steps"
 SET "target" = '#jform_sendEmail0'
