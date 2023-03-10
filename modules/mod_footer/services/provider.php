0be76c58ba218a690b58d83d2d1a50e9afca86ac
jsanders <j53.sanders@gmail.com>
2023-03-10 11:42:27 +0100

    Create provider.php

---
+++ modules/mod_footer/services/provider.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * @package     Joomla.Site
+ * @subpackage  mod_footer
+ *
+ * @copyright   (C) 2023 Open Source Matters, Inc. <https://www.joomla.org>
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// phpcs:disable PSR1.Files.SideEffects
+\defined('_JEXEC') or die;
+// phpcs:enable PSR1.Files.SideEffects
+
+use Joomla\CMS\Extension\Service\Provider\HelperFactory;
+use Joomla\CMS\Extension\Service\Provider\Module;
+use Joomla\CMS\Extension\Service\Provider\ModuleDispatcherFactory;
+use Joomla\DI\Container;
+use Joomla\DI\ServiceProviderInterface;
+
+/**
+ * The footer module service provider.
+ *
+ * @since  __DEPLOY_VERSION__
+ */
+return new class () implements ServiceProviderInterface {
+    /**
+     * Registers the service provider with a DI container.
+     *
+     * @param   Container  $container  The DI container.
+     *
+     * @return  void
+     *
+     * @since   __DEPLOY_VERSION__
+     */
+    public function register(Container $container)
+    {
+        $container->registerServiceProvider(new ModuleDispatcherFactory('\\Joomla\\Module\\Footer'));
+        $container->registerServiceProvider(new HelperFactory('\\Joomla\\Module\\Footer\\Site\\Helper'));
+
+        $container->registerServiceProvider(new Module());
+    }
+};
