0d0552dfacf8c5e6217125474631372729c05fcb
Olivier Buisard <olivier.buisard@simplifyyourweb.com>
2023-03-11 11:01:19 -0500

    Update StepModel.php
    Removed StringHelper

--- administrator/components/com_guidedtours/src/Model/StepModel.php
+++ administrator/components/com_guidedtours/src/Model/StepModel.php
@@ -15,7 +15,6 @@ use Joomla\CMS\Language\Text;
 use Joomla\CMS\MVC\Model\AdminModel;
 use Joomla\CMS\Object\CMSObject;
 use Joomla\CMS\Table\Table;
-use Joomla\String\StringHelper;
 
 // phpcs:disable PSR1.Files.SideEffects
 \defined('_JEXEC') or die;
