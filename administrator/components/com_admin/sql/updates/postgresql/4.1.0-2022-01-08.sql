171f1cb41301d5a5c6a2c682fcbd69af0578a3c5
Richard Fath <richard67@users.noreply.github.com>
2023-03-11 19:22:32 +0100

    [5.0] Update deleted files and folders and renamed files for 5.0 and remove 4.x update SQL scripts (#40083)
    * Init deleted files and folders for 5.0
    
    * Add deleted files and folders from #38406
    
    * Add deleted file from #38405
    
    * Add deleted files and folders from #39134
    
    * Init renamed files for 5.0
    
    * Add deleted and renamed files and folders from dependency updates
    
    * Fix alpha odering of deleted files
    
    * Remove j4 update SQL scripts and add initial one for j5
    
    * Use real DDL in update SQL script

--- administrator/components/com_admin/sql/updates/postgresql/4.1.0-2022-01-08.sql
+++
@@ -1,3 +0,0 @@
-UPDATE "#__mail_templates"
-   SET "params" = '{"tags": ["task_id", "task_title"]}'
- WHERE "template_id"= 'plg_system_tasknotification.orphan_mail' AND "params" = '{"tags": ["task_id", "task_title", ""]}';
