4e786b35e6b89f35d6b31b3f3aeabbdadf1c7dc1
Allon Moritz <allon.moritz@digital-peak.com>
2023-03-13 14:26:34 +0100

    Add mod_articles_news system test

---
+++ tests/cypress/integration/site/modules/mod_articles_news/Default.cy.js
@@ -0,0 +1,17 @@
+afterEach(() => {
+  cy.task('queryDB', 'DELETE FROM #__content');
+  cy.task('queryDB', 'DELETE FROM #__content_frontpage');
+  cy.task('queryDB', 'DELETE FROM #__modules where module = \'mod_articles_news\'');
+});
+
+describe('Test that the news module', () => {
+  it('can load in frontend and showing the title of the articles', () => {
+    cy.db_createArticle({ title: 'automated test article' })
+      .then(() => cy.db_createModule({ module: 'mod_articles_news', params: JSON.stringify({ item_title: 1, item_heading: 'h3' }) }))
+      .then(() => {
+        cy.visit('/');
+
+        cy.contains('h3', 'automated test article');
+      });
+  });
+});
