3783474c836abc4f5262fc4cb9ae7df929a1f35c
Allon Moritz <allon.moritz@digital-peak.com>
2023-03-14 14:28:50 +0100

    better description

--- tests/cypress/integration/site/modules/mod_footer/Default.cy.js
+++ tests/cypress/integration/site/modules/mod_footer/Default.cy.js
@@ -1,5 +1,5 @@
-describe('Test that the news module', () => {
-  it('can load in frontend and showing the title of the articles', () => {
+describe('Test that the footer module', () => {
+  it('can load in frontend and shows the copyright message from Joomla', () => {
     cy.db_createModule({ module: 'mod_footer' }).then(() => {
       cy.visit('/');
 
