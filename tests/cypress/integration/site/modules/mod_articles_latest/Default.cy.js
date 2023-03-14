dfb459ebc83bad8b2e3fd2728e5b8f88e2365362
Allon Moritz <allon.moritz@digital-peak.com>
2023-03-14 11:05:30 +0100

    cs

--- tests/cypress/integration/site/modules/mod_articles_latest/Default.cy.js
+++ tests/cypress/integration/site/modules/mod_articles_latest/Default.cy.js
@@ -1,7 +1,7 @@
 describe('Test that the latest articles module', () => {
   it('can load in frontend without ordering parameter', () => {
     cy.db_createArticle({ title: 'automated test article' })
-      .then(() => cy.db_createModule({ module: 'mod_articles_latest'}))
+      .then(() => cy.db_createModule({ module: 'mod_articles_latest' }))
       .then(() => {
         cy.visit('/');
 
