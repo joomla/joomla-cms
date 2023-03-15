ef5d5a08fb9a920418459ff63316cc2f0d515a0a
jsanders <j53.sanders@gmail.com>
2023-03-15 12:19:24 +0100

    replace code

--- tests/cypress/integration/site/modules/mod_users_latest/Default.cy.js
+++ tests/cypress/integration/site/modules/mod_users_latest/Default.cy.js
@@ -1,9 +1,11 @@
 describe('Test that the users latest module', () => {
   it('can load in frontend and displays the latest registered users', () => {
-    cy.db_createModule({ module: 'mod_users_latest' }).then(() => {
-      cy.visit('/');
+    cy.db_createUser({ username: 'automatedtestuser' })
+      .then(() => cy.db_createModule({ module: 'mod_users_latest' }))
+      .then(() => {
+        cy.visit('/');
 
-      cy.contains('Latest Registered Users');
-    });
+        cy.contains('automatedtestuser');
+      });
   });
 });
