4a1881b936f52ba82a8cf8131927725aabc58368
Allon Moritz <allon.moritz@digital-peak.com>
2023-03-16 13:56:13 +0100

    Remove codeception

--- tests/cypress/integration/api/com_media/Files.cy.js
+++ tests/cypress/integration/api/com_media/Files.cy.js
@@ -1,3 +1,8 @@
+// Ensure test dir is available and has correct permissions
+beforeEach(() => cy.task('writeFile', { path: 'images/test-dir/dummy.txt', content: '1' }));
+
+afterEach(() => cy.task('deleteFolder', 'images/test-dir'));
+
 describe('Test that media files API endpoint', () => {
   it('can deliver a list of files', () => {
     cy.api_get('/media/files')
@@ -38,5 +43,101 @@ describe('Test that media files API endpoint', () => {
   it('can deliver a single folder', () => {
     cy.api_get('/media/files/sampledata/cassiopeia')
       .then((response) => cy.wrap(response).its('body').its('data').its('attributes').its('name').should('include', 'cassiopeia'));
+  });
+
+  it('can create a file without adapter', () => {
+    cy.readFile('tests/cypress/data/com_media/test-image-1.jpg', 'binary')
+      .then((data) => cy.api_post('/media/files', { path: 'test-dir/test.jpg', content: Buffer.from(data, 'binary').toString('base64') }))
+      .then((response) => {
+        cy.wrap(response).its('body').its('data').its('attributes').its('name').should('include', 'test.jpg');
+        cy.wrap(response).its('body').its('data').its('attributes').its('path').should('include', 'local-images:/test-dir/test.jpg');
+      });
+  });
+
+  it('can create a folder without adapter', () => {
+    cy.api_post('/media/files', { path: 'test-dir/test-from-create' })
+      .then((response) => {
+        cy.wrap(response).its('body').its('data').its('attributes').its('name').should('include', 'test-from-create');
+        cy.wrap(response).its('body').its('data').its('attributes').its('path').should('include', 'local-images:/test-dir/test-from-create');
+      });
+  });
+
+  it('can create a file with adapter', () => {
+    cy.readFile('tests/cypress/data/com_media/test-image-1.jpg', 'binary')
+      .then((data) => cy.api_post('/media/files', { path: 'local-images:/test-dir/test.jpg', content: Buffer.from(data, 'binary').toString('base64') }))
+      .then((response) => {
+        cy.wrap(response).its('body').its('data').its('attributes').its('name').should('include', 'test.jpg');
+        cy.wrap(response).its('body').its('data').its('attributes').its('path').should('include', 'local-images:/test-dir/test.jpg');
+      });
+  });
+
+  it('can create a folder with adapter', () => {
+    cy.api_post('/media/files', { path: 'local-images:/test-dir/test-from-create' })
+      .then((response) => {
+        cy.wrap(response).its('body').its('data').its('attributes').its('name').should('include', 'test-from-create');
+        cy.wrap(response).its('body').its('data').its('attributes').its('path').should('include', 'local-images:/test-dir/test-from-create');
+      });
+  });
+
+  it('can update a file without adapter', () => {
+    cy.task('writeFile', { path: 'images/test-dir/override.jpg', content: '1' })
+      .then(() => cy.readFile('tests/cypress/data/com_media/test-image-1.jpg', 'binary'))
+      .then((data) => cy.api_patch(
+        '/media/files/test-dir/override.jpg',
+        { path: 'test-dir/override.jpg', content: Buffer.from(data, 'binary').toString('base64') })
+      ).then((response) => {
+        cy.wrap(response).its('body').its('data').its('attributes').its('name').should('include', 'override.jpg');
+        cy.wrap(response).its('body').its('data').its('attributes').its('path').should('include', 'local-images:/test-dir/override.jpg');
+      });
+  });
+
+  it('can update a folder without adapter', () => {
+    cy.task('writeFile', { path: 'images/test-dir/override/test.jpg', content: '1' })
+      .then(() => cy.api_patch('/media/files/test-dir/override', { path: 'test-dir/override-new' }))
+      .then((response) => {
+        cy.wrap(response).its('body').its('data').its('attributes').its('name').should('include', 'override-new');
+        cy.wrap(response).its('body').its('data').its('attributes').its('path').should('include', 'local-images:/test-dir/override-new');
+      });
+  });
+
+  it('can update a file with adapter', () => {
+    cy.task('writeFile', { path: 'images/test-dir/override.jpg', content: '1' })
+      .then(() => cy.readFile('tests/cypress/data/com_media/test-image-1.jpg', 'binary'))
+      .then((data) => cy.api_patch(
+        '/media/files/local-images:/test-dir/override.jpg',
+        { path: 'local-images:/test-dir/override.jpg', content: Buffer.from(data, 'binary').toString('base64') })
+      ).then((response) => {
+        cy.wrap(response).its('body').its('data').its('attributes').its('name').should('include', 'override.jpg');
+        cy.wrap(response).its('body').its('data').its('attributes').its('path').should('include', 'local-images:/test-dir/override.jpg');
+      });
+  });
+
+  it('can update a folder with adapter', () => {
+    cy.task('writeFile', { path: 'images/test-dir/override/test.jpg', content: '1' })
+      .then(() => cy.api_patch('/media/files/local-images:/test-dir/override', { path: 'local-images:/test-dir/override-new' }))
+      .then((response) => {
+        cy.wrap(response).its('body').its('data').its('attributes').its('name').should('include', 'override-new');
+        cy.wrap(response).its('body').its('data').its('attributes').its('path').should('include', 'local-images:/test-dir/override-new');
+      });
+  });
+
+  it('can delete a file without adapter', () => {
+    cy.task('writeFile', { path: 'images/test-dir/todelete.jpg', content: '1' })
+      .then(() => cy.api_delete( '/media/files/test-dir/todelete.jpg' ));
+  });
+
+  it('can delete a folder without adapter', () => {
+    cy.task('writeFile', { path: 'images/test-dir/todelete/dummy.txt', content: '1' })
+      .then(() => cy.api_delete( '/media/files/test-dir/todelete' ));
+  });
+
+  it('can delete a file with adapter', () => {
+    cy.task('writeFile', { path: 'images/test-dir/todelete.jpg', content: '1' })
+      .then(() => cy.api_delete( '/media/files/local-images:/test-dir/todelete.jpg' ));
+  });
+
+  it('can delete a folder with adapter', () => {
+    cy.task('writeFile', { path: 'images/test-dir/todelete/dummy.txt', content: '1' })
+      .then(() => cy.api_delete( '/media/files/local-images:/test-dir/todelete' ));
   });
 });
