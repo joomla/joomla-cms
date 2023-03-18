dcf4e1c5dd25ef270ddc87380657be6333d5989e
Allon Moritz <allon.moritz@digital-peak.com>
2023-03-18 14:42:03 +0100

    delete tasks

--- administrator/components/com_admin/sql/updates/mysql/5.0.0-2023-03-17.sql
+++ administrator/components/com_admin/sql/updates/mysql/5.0.0-2023-03-17.sql
@@ -1,1 +1,2 @@
-DELETE FROM `#__extensions` WHERE name = 'plg_task_demotasks';
+DELETE FROM `#__scheduler_tasks` WHERE `type` = 'demoTask_r1.sleep';
+DELETE FROM `#__extensions` WHERE `type` = 'plugin' AND `element` = 'demotasks' AND `folder` = 'task' AND `client_id` = 0;
--- administrator/components/com_admin/sql/updates/postgresql/5.0.0-2023-03-17.sql
+++ administrator/components/com_admin/sql/updates/postgresql/5.0.0-2023-03-17.sql
@@ -1,1 +1,2 @@
+DELETE FROM "#__scheduler_tasks" WHERE "type" = 'demoTask_r1.sleep';
 DELETE FROM "#__extensions" WHERE "type" = 'plugin' AND "element" = 'demotasks' AND "folder" = 'task' AND "client_id" = 0;
