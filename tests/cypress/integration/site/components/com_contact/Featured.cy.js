e67b22d4ebd1e3e9c4ab77180e651d691a509fe8
rajputanuj31 <115709571+rajputanuj31@users.noreply.github.com>
2023-03-20 08:22:40 +0530

    update_code_style

--- tests/cypress/integration/site/components/com_contact/Featured.cy.js
+++ tests/cypress/integration/site/components/com_contact/Featured.cy.js
@@ -1,10 +1,10 @@
 describe('Test that the front page', () => {
-    it('can display featured contact', () => {
-      cy.db_createContact({ name: 'automated test contact 1' })
+  it('can display featured contact', () => {
+    cy.db_createContact({ name: 'automated test contact 1' })
       .then(() => cy.db_createContact({ name: 'automated test contact 2' }))
       .then(() => cy.db_createContact({ name: 'automated test contact 3' }))
       .then(() => cy.db_createContact({ name: 'automated test contact 4' }))
-      .then(() => cy.db_createMenuItem({ title: 'automated test Contact', link: 'index.php?option=com_contact&view=featured' ,path:'?option=com_contact&view=featured' }))
+      .then(() => cy.db_createMenuItem({ title: 'automated test Contact', link: 'index.php?option=com_contact&view=featured', path: '?option=com_contact&view=featured' }))
       .then(() => {
         cy.visit('/');
         cy.get('a:contains(automated test Contact)').click();
@@ -14,7 +14,5 @@ describe('Test that the front page', () => {
         cy.contains('automated test contact 3');
         cy.contains('automated test contact 4');
       });
-    });
-
   });
-  
+});
