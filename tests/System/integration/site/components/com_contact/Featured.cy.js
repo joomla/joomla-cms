cb2b36330acc51ff15c3f63321b2cabaac386d09
rajputanuj31 <115709571+rajputanuj31@users.noreply.github.com>
2023-03-22 14:31:48 +0530

    not featured test

--- tests/System/integration/site/components/com_contact/Featured.cy.js
+++ tests/System/integration/site/components/com_contact/Featured.cy.js
@@ -1,4 +1,4 @@
-describe('Test that the front page', () => {
+describe('Test that the featured contacts menu item type', () => {
   it('can display featured contact', () => {
     cy.db_createContact({ name: 'automated test contact 1', featured: 1 })
       .then(() => cy.db_createContact({ name: 'automated test contact 2', featured: 1 }))
@@ -13,6 +13,17 @@ describe('Test that the front page', () => {
         cy.contains('automated test contact 2');
         cy.contains('automated test contact 3');
         cy.contains('automated test contact 4');
+      });
+  });
+  it('can not display not featured contacts', () => {
+    cy.db_createContact({ name: 'automated test contact 1', featured: 0 })
+    .then(() => cy.db_createContact({ name: 'automated test contact 2', featured: 0 }))
+      .then(() => cy.db_createMenuItem({ title: 'automated test', link: 'index.php?option=com_contact&view=featured', path: '?option=com_contact&view=featured' }))
+      .then(() => {
+        cy.visit('/');
+        cy.get('a:contains(automated test)').click();
+        cy.contains('automated test contact 1').should('not.exist');
+        cy.contains('automated test contact 2').should('not.exist');
       });
   });
 });
