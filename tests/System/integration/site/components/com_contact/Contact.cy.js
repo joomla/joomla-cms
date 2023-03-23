59d36086a6537a78b8d29b7302bbc0bd45bb6cf8
rajputanuj31 <115709571+rajputanuj31@users.noreply.github.com>
2023-03-23 23:05:00 +0530

    Update Contact.cy.js

--- tests/System/integration/site/components/com_contact/Contact.cy.js
+++ tests/System/integration/site/components/com_contact/Contact.cy.js
@@ -2,8 +2,8 @@ describe('Test that the Contact Form', () => {
   it('can display a Contact Form', () => {
     cy.db_getUserId().then((id) => {
       cy.db_createContact({ name: 'contact 1', user_id: id, featured: 1 });
-    }).then((ContactId) => {
-      cy.visit(`index.php?option=com_contact&view=contact&id='${ContactId}'`);
+    }).then((contactId) => {
+      cy.visit(`index.php?option=com_contact&view=contact&id='${contactId}'`);
 
       cy.contains('Contact Form');
       cy.get('.m-0').should('exist');
