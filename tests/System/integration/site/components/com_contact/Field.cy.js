02d7bd11ed10b7dc5ba207de2eed54c8d0d77f9a
rajputanuj31 <115709571+rajputanuj31@users.noreply.github.com>
2023-03-25 14:32:41 +0530

    changes

--- tests/System/integration/site/components/com_contact/Field.cy.js
+++ tests/System/integration/site/components/com_contact/Field.cy.js
@@ -1,16 +1,14 @@
 describe('Test that the Contact Field', () => {
-  it('can display a added field', () => {
+  it('can display an added field', () => {
     cy.db_createFieldGroup({ title: 'automated test field group', context: 'com_contact.mail' })
-      .then((id) => {
-        cy.db_createField({
-          group_id: id, context: 'com_contact.mail', type: 'checkboxes', fieldparams: '{"options":{"options0":{"name":"Facebook","value":""}}}',
-        });
-      });
-    cy.db_getUserId().then((id) => cy.db_createContact({ name: 'automated test contact 1', user_id: id }))
+      .then((id) => cy.db_createField({
+        group_id: id, context: 'com_contact.mail', type: 'checkboxes', fieldparams: JSON.stringify({ options: { options0: { name: 'test value', value: '' } } }),
+      })
+        .then(() => cy.db_getUserId().then((userId) => cy.db_createContact({ name: 'automated test contact 1', user_id: userId }))))
       .then((contactId) => cy.visit(`index.php?option=com_contact&view=contact&id='${contactId}'`));
 
     cy.contains('automated test field group');
     cy.get(':nth-child(2) > .control-group > .control-label').contains('test field');
-    cy.get('#jform_com_fields_test_field').contains('Facebook');
+    cy.get('#jform_com_fields_test_field').contains('test value');
   });
 });
