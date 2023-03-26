8c7ced5e1f739c4344a878db06a6078f757ba04d
rajputanuj31 <115709571+rajputanuj31@users.noreply.github.com>
2023-03-26 18:36:04 +0530

    updates

--- tests/System/integration/site/components/com_contact/Field.cy.js
+++ tests/System/integration/site/components/com_contact/Field.cy.js
@@ -3,12 +3,15 @@ describe('Test that the Contact Field', () => {
     cy.db_createFieldGroup({ title: 'automated test field group', context: 'com_contact.mail' })
       .then((id) => cy.db_createField({
         group_id: id, context: 'com_contact.mail', type: 'checkboxes', fieldparams: JSON.stringify({ options: { options0: { name: 'test value', value: '' } } }),
-      })
-        .then(() => cy.db_getUserId().then((userId) => cy.db_createContact({ name: 'automated test contact 1', user_id: userId }))))
-      .then((contactId) => cy.visit(`index.php?option=com_contact&view=contact&id='${contactId}'`));
+      }))
+      .then(() => cy.db_getUserId())
+      .then((userId) => cy.db_createContact({ name: 'automated test contact 1', user_id: userId }))
+      .then((contactId) => {
+        cy.visit(`index.php?option=com_contact&view=contact&id='${contactId}'`);
 
-    cy.contains('automated test field group');
-    cy.get(':nth-child(2) > .control-group > .control-label').contains('test field');
-    cy.get('#jform_com_fields_test_field').contains('test value');
+        cy.contains('automated test field group');
+        cy.get(':nth-child(2) > .control-group > .control-label').contains('test field');
+        cy.get('#jform_com_fields_test_field').contains('test value');
+      });
   });
 });
