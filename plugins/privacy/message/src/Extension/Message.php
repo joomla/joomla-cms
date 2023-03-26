bfe7eb9689cfd31fe70775c928ab7db10d4c7809
Allon Moritz <allon.moritz@digital-peak.com>
2023-03-26 15:55:13 +0200

    Update plugins/privacy/message/src/Extension/Message.php
    Co-authored-by: heelc29 <66922325+heelc29@users.noreply.github.com>

--- plugins/privacy/message/src/Extension/Message.php
+++ plugins/privacy/message/src/Extension/Message.php
@@ -8,7 +8,7 @@
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
-namespace Joomla\Plugin\Privacy\Content\Extension;
+namespace Joomla\Plugin\Privacy\Message\Extension;
 
 use Joomla\CMS\User\User;
 use Joomla\Component\Privacy\Administrator\Plugin\PrivacyPlugin;
