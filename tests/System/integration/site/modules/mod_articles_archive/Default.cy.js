bebe264988afbf365f77089eeabd05ec18582a24
carlitorweb <crsanchez8646@gmail.com>
2023-03-30 19:23:58 -0900

    cy test

--- tests/System/integration/site/modules/mod_articles_archive/Default.cy.js
+++ tests/System/integration/site/modules/mod_articles_archive/Default.cy.js
@@ -1,11 +1,11 @@
 describe('Test the articles archive module', () => {
-  it('can load in frontend and showing the title of the categories', () => {
-    cy.db_createCategory({ title: 'automated test category', parent_id: '2' })
-      .then(() => cy.db_createModule({ module: 'mod_articles_categories', params: JSON.stringify({ parent: 2 }) }))
+  it('can load in frontend and showing a list of months with archived articles', () => {
+    cy.db_createArticle()
+      .then(() => cy.db_createModule({ module: 'mod_articles_archive' }))
       .then(() => {
         cy.visit('/');
 
-        cy.contains('li', 'automated test category');
+        cy.contains('li', 'January, 2023');
       });
   });
 });
