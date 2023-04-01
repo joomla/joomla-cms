768903feda539dd3b0abba4b422af6c7f499b807
Allon Moritz <allon.moritz@digital-peak.com>
2023-04-01 15:34:23 +0200

    Update tests/System/integration/plugins/media-action/resize/ResizePlugin.cy.js
    Co-authored-by: heelc29 <66922325+heelc29@users.noreply.github.com>

--- tests/System/integration/plugins/media-action/resize/ResizePlugin.cy.js
+++ tests/System/integration/plugins/media-action/resize/ResizePlugin.cy.js
@@ -1,6 +1,6 @@
 describe('Test that the resize media action plugin', () => {
   it('is shown when editing an image', () => {
-    cy.doAdministratorLogin(Cypress.env('username'), Cypress.env('password'));
+    cy.doAdministratorLogin();
     cy.visit('/administrator/index.php?option=com_media&view=file&mediatypes=0,1,2,3&path=local-images:/joomla_black.png');
     cy.get('button[role="tab"]:contains(Resize)').click();
 
