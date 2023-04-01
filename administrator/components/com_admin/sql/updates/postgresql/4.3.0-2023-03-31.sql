9e72fdd3622c189fbcdeb78a48ab35e1163d68b9
khu5h1 <khushirauniyar8@gmail.com>
2023-04-01 20:29:21 +0530

    Update sql scripts

--- administrator/components/com_admin/sql/updates/postgresql/4.3.0-2023-03-31.sql
+++ administrator/components/com_admin/sql/updates/postgresql/4.3.0-2023-03-31.sql
@@ -1,11 +1,3 @@
 UPDATE "#__guidedtour_steps"
-SET "target" = '.col-lg-9'
-WHERE "target" = '#jform_description,#jform_description_ifr' AND "id" = 63;
-
-UPDATE "#__guidedtour_steps"
-SET "title" = 'COM_GUIDEDTOURS_TOUR_BANNERS_STEP_DETAILS_TITLE'
-WHERE "title" = 'COM_GUIDEDTOURS_TOUR_BANNERS_STEP_DESCRIPTION_TITLE';
-
-UPDATE "#__guidedtour_steps"
-SET "description" = 'COM_GUIDEDTOURS_TOUR_BANNERS_STEP_DETAILS_DESCRIPTION'
-WHERE "description" = 'COM_GUIDEDTOURS_TOUR_BANNERS_STEP_DESCRIPTION_DESCRIPTION';
+SET "title" = 'COM_GUIDEDTOURS_TOUR_BANNERS_STEP_DETAILS_TITLE', "description" = 'COM_GUIDEDTOURS_TOUR_BANNERS_STEP_DETAILS_DESCRIPTION', "target" = '.col-lg-9'
+WHERE "id" = 63;
