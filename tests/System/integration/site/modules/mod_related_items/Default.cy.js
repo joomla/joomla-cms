fae9a089ede0f4b3e7a6177d0c403f5447176615
carlitorweb <crsanchez8646@gmail.com>
2023-03-31 20:15:09 -0900

    javascript cs

--- tests/System/integration/site/modules/mod_related_items/Default.cy.js
+++ tests/System/integration/site/modules/mod_related_items/Default.cy.js
@@ -1,8 +1,10 @@
 describe('Test the related items module', () => {
   it('can load in frontend showing a list of related articles based on the metakey field', () => {
-    cy.db_createArticle({title: 'Main Article', metakey: 'joomla' })
-      .then(() => cy.db_createArticle({title: 'article with joomla keyword', metakey: 'joomla' }))
-      .then(() => cy.db_createMenuItem({ title: 'Single article', alias: 'single-article', link: 'index.php?option=com_content&view=article&id=1', path: 'single-article'}))
+    cy.db_createArticle({ title: 'Main Article', metakey: 'joomla' })
+      .then(() => cy.db_createArticle({ title: 'article with joomla keyword', metakey: 'joomla' }))
+      .then(() => cy.db_createMenuItem({
+        title: 'Single article', alias: 'single-article', link: 'index.php?option=com_content&view=article&id=1', path: 'single-article',
+      }))
       .then(() => cy.db_createModule({ module: 'mod_related_items' }))
       .then(() => {
         cy.visit('/index.php/single-article');
