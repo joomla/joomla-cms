035fd6dd6e092648b384d76ea9f476501cb8798d
carlitorweb <crsanchez8646@gmail.com>
2023-03-31 20:30:27 -0900

    cy test without need article ID

--- tests/System/integration/site/modules/mod_related_items/Default.cy.js
+++ tests/System/integration/site/modules/mod_related_items/Default.cy.js
@@ -1,14 +1,12 @@
 describe('Test the related items module', () => {
   it('can load in frontend showing a list of related articles based on the metakey field', () => {
-    cy.db_createArticle({ title: 'Main Article', metakey: 'joomla' })
+    cy.db_createArticle({ title: 'Main Article', metakey: 'joomla', featured: 1 })
       .then(() => cy.db_createArticle({ title: 'article with joomla keyword', metakey: 'joomla' }))
-      .then(() => cy.db_createMenuItem({
-        title: 'Single article', alias: 'single-article', link: 'index.php?option=com_content&view=article&id=1', path: 'single-article',
-      }))
       .then(() => cy.db_createModule({ module: 'mod_related_items' }))
       .then(() => {
-        cy.visit('/index.php/single-article');
+        cy.visit('/');
 
+        cy.contains('a', 'Main Article').click();
         cy.contains('li', 'article with joomla keyword');
       });
   });
