7533baf5a64fea2cc79d9725aaa51bbbe666ab09
Hannes Papenberg <info@joomlager.de>
2023-04-03 23:00:44 +0200

    Reverting change to Factory Making Stream::upload() b/c

--- libraries/src/Filesystem/Stream.php
+++ libraries/src/Filesystem/Stream.php
@@ -9,6 +9,8 @@
 
 namespace Joomla\CMS\Filesystem;
 
+use Joomla\CMS\Object\LegacyErrorHandlingTrait;
+use Joomla\Filesystem\Exception\FilesystemException;
 use Joomla\Filesystem\Stream as FilesystemStream;
 
 // phpcs:disable PSR1.Files.SideEffects
@@ -32,4 +34,32 @@ use Joomla\Filesystem\Stream as FilesystemStream;
  */
 class Stream extends FilesystemStream
 {
+    use LegacyErrorHandlingTrait;
+
+    /**
+     * Upload a file
+     *
+     * @param   string    $src        The file path to copy from (usually a temp folder).
+     * @param   string    $dest       The file path to copy to.
+     * @param   resource  $context    A valid context resource (optional) created with stream_context_create.
+     * @param   boolean   $usePrefix  Controls the use of a prefix (optional).
+     * @param   boolean   $relative   Determines if the filename given is relative. Relative paths do not have JPATH_ROOT stripped.
+     *
+     * @return  mixed
+     *
+     * @since   1.7.0
+     * @deprecated  4.4 will be removed in 6.0
+     *              Use Joomla\Filesystem\Stream::upload() instead.
+     *              The framework class throws Exceptions in case of error which you have to catch.
+     */
+    public function upload($src, $dest, $context = null, $usePrefix = true, $relative = false)
+    {
+        try {
+            return parent::upload($src, $dest, $context, $usePrefix, $relative);
+        } catch (FilesystemException $e) {
+            $this->setError($e->getMessage());
+
+            return false;
+        }
+    }
 }
