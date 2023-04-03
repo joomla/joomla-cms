157f3b734909095e59464cb9e2a7c67d80b0717f
Allon Moritz <allon.moritz@digital-peak.com>
2023-04-03 15:09:12 +0200

    Revert not needed layout change

--- modules/mod_articles_categories/tmpl/default.php
+++ modules/mod_articles_categories/tmpl/default.php
@@ -18,5 +18,5 @@ if (!$list) {
 
 ?>
 <ul class="mod-articlescategories categories-module mod-list">
-    <?php require ModuleHelper::getLayoutPath('mod_articles_categories', $params->get('layout', 'default') . '_items'); ?>
+<?php require ModuleHelper::getLayoutPath('mod_articles_categories', $params->get('layout', 'default') . '_items'); ?>
 </ul>
