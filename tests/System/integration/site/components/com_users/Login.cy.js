0f415d82400fb6068ef26c6db48a057ffa2f482a
Charvi <charvimehra0007@gmail.com>
2023-04-04 15:27:39 +0530

    update

--- tests/System/integration/site/components/com_users/Login.cy.js
+++ tests/System/integration/site/components/com_users/Login.cy.js
@@ -1,17 +1,16 @@
-describe('Test the Login view for test user ', () => {
-  it('can login successfully for test user ', () => {
-    cy.db_createUser({
-      name: 'automated test user',
-      username: 'test',
-      email: 'test@example.com',
-      password: '098f6bcd4621d373cade4e832627b4f6',
-    }).then(() => {
-      cy.visit('/');
+describe('Test the login view',() => {
+  it('can display the login view for test user', () =>{
+    cy.db_createUser({ name: 'test user', username: 'test', email: 'test@example.com', password: '098f6bcd4621d373cade4e832627b4f6'})
+    .then(() => cy.db_createMenuItem({ title: 'login', link: 'index.php?option=com_users&view=login', path: '?option=com_users&view=login' }))
+    .then(() => {
+      cy.visit('http://localhost/joomla');
+      cy.get('.item-104 > a').click();
+
       cy.get('#username').type('test');
       cy.get('#password').type('test');
       cy.get('#remember').check();
       cy.get('.controls > .btn').click();
       cy.get('.alert-wrapper').should('contain.text', 'You have been logged in.');
     });
-  });
 });
+})
