b680726da29524ff0c8d00f234eb71e0f2ecdb20
Allon Moritz <allon.moritz@digital-peak.com>
2023-04-04 14:18:38 +0200

    cs

--- tests/System/integration/site/components/com_contact/Form.cy.js
+++ tests/System/integration/site/components/com_contact/Form.cy.js
@@ -7,7 +7,7 @@ describe('Test that the form view ', () => {
     cy.get('#jform_name').type('test contact 1');
     cy.get('.mb-2 > .btn-primary').click();
     cy.visit('index.php?option=com_contact&view=category&id=4');
-    
+
     cy.contains('test contact 1').should('exist');
-   });
+  });
 });
