5785bd7818117768d8c543dcf4f89b69d4968f6c
Charvi <charvimehra0007@gmail.com>
2023-04-05 01:52:55 +0530

    visiting the login page by the menu item

--- tests/System/integration/site/components/com_users/Login.cy.js
+++ tests/System/integration/site/components/com_users/Login.cy.js
@@ -15,4 +15,22 @@ describe('Test the login view', () => {
         cy.get('.alert-wrapper').should('contain.text', 'You have been logged in.');
       });
   });
+
+  it('testing log in for test user through menu item', () => {
+    cy.db_createUser({
+      name: 'test user', username: 'test', email: 'test@example.com', password: '098f6bcd4621d373cade4e832627b4f6',
+    })
+      .then(() => cy.db_createMenuItem({ title: 'login', link: 'index.php?option=com_users&view=login&id=1&layout=default', path: '?option=com_users&view=login&id=1&layout=default' }))
+      .then(() => {
+        cy.visit('/');
+
+        cy.get('a:contains(login)').click();
+        cy.get('#username').type('test');
+        cy.get('#password').type('test');
+        cy.get('#remember').check();
+        cy.get('.controls > .btn').click();
+
+        cy.get('.alert-wrapper').should('contain.text', 'You have been logged in.');
+      });
+  });
 });
