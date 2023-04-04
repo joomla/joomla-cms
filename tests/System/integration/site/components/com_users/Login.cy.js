30cd6909faa22fc4a2d80f34e53d38ec3a3725b4
Charvi <charvimehra0007@gmail.com>
2023-04-04 16:31:38 +0530

    update

--- tests/System/integration/site/components/com_users/Login.cy.js
+++ tests/System/integration/site/components/com_users/Login.cy.js
@@ -1,4 +1,4 @@
-describe('Test the log in view', () => {
+describe('Test the login view', () => {
   it('can log in for test user', () => {
     cy.db_createUser({
       name: 'test user', username: 'test', email: 'test@example.com', password: '098f6bcd4621d373cade4e832627b4f6' 
