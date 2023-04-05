8c8436951ed864edc0ae11c163069043f5327195
Allon Moritz <allon.moritz@digital-peak.com>
2023-04-05 17:09:47 +0200

    Test

---
+++ tests/Unit/Libraries/Cms/User/UserFactoryAwareTraitTest.php
@@ -0,0 +1,75 @@
+<?php
+
+/**
+ * @package     Joomla.UnitTest
+ * @subpackage  Base
+ *
+ * @copyright   (C) 2023 Open Source Matters, Inc. <https://www.joomla.org>
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+namespace Joomla\Tests\Unit\Libraries\Cms\User;
+
+use Joomla\CMS\User\UserFactoryAwareTrait;
+use Joomla\CMS\User\User;
+use Joomla\CMS\User\UserFactoryInterface;
+use Joomla\Tests\Unit\UnitTestCase;
+use UnexpectedValueException;
+
+/**
+ * Test class for \Joomla\CMS\User\UserFactoryAwareTrait
+ *
+ * @package     Joomla.UnitTest
+ * @subpackage  MVC
+ * @since       __DEPLOY_VERSION__
+ */
+class UserFactoryAwareTraitTest extends UnitTestCase
+{
+    /**
+     * @testdox  The user factory can be set and accessed by the trait
+     *
+     * @return  void
+     *
+     * @since   __DEPLOY_VERSION__
+     */
+    public function testGetSetUserFactory()
+    {
+        $userFactory = new class () implements UserFactoryInterface {
+            public function loadUserById(int $id): User
+            {
+                return new User();
+            }
+
+            public function loadUserByUsername(string $username): User
+            {
+                return new User();
+            }
+        };
+
+        $trait = new class () {
+            use UserFactoryAwareTrait;
+        };
+
+        $trait->setUserFactory($userFactory);
+
+        $this->assertEquals($userFactory, $trait->getUserFactory());
+    }
+
+    /**
+     * @testdox  The user factory can be set and accessed by the trait
+     *
+     * @return  void
+     *
+     * @since   __DEPLOY_VERSION__
+     */
+    public function testGetUserFactoryThrowsException()
+    {
+        $this->expectException(UnexpectedValueException::class);
+
+        $trait = new class () {
+            use UserFactoryAwareTrait;
+        };
+
+        $trait->getUserFactory();
+    }
+}
