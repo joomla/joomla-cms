6ff27cce03300f63f1f2b1a6e414aa1860c88a70
Allon Moritz <allon.moritz@digital-peak.com>
2023-04-05 11:15:49 +0200

    Adapt to path update

--- tests/System/integration/site/components/com_users/Login.cy.js
+++ tests/System/integration/site/components/com_users/Login.cy.js
@@ -3,7 +3,6 @@ describe('Test the login view', () => {
     cy.db_createUser({
       name: 'test user', username: 'test', email: 'test@example.com', password: '098f6bcd4621d373cade4e832627b4f6',
     })
-      .then(() => cy.db_createMenuItem({ title: 'login', link: 'index.php?option=com_users&view=login', path: '?option=com_users&view=login' }))
       .then(() => {
         cy.visit('index.php?option=com_users&view=login');
 
@@ -12,7 +11,7 @@ describe('Test the login view', () => {
         cy.get('#remember').check();
         cy.get('.controls > .btn').click();
 
-        cy.get('.alert-wrapper').should('contain.text', 'You have been logged in.');
+        cy.get('#system-message-container').should('contain.text', 'You have been logged in.');
       });
   });
 
@@ -20,17 +19,17 @@ describe('Test the login view', () => {
     cy.db_createUser({
       name: 'test user', username: 'test', email: 'test@example.com', password: '098f6bcd4621d373cade4e832627b4f6',
     })
-      .then(() => cy.db_createMenuItem({ title: 'login', link: 'index.php?option=com_users&view=login&id=1&layout=default', path: '?option=com_users&view=login&id=1&layout=default' }))
+      .then(() => cy.db_createMenuItem({ title: 'Automated test login', link: 'index.php?option=com_users&view=login' }))
       .then(() => {
         cy.visit('/');
 
-        cy.get('a:contains(login)').click();
+        cy.get('a:contains(Automated test login)').click();
         cy.get('#username').type('test');
         cy.get('#password').type('test');
         cy.get('#remember').check();
         cy.get('.controls > .btn').click();
 
-        cy.get('.alert-wrapper').should('contain.text', 'You have been logged in.');
+        cy.get('#system-message-container').should('contain.text', 'You have been logged in.');
       });
   });
 });
