29da3cc5b7f70fea25205cbddb2e34dc87557136
Charvi <charvimehra0007@gmail.com>
2023-04-08 02:15:32 +0530

    resolve code style issues

--- tests/System/integration/site/components/com_users/Reset.cy.js
+++ tests/System/integration/site/components/com_users/Reset.cy.js
@@ -1,8 +1,8 @@
-describe('Test the reset view',() => {
-  it('can display the reset view for test user', () =>{
+describe('Test the reset view', () => {
+  it('can display the reset view for test user', () => {
     cy.db_createUser({
       name: 'test user', username: 'test', email: 'test@example.com', password: '098f6bcd4621d373cade4e832627b4f6',
-      })
+    })
       .then(() => {
         cy.visit('index.php?option=com_users&view=reset');
 
@@ -10,13 +10,13 @@ describe('Test the reset view',() => {
         cy.get('.controls > .btn').click();
 
         cy.get('#system-message-container').should('contain.text', 'If the email address you entered is registered on this site you will shortly receive an email with a link to reset the password for your account.');
-    });
+      });
   });
- 
+
   it('testing reset form for test user through menu item', () => {
     cy.db_createUser({
       name: 'test user', username: 'test', email: 'test@example.com', password: '098f6bcd4621d373cade4e832627b4f6',
-      })
+    })
       .then(() => cy.db_createMenuItem({ title: 'Automated test reset', link: 'index.php?option=com_users&view=reset&id=1&layout=default', path: '?option=com_users&view=reset&id=1&layout=default' }))
       .then(() => {
         cy.visit('/');
@@ -26,6 +26,6 @@ describe('Test the reset view',() => {
         cy.get('.controls > .btn').click();
 
         cy.get('#system-message-container').should('contain.text', 'If the email address you entered is registered on this site you will shortly receive an email with a link to reset the password for your account.');
-    });
+      });
   });
-})
+});
