f30236e0f935963a8040f85a454da2644de6637f
rajputanuj31 <115709571+rajputanuj31@users.noreply.github.com>
2023-04-10 16:34:34 +0530

    add a test for newsfeed

---
+++ tests/System/integration/site/components/com_newsfeed/Feed.cy.js
@@ -0,0 +1,33 @@
+describe('Test that the feed view ', () => {
+  it('can display a list of feeds in menu item', () => {
+    cy.db_createNewsfeed({ name: 'automated test feed 1' })
+      .then(() => cy.db_createNewsfeed({ name: 'automated test feed 2' }))
+      .then(() => cy.db_createNewsfeed({ name: 'automated test feed 3' }))
+      .then(() => cy.db_createNewsfeed({ name: 'automated test feed 4' }))
+      .then(() => cy.db_createMenuItem({ title: 'automated test feeds', link: 'index.php?option=com_newsfeeds&view=category&id=5' }))
+      .then(() => {
+        cy.visit('/');
+        cy.get('a:contains(automated test feeds)').click();
+
+        cy.contains('automated test feed 1');
+        cy.contains('automated test feed 2');
+        cy.contains('automated test feed 3');
+        cy.contains('automated test feed 4');
+      });
+  });
+
+  it('can display a list of feeds without menu item', () => {
+    cy.db_createNewsfeed({ name: 'automated test feed 1' })
+      .then(() => cy.db_createNewsfeed({ name: 'automated test feed 2' }))
+      .then(() => cy.db_createNewsfeed({ name: 'automated test feed 3' }))
+      .then(() => cy.db_createNewsfeed({ name: 'automated test feed 4' }))
+      .then(() => {
+        cy.visit('index.php?option=com_newsfeeds&view=category&id=5');
+
+        cy.contains('automated test feed 1');
+        cy.contains('automated test feed 2');
+        cy.contains('automated test feed 3');
+        cy.contains('automated test feed 4');
+      });
+  });
+});
