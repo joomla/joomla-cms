9cfe67e4aed886a324fd19b68d71ca2375724459
Fedik <getthesite@gmail.com>
2023-04-10 16:36:06 +0300

    Articles Batch use Joomla Dialog

--- administrator/components/com_content/tmpl/articles/default_batch_body.php
+++ administrator/components/com_content/tmpl/articles/default_batch_body.php
@@ -13,6 +13,7 @@ defined('_JEXEC') or die;
 use Joomla\CMS\Component\ComponentHelper;
 use Joomla\CMS\Factory;
 use Joomla\CMS\Language\Multilanguage;
+use Joomla\CMS\Language\Text;
 use Joomla\CMS\Layout\LayoutHelper;
 
 $params = ComponentHelper::getParams('com_content');
@@ -59,3 +60,8 @@ $user = Factory::getUser();
         <?php endif; ?>
     </div>
 </div>
+ <div class="btn-toolbar p-2">
+     <joomla-toolbar-button task="article.batch" class="ms-auto">
+         <button type="button" class="btn btn-success"><?php echo Text::_('JGLOBAL_BATCH_PROCESS'); ?></button>
+     </joomla-toolbar-button>
+ </div>
