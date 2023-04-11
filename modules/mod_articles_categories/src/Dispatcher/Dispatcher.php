751561669be9ed2e77043e1b87ec5cba0f2ae973
carlitorweb <crsanchez8646@gmail.com>
2023-04-11 09:34:27 -0900

    cache

--- modules/mod_articles_categories/src/Dispatcher/Dispatcher.php
+++ modules/mod_articles_categories/src/Dispatcher/Dispatcher.php
@@ -13,6 +13,7 @@ namespace Joomla\Module\ArticlesCategories\Site\Dispatcher;
 use Joomla\CMS\Dispatcher\AbstractModuleDispatcher;
 use Joomla\CMS\Helper\HelperFactoryAwareInterface;
 use Joomla\CMS\Helper\HelperFactoryAwareTrait;
+use Joomla\CMS\Helper\ModuleHelper;
 
 // phpcs:disable PSR1.Files.SideEffects
 \defined('JPATH_PLATFORM') or die;
@@ -36,10 +37,18 @@ class Dispatcher extends AbstractModuleDispatcher implements HelperFactoryAwareI
      */
     protected function getLayoutData(): array
     {
-        $data = parent::getLayoutData();
+        $data   = parent::getLayoutData();
+        $params = $data['params'];
 
-        $data['list'] = $this->getHelperFactory()->getHelper('ArticlesCategoriesHelper')
-            ->getChildrenCategories($data['params'], $data['app']);
+        $cacheParams               = new \stdClass();
+        $cacheParams->cachemode    = 'id';
+        $cacheParams->class        = $this->getHelperFactory()->getHelper('ArticlesCategoriesHelper');
+        $cacheParams->method       = 'getChildrenCategories';
+        $cacheParams->methodparams = [$params, $data['app']];
+        $cacheParams->modeparams   = md5(serialize($this->module->id));
+
+        $data['list'] = ModuleHelper::moduleCache($this->module, $params, $cacheParams);
+
         $data['startLevel'] = $data['list'] ? reset($data['list'])->getParent()->level : null;
 
         return $data;
