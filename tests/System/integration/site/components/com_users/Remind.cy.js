cfc5ab89a284c8f0bdf29d78e3850ba80686cc81
Charvi <charvimehra0007@gmail.com>
2023-04-12 02:08:20 +0530

    code style change

--- tests/System/integration/site/components/com_users/Remind.cy.js
+++ tests/System/integration/site/components/com_users/Remind.cy.js
@@ -1,4 +1,4 @@
-describe('Test the remind view',() => {
+describe('Test the remind view', () => {
   it('can open the reminder form for test user through menu item', () => {
     cy.db_createUser({ name: 'test user', email: 'test@example.com' })
       .then(() => cy.db_createMenuItem({ title: 'Automated test reminder', link: 'index.php?option=com_users&view=remind' }))
@@ -13,8 +13,8 @@ describe('Test the remind view',() => {
       });
   });
 
-  it('can display the remind view for test user', () =>{
-    cy.db_createUser({ name: 'test user', email: 'test@example.com'})
+  it('can display the remind view for test user', () => {
+    cy.db_createUser({ name: 'test user', email: 'test@example.com' })
       .then(() => {
         cy.visit('index.php?option=com_users&view=remind');
 
@@ -24,4 +24,4 @@ describe('Test the remind view',() => {
         cy.get('#system-message-container').should('contain.text', 'If the email address you entered is registered on this site you will shortly receive an email with a reminder.');
       });
   });
-})
+});
