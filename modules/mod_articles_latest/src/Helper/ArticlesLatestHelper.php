76536ebfeeafba5623856eaddcd74e0dc5cd4e8d
Allon Moritz <allon.moritz@digital-peak.com>
2023-04-11 07:39:51 +0200

    Revert to boot

--- modules/mod_articles_latest/src/Helper/ArticlesLatestHelper.php
+++ modules/mod_articles_latest/src/Helper/ArticlesLatestHelper.php
@@ -156,9 +156,11 @@ class ArticlesLatestHelper implements DatabaseAwareInterface
      *
      * @since   1.6
      *
-     * @deprecated  4.3 will be removed in 6.0
-     *              Use the none static function getArticles
-     *              Example: (new ArticlesLatestHelper())->getArticles($params, Factory::getApplication());
+     * @deprecated 4.3 will be removed in 6.0
+     *             Use the non-static method getArticles
+     *             Example: Factory::getApplication()->bootModule('mod_articles_latest', 'site')
+     *                          ->getHelper('ArticlesLatestHelper')
+     *                          ->getArticles($params, Factory::getApplication())
      */
     public static function getList(Registry $params, ArticlesModel $model)
     {
