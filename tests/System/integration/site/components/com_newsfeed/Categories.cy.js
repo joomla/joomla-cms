b5a09c922de4dc62201e0e8cf0d3d6e1ed18d9cc
rajputanuj31 <115709571+rajputanuj31@users.noreply.github.com>
2023-04-11 17:38:19 +0530

    add a test for categories view of com_newsfeed

---
+++ tests/System/integration/site/components/com_newsfeed/Categories.cy.js
@@ -0,0 +1,19 @@
+describe('Test that the Catagories of com_newsfeeds ', () => {
+  it('can display a list of categories of newsfeeds', () => {
+    cy.db_createCategory({ title: 'automated test category 1', extension: 'com_newsfeeds' })
+      .then((id) => cy.db_createNewsfeed({ name: 'automated test feed 1', catid: id }))
+      .then(() => cy.db_createCategory({ title: 'automated test category 2', extension: 'com_newsfeeds' }))
+      .then(async (id) => {
+        await cy.db_createNewsfeed({ name: 'automated test feed 2', catid: id });
+        await cy.db_createNewsfeed({ name: 'automated test contact 3', catid: id });
+      })
+      .then(() => {
+        cy.visit('index.php?option=com_newsfeeds&view=categories');
+
+        cy.contains('automated test category 1');
+        cy.contains('automated test category 2');
+        cy.get(':nth-child(2) > .page-header > .badge').contains('# News feeds 1');
+        cy.get(':nth-child(3) > .page-header > .badge').contains('# News feeds 2');
+      });
+  });
+});
