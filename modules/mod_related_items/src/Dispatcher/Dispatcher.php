24a98b5b397eeaec732c1bea2aab7a359a34e218
carlitorweb <crsanchez8646@gmail.com>
2023-04-11 10:22:04 -0900

    cache

--- modules/mod_related_items/src/Dispatcher/Dispatcher.php
+++ modules/mod_related_items/src/Dispatcher/Dispatcher.php
@@ -13,6 +13,7 @@ namespace Joomla\Module\RelatedItems\Site\Dispatcher;
 use Joomla\CMS\Dispatcher\AbstractModuleDispatcher;
 use Joomla\CMS\Helper\HelperFactoryAwareInterface;
 use Joomla\CMS\Helper\HelperFactoryAwareTrait;
+use Joomla\CMS\Helper\ModuleHelper;
 
 // phpcs:disable PSR1.Files.SideEffects
 \defined('JPATH_PLATFORM') or die;
@@ -39,7 +40,14 @@ class Dispatcher extends AbstractModuleDispatcher implements HelperFactoryAwareI
         $data   = parent::getLayoutData();
         $params = $data['params'];
 
-        $data['list']     = $this->getHelperFactory()->getHelper('RelatedItemsHelper')->getRelatedArticles($params, $data['app']);
+        $cacheParams               = new \stdClass();
+        $cacheParams->cachemode    = 'safeuri';
+        $cacheParams->class        = $this->getHelperFactory()->getHelper('RelatedItemsHelper');
+        $cacheParams->method       = 'getRelatedArticles';
+        $cacheParams->methodparams = [$params, $data['app']];
+        $cacheParams->modeparams   = ['id' => 'int', 'Itemid' => 'int'];
+
+        $data['list']     = ModuleHelper::moduleCache($this->module, $params, $cacheParams);
         $data['showDate'] = $params->get('showDate', 0);
 
         return $data;
