118a7fb20918eb40a4f07ace167182cd5fc321a4
Allon Moritz <allon.moritz@digital-peak.com>
2023-04-12 17:28:51 +0200

    better wording

--- libraries/import.php
+++ libraries/import.php
@@ -21,7 +21,7 @@ trigger_error(
  * Set the platform root path as a constant if necessary.
  *
  * @deprecated __DEPLOY_VERSION__ will be removed in 6.0
- *             Use defined('_JEXEC') or die; if the CMS is correctly loaded
+ *             Use defined('_JEXEC') or die; to detect if the CMS is correctly loaded
  **/
 if (!defined('JPATH_PLATFORM')) {
     define('JPATH_PLATFORM', __DIR__);
