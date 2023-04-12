b8a417c06d859a62754926269b6b4c1d79afa23a
Allon Moritz <allon.moritz@digital-peak.com>
2023-04-12 22:26:00 +0200

    removing parent id

--- tests/System/integration/site/modules/mod_articles_categories/Default.cy.js
+++ tests/System/integration/site/modules/mod_articles_categories/Default.cy.js
@@ -1,7 +1,7 @@
 describe('Test the articles categories module', () => {
   it('can load in frontend and showing the title of the categories', () => {
-    cy.db_createCategory({ title: 'automated test category', parent_id: '2' })
-      .then(() => cy.db_createModule({ module: 'mod_articles_categories', params: JSON.stringify({ parent: 2 }) }))
+    cy.db_createCategory({ title: 'automated test category' })
+      .then(() => cy.db_createModule({ module: 'mod_articles_categories' }))
       .then(() => {
         cy.visit('/');
 
