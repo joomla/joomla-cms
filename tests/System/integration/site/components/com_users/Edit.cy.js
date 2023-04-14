70cc08a8b7628575046785c1752ed257d2e05a69
Charvi <charvimehra0007@gmail.com>
2023-04-14 18:30:12 +0530

    Improve test description

--- tests/System/integration/site/components/com_users/Edit.cy.js
+++ tests/System/integration/site/components/com_users/Edit.cy.js
@@ -1,12 +1,12 @@
-describe('Test the edit user profile view ', () => {
-  it('can display the edit profile form', () => {
+describe('Test that the users profile view ', () => {
+  it('can display a edit user profile form without a menu item', () => {
     cy.doFrontendLogin();
     cy.visit('index.php?option=com_users&view=profile&layout=edit');
 
     cy.get('#member-profile > :nth-child(1) > legend').should('contain.text', 'Edit Your Profile');
   });
 
-  it('testing edit user profile view through menu item', () => {
+  it('can display a edit user profile form in a menu item', () => {
     cy.doFrontendLogin();
     cy.db_createMenuItem({ title: 'Automated test edit', link: 'index.php?option=com_users&view=profile&layout=edit' });
     cy.visit('/');
