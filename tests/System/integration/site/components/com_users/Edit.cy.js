88f3a6ce8c883002bb981871e6216af2833769d4
Charvi <charvimehra0007@gmail.com>
2023-04-14 15:31:22 +0530

    Update tests/System/integration/site/components/com_users/Edit.cy.js

--- tests/System/integration/site/components/com_users/Edit.cy.js
+++ tests/System/integration/site/components/com_users/Edit.cy.js
@@ -1,16 +1,18 @@
-describe('Test the edit view ', () => {
+describe('Test the edit user profile view ', () => {
   it('Can display the edit profile form', () => {
     cy.doFrontendLogin();
-    cy.visit('http://localhost/joomlanew/index.php?option=com_users&view=profile&layout=edit');
+    cy.visit('index.php?option=com_users&view=profile&layout=edit');
+   
     cy.get('#member-profile > :nth-child(1) > legend').should('contain.text', 'Edit Your Profile');
   });
 
-  it('testing edit for test user through menu item', () => {
+  it('testing edit user profile view through menu item', () => {
     cy.doFrontendLogin();
     cy.db_createMenuItem({ title: 'Automated test edit', link: 'index.php?option=com_users&view=profile&layout=edit' });
-        cy.visit('http://localhost/joomlanew');
+        cy.visit('/');
 
         cy.get('a:contains(Automated test edit)').click();
+
         cy.get('#member-profile > :nth-child(1) > legend').should('contain.text', 'Edit Your Profile');
   });
 })
