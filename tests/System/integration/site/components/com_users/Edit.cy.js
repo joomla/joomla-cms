ce52c6858bfa8843ad33ab86c737a13150c13627
Charvi <charvimehra0007@gmail.com>
2023-04-14 15:46:55 +0530

    resolve code style issues

--- tests/System/integration/site/components/com_users/Edit.cy.js
+++ tests/System/integration/site/components/com_users/Edit.cy.js
@@ -2,17 +2,17 @@ describe('Test the edit user profile view ', () => {
   it('Can display the edit profile form', () => {
     cy.doFrontendLogin();
     cy.visit('index.php?option=com_users&view=profile&layout=edit');
-   
+
     cy.get('#member-profile > :nth-child(1) > legend').should('contain.text', 'Edit Your Profile');
   });
 
   it('testing edit user profile view through menu item', () => {
     cy.doFrontendLogin();
     cy.db_createMenuItem({ title: 'Automated test edit', link: 'index.php?option=com_users&view=profile&layout=edit' });
-        cy.visit('/');
+    cy.visit('/');
 
-        cy.get('a:contains(Automated test edit)').click();
+    cy.get('a:contains(Automated test edit)').click();
 
-        cy.get('#member-profile > :nth-child(1) > legend').should('contain.text', 'Edit Your Profile');
+    cy.get('#member-profile > :nth-child(1) > legend').should('contain.text', 'Edit Your Profile');
   });
-})
+});
