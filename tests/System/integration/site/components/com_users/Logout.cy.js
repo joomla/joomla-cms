6ff3adc1875ffcafd468a05f2d61004eeb83647f
Charvi <charvimehra0007@gmail.com>
2023-04-15 13:35:53 +0530

    test desc. change

--- tests/System/integration/site/components/com_users/Logout.cy.js
+++ tests/System/integration/site/components/com_users/Logout.cy.js
@@ -1,16 +1,15 @@
-describe('Test that the users logout view ', () => {
-  it('can logout the user without menu item', () => {
+describe('Test in frontend that the users logout view ', () => {
+  it('can log out the user without menu item', () => {
     cy.doFrontendLogin();
     cy.visit('index.php?option=com_users&view=login&layout=logout&task=user.menulogout');
 
     cy.get('#system-message-container').should('contain.text', 'You have been logged out.');
   });
 
-  it('can logout the user in a menu item', () => {
+  it('can log out the user in a menu item', () => {
     cy.doFrontendLogin();
     cy.db_createMenuItem({ title: 'Automated logout', link: 'index.php?option=com_users&view=login&layout=logout&task=user.menulogout' });
     cy.visit('/');
-
     cy.get('a:contains(Automated logout)').click();
 
     cy.get('#system-message-container').should('contain.text', 'You have been logged out.');
