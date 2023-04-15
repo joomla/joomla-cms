4a1d4f2dd31a0c07c6736b0afbcb95a44f24ba53
jsanders <j53.sanders@gmail.com>
2023-04-15 14:34:35 +0200

    Improve test description
    In accordance with #40386

--- tests/System/integration/site/modules/mod_breadcrumbs/Default.cy.js
+++ tests/System/integration/site/modules/mod_breadcrumbs/Default.cy.js
@@ -1,5 +1,5 @@
-describe('Test that the breadcrumbs module', () => {
-  it('can load in frontend and displays breadcrumb items', () => {
+describe('Test in frondend that the breadcrumbs module', () => {
+  it('can display the breadcrumb items', () => {
     cy.db_createModule({ module: 'mod_breadcrumbs' }).then(() => {
       cy.visit('/');
 
