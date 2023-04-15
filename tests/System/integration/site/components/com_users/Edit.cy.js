6d933c8c7e2497c0ae9eaa083a8c9eddbe2d410a
Charvi <charvimehra0007@gmail.com>
2023-04-16 02:49:08 +0530

    Adding code into a .then block

--- tests/System/integration/site/components/com_users/Edit.cy.js
+++ tests/System/integration/site/components/com_users/Edit.cy.js
@@ -7,11 +7,11 @@ describe('Test that the users profile view', () => {
   });
 
   it('can display an edit user profile form in a menu item', () => {
-    cy.doFrontendLogin();
-    cy.db_createMenuItem({ title: 'Automated test edit', link: 'index.php?option=com_users&view=profile&layout=edit' });
-    cy.visit('/');
-
-    cy.get('a:contains(Automated test edit)').click();
+    cy.db_createMenuItem({ title: 'Automated test edit', link: 'index.php?option=com_users&view=profile&layout=edit' })
+      .then(() => {
+        cy.doFrontendLogin();
+        cy.visit('/');
+        cy.get('a:contains(Automated test edit)').click();
 
         cy.get('#member-profile > :nth-child(1) > legend').should('contain.text', 'Edit Your Profile');
       });
