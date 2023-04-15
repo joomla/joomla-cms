6cd6a10b18bd7f69a9357bd080e668e5ac02a37b
Charvi <charvimehra0007@gmail.com>
2023-04-16 02:35:51 +0530

    Adding code into a .then block

--- tests/System/integration/site/components/com_users/Logout.cy.js
+++ tests/System/integration/site/components/com_users/Logout.cy.js
@@ -7,11 +7,13 @@ describe('Test in frontend that the users logout view ', () => {
   });
 
   it('can log out the user in a menu item', () => {
-    cy.doFrontendLogin();
-    cy.db_createMenuItem({ title: 'Automated logout', link: 'index.php?option=com_users&view=login&layout=logout&task=user.menulogout' });
-    cy.visit('/');
-    cy.get('a:contains(Automated logout)').click();
+    cy.db_createMenuItem({ title: 'Automated logout', link: 'index.php?option=com_users&view=login&layout=logout&task=user.menulogout' })
+      .then(() => {
+        cy.doFrontendLogin();
+        cy.visit('/');
+        cy.get('a:contains(Automated logout)').click();
 
-    cy.get('#system-message-container').should('contain.text', 'You have been logged out.');
+        cy.get('#system-message-container').should('contain.text', 'You have been logged out.'); 
+      });
   });
 });
