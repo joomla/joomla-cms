ff283930f73c2e17ae138ba43c2648688281a03f
Charvi <charvimehra0007@gmail.com>
2023-04-16 02:48:07 +0530

    merge

--- tests/System/integration/site/components/com_users/Edit.cy.js
+++ tests/System/integration/site/components/com_users/Edit.cy.js
@@ -1,17 +1,17 @@
-describe('Test that the users profile view ', () => {
-  it('can display a edit user profile form without a menu item', () => {
+describe('Test that the users profile view', () => {
+  it('can display an edit user profile form without a menu item', () => {
     cy.doFrontendLogin();
     cy.visit('index.php?option=com_users&view=profile&layout=edit');
 
     cy.get('#member-profile > :nth-child(1) > legend').should('contain.text', 'Edit Your Profile');
   });
 
-  it('can display a edit user profile form in a menu item', () => {
-    cy.db_createMenuItem({ title: 'Automated test edit', link: 'index.php?option=com_users&view=profile&layout=edit' })
-      .then(() => {
-        cy.doFrontendLogin();
-        cy.visit('http://localhost/joomlanew');
-        cy.get('a:contains(Automated test edit)').click();
+  it('can display an edit user profile form in a menu item', () => {
+    cy.doFrontendLogin();
+    cy.db_createMenuItem({ title: 'Automated test edit', link: 'index.php?option=com_users&view=profile&layout=edit' });
+    cy.visit('/');
+
+    cy.get('a:contains(Automated test edit)').click();
 
         cy.get('#member-profile > :nth-child(1) > legend').should('contain.text', 'Edit Your Profile');
       });
