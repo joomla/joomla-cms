048cb9073446e952412c8d039a0d4a8ae233f804
Charvi Mehra <121369234+charvimehradu@users.noreply.github.com>
2023-04-17 23:19:01 +0530

    Adding tests for test user

--- tests/System/integration/site/components/com_users/Edit.cy.js
+++ tests/System/integration/site/components/com_users/Edit.cy.js
@@ -16,4 +16,40 @@ describe('Test that the users profile view', () => {
         cy.get('#member-profile > :nth-child(1) > legend').should('contain.text', 'Edit Your Profile');
       });
   });
+
+  it('can edit an edit user profile form for a test user without a menu item', () => {
+    cy.db_createUser({
+        name: 'automated test user', username: 'automatedtestuser', password: '098f6bcd4621d373cade4e832627b4f6',
+    })
+    .then(() => {
+        cy.doFrontendLogin('automatedtestuser', 'test');
+        cy.visit('index.php?option=com_users&view=profile&layout=edit');
+
+        cy.get('#member-profile > :nth-child(1) > legend').should('contain.text', 'Edit Your Profile');
+        cy.get('#jform_name').clear().type('automated test user edited');
+        cy.get('#jform_email1').clear().type('testedited@example.com');
+        cy.get('.controls > .btn-primary').click();
+        cy.get('#system-message-container').contains('Profile saved.');
+        cy.get('#users-profile-core').should('contain.text', 'Name');
+        cy.get('#users-profile-core').should('contain.text', 'automated test user edited');
+    });
+  });
+
+  it('can edit an edit user profile form for a test user in a menu item', () => {
+    cy.db_createUser({
+        name: 'automated test user', username: 'automatedtestuser', password: '098f6bcd4621d373cade4e832627b4f6', registerDate: '2023-03-01 20:00:00',
+    })
+    cy.db_createMenuItem({ title: 'Automated test edit test user', link: 'index.php?option=com_users&view=profile&layout=edit' })
+    .then(() => {
+        cy.doFrontendLogin('automatedtestuser', 'test');
+        cy.visit('/');
+        cy.get('a:contains(Automated test edit)').click();
+
+        cy.get('#member-profile > :nth-child(1) > legend').should('contain.text', 'Edit Your Profile');
+        cy.get('#jform_name').clear().type('automated test user edited');
+        cy.get('#jform_email1').clear().type('testedited@example.com');
+        cy.get('.controls > .btn-primary').click();
+        cy.get('#system-message-container').contains('Profile saved.');
+    });
+  });
 });
