396691bf672a569932068501e6fdaa7687011887
Allon Moritz <allon.moritz@digital-peak.com>
2023-04-17 10:25:45 +0200

    registration

--- tests/System/integration/site/components/com_users/Registration.cy.js
+++ tests/System/integration/site/components/com_users/Registration.cy.js
@@ -1,8 +1,6 @@
 describe('Test in frontend that the users registration view', () => {
-  it('can display a registration form', () => {
-    cy.db_createUser({
-      name: 'test user', username: 'test', email: 'test@example.com', password: '098f6bcd4621d373cade4e832627b4f6',
-    })
+  it('can display a registration form for a test user without a menu item', () => {
+    cy.db_createUser({ username: 'test', password: '098f6bcd4621d373cade4e832627b4f6' })
       .then(() => {
         cy.visit('index.php?option=com_users&view=registration');
         cy.get('#username').type('test');
@@ -14,10 +12,8 @@ describe('Test in frontend that the users registration view', () => {
       });
   });
 
-  it('testing registration form for test user through menu item', () => {
-    cy.db_createUser({
-      name: 'test user', username: 'test', email: 'test@example.com', password: '098f6bcd4621d373cade4e832627b4f6',
-    })
+  it('can display a registration form for a test user in a menu item', () => {
+    cy.db_createUser({ username: 'test', password: '098f6bcd4621d373cade4e832627b4f6' })
       .then(() => cy.db_createMenuItem({ title: 'Automated test registration', link: 'index.php?option=com_users&view=registration' }))
       .then(() => {
         cy.visit('/');
