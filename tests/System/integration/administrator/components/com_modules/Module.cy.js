e5e1a739d027d150bc30d0cdeb0fdd580d71414a
Allon Moritz <allon.moritz@digital-peak.com>
2023-04-18 13:37:51 +0200

    cs

--- tests/System/integration/administrator/components/com_modules/Module.cy.js
+++ tests/System/integration/administrator/components/com_modules/Module.cy.js
@@ -3,7 +3,7 @@ describe('Test in backend that the module form', () => {
   afterEach(() => cy.task('queryDB', "DELETE FROM #__modules WHERE title = 'Test module'"));
 
   it('can create a module', () => {
-    cy.visit('/administrator/index.php?option=com_modules&task=module.add&client_id=0&eid=44',{failOnStatusCode: false});
+    cy.visit('/administrator/index.php?option=com_modules&task=module.add&client_id=0&eid=44');
     cy.get('#jform_title').clear().type('Test module');
     cy.clickToolbarButton('Save & Close');
 
