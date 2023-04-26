c0ee39bfee82aba759d9dfadee1413f81c6b87b8
jsanders <j53.sanders@gmail.com>
2023-04-26 11:53:04 +0200

    Improve test description

--- tests/System/integration/site/modules/mod_related_items/Default.cy.js
+++ tests/System/integration/site/modules/mod_related_items/Default.cy.js
@@ -1,5 +1,5 @@
-describe('Test the related items module', () => {
-  it('can load in frontend showing a list of related articles based on the metakey field', () => {
+describe('Test in frontend that the related items module', () => {
+  it('can display a list of related articles based on the metakey field', () => {
     cy.db_createArticle({ title: 'Main Article', metakey: 'joomla', featured: 1 })
       .then(() => cy.db_createArticle({ title: 'article with joomla keyword', metakey: 'joomla' }))
       .then(() => cy.db_createModule({ module: 'mod_related_items' }))
