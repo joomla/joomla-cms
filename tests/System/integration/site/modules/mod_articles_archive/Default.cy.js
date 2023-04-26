ea8569bb7ebc7aebf439affae460595dcb6bea26
jsanders <j53.sanders@gmail.com>
2023-04-26 11:32:21 +0200

    Improve test description

--- tests/System/integration/site/modules/mod_articles_archive/Default.cy.js
+++ tests/System/integration/site/modules/mod_articles_archive/Default.cy.js
@@ -1,4 +1,4 @@
-describe('Test in frondend that the articles archive module', () => {
+describe('Test in frontend that the articles archive module', () => {
   it('can display a list of months with archived articles', () => {
     cy.db_createArticle({ state: 2, created: '2022-09-09 20:00:00', modified: '2022-09-09 20:00:00' })
       .then(() => cy.db_createModule({ module: 'mod_articles_archive' }))
