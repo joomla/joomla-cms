3066e8d622165966368fd936968373b34d990d35
Dimitris Grammatikogiannis <dg@dgrammatiko.dev>
2023-04-30 17:47:19 +0200

    Add a fallback just to be sure we won’t break anything

--- includes/app.php
+++ includes/app.php
@@ -22,6 +22,14 @@ if (!defined('_JDEFINES')) {
     require_once JPATH_BASE . '/includes/defines.php';
 }
 
+if (!defined('JPATH_PUBLIC')) {
+    define('JPATH_PUBLIC', JPATH_ROOT);
+}
+
+if (!defined('JPATH_PUBLICNAME')) {
+    define('JPATH_PUBLICNAME', '');
+}
+
 // Check for presence of vendor dependencies not included in the git repository
 if (!file_exists(JPATH_LIBRARIES . '/vendor/autoload.php') || !is_dir(JPATH_ROOT . '/media/vendor')) {
     echo file_get_contents(JPATH_ROOT . '/templates/system/build_incomplete.html');
