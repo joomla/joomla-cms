6691e52b78907a72cb3f320aed75f2555826383d
Allon Moritz <allon.moritz@digital-peak.com>
2023-05-02 20:23:13 +0200

    Define correct release date

--- libraries/src/Version.php
+++ libraries/src/Version.php
@@ -90,7 +90,7 @@ final class Version
      * @var    string
      * @since  3.5
      */
-    public const RELDATE = '28-April-2023';
+    public const RELDATE = '17-October-2023';
 
     /**
      * Release time.
