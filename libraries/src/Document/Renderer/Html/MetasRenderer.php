58a7183f45000355523a9208f99cb1fd4337ed04
Fedik <getthesite@gmail.com>
2023-05-06 13:02:02 +0300

    App events, create to new

--- libraries/src/Document/Renderer/Html/MetasRenderer.php
+++ libraries/src/Document/Renderer/Html/MetasRenderer.php
@@ -10,7 +10,7 @@
 namespace Joomla\CMS\Document\Renderer\Html;
 
 use Joomla\CMS\Document\DocumentRenderer;
-use Joomla\CMS\Event\AbstractEvent;
+use Joomla\CMS\Event\Application\BeforeCompileHeadEvent;
 use Joomla\CMS\Factory;
 use Joomla\CMS\Helper\TagsHelper;
 use Joomla\CMS\Uri\Uri;
@@ -61,7 +61,7 @@ class MetasRenderer extends DocumentRenderer
         // Trigger the onBeforeCompileHead event
         $app->getDispatcher()->dispatch(
             'onBeforeCompileHead',
-            AbstractEvent::create('onBeforeCompileHead', ['subject' => $app, 'document' => $this->_doc])
+            new BeforeCompileHeadEvent('onBeforeCompileHead', ['subject' => $app, 'document' => $this->_doc])
         );
 
         // Add Script Options as inline asset
