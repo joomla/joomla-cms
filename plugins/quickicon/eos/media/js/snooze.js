cd4af576462ba5c16f3079919435028850d93e04
Dimitris Grammatikogiannis <dg@dgrammatiko.dev>
2023-05-06 20:04:16 +0200

    Update snooze.js

--- plugins/quickicon/eos/media/js/snooze.js
+++ plugins/quickicon/eos/media/js/snooze.js
@@ -2,37 +2,28 @@
  * @copyright   (C) 2023 Open Source Matters, Inc. <https://www.joomla.org>
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
-
-const data = {
-  option: 'com_ajax',
-  group: 'quickicon',
-  plugin: 'SnoozeEOS',
-  format: 'json',
-};
-
+const url = new URL(`${Joomla.getOptions('system.paths').baseFull}index.php?option=com_ajax&group=quickicon&plugin=SnoozeEOS&format=json`)
 const observer = new MutationObserver(onCreated);
 observer.observe(document.querySelector('#system-message-container'), { attributes: false, childList: true, subtree: true });
 
-async function onCreated(mutationList, observer) {
+function onCreated(mutationList, observer) {
   for(let mutation of mutationList) {
-    if (!Array.from(mutation.addedNodes).length) return;
-    let alerts = Array.from(mutation.addedNodes).filter(node => node.querySelector('.eosnotify-snooze-btn'));
+    const nodes = Array.from(mutation.addedNodes);
+
+    if (!nodes.length) {
+      return;
+    }
+
+    observer.disconnect();
+    let alerts = nodes.filter(node => node.querySelector('.eosnotify-snooze-btn'));
 
     if (alerts.length) {
-      observer.disconnect();
-      alerts[0].querySelector('.eosnotify-snooze-btn').addEventListener('click', async (_) => {
-        const response = await fetch(new URL(`${Joomla.getOptions('system.paths').baseFull}index.php`),
-          {
-            method: 'POST',
-            body: JSON.stringify(data),
-            headers: { 'X-CSRF-Token': Joomla.getOptions('csrf.token') || '' }
-        });
-
-        if (response.ok) {
+      alerts[0].querySelector('.eosnotify-snooze-btn').addEventListener('click', (_) => fetch(url, { headers: { 'X-CSRF-Token': Joomla.getOptions('csrf.token') || '' } })
+        .then((response) => {
+          if (response.ok) {
           alerts[0].closest('joomla-alert').close();
         }
-      })
+      }));
     }
   }
 };
-
