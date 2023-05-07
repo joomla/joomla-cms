e6217d6a2248474c90645d6deb1283749699eba7
Mark Fleeson <mark@lindisfarne-scriptorium.co.uk>
2023-05-07 17:15:28 +0100

    Fixing Header blocks must be separated by a single blank line

--- plugins/quickicon/eos/src/Extension/Eos.php
+++ plugins/quickicon/eos/src/Extension/Eos.php
@@ -2,9 +2,7 @@
 /**
  * @package                 Joomla.Plugin
  * @subpackage              quickicon.eos
- *
- * @copyright           (C) 2023 Open Source Matters, Inc. <https:
- * //www.joomla.org>
+ * @copyright               (C) 2023 Open Source Matters, Inc. <https://www.joomla.org>
  * @license                 GNU General Public License version 2 or later; see LICENSE.txt
  * @phpcs                   :disable PSR1.Classes.ClassDeclaration.MissingNamespace
  */
