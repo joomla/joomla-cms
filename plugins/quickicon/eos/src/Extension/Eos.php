f3f492c77c8a0722a5dc153512be7dd00b3cf297
Mark Fleeson <mark@lindisfarne-scriptorium.co.uk>
2023-05-08 18:06:33 +0100

    Limit notification to one group, adding wider install sql/ExtensionHelper
    Adding plugin to #__extensions in base and update sql
    Adding plugin to ExtensionHelper.php
    Updating Eos.php to limit notification to just Notification panel on home

--- plugins/quickicon/eos/src/Extension/Eos.php
+++ plugins/quickicon/eos/src/Extension/Eos.php
@@ -11,8 +11,7 @@
 namespace Joomla\Plugin\Quickicon\Eos\Extension;
 
 // phpcs:disable PSR1.Files.SideEffects
-defined('_JEXEC') || die;
-
+\defined('_JEXEC') || die;
 // phpcs:enable PSR1.Files.SideEffects
 
 use Exception;
@@ -25,6 +24,8 @@ use Joomla\CMS\Language\Text;
 use Joomla\CMS\Plugin\CMSPlugin;
 use Joomla\Database\DatabaseAwareTrait;
 use Joomla\Event\DispatcherInterface;
+use Joomla\Event\SubscriberInterface;
+use Joomla\Module\Quickicon\Administrator\Event\QuickIconsEvent;
 
 use function defined;
 
@@ -33,8 +34,9 @@ use function defined;
  *
  * @since __DEPLOY_VERSION__
  */
-final class Eos extends CMSPlugin
+final class Eos extends CMSPlugin implements SubscriberInterface
 {
+    use DatabaseAwareTrait;
 
     /**
      * The EOS date for 4.4. and beyond
@@ -43,6 +45,7 @@ final class Eos extends CMSPlugin
      * @since 4.4.0
      */
     public const EOS_DATE = '2023-10-25';
+
     /**
      * Load the language file on instantiation.
      *
@@ -50,6 +53,7 @@ final class Eos extends CMSPlugin
      * @since 4.4.0
      */
     protected $autoloadLanguage = true;
+
     /**
      * Holding the current valid message to be shown
      *
@@ -68,6 +72,165 @@ final class Eos extends CMSPlugin
     private Document $document;
 
     /**
+     * Returns an array of events this subscriber will listen to.
+     *
+     * @return  array
+     *
+     * @since 4.4.0
+     */
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            'onGetIcons' => 'getEndOfServiceNotification',
+        ];
+    }
+
+    /**
+     * Constructor
+     *
+     * @param   DispatcherInterface  $subject   The object to observe
+     * @param   Document             $document  The document
+     * @param   array                $config    An optional associative array of configuration settings.
+     *                                          Recognized key values include 'name', 'group', 'params', 'language'
+     *                                          (this list is not meant to be comprehensive).
+     *
+     * @since 4.4.0
+     */
+    public function __construct($subject, Document $document, array $config = [])
+    {
+        parent::__construct($subject, $config);
+        $this->document       = $document;
+        $diff                 = Factory::getDate()->diff(Factory::getDate(Eos::EOS_DATE));
+        $monthsUntilEOS       = floor($diff->days / 30.417);
+        $message              = $this->getMessageInfo($monthsUntilEOS, $diff->invert);
+        $this->currentMessage = $message;
+    }
+
+    /**
+     * Check and show the the alert and quickicon message
+     *
+     * This method is called when the Quick Icons module is constructing its set
+     * of icons. You can return an array which defines a single icon and it will
+     * be rendered right after the stock Quick Icons.
+     *
+     * @param   QuickIconsEvent  $event  The event object
+     *
+     * @return  void
+     *
+     * @since   4.0.0
+     *
+     * @throws Exception
+     */
+    public function getEndOfServiceNotification(QuickIconsEvent $event)
+    {
+        $context = $event->getContext();
+
+        if ($context !== $this->params->get('context', 'update_quickicon')) {
+            return;
+        }
+
+        if (!$this->shouldDisplayMessage()) {
+            return;
+        }
+
+        // No messages yet
+        if (!$this->currentMessage) {
+            return;
+        }
+
+        // Show this only when not snoozed
+        if ($this->params->get('last_snoozed_id', 0) < $this->currentMessage['id']) {
+            // Build the  message to be displayed in the cpanel
+            $messageText = Text::sprintf($this->currentMessage['messageText'], HTMLHelper::_('date', Eos::EOS_DATE, Text::_('DATE_FORMAT_LC3')), $this->currentMessage['messageLink']);
+            if ($this->currentMessage['snoozable']) {
+                $messageText .= '<p><button class="btn btn-warning eosnotify-snooze-btn" type="button" >' . Text::_('PLG_QUICKICON_EOS_SNOOZE_BUTTON') . '</button></p>';
+            }
+            $this->getApplication()->enqueueMessage($messageText, $this->currentMessage['messageType']);
+        }
+        try {
+            $this->document->getWebAssetManager()->registerAndUseScript('plg_quickicon_eos.script', 'plg_quickicon_eos/snooze.js', [], ['type' => 'module']);
+        } catch (Exception $e) {
+            echo $e->getMessage();
+            exit();
+        }
+        // The message as quickicon
+        // Add the icon to the result array
+        $result = $event->getArgument('result', []);
+        $messageTextQuickIcon = Text::sprintf($this->currentMessage['quickiconText'], HTMLHelper::_('date', Eos::EOS_DATE, Text::_('DATE_FORMAT_LC3')));
+
+        // The message as quickicon
+
+        $result[] = [
+            [
+                'link'  => $this->currentMessage['messageLink'],
+                'image' => 'fa fa-life-ring',
+                'text'  => $messageTextQuickIcon,
+                'id'    => 'plg_quickicon_eos',
+                'group' => $this->currentMessage['groupText'],
+                'class' => 'success',
+            ],
+        ];
+
+        $event->setArgument('result', $result);
+    }
+
+    /**
+     * Save the plugin parameters
+     *
+     * @return  bool
+     *
+     * @since 4.4.0
+     */
+    private function saveParams(): bool
+    {
+        $db    = Factory::getContainer()->get('DatabaseDriver');
+        $query = $db->getQuery(true)->update($db->quoteName('#__extensions'))->set($db->quoteName('params') . ' = ' . $db->quote($this->params->toString('JSON')))->where($db->quoteName('type') . ' = ' . $db->quote('plugin'))->where($db->quoteName('folder') . ' = ' . $db->quote('quickicon'))->where($db->quoteName('element') . ' = ' . $db->quote('eos'));
+        try {
+            // Lock the tables to prevent multiple plugin executions causing a race condition
+            $db->lockTable('#__extensions');
+        } catch (Exception $e) {
+            // If we can't lock the tables it's too risky to continue execution
+            return false;
+        }
+        try {
+            // Update the plugin parameters
+            $result = $db->setQuery($query)->execute();
+            $this->clearCacheGroups();
+        } catch (Exception $e) {
+            // If we failed to execute
+            $db->unlockTables();
+            $result = false;
+        }
+        try {
+            // Unlock the tables after writing
+            $db->unlockTables();
+        } catch (Exception $e) {
+            // If we can't lock the tables assume we have somehow failed
+            $result = false;
+        }
+
+        return $result;
+    }
+
+    /**
+     * Determines if the message and quickicon should be displayed
+     *
+     * @return  bool
+     *
+     * @since 4.4.0
+     *
+     * @throws Exception
+     */
+    private function shouldDisplayMessage(): bool
+    {
+        if (!$this->getApplication()->isClient('administrator') || Factory::getApplication()->getIdentity()->guest || $this->document->getType() !== 'html' || $this->getApplication()->input->getCmd('tmpl', 'index') === 'component' || $this->getApplication()->input->get('option') !== 'com_cpanel') {
+            return false;
+        }
+
+        return true;
+    }
+
+     /**
      * Clears cache groups. We use it to clear the plugins cache after we update the last run timestamp.
      *
      *
@@ -174,19 +337,7 @@ final class Eos extends CMSPlugin
         return false;
     }
 
-    /**
-     * Returns an array of events this subscriber will listen to.
-     *
-     * @return  array
-     *
-     * @since 4.4.0
-     */
-    public static function getSubscribedEvents(): array
-    {
-        return [
-            'onGetIcons' => 'onGetIcons',
-        ];
-    }
+
 
     /**
      * Check if current user is allowed to send the data
@@ -229,127 +380,5 @@ final class Eos extends CMSPlugin
         }
 
         return '';
-    }
-
-    /**
-     * Check and show the the alert and quickicon message
-     *
-     * @param   string  $context  The calling context
-     *
-     * @return  array  A list of icon definition associative arrays, consisting of the
-     *                 keys link, image, text and access.
-     *
-     * @since 4.4.0
-     *
-     * @throws Exception
-     */
-    public function onGetIcons(string $context): array
-    {
-        if (!$this->shouldDisplayMessage()) {
-            return [];
-        }
-
-        // No messages yet
-        if (!$this->currentMessage) {
-            return [];
-        }
-
-        // Show this only when not snoozed
-        if ($this->params->get('last_snoozed_id', 0) < $this->currentMessage['id']) {
-            // Build the  message to be displayed in the cpanel
-            $messageText = Text::sprintf($this->currentMessage['messageText'], HTMLHelper::_('date', Eos::EOS_DATE, Text::_('DATE_FORMAT_LC3')), $this->currentMessage['messageLink']);
-            if ($this->currentMessage['snoozable']) {
-                $messageText .= '<p><button class="btn btn-warning eosnotify-snooze-btn" type="button" >' . Text::_('PLG_QUICKICON_EOS_SNOOZE_BUTTON') . '</button></p>';
-            }
-            $this->getApplication()->enqueueMessage($messageText, $this->currentMessage['messageType']);
-        }
-        try {
-            $this->document->getWebAssetManager()->registerAndUseScript('plg_quickicon_eos.script', 'plg_quickicon_eos/snooze.js', [], ['type' => 'module']);
-        } catch (Exception $e) {
-            echo $e->getMessage();
-            exit();
-        }
-        // The message as quickicon
-        $messageTextQuickIcon = Text::sprintf($this->currentMessage['quickiconText'], HTMLHelper::_('date', Eos::EOS_DATE, Text::_('DATE_FORMAT_LC3')));
-
-        // The message as quickicon
-        return [['link' => $this->currentMessage['messageLink'], 'target' => '_blank', 'rel' => 'noopener noreferrer', 'image' => $this->currentMessage['image'], 'text' => $messageTextQuickIcon, 'id' => 'plg_quickicon_eos', 'group' => $this->currentMessage['groupText'],]];
-    }
-
-    /**
-     * Save the plugin parameters
-     *
-     * @return  bool
-     *
-     * @since 4.4.0
-     */
-    private function saveParams(): bool
-    {
-        $db    = Factory::getContainer()->get('DatabaseDriver');
-        $query = $db->getQuery(true)->update($db->quoteName('#__extensions'))->set($db->quoteName('params') . ' = ' . $db->quote($this->params->toString('JSON')))->where($db->quoteName('type') . ' = ' . $db->quote('plugin'))->where($db->quoteName('folder') . ' = ' . $db->quote('quickicon'))->where($db->quoteName('element') . ' = ' . $db->quote('eos'));
-        try {
-            // Lock the tables to prevent multiple plugin executions causing a race condition
-            $db->lockTable('#__extensions');
-        } catch (Exception $e) {
-            // If we can't lock the tables it's too risky to continue execution
-            return false;
-        }
-        try {
-            // Update the plugin parameters
-            $result = $db->setQuery($query)->execute();
-            $this->clearCacheGroups();
-        } catch (Exception $e) {
-            // If we failed to execute
-            $db->unlockTables();
-            $result = false;
-        }
-        try {
-            // Unlock the tables after writing
-            $db->unlockTables();
-        } catch (Exception $e) {
-            // If we can't lock the tables assume we have somehow failed
-            $result = false;
-        }
-
-        return $result;
-    }
-
-    /**
-     * Determines if the message and quickicon should be displayed
-     *
-     * @return  bool
-     *
-     * @since 4.4.0
-     *
-     * @throws Exception
-     */
-    private function shouldDisplayMessage(): bool
-    {
-        if (!$this->getApplication()->isClient('administrator') || Factory::getApplication()->getIdentity()->guest || $this->document->getType() !== 'html' || $this->getApplication()->input->getCmd('tmpl', 'index') === 'component' || $this->getApplication()->input->get('option') !== 'com_cpanel') {
-            return false;
-        }
-
-        return true;
-    }
-
-    /**
-     * Constructor
-     *
-     * @param   DispatcherInterface  $subject   The object to observe
-     * @param   Document             $document  The document
-     * @param   array                $config    An optional associative array of configuration settings.
-     *                                          Recognized key values include 'name', 'group', 'params', 'language'
-     *                                          (this list is not meant to be comprehensive).
-     *
-     * @since 4.4.0
-     */
-    public function __construct($subject, Document $document, array $config = [])
-    {
-        parent::__construct($subject, $config);
-        $this->document       = $document;
-        $diff                 = Factory::getDate()->diff(Factory::getDate(Eos::EOS_DATE));
-        $monthsUntilEOS       = floor($diff->days / 30.417);
-        $message              = $this->getMessageInfo($monthsUntilEOS, $diff->invert);
-        $this->currentMessage = $message;
     }
 }
