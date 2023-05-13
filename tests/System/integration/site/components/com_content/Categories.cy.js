dfd8af6f10fe24f52f26c61bdf2ee82f507a1e24
rajputanuj31 <115709571+rajputanuj31@users.noreply.github.com>
2023-05-13 23:27:03 +0530

    added a test for categories view of articles

---
+++ tests/System/integration/site/components/com_content/Categories.cy.js
@@ -0,0 +1,19 @@
+describe('Test in frontend that the Catagories of com_content ', () => {
+  it('can display a list of categories of articles ', () => {
+    cy.db_createCategory({ title: 'automated test category 1', extension: 'com_content' })
+      .then((id) => cy.db_createArticle({ title: 'automated test article 1', catid: id }))
+      .then(() => cy.db_createCategory({ title: 'automated test category 2', extension: 'com_content' }))
+      .then(async (id) => {
+        await cy.db_createArticle({ title: 'automated test article 2', catid: id });
+        await cy.db_createArticle({ title: 'automated test article 3', catid: id });
+      })
+      .then(() => {
+        cy.visit('index.php?option=com_content&view=categories');
+
+        cy.contains('automated test category 1');
+        cy.get(':nth-child(1) > .com-content-categories__item-title-wrapper > .com-content-categories__item-title > .badge').contains('Article Count: 1');
+        cy.contains('automated test category 2');
+        cy.get(':nth-child(2) > .com-content-categories__item-title-wrapper > .com-content-categories__item-title > .badge').contains('Article Count: 2');
+      });
+  });
+});
