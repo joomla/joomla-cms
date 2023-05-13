36f07ae3f4dc3d8c922d682d57db9b38364ed8b6
rajputanuj31 <115709571+rajputanuj31@users.noreply.github.com>
2023-05-13 22:01:13 +0530

    updates

--- tests/System/integration/site/components/com_smartsearch/SmartSearch.cy.js
+++ tests/System/integration/site/components/com_smartsearch/SmartSearch.cy.js
@@ -1,4 +1,4 @@
-describe('Test that the smart search view', () => {
+describe('Test in frontend that the smart search view', () => {
   it('can display smart search', () => {
     cy.db_createMenuItem({ title: 'automated test smart search', link: 'index.php?option=com_finder&view=search' })
       .then(() => {
