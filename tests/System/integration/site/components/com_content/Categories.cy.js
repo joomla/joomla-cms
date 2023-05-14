a6d452a168b4cb3a84e7d1ddca662c2ff3b1672a
rajputanuj31 <115709571+rajputanuj31@users.noreply.github.com>
2023-05-14 15:26:03 +0530

    add test with menu item

--- tests/System/integration/site/components/com_content/Categories.cy.js
+++ tests/System/integration/site/components/com_content/Categories.cy.js
@@ -1,5 +1,5 @@
-describe('Test in frontend that the content categories view', () => {
-  it('can display a list of article categories', () => {
+describe('Test in frontend that the Catagories of com_content ', () => {
+  it('can display a list of categories of articles without menu item', () => {
     cy.db_createCategory({ title: 'automated test category 1', extension: 'com_content' })
       .then((id) => cy.db_createArticle({ title: 'automated test article 1', catid: id }))
       .then(() => cy.db_createCategory({ title: 'automated test category 2', extension: 'com_content' }))
@@ -9,6 +9,26 @@ describe('Test in frontend that the content categories view', () => {
       })
       .then(() => {
         cy.visit('index.php?option=com_content&view=categories');
+
+        cy.contains('automated test category 1');
+        cy.get(':nth-child(1) > .com-content-categories__item-title-wrapper > .com-content-categories__item-title > .badge').contains('Article Count: 1');
+        cy.contains('automated test category 2');
+        cy.get(':nth-child(2) > .com-content-categories__item-title-wrapper > .com-content-categories__item-title > .badge').contains('Article Count: 2');
+      });
+  });
+
+  it('can display a list of categories of articles in menu item', () => {
+    cy.db_createCategory({ title: 'automated test category 1', extension: 'com_content' })
+      .then((id) => cy.db_createArticle({ title: 'automated test article 1', catid: id }))
+      .then(() => cy.db_createCategory({ title: 'automated test category 2', extension: 'com_content' }))
+      .then(async (id) => {
+        await cy.db_createArticle({ title: 'automated test article 2', catid: id });
+        await cy.db_createArticle({ title: 'automated test article 3', catid: id });
+      })
+      .then(() => cy.db_createMenuItem({ title: 'automated test categories', link: 'index.php?option=com_content&view=categories' }))
+      .then(() => {
+        cy.visit('/');
+        cy.get('a:contains(automated test categories)').click();
 
         cy.contains('automated test category 1');
         cy.get(':nth-child(1) > .com-content-categories__item-title-wrapper > .com-content-categories__item-title > .badge').contains('Article Count: 1');
