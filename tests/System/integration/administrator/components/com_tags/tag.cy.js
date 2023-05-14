4769a964e9fee7fc8443ade33038c866016a3c7c
Kaushik Vishwakarma <100058812+Kaushik1216@users.noreply.github.com>
2023-05-14 10:35:31 +0000

    coding standard corrected

--- tests/System/integration/administrator/components/com_tags/tag.cy.js
+++ tests/System/integration/administrator/components/com_tags/tag.cy.js
@@ -12,12 +12,12 @@ describe('Test in backend that the tag form', () => {
   });
 
   it('can edit a tag', () => {
-    cy.db_createTag({ title: 'Test tag' }).then((id) => {
-    cy.visit(`administrator/index.php?option=com_tags&task=tag.edit&id=${id}`);
-    cy.get('#jform_title').clear().type('Test tag edited');
-    cy.clickToolbarButton('Save & Close');
-  
-    cy.contains('Test tag edited');
-  });
+      cy.db_createTag({ title: 'Test tag' }).then((id) => {
+      cy.visit(`administrator/index.php?option=com_tags&task=tag.edit&id=${id}`);
+      cy.get('#jform_title').clear().type('Test tag edited');
+      cy.clickToolbarButton('Save & Close');
+
+      cy.contains('Test tag edited');
+    });
   });
 });
