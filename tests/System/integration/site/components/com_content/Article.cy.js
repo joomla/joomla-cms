41cf859cd8aba1105d74a7def37a0e0dd002aec0
rajputanuj31 <115709571+rajputanuj31@users.noreply.github.com>
2023-05-14 15:15:00 +0530

    add test for edit article without menu item

--- tests/System/integration/site/components/com_content/Article.cy.js
+++ tests/System/integration/site/components/com_content/Article.cy.js
@@ -1,7 +1,7 @@
 describe('Test in frontend that the content article form', () => {
-  afterEach(() => cy.task('queryDB', 'DELETE FROM #__content where title = \'automated test article\''));
+  afterEach(() => cy.task('queryDB', 'DELETE FROM #__content where title = \'test article\''));
 
-  it('can edit an article', () => {
+  it('can edit an article in menu item', () => {
     cy.db_createMenuItem({ title: 'automated test article', link: 'index.php?option=com_content&view=form&layout=edit' })
       .then(() => {
         cy.doFrontendLogin();
@@ -13,5 +13,15 @@ describe('Test in frontend that the content article form', () => {
         cy.get('.success').should('exist');
         cy.get('[type="success"] > .alert-wrapper > .alert-message').should('contain', 'Article submitted.');
       });
+  });
+
+  it('can edit an article without menu item', () => {
+    cy.doFrontendLogin();
+    cy.visit('index.php?option=com_content&view=form&layout=edit');
+
+    cy.get('#jform_title').type('test article');
+    cy.get('[data-submit-task="article.save"]').click();
+    cy.get('.success').should('exist');
+    cy.get('[type="success"] > .alert-wrapper > .alert-message').should('contain', 'Article submitted.');
   });
 });
