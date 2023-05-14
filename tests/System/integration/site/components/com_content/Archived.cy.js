509d25fcad6bc90f939727b1db2f961f21351ccf
rajputanuj31 <115709571+rajputanuj31@users.noreply.github.com>
2023-05-14 22:51:04 +0530

    added test for archived articles

---
+++ tests/System/integration/site/components/com_content/Archived.cy.js
@@ -0,0 +1,38 @@
+describe('Test in frontend that the content', () => {
+  it('can display an archived article in menu item', () => {
+    cy.db_createMenuItem({ title: 'automated test archive article', link: 'index.php?option=com_content&view=archive' })
+      .then(() => cy.db_createArticle({ title: 'article 1', state: 2 }))
+      .then(() => cy.db_createArticle({ title: 'article 2', state: 2 }))
+      .then(() => {
+        cy.visit('/');
+        cy.get('a:contains(automated test archive article)').click();
+
+        cy.contains('article 1');
+        cy.contains('article 2');
+      });
+  });
+
+  it('can display an archived article without menu item', () => {
+    cy.db_createArticle({ title: 'article 1', state: 2 })
+      .then(() => cy.db_createArticle({ title: 'article 2', state: 2 }))
+      .then(() => {
+        cy.visit('/index.php?option=com_content&view=archive');
+
+        cy.contains('article 1');
+        cy.contains('article 2');
+      });
+  });
+
+  it('can not display an unarchived article', () => {
+    cy.db_createMenuItem({ title: 'automated test archive article', link: 'index.php?option=com_content&view=archive' })
+      .then(() => cy.db_createArticle({ title: 'article 1', state: 1 }))
+      .then(() => cy.db_createArticle({ title: 'article 2', state: 0 }))
+      .then(() => {
+        cy.visit('/');
+        cy.get('a:contains(automated test archive article)').click();
+
+        cy.contains('article 1').should('not.exist');
+        cy.contains('article 2').should('not.exist');
+      });
+  });
+});
