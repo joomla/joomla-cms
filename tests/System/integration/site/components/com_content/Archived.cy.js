3675fb875086d02c345d6a8471b8b1a87314992e
Allon Moritz <allon.moritz@digital-peak.com>
2023-05-15 10:32:17 +0200

    Update Archived.cy.js

--- tests/System/integration/site/components/com_content/Archived.cy.js
+++ tests/System/integration/site/components/com_content/Archived.cy.js
@@ -1,5 +1,5 @@
-describe('Test in frontend that the content', () => {
-  it('can display an archived article in menu item', () => {
+describe('Test in frontend that the content archived view', () => {
+  it('can display archived articles in a menu item', () => {
     cy.db_createMenuItem({ title: 'automated test archive article', link: 'index.php?option=com_content&view=archive' })
       .then(() => cy.db_createArticle({ title: 'article 1', state: 2 }))
       .then(() => cy.db_createArticle({ title: 'article 2', state: 2 }))
@@ -12,7 +12,7 @@ describe('Test in frontend that the content', () => {
       });
   });
 
-  it('can display an archived article without menu item', () => {
+  it('can display archived articles without a menu item', () => {
     cy.db_createArticle({ title: 'article 1', state: 2 })
       .then(() => cy.db_createArticle({ title: 'article 2', state: 2 }))
       .then(() => {
@@ -23,16 +23,18 @@ describe('Test in frontend that the content', () => {
       });
   });
 
-  it('can not display an unarchived article', () => {
+  it('can not display not archived articles', () => {
     cy.db_createMenuItem({ title: 'automated test archive article', link: 'index.php?option=com_content&view=archive' })
       .then(() => cy.db_createArticle({ title: 'article 1', state: 1 }))
       .then(() => cy.db_createArticle({ title: 'article 2', state: 0 }))
+      .then(() => cy.db_createArticle({ title: 'article 2', state: 2 }))
       .then(() => {
         cy.visit('/');
         cy.get('a:contains(automated test archive article)').click();
 
         cy.contains('article 1').should('not.exist');
         cy.contains('article 2').should('not.exist');
+        cy.contains('article 3');
       });
   });
 });
