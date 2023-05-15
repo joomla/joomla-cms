5ba500d7f42865a0e2f0330969962aca6e564577
Allon Moritz <allon.moritz@digital-peak.com>
2023-05-15 10:48:12 +0200

    Update Archived.cy.js

--- tests/System/integration/site/components/com_content/Archived.cy.js
+++ tests/System/integration/site/components/com_content/Archived.cy.js
@@ -25,16 +25,16 @@ describe('Test in frontend that the content archived view', () => {
 
   it('can not display not archived articles', () => {
     cy.db_createMenuItem({ title: 'automated test archive article', link: 'index.php?option=com_content&view=archive' })
-      .then(() => cy.db_createArticle({ title: 'article 1', state: 1 }))
-      .then(() => cy.db_createArticle({ title: 'article 2', state: 0 }))
-      .then(() => cy.db_createArticle({ title: 'article 3', state: 2 }))
+      .then(() => cy.db_createArticle({ title: 'article 1', state: 2 }))
+      .then(() => cy.db_createArticle({ title: 'article 2', state: 1 }))
+      .then(() => cy.db_createArticle({ title: 'article 3', state: 0 }))
       .then(() => {
         cy.visit('/');
         cy.get('a:contains(automated test archive article)').click();
 
-        cy.contains('article 1').should('not.exist');
+        cy.contains('article 1');
         cy.contains('article 2').should('not.exist');
-        cy.contains('article 3');
+        cy.contains('article 3').should('not.exist');
       });
   });
 });
