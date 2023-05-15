d5c738fd4ec211a9739a0380d32f70b713e0101b
Charvi Mehra <charvimehra0007@gmailcom>
2023-05-16 00:37:05 +0530

    sending test mail and confirm information req.

--- tests/System/integration/site/components/com_privacy/request.cy.js
+++ tests/System/integration/site/components/com_privacy/request.cy.js
@@ -1,13 +1,26 @@
-describe('Test in frontend that the privacy confirm request view', () => {
+describe('Test in frontend that the privacy view confirm request layout', () => {
+
   beforeEach(() => {
+    cy.task('clearEmails');
     cy.doAdministratorLogin();
     cy.visit('/administrator/index.php?option=com_config');
 
-    cy.get('#configTabs > :nth-child(1) > [aria-controls="page-server"]').click();
-    cy.get('joomla-field-send-test-mail > .options-form > .form-grid > :nth-child(1) > .control-label').should('contain.text', 'Send Mail');
-
+    cy.get('#configTabs div[role="tablist"] button[aria-controls="page-server"]').click();
     cy.get('#jform_mailonline1').check();
-    cy.get('.button-save').click();
+
+    cy.get('#jform_mailer').select('SMTP');
+    cy.get('#jform_smtphost').clear().type(Cypress.env('smtp_host'));
+    cy.get('#jform_smtpport').clear().type(Cypress.env('smtp_port'));
+    cy.get('#jform_smtpsecure').select('none');
+    cy.get('#sendtestmail').click();
+
+    cy.task('getMails').then((mails) => {
+      cy.get('#system-message-container').should('contain.text', 'The email was sent to');
+
+      cy.wrap(mails).should('have.lengthOf', 1);
+      cy.wrap(mails[0].body).should('have.string', 'This is a test mail sent using');
+      cy.wrap(mails[0].sender).should('equal', Cypress.env('email'));
+    });
   });
 
   it('can create a form to submit an information request of type export', () => {
@@ -16,6 +29,13 @@ describe('Test in frontend that the privacy confirm request view', () => {
 
     cy.get('#jform_request_type').select('Export');
     cy.get('.controls > .btn').click();
-    cy.get('.alert-message').should('contain.text', 'Your information request has been created. Before it can be processed, you must verify this request. An email has been sent to your address with additional instructions to complete this verification.');
+    cy.task('getMails').then((mails) => {
+      cy.get('.alert-message').should('contain.text', 'Your information request has been created. Before it can be processed, you must verify this request. An email has been sent to your address with additional instructions to complete this verification.');
+
+      cy.wrap(mails).should('have.lengthOf', 3);
+      cy.log(mails);
+      cy.wrap(mails[0].sender).should('equal', Cypress.env('email'));
+      cy.wrap(mails[1].body).should('have.string', `A new information request has been submitted by ${Cypress.env('email')}.\n`);
+    });
   });
 });
