eddc6ee6558094b7d26004ec058491c9b0163649
Allon Moritz <allon.moritz@digital-peak.com>
2023-05-16 05:31:44 +0200

    Update request.cy.js

--- tests/System/integration/site/components/com_privacy/request.cy.js
+++ tests/System/integration/site/components/com_privacy/request.cy.js
@@ -1,7 +1,7 @@
-describe('Test in frontend that the privacy view confirm request layout', () => {
+describe('Test in frontend that the privacy request view', () => {
   beforeEach(() => cy.task('clearEmails'));
 
-  it('can create a form to submit an information request of type export', () => {
+  it('can submit an information request of type export', () => {
     cy.doFrontendLogin();
     cy.visit('/index.php?option=com_privacy&view=request');
     cy.get('#jform_request_type').select('Export');
@@ -11,7 +11,7 @@ describe('Test in frontend that the privacy view confirm request layout', () => 
       cy.get('.alert-message').should('contain.text', 'Your information request has been created. Before it can be processed, you must verify this request. An email has been sent to your address with additional instructions to complete this verification.');
       cy.wrap(mails).should('have.lengthOf', 2);
       cy.wrap(mails[0].sender).should('equal', Cypress.env('email'));
-      cy.wrap(mails[0].body).should('have.string', `A new information request has been submitted by ${Cypress.env('email')}.\n`);
+      cy.wrap(mails[0].body).should('have.string', `A new information request has been submitted by ${Cypress.env('email')}.`);
     });
   });
 });
