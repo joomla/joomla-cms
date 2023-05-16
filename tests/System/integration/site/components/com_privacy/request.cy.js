6db79665e93aa0e2d26748dfbd91bb4da20aba7f
Charvi Mehra <charvimehra0007@gmailcom>
2023-05-16 18:12:33 +0530

    add a second test with a menu item

--- tests/System/integration/site/components/com_privacy/request.cy.js
+++ tests/System/integration/site/components/com_privacy/request.cy.js
@@ -12,7 +12,26 @@ describe('Test in frontend that the privacy request view', () => {
       cy.wrap(mails).should('have.lengthOf', 2);
       cy.wrap(mails[0].sender).should('equal', Cypress.env('email'));
       cy.wrap(mails[0].body).should('have.string', `A new information request has been submitted by ${Cypress.env('email')}.`);
-      cy.wrap(mails[1].body).should('have.string', `Someone has created a request to export all personal information related to this email address at `);
+      cy.wrap(mails[1].body).should('have.string', 'Someone has created a request to export all personal information related to this email address at ');
     });
+  });
+
+  it('can submit an information request of type export in a menu item', () => {
+    cy.db_createUser({ username: 'testuser', password: '098f6bcd4621d373cade4e832627b4f6', email: 'test@example.com' })
+      .then(() => cy.db_createMenuItem({ title: 'Automated request information', link: 'index.php?option=com_privacy&view=request' }))
+      .then(() => {
+        cy.doFrontendLogin('testuser', 'test', false);
+        cy.visit('/');
+        cy.get('a:contains(Automated request information)').click();
+        cy.get('#jform_request_type').select('Export');
+        cy.get('.controls > .btn').click();
+
+        cy.task('getMails').then((mails) => {
+          cy.get('.alert-message').should('contain.text', 'Your information request has been created. Before it can be processed, you must verify this request. An email has been sent to your address with additional instructions to complete this verification.');
+          cy.wrap(mails).should('have.lengthOf', 2);
+          cy.wrap(mails[0].body).should('have.string', `A new information request has been submitted by ${'test@example.com'}.`);
+          cy.wrap(mails[1].body).should('have.string', 'Someone has created a request to export all personal information related to this email address at ');
+        });
+      });
   });
 });
