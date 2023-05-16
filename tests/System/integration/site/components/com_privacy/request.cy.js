11a85ed945369a54d81b4f842a1dc272917098d4
Charvi Mehra <charvimehra0007@gmailcom>
2023-05-16 16:53:13 +0530

    simplify

--- tests/System/integration/site/components/com_privacy/request.cy.js
+++ tests/System/integration/site/components/com_privacy/request.cy.js
@@ -12,10 +12,7 @@ describe('Test in frontend that the privacy request view', () => {
       cy.wrap(mails).should('have.lengthOf', 2);
       cy.wrap(mails[0].sender).should('equal', Cypress.env('email'));
       cy.wrap(mails[0].body).should('have.string', `A new information request has been submitted by ${Cypress.env('email')}.`);
-      const tokenGen = /Token: ([a-f0-9]+)/;
-      const tokenMatch = mails[1].body.match(tokenGen);
-      const token = tokenMatch ? tokenMatch[1] : '';
-      cy.wrap(mails[1].body).should('have.string', `Someone has created a request to export all personal information related to this email address at ${Cypress.config('baseUrl')}/. As a security measure, you must confirm that this is a valid request for your personal information from this website.\n\nIf this was a mistake, ignore this email and nothing will happen.\n\nIn order to confirm this request, you can complete one of the following tasks:\n\n1. Visit the following URL: ${Cypress.config('baseUrl')}/index.php/confirm?confirm_token=${token}\n\n2. Copy your token from this email, visit the referenced URL, and paste your token into the form.\nURL: ${Cypress.config('baseUrl')}/index.php/confirm\nToken: ${token}\n\nPlease note that this token is only valid for 24 hours from the time this email was sent.\n`);
+      cy.wrap(mails[1].body).should('have.string', `Someone has created a request to export all personal information related to this email address at `);
     });
   });
 });
