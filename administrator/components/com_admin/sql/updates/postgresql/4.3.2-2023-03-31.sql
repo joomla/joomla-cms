b158e6838c6b3c91892ba209e8ec1fdd5c80c43b
Richard Fath <richard67@users.noreply.github.com>
2023-05-21 13:47:18 +0200

    Delete 4.3.2 update SQL scripts from upmerge

--- administrator/components/com_admin/sql/updates/postgresql/4.3.2-2023-03-31.sql
+++
@@ -1,3 +0,0 @@
-UPDATE "#__guidedtour_steps"
-SET "title" = 'COM_GUIDEDTOURS_TOUR_BANNERS_STEP_DETAILS_TITLE', "description" = 'COM_GUIDEDTOURS_TOUR_BANNERS_STEP_DETAILS_DESCRIPTION', "target" = '.col-lg-9'
-WHERE "id" = 63;
