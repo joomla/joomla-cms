822a2cd8f509eedf40e45f255e1a066354b28427
Olivier Buisard <olivier.buisard@simplifyyourweb.com>
2023-05-22 20:15:13 -0400

    Added sub-templates to the default template to create a modal where users can select a file

---
+++ administrator/components/com_guidedtours/tmpl/tours/default_import_body.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_guidedtours
+ *
+ * @copyright   (C) 2023 Open Source Matters, Inc. <https://www.joomla.org>
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Language\Text;
+?>
+
+<div class="container">
+    <div class="row">
+        <div class="control-group col-md-12 mt-3">
+            <label for="importfile" class="form-label"><?php echo Text::sprintf('COM_GUIDEDTOURS_TOURS_IMPORT_FILE_LABEL'); ?></label>
+            <div class="controls">
+                <input type="file" id="importfile" name="importfile" class="form-control" />
+            </div>
+        </div>
+    </div>
+</div>
