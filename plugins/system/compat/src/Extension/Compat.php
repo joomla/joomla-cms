34c6818e38bfb965c21dcbec8a8efab232813c18
Harald Leithner <joomla@itronic.at>
2023-05-28 16:31:21 +0200

    Cleanup plugin

--- plugins/system/compat/src/Extension/Compat.php
+++ plugins/system/compat/src/Extension/Compat.php
@@ -4,18 +4,13 @@
  * @package     Joomla.Plugin
  * @subpackage  System.compat
  *
- * @copyright   (C) 2022 Open Source Matters, Inc. <https://www.joomla.org>
+ * @copyright   (C) 2023 Open Source Matters, Inc. <https://www.joomla.org>
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 namespace Joomla\Plugin\System\Compat\Extension;
 
-use Joomla\CMS\Document\FactoryInterface as DocumentFactoryInterface;
 use Joomla\CMS\Plugin\CMSPlugin;
-use Joomla\CMS\Plugin\PluginHelper;
-use Joomla\CMS\Profiler\Profiler;
-use Joomla\CMS\Router\SiteRouter;
-use Joomla\Event\DispatcherInterface;
 use Joomla\Event\Event;
 use Joomla\Event\Priority;
 use Joomla\Event\SubscriberInterface;
@@ -25,71 +20,18 @@ use Joomla\Event\SubscriberInterface;
 // phpcs:enable PSR1.Files.SideEffects
 
 /**
- * Joomla! Page Compat Plugin.
+ * Joomla! Compat Plugin.
  *
- * @since  5.0.0
+ * @since  __DEPLOY_VERSION__
  */
 final class Compat extends CMSPlugin implements SubscriberInterface
 {
-    /**
-     * The application's document factory interface
-     *
-     * @var   DocumentFactoryInterface
-     * @since 4.2.0
-     */
-    private $documentFactory;
-
-    /**
-     * The application profiler, used when Debug Site is set to Yes in Global Configuration.
-     *
-     * @var    Profiler|null
-     * @since  4.2.0
-     */
-    private $profiler;
-
-    /**
-     * The frontend router, injected by the service provider.
-     *
-     * @var   SiteRouter|null
-     * @since 4.2.0
-     */
-    private $router;
-
-    /**
-     * Constructor
-     *
-     * @param   DispatcherInterface              $subject                 The object to observe
-     * @param   array                            $config                  An optional associative
-     *                                                                    array of configuration
-     *                                                                    settings. Recognized key
-     *                                                                    values include 'name',
-     *                                                                    'group', 'params',
-     *                                                                    'language'
-     *                                                                    (this list is not meant
-     *                                                                    to be comprehensive).
-     * @param   Profiler|null                    $profiler                The application profiler
-     * @param   SiteRouter|null                  $router                  The frontend router
-     *
-     * @since   4.2.0
-     */
-    public function __construct(
-        &$subject,
-        $config,
-        ?Profiler $profiler,
-        ?SiteRouter $router
-    ) {
-        parent::__construct($subject, $config);
-
-        $this->profiler               = $profiler;
-        $this->router                 = $router;
-    }
-
     /**
      * Returns an array of CMS events this plugin will listen to and the respective handlers.
      *
      * @return  array
      *
-     * @since   4.2.0
+     * @since  __DEPLOY_VERSION__
      */
     public static function getSubscribedEvents(): array
     {
@@ -99,11 +41,19 @@ final class Compat extends CMSPlugin implements SubscriberInterface
          * might be needed by other plugins
          */
         return [
-            'onAfterInitialise' => ['onAfterInitialise', Priority::HIGH],
+            'onAfterInitialise' => ['eventAfterInitialise', Priority::HIGH],
         ];
     }
 
-    public function onAfterInitialise(Event $event)
+    /**
+     * We run as early as possible, this should be the first event
+     *
+     * @param Event $event
+     * @return void
+     *
+     * @since  __DEPLOY_VERSION__
+     */
+    public function eventAfterInitialise(Event $event)
     {
         /**
          * Load class names which are deprecated in joomla 4.0 and which will
