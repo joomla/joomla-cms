e56e257597161726ef0a3225f55e1d8ed9f81049
Hans Kuijpers <hans2103@gmail.com>
2023-05-30 20:00:58 +0200

    use linktype in menu item

--- modules/mod_menu/tmpl/default_component.php
+++ modules/mod_menu/tmpl/default_component.php
@@ -35,32 +35,6 @@ if ($item->id == $active_id) {
     }
 }
 
-$linktype = $item->title;
-
-if ($item->menu_icon) {
-    // The link is an icon
-    if ($itemParams->get('menu_text', 1)) {
-        // If the link text is to be displayed, the icon is added with aria-hidden
-        $linktype = '<span class="p-2 ' . $item->menu_icon . '" aria-hidden="true"></span>' . $item->title;
-    } else {
-        // If the icon itself is the link, it needs a visually hidden text
-        $linktype = '<span class="p-2 ' . $item->menu_icon . '" aria-hidden="true"></span><span class="visually-hidden">' . $item->title . '</span>';
-    }
-} elseif ($item->menu_image) {
-    // The link is an image, maybe with its own class
-    $image_attributes = [];
-
-    if ($item->menu_image_css) {
-        $image_attributes['class'] = $item->menu_image_css;
-    }
-
-    $linktype = HTMLHelper::_('image', $item->menu_image, $item->title, $image_attributes);
-
-    if ($itemParams->get('menu_text', 1)) {
-        $linktype .= '<span class="image-title">' . $item->title . '</span>';
-    }
-}
-
 if ($item->browserNav == 1) {
     $attributes['target'] = '_blank';
 } elseif ($item->browserNav == 2) {
@@ -69,4 +43,4 @@ if ($item->browserNav == 1) {
     $attributes['onclick'] = "window.open(this.href, 'targetWindow', '" . $options . "'); return false;";
 }
 
-echo HTMLHelper::_('link', OutputFilter::ampReplace(htmlspecialchars($item->flink, ENT_COMPAT, 'UTF-8', false)), $linktype, $attributes);
+echo HTMLHelper::_('link', OutputFilter::ampReplace(htmlspecialchars($item->flink, ENT_COMPAT, 'UTF-8', false)), $item->menu_linktype, $attributes);
