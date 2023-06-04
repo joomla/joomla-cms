0f4b1c0e7d732310dc252ab7a1fd386de6519bb6
Fedir Zinchuk <getthesite@gmail.com>
2023-06-04 13:46:16 +0300

    Update build/media_source/system/js/esm-map.js
    Co-authored-by: Brian Teeman <brian@teeman.net>

--- build/media_source/system/js/esm-map.js
+++ build/media_source/system/js/esm-map.js
@@ -1,5 +1,5 @@
 /**
- * @copyright  (C) 2018 Open Source Matters, Inc. <https://www.joomla.org>
+ * @copyright  (C) 2023 Open Source Matters, Inc. <https://www.joomla.org>
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 
