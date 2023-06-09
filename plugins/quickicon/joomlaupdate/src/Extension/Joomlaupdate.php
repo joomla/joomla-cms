a9e10a494de6cd7b2c4350966827ed1f8a8f8c8f
Allon Moritz <allon.moritz@digital-peak.com>
2023-06-09 14:15:26 +0200

    cleanup more

--- plugins/quickicon/joomlaupdate/src/Extension/Joomlaupdate.php
+++ plugins/quickicon/joomlaupdate/src/Extension/Joomlaupdate.php
@@ -72,7 +72,7 @@ class Joomlaupdate extends CMSPlugin implements SubscriberInterface
      *
      * @since   4.0.0
      */
-    public function __construct($dispatcher, Document $document, $config = [])
+    public function __construct($dispatcher, Document $document, array $config = [])
     {
         parent::__construct($dispatcher, $config);
 
