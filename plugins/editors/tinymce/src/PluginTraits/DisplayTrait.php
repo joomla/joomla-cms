bd6e0a4ffc466cb1c250343f0414615aa0357fc8
brian teeman <brian@teeman.net>
2023-06-12 08:05:30 +0100

    importcss

--- plugins/editors/tinymce/src/PluginTraits/DisplayTrait.php
+++ plugins/editors/tinymce/src/PluginTraits/DisplayTrait.php
@@ -251,7 +251,6 @@ trait DisplayTrait
         $plugins  = [
             'autolink',
             'lists',
-            'importcss',
             'quickbars',
         ];
 
@@ -310,7 +309,7 @@ trait DisplayTrait
             : false;
 
         // Check for extra plugins, from the setoptions form
-        foreach (['wordcount' => 1, 'advlist' => 1, 'autosave' => 1] as $pName => $def) {
+        foreach (['wordcount' => 1, 'advlist' => 1, 'autosave' => 1, 'importcss' => 0] as $pName => $def) {
             if ($levelParams->get($pName, $def)) {
                 $plugins[] = $pName;
             }
