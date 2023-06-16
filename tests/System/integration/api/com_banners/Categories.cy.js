f64dc7e457c42cbcf382938e23eeea3434d0e547
Allon Moritz <allon.moritz@digital-peak.com>
2023-06-16 11:29:28 +0200

    Do deep search in system tests instead of hardcoded sorting (#40778)
    * Do deep search in system tests instead of hardcoded sorting
    
    * title
    
    * api categories

--- tests/System/integration/api/com_banners/Categories.cy.js
+++ tests/System/integration/api/com_banners/Categories.cy.js
@@ -3,7 +3,6 @@ describe('Test that content categories API endpoint', () => {
     cy.db_createCategory({ title: 'automated test category', extension: 'com_banners' })
       .then((id) => cy.db_createBanner({ name: 'automated test banner', catid: id }))
       .then(() => cy.api_get('/banners/categories'))
-      .then((response) => cy.wrap(response.body.data.map((category) => ({ title: category.attributes.title })))
-        .should('deep.include', { title: 'automated test category' }));
+      .then((response) => cy.api_responseContains(response, 'title', 'automated test category'));
   });
 });
--- tests/System/integration/api/com_contact/Categories.cy.js
+++ tests/System/integration/api/com_contact/Categories.cy.js
@@ -3,7 +3,6 @@ describe('Test that content categories API endpoint', () => {
     cy.db_createCategory({ title: 'automated test category', extension: 'com_contact' })
       .then((id) => cy.db_createContact({ name: 'automated test contact', catid: id }))
       .then(() => cy.api_get('/contacts/categories'))
-      .then((response) => cy.wrap(response.body.data.map((category) => ({ title: category.attributes.title })))
-        .should('deep.include', { title: 'automated test category' }));
+      .then((response) => cy.api_responseContains(response, 'title', 'automated test category'));
   });
 });
