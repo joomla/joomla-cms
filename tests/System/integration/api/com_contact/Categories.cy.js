63ddc6d7de051ce8122dbb0dea19fa03053ff1e2
Allon Moritz <allon.moritz@digital-peak.com>
2023-06-16 08:30:48 +0200

    Update Categories.cy.js

--- tests/System/integration/api/com_contact/Categories.cy.js
+++ tests/System/integration/api/com_contact/Categories.cy.js
@@ -1,10 +1,9 @@
-describe('Test that contact categories API endpoint', () => {
+describe('Test that content categories API endpoint', () => {
   it('can display a list of categories', () => {
     cy.db_createCategory({ title: 'automated test category', extension: 'com_contact' })
       .then((id) => cy.db_createContact({ name: 'automated test contact', catid: id }))
       .then(() => cy.api_get('/contacts/categories'))
-      .then((response) => cy.wrap(response).its('body').its('data[0]').its('attributes')
-        .its('title')
-        .should('include', 'automated test category'));
+      .then((response) => cy.wrap(response.body.data.map((category) => ({ title: category.attributes.title })))
+        .should('deep.include', { title: 'automated test category' }));
   });
 });
