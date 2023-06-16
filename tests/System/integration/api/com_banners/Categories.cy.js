d7302dd18c18e271157c579252039596f9d30925
Allon Moritz <allon.moritz@digital-peak.com>
2023-06-16 08:31:15 +0200

    Update Categories.cy.js

--- tests/System/integration/api/com_banners/Categories.cy.js
+++ tests/System/integration/api/com_banners/Categories.cy.js
@@ -1,10 +1,9 @@
-describe('Test that banner categories API endpoint', () => {
+describe('Test that content categories API endpoint', () => {
   it('can display a list of categories', () => {
     cy.db_createCategory({ title: 'automated test category', extension: 'com_banners' })
       .then((id) => cy.db_createBanner({ name: 'automated test banner', catid: id }))
       .then(() => cy.api_get('/banners/categories'))
-      .then((response) => cy.wrap(response).its('body').its('data[0]').its('attributes')
-        .its('title')
-        .should('include', 'automated test category'));
+      .then((response) => cy.wrap(response.body.data.map((category) => ({ title: category.attributes.title })))
+        .should('deep.include', { title: 'automated test category' }));
   });
 });
