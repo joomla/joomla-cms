4661892267929c0686146982fa800590678c3679
Dimitris Grammatikogiannis <dg@dgrammatiko.dev>
2023-06-22 22:34:32 +0200

    import-maps-realistic-test

--- build/media_source/plg_media-action_crop/js/crop.es6.js
+++ build/media_source/plg_media-action_crop/js/crop.es6.js
@@ -2,7 +2,8 @@
  * @copyright  (C) 2018 Open Source Matters, Inc. <https://www.joomla.org>
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
-/* global Cropper */
+import Cropper from 'cropper-module';
+
 let formElements;
 let activated = false;
 let instance;
