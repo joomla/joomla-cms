72e47a26d6d71e981f60da1a437cfff7495347c4
Allon Moritz <allon.moritz@digital-peak.com>
2023-06-24 17:06:03 +0200

    Revert version.php

--- libraries/src/Version.php
+++ libraries/src/Version.php
@@ -66,7 +66,7 @@ final class Version
      * @var    string
      * @since  3.8.0
      */
-    public const EXTRA_VERSION = 'alpha2';
+    public const EXTRA_VERSION = 'alpha2-dev';
 
     /**
      * Development status.
@@ -74,7 +74,7 @@ final class Version
      * @var    string
      * @since  3.5
      */
-    public const DEV_STATUS = 'Alpha';
+    public const DEV_STATUS = 'Development';
 
     /**
      * Code name.
@@ -90,7 +90,7 @@ final class Version
      * @var    string
      * @since  3.5
      */
-    public const RELDATE = '27-June-2023';
+    public const RELDATE = '30-May-2023';
 
     /**
      * Release time.
@@ -98,7 +98,7 @@ final class Version
      * @var    string
      * @since  3.5
      */
-    public const RELTIME = '16:00';
+    public const RELTIME = '16:02';
 
     /**
      * Release timezone.
