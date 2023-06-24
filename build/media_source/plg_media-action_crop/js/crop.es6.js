c78926a74f7cc7b48b2193e9cd75bf8909d9ef9e
Dimitris Grammatikogiannis <dg@dgrammatiko.dev>
2023-06-24 12:17:38 +0200

    CS

--- build/media_source/plg_media-action_crop/js/crop.es6.js
+++ build/media_source/plg_media-action_crop/js/crop.es6.js
@@ -2,6 +2,7 @@
  * @copyright  (C) 2018 Open Source Matters, Inc. <https://www.joomla.org>
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
+// eslint-disable-next-line  import/no-unresolved
 import Cropper from 'cropper-module';
 
 let formElements;
