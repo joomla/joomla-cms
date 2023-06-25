d6ee90055ae9c88494fdc37a28b69de3f33624fd
SD Williams <72045461+sdwjoomla@users.noreply.github.com>
2023-06-25 12:59:24 -0500

    [5.0] Changed Position Top to Left Parent Category

--- administrator/components/com_admin/sql/updates/postgresql/5.0.0-2023-06-25.sql
+++ administrator/components/com_admin/sql/updates/postgresql/5.0.0-2023-06-25.sql
@@ -1,3 +1,3 @@
-UPDATE `#__guidedtour_steps`
-SET `position` = "left"
-WHERE `title` = "COM_GUIDEDTOURS_TOUR_CATEGORIES_STEP_PARENT_TITLE"; 
+UPDATE "#__guidedtour_steps"
+SET "position" = 'left'
+WHERE "title" = 'COM_GUIDEDTOURS_TOUR_CATEGORIES_STEP_PARENT_TITLE'; 
