6377747d9af0ceb3ef75efd28ba84ab865267f43
SD Williams <72045461+sdwjoomla@users.noreply.github.com>
2023-06-25 12:49:59 -0500

    Update 5.0.0-2023-06-25.sql
    change single quote to double quote

--- administrator/components/com_admin/sql/updates/postgresql/5.0.0-2023-06-25.sql
+++ administrator/components/com_admin/sql/updates/postgresql/5.0.0-2023-06-25.sql
@@ -1,3 +1,3 @@
 UPDATE `#__guidedtour_steps`
-SET `position` = 'left'
-WHERE `title` = 'COM_GUIDEDTOURS_TOUR_CATEGORIES_STEP_PARENT_TITLE'; 
+SET `position` = "left"
+WHERE `title` = "COM_GUIDEDTOURS_TOUR_CATEGORIES_STEP_PARENT_TITLE"; 
