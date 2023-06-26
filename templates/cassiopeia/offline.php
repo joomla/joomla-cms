04440f86a2fd20f78c761944af29f8578a649a01
Brian Teeman <brian@teeman.net>
2023-06-26 12:36:04 +0100

    Update offline.php

--- templates/cassiopeia/offline.php
+++ templates/cassiopeia/offline.php
@@ -63,12 +63,6 @@ $wa->usePreset('template.cassiopeia.' . ($this->direction === 'rtl' ? 'rtl' : 'l
     ->useStyle('template.user')
     ->useScript('template.user')
     ->addInlineStyle(":root {
-		--hue: 214;
-		--template-bg-light: #f0f4fb;
-		--template-text-dark: #495057;
-		--template-text-light: #ffffff;
-		--template-link-color: #2a69b8;
-		--template-special-color: #001B4C;
 		$fontStyles
 	}");
 
