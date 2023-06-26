12a1274d587099f1ba4ae00cc55789978fc132a4
Dimitris Grammatikogiannis <d.grammatiko@gmail.com>
2023-06-26 09:41:36 +0200

    [5.0] Allow Joomla be served from a public* folder (#40509)
    ---------
    
    Signed-off-by: Dimitris Grammatikogiannis <dg@dgrammatiko.dev>
    Co-authored-by: Harald Leithner <leithner@itronic.at>

--- libraries/src/HTML/HTMLHelper.php
+++ libraries/src/HTML/HTMLHelper.php
@@ -401,7 +401,7 @@ abstract class HTMLHelper
 
                 if ($template->inheritable || !empty($template->parent)) {
                     $client     = $app->isClient('administrator') === true ? 'administrator' : 'site';
-                    $templaPath = JPATH_ROOT . "/media/templates/$client";
+                    $templaPath = JPATH_PUBLIC . "/media/templates/$client";
                 }
 
                 // For each potential files
@@ -441,7 +441,7 @@ abstract class HTMLHelper
                                     list($element, $file) = explode('/', $file, 2);
 
                                     // Try to deal with plugins group in the media folder
-                                    $found = static::addFileToBuffer(JPATH_ROOT . "/media/$extension/$element/$folder/$file", $ext, $debugMode);
+                                    $found = static::addFileToBuffer(JPATH_PUBLIC . "/media/$extension/$element/$folder/$file", $ext, $debugMode);
 
                                     if (!empty($found)) {
                                         $includes[] = $found;
@@ -450,7 +450,7 @@ abstract class HTMLHelper
                                     }
 
                                     // Try to deal with classical file in a media subfolder called element
-                                    $found = static::addFileToBuffer(JPATH_ROOT . "/media/$extension/$folder/$element/$file", $ext, $debugMode);
+                                    $found = static::addFileToBuffer(JPATH_PUBLIC . "/media/$extension/$folder/$element/$file", $ext, $debugMode);
 
                                     if (!empty($found)) {
                                         $includes[] = $found;
@@ -477,7 +477,7 @@ abstract class HTMLHelper
                                         }
                                     } else {
                                         // Try to deal with system files in the media folder
-                                        $found = static::addFileToBuffer(JPATH_ROOT . "/media/system/$folder/$element/$file", $ext, $debugMode);
+                                        $found = static::addFileToBuffer(JPATH_PUBLIC . "/media/system/$folder/$element/$file", $ext, $debugMode);
 
                                         if (!empty($found)) {
                                             $includes[] = $found;
@@ -487,7 +487,7 @@ abstract class HTMLHelper
                                     }
                                 } else {
                                     // Try to deal with files in the extension's media folder
-                                    $found = static::addFileToBuffer(JPATH_ROOT . "/media/$extension/$folder/$file", $ext, $debugMode);
+                                    $found = static::addFileToBuffer(JPATH_PUBLIC . "/media/$extension/$folder/$file", $ext, $debugMode);
 
                                     if (!empty($found)) {
                                         $includes[] = $found;
@@ -524,7 +524,7 @@ abstract class HTMLHelper
                                     }
 
                                     // Try to deal with system files in the media folder
-                                    $found = static::addFileToBuffer(JPATH_ROOT . "/media/system/$folder/$file", $ext, $debugMode);
+                                    $found = static::addFileToBuffer(JPATH_PUBLIC . "/media/system/$folder/$file", $ext, $debugMode);
 
                                     if (!empty($found)) {
                                         $includes[] = $found;
@@ -534,7 +534,7 @@ abstract class HTMLHelper
                                 }
                             } else {
                                 // Try to deal with system files in the media folder
-                                $found = static::addFileToBuffer(JPATH_ROOT . "/media/system/$folder/$file", $ext, $debugMode);
+                                $found = static::addFileToBuffer(JPATH_PUBLIC . "/media/system/$folder/$file", $ext, $debugMode);
 
                                 if (!empty($found)) {
                                     $includes[] = $found;
@@ -558,7 +558,7 @@ abstract class HTMLHelper
                      * This MD5SUM file must represent the signature of the folder content
                      */
                     foreach ($files as $file) {
-                        $path = JPATH_ROOT . "/$file";
+                        $path = JPATH_PUBLIC . '/' . $file;
 
                         $found = static::addFileToBuffer($path, $ext, $debugMode);
 
@@ -1209,7 +1209,7 @@ abstract class HTMLHelper
      */
     protected static function convertToRelativePath($path)
     {
-        $relativeFilePath = Uri::root(true) . str_replace(JPATH_ROOT, '', $path);
+        $relativeFilePath = Uri::root(true) . str_replace(JPATH_PUBLIC, '', $path);
 
         // On windows devices we need to replace "\" with "/" otherwise some browsers will not load the asset
         return str_replace(DIRECTORY_SEPARATOR, '/', $relativeFilePath);
