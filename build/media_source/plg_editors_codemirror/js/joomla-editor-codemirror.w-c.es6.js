18eaafdec1ad02193f12c5c147ae328a32bb2eae
Fedik <getthesite@gmail.com>
2023-06-27 17:37:18 +0300

    Codemirror plugin cleanup

--- build/media_source/plg_editors_codemirror/js/joomla-editor-codemirror.w-c.es6.js
+++ build/media_source/plg_editors_codemirror/js/joomla-editor-codemirror.w-c.es6.js
@@ -8,48 +8,12 @@ import { EditorState } from '@codemirror/state';
 import { keymap } from "@codemirror/view";
 
 class CodemirrorEditor extends HTMLElement {
-  // constructor() {
-  //   super();
-  //
-  //   this.instance = null;
-  //   this.host = window.location.origin;
-  //   this.element = this.querySelector('textarea');
-  //
-  //   // Observer instance to refresh the Editor when it become visible, eg after Tab switching
-  //   this.intersectionObserver = new IntersectionObserver((entries) => {
-  //     if (entries[0].isIntersecting && this.instance) {
-  //       this.instance.refresh();
-  //     }
-  //   }, { threshold: 0 });
-  // }
-
-  // static get observedAttributes() {
-  //   return ['options'];
-  // }
 
   get options() { return JSON.parse(this.getAttribute('options')); }
 
   get fsCombo() { return this.getAttribute('fs-combo'); }
 
-  // set options(value) { this.setAttribute('options', value); }
-  //
-  // attributeChangedCallback(attr, oldValue, newValue) {
-  //   switch (attr) {
-  //     case 'options':
-  //       if (oldValue && newValue !== oldValue) {
-  //         this.refresh(this.element);
-  //       }
-  //       break;
-  //     default:
-  //     // Do nothing
-  //   }
-  // }
-
   async connectedCallback() {
-    // Register Editor
-    // this.instance = window.CodeMirror.fromTextArea(this.element, this.options);
-    // this.instance.disable = (disabled) => this.setOption('readOnly', disabled ? 'nocursor' : false);
-
     const { options } = this;
 
     // Configure full screen feature
@@ -63,7 +27,7 @@ class CodemirrorEditor extends HTMLElement {
       });
     }
 
-    // Configure editor instance
+    // Create an editor instance
     this.element = this.querySelector('textarea');
     const editor = await createFromTextarea(this.element, options);
     this.instance = editor;
@@ -98,9 +62,6 @@ class CodemirrorEditor extends HTMLElement {
       },
       onSave: () => {},
     };
-
-    // Watch when the element in viewport, and refresh the editor
-    // this.intersectionObserver.observe(this);
   }
 
   disconnectedCallback() {
@@ -110,9 +71,6 @@ class CodemirrorEditor extends HTMLElement {
     }
     // Remove from the Joomla API
     delete Joomla.editors.instances[this.element.id];
-
-    // Remove from observer
-    // this.intersectionObserver.unobserve(this);
   }
 
   toggleFullScreen = () => {
@@ -122,10 +80,6 @@ class CodemirrorEditor extends HTMLElement {
   closeFullScreen = () => {
     this.classList.remove('fullscreen');
   }
-
-  // refresh = (element) => {
-  //   this.instance.fromTextArea(element, this.options);
-  // }
 }
 
 customElements.define('joomla-editor-codemirror', CodemirrorEditor);
