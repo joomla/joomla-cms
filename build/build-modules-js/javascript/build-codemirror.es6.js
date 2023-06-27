ba97393b0d95f42f8822da25a1e4002ff7c60fb1
Fedik <getthesite@gmail.com>
2023-06-27 16:57:33 +0300

    Codemirror fix build

--- build/build-modules-js/javascript/build-codemirror.es6.js
+++ build/build-modules-js/javascript/build-codemirror.es6.js
@@ -1,103 +1,164 @@
 /**
- * @copyright  (C) 2023 Open Source Matters, Inc. <https://www.joomla.org>
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * Build codemirror modules
  */
+/* eslint-disable import/no-extraneous-dependencies, global-require, import/no-dynamic-require */
 
-/**
- * Based on https://github.com/codemirror/basic-setup
- */
-import {
-  EditorView,
-  lineNumbers,
-  highlightActiveLineGutter,
-  highlightSpecialChars,
-  drawSelection,
-  dropCursor,
-  rectangularSelection,
-  crosshairCursor,
-  highlightActiveLine,
-  keymap,
-} from '@codemirror/view';
-import { EditorState } from '@codemirror/state';
-import {
-  foldGutter,
-  indentOnInput,
-  syntaxHighlighting,
-  defaultHighlightStyle,
-  bracketMatching,
-  foldKeymap,
-} from '@codemirror/language';
-import { history, defaultKeymap, historyKeymap } from '@codemirror/commands';
-import { highlightSelectionMatches, searchKeymap } from '@codemirror/search';
-import {
-  closeBrackets, autocompletion, closeBracketsKeymap, completionKeymap,
-} from '@codemirror/autocomplete';
-import { lintKeymap } from '@codemirror/lint';
+const {
+  existsSync, readFileSync, writeFile, readdirSync,
+} = require('fs-extra');
+const cliProgress = require('cli-progress');
+const rollup = require('rollup');
+const { nodeResolve } = require('@rollup/plugin-node-resolve');
+const replace = require('@rollup/plugin-replace');
+const { minify } = require('terser');
+const { resolvePackageFile } = require('../init/common/resolve-package-file');
 
-const basicSetup = (() => [
-  lineNumbers(),
-  highlightActiveLineGutter(),
-  highlightSpecialChars(),
-  history(),
-  foldGutter(),
-  drawSelection(),
-  dropCursor(),
-  EditorState.allowMultipleSelections.of(true),
-  indentOnInput(),
-  syntaxHighlighting(defaultHighlightStyle, { fallback: true }),
-  bracketMatching(),
-  closeBrackets(),
-  autocompletion(),
-  rectangularSelection(),
-  crosshairCursor(),
-  highlightActiveLine(),
-  highlightSelectionMatches(),
-  keymap.of([
-    ...closeBracketsKeymap,
-    ...defaultKeymap,
-    ...searchKeymap,
-    ...historyKeymap,
-    ...foldKeymap,
-    ...completionKeymap,
-    ...lintKeymap,
-  ]),
-])();
+// Find a list of modules for given scope, eg all sub @codemirror/...
+const retrieveListOfChildModules = (scope) => {
+  const cmModules = [];
 
-const minimalSetup = (() => [
-  highlightSpecialChars(),
-  history(),
-  drawSelection(),
-  syntaxHighlighting(defaultHighlightStyle, { fallback: true }),
-  keymap.of([
-    ...defaultKeymap,
-    ...historyKeymap,
-  ])
-])();
-
-function createFromTextarea(textarea, options) {
-  console.log(options);
-
-  const view = new EditorView({
-    doc: textarea.value,
-    extensions: [minimalSetup],
+  // Get @codemirror module roots
+  const roots = [];
+  module.paths.forEach((path) => {
+    const fullPath = `${path}/${scope}`;
+    if (existsSync(fullPath)) {
+      roots.push(fullPath);
+    }
   });
-  textarea.parentNode.insertBefore(view.dom, textarea);
-  textarea.style.display = 'none';
-  if (textarea.form) {
-    textarea.form.addEventListener('submit', () => {
-      textarea.value = view.state.doc.toString();
+
+  // List of modules
+  roots.forEach((rootPath) => {
+    readdirSync(rootPath).forEach((subModule) => {
+      cmModules.push(`${scope}/${subModule}`);
     });
-  }
+  });
 
-  // Set up sizing
-  if (options.width) {
-    view.dom.style.width = options.width;
-  }
-  if (options.height) {
-    view.dom.style.minHeight = options.height;
-  }
+  return cmModules;
+};
 
-  return view;
-}
+// Build the module
+const buildModule = async (module, externalModules, destFile) => {
+  const build = await rollup.rollup({
+    input: module,
+    external: externalModules || [],
+    plugins: [
+      nodeResolve(),
+      replace({
+        preventAssignment: true,
+        'process.env.NODE_ENV': '"production"',
+      }),
+    ],
+  });
 
-export { basicSetup, minimalSetup, createFromTextarea };
+  await build.write({
+    format: 'es',
+    sourcemap: false,
+    file: destFile,
+  });
+  await build.close();
+};
+
+// Minify a js file
+const createMinified = async (filePath) => {
+  const destFile = filePath.replace('.js', '.min.js');
+  // Read source
+  const src = readFileSync(filePath, { encoding: 'utf8' });
+  // Minify
+  const min = await minify(src, { sourceMap: false, format: { comments: false } });
+  // Save result
+  await writeFile(destFile, min.code, { encoding: 'utf8', mode: 0o644 });
+};
+
+// Update joomla.asset.json for codemirror
+const updateAssetRegistry = async (modules, externalModules) => {
+  const srcPath = 'build/media_source/plg_editors_codemirror/joomla.asset.json';
+  const destPath = 'media/plg_editors_codemirror/joomla.asset.json';
+
+  // Get base JSON and update
+  const registry = JSON.parse(readFileSync(srcPath, { encoding: 'utf8' }));
+
+  // Add dependencies to base codemirror asset
+  registry.assets.forEach((asset) => {
+    if (asset.name === 'codemirror' && asset.type === 'script') {
+      asset.dependencies = externalModules;
+    }
+  });
+
+  // Create asset for each module
+  modules.forEach((module) => {
+    const packageName = module.package;
+    const modulePathJson = resolvePackageFile(`${packageName}/package.json`);
+    const moduleOptions = require(modulePathJson);
+    const asset = {
+      type: 'script',
+      name: module.package,
+      uri: module.uri.replace('.js', '.min.js'),
+      importmap: true,
+      version: moduleOptions.version,
+    };
+
+    registry.assets.push(asset);
+  });
+
+  // Write assets registry
+  await writeFile(
+    destPath,
+    JSON.stringify(registry, null, 2),
+    { encoding: 'utf8', mode: 0o644 },
+  );
+};
+
+module.exports.compileCodemirror = async () => {
+  // eslint-disable-next-line no-console
+  console.log('Building Codemirror Components...');
+
+  const cmModules = retrieveListOfChildModules('@codemirror');
+  const lModules = retrieveListOfChildModules('@lezer');
+  const externalModules = [...cmModules, ...lModules];
+  const destBasePath = 'media/vendor/codemirror/js';
+  const assets = [];
+  const tasks = [];
+
+  const progressBar = new cliProgress.SingleBar({
+    stopOnComplete: true,
+    format: '{bar} {percentage}% | {value}/{total} files done',
+  }, cliProgress.Presets.shades_classic);
+  const totalSteps = (cmModules.length + lModules.length) * 2;
+  progressBar.start(totalSteps, 0);
+
+  // Prepare @codemirror modules
+  cmModules.forEach((module) => {
+    const destFile = `${module.replace('@codemirror/', 'codemirror-')}.js`;
+    const destPath = `${destBasePath}/${destFile}`;
+    assets.push({ package: module, uri: destPath });
+
+    const task = buildModule(module, externalModules, destPath).then(() => {
+      progressBar.increment();
+      return createMinified(destPath).then(() => {
+        progressBar.increment();
+      });
+    });
+    tasks.push(task);
+  });
+
+  // Prepare @lezer modules which @codemirror depends on
+  lModules.forEach((module) => {
+    const destFile = `${module.replace('@lezer/', 'lezer-')}.js`;
+    const destPath = `${destBasePath}/${destFile}`;
+    assets.push({ package: module, uri: destPath });
+
+    const task2 = buildModule(module, externalModules, destPath).then(() => {
+      progressBar.increment();
+      return createMinified(destPath).then(() => {
+        progressBar.increment();
+      });
+    });
+    tasks.push(task2);
+  });
+  // console.log('compileCodemirror', cmModules, lModules, tasks);
+
+  return Promise.all(tasks).then(() => {
+    progressBar.stop();
+    return updateAssetRegistry(assets, externalModules);
+  });
+};
