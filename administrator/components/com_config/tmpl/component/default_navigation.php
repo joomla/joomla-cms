464f32f40b9499ca269ef81d9f94faf6b1bce9db
Allon Moritz <allon.moritz@digital-peak.com>
2023-06-27 14:47:01 +0200

    cs

--- administrator/components/com_config/tmpl/component/default_navigation.php
+++ administrator/components/com_config/tmpl/component/default_navigation.php
@@ -8,7 +8,6 @@
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
-
 defined('_JEXEC') or die;
 ?>
 <ul class="nav flex-column">
--- administrator/components/com_finder/tmpl/statistics/default.php
+++ administrator/components/com_finder/tmpl/statistics/default.php
@@ -15,7 +15,12 @@ use Joomla\CMS\Language\Text;
 ?>
 <div class="container-popup">
     <table class="table table-sm">
-    <caption class="caption-top"><?php echo Text::sprintf('COM_FINDER_STATISTICS_STATS_DESCRIPTION', number_format($this->data->term_count, 0, $this->_('DECIMALS_SEPARATOR'), $this->_('THOUSANDS_SEPARATOR')), number_format($this->data->link_count, 0, $this->_('DECIMALS_SEPARATOR'), $this->_('THOUSANDS_SEPARATOR')), number_format($this->data->taxonomy_node_count, 0, $this->_('DECIMALS_SEPARATOR'), $this->_('THOUSANDS_SEPARATOR')), number_format($this->data->taxonomy_branch_count, 0, $this->_('DECIMALS_SEPARATOR'), $this->_('THOUSANDS_SEPARATOR'))); ?></caption>
+    <caption class="caption-top"><?php echo Text::sprintf(
+        'COM_FINDER_STATISTICS_STATS_DESCRIPTION',
+        number_format($this->data->term_count, 0, $this->_('DECIMALS_SEPARATOR'), $this->_('THOUSANDS_SEPARATOR')),
+        number_format($this->data->link_count, 0, $this->_('DECIMALS_SEPARATOR'), $this->_('THOUSANDS_SEPARATOR')),
+        number_format($this->data->taxonomy_node_count, 0, $this->_('DECIMALS_SEPARATOR'), $this->_('THOUSANDS_SEPARATOR')),
+        number_format($this->data->taxonomy_branch_count, 0, $this->_('DECIMALS_SEPARATOR'), $this->_('THOUSANDS_SEPARATOR'))); ?></caption>
         <thead>
             <tr>
                 <th scope="col">
