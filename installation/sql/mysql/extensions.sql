526f839f784633f055abf76d2fe36f5fca1aa45d
Nicola Galgano <optimus4joomla@gmail.com>
2023-07-01 09:41:32 +0200

    bettersyntax

--- installation/sql/mysql/extensions.sql
+++ installation/sql/mysql/extensions.sql
@@ -925,14 +925,13 @@ CREATE TABLE IF NOT EXISTS `#__scheduler_tasks` (
   KEY `idx_checked_out` (`checked_out`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 DEFAULT COLLATE = utf8mb4_unicode_ci;
 
-SELECT @myLeft := lft FROM `#__assets` WHERE id = 1;
+SET @myLeft = (SELECT lft FROM `#__assets` WHERE id = 1);
 UPDATE `#__assets` SET rgt = rgt + 2 WHERE rgt > @myLeft;
 UPDATE `#__assets` SET lft = lft + 2 WHERE lft > @myLeft;
-INSERT INTO `#__assets`(`parent_id`, `name`, `title`, `lft`, `rgt`, `level`, `rules`) VALUES(1, 'com_scheduler.task.22', 'com_scheduler.task.22', @myLeft + 1, @myLeft + 2, 1, '{}');
+INSERT INTO `#__assets`(`parent_id`, `name`, `title`, `lft`, `rgt`, `level`, `rules`) VALUES(1, 'com_scheduler.task.1', 'com_scheduler.task.1', @myLeft + 1, @myLeft + 2, 1, '{}');
 SET @last_id_in_assets = LAST_INSERT_ID();
-INSERT INTO `#__scheduler_tasks`(`id`, `asset_id`, `title`, `type`, `execution_rules`, `cron_rules`, `state`, `last_exit_code`, `params`, `created`)
-VALUES (1, @last_id_in_assets,'UpdateNotification','update.notification','{"rule-type":"interval-hours","interval-hours":"24","exec-day":"01","exec-time":"08:00"}', '{"type":"interval","exp":"PT24H"}', 1, 0, '{}', now());
-
+INSERT INTO `#__scheduler_tasks`(`asset_id`, `title`, `type`, `execution_rules`, `cron_rules`, `state`, `last_exit_code`, `params`, `created`)
+VALUES (@last_id_in_assets,'UpdateNotification','update.notification', CONCAT('{"rule-type":"interval-hours","interval-hours":"24","exec-day":"01","exec-time":"', TIME_FORMAT(NOW(), '%H:00'), '"}'), '{"type":"interval","exp":"PT24H"}', 1, 0, '{}', now());
 
 -- --------------------------------------------------------
 
