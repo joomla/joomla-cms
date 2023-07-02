3d8e6d2d3035e8a678a42b905e4a4bee5b83cfb5
zero-24 <zero-24@users.noreply.github.com>
2023-07-02 16:38:30 +0200

    reset to dev

--- libraries/src/Version.php
+++ libraries/src/Version.php
@@ -60,7 +60,7 @@ final class Version
 	 * @var    string
 	 * @since  3.8.0
 	 */
-	const EXTRA_VERSION = 'rc1';
+	const EXTRA_VERSION = 'rc2-dev';
 
 	/**
 	 * Release version.
@@ -78,7 +78,7 @@ final class Version
 	 * @since  3.5
 	 * @deprecated  4.0  Use separated version constants instead
 	 */
-	const DEV_LEVEL = '12-rc1';
+	const DEV_LEVEL = '12-rc2-dev';
 
 	/**
 	 * Development status.
@@ -86,7 +86,7 @@ final class Version
 	 * @var    string
 	 * @since  3.5
 	 */
-	const DEV_STATUS = 'Release Candidate';
+	const DEV_STATUS = 'Development';
 
 	/**
 	 * Build number.
@@ -119,7 +119,7 @@ final class Version
 	 * @var    string
 	 * @since  3.5
 	 */
-	const RELTIME = '15:16';
+	const RELTIME = '15:36';
 
 	/**
 	 * Release timezone.
