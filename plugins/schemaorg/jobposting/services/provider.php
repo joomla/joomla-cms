a7c9b1e7b8ca93fe51a259d4e295d92659680c90
Shazma Siddiqui <43249198+shazmasiddiqui@users.noreply.github.com>
2023-07-13 10:38:25 +0530

    Update plugins/schemaorg/jobposting/services/provider.php
    Co-authored-by: Brian Teeman <brian@teeman.net>

--- plugins/schemaorg/jobposting/services/provider.php
+++ plugins/schemaorg/jobposting/services/provider.php
@@ -4,7 +4,7 @@
  * @package     Joomla.Plugin
  * @subpackage  Schemaorg.jobposting
  *
- * @copyright   (C) 2022 Open Source Matters, Inc. <https://www.joomla.org>
+ * @copyright   (C) 2023 Open Source Matters, Inc. <https://www.joomla.org>
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
