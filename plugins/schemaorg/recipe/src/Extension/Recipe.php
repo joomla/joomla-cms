45201f55268b2657cf5a18a15d5da98e1a7a858a
Shazma Siddiqui <43249198+shazmasiddiqui@users.noreply.github.com>
2023-07-13 10:36:40 +0530

    Update plugins/schemaorg/recipe/src/Extension/Recipe.php
    Co-authored-by: Brian Teeman <brian@teeman.net>

--- plugins/schemaorg/recipe/src/Extension/Recipe.php
+++ plugins/schemaorg/recipe/src/Extension/Recipe.php
@@ -22,7 +22,7 @@ use Joomla\Event\SubscriberInterface;
 /**
  * Schemaorg Plugin
  *
- * @since  _DEPLOY_VERSION__
+ * @since  __DEPLOY_VERSION__
  */
 final class Recipe extends CMSPlugin implements SubscriberInterface
 {
