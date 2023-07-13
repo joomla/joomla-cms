58a83623bf074d423ae1fb0e2627040f51710689
Shazma Siddiqui <43249198+shazmasiddiqui@users.noreply.github.com>
2023-07-13 10:37:02 +0530

    Update plugins/schemaorg/person/src/Extension/Person.php
    Co-authored-by: Brian Teeman <brian@teeman.net>

--- plugins/schemaorg/person/src/Extension/Person.php
+++ plugins/schemaorg/person/src/Extension/Person.php
@@ -3,7 +3,7 @@
 /**
  * @package     Joomla.Plugin
  *
- * @copyright   (C) 2022 Open Source Matters, Inc. <https://www.joomla.org>
+ * @copyright   (C) 2023 Open Source Matters, Inc. <https://www.joomla.org>
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
 
  * @phpcs:disable PSR1.Classes.ClassDeclaration.MissingNamespace
