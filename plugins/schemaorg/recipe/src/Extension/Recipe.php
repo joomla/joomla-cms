161a8e1cfd961acb124ab84fe114ec578b4be26d
Shazma Siddiqui <43249198+shazmasiddiqui@users.noreply.github.com>
2023-07-13 10:45:47 +0530

    Update plugins/schemaorg/recipe/src/Extension/Recipe.php
    Co-authored-by: Brian Teeman <brian@teeman.net>

--- plugins/schemaorg/recipe/src/Extension/Recipe.php
+++ plugins/schemaorg/recipe/src/Extension/Recipe.php
@@ -4,7 +4,7 @@
  * @package     Joomla.Plugin
  * @subpackage  Schemaorg.recipe
  *
- * @copyright   (C) 2022 Open Source Matters, Inc. <https://www.joomla.org>
+ * @copyright   (C) 2023 Open Source Matters, Inc. <https://www.joomla.org>
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
 
  * @phpcs:disable PSR1.Classes.ClassDeclaration.MissingNamespace
