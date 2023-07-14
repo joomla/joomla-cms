0d1d1be6432a619337024658f01a19d88f027675
Geraint Edwards <jevents@gwesystems.com>
2023-07-14 15:45:43 +0100

    a bit more of a clean up

--- administrator/modules/mod_guidedtours/tmpl/default.php
+++ administrator/modules/mod_guidedtours/tmpl/default.php
@@ -1,11 +1,11 @@
 <?php
 
 /**
- * @package         Joomla.Administrator
- * @subpackage      mod_guidedtours
+ * @package     Joomla.Administrator
+ * @subpackage  mod_guidedtours
  *
  * @copyright   (C) 2023 Open Source Matters, Inc. <https://www.joomla.org>
- * @license         GNU General Public License version 2 or later; see LICENSE.txt
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 defined('_JEXEC') or die;
@@ -16,7 +16,7 @@ use Joomla\CMS\Uri\Uri;
 
 $hideLinks = $app->getInput()->getBool('hidemainmenu');
 
-if ($hideLinks || ! $tours) {
+if ($hideLinks || !$tours) {
     return;
 }
 
@@ -35,7 +35,7 @@ $allTours  = [];
 $lang->load('com_guidedtours.sys', JPATH_ADMINISTRATOR);
 
 foreach ($tours as $tour) :
-    if (count(array_intersect([ '*', $extension ], $tour->extensions))) :
+    if (count(array_intersect(['*', $extension], $tour->extensions))) :
         $listTours[] = $tour;
     endif;
 
