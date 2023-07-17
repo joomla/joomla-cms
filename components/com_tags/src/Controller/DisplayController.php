63ad158fd44c71ff84bc39a5785bc45652095a99
brian teeman <brian@teeman.net>
2023-07-17 14:59:28 +0100

    cleanup

--- components/com_tags/src/Controller/DisplayController.php
+++ components/com_tags/src/Controller/DisplayController.php
@@ -28,8 +28,7 @@ class DisplayController extends BaseController
      * Method to display a view.
      *
      * @param   boolean        $cachable   If true, the view output will be cached
-     * @param   mixed|boolean  $urlparams  An array of safe URL parameters and their
-     *                                     variable types.
+     * @param   mixed|boolean  $urlparams  An array of safe URL parameters and their variable types.
      * @see     \Joomla\CMS\Filter\InputFilter::clean() for valid values.
      *
      * @return  static  This object to support chaining.
