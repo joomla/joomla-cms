b93f5c44317d6caf4ab4b1d1fc703492bceee48c
heelc29 <66922325+heelc29@users.noreply.github.com>
2023-07-25 22:30:39 +0200

    [5.0] schemaorg replace JPATH_PLATFORM with _JEXEC (#41244)
    * [5.0] schemaorg replace JPATH_PLATFORM with _JEXEC
    
    * add copyright

--- libraries/src/Form/Field/SchemaorgComponentSectionsField.php
+++ libraries/src/Form/Field/SchemaorgComponentSectionsField.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+ * Joomla! Content Management System
+ *
+ * @copyright  (C) 2023 Open Source Matters, Inc. <https://www.joomla.org>
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
 namespace Joomla\CMS\Form\Field;
 
 use Joomla\CMS\Factory;
@@ -8,7 +15,7 @@ use Joomla\CMS\Language\Text;
 use Joomla\CMS\Schemaorg\SchemaorgServiceInterface;
 
 // phpcs:disable PSR1.Files.SideEffects
-\defined('JPATH_PLATFORM') or die;
+\defined('_JEXEC') or die;
 // phpcs:enable PSR1.Files.SideEffects
 
 /**
