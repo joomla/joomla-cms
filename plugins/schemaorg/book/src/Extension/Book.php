4fe31d49bd629197cba1a52b6b65877bda429e2b
brian teeman <brian@teeman.net>
2023-07-25 16:01:46 +0100

    [5.0] schemaorg missing namespace
    Removes the code that disables phpcs checking for a missing namespace as it is present.
    
    fixes some codestyle at the same time
    
    codereview and if it passes phpcs checks

--- plugins/schemaorg/book/src/Extension/Book.php
+++ plugins/schemaorg/book/src/Extension/Book.php
@@ -2,12 +2,10 @@
 
 /**
  * @package     Joomla.Plugin
-  * @subpackage  Schemaorg.book
-  *
+ * @subpackage  Schemaorg.book
+ *
  * @copyright   (C) 2023 Open Source Matters, Inc. <https://www.joomla.org>
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
-
- * @phpcs:disable PSR1.Classes.ClassDeclaration.MissingNamespace
  */
 
 namespace Joomla\Plugin\Schemaorg\Book\Extension;
