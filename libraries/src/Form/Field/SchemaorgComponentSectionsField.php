ce6be43b34a66553b620163122da092a3eb1515a
Christian Heel <66922325+heelc29@users.noreply.github.com>
2023-07-25 20:44:23 +0200

    [5.0] schemaorg replace JPATH_PLATFORM with _JEXEC

--- libraries/src/Form/Field/SchemaorgComponentSectionsField.php
+++ libraries/src/Form/Field/SchemaorgComponentSectionsField.php
@@ -8,7 +8,7 @@ use Joomla\CMS\Language\Text;
 use Joomla\CMS\Schemaorg\SchemaorgServiceInterface;
 
 // phpcs:disable PSR1.Files.SideEffects
-\defined('JPATH_PLATFORM') or die;
+\defined('_JEXEC') or die;
 // phpcs:enable PSR1.Files.SideEffects
 
 /**
