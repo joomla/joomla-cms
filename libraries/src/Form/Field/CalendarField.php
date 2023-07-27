ca42819bc1ec5c3235ad503631a5fa9250ae70ff
Сергей <koreshs@mail.ru>
2023-07-27 21:33:43 +0300

    New 'strftime()' method Date class

--- libraries/src/Form/Field/CalendarField.php
+++ libraries/src/Form/Field/CalendarField.php
@@ -14,6 +14,7 @@ use Joomla\CMS\Factory;
 use Joomla\CMS\Form\FormField;
 use Joomla\CMS\Language\Text;
 use Joomla\Registry\Registry;
+use Joomla\CMS\Date'
 
 // phpcs:disable PSR1.Files.SideEffects
 \defined('JPATH_PLATFORM') or die;
@@ -319,7 +320,7 @@ class CalendarField extends FormField
                 $date        = \DateTimeImmutable::createFromFormat('U', strtotime($this->value));
                 $this->value = $date->format($this->filterFormat);
             } else {
-                $this->value = (new DateTime($this->value))->format($this->format);
+                $this->value = \Date::strftime($this->format, strtotime($this->value));
             }
 
             date_default_timezone_set($tz);
