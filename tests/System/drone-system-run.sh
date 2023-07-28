a819701224965871c578cb390f76debc2b648c25
Fedik <getthesite@gmail.com>
2023-07-28 15:45:30 +0300

    Revert "testing test drone system"
    This reverts commit e48e0c0fe598dfbbb99d2b9570786aedf62d9996.

--- tests/System/drone-system-run.sh
+++ tests/System/drone-system-run.sh
@@ -5,14 +5,6 @@ TEST_GROUP=$2
 DB_ENGINE=$3
 DB_HOST=$4
 
-DB_USERNAME=root
-DB_PASSWORD=joomla_ut
-DB_NAME=test_joomla
-
-#BASE_URL="http://localhost:8181/${TEST_GROUP}"
-
-BASE_URL="http://localhost:8181"
-
 echo "[RUNNER] Prepare test environment"
 
 # Switch to Joomla base directory
@@ -26,10 +18,9 @@ chown -R www-data /tests/www/$TEST_GROUP/
 chmod -R 777 /tests/www/$TEST_GROUP/images
 
 echo "[RUNNER] Start Apache"
-#apache2ctl -D FOREGROUND &
-JOOMLA_INSTALLATION_DISABLE_LOCALHOST_CHECK="1" php -S 127.0.0.1:8181 -t /tests/www/$TEST_GROUP/ &
+apache2ctl -D FOREGROUND &
 
 echo "[RUNNER] Run cypress tests"
 chmod +rwx /root
 
-npx cypress run --headed --browser=firefox --e2e --env cmsPath="/tests/www/${TEST_GROUP}",db_type="${DB_ENGINE}",db_host="${DB_HOST}",db_name="${DB_NAME}",db_user="${DB_USERNAME}",db_password="${DB_PASSWORD}",db_prefix="${TEST_GROUP}_" --config baseUrl="${BASE_URL}",screenshotsFolder=$JOOMLA_BASE/tests/System/output/screenshots
+npx cypress run --browser=firefox --e2e --env cmsPath=/tests/www/$TEST_GROUP,db_type=$DB_ENGINE,db_host=$DB_HOST,db_password=joomla_ut,db_prefix="${TEST_GROUP}_" --config baseUrl=http://localhost/$TEST_GROUP,screenshotsFolder=$JOOMLA_BASE/tests/System/output/screenshots
