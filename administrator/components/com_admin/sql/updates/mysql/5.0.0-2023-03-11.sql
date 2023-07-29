0dee60fd1dedc9cbd5052eae83e07c3b8d581f37
Harald Leithner <leithner@itronic.at>
2023-07-29 12:24:36 +0200

    Move the compatibility plugin from system to behaviour folder
    This allows system plugins using the JPlugin class to still work with joomla 5.0 compatibility enabled

--- administrator/components/com_admin/sql/updates/mysql/5.0.0-2023-03-11.sql
+++ administrator/components/com_admin/sql/updates/mysql/5.0.0-2023-03-11.sql
@@ -1,4 +1,4 @@
 DROP TABLE IF EXISTS `#__utf8_conversion`;
 
 INSERT INTO `#__extensions` (`package_id`, `name`, `type`, `element`, `folder`, `client_id`, `enabled`, `access`, `protected`, `locked`, `manifest_cache`, `params`, `custom_data`, `ordering`, `state`) VALUES
-(0, 'plg_system_compat', 'plugin', 'compat', 'system', 0, 1, 1, 0, 1, '', '{"classes_aliases":"1","es5_assets":"1"}', '', 0, 0);
+(0, 'plg_behaviour_compat', 'plugin', 'compat', 'behaviour', 0, 1, 1, 0, 1, '', '{"classes_aliases":"1","es5_assets":"1"}', '', -1, 0);
