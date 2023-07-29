71c7f5f41e76f865970b1e2a3c54d8fec0382e1b
Harald Leithner <leithner@itronic.at>
2023-07-29 13:27:49 +0200

    Fix SQL update scripts for compat plugin

--- administrator/components/com_admin/sql/updates/postgresql/5.0.0-2023-03-11.sql
+++ administrator/components/com_admin/sql/updates/postgresql/5.0.0-2023-03-11.sql
@@ -1,4 +1,1 @@
 DROP TABLE IF EXISTS "#__utf8_conversion";
-
-INSERT INTO "#__extensions" ("package_id", "name", "type", "element", "folder", "client_id", "enabled", "access", "protected", "locked", "manifest_cache", "params", "custom_data", "ordering", "state") VALUES
-(0, 'plg_behaviour_compat', 'plugin', 'compat', 'behaviour', 0, 1, 1, 0, 1, '', '{"classes_aliases":"1","es5_assets":"1"}', '', -1, 0);
