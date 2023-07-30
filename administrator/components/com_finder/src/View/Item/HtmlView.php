29117ee5762e0138441c27c71bbc2eda5347c1a5
Harald Leithner <leithner@itronic.at>
2023-07-30 17:29:27 +0200

    Update administrator/components/com_finder/src/View/Item/HtmlView.php
    Co-authored-by: heelc29 <66922325+heelc29@users.noreply.github.com>

--- administrator/components/com_finder/src/View/Item/HtmlView.php
+++ administrator/components/com_finder/src/View/Item/HtmlView.php
@@ -12,6 +12,7 @@ namespace Joomla\Component\Finder\Administrator\View\Item;
 
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\MVC\View\HtmlView as BaseHtmlView;
+use Joomla\CMS\Router\Route;
 use Joomla\CMS\Toolbar\Toolbar;
 use Joomla\CMS\Toolbar\ToolbarHelper;
 
