joomla/joomla-cms

commit 3d5a9aa00dd699cfc3e4c300fedb638a1d62531d

Display quickicons only for active components (#40715)

administrator/modules/mod_quickicon/src/Helper/QuickIconHelper.php
@@ -11,6 +11,7 @@
 namespace Joomla\Module\Quickicon\Administrator\Helper;
 
 use Joomla\CMS\Application\CMSApplication;
+use Joomla\CMS\Component\ComponentHelper;
 use Joomla\CMS\Factory;
 use Joomla\CMS\Plugin\PluginHelper;
 use Joomla\CMS\Router\Route;
@@ -115,7 +116,7 @@ class QuickIconHelper
                 $this->buttons[$key][] = $tmp;
             }
 
-            if ($params->get('show_tags')) {
+            if (ComponentHelper::isEnabled('com_tags') && $params->get('show_tags')) {
                 $tmp = [
                     'image'   => 'icon-tag',
                     'link'    => Route::_('index.php?option=com_tags&view=tags'),
@@ -281,7 +282,7 @@ class QuickIconHelper
                 ];
             }
 
-            if ($params->get('show_banners')) {
+            if (ComponentHelper::isEnabled('com_banners') && $params->get('show_banners')) {
                 $tmp = [
                     'image'   => 'icon-bookmark banners',
                     'link'    => Route::_('index.php?option=com_banners&view=banners'),
@@ -298,7 +299,7 @@ class QuickIconHelper
                 $this->buttons[$key][] = $tmp;
             }
 
-            if ($params->get('show_contact')) {
+            if (ComponentHelper::isEnabled('com_contacts') && $params->get('show_contact')) {
                 $tmp = [
                     'image'   => 'icon-address-book contact',
                     'link'    => Route::_('index.php?option=com_contact&view=contacts'),
@@ -315,7 +316,7 @@ class QuickIconHelper
                 $this->buttons[$key][] = $tmp;
             }
 
-            if ($params->get('show_newsfeeds')) {
+            if (ComponentHelper::isEnabled('com_newsfeeds') && $params->get('show_newsfeeds')) {
                 $tmp = [
                     'image'   => 'icon-rss newsfeeds',
                     'link'    => Route::_('index.php?option=com_newsfeeds&view=newsfeeds'),
@@ -332,7 +333,7 @@ class QuickIconHelper
                 $this->buttons[$key][] = $tmp;
             }
 
-            if ($params->get('show_redirect')) {
+            if (ComponentHelper::isEnabled('com_redirect') && $params->get('show_redirect')) {
                 $this->buttons[$key][] = [
                     'image'   => 'icon-map-signs redirect',
                     'link'    => Route::_('index.php?option=com_redirect&view=links'),
@@ -343,7 +344,7 @@ class QuickIconHelper
                 ];
             }
 
-            if ($params->get('show_associations')) {
+            if (ComponentHelper::isEnabled('com_associations') && $params->get('show_associations')) {
                 $this->buttons[$key][] = [
                     'image'  => 'icon-language',
                     'link'   => Route::_('index.php?option=com_associations&view=associations'),
@@ -353,7 +354,7 @@ class QuickIconHelper
                 ];
             }
 
-            if ($params->get('show_finder')) {
+            if (ComponentHelper::isEnabled('com_finder') && $params->get('show_finder')) {
                 $this->buttons[$key][] = [
                     'image'  => 'icon-search-plus finder',
                     'link'   => Route::_('index.php?option=com_finder&view=index'),

commit 9981e3bb4f5a0330d73eb2909efe4426165aa5c5

[4.3] Display Contact quickicon
Fixes a typo introduced with #40715 that prevented the contacts quickicon being displayed

administrator/modules/mod_quickicon/src/Helper/QuickIconHelper.php
@@ -299,7 +299,7 @@ class QuickIconHelper
                 $this->buttons[$key][] = $tmp;
             }
 
-            if (ComponentHelper::isEnabled('com_contacts') && $params->get('show_contact')) {
+            if (ComponentHelper::isEnabled('com_contact') && $params->get('show_contact')) {
                 $tmp = [
                     'image'   => 'icon-address-book contact',
                     'link'    => Route::_('index.php?option=com_contact&view=contacts'),