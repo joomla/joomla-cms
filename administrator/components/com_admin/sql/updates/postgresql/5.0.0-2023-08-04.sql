a5f9c11b82be8a3967a56d0190f075a2016d7856
Christian Heel <66922325+heelc29@users.noreply.github.com>
2023-08-11 19:27:18 +0200

    remove old mail template on update

--- administrator/components/com_admin/sql/updates/postgresql/5.0.0-2023-08-04.sql
+++ administrator/components/com_admin/sql/updates/postgresql/5.0.0-2023-08-04.sql
@@ -2,4 +2,5 @@ INSERT INTO "#__extensions" ("name", "type", "element", "folder", "client_id", "
 ('plg_task_updatenotification', 'plugin', 'updatenotification', 'task', 0, 1, 1, 0, 1, '', '{}', '', NULL, NULL, 0, 0);
 INSERT INTO "#__mail_templates" ("template_id", "extension", "language", "subject", "body", "htmlbody", "attachments", "params") VALUES
 ('plg_task_updatenotification.mail', 'plg_task_updatenotification', '', 'PLG_TASK_UPDATENOTIFICATION_EMAIL_SUBJECT', 'PLG_TASK_UPDATENOTIFICATION_EMAIL_BODY', '', '', '{"tags":["newversion","curversion","sitename","url","link","releasenews"]}');
+DELETE FROM "#__mail_templates" WHERE "template_id" = 'plg_system_updatenotification.mail';
 DELETE FROM "#__postinstall_messages" WHERE "condition_file" = 'site://plugins/system/updatenotification/postinstall/updatecachetime.php';
