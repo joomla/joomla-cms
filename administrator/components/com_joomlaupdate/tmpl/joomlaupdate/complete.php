69a547ce722c8e18b8c047f69ef88dee581ee9fc
Fedik <getthesite@gmail.com>
2023-08-16 17:01:36 +0300

    phpcs

--- administrator/components/com_joomlaupdate/tmpl/joomlaupdate/complete.php
+++ administrator/components/com_joomlaupdate/tmpl/joomlaupdate/complete.php
@@ -23,24 +23,24 @@ $installerMsg = $this->state->get('installer_message');
 <div class="card">
     <h2 class="card-header"><?php echo Text::_('COM_JOOMLAUPDATE_VIEW_COMPLETE_HEADING'); ?></h2>
     <div class="card-body">
-        <?php if (!$hadErrors): ?>
+        <?php if (!$hadErrors) : ?>
             <div class="alert alert-success">
                 <span class="icon-check-circle" aria-hidden="true"></span><span class="visually-hidden"><?php echo Text::_('NOTICE'); ?></span>
                 <?php echo Text::sprintf('COM_JOOMLAUPDATE_VIEW_COMPLETE_MESSAGE', '&#x200E;' . JVERSION); ?>
             </div>
-        <?php else: ?>
+        <?php else : ?>
             <div class="alert alert-error">
                 <span class="icon-check-circle" aria-hidden="true"></span><span class="visually-hidden"><?php echo Text::_('NOTICE'); ?></span>
                 <?php echo Text::sprintf('COM_JOOMLAUPDATE_VIEW_COMPLETE_WITH_ERROR_MESSAGE', $logFile); ?>
             </div>
 
-			<h3><?php echo Text::_('COM_JOOMLAUPDATE_VIEW_COMPLETE_UPDATE_ERRORS'); ?></h3>
-			<?php foreach ($errors as $error): ?>
-				<div class="alert alert-error"><?php echo $error; ?></div>
-			<?php endforeach; ?>
+            <h3><?php echo Text::_('COM_JOOMLAUPDATE_VIEW_COMPLETE_UPDATE_ERRORS'); ?></h3>
+            <?php foreach ($errors as $error) : ?>
+                <div class="alert alert-error"><?php echo $error; ?></div>
+            <?php endforeach; ?>
         <?php endif; ?>
 
-        <?php if ($installerMsg): ?>
+        <?php if ($installerMsg) : ?>
         <div>
             <h3><?php echo Text::_('COM_JOOMLAUPDATE_VIEW_COMPLETE_INSTALLER_MESSAGE'); ?></h3>
             <div class="alert alert-warning"><?php echo $installerMsg ?></div>
