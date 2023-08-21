1f8446d7f2f1563a8bc33d608902141eb62d463c
Richard Fath <richard.fath@t-online.de>
2023-08-21 14:34:54 +0200

    Limit migration on update to relevant core plugins

--- administrator/components/com_admin/sql/updates/mysql/5.0.0-2023-08-21.sql
+++ administrator/components/com_admin/sql/updates/mysql/5.0.0-2023-08-21.sql
@@ -2,5 +2,6 @@ UPDATE `#__extensions`
    SET `params` = JSON_REPLACE(`params`, '$.filter' , '\\Joomla\\CMS\\Component\\ComponentHelper::filterText')
  WHERE `type` = 'plugin'
    AND `folder` = 'fields'
+   AND `element` IN ('editor', 'text', 'textarea')
    AND `params` <> ''
    AND JSON_EXTRACT(`params`, '$.filter') = 'JComponentHelper::filterText';
--- administrator/components/com_admin/sql/updates/postgresql/5.0.0-2023-08-21.sql
+++ administrator/components/com_admin/sql/updates/postgresql/5.0.0-2023-08-21.sql
@@ -2,5 +2,6 @@ UPDATE "#__extensions"
    SET "params" = jsonb_set("params"::jsonb, '{filter}' , '"\\Joomla\\CMS\\Component\\ComponentHelper::filterText"')
  WHERE "type" = 'plugin'
    AND "folder" = 'fields'
+   AND "element" IN ('editor', 'text', 'textarea')
    AND "params" <> ''
    AND "params"::jsonb->>'filter' = 'JComponentHelper::filterText';
