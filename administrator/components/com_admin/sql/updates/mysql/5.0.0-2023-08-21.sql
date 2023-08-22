3b90e72ebb82a37ad54bf3fe559dbc9f551f32c8
Richard Fath <richard.fath@t-online.de>
2023-08-22 12:36:14 +0200

    Revert changes for MySQL and add a comment

--- administrator/components/com_admin/sql/updates/mysql/5.0.0-2023-08-21.sql
+++ administrator/components/com_admin/sql/updates/mysql/5.0.0-2023-08-21.sql
@@ -1,5 +1,9 @@
+--
+-- Attention: In the below SQL statements, the value of the filter is unescaped, i.e. uses "\\", while
+-- in base.sql the same value is using "\\\\". This is expected because of how JSON_REPLACE works.
+--
 UPDATE `#__extensions`
-   SET `params` = JSON_REPLACE(`params`, '$.filter' , JSON_UNQUOTE('\\\\Joomla\\\\CMS\\\\Component\\\\ComponentHelper::filterText'))
+   SET `params` = JSON_REPLACE(`params`, '$.filter' , '\\Joomla\\CMS\\Component\\ComponentHelper::filterText')
  WHERE `type` = 'plugin'
    AND `folder` = 'fields'
    AND `element` IN ('editor', 'text', 'textarea')
@@ -7,7 +11,7 @@ UPDATE `#__extensions`
    AND JSON_EXTRACT(`params`, '$.filter') = 'JComponentHelper::filterText';
 
 UPDATE `#__fields`
-   SET `fieldparams` = JSON_REPLACE(`fieldparams`, '$.filter' , JSON_UNQUOTE('\\\\Joomla\\\\CMS\\\\Component\\\\ComponentHelper::filterText'))
+   SET `fieldparams` = JSON_REPLACE(`fieldparams`, '$.filter' , '\\Joomla\\CMS\\Component\\ComponentHelper::filterText')
  WHERE `type` IN ('editor', 'text', 'textarea')
    AND `fieldparams` <> ''
    AND JSON_EXTRACT(`fieldparams`, '$.filter') = 'JComponentHelper::filterText';
