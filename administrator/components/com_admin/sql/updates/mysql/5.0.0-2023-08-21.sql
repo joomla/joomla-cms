e9b29b6c273e3ad2aa886de747b75fe48ca2d0a0
Richard Fath <richard67@users.noreply.github.com>
2023-08-22 12:47:23 +0200

    [5.0] Fix JSON error on update with PostgreSQL (#41430)
    * Use quadrupel backslashes in JSON also in update SQL
    
    * Use JSON_UNQUOTE for MySQL for consistency with base.sql
    
    * Revert changes for MySQL and add a comment

--- administrator/components/com_admin/sql/updates/mysql/5.0.0-2023-08-21.sql
+++ administrator/components/com_admin/sql/updates/mysql/5.0.0-2023-08-21.sql
@@ -1,3 +1,7 @@
+--
+-- Attention: In the below SQL statements, the value of the filter is unescaped, i.e. uses "\\", while
+-- in base.sql the same value is using "\\\\". This is expected because of how JSON_REPLACE works.
+--
 UPDATE `#__extensions`
    SET `params` = JSON_REPLACE(`params`, '$.filter' , '\\Joomla\\CMS\\Component\\ComponentHelper::filterText')
  WHERE `type` = 'plugin'
