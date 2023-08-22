6f3e031b473656f4054450165f6d2b67d3952783
Fedik <getthesite@gmail.com>
2023-08-22 16:19:42 +0300

    Make sure that all registry files parsed

--- libraries/src/WebAsset/WebAssetRegistry.php
+++ libraries/src/WebAsset/WebAssetRegistry.php
@@ -155,6 +155,9 @@ class WebAssetRegistry implements WebAssetRegistryInterface, DispatcherAwareInte
             $this->assets[$type] = [];
         }
 
+        // Check if any new file was added
+        $this->parseRegistryFiles();
+
         $eventChange = 'new';
         $eventAsset  = $asset;
 
@@ -183,6 +186,9 @@ class WebAssetRegistry implements WebAssetRegistryInterface, DispatcherAwareInte
      */
     public function remove(string $type, string $name): WebAssetRegistryInterface
     {
+        // Check if any new file was added
+        $this->parseRegistryFiles();
+
         if (!empty($this->assets[$type][$name])) {
             $asset = $this->assets[$type][$name];
 
@@ -206,6 +212,9 @@ class WebAssetRegistry implements WebAssetRegistryInterface, DispatcherAwareInte
      */
     public function exists(string $type, string $name): bool
     {
+        // Check if any new file was added
+        $this->parseRegistryFiles();
+
         return !empty($this->assets[$type][$name]);
     }
 
