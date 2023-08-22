2c0f4bbde2a129f452aca0432eea703326877557
Richard Fath <richard67@users.noreply.github.com>
2023-08-22 10:38:18 +0200

    [5.0] SQL fix and update scripts for PR #41259  (#41417)
    * Change JComponentHelper filter usage to namespace version
    
    This commit changes all usages of JComponentHelper to \Joomla\CMS\Component\ComponentHelper.
    Additionally an exception is thrown when JComponehtHelper is used as filter but is not resolvable.
    
    Existing entries have to converted manually or per migration script.
    
    * Escape backslash in filter parameter value
    
    * Add update SQL scripts
    
    * Limit migration on update to relevant core plugins
    
    * Migrate also custom field parameters on update
    
    * PostgreSQL: Don't add parameter value when missing
    
    * Revert "PostgreSQL: Don't add parameter value when missing"
    
    This reverts commit 95d89720d56a77978a11c03560ae4616c40ac1e8.
    
    * Update installation/sql/postgresql/base.sql
    
    Co-authored-by: Richard Fath <richard67@users.noreply.github.com>
    
    * Update installation/sql/mysql/base.sql
    
    Co-authored-by: Richard Fath <richard67@users.noreply.github.com>
    
    * Update installation/sql/mysql/base.sql
    
    Co-authored-by: Richard Fath <richard67@users.noreply.github.com>
    
    * Update installation/sql/postgresql/base.sql
    
    Co-authored-by: Richard Fath <richard67@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Harald Leithner <leithner@itronic.at>
    Co-authored-by: Allon Moritz <allon.moritz@digital-peak.com>

---
+++ administrator/components/com_admin/sql/updates/mysql/5.0.0-2023-08-21.sql
@@ -0,0 +1,13 @@
+UPDATE `#__extensions`
+   SET `params` = JSON_REPLACE(`params`, '$.filter' , '\\Joomla\\CMS\\Component\\ComponentHelper::filterText')
+ WHERE `type` = 'plugin'
+   AND `folder` = 'fields'
+   AND `element` IN ('editor', 'text', 'textarea')
+   AND `params` <> ''
+   AND JSON_EXTRACT(`params`, '$.filter') = 'JComponentHelper::filterText';
+
+UPDATE `#__fields`
+   SET `fieldparams` = JSON_REPLACE(`fieldparams`, '$.filter' , '\\Joomla\\CMS\\Component\\ComponentHelper::filterText')
+ WHERE `type` IN ('editor', 'text', 'textarea')
+   AND `fieldparams` <> ''
+   AND JSON_EXTRACT(`fieldparams`, '$.filter') = 'JComponentHelper::filterText';
