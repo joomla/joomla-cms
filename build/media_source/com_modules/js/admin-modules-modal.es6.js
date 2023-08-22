a5bc832b92d683f1cda3b07498504ce056e56c1c
Fedik <getthesite@gmail.com>
2023-08-22 17:54:27 +0300

    Fix module select

--- build/media_source/com_modules/js/admin-modules-modal.es6.js
+++ build/media_source/com_modules/js/admin-modules-modal.es6.js
@@ -3,49 +3,41 @@
   * @license    GNU General Public License version 2 or later; see LICENSE.txt
   */
 
-document.addEventListener('DOMContentLoaded', () => {
-  'use strict';
+// Assign listener for click event (for single module id insertion)
+document.querySelectorAll('.js-module-insert').forEach((element) => {
+  element.addEventListener('click', (event) => {
+    event.preventDefault();
+    const modid = event.target.getAttribute('data-module');
+    const editor = event.target.getAttribute('data-editor');
 
-  // Get the elements
-  const modulesLinks = [].slice.call(document.querySelectorAll('.js-module-insert'));
-  const positionsLinks = [].slice.call(document.querySelectorAll('.js-position-insert'));
+    // Use the API
+    if (window.parent.Joomla && window.parent.Joomla.editors
+      && window.parent.Joomla.editors.instances
+      && Object.prototype.hasOwnProperty.call(window.parent.Joomla.editors.instances, editor)) {
+      window.parent.Joomla.editors.instances[editor].replaceSelection(`{loadmoduleid ${modid}}`);
+    }
 
-  // Assign listener for click event (for single module id insertion)
-  modulesLinks.forEach((element) => {
-    element.addEventListener('click', (event) => {
-      event.preventDefault();
-      const modid = event.target.getAttribute('data-module');
-      const editor = event.target.getAttribute('data-editor');
-
-      // Use the API
-      if (window.parent.Joomla && window.parent.Joomla.editors
-        && window.parent.Joomla.editors.instances
-        && Object.prototype.hasOwnProperty.call(window.parent.Joomla.editors.instances, editor)) {
-        window.parent.Joomla.editors.instances[editor].replaceSelection(`{loadmoduleid ${modid}}`);
-      }
-
-      if (window.parent.Joomla.Modal) {
-        window.parent.Joomla.Modal.getCurrent().close();
-      }
-    });
+    if (window.parent.Joomla.Modal) {
+      window.parent.Joomla.Modal.getCurrent().close();
+    }
   });
+});
 
   // Assign listener for click event (for position insertion)
-  positionsLinks.forEach((element) => {
-    element.addEventListener('click', (event) => {
-      event.preventDefault();
-      const position = event.target.getAttribute('data-position');
-      const editor = event.target.getAttribute('data-editor');
+document.querySelectorAll('.js-position-insert').forEach((element) => {
+  element.addEventListener('click', (event) => {
+    event.preventDefault();
+    const position = event.target.getAttribute('data-position');
+    const editor = event.target.getAttribute('data-editor');
 
-      // Use the API
-      if (window.Joomla && window.Joomla.editors && Joomla.editors.instances
-        && Object.prototype.hasOwnProperty.call(window.parent.Joomla.editors.instances, editor)) {
-        window.parent.Joomla.editors.instances[editor].replaceSelection(`{loadposition ${position}}`);
-      }
+    // Use the API
+    if (window.Joomla && window.Joomla.editors && Joomla.editors.instances
+      && Object.prototype.hasOwnProperty.call(window.parent.Joomla.editors.instances, editor)) {
+      window.parent.Joomla.editors.instances[editor].replaceSelection(`{loadposition ${position}}`);
+    }
 
-      if (window.parent.Joomla.Modal) {
-        window.parent.Joomla.Modal.getCurrent().close();
-      }
-    });
+    if (window.parent.Joomla.Modal) {
+      window.parent.Joomla.Modal.getCurrent().close();
+    }
   });
 });
