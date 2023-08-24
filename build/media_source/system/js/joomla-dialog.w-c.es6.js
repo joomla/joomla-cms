404ea19b71e19d6622295574c35a5f1a7f307069
Fedik <getthesite@gmail.com>
2023-08-24 11:12:29 +0300

    Targetable buttons

--- build/media_source/system/js/joomla-dialog.w-c.es6.js
+++ build/media_source/system/js/joomla-dialog.w-c.es6.js
@@ -236,7 +236,7 @@ class JoomlaDialog extends HTMLElement {
       const btn = document.createElement('button');
       btn.type = 'button';
       btn.textContent = btnData.label || '';
-      btn.ariaLabel = btnData.ariaLabel || '';
+      btn.ariaLabel = btnData.ariaLabel || null;
 
       if (btnData.className) {
         btn.classList.add(...btnData.className.split(' '));
@@ -571,6 +571,7 @@ class JoomlaDialog extends HTMLElement {
       popup.textHeader = title || Joomla.Text._('INFO', 'Info');
       popup.popupButtons = [{
         label: Joomla.Text._('JOK', 'Okay'),
+        data: { buttonOk: '' },
         onClick: () => popup.close(),
       }];
       popup.classList.add('joomla-dialog-alert');
@@ -599,6 +600,7 @@ class JoomlaDialog extends HTMLElement {
       popup.popupButtons = [
         {
           label: Joomla.Text._('JYES', 'Yes'),
+          data: { buttonOk: '' },
           onClick: () => {
             result = true;
             popup.destroy();
@@ -606,11 +608,12 @@ class JoomlaDialog extends HTMLElement {
         },
         {
           label: Joomla.Text._('JNO', 'No'),
+          data: { buttonCancel: '' },
           onClick: () => {
             result = false;
             popup.destroy();
           },
-          className: 'button btn btn-outline-secondary',
+          className: 'button button-secondary btn btn-outline-secondary',
         },
       ];
       popup.cancelable = false;
