744decda2e3010adaa13c5cd4535013f656793dc
andyforrest <forrestandrew7@gmail.com>
2023-08-25 22:16:01 +0100

    api tests for com_privacy

--- tests/System/support/commands/db.js
+++ tests/System/support/commands/db.js
@@ -42,6 +42,24 @@ function createInsertQuery(table, values) {
 
   return query;
 }
+/**
+ * Creates a privacy request in the database with the given data.
+ * The privacy request contains some default values when not all required fields are passed in the given data.
+ * The id of the inserted privacy request is returned
+ *
+ * @param {Object} privacyRequest The tag data to insert
+ *
+ * @returns integer
+ */
+Cypress.Commands.add('db_createPrivacyRequest', (privacyRequest) => {
+  const defaultPrivacyRequestOptions = {
+    email: 'test@example.com',
+    requested_at: '2023-01-01 20:00:00',
+    status: '0',
+  };
+
+  return cy.task('queryDB', createInsertQuery('privacy_requests', { ...defaultPrivacyRequestOptions, ...privacyRequest })).then(async (info) => info.insertId);
+});
 
 /**
  * Creates an article in the database with the given data. The article contains some default values when
