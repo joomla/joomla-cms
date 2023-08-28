a4cff3c0d68d38ada4e683168b02fadcab068702
Domo <82273358+domo12@users.noreply.github.com>
2023-08-28 12:37:21 +0800

    Update CliInstallationApplication.php

--- installation/src/Application/CliInstallationApplication.php
+++ installation/src/Application/CliInstallationApplication.php
@@ -7,7 +7,7 @@
  * @copyright   (C) 2022 Open Source Matters, Inc. <https://www.joomla.org>
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
-
+/*testing*/
 namespace Joomla\CMS\Installation\Application;
 
 use Joomla\Application\Web\WebClient;
