32d3c7b2cdfd03b8b47fe370da7a8ba0d9eea235
Nicola Galgano <optimus4joomla@gmail.com>
2023-08-28 09:00:45 +0200

    delete mail_template

--- administrator/components/com_admin/sql/updates/mysql/5.0.0-2023-08-23.sql
+++ administrator/components/com_admin/sql/updates/mysql/5.0.0-2023-08-23.sql
@@ -2,3 +2,4 @@ INSERT INTO `#__extensions` (`name`, `type`, `element`, `folder`, `client_id`, `
 ('plg_task_privacyconsent', 'plugin', 'privacyconsent', 'task', 0, 1, 1, 0, 1, '', '{}', '', NULL, NULL, 0, 0);
 INSERT INTO `#__mail_templates` (`template_id`, `extension`, `language`, `subject`, `body`, `htmlbody`, `attachments`, `params`) VALUES
 ('plg_task_privacyconsent.request.reminder', '', 'PLG_TASK_PRIVACYCONSENT_EMAIL_REMIND_SUBJECT', 'PLG_TASK_PRIVACYCONSENT_EMAIL_REMIND_BODY', '', '', '{"tags":["sitename","url","tokenurl","formurl","token"]}');
+DELETE FROM `#__mail_templates` WHERE `template_id` = 'plg_system_privacyconsent.request.reminder';
