f75609ef620ac7cd0ad6b9f8c5eebe3f78cfad36
MarkRS-UK <mark@stantoncentral.co.uk>
2023-08-28 21:46:56 +0100

    Update administrator/components/com_config/src/View/Component/HtmlView.php
    Co-authored-by: Richard Fath <richard67@users.noreply.github.com>

--- administrator/components/com_config/src/View/Component/HtmlView.php
+++ administrator/components/com_config/src/View/Component/HtmlView.php
@@ -84,8 +84,7 @@ class HtmlView extends BaseHtmlView
 
         // Remove unauthorised preference tabs.
         foreach($this->fieldsets as $key => $value) {
-            if($key == 'permissions')
-            {
+            if ($key == 'permissions') {
                 if ((!$user->authorise('core.admin', $component->option) || !$user->authorise('core.options.permission', $component->option) )
                     && isset($this->fieldsets['permissions']))
                 {
