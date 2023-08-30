21aae1bf57da3d73414d6970c412a9245803eb5c
andyforrest <forrestandrew7@gmail.com>
2023-08-30 17:07:17 +0100

    Tests for com_privacy consent on user site

--- tests/System/support/commands/db.js
+++ tests/System/support/commands/db.js
@@ -44,11 +44,31 @@ function createInsertQuery(table, values) {
 }
 
 /**
+ * Creates a privacy consent in the database with the given data.
+ * The privacy consent contains some default values when not all required fields are passed in the given data.
+ * The id of the inserted privacy consent is returned
+ *
+ * @param {Object} privacyRequest The consent data to insert
+ *
+ * @returns integer
+ */
+Cypress.Commands.add('db_createPrivacyConsent', (privacyConsent) => {
+  const defaultPrivacyConsentOptions = {
+    state: '0',
+    created: '2023-01-01 20:00:00',
+    subject: 'PLG_SYSTEM_PRIVACYCONSENT_SUBJECT',
+    body: '',
+  };
+
+  return cy.task('queryDB', createInsertQuery('privacy_consents', { ...defaultPrivacyConsentOptions, ...privacyConsent })).then(async (info) => info.insertId);
+});
+
+/**
  * Creates a privacy request in the database with the given data.
  * The privacy request contains some default values when not all required fields are passed in the given data.
  * The id of the inserted privacy request is returned
  *
- * @param {Object} privacyRequest The tag data to insert
+ * @param {Object} privacyRequest The request data to insert
  *
  * @returns integer
  */
