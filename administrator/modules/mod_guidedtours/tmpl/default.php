45f77645e57c95c5e6ce5b69b08e08ea3e51ffa8
Olivier Buisard <olivier.buisard@simplifyyourweb.com>
2023-09-01 21:28:12 -0400

    [5.0] [Guided Tours] Add context to core tours (#41303)

--- administrator/modules/mod_guidedtours/tmpl/default.php
+++ administrator/modules/mod_guidedtours/tmpl/default.php
@@ -25,22 +25,55 @@ $app->getDocument()
     ->getWebAssetManager()
     ->useScript('bootstrap.dropdown');
 
-$lang       = $app->getLanguage();
-$extension  = $app->getInput()->get('option');
-$listTours  = [];
-$allTours   = [];
-$toursCount = $params->get('tourscount', 7);
+$lang         = $app->getLanguage();
+$extension    = $app->getInput()->get('option');
+$contextTours = [];
+$starTours    = [];
+$listTours    = [];
+$allTours     = [];
+$contextCount = $params->get('contextcount', 7);
+$toursCount   = $params->get('tourscount', 7);
 
 foreach ($tours as $tour) :
-    if ($toursCount > 0 && count(array_intersect(['*', $extension], $tour->extensions))) :
+    $uri = new Uri($tour->url);
+
+    if (in_array('*', $tour->extensions)) :
+        $starTours[] = $tour;
+    elseif (in_array($extension, $tour->extensions)) :
+        if ($extension === 'com_categories') :
+            // Special case for the categories page, where the context is complemented with the extension the categories apply to
+            if ($uri->getVar('option', '') === 'com_categories') :
+                if ($uri->getVar('extension', '') === $app->getInput()->get('extension', '')) :
+                    if ($contextCount > 0) :
+                        $contextTours[] = $tour;
+                        $contextCount--;
+                    endif;
+                elseif ($toursCount > 0) :
+                    $listTours[] = $tour;
+                    $toursCount--;
+                endif;
+            else :
+                if (in_array($app->getInput()->get('extension', ''), $tour->extensions)) :
+                    if ($contextCount > 0) :
+                        $contextTours[] = $tour;
+                        $contextCount--;
+                    endif;
+                elseif ($toursCount > 0) :
+                    $listTours[] = $tour;
+                    $toursCount--;
+                endif;
+            endif;
+        elseif ($contextCount > 0) :
+            $contextTours[] = $tour;
+            $contextCount--;
+        endif;
+    elseif ($toursCount > 0) :
         $listTours[] = $tour;
         $toursCount--;
     endif;
 
-    $uri = new Uri($tour->url);
-
     // We assume the url is the starting point
-    $key = $uri->getVar('option') ?? Text::_('MOD_GUIDEDTOURS_GENERIC_TOUR');
+    $key = $uri->getVar('option') ?? 'com_cpanel';
 
     if (!isset($allTours[$key])) :
         $lang->load("$key.sys", JPATH_ADMINISTRATOR)
@@ -52,6 +85,10 @@ foreach ($tours as $tour) :
     $allTours[$key][] = $tour;
 endforeach;
 
+if ($contextCount > 0) :
+    // The '*' tours have lower priority than contextual tours and are added after them, room permitting
+    $contextTours = array_slice(array_merge($contextTours, $starTours), 0, $params->get('contextcount', 7));
+endif;
 ?>
 <div class="header-item-content dropdown header-tours d-none d-sm-block">
     <button class="dropdown-toggle d-flex align-items-center ps-0 py-0" data-bs-toggle="dropdown" type="button" title="<?php echo Text::_('MOD_GUIDEDTOURS_MENU'); ?>">
@@ -64,12 +101,32 @@ endforeach;
         <span class="icon-angle-down" aria-hidden="true"></span>
     </button>
     <div class="dropdown-menu dropdown-menu-end">
-        <?php foreach ($listTours as $tour) : ?>
-            <button type="button" class="button-start-guidedtour dropdown-item" data-id="<?php echo $tour->id ?>">
-                <span class="icon-map-signs" aria-hidden="true"></span>
-                <?php echo $tour->title; ?>
-            </button>
-        <?php endforeach; ?>
+        <?php if (count($contextTours) > 0) : ?>
+            <ul class="list-unstyled m-0">
+                <?php foreach ($contextTours as $tour) : ?>
+                    <li>
+                        <button type="button" class="button-start-guidedtour dropdown-item" data-id="<?php echo $tour->id; ?>">
+                            <span class="icon-star icon-fw" aria-hidden="true"></span>
+                            <?php echo $tour->title; ?>
+                        </button>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
+            <hr class="dropdown-divider m-0" role="separator" />
+        <?php endif; ?>
+        <?php if (count($listTours) > 0) : ?>
+            <ul class="list-unstyled m-0">
+                <?php foreach ($listTours as $tour) : ?>
+                    <li>
+                        <button type="button" class="button-start-guidedtour dropdown-item" data-id="<?php echo $tour->id; ?>">
+                            <span class="icon-map-signs icon-fw" aria-hidden="true"></span>
+                            <?php echo $tour->title; ?>
+                        </button>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
+            <hr class="dropdown-divider m-0" role="separator" />
+        <?php endif; ?>
         <button type="button" class="dropdown-item text-center" data-bs-toggle="modal" data-bs-target="#modGuidedTours-modal">
             <?php echo Text::_('MOD_GUIDEDTOURS_SHOW_ALL'); ?>
         </button>
