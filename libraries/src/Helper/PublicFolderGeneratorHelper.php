9817ab6a55a406f6ea778dc188cd627fce729d0f
Dimitris Grammatikogiannis <dg@dgrammatiko.dev>
2023-09-04 10:33:29 +0300

    cleanup
    Signed-off-by: Dimitris Grammatikogiannis <dg@dgrammatiko.dev>

--- libraries/src/Helper/PublicFolderGeneratorHelper.php
+++ libraries/src/Helper/PublicFolderGeneratorHelper.php
@@ -95,68 +95,24 @@ class PublicFolderGeneratorHelper
             $this->createFile($destinationPath . $file, file_get_contents(JPATH_ROOT . $file));
         }
 
-        $indexTemplate = <<<HTML
-<?php
+        $indexTemplate = file_get_contents(__DIR__ . '/indexTemplate.php');
 
-/**
- * File automatically generated
- *
- * @copyright  (C) 2005 Open Source Matters, Inc. <https://www.joomla.org>
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
-
-defined('_JEXEC') || die;
-
-// Defines.
-define('JPATH_BASE', {{BASEFOLDER}});
-define('JPATH_ROOT', {{ROOTFOLDER}});
-define('JPATH_PUBLIC', {{PUBLICFOLDER}});
-
-// Define the application's minimum supported PHP version as a constant so it can be referenced within the application.
-define('JOOMLA_MINIMUM_PHP', '8.1.0');
-
-if (version_compare(PHP_VERSION, JOOMLA_MINIMUM_PHP, '<')) {
-    {{DIECONDITIONAL}}
-}
-
-/**
- * Constant that is checked in included files to prevent direct access.
- * define() is used rather than "const" to not error for PHP 5.2 and lower
- */
-define('_JEXEC', 1);
-
-// Run the application - All executable code should be triggered through this file
-require_once JPATH_ROOT . '/includes/app.php';
-HTML;
+        if (!$indexTemplate) {
+            throw new \Exception('Could\'t read the index template file.');
+        }
 
         $search  = ['{{ROOTFOLDER}}', '{{PUBLICFOLDER}}', '{{BASEFOLDER}}', '{{DIECONDITIONAL}}'];
         $replace = ['"' . JPATH_ROOT . '"', '"' . $destinationPath . '"'];
 
         // The root index.php
         $replace[] = '"' . JPATH_ROOT . '"';
-        $replace[] = <<<PHP
-    die(
-        str_replace(
-            '{{phpversion}}',
-            JOOMLA_MINIMUM_PHP,
-            file_get_contents(JPATH_ROOT . '/includes/incompatible.html')
-        )
-    );
-PHP;
+        $replace[] = "die(str_replace('{{phpversion}}', JOOMLA_MINIMUM_PHP, file_get_contents(JPATH_ROOT . '/includes/incompatible.html')));";
 
         $this->createFile($destinationPath . '/index.php', str_replace($search, $replace, $indexTemplate));
 
         // The administrator root index.php
         $replace[] = '"' . JPATH_ROOT . '/administrator"';
-        $replace[] = <<<PHP
-    die(
-        str_replace(
-            '{{phpversion}}',
-            JOOMLA_MINIMUM_PHP,
-            file_get_contents(JPATH_ROOT . '/administrator/includes/incompatible.html')
-        )
-    );
-PHP;
+        $replace[] = "die(str_replace('{{phpversion}}', JOOMLA_MINIMUM_PHP, file_get_contents(JPATH_ROOT . '/administrator/includes/incompatible.html')));";
 
         $this->createFile($destinationPath . '/administrator/index.php', str_replace($search, $replace, $indexTemplate));
 
@@ -164,9 +120,7 @@ PHP;
         $replace[] = '"' . JPATH_ROOT . '/api"';
         $replace[] = <<<PHP
     header('HTTP/1.1 500 Internal Server Error');
-    echo json_encode(
-        ['error' => sprintf('Joomla requires PHP version %s to run', JOOMLA_MINIMUM_PHP)]
-    );
+    echo json_encode(['error' => sprintf('Joomla requires PHP version %s to run', JOOMLA_MINIMUM_PHP)]);
 
     return;
 PHP;
