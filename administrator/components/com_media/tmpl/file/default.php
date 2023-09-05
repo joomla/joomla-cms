2a6f1be5567f54e888bb16799a1f459df56159cb
Fedik <getthesite@gmail.com>
2023-09-05 15:38:42 +0300

    Toolbar for modal views

--- administrator/components/com_media/tmpl/file/default.php
+++ administrator/components/com_media/tmpl/file/default.php
@@ -15,7 +15,6 @@ use Joomla\CMS\Factory;
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Layout\LayoutHelper;
 use Joomla\CMS\Session\Session;
-use Joomla\CMS\Toolbar\Toolbar;
 use Joomla\CMS\Uri\Uri;
 
 /** @var Joomla\CMS\WebAsset\WebAssetManager $wa */
