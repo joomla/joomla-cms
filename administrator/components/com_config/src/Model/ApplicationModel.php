joomla/joomla-cms

commit 33327bd2648f2c37a403f25e65dccce14a8c5155

[4.0] Remove a "development" error_reporting as obsolete (#28580)

administrator/components/com_config/src/Model/ApplicationModel.php
@@ -110,6 +110,13 @@ class ApplicationModel extends FormModel
 			$data = array_merge($data, $temp);
 		}
 
+		// Correct error_reporting value, since we removed "development", the "maximum" should be set instead
+		// @TODO: This can be removed in 5.0
+		if (!empty($data['error_reporting']) && $data['error_reporting'] === 'development')
+		{
+			$data['error_reporting'] = 'maximum';
+		}
+
 		return $data;
 	}
 

administrator/includes/framework.php
@@ -61,13 +61,8 @@ switch ($config->error_reporting)
 		break;
 
 	case 'maximum':
+	case 'development': // <= Stays for backward compatibility, @TODO: can be removed in 5.0
 		error_reporting(E_ALL);
-		ini_set('display_errors', 1);
-
-		break;
-
-	case 'development':
-		error_reporting(-1);
 		ini_set('display_errors', 1);
 
 		break;

api/includes/framework.php
@@ -66,13 +66,8 @@ switch ($config->error_reporting)
 		break;
 
 	case 'maximum':
+	case 'development': // <= Stays for backward compatibility, @TODO: can be removed in 5.0
 		error_reporting(E_ALL);
-		ini_set('display_errors', 1);
-
-		break;
-
-	case 'development':
-		error_reporting(-1);
 		ini_set('display_errors', 1);
 
 		break;

includes/framework.php
@@ -61,13 +61,8 @@ switch ($config->error_reporting)
 		break;
 
 	case 'maximum':
+	case 'development': // <= Stays for backward compatibility, @TODO: can be removed in 5.0
 		error_reporting(E_ALL);
-		ini_set('display_errors', 1);
-
-		break;
-
-	case 'development':
-		error_reporting(-1);
 		ini_set('display_errors', 1);
 
 		break;

administrator/components/com_config/forms/application.xml
@@ -788,7 +788,6 @@
 			<option value="none">COM_CONFIG_FIELD_VALUE_NONE</option>
 			<option value="simple">COM_CONFIG_FIELD_VALUE_SIMPLE</option>
 			<option value="maximum">COM_CONFIG_FIELD_VALUE_MAXIMUM</option>
-			<option value="development">COM_CONFIG_FIELD_VALUE_DEVELOPMENT</option>
 		</field>
 
 		<field

administrator/language/en-GB/com_config.ini
@@ -146,7 +146,6 @@ COM_CONFIG_FIELD_VALUE_BEFORE="Before"
 COM_CONFIG_FIELD_VALUE_CACHE_OFF="OFF - Caching disabled"
 COM_CONFIG_FIELD_VALUE_CACHE_CONSERVATIVE="ON - Conservative caching"
 COM_CONFIG_FIELD_VALUE_CACHE_PROGRESSIVE="ON - Progressive caching"
-COM_CONFIG_FIELD_VALUE_DEVELOPMENT="Development"
 COM_CONFIG_FIELD_VALUE_DISPLAY_OFFLINE_MESSAGE_CUSTOM="Use Custom Message"
 COM_CONFIG_FIELD_VALUE_DISPLAY_OFFLINE_MESSAGE_LANGUAGE="Use Site Language Default Message"
 COM_CONFIG_FIELD_VALUE_ENTIRE_SITE="Entire Site"

commit 64b2b2099d9776db54169ea31d6e3f5a7b1ca54b

[5.0] Remove obsolete error reporting
Completion of the work started in #28580

administrator/components/com_config/src/Model/ApplicationModel.php
@@ -129,12 +129,6 @@ class ApplicationModel extends FormModel implements MailerFactoryAwareInterface
             $data = array_merge($temp, $data);
         }
 
-        // Correct error_reporting value, since we removed "development", the "maximum" should be set instead
-        // @TODO: This can be removed in 5.0
-        if (!empty($data['error_reporting']) && $data['error_reporting'] === 'development') {
-            $data['error_reporting'] = 'maximum';
-        }
-
         return $data;
     }
 

administrator/includes/framework.php
@@ -65,7 +65,6 @@ switch ($config->error_reporting) {
         break;
 
     case 'maximum':
-    case 'development': // <= Stays for backward compatibility, @TODO: can be removed in 5.0
         error_reporting(E_ALL);
         ini_set('display_errors', 1);
 

api/includes/framework.php
@@ -65,7 +65,6 @@ switch ($config->error_reporting) {
         break;
 
     case 'maximum':
-    case 'development': // <= Stays for backward compatibility, @TODO: can be removed in 5.0
         error_reporting(E_ALL);
         ini_set('display_errors', 1);
 

includes/framework.php
@@ -65,7 +65,6 @@ switch ($config->error_reporting) {
         break;
 
     case 'maximum':
-    case 'development': // <= Stays for backward compatibility, @TODO: can be removed in 5.0
         error_reporting(E_ALL);
         ini_set('display_errors', 1);
 