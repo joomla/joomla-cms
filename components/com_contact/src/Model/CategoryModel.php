244fa214cbcba3a7d687c10772a774ee9e3e4274
Artur Stępień <artur.stepien@bestproject.pl>
2023-09-07 14:33:07 +0200

    Update CategoryModel.php
    Fixing = alignment

--- components/com_contact/src/Model/CategoryModel.php
+++ components/com_contact/src/Model/CategoryModel.php
@@ -177,7 +177,7 @@ class CategoryModel extends ListModel
             ->whereIn($db->quoteName('a.access'), $groups);
 
         // Filter by category.
-        $categoryId = $this->getState('category.id');
+        $categoryId           = $this->getState('category.id');
         $includeSubcategories = (int)$this->getState('filter.max_category_levels', 1) !== 0;
 
         if ($includeSubcategories) {
