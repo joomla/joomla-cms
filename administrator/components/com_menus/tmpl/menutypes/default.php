cd7c65242b25736c16d29ed3973d7a8feaf9f1e4
Fedik <getthesite@gmail.com>
2023-09-07 13:19:07 +0300

    MenutypeField layout

--- administrator/components/com_menus/tmpl/menutypes/default.php
+++ administrator/components/com_menus/tmpl/menutypes/default.php
@@ -17,12 +17,11 @@ use Joomla\CMS\Language\Text;
 $input = Factory::getApplication()->getInput();
 
 // Checking if loaded via index.php or component.php
-$tmpl = ($input->getCmd('tmpl') != '') ? '1' : '';
-$tmpl = json_encode($tmpl, JSON_NUMERIC_CHECK);
+$tmpl = $input->getCmd('tmpl') ? '1' : '';
 
 /** @var Joomla\CMS\WebAsset\WebAssetManager $wa */
 $wa = $this->document->getWebAssetManager();
-$wa->useScript('com_menus.admin-item-modal');
+$wa->useScript('com_menus.admin-item-modal')->useScript('modal-content-select');
 
 ?>
 <?php echo HTMLHelper::_('bootstrap.startAccordion', 'collapseTypes', ['active' => 'slide1']); ?>
@@ -30,18 +29,25 @@ $wa->useScript('com_menus.admin-item-modal');
     <?php foreach ($this->types as $name => $list) : ?>
         <?php echo HTMLHelper::_('bootstrap.addSlide', 'collapseTypes', $name, 'collapse' . ($i++)); ?>
             <div class="list-group">
-                <?php foreach ($list as $title => $item) : ?>
-                    <?php $menutype = ['id' => $this->recordId, 'title' => $item->type ?? $item->title, 'request' => $item->request]; ?>
-                    <?php $menutype = base64_encode(json_encode($menutype)); ?>
-                    <a class="choose_type list-group-item list-group-item-action" href="#"
-                        onclick="Joomla.setMenuType('<?php echo $menutype; ?>', '<?php echo $tmpl; ?>')">
+                <?php foreach ($list as $title => $item) :
+                    $menutype = ['id' => $this->recordId, 'title' => $item->type ?? $item->title, 'request' => $item->request];
+                    $encoded  = base64_encode(json_encode($menutype));
+
+                    $attrs = 'data-content-select data-content-type="com_menus.menutype"'
+                        . ' data-item-id="' . (int) $this->recordId . '"'
+                        . ' data-type="' . $this->escape($item->type ?? $item->title) . '"'
+                        . ' data-request="' . ($item->request ? $this->escape(json_encode($item->request)) : '') . '"'
+                        . ' data-encoded="' . $this->escape($encoded) . '"'
+                        . ' data-tmpl-view="' . $tmpl . '"';
+                ?>
+                    <button type="button" class="choose_type list-group-item list-group-item-action" <?php echo $attrs; ?>>
                         <div class="pe-2">
                             <?php echo $title; ?>
                         </div>
                         <small class="text-muted">
                             <?php echo Text::_($item->description); ?>
                         </small>
-                    </a>
+                    </button>
                 <?php endforeach; ?>
             </div>
         <?php echo HTMLHelper::_('bootstrap.endSlide'); ?>
--- build/media_source/com_menus/js/admin-item-modal.es6.js
+++ build/media_source/com_menus/js/admin-item-modal.es6.js
@@ -2,17 +2,32 @@
  * @copyright  (C) 2018 Open Source Matters, Inc. <https://www.joomla.org>
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
-Joomla = window.Joomla || {};
+window.Joomla = window.Joomla || {};
 
-((Joomla) => {
-  'use strict';
+Joomla.setMenuType = (type, tmpl) => {
+  // eslint-disable-next-line no-console
+  console.warn('Method Joomla.setMenuType() is deprecated. Use "modal-content-select" asset and elements [data-content-select] attribute.');
 
-  Joomla.setMenuType = (type, tmpl) => {
-    if (tmpl !== '') {
-      window.parent.Joomla.submitbutton('item.setType', type);
+  if (tmpl !== '') {
+    window.parent.Joomla.submitbutton('item.setType', type);
+
+    if (window.parent.Joomla.Modal && window.parent.Joomla.Modal.getCurrent()) {
       window.parent.Joomla.Modal.getCurrent().close();
-    } else {
-      window.location = `index.php?option=com_menus&view=item&task=item.setType&layout=edit&type=${type}`;
     }
-  };
-})(Joomla);
+  } else {
+    window.location = `index.php?option=com_menus&view=item&task=item.setType&layout=edit&type=${type}`;
+  }
+};
+
+// Bind the buttons
+document.addEventListener('click', (event) => {
+  const button = event.target.closest('[data-content-select]');
+  if (!button) return;
+  event.preventDefault();
+
+  // Check the data
+  if (!button.dataset.tmplView) {
+    // In non-modal view redirect to form with new type selected
+    window.location = `index.php?option=com_menus&view=item&task=item.setType&layout=edit&type=${button.dataset.encoded}`;
+  }
+});
