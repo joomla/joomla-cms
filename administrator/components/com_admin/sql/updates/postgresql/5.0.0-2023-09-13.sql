6bc44ff3ab61b13237d65723fa9da761a3a31670
Richard Fath <richard.fath@t-online.de>
2023-09-13 17:04:41 +0200

    Revert unnecessary update SQL scripts

--- administrator/components/com_admin/sql/updates/postgresql/5.0.0-2023-09-13.sql
+++
@@ -1,2 +0,0 @@
-UPDATE "#__extensions" SET "protected" = 0, "params" = '{"classes_aliases":"1","es5_assets":"1"}'
- WHERE "type" = 'plugin' AND "element" = 'compat' AND "folder" = 'behaviour' AND "client_id" = 0 AND "protected" = 1;
