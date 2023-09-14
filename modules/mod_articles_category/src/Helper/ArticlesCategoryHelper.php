496a819193035c393da7dddd126e012c3bf9c251
brian teeman <brian@teeman.net>
2023-09-14 08:40:52 +0100

    [5.0] Include article filter

--- modules/mod_articles_category/src/Helper/ArticlesCategoryHelper.php
+++ modules/mod_articles_category/src/Helper/ArticlesCategoryHelper.php
@@ -194,6 +194,7 @@ class ArticlesCategoryHelper implements DatabaseAwareInterface
         $articles->setState('filter.author_alias', $params->get('created_by_alias', []));
         $articles->setState('filter.author_alias.include', $params->get('author_alias_filtering_type', 1));
         $excluded_articles = $params->get('excluded_articles', '');
+        $included_articles = $params->get('included_articles', '');
 
         if ($excluded_articles) {
             $excluded_articles = explode("\r\n", $excluded_articles);
@@ -201,6 +202,15 @@ class ArticlesCategoryHelper implements DatabaseAwareInterface
 
             // Exclude
             $articles->setState('filter.article_id.include', false);
+        }
+
+
+        if ($included_articles) {
+            $included_articles = explode("\r\n", $included_articles);
+            $articles->setState('filter.article_id', $included_articles);
+
+            // Include
+            $articles->setState('filter.article_id.include', true);
         }
 
         $date_filtering = $params->get('date_filtering', 'off');
