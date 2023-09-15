2ae9f38113739fa4152bccc2b16cd9f74d21916b
Nicola Galgano <optimus4joomla@gmail.com>
2023-09-15 18:09:30 +0200

    correct

--- libraries/src/Table/Module.php
+++ libraries/src/Table/Module.php
@@ -150,13 +150,12 @@ class Module extends Table
             $this->publish_down = null;
         }
 
-        if (!\is_null($this->content) && !\is_null($this->params)) {
+        if ((!\is_null($this->content) && \strlen($this->content) > 65535) || (!\is_null($this->params) && \strlen($this->params) > 65535)) {
 
             // Prevent to save too large content > 65535
-            if ((\strlen($this->content) > 65535) || (\strlen($this->params) > 65535)) {
-                $this->setError(Text::_('COM_MODULES_FIELD_CONTENT_TOO_LARGE'));
+            $this->setError(Text::_('COM_MODULES_FIELD_CONTENT_TOO_LARGE'));
 
-                return false;
+            return false;
             }
         }
 
