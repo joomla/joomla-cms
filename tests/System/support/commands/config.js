24544976bd535570f1fd679cdba11e51837ad96a
Allon Moritz <allon.moritz@digital-peak.com>
2023-09-18 14:55:12 +0200

    cs

--- tests/System/support/commands/config.js
+++ tests/System/support/commands/config.js
@@ -1,15 +1,15 @@
 Cypress.Commands.add('config_setParameter', (parameter, value) => {
   cy.readFile(`${Cypress.env('cmsPath')}/configuration.php`).then((fileContent) => {
-		// Setup the new value
+    // Setup the new value
     let newValue = value;
     if (typeof value === 'string') {
       newValue = `'${value}'`;
     }
 
-		// The regex to find the line of the parameter
+    // The regex to find the line of the parameter
     const regex = new RegExp(`^.*\\$${parameter}\\s.*$`, 'mg');
 
-		// Replace the whole line with the new value
+    // Replace the whole line with the new value
     const content = fileContent.replace(regex, `public $${parameter} = ${newValue};`);
 
     // Write the modified content back to the configuration file
