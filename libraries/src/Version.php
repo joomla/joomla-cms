6e07ef7436962a1fdd7dbc6dc1ef3799993465a9
Harald Leithner <leithner@itronic.at>
2023-09-26 16:51:35 +0200

    Joomla! 5.0.0 Beta 3

--- libraries/src/Version.php
+++ libraries/src/Version.php
@@ -66,7 +66,7 @@ final class Version
      * @var    string
      * @since  3.8.0
      */
-    public const EXTRA_VERSION = 'beta3-dev';
+    public const EXTRA_VERSION = 'beta3';
 
     /**
      * Development status.
@@ -74,7 +74,7 @@ final class Version
      * @var    string
      * @since  3.5
      */
-    public const DEV_STATUS = 'Development';
+    public const DEV_STATUS = 'Beta';
 
     /**
      * Code name.
@@ -90,7 +90,7 @@ final class Version
      * @var    string
      * @since  3.5
      */
-    public const RELDATE = '19-September-2023';
+    public const RELDATE = '26-September-2023';
 
     /**
      * Release time.
@@ -98,7 +98,7 @@ final class Version
      * @var    string
      * @since  3.5
      */
-    public const RELTIME = '15:43';
+    public const RELTIME = '16:00';
 
     /**
      * Release timezone.
