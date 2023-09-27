e9ab8bb968f8b7de4145ccce965d3b2e5fd7cdd7
brian teeman <brian@teeman.net>
2023-09-27 17:01:44 +0100

    one more

--- administrator/components/com_joomlaupdate/src/Model/UpdateModel.php
+++ administrator/components/com_joomlaupdate/src/Model/UpdateModel.php
@@ -1819,7 +1819,7 @@ ENDDATA;
     }
 
     /**
-     * Check the update package without using the ZipArchive classs from zip PHP extension
+     * Check the update package without using the ZipArchive class from zip PHP extension
      *
      * @param   string  $filePath  Full path to the update package to test
      *
