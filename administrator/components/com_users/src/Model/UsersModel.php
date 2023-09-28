80d76082c45e30eb48ea855847e52f63cd6088aa
kochinc <1756865+kochinc@users.noreply.github.com>
2023-09-28 10:08:04 -0400

    Update UsersModel.php for 'GROUP BY' fix

--- administrator/components/com_users/src/Model/UsersModel.php
+++ administrator/components/com_users/src/Model/UsersModel.php
@@ -365,10 +365,7 @@ class UsersModel extends ListModel
         $groups  = $this->getState('filter.groups');
 
         if ($groupId || isset($groups)) {
-            $query->join('LEFT', '#__user_usergroup_map AS map2 ON map2.user_id = a.id')
-                ->group(
-                    $db->quoteName(
-                        [
+            $group_by = [
                             'a.id',
                             'a.name',
                             'a.username',
@@ -385,9 +382,13 @@ class UsersModel extends ListModel
                             'a.otpKey',
                             'a.otep',
                             'a.requireReset',
-                        ]
-                    )
-                );
+                        ];
+            if (PluginHelper::isEnabled('multifactorauth')) {
+                $group_by[] = 'mfa.mfaRecords';
+            }
+
+            $query->join('LEFT', '#__user_usergroup_map AS map2 ON map2.user_id = a.id')
+                  ->group($db->quoteName($group_by));
 
             if ($groupId) {
                 $groupId = (int) $groupId;
