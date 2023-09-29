joomla/joomla-cms

commit b9f1c61a60fbfd940542112c3cae7f5343c7b019

[5.0] Simplify boolean logic (#41655)
Co-authored-by: Quy <quy@nomonkeybiz.com>
Co-authored-by: Harald Leithner <leithner@itronic.at>

libraries/src/Form/Field/LimitboxField.php
@@ -82,7 +82,7 @@ class LimitboxField extends ListField
             asort($limits);
 
             // Add an option to show all?
-            $showAll = isset($this->element['showall']) ? (string) $this->element['showall'] === 'true' : true;
+            $showAll = isset($this->element['showall']) && (string) $this->element['showall'] === 'true';
 
             if ($showAll) {
                 $limits[] = 0;

administrator/components/com_config/src/Model/ApplicationModel.php
@@ -893,7 +893,7 @@ class ApplicationModel extends FormModel implements MailerFactoryAwareInterface
         $isSuperUserGroupBefore = Access::checkGroup($permission['rule'], 'core.admin');
 
         // Check if current user belongs to changed group.
-        $currentUserBelongsToGroup = in_array((int) $permission['rule'], $user->groups) ? true : false;
+        $currentUserBelongsToGroup = in_array((int) $permission['rule'], $user->groups);
 
         // Get current user groups tree.
         $currentUserGroupsTree = Access::getGroupsByUser($user->id, true);

administrator/components/com_config/src/View/Component/HtmlView.php
@@ -120,7 +120,7 @@ class HtmlView extends BaseHtmlView
         $toolbar->cancel('component.cancel');
         $toolbar->divider();
 
-        $inlinehelp  = (string) $this->form->getXml()->config->inlinehelp['button'] == 'show' ?: false;
+        $inlinehelp  = (string) $this->form->getXml()->config->inlinehelp['button'] === 'show';
         $targetClass = (string) $this->form->getXml()->config->inlinehelp['targetclass'] ?: 'hide-aware-inline-help';
 
         if ($inlinehelp) {

administrator/components/com_finder/src/Indexer/Query.php
@@ -217,7 +217,7 @@ class Query
         $this->input = $options['input'] ?? '';
 
         // Get the empty query setting.
-        $this->empty = isset($options['empty']) ? (bool) $options['empty'] : false;
+        $this->empty = !empty($options['empty']);
 
         // Get the input language.
         $this->language = !empty($options['language']) ? $options['language'] : Helper::getDefaultLanguage();

administrator/components/com_guidedtours/src/View/Step/HtmlView.php
@@ -145,7 +145,7 @@ class HtmlView extends BaseHtmlView
         }
 
         ToolbarHelper::divider();
-        $inlinehelp  = (string) $this->form->getXml()->config->inlinehelp['button'] == 'show' ?: false;
+        $inlinehelp  = (string) $this->form->getXml()->config->inlinehelp['button'] === 'show';
         $targetClass = (string) $this->form->getXml()->config->inlinehelp['targetclass'] ?: 'hide-aware-inline-help';
 
         if ($inlinehelp) {

administrator/components/com_guidedtours/src/View/Tour/HtmlView.php
@@ -147,7 +147,7 @@ class HtmlView extends BaseHtmlView
 
         ToolbarHelper::divider();
 
-        $inlinehelp  = (string) $this->form->getXml()->config->inlinehelp['button'] == 'show' ?: false;
+        $inlinehelp  = (string) $this->form->getXml()->config->inlinehelp['button'] === 'show';
         $targetClass = (string) $this->form->getXml()->config->inlinehelp['targetclass'] ?: 'hide-aware-inline-help';
 
         if ($inlinehelp) {

administrator/components/com_installer/src/Helper/InstallerHelper.php
@@ -272,7 +272,7 @@ class InstallerHelper
 
         $downloadKey = [
             'supported' => true,
-            'valid'     => $value ? true : false,
+            'valid'     => (bool) $value,
             'prefix'    => $prefix,
             'suffix'    => $suffix,
             'value'     => $value,

administrator/components/com_menus/src/Field/Modal/MenuField.php
@@ -160,7 +160,7 @@ class MenuField extends ModalSelectField
         $clientId  = (int) $this->element['clientid'];
 
         // Prepare enabled actions
-        $this->canDo['propagate']  = ((string) $this->element['propagate'] == 'true') && count($languages) > 2;
+        $this->canDo['propagate']  = ((string) $this->element['propagate'] === 'true') && count($languages) > 2;
 
         // Creating/editing menu items is not supported in frontend.
         if (!$app->isClient('administrator')) {

commit c527c4cec197f51f5690b3b7a659e195c668b1d3

fix limitbox field
broken in #41655

libraries/src/Form/Field/LimitboxField.php
@@ -82,7 +82,7 @@ class LimitboxField extends ListField
             asort($limits);
 
             // Add an option to show all?
-            $showAll = isset($this->element['showall']) && (string) $this->element['showall'] === 'true';
+            $showAll = !isset($this->element['showall']) || (string) $this->element['showall'] === 'true';
 
             if ($showAll) {
                 $limits[] = 0;