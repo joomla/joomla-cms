joomla/joomla-cms

commit b9f1c61a60fbfd940542112c3cae7f5343c7b019

[5.0] Simplify boolean logic (#41655)
Co-authored-by: Quy <quy@nomonkeybiz.com>
Co-authored-by: Harald Leithner <leithner@itronic.at>

libraries/src/Form/Field/LimitboxField.php
@@ -82,7 +82,7 @@ class LimitboxField extends ListField
             asort($limits);
 
             // Add an option to show all?
-            $showAll = isset($this->element['showall']) ? (string) $this->element['showall'] === 'true' : true;
+            $showAll = isset($this->element['showall']) && (string) $this->element['showall'] === 'true';
 
             if ($showAll) {
                 $limits[] = 0;

plugins/system/accessibility/src/Extension/Accessibility.php
@@ -89,7 +89,7 @@ final class Accessibility extends CMSPlugin
                             'units' => 'px',
                         ],
                     ],
-                    'useEmojis' => $this->params->get('useEmojis') != 'false' ? true : false,
+                    'useEmojis' => $this->params->get('useEmojis') === 'true',
                 ],
                 'hotkeys' => [
                     'enabled'    => true,

administrator/components/com_config/src/Model/ApplicationModel.php
@@ -893,7 +893,7 @@ class ApplicationModel extends FormModel implements MailerFactoryAwareInterface
         $isSuperUserGroupBefore = Access::checkGroup($permission['rule'], 'core.admin');
 
         // Check if current user belongs to changed group.
-        $currentUserBelongsToGroup = in_array((int) $permission['rule'], $user->groups) ? true : false;
+        $currentUserBelongsToGroup = in_array((int) $permission['rule'], $user->groups);
 
         // Get current user groups tree.
         $currentUserGroupsTree = Access::getGroupsByUser($user->id, true);

administrator/components/com_config/src/View/Component/HtmlView.php
@@ -120,7 +120,7 @@ class HtmlView extends BaseHtmlView
         $toolbar->cancel('component.cancel');
         $toolbar->divider();
 
-        $inlinehelp  = (string) $this->form->getXml()->config->inlinehelp['button'] == 'show' ?: false;
+        $inlinehelp  = (string) $this->form->getXml()->config->inlinehelp['button'] === 'show';
         $targetClass = (string) $this->form->getXml()->config->inlinehelp['targetclass'] ?: 'hide-aware-inline-help';
 
         if ($inlinehelp) {

administrator/components/com_finder/src/Indexer/Query.php
@@ -217,7 +217,7 @@ class Query
         $this->input = $options['input'] ?? '';
 
         // Get the empty query setting.
-        $this->empty = isset($options['empty']) ? (bool) $options['empty'] : false;
+        $this->empty = !empty($options['empty']);
 
         // Get the input language.
         $this->language = !empty($options['language']) ? $options['language'] : Helper::getDefaultLanguage();

administrator/components/com_guidedtours/src/View/Step/HtmlView.php
@@ -145,7 +145,7 @@ class HtmlView extends BaseHtmlView
         }
 
         ToolbarHelper::divider();
-        $inlinehelp  = (string) $this->form->getXml()->config->inlinehelp['button'] == 'show' ?: false;
+        $inlinehelp  = (string) $this->form->getXml()->config->inlinehelp['button'] === 'show';
         $targetClass = (string) $this->form->getXml()->config->inlinehelp['targetclass'] ?: 'hide-aware-inline-help';
 
         if ($inlinehelp) {

administrator/components/com_guidedtours/src/View/Tour/HtmlView.php
@@ -147,7 +147,7 @@ class HtmlView extends BaseHtmlView
 
         ToolbarHelper::divider();
 
-        $inlinehelp  = (string) $this->form->getXml()->config->inlinehelp['button'] == 'show' ?: false;
+        $inlinehelp  = (string) $this->form->getXml()->config->inlinehelp['button'] === 'show';
         $targetClass = (string) $this->form->getXml()->config->inlinehelp['targetclass'] ?: 'hide-aware-inline-help';
 
         if ($inlinehelp) {

administrator/components/com_installer/src/Helper/InstallerHelper.php
@@ -272,7 +272,7 @@ class InstallerHelper
 
         $downloadKey = [
             'supported' => true,
-            'valid'     => $value ? true : false,
+            'valid'     => (bool) $value,
             'prefix'    => $prefix,
             'suffix'    => $suffix,
             'value'     => $value,

commit a306ee6e21801c874c279b264b0241d66c9b0b53

[5.0] reactivate disabled system tests (#41997)
* [5.0] reactivate test

revert #41938 (part)

* fix joomla dialog

* fix limitbox field

broken in #41655

* useEmojis by default

---------

Co-authored-by: Denitz <197527+Denitz@users.noreply.github.com>

libraries/src/Form/Field/LimitboxField.php
@@ -82,7 +82,7 @@ class LimitboxField extends ListField
             asort($limits);
 
             // Add an option to show all?
-            $showAll = isset($this->element['showall']) && (string) $this->element['showall'] === 'true';
+            $showAll = !isset($this->element['showall']) || (string) $this->element['showall'] === 'true';
 
             if ($showAll) {
                 $limits[] = 0;

plugins/system/accessibility/src/Extension/Accessibility.php
@@ -89,7 +89,7 @@ final class Accessibility extends CMSPlugin
                             'units' => 'px',
                         ],
                     ],
-                    'useEmojis' => $this->params->get('useEmojis') === 'true',
+                    'useEmojis' => $this->params->get('useEmojis', 'true') === 'true',
                 ],
                 'hotkeys' => [
                     'enabled'    => true,

tests/System/integration/administrator/components/com_privacy/Consent.cy.js
@@ -17,7 +17,7 @@ describe('Test in backend that privacy consent component', () => {
     cy.get('tbody > tr > :nth-child(4)').should('contain', 'test user');
     cy.get('table').find('tr').should('have.length', 4);
   });
-  /*
+
   it('can invalidate privacy consent', () => {
     cy.db_enableExtension('0', 'plg_system_privacyconsent');
     cy.db_createUser().then((id) => {
@@ -28,6 +28,8 @@ describe('Test in backend that privacy consent component', () => {
     cy.get('tbody > tr > :nth-child(4)').should('contain', 'test user');
     cy.get('#cb0').click();
     cy.get('.button-trash').click();
+    cy.clickDialogConfirm(true);
+
     cy.get('.alert-message').should('contain', 'The consent was invalidated.');
   });
 
@@ -42,10 +44,11 @@ describe('Test in backend that privacy consent component', () => {
 
     cy.get('.w-1.text-center > .form-check-input').click();
     cy.get('#toolbar-trash').click();
+    cy.clickDialogConfirm(true);
 
     cy.get('.alert-message').should('contain', '2 consents were invalidated');
   });
-  */
+
   it('can filter by valid consent', () => {
     cy.db_enableExtension('0', 'plg_system_privacyconsent');
     cy.db_createUser().then((id) => {
@@ -154,7 +157,7 @@ describe('Test in backend that privacy consent component', () => {
     cy.get('.filter-search-bar__button').click();
     cy.get('.alert').should('contain', 'No Matching Results');
   });
-  /*
+
   it('can displays correct number of consents', () => {
     cy.db_enableExtension('0', 'plg_system_privacyconsent');
     cy.db_createUser().then((id) => {
@@ -187,7 +190,7 @@ describe('Test in backend that privacy consent component', () => {
     cy.get('#list_limit').select('All');
     cy.get('table').find('tr').should('have.length', 551);
   });
-  */
+
   it('can list by status in ascending order', () => {
     cy.db_enableExtension('0', 'plg_system_privacyconsent');
     cy.db_createUser().then((id) => {

tests/System/integration/api/com_users/Users.cy.js
@@ -54,12 +54,12 @@ describe('Test that users API endpoint', () => {
         .its('name')
         .should('include', 'updated automated test user'));
   });
-  /*
+
   it('can delete a user', () => {
     cy.db_createUser({ name: 'automated test user' })
       .then((id) => cy.api_delete(`/users/${id}`));
   });
-  */
+
   it('can login after update a user', () => {
     cy.db_createUser({ name: 'test', password: '098f6bcd4621d373cade4e832627b4f6' })
       .then((id) => {