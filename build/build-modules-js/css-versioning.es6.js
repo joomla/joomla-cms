3f94b387dd290c624038aa6ea94d72c210ae8011
Dimitris Grammatikogiannis <dg@dgrammatiko.dev>
2023-10-04 21:07:25 +0300

    - hash lenght
    - Reduce hash to 6 chars
    - update to 1.0.4 (Only diff is the removal of the `@document` rule)
    
    Signed-off-by: Dimitris Grammatikogiannis <dg@dgrammatiko.dev>

--- build/build-modules-js/css-versioning.es6.js
+++ build/build-modules-js/css-versioning.es6.js
@@ -1,8 +1,31 @@
 const { readFile, writeFile } = require('fs/promises');
+const { existsSync, readFileSync } = require('fs');
+const { resolve, dirname } = require('path');
 const jetpack = require('fs-jetpack');
 const Postcss = require('postcss');
 const UrlVersion = require('postcss-url-version');
 const { Timer } = require('./utils/timer.es6.js');
+
+global.crypto = require('crypto');
+
+const opts = {
+  version: (imagePath, sourceCssPath) => {
+    if (!sourceCssPath) {
+      return (new Date()).valueOf().toString();
+    }
+
+    const directory = dirname(sourceCssPath);
+    if (!(imagePath.startsWith('http') || imagePath.startsWith('//')) && existsSync(resolve(`${directory}/${imagePath}`))) {
+      const fileBuffer = readFileSync(resolve(`${directory}/${imagePath}`));
+      const hashSum = crypto.createHash('md5');
+      hashSum.update(fileBuffer);
+
+      return (hashSum.digest('hex')).substring(0, 6);
+    }
+
+    return (new Date()).valueOf().toString();
+  },
+};
 
 /**
  * Adds a hash to the url() parts of the static css
@@ -13,7 +36,7 @@ const { Timer } = require('./utils/timer.es6.js');
 const fixVersion = async (file) => {
   try {
     const cssString = await readFile(file, { encoding: 'utf8' });
-    const data = await Postcss([UrlVersion()]).process(cssString, { from: file });
+    const data = await Postcss([UrlVersion(opts)]).process(cssString, { from: file });
     await writeFile(file, data.css, { encoding: 'utf8', mode: 0o644 });
   } catch (error) {
     throw new Error(error);
@@ -32,4 +55,3 @@ module.exports.cssVersioning = async () => {
   await Promise.all(cssFiles.map((file) => fixVersion(file)));
 
   bench.stop();
-};
