aa9675cf23a3b58f35025b31b09f95b35439c511
Benjamin Trenkle <benjamin.trenkle@wicked-software.de>
2023-10-04 16:17:17 +0200

    First version of generic plugin

---
+++ plugins/schemaorg/generic/services/provider.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * @package     Joomla.Plugin
+ * @subpackage  Schemaorg.event
+ *
+ * @copyright   (C) 2023 Open Source Matters, Inc. <https://www.joomla.org>
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Extension\PluginInterface;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Plugin\PluginHelper;
+use Joomla\DI\Container;
+use Joomla\DI\ServiceProviderInterface;
+use Joomla\Event\DispatcherInterface;
+use Joomla\Plugin\Schemaorg\Generic\Extension\Generic;
+
+return new class () implements ServiceProviderInterface {
+    /**
+     * Registers the service provider with a DI container.
+     *
+     * @param   Container  $container  The DI container.
+     *
+     * @return  void
+     *
+     * @since   __DEPLOY_VERSION__
+     */
+    public function register(Container $container)
+    {
+        $container->set(
+            PluginInterface::class,
+            function (Container $container) {
+                $dispatcher = $container->get(DispatcherInterface::class);
+                $plugin     = new Generic(
+                    $dispatcher,
+                    (array) PluginHelper::getPlugin('schemaorg', 'generic')
+                );
+
+                $plugin->setApplication(Factory::getApplication());
+
+                return $plugin;
+            }
+        );
+    }
+};
