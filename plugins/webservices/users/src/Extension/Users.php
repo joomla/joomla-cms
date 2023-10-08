e6534ea9f3accde88bc09f8801570a87afb9d944
Christian Heel <66922325+heelc29@users.noreply.github.com>
2023-10-08 19:44:55 +0200

    [5.1] webservices plugins use concrete event classes

--- plugins/webservices/users/src/Extension/Users.php
+++ plugins/webservices/users/src/Extension/Users.php
@@ -10,8 +10,9 @@
 
 namespace Joomla\Plugin\WebServices\Users\Extension;
 
+use Joomla\CMS\Event\Application\BeforeApiRouteEvent;
 use Joomla\CMS\Plugin\CMSPlugin;
-use Joomla\CMS\Router\ApiRouter;
+use Joomla\Event\SubscriberInterface;
 
 // phpcs:disable PSR1.Files.SideEffects
 \defined('_JEXEC') or die;
@@ -22,19 +23,35 @@ use Joomla\CMS\Router\ApiRouter;
  *
  * @since  4.0.0
  */
-final class Users extends CMSPlugin
+final class Users extends CMSPlugin implements SubscriberInterface
 {
+    /**
+     * Returns an array of events this subscriber will listen to.
+     *
+     * @return  array
+     *
+     * @since   __DEPLOY_VERSION__
+     */
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            'onBeforeApiRoute' => 'onBeforeApiRoute',
+        ];
+    }
+
     /**
      * Registers com_users's API's routes in the application
      *
-     * @param   ApiRouter  &$router  The API Routing object
+     * @param   BeforeApiRouteEvent  $event  The event object
      *
      * @return  void
      *
      * @since   4.0.0
      */
-    public function onBeforeApiRoute(&$router)
+    public function onBeforeApiRoute(BeforeApiRouteEvent $event): void
     {
+        $router = $event->getRouter();
+
         $router->createCRUDRoutes(
             'v1/users',
             'users',
@@ -65,7 +82,7 @@ final class Users extends CMSPlugin
      *
      * @since   4.0.0
      */
-    private function createFieldsRoutes(&$router)
+    private function createFieldsRoutes(&$router): void
     {
         $router->createCRUDRoutes(
             'v1/fields/users',
