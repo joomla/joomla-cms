6265a1bfa18c3d0814a422bb4ab5b9bf4b3b098e
George Wilson <georgejameswilson@googlemail.com>
2023-10-20 15:59:01 +0100

    Update version

--- libraries/src/Table/Table.php
+++ libraries/src/Table/Table.php
@@ -325,7 +325,7 @@ abstract class Table implements TableInterface, DispatcherAwareInterface
             /*
             * If unable to find the class file in the Table include paths. Return false.
             * The warning JLIB_DATABASE_ERROR_NOT_SUPPORTED_FILE_NOT_FOUND has been removed in 3.6.3.
-            * In 4.0 an Exception (type to be determined) will be thrown.
+            * In 6.0 an Exception (type to be determined) will be thrown.
             * For more info see https://github.com/joomla/joomla-cms/issues/11570
             */
 
