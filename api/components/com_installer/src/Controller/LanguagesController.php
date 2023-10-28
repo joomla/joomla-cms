ad60c395f8ea895fddaeac6501ee51b55be49792
Nicola Galgano <optimus4joomla@gmail.com>
2023-10-28 08:12:10 +0200

    2023

--- api/components/com_installer/src/Controller/LanguagesController.php
+++ api/components/com_installer/src/Controller/LanguagesController.php
@@ -4,7 +4,7 @@
  * @package     Joomla.API
  * @subpackage  com_installer
  *
- * @copyright   (C) 2020 Open Source Matters, Inc. <https://www.joomla.org>
+ * @copyright   (C) 2023 Open Source Matters, Inc. <https://www.joomla.org>
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
