c42bcff95c50cb9a80e363c2e24e89d21ee790c3
Fedik <getthesite@gmail.com>
2023-11-09 13:48:22 +0200

    Contact Modal use ModalSelectField

--- administrator/components/com_contact/src/Field/Modal/ContactField.php
+++ administrator/components/com_contact/src/Field/Modal/ContactField.php
@@ -11,11 +11,13 @@
 namespace Joomla\Component\Contact\Administrator\Field\Modal;
 
 use Joomla\CMS\Factory;
-use Joomla\CMS\Form\FormField;
+use Joomla\CMS\Form\Field\ModalSelectField;
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\LanguageHelper;
 use Joomla\CMS\Language\Text;
+use Joomla\CMS\Layout\FileLayout;
 use Joomla\CMS\Session\Session;
+use Joomla\CMS\Uri\Uri;
 use Joomla\Database\ParameterType;
 
 // phpcs:disable PSR1.Files.SideEffects
@@ -27,7 +29,7 @@ use Joomla\Database\ParameterType;
  *
  * @since  1.6
  */
-class ContactField extends FormField
+class ContactField extends ModalSelectField
 {
     /**
      * The form field type.
@@ -38,13 +40,98 @@ class ContactField extends FormField
     protected $type = 'Modal_Contact';
 
     /**
+     * Method to attach a Form object to the field.
+     *
+     * @param   \SimpleXMLElement  $element  The SimpleXMLElement object representing the `<field>` tag for the form field object.
+     * @param   mixed              $value    The form field value to validate.
+     * @param   string             $group    The field name group control value.
+     *
+     * @return  boolean  True on success.
+     *
+     * @see     FormField::setup()
+     * @since   __DEPLOY_VERSION__
+     */
+    public function setup(\SimpleXMLElement $element, $value, $group = null)
+    {
+        // Check if the value consist with id:alias, extract the id only
+        if ($value && str_contains($value, ':')) {
+            [$id]  = explode(':', $value, 2);
+            $value = (int) $id;
+        }
+
+        $result = parent::setup($element, $value, $group);
+
+        if (!$result) {
+            return $result;
+        }
+
+        Factory::getApplication()->getLanguage()->load('com_contact', JPATH_ADMINISTRATOR);
+
+        $languages = LanguageHelper::getContentLanguages([0, 1], false);
+        $language  = (string) $this->element['language'];
+
+        // Prepare enabled actions
+        $this->canDo['propagate']  = ((string) $this->element['propagate'] == 'true') && \count($languages) > 2;
+
+        // Prepare Urls
+        $linkItems = (new Uri())->setPath(Uri::base(true) . '/index.php');
+        $linkItems->setQuery([
+            'option'                => 'com_contact',
+            'view'                  => 'contacts',
+            'layout'                => 'modal',
+            'tmpl'                  => 'component',
+            Session::getFormToken() => 1,
+        ]);
+        $linkItem = clone $linkItems;
+        $linkItem->setVar('view', 'contact');
+        $linkCheckin = (new Uri())->setPath(Uri::base(true) . '/index.php');
+        $linkCheckin->setQuery([
+            'option'                => 'com_contact',
+            'task'                  => 'contacts.checkin',
+            'format'                => 'json',
+            Session::getFormToken() => 1,
+        ]);
+
+        if ($language) {
+            $linkItems->setVar('forcedLanguage', $language);
+            $linkItem->setVar('forcedLanguage', $language);
+
+            $modalTitle = Text::_('COM_CONTACT_SELECT_A_CONTACT') . ' &#8212; ' . $this->getTitle();
+
+            $this->dataAttributes['data-language'] = $language;
+        } else {
+            $modalTitle = Text::_('COM_CONTACT_SELECT_A_CONTACT');
+        }
+
+        $urlSelect = $linkItems;
+        $urlEdit   = clone $linkItem;
+        $urlEdit->setVar('task', 'contact.edit');
+        $urlNew    = clone $linkItem;
+        $urlNew->setVar('task', 'contact.add');
+
+        $this->urls['select']  = (string) $urlSelect;
+        $this->urls['new']     = (string) $urlNew;
+        $this->urls['edit']    = (string) $urlEdit;
+        $this->urls['checkin'] = (string) $linkCheckin;
+
+        // Prepare titles
+        $this->modalTitles['select']  = $modalTitle;
+        $this->modalTitles['new']     = Text::_('COM_CONTACT_NEW_CONTACT');
+        $this->modalTitles['edit']    = Text::_('COM_CONTACT_EDIT_CONTACT');
+
+        $this->hint = $this->hint ?: Text::_('COM_CONTACT_SELECT_A_CONTACT');
+
+        return $result;
+    }
+
+    /**
      * Method to get the field input markup.
      *
      * @return  string  The field input markup.
      *
      * @since   1.6
      */
-    protected function getInput()
+    protected function getInput1()
     {
         $allowNew       = ((string) $this->element['new'] == 'true');
         $allowEdit      = ((string) $this->element['edit'] == 'true');
@@ -298,8 +385,72 @@ class ContactField extends FormField
      *
      * @since   3.4
      */
-    protected function getLabel()
+    protected function getLabel1()
     {
         return str_replace($this->id, $this->id . '_name', parent::getLabel());
+    }
+
+    /**
+     * Method to retrieve the title of selected item.
+     *
+     * @return string
+     *
+     * @since   __DEPLOY_VERSION__
+     */
+    protected function getValueTitle()
+    {
+        $value = (int) $this->value ?: '';
+        $title = '';
+
+        if ($value) {
+            try {
+                $db    = $this->getDatabase();
+                $query = $db->getQuery(true)
+                    ->select($db->quoteName('name'))
+                    ->from($db->quoteName('#__contact_details'))
+                    ->where($db->quoteName('id') . ' = :value')
+                    ->bind(':value', $value, ParameterType::INTEGER);
+                $db->setQuery($query);
+
+                $title = $db->loadResult();
+            } catch (\Throwable $e) {
+                Factory::getApplication()->enqueueMessage($e->getMessage(), 'error');
+            }
+        }
+
+        return $title ?: $value;
+    }
+
+    /**
+     * Method to get the data to be passed to the layout for rendering.
+     *
+     * @return  array
+     *
+     * @since __DEPLOY_VERSION__
+     */
+    protected function getLayoutData()
+    {
+        $data             = parent::getLayoutData();
+        $data['language'] = (string) $this->element['language'];
+
+        return $data;
+    }
+
+    /**
+     * Get the renderer
+     *
+     * @param   string  $layoutId  Id to load
+     *
+     * @return  FileLayout
+     *
+     * @since   __DEPLOY_VERSION__
+     */
+    protected function getRenderer($layoutId = 'default')
+    {
+        $layout = parent::getRenderer($layoutId);
+        $layout->setComponent('com_contact');
+        $layout->setClient(1);
+
+        return $layout;
     }
 }
