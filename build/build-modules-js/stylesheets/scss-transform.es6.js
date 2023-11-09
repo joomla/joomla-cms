488bc8705f004194ba0d7399ea03309dd1617634
C-Lodder <lodder15@hotmail.co.uk>
2023-11-09 10:29:28 +0000

    Improve CSS/JS build times

--- build/build-modules-js/stylesheets/scss-transform.es6.js
+++ build/build-modules-js/stylesheets/scss-transform.es6.js
@@ -1,10 +1,8 @@
-const Autoprefixer = require('autoprefixer');
-const CssNano = require('cssnano');
 const Fs = require('fs').promises;
 const FsExtra = require('fs-extra');
 const { dirname, sep } = require('path');
-const Postcss = require('postcss');
 const Sass = require('sass-embedded');
+const LightningCSS = require('lightningcss');
 
 module.exports.compile = async (file) => {
   const cssFile = file.replace(`${sep}scss${sep}`, `${sep}css${sep}`)
@@ -20,18 +18,24 @@ module.exports.compile = async (file) => {
   }
 
   // Auto prefixing
-  const cleaner = Postcss([Autoprefixer()]);
-  const res = await cleaner.process(compiled.css.toString(), { from: undefined });
+  const { code } = LightningCSS.transform({
+    code: Buffer.from(compiled.css.toString()),
+    minify: false,
+  });
 
   // Ensure the folder exists or create it
   await FsExtra.mkdirs(dirname(cssFile), {});
   await Fs.writeFile(
     cssFile,
-    res.css.toString(),
+    code,
     { encoding: 'utf8', mode: 0o644 },
   );
 
-  const cssMin = await Postcss([CssNano]).process(res.css.toString(), { from: undefined });
+  const cssMin = LightningCSS.transform({
+    code: Buffer.from(code),
+    minify: true,
+    exclude: LightningCSS.Features.VendorPrefixes
+  });
 
   // Ensure the folder exists or create it
   FsExtra.mkdirs(dirname(cssFile.replace('.css', '.min.css')), {});
