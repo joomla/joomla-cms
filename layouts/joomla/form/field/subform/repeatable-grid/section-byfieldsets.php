8d666e430895b1235ef56a2287e904d772f2793a
Fedik <getthesite@gmail.com>
2023-11-15 13:15:12 +0200

    Subform grid layout

--- layouts/joomla/form/field/subform/repeatable-grid/section-byfieldsets.php
+++ layouts/joomla/form/field/subform/repeatable-grid/section-byfieldsets.php
@@ -25,7 +25,7 @@ extract($displayData);
  */
 ?>
 
-<div class="subform-repeatable-group subform-grid-row" data-base-name="<?php echo $basegroup; ?>" data-group="<?php echo $group; ?>">
+<div class="subform-repeatable-group" data-base-name="<?php echo $basegroup; ?>" data-group="<?php echo $group; ?>">
     <?php if (!empty($buttons)) : ?>
     <div class="btn-toolbar text-end">
         <div class="btn-group">
@@ -41,6 +41,7 @@ extract($displayData);
         </div>
     </div>
     <?php endif; ?>
+    <div class="subform-grid-row">
     <?php foreach ($form->getFieldsets() as $fieldset) : ?>
     <fieldset class="<?php echo !empty($fieldset->class) ? $this->escape($fieldset->class) : ''; ?>">
         <?php if (!empty($fieldset->label)) : ?>
@@ -51,4 +52,5 @@ extract($displayData);
         <?php endforeach; ?>
     </fieldset>
     <?php endforeach; ?>
+    </div>
 </div>
