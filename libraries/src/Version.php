4ee38101e132f2b422a9cd15a2cb69c24bca45bb
Martin Kopp <kopp05@infotech.ch>
2023-11-23 11:16:58 +0100

    Joomla! 4.4.1 Stable

--- libraries/src/Version.php
+++ libraries/src/Version.php
@@ -66,7 +66,7 @@ final class Version
      * @var    string
      * @since  3.8.0
      */
-    public const EXTRA_VERSION = 'rc2';
+    public const EXTRA_VERSION = '';
 
     /**
      * Development status.
@@ -74,7 +74,7 @@ final class Version
      * @var    string
      * @since  3.5
      */
-    public const DEV_STATUS = 'Release Candidate';
+    public const DEV_STATUS = 'Stable';
 
     /**
      * Code name.
