d2a4d1b243a721d284a743be96e5df6ea8642e85
alikon <optimus4joomla@gmail.com>
2023-12-16 18:30:37 +0100

    cs

--- administrator/components/com_scheduler/tmpl/logs/default.php
+++ administrator/components/com_scheduler/tmpl/logs/default.php
@@ -84,7 +84,7 @@ $listDirn  = $this->escape($this->state->get('list.direction'));
                             <?php if ($canEdit) : ?>
                                 <?php
                                     $link = Route::_('index.php?option=com_scheduler&view=tasks&filter[search]=id:' . $item->jobid . '&tmpl=component&layout=modal');
-                                    $href ='#plugin' . $item->jobid . 'Modal'
+                                    $href = '#plugin' . $item->jobid . 'Modal'
                                 ?>
                                 <a title="<?php echo Text::_("JACTION_EDIT");?>" data-bs-toggle="modal" href="<?php echo $href; ?>"><?php echo $this->escape(str_replace(Uri::root(), '', rawurldecode($item->taskname))); ?></a>
                                 <?php echo HTMLHelper::_(
@@ -124,20 +124,18 @@ $listDirn  = $this->escape($this->state->get('list.direction'));
                         </td>
                         <td class="d-none d-md-table-cell">
                             <?php
-                                switch ($item->exitcode) {
-                                    case '123':
-                                        echo "<span class='badge bg-secondary'>Task Will Resume: " . $item->exitcode . "</span>";
-                                        break;
-                                    case '0':
-                                        echo "<span class='badge bg-success'>Task Executed: " . $item->exitcode . "</span>";
-                                        break;
-                                    default:
-                                        echo "<span class='badge bg-danger'>Task Failed: " . $item->exitcode . "</span>";
-                                        break;
-                                }
-
+                            switch ($item->exitcode) {
+                                case '123':
+                                    echo "<span class='badge bg-secondary'>Task Will Resume: " . $item->exitcode . "</span>";
+                                    break;
+                                case '0':
+                                    echo "<span class='badge bg-success'>Task Executed: " . $item->exitcode . "</span>";
+                                    break;
+                                default:
+                                    echo "<span class='badge bg-danger'>Task Failed: " . $item->exitcode . "</span>";
+                                    break;
+                            }
                             ?>
-
                         </td>
                         <td class="d-none d-md-table-cell">
                             <?php echo HTMLHelper::_('date', $item->nextdate, Text::_('DATE_FORMAT_LC6')); ?>
@@ -147,7 +145,7 @@ $listDirn  = $this->escape($this->state->get('list.direction'));
                         </td>
                     </tr>
                     <input type="hidden" name="jobid[]" value="<?php echo $item->jobid; ?>">
-                    <?php endforeach; ?>
+                <?php endforeach; ?>
                 </tbody>
             </table>
             <?php // load the pagination.?>
