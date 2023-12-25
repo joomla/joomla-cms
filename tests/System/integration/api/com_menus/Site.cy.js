065484a2fa55993480fcbe01a426e1c2a5fb4a3e
rajputanuj31 <rajputanuj041@gmail.com>
2023-12-25 21:45:34 +0530

    Updates

--- tests/System/integration/api/com_menus/Site.cy.js
+++ tests/System/integration/api/com_menus/Site.cy.js
@@ -4,9 +4,7 @@ describe('Test that menus site API endpoint', () => {
   it('can deliver a list of site menus', () => {
     cy.db_createMenuType({ title: 'automated test site menu' })
       .then(() => cy.api_get('/menus/site'))
-      .then((response) => cy.wrap(response).its('body').its('data.0').its('attributes')
-        .its('title')
-        .should('include', 'automated test site menu'));
+      .then((response) => cy.api_responseContains(response, 'title', 'automated test site menu'));
   });
 
   it('can deliver a single site menu', () => {
