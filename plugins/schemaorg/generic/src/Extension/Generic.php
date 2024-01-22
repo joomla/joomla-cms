81cfb1542dfd7e17fb51620de08578146cf97df1
Benjamin Trenkle <benjamin.trenkle@wicked-software.de>
2024-01-23 00:25:27 +0100

    Implement output & saving

--- plugins/schemaorg/generic/src/Extension/Generic.php
+++ plugins/schemaorg/generic/src/Extension/Generic.php
@@ -11,10 +11,13 @@
 namespace Joomla\Plugin\Schemaorg\Generic\Extension;
 
 use Joomla\CMS\Event\Plugin\System\Schemaorg\BeforeCompileHeadEvent;
+use Joomla\CMS\Event\Plugin\System\Schemaorg\PrepareSaveEvent;
+use Joomla\CMS\Language\Text;
 use Joomla\CMS\Plugin\CMSPlugin;
 use Joomla\CMS\Schemaorg\SchemaorgPluginTrait;
 use Joomla\Event\Priority;
 use Joomla\Event\SubscriberInterface;
+use Joomla\Registry\Registry;
 
 // phpcs:disable PSR1.Files.SideEffects
 \defined('_JEXEC') or die;
@@ -57,7 +60,35 @@ final class Generic extends CMSPlugin implements SubscriberInterface
         return [
             'onSchemaPrepareForm'       => 'onSchemaPrepareForm',
             'onSchemaBeforeCompileHead' => ['onSchemaBeforeCompileHead', Priority::BELOW_NORMAL],
+            'onSchemaPrepareSave'       => 'onSchemaPrepareSave',
         ];
+    }
+
+    public function onSchemaPrepareSave(PrepareSaveEvent $event): void
+    {
+        $subject = $event->getData();
+
+        if (empty($subject->schemaType) || $subject->schemaType !== 'Generic' || !isset($subject->schema)) {
+            return;
+        }
+
+        try {
+            $schema = new Registry($subject->schema);
+
+            $json = (new Registry($schema->get('json')))->toArray();
+        } catch (\RuntimeException $e) {
+            throw new \RuntimeException('PLG_SCHEMAORG_GENERIC_JSON_ERROR');
+        }
+
+        if (!isset($json['@context']) || $json['@context'] !== 'https://schema.org' || !isset($json['@type'])) {
+            throw new \RuntimeException(Text::_('PLG_SCHEMAORG_GENERIC_JSON_ERROR'));
+        }
+
+        $schema->set('json', json_encode($json, JSON_PRETTY_PRINT));
+
+        $subject->schema = $schema->toString();
+
+        $event->setArgument('subject', $subject);
     }
 
     /**
@@ -75,10 +106,19 @@ final class Generic extends CMSPlugin implements SubscriberInterface
 
         $graph = $schema->get('@graph');
 
-        foreach ($graph as &$entry) {
+        foreach ($graph as $i => $entry) {
+
             if (!isset($entry['@type']) || $entry['@type'] !== 'Generic') {
                 continue;
             }
+
+            $json = (new Registry($entry['json']))->toArray();
+
+            if (isset($json['@context'])) {
+                unset($json['@context']);
+            }
+
+            $graph[$i] = $json;
         }
 
         $schema->set('@graph', $graph);
