87ed8dfd74ddf5ed592ea2511cdf8fd2fd20c910
David Jardin <d.jardin@djumla.de>
2024-01-27 14:39:22 +0100

    added system test for both cases

--- tests/System/integration/administrator/components/com_joomlaupdate/Update.cy.js
+++ tests/System/integration/administrator/components/com_joomlaupdate/Update.cy.js
@@ -1,13 +1,31 @@
-describe('Test in backend that the field form', () => {
+
+
+describe('Test the update retrieval logic', () => {
   beforeEach(() => {
     cy.doAdministratorLogin();
   });
 
-  it('Can fetch available updates', () => {
+  afterEach(() => {
+    cy.db_setValidTufRoot()
+  });
+
+  it('Can fetch available updates with valid metadata', () => {
+    cy.db_setValidTufRoot()
+
     cy.visit('/administrator/index.php?option=com_joomlaupdate');
 
-    cy.get('#toolbar joomla-toolbar-button[task="update.purge"] button').click()
+    cy.get('#confirmButton').click()
 
     cy.get('#system-message-container').contains('Checked for updates.').should('exist');
+  });
+
+  it('Receives error fetching available updates with invalid metadata', () => {
+    cy.db_setInvalidTufRoot()
+
+    cy.visit('/administrator/index.php?option=com_joomlaupdate');
+
+    cy.get('#confirmButton').click()
+
+    cy.get('#system-message-container').contains('Update not possible because the offered update does not have enough signatures.').should('exist');
   });
 });
