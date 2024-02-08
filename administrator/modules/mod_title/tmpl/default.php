6a7c2475c624e75356b41e1a1652a80627af671f
Miroslav Orinčák <info@joomla-webstranky.sk>
2024-02-08 22:57:18 +0100

    Convert mod_title to service provider

--- administrator/modules/mod_title/tmpl/default.php
+++ administrator/modules/mod_title/tmpl/default.php
@@ -4,11 +4,11 @@
  * @package     Joomla.Administrator
  * @subpackage  mod_title
  *
- * @copyright   (C) 2010 Open Source Matters, Inc. <https://www.joomla.org>
+ * @copyright   (C) 2024 Open Source Matters, Inc. <https://www.joomla.org>
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
-defined('_JEXEC') or die;
+\defined('_JEXEC') or die;
 ?>
 <?php if (!empty($title)) : ?>
 <div class="d-flex align-items-center">
