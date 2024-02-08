3d764910a70e52764a1013b73bc89eea943bd0ad
Fedik <getthesite@gmail.com>
2024-02-08 17:10:19 +0200

    MathCaptcha

---
+++ administrator/components/com_admin/sql/updates/mysql/5.1.0-2024-02-08.sql
@@ -0,0 +1,2 @@
+INSERT INTO `#__extensions` (`package_id`, `name`, `type`, `element`, `folder`, `client_id`, `enabled`, `access`, `protected`, `locked`, `manifest_cache`, `params`, `custom_data`, `ordering`, `state`) VALUES
+(0, 'plg_captcha_math', 'plugin', 'math', 'captcha', 0, 0, 1, 0, 1, '', '{}', '', 1, 0);
