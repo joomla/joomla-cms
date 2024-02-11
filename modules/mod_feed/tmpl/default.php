23c8a9c8e0772d272f1f16a5ca2b88762c5733eb
Miroslav Orinčák <info@joomla-webstranky.sk>
2024-02-11 12:46:33 +0100

    Convert mod_feed to service provider

--- modules/mod_feed/tmpl/default.php
+++ modules/mod_feed/tmpl/default.php
@@ -4,11 +4,11 @@
  * @package     Joomla.Site
  * @subpackage  mod_feed
  *
- * @copyright   (C) 2006 Open Source Matters, Inc. <https://www.joomla.org>
+ * @copyright   (C) 2024 Open Source Matters, Inc. <https://www.joomla.org>
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
-defined('_JEXEC') or die;
+\defined('_JEXEC') or die;
 
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
@@ -21,7 +21,7 @@ if (empty($rssurl)) {
     return;
 }
 
-if (!empty($feed) && is_string($feed)) {
+if (!empty($feed) && \is_string($feed)) {
     echo $feed;
 } else {
     $lang      = $app->getLanguage();
@@ -80,7 +80,7 @@ if (!empty($feed) && is_string($feed)) {
     <!-- Show items -->
         <?php if (!empty($feed)) { ?>
         <ul class="newsfeed">
-            <?php for ($i = 0, $max = min(count($feed), $params->get('rssitems', 3)); $i < $max; $i++) { ?>
+            <?php for ($i = 0, $max = min(\count($feed), $params->get('rssitems', 3)); $i < $max; $i++) { ?>
                 <?php
                 $uri  = $feed[$i]->uri || !$feed[$i]->isPermaLink ? trim($feed[$i]->uri) : trim($feed[$i]->guid);
                 $uri  = !$uri || stripos($uri, 'http') !== 0 ? $rssurl : $uri;
@@ -116,5 +116,6 @@ if (!empty($feed) && is_string($feed)) {
         </ul>
         <?php } ?>
     </div>
-    <?php }
+        <?php
+    }
 }
