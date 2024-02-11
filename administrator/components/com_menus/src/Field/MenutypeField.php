d300dc9d9f87bbf3ab08abcb23cd41e6966ff754
Martina Scholz <64533137+LadySolveig@users.noreply.github.com>
2024-02-11 01:43:16 +0100

    Fix for api menu/site/items

--- administrator/components/com_menus/src/Field/MenutypeField.php
+++ administrator/components/com_menus/src/Field/MenutypeField.php
@@ -59,7 +59,7 @@ class MenutypeField extends ModalSelectField
         $recordId = (int) $this->form->getValue('id');
         $clientId = (int) $this->element['clientid'] ?: 0;
 
-        $url = Route::_('index.php?option=com_menus&view=menutypes&tmpl=component&client_id=' . $clientId . '&recordId=' . $recordId, false);
+        $url = 'index.php?option=com_menus&view=menutypes&tmpl=component&client_id=' . $clientId . '&recordId=' . $recordId;
 
         $this->urls['select']        = $url;
         $this->canDo['clear']        = false;
