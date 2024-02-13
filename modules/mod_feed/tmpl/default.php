bbe656da66865df39014bf4617f87266bc0cc927
Nicola Galgano <optimus4joomla@gmail.com>
2024-02-13 12:37:04 +0100

    backport to 4.4

--- modules/mod_feed/tmpl/default.php
+++ modules/mod_feed/tmpl/default.php
@@ -59,7 +59,7 @@ if (!empty($feed) && is_string($feed)) {
             <?php
         }
         // Feed date
-        if ($params->get('rssdate', 1)) : ?>
+        if ($params->get('rssdate', 1) && ($feed->publishedDate !== null)) : ?>
             <h3>
             <?php echo HTMLHelper::_('date', $feed->publishedDate, Text::_('DATE_FORMAT_LC3')); ?>
             </h3>
