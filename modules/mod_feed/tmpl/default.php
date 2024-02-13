110ebef7e4b9d14607d4afff02063e2266fa97e4
Miroslav Orinčák <info@joomla-webstranky.sk>
2024-02-13 20:16:07 +0100

    Remove \ from defines

--- modules/mod_feed/tmpl/default.php
+++ modules/mod_feed/tmpl/default.php
@@ -4,11 +4,11 @@
  * @package     Joomla.Site
  * @subpackage  mod_feed
  *
- * @copyright   (C) 2024 Open Source Matters, Inc. <https://www.joomla.org>
+ * @copyright   (C) 2006 Open Source Matters, Inc. <https://www.joomla.org>
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
-\defined('_JEXEC') or die;
+defined('_JEXEC') or die;
 
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
