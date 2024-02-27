de1dc01d0bf4cb83fc0637ba7d548787abbd2cb3
Christian Heel <66922325+heelc29@users.noreply.github.com>
2024-02-27 18:21:09 +0100

    [5.1] update plugin services provider
    Remove extra reference assignment in plugin constructor for dispatcher argument #40746

--- plugins/schemaorg/organization/services/provider.php
+++ plugins/schemaorg/organization/services/provider.php
@@ -33,9 +33,8 @@ return new class () implements ServiceProviderInterface {
         $container->set(
             PluginInterface::class,
             function (Container $container) {
-                $dispatcher = $container->get(DispatcherInterface::class);
-                $plugin     = new Organization(
-                    $dispatcher,
+                $plugin = new Organization(
+                    $container->get(DispatcherInterface::class),
                     (array) PluginHelper::getPlugin('schemaorg', 'organization')
                 );
                 $plugin->setApplication(Factory::getApplication());
