7da60c48240af3d8352ca0bdeef71cc0b2b415a1
Benjamin Trenkle <bembelimen@users.noreply.github.com>
2024-02-29 09:56:41 +0100

    Update administrator/modules/mod_latestactions/src/Helper/LatestActionsHelper.php

--- administrator/modules/mod_latestactions/src/Helper/LatestActionsHelper.php
+++ administrator/modules/mod_latestactions/src/Helper/LatestActionsHelper.php
@@ -106,7 +106,7 @@ class LatestActionsHelper
      *
      * @throws  \Exception
      *
-     * @deprecated __DEPLOY_VERSION__ will be removed in 6.0
+     * @deprecated __DEPLOY_VERSION__ will be removed in 7.0
      *             Use the non-static method getActions
      *             Example: Factory::getApplication()->bootModule('mod_latestactions', 'administrator')
      *                          ->getHelper('LatestActionsHelper')
