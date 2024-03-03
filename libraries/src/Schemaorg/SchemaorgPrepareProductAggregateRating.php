2110d3b0f86f95c4b33f6e1a973e830c12e6b09a
pmleconte <pascal.leconte@nordnet.fr>
2024-03-03 14:40:50 +0100

    [5.1]  Add aggregateRating

---
+++ libraries/src/Schemaorg/SchemaorgPrepareProductAggregateRating.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * Joomla! Content Management System
+ *
+ * @copyright  (C) 2024 Open Source Matters, Inc. <https://www.joomla.org>
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+namespace Joomla\CMS\Schemaorg;
+
+// phpcs:disable PSR1.Files.SideEffects
+\defined('_JEXEC') or die;
+// phpcs:enable PSR1.Files.SideEffects
+use Joomla\CMS\Uri\Uri;
+/**
+ * Prepare Product AggregateRating to be valid for JSON-LD output
+ *
+ * @since  5.0.0
+ */
+trait SchemaorgPrepareProductAggregateRating
+{
+    /**
+     * Prepare duration
+     *
+     * @param   array $duration
+     *
+     * @return  ?string
+     *
+     * @since   5.0.0
+     */
+    protected function prepareProductAggregateRating($context)
+    {
+		[$extension, $view, $id] = explode('.', $context);
+		if ($view ==  'article') {
+	        $baseId   = Uri::root() . '#/schema/';
+			$schemaId = $baseId . str_replace('.', '/', $context);
+
+			$component = $this->getApplication()->bootComponent('com_content')->getMVCFactory();
+			$model = $component->createModel('Article', 'Site');
+            $article = $model->getItem($id);
+			return ['@isPartOf' => ['@id' => $schemaId,'@type' => 'Product','name' => $article->title,'aggregateRating' => ['@type'=> "AggregateRating","ratingCount" => (string)$article->rating_count,"ratingValue" => (string)$article->rating]]];
+		}
+		return false;
+    }
+}
---
+++ plugins/schemaorg/aggregaterating/src/Extension/AggregateRating.php
@@ -0,0 +1,101 @@
+<?php
+
+/**
+ * @package     Joomla.Plugin
+ * @subpackage  Schemaorg.AggregateRating
+ *
+ * @copyright   (C) 2023 Open Source Matters, Inc. <https://www.joomla.org>
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+namespace Joomla\Plugin\Schemaorg\AggregateRating\Extension;
+
+use Joomla\CMS\Event\Plugin\System\Schemaorg\BeforeCompileHeadEvent;
+use Joomla\CMS\Plugin\CMSPlugin;
+use Joomla\CMS\Schemaorg\SchemaorgPrepareProductAggregateRating;
+use Joomla\CMS\Schemaorg\SchemaorgPrepareRecipeAggregateRating;
+use Joomla\Event\Priority;
+use Joomla\CMS\Plugin\PluginHelper;
+use Joomla\Event\SubscriberInterface;
+
+// phpcs:disable PSR1.Files.SideEffects
+\defined('_JEXEC') or die;
+// phpcs:enable PSR1.Files.SideEffects
+
+/**
+ * Schemaorg Plugin
+ *
+ * @since  5.0.0
+ */
+final class AggregateRating extends CMSPlugin implements SubscriberInterface
+{
+    use SchemaorgPrepareProductAggregateRating;
+	use SchemaorgPrepareRecipeAggregateRating;
+
+    /**
+     * Load the language file on instantiation.
+     *
+     * @var    boolean
+     * @since  5.0.0
+     */
+    protected $autoloadLanguage = true;
+
+    /**
+     * The name of the schema form
+     *
+     * @var   string
+     * @since 5.0.0
+     */
+    protected $pluginName = 'AggregateRating';
+
+    /**
+     * Returns an array of events this subscriber will listen to.
+     *
+     * @return  array
+     *
+     * @since   5.0.0
+     */
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            'onSchemaBeforeCompileHead' => ['onSchemaBeforeCompileHead', Priority::BELOW_NORMAL],
+        ];
+    }
+
+    /**
+     * Cleanup all BlogPosting types
+     *
+     * @param   BeforeCompileHeadEvent  $event  The given event
+     *
+     * @return  void
+     *
+     * @since   5.0.0
+     */
+    public function onSchemaBeforeCompileHead(BeforeCompileHeadEvent $event): void
+    {
+        $schema = $event->getSchema();
+
+        $graph = $schema->get('@graph');
+		
+        $need_vote = PluginHelper::isEnabled('content', 'vote');
+
+		if (!$need_vote) {
+			return;
+		}
+        foreach ($graph as $key => &$entry) {
+            if (!isset($entry['@type']))  {
+                continue;
+            }
+			if ($entry['@type'] == 'Recipe') {
+				$rating = $this->prepareRecipeAggregateRating($event->getContext());
+				continue;
+			}	
+			$rating = $this->prepareProductAggregateRating($event->getContext());
+		}
+
+	    if ($rating) { 
+			$graph[] = $rating;
+			$schema->set('@graph', $graph);
+		}
+    }
+}
