0105c24448e9e69dc4120eaf085967ab56c8f5f3
Allon Moritz <allon.moritz@digital-peak.com>
2024-03-05 14:36:21 +0100

    cs

--- administrator/components/com_users/src/View/Users/HtmlView.php
+++ administrator/components/com_users/src/View/Users/HtmlView.php
@@ -10,7 +10,6 @@
 
 namespace Joomla\Component\Users\Administrator\View\Users;
 
-use Joomla\CMS\Factory;
 use Joomla\CMS\Helper\ContentHelper;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\MVC\View\GenericDataException;
@@ -18,7 +17,6 @@ use Joomla\CMS\MVC\View\HtmlView as BaseHtmlView;
 use Joomla\CMS\Toolbar\Button\DropdownButton;
 use Joomla\CMS\Toolbar\Toolbar;
 use Joomla\CMS\Toolbar\ToolbarHelper;
-use Joomla\Database\DatabaseDriver;
 
 // phpcs:disable PSR1.Files.SideEffects
 \defined('_JEXEC') or die;
