5587adc1b2b85546324229433a7735804be357de
Benjamin Trenkle <benjamin.trenkle@wicked-software.de>
2024-03-11 22:06:11 +0100

    Remove unneded (and broken) tests

--- tests/System/support/commands.js
+++ tests/System/support/commands.js
@@ -13,58 +13,6 @@ const { registerCommands } = require('../../../node_modules/joomla-cypress/src/i
 
 registerCommands();
 
-Cypress.Commands.overwrite('doFrontendLogin', (originalFn, username, password, useSnapshot = true) => {
-  // Ensure there are valid credentials
-  const user = username ?? Cypress.env('username');
-  const pw = password ?? Cypress.env('password');
-
-  // Do normal login when no snapshot should be used
-  if (!useSnapshot) {
-    // Clear the session data
-    Cypress.session.clearAllSavedSessions();
-
-    // Call the normal function
-    return originalFn(user, pw);
-  }
-
-  // Do login through the session
-  return cy.session([user, pw, 'front'], () => originalFn(user, pw), { cacheAcrossSpecs: true });
-});
-
-Cypress.Commands.overwrite('doFrontendLogout', (originalFn) => {
-  // Call the login function
-  originalFn();
-
-  // Clear the session data
-  Cypress.session.clearAllSavedSessions();
-});
-
-/* Cypress.Commands.overwrite('doAdministratorLogin', (originalFn, username, password, useSnapshot = true) => {
-  // Ensure there are valid credentials
-  const user = username ?? Cypress.env('username');
-  const pw = password ?? Cypress.env('password');
-
-  // Do normal login when no snapshot should be used
-  if (!useSnapshot) {
-    // Clear the session data
-    Cypress.session.clearAllSavedSessions();
-
-    // Call the normal function
-    return originalFn(user, pw);
-  }
-
-  // Do login through the session
-  return cy.session([user, pw, 'back'], () => originalFn(user, pw), { cacheAcrossSpecs: true });
-});
-
-Cypress.Commands.overwrite('doAdministratorLogout', (originalFn) => {
-  // Call the login function
-  originalFn();
-
-  // Clear the session data
-  Cypress.session.clearAllSavedSessions();
-}); */
-
 // Click Joomla Dialog Confirm, isOkay: true = push "ok" button, false = push "cancel" button
 Cypress.Commands.add('clickDialogConfirm', (isOkay) => {
   let selector = '.joomla-dialog-confirm';
