0ccdd566ca241a15c2c1a7d6bced3dd49d7f2d46
Benjamin Trenkle <benjamin.trenkle@wicked-software.de>
2024-03-13 19:48:46 +0100

    Fix CS

--- components/com_newsfeeds/src/View/Category/HtmlView.php
+++ components/com_newsfeeds/src/View/Category/HtmlView.php
@@ -88,8 +88,8 @@ class HtmlView extends CategoryView
             $category = $this->category->getParent();
 
             while (
-                isset($category->id) && $category->id > 1 &&
-                (!isset($menu->query['option']) || $menu->query['option'] !== 'com_newsfeeds' || $menu->query['view'] === 'newsfeed'
+                isset($category->id) && $category->id > 1
+                && (!isset($menu->query['option']) || $menu->query['option'] !== 'com_newsfeeds' || $menu->query['view'] === 'newsfeed'
                 || $id != $category->id)
             ) {
                 $path[]   = ['title' => $category->title, 'link' => RouteHelper::getCategoryRoute($category->id, $category->language)];
