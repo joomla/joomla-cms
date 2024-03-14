cdc2480aee53b81ada80e00e25280450d5c024bb
Nicola Galgano <optimus4joomla@gmail.com>
2024-03-14 14:42:32 +0100

    unnecessary

--- administrator/components/com_fields/src/Plugin/FieldsListPlugin.php
+++ administrator/components/com_fields/src/Plugin/FieldsListPlugin.php
@@ -56,14 +56,8 @@ class FieldsListPlugin extends FieldsPlugin
         // Set data attributes
         $dataMaxResult = $field->params->get('data_max_results', 10);
         $dataMaxRender = $field->params->get('data_max_render', -1);
-
-        if ($dataMaxResult) {
-            $fieldNode->setAttribute('data-max-results', (string) $dataMaxResult);
-        }
-
-        if ($dataMaxRender) {
-            $fieldNode->setAttribute('data-max-render', (string) $dataMaxRender);
-        }
+        $fieldNode->setAttribute('data-max-results', (string) $dataMaxResult);
+        $fieldNode->setAttribute('data-max-render', (string) $dataMaxRender);
 
         return $fieldNode;
     }
