a79eeda0e9dfe6517c93feb9fa8ff210228356c7
George Wilson <georgejameswilson@googlemail.com>
2024-03-14 15:22:12 +0000

    [5.1] TUF Code Cleanup (#42900)
    Co-authored-by: Harald Leithner <leithner@itronic.at>
    Co-authored-by: Richard Fath <richard67@users.noreply.github.com>

--- libraries/src/Updater/Adapter/TufAdapter.php
+++ libraries/src/Updater/Adapter/TufAdapter.php
@@ -14,6 +14,8 @@ namespace Joomla\CMS\Updater\Adapter;
 // phpcs:enable PSR1.Files.SideEffects
 
 use Joomla\CMS\Application\ApplicationHelper;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Http\HttpFactory;
 use Joomla\CMS\Table\Table;
 use Joomla\CMS\Table\Tuf as MetadataTable;
 use Joomla\CMS\TUF\TufFetcher;
@@ -74,7 +76,7 @@ class TufAdapter extends UpdateAdapter
         $metadataTable = new MetadataTable($this->db);
         $metadataTable->load(['update_site_id' => $options['update_site_id']]);
 
-        $tufFetcher = new TufFetcher($metadataTable, $options['location']);
+        $tufFetcher = new TufFetcher($metadataTable, $options['location'], $this->db, (new HttpFactory())->getHttp(), Factory::getApplication());
         $metaData   = $tufFetcher->getValidUpdate();
 
         $metaData = json_decode((string) $metaData, true);
