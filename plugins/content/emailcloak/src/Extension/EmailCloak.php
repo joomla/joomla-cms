86116657250e77109f848b0738019884f8dcb77a
Benjamin Trenkle <bembelimen@users.noreply.github.com>
2024-03-27 19:56:16 +0100

    Use isset

--- plugins/content/emailcloak/src/Extension/EmailCloak.php
+++ plugins/content/emailcloak/src/Extension/EmailCloak.php
@@ -58,11 +58,7 @@ final class EmailCloak extends CMSPlugin implements SubscriberInterface
         $item = $event->getItem();
 
         // If the item does not have a text property there is nothing to do
-        if (!property_exists($item, 'text')) {
-            return;
-        }
-
-        if (\is_null($item->text)) {
+        if (!isset($item->text)) {
             return;
         }
 
