e3200de62aa719fafb00808af7d94933f0222bd1
Richard Fath <richard67@users.noreply.github.com>
2024-03-28 13:16:28 +0100

    Fix copyright year
    Co-authored-by: Quy <quy@nomonkeybiz.com>

--- administrator/components/com_admin/postinstall/htaccessbrotli.php
+++ administrator/components/com_admin/postinstall/htaccessbrotli.php
@@ -4,7 +4,7 @@
  * @package     Joomla.Administrator
  * @subpackage  com_admin
  *
- * @copyright   (C) 2023 Open Source Matters, Inc. <https://www.joomla.org>
+ * @copyright   (C) 2024 Open Source Matters, Inc. <https://www.joomla.org>
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  *
  * This file contains post-installation message handling for notifying users of a change
