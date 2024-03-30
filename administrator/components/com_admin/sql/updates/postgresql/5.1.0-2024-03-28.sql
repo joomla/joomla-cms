ef288439c24eb86d20f49c8e2a0089054eea856e
Richard Fath <richard.fath@t-online.de>
2024-03-30 13:44:24 +0100

    No INSERT IGNORE/ON CONFLICT DO NOTHING

--- administrator/components/com_admin/sql/updates/postgresql/5.1.0-2024-03-28.sql
+++ administrator/components/com_admin/sql/updates/postgresql/5.1.0-2024-03-28.sql
@@ -5,5 +5,4 @@ INSERT INTO "#__postinstall_messages" ("extension_id", "title_key", "description
 SELECT "extension_id", 'COM_ADMIN_POSTINSTALL_MSG_HTACCESS_BROTLI_TITLE', 'COM_ADMIN_POSTINSTALL_MSG_HTACCESS_BROTLI_DESCRIPTION', '', 'com_admin', 1, 'message', '', '', 'admin://components/com_admin/postinstall/htaccessbrotli.php', 'admin_postinstall_htaccessbrotli_condition', '5.1.0', 1
   FROM "#__extensions"
  WHERE "name" = 'files_joomla'
-   AND (SELECT COUNT(a."postinstall_message_id") FROM "#__postinstall_messages" a WHERE a."title_key" = 'COM_ADMIN_POSTINSTALL_MSG_HTACCESS_BROTLI_TITLE') = 0
-ON CONFLICT DO NOTHING;
+   AND (SELECT COUNT(a."postinstall_message_id") FROM "#__postinstall_messages" a WHERE a."title_key" = 'COM_ADMIN_POSTINSTALL_MSG_HTACCESS_BROTLI_TITLE') = 0;
