7c52ffc724dc8b5ffd30d0e216fb7adfe9026f85
Benjamin Trenkle <benjamin.trenkle@wicked-software.de>
2024-04-01 21:05:57 +0200

    Revert "Remove redunant sentence"
    This reverts commit 5a506a5f7ac86810a91cb17a534e77a267524b4e.

--- libraries/src/TUF/HttpLoader.php
+++ libraries/src/TUF/HttpLoader.php
@@ -19,6 +19,7 @@ use Tuf\Loader\LoaderInterface;
  * @since  5.1.0
  *
  * @internal Currently this class is only used for Joomla! updates and will be extended in the future to support 3rd party updates
+ *           Don't extend this class in your own code, it is subject to change without notice.
  */
 class HttpLoader implements LoaderInterface
 {
