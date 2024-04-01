1b1d7465501a4f0733ad3b9a65f37c9e86610b67
Benjamin Trenkle <benjamin.trenkle@wicked-software.de>
2024-04-01 22:28:54 +0200

    Order use statement

--- tests/Unit/Libraries/Cms/Updater/ConstraintCheckerTest.php
+++ tests/Unit/Libraries/Cms/Updater/ConstraintCheckerTest.php
@@ -12,9 +12,9 @@ namespace Joomla\Tests\Unit\Libraries\Cms\Updater;
 
 use Joomla\CMS\Factory;
 use Joomla\CMS\Updater\ConstraintChecker;
+use Joomla\CMS\Version;
 use Joomla\Database\DatabaseDriver;
 use Joomla\Tests\Unit\UnitTestCase;
-use Joomla\CMS\Version;
 
 /**
  * Test class for Constraint Checker.
