5544cbdfbad53e0c23e8af18f8a486c1d5c82114
Benjamin Trenkle <benjamin.trenkle@wicked-software.de>
2024-04-01 21:05:51 +0200

    Revert "Remove redunant sentence"
    This reverts commit 427d62bce36720a052b8b43ce590bc74a43f1e27.

--- libraries/src/Updater/Adapter/TufAdapter.php
+++ libraries/src/Updater/Adapter/TufAdapter.php
@@ -31,6 +31,7 @@ use Tuf\Exception\MetadataException;
  * @since   5.1.0
  *
  * @internal Currently this class is only used for Joomla! updates and will be extended in the future to support 3rd party updates
+ *           Don't extend this class in your own code, it is subject to change without notice.
  */
 class TufAdapter extends UpdateAdapter
 {
