ba0ab8de8428f1b431ba692ebd3ff972a3462508
Sina Haghparast <33615396+sinahaghparast@users.noreply.github.com>
2024-04-09 11:14:59 +0330

    Update hits.php

--- layouts/joomla/content/info_block/hits.php
+++ layouts/joomla/content/info_block/hits.php
@@ -15,6 +15,6 @@ use Joomla\CMS\Language\Text;
 ?>
 <dd class="hits">
     <span class="icon-eye icon-fw" aria-hidden="true"></span>
-    <meta content="UserPageVisits:<?php echo $displayData['item']->hits; ?>">
+    <meta content="UserPageVisits:<?php echo $displayData['item']->hits; ?>" itemprop="userPageVisits">
     <?php echo Text::sprintf('COM_CONTENT_ARTICLE_HITS', $displayData['item']->hits); ?>
 </dd>
