51491db9568b263cb507fd8f1a10267a3b1eac03
BrianTeeman <brian@teeman.net>
2024-04-18 21:31:22 +0100

    [5.1] DarkMode selector
    On updated versions of Joomla there is no value set for the colorscheme and the only way to enable the colorscheme switch is to op4en the atum template style and then save it.
    
    This PR is (hopefully) sql that will run on the next patch release that will add a default value for the colorScheme so that the switch is always available. Note that if a user has already set a value then nothing is changed.
    
    I dont have a current postgres server so am relying on others to test that carefuly.

---
+++ administrator/components/com_admin/sql/updates/postgresql/5.1.1-2024-04-18.sql
@@ -0,0 +1,10 @@
+--
+-- Add a default value for the colorScheme in the Atum tenmplate on Joomla update
+-- only when a value is not already set.
+-- New installs will have the default value set in the installation sql.
+--
+
+UPDATE "__template_styles"
+SET "params" = jsonb_set("params", '{colorScheme}', '"os"', true)
+WHERE "template" = 'atum'
+AND NOT ("params" -> 'colorScheme' ? 'os');
