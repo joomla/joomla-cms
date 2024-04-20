29894ef94bb5d3701a9fbc1f77b1fa49ee53fad4
Christian Heel <66922325+heelc29@users.noreply.github.com>
2024-04-20 19:49:46 +0200

    installation

--- installation/src/Application/CliInstallationApplication.php
+++ installation/src/Application/CliInstallationApplication.php
@@ -213,13 +213,13 @@ final class CliInstallationApplication extends Application implements CMSApplica
     /**
      * Returns the installed language files in the administrative and frontend area.
      *
-     * @param   DatabaseInterface|null  $db  Database driver.
+     * @param   ?DatabaseInterface  $db  Database driver.
      *
      * @return  array  Array with installed language packs in admin and site area.
      *
      * @since   4.3.0
      */
-    public function getLocaliseAdmin(DatabaseInterface $db = null)
+    public function getLocaliseAdmin(?DatabaseInterface $db = null)
     {
         $langfiles = [];
 
