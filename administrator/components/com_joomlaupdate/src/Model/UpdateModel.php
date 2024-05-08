8bd76db91348d4c85f4bbd944ebd35e8cb834467
Martin Kopp <kopp05@infotech.ch>
2024-05-08 18:56:19 +0200

    keep downloadsimple

--- administrator/components/com_joomlaupdate/src/Model/UpdateModel.php
+++ administrator/components/com_joomlaupdate/src/Model/UpdateModel.php
@@ -403,7 +403,7 @@ class UpdateModel extends BaseDatabaseModel
      *
      * @since   2.5.4
      */
-    public function olddownload()
+    public function downloadsimple()
     {
         $updateInfo = $this->getUpdateInformation();
         $packageURL = trim($updateInfo['object']->downloadurl->_data);
@@ -2236,7 +2236,7 @@ ENDDATA;
             'downloaded' => 0,
             'frag'       => 0,
             'done'       => false,
-            'valid'      => true
+            'valid'      => false
         ];
 
         $packageURL  = null;
