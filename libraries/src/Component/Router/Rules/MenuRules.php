534d8cf73d42ae6189f878b95a9515677c317bcb
Hannes Papenberg <info@joomlager.de>
2024-05-11 22:27:08 +0200

    Switch to strictrouting

--- libraries/src/Component/Router/Rules/MenuRules.php
+++ libraries/src/Component/Router/Rules/MenuRules.php
@@ -167,7 +167,7 @@ class MenuRules implements RulesInterface
         }
 
         // TODO: Remove this whole block in 6.0 as it is a bug
-        if (!$this->sefparams->get('enforcesef', 0)) {
+        if (!$this->sefparams->get('strictrouting', 0)) {
             // Check if the active menuitem matches the requested language
             if (
                 $active && $active->component === 'com_' . $this->router->getName()
