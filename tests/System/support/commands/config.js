5cf1bc1d8f99924c88a9ce1e04ddaa48b7f0bd28
muhme <github@heikol.de>
2024-05-14 18:57:51 +0200

    deleted failure handler config_setParameter()
    deleted failure handler for readFile as it is not needed, tested with chmod 0, Cypress fails with clear reason:
    
    	CypressError: `cy.readFile("./configuration.php")` failed while trying to read the file at the following path:
    	`.../43465/joomla-cms/configuration.php`
    	The following error occurred:
    	> "EACCES: permission denied, open '/Users/hlu/Desktop/no_backup/43465/joomla-cms/configuration.php'"

--- tests/System/support/commands/config.js
+++ tests/System/support/commands/config.js
@@ -1,35 +1,26 @@
 Cypress.Commands.add('config_setParameter', (parameter, value) => {
   const configPath = `${Cypress.env('cmsPath')}/configuration.php`;
 
-  cy.readFile(configPath).then(
-    // Success handler
-    (fileContent) => {
-      // Setup the new value
-      let newValue = typeof value === 'string' ? `'${value}'` : value;
+  cy.readFile(configPath).then((fileContent) => {
+    // Setup the new value
+    let newValue = typeof value === 'string' ? `'${value}'` : value;
 
-      // The regex to find the line of the parameter
-      const regex = new RegExp(`^.*\\$${parameter}\\s.*$`, 'mg');
+    // The regex to find the line of the parameter
+    const regex = new RegExp(`^.*\\$${parameter}\\s.*$`, 'mg');
 
-      // Replace the whole line with the new value
-      const content = fileContent.replace(regex, `public $${parameter} = ${newValue};`);
+    // Replace the whole line with the new value
+    const content = fileContent.replace(regex, `public $${parameter} = ${newValue};`);
 
-      // Remember the original file permissions
-      cy.task('getFilePermissions', configPath).then((originalPermissions) => {
-        // To be save set read and write for owner
-        cy.task('changeFilePermissions', { path: configPath, mode: '644' }).then(() => {
-          // Write the changed file content back
-          cy.task('writeFile', { path: configPath, content }).then(() => {
-            // Restore the original file permissions
-            cy.task('changeFilePermissions', { path: configPath, mode: originalPermissions });
-          });
+    // Remember the original file permissions
+    cy.task('getFilePermissions', configPath).then((originalPermissions) => {
+      // To be save, set write for owner and read for all
+      cy.task('changeFilePermissions', { path: configPath, mode: '644' }).then(() => {
+        // Write the changed file content back
+        cy.task('writeFile', { path: configPath, content }).then(() => {
+          // Restore the original file permissions
+          cy.task('changeFilePermissions', { path: configPath, mode: originalPermissions });
         });
       });
-    },
-    // Failure handler
-    (err) => {
-      cy.log(`Failed to read the file ${configPath}: ${err.message}`);
-      // Rethrow to fail
-      throw err;
-    },
-  );
+    });
+  });
 });
