8a6b2719f3e963a22739f2af389d61d723068eac
Martin Kopp <kopp05@infotech.ch>
2024-05-20 22:07:05 +0200

    language strings fix, comment fix

--- administrator/components/com_joomlaupdate/src/Model/UpdateModel.php
+++ administrator/components/com_joomlaupdate/src/Model/UpdateModel.php
@@ -382,7 +382,7 @@ class UpdateModel extends BaseDatabaseModel
     }
 
     /**
-     * Backwards compatibility shim. Not used in Joomla Update anymore.
+     * Downloads the update package to the site - no chunks.
      *
      * @return  array
      *
