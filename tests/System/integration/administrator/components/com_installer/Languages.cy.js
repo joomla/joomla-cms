8954581ce4718e64d139e06be7523e467985eec4
alikon <optimus4joomla@gmail.com>
2024-05-25 08:40:41 +0200

    languages

---
+++ tests/System/integration/administrator/components/com_installer/Languages.cy.js
@@ -0,0 +1,18 @@
+describe('Test in backend that the Installer', () => {
+  beforeEach(() => {
+    cy.doAdministratorLogin();
+    cy.visit('/administrator/index.php?option=com_installer&view=languages');
+  });
+
+  it('has a title', () => {
+    cy.get('h1.page-title').should('contain.text', 'Extensions: Languages');
+  });
+
+  it('has Afrikaans Language', () => {
+    cy.get('tr.row0').should('contain.text', 'Afrikaans');
+  });
+
+  it('has install button', () => {
+    cy.get('input.btn.btn-primary.btn-sm').should('exist');
+  });
+});
