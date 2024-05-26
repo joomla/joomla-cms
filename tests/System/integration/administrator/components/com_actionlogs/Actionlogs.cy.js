7dd2f6fa96e268d6adf1ccaeaa4c4d79a01e932d
alikon <optimus4joomla@gmail.com>
2024-05-26 12:05:05 +0200

    cy-actionlogs

---
+++ tests/System/integration/administrator/components/com_actionlogs/Actionlogs.cy.js
@@ -0,0 +1,56 @@
+describe('Test in backend that the Actionlogs', () => {
+
+  beforeEach(() => {
+    cy.doAdministratorLogin();
+    cy.visit('/administrator/index.php?option=com_actionlogs&view=actionlogs');
+  });
+
+  it('has a title', () => {
+    cy.get('h1.page-title').should('contain.text', 'User Actions Log');
+  });
+
+  it('has no results', () => {
+    cy.get('div.alert.alert-info').should('contain.text', 'No Matching Results');
+  });
+
+  it('can display a list of actions', () => {
+    cy.db_enableExtension(1, 'plg_actionlog_joomla').then(() => {
+      cy.doAdministratorLogout();
+      cy.doAdministratorLogin();
+      cy.visit('/administrator/index.php?option=com_actionlogs&view=actionlogs');
+      cy.contains('User ci-admin logged in to admin');
+      cy.db_enableExtension(0, 'plg_actionlog_joomla');
+      cy.task('queryDB', `TRUNCATE #__action_logs`)
+    });
+  });
+
+  it('has export button', () => {
+    cy.get('#toolbar-download1').click();
+    cy.get('#system-message-container').contains('There are no User Action logs to export').should('exist');
+
+  });
+
+  it('can clear logs', () => {
+    cy.get('#toolbar-delete1').click();
+    cy.on('window:confirm', () => true);
+    cy.get('#system-message-container').contains('All User Action logs have been deleted').should('exist');
+  });
+
+  it('can delete selected logs', () => {
+    cy.get('#toolbar-delete').click();
+    cy.on('window:confirm', () => true);
+    cy.get('#system-message-container').contains('Please first make a selection from the list').should('exist');
+    cy.log('Make a selection first');
+    cy.db_enableExtension(1, 'plg_actionlog_joomla').then(() => {
+      cy.doAdministratorLogout();
+      cy.doAdministratorLogin();
+      cy.visit('/administrator/index.php?option=com_actionlogs&view=actionlogs');
+      cy.checkAllResults();
+      cy.get('#toolbar-delete').click();
+      cy.on('window:confirm', () => true);
+      cy.get('#system-message-container').contains('2 logs deleted').should('exist');
+      cy.db_enableExtension(0, 'plg_actionlog_joomla');
+      cy.task('queryDB', `TRUNCATE #__action_logs`)
+    });
+  });
+});
