f29f97e6057e778f596cc50edc6c92dad50d4621
pe7er <github@pe7er.com>
2024-05-27 10:21:47 +0200

    Joomla! 5.2.0 Alpha 1

--- libraries/src/Version.php
+++ libraries/src/Version.php
@@ -66,7 +66,7 @@ final class Version
      * @var    string
      * @since  3.8.0
      */
-    public const EXTRA_VERSION = 'alpha1-dev';
+    public const EXTRA_VERSION = 'alpha1';
 
     /**
      * Development status.
@@ -74,7 +74,7 @@ final class Version
      * @var    string
      * @since  3.5
      */
-    public const DEV_STATUS = 'Development';
+    public const DEV_STATUS = 'Alpha';
 
     /**
      * Code name.
@@ -90,7 +90,7 @@ final class Version
      * @var    string
      * @since  3.5
      */
-    public const RELDATE = '27-May-2024';
+    public const RELDATE = '28-May-2024';
 
     /**
      * Release time.
