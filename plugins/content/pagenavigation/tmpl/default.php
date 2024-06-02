efcec52d7a358da15dd8e9efad96e5a3821d0978
chmst <dev@chmst.de>
2024-06-02 13:14:52 +0200

    Better aria-label

--- plugins/content/pagenavigation/tmpl/default.php
+++ plugins/content/pagenavigation/tmpl/default.php
@@ -16,7 +16,7 @@ use Joomla\CMS\Router\Route;
 $lang = $this->getLanguage();
 ?>
 
-<nav class="pagenavigation"  aria-label="<?php echo Text::_('JLIB_HTML_PAGINATION'); ?>">
+<nav class="pagenavigation"  aria-label="<?php echo Text::_('PLG_PAGENAVIGATION_ARIA_LABEL'); ?>">
     <span class="pagination ms-0">
     <?php if ($row->prev) :
         $direction = $lang->isRtl() ? 'right' : 'left'; ?>
