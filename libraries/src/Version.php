e16a78f58890bffb4ba0c24b31e6254b649fc903
pe7er <github@pe7er.com>
2024-06-03 21:07:29 +0200

    reset to dev

--- libraries/src/Version.php
+++ libraries/src/Version.php
@@ -66,7 +66,7 @@ final class Version
      * @var    string
      * @since  3.8.0
      */
-    public const EXTRA_VERSION = 'alpha1';
+    public const EXTRA_VERSION = 'alpha2-dev';
 
     /**
      * Development status.
@@ -74,7 +74,7 @@ final class Version
      * @var    string
      * @since  3.5
      */
-    public const DEV_STATUS = 'Alpha';
+    public const DEV_STATUS = 'Development';
 
     /**
      * Code name.
@@ -90,7 +90,7 @@ final class Version
      * @var    string
      * @since  3.5
      */
-    public const RELDATE = '28-May-2024';
+    public const RELDATE = '3-June-2024';
 
     /**
      * Release time.
@@ -99,7 +99,7 @@ final class Version
      * @since  3.5
      */
 
-    public const RELTIME = '17:00';
+    public const RELTIME = '20:06';
 
     /**
      * Release timezone.
