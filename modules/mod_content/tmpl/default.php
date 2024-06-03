83d2fd153b5bab112e93d65ec971def714cf11d1
BrianTeeman <brian@teeman.net>
2024-06-03 11:30:09 +0100

    second commit

--- modules/mod_content/tmpl/default.php
+++ modules/mod_content/tmpl/default.php
@@ -4,23 +4,29 @@
  * @package     Joomla.Site
  * @subpackage  mod_content
  *
- * @copyright   (C) 2006 Open Source Matters, Inc. <https://www.joomla.org>
+ * @copyright   (C) 2010 Open Source Matters, Inc. <https://www.joomla.org>
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 defined('_JEXEC') or die;
 
 use Joomla\CMS\Helper\ModuleHelper;
+use Joomla\CMS\Language\Text;
 
 if (!$list) {
     return;
 }
 
 ?>
-<div class="mod-content newsflash">
-    <?php foreach ($list as $item) : ?>
-        <div class="mod-content__item" itemscope itemtype="https://schema.org/Article">
-            <?php require ModuleHelper::getLayoutPath('mod_content', '_item'); ?>
-        </div>
-    <?php endforeach; ?>
+<div class="mod-content category-module mod-list">
+    <?php if ($grouped) : ?>
+        <?php foreach ($list as $groupName => $items) : ?>
+            <div class="mod-content-group"><?php echo Text::_($groupName); ?></div>
+                <?php require ModuleHelper::getLayoutPath('mod_content', $params->get('layout', 'default') . '_items'); ?>
+            </div>
+        <?php endforeach; ?>
+    <?php else : ?>
+        <?php $items = $list; ?>
+        <?php require ModuleHelper::getLayoutPath('mod_content', $params->get('layout', 'default') . '_items'); ?>
+    <?php endif; ?>
 </div>
