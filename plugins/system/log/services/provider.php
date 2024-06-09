ba0a114e3bb3a0ceefaa8234617469c7cc7d31fa
Fedik <getthesite@gmail.com>
2024-06-09 14:18:53 +0300

    system log

--- plugins/system/log/services/provider.php
+++ plugins/system/log/services/provider.php
@@ -11,7 +11,6 @@
 \defined('_JEXEC') or die;
 
 use Joomla\CMS\Extension\PluginInterface;
-use Joomla\CMS\Factory;
 use Joomla\CMS\Plugin\PluginHelper;
 use Joomla\DI\Container;
 use Joomla\DI\ServiceProviderInterface;
