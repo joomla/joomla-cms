3908874dce78d921e1aaa9041f5a2979704a952c
Nicola Galgano <optimus4joomla@gmail.com>
2024-06-19 08:37:47 +0200

    4.4-only

--- tests/System/integration/administrator/components/com_cache/Default.cy.js
+++ tests/System/integration/administrator/components/com_cache/Default.cy.js
@@ -19,12 +19,6 @@ describe('Test in backend that the cache', () => {
   it('can clear expired cache', () => {
     cy.get('#toolbar-delete2').click();
     cy.on('window:confirm', () => true);
-    // Starting with Joomla 5.1 check if the 'Yes' button exists and click it if present
-    cy.get('body').then(($body) => {
-      if ($body.find('div.buttons-holder button[data-button-ok]').length > 0) {
-        cy.get('div.buttons-holder button[data-button-ok]').click();
-      }
-    });
     cy.get('#system-message-container').contains('Expired cached items have been cleared').should('exist');
   });
 
