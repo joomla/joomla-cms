e9e6513102436001cb77f671a4d6c07385866145
Allon Moritz <allon.moritz@digital-peak.com>
2024-06-19 08:24:16 +0200

    Do real login test

--- tests/System/integration/administrator/components/com_login/Login.cy.js
+++ tests/System/integration/administrator/components/com_login/Login.cy.js
@@ -1,6 +1,15 @@
 describe('Test in backend that the login component', () => {
   it('can log in and out', () => {
-    cy.doAdministratorLogin(null, null, false);
-    cy.doAdministratorLogout();
+		cy.visit('administrator/index.php');
+    cy.get('#mod-login-username').type(Cypress.env('username'));
+    cy.get('#mod-login-password').type(Cypress.env('password'));
+    cy.get('#btn-login-submit').click();
+
+    cy.get('h1.page-title').should('contain', 'Home Dashboard');
+
+		cy.get('.header-item .header-profile > .dropdown-toggle').click();
+    cy.get('.header-item .header-profile a.dropdown-item:last-child').click();
+
+    cy.get('#mod-login-username').should('exist');
   });
 });
