cd3085d1a1941f9752b7913e607e6c1b6251bff1
pe7er <github@pe7er.com>
2024-06-26 09:23:41 +0200

    corrected release date/time

--- libraries/src/Version.php
+++ libraries/src/Version.php
@@ -90,7 +90,7 @@ final class Version
      * @var    string
      * @since  3.5
      */
-    public const RELDATE = '23-July-2024';
+    public const RELDATE = '25-June-2024';
 
     /**
      * Release time.
@@ -98,7 +98,7 @@ final class Version
      * @var    string
      * @since  3.5
      */
-    public const RELTIME = '16:01';
+    public const RELTIME = '17:01';
 
     /**
      * Release timezone.
