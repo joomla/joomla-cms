3f37dd752517a39ea66339ea4f43a9b80a12039d
muhme <github@heikol.de>
2024-06-29 17:34:54 +0200

    Hack to prevent deprecated in mod_related_items

--- tests/System/integration/site/modules/mod_related_items/Default.cy.js
+++ tests/System/integration/site/modules/mod_related_items/Default.cy.js
@@ -8,6 +8,14 @@ describe('Test in frontend that the related items module', () => {
         cy.contains('a', 'Main Article').click();
 
         cy.contains('li', 'article with joomla keyword');
+
+        // HACK, muhme, June-29-2024 - DO NOT UP-MERGE
+        // Only hacked in System Tests, as 4.4-dev is only active for a limited time.
+        // But if you have a cheap solution, let me know or create your own PR.
+        // To prevent: Deprecated: DateTime::__construct(): Passing null to parameter #1 ($datetime) of type string
+        //             is deprecated in /joomla-cms/libraries/src/Date/Date.php on line 126
+        // Hack: Back to the home, as only the article page contains the deprecated warning.
+        cy.visit('/');
       });
   });
 });
