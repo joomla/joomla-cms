14dede83e71a71549f3a3387facd01564f813ea9
Nicola Galgano <optimus4joomla@gmail.com>
2024-07-01 19:27:29 +0200

    related-items

--- tests/System/integration/site/modules/mod_related_items/Default.cy.js
+++ tests/System/integration/site/modules/mod_related_items/Default.cy.js
@@ -1,7 +1,7 @@
 describe('Test in frontend that the related items module', () => {
   it('can display a list of related articles based on the metakey field', () => {
-    cy.db_createArticle({ title: 'Main Article', metakey: 'joomla', featured: 1 })
-      .then(() => cy.db_createArticle({ title: 'article with joomla keyword', metakey: 'joomla' }))
+    cy.db_createArticle({ title: 'Main Article', metakey: 'joomla', featured: 1, created: '2022-09-09 20:00:00', modified: '2022-09-09 20:00:00'})
+      .then(() => cy.db_createArticle({ title: 'article with joomla keyword', metakey: 'joomla', created: '2022-09-09 20:00:00', modified: '2022-09-09 20:00:00' }))
       .then(() => cy.db_createModule({ module: 'mod_related_items' }))
       .then(() => {
         cy.visit('/');
