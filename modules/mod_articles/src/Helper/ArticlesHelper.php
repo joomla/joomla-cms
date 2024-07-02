ea0ab8ff9d7f5a88f98731374abb11393ef19332
Viviana Menzel <vivianamenzel@dr-menzel-it.de>
2024-07-02 21:16:16 +0200

    First draft

---
+++ modules/mod_articles/src/Helper/ArticlesHelper.php
@@ -0,0 +1,448 @@
+<?php
+
+/**
+ * @package     Joomla.Site
+ * @subpackage  mod_articles
+ *
+ * @copyright   (C) 2024 Open Source Matters, Inc. <https://www.joomla.org>
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+namespace Joomla\Module\Articles\Site\Helper;
+
+use Joomla\CMS\Access\Access;
+use Joomla\CMS\Application\SiteApplication;
+use Joomla\CMS\Component\ComponentHelper;
+use Joomla\CMS\Date\Date;
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\HTML\Helpers\StringHelper as SpecialStringHelper;
+use Joomla\CMS\Plugin\PluginHelper;
+use Joomla\CMS\Router\Route;
+use Joomla\Component\Content\Administrator\Extension\ContentComponent;
+use Joomla\Component\Content\Site\Helper\RouteHelper;
+use Joomla\Database\DatabaseAwareInterface;
+use Joomla\Database\DatabaseAwareTrait;
+use Joomla\Registry\Registry;
+use Joomla\String\StringHelper;
+
+// phpcs:disable PSR1.Files.SideEffects
+\defined('_JEXEC') or die;
+// phpcs:enable PSR1.Files.SideEffects
+
+/**
+ * Helper for mod_articles
+ *
+ * @since  __DEPLOY_VERSION__
+ */
+class ArticlesHelper implements DatabaseAwareInterface
+{
+    use DatabaseAwareTrait;
+
+    /**
+     * Retrieve a list of articles
+     *
+     * @param   Registry         $params  The module parameters.
+     * @param   SiteApplication  $app     The current application.
+     *
+     * @return  object[]
+     *
+     * @since   __DEPLOY_VERSION__
+     */
+    public function getArticles(Registry $params, SiteApplication $app)
+    {
+        $factory = $app->bootComponent('com_content')->getMVCFactory();
+
+        // Get an instance of the generic articles model
+        $articles = $factory->createModel('Articles', 'Site', ['ignore_request' => true]);
+
+        // Set application parameters in model
+        $input     = $app->getInput();
+        $appParams = $app->getParams();
+        $articles->setState('params', $appParams);
+
+        $articles->setState('list.start', 0);
+        $articles->setState('filter.published', ContentComponent::CONDITION_PUBLISHED);
+
+        // Set the filters based on the module params
+        $articles->setState('list.limit', (int) $params->get('count', 0));
+        $articles->setState('load_tags', $params->get('show_tags', 0) || $params->get('article_grouping', 'none') === 'tags');
+
+        // Access filter
+        $access     = !ComponentHelper::getParams('com_content')->get('show_noauth');
+        $authorised = Access::getAuthorisedViewLevels($app->getIdentity()->get('id'));
+        $articles->setState('filter.access', $access);
+
+        $catids = $params->get('catid');
+        $articles->setState('filter.category_id.include', (bool) $params->get('category_filtering_type', 1));
+
+        // Category filter
+        if ($catids) {
+            if ($params->get('show_child_category_articles', 0) && (int) $params->get('levels', 0) > 0) {
+                // Get an instance of the generic categories model
+                $categories = $factory->createModel('Categories', 'Site', ['ignore_request' => true]);
+                $categories->setState('params', $appParams);
+                $levels = $params->get('levels', 1) ?: 9999;
+                $categories->setState('filter.get_children', $levels);
+                $categories->setState('filter.published', 1);
+                $categories->setState('filter.access', $access);
+                $additional_catids = [];
+
+                foreach ($catids as $catid) {
+                    $categories->setState('filter.parentId', $catid);
+                    $recursive = true;
+                    $items     = $categories->getItems($recursive);
+
+                    if ($items) {
+                        foreach ($items as $category) {
+                            $condition = (($category->level - $categories->getParent()->level) <= $levels);
+
+                            if ($condition) {
+                                $additional_catids[] = $category->id;
+                            }
+                        }
+                    }
+                }
+
+                $catids = array_unique(array_merge($catids, $additional_catids));
+            }
+
+            $articles->setState('filter.category_id', $catids);
+        }
+
+        // Ordering
+        $ordering = $params->get('article_ordering', 'a.ordering');
+
+        switch ($ordering) {
+            case 'random':
+                $articles->setState('list.ordering', $this->getDatabase()->getQuery(true)->rand());
+                break;
+
+            case 'rating_count':
+            case 'rating':
+                $articles->setState('list.ordering', $ordering);
+                $articles->setState('list.direction', $params->get('article_ordering_direction', 'ASC'));
+
+                if (!PluginHelper::isEnabled('content', 'vote')) {
+                    $articles->setState('list.ordering', 'a.ordering');
+                }
+
+                break;
+
+            default:
+                $articles->setState('list.ordering', $ordering);
+                $articles->setState('list.direction', $params->get('article_ordering_direction', 'ASC'));
+                break;
+        }
+
+        // Filter by multiple tags
+        $articles->setState('filter.tag', $params->get('filter_tag', []));
+
+        $articles->setState('filter.featured', $params->get('show_featured', 'show'));
+        $articles->setState('filter.author_id', $params->get('created_by', []));
+        $articles->setState('filter.author_id.include', $params->get('author_filtering_type', 1));
+        $articles->setState('filter.author_alias', $params->get('created_by_alias', []));
+        $articles->setState('filter.author_alias.include', $params->get('author_alias_filtering_type', 1));
+
+        // Filter archived articles
+        if ($params->get('show_archived', 'hide') === 'show') {
+            $articles->setState('filter.published', ContentComponent::CONDITION_ARCHIVED);
+        }
+
+        $excluded_articles = $params->get('excluded_articles', '');
+        if ($excluded_articles) {
+            $excluded_articles = explode("\r\n", $excluded_articles);
+            $articles->setState('filter.article_id', $excluded_articles);
+
+            // Exclude
+            $articles->setState('filter.article_id.include', false);
+        }
+
+        $date_filtering = $params->get('date_filtering', 'off');
+
+        if ($date_filtering !== 'off') {
+            $articles->setState('filter.date_filtering', $date_filtering);
+            $articles->setState('filter.date_field', $params->get('date_field', 'a.created'));
+            $articles->setState('filter.start_date_range', $params->get('start_date_range', '1000-01-01 00:00:00'));
+            $articles->setState('filter.end_date_range', $params->get('end_date_range', '9999-12-31 23:59:59'));
+            $articles->setState('filter.relative_date', $params->get('relative_date', 30));
+        }
+
+        // Filter by language
+        $articles->setState('filter.language', $app->getLanguageFilter());
+
+        $items = $articles->getItems();
+
+        // Display options
+        $show_date          = $params->get('show_date', 0);
+        $show_date_field    = $params->get('show_date_field', 'created');
+        $show_date_format   = $params->get('show_date_format', 'Y-m-d H:i:s');
+        $show_category      = $params->get('show_category', 0);
+        $show_category_link = $params->get('show_category_link', 0);
+        $show_hits          = $params->get('show_hits', 0);
+        $show_author        = $params->get('show_author', 0);
+        $show_introtext     = $params->get('show_introtext', 0);
+        $introtext_limit    = $params->get('introtext_limit', 100);
+
+        // Find current Article ID if on an article page
+        $option = $input->get('option');
+        $view   = $input->get('view');
+
+        if ($option === 'com_content' && $view === 'article') {
+            $active_article_id = $input->getInt('id');
+        } else {
+            $active_article_id = 0;
+        }
+
+        // Prepare data for display using display options
+        foreach ($items as &$item) {
+            $item->slug = $item->id . ':' . $item->alias;
+
+            if ($access || \in_array($item->access, $authorised)) {
+                // We know that user has the privilege to view the article
+                $item->link = Route::_(RouteHelper::getArticleRoute($item->slug, $item->catid, $item->language));
+            } else {
+                $menu      = $app->getMenu();
+                $menuitems = $menu->getItems('link', 'index.php?option=com_users&view=login');
+
+                if (isset($menuitems[0])) {
+                    $Itemid = $menuitems[0]->id;
+                } elseif ($input->getInt('Itemid') > 0) {
+                    // Use Itemid from requesting page only if there is no existing menu
+                    $Itemid = $input->getInt('Itemid');
+                }
+
+                $item->link = Route::_('index.php?option=com_users&view=login&Itemid=' . $Itemid);
+            }
+
+            // Used for styling the active article
+            $item->active      = $item->id == $active_article_id ? 'active' : '';
+            $item->displayDate = '';
+
+            if ($show_date) {
+                $item->displayDate = HTMLHelper::_('date', $item->$show_date_field, $show_date_format);
+            }
+
+            if ($show_category && $show_category_link) {
+                $item->displayCategoryLink  = Route::_(RouteHelper::getCategoryRoute($item->catid, $item->category_language));
+                $item->displayCategoryTitle = '<a href="' . $item->displayCategoryLink . '">' . $item->category_title . '</a>';
+            } else {
+                $item->displayCategoryTitle = $show_category ? $item->category_title : '';
+            }
+
+            $item->displayHits       = $show_hits ? $item->hits : '';
+            $item->displayAuthorName = $show_author ? $item->author : '';
+
+
+            if ($show_introtext) {
+                $item->introtext = HTMLHelper::_('content.prepare', $item->introtext, '', 'mod_articles.content');
+            }
+
+            // Remove any images belongs to the text
+            if (!$params->get('image')) {
+                $item->introtext = preg_replace('/<img[^>]*>/', '', $item->introtext);
+            }
+
+            // Show the Intro/Full image field of the article
+            if ($params->get('img_intro_full') !== 'none') {
+                $images             = json_decode($item->images);
+                $item->imageSrc     = '';
+                $item->imageAlt     = '';
+                $item->imageCaption = '';
+
+                if ($params->get('img_intro_full') === 'intro' && !empty($images->image_intro)) {
+                    $item->imageSrc = htmlspecialchars($images->image_intro, ENT_COMPAT, 'UTF-8');
+                    $item->imageAlt = htmlspecialchars($images->image_intro_alt, ENT_COMPAT, 'UTF-8');
+
+                    if ($images->image_intro_caption) {
+                        $item->imageCaption = htmlspecialchars($images->image_intro_caption, ENT_COMPAT, 'UTF-8');
+                    }
+                } elseif ($params->get('img_intro_full') === 'full' && !empty($images->image_fulltext)) {
+                    $item->imageSrc = htmlspecialchars($images->image_fulltext, ENT_COMPAT, 'UTF-8');
+                    $item->imageAlt = htmlspecialchars($images->image_fulltext_alt, ENT_COMPAT, 'UTF-8');
+
+                    if ($images->image_intro_caption) {
+                        $item->imageCaption = htmlspecialchars($images->image_fulltext_caption, ENT_COMPAT, 'UTF-8');
+                    }
+                }
+            }
+            $item->displayIntrotext = $show_introtext ? SpecialStringHelper::truncate($item->introtext, $introtext_limit, true, false) : '';
+            $item->displayReadmore  = $item->alternative_readmore;
+        }
+
+        // Check if items need be grouped
+        $article_grouping           = $params->get('article_grouping', 'none');
+        $article_grouping_direction = $params->get('article_grouping_direction', 'ksort');
+        $grouped                    = $article_grouping !== 'none';
+
+        if ($items && $grouped) {
+            switch ($article_grouping) {
+                case 'year':
+                case 'month_year':
+                    $items = ArticlesHelper::groupByDate(
+                        $items,
+                        $article_grouping_direction,
+                        $article_grouping,
+                        $params->get('month_year_format', 'F Y'),
+                        $params->get('date_grouping_field', 'created')
+                    );
+                    break;
+                case 'author':
+                case 'category_title':
+                    $items = ArticlesHelper::groupBy($items, $article_grouping, $article_grouping_direction);
+                    break;
+                case 'tags':
+                    $items = ArticlesHelper::groupByTags($items, $article_grouping_direction);
+                    break;
+            }
+        }
+
+        return $items;
+    }
+
+    /**
+     * Groups items by field
+     *
+     * @param   array   $list             list of items
+     * @param   string  $fieldName        name of field that is used for grouping
+     * @param   string  $direction        ordering direction
+     * @param   null    $fieldNameToKeep  field name to keep
+     *
+     * @return  array
+     *
+     * @since   __DEPLOY_VERSION__
+     */
+    public static function groupBy($list, $fieldName, $direction, $fieldNameToKeep = null)
+    {
+        $grouped = [];
+
+        if (!\is_array($list)) {
+            if ($list === '') {
+                return $grouped;
+            }
+
+            $list = [$list];
+        }
+
+        foreach ($list as $key => $item) {
+            if (!isset($grouped[$item->$fieldName])) {
+                $grouped[$item->$fieldName] = [];
+            }
+
+            if ($fieldNameToKeep === null) {
+                $grouped[$item->$fieldName][$key] = $item;
+            } else {
+                $grouped[$item->$fieldName][$key] = $item->$fieldNameToKeep;
+            }
+
+            unset($list[$key]);
+        }
+
+        $direction($grouped);
+
+        return $grouped;
+    }
+
+    /**
+     * Groups items by date
+     *
+     * @param   array   $list             list of items
+     * @param   string  $direction        ordering direction
+     * @param   string  $type             type of grouping
+     * @param   string  $monthYearFormat  date format to use
+     * @param   string  $field            date field to group by
+     *
+     * @return  array
+     *
+     * @since   __DEPLOY_VERSION__
+     */
+    public static function groupByDate($list, $direction = 'ksort', $type = 'year', $monthYearFormat = 'F Y', $field = 'created')
+    {
+        $grouped = [];
+
+        if (!\is_array($list)) {
+            if ($list === '') {
+                return $grouped;
+            }
+
+            $list = [$list];
+        }
+
+        foreach ($list as $key => $item) {
+            switch ($type) {
+                case 'month_year':
+                    $month_year = StringHelper::substr($item->$field, 0, 7);
+
+                    if (!isset($grouped[$month_year])) {
+                        $grouped[$month_year] = [];
+                    }
+
+                    $grouped[$month_year][$key] = $item;
+                    break;
+
+                default:
+                    $year = StringHelper::substr($item->$field, 0, 4);
+
+                    if (!isset($grouped[$year])) {
+                        $grouped[$year] = [];
+                    }
+
+                    $grouped[$year][$key] = $item;
+                    break;
+            }
+
+            unset($list[$key]);
+        }
+
+        $direction($grouped);
+
+        if ($type === 'month_year') {
+            foreach ($grouped as $group => $items) {
+                $date                      = new Date($group);
+                $formatted_group           = $date->format($monthYearFormat);
+                $grouped[$formatted_group] = $items;
+
+                unset($grouped[$group]);
+            }
+        }
+
+        return $grouped;
+    }
+
+    /**
+     * Groups items by tags
+     *
+     * @param   array   $list       list of items
+     * @param   string  $direction  ordering direction
+     *
+     * @return  array
+     *
+     * @since   __DEPLOY_VERSION__
+     */
+    public static function groupByTags($list, $direction = 'ksort')
+    {
+        $grouped  = [];
+        $untagged = [];
+
+        if (!$list) {
+            return $grouped;
+        }
+
+        foreach ($list as $item) {
+            if ($item->tags->itemTags) {
+                foreach ($item->tags->itemTags as $tag) {
+                    $grouped[$tag->title][] = $item;
+                }
+            } else {
+                $untagged[] = $item;
+            }
+        }
+
+        $direction($grouped);
+
+        if ($untagged) {
+            $grouped['MOD_ARTICLES_UNTAGGED'] = $untagged;
+        }
+
+        return $grouped;
+    }
+}
---
+++ modules/mod_articles/tmpl/_items.php
@@ -0,0 +1,132 @@
+<?php
+
+/**
+ * @package     Joomla.Site
+ * @subpackage  mod_articles
+ *
+ * @copyright   (C) 2024 Open Source Matters, Inc. <https://www.joomla.org>
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Layout\LayoutHelper;
+
+?>
+<ul class="mod-articles-items <?php echo ($params->get('articles_layout') == 1 ? 'mod-articles-grid' : ''); ?> mod-list">
+    <?php foreach ($items as $item) : ?>
+        <?php
+            $displayInfo = $item->displayHits || $item->displayAuthorName || $item->displayCategoryTitle || $item->displayDate;
+        ?>
+        <li>
+            <article class="mod-articles-item <?php echo ($params->get('card_link') ? 'mod-articles-item-card' : ''); ?>" itemscope itemtype="https://schema.org/Article">
+
+                <?php if ($params->get('item_title') || $displayInfo || $params->get('show_tags') || $params->get('show_introtext') || $params->get('show_readmore')) : ?>
+                    <div class="mod-articles-item-content">
+
+                        <?php if ($params->get('item_title')) : ?>
+                            <?php $item_heading = $params->get('item_heading', 'h4'); ?>
+                            <<?php echo $item_heading; ?> class="mod-articles-title" itemprop="name">
+                                <?php if ($params->get('link_titles') == 1) : ?>
+                                    <?php $attributes = ['class' => 'mod-articles-link ' . $item->active, 'itemprop' => 'url']; ?>
+                                    <?php $link = htmlspecialchars($item->link, ENT_COMPAT, 'UTF-8', false); ?>
+                                    <?php $title = htmlspecialchars($item->title, ENT_COMPAT, 'UTF-8', false); ?>
+                                    <?php echo HTMLHelper::_('link', $link, $title, $attributes); ?>
+                                <?php else : ?>
+                                    <?php echo $item->title; ?>
+                                <?php endif; ?>
+                            </<?php echo $item_heading; ?>>
+                        <?php endif; ?>
+
+                        <?php if ($displayInfo) : ?>
+                            <?php
+                                $listClass = 'list-unstyled';
+                                if ($params->get('info_layout') == 1) {
+                                    $listClass = 'list-inline';
+                                }
+                            ?>
+                            <ul class="<?php echo $listClass; ?>">
+
+                                <?php if ($item->displayHits) : ?>
+                                    <li class="mod-articles-hits <?php echo ($params->get('info_layout') == 1 ? 'list-inline-item' : ''); ?>">
+                                        (<?php echo $item->displayHits; ?>)
+                                    </li>
+                                <?php endif; ?>
+
+                                <?php if ($item->displayAuthorName) : ?>
+                                    <li class="mod-articles-writtenby <?php echo ($params->get('info_layout') == 1 ? 'list-inline-item' : ''); ?>">
+                                        <?php echo $item->displayAuthorName; ?>
+                                    </li>
+                                <?php endif; ?>
+
+                                <?php if ($item->displayCategoryTitle) : ?>
+                                    <li class="mod-articles-category <?php echo ($params->get('info_layout') == 1 ? 'list-inline-item' : ''); ?>">
+                                        (<?php echo $item->displayCategoryTitle; ?>)
+                                    </li>
+                                <?php endif; ?>
+
+                                <?php if ($item->displayDate) : ?>
+                                    <li class="mod-articles-date <?php echo ($params->get('info_layout') == 1 ? 'list-inline-item' : ''); ?>">
+                                        <?php echo $item->displayDate; ?>
+                                    </li>
+                                <?php endif; ?>
+                            </ul>
+
+                        <?php endif; ?>
+
+                        <?php if ($params->get('show_tags', 0) && $item->tags->itemTags) : ?>
+                            <div class="mod-articles-tags">
+                                <?php echo LayoutHelper::render('joomla.content.tags', $item->tags->itemTags); ?>
+                            </div>
+                        <?php endif; ?>
+
+                        <?php if ($params->get('show_introtext')) : ?>
+                            <p><?php echo $item->displayIntrotext; ?></p>
+                        <?php endif; ?>
+
+                        <?php if ($params->get('show_readmore')) : ?>
+                            <p class="mod-articles-readmore">
+
+                                <<?php echo ($params->get('card_link') ? 'span' : 'a'); ?> class="btn btn-secondary" href="<?php echo $item->link; ?>">
+                                    <?php if ($item->params->get('access-view') == false) : ?>
+                                        <?php echo Text::_('MOD_ARTICLES_REGISTER_TO_READ_MORE'); ?>
+                                    <?php elseif ($item->alternative_readmore) : ?>
+                                        <?php echo $item->alternative_readmore; ?>
+                                        <?php echo HTMLHelper::_('string.truncate', $item->title, $params->get('readmore_limit')); ?>
+                                            <?php if ($params->get('show_readmore_title', 0)) : ?>
+                                                <?php echo HTMLHelper::_('string.truncate', $item->title, $params->get('readmore_limit')); ?>
+                                            <?php endif; ?>
+                                    <?php elseif ($params->get('show_readmore_title', 0)) : ?>
+                                        <?php echo Text::_('MOD_ARTICLES_READ_MORE'); ?>
+                                        <?php echo HTMLHelper::_('string.truncate', $item->title, $params->get('readmore_limit')); ?>
+                                    <?php else : ?>
+                                        <?php echo Text::_('MOD_ARTICLES_READ_MORE_TITLE'); ?>
+                                    <?php endif; ?>
+                                </<?php echo ($params->get('card_link') ? 'span' : 'a'); ?>>
+                            </p>
+                        <?php endif; ?>
+                    </div>
+                <?php endif; ?>
+
+                <?php if ($params->get('img_intro_full') !== 'none' && !empty($item->imageSrc)) : ?>
+                    <figure class="mod-articles-image">
+                        <?php echo LayoutHelper::render(
+                            'joomla.html.image',
+                            [
+                                'src' => $item->imageSrc,
+                                'alt' => $item->imageAlt,
+                            ]
+                        ); ?>
+                        <?php if (!empty($item->imageCaption)) : ?>
+                            <figcaption>
+                                <?php echo $item->imageCaption; ?>
+                            </figcaption>
+                        <?php endif; ?>
+                    </figure>
+                <?php endif; ?>
+            </article>
+        </li>
+    <?php endforeach; ?>
+</ul>
