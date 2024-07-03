6418600e7ca63b76aa8472009e91fb8c2e02a201
janschoenherr <jan@yootheme.com>
2024-07-03 10:49:04 +0200

    Change redirect url (from headers['Location']) from array to string

--- libraries/src/Http/Transport/SocketTransport.php
+++ libraries/src/Http/Transport/SocketTransport.php
@@ -131,7 +131,7 @@ class SocketTransport extends AbstractTransport implements TransportInterface
 
         // Follow Http redirects
         if ($content->code >= 301 && $content->code < 400 && isset($content->headers['Location'])) {
-            return $this->request($method, new Uri($content->headers['Location']), $data, $headers, $timeout, $userAgent);
+            return $this->request($method, new Uri($content->headers['Location'][0]), $data, $headers, $timeout, $userAgent);
         }
 
         return $content;
