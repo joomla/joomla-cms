ff1de3b855677fe0d50426b542859b203660203c
Jonathan Brain <3941269+BrainforgeUK@users.noreply.github.com>
2024-07-06 13:54:48 +0100

    Update default.php
    groupByFieldset functionality added.

--- layouts/joomla/form/field/subform/default.php
+++ layouts/joomla/form/field/subform/default.php
@@ -14,28 +14,68 @@ use Joomla\CMS\Form\Form;
 
 extract($displayData);
 
-/**
- * Layout variables
- * -----------------
- * @var   Form    $tmpl             The Empty form for template
- * @var   array   $forms            Array of JForm instances for render the rows
- * @var   bool    $multiple         The multiple state for the form field
- * @var   int     $min              Count of minimum repeating in multiple mode
- * @var   int     $max              Count of maximum repeating in multiple mode
- * @var   string  $name             Name of the input field.
- * @var   string  $fieldname        The field name
- * @var   string  $fieldId          The field ID
- * @var   string  $control          The forms control
- * @var   string  $label            The field label
- * @var   string  $description      The field description
- * @var   array   $buttons          Array of the buttons that will be rendered
- * @var   bool    $groupByFieldset  Whether group the subform fields by it`s fieldset
- */
-$form = $forms[0];
-?>
-
 <div class="subform-wrapper">
-<?php foreach ($form->getGroup('') as $field) : ?>
-    <?php echo $field->renderField(); ?>
-<?php endforeach; ?>
+    <?php
+    if ($groupByFieldset)
+    {
+        foreach($form->getFieldsets() as $fieldSet)
+        {
+            /*
+             * Fieldset attributes (see https://docs.joomla.org/Advanced_form_guide)
+             *  name                Element name
+             *                      Required value.
+             *  display             Element display style property.
+             *                      Default: "grid"
+             *  gridtemplatecolumns Element grid-template-columns style property.
+             *                      Default: 1fr for each grid field (i.e. all fields in one row)
+             *                          If you have 6 fields then all 6 fields will be in one row.
+             *                          If you set this to "1fr 1fr" then you will have 4 rows with 2 fields in each.
+             *  columngap           Element column-gap style property.
+             *                      Default: "1rem"
+             *  style               Optional style properties for this field set.
+             *                      Example: "border:1px dotted #ccc;padding:2px;box-shadow:2px 2px #888;"
+             */
+
+            $fields = $form->getFieldset($fieldSet->name);
+
+            $cols = implode(' ', array_fill(0, count($fields), '1fr'));
+
+            $fieldsetStyle=[];
+
+            $fieldsetStyle[] = 'display:' . ($fieldSet->display ?? 'grid') . ';';
+
+            $fieldsetStyle[] = 'grid-template-columns:' . ($fieldSet->gridtemplatecolumns ?? $cols)  . ';';
+
+            $fieldsetStyle[] = 'column-gap:' . ($fieldSet->columngap ?? '1rem') . ';';
+
+            $fieldsetStyle[] = $fieldSet->style ?? '';
+
+            ?>
+            <div class="<?php echo $fieldSet->class ?? ''; ?>"
+                 style="<?php echo implode('', $fieldsetStyle); ?>">
+                <?php
+                foreach ($fields as $field)
+                {
+                    ?>
+                    <div class="<?php echo $fieldSet->name . '-fieldwrapper ' .
+                                           $fieldSet->name . '-' . $field->fieldname . '-fieldwrapper'; ?>"
+                         style="<?php echo $fieldSet->fieldwrapperstyle ?? ''; ?>"
+                    >
+                        <?php echo $field->renderField(); ?>
+                    </div>
+                    <?php
+                }
+                ?>
+            </div>
+            <?php
+        }
+    }
+    else
+    {
+        foreach ($form->getGroup('') as $field)
+        {
+            echo $field->renderField();
+        }
+    }
+    ?>
 </div>
