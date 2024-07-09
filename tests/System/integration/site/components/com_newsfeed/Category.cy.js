2aaf9be46947c10d041ff0807f461538be0b52e8
Nicola Galgano <optimus4joomla@gmail.com>
2024-07-09 08:35:17 +0200

    nomore

--- tests/System/integration/site/components/com_newsfeed/Category.cy.js
+++ tests/System/integration/site/components/com_newsfeed/Category.cy.js
@@ -28,14 +28,6 @@ describe('Test in frontend that the newsfeeds category view', () => {
         cy.contains('automated test feed 2');
         cy.contains('automated test feed 3');
         cy.contains('automated test feed 4');
-
-        // HACK, muhme, June-30-2024 - DO NOT UP-MERGE, no problem in 5.1 upwards
-        // Only avoided in System Tests, as the end of regular bugfix support for 4.x is 15 October 2024.
-        // However, if you prefer a real solution in view with down-merge, create a PR or let me know.
-        // To prevent: Warning: Attempt to read property "id" on null in
-        //             /components/com_newsfeeds/src/View/Category/HtmlView.php on line 92
-        // Workaround: Back to the home, as only the newsfeed view page contains the warning.
-        cy.visit('/');
       });
   });
 });
