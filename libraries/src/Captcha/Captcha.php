84a42224a876510e40875da8f13b8220f80f7ac4
Artur Stępień <artur.stepien@bestproject.pl>
2024-07-11 13:49:57 +0200

    Update Captcha.php

--- libraries/src/Captcha/Captcha.php
+++ libraries/src/Captcha/Captcha.php
@@ -281,10 +281,10 @@ class Captcha implements DispatcherAwareInterface
         // Build the path to the needed captcha plugin
         $name = InputFilter::getInstance()->clean($this->name, 'cmd');
 
-        // Boot the editor plugin
+        // Boot the captcha plugin
         $this->captcha = Factory::getApplication()->bootPlugin($name, 'captcha');
 
-        // Check if the editor can be loaded
+        // Check if the captcha can be loaded
         if (!$this->captcha) {
             throw new \RuntimeException(Text::sprintf('JLIB_CAPTCHA_ERROR_PLUGIN_NOT_FOUND', $name));
         }
