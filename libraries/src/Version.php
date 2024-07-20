f893b2e6c7442705d1e18bbc2d10d0494851f455
Hannes Papenberg <info@joomlager.de>
2024-07-20 09:44:39 +0200

    Release Joomla! 5.2.0 Alpha 3

--- libraries/src/Version.php
+++ libraries/src/Version.php
@@ -66,7 +66,7 @@ final class Version
      * @var    string
      * @since  3.8.0
      */
-    public const EXTRA_VERSION = 'alpha3-dev';
+    public const EXTRA_VERSION = 'alpha3';
 
     /**
      * Development status.
@@ -74,7 +74,7 @@ final class Version
      * @var    string
      * @since  3.5
      */
-    public const DEV_STATUS = 'Development';
+    public const DEV_STATUS = 'Alpha';
 
     /**
      * Code name.
