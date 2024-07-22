f669b9fc062d1429458d22691fa415c6bd209813
Dimitris Grammatikogiannis <dg@dgrammatiko.dev>
2024-07-22 20:53:49 +0300

    Update actions.es6.js

--- administrator/components/com_media/resources/scripts/store/actions.es6.js
+++ administrator/components/com_media/resources/scripts/store/actions.es6.js
@@ -138,12 +138,6 @@ export const uploadFile = (context, payload) => {
     .then((file) => {
       context.commit(types.UPLOAD_SUCCESS, file);
       context.commit(types.SET_IS_LOADING, false);
-
-      // Automatically select the last uploaded item when media manager is inside an iframe
-      if (window.location !== window.parent.location) {
-        context.commit(types.UNSELECT_ALL_BROWSER_ITEMS);
-        context.commit(types.SELECT_BROWSER_ITEM, file);
-      }
     })
     .catch((error) => {
       context.commit(types.SET_IS_LOADING, false);
