1f53da6ce2c36f270f1c5d2b24fa33c96abe7a81
Martina  Scholz <64533137+LadySolveig@users.noreply.github.com>
2024-07-23 15:04:22 +0200

    Update layouts/joomla/mail/mailtemplate.php
    Co-authored-by: Brian Teeman <brian@teeman.net>

--- layouts/joomla/mail/mailtemplate.php
+++ layouts/joomla/mail/mailtemplate.php
@@ -2,7 +2,7 @@
 
 /**
  * @package     Joomla.Site
- * @subpackage  com_mails
+ * @subpackage  Layout
  *
  * @copyright   (C) 2024 Open Source Matters, Inc. <https://www.joomla.org>
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
