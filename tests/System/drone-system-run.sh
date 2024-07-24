a30e476bc20febb252883f595e3371a70447823f
Hannes Papenberg <info@joomlager.de>
2024-07-24 19:51:38 +0200

    Tests

--- tests/System/drone-system-run.sh
+++ tests/System/drone-system-run.sh
@@ -22,5 +22,5 @@ apache2ctl -D FOREGROUND &
 
 echo "[RUNNER] Run cypress tests"
 chmod +rwx /root
-
+cd /tests/www/$TEST_GROUP/
 npx cypress run --browser=firefox --e2e --env cmsPath=/tests/www/$TEST_GROUP,db_type=$DB_ENGINE,db_host=$DB_HOST,db_password=joomla_ut,db_prefix="${TEST_GROUP}_" --config baseUrl=http://localhost/$TEST_GROUP,screenshotsFolder=$JOOMLA_BASE/tests/System/output/screenshots
