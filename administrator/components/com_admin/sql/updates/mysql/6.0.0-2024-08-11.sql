52236fc1eb5278db6a2e45be14c3e075af30b877
chmst <dev@chmst.de>
2024-08-11 13:45:09 +0200

    sql cupdate space

--- administrator/components/com_admin/sql/updates/mysql/6.0.0-2024-08-11.sql
+++ administrator/components/com_admin/sql/updates/mysql/6.0.0-2024-08-11.sql
@@ -2,5 +2,5 @@
 
 UPDATE `#__menu`
 SET `link` = 'index.php?option=com_content&view=articles&featured=1'
- WHERE `link` = 'index.php?option=com_content&view=featured'
+WHERE `link` = 'index.php?option=com_content&view=featured'
 AND `client_id` = 1;
