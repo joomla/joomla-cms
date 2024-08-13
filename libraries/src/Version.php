51032f65eb10bb9a78be7c264b511a933740318e
Martin Kopp <kopp05@infotech.ch>
2024-08-13 12:36:45 +0200

    Revert to dev
    Signed-off-by: Martin Kopp <kopp05@infotech.ch>

--- libraries/src/Version.php
+++ libraries/src/Version.php
@@ -66,7 +66,7 @@ final class Version
      * @var    string
      * @since  3.8.0
      */
-    public const EXTRA_VERSION = 'rc1';
+    public const EXTRA_VERSION = 'rc2-dev';
 
     /**
      * Development status.
@@ -74,7 +74,7 @@ final class Version
      * @var    string
      * @since  3.5
      */
-    public const DEV_STATUS = 'Release Candidate';
+    public const DEV_STATUS = 'Development';
 
     /**
      * Code name.
@@ -90,7 +90,7 @@ final class Version
      * @var    string
      * @since  3.5
      */
-    public const RELDATE = '20-July-2024';
+    public const RELDATE = '13-August-2024';
 
     /**
      * Release time.
@@ -98,7 +98,7 @@ final class Version
      * @var    string
      * @since  3.5
      */
-    public const RELTIME = '16:00';
+    public const RELTIME = '13:01';
 
     /**
      * Release timezone.
