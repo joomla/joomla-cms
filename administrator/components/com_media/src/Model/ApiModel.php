6079aa70237eba6c2c1e9ffd08c248e13b7bc7f5
Matt Elkins <matt.elkins@bluefrontier.co.uk>
2024-08-14 08:52:30 +0100

    Reapplying relevant changes from PR #43640.

--- administrator/components/com_media/src/Model/ApiModel.php
+++ administrator/components/com_media/src/Model/ApiModel.php
@@ -451,8 +451,8 @@ class ApiModel extends BaseDatabaseModel
 
         // Initialize the allowed extensions
         if ($this->allowedExtensions === null) {
-            // Get options from the input or fallback to images only
-            $mediaTypes = explode(',', Factory::getApplication()->getInput()->getString('mediatypes', '0'));
+            // Get options from the input or fallback to all supported formats
+            $mediaTypes = explode(',', Factory::getApplication()->getInput()->getString('mediatypes', '0,1,2,3'));
             $types      = [];
             $extensions = [];
 
