26f11906a8568f80df8d6070b66741f61ab6444c
Allon Moritz <allon.moritz@digital-peak.com>
2024-08-14 15:32:24 +0200

    Initial

---
+++ build/phpstan/joomla-bootstrap.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * @package    Joomla.Build
+ *
+ * @copyright  (C) 2024 Open Source Matters, Inc. <https://www.joomla.org>
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ *
+ * @phpcs:disable PSR1.Classes.ClassDeclaration.MissingNamespace
+ */
+
+\define('_JEXEC', 1);
+\define('JPATH_PLATFORM', 1);
+\define('JPATH_BASE', \dirname(__DIR__, 2));
+
+
+// Load the Joomla class loader
+require_once JPATH_BASE . '/includes/defines.php';
+
+require_once JPATH_LIBRARIES . '/loader.php';
+JLoader::setup();
+require_once JPATH_LIBRARIES . '/vendor/autoload.php';
+
+// Rector crashes as it is inside a phar
+//require_once JPATH_BASE . '/libraries/bootstrap.php';
+
+// Load the extension namespaces
+JLoader::register('JNamespacePsr4Map', JPATH_LIBRARIES . '/namespacemap.php');
+$map = new JNamespacePsr4Map();
+$map->load();
---
+++ build/phpstan/src/DynamicReturnType/DIContainer.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * @package     Joomla.Build
+ * @subpackage  phpstan
+ *
+ * @copyright   (C) 2024 Open Source Matters, Inc. <https://www.joomla.org>
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+namespace Joomla\PHPStan\DynamicReturnType;
+
+use Joomla\DI\Container;
+use PhpParser\Node\Expr\MethodCall;
+use PHPStan\Analyser\Scope;
+use PHPStan\Reflection\MethodReflection;
+use PHPStan\Type\DynamicMethodReturnTypeExtension;
+use PHPStan\Type\ObjectType;
+use PHPStan\Type\Type;
+
+class DIContainer implements DynamicMethodReturnTypeExtension
+{
+    public function getClass(): string
+    {
+        return Container::class;
+    }
+
+    public function isMethodSupported(MethodReflection $methodReflection): bool
+    {
+        return $methodReflection->getName() === 'get';
+    }
+
+    public function getTypeFromMethodCall(MethodReflection $methodReflection, MethodCall $methodCall, Scope $scope): ?Type
+    {
+        if (\count($methodCall->getArgs()) === 0) {
+            return null;
+        }
+
+        $arg = $methodCall->getArgs()[0]->value;
+
+        return new ObjectType($scope->getType($arg)->getValue());
+    }
+}
