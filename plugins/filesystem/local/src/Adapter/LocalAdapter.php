af8d985aa1d428dadc96fad1b6a4e4140ad15162
Hannes Papenberg <info@joomlager.de>
2024-08-21 22:58:44 +0200

    Update LocalAdapter.php

--- plugins/filesystem/local/src/Adapter/LocalAdapter.php
+++ plugins/filesystem/local/src/Adapter/LocalAdapter.php
@@ -337,10 +337,14 @@ class LocalAdapter implements AdapterInterface
                 throw new FileNotFoundException();
             }
 
-            $success = Folder::delete($localPath);
+            try {
+                $success = Folder::delete($localPath);
 
-            if ($this->thumbnails && !empty($thumbnailPaths['fs']) && is_dir($thumbnailPaths['fs'])) {
-                Folder::delete($thumbnailPaths['fs']);
+                if ($this->thumbnails && !empty($thumbnailPaths['fs']) && is_dir($thumbnailPaths['fs'])) {
+                    Folder::delete($thumbnailPaths['fs']);
+                }
+            } catch (FilesystemException $exception) {
+                throw new \Exception('Delete not possible!');
             }
         }
 
