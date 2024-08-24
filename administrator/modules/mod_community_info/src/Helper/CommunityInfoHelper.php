3d4f244f1bfa598085bf0f036d0ef8095ac1bd82
Elfangor <tech.spuur@quickline.ch>
2024-08-24 10:38:06 +0200

    move default info to helper constant

--- administrator/modules/mod_community_info/src/Helper/CommunityInfoHelper.php
+++ administrator/modules/mod_community_info/src/Helper/CommunityInfoHelper.php
@@ -49,6 +49,26 @@ class CommunityInfoHelper
     protected static $moduleId = null;
 
     /**
+     * Fallback community info
+     * 
+     * @var array
+     */
+    const DEFAULT_INFO = [
+        "name"        => "joomla.org",
+        "type"        => "default",
+        "level"       => "0",
+        "jug"         => "https://community.joomla.org/user-groups",
+        "forum"       => "https://forum.joomla.org",
+        "jday"        => "https://community.joomla.org/events?filter[calendars][1]=97",
+        "messanger"   => "https://joomlacommunity.cloud.mattermost.com",
+        "vportal"     => "https://volunteers.joomla.org",
+        "geolocation" => "51.5000,0.0000",
+        "news_feed"   => "https://community.joomla.org/blogs?format=feed&type=rss",
+        "events_feed" => "https://djumla.dev/joomla-community-api/events.php?url=https://community.joomla.org/events\?format=feed&type=ical",
+        "newsletter"  => "https://community.joomla.org/general-newsletter",
+    ];
+
+    /**
      * Initialize the helper variables
      *
      * @param   integer    $id      Id of the current module
@@ -78,8 +98,7 @@ class CommunityInfoHelper
         self::setParams($params);
 
         // Load the local default values
-        require_once JPATH_ADMINISTRATOR . '/modules/mod_community_info/includes/default_links.php';
-        $links = new Registry($defaultLinksArray);
+        $links = new Registry(self::DEFAULT_INFO);
 
         // Load links from endpoint
         $vars = ['location' => self::getLocation($params, 'geolocation')];
