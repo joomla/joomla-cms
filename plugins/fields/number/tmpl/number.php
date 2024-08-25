3ec13f1143dae191bca2f04fa3236a23ac34c86d
Tom van der Laan <info@tlwebdesign.nl>
2024-08-25 20:39:10 +0200

    Apply suggestions from code review
    Co-authored-by: Brian Teeman <brian@teeman.net>

--- plugins/fields/number/tmpl/number.php
+++ plugins/fields/number/tmpl/number.php
@@ -4,7 +4,7 @@
  * @package     Joomla.Plugin
  * @subpackage  Fields.Number
  *
- * @copyright   (C) 2016 Open Source Matters, Inc. <https://www.joomla.org>
+ * @copyright   (C) 2024 Open Source Matters, Inc. <https://www.joomla.org>
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
