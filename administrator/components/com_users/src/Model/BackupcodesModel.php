63ba5aa651d6f5719b3498efafb68661d1438b2d
Denitz <197527+Denitz@users.noreply.github.com>
2024-09-12 13:23:22 +0300

    [5.2] foreach usage (#41982)

--- administrator/components/com_users/src/Model/BackupcodesModel.php
+++ administrator/components/com_users/src/Model/BackupcodesModel.php
@@ -230,17 +230,17 @@ class BackupcodesModel extends BaseDatabaseModel
         $realLength = \count($codes);
         $restLength = 10 - $realLength;
 
-        for ($i = 0; $i < $realLength; $i++) {
-            if (hash_equals($codes[$i], $code)) {
+        foreach ($codes as $value) {
+            if (hash_equals($value, $code)) {
                 // This may seem redundant but makes sure both branches of the if-block are isochronous
                 $result       = $result || true;
                 $newArray[]   = '';
-                $dummyArray[] = $codes[$i];
+                $dummyArray[] = $value;
             } else {
                 // This may seem redundant but makes sure both branches of the if-block are isochronous
                 $result       = $result || false;
                 $dummyArray[] = '';
-                $newArray[]   = $codes[$i];
+                $newArray[]   = $value;
             }
         }
 
