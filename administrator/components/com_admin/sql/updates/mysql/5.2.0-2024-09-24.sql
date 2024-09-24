f3beec219c056407bf5af2a892ea8fe067ec415a
Konstantin Kolos <k0nstantinkolos88@gmail.com>
2024-09-24 20:39:30 +0300

    Files for translation strings in #__assets

---
+++ administrator/components/com_admin/sql/updates/mysql/5.2.0-2024-09-24.sql
@@ -0,0 +1,7 @@
+UPDATE `#__assets` SET `title` = 'PUBLISH' WHERE `title`= 'Publish';
+UPDATE `#__assets` SET `title` = 'UNPUBLISH' WHERE `title`= 'Unpublish';
+UPDATE `#__assets` SET `title` = 'TRASH' WHERE `title`= 'Trash';
+UPDATE `#__assets` SET `title` = 'ARCHIVE' WHERE `title`= 'Archive';
+UPDATE `#__assets` SET `title` = 'FEATURE' WHERE `title`= 'Feature';
+UPDATE `#__assets` SET `title` = 'UNFEATURE' WHERE `title`= 'Unfeature';
+UPDATE `#__assets` SET `title` = 'PUBLISH_AND_FEATURE' WHERE `title`= 'Publish & Feature';
