e86efc0e8175525bf8a9087aedcf167d89a1bf4b
Rick R2H <rick@r2h.nl>
2024-09-25 16:17:59 +0200

    Change context

--- modules/mod_articles/src/Helper/ArticlesHelper.php
+++ modules/mod_articles/src/Helper/ArticlesHelper.php
@@ -250,7 +250,7 @@ class ArticlesHelper implements DatabaseAwareInterface
                 PluginHelper::importPlugin('content', null, true, $dispatcher);
 
                 $contentEventArguments = [
-                    'context' => 'com_content.article',
+                    'context' => 'mod_articles.article',
                     'subject' => $item,
                     'params'  => $item->params,
                 ];
