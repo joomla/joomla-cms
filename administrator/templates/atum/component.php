ec3b04ae11b2c6b64d3e23482ea8272af0a4fa6d
Rick Spaan <rick@r2h.nl>
2024-10-02 01:15:27 +0200

    Remove drone comments

--- administrator/templates/atum/component.php
+++ administrator/templates/atum/component.php
@@ -8,9 +8,7 @@
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
-// phpcs:disable PSR1.Files.SideEffects
 defined('_JEXEC') or die;
-// phpcs:enable PSR1.Files.SideEffects
 
 use Joomla\CMS\Factory;
 use Joomla\CMS\HTML\HTMLHelper;
