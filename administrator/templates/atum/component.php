822e8f8d31c87b871acc3f2fc5d7d0e2f640a6e7
Rick R2H <rick@r2h.nl>
2024-10-01 16:35:19 +0200

    Added phpcs:disable Side Effects to prevent drone warning

--- administrator/templates/atum/component.php
+++ administrator/templates/atum/component.php
@@ -8,7 +8,9 @@
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
+// phpcs:disable PSR1.Files.SideEffects
 defined('_JEXEC') or die;
+// phpcs:enable PSR1.Files.SideEffects
 
 use Joomla\CMS\Factory;
 use Joomla\CMS\HTML\HTMLHelper;
