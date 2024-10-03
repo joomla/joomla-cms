28a1ebc40c6e85fd48a3a40dc0975d19ca3425ba
Richard Fath <richard.fath@t-online.de>
2024-10-03 12:21:01 +0200

    Delete 5.2.0 update SQL scripts

--- administrator/components/com_admin/sql/updates/postgresql/5.2.0-2024-07-02.sql
+++
@@ -1,3 +0,0 @@
--- Add mod_articles module
-INSERT INTO "#__extensions" ("package_id", "name", "type", "element", "folder", "client_id", "enabled", "access", "protected", "locked", "manifest_cache", "params", "custom_data", "ordering", "state") VALUES
-(0, 'mod_articles', 'module', 'mod_articles', '', 0, 1, 0, 0, 1, '', '', '', 0, 0);
