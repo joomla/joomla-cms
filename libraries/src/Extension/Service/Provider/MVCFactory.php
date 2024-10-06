joomla/joomla-cms

commit e2170c8a6224ebb6ed754853de0d5e1b97ba1ce9

Replace JPATH_PLATFORM with _JEXEC (#40343)
* Replace JPATH_PLATFORM with _JEXEC

* tests

* libraries

libraries/src/Extension/Service/Provider/MVCFactory.php
@@ -21,8 +21,8 @@ use Joomla\DI\ServiceProviderInterface;
 use Joomla\Event\DispatcherInterface;
 
 // phpcs:disable PSR1.Files.SideEffects
-\defined('JPATH_PLATFORM') or die;
-// phpcs:enable PSR1.Files.SideEffects
+\defined('_JEXEC') or die;
+// phpcs:e_JEXECes.SideEffects
 
 /**
  * Service provider for the service MVC factory.

libraries/src/MVC/Factory/MVCFactory.php
@@ -29,7 +29,7 @@ use Joomla\Event\DispatcherAwareTrait;
 use Joomla\Input\Input;
 
 // phpcs:disable PSR1.Files.SideEffects
-\defined('JPATH_PLATFORM') or die;
+\defined('_JEXEC') or die;
 // phpcs:enable PSR1.Files.SideEffects
 
 /**

commit 0c6ea05587b3ac4a058dbcb48c69f4693ae7fd55

fix phpcs enable rule
caused by #40343

libraries/src/Extension/Service/Provider/MVCFactory.php
@@ -23,7 +23,7 @@ use Joomla\Event\DispatcherInterface;
 
 // phpcs:disable PSR1.Files.SideEffects
 \defined('_JEXEC') or die;
-// phpcs:e_JEXECes.SideEffects
+// phpcs:enable PSR1.Files.SideEffects
 
 /**
  * Service provider for the service MVC factory.