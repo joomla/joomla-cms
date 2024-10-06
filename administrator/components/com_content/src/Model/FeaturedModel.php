joomla/joomla-cms

commit 5b21db67ed8fdf7c181ea5258e995b3cc9d3e757

[5.2] Fixing typehint for getListQuery to QueryInterface (#43265)
* Fixing typehint for getListQuery to QueryInterface

* Missed 2 things

* Updating docblock comments

* docblock fixes

administrator/components/com_content/src/Model/FeaturedModel.php
@@ -11,6 +11,8 @@
 namespace Joomla\Component\Content\Administrator\Model;
 
 // phpcs:disable PSR1.Files.SideEffects
+use Joomla\Database\QueryInterface;
+
 \defined('_JEXEC') or die;
 // phpcs:enable PSR1.Files.SideEffects
 
@@ -88,7 +90,7 @@ class FeaturedModel extends ArticlesModel
     /**
      * Build an SQL query to load the list data.
      *
-     * @return  \Joomla\Database\DatabaseQuery
+     * @return  QueryInterface
      *
      * @since   4.0.0
      */

components/com_contact/src/Model/FeaturedModel.php
@@ -14,8 +14,8 @@ use Joomla\CMS\Component\ComponentHelper;
 use Joomla\CMS\Factory;
 use Joomla\CMS\Language\Multilanguage;
 use Joomla\CMS\MVC\Model\ListModel;
-use Joomla\Database\DatabaseQuery;
 use Joomla\Database\ParameterType;
+use Joomla\Database\QueryInterface;
 use Joomla\Registry\Registry;
 
 // phpcs:disable PSR1.Files.SideEffects
@@ -78,7 +78,7 @@ class FeaturedModel extends ListModel
     /**
      * Method to build an SQL query to load the list data.
      *
-     * @return  DatabaseQuery    An SQL query
+     * @return  QueryInterface    An SQL query
      *
      * @since   1.6
      */

components/com_content/src/Model/FeaturedModel.php
@@ -13,6 +13,7 @@ namespace Joomla\Component\Content\Site\Model;
 use Joomla\CMS\Factory;
 use Joomla\Component\Content\Administrator\Extension\ContentComponent;
 use Joomla\Component\Content\Site\Helper\QueryHelper;
+use Joomla\Database\QueryInterface;
 use Joomla\Registry\Registry;
 use Joomla\Utilities\ArrayHelper;
 
@@ -149,7 +150,7 @@ class FeaturedModel extends ArticlesModel
     /**
      * Get the list of items.
      *
-     * @return  \Joomla\Database\DatabaseQuery
+     * @return  QueryInterface
      */
     protected function getListQuery()
     {

commit b81304570ad692a8fea12ac5fad0d473c32f3f69

move use statement out of phpcs disable
caused by #43265

administrator/components/com_content/src/Model/FeaturedModel.php
@@ -10,9 +10,9 @@
 
 namespace Joomla\Component\Content\Administrator\Model;
 
-// phpcs:disable PSR1.Files.SideEffects
 use Joomla\Database\QueryInterface;
 
+// phpcs:disable PSR1.Files.SideEffects
 \defined('_JEXEC') or die;
 // phpcs:enable PSR1.Files.SideEffects
 