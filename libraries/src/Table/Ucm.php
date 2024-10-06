joomla/joomla-cms

commit f19e3f3e0a28707535c72b09c3341e03b04bcb56

[5.0] Improve constructors of JTable objects (#40129)
* Improve constructors of JTable objects

* Move static to self

---------

Co-authored-by: Martin Carl Kopp <6154099+MacJoom@users.noreply.github.com>
Co-authored-by: Allon Moritz <allon.moritz@digital-peak.com>
Co-authored-by: Richard Fath <richard67@users.noreply.github.com>

libraries/src/Table/Ucm.php
@@ -10,6 +10,9 @@
 namespace Joomla\CMS\Table;
 
 // phpcs:disable PSR1.Files.SideEffects
+use Joomla\Database\DatabaseDriver;
+use Joomla\Event\DispatcherInterface;
+
 \defined('_JEXEC') or die;
 // phpcs:enable PSR1.Files.SideEffects
 
@@ -23,12 +26,13 @@ class Ucm extends Table
     /**
      * Constructor
      *
-     * @param   \Joomla\Database\DatabaseDriver  $db  A database connector object
+     * @param   DatabaseDriver        $db          Database connector object
+     * @param   ?DispatcherInterface  $dispatcher  Event dispatcher for this table
      *
      * @since   3.1
      */
-    public function __construct($db)
+    public function __construct(DatabaseDriver $db, DispatcherInterface $dispatcher = null)
     {
-        parent::__construct('#__ucm_base', 'ucm_id', $db);
+        parent::__construct('#__ucm_base', 'ucm_id', $db, $dispatcher);
     }
 }

administrator/components/com_banners/src/Table/BannerTable.php
@@ -18,6 +18,7 @@ use Joomla\CMS\Table\Table;
 use Joomla\CMS\Versioning\VersionableTableInterface;
 use Joomla\Database\DatabaseDriver;
 use Joomla\Database\ParameterType;
+use Joomla\Event\DispatcherInterface;
 use Joomla\Registry\Registry;
 use Joomla\Utilities\ArrayHelper;
 
@@ -43,15 +44,16 @@ class BannerTable extends Table implements VersionableTableInterface
     /**
      * Constructor
      *
-     * @param   DatabaseDriver  $db  Database connector object
+     * @param   DatabaseDriver        $db          Database connector object
+     * @param   ?DispatcherInterface  $dispatcher  Event dispatcher for this table
      *
      * @since   1.5
      */
-    public function __construct(DatabaseDriver $db)
+    public function __construct(DatabaseDriver $db, DispatcherInterface $dispatcher = null)
     {
         $this->typeAlias = 'com_banners.banner';
 
-        parent::__construct('#__banners', 'id', $db);
+        parent::__construct('#__banners', 'id', $db, $dispatcher);
 
         $this->created = Factory::getDate()->toSql();
         $this->setColumnAlias('published', 'state');
@@ -252,16 +254,14 @@ class BannerTable extends Table implements VersionableTableInterface
             parent::store($updateNulls);
         } else {
             // Get the old row
-            /** @var BannerTable $oldrow */
-            $oldrow = Table::getInstance('BannerTable', __NAMESPACE__ . '\\', ['dbo' => $db]);
+            $oldrow = new self($db, $this->getDispatcher());
 
             if (!$oldrow->load($this->id) && $oldrow->getError()) {
                 $this->setError($oldrow->getError());
             }
 
             // Verify that the alias is unique
-            /** @var BannerTable $table */
-            $table = Table::getInstance('BannerTable', __NAMESPACE__ . '\\', ['dbo' => $db]);
+            $table = new self($db, $this->getDispatcher());
 
             if ($table->load(['alias' => $this->alias, 'catid' => $this->catid]) && ($table->id != $this->id || $this->id == 0)) {
                 $this->setError(Text::_('COM_BANNERS_ERROR_UNIQUE_ALIAS'));
@@ -317,8 +317,7 @@ class BannerTable extends Table implements VersionableTableInterface
         }
 
         // Get an instance of the table
-        /** @var BannerTable $table */
-        $table = Table::getInstance('BannerTable', __NAMESPACE__ . '\\', ['dbo' => $this->_db]);
+        $table = new self($this->getDbo(), $this->getDispatcher());
 
         // For all keys
         foreach ($pks as $pk) {

administrator/components/com_banners/src/Table/ClientTable.php
@@ -14,6 +14,7 @@ use Joomla\CMS\Language\Text;
 use Joomla\CMS\Table\Table;
 use Joomla\CMS\Versioning\VersionableTableInterface;
 use Joomla\Database\DatabaseDriver;
+use Joomla\Event\DispatcherInterface;
 
 // phpcs:disable PSR1.Files.SideEffects
 \defined('_JEXEC') or die;
@@ -37,17 +38,18 @@ class ClientTable extends Table implements VersionableTableInterface
     /**
      * Constructor
      *
-     * @param   DatabaseDriver  $db  Database connector object
+     * @param   DatabaseDriver        $db          Database connector object
+     * @param   ?DispatcherInterface  $dispatcher  Event dispatcher for this table
      *
      * @since   1.5
      */
-    public function __construct(DatabaseDriver $db)
+    public function __construct(DatabaseDriver $db, DispatcherInterface $dispatcher = null)
     {
         $this->typeAlias = 'com_banners.client';
 
         $this->setColumnAlias('published', 'state');
 
-        parent::__construct('#__banner_clients', 'id', $db);
+        parent::__construct('#__banner_clients', 'id', $db, $dispatcher);
     }
 
     /**

administrator/components/com_contact/src/Table/ContactTable.php
@@ -22,6 +22,7 @@ use Joomla\CMS\User\CurrentUserInterface;
 use Joomla\CMS\User\CurrentUserTrait;
 use Joomla\CMS\Versioning\VersionableTableInterface;
 use Joomla\Database\DatabaseDriver;
+use Joomla\Event\DispatcherInterface;
 use Joomla\String\StringHelper;
 
 // phpcs:disable PSR1.Files.SideEffects
@@ -57,15 +58,16 @@ class ContactTable extends Table implements VersionableTableInterface, TaggableT
     /**
      * Constructor
      *
-     * @param   DatabaseDriver  $db  Database connector object
+     * @param   DatabaseDriver        $db          Database connector object
+     * @param   ?DispatcherInterface  $dispatcher  Event dispatcher for this table
      *
      * @since   1.0
      */
-    public function __construct(DatabaseDriver $db)
+    public function __construct(DatabaseDriver $db, DispatcherInterface $dispatcher = null)
     {
         $this->typeAlias = 'com_contact.contact';
 
-        parent::__construct('#__contact_details', 'id', $db);
+        parent::__construct('#__contact_details', 'id', $db, $dispatcher);
 
         $this->setColumnAlias('title', 'name');
     }
@@ -119,7 +121,7 @@ class ContactTable extends Table implements VersionableTableInterface, TaggableT
         }
 
         // Verify that the alias is unique
-        $table = Table::getInstance('ContactTable', __NAMESPACE__ . '\\', ['dbo' => $this->getDbo()]);
+        $table = new self($this->getDbo(), $this->getDispatcher());
 
         if ($table->load(['alias' => $this->alias, 'catid' => $this->catid]) && ($table->id != $this->id || $this->id == 0)) {
             // Is the existing contact trashed?
@@ -255,7 +257,7 @@ class ContactTable extends Table implements VersionableTableInterface, TaggableT
 
 
     /**
-     * Get the type alias for the history table
+     * Get the type alias for the history and tags mapping table
      *
      * @return  string  The alias as described above
      *

administrator/components/com_content/src/Table/FeaturedTable.php
@@ -12,6 +12,7 @@ namespace Joomla\Component\Content\Administrator\Table;
 
 use Joomla\CMS\Table\Table;
 use Joomla\Database\DatabaseDriver;
+use Joomla\Event\DispatcherInterface;
 
 // phpcs:disable PSR1.Files.SideEffects
 \defined('_JEXEC') or die;
@@ -27,12 +28,13 @@ class FeaturedTable extends Table
     /**
      * Constructor
      *
-     * @param   DatabaseDriver  $db  Database connector object
+     * @param   DatabaseDriver        $db          Database connector object
+     * @param   ?DispatcherInterface  $dispatcher  Event dispatcher for this table
      *
      * @since   1.6
      */
-    public function __construct(DatabaseDriver $db)
+    public function __construct(DatabaseDriver $db, DispatcherInterface $dispatcher = null)
     {
-        parent::__construct('#__content_frontpage', 'content_id', $db);
+        parent::__construct('#__content_frontpage', 'content_id', $db, $dispatcher);
     }
 }

administrator/components/com_fields/src/Table/FieldTable.php
@@ -18,6 +18,7 @@ use Joomla\CMS\Table\Table;
 use Joomla\CMS\User\CurrentUserInterface;
 use Joomla\CMS\User\CurrentUserTrait;
 use Joomla\Database\DatabaseDriver;
+use Joomla\Event\DispatcherInterface;
 use Joomla\Registry\Registry;
 use Joomla\String\StringHelper;
 
@@ -45,13 +46,14 @@ class FieldTable extends Table implements CurrentUserInterface
     /**
      * Class constructor.
      *
-     * @param   DatabaseDriver  $db  DatabaseDriver object.
+     * @param   DatabaseDriver        $db          Database connector object
+     * @param   ?DispatcherInterface  $dispatcher  Event dispatcher for this table
      *
      * @since   3.7.0
      */
-    public function __construct($db = null)
+    public function __construct(DatabaseDriver $db, DispatcherInterface $dispatcher = null)
     {
-        parent::__construct('#__fields', 'id', $db);
+        parent::__construct('#__fields', 'id', $db, $dispatcher);
 
         $this->setColumnAlias('published', 'state');
     }
@@ -150,7 +152,7 @@ class FieldTable extends Table implements CurrentUserInterface
         $this->name = str_replace(',', '-', $this->name);
 
         // Verify that the name is unique
-        $table = new static($this->_db);
+        $table = new self($this->_db, $this->getDispatcher());
 
         if ($table->load(['name' => $this->name]) && ($table->id != $this->id || $this->id == 0)) {
             $this->setError(Text::_('COM_FIELDS_ERROR_UNIQUE_NAME'));

administrator/components/com_fields/src/Table/GroupTable.php
@@ -17,6 +17,7 @@ use Joomla\CMS\Table\Table;
 use Joomla\CMS\User\CurrentUserInterface;
 use Joomla\CMS\User\CurrentUserTrait;
 use Joomla\Database\DatabaseDriver;
+use Joomla\Event\DispatcherInterface;
 use Joomla\Registry\Registry;
 
 // phpcs:disable PSR1.Files.SideEffects
@@ -43,13 +44,14 @@ class GroupTable extends Table implements CurrentUserInterface
     /**
      * Class constructor.
      *
-     * @param   DatabaseDriver  $db  DatabaseDriver object.
+     * @param   DatabaseDriver        $db          Database connector object
+     * @param   ?DispatcherInterface  $dispatcher  Event dispatcher for this table
      *
      * @since   3.7.0
      */
-    public function __construct($db = null)
+    public function __construct(DatabaseDriver $db, DispatcherInterface $dispatcher = null)
     {
-        parent::__construct('#__fields_groups', 'id', $db);
+        parent::__construct('#__fields_groups', 'id', $db, $dispatcher);
 
         $this->setColumnAlias('published', 'state');
     }

administrator/components/com_finder/src/Table/FilterTable.php
@@ -17,6 +17,7 @@ use Joomla\CMS\Table\Table;
 use Joomla\CMS\User\CurrentUserInterface;
 use Joomla\CMS\User\CurrentUserTrait;
 use Joomla\Database\DatabaseDriver;
+use Joomla\Event\DispatcherInterface;
 use Joomla\Registry\Registry;
 
 // phpcs:disable PSR1.Files.SideEffects
@@ -51,13 +52,14 @@ class FilterTable extends Table implements CurrentUserInterface
     /**
      * Constructor
      *
-     * @param   DatabaseDriver  $db  Database Driver connector object.
+     * @param   DatabaseDriver        $db          Database connector object
+     * @param   ?DispatcherInterface  $dispatcher  Event dispatcher for this table
      *
      * @since   2.5
      */
-    public function __construct(DatabaseDriver $db)
+    public function __construct(DatabaseDriver $db, DispatcherInterface $dispatcher = null)
     {
-        parent::__construct('#__finder_filters', 'filter_id', $db);
+        parent::__construct('#__finder_filters', 'filter_id', $db, $dispatcher);
 
         $this->setColumnAlias('published', 'state');
     }
@@ -158,7 +160,7 @@ class FilterTable extends Table implements CurrentUserInterface
         }
 
         // Verify that the alias is unique
-        $table = new static($this->getDbo());
+        $table = new self($this->getDbo(), $this->getDispatcher());
 
         if ($table->load(['alias' => $this->alias]) && ($table->filter_id != $this->filter_id || $this->filter_id == 0)) {
             $this->setError(Text::_('COM_FINDER_FILTER_ERROR_UNIQUE_ALIAS'));

commit 7b3039fdc37d1223a6a726e199553986e8d10672

move use statement out of phpcs disable
caused by #40129

libraries/src/Table/Ucm.php
@@ -9,10 +9,10 @@
 
 namespace Joomla\CMS\Table;
 
-// phpcs:disable PSR1.Files.SideEffects
 use Joomla\Database\DatabaseDriver;
 use Joomla\Event\DispatcherInterface;
 
+// phpcs:disable PSR1.Files.SideEffects
 \defined('_JEXEC') or die;
 // phpcs:enable PSR1.Files.SideEffects
 