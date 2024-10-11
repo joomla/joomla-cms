f11a9263403d881561c852536d818bc4407e54ac
coolcat-creations <mail@coolcat-creations.com>
2024-10-11 08:27:31 +0200

    Only show "OR" if extra login available

--- administrator/modules/mod_login/tmpl/default.php
+++ administrator/modules/mod_login/tmpl/default.php
@@ -99,38 +99,41 @@ Text::script('JHIDEPASSWORD');
             </div>
         </div>
 
-        <hr>
         <div class="mt-4">
-            <legend class="text-center"><?php echo Text::_('MOD_LOGIN_OR'); ?></legend>
+            <?php if ($extraButtons) : ?>
+                <hr>
 
-            <?php foreach ($extraButtons as $button) :
-                $dataAttributeKeys = array_filter(array_keys($button), function ($key) {
-                    return substr($key, 0, 5) == 'data-';
-                });
-                ?>
-                <div class="form-group">
-                    <button type="button"
-                            class="btn btn-secondary w-100 <?php echo $button['class'] ?? '' ?>"
-                    <?php foreach ($dataAttributeKeys as $key) : ?>
-                        <?php echo $key ?>="<?php echo $button[$key] ?>"
-                    <?php endforeach; ?>
-                    <?php if ($button['onclick']) : ?>
-                        onclick="<?php echo $button['onclick'] ?>"
-                    <?php endif; ?>
-                    title="<?php echo Text::_($button['label']) ?>"
-                    id="<?php echo $button['id'] ?>"
-                    >
-                    <?php if (!empty($button['icon'])) : ?>
-                        <span class="<?php echo $button['icon'] ?>"></span>
-                    <?php elseif (!empty($button['image'])) : ?>
-                        <?php echo $button['image']; ?>
-                    <?php elseif (!empty($button['svg'])) : ?>
-                        <?php echo $button['svg']; ?>
-                    <?php endif; ?>
-                    <?php echo Text::_($button['label']) ?>
-                    </button>
-                </div>
-            <?php endforeach; ?>
+                <legend class="text-center"><?php echo Text::_('MOD_LOGIN_OR'); ?></legend>
+
+                <?php foreach ($extraButtons as $button) :
+                    $dataAttributeKeys = array_filter(array_keys($button), function ($key) {
+                        return substr($key, 0, 5) == 'data-';
+                    });
+                    ?>
+                    <div class="form-group">
+                        <button type="button"
+                                class="btn btn-secondary w-100 <?php echo $button['class'] ?? '' ?>"
+                        <?php foreach ($dataAttributeKeys as $key) : ?>
+                            <?php echo $key ?>="<?php echo $button[$key] ?>"
+                        <?php endforeach; ?>
+                        <?php if ($button['onclick']) : ?>
+                            onclick="<?php echo $button['onclick'] ?>"
+                        <?php endif; ?>
+                        title="<?php echo Text::_($button['label']) ?>"
+                        id="<?php echo $button['id'] ?>"
+                        >
+                        <?php if (!empty($button['icon'])) : ?>
+                            <span class="<?php echo $button['icon'] ?>"></span>
+                        <?php elseif (!empty($button['image'])) : ?>
+                            <?php echo $button['image']; ?>
+                        <?php elseif (!empty($button['svg'])) : ?>
+                            <?php echo $button['svg']; ?>
+                        <?php endif; ?>
+                        <?php echo Text::_($button['label']) ?>
+                        </button>
+                    </div>
+                <?php endforeach; ?>
+            <?php endif; ?>
 
             <input type="hidden" name="option" value="com_login">
             <input type="hidden" name="task" value="login">
