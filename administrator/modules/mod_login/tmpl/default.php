52e7bff4a3131fb26e10528287649f18586dbcb8
Elisa Foltyn <mail@coolcat-creations.com>
2024-10-11 10:04:13 +0200

    Update administrator/modules/mod_login/tmpl/default.php
    Co-authored-by: Richard Fath <richard67@users.noreply.github.com>

--- administrator/modules/mod_login/tmpl/default.php
+++ administrator/modules/mod_login/tmpl/default.php
@@ -5,7 +5,7 @@
  * @subpackage  mod_login
  *
  * @copyright   (C) 2010 Open Source Matters, Inc. <https://www.joomla.org>
- * @license         GNU General Public License version 2 or later; see LICENSE.txt
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 defined('_JEXEC') or die;
