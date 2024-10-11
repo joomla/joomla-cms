73c640a6f8164254405d2aac0b38c34dff3a0666
Elisa Foltyn <mail@coolcat-creations.com>
2024-10-11 10:04:07 +0200

    Update administrator/modules/mod_login/tmpl/default.php
    Co-authored-by: Richard Fath <richard67@users.noreply.github.com>

--- administrator/modules/mod_login/tmpl/default.php
+++ administrator/modules/mod_login/tmpl/default.php
@@ -1,8 +1,8 @@
 <?php
 
 /**
- * @package         Joomla.Administrator
- * @subpackage      mod_login
+ * @package     Joomla.Administrator
+ * @subpackage  mod_login
  *
  * @copyright   (C) 2010 Open Source Matters, Inc. <https://www.joomla.org>
  * @license         GNU General Public License version 2 or later; see LICENSE.txt
