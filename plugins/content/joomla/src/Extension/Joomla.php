3c2c16af22aec090883015090b7484dfcc2dc37b
alikon <optimus4joomla@gmail.com>
2024-10-13 12:16:21 +0200

    newarticle2mailtemplate

--- plugins/content/joomla/src/Extension/Joomla.php
+++ plugins/content/joomla/src/Extension/Joomla.php
@@ -15,10 +15,15 @@ use Joomla\CMS\Component\ComponentHelper;
 use Joomla\CMS\Event\Plugin\System\Schemaorg\BeforeCompileHeadEvent;
 use Joomla\CMS\Factory;
 use Joomla\CMS\HTML\HTMLHelper;
-use Joomla\CMS\Language\Language;
+use Joomla\CMS\Language\LanguageFactoryInterface;
 use Joomla\CMS\Language\Text;
+use Joomla\CMS\Log\Log;
+use Joomla\CMS\Mail\Exception\MailDisabledException;
+use Joomla\CMS\Mail\MailTemplate;
 use Joomla\CMS\Plugin\CMSPlugin;
+use Joomla\CMS\Router\Route;
 use Joomla\CMS\Table\CoreContent;
+use Joomla\CMS\String\PunycodeHelper;
 use Joomla\CMS\Uri\Uri;
 use Joomla\CMS\User\UserFactoryAwareTrait;
 use Joomla\CMS\Workflow\WorkflowServiceInterface;
@@ -28,6 +33,7 @@ use Joomla\Database\DatabaseAwareTrait;
 use Joomla\Database\ParameterType;
 use Joomla\Registry\Registry;
 use Joomla\Utilities\ArrayHelper;
+use PHPMailer\PHPMailer\Exception as phpMailerException;
 
 // phpcs:disable PSR1.Files.SideEffects
 \defined('_JEXEC') or die;
@@ -129,26 +135,35 @@ final class Joomla extends CMSPlugin
         }
 
         $user = $this->getApplication()->getIdentity();
+        $this->loadLanguage();
 
         // Messaging for new items
-
-        $default_language = ComponentHelper::getParams('com_languages')->get('administrator');
-        $debug            = $this->getApplication()->get('debug_lang');
-
         foreach ($users as $user_id) {
             if ($user_id != $user->id) {
-                // Load language for messaging
-                $receiver = $this->getUserFactory()->loadUserById($user_id);
-                $lang     = Language::getInstance($receiver->getParam('admin_language', $default_language), $debug);
-                $lang->load('com_content');
-                $message = [
-                    'user_id_to' => $user_id,
-                    'subject'    => $lang->_('COM_CONTENT_NEW_ARTICLE'),
-                    'message'    => \sprintf($lang->_('COM_CONTENT_ON_NEW_CONTENT'), $user->name, $article->title),
+                $receiver     = $this->getUserFactory()->loadUserById($user_id);
+                $linkMode     = $this->getApplication()->get('force_ssl', 0) == 2 ? Route::TLS_FORCE : Route::TLS_IGNORE;
+                $templateData = [
+                    'sitename' => $this->getApplication()->get('sitename'),
+                    'name'     => $user->name,
+                    'email'    => PunycodeHelper::emailToPunycode($user->email),
+                    'title'    => $article->title,
+                    'url'      => Route::link('administrator', 'index.php?option=com_content&view=articles&filter[search]=id:' . $article->id, false, $linkMode, true),
                 ];
-                $model_message = $this->getApplication()->bootComponent('com_messages')->getMVCFactory()
-                    ->createModel('Message', 'Administrator');
-                $model_message->save($message);
+
+                // Send email
+                try {
+                    $mailer = new MailTemplate('plg_content_joomla.newarticle', $receiver->getParam('admin_language', $this->getLanguage()->getTag()));
+                    $mailer->addTemplateData($templateData);
+                    $mailer->addRecipient($receiver->email, $receiver->name);
+
+                    $mailer->send();
+                } catch (MailDisabledException | phpMailerException $exception) {
+                    try {
+                        Log::add(Text::_($exception->getMessage()), Log::WARNING, 'jerror');
+                    } catch (\RuntimeException $exception) {
+                        $this->getApplication()->enqueueMessage(Text::_($exception->errorMessage()), 'warning');
+                    }
+                }
             }
         }
     }
@@ -180,8 +195,6 @@ final class Joomla extends CMSPlugin
             case 'com_workflow.stage':
                 return $this->stageNotUsed($data->id);
         }
-
-        return true;
     }
 
     /**
