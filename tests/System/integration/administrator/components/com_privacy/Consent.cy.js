c6d211ea2b37d0346475ac2f4f7dc93bddd81d9c
Robert Deutz <rdeutz@googlemail.com>
2024-10-18 11:26:27 +0200

    Update tests/System/integration/administrator/components/com_privacy/Consent.cy.js
    Co-authored-by: Richard Fath <richard67@users.noreply.github.com>

--- tests/System/integration/administrator/components/com_privacy/Consent.cy.js
+++ tests/System/integration/administrator/components/com_privacy/Consent.cy.js
@@ -1,7 +1,7 @@
 describe('Test in backend that the privacy consent component', () => {
   beforeEach(() => cy.doAdministratorLogin());
   afterEach(() => {
-    cy.task('queryDB', 'DELETE FROM  #__privacy_consents');
+    cy.task('queryDB', 'DELETE FROM #__privacy_consents');
     cy.task('queryDB', "DELETE FROM #__users WHERE name LIKE '%test user%'").then(() => {
       cy.task('queryDB', 'DELETE FROM #__user_usergroup_map WHERE user_id NOT IN (SELECT id FROM #__users)');
     });
