5f706308d4a70d5135876244385005afeaf07985
chmst <dev@chmst.de>
2024-10-20 10:27:58 +0200

    mod_menu php fixes

--- administrator/modules/mod_menu/src/Menu/CssMenu.php
+++ administrator/modules/mod_menu/src/Menu/CssMenu.php
@@ -293,7 +293,8 @@ class CssMenu
                 continue;
             }
 
-            if (substr($item->link, 0, 8) === 'special:') {
+            $itemLink = !empty($item->link) ? $item->link : ''; 
+            if (substr($itemLink, 0, 8) === 'special:') {
                 $special = substr($item->link, 8);
 
                 if ($special === 'language-forum') {
@@ -311,12 +312,11 @@ class CssMenu
              * processing. It is needed for links from menu items of third party extensions link to Joomla! core
              * components like com_categories, com_fields...
              */
-            if ($option = $uri->getVar('option')) {
-                $item->element = $option;
-            }
+            $item->element = !empty($uri->getVar('option')) ? $uri->getVar('option') : '';
+
 
             // Exclude item if is not enabled
-            if ($item->element && !ComponentHelper::isEnabled($item->element)) {
+            if (!ComponentHelper::isEnabled($item->element)) {
                 $parent->removeChild($item);
                 continue;
             }
@@ -460,7 +460,7 @@ class CssMenu
             }
 
             // Ok we passed everything, load language at last only
-            if ($item->element) {
+            if (!empty($item->element)) {
                 $language->load($item->element . '.sys', JPATH_ADMINISTRATOR) ||
                 $language->load($item->element . '.sys', JPATH_ADMINISTRATOR . '/components/' . $item->element);
             }
@@ -492,7 +492,7 @@ class CssMenu
      */
     public function getIconClass($node)
     {
-        $identifier = $node->class;
+        $identifier = !empty($node->class) ? $node->class : '';
 
         // Top level is special
         if (trim($identifier) == '') {
