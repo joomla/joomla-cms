c27037e10e355a1faa5ab2cc15ead47395743a91
Dimitris Grammatikogiannis <dg@dgrammatiko.dev>
2025-02-24 09:16:06 +0200

    Drop Bootstrap::framework

--- libraries/src/HTML/Helpers/Bootstrap.php
+++ libraries/src/HTML/Helpers/Bootstrap.php
@@ -617,35 +617,6 @@ abstract class Bootstrap
     }
 
     /**
-     * Method to load the ALL the Bootstrap Components
-     *
-     * If debugging mode is on an uncompressed version of Bootstrap is included for easier debugging.
-     *
-     * @param   mixed  $debug  Is debugging mode on? [optional]
-     *
-     * @return  void
-     *
-     * @since   3.0
-     *
-     * @deprecated  4.0 will be removed in 6.0
-     *              Will be removed without replacement
-     *              Load the different scripts with their individual method calls
-     */
-    public static function framework($debug = null): void
-    {
-        $wa = Factory::getApplication()
-            ->getDocument()
-            ->getWebAssetManager();
-
-        array_map(
-            function ($script) use ($wa) {
-                $wa->useScript('bootstrap.' . $script);
-            },
-            ['alert', 'button', 'carousel', 'collapse', 'dropdown', 'modal', 'offcanvas', 'popover', 'scrollspy', 'tab', 'toast']
-        );
-    }
-
-    /**
      * Loads CSS files needed by Bootstrap
      *
      * @param   boolean  $includeMainCss  If true, main bootstrap.css files are loaded
