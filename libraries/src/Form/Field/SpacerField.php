8e39eae4ac96dc65956684eabc32a06082d5b6d6
BrianTeeman <brian@teeman.net>
2025-05-22 10:00:37 +0100

    [5.3] Spacer Field Double Translation
    [4.1] Workflows double translation
    ## Test
    Enable language debug
    Enable workflows
    Open an existing article
    See the highlighted indicators **??
    
    Signed-off-by: BrianTeeman <brian@teeman.net>
    
    ### Before
    
    ### After

--- libraries/src/Form/Field/SpacerField.php
+++ libraries/src/Form/Field/SpacerField.php
@@ -71,7 +71,6 @@ class SpacerField extends FormField
 
             // Get the label text from the XML element, defaulting to the element name.
             $text = $this->element['label'] ? (string) $this->element['label'] : (string) $this->element['name'];
-            $text = $this->translateLabel ? Text::_($text) : $text;
 
             // Build the class for the label.
             $class = !empty($this->description) ? 'hasPopover' : '';
