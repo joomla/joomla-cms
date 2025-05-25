9c80dec375c3ab2826cdf7634a0bf696375eff94
Richard Fath <richard67@users.noreply.github.com>
2025-05-25 11:36:19 +0200

    Fix wrong folder in uninstallation of compat (5) plugin.

--- administrator/components/com_admin/script.php
+++ administrator/components/com_admin/script.php
@@ -288,7 +288,7 @@ class JoomlaInstallerScript
              * ['type' => 'plugin', 'element' => 'compat', 'folder' => 'system', 'client_id' => 0, 'pre_function' => 'migrateCompatPlugin'],
              */
             // Uninstall the old joomla 5 compatibility plugin
-            ['type' => 'plugin', 'element' => 'compat', 'folder' => 'system', 'client_id' => 0, 'pre_function' => null],
+            ['type' => 'plugin', 'element' => 'compat', 'folder' => 'behaviour', 'client_id' => 0, 'pre_function' => null],
         ];
 
         $db = Factory::getDbo();
