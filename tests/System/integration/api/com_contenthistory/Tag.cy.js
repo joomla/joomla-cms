db497075a5f9f0eb57c571ee340deab54e34dc82
Nicola Galgano <optimus4joomla@gmail.com>
2025-05-26 11:31:46 +0200

    delete and patch tests

--- tests/System/integration/api/com_contenthistory/Tag.cy.js
+++ tests/System/integration/api/com_contenthistory/Tag.cy.js
@@ -15,7 +15,6 @@ describe('Test that contenthistory for tag API endpoint', () => {
 
         // Extract the `data` array
         const { data: historyEntries } = response.body;
-        cy.log(`History Entries: ${historyEntries.length}`);
 
         // Iterate through each history entry
         historyEntries.forEach((entry) => {
@@ -34,16 +33,6 @@ describe('Test that contenthistory for tag API endpoint', () => {
           const createdTime = versionData.created_time;
           const modifiedTime = versionData.modified_time;
 
-          // Log details for debugging
-          cy.log(`History ID: ${historyId}`);
-          cy.log(`Save Date: ${saveDate}`);
-          cy.log(`Editor: ${editor}`);
-          cy.log(`Character Count: ${characterCount}`);
-          cy.log(`Tag Title: ${tagTitle}`);
-          cy.log(`Alias: ${alias}`);
-          cy.log(`Created Time: ${createdTime}`);
-          cy.log(`Modified Time: ${modifiedTime}`);
-
           // Perform assertions
           expect(attributes).to.have.property('editor_user_id');
           expect(versionData).to.have.property('title');
@@ -54,7 +43,48 @@ describe('Test that contenthistory for tag API endpoint', () => {
         // Check the total pages from metadata
         const totalPages = response.body.meta['total-pages'];
         expect(totalPages).to.eq(1);
-        cy.log(`Total Pages: ${totalPages}`);
+      });
+  });
+
+  it('can delete the history of an existing tag', () => {
+   cy.api_post('/tags', {
+      title: 'automated test tag', parent_id: 1, level: 1, description: '', language: '*',
+    })
+      .then((tag) => cy.api_get(`/tags/${tag.body.data.attributes.id}/contenthistory`))
+      .then((response) => {
+        // Assert response status
+        expect(response.status).to.eq(200);
+
+        // Extract the `data` array
+        const historyEntries = response.body.data;
+
+        // Iterate through each history entry
+        historyEntries.forEach((entry) => {
+          // Access top-level attributes
+          cy.api_delete(`/tags/${entry.id}/contenthistory`)
+            .then((result) => expect(result.status).to.eq(204));
+        });
+      });
+  });
+
+  it('can keep the forever for the history of an existing tag', () => {
+     cy.api_post('/tags', {
+      title: 'automated test tag', parent_id: 1, level: 1, description: '', language: '*',
+    })
+      .then((tag) => cy.api_get(`/tags/${tag.body.data.attributes.id}/contenthistory`))
+      .then((response) => {
+        // Assert response status
+        expect(response.status).to.eq(200);
+
+        // Extract the `data` array
+        const historyEntries = response.body.data;
+
+        // Iterate through each history entry
+        historyEntries.forEach((entry) => {
+          // Access top-level attributes
+          cy.api_patch(`/tags/${entry.id}/contenthistory/keep`, { keep_forever: 1 })
+            .then((result) => expect(result.status).to.eq(200));
+        });
       });
   });
 });
