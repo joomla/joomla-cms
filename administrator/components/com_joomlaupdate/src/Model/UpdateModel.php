de6fcbac9e6c79acf4008c5fbda9331b4a36b463
Harald Leithner <leithner@itronic.at>
2025-05-29 13:31:31 +0200

    Set tuf alpha update server

--- administrator/components/com_joomlaupdate/src/Model/UpdateModel.php
+++ administrator/components/com_joomlaupdate/src/Model/UpdateModel.php
@@ -121,7 +121,7 @@ class UpdateModel extends BaseDatabaseModel
                  * case 'nochange':
                  * case 'testing':
                  */
-                $updateURL = 'https://update.joomla.org/cms/';
+                $updateURL = 'https://update.joomla.org/alpha/';
         }
 
         $updateType = (pathinfo($updateURL, PATHINFO_EXTENSION) === 'xml') ? 'collection' : 'tuf';
