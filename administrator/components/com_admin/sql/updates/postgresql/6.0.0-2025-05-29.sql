664d6a59768b259d54ea21f7a7b019b2a668e4d6
Viviana Menzel <vivianamenzel@dr-menzel-it.de>
2025-05-29 15:41:36 +0200

    Install and update sql

---
+++ administrator/components/com_admin/sql/updates/postgresql/6.0.0-2025-05-29.sql
@@ -0,0 +1,10 @@
+-- Templates
+INSERT INTO `#__extensions` (`package_id`, `name`, `type`, `element`, `folder`, `client_id`, `enabled`, `access`, `protected`, `locked`, `manifest_cache`, `params`, `custom_data`, `ordering`, `state`) VALUES
+(0, 'cassiopeia_alpha', 'template', 'cassiopeia_alpha', '', 0, 1, 1, 0, 1, '', '{"brand":"1","logoFile":"","siteTitle":"Test","siteDescription":"","useFontScheme":"0","systemFontBody":"","systemFontHeading":"","colorName":"colors_standard","fluidContainer":"0","stickyHeader":0,"backTop":0,"headerbg":"rgb(193, 205, 207)","headercolor":"rgb(23, 23, 23)","bodybg":"rgb(254, 254, 254)","bodycolor":"rgb(23, 23, 23)","linkcolor":"rgb(29, 121, 137)","linkcolorh":"rgb(14, 59, 67)","btnbg":"rgb(206, 60, 55)","btnbgh":"rgb(131, 35, 32)","btncolor":"rgb(254, 254, 254)","btncolorh":"rgb(254, 254, 254)","footerbg":"rgb(29, 121, 137)","footercolor":"rgb(254, 254, 254)","bodysize":"1","h1size":"2","h2size":"1.7","h3size":"1.5"}', '', 0, 0);
+
+--
+-- Dumping data for table `#__template_styles`
+--
+
+INSERT INTO `#__template_styles` (`id`, `template`, `client_id`, `home`, `title`, `inheritable`, `parent`, `params`) VALUES
+(12, 'cassiopeia_alpha', 0, '0', 'Cassiopeia Alpha - Default', 0, 'cassiopeia', '{"brand":"1","logoFile":"","siteTitle":"Test","siteDescription":"","useFontScheme":"0","systemFontBody":"","systemFontHeading":"","colorName":"colors_standard","fluidContainer":"0","stickyHeader":0,"backTop":0,"headerbg":"rgb(193, 205, 207)","headercolor":"rgb(23, 23, 23)","bodybg":"rgb(254, 254, 254)","bodycolor":"rgb(23, 23, 23)","linkcolor":"rgb(29, 121, 137)","linkcolorh":"rgb(14, 59, 67)","btnbg":"rgb(206, 60, 55)","btnbgh":"rgb(131, 35, 32)","btncolor":"rgb(254, 254, 254)","btncolorh":"rgb(254, 254, 254)","footerbg":"rgb(29, 121, 137)","footercolor":"rgb(254, 254, 254)","bodysize":"1","h1size":"2","h2size":"1.7","h3size":"1.5"}');
