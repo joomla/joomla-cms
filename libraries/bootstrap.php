2b5e1533776c28d0f0332ad61cf940ca969b2a7e
Fedik <getthesite@gmail.com>
2025-05-31 11:06:01 +0300

    Check for empty env and use short "dev"

--- libraries/bootstrap.php
+++ libraries/bootstrap.php
@@ -42,9 +42,18 @@ $loader->unregister();
 spl_autoload_register([new \Joomla\CMS\Autoload\ClassLoader($loader), 'loadClass'], true, true);
 
 // Check if load of the environment variables is enabled.
-if (is_file(JPATH_ROOT . '/.env') || is_file(JPATH_ROOT . '/.env.development')) {
-    $dotenv  = Dotenv\Dotenv::createImmutable(JPATH_ROOT, ['.env', '.env.development']);
-    $dotenv->safeLoad();
+if (is_file(JPATH_ROOT . '/.env') || is_file(JPATH_ROOT . '/.env.dev')) {
+    /**
+     * Some PHP configurations set variables_order="GPCS" not "EGPCS", in which case $_ENV is NOT populated.
+     * Detect if the $_ENV  was empty and handle it by explicitly populating through getenv() for CLI,
+     * or filter variables from  $_SERVER prefixed with JOOMLA_ for web requests.
+     * To make it work the same for CLI and WEB set variables_order="EGPCS" in php.ini.
+     */
+    if (!$_ENV) {
+        $_ENV = PHP_SAPI === 'cli' ? getenv() : \array_filter($_SERVER, fn($k) => str_starts_with($k, 'JOOMLA_'), ARRAY_FILTER_USE_KEY);
+    }
+
+    Dotenv\Dotenv::createImmutable(JPATH_ROOT, ['.env', '.env.dev'], false)->safeLoad();
 }
 
 /**
