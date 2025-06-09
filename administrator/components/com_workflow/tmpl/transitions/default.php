21e77f37d63590bea8434fbe8e8687a4a0b8a4cc
Christian Heel <66922325+heelc29@users.noreply.github.com>
2025-06-09 08:24:05 +0200

    table id

--- administrator/components/com_workflow/tmpl/transitions/default.php
+++ administrator/components/com_workflow/tmpl/transitions/default.php
@@ -60,7 +60,7 @@ if ($saveOrder) {
                         <?php echo Text::_('JGLOBAL_NO_MATCHING_RESULTS'); ?>
                     </div>
                 <?php else : ?>
-                    <table class="table">
+                    <table class="table" id="transitionList">
                         <caption class="visually-hidden">
                             <?php echo Text::_('COM_WORKFLOW_TRANSITIONS_TABLE_CAPTION'); ?>,
                             <span id="orderedBy"><?php echo Text::_('JGLOBAL_SORTED_BY'); ?> </span>,
