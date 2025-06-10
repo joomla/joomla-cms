d996002c7f42bafa3a4ccfc4ba3312fd7fe12924
Hannes Papenberg <info@joomlager.de>
2025-06-10 13:45:09 +0200

    Revert deprecation of CoreContent Table class

--- libraries/src/Table/CoreContent.php
+++ libraries/src/Table/CoreContent.php
@@ -28,7 +28,6 @@ use Joomla\String\StringHelper;
  * Core content table
  *
  * @since  3.1
- * @deprecated  __DEPLOY_VERSION__ will be removed in 7.0 without replacement
  */
 class CoreContent extends Table implements CurrentUserInterface
 {
@@ -39,7 +38,6 @@ class CoreContent extends Table implements CurrentUserInterface
      *
      * @var    boolean
      * @since  4.0.0
-     * @deprecated  __DEPLOY_VERSION__ will be removed in 7.0 without replacement
      */
     protected $_supportNullValue = true;
 
@@ -48,7 +46,6 @@ class CoreContent extends Table implements CurrentUserInterface
      *
      * @var    array
      * @since  4.0.0
-     * @deprecated  __DEPLOY_VERSION__ will be removed in 7.0 without replacement
      */
     protected $_jsonEncode = ['core_params', 'core_metadata', 'core_images', 'core_urls', 'core_body'];
 
@@ -59,7 +56,6 @@ class CoreContent extends Table implements CurrentUserInterface
      * @param   ?DispatcherInterface  $dispatcher  Event dispatcher for this table
      *
      * @since   3.1
-     * @deprecated  __DEPLOY_VERSION__ will be removed in 7.0 without replacement
      */
     public function __construct(DatabaseInterface $db, ?DispatcherInterface $dispatcher = null)
     {
@@ -79,7 +75,6 @@ class CoreContent extends Table implements CurrentUserInterface
      *
      * @see     Table::check()
      * @since   3.1
-     * @deprecated  __DEPLOY_VERSION__ will be removed in 7.0 without replacement
      */
     public function check()
     {
@@ -160,24 +155,6 @@ class CoreContent extends Table implements CurrentUserInterface
     }
 
     /**
-     * Override \Joomla\CMS\Table\Table delete method to include deleting corresponding row from #__ucm_base.
-     *
-     * @param   integer  $pk  primary key value to delete. Must be set or throws an exception.
-     *
-     * @return  boolean  True on success.
-     *
-     * @throws  \UnexpectedValueException
-     * @deprecated  __DEPLOY_VERSION__ will be removed in 7.0 without replacement
-     * @since      3.1
-     */
-    public function delete($pk = null)
-    {
-        $baseTable = new Ucm($this->getDatabase(), $this->getDispatcher());
-
-        return parent::delete($pk) && $baseTable->delete($pk);
-    }
-
-    /**
      * Method to delete a row from the #__ucm_content table by content_item_id.
      *
      * @param   integer  $contentItemId  value of the core_content_item_id to delete. Corresponds to the primary key of the content table.
@@ -186,7 +163,6 @@ class CoreContent extends Table implements CurrentUserInterface
      * @return  boolean  True on success.
      *
      * @throws  \UnexpectedValueException
-     * @deprecated  __DEPLOY_VERSION__ will be removed in 7.0 without replacement
      * @since      3.1
      */
     public function deleteByContentId($contentItemId = null, $typeAlias = null)
@@ -231,7 +207,6 @@ class CoreContent extends Table implements CurrentUserInterface
      * @return  boolean  True on success.
      *
      * @since   3.1
-     * @deprecated  __DEPLOY_VERSION__ will be removed in 7.0 without replacement
      */
     public function store($updateNulls = true)
     {
@@ -271,9 +246,7 @@ class CoreContent extends Table implements CurrentUserInterface
             $this->setRules('{}');
         }
 
-        $result = parent::store($updateNulls);
-
-        return $result && $this->storeUcmBase($updateNulls, $isNew);
+        return parent::store($updateNulls);
     }
 
     /**
