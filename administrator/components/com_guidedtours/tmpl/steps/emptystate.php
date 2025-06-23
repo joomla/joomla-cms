36518b8a7f9731448b11b5bd4a8a1c011998c885
Olivier Buisard <olivier.buisard@simplifyyourweb.com>
2025-06-23 08:41:54 -0400

    Added tour

--- administrator/components/com_guidedtours/tmpl/steps/emptystate.php
+++ administrator/components/com_guidedtours/tmpl/steps/emptystate.php
@@ -18,6 +18,7 @@ $displayData = [
     'textPrefix' => 'COM_GUIDEDTOURS_STEPS',
     'formURL'    => 'index.php?option=com_guidedtours&view=steps',
     'helpURL'    => 'https://docs.joomla.org/Special:MyLanguage/Help5.x:Guided_Tours:_Steps',
+    'tourUID'    => 'joomla-guidedtoursteps',
     'icon'       => 'icon-map-signs',
 ];
 
