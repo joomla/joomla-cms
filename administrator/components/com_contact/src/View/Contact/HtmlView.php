677d44167c94cf405595d0ff3f3d57a370be6ea5
Fedik <getthesite@gmail.com>
2025-07-06 12:23:57 +0300

    form controls: fix

--- administrator/components/com_contact/src/View/Contact/HtmlView.php
+++ administrator/components/com_contact/src/View/Contact/HtmlView.php
@@ -91,7 +91,9 @@ class HtmlView extends BaseHtmlView
         }
 
         // If we are forcing a language in modal (used for associations).
-        if ($this->getLayout() === 'modal' && $forcedLanguage = Factory::getApplication()->getInput()->get('forcedLanguage', '', 'cmd')) {
+        $forcedLanguage = Factory::getApplication()->getInput()->get('forcedLanguage', '', 'cmd');
+
+        if ($this->getLayout() === 'modal' && $forcedLanguage) {
             // Set the language field to the forcedLanguage and disable changing it.
             $this->form->setValue('language', null, $forcedLanguage);
             $this->form->setFieldAttribute('language', 'readonly', 'true');
@@ -112,7 +114,7 @@ class HtmlView extends BaseHtmlView
         // Add form control fields
         $this->form
             ->addControlField('task', '')
-            ->addControlField('forcedLanguage', $forcedLanguage ?? '');
+            ->addControlField('forcedLanguage', $forcedLanguage);
 
         parent::display($tpl);
     }
