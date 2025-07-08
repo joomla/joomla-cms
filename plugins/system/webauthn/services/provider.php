joomla/joomla-cms

commit 79f439ddab8e248b441d7c9ca8fc603c1ff33b6b

Remove extra reference assignment in plugin constructor for dispatcher argument (#40746)
---------

Co-authored-by: Harald Leithner <leithner@itronic.at>

plugins/system/webauthn/services/provider.php
@@ -22,6 +22,7 @@ use Joomla\Plugin\System\Webauthn\Authentication;
 use Joomla\Plugin\System\Webauthn\CredentialRepository;
 use Joomla\Plugin\System\Webauthn\Extension\Webauthn;
 use Joomla\Plugin\System\Webauthn\MetadataRepository;
+use Joomla\Registry\Registry;
 use Webauthn\MetadataService\MetadataStatementRepository;
 use Webauthn\PublicKeyCredentialSourceRepository;
 
@@ -40,9 +41,6 @@ return new class () implements ServiceProviderInterface {
         $container->set(
             PluginInterface::class,
             function (Container $container) {
-                $config  = (array) PluginHelper::getPlugin('system', 'webauthn');
-                $subject = $container->get(DispatcherInterface::class);
-
                 $app     = Factory::getApplication();
                 $session = $container->has('session') ? $container->get('session') : $this->getSession($app);
 
@@ -52,7 +50,7 @@ return new class () implements ServiceProviderInterface {
                     : new CredentialRepository($db);
 
                 $metadataRepository = null;
-                $params             = new Joomla\Registry\Registry($config['params'] ?? '{}');
+                $params             = new Registry($config['params'] ?? '{}');
 
                 if ($params->get('attestationSupport', 0) == 1) {
                     $metadataRepository    = $container->has(MetadataStatementRepository::class)
@@ -64,7 +62,11 @@ return new class () implements ServiceProviderInterface {
                     ? $container->get(Authentication::class)
                     : new Authentication($app, $session, $credentialsRepository, $metadataRepository);
 
-                $plugin = new Webauthn($subject, $config, $authenticationHelper);
+                $plugin = new Webauthn(
+                    $container->get(DispatcherInterface::class),
+                    (array) PluginHelper::getPlugin('system', 'webauthn'),
+                    $authenticationHelper
+                );
                 $plugin->setApplication($app);
 
                 return $plugin;

plugins/api-authentication/basic/services/provider.php
@@ -35,9 +35,8 @@ return new class () implements ServiceProviderInterface {
         $container->set(
             PluginInterface::class,
             function (Container $container) {
-                $dispatcher = $container->get(DispatcherInterface::class);
                 $plugin     = new Basic(
-                    $dispatcher,
+                    $container->get(DispatcherInterface::class),
                     (array) PluginHelper::getPlugin('api-authentication', 'basic'),
                 );
                 $plugin->setApplication(Factory::getApplication());

plugins/authentication/cookie/services/provider.php
@@ -35,10 +35,8 @@ return new class () implements ServiceProviderInterface {
         $container->set(
             PluginInterface::class,
             function (Container $container) {
-                $dispatcher = $container->get(DispatcherInterface::class);
-
                 $plugin = new Cookie(
-                    $dispatcher,
+                    $container->get(DispatcherInterface::class),
                     (array) PluginHelper::getPlugin('authentication', 'cookie')
                 );
                 $plugin->setApplication(Factory::getApplication());

plugins/authentication/joomla/services/provider.php
@@ -35,10 +35,8 @@ return new class () implements ServiceProviderInterface {
         $container->set(
             PluginInterface::class,
             function (Container $container) {
-                $dispatcher = $container->get(DispatcherInterface::class);
-
                 $plugin = new Joomla(
-                    $dispatcher,
+                    $container->get(DispatcherInterface::class),
                     (array) PluginHelper::getPlugin('authentication', 'joomla')
                 );
                 $plugin->setApplication(Factory::getApplication());

plugins/authentication/ldap/services/provider.php
@@ -34,11 +34,9 @@ return new class () implements ServiceProviderInterface {
         $container->set(
             PluginInterface::class,
             function (Container $container) {
-                $dispatcher = $container->get(DispatcherInterface::class);
-
                 $plugin = new Ldap(
                     new LdapFactory(),
-                    $dispatcher,
+                    $container->get(DispatcherInterface::class),
                     (array) PluginHelper::getPlugin('authentication', 'ldap')
                 );
                 $plugin->setApplication(Factory::getApplication());

plugins/behaviour/taggable/services/provider.php
@@ -32,9 +32,8 @@ return new class () implements ServiceProviderInterface {
         $container->set(
             PluginInterface::class,
             function (Container $container) {
-                $dispatcher = $container->get(DispatcherInterface::class);
                 $plugin     = new Taggable(
-                    $dispatcher,
+                    $container->get(DispatcherInterface::class),
                     (array) PluginHelper::getPlugin('behaviour', 'taggable')
                 );
 

plugins/behaviour/versionable/services/provider.php
@@ -35,9 +35,8 @@ return new class () implements ServiceProviderInterface {
         $container->set(
             PluginInterface::class,
             function (Container $container) {
-                $dispatcher = $container->get(DispatcherInterface::class);
                 $plugin     = new Versionable(
-                    $dispatcher,
+                    $container->get(DispatcherInterface::class),
                     (array) PluginHelper::getPlugin('behaviour', 'versionable'),
                     new InputFilter(),
                     new CMSHelper()

plugins/content/confirmconsent/services/provider.php
@@ -33,9 +33,8 @@ return new class () implements ServiceProviderInterface {
         $container->set(
             PluginInterface::class,
             function (Container $container) {
-                $dispatcher = $container->get(DispatcherInterface::class);
                 $plugin     = new ConfirmConsent(
-                    $dispatcher,
+                    $container->get(DispatcherInterface::class),
                     (array) PluginHelper::getPlugin('content', 'confirmconsent')
                 );
                 $plugin->setApplication(Factory::getApplication());

commit 5ab26d7e6af259ff6f3638231d9eb4cae826f9de

fix webauthn attestationSupport not working
broken since 79f439ddab8e248b441d7c9ca8fc603c1ff33b6b (#40746)

plugins/system/webauthn/services/provider.php
@@ -43,6 +43,7 @@ return new class () implements ServiceProviderInterface {
             PluginInterface::class,
             function (Container $container) {
                 $app     = Factory::getApplication();
+                $config  = (array) PluginHelper::getPlugin('system', 'webauthn');
                 $session = $container->has('session') ? $container->get('session') : $this->getSession($app);
 
                 $db                    = $container->get(DatabaseInterface::class);
@@ -65,7 +66,7 @@ return new class () implements ServiceProviderInterface {
 
                 $plugin = new Webauthn(
                     $container->get(DispatcherInterface::class),
-                    (array) PluginHelper::getPlugin('system', 'webauthn'),
+                    $config,
                     $authenticationHelper
                 );
                 $plugin->setApplication($app);