03cad900fce61916bb2f33937657de607efb1b22
Olivier Buisard <olivier.buisard@simplifyyourweb.com>
2025-07-19 22:45:21 +0200

    Check tour state

--- administrator/components/com_finder/tmpl/filters/emptystate.php
+++ administrator/components/com_finder/tmpl/filters/emptystate.php
@@ -10,6 +10,7 @@
 
 defined('_JEXEC') or die;
 
+use Joomla\CMS\Factory;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\Layout\LayoutHelper;
 
@@ -19,7 +20,6 @@ $displayData = [
     'textPrefix' => 'COM_FINDER',
     'formURL'    => 'index.php?option=com_finder&view=filters',
     'helpURL'    => 'https://docs.joomla.org/Special:MyLanguage/Smart_Search_quickstart_guide',
-    'tourUID'    => 'joomla-smartsearch',
     'icon'       => 'icon-search-plus finder',
     'btnadd'     => Text::_('COM_FINDER_FILTERS_EMPTYSTATE_BUTTON_ADD'),
     'content'    => Text::_('COM_FINDER_FILTERS_EMPTYSTATE_CONTENT'),
@@ -28,6 +28,17 @@ $displayData = [
 
 if ($this->getCurrentUser()->authorise('core.create', 'com_finder')) {
     $displayData['createURL']  = "index.php?option=com_finder&task=filter.add";
+}
+
+$factory = Factory::getApplication()->bootComponent('com_guidedtours')->getMVCFactory();
+
+// Get an instance of the guided tour model
+$tourModel = $factory->createModel('Tour', 'Administrator', ['ignore_request' => true]);
+
+$tourUid = 'joomla-smartsearch';
+
+if ($tourModel->isAvailable($tourUid) !== false) {
+    $displayData['tourUID'] = $tourUid;
 }
 
 echo LayoutHelper::render('joomla.content.emptystate', $displayData);
