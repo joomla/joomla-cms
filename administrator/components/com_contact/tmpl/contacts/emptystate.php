0685256d531f14d2f11e636c70d62a9c29c8462e
Olivier Buisard <olivier.buisard@simplifyyourweb.com>
2025-07-19 22:43:11 +0200

    Check tour state

--- administrator/components/com_contact/tmpl/contacts/emptystate.php
+++ administrator/components/com_contact/tmpl/contacts/emptystate.php
@@ -10,6 +10,7 @@
 
 defined('_JEXEC') or die;
 
+use Joomla\CMS\Factory;
 use Joomla\CMS\Layout\LayoutHelper;
 
 /** @var \Joomla\Component\Contact\Administrator\View\Contacts\HtmlView $this */
@@ -18,7 +19,6 @@ $displayData = [
     'textPrefix' => 'COM_CONTACT',
     'formURL'    => 'index.php?option=com_contact',
     'helpURL'    => 'https://docs.joomla.org/Special:MyLanguage/Help5.x:Contacts',
-    'tourUID'    => 'joomla-contacts',
     'icon'       => 'icon-address-book contact',
 ];
 
@@ -26,6 +26,17 @@ $user = $this->getCurrentUser();
 
 if ($user->authorise('core.create', 'com_contact') || count($user->getAuthorisedCategories('com_contact', 'core.create')) > 0) {
     $displayData['createURL'] = 'index.php?option=com_contact&task=contact.add';
+}
+
+$factory = Factory::getApplication()->bootComponent('com_guidedtours')->getMVCFactory();
+
+// Get an instance of the guided tour model
+$tourModel = $factory->createModel('Tour', 'Administrator', ['ignore_request' => true]);
+
+$tourUid = 'joomla-contacts';
+
+if ($tourModel->isAvailable($tourUid) !== false) {
+    $displayData['tourUID'] = $tourUid;
 }
 
 echo LayoutHelper::render('joomla.content.emptystate', $displayData);
