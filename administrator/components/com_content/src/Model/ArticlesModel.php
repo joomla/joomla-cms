07df4f5d1a1697ff37c80e724219a606c2bab5e8
Aashish Jha <aashishjha1107@gmail.com>
2025-07-22 07:09:26 +0000

    The filter should now work correctly for all articles regardless of whether their checked_out field contains NULL or 0

--- administrator/components/com_content/src/Model/ArticlesModel.php
+++ administrator/components/com_content/src/Model/ArticlesModel.php
@@ -460,8 +460,8 @@ class ArticlesModel extends ListModel
                 // Only checked out articles
                 $query->where($db->quoteName('a.checked_out') . ' > 0');
             } elseif ($checkedOut == '0') {
-                // Only not checked out articles
-                $query->where($db->quoteName('a.checked_out') . ' = 0');
+                // Only not checked out articles (checked_out is 0 or NULL)
+                $query->where('(' . $db->quoteName('a.checked_out') . ' = 0 OR ' . $db->quoteName('a.checked_out') . ' IS NULL)');
             }
         }
 
