8ebf047771083118718459f6a44868765b2a07f2
Hannes Papenberg <info@joomlager.de>
2025-07-24 10:28:48 +0200

    More updates

--- plugins/user/joomla/src/Extension/Joomla.php
+++ plugins/user/joomla/src/Extension/Joomla.php
@@ -361,11 +361,13 @@ final class Joomla extends CMSPlugin implements SubscriberInterface
             $this->getApplication()->getInput()->cookie->set(
                 'joomla_user_state',
                 'logged_in',
-                0,
-                $this->getApplication()->get('cookie_path', '/'),
-                $this->getApplication()->get('cookie_domain', ''),
-                $this->getApplication()->isHttpsForced(),
-                true
+                [
+                    'expires'  => 0,
+                    'path'     => $this->getApplication()->get('cookie_path', '/'),
+                    'domain'   => $this->getApplication()->get('cookie_domain', ''),
+                    'secure'   => $this->getApplication()->isHttpsForced(),
+                    'httponly' => true
+                ]
             );
         }
     }
