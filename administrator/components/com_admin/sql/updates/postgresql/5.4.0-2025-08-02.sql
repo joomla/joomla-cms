0898fd3a3b0ec20bf80257167b23e020bb97df3e
Richard Fath <richard.fath@t-online.de>
2025-08-02 17:44:46 +0200

    Fix unlocked core extensions for new installs and updates

---
+++ administrator/components/com_admin/sql/updates/postgresql/5.4.0-2025-08-02.sql
@@ -0,0 +1,9 @@
+UPDATE "#__extensions" SET "locked" = 1
+ WHERE "type" = 'plugin' AND "folder" = 'schemaorg'
+   AND "element" IN ('article', 'blogposting', 'book', 'event', 'jobposting', 'organization', 'person', 'recipe', 'custom');
+
+UPDATE "#__extensions" SET "locked" = 1
+ WHERE "type" = 'plugin' AND "element" = 'schemaorg' AND "folder" = 'system';
+
+UPDATE "#__extensions" SET "locked" = 1
+ WHERE "type" = 'plugin' AND "element" = 'globalcheckin' AND "folder" = 'task';
