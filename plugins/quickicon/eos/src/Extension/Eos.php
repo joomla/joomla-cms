fb6eb490f2ce7e0ef4322a9b62f967f44d8dfebd
BrianTeeman <brian@teeman.net>
2025-08-04 09:48:08 +0100

    url

--- plugins/quickicon/eos/src/Extension/Eos.php
+++ plugins/quickicon/eos/src/Extension/Eos.php
@@ -189,7 +189,7 @@ final class Eos extends CMSPlugin implements SubscriberInterface
                 'id'          => 5,
                 'messageText' => 'PLG_QUICKICON_EOS_MESSAGE_ERROR_SUPPORT_ENDED',
                 'messageType' => 'error',
-                'messageLink' => 'https://docs.joomla.org/Special:MyLanguage/Joomla_5.4.x_to_6.x_Planning_and_Upgrade_Step_by_Step',
+                'messageLink' => 'https://docs.joomla.org/Special:MyLanguage/Joomla_6.4.x_to_7.x_Planning_and_Upgrade_Step_by_Step',
                 'snoozable'   => false,
             ];
         }
@@ -200,7 +200,7 @@ final class Eos extends CMSPlugin implements SubscriberInterface
                 'id'          => 4,
                 'messageText' => 'PLG_QUICKICON_EOS_MESSAGE_WARNING_SUPPORT_ENDING',
                 'messageType' => 'warning',
-                'messageLink' => 'https://docs.joomla.org/Special:MyLanguage/Joomla_5.4.x_to_6.x_Planning_and_Upgrade_Step_by_Step',
+                'messageLink' => 'https://docs.joomla.org/Special:MyLanguage/Joomla_6.4.x_to_7.x_Planning_and_Upgrade_Step_by_Step',
                 'snoozable'   => true,
             ];
         }
@@ -211,7 +211,7 @@ final class Eos extends CMSPlugin implements SubscriberInterface
                 'id'          => 3,
                 'messageText' => 'PLG_QUICKICON_EOS_MESSAGE_WARNING_SECURITY_ONLY',
                 'messageType' => 'warning',
-                'messageLink' => 'https://docs.joomla.org/Special:MyLanguage/Joomla_5.4.x_to_6.x_Planning_and_Upgrade_Step_by_Step',
+                'messageLink' => 'https://docs.joomla.org/Special:MyLanguage/Joomla_6.4.x_to_7.x_Planning_and_Upgrade_Step_by_Step',
                 'snoozable'   => true,
             ];
         }
@@ -233,7 +233,7 @@ final class Eos extends CMSPlugin implements SubscriberInterface
                 'id'          => 1,
                 'messageText' => 'PLG_QUICKICON_EOS_MESSAGE_INFO_01',
                 'messageType' => 'info',
-                'messageLink' => 'https://joomla.org/6',
+                'messageLink' => 'https://joomla.org/7',
                 'snoozable'   => true,
             ];
         }
