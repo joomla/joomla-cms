254e64a3d860926f4f8a5341fc2b580074bdfe80
Miroslav Orinčák <info@joomla-webstranky.sk>
2025-08-04 18:40:40 +0200

    dispatch method for mod_login

--- modules/mod_login/src/Dispatcher/Dispatcher.php
+++ modules/mod_login/src/Dispatcher/Dispatcher.php
@@ -14,6 +14,7 @@ use Joomla\CMS\Dispatcher\AbstractModuleDispatcher;
 use Joomla\CMS\Helper\AuthenticationHelper;
 use Joomla\CMS\Helper\HelperFactoryAwareInterface;
 use Joomla\CMS\Helper\HelperFactoryAwareTrait;
+use Joomla\CMS\Helper\ModuleHelper;
 
 // phpcs:disable PSR1.Files.SideEffects
 \defined('_JEXEC') or die;
@@ -27,6 +28,53 @@ use Joomla\CMS\Helper\HelperFactoryAwareTrait;
 class Dispatcher extends AbstractModuleDispatcher implements HelperFactoryAwareInterface
 {
     use HelperFactoryAwareTrait;
+
+    /**
+     * Runs the dispatcher.
+     *
+     * @return  void
+     *
+     * @since   __DEPLOY_VERSION__
+     */
+    public function dispatch()
+    {
+        $this->loadLanguage();
+
+        $displayData = $this->getLayoutData();
+
+        // Stop when display data is false
+        if ($displayData === false) {
+            return;
+        }
+
+        // Execute the layout without the module context
+        $loader = static function (array $displayData) {
+            // If $displayData doesn't exist in extracted data, unset the variable.
+            if (!\array_key_exists('displayData', $displayData)) {
+                extract($displayData);
+                unset($displayData);
+            } else {
+                extract($displayData);
+            }
+
+            /**
+             * Extracted variables
+             * -----------------
+             * @var   \stdClass  $module
+             * @var   Registry   $params
+             */
+
+
+            // Logged users must load the logout sublayout
+            if (!$user->guest) {
+                $layout .= '_logout';
+            }
+
+            require ModuleHelper::getLayoutPath('mod_login', $layout);
+        };
+
+        $loader($displayData);
+    }
 
     /**
      * Returns the layout data.
@@ -49,12 +97,7 @@ class Dispatcher extends AbstractModuleDispatcher implements HelperFactoryAwareI
         $data['return']       = $helper->getReturnUrlString($data['params'], $type, $data['app']);
         $data['registerLink'] = $helper->getRegistrationUrlString($data['params'], $data['app']);
         $data['extraButtons'] = AuthenticationHelper::getLoginButtons($formId);
-        $layout               = $data['params']->get('layout', 'default');
-
-        // Logged users must load the logout sublayout
-        if (!$data['user']->guest) {
-            $data['params']->set('layout', $layout . '_logout');
-        }
+        $data['layout']       = $data['params']->get('layout', 'default');
 
         return $data;
     }
