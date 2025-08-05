6a5b65f5c7280533681e453657b055afa83f7eb5
Richard Fath <richard.fath@t-online.de>
2025-08-05 22:37:29 +0200

    Update only if not locked

--- administrator/components/com_admin/sql/updates/mysql/6.0.0-2025-08-02.sql
+++ administrator/components/com_admin/sql/updates/mysql/6.0.0-2025-08-02.sql
@@ -1,9 +1,12 @@
 UPDATE `#__extensions` SET `locked` = 1
  WHERE `type` = 'plugin' AND `folder` = 'schemaorg'
-   AND `element` IN ('article', 'blogposting', 'book', 'event', 'jobposting', 'organization', 'person', 'recipe', 'custom');
+   AND `element` IN ('article', 'blogposting', 'book', 'event', 'jobposting', 'organization', 'person', 'recipe', 'custom')
+   AND `locked` <> 1;
 
 UPDATE `#__extensions` SET `locked` = 1
- WHERE `type` = 'plugin' AND `element` = 'schemaorg' AND `folder` = 'system';
+ WHERE `type` = 'plugin' AND `element` = 'schemaorg' AND `folder` = 'system'
+   AND `locked` <> 1;
 
 UPDATE `#__extensions` SET `locked` = 1
- WHERE `type` = 'plugin' AND `element` = 'globalcheckin' AND `folder` = 'task';
+ WHERE `type` = 'plugin' AND `element` = 'globalcheckin' AND `folder` = 'task'
+   AND `locked` <> 1;
