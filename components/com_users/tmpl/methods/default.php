06a678deb4a51a5c1300b1d9bf267cca8415a1ec
Hannes Papenberg <info@joomlager.de>
2025-08-06 19:06:13 +0200

    Update default.php

--- components/com_users/tmpl/methods/default.php
+++ components/com_users/tmpl/methods/default.php
@@ -18,7 +18,7 @@ use Joomla\CMS\Router\Route;
 /** @var \Joomla\Component\Users\Site\View\Methods\HtmlView $this */
 ?>
 <div id="com-users-methods-list">
-    <?php if (isset($this->forHMVC) && !$this->forHMVC) : ?>
+    <?php if (!isset($this->forHMVC) || !$this->forHMVC) : ?>
         <h2 id="com-users-methods-list-head">
             <?php echo Text::_('COM_USERS_MFA_LIST_PAGE_HEAD'); ?>
         </h2>
