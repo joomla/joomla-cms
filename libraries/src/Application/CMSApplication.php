8b5f04057352b6741a67c31761dbec72e6b278f3
Hannes Papenberg <info@joomlager.de>
2025-08-07 14:31:01 +0200

    Remove deprecated code in application classes

--- libraries/src/Application/CMSApplication.php
+++ libraries/src/Application/CMSApplication.php
@@ -475,35 +475,6 @@ abstract class CMSApplication extends WebApplication implements ContainerAwareIn
     }
 
     /**
-     * Gets a configuration value.
-     *
-     * @param   string  $varname  The name of the value to get.
-     * @param   string  $default  Default value to return
-     *
-     * @return  mixed  The user state.
-     *
-     * @since   3.2
-     *
-     * @deprecated  3.2 will be removed in 6.0
-     *              Use get() instead
-     *              Example: Factory::getApplication()->get($varname, $default);
-     */
-    public function getCfg($varname, $default = null)
-    {
-        try {
-            Log::add(
-                \sprintf('%s() is deprecated and will be removed in 6.0. Use Factory->getApplication()->get() instead.', __METHOD__),
-                Log::WARNING,
-                'deprecated'
-            );
-        } catch (\RuntimeException) {
-            // Informational log only
-        }
-
-        return $this->get($varname, $default);
-    }
-
-    /**
      * Gets the client id of the current running application.
      *
      * @return  integer  A client identifier.
@@ -1284,40 +1255,6 @@ abstract class CMSApplication extends WebApplication implements ContainerAwareIn
         $session = $this->getSession();
 
         return $session::checkToken($method);
-    }
-
-    /**
-     * No longer used
-     *
-     * @return  boolean
-     *
-     * @since   4.0.0
-     *
-     * @throws \Exception
-     *
-     * @deprecated  4.2 will be removed in 6.0
-     *              Will be removed without replacements
-     */
-    protected function isTwoFactorAuthenticationRequired(): bool
-    {
-        return false;
-    }
-
-    /**
-     * No longer used
-     *
-     * @return  boolean
-     *
-     * @since   4.0.0
-     *
-     * @throws \Exception
-     *
-     * @deprecated  4.2 will be removed in 6.0
-     *              Will be removed without replacements
-     */
-    private function hasUserConfiguredTwoFactorAuthentication(): bool
-    {
-        return false;
     }
 
     /**
