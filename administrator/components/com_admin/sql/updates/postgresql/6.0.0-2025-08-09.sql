7b27377bf868aa7f5c403b9b21cbd74e6ff6f3d2
Olivier Buisard <olivier.buisard@simplifyyourweb.com>
2025-08-09 20:50:06 -0400

    Uninstall What's New tours from Joomla 5

---
+++ administrator/components/com_admin/sql/updates/postgresql/6.0.0-2025-08-09.sql
@@ -0,0 +1,18 @@
+-- uninstall previous What's New tours
+DELETE FROM "#__guidedtour_steps"
+ WHERE "tour_id" IN (SELECT "id" FROM "#__guidedtours" WHERE "uid" LIKE 'joomla-whatsnew-5-2');
+
+DELETE FROM "#__guidedtours"
+ WHERE "uid" LIKE 'joomla-whatsnew-5-2';
+
+DELETE FROM "#__guidedtour_steps"
+ WHERE "tour_id" IN (SELECT "id" FROM "#__guidedtours" WHERE "uid" LIKE 'joomla-whatsnew-5-3');
+
+DELETE FROM "#__guidedtours"
+ WHERE "uid" LIKE 'joomla-whatsnew-5-3';
+
+DELETE FROM "#__guidedtour_steps"
+ WHERE "tour_id" IN (SELECT "id" FROM "#__guidedtours" WHERE "uid" LIKE 'joomla-whatsnew-5-4');
+
+DELETE FROM "#__guidedtours"
+ WHERE "uid" LIKE 'joomla-whatsnew-5-4';
