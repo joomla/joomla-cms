cadbda7090d8894a6dddcbb42456584c422ac729
Benjamin Trenkle <benjamin.trenkle@wicked-software.de>
2025-08-11 02:10:21 +0200

    Fix namespace usage

--- administrator/components/com_banners/src/View/Banners/HtmlView.php
+++ administrator/components/com_banners/src/View/Banners/HtmlView.php
@@ -10,17 +10,10 @@
 
 namespace Joomla\Component\Banners\Administrator\View\Banners;
 
-use Joomla\CMS\Form\Form;
 use Joomla\CMS\Helper\ContentHelper;
 use Joomla\CMS\Language\Multilanguage;
-use Joomla\CMS\Language\Text;
-use Joomla\CMS\MVC\View\GenericDataException;
 use Joomla\CMS\MVC\View\ListView;
-use Joomla\CMS\Pagination\Pagination;
-use Joomla\CMS\Toolbar\Button\DropdownButton;
-use Joomla\CMS\Toolbar\ToolbarHelper;
 use Joomla\Component\Banners\Administrator\Model\BannersModel;
-use Joomla\Registry\Registry;
 
 // phpcs:disable PSR1.Files.SideEffects
 \defined('_JEXEC') or die;
