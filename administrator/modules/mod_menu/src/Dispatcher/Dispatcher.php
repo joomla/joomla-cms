4c17b2e5be1478e4ecb21718280d169ef5e4bf18
Richard Fath <richard.fath@t-online.de>
2025-08-12 12:03:32 +0200

    Use database from container and fix menuType table instantiation

--- administrator/modules/mod_menu/src/Dispatcher/Dispatcher.php
+++ administrator/modules/mod_menu/src/Dispatcher/Dispatcher.php
@@ -11,6 +11,7 @@
 namespace Joomla\Module\Menu\Administrator\Dispatcher;
 
 use Joomla\CMS\Dispatcher\AbstractModuleDispatcher;
+use Joomla\Database\DatabaseInterface;
 use Joomla\Module\Menu\Administrator\Menu\CssMenu;
 
 // phpcs:disable PSR1.Files.SideEffects
@@ -33,11 +34,12 @@ class Dispatcher extends AbstractModuleDispatcher
      */
     protected function getLayoutData()
     {
+        $db   = Factory::getContainer()->get(DatabaseInterface::class);
         $data = parent::getLayoutData();
 
         $data['enabled'] = !$data['app']->getInput()->getBool('hidemainmenu');
 
-        $data['menu']        = new CssMenu($data['app']);
+        $data['menu']        = new CssMenu($data['app'], $db);
         $data['root']        = $data['menu']->load($data['params'], $data['enabled']);
         $data['root']->level = 0;
 
