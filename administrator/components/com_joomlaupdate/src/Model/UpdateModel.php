a0a067b0ed77a7c6d66a42f4b872e3273d28a149
BrianTeeman <brian@teeman.net>
2025-08-14 13:08:11 +0100

    [5.4] php_minimum
    To return the value of php_minimum from the updateInformation['object'], you need to access the php_minimum property correctly. Since php_minimum is a protected property of the Update object, you cannot access it directly using $updateInformation['object']->php_minimum. Instead, you need to use the get() method provided by the Update class.
    
    Signed-off-by: BrianTeeman <brian@teeman.net>

--- administrator/components/com_joomlaupdate/src/Model/UpdateModel.php
+++ administrator/components/com_joomlaupdate/src/Model/UpdateModel.php
@@ -1558,9 +1558,13 @@ ENDDATA;
     {
         $updateInformation = $this->getUpdateInformation();
 
-        return isset($updateInformation['object']->php_minimum) ?
-            $updateInformation['object']->php_minimum->_data :
-            JOOMLA_MINIMUM_PHP;
+        // Check if php_minimum exists and return its value
+        if (isset($updateInformation['object']) && $updateInformation['object']->get('php_minimum')) {
+            return $updateInformation['object']->get('php_minimum')->_data;
+        }
+
+        // Fallback to JOOMLA_MINIMUM_PHP if php_minimum is not set
+        return JOOMLA_MINIMUM_PHP;
     }
 
     /**
