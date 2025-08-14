caf240f1a5b966dbeec311f614eba41d92ff5912
Benjamin Trenkle <benjamin.trenkle@wicked-software.de>
2025-08-14 14:37:17 +0200

    Add category permission support

--- libraries/src/MVC/View/FormView.php
+++ libraries/src/MVC/View/FormView.php
@@ -223,7 +223,8 @@ class FormView extends HtmlView
         // For new records, check the create permission.
         if ($isNew) {
 
-            if ($canDo->get('core.create')) {
+            if ($canDo->get('core.create') || (isset($this->categorySection) && \count($user->getAuthorisedCategories($this->categorySection, 'core.create')) > 0)) {
+
                 $toolbar->apply($viewName . '.apply');
 
                 $saveGroup = $toolbar->dropdownButton('save-group');
