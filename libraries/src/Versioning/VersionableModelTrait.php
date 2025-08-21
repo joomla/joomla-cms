f4d64797864ca4c8fe99eff6c991ef1931ca429a
Robert Deutz <rd@rdbs.de>
2025-08-21 15:42:05 +0200

    move functionality to trait

--- libraries/src/Versioning/VersionableModelTrait.php
+++ libraries/src/Versioning/VersionableModelTrait.php
@@ -9,8 +9,14 @@
 
 namespace Joomla\CMS\Versioning;
 
+use Joomla\CMS\Component\ComponentHelper;
+use Joomla\CMS\Event\AbstractEvent;
 use Joomla\CMS\Factory;
+use Joomla\CMS\Plugin\PluginHelper;
 use Joomla\CMS\Table\ContentHistory;
+use Joomla\CMS\Table\ContentType;
+use Joomla\CMS\Workflow\WorkflowServiceInterface;
+use Joomla\Database\ParameterType;
 use Joomla\Utilities\ArrayHelper;
 
 // phpcs:disable PSR1.Files.SideEffects
@@ -24,6 +30,22 @@ use Joomla\Utilities\ArrayHelper;
  */
 trait VersionableModelTrait
 {
+    /**
+     * Fields to be ignored when calulation the hash.
+     *
+     * @var    array
+     * @since  __DEPLOY_VERSION__
+     */
+    protected $ignoreChanges = [];
+
+    /**
+     * Fields to be converted to int when calulation the hash.
+     *
+     * @var    array
+     * @since  __DEPLOY_VERSION__
+     */
+    protected $convertToInt = [];
+
     /**
      * Method to get the item id from the version history table.
      *
@@ -50,6 +72,7 @@ trait VersionableModelTrait
 
         return false;
     }
+
 
     /**
      * Method to get the version data from the version history table.
@@ -133,4 +156,217 @@ trait VersionableModelTrait
 
         return true;
     }
+
+    /**
+     * Utility method to get the hash after removing selected values. This lets us detect changes other than
+     * modified date (which will change on every save).
+     *
+     * @param   mixed        $jsonData   Either an object or a string with json-encoded data
+     *
+     * @return  string  SHA1 hash on success. Empty string on failure.
+     *
+     * @since   __DEPLOY_VERSION__
+     */
+    public function getSha1($jsonData)
+    {
+        $object = \is_object($jsonData) ? $jsonData : json_decode($jsonData);
+
+        foreach ($this->ignoreChanges as $remove) {
+            if (property_exists($object, $remove)) {
+                unset($object->$remove);
+            }
+        }
+
+        // Convert integers, booleans, and nulls to strings to get a consistent hash value
+        foreach ($object as $name => $value) {
+            if (\is_object($value)) {
+                // Go one level down for JSON column values
+                foreach ($value as $subName => $subValue) {
+                    $object->$subName = \is_int($subValue) || \is_bool($subValue) || $subValue === null ? (string) $subValue : $subValue;
+                }
+            } else {
+                $object->$name = \is_int($value) || \is_bool($value) || $value === null ? (string) $value : $value;
+            }
+        }
+
+        // Work around empty values
+        foreach ($this->convertToInt as $convert) {
+            if (isset($object->$convert)) {
+                $object->$convert = (int) $object->$convert;
+            }
+        }
+
+        if (isset($object->review_time)) {
+            $object->review_time = (int) $object->review_time;
+        }
+
+        return sha1(json_encode($object));
+    }
+
+    /**
+     * Setter for the value
+     *
+     * @param   array  $ignoreChanges
+     *
+     * @return  void
+     *
+     * @since   __DEPLOY_VERSION__
+     */
+    public function setIgnoreChanges(array $ignoreChanges): void
+    {
+        $this->ignoreChanges = $ignoreChanges;
+    }
+
+    /**
+     * Setter for the value
+     *
+     * @param   array  $convertToInt
+     *
+     * @return  void
+     *
+     * @since   __DEPLOY_VERSION__
+     */
+    public function setConvertToInt(array $convertToInt): void
+    {
+        $this->convertToInt = $convertToInt;
+    }
+
+    /**
+     * Method to save the history.
+     *
+     * @param   array   $data     The form data.
+     * @param   string  $context  The model context.
+     *
+     * @return  boolean  True on success, False on error.
+     *
+     * @since   __DEPLOY_VERSION__
+     */
+    public function saveHistory(array $data, string $context)
+    {
+        $id = $this->getState($this->getName() . '.id');
+
+        $versionNote = \array_key_exists('version_note', $data) ? $data['version_note'] : '';
+        $hash = $this->getSha1($data);
+
+        $result = $this->store($context, $id, ArrayHelper::toObject($data), $versionNote, $hash);
+
+        return $result;
+    }
+
+    /**
+     * Method to delete the history for an item.
+     *
+     * @param   string   $typeAlias  Typealias of the component
+     * @param   integer  $id         ID of the content item to delete
+     *
+     * @return  boolean  true on success, otherwise false.
+     *
+     * @since   __DEPLOY_VERSION__
+     */
+    public function delete($typeAlias, $id)
+    {
+        $db     = $this->getDatabase();
+        $itemid = $typeAlias . '.' . $id;
+        $query  = $db->createQuery();
+        $query->delete($db->quoteName('#__history'))
+            ->where($db->quoteName('item_id') . ' = :item_id')
+            ->bind(':item_id', $itemid, ParameterType::STRING);
+        $db->setQuery($query);
+
+        return $db->execute();
+    }
+
+    /**
+     * Method to save a version snapshot to the content history table.
+     *
+     * @param   string   $typeAlias  Typealias of the content type
+     * @param   integer  $id         ID of the content item
+     * @param   mixed    $data       Array or object of data that can be
+     *                               en- and decoded into JSON
+     * @param   string   $note       Note for the version to store
+     * @param   string   $hash
+     *
+     * @return  boolean  True on success, otherwise false.
+     *
+     * @since   __DEPLOY_VERSION__
+     * @throws \Exception
+     */
+    public function store(string $typeAlias, int $id, mixed $data, string $note = '', string $hash = '')
+    {
+        $typeTable = new ContentType($this->getDatabase());
+        $typeTable->load(['type_alias' => $typeAlias]);
+
+        $historyTable          = new ContentHistory($this->getDatabase());
+        $historyTable->item_id = $typeAlias . '.' . $id;
+
+        [$extension, $type] =  explode('.', $typeAlias);
+
+        // Don't store unless we have a non-zero item id
+        if (!$historyTable->item_id) {
+            return true;
+        }
+
+        // We should allow workflow items interact with the versioning
+        $component = Factory::getApplication()->bootComponent($extension);
+
+        if ($component instanceof WorkflowServiceInterface && $component->isWorkflowActive($typeAlias)) {
+            PluginHelper::importPlugin('workflow');
+
+            // Pre-processing by observers
+            $event = AbstractEvent::create(
+                'onContentVersioningPrepareTable',
+                [
+                    'subject'   => $historyTable,
+                    'extension' => $typeAlias,
+                ]
+            );
+
+            Factory::getApplication()->getDispatcher()->dispatch('onContentVersioningPrepareTable', $event);
+        }
+
+        // Fix for null ordering - set to 0 if null
+        if (\is_object($data)) {
+            if (property_exists($data, 'ordering') && $data->ordering === null) {
+                $data->ordering = 0;
+            }
+        } elseif (\is_array($data)) {
+            if (\array_key_exists('ordering', $data) && $data['ordering'] === null) {
+                $data['ordering'] = 0;
+            }
+        }
+
+        $historyTable->version_data = json_encode($data);
+        $historyTable->version_note = $note;
+
+        // Don't save if hash already exists and same version note
+        $historyTable->sha1_hash = $hash;
+
+        $hashMatch = $historyRow = $historyTable->getHashMatch();
+
+        if ($hashMatch) {
+            if (!$note || ($historyRow->version_note === $note)) {
+                return true;
+            }
+
+            // Update existing row to set version note
+            $historyTable->version_id = $historyRow->version_id;
+        }
+
+        $result = $historyTable->store();
+
+        // Load history_limit config from extension.
+        $context = $type ?? '';
+
+        $maxVersionsContext = ComponentHelper::getParams($extension)->get('history_limit_' . $context, 0);
+        $maxVersions        = ComponentHelper::getParams($extension)->get('history_limit', 0);
+
+        if ($maxVersionsContext) {
+            $historyTable->deleteOldVersions($maxVersionsContext);
+        } elseif ($maxVersions) {
+            $historyTable->deleteOldVersions($maxVersions);
+        }
+
+        return $result;
+    }
+
 }
