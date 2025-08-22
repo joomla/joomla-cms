77c1e5ce8b11d62a1a14b611918557ec2b6d904e
David Jardin <d.jardin@djumla.de>
2025-08-22 10:48:08 +0200

    prevent usage of TUF targets that are older than the current version

--- libraries/src/Updater/Update.php
+++ libraries/src/Updater/Update.php
@@ -562,6 +562,11 @@ class Update
         $constraintChecker = new ConstraintChecker();
 
         foreach ($data['signed']['targets'] as $target) {
+            // Check if this target is older than the currently installed version
+            if (version_compare($target['custom']['version'], JVERSION, '<')) {
+                continue;
+            }
+
             // Check if this target is newer than the current version
             if (isset($this->latest) && version_compare($target['custom']['version'], $this->latest->version, '<')) {
                 continue;
