365cd2ea159a6a795818650343da28b4a58da2f7
BrianTeeman <brian@teeman.net>
2025-08-27 12:30:32 +0100

    sql

---
+++ administrator/components/com_admin/sql/updates/postgresql/6.0.0-2025-08-27.sql
@@ -0,0 +1,2 @@
+INSERT INTO "#__mail_templates" ("template_id", "extension", "language", "subject", "body", "htmlbody", "attachments", "params") VALUES
+('com_actionlogs.notification', 'com_actionlogs', '', 'COM_ACTIONLOGS_EMAIL_SUBJECT', 'COM_ACTIONLOGS_EMAIL_BODY', 'COM_ACTIONLOGS_EMAIL_HTMLBODY', '', '{"tags":["messages","message","date","extension","username","ip_address"]}'),
