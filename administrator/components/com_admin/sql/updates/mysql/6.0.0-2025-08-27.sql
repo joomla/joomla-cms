731963ff479dd77fa437335b903e91b09a365c75
BrianTeeman <brian@teeman.net>
2025-08-27 14:24:45 +0100

    mysql

--- administrator/components/com_admin/sql/updates/mysql/6.0.0-2025-08-27.sql
+++ administrator/components/com_admin/sql/updates/mysql/6.0.0-2025-08-27.sql
@@ -1,2 +1,17 @@
-INSERT INTO `#__mail_templates` (`template_id`, `extension`, `language`, `subject`, `body`, `htmlbody`, `attachments`, `params`) VALUES
-('com_actionlogs.notification', 'com_actionlogs', '', 'COM_ACTIONLOGS_EMAIL_SUBJECT', 'COM_ACTIONLOGS_EMAIL_BODY', 'COM_ACTIONLOGS_EMAIL_HTMLBODY', '', '{"tags":["messages","message","date","extension","username","ip_address"]}'),
+--
+-- Add a template tag for ip_address on Joomla update
+-- only when a value is not already set.
+-- New installs will have the default value set in the installation support sql.
+--
+
+UPDATE `#_mail_templates`
+SET `params` = JSON_SET(
+    CAST(`params` AS JSON),
+    CONCAT('$.tags[', JSON_LENGTH(JSON_EXTRACT(CAST(`params` AS JSON), '$.tags')), ']'),
+    'ip_address'
+)
+WHERE `template_id` = 'com_actionlogs.notification'
+  AND JSON_CONTAINS(
+        JSON_EXTRACT(CAST(`params` AS JSON), '$.tags'),
+        JSON_QUOTE('ip_address')
+      ) = 0;
