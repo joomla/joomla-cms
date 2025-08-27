486ba59230ac5dc0749e206403ff745226b3c85c
BrianTeeman <brian@teeman.net>
2025-08-27 21:46:59 +0100

    oops

--- administrator/components/com_admin/sql/updates/postgresql/6.0.0-2025-03-25.sql
+++ administrator/components/com_admin/sql/updates/postgresql/6.0.0-2025-03-25.sql
@@ -1,18 +1,2 @@
---
--- Add a template tag for ip_address on Joomla update
--- only when a value is not already set.
--- New installs will have the default value set in the installation support sql.
---
-UPDATE #__mail_templates
-SET params = (
-    params::jsonb || jsonb_build_object(
-        'tags',
-        (params::jsonb -> 'tags') || '"ip_address"'::jsonb
-    )
-)::text
-WHERE template_id = 'com_actionlogs.notification'
-  AND NOT EXISTS (
-      SELECT 1
-      FROM jsonb_array_elements_text(params::jsonb -> 'tags') AS tag
-      WHERE tag = 'ip_address'
-  );
+INSERT INTO "#__mail_templates" ("template_id", "extension", "language", "subject", "body", "htmlbody", "attachments", "params") VALUES
+('plg_content_joomla.newarticle', 'plg_content_joomla', '', 'PLG_CONTENT_JOOMLA_NEW_ARTICLE_SUBJECT', 'PLG_CONTENT_JOOMLA_NEW_ARTICLE_BODY', '', '', '{"tags":["sitename","name","email","title","url"]}');
