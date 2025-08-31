401b41a86c834d87cae2bd158d7970341afdb69b
Robert Deutz <rd@rdbs.de>
2025-08-31 14:29:53 +0200

    Revert "delete no longer used events"
    This reverts commit 88921b70f4ce9517cfb561b84ceced093076aa68.

---
+++ libraries/src/Event/Table/BeforeDeleteEvent.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Joomla! Content Management System
+ *
+ * @copyright  (C) 2016 Open Source Matters, Inc. <https://www.joomla.org>
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+namespace Joomla\CMS\Event\Table;
+
+// phpcs:disable PSR1.Files.SideEffects
+\defined('_JEXEC') or die;
+// phpcs:enable PSR1.Files.SideEffects
+
+/**
+ * Event class for \Joomla\CMS\Table\Table onBeforeDelete event
+ *
+ * @since  4.0.0
+ */
+class BeforeDeleteEvent extends AbstractEvent
+{
+    /**
+     * Constructor.
+     *
+     * Mandatory arguments:
+     * subject      \Joomla\CMS\Table\TableInterface The table we are operating on
+     * pk           An optional primary key value to delete.
+     *
+     * @param   string  $name       The event name.
+     * @param   array   $arguments  The event arguments.
+     *
+     * @throws  \BadMethodCallException
+     */
+    public function __construct($name, array $arguments = [])
+    {
+        if (!\array_key_exists('pk', $arguments)) {
+            throw new \BadMethodCallException("Argument 'pk' is required for event $name");
+        }
+
+        parent::__construct($name, $arguments);
+    }
+}
