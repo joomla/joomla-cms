3d2add3e507464174ed25fa5c09d6f6d897aa9fe
Nicola Galgano <optimus4joomla@gmail.com>
2025-09-05 15:32:31 +0000

    cs

--- libraries/src/MVC/Model/AdminModel.php
+++ libraries/src/MVC/Model/AdminModel.php
@@ -968,15 +968,15 @@ abstract class AdminModel extends FormModel
                     $this->setError($table->getError());
 
                     return false;
-                }      
+                }
                 if (Factory::getApplication()->isClient('api')) {
-                        $session = Factory::getApplication()->getSession();
-                        $session->set('http_status_code_409', true);
+                    $session = Factory::getApplication()->getSession();
+                    $session->set('http_status_code_409', true);
                 }
 
                 Log::add(Text::_('JLIB_APPLICATION_ERROR_DELETE_NOT_PERMITTED'), Log::WARNING, 'jerror');
 
-                return true;  
+                return true;
             }
         }
 
