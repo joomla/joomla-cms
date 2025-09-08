10e8f8c484980a88a298faf394c6eee8b688cd96
Jonathan Brain <3941269+BrainforgeUK@users.noreply.github.com>
2025-09-08 19:32:17 +0100

    Added after package download events.

---
+++ libraries/src/Event/Installer/AfterPackageDownloadEvent.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Joomla! Content Management System
+ *
+ * @copyright  (C) 2023 Open Source Matters, Inc. <https://www.joomla.org>
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+namespace Joomla\CMS\Event\Installer;
+
+use Joomla\Event\Event;
+
+// phpcs:disable PSR1.Files.SideEffects
+\defined('_JEXEC') or die;
+// phpcs:enable PSR1.Files.SideEffects
+
+/**
+ * Class for Installer events
+ *
+ * @since  6.1.0
+ */
+class AfterPackageDownloadEvent extends Event
+{
+}
