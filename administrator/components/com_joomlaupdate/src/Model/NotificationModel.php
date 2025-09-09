69feeff25bec5801a4ef6a390f5653ae751ee751
David Jardin <d.jardin@djumla.de>
2025-09-09 12:51:39 +0200

    fix language handling for multiple users

--- administrator/components/com_joomlaupdate/src/Model/NotificationModel.php
+++ administrator/components/com_joomlaupdate/src/Model/NotificationModel.php
@@ -14,6 +14,7 @@ use Joomla\CMS\Access\Access;
 use Joomla\CMS\Component\ComponentHelper;
 use Joomla\CMS\Factory;
 use Joomla\CMS\Helper\UserGroupsHelper;
+use Joomla\CMS\Language\LanguageFactoryInterface;
 use Joomla\CMS\Mail\MailHelper;
 use Joomla\CMS\Mail\MailTemplate;
 use Joomla\CMS\MVC\Model\BaseDatabaseModel;
@@ -71,9 +72,8 @@ final class NotificationModel extends BaseDatabaseModel
             $emailReceivers  = $this->getEmailReceivers($superUserGroups);
         }
 
-        $app        = Factory::getApplication();
-        $jLanguage  = $app->getLanguage();
-        $sitename   = $app->get('sitename');
+        $app       = Factory::getApplication();
+        $sitename  = $app->get('sitename');
 
         $substitutions = [
             'oldversion' => $oldVersion,
@@ -82,21 +82,46 @@ final class NotificationModel extends BaseDatabaseModel
             'url'        => Uri::root(),
         ];
 
-        // Determine the default admin language
-        $defaultLanguage = ComponentHelper::getParams('com_languages')->get('administrator', 'en-GB');
+        // Determine the default admin language and load the language file for the fallback and default language
+        $defaultLocale = ComponentHelper::getParams('com_languages')->get('administrator', 'en-GB');
+        $defaultLanguage = $app->getLanguage();
+        $defaultLanguage->load('com_joomlaupdate', JPATH_ADMINISTRATOR, 'en-GB', true, true);
+        $defaultLanguage->load('com_joomlaupdate', JPATH_ADMINISTRATOR, $defaultLocale);
 
         // Send emails to all receivers
         foreach ($emailReceivers as $receiver) {
             $receiverParams   = new Registry($receiver->params);
-            $receiverLanguage = $receiverParams->get('admin_language', $defaultLanguage);
+            $receiverLocale = $receiverParams->get('admin_language', $defaultLocale);
 
-            $jLanguage->load('com_joomlaupdate', JPATH_ADMINISTRATOR, 'en-GB', true, true);
-            $jLanguage->load('com_joomlaupdate', JPATH_ADMINISTRATOR, $receiverLanguage);
+            // Temporarily set application language to user's language.
+            if ($receiverLocale !== $defaultLocale) {
+                $receiverLanguage = Factory::getContainer()
+                    ->get(LanguageFactoryInterface::class)
+                    ->createLanguage($receiverLocale, $app->get('debug_lang', false));
 
-            $mailer = new MailTemplate('com_joomlaupdate.update.' . $type, $receiverLanguage);
+                Factory::$language = $receiverLanguage;
+
+                if (method_exists($app, 'loadLanguage')) {
+                    $app->loadLanguage($receiverLanguage);
+                }
+
+                $receiverLanguage->load('com_joomlaupdate', JPATH_ADMINISTRATOR, $receiverLocale);
+            }
+
+            $mailer = new MailTemplate('com_joomlaupdate.update.' . $type, $receiverLocale);
             $mailer->addRecipient($receiver->email);
             $mailer->addTemplateData($substitutions);
             $mailer->send();
+
+            // Set application language back to default if we changed it
+            if ($receiverLocale !== $defaultLocale) {
+                Factory::$language = $defaultLanguage;
+
+                if (method_exists($app, 'loadLanguage'))
+                {
+                    $app->loadLanguage($defaultLanguage);
+                }
+            }
         }
     }
 
