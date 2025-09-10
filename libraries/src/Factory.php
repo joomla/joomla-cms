70cb0c4cf7243def8206a761cd8d6020406b3b80
Harald Leithner <leithner@itronic.at>
2025-09-10 22:29:54 +0200

    Readd getSession method and disable it by compat plugin

--- libraries/src/Factory.php
+++ libraries/src/Factory.php
@@ -21,6 +21,7 @@ use Joomla\CMS\Language\Language;
 use Joomla\CMS\Language\LanguageFactoryInterface;
 use Joomla\CMS\Mail\Mail;
 use Joomla\CMS\Mail\MailerFactoryInterface;
+use Joomla\CMS\Session\Session;
 use Joomla\CMS\User\User;
 use Joomla\Database\DatabaseDriver;
 use Joomla\Database\DatabaseInterface;
@@ -228,6 +229,45 @@ abstract class Factory
         }
 
         return self::$container;
+    }
+
+    /**
+     * Get a session object.
+     *
+     * Returns the global {@link Session} object, only creating it if it doesn't already exist.
+     *
+     * @param   array  $options  An array containing session options
+     *
+     * @return  Session object
+     *
+     * @see         Session
+     * @since       1.7.0
+     *
+     * @deprecated  4.3 will be removed in 7.0
+     *              Use the session service in the DI container or get from the application object
+     *              Example:
+     *              Factory::getApplication()->getSession();
+     */
+    public static function getSession(array $options = [])
+    {
+        if (!\defined('JCOMPAT6_FACTORY_GET_SESSION')) {
+            throw new \BadMethodCallException(\sprintf(
+                '%1$s() has been removed and compatibility plugin is disabled. Load the session from the dependency injection container or via %2$s::getApplication()->getSession().',
+                __METHOD__,
+                __CLASS__
+            ), 500);
+        }
+
+        @trigger_error(
+            \sprintf(
+                '%1$s() is deprecated. Load the session from the dependency injection container or via %2$s::getApplication()->getSession().',
+                __METHOD__,
+                __CLASS__
+            ),
+            E_USER_DEPRECATED
+        );
+
+        return self::getApplication()->getSession();
     }
 
     /**
