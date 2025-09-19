bff6ce3db6c6a9f3b61a44f29baa423c7f131a64
Richard Fath <richard67@users.noreply.github.com>
2025-09-19 09:36:15 +0200

    Rename postgresql 5.3.0-2025-08-12.sql to 5.3.4-2025-09-19.sql

--- administrator/components/com_admin/sql/updates/postgresql/5.3.4-2025-09-19.sql
+++ administrator/components/com_admin/sql/updates/postgresql/5.3.4-2025-09-19.sql
@@ -5,3 +5,4 @@ WHERE "type_alias" = 'com_content.article';
 UPDATE "#__content_types"
 SET "field_mappings" = jsonb_set("field_mappings"::jsonb, '{common, core_created_by_alias}' , '"created_by_alias"', true)
 WHERE "type_alias" = 'com_content.article';
+
