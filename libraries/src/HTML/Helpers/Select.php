2d5706a12066daf703eb1cd10f47f94a9c130e6e
Dimitris Grammatikogiannis <dg@dgrammatiko.dev>
2025-09-20 18:01:34 +0300

    PHP8.5 Deprecations

--- libraries/src/HTML/Helpers/Select.php
+++ libraries/src/HTML/Helpers/Select.php
@@ -488,46 +488,46 @@ abstract class Select
                 $key  = $options['option.key'] === null ? $elementKey : $element[$options['option.key']];
                 $text = $element[$options['option.text']];
 
-                if (isset($element[$options['option.attr']])) {
+                if ($options['option.attr'] && isset($element[$options['option.attr']])) {
                     $attr = $element[$options['option.attr']];
                 }
 
-                if (isset($element[$options['option.id']])) {
+                if ($options['option.id'] && isset($element[$options['option.id']])) {
                     $id = $element[$options['option.id']];
                 }
 
-                if (isset($element[$options['option.label']])) {
+                if ($options['option.label'] && isset($element[$options['option.label']])) {
                     $label = $element[$options['option.label']];
                 }
 
-                if (isset($element[$options['option.disable']]) && $element[$options['option.disable']]) {
+                if ($options['option.disable'] && isset($element[$options['option.disable']]) && $element[$options['option.disable']]) {
                     $extra .= ' disabled="disabled"';
                 }
             } elseif (\is_object($element)) {
                 $key  = $options['option.key'] === null ? $elementKey : $element->{$options['option.key']};
                 $text = $element->{$options['option.text']};
 
-                if (isset($element->{$options['option.attr']})) {
+                if ($options['option.attr'] && isset($element->{$options['option.attr']})) {
                     $attr = $element->{$options['option.attr']};
                 }
 
-                if (isset($element->{$options['option.id']})) {
+                if ($options['option.id'] && isset($element->{$options['option.id']})) {
                     $id = $element->{$options['option.id']};
                 }
 
-                if (isset($element->{$options['option.label']})) {
+                if ($options['option.label'] && isset($element->{$options['option.label']})) {
                     $label = $element->{$options['option.label']};
                 }
 
-                if (isset($element->{$options['option.disable']}) && $element->{$options['option.disable']}) {
+                if ($options['option.disable'] && isset($element->{$options['option.disable']}) && $element->{$options['option.disable']}) {
                     $extra .= ' disabled="disabled"';
                 }
 
-                if (isset($element->{$options['option.class']}) && $element->{$options['option.class']}) {
+                if ($options['option.class'] && isset($element->{$options['option.class']}) && $element->{$options['option.class']}) {
                     $extra .= ' class="' . $element->{$options['option.class']} . '"';
                 }
 
-                if (isset($element->{$options['option.onclick']}) && $element->{$options['option.onclick']}) {
+                if ($options['option.onclick'] && isset($element->{$options['option.onclick']}) && $element->{$options['option.onclick']}) {
                     $extra .= ' onclick="' . $element->{$options['option.onclick']} . '"';
                 }
             } else {
