97ec15d75de53cb0955f847cd41da847c3a5acb9
Martina Scholz <martina@simplysmart-it.de>
2025-09-22 18:03:14 +0200

    Refactor urlVersioning function to export directly and clean up unused code

--- build/build-modules-js/css-versioning.mjs
+++ build/build-modules-js/css-versioning.mjs
@@ -1,9 +1,6 @@
 import { createHash } from 'node:crypto';
-import { readdir, readFile, writeFile } from 'node:fs/promises';
 import { existsSync, readFileSync } from 'node:fs';
 import { dirname, extname, resolve } from 'node:path';
-import { transform, composeVisitors } from 'lightningcss';
-import { Timer } from './utils/timer.mjs';
 
 const RootPath = process.cwd();
 const skipExternal = true;
@@ -37,7 +34,7 @@ function version(urlString, fromFile) {
  * @param {from: String} - the filepath for the css file
  * @returns {import('lightningcss').Visitor} - A visitor that replaces the url
  */
-const urlVersioning = (fromFile) => {
+export const urlVersioning = (fromFile) => {
   return {
     /**
      * @param {import('lightningcss').Url} url - The url object to transform
@@ -47,44 +44,4 @@ const urlVersioning = (fromFile) => {
       return { ...url, ...{ url: version(url.url, fromFile) } };
     },
   };
-}
-
-/**
- * Adds a hash to the url() parts of the static css
- *
- * @param file
- * @returns {Promise<void>}
- */
-const fixVersion = async (file) => {
-  try {
-    const { code } = transform({
-      code: await readFile(file),
-      minify: file.endsWith('.min.css'),
-      visitor: composeVisitors([urlVersioning(file)]),
-    });
-    await writeFile(file, `@charset "UTF-8";${file.endsWith('.min.css') ? '' : '\n'}${code}`, {
-      encoding: 'utf8',
-      mode: 0o644,
-    });
-  } catch (error) {
-    throw new Error(error);
-  }
 };
-
-/**
- * Loop the media folder and add version to all url() entries in all the css files
- *
- * @returns {Promise<void>}
- */
-export const cssVersioning = async () => {
-  const bench = new Timer('Versioning');
-
-  const cssFiles = (await readdir(`${RootPath}/media`, { withFileTypes: true, recursive: true }))
-    .filter((file) => (!file.isDirectory() && extname(file.name) === '.css'))
-    .map((file) => `${file.path}/${file.name}`);
-
-  Promise.all(cssFiles.map((file) => fixVersion(file)))
-    .then(() => bench.stop());
-};
-
-export { urlVersioning };
