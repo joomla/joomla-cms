65a2286ae6b33c972d3ff6345f7de0d218bec6aa
Nicola Galgano <optimus4joomla@gmail.com>
2025-09-26 10:29:43 +0200

    translatable error message

--- installation/template/index.php
+++ installation/template/index.php
@@ -49,6 +49,7 @@ Text::script('JLIB_JS_AJAX_ERROR_NO_CONTENT');
 Text::script('JLIB_JS_AJAX_ERROR_OTHER');
 Text::script('JLIB_JS_AJAX_ERROR_PARSE');
 Text::script('JLIB_JS_AJAX_ERROR_TIMEOUT');
+Text::script('INSTL_ADMIN_PASSWORD_LENGTH');
 Text::script('INSTL_DATABASE_RESPONSE_ERROR');
 
 // Add strings for installation progress
