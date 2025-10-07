655c2590729fbb5b68928958c9ad20b734194c5b
Richard Fath <richard67@users.noreply.github.com>
2025-10-07 09:19:41 +0200

    [CMS PR 46174] Update the update SQL scripts and for new installations supports.sql (#21)
    * Names quotes for column names
    
    * Remove some tabs
    
    * Use separate lines for closing brackets
    
    * Fix SQL error "operator does not exist"
    
    * Append only once to array
    
    * Update supports.sql for new installations

--- administrator/components/com_admin/sql/updates/mysql/6.0.0-2025-09-26.sql
+++ administrator/components/com_admin/sql/updates/mysql/6.0.0-2025-09-26.sql
@@ -1,16 +1,17 @@
 -- Update content types for lookup tags
 
 UPDATE `#__content_types`
-SET content_history_options = JSON_ARRAY_APPEND(
-	content_history_options,
+SET `content_history_options` = JSON_ARRAY_APPEND(
+	`content_history_options`,
 	'$.displayLookup',
 	JSON_OBJECT(
 		'sourceColumn', 'tags',
 		'targetTable', '#__tags',
 		'targetColumn', 'id',
 		'displayColumn', 'title'
-	))
-WHERE type_alias IN (
+		)
+	)
+WHERE `type_alias` IN (
 		 'com_content.article',
 		 'com_contact.contact',
 		 'com_newsfeeds.newsfeed',
@@ -19,4 +20,13 @@ WHERE type_alias IN (
 		 'com_newsfeeds.category',
 		 'com_banners.category',
 		 'com_users.category'
+	)
+AND NOT JSON_CONTAINS(
+	JSON_EXTRACT(`content_history_options`, '$.displayLookup'),
+	JSON_OBJECT(
+		'sourceColumn', 'tags',
+		'targetTable', '#__tags',
+		'targetColumn', 'id',
+		'displayColumn', 'title'
+		)
 	);
--- administrator/components/com_admin/sql/updates/postgresql/6.0.0-2025-09-26.sql
+++ administrator/components/com_admin/sql/updates/postgresql/6.0.0-2025-09-26.sql
@@ -1,18 +1,18 @@
 -- Update content types for lookup tags
 
 UPDATE "#__content_types"
-SET content_history_options = jsonb_set(
-	content_history_options::jsonb,
+SET "content_history_options" = jsonb_set(
+	"content_history_options"::jsonb,
 	'{displayLookup}',
-	(content_history_options->'displayLookup')::jsonb ||
+	"content_history_options"::jsonb->'displayLookup' ||
 		jsonb_build_object(
 		'sourceColumn', 'tags',
 		'targetTable', '#__tags',
 		'targetColumn', 'id',
 		'displayColumn', 'title'
 		)
-				)
-WHERE type_alias IN (
+	)
+WHERE "type_alias" IN (
 		 'com_content.article',
 		 'com_contact.contact',
 		 'com_newsfeeds.newsfeed',
@@ -21,4 +21,13 @@ WHERE type_alias IN (
 		 'com_newsfeeds.category',
 		 'com_banners.category',
 		 'com_users.category'
+	)
+AND NOT EXISTS (
+	SELECT * FROM jsonb_array_elements("content_history_options"::jsonb->'displayLookup')
+	WHERE value = jsonb_build_object(
+		'sourceColumn', 'tags',
+		'targetTable', '#__tags',
+		'targetColumn', 'id',
+		'displayColumn', 'title'
+		)
 	);
