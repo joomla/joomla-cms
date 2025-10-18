joomla/joomla-cms

commit 595008c7c66a232d9f1fe4d64bfc268303c47cd7

[5.4] Fix deprecated direct property access of model state (#45704)
* [5.4] Remove direct property access of model state
* Update phpstan-baseline.neon

components/com_content/src/Model/CategoryModel.php
@@ -280,7 +280,7 @@ class CategoryModel extends ListModel
     {
         $app       = Factory::getApplication();
         $db        = $this->getDatabase();
-        $params    = $this->state->params;
+        $params    = $this->state->get('params');
         $itemid    = $app->getInput()->get('id', 0, 'int') . ':' . $app->getInput()->get('Itemid', 0, 'int');
         $orderCol  = $app->getUserStateFromRequest('com_content.category.list.' . $itemid . '.filter_order', 'filter_order', '', 'string');
         $orderDirn = $app->getUserStateFromRequest('com_content.category.list.' . $itemid . '.filter_order_Dir', 'filter_order_Dir', '', 'cmd');
@@ -335,8 +335,8 @@ class CategoryModel extends ListModel
     public function getCategory()
     {
         if (!\is_object($this->_item)) {
-            if (isset($this->state->params)) {
-                $params                = $this->state->params;
+            if (!empty($this->state->get('params'))) {
+                $params                = $this->state->get('params');
                 $options               = [];
                 $options['countItems'] = $params->get('show_cat_num_articles', 1) || !$params->get('show_empty_categories_cat', 0);
                 $options['access']     = $params->get('check_access_rights', 1);

administrator/components/com_banners/src/View/Banner/HtmlView.php
@@ -138,7 +138,7 @@ class HtmlView extends BaseHtmlView
         } else {
             $toolbar->cancel('banner.cancel');
 
-            if (ComponentHelper::isEnabled('com_contenthistory') && $this->state->params->get('save_history', 0) && $canDo->get('core.edit')) {
+            if (ComponentHelper::isEnabled('com_contenthistory') && $this->state->get('params')->get('save_history', 0) && $canDo->get('core.edit')) {
                 $toolbar->versions('com_banners.banner', $this->item->id);
             }
         }

administrator/components/com_banners/src/View/Client/HtmlView.php
@@ -147,7 +147,7 @@ class HtmlView extends BaseHtmlView
         } else {
             $toolbar->cancel('client.cancel');
 
-            if (ComponentHelper::isEnabled('com_contenthistory') && $this->state->params->get('save_history', 0) && $canDo->get('core.edit')) {
+            if (ComponentHelper::isEnabled('com_contenthistory') && $this->state->get('params')->get('save_history', 0) && $canDo->get('core.edit')) {
                 $toolbar->versions('com_banners.client', $this->item->id);
             }
         }

administrator/components/com_contact/src/View/Contact/HtmlView.php
@@ -193,7 +193,7 @@ class HtmlView extends BaseHtmlView
 
             $toolbar->cancel('contact.cancel');
 
-            if (ComponentHelper::isEnabled('com_contenthistory') && $this->state->params->get('save_history', 0) && $itemEditable) {
+            if (ComponentHelper::isEnabled('com_contenthistory') && $this->state->get('params')->get('save_history', 0) && $itemEditable) {
                 $toolbar->versions('com_contact.contact', $this->item->id);
             }
 

administrator/components/com_content/src/View/Article/HtmlView.php
@@ -230,7 +230,7 @@ class HtmlView extends BaseHtmlView
             $toolbar->cancel('article.cancel', 'JTOOLBAR_CLOSE');
 
             if (!$isNew) {
-                if (ComponentHelper::isEnabled('com_contenthistory') && $this->state->params->get('save_history', 0) && $itemEditable) {
+                if (ComponentHelper::isEnabled('com_contenthistory') && $this->state->get('params')->get('save_history', 0) && $itemEditable) {
                     $toolbar->versions('com_content.article', $this->item->id);
                 }
 

administrator/components/com_contenthistory/src/View/History/HtmlView.php
@@ -105,7 +105,7 @@ class HtmlView extends BaseHtmlView
 
         // Clean up input to ensure a clean url.
         $filter     = InputFilter::getInstance();
-        $aliasArray = explode('.', $this->state->item_id);
+        $aliasArray = explode('.', $this->state->get('item_id'));
 
         if ($aliasArray[1] === 'category') {
             $option = 'com_categories';

administrator/components/com_finder/src/View/Searches/HtmlView.php
@@ -112,7 +112,7 @@ class HtmlView extends BaseHtmlView
         $this->state         = $model->getState();
         $this->filterForm    = $model->getFilterForm();
         $this->activeFilters = $model->getActiveFilters();
-        $this->enabled       = $this->state->params->get('gather_search_statistics', 0);
+        $this->enabled       = $this->state->get('params')->get('gather_search_statistics', 0);
         $this->canDo         = ContentHelper::getActions('com_finder');
         $uri                 = Uri::getInstance();
         $link                = 'index.php?option=com_config&view=component&component=com_finder&return=' . base64_encode($uri);

administrator/components/com_newsfeeds/src/View/Newsfeed/HtmlView.php
@@ -171,7 +171,7 @@ class HtmlView extends BaseHtmlView
         } else {
             $toolbar->cancel('newsfeed.cancel');
 
-            if (ComponentHelper::isEnabled('com_contenthistory') && $this->state->params->get('save_history', 0) && $canDo->get('core.edit')) {
+            if (ComponentHelper::isEnabled('com_contenthistory') && $this->state->get('params')->get('save_history', 0) && $canDo->get('core.edit')) {
                 $toolbar->versions('com_newsfeeds.newsfeed', $this->item->id);
             }
         }

commit 8c8290de8b3d5893582fc3b716224b42d16f103b

[5.4] check if state is initialized in content CategoryModel
regression from #45704

components/com_content/src/Model/CategoryModel.php
@@ -335,7 +335,7 @@ class CategoryModel extends ListModel
     public function getCategory()
     {
         if (!\is_object($this->_item)) {
-            if (!empty($this->state->get('params'))) {
+            if (isset($this->state) && !empty($this->state->get('params'))) {
                 $params                = $this->state->get('params');
                 $options               = [];
                 $options['countItems'] = $params->get('show_cat_num_articles', 1) || !$params->get('show_empty_categories_cat', 0);