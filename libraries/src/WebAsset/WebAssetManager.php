f1902a65f9c0d61aa08fb686d080c82f801f8227
Fedik <getthesite@gmail.com>
2025-10-19 15:01:32 +0300

    WebAsset fix type check to be interface instead of class

--- libraries/src/WebAsset/WebAssetManager.php
+++ libraries/src/WebAsset/WebAssetManager.php
@@ -753,14 +753,14 @@ class WebAssetManager implements WebAssetManagerInterface
     /**
      * Update Dependencies state for all active Assets or only for given
      *
-     * @param   ?string        $type   The asset type, script or style
-     * @param   ?WebAssetItem  $asset  The asset instance to which need to enable dependencies
+     * @param   ?string                 $type   The asset type, script or style
+     * @param   ?WebAssetItemInterface  $asset  The asset instance to which need to enable dependencies
      *
      * @return  self
      *
      * @since  4.0.0
      */
-    protected function enableDependencies(?string $type = null, ?WebAssetItem $asset = null): self
+    protected function enableDependencies(?string $type = null, ?WebAssetItemInterface $asset = null): self
     {
         if ($type === 'preset') {
             // Preset items already was enabled by usePresetItems()
@@ -971,11 +971,11 @@ class WebAssetManager implements WebAssetManagerInterface
     /**
      * Return dependencies for Asset as array of WebAssetItem objects
      *
-     * @param   string         $type           The asset type, script or style
-     * @param   WebAssetItem   $asset          Asset instance
-     * @param   boolean        $recursively    Whether to search for dependency recursively
-     * @param   ?string        $recursionType  The type of initial item to prevent loop
-     * @param   ?WebAssetItem  $recursionRoot  Initial item to prevent loop
+     * @param   string                  $type           The asset type, script or style
+     * @param   WebAssetItemInterface   $asset          Asset instance
+     * @param   boolean                 $recursively    Whether to search for dependency recursively
+     * @param   ?string                 $recursionType  The type of initial item to prevent loop
+     * @param   ?WebAssetItemInterface  $recursionRoot  Initial item to prevent loop
      *
      * @return  array
      *
@@ -985,10 +985,10 @@ class WebAssetManager implements WebAssetManagerInterface
      */
     protected function getDependenciesForAsset(
         string $type,
-        WebAssetItem $asset,
+        WebAssetItemInterface $asset,
         $recursively = false,
         ?string $recursionType = null,
-        ?WebAssetItem $recursionRoot = null
+        ?WebAssetItemInterface $recursionRoot = null
     ): array {
         $assets        = [];
         $recursionRoot ??= $asset;
