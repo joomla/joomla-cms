b62bc0f5cb5d4728996e22d794869dc14485835f
Cyril Rezé <cyril.reze@joomlic.com>
2025-10-19 14:24:05 +0200

    Update error message for pre-update check

--- administrator/components/com_joomlaupdate/tmpl/joomlaupdate/preupdatecheck.php
+++ administrator/components/com_joomlaupdate/tmpl/joomlaupdate/preupdatecheck.php
@@ -175,7 +175,7 @@ if (version_compare($this->updateInfo['latest'], Version::MAJOR_VERSION + 1, '>=
                                     <?php if ($option->state) : ?>
                                         <span class="icon-checkmark-2 text-success" aria-hidden="true"></span> <span><?php echo Text::_('JOK'); ?></span>
                                     <?php else : ?>
-                                        <span class="icon-cancel-circle text-danger" aria-hidden="true"></span> <span class="fw-bold"><?php echo Text::_('ERROR'); ?></span>
+                                        <span class="icon-cancel-circle text-danger" aria-hidden="true"></span> <span class="fw-bold"><?php echo Text::_('COM_JOOMLAUPDATE_ACTION_REQUIRED'); ?></span>
                                     <?php endif; ?>
                                 </td>
                             </tr>
