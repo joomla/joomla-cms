a2bd1cd5a0275741afc0db21aac64ce439e3877b
Cliff Ford <cliff@ford.myzen.co.uk>
2025-10-28 15:30:54 +0000

    Fixed JavaScript affecting 'active' removal.

--- build/media_source/com_admin/js/admin-help.es6.js
+++ build/media_source/com_admin/js/admin-help.es6.js
@@ -7,46 +7,44 @@ document.addEventListener("DOMContentLoaded", function (event) {
   new MetisMenu('#helpmenu', {
     toggle: true
   });
-
   const helpIndex = document.getElementById('helpmenu');
   if (helpIndex) {
-    helpIndex.querySelectorAll('a').forEach(element =>
-      element.addEventListener('click', () => {
-        window.scroll(0, 0);
-        if (element.classList.contains('has-arrow')) {
-          // 🔸 Action for a link to a folder, where <a class="has-arrow">
+    helpIndex.querySelectorAll('a').forEach(element => element.addEventListener('click', () => {
+      window.scroll(0, 0);
+      if (element.classList.contains('has-arrow')) {
+        // 🔸 Action for a link to a folder, where <a class="has-arrow">
+        helpIndex.querySelectorAll('a.has-arrow').forEach(a => {
+          // Remove 'active' from all collapsed folders.
           if (element.classList.contains('mm-collapsed')) {
             element.classList.remove('active');
-          } else {
-            element.classList.add('active');
           }
-        } else {
-          // Action for a link to an article.
-          const id = element.dataset.id;
-          if (id) {
-            // First, reset all other links to default state
-            helpIndex.querySelectorAll('a:not(has-arrow)').forEach(a => {
-              if (a.dataset.id !== id) {
-                a.classList.remove('active');
-                a.removeAttribute('aria-current');
-              } else {
-                element.classList.add('active');
-                a.setAttribute('aria-current', 'page');
-              }
-            });
-          }
-          localStorage.setItem('helpIndex.lastClick', id);
-
-          // In narrow screens, close the help menu after selecting an item.
-          const btn = document.querySelector('button[data-bs-target="#help-index"]');
-          const isVisible = !!(btn && btn.offsetParent !== null);
-          if (isVisible) {
-            document.querySelector(`nav#help-index`).classList.add('collapse');
-            document.querySelector(`nav#help-index`).classList.remove('show');
-          }
+        });
+      } else {
+        // Action for a link to an article.
+        const id = element.dataset.id;
+        if (id) {
+          // First, reset all other links to default state
+          helpIndex.querySelectorAll('a:not(has-arrow)').forEach(a => {
+            if (a.dataset.id !== id) {
+              a.classList.remove('active');
+              a.removeAttribute('aria-current');
+            } else {
+              element.classList.add('active');
+              a.setAttribute('aria-current', 'page');
+            }
+          });
         }
-      })
-    );
+        localStorage.setItem('helpIndex.lastClick', id);
+
+        // In narrow screens, close the help menu after selecting an item.
+        const btn = document.querySelector('button[data-bs-target="#help-index"]');
+        const isVisible = !!(btn && btn.offsetParent !== null);
+        if (isVisible) {
+          document.querySelector(`nav#help-index`).classList.add('collapse');
+          document.querySelector(`nav#help-index`).classList.remove('show');
+        }
+      }
+    }));
   }
 
   // Async restore function
@@ -55,7 +53,6 @@ document.addEventListener("DOMContentLoaded", function (event) {
     if (!lastClick) {
       lastClick = 'start-here';
     }
-
     const selectedLink = helpIndex.querySelector(`a[data-id="${lastClick}"]`);
     if (!selectedLink) return;
 
