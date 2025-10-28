a730c0b55c780275ad979c918f3b1c036776e063
Cliff Ford <cliff@ford.myzen.co.uk>
2025-10-28 20:17:05 +0000

    Fixed layout problems.

--- administrator/components/com_admin/tmpl/help/default.php
+++ administrator/components/com_admin/tmpl/help/default.php
@@ -26,13 +26,13 @@ include_once 'toc-src.php';
 <div class="d-flex flex-column flex-md-row">
     <div id="help-sidebar" class="flex-shrink-0 mt-md-2">
         <!-- Left menu -->
-        <button class="btn btn-sm btn-secondary my-md-2 options-menu d-md-none" type="button" data-bs-toggle="collapse" data-bs-target="#help-index" aria-controls="help-index" aria-expanded="false">
+        <button class="btn btn-sm btn-secondary my-2 options-menu d-md-none" type="button" data-bs-toggle="collapse" data-bs-target="#help-index" aria-controls="help-index" aria-expanded="false">
             <span class="icon-align-justify" aria-hidden="true"></span>
             <?php echo Text::_('JTOGGLE_SIDEBAR_MENU'); ?>
         </button>
         <nav id="help-index" class="main-nav help-nav sidebar-wrapper">
             <h2><?php echo Text::_('COM_ADMIN_HELP_INDEX'); ?></h2>
-            <ul id="helpmenu" class="help-nav flex-column pt-2">
+            <ul id="helpmenu" class="help-nav flex-column">
                 <?php
                     // WARNING: Do not use direct 'include' or 'require' as it is important to isolate the scope for each call
                     $this->renderSubmenu(JPATH_ADMINISTRATOR . '/components/com_admin/tmpl/help/toc-build.php', $menu);
