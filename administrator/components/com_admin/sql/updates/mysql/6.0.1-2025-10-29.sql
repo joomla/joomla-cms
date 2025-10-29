4f3a453593893c7f9fc47998d7a9e3dc83d7043d
Christian Heel <66922325+heelc29@users.noreply.github.com>
2025-10-29 19:45:41 +0100

    use standard replace
    REGEXP_REPLACE does not support backreferences in replacement string (mysql 8.0.13)
    
    All categories is always first index

--- administrator/components/com_admin/sql/updates/mysql/6.0.1-2025-10-29.sql
+++ administrator/components/com_admin/sql/updates/mysql/6.0.1-2025-10-29.sql
@@ -9,10 +9,6 @@ WHERE JSON_VALID(`params`) = 1
     AND `link` = 'index.php?option=com_content&view=featured';
 
 UPDATE `#__menu`
-SET `link` = REGEXP_REPLACE(
-    `link`,
-    '(catid\\[\\d+\\]=)\\s+',
-    '\\1'
-    )
+SET `link` = REPLACE(`link`, 'catid[0]= ', 'catid[0]=')
 WHERE `type` = 'component'
     AND `link` LIKE 'index.php?option=com_content&view=archive%= %';
