281073098c48d5609e4a1d44e07702e44cf79569
Christian Heel <66922325+heelc29@users.noreply.github.com>
2025-10-29 19:12:37 +0100

    update mysql

---
+++ administrator/components/com_admin/sql/updates/mysql/6.0.1-2025-10-29.sql
@@ -0,0 +1,18 @@
+UPDATE `#__menu`
+SET `params` = JSON_SET(`params`,
+    JSON_UNQUOTE(JSON_SEARCH(`params`, 'one', ' ', NULL, '$.featured_categories[*]')),
+    ''
+    )
+WHERE JSON_VALID(`params`) = 1
+    AND JSON_SEARCH(`params`, 'one', ' ', NULL, '$.featured_categories[*]') IS NOT NULL
+    AND `type` = 'component'
+    AND `link` = 'index.php?option=com_content&view=featured';
+
+UPDATE `#__menu`
+SET `link` = REGEXP_REPLACE(
+    `link`,
+    '(catid\\[\\d+\\]=)\\s+',
+    '\\1'
+    )
+WHERE `type` = 'component'
+    AND `link` LIKE 'index.php?option=com_content&view=archive%= %';
