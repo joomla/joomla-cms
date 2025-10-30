1b288c5b000f6af3e3e89ca4b5563faa2781ca60
Harald Leithner <leithner@itronic.at>
2025-10-30 10:35:22 +0100

    Update libraries/src/Document/Renderer/Html/StylesRenderer.php
    Co-authored-by: Richard Fath <richard67@users.noreply.github.com>

--- libraries/src/Document/Renderer/Html/StylesRenderer.php
+++ libraries/src/Document/Renderer/Html/StylesRenderer.php
@@ -175,7 +175,7 @@ class StylesRenderer extends DocumentRenderer
 
         // Check if script uses media version.
         if ($version && !str_contains($src, '?') && ($mediaVersion || $version !== 'auto')) {
-            $src .= '?' . match($version) {
+            $src .= '?' . match ($version) {
                 'auto'    => $mediaVersion,
                 'nocache' => 'nocache-' . rand(0, 1_000_000),
                 default   => $version,
