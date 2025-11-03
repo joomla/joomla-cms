2dd050f88b8dc9d2f6d113823ab17c85d3f6f023
bram <github@brambring.nl>
2025-11-03 09:34:33 +0100

    (re)added constanst for consistency

--- libraries/src/Application/CMSApplicationInterface.php
+++ libraries/src/Application/CMSApplicationInterface.php
@@ -92,6 +92,23 @@ interface CMSApplicationInterface extends ExtensionManagerInterface, Configurati
      */
     public const MSG_DEBUG = 'debug';
 
+     /**
+     * Constant defining an enqueued message message
+     *
+     * @var    string
+     * @since  __DEPLOY_VERSION__
+     */
+    public const MSG_MESSAGE = 'message';
+
+    /**
+     * Constant defining an enqueued success message
+     *
+     * @var    string
+     * @since  __DEPLOY_VERSION__
+     */
+    public const MSG_SUCCESS = 'success';
+
+
     /**
      * Enqueue a system message.
      *
