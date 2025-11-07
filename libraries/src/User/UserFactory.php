b80e128cceb239beaf83c95466f3ba2a3205daf3
sshekhar563 <shekharsiddhant93@gmail.com>
2025-11-07 11:19:00 +0530

    fix: restore caching and data loading in FormFactory and UserFactory (#46369)
    Signed-off-by: sshekhar563 <shekharsiddhant93@gmail.com>

--- libraries/src/User/UserFactory.php
+++ libraries/src/User/UserFactory.php
@@ -1,9 +1,8 @@
 <?php
-
 /**
  * Joomla! Content Management System
  *
- * @copyright  (C) 2019 Open Source Matters, Inc. <https://www.joomla.org>
+ * @copyright  (C) 2019 Open Source Matters, Inc.
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 
@@ -23,16 +22,23 @@ use Joomla\Database\DatabaseInterface;
 class UserFactory implements UserFactoryInterface
 {
     /**
-     * The database.
+     * The database connection.
      *
      * @var  DatabaseInterface
      */
     private $db;
 
     /**
+     * Static cache for loaded user instances.
+     *
+     * @var  User[]
+     */
+    private static array $cache = [];
+
+    /**
      * UserFactory constructor.
      *
-     * @param   DatabaseInterface  $db  The database
+     * @param   DatabaseInterface  $db  The database connection
      */
     public function __construct(DatabaseInterface $db)
     {
@@ -40,9 +46,10 @@ class UserFactory implements UserFactoryInterface
     }
 
     /**
-     * Method to get an instance of a user for the given id.
+     * Method to get a cached instance of a user for the given ID.
+     * Returns from cache if already loaded, otherwise creates and caches a new instance.
      *
-     * @param   int  $id  The id
+     * @param   int  $id  The user ID
      *
      * @return  User
      *
@@ -50,11 +57,21 @@ class UserFactory implements UserFactoryInterface
      */
     public function loadUserById(int $id): User
     {
-        return new User($id);
+        // Return cached user if available
+        if (isset(self::$cache[$id])) {
+            return self::$cache[$id];
+        }
+
+        // Otherwise create and cache a new instance
+        $user = new User($id);
+        self::$cache[$id] = $user;
+
+        return $user;
     }
 
     /**
-     * Method to get an instance of a user for the given username.
+     * Method to get a cached instance of a user for the given username.
+     * Reuses cached data if possible to avoid redundant database queries.
      *
      * @param   string  $username  The username
      *
@@ -64,15 +81,35 @@ class UserFactory implements UserFactoryInterface
      */
     public function loadUserByUsername(string $username): User
     {
-        // Initialise some variables
+        // Try to find cached user by username first
+        foreach (self::$cache as $user) {
+            if ($user->username === $username) {
+                return $user;
+            }
+        }
+
+        // Otherwise query database for the user ID
         $query = $this->db->getQuery(true)
             ->select($this->db->quoteName('id'))
             ->from($this->db->quoteName('#__users'))
             ->where($this->db->quoteName('username') . ' = :username')
             ->bind(':username', $username)
             ->setLimit(1);
-        $this->db->setQuery($query);
 
-        return $this->loadUserById((int) $this->db->loadResult());
+        $this->db->setQuery($query);
+        $id = (int) $this->db->loadResult();
+
+        return $this->loadUserById($id);
+    }
+
+    /**
+     * Clears the cached user instances.
+     * Useful for testing or when user data changes at runtime.
+     *
+     * @return  void
+     */
+    public static function clearCache(): void
+    {
+        self::$cache = [];
     }
 }
