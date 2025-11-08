7bb02bfd1e9bf25db1da3f33ad799ad5d966c1f4
sshekhar563 <shekharsiddhant93@gmail.com>
2025-11-08 17:47:20 +0530

    style: fix PHPCS issues (tabs, line endings, and newline at EOF) in factory files
    Signed-off-by: sshekhar563 <shekharsiddhant93@gmail.com>

---
+++ libraries/src/Form/FormFactory.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * Joomla! Content Management System
+ *
+ * @copyright  (C) 2017 Open Source Matters, Inc. <https://www.joomla.org>
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+namespace Joomla\CMS\Form;
+
+use Joomla\Database\DatabaseAwareTrait;
+
+// phpcs:disable PSR1.Files.SideEffects
+\defined('_JEXEC') or die;
+// phpcs:enable PSR1.Files.SideEffects
+
+/**
+ * Default factory for creating Form objects
+ *
+ * @since  4.0.0
+ */
+class FormFactory implements FormFactoryInterface
+{
+	use DatabaseAwareTrait;
+
+	/**
+	 * Method to get an instance of a form.
+	 *
+	 * @param   string  $name     The name of the form.
+	 * @param   array   $options  An array of form options.
+	 *
+	 * @return  Form
+	 *
+	 * @since   4.0.0
+	 */
+	public function createForm(string $name, array $options = []): Form
+	{
+		$form = new Form($name, $options);
+
+		$form->setDatabase($this->getDatabase());
+
+		return $form;
+	}
+}
