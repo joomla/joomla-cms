d057b8dd5f1a2db442dc1ec94b363b8c1200907a
Nicola Galgano <optimus4joomla@gmail.com>
2025-11-09 09:18:01 +0100

    Add backward compatibility module and extension entries

--- administrator/components/com_admin/sql/updates/postgresql/5.4.0-2025-11-08.sql
+++ administrator/components/com_admin/sql/updates/postgresql/5.4.0-2025-11-08.sql
@@ -2,3 +2,6 @@ INSERT INTO "#__modules" ("title", "note", "content", "ordering", "position", "c
 ('Backward Compatibility', '', '', 1, 'status', 0, NULL, NULL, NULL, 1, 'mod_backward', 1, 1, '', 1, '*');
 
 INSERT INTO "#__modules_menu" ("moduleid", "menuid") VALUES (currval(pg_get_serial_sequence('#__modules','id')), 0);
+
+INSERT INTO "#__extensions" ("package_id", "name", "type", "element", "folder", "client_id", "enabled", "access", "protected", "locked", "manifest_cache", "params", "custom_data", "ordering", "state") VALUES
+(0, 'mod_backward', 'module', 'mod_backward', '', 1, 1, 1, 0, 1, '', '{}', '', 0, 0);
