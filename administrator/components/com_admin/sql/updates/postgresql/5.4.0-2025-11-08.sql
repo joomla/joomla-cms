6eb4b56599326903b3aad99ddfb3f9d85a2ff8f4
Nicola Galgano <optimus4joomla@gmail.com>
2025-11-09 16:45:45 +0100

    same as mysql

--- administrator/components/com_admin/sql/updates/postgresql/5.4.0-2025-11-08.sql
+++ administrator/components/com_admin/sql/updates/postgresql/5.4.0-2025-11-08.sql
@@ -7,5 +7,9 @@ SELECT 'Backward Compatibility', '', '', 1, 'status', 0, NULL, NULL, NULL, 1, 'm
 WHERE NOT EXISTS (SELECT * FROM "#__modules" m WHERE m."title" = 'Backward Compatibility' AND m."position" = 'status' AND m."module" = 'mod_backward' AND m."client_id" = 1);
 
 INSERT INTO "#__modules_menu" ("moduleid", "menuid") 
-VALUES (currval(pg_get_serial_sequence('#__modules','id')), 0)
-ON CONFLICT ("moduleid", "menuid") DO NOTHING;
+SELECT currval(pg_get_serial_sequence('#__modules','id')), 0
+WHERE NOT EXISTS (
+    SELECT 1 FROM "#__modules_menu" 
+    WHERE "moduleid" = currval(pg_get_serial_sequence('#__modules','id')) AND "menuid" = 0
+);
+
