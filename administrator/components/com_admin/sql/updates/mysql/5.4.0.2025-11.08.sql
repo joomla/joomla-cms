3bb2c75d6ee6e38df04708cd28abe32effeb1cfd
Nicola Galgano <optimus4joomla@gmail.com>
2025-11-09 13:24:28 +0100

    Update administrator/components/com_admin/sql/updates/mysql/5.4.0.2025-11.08.sql
    Co-authored-by: Richard Fath <richard67@users.noreply.github.com>

--- administrator/components/com_admin/sql/updates/mysql/5.4.0.2025-11.08.sql
+++ administrator/components/com_admin/sql/updates/mysql/5.4.0.2025-11.08.sql
@@ -1,6 +1,6 @@
 INSERT INTO `#__extensions` (`package_id`, `name`, `type`, `element`, `folder`, `client_id`, `enabled`, `access`, `protected`, `locked`, `manifest_cache`, `params`, `custom_data`, `ordering`, `state`) 
 SELECT 0, 'mod_backward', 'module', 'mod_backward', '', 1, 1, 1, 0, 1, '', '{}', '', 0, 0
-WHERE NOT EXISTS (SELECT * FROM `a#__extensions` e WHERE e.`type` = 'module' AND e.`element` = 'mod_backward' AND e.`name` = 'mod_backward' AND e.`client_id` = 1);
+WHERE NOT EXISTS (SELECT * FROM `#__extensions` e WHERE e.`type` = 'module' AND e.`element` = 'mod_backward' AND e.`name` = 'mod_backward' AND e.`client_id` = 1);
 
 INSERT INTO `#__modules` (`title`, `note`, `content`, `ordering`, `position`, `checked_out`, `checked_out_time`, `publish_up`, `publish_down`, `published`, `module`, `access`, `showtitle`, `params`, `client_id`, `language`)
 SELECT 'Backward Compatibility', '', '', 1, 'status', 0, NULL, NULL, NULL, 1, 'mod_backward', 1, 1, '', 1, '*'
