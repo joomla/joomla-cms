d3e0ea2e348f9f75c8814e4883d0208ec2d9cb97
Jonathan Brain <3941269+BrainforgeUK@users.noreply.github.com>
2025-11-11 20:47:27 +0000

    Clean up unused imports in Browser.php
    Remove unused imports from Browser.php

--- libraries/src/Environment/Browser.php
+++ libraries/src/Environment/Browser.php
@@ -9,9 +9,6 @@
 
 namespace Joomla\CMS\Environment;
 
-use Joomla\CMS\Factory;
-use Joomla\Event\Event;
-
 // phpcs:disable PSR1.Files.SideEffects
 \defined('_JEXEC') or die;
 // phpcs:enable PSR1.Files.SideEffects
