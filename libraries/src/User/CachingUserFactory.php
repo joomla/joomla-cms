d7ab329ee78ac49f70a334381589358664e0656a
sshekhar563 <shekharsiddhant93@gmail.com>
2025-11-12 18:01:29 +0530

    style: apply php-cs-fixer and phpcs fixes for caching factories

--- libraries/src/User/CachingUserFactory.php
+++ libraries/src/User/CachingUserFactory.php
@@ -1,11 +1,10 @@
 <?php
 
 /**
- * @package     Joomla.CMS
- * @subpackage  User
+ * Joomla! Content Management System
  *
- * @copyright   (C) 2005 - 2024 Open Source Matters, Inc. <https://www.joomla.org>
- * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ * @copyright  (C) 2018 Open Source Matters, Inc. <https://www.joomla.org>
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 declare(strict_types=1);
