d86bd12ec1fc88fe5e4bdc6aed19802fc5cd2f01
chmst <dev@chmst.de>
2025-11-23 11:32:16 +0100

    back to registry

--- administrator/components/com_messages/src/Model/ConfigModel.php
+++ administrator/components/com_messages/src/Model/ConfigModel.php
@@ -13,6 +13,7 @@ namespace Joomla\Component\Messages\Administrator\Model;
 use Joomla\CMS\Component\ComponentHelper;
 use Joomla\CMS\MVC\Model\FormModel;
 use Joomla\Database\ParameterType;
+use Joomla\Registry\Registry;
 
 // phpcs:disable PSR1.Files.SideEffects
 \defined('_JEXEC') or die;
@@ -58,6 +59,7 @@ class ConfigModel extends FormModel
      */
     public function getItem()
     {
+        $item   = new Registry();
         $userid = (int) $this->getState('user.id');
 
         $db    = $this->getDatabase();
@@ -82,7 +84,10 @@ class ConfigModel extends FormModel
             return false;
         }
 
-        $item = (object) array_column($rows, 'cfg_value', 'cfg_name');
+        foreach ($rows as $row) {
+            $property        = $row->cfg_name;
+            $item->set($property, $row->cfg_value);
+        }
 
         $this->preprocessData('com_messages.config', $item);
 
