59fafe4105b8d32bc2b6de1fea7438f5260aeb01
coolcat-creations <mail@coolcat-creations.com>
2025-12-05 16:06:17 +0100

    Replicate mod_articles behaviour

--- components/com_content/src/Model/CategoryModel.php
+++ components/com_content/src/Model/CategoryModel.php
@@ -220,9 +220,14 @@ class CategoryModel extends ListModel
         // Set the featured articles state
         $this->setState('filter.featured', $params->get('show_featured'));
 
-        // Limit to articles created by the current user when requested
-        if ($params->get('list_show_own')) {
-            $this->setState('filter.author_id', (int) $user->id);
+        $authorFilteringType = (int) $params->get('list_author_filtering_type', 1);
+
+        if ($authorFilteringType === 2) {
+            $this->setState('filter.author_id', [(int) $user->id]);
+            $this->setState('filter.author_id.include', true);
+        } else {
+            $this->setState('filter.author_id', (array) $params->get('list_author', []));
+            $this->setState('filter.author_id.include', (bool) $authorFilteringType);
         }
     }
 
@@ -253,6 +258,7 @@ class CategoryModel extends ListModel
             $model->setState('list.filter', $this->getState('list.filter'));
             $model->setState('filter.tag', $this->getState('filter.tag'));
             $model->setState('filter.author_id', $this->getState('filter.author_id'));
+            $model->setState('filter.author_id.include', $this->getState('filter.author_id.include', true));
 
             // Filter.subcategories indicates whether to include articles from subcategories in the list or blog
             $model->setState('filter.subcategories', $this->getState('filter.subcategories'));
