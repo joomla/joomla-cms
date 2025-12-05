142e6ecbe831d777c9b6cfa0aaadd5de366a8591
coolcat-creations <mail@coolcat-creations.com>
2025-12-05 15:00:34 +0100

    Restore file to main

--- build/media_source/system/js/showon.es6.js
+++ build/media_source/system/js/showon.es6.js
@@ -86,12 +86,6 @@ class Showon {
               }
             });
           }
-          if (this.fields[showonData[0].field] && !this.fields[showonData[0].field].origin.length) {
-            field.classList.add('hidden');
-            field.dispatchEvent(new CustomEvent('joomla:showon-hide', {
-              bubbles: true,
-            }));
-          }
         }
       });
 
