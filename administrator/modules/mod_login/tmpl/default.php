c6a4c7875c809c3d212d749fb88a2be18a54e6a7
coolcat-creations <mail@coolcat-creations.com>
2026-01-03 13:28:05 +0100

    Change Login Flow to a Login Pattern

--- administrator/modules/mod_login/tmpl/default.php
+++ administrator/modules/mod_login/tmpl/default.php
@@ -22,31 +22,53 @@ $wa->useScript('keepalive')
 
 Text::script('JSHOWPASSWORD');
 Text::script('JHIDEPASSWORD');
+Text::script('MOD_LOGIN_USERNAME_REQUIRED_FOR_PASSKEY');
+
+// Identify passkey button from extraButtons (by id/class/onclick containing passkey/webauthn/passwordless)
+$passkeyButton = null;
+$otherButtons = [];
+
+if (!empty($extraButtons)) {
+    foreach ($extraButtons as $button) {
+        $isPasskey = false;
+
+        // Check id
+        if (!empty($button['id']) && preg_match('/passkey|webauthn|passwordless/i', $button['id'])) {
+            $isPasskey = true;
+        }
+
+        // Check class
+        if (!$isPasskey && !empty($button['class']) && preg_match('/passkey|webauthn|passwordless/i', $button['class'])) {
+            $isPasskey = true;
+        }
+
+        // Check onclick
+        if (!$isPasskey && !empty($button['onclick']) && preg_match('/passkey|webauthn|passwordless/i', $button['onclick'])) {
+            $isPasskey = true;
+        }
+
+        // Check label
+        if (!$isPasskey && !empty($button['label']) && preg_match('/passkey|webauthn|passwordless/i', $button['label'])) {
+            $isPasskey = true;
+        }
+
+        if ($isPasskey && $passkeyButton === null) {
+            $passkeyButton = $button;
+        } else {
+            $otherButtons[] = $button;
+        }
+    }
+}
 ?>
 <form class="form-validate" action="<?php echo Route::_('index.php', true); ?>" method="post" id="form-login">
     <fieldset>
         <legend class="visually-hidden"><?php echo Text::_('MOD_LOGIN'); ?></legend>
 
-        <?php if (!empty($langs)) : ?>
-            <legend><?php echo Text::_('MOD_LOGIN_CHOSE_LANGUAGE'); ?></legend>
-
-            <div>
-                <div class="form-group">
-                    <label for="lang">
-                        <?php echo Text::_('MOD_LOGIN_LANGUAGE'); ?>
-                    </label>
-                    <?php echo $langs; ?>
-                </div>
-            </div>
-        <?php endif; ?>
-        <legend class="text-center"><?php echo Text::_('MOD_LOGIN_WITH_USERNAME_AND_PASSWORD'); ?></legend>
-
-        <div class="form-group">
+        <div class="form-group login-username">
             <label for="mod-login-username">
                 <?php echo Text::_('JGLOBAL_USERNAME'); ?>
             </label>
             <div class="input-group">
-
                 <input
                     name="username"
                     id="mod-login-username"
@@ -58,31 +80,70 @@ Text::script('JHIDEPASSWORD');
                 >
             </div>
         </div>
-        <div class="form-group">
+
+        <?php if ($passkeyButton) : ?>
+            <div class="login-methods form-group">
+                <button type="button"
+                    id="btn-login-password"
+                    class="btn btn-primary"
+                    aria-controls="login-password-section">
+                    <span class="icon-key icon-fw" aria-hidden="true"></span>
+                    <?php echo Text::_('MOD_LOGIN_CONTINUE_WITH_PASSWORD'); ?>
+                </button>
+
+                <?php
+                $dataAttributeKeys = array_filter(array_keys($passkeyButton), function ($key) {
+                    return substr($key, 0, 5) == 'data-';
+                });
+                ?>
+                <button type="button"
+                        class="btn btn-secondary <?php echo htmlspecialchars($passkeyButton['class'] ?? '', ENT_QUOTES, 'UTF-8'); ?>"
+                        data-is-passkey="true"
+                    <?php foreach ($dataAttributeKeys as $key) : ?>
+                        <?php echo htmlspecialchars($key, ENT_QUOTES, 'UTF-8'); ?>="<?php echo htmlspecialchars($passkeyButton[$key], ENT_QUOTES, 'UTF-8'); ?>"
+                    <?php endforeach; ?>
+                    <?php if (!empty($passkeyButton['onclick'])) : ?>
+                        onclick="<?php echo htmlspecialchars($passkeyButton['onclick'], ENT_QUOTES, 'UTF-8'); ?>"
+                    <?php endif; ?>
+                    title="<?php echo Text::_('MOD_LOGIN_CONTINUE_WITH_PASSKEY'); ?>"
+                    id="<?php echo htmlspecialchars($passkeyButton['id'], ENT_QUOTES, 'UTF-8'); ?>"
+                >
+                    <?php if (!empty($passkeyButton['icon'])) : ?>
+                        <span class="<?php echo htmlspecialchars($passkeyButton['icon'], ENT_QUOTES, 'UTF-8'); ?>" aria-hidden="true"></span>
+                    <?php elseif (!empty($passkeyButton['image'])) : ?>
+                        <?php echo $passkeyButton['image']; ?>
+                    <?php elseif (!empty($passkeyButton['svg'])) : ?>
+                        <?php echo $passkeyButton['svg']; ?>
+                    <?php endif; ?>
+                    <?php echo Text::_('MOD_LOGIN_CONTINUE_WITH_PASSKEY') ?>
+                </button>
+            </div>
+        <?php endif; ?>
+
+        <div id="login-password-section" class="login-password form-group">
             <label for="mod-login-password">
                 <?php echo Text::_('JGLOBAL_PASSWORD'); ?>
             </label>
             <div class="input-group">
-
                 <input
                     name="passwd"
                     id="mod-login-password"
                     type="password"
                     class="form-control input-full"
-                    required="required"
                     autocomplete="current-password"
+                    required
                 >
                 <button type="button" class="btn btn-primary input-password-toggle">
                     <span class="icon-eye icon-fw" aria-hidden="true"></span>
                     <span class="visually-hidden"><?php echo Text::_('JSHOWPASSWORD'); ?></span>
                 </button>
-
             </div>
-
         </div>
-        <div class="form-group">
-            <button type="submit" id="btn-login-submit"
-                    class="btn btn-primary w-100 btn-lg"><?php echo Text::_('JLOGIN'); ?></button>
+
+        <div class="login-submit form-group">
+            <button type="submit" id="btn-login-submit" class="btn btn-primary w-100 btn-lg">
+                <?php echo Text::_('JLOGIN'); ?>
+            </button>
         </div>
 
         <div class="text-center">
@@ -99,46 +160,56 @@ Text::script('JHIDEPASSWORD');
             </div>
         </div>
 
-        <div class="mt-4">
-            <?php if ($extraButtons) : ?>
+        <?php if (!empty($otherButtons)) : ?>
+            <div class="login-other-methods mt-4">
                 <hr>
+                <p class="text-center fw-bold"><?php echo Text::_('MOD_LOGIN_OR'); ?></p>
 
-                <legend class="text-center"><?php echo Text::_('MOD_LOGIN_OR'); ?></legend>
-
-                <?php foreach ($extraButtons as $button) :
+                <?php foreach ($otherButtons as $button) :
                     $dataAttributeKeys = array_filter(array_keys($button), function ($key) {
                         return substr($key, 0, 5) == 'data-';
                     });
                     ?>
                     <div class="form-group">
                         <button type="button"
-                                class="btn btn-secondary w-100 <?php echo $button['class'] ?? '' ?>"
-                        <?php foreach ($dataAttributeKeys as $key) : ?>
-                            <?php echo $key ?>="<?php echo $button[$key] ?>"
-                        <?php endforeach; ?>
-                        <?php if ($button['onclick']) : ?>
-                            onclick="<?php echo $button['onclick'] ?>"
-                        <?php endif; ?>
-                        title="<?php echo Text::_($button['label']) ?>"
-                        id="<?php echo $button['id'] ?>"
+                                class="btn btn-secondary w-100 <?php echo htmlspecialchars($button['class'] ?? '', ENT_QUOTES, 'UTF-8'); ?>"
+                            <?php foreach ($dataAttributeKeys as $key) : ?>
+                                <?php echo htmlspecialchars($key, ENT_QUOTES, 'UTF-8'); ?>="<?php echo htmlspecialchars($button[$key], ENT_QUOTES, 'UTF-8'); ?>"
+                            <?php endforeach; ?>
+                            <?php if (!empty($button['onclick'])) : ?>
+                                onclick="<?php echo htmlspecialchars($button['onclick'], ENT_QUOTES, 'UTF-8'); ?>"
+                            <?php endif; ?>
+                            title="<?php echo Text::_($button['label']); ?>"
+                            id="<?php echo htmlspecialchars($button['id'], ENT_QUOTES, 'UTF-8'); ?>"
                         >
-                        <?php if (!empty($button['icon'])) : ?>
-                            <span class="<?php echo $button['icon'] ?>"></span>
-                        <?php elseif (!empty($button['image'])) : ?>
-                            <?php echo $button['image']; ?>
-                        <?php elseif (!empty($button['svg'])) : ?>
-                            <?php echo $button['svg']; ?>
-                        <?php endif; ?>
-                        <?php echo Text::_($button['label']) ?>
+                            <?php if (!empty($button['icon'])) : ?>
+                                <span class="<?php echo htmlspecialchars($button['icon'], ENT_QUOTES, 'UTF-8'); ?>" aria-hidden="true"></span>
+                            <?php elseif (!empty($button['image'])) : ?>
+                                <?php echo $button['image']; ?>
+                            <?php elseif (!empty($button['svg'])) : ?>
+                                <?php echo $button['svg']; ?>
+                            <?php endif; ?>
+                            <?php echo Text::_($button['label']); ?>
                         </button>
                     </div>
                 <?php endforeach; ?>
-            <?php endif; ?>
+            </div>
+        <?php endif; ?>
 
-            <input type="hidden" name="option" value="com_login">
-            <input type="hidden" name="task" value="login">
-            <input type="hidden" name="return" value="<?php echo $return; ?>">
-            <?php echo HTMLHelper::_('form.token'); ?>
-        </div>
+        <?php if (!empty($langs)) : ?>
+            <div class="login-language mt-4">
+                <div class="form-group">
+                    <label for="lang">
+                        <?php echo Text::_('MOD_LOGIN_LANGUAGE'); ?>
+                    </label>
+                    <?php echo $langs; ?>
+                </div>
+            </div>
+        <?php endif; ?>
+
+        <input type="hidden" name="option" value="com_login">
+        <input type="hidden" name="task" value="login">
+        <input type="hidden" name="return" value="<?php echo htmlspecialchars($return, ENT_QUOTES, 'UTF-8'); ?>">
+        <?php echo HTMLHelper::_('form.token'); ?>
     </fieldset>
 </form>
--- build/media_source/mod_login/js/admin-login.es6.js
+++ build/media_source/mod_login/js/admin-login.es6.js
@@ -7,13 +7,141 @@
   'use strict';
 
   document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('form-login');
     const btn = document.getElementById('btn-login-submit');
+    const usernameInput = document.getElementById('mod-login-username');
+    const passwordInput = document.getElementById('mod-login-password');
+    const passwordSection = form ? form.querySelector('.login-password') : null;
+    const submitSection = form ? form.querySelector('.login-submit') : null;
+    const methodsSection = form ? form.querySelector('.login-methods') : null;
+    const continuePasswordBtn = document.getElementById('btn-login-password');
+    const passkeyBtn = form ? form.querySelector('[data-is-passkey="true"]') : null;
 
+    // Track if password flow is active (for Pattern A)
+    let passwordFlowActive = !methodsSection;
+
+    /**
+     * Show the password section (Pattern A flow)
+     */
+    const showPasswordSection = () => {
+      if (!passwordSection || !submitSection || !passwordInput) {
+        return;
+      }
+
+      // Hide the methods section
+      if (methodsSection) {
+        methodsSection.style.display = 'none';
+      }
+
+      // Show password and submit sections
+      passwordSection.style.display = '';
+      submitSection.style.display = '';
+
+      // Update ARIA state on continue button
+      if (continuePasswordBtn) {
+        continuePasswordBtn.setAttribute('aria-expanded', 'true');
+      }
+
+      // Set password as required and mark flow as active
+      passwordInput.required = true;
+      passwordFlowActive = true;
+
+      // Focus password field
+      passwordInput.focus();
+    };
+
+    // Pattern A: Only apply if methods section exists (passkey available)
+    if (methodsSection && passwordSection && submitSection && continuePasswordBtn && passwordInput) {
+      // Mark JS-enabled mode
+      document.documentElement.classList.add('js-enabled');
+
+      // Hide password and submit sections initially
+      passwordSection.style.display = 'none';
+      submitSection.style.display = 'none';
+
+      // Remove required from hidden password field to prevent browser validation blocking
+      passwordInput.required = false;
+
+      // Update ARIA state on continue button
+      continuePasswordBtn.setAttribute('aria-expanded', 'false');
+
+      // "Continue with password" button handler
+      continuePasswordBtn.addEventListener('click', showPasswordSection);
+
+      // Passkey button handling - intercept if username is empty
+      if (passkeyBtn && usernameInput) {
+        // Clear aria-invalid when username is filled
+        usernameInput.addEventListener('input', () => {
+          if (usernameInput.value.trim() !== '') {
+            usernameInput.removeAttribute('aria-invalid');
+          }
+        });
+
+        // Use capturing phase to check username before original onclick runs
+        passkeyBtn.addEventListener('click', (e) => {
+          if (usernameInput.value.trim() === '') {
+            // Prevent the original onclick and any other handlers
+            e.preventDefault();
+            e.stopPropagation();
+            e.stopImmediatePropagation();
+
+            // Set aria-invalid on username field
+            usernameInput.setAttribute('aria-invalid', 'true');
+
+            // Ensure message container has aria-live for screen readers
+            const messageContainer = document.getElementById('system-message-container');
+            if (messageContainer && !messageContainer.hasAttribute('aria-live')) {
+              messageContainer.setAttribute('aria-live', 'polite');
+              messageContainer.setAttribute('role', 'status');
+            }
+
+            // Show warning using Joomla's message system
+            Joomla.renderMessages({
+              warning: [Joomla.Text._('MOD_LOGIN_USERNAME_REQUIRED_FOR_PASSKEY')],
+            });
+
+            // Focus username
+            usernameInput.focus();
+          }
+          // If username is present, let the event continue to the original onclick handler
+        }, true); // Capturing phase
+      }
+    }
+
+    // Form submit handler
     if (btn) {
       btn.addEventListener('click', (event) => {
         event.preventDefault();
+
+        // If Pattern A is active and password flow not started, show password section
+        if (methodsSection && !passwordFlowActive) {
+          showPasswordSection();
+          return;
+        }
+
+        // Normal form validation and submit
         if (document.formvalidator.isValid(btn.form)) {
           Joomla.submitbutton('login');
+        }
+      });
+    }
+
+    // Intercept other submit paths when Pattern A is active
+    if (form && methodsSection) {
+      form.addEventListener('submit', (event) => {
+        if (!passwordFlowActive) {
+          event.preventDefault();
+          showPasswordSection();
+        }
+      });
+    }
+
+    // Intercept Enter key in username field when Pattern A is active
+    if (usernameInput && methodsSection) {
+      usernameInput.addEventListener('keydown', (e) => {
+        if (e.key === 'Enter' && !passwordFlowActive) {
+          e.preventDefault();
+          showPasswordSection();
         }
       });
     }
