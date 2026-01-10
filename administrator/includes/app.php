51cb3d5ebe101acd0d7c672a24c75eab0a1eb4b1
Fedik <getthesite@gmail.com>
2026-01-10 11:27:42 +0200

    simplify check

--- administrator/includes/app.php
+++ administrator/includes/app.php
@@ -29,7 +29,7 @@ if (!file_exists(JPATH_LIBRARIES . '/vendor/autoload.php') || !is_dir(JPATH_PUBL
 require_once JPATH_BASE . '/includes/framework.php';
 
 // Prevent User from leaving .env file in the root folder for the production environment
-if (($_ENV['JOOMLA_ENV'] ?? '') === 'prod' && !is_file(JPATH_ROOT . '/.env.local.php') && is_file(JPATH_ROOT . '/.env')) {
+if (($_ENV['JOOMLA_ENV'] ?? '') === 'prod' && is_file(JPATH_ROOT . '/.env')) {
     throw new RuntimeException('Use of raw .env file instead of cached .env.local.php file in the production environment is discouraged.');
 }
 
