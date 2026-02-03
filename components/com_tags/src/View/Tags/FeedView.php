45b8b910139bcc68a2b00225d59b012c24eb9a21
Tomás B.F. <berjoyo@gmail.com>
2026-02-03 10:27:41 +0100

    Refactor FeedView class -  feedEnclosure joomla  class for intro images

--- components/com_tags/src/View/Tags/FeedView.php
+++ components/com_tags/src/View/Tags/FeedView.php
@@ -1,4 +1,5 @@
 <?php
+
 /** @package    Joomla.Site
  * @subpackage  com_tags
  * @copyright   (C) 2013 Open Source Matters, Inc. <https://www.joomla.org>
@@ -18,23 +19,26 @@ use Joomla\Component\Tags\Site\Model\TagsModel;
 use Joomla\CMS\Uri\Uri;
 
 \defined('_JEXEC') or die;
-
 /* HTML View class for the Tags component all tags view
  * @since  3.1  */
 
-class FeedView extends BaseHtmlView {
+class FeedView extends BaseHtmlView 
+{
     /** Execute and display a template script.
      * @param   string  $tpl  The name of the template file to parse; automatically searches through the template paths.
      * @return  void */
     
-     public function display($tpl = null) {
+     public function display($tpl = null)
+    {
         $app                        = Factory::getApplication();
-        $siteName                   = $app->get('sitename');                      
+        $siteName                   = $app->get('sitename');
         $categories                 = Categories::getInstance('Content');
         $this->getDocument()->link  = Route::_('index.php?option=com_tags&view=tags');
         $params                     = $app->getParams();
 
-        if ($params->get('show_feed_link', 1) == 0) throw new \Exception(Text::_('JGLOBAL_RESOURCE_NOT_FOUND'), 404);
+        if ($params->get('show_feed_link', 1) == 0) {
+            throw new \Exception(Text::_('JGLOBAL_RESOURCE_NOT_FOUND'), 404);
+        }
         
         $app->getInput()->set('limit', $app->get('feed_limit'));
         $siteEmail = $app->get('mailfrom');
@@ -48,33 +52,36 @@ class FeedView extends BaseHtmlView {
         /** @var TagModel $model */
         $model = $this->getModel();
         $items = $model->getItems();
-        $tagTitle = $model->getName(); 
+        $tagTitle = $model->getName();
 
         if ($items !== false) {
             foreach ($items as $item) {
-
                 // Load individual item creator class
-                $feeditem              = new FeedItem();
-                $feeditem->title       = html_entity_decode($this->escape($item->core_title), ENT_COMPAT, 'UTF-8');
-                $feeditem->link        = Route::_($item->link);
+                $feeditem = new FeedItem();
+                $feeditem->title = html_entity_decode($this->escape($item->core_title), ENT_COMPAT, 'UTF-8');
+                $feeditem->link = Route::_($item->link);
                 $feeditem->description = $item->core_body;
-                $feeditem->category    = $item->core_category_title;
-                $feeditem->author      = $item->core_created_by_alias ? : $item->author;
-                $feeditem->date         = ($item->displayDate ? date('r', strtotime($item->displayDate)) : '');
+                $feeditem->category = $item->core_category_title;
+                $feeditem->author = $item->core_created_by_alias ? : $item->author;
+                $feeditem->date = ($item->displayDate ? date('r', strtotime($item->displayDate)) : '');
 
                 $images = json_decode($item->core_images, false);
                 if (!empty($images->image_intro)) {
-                    if (preg_match('/^([^#]*)/', $images->image_intro, $matches)) $url_img = $matches[1]; else $url_img = $images->image_intro;
+                    if (preg_match('/^([^#]*)/', $images->image_intro, $matches)) $url_img = $matches[1];
+                    else $url_img = $images->image_intro;
 
                     $lastDotPos = strrpos($url_img, '.');
-                    if ($lastDotPos !== false) { $extension = substr($url_img, $lastDotPos + 1); $extension = mb_strtolower($extension); } else $extension = '-';
+                    if ($lastDotPos !== false) {
+                        $extension = substr($url_img, $lastDotPos + 1);
+                        $extension = mb_strtolower($extension);
+                    } else $extension = '-';
 
                     // Use of Joomla FeedEnclosure class for items intro images
-                    $feedEnclosure         = new FeedEnclosure();
-                    $feedEnclosure->url    = Uri::root().$url_img;
+                    $feedEnclosure = new FeedEnclosure();
+                    $feedEnclosure->url = Uri::root() . $url_img;
                     $feedEnclosure->length = filesize($url_img);
-                    $feedEnclosure->type   = 'image/'.$extension;
-                    $feeditem->enclosure   = $feedEnclosure;
+                    $feedEnclosure->type = 'image/' . $extension;
+                    $feeditem->enclosure = $feedEnclosure;
                 }
 
                 if ($feedEmail === 'site') $feeditem->authorEmail = $siteEmail;
