ec418e4fb3c17dbba116e0125a7ee3c2b33f67c1
krishnaGandhi11 <krishnagandhi0911@gmail.com>
2026-02-04 23:09:20 +0530

    tabs to spaces

--- libraries/src/Form/Field/CalendarField.php
+++ libraries/src/Form/Field/CalendarField.php
@@ -402,14 +402,14 @@ class CalendarField extends FormField
         }
 
         if ($this->filterFormat) {
-			$date = \DateTime::createFromFormat($this->filterFormat, $value);
-			if ($date === false) {
-				// Result: "Invalid form: Date"
-				Factory::getApplication()->enqueueMessage(Text::_('JGLOBAL_VALIDATION_FORM_FAILED') . ': ' . Text::_('JDATE'), 'warning');
-				return '';
-			}
-			$value = $date->format('Y-m-d H:i:s');
-		}
+            $date = \DateTime::createFromFormat($this->filterFormat, $value);
+            if ($date === false) {
+                // Result: "Invalid form: Date"
+                Factory::getApplication()->enqueueMessage(Text::_('JGLOBAL_VALIDATION_FORM_FAILED') . ': ' . Text::_('JDATE'), 'warning');
+                return '';
+            }
+            $value = $date->format('Y-m-d H:i:s');
+        }
 
         $app = Factory::getApplication();
 
