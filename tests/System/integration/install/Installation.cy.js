d5a89e6f3721e025013920fefada5ead735271af
Nicola Galgano <optimus4joomla@gmail.com>
2026-02-06 12:27:26 +0100

    clean db

--- tests/System/integration/install/Installation.cy.js
+++ tests/System/integration/install/Installation.cy.js
@@ -15,6 +15,10 @@ describe('Install Joomla', () => {
       db_prefix: Cypress.env('db_prefix'),
     };
 
+    cy.task('db_clean');
+    // If exists, delete PHP configuration file to force a new installation
+    cy.task('deleteRelativePath', 'configuration.php');
+    cy.installJoomla(config);
 
     // Disable compat plugin
     cy.db_enableExtension(0, 'plg_behaviour_compat');
