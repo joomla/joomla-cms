61e1c6a171354285fd412a7387587724fadd131d
Fedik <getthesite@gmail.com>
2026-02-08 17:18:17 +0200

    Building build vendor clean

--- build/media_source/vendor/builder.mjs
+++ build/media_source/vendor/builder.mjs
@@ -114,48 +114,6 @@ const prepareVendorAssets = async (vendor, packageName) => {
 export default class VendorModuleBuilder extends DefaultModuleBuilder
 {
   /**
-   * List of Joomla custom stuff in /vendor dirs
-   * @returns { Promise<[]> }
-   * /
-  getExcludedFolders () {
-    return fsp.readdir(this.basePath, { recursive: false, withFileTypes: true })
-      .then((files) => {
-        const exclude = [];
-
-        files.forEach((file) => {
-          if (!file.isDirectory()) return;
-          exclude.push(file.name);
-        });
-
-        return exclude;
-      });
-  }
-
-  /**
-   * Remove files on target location
-   * @returns { Promise }
-   * /
-  async clear() {
-    // Remove all except Joomla custom
-    return this.getExcludedFolders().then((exclude) => {
-      // Lookup folders on target
-      return fsp.readdir(this.targetPath, { recursive: false, withFileTypes: true })
-        .then((files) => {
-          const rms = [];
-          files.forEach((file) => {
-            if (exclude.includes(file.name)) return;
-
-            rms.push(
-              fsp.rm(path.join(file.path, file.name), { recursive: true })
-            );
-          });
-
-          return Promise.all(rms);
-        });
-    });
-  }//*/
-
-  /**
    * Copy all vendor files according to Joomla's specs from build/settings.json
    * And create vendor/joomla.asset.json registry.
    *
