ecf224a7bd45768b39bca309fd2d658b4f2976f4
Fedik <getthesite@gmail.com>
2026-02-14 13:40:28 +0200

    test

--- tests/System/integration/administrator/components/com_templates/Styles.cy.js
+++ tests/System/integration/administrator/components/com_templates/Styles.cy.js
@@ -1,10 +1,6 @@
 describe('Test in backend that the styles list', () => {
   beforeEach(() => cy.doAdministratorLogin());
 
-  it('can do anything', () => {
-    cy.log('Doing test!!!!!');
-  });
-
   // it('can list site styles', () => {
   //   cy.visit('/administrator/index.php?option=com_templates&view=styles&client_id=0');
   //   cy.get('h1.page-title').should('contain.text', 'Templates: Styles (Site)');
@@ -15,16 +11,12 @@ describe('Test in backend that the styles list', () => {
   // });
 
   it('can list administrator styles', () => {
-    cy.log('Before visit URL');
     cy.visit('/administrator/index.php?option=com_templates&view=styles&client_id=1');
-    cy.log('Opened URL');
     cy.get('h1.page-title').should('contain.text', 'Templates: Styles (Administrator)');
     cy.get('#client_id option:selected').should('have.text', 'Administrator');
     cy.get('#styleList tbody a').contains('Atum - Default').click();
     cy.get('h1.page-title').should('contain.text', 'Templates: Edit Style');
-  //   cy.log('Before Cancel');
-  //   cy.clickToolbarButton('Cancel');
-  //   cy.log('After Cancel');
+    // cy.clickToolbarButton('Cancel');
   });
 
   // it('can select client', () => {
