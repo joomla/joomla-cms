7f850d995677286f0138ec2923af3b9e5e067ca0
Vikash Kumar <163628932+Vikash-Kumar-23@users.noreply.github.com>
2026-02-28 05:30:52 +0530

    Update libraries/src/MVC/Model/WorkflowBehaviorTrait.php
    Co-authored-by: Richard Fath <richard67@users.noreply.github.com>

--- libraries/src/MVC/Model/WorkflowBehaviorTrait.php
+++ libraries/src/MVC/Model/WorkflowBehaviorTrait.php
@@ -340,7 +340,7 @@ trait WorkflowBehaviorTrait
         $field->addAttribute('type', $this->workflowEnabled ? 'transition' : 'hidden');
         $field->addAttribute('label', 'COM_CONTENT_WORKFLOW_STAGE');
         $field->addAttribute('extension', $extension);
-        
+
         if ($this->workflowEnabled) {
             $field->addAttribute('layout', 'joomla.form.field.groupedlist-transition');
         }
