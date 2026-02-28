98cf8c1988c2fa84b80690edd9ba295a4dd1a256
Richard Fath <richard.fath@t-online.de>
2026-02-28 17:17:33 +0100

    Fix conflict resolution from previous branch update

--- build/build-modules-js/stylesheets/handle-css.mjs
+++ build/build-modules-js/stylesheets/handle-css.mjs
@@ -5,37 +5,18 @@ import { transform as transformCss, composeVisitors } from 'lightningcss';
 import { urlVersioning } from './css-versioning.mjs';
 
 const {
-  readFile, writeFile, ensureDir,
+  copy, readFile, writeFile, ensureDir,
 } = pkg;
 
 export const handleCssFile = async (file) => {
   const outputFile = file.replace(`${sep}build${sep}media_source${sep}`, `${sep}media${sep}`);
   try {
-    // CSS file, we will process the file and then minify it in place
+    // CSS file, we will copy the file and then minify it in place
     // Ensure that the directories exist or create them
     await ensureDir(dirname(outputFile), { recursive: true, mode: 0o755 });
 
-    let content = await readFile(file, { encoding: 'utf8' });
-
-    // To preserve the licence the comment needs to start at the beginning of the file
-    content = content.startsWith('@charset "UTF-8";\n') ? content.replace('@charset "UTF-8";\n', '') : content;
-
     if (file !== outputFile) {
-      const { code: css } = transformCss({
-        code: Buffer.from(content),
-        minify: false,
-        visitor: composeVisitors([urlVersioning(file)]), // Adds a hash to the url() parts of the static css
-      });
-
-      // Save optimized css file
-      await writeFile(
-        outputFile,
-        content.startsWith('@charset "UTF-8";')
-          ? css
-          : `@charset "UTF-8";
-${css}`,
-        { encoding: 'utf8', mode: 0o644 },
-      );
+      await copy(file, outputFile, { preserveTimestamps: true, overwrite: true });
     }
 
     let content = await readFile(file, { encoding: 'utf8' });
@@ -49,11 +30,13 @@ ${css}`,
       visitor: composeVisitors([urlVersioning(outputFile)]), // Adds a hash to the url() parts of the static css
     });
 
-    // Save minified css file
-    await writeFile(outputFile.replace('.css', '.min.css'), `@charset "UTF-8";${cssMin}`, { encoding: 'utf8', mode: 0o644 });
+    // Ensure the folder exists or create it
+    await writeFile(outputFile.replace('.css', '.min.css'), `@charset "UTF-8";${code}`, { encoding: 'utf8', mode: 0o644 });
 
+    // eslint-disable-next-line no-console
     console.log(`✅ CSS file copied/minified: ${file}`);
   } catch (err) {
+    // eslint-disable-next-line no-console
     console.log(err);
   }
 };
