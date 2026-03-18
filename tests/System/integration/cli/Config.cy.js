a2cd0b9611dd30246487e08aa1b7c854988c15be
Christian Heel <66922325+heelc29@users.noreply.github.com>
2026-03-18 15:14:46 -0500

    update test
    see #46876

--- tests/System/integration/cli/Config.cy.js
+++ tests/System/integration/cli/Config.cy.js
@@ -1,18 +1,18 @@
 describe('Test that console command config', () => {
   it('can get sitename', () => {
-    cy.exec(`php ${Cypress.env('cmsPath')}/cli/joomla.php config:get sitename`)
+    cy.exec(`php ${Cypress.expose('cmsPath')}/cli/joomla.php config:get sitename`)
       .its('stdout')
-      .should('contain', Cypress.env('sitename'));
+      .should('contain', Cypress.expose('sitename'));
   });
 
   it('can set sitename', () => {
-    cy.exec(`php ${Cypress.env('cmsPath')}/cli/joomla.php config:set sitename="${Cypress.env('sitename')}"`)
+    cy.exec(`php ${Cypress.expose('cmsPath')}/cli/joomla.php config:set sitename="${Cypress.expose('sitename')}"`)
       .its('stdout')
       .should('contain', '[OK] Configuration set');
   });
 
   it('can not set invalid database name', () => {
-    cy.exec(`php ${Cypress.env('cmsPath')}/cli/joomla.php config:set db=invalid`, { failOnNonZeroExit: false }).then((result) => {
+    cy.exec(`php ${Cypress.expose('cmsPath')}/cli/joomla.php config:set db=invalid`, { failOnNonZeroExit: false }).then((result) => {
       cy.wrap(result).its('exitCode')
         .should('equal', 4);
       cy.wrap(result).its('stdout')
