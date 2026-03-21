2e2a27796af7faaad67028503c567e2cfdd09927
Heiko Lübbe <github@heikol.de>
2026-03-21 10:18:59 +0100

    Joomla! 5.4.4

--- administrator/components/com_associations/tmpl/association/edit.php
+++ administrator/components/com_associations/tmpl/association/edit.php
@@ -44,11 +44,11 @@ $options = [
                     src="<?php echo Route::_($this->editUri . '&task=' . $this->typeName . '.edit&id=' . (int) $this->referenceId); ?>"
                     height="400" width="400"
                     data-action="edit"
-                    data-item="<?php echo $this->typeName; ?>"
-                    data-id="<?php echo $this->referenceId; ?>"
-                    data-title="<?php echo $this->referenceTitle; ?>"
-                    data-title-value="<?php echo $this->referenceTitleValue; ?>"
-                    data-language="<?php echo $this->referenceLanguage; ?>"
+                    data-item="<?php echo $this->escape($this->typeName); ?>"
+                    data-id="<?php echo $this->escape($this->referenceId); ?>"
+                    data-title="<?php echo $this->escape($this->referenceTitle); ?>"
+                    data-title-value="<?php echo $this->escape($this->referenceTitleValue); ?>"
+                    data-language="<?php echo $this->escape($this->referenceLanguage); ?>"
                     data-editurl="<?php echo Route::_($this->editUri); ?>">
                 </iframe>
             </div>
