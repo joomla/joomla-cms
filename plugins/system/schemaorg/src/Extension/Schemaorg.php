a1e0c00b60c1f90771bd9989891bea96388a528f
Mohsin Khan <mohsinkhanaiml@gmail.com>
2026-03-22 10:34:10 +0530

    Support array for image field in schemaorg plugin

--- plugins/system/schemaorg/src/Extension/Schemaorg.php
+++ plugins/system/schemaorg/src/Extension/Schemaorg.php
@@ -436,11 +436,29 @@ final class Schemaorg extends CMSPlugin implements SubscriberInterface, Dispatch
 
                 $itemSchema = $localSchema->toArray();
 
-                if (!empty($itemSchema['image'])) {
-                    $url = $itemSchema['image'] ?? '';
+                if (!empty($itemSchema['image']))
+                {
+                    $images = (array) $itemSchema['image'];
+                    $processedImages = [];
 
-                    if (!preg_match('#^(https?:)?//#i', $url)) {
-                        $itemSchema['image'] = Uri::root() . HTMLHelper::_('cleanImageUrl', $url)->url;
+                    foreach ($images as $img)
+                    {
+                        if (!is_string($img) || $img === '')
+                        {
+                            continue;
+                        }
+
+                        if (!preg_match('#^(https?:)?//#i', $img))
+                        {
+                            $img = Uri::root() . HTMLHelper::_('cleanImageUrl', $img)->url;
+                        }
+
+                        $processedImages[] = $img;
+                    }
+
+                    if (!empty($processedImages))
+                    {
+                        $schema['image'] = count($processedImages) === 1 ? $processedImages[0] : $processedImages;
                     }
                 }
 
