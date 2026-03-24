ad111b51e51d67a7966ec7ea04925031c2e4e650
chmst <dev@chmst.de>
2026-03-24 23:28:08 +0100

    onContentPrepare in mod_articles

--- modules/mod_articles/src/Helper/ArticlesHelper.php
+++ modules/mod_articles/src/Helper/ArticlesHelper.php
@@ -341,6 +341,7 @@ class ArticlesHelper implements DatabaseAwareInterface
                 // Extra content from events
 
                 $contentEvents = [
+                    'onContentPrepare'     => new Content\ContentPrepareEvent('onContentPrepare', $contentEventArguments),
                     'afterDisplayTitle'    => new Content\AfterTitleEvent('onContentAfterTitle', $contentEventArguments),
                     'beforeDisplayContent' => new Content\BeforeDisplayEvent('onContentBeforeDisplay', $contentEventArguments),
                     'afterDisplayContent'  => new Content\AfterDisplayEvent('onContentAfterDisplay', $contentEventArguments),
@@ -352,6 +353,7 @@ class ArticlesHelper implements DatabaseAwareInterface
                     $item->event->{$resultKey} = $results ? trim(implode("\n", $results)) : '';
                 }
             } else {
+                $item->event->onContentPrepare     = '';
                 $item->event->afterDisplayTitle    = '';
                 $item->event->beforeDisplayContent = '';
                 $item->event->afterDisplayContent  = '';
