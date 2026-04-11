78b20e7a4938300e73a618a2da0f36d4bc35684d
csgoat0 <elturkabdelrahman@gmain.com>
2026-04-11 06:20:28 +0200

    add #__content_preview_tokens table to postgresql

--- installation/sql/postgresql/extensions.sql
+++ installation/sql/postgresql/extensions.sql
@@ -219,6 +219,23 @@ CREATE TABLE IF NOT EXISTS "#__content_rating" (
 );
 
 --
+-- Table structure for table `#__content_preview_tokens`
+--
+
+CREATE TABLE IF NOT EXISTS "#__content_preview_tokens" (
+  "id" serial NOT NULL,
+  "token" varchar(64) NOT NULL DEFAULT '',
+  "user_id" integer NOT NULL DEFAULT 0,
+  "article_id" integer NOT NULL DEFAULT 0,
+  "expires" timestamp without time zone NOT NULL,
+  PRIMARY KEY ("id")
+);
+
+CREATE UNIQUE INDEX "#__content_preview_tokens_idx_token" ON "#__content_preview_tokens" ("token");
+CREATE INDEX "#__content_preview_tokens_idx_article_id" ON "#__content_preview_tokens" ("article_id");
+CREATE INDEX "#__content_preview_tokens_idx_expires" ON "#__content_preview_tokens" ("expires");
+
+--
 -- Table structure for table `#__finder_filters`
 --
 
