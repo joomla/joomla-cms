0241e89e97fe60096cfbf7dc4308cb21a3f0aa45
dawe78 <daniel@75.63.205.92.host.secureserver.net>
2026-04-23 11:07:23 +0200

    Fix PHPCS side-effects warning in Oauth2TokenField

--- administrator/components/com_config/src/Field/Oauth2TokenField.php
+++ administrator/components/com_config/src/Field/Oauth2TokenField.php
@@ -10,7 +10,9 @@
 
 namespace Joomla\Component\Config\Administrator\Field;
 
-defined('_JEXEC') or die;
+// phpcs:disable PSR1.Files.SideEffects
+\defined('_JEXEC') or die;
+// phpcs:enable PSR1.Files.SideEffects
 
 use Joomla\CMS\Form\FormField;
 use Joomla\CMS\Language\Text;
