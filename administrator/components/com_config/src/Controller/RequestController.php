3ad7635ead5db05e9029e5de6863c4da906968fe
Daniel Heinemann <32469032+dawe78@users.noreply.github.com>
2026-04-26 07:57:11 +0200

    Update administrator/components/com_config/src/Controller/RequestController.php
    Co-authored-by: Krishna Gandhi <krishnawork0911@gmail.com>

--- administrator/components/com_config/src/Controller/RequestController.php
+++ administrator/components/com_config/src/Controller/RequestController.php
@@ -272,7 +272,7 @@ class RequestController extends BaseController
     {
         $params = $this->app->getConfig();
 
-        $provider = (string) ($params->get('oauth2_provider') ?: 'microsoft');
+        $provider     = (string) ($params->get('oauth2_provider') ?: 'microsoft');
         $clientId = (string) $params->get('oauth2_client_id');
         $clientSecret = (string) $params->get('oauth2_client_secret');
         $refreshToken = (string) $params->get('oauth2_refresh_token');
