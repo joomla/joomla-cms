d7ee640f839a9e533c8da2af341be0e4f5766596
Allon Moritz <allon.moritz@digital-peak.com>
2026-05-06 10:36:15 +0200

    Reorder

--- tests/System/integration/install/Installation.cy.js
+++ tests/System/integration/install/Installation.cy.js
@@ -1,40 +1,4 @@
 describe('Install Joomla', () => {
-  it('Install Joomla', () => {
-    const config = {
-      sitename: Cypress.env('sitename'),
-      name: Cypress.env('name'),
-      username: Cypress.env('username'),
-      password: Cypress.env('password'),
-      email: Cypress.env('email'),
-      db_type: Cypress.env('db_type'),
-      db_host: Cypress.env('db_host'),
-      db_port: Cypress.env('db_port'),
-      db_user: Cypress.env('db_user'),
-      db_password: Cypress.env('db_password'),
-      db_name: Cypress.env('db_name'),
-      db_prefix: Cypress.env('db_prefix'),
-    };
-
-    // If exists, delete PHP configuration file to force a new installation
-    cy.task('deleteRelativePath', 'configuration.php');
-    cy.installJoomla(config);
-
-    // Disable compat plugin
-    cy.db_enableExtension(0, 'plg_behaviour_compat');
-
-    cy.doAdministratorLogin(config.username, config.password, false);
-    cy.cancelTour();
-    cy.disableStatistics();
-    cy.setErrorReportingToDevelopment();
-    cy.doAdministratorLogout();
-
-    // Setup mailing
-    cy.config_setParameter('mailonline', true);
-    cy.config_setParameter('mailer', 'smtp');
-    cy.config_setParameter('smtphost', Cypress.env('smtp_host'));
-    cy.config_setParameter('smtpport', Cypress.env('smtp_port'));
-  });
-
   it('blocks admin password with less than 12 characters', () => {
     cy.task('deleteRelativePath', 'configuration.php');
     cy.visit('/installation/index.php');
@@ -124,5 +88,41 @@ describe('Install Joomla', () => {
     cy.get('#step2').click();
 
     cy.get('#installStep3').should('be.visible');
+  });
+
+  it('Install Joomla', () => {
+    const config = {
+      sitename: Cypress.env('sitename'),
+      name: Cypress.env('name'),
+      username: Cypress.env('username'),
+      password: Cypress.env('password'),
+      email: Cypress.env('email'),
+      db_type: Cypress.env('db_type'),
+      db_host: Cypress.env('db_host'),
+      db_port: Cypress.env('db_port'),
+      db_user: Cypress.env('db_user'),
+      db_password: Cypress.env('db_password'),
+      db_name: Cypress.env('db_name'),
+      db_prefix: Cypress.env('db_prefix'),
+    };
+
+    // If exists, delete PHP configuration file to force a new installation
+    cy.task('deleteRelativePath', 'configuration.php');
+    cy.installJoomla(config);
+
+    // Disable compat plugin
+    cy.db_enableExtension(0, 'plg_behaviour_compat');
+
+    cy.doAdministratorLogin(config.username, config.password, false);
+    cy.cancelTour();
+    cy.disableStatistics();
+    cy.setErrorReportingToDevelopment();
+    cy.doAdministratorLogout();
+
+    // Setup mailing
+    cy.config_setParameter('mailonline', true);
+    cy.config_setParameter('mailer', 'smtp');
+    cy.config_setParameter('smtphost', Cypress.env('smtp_host'));
+    cy.config_setParameter('smtpport', Cypress.env('smtp_port'));
   });
 });
