ea928bc77a9bef423d2bb14e8786bdad2991b6f3
Allon Moritz <allon.moritz@digital-peak.com>
2026-05-06 09:12:07 +0200

    Do not go to database setup in installer when password has spaces at the beginning/end

--- tests/System/integration/install/Installation.cy.js
+++ tests/System/integration/install/Installation.cy.js
@@ -34,4 +34,95 @@ describe('Install Joomla', () => {
     cy.config_setParameter('smtphost', Cypress.env('smtp_host'));
     cy.config_setParameter('smtpport', Cypress.env('smtp_port'));
   });
+
+  it('blocks admin password with less than 12 characters', () => {
+    cy.task('deleteRelativePath', 'configuration.php');
+    cy.visit('/installation/index.php');
+    cy.get('#jform_site_name').clear().type(Cypress.env('sitename'));
+    cy.get('#step1').click();
+    cy.get('#jform_admin_user').clear().type(Cypress.env('name'));
+    cy.get('#jform_admin_username').clear().type(Cypress.env('username'));
+    cy.get('#jform_admin_email').clear().type(Cypress.env('email'));
+    cy.get('#jform_admin_password').clear().type('Short1!');
+    cy.get('#jform_admin_password').blur();
+
+    cy.contains('Password doesn\'t meet the site\'s requirements');
+
+    cy.get('#step2').click();
+
+    cy.get('#installStep3').should('not.be.visible');
+  });
+
+  it('allows admin password with 12 or more characters', () => {
+    cy.task('deleteRelativePath', 'configuration.php');
+    cy.visit('/installation/index.php');
+    cy.get('#jform_site_name').clear().type(Cypress.env('sitename'));
+    cy.get('#step1').click();
+    cy.get('#jform_admin_user').clear().type(Cypress.env('name'));
+    cy.get('#jform_admin_username').clear().type(Cypress.env('username'));
+    cy.get('#jform_admin_email').clear().type(Cypress.env('email'));
+    cy.get('#jform_admin_password').clear().type('ValidPass123!');
+    cy.get('#jform_admin_password').blur();
+
+    cy.get('#jform_admin_password').should('have.value', 'ValidPass123!');
+
+    cy.get('#step2').click();
+
+    cy.get('#installStep3').should('be.visible');
+  });
+
+  it('blocks admin password with spaces at the beginning', () => {
+    cy.task('deleteRelativePath', 'configuration.php');
+    cy.visit('/installation/index.php');
+    cy.get('#jform_site_name').clear().type(Cypress.env('sitename'));
+    cy.get('#step1').click();
+    cy.get('#jform_admin_user').clear().type(Cypress.env('name'));
+    cy.get('#jform_admin_username').clear().type(Cypress.env('username'));
+    cy.get('#jform_admin_email').clear().type(Cypress.env('email'));
+    cy.get('#jform_admin_password').clear().type(' ValidPass123!');
+    cy.get('#jform_admin_password').blur();
+    cy.get('#step2').click();
+
+    cy.contains('Password must not have spaces at the beginning or end');
+
+    cy.get('#step2').click();
+
+    cy.get('#installStep3').should('not.be.visible');
+  });
+
+  it('blocks admin password with spaces at the end', () => {
+    cy.task('deleteRelativePath', 'configuration.php');
+    cy.visit('/installation/index.php');
+    cy.get('#jform_site_name').clear().type(Cypress.env('sitename'));
+    cy.get('#step1').click();
+    cy.get('#jform_admin_user').clear().type(Cypress.env('name'));
+    cy.get('#jform_admin_username').clear().type(Cypress.env('username'));
+    cy.get('#jform_admin_email').clear().type(Cypress.env('email'));
+    cy.get('#jform_admin_password').clear().type('ValidPass123! ');
+    cy.get('#jform_admin_password').blur();
+
+    cy.contains('Password must not have spaces at the beginning or end');
+
+    cy.get('#step2').click();
+
+    cy.get('#installStep3').should('not.be.visible');
+  });
+
+  it('allows spaces in the middle of admin_password', () => {
+    cy.task('deleteRelativePath', 'configuration.php');
+    cy.visit('/installation/index.php');
+    cy.get('#jform_site_name').clear().type(Cypress.env('sitename'));
+    cy.get('#step1').click();
+    cy.get('#jform_admin_user').clear().type(Cypress.env('name'));
+    cy.get('#jform_admin_username').clear().type(Cypress.env('username'));
+    cy.get('#jform_admin_email').clear().type(Cypress.env('email'));
+    cy.get('#jform_admin_password').clear().type('Valid Pass 123!');
+    cy.get('#jform_admin_password').blur();
+
+    cy.get('#jform_admin_password').should('have.value', 'Valid Pass 123!');
+
+    cy.get('#step2').click();
+
+    cy.get('#installStep3').should('be.visible');
+  });
 });
