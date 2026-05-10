f8389a454544c4b7f950dcd6ae2f62285cd8b032
Nicola Galgano <optimus4joomla@gmail.com>
2026-05-10 09:06:51 +0200

    remove unused

--- plugins/fields/sql/tmpl/sql.php
+++ plugins/fields/sql/tmpl/sql.php
@@ -8,9 +8,8 @@
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
-defined('_JEXEC') or die;
+\defined('_JEXEC') or die;
 
-use Joomla\CMS\Factory;
 use Joomla\Database\ParameterType;
 
 $value = $field->value;
