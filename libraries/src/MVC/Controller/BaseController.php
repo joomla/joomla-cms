960cde73a7fe2fce5872fb9b94dc0f5fff402d01
Mahesh <maheshgite2811@gmail.com>
2026-05-11 22:25:50 +0530

    Simplify debug log JSON encoding

--- libraries/src/MVC/Controller/BaseController.php
+++ libraries/src/MVC/Controller/BaseController.php
@@ -545,7 +545,7 @@ class BaseController implements ControllerInterface, DispatcherAwareInterface, L
                         $context,
                         $id,
                         (int) $result,
-                        json_encode($values, JSON_UNESCAPED_SLASHES)
+                        json_encode($values)
                     ),
                     ['category' => 'controller']
                 );
@@ -918,7 +918,7 @@ class BaseController implements ControllerInterface, DispatcherAwareInterface, L
                         'Holding edit ID %s.%s %s',
                         $context,
                         $id,
-                        json_encode($values, JSON_UNESCAPED_SLASHES)
+                        json_encode($values)
                     ),
                     ['category' => 'controller']
                 );
@@ -1048,7 +1048,7 @@ class BaseController implements ControllerInterface, DispatcherAwareInterface, L
                         'Releasing edit ID %s.%s %s',
                         $context,
                         $id,
-                        json_encode($values, JSON_UNESCAPED_SLASHES)
+                        json_encode($values)
                     ),
                     ['category' => 'controller']
                 );
