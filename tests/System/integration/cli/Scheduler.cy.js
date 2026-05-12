8c1f195a56e9d73868136792931439611e2ae6f9
Nicola Galgano <optimus4joomla@gmail.com>
2026-05-12 11:39:20 +0200

    Add tests for updating scheduled task state

--- tests/System/integration/cli/Scheduler.cy.js
+++ tests/System/integration/cli/Scheduler.cy.js
@@ -4,4 +4,22 @@ describe('Test that console command scheduler', () => {
       .its('stdout')
       .should('contain', 'Rotate Logs');
   });
+
+  it('can update scheduled task state', () => {
+    cy.exec(`php ${Cypress.env('cmsPath')}/cli/joomla.php scheduler:state --id=1 --state=1 -n`)
+      .its('stdout')
+      .should('contain', 'Task ID 1 enabled.');
+  });
+
+  it('cannot update state of non-existent task', () => {
+    cy.exec(`php ${Cypress.env('cmsPath')}/cli/joomla.php scheduler:state --id=123 --state=1 -n`, { failOnNonZeroExit: false })
+      .its('stdout')
+      .should('contain', "Task ID '123' does not exist!");
+  });
+
+  it('cannot update to non-existent state for a task', () => {
+    cy.exec(`php ${Cypress.env('cmsPath')}/cli/joomla.php scheduler:state --id=1 --state=123 -n`, { failOnNonZeroExit: false })
+      .its('stdout')
+      .should('contain', 'Invalid state passed!');
+  });
 });
