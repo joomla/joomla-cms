8e698af6fb89f048400ee7307cdd91cd10285fe4
Allon Moritz <allon.moritz@digital-peak.com>
2026-05-13 14:48:50 +0200

    stan

--- libraries/src/Mail/MailTemplate.php
+++ libraries/src/Mail/MailTemplate.php
@@ -599,8 +599,7 @@ class MailTemplate
                 'htmlbody'    => $htmlbody,
                 'params'      => json_encode(['tags' => (array) $tags]),
                 'extension'   => explode('.', $key, 2)[0] ?? '',
-                'attachments' => '',
-                'params'      => json_encode(['tags' => (array) $tags]),
+                'attachments' => ''
             ]);
     }
 
