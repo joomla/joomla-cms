ffa47945ff88540aac422856fafbfc9fbfc9519e
Tom van der Laan <info@tlwebdesign.nl>
2026-05-17 07:31:07 +0200

    Update 6.2.0-2026-04-10.sql
    Co-authored-by: Richard Fath <richard67@users.noreply.github.com>

--- administrator/components/com_admin/sql/updates/mysql/6.2.0-2026-04-10.sql
+++ administrator/components/com_admin/sql/updates/mysql/6.2.0-2026-04-10.sql
@@ -1,1 +1,1 @@
-ALTER TABLE `#__modules_menu` ADD COLUMN `inherit` tinyint NOT NULL DEFAULT 0 AFTER `menuid`;
+ALTER TABLE `#__modules_menu` ADD COLUMN `inherit` tinyint NOT NULL DEFAULT 0 AFTER `menuid` /** CAN FAIL **/;
