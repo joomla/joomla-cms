7c5f82e51638dceb54dafeaf642e75c163d106d1
ManhThuan <manhthuan.2902@gmail.com>
2026-05-28 08:27:39 +0700

    [5.4] Restrict unserialize() to Registry class in JoomlaStorage::start()
    Adds the allowed_classes option to unserialize() in JoomlaStorage::start()
    so that only Joomla\Registry\Registry objects can be reconstituted from
    session data. A malicious or corrupted session value can no longer trigger
    PHP Object Injection; non-Registry payloads are silently discarded and the
    session falls back to an empty Registry, requiring the user to log in again.

--- libraries/src/Session/Storage/JoomlaStorage.php
+++ libraries/src/Session/Storage/JoomlaStorage.php
@@ -314,7 +314,11 @@ class JoomlaStorage extends NativeStorage
 
         // Try loading data from the session
         if (!empty($_SESSION['joomla'])) {
-            $this->data = unserialize(base64_decode($_SESSION['joomla']));
+            $data = unserialize(base64_decode($_SESSION['joomla']), ['allowed_classes' => [Registry::class]]);
+
+            if ($data instanceof Registry) {
+                $this->data = $data;
+            }
         }
     }
 }
---
+++ tests/Unit/Libraries/Cms/Session/Storage/JoomlaStorageTest.php
@@ -0,0 +1,123 @@
+<?php
+
+/**
+ * @package     Joomla.UnitTest
+ * @subpackage  Session
+ *
+ * @copyright   (C) 2024 Open Source Matters, Inc. <https://www.joomla.org>
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+namespace Joomla\Tests\Unit\Libraries\Cms\Session\Storage;
+
+use Joomla\Registry\Registry;
+use Joomla\Tests\Unit\UnitTestCase;
+
+/**
+ * Test class for \Joomla\CMS\Session\Storage\JoomlaStorage
+ *
+ * @package     Joomla.UnitTest
+ * @subpackage  Session
+ *
+ * @testdox     The JoomlaStorage
+ *
+ * @since       __DEPLOY_VERSION__
+ */
+class JoomlaStorageTest extends UnitTestCase
+{
+    /**
+     * Encodes a value the same way JoomlaStorage::close() does.
+     *
+     * @param   mixed  $value  The value to encode.
+     *
+     * @return  string
+     *
+     * @since   __DEPLOY_VERSION__
+     */
+    private function encodeSessionData($value): string
+    {
+        return base64_encode(serialize($value));
+    }
+
+    /**
+     * @testdox  loads a valid Registry from session data correctly
+     *
+     * @return void
+     * @since  __DEPLOY_VERSION__
+     */
+    public function testDeserializeAcceptsRegistry(): void
+    {
+        $registry = new Registry(['foo' => 'bar', 'nested' => ['key' => 'val']]);
+        $encoded  = $this->encodeSessionData($registry);
+
+        $result = unserialize(base64_decode($encoded), ['allowed_classes' => [Registry::class]]);
+
+        $this->assertInstanceOf(Registry::class, $result);
+        $this->assertSame('bar', $result->get('foo'));
+        $this->assertSame('val', $result->get('nested.key'));
+    }
+
+    /**
+     * @testdox  rejects a serialized object that is not a Registry
+     *
+     * @return void
+     * @since  __DEPLOY_VERSION__
+     */
+    public function testDeserializeRejectsArbitraryObject(): void
+    {
+        $malicious      = new \stdClass();
+        $malicious->cmd = 'rm -rf /';
+        $encoded        = $this->encodeSessionData($malicious);
+
+        $result = unserialize(base64_decode($encoded), ['allowed_classes' => [Registry::class]]);
+
+        $this->assertNotInstanceOf(Registry::class, $result);
+        $this->assertNotInstanceOf(\stdClass::class, $result);
+    }
+
+    /**
+     * @testdox  falls back to empty Registry when session contains a non-Registry object
+     *
+     * @return void
+     * @since  __DEPLOY_VERSION__
+     */
+    public function testStartFallsBackToEmptyRegistryOnInvalidData(): void
+    {
+        $malicious          = new \stdClass();
+        $malicious->payload = 'exploit';
+
+        $sessionValue = $this->encodeSessionData($malicious);
+
+        $data = unserialize(base64_decode($sessionValue), ['allowed_classes' => [Registry::class]]);
+
+        // Simulate the guard in JoomlaStorage::start()
+        $storage = new Registry();
+        if ($data instanceof Registry) {
+            $storage = $data;
+        }
+
+        $this->assertInstanceOf(Registry::class, $storage);
+        $this->assertEmpty($storage->toArray());
+    }
+
+    /**
+     * @testdox  falls back to empty Registry when session data is corrupted
+     *
+     * @return void
+     * @since  __DEPLOY_VERSION__
+     */
+    public function testStartFallsBackToEmptyRegistryOnCorruptedData(): void
+    {
+        $corrupted = base64_encode('not-valid-serialized-data!!!');
+
+        $data = @unserialize(base64_decode($corrupted), ['allowed_classes' => [Registry::class]]);
+
+        $storage = new Registry();
+        if ($data instanceof Registry) {
+            $storage = $data;
+        }
+
+        $this->assertInstanceOf(Registry::class, $storage);
+        $this->assertEmpty($storage->toArray());
+    }
+}
