00d568ba0ed90bc77699a4d34d26bb4c43f49384
Morteza Honar <126716171+mortezahonar@users.noreply.github.com>
2026-05-30 22:55:00 +0330

    Fix subpackage name in Localbusiness.php

--- plugins/schemaorg/localbusiness/src/Extension/Localbusiness.php
+++ plugins/schemaorg/localbusiness/src/Extension/Localbusiness.php
@@ -2,7 +2,7 @@
 
 /**
  * @package     Joomla.Plugin
- * @subpackage  Schemaorg.localbusiness
+ * @subpackage  Schemaorg.localBusiness
  *
  * @copyright   (C) 2026 Open Source Matters, Inc. <https://www.joomla.org>
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
