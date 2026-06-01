da31f14f6491fbc0837373cdebc67fdd95dc0aa2
Allon Moritz <allon.moritz@digital-peak.com>
2026-06-01 14:45:07 +0200

    move to install test

--- tests/System/support/index.js
+++ tests/System/support/index.js
@@ -1,13 +1,6 @@
 import('./commands.mjs');
 import('joomla-cypress');
 
-Cypress.on('window:before:load', (win) => {
-  // Disable CSS-native cross-document view transitions
-  const style = win.document.createElement('style');
-  style.textContent = '* { view-transition-name: none !important; }';
-  win.document.head?.appendChild(style);
-});
-
 before(() => {
   cy.task('startMailServer');
   cy.task('clearLogs');
