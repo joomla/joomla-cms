55f6783dfed19627a6a891db57947a91b1da57ea
Allon Moritz <allon.moritz@digital-peak.com>
2026-06-06 15:34:34 +0200

    copy paste

--- administrator/components/com_finder/src/Indexer/DebugAdapter.php
+++ administrator/components/com_finder/src/Indexer/DebugAdapter.php
@@ -144,7 +144,7 @@ abstract class DebugAdapter extends CMSPlugin
         parent::__construct($dispatcher, $config);
 
         if ($db === null) {
-            @trigger_error(__CLASS__. ': Database must be set, this will not be caught anymore in 7.0.', E_USER_DEPRECATED);
+            @trigger_error(__CLASS__. ': Database must be set, this will not be caught anymore in 9.0.', E_USER_DEPRECATED);
 
             $db = Factory::getContainer()->get(DatabaseInterface::class);
         }
