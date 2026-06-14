75ca02343d01e49285f4d1d078b68ca525121435
Olivier Buisard <olivier.buisard@simplifyyourweb.com>
2026-06-14 23:34:49 +0200

    Update administrator/modules/mod_healthcheck/services/provider.php
    Co-authored-by: Christian Heel <66922325+heelc29@users.noreply.github.com>

--- administrator/modules/mod_healthcheck/services/provider.php
+++ administrator/modules/mod_healthcheck/services/provider.php
@@ -31,7 +31,7 @@ return new class () implements ServiceProviderInterface {
      *
      * @since   __DEPLOY_VERSION__
      */
-    public function register(Container $container)
+    public function register(Container $container): void
     {
         $container->registerServiceProvider(new ModuleDispatcherFactory('\\Joomla\\Module\\Healthcheck'));
         $container->registerServiceProvider(new HelperFactory('\\Joomla\\Module\\Healthcheck\\Administrator\\Helper'));
