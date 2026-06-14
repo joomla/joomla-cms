c380ee6d70e2b9594f09aef5171a77a029f9d882
Christian Heel <66922325+heelc29@users.noreply.github.com>
2026-06-14 18:14:13 +0200

    add stylistic/eslint-plugin

--- build/eslint-tests.mjs
+++ build/eslint-tests.mjs
@@ -1,12 +1,17 @@
 import { defineConfig } from 'eslint/config';
+import stylistic from '@stylistic/eslint-plugin';
 
 export default defineConfig([
+  stylistic.configs.customize({
+    arrowParens: true,
+    braceStyle: '1tbs',
+    quoteProps: 'as-needed',
+    quotes: 'single',
+    semi: true,
+  }),
   {
-    files: [
-      'tests/**/*.js',
-      'tests/**/*.mjs',
-      'tests/**/*.es6',
-      'tests/**/*.vue',
-    ],
+    rules: {
+      '@stylistic/quotes': ['error', 'single', { avoidEscape: true }],
+    },
   },
 ]);
