09525fc6f24b0a461633f97b367aabae793a40ba
Richard Fath <richard.fath@t-online.de>
2026-06-27 09:19:50 +0200

    Joomla! 5.4.7

--- libraries/src/Version.php
+++ libraries/src/Version.php
@@ -66,7 +66,7 @@ final class Version
      * @var    string
      * @since  3.8.0
      */
-    public const EXTRA_VERSION = 'rc2-dev';
+    public const EXTRA_VERSION = '';
 
     /**
      * Development status.
@@ -74,7 +74,7 @@ final class Version
      * @var    string
      * @since  3.5
      */
-    public const DEV_STATUS = 'Development';
+    public const DEV_STATUS = 'Stable';
 
     /**
      * Code name.
@@ -90,7 +90,7 @@ final class Version
      * @var    string
      * @since  3.5
      */
-    public const RELDATE = '27-June-2026';
+    public const RELDATE = '7-July-2026';
 
     /**
      * Release time.
@@ -98,7 +98,7 @@ final class Version
      * @var    string
      * @since  3.5
      */
-    public const RELTIME = '07:01';
+    public const RELTIME = '16:00';
 
     /**
      * Release timezone.
