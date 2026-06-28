0a3fa987484bc5c7a965866ebd7454591092f8ad
Сергей <koreshs@mail.ru>
2026-06-29 00:28:25 +0300

    Update HtmlView.php

--- components/com_content/src/View/Archive/HtmlView.php
+++ components/com_content/src/View/Archive/HtmlView.php
@@ -157,8 +157,6 @@ class HtmlView extends BaseHtmlView
                 $item->parent_id = null;
             }
 
-            $item->event = new \stdClass();
-
             $dispatcher->dispatch(
                 'onContentPrepare',
                 new ContentPrepareEvent('onContentPrepare', ['context' => 'com_content.archive', 'subject' => $item, 'params' => $item->params, 'page' => 0])
@@ -178,6 +176,8 @@ class HtmlView extends BaseHtmlView
                 'beforeDisplayContent' => new BeforeDisplayEvent('onContentBeforeDisplay', $contentEventArguments),
                 'afterDisplayContent'  => new AfterDisplayEvent('onContentAfterDisplay', $contentEventArguments),
             ];
+
+            $item->event = new \stdClass();
 
             foreach ($contentEvents as $resultKey => $event) {
                 $results = $dispatcher->dispatch($event->getName(), $event)->getArgument('result', []);
