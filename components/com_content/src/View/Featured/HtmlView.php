a1f5dafbcc272c99efaca4eb181d5abd1afced77
Сергей <koreshs@mail.ru>
2026-06-28 04:50:46 +0300

    Update HtmlView.php

--- components/com_content/src/View/Featured/HtmlView.php
+++ components/com_content/src/View/Featured/HtmlView.php
@@ -213,7 +213,7 @@ class HtmlView extends BaseHtmlView
         }
 
         // Prepare category in dispatcher
-        $this->eventResult = new \stdClass();
+        $this->eventResult     = new \stdClass();
         $contentEventArguments = [
             'context' => 'com_content.featured',
             'subject' => $this,
