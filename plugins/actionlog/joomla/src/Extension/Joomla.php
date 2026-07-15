a0d969748f7c88da891ef2e0724a79b74c060bef
Nicola Galgano <optimus4joomla@gmail.com>
2026-07-15 10:00:12 +0200

    log block/unblock only 1

--- plugins/actionlog/joomla/src/Extension/Joomla.php
+++ plugins/actionlog/joomla/src/Extension/Joomla.php
@@ -306,7 +306,7 @@ final class Joomla extends ActionLogPlugin implements SubscriberInterface
         }
 
         $db    = $this->getDatabase();
-        $query = $db->getQuery(true)
+        $query = $db->createQuery()
             ->select($db->quoteName([$params->title_holder, $params->id_holder]))
             ->from($db->quoteName($params->table_name))
             ->whereIn($db->quoteName($params->id_holder), ArrayHelper::toInteger($pks));
@@ -678,9 +678,12 @@ final class Joomla extends ActionLogPlugin implements SubscriberInterface
         if ($task === 'block' || $task === 'unblock') {
             $messageLanguageKey = $task === 'block' ? 'PLG_ACTIONLOG_JOOMLA_USER_BLOCK' : 'PLG_ACTIONLOG_JOOMLA_USER_UNBLOCK';
             $message['action']  = $task;
+            $this->addLog([$message], $messageLanguageKey, $context, $userId);
+
+            return;
         }
 
-        $this->addLog([$message], $messageLanguageKey, $context, $userId);
+        
 
         // Check if on save a block / unblock has changed
         if ($action === 'update') {
@@ -864,7 +867,7 @@ final class Joomla extends ActionLogPlugin implements SubscriberInterface
 
         // Get the user id for the given username
         $db    = $this->getDatabase();
-        $query = $db->getQuery(true)
+        $query = $db->createQuery()
             ->select($db->quoteName(['id', 'username']))
             ->from($db->quoteName('#__users'))
             ->where($db->quoteName('username') . ' = :username')
