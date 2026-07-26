737f37ca62195ea2f294f3fb3c65f88181b4b961
Allon Moritz <allon.moritz@digital-peak.com>
2026-07-26 17:04:50 +0200

    Update administrator/components/com_admin/sql/updates/postgresql/6.2.0-2026-07-21.sql
    Co-authored-by: Richard Fath <richard67@users.noreply.github.com>

--- administrator/components/com_admin/sql/updates/postgresql/6.2.0-2026-07-21.sql
+++ administrator/components/com_admin/sql/updates/postgresql/6.2.0-2026-07-21.sql
@@ -2,4 +2,4 @@
 -- Add security column to updates table
 --
 
-ALTER TABLE "#__updates" ADD "security" smallint NULL DEFAULT NULL;
+ALTER TABLE "#__updates" ADD COLUMN "security" smallint NULL DEFAULT NULL /** CAN FAIL **/;
