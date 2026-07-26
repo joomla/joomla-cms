12df09fedfbcdc69021c7d6af6d7ed6d7ac44a02
Olivier <olivier.buisard@simplifyyourweb.com>
2026-07-26 14:15:31 -0400

    Removed renderTableCellContent, moved to layouts.

--- administrator/modules/mod_healthcheck/src/Helper/HealthCheckHelper.php
+++ administrator/modules/mod_healthcheck/src/Helper/HealthCheckHelper.php
@@ -12,8 +12,6 @@ namespace Joomla\Module\Healthcheck\Administrator\Helper;
 
 use Joomla\CMS\Application\CMSApplication;
 use Joomla\CMS\Factory;
-use Joomla\CMS\HTML\HTMLHelper;
-use Joomla\CMS\Language\Text;
 use Joomla\CMS\Plugin\PluginHelper;
 use Joomla\Event\DispatcherInterface;
 use Joomla\Module\Healthcheck\Administrator\Event\HealthChecksEvent;
@@ -318,7 +316,6 @@ class HealthCheckHelper
             'access'  => true,
             'class'   => null,
             'group'   => 'general',
-            'helper'  => $this,
         ];
 
         $requiredFields = [
@@ -441,94 +438,5 @@ class HealthCheckHelper
             $requiredFields,
             $application
         );
-    }
-
-    // Function to render cell content based on column type
-    public function renderTableCellContent($column, $item, $rowIndex)
-    {
-        $key   = $column['key'] ?? '';
-        $type  = $column['type'] ?? 'text';
-        $value = \is_object($item) ? ($item->$key ?? '') : ($item[$key] ?? '');
-
-        switch ($type) {
-            case 'badge':
-                $badgeClass = $column['badgeClass'] ?? 'secondary';
-                if (isset($column['badgeClass']) && \is_callable($column['badgeClass'])) {
-                    $badgeClass = \call_user_func($column['badgeClass'], $value, $item, $rowIndex);
-                }
-
-                return '<span class="badge bg-' . htmlspecialchars($badgeClass, ENT_QUOTES, 'UTF-8') . '">' . htmlspecialchars($value, ENT_QUOTES, 'UTF-8') . '</span>';
-
-            case 'link':
-                $url = $column['url'] ?? '';
-                if (isset($column['url']) && \is_callable($column['url'])) {
-                    $url = \call_user_func($column['url'], $value, $item, $rowIndex);
-                }
-                $title = $column['linkTitle'] ?? '';
-                if (isset($column['linkTitle']) && \is_callable($column['linkTitle'])) {
-                    $title = \call_user_func($column['linkTitle'], $value, $item, $rowIndex);
-                }
-
-                return '<a href="' . htmlspecialchars($url, ENT_QUOTES, 'UTF-8') . '"' .
-                    ($title ? ' title="' . htmlspecialchars($title, ENT_QUOTES, 'UTF-8') . '"' : '') . '>' .
-                    htmlspecialchars($value, ENT_QUOTES, 'UTF-8') . '</a>';
-
-            case 'date':
-                $format = $column['dateFormat'] ?? Text::_('DATE_FORMAT_LC4');
-
-                return HTMLHelper::_('date', $value, $format);
-
-            case 'boolean':
-                $trueText   = $column['trueText'] ?? Text::_('JYES');
-                $falseText  = $column['falseText'] ?? Text::_('JNO');
-                $trueClass  = $column['trueClass'] ?? 'success';
-                $falseClass = $column['falseClass'] ?? 'danger';
-                $isTrue     = (bool) $value;
-
-                return '<span class="badge bg-' . ($isTrue ? $trueClass : $falseClass) . '">' .
-                    ($isTrue ? $trueText : $falseText) . '</span>';
-
-            case 'progress':
-                $percentage    = (float) $value;
-                $progressClass = $column['progressClass'] ?? 'primary';
-                if (isset($column['progressClass']) && \is_callable($column['progressClass'])) {
-                    $progressClass = \call_user_func($column['progressClass'], $value, $item, $rowIndex);
-                }
-
-                return '<div class="progress" style="height: 20px;">
-                        <div class="progress-bar bg-' . htmlspecialchars($progressClass, ENT_QUOTES, 'UTF-8') . '"
-                             role="progressbar"
-                             style="width: ' . $percentage . '%;"
-                             aria-valuenow="' . $percentage . '"
-                             aria-valuemin="0"
-                             aria-valuemax="100">' . $percentage . '%
-                        </div>
-                    </div>';
-
-            case 'icon':
-                $iconClass = $column['iconClass'] ?? 'fas fa-info';
-                if (isset($column['iconClass']) && \is_callable($column['iconClass'])) {
-                    $iconClass = \call_user_func($column['iconClass'], $value, $item, $rowIndex);
-                }
-
-                return '<span class="' . htmlspecialchars($iconClass, ENT_QUOTES, 'UTF-8') . '" aria-hidden="true"></span>';
-
-            case 'custom':
-                if (isset($column['renderer']) && \is_callable($column['renderer'])) {
-                    return \call_user_func($column['renderer'], $value, $item, $rowIndex, $column);
-                }
-
-                return htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
-
-            case 'text':
-            default:
-                $maxLength = $column['maxLength'] ?? null;
-                $text      = htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
-                if ($maxLength && \strlen($text) > $maxLength) {
-                    $text = substr($text, 0, $maxLength) . '...';
-                }
-
-                return $text;
-        }
     }
 }
