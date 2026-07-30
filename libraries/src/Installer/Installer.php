ddc7bbb53e0d4e29a9dc7a851b688a24d62367a4
Raheem Elnashar <fastfast415@gmail.com>
2026-07-30 17:04:28 +0300

    fix misleading installer error when disk space/quota exceeded

--- libraries/src/Installer/Installer.php
+++ libraries/src/Installer/Installer.php
@@ -617,7 +617,11 @@ class Installer extends Adapter implements DatabaseAwareInterface
         if ($path && is_dir(Path::clean($path))) {
             $this->setPath('source', $path);
         } else {
-            $this->abort(Text::_('JLIB_INSTALLER_ABORT_NOINSTALLPATH'));
+            if (disk_free_space(dirname($path)) < 51200) {
+                $this->abort(Text::_('JLIB_INSTALLER_ABORT_DISK_FULL'));
+            }else{
+                $this->abort(Text::_('JLIB_INSTALLER_ABORT_NOINSTALLPATH'));
+            }
 
             return false;
         }
