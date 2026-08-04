3be279cc80cf59da5e9f9e25f8633b25763bcad4
Olivier <olivier.buisard@simplifyyourweb.com>
2026-08-04 13:27:26 -0400

    Fixed SQL to ensure failure fails silently.

--- administrator/components/com_admin/sql/updates/mysql/6.2.0-2026-06-14.sql
+++ administrator/components/com_admin/sql/updates/mysql/6.2.0-2026-06-14.sql
@@ -1,6 +1,8 @@
-INSERT INTO `#__extensions` (`package_id`, `name`, `type`, `element`, `folder`, `client_id`, `enabled`, `access`, `protected`, `locked`, `manifest_cache`, `params`, `custom_data`, `ordering`, `state`) VALUES
-(0, 'mod_healthcheck', 'module', 'mod_healthcheck', '', 1, 1, 1, 0, 1, '', '{"context":"general"}', '', 0, 0),
+INSERT IGNORE INTO `#__extensions` (`package_id`, `name`, `type`, `element`, `folder`, `client_id`, `enabled`, `access`, `protected`, `locked`, `manifest_cache`, `params`, `custom_data`, `ordering`, `state`) VALUES
+(0, 'mod_healthcheck', 'module', 'mod_healthcheck', '', 1, 1, 1, 0, 1, '', '{"context":"general"}', '', 0, 0);
+
+INSERT IGNORE INTO `#__extensions` (`package_id`, `name`, `type`, `element`, `folder`, `client_id`, `enabled`, `access`, `protected`, `locked`, `manifest_cache`, `params`, `custom_data`, `ordering`, `state`) VALUES
 (0, 'plg_healthcheck_usermaintenance', 'plugin', 'usermaintenance', 'healthcheck', 0, 1, 1, 0, 1, '', '{"context":"general"}', '', 0, 0);
 
-INSERT INTO `#__modules` (`title`, `note`, `content`, `ordering`, `position`, `checked_out`, `checked_out_time`, `publish_up`, `publish_down`, `published`, `module`, `access`, `showtitle`, `params`, `client_id`, `language`) VALUES
+INSERT IGNORE INTO `#__modules` (`title`, `note`, `content`, `ordering`, `position`, `checked_out`, `checked_out_time`, `publish_up`, `publish_down`, `published`, `module`, `access`, `showtitle`, `params`, `client_id`, `language`) VALUES
 ('Health Checks', '', '', 1, 'cpanel-healthcheck', 0, NULL, NULL, NULL, 1, 'mod_healthcheck', 1, 1, '{"context":"general"}', 1, '*');
