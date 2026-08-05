a69bce22eec002a2eb530e0c1cfda039e5b75873
Brian Teeman <brian@teeman.net>
2026-08-05 13:24:19 +0100

    fix mysql update query

--- administrator/components/com_admin/sql/updates/mysql/6.2.0-2026-08-05.sql
+++ administrator/components/com_admin/sql/updates/mysql/6.2.0-2026-08-05.sql
@@ -1,5 +1,8 @@
 --
--- Add security column to updates table
+-- Add show_subcat_desc param to com_content
 --
 
-ALTER TABLE "#__updates" ADD COLUMN "security" smallint NULL DEFAULT NULL /** CAN FAIL **/;
+UPDATE `#_extensions`
+SET `params` = JSON_SET(`params`, '$.show_subcat_image', '0')
+WHERE `element` = 'com_content'
+  AND `type` = 'component';
