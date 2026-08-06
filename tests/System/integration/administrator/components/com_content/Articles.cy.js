11cb2be9069865a345ee7447e9ac87daa74a019d
Reda <reda.mohamed.reda.201@gmail.com>
2026-08-06 16:06:16 +0300

    Fixed the four CI-failing category match tests.

--- tests/System/integration/administrator/components/com_content/Articles.cy.js
+++ tests/System/integration/administrator/components/com_content/Articles.cy.js
@@ -24,8 +24,8 @@ describe('Test in backend that the articles list', () => {
 
   it('shows category match filter options', () => {
     cy.get('.js-stools-btn-filter').click();
-    cy.get('#filter_category_match').should('contain.text', '- Select Category Match -');
-    cy.get('#filter_category_match').should('contain.text', 'Main Category');
+    cy.get('#filter_category_match option[value=""]').should('have.text', '- Select Category Match -');
+    cy.get('#filter_category_match option[value="1"]').should('have.text', 'Main Category');
     cy.get('#filter_category_match option[value="2"]').should('have.text', 'Additional Categories');
   });
 
