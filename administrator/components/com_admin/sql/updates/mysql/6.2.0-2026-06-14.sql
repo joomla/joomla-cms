e8f3b75f289d2e524c32e32cb3c91dc9f4957ed3
Olivier <olivier.buisard@simplifyyourweb.com>
2026-08-07 19:14:46 -0400

    Use proper queries to avoid duplicated data

--- administrator/components/com_admin/sql/updates/mysql/6.2.0-2026-06-14.sql
+++ administrator/components/com_admin/sql/updates/mysql/6.2.0-2026-06-14.sql
@@ -1,8 +1,7 @@
-INSERT IGNORE INTO `#__extensions` (`package_id`, `name`, `type`, `element`, `folder`, `client_id`, `enabled`, `access`, `protected`, `locked`, `manifest_cache`, `params`, `custom_data`, `ordering`, `state`) VALUES
-(0, 'mod_healthcheck', 'module', 'mod_healthcheck', '', 1, 1, 1, 0, 1, '', '{"context":"general"}', '', 0, 0);
+INSERT INTO `#__extensions` (`package_id`, `name`, `type`, `element`, `folder`, `client_id`, `enabled`, `access`, `protected`, `locked`, `manifest_cache`, `params`, `custom_data`, `ordering`, `state`)
+SELECT 0, 'mod_healthcheck', 'module', 'mod_healthcheck', '', 1, 1, 1, 0, 1, '', '{"context":"general"}', '', 0, 0
+WHERE NOT EXISTS (SELECT * FROM `#__extensions` g WHERE g.`element` = 'mod_healthcheck' AND g.`client_id` = 1);
 
-INSERT IGNORE INTO `#__extensions` (`package_id`, `name`, `type`, `element`, `folder`, `client_id`, `enabled`, `access`, `protected`, `locked`, `manifest_cache`, `params`, `custom_data`, `ordering`, `state`) VALUES
-(0, 'plg_healthcheck_usermaintenance', 'plugin', 'usermaintenance', 'healthcheck', 0, 1, 1, 0, 1, '', '{"context":"general"}', '', 0, 0);
-
-INSERT IGNORE INTO `#__modules` (`title`, `note`, `content`, `ordering`, `position`, `checked_out`, `checked_out_time`, `publish_up`, `publish_down`, `published`, `module`, `access`, `showtitle`, `params`, `client_id`, `language`) VALUES
-('Health Checks', '', '', 1, 'cpanel-healthcheck', 0, NULL, NULL, NULL, 1, 'mod_healthcheck', 1, 1, '{"context":"general"}', 1, '*');
+INSERT INTO `#__extensions` (`package_id`, `name`, `type`, `element`, `folder`, `client_id`, `enabled`, `access`, `protected`, `locked`, `manifest_cache`, `params`, `custom_data`, `ordering`, `state`)
+SELECT 0, 'plg_healthcheck_usermaintenance', 'plugin', 'usermaintenance', 'healthcheck', 0, 1, 1, 0, 1, '', '{"context":"general"}', '', 0, 0
+WHERE NOT EXISTS (SELECT * FROM `#__extensions` g WHERE g.`element` = 'usermaintenance' AND g.`folder` = 'healthcheck');
