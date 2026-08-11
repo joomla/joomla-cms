6b2ab0e8d62701bcfb55ae9cde85016dce96a114
Heiko Lübbe <github@heikol.de>
2026-08-11 19:12:52 +0200

    Joomla! 5.4.8 Release Candidate 1

--- administrator/components/com_menus/src/Model/ItemModel.php
+++ administrator/components/com_menus/src/Model/ItemModel.php
@@ -1060,7 +1060,7 @@ class ItemModel extends AdminModel
      *
      * @return  array  An array of menu item objects with id, title, and level properties.
      *
-     * @since   __DEPLOY_VERSION__
+     * @since   5.4.8
      */
     public function getParentItems(string $menutype): array
     {
