6f5ce270c92c89f2f3dcaaf43f858d114a08fbb3
Olivier <olivier.buisard@simplifyyourweb.com>
2026-08-11 22:26:44 -0400

    Renamed files so they are in the right order. Disabled the User Maintenance plugin.

--- administrator/components/com_admin/sql/updates/postgresql/6.2.0-2026-08-12.sql
+++ administrator/components/com_admin/sql/updates/postgresql/6.2.0-2026-08-12.sql
@@ -3,5 +3,5 @@ SELECT 0, 'mod_healthcheck', 'module', 'mod_healthcheck', '', 1, 1, 1, 0, 1, '',
 WHERE NOT EXISTS (SELECT * FROM "#__extensions" g WHERE g."element" = 'mod_healthcheck' AND g."client_id" = 1);
 
 INSERT INTO "#__extensions" ("package_id", "name", "type", "element", "folder", "client_id", "enabled", "access", "protected", "locked", "manifest_cache", "params", "custom_data", "ordering", "state")
-SELECT 0, 'plg_healthcheck_usermaintenance', 'plugin', 'usermaintenance', 'healthcheck', 0, 1, 1, 0, 1, '', '{"context":"general"}', '', 0, 0
+SELECT 0, 'plg_healthcheck_usermaintenance', 'plugin', 'usermaintenance', 'healthcheck', 0, 0, 1, 0, 1, '', '{"context":"general"}', '', 0, 0
 WHERE NOT EXISTS (SELECT * FROM "#__extensions" g WHERE g."element" = 'usermaintenance' AND g."folder" = 'healthcheck');
