1a38bc8fec89965cd3afba3502ae5ef091971406
Harald Leithner <leithner@itronic.at>
2026-08-27 09:46:03 +0200

    Remove old sql files

--- administrator/components/com_admin/sql/updates/mysql/6.2.0-2026-07-21.sql
+++
@@ -1,5 +0,0 @@
---
--- Add security column to updates table
---
-
-ALTER TABLE `#__updates` ADD COLUMN `security` TINYINT NULL DEFAULT NULL AFTER `extra_query` /** CAN FAIL **/;
--- administrator/components/com_admin/sql/updates/postgresql/6.2.0-2026-07-21.sql
+++
@@ -1,5 +0,0 @@
---
--- Add security column to updates table
---
-
-ALTER TABLE "#__updates" ADD COLUMN "security" smallint NULL DEFAULT NULL /** CAN FAIL **/;
