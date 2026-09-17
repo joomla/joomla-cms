ea9a442b36c3f02fd8d8c2c8b6d57ca2a3ad299f
Stefan Wendhausen <stefan.wendhausen@tec-promotion.de>
2026-09-17 17:28:32 +0200

    Joomla! 6.1.4 Release Candidate 1

--- administrator/components/com_banners/src/Model/TracksModel.php
+++ administrator/components/com_banners/src/Model/TracksModel.php
@@ -504,7 +504,7 @@ class TracksModel extends ListModel
      *
      * @return  string
      *
-     * @since   __DEPLOY_VERSION__
+     * @since   6.1.4
      */
     private function escapeCsvFormula(string $value): string
     {
--- libraries/src/Updater/Adapter/ExtensionAdapter.php
+++ libraries/src/Updater/Adapter/ExtensionAdapter.php
@@ -37,7 +37,7 @@ class ExtensionAdapter extends UpdateAdapter
      *
      * @var    array
      *
-     * @since  __DEPLOY_VERSION__
+     * @since  6.1.4
      */
     protected array $security = [];
 
