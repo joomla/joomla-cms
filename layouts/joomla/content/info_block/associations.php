0275144114a5f70e43cbd313724d342da82c1ecf
Brian Teeman <brian@teeman.net>
2026-09-19 10:56:35 +0100

    style

--- layouts/joomla/content/info_block/associations.php
+++ layouts/joomla/content/info_block/associations.php
@@ -27,14 +27,17 @@ use Joomla\CMS\Router\Route;
         <?php if ($display === 1 && $association['language']->image) : ?>
             <?php $flag = HTMLHelper::_('image', 'mod_languages/' . $association['language']->image . '.gif', $association['language']->title_native, ['title' => $association['language']->title_native], true); ?>
             <a href="<?php echo Route::_($association['item']); ?>"><?php echo $flag; ?></a>
-            <?php elseif ($display === 2) : ?>
-                <a href="<?php echo Route::_($association['item']); ?>"><?php echo $association['language']->title_native; ?></a>
-            <?php elseif ($display === 3) : ?>
-                <a href="<?php echo Route::_($association['item']); ?>"><?php echo $association['language']->title; ?></a>
-            <?php else : ?>
+        <?php else : ?>
             <?php $class = 'btn btn-secondary btn-sm btn-' . strtolower($association['language']->lang_code); ?>
-            <a class="<?php echo $class; ?>" title="<?php echo $association['language']->title_native; ?>" href="<?php echo Route::_($association['item']); ?>"><?php echo $association['language']->lang_code; ?>
-                <span class="visually-hidden"><?php echo $association['language']->title_native; ?></span>
+            <?php if ($display === 2) : ?>
+                <?php $label = $association['language']->title_native; ?>
+            <?php elseif ($display === 3) : ?>
+                <?php $label = $association['language']->title; ?>
+            <?php else : ?>
+                <?php $label = $association['language']->lang_code; ?>
+            <?php endif; ?>
+            <a class="<?php echo $class; ?>" title="<?php echo $association['language']->title_native; ?>" href="<?php echo Route::_($association['item']); ?>">
+                <?php echo $label; ?>
             </a>
         <?php endif; ?>
     <?php endforeach; ?>
